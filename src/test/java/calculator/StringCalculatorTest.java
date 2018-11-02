86cc1b4e9a2b9e7812049892a7d11d1cc85418ac
Summer <jihan805@coupang.com>
2018-11-02 22:05:48 +0900

    add string calculator

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void 덧셈(){
+        int result = StringCalculator.calculator("2 + 3");
+        assertThat(result).isEqualTo(5);
+    }
+
+    @Test
+    public void 뺄셈(){
+        int result = StringCalculator.calculator("5 - 3");
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    public void 곱셈(){
+        int result = StringCalculator.calculator("5 * 3");
+        assertThat(result).isEqualTo(15);
+    }
+
+    @Test
+    public void 나눗셈(){
+        int result = StringCalculator.calculator("5 / 3");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void 사칙연산(){
+        int result = StringCalculator.calculator("2 + 3 * 4 / 2");
+        assertThat(result).isEqualTo(10);
+    }
+}
