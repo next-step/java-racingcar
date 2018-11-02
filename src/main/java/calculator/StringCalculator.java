65f44a0cc39753aecbc23088b24b11afa4cc87e6
hwanseok Ju <hwandol@hwanseoks-MacBook-Pro.local>
2018-11-02 22:03:16 +0900

    add StringCalculator

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,27 @@
+package calculator;
+
+public class StringCalculator {
+    public static int calculate(String text) {
+        String[] values = text.split(" ");
+        int first = Integer.parseInt(values[0]);
+        int second = Integer.parseInt(values[2]);
+        return calculate(values[1], first, second);
+    }
+
+    private static int calculate(String value, int first, int second) {
+        if(value.equals("+")){
+            return first + second;
+        }
+        if(value.equals("-")){
+            return first - second;
+        }
+        if(value.equals("*")){
+            return first*second;
+        }
+        if(value.equals("/")){
+            return first/second;
+        }
+            return 0;
+    }
+
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void add() {
+    }
+}
