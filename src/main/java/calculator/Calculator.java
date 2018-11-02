1a4692bab31d795b77b26fd9ea1e85c0068155a2
Hwatu <phs1116@gmail.com>
2018-11-02 22:34:59 +0900

    Calculator, IntCalculator Interface 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,16 +3,7 @@ package calculator;
 /**
  * Created by hspark on 02/11/2018.
  */
-public class Calculator {
-	public static double add(double from, double to) {
-		return from + to;
-	}
-
-	public static double sub(double from, double to) {
-		return from - to;
-	}
-
-	public static double divide(double from, double to){
-		return from / to;
-	}
+@FunctionalInterface
+public interface Calculator<T, U, R> {
+	R calculate(T from, U to);
 }
