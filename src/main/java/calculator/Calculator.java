c49982aa58b37643d8a7b78a65bd2929031e7895
Dave <developerkhy@gmail.com>
2018-11-02 22:12:05 +0900

    add StringCalculator (#5)

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package calculator;
+
+public class Calculator {
+    public static int add(int i, int j) {
+        return i + j;
+    }
+
+    public static int min(int i, int j) {
+        return i-j;
+    }
+
+    public static int mul(int i, int j) {
+        return i*j;
+    }
+
+    public static int div(int i, int j) {
+        return i/j;
+    }
+}
