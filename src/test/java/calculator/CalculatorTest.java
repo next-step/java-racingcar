809df4a2c30f3976a38d28220204f528d6a32dfd
hyunwoo <hyunwoo9283@gmail.com>
2018-11-03 14:02:34 +0900

    문자열 계산기 구현

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,20 +7,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+    private int result;
+
     @Before
-    public void setup() {
-        System.out.println("before");
+    public void setUp() {
+        calculator = new Calculator();
     }
 
     @Test
-    public void add() {
-        int result = Calculator.add(3,5);
-        assertThat(result).isEqualTo(7);
+    public void 덧셈() {
+        result = calculator.add(3, 5);
+        assertThat(result).isEqualTo(8);
     }
 
     @Test
-    public void add2() {
-
+    public void 뺄셈() {
+        result = calculator.sub(5, 3);
+        assertThat(result).isEqualTo(2);
     }
 
+    @Test
+    public void 곱셈() throws Exception {
+        result = calculator.multiply(3, 4);
+        assertThat(result).isEqualTo(12);
+    }
+
+    @Test
+    public void 나눗셈() throws Exception {
+        result = calculator.divide(10, 2);
+        assertThat(result).isEqualTo(5);
+    }
 }
