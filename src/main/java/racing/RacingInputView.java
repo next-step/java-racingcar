b7bd4cda02dc3c646226d10ae5c9f5602e2185dd
korkorna <korkorna@gmail.com>
2018-11-04 17:18:03 +0900

    inputView에 n개 이름 입력 기능(콤마 기준으로 구분) 추가

--- src/main/java/racing/RacingInputView.java
+++ src/main/java/racing/RacingInputView.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 
 public class RacingInputView {
     private static Scanner scanner = new Scanner(System.in);
+    private final String NAME_SEPERATOR = ",";
+
+    public static String[] inputTryNames(String message) {
+        System.out.println(message);
+        return splitNames(scanner.next());
+    }
+
+    private static String[] splitNames(String text) {
+        return text.split(NAME_SEPERATOR);
+    }
 
     public static int inputTryNo(String message) {
         System.out.println(message);
         return scanner.nextInt();
     }
-
 }
