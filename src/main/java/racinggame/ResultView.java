fca704f987521a47af231811ca37890ec7f47e12
lalwr <lalwrj@gmail.com>
2018-11-04 13:23:05 +0900

    자동차 경주 개임 2단계 개발 수정

--- src/main/java/racinggame/ResultView.java
+++ src/main/java/racinggame/ResultView.java
@@ -1,8 +1,13 @@
 package racinggame;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ResultView {
+
+    private int maxLocation = 0;
+    private List<String> victoryCar;
 
     public void print(List<Car> cars) {
         for(int i =0; i < cars.size(); i++){
@@ -10,6 +15,43 @@ public class ResultView {
             printCar(cars.get(i));
         }
         System.out.println();
+        printVictoryCar(cars);
+        printVictoryCarPrint();
+    }
+
+    private void printVictoryCar(List<Car> cars) {
+        maxLcationSetting(cars);
+        victoryCarSetting(cars);
+    }
+
+    private void victoryCarSetting(List<Car> cars) {
+        victoryCar = new ArrayList();
+        for(int i =0; i < cars.size(); i++){
+            vicotryCar(cars.get(i));
+        }
+    }
+
+    private void vicotryCar(Car car) {
+        if(maxLocation == car.getLocation()){
+            victoryCar.add(car.getName());
+        }
+
+    }
+
+    private void printVictoryCarPrint() {
+        System.out.println(Arrays.toString(victoryCar.toArray()) + "가 최종 우승했습니다.");
+    }
+
+    private void maxLcationSetting(List<Car> cars) {
+        for(int i =0; i < cars.size(); i++){
+            maxLocation(cars.get(i).getLocation());
+        }
+    }
+
+    private void maxLocation(int location) {
+        if(maxLocation < location){
+            maxLocation = location;
+        }
     }
 
     private void printCarName(Car car) {
