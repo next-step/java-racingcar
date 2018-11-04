12ab0b9d6e3d22f23204fb42df5cdad1276da09a
Sihong Park <pozt1234@gmail.com>
2018-11-04 16:25:11 +0900

    refactor: InputView 유틸성 클래스로 변경

--- src/main/java/racinggame/RacingGameConsole.java
+++ src/main/java/racinggame/RacingGameConsole.java
@@ -6,10 +6,8 @@ import racinggame.domain.Car;
 public class RacingGameConsole {
 
 	public static void main(String[] args) {
-		InputView inputView = new InputView();
-
-		String carNames = inputView.inputString("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-		int time = inputView.inputNumber("시도할 회수는 몇 회 인가요?");
+		String carNames = InputView.inputString("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+		int time = InputView.inputNumber("시도할 회수는 몇 회 인가요?");
 		RacingGame racingGame = new RacingGame(carNames, time);
 
 		ResultView resultView = new ResultView();
