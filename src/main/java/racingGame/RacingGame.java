4038bcc5e700fd30eab9b80e16bc4401a8dd0c19
dotorify <dotorify@gmail.com>
2018-11-04 22:41:09 +0900

    init racing game

---
+++ src/main/java/racingGame/RacingGame.java
@@ -0,0 +1,13 @@
+package racingGame;
+
+import java.util.ArrayList;
+
+public class RacingGame {
+    private int time;
+    private ArrayList<Integer> carPositions;
+
+    public ArrayList<Integer> move() {
+        // TODO: 구현
+        return null;
+    }
+}
