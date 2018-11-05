6e0a2a87fec2881c86de6948b94576071cf89316
serena <noriming2@gmail.com>
2018-11-05 23:37:27 +0900

    add StringCalculator

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -8,20 +8,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CalculatorTest {
 
-    @Before
-    public void setup(){
-        System.out.println("before");
-    }
-    //Before는 테스트 실행전 매번 실행
-    //테스트의 실행순서는 랜덤임 그래서 테스트사이에 의존성이 있으면 안
     @Test
     public void add() {
         int result = Calculator.add(5,10);
         assertThat(result).isEqualTo(15);
-        System.out.println("add");
-    }
-    @Test
-    public void add2(){
-        System.out.println("add2");
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -8,24 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringCalculatorTest {
 
     @Test
-    public void 덧셈() {
-      int result = StringCalculator.calculate("3 + 3");
-      assertThat(result).isEqualTo(6);
+    public void 문자열계산_3반환(){
+        int result = StringCalculator.calculate("1 + 2 / 3 * 3");
+        assertThat(result).isEqualTo(3);
     }
     @Test
-    public void 뺄셈(){
-        int result = StringCalculator.calculate("3 - 3");
+    public void 빈문자열계산_0반환(){
+        int result = StringCalculator.calculate(" ");
         assertThat(result).isEqualTo(0);
-
     }
-    @Test
-    public void 나눗셈(){
-        int result = StringCalculator.calculate("5 / 1");
-        assertThat(result).isEqualTo(5);
-    }
-    @Test
-    public void 곱셈(){
-        int result = StringCalculator.calculate("2 * 2");
-        assertThat(result).isEqualTo(4);
+    @Test(expected = IllegalArgumentException.class)
+    public void 잘못된연산자_예외반환(){
+        StringCalculator.calculate("4 % 2");
     }
 }
