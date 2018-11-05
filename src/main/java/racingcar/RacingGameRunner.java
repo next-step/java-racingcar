c86e64efe0bd3427a2b94446bd0d6ff8e68e693a
teachd000000 <teachd000000@gmail.com>
2018-11-05 23:58:32 +0900

    클래스 메서드로 변경

--- src/main/java/racingcar/RacingGameRunner.java
+++ src/main/java/racingcar/RacingGameRunner.java
@@ -5,13 +5,11 @@ import java.util.List;
 public class RacingGameRunner {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        RacingGameOption option = inputView.inputRacingGameOption();
+        RacingGameOption option = InputView.inputRacingGameOption();
 
         RacingGame racingGame = new RacingGame(option);
         List<RacingGameResultSet> resultSets = racingGame.start();
 
-        ResultView resultView =  new ResultView();
-        resultView.showRacingGameResults(resultSets);
+        ResultView.showRacingGameResults(resultSets);
     }
 }
