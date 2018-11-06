7fd71ecb5c5d42ba32c7e62dcd71fc615d896760
teachd000000 <teachd000000@gmail.com>
2018-11-06 22:09:40 +0900

    랜덤숫자 생성 테스트 코드 추가

--- src/test/java/racingcar/RandomUtilsTest.java
+++ src/test/java/racingcar/RandomUtilsTest.java
@@ -1,5 +1,16 @@
-import static org.junit.Assert.*;
+package racingcar;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 public class RandomUtilsTest {
 
+    @Test
+    public void 랜덤숫자를_생성할수있다() {
+        assertThat(RandomUtils.generateRandomInt()).isNotNull();
+        assertThat(RandomUtils.generateRandomInt()).isPositive();
+
+    }
 }
