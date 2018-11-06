bcd55cfac66a36d0c3bd508e0c496cb9ef3c0671
teachd000000 <teachd000000@gmail.com>
2018-11-06 22:09:22 +0900

    Racing Car 테스트 코드 수정

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -10,21 +11,22 @@ public class RacingCarTest {
 
     @Test
     public void 레이싱카를_생성할수있다() {
-        RacingCar racingCar = RacingCar.create();
+        RacingCar racingCar = RacingCar.create("pobi");
         assertThat(racingCar).isNotNull();
     }
 
     @Test
     public void 여러개의_레이싱카를_생성할수있다() {
-        int numberOfCar = 5;
-        List<RacingCar> racingCars = RacingCar.create(numberOfCar);
+        List<String> racingCarNames = Arrays.asList("foo", "bar");
+
+        List<RacingCar> racingCars = RacingCar.create(racingCarNames);
         assertThat(racingCars).isNotNull();
-        assertThat(racingCars.size()).isEqualTo(numberOfCar);
+        assertThat(racingCars.size()).isEqualTo(racingCarNames.size());
     }
 
     @Test
     public void 레이싱카의_처음위치는_1이다() {
-        RacingCar racingCar = RacingCar.create();
+        RacingCar racingCar = RacingCar.create("pobi");
         assertThat(racingCar.getPosition()).isEqualTo(1);
     }
 }
