384b4817913c9a7ff92e23c747e72cc4edde7fc9
serena <noriming2@gmail.com>
2018-11-06 15:07:48 +0900

    add Racing Game 단위테스트

--- src/test/java/game/RacingGameTest.java
+++ src/test/java/game/RacingGameTest.java
@@ -1,5 +1,33 @@
-import static org.junit.Assert.*;
+package game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
+    RacingGame game;
 
+    @Before
+    public void setUp() {
+        game = new RacingGame(2, 5);
+    }
+
+    @Test
+    public void 랜덤값_0과9사이반환() {
+        int randomValue = game.getRandomValue();
+        assertThat(randomValue).isBetween(0, 9);
+    }
+
+    @Test
+    public void 이동한다(){
+        boolean value = game.isMove(5);
+        assertThat(value).isEqualTo(true);
+    }
+
+    @Test
+    public void 이동하지않는다(){
+        boolean value = game.isMove(0);
+        assertThat(value).isEqualTo(false);
+    }
 }
