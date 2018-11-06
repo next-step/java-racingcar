3a1d98521fbd47bda549f83930d5c9c281cfbbe1
korkorna <korkorna@gmail.com>
2018-11-06 15:59:33 +0900

    RacingRanking 테스트코드에 불필요한 인스턴스 변수 제거

--- src/test/java/racing/domain/RacingRankingTest.java
+++ src/test/java/racing/domain/RacingRankingTest.java
@@ -1,21 +1,16 @@
 package racing.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class RacingRankingTest {
-
-    private RacingGame game = null;
-    private RacingRanking ranking = null;
-    private String[] winners = null;
     private List<RacingCar> mockCars = null;
-    private int mockTimes = 3;
     
     @Before
     public void setup() {
@@ -24,18 +19,16 @@ public class RacingRankingTest {
 
     @Test
     public void 레이싱경기_1명_우승자_확인() {
-        ranking = new RacingRanking(testSetWinner1AndLoser1());
-        winners = ranking.getWinners();
+        RacingRanking ranking = new RacingRanking(testSetWinner1AndLoser1());
 
-        assertThat(Arrays.toString(winners)).isEqualTo("[winner]");
+        assertThat(Arrays.toString(ranking.getWinners())).isEqualTo("[winner]");
     }
 
     @Test
     public void 레이싱경기_2명_우승자_확인() {
-        ranking = new RacingRanking(testSetWinner2AndLoser1());
-        winners = ranking.getWinners();
+        RacingRanking ranking = new RacingRanking(testSetWinner2AndLoser1());
 
-        assertThat(Arrays.toString(winners)).isEqualTo("[winner1, winner2]");
+        assertThat(Arrays.toString(ranking.getWinners())).isEqualTo("[winner1, winner2]");
     }
 
     private List<RacingCar>  testSetWinner2AndLoser1() {
