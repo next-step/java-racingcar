0d93188e53c284196e2307a8ac6a05fef9ad0bdd
EXNTU 이경진 <exntu-metatron@googlegroups.com>
2018-11-06 11:40:03 +0900

    카레이서 VO 추가

---
+++ src/main/java/racing/CarRacer.java
@@ -0,0 +1,15 @@
+package racing;
+
+public class CarRacer {
+
+    private final String name;
+
+    public CarRacer(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
