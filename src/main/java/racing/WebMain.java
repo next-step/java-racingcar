916cd36bd67cc634953f700f077699987336048a
korkorna <korkorna@gmail.com>
2018-11-07 09:11:01 +0900

    route /result 구현

--- src/main/java/racing/WebMain.java
+++ src/main/java/racing/WebMain.java
@@ -7,30 +7,54 @@ import static spark.Spark.post;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.jetty.util.UrlEncoded;
+
+import racing.domain.RacingGame;
+import racing.domain.RacingRanking;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class WebMain {
 
     public static void main(String[] args) {
-        
+
         webServerConfigure();
-        
+
         routes();
     }
 
-    private static void routes() {       
-        get("/",  (req, res) -> {
+    private static void routes() {
+        get("/", (req, res) -> {
+            res.removeCookie("cars");
             return render(null, "index.html");
         });
-        
+
         post("/name", (req, res) -> {
-            String names = req.queryParams("names");
             Map<String, Object> model = new HashMap<>();
+            String names = req.queryParams("names");
+            res.cookie("cars", UrlEncoded.encodeString(names));
             model.put("cars", names.split(" "));
             return render(model, "game.html");
         });
 
+        get("/result", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            
+            int times = Integer.parseInt(req.queryParams("turn"));
+            String names = UrlEncoded.decodeString(req.cookie("cars"));
+
+            RacingGame racingGame = new RacingGame(times, names.split(" "));
+            
+            while (!racingGame.isFinish()) {
+                racingGame.move();
+            }
+            
+            RacingRanking racingRanking = new RacingRanking(racingGame.getResultOfTheGame());
+            
+            model.put("gameResults", racingGame.getResultOfTheGame());
+            model.put("winners", String.join(", ", racingRanking.getWinners()));
+            return render(model, "result.html");
+        });
     }
 
     private static void webServerConfigure() {
