dfd99a485d7af8787dcb61190699f49c9f7218d3
teachd000000 <teachd000000@gmail.com>
2018-11-07 12:29:11 +0900

    레이싱 게임 우승자 보여주는 기능 추가

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import java.text.MessageFormat;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
@@ -9,9 +10,10 @@ public class ResultView {
 
     }
 
-    public static void showRacingGameResultSet(List<RacingGameResultSet> racingGameResultSets) {
+    public static void showRacingGameResultSet(List<RacingGameResultSet> racingGameResultSets, List<RacingGameWinner> racingGameWinners) {
         System.out.println("\n실행결과");
         racingGameResultSets.forEach(ResultView::showRacingGameResult);
+        showRacingGameWinner(racingGameWinners);
     }
 
     private static void showRacingGameResult(RacingGameResultSet racingGameResultSet) {
@@ -21,6 +23,11 @@ public class ResultView {
 
     private static void printResultMessage(RacingGameResultSet.RacingGameResult racingGameResult) {
         String message = MessageFormat.format("{0}: {1}", racingGameResult.getName(), StringUtils.convertToHyphen(racingGameResult.getPosition()));
+        System.out.println(message);
+    }
+
+    private static void showRacingGameWinner(List<RacingGameWinner> racingGameWinners) {
+        String message = MessageFormat.format("{0}가 최종 우승했습니다.", racingGameWinners.stream().map(RacingGameWinner::getName).collect(Collectors.joining(", ")));
         System.out.println(message);
     }
 }
