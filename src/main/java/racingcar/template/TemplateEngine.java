f91b18f32d8e04d1f308b8c95486dfe02d11561a
teachd000000 <teachd000000@gmail.com>
2018-11-09 01:14:10 +0900

    템플릿 엔진 관련 함수 추가

---
+++ src/main/java/racingcar/template/TemplateEngine.java
@@ -0,0 +1,16 @@
+package racingcar.template;
+
+import spark.ModelAndView;
+
+import java.util.Map;
+
+public class TemplateEngine {
+
+    public static String render(Map<String, Object> model, String templatePath) {
+        return new BasicHandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
+
+    public static String render(String templatePath) {
+        return render(null, templatePath);
+    }
+}
