76df56149f4aa52e5f89cb312f412b2d9c6919c6
serena <noriming2@gmail.com>
2018-11-09 12:43:38 +0900

    add 우승자선별 클래스

--- src/main/java/game/Winners.java
+++ src/main/java/game/Winners.java
@@ -1,14 +1,34 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class SelectWinners {
-    List<Car> cars;
-    List<Car> winners;
-    public SelectWinners(List<Car> cars) {
-        this.cars = cars;
+public class Winners {
+
+    private static StringBuffer winners;
+
+    public static String findWinners(List<Car> cars) {
+        winners = new StringBuffer();
+        sortBiggest(cars);
+        Car car = cars.get(0);
+        for (Car target : cars) {
+            toWinners(car, target);
+        }
+        return trimString();
     }
 
-    public sortBiggest
+    private static String trimString(){
+        return winners.substring(0,winners.length()-2);
+    }
 
+    private static void sortBiggest(List<Car> cars) {
+        Collections.sort(cars);
+    }
+
+    private static void toWinners(Car car, Car target) {
+        if (car.isSameLocation(target)) {
+            winners.append(target.getName()+", ");
+        }
+    }
 }
