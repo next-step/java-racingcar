616cd4cca7bfde77062de743525492655dfac463
serena <noriming2@gmail.com>
2018-11-09 12:41:29 +0900

    add CarList반환 클래스

--- src/main/java/game/CarList.java
+++ src/main/java/game/CarList.java
@@ -1,4 +1,16 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarList {
+
+    public static List<Car> makeCars(String data) {
+        String[] names = data.split(",");
+        List<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
 }
