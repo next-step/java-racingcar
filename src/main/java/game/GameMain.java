c509536d76af457fa81cea45f058483f45881c9f
serena <noriming2@gmail.com>
2018-11-09 12:42:42 +0900

    update 우승자출력 및 Car 이름 입력

--- src/main/java/game/GameMain.java
+++ src/main/java/game/GameMain.java
@@ -3,14 +3,21 @@ package game;
 import java.util.List;
 
 public class GameMain {
-    public static void main(String[] args) {
-        int numberofCars = InputView.getNumberofCars();
-        int numberofAttempts = InputView.getNumberofAttempts();
 
-        RacingGame racingGame = new RacingGame(numberofCars);
-        for(int i = 0; i < numberofAttempts; i++){
-            List<Car> cars = racingGame.playGame();
+    public static void main(String[] args) {
+        String nameofCars = InputView.getNumberofCars();
+        int numberofAttempts = InputView.getNumberofAttempts();
+        List<Car> cars = null;
+
+        RacingGame racingGame = new RacingGame(nameofCars);
+        for (int i = 0; i < numberofAttempts; i++) {
+            cars = racingGame.playGame();
             OutputView.print(cars);
         }
+
+        String winners = Winners.findWinners(cars);
+        OutputView.printWinners(winners);
     }
 }
+
+
