298daf9ca0f3afb15466cee20c9c77a20dccc599
serena <noriming2@gmail.com>
2018-11-12 03:20:32 +0900

    update 테스트케이스 추가

--- src/test/java/game/WinnersTest.java
+++ src/test/java/game/WinnersTest.java
@@ -3,6 +3,7 @@ package game;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,12 +11,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WinnersTest {
 
     @Test
-    public void 리스트를넘기면_우승자반환() {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car(3,"A"));
-        cars.add(new Car(3,"B"));
-        cars.add(new Car(1,"C"));
+    public void 리스트를넘기면_공동우승자반환() {
+        List<Car> cars = Arrays.asList(new Car(3,"A"),new Car(3,"B"),new Car(1,"C"));
+
         String anwser = "A, B";
+        String winner = Winners.findWinners(cars);
+
+        assertThat(winner).isEqualTo(anwser);
+    }
+
+    @Test
+    public void 리스트를넘기면_단우승자반환() {
+        List<Car> cars = Arrays.asList(new Car(4,"A"),new Car(3,"B"),new Car(1,"C"));
+
+        String anwser = "A";
         String winner = Winners.findWinners(cars);
 
         assertThat(winner).isEqualTo(anwser);
