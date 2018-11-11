a34a7e82e740b10f4def6269ad1bfda29a0d160a
serena <noriming2@gmail.com>
2018-11-12 03:18:52 +0900

    update 상수추가

--- src/main/java/game/CarList.java
+++ src/main/java/game/CarList.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarList {
-
+    private static final String SPERATOR = ",";
     public static List<Car> makeCars(String data) {
-        String[] names = data.split(",");
+        String[] names = data.split(SPERATOR);
         List<Car> cars = new ArrayList<>();
         for (String name : names) {
             cars.add(new Car(name));
