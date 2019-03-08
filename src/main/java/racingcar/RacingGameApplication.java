c91fa6ab24e63f9783fa5284fdf25e362c191e73
devson <sonyc5720@gmail.com>
2019-03-09 02:10:01 +0900

    자동차 경주게임 실행용 메인클래스 생성

---
+++ src/main/java/racingcar/RacingGameApplication.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class RacingGameApplication {
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        racingGame.start();
+    }
+}
