f04e438140321448131226a091f78ee54cd599a1
crystal_rim <remember2889@gmail.com>
2019-03-08 22:02:04 +0900

    [UPDATE] implement calculator

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+    @Test
+    public void 덧셈() {
+        int result = StringCalculator.calculate("1 + 2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void 뺄셈() {
+        int result = StringCalculator.calculate("2 - 2");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void 곱셈() {
+        int result = StringCalculator.calculate("1 * 2");
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    public void 나눗셈() {
+        int result = StringCalculator.calculate("4 / 2");
+        assertThat(result).isEqualTo(2);
+    }
+}
