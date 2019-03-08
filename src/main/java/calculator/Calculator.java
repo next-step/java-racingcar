2ad4d9f6781670615e507b592217cdeb1e0a386f
hyoj <chohyojoy@gmail.com>
2019-03-08 22:01:05 +0900

    implement calculator

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package calculator;
+
+public class Calculator {
+    static int add(int i, int j) {
+        return i + j;
+    }
+
+    static int substract(int i, int j) {
+        return i - j;
+    }
+
+    static int supply(int i, int j) {
+        return i * j;
+    }
+
+    static int division(int i, int j) {
+        return i / j;
+    }
+}
