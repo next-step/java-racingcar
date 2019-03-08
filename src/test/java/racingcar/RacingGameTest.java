cda68482037f14e8f9a999e7dea19aa835a04fd1
devson <sonyc5720@gmail.com>
2019-03-09 02:06:28 +0900

    RacingGame 테스트코드 변경

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -21,26 +21,16 @@ public class RacingGameTest {
     }
 
     @Test
-    public void 자동차_전진() {
+    public void 레이스_종료_후_움직인거리가_시도횟수보다_크면_안됨() {
         RacingGame racingGame = new RacingGame();
         int numberOfCar = 3;
+        int tryCount = 5;
 
         Car[] cars = racingGame.carSetUp(numberOfCar);
+        racingGame.start(tryCount);
 
-        // 1회 전진
-        racingGame.run(cars);
-
-        for (int i = 0, loop = cars.length; i < loop; i++) {
-            assertThat(cars[i].getMovedDistance())
-                    .isLessThanOrEqualTo(1);
-        }
-
-        // 2회 전진
-        racingGame.run(cars);
-
-        for (int i = 0, loop = cars.length; i < loop; i++) {
-            assertThat(cars[i].getMovedDistance())
-                    .isLessThanOrEqualTo(2);
+        for (int i = 0; i < numberOfCar; i++) {
+            assertThat(cars[i].getMovedDistance()).isLessThanOrEqualTo(tryCount);
         }
     }
 
