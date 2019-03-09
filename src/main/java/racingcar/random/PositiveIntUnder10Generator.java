4406e90adf5f7997f96abf68523884645c6fa0db
devson <sonyc5720@gmail.com>
2019-03-09 19:06:39 +0900

    magic number 상수화

--- src/main/java/racingcar/random/PositiveIntUnder10Generator.java
+++ src/main/java/racingcar/random/PositiveIntUnder10Generator.java
@@ -3,8 +3,10 @@ package racingcar.random;
 import java.util.Random;
 
 public class PositiveIntUnder10Generator implements RandomIntGenerator {
+    private static final int BOUND = 10;
+
     @Override
     public int getRandomInt() {
-        return new Random().nextInt(10);
+        return new Random().nextInt(BOUND);
     }
 }
