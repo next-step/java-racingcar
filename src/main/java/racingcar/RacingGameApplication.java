19a1c1a5e629aafcd756a7e5db992e47059fa7aa
devson <sonyc5720@gmail.com>
2019-03-09 10:47:20 +0900

    RacingGameApplication 메서드 분리

--- src/main/java/racingcar/RacingGameApplication.java
+++ src/main/java/racingcar/RacingGameApplication.java
@@ -4,16 +4,37 @@ import java.util.Scanner;
 
 public class RacingGameApplication {
     public static void main(String[] args) {
+        RacingGameApplication.start();
+    }
+
+    public static void start() {
+        RacingGameParameter parameter = readRacingGameParameter();
+        RacingGame racingGame = new RacingGame(parameter.numberOfCar);
+        racingGame.start(parameter.tryCount);
+    }
+
+    private static class RacingGameParameter {
+        public RacingGameParameter(int numberOfCar, int tryCount) {
+            this.numberOfCar = numberOfCar;
+            this.tryCount = tryCount;
+        }
+
+        private int numberOfCar;
+        private int tryCount;
+    }
+
+    private static RacingGameParameter readRacingGameParameter() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("자동차 대수는 몇 대 인가요?");
         int numberOfCar = scanner.nextInt();
         System.out.println("시도할 회수는 몇 회 인가요?");
         int tryCount = scanner.nextInt();
+        System.out.println();
 
         scanner.close();
 
-        RacingGame racingGame = new RacingGame();
-        racingGame.start(numberOfCar, tryCount);
+        return new RacingGameParameter(numberOfCar, tryCount);
     }
+
 }
