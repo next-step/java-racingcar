e9626666a6da634e0e99315761055551ac0ea23f
JoonT92 <pk920207@gmail.com>
2019-03-09 11:41:49 +0900

    StringCalculator 완성

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,26 +1,53 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
-    static int calculate(String text){
-        String[] strs = text.split("");
+    static int calculate(String calculation){
+        if(calculation == null || calculation.equals(""))
+            throw new IllegalArgumentException();
 
-        int a = 0;
-        int b = 0;
-        for (int i = 0; i < strs.length; i++) {
+        int[] operands = extractOperands(calculation);
+        char[] operators = extractOperators(calculation);
 
+        if(operands.length == operators.length-1)
+            throw new IllegalStateException();
+
+        int result = operands[0];
+        for (int i = 1; i < operands.length; i++) {
+            result = calculate2Operands(result, operands[i], operators[i-1]);
         }
+
+        return result;
+    }
+
+    private static int calculate2Operands(int i, int j, char operator) {
+        if(operator == '+')
+            return i + j;
+        if(operator == '-')
+            return i - j;
+        if(operator == '*')
+            return i * j;
+        if(operator == '/')
+            return i / j;
 
         return 0;
     }
 
-    static int calculator(int i, int j, String expression){
-        if(expression.equals("+"))
-            return i + j;
-        if(expression.equals("-"))
-            return i - j;
-        if(expression.equals("*"))
-            return i * j;
-        if(expression.equals("/"))
-            return i / j;
+    private static int[] extractOperands(String calculation){
+        return Arrays.stream(calculation.split("[+\\-*/]"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private static char[] extractOperators(String calculation){
+        String[] operators = calculation.split("[0-9]+");
+
+        StringBuilder result = new StringBuilder();
+        for (String operator : operators) {
+            result.append(operator);
+        }
+
+        return result.toString().toCharArray();
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -5,14 +5,36 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
-    @Test
-    public void 덧셈() {
-        assertThat(StringCalculator.calculate("1+2")).isEqualTo(3);
-        assertThat(StringCalculator.calculate("1+2+3")).isEqualTo(6);
+    @Test(expected = IllegalArgumentException.class)
+    public void 잘못된입력(){
+        assertThat(StringCalculator.calculate("")).isEqualTo(0);
+        assertThat(StringCalculator.calculate(null)).isEqualTo(0);
     }
 
     @Test
-    public void 뺄셈() {
-        assertThat(StringCalculator.calculate("3-1")).isEqualTo(2);
+    public void 피연산자1개(){
+        assertThat(StringCalculator.calculate("1")).isEqualTo(1);
+        assertThat(StringCalculator.calculate("33")).isEqualTo(33);
+    }
+
+    @Test
+    public void 피연산자2개_1자리수() {
+        assertThat(StringCalculator.calculate("1+2")).isEqualTo(3);
+        assertThat(StringCalculator.calculate("2-1")).isEqualTo(1);
+        assertThat(StringCalculator.calculate("4*2")).isEqualTo(8);
+    }
+
+    @Test
+    public void 피연산자2개_2자리수이상() {
+        // 2글자 이상
+        assertThat(StringCalculator.calculate("22+22")).isEqualTo(44);
+        assertThat(StringCalculator.calculate("123*2")).isEqualTo(246);
+    }
+
+    @Test
+    public void 피연산자n개() {
+        assertThat(StringCalculator.calculate("1+2+10")).isEqualTo(13);
+        assertThat(StringCalculator.calculate("2/1+3*1000")).isEqualTo(5000);
+        assertThat(StringCalculator.calculate("44+11/5*20")).isEqualTo(220);
     }
 }
