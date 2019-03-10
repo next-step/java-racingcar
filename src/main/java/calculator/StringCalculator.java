4e8883bb36c89d533bf3547c2c34d7f9f73bee84
syeongee <dev.summer.kim@gmail.com>
2019-03-11 00:50:59 +0900

    null, 공백 입력시 예외처리 및 테스트 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,37 +1,38 @@
 package calculator;
 
 public class StringCalculator {
+
     static int calculate(String text) {
+
+        if(text == null || text.isEmpty()) {
+            throw new IllegalAccessError();
+        }
+
         String[] values = text.split(" ");
+        int result = Integer.parseInt(values[0]);
 
-        int value1 = Integer.parseInt(values[0]);
-        int value2 = Integer.parseInt(values[2]);
-
-        int result = calculate(values[1], value1, value2);
-
-        return result;
-
-
-    }
-
-    private static int calculate(String operation, int value1, int value2) {
-        int result = 0;
-
-        switch(operation) {
-            case "+" :
-                result = value1 + value2;
-                break;
-            case "-" :
-                result = value1 - value2;
-                break;
-            case "*" :
-                result = value1 * value2;
-                break;
-            case "/" :
-                result = value1 / value2;
+        for(int i = 1 ; i<values.length ; i = i + 2) {
+            int value = Integer.parseInt(values[i+1]);
+            result = calculate(values[i], result, value);
         }
 
         return result;
+    }
+
+    private static int calculate(String operation, int value1, int value2) {
+
+        switch(operation) {
+            case "+" :
+                return value1 + value2;
+            case "-" :
+                return value1 - value2;
+            case "*" :
+                return value1 * value2;
+            case "/" :
+                return value1 / value2;
+            default :
+                throw new IllegalAccessError();
+        }
     }
 
 }
