21ae54ad5ef9b7d91ebf5e4ab642ddda4139d566
devson <sonyc5720@gmail.com>
2019-03-10 20:17:51 +0900

    1. RacingGame 생성시 자동차의 수가 아닌 자동차이름의 List를 사용하도록 변경 2. 우승자 확인 테스트 추가

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -5,30 +5,39 @@ import racingcar.random.CarMoveThresholdGenerator;
 import racingcar.random.IntMoreThanCarMoveThresholdGenerator;
 import racingcar.random.RandomIntGenerator;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
+    private List<String> carNames = Arrays.asList("pobi", "crong", "honux");
 
     @Test
     public void RacingGame_생성_시_자동차_셋업() {
-        int numberOfCar = 3;
-        RacingGame racingGame = new RacingGame(numberOfCar);
+        RacingGame racingGame = new RacingGame(carNames);
         List<Car> cars = racingGame.getCars();
 
         assertThat(cars)
-                .hasSize(numberOfCar)
+                .hasSize(carNames.size())
                 .doesNotContain((Car) null);
     }
 
     @Test
+    public void RacingGame_생성_시_자동차_이름_셋업() {
+        RacingGame racingGame = new RacingGame(carNames);
+        List<Car> cars = racingGame.getCars();
+
+        assertThat(cars.stream().map(Car::getName))
+                .containsExactlyElementsOf(carNames);
+    }
+
+    @Test
     public void 전진하지않는_레이싱_경주() {
-        int numberOfCar = 3;
         int tryCount = 5;
         RandomIntGenerator randomIntGenerator = new CarMoveThresholdGenerator();
 
-        RacingGame racingGame = new RacingGame(numberOfCar, randomIntGenerator);
+        RacingGame racingGame = new RacingGame(carNames, randomIntGenerator);
         racingGame.start(tryCount);
 
         List<Car> cars = racingGame.getCars();
@@ -40,11 +49,10 @@ public class RacingGameTest {
 
     @Test
     public void 항상_전진하는_레이싱_경주() {
-        int numberOfCar = 3;
         int tryCount = 5;
         RandomIntGenerator randomIntGenerator = new IntMoreThanCarMoveThresholdGenerator();
 
-        RacingGame racingGame = new RacingGame(numberOfCar, randomIntGenerator);
+        RacingGame racingGame = new RacingGame(carNames, randomIntGenerator);
         racingGame.start(tryCount);
 
         List<Car> cars = racingGame.getCars();
@@ -52,5 +60,20 @@ public class RacingGameTest {
         cars.forEach(car -> {
             assertThat(car.getMovedDistance()).isEqualTo(tryCount);
         });
+    }
+
+    @Test
+    public void 우승자_확인() {
+        RacingGame racingGame = new RacingGame(carNames);
+        List<Car> cars = racingGame.getCars();
+
+        Car winner1 = cars.get(0);
+        Car winner2 = cars.get(1);
+        winner1.go();
+        winner2.go();
+
+        List<Car> winners = racingGame.getWinners();
+        assertThat(winners)
+                .containsExactly(winner1, winner2);
     }
 }
