9f93ccea51aacfe21908799b386d9de9e5abfbff
yesjehmi <yesjehmi@gmail.com>
2019-03-10 21:41:45 +0900

    car racing 1st step

---
+++ src/main/java/car/Car.java
@@ -0,0 +1,4 @@
+package car;
+
+public class Car {
+}
---
+++ src/main/java/car/RacingGame.java
@@ -0,0 +1,10 @@
+package car;
+
+public class RacingCar {
+    public static gameStart() {
+        //car count
+        
+
+        //try count
+    }
+}
