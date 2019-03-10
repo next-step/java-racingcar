ea410274a79bd87f5ab942d36d240ef7fbb1ccc1
devson <sonyc5720@gmail.com>
2019-03-10 20:17:23 +0900

    Car 생성자에 자동차 이름(name)을 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
+
+    private String carName = "pobi";
+
+    @Test
+    public void 자동차_생성_후_이름_확인() {
+        Car car = new Car(carName);
+        assertThat(car.getName()).isEqualTo(carName);
+    }
+
     @Test
     public void 전진() {
-        Car car = new Car();
+        Car car = new Car(carName);
         car.go();
 
         int movedDistance = car.getMovedDistance();
@@ -16,7 +25,7 @@ public class CarTest {
 
     @Test
     public void 전진_성공() {
-        Car car = new Car();
+        Car car = new Car(carName);
         int value = Car.MOVE_THRESHOLD + 1;
 
         car.goWhenGreaterThanThreshold(value);
@@ -25,7 +34,7 @@ public class CarTest {
 
     @Test
     public void 전진_실패() {
-        Car car = new Car();
+        Car car = new Car(carName);
         int value = Car.MOVE_THRESHOLD;
 
         car.goWhenGreaterThanThreshold(value);
