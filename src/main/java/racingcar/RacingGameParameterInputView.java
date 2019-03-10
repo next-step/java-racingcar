9d0f54ca11a5e6ad2ca5fd61288015b0d3258af4
devson <sonyc5720@gmail.com>
2019-03-10 20:20:15 +0900

    '자동차 댓수 -> 자동차 이름 리스트'로 변경으로 인한 로직 및 필드 변경

--- src/main/java/racingcar/RacingGameParameterInputView.java
+++ src/main/java/racingcar/RacingGameParameterInputView.java
@@ -2,20 +2,32 @@ package racingcar;
 
 import racingcar.vo.RacingGameParameter;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class RacingGameParameterInputView {
     public RacingGameParameter readRacingGameParameter() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int numberOfCar = scanner.nextInt();
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String carNamesString = scanner.nextLine();
         System.out.println("시도할 회수는 몇 회 인가요?");
         int tryCount = scanner.nextInt();
         System.out.println();
 
         scanner.close();
 
-        return new RacingGameParameter(numberOfCar, tryCount);
+        List<String> carNames = convertCarNamesStringToList(carNamesString);
+        return new RacingGameParameter(carNames, tryCount);
+    }
+
+    private List<String> convertCarNamesStringToList(String carNamesString) {
+        List<String> convertedCarNamesList = Arrays.stream(carNamesString.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        return convertedCarNamesList;
     }
 }
