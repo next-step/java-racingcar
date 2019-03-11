4381406054895f16ec13d46b6015d96d19db5dc3
nekisse <lsh891224@gmail.com>
2019-03-11 18:04:23 +0900

    step2 ***수정사항 -version2  버전 수정    Car.class 의 지우지 못한 부분 삭제.
    ***기존질문 + 미해결 질문 + 추가적인질문
    Car.class 의  canMove()(랜덤값로직?)에 대한 테스트코드를 생각해 보았는데 조금더 힌트를 주시면 감사하겠습니다.
    version2방식으로 나누는것이 더 좋은 방식인걸까요?

--- src/main/java/racingcar/racingcar_refactoring_version2/Car.java
+++ src/main/java/racingcar/racingcar_refactoring_version2/Car.java
@@ -1,12 +1,6 @@
 package racingcar.racingcar_refactoring_version2;
 
-import java.util.Random;
-
 public class Car {
-    //이동기능, 이동거리 기록
-    private static final int RULE_COUNT = 10;
-    private static final int RUNNABLE_RULE_COUNT = 3;
-
 
     private int movingDistance = 0;
 
@@ -28,7 +22,5 @@ public class Car {
         movingDistance++;
     }
 
-    public boolean canMove() {
-        return new Random().nextInt(RULE_COUNT) > RUNNABLE_RULE_COUNT;
-    }
+
 }
