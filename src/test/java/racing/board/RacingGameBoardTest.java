4ab2d81f9aa95e7925a68fdd9bfe6f169ac9a3a0
jcheolho <fnel159@naver.com>
2019-03-11 19:31:39 +0900

    step1: moveCars, createCars 단위 테스트 추가

--- src/test/java/racing/board/RacingGameBoardTest.java
+++ src/test/java/racing/board/RacingGameBoardTest.java
@@ -2,8 +2,10 @@ package racing.board;
 
 import org.junit.*;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,26 +15,57 @@ public class RacingGameBoardTest {
     public void test_차_생성() {
         int size = 3;
         RacingGameBoard board = new RacingGameBoard();
-        assertThat(board.createCars(size)).isEqualTo(size);
+        assertThat(board.createCars(size))
+                .isEqualTo(size);
     }
 
     @Test
-    public void test_모든_차가_이동가능한_보드() {
+    public void test_랜덤이동_0대_1회() {
+        RacingGameBoard board = new RacingGameBoard();
+        assertThat(board.moveCars())
+                .isEqualTo(Collections.emptyList());
+    }
+
+    @Test
+    public void test_랜덤이동_3대_1회() {
+        RacingGameBoard board = new RacingGameBoard();
+        int size = board.createCars(3);
+        List<Integer> positions = board.moveCars();
+
+        assertThat(positions.size())
+                .isEqualTo(size);
+
+        for (int position : positions) {
+            assertThat(position).isGreaterThanOrEqualTo(0);
+        }
+    }
+
+    @Test
+    public void test_1칸이동_3대_3회() {
         RacingGameBoard board = new RacingGameBoard(() -> 10);
-        board.createCars(5);
-        board.moveCars();
-        board.moveCars();;
-        List<Integer> positions = board.moveCars();
-        assertThat(positions).isEqualTo(Arrays.asList(3, 3, 3, 3, 3));
+        int size = board.createCars(3);
+        int time = 3;
+        for (int position = 1; position <= time; position++) {
+            assertThat(board.moveCars())
+                    .isEqualTo(getPositions(size, position));
+        }
     }
 
     @Test
-    public void test_모든_차가_이동불가능한_보드() {
+    public void test_0칸이동_3대_3회() {
         RacingGameBoard board = new RacingGameBoard(() -> 3);
-        board.createCars(5);
-        board.moveCars();
-        board.moveCars();
-        List<Integer> positions = board.moveCars();
-        assertThat(positions).isEqualTo(Arrays.asList(0, 0, 0, 0, 0));
+        int size = board.createCars(3);
+        int time = 3;
+        for (int positoin = 1; positoin <= time; positoin++) {
+            assertThat(board.moveCars())
+                    .isEqualTo(getPositions(size, 0));
+        }
+    }
+
+    private List<Integer> getPositions(int size, int value) {
+        return IntStream.range(0, size)
+                .boxed()
+                .map(i -> value)
+                .collect(Collectors.toList());
     }
 }
