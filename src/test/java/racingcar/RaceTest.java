ddbc1e086511997127fea73c85da51fc572136b7
nekisse <lsh891224@gmail.com>
2019-03-11 14:56:02 +0900

    step2 - RaceTest.class 수정

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,24 +1,25 @@
 package racingcar;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RaceTest {
+    private String inputThreeCarName = "가,나,다 ";
+
+
 
     @Test
     public void 자동차대수_셋업() {
-        Race race = new Race("소나타");
-        assertThat(race.getCars().size()).isEqualTo(1);
+        Race race = new Race(inputThreeCarName);
+        assertThat(race.getCars().size()).isEqualTo(3);
     }
 
 
     @Test
-    @Ignore
     public void canMove가_true_일때_모든_자동차들이_이동한다() {
         boolean canMove = true;
-        Race race = new Race("소나타") {
+        Race race = new Race(inputThreeCarName) {
             @Override
             public void run() {
                 for (Car car : getCars()) {
@@ -31,20 +32,20 @@ public class RaceTest {
         race.run();
         race.run();
         race.run();
-        assertThat(race.getCarList().get(0).getMovingDistance()).isEqualTo(3);
-        assertThat(race.getCarList().get(1).getMovingDistance()).isEqualTo(3);
-        assertThat(race.getCarList().get(2).getMovingDistance()).isEqualTo(3);
+        assertThat(race.getCars().size()).isEqualTo(3);
+        assertThat(race.getCars().get(0).getMovingDistance()).isEqualTo(3);
+        assertThat(race.getCars().get(1).getMovingDistance()).isEqualTo(3);
+        assertThat(race.getCars().get(2).getMovingDistance()).isEqualTo(3);
     }
 
 
     @Test
-    @Ignore
-    public void canMove가_false_일때_모든_자동차들이_이동한다() {
+    public void canMove가_false_일때_모든_자동차들이_이동하지않는다() {
         boolean canMove = false;
-        Race race = new Race(3) {
+        Race race = new Race(inputThreeCarName) {
             @Override
             public void run() {
-                for (Car car : getCarList()) {
+                for (Car car : getCars()) {
                     if (canMove) {
                         car.moveCar();
                     }
@@ -54,6 +55,7 @@ public class RaceTest {
         race.run();
         race.run();
         race.run();
+        assertThat(race.getCars().size()).isEqualTo(3);
         assertThat(race.getCars().get(0).getMovingDistance()).isEqualTo(0);
         assertThat(race.getCars().get(1).getMovingDistance()).isEqualTo(0);
         assertThat(race.getCars().get(2).getMovingDistance()).isEqualTo(0);
