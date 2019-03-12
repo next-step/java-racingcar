80d11e7553723adbd56a67a79f657e23c9b2f4a8
devson <sonyc5720@gmail.com>
2019-03-12 14:49:11 +0900

    getWinners의 인자를 List<Car>에서 RacingResultOfRound로 변경

--- src/main/java/racingcar/domain/RacingGameJudge.java
+++ src/main/java/racingcar/domain/RacingGameJudge.java
@@ -1,19 +1,23 @@
-package racingcar;
+package racingcar.domain;
+
+import racingcar.vo.RacingResultOfRound;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingGameJudge {
 
-    public List<Car> getWinners(List<Car> cars) {
-        int maxMovedDistance = getMaxMovedDistanceOfCars(cars);
+    public List<Car> getWinners(RacingResultOfRound racingResultOfRound) {
+        List<Car> cars = racingResultOfRound.getCarsOfRound();
+
+        int maxMovedDistance = getMaxMovedDistanceFromCars(cars);
 
         return cars.stream()
                 .filter(car -> maxMovedDistance == car.getMovedDistance())
                 .collect(Collectors.toList());
     }
 
-    private int getMaxMovedDistanceOfCars(List<Car> cars) {
+    private int getMaxMovedDistanceFromCars(List<Car> cars) {
         return cars.stream()
                 .mapToInt(Car::getMovedDistance)
                 .max()
