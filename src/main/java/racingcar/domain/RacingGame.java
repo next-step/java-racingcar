9392c970262d2f024252297e0a82197bbe61dae7
devson <sonyc5720@gmail.com>
2019-03-12 15:24:21 +0900

    1. 현재 RacingGame의 상태를 담기위한 RacingGameInfo를 필드에 추가 2. 다음 라운드 실행 여부를 알려주는 hasNextRound 메서드 추가 3. 1회 실행(runOnce) 후 해당 라운드의 경주 결과(RacingResultOfRound) 리턴하는 로직 추가 4. 해당 RacingGame을 초기화(initializeRacing) 메서드 추가

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,85 @@
+package racingcar.domain;
+
+import racingcar.random.PositiveIntUnder10Generator;
+import racingcar.random.RandomIntGenerator;
+import racingcar.vo.RacingGameParameter;
+import racingcar.vo.RacingGameRound;
+import racingcar.vo.RacingResultOfRound;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingGame {
+    private RacingGameInfo racingGameInfo;
+    private RandomIntGenerator randomIntGenerator;
+
+    public RacingGame(RacingGameParameter parameter) {
+        this(parameter, new PositiveIntUnder10Generator());
+    }
+
+    public RacingGame(RacingGameParameter parameter, RandomIntGenerator randomIntGenerator) {
+        List<String> carNames = parameter.getCarNames();
+        List<Car> cars = carSetUp(carNames);
+
+        int totalRound = parameter.getTotalRound();
+        RacingGameRound racingGameRound = new RacingGameRound(totalRound);
+
+        this.racingGameInfo = new RacingGameInfo(cars, racingGameRound);
+        this.randomIntGenerator = randomIntGenerator;
+    }
+
+    private List<Car> carSetUp(List<String> carNames) {
+        return carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public int getTotalRound() {
+        return this.racingGameInfo
+                .getRacingGameRound()
+                .getTotalRound();
+    }
+
+    public int getCurrentRound() {
+        return this.racingGameInfo
+                .getRacingGameRound()
+                .getCurrentRound();
+    }
+
+    public List<Car> getCars() {
+        return this.racingGameInfo.getCars();
+    }
+
+    public RacingResultOfRound runOnce() {
+        if (!hasNextRound()) {
+            throw new IllegalStateException("종료된 경주 입니다. 초기화를 해주세요.");
+        }
+
+        List<Car> cars = getCars();
+
+        cars.forEach(car -> {
+            int randomInt = this.randomIntGenerator.getRandomInt();
+            car.goWhenGreaterThanThreshold(randomInt);
+        });
+
+        int currentRound = getCurrentRound();
+        List<Car> carsOfRound = copyCarList(cars);
+
+        this.racingGameInfo.incrementCurrentRound();
+        return new RacingResultOfRound(carsOfRound, currentRound);
+    }
+
+    private List<Car> copyCarList(List<Car> originalCars) {
+        return originalCars.stream()
+                .map(Car::copy)
+                .collect(Collectors.toList());
+    }
+
+    public boolean hasNextRound() {
+        return this.racingGameInfo.hasNextRound();
+    }
+
+    public void initializeRacingGame() {
+        this.racingGameInfo.initialize();
+    }
+}
