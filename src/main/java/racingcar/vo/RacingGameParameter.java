280846f61f98848d17457996f8a2c6d595027ac0
devson <sonyc5720@gmail.com>
2019-03-12 14:20:15 +0900

    필드: tryCount -> totalRound로 변경

--- src/main/java/racingcar/vo/RacingGameParameter.java
+++ src/main/java/racingcar/vo/RacingGameParameter.java
@@ -1,21 +1,22 @@
 package racingcar.vo;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RacingGameParameter {
     private final List<String> carNames;
-    private final int tryCount;
+    private final int totalRound;
 
-    public RacingGameParameter(List<String> carNames, int tryCount) {
+    public RacingGameParameter(List<String> carNames, int totalRound) {
         this.carNames = carNames;
-        this.tryCount = tryCount;
+        this.totalRound = totalRound;
     }
 
     public List<String> getCarNames() {
-        return carNames;
+        return Collections.unmodifiableList(carNames);
     }
 
-    public int getTryCount() {
-        return tryCount;
+    public int getTotalRound() {
+        return totalRound;
     }
 }
