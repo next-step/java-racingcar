d791eb49caca8b9b050e9206371602a42bac08c0
isisrin <isisrin@nate.com>
2019-03-12 16:02:18 +0900

    racing Game 피드백 반영

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,9 +1,11 @@
+
 package racingcar;
 
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import racingcar.domain.RacingGame;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,38 +15,12 @@ public class RacingGameTest {
 
     @Before
     public void setUp() {
-        racingGame = new RacingGame();
+        racingGame = new RacingGame(3, 5);
     }
 
+    //여기는 테스트 코드를 어떻게 짜면 좋을지;;;
     @Test
-    public void 게임세팅() {
-        racingGame.setGame(3, 5);
-        assertThat(racingGame.getCarPositions().size()).isEqualTo(3);
-        assertThat(racingGame.getTime()).isEqualTo(5);
-    }
-
-    @Test
-    public void 새포지션_테스트() {
-        int newPosition = racingGame.createNewPosition();
-        assertThat(newPosition).isBetween(0,9);
-    }
-
-    @Test
-    public void 달리기() {
-        int result = racingGame.move(0);
-        System.out.println("이건.. 어떻게 assert해야 할까요;; "  + result);
-    }
-
-    @Test
-    public void 한라운드_결과() {
-        racingGame.setGame(1,1);
-        racingGame.playRound();
-        System.out.println("랜덤값.. 테스트 코드... 어떻게... 확인하나요;;;  " + racingGame.getCarPositions());
-    }
-
-    @Test
-    public void 게임결과() {
-        racingGame.setGame(3,5);
+    public void 게임시작() {
         racingGame.play();
     }
 
