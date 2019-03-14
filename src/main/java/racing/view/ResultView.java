205eff0ddece14a1ad0a4c2d898ad4e3a4009636
jcheolho <fnel159@naver.com>
2019-03-15 03:14:22 +0900

    step3: 콘솔 ResultView에서 StepResult를 이용해 history 출력하도록 변경

--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -2,8 +2,10 @@ package racing.view;
 
 
 import racing.board.GameResult;
+import racing.board.StepResult;
 import racing.model.RacingCar;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -15,24 +17,29 @@ public class ResultView {
         System.out.println("\n실행 결과");
     }
 
-    public static void viewRunningStep(String name, Integer position) {
-        System.out.println(name + " : " + convertPosition(position));
-    }
-
-    private static String convertPosition(int position) {
-        return IntStream.range(0, position)
-                .mapToObj(p -> CHAR_TO_CONVERT)
-                .collect(Collectors.joining());
-    }
-
-    public static void viewEndingOfStep() {
-        System.out.println();
-    }
-
     public static void viewWinners(GameResult gameResult) {
         System.out.printf("%s가 최종 우승했습니다.\n ", gameResult.getWinners()
                 .stream()
                 .map(RacingCar::toString)
                 .collect(Collectors.joining(", ")));
+    }
+
+    public static void viewHistory(List<StepResult> history) {
+        for (StepResult stepResult : history) {
+            viewRunningStep(stepResult.getCars());
+            System.out.println();
+        }
+    }
+
+    private static void viewRunningStep(List<RacingCar> stepCars) {
+        System.out.println(stepCars.stream()
+                .map(car -> car.toString() + " : " + convertToHyphens(car.getPosition()))
+                .collect(Collectors.joining("\n")));
+    }
+
+    public static String convertToHyphens(int position) {
+        return IntStream.range(0, position)
+                .mapToObj(p -> CHAR_TO_CONVERT)
+                .collect(Collectors.joining());
     }
 }
