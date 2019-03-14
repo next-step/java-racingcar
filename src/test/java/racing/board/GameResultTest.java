2fc9c88dcabe5beab542ea8efdbef109b6d6f5f1
jcheolho <fnel159@naver.com>
2019-03-14 17:31:28 +0900

    step2: 피드백반영: winners가 모두 같은 위치에 있는지 검사하는 테스트 추가

--- src/test/java/racing/board/GameResultTest.java
+++ src/test/java/racing/board/GameResultTest.java
@@ -1,10 +1,10 @@
 package racing.board;
 
 import org.junit.*;
-import racing.model.NamedRacingCar;
 import racing.model.RacingCar;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,7 +15,7 @@ public class GameResultTest {
     public void test_결과_없음() {
         GameResult gameResult = new GameResult(Arrays.asList());
 
-        assertThat(gameResult.getWinnerNames())
+        assertThat(gameResult.getWinners())
             .isEmpty();
     }
 
@@ -23,7 +23,18 @@ public class GameResultTest {
     public void test_결과_있음() {
         GameResult gameResult = new GameResult(Arrays.asList(new RacingCar()));
 
-        assertThat(gameResult.getWinnerNames().size())
+        assertThat(gameResult.getWinners().size())
             .isGreaterThan(0);
+    }
+
+    @Test
+    public void test_1회_이동후_같은_위치에_있는지() {
+        List<RacingCar> cars = Arrays.asList(new RacingCar(), new RacingCar(), new RacingCar());
+        cars.forEach(car -> car.move(RacingCar.THRESHOLD_POWER));
+
+        GameResult gameResult = new GameResult(cars);
+
+        assertThat(gameResult.getWinners())
+                .allMatch(car -> car.compareTo(cars.get(0)) == 0);
     }
 }
