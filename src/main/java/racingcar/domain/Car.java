8736f9cd4e93b661db57371ab27a9cc7f4c5e08f
isisrin <isisrin@nate.com>
2019-03-14 17:11:54 +0900

    Car 불필요한 공백삭제 및 한칸 전진으로 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,23 +1,31 @@
 package racingcar.domain;
 
-public class Car {
-
+public class Car implements Comparable<Car> {
     private static final int ADVANCE_CONDITION = 3;
     private int position;
+    private String name;
 
-    public Car(int position) {
+    public Car(int position, String name) {
         this.position = position;
+        this.name = name;
     }
 
     public int getPosition() {
         return position;
     }
 
-    public void move() {
-        int newPosition = RandomCreator.createNewPosition();
+    public String getName() {
+        return name;
+    }
+    
+    public void move(int newPosition) {
         if(ADVANCE_CONDITION < newPosition) {
-            this.position += newPosition;
+            this.position++;
         }
     }
 
+    public int compareTo(Car compareCar) { // override
+        // 내림차순
+        return compareCar.position - this.position;
+    }
 }
