ca4df67b7b447d55287db6f1af62d72ec719462b
isisrin <isisrin@nate.com>
2019-03-14 17:13:10 +0900

    전진, 멈춤으로 테스트코드 추가

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,25 @@
+package racingcar.domain;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+    private static String TEST_NAME = "testName";
+
+    @Test
+    public void 이동테스트() {
+        Car car = new Car(1, TEST_NAME);
+        int moveValue = 4;
+        car.move(moveValue);
+        assertThat(car.getPosition()).isEqualTo(2);
+    }
+    
+    @Test
+    public void 정지테스트() {
+        Car car = new Car(1, TEST_NAME);
+        int stopValue = 3;
+        car.move(stopValue);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+}
