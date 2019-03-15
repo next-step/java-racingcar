b81984ea68016b0160464cb7e42f4e30e251f6c6
devson <sonyc5720@gmail.com>
2019-03-15 21:47:42 +0900

    문자열 계산기 : 쉼표 덧셈 기능 추가

---
+++ src/main/java/calculator_tdd/StringAdder.java
@@ -0,0 +1,13 @@
+package calculator_tdd;
+
+import java.util.Arrays;
+
+public class StringAdder {
+    public static int calculate(String expression) {
+        String[] numbers = expression.split(",");
+
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+}
---
+++ src/test/java/calculator_tdd/StringAdderTest.java
@@ -0,0 +1,19 @@
+package calculator_tdd;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringAdderTest {
+    @Test
+    public void 쉼표_덧셈() {
+        // given
+        String expression = "1,2,3,4";
+        
+        // when
+        int result = StringAdder.calculate(expression);
+        
+        // then
+        assertThat(result).isEqualTo(10);
+    }
+}
