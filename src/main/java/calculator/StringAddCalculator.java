efe0a1c7b509faa3eef958258721540f792684fe
ninjasul@gmail.com <ninjasul@gmail.com>
2019-03-16 00:59:20 +0900

    문자열 덧셈 계산기 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,4 +1,47 @@
 package calculator;
 
+import util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringAddCalculator {
+
+    public int add(String input) {
+        if(StringUtils.isNullOrEmpty(input) ) {
+            return 0;
+        }
+
+        List<Integer> operands = getOperands(input);
+
+        return getSum(operands);
+    }
+
+    List<Integer> getOperands(String input) {
+
+        List<Integer> operands = new ArrayList<>();
+
+        String[] splittedInput = input.split(",|:");
+
+        for( String str : splittedInput ) {
+            operands.add(parseOperand(str));
+        }
+
+        return operands;
+    }
+
+    private int parseOperand(String str) {
+        Integer operand = Integer.parseInt(str);
+
+        if( operand.intValue() < 0 ) {
+            throw new RuntimeException();
+        }
+
+        return operand.intValue();
+    }
+
+    private int getSum(List<Integer> operands) {
+        return operands.stream().mapToInt(i -> i.intValue()).sum();
+    }
+
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,13 +1,70 @@
 package calculator;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class StringAddCalculatorTest {
 
-    private StringAddCalculator stringAddCalculator;
+    private StringAddCalculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new StringAddCalculator();
+    }
 
     @Test
-    public void name() {
-        stringAddCalculator = new StringAddCalculator();
+    public void add_null_or_empty() {
+        assertEquals(0, calculator.add(null));
+        assertEquals(0, calculator.add(""));
     }
+
+    @Test
+    public void add_one_number() {
+        assertEquals(1, calculator.add("1"));
+        assertEquals(2, calculator.add("2"));
+        assertEquals(3, calculator.add("3"));
+    }
+
+    @Test
+    public void add_two_numbers() {
+        assertEquals(2, calculator.add("1,1"));
+        assertEquals(3, calculator.add("1:2"));
+        assertEquals(5, calculator.add("2,3"));
+    }
+
+    @Test
+    public void add_three_numbers() {
+        assertEquals(6, calculator.add("1,2:3"));
+        assertEquals(9, calculator.add("1:3,5"));
+        assertEquals(15, calculator.add("3,5,7"));
+        assertEquals(24, calculator.add("4:8:12"));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void add_negative_number() {
+        calculator.add("-1");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void add_negative_number2() {
+        calculator.add("1,-2");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void add_negative_numbers() {
+        calculator.add("-2:-3");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void add_negative_numbers2() {
+        calculator.add("1,-2:-3");
+    }
+
+    @Test
+    public void add_with_custom_delimeter() {
+        assertEquals(6, "//;\n1;2;3");
+    }
+
 }
