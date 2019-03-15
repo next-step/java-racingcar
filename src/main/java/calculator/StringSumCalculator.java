df808c7d0692931dc3889306764e650ea94704c4
Park Dongwook <wanderhoward@gmail.com>
2019-03-15 22:13:26 +0900

    문자열 덧셈 계산기  2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)  3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)  4. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)  5. “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\n1;2;3” => 6)  6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)

--- src/main/java/calculator/StringSumCalculator.java
+++ src/main/java/calculator/StringSumCalculator.java
@@ -1,13 +1,59 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringSumCalculator {
 
   public static int sum(String input) {
 
-    if (input == null || input.isEmpty()) {
+    if (isEmptyString(input)) {
       return 0;
     }
 
-    return 0;
+    // java.util.regex 패키지의 Matcher, Pattern import
+    Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+    if (!m.find()) {
+      String[] numbers = input.split(",|:");
+      return sumNumbers(input, numbers);
+    }
+
+    String customDelimiter = m.group(1);
+    String[] numbers= m.group(2).split(customDelimiter);
+
+    // 덧셈 구현
+    return sumNumbers(input, numbers);
+  }
+
+  private static int sumNumbers(String input, String[] numbers) {
+
+    if (numbers.length == 1) {
+      return parseNumberToInt(numbers[0]);
+    }
+
+    int sum = 0;
+    for(String number: numbers) {
+      sum += parseNumberToInt(number);
+    }
+    return sum;
+  }
+
+  private static int parseNumberToInt(String number) {
+
+    int intNumber = Integer.parseInt(number);
+    isNegativeNumberCheck(intNumber);
+
+    return intNumber;
+  }
+
+  private static void isNegativeNumberCheck(int intNumber) {
+
+    if (intNumber < 0) {
+      throw new RuntimeException();
+    }
+  }
+
+  private static boolean isEmptyString(String input) {
+    return input == null || input.isEmpty();
   }
 }
--- src/test/java/calculator/StringSumCalculatorTest.java
+++ src/test/java/calculator/StringSumCalculatorTest.java
@@ -31,4 +31,91 @@ public class StringSumCalculatorTest {
     // Then
     assertThat(result).isEqualTo(0);
   }
+
+  @Test
+  public void test_oneInput_result() {
+
+    // Given
+    int oneNumber = 1;
+    String oneNumberString = oneNumber + "";
+
+    // When
+    int result = StringSumCalculator.sum(oneNumberString);
+
+    // Then
+    assertThat(result).isEqualTo(oneNumber);
+  }
+
+  @Test
+  public void test_multiInput_splitComma_result() {
+
+    // Given
+    int first = 1;
+    int second = 2;
+    String inputString = first + "," + second;
+
+    // When
+    int result = StringSumCalculator.sum(inputString);
+
+    // Then
+    assertThat(result).isEqualTo(first + second);
+  }
+
+  @Test
+  public void test_multiInput_splitCommaAndColon_result() {
+
+    // Given
+    int first = 1;
+    int second = 2;
+    int third = 3;
+    String inputString = first + "," + second + ":" + third;
+
+    // When
+    int result = StringSumCalculator.sum(inputString);
+
+    // Then
+    assertThat(result).isEqualTo(first + second + third);
+  }
+
+  @Test
+  public void test_multiInput_customSeparator_result() {
+
+    // Given
+    int first = 1;
+    int second = 2;
+    int third = 3;
+    String separator = ";";
+    String inputString = "//" + separator + "\n"
+        + first + separator + second + separator + third;
+
+    // When
+    int result = StringSumCalculator.sum(inputString);
+
+    // Then
+    assertThat(result).isEqualTo(first + second + third);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void test_oneInput_negativeNumber_result() {
+
+    // Given
+    int first = -1;
+    String inputString = first + "";
+
+    // When
+    StringSumCalculator.sum(inputString);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void test_multiInput_negativeNumber_result() {
+
+    // Given
+    int first = -1;
+    int second = 2;
+    int third = 3;
+    String inputString = first + "," + second + "," + third;
+
+    // When
+    StringSumCalculator.sum(inputString);
+  }
 }
