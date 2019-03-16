3b1f72704d35fb04b1a403abdaeee1bc1efd8a54
jcheolho <fnel159@naver.com>
2019-03-16 18:44:39 +0900

    step3: test: 우승자를 임의로 만들기 위해 move 메소드를 호출하던 부분을 초기위치 생성자 호출로 변경

--- src/test/java/racing/board/GameResultTest.java
+++ src/test/java/racing/board/GameResultTest.java
@@ -16,7 +16,7 @@ public class GameResultTest {
     public void test_스텝_추가() {
         GameResult gameResult = new GameResult();
 
-        List<RacingCar> cars = Arrays.asList(new RacingCar(), new NamedRacingCar("pobi"));
+        List<RacingCar> cars = Arrays.asList(new RacingCar(), new RacingCar());
         assertThat(gameResult.addStep(cars)).isEqualTo(1);
         assertThat(gameResult.addStep(cars)).isEqualTo(2);
     }
@@ -32,21 +32,20 @@ public class GameResultTest {
     @Test
     public void test_결과_있음() {
         GameResult gameResult = new GameResult();
-        gameResult.addStep(Arrays.asList(new NamedRacingCar("pobi")));
+        gameResult.addStep(Arrays.asList(new RacingCar()));
 
         assertThat(gameResult.getWinners().size())
             .isGreaterThan(0);
     }
 
     @Test
-    public void test_1회_이동후_우승자들이_같은_위치에_있는지() {
-        List<RacingCar> cars = Arrays.asList(new NamedRacingCar("pobi"));
-        cars.forEach(car -> car.move(RacingCar.THRESHOLD_POWER));
+    public void test_우승자들이_같은_위치에_있는지() {
+        List<RacingCar> cars = Arrays.asList(new RacingCar(2), new RacingCar(2), new RacingCar(1));
 
         GameResult gameResult = new GameResult();
         gameResult.addStep(cars);
 
         assertThat(gameResult.getWinners())
-                .allMatch(car -> car.compareTo(cars.get(0)) == 0);
+                .allMatch(car -> car.compareTo(new RacingCar(2)) == 0);
     }
 }
