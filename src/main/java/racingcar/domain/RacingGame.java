375702b78f9b88c1f3fb431ab6d607ccb14909bf
isisrin <isisrin@nate.com>
2019-03-17 20:39:06 +0900

    Car 생성 파라미터 위치변경

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -10,7 +10,7 @@ public class RacingGame {
     
     public RacingGame(String[] carNames, int time) {
         for(String carName : carNames) {
-            cars.add(new Car(1, carName));
+            cars.add(new Car(carName, 1));
         }
         this.time = time;
     }
