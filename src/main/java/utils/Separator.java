a442437568c5a46418b232bbc0185c6fe4ebd3e4
SGKIM94 <tkdrn8578@naver.com>
2019-03-17 22:03:39 +0900

    - Separator  * 입력값에 대한 뛰어쓰기 구분자 추가

--- src/main/java/utils/Separator.java
+++ src/main/java/utils/Separator.java
@@ -1,8 +1,9 @@
 package utils;
 import static utils.ConstantCollection.DELIMITER;
+import static utils.ConstantCollection.WHITESPACE;
 
 public class Separator {
     public static String [] separateName(String carsName) {
-        return carsName.split(DELIMITER);
+        return carsName.split(DELIMITER + "|" + WHITESPACE);
     }
 }
