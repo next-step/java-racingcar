8418aec393955d517bba4aaee3a47c45732bbc2a
isisrin <isisrin@nate.com>
2019-03-17 18:42:32 +0900

    StringAddCalculator 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,7 +1,5 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,7 +12,6 @@ public class StringAddCalculator {
         if(isNull(inputText) || inputText.isEmpty()) {
             return EMPTY_RETURN;
         }
-        
         if(inputText.length() == MONO_TEXT) {
             return Integer.parseInt(inputText);
         }
@@ -27,41 +24,31 @@ public class StringAddCalculator {
             return sum(parseString(inputText));
         }
 
-        if (inputText.contains("//") & inputText.contains("\n")) {
-            return sum(convertInts(parseMatcher(inputText)));
+        Matcher matcher = Pattern.compile("//(.)\\n(.*)").matcher(inputText);
+        if (matcher.find()) {
+            return sum(parseMatcher(matcher));
         }
-
-        return 0;
+        return 1;
     }
 
-    private String[] parseMatcher(String inputText) {
-        Matcher matcher = Pattern.compile("//(.)\\n(.*)").matcher(inputText);
-        matcher.find();
+    private String[] parseMatcher(Matcher matcher) {
         String customDelimiter = matcher.group(1);
         return matcher.group(2).split(customDelimiter);
     }
 
-    private boolean isNull(String inputText) {
-        return inputText == null;
+    private boolean isNull(String text) {
+        return text == null;
     }
 
-    private List<Integer> parseString(String inputText) {
-        return convertInts(inputText.split(",|:"));
+    private String[] parseString(String text) {
+        return text.split(",|:");
     }
 
-    private int sum(List<Integer> numbers) {
+    private int sum(String[] numbers) {
         int sum = 0;
-        for (int number : numbers) {
-            sum += number;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
         }
         return sum;
-    }
-
-    private List<Integer> convertInts(String[] values) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int index = 0; index < values.length ; index++) {
-            numbers.add(Integer.parseInt(values[index]));
-        }
-        return numbers;
     }
 }
