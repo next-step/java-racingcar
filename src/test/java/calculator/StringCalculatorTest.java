4c3b1664bbe0c44ce2b7af9aabb61f201f85f5e1
SGKIM94 <tkdrn8578@naver.com>
2019-03-17 14:21:14 +0900

    StringCalculatorTest  - 문자열_덧셈_계산기(입력한 값이 정상적으로 더해지는지 테스트)  - 입력값_분리_테스트(정상적으로 값이 분리되는지 테스트)  - 입력값_오류_테스트(정상적인 값이 들어가지 않았을 때 테스트)  - 숫자_하나_입력(한개의 입력값만 들어간 경우 오류 테스트)  - 커스텀_구분자_테스트(특정한 구분자가 들어갔을 경우 테스트)  - 음수_입력값_테스트(음수를 입력한 경우 오류 테스트)

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static calculator.AddStringCalculator.seperatorInput;
+import static calculator.AddStringCalculator.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class StringCalculatorTest {
@@ -18,15 +18,15 @@ public class StringCalculatorTest {
 
     @Test
     public void 입력값_분리_테스트() {
-        List<String> inputs = seperatorInput("3,5");
+        List<String> inputs = splitInput("3,5");
 
         assertThat(inputs.get(0)).isEqualTo("3");
         assertThat(inputs.get(1)).isEqualTo("5");
     }
 
-    @Test
-    public void 입력값_오류_테스트() {
-        AddStringCalculator.checkInputValidation(null);
+    @Test(expected = IllegalAccessException.class)
+    public void 입력값_오류_테스트(){
+        AddStringCalculator.checkInputValidation("");
     }
 
     @Test
@@ -37,5 +37,19 @@ public class StringCalculatorTest {
     @Test
     public void 합계_테스트() {
         assertThat(8).isEqualTo(Arrays.asList("3", "5"));
+    }
+
+    @Test
+    public void 커스텀_구분자_테스트() {
+        List<String> inputs = splitInput("3,5/6");
+
+        assertThat(inputs.get(0)).isEqualTo("3");
+        assertThat(inputs.get(1)).isEqualTo("5");
+        assertThat(inputs.get(2)).isEqualTo("6");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 음수_입력값_테스트() {
+        checkMinusInput("-3");
     }
 }
