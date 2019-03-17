3689d098e1fc49069c553bb9f425b34db9d45ba1
SGKIM94 <tkdrn8578@naver.com>
2019-03-17 14:18:01 +0900

    AddStringCalculator.java  - addCalculator(문자열 계산기를 구현하는 main 메서드)  - add(입력받은 값을 더해주는 메서드)  - splitInput(입력값을 커스텀 구분자를 통해 분리해주는 메서드)  - checkMinusInput(입력값에 음수가 포함되는지 확인하는 메서드)  - checkInputValidation(입력값이 하나이거나 empty인 것을 체크해주는 메서드)

--- src/main/java/calculator/AddStringCalculator.java
+++ src/main/java/calculator/AddStringCalculator.java
@@ -2,23 +2,19 @@ package calculator;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class AddStringCalculator {
 
     public static int addCalculator(String input) {
-        List<String> inputs = new ArrayList<>();
-        List<Integer> intInput = new ArrayList<>();
 
-        inputs = seperatorInput(input);
+        checkInputValidation(input);
 
+        List<String> inputs = splitInput(input);
 
-        add(inputs);
-
-
-        return 20;
+        return add(inputs);
     }
 
     private static int add(List<String> inputs) {
@@ -31,19 +27,26 @@ public class AddStringCalculator {
         return sum;
     }
 
-    public static List<String> seperatorInput(String input) {
+    public static List<String> splitInput(String input) {
         List<String> inputs = new ArrayList<>();
 
-        for (String value : input.split(",")) {
+        for (String value : input.split(",|/")) {
+            checkMinusInput(value);
             inputs.add(value);
         }
 
         return inputs;
     }
 
+    public static void checkMinusInput(String input) {
+        if (StringInput.isMinusInput(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void checkInputValidation(String input) {
 
-        if(input.length() == 1) {
+        if (StringInput.isMoreThanOne(input)) {
             throw new IllegalArgumentException();
         }
         if (StringUtils.isEmpty(input)) {
