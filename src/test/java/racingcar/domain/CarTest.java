f2102d1b989f84e27dd9fca9d83cdb69e93808fb
isisrin <isisrin@nate.com>
2019-03-18 20:36:33 +0900

    step 3 : 매직넘버 -> 상수화

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -6,10 +6,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
     private static String TEST_NAME = "testName";
+    private static int INIT_POSITION = 1;
 
     @Test
     public void 이동테스트() {
-        Car car = new Car(TEST_NAME, 1);
+        Car car = new Car(TEST_NAME, INIT_POSITION);
         int moveValue = 4;
         car.move(moveValue);
         assertThat(car.getPosition()).isEqualTo(2);
@@ -17,7 +18,7 @@ public class CarTest {
     
     @Test
     public void 정지테스트() {
-        Car car = new Car(TEST_NAME,1);
+        Car car = new Car(TEST_NAME,INIT_POSITION);
         int stopValue = 3;
         car.move(stopValue);
         assertThat(car.getPosition()).isEqualTo(1);
