a2effaecdfbf6b1cd4adbe32dbc8ea51f0839820
SGKIM94 <tkdrn8578@naver.com>
2019-03-21 22:47:10 +0900

    Separator, StringCalculator, StringInput  * for, if Convention 수정

--- src/main/java/utils/Separator.java
+++ src/main/java/utils/Separator.java
@@ -1,8 +1,9 @@
 package utils;
-import static utils.ConstantCollection.DELIMITER;
-import static utils.ConstantCollection.WHITESPACE;
 
 public class Separator {
+    public static final String WHITESPACE = " ";
+    public static final String DELIMITER = ",";
+
     public static String [] separateName(String carsName) {
         return carsName.split(DELIMITER + "|" + WHITESPACE);
     }
