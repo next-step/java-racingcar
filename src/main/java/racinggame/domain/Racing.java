1086ff00ca512a407276ed060ac5931479d6f23f
SGKIM94 <tkdrn8578@naver.com>
2019-03-21 22:51:06 +0900

    Racing  * WebGame 제거

--- src/main/java/racinggame/domain/Racing.java
+++ src/main/java/racinggame/domain/Racing.java
@@ -1,30 +1,27 @@
 package racinggame.domain;
 
-import racinggame.dto.WebGameDto;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static racinggame.view.WebRacing.printWebRacing;
-import static utils.ConstantCollection.*;
 import static utils.GenerateRandom.generatingRandomNumber;
 import static utils.Separator.separateName;
 
 public class Racing {
+    final static int RANGE_RANDOM = 10;
+    public static final int EXCEPTION_VALUE = -1;
+    public static final int FORWARD_NUMBER = 4;
+
     private List<Car> cars;                 // 자동차들을 저장하는 리스트
     private int tryGame;                    // 시도할 횟수
 
-    // 사용자에게 입력받은 자동차 회수와 game 횟수를 저장하는 생성자
     public Racing(String carsName, int tryGame) {
-        this.tryGame = 0;
-        cars = new ArrayList<>();
+        this.cars = new ArrayList<>();
 
-        //이름을 하나씩 분리해주는 메서드
         String [] names = separateName(carsName);
 
-        for(int i = 0; i < names.length; i++) {
-            cars.add(new Car(names[i]));
+        for (int i = 0; i < names.length; i++) {
+            this.cars.add(new Car(names[i]));
         }
 
         this.tryGame = tryGame;
@@ -41,12 +38,12 @@ public class Racing {
     public List<String> getWinners() {
         List<String> winners = new ArrayList<>();
 
-        if(getMaxMovement() == EXCEPTION_VALUE) {
+        if (getMaxMovement() == EXCEPTION_VALUE) {
             throw new IllegalArgumentException();
         }
         int maxMovement = getMaxMovement();
 
-        for(Car car : this.cars) {
+        for (Car car : this.cars) {
             if (car.isWinner(maxMovement)) {
                 winners.add(car.getName());
             }
@@ -57,7 +54,7 @@ public class Racing {
     public int getMaxMovement() {
         List<Integer> carsMovement = new ArrayList<>();
 
-        for(Car car : this.cars) {
+        for (Car car : this.cars) {
             carsMovement.add(car.getCountMoving());
         }
 
@@ -66,9 +63,9 @@ public class Racing {
 
     // 난수를 생성하여 자동차가 움직일지의 전진 여부를 결정하여 추가하는 메서드
     public void carForwardStatus() {
-        for(Car car : this.cars) {
+        for (Car car : this.cars) {
             int random = generatingRandomNumber(RANGE_RANDOM);
-            if(isCarMoving(random)){
+            if (isCarMoving(random)){
                 car.move();
             }
         }
@@ -82,9 +79,4 @@ public class Racing {
         return false;
     }
 
-    public void makeWebGame(List<WebGameDto> webGameDtos) {
-        for (Car car :  getCars()) {
-            webGameDtos.add(printWebRacing(car));
-        }
-    }
 }
