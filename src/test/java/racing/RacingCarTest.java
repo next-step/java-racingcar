32b61c3ebb69d6a28bc2f70ed1bdb7829af5a534
RudiaMoon <impactyoung@gmail.com>
2019-05-03 23:31:31 +0900

    Add given/when/thne 기입

--- src/test/java/racing/RacingCarTest.java
+++ src/test/java/racing/RacingCarTest.java
@@ -10,24 +10,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RacingCarTest {
     @Test
     public void move_동작_테스트_움직임_조건이_아닐_경우_정지() {
+        //given
         RacingCar car = new RacingCar();
 
+        //when
         int result = car.move2(() -> false);
+
+        //then
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     public void move_동작_테스트_움직임_조건일_경우_이동() {
+        //given
         RacingCar car = new RacingCar();
 
+        //when
         int result = car.move2(() -> true);
+
+        //then
         assertThat(result).isEqualTo(1);
     }
 
     @Test(expected = ClassCastException.class)
     public void 레이싱카_외부에서_변경_못하게_설정() {
+        //given
         Racing racing = new Racing(2, 3);
+
+        //when
         List<RacingCar> racingCars = racing.getRacingCars();
+
+        //then
         racingCars.add(new RacingCar());
     }
 }
--- src/test/java/racing/UtilsTest.java
+++ src/test/java/racing/UtilsTest.java
@@ -7,7 +7,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UtilsTest {
     @Test
     public void 랜덤_숫자_생성() {
+        //when
         int random = Utils.generateRandomNum();
+
+        //then
         assertThat(random).isBetween(0, 9);
     }
 }
