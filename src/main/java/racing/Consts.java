181bf7fd082cda054c4c9635f2807ca3206bba7a
RudiaMoon <impactyoung@gmail.com>
2019-05-11 01:10:22 +0900

    Refactor 상수변수 통일

--- src/main/java/racing/Consts.java
+++ src/main/java/racing/Consts.java
@@ -4,6 +4,6 @@ public class Consts {
     public static final int MOVE_MIN_COUNT = 4;
     public static final int RANDOM_NUMBER_BOUNDARY = 10;
 
-    public final static String FORMAT_COMMA = ",";
+    public static final String FORMAT_COMMA = ",";
     public static final String PROGRESS_POSITION = "-";
 }
