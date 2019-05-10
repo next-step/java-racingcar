f3b6372fb2297fc76fb6ffa5cdbdeb97c1b57572
RudiaMoon <impactyoung@gmail.com>
2019-05-11 05:22:56 +0900

    Refactor 우승자 이름 구하기

--- src/test/java/racing/RacingCarsTest.java
+++ src/test/java/racing/RacingCarsTest.java
@@ -55,7 +55,7 @@ public class RacingCarsTest {
 
     @Test
     public void 우승자_이름_구하기() {
-        System.out.println(resultRacingCars.getWinnerName());
+        //then
         assertThat(resultRacingCars.getWinnerName()).contains("m1");
         assertThat(resultRacingCars.getWinnerName()).contains("m2");
     }
