480aae89e0576b257f55f5909eb84cd6cfc61e4d
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-14 00:53:51 +0900

    [refactor] calculator add 사용

--- src/main/java/com/jaeyeonling/calculator/StringCalculator.java
+++ src/main/java/com/jaeyeonling/calculator/StringCalculator.java
@@ -2,8 +2,11 @@ package com.jaeyeonling.calculator;
 
 public class StringCalculator {
 
+    private static final Calculator calculator = new Calculator();
+
     public int calculate(final String input) {
         final String[] splitInput = input.split(" ");
-        return Integer.valueOf(splitInput[0]) + Integer.valueOf(splitInput[2]);
+
+        return calculator.add(Integer.valueOf(splitInput[0]), Integer.valueOf(splitInput[2]));
     }
 }
