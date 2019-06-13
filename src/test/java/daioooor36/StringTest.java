9ae7a060ad60dae218f9c1d2eee364009bbe4e19
IKSOO SHIN <iksoo@IKSOOui-MacBook-Pro.local>
2019-06-14 01:08:39 +0900

    test(Set<Integer>) Integer Test

--- src/test/java/daioooor36/StringTest.java
+++ src/test/java/daioooor36/StringTest.java
@@ -3,11 +3,12 @@ package daioooor36;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class StringTest {
 
     @Test
-    void splitTest() {
+    void split_1() {
         String input = "1,2";
         String[] results = input.split(",");
 
@@ -16,5 +17,20 @@ public class StringTest {
         assertThat(results.length).isEqualTo(2);
         assertThat(results[0]).isEqualTo("1");
         assertThat(results[1]).isEqualTo("2");
+    }
+
+    @Test
+    void split_2() {
+        String input = "(1,2)";
+        String results = input.substring(1,input.length()-1);
+
+        assertThat(results).isEqualTo("1,2");
+    }
+
+    @Test
+    void split_3() {
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() ->
+                "abc".charAt(4))
+                .withMessageMatching("String index out of range: \\d+");
     }
 }
