c6d39ad00dbcdb0905602ec8078babe76aab69fa
Juyoung <juy5790@outlook.com>
2019-06-13 21:54:04 +0900

    study test to String and Set

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.assertj.core.util.Strings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
+    void setSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void setContains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input){
+        assertThat(Strings.isNullOrEmpty(input));
+    }
+
+
+}
