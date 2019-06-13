ad71973639862d15d9d6b2c5035684cfb38ed3bb
최범균(Beomgyun Choe)/Platform Engineering팀/11ST <choebk@sk.com>
2019-06-13 21:54:06 +0900

    step0

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3"})
+    void contains(String num) {
+        assertThat(numbers.contains(num)).isTrue();
+    }
+
+}
