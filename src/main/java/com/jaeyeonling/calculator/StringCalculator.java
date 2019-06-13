502eb97a22a9f8cba7e0ccdcbd9eb2879639b957
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-14 00:51:47 +0900

    [feat] add 테스트 통과 (green)

--- src/main/java/com/jaeyeonling/calculator/StringCalculator.java
+++ src/main/java/com/jaeyeonling/calculator/StringCalculator.java
@@ -3,6 +3,7 @@ package com.jaeyeonling.calculator;
 public class StringCalculator {
 
     public int calculate(final String input) {
-        return 2;
+        final String[] splitInput = input.split(" ");
+        return Integer.valueOf(splitInput[0]) + Integer.valueOf(splitInput[2]);
     }
 }
