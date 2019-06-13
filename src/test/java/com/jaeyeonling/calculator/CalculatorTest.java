ffc2d72dd12343c7952842b8c64845f9e0c8be3c
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-14 00:29:02 +0900

    [refactor] 중복 코드 제거 (refactor)

--- src/test/java/com/jaeyeonling/calculator/CalculatorTest.java
+++ src/test/java/com/jaeyeonling/calculator/CalculatorTest.java
@@ -1,16 +1,15 @@
 package com.jaeyeonling.calculator;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
 
+    private final Calculator calculator = new Calculator();
+
     @Test
     void add() {
-        final Calculator calculator = new Calculator();
-
         assertThat(calculator.add(1, 2)).isEqualTo(3);
         assertThat(calculator.add(3, 4)).isEqualTo(7);
         assertThat(calculator.add(100, 200)).isEqualTo(300);
@@ -18,8 +17,6 @@ class CalculatorTest {
 
     @Test
     void minus() {
-        final Calculator calculator = new Calculator();
-
         assertThat(calculator.minus(1, 2)).isEqualTo(-1);
         assertThat(calculator.minus(3, 4)).isEqualTo(-1);
         assertThat(calculator.minus(100, 200)).isEqualTo(-100);
