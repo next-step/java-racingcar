4662da8fb27abdb6ceb4d2697b73475e695c426e
feelydh <feelydh@echomarketing.co.kr>
2019-06-13 21:54:34 +0900

    study test StringTest

---
+++ src/test/java/study/CollectionTest.java
@@ -0,0 +1,42 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void setSize(){
+        assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    @Test
+    void contains(){
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void duplicateSource(int number){
+        assertThat(numbers.contains(number)).isTrue();
+    }
+}
