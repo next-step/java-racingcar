662bd37bc344523865b26989901c3a0359ba98ec
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-14 02:57:57 +0900

    [test] invalid text 에러 테스트 통과 (green)

--- src/main/java/com/jaeyeonling/calculator/CalculateValue.java
+++ src/main/java/com/jaeyeonling/calculator/CalculateValue.java
@@ -1,5 +1,7 @@
 package com.jaeyeonling.calculator;
 
+import com.jaeyeonling.calculator.type.Operator;
+
 public class CalculateValue {
 
     private static final String SEPARATOR = " ";
@@ -8,6 +10,8 @@ public class CalculateValue {
 
     private CalculateValue (final String[] splitValue) {
         this.splitValue = splitValue;
+
+        checkConstraints();
     }
 
     public static CalculateValue text(final String text) {
@@ -18,6 +22,21 @@ public class CalculateValue {
         return new CalculateValue(text.split(SEPARATOR));
     }
 
+    //
+    //
+    //
+
+    public int length() {
+        return splitValue.length;
+    }
+
+    public int getValue(final int index) {
+        return Integer.parseInt(splitValue[index]);
+    }
+
+    public Operator getOperator(final int index) {
+        return Operator.of(splitValue[index]);
+    }
 
     //
     //
@@ -25,5 +44,37 @@ public class CalculateValue {
 
     private static boolean isInvalidText(final String text) {
         return text == null || text.isEmpty();
+    }
+
+    private void checkConstraints() {
+        if (isInvalidValueLength()) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < length(); i += 3) {
+            checkValue(i);
+            checkOperator(i + 1);
+            checkValue(i + 2);
+        }
+    }
+
+    private boolean isInvalidValueLength() {
+        return length() < 2 || isEvenLength();
+    }
+
+    private boolean isEvenLength() {
+        return length() / 2 == 0;
+    }
+
+    private void checkValue(final int index) {
+        try {
+            getValue(index);
+        } catch (final NumberFormatException ignore) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkOperator(final int index) {
+        getOperator(index);
     }
 }
