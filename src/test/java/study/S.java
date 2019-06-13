2b225ec019ec0ad6ed2fee1d79a52bdcf5e895f7
이창준 <31179459+changjun89@users.noreply.github.com>
2019-06-13 22:04:29 +0900

    compile test (#218)

---
+++ src/test/java/study/S.java
@@ -0,0 +1,29 @@
+package study;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class S {
+
+  private Set<Integer> numbers;
+
+  @BeforeEach
+  void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+
+
+  @ParameterizedTest
+  @ValueSource(ints = {1,2,3})
+  void contains(int number) {
+
+  }
+}
