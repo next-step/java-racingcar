c98c270d5959d79fd875e571996f88ea515e220d
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-14 20:20:54 +0900

    [refactor] 매직넘버 상수로 분리

--- src/main/java/com/jaeyeonling/calculator/StringCalculateValue.java
+++ src/main/java/com/jaeyeonling/calculator/StringCalculateValue.java
@@ -6,6 +6,11 @@ public class StringCalculateValue {
 
     private static final String SEPARATOR = " ";
 
+    private static final int DEFAULT_INDEX = 0;
+    private static final int START_INDEX = 1;
+    private static final int CALCULATION_SIZE = 2;
+
+
     private final String[] splitValue;
 
     private int result;
@@ -38,10 +43,10 @@ public class StringCalculateValue {
             return;
         }
 
-        int result = getValue(0);
-        for (int i = 1; i < length(); i += 2) {
+        int result = getValue(DEFAULT_INDEX);
+        for (int i = START_INDEX; i < length(); i += CALCULATION_SIZE) {
             final Operator operator = getOperator(i);
-            final int nextValue = getValue(i + 1);
+            final int nextValue = getValue(i + START_INDEX);
 
             result = operator.apply(result, nextValue);
         }
@@ -75,19 +80,19 @@ public class StringCalculateValue {
             throw new IllegalArgumentException("올바르지 않은 Expression");
         }
 
-        checkValue(0);
-        for (int i = 1; i < length(); i += 2) {
+        checkValue(DEFAULT_INDEX);
+        for (int i = START_INDEX; i < length(); i += CALCULATION_SIZE) {
             checkOperator(i);
-            checkValue(i + 1);
+            checkValue(i + START_INDEX);
         }
     }
 
     private boolean isInvalidValueLength() {
-        return length() < 2 || isEvenLength();
+        return length() < CALCULATION_SIZE || isEvenLength();
     }
 
     private boolean isEvenLength() {
-        return length() / 2 == 0;
+        return length() / CALCULATION_SIZE == 0;
     }
 
     private void checkValue(final int index) {
