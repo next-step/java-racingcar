a5e4d75ca5926fc5922147a9f19a597620908383
github-byrage <byrage91@gmail.com>
2019-06-14 23:29:50 +0900

    refactor: 클래스 분리

---
+++ src/test/java/step1/enums/OperationSymbolTest.java
@@ -0,0 +1,26 @@
+package step1.enums;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class OperationSymbolTest {
+
+    @Test
+    @DisplayName("OperationSymbol 변환 테스트")
+    void from() {
+        List<String> supportedOperations = Arrays.asList("+", "-", "*", "/");
+        supportedOperations.forEach(operation ->
+                    assertThatCode(() -> OperationSymbol.from(operation)).doesNotThrowAnyException());
+
+        List<String> unsupportedOperations = Arrays.asList("%", ",", ".", "&", "!");
+        unsupportedOperations.forEach(operation ->
+                      assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> OperationSymbol.from(operation)));
+
+    }
+}
