a82d87704313276186b7d2eced18896fd741f4ff
sogoagain <imyong0@gmail.com>
2019-06-15 01:48:52 +0900

    feat(다항연산기능): 다항식을 표현하는 Polynomial 클래스 작성
    Polynomial 제공 기능:
    - createPolynomialWithExpression: 문자열로 입력된 수식으로부터 다항식 객체 생성
    - calculate: 다항식 계산 결과 값 반환

---
+++ src/main/java/calculator/Polynomial.java
@@ -0,0 +1,71 @@
+package calculator;
+
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Polynomial {
+    private List<String> operators;
+    private List<Integer> operands;
+    private Integer resultCache = null;
+
+    private Polynomial(List<Integer> operands, List<String> operators) {
+        this.operands = operands;
+        this.operators = operators;
+    }
+
+    public static Polynomial createPolynomialWithExpression(String expression) {
+        if (!PolynomialParser.isValid(expression)) {
+            throw new IllegalArgumentException("수식이 올바르지 않습니다.");
+        }
+
+        List<Integer> operands = PolynomialParser.extractOperands(expression);
+        List<String> operators = PolynomialParser.extractOperators(expression);
+
+        return new Polynomial(operands, operators);
+    }
+
+
+    public int calculate() {
+        if (resultCache != null) {
+            return resultCache;
+        }
+
+        Stack<Integer> operands = convertListToStack(reverse(this.operands));
+        Stack<String> operators = convertListToStack(reverse(this.operators));
+
+        while (!operators.isEmpty()) {
+            resultCache = performBinaryOperation(operands, operators);
+        }
+
+        return resultCache;
+    }
+
+    private int performBinaryOperation(Stack<Integer> operands, Stack<String> operators) {
+        int operand1 = operands.pop();
+        int operand2 = operands.pop();
+        String operator = operators.pop();
+
+        Calculable calculable = BinaryOperator.of(operator);
+        int result = calculable.calculate(operand1, operand2);
+
+        operands.add(result);
+        return result;
+    }
+
+    private <E> Stack<E> convertListToStack(List<E> list) {
+        Stack<E> stack = new Stack<>();
+        stack.addAll(list);
+
+        return stack;
+    }
+
+    private <E> List<E> reverse(List<E> list) {
+        return IntStream.range(0, list.size())
+                .map(i -> (list.size() - 1 - i))
+                .mapToObj(list::get)
+                .collect(Collectors.toList());
+    }
+}
+
---
+++ src/test/java/calculator/PolynomialTest.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PolynomialTest {
+    @Test
+    void 다항식_생성_테스트() {
+        String expression = "12 + 252 - 3";
+        Polynomial polynomial = Polynomial.createPolynomialWithExpression(expression);
+
+        assertThat(polynomial).isInstanceOf(Polynomial.class);
+    }
+
+    @Test
+    void 잘못된_수식으로_다항식을_생성하면_예외가_발생한다() {
+        assertThatThrownBy(() -> {
+            Polynomial.createPolynomialWithExpression("12 + * 3");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1 - 2, -1"
+            , "1 + 2 + 3, 6"
+            , "2 + 3 * 4 / 2, 10"
+            , "1343 + 231 * 2 / 4, 787"
+    })
+    void 다항식_연산_수행_테스트(String expression, int result) {
+        Polynomial polynomial = Polynomial.createPolynomialWithExpression(expression);
+
+        assertThat(polynomial.calculate()).isEqualTo(result);
+    }
+}
