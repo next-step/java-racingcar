6f5badcd18982a34e21c96900c46425297617e1f
Bentley <p2c2kbf@gmail.com>
2019-06-14 20:30:28 +0900

    feature(add): adding main fuction and modification for add fuction

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,10 +1,6 @@
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+
 
 import java.util.Scanner;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class StringCalculator {
 
@@ -16,7 +12,7 @@ public class StringCalculator {
         //사용자에게 문자열 입력
         String input = sc.nextLine();
         int addResult= add(input);
-        //더하기 결과 출력
+
         System.out.println("더하기 결과는 " + addResult);
 
 
@@ -27,13 +23,13 @@ public class StringCalculator {
     add() 함수 단위 테스트 구현
     해당 함수는 더하기 기능 구현.
      */
-    @ParameterizedTest
-    @ValueSource(strings = {"1+2"})
+//    @ParameterizedTest
+//    @ValueSource(strings = {"1+2"})
     static int add(String input){
         int plusIndex = input.indexOf("+");
-        int num1 = Character.getNumericValue(input.charAt(plusIndex-1));
+        int num1 =  Character.getNumericValue(input.charAt(plusIndex-1));
         int num2 = Character.getNumericValue(input.charAt(plusIndex+1));
-        assertThat(num1 + num2).toString().contains("3");
+//        assertThat(num1 + num2).toString().contains("3");
         return num1+num2;
 
     }
