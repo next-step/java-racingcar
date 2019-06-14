b3435d1393b8b07cb961fcdf69c5135f615810d8
Juyoung <juy5790@outlook.com>
2019-06-14 13:03:15 +0900

    feat(stringCalculator): 문자열을 공백기준으로 split, 입력값 빈 값일 경우 exception 처리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,42 +1,85 @@
 package calculator;
 
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.IntBinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringCalculator {
 
-    public StringCalculator() {
+    private int result;
 
+    String[] split(String input) {
+        validateBlank(input);
+        return input.split(" ");
     }
 
-    public int calculate(int num, Operator operator, int num2) {
+    private void validateBlank(String input) {
+        if(input == null || input.trim().isEmpty()){
+            throw new IllegalArgumentException("문자열을 입력하세요. ex) 4 + 2 / 3 ..");
+        }
+    }
+
+    public int calculate(String input) {
+        String[] stringArray = split(input);
+        result = parseInt(stringArray[0]);
+        for (int index = 1; index < stringArray.length; index++) {
+            operator(stringArray, index);
+        }
+        return result;
+    }
+
+    private void operator(String[] strArr, int index) {
+        if (index % 2 == 1) {
+            Operator operator = Operator.from(strArr[index]);
+            result = calculate(result, operator, parseInt(strArr[index+1]));
+        }
+    }
+
+    private int parseInt(String num) {
+        return Integer.parseInt(num);
+    }
+
+    int calculate(int num, Operator operator, int num2) {
         return operator.calculator(num, num2);
     }
 
     enum Operator {
-        PLUS('+', (num, num2) -> num + num2),
-        MINUS('-', (num, num2) -> num - num2),
-        MULTIPLY('*', (num, num2) -> num * num2),
-        DIVIDE('/', (num, num2) -> {
+        PLUS('+', (num1, num2) -> num1 + num2),
+        MINUS('-', (num1, num2) -> num1 - num2),
+        MULTIPLY('*', (num1, num2) -> num1 * num2),
+        DIVIDE('/', (num1, num2) -> {
             validateDivision(num2);
-            return num / num2;
+            return num1 / num2;
         });
 
         private final char symbol;
         private final IntBinaryOperator expression;
+        private static final Map<String, Operator> symbolToEnum = Stream.of(values()).collect(Collectors.toMap(Operator::getSymbol, operator -> operator));
 
         Operator(char symbol, IntBinaryOperator expression) {
             this.symbol = symbol;
             this.expression = expression;
         }
 
-        public int calculator(int num, int num2) {
-            return expression.applyAsInt(num, num2);
+        public int calculator(int num1, int num2) {
+            return expression.applyAsInt(num1, num2);
         }
 
         private static void validateDivision(int number) {
             if (number == 0) {
                 throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
             }
+        }
+
+        public String getSymbol() {
+            return String.valueOf(symbol);
+        }
+
+        public static Operator from(String symbol) {
+            return Optional.ofNullable(symbolToEnum.get(symbol))
+                    .orElseThrow(() -> new IllegalArgumentException(symbol + "은 지원하지 않는 연산기호입니다."));
         }
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,8 +1,11 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static calculator.StringCalculator.Operator.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,7 +14,40 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 class StringCalculatorTest {
 
-    StringCalculator stringCalculator = new StringCalculator();
+    StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    void calculator() {
+        assertThat(stringCalculator.calculate("1 + 2 * 3")).isEqualTo(9);
+        assertThat(stringCalculator.calculate("4 / 2 - 1")).isEqualTo(1);
+        assertThat(stringCalculator.calculate("4 / 2 - 1 * 3 ")).isEqualTo(3);
+    }
+
+    @Test
+    void operatorWrongSymbolThanFail() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> StringCalculator.Operator.from("^"));
+    }
+
+    @Test
+    void split() {
+        String[] result = stringCalculator.split("1 + 2");
+        assertThat(result).containsExactly("1", "+", "2")
+                          .hasSize(3);
+    }
+
+    @DisplayName("입력 값이 공백 또는 Null일 경우 Exception")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void inputString2(String inputNullAndEmpty) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> stringCalculator.split(inputNullAndEmpty));
+    }
 
     @ParameterizedTest
     @MethodSource("operatorProvider")
@@ -29,9 +65,10 @@ class StringCalculatorTest {
     }
 
     @Test
+    @DisplayName("제수가 0일 경우 Exception")
     void divideByZeroThanFail() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> StringCalculator.Operator.DIVIDE.calculator(4, 0));
+                .isThrownBy(() -> DIVIDE.calculator(4, 0));
     }
 
     private void assertCalculate(int num1, StringCalculator.Operator operator, int num2, int expectedResult) {
