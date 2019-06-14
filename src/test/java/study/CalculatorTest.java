2c64562b91c0c5367103346ba4866087fb367620
Minseok <iamminseokpark@gmail.com>
2019-06-14 22:09:36 +0900

    feat(calc): add 기능 추가

---
+++ src/test/java/study/CalculatorTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class CalculatorTest {
+
+    @Test
+    void add() {
+        String sample = "2 + 3";
+
+        Calculator calc = new Calculator();
+
+        assertThat(calc.calculation(sample)).isEqualTo(5);
+    }
+
+    class Calculator {
+        int result = 0;
+
+        int calculation(String equation) {
+            String[] operList = equation.split(" ");
+
+            if (isAddOperator(operList[1])) {
+                int oper1 = parseInt(operList[0]);
+                int oper2 = parseInt(operList[2]);
+
+                result = add(oper1);
+                result = add(oper2);
+            }
+
+            return result;
+        }
+
+        private boolean isAddOperator(String word) {
+            return word.equals("+");
+        }
+
+        private int parseInt(String word) {
+            return Integer.parseInt(word);
+        }
+
+        private int add(int oper) {
+            return result + oper;
+        }
+    }
+}
