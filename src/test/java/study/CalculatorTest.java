6cf0121e2589a45d8e31de4cf3bf159ff2883222
Minseok <iamminseokpark@gmail.com>
2019-06-14 22:26:28 +0900

    feat(calc): 나누기 기능 추가

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -34,30 +34,37 @@ public class CalculatorTest {
         assertThat(calc.calculation(sample)).isEqualTo(6);
     }
 
+    @Test
+    void divide() {
+        String sample = "6 / 3";
+
+        Calculator calc = new Calculator();
+
+        assertThat(calc.calculation(sample)).isEqualTo(2);
+    }
+
     class Calculator {
         int result = 0;
 
         int calculation(String equation) {
             String[] operList = equation.split(" ");
 
+            int oper1 = parseInt(operList[0]);
+            int oper2 = parseInt(operList[2]);
+
+            result = add(oper1);
             if (isAddOperator(operList[1])) {
-                int oper1 = parseInt(operList[0]);
-                int oper2 = parseInt(operList[2]);
-
-                result = add(oper1);
                 result = add(oper2);
+
             } else if (isSubtractOperator(operList[1])) {
-                int oper1 = parseInt(operList[0]);
-                int oper2 = parseInt(operList[2]);
-
-                result = add(oper1);
                 result = subtract(oper2);
-            } else if (isMultiplyOperator(operList[1])) {
-                int oper1 = parseInt(operList[0]);
-                int oper2 = parseInt(operList[2]);
 
-                result = add(oper1);
+            } else if (isMultiplyOperator(operList[1])) {
                 result = multiply(oper2);
+
+            } else if (isDivideOperator(operList[1])) {
+                result = divide(oper2);
+
             }
 
             return result;
@@ -75,6 +82,10 @@ public class CalculatorTest {
             return word.equals("*");
         }
 
+        private boolean isDivideOperator(String word) {
+            return word.equals("/");
+        }
+
         private int parseInt(String word) {
             return Integer.parseInt(word);
         }
@@ -89,6 +100,10 @@ public class CalculatorTest {
 
         private int multiply(int oper) {
             return result * oper;
+        }
+
+        private int divide(int oper) {
+            return result / oper;
         }
     }
 }
