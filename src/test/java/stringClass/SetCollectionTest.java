2561951ce383d0515db5cf169e99f64eb3834225
SunKyung <sunkyung@queryjet.com>
2019-06-14 18:49:54 +0900

    refactor(StringCalculator) 프로젝트 구조 변경

--- src/test/java/stringClass/SetCollectionTest.java
+++ src/test/java/stringClass/SetCollectionTest.java
@@ -1,3 +1,5 @@
+package stringClass;
+
 import org.assertj.core.internal.Numbers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- src/test/java/stringClass/StringTest.java
+++ src/test/java/stringClass/StringTest.java
@@ -1,3 +1,5 @@
+package stringClass;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
