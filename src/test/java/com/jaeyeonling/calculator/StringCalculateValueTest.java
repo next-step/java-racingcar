b0ac666e087e216659c1c128f3edc2194dd1e887
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-14 10:23:09 +0900

    [test] add 테스트 추가 (green)

--- src/test/java/com/jaeyeonling/calculator/StringCalculateValueTest.java
+++ src/test/java/com/jaeyeonling/calculator/StringCalculateValueTest.java
@@ -43,4 +43,37 @@ public class StringCalculateValueTest {
                     StringCalculateValue.text("123 ^ 23423");
                 });
     }
+
+    @Test
+    void add1() {
+        final StringCalculateValue value = StringCalculateValue.text("1 + 1");
+
+        int result = value.getValue(0);
+        for (int i = 1; i < value.length(); i += 2) {
+            result = value.getOperator(i).apply(result, value.getValue(i + 1));
+        }
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void add2() {
+        final StringCalculateValue value = StringCalculateValue.text("2 + 5");
+
+        int result = value.getValue(0);
+        for (int i = 1; i < value.length(); i += 2) {
+            result = value.getOperator(i).apply(result, value.getValue(i + 1));
+        }
+        assertThat(result).isEqualTo(7);
+    }
+
+    @Test
+    void add3() {
+        final StringCalculateValue value = StringCalculateValue.text("123 + 123");
+
+        int result = value.getValue(0);
+        for (int i = 1; i < value.length(); i += 2) {
+            result = value.getOperator(i).apply(result, value.getValue(i + 1));
+        }
+        assertThat(result).isEqualTo(246);
+    }
 }
