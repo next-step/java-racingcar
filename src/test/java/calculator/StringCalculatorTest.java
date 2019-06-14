50d42795b1a58677446fcaa3d988e0723b07de47
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-14 15:04:37 +0900

    test(calculator): add minus test code

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,12 +3,20 @@ package calculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class StringCalculatorTest {
 
 
     @Test
     void plus() {
         String input = "2 + 3";
-        Assertions.assertThat(StringCalculator.of(input).calculate()).isEqualTo(5);
+        assertThat(StringCalculator.of(input).calculate()).isEqualTo(5);
+    }
+
+    @Test
+    void minut(){
+        String input = "5 - 3";
+        assertThat(StringCalculator.of(input).calculate()).isEqualTo(2);
     }
 }
