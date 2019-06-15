a57a4b48d8715616889f0d367a8fdcb156695f2a
github-byrage <byrage91@gmail.com>
2019-06-15 15:36:14 +0900

    [step2] feature: Car 모델 구현

--- src/main/java/step2/racing/model/Car.java
+++ src/main/java/step2/racing/model/Car.java
@@ -1,4 +1,46 @@
 package step2.racing.model;
 
+import step2.racing.random.RandomGenerator;
+
 public class Car {
+
+    private int position = 0;
+
+    /**
+     * 차량번호
+     */
+    private final int number;
+    private final RandomGenerator randomGenerator;
+
+    private static final int BOUND = 10;
+    private static final int MOVE_VALUE = 4;
+
+    private Car(int number, RandomGenerator randomGenerator) {
+
+        this.number = number;
+        this.randomGenerator = randomGenerator;
+    }
+
+    public static Car of(int number, RandomGenerator randomGenerator) {
+
+        return new Car(number, randomGenerator);
+    }
+
+    public void race() {
+
+        int randomInt = randomGenerator.getRandomIntValue(BOUND);
+        if (isMove(randomInt)) {
+            position++;
+        }
+    }
+
+    private boolean isMove(int randomInt) {
+
+        return randomInt >= MOVE_VALUE;
+    }
+
+    int checkPosition() {
+
+        return position;
+    }
 }
---
+++ src/test/java/step2/racing/model/CarTest.java
@@ -0,0 +1,38 @@
+package step2.racing.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step2.racing.random.StubRandomGenerator;
+
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    private Car car;
+
+    @Test
+    @DisplayName("랜덤숫자가 4 이상 9 이하라면 이동한다.")
+    void race_should_move() {
+
+        IntStream.rangeClosed(4, 9)
+                .forEach(fixedNumber -> {
+                    car = Car.of(0, new StubRandomGenerator(fixedNumber));
+                    car.race();
+                    assertThat(car.checkPosition()).isOne();
+                });
+    }
+
+    @Test
+    @DisplayName("랜덤숫자가 0 이상 3 이하라면 이동하지 않는다.")
+    void race_should_not_move() {
+
+        IntStream.rangeClosed(0, 3)
+                .forEach(fixedNumber -> {
+                    car = Car.of(0, new StubRandomGenerator(fixedNumber));
+                    car.race();
+                    assertThat(car.checkPosition()).isZero();
+                });
+    }
+}
