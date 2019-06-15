dd85279de02054ebca94bc3e78db2f1863156e81
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-16 00:38:53 +0900

    [test] InputView readInt 테스트 추가

--- src/test/java/com/jaeyeonling/racingcar/view/InputViewTest.java
+++ src/test/java/com/jaeyeonling/racingcar/view/InputViewTest.java
@@ -5,23 +5,45 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class InputViewTest {
 
-    private static final String TEST_INPUT = "리뷰어님, 잘부탁드립니다.";
+    private static final String TEST_STRING_INPUT = "리뷰어님, 잘부탁드립니다.";
+    private static final int TEST_INT_INPUT = 100;
 
-    private InputView inputView;
+    @DisplayName("String 값 입력 검증")
+    @Test
+    void readString() {
+        // given
+        final InputView inputView = new InputView(new ByteArrayInputStream(TEST_STRING_INPUT.getBytes()));
 
-    @BeforeEach
-    void setUp() {
-        inputView = new InputView(new ByteArrayInputStream(TEST_INPUT.getBytes()));
+        // when
+        final String readString = inputView.readString();
+
+        // then
+        assertThat(readString).isEqualTo(TEST_STRING_INPUT);
     }
 
-    @DisplayName("값 입력 검증")
+    @DisplayName("int 값 입력 검증")
     @Test
-    void readFromConsole() {
-        assertThat(inputView.readString()).isEqualTo(TEST_INPUT);
+    void readInt() {
+        // given
+        final InputStream inputStream = new DataInputStream(new InputStream() {
+            @Override
+            public int read() {
+                return TEST_INT_INPUT;
+            }
+        });
+        final InputView inputView = new InputView(inputStream);
+
+        // when
+        final int readInt = inputView.readInt();
+
+        // then
+        assertThat(readInt).isEqualTo(TEST_INT_INPUT);
     }
 }
