85bc5ceeaf5959b98c7cde90cb291729f120734d
권병윤 <sksggg123@gmail.com>
2019-06-15 09:53:13 +0900

    test(validationTest) : 연산자 데이터 체크 테스트

--- src/test/java/edu/nextstep/stepone/ValidationTest.java
+++ src/test/java/edu/nextstep/stepone/ValidationTest.java
@@ -1,5 +1,12 @@
 package edu.nextstep.stepone;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +18,32 @@ package edu.nextstep.stepone;
  * create date  : 2019-06-14 15:39
  */
 public class ValidationTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1","2","3", "+", "-", "*", "/"})
+    void 공백제거_리스트반환(String compareStr) {
+        Validation validation = new Validation();
+        assertThat(validation.splitSpace("1 + 2 - 1 * 2 / 3").contains(compareStr)).isTrue();
+    }
+
+    @Test
+    void 연산자_데이터유무체크() {
+        Validation validation = new Validation();
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            validation.isBlank(" ");
+        });
+    }
+
+    @Test
+    void 연산자_타입체크() {
+        OperatorType type = OperatorType.typeCheck("+");
+        assertThat(type.getType()).isEqualTo("+");
+    }
+
+    @Test
+    void 연잔자_예외상황() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            OperatorType type = OperatorType.typeCheck("&");
+        });
+    }
 }
