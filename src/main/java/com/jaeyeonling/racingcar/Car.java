355cd182bd389a9eac7870e6328713888193a8b5
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-15 17:37:01 +0900

    [test] Car 이동 후 위치 변경 테스트 통과

--- src/main/java/com/jaeyeonling/racingcar/Car.java
+++ src/main/java/com/jaeyeonling/racingcar/Car.java
@@ -1,5 +1,14 @@
 package com.jaeyeonling.racingcar;
 
 class Car {
-    
+
+    private int position;
+
+    void moveForward() {
+        position++;
+    }
+
+    int getPosition() {
+        return position;
+    }
 }
