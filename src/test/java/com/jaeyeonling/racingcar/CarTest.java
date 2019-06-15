b6085de9307c7332ccff0f4bbd1d9ff3c7913cb6
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-15 17:36:27 +0900

    [test] Car 이동 후 위치 변경 테스트 추가

--- src/test/java/com/jaeyeonling/racingcar/CarTest.java
+++ src/test/java/com/jaeyeonling/racingcar/CarTest.java
@@ -2,6 +2,8 @@ package com.jaeyeonling.racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,5 +13,21 @@ public class CarTest {
     @Test
     void createCar() {
         assertThat(new Car()).isNotNull();
+    }
+
+    @DisplayName("이동 후 위치 변경 확인")
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 1, 5, 4, 35, 41, 23 })
+    void move(final int moveCount) {
+        // given
+        final Car car = new Car();
+
+        // when
+        for (int i = 0; i < moveCount; i++) {
+            car.moveForward();
+        }
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(moveCount);
     }
 }
