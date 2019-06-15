6645e37fe94a042cda690e4062eba38d1a32ad83
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-15 23:17:34 +0900

    [test] InputView 테스트 코드 추가

---
+++ src/test/java/com/jaeyeonling/racingcar/view/InputViewTest.java
@@ -0,0 +1,28 @@
+package com.jaeyeonling.racingcar.view;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputViewTest {
+
+    private static final String TEST_INPUT = "리뷰어님, 잘부탁드립니다.";
+
+    private InputView inputView;
+
+    @BeforeEach
+    void setUp() {
+        inputView = new InputView(new ByteArrayInputStream(TEST_INPUT.getBytes()));
+    }
+
+    @DisplayName("값 입력받기")
+    @Test
+    void readFromConsole() {
+        assertThat(inputView.readFromConsole()).isEqualTo(TEST_INPUT);
+    }
+}
