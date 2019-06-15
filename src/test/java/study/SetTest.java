109ccfda1fbf6c18f23c548b6cece226d697552f
jaeheon209 <jaeheon209@gmail.com>
2019-06-15 18:26:40 +0900

    test(add) : add set test
    size test 추가
    contains test 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,15 +1,20 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         numbers = new HashSet<Integer>();
 
         numbers.add(1);
@@ -18,5 +23,17 @@ public class SetTest {
         numbers.add(3);
     }
 
-    // test 요구사항 진행
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    // 파라미터를 지정하여 테스트 할 수 있는 어노테이션
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input){
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+
 }
