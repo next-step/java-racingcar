b01e0c25532ef7699b33ddf810b5c9d74650e501
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-15 23:52:15 +0900

    [test] 불필요한 코드 제거

--- src/test/java/com/jaeyeonling/racingcar/view/ResultViewTest.java
+++ src/test/java/com/jaeyeonling/racingcar/view/ResultViewTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
