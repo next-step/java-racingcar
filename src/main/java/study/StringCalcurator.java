d6684114b25dd1a199a2acc4995f6faf0bc934a6
wyparks2 <wyparks2@mrblue.com>
2019-06-15 14:28:58 +0900

    [feature] 문자열 계산기 기능 구현

---
+++ src/main/java/study/StringCalcurator.java
@@ -0,0 +1,28 @@
+package study;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-15
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public class StringCalcurator {
+
+    public int caculate(String input) {
+        String[] splitInputs = input.split(" ");
+        return calculate(splitInputs);
+
+    }
+
+    private int calculate(String[] splitInputs) {
+        int total = Integer.parseInt(splitInputs[0]);
+
+        for (int index = 1; index < splitInputs.length; index += 2) {
+            int left = total;
+            int right = Integer.parseInt(splitInputs[index + 1]);
+            CalculatorOperator operator = CalculatorOperator.fromString(splitInputs[index]);
+            total = operator.calculate(left, right);
+        }
+
+        return total;
+    }
+}
