cdbdf3d6b9f61ee027993fcfa127dcc693c297d2
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-16 01:19:18 +0900

    [refactor] 최대 허용 갯수 수정

--- src/main/java/com/jaeyeonling/racingcar/domain/RacingGameOption.java
+++ src/main/java/com/jaeyeonling/racingcar/domain/RacingGameOption.java
@@ -41,9 +41,9 @@ public class RacingGameOption {
         private static final MoveStrategy DEFAULT_MOVE_STRATEGY = new RandomMoveStrategy();
 
         private static final int MINIMUM_NUMBER_OF_PARTICIPANTS = 0;
-        private static final int MAXIMUM_NUMBER_OF_PARTICIPANTS = 10;
+        private static final int MAXIMUM_NUMBER_OF_PARTICIPANTS = 10_000;
         private static final int MINIMUM_MOVING_COUNT = 0;
-        private static final int MAXIMUM_MOVING_COUNT = 10;
+        private static final int MAXIMUM_MOVING_COUNT = 10_000;
 
         private int numberOfParticipants = DEFAULT_NUMBER_OF_PARTICIPANTS;
         private int movingCount = DEFAULT_MOVING_COUNT;
