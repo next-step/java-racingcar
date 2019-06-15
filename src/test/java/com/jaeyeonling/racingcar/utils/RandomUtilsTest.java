896df9417489e4dad4825857a2a678e60758744b
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-15 18:05:31 +0900

    [test] getIntWithBound 양수 값이 아닐 경우 예외처리 테스트 추가

--- src/test/java/com/jaeyeonling/racingcar/utils/RandomUtilsTest.java
+++ src/test/java/com/jaeyeonling/racingcar/utils/RandomUtilsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class RandomUtilsTest {
 
@@ -12,11 +12,19 @@ public class RandomUtilsTest {
 
     @DisplayName("getIntWithBound 테스트")
     @ParameterizedTest
-    @ValueSource(ints = { 0, 1, 100, 500 })
+    @ValueSource(ints = { 1, 100, 500, 234, 3453 })
     void getIntWithBound(final int bound) {
         for (int i = 0; i < RANDOM_VERIFY_REPEAT_COUNT; i++) {
             final int randomValue = RandomUtils.getIntWithBound(bound);
             assertThat(randomValue).isLessThan(bound);
         }
+    }
+
+    @DisplayName("getIntWithBound bound 양수가 아닌 값 입력 시 예외처리 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = { 0, -1, -324, -4352452 })
+    void getIntWithBoundWithNegative(final int bound) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> RandomUtils.getIntWithBound(bound));
     }
 }
