ad766326e0b2d031698ca2e1b94ef3ea02193f7e
권병윤 <sksggg123@gmail.com>
2019-06-15 09:51:53 +0900

    feat(validation) : 연산자 null, space 체크 기능

--- src/main/java/edu/nextstep/stepone/Validation.java
+++ src/main/java/edu/nextstep/stepone/Validation.java
@@ -1,5 +1,8 @@
 package edu.nextstep.stepone;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +14,34 @@ package edu.nextstep.stepone;
  * create date  : 2019-06-14 15:09
  */
 public class Validation {
+    private String SEPARATOR_SPLIT = " ";
+    private List<String> letter;
+
+    /*
+    null & " " 데이터 체크
+     */
+    public boolean isBlank(String inputData) {
+        if(inputData.trim().equals("") || inputData == null) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    /*
+    사용자가 입력한 문자열(숫자 + 연산자)를 List에 저장
+     */
+    public List<String> splitSpace(String inputData) {
+        String[] tempArray = inputData.split(SEPARATOR_SPLIT);
+        this.letter = Arrays.asList(tempArray);
+
+        return this.letter;
+    }
+
+    /*
+    @return: List data
+     */
+    public List<String> getLetter() {
+        return this.letter;
+    }
+
 }
