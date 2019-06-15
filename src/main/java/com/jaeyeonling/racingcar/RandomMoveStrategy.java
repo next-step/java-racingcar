38058f69f25f18edb9a79b40f88412ef66629c97
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-15 18:24:39 +0900

    [refactor] 매직넘버 제거 및 네이밍 변경

--- src/main/java/com/jaeyeonling/racingcar/RandomMoveStrategy.java
+++ src/main/java/com/jaeyeonling/racingcar/RandomMoveStrategy.java
@@ -4,10 +4,11 @@ import com.jaeyeonling.racingcar.utils.RandomUtils;
 
 public class RandomMoveStrategy implements MoveStrategy {
 
-    private static final int RANDOM_BOUND = 9;
+    private static final int MOVING_RANDOM_BOUND = 9;
+    private static final int MORE_THAN_MOVING_CONDITION = 4;
 
     @Override
     public boolean isMove() {
-        return RandomUtils.getIntWithBound(RANDOM_BOUND) >= 4;
+        return RandomUtils.getIntWithBound(MOVING_RANDOM_BOUND) >= MORE_THAN_MOVING_CONDITION;
     }
 }
