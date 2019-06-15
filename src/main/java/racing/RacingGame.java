6cfa256d8a12776f77c4b95131d1041107fb02e3
Minseok <iamminseokpark@gmail.com>
2019-06-15 17:01:46 +0900

    feat(race): 자동차 수와 이동 횟수 입력 기능 추가

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -1,10 +1,48 @@
 package racing;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class RacingGame {
     private int time;
-    private int[] carPositions = {0, 0, 0};
+    private ArrayList<Car> cars;
 
-    public int[] move() {
+    public RacingGame() {
+
+    }
+
+    public int getNumOfCars() {
+        return cars.size();
+    }
+
+    public void setNumOfCars(int numberOfCars) {
+        cars = new ArrayList<>(numberOfCars);
+
+        for (int i = 0; i < numberOfCars; i++) {
+            cars.add(new Car());
+        }
+    }
+
+    public int getNumOfMove() {
+        return this.time;
+    }
+
+    public void setNumOfMove(int numberOfMoves) {
+        this.time = numberOfMoves;
+    }
+
+    public List<Integer> move() {
         // TODO 구현
+        return Collections.emptyList();
+    }
+
+}
+
+class Car {
+    public int position;
+
+    public Car() {
+        position = 0;
     }
 }
--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -1,4 +1,29 @@
 package racing;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RacingTest {
+    private RacingGame mGame;
+
+    @BeforeEach
+    void setUp() {
+        mGame = new RacingGame();
+    }
+
+    @Test
+    void input_generate_cars() {
+        mGame.setNumOfCars(3);
+
+        assertThat(mGame.getNumOfCars()).isEqualTo(3);
+    }
+
+    @Test
+    void input_move_times() {
+        mGame.setNumOfMove(2);
+
+        assertThat(mGame.getNumOfMove()).isEqualTo(2);
+    }
 }
