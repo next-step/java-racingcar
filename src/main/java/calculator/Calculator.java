f4fc1f40b1f012225514dbfa42f3b5f788842091
seungkil choi <seung-kil.choi@raizegls.com>
2019-06-15 17:52:43 +0900

    [REFACTOR] 입력값 파싱, Integer 값 추출해서 단순 합을 반환하는 로직으로 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,49 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 
-	String input;
+
+	List<Term> terms;
 
 	public Calculator(String input) {
-		this.input = input;
+		terms = parseInput(input);
+	}
+
+	private List<Term> parseInput(String expression){
+		List<Term> terms = new ArrayList<>();
+		String[] tokens = expression.split(" ");
+		for(String token : tokens){
+			try {
+				int value = Integer.parseInt(token);
+				terms.add(new Term(value));
+			}catch (NumberFormatException e){
+				continue;
+			}
+		}
+
+		return terms;
 	}
 
 	public int execute() {
-		return (input == "2 + 4") ? 6 : 5;
+		return terms.stream()
+				.reduce((subtotal, element) -> new Term(subtotal.getValue() + element.getValue()))
+				.orElse(new Term(0))
+				.getValue();
+	}
+
+	private class Term {
+		int value;
+
+		public Term(int value) {
+
+			this.value = value;
+		}
+
+		public int getValue(){
+			return value;
+		}
 	}
 }
