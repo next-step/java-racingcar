addf5c3d505e376556fb056219b87442b09ae638
Jason <jason.sohn28@gmail.com>
2019-06-16 23:18:34 +0900

    add new calculator

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+    }
+
+
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("setup Calculator");
+    }
+
+    @Test
+    void add() {
+        System.out.println("add");
+    }
+}
