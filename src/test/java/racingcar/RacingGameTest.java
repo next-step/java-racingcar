8260666d42d8766b7240471309a431d6aa9854db
KIM JUNGHWAN <writer0713@naver.com>
2019-06-17 00:05:57 +0900

    #step2 [test] RacingGameTest 추가

---
+++ src/test/java/racingcar/RacingGameTest.java
@@ -0,0 +1,63 @@
+package racingcar;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.mocks.MockRandomUtil;
+import racingcar.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import static java.util.stream.Collectors.joining;
+
+public class RacingGameTest {
+
+
+  private MockRandomUtil random;
+  private RacingGame racingGame;
+
+  @BeforeEach
+  void setUp() {
+    this.random = new MockRandomUtil();
+    this.racingGame = new RacingGame();
+  }
+
+  @Test
+  @DisplayName("console에 '--' 가 표시된다")
+  void test() {
+
+    String currentPosition = IntStream.rangeClosed(0, 1)
+            .mapToObj(pos -> "-")
+            .collect(joining());
+
+    System.out.println(currentPosition);
+  }
+
+  @Test
+  @DisplayName("console에 '--' 3줄이 표시된다")
+  void printRacingStatus() {
+
+    int rounds = 3;
+    List<Car> cars = Arrays.asList(
+            new Car(1, random),
+            new Car(2, random),
+            new Car(3, random));
+
+    racingGame.printRacingStatus(rounds, cars);
+
+  }
+
+  @Test
+  @DisplayName("총 3라운드 진행, 1라운드에 '--' 3줄, 2라운드에 '---' 3줄, 3라운드에 '----' 3줄 표시된다")
+  void drive() {
+    List<Car> cars = Arrays.asList(
+            new Car(1, random),
+            new Car(2, random),
+            new Car(3, random));
+
+    racingGame.drive(cars);
+  }
+
+}
