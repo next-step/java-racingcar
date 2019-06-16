66d8431a8a921cd3ddd1c662b8363a168a1f2ceb
jojonari <xkgun@naver.com>
2019-06-16 20:37:30 +0900

    feat(calculator): 곱셈 기능 추가

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -8,4 +8,8 @@ public class Calculator {
     public int subtraction(int num1, int num2) {
         return num1 - num2;
     }
+
+    public int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
 }
