d701b9b28925b9153c864bebb4f0db4bef1bf8f1
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-16 17:33:02 +0900

    [refactor] 상수로 분리

--- src/main/java/com/jaeyeonling/racingcar/utils/RandomUtils.java
+++ src/main/java/com/jaeyeonling/racingcar/utils/RandomUtils.java
@@ -5,12 +5,13 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public final class RandomUtils {
 
+    private static final int MINIMUM_BOUND = 1;
     private static final Random random = ThreadLocalRandom.current();
 
     private RandomUtils() { }
 
     public static int getIntWithBound(final int bound) {
-        if (bound < 1) {
+        if (bound < MINIMUM_BOUND) {
             throw new IllegalArgumentException("bound 값은 양수만 가능합니다.");
         }
 
