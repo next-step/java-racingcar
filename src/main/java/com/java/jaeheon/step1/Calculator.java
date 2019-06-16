8873e85f42de53bcb1c58b5c2ea77ad1d64e7618
jaeheon209 <jaeheon209@gmail.com>
2019-06-17 02:32:29 +0900

    feat(step1): 문자열 계산기 구현
    - Operator 클래스의 OperatorChecker 기능 구현
    - OperatorTest 클래스의 OperatorChecker Test 구현
    
    - Validator 클래스의 isNotBlank, validateSize, isOdd, isNumber 기능 구현
    - ValidatorTest 클래스의 isNotBlank, validateSize, isOdd, isNumber Test 구현
    
    - Calculator 클래스의 extractor, calculator 기능 구현

--- src/main/java/com/java/jaeheon/step1/Calculator.java
+++ src/main/java/com/java/jaeheon/step1/Calculator.java
@@ -1,6 +1,59 @@
 package com.java.jaeheon.step1;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Calculator {
+    String[] inputArray;
+    Validator validator = new Validator();
 
+    List<Integer> extractNumber = new LinkedList<Integer>();
+    List<String> extractOperator = new LinkedList<String>();
 
+    public Calculator(String input) {
+        this.inputArray = input.split(" ");
+        validator.isNotBlank(input);
+        validator.validateSize(inputArray.length);
+    }
+
+    public int run (){
+        extractor();
+        return calculator();
+    }
+
+    private void extractor() {
+        String number;
+        String symbol;
+        int size = inputArray.length;
+
+        for (int i = 1; i < size; i += 2) {
+            number = inputArray[i - 1];
+            symbol = inputArray[i];
+
+            if (validator.isNumber(number)) {
+                extractNumber.add(Integer.parseInt(number));
+            }
+            extractOperator.add(symbol);
+        }
+
+        if (validator.isNumber(inputArray[size - 1])) {
+            extractNumber.add(Integer.parseInt(inputArray[size - 1]));
+        }
+    }
+
+    private int calculator() {
+        Iterator<String> operatorIterator = extractOperator.iterator();
+
+        int output = 0;
+        while (operatorIterator.hasNext()) {
+            String operator = operatorIterator.next();
+            int operand1 = extractNumber.get(0);
+            int operand2 = extractNumber.get(1);
+
+            output = Operator.OperatorChecker(operator).calculator(operand1, operand2);
+            extractNumber.set(0, output);
+        }
+        return output;
+    }
 }
--- src/test/java/com/java/jaeheon/step1/OperatorTest.java
+++ src/test/java/com/java/jaeheon/step1/OperatorTest.java
@@ -3,6 +3,7 @@ package com.java.jaeheon.step1;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class OperatorTest {
 
@@ -19,12 +20,23 @@ public class OperatorTest {
     @Test
     void multiplication() {
         assertThat(Operator.MULTIPLICATION.calculator(5, 3)).isEqualTo(5 * 3);
-
     }
 
     @Test
     void division() {
         assertThat(Operator.DIVISION.calculator(5, 3)).isEqualTo(5 / 3);
-
     }
+
+    @Test
+    void operatorChecker() {
+        assertThat(Operator.OperatorChecker("+")).isEqualTo(Operator.ADDITION);
+        assertThat(Operator.OperatorChecker("-")).isEqualTo(Operator.SUBTRACTION);
+        assertThat(Operator.OperatorChecker("*")).isEqualTo(Operator.MULTIPLICATION);
+        assertThat(Operator.OperatorChecker("/")).isEqualTo(Operator.DIVISION);
+        assertThatIllegalArgumentException().isThrownBy(() -> Operator.OperatorChecker("!"));
+        assertThatIllegalArgumentException().isThrownBy(() -> Operator.OperatorChecker("@"));
+        assertThatIllegalArgumentException().isThrownBy(() -> Operator.OperatorChecker("#"));
+        assertThatIllegalArgumentException().isThrownBy(() -> Operator.OperatorChecker("$"));
+    }
+
 }
