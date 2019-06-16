e609fef2f71945e16c9d7277e3a811ce81c2126a
권병윤 <sksggg123@gmail.com>
2019-06-16 16:00:50 +0900

    test(CarRacingTest) : 자동차들의 전진 기능 테스트

--- src/test/java/edu/nextstep/raing/CarRacingTest.java
+++ src/test/java/edu/nextstep/raing/CarRacingTest.java
@@ -1,7 +1,14 @@
 package edu.nextstep.raing;
 
+import edu.nextstep.raing.model.Car;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -16,7 +23,20 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class CarRacingTest {
 
+    private CarRacing race;
+    private List<Car> cars;
+
     @BeforeEach
     void setUp() {
+        race = new CarRacing(3, 5);
+        cars = race.getCarList();
+    }
+
+    @Test
+    void 자동차_전진() {
+        cars = race.move();
+        int position = cars.get(0).currentPosition();
+        assertThat(position).isGreaterThan(-1);
+        assertThat(position).isLessThan(6);
     }
 }
