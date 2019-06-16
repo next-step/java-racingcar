855f19a722e4a18e708d77bafff0d5ec1d0a0095
1nteger <rhep0820@gmail.com>
2019-06-16 22:50:38 +0900

    [추가] step1 문자열 연산 메서드 추가

--- src/main/java/step1/CalculatorApplication.java
+++ src/main/java/step1/CalculatorApplication.java
@@ -14,12 +14,42 @@ public class CalculatorApplication {
         String inputString = scan.nextLine();
         scan.close();
 
-        int result = operateCalculator(inputString);
+        String result = operateCalculator(inputString);
         System.out.println("연산 결과 : " + result);
     }
 
-    public static int operateCalculator(String inputString) {
-        return 1;
+    public static String operateCalculator(String inputString) {
+
+        try {
+            stringValidation(inputString);
+            String[] inputElements = inputString.split(" ");
+            int calculationResult = Integer.parseInt(inputElements[0]);
+
+            for(int i=1; i<inputElements.length; i+=2) {
+                operatorTypeValidation(inputElements[i]);
+                calculationResult = calculate(inputElements[i], calculationResult, inputElements[i+1]);
+            }
+            return String.valueOf(calculationResult);
+
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return "공백을 입력하셨거나, 연산자 기호가 아닌 기호를 입력하셨습니다.";
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            return "0으로 나눌 수 없습니다.";
+        }
+    }
+
+    public static int calculate(String operator, int leftValue, String rightValueInString) {
+
+        int rightValue = Integer.parseInt(rightValueInString);
+
+        if("+".equals(operator)) return Calculator.add(leftValue, rightValue);
+        if("-".equals(operator)) return Calculator.subtract(leftValue, rightValue);
+        if("*".equals(operator)) return Calculator.multiple(leftValue, rightValue);
+        if("/".equals(operator)) return Calculator.divide(leftValue, rightValue);
+
+        return -1;
     }
 
     public static void stringValidation(String inputString) {
