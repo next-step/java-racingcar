05c26ec9b7de233dca29352a67aac49144e01386
권병윤 <sksggg123@gmail.com>
2019-06-16 14:45:41 +0900

    test(Car) : 자동차 객체 생성 및 멤버변수 테스트

---
+++ src/test/java/edu/nextstep/raing/model/CarTest.java
@@ -0,0 +1,26 @@
+package edu.nextstep.raing.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-racingcar
+ * create date  : 2019-06-16 14:40
+ */
+class CarTest {
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void currentPosition() {
+    }
+}
