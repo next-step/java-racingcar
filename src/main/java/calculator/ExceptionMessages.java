6c552febaf32adf359612f3e172f0e97d20c00c8
KIM JUNGHWAN <writer0713@naver.com>
2019-06-16 16:12:36 +0900

    [refactor] Exception 관련 message들을 한 곳에서 관리하도록 분리

---
+++ src/main/java/calculator/ExceptionMessages.java
@@ -0,0 +1,8 @@
+package calculator;
+
+/**
+ * Exception Message 들을 이곳에 모아서 공통으로 관리합니다.
+ */
+public interface ExceptionMessages {
+     String ILLEGAL_ARGUMENT_EXCEPTION_MSG = "입력값을 제대로 입력해주세요.";
+}
