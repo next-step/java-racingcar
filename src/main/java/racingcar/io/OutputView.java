2bf627f8f17b61347ab8dffcbd0552a69e5badae
Juyoung <juy5790@outlook.com>
2019-06-16 11:43:25 +0900

    feat(racingCar): io 분리

---
+++ src/main/java/racingcar/io/OutputView.java
@@ -0,0 +1,41 @@
+package racingcar.io;
+
+import racingcar.model.Car;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class OutputView {
+
+    private static final String VISUAL_EXPRESSION = "-";
+
+    public static void start(){
+        System.out.println("게임을 시작합니다.");
+    }
+
+    public static void playResult(List<Car> cars){
+        for (Car car : cars) {
+            carPosition(car);
+        }
+        newLine();
+    }
+
+    private static void carPosition(Car car) {
+        print(VISUAL_EXPRESSION);
+        IntStream.range(0, car.getPosition())
+                .forEach(value -> print(VISUAL_EXPRESSION));
+        newLine();
+    }
+
+    public static void end(){
+        System.out.println("게임을 종료합니다.");
+    }
+
+    private static void newLine() {
+        System.out.println();
+    }
+
+    private static void print(String text){
+        System.out.print(text);
+    }
+}
