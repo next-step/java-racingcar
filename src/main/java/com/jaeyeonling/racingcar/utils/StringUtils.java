f9c39c4d487574abf63fc423385f21c22d4c3f16
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-16 17:37:42 +0900

    [refactor] 매번 String이 생성되지 않게 개선

--- src/main/java/com/jaeyeonling/racingcar/utils/StringUtils.java
+++ src/main/java/com/jaeyeonling/racingcar/utils/StringUtils.java
@@ -3,12 +3,18 @@ package com.jaeyeonling.racingcar.utils;
 public final class StringUtils {
 
     public static final String NEW_LINE = "\n";
-    public static final String NULL_STRING = "\0";
+
+    private static final int REPEAT_START_INDEX = 0;
 
     private StringUtils() { }
 
     public static String repeat(final CharSequence target,
                                 final int repeatCount) {
-        return new String(new char[repeatCount]).replace(NULL_STRING, target);
+        final StringBuilder repeatBuilder = new StringBuilder();
+        for (int i = REPEAT_START_INDEX; i < repeatCount; i++) {
+            repeatBuilder.append(target);
+        }
+
+        return repeatBuilder.toString();
     }
 }
