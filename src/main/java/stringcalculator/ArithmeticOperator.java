5f3042f317951404f02dd73e6a23741ea50243f8
jhnam <jhnam@woowahan.com>
2019-06-16 17:33:01 +0900

    feat: 산술연산을 처리하는 객체 추가

---
+++ src/main/java/stringcalculator/ArithmeticOperator.java
@@ -0,0 +1,35 @@
+package stringcalculator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+
+public class ArithmeticOperator {
+
+    private Map<Type, BinaryOperator<Integer>> operator = new HashMap<>();
+
+    public ArithmeticOperator() {
+        operator.put(Type.ADDITION, (num1, num2) -> num1 + num2);
+        operator.put(Type.SUBTRACTION, (num1, num2) -> num1 - num2);
+        operator.put(Type.MULTIPLICATION, (num1, num2) -> num1 * num2);
+        operator.put(Type.DIVISION, (num1, num2) -> {
+            if (num1 == 0) {
+                throw new IllegalArgumentException("Can not divide by zero");
+            }
+            return num1 / num2;} );
+    }
+
+    public Integer operate(Type type, Integer num1, Integer num2) {
+        return operator.get(type).apply(num1, num2);
+    }
+
+    public enum Type {
+        ADDITION("+"), SUBTRACTION("-"), MULTIPLICATION("*"), DIVISION("/");
+
+        private String symbol;
+
+        Type(String symbol) {
+            this.symbol = symbol;
+        }
+    }
+}
