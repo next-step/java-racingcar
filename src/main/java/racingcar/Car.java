d80ae2066c3eb37fad2f934a026f2825f7ae6bdc
sogoagain <imyong0@gmail.com>
2019-06-16 19:11:50 +0900

    feat(자동차): 자동차 역할하는 Car 클래스 작성, DrivingRule 인터페이스 추출
    Car 제공 기능:
    - move: 규칙에 따라 이동할 수 있다.
    
    DrivingRule 작성 이유:
    - Car의 이동 기능을 테스트 하기 위해 Random 조건으로 이동하는 것뿐만 아니라 무조건 이동할 수 있는 규칙을 적용 하기 위하여 인터페이스 추출

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class Car {
+    private DrivingRule drivingRule;
+    private int position = 0;
+
+    private Car(DrivingRule drivingRule) {
+        this.drivingRule = drivingRule;
+    }
+
+    public static Car createCar() {
+        return new Car(RandomDrivingRule.getInstance());
+    }
+
+    public static Car createCarWithDrivingRule(DrivingRule drivingRule) {
+        if (drivingRule == null) {
+            return createCar();
+        }
+        return new Car(drivingRule);
+    }
+
+    public int move() {
+        if (!drivingRule.isMovable()) {
+            return position;
+        }
+        return ++position;
+    }
+}
