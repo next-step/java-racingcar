a3569cc81a8b14cee55867370ba9bff984045fa9
sy.kim <kauksy9022@gmail.com>
2019-06-16 17:31:00 +0900

    main패키지 구현완료

--- src/main/java/step1/InputMain.java
+++ src/main/java/step1/InputMain.java
@@ -1,7 +1,9 @@
 package step1;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+/**
+ * 메인클래스: 입력받고, 각 유효성 검사를 호출 및 결과 값 도출
+ */
 
 public class InputMain {
 
@@ -17,15 +19,18 @@ public class InputMain {
         // 1-2. operatorCheck
         Calculator cal = new Calculator();
         final String[] data = str.split(" ");
-        int answer = Integer.parseInt(data[0]);
+        vCheck.numberCheck(data[0]);
+        double answer = Integer.parseInt(data[0]);
 
         for(int i =1; i < data.length; i+=2){
             vCheck.operatorCheck(data[i]);
-//            answer =
+            vCheck.numberCheck(data[i+1]);
+            answer = cal.callCalculate(answer, data[i], Integer.parseInt(data[i+1]));
 
         }
 
-
+        System.out.println("연산 결과는 나누기를 고려하여, 소수점 둘째자리까지 출력됩니다.");
+        System.out.format("%.2f%n", answer);
     }
 
 }
--- src/main/java/step1/ValidationCheck.java
+++ src/main/java/step1/ValidationCheck.java
@@ -4,17 +4,32 @@ import org.apache.commons.lang3.ObjectUtils;
 
 import java.util.*;
 
+/**
+ * 유효성 검사를 하는 클래스
+ */
+
 public class ValidationCheck {
    ArrayList<String> operatorList = new ArrayList<>();
-    // 1. nul Check
+    // 1. null Check
     public void nullCheck(String str) {
 
         if(ObjectUtils.equals(str, null) || str.isEmpty()){
-            throw new IllegalArgumentException("NULL 값으로 계산 불가능 합니다. 재입력 해주세요.");
+            throw new IllegalArgumentException("NULL 혹은 빈 값으로 계산 불가능 합니다. 재입력 해주세요.");
         }
     }
 
-    // 2. operatorCheck
+    // 2. numberCheck
+    public void numberCheck(String str) {
+
+        if(ObjectUtils.equals(str, null) || str.isEmpty()){
+            throw new IllegalArgumentException("정수가 입력되어야 할 자리에 NULL 혹은 빈 값이 입력 되었습니다. 재입력 해주세요.");
+        }
+        if(!str.matches("^[0-9]*$")){
+            throw new IllegalArgumentException("숫자가 아닌 값이 입력 되었거나 소수가 입력되었습니다. 재입력 해주세요.");
+        }
+    }
+
+    // 3. operatorCheck
     public void operatorCheck(String str){
         operatorList.add("+");
         operatorList.add("-");
@@ -24,7 +39,6 @@ public class ValidationCheck {
         if(!operatorList.contains(str)){
             throw new IllegalArgumentException("연산자(+, -, *, /)가 아닌 값이 있습니다. 재입력 해주세요.");
         }
-
 
     }
 
