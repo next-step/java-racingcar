9c46ed7be3b83f40f70a24148c1e954ee752f902
seungkil choi <seung-kil.choi@raizegls.com>
2019-06-18 03:44:30 +0900

    게임 실행코드에 주석 추가

--- src/main/java/RacingGameRunner.java
+++ src/main/java/RacingGameRunner.java
@@ -17,15 +17,22 @@ public class RacingGameRunner {
 		System.out.println("시도할 회수는 몇 회 인가요?");
 		int round = scanner.nextInt();
 
+		// 게임 스테이지빌더 생성
 		Stage.StageBuilder builder = Stage.builder(entrySize, round);
+
+		// 플레이어 추가
 		for(int i = 0; i < entrySize; i++) {
 			builder.addToEntry(new RandomAcceleratePlayer(new ThresholdEngineCar()));
 		}
 
+		// 출력개체 설정
 		RacingWatcher watcher = new DashTrackingWatcher((message -> System.out.println(message)));
 		builder.watcher(watcher);
+
+		// 스테이지 생성
 		Stage stage = builder.build();
 
+		// Play!
 		while(stage.getRemainingRounds() > 0){
 			stage.playRound();
 		}
