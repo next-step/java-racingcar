5fcb8b69240896a73ad5e140e85cd940764dac1a
권병윤 <sksggg123@gmail.com>
2019-06-17 15:53:14 +0900

    refactor(CalculatorApplication) : 연산횟수를 별도의 메서드로 분리하여 반환

--- src/main/java/edu/nextstep/stepone/CalculatorApplication.java
+++ src/main/java/edu/nextstep/stepone/CalculatorApplication.java
@@ -20,7 +20,7 @@ public class CalculatorApplication {
 
     private List<String> letter;
     private static final int FIRST_INDEX = 0;
-    private static final int LAST_INDEX = 2;
+    private static final int NEXT_INDEX = 2;
     private int totalNumber = 0;
 
     public static void main(String[] args) {
@@ -41,7 +41,7 @@ public class CalculatorApplication {
 
         totalNumber = this.utils.convertInt(this.letter.get(0));
 
-        for (int i = FIRST_INDEX; i < this.letter.size() - 2; i += LAST_INDEX) {
+        for (int i = FIRST_INDEX; i < calculateLastIndex(); i += NEXT_INDEX) {
             OperatorType type = OperatorType.typeCheck(this.letter.get(i + 1));
             int second = this.utils.convertInt(this.letter.get(i + 2));
 
@@ -51,4 +51,7 @@ public class CalculatorApplication {
         return totalNumber;
     }
 
+    private int calculateLastIndex() {
+        return this.letter.size() - NEXT_INDEX;
+    }
 }
