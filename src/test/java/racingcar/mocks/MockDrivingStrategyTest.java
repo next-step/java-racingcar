2c198a37984ec8d8ca2228804873b0c23b287ceb
KIM JUNGHWAN <writer0713@naver.com>
2019-06-17 14:13:06 +0900

    #step3 [test] 테스트추가
    MockDrivingStrategy 클래스에 대한 테스트 추가

---
+++ src/test/java/racingcar/mocks/MockDrivingStrategyTest.java
@@ -0,0 +1,30 @@
+package racingcar.mocks;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+import racingcar.util.DrivingStrategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MockDrivingStrategyTest {
+
+    @Test
+    @DisplayName("Singleton으로 동작하는지")
+    void getInstance() {
+        DrivingStrategy strategy1 = MockDrivingStrategy.getInstance();
+        DrivingStrategy strategy2 = MockDrivingStrategy.getInstance();
+
+        assertThat(strategy1).isSameAs(strategy2);
+    }
+
+    @Test
+    @DisplayName("throwDice는 무조건 10 return")
+    void throwDice() {
+        DrivingStrategy strategy = MockDrivingStrategy.getInstance();
+        assertThat(strategy.throwDice()).isEqualTo(10);
+        assertThat(strategy.throwDice()).isEqualTo(10);
+        assertThat(strategy.throwDice()).isEqualTo(10);
+        assertThat(strategy.throwDice()).isEqualTo(10);
+    }
+}
