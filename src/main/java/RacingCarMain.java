b0ba97bae347c9c8c94a4a555baae79aa55d21d7
sogoagain <imyong0@gmail.com>
2019-06-17 19:01:21 +0900

    refactor: RacingGame의 run 메서드가 boolean값을 리턴하도록 변경
    변경 내역:
    -  RacingGame의 run 메서드가 정상 종료되면 true를 반환하고 그렇지 않으면 false를 반환하도록 변경

--- src/main/java/RacingCarMain.java
+++ src/main/java/RacingCarMain.java
@@ -3,6 +3,10 @@ import racingcar.controller.RacingGame;
 public class RacingCarMain {
     public static void main(String[] args) {
         RacingGame racingGame = new RacingGame();
-        racingGame.run();
+        boolean isGameEnd = racingGame.run();
+
+        if (!isGameEnd) {
+            System.out.println("경주가 진행되지 못했습니다.");
+        }
     }
 }
--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -4,20 +4,27 @@ import racingcar.model.Cars;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class RacingGame {
-    public void run() {
-        int numberOfCars = InputView.inputNumberOfCars();
-        int numberOfTries = InputView.inputNumberOfTries();
+    public boolean run() {
+        int numberOfCars, numberOfTries;
+
+        try {
+            numberOfCars = InputView.inputNumberOfCars();
+            numberOfTries = InputView.inputNumberOfTries();
+        } catch (InputMismatchException exception) {
+            return false;
+        }
 
         Cars cars = new Cars(numberOfCars);
-
         OutputView.printResultMessage();
-
         for (int i = 0; i < numberOfTries; i++) {
             List<Integer> positions = cars.move();
             OutputView.printCarPositions(positions);
         }
+
+        return true;
     }
 }
