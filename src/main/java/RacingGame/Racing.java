e81a13124b88da2394cc825669555470fac131a2
jojonari <xkgun@naver.com>
2019-06-17 21:02:27 +0900

    feat(racingGame): 경주 객체에 입력받은 자동차 수만큼 자동차 객체를 세팅한다.

--- src/main/java/RacingGame/Racing.java
+++ src/main/java/RacingGame/Racing.java
@@ -1,5 +1,8 @@
 package RacingGame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Racing {
     private int time;
 
@@ -9,5 +12,14 @@ public class Racing {
 
     public void setTime(int time) {
         this.time = time;
+    }
+
+    public List<Car> arrayWithCar(int carCount) {
+        ArrayList<Car> cars = new ArrayList<>(carCount);
+        for (int i = 0; i < carCount; i++) {
+            cars.add(new Car());
+        }
+
+        return cars;
     }
 }
--- src/test/java/RacingGame/RacingTest.java
+++ src/test/java/RacingGame/RacingTest.java
@@ -2,10 +2,15 @@ package RacingGame;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class RacingTest {
 
@@ -21,5 +26,13 @@ class RacingTest {
     void setTime(int time) {
         racing.setTime(time);
         assertThat(racing.getTime()).isEqualTo(time);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void setCars(int carCount) {
+        List<Car> cars = racing.arrayWithCar(carCount);
+        assertThat(cars.size()).isEqualTo(carCount);
+        assertThat(cars.get(0).getPosition()).isEqualTo(0);
     }
 }
