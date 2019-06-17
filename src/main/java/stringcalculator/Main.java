e690310ad2a6a271055371e0e1f07f3faf977b3a
Jinho Nam <greatpn@gmail.com>
2019-06-17 09:27:05 +0900

    feat: 문자열계산 main 객체 추가

---
+++ src/main/java/stringcalculator/Main.java
@@ -0,0 +1,8 @@
+package stringcalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        StringCalculator stringCalculator = new StringCalculator();
+        System.out.println(stringCalculator.calculate(args[0]));
+    }
+}
