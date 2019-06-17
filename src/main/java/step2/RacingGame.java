95bbca624141f7103ce950cde049085987344d74
feelydh <feelydh@echomarketing.co.kr>
2019-06-18 02:28:30 +0900

    style: 불필요한 import 제거

--- src/main/java/step2/RacingGame.java
+++ src/main/java/step2/RacingGame.java
@@ -2,7 +2,6 @@ package step2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class RacingGame {
 
--- src/test/java/step2/RacingGameTest.java
+++ src/test/java/step2/RacingGameTest.java
@@ -1,11 +1,9 @@
 package step2;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
