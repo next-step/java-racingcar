2b031e255fbc8fd084af297c797386dc54dd1be9
wyparks2 <wyparks2@mrblue.com>
2019-06-17 23:30:14 +0900

    [feat] RacingGame 클래스 생성

---
+++ src/main/java/study/racingcar/RacingGame.java
@@ -0,0 +1,20 @@
+package study.racingcar;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-17
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public class RacingGame {
+    private final int carCount;
+    private final int time;
+
+
+    public RacingGame(int carCount, int time) {
+        this.carCount = carCount;
+        this.time = time;
+    }
+
+    public void start() {
+    }
+}
