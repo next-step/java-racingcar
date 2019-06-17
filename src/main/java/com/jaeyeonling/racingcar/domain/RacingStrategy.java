e26135fac660a6793e18c4394e0580fc1a7f1a7c
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-17 10:39:25 +0900

    [refactor] 테스트를 위해 상수 접근제어자 수정

--- src/main/java/com/jaeyeonling/racingcar/domain/RacingStrategy.java
+++ src/main/java/com/jaeyeonling/racingcar/domain/RacingStrategy.java
@@ -2,7 +2,7 @@ package com.jaeyeonling.racingcar.domain;
 
 public class RacingStrategy extends StandardOverMoveStrategy {
 
-    private static final int MORE_THAN_MOVING_CONDITION = 4;
+    static final int MORE_THAN_MOVING_CONDITION = 4;
 
     @Override
     protected int getStandard() {
