11429b5cf5cfbf47d9da228e1d5e856cb6ea9be8
pch8388 <pch838811@gmail.com>
2019-06-18 01:55:34 +0900

    refactor : 피드백 반영 랜덤 값 체크 Car 클래스에서 하도록 변경 가독성을 위해 ++ 연산자 사용

--- src/main/java/racinggame/model/Car.java
+++ src/main/java/racinggame/model/Car.java
@@ -1,21 +1,22 @@
 package racinggame.model;
 
+import racinggame.util.RandomValueUtil;
+
 import java.util.stream.IntStream;
 
 public class Car {
-	private int status;
+
 	private static final int DEFAULT_STATUS = 1;
+	private static final int MOVE_VALUE = 4;
+
+	private int status;
 
 	public Car() {
 		this.status = DEFAULT_STATUS;
 	}
 
 	public int move() {
-		status = status + 1;
-		return status;
-	}
-
-	public int getStatus() {
+		status++;
 		return status;
 	}
 
@@ -24,5 +25,9 @@ public class Car {
 		IntStream.range(0, status)
 						 .forEach(i -> stringBuilder.append("-"));
 		return stringBuilder.toString();
+	}
+
+	public boolean isMove() {
+			return RandomValueUtil.getRandomValue() >= MOVE_VALUE;
 	}
 }
--- src/main/java/racinggame/model/Cars.java
+++ src/main/java/racinggame/model/Cars.java
@@ -1,41 +1,30 @@
 package racinggame.model;
 
-import racinggame.util.RandomValueUtil;
-
 import java.util.List;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
 
 public class Cars {
-	private final List<Car> cars;
-	private final static int MOVE_VALUE = 4;
+  private final List<Car> cars;
 
-	public Cars(int carCount) {
-		cars = IntStream.range(0, carCount)
-										.mapToObj(i -> new Car())
-										.collect(toList());
-	}
 
-	public int[] getCarsStatus() {
-		return cars.stream()
-							 .mapToInt(Car::getStatus)
-			         .toArray();
-	}
+  public Cars(int carCount) {
+    cars = IntStream.range(0, carCount)
+                    .mapToObj(i -> new Car())
+                    .collect(toList());
+  }
 
-	public void carsMove() {
-		cars.stream()
-			  .filter(c -> isMove())
-			  .forEach(Car::move);
-	}
+  public void carsMove() {
+    cars.stream()
+        .filter(Car::isMove)
+        .forEach(Car::move);
+  }
 
-	public List<String> viewCarsStatus() {
-		return cars.stream()
-							 .map(Car::viewStatus)
-							 .collect(toList());
-	}
+  public List<String> viewCarsStatus() {
+    return cars.stream()
+               .map(Car::viewStatus)
+               .collect(toList());
+  }
 
-	private boolean isMove() {
-		return RandomValueUtil.getRandomValue() >= MOVE_VALUE;
-	}
 }
