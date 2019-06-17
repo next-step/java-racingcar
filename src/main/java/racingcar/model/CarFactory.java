e0822b77781e2d4e93e08eb9dbd4f53603fa3057
KIM JUNGHWAN <writer0713@naver.com>
2019-06-17 19:58:44 +0900

    #step3_1 [refactor] CarFactory의 generate(String name) 메소드 추가
    car name을 인자로 받아서 Car을 생성하는 generate 메소드 추가

--- src/main/java/racingcar/model/CarFactory.java
+++ src/main/java/racingcar/model/CarFactory.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,6 +11,12 @@ public class CarFactory {
     public static List<Car> generate(int players) {
         return range(0, players)
                 .mapToObj(Car::new)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Car> generate(String[] names) {
+        return Arrays.stream(names)
+                .map(Car::new)
                 .collect(Collectors.toList());
     }
 }
