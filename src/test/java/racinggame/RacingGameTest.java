fa260058e8a0361dbd9817314c2b500f15f75056
pch8388 <pch838811@gmail.com>
2019-06-18 02:30:12 +0900

    refactor : test case 가독성 향상을 위해 리팩토링

--- src/test/java/racinggame/RacingGameTest.java
+++ src/test/java/racinggame/RacingGameTest.java
@@ -1,30 +1,24 @@
 package racinggame;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racinggame.model.Car;
 import racinggame.model.Cars;
 import racinggame.util.RandomValueUtil;
 
-import java.util.stream.IntStream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
 
 	@DisplayName("랜덤함수 테스트")
-	@Test
+	@RepeatedTest(10)
 	void getRandomValue() {
-		int testSize = 10000;
 
-		for (int i = 0; i < testSize; i++) {
-			int randomValue = RandomValueUtil.getRandomValue();
-			assertThat(IntStream.rangeClosed(0, 9)
-				.anyMatch(r -> r == randomValue))
-				.isTrue();
-		}
+		int randomValue = RandomValueUtil.getRandomValue();
+		assertThat(randomValue).isGreaterThan(-1)
+													 .isLessThan(10);
 	}
 
 	@DisplayName("자동차 이동 테스트")
@@ -37,7 +31,7 @@ public class RacingGameTest {
 			car.move();
 		}
 
-		assertThat(car.getStatus()).isEqualTo(movePoint);
+		assertThat(car.getStatus()).isEqualTo(movePoint + 1);
 	}
 
 	@DisplayName("자동차 생성 테스트")
@@ -45,6 +39,6 @@ public class RacingGameTest {
 	@ValueSource(ints = {3, 4, 7})
 	void createCars(int carCount) {
 		final Cars cars = new Cars(carCount);
-		assertThat(cars.getCarsStatus().length).isEqualTo(carCount);
+		assertThat(cars.viewCarsStatus()).hasSize(carCount);
 	}
 }
