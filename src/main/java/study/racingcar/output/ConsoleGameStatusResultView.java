14fb5234d358c47b540cfb51e1c899126915634e
wyparks2 <wyparks2@mrblue.com>
2019-06-17 23:38:29 +0900

    [refactor] ConsoleGameStatusResultView 구현

---
+++ src/main/java/study/racingcar/output/ConsoleGameStatusResultView.java
@@ -0,0 +1,37 @@
+package study.racingcar.output;
+
+import study.racingcar.model.Car;
+
+import java.util.List;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-17
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public class ConsoleGameStatusResultView implements ResultView {
+    private static final String POSITION_TEXT = "-";
+
+    @Override
+    public void print(List<Car> cars) {
+        for (Car car : cars)
+            this.print(car);
+
+        System.out.println();
+    }
+
+    private void print(Car car) {
+        System.out.println(convertPositionToString(car));
+    }
+
+    private String convertPositionToString(Car car) {
+        final StringBuilder sb = new StringBuilder();
+
+        int movedDistance = car.getPosition();
+        while (movedDistance-- > 0)
+            sb.append(POSITION_TEXT);
+
+        return sb.toString();
+    }
+
+}
--- src/main/java/study/racingcar/output/ResultView.java
+++ src/main/java/study/racingcar/output/ResultView.java
@@ -1,9 +1,14 @@
 package study.racingcar.output;
 
+import study.racingcar.model.Car;
+
+import java.util.List;
+
 /**
  * Created by wyparks2@gmail.com on 2019-06-17
  * Blog : http://wyparks2.github.io
  * Github : http://github.com/wyparks2
  */
 public interface ResultView {
+    void print(List<Car> cars);
 }
