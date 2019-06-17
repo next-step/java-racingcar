797bc4fb3da6669cba65fe767b2b90857107c253
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-17 11:57:05 +0900

    [refector] repeat 리팩토링

--- src/main/java/com/jaeyeonling/racingcar/utils/StringUtils.java
+++ src/main/java/com/jaeyeonling/racingcar/utils/StringUtils.java
@@ -4,17 +4,11 @@ public final class StringUtils {
 
     public static final String NEW_LINE = "\n";
 
-    private static final int REPEAT_START_INDEX = 0;
-
     private StringUtils() { }
 
     public static String repeat(final CharSequence target,
                                 final int repeatCount) {
-        final StringBuilder repeatBuilder = new StringBuilder();
-        for (int i = REPEAT_START_INDEX; i < repeatCount; i++) {
-            repeatBuilder.append(target);
-        }
-
-        return repeatBuilder.toString();
+        return String.valueOf(target)
+                .repeat(repeatCount);
     }
 }
