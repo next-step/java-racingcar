b118b413edb0f0fc86539583753d2fd1b1adb8b2
wyparks2 <wyparks2@mrblue.com>
2019-06-17 23:20:53 +0900

    [feat] InputView, OutputView 인터페이스 생성

---
+++ src/main/java/study/racingcar/input/InputView.java
@@ -0,0 +1,9 @@
+package study.racingcar.input;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-17
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public interface InputView {
+}
---
+++ src/main/java/study/racingcar/output/ResultView.java
@@ -0,0 +1,9 @@
+package study.racingcar.output;
+
+/**
+ * Created by wyparks2@gmail.com on 2019-06-17
+ * Blog : http://wyparks2.github.io
+ * Github : http://github.com/wyparks2
+ */
+public interface ResultView {
+}
