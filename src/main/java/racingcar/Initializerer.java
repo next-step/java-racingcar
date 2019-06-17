d887c1fd7feb91746a3cd438a94451f60ed22d5c
changjun89 <leechang0423@naver.com>
2019-06-17 15:12:31 +0900

    feature(racingCar) 사용자가 몇 대의 자동차로 몇 번의 이동을 할껀지 입력할수 있다.

---
+++ src/main/java/racingcar/Initializerer.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class Initializerer {
+
+  Scanner scanner = new Scanner(System.in);
+  private int numberOfCar;
+  private int numberOfTimes;
+
+  public void initNumberOfCar() {
+    numberOfCar = scanner.nextInt();
+  }
+
+  public void initNumberOfTimes() {
+    numberOfTimes = scanner.nextInt();
+  }
+
+  public int getNumberOfCar() {
+    return numberOfCar;
+  }
+
+  public int getNumberOfTimes() {
+    return numberOfTimes;
+  }
+
+}
--- src/main/java/racingcar/RacingGameLauncher.java
+++ src/main/java/racingcar/RacingGameLauncher.java
@@ -3,8 +3,11 @@ package racingcar;
 public class RacingGameLauncher {
 
   public static void launch() {
+    Initializerer initializerer = new Initializerer();
     InputView.askNumberOfCar();
+    initializerer.initNumberOfCar();
     InputView.askNumberOfTimes();
+    initializerer.initNumberOfTimes();
   }
 
   public static void main(String[] args) {
