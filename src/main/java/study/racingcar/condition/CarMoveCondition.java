dd2ec6ec215a0e81735ceb67e787c5f01a72927e
wyparks2 <wyparks2@mrblue.com>
2019-06-17 23:28:19 +0900

    CarMoveCondition 구현

--- src/main/java/study/racingcar/condition/CarMoveCondition.java
+++ src/main/java/study/racingcar/condition/CarMoveCondition.java
@@ -1,9 +1,21 @@
 package study.racingcar.condition;
 
+import java.util.Random;
+
 /**
  * Created by wyparks2@gmail.com on 2019-06-17
  * Blog : http://wyparks2.github.io
  * Github : http://github.com/wyparks2
  */
 public class CarMoveCondition {
+    private static final Random RANDOM = new Random();
+    private static final int NUMBER_OF_CASE = 10;
+    private static final int CAN_MOVING_CONDITION = 4;
+
+    private CarMoveCondition() {
+    }
+
+    public static boolean canMove() {
+        return RANDOM.nextInt(NUMBER_OF_CASE) >= CAN_MOVING_CONDITION;
+    }
 }
