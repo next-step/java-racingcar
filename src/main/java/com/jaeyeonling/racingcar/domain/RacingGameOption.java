b562902377ea30534d5e861d989fdca031e76981
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-17 22:21:58 +0900

    [refactor] 함수 매개변수 제거

--- src/main/java/com/jaeyeonling/racingcar/domain/RacingGameOption.java
+++ src/main/java/com/jaeyeonling/racingcar/domain/RacingGameOption.java
@@ -39,23 +39,23 @@ public class RacingGameOption {
         private MoveStrategy moveStrategy = DEFAULT_MOVE_STRATEGY;
 
         public Builder numberOfParticipants(final int numberOfParticipants) {
-            checkNumberOfParticipantsConstraints(numberOfParticipants);
-
             this.numberOfParticipants = numberOfParticipants;
+            checkNumberOfParticipantsConstraints();
+
             return this;
         }
 
         public Builder movingCount(final int movingCount) {
-            checkMovingCountConstraints(movingCount);
-
             this.movingCount = movingCount;
+            checkMovingCountConstraints();
+
             return this;
         }
 
         public Builder moveStrategy(final MoveStrategy moveStrategy) {
-            checkMovingStrategyConstraints(moveStrategy);
-
             this.moveStrategy = moveStrategy;
+            checkMovingStrategyConstraints();
+
             return this;
         }
 
@@ -63,7 +63,7 @@ public class RacingGameOption {
             return new RacingGameOption(this);
         }
 
-        private void checkNumberOfParticipantsConstraints(final int numberOfParticipants) {
+        private void checkNumberOfParticipantsConstraints() {
             if (numberOfParticipants >= MAXIMUM_NUMBER_OF_PARTICIPANTS) {
                 throwConstraintsException(String.format("numberOfParticipants 값은 %d 이상이 될 수 없습니다. (입력 값: %d)",
                         MAXIMUM_NUMBER_OF_PARTICIPANTS,
@@ -76,7 +76,7 @@ public class RacingGameOption {
             }
         }
 
-        private void checkMovingCountConstraints(final int movingCount) {
+        private void checkMovingCountConstraints() {
             if (movingCount >= MAXIMUM_MOVING_COUNT) {
                 throwConstraintsException(String.format("movingCount 값은 %d 이상이 될 수 없습니다. (입력 값: %d)",
                         MAXIMUM_MOVING_COUNT,
@@ -89,7 +89,7 @@ public class RacingGameOption {
             }
         }
 
-        private void checkMovingStrategyConstraints(final MoveStrategy moveStrategy) {
+        private void checkMovingStrategyConstraints() {
             if (Objects.isNull(moveStrategy)) {
                 throwConstraintsException("MoveStrategy 는 null 일 수 없습니다.");
             }
