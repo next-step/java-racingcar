b70b77823010f949273c6eeb09619ea77cc32e78
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-17 12:01:14 +0900

    [refactor] 조금 더 깔끔하게 수정

--- src/main/java/com/jaeyeonling/racingcar/utils/StringUtils.java
+++ src/main/java/com/jaeyeonling/racingcar/utils/StringUtils.java
@@ -3,12 +3,18 @@ package com.jaeyeonling.racingcar.utils;
 public final class StringUtils {
 
     public static final String NEW_LINE = "\n";
+    private static final int MIN_REPEAT_COUNT = 0;
 
     private StringUtils() { }
 
-    public static String repeat(final CharSequence target,
+    public static String repeat(final String target,
                                 final int repeatCount) {
-        return String.valueOf(target)
-                .repeat(repeatCount);
+        if (repeatCount < MIN_REPEAT_COUNT) {
+            throw new IllegalArgumentException(String.format("반복 횟수는 %d 미만이 될 수 없습니다. (입력 값: %d)",
+                    MIN_REPEAT_COUNT,
+                    repeatCount));
+        }
+
+        return target.repeat(repeatCount);
     }
 }
