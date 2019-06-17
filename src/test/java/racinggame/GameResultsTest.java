a3099238606fbe7e17c562a4e9374a92a4bc9f68
SangHyoun Yun <hainco@gmail.com>
2019-06-18 00:31:14 +0900

    test(racingGame): GameResult 에서 `라운드_진행` 추가

--- src/test/java/racinggame/GameResultsTest.java
+++ src/test/java/racinggame/GameResultsTest.java
@@ -9,4 +9,17 @@ class GameResultsTest {
         GameResults gameResults = new GameResults();
         Assertions.assertThat(gameResults.getGameResultList().size()).isEqualTo(0);
     }
+
+    @Test
+    void 라운드_진행() {
+        int carQuantity = 3;
+        GameResults gameResults = new GameResults();
+        Assertions.assertThat(gameResults.getGameResultList().size()).isEqualTo(0);
+
+        CarLists carLists = new CarLists(carQuantity);
+        GameResult gameResult = new GameResult(carLists);
+        gameResults.addRoundResult(gameResult);
+
+        Assertions.assertThat(gameResults.getGameResultList().size()).isEqualTo(1);
+    }
 }
