3140b9811fa135d230a9add3ea2324c8b3814684
문재헌 <jaeheon209@gmail.com>
2019-06-18 23:47:55 +0900

    feat(carRacing) : CarManagement 클래스 구현

---
+++ src/main/java/com/java/jaeheon/step2/CarManagement.java
@@ -0,0 +1,30 @@
+package com.java.jaeheon.step2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarManagement {
+    private final List<Car> cars;
+
+    public CarManagement() {
+        this.cars = new ArrayList<>();
+    }
+
+    public CarManagement register() {
+        Car car = new Car();
+        cars.add(car);
+        return this;
+    }
+
+    public CarManagement registerByNumber(int numberOfCars) {
+        for (int i = 0; i < numberOfCars; i++) {
+            register();
+        }
+        return this;
+    }
+
+    public long numberOfRegisteredCars() {
+        return cars.stream().count();
+    }
+
+}
---
+++ src/test/java/com/java/jaeheon/step2/CarManagementTest.java
@@ -0,0 +1,30 @@
+package com.java.jaeheon.step2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarManagementTest {
+    CarManagement cars;
+
+    @BeforeEach
+    void setup(){
+        this.cars = new CarManagement();
+    }
+
+    @Test
+    void register(){
+        assertThat(cars.register().numberOfRegisteredCars()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 5, 7, 9})
+    void registerByNumber(int numberOfCars) {
+        this.cars = new CarManagement();
+        assertThat(cars.registerByNumber(numberOfCars).numberOfRegisteredCars()).isEqualTo(numberOfCars);
+    }
+
+}
