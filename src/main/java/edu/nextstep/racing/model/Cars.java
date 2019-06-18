c4f256271ed7e589ebe36a4dcf2fe40c72a3d167
권병윤 <sksggg123@gmail.com>
2019-06-18 19:25:40 +0900

    [feat] : 자동차그룹 객체 생성 및 자동차의 이름가져오기 기능

--- src/main/java/edu/nextstep/racing/model/Cars.java
+++ src/main/java/edu/nextstep/racing/model/Cars.java
@@ -1,5 +1,8 @@
 package edu.nextstep.racing.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +14,25 @@ package edu.nextstep.racing.model;
  * create date  : 2019-06-18 19:12
  */
 public class Cars {
+    private List<Car> cars;
+
+    public Cars(List<String> cars) {
+        this.cars = new ArrayList<>();
+        convertStringToCar(cars);
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    private void convertStringToCar(List<String> cars) {
+        for (String carName : cars) {
+            Car car = new Car(carName);
+            this.cars.add(car);
+        }
+    }
 }
