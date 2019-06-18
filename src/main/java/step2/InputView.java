cc86c31f256ef73a1ccf725d04819a20f400d039
feelydh <feelydh@echomarketing.co.kr>
2019-06-18 22:23:45 +0900

    refactor: inputMessage 메소드 입력 단위 분리

--- src/main/java/step2/InputView.java
+++ src/main/java/step2/InputView.java
@@ -4,19 +4,16 @@ import java.util.Scanner;
 
 public class InputView {
 
-    static Integer[] inputMessage(){
-        Integer[] userInputArray = new Integer[2];
-        Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
+    static int inputCarNumber() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        userInputArray[0] = Integer.parseInt(scanner.nextLine());
+        return Integer.parseInt(scanner.nextLine());
+    }
 
+    static int inputExecuteNumber() {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        userInputArray[1] = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("레이싱 결과 출력 입니다.");
-
-        return userInputArray;
+        return Integer.parseInt(scanner.nextLine());
     }
 
 }
--- src/main/java/step2/RacingMain.java
+++ src/main/java/step2/RacingMain.java
@@ -5,12 +5,14 @@ import java.util.List;
 public class RacingMain {
 
     public static void main(String[] arg) {
-        Integer[] userInput = InputView.inputMessage();
-        RacingGame racingGame = new RacingGame(userInput[0]);
-        while (userInput[1] > 0) {
+        int inputCarNumber = InputView.inputCarNumber();
+        int inputExecuteNumber = InputView.inputExecuteNumber();
+        RacingGame racingGame = new RacingGame(inputCarNumber);
+
+        while (inputExecuteNumber > 0) {
             List<Car> carPositions = racingGame.move();
             OutputView.racingResult(carPositions);
-            userInput[1]--;
+            inputExecuteNumber--;
         }
     }
 
