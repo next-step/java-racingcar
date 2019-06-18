0ae408d59ccc0991c93b3aa637520de25480d896
권병윤 <sksggg123@gmail.com>
2019-06-18 22:47:02 +0900

    [feat] : 우승자 출력 기능 추가

--- src/main/java/edu/nextstep/racing/utils/ViewUtils.java
+++ src/main/java/edu/nextstep/racing/utils/ViewUtils.java
@@ -1,5 +1,11 @@
 package edu.nextstep.racing.utils;
 
+import edu.nextstep.racing.model.Car;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -12,8 +18,37 @@ package edu.nextstep.racing.utils;
  */
 public final class ViewUtils {
 
+    private static final String DASH = "- ";
+    private static final String COLON = " : ";
+    private static final String COMMA = ",";
+
     public static void printMessage(String message) {
-        assert (message == null || "".equals(message)): "Message is Null -> [" + message + "]";
+        assert (message == null || "".equals(message)) : "Message is Null -> [" + message + "]";
         System.out.println(message);
+    }
+
+    public static void combinDash(Car car, int combinCount) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(car.getCarName());
+        sb.append(COLON);
+
+        for (int i = 0; i < combinCount; i++) {
+            sb.append(DASH);
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void printWinner(List<Car> winner) {
+        String win = winner.stream()
+                .map(Car::getCarName)
+                .collect(Collectors.toList())
+                .stream()
+                .reduce((s1, s2) -> s1 + COMMA + s2)
+                .get();
+        System.out.println(win);
+    }
+
+    public static void print() {
+        System.out.println();
     }
 }
