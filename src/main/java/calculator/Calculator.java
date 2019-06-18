41a843ba8b1a81d3d02d3b28c92390dffd381f8a
Jason Sohn <jason.sohn28@gmail.com>
2019-06-18 09:28:15 +0900

    Step1 문자열 계산기 구현 (#311)
    * implement calculator
    
    * implement calculator
    
    * add new calculator
    
    * 사칙연산 구현 및 단위테스트

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class Calculator {
+
+    static int add(int operand1, int operand2) {
+        return operand1 + operand2;
+    }
+
+    static int subtract(int operand1, int operand2) {
+        return operand1 - operand2;
+    }
+
+    static int multiply(int operand1, int operand2) {
+        return operand1 * operand2;
+    }
+
+    static int divide(int operand1, int operand2) {
+        return operand1 / operand2;
+    }
+
+}
