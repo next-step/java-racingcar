a337b5d883f12d0961b703ac14643d7b5f96e717
권병윤 <sksggg123@gmail.com>
2019-06-18 19:45:18 +0900

    [test} : 게임횟수 가져오기(성공)

--- src/test/java/edu/nextstep/racing/domain/RoundGameTest.java
+++ src/test/java/edu/nextstep/racing/domain/RoundGameTest.java
@@ -4,7 +4,7 @@ import edu.nextstep.racing.model.Race;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * author       : gwonbyeong-yun <sksggg123>
