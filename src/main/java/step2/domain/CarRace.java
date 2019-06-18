ff8683bdac2f71f91be971e3821e5c4d3389191c
1nteger <rhep0820@gmail.com>
2019-06-19 00:30:30 +0900

    feedback #step2 - 일급 컬렉션 객체(Cars) 생성, CarRace 클래스에 Cars 사용

--- src/main/java/step2/domain/CarRace.java
+++ src/main/java/step2/domain/CarRace.java
@@ -1,30 +1,27 @@
 package step2.domain;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class CarRace {
 
-    private List<Car> cars;
     private int numberOfTrials;
     private RaceExecutor raceExecutor;
 
     public CarRace(int numberOfCars, int numberOfTrials) {
-        this.cars = makeCars(numberOfCars);
+        makeCars(numberOfCars);
         this.numberOfTrials = numberOfTrials;
-        this.raceExecutor = new RaceExecutor(this.cars); //TODO : RaceExcutor는 CarRace가 생성되어야만 필요하니까 이렇게 생성했는데 문제없을까요?
+        this.raceExecutor = new RaceExecutor(this.cars);
     }
 
-    private List<Car> makeCars(int numberOfCars) {
-        List<Car> cars = new ArrayList<>();
+    private void makeCars(int numberOfCars) {
+        Cars cars = new Cars(new ArrayList<>());
         for (int i = 0; i < numberOfCars; i++) {
-            cars.add(new Car());
+            cars.getCars().add(new Car());
         }
-        return cars;
     }
 
     public void raceExecute() {
-        System.out.println("실행 결과");
+        System.out.println("실행 결과"); //TODO: view에 위임
         raceExecutor.executeTrials(numberOfTrials);
     }
 }
