d1565192e18b9f2194bae94bd94dc2f062b5c35b
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-19 00:09:38 +0900

    feat(racing): add feat for car to move when higher than 4

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -5,11 +5,20 @@ public class Car {
 
     private int position;
 
+
     public void move() {
         this.position += 1;
     }
 
+
+    public void move(int randomNumber) {
+        if (randomNumber > 4) move();
+    }
+
+
     public int getPosition() {
         return this.position;
     }
+
+
 }
--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -12,4 +12,11 @@ public class CarTest {
         car.move();
         assertThat(car.getPosition()).isEqualTo(1);
     }
+
+    @Test
+    public void moveWhenHigherThan4() {
+        Car car = new Car();
+        car.move(5);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
 }
