d0a14c75bd214e91e466aa4921afec110e272764
Jaeyeonling <jaeyeonling@gmail.com>
2019-06-19 01:26:58 +0900

    [refactor] 경기 참여자 로직 메서드 이동

--- src/main/java/com/jaeyeonling/racingcar/domain/Participants.java
+++ src/main/java/com/jaeyeonling/racingcar/domain/Participants.java
@@ -1,5 +1,6 @@
 package com.jaeyeonling.racingcar.domain;
 
+import com.jaeyeonling.racingcar.utils.RandomUtils;
 import com.jaeyeonling.racingcar.utils.StringUtils;
 import com.jaeyeonling.racingcar.view.Visualizable;
 
@@ -17,14 +18,12 @@ class Participants implements Visualizable {
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    public int maxPosition() {
-        return participants.stream()
-                .mapToInt(Car::getPosition)
-                .max()
-                .orElse(Car.DEFAULT_POSITION);
+    @Override
+    public String visualize() {
+        return getVisualGameStatus();
     }
 
-    public List<Car> getLeadingCars() {
+    List<Car> getLeadingCars() {
         final int leaderPosition = maxPosition();
 
         return participants.stream()
@@ -32,22 +31,29 @@ class Participants implements Visualizable {
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    @Override
-    public String visualize() {
-        return getVisualGameStatus();
-    }
-
-    List<Car> getCars() {
-        return participants;
+    void move() {
+        participants.forEach(this::move);
     }
 
     private String getVisualGameStatus() {
         final StringBuilder visualBuilder = new StringBuilder();
-        for (final Car car : getCars()) {
+        for (final Car car : participants) {
             visualBuilder.append(car.visualize())
                     .append(StringUtils.NEW_LINE);
         }
 
         return visualBuilder.toString();
+    }
+
+    private int maxPosition() {
+        return participants.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(Car.DEFAULT_POSITION);
+    }
+
+    private void move(final Car car) {
+        final int condition = RandomUtils.getIntWithBound(RacingGame.MOVING_RANDOM_BOUND);
+        car.moveForward(condition);
     }
 }
--- src/main/java/com/jaeyeonling/racingcar/domain/RacingGame.java
+++ src/main/java/com/jaeyeonling/racingcar/domain/RacingGame.java
@@ -1,19 +1,15 @@
 package com.jaeyeonling.racingcar.domain;
 
-import com.jaeyeonling.racingcar.utils.RandomUtils;
-
-import java.util.List;
-
 public class RacingGame {
 
-    private static final int MOVING_RANDOM_BOUND = 9;
+    static final int MOVING_RANDOM_BOUND = 9;
 
-    private final RacingGameOption option;
-
-    private int movedCount = Car.DEFAULT_POSITION;
+    private final Participants participants;
+    private int movingCount;
 
     public RacingGame(final RacingGameOption option) {
-        this.option = option;
+        this.participants = option.getParticipants();
+        this.movingCount = option.getMovingCount();
     }
 
     public void move() {
@@ -21,12 +17,12 @@ public class RacingGame {
             throw new IllegalStateException("이미 결과가 나온 게임입니다.");
         }
 
-        moveAllCar();
-        movedCount++;
+        participants.move();
+        movingCount--;
     }
 
     public boolean isComplete() {
-        return movedCount == option.getMovingCount();
+        return Car.DEFAULT_POSITION == movingCount;
     }
 
     public RacingGameResult getResult() {
@@ -34,22 +30,10 @@ public class RacingGame {
             throw new IllegalStateException("결과가 나오지 않은 게임입니다.");
         }
 
-        return new RacingGameResult(getParticipants());
+        return new RacingGameResult(participants);
     }
 
     public Participants getParticipants() {
-        return option.getParticipants();
-    }
-
-    private List<Car> getCars() {
-        return getParticipants().getCars();
-    }
-
-    private void moveAllCar() {
-        getCars().forEach(this::moveCar);
-    }
-
-    private void moveCar(final Car car) {
-        car.moveForward(RandomUtils.getIntWithBound(MOVING_RANDOM_BOUND));
+        return participants;
     }
 }
