4a540d4df3438400447b1b8ad9908e215d26c68e
권병윤 <sksggg123@gmail.com>
2019-06-18 19:41:58 +0900

    [test] : 게임횟수 가져오기(실패)

---
+++ src/test/java/edu/nextstep/racing/domain/RoundGameTest.java
@@ -0,0 +1,32 @@
+package edu.nextstep.racing.domain;
+
+import edu.nextstep.racing.model.Race;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-racingcar
+ * create date  : 2019-06-18 19:37
+ */
+class RoundGameTest {
+
+    private RoundGame game;
+
+    @BeforeEach
+    void 사전설정() {
+        game = new RoundGame(new Race(10));
+    }
+
+    @Test
+    void 게임경기_횟수_가져오기() {
+        assertThat(game.getGameTime()).isEqualTo(10);
+    }
+}
