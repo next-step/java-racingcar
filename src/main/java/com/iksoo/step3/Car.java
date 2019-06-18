f5144bdef380bfafebdedea96b8a509e085fbab8
IKSOO SHIN <iksoo@IKSOOui-MacBook-Pro.local>
2019-06-18 23:35:59 +0900

    fix(Car) : maxDistance 변수명 변경 -> currentPosition

--- src/main/java/com/iksoo/step3/Car.java
+++ src/main/java/com/iksoo/step3/Car.java
@@ -13,12 +13,12 @@ package com.iksoo.step3;
 public class Car {
     private String carName;
     public String carPosition;
-    public int maxDistance;
+    public int currentDistance;
 
     Car(String Name) {
         this.carName = Name;
         this.carPosition = "";
-        this.maxDistance = 0;
+        this.currentDistance = 0;
     }
 
     public String getCarName() {
