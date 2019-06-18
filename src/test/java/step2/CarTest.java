3f5431f4ba50a53b7b8936161af61b708e7858a8
1nteger <rhep0820@gmail.com>
2019-06-18 23:44:05 +0900

    feedback #step2 - CarTest의 @BeforEach 대신 Car 객체 인스턴스화

--- src/test/java/step2/CarTest.java
+++ src/test/java/step2/CarTest.java
@@ -1,6 +1,5 @@
 package step2;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,12 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
-    private Car car;
-
-    @BeforeEach
-    void init() {
-        car = new Car();
-    }
+    private Car car = new Car();
 
     @Test
     void 자동차가_생성된다() {
@@ -27,10 +21,16 @@ public class CarTest {
         assertThat(car.getPosition()).isEqualTo(0);
     }
 
+    @Test
+    void 자동차는_랜덤숫자가_4이상인_경우만_이동한다() {
+
+        // RaceExecutor 리팩토링 후 작성 예정
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5})
     void 자동차가_주어진_거리만큼_이동한다(int numberOfMoves) {
-        for(int i=0; i<numberOfMoves; i++) {
+        for (int i = 0; i < numberOfMoves; i++) {
             car.move();
         }
         assertThat(car.getPosition()).isEqualTo(numberOfMoves);
