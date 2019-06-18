6c499efbe24f6759d60c154ea9d5ed09fa22bd73
IKSOO SHIN <iksoo@IKSOOui-MacBook-Pro.local>
2019-06-18 22:02:24 +0900

    fix(Car) : CarPosition 변수 자료형 변경(String -> int)

--- src/main/java/com/iksoo/step3/Car.java
+++ src/main/java/com/iksoo/step3/Car.java
@@ -12,23 +12,23 @@ package com.iksoo.step3;
 
 public class Car {
     private String carName;
-    private String carPosition;
+    public int carPosition;
     private static final int GOSTOP_CRITERION = 4;
 
     Car(String Name) {
         this.carName = Name;
-        this.carPosition = "";
+        this.carPosition = 0;
     }
 
-    public boolean judgeGoOrStop(int number) {
-        return number >= GOSTOP_CRITERION ? true : false;
+    public int judgeGoOrStop(int number) {
+        return number >= GOSTOP_CRITERION ? 1 : 0;
     }
 
     public String getCarName() {
         return this.carName;
     }
 
-    public String getCarPosition() {
+    public int getCarPosition() {
         return this.carPosition;
     }
 }
