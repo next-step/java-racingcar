a25aa5a46559b8dfd4ec1cf912e34c2e59715f53
권병윤 <sksggg123@gmail.com>
2019-06-18 20:52:25 +0900

    [feat] : Validaion 유틸 클래스 추가

---
+++ src/main/java/edu/nextstep/racing/utils/ValidationUtils.java
@@ -0,0 +1,39 @@
+package edu.nextstep.racing.utils;
+
+import java.util.Objects;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-racingcar
+ * create date  : 2019-06-18 20:38
+ */
+public final class ValidationUtils {
+
+    public static void checkObject(Object obj) {
+        checkEmpty(obj);
+        checkNull(obj);
+    }
+
+    public static void checkNull(Object obj) {
+        if(Objects.isNull(obj)) {
+            throw new IllegalArgumentException("Object is NULL" + obj);
+        }
+    }
+
+    public static void checkEmpty(Object obj) {
+        if("".equals(obj)) {
+            throw new IllegalArgumentException("Object is Empty" + obj);
+        }
+    }
+
+    public static void checkMinValue(int minValue, int originValue) {
+        if(minValue > originValue) {
+            throw new IllegalArgumentException(minValue + "보다 " + originValue + " 가 더 작습니다.");
+        }
+    }
+}
---
+++ src/test/java/edu/nextstep/racing/utils/ValidationUtilsTest.java
@@ -0,0 +1,34 @@
+package edu.nextstep.racing.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-racingcar
+ * create date  : 2019-06-18 20:39
+ */
+class ValidationUtilsTest {
+
+    @Test
+    void checkObject() {
+    }
+
+    @Test
+    void checkNull() {
+    }
+
+    @Test
+    void checkEmpty() {
+    }
+
+    @Test
+    void checkMinValue() {
+    }
+}
