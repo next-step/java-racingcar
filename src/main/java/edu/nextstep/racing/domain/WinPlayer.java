1c4b247d55a190e47db6de28c6b2f7f20d371f2a
권병윤 <sksggg123@gmail.com>
2019-06-18 20:35:23 +0900

    [feat] : 우승자 선정하는 기능 추가

--- src/main/java/edu/nextstep/racing/domain/WinPlayer.java
+++ src/main/java/edu/nextstep/racing/domain/WinPlayer.java
@@ -1,5 +1,12 @@
 package edu.nextstep.racing.domain;
 
+import edu.nextstep.racing.model.Car;
+import edu.nextstep.racing.model.Winner;
+import edu.nextstep.racing.utils.NumberUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +18,31 @@ package edu.nextstep.racing.domain;
  * create date  : 2019-06-18 20:22
  */
 public class WinPlayer {
+    private Winner winner;
+
+    public WinPlayer(Winner Winner) {
+        this.winner = Winner;
+    }
+
+    public Winner getWinners() {
+        return this.winner;
+    }
+
+    public int getWinScore() {
+        return this.winner.getWinScore();
+    }
+
+    public void doWinnerCheck() {
+        calculateWinScore(this.winner.getWinners());
+
+        this.winner.setWinners(this.winner.getWinners().stream()
+                .filter(car -> car.currentPosition() >= this.winner.getWinScore())
+                .collect(Collectors.toList()));
+    }
+
+    private void calculateWinScore(List<Car> cars) {
+        for (Car car : cars) {
+            this.winner.setWinScore(NumberUtils.getMax(this.winner.getWinScore(), car.currentPosition()));
+        }
+    }
 }
