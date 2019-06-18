25e7c12aee4fe7cf7d13b9406f3f73243538160a
권병윤 <sksggg123@gmail.com>
2019-06-18 20:55:56 +0900

    [refactor] : 사용자 UI(자동차 이름 입력) 부분 수정

--- src/main/java/edu/nextstep/racing/view/InputView.java
+++ src/main/java/edu/nextstep/racing/view/InputView.java
@@ -1,5 +1,10 @@
 package edu.nextstep.racing.view;
 
+import edu.nextstep.racing.utils.ValidationUtils;
+import edu.nextstep.racing.utils.ViewUtils;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -16,14 +21,27 @@ public class InputView {
 
     private Scanner scanner = new Scanner(System.in);
 
-    public int setCarNumber() {
-        System.out.println("자동차 대수는 몇대 인가요?");
-        return scanner.nextInt();
+    private static final String SPLIT_CAR_SEPERATOR = ",";
+
+    public List<String> getNameOfCars() {
+        ViewUtils.printMessage("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String cars = scanner.nextLine();
+
+        ValidationUtils.checkNull(cars);
+
+        return Arrays.asList(splitCars(cars));
     }
 
-    public int setMoveNumber() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        return scanner.nextInt();
+    public int getNumberOfTime() {
+        ViewUtils.printMessage("시도할 회수는 몇회인가요.");
+        int time = Integer.parseInt(scanner.nextLine());
 
+        ValidationUtils.checkNull(time);
+
+        return time;
+    }
+
+    public String[] splitCars(String cars) {
+        return cars.split(SPLIT_CAR_SEPERATOR);
     }
 }
