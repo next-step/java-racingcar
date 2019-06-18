8a88e225fc5854a40a80a75e502d4fc10e15269d
권병윤 <sksggg123@gmail.com>
2019-06-18 22:49:15 +0900

    [refactor] : main class 기능별 호출로직 변경

--- src/main/java/edu/nextstep/racing/CarRacingApplication.java
+++ src/main/java/edu/nextstep/racing/CarRacingApplication.java
@@ -1,7 +1,16 @@
 package edu.nextstep.racing;
 
 import edu.nextstep.racing.domain.CarRacing;
+import edu.nextstep.racing.domain.RoundGame;
+import edu.nextstep.racing.domain.WinPlayer;
+import edu.nextstep.racing.model.Cars;
+import edu.nextstep.racing.model.Race;
+import edu.nextstep.racing.model.Winner;
 import edu.nextstep.racing.view.InputView;
+import edu.nextstep.racing.view.ResultView;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * author       : gwonbyeong-yun <sksggg123>
@@ -15,21 +24,42 @@ import edu.nextstep.racing.view.InputView;
  */
 public class CarRacingApplication {
 
-    private InputView input;
+    private InputView ui;
+    private ResultView output;
+    private CarRacing carRacing;
+    private RoundGame roundGame;
+    private WinPlayer winPlayer;
+
+    private static final int INIT_SCORE = 0;
 
     public CarRacingApplication() {
-        this.input = new InputView();
+        this.ui = new InputView();
+        this.output = new ResultView();
     }
 
     public static void main(String[] args) {
         CarRacingApplication app = new CarRacingApplication();
-        int carCount = app.input.setCarNumber();
-        int moveCount = app.input.setMoveNumber();
-        app.start(carCount, moveCount);
+        app.start();
     }
 
-    public void start(int carCount, int moveCount) {
-        CarRacing race = new CarRacing(carCount, moveCount);
-        race.move();
+    public void start() {
+        List<String> carList = ui.getNameOfCars()
+                .stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        Cars cars = new Cars(carList);
+        Race race = new Race(ui.getNumberOfTime());
+
+        this.carRacing = new CarRacing(cars);
+        this.roundGame = new RoundGame(race);
+
+        for (int i = 0; i < this.roundGame.getGameTime(); i++) {
+            this.carRacing.starRoundGame();
+        }
+        this.winPlayer = new WinPlayer(new Winner(this.carRacing.finishGame().getCars(), INIT_SCORE));
+        this.winPlayer.doWinnerCheck();
+
+        output.printWinnerPlayer(this.winPlayer.getWinners().getWinners());
     }
 }
