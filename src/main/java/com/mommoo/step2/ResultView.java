d0785487c659f88880a55188910f141b0f70babb
최경수/Dooray개발실/NE <nova1122@naver.com>
2019-06-19 22:46:45 +0900

    feat(ResultView) :  - 자동차 이름과 위치 라인을 그리는 기능 추가.  - CarWinnerComputer 객체를 이용한 결과 출력 기능 추가.

--- src/main/java/com/mommoo/step2/ResultView.java
+++ src/main/java/com/mommoo/step2/ResultView.java
@@ -1,14 +1,41 @@
 package com.mommoo.step2;
 
+import java.util.List;
+
 public class ResultView {
     private static final String LINE = "-";
     private final StringBuilder BUILDER = new StringBuilder();
 
-    public void printCarMoveLine(int moveCount) {
+    public void printCarMoveLine(Car car) {
         BUILDER.setLength(0);
+
+        BUILDER.append(car.getName())
+               .append(" : ");
+
+        int moveCount = car.getPosition();
         while (moveCount-- > 0) {
             BUILDER.append(LINE);
         }
+        System.out.println(BUILDER);
+    }
+
+    public void printFinalWinner(List<Car> cars) {
+        CarWinnerComputer carWinnerComputer = new CarWinnerComputer(cars);
+
+        if (carWinnerComputer.isWinnerNotExist()) {
+            System.out.println("우승자가 없습니다.");
+            return;
+        }
+
+        List<String> winnerCarNameList = carWinnerComputer.getWinnerCarNameList();
+
+        String prefixCarNames = String.join(",", winnerCarNameList);
+
+        BUILDER.setLength(0);
+
+        BUILDER.append(prefixCarNames)
+               .append("가 최종 우승했습니다.");
+
         System.out.println(BUILDER);
     }
 }
