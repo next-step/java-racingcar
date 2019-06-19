d7d58e78c23d7c5699211c58deede21050799757
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-19 21:04:37 +0900

    refactor(racing): refactor CarFactoryTest

--- src/main/java/racing/model/CarFactory.java
+++ src/main/java/racing/model/CarFactory.java
@@ -7,7 +7,7 @@ public class CarFactory {
 
     public static List<Car> createByCount(int count) {
         List<Car> cars = new ArrayList<>();
-        for (int o = 0; count != 0; count--) {
+        for (; count != 0; count--) {
             cars.add(new Car());
         }
         return cars;
