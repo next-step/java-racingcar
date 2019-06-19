9811454d368bdfa8e1a99b30c21c8189d220d40e
pch8388 <pch838811@gmail.com>
2019-06-20 02:04:47 +0900

    refactor : 이름과 같이 출력할 문자열 생성하도록 변경

--- src/main/java/racinggame/util/StringUtil.java
+++ src/main/java/racinggame/util/StringUtil.java
@@ -4,8 +4,8 @@ public class StringUtil {
   private static final String CAR_RACING_PRINT = "-";
   private static final int START_INDEX = 0;
 
-  public static String getCarRacingPrint(int repeat) {
-    StringBuilder stringBuilder = new StringBuilder();
+  public static String getCarRacingPrint(String name, int repeat) {
+    StringBuilder stringBuilder = new StringBuilder(name + " : ");
     for (int i = START_INDEX; i < repeat; i++) {
       stringBuilder.append(CAR_RACING_PRINT);
     }
