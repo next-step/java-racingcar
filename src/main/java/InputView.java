282e4114d95b6e977861f58a95fdd75b7c146240
Bentley <p2c2kbf@gmail.com>
2019-06-19 16:13:18 +0900

    fix: 자동차 입력 함수 변경(step3 일부 적용)

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,12 +1,23 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputView {
-    void userInput(RacingGame racingGame){
+    void userInput(RacingGame racingGame, ArrayList<CarInfo> carInfo){
         Scanner scanner = new Scanner(System.in);
+        String inputCarList;
+        String[] carList;
+        CarInfo inputCarInfo;
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        racingGame.setNumberOfCar(scanner.nextInt());
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        inputCarList = scanner.nextLine();
+        carList = inputCarList.split(",");
+        racingGame.setNumberOfCar(carList.length);
 
+        for(int i=0; i<carList.length; i++){
+            inputCarInfo = new CarInfo();
+            inputCarInfo.setCarName(carList[i]);
+            carInfo.add(inputCarInfo);
+        }
         System.out.println("시도할 회수는 몇 회 인가요?");
         racingGame.setTime(scanner.nextInt());
     }
