7e9bff691c62109e25e24b556f2204501daab083
최경수/Dooray개발실/NE <nova1122@naver.com>
2019-06-19 23:16:41 +0900

    feat(Main) : Car 자료형을 이용한 전체 기능 실행 코드 추가.

--- src/main/java/com/mommoo/step2/Main.java
+++ src/main/java/com/mommoo/step2/Main.java
@@ -1,21 +1,26 @@
 package com.mommoo.step2;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        int carCount = inputView.getCountOfCar();
+        String[] carNames = inputView.getCarNames();
         int times = inputView.getCountOfTrying();
 
-        RacingGame racingGame = new RacingGame(carCount);
+        RacingGame racingGame = new RacingGame(carNames);
 
         ResultView resultView = new ResultView();
 
         while(times-- > 0) {
-            int[] result = racingGame.move();
-            for (int moveCount : result) {
-                resultView.printCarMoveLine(moveCount);
+            List<Car> carList = racingGame.move();
+            for (Car car : carList) {
+                resultView.printCarMoveLine(car);
             }
             System.out.println();
         }
+
+        List<Car> finalCarList = racingGame.currentCarList();
+        resultView.printFinalWinner(finalCarList);
     }
 }
