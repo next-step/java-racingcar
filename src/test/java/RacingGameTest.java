c56e17fb5f69a9460cd5e232e9516b50a7c16a7e
Bentley <p2c2kbf@gmail.com>
2019-06-19 16:06:38 +0900

    fix:Junit 테스트 수정

--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -1,15 +1,40 @@
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RacingGameTest {
     // 자동차 경주의 최초 설정 세팅 함수에 대한 Junit 테스트 구현
-    @ParameterizedTest
-    @ValueSource(ints = {2, 0, 5, 1})
-    public void gameInitialSet(int number) {
 
-        if (number <= 1) {
-            throw new IllegalArgumentException("입력값이 잘못되었습니다. 레이싱 게임을 위해서 최소한 1보다 큰 숫자로 다시 입력해주세요.");
-        }
+    @Test
+    public void gameInitialSet(){
+        int numberOfCar = 1;
+        ArrayList<CarInfo> carInfo = null;
+
+        assertThatNullPointerException().isThrownBy(()  -> {
+            if(carInfo == null)
+                throw new NullPointerException("함수의 객체가 null 입니다. 확인하시고 다시 실행해주시길 바랍니다.");
+        });
+
+        assertTrue(numberOfCar <= 1);
     }
+
+    // 랜덤값 생성 및 자동차 경주 결과를 호출하는 함수에 대한 Junit 테스트 구현
+    @Test
+    public void makeRandomNumberAndGameResult() {
+        RacingGame racingGame = new RacingGame();
+        ArrayList<CarInfo> carInfo = null;
+        Random generatorNumber = new Random();
+        ResultView resultView = new ResultView(); // 자동차 경주 결과의 출력을 위한 객체 생성
+
+        assertThatNullPointerException().isThrownBy(() -> {
+            if (racingGame == null || carInfo == null || generatorNumber == null || resultView == null)
+                throw new NullPointerException("함수의 객체 중에 null값이 있습니다. 확인하시고 다시 실행해주시길 바랍니다.");
+        });
+    }
+
 }
 
