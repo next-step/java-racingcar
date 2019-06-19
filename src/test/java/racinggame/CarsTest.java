2730d8b44538015c84fa5589517be1e74c3a4ad1
pch8388 <pch838811@gmail.com>
2019-06-20 01:57:53 +0900

    test : 우승자를 반환 test 추가

--- src/test/java/racinggame/CarsTest.java
+++ src/test/java/racinggame/CarsTest.java
@@ -1,5 +1,6 @@
 package racinggame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racinggame.model.Cars;
@@ -8,10 +9,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
 
+  private Cars cars;
+
+  @BeforeEach
+  void setUp() {
+    cars = new Cars("test1, test2");
+  }
+
   @DisplayName("자동차 생성 테스트")
   @Test
   void createCars() {
-    final Cars cars = new Cars("test1, test2");
-    assertThat(cars.getCarsStatus()).hasSize(2);
+    assertThat(cars.getCars()).hasSize(2);
+  }
+
+  @DisplayName("우승자 테스트")
+  @Test
+  void getWinner() {
+    assertThat(cars.getWinner()).isNotBlank().isNotEmpty();
   }
 }
