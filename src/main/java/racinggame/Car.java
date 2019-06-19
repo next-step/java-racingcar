ba903b36201eea4ad229a7e8cc904b7cea109119
SangHyoun Yun <hainco@gmail.com>
2019-06-19 21:57:40 +0900

    feat(racingGame): Car 클래스에 carName 추가, 테스트코드 추가.

--- src/main/java/racinggame/Car.java
+++ src/main/java/racinggame/Car.java
@@ -3,6 +3,11 @@ package racinggame;
 public class Car {
     public static final int MOVE_DISTANCE_PER_ONCE = 1;
     private int position = 1;
+    private String carName;
+
+    public Car(String carName) {
+        this.carName = carName;
+    }
 
     public void move() {
         this.position += MOVE_DISTANCE_PER_ONCE;
@@ -10,5 +15,9 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    public String getCarName() {
+        return carName;
     }
 }
--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -6,16 +6,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
     private int DEFAULT_POSITION = 1;
+    private String carName = "실험자동차";
 
     @Test
     void 생성후_기본위치() {
-        Car car = new Car();
+        Car car = new Car(carName);
         assertThat(car.getPosition()).isEqualTo(DEFAULT_POSITION);
     }
 
     @Test
+    void 자동차는_주어진_이름으로_생성됨() {
+        Car car = new Car(carName);
+        assertThat(car.getCarName()).isEqualTo(carName);
+    }
+
+    @Test
     void move_호출시_위치_1만큼_이동() {
-        Car car = new Car();
+        Car car = new Car(carName);
         car.move();
         assertThat(car.getPosition()).isEqualTo(DEFAULT_POSITION + 1);
     }
