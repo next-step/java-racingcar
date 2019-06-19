8ad580e3e8fdeb03e3a52384bf1a618c4e3d3bbc
feelydh <feelydh@echomarketing.co.kr>
2019-06-19 19:12:19 +0900

    stype: RacingPositionTest -> CarTest로 변경

--- src/test/java/step2/CarTest.java
+++ src/test/java/step2/CarTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RacingPositionTest {
+public class CarTest {
 
     private Car car;
     private static final int INIT_RANDOMNUMBER = 1;
