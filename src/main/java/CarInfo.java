0cd753e9ac18add46eee9ebf67ac0b875b310964
Bentley <p2c2kbf@gmail.com>
2019-06-19 16:21:28 +0900

    feat: CarInfo 객체 추가 및 일부수정

--- src/main/java/CarInfo.java
+++ src/main/java/CarInfo.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class CarInfo {
+    private String carName;
+    private int carPosition;
+    private String carStatus;
+
+    public int getCarPosition(){return carPosition;}
+    public String getCarStatus(){return carStatus;}
+    public String getCarName() {return  carName;}
+
+    public void setCarName(String carName ){
+        this.carName = carName;
+    }
+    public void setCarPositions(int carPosition) {this.carPosition = carPosition; }
+    public void setCarStatus(String carStatus) {this.carStatus = carStatus;}
 }
