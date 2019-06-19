69a6bee3f523b652ea02eeddf4adb4354e9cc3c9
pch8388 <pch838811@gmail.com>
2019-06-19 22:45:14 +0900

    test : 이름을 받아서 객체를 생성하도록 Cars class 변경 - 객체생성 테스트

--- src/test/java/racinggame/CarsTest.java
+++ src/test/java/racinggame/CarsTest.java
@@ -1,8 +1,7 @@
 package racinggame;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 import racinggame.model.Cars;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,10 +9,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarsTest {
 
   @DisplayName("자동차 생성 테스트")
-  @ParameterizedTest
-  @ValueSource(ints = {3, 4, 7})
-  void createCars(int carCount) {
-    final Cars cars = new Cars(carCount);
-    assertThat(cars.getCarsStatus()).hasSize(carCount);
+  @Test
+  void createCars() {
+    final Cars cars = new Cars("test1, test2");
+    assertThat(cars.getCarsStatus()).hasSize(2);
   }
 }
