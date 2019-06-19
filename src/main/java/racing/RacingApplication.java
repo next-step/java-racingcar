941d1fc40fff105ba224984d22394f08f14c1870
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-19 21:16:17 +0900

    refactor(racing): refactor RacingApplication and InputView

--- src/main/java/racing/RacingApplication.java
+++ src/main/java/racing/RacingApplication.java
@@ -1,17 +1,12 @@
 package racing;
 
 import racing.io.InputView;
-import racing.model.Car;
 import racing.model.CarFactory;
 import racing.model.RacingGame;
-
-import java.util.List;
 
 public class RacingApplication {
 
     public static void main(String[] args) {
-        List<Car> cars = CarFactory.createByCount(InputView.carCount());
-        int raceCount = InputView.raceCount();
-        RacingGame.of(cars, raceCount).run();
+        RacingGame.of(CarFactory.createByCount(InputView.getCarCount()), InputView.getRaceCount()).run();
     }
 }
--- src/main/java/racing/io/InputView.java
+++ src/main/java/racing/io/InputView.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public static int carCount() {
+    public static int getCarCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         return new Scanner(System.in).nextInt();
     }
 
-    public static int raceCount() {
+    public static int getRaceCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         return new Scanner(System.in).nextInt();
     }
