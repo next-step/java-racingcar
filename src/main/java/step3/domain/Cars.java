cd7f3c7054f35a6f3770fb85fe17dacba3417f3a
1nteger <rhep0820@gmail.com>
2019-06-20 00:28:08 +0900

    feedback #step3 - 우승자를 찾는 메서드들을 Winners -> Cars 로 이전

--- src/main/java/step3/domain/Cars.java
+++ src/main/java/step3/domain/Cars.java
@@ -1,6 +1,7 @@
 package step3.domain;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +18,17 @@ public class Cars {
                 .stream()
                 .map(Car::new)
                 .collect(Collectors.toList()));
+    }
+
+    public int findWinnerPosition() {
+        return cars.stream()
+                .map(Car::getPosition)
+                .max(Comparator.comparing(Integer::valueOf))
+                .get();
+    }
+
+    public boolean isWinnerPosition(int winnerPosition, Car car) {
+        return car.getPosition() == winnerPosition; //TODO: Car 객체에 메세지를 던져서 결과 반환하도록 구현하기
     }
 
     public List<Car> getCars() {
--- src/main/java/step3/domain/Winners.java
+++ src/main/java/step3/domain/Winners.java
@@ -1,7 +1,6 @@
 package step3.domain;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,23 +13,12 @@ public class Winners {
     }
 
     public static Winners pickWinners(Cars cars) {
-        int winnerPosition = findWinnerPosition(cars);
+        int winnerPosition = cars.findWinnerPosition();
         List<Car> winners = cars.getCars().stream()
-                .filter(car -> isWinnerPosition(winnerPosition, car))
+                .filter(car -> cars.isWinnerPosition(winnerPosition, car))
                 .collect(Collectors.toList());
 
         return new Winners(winners);
-    }
-
-    private static int findWinnerPosition(Cars cars) { //TODO: Q. 이렇게 static을 난사(?)하고있는데 괜찮을까요?
-        return cars.getCars().stream()
-                .map(Car::getPosition)
-                .max(Comparator.comparing(Integer::valueOf))
-                .get();
-    }
-
-    private static boolean isWinnerPosition(int winnerPosition, Car car) {
-        return car.getPosition() == winnerPosition;
     }
 
     public List<Car> getWinners() {
