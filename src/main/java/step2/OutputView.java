23cf7f69b93428071bf676a66e92663291f76c3c
feelydh <feelydh@echomarketing.co.kr>
2019-06-19 19:06:50 +0900

    refactor: 레이싱게임에 자동차 이름 표시

--- src/main/java/step2/OutputView.java
+++ src/main/java/step2/OutputView.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 public class OutputView {
 
-    static void racingResult(List<Car> carList) {
-        carList.forEach((car) -> carResult(car.lastCarPosition()));
+    static void racingResult(List<Car> cars) {
+        cars.forEach((car) -> carResult(car.lastCarPosition(), car.name()));
         System.out.println();
     }
 
-    static void carResult(int carMoveStatusNumber) {
+    static void carResult(int carMoveStatusNumber, String carNmae) {
         StringBuffer carMoveBuffer = new StringBuffer();
+        carMoveBuffer.append(carNmae + " : ");
         while (carMoveStatusNumber > 0) {
             carMoveBuffer.append("-");
             carMoveStatusNumber--;
