511ba7e2cf400381738f074b2acb5b6707cfc5e8
Bentley <p2c2kbf@gmail.com>
2019-06-19 16:22:31 +0900

    fix: 피드백 사항 적용하여 수정

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RacingGame {
     private int timeOfGame; // 자동차 경주 횟수
     private int numberOfCar;    // 자동차 대수
-    private int[] carPositions; // 자동차 위치 정보
-    private String[] carStatus; // 자동차 경주 결과 정보
+    public static int MAX_RANDOM_NUMBER = 10;
 
     public int getTime() {
         return timeOfGame;
@@ -23,47 +23,41 @@ public class RacingGame {
     }
 
     // 자동차 경주의 최초 설정 세팅 함수
-    public void gameInitialSet(int numberOfCar) {
-        carPositions = new int[numberOfCar];
-        carStatus = new String[numberOfCar];
+    public void gameInitialSet(int numberOfCar, ArrayList<CarInfo> carInfo) {
+        if(carInfo == null)
+            throw new NullPointerException("함수의 인자값이 Null값이니다. 확인하시고 다시 실행해주시길 바랍니다.");
 
-        if (numberOfCar <= 1) {
-            throw new IllegalArgumentException("입력값이 잘못되었습니다. 레이싱 게임을 위해서 최소한 1보다 큰 숫자로 다시 입력해주세요.");
-        }
+        if(numberOfCar <= 1)
+            throw  new IllegalArgumentException("입력하신 자동차의 숫자가 0이거나 1입니다. 다시 입력해주세요");
 
-        for (int i = 0; i < numberOfCar; i++) {
-            carPositions[i] = 0;
-            carStatus[i] = "-";
-        }
-    }
+        for (int i = 0; i < carInfo.size(); i++)
+            carInfo.get(i).setCarPositions(0);
 
-    public String[] getCarStatus() {
-        return carStatus;
     }
 
     // 생성된 랜덤값이 4이상일때 자동차 위치를 전진하는 함수
-    public void move(int position) {
-        carPositions[position] += 1;
-        carStatus[position] += "-";
-
+    public void move(int position, ArrayList<CarInfo> carInfo) {
+        int prePosition = carInfo.get(position).getCarPosition();
+        carInfo.get(position).setCarPositions(prePosition+1);
     }
 
     // 랜덤값 생성 및 자동차 경주 결과를 호출하는 함수
-    public void makeRandomNumberAndGameResult(RacingGame racingGame) {
+    public void makeRandomNumberAndGameResult(RacingGame racingGame, ArrayList<CarInfo> carInfo) {
         Random generatorNumber = new Random();
         ResultView resultView = new ResultView(); // 자동차 경주 결과의 출력을 위한 객체 생성
-        int result;
+        int randomResult;
+
+        if (racingGame == null || carInfo == null || generatorNumber == null || resultView == null)
+            throw new NullPointerException("함수의 객체 중에 null값이 있습니다. 확인하시고 다시 실행해주시길 바랍니다.");
 
         System.out.println("실행 결과");
-
         for (int i = 0; i < racingGame.getTime(); ++i) {
-            for (int j = 0; j < racingGame.getNumberOfCar(); ++j) {
-
-                result = generatorNumber.nextInt(10); // 랜덤값 생성
-                if (result >= 4) {
-                    move(j); // 자동차 위치 전진 함수 호출
+            for (int j = 0; j < carInfo.size(); ++j) {
+                randomResult = generatorNumber.nextInt(MAX_RANDOM_NUMBER); // 랜덤값 생성
+                if (i == 0 || randomResult >= 4) {  // 무조건 최초 1회 && 랜덤값이 4이상 이면 전진 실행되도록
+                    move(j, carInfo); // 자동차 위치 전진 함수 호출
                 }
-                resultView.disPlayRacingResult(j, racingGame.getCarStatus()); //자동차 경주 결과 출력
+                resultView.disPlayRacingResult(carInfo.get(j)); //자동차 경주 결과 출력
             }
             System.out.println("\n");
         }
@@ -71,10 +65,12 @@ public class RacingGame {
 
     public static void main(String[] args) {
         RacingGame racingGame = new RacingGame();
+        ArrayList<CarInfo> carInfo = new ArrayList<>();
         InputView inputView = new InputView(); // 사용자 입력을 위한 객체 생성
 
-        inputView.userInput(racingGame);  //사용자 입력 실행
-        racingGame.gameInitialSet(racingGame.getNumberOfCar()); // 유저 입력값을 기반으로 최초 설정값 세팅
-        racingGame.makeRandomNumberAndGameResult(racingGame); //Random 값 생성 및 자동차 상태 출력
+
+        inputView.userInput(racingGame, carInfo);  //사용자 입력 실행
+        racingGame.gameInitialSet(racingGame.getNumberOfCar(), carInfo); // 유저 입력값을 기반으로 최초 설정값 세팅
+        racingGame.makeRandomNumberAndGameResult(racingGame, carInfo); //Random 값 생성 및 자동차 상태 출력
     }
 }
