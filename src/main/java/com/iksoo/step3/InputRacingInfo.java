cb8aa0778238add33af92ee9a253f07b04299de3
IKSOO SHIN <iksoo@IKSOOui-MacBook-Pro.local>
2019-06-19 22:39:07 +0900

    fix(input) : getter 메소드를 제거하고 직접리턴하도록 변경

--- src/main/java/com/iksoo/step3/InputRacingInfo.java
+++ src/main/java/com/iksoo/step3/InputRacingInfo.java
@@ -1,7 +1,7 @@
 /*
  * ClassName : InputRacingInfo.java
  *
- * Version : 1.0
+ * Version : 1.1
  *
  * Create Date : 2019.06.17
  *
@@ -14,45 +14,39 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputRacingInfo {
-    private String carTotalNames;
-    private int tryOfNumbers;
-    private Scanner in;
-
-    InputRacingInfo() {
-        in = new Scanner(System.in);
-
-        try {
-            this.carTotalNames = inputCarNames();
-            this.tryOfNumbers = inputTryOfNumber();
-        } catch (InputMismatchException e) {
-            System.out.println("[Error] 올바른 값을 입력해주세요.");
-            e.printStackTrace();
-        }
-    }
+    private Scanner in = new Scanner(System.in);
 
     public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
-        return in.nextLine();
+        String carName = "";
+
+        try {
+            carName = in.nextLine();
+        }catch (InputMismatchException e) {
+            System.out.println("[Error] 올바른 값을 입력해주세요.");
+            e.printStackTrace();
+        }
+
+        return carName;
     }
 
     public int inputTryOfNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
 
-        int num = in.nextInt();
+        int num = 0;
+
+        try {
+            num = in.nextInt();
+        }catch (InputMismatchException e) {
+            System.out.println("[Error] 올바른 값을 입력해주세요.");
+            e.printStackTrace();
+        }
 
         if (num <= 0) {
             throw new IllegalArgumentException("자동차 대수는 0 이하일 수 없습니다.");
         }
 
         return num;
-    }
-
-    public String getCarTotalNames() {
-        return this.carTotalNames;
-    }
-
-    public int getTryOfNumbers() {
-        return this.tryOfNumbers;
     }
 }
