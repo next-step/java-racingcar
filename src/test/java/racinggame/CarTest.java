9e4f018b63938ca77e59c728bbcad17aa260580a
pch8388 <pch838811@gmail.com>
2019-06-19 22:39:52 +0900

    test : 이름을 받아서 객체를 생성하도록 Car class 변경 - 객체생성 테스트

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -1,6 +1,8 @@
 package racinggame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racinggame.model.Car;
@@ -9,11 +11,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
+  private Car car;
+
+  @BeforeEach
+  void setUp() {
+    car = new Car("TEST1");
+  }
+
   @DisplayName("자동차 이동 - 이동하지 않는 경우")
   @ParameterizedTest
   @ValueSource(ints = {1, 2, 3})
   void notMove(int randomValue) {
-    final Car car = new Car();
     assertThat(car.move(randomValue)).isEqualTo(1);
   }
 
@@ -21,7 +29,11 @@ public class CarTest {
   @ParameterizedTest
   @ValueSource(ints = {4, 5, 6, 7, 8, 9})
   void move(int randomValue) {
-    final Car car = new Car();
     assertThat(car.move(randomValue)).isEqualTo(2);
+  }
+
+  @Test
+  void createCar() {
+    assertThat(car.getName()).isEqualTo("TEST1");
   }
 }
