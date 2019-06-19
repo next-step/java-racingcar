6e0a760ac9c0937a968680618c338311b3281e8b
sy.kim <kauksy9022@gmail.com>
2019-06-20 00:44:36 +0900

    fix(package step2): 2nd request review of step2

--- src/main/java/step2/ValidationCheck.java
+++ src/main/java/step2/ValidationCheck.java
@@ -1,15 +1,42 @@
 package step2;
 
+import org.apache.commons.lang3.ObjectUtils;
+
 public class ValidationCheck {
 
-    public void checkInputZero(int carQty, int tryCnt){
-        if(carQty == 0){
-            throw new IllegalArgumentException("경주에 출전하는 차량이 없습니다. 재입력해주세요.");
+    public void checkInputCarNumber(int carNumber) {
+        if (ObjectUtils.equals(null, carNumber)) {
+            throw new IllegalArgumentException("경주에 출전하는 차량이 없습니다(NULL). 재입력해주세요.");
         }
-
-        if(tryCnt == 0){
-            throw new IllegalArgumentException("경주 시도횟수가 0회이므로 게임을 시작할 수 없습니다. 재입력해주세요.");
+        if (carNumber == 0) {
+            throw new IllegalArgumentException("경주에 출전하는 차량은 적어도 1대 이상이어야 합니다. 재입력해주세요.");
         }
     }
 
+    public void checkInputTryCount(int tryCount) {
+        if (ObjectUtils.equals(null, tryCount)) {
+            throw new IllegalArgumentException("경주에 출전하는 차량이 없습니다(NULL). 재입력해주세요.");
+        }
+        if (tryCount == 0) {
+            throw new IllegalArgumentException("경주 시행 횟수는 적어도 1회 이상이어야 합니다. 재입력해주세요.");
+        }
+    }
+
+    public void checkCarNumber(int carNumber, int[] carPositions) {
+        if (carNumber != carPositions.length) {
+            throw new IllegalArgumentException("입력한 값과 출전하는 차량의 수가 일치하지 않습니다.");
+        }
+    }
+
+    public void checkRandomValue(int randomNum) {
+        if (randomNum > 9 || randomNum < 0) {
+            throw new IllegalArgumentException("주어진 랜덤 값의 범위에 해당하지 않습니다.(랜덤값: 0~9)");
+        }
+    }
+
+    public void checkGo(int randomNum) {
+        if (randomNum < 4) {
+            throw new IllegalArgumentException("전진 할 수 없는 랜덤 값 입니다.");
+        }
+    }
 }
--- src/test/java/step2/RacingGameTest.java
+++ src/test/java/step2/RacingGameTest.java
@@ -1,25 +1,55 @@
 package step2;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class RacingGameTest {
 
-        ValidationCheck vCheck = new ValidationCheck();
+    ValidationCheck validationCheck = new ValidationCheck();
 
-        @BeforeEach
-        void setUp(){
-            System.out.println("!! SetUp !! ");
-        }
-
-        @DisplayName("0이 아닌 양수가 입력되었는지 확인")
-        @Test
-        void inputZeroTest(){
-            assertThatIllegalArgumentException().isThrownBy(() -> vCheck.checkInputZero(0, 0));
-        }
-
-        @AfterEach
-        void tearDown() {
-            System.out.println("tearDown");
-        }
+    @BeforeEach
+    void setUp() {
+        System.out.println("!! SetUp !! ");
     }
+
+    @DisplayName("입력하는 차 수량 검증테스트")
+    @Test
+    void inputCarNumber() {
+        assertThatIllegalArgumentException().isThrownBy(() -> validationCheck.checkInputCarNumber(0));
+    }
+
+    @DisplayName("입력하는 게임시도 횟수 검증테스트")
+    @Test
+    void inputTryCountTest() {
+        assertThatIllegalArgumentException().isThrownBy(() -> validationCheck.checkInputTryCount(0));
+    }
+
+    @DisplayName("생성된 자동차 수량이 입력 값과 일치하는지 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 7})
+    void carNumberTest(int[] inputNum) {
+        assertThatIllegalArgumentException().isThrownBy(() -> validationCheck.checkCarNumber(3, inputNum));
+    }
+
+    @DisplayName("랜덤 값 범위 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 4, 0, 99})
+    void randomValueTest(int inputNum) {
+        assertThatIllegalArgumentException().isThrownBy(() -> validationCheck.checkRandomValue(inputNum));
+    }
+
+    @DisplayName("전진 할 수 있는 값인지 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 0, 4})
+    void goTest(int inputNum) {
+        assertThatIllegalArgumentException().isThrownBy(() -> validationCheck.checkGo(inputNum));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("tearDown");
+    }
+}
