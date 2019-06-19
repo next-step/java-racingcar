be4f9c960472bb18dd83aeae8efe4153e82414b3
최경수/Dooray개발실/NE <nova1122@naver.com>
2019-06-19 23:44:22 +0900

    refactor(CarWinnerComputerTest) : 데이터를 하드코딩으로 처리한 부분을 동적으로 처리하도록 리팩토링 했습니다.

--- src/test/java/com/mommoo/step2/CarWinnerComputerTest.java
+++ src/test/java/com/mommoo/step2/CarWinnerComputerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarWinnerComputerTest {
 
@@ -28,15 +28,25 @@ public class CarWinnerComputerTest {
         CarWinnerComputer carWinnerComputer = new CarWinnerComputer(cars);
         assertThat(carWinnerComputer.isWinnerNotExist()).isFalse();
         assertThat(carWinnerComputer.getWinnerCarNameList()).hasSize(2);
-        System.out.println(carWinnerComputer.getWinnerCarNameList());
-        assertThat(carWinnerComputer.getWinnerCarNameList()).contains("Mommoo1", "Mommoo2");
-        assertThat(carWinnerComputer.getWinnerCarNameList()).doesNotContain("Mommoo3");
+
+        String[] finalWinnerCarNames = cars.stream()
+                                           .limit(2)
+                                           .map(Car::getName)
+                                           .toArray(String[]::new);
+
+        String[] notWinnerCarNames = cars.stream()
+                                         .skip(2)
+                                         .map(Car::getName)
+                                         .toArray(String[]::new);
+
+        assertThat(carWinnerComputer.getWinnerCarNameList()).contains(finalWinnerCarNames);
+        assertThat(carWinnerComputer.getWinnerCarNameList()).doesNotContain(notWinnerCarNames);
     }
 
-    private List<Car> createCarListByPositions(int ...movePositions) {
+    private List<Car> createCarListByPositions(int... movePositions) {
         List<Car> cars = new LinkedList<>();
-        for (int index = 0 ; index < movePositions.length ; index ++) {
-            String carName = "Mommoo" + (index+1);
+        for (int index = 0; index < movePositions.length; index++) {
+            String carName = "Mommoo" + (index + 1);
             int movePosition = movePositions[index];
             Car car = new Car(carName, movePosition);
             cars.add(car);
