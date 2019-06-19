b4e6208c7f3a78642d0fda451a710cb280e57fe9
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-19 22:04:02 +0900

    feat(racing): add Car attribute name

--- src/main/java/racing/model/Car.java
+++ src/main/java/racing/model/Car.java
@@ -3,6 +3,15 @@ package racing.model;
 public class Car {
 
     private int position;
+    private String name;
+
+    private Car(String name) {
+        this.name = name;
+    }
+
+    public Car of(String name) {
+        return new Car(name);
+    }
 
     public void move() {
         position++;
