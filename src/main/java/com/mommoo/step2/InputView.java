7c305bebd148d29a8e6aca64fd20bd4344f6d6d7
최경수/Dooray개발실/NE <nova1122@naver.com>
2019-06-19 21:41:55 +0900

    feat(InputView) : 자동차 이름을 받을 수 있는 기능 추가.

--- src/main/java/com/mommoo/step2/InputView.java
+++ src/main/java/com/mommoo/step2/InputView.java
@@ -3,21 +3,19 @@ package com.mommoo.step2;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String GUIDE_TEXT_OF_WHAT_COUNT_OF_CAR = "자동차 대수는 몇 대 인가요?";
+    private static final String GUIDE_TEXT_OF_NAMING_OF_CAR = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분";
     private static final String GUIDE_TEXT_OF_WHAT_COUNT_OF_TRYING = "시도할 회수는 몇 회 인가요?";
 
     private final Scanner SCANNER = new Scanner(System.in);
 
-    public int getCountOfCar() {
-        return getIntValue(GUIDE_TEXT_OF_WHAT_COUNT_OF_CAR);
+    public String[] getCarNames() {
+        System.out.println(GUIDE_TEXT_OF_NAMING_OF_CAR);
+        String inputCarNames = SCANNER.nextLine();
+        return inputCarNames.split(",");
     }
 
     public int getCountOfTrying() {
-        return getIntValue(GUIDE_TEXT_OF_WHAT_COUNT_OF_TRYING);
-    }
-
-    private int getIntValue(String guideText) {
-        System.out.println(guideText);
+        System.out.println(GUIDE_TEXT_OF_WHAT_COUNT_OF_TRYING);
         return SCANNER.nextInt();
     }
 }
