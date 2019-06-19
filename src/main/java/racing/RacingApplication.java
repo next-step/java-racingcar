f80c87b42393059f384816082621b96b9f6a0122
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-20 06:58:27 +0900

    refactor(racing): refactoring InputView

--- src/main/java/racing/RacingApplication.java
+++ src/main/java/racing/RacingApplication.java
@@ -3,28 +3,15 @@ package racing;
 import racing.io.InputView;
 import racing.model.Car;
 import racing.model.RacingGame;
-import racing.model.RandomNumberCreator;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class RacingApplication {
 
     public static void main(String[] args) {
-        List<Car> cars = createCars();
-        int raceCount = getRaceCount();
+        List<Car> cars = InputView.createCarsByNames();
+        int raceCount = InputView.getRaceCount();
         RacingGame.of(cars, raceCount).run();
 
-    }
-
-    private static List<Car> createCars() {
-        return InputView.getCarNames()
-                .stream()
-                .map(Car::of)
-                .collect(Collectors.toList());
-    }
-
-    private static int getRaceCount() {
-        return InputView.getRaceCount();
     }
 }
--- src/main/java/racing/io/InputView.java
+++ src/main/java/racing/io/InputView.java
@@ -1,24 +1,26 @@
 package racing.io;
 
+import racing.model.Car;
 import racing.model.Splitter;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
-
-    public static int getCarCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return new Scanner(System.in).nextInt();
-    }
 
     public static int getRaceCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         return new Scanner(System.in).nextInt();
     }
 
+    public static List<Car> createCarsByNames() {
+        return getCarNames().stream()
+                .map(Car::of)
+                .collect(Collectors.toList());
+    }
 
-    public static List<String> getCarNames() {
+    private static List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return Splitter.spiltWithComma(new Scanner(System.in).nextLine());
     }
