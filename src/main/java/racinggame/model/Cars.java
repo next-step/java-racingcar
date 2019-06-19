4abf05464e98ff22c1dfd4d9c4e1bce0b7dfe129
pch8388 <pch838811@gmail.com>
2019-06-19 22:44:26 +0900

    feat : 이름을 받아서 객체를 생성하도록 Cars class 변경

--- src/main/java/racinggame/model/Cars.java
+++ src/main/java/racinggame/model/Cars.java
@@ -2,8 +2,11 @@ package racinggame.model;
 
 import racinggame.util.RandomValueUtil;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -11,10 +14,10 @@ public class Cars {
   private final List<Car> cars;
 
 
-  public Cars(int carCount) {
-    cars = IntStream.range(0, carCount)
-                    .mapToObj(i -> new Car())
-                    .collect(toList());
+  public Cars(String names) {
+    cars = Stream.of(names.split(","))
+                 .map(Car::new)
+                 .collect(toList());
   }
 
   public Cars moveCars() {
@@ -27,5 +30,4 @@ public class Cars {
                .map(Car::getStatus)
                .collect(toList());
   }
-
 }
