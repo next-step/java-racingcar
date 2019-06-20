5c92101ca9c9e19a1b9c602406875a081572a77a
Bentley <p2c2kbf@gmail.com>
2019-06-20 14:44:09 +0900

    fix:[피드백 적용]: carInfo 생성자를 활용하여 객체 생성/ set함수 대체

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,8 +1,8 @@
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    public static void userInput(RacingGame racingGame, ArrayList<CarInfo> carInfo) {
+    public static RacingGame userInput(List<CarInfo> carInfo) {
         Scanner scanner = new Scanner(System.in);
         String inputCarList;
         String[] carList;
@@ -11,14 +11,14 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         inputCarList = scanner.nextLine();
         carList = inputCarList.split(",");
-        racingGame.setNumberOfCar(carList.length);
 
-        for (int i = 0; i < carList.length; i++) {
-            inputCarInfo = new CarInfo();
-            inputCarInfo.setCarName(carList[i]);
+        for (int i = 0; i < carList.length; i++) { //입력받은 자동차 이름을 이용하여 List carInfo에 추가
+            inputCarInfo = new CarInfo(carList[i]);
             carInfo.add(inputCarInfo);
         }
         System.out.println("시도할 회수는 몇 회 인가요?");
-        racingGame.setTime(scanner.nextInt());
+        int timeofRace = scanner.nextInt();
+
+        return new RacingGame(timeofRace);
     }
 }
