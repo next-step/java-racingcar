cf90ffe909a448d0f1a34e5eef8552d38558d7b7
KIM JUNGHWAN <writer0713@naver.com>
2019-06-20 14:49:49 +0900

    #step4 [refactor] RacingGameRunner
    InputView, ResultView, RacingGame 세개의 객체를 사용하여 각각의 임무(입력, 로직, 출력)를 수행

--- src/main/java/racingcar/RacingGameRunner.java
+++ src/main/java/racingcar/RacingGameRunner.java
@@ -1,10 +1,12 @@
 package racingcar;
 
-import racingcar.controller.RacingController;
+import racingcar.model.Car;
 import racingcar.view.ConsoleInputView;
 import racingcar.view.ConsoleResultView;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
+
+import java.util.List;
 
 public class RacingGameRunner {
 
@@ -13,7 +15,20 @@ public class RacingGameRunner {
     InputView inputView = new ConsoleInputView();
     ResultView resultView = new ConsoleResultView();
 
-    RacingController racingController = new RacingController(inputView, resultView);
-    racingController.startRacing();
+    inputView.askQuestions();
+    String[] names = inputView.getNames();
+    RacingGame racingGame = new RacingGame(names);
+
+    int rounds = inputView.getRounds();
+    for (int i = 1; i <= rounds; i++) {
+      resultView.printCurrentRound(i);
+      List<Car> cars = racingGame.race();
+      cars.forEach(resultView::printCurrentPositionOf);
+      resultView.addLineBreak();
+    }
+
+    String namesOfWinners = racingGame.awards();
+    resultView.printWinner(namesOfWinners);
+
   }
 }
