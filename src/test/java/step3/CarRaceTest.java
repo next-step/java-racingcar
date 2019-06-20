ca0e52fe8adac7f3cbf2abfae111da55920792b1
한정수 <integer@zuminternet.com>
2019-06-20 12:41:05 +0900

    feedback #step3 - CarRace 테스트, Winner 테스트 추가

--- src/test/java/step3/CarRaceTest.java
+++ src/test/java/step3/CarRaceTest.java
@@ -1,11 +1,40 @@
 package step3;
 
 import org.junit.jupiter.api.Test;
+import step3.domain.CarRace;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class CarRaceTest {
 
     @Test
-    void 입력된_값의_유효성검증이_유효하다() {
-        //TODO: 입력된 값 유효성 검증 로직이 유효한지 테스트 코드 작성
+    void 입력된_자동차이름이_유효하지않으면_예외가_발생한다() {
+        String invalidCarName = "";
+        int validNumberOfTrials = 3;
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    CarRace.raceStart(invalidCarName, validNumberOfTrials);
+                });
+    }
+
+    @Test
+    void 입력된_시도횟수가_유효하지않으면_예외가_발생한다() {
+        String validCarName = "testCar";
+        int invalidNumberOfTrials = 0;
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    CarRace.raceStart(validCarName, invalidNumberOfTrials);
+                });
+    }
+
+    @Test
+    void 입력된_자동차이름과_시도횟수가_유효하면_CarRace_객체가_생성된다() {
+        String validCarName = "testCar";
+        int validNumberOfTrials = 3;
+
+        assertThat(CarRace.raceStart(validCarName, validNumberOfTrials)).isNotNull();
     }
 }
