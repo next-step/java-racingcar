c018f88cfebfb3de1ef196cd28405550c9980ad6
pch8388 <pch838811@gmail.com>
2019-06-20 13:44:20 +0900

    refactor : 인스턴스 변수를 2개로 줄임

--- src/main/java/racinggame/model/RacingGame.java
+++ src/main/java/racinggame/model/RacingGame.java
@@ -8,19 +8,18 @@ public class RacingGame {
 
   private Cars cars;
   private int time;
-  private List<RacingInfo> racingInfos;
 
   public RacingGame(String names, int time) {
     cars = new Cars(names);
     this.time = time;
-    racingInfos = new ArrayList<>();
   }
 
   public List<RacingInfo> startGame() {
+    List<RacingInfo> racingInfos = new ArrayList<>();
+
     for (int i = RACING_START_VALUE; i <= time; i++) {
       move().getCars()
-            .stream()
-            .forEach(c -> racingInfos.add(new RacingInfo(c.getName(), c.getStatus())));
+            .forEach(car -> racingInfos.add(new RacingInfo(car.getName(), car.getStatus())));
     }
     return racingInfos;
   }
