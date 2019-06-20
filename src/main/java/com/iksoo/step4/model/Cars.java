48357f4fbaa30aeba0213bee5cea8aafc6594c37
IKSOO SHIN <iksoo@IKSOOui-MacBook-Pro.local>
2019-06-20 14:44:15 +0900

    fix(Cars) : Inning별 결과출력기능 제거 및 출력기능 view로 위임

---
+++ src/main/java/com/iksoo/step4/model/Cars.java
@@ -0,0 +1,31 @@
+package com.iksoo.step4.model;
+
+import com.iksoo.step4.controller.CarMainController;
+
+import java.util.List;
+import static com.iksoo.step4.view.CarOutputData.printBlankLine;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+        startRacing(this.cars);
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
+    }
+
+    public void startRacing(List<Car> cars) {
+        for (int i = 0; i < cars.get(0).getTotalInning(); i++) {
+            doOneInning(cars);
+        }
+    }
+
+    public void doOneInning(List<Car> cars) {
+        for (int i = 0; i < cars.size(); i++) {
+            cars.get(i).doNextInning(CarMainController.getRandom());
+        }
+    }
+}
