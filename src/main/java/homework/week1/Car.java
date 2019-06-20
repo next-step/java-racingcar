1abe815b441c7cf2f131476034ce4e05c4d31801
rabbitfoot <wowyowowyo@gmail.com>
2019-06-20 15:45:40 +0900

    [feat]자동차의 이름과 위치정보를 가지는 Car 클래스 추가

---
+++ src/main/java/homework/week1/Car.java
@@ -0,0 +1,19 @@
+package homework.week1;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
