df6f777043c1c14bd05723091361d38214809da3
IKSOO SHIN <iksoo@IKSOOui-MacBook-Pro.local>
2019-06-20 15:39:51 +0900

    test(TestCode) : 테스트코드 작성

--- src/test/java/com/iksoo/step4/model/CarTest.java
+++ src/test/java/com/iksoo/step4/model/CarTest.java
@@ -1,4 +1,41 @@
 package com.iksoo.step4.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 public class CarTest {
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("pobi", 3);
+        car.doRacing(4);
+    }
+
+    @Test
+    void getCarNameTest() {
+        assertThat(car.getCarName()).isEqualTo("pobi");
+    }
+
+    @Test
+    void getTotalInningTest() {
+        assertThat(car.getTotalInning()).isEqualTo(3);
+    }
+
+    @Test
+    void getRanHistoryTest() {
+        assertThat(car.getRanHistory(1)).isEqualTo("pobi : -");
+    }
+
+    @Test
+    void isVictoryTest() {
+        assertThat(car.isVictory(1)).isTrue();
+    }
+
+    @Test
+    void getFinalPositionTest() {
+        assertThat(car.getFinalPosition()).isEqualTo(1);
+    }
 }
