17a8672b13594f4f70e95c9fccd11601d8c56b84
권병윤 <sksggg123@gmail.com>
2019-06-22 15:21:35 +0900

    [test] : Car의 위치 테스트 추가

--- src/test/java/edu/nextstep/racing/domain/PositionTest.java
+++ src/test/java/edu/nextstep/racing/domain/PositionTest.java
@@ -1,5 +1,9 @@
 package edu.nextstep.racing.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +15,16 @@ package edu.nextstep.racing.domain;
  * create date  : 2019-06-22 14:30
  */
 public class PositionTest {
+
+    @Test
+    void 현재위치_가져오기() {
+        Position position = new Position(3);
+        assertThat(position.getPosition()).isEqualTo(3);
+    }
+
+    @Test
+    void 자동차_이동시키기() {
+        Position position = new Position();
+        assertThat(position.move()).isEqualTo(1);
+    }
 }
