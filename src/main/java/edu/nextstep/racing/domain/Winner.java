e7b07a97e14e292e70fe898f56fb587c58f17b02
권병윤 <sksggg123@gmail.com>
2019-06-22 20:30:32 +0900

    [feat] : 우승자 선별 기능 추가

--- src/main/java/edu/nextstep/racing/domain/Winner.java
+++ src/main/java/edu/nextstep/racing/domain/Winner.java
@@ -1,5 +1,8 @@
 package edu.nextstep.racing.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +14,38 @@ package edu.nextstep.racing.domain;
  * create date  : 2019-06-22 19:52
  */
 public class Winner {
+
+    private final int winScore;
+    private Cars winner;
+
+    public Winner(Cars winner, int winScore) {
+        this.winner = winner;
+        this.winScore = winScore;
+    }
+
+    public static Winner checkPosition(List<Car> candinateWinner) {
+        List<Integer> positionList = candinateWinner.stream()
+                .map(Car::getCarPosition)
+                .collect(Collectors.toList());
+
+        int maxPosition = getMaxPosition(positionList);
+
+        List<Car> winners = candinateWinner.stream()
+                .filter(car -> car.getCarPosition() == maxPosition)
+                .collect(Collectors.toList());
+
+        return new Winner(new Cars(winners), maxPosition);
+    }
+
+    public static int getMaxPosition(List<Integer> positionList) {
+        return positionList.stream()
+                .max(Integer::compare)
+                .get();
+    }
+
+    public List<String> getCarNames() {
+        return this.winner.stream()
+                .map(Car::getCarName)
+                .collect(Collectors.toList());
+    }
 }
