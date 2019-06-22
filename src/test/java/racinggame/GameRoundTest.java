1a69f6e9655b7ce08c9b30e927510e8db5e74587
SangHyoun Yun <hainco@gmail.com>
2019-06-23 02:30:33 +0900

    test(racingGame): GameRoundTest 를 setUp 으로 최적화

--- src/test/java/racinggame/GameRoundTest.java
+++ src/test/java/racinggame/GameRoundTest.java
@@ -1,22 +1,26 @@
 package racinggame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class GameRoundTest {
+    private GameRound gameRound;
+
+    @BeforeEach
+    void setUp() {
+        gameRound = new GameRound(2);
+    }
+
     @Test
     void 게임이_진행중이면_isFinished_함수는_false_반환() {
-        GameRound gameRound = new GameRound(2);
-
         assertThat(gameRound.isFinished()).isFalse();
     }
 
     @Test
     void 게임_라운드를_초과하면_isFinished_함수는_true_반환() {
-        GameRound gameRound = new GameRound(2);
-
         gameRound = gameRound.increaseCurrentRound();
         gameRound = gameRound.increaseCurrentRound();
 
@@ -25,8 +29,6 @@ class GameRoundTest {
 
     @Test
     void 라운드가_끝났을때_increaseCurrentRound_호출시_에러() {
-        GameRound gameRound = new GameRound(2);
-
         gameRound = gameRound.increaseCurrentRound();
         gameRound = gameRound.increaseCurrentRound();
 
