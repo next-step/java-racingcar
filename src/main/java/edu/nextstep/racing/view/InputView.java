a2ed1aa0a2ab09315345f55786886362b7f9bbd9
권병윤 <sksggg123@gmail.com>
2019-06-22 15:56:15 +0900

    [feat] : 사용자 입력 UI 추가

--- src/main/java/edu/nextstep/racing/view/InputView.java
+++ src/main/java/edu/nextstep/racing/view/InputView.java
@@ -1,5 +1,7 @@
 package edu.nextstep.racing.view;
 
+import java.util.Scanner;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +13,24 @@ package edu.nextstep.racing.view;
  * create date  : 2019-06-22 15:04
  */
 public class InputView {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String inputCars() {
+        printMessage("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String playerNames = scanner.nextLine();
+
+        return playerNames;
+    }
+
+    public static int inputRound() {
+        printMessage("시도할 회수는 몇회인가요?");
+        String round = scanner.nextLine();
+
+        return Integer.parseInt(round);
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
+    }
 }
