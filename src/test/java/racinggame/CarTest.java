5aa4d3b1339039d778dfc74ac0804006308a5031
SangHyoun Yun <hainco@gmail.com>
2019-06-23 02:15:21 +0900

    test(racingGame): CarTest 를 setUp 으로 최적화

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -1,27 +1,32 @@
 package racinggame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
-    private String carName = "실험자동차";
+    private String carName;
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        carName = "실험자동차";
+        car = new Car(carName);
+    }
 
     @Test
     void 생성후_기본위치() {
-        Car car = new Car(carName);
         assertThat(car.getPosition()).isEqualTo(Car.INITIAL_CAR_POSITION);
     }
 
     @Test
     void 자동차는_주어진_이름으로_생성됨() {
-        Car car = new Car(carName);
         assertThat(car.getCarName()).isEqualTo(carName);
     }
 
     @Test
     void move_호출시_위치_1만큼_이동() {
-        Car car = new Car(carName);
         car = car.move();
         assertThat(car.getPosition()).isEqualTo(Car.INITIAL_CAR_POSITION + Car.MOVE_DISTANCE_PER_ONCE);
     }
