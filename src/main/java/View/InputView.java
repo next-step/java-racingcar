1039151a31196a3af0c440afb6817eac0c66d396
Bentley <p2c2kbf@gmail.com>
2019-06-22 15:50:32 +0900

    fix(피드백 사항): 누락된 중괄호 추가

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class InputView {
     public static RacingGame userInput(List<CarInfo> carInfo) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String inputCarList = scanner.nextLine();
         String[] carList = inputCarList.split(",");
@@ -28,9 +28,11 @@ public class InputView {
 
     // 사용자가 입력한 공백 문자 혹은 개행문자가 포함되었는지 체크하는 함수
     public static void checkNameOfCar(String[] inputStrings) {
-        for (int i = 0; i < inputStrings.length; ++i)
-            if (inputStrings[i].isEmpty() || inputStrings[i].equals(" ") || inputStrings[i].equals("\n"))
+        for (int i = 0; i < inputStrings.length; ++i) {
+            if (inputStrings[i].isEmpty() || inputStrings[i].equals(" ") || inputStrings[i].equals("\n")) {
                 throw new IllegalArgumentException("입력값이 잘못되었습니다. 자동차 이름을 다시 한번 입력해주세요.");
+            }
+        }
     }
 }
 
