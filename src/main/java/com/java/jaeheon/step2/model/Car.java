f8d3949c9fe8908656b8fe50ba9be04bca9cfeb2
문재헌 <jaeheon209@gmail.com>
2019-06-23 00:29:05 +0900

    * refactor(step3) : Car 클래스 리팩토링
    - 상수에 static 예약어 추가
    - attemptsToMove 메소드에 삼항 연산자 적용
    - attemptsToMove return 부분을 go/stop 메소드로 변경

--- src/main/java/com/java/jaeheon/step2/model/Car.java
+++ src/main/java/com/java/jaeheon/step2/model/Car.java
@@ -1,42 +1,47 @@
 package com.java.jaeheon.step2.model;
 
 public class Car {
-	private final int MOVING_CONDITION = 4;
-	private final int INITIAL_VALUE = 0;
+    private static final int INITIAL_VALUE = 0;
+    private static final int MOVING_CONDITION = 4;
 
-	private final int numberOfAttempts;
-	private final int position;
-	private final String nameOfCar;
+    private final int numberOfAttempts;
+    private final int position;
+    private final String nameOfCar;
 
-	public Car(String nameOfCar) {
-		this.numberOfAttempts = INITIAL_VALUE;
-		this.position = INITIAL_VALUE;
-		this.nameOfCar = nameOfCar;
-	}
+    public Car(String nameOfCar) {
+        this.numberOfAttempts = INITIAL_VALUE;
+        this.position = INITIAL_VALUE;
+        this.nameOfCar = nameOfCar;
+    }
 
-	public Car(int numberOfAttempts, int position, String nameOfCar) {
-		this.numberOfAttempts = numberOfAttempts;
-		this.position = position;
-		this.nameOfCar = nameOfCar;
-	}
+    public Car(int numberOfAttempts, int position, String nameOfCar) {
+        this.numberOfAttempts = numberOfAttempts;
+        this.position = position;
+        this.nameOfCar = nameOfCar;
+    }
 
-	public Car attemptsToMove(int condition) {
-		if (MOVING_CONDITION <= condition) {
-			return new Car(getNumberOfAttempts() + 1, getPosition() + 1, getNameOfCar());
-		}
-		return new Car(getNumberOfAttempts() + 1, getPosition(), getNameOfCar());
-	}
+    public Car attemptsToMove(int condition) {
+        return MOVING_CONDITION <= condition ? go() : stop();
+    }
 
-	public int getNumberOfAttempts() {
-		return numberOfAttempts;
-	}
+    private Car go() {
+        return new Car(getNumberOfAttempts() + 1, getPosition() + 1, getNameOfCar());
+    }
 
-	public Integer getPosition() {
-		return position;
-	}
+    private Car stop() {
+        return new Car(getNumberOfAttempts() + 1, getPosition(), getNameOfCar());
+    }
 
-	public String getNameOfCar() {
-		return nameOfCar;
-	}
+    public int getNumberOfAttempts() {
+        return numberOfAttempts;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public String getNameOfCar() {
+        return nameOfCar;
+    }
 
 }
