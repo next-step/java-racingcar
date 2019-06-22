12493f7d2757d22c17479853b4fd63b6e81976b5
권병윤 <sksggg123@gmail.com>
2019-06-22 15:20:28 +0900

    [test] : CarName 테스트 추가

--- src/test/java/edu/nextstep/racing/domain/CarNameTest.java
+++ src/test/java/edu/nextstep/racing/domain/CarNameTest.java
@@ -1,5 +1,10 @@
 package edu.nextstep.racing.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +16,17 @@ package edu.nextstep.racing.domain;
  * create date  : 2019-06-22 14:25
  */
 public class CarNameTest {
+
+    @Test
+    void 자동차이름_생성시_예외상황() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+           CarName name = new CarName("");
+        });
+    }
+
+    @Test
+    void 자동차이름_가져오기() {
+        CarName name = new CarName("TEST");
+        assertThat(name.getName()).isEqualTo("TEST");
+    }
 }
