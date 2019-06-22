5486bc9b3f644cd403964ec3b5e3d5e7be896bf9
권병윤 <sksggg123@gmail.com>
2019-06-22 15:55:28 +0900

    [feat] : 자동차들의 전진 기능

--- src/main/java/edu/nextstep/racing/domain/Cars.java
+++ src/main/java/edu/nextstep/racing/domain/Cars.java
@@ -1,5 +1,10 @@
 package edu.nextstep.racing.domain;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +16,27 @@ package edu.nextstep.racing.domain;
  * create date  : 2019-06-22 14:33
  */
 public class Cars {
+    private static final int RANDOM_NUMBER_RANGE = 9;
+    private List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public Cars doMoveGame() {
+        cars.stream()
+                .peek(car -> car.carMove(validMoveNumber()))
+                .collect(Collectors.toList());
+
+        return new Cars(this.cars);
+    }
+
+    public Stream<Car> stream() {
+        return this.cars.stream();
+    }
+
+    private int validMoveNumber() {
+        Random random = new Random();
+        return random.nextInt(RANDOM_NUMBER_RANGE);
+    }
 }
