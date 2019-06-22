e35b88dad5181ac474c6f54911d52ab2219e27d2
권병윤 <sksggg123@gmail.com>
2019-06-22 16:32:52 +0900

    [feat] : 입력받은 문자열 구분자(,)로 자르는 기능

--- src/main/java/edu/nextstep/racing/utils/SplitUtils.java
+++ src/main/java/edu/nextstep/racing/utils/SplitUtils.java
@@ -1,5 +1,8 @@
 package edu.nextstep.racing.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * author       : gwonbyeong-yun <sksggg123>
  * ------------------------------------------
@@ -11,4 +14,8 @@ package edu.nextstep.racing.utils;
  * create date  : 2019-06-22 16:00
  */
 public class SplitUtils {
+
+    public static List<String> commaSplit(String carNames) {
+        return Arrays.asList(carNames.split(","));
+    }
 }
