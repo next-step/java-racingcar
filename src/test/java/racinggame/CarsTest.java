09f813ac13a342af1c723a51dee33012e2135c30
SangHyoun Yun <hainco@gmail.com>
2019-06-23 01:29:13 +0900

    test(racingGame): Cars 테스트를 리팩토링

--- src/test/java/racinggame/CarsTest.java
+++ src/test/java/racinggame/CarsTest.java
@@ -1,19 +1,36 @@
 package racinggame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
     private String carNameString = "pobi,crong,honux";
-    private int carQuantity = 3;
+    private Cars cars;
+
+    @BeforeEach
+    void setUp() {
+        CarNames carNames = new CarNames(carNameString);
+        cars = new Cars(carNames);
+    }
 
     @Test
     void 생성자_정상() {
-        CarNames carNames = new CarNames(carNameString);
-
-        Cars cars = new Cars(carNames);
         assertThat(cars.getPositions()).containsKeys("pobi", "crong", "honux");
         assertThat(cars.getPositions()).containsValues(1, 1, 1);
+    }
+
+    @Test
+    void 모든_자동차가_이동한다() {
+        MoveDecider moveDecider = new MoveDecider() {
+            @Override
+            public boolean canMove() {
+                return true;
+            }
+        };
+        cars = cars.moveCarsByDecider(moveDecider);
+        assertThat(cars.getPositions()).containsKeys("pobi", "crong", "honux");
+        assertThat(cars.getPositions()).containsValues(2, 2, 2);
     }
 }
