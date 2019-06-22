a56ced167807320ca8b27c49a02582c233158c93
권병윤 <sksggg123@gmail.com>
2019-06-22 15:19:35 +0900

    [feat] : Car 클래스 기능 구현

--- src/main/java/edu/nextstep/racing/domain/Car.java
+++ src/main/java/edu/nextstep/racing/domain/Car.java
@@ -11,4 +11,37 @@ package edu.nextstep.racing.domain;
  * create date  : 2019-06-22 14:11
  */
 public class Car {
+
+    private static final int MOVE_POSSIBLE_NUMBER = 4;
+    private CarName name;
+    private Position position;
+
+    public Car(String name) {
+        this.name = new CarName(name);
+        this.position = new Position();
+    }
+
+    public Car(String name, int position) {
+        this.name = new CarName(name);
+        this.position = new Position(position);
+    }
+
+    public void carMove(int validNumber) {
+        if(isMove(validNumber)) {
+            position.move();
+        }
+    }
+
+    public int getCarPosition() {
+        return this.position.getPosition();
+    }
+
+    public String getCarName() {
+        return this.name.getName();
+    }
+
+    private boolean isMove(int validNumber) {
+        return validNumber >= MOVE_POSSIBLE_NUMBER ? true : false;
+    }
+
 }
