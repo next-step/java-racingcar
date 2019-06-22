ebbdf6c015e5e7d264bac87fb0252544e95151ff
권병윤 <sksggg123@gmail.com>
2019-06-22 15:21:03 +0900

    [feat] : Car 클래스의 원시타입(name)을 객체로 분리

--- src/main/java/edu/nextstep/racing/domain/CarName.java
+++ src/main/java/edu/nextstep/racing/domain/CarName.java
@@ -11,4 +11,21 @@ package edu.nextstep.racing.domain;
  * create date  : 2019-06-22 14:17
  */
 public class CarName {
+
+    private String name;
+
+    public CarName(String name) {
+        nameValidation(name);
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private void nameValidation(String carName) {
+        if(carName == null || "".equals(carName)) {
+            throw new IllegalArgumentException("자동차 이름이 유효하지 않습니다.");
+        }
+    }
 }
