1d86300abcc158044ce36769a4c27aec4227fb46
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-22 23:16:24 +0900

    test(car): add test code for car move

---
+++ src/main/java/racing/Car.java
@@ -0,0 +1,18 @@
+package racing;
+
+public class Car {
+
+    private Position position;
+
+    public Car() {
+        position = new Position();
+    }
+
+    public void move() {
+
+    }
+
+    public int getPosition() {
+        return position.getPositon();
+    }
+}
---
+++ src/main/java/racing/Position.java
@@ -0,0 +1,10 @@
+package racing;
+
+public class Position {
+
+    private int position = 0;
+
+    public int getPositon() {
+        return this.position;
+    }
+}
