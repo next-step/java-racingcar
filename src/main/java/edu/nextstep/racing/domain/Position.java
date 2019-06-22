0850a62be89918004add84863aa5bfc652042ed8
권병윤 <sksggg123@gmail.com>
2019-06-22 15:22:11 +0900

    [feat] : Car 클래스의 원시타입(position)을 객체로 분리

--- src/main/java/edu/nextstep/racing/domain/Position.java
+++ src/main/java/edu/nextstep/racing/domain/Position.java
@@ -11,4 +11,23 @@ package edu.nextstep.racing.domain;
  * create date  : 2019-06-22 14:12
  */
 public class Position {
+
+    private static final int INIT_POSITION = 0;
+    private int position;
+
+    public Position() {
+        this.position = INIT_POSITION;
+    }
+
+    public Position(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public int move() {
+        return ++this.position;
+    }
 }
