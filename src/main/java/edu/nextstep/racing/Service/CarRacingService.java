8846cf2edf94cec3c2c029094168b3ca07d303e9
권병윤 <sksggg123@gmail.com>
2019-06-22 16:32:21 +0900

    [test] : 입력받은 문자열 구분자(,)로 자르는 테스트

---
+++ src/main/java/edu/nextstep/racing/Service/CarRacingService.java
@@ -0,0 +1,14 @@
+package edu.nextstep.racing.Service;
+
+/**
+ * author       : gwonbyeong-yun <sksggg123>
+ * ------------------------------------------
+ * | email        : sksggg123               |
+ * | github       : github.com/sksggg123    |
+ * | blog         : sksggg123.github.io     |
+ * ------------------------------------------
+ * project      : java-racingcar
+ * create date  : 2019-06-22 15:59
+ */
+public class CarRacingService {
+}
