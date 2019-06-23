ed9d237bf324d140bf8303ff2663a82cb31724a4
HyeonAh Lee <bono.yi@gmail.com>
2019-06-23 23:22:11 +0900

    refactor: 질문 InputView로 이동

--- src/main/java/racingcar/RacingMain.java
+++ src/main/java/racingcar/RacingMain.java
@@ -5,21 +5,15 @@ import racingcar.model.RacingGame;
 import racingcar.utils.NumGenerator;
 import racingcar.utils.RandomNumGenerator;
 
-import java.util.Scanner;
-
 public class RacingMain {
     public static void main(String[] args) {
 
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String carNames = Input.getCarNames();
-
-        System.out.println("시도할 회수는 몇회인가요?");
         int times = Input.getTimes();
         NumGenerator numGenerator = new RandomNumGenerator();
 
         RacingGame rg = new RacingGame(carNames, times, numGenerator);
         Cars racingCars = rg.run();
         racingCars.printWinners();
-
     }
 }
