09fe7eef29d04a86c9771af06b516a7b52cdaf47
sunkyung <sunkyungch@gmail.com>
2019-06-24 01:14:55 +0900

    fix(carRacing) 승자를 선택 클래스를 사용하도록 수정

--- src/main/java/carRacing/model/Car.java
+++ src/main/java/carRacing/model/Car.java
@@ -1,12 +1,11 @@
 package carRacing.model;
 
 import com.google.common.base.Strings;
-
 import java.util.Objects;
 
 public class Car {
 
-    private final static int MOVEMENT_LIMIT_BY_OUTPUT = 4;
+    final static int MOVEMENT_LIMIT_BY_OUTPUT = 4;
     private String name;
     private int position;
 
@@ -15,11 +14,21 @@ public class Car {
         this.name = name;
     }
 
+    public Car(String name, int position) {
+        if(Strings.isNullOrEmpty(name)) throw new IllegalArgumentException("");
+        this.name = name;
+        this.position = position;
+    }
+
     public int move(int gamePoint) {
         if (canMove(gamePoint)) {
             position ++;
         }
         return position;
+    }
+
+    public boolean isWinner(int bestPosition) {
+        return position == bestPosition;
     }
 
     public int getPosition() {
@@ -30,12 +39,16 @@ public class Car {
         return name;
     }
 
+    private boolean canMove(int output) {
+        return MOVEMENT_LIMIT_BY_OUTPUT <= output;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return name.equals(car.name);
+        return Objects.equals(name, car.name);
     }
 
     @Override
@@ -43,8 +56,11 @@ public class Car {
         return Objects.hash(name);
     }
 
-    private boolean canMove(int output) {
-        return MOVEMENT_LIMIT_BY_OUTPUT <= output;
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", position=" + position +
+                '}';
     }
-
 }
--- src/test/java/carRacing/model/RacingGameTest.java
+++ src/test/java/carRacing/model/RacingGameTest.java
@@ -28,7 +28,7 @@ class RacingGameTest {
     @DisplayName("빈 자동차 리스트로 생성시 예외처리")
     void RacingGameTest() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new RacingGame(new RacingCarGroup(new ArrayList<>()), new MockGamePoint(3));
+            new RacingGame(new RacingCars(new ArrayList<>()), new MockGamePoint(3));
         });
     }
 
@@ -36,11 +36,11 @@ class RacingGameTest {
     @DisplayName("자동차 경주 자동차 이동")
     void race() {
         MockGamePoint mockGamePoint = new MockGamePoint(5); // 항상 전진
-        RacingGame racingGame = new RacingGame(new RacingCarGroup(cars), mockGamePoint);
+        RacingGame racingGame = new RacingGame(new RacingCars(cars), mockGamePoint);
 
 
         racingGame.race();
-        RacingCarGroup race = racingGame.race();
+        RacingCars race = racingGame.race();
         List<Car> racingCars = race.getRacingCars();
         for (Car racingCar : racingCars) {
             assertThat(racingCar.getPosition()).isEqualTo(2);
@@ -51,11 +51,11 @@ class RacingGameTest {
     @DisplayName("자동차 경주 자동차 이동할 수 없음")
     void cannotMove() {
         MockGamePoint mockGamePoint = new MockGamePoint(3); // 항상 Stop
-        RacingGame racingGame = new RacingGame(new RacingCarGroup(cars), mockGamePoint);
+        RacingGame racingGame = new RacingGame(new RacingCars(cars), mockGamePoint);
 
 
         racingGame.race();
-        RacingCarGroup race = racingGame.race();
+        RacingCars race = racingGame.race();
         List<Car> racingCars = race.getRacingCars();
         for (Car racingCar : racingCars) {
             assertThat(racingCar.getPosition()).isEqualTo(0);
@@ -66,10 +66,10 @@ class RacingGameTest {
     @DisplayName("우승자 판별")
     void gameResult() {
         MockGamePoint mockGamePoint = new MockGamePoint(5); // 항상 전진
-        RacingGame racingGame = new RacingGame(new RacingCarGroup(cars), mockGamePoint);
+        RacingGame racingGame = new RacingGame(new RacingCars(cars), mockGamePoint);
 
         racingGame.race();
-        RacingCarGroup winners = racingGame.gameResult();
+        RacingCars winners = racingGame.gameResult();
 
         Car ferrari = new Car("ferrari");
         Car bmw = new Car("bmw");
