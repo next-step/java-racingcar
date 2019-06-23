6e31e7ccc034b563ef6913d543dd177a4de3b3a0
HyeonAh Lee <bono.yi@gmail.com>
2019-06-23 11:36:02 +0900

    feat: 이름과 position 을 가지고 있는 자동차 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int position;
+
+    private Car(){
+
+    }
+
+    public Car(String name, int position){
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getPosition(){
+        return this.position;
+    }
+}
