d98d4e80106717bef122653ccc46418b2913ac30
HyeonAh Lee <bono.yi@gmail.com>
2019-06-23 11:31:19 +0900

    feat: 시도할 횟수 입력 받기

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -6,4 +6,7 @@ public class Input {
     public static String getCarNames(Scanner scanner){
         return scanner.nextLine();
     }
+    public static int getTimes(Scanner scanner){
+        return scanner.nextInt();
+    }
 }
--- src/main/java/racingcar/RacingMain.java
+++ src/main/java/racingcar/RacingMain.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class RacingMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String carNames = Input.getCarNames(sc);
+
+        System.out.println("시도할 회수는 몇회인가요?");
+        int times = Input.getTimes(sc);
     }
 }
