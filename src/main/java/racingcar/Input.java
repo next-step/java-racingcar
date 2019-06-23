3c6d00c7f5f2ba93812fd07dc2ddb97e4e4e3b15
HyeonAh Lee <bono.yi@gmail.com>
2019-06-23 15:44:30 +0900

    style: reformat code

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -3,10 +3,11 @@ package racingcar;
 import java.util.Scanner;
 
 public class Input {
-    public static String getCarNames(Scanner scanner){
+    public static String getCarNames(Scanner scanner) {
         return scanner.nextLine();
     }
-    public static int getTimes(Scanner scanner){
+
+    public static int getTimes(Scanner scanner) {
         return scanner.nextInt();
     }
 }
