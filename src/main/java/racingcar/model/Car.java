7208c74a027b75fca42568c7393820d538f5125d
HyeonAh Lee <bono.yi@gmail.com>
2019-06-23 12:02:34 +0900

    feat: 랜덤값 조건에 따른 자동자 전진 또는 멈춤 기능

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,16 +1,22 @@
 package racingcar.model;
 
+import racingcar.utils.NumGenerator;
+
 public class Car {
+    private final int GO_CONDITION = 4;
     private String name;
     private int position;
+    private NumGenerator randomNum;
 
     private Car(){
 
     }
 
-    public Car(String name, int position){
+    public Car(String name, int position, NumGenerator randomNum){
         this.name = name;
         this.position = position;
+        this.randomNum = randomNum;
+
     }
 
     public String getName(){
@@ -21,7 +27,13 @@ public class Car {
         return this.position;
     }
 
+    public int getRandomNum(){
+        return randomNum.nextInt();
+    }
+
     public void move(){
-        this.position++;
+        if(getRandomNum() > GO_CONDITION) {
+            this.position++;
+        }
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,25 +3,37 @@ package racingcar;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
+import racingcar.utils.NumGenerator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+
 public class CarTest {
-    private Car car;
+    private Car goingCar;
+    private Car stoppdCar;
+
     @BeforeEach
     void setUp() {
-        car = new Car("alice", 0);
+        NumGenerator goingNum = new FixedNumGenerator(5);
+        NumGenerator stopNum = new FixedNumGenerator(3);
+        goingCar = new Car("alice", 0, goingNum);
+        stoppdCar = new Car("alice", 0, stopNum);
     }
 
     @Test
-    void testCarMove() {
-        car.move();
-        assertThat(car.getPosition()).isEqualTo(1);
+    void testGoCar() {
+        goingCar.move();
+        assertThat(goingCar.getPosition()).isEqualTo(1);
 
-        car.move();
-        car.move();
-        assertThat(car.getPosition()).isEqualTo(3);
+        goingCar.move();
+        goingCar.move();
+        assertThat(goingCar.getPosition()).isEqualTo(3);
+    }
 
-
+    @Test
+    void testStopCar(){
+        stoppdCar.move();
+        assertThat(stoppdCar.getPosition()).isEqualTo(0);
     }
 }
