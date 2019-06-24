76b2ca1b253133375cfb073c1399dad32e920f38
sy.kim <kauksy9022@gmail.com>
2019-06-24 22:34:54 +0900

    fix(Application): Modify to input case.

--- src/main/java/step3/RacingGameWinnerApplication.java
+++ src/main/java/step3/RacingGameWinnerApplication.java
@@ -1,8 +1,5 @@
 package step3;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class RacingGameWinnerApplication {
 
     private InputView input;
@@ -15,23 +12,19 @@ class RacingGameWinnerApplication {
 
         RacingGameWinnerApplication app = new RacingGameWinnerApplication();
 
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String[] playerName = app.input.inputPlayerName();
-        int carNumber = app.input.inputCarNumber();
+        String[] carName = app.input.inputCarName();
+        int carNumber = carName.length;
         int tryCount = app.input.inputTryCount();
 
         RacingGameWinner racingGameWinner = new RacingGameWinner();
-         Map<String, Integer > racingScore = racingGameWinner.connectPlayerToCar(playerName, carNumber);
-        // 유효성 체크는 InputView에서 했으므로 PASS(Player 수와 차 대수가 동일한지)
-//        racingScore = racingGameWinner.connectPlayerToCar(playerName, carNumber);
-
-//        racingGameWinner.setInitPosition(carNumber); => Map초기화 시, 같이 초기화 되서 괜춘
+        racingGameWinner.setInitPosition(carNumber);
 
         System.out.println("실행결과");
 
         while (tryCount-- > 0) {
-            racingGameWinner.startRacingToSelectCar(racingScore);
-            racingGameWinner.resultView();
+            racingGameWinner.startRacing(carNumber);
+            racingGameWinner.racingView(carName);
         }
+        racingGameWinner.findMaxScoreWinner(carName);
     }
 }
