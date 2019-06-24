a813e84d70d90807496ab1d232c7608aa00dbb7c
sy.kim <kauksy9022@gmail.com>
2019-06-24 22:38:56 +0900

    fix(ResultView): Modify and add method to print carName.

--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -1,10 +1,17 @@
 package step3;
 
+import java.util.List;
+
 public class ResultView {
 
-    public void result(int[] carPositions) {
-        for (int i = 0; i < carPositions.length; i++) {
-            outputView(carPositions[i]);
+    private int maxScore = 0;
+    private String winner = "";
+
+    public void racingView(List<Integer> carPositions, String[] carName) {
+        for (int i = 0; i < carPositions.size(); i++) {
+            System.out.print(carName[i] + " : ");
+            outputView(carPositions.get(i));
+            maxScore = (maxScore < carPositions.get(i) ? carPositions.get(i) : maxScore);
         }
         System.out.println();
     }
@@ -14,5 +21,19 @@ public class ResultView {
             System.out.print("-");
         }
         System.out.println();
+    }
+
+    public void findMaxScoreWinner(List<Integer> carPositions, String[] carName) {
+        for (int i = 0; i < carPositions.size(); i++) {
+            findWinner(i, carPositions, carName);
+        }
+        System.out.print(winner.substring(0, winner.length() - 2));
+        System.out.println("가 최종 우승했습니다.");
+    }
+
+    private void findWinner(int index, List<Integer> carPositions, String[] carName) {
+        if (carPositions.get(index) == maxScore) {
+            winner += carName[index] + ", ";
+        }
     }
 }
