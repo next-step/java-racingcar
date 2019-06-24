9c01a0939588fd0cbae371486b5f54029c3d2a39
sy.kim <kauksy9022@gmail.com>
2019-06-24 22:33:38 +0900

    fix(ValidationCheck): Modify test case.

--- src/main/java/step3/ValidationCheck.java
+++ src/main/java/step3/ValidationCheck.java
@@ -4,37 +4,46 @@ import org.apache.commons.lang3.ObjectUtils;
 
 public class ValidationCheck {
 
-    public void checkInputCarNumber(int carNumber) {
-        if (ObjectUtils.equals(null, carNumber)) {
-            throw new IllegalArgumentException("경주에 출전하는 차량이 없습니다(NULL). 재입력해주세요.");
+    void checkInputPlayerName(String playerNames) {
+        if (ObjectUtils.equals(null, playerNames)) {
+            throw new IllegalArgumentException("경주에 출전하는 사람이 없습니다(NULL). 재입력해주세요.");
         }
-        if (carNumber == 0) {
-            throw new IllegalArgumentException("경주에 출전하는 차량은 적어도 1대 이상이어야 합니다. 재입력해주세요.");
+        if (playerNames.isEmpty()) {
+            throw new IllegalArgumentException("경주에 출전하는 사람이 없습니다(Empty). 재입력해주세요.");
         }
     }
 
-    public void checkInputTryCount(int tryCount) {
+    void checkInputSplitName(String[] eachName) {
+        if (eachName.length == 0) {
+            throw new IllegalArgumentException("경주에 출전하는 사람이 없습니다(NULL). 구분자(,)를 이용하여 입력해주세요.");
+        }
+        if (ObjectUtils.equals(null, eachName)) {
+            throw new IllegalArgumentException("경주에 출전하는 사람이 없습니다(NULL). 구분자(,)를 이용하여 입력해주세요.");
+        }
+    }
+
+    void checkInputTryCount(int tryCount) {
         if (ObjectUtils.equals(null, tryCount)) {
-            throw new IllegalArgumentException("경주에 출전하는 차량이 없습니다(NULL). 재입력해주세요.");
+            throw new IllegalArgumentException("경주 시행 횟수는 적어도 1회 이상이어야 합니다(NULL). 재입력해주세요.");
         }
         if (tryCount == 0) {
             throw new IllegalArgumentException("경주 시행 횟수는 적어도 1회 이상이어야 합니다. 재입력해주세요.");
         }
     }
 
-    public void checkCarNumber(int carNumber, int carPositions) {
-        if (carNumber != carPositions ) {
+    void checkCarNumber(int carNumber, int carPositionSize) {
+        if (carNumber != carPositionSize) {
             throw new IllegalArgumentException("입력한 값과 출전하는 차량의 수가 일치하지 않습니다.");
         }
     }
 
-    public void checkRandomValue(int randomNum) {
+    void checkRandomValue(int randomNum) {
         if (randomNum > 9 || randomNum < 0) {
             throw new IllegalArgumentException("주어진 랜덤 값의 범위에 해당하지 않습니다.(랜덤값: 0~9)");
         }
     }
 
-    public void checkGo(int randomNum) {
+    void checkGo(int randomNum) {
         if (randomNum < 4) {
             throw new IllegalArgumentException("전진 할 수 없는 랜덤 값 입니다.");
         }
