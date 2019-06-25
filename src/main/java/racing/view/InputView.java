bb73bc71994f506614657a72b006c52924297608
choihyunjin <iamchoihyunjin@gmail.com>
2019-06-25 11:58:56 +0900

    feat(racing): add car input for get racing  count

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -12,10 +12,16 @@ import java.util.stream.Stream;
 public class InputView {
 
     private static final String COMMA = ",";
+    private static Scanner scanner = new Scanner(System.in);
 
     public static List<Car> getCarsByNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return createCarsByName(splitWithComma(new Scanner(System.in).nextLine()));
+        return createCarsByName(splitWithComma(scanner.nextLine()));
+    }
+
+    public static int getRacingCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return scanner.nextInt();
     }
 
     private static List<Car> createCarsByName(String[] splitWithComma) {
