128ae63d06c93d8b00fdb77016d0102c8ff91220
sy.kim <kauksy9022@gmail.com>
2019-06-27 00:33:34 +0900

    fix(Main): 하나의 쓰레드에서 사용 할 수 있도록, main 안에 생성하여 사용.

--- src/main/java/step4/RacingGameWinnerApplication.java
+++ src/main/java/step4/RacingGameWinnerApplication.java
@@ -2,32 +2,23 @@ package step4;
 
 class RacingGameWinnerApplication {
 
-    private InputView input;
-    private ResultView result;
-    private RacingGameWinner racingGameWinner;
-
-    private RacingGameWinnerApplication() {
-        this.input = new InputView();
-        this.result = new ResultView();
-        this.racingGameWinner = new RacingGameWinner();
-    }
-
     public static void main(String[] args) {
+        InputView input = new InputView();
+        ResultView result = new ResultView();
+        RacingGameWinner racingGameWinner = new RacingGameWinner();
 
-        RacingGameWinnerApplication app = new RacingGameWinnerApplication();
-
-        String[] carName = app.input.inputCarName();
+        String[] carName = input.inputCarName();
         int carNumber = carName.length;
-        int tryCount = app.input.inputTryCount();
+        int tryCount = input.inputTryCount();
 
-        app.racingGameWinner.setInitPosition(carNumber);
+        racingGameWinner.setInitPosition(carNumber);
 
         System.out.println("실행결과");
 
         while (tryCount-- > 0) {
-            app.racingGameWinner.startRacing(carNumber);
-            app.result.racingView(app.racingGameWinner.carPositions, carName);
+            racingGameWinner.startRacing(carNumber);
+            result.racingView(racingGameWinner.carPositions, carName);
         }
-        app.result.findMaxScoreWinner(app.racingGameWinner.carPositions, carName);
+        result.findMaxScoreWinner(racingGameWinner.carPositions, carName);
     }
 }
