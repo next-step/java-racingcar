4e357e6b42a43d5ca292c8516185fde764f618a9
Bentley <p2c2kbf@gmail.com>
2019-06-26 12:52:48 +0900

    refector:  단위 테스트 함수 정리

--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -9,11 +9,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class RacingGameTest {
     int carPosition = 5;
     int maxPosition = 4;
+    String namesOfCar = "a,b,c";
+    int timeofGame = 10;
 
     @Test
     void checkNameOfCar() {
         String inputString = " ";
-
         assertThatIllegalArgumentException().isThrownBy(() -> {
                     if (inputString.isEmpty() || inputString.equals(" ") || inputString.equals("\n"))
                         throw new IllegalArgumentException("입력값이 잘못되었습니다. 자동차 이름을 다시 한번 입력해주세요.");
@@ -23,27 +24,24 @@ public class RacingGameTest {
 
     @Test
     void splitName() {
-        String names = "a,b,c";
-        assertThat(names.split(",")).containsExactly("a", "b", "c");
+        assertThat(namesOfCar.split(",")).containsExactly("a", "b", "c");
     }
 
     @Test
-    void isEnd() {
-        int timeOfGame = 5;
+    void isRaceOnging() {
         int indexofTimeofGame = 6;
 
-        assertThat(timeOfGame < indexofTimeofGame).isTrue();
+        assertThat(timeofGame > indexofTimeofGame).isTrue();
     }
 
     @Test
     public void getMaxPosition() {
-        int maxPosition = 4;
-        assertThat(Math.max(maxPosition, this.carPosition)).isEqualTo(5);
+        assertThat(Math.max(maxPosition, carPosition)).isEqualTo(5);
     }
 
     @Test
     void compareWithMaxPosition() {
-        assertThat(this.carPosition == this.maxPosition).isFalse();
+        assertThat(carPosition == maxPosition).isFalse();
     }
 
 }
