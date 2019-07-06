f3510aeb731903704e868493dec366982a8a4684
choihyunjin <iamchoihyunjin@gmail.com>
2019-07-06 20:06:42 +0900

    refacotr(CarName): extract constant condition bound

--- src/main/java/racing/model/CarCondition.java
+++ src/main/java/racing/model/CarCondition.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class CarCondition {
 
+    private static final int CONDITION_BOUND = 10;
     private static Random random = new Random();
 
     public static int get() {
-        return random.nextInt(10);
+        return random.nextInt(CONDITION_BOUND);
     }
 }
