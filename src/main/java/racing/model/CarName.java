d442667c28c35a362018dfe015ca6cf95604c619
choihyunjin <iamchoihyunjin@gmail.com>
2019-07-06 20:00:00 +0900

    refactor(CarName): add exception message when input invalid car name

--- src/main/java/racing/model/CarName.java
+++ src/main/java/racing/model/CarName.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class CarName {
 
+    private static final String EXCEPTION_MESSAGE_INVALID_NAME = "자동차 이름이 입력되어야 합니다.";
     private String name;
 
     public CarName(String name) {
@@ -13,7 +14,7 @@ public class CarName {
 
     private void validate(String name) {
         if (StringUtils.isEmpty(name))
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_INVALID_NAME);
     }
 
     public String getName() {
