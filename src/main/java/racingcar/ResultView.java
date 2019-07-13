9cd31ab8a66f575786a27a12ab6b480124c1629d
Jason <jason.sohn28@gmail.com>
2019-07-13 11:13:36 +0900

    * [feat] : 경주 우승자 리스트 받았을때 출력결과 print메서드 구현

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -1,19 +1,38 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class ResultView {
 
-    public static void printResult(Map<Integer, List<Car>> gameRecord) {
-        for (int i = 0; i < gameRecord.size(); i++) {
-            for (Car car : gameRecord.get(i)) {
-                for (int j = 0; j < car.getPosition(); j++) {
-                    System.out.print("-");
-                }
-                System.out.println();
-            }
+    private static final String COLON = " : ";
+    private static final String FINAL_MESSAGE = "가 최종 우승했습니다.";
+
+    public static void printGameMessage() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + COLON);
+            printDistance(car);
             System.out.println();
         }
+        System.out.println();
+    }
+
+    public static void printDistance(Car car) {
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
+        }
+    }
+
+    public static void printWinners(List<Car> winners) {
+        ArrayList winnerNames = new ArrayList<>();
+        for (Car car : winners) {
+            winnerNames.add(car.getName());
+        }
+        System.out.print(String.join(", ", winnerNames));
+        System.out.println(FINAL_MESSAGE);
     }
 }
