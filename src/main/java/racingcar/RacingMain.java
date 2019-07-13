01ce6c646c8b6638043627ca560f6567bddc6f48
Jason <jason.sohn28@gmail.com>
2019-07-13 11:14:03 +0900

    * [feat] : 자동차경주 실행하는 main메서드 구현

--- src/main/java/racingcar/RacingMain.java
+++ src/main/java/racingcar/RacingMain.java
@@ -1,23 +1,17 @@
 package racingcar;
 
-import racingcar.InputView;
-import racingcar.RacingGame;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class RacingMain {
-
-    static Map<Integer, List<Car>> gameRecord = new HashMap<>();
-
     public static void main(String[] args) {
-        int numberOfCars = InputView.inputNumberOfCars();
+        String namesOfCars = InputView.inputNamesOfCars();
         int numberOfGames = InputView.inputNumberOfGames();
 
-        RacingGame racingGame = new RacingGame(numberOfCars, numberOfGames);
-        racingGame.carsLineup();
-        gameRecord = racingGame.gameStart();
-        ResultView.printResult(gameRecord);
+        RacingGame racingGame = new RacingGame(namesOfCars, numberOfGames);
+        ResultView.printGameMessage();
+        while (!racingGame.isEnd()) {
+            racingGame.race();
+            ResultView.printResult(racingGame.getCars());
+        }
+        Winners winners = new Winners(racingGame.getCars());
+        ResultView.printWinners(winners.getCars());
     }
 }
