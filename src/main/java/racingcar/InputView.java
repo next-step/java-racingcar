6053408537f3dd4b601f68198f3873f8368b6f95
Jason <jason.sohn28@gmail.com>
2019-07-13 11:10:17 +0900

    * [feat] : 3단계 요구사항에 맞게 INPUT MESSAGE 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,16 +3,17 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
-    static int i = 0;
+    private static final String INPUT_MESSAGE_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String INPUT_MESSAGE_COUNTS = "시도할 횟수는 몇 회 인가요?";
     static Scanner scanner = new Scanner(System.in);
 
-    public static int inputNumberOfCars() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.nextInt();
+    public static String inputNamesOfCars() {
+        System.out.println(INPUT_MESSAGE_NAMES);
+        return scanner.nextLine();
     }
 
     public static int inputNumberOfGames() {
-        System.out.println("시도할 횟수는 몇 회 인가요?");
+        System.out.println(INPUT_MESSAGE_COUNTS);
         return scanner.nextInt();
     }
 }
