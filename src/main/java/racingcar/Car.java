7879f2cefab7cd9c7cd2859bea9153884432f59a
Jason <jason.sohn28@gmail.com>
2019-07-13 11:09:50 +0900

    * [refactor] : 자동차 클래스 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,25 +1,31 @@
 package racingcar;
 
-import java.util.Random;
-
 public class Car {
-
-    private static final int STARTING_POINT = 0;
     private static final int RULE_OF_MOVE = 4;
+    private static final int INIT_POSITION = 0;
 
+    private final String name;
     private int position;
 
-    public Car() {
-        this.position = STARTING_POINT;
+    public Car(final String name) {
+        this(name, INIT_POSITION);
+    }
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
     }
 
     public void move(int randomNumber) {
         if (randomNumber >= RULE_OF_MOVE)
-            this.position += 1;
+            this.position++;
     }
 
     public int getPosition() {
         return this.position;
     }
 
+    public String getName() {
+        return this.name;
+    }
 }
