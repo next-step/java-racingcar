7d78065f315bf310746091f404a3aa46ad893f3e
pobiconan <pobi@woowahan.com>
2019-10-07 10:11:56 +0900

    implement StringCalculator

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,19 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+}
