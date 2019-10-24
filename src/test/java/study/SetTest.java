53a77036859a52f252d3691c22aecdeac3255baa
jahun.kim <jahun.kim@linecorp.com>
2019-10-24 21:46:59 +0900

    test String, Set

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(3);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int no) {
+        assertThat(numbers.contains(no)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 : true", "4 : false"}, delimiter = ':')
+    void contains2(int no, boolean result) {
+        assertThat(numbers.contains(no)).isEqualTo(result);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,19 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    void subString() {
+        String result = "{1,2}".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+}
