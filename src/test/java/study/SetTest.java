0f72452a2114c2939c588ef46b2c4f7bde8bff4d
dave915 <htaehyun@gmail.com>
2019-10-24 21:46:22 +0900

    [test] String, Set

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,50 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 사이즈 구하는 테스트")
+    void size() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int no) {
+        assertThat(numbers.contains(no)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "4:false"}, delimiter = ':')
+    void contains2(int no, boolean result) {
+        assertThat(numbers.contains(no)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  "})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input) {
+        assertThat(input).isBlank();
+    }
+}
