a8c790a09f85bd028a5e963b08676bf85955d964
dave915 <htaehyun@gmail.com>
2019-10-25 08:16:10 +0900

    [feat] 계산 할 수 있는 형태가 아니면 IllegalArgumentException throw

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,9 +5,12 @@ import java.util.Objects;
 public class Calculator {
     private static final String INPUT_IS_NULL_EXCEPTION_MESSAGE = "Input이 Null 입니다.";
     private static final String INPUT_IS_EMPTY_EXCEPTION_MESSAGE = "Input이 공백 입니다.";
+    private static final String CAN_NOT_CALCULATE_STRING_FORMAT = "계산 할 수 없는 문자열 입니다.";
 
     public long calculate(String input) {
         checkInput(input);
+        checkInputFormat(input);
+
         return 0;
     }
 
@@ -19,5 +22,43 @@ public class Calculator {
         if (input.trim().length() == 0) {
             throw new IllegalArgumentException(INPUT_IS_EMPTY_EXCEPTION_MESSAGE);
         }
+    }
+
+    private void checkInputFormat(String input) {
+        String[] strings = input.split(" ");
+        for (int i = 0; i < strings.length; i++) {
+            checkFormat(i, strings[i]);
+        }
+    }
+
+    private void checkFormat(int index, String string) {
+        if (isEven(index)) {
+            checkNumberFormat(string);
+            return;
+        }
+        checkOperator(string);
+    }
+
+    private boolean isEven(int i) {
+        return i % 2 == 0;
+    }
+
+    private void checkNumberFormat(String string) {
+        try {
+            Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(CAN_NOT_CALCULATE_STRING_FORMAT);
+        }
+
+    }
+
+    private void checkOperator(String string) {
+        if (!isSupportOperator(string)) {
+            throw new IllegalArgumentException(CAN_NOT_CALCULATE_STRING_FORMAT);
+        }
+    }
+
+    private boolean isSupportOperator(String string) {
+        return "-+*/".contains(string);
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,21 +1,38 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
 
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("계산기가 입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw하는지 테스트")
     void checkInputNullOrEmptyString(String input) {
-        Calculator calCulator = new Calculator();
         assertThatThrownBy(() -> {
-            calCulator.calculate(input);
+            calculator.calculate(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2 3 4 / 2", "2  3", "/ 2 3"})
+    @DisplayName("숫자 사칙연산기호 숫자 사칙연산기호 숫자 형태가 아니면 IllegalArgumentException throw하는지 테스트")
+    void checkInputFormat(String input) {
+        assertThatThrownBy(() -> {
+            calculator.calculate(input);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
