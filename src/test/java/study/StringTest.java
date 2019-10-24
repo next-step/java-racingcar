4944ff6b8a69f7621285dcda81a3974c196c2a3e
showpointer <wanho92@gmail.com>
2019-10-24 21:46:32 +0900

    test String, Set

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,35 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    void chartAt() {
+        assertThatThrownBy(() -> {
+            "123".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+        /*
+        assertThatThrownBy(() -> {
+            "123".charAt(3);
+        }).isInstanceOf(IllegalArgumentException.class);
+        */
+    }
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("3");
+        assertThat(result).hasSize(2);
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+}
