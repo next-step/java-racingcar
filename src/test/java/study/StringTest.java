e04b501fb0191bdb6f0945c39a11a49b6b841988
seok2 <seogi89@gmail.com>
2019-10-24 21:46:31 +0900

    String

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,27 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    void chatAt() {
+        assertThatThrownBy(() -> "123".charAt(3))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+}
