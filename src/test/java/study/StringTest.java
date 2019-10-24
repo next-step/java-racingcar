a0a3df484b7b0865fb2a57033fc227c22bf0abbd
lannstark <choi-0202@kaist.ac.kr>
2019-10-24 21:50:52 +0900

    학습 테스트, String / Set

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class StringTest {
+
+	@Test
+	void charAt() {
+		assertThatThrownBy(()-> {
+			"123".charAt(3);
+		}).isInstanceOf(StringIndexOutOfBoundsException.class);
+	}
+
+	@Test
+	void split() {
+		String[] result = "1,2".split(",");
+		assertThat(result).hasSize(2);
+		assertThat(result).containsExactly("1", "2");
+	}
+
+	@Test
+	void substring() {
+		String result = "(1,2)".substring(1, 4);
+		assertThat(result).isEqualTo("1,2");
+	}
+
+}
