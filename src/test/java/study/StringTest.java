31fcb2773c817fa4835e9ed102ebce417ca72864
oh29oh29 <hyukjae89@gmail.com>
2019-10-24 21:46:48 +0900

    test String, Set

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,27 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    void charAt() {
+        assertThatThrownBy(() -> {
+            "123".charAt('3');
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+}
