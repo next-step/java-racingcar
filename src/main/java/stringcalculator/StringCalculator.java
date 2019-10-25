59717d186bd366801d4a3c1b3a46653639964312
oh29oh29 <hyukjae89@gmail.com>
2019-10-25 10:58:40 +0900

    [STEP01] 문자열 계산기 - 덧셈 기능

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,7 @@
+package stringcalculator;
+
+public class StringCalculator {
+    public int plus(int operand1, int operand2) {
+        return operand1 + operand2;
+    }
+}
---
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,24 @@
+package stringcalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+         stringCalculator = new StringCalculator();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2"}, delimiter = ',')
+    void plus(int operand1, int operand2) {
+        int result = stringCalculator.plus(operand1, operand2);
+        assertThat(result).isEqualTo(3);
+    }
+}
