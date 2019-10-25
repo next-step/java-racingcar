63f971acb0becf14cef82d5d282dd3bf4c493b1a
liquidjoo <liquidjoo@gmail.com>
2019-10-26 01:06:29 +0900

    String calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        String input = "2 + 3 * 4 / 2";
+        new StringCalculator(input);
+    }
 }
--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
-
 public class StringCalculator {
+    private String[] input;
+
+    public StringCalculator(String input) {
+        inputValidator(input);
+        this.input = input.split(" ");
+        resultPrint();
+    }
+
+    private void inputValidator(String input) {
+        if (input == null || input.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void resultPrint() {
+        System.out.println(calculator(this.input.length - 1));
+    }
+
+    private int calculator(int number) {
+        if (number == 0) {
+            return Integer.parseInt(input[number]);
+        }
+        return branchMethod(number);
+    }
+
+
+    private int branchMethod(int number) {
+        switch (input[number]) {
+            case "+" : return add(number);
+            case "-" : return subtract(number);
+            case "*" : return multiply(number);
+            case "/" : return divide(number);
+            default: return isNumber(number);
+        }
+    }
+
+    private int add(int number) {
+        return calculator(number -1) + Integer.parseInt(input[number + 1]);
+    }
+
+    private int subtract(int number) {
+        return calculator(number -1) - Integer.parseInt(input[number + 1]);
+    }
+
+    private int divide(int number) {
+        return calculator(number -1) / Integer.parseInt(input[number + 1]);
+    }
+
+    private int multiply(int number) {
+        return calculator(number -1) * Integer.parseInt(input[number + 1]);
+    }
+
+    private int isNumber(int number) {
+        try {
+            Integer.parseInt(input[number]);
+            return calculator(number - 1);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
