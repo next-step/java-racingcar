b43890fb180d8e4e494aae2f4cb7d3e454794438
dave915 <40193460+dave915@users.noreply.github.com>
2019-10-25 16:34:33 +0900

    [Step1] 문자열 계산기 구현 (#412)
    * [doc] 문자열 계산기 기능 정의
    
    * [feat] 입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw
    
    * [feat] 계산 할 수 있는 형태가 아니면 IllegalArgumentException throw
    
    * [feat] 덧셈 기능 구현
    
    * [feat] 뺄셈 기능 구현
    
    * [feat] 곱셈 기능 구현
    
    * [feat] 나눗셈 기능 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,93 @@
+package calculator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Calculator {
+    private static final String INPUT_IS_NULL_EXCEPTION_MESSAGE = "Input이 Null 입니다.";
+    private static final String INPUT_IS_EMPTY_EXCEPTION_MESSAGE = "Input이 공백 입니다.";
+    private static final String CAN_NOT_CALCULATE_STRING_FORMAT = "계산 할 수 없는 문자열 입니다.";
+
+    private static final String INPUT_SPLIT_SEPARATOR = " ";
+
+    private static final String PLUS_OPERATOR = "+";
+    private static final String SUBTRACTION_OPERATOR = "-";
+    private static final String MULTIPLICATION_OPERATOR = "*";
+    private static final String DIVISION_OPERATOR = "/";
+
+    private Map<String, OperatorFunction> operators = new HashMap<>();
+
+    public Calculator() {
+        operators.put(PLUS_OPERATOR, Long::sum);
+        operators.put(SUBTRACTION_OPERATOR, (a, b) -> a - b);
+        operators.put(MULTIPLICATION_OPERATOR, (a, b) -> a * b);
+        operators.put(DIVISION_OPERATOR, (a, b) -> a / b);
+    }
+
+    public long result(String input) {
+        checkInput(input);
+
+        String[] strings = input.split(INPUT_SPLIT_SEPARATOR);
+        checkInputFormat(strings);
+        return calculate(strings);
+    }
+
+    private void checkInput(String input) {
+        if (Objects.isNull(input)) {
+            throw new IllegalArgumentException(INPUT_IS_NULL_EXCEPTION_MESSAGE);
+        }
+
+        if (input.trim().length() == 0) {
+            throw new IllegalArgumentException(INPUT_IS_EMPTY_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void checkInputFormat(String[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            checkFormat(i, strings[i]);
+        }
+    }
+
+    private void checkFormat(int index, String string) {
+        if (isEven(index)) {
+            checkNumberFormat(string);
+            return;
+        }
+        checkOperator(string);
+    }
+
+    private boolean isEven(int i) {
+        return i % 2 == 0;
+    }
+
+    private void checkNumberFormat(String string) {
+        try {
+            Long.parseLong(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(CAN_NOT_CALCULATE_STRING_FORMAT);
+        }
+    }
+
+    private void checkOperator(String string) {
+        if (!isSupportOperator(string)) {
+            throw new IllegalArgumentException(CAN_NOT_CALCULATE_STRING_FORMAT);
+        }
+    }
+
+    private boolean isSupportOperator(String string) {
+        return operators.containsKey(string);
+    }
+
+    private long calculate(String[] strings) {
+        long result = Long.parseLong(strings[0]);
+        int lastIndex = !isEven(strings.length) ? strings.length : strings.length - 1;
+
+        for (int i = 1; i < lastIndex; i += 2) {
+            OperatorFunction operatorFunction = operators.get(strings[i]);
+            long nextNumber = Long.parseLong(strings[i + 1]);
+            result = operatorFunction.operate(result, nextNumber);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,74 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("계산기가 입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw하는지 테스트")
+    void checkInputNullOrEmptyString(String input) {
+        assertThatThrownBy(() -> {
+            calculator.result(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2 3 4 / 2", "2  3", "/ 2 3"})
+    @DisplayName("숫자 사칙연산기호 숫자 사칙연산기호 숫자 형태가 아니면 IllegalArgumentException throw하는지 테스트")
+    void checkInputFormat(String input) {
+        assertThatThrownBy(() -> {
+            calculator.result(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 2:3", "3 + 1:4", "3 + 1 +:4"}, delimiter = ':')
+    @DisplayName("덧셈 테스트")
+    void add(String input, int result) {
+        assertThat(calculator.result(input)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2 - 1:1", "3 - 1:2", "3 - 1 -:2"}, delimiter = ':')
+    @DisplayName("뺄셈 테스트")
+    void subtraction(String input, int result) {
+        assertThat(calculator.result(input)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2 * 1:2", "3 * 2:6", "3 * 0:0"}, delimiter = ':')
+    @DisplayName("곱셈 테스트")
+    void multiplication(String input, int result) {
+        assertThat(calculator.result(input)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2 / 1:2", "6 / 2:3"}, delimiter = ':')
+    @DisplayName("나눗셈 테스트")
+    void division(String input, int result) {
+        assertThat(calculator.result(input)).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("문자열 계산기의 동작을 테스트한다.")
+    void calculate() {
+        String input = "2 + 3 * 4 / 2";
+        assertThat(calculator.result(input)).isEqualTo(10);
+    }
+}
