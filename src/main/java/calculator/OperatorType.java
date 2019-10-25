4ffe3a56276c42122ce710032a0f1c3ec5e0a1c6
dellife <seheekwon21@gmail.com>
2019-10-25 22:51:49 +0900

    [feat] 연산자 enum 추가

---
+++ src/main/java/calculator/OperatorType.java
@@ -0,0 +1,37 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+
+public enum OperatorType {
+
+    PLUS("+", Long::sum),
+    MINUS("-", (a, b) -> a - b),
+    MULTIPLY("*", (a, b) -> a * b),
+    DIVISION("/", (a, b) -> a / b);
+
+    private String code;
+    private BiFunction<Long, Long, Long> expression;
+
+    OperatorType(String code, BiFunction<Long, Long, Long> expression) {
+        this.code = code;
+        this.expression = expression;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public BiFunction<Long, Long, Long> getExpression() {
+        return expression;
+    }
+
+    public static List<String> getOperatorCodes() {
+        return Arrays.stream(OperatorType.values())
+                .map(OperatorType::getCode)
+                .collect(Collectors.toList());
+    }
+}
