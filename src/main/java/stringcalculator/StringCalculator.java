bdc434804a6f53991843ec92bc1260fdb8f6b698
chanin <pci2676@gmail.com>
2019-10-25 21:41:58 +0900

    String Calculator - 기능구현1  -. 덧셈, 뺄셈, 곱셈, 나눗셈 기능 구현  -. 없는 연산자 입력시 Exception 발생

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,5 @@
+package stringcalculator;
+
+public class StringCalculator {
+
+}
---
+++ src/main/java/stringcalculator/operation/OperatorGroup.java
@@ -0,0 +1,33 @@
+package stringcalculator.operation;
+
+import java.util.Arrays;
+
+public enum OperatorGroup {
+    PLUS("+", (Double::sum)),
+    MINUS("-", (x, y) -> x - y),
+    MULTIPLY("*", (x, y) -> x * y),
+    MODIFY("/", (x, y) -> x / y);
+
+    private String operator;
+    private OperationInterface operationInterface;
+
+    OperatorGroup(String operator, OperationInterface operationInterface) {
+        this.operator = operator;
+        this.operationInterface = operationInterface;
+    }
+
+    public static OperatorGroup findOperator(String operator) {
+        return Arrays.stream(values())
+                .filter(op -> op.isEqual(operator))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public Double operate(Double x, Double y) {
+        return this.operationInterface.operate(x, y);
+    }
+
+    private boolean isEqual(String operator) {
+        return this.operator.equals(operator);
+    }
+}
