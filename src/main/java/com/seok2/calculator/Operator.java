06e4fb74a08db9333055ff5e5975cd11379e0c1f
seok <seogi89@gmail.com>
2019-10-25 17:33:37 +0900

    <style> 불필요 import 정리

--- src/main/java/com/seok2/calculator/Operator.java
+++ src/main/java/com/seok2/calculator/Operator.java
@@ -1,7 +1,6 @@
 package com.seok2.calculator;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.BiFunction;
--- src/test/java/com/seok2/calculator/StringCalculatorTest.java
+++ src/test/java/com/seok2/calculator/StringCalculatorTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
