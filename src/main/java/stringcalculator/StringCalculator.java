024096598f7cfa6c7455e5975a6c7366a29f48e6
Hyukjae Kwon <hyukjae89@gmail.com>
2019-10-26 01:28:41 +0900

    [STEP01] 문자열 계산기 (#414)
    * [STEP01] 문자열 계산기 - 덧셈 기능
    
    * [STEP01] 문자열 계산기 - 뺄셈 기능
    
    * [STEP01] 문자열 계산기 - 곱셈 기능
    
    * [STEP01] 문자열 계산기 - 나눗셈 기능
    
    * [STEP01] 문자열 계산기 - 입력값 빈문자열 또는 null 체크
    
    * [STEP01] 문자열 계산기 - 연산자 검증
    
    * [STEP01] 문자열 계산기 - 피연산자 검증
    
    * [STEP01] 문자열 계산기 - 검증 리팩토링
    
    * [STEP01] 문자열 계산기 - 누락된 피연산자 검증
    
    * [STEP01] 문자열 계산기 - 테스트 대상 수정
    
    * [STEP01] 문자열 계산기 - 계산 기능
    
    * [STEP01] 문자열 계산기 - 검증 리팩토링
    
    * [STEP01] 문자열 계산기 - 패턴정의 변수를 final로 변경

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,77 @@
+package stringcalculator;
+
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    private final String[] availablePattern = new String[]{"^[0-9]*$", "^(\\+|\\-|\\*|\\/)$"};
+
+    public int plus(int operand1, int operand2) {
+        return operand1 + operand2;
+    }
+
+    public int minus(int operand1, int operand2) {
+        return operand1 - operand2;
+    }
+
+    public int multiply(int operand1, int operand2) {
+        return operand1 * operand2;
+    }
+
+    public int divide(int operand1, int operand2) {
+        return operand1 / operand2;
+    }
+
+    public void validate(String expression) {
+        checkWhetherEmpty(expression);
+
+        String[] seperatedExpression = expression.split(" ");
+
+        if (seperatedExpression.length % 2 == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < seperatedExpression.length; i++) {
+            matches(availablePattern[i % 2], seperatedExpression[i]);
+        }
+    }
+
+    private void checkWhetherEmpty(String expression) {
+        if (expression == null || expression.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void matches(String availablePattern, CharSequence input) {
+        if (!Pattern.matches(availablePattern, input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int calculate(String expression) {
+        String[] seperatedExpression = expression.split(" ");
+
+        int result = Integer.parseInt(seperatedExpression[0]);
+
+        for (int i = 0; i < seperatedExpression.length / 2; i++) {
+            result = operate(result, Integer.parseInt(seperatedExpression[i * 2 + 2]), seperatedExpression[i * 2 + 1]);
+        }
+
+        return result;
+    }
+
+    private int operate(int operand1, int operand2, String operator) {
+        if (operator.equals("+")) {
+            return plus(operand1, operand2);
+        }
+
+        if (operator.equals("-")) {
+            return minus(operand1, operand2);
+        }
+
+        if (operator.equals("*")) {
+            return multiply(operand1, operand2);
+        }
+
+        return divide(operand1, operand2);
+    }
+}
---
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,95 @@
+package stringcalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+         stringCalculator = new StringCalculator();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2"}, delimiter = ',')
+    void plus(int operand1, int operand2) {
+        int result = stringCalculator.plus(operand1, operand2);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 2"}, delimiter = ',')
+    void minus(int operand1, int operand2) {
+        int result = stringCalculator.minus(operand1, operand2);
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2, 3"}, delimiter = ',')
+    void multiply(int operand1, int operand2) {
+        int result = stringCalculator.multiply(operand1, operand2);
+        assertThat(result).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2, 2"}, delimiter = ',')
+    void divide(int operand1, int operand2) {
+        int result = stringCalculator.divide(operand1, operand2);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "      "})
+    void validate_ShouldReturnIllegalArgumentExceptionForEmptyString(String expression) {
+        assertThatThrownBy(() -> {
+            stringCalculator.validate(expression);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void validate_ShouldReturnIllegalArgumentExceptionForNull(String expression) {
+        assertThatThrownBy(() -> {
+            stringCalculator.validate(expression);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"5 ! 4 . 3 & 2 @ 3"})
+    void validate_ShouldReturnIllegalArgumentExceptionForWrongOperator(String expression) {
+        assertThatThrownBy(() -> {
+            stringCalculator.validate(expression);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"5 + 4 - a * 2 / 3"})
+    void validate_ShouldReturnIllegalArgumentExceptionForWrongOperand(String expression) {
+        assertThatThrownBy(() -> {
+            stringCalculator.validate(expression);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"5 + 4 - 3 * 2 / 3 +"})
+    void validate_ShouldReturnIllegalArgumentExceptionForMissingOperand(String expression) {
+        assertThatThrownBy(() -> {
+            stringCalculator.validate(expression);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"5 + 4 - 3 * 2 / 3"})
+    void calculate(String expression) {
+        int result = stringCalculator.calculate(expression);
+        assertThat(result).isEqualTo(4);
+    }
+}
