d42b19b409ccc048f5d32850e701aaf7a1feb6cd
vagabond95 <progjh@gmail.com>
2019-10-26 15:53:45 +0900

    [구현] 수식을 공백문자를 기준으로 나누는 메소드 추가

--- src/main/java/calculator/Expression.java
+++ src/main/java/calculator/Expression.java
@@ -4,11 +4,21 @@ import com.sun.istack.internal.NotNull;
 
 public class Expression {
 
-    private String mRawExpression;
+    private static final String SPACE = " ";
+
     private NumberType mNumberType;
 
     public Expression(@NotNull String rawExpression, NumberType numberType) {
-        this.mRawExpression = rawExpression;
         this.mNumberType = numberType;
+        parse(rawExpression);
+    }
+
+    private void parse(String rawExpression) {
+        String[] splitedExpression = splitFromSpace(rawExpression);
+    }
+
+
+    private String[] splitFromSpace(@NotNull String text) {
+        return text.split(SPACE);
     }
 }
