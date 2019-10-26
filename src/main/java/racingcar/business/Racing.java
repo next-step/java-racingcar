400fd8e4cdba8157285808d206534870e288bda3
oh29oh29 <hyukjae89@gmail.com>
2019-10-27 01:11:21 +0900

    feat 경주 기록 기능 및 입출력 기능

--- src/main/java/racingcar/business/Racing.java
+++ src/main/java/racingcar/business/Racing.java
@@ -8,12 +8,11 @@ import java.util.Map;
 public class Racing {
 
     private final List<Car> cars = new ArrayList<>();
-    private Map<Car, Integer> currentSituation = new HashMap<>();
+    private final Recording recording = new Recording();
     private int countOfMovesAttemps = 0;
 
     public void initialize(int countOfCars, int countOfMovesAttemps) {
         registerCarsToParticipateInRace(countOfCars);
-        updateCurrentSituation();
         setCountOfMovesAttemps(countOfMovesAttemps);
     }
 
@@ -23,23 +22,38 @@ public class Racing {
         }
     }
 
-    private void updateCurrentSituation() {
-        for (Car car : cars) {
-            this.currentSituation.put(car, car.getTotalDistance());
-        }
-    }
-
     private void setCountOfMovesAttemps(int countOfMovesAttemps) {
         this.countOfMovesAttemps = countOfMovesAttemps;
     }
 
-    public void progress() {
-        for (Car car : cars) {
-            car.race();
+    public void start() {
+        for (int i = 0; i < countOfMovesAttemps; i++) {
+            progress();
         }
     }
 
-    public Map<Car, Integer> getCurrentSituation() {
-        return this.currentSituation;
+    private void progress() {
+        cars.forEach(Car::race);
+        recording.saveRacingHistory(cars);
+    }
+
+    public Recording getRecordingData() {
+        return this.recording;
+    }
+
+    public static class Recording {
+        private final List<Map<String, Integer>> racingHistory = new ArrayList<>();
+
+        void saveRacingHistory(List<Car> cars) {
+            Map<String, Integer> history = new HashMap<>();
+
+            cars.forEach(car -> history.put(car.getName(), car.getTotalDistance()));
+
+            racingHistory.add(history);
+        }
+
+        public List<Map<String, Integer>> getRacingHistory() {
+            return this.racingHistory;
+        }
     }
 }
