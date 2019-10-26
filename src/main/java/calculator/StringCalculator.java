ce8aab119f7cd6a2b4afdeca9929c480a90ececc
Kim YoonHo <kimunoya@gmail.com>
2019-10-27 00:10:49 +0900

    매직넘버 제거 - 상수를 활용하여 splitText에 첫 번째 값을 명시적으로 표시

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -10,6 +10,9 @@ import java.util.regex.Pattern;
  * @date : 2019-10-25 18:39
  */
 public class StringCalculator {
+
+    private static final int FIRST_VALUE = 0;
+
     public static double calculate(String inputString) {
         if (inputString == null || inputString.replaceAll(" ", "").isEmpty()) {
             throw new IllegalArgumentException("Invalid Argument");
@@ -26,7 +29,7 @@ public class StringCalculator {
 
     private static final double calculate(String... splitText) {
         final Function<String, Double> stringToInt = str -> Double.parseDouble(str);
-        double resultValue = stringToInt.apply(splitText[0]);
+        double resultValue = stringToInt.apply(splitText[FIRST_VALUE]);
         for (int i = 1; i < splitText.length; i += 2) {
             resultValue = Calculator.findOperator(splitText[i]).calculate(resultValue, stringToInt.apply(splitText[i + 1]));
         }
