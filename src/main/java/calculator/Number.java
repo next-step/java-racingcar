4f6148b3113bd4747614b94bf62e6d2dbcf781cd
vagabond95 <progjh@gmail.com>
2019-10-26 13:42:46 +0900

    [구현] BigInteger 를 value 로 가지도록 구현

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -1,4 +1,16 @@
 package calculator;
 
+import java.math.BigInteger;
+
 public class Number {
+
+    private BigInteger mValue;
+
+    Number(BigInteger value) {
+        mValue = value;
+    }
+
+    public BigInteger getValue() {
+        return mValue;
+    }
 }
