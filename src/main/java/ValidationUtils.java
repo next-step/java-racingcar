437449f219b359c13047f2b80c738f7233c0b11b
vagabond95 <progjh@gmail.com>
2019-10-26 13:34:30 +0900

    [구현] Validation 클래스에 null 검사를 수행하는 메소드 추가

--- src/main/java/ValidationUtils.java
+++ src/main/java/ValidationUtils.java
@@ -1,2 +1,8 @@
 public class ValidationUtils {
+
+    public static void assertNull(Object object) {
+        if (object == null) {
+            throw new NullPointerException();
+        }
+    }
 }
