5f8baf6e5534048fc206575923c11e29c7669231
iltaek <iltaekkwon@gmail.com>
2019-10-27 00:00:06 +0900

    style($feedback): 코딩 컨벤션 반영

--- src/main/java/calc/InputChecker.java
+++ src/main/java/calc/InputChecker.java
@@ -9,6 +9,7 @@ import java.util.stream.IntStream;
 import org.apache.commons.lang3.StringUtils;
 
 public class InputChecker {
+
     private static final String NULL_INPUT = "아무 것도 입력하지 않았습니다.";
     private static final String ILLEGAL_FORMULA = "입력한 식을 다시 한번 확인해주세요.";
     private String[] formula;
@@ -22,31 +23,36 @@ public class InputChecker {
     }
 
     private String[] checkInput(String input) {
-        if (Objects.isNull(input))
+        if (Objects.isNull(input)) {
             throw new IllegalArgumentException(NULL_INPUT);
+        }
 
         String[] formula = input.split(" ");
-        if (isEven(formula.length))
+        if (isEven(formula.length)) {
             throw new IllegalArgumentException(ILLEGAL_FORMULA);
+        }
 
-        if (!isValidFormula(formula))
+        if (!isValidFormula(formula)) {
             throw new IllegalArgumentException(ILLEGAL_FORMULA);
+        }
 
         return formula;
     }
 
     private boolean isValidFormula(String[] formula) {
         // 짝수번째 있는 값이 숫자인가?
-        if ( !IntStream.range(0, formula.length)
+        if (!IntStream.range(0, formula.length)
             .filter(NumberUtils::isEven)
-            .allMatch(i -> StringUtils.isNumeric(formula[i])) )
+            .allMatch(i -> StringUtils.isNumeric(formula[i]))) {
             return false;
+        }
 
         // 홀수번째 있는 값이 operation인가?
-        if ( !IntStream.range(0, formula.length)
+        if (!IntStream.range(0, formula.length)
             .filter(NumberUtils::isOdd)
-            .allMatch(i -> Operators.isOperator(formula[i])) )
+            .allMatch(i -> Operators.isOperator(formula[i]))) {
             return false;
+        }
 
         return true;
     }
