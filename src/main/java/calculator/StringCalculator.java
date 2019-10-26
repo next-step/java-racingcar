702e6f11933b1405fd69621643e17ea32f173614
Kim YoonHo <kimunoya@gmail.com>
2019-10-27 01:51:33 +0900

    - inputString 값에 첫 글자와 마지막 글자가 숫자인지 체크하는 로직 추가 - Empty String, Blank String 값을 상수로 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -12,16 +12,19 @@ import java.util.regex.Pattern;
 public class StringCalculator {
 
     private static final int FIRST_VALUE = 0;
+    private static final Function<String[], Integer> LAST_VALUE_INDEX = str -> str.length - 1;
+    private static final String EMPTY_STRING = "";
+    private static final String BLANK_STRING = " ";
 
     public static double calculate(String inputString) {
-        if (inputString == null || inputString.replaceAll(" ", "").isEmpty()) {
-            throw new IllegalArgumentException("숫자나 오퍼레이터만 넣어주세요.");
+        if (inputString == null || inputString.replaceAll(BLANK_STRING, EMPTY_STRING).isEmpty()) {
+            throw new IllegalArgumentException("null이나 빈 문자열을 입력할 수 없습니다.");
         }
 
         String[] values = inputString.split(" ");
 
         if (!checkValues(values)) {
-            throw new IllegalArgumentException("숫자나 오퍼레이터만 넣어주세요.");
+            throw new IllegalArgumentException("유효하지 않은 입력 값입니다.");
         }
 
         return calculate(values);
@@ -36,6 +39,10 @@ public class StringCalculator {
     }
 
     private static boolean checkValues(String... splitText) {
+        if (!RegxUtils.isNumeric(splitText[FIRST_VALUE]) || !RegxUtils.isNumeric(splitText[LAST_VALUE_INDEX.apply(splitText)])) {
+            return false;
+        }
+
         return Arrays.stream(splitText)
                 .allMatch(value -> RegxUtils.isNumeric(value) || RegxUtils.isOperator(value));
     }
