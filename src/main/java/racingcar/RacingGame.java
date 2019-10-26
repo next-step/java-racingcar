6d07fe33f12ad814244f8830c56066f4ae380209
dellife <seheekwon21@gmail.com>
2019-10-26 16:59:12 +0900

    [feat] 자동차 게임 기능 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,9 +1,12 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class RacingGame {
+
+    private List<Car> cars = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,13 +17,46 @@ public class RacingGame {
         System.out.println("시도할 회수는 몇 회 인가요?");
         int runNumber = scanner.nextInt();
 
-        List<String> result = new RacingGame().run(carNumber, runNumber);
-
+        new RacingGame().run(carNumber, runNumber);
 
     }
 
-    private List<String> run(int carNumber, int runNumber) {
+    private void run(int carNumber, int runNumber) {
 
-        return null;
+        createCars(carNumber);
+
+        for (int i = 0; i < runNumber; i++) {
+            doCycle();
+            printStatus();
+        }
+
+    }
+
+    private void createCars(int carNumber) {
+        for (int i = 0; i < carNumber; i++) {
+            cars.add(new Car());
+        }
+    }
+
+    private void doCycle() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+
+    private void printStatus() {
+        for (Car car : cars) {
+            System.out.println(drawLocation(car.getLocation()));
+        }
+
+        System.out.println();
+    }
+
+    private String drawLocation(int location) {
+        StringBuilder locationBuilder = new StringBuilder();
+        for (int i = 0; i < location; i++) {
+            locationBuilder.append("-");
+        }
+        return locationBuilder.toString();
     }
 }
