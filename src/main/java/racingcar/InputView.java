efc3a7e2c3aefff61be584551bb408d57ed6f5d4
dellife <seheekwon21@gmail.com>
2019-10-26 17:23:15 +0900

    [feat] input view 생성

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private UserInput input;
+    private Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+        this.input = new UserInput(enterCarNumber(), enterRunNumber());
+    }
+
+    private int enterCarNumber() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return scanner.nextInt();
+    }
+
+    private int enterRunNumber() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
+    }
+
+    public UserInput getInput() {
+        return input;
+    }
+}
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,22 +2,16 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class RacingGame {
 
     private List<Car> cars = new ArrayList<>();
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int carNumber = scanner.nextInt();
+        UserInput input = new InputView().getInput();
 
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int runNumber = scanner.nextInt();
-
-        new RacingGame().run(carNumber, runNumber);
+        new RacingGame().run(input.getCarNumber(), input.getRunNumber());
 
     }
 
