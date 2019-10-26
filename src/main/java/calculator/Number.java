634d65e524d97155bf77196a5bb3ac9d37f3fd83
vagabond95 <progjh@gmail.com>
2019-10-26 13:43:26 +0900

    [추가] Number 클래스가 스스로 덧셈을 수행할 수 있는 plus 메소드 추가

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import com.sun.istack.internal.NotNull;
+import com.*;
+
 import java.math.BigInteger;
 
 public class Number {
@@ -8,6 +11,11 @@ public class Number {
 
     Number(BigInteger value) {
         mValue = value;
+    }
+
+    public Number plus(@NotNull Number number) {
+        ValidationUtils.assertNull(number);
+        return new Number(mValue.add(number.mValue));
     }
 
     public BigInteger getValue() {
