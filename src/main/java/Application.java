775f78764b770699478e11b1c7014fd783bc6c42
naheenosaur <log.info.527@gmail.com>
2019-10-26 23:48:12 +0900

    [step1][2] 역할 별 메소드 분리 - Calculator

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,7 @@ import step1.Calculator;
 public class Application {
     public static void main(String[] args){
         String input = "2 + 3 * 4 / 2";
-        System.out.println(Calculator.run(input));
+        int result = new Calculator().run(input);
+        System.out.println(result);
     }
 }
--- src/test/java/step1/CalculatorTest.java
+++ src/test/java/step1/CalculatorTest.java
@@ -1,6 +1,5 @@
 package step1;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -17,17 +16,18 @@ class CalculatorTest {
             "3 + 3 / 2:3"
     }, delimiter = ':')
     void run(String input, Integer result) {
-        assertThat(Calculator.run(input)).isEqualTo(result);
+        assertThat(new Calculator().run(input)).isEqualTo(result);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
             "6 * *",
             "3 + 3 2",
-            "3 & 3"
+            "3 & 3",
+            "  "
     })
     void throwError(String input) {
-        assertThatThrownBy(() -> Calculator.run(input))
+        assertThatThrownBy(() -> new Calculator().run(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
