9fb65470ee5eae87edfb1aacfa9859725beeaf8d
vagabond95 <progjh@gmail.com>
2019-10-26 16:05:22 +0900

    [구현] split 된 연산자를 queue 에 넣는 메소드 추가

--- src/main/java/calculator/Expression.java
+++ src/main/java/calculator/Expression.java
@@ -2,23 +2,37 @@ package calculator;
 
 import com.sun.istack.internal.NotNull;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Expression {
 
     private static final String SPACE = " ";
 
     private NumberType mNumberType;
+    private Queue<String> mElements;
 
     public Expression(@NotNull String rawExpression, NumberType numberType) {
         this.mNumberType = numberType;
+        this.mElements = new LinkedList<>();
         parse(rawExpression);
     }
 
     private void parse(String rawExpression) {
-        String[] splitedExpression = splitFromSpace(rawExpression);
+        String[] splitExpression = splitFromSpace(rawExpression);
+        addToElement(splitExpression, mElements);
     }
-
 
     private String[] splitFromSpace(@NotNull String text) {
         return text.split(SPACE);
+    }
+
+    private void addToElement(String[] splitExpression, Queue<String> elements) {
+        elements.addAll(Arrays.asList(splitExpression));
+    }
+
+    public Queue<String> getElements() {
+        return mElements;
     }
 }
