6804ae3d2b7f8b5838f6ba0238b119bd4be04480
vagabond95 <progjh@gmail.com>
2019-10-26 15:41:38 +0900

    [구현] Expression 클래스가 rawText 와 numberType 을 생성자로 받도록 추가

--- src/main/java/calculator/Expression.java
+++ src/main/java/calculator/Expression.java
@@ -1,5 +1,14 @@
 package calculator;
 
+import com.sun.istack.internal.NotNull;
+
 public class Expression {
 
+    private String mRawExpression;
+    private NumberType mNumberType;
+
+    public Expression(@NotNull String rawExpression, NumberType numberType) {
+        this.mRawExpression = rawExpression;
+        this.mNumberType = numberType;
+    }
 }
