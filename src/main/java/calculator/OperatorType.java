f5839de1050fe72eaf13f721f32e00dce6c77f3d
vagabond95 <progjh@gmail.com>
2019-10-26 17:29:04 +0900

    [구현] operterType 에서 계산로직을 수행하도록 구현

--- src/main/java/calculator/OperatorType.java
+++ src/main/java/calculator/OperatorType.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import com.sun.istack.internal.Nullable;
+
 public enum OperatorType {
     PLUS("+"), MINUS("-"), MULTIPLY("*"), DIVIDE("/");
 
@@ -11,6 +13,29 @@ public enum OperatorType {
 
     public boolean isEqual(String text) {
         return mValue.equals(text);
+    }
+
+    @Nullable
+    public Number calculate(@Nullable Number firstOperand, @Nullable Number secondOperand) {
+        ValidationUtils.assertNull(firstOperand);
+        ValidationUtils.assertNull(secondOperand);
+
+        switch (this) {
+            case PLUS:
+                return firstOperand.plus(secondOperand);
+
+            case MINUS:
+                return firstOperand.minus(secondOperand);
+
+            case MULTIPLY:
+                return firstOperand.multiply(secondOperand);
+
+            case DIVIDE:
+                return firstOperand.divide(secondOperand);
+
+            default:
+                return null;
+        }
     }
 
     public static OperatorType find(String text) {
