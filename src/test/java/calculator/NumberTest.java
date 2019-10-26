71cb53623c628562810e2881ff0ac5e0b169e179
vagabond95 <progjh@gmail.com>
2019-10-26 14:15:38 +0900

    [구현] nullable 한 Number 클래스와 연산시 발생하는 NPE 테스트 코드 작성

--- src/test/java/calculator/NumberTest.java
+++ src/test/java/calculator/NumberTest.java
@@ -1,5 +1,31 @@
 package calculator;
 
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+
 public class NumberTest {
 
+    @Nullable
+    private Number nullableNumber;
+
+    @NotNull
+    private Number one;
+
+    @BeforeEach
+    void setUp() {
+        nullableNumber = null;
+        one = new Number(1);
+    }
+
+    @Test
+    void operatorThrowTest() {
+        assertThatNullPointerException().isThrownBy(() -> one.plus(nullableNumber));
+        assertThatNullPointerException().isThrownBy(() -> one.minus(nullableNumber));
+        assertThatNullPointerException().isThrownBy(() -> one.multiply(nullableNumber));
+        assertThatNullPointerException().isThrownBy(() -> one.divide(nullableNumber));
+    }
 }
