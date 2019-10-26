a9947a93a9435a46280a33c8b56300e7cc549b9a
oh29oh29 <hyukjae89@gmail.com>
2019-10-26 17:30:30 +0900

    [STEP02] 자동차 경주 - 참여할 자동차 등록 / 경주 진행 상황 업데이트 / 경주 진행 상황 확인 기능

--- src/main/java/racingcar/business/Racing.java
+++ src/main/java/racingcar/business/Racing.java
@@ -1,4 +1,30 @@
 package racingcar.business;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Racing {
+
+    private final List<Car> cars = new ArrayList<>();
+    private Map<Car, Integer> currentSituation = new HashMap<>();
+
+    public void registerCarsToParticipateInRace(int carCount) {
+        for (int i = 0; i < carCount; i++) {
+            cars.add(Car.of(CarName.get(i)));
+        }
+
+        updateCurrentSituation();
+    }
+
+    public Map<Car, Integer> getCurrentSituation() {
+        return this.currentSituation;
+    }
+
+    private void updateCurrentSituation() {
+        for (Car car : cars) {
+            this.currentSituation.put(car, car.getTotalDistance());
+        }
+    }
 }
--- src/test/java/racingcar/business/RacingTest.java
+++ src/test/java/racingcar/business/RacingTest.java
@@ -1,6 +1,12 @@
 package racingcar.business;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingTest {
 
@@ -10,4 +16,17 @@ public class RacingTest {
     void setUp() {
         racing = new Racing();
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3})
+    void registerCarsToParticipateInRace_And_getCurrentSituation(int carCount) {
+        racing.registerCarsToParticipateInRace(carCount);
+        Map<Car, Integer> currentSituation = racing.getCurrentSituation();
+
+        // 등록된 자동차 이름 확인
+        assertThat(currentSituation).containsKeys(Car.of(CarName.get(0)), Car.of(CarName.get(1)), Car.of(CarName.get(2)));
+        // 등록된 자동차 이동 거리 확인
+        assertThat(currentSituation).containsValues(0, 0, 0);
+    }
+
 }
