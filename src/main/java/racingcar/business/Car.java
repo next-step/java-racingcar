ef810ecfc2201ffe9b9b843ce31df465d06ef185
oh29oh29 <hyukjae89@gmail.com>
2019-10-26 16:58:36 +0900

    [STEP02] 자동차 경주 - 자동차 이동 및 이동 거리 확인 기능

--- src/main/java/racingcar/business/Car.java
+++ src/main/java/racingcar/business/Car.java
@@ -1,4 +1,23 @@
 package racingcar.business;
 
 public class Car {
+
+    private String name;
+    private int totalDistance = 0;
+
+    private Car(String name) {
+        this.name = name;
+    }
+
+    public static Car of(String name) {
+        return new Car(name);
+    }
+
+    public void move() {
+        this.totalDistance++;
+    }
+
+    public int getTotalDistance() {
+        return this.totalDistance;
+    }
 }
--- src/test/java/racingcar/business/CarTest.java
+++ src/test/java/racingcar/business/CarTest.java
@@ -1,6 +1,10 @@
 package racingcar.business;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
@@ -8,6 +12,16 @@ public class CarTest {
 
     @BeforeEach
     void setUp() {
-        car = new Car();
+        car = Car.of(CarName.get(0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void moveAndGetDistance(int distance) {
+        for (int i = 0; i < distance; i++) {
+            car.move();
+        }
+        int result = car.getTotalDistance();
+        assertThat(result).isEqualTo(distance);
     }
 }
