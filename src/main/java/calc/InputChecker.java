eea99b34da8e683dc53bac81b62dabc6a816472a
iltaek <iltaekkwon@gmail.com>
2019-10-27 10:17:52 +0900

    refactor($feedback): 성능 개선 by regex
    - Input 검증 시 정규표현식 사용
    - 불필요해진 NumberUtils 삭제

--- src/main/java/calc/InputChecker.java
+++ src/main/java/calc/InputChecker.java
@@ -1,18 +1,14 @@
 package calc;
 
-import static calc.utils.NumberUtils.isEven;
-
-import calc.operations.Operators;
-import calc.utils.NumberUtils;
 import java.util.Objects;
-import java.util.stream.IntStream;
-import org.apache.commons.lang3.StringUtils;
+import java.util.regex.Pattern;
 
 public class InputChecker {
 
     private static final String NULL_INPUT = "아무 것도 입력하지 않았습니다.";
     private static final String ILLEGAL_FORMULA = "입력한 식을 다시 한번 확인해주세요.";
     private static final String INPUT_DELIMETER = " ";
+    private static final String INPUT_PATTERN = "^[\\d]+( [+|\\-|*|/] [\\d]+)*";
     private String[] formula;
 
     public InputChecker(String input) {
@@ -28,39 +24,14 @@ public class InputChecker {
             throw new IllegalArgumentException(NULL_INPUT);
         }
 
-        String[] formula = input.split(INPUT_DELIMETER);
-        if (isEven(formula.length)) {
+        if (!isValidFormula(input)) {
             throw new IllegalArgumentException(ILLEGAL_FORMULA);
         }
 
-        if (!isValidFormula(formula)) {
-            throw new IllegalArgumentException(ILLEGAL_FORMULA);
-        }
-
-        return formula;
+        return input.split(INPUT_DELIMETER);
     }
 
-    private boolean isValidFormula(String[] formula) {
-        if (!isEvenOrderNumeric(formula)) {
-            return false;
-        }
-
-        if (!isaOddOrderOperation(formula)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean isaOddOrderOperation(String[] formula) {
-        return IntStream.range(0, formula.length)
-            .filter(NumberUtils::isOdd)
-            .allMatch(i -> Operators.isOperator(formula[i]));
-    }
-
-    private boolean isEvenOrderNumeric(String[] formula) {
-        return IntStream.range(0, formula.length)
-            .filter(NumberUtils::isEven)
-            .allMatch(i -> StringUtils.isNumeric(formula[i]));
+    private boolean isValidFormula(String input) {
+        return Pattern.matches(INPUT_PATTERN, input);
     }
 }
