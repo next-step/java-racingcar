23a8f9a1e7c633d008bce0b09a2c103972a60ec7
naheenosaur <log.info.527@gmail.com>
2019-10-27 18:17:25 +0900

    feat(input view) : 사용자 자동차 수 입력

--- src/main/java/step2/InputView.java
+++ src/main/java/step2/InputView.java
@@ -12,9 +12,11 @@ import java.util.Scanner;
  */
 public class InputView {
     private static Scanner scanner = new Scanner(System.in);
+    private static String CAR_COUNT_QUESTION = "자동차 대수는 몇 대 인가요?";
     private static String TURN_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
 
     public void run() {
+        int num = getAnswerForQuestion(CAR_COUNT_QUESTION);
         int turn = getAnswerForQuestion(TURN_COUNT_QUESTION);
     }
 
