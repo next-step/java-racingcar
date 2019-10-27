5bc55613b6f4d5e69b30e02e5c76aa96721c7c07
kimuno89 <49067099+kimuno89@users.noreply.github.com>
2019-10-27 13:43:41 +0900

    문자열 계산기 피드백 반영했습니다. (#432)
    * enum을 활용한 기본 계산기 구현
    - findOperator 메소드는 유효하지 않은 operator가 들어오면 IllegalArgumentException을 throw 하도록 구현
    
    * input string이 null 이거나 empty string이면 exception throw
    
    Calculator에서 operator를 찾은 후 계산
    
    * 계산기 테스트 코드 구현
    덧셈, 뺄셈, 곱하기, 나눗셈, 잘못된 operator, null string, empty string
    
    * 정규식 유틸 추가
    
    * 정규식을 통한 파라미터 체크 추가
    
    * 매직넘버 제거
    - 상수를 활용하여 splitText에 첫 번째 값을 명시적으로 표시
    
    * Function을 제거하고 Double.parseDouble을 사용하도록 로직을 변경
    
    -> Function을 static 변수로 변경할까 생각도 해봤습니다만 Double.parseDouble의 구현체를 보니 static 메소드로 이미 구현이 되어있어 제거하는게 맞다고 판단하였습니다.
    
    -> 피드백 주신 내용에 Double.parseDouble(str)를 직접 호출하는 것과 stringToInt.apply(str)을 호출하는 것의 차이를 물어보셨는데 아직 명확한 답을 찾지 못하였습니다.. 설명해주시면 감사하겠습니다.
    
    * 불필요 delimiter 제거
    
    * 코드의 일관성 유지를 위해 에러 메시지 한글로 변경
    
    * - inputString 값에 첫 글자와 마지막 글자가 숫자인지 체크하는 로직 추가
    - Empty String, Blank String 값을 상수로 분리
    
    * - Operator 정규식 변경
    
    * RegxUtils 클래스명 변경 (CheckUtil)
    - 입력값을 검증하는 클래스이기 때문에 CheckUtil로 변경

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -10,31 +10,40 @@ import java.util.regex.Pattern;
  * @date : 2019-10-25 18:39
  */
 public class StringCalculator {
-    public static double calculate(String inputString) {
-        if (inputString == null || inputString.replaceAll(" ", "").isEmpty()) {
-            throw new IllegalArgumentException("Invalid Argument");
-        }
 
+    private static final int FIRST_VALUE = 0;
+    private static final Function<String[], Integer> LAST_VALUE_INDEX = str -> str.length - 1;
+    private static final String EMPTY_STRING = "";
+    private static final String BLANK_STRING = " ";
+
+    public static double calculate(String inputString) {
+        if (inputString == null || inputString.replaceAll(BLANK_STRING, EMPTY_STRING).isEmpty()) {
+            throw new IllegalArgumentException("null이나 빈 문자열을 입력할 수 없습니다.");
+        }
+          
         String[] values = inputString.split(" ");
 
         if (!checkValues(values)) {
-            throw new IllegalArgumentException("숫자나 오퍼레이터만 넣어주세요.");
+            throw new IllegalArgumentException("유효하지 않은 입력 값입니다.");
         }
 
         return calculate(values);
     }
 
     private static final double calculate(String... splitText) {
-        final Function<String, Double> stringToInt = str -> Double.parseDouble(str);
-        double resultValue = stringToInt.apply(splitText[0]);
+        double resultValue = Double.parseDouble(splitText[FIRST_VALUE]);
         for (int i = 1; i < splitText.length; i += 2) {
-            resultValue = Calculator.findOperator(splitText[i]).calculate(resultValue, stringToInt.apply(splitText[i + 1]));
+            resultValue = Calculator.findOperator(splitText[i]).calculate(resultValue, Double.parseDouble(splitText[i + 1]));
         }
         return resultValue;
     }
 
-    private static boolean checkValues(String... splitText) {
+     private static boolean checkValues(String... splitText) {
+        if (!CheckUtil.isNumeric(splitText[FIRST_VALUE]) || !CheckUtil.isNumeric(splitText[LAST_VALUE_INDEX.apply(splitText)])) {
+            return false;
+        }
+
         return Arrays.stream(splitText)
-                .allMatch(value -> RegxUtils.isNumeric(value) || RegxUtils.isOperator(value));
+                .allMatch(value -> CheckUtil.isNumeric(value) || CheckUtil.isOperator(value));
     }
 }
