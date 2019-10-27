55ddf7620f9fc4aa5775d294e904f7748475232e
vagabond95 <progjh@gmail.com>
2019-10-27 15:13:36 +0900

    [feat] 실제 랜덤 넘버를 리턴하는 RandomNumberProviderImpl 클래스 추가

---
+++ src/main/java/game/RandomNumberProviderImpl.java
@@ -0,0 +1,19 @@
+package game;
+
+import java.util.Random;
+
+public class RandomNumberProviderImpl implements RandomNumberProvider {
+
+    private Random mRandom;
+    private int mRandomBound;
+
+    public RandomNumberProviderImpl(int randomBound) {
+        this.mRandom = new Random();
+        this.mRandomBound = randomBound;
+    }
+
+    @Override
+    public int getRandomNumber() {
+        return mRandom.nextInt(mRandomBound);
+    }
+}
