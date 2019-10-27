82efee45180294b770a069394fad9cb9eb04d542
vagabond95 <progjh@gmail.com>
2019-10-27 15:01:37 +0900

    [test] RandomMoveRule 테스트 케이스 추가 - Random 값을 테스트하는 것은 결과의 일관성이 없기때문에, 조건의 boundary 값 범위내에서 특정값을 항상 리턴하도록 하여 테스트 진행

---
+++ src/test/java/game/MoveRuleTest.java
@@ -0,0 +1,28 @@
+package game;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoveRuleTest {
+
+    private MoveRule mAlwaysCanMoveRule;
+    private MoveRule mAlwaysCannotMoveRule;
+
+    @BeforeEach
+    void setUp() {
+        RandomNumberProvider alwaysReturnThreeNumberProvider = () -> 3;
+        RandomNumberProvider alwaysReturnFourNumberProvider = () -> 4;
+
+        mAlwaysCanMoveRule = new RandomlyMoveRule(alwaysReturnFourNumberProvider);
+        mAlwaysCannotMoveRule = new RandomlyMoveRule(alwaysReturnThreeNumberProvider);
+    }
+
+    // instance 는 @ParameterizedTest 를 할 수 없는 것일까?
+    @Test
+    void testRandomlyMoveRule() {
+        assertThat(mAlwaysCanMoveRule.canMove()).isTrue();
+        assertThat(mAlwaysCannotMoveRule.canMove()).isFalse();
+    }
+}
