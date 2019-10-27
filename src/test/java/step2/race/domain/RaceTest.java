65413e95bd21646dadc8998589387e6fe64283be
chanin <pci2676@gmail.com>
2019-10-27 10:59:02 +0900

    자동차경주 - 기능구현 2  - 모든 자동차의 전진 멈춤 구현  - 모든 자동차의 현재 위치 알려주기 구현  - 경기 기록 일급 컬렉션 생성  - 경기 진행 하기 구현

---
+++ src/test/java/step2/race/domain/RaceTest.java
@@ -0,0 +1,30 @@
+package step2.race.domain;
+
+import org.junit.jupiter.api.Test;
+import step2.car.domain.car.Car;
+import step2.car.domain.car.engine.Engine;
+import step2.car.domain.car.engine.FixedEngine;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RaceTest {
+    private static final int FIRST_HISTORY = 0;
+
+    @Test
+    void moveAll_모든_자동차_움직이기() {
+        //given
+        Engine engine = new FixedEngine(10);
+        Car car1 = new Car(engine);
+        Car car2 = new Car(engine);
+        Race race = new Race(Arrays.asList(car1, car2));
+
+        //when
+        race.moveAll();
+
+        //then
+        assertThat(race.getRaceHistories().get(FIRST_HISTORY).getHistory()).isEqualTo(Arrays.asList(1, 1));
+    }
+
+}
---
+++ src/test/java/step2/race/service/RaceServiceTest.java
@@ -0,0 +1,48 @@
+package step2.race.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import step2.car.domain.car.Car;
+import step2.car.domain.car.engine.Engine;
+import step2.car.domain.car.engine.RandomEngine;
+import step2.race.domain.Race;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RaceServiceTest {
+
+    private RaceService raceService;
+    private Engine engine;
+
+    @BeforeEach
+    void setUp() {
+        raceService = new RaceService();
+        engine = new RandomEngine();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "1:1", "2:2"}, delimiter = ':')
+    void startRacing_전체_여러번_움직였을때_움직인만큼_경기기록_누적(Integer moveAmount, Integer answer) {
+        //given
+        List<Car> cars = raceService.makeRacingCars(1, engine);
+        Race race = new Race(cars);
+
+        //when
+        raceService.startRacing(race, moveAmount);
+
+        //then
+        assertThat(race.getRaceHistories()).hasSize(answer);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:2", "3:3"}, delimiter = ':')
+    void makeRace_자동차만들기(Integer carAmount, Integer answer) {
+        List<Car> cars = raceService.makeRacingCars(carAmount, engine);
+
+        assertThat(cars).hasSize(answer);
+    }
+
+}
