e9dfcd2895a719ee67b0faee698a627d21c18577
byungkyu-ju <noel1227@gmail.com>
2019-10-27 15:57:32 +0900

    불필요한 공백 제거, 접근제어자 추가

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,11 +1,7 @@
 package calculator;
 
-import java.util.Scanner;
-
 public class Main {
-
   public static void main(String[] args) {
-
     Long result = Calculator.run("2 + 3 * 4 / 2");
     System.out.println(result);
   }
