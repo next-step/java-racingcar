66d19af3df96956d88c9814d7e4d40cc5a640a3b
chanin <pci2676@gmail.com>
2019-10-27 15:18:10 +0900

    자동차경주 - 기능구현 3  - 경기 입 출력 구현

--- src/test/java/step2/race/controller/InputViewTest.java
+++ src/test/java/step2/race/controller/InputViewTest.java
@@ -56,6 +56,17 @@ class InputViewTest {
                 .hasMessage("0보다 많은 수의 자동차가 필요합니다.");
     }
 
+    @Test
+    void inputMoveAmount_최소_시도횟수보다_적게_입력시() {
+        //given
+        ByteArrayInputStream input = new ByteArrayInputStream("0".getBytes());
+        inputView = new InputView(input);
+
+        assertThatThrownBy(() -> inputView.inputMoveAmount())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("0보다 많은 수의 시도 횟수가 필요합니다.");
+    }
+
     @ParameterizedTest
     @CsvSource(value = {"1:1", "2:2", "3:3"}, delimiter = ':')
     void inputMoveAmount(String moveAmount, Integer answer) {
