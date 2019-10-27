fe7f2d2cc0670cfddc7aba96cae018d1f493b0fc
liquidjoo <liquidjoo@gmail.com>
2019-10-27 16:16:56 +0900

    step1 feedback

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import step1.StringCalculator;
+
 public class Main {
 
     public static void main(String[] args) {
         String input = "2 + 3 * 4 / 2";
-        System.out.println(new StringCalculator().execute(input));
+        System.out.println(new StringCalculator(input).execute());
     }
 }
