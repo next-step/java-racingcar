1fe07ea1d0fe4010ea7c7a3153d109b4ec11c395
Kim YoonHo <kimunoya@gmail.com>
2019-10-28 17:46:25 +0900

    feat: 자동차의 go, stop enum 작성
    넘어오는 파라미터 값에 따라 GO, STOP 리턴

---
+++ src/main/java/racing/CarNextStep.java
@@ -0,0 +1,18 @@
+package racing;
+
+import java.util.function.Function;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-10-28 17:16
+ */
+public enum  CarNextStep {
+    GO,
+    STOP;
+
+    static Function<Integer, Boolean> moveStep = value -> value >= 4;
+    public static CarNextStep isMoved(int value) {
+        return moveStep.apply(value) ? GO : STOP;
+    }
+}
