6ef783272b5bcf2097e7cb27bf3cfb75ffcbdbd3
임완호 <wanho.im@kt.com>
2019-10-29 00:43:19 +0900

    test: iterateCarMove, move 테스트 케이스 추가

--- src/test/java/step2/RacingGameTest.java
+++ src/test/java/step2/RacingGameTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
@@ -18,5 +20,17 @@ public class RacingGameTest {
     @CsvSource(value = {"1:1:1", "4:4:5"}, delimiter = ':')
     void moveForwardTest(int random, int before, int after) {
         assertThat(racingGame.moveForward(random, before)).isEqualTo(after);
+    }
+
+    @Test
+    void iterateCarMoveTest() {
+        assertThat(racingGame.iterateCarMove(3)).isBetween(3, 4);
+    }
+
+    @Test
+    void moveTest() {
+        RacingGame racingGameForMoveTest = new RacingGame(3);
+        assertThat(racingGameForMoveTest.move()).hasSize(3);
+        assertThat(racingGameForMoveTest.move()).isInstanceOf(List.class);
     }
 }
