287253d614c68d4e58c13e24eb441ab6ab7440d4
Kim YoonHo <kimunoya@gmail.com>
2019-10-28 20:37:51 +0900

    test: 테스트 코드 작성
    - 자동차 게임에 참가한 자동차 카운트 테스트 추가
    - 자동차가 이동했는지 여부에 대한 테스트 추가

--- src/test/java/racing/RacingGameTest.java
+++ src/test/java/racing/RacingGameTest.java
@@ -18,15 +18,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RacingGameTest {
 
     private final InputStream systemIn = System.in;
+    private RacingGame racingGame;
+    private static final String CAR_AMOUNT = "3";
+    private static final String ROUND_AMOUNT = "5";
+
     @BeforeEach
     void setUp() {
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("3\n5".getBytes());
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(String.format("%s\n%s", CAR_AMOUNT, ROUND_AMOUNT).getBytes());
         System.setIn(byteArrayInputStream);
+        racingGame = new RacingGame(System.in);;
     }
 
     @Test
     void random_int_test() {
-        RacingGame racingGame = new RacingGame(System.in);
         assertThat(racingGame.getRandomIntValue()).isBetween(0, 10);
+    }
+
+    @Test
+    void putParticipantCarMap_게임에_참가하는_자동차_테스트() {
+        assertThat(racingGame.getParticipantCarCount()).isEqualTo(3);
+        assertThat(racingGame.getParticipantCarCount()).isEqualTo(5);
+    }
+
+    @Test
+    void car_move_test() {
+        assertThat(CarNextStep.isMoved(racingGame.getRandomIntValue())).isEqualTo(CarNextStep.GO);
+        assertThat(CarNextStep.isMoved(racingGame.getRandomIntValue())).isEqualTo(CarNextStep.STOP);
     }
 }
