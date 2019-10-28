416bf6df6a046f14d8d8e118e2be31b2920434a0
vagabond95 <progjh@gmail.com>
2019-10-29 00:02:55 +0900

    [test] @MethodSource 를 이용한 MoveRule 테스트 코드 추가

--- src/test/java/project/game/MoveRuleTest.java
+++ src/test/java/project/game/MoveRuleTest.java
@@ -1,7 +1,10 @@
 package project.game;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,9 +23,32 @@ public class MoveRuleTest {
     }
 
     // instance 는 @ParameterizedTest 를 할 수 없는 것일까?
-    @Test
-    void testRandomlyMoveRule() {
-        assertThat(mAlwaysCanMoveRule.canMove()).isTrue();
-        assertThat(mAlwaysCannotMoveRule.canMove()).isFalse();
+    @ParameterizedTest
+    @MethodSource("createAlwaysCanMoveRules")
+    void alwaysCanMoveRuleTest(MoveRule word) {
+        assertThat(word.canMove()).isTrue();
+    }
+
+    private static Stream<MoveRule> createAlwaysCanMoveRules() {
+        return Stream.of(
+                new RandomlyMoveRule(() -> 8),
+                new RandomlyMoveRule(() -> 7),
+                new RandomlyMoveRule(() -> 6),
+                new RandomlyMoveRule(() -> 5),
+                new RandomlyMoveRule(() -> 4));
+    }
+
+    @ParameterizedTest
+    @MethodSource("createAlwaysCannotMoveRules")
+    void alwaysCannotMoveRuleTest(MoveRule word) {
+        assertThat(word.canMove()).isFalse();
+    }
+
+    private static Stream<MoveRule> createAlwaysCannotMoveRules() {
+        return Stream.of(
+                new RandomlyMoveRule(() -> 3),
+                new RandomlyMoveRule(() -> 2),
+                new RandomlyMoveRule(() -> 1),
+                new RandomlyMoveRule(() -> 0));
     }
 }
