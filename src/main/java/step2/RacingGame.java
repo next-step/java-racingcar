a3862f4fc6ad2ff18f80c4f620534dd622fd1d45
임완호 <wanho.im@kt.com>
2019-10-28 22:53:15 +0900

    feat: 4이상일 때 전진

--- src/main/java/step2/RacingGame.java
+++ src/main/java/step2/RacingGame.java
@@ -1,18 +1,44 @@
 package step2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RacingGame {
     private int time;
     private int[] carPositions = {0, 0, 0};
+    private List<Integer> resultPositions;
 
     public RacingGame(int time) {
         this.time = time;
+        resultPositions = new ArrayList<>();
     }
 
-    public void move() {
+    public List<Integer> move() {
         // TODO 구현
-        int randomNumber = generateRandom();
+        int length = carPositions.length;
+        int position;
+        int resultPosition;
+
+        for(int i = 0; i < length; i++) {
+            position = carPositions[i];
+            resultPosition = iterateCarMove(position);
+            resultPositions.add(resultPosition);
+        }
+
+        return resultPositions;
+    }
+
+    public int iterateCarMove(int position) {
+        int randomNumber;
+        int movedPosition = position;
+
+        for(int i = 0; i < time; i++) {
+            randomNumber = generateRandom();
+            movedPosition = moveForward(randomNumber, movedPosition);
+        }
+
+        return movedPosition;
     }
 
     public int generateRandom() {
@@ -20,5 +46,13 @@ public class RacingGame {
         int randomNumber = random.nextInt(10);
 
         return randomNumber;
+    }
+
+    public int moveForward(int number, int position) {
+        if (number >= 4) {
+            position = position + 1;
+        }
+
+        return position;
     }
 }
--- src/test/java/step2/RacingGameTest.java
+++ src/test/java/step2/RacingGameTest.java
@@ -1,6 +1,8 @@
 package step2;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,7 +10,13 @@ public class RacingGameTest {
     RacingGame racingGame = new RacingGame(1);
 
     @Test
-    void calculatorTest() {
+    void generateRandomTest() {
         assertThat(racingGame.generateRandom()).isBetween(0, 9);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:1:1", "4:4:5"}, delimiter = ':')
+    void moveForwardTest(int random, int before, int after) {
+        assertThat(racingGame.moveForward(random, before)).isEqualTo(after);
     }
 }
