6e6b3d09b108d0c13b821ca3fcaed7ce9daee82d
seok <seogi89@gmail.com>
2019-10-28 09:21:27 +0900

    <feat> 참가자 입력 방식 변경

--- src/main/java/com/seok/racing/RacingGameInputView.java
+++ src/main/java/com/seok/racing/RacingGameInputView.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class RacingGameInputView {
 
-    private static final String NUMBER_OF_CARS_MSG = "자동차 대수는 몇 대 인가요?";
+    private static final String NAMES_OF_CARS_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String NUMBER_OF_ATTEMPTS_MSG = "시도할 횟수는 몇 회 인가요 ?";
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int getNumberOfCars() {
-        System.out.println(NUMBER_OF_CARS_MSG);
-        return scanner.nextInt();
+    public static String getNamesOfCars() {
+        System.out.println(NAMES_OF_CARS_MSG);
+        return scanner.nextLine();
     }
 
     public static int getNumberOfAttempts() {
