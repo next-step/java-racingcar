87aa38414cb7ce1141ec02f9c8e35a1d1b099b5d
Kim YoonHo <kimunoya@gmail.com>
2019-10-28 21:15:19 +0900

    test: 테스트 코드 추가
    - 최소값 검증 테스트 추가
    - 잘못된 입력값 검증 테스트 추가

--- src/test/java/racing/RacingGameTest.java
+++ src/test/java/racing/RacingGameTest.java
@@ -7,8 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.InputMismatchException;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * @author : 김윤호
@@ -22,27 +23,45 @@ public class RacingGameTest {
     private static final String CAR_AMOUNT = "3";
     private static final String ROUND_AMOUNT = "5";
 
-    @BeforeEach
-    void setUp() {
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(String.format("%s\n%s", CAR_AMOUNT, ROUND_AMOUNT).getBytes());
+    void setInputAmount(String carAmount, String roundAmount) {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(String.format("%s\n%s", carAmount, roundAmount).getBytes());
         System.setIn(byteArrayInputStream);
-        racingGame = new RacingGame(System.in);;
+        racingGame = new RacingGame(System.in);
     }
 
     @Test
     void random_int_test() {
+        setInputAmount("3", "5");
         assertThat(racingGame.getRandomIntValue()).isBetween(0, 10);
     }
 
     @Test
     void putParticipantCarMap_게임에_참가하는_자동차_테스트() {
+        setInputAmount("3", "5");
         assertThat(racingGame.getParticipantCarCount()).isEqualTo(3);
         assertThat(racingGame.getParticipantCarCount()).isEqualTo(5);
     }
 
     @Test
     void car_move_test() {
+        setInputAmount("3", "5");
         assertThat(CarNextStep.isMoved(racingGame.getRandomIntValue())).isEqualTo(CarNextStep.GO);
         assertThat(CarNextStep.isMoved(racingGame.getRandomIntValue())).isEqualTo(CarNextStep.STOP);
+    }
+
+    @Test
+    void mit_input_value_test() {
+        assertThatExceptionOfType(InputMismatchException.class).isThrownBy(() -> {
+            setInputAmount("0", "5");
+            racingGame.start();
+        }).withMessageMatching("0보다 큰 숫자만 입력이 가능합니다.");
+    }
+
+    @Test
+    void invalid_input_value_test() {
+        assertThatExceptionOfType(InputMismatchException.class).isThrownBy(() -> {
+            setInputAmount("asdfs", "5");
+            racingGame.start();
+        }).withMessageMatching("숫자만 입력 가능합니다.");
     }
 }
