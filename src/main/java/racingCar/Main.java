37682f42fb4ba761aa6fdc6a8eff3a9c4bba01fa
leekyunghee <qkqhlkh@gmail.com>
2019-10-30 00:55:26 +0900

    feat: 경주 시도 횟수를 입력

--- src/main/java/racingCar/Main.java
+++ src/main/java/racingCar/Main.java
@@ -3,5 +3,6 @@ package racingCar;
 public class Main {
     public static void main(String[] args) {
         int cars = InputView.nextInt("자동차 대수는 몇 대 인가요?");
+        int times = InputView.nextInt("시도할 회수는 몇 회 인가요?");
     }
 }
