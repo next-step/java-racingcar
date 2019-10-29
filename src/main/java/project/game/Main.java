a45be973da00528cd257fd7bf899b4b9853ed6dc
vagabond95 <progjh@gmail.com>
2019-10-30 01:51:36 +0900

    [refactor] String 상수를 별도 클래스에서 관리하도록 변경

--- src/main/java/project/game/Main.java
+++ src/main/java/project/game/Main.java
@@ -1,11 +1,9 @@
 package project.game;
 
+import static project.game.StringConstant.*;
+
 public class Main {
-    private static final String RACING_GAME_START_TEXT = "레이싱 시작!";
-    private static final String CAR_COUNT_INPUT_FORM_TEXT = "자동차 대수는 몇 대 인가요?";
-    private static final String ROUND_COUNT_INPUT_FORM_TEXT = "시도할 회수는 몇 회 인가요?";
-    private static final String INPUT_FORMAT_ERROR = "숫자만 입력할 수 있습니다. 다시 입력해주세요. Error : %s";
-    private static final String RACING_GAME_END_TEXT = "레이싱 종료!";
+
 
     public static void main(String[] args) {
         View view = new ConsoleView();
---
+++ src/main/java/project/game/StringConstant.java
@@ -0,0 +1,9 @@
+package project.game;
+
+public class StringConstant {
+    public static final String RACING_GAME_START_TEXT = "레이싱 시작!";
+    public static final String CAR_COUNT_INPUT_FORM_TEXT = "자동차 대수는 몇 대 인가요?";
+    public static final String ROUND_COUNT_INPUT_FORM_TEXT = "시도할 회수는 몇 회 인가요?";
+    public static final String INPUT_FORMAT_ERROR = "숫자만 입력할 수 있습니다. 다시 입력해주세요. Error : %s";
+    public static final String RACING_GAME_END_TEXT = "레이싱 종료!";
+}
