a9300e46f8464db8ecc514152541e108e0f2189b
whlee0525 <whlee0525@gmail.com>
2019-10-29 19:51:00 +0900

    Step1 수정 (#456)
    * step1 calculator start
    
    * step1 try2

--- src/test/java/step1/CalculatorTest.java
+++ src/test/java/step1/CalculatorTest.java
@@ -1,79 +1,81 @@
+/**
+* Step1 계산기 테스트
+* @author whlee0525
+* @version 1.0.0
+* @since 2019-10-29 오후 6:16
+**/
+
 package step1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class CalculatorTest {
+
+    @DisplayName("데이터입력 - isNull, isBlank 테스트")
     @Test
-    void calculateSentenceTest(){
+    void calculateSentenceIsNullTest() {
         Calculator c = new Calculator();
-        assertThat(c.calculateSentence("2 + 3 * 4 / 2")).isEqualTo(10);
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            c.calculateSentence("3 & 8");
+            c.calculateSentence(null);
+        });
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            c.calculateSentence("");
+        });
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            c.calculateSentence(" ");
         });
     }
 
-    /*@Test
-    void makeDataArrTest(){
+    @DisplayName("데이터입력 - isInvalid 테스트")
+    @Test
+    void calculateSentenceIsInvalidTest() {
         Calculator c = new Calculator();
-
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            c.makeDataArr("");
+            c.calculateSentence("3 & 8");
         });
-
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            c.makeDataArr(" ");
+            c.calculateSentence("3 8 7");
         });
-
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            c.makeDataArr("    ");
+            c.calculateSentence("3 *");
         });
+    }
 
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            c.makeDataArr("1 & 2");
-        });
-
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            c.makeDataArr( "* 3 6");
-        });
-
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            c.makeDataArr( "1 * ");
-        });
-
-        assertThat(c.makeDataArr("2 + 3 * 4 / 2")).containsExactly("2","+","3","*","4","/","2");
-    }*/
-
-    /*@Test
-    void calculateCurrTest(){
+    @DisplayName("데이터 연산 - 단일연산")
+    @Test
+    void calculateSentenceSimpleCalTest() {
         Calculator c = new Calculator();
-        String[] dataArr = new String[]{"2","+","3","*","4","/","2"};
-        int result = c.calculateCurr(dataArr, dataArr.length-1);
-        assertThat(result).isEqualTo(10);
-    }*/
+        assertThat(c.calculateSentence("2 + 2")).isEqualTo(4);
+        assertThat(c.calculateSentence("2 - 3")).isEqualTo(-1);
+        assertThat(c.calculateSentence("2 * 4")).isEqualTo(8);
+        assertThat(c.calculateSentence("3 / 2")).isEqualTo(1);
 
-    /*@Test
-    void isOddDataButNotNumberTest(){
-        Calculator c = new Calculator();
-        assertThat(c.isOddDataButNotNumber(new String[]{"1","a","3","a","f"})).isEqualTo(true);
-        assertThat(c.isOddDataButNotNumber(new String[]{"","a","0","a","3"})).isEqualTo(true);
-        assertThat(c.isOddDataButNotNumber(new String[]{"1","a","5","a","5"})).isEqualTo(false);
-    }*/
+    }
 
-   /* @Test
-    void isEvenDataButNotSymbolTest(){
+    @DisplayName("데이터연산 - 다수연산")
+    @Test
+    void calculateSentenceComplexCalTest() {
         Calculator c = new Calculator();
-        assertThat(c.isEvenDataButNotSymbol(new String[]{"1","a","3"})).isEqualTo(true);
-        assertThat(c.isEvenDataButNotSymbol(new String[]{""," ","0"})).isEqualTo(true);
-        assertThat(c.isEvenDataButNotSymbol(new String[]{"","%","0"})).isEqualTo(true);
-        assertThat(c.isEvenDataButNotSymbol(new String[]{"","//","0"})).isEqualTo(true);
-        assertThat(c.isEvenDataButNotSymbol(new String[]{"","++","0"})).isEqualTo(true);
-        assertThat(c.isEvenDataButNotSymbol(new String[]{"1","+",""})).isEqualTo(false);
-        assertThat(c.isEvenDataButNotSymbol(new String[]{"1","-",""})).isEqualTo(false);
-        assertThat(c.isEvenDataButNotSymbol(new String[]{"1","*",""})).isEqualTo(false);
-        assertThat(c.isEvenDataButNotSymbol(new String[]{"1","/",""})).isEqualTo(false);
-    }*/
+        assertThat(c.calculateSentence("2 + 3 - 1")).isEqualTo(4);
+        assertThat(c.calculateSentence("2 + 3 * 4")).isEqualTo(20);
+        assertThat(c.calculateSentence("2 / 3 + 4 / 2")).isEqualTo(2);
+        assertThat(c.calculateSentence("2 + 3 * 4 / 2")).isEqualTo(10);
+    }
+
+    @DisplayName("데이터연산 - 0으로 나누기 연산")
+    @Test
+    void calculateSentenceDivideZeroTest() {
+        Calculator c = new Calculator();
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            c.calculateSentence("3 / 0");
+        });
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            c.calculateSentence("3 + 5 / 0");
+        });
+    }
 
 
 }
