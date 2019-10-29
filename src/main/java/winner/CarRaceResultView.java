0057e76c21928d4b4ed94ad89678677cbda4e7d1
Kim YoonHo <kimunoya@gmail.com>
2019-10-30 02:52:08 +0900

    feat: 출력 클래스 구현

---
+++ src/main/java/winner/CarRaceResultView.java
@@ -0,0 +1,16 @@
+package winner;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-10-29 22:22
+ */
+public class CarRaceResultView {
+    public static void printCurrentCarPosition(Car car) {
+        System.out.println(car.showPosition());
+    }
+
+    public static void printBlankLine() {
+        System.out.println();
+    }
+}
