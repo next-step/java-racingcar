620db99f7342b79b25dbf75e37d4a481196b82af
Kim YoonHo <kimunoya@gmail.com>
2019-10-29 21:49:33 +0900

    refactor: 레이싱게임 코드 분리
    - 하나의 클래스가 많은 역할을 하지않도록 분리
    -

---
+++ src/main/java/racing/CarRace.java
@@ -0,0 +1,36 @@
+package racing;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-10-29 01:51
+ */
+public class CarRace {
+
+    private static final int RANDOM_INT_VALUE_BOUND = 10;
+
+    public void start(Map<Integer, List<CarNextStep>> participant) {
+        playGame(participant, inputGameRound());
+    }
+
+    private int inputGameRound() {
+        RacingGameInputView racingGameInputView = new RacingGameInputView();
+        return racingGameInputView.inputNumberGameRound();
+    }
+
+    private void playGame(Map<Integer, List<CarNextStep>> participant, int gameRound) {
+        for (int round = 0; round < gameRound; round++) {
+            moveCar(participant);
+            RacingGameResultView.printResult(participant);
+        }
+    }
+
+    private void moveCar(Map<Integer, List<CarNextStep>> participant) {
+        participant.values().forEach(steps -> {
+            steps.add(CarNextStep.getGoOrStop(RandomValue.getInt(RANDOM_INT_VALUE_BOUND)));
+        });
+    }
+}
--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -1,12 +1,33 @@
 package racing;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author : 김윤호
  * @version : 1.0
- * @date : 2019-10-28 10:28
+ * @date : 2019-10-29 01:39
  */
-public interface RacingGame {
-    public void start();
-    public int getRandomIntValue();
-    public int getParticipantCarCount();
+public class RacingGame {
+
+    public static void main(String[] args) {
+        startCarRacing();
+    }
+
+    public static void startCarRacing() {
+        RacingGameInputView racingGameInputView = new RacingGameInputView();
+        CarRace carRace = new CarRace();
+        carRace.start(createParticipant(racingGameInputView.inputNumberCars()));
+    }
+
+    private static Map<Integer, List<CarNextStep>> createParticipant(int numberOfCars) {
+        Map<Integer, List<CarNextStep>> participant = new HashMap<>();
+        for (int i = 0; i < numberOfCars; i++) {
+            participant.put(i, new ArrayList<>());
+        }
+
+        return participant;
+    }
 }
