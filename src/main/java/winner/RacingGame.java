df27a5e0506026f655040c2331b1526cba45a1ff
Kim YoonHo <kimunoya@gmail.com>
2019-10-30 02:56:53 +0900

    refactor: InputView에 대한 의존성 제거
    - CarRaceWinnerGame의 inputView에 대한 의존성을 줄이기 위해 RacingGame 클래스 생성

--- src/main/java/winner/RacingGame.java
+++ src/main/java/winner/RacingGame.java
@@ -9,18 +9,8 @@ import java.util.List;
  * @date : 2019-10-29 01:39
  */
 public class RacingGame {
-
     public static void startCarRacingWinner() {
-        CarRaceInputView carRaceInputView = new CarRaceInputView();
-        CarRaceWinnerGame carRaceWinnerGame = new CarRaceWinnerGame(createCars(carRaceInputView.inputCarName()), carRaceInputView.inputGameRound());
+        CarRaceWinnerGame carRaceWinnerGame = new CarRaceWinnerGame(CarRaceInputView.inputCarName(), CarRaceInputView.inputGameRound());
         carRaceWinnerGame.play();
-    }
-
-    private static List<Car> createCars(String[] carNames) {
-        List<Car> cars = new ArrayList<>();
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
-        return cars;
     }
 }
