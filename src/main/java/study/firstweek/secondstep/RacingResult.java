608d533c7736f4233b25001733fb57d6be1bf645
seungeuncho <chose@woowahan.com>
2019-10-29 23:17:22 +0900

    feat create result view for a game

---
+++ src/main/java/study/firstweek/secondstep/RacingResult.java
@@ -0,0 +1,16 @@
+package study.firstweek.secondstep;
+
+import java.util.ArrayList;
+
+public class RacingResult {
+
+    private ArrayList<Integer> results;
+
+    public RacingResult(ArrayList<Integer> results) {
+        this.results = results;
+    }
+
+    public ArrayList<Integer> getResults() {
+        return results;
+    }
+}
