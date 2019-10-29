7933bdb5e9c4eb47ce02897659b20bcf84e27f5b
Kim YoonHo <kimunoya@gmail.com>
2019-10-30 02:55:07 +0900

    refactor: 입력 클래스 패턴 변
    - 기존 생성자를 통해서 inputStream을 입력 받지 않고 상수로 변경
    - 객체의 생성을 피하고 정적 팩토리 메소드를 이용하여 구현

--- src/main/java/winner/CarRaceInputView.java
+++ src/main/java/winner/CarRaceInputView.java
@@ -12,21 +12,12 @@ import java.util.regex.Pattern;
  */
 public class CarRaceInputView {
 
-
     private static final String DELIMITER = ",";
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_GAME_ROUND = "시도할 회수는 몇 회 인가요?";
-    private final Scanner scanner;
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public CarRaceInputView() {
-        this.scanner = new Scanner(System.in);
-    }
-
-    public CarRaceInputView(InputStream inputStream) {
-        this.scanner = new Scanner(inputStream);
-    }
-
-    public String[] inputCarName() {
+    public static String[] inputCarName() {
         System.out.println(INPUT_CAR_NAME);
         final String names = scanner.nextLine();
         if (CheckUtil.stringNullOrEmpty(names)) {
@@ -35,7 +26,7 @@ public class CarRaceInputView {
         return names.split(DELIMITER);
     }
 
-    public int inputGameRound() {
+    public static int inputGameRound() {
         System.out.println(INPUT_GAME_ROUND);
 
         String inputGameRound = scanner.nextLine();
