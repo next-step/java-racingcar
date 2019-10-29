8fa180aae0d10ea7f4c0e2af9ffe3a39ea951468
jahun.kim <jahun.kim@linecorp.com>
2019-10-29 11:04:11 +0900

    feat(view): Add StatusMonitor class

---
+++ src/main/java/step1/racing/StatusMonitor.java
@@ -0,0 +1,14 @@
+package step1.racing;
+
+import java.util.Scanner;
+
+public class StatusMonitor {
+    private final Scanner scanner;
+
+    public StatusMonitor() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void play() {
+    }
+}
