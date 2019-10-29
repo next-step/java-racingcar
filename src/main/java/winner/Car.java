d56cf2b0ee493edf73cc7e357273f0edb7472907
Kim YoonHo <kimunoya@gmail.com>
2019-10-30 00:06:48 +0900

    feat: 자동차 객체 생성

---
+++ src/main/java/winner/Car.java
@@ -0,0 +1,15 @@
+package winner;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-10-29 22:17
+ */
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
