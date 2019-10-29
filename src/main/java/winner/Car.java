3785c2f15d60c99c5279fc6a26269c61fe94f224
Kim YoonHo <kimunoya@gmail.com>
2019-10-30 02:40:43 +0900

    feat: 자동차 객체 기능 추가
    - 자동차를 움직이는 기능 구현
    - 현재의 포지션을 반환하는 기능 구현
    - 연료가 주입되면 자동차가 움직일지 말지 판단하는 기능 구현

--- src/main/java/winner/Car.java
+++ src/main/java/winner/Car.java
@@ -6,10 +6,32 @@ package winner;
  * @date : 2019-10-29 22:17
  */
 public class Car {
+
+    private static final int MINIMUM_FUEL = 4;
+    private static final String POSITION_TEXT = "-";
+    private final StringBuilder POSITION = new StringBuilder();
     private final String name;
     private int position;
+
 
     public Car(String name) {
         this.name = name;
     }
+
+    public void move(int fuel) {
+        if (isMove(fuel)) {
+            position++;
+            POSITION.append(POSITION_TEXT);
+        }
+    }
+
+    public String showPosition() {
+        return String.format("%s : %s", name, POSITION);
+    }
+
+    private boolean isMove(int fuel) {
+        return fuel >= MINIMUM_FUEL;
+    }
+
+
 }
