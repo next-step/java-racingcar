1ececd90a894af5c891da0f43a5fe75f2907f0e8
dave915 <htaehyun@gmail.com>
2019-10-31 00:13:43 +0900

    [refector] 시도 할 횟수에 관한 유저 input 벨리데이션 체크

---
+++ src/test/java/racing/RacingInputTest.java
@@ -0,0 +1,22 @@
+package racing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingInputTest {
+
+    @Test
+    @DisplayName("시도 할 횟수 유저 입력 테스트")
+    void inputTryCount() {
+        String input = "-1\n2";
+        Scanner scanner = new Scanner(new ByteArrayInputStream(input.getBytes()));
+        RacingInput racingInput = new RacingInput(scanner);
+
+        assertThat(racingInput.inputTryCount()).isEqualTo(2);
+    }
+}
