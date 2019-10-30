e106747924dc6bf1645b36fef2688f140cc0af88
dave915 <htaehyun@gmail.com>
2019-10-30 23:12:29 +0900

    [fix] CountGenerator.getRandomInt에 범위(0~9)  설정

--- src/main/java/racing/CountGenerator.java
+++ src/main/java/racing/CountGenerator.java
@@ -3,9 +3,10 @@ package racing;
 import java.util.Random;
 
 public class CountGenerator {
+    private static final int RANDOM_INT_BOUND = 10;
     private static Random random = new Random(System.currentTimeMillis());
 
     public static int getRandomInt() {
-        return random.nextInt();
+        return random.nextInt(RANDOM_INT_BOUND);
     }
 }
