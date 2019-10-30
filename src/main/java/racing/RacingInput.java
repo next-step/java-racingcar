4e3df7e61121e81808f32308f7fa830133528863
dave915 <htaehyun@gmail.com>
2019-10-31 07:37:14 +0900

    [refector] 하드코딩 제거

--- src/main/java/racing/RacingInput.java
+++ src/main/java/racing/RacingInput.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class RacingInput {
     private static final String TRY_COUNT_INPUT_MESSAGE = "시도 할 회수는 몇 회 인가요?";
+    private static final String TRY_COUNT_INPUT_ERROR_MESSAGE = "시도 할 회수는 0 이상이어야 합니다.";
     private static final String RACER_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String RACER_NAMES_INPUT_ERROR_MESSAGE = "자동차 이름은 공백이 될 수 없습니다.";
     private Scanner scanner;
 
     public RacingInput(Scanner scanner) {
@@ -27,7 +29,7 @@ public class RacingInput {
 
     private void checkTryCountInput(int input) {
         if (input < 1) {
-            throw new IllegalArgumentException("시도 할 회수는 0 이상이어야 합니다.");
+            throw new IllegalArgumentException(TRY_COUNT_INPUT_ERROR_MESSAGE);
         }
     }
 
@@ -47,7 +49,7 @@ public class RacingInput {
 
     private void checkRacerNamesInput(String racerNames) {
         if (StringUtils.isBlank(racerNames)) {
-            throw new IllegalArgumentException("자동차 이름은 공백이 될 수 없습니다.");
+            throw new IllegalArgumentException(RACER_NAMES_INPUT_ERROR_MESSAGE);
         }
     }
 }
