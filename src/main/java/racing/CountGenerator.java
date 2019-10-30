fc6c907c2a699d8192f87b842b499eeb7822adae
dave915 <htaehyun@gmail.com>
2019-10-31 07:51:25 +0900

    [test] CountGeneratorTest 추가

--- src/main/java/racing/CountGenerator.java
+++ src/main/java/racing/CountGenerator.java
@@ -3,10 +3,8 @@ package racing;
 import java.util.Random;
 
 public class CountGenerator {
-    private static final int RANDOM_INT_BOUND = 10;
-    private static Random random = new Random(System.currentTimeMillis());
-
-    public static int getRandomInt() {
-        return random.nextInt(RANDOM_INT_BOUND);
+    public static int getRandomInt(int bound) {
+        Random random = new Random(System.currentTimeMillis());
+        return random.nextInt(bound);
     }
 }
