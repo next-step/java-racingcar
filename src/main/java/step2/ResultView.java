464672b0a767f0ecc4ef0de808e7df20b2d38bcf
임완호 <wanho.im@kt.com>
2019-10-30 22:09:44 +0900

    feat: 실행 결과에 자동차 이름 출력

--- src/main/java/step2/ResultView.java
+++ src/main/java/step2/ResultView.java
@@ -1,27 +1,39 @@
 package step2;
 
-import java.util.List;
-
 public class ResultView {
     private static final String POSITION = "-";
 
-    public static void printStartMessage() {
+    public static void printStartMessage(String[] carNames) {
         System.out.println("실행 결과");
+        for (String carName : carNames) {
+            printCarInfo(carName);
+        }
+        newLine();
     }
 
-    public static void print(List<Integer> moveResults) {
-        for (int moveResult : moveResults) {
-            System.out.print(POSITION);
-            iteratePrintDash(moveResult);
+    public static void print(Car[] cars) {
+        for (Car car : cars) {
+            iteratePrint(car);
             newLine();
         }
         newLine();
     }
 
-    private static void iteratePrintDash(int moveResult) {
-        for (int i = 0; i < moveResult; i++) {
+    private static void printCarInfo(String carName) {
+        System.out.println(carName + " : " + "-");
+    }
+
+    private static void iteratePrint(Car car) {
+        int movedPosition = car.getPosition();
+        printCarName(car.getName());
+
+        for (int i = 0; i < movedPosition; i++) {
             System.out.print(POSITION);
         }
+    }
+
+    private static void printCarName(String carName) {
+        System.out.print(carName + " : ");
     }
 
     private static void newLine() {
