21c6ed69517c9020df8e13854575dcdfd652b945
liquidjoo <liquidjoo@gmail.com>
2019-10-30 22:30:23 +0900

    Car Domain Test

--- src/test/java/study/step3/RacingGameTest.java
+++ src/test/java/study/step3/RacingGameTest.java
@@ -13,18 +13,29 @@ public class RacingGameTest {
 
     private static final int TIME = 3;
     private static final String INPUT = "pobi,crong,honux";
+    private static final String TEST_NAME = "car";
     private RacingGame racingGame;
     private Car car;
 
     @BeforeEach
     void setUp() {
         racingGame = new RacingGame(TIME, INPUT);
-        car = new Car("tom");
+        car = new Car(TEST_NAME);
     }
 
     @Test
     void domainRacingGameCreateCarObjectTest() {
         assertThat(racingGame.initiateCar(INPUT)).hasSize(3);
+    }
+
+    @Test
+    void domainCarInitiatePositionTest() {
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void domainCarNameTest() {
+        assertThat(car.getName()).isEqualTo(TEST_NAME);
     }
 
     @Test
