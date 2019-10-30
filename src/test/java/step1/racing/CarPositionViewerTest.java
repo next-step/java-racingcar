0bef14f4fced0b559a55cb28454c1bb66797bccc
jahun.kim <jahun.kim@linecorp.com>
2019-10-31 01:51:18 +0900

    test(position viewer): Add car position rendering test code

---
+++ src/test/java/step1/racing/CarPositionViewerTest.java
@@ -0,0 +1,24 @@
+package step1.racing;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class CarPositionViewerTest {
+    @ParameterizedTest
+    @MethodSource("carPositionsAndFlag")
+    void rendering_test(List<Integer> carPositions, String flag) {
+        CarPositionViewer.rendering(carPositions, flag);
+    }
+
+    private static Stream<Arguments> carPositionsAndFlag() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(3, 4, 5), "&"),
+                Arguments.of(Arrays.asList(3, 4, 5, 1, 2), "@")
+        );
+    }
+}
