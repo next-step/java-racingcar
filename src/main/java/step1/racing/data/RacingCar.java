65c003138c96d84254f62a060dcfebe23af92e49
jahun.kim <jahun.kim@linecorp.com>
2019-10-31 19:45:44 +0900

    feat(feature): Add RacingCar class handling position and name

---
+++ src/main/java/step1/racing/data/RacingCar.java
@@ -0,0 +1,32 @@
+package step1.racing.data;
+
+import java.util.stream.IntStream;
+
+public class RacingCar {
+    private final String name;
+    private int position;
+
+    public RacingCar(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void moveForward() {
+        position++;
+    }
+
+    public String printStatus(String flag) {
+        String status = IntStream.range(0, position)
+                                 .mapToObj(pos -> flag)
+                                 .reduce(String::concat).get();
+
+        return name + " : " + status;
+    }
+}
