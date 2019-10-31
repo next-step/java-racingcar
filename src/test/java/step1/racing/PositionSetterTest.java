281565eff5c67a879d2a219c02630c084fe15e0b
jahun.kim <jahun.kim@linecorp.com>
2019-10-31 19:36:43 +0900

    fix(test code): Fix parameter type to match origin code

--- src/test/java/step1/racing/PositionSetterTest.java
+++ src/test/java/step1/racing/PositionSetterTest.java
@@ -5,9 +5,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import step1.racing.data.RacingCar;
 
 class PositionSetterTest {
     private Evaluator<Integer> evaluator = new IntegerEvaluator();
@@ -16,28 +21,32 @@ class PositionSetterTest {
     @ValueSource(strings = { "3", "4", "5" })
     void throw_exception_if_different_args_list_size(String threshold) {
         PositionSetter<Integer> sut = new PositionSetter(evaluator, threshold);
-        List<Integer> targetList = Arrays.asList(1, 2, 3);
+        List<RacingCar> targetList = Arrays.asList(new RacingCar("a"), new RacingCar("b"));
         List<Integer> evalList = Arrays.asList(3);
 
-        assertThatThrownBy(() -> sut.movePosition(targetList, evalList)).isInstanceOf(IndexOutOfBoundsException.class);
+        assertThatThrownBy(() -> sut.movePosition(targetList, evalList)).isInstanceOf(
+                IndexOutOfBoundsException.class);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "-1"})
-    void check_position_actually_moved(String threshold) {
-        PositionSetter<Integer> sut = new PositionSetter(evaluator, threshold);
-        List<Integer> targetList = Arrays.asList(0, 0, 0);
+    @MethodSource("provideTestArgs")
+    void check_position_actually_moved(List<RacingCar> lists) {
+        PositionSetter<Integer> sut = new PositionSetter(evaluator, "-1");
         List<Integer> evalList = Arrays.asList(3, 3, 3);
 
-        targetList = sut.movePosition(targetList, evalList);
-        targetList = sut.movePosition(targetList, evalList);
-        targetList = sut.movePosition(targetList, evalList);
-        targetList = sut.movePosition(targetList, evalList);
-        targetList = sut.movePosition(targetList, evalList);
+        lists = sut.movePosition(lists, evalList);
+        lists = sut.movePosition(lists, evalList);
+        lists = sut.movePosition(lists, evalList);
+        lists = sut.movePosition(lists, evalList);
+        lists = sut.movePosition(lists, evalList);
 
-        assertThat(targetList.get(0)).isEqualTo(5);
-        assertThat(sut.movePosition(targetList, evalList).get(0)).isEqualTo(6);
+        assertThat(lists.get(0).getPosition()).isEqualTo(5);
+        assertThat(sut.movePosition(lists, evalList).get(0).getPosition()).isEqualTo(6);
     }
 
-
+    private static Stream<Arguments> provideTestArgs() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(new RacingCar("test")))
+        );
+    }
 }
