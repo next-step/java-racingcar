785ace1268b03c4e10eb3f71904b6f7a248b0a83
jahun.kim <jahun.kim@linecorp.com>
2019-10-31 22:12:33 +0900

    feat(feature): Add generator and compare position method

--- src/main/java/step1/racing/data/RacingCar.java
+++ src/main/java/step1/racing/data/RacingCar.java
@@ -8,6 +8,12 @@ public class RacingCar {
 
     public RacingCar(String name) {
         this.name = name;
+        this.position = 1;
+    }
+
+    public RacingCar(String name, int position) {
+        this.name = name;
+        this.position = position > 0 ? position : 1;
     }
 
     public String getName() {
@@ -16,6 +22,10 @@ public class RacingCar {
 
     public int getPosition() {
         return position;
+    }
+
+    public boolean isGreaterThan(int position) {
+        return this.position >= position;
     }
 
     public void moveForward() {
