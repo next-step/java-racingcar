75a2b4e7a50928caee33e2f7255aac319fc7d33b
jahun.kim <jahun.kim@linecorp.com>
2019-10-31 15:52:27 +0900

    feat(feature): Add PositionSetter class returns reset position lists evaluated by evaluator

---
+++ src/main/java/step1/racing/PositionSetter.java
@@ -0,0 +1,29 @@
+package step1.racing;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class PositionSetter<T> {
+    private final Evaluator<T> evaluator;
+    private final T threshold;
+
+    public PositionSetter(Evaluator<T> evaluator, T threshold) {
+        this.evaluator = evaluator;
+        this.threshold = threshold;
+    }
+
+    public List<Integer> movePosition(List<Integer> target, List<T> evaluationFactors) {
+        return IntStream.range(0, target.size())
+                 .map(index -> updatePositionIfWon(target, evaluationFactors, index))
+                 .boxed()
+                 .collect(Collectors.toList());
+    }
+
+    private int updatePositionIfWon(List<Integer> target, List<T> evaluationFactors, int index) {
+        if (evaluator.evaluate(evaluationFactors.get(index), threshold)) {
+            return target.get(index) + 1;
+        }
+        return target.get(index);
+    }
+}
