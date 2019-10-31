1d4b40ae241f13b3421c04ae6856d03af1fa17a7
Jaeeun Na <lette1394@gmail.com>
2019-10-31 20:39:21 +0900

    refactoring move get winner logic to GameResult

--- src/main/java/step2/view/format/GameResultNamedWinnerStringFormatter.java
+++ src/main/java/step2/view/format/GameResultNamedWinnerStringFormatter.java
@@ -1,16 +1,9 @@
 package step2.view.format;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import step2.domain.GameResult;
 import step2.domain.Car;
-
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
+import step2.domain.GameResult;
 
 public class GameResultNamedWinnerStringFormatter extends GameResultNamedStringFormatter {
     private static final String WINNER_MESSAGE = "이(가) 최종 우승했습니다.";
@@ -31,18 +24,8 @@ public class GameResultNamedWinnerStringFormatter extends GameResultNamedStringF
     }
 
     private String formatWinner(final GameResult gameResult) {
-        List<Car> winnerCars = gameResult.getPhase(gameResult.getNumberOfPhases() - 1)
-                                         .getAllCars()
-                                         .stream()
-                                         .collect(toMap(Car::current,
-                                                        Arrays::asList,
-                                                        (l1, l2) -> Stream.concat(l1.stream(), l2.stream())
-                                                                          .collect(toList()),
-                                                        TreeMap::new))
-                                         .lastEntry()
-                                         .getValue();
-
-        return winnerCars.stream()
+        return gameResult.getWinners()
+                         .stream()
                          .map(Car::getName)
                          .collect(Collectors.joining(", "));
     }
