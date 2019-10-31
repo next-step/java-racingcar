75884cfed1fd18bda4b0e7cb3f912dbed57da6b0
jahun.kim <jahun.kim@linecorp.com>
2019-10-31 17:26:00 +0900

    fix(): Fix main method call step1_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
 import step1.calculator.StringCalculator;
+import step1.racing.ReadyStage;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Not found arguments");
-        }
-        String type = args[0];
-        String equation = args[1];
-
-        System.out.println(new StringCalculator<>(type).calculate(equation));
+        ReadyStage readyStage = new ReadyStage();
+        readyStage.play();
     }
 }
