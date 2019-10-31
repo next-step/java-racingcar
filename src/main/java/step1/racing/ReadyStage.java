09706d313885c32dd49a912e0ff1920ec525a090
jahun.kim <jahun.kim@linecorp.com>
2019-10-31 19:48:56 +0900

    fix(init values): Fix init values and methods

--- src/main/java/step1/racing/ReadyStage.java
+++ src/main/java/step1/racing/ReadyStage.java
@@ -1,22 +1,27 @@
 package step1.racing;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import step1.racing.data.RacingCar;
+import step1.racing.factories.EvaluatorFactory;
+import step1.racing.factories.RandomListGeneratorFactory;
 
 public class ReadyStage {
     private static final Pattern CONTAINS_DOT = Pattern.compile("^-?\\d+\\.\\d+");
+    private static final String SPLIT_DELIMITER = ",";
 
     private final Scanner scanner;
 
     private RandomListGenerator randomListGenerator;
     private PositionSetter positionSetter;
+    private List<RacingCar> carPositions;
 
-    private int players;
     private int playTimes;
     private int boundary;
-
-    private String winningThreshold;
-    private Class type;
 
     public ReadyStage() {
         this.scanner = new Scanner(System.in);
@@ -24,17 +29,18 @@ public class ReadyStage {
     }
 
     public void play() {
-        RacingGame racingGame = new RacingGame(randomListGenerator, positionSetter, players, playTimes, boundary);
+        RacingGame racingGame = new RacingGame(randomListGenerator, positionSetter, carPositions, playTimes, boundary);
         racingGame.move();
     }
 
     private void initValues() {
-        players = scanner.nextInt();
+        String players = scanner.nextLine();
         playTimes = scanner.nextInt();
         boundary = scanner.nextInt();
-        winningThreshold = scanner.next();
-        type = resolveType(winningThreshold);
+        String winningThreshold = scanner.next();
+        Class type = resolveType(winningThreshold);
 
+        carPositions = initCarPositions(players);
         randomListGenerator = RandomListGeneratorFactory.getRandomListGenerator(type);
         positionSetter = new PositionSetter(EvaluatorFactory.getEvaluator(type), winningThreshold);
     }
@@ -45,5 +51,11 @@ public class ReadyStage {
         }
 
         return Integer.class;
+    }
+
+    private List<RacingCar> initCarPositions(String players) {
+        return Arrays.stream(players.split(SPLIT_DELIMITER))
+              .map(RacingCar::new)
+              .collect(Collectors.toList());
     }
 }
