630bed87f7fbc7d26f4db8fa38d1cc922999e4d4
Kim YoonHo <kimunoya@gmail.com>
2019-11-02 01:31:26 +0900

    test: RacingCarTest 코드 추가

---
+++ src/test/java/winner/domain/RacingCarsTest.java
@@ -0,0 +1,35 @@
+package winner.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-01 22:53
+ */
+public class RacingCarsTest {
+
+    RacingCars racingCars;
+    @BeforeEach
+    void setUp() {
+        racingCars = new RacingCars(Arrays.asList(new Car("pobi", 3), new Car("kim", 5)));
+
+    }
+
+    @Test
+    void 레이싱카_객체_생성() {
+        assertThat(racingCars).isEqualTo(new RacingCars(Arrays.asList(new Car("pobi", 3), new Car("kim", 5))));
+    }
+
+    @Test
+    void 우승자_찾기() {
+        List<String> result = racingCars.findWinner();
+        assertThat(result.get(0)).isEqualTo("kim");
+    }
+}
