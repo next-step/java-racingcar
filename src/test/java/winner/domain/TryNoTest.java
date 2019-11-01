5097cb158afb10d6fa46add46c19617604aefe03
Kim YoonHo <kimunoya@gmail.com>
2019-11-02 00:31:48 +0900

    test: TryNo 테스트 코드 추가

---
+++ src/test/java/winner/domain/TryNoTest.java
@@ -0,0 +1,32 @@
+package winner.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-01 14:19
+ */
+public class TryNoTest {@Test
+void tryNo_객체_생성() {
+    assertThat(new TryNo(3)).isEqualTo(new TryNo(3));
+}
+
+    @Test
+    void tryNo_is_end_test() {
+        TryNo tryNo = new TryNo(1);
+        tryNo.decrease();
+        assertThat(tryNo.isEnd()).isTrue();
+    }
+
+    @Test
+    void try_no_0_입력() {
+        assertThatThrownBy(() -> {
+            TryNo tryNo = new TryNo(0);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
