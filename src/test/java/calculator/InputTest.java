0fa58da41fa0ecab126d297e79b99623c71d8bc4
cwpark723 <chiwon7942@naver.com>
2019-11-01 15:40:59 +0900

    Step1 피드백 반영2

--- src/test/java/calculator/InputTest.java
+++ src/test/java/calculator/InputTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -19,25 +20,22 @@ public class InputTest {
 
     @ParameterizedTest
     @NullSource
+    @DisplayName("null 입력 시 확인")
     void nullTest(String inputStr) {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            inputValidator.valueToCalulateValidate(inputStr);
-        });
+        assertThatIllegalArgumentException().isThrownBy(() -> inputValidator.valueToCalulateValidate(inputStr));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "    "})
+    @DisplayName("공백 문자 입력 시 확인")
     void emptyStringTest(String inputStr) {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-           inputValidator.valueToCalulateValidate(inputStr);
-        });
+        assertThatIllegalArgumentException().isThrownBy(() -> inputValidator.valueToCalulateValidate(inputStr));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"@", "$", "%"})
+    @DisplayName("사칙연산 기호 정상여부 확인")
     void calcStringTest(String inputStr) {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-           inputValidator.operationSymbolValidate(inputStr);
-        });
+        assertThatIllegalArgumentException().isThrownBy(() -> inputValidator.operationSymbolValidate(inputStr));
     }
 }
