269a7219ea4cee4cb96ba9597fb232e528dfcf79
Kim YoonHo <kimunoya@gmail.com>
2019-11-01 22:46:03 +0900

    feat: 자동차 이동 패턴 추가

---
+++ src/main/java/winner/domain/CarMoveStrategy.java
@@ -0,0 +1,20 @@
+package winner.domain;
+
+import java.util.Random;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-01 21:27
+ */
+public class CarMoveStrategy implements MovableStrategy {
+
+    public final static Random random = new Random();
+    public final static int MAXIMUM_BOUND = 10;
+    public final static int MINIMUM_CAR_MOVE = 4;
+
+    @Override
+    public boolean isMove() {
+        return random.nextInt(MAXIMUM_BOUND) <= MINIMUM_CAR_MOVE;
+    }
+}
---
+++ src/main/java/winner/domain/MovableStrategy.java
@@ -0,0 +1,10 @@
+package winner.domain;
+
+/**
+ * @author : 김윤호
+ * @version : 1.0
+ * @date : 2019-11-01 21:26
+ */
+public interface MovableStrategy {
+    public boolean isMove();
+}
