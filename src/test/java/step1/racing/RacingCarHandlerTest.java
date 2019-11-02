15a9b2c6321c31b52b4cfd4417ca33ff156e8686
jahun.kim <jahun.kim@linecorp.com>
2019-11-02 16:08:05 +0900

    test(): Add RacingCarHandler race test code

--- src/test/java/step1/racing/RacingCarHandlerTest.java
+++ src/test/java/step1/racing/RacingCarHandlerTest.java
@@ -14,7 +14,7 @@ class RacingCarHandlerTest {
 
     @Test
     void throw_exception_if_empty_args() {
-        assertThatThrownBy(() -> new RacingCarHandler(Collections.EMPTY_LIST))
+        assertThatThrownBy(() -> new RacingCarHandler(Collections.EMPTY_LIST, 1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -23,7 +23,18 @@ class RacingCarHandlerTest {
         RacingCar c1 = new RacingCar("c1", 3);
         RacingCar c2 = new RacingCar("c2", 2);
 
-        RacingCarHandler sut = new RacingCarHandler(Arrays.asList(c1, c2));
+        RacingCarHandler sut = new RacingCarHandler(Arrays.asList(c1, c2), 1);
+
         assertThat(sut.getWinners()).contains(c1);
+    }
+
+    @Test
+    void check_car_position_moved_when_race() {
+        RacingCar c1 = new RacingCar("c1", 1);
+        RacingCarHandler sut = new RacingCarHandler(Arrays.asList(c1), 1);
+
+        sut.race(() -> true);
+
+        assertThat(c1.getPosition()).isEqualTo(2);
     }
 }
