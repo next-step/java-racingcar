4c1f1c3a50c0a506f563011f37c321456114d304
jahun.kim <jahun.kim@linecorp.com>
2019-11-02 18:59:56 +0900

    feat(Parse): Add check input string and delimiter matches pattern

--- src/main/java/step2/StringCalculator/Parser.java
+++ src/main/java/step2/StringCalculator/Parser.java
@@ -1,16 +1,38 @@
 package step2.StringCalculator;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Parser {
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("^//.+\\\\n");
+    private static final String DEFAULT_DELIMITER = "\\:|\\,";
+    private static final String CUSTOM_PARSER_END_STRING = "\\n";
+    private static final String EMPTY_STRING = "";
+    private static final int CUSTOM_PARSER_STARTING_INDEX = 2;
+
+    private final Matcher matcher;
     private final String rawInput;
+    private final String delimiter;
+
     public Parser(String rawInput) {
+        this.matcher = CUSTOM_DELIMITER_PATTERN.matcher(rawInput);
         this.rawInput = rawInput;
     }
 
-    public List<String> parse(String delimiter) {
-        return Arrays.asList(rawInput.split(delimiter));
+    private String getInputString() {
+        if (matcher.find(0)) {
+            return matcher.replaceAll(EMPTY_STRING);
+        }
+        return rawInput;
+    }
+
+    private String getDelimiter() {
+        if (matcher.find(0)) {
+            return rawInput.substring(CUSTOM_PARSER_STARTING_INDEX, rawInput.indexOf(CUSTOM_PARSER_END_STRING));
+        }
+        return DEFAULT_DELIMITER;
     }
 }
