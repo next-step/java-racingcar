f38c7af367c70df473a3b1e155ca0d614f9b1b59
jahun.kim <jahun.kim@linecorp.com>
2019-11-02 16:03:20 +0900

    fix(remove classes): Remove unused classes

--- src/main/java/step1/racing/Evaluator.java
+++
@@ -1,6 +0,0 @@
-package step1.racing;
-
-public interface Evaluator<T extends Number> {
-    boolean evaluate(T evaluateFactor, T threshold);
-    T parseValue(String value);
-}
--- src/main/java/step1/racing/RandomListGenerator.java
+++
@@ -1,7 +0,0 @@
-package step1.racing;
-
-import java.util.List;
-
-public interface RandomListGenerator<T extends Number> {
-    List<T> gacha(int size, int bound);
-}
