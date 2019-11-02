cd0ad2e48adb74cc0d378a1eb455638cc5d78a00
jahun.kim <jahun.kim@linecorp.com>
2019-11-03 02:47:08 +0900

    feat(RacingCars): Add RacingCars class to handle racing car list as separate class

---
+++ src/main/java/step1/racing/data/RacingCars.java
@@ -0,0 +1,50 @@
+package step1.racing.data;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import step1.racing.Moveable;
+import step1.racing.data.RacingCar;
+
+public class RacingCars {
+    private final List<RacingCar> racingCars;
+
+    public RacingCars(List<RacingCar> racingCars) {
+        this.racingCars = racingCars;
+        validate();
+    }
+
+    public void race(Moveable moveStrategy) {
+        for (RacingCar racingCar : racingCars) {
+            moveForwardIfMoveable(moveStrategy, racingCar);
+        }
+    }
+
+    public List<RacingCar> getWinners() {
+        int max = racingCars.stream()
+                            .map(RacingCar::getPosition)
+                            .max(Integer::compareTo)
+                            .orElse(Integer.MAX_VALUE);
+
+        return racingCars.stream()
+                         .filter(car -> car.isGreaterThan(max))
+                         .collect(Collectors.toList());
+    }
+
+    public List<RacingCar> getRacingCars() {
+        return Collections.unmodifiableList(racingCars);
+    }
+
+    private void moveForwardIfMoveable(Moveable moveStrategy, RacingCar racingCar) {
+        if (moveStrategy.isMoveable()) {
+            racingCar.moveForward();
+        }
+    }
+
+    private void validate() {
+        if (racingCars.size() < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
