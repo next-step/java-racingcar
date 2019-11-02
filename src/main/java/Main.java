ca1f9a9d75468af353172f5aeabfaeba66a49668
jahun.kim <jahun.kim@linecorp.com>
2019-11-02 20:13:59 +0900

    fix(main): Fix main method execute IntegerAdder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import step1.calculator.StringCalculator;
 import step1.racing.ReadyStage;
+import step2.StringCalculator.IntegerAdder;
 
 public class Main {
     public static void main(String[] args) {
-        ReadyStage readyStage = new ReadyStage();
-        readyStage.play();
+        String equation = "//;\\n1;2;3";
+        IntegerAdder adder = new IntegerAdder(equation);
+        System.out.println(adder.calculate());
     }
 }
