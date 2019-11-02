5d2fd8df9bd37ceaf40a3f94865346cfea95975b
jahun.kim <jahun.kim@linecorp.com>
2019-11-02 16:09:33 +0900

    fix(RacingGame): Fix RacingGame class role just call move with flexible move strategy

--- src/main/java/step1/racing/RacingGame.java
+++ src/main/java/step1/racing/RacingGame.java
@@ -1,30 +1,15 @@
 package step1.racing;
 
-import java.util.List;
-import java.util.stream.IntStream;
-
 public class RacingGame {
-    private final RandomListGenerator randomListGenerator;
-    private final PositionSetter positionSetter;
-    private final RacingCarHandler carPositions;
-    private final int playTimes;
-    private final int boundary;
+    private final Moveable moveStrategy;
+    private final RacingCarHandler racingCarHandler;
 
-
-    public RacingGame(RandomListGenerator randomListGenerator, PositionSetter positionSetter, RacingCarHandler carPositions, int playTimes, int boundary) {
-        this.randomListGenerator = randomListGenerator;
-        this.positionSetter = positionSetter;
-        this.carPositions = carPositions;
-        this.playTimes = playTimes;
-        this.boundary = boundary;
+    public RacingGame(RacingCarHandler racingCarHandler, Moveable moveStrategy) {
+        this.moveStrategy = moveStrategy;
+        this.racingCarHandler = racingCarHandler;
     }
 
     public void move() {
-        IntStream.range(0, playTimes)
-                 .forEach(count -> positionSetter.movePosition(carPositions, getGachaList()));
-    }
-
-    private <T extends Number> List<T> getGachaList() {
-        return randomListGenerator.gacha(carPositions.size(), boundary);
+        racingCarHandler.race(moveStrategy);
     }
 }
