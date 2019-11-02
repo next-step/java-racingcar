766264f0ae50c38fde41d41744deb1a1125f3bd8
jahun.kim <jahun.kim@linecorp.com>
2019-11-02 16:11:58 +0900

    fix(Main): Fix main methods execute Racing game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
-import step1.calculator.StringCalculator;
-import step1.racing.ReadyStage;
+import step1.racing.controller.ReadyStage;
 
 public class Main {
     public static void main(String[] args) {
+//        if (args.length < 2) {
+//            throw new IllegalArgumentException("Not found arguments");
+//        }
+//        String type = args[0];
+//        String equation = args[1];
+//
+//        System.out.println(new StringCalculator<>(type).calculate(equation));
+
         ReadyStage readyStage = new ReadyStage();
         readyStage.play();
     }
