b5e02900740128bd0fe4a17170fc85665171a175
oh29oh29 <hyukjae89@gmail.com>
2019-11-02 15:47:32 +0900

    refactor 자동차경주 - StringBuilder가 아닌 String#join으로 수정

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import racingcar.domain.Car;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
@@ -27,18 +28,9 @@ public class ResultView {
     }
 
     public static void printWinners(List<Car> winners) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int winnersCount = winners.size();
-        for (int i = 0; i < winnersCount; i++) {
-            stringBuilder.append(winners.get(i).getName());
-            addDelimiter(winners, stringBuilder, i);
-        }
-        System.out.println(stringBuilder + " 가 최종 우승했습니다.");
-    }
-
-    private static void addDelimiter(List<Car> winners, StringBuilder stringBuilder, int i) {
-        if (i != winners.size() - 1) {
-            stringBuilder.append(", ");
-        }
+        List<String> winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+        System.out.println(String.join(", ", winnerNames) + " 가 최종 우승했습니다.");
     }
 }
