3b662e4f41e4821cfb5a01425b3f556e95c52ad8
jahun.kim <jahun.kim@linecorp.com>
2019-11-03 21:28:15 +0900

    fix(RacingStage): Separate generate instance to GameGenerator

--- src/main/java/step1/racing/controller/ReadyStage.java
+++ src/main/java/step1/racing/controller/ReadyStage.java
@@ -1,48 +1,19 @@
 package step1.racing.controller;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-
-import step1.racing.MoveStrategy;
-import step1.racing.Moveable;
-import step1.racing.RacingCarHandler;
 import step1.racing.RacingGame;
-import step1.racing.common.RandomIntegerGenerator;
-import step1.racing.data.RacingCar;
 import step1.racing.view.CarPositionViewer;
 
 public class ReadyStage {
-    private static final String SPLIT_DELIMITER = ",";
-    private static final String CAR_INDICATOR = "-";
-
-    private RacingCarHandler racingCarHandler;
-    private Moveable moveStrategy;
+    private final RacingGame racingGame;
 
     public ReadyStage() {
-        initValues();
+        this.racingGame = GameGenerator.getRacingGame();
     }
 
     public void play() {
-        RacingGame racingGame = new RacingGame(racingCarHandler, moveStrategy);
-        CarPositionViewer.printStatus(racingCarHandler, CAR_INDICATOR);
+        CarPositionViewer.printStatus(racingGame);
         racingGame.move();
-        CarPositionViewer.printStatus(racingCarHandler, CAR_INDICATOR);
-        CarPositionViewer.printWinner(racingCarHandler);
-    }
-
-    private void initValues() {
-        Scanner scanner = new Scanner(System.in);
-        String players = scanner.nextLine();
-        int playTimes = scanner.nextInt();
-
-        racingCarHandler = initCarPositions(players, playTimes);
-        moveStrategy = new MoveStrategy(RandomIntegerGenerator.getInstance());
-    }
-
-    private RacingCarHandler initCarPositions(String players, int playTimes) {
-        return new RacingCarHandler(Arrays.stream(players.split(SPLIT_DELIMITER))
-                                          .map(RacingCar::new)
-                                          .collect(Collectors.toList()), playTimes);
+        CarPositionViewer.printStatus(racingGame);
+        CarPositionViewer.printWinner(racingGame);
     }
 }
