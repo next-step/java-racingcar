3b82fd996d5eea1ab7d23e3f84b28c9ea07eafdf
byungkyu-ju <noel1227@gmail.com>
2019-11-05 02:53:03 +0900

    restore

--- src/main/java/calculator/Main.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-public class Main {
-
-  public static void main(String[] args) {
-    Long result = Calculator.run("2 + 3 * 4 / 2");
-    System.out.println(result);
-  }
-}
--- src/main/java/step1/calculator/Main.java
+++ src/main/java/step1/calculator/Main.java
@@ -2,9 +2,8 @@ package step1.calculator;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Long result = Calculator.run("2 + 3 * 4 / 2");
-        System.out.println(result);
-    }
+  public static void main(String[] args) {
+    Long result = Calculator.run("2 + 3 * 4 / 2");
+    System.out.println(result);
+  }
 }
