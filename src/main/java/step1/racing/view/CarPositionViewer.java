c57bbd2b187e9956b03f25ba9fbbea4a63ca9fde
jahun.kim <jahun.kim@linecorp.com>
2019-11-05 01:06:48 +0900

    fix(): Fix viewer methods get RaceSnapshot as parameter

--- src/main/java/step1/racing/view/CarPositionViewer.java
+++ src/main/java/step1/racing/view/CarPositionViewer.java
@@ -1,28 +1,15 @@
 package step1.racing.view;
 
 import step1.racing.RacingGame;
-import step1.racing.data.RacingCar;
 
 public class CarPositionViewer {
-    private static final String CAR_INDICATOR = "-";
-
-    public static void printStatus(RacingGame racingGame) {
-        for (RacingCar racingCar : racingGame.getRacingCars()) {
-            System.out.println(racingCar.getName() + " : " + printConcatenatedChar(CAR_INDICATOR, racingCar.getPosition()));
-        }
+    public static void printStatus(RaceSnapshot snapshot, String notice) {
+        System.out.println(notice);
+        System.out.println(snapshot.toString());
     }
 
-    public static void printWinner(RacingGame racingGame) {
-        racingGame.getWinners().forEach(car -> System.out.println(car.getName()));
-    }
-
-    private static String printConcatenatedChar(String flag, int num) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < num ; i ++) {
-            sb.append(flag);
-        }
-
-        return sb.toString();
+    public static void printWinner(RacingGame racingGame, String notice) {
+        System.out.println(notice);
+        racingGame.getWinners().forEach(car -> System.out.print(car.getName()));
     }
 }
