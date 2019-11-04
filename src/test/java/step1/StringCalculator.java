e6960218344a8831258cd0774b75a473d583f151
Depther <victory2373@gmail.com>
2019-11-04 21:58:55 +0900

    Add minus method

--- src/test/java/step1/StringCalculator.java
+++ src/test/java/step1/StringCalculator.java
@@ -8,12 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringCalculator {
 
 	@ParameterizedTest
-	@CsvSource(value = {"1+2:3", "4+5:9"}, delimiter = ':')
+	@CsvSource(value = {"1 + 2:3", "4+5:9"}, delimiter = ':')
 	void plus(String param, int result) {
+		param = param.replaceAll(" ", "");
 		String[] operands = param.split("\\+");
 		int left = Integer.parseInt(operands[0]);
 		int right = Integer.parseInt(operands[1]);
 		assertThat(left + right).isEqualTo(result);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"5-2:3", "13-3:10"}, delimiter = ':')
+	void minus(String param, int result) {
+		param = param.replaceAll(" ", "");
+		String[] operands = param.split("-");
+		int left = Integer.parseInt(operands[0]);
+		int right = Integer.parseInt(operands[1]);
+		assertThat(left - right).isEqualTo(result);
 	}
 
 }
