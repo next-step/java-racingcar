82b16619c5bc3e1b1103b0587ee4823936a1bae9
Depther <victory2373@gmail.com>
2019-11-05 15:07:51 +0900

    Add main method

--- src/test/java/step1/StringCalculator.java
+++ src/test/java/step1/StringCalculator.java
@@ -3,14 +3,46 @@ package step1;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 import static org.assertj.core.api.Assertions.*;
 
 class StringCalculator {
 
+	private Deque<Integer> numbers = new ArrayDeque<>();
+
+	private Deque<String> operators = new ArrayDeque<>();
+
+	private StringBuilder sb = new StringBuilder();
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,2,3", "9,8,17"})
+	void plus(int left, int right, int answer) {
+		assertThat(left + right).isEqualTo(answer);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"5,2,3", "13,3,10"})
+	void minus(int left, int right, int answer) {
+		assertThat(left - right).isEqualTo(answer);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"7,4,28", "21,3,63"})
+	void multiply(int left, int right, int answer) {
+		assertThat(left * right).isEqualTo(answer);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"16,4,4", "21,3,7"})
+	void divide(int left, int right, int answer) {
+		assertThat(left * right).isEqualTo(answer);
+	}
+
 	@Test
-	void inputValueCheck() {
+	void checkNullString() {
 		String input = null;
 		assertThatThrownBy(() -> {
 			if (input == null || input.equals("")) {
@@ -27,7 +59,7 @@ class StringCalculator {
 	}
 
 	@Test
-	void operatorCheck() {
+	void checkOperatorYn() {
 		char ch = '&';
 		assertThatThrownBy(() -> {
 			if (ch != '+' && ch != '-' && ch != '*' && ch != '/' ) {
@@ -37,27 +69,112 @@ class StringCalculator {
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"1+2-3*4/5"})
-	void splitInput(String param) {
-		assertThat(param.split("")).containsExactly("1", "+", "2", "-", "3", "*", "4", "/", "5");
+	@CsvSource(value = {"2+3*4/2, 10", "18 / 3 * 2 + 22 - 7, 27"})
+	void main(String input, int answer) {
+		checkNullString(input);
+		splitString(input);
+		int result = calculate();
+		assertThat(result).isEqualTo(answer);
 	}
 
-	@ParameterizedTest
-	@CsvSource(value = {"1,2,3", "9,8,17"})
-	void plus(int left, int right, int result) {
-		assertThat(left + right).isEqualTo(result);
+	void checkNullString(String input) {
+		if (input == null || input.equals("")) {
+			throw new IllegalArgumentException();
+		}
 	}
 
-	@ParameterizedTest
-	@CsvSource(value = {"5,2,3", "13,3,10"})
-	void minus(int left, int right, int result) {
-		assertThat(left - right).isEqualTo(result);
+	private void splitString(String input) {
+		input = removeSpaceChar(input);
+		checkStringValidation(input);
+
+		for (char ch : input.toCharArray()) {
+			storeNumberAndOperator(ch);
+		}
+		numbers.offerLast(Integer.parseInt(sb.toString()));
 	}
 
-	@ParameterizedTest
-	@CsvSource(value = {"7,4,28", "21,3,63"})
-	void multiply(int left, int right, int result) {
-		assertThat(left * right).isEqualTo(result);
+	private String removeSpaceChar(String input) {
+		return input.replaceAll(" ", "");
+	}
+
+	private void checkStringValidation(String input) {
+		String[] numbers = input.split("\\+|-|\\*|/");
+
+		for (String number : numbers) {
+			isNumeric(number);
+		}
+	}
+
+	private void isNumeric(String number) {
+		try {
+			Integer.parseInt(number);
+		} catch (Exception e) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void storeNumberAndOperator(char ch) {
+		if (checkOperatorYn(ch)) {
+			numbers.offerLast(Integer.parseInt(sb.toString()));
+			operators.offerLast(String.valueOf(ch));
+			sb = new StringBuilder();
+			return;
+		}
+
+		sb.append(ch);
+	}
+
+	private boolean checkOperatorYn(char ch) {
+		if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+			return true;
+		}
+		return false;
+	}
+
+	private int calculate() {
+		int result = numbers.pollFirst();
+
+		while (!numbers.isEmpty()) {
+			result = branchOffOperator(result, numbers.pollFirst(), operators.pollFirst());
+		}
+
+		return result;
+	}
+
+	private int branchOffOperator(int left, int right, String operator) {
+		if (operator.equals("+")) {
+			return plus(left, right);
+		}
+
+		if (operator.equals("-")) {
+			return minus(left, right);
+		}
+
+		if (operator.equals("*")) {
+			return multiply(left, right);
+		}
+
+		if (operator.equals("/")) {
+			return divide(left, right);
+		}
+
+		return 0;
+	}
+
+	private int plus(int left, int right) {
+		return left + right;
+	}
+
+	private int minus(int left, int right) {
+		return left - right;
+	}
+
+	private int multiply(int left, int right) {
+		return left * right;
+	}
+
+	private int divide(int left, int right) {
+		return left / right;
 	}
 
 }
