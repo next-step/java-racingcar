486dfe3d2b3c26426a30e8370c5886c0316e7657
Depther <victory2373@gmail.com>
2019-11-07 19:13:09 +0900

    feat 자동차 이동 기능

--- src/main/java/step2/RacingGame.java
+++ src/main/java/step2/RacingGame.java
@@ -1,7 +1,10 @@
 package step2;
 
+import java.util.Random;
+
 public class RacingGame {
 	private int time;
+
 	private int[] carPositions;
 
 	public static void main(String[] args) {
@@ -10,10 +13,28 @@ public class RacingGame {
 
 		RacingGame racingGame = new RacingGame();
 		racingGame.initSetting(userInput);
+		racingGame.doRacing();
 	}
 
 	private void initSetting(UserInput userInput) {
 		time = userInput.getMOVE_COUNT();
 		carPositions = new int[userInput.getCAR_COUNT()];
+	}
+
+	private void doRacing() {
+		for (int i = 0; i < time; i++) {
+			move();
+		}
+	}
+
+	private void move() {
+		Random random = new Random();
+		for (int i = 0; i < carPositions.length; i++) {
+			int randomNumber = random.nextInt(10);
+
+			if (randomNumber >= 4) {
+				carPositions[i]++;
+			}
+		}
 	}
 }
