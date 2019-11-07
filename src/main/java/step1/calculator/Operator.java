703ccab4f772bf6f5302af816e55c3e7a855db1f
byungkyu-ju <noel1227@gmail.com>
2019-11-07 11:06:18 +0900

    refactor (default java convention으로 변경)

--- src/main/java/step1/calculator/Operator.java
+++ src/main/java/step1/calculator/Operator.java
@@ -1,18 +1,18 @@
 package step1.calculator;
 
 public enum Operator {
-  PLUS_OPERATOR("+"),
-  MINUS_OPERATOR("-"),
-  MULTIPLY_OPERATOR("*"),
-  DIVIDE_OPERATOR("/");
+    PLUS_OPERATOR("+"),
+    MINUS_OPERATOR("-"),
+    MULTIPLY_OPERATOR("*"),
+    DIVIDE_OPERATOR("/");
 
-  private String name;
+    private String name;
 
-  Operator(String name) {
-    this.name = name;
-  }
+    Operator(String name) {
+        this.name = name;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 }
