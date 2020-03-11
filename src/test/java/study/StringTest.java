48f7446334de905dc375706b5b467e146356a772
LeeSeungHee <palmseung@gmail.com>
2020-03-12 01:16:47 +0900

    Test : StringTest - charAt, assertThatThrownBy, assertThatExceptionTypeOf

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,11 +1,17 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class StringTest {
-
+    @Order(1)
+    @DisplayName("요구사항1 : split 활용하기")
     @Test
     void split() {
         //given, when
@@ -22,8 +28,10 @@ public class StringTest {
         assertThat(split).endsWith("1", "2");
     }
 
+    @Order(2)
+    @DisplayName("요구사항2 : substring 활용하기")
     @Test
-    void substring(){
+    void substring() {
         //when
         String substring = "(1,2)".substring(1, 4);
 
@@ -31,10 +39,47 @@ public class StringTest {
         assertThat(substring).isEqualTo("1,2");
     }
 
+    @Order(3)
+    @DisplayName("요구사항2 : substring 활용하기 (2)")
     @Test
-    void substring2(){
+    void substring2() {
         //when
         String substring = "(1,2,3)".substring(1, 6);
         assertThat(substring).isEqualTo("1,2,3");
+    }
+
+    @Order(4)
+    @DisplayName("요구사항3 : charAt 활용하기")
+    @Test
+    void charAt() {
+        //when
+        char result1 = "abc".charAt(0);
+        char result2 = "abc".charAt(1);
+        char result3 = "abc".charAt(2);
+
+        //then
+        assertThat(result1).isEqualTo('a');
+        assertThat(result2).isEqualTo('b');
+        assertThat(result3).isEqualTo('c');
+    }
+
+    @Order(5)
+    @DisplayName("요구사항3 : assertThatThrownBy 활용하기")
+    @Test
+    void charAt2() {
+        assertThatThrownBy(() -> {
+            char result = "abc".charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("index");
+    }
+
+    @Order(6)
+    @DisplayName("요구사항3 : assertThatExceptionOfType 활용하기")
+    @Test
+    void charAt3() {
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char result = "abc".charAt(3);
+                }).withMessageContaining("index");
     }
 }
