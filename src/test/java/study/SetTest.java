e6a6bfc7644cb287f890462b6e0849c52e4be15e
LeeSeungHee <palmseung@gmail.com>
2020-03-12 01:40:33 +0900

    Test : SetTest - contains

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,10 +1,11 @@
 package study;
 
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,7 +16,7 @@ public class SetTest {
     private Set numbers;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         numbers = new HashSet<>();
         numbers.add(1);
         numbers.add(1);
@@ -25,9 +26,24 @@ public class SetTest {
 
     @DisplayName("요구사항1 : size() 활용하기")
     @Test
-    void size(){
+    void size() {
         //then
         assertThat(numbers.size()).isEqualTo(3);
         assertThat(numbers).hasSize(3);
+    }
+
+    @Test
+    void contain(){
+        //then
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contain2(int number){
+        //then
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
