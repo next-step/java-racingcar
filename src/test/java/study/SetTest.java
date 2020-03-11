77b090ab7ea97d90189bde38b398abc929049aa6
LeeSeungHee <palmseung@gmail.com>
2020-03-12 01:30:16 +0900

    Test : SetTest - size

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,33 @@
+package study;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("요구사항1 : size() 활용하기")
+    @Test
+    void size(){
+        //then
+        assertThat(numbers.size()).isEqualTo(3);
+        assertThat(numbers).hasSize(3);
+    }
+}
