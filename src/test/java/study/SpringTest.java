7a85bd177b17de52c2fc20a506a8594a868c2fdd
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-13 20:13:00 +0900

    substring study test

--- src/test/java/study/SpringTest.java
+++ src/test/java/study/SpringTest.java
@@ -8,7 +8,13 @@ public class SpringTest {
     @Test
     void split() {
         String[] result = "1,2".split(",");
-        assertThat(result).containsExactly("1","2");
+        assertThat(result).containsExactly("1", "2");
 
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
     }
 }
