47665e8afb7bbcfc4d7c7b16c9c9cea30306afef
Sooduck <psd177@gmail.com>
2020-03-17 21:03:17 +0900

    split CollectionSet study test

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,55 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class SetCollectionTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        int result = numbers.size();
+        assertThat(result).isEqualTo(3);
+    }
+
+
+    @Test
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void isContains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+        String actualValue = input.toLowerCase();
+        assertEquals(expected, actualValue);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,40 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void subString() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자 가지고 오기 테스트")
+    void charAtMessageContaining() {
+        assertThatThrownBy(() -> {
+            char result = "abc".charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageMatching("String index out of range: \\d+");
+    }
+
+    @Test
+    @DisplayName("문자 가지고 오기 테스트")
+    void charAtMessageMatch() {
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char result = "abs".charAt(3);
+                }).withMessageMatching("[A-Za-z ]* range: [0-9]*");
+    }
+}
