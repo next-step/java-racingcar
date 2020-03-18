4b9f2175acabcf21c7fb58a783f7a09f4a025aa3
isntyet <isntyet@gmail.com>
2020-03-18 21:56:24 +0900

    test2 with study step1

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 메소드를 활용해 Set의 크기를 확인")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인")
+    void containsFromValueSource(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("입력 값에 따라 결과 값이 다른 경우에 대한 contains() 테스트")
+    void containsFromTestCases(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -11,6 +13,25 @@ public class StringTest {
         String[] result = "1,2".split(",");
 
         assertThat(result).contains("1");
-
     }
+
+    @Test
+    void substring() {
+        String tmpString = "(1,2)";
+        String result = tmpString.substring(1,4);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt()을 이용해 특정위치의 문자를 가져오기")
+    void charAt() {
+        String tmpString = "abc";
+
+        assertThatThrownBy(() -> {
+                assertThat(tmpString.charAt(10)).isEqualTo('a');
+            }
+        ).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
 }
