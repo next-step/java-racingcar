530955cae1bd1229eca0d07c73252ec7a8c7779f
jwee0330 <jwee0330@gmail.com>
2020-03-18 21:30:47 +0900

    test: SetTest 클래스 추가 & 요구사항 1 테스트 코드 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("요구사항 1: Set의 size를 확인한다.")
+    @Test
+    public void set_size_test() {
+        assertThat(numbers.size())
+                .isEqualTo(3);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -41,5 +41,4 @@ public class StringTest {
         .isInstanceOf(IndexOutOfBoundsException.class);
     }
 
-
 }
