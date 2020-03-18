428ffc81f1057285343854ea49f15d91ffac617d
yeoseon <54384004+yeoseon@users.noreply.github.com>
2020-03-18 22:13:55 +0900

    충돌 문제 해결

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
@@ -31,8 +28,5 @@ public class StringTest {
         assertThatThrownBy(() -> {
             "abc".charAt(4);
         }).isInstanceOf(StringIndexOutOfBoundsException.class);
-    }
-        assertThat(result).contains("1"); // assertThat이라는 라이브러리를 이용해서, data에 내가 원하는 값이 있는지 확인한다.
-        assertThat(result).containsExactly("1", "2"); //containsExactly : 해당 순서로 값이 잘 들어있는지까지 확인
     }
 }
