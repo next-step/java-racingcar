b10c2842eb00b7e3f4284a6932a3d5616e0c3144
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-18 21:55:47 +0900

    step1
    set test 미완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -17,4 +19,17 @@ public class StringTest {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
     }
+
+    @DisplayName("charAt() 테스트")
+    @Test
+    void charAt() {
+        char result = "abc".charAt(2);
+        assertThat(result).isEqualTo('c');
+
+        assertThatThrownBy(() -> {
+            char result2 = "abc".charAt(5);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+
 }
