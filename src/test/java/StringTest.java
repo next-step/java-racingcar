d9bc3e2615f8de51694219c0614053028f528021
manhyuk <manhyuk@kw.ac.kr>
2020-03-18 21:39:25 +0900

    chore: remove unused code

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.in;
 
 public class StringTest {
 
