e6b1da6aa07b670b993ec27d0d59847279f067c3
kiyeoneee <kyeoneee@gmail.com>
2020-03-18 21:24:58 +0900

    feat: add substring test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,12 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    void substring() {
+        String value = "(1,2)";
+
+        String result = "(1,2)".substring(1, value.length() - 1);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
