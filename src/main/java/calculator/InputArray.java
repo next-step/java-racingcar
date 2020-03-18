0f0a4dd5f93b2c91f410f3be35e4844f660643f6
LeeSeungHee <palmseung@gmail.com>
2020-03-19 02:58:20 +0900

    feat (input) : Add InputArray.validateOperator

--- src/main/java/calculator/InputArray.java
+++ src/main/java/calculator/InputArray.java
@@ -1,9 +1,33 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputArray {
+    private static final List<String> OPERATORS = Arrays.asList("+", "-", "/", "*");
     private String[] inputArr;
 
     public InputArray(String[] inputArr) {
+        validateOperator(inputArr);
         this.inputArr = inputArr;
+    }
+
+    private void validateOperator(String[] inputArr) {
+        boolean present = extractEvenChar(inputArr).stream()
+                .filter(it -> !OPERATORS.contains(it))
+                .findAny()
+                .isPresent();
+        if(present){
+            throw new IllegalArgumentException("사칙연산 기호만 입력 가능합니다.");
+        }
+    }
+
+    public List<String> extractEvenChar(String[] inputArr) {
+        List<String> operators = new ArrayList<>();
+        for (int i = 1; i < inputArr.length; i += 2) {
+            operators.add(inputArr[i]);
+        }
+        return operators;
     }
 }
--- src/test/java/calculator/InputArrayTest.java
+++ src/test/java/calculator/InputArrayTest.java
@@ -1,15 +1,22 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputArrayTest {
+    private static final List<String> OPERATORS = Arrays.asList("+", "-", "/", "*");
+
     @DisplayName("split된 input의 짝수번째 자리에 지정된 연산자가 아닌 문자열이 오면 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(strings ="&, @, =, 3" )
+    @ValueSource(strings ={"1", "%", "!", "&", "@"})
     void validateInputArrayTest(String operator){
         //given
         String[] inputArr = {"1", operator, "3"};
@@ -18,8 +25,6 @@ public class InputArrayTest {
         assertThrows(IllegalArgumentException.class, () -> {
            new InputArray(inputArr);
         });
-
-
-
     }
+
 }
