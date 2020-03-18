5b55ea3ed6490608fc61a6ac2fdc0fbfb3e11521
LeeSeungHee <palmseung@gmail.com>
2020-03-19 01:49:03 +0900

    feat (input) : Add feature to split input

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -1,27 +1,28 @@
 package calculator;
 
 public class InputView {
+    private static final String BLANK = " ";
     private String input;
 
     public InputView(String input) {
-        validateNull();
-        validateEmpty();
+        validateNull(input);
+        validateEmpty(input);
         this.input = input;
     }
 
-    private void validateNull() {
+    private void validateNull(String input) {
         if (input == null) {
             throw new IllegalArgumentException("null은 입력할 수 없습니다.");
         }
     }
 
-    private void validateEmpty() {
+    private void validateEmpty(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException("빈 문자열은 입력할 수 없습니다.");
         }
     }
 
     public String[] split() {
-        return new String[0];
+        return input.split(BLANK);
     }
 }
--- src/test/java/calculator/InputViewTest.java
+++ src/test/java/calculator/InputViewTest.java
@@ -1,11 +1,14 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputViewTest {
@@ -20,7 +23,7 @@ public class InputViewTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1 + 3:3", "13 - 2 / 2:5", "3 * 5 - 2 + 7:7"})
+    @CsvSource(value = {"1 + 3:3", "13 - 2 / 2:5", "3 * 5 - 2 + 7:7"}, delimiter = ':')
     void splitTest(String input, int expectedLength){
         //given
         InputView inputView = new InputView(input);
