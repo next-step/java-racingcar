ed0ce12f2446f979c0d6aa30c6bf4159f913c9d6
ookhub <59785963+ookhub@users.noreply.github.com>
2020-03-18 22:03:18 +0900

    Step1 (#525)
    * split CollectionSet study test
    
    * split Set study test
    
    Co-authored-by: Sooduck <psd177@gmail.com>

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,52 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers).hasSize(3);
+    }
+
+
+    @Test
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void isContains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true, 4:false"}, delimiter = ':')
+    void isExpectedByCsv(int input, boolean expected) {
+        assertThat(expected).isEqualTo(expected);
+    }
+}
