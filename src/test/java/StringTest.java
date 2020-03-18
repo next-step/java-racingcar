a04f35bd1c863cdb6a99cb8179d0d7e6c3109010
jinwoo0607 <37197115+jinwoo0607@users.noreply.github.com>
2020-03-18 22:01:27 +0900

    테스트 1단계 완료 (#521)
    Co-authored-by: jinwoo <jwlee@qualson.com>

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest { // Convention production naming + 'Test'
+    private Set numbers;
+
+    @BeforeEach // -> 각 테스트에서 중복으로 데이터를 사용할때 데이터 초기화작업은 여기서 한다.
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void getSize() {
+        final int size = numbers.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number){
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsIncludeFalseCase(int number, boolean result) {
+        assertThat(numbers.contains(number)).isEqualTo(result);
+    }
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void subString() {
+        String value = "(1,2)";
+        String result = value.replaceAll("\\(", "").replaceAll("\\)","");
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("chartAt의 범위를 벗어나는 경우")
+    @Test
+    void chartAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+    }
+}
