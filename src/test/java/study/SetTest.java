b49283a14c082dfc1248c370bacc96f94e580a79
yeoseon <54384004+yeoseon@users.noreply.github.com>
2020-03-18 22:17:22 +0900

    Step1 학습 테스트 재반영  (#537)
    * split study test
    
    * step1 학습테스트
    
    * step1 재반영
    
    * 충돌 문제 해결

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,4 +1,50 @@
 package study;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SetTest {
-}
+
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers).contains(1);
+//        assertThat(numbers.size()).isEqualTo(3); // 이렇게 쓰는 것보다 배열, 콜렉션의 경우는 아래 구문으로 쓰는 것이 좋다.
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains1(int no) {
+        //중복 코드 발생
+//        assertThat(numbers.contains(1)).isTrue();
+//        assertThat(numbers.contains(2)).isTrue();
+//        assertThat(numbers.contains(3)).isTrue();
+
+        assertThat(numbers.contains(no)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    void contains2(int no, boolean expected) {
+        assertThat(numbers.contains(no)).isEqualTo(expected);
+    }
+
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,32 @@
 package study;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void split() {
         String[] result = "1,2".split(",");
-        assertThat(result).contains("1"); // assertThat이라는 라이브러리를 이용해서, data에 내가 원하는 값이 있는지 확인한다.
+        assertThat(result).contains("1"); // assertThat이라는 라이브러리를 이용해서, data에 내가 원하는 값이 있는지 확인한다. 배열, Collection 쓸 때 많이 사용한다.
         assertThat(result).containsExactly("1", "2"); //containsExactly : 해당 순서로 값이 잘 들어있는지까지 확인
+    }
+
+    @Test
+    void substring() {
+        String value = "(1,2)";
+        String result = "(1,2)".substring(1, value.length() - 1); // 숫자를 명시적으로 하고싶지 않다.
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자를 가져온다.")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
