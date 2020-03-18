8f4f1467e093b48df7e94370a3bd1f851dd568f9
sang5c <skah321@gmail.com>
2020-03-18 21:54:25 +0900

    assertj 학습테스트 추가

---
+++ src/test/java/study/AssertjTest.java
@@ -0,0 +1,40 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AssertjTest {
+
+    String result = "assertj test code";
+
+    @Test
+    @DisplayName("시작 끝 문자열 확인 테스트")
+    void stringContain() {
+        assertThat(result).startsWith("assertj")
+                .endsWith("code");
+    }
+
+    @Test
+    @DisplayName("equal, not equal test")
+    void equalAndNotEqual() {
+        assertThat(result).isEqualTo("assertj test code")
+                .isNotEqualTo("junit5");
+    }
+
+    @Test
+    @DisplayName("size, contain")
+    void sizeAndContain() {
+        assertThat(result).hasSize(17)
+                .contains("test")
+                .doesNotContain("junit");
+    }
+
+    @Test
+    @DisplayName("as 에러메시지 테스트")
+    void asErrorMessage() {
+        assertThat(result).as("error message test").contains("junit");
+    }
+
+}
