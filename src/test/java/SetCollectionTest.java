abff45530e5ec2e8b29439e096cb4f174ba4b22b
Kim Kyoung Sun <rudtjs9956@gmail.com>
2020-03-18 21:55:05 +0900

    1단계 - 학습 테스트 실습 구현완료 (#506)
    * String 클래스에 대한 학습테스트 구현완료
    
    * Set Collection에 대한 학습 테스트 구현완료
    
    * Test Class 파일명 Convention 적용

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetCollectionTest {
+
+    private Set numbers;
+
+    @BeforeEach
+    void setup() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    public void requirements1() {
+//        assertThat(numbers.size()).isEqualTo(3);
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void requirements2(int param) {
+        assertTrue(numbers.contains(param));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    public void requirements3(String param, String expected) {
+        assertEquals(Boolean.valueOf(expected), numbers.contains(Integer.parseInt(param)));
+    }
+
+}
