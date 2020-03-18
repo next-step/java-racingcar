e9464f4bf832dc7c7baf75e36e83d490aa25f6cb
wholemann <wholeman.dev@gmail.com>
2020-03-18 21:53:07 +0900

    Add test for String

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,33 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void split() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
+
+        String[] values = "1".split(",");
+        assertThat(values).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String values = "(1,2)";
+        String result = values.substring(1, values.length() - 1);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt의 범위를 벗어나는 경우")
+    @Test
+    void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
