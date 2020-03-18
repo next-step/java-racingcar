551833ee6cbd2e8bf01c474e82d035e9839c325d
LeeSeungHee <palmseung@gmail.com>
2020-03-19 01:08:25 +0900

    feat (input) : Add feature to validate NullOrEmptySource

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -4,6 +4,20 @@ public class InputView {
     private String input;
 
     public InputView(String input) {
+        validateNull();
+        validateEmpty();
         this.input = input;
+    }
+
+    private void validateNull() {
+        if (input == null) {
+            throw new IllegalArgumentException("null은 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateEmpty() {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("빈 문자열은 입력할 수 없습니다.");
+        }
     }
 }
--- src/test/java/calculator/InputViewTest.java
+++ src/test/java/calculator/InputViewTest.java
@@ -2,14 +2,14 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InputViewTest {
-    @DisplayName("공백을 입력받으면 예외가 발생한다.")
+    @DisplayName("공백 또는 null을 입력받으면 예외가 발생한다.")
     @ParameterizedTest
-    @NullSource
+    @NullAndEmptySource
     void split(String input) {
         //when, then
         assertThrows(IllegalArgumentException.class, () -> {
