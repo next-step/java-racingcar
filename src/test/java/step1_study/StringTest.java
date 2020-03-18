9502b4ddf9e42f57ca0c5d67981ca75a209457d0
Gwangyu Kim <kwangyu326@gmail.com>
2020-03-18 21:58:21 +0900

    Step1 commit (#516)
    * step1 test study
    
    * step1 Set Collection test study
    
    * String 클래스 학습 테스트 완료
    
    * StringTest로 이름 변경 (SplitTest -> StringTest)
    
    * SetTest 요구사항 완료

---
+++ src/test/java/step1_study/StringTest.java
@@ -0,0 +1,32 @@
+package step1_study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    public void 요구사항1_test() {
+        String data = "1,2";
+        assertThat(data.split(",")).contains("1");
+        assertThat(data.split(",")).containsExactly("1", "2");
+        assertThat(data.split(",")).containsExactly(new String[]{"1", "2"});
+    }
+
+    @Test
+    void 요구사항2_test() {
+        String data = "(1,2)";
+        assertThat(data.substring(1, data.length() - 1)).isEqualTo("1,2");
+    }
+
+    @DisplayName("indexOutofBoundException 테스트")
+    @Test
+    void 요구사항3_test() {
+        String data = "abc";
+        assertThatThrownBy(() -> {
+            data.charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+}
