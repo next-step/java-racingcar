92dbc4bc67d5e40ee60ac913ebfe19f372d83487
jwee0330 <jwee0330@gmail.com>
2020-03-18 21:43:19 +0900

    chroe: StringTest 소스 정리

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,16 +1,15 @@
 package study;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class StringTest {
     @DisplayName("요구사항 1")
     @Test
-    public void string_test() {
+    public void stringTest() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
         String[] numberOne = "1".split(",");
@@ -19,7 +18,7 @@ public class StringTest {
 
     @DisplayName("요구사항 2")
     @Test
-    public void eliminate_brace_test() {
+    public void eliminateBraceTest() {
         //given
         String source = "(1,2)";
         //when
@@ -30,15 +29,14 @@ public class StringTest {
 
     @DisplayName("요구사항 3 : 범위를 벗어난 인덱스를 호출하면 예외를 발생시킨다")
     @Test
-    public void chatAt_test() {
+    public void chatAtTest() {
         //given
         String source = "abc";
 
         //when & then
         assertThatThrownBy(() -> {
             source.charAt(-1);
-        })
-        .isInstanceOf(IndexOutOfBoundsException.class);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
     }
 
 }
