ac999e0292db62ebaeacb3afe8231347486c230d
jbyiop <jbyiop@gmail.com>
2020-03-18 21:54:24 +0900

    step1

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,12 +1,51 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class Sample {
 
-    @DisplayName("HELLO WORLD")
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트")
     @Test
-    public void abc() {
-        Assertions.assertThat(2).isEqualTo(2);
+    public void split() {
+        String target = "1,2";
+        String[] tokens = target.split(",");
+        assertThat(tokens).containsExactly("1", "2");
     }
+
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트")
+    @Test
+    public void splitSingleToken() {
+        String target = "1";
+        String[] tokens = target.split(",");
+        assertThat(tokens).containsExactly("1");
+    }
+
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환")
+    @Test
+    public void substring() {
+        String target = "(1,2)";
+        String result = target.substring(1, target.length() - 1);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트")
+    @Test
+    public void charAt() {
+        final String target = "abc";
+        char result = target.charAt(1);
+        assertThat(result).isEqualTo('b');
+    }
+
+    @DisplayName("StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트")
+    @Test
+    public void charAtThrowsException() {
+        final String target = "abc";
+
+        assertThatThrownBy(() -> {
+            target.charAt(5);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
 }
---
+++ src/test/java/SetTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트")
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @DisplayName("1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains2(int value, boolean expected) {
+        assertThat(numbers.contains(value)).isEqualTo(expected);
+    }
+}
