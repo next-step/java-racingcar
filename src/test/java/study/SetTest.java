e1fbeea33346f76e60ef245c9909af971166464e
kiyeoneee <kyeoneee@gmail.com>
2020-03-18 21:48:26 +0900

    feat: add Set contains both true and false results test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -32,5 +33,11 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int input) {
         assertTrue(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false"})
+    void containsCheck(int input, boolean output) {
+        assertThat(numbers.contains(input)).isEqualTo(output);
     }
 }
