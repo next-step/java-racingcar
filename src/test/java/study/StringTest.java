6ef21c8925eb71261757195670d9aae9aba40da4
jwee0330 <jwee0330@gmail.com>
2020-03-18 21:20:30 +0900

    test: 요구사항 1의 두번째 테스트 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,5 +9,7 @@ public class StringTest {
     public void string_test() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
+        String[] numberOne = "1".split(",");
+        assertThat(numberOne).contains("1");
     }
 }
