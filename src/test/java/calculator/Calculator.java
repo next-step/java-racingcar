12380afd4d046d60908602db748ad1a368189f3b
yohan <yohan.lee@uneedcomms.com>
2020-03-19 23:23:54 +0900

    feat: calculate 추상 메소드와 구현을 가진 Operator enum 생성

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -13,7 +13,6 @@ public class Calculator {
         verifyInput(input);
         calculationOrder.init(input.split(Separator));
         long x = stringToNumber(calculationOrder.next());
-
         return 0;
     }
 
@@ -29,5 +28,45 @@ public class Calculator {
         if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public enum Operator {
+        ADD("+") {
+            long calculate(long x, long y) {
+                return x + y;
+            }
+        },
+        MINUS("-") {
+            long calculate(long x, long y) {
+                return x - y;
+            }
+        },
+        MULTIPLE("*") {
+            long calculate(long x, long y) {
+                return x * y;
+            }
+        },
+        DIVIDE("/") {
+            long calculate(long x, long y) {
+                return x / y;
+            }
+        };
+
+        String value;
+
+        Operator(String value) {
+            this.value = value;
+        }
+
+        static Operator findOperatorByValue(String operatorValue) {
+            for (Operator operator : values()) {
+                if (operator.value.equals(operatorValue)) {
+                    return operator;
+                }
+            }
+            throw new IllegalArgumentException();
+        }
+
+        abstract long calculate(long x, long y);
     }
 }
