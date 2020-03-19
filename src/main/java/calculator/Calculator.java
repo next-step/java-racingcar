b207edd951d6dff99ed03f97aa07420fbde69069
jbyiop <jbyiop@gmail.com>
2020-03-19 16:49:44 +0900

    feat (Calculator) : 사칙연산 테스트 통과를 위한 구현
    테스트 코드를 통과하기 위하여 기능을 우선 구현한다.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,45 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Calculator {
+
     public int calc(String expression) {
-        String[] tokens = expression.split("[+]");
-        return Stream.of(tokens)
+        String[] tokens = expression.split("[-+/*]", -1);
+        int[] numbers = Stream.of(tokens)
                 .mapToInt(Integer::parseInt)
-                .sum();
+                .toArray();
+
+        List<String> operations = Arrays.stream(expression.split("[0-9]"))
+                .filter((item) -> !"".equals(item))
+                .collect(Collectors.toList());
+
+        List<BiFunction<Integer, Integer, Integer>> functions = operations.stream()
+                .map(op -> {
+                    switch (op) {
+                        case "+":
+                            return (BiFunction<Integer, Integer, Integer>) (a, b) -> a + b;
+                        case "-":
+                            return (BiFunction<Integer, Integer, Integer>) (a, b) -> a - b;
+                        case "*":
+                            return (BiFunction<Integer, Integer, Integer>) (a, b) -> a * b;
+                        case "/":
+                            return (BiFunction<Integer, Integer, Integer>) (a, b) -> a / b;
+                        default:
+                            throw new RuntimeException("구현되지 않은 연산자");
+                    }
+                })
+                .collect(Collectors.toList());
+
+        int result = numbers[0];
+
+        for(int i=0; i<functions.size(); i++) {
+            result = functions.get(i).apply(result, numbers[i+1]);
+        }
+        return result;
     }
 }
