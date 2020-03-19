fc0be9b2b5ea410497461840c186b7ef0e4cec49
jwee0330 <jwee0330@gmail.com>
2020-03-19 20:55:29 +0900

    test: 입력값이 빈 공백 문자일 경우 IllegalArgumentException throw

--- src/main/java/stringcalculator/Expression.java
+++ src/main/java/stringcalculator/Expression.java
@@ -4,6 +4,9 @@ public class Expression {
     private final String source;
 
     public Expression(String source) {
+        if (source == null || "".equals(source.trim())) {
+            throw new IllegalArgumentException("입력 값은 null 이거나 빈 공백 문자일 수 없습니다.");
+        }
         this.source = source;
     }
 
--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -5,15 +5,25 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculator {
 
     @DisplayName("사용자 입력 받기")
     @ParameterizedTest
     @ValueSource(strings = {"2 + 3 * 4 / 2"})
-    public void init(String source) {
+    void init(String source) {
         Expression expression = new Expression(source);
 
         assertThat(expression.getSource()).isEqualTo(source);
+    }
+
+    @DisplayName("입력값이 빈 공백 문자일 경우 IllegalArgumentException throw")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void nullOrEmptyStringThrowIllegalArgumentException(String source) {
+        assertThatThrownBy(() -> new Expression(source))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력 값은 null 이거나 빈 공백 문자일 수 없습니다.");
     }
 }
