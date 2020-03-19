1968a1a020c8664f5b683c6f46cf8d0b95a640af
LeeSeungHee <palmseung@gmail.com>
2020-03-19 15:10:55 +0900

    test (input) : Add InputViewTest.validInputTest

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class InputView {
+    private String input;
+    private int carCount;
+    private int tryCount;
+
+    public InputView(String input) {
+        this.input = input;
+    }
+}
--- src/test/java/racingcar/InputViewTest.java
+++ src/test/java/racingcar/InputViewTest.java
@@ -1,11 +1,17 @@
 package racingcar;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class InputViewTest {
-
-    @Test
-    void validInputTest(){
-
+    @DisplayName("null 또는 공백문자열이 입력되면 예외가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void validInputTest(String input){
+        //when, then
+        assertThatIllegalArgumentException().isThrownBy(() -> new InputView(input));
     }
 }
