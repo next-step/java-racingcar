ed4006535b6cde8e70827fa65cc73f6f74124da4
ohtaeg <otk1090@naver.com>
2020-03-19 17:24:17 +0900

    feat (operand) : 피연산자를 구분하는 기능을 추가하여 stack에 쌓는 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,12 +1,25 @@
 package calculator;
 
+import calculator.operand.Number;
+
+import java.util.Stack;
+
 public class StringCalculator {
     private static final String BLANK = " ";
     public int calculate(String input) {
         isEmpty(input);
 
         String[] source = toStringArray(input);
-        return 0;
+        Stack<Number> operandStack = new Stack<>();
+        int result = 0;
+
+        for (String value : source) {
+            // 피연산자
+            if (isNumber(value)) {
+                operandStack.push(Number.intValueOf(value));
+            }
+        }
+        return result;
     }
 
     private void isEmpty(final String input) {
@@ -17,5 +30,14 @@ public class StringCalculator {
 
     String[] toStringArray(final String input) {
         return input.split(BLANK);
+    }
+
+    public boolean isNumber(final String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
