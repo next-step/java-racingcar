44d432ef0a996f94fd153bcc66765c8412334ffc
yohan <yohan.lee@uneedcomms.com>
2020-03-19 23:18:27 +0900

    feat: 산술연산을 위한 String -> long 캐스팅 구현

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -12,7 +12,17 @@ public class Calculator {
     public long calculate(String input) {
         verifyInput(input);
         calculationOrder.init(input.split(Separator));
+        long x = stringToNumber(calculationOrder.next());
+
         return 0;
+    }
+
+    private long stringToNumber(String value) {
+        try {
+            return Long.parseLong(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void verifyInput(String input) {
