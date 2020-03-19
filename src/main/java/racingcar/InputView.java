0236e6e3eb0809f59a574cd4f972bfdc81248521
LeeSeungHee <palmseung@gmail.com>
2020-03-19 15:13:54 +0900

    feat (input) : Add InputView.validateNull, validateEmpty

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,11 +1,23 @@
 package racingcar;
 
 public class InputView {
-    private String input;
     private int carCount;
     private int tryCount;
 
     public InputView(String input) {
-        this.input = input;
+        validateNull(input);
+        validateEmpty(input);
+    }
+
+    private void validateNull(String input){
+        if(input == null){
+            throw new IllegalArgumentException("null은 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateEmpty(String input){
+        if(input.isEmpty()){
+            throw new IllegalArgumentException("공백 문자열은 입력할 수 없습니다.");
+       }
     }
 }
