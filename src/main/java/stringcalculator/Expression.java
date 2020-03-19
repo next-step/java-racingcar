f7d4902fb0631a9ae5ccec965e5c72c0daa33021
jwee0330 <jwee0330@gmail.com>
2020-03-19 20:43:11 +0900

    test: 사용자 입력값 받기

---
+++ src/main/java/stringcalculator/Expression.java
@@ -0,0 +1,13 @@
+package stringcalculator;
+
+public class Expression {
+    private final String source;
+
+    public Expression(String source) {
+        this.source = source;
+    }
+
+    public String getSource() {
+        return source;
+    }
+}
--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -1,10 +1,19 @@
 package stringcalculator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculator {
 
-    @Test
-    public void init() {
+    @DisplayName("사용자 입력 받기")
+    @ParameterizedTest
+    @ValueSource(strings = {"2 + 3 * 4 / 2"})
+    public void init(String source) {
+        Expression expression = new Expression(source);
+
+        assertThat(expression.getSource()).isEqualTo(source);
     }
 }
