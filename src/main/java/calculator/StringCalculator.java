1e7f41a7aa6271da785fde78d12ab841ed9f7ae6
ohtaeg <otk1090@naver.com>
2020-03-19 17:31:58 +0900

    feat (operator check) : 사칙 연산에 맞는 연산자 체크하는 메서드 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -11,12 +11,18 @@ public class StringCalculator {
 
         String[] source = toStringArray(input);
         Stack<Number> operandStack = new Stack<>();
+        Stack<String> operatorStack = new Stack<>();
         int result = 0;
 
         for (String value : source) {
             // 피연산자
             if (isNumber(value)) {
                 operandStack.push(Number.intValueOf(value));
+            }
+
+            // 연산자
+            if (isOperator(value)) {
+                operatorStack.push(value);
             }
         }
         return result;
@@ -39,5 +45,9 @@ public class StringCalculator {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    public boolean isOperator(final String value) {
+        return "+".equals(value) || "-".equals(value) || "*".equals(value) || "/".equals(value);
     }
 }
