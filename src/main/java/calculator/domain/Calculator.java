76e49e6aae9b45a98854539a2196ebb0f6bd19ee
parkeeseul <eeseul.p@gmail.com>
2020-03-19 23:47:19 +0900

    feat: 문자열 계산기 추가

---
+++ src/main/java/calculator/domain/Calculator.java
@@ -0,0 +1,50 @@
+package calculator.domain;
+
+import java.util.Objects;
+
+public class Calculator {
+    public int calculate(String expression) {
+        validValue(expression);
+
+        String[] value = expression.split(" ");
+
+        int result = Integer.valueOf(value[0]);
+
+        boolean isOperationSymbol = true;
+
+        for (int i = 1; i < value.length - 1; i++) {
+            if (isOperationSymbol) {
+                result = apply(value[i], result, Integer.valueOf(value[i + 1]));
+
+                isOperationSymbol = false;
+                continue;
+            }
+
+            isOperationSymbol = true;
+        }
+
+        return result;
+    }
+
+    public int apply(String symbol, int x, int y) {
+        switch (symbol) {
+            case "+":
+                return Operator.addition(x, y);
+            case "-":
+                return Operator.subtraction(x, y);
+            case "*":
+                return Operator.multiplication(x, y);
+            case "/":
+                return Operator.division(x, y);
+            default: {
+                throw new IllegalArgumentException("올바르지 않은 symbol");
+            }
+        }
+    }
+
+    private void validValue(String value) {
+        if(Objects.isNull(value) || value.trim().isEmpty()) {
+            throw new IllegalArgumentException("null 혹은 빈 값");
+        }
+    }
+}
---
+++ src/main/java/calculator/domain/Operator.java
@@ -0,0 +1,19 @@
+package calculator.domain;
+
+public class Operator {
+    public static int addition(int x, int y) {
+        return x + y;
+    }
+
+    public static int subtraction(int x, int y) {
+        return x - y;
+    }
+
+    public static int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    public static int division(int x, int y) {
+        return x / y;
+    }
+}
