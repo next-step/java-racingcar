4bb4569c73433d8699d00971772c47dcfabd782a
Yeoseon Yun <devyyskr@gmail.com>
2020-03-19 19:40:31 +0900

    feat(step2): 사칙연산 테스트 메소드 작성

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -2,19 +2,19 @@ package step2;
 
 public class StringCalculator {
 
-    int add(int a, int b) {
+    static int add(int a, int b) {
         return a + b;
     }
 
-    int subtract(int a, int b) {
+    static int subtract(int a, int b) {
         return a - b;
     }
 
-    int multiple(int a, int b) {
+    static int multiple(int a, int b) {
         return a * b;
     }
 
-    int divide(int a, int b) {
+    static int divide(int a, int b) {
         return a / b;
     }
 }
--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -1,6 +1,40 @@
 package step2;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringCalculatorTest {
 
 
+    @Test
+    @DisplayName("덧셈 테스트")
+    void add() {
+        assertThat(StringCalculator.add(2, 3)).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("뺄셈 테스트")
+    void subtract() {
+        assertThat(StringCalculator.subtract(2, 3)).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("곱셈 테스트")
+    void multiple() {
+        assertThat(StringCalculator.multiple(2, 3)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("나눗셈 테스트")
+    void divide() {
+        assertThat(StringCalculator.divide(4, 2)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("나눗셈 결과 infinity 값이 나올 경우의 예외처리 테스트")
+    void divideException() {
+
+    }
 }
