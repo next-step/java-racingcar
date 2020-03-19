730c6100604af7502ceb148a596400fd8a866045
yohan <yohan.lee@uneedcomms.com>
2020-03-19 23:08:57 +0900

    feat: 입력 값이 null, 공백 문자 일 경우 예외 발생 로직 구현

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -1,8 +1,18 @@
 package calculator;
 
+
+import org.junit.platform.commons.util.StringUtils;
+
 public class Calculator {
 
     public long calculate(String input) {
+        verifyInput(input);
         return 0;
+    }
+
+    private void verifyInput(String input) {
+        if (StringUtils.isBlank(input)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
