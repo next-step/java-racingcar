4362fdd9f90cebf4b5ec86ddb065d6a796c168cb
ohtaeg <otk1090@naver.com>
2020-03-19 15:01:00 +0900

    feat(empty check) : 계산기 클래스 추가 및 입력값 validation 추가

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class StringCalculator {
+    public int calculate(String input) {
+        if (input == null || input.equals(" ")) {
+            throw new IllegalArgumentException("input value ");
+        }
+        return 0;
+    }
+}
