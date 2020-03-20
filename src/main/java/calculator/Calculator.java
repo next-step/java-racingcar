a8796befa42a9f27103a9d8f66b0424e5f13ea43
isntyet <isntyet@gmail.com>
2020-03-20 20:45:38 +0900

    feat(step2):  잘못된 수식 체크
    숫자 또는 사칙연산자가 들어가야하는 자리에 올바르지 않은 문자가 들어왔을때 exception처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,46 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
+
+    private final static String FORMULA_SEPARATION_KEYWROD = " ";
+    private final static String[] OPERATOR_KEWORDS = {"+", "-", "*", "/"};
 
     private String[] formula;
 
     public Calculator(String input) {
-        validationEmptyWithFormula(input);
+        validationEmptyWithInput(input);
+        this.formula = validationInvalidFormulaWithInput(input);
     }
 
-    private void validationEmptyWithFormula(String input) {
+    private String[] validationInvalidFormulaWithInput(String input){
+        String[] splitInput = input.split(FORMULA_SEPARATION_KEYWROD);
+        for (int i = 1; i <= splitInput.length; i++) {
+            if (i % 2 == 1) {
+                isNumber(splitInput[i - 1]);
+                continue;
+            }
+            isOperator(splitInput[i - 1]);
+        }
+        return splitInput;
+    }
+
+    private void isOperator(String inputWord) {
+        if (!Arrays.stream(OPERATOR_KEWORDS).anyMatch(inputWord::equals)) {
+            throw new IllegalArgumentException("수식 중 사칙연산 기호가 잘못되었습니다.");
+        }
+    }
+
+    private void isNumber(String inputWord) {
+        try {
+            double number = Double.parseDouble(inputWord);
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("수식 중 숫자가 잘못되었습니다.");
+        }
+    }
+
+    private void validationEmptyWithInput(String input) {
         if (input == null || "".equals(input.trim())) {
             throw new IllegalArgumentException("수식이 비워져 있습니다.");
         }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
@@ -11,9 +12,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class CalculatorTest {
 
     @ParameterizedTest
+    @DisplayName("잘못된 수식일때 IllegalArgumentException 테스트")
+    @ValueSource(strings = {"1 $ 2 - 3", "1 / 2 # 3", "1 ! 2 @ 3", "1 > 2"})
+    void validationInvalidFormulaWithInputTest(String input) {
+        assertThatThrownBy(() -> {
+                    Calculator calculator = new Calculator(input);
+                }
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
     @DisplayName("입력 값이 null이거나 빈 공백 문자일때 IllegalArgumentException 테스트")
     @MethodSource("blankStrings")
-    void validationEmptyWithFormula(String input) {
+    void validationEmptyWithInputTest(String input) {
         assertThatThrownBy(() -> {
                     Calculator calculator = new Calculator(input);
                 }
