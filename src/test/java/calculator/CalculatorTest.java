421c0c5803d11c10e20e2461c8262bd7ebb0f278
kiyeoneee <kyeoneee@gmail.com>
2020-03-20 20:44:19 +0900

    feat: 계산 테스트 추가 * 계산 불가능한 경우 exception 확인 테스트 * 일반적인 연산 우선순위가 아닌 순차적 연산을 하는지 확인 테스트

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,8 +17,8 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"", " "})
-    @DisplayName("입력 값이 null 이거나 빈 공백 문자일 경우 IllegalArgumentException throw")
+    @ValueSource(strings = {"", " "})
+    @DisplayName("입력 값이 null 이거나 빈 공백 문자일 경우 IllegalArgumentException throw 테스트")
     public void emptyInputThrowExceptionTest(String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             calculator.splitInput(input);
@@ -25,8 +26,17 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1 % 2", "1 | 2"})
-    @DisplayName("입력 값이 사칙연산 기호가 아닌 경우 IllegalArgumentException throw")
+    @ValueSource(strings = {"a + 3"})
+    @DisplayName("입력 값이 숫자가 아닌 문자가 있는 경우 IllegalArgumentException throw 테스트")
+    public void IllegalValueInputThrowExceptionTest(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            calculator.splitInput(input);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1 % 2", "1 | 2"})
+    @DisplayName("입력 값이 사칙연산 기호가 아닌 경우 IllegalArgumentException throw 테스트")
     public void illegalOperatorInputThrowExceptionTest(String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             calculator.splitInput(input);
@@ -65,5 +75,20 @@ public class CalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 2 * 3=9", "4 / 2 + 1 * 0=0"}, delimiter = '=')
+    @DisplayName("일반적인 연산 우선순위를 따르는 게 아닌 순차적 연산 여부 테스트")
+    public void CalculateTest(String input, int expected) {
+        int result = calculator.calculate(input);
+        assertThat(result).isEqualTo(expected);
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1 / 0 + 3"})
+    @DisplayName("연산이 불가능 한 경우 exception throw 여부 테스트")
+    public void calculatorThrowExceptionTest(String input) {
+        assertThatExceptionOfType(CalculateException.class).isThrownBy(() -> {
+            calculator.splitInput(input);
+        });
+    }
 }
