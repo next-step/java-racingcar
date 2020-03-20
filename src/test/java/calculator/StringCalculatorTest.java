95daaf910da5ca8cc679ecc1bb5ceee675ca1aca
etff <wish2rich@gmail.com>
2020-03-21 01:08:58 +0900

    style (formatting)

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -25,7 +25,7 @@ class StringCalculatorTest {
         assertThat(calculator.calculate(textInput)).isEqualTo(6);
     }
 
-    @DisplayName(value = "숫자 두개에 뺄셈(-)을 입력할 경우 두 숫자의 합을 반환한다.")
+    @DisplayName(value = "숫자 두개에 뺄셈(-)을 입력할 경우 두 숫자의 차를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"4 - 2"})
     void minus(final String textInput) {
@@ -39,23 +39,27 @@ class StringCalculatorTest {
         assertThat(calculator.calculate(textInput)).isEqualTo(8);
     }
 
-    @DisplayName(value = "숫자 두개에 나눗셈(/)을 입력할 경우 두 숫자의 나눈을 반환한다.")
+    @DisplayName(value = "숫자 두개에 나눗셈(/)을 입력할 경우 두 숫자의 나눈값을 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"4 / 2"})
-    void divide(final String textInput) { assertThat(calculator.calculate(textInput)).isEqualTo(2);
+    void divide(final String textInput) {
+        assertThat(calculator.calculate(textInput)).isEqualTo(2);
     }
 
     @DisplayName(value = "숫자를 0으로 나누었을때 IllegalArgumentException 이 발생")
     @ParameterizedTest
     @ValueSource(strings = {"4 / 0"})
-    void divideByZero(final String textInput) {  assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> calculator.calculate(textInput));;
+    void divideByZero(final String textInput) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> calculator.calculate(textInput));
+        ;
     }
 
     @DisplayName(value = "복합적으로 들어있는 수식의 결과를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"2 + 3 * 4 / 2"})
-    void calculateAll(final String textInput) { assertThat(calculator.calculate(textInput)).isEqualTo(10);
+    void calculateAll(final String textInput) {
+        assertThat(calculator.calculate(textInput)).isEqualTo(10);
     }
 
     @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 IllegalArgumentException 이 발생")
