bafa923313540b4252fa3666004937e5286eeda0
kwanguuuu <kwangyu326@gmail.com>
2020-03-20 15:02:55 +0900

    separator 를 상수로 변경 + for안에 step도 상수로 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,19 +1,15 @@
 package calculator;
 
 public class Calculator {
-    InputValidator validator;
-    public int sum;
-
-    public Calculator() {
-        this.validator = new InputValidator();
-        this.sum = 0;
-    }
+    public static final int MOVE_STEP = 2;
+    private static final String SEPARATOR = " ";
+    private InputValidator validator = new InputValidator();
 
     public int calculate(String inputs) {
         validator.nullCheck(inputs);
-        String[] inputArray = inputs.split(" ");
+        String[] inputArray = inputs.split(SEPARATOR);
         int sum = parseInt(inputArray[0]);
-        for (int i = 1; i < inputArray.length; i = i + 2) {
+        for (int i = 1; i < inputArray.length; i = i + MOVE_STEP) {
             sum = operate(sum, inputArray[i], inputArray[i + 1]);
         }
         return sum;
