bd3dcb5c151072478350f86cf775f2dca79247cf
Sooduck <psd177@gmail.com>
2020-03-21 00:19:44 +0900

    feat add Calculator

--- src/test/java/stringCalculator/CalculatorTest.java
+++ src/test/java/stringCalculator/CalculatorTest.java
@@ -1,6 +1,5 @@
 package stringCalculator;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -12,23 +11,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CalculatorTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"2 + 3 + 2:3 + 2"}, delimiter = ':')
+    @CsvSource(value = {"2 + 3 + 2:5.0 + 2", "1 / 2 + 2:0.5 + 2"}, delimiter = ':')
     void assembleFormulaTest(String input, String expected) {
         Calculator calulator = new Calculator(input);
         ArrayList<String> expectedList = new ArrayList<>(Arrays.asList(expected.split(" ")));
 
-        ArrayList<String> formula = calulator.assembleFormula(inputStringList);
+        ArrayList<String> formula = calulator.assembleFormula();
 
-        assertThat(formula.toString()).isEqualTo(expectedList);
+        assertThat(formula).isEqualTo(expectedList);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"2 + 3 * 4 / 2:10.0"}, delimiter = ':')
+    @CsvSource(value = {"2 + 3 * 4 / 2:10.0", "7 / 14 * 7 + 7:10.5", "9 / 3 * 2 * 5:30.0"}, delimiter = ':')
     void workTest(String input, String expected) {
         Calculator calulator = new Calculator(input);
 
         Double result = calulator.work();
 
-        assertThat(result).isEqualTo(expected);
+        assertThat(result).isEqualTo(Double.valueOf(expected));
     }
 }
