33dc2630786027bde78f532db20c43ad0f15f06c
hongjun <hjdeepsleep@gmail.com>
2020-03-20 10:53:27 +0900

    Step1 (#546)
    * split study test
    
    * substring study test
    
    * split study test
    
    * substring study test
    
    * step1
    
    set test 미완료
    
    * step1 마무리
    
    * step1 마무리

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,34 +4,52 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
     @Test
+    @DisplayName("요구사항1 : split() 테스트")
     void split() {
         String[] result = "1,2".split(",");
+
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
         assertThat(result).containsExactly("1", "2");
     }
 
+    @DisplayName("요구사항2 : substring() 테스트")
     @Test
     void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
     }
 
-
-    @DisplayName("charAt() 테스트")
+    @DisplayName("요구사항3 : charAt() 테스트")
     @Test
     void charAt() {
-        char result = "abc".charAt(2);
-        assertThat(result).isEqualTo('c');
+        char result1 = "abc".charAt(1);
+        char result2 = "abc".charAt(2);
+        assertThat(result1).isEqualTo('b');
+        assertThat(result2).isEqualTo('c');
 
         assertThatThrownBy(() -> {
-            char result2 = "abc".charAt(5);
+            "abc".charAt(5);
         }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 
+    @DisplayName("요구사항3 : charAt() 테스트, assertThatExceptionOfType 활용")
+    @Test
+    void charAt2() {
+        char result1 = "abc".charAt(1);
+        char result2 = "abc".charAt(2);
+        assertThat(result1).isEqualTo('b');
+        assertThat(result2).isEqualTo('c');
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    "abc".charAt(5);
+                }).withMessageContaining("index");
+    }
 
 }
