3cc3c72c2298ae3fb6a138d6c140790710465642
teahyuk <jtaeh57@gmail.com>
2020-03-20 17:15:18 +0900

    add(Step3) : 최종 view 클래스 들과 메일 로직 을 합친 레이싱 게임 구현
    최종 진입점 개발 :
      - RacingGamePlayer 추가.
      - main 함수 작성
      - 최종 구현.

--- src/main/java/racingGame/InputView.java
+++ src/main/java/racingGame/InputView.java
@@ -6,21 +6,22 @@ public class InputView {
 	private static String GET_COUNT_OF_CARS_MSG = "자동차 대수는 몇 대 인가요?";
 	private static String GET_TIMES_OF_CARS_GO_MSG = "시도할 회수는 몇 회 인가요?";
 
-	private String value;
-	private int number;
+	private int carCount;
+	private int racingTime;
 
 	public void startInput(){
 		Scanner scanner = new Scanner(System.in);
-		System.out.println();
-		String value = scanner.nextLine();
-		int number = scanner.nextInt();
+		System.out.println(GET_COUNT_OF_CARS_MSG);
+		carCount = scanner.nextInt();
+		System.out.println(GET_TIMES_OF_CARS_GO_MSG);
+		racingTime = scanner.nextInt();
 	}
 
-	public String getValue() {
-		return value;
+	public int getCarCount() {
+		return carCount;
 	}
 
-	public int getNumber() {
-		return number;
+	public int getRacingTime() {
+		return racingTime;
 	}
 }
