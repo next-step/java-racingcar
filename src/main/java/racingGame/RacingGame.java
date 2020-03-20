031698b17d0a1039b28a9faa0057a89a4f7520f8
teahyuk <jtaeh57@gmail.com>
2020-03-20 16:20:53 +0900

    test(Step3) : 최초 테스트 추가
    기본 기능을 테스트 하기 위한 테스트 코드 추가 :
      - 한번 움직였을 때의 자동차 위치 확인 테스트
      - 몇번 움직였을 때의 얼마나 움직였는지 체크하는 테스트

---
+++ src/main/java/racingGame/RacingGame.java
@@ -0,0 +1,20 @@
+package racingGame;
+
+public class RacingGame {
+	private int time;
+	private int[] carPositions;
+
+	public RacingGame(int cars) {
+		this.carPositions = new int[cars];
+	}
+
+	public int[] move() {
+		// TODO 구현
+		return null;
+	}
+
+	public int getTime(){
+		return time;
+	}
+
+}
---
+++ src/test/java/racingGame/RacingGameTest.java
@@ -0,0 +1,46 @@
+package racingGame;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingGameTest {
+
+	private int carCount = 3;
+	private RacingGame racingGame;
+
+	@BeforeEach
+	void setting(){
+		racingGame = new RacingGame(carCount);
+	}
+
+	@Test
+	@DisplayName("한번 움직였을 때의 결과값 테스트")
+	void moveTest(){
+		assertThat(racingGame.move())
+				.containsAnyOf(0,1);
+		assertThat(racingGame.getTime())
+				.isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("여러번 움직였을 때의 움직인 횟수 확인 테스트")
+	void moveCountTest(){
+
+		int moveTime = 5;
+
+		for (int i = 0; i < moveTime; i++) {
+			racingGame.move();
+		}
+
+		assertThat(racingGame.getTime())
+				.isEqualTo(1);
+	}
+
+}
