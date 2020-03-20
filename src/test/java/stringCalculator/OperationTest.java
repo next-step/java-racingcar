cbe16a88039432f3f3c796cd94ad073ee00113aa
Sooduck <psd177@gmail.com>
2020-03-20 22:35:33 +0900

    test: add Operation

--- src/test/java/stringCalculator/OperationTest.java
+++ src/test/java/stringCalculator/OperationTest.java
@@ -3,10 +3,33 @@ package stringCalculator;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class OperationTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2 % 3", "5 @ 2", "2 2 6", "6 ! 2"})
+    void operationExceptionTest(String input) {
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            new Formula(Arrays.asList(input.split(" "))).operate();
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2 + 3:5", "5 - 2:3", "2 * 6:12", "6 / 2:3", "1 / 2:0.5"}, delimiter = ':')
+    void calculateTest(String input, String expected) {
+        Formula fomula = new Formula(Arrays.asList(input.split(" ")));
+
+        Double result = fomula.calculate();
+
+        assertThat(result).isEqualTo(Double.parseDouble(expected));
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"2 4:6", "5 9:14"}, delimiter = ':')
