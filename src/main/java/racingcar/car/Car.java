8eb49bb8134a97401b8a944da3b5a301e056635d
dongchul <sah3122@naver.com>
2020-03-22 01:48:47 +0900

    feat(RacingCar): add Car name & clone

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,7 +1,27 @@
 package racingcar.car;
 
+import java.util.Objects;
+
 public class Car {
+    private String name;
     private int position;
+
+    public Car(String name) {
+        this(name, 0);
+    }
+
+    private Car(String name, int position) {
+        validate(name);
+
+        this.name = name;
+        this.position = position;
+    }
+
+    private void validate(String name) {
+        if (Objects.isNull(name) || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름이 잘못 되었습니다.");
+        }
+    }
 
     public void move(MovingStrategy movingStrategy) {
         if (movingStrategy.isMovable()) {
@@ -9,7 +29,15 @@ public class Car {
         }
     }
 
+    public Car clone() {
+        return new Car(name, position);
+    }
+
     public int getPosition() {
         return position;
+    }
+
+    public String getName() {
+        return name;
     }
 }
