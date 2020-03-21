d63b3a56f4fa9ae0e7c2a9b470e66cb2c277da67
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-21 10:43:12 +0900

    feat : step2 리뷰 대응
                            - @NullAndEmptySource 이용

--- src/test/java/calculator/InputVoTest.java
+++ src/test/java/calculator/InputVoTest.java
@@ -1,9 +1,10 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,19 +12,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class InputVoTest {
 
     @DisplayName("null, 공백 체크")
-    @Test
-    void validateNull() {
-        //given
-        String test1 = "";
-        String test2 = " ";
-        String test3 = null;
-
-        //when
-        //then
+    @NullAndEmptySource
+    @ValueSource(strings = {" "})
+    void validateNull(String input) {
         assertThatThrownBy(() -> {
-            new InputVo(test1);
-            new InputVo(test2);
-            new InputVo(test3);
+            new InputVo(input);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
