3f07ba33b2d840ee8b23a8f499c2c7413bbeeea5
yohan <yohan.lee@uneedcomms.com>
2020-03-21 19:07:53 +0900

    test: 경주를 시작한다는 메시지를 받고 조건(랜덤)을 받아아 전진기능을 수행하는 기능 구현

--- src/test/java/racingGame/Car.java
+++ src/test/java/racingGame/Car.java
@@ -2,7 +2,15 @@ package racingGame;
 
 public class Car {
 
+    private Condition condition = new Condition();
     private int position;
+
+    public void start(RandomNumber randomNumber) {
+        condition.init(randomNumber);
+        if (condition.isOk()) {
+            move();
+        }
+    }
 
     public void move() {
         position++;
