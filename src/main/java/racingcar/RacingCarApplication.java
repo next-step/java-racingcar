d1414e15d47fb3f46054a4c5ccf74ef863ff8771
dongchul <sah3122@naver.com>
2020-03-22 01:54:32 +0900

    fix(RacingCar): fix Racing Comment

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -11,7 +11,7 @@ public class RacingCarApplication {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요 ?");
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String names = scanner.nextLine();
 
         System.out.println("시도할 회수는 몇 회 인가요 ?");
