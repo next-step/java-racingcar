55a8bb58e45fb183f0a6be91de51aa385a37a27e
kwanguuuu <kwangyu326@gmail.com>
2020-03-21 22:27:20 +0900

    step3.
    feature : 요구사항에 정의된 클래스 생성

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class RacingGame {
+    private int time;
+    private int[] carPositions;
+
+    public int[] move() {
+        // TODO 구현
+        return null;
+    }
+}
---
+++ src/main/java/racingcar/view/ResultView.java
@@ -0,0 +1,5 @@
+package racingcar.view;
+
+public class ResultView {
+    private static final String VIEW = "-";
+}
