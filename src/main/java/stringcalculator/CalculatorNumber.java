ceb6a625b2c45892e7867bb63ff7947e12c7ff60
dongchul <sah3122@naver.com>
2020-03-22 01:48:05 +0900

    refactor(StringCalculator): move calculate to OperatorType

--- src/main/java/stringcalculator/CalculatorNumber.java
+++ src/main/java/stringcalculator/CalculatorNumber.java
@@ -2,11 +2,10 @@ package stringcalculator;
 
 import java.util.Objects;
 
-import static stringcalculator.OperatorType.*;
+import static stringcalculator.OperatorType.createOperator;
 
 public class CalculatorNumber {
     private static final String OPERATOR_GROUP = "+-*/";
-    private static final String ZERO_STRING = "0";
     private static final int NEXT_INDEX = 1;
 
     private String[] inputDatas;
@@ -22,24 +21,9 @@ public class CalculatorNumber {
     public double calculate() {
         validateOperator();
         String operator = findOperator();
+        OperatorType operatorType = createOperator(operator);
 
-        if (PLUS_OPERATOR.isEqual(operator)) {
-            return plus();
-        }
-
-        if (MINUS_OPERATOR.isEqual(operator)) {
-            return minus();
-        }
-
-        if (MULTIPLY_OPERATOR.isEqual(operator)) {
-            return multiply();
-        }
-
-        if (DIVIDE_OPERATOR.isEqual(operator)) {
-            return divide();
-        }
-
-        throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
+        return operatorType.calculate(sum, nextNumber());
     }
 
     private String findOperator() {
@@ -54,25 +38,6 @@ public class CalculatorNumber {
         if (!OPERATOR_GROUP.contains(findOperator())) {
             throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
         }
-    }
-
-    private double plus() {
-        return sum + nextNumber();
-    }
-
-    private double minus() {
-        return sum - nextNumber();
-    }
-
-    private double multiply() {
-        return sum * nextNumber();
-    }
-
-    private double divide() {
-        if (inputDatas[index + NEXT_INDEX].equals(ZERO_STRING)) {
-            throw new IllegalArgumentException("0 으로 나눌수 없습니다.");
-        }
-        return sum / nextNumber();
     }
 
     private double nextNumber() {
