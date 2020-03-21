523842c290b7ceab64236614ec0e28522b9e9c49
gale34 <gales34@gmail.com>
2020-03-21 23:16:09 +0900

    feat: add RacingGame, RacingGameResult, edit RacingGameTests

--- src/test/java/RacingCarGameTests/RacingGameTests.java
+++ src/test/java/RacingCarGameTests/RacingGameTests.java
@@ -1,12 +1,14 @@
 package RacingCarGameTests;
 
-import org.assertj.core.api.Condition;
+import domain.RacingGameResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import domain.RacingGame;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -17,7 +19,7 @@ public class RacingGameTests {
     @ParameterizedTest
     @CsvSource({"5,3", "2,6", "7,4"})
     public void generateRacingGameTest(int carNumber, int roundNumber) {
-        assertThatCode(() -> RacingGame.newInstance(carNumber, roundNumber)).doesNotThrowAnyException();
+        assertThatCode(() -> RacingGame.create(carNumber, roundNumber)).doesNotThrowAnyException();
     }
 
     @DisplayName("레이싱 게임 생성 테스트 - 비정상 케이스")
@@ -26,7 +28,7 @@ public class RacingGameTests {
     @CsvSource({"-1,3", "-2,-3", "0,4"})
     public void generateRacingGameAbnormalCasesTest(int carNumber, int roundNumber) {
         assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> RacingGame.newInstance(carNumber, roundNumber))
+                .isThrownBy(() -> RacingGame.create(carNumber, roundNumber))
                 .withMessageMatching("car number and round number must be greater than zero.");
     }
 
@@ -35,13 +37,18 @@ public class RacingGameTests {
     @NullAndEmptySource
     @CsvSource({"4,1", "5,0", "7,4"})
     public void startRacingGameTest(int carNumber, int roundNumber) {
-        RacingGame racingGame = RacingGame.newInstance(carNumber, roundNumber);
+        RacingGame racingGame = RacingGame.create(carNumber, roundNumber);
         RacingGameResult racingGameResult = racingGame.start();
-        int[] positionResult = racingGameResult.toArray();
+        List<List<Integer>> positionResult = racingGameResult.getResult();
 
-        assertThat(positionResult).hasSize(carNumber);
-        Arrays.stream(positionResult)
-                .forEach(number -> assertThat(number).isGreaterThanOrEqualTo(0));
+        assertThat(positionResult).hasSize(roundNumber);
+        positionResult.stream()
+                .forEach(round -> assertRound(round));
+    }
+
+    private void assertRound(List<Integer> round) {
+        round.stream()
+                .forEach(position -> assertThat(position).isGreaterThanOrEqualTo(0));
     }
 
 }
