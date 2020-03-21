5fca87d67292c6d66e084be3333818bef4b8f35c
kwanguuuu <kwangyu326@gmail.com>
2020-03-22 00:01:25 +0900

    step3
    feature : 입력값에 대한 exception 처리

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -6,6 +6,7 @@ public class RacingCarApplication {
     public static void main(String[] args) {
         int carNumber = InputView.carNumber();
         int moveCount = InputView.moveCount();
+        InputView.inputClose();
 
         RacingGame racing = new RacingGame(carNumber,moveCount);
         racing.racing();
