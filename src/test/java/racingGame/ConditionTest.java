d985c4c80774874c9f345b964e5b82ad528d9d0f
yohan <yohan.lee@uneedcomms.com>
2020-03-21 18:06:48 +0900

    test: Mock 테스트를 위한 mockito 의존성 추가 & Condition init() 테스트 구현
    - 기능 구현 목록 수정 및 테스트 코드 설명 수정

--- src/test/java/racingGame/ConditionTest.java
+++ src/test/java/racingGame/ConditionTest.java
@@ -4,23 +4,32 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ConditionTest {
+
+    @Mock
+    private RandomNumber randomNumber;
 
     private Condition condition;
 
     @BeforeEach
     void setUp() {
         condition = new Condition();
+        randomNumber = mock(RandomNumber.class);
+        assertThat(randomNumber).isNotNull();
     }
 
     @ParameterizedTest
     @CsvSource(value = {"2:false", "4:true", "9:true", "3:false"}, delimiter = ':')
-    @DisplayName("랜덤한 숫자를 입력으로 Condition 의 상태를 체크 (기준 숫자: 4 )")
-    void initStatus(int input, boolean expected) {
-        condition.init(input);
+    @DisplayName("랜덤 값을 받고 자동차가 전진가능한 여부를 판단하는 기능 구현")
+    void init(int input, boolean expected) {
+        when(randomNumber.generate()).thenReturn(input);
+        condition.init(randomNumber);
 
         assertThat(condition.isOk()).isEqualTo(expected);
     }
