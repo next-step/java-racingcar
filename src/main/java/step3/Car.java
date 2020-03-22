fa3763d0ea8629e710370ee4d0e6705876923a7b
Yeoseon Yun <devyyskr@gmail.com>
2020-03-22 14:44:56 +0900

    feat(Car): Car별 회차 정보를 관리하는 메소드 Car 클래스에 추가

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -1,6 +1,16 @@
 package step3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
+    private List<Round> roundInfoList = new ArrayList<Round>();
 
+    public List<Round> getRoundInfoList() {
+        return this.roundInfoList;
+    }
 
+    private void setRoundInfoList(List<Round> roundInfoList) {
+        this.roundInfoList = roundInfoList;
+    }
 }
