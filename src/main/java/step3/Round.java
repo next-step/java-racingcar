1877b66e1aa84254e26f372b605a8f9a216cf229
Yeoseon Yun <devyyskr@gmail.com>
2020-03-22 14:49:43 +0900

    add(ValidationUtils): Validation 관련 설계 내용 추가 및 Validation 클래스 추가

--- src/main/java/step3/Round.java
+++ src/main/java/step3/Round.java
@@ -1,6 +1,22 @@
 package step3;
 
 public class Round {
+    private int time;               // 회차 정보
+    private int randomNumber;       // 회차별 랜덤 수
 
+    public int getTime() {
+        return time;
+    }
 
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
 }
