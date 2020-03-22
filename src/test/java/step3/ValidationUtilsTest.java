24c2819532952a2d32940bbc3bb939451824bb9b
Yeoseon Yun <devyyskr@gmail.com>
2020-03-23 08:51:14 +0900

    test(ValidationUtilsTest): 0이하인 경우 검증 테스트 메소드 추가

--- src/test/java/step3/ValidationUtilsTest.java
+++ src/test/java/step3/ValidationUtilsTest.java
@@ -8,12 +8,22 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 public class ValidationUtilsTest {
 
     @Test
-    @DisplayName("숫자가 0보다 작거나 9보다 클 경우의 예외 처리 테스트")
-    void isSingleDigitNumber() {
-        int randomValue = 10;
+    @DisplayName("입력받은 숫자가 0 이하인 경우 예외 처리 테스트")
+    void isBiggerThanZero() {
+        int value = 0;
 
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            ValidationUtils.isSingleDigitNumber(randomValue);
+            ValidationUtils.isBiggerThanZero(value);
+        });
+    }
+
+    @Test
+    @DisplayName("숫자가 0보다 작거나 9보다 클 경우의 예외 처리 테스트")
+    void isSingleDigitNumber() {
+        int value = 10;
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            ValidationUtils.isSingleDigitNumber(value);
         });
     }
 }
