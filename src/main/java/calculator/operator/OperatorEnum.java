24aa8dcdcb376e0e8d066319e0b27d7202368a7e
isntyet <isntyet@gmail.com>
2020-03-23 01:17:02 +0900

    refactor(step2):  Stream API를 활용
    반복문을 라이브러리 내부에 위임하고, 필터 조건/매핑/예외처리 방식 적극 노출

--- src/main/java/calculator/operator/OperatorEnum.java
+++ src/main/java/calculator/operator/OperatorEnum.java
@@ -1,5 +1,7 @@
 package calculator.operator;
 
+import java.util.Arrays;
+
 public enum OperatorEnum {
 
     PLUS("+", (operand1, operand2) -> operand1 + operand2),
@@ -30,20 +32,9 @@ public enum OperatorEnum {
     }
 
     public static OperatorEnum getOperatorEnumFromKeyword(String operatorKeyword) {
-        for (OperatorEnum operator : OperatorEnum.values()) {
-            if (operator.getKeyword().equals(operatorKeyword)) {
-                return operator;
-            }
-        }
-        throw new IllegalArgumentException("수식 중 사칙연산 기호가 잘못되었습니다.");
-    }
-
-    public static boolean isKeyword(String operatorKeyword) {
-        for (OperatorEnum operator : OperatorEnum.values()) {
-            if (operator.getKeyword().equals(operatorKeyword)) {
-                return true;
-            }
-        }
-        return false;
+        return Arrays.stream(values())
+                .filter(operator -> operator.getKeyword().equals(operatorKeyword))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("수식 중 사칙연산 기호가 잘못되었습니다."));
     }
 }
