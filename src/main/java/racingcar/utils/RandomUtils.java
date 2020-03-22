b8e78018d5417bc2c79c521f69f9be9eb2e3c8f6
parkeeseul <eeseul.p@gmail.com>
2020-03-22 14:50:37 +0900

    refactor: RandomUtils private 생성자 추가

--- src/main/java/racingcar/utils/RandomUtils.java
+++ src/main/java/racingcar/utils/RandomUtils.java
@@ -3,6 +3,10 @@ package racingcar.utils;
 import java.util.Random;
 
 public class RandomUtils {
+    private RandomUtils() {
+        throw new AssertionError();
+    }
+
     public static int random(int randomBound) {
         Random random = new Random();
         return random.nextInt(randomBound);
