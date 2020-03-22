92a2353938e13ee2978812a4bc3893de3d73ac53
kks <rudtjs9956@gmail.com>
2020-03-22 21:30:54 +0900

    feat: 자동차수, 진행할 라운드 수 입력기능 추가

---
+++ src/main/java/AutoRacing.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AutoRacing {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        GameRole gameRole = inputView.getGameRole();
+    }
+
+
+}
---
+++ src/main/java/GameRole.java
@@ -0,0 +1,10 @@
+public class GameRole {
+
+    private int numberOfCars;
+    private int numberOfAttempts;
+
+    public GameRole(int numberOfCars, int numberOfAttempts) {
+        this.numberOfCars = numberOfCars;
+        this.numberOfAttempts = numberOfAttempts;
+    }
+}
