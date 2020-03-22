0d96e95e065fb5576d94c798fa9453ec26a4e353
jwee0330 <jwee0330@gmail.com>
2020-03-22 16:26:14 +0900

    feat(racingcar): Cars 는 소유한 모든 Car 들을 이동시킬 수 있다

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,44 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
 public class Cars {
+    private final List<Car> cars;
+
+    public Cars(int numberOfCars) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < numberOfCars; i++) {
+            cars.add(new Car());
+        }
+        this.cars = cars;
+    }
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public Cars moveAllCar() {
+        List<Car> carResults = new ArrayList<>();
+        for (int carNumber = 0; carNumber < cars.size(); carNumber++) {
+            int randomValue = new Random().nextInt(RacingGameConstant.RANDOM_LIMIT);
+            carResults.add(cars.get(carNumber).move(new MoveStrategy(randomValue)));
+        }
+        return new Cars(carResults);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cars)) return false;
+        Cars cars1 = (Cars) o;
+        return Objects.equals(cars, cars1.cars);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cars);
+    }
 }
---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+
+    @DisplayName("자동차 대수를 입력하면 자동차 컬렉션을 생성할 수 있다")
+    @Test
+    public void newCarsWithNumberOfCars() {
+        //given
+        int numberOfCars = 5;
+        Cars cars = new Cars(numberOfCars);
+
+        //when & then
+        assertThat(cars).isNotNull();
+    }
+
+    @DisplayName("자동차 컬렉션은 포함하고 있는 모든 자동차를 전진시킬 수 있다")
+    @Test
+    public void makeAllCarsToMove() {
+        //given
+        int numberOfCars = 5;
+        Cars cars = new Cars(numberOfCars);
+
+        //when & then
+        assertThat(cars.moveAllCar()).isNotNull();
+    }
+
+}
