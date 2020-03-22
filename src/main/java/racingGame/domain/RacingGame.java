0e714aa67c43a2fe8849066c1017df81cf5eb5ab
teahyuk <jtaeh57@gmail.com>
2020-03-22 18:44:06 +0900

    teset(Step5) : RacingGame 인터페이스 수정 및 TC 수정
    RacingGame TC 및 클래스 인터페이스 수정 :
      - CarTest로 인해 필요 없는 테스트 제거
      - moveTest 는 모든 차들 움직인 테스트 확인
      - getWinnerTest 는 복수의 승자들 잘 뽑는지 확인
    
    issue #569

--- src/main/java/racingGame/domain/RacingGame.java
+++ src/main/java/racingGame/domain/RacingGame.java
@@ -2,21 +2,23 @@ package racingGame.domain;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class RacingGame {
 	private static final int RANDOM_RANGE = 10;
 	private static final int GO_THRESHOLD = 4;
-	private Map<String, Integer> carPositions;
+	private List<Car> carPositions;
 
-	public RacingGame(String... racers) {
-		this.carPositions = Arrays.stream(racers)
-				.collect(Collectors.toMap(Function.identity(), racer -> 0));
+	public RacingGame(Car... cars) {
+		this.carPositions = Arrays.stream(cars)
+				.collect(Collectors.toList());
 	}
 
-	public Map<String, Integer> move() {
+	public List<Car> move() {
 		carPositions.replaceAll((r, v) -> moveCar(carPositions.get(r)));
 		return new HashMap<>(carPositions);
 	}
@@ -32,7 +34,7 @@ public class RacingGame {
 		return Math.random() * RANDOM_RANGE > GO_THRESHOLD;
 	}
 
-	public String[] getWinners() {
+	public List<String> getWinners() {
 		return carPositions.entrySet()
 				.stream()
 				.filter(racer -> racer.getValue().equals(getWinnerPosition()))
--- src/test/java/racingGame/domain/RacingGameTest.java
+++ src/test/java/racingGame/domain/RacingGameTest.java
@@ -1,6 +1,5 @@
 package racingGame.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,45 +7,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingGameTest {
 
-	private String[] racers ={"pobi","lannstark","teahyuk"};
-	private RacingGame racingGame;
+	private static final String RACER1 = "pobi";
+	private static final String RACE2 = "lannstark";
+	private static final String RACER3 = "teahyuk";
 
-	@BeforeEach
-	void setting() {
-		racingGame = new RacingGame(racers);
-	}
+	private static final Engine COMMON_ENGINE = () -> 5;
+	private static final Engine BOOST_ENGINE = () -> 10;
 
 	@Test
-	@DisplayName("한번 움직였을 때의 결과값 테스트")
+	@DisplayName("움직임 테스트")
 	void moveTest() {
-		assertThat(racingGame.move().values())
-				.containsAnyOf(0, 1);
-	}
+		RacingGame racingGame = new RacingGame(new Car(RACER1, COMMON_ENGINE),
+				new Car(RACE2, COMMON_ENGINE),
+				new Car(RACER3, COMMON_ENGINE));
 
-	@Test
-	@DisplayName("여러번 움직였을 때 매번 움직일 때 마다 1칸 전진 또는 제자리 인지 확인 하는 테스트")
-	void moveRepeatTest() {
-		int moveTime = 5;
-
-		int tmpPosition = 0;
-		for (int i = 0; i < moveTime; i++) {
-			int nextPosition = racingGame.move().get(racers[0]);
-			assertThat(nextPosition)
-					.isIn(tmpPosition,tmpPosition+1);
-			tmpPosition = nextPosition;
-		}
+		assertThat(racingGame.move())
+				.allMatch(car -> car.getDistance() == 5);
 	}
 
 	@Test
 	@DisplayName("우승자 확인 테스트")
 	void getWinnerTest() {
-		int moveTime = 5;
-		for (int i = 0; i < moveTime; i++) {
-			racingGame.move();
-		}
+		RacingGame racingGame = new RacingGame(new Car(RACER1, COMMON_ENGINE),
+				new Car(RACE2, BOOST_ENGINE),
+				new Car(RACER3, BOOST_ENGINE));
 
 		assertThat(racingGame.getWinners())
-				.containsAnyOf(racers);
+				.containsExactly(RACE2, RACER3);
+
 	}
 
 }
