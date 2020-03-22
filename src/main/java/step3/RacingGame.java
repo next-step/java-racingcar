9c503dfce3851cd5aa060c5474ba2ae1b688efa4
Yeoseon Yun <devyyskr@gmail.com>
2020-03-22 14:41:00 +0900

    feat(RacingGame): Car 대수에 따라 Car dlstmxjstm를 생성하는 메소드 RacingGame 클래스에 추가

--- src/main/java/step3/RacingGame.java
+++ src/main/java/step3/RacingGame.java
@@ -1,4 +1,26 @@
 package step3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
+    private int time;
+    private List<Car> carList = new ArrayList<Car>();
+
+    public void createCars(int carCount) {
+        List<Car> carList = new ArrayList<Car>();
+
+        for(int i = 0; i < carCount; i++) {
+            carList.add(_createCar());
+        }
+        this.carList = carList;
+    }
+
+    private Car _createCar() {
+        Car car = new Car();
+
+        return car;
+    }
 }
+
+
