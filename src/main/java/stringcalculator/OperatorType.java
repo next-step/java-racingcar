d09c956ea134caaf96ae634260985fbb43c12f11
dongchul <sorkehdcjf@gmail.com>
2020-03-22 21:00:09 +0900

    Step4 - 자동차 경주(우승자) 리뷰 요청 드립니다. (#585)
    * refactor(StringCalculator): move calculate to OperatorType
    
    * feat(RacingCar): add Car name & clone
    
    * refactor(RacingCar): add RacingScore
    
    자동자 경주의 결과를 RacingScore 클래스에 저장하여
    각 경주가 끝날때 결과를 노출하지 않고 모든 경주가 끝날시 결과를 노출 할 수 있는 구조로 변경
    
    * docs(RacingCar): add requirements
    
    * test(RacingCar): refactor tests 
    
    Car name 속성 추가에 따라 테스트 코드 수정
    
    * fix(RacingCar): fix Racing Comment

--- src/main/java/stringcalculator/OperatorType.java
+++ src/main/java/stringcalculator/OperatorType.java
@@ -1,12 +1,33 @@
 package stringcalculator;
 
-import java.util.function.Function;
-
 public enum OperatorType {
-    PLUS_OPERATOR("+"),
-    MINUS_OPERATOR("-"),
-    MULTIPLY_OPERATOR("*"),
-    DIVIDE_OPERATOR("/");
+    PLUS_OPERATOR("+") {
+        @Override
+        public double calculate(double sum, double nextNumber) {
+            return sum + nextNumber;
+        }
+    },
+    MINUS_OPERATOR("-") {
+        @Override
+        public double calculate(double sum, double nextNumber) {
+            return sum - nextNumber;
+        }
+    },
+    MULTIPLY_OPERATOR("*") {
+        @Override
+        public double calculate(double sum, double nextNumber) {
+            return sum * nextNumber;
+        }
+    },
+    DIVIDE_OPERATOR("/") {
+        @Override
+        public double calculate(double sum, double nextNumber) {
+            if (nextNumber == 0) {
+                throw new IllegalArgumentException("0 으로 나눌수 없습니다.");
+            }
+            return sum / nextNumber;
+        }
+    };
 
     private String operator;
 
@@ -14,7 +35,25 @@ public enum OperatorType {
         this.operator = operator;
     }
 
-    public boolean isEqual(String operator) {
-        return this.operator.equals(operator);
+    public static OperatorType createOperator(String operator) {
+        if(operator.equals(PLUS_OPERATOR.operator)) {
+            return PLUS_OPERATOR;
+        }
+
+        if(operator.equals(MINUS_OPERATOR.operator)) {
+            return MINUS_OPERATOR;
+        }
+
+        if(operator.equals(MULTIPLY_OPERATOR.operator)) {
+            return MULTIPLY_OPERATOR;
+        }
+
+        if(operator.equals(DIVIDE_OPERATOR.operator)) {
+            return DIVIDE_OPERATOR;
+        }
+
+        throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
     }
+
+    public abstract double calculate(double sum, double nextNumber);
 }
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -7,18 +7,20 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.car.RandomMovingStrategy;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class RacingGameTest {
     private static final int ZERO = 0;
-    private static final int NUMBER_OF_CAR = 3;
+    private static final String[] RACING_CAR_NAMES = {"ALPHA", "BETA", "CHARLIE"};
 
     @DisplayName("자동차 경주 정상 실행")
     @ParameterizedTest
     @ValueSource(ints = {3, 5, 7})
     void racingGameExecute(int numberOfTime) {
-        RacingGame racingGame = new RacingGame(NUMBER_OF_CAR, new RandomMovingStrategy());
+        RacingGame racingGame = new RacingGame(RACING_CAR_NAMES, new RandomMovingStrategy());
         for (int i = 0; i < numberOfTime; i++) {
             racingGame.execute();
         }
@@ -29,6 +31,7 @@ class RacingGameTest {
     @DisplayName("자동자 개수가 1보다 작을 시 생성 실패")
     @Test
     void createFailByNumberOfCar() {
-        assertThatIllegalArgumentException().isThrownBy(() -> new RacingGame(ZERO, new RandomMovingStrategy()));
+        String[] racingCars = {};
+        assertThatIllegalArgumentException().isThrownBy(() -> new RacingGame(racingCars, new RandomMovingStrategy()));
     }
 }
