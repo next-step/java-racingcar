c019d52626a2c78d72d3a4abae443496d73461ed
wholemann <wholeman.dev@gmail.com>
2020-03-22 21:02:01 +0900

    feat(racinggame): RacingGame에서 자동차의 이동을 진행한다

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,11 +1,33 @@
 package racingcar;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RacingGame {
 
+    private static Random randomGenerator;
+    private List<Car> cars;
+
+    public RacingGame(Random randomGenerator, int numberOfCar) {
+        this.randomGenerator = randomGenerator;
+        generateCars(numberOfCar);
+    }
+
     public List<Integer> move() {
-        return Arrays.asList(1, 0, 1);
+        cars.stream()
+                .forEach(car -> car.move());
+
+        return IntStream.range(0, cars.size())
+                .mapToObj(i -> cars.get(i).getMoveCount())
+                .collect(Collectors.toList());
+    }
+
+    private void generateCars(int numberOfCar) {
+        cars = IntStream
+                .range(0, numberOfCar)
+                .mapToObj(__ -> new Car(randomGenerator))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -3,16 +3,24 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RacingGameTest {
 
     @Test
     @DisplayName("게임을 진행할 때마다 자동차의 현재 위치를 리턴한다")
     public void moveCarsReturnsCurrentCarPosition() {
-        RacingGame game = new RacingGame();
+        Random random = mock(Random.class);
+        when(random.nextInt(10))
+                .thenReturn(4);
 
-        assertThat(game.move()).containsExactly(1, 0, 1);
-        assertThat(game.move()).containsExactly(2, 0, 2);
+        RacingGame game = new RacingGame(random, 3);
+
+        assertThat(game.move()).containsExactly(1, 1, 1);
+        assertThat(game.move()).containsExactly(2, 2, 2);
     }
 }
