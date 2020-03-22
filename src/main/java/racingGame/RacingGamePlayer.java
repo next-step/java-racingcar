9982e127e27df8ec03938c3e772fca726d1f7cbd
teahyuk <jtaeh57@gmail.com>
2020-03-22 19:17:56 +0900

    fix(Step5) : 프로그램 play 버그 수정
    프로그램 실행 하면서 전부 움직이지 않는 버그를 발견하여 수정함 : 
      - 주입한 randomEngine... threshold값이 10 이었음 (무조건 전진 못함...)
      - threshold값 수정
      - 클래스 길어진거 보기 싫어서 RandomEngine 추출함.
      - 전체적으로 import 정리 및 라인 정리

--- src/main/java/racingGame/RacingGamePlayer.java
+++ src/main/java/racingGame/RacingGamePlayer.java
@@ -7,29 +7,10 @@ import racingGame.ui.InputView;
 import racingGame.ui.ResultView;
 
 import java.util.Arrays;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 public class RacingGamePlayer {
-	private static final Engine randomEngine = new Engine() {
-		private static final int RANDOM_RANGE = 10;
-		private static final int GO_THRESHOLD = 10;
-
-		private Random random = new Random();
-
-		@Override
-		public int moveDistanceForOneTime() {
-			if (canGo()) {
-				return 1;
-			}
-
-			return 0;
-		}
-
-		private boolean canGo() {
-			return random.nextInt(RANDOM_RANGE) > GO_THRESHOLD;
-		}
-	};
+	private static final Engine randomEngine = new RandomEngine();
 
 	private InputView inputView;
 	private ResultView resultView;
--- src/main/java/racingGame/domain/RacingGame.java
+++ src/main/java/racingGame/domain/RacingGame.java
@@ -2,7 +2,6 @@ package racingGame.domain;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RacingGame {
