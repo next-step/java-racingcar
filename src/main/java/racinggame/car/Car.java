e8959c09396592e37983aadaf05207a5aae6d099
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-22 12:31:36 +0900

    feat : Car-carName 필드 추가
    - 자동차의 이름을 표기하기 위한 필드 추가

---
+++ src/main/java/racinggame/car/Car.java
@@ -0,0 +1,34 @@
+package racinggame.car;
+
+import racinggame.util.NumberUtil;
+
+/**
+ * 1. 움직이는 거리 필드
+ * 1. 자동차 이름 필드
+ * 1. 게임 진행시 자동차 이동
+ */
+public class Car {
+    private int moveRange = 0;
+    private String carName;
+
+    public Car(String carName) {
+        this.carName = carName;
+    }
+
+    /**
+     * 랜던값이 4보다 크면 자동차 한칸 이동
+     */
+    public void moveCar() {
+        if (NumberUtil.isGreaterThan4((NumberUtil.createRandomIntIn0to9()))) {
+            this.moveRange++;
+        }
+    }
+
+    public int getMoveRange() {
+        return moveRange;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+}
