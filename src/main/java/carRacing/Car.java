5f36382eb12617ef6f611788c2e654580e1788aa
ookhub <damlook@naver.com>
2020-03-22 23:03:56 +0900

    feat: add car

---
+++ src/main/java/carRacing/Car.java
@@ -0,0 +1,38 @@
+package carRacing;
+
+
+
+import org.assertj.core.util.VisibleForTesting;
+
+import java.util.Random;
+
+public class Car {
+
+    Random random;
+    int position;
+
+    public Car() {
+        random = new Random();
+        this.position = 0;
+    }
+
+    public void move() {
+        if(movable()) {
+            position++;
+        }
+    }
+
+    @VisibleForTesting
+    public boolean movable() {
+        return (random.nextInt(10) >= 4);
+    }
+
+    public int inquiryPosition() {
+        return position;
+    }
+
+    @VisibleForTesting
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+}
