6de45c7c90811a2c4f4d673a1e5d8c6aef638c4d
sang5c <skah321@gmail.com>
2020-03-22 22:21:26 +0900

    fix: 2단계 피드백 적용 및 기본 구조 생성

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+public class RacingGame {
+    private int time;
+    private int[] carPositions = {0, 0, 0};
+
+    public int[] move() {
+        // TODO 구현
+        return carPositions;
+    }
+
+    public void print() {
+
+    }
+}
