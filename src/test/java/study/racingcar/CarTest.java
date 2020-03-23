68bdbc2ae213a23d3f47f85196c43f173df53f10
luvram <sahul0804@gmail.com>
2020-03-23 21:29:54 +0900

    refactor: 불필요한 테스트 코드 개선

--- src/test/java/study/racingcar/CarTest.java
+++ src/test/java/study/racingcar/CarTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
-    Car car;
+    private Car car;
 
     @BeforeEach
     void setUp () {
--- src/test/java/study/racingcar/RacingGameTest.java
+++ src/test/java/study/racingcar/RacingGameTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
-    private final String[] EXAMPLE_CARS = "pobi,crong,honux".split(",");
+    private final String[] EXAMPLE_CARS = {"pobi","crong","honux"};
     private MovableDistance movableDistance;
 
     @BeforeEach
