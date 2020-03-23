ef6cb3f0a4249fbd01b344faa0e534f83ec199d4
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-23 20:47:23 +0900

    refactor : step2 리뷰 대응 - 기본자료형사용, 오타 수정, 접근제한자 수정
    - 사용하는곳 없는 메서드 private로 수정
    - 기본 자료형 사용 하도록 수정 (Double -> double)
    - isEquals() 오타 수정

--- src/main/java/calculator/ArithmeticOperation.java
+++ src/main/java/calculator/ArithmeticOperation.java
@@ -17,17 +17,17 @@ public enum ArithmeticOperation {
         this.calculate = calculate;
     }
 
-    public Double calculate(Double operand1, Double operand2) {
+    private double calculate(double operand1, double operand2) {
         return calculate.apply(operand1, operand2);
     }
 
-    public boolean isEqusls(String val) {
+    public boolean isEquals(String val) {
         return this.expression.equals(val);
     }
 
-    public static ArithmeticOperation findMatchedExpression(String operator) {
+    private static ArithmeticOperation findMatchedExpression(String operator) {
         for (ArithmeticOperation valus : ArithmeticOperation.values()) {
-            if (valus.isEqusls(operator)) {
+            if (valus.isEquals(operator)) {
                 return valus;
             }
         }
@@ -36,9 +36,5 @@ public enum ArithmeticOperation {
 
     public static Double calculateInput(Double operand1, Double operand2, String operator) {
         return findMatchedExpression(operator).calculate(operand1, operand2);
-    }
-
-    public String getExpression() {
-        return expression;
     }
 }
--- src/test/java/calculator/ArithmeticOperationTest.java
+++ src/test/java/calculator/ArithmeticOperationTest.java
@@ -2,60 +2,11 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ArithmeticOperationTest {
-
-    private final String PLUS = ArithmeticOperation.PLUS.getExpression();
-    private final String MINUS = ArithmeticOperation.MINUS.getExpression();
-    private final String MULTIPLICATION = ArithmeticOperation.MULTIPLICATION.getExpression();
-    private final String DIVISION = ArithmeticOperation.DIVISION.getExpression();
-
-    @DisplayName("덧셈 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"1:2:3", "1:3:4"}, delimiter = ':')
-    public void plus(double operand1, double operand2, double expect) throws Exception {
-        //then
-        assertThat(ArithmeticOperation.PLUS.calculate(operand1, operand2)).isEqualTo(expect);
-    }
-
-    @DisplayName("뺄셈 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"1:2:-1", "10:3:7"}, delimiter = ':')
-    public void minus(double operand1, double operand2, double expect) throws Exception {
-        //then
-        assertThat(ArithmeticOperation.MINUS.calculate(operand1, operand2)).isEqualTo(expect);
-    }
-
-    @DisplayName("곱셈 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"1:2:2", "10:3:30"}, delimiter = ':')
-    public void multiplication(double operand1, double operand2, double expect) throws Exception {
-        //then
-        assertThat(ArithmeticOperation.MULTIPLICATION.calculate(operand1, operand2)).isEqualTo(expect);
-    }
-
-    @DisplayName("나눗셈 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"1:2:0.5", "30:3:10"}, delimiter = ':')
-    public void division(double operand1, double operand2, double expect) throws Exception {
-        //then
-        assertThat(ArithmeticOperation.DIVISION.calculate(operand1, operand2)).isEqualTo(expect);
-    }
-
-    @DisplayName("사칙 연산 enum 값 찾기")
-    @Test
-    public void findMatchedExpression() throws Exception {
-        //then
-        assertThat(ArithmeticOperation.findMatchedExpression(PLUS)).isEqualTo(ArithmeticOperation.PLUS);
-        assertThat(ArithmeticOperation.findMatchedExpression(MINUS)).isEqualTo(ArithmeticOperation.MINUS);
-        assertThat(ArithmeticOperation.findMatchedExpression(MULTIPLICATION)).isEqualTo(ArithmeticOperation.MULTIPLICATION);
-        assertThat(ArithmeticOperation.findMatchedExpression(DIVISION)).isEqualTo(ArithmeticOperation.DIVISION);
-    }
 
     @DisplayName("사칙연산 테스트")
     @Test
