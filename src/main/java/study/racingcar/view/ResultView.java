90db4de5739d688fe89b974462eacb1486300d6c
luvram <sahul0804@gmail.com>
2020-03-24 00:31:20 +0900

    refactor: 사용하지 않는 함수 삭제

--- src/main/java/study/racingcar/view/ResultView.java
+++ src/main/java/study/racingcar/view/ResultView.java
@@ -7,7 +7,6 @@ public class ResultView {
     private static final String CAR_DISPLAY = "-";
 
     private static final String WINNER_MESSAGE = "%s가 최종 우승했습니다.";
-    private static final String INVALID_INPUT_MESSAGE = "잘못된 입력입니다.";
     private static final String START_GAME_MESSAGE = "실행 결과";
 
     public static void displayGameStatus(RacingGame racingGame) {
@@ -21,10 +20,6 @@ public class ResultView {
 
     public static void displayWinners(String winnerNames) {
         System.out.println(String.format(WINNER_MESSAGE, winnerNames));
-    }
-
-    public static void displayInvalidInputError() {
-        System.out.println(INVALID_INPUT_MESSAGE);
     }
 
     public static void displayStartGame() {
