c380dd85e6f066244aebdfecfb5ab1d203081b12
sang5c <skah321@gmail.com>
2020-03-23 22:07:32 +0900

    test: RacingGame 생성 테스트 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,16 +3,17 @@ package racingcar;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class RacingGameTest {
+class CarTest {
 
     @Test
-    void create() {
-        RacingGame racingGame = new RacingGame(3, 4);
-        assertThat(racingGame.getResult().size()).isEqualTo(3);
+    void getCarPositions() {
+    }
+
+    @Test
+    void move() {
     }
 }
