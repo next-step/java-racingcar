c5a58861cd31524c9e3597fca7575ab687c9bd54
ohtaeg <otk1090@naver.com>
2020-03-23 22:30:36 +0900

    refactor (MovingPolicy) : 움직임 조건 상수 추가

--- src/main/java/racingcar/policy/RandomMovingPolicy.java
+++ src/main/java/racingcar/policy/RandomMovingPolicy.java
@@ -4,23 +4,26 @@ import java.util.Random;
 
 public class RandomMovingPolicy implements MovingPolicy{
     private final Random random = new Random();
-    private static final int POSSIBLE_MIN_MOVE_CONDITION = 4;
+    private static final int DEFAULT_MOVE_CONDITION = 4;
+    private static final int DEFAULT_MIN_VALUE = 0;
     private static final int DEFAULT_MAX_RANGE = 10;
     private int minRange;
     private int maxRange;
+    private int condition;
 
     public RandomMovingPolicy() {
-        this(POSSIBLE_MIN_MOVE_CONDITION, DEFAULT_MAX_RANGE);
+        this(DEFAULT_MIN_VALUE, DEFAULT_MAX_RANGE, DEFAULT_MOVE_CONDITION);
     }
 
-    public RandomMovingPolicy(final int minRange, final int maxRange) {
+    public RandomMovingPolicy(final int minRange, final int maxRange, final int condition) {
         this.minRange = minRange;
         this.maxRange = maxRange;
+        this.condition = condition;
     }
 
     @Override
     public boolean isPossibleMove(int distance) {
-        return minRange <= distance && distance < maxRange;
+        return condition <= distance && distance < maxRange;
     }
 
     @Override
