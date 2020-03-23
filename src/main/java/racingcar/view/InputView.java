c14376806d4aa1f0fe1c62c16d17ec1992f45145
ohtaeg <otk1090@naver.com>
2020-03-23 21:58:18 +0900

    fix (modifier) : static 메서드 public으로 접근제한자 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,8 +3,8 @@ package racingcar.view;
 import java.io.InputStream;
 
 public class InputView {
-    private static final String INPUT_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
-    private static final String INPUT_TIME_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    public static final String INPUT_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    public static final String INPUT_TIME_MESSAGE = "시도할 회수는 몇 회 인가요?";
     private Input input;
 
     public InputView() {
