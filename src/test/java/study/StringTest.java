27b75e1b9aac87ab46ba075a3cd04d90aa24492b
kiyeoneee <kyeoneee@gmail.com>
2020-03-24 01:10:24 +0900

    fix (StringTest) : step1 - 불필요한 exceptionMessage 체크 제거

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -21,13 +21,6 @@ public class StringTest {
         assertThat(result).isEqualTo("1,2");
     }
 
-    /*
-    * 요구사항 3
-"abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
-String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
-JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
-힌트
-    * */
     @Test
     void charAt() {
         String value = "abc";
@@ -43,7 +36,6 @@ JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다
 
         assertThatThrownBy(() -> {
             value.charAt(3);
-        }).isInstanceOf(StringIndexOutOfBoundsException.class)
-                .hasMessageContaining("Index: 2, Size: 2");
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
