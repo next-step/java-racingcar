404a1029ad563115d7ac6b1faec617aac77cb2b7
kwanguuuu <kwangyu326@gmail.com>
2020-03-23 15:34:11 +0900

    feature : 우승자 로직 추가

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,15 +1,28 @@
 package racingcar.view;
 
+import racingcar.util.Delimeters;
+
+import java.util.List;
+
 public class ResultView {
-    private static final String VIEW = "-";
+    private static final String WINNER_ALARM_MESSAGE = "가 우승했습니다.";
 
     public static void nextLine() {
         System.out.println();
     }
 
-    public static void printResultView(int carPosition) {
+    public static void printPosition(int carPosition) {
         for (int i = 0; i < carPosition; i++) {
-            System.out.printf(VIEW);
+            System.out.printf(Delimeters.DASH);
         }
+        nextLine();
+    }
+
+    public static void printCarName(String carName) {
+        System.out.printf(carName + Delimeters.COLON);
+    }
+
+    public static void printWinnerName(List<String> winners) {
+        System.out.println(String.join(Delimeters.COMMA, winners) + WINNER_ALARM_MESSAGE);
     }
 }
