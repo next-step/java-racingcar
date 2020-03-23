b7b58a88236f0a05658491461bab13efbd36907e
ohtaeg <otk1090@naver.com>
2020-03-23 21:08:17 +0900

    refactor (RacingGame) : 자동차 이동정책 추가 및 게임 진행 메서드 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import racingcar.model.Car;
+import racingcar.policy.MovingPolicy;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -9,10 +10,25 @@ import java.util.List;
 public class RacingGame {
     private final int time;
     private final List<Car> cars;
+    private final MovingPolicy movingPolicy;
 
-    public RacingGame(final int time, final int count) {
+    public RacingGame(final int time, final int count, final MovingPolicy movingPolicy) {
         this.time = time;
         this.cars = ready(count);
+        this.movingPolicy = movingPolicy;
+    }
+
+    // TODO : INDET 3
+    public void play() {
+        int distance;
+        for (int i = 0; i < time; i++) {
+            for (Car car : cars) {
+                distance = movingPolicy.distance();
+                if (movingPolicy.isPossibleMove(distance)) {
+                    car.move();
+                }
+            }
+        }
     }
 
     public List<Car> getCars() {
