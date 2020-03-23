0973d58415322bdf6a7cfadc87e2d7d01f2eb500
dongchul <sah3122@naver.com>
2020-03-23 23:34:04 +0900

    feat(RacingCar): add RacingCarController

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,25 @@
+package racingcar.controller;
+
+import racingcar.domain.car.Car;
+import racingcar.domain.car.RandomMovingStrategy;
+import racingcar.dto.RacingCarRequestDto;
+import racingcar.dto.RacingCarResponseDto;
+import racingcar.service.RacingGameService;
+import racingcar.domain.RacingScore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCarController {
+    private static final String DELIMITER = ",";
+
+    public RacingCarResponseDto startRacingGame(RacingCarRequestDto racingCarRequestDto) {
+        String[] racingCarNames = racingCarRequestDto.getRacingCarNames().split(DELIMITER);
+        RacingGameService racingGameService = new RacingGameService(racingCarNames, new RandomMovingStrategy());
+
+        List<RacingScore> racingScores = racingGameService.executeRacingGameNumberOfTimes(racingCarRequestDto.getNumberOfTime());
+        List<Car> winners = racingGameService.selectWinners(racingScores);
+
+        return new RacingCarResponseDto(racingScores, winners);
+    }
+}
