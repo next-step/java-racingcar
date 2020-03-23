dd1d4056e4adc82d1cb99b0f67d2fd8e5bce329b
kwanguuuu <kwangyu326@gmail.com>
2020-03-23 15:34:28 +0900

    테스트 코드 추가.

---
+++ src/test/java/racingcar/RacingWinnerTest.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingWinnerTest {
+    List<RacingCar> racers = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        RacingCar racer1 = new RacingCar("광유", 3);
+        RacingCar racer2 = new RacingCar("재석", 5);
+        RacingCar racer3 = new RacingCar("포비", 5);
+        racers.add(racer1);
+        racers.add(racer2);
+        racers.add(racer3);
+    }
+
+    @Test
+    void createRaceWinner() {
+        assertThat(new RacingWinner()).isNotNull();
+    }
+
+    @Test
+    void findWinnerNameTest() {
+        assertThat(new RacingWinner().findWinnerName(racers)).contains("재석","포비");  //updateBoard
+    }
+}
