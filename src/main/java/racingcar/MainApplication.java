5b5c20e50f6114bf0dda159aa93a4493c2b4a384
LeeSeungHee <palmseung@gmail.com>
2020-03-23 13:40:13 +0900

    refactor : Refactor codes (13) and add InputProcessor

--- src/main/java/racingcar/MainApplication.java
+++ src/main/java/racingcar/MainApplication.java
@@ -1,13 +1,14 @@
 package racingcar;
 
 import racingcar.controller.RacingCar;
+import racingcar.view.InputProcessor;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class MainApplication {
     public static void main(String[] args) {
-        String carNames = InputView.getCarNamesFromUser();
-        String tryCount = InputView.getTryCountsFromUser();
+        String carNames = InputProcessor.getCarNamesFromUser();
+        String tryCount = InputProcessor.getTryCountsFromUser();
 
         RacingCar racingCar = new RacingCar(InputView.of(carNames, tryCount));
         racingCar.start();
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,11 +2,9 @@ package racingcar.view;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class InputView {
     private static final String DELIMITER_FOR_INPUT_CAR = ",";
-    private static final Scanner scanner = new Scanner(System.in);
 
     private List<String> carNames;
     private int carCount;
@@ -15,16 +13,6 @@ public class InputView {
     public InputView(String carNames, String tryCounts) {
         insertCarNames(carNames);
         insertTryCount(tryCounts);
-    }
-
-    public static String getCarNamesFromUser() {
-        System.out.println("자동차 이름을 입력하세요, (단, 자동차 이름은 쉼표(,)로 구분해야 합니다.)");
-        return scanner.nextLine();
-    }
-
-    public static String getTryCountsFromUser() {
-        System.out.println("몇 회를 시도하실 건가요?");
-        return scanner.nextLine();
     }
 
     private void insertCarNames(String carNameString) {
