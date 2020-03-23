445ab0657d7280a3069afa88f3891882b648f206
kiyeoneee <kyeoneee@gmail.com>
2020-03-24 03:09:01 +0900

    feat (Car) : step3 - 초기 자동차 위치 설정 테스트 추가

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -7,4 +7,8 @@ public class Car {
     public int moveByCondition(int condition) {
         return 0;
     }
+
+    public int getPosition() {
+        return 0;
+    }
 }
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -1,7 +1,9 @@
 package racing.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,18 +13,26 @@ public class CarTest {
 
     private Car car;
 
-    private static final int DEFAULT_STATE_FOR_TEST = 3;
+    private static final int DEFAULT_POSITION = 1;
+    private static final int DEFAULT_POSITION_FOR_TEST = 3;
 
     @BeforeEach
     void setUp() {
         car = new Car();
-        car.setPosition(DEFAULT_STATE_FOR_TEST);
+    }
+
+    @Test
+    @DisplayName("자동차의 초기값이 0으로 설정되는지 테스트")
+    void defaultCarPositionTest() {
+        Assertions.assertEquals(DEFAULT_POSITION, car.getPosition());
     }
 
     @ParameterizedTest
     @CsvSource(value = {"5:4", "2:3", "-1:3"}, delimiter = ':')
     @DisplayName("이동 조건에 부합 하는 경우만 전진하는지 테스트")
     void moveByCondition(int input, int expected) {
+        car.setPosition(DEFAULT_POSITION_FOR_TEST);
+
         assertThat(car.moveByCondition(input)).isEqualTo(expected);
     }
 }
