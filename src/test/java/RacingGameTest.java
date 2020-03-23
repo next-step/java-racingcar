ca5b7bf3cd17df917f1af744e3c31476b1cc1682
manhyuk <manhyuk@kw.ac.kr>
2020-03-24 03:15:23 +0900

    test: add test code

---
+++ src/test/java/RacingGameTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class RacingGameTest {
+
+    private static final int CAR_COUNT = 3;
+    private static final int STAGE_COUNT = 5;
+    private static final int THRESHOLD = 4;
+
+
+    @DisplayName("결과 테스트")
+    @Test
+    void start() {
+        List<List<Integer>> results = RacingGame.start(CAR_COUNT, STAGE_COUNT);
+        assertThat(results.size()).isEqualTo(STAGE_COUNT);
+    }
+
+
+    @DisplayName("랜덤 이동 값 테스트 4이하면 false")
+    @Test
+    void isMovable() {
+        boolean isMove = RacingGame.isMovable(THRESHOLD - 1);
+        System.out.println(isMove);
+        assertThat(isMove).isFalse();
+    }
+}
+
