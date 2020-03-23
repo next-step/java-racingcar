ca3f84ec023347b5525919b8d38b5fe8f9b968e2
manhyuk <manhyuk@kw.ac.kr>
2020-03-24 03:14:06 +0900

    feat: add result view

---
+++ src/main/java/ResultView.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class ResultView {
+
+
+    static void print(List<List<Integer>> results) {
+
+        results.forEach(stage -> {
+            stage.forEach(count -> {
+                System.out.println(getMoveString(count));
+            });
+            System.out.println();
+        });
+    }
+
+    private static String getMoveString(int count) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            stringBuilder.append('-');
+        }
+        return stringBuilder.toString();
+    }
+
+}
+
