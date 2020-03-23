49d16d1800d460478f824309856989670124a095
ohtaeg <otk1090@naver.com>
2020-03-23 16:23:53 +0900

    feat (input valid) : 입력값이 올바른지 기능 추가

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -5,12 +5,21 @@ import java.util.Scanner;
 
 public final class Input {
     private final Scanner SCANNER;
+    private static final String EMPTY_STRING = "";
 
     public Input(final InputStream inputStream) {
         SCANNER = new Scanner(inputStream);
     }
 
     public String read() {
-        return SCANNER.nextLine();
+        String input = SCANNER.nextLine();
+        valid(input);
+        return input;
+    }
+
+    private void valid(final String input) {
+        if (input == null || input.trim().equals(EMPTY_STRING)) {
+            throw new IllegalArgumentException("올바른 입력값이 아닙니다.");
+        }
     }
 }
