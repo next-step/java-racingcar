58faaebc09aabb13d85d6cd1fff01b1d66903c2d
ohtaeg <otk1090@naver.com>
2020-03-23 22:13:15 +0900

    refactor (access modifier) : 상수와 출력메서드 접근제한자를 private -> public으로 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,22 +5,21 @@ import racingcar.model.Car;
 import java.util.List;
 
 public class OutputView {
-    private static final String GAME_RESULT_MESSAGE = "실행 결과";
+    public static final String GAME_RESULT_MESSAGE = "실행 결과";
 
     public OutputView() {}
 
     public void view(List<Car> cars) {
         Output output;
-        print(GAME_RESULT_MESSAGE);
         for (Car car : cars) {
             output = new Output();
             output.mappingToBar(car.getDistance());
             print(output.getBar());
-            nextLine();
         }
+        nextLine();
     }
 
-    private void print(String message) {
+    public void print(String message) {
         System.out.println(message);
     }
 
