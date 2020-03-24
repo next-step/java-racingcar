027df461cdd6df81f140b4a364e08705a5bed4b6
kiyeoneee <kyeoneee@gmail.com>
2020-03-25 00:32:17 +0900

    fix (MovableTest) : step3 - 자동차 전진 조건 테스트 오류 수정 * 4 이상인 경우에만 true로 결과값으로 수정

--- src/test/java/racing/domain/MovableTest.java
+++ src/test/java/racing/domain/MovableTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class MovableTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"1:true", "7:false"}, delimiter = ':')
+    @CsvSource(value = {"1:false", "4:true", "7:true"}, delimiter = ':')
     @DisplayName("조건에 따라 자동차가 움직일 지 여부를 제대로 return 하는지 확인 - 4 미만일 경우만 이동")
     void canDecisionMovable(int input, boolean expected) {
         Assertions.assertThat(Movable.isMovable(input)).isEqualTo(expected);
