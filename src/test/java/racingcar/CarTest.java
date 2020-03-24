d5bf69a603325ff39fdb80ef3407413708f1cb66
wholemann <wholeman.dev@gmail.com>
2020-03-25 02:10:49 +0900

    refactor(cartest): 랜덤을 테스트 하는게 아닌 전진을 테스트한다

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -8,12 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarTest {
 
     @Test
-    @DisplayName("자동차의 이동 횟수가 랜덤한 확률로 증가한다")
+    @DisplayName("자동차가 전진하면 자동차의 위치가 1씩 증가한다")
     public void moveCarsChangePositionByRandom() {
-        Car car = new Car(() -> true);
+        Car car = new Car();
 
-        car.move();
-
+        car.move(true);
         assertThat(car.getPosition()).isEqualTo(1);
+
+        car.move(true);
+        assertThat(car.getPosition()).isEqualTo(2);
     }
 }
