500ab5fcaffead247ac5e343d944a85ebfb49cb3
rkdvnfma90 <rkdvnfma90@hanmail.net>
2020-03-24 21:38:08 +0900

    (fix) 변수 명칭 수정

--- src/main/java/racing/ResultView.java
+++ src/main/java/racing/ResultView.java
@@ -12,31 +12,25 @@ public class ResultView {
     public void view() {
         int count = racingVO.getCount();
         System.out.println("실행결과");
-
         for(int i = 0; i < count; ++i) {
             viewResult(racingGame.move());
         }
-
     }
 
     public void viewResult(int[] carPositions) {
         int length = carPositions.length;
-
         for(int i = 0; i < length; i++) {
             int carPosition = carPositions[i];
             System.out.println(printCar(carPosition));
         }
-
         System.out.println("###############################################");
     }
 
     public String printCar(int carPosition) {
         StringBuffer sb = new StringBuffer();
-
         for(int i = 0; i < carPosition; ++i) {
             sb.append("-");
         }
-
         return sb.toString();
     }
 }
