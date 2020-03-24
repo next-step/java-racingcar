0c4fdcec6b2f3f94ea00d8b7bcb8ba78155d8e0d
ohtaeg <otk1090@naver.com>
2020-03-24 16:25:01 +0900

    feat (RacingGameSetting) : 입력값들과 이동정책을 하나의 객체로 관리하는 RacingGameSetting 객체 구현

--- src/main/java/racingcar/model/RacingGameSetting.java
+++ src/main/java/racingcar/model/RacingGameSetting.java
@@ -3,11 +3,31 @@ package racingcar.model;
 import racingcar.policy.MovingPolicy;
 import racingcar.policy.RandomMovingPolicy;
 
-public class RacingGameSetting {
-    private MovingPolicy movingPolicy;
+import java.util.ArrayList;
+import java.util.List;
 
-    public RacingGameSetting(final MovingPolicy movingPolicy) {
+public class RacingGameSetting {
+    private static final String CAR_NAME_SEPARATOR = ",";
+    private MovingPolicy movingPolicy;
+    private Cars cars;
+    private int time;
+
+    public RacingGameSetting(final MovingPolicy movingPolicy, final String carNames, final int time) {
         this.movingPolicy = settingMovingPolicy(movingPolicy);
+        this.cars = settingCars(carNames);
+        this.time = time;
+    }
+
+    public MovingPolicy getMovingPolicy() {
+        return movingPolicy;
+    }
+
+    public Cars getCars() {
+        return cars;
+    }
+
+    public int getTime() {
+        return time;
     }
 
     private MovingPolicy settingMovingPolicy(MovingPolicy movingPolicy) {
@@ -17,7 +37,12 @@ public class RacingGameSetting {
         return movingPolicy;
     }
 
-    public MovingPolicy getMovingPolicy() {
-        return movingPolicy;
+    private Cars settingCars(final String carNames) {
+        String[] names = carNames.split(CAR_NAME_SEPARATOR);
+        List<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name, Car.DEFAULT_DISTANCE));
+        }
+        return new Cars(cars);
     }
 }
