2e23067c46413000a84a1529d452e6923394c6ba
kiyeoneee <kyeoneee@gmail.com>
2020-03-25 00:40:41 +0900

    feat (Car) : step3 - Car 객체의 위치 관련 로직 추가 * 객체가 생성되는 시점에 기본 위치값을 1로 설정하는 기능 추가 * 조건에 따라 이동하는 기능 추가

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,15 +1,30 @@
 package racing.domain;
 
 public class Car {
+    private static final int DEFAULT_POSITION = 1;
+    private int position;
+
+    public Car() {
+        position = DEFAULT_POSITION;
+    }
+
     public Car setPosition(int defaultStateForTest) {
         return new Car();
     }
 
     public int moveByCondition(int condition) {
-        return 0;
+        if (Movable.isMovable(condition)) {
+            return moveForward();
+        }
+
+        return getPosition();
+    }
+
+    private int moveForward() {
+        return ++position;
     }
 
     public int getPosition() {
-        return 0;
+        return position;
     }
 }
