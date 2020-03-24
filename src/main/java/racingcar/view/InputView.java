fbf018ec378e5f5f38e55efc391e8533225ebc4d
ohtaeg <otk1090@naver.com>
2020-03-24 15:30:43 +0900

    refactor (InputView) : 자동차 이름과 자동차 갯수를 받는 메서드를 분리한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,8 +3,8 @@ package racingcar.view;
 import java.io.InputStream;
 
 public class InputView {
-    public static final String INPUT_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
-    public static final String INPUT_TIME_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)를 기준으로 구분)";
+    private final String INPUT_TIME_MESSAGE = "시도할 회수는 몇 회 인가요?";
     private Input input;
 
     public InputView() {
@@ -15,8 +15,13 @@ public class InputView {
         input = new Input(inputStream);
     }
 
-    public int view(String message) {
-        print(message);
+    public String inputCarNames() {
+        print(INPUT_CAR_NAMES_MESSAGE);
+        return input.read();
+    }
+
+    public int inputCarCount() {
+        print(INPUT_TIME_MESSAGE);
         return Integer.parseInt(input.read());
     }
 
