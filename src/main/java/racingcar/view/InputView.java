463b30e7037579c6ade9f24c638bd8e594f707c7
dongchul <sah3122@naver.com>
2020-03-24 22:02:32 +0900

    feat(RacingCar): fix typo

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
     }
 
     public static int inputRacingGameTimes() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        System.out.println("시도할 회수는 몇 회 인가요?");
         return scanner.nextInt();
     }
 }
