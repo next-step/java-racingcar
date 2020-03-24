206f0772ddc3c90fcafed0905892353f5b7458b3
Yeoseon Yun <devyyskr@gmail.com>
2020-03-24 21:44:07 +0900

    test(Car): Car관련 테스트 코드 추가

--- src/test/java/step3/CarsTest.java
+++ src/test/java/step3/CarsTest.java
@@ -1,5 +1,6 @@
 package step3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,21 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
+
+    private Cars cars;
+    private String[] carNames = {"jihoon","yeoseon"};
+
+    @BeforeEach
+    void setUp() {
+        cars = new Cars(carNames);
+    }
+
+    @Test
+    @DisplayName("Cars 인스턴스 생성 테스트")
+    void newCars() {
+        assertThat(cars).isNotNull();
+    }
+
     @Test
     @DisplayName("입력받은 숫자 List 중 최대 숫자를 잘 고르는 지 테스트")
     void getMaxNumber() {
