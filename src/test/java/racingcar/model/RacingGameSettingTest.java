4991361a103dd2cf68680de927bf025dd205e825
ohtaeg <otk1090@naver.com>
2020-03-24 16:24:03 +0900

    test (RacingGameSetting) : 자동차 이름 문자열을 Cars 객체로 변환하는 테스트 코드 추가 및 자동차 이름 갯수만큼 Cars 리스트 갯수가 맞는지 테스트 코드 추가

--- src/test/java/racingcar/model/RacingGameSettingTest.java
+++ src/test/java/racingcar/model/RacingGameSettingTest.java
@@ -9,12 +9,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingGameSettingTest {
 
+    private String CAR_NAMES = "pobi,crong,honux";
+
     @DisplayName("이동 정책이 null일 경우 기본정책인 랜덤으로 세팅한다.")
     @Test
     void settingMovingPolicy() {
         final MovingPolicy movingPolicy = null;
-        RacingGameSetting setting = new RacingGameSetting(movingPolicy);
+        RacingGameSetting setting = new RacingGameSetting(movingPolicy, CAR_NAMES, 0);
         assertThat(setting.getMovingPolicy()).isNotNull();
         assertThat(setting.getMovingPolicy()).isInstanceOf(RandomMovingPolicy.class);
+    }
+
+    @DisplayName("자동차 이름 문자열을 Cars 객체로 변환할 수 있다.")
+    @Test
+    void settingCars() {
+        RacingGameSetting setting = new RacingGameSetting(null, CAR_NAMES, 0);
+        assertThat(setting.getCars()).isNotNull();
+        assertThat(setting.getCars()).isInstanceOf(Cars.class);
+    }
+
+    @DisplayName("자동차 이름갯수 만큼 자동차가 생성된다.")
+    @Test
+    void carCount() {
+        final int length = CAR_NAMES.split(",").length;
+        RacingGameSetting setting = new RacingGameSetting(null, CAR_NAMES, 0);
+        assertThat(setting.getCars().getCars()).hasSize(length);
     }
 }
