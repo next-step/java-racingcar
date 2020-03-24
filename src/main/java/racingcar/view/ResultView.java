b38a066230425e33583bc5ace9c1ee069a8d3afb
kwanguuuu <kwangyu326@gmail.com>
2020-03-24 12:01:20 +0900

    refactor : ResultView 변경( RacingResult만 받도록 수정 및 메소드 추가)

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import racingcar.domain.RacingResult;
 import racingcar.util.Delimeters;
 
 import java.util.List;
@@ -7,22 +8,31 @@ import java.util.List;
 public class ResultView {
     private static final String WINNER_ALARM_MESSAGE = "가 우승했습니다.";
 
-    public static void nextLine() {
-        System.out.println();
-    }
-
-    public static void printPosition(int carPosition) {
-        for (int i = 0; i < carPosition; i++) {
-            System.out.printf(Delimeters.DASH);
+    public static void printRaceResult(List<RacingResult> cars) {
+        for(RacingResult car: cars){
+            printCarName(car.getName());
+            printPosition(car.getPosition());
+            nextLine();
         }
         nextLine();
-    }
-
-    public static void printCarName(String carName) {
-        System.out.printf(carName + Delimeters.COLON);
     }
 
     public static void printWinnerName(List<String> winners) {
         System.out.println(String.join(Delimeters.COMMA, winners) + WINNER_ALARM_MESSAGE);
     }
+
+    public static void nextLine() {
+        System.out.println();
+    }
+
+    private static void printPosition(int carPosition) {
+        for (int i = 0; i < carPosition; i++) {
+            System.out.printf(Delimeters.DASH);
+        }
+    }
+
+    private static void printCarName(String carName) {
+        System.out.printf(carName + Delimeters.COLON);
+    }
+
 }
