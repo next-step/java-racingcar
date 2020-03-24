38c47d683bbb6280449a6a151e0604c2408cd9e3
sang5c <skah321@gmail.com>
2020-03-24 22:48:08 +0900

    fix: 불필요한 코드 제거

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,14 +1,10 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
--- src/test/java/racingcar/RacingCarMovableStrategyTest.java
+++ src/test/java/racingcar/RacingCarMovableStrategyTest.java
@@ -1,12 +1,10 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RacingCarMovableStrategyTest {
 
