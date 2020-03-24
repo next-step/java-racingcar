8d020aed0ca171f2b854192eeddbb41951ebe709
dongchul <sah3122@naver.com>
2020-03-24 12:57:46 +0900

    rafactor(RacingCar): add outputview

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -3,6 +3,8 @@ package racingcar;
 import racingcar.controller.RacingCarController;
 import racingcar.dto.RacingCarRequestDto;
 import racingcar.dto.RacingCarResponseDto;
+import racingcar.service.RacingGameService;
+import racingcar.view.OutputView;
 import racingcar.view.ResultView;
 
 import java.util.Scanner;
@@ -11,14 +13,9 @@ public class RacingCarApplication {
 
     public static void main(String[] args) {
         ResultView resultView = new ResultView();
-        RacingCarController racingCarController = new RacingCarController();
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String racingCarNames = scanner.nextLine();
-
-        System.out.println("시도할 회수는 몇 회 인가요 ?");
-        int numberOfTime = scanner.nextInt();
+        RacingCarController racingCarController = new RacingCarController(new RacingGameService());
+        String racingCarNames = OutputView.inputRacingCars();
+        int numberOfTime = OutputView.inputRacingGameTimes();
 
         RacingCarResponseDto racingCarResponseDto = racingCarController
                 .startRacingGame(new RacingCarRequestDto(racingCarNames, numberOfTime));
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class OutputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputRacingCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine();
+    }
+
+    public static int inputRacingGameTimes() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextInt();
+    }
+}
