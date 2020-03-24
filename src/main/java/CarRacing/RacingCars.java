70884937db60631cea0e4467bdbf5efc69fb0ccd
jinwoo <jwlee@qualson.com>
2020-03-24 17:50:39 +0900

    feat: Add RacingCars contructor with string arrays in RacingCars Class

--- src/main/java/CarRacing/RacingCars.java
+++ src/main/java/CarRacing/RacingCars.java
@@ -1,15 +1,17 @@
 package CarRacing;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCars {
-    final private List<Car> racingCars = new ArrayList<>();
+    private final List<Car> racingCars;
 
-    public RacingCars(final int carCount) {
-        for (int i = 0; i < carCount; i++) {
-            this.racingCars.add(new Car());
-        }
+    public RacingCars(final String[] carNameArr) {
+        this.racingCars = Arrays.stream(carNameArr)
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 
     public List<Car> getRacingCars() {
@@ -25,7 +27,7 @@ public class RacingCars {
     public void showCurrentStatus() {
         final StringBuilder sb = new StringBuilder();
         for (Car car : racingCars) {
-            sb.append(StringUtils.gaugeMaker(car.getCurrentPosition()));
+            sb.append(StringUtils.makeGauge(car.getCurrentPosition()));
         }
 
         System.out.println(sb.toString());
