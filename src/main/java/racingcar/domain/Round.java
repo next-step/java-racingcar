db1f6d0f6a4a90cd271039e1609547bb3a1bd8ae
ohtaeg <otk1090@naver.com>
2020-03-25 16:53:57 +0900

    refactor (Round) : 시도 횟수 객체를 0부터가 아닌 입력값부터 0까지 마이너스 되도록 변경

--- src/main/java/racingcar/domain/Round.java
+++ src/main/java/racingcar/domain/Round.java
@@ -1,27 +1,39 @@
 package racingcar.domain;
 
-public final class Round {
-    private static final int DEFAULT_ROUND = 0;
-    private static final int NEXT = 1;
-    private final int round;
+import java.util.Objects;
 
-    public Round() {
-        this.round = DEFAULT_ROUND;
-    }
+public final class Round {
+    private static final int END_ROUND = 0;
+    private static final int NEXT = 1;
+
+    private int round;
 
     public Round(final int round) {
         this.round = round;
     }
 
     public Round next() {
-        return new Round(round + NEXT);
+        return new Round(round - NEXT);
     }
 
-    public boolean isEnd(int time) {
-        return round == time;
+    public boolean isEnd() {
+        return round == END_ROUND;
     }
 
-    public int getRound() {
-        return round;
+    public Round getRound() {
+        return new Round(round);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Round round1 = (Round) o;
+        return round == round1.round;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(round);
     }
 }
