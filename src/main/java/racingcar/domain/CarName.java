f83f64cfe0b007ce59f079189545f65277f1698b
ohtaeg <otk1090@naver.com>
2020-03-25 15:54:23 +0900

    feat (CarName) : 자동차 이름을 갖는 값 객체 구현

---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class CarName {
+    private String name;
+
+    public CarName(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
