85256f470ba16ec098cd575a43d5e7a04bdc39ee
jinwoo <jwlee@qualson.com>
2020-03-25 18:01:32 +0900

    feat: Add getWInnerFunction in RacingGameClass

--- src/main/java/CarRacing/RacingGame.java
+++ src/main/java/CarRacing/RacingGame.java
@@ -1,5 +1,10 @@
 package CarRacing;
 
+import CarRacing.Utils.DisplayUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RacingGame {
 
     final private RacingStrategy racingStrategy;
@@ -13,8 +18,23 @@ public class RacingGame {
     }
 
     public void raceStart() {
-        for (int i = 0; i < this.racingTime; i++){
+        for (int i = 0; i < this.racingTime; i++) {
             this.racingCars.race(this.racingStrategy);
+            DisplayUtils.makeCarsCurrentStatusToString(this.racingCars);
         }
+    }
+
+    public List<String> getRacingWinner() {
+        final int maxPosition = this.racingCars.getRacingCars()
+                .stream()
+                .mapToInt(Car::getCurrentPosition)
+                .max()
+                .orElseThrow(() -> new RuntimeException("Winner does not exist"));
+
+        return this.racingCars.getRacingCars()
+                .stream()
+                .filter(car -> car.getCurrentPosition() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 }
