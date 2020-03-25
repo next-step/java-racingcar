f0c839b6ceb294a10b8edcabf7f11c711b77eb6d
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-25 16:55:29 +0900

    feat : step3 리뷰 반영 - playAllRound 테스트 코드 작성
    - Racing - playOneRound() 테스트 코드 작성

--- src/test/java/racinggame/domain/racing/RacingTest.java
+++ src/test/java/racinggame/domain/racing/RacingTest.java
@@ -3,13 +3,16 @@ package racinggame.domain.racing;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import racinggame.domain.view.ResultView;
+import racinggame.domain.view.InputView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class RacingTest {
 
@@ -28,5 +31,20 @@ class RacingTest {
     public void construct(int carCount, int gameCount, List<String> carNames) throws Exception {
         //then
         new Racing(gameCount, carNames);
+    }
+
+    @DisplayName("레이싱 실행시 라운드수 와 레이싱 진행으로 인해 생긴 result의 크기를 비교")
+    @ParameterizedTest
+    @CsvSource(value = {"1:a:1", "3:a,b,c:3", "5:a,b,c,d,e:5"}, delimiter = ':')
+    public void playAllRound(String gameCount, String carNames, int expect) throws Exception {
+        //given
+        InputView inputView = new InputView(gameCount, carNames);
+        Racing racing = new Racing(inputView.getGameCount(), inputView.getCarNames());
+
+        //when
+        racing.playAllRound();
+
+        //then
+        assertThat(racing.getRoundResult().size()).isEqualTo(expect);
     }
 }
