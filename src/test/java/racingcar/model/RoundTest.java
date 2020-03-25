d911ae848744f1c0badd1ed038937eb50e08f407
ohtaeg <otk1090@naver.com>
2020-03-25 16:56:10 +0900

    test (Round) : 역순으로 Round가 감소되는 테스트 코드로 변경

--- src/test/java/racingcar/model/RoundTest.java
+++ src/test/java/racingcar/model/RoundTest.java
@@ -8,33 +8,39 @@ import racingcar.domain.Round;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RoundTest {
+    private int tryCount = 3;
 
     private Round round;
 
     @BeforeEach
     void setUp() {
-        round = new Round();
+        round = new Round(tryCount);
     }
 
     @DisplayName("다음 라운드로 넘어 갈 수 있다.")
     @Test
     void next() {
         // given
-        final int expect = 1;
+        final Round expect = new Round(2);
 
         // when
         final Round actual = round.next();
 
         // then
         assertThat(actual).isNotNull();
-        assertThat(actual.getRound()).isEqualTo(1);
+        assertThat(actual.getRound()).isEqualTo(expect);
     }
 
     @DisplayName("라운드 종료 확인을 성공한다.")
     @Test
     void isEnd() {
+        // given
+        for (int i = 0; i < tryCount; i++) {
+            round = round.next();
+        }
+
         // when
-        final boolean actual = round.isEnd(0);
+        final boolean actual = round.isEnd();
 
         // then
         assertThat(actual).isNotNull();
