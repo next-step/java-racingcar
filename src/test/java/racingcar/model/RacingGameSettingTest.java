90be00eacfae3b1920c45acf6f8237aa7d1e8d4b
ohtaeg <otk1090@naver.com>
2020-03-25 15:30:43 +0900

    test (RacingGameSetting) : 이동정책을 제거함에 따라 불필요한 테스트 코드 제거 및 수정

--- src/test/java/racingcar/model/RacingGameSettingTest.java
+++ src/test/java/racingcar/model/RacingGameSettingTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Cars;
 import racingcar.domain.RacingGameSetting;
-import racingcar.policy.MovingPolicy;
-import racingcar.policy.RandomMovingPolicy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,19 +11,10 @@ class RacingGameSettingTest {
 
     private String CAR_NAMES = "pobi,crong,honux";
 
-    @DisplayName("이동 정책이 null일 경우 기본정책인 랜덤으로 세팅한다.")
-    @Test
-    void settingMovingPolicy() {
-        final MovingPolicy movingPolicy = null;
-        RacingGameSetting setting = new RacingGameSetting(movingPolicy, CAR_NAMES, 0);
-        assertThat(setting.getMovingPolicy()).isNotNull();
-        assertThat(setting.getMovingPolicy()).isInstanceOf(RandomMovingPolicy.class);
-    }
-
     @DisplayName("자동차 이름 문자열을 Cars 객체로 변환할 수 있다.")
     @Test
     void settingCars() {
-        RacingGameSetting setting = new RacingGameSetting(null, CAR_NAMES, 0);
+        RacingGameSetting setting = new RacingGameSetting(CAR_NAMES, 0);
         assertThat(setting.getCars()).isNotNull();
         assertThat(setting.getCars()).isInstanceOf(Cars.class);
     }
@@ -34,7 +23,7 @@ class RacingGameSettingTest {
     @Test
     void carCount() {
         final int length = CAR_NAMES.split(",").length;
-        RacingGameSetting setting = new RacingGameSetting(null, CAR_NAMES, 0);
+        RacingGameSetting setting = new RacingGameSetting(CAR_NAMES, 0);
         assertThat(setting.getCars().toList()).hasSize(length);
     }
 }
