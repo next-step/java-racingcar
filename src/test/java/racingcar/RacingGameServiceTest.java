6fa97e2642cfc97c164bb5cc6495e300e3b0441d
dongchul <sah3122@naver.com>
2020-03-26 12:00:02 +0900

    Code Clean

--- src/test/java/racingcar/RacingGameServiceTest.java
+++ src/test/java/racingcar/RacingGameServiceTest.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Cars;
