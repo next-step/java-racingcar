aaf365f0f526c78dbad1c6c44c351c9f308e2513
dongchul <sah3122@naver.com>
2020-03-26 11:43:15 +0900

    refactor(RacingCar): remove Controller class

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -1,24 +1,34 @@
 package racingcar;
 
-import racingcar.controller.RacingCarController;
+import racingcar.domain.Cars;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.RandomMovingStrategy;
 import racingcar.dto.RacingCarRequestDto;
 import racingcar.dto.RacingCarResponseDto;
 import racingcar.service.RacingGameService;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
+import java.util.List;
+
 public class RacingCarApplication {
 
     public static void main(String[] args) {
+        RacingGameService racingGameService = new RacingGameService();
         ResultView resultView = new ResultView();
-        RacingCarController racingCarController = new RacingCarController(new RacingGameService());
+
         String racingCarNames = InputView.inputRacingCars();
         int numberOfTime = InputView.inputRacingGameTimes();
 
-        RacingCarResponseDto racingCarResponseDto = racingCarController
-                .startRacingGame(new RacingCarRequestDto(racingCarNames, numberOfTime));
+        RacingCarRequestDto racingCarRequestDto = new RacingCarRequestDto(racingCarNames, numberOfTime);
 
-        resultView.print(racingCarResponseDto.getRacingScores());
+        List<Cars> cars = racingGameService
+                .executeRacingGameNumberOfTimes(racingCarRequestDto, new RandomMovingStrategy());
+        List<Car> winners = racingGameService.findWinnerInRacingScores(cars);
+
+        RacingCarResponseDto racingCarResponseDto = new RacingCarResponseDto(cars, winners);
+
+        resultView.print(racingCarResponseDto.getCars());
         resultView.printWinner(racingCarResponseDto.getWinners());
     }
 }
