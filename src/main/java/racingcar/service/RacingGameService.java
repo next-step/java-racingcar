98a80b99ddea7d1759475cc7e9b52a92c4a78707
dongchul <sah3122@naver.com>
2020-03-26 11:59:31 +0900

    refactor(RacingCar): make racingGameService to stateless

--- src/main/java/racingcar/service/RacingGameService.java
+++ src/main/java/racingcar/service/RacingGameService.java
@@ -1,52 +1,62 @@
 package racingcar.service;
 
-import racingcar.domain.RacingScore;
+import racingcar.domain.Cars;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.MovingStrategy;
+import racingcar.dto.RacingCarRequestDto;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class RacingGameService {
+    private static final String DELIMITER = ",";
     private static final int INT_ZERO = 0;
     private static final int INT_ONE = 1;
 
-    private List<Car> cars = new ArrayList<>();
+    public List<Cars> executeRacingGameNumberOfTimes(RacingCarRequestDto racingCarRequestDto, MovingStrategy movingStrategy) {
+        String[] racingCarNames = racingCarRequestDto.getRacingCarNames().split(DELIMITER);
+        List<Car> cars = createCars(racingCarNames);
+        List<Cars> racingScores = new ArrayList<>();
 
-    private void createCars(String[] names) {
+        for (int i = 0; i < racingCarRequestDto.getNumberOfTime(); i++) {
+            racingScores.add(executeRacingGame(cars, movingStrategy));
+        }
+
+        return racingScores;
+    }
+
+    private List<Car> createCars(String[] names) {
         if (Objects.isNull(names) || names.length <= INT_ZERO) {
             throw new IllegalArgumentException("자동차의 개수는 1보다 커야 합니다.");
         }
+
+        List<Car> cars = new ArrayList<>();
+
         for (String name : names) {
             cars.add(new Car(name));
         }
+
+        return cars;
     }
 
-    private RacingScore executeRacingGame(MovingStrategy movingStrategy) {
+    private Cars executeRacingGame(List<Car> cars, MovingStrategy movingStrategy) {
         List<Car> executedCars = new ArrayList<>();
+
         for (Car car : cars) {
             car.move(movingStrategy);
             executedCars.add(car.clone());
         }
 
-        return new RacingScore(executedCars);
+        return new Cars(executedCars);
     }
 
-    public List<RacingScore> executeRacingGameNumberOfTimes(int numberOfTime, MovingStrategy movingStrategy) {
-        List<RacingScore> racingScores = new ArrayList<>();
-        for (int i = 0; i < numberOfTime; i++) {
-            racingScores.add(executeRacingGame(movingStrategy));
-        }
-        return racingScores;
+    public List<Car> findWinnerInRacingScores(List<Cars> cars) {
+        Cars lastScore = findLastScore(cars);
+        return lastScore.findWinners();
     }
 
-    public List<Car> findWinnerInRacingScores(List<RacingScore> racingScores) {
-        RacingScore lastRacingScore = racingScores.get(racingScores.size() - INT_ONE);
-        return lastRacingScore.findWinners();
-    }
-
-    public void createRacingCar(String[] racingCarNames) {
-        createCars(racingCarNames);
+    private Cars findLastScore(List<Cars> cars) {
+        return cars.get(cars.size() - INT_ONE);
     }
 }
--- src/test/java/racingcar/RacingGameServiceTest.java
+++ src/test/java/racingcar/RacingGameServiceTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.domain.RacingScore;
+import racingcar.domain.Cars;
+import racingcar.dto.RacingCarRequestDto;
 import racingcar.service.RacingGameService;
 
 import java.util.List;
@@ -13,23 +14,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class RacingGameServiceTest {
-    private static final String[] RACING_CAR_NAMES = {"ALPHA", "BETA", "CHARLIE"};
+    private static final String RACING_CAR_NAMES = "ALPHA,BETA,CHARLIE";
 
     @DisplayName("자동차 경주 정상 실행")
     @ParameterizedTest
     @ValueSource(ints = {3, 5, 7})
     void racingGameExecute(int numberOfTime) {
         RacingGameService racingGameService = new RacingGameService();
-        racingGameService.createRacingCar(RACING_CAR_NAMES);
-        List<RacingScore> racingScores = racingGameService.executeRacingGameNumberOfTimes(numberOfTime, () -> true);
+        List<Cars> cars = racingGameService.executeRacingGameNumberOfTimes(new RacingCarRequestDto(RACING_CAR_NAMES, numberOfTime), () -> true);
 
-        assertThat(racingScores).size().isEqualTo(numberOfTime);
+        assertThat(cars).size().isEqualTo(numberOfTime);
     }
 
-    @DisplayName("자동자 개수가 1보다 작을 시 생성 실패")
-    @Test
-    void createFailByNumberOfCar() {
-        String[] racingCars = {};
-        assertThatIllegalArgumentException().isThrownBy(() -> new RacingGameService().createRacingCar(racingCars));
+    @DisplayName("자동자 개수가 1보다 작을 시 레이싱 게임 실행 실패")
+    @ParameterizedTest
+    @ValueSource(ints = {3})
+    void createFailByNumberOfCar(int numberOfTime) {
+        String racingCars = "";
+        RacingGameService racingGameService = new RacingGameService();
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            racingGameService.executeRacingGameNumberOfTimes(new RacingCarRequestDto(racingCars, numberOfTime), () -> true);
+        });
     }
 }
