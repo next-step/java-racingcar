53b9aa4f16a63e17c18d4fc29d9e23c5e8e2342a
kesol0328 <kesol0328@gmail.com>
2020-03-28 00:55:27 +0900

    feat create Car

---
+++ src/main/java/racingwinner/Car.java
@@ -0,0 +1,11 @@
+package racingwinner;
+
+public class Car {
+    String name;
+    int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+}
