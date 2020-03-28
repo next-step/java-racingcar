134a526538ff2ceac13c90a34172cf39785dfa18
manhyuk <manhyuk@kw.ac.kr>
2020-03-28 21:33:57 +0900

    refact: 우승자 출력 추가

--- src/main/java/RacingCar/ResultView.java
+++ src/main/java/RacingCar/ResultView.java
@@ -1,24 +1,67 @@
 package RacingCar;
 
+import RacingCar.model.Car;
 import RacingCar.model.Result;
 
-import java.util.List;
+import java.util.*;
 
 public class ResultView {
 
     static void print(List<Result> results) {
-        results.forEach(stage -> {
-            printStageResult(stage);
-        });
+        results.forEach(stage -> printStageResult(stage));
+        printRaceWinners(results);
     }
 
     private static void printStageResult(Result result) {
         result.getCars().forEach(car -> {
+            System.out.print(car.getName() + " : ");
             System.out.println(getMoveString(car.getMove()));
         });
         System.out.println();
     }
 
+    private static void printRaceWinners(List<Result> results) {
+        final String WINNERS_MESSAGE = "가 최종 우승했습니다.";
+
+        List<String> winners = getWinners(results);
+        System.out.println(winners.toString() + WINNERS_MESSAGE);
+    }
+
+    private static List<String> getWinners(List<Result> results) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (Result result : results) {
+            sumStage(result, map);
+        }
+
+        return getKeysFromValue(map,getMaxMove(map));
+    }
+
+    private static void sumStage(Result result, Map<String, Integer> map) {
+        for (Car car : result.getCars()) {
+            sumStageMove(map, car);
+        }
+    }
+
+    private static void sumStageMove(Map<String, Integer> map, Car car) {
+        if (map.containsKey(car.getName())) {
+            int count = map.get(car.getName()) + car.getMove();
+            map.put(car.getName(), count);
+            return;
+        }
+
+        map.put(car.getName(), car.getMove());
+    }
+
+    private static int getMaxMove(Map<String, Integer> map) {
+        int max = Integer.MIN_VALUE;
+        for (int move: map.values()) {
+            if (move > max) {
+                max = move;
+            }
+        }
+        return max;
+    }
 
     private static String getMoveString(int count) {
         StringBuilder stringBuilder = new StringBuilder();
@@ -26,6 +69,16 @@ public class ResultView {
             stringBuilder.append('-');
         }
         return stringBuilder.toString();
+    }
+
+    private static List<String> getKeysFromValue(Map map, int value) {
+        List<String> list = new ArrayList<>();
+        for (Object key : map.keySet()) {
+            if (map.get(key).equals(value)) {
+                list.add(key.toString());
+            }
+        }
+        return list;
     }
 
 }
