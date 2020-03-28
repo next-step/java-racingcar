cc8acee546567f45785bf9545806c5b9e82aa7b6
kesol0328 <kesol0328@gmail.com>
2020-03-28 17:32:45 +0900

    feat find winner of racing.

--- src/main/java/racingwinner/Car.java
+++ src/main/java/racingwinner/Car.java
@@ -4,6 +4,10 @@ public class Car {
     String name;
     int position;
 
+    public Car(String name) {
+        this(name,0);
+    }
+
     public Car(String name, int position) {
         this.name = name;
         this.position = position;
