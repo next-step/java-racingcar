12cedd0104db88515366939e118c21d1d8ce38ac
manhyuk <manhyuk@kw.ac.kr>
2020-03-28 21:32:45 +0900

    refact: InputView 수정

--- src/main/java/RacingCar/InputView.java
+++ src/main/java/RacingCar/InputView.java
@@ -1,39 +1,48 @@
 package RacingCar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 class InputView {
 
     private Scanner scanner = new Scanner(System.in);
 
-    private int car;
+    private List<String> names;
     private int stage;
 
     void showInput() {
-        String CAR_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+        String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
         String STAGE_INPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
 
 
-        System.out.println(CAR_INPUT_MESSAGE);
-        setCar(scanner.nextInt());
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
+        String expression = scanner.nextLine();
+        String[] splitedNames = expression.split(",");
+
+        setNames(splitedNames);
+
+        System.out.println(getNames());
 
         System.out.println(STAGE_INPUT_MESSAGE);
         setStage(scanner.nextInt());
     }
 
-    private void setCar(int car) {
-        this.car = car;
-    }
 
     private void setStage(int stage) {
         this.stage = stage;
     }
 
-    public int getCar() {
-        return car;
+
+    private void setNames(String[] names) {
+        this.names = new ArrayList<>(Arrays.asList(names));
     }
 
-    public int getStage() {
+    List<String> getNames() {
+        return names;
+    }
+    int getStage() {
         return stage;
     }
 }
