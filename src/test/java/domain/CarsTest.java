47bafbd11dfd4398b3f779d1112afb4aaa90a406
kks <rudtjs9956@gmail.com>
2020-03-28 15:09:44 +0900

    feat(CarsTest): getWinners 테스트 추가

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -2,6 +2,8 @@ package domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
@@ -13,4 +15,26 @@ public class CarsTest {
         assertThat(cars.getCars()).hasSize(3);
         assertThat(cars.getCars()).contains(new Car("a"), new Car("b"), new Car("c"));
     }
+
+    @Test
+    public void getWinnersTest() {
+        Cars cars = new Cars(Arrays.asList(new Car("kks", 0),
+                new Car("kjm", 0),
+                new Car("bjs", 4)));
+
+        assertThat(cars.getWinners()).hasSize(1);
+        assertThat(cars.getWinners()).contains("bjs");
+    }
+
+    @Test
+    public void canGetGreaterThanTwoWinnersTest() {
+        Cars cars = new Cars(Arrays.asList(new Car("kks", 0),
+                new Car("kjm", 4),
+                new Car("bjs", 4)));
+
+        assertThat(cars.getWinners()).hasSize(2);
+        assertThat(cars.getWinners()).contains("bjs", "kjm");
+    }
+
+
 }
