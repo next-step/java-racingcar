60f9e64578b24bcee4c135514efb237eeb645f17
manhyuk <manhyuk@kw.ac.kr>
2020-03-29 19:50:39 +0900

    refact: 2depth -> 1depth

--- src/main/java/RacingCarV2/view/WinnerView.java
+++ src/main/java/RacingCarV2/view/WinnerView.java
@@ -49,20 +49,27 @@ public class WinnerView {
     int getMaxPosition(Map<String, Integer> map) {
         int max = Integer.MIN_VALUE;
         for (int position: map.values()) {
-            if (position > max) {
-                max = position;
-            }
+            max = getBigInteger(position, max);
         }
         return max;
     }
 
     private List<String> getKeysFromValue(Map<String, Integer> map, int value) {
         List<String> list = new ArrayList<>();
-        for (Object key : map.keySet()) {
-            if (map.get(key).equals(value)) {
-                list.add(key.toString());
-            }
+        for (String key : map.keySet()) {
+            boolean isAdd = map.get(key).equals(value);
+            addTargetValue(list, key, isAdd);
         }
         return list;
+    }
+
+    private void addTargetValue(List<String> list, String key, boolean isPush) {
+        if (isPush) {
+            list.add(key);
+        }
+    }
+
+    int getBigInteger(int position, int max) {
+        return position > max ? position : max;
     }
 }
