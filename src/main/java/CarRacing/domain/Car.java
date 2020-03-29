9a7c805eabe89f6e842809089a679ac1466c6fa0
jinwoo <jwlee@qualson.com>
2020-03-29 22:07:37 +0900

    test(CarTest) : Change MovingStrategy to MustMovingStrategy to test currentPosition change

--- src/main/java/CarRacing/domain/Car.java
+++ src/main/java/CarRacing/domain/Car.java
@@ -26,4 +26,5 @@ public class Car {
             this.currentPosition++;
         }
     }
+
 }
--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -2,6 +2,7 @@ package step3;
 
 import CarRacing.domain.Car;
 import CarRacing.domain.MovingStrategy;
+import CarRacing.domain.MustMovingStrategy;
 import CarRacing.domain.RacingStrategy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,14 +16,14 @@ public class CarTest {
 
     @BeforeEach
     void setUp() {
-        racingStrategy = new MovingStrategy();
-        car = new Car("jinwoo");
+        racingStrategy = new MustMovingStrategy();
+        car = new Car("jinwoo",4);
     }
 
     @Test
-    @DisplayName("움직이는 테스트 진행 움직인다면 1, 움직이지 않으면 1이 나옴")
+    @DisplayName("움직이는 테스트")
     void carMoveTest() {
         car.move(racingStrategy);
-        assertThat(car.getCurrentPosition()).isIn(0, 1);
+        assertThat(car.getCurrentPosition()).isEqualTo(5);
     }
 }
