8a67da5ae15f90ec8d6d4cccf7c2dcea9634a4cc
kiyeoneee <kyeoneee@gmail.com>
2020-03-30 21:32:16 +0900

    fix (CarsTest) : 빈/null 문자열이 input일 경우에 대한 테스트 삭제 * 이미 valid 체크를 하고 Cars 생성을 하므로 테스트가 필요 없음

--- src/test/java/racing/domain/CarsTest.java
+++ src/test/java/racing/domain/CarsTest.java
@@ -16,14 +16,6 @@ public class CarsTest {
     private static final String DEFAULT_CAR_NAME = "abc";
 
     @ParameterizedTest
-    @EmptySource
-    @DisplayName("주어진 문자열이 null 이거나 빈 문자열일 경우 empty list return 테스트")
-    void getEmptyListWithCarSizeZero(String input) {
-        Cars cars = new Cars(input);
-        assertTrue(cars.getList().isEmpty());
-    }
-
-    @ParameterizedTest
     @CsvSource(value = {"a,b,c:3", "a,b,123,a2b:4"}, delimiter = ':')
     @DisplayName("주어진 자동차들의 이름을 가진 차들의 배열 생성 테스트")
     void generateCarsListTest(String input, int expected) {
