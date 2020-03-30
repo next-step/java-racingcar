1ead9c34d0cae92a033064e1fb56f1c29312d460
hjdeepsleep <hjdeepsleep@gmail.com>
2020-03-31 08:50:02 +0900

    refactor(UserInput.java): 불필요한 클래스 삭제

--- src/main/java/racinggame/controller/RacingGameApplication.java
+++ src/main/java/racinggame/controller/RacingGameApplication.java
@@ -1,19 +1,21 @@
 package racinggame.controller;
 
-import racinggame.domain.UserInput;
 import racinggame.domain.racing.Racing;
+import racinggame.util.StringUtil;
 import racinggame.view.InputView;
 import racinggame.view.ResultView;
 
 public class RacingGameApplication {
 
+    private static final String CAR_NAME_DELIMITER = ",";
+
     public static void main(String[] args) {
         InputView inputView = new InputView();
         inputView.readUserInput();
 
-        UserInput userInput = new UserInput(inputView.getCarNames(), inputView.getGameCount());
-
-        Racing racing = new Racing(userInput.getCarNames(), userInput.getGameCount());
+        Racing racing = new Racing(
+                StringUtil.splitString(inputView.getCarNames(), CAR_NAME_DELIMITER),
+                inputView.getGameCount());
         racing.playAllRound();
 
         ResultView resultView = new ResultView();
