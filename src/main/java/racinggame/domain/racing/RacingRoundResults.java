7944e008c364727d6567bf081e90d9cdf48c7e79
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-02 01:50:32 +0900

    refactor (RacingRoundResults.java): 사용 안하는 생성자 삭제

--- src/main/java/racinggame/domain/racing/RacingRoundResults.java
+++ src/main/java/racinggame/domain/racing/RacingRoundResults.java
@@ -3,7 +3,6 @@ package racinggame.domain.racing;
 import racinggame.domain.car.Cars;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,11 +12,6 @@ public class RacingRoundResults {
 
     public RacingRoundResults() {
         this(new ArrayList<>());
-    }
-
-    public RacingRoundResults(Cars cars) {
-        this.results = Collections.unmodifiableList(
-                new ArrayList<>(Arrays.asList(cars)));
     }
 
     public RacingRoundResults(List<Cars> cars) {
