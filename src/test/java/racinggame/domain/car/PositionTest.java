51b5bed685f492f2d57bbe7cb2bbf29e013e298b
hjdeepsleep <hjdeepsleep@gmail.com>
2020-04-02 00:26:43 +0900

    test (PositionTest.java): 객체 비교 테스트 코드 추카

--- src/test/java/racinggame/domain/car/PositionTest.java
+++ src/test/java/racinggame/domain/car/PositionTest.java
@@ -9,7 +9,7 @@ class PositionTest {
 
     @DisplayName("동일한 매개변수를 전달하면 같은 것으로 비교한다")
     @Test
-    public void constructor() throws Exception {
+    public void constructor_success() throws Exception {
         //given
         Position position1 = new Position(1);
         Position position2 = new Position(1);
@@ -20,7 +20,7 @@ class PositionTest {
 
     @DisplayName("거리를 한칸 이동 한다")
     @Test
-    public void move() throws Exception {
+    public void move_success() throws Exception {
         //given
         Position position = new Position();
         Position compare = new Position(1);
@@ -30,6 +30,20 @@ class PositionTest {
 
         //then
         assertThat(position.equals(compare)).isTrue();
+    }
+
+    @DisplayName("멤버 변수를 이용하여 크기를 비교 한다")
+    @Test
+    public void compare_success() throws Exception {
+        //given
+        Position position1 = new Position(10);
+        Position position2 = new Position(20);
+
+        //when
+        int i = position1.compareTo(position2);
+
+        //then
+        assertThat(i).isEqualTo(-1);
     }
 }
 
