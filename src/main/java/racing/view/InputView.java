524c24e6c838ee5d9168b254cc57db00f95b2b91
kiyeon <kyeoneee@gmail.com>
2020-04-04 16:33:34 +0900

    refactor (Cars) : step4 피드백 반영 - 재사용하지 않는 지역변수의 사용을 최소화 하도록 수정

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    private static String GET_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private static String GET_GAME_CNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private static final String GET_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String GET_GAME_CNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
 
     public static String getCarNames() {
         System.out.println(GET_CARS_NAME_MESSAGE);
@@ -14,15 +14,11 @@ public class InputView {
     }
 
     public static int getTryCnt() {
-        int result;
-
         try {
             System.out.println(GET_GAME_CNT_MESSAGE);
-            result = Integer.parseInt(scanner.nextLine());
+            return Integer.parseInt(scanner.nextLine());
         } catch (NumberFormatException e) {
-            result = Integer.MIN_VALUE;
+            return Integer.MIN_VALUE;
         }
-
-        return result;
     }
 }
