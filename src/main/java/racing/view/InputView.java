8b85f7ed855122143fcadb76eba949c4649061ad
kiyeoneee <kyeoneee@gmail.com>
2020-04-10 02:52:42 +0900

    refactor (InputView) : View의 요구사항 중 Controller에 있는 기능 View로 이동

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -1,26 +1,47 @@
 package racing.view;
 
+import racing.domain.ValidChecker;
+
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
-
+    private static final int DEFAULT_TRY_CNT = 0;
     private static final String GET_CARS_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String GET_GAME_CNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
     private static final String INVALID_STRING_INPUT_MSG = "비어있지 않은 문자열을 입력 해 주세요!";
     private static final String INVALID_INT_INPUT_MSG = "양의 정수 값을 입력 해 주세요!";
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static String getCarNames() {
-        System.out.println(GET_CARS_NAME_MESSAGE);
-        return scanner.nextLine();
+        String carNames;
+
+        do {
+            System.out.println(GET_CARS_NAME_MESSAGE);
+
+            carNames = scanner.nextLine();
+        } while(!ValidChecker.checkInput(carNames));
+
+        return carNames;
     }
 
     public static int getTryCnt() {
-        try {
+        int tryCnt;
+
+        do {
             System.out.println(GET_GAME_CNT_MESSAGE);
+
+            tryCnt = parseTryCnt();
+        } while (!ValidChecker.checkInput(tryCnt));
+
+        return tryCnt;
+    }
+
+    private static int parseTryCnt() {
+        try {
             return Integer.parseInt(scanner.nextLine());
         } catch (NumberFormatException e) {
-            return Integer.MIN_VALUE;
+            return DEFAULT_TRY_CNT;
         }
     }
 
