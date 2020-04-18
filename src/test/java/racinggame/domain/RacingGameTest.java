1bf58b0184f7f9987f95527486b61da8e8c129f9
kesol0328 <kesol0328@gmail.com>
2020-04-18 21:44:27 +0900

    (test) 1. 항상 움직이는 상황 2. 항상 움직이지 못하는 상황 테스트 케이스 추가

--- src/test/java/racinggame/domain/RacingGameTest.java
+++ src/test/java/racinggame/domain/RacingGameTest.java
@@ -3,15 +3,15 @@ package racinggame.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racinggame.domain.RacingGame;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
     private RacingGame racingGame;
-    MoveStrategy moveStrategy = new RandomBasedMoveStrategy();
+    private MoveStrategy moveStrategy = new RandomBasedMoveStrategy();
 
     @BeforeEach
     void setUp() {
@@ -29,5 +29,27 @@ public class RacingGameTest {
     @Test
     void racingTest() {
         assertThat(racingGame.allRounds(moveStrategy)).hasSize(15);
+    }
+
+    @DisplayName("항상 움직일 수 있다고 가정, 모든 차가 1라운드 후 전진했는지 확인")
+    @Test
+    void everyCarMoveTest() {
+        racingGame = new RacingGame(Arrays.asList(new String[]{"pobi", "crong", "honux"}), 1);
+        List<Car> allCars = racingGame.allRounds(() -> true);
+
+        assertThat(allCars).allSatisfy(car -> {
+            assertThat(car.getPosition()).isEqualTo(1);
+        });
+    }
+
+    @DisplayName("항상 움직일 수 없다고 가정, 모든 차가 1라운드 후 그대로인지 확인")
+    @Test
+    void everyCarDoNotMoveTest() {
+        racingGame = new RacingGame(Arrays.asList(new String[]{"pobi", "crong", "honux"}), 1);
+        List<Car> allCars = racingGame.allRounds(() -> false);
+
+        assertThat(allCars).allSatisfy(car -> {
+            assertThat(car.getPosition()).isEqualTo(0);
+        });
     }
 }
