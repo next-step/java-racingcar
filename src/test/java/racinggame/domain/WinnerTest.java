4191d17c7ad245fa9d3724a54f71f717809a5335
kesol0328 <kesol0328@gmail.com>
2020-04-19 13:15:38 +0900

    (fix) 인스턴스 변수 private 으로 선언

--- src/test/java/racinggame/domain/WinnerTest.java
+++ src/test/java/racinggame/domain/WinnerTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinnerTest {
-    List<Car> racingCars;
+    private List<Car> racingCars;
 
     @BeforeEach
     void setUp() {
