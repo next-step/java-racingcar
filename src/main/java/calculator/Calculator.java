07a3fd4c9fa06c589a2af287b016273f2202ff7d
chulman <chlcjfals0122@gmail.com>
2020-05-09 23:57:17 +0900

    step2: Calculator 인터페이스 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package calculator;
+
+@FunctionalInterface
+public interface Calculator {
+
+    double calculate(String input) throws IllegalArgumentException;
+
+    default double plus(double a, double b) {
+        return a + b;
+    }
+
+    default double minus(double a, double b) {
+        return a - b;
+    }
+
+    default double multiply(double a, double b) {
+        return a * b;
+    }
+
+    default double divide(double a, double b) {
+        if(b == 0) {
+            throw new ArithmeticException("division with zero is not allowed.");
+        }
+        return a / b;
+    }
+}
