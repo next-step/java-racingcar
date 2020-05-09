ce6eed5e33f2ce845eca8b65e391f27622aff244
RudiaMoon <impactyoung@gmail.com>
2020-05-09 11:28:43 +0900

    test 요구사항2_split

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -6,8 +6,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
-    void split() {
+    void 요구사항1_split() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void 요구사항2_split() {
+        String data = "(1,2)";
+        String result = data.substring(1, data.length()-1);
+        assertThat("1,2").isEqualTo(result);
     }
 }
