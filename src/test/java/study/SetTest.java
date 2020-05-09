bf555f102ff80c18ab513dd212a8df2733643c2e
iljun <sumusb34@gmail.com>
2020-05-09 15:47:16 +0900

    1단계 - 학습 테스트 실시 (#671)
    * implement requirements 1
    
    * implement requirements 2
    
    * implement requirements 3
    
    * implement requirements 1 for Set Collection study
    
    * implement requirements 2 for Set Collection study
    
    * implement requirements 3 for Set Collection study

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("요구사항 1 : size method를 이용한 Set의 크기 확인")
+    void size() {
+        assertThat(this.numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("요구사항 2 : parameterizedTest를 활용한 중복 코드 제거")
+    void contains(int searchValue) {
+        assertThat(this.numbers.contains(searchValue)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("요구사항 3 : contains method refactoring")
+    void contains(int searchValue, boolean expected) {
+        assertThat(this.numbers.contains(searchValue)).isEqualTo(expected);
+    }
+}
