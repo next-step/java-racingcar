841b7eb77537a7ca9ad2237d4ed2e713174a9234
Gobukgol <zxczc159@gmail.com>
2020-05-09 21:54:19 +0900

    1단계 학습테스트 실습 요구사항 2 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -17,4 +17,11 @@ public class StringTest {
         String[] result = "1".split(",");
         assertThat(result).containsExactly("1");
     }
+
+    @Test
+    void subString() {
+        String source = "(1,2)";
+        String result = source.substring(1, source.length() - 1);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
