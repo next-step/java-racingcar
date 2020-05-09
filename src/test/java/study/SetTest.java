693b6aaf7a3b353f929ee571d4c6cab5878bb19e
CoDingDongHyuk <48036767+DongHyukki@users.noreply.github.com>
2020-05-09 15:43:15 +0900

    Step 1 (#670)
    * split study test
    
    * set test
    
    * display name

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void setContainsTest() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("set contains parameterized test")
+    @ValueSource(ints = {1,2,3})
+    void setContainsParameterizedTest(int testNumbers) {
+        assertThat(numbers.contains(testNumbers)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("set contains parameterized test with csv")
+    @CsvSource({"1,true", "2,true", "3,true", "4,false"})
+    void setContainsParameterizedTestWithCsv(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
