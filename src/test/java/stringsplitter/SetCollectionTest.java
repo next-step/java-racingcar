fd4b623b7f5a9916645f8980ddace53063472f8e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-10 01:32:12 +0900

    test: check set size method
    - using size method from Set
    
    요구사항 1

--- src/test/java/stringsplitter/SetCollectionTest.java
+++ src/test/java/stringsplitter/SetCollectionTest.java
@@ -1,8 +1,12 @@
 package stringsplitter;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class SetCollectionTest {
   private Set<Integer> numbers;
@@ -14,5 +18,11 @@ public class SetCollectionTest {
     numbers.add(1);
     numbers.add(2);
     numbers.add(3);
+  }
+
+  @DisplayName("Test size method of Set")
+  @Test
+  void checkSetSizeMethod() {
+    assertThat(numbers.size()).isEqualTo(3);
   }
 }
