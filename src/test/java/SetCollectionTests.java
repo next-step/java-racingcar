11397d5d5afdfa64d02a995bc7b70bf7275dd26d
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-09 17:48:25 +0900

    구현: Set 학습 테스트를 위한 테스트 데이터 생성
    - BeforeEach를 이용해 1, 2, 3으로 구성된 테스트용 HashSet 구현
    - 1을 중복해서 두번 추가함

---
+++ src/test/java/SetCollectionTests.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetCollectionTests {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setup() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+}
