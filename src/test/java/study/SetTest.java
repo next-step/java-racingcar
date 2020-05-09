5bc989fd5f820995999c3150411dfdbbbe4806fc
Gobukgol <zxczc159@gmail.com>
2020-05-09 22:13:17 +0900

    1단계 Set Collection 학습 테스트 요구사항 2 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,12 @@ public class SetTest {
     @Test
     void size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("contains 메소드는 Set 안에 매개값으로 받은 값의 존재 여부를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
