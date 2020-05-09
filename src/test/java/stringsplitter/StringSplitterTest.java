78e89e453c52fa040272aab4818046f39b88e812
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-10 01:21:00 +0900

    test :white_check_mark: add testing method to trim brackets
    - acquiring "1,2" from "(1,2)" by trimming brackets, using substring method of String
    
    요구사항 2

--- src/test/java/stringsplitter/StringSplitterTest.java
+++ src/test/java/stringsplitter/StringSplitterTest.java
@@ -5,17 +5,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+//String 클래스에 대한 학습 테스트
 public class StringSplitterTest {
 
+  //요구사항 1
   @DisplayName("Verify the feature of splitting the string `String '1,2'` according to the spliter `:`")
   @Test
   void splitStringTestToHaveTwoElements() {
+    assertThat("1,2".split(",")).contains("1");
+    assertThat("1,2".split(",")).contains("2");
+    assertThat("1,2".split(",")).contains("1", "2");
     assertThat("1,2".split(",")).containsExactly("1", "2");
   }
 
   @DisplayName("Verify the feature of splitting the string `1` to have only one substance `1` in list")
   @Test
   void splitStringTestToHaveOneElement() {
+    assertThat("1".split(",")).contains("1");
     assertThat("1".split(",")).containsExactly("1");
+  }
+
+  //요구사항 2
+  @DisplayName("Return `1,2` by trimming brackets from (1,2) utilizing substring method()")
+  @Test
+  void trimBracketsFromString() {
+    assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
   }
 }
