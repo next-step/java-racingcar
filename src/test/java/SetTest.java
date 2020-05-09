7b04cd949c986391fc3b1bacd27bf64736c0c5c0
eunji <43842782+eunji12@users.noreply.github.com>
2020-05-09 19:39:56 +0900

    1단계 - 학습 테스트 실습 PR (#676)
    * String 클래스에 대한 학습 테스트
    
    * String 클래스에 대한 학습 테스트
    
    메서드 명 변경
    
    * Set Collection에 대한 학습 테스트
    
    Co-authored-by: eunji <들쥐12@>

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,55 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+
+  private Set<Integer> numbers;
+
+  @BeforeEach
+  void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+
+  @Test
+  @DisplayName("numbers Set의 사이즈를 확인")
+  void getSetSize() {
+    assertThat(numbers.size()).isEqualTo(3);
+  }
+
+  @Test
+  @DisplayName("Set에 1,2,3이 존재하는지 확인")
+  void checkContains() {
+    assertThat(numbers.contains(1)).isTrue();
+    assertThat(numbers.contains(2)).isTrue();
+    assertThat(numbers.contains(3)).isTrue();
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 3})
+  @DisplayName("@ValueSource를 이용하여 Set에 1,2,3이 존재하는지 확인")
+  void checkContinsUsingPram(int value) {
+    assertThat(numbers.contains(value)).isTrue();
+  }
+
+  @ParameterizedTest
+  @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false" })
+  @DisplayName("@CsvSource를 이용하여 Set에 1,2,3이 존재하는지 확인")
+  void checkContainsUsingCsv(int input, boolean expected) {
+    boolean result = numbers.contains(input);
+    assertEquals(expected, result);
+  }
+
+}
