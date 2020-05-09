4b373394be9c210348cb420df7a2ae506884eb51
iljun <sumusb@naver.com>
2020-05-09 15:29:28 +0900

    implement requirements 1 for Set Collection study

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("요구사항 1 : size method를 이용한 Set의 크기 확인")
+    void size() {
+        assertThat(this.numbers.size()).isEqualTo(3);
+    }
+}
