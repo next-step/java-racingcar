d1abf6c82d0c3ac7f3daa966c1367d8f931dec92
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-09 17:52:33 +0900

    구현: Set 요구사항2
    - contains 메소드를 통해 해당 Set의 내부에 해당 값 존재 여부 확인 가능한지 테스트
    - ParameterizedTest 기능을 통해 테스트 코드에서 불필요한 중복 제거

--- src/test/java/SetCollectionTests.java
+++ src/test/java/SetCollectionTests.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -23,5 +25,12 @@ public class SetCollectionTests {
     @Test
     void setSizeMethodTest() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Set의 contains 메소드를 통해 내부의 값 존재 여부 확인 가능한지 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void setContainsTestWithParameterizedTest(int indexNumber) {
+        assertThat(numbers.contains(indexNumber)).isTrue();
     }
 }
