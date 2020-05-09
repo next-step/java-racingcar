d46742701499ae9e62b2acb958220f7795585837
RudiaMoon <impactyoung@gmail.com>
2020-05-09 23:45:24 +0900

    refactor : 덧셈 테스트 네이밍 및 setup에 계산하기 위한 default값 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,9 +10,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Calculator 사칙 연산")
 class CalculatorTest {
+    private int defaultNumber;
+
+    @BeforeEach
+    void setUp() {
+        defaultNumber = 2;
+    }
+
     @ParameterizedTest
-    @CsvSource(value = {"1=2", "2=3", "-1=0", "-3=-2"}, delimiter = '=')
-    void 덧셈(int input, int result) {
-        assertThat(Calculator.add(1, input)).isEqualTo(result);
+    @CsvSource(value = {"1=3", "2=4", "-1=1", "-3=-1"}, delimiter = '=')
+    void 덧셈(int startNumber, int result) {
+        assertThat(Calculator.add(startNumber, defaultNumber)).isEqualTo(result);
     }
 }
