a9a0810971a4fb1647a89a15427eead3f426de20
iljun <sumusb@naver.com>
2020-05-09 15:02:19 +0900

    implement requirements 1

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,16 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+        
+        result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+}
