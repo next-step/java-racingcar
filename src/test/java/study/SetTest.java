e5e3ea014ac5bfd4ddea16991ca4dee1858b9955
Gobukgol <zxczc159@gmail.com>
2020-05-09 22:18:32 +0900

    1단계 Set Collection 학습 테스트 요구사항 3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,5 +35,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @DisplayName("@Csvsource를 이용한 contains 메소드 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsWithCsvSource(int input, boolean expect) {
+        assertThat(numbers.contains(input)).isEqualTo(expect);
     }
 }
