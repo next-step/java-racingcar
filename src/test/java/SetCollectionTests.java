efb3eaa22dd86fedbc630fdf04201ee75f7ed232
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-09 18:02:56 +0900

    구현: Set Collection 요구사항3
    - Set contains 메소드로 수행할 수 있는 경우와 없는 경우를 섞어서 테스트
    - CsvSource로 간단한 복합 타입을 파라미터로 던지는 방법을 배움

--- src/test/java/SetCollectionTests.java
+++ src/test/java/SetCollectionTests.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -32,5 +33,12 @@ public class SetCollectionTests {
     @ValueSource(ints = {1, 2, 3})
     void setContainsTestWithParameterizedTest(int indexNumber) {
         assertThat(numbers.contains(indexNumber)).isTrue();
+    }
+
+    @DisplayName("Set contains 메소드에 수행할 수 있는 경우와 수행할 수 없는 경우를 섞어서 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "10:false", "2:true", "100:false", "3:true"}, delimiter = ':')
+    void setContainsTestWithValidAndInvalidMixinCase(int targetNumber, boolean result) {
+        assertThat(numbers.contains(targetNumber)).isEqualTo(result);
     }
 }
