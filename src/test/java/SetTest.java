e409fc44580090efb7f02d284c8a44fde9cf72fa
Yoon Jisoo <34590027+yjs2952@users.noreply.github.com>
2020-05-09 13:42:28 +0900

    Step1 (#665)
    * split study test
    
    * substring study test
    
    * step1 study test

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,48 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    public void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(1);
+    }
+
+    @DisplayName("size 테스트")
+    @Test
+    public void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("contains 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void containsTest(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("true 인 경우만 테스트 가능하다")
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    public void lowerCaseTest(String inputStr, String expectedStr){
+        Integer input = Integer.parseInt(inputStr);
+        Boolean expected = Boolean.parseBoolean(expectedStr);
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    public void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    public void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("StringIndexOutOfBoundsException 예외 테스트")
+    @Test
+    public void exception(){
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
