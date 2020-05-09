3f00d1021e017b793347849bf0b1d8703b5a6980
RudiaMoon <impactyoung@gmail.com>
2020-05-09 12:01:58 +0900

    test StringTes와 SetTest 미션완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -14,7 +16,19 @@ public class StringTest {
     @Test
     void 요구사항2_split() {
         String data = "(1,2)";
-        String result = data.substring(1, data.length()-1);
+        String result = data.substring(1, data.length() - 1);
         assertThat("1,2").isEqualTo(result);
+    }
+
+    @DisplayName("charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트")
+    @Test
+    void 요구사항3_split() {
+        String data = "abc";
+
+        assertThatThrownBy(() -> {
+            data.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+
     }
 }
