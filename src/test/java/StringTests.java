4dd2566351b127c9e78a1eb1e31c52597385d58e
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-09 17:29:05 +0900

    구현: 요구사항 1-2
    - "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현
    - 1-1 케이스와 마찬가지로 배열이 반환되기 때문에 containsExactly를 사용하여 구현

--- src/test/java/StringTests.java
+++ src/test/java/StringTests.java
@@ -6,7 +6,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTests {
     @DisplayName("String '1,2'를 ','로 split 했을 때 1과 2로 잘 분리되는지 확인")
     @Test
-    void stringSplitTest() {
+    void stringSplitWithTwoNumbers() {
         assertThat("1,2".split(",")).containsExactly("1", "2");
+    }
+
+    @DisplayName("String '1'을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인")
+    @Test
+    void stringSplitWithOneNumber() {
+        assertThat("1".split(",")).containsExactly("1");
     }
 }
