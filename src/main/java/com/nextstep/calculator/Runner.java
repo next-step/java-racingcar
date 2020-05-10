f385e8b1281311f21ec5c0b79caee00f29aead20
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-10 22:30:34 +0900

    리팩토링: Runner
    - 기존 기능에서 Operator를 불러오는 책임을 OperatorSelector에 위임
    - 실질적인 계산 책임을 반환된 Operator 객체에 위임

--- src/main/java/com/nextstep/calculator/Runner.java
+++ src/main/java/com/nextstep/calculator/Runner.java
@@ -10,27 +10,11 @@ public class Runner {
 
         for (int i = 1; i < parsed.size() - 1; i += 2) {
             Number secondNumber = Number.stringToNumber(parsed.get(i + 1));
-            String operator = parsed.get(i);
+            Operator operator = OperatorSelector.select(parsed.get(i));
 
-            result = numberCalculate(result, secondNumber, operator);
+            result = operator.operate(result, secondNumber);
         }
 
         return result.value();
-    }
-
-    private static Number numberCalculate(Number firstNumber, Number secondNumber, String operator) {
-        if (operator.equals("+")) {
-            return firstNumber.plus(secondNumber);
-        }
-        if (operator.equals("-")) {
-            return firstNumber.minus(secondNumber);
-        }
-        if (operator.equals("*")) {
-            return firstNumber.multiply(secondNumber);
-        }
-        if (operator.equals("/")) {
-            return firstNumber.divide(secondNumber);
-        }
-        throw new IllegalArgumentException();
     }
 }
