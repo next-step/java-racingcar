4bd30855c81b0fe802ff4e2e1f5bbf474046ecfa
eunjeong <cs801458j@gmail.com>
2020-05-11 03:31:06 +0900

    요구사항 3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -44,4 +44,11 @@ public class SetTest {
         assertThat(numbers.contains(input)).isTrue();
     }
 
+    //  요구사항 3
+    @DisplayName("")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void checkContainValues(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
 }
