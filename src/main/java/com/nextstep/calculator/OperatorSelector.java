6cd3f036647816a1538ffd6e064890c101579ad4
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-10 22:36:31 +0900

    리팩토링: OperatorSelector
    - NPL 방지를 위해 equals 비교 대상의 위치 변경

--- src/main/java/com/nextstep/calculator/OperatorSelector.java
+++ src/main/java/com/nextstep/calculator/OperatorSelector.java
@@ -2,16 +2,16 @@ package com.nextstep.calculator;
 
 public class OperatorSelector {
     public static Operator select(String stringValue) {
-        if (stringValue.equals("+")) {
+        if ("+".equals(stringValue)) {
             return Operator.PLUS;
         }
-        if (stringValue.equals("-")) {
+        if ("-".equals(stringValue)) {
             return Operator.MINUS;
         }
-        if (stringValue.equals("*")) {
+        if ("*".equals(stringValue)) {
             return Operator.MULTIPLY;
         }
-        if (stringValue.equals("/")) {
+        if ("/".equals(stringValue)) {
             return Operator.DIVIDE;
         }
         throw new IllegalArgumentException();
