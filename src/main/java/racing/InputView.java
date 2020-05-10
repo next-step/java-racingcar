30edda0ae6d968b80fd4c56c4a928878f9fff0ad
iljun <sumusb@naver.com>
2020-05-10 23:52:50 +0900

    [feature] implement Scanner input

---
+++ src/main/java/racing/InputView.java
@@ -0,0 +1,14 @@
+package racing;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public void input() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int number = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int count = scanner.nextInt();
+    }
+}
