5fd5dd76be32208709ee33a2f0eb198af7d71a04
chulman <chlcjfals0122@gmail.com>
2020-05-10 21:06:21 +0900

    refactor (Operator) : step2 PR 리뷰 사항 반영

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -8,24 +8,16 @@ public class StringCalculator implements Calculator {
 
     @Override
     public double calculate(String input) {
-        List<String> arguments = Argument.toList(input);
+        List<String> arguments = Argument.stringExpressionToList(input);
 
         double result = Double.valueOf(arguments.get(0));
 
         for (int i = 1; i < arguments.size(); i += 2) {
             Operator operator = Operator.getOperator(arguments.get(i));
             double secondValue = Double.valueOf(arguments.get(i+1));
-            result = operate(result, secondValue, operator);
+
+            result = operator.apply(result, secondValue);
         }
        return result;
-    }
-
-    private double operate(double a, double b, Operator operator) {
-        if (operator == Operator.PLUS) return plus(a, b);
-        if (operator == Operator.MINUS) return minus(a, b);
-        if (operator == Operator.DIVIDE) return divide(a, b);
-        if (operator == Operator.MULTIPLY) return multiply(a, b);
-
-        throw new IllegalArgumentException("operator is not found");
     }
 }
