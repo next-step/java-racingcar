5ede3b6be6434174dd766068299df9fd1296d9f2
JuWanHan <baekseongsa@naver.com>
2020-05-10 22:13:36 +0900

    OperationSymbol.getOperationSymbol() 반환값 정확성 테스트케이스 추가

--- src/test/java/calculator/OperationSymbolTest.java
+++ src/test/java/calculator/OperationSymbolTest.java
@@ -3,8 +3,12 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -41,8 +45,8 @@ class OperationSymbolTest {
 
     @ParameterizedTest
     @ValueSource(strings = { "+", "-", "*", "/" })
-    @DisplayName("사칙 연산기호 찾기 테스트")
-    void getOperationSymbol(String symbol) {
+    @DisplayName("사칙 연산기호 찾기 Not Null 여부 테스트")
+    void findOperationSymbol(String symbol) {
         assertThat(OperationSymbol.getOperationSymbol(symbol)).isNotNull();
     }
 
@@ -50,5 +54,21 @@ class OperationSymbolTest {
     @DisplayName("사칙 연산기호가 아닐 경우 IllegalArgumentException 발생")
     void notOperationSymbol() {
         assertThatThrownBy(() -> OperationSymbol.getOperationSymbol("!")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForOperationSymbol")
+    @DisplayName("사칙 연산기호 찾기 테스트")
+    void getOperationSymbol(String symbol, OperationSymbol operationSymbol) {
+        assertThat(OperationSymbol.getOperationSymbol(symbol)).isEqualTo(operationSymbol);
+    }
+
+    private static Stream<Arguments> provideStringsForOperationSymbol() {
+        return Stream.of(
+                Arguments.of("+", OperationSymbol.PLUS),
+                Arguments.of("-", OperationSymbol.MINUS),
+                Arguments.of("*", OperationSymbol.MULTIPLY),
+                Arguments.of("/", OperationSymbol.DIVIDE)
+        );
     }
 }
