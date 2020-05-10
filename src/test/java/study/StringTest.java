949af9952288926136c21de2584491ea49e398b4
bgpark <bgpark82@gmail.com>
2020-05-10 23:54:36 +0900

    1단계 - 학습 테스트 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,19 +1,15 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
-
-/**
- * String 클래스에 대한 학습 테스트
- *
- * 요구사항 1.
- *  - "1,2"을 ","로 split 했을 때, 1과 2로 잘 분리되는지 확인
- */
+@DisplayName("String 클래스에 대한 학습 테스트")
 public class StringTest {
 
     @Test
+    @DisplayName("1,2를 ,로 분리")
     void split() {
         // Given
         String test = "1,2";
@@ -24,4 +20,34 @@ public class StringTest {
         // Then
         assertThat(result).containsExactly("1","2");
     }
+
+    @Test
+    void substring() {
+        // Given
+        String test = "(1,2)";
+
+        // When
+        String result = test.substring(1,4);
+
+        // Then
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("특정 위치의 문자 가져오기")
+    @Test
+    void 특정_위치의_문자_가져오기() {
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+        .isThrownBy(() -> {
+            // Given
+            String test = "abc";
+
+            // When
+            char result = test.charAt(7);
+
+            // Then
+            assertThat(result).isEqualTo('b');
+        }).withMessageMatching("String index out of range: \\d+");
+    }
 }
+
+
