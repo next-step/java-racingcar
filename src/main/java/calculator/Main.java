7d130e9dd6417236bdcb321a14414fabc860ce9e
junpil park <iamfeel2@gmail.com>
2020-05-11 08:52:29 +0900

    feat (Main) : Main Class 추가

---
+++ src/main/java/calculator/Main.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("계산할 값을 입력 하세요.");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        System.out.println("-> " + input);
+    }
+}
