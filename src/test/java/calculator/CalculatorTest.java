356f2d6bd14cc89c4649775efb921623cf639d6f
iljun <sumusb@naver.com>
2020-05-10 15:52:29 +0900

    refactor @CsvSource to @MethodSource

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,8 +2,11 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class CalculatorTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"2 + 3 * 4 / 2,10.0", "1 + 3 + 5,9.0", "10 - 3 / 7, 1.0"})
+    @MethodSource("provideExpression")
     @DisplayName("계산 테스트")
     void calculate(String expression, double result) {
         Calculator calculator = new Calculator(expression);
@@ -26,4 +29,13 @@ class CalculatorTest {
             new Calculator(expression);
         }).isInstanceOf(IllegalArgumentException.class);
     }
+
+    private static Stream<Arguments> provideExpression() {
+        return Stream.of(
+                Arguments.of("2 + 3 * 4 / 2", "10.0"),
+                Arguments.of("1 + 3 + 5", "9.0"),
+                Arguments.of("10 - 3 / 7", "1.0")
+        );
+    }
+
 }
