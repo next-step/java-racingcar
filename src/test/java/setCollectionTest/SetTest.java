174e253a51fd474e82dc33bd9a9e2b78e3663e6f
hy43 <hyde0401@gmail.com>
2020-05-10 16:50:52 +0900

    (create) SetCollectionTest 요구사항 1 구현

---
+++ src/test/java/setCollectionTest/SetTest.java
@@ -0,0 +1,31 @@
+package setCollectionTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+    
+    @Test
+    @DisplayName("요구사항 1 : size 메소드를 활용한 Set Collection의 사이즈 확인 테스트")
+    void setCollectionSizeTest() {
+    	assertThat(numbers.size()).isEqualTo(4);
+    }
+}
