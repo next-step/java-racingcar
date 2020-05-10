087a854c5c5463d9b4c33ab16cb27cece165ce94
hanjae.jea <hanjae.jea@gmail.com>
2020-05-10 11:57:38 +0900

    Set Collection에 대한 학습 테스트 Requirement 2
    ParameterizedTest를 활용해 contains 메소드 테스트 완료

--- src/test/java/study/nextstep/SetCollectionStudyTest.java
+++ src/test/java/study/nextstep/SetCollectionStudyTest.java
@@ -3,12 +3,15 @@ package study.nextstep;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
 
+@DisplayName("Set Collection에 대한 학습 테스")
 public class SetCollectionStudyTest {
     private Set<Integer> numbers;
 
@@ -23,7 +26,14 @@ public class SetCollectionStudyTest {
 
     @Test
     @DisplayName("size() 메소드를 활용해 Set의 크기를 확인하는 테스트")
-    public void testSizeMethod2ConfirmSizeOfSet(){
+    public void Req1_testSizeMethod2ConfirmSizeOfSet(){
         assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("contains() 메소드를 활용해 1,2,3 값이 존재하는지 확인하는 테스트")
+    public void Req2_testContainsMethod2ExistsOfNumbers(int number){
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
