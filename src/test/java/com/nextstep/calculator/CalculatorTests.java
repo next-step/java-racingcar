c918cf96647b24f77532f45461b60122d38fa4b1
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-10 14:13:07 +0900

    테스트: CalculatorTests 케이스 추가
    - 홈페이지 요구사항에 있는 예시 계산 추가 및 확인

--- src/test/java/com/nextstep/calculator/CalculatorTests.java
+++ src/test/java/com/nextstep/calculator/CalculatorTests.java
@@ -27,7 +27,7 @@ class CalculatorTests {
 
     @DisplayName("여러항 사칙연산 계산하기")
     @ParameterizedTest
-    @CsvSource(value = { "2 + 3 - 1:4", "3 + 5 - 2 / 3 * 2:4" }, delimiter = ':')
+    @CsvSource(value = { "2 + 3 - 1:4", "3 + 5 - 2 / 3 * 2:4", "2 + 3 * 4 / 2:10" }, delimiter = ':')
     void calculateMulti(String input, int result) {
         int number = Calculator.run(input);
 
