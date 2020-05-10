6d3a67cae1cde5e13ee14151fe548ab05c1f430a
junilHwang <tjsdlf4261@naver.com>
2020-05-10 23:17:05 +0900

    feat: String 클래스에 대한 학습 테스트 - 요구사항 2 완료

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -4,9 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 
   @Test
-  void split () {
-    String[] res = "1,2".split(",");
-    assertThat(res).contains("1", "2");
-    assertThat(res).containsExactly("1", "2");
+  void split1 () {
+    String[] result = "1,2".split(",");
+    assertThat(result).contains("1", "2");
+  }
+
+  @Test
+  void split2 () {
+    String input = "(1,2)";
+    String result = input.substring(1, input.length() - 1);
+    assertThat(result).contains("1,2");
   }
 }
