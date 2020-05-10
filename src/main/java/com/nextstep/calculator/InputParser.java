9a3f671fb9e5c6c5c70fe6e5fc3cee1f28ae938e
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-10 12:29:44 +0900

    구현: InputParser.parse
    - String 입력값을 계산하기 편하도록 구분자로 쪼개서 List타입으로 반환하도록 구현
    - 내부에 공백 없이 필요한 값만 받는 것을 확인

--- src/main/java/com/nextstep/calculator/InputParser.java
+++ src/main/java/com/nextstep/calculator/InputParser.java
@@ -1,8 +1,15 @@
 package com.nextstep.calculator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputParser {
     public static void validate(String input) {
         if (input == null) throw new IllegalArgumentException();
         if (input.trim().isEmpty()) throw new IllegalArgumentException();
+    }
+
+    public static List<String> parse(String validInput) {
+        return Arrays.asList(validInput.split(" "));
     }
 }
--- src/test/java/com/nextstep/calculator/InputParserTests.java
+++ src/test/java/com/nextstep/calculator/InputParserTests.java
@@ -1,10 +1,14 @@
 package com.nextstep.calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputParserTests {
@@ -14,5 +18,15 @@ class InputParserTests {
     @ValueSource(strings = "   ")
     void inputValidator(String invalidInput) {
         assertThatThrownBy(() -> InputParser.validate(invalidInput)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력 값을 공백으로 구분해서 List 반환")
+    @Test
+    void parse() {
+        String validInput = "3 + 2 / 4 * 5";
+        List<String> parsed = InputParser.parse(validInput);
+
+        assertThat(parsed).containsExactly("3", "+", "2", "/", "4", "*", "5");
+        assertThat(parsed).doesNotContain(" ");
     }
 }
