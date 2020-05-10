8671dd09200db6a238dabd67a4ae4567d2f4c854
wangyongchoi <choiwangyong@gmail.com>
2020-05-10 17:56:28 +0900

    add practice #2
    - "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.

--- src/test/java/StringClassTest.java
+++ src/test/java/StringClassTest.java
@@ -32,4 +32,17 @@ class StringClassTest {
         //then
         assertThat(splitStringList).containsExactly("1");
     }
+
+    @Test
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현")
+    public void subStringTest() {
+        //given
+        String testParameter = "(1,2)";
+
+        //when
+        String result = testParameter.substring(1, testParameter.length()-1);
+
+        //then
+        assertThat(result).isEqualTo("1,2");
+    }
 }
