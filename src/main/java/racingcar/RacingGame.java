d5b3f0c8cd2227f04a761052cf6f6da43582c7ea
chulman <chlcjfals0122@gmail.com>
2020-05-10 23:36:13 +0900

    feat (RacingGame) : play 메소드 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,5 +1,8 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class RacingGame {
     private int round;
     private Car[] cars;
@@ -10,7 +13,17 @@ public class RacingGame {
     }
 
     public int[] play() {
-        // TODO 구현
-        return null;
+        IntStream.range(0, round)
+                .forEach(value -> moveAll());
+
+        return Arrays.stream(cars)
+                .mapToInt(Car::getPosition)
+                .toArray();
     };
+
+    private int[] moveAll() {
+      return Arrays.stream(cars)
+                .mapToInt(Car::move)
+                .toArray();
+    }
 }
