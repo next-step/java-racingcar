c386a6b29eb6461b22cbf6e249ce264474b0c63f
iljun <sumusb@naver.com>
2020-05-10 16:56:40 +0900

    refactor validation & add unit test for validation

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,26 +3,41 @@ package calculator;
 import java.util.Objects;
 
 public class Calculator {
-    private String expression;
 
-    public Calculator(String expression) {
-        this.validateExpression(expression);
-        this.expression = expression;
+    private Calculator() {
     }
 
-    private void validateExpression(String expression) {
+    private static void validateExpression(String expression) {
         if (Objects.isNull(expression) || expression.isEmpty()) {
             throw new IllegalArgumentException();
         }
+
+        String[] splitExpression = expression.split(" ");
+        validateSplitLength(splitExpression);
+        validateSplitExpression(splitExpression);
     }
 
-    public double calculate() {
-        String[] splitExpression = this.expression.split(" ");
+    public static double calculate(String expression) {
+        validateExpression(expression);
+
+        String[] splitExpression = expression.split(" ");
         double result = Double.parseDouble(splitExpression[0]);
         for (int i = 1; i < splitExpression.length; i += 2) {
             Operator operator = Operator.findOperator(splitExpression[i]);
             result = operator.calculate(result, Double.parseDouble(splitExpression[i + 1]));
         }
         return result;
+    }
+
+    private static void validateSplitLength(String[] splitExpression) {
+        if (splitExpression.length == 1 || splitExpression.length % 2 == 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateSplitExpression(String[] splitExpression) {
+        for (int i = 0; i < splitExpression.length; i += 2) {
+            Double.parseDouble(splitExpression[i]);
+        }
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -17,15 +17,22 @@ class CalculatorTest {
     @MethodSource("provideExpression")
     @DisplayName("계산 테스트")
     void calculate(String expression, double result) {
-        Calculator calculator = new Calculator(expression);
-        assertThat(calculator.calculate()).isEqualTo(result);
+        assertThat(Calculator.calculate(expression)).isEqualTo(result);
     }
 
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("입력 값이 null이거나 빈 공백일 경우 Exception")
     void validationExpression(String expression) {
-        assertThatThrownBy(() -> new Calculator(expression))
+        assertThatThrownBy(() -> Calculator.calculate(expression))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNotValidExpression")
+    @DisplayName("입력 값이 올바른 경우가 아닐경우 Exception")
+    void expression(String expression) {
+        assertThatThrownBy(() -> Calculator.calculate(expression))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -33,8 +40,20 @@ class CalculatorTest {
         return Stream.of(
                 Arguments.of("2 + 3 * 4 / 2", "10.0"),
                 Arguments.of("1 + 3 + 5", "9.0"),
-                Arguments.of("10 - 3 / 7", "1.0")
+                Arguments.of("10 - 3 / 7", "1.0"),
+                Arguments.of("1 + 3", "4.0"),
+                Arguments.of("23 - 3 - 1 - 2", "17")
         );
     }
 
+    private static Stream<Arguments> provideNotValidExpression() {
+        return Stream.of(
+                Arguments.of("2"),
+                Arguments.of("+"),
+                Arguments.of("2 + "),
+                Arguments.of("- 2"),
+                Arguments.of("2 + 1 -"),
+                Arguments.of("2 1 3")
+        );
+    }
 }
