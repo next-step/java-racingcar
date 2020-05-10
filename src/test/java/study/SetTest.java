b2f9205bd2fe48fe4fff81d5d8d77346a2d98b0f
eunjeong <cs801458j@gmail.com>
2020-05-11 03:30:00 +0900

    요구사항 1 구현

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,39 @@
+package study;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+
+    //  요구사항 1
+    @DisplayName("Set 크기 확인 테스트")
+    @Test
+    void checkSetSize(){
+        int size = 3;
+        assertThat(numbers.size()).isEqualTo(size);
+    }
+
+}
