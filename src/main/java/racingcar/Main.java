e8015cba8c27351e9c6a058e2d8bd736ccbb148e
chulman <chlcjfals0122@gmail.com>
2020-05-11 00:04:17 +0900

    feat (Main) : Main 클래스 구현

---
+++ src/main/java/racingcar/Main.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int carCount = InputView.getCarCount();
+        int gameRound = InputView.getGameRound();
+
+        Car[] cars = new Car[carCount];
+        for (int i = 0; i < carCount; i++) {
+            cars[i] = Car.makeDefaultInstance();
+        }
+
+        ResultView.printBanner();
+        new RacingGame(gameRound, cars).play();
+    }
+}
