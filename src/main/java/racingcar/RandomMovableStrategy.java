4309406d805dd3f6e722c84518f4c147d5696cfb
chulman <chlcjfals0122@gmail.com>
2020-05-11 00:22:40 +0900

    fix (RandomMovableStrategy) : 똑같은 값이 계속 나오지 않도록 nextInt에 값 설정

--- src/main/java/racingcar/RandomMovableStrategy.java
+++ src/main/java/racingcar/RandomMovableStrategy.java
@@ -7,7 +7,7 @@ public class RandomMovableStrategy implements MovableStrategy {
     private final int DEFAULT_RANDOM_RANGE = 10;
     private final int MINIMUM_MOVABLE_VALUE = 4;
 
-    private final Random randomGenerator = new Random(DEFAULT_RANDOM_RANGE);
+    private final Random randomGenerator = new Random();
 
     @Override
     public int move() {
@@ -15,7 +15,7 @@ public class RandomMovableStrategy implements MovableStrategy {
     }
 
     public int movedByNumberFourOrMore() {
-        int value = randomGenerator.nextInt();
+        int value = randomGenerator.nextInt(DEFAULT_RANDOM_RANGE);
         if(value >= MINIMUM_MOVABLE_VALUE) {
             return 1;
         }
