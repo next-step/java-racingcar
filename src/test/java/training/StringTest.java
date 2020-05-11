cde36fecd9204cb0359a42413a3d617a9a5c4eec
DominiqN <dmut7691@gmail.com>
2020-05-11 21:40:24 +0900

    Change assert function

--- src/test/java/training/StringTest.java
+++ src/test/java/training/StringTest.java
@@ -8,7 +8,7 @@ public class StringTest {
     @Test
     void split() {
         String[] result = "1,2".split(",");
-        assertThat(result).containsExactly("1", "2");
+        assertThat(result).contains("1", "2");
     }
 
     @Test
