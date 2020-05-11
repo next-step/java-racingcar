8e9505a41935d9ea429b002b7e3bfe7b00086321
Choi hong hee <vvshinevv@gmail.com>
2020-05-11 10:34:43 +0900

    split study test (#704)
    * split study test
    
    * [step1]학습 테스트 실습

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,38 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetCollectionTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(final int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    void containsTest(final int input, final boolean expected) {
+        boolean result = numbers.contains(input);
+        assertThat(result).isEqualTo(expected);
+    }
+}
