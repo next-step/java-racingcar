61997228fc74d75c8a6d407b0e4b91d40cb2c21d
DominiqN <dmut7691@gmail.com>
2020-05-11 21:37:57 +0900

    Implement Request 1

---
+++ src/test/java/training/StringTest.java
@@ -0,0 +1,19 @@
+package training;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void splitExactly1() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+}
