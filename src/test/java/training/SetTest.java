5d12e4ceeb7c4cdeb91efa84a11b911de3aa97cf
DominiqN <dmut7691@gmail.com>
2020-05-11 22:14:13 +0900

    Add test code for requirement 1 of Set Collection

--- src/test/java/training/SetTest.java
+++ src/test/java/training/SetTest.java
@@ -1,8 +1,12 @@
 package training;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -14,5 +18,13 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인")
+    void size() {
+        int size = numbers.size();
+        assertThat(size).isEqualTo(3);
     }
 }
