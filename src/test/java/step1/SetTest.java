8eabe3a4c34748db549cbbb8f0791d6dc876c3e0
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-12 02:17:21 +0900

    create test of containsWithFail

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -4,9 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -26,11 +29,18 @@ public class SetTest {
 	void size() {
 		assertThat(numbers.size()).isEqualTo(3);
 	}
-	
+
 	// 요구사항2
 	@ParameterizedTest
-	@ValueSource(ints= {1,2,3})
-	 void contains(int input) {
-        assertThat(numbers.contains(input)).isTrue();
-    }
+	@ValueSource(ints = { 1, 2, 3 })
+	void contains(int input) {
+		assertThat(numbers.contains(input)).isTrue();
+	}
+
+	// 요구사항3
+	@ParameterizedTest
+	@CsvSource(value = { "1:true", "2:true", "3:true","4:false","5:false" }, delimiter = ':')
+	void containsWithFail(int input, boolean expected) {
+		assertEquals(expected, numbers.contains(input));
+	}
 }
