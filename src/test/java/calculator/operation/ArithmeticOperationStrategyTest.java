cdd77bae015fa19665052a23c0025127b68a2135
kwonheejeong <gmlwjd9405@naver.com>
2020-05-11 23:52:23 +0900

    test: 사칙연산 테스트 클래스 패키지 변경

--- src/test/java/calculator/operation/ArithmeticOperationStrategyTest.java
+++ src/test/java/calculator/operation/ArithmeticOperationStrategyTest.java
@@ -1,7 +1,6 @@
-package operation;
+package calculator.operation;
 
 import calculator.exception.ErrorMessage;
-import calculator.operation.ArithmeticOperationStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
