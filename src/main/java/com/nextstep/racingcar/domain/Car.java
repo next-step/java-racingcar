7f195655c1113101640b0cc7486e84df7e80c43a
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-12 08:03:42 +0900

    feat: Car
    - 레이싱 게임에서 움직이는 차를 구현하기 위해 Car 클래스 구현
    - 움직인 거리를 속성으로 갖고, 최초 생성시 움직인 거리는 0이다

---
+++ src/main/java/com/nextstep/racingcar/domain/Car.java
@@ -0,0 +1,17 @@
+package com.nextstep.racingcar.domain;
+
+public class Car {
+    private int moveLength;
+
+    private Car(int moveLength) {
+        this.moveLength = moveLength;
+    }
+
+    public Car() {
+        this(0);
+    }
+
+    public int getMoveLength() {
+        return moveLength;
+    }
+}
---
+++ src/test/java/com/nextstep/racingcar/domain/CarTests.java
@@ -0,0 +1,22 @@
+package com.nextstep.racingcar.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTests {
+    private Car newCar;
+
+    @BeforeEach
+    public void setup() {
+        newCar = new Car();
+    }
+
+    @DisplayName("처음 만들어진 차량은 움직인 거리가 0이다.")
+    @Test
+    void newCarTest() {
+        assertThat(newCar.getMoveLength()).isEqualTo(0);
+    }
+}
