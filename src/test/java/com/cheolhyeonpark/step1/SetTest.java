2e18f9fe104c988660c99dfb3be33aa350448567
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-11 20:15:34 +0900

    Add test for contains method using ParameterizedTest

--- src/test/java/com/cheolhyeonpark/step1/SetTest.java
+++ src/test/java/com/cheolhyeonpark/step1/SetTest.java
@@ -3,6 +3,8 @@ package com.cheolhyeonpark.step1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -32,5 +34,17 @@ public class SetTest {
 
         //then
         assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains 메소드를 통해 존재하는 값을 확인")
+    void contains(int inputNumber) {
+        //given
+
+        //when
+
+        //then
+        assertThat(numbers.contains(inputNumber)).isTrue();
     }
 }
