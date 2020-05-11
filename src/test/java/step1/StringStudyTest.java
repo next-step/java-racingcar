2fe72e45daa241a36e28052d7ff195eaea6899e0
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-12 01:44:05 +0900

    create test of charAtTest

--- src/test/java/step1/StringStudyTest.java
+++ src/test/java/step1/StringStudyTest.java
@@ -1,5 +1,6 @@
 package step1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
@@ -24,12 +25,33 @@ public class StringStudyTest {
 	@Test
 	public void testRemoveBracket() {
 		String result = StringStudy.removeBracket("(1,2)");
-		
+
 		assertThat(result).isEqualTo("1,2");
 	}
 
 	@Test
+	@DisplayName("charAt() 사용 시 위치 값을 벗어났을 경우 StringIndexOutOfBoundsException 발생 테스트")
 	public void testCharAtTest() {
+		// using assertThatThrownBy
+		assertThatThrownBy(() -> {
+			StringStudy.charAtTest("abc", -1);
+		}).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("range: -1");
+		
+		// using assertThatExceptionOfType
+		assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+			StringStudy.charAtTest("abc", 3);
+		}).withMessageMatching("String index out of range: 3");
+		
+		assertThatNullPointerException().isThrownBy(()->{
+			StringStudy.charAtTest(null, 0);
+		});
+		
+		// Separating the Exception From the Assertion
+		Throwable thrown = catchThrowable(() -> {
+			StringStudy.charAtTest("abc", 4);
+		});
 
+		assertThat(thrown).isInstanceOf(IndexOutOfBoundsException.class)
+				.hasMessageMatching("String index out of range: 4");
 	}
 }
