c51ac94de0e805a4b7f33bc297af7621446ff5ab
kwonheejeong <gmlwjd9405@naver.com>
2020-05-11 22:52:11 +0900

    test: enum 클래스 반환 및 사칙연산 기호가 아닌 경우 예외 처리

---
+++ src/test/java/operation/ArithmeticOperationStrategyTest.java
@@ -0,0 +1,42 @@
+package operation;
+
+import calculator.exception.ErrorMessage;
+import calculator.operation.ArithmeticOperationStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ArithmeticOperationStrategyTest {
+
+    @DisplayName("인자로 들어온 값에 해당하는 Enum 클래스 반환")
+    @ParameterizedTest
+    @MethodSource("existValueCase")
+    void of(final String value, final ArithmeticOperationStrategy expected) {
+        assertThat(ArithmeticOperationStrategy.of(value)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> existValueCase() {
+        return Stream.of(
+                Arguments.of("+", ArithmeticOperationStrategy.PLUS),
+                Arguments.of("-", ArithmeticOperationStrategy.MINUS),
+                Arguments.of("*", ArithmeticOperationStrategy.TIMES),
+                Arguments.of("/", ArithmeticOperationStrategy.DIVIDE)
+        );
+    }
+
+    @DisplayName("Enum 클래스에 없는 인자의 경우 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"#", "%", "s", "test"})
+    void failureOf(final String value) {
+        assertThatThrownBy(() -> ArithmeticOperationStrategy.of(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.NOT_SUPPORTED_OPERATOR);
+    }
+}
