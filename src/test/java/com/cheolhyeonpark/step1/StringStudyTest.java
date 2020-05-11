ebc017425ea36fabeef4c44701b3a39316d1b430
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-11 19:58:30 +0900

    Add test for outOfBoundException

--- src/test/java/com/cheolhyeonpark/step1/StringStudyTest.java
+++ src/test/java/com/cheolhyeonpark/step1/StringStudyTest.java
@@ -3,6 +3,7 @@ package com.cheolhyeonpark.step1;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 
 class StringStudyTest {
@@ -31,5 +32,17 @@ class StringStudyTest {
 
         //then
         assertThat(resultText).isEqualTo("1,2");
+    }
+
+    @Test
+    void throwOutOfBoundExceptionUsingCharAt() {
+        //given
+        String inputText = "abc";
+
+        //when
+
+        //then
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> inputText.charAt(inputText.length()));
     }
 }
