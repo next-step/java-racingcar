beff0b375667f461ceda5e8165bb940b066ced71
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-11 20:22:01 +0900

    Add test for contains method using ValueSource

--- src/test/java/com/cheolhyeonpark/step1/SetTest.java
+++ src/test/java/com/cheolhyeonpark/step1/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -46,5 +47,17 @@ public class SetTest {
 
         //then
         assertThat(numbers.contains(inputNumber)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    @DisplayName("contains 메소드를 통해 존재하는 값과 존재하지 않는 값을 확인")
+    void contains(int number, boolean result) {
+        //given
+
+        //when
+
+        //then
+        assertThat(numbers.contains(number)).isEqualTo(result);
     }
 }
