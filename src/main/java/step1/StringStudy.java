418740219b341d29622e2c8b397c3a3f046e01cc
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-12 00:29:25 +0900

    create StringStudy.split()

--- src/main/java/step1/StringStudy.java
+++ src/main/java/step1/StringStudy.java
@@ -2,4 +2,15 @@ package step1;
 
 public class StringStudy {
 
+  /**
+   * 문자(param)를 구분자(delimiter)로 split한다.
+   * 
+   * @param param     : String, 나눌 문자
+   * @param delimiter : String, 구분자
+   * @return String[], 구분자로 나뉘어진 문자를 배열로 반환한다.
+   */
+  public String[] split(String param, String delimiter) {
+    return param.split(delimiter);
+  }
+
 }
