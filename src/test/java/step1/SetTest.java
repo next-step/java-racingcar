c6a6fdc9b7f1bb5a2a228cd35a9b3d4075164a82
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-12 01:57:13 +0900

    create test of size

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -3,7 +3,9 @@ package step1;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SetTest {
 	private Set<Integer> numbers;
@@ -15,5 +17,11 @@ public class SetTest {
 		numbers.add(1);
 		numbers.add(2);
 		numbers.add(3);
+	}
+
+	// 요구사항1
+	@Test
+	void size() {
+		assertThat(numbers.size()).isEqualTo(3);
 	}
 }
