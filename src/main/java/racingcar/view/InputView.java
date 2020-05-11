9b63d93fcd2b1e3a8cd6bfc45c564a7bdcef1dc5
chulman <chlcjfals0122@gmail.com>
2020-05-11 21:56:10 +0900

    refactor (InputVIew) : step3 feedback

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private InputView(){
-        throw new AssertionError();
-    }
     private static final Scanner scanner = new Scanner(System.in);
 
-    private static String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
-    private static String GAME_ROUND_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private static final String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String GAME_ROUND_MESSAGE = "시도할 회수는 몇 회 인가요?";
+
+    private InputView() {
+        throw new AssertionError();
+    }
 
     public static int getCarCount() {
         System.out.println(CAR_COUNT_MESSAGE);
         return scanner.nextInt();
     }
 
-    public static int getGameRound(){
+    public static int getGameRound() {
         System.out.println(GAME_ROUND_MESSAGE);
         return scanner.nextInt();
     }
