9a4ff7c80ee5c13b5fede9b1a4fd6892e9ee8a4f
RudiaMoon <impactyoung@gmail.com>
2020-05-12 00:51:13 +0900

    feat : Operator enums plus, minus, multiply, divide

---
+++ src/main/java/calculator/Operator.java
@@ -0,0 +1,44 @@
+package calculator;
+
+import java.util.function.DoubleBinaryOperator;
+
+public enum Operator {
+    PLUS("+", Operator::add),
+    MINUS("-", Operator::subtract),
+    MULTIPLY("*", Operator::multiply),
+    DIVIDE("/", Operator::divide);
+
+    private String symbol;
+    private DoubleBinaryOperator binaryOperator;
+
+    Operator(String symbol, DoubleBinaryOperator binaryOperator) {
+        this.symbol = symbol;
+        this.binaryOperator = binaryOperator;
+    }
+
+    private static double add(double startNumber, double endNumber) {
+        return startNumber + endNumber;
+    }
+
+    private static double subtract(double startNumber, double endNumber) {
+        return startNumber - endNumber;
+    }
+
+    private static void checkZeroDivide(double endNumber) {
+        if (endNumber == Calculator.ZERO_NUMBER)
+            throw new ArithmeticException("제로로 나누는건 허용되지 않습니다.");
+    }
+
+    private static double divide(double startNumber, double endNumber) {
+        checkZeroDivide(endNumber);
+        return startNumber / endNumber;
+    }
+
+    private static double multiply(double startNumber, double endNumber) {
+        return startNumber * endNumber;
+    }
+
+    public double apply(int startNumber, int endNumber) {
+        return binaryOperator.applyAsDouble(startNumber, endNumber);
+    }
+}
---
+++ src/test/java/calculator/OperatorTest.java
@@ -0,0 +1,40 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OperatorTest {
+    private int defaultNumber;
+
+    @BeforeEach
+    void setUp() {
+        defaultNumber = 2;
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1=3", "2=4", "-1=1", "-3=-1"}, delimiter = '=')
+    void 덧셈(int endNumber, int result) {
+        assertThat(Operator.PLUS.apply(defaultNumber, endNumber)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1=1", "2=0", "-1=3", "-3=5"}, delimiter = '=')
+    void 뺄셈(int endNumber, int result) {
+        assertThat(Operator.MINUS.apply(defaultNumber, endNumber)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1=2", "2=1"}, delimiter = '=')
+    void 나눗셈(int endNumber, int result) {
+        assertThat(Operator.DIVIDE.apply(defaultNumber, endNumber)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1=2", "2=4", "3=6"}, delimiter = '=')
+    void 곱셈(int endNumber, int result) {
+        assertThat(Operator.MULTIPLY.apply(defaultNumber, endNumber)).isEqualTo(result);
+    }
+}
