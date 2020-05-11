0ae0619282df4b0f53b73374d4745426164033d5
chulman <chlcjfals0122@gmail.com>
2020-05-12 00:03:23 +0900

    refactor (Car) : add carName and refactoring

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,28 +1,51 @@
 package racingcar;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Objects;
 
 public class Car {
     private static final int DEFAULT_POSITION = 0;
 
     private int position;
+    private String name;
     private MovableStrategy movableStrategy;
 
-    public Car(int position, MovableStrategy movableStrategy) {
+    private Car(int position, String name, MovableStrategy movableStrategy) {
         this.position = position;
+        this.name = name;
         this.movableStrategy = movableStrategy;
     }
 
-    public static Car newInstance() {
-        return new Car(DEFAULT_POSITION, new RandomMovableStrategy());
+    public static Car newInstance(String name) {
+        if (StringUtils.isEmpty(name)) {
+            throw new IllegalArgumentException("이름이 비어있습니다.");
+        }
+        return new Car(DEFAULT_POSITION, name, new RandomMovableStrategy());
     }
 
     public int getPosition() {
         return position;
     }
 
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public MovableStrategy getMovableStrategy() {
         return movableStrategy;
+    }
+
+    public void setMovableStrategy(MovableStrategy movableStrategy) {
+        this.movableStrategy = movableStrategy;
     }
 
     public int move() {
@@ -31,8 +54,8 @@ public class Car {
         return position;
     }
 
-    private void validate(){
-        if(position < DEFAULT_POSITION || Objects.isNull(movableStrategy)) {
+    private void validate() {
+        if (position < DEFAULT_POSITION || Objects.isNull(movableStrategy)) {
             throw new IllegalArgumentException();
         }
     }
