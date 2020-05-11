000369b519f36fc90ae835b21b6df347a9f76740
hy43 <hyde0401@gmail.com>
2020-05-11 18:43:07 +0900

    (create) Calculator Class 기본 사칙연산 기능 및 테스트 케이스 구현

---
+++ src/main/java/stringCalculator/Calculator.java
@@ -0,0 +1,20 @@
+package stringCalculator;
+
+public class Calculator {
+
+	public int add(int num1, int num2) {
+		return num1+num2;
+	}
+
+	public int subStract(int num1, int num2) {
+		return num1-num2;
+	}
+	
+	public int multifly(int num1, int num2) {
+		return num1*num2;
+	}
+	
+	public int devide(int num1, int num2) {
+		return num1/num2;
+	}
+}
---
+++ src/test/java/stringCalculator/CalculatorTest.java
@@ -0,0 +1,41 @@
+package stringCalculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+    
+	@Test
+	@DisplayName("µ¡¼À ±â´É ´ëÇÑ Å×½ºÆ®")
+	public void add() {
+		assertThat(calculator.add(1, 2)).isEqualTo(3);
+	}
+
+	@Test
+	@DisplayName("»¬¼À ±â´É ´ëÇÑ Å×½ºÆ®")
+	public void subStract() {
+		assertThat(calculator.subStract(2, 1)).isEqualTo(1);
+	}
+	
+	@Test
+	@DisplayName("°ö¼À ±â´É ´ëÇÑ Å×½ºÆ®")
+	public void multifly() {
+		assertThat(calculator.multifly(2, 3)).isEqualTo(6);
+	}
+	
+	@Test
+	@DisplayName("³ª´°¼À ±â´É ´ëÇÑ Å×½ºÆ®")
+	public void devide() {
+		assertThat(calculator.devide(4, 2)).isEqualTo(2);
+	}
+}
