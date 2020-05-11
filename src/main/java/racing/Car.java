09d8a314f517bdf655db01b06670b9a5383e1e26
iljun <sumusb@naver.com>
2020-05-11 23:35:27 +0900

    [refactor] car Object

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -2,18 +2,12 @@ package racing;
 
 public class Car {
     private int position = 0;
-    private CarMovement carMovement;
 
-    public Car(CarMovement carMovement) {
-        if (carMovement == null) {
-            throw new IllegalArgumentException();
-        }
-        this.carMovement = carMovement;
+    public Car() {
     }
 
-    public void move() {
-        boolean isRun = this.carMovement.isMove();
-        if (isRun) {
+    public void move(CarMovement carMovement) {
+        if (carMovement.isMove()) {
             this.position++;
         }
     }
