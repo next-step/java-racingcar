0478f894b4bea2b31151c37e39599c86f1c52d25
chulman <chlcjfals0122@gmail.com>
2020-05-12 01:59:34 +0900

    refactor (InputView) : refactoring InputView

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,19 +3,20 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
-
     private static final Scanner scanner = new Scanner(System.in);
 
-    private static final String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String SEPARATOR = ",";
+    private static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String GAME_ROUND_MESSAGE = "시도할 회수는 몇 회 인가요?";
 
     private InputView() {
         throw new AssertionError();
     }
 
-    public static int getCarCount() {
-        System.out.println(CAR_COUNT_MESSAGE);
-        return scanner.nextInt();
+    public static String[] getCarNames() {
+        System.out.println(CAR_NAME_MESSAGE);
+        String line = scanner.nextLine();
+        return line.split(SEPARATOR);
     }
 
     public static int getGameRound() {
