1c3b041128b59afd462d475843585c49a305545b
junpil park <iamfeel2@gmail.com>
2020-05-12 00:43:53 +0900

    feat (StringCalculator) : 문자열을 실제 계산하는 로직 추가, 파라메터 NullOrEmpty 체크하는 함수 수정
    아래는 상세 추가 내용
    1. 문자열을 스페이스 단위로 split
    2. 문자열을 연산자와 피연산자로 분리
    3. 연산자와 피연산자로 실제 계산

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,13 +1,57 @@
 package calculator;
 
-import jdk.internal.joptsimple.internal.Strings;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class StringCalculator {
 
-    private void checkNullOrEmpty(String str) throws IllegalArgumentException {
-       if(Strings.isNullOrEmpty(str)) {
-           throw new IllegalArgumentException("Argument is null or empty.");
-       }
+    Queue<Float> operands = new ArrayDeque<>();
+    Queue<Operator> operators = new ArrayDeque<>();
+
+    Float beforeNumber = null;
+
+    public float calculate(String formula) {
+        checkNullOrEmpty(formula);
+
+        String[] formulaSplit = formula.split(" ");
+        separateOperandFromOperator(formulaSplit);
+
+        for(Operator operator = operators.poll() ; operator != null ; operator = operators.poll()) {
+            beforeNumber = calculate(getBeforeNumber(), operator, getAfterNumber());
+        }
+
+        return beforeNumber;
     }
 
+    private Float calculate(Float beforeNumber, Operator operator, Float afterNumber) {
+        return operator.calculate(beforeNumber, afterNumber);
+    }
+
+    private void separateOperandFromOperator(String[] formulaSplit) {
+        for(String element : formulaSplit) {
+            if(Operator.isOperatorSymbol(element)) {
+                operators.add(Operator.getOperator(element));
+            }
+            else{
+                operands.add(Float.valueOf(element));
+            }
+        }
+    }
+
+    private Float getBeforeNumber() {
+        if(beforeNumber == null) {
+            beforeNumber = operands.poll();
+        }
+        return beforeNumber;
+    }
+
+    private Float getAfterNumber() {
+        return operands.poll();
+    }
+
+    private void checkNullOrEmpty(String str) throws IllegalArgumentException {
+        if(str == null || str.isEmpty()) {
+            throw new IllegalArgumentException("Argument is null or empty.");
+        }
+    }
 }
