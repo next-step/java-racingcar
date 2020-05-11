252d12c2062f9944e34886d59e49d06b053f8ac6
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-11 22:49:01 +0900

    init StringStudy with Test

---
+++ src/main/java/step1/StringStudy.java
@@ -0,0 +1,5 @@
+package step1;
+
+public class StringStudy {
+
+}
---
+++ src/test/java/step1/StringStudyTest.java
@@ -0,0 +1,22 @@
+package step1;
+
+import org.junit.jupiter.api.Test;
+
+public class StringStudyTest {
+  StringStudy StringStudy = new StringStudy();
+
+  @Test
+  public void splitTest() {
+
+  }
+
+  @Test
+  public void substringTest() {
+
+  }
+
+  @Test
+  public void charAtTest() {
+
+  }
+}
