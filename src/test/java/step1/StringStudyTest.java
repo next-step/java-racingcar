30c11e267f5148da0f509734c138a09d1053e095
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-12 00:30:46 +0900

    create test of StringStudy.split()

--- src/test/java/step1/StringStudyTest.java
+++ src/test/java/step1/StringStudyTest.java
@@ -1,13 +1,22 @@
 package step1;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 
 public class StringStudyTest {
   StringStudy StringStudy = new StringStudy();
 
   @Test
   public void splitTest() {
-
+    String[] result1 = StringStudy.split("1,2", ",");
+    String[] result2 = StringStudy.split("1", ",");
+    
+    assertThat(result1).contains(new String[]{"1","2"});
+    assertThat(result2).contains(new String[]{"1"});
   }
 
   @Test
