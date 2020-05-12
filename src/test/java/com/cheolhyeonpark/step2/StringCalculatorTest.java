a2b4d717e3c9d8eb189bdbdf79bf06a07af0cda0
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-12 22:51:35 +0900

    Add null check test method

--- src/test/java/com/cheolhyeonpark/step2/StringCalculatorTest.java
+++ src/test/java/com/cheolhyeonpark/step2/StringCalculatorTest.java
@@ -1,17 +1,19 @@
 package com.cheolhyeonpark.step2;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class StringCalculatorTest {
 
     private StringCalculator stringCalculator = new StringCalculator();
 
     @ParameterizedTest
-    @DisplayName("문자열로 이루어진 수식이 입력되면 정수 해답을 출력한다")
+    @DisplayName("문자열로 이루어진 수식이 입력되면 정수 해답을 출력")
     @CsvSource(value = {"2 + 3 * 4 / 2, 10", "5 * 5 * 5, 125", "5 - 10 * 4 / 2, -10"})
     public void calculateStringExpression(String input, int answer) {
         //given
@@ -22,5 +24,15 @@ class StringCalculatorTest {
 
         //then
         assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("입력값이 Null이면 예외를 출력")
+    public void throwIllegalArgumentExceptionWhenThereIsNoInput() {
+        // given
+        String expression = null;
+
+        //when, then
+        assertThatIllegalArgumentException().isThrownBy(() -> stringCalculator.calculate(expression));
     }
 }
