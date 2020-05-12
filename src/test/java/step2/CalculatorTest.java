a6ded02348294d42964d67122683de8b2cddbffa
yjs <yjs2952@naver.com>
2020-05-12 21:55:26 +0900

    테스트 코드 추가

--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -3,6 +3,8 @@ package step2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -14,6 +16,22 @@ public class CalculatorTest {
     @BeforeEach
     public void setUp(){
         calculator = new Calculator();
+    }
+
+    @Test
+    public void operatorTest() {
+        int num1 = 6;
+        int num2 = 3;
+
+        int plusResult = Operator.PLUS.operate(num1, num2);
+        int minusResult = Operator.MINUS.operate(num1, num2);
+        int multipleResult = Operator.MULTIPLE.operate(num1, num2);
+        int divideResult = Operator.DIVIDE.operate(num1, num2);
+
+        assertThat(plusResult).isEqualTo(9);
+        assertThat(minusResult).isEqualTo(3);
+        assertThat(multipleResult).isEqualTo(18);
+        assertThat(divideResult).isEqualTo(2);
     }
 
     @Test
@@ -68,17 +86,15 @@ public class CalculatorTest {
         assertThat(result).isEqualTo(2);
     }
 
-    @Test
-    public void complexCalculateTest(){
-
-        // given
-        String input = "5 / 2 + 3 * 4 - 1";
+    @ParameterizedTest
+    @CsvSource(value = {"5 / 2 + 3 * 4 - 1, 19", "4 + 1 - 2 * 3 - 1 / 2, 4"})
+    public void complexCalculateTest(String input, int expected){
 
         // when
         int result = calculator.calculate(input);
 
         // then
-        assertThat(result).isEqualTo(19);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
