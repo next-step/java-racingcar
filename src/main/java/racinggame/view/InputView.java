ae8c11a3ed3ef24b999fd23a892706948b606cfe
seojaeyeon <seojaeyeon@lotte.net>
2020-05-12 16:48:20 +0900

    feature : inputView 구현 1. 숫자로 변환하는 역할 2. 검증로직의 위치에 대한 고민

---
+++ src/main/java/racinggame/view/InputView.java
@@ -0,0 +1,18 @@
+package racinggame.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static String askNumberOfCar() {
+        System.out.println("자동차 대수는 몇대인가요?");
+        return SCANNER.nextLine();
+    }
+
+    public static String askAttemptOfMovement() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return SCANNER.nextLine();
+    }
+}
