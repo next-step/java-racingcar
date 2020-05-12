fe766c7b9222b8285d0cafa6bf89a04b2eeb240b
kwonheejeong <gmlwjd9405@naver.com>
2020-05-12 21:44:29 +0900

    refactor: input 문자열에 대한 유효성 검사 메서드 분리

--- src/main/java/calculator/calculator/StringCalculator.java
+++ src/main/java/calculator/calculator/StringCalculator.java
@@ -3,6 +3,7 @@ package calculator.calculator;
 
 import calculator.exception.ErrorMessage;
 import calculator.operation.ArithmeticOperationStrategy;
+import calculator.util.IntegerUtil;
 import calculator.util.StringUtil;
 
 public class StringCalculator {
@@ -16,6 +17,7 @@ public class StringCalculator {
     public Integer run(final String input) {
         this.validateNullOrEmpty(input);
         String[] values = input.split(DELIMITER);
+        this.validateInputValues(values);
         try {
             return calculate(values);
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -26,6 +28,12 @@ public class StringCalculator {
     private void validateNullOrEmpty(final String input) {
         if (StringUtil.isEmpty(input)) {
             throw new IllegalArgumentException(ErrorMessage.IS_NULL_OR_EMPTY);
+        }
+    }
+
+    private void validateInputValues(final String[] values) {
+        if (IntegerUtil.isZero(values.length % 2)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_SUPPORTED_FORMAT);
         }
     }
 
