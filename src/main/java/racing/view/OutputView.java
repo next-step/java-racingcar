ba82df16f3284566ef377088dae1d76f2c2de8e1
iljun <sumusb@naver.com>
2020-05-12 23:43:30 +0900

    [refactor] OutputView Object

--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -7,16 +7,20 @@ import java.util.List;
 public class OutputView {
 
     public OutputView() {
+        this.printGuidance();
+    }
+
+    private void printGuidance() {
         System.out.println("실행 결과");
     }
 
     public void printRacingResult(RacingResultDto racingResultDto) {
-        List<Integer> racingPositions = racingResultDto.result();
+        List<Integer> racingPositions = racingResultDto.calculateRacingResult();
         racingPositions.forEach(position -> this.outputCarPosition(position));
         System.out.println();
     }
 
-    public void outputCarPosition(Integer racingPosition) {
+    private void outputCarPosition(Integer racingPosition) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < racingPosition; i++) {
             stringBuilder.append("-");
