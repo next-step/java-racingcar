623e05ecd473400a19832f5159584bd557f902b2
Lim Geun <shing100@naver.com>
2020-05-12 21:03:08 +0900

    feat(자동차 생성) 자동차 생성 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -5,16 +5,24 @@ import java.util.ArrayList;
 public class RacingGame {
     private int time;
     private int carCount;
-    private ArrayList<Integer> carPositions = new ArrayList<Integer>();
+    private ArrayList<Racingcar> carPositions = new ArrayList<Racingcar>();
 
     public RacingGame(int carCount, int time) {
         this.time = time;
         this.carCount = carCount;
     }
 
-    public ArrayList<Integer> move() {
+    public ArrayList<Racingcar> move() {
 
         return null;
+    }
+
+    private void createCar() {
+        Racingcar racingcar;
+        for (int i = 0; i < carCount; i++) {
+            racingcar = new Racingcar(i);
+            carPositions.add(racingcar);
+        }
     }
 
 }
--- src/test/java/AppMainTest.java
+++ src/test/java/AppMainTest.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
 
 public class AppMainTest {
+
 }
