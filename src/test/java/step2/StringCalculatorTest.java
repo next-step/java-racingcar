a350c65e78f393a1965534f73372050ecca1ef1b
junilHwang <tjsdlf4261@naver.com>
2020-05-12 22:23:07 +0900

    feat: Step2 - 문자열 계산기 - 단위 테스트 작성 중 / 덧셈,뺄셈,곱셈 완료

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.HashSet;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
@@ -19,10 +17,48 @@ public class StringCalculatorTest {
     calculator = new StringCalculator();
   }
 
-  @DisplayName("문자열 계산기 테스트")
-  @Test
-  void 사칙연산_테스트_01 () {
-     calculator.getExpression("2 + 3 + 4 + 5");
-     assertThat(2 + 3 + 4 + 5).isEqualTo(calculator.getResult());
+  @DisplayName("음수와 양수에 대한 덧셈 테스트")
+  @ParameterizedTest
+  @CsvSource(value = {
+    "2 + 3 + 4:"+(2 + 3 + 4),
+    "-2 + -3 + -4:"+(-2 + -3 + -4),
+  }, delimiter = ':')
+  void 덧셈_테스트 (String expression, int expected) {
+     calculator.getExpression(expression);
+     assertThat(expected).isEqualTo(calculator.getResult());
+  }
+
+  @DisplayName("음수와 양수에 대한 뺄셈 테스트")
+  @ParameterizedTest
+  @CsvSource(value = {
+    "2 - 3 - 4:" + (2 - 3 - 4),
+    "2 - -3 - -4:" + (2 - -3 - -4),
+  }, delimiter = ':')
+  void 뺄셈_테스 (String expression, int expected) {
+     calculator.getExpression(expression);
+     assertThat(expected).isEqualTo(calculator.getResult());
+  }
+
+  @DisplayName("음수와 양수에 대한 곱셈 테스트")
+  @ParameterizedTest
+  @CsvSource(value = {
+    "2 * 3 * 4:" + (2 * 3 * 4),
+    "-2 * -3 * -4:" + (-2 * -3 * -4),
+  }, delimiter = ':')
+  void 곱셈_테스트 (String expression, int expected) {
+    calculator.getExpression(expression);
+    assertThat(expected).isEqualTo(calculator.getResult());
+  }
+
+  @DisplayName("음수, 양수, 소수에 대한 나눗셈 테스트")
+  @ParameterizedTest
+  @CsvSource(value = {
+    "20 / 10:" + (20 / 10),
+    "-20 / 15:" + (-20.0 / 15),
+    "-20 / -12:" + (-20.0 / -12),
+  }, delimiter = ':')
+  void 나눗셈_테스트 (String expression, int expected) {
+    calculator.getExpression(expression);
+    assertThat(expected).isEqualTo(calculator.getResult());
   }
 }
