328e71aeeaf212b60ff32a675c56a434acbeab1b
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-12 12:41:15 +0900

    refactor(Round)
    - Car 인터페이스에 의존하도록 구현 변경

--- src/main/java/com/nextstep/racingcar/domain/round/Round.java
+++ src/main/java/com/nextstep/racingcar/domain/round/Round.java
@@ -1,30 +1,26 @@
 package com.nextstep.racingcar.domain.round;
 
-import com.nextstep.racingcar.domain.car.RandomMoveCar;
+import com.nextstep.racingcar.domain.car.Car;
 import com.nextstep.racingcar.domain.car.Power;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class Round {
-    private List<RandomMoveCar> randomMoveCars = new ArrayList<>();
+    private List<Car> cars;
 
-    private Round(int numberOfCar) {
-        IntStream.range(0, numberOfCar).forEach(number -> {
-            randomMoveCars.add(new RandomMoveCar());
-        });
+    private Round(List<Car> cars) {
+        this.cars = cars;
     }
 
-    public static Round newRound(int numberOfCar) {
-        return new Round(numberOfCar);
+    public static Round newRound(List<Car> cars) {
+        return new Round(cars);
     }
 
     public int getCarNumber() {
-        return this.randomMoveCars.size();
+        return this.cars.size();
     }
 
     public void moveAll() {
-        this.randomMoveCars.forEach(randomMoveCar -> randomMoveCar.move(Power.generate()));
+        this.cars.forEach(car -> car.move(Power.generate()));
     }
 }
