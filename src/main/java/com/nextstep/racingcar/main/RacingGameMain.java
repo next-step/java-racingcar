d04adae80ab357193881b61de01b17ec4315128b
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-12 22:05:44 +0900

    feat(ui)
    - InputView를 통해 차량수와 반복할 회수를 입력받음
    - OutputView를 통해 수행한 결과를 출력함

---
+++ src/main/java/com/nextstep/racingcar/main/RacingGameMain.java
@@ -0,0 +1,23 @@
+package com.nextstep.racingcar.main;
+
+import com.nextstep.racingcar.application.RacingGame;
+import com.nextstep.racingcar.domain.car.CarFactory;
+import com.nextstep.racingcar.domain.car.RandomMoveCarFactory;
+import com.nextstep.racingcar.ui.InputView;
+import com.nextstep.racingcar.ui.OutputView;
+
+import java.util.Scanner;
+
+public class RacingGameMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        InputView inputView = InputView.create(scanner);
+
+        RacingGame racingGame = new RacingGame(inputView.getCarNumber(), inputView.getRoundNumber());
+        CarFactory randomFactory = new RandomMoveCarFactory();
+        racingGame.run(randomFactory);
+
+        OutputView outputView = new OutputView(racingGame.checkResults());
+        outputView.printResults();
+    }
+}
