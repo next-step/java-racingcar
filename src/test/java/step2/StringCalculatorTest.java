ceddf063f598173890cff22b25cd639ed7863424
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-12 22:36:33 +0900

    init StringCalculator

---
+++ src/test/java/step2/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package step2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringCalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
