1e44d3ec3625b9d97e36b1f2c5606f1d9af3fdb2
Kim yongjin <yongjin5184@gmail.com>
2020-05-13 07:20:26 +0900

    Operate 메소드를 검증하는 테스트 코드 추가

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -3,8 +3,11 @@ package calculator;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -18,6 +21,22 @@ class OperatorTest {
     void findRightOperator(final String symbol) {
         Operator operator = Operator.findOperator(symbol);
         assertThat(symbol).isIn(operator.getSymbol());
+    }
+
+    @DisplayName("연산자와 피연산자를 받아 Operate 메소드를 수행하는 테스트")
+    @ParameterizedTest
+    @MethodSource("operations")
+    void validateOperate (final Operand leftOperand, final Operator operator, final Operand rightOperand, final Operand expected) {
+        assertThat(operator.operate(leftOperand, rightOperand)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> operations() {
+        return Stream.of(
+            Arguments.of(Operand.of(2), Operator.PLUS, Operand.of(3), Operand.of(5)),
+            Arguments.of(Operand.of(-42), Operator.MINUS, Operand.of(15), Operand.of(-57)),
+            Arguments.of(Operand.of(7), Operator.MULTIPLY, Operand.of(-9), Operand.of(-63)),
+            Arguments.of(Operand.of(42), Operator.DIVIDE, Operand.of(2), Operand.of(21))
+        );
     }
 
     @DisplayName("나누기 연산일 때, 오른쪽 피연산자가 0이면 0으로 나누었을 때, throw IllegalArgumentException 를 실행")
