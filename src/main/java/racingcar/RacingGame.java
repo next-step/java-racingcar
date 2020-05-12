634262b82c14da1d876d05a81c15ef7e969f6ec4
Lim Geun <shing100@naver.com>
2020-05-12 21:13:47 +0900

    feat(이동 조건 구현) 자동차 이동 조건 구현

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,11 +1,14 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RacingGame {
     private int time;
     private int carCount;
     private ArrayList<Racingcar> carPositions = new ArrayList<Racingcar>();
+
+    private int GO_CONDITION_COUNT = 4;
 
     public RacingGame(int carCount, int time) {
         this.time = time;
@@ -15,6 +18,11 @@ public class RacingGame {
     public ArrayList<Racingcar> move() {
 
         return null;
+    }
+
+    private Boolean isGo() {
+        Random random = new Random();
+        return random.nextInt(10) > GO_CONDITION_COUNT;
     }
 
     private void createCar() {
--- src/test/java/step3/RacingGameTest.java
+++ src/test/java/step3/RacingGameTest.java
@@ -1,4 +1,21 @@
 package step3;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 public class RacingGameTest {
+
+    @Test
+    @DisplayName("랜덤 숫자 생성 테스트")
+    void createRandomNumberTest() {
+        Random random = new Random();
+        int count = 100;
+
+        for (int i = 0; i < count; i++) {
+            Assertions.assertThat(random.nextInt(10)).isBetween(0, 9);
+        }
+    }
 }
