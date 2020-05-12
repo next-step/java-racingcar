fe6bd63435c75ebf43c02f7930acdceceb700a55
jun-pil park <iamfeel2@gmail.com>
2020-05-12 15:52:25 +0900

    test (StringCalculator) : null, 비정상적인 입력 등에 대한 테스트 케이스 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,14 +1,18 @@
 package calculator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 class StringCalculatorTest {
 
     private StringCalculator stringCalculator;
+
     @BeforeEach
     void setUp() {
         stringCalculator = new StringCalculator();
@@ -20,6 +24,29 @@ class StringCalculatorTest {
     void calculate(String formula, float calculatedValue) {
         Float result = stringCalculator.calculate(formula);
 
-        Assertions.assertThat(result).isEqualTo(calculatedValue);
+        assertThat(result).isEqualTo(calculatedValue);
     }
+
+    @DisplayName("null 또는 빈 입력이 들어왔을 때 IllegalArgumentException 발생 여부 테스트")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void calculateNullAndEmptyTest(String formula) {
+        Throwable throwable = catchThrowable(() -> {
+            Float result = stringCalculator.calculate(formula);
+        });
+
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("정상적인 입력으로 계산이 되는지 확인하는 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1 & 2 + 3:6" , "1 ! 2 / 3:1", "3 % 4:-1"}, delimiter = ':')
+    void calculateNotOperatorSymbolTest(String formula, float calculatedValue) {
+        Throwable throwable = catchThrowable(() -> {
+            Float result = stringCalculator.calculate(formula);
+        });
+
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
