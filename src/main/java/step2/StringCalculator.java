f25aa54efe7cc54dfc09b6353257b15656701ff1
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-12 22:57:58 +0900

    create mulitply  with test

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -1,24 +1,38 @@
 package step2;
 
 public class StringCalculator {
-    
+
     /**
      * left와 right를 더해준다.
-     * @param left : int, 더할 값
+     * 
+     * @param left  : int, 더할 값
      * @param right : int, 더할 값
      * @return String, 더한 값을 반환
      */
     public String plus(int left, int right) {
-        return String.valueOf(left+right);
+        return String.valueOf(left + right);
     }
+
     /**
      * left에서 right를 뺀다.
-     * @param left : int, 뺄셈 대상
+     * 
+     * @param left  : int, 뺄셈 대상
      * @param right : int, left에서 빼줄 값
      * @return String, left에서 right를 뺀 값을 반환
      */
     public String minus(int left, int right) {
-        return String.valueOf(left-right);
+        return String.valueOf(left - right);
+    }
+
+    /**
+     * left와 right를 곱한다.
+     * 
+     * @param left  : int, 곱할 값
+     * @param right : int, 곱할 값
+     * @return String, 곱한 값을 반환
+     */
+    public String multiply(int left, int right) {
+        return String.valueOf(left * right);
     }
 
 }
--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -1,52 +1,54 @@
 package step2;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
     StringCalculator stringCalculator = new StringCalculator();
 
-	@Test
-	@DisplayName("덧셈 테스트")
-	void plus() {
-	    String result = stringCalculator.plus(2,3);
-	    
-	    Assertions.assertThat(result).isEqualTo("5");
-	}
+    @Test
+    @DisplayName("덧셈 테스트")
+    void plus() {
+        String result = stringCalculator.plus(2, 3);
 
-	@Test
-	@DisplayName("뺄셈 테스트")
-	void minus() {
-	    String result = stringCalculator.minus(2,3);
-	    
-	    Assertions.assertThat(result).isEqualTo("-1");
-	}
+        assertThat(result).isEqualTo("5");
+    }
 
-	@Test
-	@DisplayName("곱셈 테스트")
-	void multiply() {
-	}
+    @Test
+    @DisplayName("뺄셈 테스트")
+    void minus() {
+        String result = stringCalculator.minus(2, 3);
 
-	@Test
-	@DisplayName("나눗셈 테스트")
-	void division() {
-	}
+        assertThat(result).isEqualTo("-1");
+    }
 
-	@Test
-	@DisplayName("입력값이 null이거나 빈 공백 문자일 경우")
-	void nullOrEmpty() {
-	}
+    @Test
+    @DisplayName("곱셈 테스트")
+    void multiply() {
+        String result = stringCalculator.multiply(2, 3);
 
-	@Test
-	@DisplayName("사칙연산 기호가 아닌 경우")
-	void notSign() {
-	}
-	
-	@Test
-	@DisplayName("사칙연산을 모두 포함하는 테스트")
-	void testAll() {
-	}
+        assertThat(result).isEqualTo("6");
+    }
+
+    @Test
+    @DisplayName("나눗셈 테스트")
+    void division() {
+    }
+
+    @Test
+    @DisplayName("입력값이 null이거나 빈 공백 문자일 경우")
+    void nullOrEmpty() {
+    }
+
+    @Test
+    @DisplayName("사칙연산 기호가 아닌 경우")
+    void notSign() {
+    }
+
+    @Test
+    @DisplayName("사칙연산을 모두 포함하는 테스트")
+    void testAll() {
+    }
 }
