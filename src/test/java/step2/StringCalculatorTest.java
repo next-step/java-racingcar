6a8974374a7db051f33e60fb6531ec9b51cdf8ce
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-13 00:09:25 +0900

    refactor test cases with @ParameterizedTest

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -5,69 +5,74 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class StringCalculatorTest {
     StringCalculator stringCalculator = new StringCalculator();
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = { "2, 3, 5.0" })
     @DisplayName("덧셈 테스트")
-    void plus() {
-        String result = stringCalculator.plus(2.0, 3);
+    void plus(double left, double right, String excepted) {
+        String result = stringCalculator.plus(left, right);
 
-        assertThat(result).isEqualTo("5.0");
+        assertThat(excepted).isEqualTo(result);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = { "2, 3, -1.0" })
     @DisplayName("뺄셈 테스트")
-    void minus() {
-        String result = stringCalculator.minus(2.0, 3);
+    void minus(double left, double right, String excepted) {
+        String result = stringCalculator.minus(left, right);
 
-        assertThat(result).isEqualTo("-1.0");
+        assertThat(excepted).isEqualTo(result);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = { "2, 3, 6.0" })
     @DisplayName("곱셈 테스트")
-    void multiply() {
-        String result = stringCalculator.multiply(2.0, 3);
+    void multiply(double left, double right, String excepted) {
+        String result = stringCalculator.multiply(left, right);
 
-        assertThat(result).isEqualTo("6.0");
+        assertThat(excepted).isEqualTo(result);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = { "2, 4, 0.5" })
     @DisplayName("나눗셈 테스트")
-    void division() {
-        String result = stringCalculator.division(2.0, 4);
+    void division(double left, double right, String excepted) {
+        String result = stringCalculator.division(left, right);
 
-        assertThat(result).isEqualTo("0.5");
+        assertThat(excepted).isEqualTo(result);
     }
 
     @Test
     @DisplayName("입력값이 null이거나 빈 공백 문자일 경우")
-    void nullOrEmpty() {
-        // null인경우
+    void checkNull() {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
             stringCalculator.checkNull(null);
-        });
-        // 빈 몬자열인경우
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
-            stringCalculator.checkEmpty("");
-        });
-        // 빈 공백 문자열일경우
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
-            stringCalculator.checkEmpty("        ");
         });
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"+","-","*","/"})
+    @ValueSource(strings = { "", "  " })
+    @DisplayName("입력값이 null이거나 빈 공백 문자일 경우")
+    void checkEmpty(String param) {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            stringCalculator.checkEmpty(param);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "+", "-", "*", "/" })
     @DisplayName("사칙연산 기호인 경우")
     void correctSign(String sign) {
         stringCalculator.checkSign(sign);
     }
-    
+
     @ParameterizedTest
-    @ValueSource(strings = {" ","1","a","%"})
+    @ValueSource(strings = { " ", "1", "a", "%" })
     @DisplayName("사칙연산 기호가 아닌 경우")
     void notCorrectSign(String sign) {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
