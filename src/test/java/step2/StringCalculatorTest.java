efc178e5d1492fae7dd2235d01d8fa3d87bd482c
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-12 22:42:46 +0900

    create test list

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -2,13 +2,43 @@ package step2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("덧셈 테스트")
+	void plus() {
 	}
 
+	@Test
+	@DisplayName("뺄셈 테스트")
+	void minus() {
+	}
+
+	@Test
+	@DisplayName("곱셈 테스트")
+	void multiply() {
+	}
+
+	@Test
+	@DisplayName("나눗셈 테스트")
+	void division() {
+	}
+
+	@Test
+	@DisplayName("입력값이 null이거나 빈 공백 문자일 경우")
+	void nullOrEmpty() {
+	}
+
+	@Test
+	@DisplayName("사칙연산 기호가 아닌 경우")
+	void notSign() {
+	}
+	
+	@Test
+	@DisplayName("사칙연산을 모두 포함하는 테스트")
+	void testAll() {
+	}
 }
