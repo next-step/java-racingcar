89d59bc2122a76c00ed04f9bc62a3b5f304d1c5d
kwonheejeong <gmlwjd9405@naver.com>
2020-05-14 07:24:01 +0900

    refactor: InputView 메시지를 내부로 이동

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -8,12 +8,9 @@ import java.util.List;
 
 public class RacingGame {
 
-    private static final String NUM_OF_CAR_MESSAGE = "자동차 대수는 몇 대 인가요?";
-    private static final String LOOP_COUNT = "시도할 횟수는 몇 회 인가요?";
-
     public static void main(String[] args) {
-        int numOfCar = InputView.input(NUM_OF_CAR_MESSAGE);
-        int loopCount = InputView.input(LOOP_COUNT);
+        int numOfCar = InputView.inputNumOfCar();
+        int loopCount = InputView.inputLoopCount();
 
         Racing racing = Racing.of(numOfCar, new RandomMovingStrategy());
         ResultView.printResultMessage();
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,17 +6,30 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String NUM_OF_CAR_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String LOOP_COUNT_MESSAGE = "시도할 횟수는 몇 회 인가요?";
+
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static int input(String content) {
-        System.out.println(content);
-        return validatedInput(SCANNER.nextInt());
+    public static int inputNumOfCar() {
+        return input(NUM_OF_CAR_MESSAGE);
     }
 
-    private static int validatedInput(final int input) {
+    public static int inputLoopCount() {
+        return input(LOOP_COUNT_MESSAGE);
+    }
+
+    private static int input(String content) {
+        System.out.println(content);
+        int value = SCANNER.nextInt();
+        validateInput(value);
+
+        return value;
+    }
+
+    private static void validateInput(final int input) {
         if (input < 0) {
             throw new IllegalArgumentException(ErrorMessage.MORE_THAN_ZERO);
         }
-        return input;
     }
 }
