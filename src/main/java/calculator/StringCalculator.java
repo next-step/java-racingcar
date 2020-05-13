988f4b3a5336df1ca2ec9725995b23988e19f980
sangyounkim <505>
2020-05-14 01:47:16 +0900

    - 자바 컨벤션 위배 수정. - 계산 구현체를 모두 enum에서 처리하도록 수정. - 불필요한 주석 제거.

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,50 @@
+package calculator;
+
+import java.util.Arrays;
+
+public class StringCalculator {
+    final static String SEPARATOR = " "; //입력되는 inputData Seperator
+    final static int CALCULATE_COUNT = 2; //계산하는 대상 갯수 현재 계산기는 2개만 계산 될 수 있다.
+    final static int MIN_CALCULATE_LENGTH = 3;
+
+    public StringCalculator(){
+        super();
+    }
+
+    public double calculate(String inputData) {
+        if (inputData == null || inputData.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] inputArray = inputData.split(SEPARATOR);
+
+        if (isInValidLength(inputArray)) {
+            throw new IllegalArgumentException();
+        }
+
+        double result = calculate(inputArray);
+
+        return result;
+    }
+
+    public double calculate(String[] inputArray) {
+        if (isInValidLength(inputArray)) {
+            return Double.parseDouble(inputArray[0]);
+        }
+
+        Operator operator= Operator.find(inputArray[CALCULATE_COUNT-1]);
+      //  Calculator calculator = operator.calculate(operator);
+        try {
+            //double result = calculator.calculator(Double.parseDouble(inputArray[CALCULATE_COUNT-2]), Double.parseDouble(inputArray[CALCULATE_COUNT]));
+            double result = operator.calculate(Double.parseDouble(inputArray[CALCULATE_COUNT-2]), Double.parseDouble(inputArray[CALCULATE_COUNT]));
+            inputArray[CALCULATE_COUNT] = Double.toString(result);
+            String[] _inputArray = Arrays.copyOfRange(inputArray, CALCULATE_COUNT, inputArray.length);
+            return calculate(_inputArray);
+        } catch (NumberFormatException exception) {
+            throw new NumberFormatException("operand is must double");
+        }
+    }
+    public boolean isInValidLength(String[] array) {
+        return array.length < MIN_CALCULATE_LENGTH ? true : false;
+    }
+}
