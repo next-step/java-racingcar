151d0ce89a08c89769324b58177d96f45814c0da
kwonheejeong <gmlwjd9405@naver.com>
2020-05-13 20:23:29 +0900

    feat: Random 전진 전략 구현

---
+++ src/main/java/racingcar/moving/RandomMovingStrategy.java
@@ -0,0 +1,19 @@
+package racingcar.moving;
+
+import java.util.Random;
+
+public class RandomMovingStrategy implements MovingStrategy {
+
+    private static final int MIN_NUM = 0;
+    private static final int MAX_NUM = 9;
+    private static final int BASE_LINE_NUM = 4;
+
+    @Override
+    public boolean move() {
+        return random() >= BASE_LINE_NUM;
+    }
+
+    private int random() {
+        return new Random().nextInt(RandomMovingStrategy.MAX_NUM) + RandomMovingStrategy.MIN_NUM;
+    }
+}
