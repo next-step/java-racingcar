0fa75d7047bdbfe6b62d5ccfef6e1e33b82b1b0a
Gobukgol <zxczc159@gmail.com>
2020-05-13 16:09:45 +0900

    feat(RacingCarApplication): 메인 함수 추가 refactor(OutPutView): 각 시도 결과를 출력할때 번호도 출력하도록 변경

---
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import racingcar.domain.GameInfo;
+import racingcar.domain.GameResult;
+import racingcar.domain.RacingGame;
+import racingcar.dto.InputValueDto;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarApplication {
+    public static void main(String[] args) {
+        String numberOfCar = InputView.inputNumberOfCar();
+        String numberOfPhase = InputView.inputNumberOfPhase();
+
+        InputValueDto inputValueDto = new InputValueDto(numberOfCar, numberOfPhase);
+
+        GameInfo gameInfo = new GameInfo(inputValueDto.getNumberOfCar(), inputValueDto.getNumberOfPhase());
+
+        RacingGame racingGame = new RacingGame(gameInfo);
+
+        GameResult gameResult = racingGame.startGame();
+
+        OutputView.printGameResult(gameResult);
+    }
+}
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -11,15 +11,15 @@ public class OutputView {
         int numberOfPhase = gameResult.getNumberOfPhase();
         System.out.println("실행 결과");
 
-
         for (int i = 1; i <= numberOfPhase; i++) {
-            printPhaseResult(gameResult.findByPhaseNumber(i));
+            printPhaseResult(gameResult.findByPhaseNumber(i), i);
             System.out.println();
         }
 
     }
 
-    public static void printPhaseResult(PhaseResult phaseResult) {
+    public static void printPhaseResult(PhaseResult phaseResult, int phase) {
+        System.out.println("시도 번호 - " + phase);
         phaseResult.getRaceResult().stream()
                 .map(OutputView::visualizeLocation)
                 .forEach(System.out::println);
