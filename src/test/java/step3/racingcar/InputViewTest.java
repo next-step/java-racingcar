c90604ebec733fc3b8a4518a0c1f4c17fc5fe620
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-13 16:01:17 +0900

    [docs] 사용자 입력값을 받는 System.in UI 기능 목록 README 추가

---
+++ src/test/java/step3/racingcar/InputViewTest.java
@@ -0,0 +1,13 @@
+package step3.racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputViewTest {
+}
---
+++ src/test/java/step3/racingcar/RacingGameTest.java
@@ -0,0 +1,4 @@
+package step3.racingcar;
+
+public class RacingGameTest {
+}
