ef2f3c43edb9602989fdac1b6e0a11d5ca6d2bf5
yjs <yjs2952@naver.com>
2020-05-13 19:40:32 +0900

    feat: ResultView 출력 UI를 담당하는 클래스 추가

---
+++ src/main/java/step3/ResultView.java
@@ -0,0 +1,26 @@
+package step3;
+
+import java.util.List;
+
+public class ResultView {
+
+    public void print(List<int[]> result){
+        System.out.println("실행 결과");
+
+        for (int[] positions : result) {
+
+            printResult(positions);
+        }
+    }
+
+    private void printResult(int[] positions) {
+        for (int position : positions) {
+
+            for (int i = 0; i < position; i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
