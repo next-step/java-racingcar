3c8a2377dadbc594e9b540def810174d8d8d9692
iljun <sumusb@naver.com>
2020-05-13 23:27:18 +0900

    [refactor] RacingGameResult Object

--- src/main/java/racing/dto/RacingGameResult.java
+++ src/main/java/racing/dto/RacingGameResult.java
@@ -1,17 +1,40 @@
 package racing.dto;
 
-import racing.domain.RacingGame;
+import racing.domain.Car;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGameResult {
-    private List<Integer> racingResult;
+    private List<Car> cars;
+    private int maxPosition;
 
-    public RacingGameResult(RacingGame racingGame) {
-        this.racingResult = racingGame.calculateRacingScore();
+    public RacingGameResult(List<Car> cars) {
+        this.cars = cars;
+        this.calculateMaxPosition();
     }
 
-    public List<Integer> getRacingResult() {
-        return this.racingResult;
+    private void calculateMaxPosition() {
+        this.maxPosition = this.cars
+                .stream()
+                .map(car -> car.findCurrentPosition())
+                .max(Integer::compareTo)
+                .orElse(0);
+    }
+
+    public List<Car> calculateWinners() {
+        List<Car> winners = this.cars.stream()
+                .filter(car -> car.findCurrentPosition() == this.maxPosition)
+                .collect(Collectors.toList());
+
+        if (winners.isEmpty()) {
+            return Collections.EMPTY_LIST;
+        }
+        return winners;
+    }
+
+    public List<Car> getCars() {
+        return this.cars;
     }
 }
