ee55483e706c6268a3bc7bac2923b98c6ada5490
jwooss <wodn4131@gmail.com>
2020-05-13 23:08:34 +0900

    feat: 랜덤 값 결과에 따라서 움직임 상태를 가지는 객체 구현
    - 전략패턴 활용

---
+++ src/main/java/step3/ForwardMoveStrategy.java
@@ -0,0 +1,9 @@
+package step3;
+
+public class ForwardMoveStrategy implements MoveStrategy {
+
+    @Override
+    public boolean isMovable() {
+        return true;
+    }
+}
---
+++ src/test/java/step3/MoveStrategyTest.java
@@ -0,0 +1,37 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveStrategyTest {
+
+    @DisplayName("ForwardMoveStrategy는 움직일 수 있는 상태이기 떄문에 isMovable() 호출시 항상 true를 반환한다")
+    @Test
+    void forwardMoveStrategyTest() {
+        // given
+        MoveStrategy moveStrategy = new ForwardMoveStrategy();
+
+        // when
+        boolean isMovable = moveStrategy.isMovable();
+
+        // then
+        assertThat(isMovable).isTrue();
+    }
+
+
+    @DisplayName("StopMoveStrategy는 움직일 수 없는 상태이기 떄문에 isMovable() 호출시 항상 false를 반환한다")
+    @Test
+    void stopMoveStrategyTest() {
+        // given
+        MoveStrategy moveStrategy = new StopMoveStrategy();
+
+        // when
+        boolean isMovable = moveStrategy.isMovable();
+
+        // then
+        assertThat(isMovable).isFalse();
+    }
+}
