149b11cf8d0bc0d4fbf3fe3602bfa7068e9123f1
jun-pil park <iamfeel2@gmail.com>
2020-05-13 20:57:16 +0900

    fix (StringCalculator) : calculate 계산 로직 변경, validate 상세 확인 함수들 추가
    1. Queue 에 저장하지 않고, Stirng[] 에서 계산하도록 변경
    2. 수식의 연산자와 피연산자의 순서 확인하는 함수 추가
    3. 수식의 연산자와 피연산자의 수(Count) 가 계산 가능한 수 인지 확인하는 함수 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,57 +1,54 @@
 package calculator;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class StringCalculator {
 
-    private Queue<Float> operands = new ArrayDeque<>();
-    private Queue<Operator> operators = new ArrayDeque<>();
+    private String[] formulaElements;
 
-    private Float beforeNumber = null;
-
-    public float calculate(String formula) {
+    public StringCalculator(String formula) {
         checkNullOrEmpty(formula);
+        formulaElements = formula.split(" ");
+        checkOrderFormula(formulaElements);
+    }
 
-        String[] formulaSplit = formula.split(" ");
-        separateOperandFromOperator(formulaSplit);
-
-        for(Operator operator = operators.poll() ; operator != null ; operator = operators.poll()) {
-            beforeNumber = calculate(getBeforeNumber(), operator, getAfterNumber());
+    public float calculate() {
+        Operand result = new Operand(formulaElements[0]);
+        for(int i = 1 ; i < formulaElements.length ; i += 2) {
+            Operator operator = Operator.getOperator(formulaElements[i]);
+            result = result.calculate(operator, new Operand(formulaElements[i+1]));
         }
-
-        return beforeNumber;
-    }
-
-    private Float calculate(Float beforeNumber, Operator operator, Float afterNumber) {
-        return operator.calculate(beforeNumber, afterNumber);
-    }
-
-    private void separateOperandFromOperator(String[] formulaSplit) {
-        for(String element : formulaSplit) {
-            if(Operator.isOperatorSymbol(element)) {
-                operators.add(Operator.getOperator(element));
-            }
-            else{
-                operands.add(Float.valueOf(element));
-            }
-        }
-    }
-
-    private Float getBeforeNumber() {
-        if(beforeNumber == null) {
-            beforeNumber = operands.poll();
-        }
-        return beforeNumber;
-    }
-
-    private Float getAfterNumber() {
-        return operands.poll();
+        return result.getNumber();
     }
 
     private void checkNullOrEmpty(String str) throws IllegalArgumentException {
         if(str == null || str.isEmpty()) {
             throw new IllegalArgumentException("Argument is null or empty.");
+        }
+    }
+
+    private void checkOrderFormula(String[] formulaElements) throws IllegalArgumentException {
+        checkFormulaElementCount(formulaElements);
+
+        for(int i = 0 ; i < formulaElements.length ; i++) {
+            checkOrderOperand(i, formulaElements[i]);
+            checkOrderOperator(i, formulaElements[i]);
+        }
+    }
+
+    private void checkFormulaElementCount(String[] formulaElements) {
+        if(formulaElements.length % 2 == 0) {
+            throw new IllegalArgumentException("There are errors in the number of operators and operands.");
+        }
+    }
+
+    private void checkOrderOperand(int index, String element) {
+        if(index % 2 == 0 && !Operand.isOperand(element)) {
+            throw new IllegalArgumentException("The number must come at index " + index);
+        }
+    }
+
+    private void checkOrderOperator(int index, String element) {
+        if(index % 2 == 1 && !Operator.isOperatorSymbol(element)) {
+            throw new IllegalArgumentException("The operator must come at index " + index);
         }
     }
 }
