c2ac5b6fbbdf5548630e7ccc0fb41958acca895b
Gobukgol <zxczc159@gmail.com>
2020-05-13 15:09:31 +0900

    feat(GameResult): 게임 전체 결과를 관리하는 GameResult.java 추가

---
+++ src/main/java/racingcar/domain/GameResult.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameResult {
+    private final int numberOfPhase;
+    private List<PhaseResult> phaseResults;
+
+    public GameResult(int numberOfPhase) {
+        this.numberOfPhase = numberOfPhase;
+        phaseResults = new ArrayList<>();
+    }
+
+    public int addPhaseResult(PhaseResult phaseResult) {
+        this.phaseResults.add(phaseResult);
+        return this.phaseResults.size();
+    }
+
+    public PhaseResult findByPhaseNumber(int phaseNumber) {
+        validatePhaseNumber(phaseNumber);
+        PhaseResult findPhaseResult = phaseResults.get(phaseNumber - 1);
+        return new PhaseResult(findPhaseResult.getRaceResult());
+    }
+
+    private void validatePhaseNumber(int phaseNumber) {
+        if (phaseNumber < 1 || phaseNumber > numberOfPhase) {
+            throw new IllegalArgumentException(String.format("잘못된 시도 번호입니다. - %d\n", phaseNumber));
+        }
+    }
+}
---
+++ src/test/java/racingcar/domain/GameResultTest.java
@@ -0,0 +1,52 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameResultTest {
+
+    @DisplayName("시도 결과를 GameResult에 추가하고 그 번호를 반환한다.")
+    @Test
+    void addPhaseResult() {
+        GameResult gameResult = new GameResult(1);
+        PhaseResult phaseResult = new PhaseResult(Arrays.asList(1, 0, 1));
+
+        int actualResult = gameResult.addPhaseResult(phaseResult);
+        assertThat(actualResult).isEqualTo(1);
+    }
+
+    @DisplayName("시도 번호에 맞는 PhaseResult를 반환받는다.")
+    @Test
+    void findByPhaseNumber() {
+        PhaseResult phaseResult = new PhaseResult(Arrays.asList(1, 0, 1));
+        GameResult gameResult = new GameResult(1);
+        gameResult.addPhaseResult(phaseResult);
+
+        PhaseResult findPhaseResult = gameResult.findByPhaseNumber(1);
+        assertThat(findPhaseResult.getRaceResult()).isEqualTo(phaseResult.getRaceResult());
+    }
+
+    @DisplayName("전체 게임 시도 횟수보다 크거나 1보다 작은 값은 IllegalArgumentException을 throw 한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 2})
+    void findByPhaseNumberThrowsException(int wrongPhaseNumber) {
+        PhaseResult phaseResult = new PhaseResult(Arrays.asList(1, 0, 1));
+        GameResult gameResult = new GameResult(1);
+        gameResult.addPhaseResult(phaseResult);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> gameResult.findByPhaseNumber(wrongPhaseNumber))
+                .withMessage(String.format("잘못된 시도 번호입니다. - %d\n", wrongPhaseNumber));
+    }
+}
