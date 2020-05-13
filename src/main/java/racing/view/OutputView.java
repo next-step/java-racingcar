a43503da4d7bc074268fb37d971f44eb167c7256
iljun <sumusb@naver.com>
2020-05-13 23:27:30 +0900

    [refactor] OutputView

--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -1,26 +1,51 @@
 package racing.view;
 
+import racing.domain.Car;
 import racing.dto.RacingGameResult;
 
 import java.util.List;
 
 public class OutputView {
+    private static final String HYPHEN = "-";
+    private static final String COLON = " : ";
 
     public void printGuidance() {
         System.out.println("실행 결과");
     }
 
     public void printRacingResult(RacingGameResult racingGameResult) {
-        List<Integer> racingPositions = racingGameResult.getRacingResult();
-        racingPositions.forEach(position -> this.outputCarPosition(position));
+        List<Car> cars = racingGameResult.getCars();
+        cars.forEach(car -> this.outputCarPosition(car.getName(), car.findCurrentPosition()));
         System.out.println();
     }
 
-    private void outputCarPosition(Integer racingPosition) {
+    private void outputCarPosition(String name, int position) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < racingPosition; i++) {
-            stringBuilder.append("-");
+        stringBuilder.append(name);
+        stringBuilder.append(COLON);
+        for (int i = 0; i < position; i++) {
+            stringBuilder.append(HYPHEN);
         }
         System.out.println(stringBuilder);
+    }
+
+    public void printRacingWinners(RacingGameResult racingGameResult) {
+        List<Car> winners = racingGameResult.calculateWinners();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Car car : winners) {
+            stringBuilder.append(car.getName());
+            stringBuilder.append(", ");
+        }
+        stringBuilder = this.removeLastColon(stringBuilder);
+        stringBuilder.append("가 최종 우승했습니다.");
+        System.out.println(stringBuilder);
+    }
+
+    private StringBuilder removeLastColon(StringBuilder stringBuilder) {
+        int lastColonIndex = stringBuilder.lastIndexOf(", ");
+        if (lastColonIndex != -1) {
+            stringBuilder.replace(lastColonIndex, lastColonIndex + 1, "");
+        }
+        return stringBuilder;
     }
 }
