b616dbdeaf6db9e9b98e51b3785b049b6f08473b
이준형 <p_illusion@naver.com>
2020-05-13 09:38:15 +0900

    Step2 (#745)
    * step2
    
    * step2
    
    * dependencies modify : 잘못 추가된 의존성 제거
    
    * refactoring : 1.statement 괄호 추가 2.equals 순서 변경
    
    * refactoring : 불필요한 검증 제거
    
    * refactoring : 접근제어자 설정
    
    * refactoring : DELIMETER 상수화
    
    * refactoring : 사칙연산 별도 분리 및 로직 단순화
    
    * refactoring : 코드 스타일 K&R 스타일로 변경
    
    * refactoring : 1. 메서드당 들여쓰기 한번 수정 2. valCehck 메소드 초기화 삭제 3. 안티패턴 제거
    
    * refactoring : 1. 많은 양의 테스트 케이스 삭제
    
    * refactoring : K&R 스타일로 변경

---
+++ src/main/java/step2/Calculator.java
@@ -0,0 +1,44 @@
+package step2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Calculator {
+
+    private static final String DELIMETER = " ";
+
+    public static boolean valCheck(String[] input) {
+
+        if (input == null || input.length == 0) {
+            throw new IllegalArgumentException("계산식을 입력해주세요.");
+        }
+
+        if (input.length % 2 == 0) {
+            throw new IllegalArgumentException("계산식이 올바르지 않습니다.");
+        }
+
+        return true;
+    }
+
+    public static double isNumeric(String s) {
+        try {
+            return Double.parseDouble(s);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다. " + s);
+        }
+    }
+
+    public static double calculator(String input) {
+
+        String[] splitInput = input.split(DELIMETER);
+        valCheck(splitInput);
+
+        double result = isNumeric(splitInput[0]);
+        for(int i = 1; i < splitInput.length; i += 2) {
+            Operator operator = Operator.getOperation(splitInput[i]);
+            result = operator.operate(result, isNumeric(splitInput[i+1]));
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/step2/CalTest.java
@@ -0,0 +1,106 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalTest {
+
+    Calculator cal = new Calculator();
+
+    @Test
+    void plus(){
+        String input = "1 + 2 + 5";
+
+        double result = cal.calculator(input);
+
+        assertThat(result).isEqualTo(8);
+    }
+
+    @Test
+    void minu(){
+        String input = "5 - 2 - 2";
+
+        double result = cal.calculator(input);
+
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void mul(){
+        String input = "2 * 2 * 5";
+
+        double result = cal.calculator(input);
+
+        assertThat(result).isEqualTo(20);
+    }
+
+    @Test
+    void div(){
+        String input = "6 / 2";
+
+        double result = cal.calculator(input);
+
+        assertThat(result).isEqualTo(3);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = { "", " ", "  " })
+    @DisplayName("Null 체크")
+    void nullCheck(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() ->
+            {
+                cal.calculator(input);
+            }
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "1 ^ 2", "2 ^ 2", "2 % 2" })
+    @DisplayName("사칙연산이 기호가 아닌 경우")
+    void arithmeticOperationsCheck(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                {
+                    cal.calculator(input);
+                }
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "1 + a", "3 + &"})
+    @DisplayName("숫자가 아닌 경우")
+    void NumCheck(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                {
+                    cal.calculator(input);
+                }
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForCalculate")
+    @DisplayName("다수의 사칙 연산 체크")
+    void calculate(String input, double result) {
+        assertThat(Calculator.calculator(input)).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> provideStringsForCalculate() {
+        return Stream.of(
+                Arguments.of("2 + 3 - 4 * 2", "2"),
+                Arguments.of("4 * 1 / 2 + 5", "7"),
+                Arguments.of("6 + 1 * 8 - 2", "54")
+        );
+    }
+
+}
