8fd135c773d48519114f4bb09a90ca4a8e5c7e30
wangyongchoi <choiwangyong@gmail.com>
2020-05-14 00:47:20 +0900

    test: 자동차 전진 기능 테스트 코드 추가
    자동차 전진 기능 테스트 코드를 추가하였습니다.

--- src/test/java/step3/RacingGameTest.java
+++ src/test/java/step3/RacingGameTest.java
@@ -1,17 +1,26 @@
 package step3;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
 class RacingGameTest {
 
-    RacingGame racingGame = new RacingGame();
+    RacingGame racingGame = new RacingGame(5,2);
 
     @RepeatedTest(10)
     void getRandomPosition() {
         assertThat(racingGame.getRandomPosition()).isBetween(0, 10);
+    }
+
+    @RepeatedTest(10)
+    void move() {
+        //when
+        int[] result = racingGame.move();
+
+        //then
+        for (int i = 0; i < result.length; ++i) {
+            assertThat(result[i]).isGreaterThanOrEqualTo(0);
+        }
     }
 }
