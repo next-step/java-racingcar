4184e9364245400366d3902c6929a317af535c30
DongHyukki <kderr2791@gmail.com>
2020-05-13 23:13:51 +0900

    test : CarRacingTest
    Car Unit Tests Moved to CarTest.Class

---
+++ src/test/java/CarRacing/CarTest.java
@@ -0,0 +1,44 @@
+package CarRacing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("Car Init")
+    public void carInitTest() {
+        Car car = new Car("1", new Position());
+        assertThat(car.getPosition().location).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Car Forward Move")
+    public void carForwardMoveTest() {
+        Car car = new Car("1", new Position());
+        car.move(Direction.FORWARD);
+        assertThat(car.getPosition().location).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Car Back Move")
+    public void carBackMoveTest() {
+        Position position = new Position();
+        position.location = 1;
+        Car car = new Car("1", position);
+        car.move(Direction.BACK);
+        assertThat(car.getPosition().location).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Car Zero Back Move")
+    public void carZeroBackMoveTest() {
+        assertThatThrownBy(() -> {
+            Car car = new Car("1", new Position());
+            car.move(Direction.BACK);
+        }).isInstanceOf(IllegalStateException.class);
+    }
+}
