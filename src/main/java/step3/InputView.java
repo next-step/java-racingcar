654a8c3910db2a29695a86f4d52982d05745e0ec
yjs <yjs2952@naver.com>
2020-05-13 18:11:04 +0900

    feat: InputView 입력 UI를 담당하는 클래스 추가

---
+++ src/main/java/step3/InputView.java
@@ -0,0 +1,27 @@
+package step3;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private int numberOfCars;
+    private int moveCount;
+
+    public void input(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        numberOfCars = scanner.nextInt();
+
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        moveCount = scanner.nextInt();
+    }
+
+    public int getNumberOfCars() {
+        return numberOfCars;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+}
