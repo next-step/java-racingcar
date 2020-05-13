b9cbc500cf0cdb20a8e82a019a80ec4fbc568251
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-13 11:27:57 +0900

    Refactor StringCalculator

--- src/main/java/com/cheolhyeonpark/step2/StringCalculator.java
+++ src/main/java/com/cheolhyeonpark/step2/StringCalculator.java
@@ -1,34 +1,99 @@
 package com.cheolhyeonpark.step2;
 
-import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
+    public static final Map<String, BiFunction<Integer, Integer, Integer>> functions = new HashMap<>();
+
+    static  {
+        functions.put("+", (a , b) -> a + b);
+        functions.put("-", (a , b) -> a - b);
+        functions.put("*", (a , b) -> a * b);
+        functions.put("/", (a , b) -> {
+            if (b == 0) {
+                throw new IllegalArgumentException("Wrong input! Can not be divided by 0.");
+            }
+            return a / b;
+        });
+    }
+
     public int calculate(String input) {
-        if (input == null || input.replaceAll(" ", "").length() < 1) {
+        validateInput(input);
+        String expression = getExpression(input);
+        List<Integer> numbers = getNumbers(expression);
+        List<String> operators = getOperators(expression);
+        validateExpression(numbers, operators);
+        return getAnswer(numbers, operators);
+    }
+
+    private void validateInput(String input) {
+        if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException("There is no input. Please check your input data.");
         }
-        String expression = input.replaceAll(" ", "");
-        String[] numberStrings = expression.split("[\\+\\-\\*\\/]");
-        String[] operators = expression.replaceAll("[0-9]", "").split("");
+    }
 
-        int result = Integer.parseInt(numberStrings[0]);
-        for (int i = 0; i < operators.length; i++) {
+    private String getExpression(String input) {
+        return input.replaceAll(" ", "");
+    }
+
+    private List<Integer> getNumbers(String expression) {
+        String[] numberStrings = getNumberStrings(expression);
+        return Arrays.stream(numberStrings).map(this::getNumber).collect(Collectors.toList());
+    }
+
+    private String[] getNumberStrings(String expression) {
+        return expression.split("[+\\-*/]");
+    }
+
+    private Integer getNumber(String numberString) {
+        try {
+            return Integer.parseInt(numberString);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Wrong input! Only numbers and operators are allowed.");
+        }
+    }
+
+    private List<String> getOperators(String expression) {
+        return new ArrayList<>(Arrays.asList(getOperatorArray(expression)));
+    }
+
+    private String[] getOperatorArray(String expression) {
+        return expression.replaceAll("[0-9]", "").split("");
+    }
+
+    private void validateExpression(List<Integer> numbers, List<String> operators) {
+        if (!isRightExpression(numbers, operators)) {
+            throw new IllegalArgumentException("Wrong input! Please check your input data.");
+        }
+    }
+
+    private boolean isRightExpression(List<Integer> numbers, List<String> operators) {
+        return numbers.size() - operators.size() == 1;
+    }
+
+    private int getAnswer(List<Integer> numbers, List<String> operators) {
+        int result = numbers.get(0);
+        for (int i = 0; i < operators.size(); i++) {
             int a = result;
-            int b = Integer.parseInt(numberStrings[i + 1]);
-            if (operators[i].equals("+")) {
-                result = a + b;
-            }
-            if (operators[i].equals("-")) {
-                result = a - b;
-            }
-            if (operators[i].equals("*")) {
-                result = a * b;
-            }
-            if (operators[i].equals("/")) {
-                result = a / b;
-            }
+            int b = numbers.get(i + 1);
+            result = operate(operators.get(i), a, b);
         }
         return result;
+    }
+
+    private int operate(String operator, int a, int b) {
+        validateOperator(operator);
+        return functions.get(operator).apply(a, b);
+    }
+
+    private void validateOperator(String operator) {
+        if (!functions.containsKey(operator)) {
+            throw new IllegalArgumentException("Wrong input! Only numbers and operators are allowed.");
+        }
     }
 }
--- src/test/java/com/cheolhyeonpark/step2/StringCalculatorTest.java
+++ src/test/java/com/cheolhyeonpark/step2/StringCalculatorTest.java
@@ -14,7 +14,7 @@ class StringCalculatorTest {
 
     @ParameterizedTest
     @DisplayName("문자열로 이루어진 수식이 입력되면 정수 해답을 출력")
-    @CsvSource(value = {"2 + 3 * 4 / 2, 10", "5 * 5 * 5, 125", "5 - 10 * 4 / 2, -10"})
+    @CsvSource(value = {"2 + 3 * 4 / 2, 10", "5 * 5 * 5, 125", "5 - 10 * 4 / 2, -10", "5/5+1-2, 0", "11*10/11, 10"})
     public void calculateStringExpression(String input, int answer) {
         //given
         String expression = input;
@@ -41,6 +41,36 @@ class StringCalculatorTest {
     public void throwIllegalArgumentExceptionWhenInputIsBlank() {
         // given
         String expression = " ";
+
+        //when, then
+        assertThatIllegalArgumentException().isThrownBy(() -> stringCalculator.calculate(expression));
+    }
+
+    @Test
+    @DisplayName("0으로 나누면 예외를 출력")
+    public void throwIllegalArgumentExceptionWhenDividedByZero() {
+        // given
+        String expression = "1/0";
+
+        //when, then
+        assertThatIllegalArgumentException().isThrownBy(() -> stringCalculator.calculate(expression));
+    }
+
+    @Test
+    @DisplayName("사칙연산 연산자가 아니면 예외를 출력")
+    public void throwIllegalArgumentExceptionWhenUsedWrongOperator() {
+        // given
+        String expression = "5^5";
+
+        //when, then
+        assertThatIllegalArgumentException().isThrownBy(() -> stringCalculator.calculate(expression));
+    }
+
+    @Test
+    @DisplayName("숫자가 아니면 예외를 출력")
+    public void throwIllegalArgumentExceptionWhenUsedNotNumber() {
+        // given
+        String expression = "a*b";
 
         //when, then
         assertThatIllegalArgumentException().isThrownBy(() -> stringCalculator.calculate(expression));
