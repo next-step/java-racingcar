e043ae4ae3e1789c27a5e7c781f4cbec3f911752
rlapwl <khji1210@naver.com>
2020-05-15 00:43:19 +0900

    feat: RacingCar 객체 구현

---
+++ src/main/java/racingCar/Entity/RacingCar.java
@@ -0,0 +1,17 @@
+package racingCar.Entity;
+
+public class RacingCar {
+    private int position;
+
+    public RacingCar(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
