084dc0143ab6b70121bfb1672da02f9f5dce6292
yjs <yjs2952@naver.com>
2020-05-14 17:52:35 +0900

    refactor: 이중 for 문 메소드 추출

--- src/main/java/step3/view/ResultView.java
+++ src/main/java/step3/view/ResultView.java
@@ -8,18 +8,21 @@ public class ResultView {
         System.out.println("실행 결과");
 
         for (int[] positions : result) {
-
             printResult(positions);
+            System.out.println();
         }
     }
 
     private void printResult(int[] positions) {
         for (int position : positions) {
-            for (int i = 0; i < position; i++) {
-                System.out.print("-");
-            }
+            printPosition(position);
             System.out.println();
         }
-        System.out.println();
+    }
+
+    private void printPosition(int position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
     }
 }
