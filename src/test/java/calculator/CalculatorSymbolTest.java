66ac8d0430f9246ee2dea3ebd185022c8959afb2
Ho <doitluke@naver.com>
2020-05-15 03:02:43 +0900

    CalculatorSymbol class에 대한 테스트 추가

---
+++ src/test/java/calculator/CalculatorSymbolTest.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorSymbolTest {
+
+    private CalculatorSymbol calculatorSymbol;
+
+    @BeforeEach
+    void setUp() {
+        calculatorSymbol = new CalculatorSymbol();
+    }
+    @DisplayName("계산 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"+:6","-:2","/:2","*:8"}, delimiter = ':')
+    void findTypeAndCalculator(String input,int expected) {
+        int result = calculatorSymbol.findTypeAndCalculator(4, 2, input);
+        assertThat(result).isEqualTo(expected);
+    }
+}
