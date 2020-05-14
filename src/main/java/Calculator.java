3140799e0fc5e8a844247ff5b9ccd74d99b3555f
Jaehun Lee <ljhokgo@gmail.com>
2020-05-14 09:44:40 +0900

    [Step2] 리뷰 요청 드립니다. (#783)
    * [Step2]
     - 문자열 계산기 구현 (Calculator)
     - 문자열 계산기 테스트 구현 (CalculatorTest)
    
    * [Step2] 계산 초기 값 변경

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,59 @@
+public class Calculator {
+    public String calculate(String input) {
+        if(input == null || input.equalsIgnoreCase(""))
+            throw new IllegalArgumentException();
+
+        String[] splitInput = input.split(" ");
+
+        if(splitInput.length < 3 || splitInput.length % 2 != 1)
+            throw new IllegalArgumentException();
+
+        String result = splitInput[0];
+
+        for(int i=1; i<splitInput.length; i+=2)
+            result = _calculate(result, splitInput[i], splitInput[i+1]);
+
+        return result;
+    }
+
+    private String _calculate(String strNum1, String symbol, String strNum2) {
+        int result;
+        int num1 = Integer.parseInt(strNum1);
+        int num2 = Integer.parseInt(strNum2);
+
+        switch (symbol) {
+            case "+":
+                result = plus(num1, num2);
+                break;
+            case "-":
+                result = minus(num1, num2);
+                break;
+            case "*":
+                result = multiply(num1, num2);
+                break;
+            case "/":
+                result = divide(num1, num2);
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+
+        return String.valueOf(result);
+    }
+
+    private int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    private int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    private int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+    
+    private int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
