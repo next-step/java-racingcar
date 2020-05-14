4f8483dc080623f76bfb5896440f7889cd7cc24f
bluehoped <bluehoped@hanmail.net>
2020-05-14 13:21:27 +0900

    추가 SplitUtils

---
+++ src/main/java/calculator/utils/SplitUtils.java
@@ -0,0 +1,14 @@
+package calculator.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class SplitUtils {
+
+    public static String[] split(String target, String separator) {
+        if (StringUtils.isBlank(target)) {
+            throw new IllegalArgumentException("올바른 값이 아닙니다. [" + target + "]");
+        }
+
+        return target.split(separator);
+    }
+}
