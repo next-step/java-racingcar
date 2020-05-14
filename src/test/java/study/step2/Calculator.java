53e94aafce6c0183f80a56dfc45fb577fc1465f5
eunjeong <cs801458j@gmail.com>
2020-05-14 23:23:52 +0900

    계산기 클래스 간단하게 구현하기

---
+++ src/test/java/study/step2/Calculator.java
@@ -0,0 +1,52 @@
+package study.step2;
+
+import java.util.List;
+
+public class Calculator {
+
+    private String inputExpression;
+    //  먼저 계산기 본체 만들기 최대한 단순하게 만들자 생성자
+    public Calculator(String input){
+        this.inputExpression = input;
+    }
+
+    //  연산식으로 만들기
+    public void trimInputExpression(String targetExpression){
+        //  일단
+        String trimmedExpression = targetExpression.trim();
+        String operators[] = trimmedExpression.replaceAll("[^0-9]","").split("");  //  연산자 리스트
+        String operands[] = trimmedExpression.replaceAll("[0-9]","").split("");;       //  피연산자 리스트
+
+        //  계산
+        calculate(operators, operands);
+    }
+
+
+    //  람다식으로 만들어서 리스트에서 뽑아서 계산
+    //  고려 해야할 점 double로 만들기?
+
+    public double calculate(String[] operators, String[] operands){
+        double result = Double.parseDouble(operands[0]);
+
+        for(int i=0; i< operators.length; i++){
+            switch (operators[i]){
+                case "+":
+                    result += Double.parseDouble(operands[i+1]);
+                    break;
+                case "-":
+                    result -= Double.parseDouble(operands[i+1]);
+                    break;
+                case "*":
+                    result *= Double.parseDouble(operands[i+1]);
+                    break;
+                case "/":
+                    result /= Double.parseDouble(operands[i+1]);
+                default:
+                    break;
+            }
+        }
+
+        return result;
+    }
+
+}
