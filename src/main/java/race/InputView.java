f8b1743162b2920c60b2589c47884922bcf642d5
Kim yongjin <yongjin5184@gmail.com>
2020-05-15 08:52:05 +0900

    <refactor> InputView - print 매서드 추가, InputView() 에서 하던 일 getCountOfCar, getTime 으로 추출

--- src/main/java/race/InputView.java
+++ src/main/java/race/InputView.java
@@ -2,14 +2,27 @@ package race;
 
 import java.util.Scanner;
 
-public class InputView {
+public final class InputView {
 
-    public InputView() {
+    private InputView() {
+
+    }
+
+    public static RacingGame print() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        scanner.nextInt();
+        int count = getCountOfCar(scanner);
+        int time = getTime(scanner);
 
+        return RacingGame.of(count, time);
+    }
+
+    private static int getCountOfCar(Scanner scanner) {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return scanner.nextInt();
+    }
+
+    private static int getTime(Scanner scanner) {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        scanner.nextInt();
+        return scanner.nextInt();
     }
 }
