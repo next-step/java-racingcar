684203cc3b5ddc787c4a61ad51fcf7dd4f163c06
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-14 17:20:18 +0900

    refactor(InputView)
    - InputView에서 차량 대수가 아니라 운전자명 리스트로 입력을 관리하도록 변경

--- src/main/java/com/nextstep/racingcar/ui/InputView.java
+++ src/main/java/com/nextstep/racingcar/ui/InputView.java
@@ -1,30 +1,33 @@
 package com.nextstep.racingcar.ui;
 
+import com.nextstep.racingcar.util.DriverNameParser;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private int carNumber;
+    private List<String> driverNames;
     private int roundNumber;
 
-    public InputView(int carNumber, int roundNumber) {
-        this.carNumber = carNumber;
+    public InputView(List<String> driverNames, int roundNumber) {
+        this.driverNames = driverNames;
         this.roundNumber = roundNumber;
     }
 
     public static InputView create(Scanner scanner) {
-        System.out.println("자동차 대수는 몇 대 인가요");
-        int carNumber = scanner.nextInt();
+        System.out.println("참여 할 운전자들의 이름을 입력해주세요(공백 없이 쉼표로 구분)");
+        String driverNameString = scanner.nextLine();
         System.out.println("시도할 회수는 몇 회 인가요");
         int roundNumber = scanner.nextInt();
 
-        return new InputView(carNumber, roundNumber);
+        return new InputView(DriverNameParser.toUserNameList(driverNameString), roundNumber);
     }
 
-    public int getCarNumber() {
-        return carNumber;
+    public List<String> getDriverNames() {
+        return this.driverNames;
     }
 
     public int getRoundNumber() {
-        return roundNumber;
+        return this.roundNumber;
     }
 }
