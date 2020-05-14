d253b6aefa546fc4cd2c795fa93cdf5dd93bf096
bo0216 <bo216@naver.com>
2020-05-15 01:20:15 +0900

    StringCalculator 클래스 operator 메소드 추가, StringCalculatorTest의 공백 테스트 수정

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,27 +3,29 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
 
     private StringCalculator stringCalculator;
 
     @ParameterizedTest
-    @DisplayName("연산 테스트")
-    @CsvSource(value = {"2 + 3 * 4 / 2 = 10", "1 + 0 = 1", "1 - 2 = -1", "1 2 2"}, delimiter = '=')
-    void calculateTest(String value, String expected){
-        assertThat(stringCalculator.calculate(value)).isEqualTo(Integer.parseInt(expected));
+    @DisplayName("조건에 맞는 문자열 연산 테스트")
+    @CsvSource(value = {"2 + 3 * 4 / 2 = 10", "1 + 0 = 1", "1 - 2 = -1"}, delimiter = '=')
+    void calculateTest(String value, Double expected) {
+        assertThat(stringCalculator.calculate(value)).isEqualTo(expected);
     }
 
-//    @ParameterizedTest
-//    @DisplayName("입력값이 공백일 때 테스트")
-//    @ValueSource(strings = {""})
-//    void checkNullTest(String value){
-//        assertThatThrownBy(() -> stringCalculator.calculate(value))
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessage("공백입니다.");
-//
-//    }
+    @ParameterizedTest
+    @DisplayName("문자열이 공백일 때 테스트")
+    @NullSource
+    void checkNullTest(String input) {
+        assertThatThrownBy(() -> stringCalculator.checkNull(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("empty string");
+    }
 }
