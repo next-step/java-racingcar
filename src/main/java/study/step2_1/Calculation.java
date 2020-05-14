8313ae441774f4a401360f8ab009da4112d80cf9
bbbnam <bbbnam@naver.com>
2020-05-14 23:45:30 +0900

    refactor(step2) : Calculation.java 리팩토링

---
+++ src/main/java/study/step2_1/Calculation.java
@@ -0,0 +1,35 @@
+package study.step2_1;
+
+import java.util.List;
+
+public class Calculation implements Calculator{
+
+    private static final int INIT_INDEX = 0;
+    private static final int INCREASE_INDEX = 1;
+
+    private List<Double> numbers;
+    private List<String> operators;
+
+    public Calculation(List<Double> separateNumber, List<String> separateOperator) {
+        numbers = separateNumber;
+        operators = separateOperator;
+    }
+
+    public double calculate() {
+        double result = numbers.get(INIT_INDEX);
+        int size = operators.size() - 1;
+
+        for (int i = INIT_INDEX; i <= size; i++){
+            result = Operator.calculate(operators.get(i), result, numbers.get(INCREASE_INDEX));
+            numbers.removeIf(value -> isConsumeNumbers(value));
+            numbers.set(INIT_INDEX,result);
+        }
+
+        return result;
+    }
+
+    private boolean isConsumeNumbers(Double value) {
+        return value == numbers.get(INIT_INDEX);
+    }
+
+}
