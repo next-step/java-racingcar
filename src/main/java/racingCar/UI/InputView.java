49bf506b8eff96befbfa945c57f43baf074a4f36
rlapwl <khji1210@naver.com>
2020-05-15 00:40:16 +0900

    feat: InputView 구현

---
+++ src/main/java/racingCar/UI/InputView.java
@@ -0,0 +1,24 @@
+package racingCar.UI;
+
+import racingCar.Application.RacingGame;
+
+import java.util.Scanner;
+
+public class InputView {
+    public RacingGame setStartInput() {
+        Scanner scanner = new Scanner(System.in);
+        int carCount = getCarCount(scanner);
+        int time = getTime(scanner);
+        return new RacingGame(carCount, time);
+    }
+
+    public int getCarCount(Scanner scanner) {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return scanner.nextInt();
+    }
+
+    public int getTime(Scanner scanner) {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
+    }
+}
