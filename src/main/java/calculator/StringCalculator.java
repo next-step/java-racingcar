2be9922ebbfe956ca0fb29f57174d0c9373c0d87
jun-pil park <iamfeel2@gmail.com>
2020-05-14 10:12:49 +0900

    refactor (StringCalculator) : FormulaValidation 추가에 따른 변경 외
    1. FormulaValidation 추가에 따른 변경
    2. checkNullOrEmpty 리턴타입을 void에서 boolean 타입으로 변경 적용

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,12 +5,15 @@ public class StringCalculator {
     private String[] formulaElements;
 
     public StringCalculator(String formula) {
-        checkNullOrEmpty(formula);
-        formulaElements = formula.split(" ");
-        checkOrderFormula(formulaElements);
+        if(isNullOrEmpty(formula)) {
+            throw new IllegalArgumentException("string is null.");
+        }
+
+        String[] formulaBeforeChecking = formula.split(" ");
+        formulaElements = new FormulaValidation(formulaBeforeChecking).check();
     }
 
-    public float calculate() {
+    public float calculate() throws ArithmeticException {
         Operand result = new Operand(formulaElements[0]);
         for(int i = 1 ; i < formulaElements.length ; i += 2) {
             Operator operator = Operator.getOperator(formulaElements[i]);
@@ -19,36 +22,10 @@ public class StringCalculator {
         return result.getNumber();
     }
 
-    private void checkNullOrEmpty(String str) throws IllegalArgumentException {
-        if(str == null || str.isEmpty()) {
-            throw new IllegalArgumentException("Argument is null or empty.");
-        }
+    private boolean isNullOrEmpty(String str) {
+        return (str == null || str.isEmpty());
     }
 
-    private void checkOrderFormula(String[] formulaElements) throws IllegalArgumentException {
-        checkFormulaElementCount(formulaElements);
 
-        for(int i = 0 ; i < formulaElements.length ; i++) {
-            checkOrderOperand(i, formulaElements[i]);
-            checkOrderOperator(i, formulaElements[i]);
-        }
-    }
 
-    private void checkFormulaElementCount(String[] formulaElements) {
-        if(formulaElements.length % 2 == 0) {
-            throw new IllegalArgumentException("There are errors in the number of operators and operands.");
-        }
-    }
-
-    private void checkOrderOperand(int index, String element) {
-        if(index % 2 == 0 && !Operand.isOperand(element)) {
-            throw new IllegalArgumentException("The number must come at index " + index);
-        }
-    }
-
-    private void checkOrderOperator(int index, String element) {
-        if(index % 2 == 1 && !Operator.isOperatorSymbol(element)) {
-            throw new IllegalArgumentException("The operator must come at index " + index);
-        }
-    }
 }
