e23feb419eb13be22dcb10cbd276243038a2d5de
hanjae.jea <hanjae.jea@gmail.com>
2020-05-15 18:31:41 +0900

    feature add test

--- src/test/java/study/nextstep/stage3/RacingGameTest.java
+++ src/test/java/study/nextstep/stage3/RacingGameTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class RacingGameTest {
     @Test
     public void stdRacingGameTest(){
@@ -12,13 +14,35 @@ public class RacingGameTest {
     }
 
     @Test
-    public void stdRacingGameTestWithKeepRun(){
+    public void racingGameTestWithKeepRun(){
         RacingGame game = new RacingGame(3, 5).setRandomModule(new Random(){
             @Override
             public int nextInt(int bound) {
-                return 9;
+                return bound - 1;
             }
         });
         game.game();
+        for( int i = 0 ; i < game.gameStatus.size() ; i ++ ) {
+            assertThat(game.gameStatus.renderCarPostion(i)).isEqualTo(5);
+        }
+    }
+
+    @Test
+    public void racingGameTestWithFirstCarStopSecondCarRun() {
+        RacingGame game = new RacingGame(2, 4).setRandomModule(new Random(){
+            int status = 0;
+            @Override
+            public int nextInt(int bound) {
+                if( status == 0 ){
+                    status = 1;
+                    return 0;
+                }
+                status = 0;
+                return bound - 1;
+            }
+        });
+        game.gameStatus.turn();
+        assertThat(game.gameStatus.renderCarPostion(0)).isEqualTo(1);
+        assertThat(game.gameStatus.renderCarPostion(1)).isEqualTo(2);
     }
 }
