9551dd7cdf8f06d9a6b78a6a8a8ad098f6463bc3
yjs <yjs2952@naver.com>
2020-05-15 18:16:06 +0900

    refactor: 자동차 생성 메소드 정적 팩토리 패턴으로 변경

--- src/main/java/step3/car/CarFactory.java
+++ src/main/java/step3/car/CarFactory.java
@@ -5,29 +5,11 @@ import java.util.List;
 
 public class CarFactory {
 
-    private final int numberOfCars;
-
-
-    private CarFactory(int numberOfCars) {
-        validationCheck(numberOfCars);
-        this.numberOfCars = numberOfCars;
-    }
-
-    public static CarFactory of(int numberOfCars){
-        return new CarFactory(numberOfCars);
-    }
-
-    public List<Car> createCars(){
-        final  List<Car> cars = new ArrayList<>();
+    public static List<Car> createCars(int numberOfCars){
+        final List<Car> cars = new ArrayList<>();
         for (int i = 0; i < numberOfCars; i++) {
-            cars.add(Car.of());
+            cars.add(new Car());
         }
         return cars;
-    }
-
-    private void validationCheck(int numberOfCars){
-        if (numberOfCars < 0) {
-            throw new IllegalArgumentException("자동차 대수는 음수를 입력할 수 없습니다.");
-        }
     }
 }
