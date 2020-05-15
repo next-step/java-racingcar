9e3e2079efb625f0f934910cfa8ad0cb0820a80f
eunji <12@>
2020-05-16 00:42:09 +0900

    실행 결과를 출력하기 위한 클래스 추가

---
+++ src/main/java/step3/ResultView.java
@@ -0,0 +1,15 @@
+package step3;
+
+import java.util.List;
+
+public class ResultView {
+
+    ResultView() {
+        System.out.println("실행 결과");
+    }
+
+    public void printResult(List<Car> carList) {
+        for (Car car : carList)
+            System.out.println("-".repeat(car.getPosition()));
+    }
+}
