5d70725407e2bab4ad358f2a0c5fe6e2b13a9b64
Gobukgol <zxczc159@gmail.com>
2020-05-15 15:04:29 +0900

    feat(InputView, GameInfo): 자동차의 이름을 입력받도록 변경, GameInfo 에서 자동차의 이름을 가지고있도록 변경

--- src/main/java/racingcar/domain/game/GameInfo.java
+++ src/main/java/racingcar/domain/game/GameInfo.java
@@ -1,13 +1,23 @@
 package racingcar.domain.game;
 
+import java.util.Objects;
+
 public class GameInfo {
     private static final int GAME_INFO_NUMBER_BOUND = 0;
-    private final int numberOfCar;
+    private static final String DELIMITER = ",";
+    private int numberOfCar;
     private final int numberOfPhase;
+    private String[] nameOfCars;
 
     public GameInfo(int numberOfCar, int numberOfPhase) {
         validateInputValue(numberOfCar, numberOfPhase);
         this.numberOfCar = numberOfCar;
+        this.numberOfPhase = numberOfPhase;
+    }
+
+    public GameInfo(String nameOfCars, int numberOfPhase) {
+        validateInputValue(nameOfCars, numberOfPhase);
+        this.nameOfCars = nameOfCars.split(DELIMITER);
         this.numberOfPhase = numberOfPhase;
     }
 
@@ -19,9 +29,23 @@ public class GameInfo {
         return numberOfPhase;
     }
 
+    public String[] getNameOfCars() {
+        return nameOfCars;
+    }
+
     private void validateInputValue(int numberOfCar, int numberOfPhase) {
         if (numberOfCar <= GAME_INFO_NUMBER_BOUND) {
             throw new IllegalArgumentException("참여할 자동차의 대수는 0 보다 커야합니다.");
+        }
+
+        if (numberOfPhase <= GAME_INFO_NUMBER_BOUND) {
+            throw new IllegalArgumentException("시도할 횟수는 0 보다 커야합니다.");
+        }
+    }
+
+    private void validateInputValue(String nameOfCars, int numberOfPhase) {
+        if (Objects.isNull(nameOfCars) || nameOfCars.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
         }
 
         if (numberOfPhase <= GAME_INFO_NUMBER_BOUND) {
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,7 @@
 package racingcar.view;
 
+import racingcar.domain.game.GameInfo;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,6 +9,18 @@ public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {
+    }
+
+    public static GameInfo inputGameInfo() {
+        String nameOfCars = inputNameOfCars();
+        int numberOfPhase = inputNumberOfPhase();
+
+        return new GameInfo(nameOfCars, numberOfPhase);
+    }
+
+    private static String inputNameOfCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return SCANNER.nextLine();
     }
 
     public static int inputNumberOfCar() {
