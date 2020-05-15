9e3efdb6513f5cb0da6cdda82350700e8206b1d6
y00s22 <ryusuy22@gmail.com>
2020-05-15 21:38:33 +0900

    random 메소드 생성

--- src/main/java/mission1/racing/RacingGame.java
+++ src/main/java/mission1/racing/RacingGame.java
@@ -1,11 +1,13 @@
 package mission1.racing;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class RacingGame {
     private int time;
     private int carNumber;
     private int[] carPositions = {0, 0, 0};
+    private Random rand = new Random();
 
     private void setGame() {
         Scanner scanner = new Scanner(System.in);
@@ -14,6 +16,12 @@ public class RacingGame {
         carNumber = scanner.nextInt();
         System.out.println("시도할 회수는 몇 회 인가요?");
         time = scanner.nextInt();
+    }
+
+    private int random() {
+        int randomNum = rand.nextInt(10);
+
+        return randomNum;
     }
 
     public int[] move() {
