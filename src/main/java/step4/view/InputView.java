1060c1b8399b4c4cdf1bd04d79ef477c175620a5
yjs <yjs2952@naver.com>
2020-05-15 13:16:54 +0900

    feat: 입력 뷰 기능 구현

---
+++ src/main/java/step4/view/InputView.java
@@ -0,0 +1,31 @@
+package step4.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private String[] names;
+    private int moveCount;
+
+    public void input(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        names = splitNames(scanner.nextLine());
+
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        moveCount = scanner.nextInt();
+    }
+
+    private String[] splitNames(String input) {
+        return input.split(",");
+    }
+
+    public String[] getNames() {
+        return names;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+}
