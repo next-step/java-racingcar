a38ef95fcad556785e6c64ea6b55338e0eefd68a
eunji <12@>
2020-05-15 23:41:10 +0900

    값 입력받는 메서드 분리

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -1,16 +1,24 @@
 package step3;
 
 import java.util.Scanner;
+
 public class InputView {
+    Scanner scanner;
     private int carCount;
     private int repeatCount;
 
     public void initInput() {
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
+        askNumberCount();
+        askRepeatCount();
+    }
 
+    private void askNumberCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         carCount = scanner.nextInt();
+    }
 
+    private void askRepeatCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         repeatCount = scanner.nextInt();
     }
