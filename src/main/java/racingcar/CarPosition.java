514e06a9f506e2d7c039ac6bba8a1aa281725153
chulman <chlcjfals0122@gmail.com>
2020-05-16 00:38:43 +0900

    refactor (CarPosition) : CarPosition, CarPositionTest add

---
+++ src/main/java/racingcar/CarPosition.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+public class CarPosition {
+
+    public static int DEFAULT_POSITION = 0;
+    private int position;
+
+    private CarPosition(int position) {
+        validate(position);
+        this.position = position;
+    }
+
+    public static CarPosition of(int position) {
+        return new CarPosition(position);
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    private void validate(int position) {
+        if (position < DEFAULT_POSITION) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/racingcar/CarPositionTest.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CarPositionTest {
+
+    @ValueSource(ints = {1, 2, 3})
+    @ParameterizedTest
+    @DisplayName("CarPosition 생성 테스트")
+    void generateCarPositionTest(int position) {
+        assertThatCode(() -> CarPosition.of(position)).doesNotThrowAnyException();
+    }
+
+    @ValueSource(ints = {-1, -2, -3})
+    @ParameterizedTest
+    @DisplayName("CarPosition 생성 예외 테스트")
+    void generateCarPositionExceptionTest(int position) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> CarPosition.of(position));
+    }
+
+}
