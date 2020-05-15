e0da1ef7f95b18a87c044d66080bee17a9271a5d
yjs <yjs2952@naver.com>
2020-05-15 19:56:02 +0900

    docs: 문서 기능 추가

--- src/test/java/step4/car/CarTest.java
+++ src/test/java/step4/car/CarTest.java
@@ -1,19 +1,15 @@
 package step4.car;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CarTest {
 
-    Car car1;
-    Car car2;
-    Car car3;
-
-    @BeforeEach
-    public void setUp(){
-        car1 = Car.of("car1");
-        car2 = Car.of("car2");
-        car3 = Car.of("car3");
+    @DisplayName("생성된 값이 4보다 큰 경우에만 position 이 증가한다.")
+    @ParameterizedTest
+    @CsvSource({"car1,1,0", "car2,4,1", "car3,5,1"})
+    public void whenGeneratedNumberGreaterThan4ThenAddPositionTest(String name, int input, int expected){
+        Car car = Car.of(name);
     }
-
-
 }
