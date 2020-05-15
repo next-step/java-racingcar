5547961d58a5f3fa64805b69a5a4909ef5eecb2f
eunji <12@>
2020-05-16 01:33:47 +0900

    출력 부분 빈 라인 추가

--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -5,11 +5,12 @@ import java.util.List;
 public class ResultView {
 
     ResultView() {
-        System.out.println("실행 결과");
+        System.out.println("\n실행 결과");
     }
 
     public void print(List<Car> carList) {
         for (Car car : carList)
             System.out.println("-".repeat(car.getPosition()));
+        System.out.println();
     }
 }
