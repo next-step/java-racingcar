5fb8c5ee711b7b51d4a3aaa04f8807867db8f8fe
kwonheejeong <gmlwjd9405@naver.com>
2020-05-16 02:03:14 +0900

    test: RacingTest 공통 사용 객체를 필드로 선언

--- src/test/java/racingcar/domain/RacingTest.java
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,22 +9,27 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 public class RacingTest {
 
+    private String carNames;
+    private Round round;
+
+    @BeforeEach
+    void setUp() {
+        carNames = "name1,name2";
+        round = Round.of(1);
+    }
+
     @DisplayName("Racing 생성")
     @Test
     void create() {
-        String[] carNames = new String[]{"name1", "name2"};
-
-        assertThatCode(() -> Racing.of(Cars.of(carNames), Round.of(1)))
+        assertThatCode(() -> Racing.of(Cars.of(carNames), round))
                 .doesNotThrowAnyException();
     }
 
     @DisplayName("경주 실패: 전략이 null 인 경우")
     @Test
     void run() {
-        String[] carNames = new String[]{"name1", "name2"};
         Cars cars = Cars.of(carNames);
-
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> Racing.of(cars, Round.of(1)).run(null));
+                .isThrownBy(() -> Racing.of(cars, round).run(null));
     }
 }
