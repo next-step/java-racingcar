778c72a76ea542ea708bad8483985692a1288bff
kwonheejeong <gmlwjd9405@naver.com>
2020-05-15 20:32:17 +0900

    test: Cars 로 클래스를 분리함에 따른 테스트 코드 수정

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -2,57 +2,28 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.moving.RandomMovingStrategy;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class RacingTest {
 
-    private List<String[]> carNames = Arrays.asList(
-            new String[] { "", "name" },
-            new String[] { },
-            new String[] { null, "name" },
-            new String[] { "name1", "name2", "name3" }
-    );
-
-    @DisplayName("Racing 생성: Car 의 이름 배열을 생성자의 인자로 받아 생성")
+    @DisplayName("Racing 생성")
     @Test
     void create() {
-        assertThatCode(() -> Racing.of(carNames.get(3)))
+        String[] carNames = new String[]{"name1", "name2"};
+
+        assertThatCode(() -> Racing.of(Cars.of(carNames)))
                 .doesNotThrowAnyException();
     }
 
-    @DisplayName("Racing 생성 실패: name 이 빈 문자열이나 null 인 경우 예외 발생")
+    @DisplayName("경주 실패: 전략이 null 인 경우")
     @Test
-    void createByThrown() {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> Racing.of(carNames.get(0)));
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> Racing.of(carNames.get(1)));
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> Racing.of(carNames.get(2)));
-    }
+    void run() {
+        String[] carNames = new String[]{"name1", "name2"};
+        Cars cars = Cars.of(carNames);
 
-    @DisplayName("run 실패: Car 의 이동 전략이 null 인 경우 IllegalArgumentException 발생")
-    @Test
-    void failureRunByStrategyIsNull() {
-        Racing racing = Racing.of(carNames.get(3));
-
-        assertThatThrownBy(() -> racing.run(null))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("게임을 한 번 수행할 때마다 만들어진 Car 는 이동 또는 정지")
-    @Test
-    void move() {
-        Racing racing = Racing.of(carNames.get(3));
-        racing.run(new RandomMovingStrategy());
-
-        assertThat(racing.getCars()
-                .stream()
-                .map(Car::getPosition)).containsAnyOf(Car.DEFAULT_DISTANCE, Car.DEFAULT_DISTANCE + 1);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Racing.of(cars).run(null));
     }
 }
