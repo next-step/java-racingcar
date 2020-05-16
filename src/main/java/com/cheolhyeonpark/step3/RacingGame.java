60d58e44e1c063ee99aa52a5db61ef1153b60dec
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-16 16:12:51 +0900

    Feat: add generateStage method

--- src/main/java/com/cheolhyeonpark/step3/RacingGame.java
+++ src/main/java/com/cheolhyeonpark/step3/RacingGame.java
@@ -6,6 +6,12 @@ import java.util.stream.IntStream;
 
 public class RacingGame {
 
+    private Stage stage;
+
+    private void generateStage(int numberOfCars) {
+        this.stage = new Stage(new Dice(), getCars(numberOfCars));
+    }
+
     private List<Car> getCars(int numberOfCars) {
         return IntStream.range(0, numberOfCars).mapToObj(i -> new Car()).collect(Collectors.toList());
     }
