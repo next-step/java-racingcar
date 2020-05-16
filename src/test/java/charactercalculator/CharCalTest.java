491849a1c6ee45d75e9f6ba7d7d01acbc3d644d6
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-16 18:02:48 +0900

    test: check thrown IllegalArgumentException test
    - 영으로_나눠지지_않는다() method added
    - 사칙연산_외의_기호는_오류가_처리된다() method added
    - EMPTY는_오류가_처리된다() method added

--- src/test/java/charactercalculator/CharCalTest.java
+++ src/test/java/charactercalculator/CharCalTest.java
@@ -1,11 +1,13 @@
 package charactercalculator;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import chactercalculator.Calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
 
 @DisplayName("2단계 문자열 계산기 테스트")
 public class CharCalTest {
@@ -25,5 +27,32 @@ public class CharCalTest {
   @CsvSource({"'2 + 3 * 4 / 2', 10", "'3 + 3 * 4 / 2', 12", "'2 * 9 * 2 / 3 + 3', 15"})
   void 복수_연산자로_계산된다(String given, Integer expected) {
     assertThat(calculator.calculate(given)).isEqualTo(expected);
+  }
+
+  @DisplayName("0으로 나눌 수 없는 지 확인하는 테스트")
+  @ParameterizedTest
+  @CsvSource({"'3 + 3 / 0", "2 / 0", "7 * 7 / 0"})
+  void 영으로_나눠지지_않는다(String given) {
+    assertThatIllegalArgumentException().isThrownBy(() -> {
+      calculator.calculate(given);
+    });
+  }
+
+  @DisplayName("사칙연산 기호가 아니면 IllegalArgumentException을 반환한다")
+  @ParameterizedTest
+  @CsvSource({"'2 # 3 * 4 / 2', 10", "'3 ! 3 * 4 / 2', 12", "'2 @ 9 * 2 / 3 & 3', 15"})
+  void 사칙연산_외의_기호는_오류가_처리된다(String given) {
+    assertThatIllegalArgumentException().isThrownBy(() -> {
+      calculator.calculate(given);
+    });
+  }
+
+  @DisplayName("입력값이 null 혹은 공백이면 IllegalArgumentException을 반환한다")
+  @ParameterizedTest(name = "입력값 : {arguments}")
+  @EmptySource
+  void EMPTY는_오류가_처리된다(String given) {
+    assertThatIllegalArgumentException().isThrownBy(() -> {
+      calculator.calculate(given);
+    });
   }
 }
