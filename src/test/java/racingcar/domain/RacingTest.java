b1fc0619c19259cffa8cd6b5fccd942ff2df8ea6
kwonheejeong <gmlwjd9405@naver.com>
2020-05-16 11:12:05 +0900

    test: Racing 테스트 코드 리팩토링

--- src/test/java/racingcar/domain/RacingTest.java
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -9,26 +9,25 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 public class RacingTest {
 
-    private String carNames;
+    private Cars cars;
     private Round round;
 
     @BeforeEach
     void setUp() {
-        carNames = "name1,name2";
+        cars = Cars.of("name1,name2");
         round = Round.of(1);
     }
 
     @DisplayName("Racing 생성")
     @Test
     void create() {
-        assertThatCode(() -> Racing.of(Cars.of(carNames), round))
+        assertThatCode(() -> Racing.of(cars, round))
                 .doesNotThrowAnyException();
     }
 
     @DisplayName("경주 실패: 전략이 null 인 경우")
     @Test
-    void run() {
-        Cars cars = Cars.of(carNames);
+    void failureRun() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> Racing.of(cars, round).run(null));
     }
