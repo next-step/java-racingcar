7445a381883a5dda599e2f1d40c1908063df88fe
bluehoped <bluehoped@hanmail.net>
2020-05-16 21:49:12 +0900

    수정 split utils 위치 변경

--- src/main/java/calculator/SplitUtils.java
+++ src/main/java/calculator/SplitUtils.java
@@ -1,4 +1,4 @@
-package calculator.utils;
+package calculator;
 
 import org.apache.commons.lang3.StringUtils;
 
--- src/test/java/calculator/SplitUtilsTest.java
+++ src/test/java/calculator/SplitUtilsTest.java
@@ -1,6 +1,5 @@
-package calculator.utlis;
+package calculator;
 
-import calculator.utils.SplitUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
