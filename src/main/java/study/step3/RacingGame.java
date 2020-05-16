3d6c9f3eb1f77e401518c75e005f609fc0d2d288
bbbnam <bbbnam@naver.com>
2020-05-16 11:18:18 +0900

    feat : RacingGame 클래스 생성

--- src/main/java/study/step3/RacingGame.java
+++ src/main/java/study/step3/RacingGame.java
@@ -2,64 +2,60 @@ package study.step3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class RacingGame {
+    private final static int INIT_INDEX = 0;
+    private final static int FIRST_DISTINCTION_NUMBER = 1;
+    private final static Pattern PATTERN = Pattern.compile("^[0-9]+$");
+    private final static String ILLEGALARGUMENT_EXCEPTION_MASSEAGE = "올바르지 않은 값을 입력했습니다.";
+
     private int time= 0;
     private int carNumbers = 0;
-    private int round = 0;
-    private List<Round> roundInfos;
-
+    private List<Car> carList;
 
     private RacingGame(int time, int carNumbers) {
         this.time = time;
         this.carNumbers = carNumbers;
+        carList = new ArrayList<>();
     }
 
-    public int showEnterCarNumbers(){
-        return this.carNumbers;
-    }
-
-    public int showTryTimes(){
-        return this.time;
-    }
-
-    public Round showCarPositions(int round) {
-        return this.roundInfos.get(round);
-    }
-
-    public List<Car> move(int round){
-        this.round = round;
-        List<Car> result = new ArrayList<>();
-        result.add(new Car());
-
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "RacingGame{" +
-                "time=" + time +
-                ", carNumbers=" + carNumbers +
-                ", roundInfos=" + roundInfos.get(round) +
-                '}';
+    public List<Car> gameStart() {
+        for (int i = INIT_INDEX; i < carNumbers; i++){
+            carList.add(new Car(String.valueOf(i + FIRST_DISTINCTION_NUMBER)).move(time));
+        }
+        return carList;
     }
 
     public static class RacingGameBuilder{
-        private int time= 0;
-        private int carNumbers = 0;
+        private int time= INIT_INDEX;
+        private int carNumbers = INIT_INDEX;
 
-        public RacingGameBuilder setCarNumbers(int carNumbers) {
-            this.carNumbers = carNumbers;
+        public RacingGameBuilder setCarNumbers(String carNumbers) {
+            this.carNumbers = inputValidate(carNumbers);
             return this;
         }
 
-        public RacingGameBuilder setTime(int time){
-            this.time = time;
+        public RacingGameBuilder setTime(String time){
+            this.time = inputValidate(time);
             return this;
+        }
+
+        private int inputValidate(String nextInt) {
+            if(isNumber(nextInt)){
+                return Integer.parseInt(nextInt);
+            }
+            throw  new IllegalArgumentException(ILLEGALARGUMENT_EXCEPTION_MASSEAGE);
+        }
+
+        private boolean isNumber(String nextInt) {
+            return !Objects.isNull(nextInt) && !nextInt.isEmpty() && PATTERN.matcher(nextInt).matches();
         }
 
         public RacingGame build(){
             return new RacingGame(time,carNumbers);
         }
+
     }
 }
