bf95c9e60a5289bdf81af52d05680a47bd267d79
iljun <sumusb@naver.com>
2020-05-16 19:49:23 +0900

    [refactor] RacingGameApplication

--- src/main/java/racing/RacingGameApplication.java
+++ src/main/java/racing/RacingGameApplication.java
@@ -1,14 +1,12 @@
 package racing;
 
-import racing.domain.RacingGame;
-import racing.domain.RandomMovement;
 import racing.dto.RaceInformation;
 import racing.dto.RacingGameResult;
+import racing.service.RacingGameService;
 import racing.view.InputView;
 import racing.view.OutputView;
 
 import java.util.List;
-import java.util.Random;
 
 public class RacingGameApplication {
 
@@ -16,19 +14,12 @@ public class RacingGameApplication {
         InputView inputView = new InputView();
         RaceInformation raceInformation = inputView.createByUserInput();
 
-        RacingGame racingGame = new RacingGame(raceInformation, new RandomMovement(new Random()));
+        RacingGameService racingGameService = new RacingGameService();
+        List<RacingGameResult> racingGameResults = racingGameService.executeRace(raceInformation);
 
         OutputView outPutView = new OutputView();
         outPutView.printGuidance();
-
-        List<RacingGameResult> racingGameResults = null;
-        for (int i = 0; i < raceInformation.getTotalRacingCount(); i++) {
-            racingGame.executeRacing();
-            racingGameResults = racingGame.calculateRacingGameResults();
-            outPutView.printRacingResult(racingGameResults);
-        }
-
-        int maxPosition = racingGame.calculateMaxPosition();
-        outPutView.printRacingWinners(racingGameResults, maxPosition);
+        outPutView.printRacingResult(racingGameResults);
+        outPutView.printRacingWinners(racingGameResults);
     }
 }
