c96ea2fe0e1977214c00e1e256f7a3e7afe9b631
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-16 16:09:49 +0900

    Test: add generateStage test

--- src/test/java/com/cheolhyeonpark/step3/RacingGameTest.java
+++ src/test/java/com/cheolhyeonpark/step3/RacingGameTest.java
@@ -3,6 +3,7 @@ package com.cheolhyeonpark.step3;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -12,6 +13,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RacingGameTest {
 
     RacingGame racingGame = new RacingGame();
+
+    @Test
+    @DisplayName("스테이지 객체를 생성한다")
+    public void generateStage() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+        //given
+        Method method = racingGame.getClass().getDeclaredMethod("generateStage", int.class);
+        method.setAccessible(true);
+        Field field = racingGame.getClass().getDeclaredField("stage");
+        field.setAccessible(true);
+
+        //when
+        method.invoke(racingGame, 3);
+
+        //then
+        assertThat(field.getType()).isEqualTo(Stage.class);
+    }
 
     @Test
     @DisplayName("입렫받은 차의 대수 만큼 Car객체를 생성해야한다")
