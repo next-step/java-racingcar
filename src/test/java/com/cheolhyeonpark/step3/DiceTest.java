f48f7063d64edbfecf744496d46ae49cd6d2300a
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-16 13:57:25 +0900

    Feat: rolling the dice

---
+++ src/test/java/com/cheolhyeonpark/step3/DiceTest.java
@@ -0,0 +1,24 @@
+package com.cheolhyeonpark.step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DiceTest {
+
+    Dice dice = new Dice();
+
+    @Test
+    @DisplayName("주사위를 굴리면 0 ~ 9 까지 숫자가 반환된다")
+    public void returnNumberAfterRollDice() {
+        //given
+        int diceNumber;
+
+        //when
+        diceNumber = dice.rollDice();
+
+        //then
+        assertThat(diceNumber).isBetween(0, 9);
+    }
+}
