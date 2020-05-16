7036655a090ff3eda8fcc37edf6207e8b408a39f
bbbnam <bbbnam@naver.com>
2020-05-16 11:17:02 +0900

    feat : InputView 클래스 생성(입력 UI)

--- src/main/java/study/step3/InputView.java
+++ src/main/java/study/step3/InputView.java
@@ -1,40 +1,30 @@
 package study.step3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputView {
     private final static String CAR_INPUT_QUESTION ="자동차 대수는 몇 대 인가요?";
     private final static String TRY_TIME_INPUT_QUESTION ="시도할 회수는 몇 회 인가요?";
-    private final static  Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner;
 
-    public InputView() {
-
-        //InputLogic.scanUserInputValue();
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
     }
 
-    public static int inputCarNumber(){
+    public String inputCarNumber(){
         System.out.println(CAR_INPUT_QUESTION);
-        return scanner.nextInt();
+        return scanner.nextLine();
     }
 
-    public static int inputTryTime() {
+    public String inputTryTime() {
         System.out.println(TRY_TIME_INPUT_QUESTION);
-        return scanner.nextInt();
+        return scanner.nextLine();
     }
-
-    public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame.RacingGameBuilder()
-                                              .setCarNumbers(inputCarNumber())
-                                              .setTime(inputTryTime())
-                                              .build();
-
-        for (int i = 0; i < racingGame.showTryTimes(); i++){
-            racingGame.move(i+1);
-        }
-
-        System.out.println(racingGame.toString());
-
-    }
-
 
 }
