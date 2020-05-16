5629d8912250bae4ed5a2e9d503c3601ec8f2679
eunji <12@>
2020-05-16 19:54:47 +0900

    Car Class move메서드 테스트코드 추가

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-    @Test
-    @DisplayName("값이 +1된다")
-    void move() {
 
+    @Test
+    @DisplayName("호출시 값을 +1 한다")
+    void move() {
+        int given = 1;
+        given++;
+        assertThat(given).isEqualTo(2);
     }
 }
