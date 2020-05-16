c0ef54ea8839b33b5d3a3c4655160f103a75712c
bluehoped <bluehoped@hanmail.net>
2020-05-16 21:49:36 +0900

    추가 FourOperation (enum), 테스트

---
+++ src/main/java/calculator/FourOperation.java
@@ -0,0 +1,30 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.function.IntBinaryOperator;
+
+public enum  FourOperation {
+    PLUS("+" , (a , b) -> a + b),
+
+    MINUS("-" , (a , b) -> a - b),
+
+    TIMES("*" , (a , b) -> a * b),
+
+    DIVIDE("/" , (a , b) -> a / b);
+
+    private final String operator;
+    private final IntBinaryOperator op;
+
+    FourOperation(String operator, IntBinaryOperator op) {
+        this.operator = operator;
+        this.op = op;
+    }
+
+    public static FourOperation valueOfOperator(String operator) {
+        return Arrays.stream(values()).filter(fourOperation -> fourOperation.operator.equals(operator)).findFirst().orElseThrow(IllegalArgumentException::new);
+    }
+
+    public int apply(int a, int b) {
+        return op.applyAsInt(a, b);
+    }
+}
---
+++ src/test/java/calculator/FourOperationTest.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+
+class FourOperationTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"+:2", "-:0", "*:1", "/:1"}, delimiter = ':')
+    @DisplayName("1, 1로 사칙 연산 결과 비교 테스트")
+    void operatorTest(String input, int expected) {
+        assertThat(FourOperation.valueOfOperator(input).apply(1, 1)).isEqualTo(expected);
+
+    }
+
+    @Test
+    @DisplayName("사칙연산이 아닌 부호 테스트")
+    void operatorFaultTest() {
+        assertThatIllegalArgumentException().isThrownBy(() ->FourOperation.valueOfOperator("^"));
+    }
+
+}
