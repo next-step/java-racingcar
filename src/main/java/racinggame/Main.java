fc9eb4aa4409cbdfe776e986d65048614bde9695
JuWanHan <baekseongsa@naver.com>
2020-05-16 19:36:37 +0900

    refactor(Main): 자동자 경주 실행 로직 변경

--- src/main/java/racinggame/Main.java
+++ src/main/java/racinggame/Main.java
@@ -1,14 +1,23 @@
 package racinggame;
 
+import racinggame.car.RandomMovableStrategy;
+import racinggame.game.RacingGame;
+import racinggame.view.InputView;
+import racinggame.view.ResultView;
+
 public class Main {
     public static void main(String[] args) {
-        int carCount = InputView.enterCarCount();
+        String[] carNames = InputView.enterCarNames();
         int round = InputView.enterRound();
 
-        RacingGame racingGame = new RacingGame(carCount, round, new RandomMovableStrategy());
-        racingGame.start();
+        RacingGame racingGame = new RacingGame(carNames, new RandomMovableStrategy());
 
-        ResultView resultView = new ResultView(racingGame.getResultCarPositions());
-        resultView.show();
+        ResultView.showTitle();
+
+        for (int i = 0; i < round; i++) {
+            ResultView.showCarPositions(racingGame.start());
+        }
+
+        ResultView.showWinnerMessage(racingGame.getWinnerCarNames());
     }
 }
