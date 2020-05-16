18895ede6f525b994ccaa7774f3e59154b31412a
eunji <12@>
2020-05-16 09:50:16 +0900

    print() static메서드로 변경- 인스턴스 생성없이 사용

--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class ResultView {
 
-    ResultView() {
+    private ResultView() {
         System.out.println("\n실행 결과");
     }
 
-    public void print(List<Car> carList) {
+    public static void print(List<Car> carList) {
         for (Car car : carList)
             System.out.println("-".repeat(car.getPosition()));
         System.out.println();
