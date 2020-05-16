1aa45956f7bbc924e00ce1593cf1fbd920af238c
kwonheejeong <gmlwjd9405@naver.com>
2020-05-16 10:36:02 +0900

    refactor: StringBuilder 동시성 문제를 대비한 init 메서드 제거

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -14,46 +14,35 @@ public class ResultView {
     private static final String WINNER_NAME_SEPARATOR = ", ";
     private static final String WINNER_MESSAGE = " 가 최종 우승했습니다.";
 
-    private static StringBuilder stringBuilder;
-
-    private static void initStringBuilder() {
-        stringBuilder = new StringBuilder();
-    }
-
     public static void printResultMessage() {
         System.out.println(NEW_LINE + RESULT_MESSAGE);
     }
 
     public static void printResult(final List<Car> cars) {
-        initStringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
         cars.forEach(car -> {
-            printName(car);
-            printPosition(car);
+            stringBuilder.append(getCarNameWithSuffix(car));
+            stringBuilder.append(getCarFinalPosition(car));
         });
         System.out.println(stringBuilder);
     }
 
-    private static void printName(final Car car) {
-        stringBuilder.append(car.getName())
-                .append(SUFFIX_OF_CAR_NAME);
+    private static String getCarNameWithSuffix(final Car car) {
+        return car.getName() + SUFFIX_OF_CAR_NAME;
     }
 
-    private static void printPosition(final Car car) {
+    private static String getCarFinalPosition(final Car car) {
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < car.getPosition(); i++) {
             stringBuilder.append(POSITION_MARK);
         }
-        addNewLine();
-    }
-
-    private static void addNewLine() {
         stringBuilder.append(NEW_LINE);
+        return stringBuilder.toString();
     }
 
     public static void printWinner(final List<Car> winnerCars) {
-        initStringBuilder();
-        stringBuilder.append(getNamesOfWinnerCars(winnerCars))
-                .append(WINNER_MESSAGE);
-        System.out.println(stringBuilder);
+        String winner = getNamesOfWinnerCars(winnerCars) + WINNER_MESSAGE;
+        System.out.println(winner);
     }
 
     private static String getNamesOfWinnerCars(final List<Car> winnerCars) {
