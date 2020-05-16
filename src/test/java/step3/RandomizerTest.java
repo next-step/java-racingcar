947884f71787a20fd834f8edb474be2208326c10
eunji <12@>
2020-05-16 20:15:44 +0900

    README.md에 궁금한 점 추가

--- src/test/java/step3/RandomizerTest.java
+++ src/test/java/step3/RandomizerTest.java
@@ -3,7 +3,6 @@ package step3;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RandomizerTest {
 
