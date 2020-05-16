2f2fe9428c0afcee252859cc0ac9128540993434
kwonheejeong <gmlwjd9405@naver.com>
2020-05-16 10:14:59 +0900

    test: 우승한 Cars 이름 배열 반환 테스트 코드 수정

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -15,14 +15,14 @@ import static org.assertj.core.api.Assertions.*;
 public class CarsTest {
 
     private enum CASE {
-        INCLUDED_EMPTY_VALUE, NULL_VALUE, THREE_VALUES, TWO_VALUES
+        INCLUDED_EMPTY_VALUE, NULL_VALUE, THREE_VALUES, FOUR_VALUES
     }
     private static final Map<CASE, String> carNames = new HashMap<>();
     static {
         carNames.put(CASE.INCLUDED_EMPTY_VALUE, ",name");
         carNames.put(CASE.NULL_VALUE, null);
         carNames.put(CASE.THREE_VALUES, "name1,name2,name3");
-        carNames.put(CASE.TWO_VALUES, "name1,name2");
+        carNames.put(CASE.FOUR_VALUES, "name1,name2,name3,name4");
     }
 
     @DisplayName("Cars 생성: Car 의 이름 배열을 생성자의 인자로 받아 생성")
@@ -55,21 +55,25 @@ public class CarsTest {
     @DisplayName("우승한(가장 멀리간) Cars 의 이름 배열 반환")
     @Test
     void getWinnerCars() {
-        Cars cars = Cars.of(carNames.get(CASE.THREE_VALUES));
-        cars.moveCars(new SuccessMovingStrategy());
+        Cars cars = Cars.of(carNames.get(CASE.FOUR_VALUES));
+        cars.moveCars(new EvenCarSuccessMovingStrategy());
 
         List<String> winnerNames = cars.getWinnerCars()
                 .stream()
                 .map(Car::getName)
                 .collect(Collectors.toList());
 
-        assertThat(winnerNames).containsExactly("name1", "name2", "name3");
+        assertThat(winnerNames).containsExactly("name2", "name4");
     }
 
-    public static class SuccessMovingStrategy implements MovingStrategy {
+    public static class EvenCarSuccessMovingStrategy implements MovingStrategy {
+
+        private int value;
+
         @Override
         public boolean isMovable() {
-            return true;
+            value++;
+            return value % 2 == 0;
         }
     }
 }
