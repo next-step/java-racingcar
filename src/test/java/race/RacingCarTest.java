5f171efec7ccee95383b7b1e8e0c7a7c02f309ff
Kim yongjin <yongjin5184@gmail.com>
2020-05-16 22:44:18 +0900

    <feature> Test 코드 추가, forward, stop, 생성 테스트 추가

---
+++ src/test/java/race/RacingCarTest.java
@@ -0,0 +1,33 @@
+package race;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RacingCarTest {
+
+    private RacingCar racingCar;
+
+    @BeforeEach
+    void setUp() {
+        racingCar = RacingCar.ready();
+    }
+
+    @DisplayName("forward 메소드 테스트")
+    @Test
+    public void forward() {
+        assertThat(racingCar.forward()).isEqualTo(1);
+    }
+
+    @DisplayName("Stop 메소드 테스트")
+    @Test
+    public void stop() {
+        assertThat(racingCar.stop()).isEqualTo(0);
+
+        racingCar.forward();
+
+        assertThat(racingCar.stop()).isEqualTo(1);
+    }
+}
---
+++ src/test/java/race/RacingGameTest.java
@@ -0,0 +1,19 @@
+package race;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RacingGameTest {
+    public static final int DEFAULT_TIME = 3;
+
+    @DisplayName("자동차 생성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5})
+    void initRacingCarTest(int racingCarCount) {
+        RacingGame racingGame = RacingGame.of(racingCarCount, DEFAULT_TIME);
+        assertThat(racingGame.move().length).isEqualTo(racingCarCount);
+    }
+}
