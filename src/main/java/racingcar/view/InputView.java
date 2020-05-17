dc327265c7cb15237c311ae94072fc4dcde2b64c
Sungjun <sungjunpizz@gmail.com>
2020-05-17 20:35:21 +0900

    feat(InputView): 자동차 이름을 입력 받도록 수정
    - 기존 자동차 대수 입력에서 쉼표를 구분으로 자동차 이름을 입력하도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,28 +3,28 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
-    private final int carCount;
+    private final String carNames;
     private final int moveCount;
 
-    public InputView(int carCount, int moveCount) {
-        this.carCount = carCount;
+    public InputView(String carNames, int moveCount) {
+        this.carNames = carNames;
         this.moveCount = moveCount;
     }
 
     public static InputView create() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int carCount = scanner.nextInt();
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String carNames = scanner.nextLine();
 
         System.out.println("시도할 회수는 몇 회 인가요?");
         int moveCount = scanner.nextInt();
 
-        return new InputView(carCount, moveCount);
+        return new InputView(carNames, moveCount);
     }
 
-    public int getCarCount() {
-        return carCount;
+    public String getCarNames() {
+        return carNames;
     }
 
     public int getMoveCount() {
