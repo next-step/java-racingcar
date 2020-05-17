9cfa9a7ebf25d4fb5cd436e985ef169bb2ca79b4
junilHwang <tjsdlf4261@naver.com>
2020-05-17 18:47:28 +0900

    [Step3 - 자동차 경주] test: MoveStrategyTest 작성 완료

--- src/test/java/step3/MoveStrategyTest.java
+++ src/test/java/step3/MoveStrategyTest.java
@@ -11,12 +11,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoveStrategyTest {
 
-  private static MoveStrategy moveStrategy = new RandomNumberMoveStrategy();
+  private static MoveStrategy moveStrategy = RandomNumberMoveStrategy.of();
 
   @DisplayName("전진 조건 테스트: Random에 대한 입력값이 4 이상일 때 전진")
   @ParameterizedTest
   @MethodSource("provideNumberForIsMoved")
-  void 전진_조건_테스트 (int number, boolean expected) {
+  void 전진_조건_테스트 (int number, int expected) {
     assertThat(moveStrategy.isMoved(number)).isEqualTo(expected);
   }
 
