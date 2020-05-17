273fe577bb93596a501b7d8465af3a5be7c9e6f5
junilHwang <tjsdlf4261@naver.com>
2020-05-17 21:28:32 +0900

    [Step3 - 자동차 경주] refactor: RacingGame 기능 추가

--- src/main/java/step3/RandomNumberMoveStrategy.java
+++ src/main/java/step3/RandomNumberMoveStrategy.java
@@ -6,11 +6,11 @@ public class RandomNumberMoveStrategy implements MoveStrategy {
 
   private final Random RANDOM;
 
-  private RandomNumberMoveStrategy() {
+  public RandomNumberMoveStrategy() {
     this(new Random());
   }
 
-  private RandomNumberMoveStrategy(final Random RANDOM) {
+  public RandomNumberMoveStrategy(final Random RANDOM) {
     this.RANDOM = RANDOM;
   }
 
@@ -22,9 +22,5 @@ public class RandomNumberMoveStrategy implements MoveStrategy {
   @Override
   public boolean isMoved (int number) {
     return number >= 4;
-  }
-
-  public static MoveStrategy of () {
-    return new RandomNumberMoveStrategy();
   }
 }
--- src/main/java/step3/view/ResultView.java
+++ src/main/java/step3/view/ResultView.java
@@ -1,21 +1,23 @@
 package step3.view;
 
-import step3.Cars;
+import step3.Car;
 
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ResultView {
 
   private ResultView() {};
 
-  public void print (Cars cars) {
-    System.out.println(
-      cars.stream()
-          .map(car -> car.getPositionString())
-          .collect(Collectors.joining("\n"))
-    );
+  public static void printResultText () {
+    System.out.println("실행 결과");
   }
 
-  public static ResultView of () { return new ResultView(); }
+  public static void printRace (Stream<Car> stream) {
+    System.out.println(
+      stream.map(car -> car.getPositionString()).collect(Collectors.joining("\n"))
+      + "\n"
+    );
+  }
 
 }
