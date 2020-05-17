b6ee515a136dc9f55a9f1646ec60afa8a5fe87be
iljun <sumusb@naver.com>
2020-05-17 12:03:45 +0900

    [feature] add EmptyCheckUtils test

---
+++ src/test/java/racing/utils/EmptyCheckUtilTest.java
@@ -0,0 +1,51 @@
+package racing.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class EmptyCheckUtilTest {
+
+    @DisplayName("null String 객체 테스")
+    @Test
+    void emptyCheckUtilsTestByNullString() {
+        String argument = null;
+        assertThatThrownBy(() -> EmptyCheckUtil.emptyCheck(argument))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("비어있는 String 객체 테스트")
+    @Test
+    void emptyCheckUtilsTestByEmptyString() {
+        assertThatThrownBy(() -> EmptyCheckUtil.emptyCheck(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("null Object 객체 테스트")
+    @Test
+    void emptyCheckUtilsTestByNullObject() {
+        Object argument = null;
+        assertThatThrownBy(() -> EmptyCheckUtil.emptyCheck(argument))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("null List 객체 테스트")
+    @Test
+    void emptyCheckUtilsTestByNullList() {
+        List argument = null;
+        assertThatThrownBy(() -> EmptyCheckUtil.emptyCheck(argument))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("비어있는 List 객체 테스트")
+    @Test
+    void emptyCheckUtilsTestByEmptyList() {
+        List argument = new ArrayList();
+        assertThatThrownBy(() -> EmptyCheckUtil.emptyCheck(argument))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
