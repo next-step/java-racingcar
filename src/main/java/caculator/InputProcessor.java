e4971cea03dba8bc18971ed2114b8e6f1495586e
wyh951753 <wyh951753@gmail.com>
2020-05-17 17:48:23 +0900

    Calculator 입력값 예외 처리 추가

---
+++ src/main/java/caculator/InputProcessor.java
@@ -0,0 +1,36 @@
+package caculator;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputProcessor {
+    private static final String SPLIT_TOKEN = " ";
+
+    public String[] processInput(final String input) {
+        validateNullOrEmpty(input);
+
+        String[] splitInput = input.split(SPLIT_TOKEN);
+        validateNumber(splitInput);
+        validateOperator(splitInput);
+        return splitInput;
+    }
+
+    private void validateNullOrEmpty(final String input) {
+        if (StringUtils.isBlank(input)) throw new IllegalArgumentException();
+    }
+
+    private void validateNumber(String[] splitInput) {
+        for(int i = 0; i < splitInput.length; i += 2) {
+            if (Double.isNaN(Double.parseDouble(splitInput[i]))) throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateOperator(String[] splitInput) {
+        List<String> operatorList = Arrays.asList("+", "-", "*", "/");
+        for(int i = 1; i < splitInput.length; i += 2) {
+            if (!operatorList.contains(splitInput[i])) throw new IllegalArgumentException();
+        }
+    }
+}
