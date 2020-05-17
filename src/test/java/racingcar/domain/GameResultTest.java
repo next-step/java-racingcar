8c67d26ace107533f0c0999bdf7446f912cc940c
Sungjun <sungjunpizz@gmail.com>
2020-05-17 20:46:54 +0900

    test(GameResultTest): 자동차 생성자 수정, 테스트 결과 수정

--- src/test/java/racingcar/domain/GameResultTest.java
+++ src/test/java/racingcar/domain/GameResultTest.java
@@ -10,16 +10,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("GameResult 클래스 테스트")
 public class GameResultTest {
+    private final String carName = "테스터";
 
     @Test
     @DisplayName("getResult 메소드는 자동차의 위치를 문자로 변환 후 리턴한다.")
     void getResult() {
         // given
-        Car car = new Car();
+        Car car = new Car(carName);
         int moveNumber = 5;
         car.move(moveNumber);
 
-        List<Car> cars = Arrays.asList(car, new Car());
+        List<Car> cars = Arrays.asList(car, new Car(carName));
         GameResult gameResult = new GameResult(cars);
 
         // when
@@ -28,6 +29,6 @@ public class GameResultTest {
         // then
         assertThat(actual).isNotNull();
         assertThat(actual).hasSize(cars.size());
-        assertThat(actual.get(0)).isEqualTo("-");
+        assertThat(actual.get(0)).isEqualTo(carName + " : -");
     }
 }
