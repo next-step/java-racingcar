9b4e9c9ded60309aee25cb0bc6895fa4e500a777
junilHwang <tjsdlf4261@naver.com>
2020-05-17 17:35:10 +0900

    [Step3 - 자동차 경주] refactor: Cars Class 생성

--- src/main/java/step3/Cars.java
+++ src/main/java/step3/Cars.java
@@ -1,7 +1,5 @@
 package step3;
 
-import step3.view.ResultView;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,32 +7,36 @@ import java.util.stream.Stream;
 
 public class Cars {
   private List<Car> cars;
+  private MoveStrategy moveStrategy;
 
-  private Cars (List<Car> cars) {
+  private Cars (List<Car> cars, MoveStrategy moveStrategy) {
     this.cars = cars;
+    this.moveStrategy = moveStrategy;
   }
 
-  public Cars move (MoveStrategy moveStrategy) {
-    this.cars.stream()
-             .filter(car -> moveStrategy.isMoved())
-             .forEach(car -> car.move());
-    return this;
+  public void move () {
+    this.cars.forEach(car -> {
+      if (moveStrategy.isMoved())
+        car.move();
+    });
   }
 
   public Stream<Car> stream () {
     return this.cars.stream();
   }
 
-  public static Cars of (int carCount) {
+  public static Cars of (int carCount, MoveStrategy moveStrategy) {
     return new Cars(
       Arrays.stream(new int[carCount])
-        .mapToObj(v -> Car.of())
-        .collect(Collectors.toList())
+        .boxed()
+        .map(v -> Car.of())
+        .collect(Collectors.toList()),
+      moveStrategy
     );
   }
 
-  public static Cars of (List<Car> cars) {
-    return new Cars(cars);
+  public static Cars of (List<Car> cars, MoveStrategy moveStrategy) {
+    return new Cars(cars, moveStrategy);
   }
 
 }
