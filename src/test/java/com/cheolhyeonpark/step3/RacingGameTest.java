709b7e79729a5da5d896f4569c528a69e324ac3a
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-17 21:34:09 +0900

    test: remove create stage test

--- src/test/java/com/cheolhyeonpark/step3/RacingGameTest.java
+++ src/test/java/com/cheolhyeonpark/step3/RacingGameTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,22 +21,8 @@ class RacingGameTest {
     }
 
     @Test
-    @DisplayName("스테이지 객체를 생성한다")
-    public void generateStage() throws NoSuchFieldException {
-        //given
-        RacingGame racingGame = new RacingGame(3, 5);
-
-        //given
-        Field field = racingGame.getClass().getDeclaredField("stage");
-        field.setAccessible(true);
-
-        //then
-        assertThat(field.getType()).isEqualTo(Stage.class);
-    }
-
-    @Test
     @DisplayName("입렫받은 차의 대수 만큼 Car객체를 생성해야한다")
-    public void getCars() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void getCars() {
         //given
         int numberOfCars = 3;
         RacingGame racingGame = new RacingGame(numberOfCars, 0);
@@ -51,7 +35,7 @@ class RacingGameTest {
     }
 
     @Test
-    @DisplayName("입력받은만큼 스테이지가 실행된다")
+    @DisplayName("스테이지 실행 확인")
     public void runStage() {
         //given
         RacingGame racingGame = spy(new RacingGame(3, 5));
@@ -64,7 +48,7 @@ class RacingGameTest {
     }
 
     @Test
-    @DisplayName("스테이지가 남아있는지 검사한다.")
+    @DisplayName("스테이지가 남아있는지 검사")
     public void checkStageLeft() {
         //given
         RacingGame racingGame = new RacingGame(3, 1);
