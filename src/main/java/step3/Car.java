fe95d1d593dd9938edad87eb1458e09a4512bae2
eunji <12@>
2020-05-17 22:11:24 +0900

    연료를 주입받는 방식으로 move메서드 변경
    전진 여부를 판단하는 checkPass() 메서드를 Car클래스로 이동

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -1,17 +1,39 @@
 package step3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
+    private static List<Car> carList;
+    private final int WIN_CONDITION = 4;
     private int position;
 
-    Car() {
+    public Car() {
         position = 1;
     }
 
-    public int move() {
-        return position++;
+    public static void generate(int carCount) {
+        carList = new ArrayList<>();
+        for (int i = 0; i < carCount; i++)
+            carList.add(new Car());
+    }
+
+    public static List<Car> getCarList() {
+        return carList;
+    }
+
+    public int move(int fuel) {
+        if (checkPass(fuel))
+            this.position = this.position + 1;
+        return this.position;
     }
 
     public int getPosition() {
         return position;
     }
+
+    boolean checkPass(int number) {
+        return number >= WIN_CONDITION;
+    }
+
 }
