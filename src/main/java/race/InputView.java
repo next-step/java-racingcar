e942d40c571aab4c4a99e21308b13c48ff22ba41
Kim yongjin <yongjin5184@gmail.com>
2020-05-17 22:53:00 +0900

    <refactor> InputView 는 값을 받는 역할만 하도록 변경

--- src/main/java/race/InputView.java
+++ src/main/java/race/InputView.java
@@ -4,24 +4,19 @@ import java.util.Scanner;
 
 public final class InputView {
 
-    private InputView() {
+    private final static Scanner scanner = new Scanner(System.in);
+    private InputView() {}
 
+    public static InputView of() {
+        return new InputView();
     }
 
-    public static RacingGame print() {
-        Scanner scanner = new Scanner(System.in);
-        int count = getCountOfCar(scanner);
-        int time = getTime(scanner);
-
-        return RacingGame.of(count, time);
-    }
-
-    private static int getCountOfCar(Scanner scanner) {
+    public int getCountOfCar() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         return scanner.nextInt();
     }
 
-    private static int getTime(Scanner scanner) {
+    public int getTime() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         return scanner.nextInt();
     }
