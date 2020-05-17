aa9cac7c690648d44cf656389f47d456a4f21b60
eunji <12@>
2020-05-17 22:15:09 +0900

    getter사용 제거 및 입력을  static메서드로 변경
    Scanner 생성 부분을 메서드마다 처리해줘야하는지/static메서드만 존재하는 클래스의 생성자에서 세팅해줘야하는지 궁금/고민

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -3,31 +3,22 @@ package step3;
 import java.util.Scanner;
 
 public class InputView {
-    Scanner scanner;
-    private int carCount;
-    private int repeatCount;
+    private static Scanner scanner;
+    private InputView() {
+    }
 
-    public void initInput() {
+    public static int askNumberCount() {
         scanner = new Scanner(System.in);
-        askNumberCount();
-        askRepeatCount();
-    }
-
-    private void askNumberCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        carCount = scanner.nextInt();
-    }
-
-    private void askRepeatCount() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        repeatCount = scanner.nextInt();
-    }
-
-    public int getCarCount() {
+        int carCount = scanner.nextInt();
         return carCount;
     }
 
-    public int getRepeatCount() {
+    public static int askRepeatCount() {
+        scanner = new Scanner(System.in);
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int repeatCount = scanner.nextInt();
         return repeatCount;
     }
+
 }
