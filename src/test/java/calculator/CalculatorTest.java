f0718a9dbc4105dd9cce0e08f3c5badef3eeb5ff
wyh951753 <wyh951753@gmail.com>
2020-05-17 17:44:52 +0900

    Caculator Teat 코드 추가

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,74 @@
+package calculator;
+
+import caculator.Calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    @DisplayName("덧셈을 테스트")
+    void testAddition() {
+        double actual = calculator.calculate("5 + 4");
+        assertThat(actual).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("뺄셈을 테스트")
+    void testSubtraction() {
+        double actual = calculator.calculate("5 - 2");
+        assertThat(actual).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("곱셈을 테스트")
+    void testMultiply() {
+        double actual = calculator.calculate("5 * 4");
+        assertThat(actual).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("나눗셈을 테스트")
+    void testDivision() {
+        double actual = calculator.calculate("60 / 5");
+        assertThat(actual).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("모든 사칙 연산 테스트")
+    void testAllOperator() {
+        double actual = calculator.calculate("10 / 5 + 7 - 2 * 3");
+        assertThat(actual).isEqualTo(21);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("입력 값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException")
+    void testExpectedExceptionNullOrEmpty(String input) {
+        assertThatThrownBy(() -> {
+            calculator.calculate(input);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("사칙 연산 기호가 아닌 경우 IllegalArgumentException")
+    void testExpectedExceptionArithmeticOperation() {
+        assertThatThrownBy(() -> {
+            calculator.calculate("3 + 4 & 7");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 경우 IllegalArgumentException")
+    void testExpectedExceptionNumber() {
+        assertThatThrownBy(() -> {
+            calculator.calculate("& 5 4 & 7");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
