b98774716a6521073abb10862d1f64da0a827bfd
junilHwang <tjsdlf4261@naver.com>
2020-05-17 18:56:45 +0900

    [Step3 - 자동차 경주] test: CarTest 작성 완료

---
+++ src/test/java/step3/CarsTest.java
@@ -0,0 +1,19 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class CarsTest {
+}
