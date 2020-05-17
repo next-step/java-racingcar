2dcf2ac908f88fcfdd46724e7198bb7f2832f35c
JuWanHan <baekseongsa@naver.com>
2020-05-17 17:24:30 +0900

    refactor(RacingGame): 이동 전략 결정 및 변경 가능하도록 수정

--- src/main/java/racinggame/game/RacingGame.java
+++ src/main/java/racinggame/game/RacingGame.java
@@ -2,13 +2,14 @@ package racinggame.game;
 
 import racinggame.car.MovableStrategy;
 import racinggame.car.RacingCars;
+import racinggame.car.RandomMovableStrategy;
 
 public class RacingGame {
 
     private RacingCars racingCars;
 
-    public RacingGame(String[] carNames, MovableStrategy movableStrategy) {
-        this.racingCars = RacingCars.newInstance(carNames, movableStrategy);
+    public RacingGame(String[] carNames) {
+        this.racingCars = RacingCars.newInstance(carNames, new RandomMovableStrategy());
     }
 
     public RacingCars start() {
@@ -18,5 +19,9 @@ public class RacingGame {
 
     public String getWinnerCarNames() {
         return this.racingCars.getWinnerCarNames();
+    }
+
+    public void changeMovableStrategy(MovableStrategy movableStrategy) {
+        this.racingCars.changeMovableStrategy(movableStrategy);
     }
 }
