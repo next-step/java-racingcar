17cd88271d6bca6af72c77c9a230a0d38dd502be
y00s22 <ryusuy22@gmail.com>
2020-05-17 11:45:15 +0900

    전진 조건 완성. 게임 횟수만큼 이동

--- src/main/java/mission1/racing/RacingGame.java
+++ src/main/java/mission1/racing/RacingGame.java
@@ -9,11 +9,34 @@ public class RacingGame {
     private int[] carPositions = {0, 0, 0};
     private Random rand = new Random();
 
+    public void startGame() {
+        setGame();
+
+        for(int i = 0; i < time; i++) {
+            move();
+        }
+    }
+
+    public void move() {
+        for(int i = 0; i < carPositions.length; i++) {
+            carPositions[i] += moveCondition();
+        }
+    }
+
+    private int moveCondition() {
+        if (random() >= 4) {
+            return 1;
+        }
+
+        return 0;
+    }
+
     private void setGame() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("자동차 대수는 몇 대 인가요?");
         carNumber = scanner.nextInt();
+
         System.out.println("시도할 회수는 몇 회 인가요?");
         time = scanner.nextInt();
     }
@@ -22,10 +45,5 @@ public class RacingGame {
         int randomNum = rand.nextInt(10);
 
         return randomNum;
-    }
-
-    public int[] move() {
-        setGame();
-        return carPositions;
     }
 }
