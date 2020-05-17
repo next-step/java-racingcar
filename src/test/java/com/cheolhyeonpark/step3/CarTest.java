8e3e2000e7e64cdbfa276d517ff16928d9c0abc2
cheolhyeonpark <cheolhyeonpark90@gmail.com>
2020-05-17 21:15:41 +0900

    test: add move test

--- src/test/java/com/cheolhyeonpark/step3/CarTest.java
+++ src/test/java/com/cheolhyeonpark/step3/CarTest.java
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CarTest {
 
     @Test
-    @DisplayName("move 메서드를 실행하면 car의 position이 1 증가한다")
-    void move() {
+    @DisplayName("true값을 매개변수로 move 메서드를 실행하면 car의 position이 1 증가한다")
+    void moveWhenParameterIsTrue() {
         //given
         Car car = new Car();
 
@@ -18,5 +18,18 @@ class CarTest {
 
         //then
         assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("false값을 매개변수로 move 메서드를 실행하면 car의 position이 증가하지 않는다")
+    void moveWhenParameterIsFalse() {
+        //given
+        Car car = new Car();
+
+        //when
+        car.move(false);
+
+        //then
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
