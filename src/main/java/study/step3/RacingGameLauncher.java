d4fc5bfed1614f36c2b088609b4c08cb29a1bcc9
bbbnam <bbbnam@naver.com>
2020-05-18 19:59:24 +0900

    refactor : RacingGameLauncher 클래스 refactoring

--- src/main/java/study/step3/RacingGameLauncher.java
+++ src/main/java/study/step3/RacingGameLauncher.java
@@ -1,22 +1,40 @@
 package study.step3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class RacingGameLauncher {
-    private final static Scanner scanner = new Scanner(System.in);
+    private final static int INIT_ZERO = 0;
 
-    public static void main(String[] args) {
-        InputView inputView = new InputView(scanner);
-
-        RacingGame racingGame = new RacingGame.RacingGameBuilder()
-                .setCarNumbers(inputView.inputCarNumber())
-                .setTime(inputView.inputTryTime())
-                .build();
-
-        List<Car> result = racingGame.gameStart();
-        ResultView resultView = new ResultView(result);
-        resultView.print();
+    private static List<Round> gameStartAllrounds(RacingGame racingGame, RandomCarMoveCondition moveCondition) {
+        List<Round> rounds = new ArrayList<>();
+        Round round = new Round(racingGame);
+        for (int i = INIT_ZERO; i < racingGame.getTryTime(); i++) {
+            if (i == INIT_ZERO){
+                rounds.add(round);
+                continue;
+            }
+            round = perRoundGame(round, moveCondition);
+            rounds.add(round);
+        }
+        return rounds;
     }
 
+    private static Round perRoundGame(Round round, RandomCarMoveCondition moveCondition) {
+        List<Car> cars = new ArrayList<>();
+        for (Car car : round.getCars()){
+            cars.add(car.move(moveCondition));
+        }
+        return new Round(cars);
+    }
+
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame(InputView.inputTryTime(), InputView.inputCarNumber());
+
+        List<Round> rounds = gameStartAllrounds(racingGame, new RandomCarMoveCondition());
+
+        ResultView resultView = new ResultView(rounds);
+        resultView.print();
+    }
 }
