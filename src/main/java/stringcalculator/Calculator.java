701417fa936148aaa942e752cfa218f57d870cfb
eunjeong <cs801458j@gmail.com>
2020-05-18 22:07:07 +0900

    Enum 내부클래스로 변경. 함수형 인터페이스 사용

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -1,52 +1,46 @@
 package stringcalculator;
 
 import java.util.ArrayList;
-
-//  enum 만들기
-enum Operators{
-    PLUS {
-        public double calculate(double operand1, double operand2){
-            return operand1 + operand2;
-        }
-    },
-    MINUS {
-        public double calculate(double operand1, double operand2){
-            return operand1 - operand2;
-        }
-    },
-    MULTIPLE {
-        public double calculate(double operand1, double operand2){
-            return operand1 * operand2;
-        }
-    },
-    DIVIDE {
-        public double calculate(double operand1, double operand2){
-            return operand1 / operand2;
-        }
-    };
-
-    public abstract double calculate(double operand1, double operand2);
-}
+import java.util.function.BiFunction;
 
 public class Calculator {
 
-    //  연산식으로 만들기
-    public double trimInputExpression(String targetExpression){
-        //  일단
-        String trimmedExpression = targetExpression.replace(" ", "");
-        String operands[] = trimmedExpression.replaceAll("[^0-9]","").split("");  //  연산자 리스트
-        String operators[] = trimmedExpression.replaceAll("[0-9]", "").split("");       //  피연산자 리스트
+    enum Operator{
+        PLUS("+", (operand1, operand2) -> operand1 + operand2),
+        MINUS("-", (operand1, operand2) -> operand1 - operand2),
+        MULTIPLE("*", (operand1, operand2) -> operand1 * operand2),
+        DIVIDE("-", (operand1, operand2) -> operand1 / operand2);
 
-        //  연산자와 피연산자 홀짝으로 구분해보기
-        //  계산
-        return calculate(operators, operands);
+        private BiFunction<Double, Double, Double> mathExpression;
+        private String operator;
+
+        //  생성자
+        Operator(String operator, BiFunction<Double, Double, Double> mathExpression) {
+            this.operator = operator;
+            this.mathExpression = mathExpression;
+        }
+
+        //  계산기능 구현
+        public double calculate(double operand1, double operand2) {
+            return this.mathExpression.apply(operand1, operand2);
+        }
+    }
+
+
+    //  연산식으로 만들기
+    public String trimInputExpression(String targetExpression){
+        return targetExpression.replace(" ", "");
     }
 
     //  람다식으로 만들어서 리스트에서 뽑아서 계산
     //  고려 해야할 점 double로 만들기?
 
     public double calculate(String[] operators, String[] operands){
+        //String operands[] = trimmedExpression.replaceAll("[^0-9]","").split("");  //  연산자 리스트
+        //String operators[] = trimmedExpression.replaceAll("[0-9]", "").split("");       //  피연산자 리스트
+
         double result = Double.parseDouble(operands[0]);
+
 
         for(int i=0; i< operators.length; i++){
             switch (operators[i]){
