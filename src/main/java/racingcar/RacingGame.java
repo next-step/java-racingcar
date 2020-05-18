4a64e370ba0673f6dda0c7c6146ef2ba0eda850e
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-19 00:44:28 +0900

    feat: add playGame() logic that controllers InputView and GameController

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,19 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import view.InputView;
+
 public class RacingGame {
 
-  
+  public static void main(String[] args) {
+    playGame();
+  }
+
+  private static void playGame() {
+    int carNum = InputView.getUserInput(InputView.carInputMessage);
+    int attemptNum = InputView.getUserInput(InputView.attemptInputMessage);
+
+    GameController gameController = GameController.create(carNum, attemptNum);
+    gameController.proceedGame();
+  }
 }
