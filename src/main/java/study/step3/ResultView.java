00a936655f5b64d5f9e94b78ef00becc3bc2db87
bbbnam <bbbnam@naver.com>
2020-05-18 19:59:38 +0900

    refactor : ResultView클래스 refactoring

--- src/main/java/study/step3/ResultView.java
+++ src/main/java/study/step3/ResultView.java
@@ -4,22 +4,12 @@ import java.util.List;
 
 public class ResultView {
     private static final String UNDER_BAR_SYMBOL ="_";
-    private static final int INIT_INDEX = 0;
+    private static final int INIT_ZERO = 0;
 
-    private final int rounds;
-    private List<Car> results;
+    private List<Round> rounds;
 
-    public ResultView(List<Car> result) {
-        this.results = result;
-        this.rounds = result.get(INIT_INDEX).rounds();
-    }
-
-    private String convertToUnderBarSymbol(Integer pointInTimeCarPostion) {
-        StringBuffer stringBuffer = new StringBuffer(UNDER_BAR_SYMBOL);
-        for (int i = INIT_INDEX; i < pointInTimeCarPostion; i++){
-            stringBuffer.append(UNDER_BAR_SYMBOL);
-        }
-        return stringBuffer.toString();
+    public ResultView(List<Round> rounds) {
+        this.rounds = rounds;
     }
 
     public void print() {
@@ -27,16 +17,23 @@ public class ResultView {
     }
 
     private void roundToString() {
-        for (int i = INIT_INDEX; i < rounds; i++){
-            carStatusToString(i);
+        for (Round round : rounds){
+            carToString(round.getCars());
             System.out.println();
         }
     }
 
-    private void carStatusToString(int i) {
-        for (Car car : results){
-            System.out.println(car.carNameToString()+convertToUnderBarSymbol(car.pointInTimeCarPostion(i)));
+    private void carToString(List<Car> cars) {
+        for (Car car: cars){
+            System.out.println(convertToUnderBarSymbol(car.getCarPosition()));
         }
     }
 
+   private String convertToUnderBarSymbol(int pointInTimeCarPostion) {
+        StringBuffer stringBuffer = new StringBuffer(UNDER_BAR_SYMBOL);
+        for (int i = INIT_ZERO; i < pointInTimeCarPostion; i++){
+            stringBuffer.append(UNDER_BAR_SYMBOL);
+        }
+        return stringBuffer.toString();
+    }
 }
