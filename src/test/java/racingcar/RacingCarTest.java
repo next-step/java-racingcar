f2bdf502ffe57033a2a211ff75095303b6ac069b
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-18 10:00:19 +0900

    test: check random value is in between 0, 9

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +10,8 @@ public class RacingCarTest {
 
   @Test
   void 랜덤값이_0과_9_사이이다() {
-    //
+    RandomUtil randomUtil = new RandomUtil();
+    assertThat(randomUtil.getRandomValue()).isBetween(0, 9);
   }
 
   @Test
