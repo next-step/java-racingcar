cb754eaefee9009a12037086cbd7b5424d9aae5a
Kim yongjin <yongjin5184@gmail.com>
2020-05-18 22:25:49 +0900

    Step3 자동차 경주 (#825)
    * <docs> 기능 요구 사항 추가
    
    * <feat> InputView 추가 (scanner 를 통해, 자동차 대수와 시도할 횟수를 받는 기능)
    
    * <feat> RacingGame 구현 - 정적 팩토리 매서드, move 매서드 구현
    
    * <refactor> InputView - print 매서드 추가, InputView() 에서 하던 일 getCountOfCar, getTime 으로 추출
    
    * <feature> isMoveForward, readyRacingCars 메서드 추가
    
    * <feature> OupPutView 클래스 추가
    
    * <feature> RacingCar Class 추가
    
    * <feature> Main Class 에서 OutputView Class 를 통해 print 하도록 처리
    
    * <feature> Test 코드 추가, forward, stop, 생성 테스트 추가
    
    * <feature> 상수 private 으로 변경
    
    * <refactor> 상수/메소드명 변경, for -> stream 으로 변경, 메소드 추출
    
    * <refactor> move 메소드 return 값 변경, racingCars 초기화 함수 추가
    
    * <refactor> 테스트 코드 변경
    
    * <refactor> InputView 는 값을 받는 역할만 하도록 변경
    
    * <refactor> getPosition 추가
    
    * <refactor> OutputView 에서는 출력만 하도록 로직 변경
    
    * <refactor> RacingCar 의 start 메소드를 테스트 하기 위해 DummyMovingStrategy class 생성 Test Double
    
    * <refactor> hasSize 메소드 사용
    
    * <refactor> 코드리뷰 변경 사항 수정
    
    * <refactor> 동일 참조 방지를 위해 새로운 변수에 할당하여 리턴, Stub 객체를 통한 테스트 코드 추가
    
    * <refactor> List 선언 변경
    
    * <refactor> 동일 참조 방지를 위해 새로운 객체 만들어서 리턴

---
+++ src/test/java/race/RacingGameTest.java
@@ -0,0 +1,56 @@
+package race;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RacingGameTest {
+
+    private static final int DEFAULT_TIME = 3;
+
+    @DisplayName("자동차 게임 생성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5})
+    void initRacingCarTest(int racingCarCount) {
+        RacingGame racingGame = RacingGame.create(DEFAULT_TIME, new RandomMovingStrategy());
+        racingGame.readyRacingCars(racingCarCount);
+    }
+
+    @DisplayName("자동차의 start 메소드 테스트")
+    @ParameterizedTest
+    @MethodSource("startRacingGame")
+    void startRacingGame(int racingCarCount, int time, boolean isMovable, int expected) {
+        //given
+        RacingGame racingGame = RacingGame.create(time, new StubMovingStrategyTest(isMovable));
+        racingGame.readyRacingCars(racingCarCount);
+
+        //when
+        List<RacingCar> racingCars = new ArrayList<>();
+        for (int i = 0; i < time; i++) {
+            racingCars = racingGame.startRacing();
+        }
+
+        //then
+        for (int i = 0; i < time; i++) {
+            for (RacingCar racingCar : racingCars) {
+                assertThat(racingCar.currentPosition()).isEqualTo(expected);
+            }
+        }
+    }
+
+    static Stream<Arguments> startRacingGame() {
+        return Stream.of(
+            Arguments.of(3, 4, true, 4),
+            Arguments.of(3, 4, false, 0)
+        );
+    }
+}
