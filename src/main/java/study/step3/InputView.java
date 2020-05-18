12622b43e2265734e4340d511bae5ad54d4f705d
bbbnam <bbbnam@naver.com>
2020-05-18 19:58:37 +0900

    refactor : InputView 클래스 refactoring

--- src/main/java/study/step3/InputView.java
+++ src/main/java/study/step3/InputView.java
@@ -1,30 +1,38 @@
 package study.step3;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class InputView {
-    private final static String CAR_INPUT_QUESTION ="자동차 대수는 몇 대 인가요?";
-    private final static String TRY_TIME_INPUT_QUESTION ="시도할 회수는 몇 회 인가요?";
-    private final Scanner scanner;
+    private final static String CAR_TEXT ="자동차";
+    private final static String TRY_TIME_TEXT ="시도";
+    private final static String CAR_INPUT_QUESTION =" 대수는 몇 대 인가요?";
+    private final static String TRY_TIME_INPUT_QUESTION =" 회수는 몇 회 인가요?";
+    private final static String DELIMITER =":";
+    private final static String ILLEGALARGUMENT_EXCEPTION_MASSEAGE = "올바르지 않은 값을 입력했습니다.";
+    private final static String ILLEGALARGUMENT_VALIDATE_MASSEAGE = "보다는 큰 값을 입력해야 합니다.";
+    private final static Scanner scanner = new Scanner(System.in);
 
-    public InputView(Scanner scanner) {
-        this.scanner = scanner;
+    public static int inputCarNumber(){
+        System.out.println(CAR_TEXT + CAR_INPUT_QUESTION);
+        return inputIntNumber(CAR_TEXT);
     }
 
-    public String inputCarNumber(){
-        System.out.println(CAR_INPUT_QUESTION);
-        return scanner.nextLine();
+    public static int inputTryTime() {
+        System.out.println(TRY_TIME_TEXT + TRY_TIME_INPUT_QUESTION);
+        return inputIntNumber(TRY_TIME_TEXT);
     }
 
-    public String inputTryTime() {
-        System.out.println(TRY_TIME_INPUT_QUESTION);
-        return scanner.nextLine();
+    private static int inputIntNumber(String targetText) {
+        if (scanner.hasNextInt()) {
+            return valitate(targetText, scanner.nextInt());
+        }
+        throw new IllegalArgumentException(targetText + DELIMITER + ILLEGALARGUMENT_EXCEPTION_MASSEAGE);
     }
 
+    private static int valitate(String targetText, int nextInt) {
+        if (nextInt <= 1){
+            throw new IllegalArgumentException(targetText + DELIMITER +nextInt+ ILLEGALARGUMENT_VALIDATE_MASSEAGE);
+        }
+        return nextInt;
+    }
 }
