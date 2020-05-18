ba8b2a979511515f5ea30560a36a94cf51751ddd
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-18 09:46:33 +0900

    test: create new test file for business logic

---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("자동차 경주의 핵심 로직을 테스트한다.")
+public class RacingCarTest {
+
+  @Test
+  void 랜덤값이_0과_9_사이이다() {
+    //
+  }
+
+  @Test
+  void 랜덤값이_4이상일때_전진한다() {
+    //
+  }
+}
