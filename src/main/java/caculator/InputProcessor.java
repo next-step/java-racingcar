7f2abb836a8eb6d735c9851a368a9797b4d1ae63
wyh951753 <wyh951753@gmail.com>
2020-05-19 00:11:40 +0900

    Implements - 잘라진 배열에 대한 얘외 처리 코드 추가

--- src/main/java/caculator/InputProcessor.java
+++ src/main/java/caculator/InputProcessor.java
@@ -1,36 +1,32 @@
 package caculator;
 
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.Arrays;
-import java.util.List;
-
 public class InputProcessor {
     private static final String SPLIT_TOKEN = " ";
 
-    public String[] processInput(final String input) {
+    public String[] process(final String input) {
         validateNullOrEmpty(input);
 
         String[] splitInput = input.split(SPLIT_TOKEN);
+        validateSplitInputLength(splitInput);
         validateNumber(splitInput);
-        validateOperator(splitInput);
         return splitInput;
     }
 
     private void validateNullOrEmpty(final String input) {
-        if (StringUtils.isBlank(input)) throw new IllegalArgumentException();
+        if (isBlank(input)) throw new IllegalArgumentException();
+    }
+
+    private boolean isBlank(final String input) {
+        return input == null || input.trim().length() == 0;
+    }
+
+    private void validateSplitInputLength(final String[] splitInput) {
+        if (splitInput.length % 2 == 0) throw new IllegalArgumentException();
     }
 
     private void validateNumber(final String[] splitInput) {
-        for(int i = 0; i < splitInput.length; i += 2) {
+        for (int i = 0; i < splitInput.length; i += 2) {
             if (Double.isNaN(Double.parseDouble(splitInput[i]))) throw new IllegalArgumentException();
-        }
-    }
-
-    private void validateOperator(final String[] splitInput) {
-        List<String> operatorList = Arrays.asList("+", "-", "*", "/");
-        for(int i = 1; i < splitInput.length; i += 2) {
-            if (!operatorList.contains(splitInput[i])) throw new IllegalArgumentException();
         }
     }
 }
