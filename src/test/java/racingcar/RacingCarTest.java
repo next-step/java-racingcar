9d61fa5851bd8e91104142233298649d0858559a
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-18 12:44:11 +0900

    test: refactor reference case for copying car list

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -14,25 +14,6 @@ import racingcar.util.RandomUtil;
 @DisplayName("자동차 경주의 핵심 로직을 테스트한다.")
 public class RacingCarTest {
 
-  List<Car> carList;
-
-  @BeforeEach
-  void setUp() {
-    //given
-    Car car1 = Car.create("first car");
-    Car car2 = Car.create("second car");
-    Car car3 = Car.create("third car");
-    Car car4 = Car.create("fourth car");
-    Car car5 = Car.create("fifth car");
-
-    carList = new ArrayList<>();
-    carList.add(car1);
-    carList.add(car2);
-    carList.add(car3);
-    carList.add(car4);
-    carList.add(car5);
-  }
-
   @Test
   void 랜덤값이_0과_9_사이이다() {
     assertThat(RandomUtil.getRandomValue()).isBetween(0, 9);
@@ -41,24 +22,31 @@ public class RacingCarTest {
   @Test
   void 랜덤값이_4이상일때_전진한다() {
 
-    //game에 들어간 객체 내부 리스트는 독립적이지 않나? 외부 리스트가 변동되면 내부 리스트도 바뀐다.
-    //아마 stream 때문인 듯
-    //이게 pass by reference라는 건가??
-    //https://stackoverflow.com/questions/27635637/java-no-arraylist-modifications-from-outside-classes
-    //https://stackoverflow.com/questions/47177561/java-list-changed-when-update-another-one
+    //given
+    Car car1 = Car.create("first car");
+    int previousPositionOfCar1 = car1.getPosition();
+    Car car2 = Car.create("second car");
+    Car car3 = Car.create("third car");
+    Car car4 = Car.create("fourth car");
+    Car car5 = Car.create("fifth car");
+
+    List<Car> carList = new ArrayList<>();
+    carList.add(car1);
+    carList.add(car2);
+    carList.add(car3);
+    carList.add(car4);
+    carList.add(car5);
+
     Game game = Game.create(carList);
 
     //when
-    game.doRace(4);
-//    assertThat(game.getCars().get(0).getPosition()).isGreaterThan(car1.getPosition());
+    assertThat(game.doRace(3).get(0).getPosition())
+        //then
+        .isEqualTo(previousPositionOfCar1);
 
-//    assertThat(game.doRace(3)et(0).getPosition())
-//        //then
-//        .isEqualTo(car1.getPosition());
-//
-//    //when
-//    assertThat(game.doRace(4).get(0).getPosition())
-//        //then
-//        .isGreaterThan(car1.getPosition());
+    //when
+    assertThat(game.doRace(4).get(0).getPosition())
+        //then
+        .isGreaterThan(previousPositionOfCar1);
   }
 }
