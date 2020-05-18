22e313b76f22797c80a98b02b874cd94e168123d
wyh951753 <wyh951753@gmail.com>
2020-05-19 00:13:48 +0900

    Fix - Map을 초기화 하는 코드 프로젝트 컨벤션 반영

--- src/main/java/caculator/Operator.java
+++ src/main/java/caculator/Operator.java
@@ -1,22 +1,35 @@
 package caculator;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 
 public class Operator {
 
-    private static final Map<String, BiFunction<Double, Double, Double>> OPERATOR_MAP = Map.of(
-            "+", (left, right) -> left + right,
-            "-", (left, right) -> left - right,
-            "/", (left, right) -> left / right,
-            "*", (left, right) -> left * right
-    );
+    private static final Map<String, BiFunction<Double, Double, Double>> OPERATOR_MAP  = new HashMap<String, BiFunction<Double, Double, Double>>()
+    {{
+        put("+", (left, right) -> left + right);
+        put("-", (left, right) -> left - right);
+        put("/", (left, right) -> left / right);
+        put("*", (left, right) -> left * right);
+    }};
 
     public double calculate(final double left, final String operator, final String right) {
+        if (!isNumeric(right)) throw new IllegalArgumentException();
         return calculate(left, operator, Double.parseDouble(right));
     }
 
     public double calculate(final double left, final String operator, final double right) {
+        if (!OPERATOR_MAP.containsKey(operator)) throw new IllegalArgumentException();
         return OPERATOR_MAP.get(operator).apply(left, right);
+    }
+
+    private boolean isNumeric(final String stringNumber) {
+        try {
+            Double.parseDouble(stringNumber);
+            return true;
+        } catch (NumberFormatException e){
+            return false;
+        }
     }
 }
