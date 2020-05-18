817dbbb391095793516665a5daf08a94cdb66b62
Sungjun <sungjunpizz@gmail.com>
2020-05-18 22:22:13 +0900

    refactor(RacingCarGame): Cars 클래스 추가에 따른 수정

--- src/main/java/racingcar/domain/RacingCarGame.java
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -1,61 +1,29 @@
 package racingcar.domain;
 
-import java.util.*;
-
-import static java.util.stream.Collectors.toList;
-
 public class RacingCarGame {
 
-    private List<Car> cars;
+    private final Cars cars;
     private final int moveCount;
 
     public RacingCarGame(String carNames, int moveCount) {
-        this.cars = createCars(carNames);
+        this.cars = Cars.createCars(carNames);
         this.moveCount = moveCount;
     }
 
-    public List<Car> getCars() {
-        return cars;
-    }
-
-    private List<Car> createCars(String carNames) {
-        String[] names = carNames.split(",");
-
-        cars = new ArrayList<>();
-
-        Arrays.stream(names).forEach(name -> cars.add(new Car(name)));
-
+    public Cars getCars() {
         return cars;
     }
 
     public void execute(Strategy strategy) {
+        GameResult gameResult = new GameResult(cars);
+
         for (int i = 0; i < moveCount; i++) {
-            GameResult result = carRacing(strategy);
-            result.displayResult();
+            cars.carRacing(strategy);
+            gameResult.displayResult();
         }
 
-        List<Car> winners = findWinners();
-        GameResult.displayWinner(winners);
-    }
-
-    private GameResult carRacing(Strategy strategy) {
-        cars.forEach(car -> {
-            int randomNumber = strategy.getNumber();
-            car.move(randomNumber);
-        });
-
-        return new GameResult(cars);
-    }
-
-    private List<Car> findWinners() {
-        int largest = cars.stream()
-                .mapToInt(Car::getPosition)
-                .max()
-                .orElse(0);
-
-        return cars.stream()
-                .filter(car -> car.getPosition() == largest)
-                .collect(toList());
+        Cars winners = cars.getWinners();
+        gameResult.displayWinner(winners);
     }
 }
 
