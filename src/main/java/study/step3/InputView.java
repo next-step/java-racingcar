e16fd0f7b9ddd18fda9ad27fa333b0c44b85861e
bbbnam <bbbnam@naver.com>
2020-05-18 21:54:44 +0900

    refactor : 매직넘버 상수화

--- src/main/java/study/step3/InputView.java
+++ src/main/java/study/step3/InputView.java
@@ -10,6 +10,9 @@ public class InputView {
     private final static String DELIMITER =":";
     private final static String ILLEGALARGUMENT_EXCEPTION_MASSEAGE = "올바르지 않은 값을 입력했습니다.";
     private final static String ILLEGALARGUMENT_VALIDATE_MASSEAGE = "보다는 큰 값을 입력해야 합니다.";
+
+    private final static int MINIMUM_NUMBER = 1;
+
     private final static Scanner scanner = new Scanner(System.in);
 
     public static int inputCarNumber(){
@@ -30,7 +33,7 @@ public class InputView {
     }
 
     private static int valitate(String targetText, int nextInt) {
-        if (nextInt <= 1){
+        if (nextInt <= MINIMUM_NUMBER){
             throw new IllegalArgumentException(targetText + DELIMITER +nextInt+ ILLEGALARGUMENT_VALIDATE_MASSEAGE);
         }
         return nextInt;
