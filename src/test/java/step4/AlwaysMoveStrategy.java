a9e812ba4354b131d107870770f941ae7fd7dd1d
junilHwang <tjsdlf4261@naver.com>
2020-05-19 23:38:00 +0900

    [Step4 - 자동차 경주 우승자] test: test를 위한 MoveStrategy 구현체와, MoveStrategyTest 추가

---
+++ src/test/java/step4/AlwaysMoveStrategy.java
@@ -0,0 +1,13 @@
+package step4;
+
+public class AlwaysMoveStrategy implements MoveStrategy {
+  @Override
+  public boolean isMoved() {
+    return true;
+  }
+
+  @Override
+  public boolean isMoved(int number) {
+    return true;
+  }
+}
