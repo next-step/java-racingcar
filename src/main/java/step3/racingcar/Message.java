e39c37daf416b50fd5a2b357bc07968c9fb35d0b
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-19 11:53:09 +0900

    refactor (RacingGame, Message) exception 생성 메시지 추가

--- src/main/java/step3/racingcar/Message.java
+++ src/main/java/step3/racingcar/Message.java
@@ -4,7 +4,8 @@ public class Message {
 
     public static final String CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String GAME_TRY_COUNTS = "시도할 회수는 몇 회 인가요?";
-    public static final String ERROR = "ERROR : 입력 숫자는 1 이상이어야 합니다.";
+    public static final String ERROR_CAR_NAMES = "ERROR : 자동차 이름 입력이 잘못되었습니다.";
+    public static final String ERROR_GAME_TRY_COUNTS = "ERROR : 입력 숫자는 1 이상이어야 합니다.";
     public static final String RESULT_HEADER = "실행 결과";
 
     private Message() {
--- src/main/java/step3/racingcar/RacingGame.java
+++ src/main/java/step3/racingcar/RacingGame.java
@@ -18,9 +18,9 @@ public class RacingGame {
 
     private void validateConstructors(String[] carNames, int gameTryCounts) {
         if (carNames.length < MINIMUM_LIMIT || carNames[INDEX_ZERO].equals(BLANK_NAME))
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Message.ERROR_CAR_NAMES);
         if (gameTryCounts < MINIMUM_LIMIT)
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(Message.ERROR_GAME_TRY_COUNTS);
     }
 
     public void run() {
