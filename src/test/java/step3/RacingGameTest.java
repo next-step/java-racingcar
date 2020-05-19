0f616a5649f437934209d064e4766b1190b2b97d
Jun <p_illusion@naver.com>
2020-05-19 16:11:44 +0900

    [feat] 게임 기능 및 테스트 추가

---
+++ src/test/java/step3/RacingGameTest.java
@@ -0,0 +1,15 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class RacingGameTest {
+    @Test
+    @DisplayName("RacingGameTest 초기화 테스")트
+    void move() {
+
+    }
+}
