b0a7d9119dcbaee6104342582c3f30d5abaaa94d
Jun <p_illusion@naver.com>
2020-05-19 16:17:06 +0900

    [feat] 래이싱 메인 클래스 추가

---
+++ src/main/java/step3/RacingMain.java
@@ -0,0 +1,22 @@
+package step3;
+
+import java.util.List;
+
+public class RacingView {
+    private static final String TIREMARK = "-";
+
+    public RacingView() {
+        System.out.println("\n실행 결과");
+    }
+
+    public static void print(List<RacingCar> racingCar) {
+        for(RacingCar car : racingCar) {
+            for(int i=0; i<car.getCurrentPosition(); i++) {
+                System.out.print(TIREMARK);
+            }
+            System.out.println(" ");
+        }
+        System.out.println("");
+    }
+}
+
