7fdf3f6bb02bb32eaba240b3baba026f9a9e3085
junilHwang <tjsdlf4261@naver.com>
2020-05-19 23:17:01 +0900

    [Step4 - 자동차 경주 우승자] feat: View Code 수정

--- src/main/java/step4/view/ResultView.java
+++ src/main/java/step4/view/ResultView.java
@@ -2,9 +2,26 @@ package step4.view;
 
 import step4.Car;
 
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public interface ResultView {
-  void print(List<Car> cars);
-  void winner(String winners);
+public class ResultView {
+
+  private ResultView() {};
+
+  public static void printResultText () {
+    System.out.println("실행 결과");
+  }
+
+  public static void printRace (Stream<Car> stream) {
+    System.out.println(
+      stream.map(car -> car.getPositionString()).collect(Collectors.joining("\n"))
+      + "\n"
+    );
+  }
+
+  public static void printWinner (String winners) {
+    System.out.printf("%s가 최종 우승 했습니다.\n", winners);
+  }
+
 }
