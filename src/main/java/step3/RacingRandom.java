4842bda1485397c40d99095e18621ae2ffed5284
Jun <p_illusion@naver.com>
2020-05-19 20:06:21 +0900

    [refactor] Random 한번만 선언으로 변경

--- src/main/java/step3/RacingRandom.java
+++ src/main/java/step3/RacingRandom.java
@@ -5,11 +5,7 @@ import java.util.Random;
 public class RacingRandom {
     private static final int BOUND_NUMBER = 4;
 
-    private Random random;
-
-    public RacingRandom() {
-        random = new Random();
-    }
+    private static final Random random = new Random();
 
     public int getRandomNumber() {
         return random.nextInt(BOUND_NUMBER);
