59ba658c1a2ec537ace67aee3d1fd5a2a462ff2b
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-19 14:46:57 +0900

    test (GameResultBoard) 기록 테스트 추가

--- src/test/java/step3/racingcar/GameResultBoardTest.java
+++ src/test/java/step3/racingcar/GameResultBoardTest.java
@@ -1,10 +1,15 @@
 package step3.racingcar;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,5 +31,27 @@ public class GameResultBoardTest {
                 Arguments.of("java,c,js,sql".split(","), 7),
                 Arguments.of("pizza,hamburger,chicken".split(","), 13)
         );
+    }
+
+    @DisplayName("게임 진행 후 자동차의 위치가 GameResultBoard에 정상 기록됨")
+    @ParameterizedTest
+    @MethodSource("mockGameResultBoardBuilder")
+    public void recordRoundInGameResultBoard(String[] carNames, int gameTryCounts) {
+        //Given
+        List<Car> cars = CarFactory.makeCars(carNames);
+        GameResultBoard gameResultBoard = new GameResultBoard(carNames, gameTryCounts);
+
+        //When
+        for (int i = 0; i < gameTryCounts; i++) {
+            cars.forEach(Car::move);
+            gameResultBoard.recordRound(cars, i);
+        }
+
+        //Then
+        int[][] resultArray = gameResultBoard.getGameResultBoard();
+        for (int[] tmp : resultArray) {
+            for (int i = 0; i < tmp.length; i++)
+                assertThat(tmp[i]).isGreaterThanOrEqualTo(0);
+        }
     }
 }
