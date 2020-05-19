529486000d08be5f6c18bab937391cb16038d2d3
y00s22 <ryusuy22@gmail.com>
2020-05-19 23:35:14 +0900

    test case 작성

--- src/main/java/mission1/racing/RacingRandom.java
+++ src/main/java/mission1/racing/RacingRandom.java
@@ -3,7 +3,7 @@ package mission1.racing;
 import java.util.Random;
 
 public class RacingRandom {
-    private Random rand = new java.util.Random();
+    private Random rand = new Random();
 
     public int random() {
         int randomNum = rand.nextInt(10);
--- src/test/java/RacingTest.java
+++ src/test/java/RacingTest.java
@@ -1,6 +1,25 @@
 import mission1.racing.RacingGame;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class RacingTest {
     RacingGame racingGame = new RacingGame();
 
+    @DisplayName("게임이 제대로 이루어지는 경우")
+    @Test
+    void testRacingGame() {
+
+    }
+
+    @DisplayName("자동차 대수가 0인 경우")
+    @Test
+    void testNoCar() {
+
+    }
+
+    @DisplayName("시도할 횟수가 0인 경우")
+    @Test
+    void testZeroTime() {
+
+    }
 }
