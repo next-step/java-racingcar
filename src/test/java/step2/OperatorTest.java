6436a7fd36614ce57f414146f7979382e6ac1681
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-19 15:59:34 +0900

    test(Operator) : 연산자 기능 테스트

--- src/test/java/step2/OperatorTest.java
+++ src/test/java/step2/OperatorTest.java
@@ -5,26 +5,35 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+
+import step2.Operator;
 
 class OperatorTest {
-
     @ParameterizedTest
-    @CsvSource({ "+", "-", "*", "/" })
-    @DisplayName("String 값으로 enum 객체 찾기")
-    void findOperatorBySymbol(String param) {
-        Operator operator = Operator.findOperatorBySymbol(param);
-
-        assertThat(operator.symbol).isEqualTo(param);
-    }
-    
-    @ParameterizedTest
-    @ValueSource(strings = { " ", "1", "a", "%" })
-    @DisplayName("사칙연산 기호가 아닌 경우 ")
-    void findOperatorBySymbol_withNonOperator(String operator) {
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
-            Operator.findOperatorBySymbol(operator);
-        });
+    @CsvSource({ "2, 3, 5" })
+    @DisplayName("더하기")
+    void plus(double left, double right, double expected) {
+        assertThat(Operator.PLUS.calculate(left, right)).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @CsvSource({ "2, 3, -1" })
+    @DisplayName("뺴기")
+    void minus(double left, double right, double expected) {
+        assertThat(Operator.PLUS.calculate(left, right)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "2, 3, 6" })
+    @DisplayName("곱하기")
+    void multiply(double left, double right, double expected) {
+        assertThat(Operator.PLUS.calculate(left, right)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "2, 4, 0.5" })
+    @DisplayName("나누기")
+    void division(double left, double right, double expected) {
+        assertThat(Operator.PLUS.calculate(left, right)).isEqualTo(expected);
+    }
 }
