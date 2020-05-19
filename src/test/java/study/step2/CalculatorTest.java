828235b05d120058bcc4f5d578d608df91f592dc
eunjeong <cs801458j@gmail.com>
2020-05-20 02:16:52 +0900

    테스트코드 질문용 커밋

--- src/test/java/study/step2/CalculatorTest.java
+++ src/test/java/study/step2/CalculatorTest.java
@@ -5,20 +5,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.w3c.dom.css.CSSValue;
+import stringcalculator.Calculator;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
+
+    private static double calculate(String operator, double operand1, double operand2){
+        return Calculator.calculate(operator, operand1, operand2);
+    }
+
 
     @BeforeEach
     void Calculator(){
@@ -28,29 +36,47 @@ public class CalculatorTest {
     @DisplayName("덧셈기능 테스트")
     @Test
     void add(){
-        double result = calculator.trimInputExpression("2 + 3");
-        assertThat(result).isEqualTo(5.0);
+        //String result = calculator.trimInputExpression("2 + 3");
+
     }
+
+    @DisplayName("덧셈기능 테스트")
+    @Test
+    void calculateTest(){
+
+        String input = "2 + 3 + 5";
+        String[] split = input.split(" ");
+
+        //  홀짝으로 분리. 그다음 calculate mapping은 어떻게 진행해야할까요?
+        IntStream.range(0, split.length).filter(i -> i % 2 == 0).forEach(i -> System.out.println(split[i]));
+        IntStream myIntStream = IntStream.range(0, split.length).filter(i -> i % 2 == 0);
+
+        //List<Integer> intList = myIntStream.mapToObj(i->i).collect(Collectors.toList());
+
+
+        IntStream.range(0, split.length).filter(i -> i % 2 != 0).forEach(i -> System.out.println(split[i]));
+    }
+
 
     @DisplayName("뺄셈기능 테스트")
     @Test
     void sub(){
-        double result = calculator.trimInputExpression("5 - 4");
-        assertThat(result).isEqualTo(1.0);
+//        double result = calculator.trimInputExpression("5 - 4");
+//        assertThat(result).isEqualTo(1.0);
     }
 
     @DisplayName("뺄셈기능 테스트")
     @Test
     void mutiple(){
-        double result = calculator.trimInputExpression("3 * 4");
-        assertThat(result).isEqualTo(12.0);
+//        double result = calculator.trimInputExpression("3 * 4");
+//        assertThat(result).isEqualTo(12.0);
     }
 
     @DisplayName("나눗셈 기능 테스트")
     @Test
     void divide(){
-        double result = calculator.trimInputExpression("4 / 2");
-        assertThat(result).isEqualTo(20.0);
+//        double result = calculator.trimInputExpression("4 / 2");
+//        assertThat(result).isEqualTo(20.0);
     }
 
     @DisplayName("입력값 null or 공백 테스트")
@@ -73,9 +99,11 @@ public class CalculatorTest {
     @DisplayName("사칙연산을 모두 포함하는 기능 구현")
     @Test
     void calculatorTest(){
-        double result = calculator.trimInputExpression("2 + 3 * 4");
-        assertThat(result).isEqualTo(20.0);
+//        double result = calculator.trimInputExpression("2 + 3 * 4");
+//        assertThat(result).isEqualTo(20.0);
     }
+
+
 
 
 }
