a067f56347265c26013a6ece2d777c6b347ff2ab
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-19 11:01:59 +0900

    refactor (InputViewProcessor) userName 받는 기능 추가로 인한 refactor 및 예외 처리 기능 삭제

--- src/main/java/step3/racingcar/Application.java
+++ src/main/java/step3/racingcar/Application.java
@@ -3,6 +3,5 @@ package step3.racingcar;
 public class Application {
 
     public static void main(String[] args) {
-        
     }
 }
--- src/main/java/step3/racingcar/Message.java
+++ src/main/java/step3/racingcar/Message.java
@@ -2,7 +2,7 @@ package step3.racingcar;
 
 public class Message {
 
-    public static final String CAR_COUNTS = "자동차 대수는 몇 대 인가요?";
+    public static final String CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String GAME_TRY_COUNTS = "시도할 회수는 몇 회 인가요?";
     public static final String ERROR = "ERROR : 입력 숫자는 1 이상이어야 합니다.";
     public static final String RESULT_HEADER = "실행 결과";
