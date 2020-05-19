d0535f28f8fcc5242b0a469708c53f471ae90f63
Jaehun Lee <ljhokgo@gmail.com>
2020-05-20 00:40:53 +0900

    test: 자동차 경주 테스트 추가
    - 자동차 이동 테스트
    - 자동차 이동 후 정렬 테스트

--- src/test/java/study/racing/RacingGameTest.java
+++ src/test/java/study/racing/RacingGameTest.java
@@ -2,17 +2,44 @@ package study.racing;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import study.racing.model.Car;
+import study.racing.ui.ResultView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
-    private RacingGame racingGame;
+    private List<Car> carList;
 
     @BeforeEach
     private void setUp() {
-        racingGame = new RacingGame(3, 5);
+        carList = new ArrayList<>();
+        carList.add(new Car("car1"));
+        carList.add(new Car("car2"));
+        carList.add(new Car("car3"));
     }
 
     @Test
-    void play() {
-        racingGame.play();
+    void move() {
+        for(Car car : carList) {
+            car.move();
+            assertThat(car.getCount()).isBetween(0, 1);
+        }
+    }
+
+    @Test
+    void sort() {
+        for(int i=0; i<10; i++) {
+            for(Car car : carList) {
+                car.move();
+            }
+        }
+
+        Collections.sort(carList);
+
+        assertThat(carList.get(0).getCount()).isLessThanOrEqualTo(carList.get(carList.size()-1).getCount());
     }
 }
