ec6b8f4a1853e0d581b74562ba88c98d788f965e
Jaehun Lee <ljhokgo@gmail.com>
2020-05-19 16:21:00 +0900

    Step3 리뷰 요청드립니다. (#860)
    * docs(README):
    
    기능 목록 추가:
    - UI
    - 경주
    
    * feat(ui): input view
    
    사용자 입력 UI 추가:
    - 질문 출력하고 사용자 입력 대기
    - 숫자 아닌 값 입력 시 예외처리
    
    * feat(ui): result view
    
    자동차 상태 출력 UI 추가:
    - 자동차 현재 위치 출력
    
    * feat(game): racing game
    
    자동차 경주 로직 추가:
    - 자동차 전진 여부 판단
    - 자동차 이동
    
    * feat(game): racing game 수정
    
    자동차 경주 생성자 추가:
    - 테스트를 위한 임의의 입력 값을 바로 입력받는 생성자 추가
    
    * test:
    
    자동차 경주 유닛 테스트 추가:
    - 임의의 값 입력 받아 게임 실행

---
+++ src/main/java/study/racing/RacingGame.java
@@ -0,0 +1,73 @@
+package study.racing;
+
+import study.racing.ui.InputView;
+import study.racing.ui.ResultView;
+
+public class RacingGame {
+    private final InputView inputView;
+    private final ResultView resultView;
+
+    private int[] carPositions;
+    private int time;
+
+    public RacingGame() {
+        inputView = new InputView();
+        resultView = new ResultView();
+    }
+
+    public RacingGame(int numOfCars, int time) {
+        inputView = new InputView();
+        resultView = new ResultView();
+
+        carPositions = new int[numOfCars];
+        this.time = time;
+    }
+
+    public void play() {
+        if(carPositions == null) {
+            configureGameSettings();
+        }
+
+        System.out.println("실행 결과");
+
+        for(int i=0; i<time; i++) {
+            move();
+            resultView.printCarPositions(carPositions);
+        }
+    }
+
+    private void configureGameSettings() {
+        int numOfCars = inputView.scanIntWithQuestion("자동차 대수는 몇 대 인가요?");
+        this.time = inputView.scanIntWithQuestion("시도할 회수는 몇 회 인가요?");
+
+        carPositions = new int[numOfCars];
+    }
+
+    private void move() {
+        for(int i=0; i<carPositions.length; i++) {
+            if(canMove()) {
+                carPositions[i]++;
+            }
+        }
+    }
+
+    private boolean canMove() {
+        boolean result = false;
+
+        if(getRandomNumber() >= 4) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    private int getRandomNumber() {
+        double tmp = Math.random();
+        return (int)(tmp * 10);
+    }
+
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        racingGame.play();
+    }
+}
