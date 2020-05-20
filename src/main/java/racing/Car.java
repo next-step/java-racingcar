aa5bb6197cd3179554979c6af925a39d0c7c4b2a
bo0216 <bo216@naver.com>
2020-05-20 22:27:53 +0900

    feat: add move()

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -9,4 +9,10 @@ public class Car {
         this.position = position;
     }
 
+    public int move(int random) {
+        if (random >= 4) {
+            this.position = this.position + 1;
+        }
+        return this.position;
+    }
 }
