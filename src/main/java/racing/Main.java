103d8be98ecb4952d453db38e9ff9df1edd2d4aa
bluehoped <bluehoped@hanmail.net>
2020-05-21 01:49:58 +0900

    수정 Main

--- src/main/java/racing/Main.java
+++ src/main/java/racing/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         InputView carCountQuestion = InputView.addQuestion("자동차 대수는 몇 대 인가요?");
+        int carCount = carCountQuestion.input();
         InputView roundQuestion = InputView.addQuestion("시도할 회수는 몇 회 인가요?");
+        int round = roundQuestion.input();
 
-        RacingGame racingGame = new RacingGame(carCountQuestion.input(), roundQuestion.input());
+        RacingGame racingGame = new RacingGame(carCount, round);
 
         racingGame.playGame();
     }
