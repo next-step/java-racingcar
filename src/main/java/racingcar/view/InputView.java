3370fcdb7dec5aa86f3d01060422a82d0dce3a99
jun-pil park <iamfeel2@gmail.com>
2020-05-20 15:24:22 +0900

    feat (InputView) : 자동차의 수를 입력받는 기능을 '이름을 입력받는 기능'으로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,7 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static String TEXT_QUESTION_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
+    private static String TEXT_QUESTION_CAR_NAME_JOIN_RACE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static String TEXT_QUESTION_RACING_TIME = "시도할 회수는 몇 회 인가요?";
 
     private static Scanner scanner = new Scanner(System.in);
@@ -12,16 +12,13 @@ public class InputView {
 
     }
 
-    public static int inputCarCountJoinRace() {
-        return getInputConsole(TEXT_QUESTION_CAR_COUNT);
+    public static String inputCarCountJoinRace() {
+        System.out.println(TEXT_QUESTION_CAR_NAME_JOIN_RACE);
+        return scanner.nextLine();
     }
 
     public static int inputRaceTime() {
-        return getInputConsole(TEXT_QUESTION_RACING_TIME);
-    }
-
-    private static int getInputConsole(String question) {
-        System.out.println(question);
+        System.out.println(TEXT_QUESTION_RACING_TIME);
         return scanner.nextInt();
     }
 }
