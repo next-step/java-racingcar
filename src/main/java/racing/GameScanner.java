3312ca48b5b46ca5dbce964be512fad0b8a82f33
bluehoped <bluehoped@hanmail.net>
2020-05-20 23:29:50 +0900

    변경 GameScanner scanner 직접 리턴

--- src/main/java/racing/GameScanner.java
+++ src/main/java/racing/GameScanner.java
@@ -1,29 +1,10 @@
 package racing;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GameScanner {
 
     public static int input() {
-        String input;
-        Scanner scanner = new Scanner(System.in);
-
-        input = scanner.nextLine();
-
-        if (isInteger(input)) {
-            return Integer.parseInt(input);
-        }
-
-        throw new InputMismatchException("숫자를 입력해주세요.");
-    }
-
-    static boolean isInteger(String input) {
-        try {
-            Integer.parseInt(input);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        return new Scanner(System.in).nextInt();
     }
 }
--- src/test/java/racing/GameScannerTest.java
+++ src/test/java/racing/GameScannerTest.java
@@ -1,19 +1,8 @@
 package racing;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@DisplayName("입력받는 게임스캐너 클래스 테스트")
+@DisplayName("입력받는 메소드만 존재하여 테스트할 수 없습니다.")
 class GameScannerTest {
-
-    @ParameterizedTest
-    @CsvSource(value = {" :false", "a:false", "e:false", "3:true"}, delimiter = ':')
-    @DisplayName("입력 값이 숫자인지 체크가 올바른지 하는 테스트")
-    void isIntegerMethodTest(String input, boolean expected) {
-        assertThat(GameScanner.isInteger(input)).isEqualTo(expected);
-    }
 
 }
