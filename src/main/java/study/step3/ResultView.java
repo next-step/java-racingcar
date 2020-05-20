d78c0cbbabaf0c09eb34fcb3da04ce83c8b62e50
bbbnam <bbbnam@naver.com>
2020-05-20 21:34:18 +0900

    refactor : ResultView 상태 제거

--- src/main/java/study/step3/ResultView.java
+++ src/main/java/study/step3/ResultView.java
@@ -1,39 +1,43 @@
 package study.step3;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
     private static final String UNDER_BAR_SYMBOL ="_";
+    private static final String CAR_NAME_DELIMITER =" : ";
+    private static final String CHAMPION_RESULT_MESSAGE ="가 최종 우승했습니다.";
     private static final int INIT_ZERO = 0;
 
-    private List<Round> rounds;
-
-    public ResultView(List<Round> rounds) {
-        this.rounds = rounds;
-    }
-
-    public void print() {
-        roundToString();
-    }
-
-    private void roundToString() {
+    public static void racingResultPrint(List<Round> rounds) {
         for (Round round : rounds){
             carToString(round.getCars());
             System.out.println();
         }
     }
 
-    private void carToString(List<Car> cars) {
+    private static void carToString(List<Car> cars) {
         for (Car car: cars){
-            System.out.println(convertToUnderBarSymbol(car.getCarPosition()));
+            System.out.println(car.getCarName()+CAR_NAME_DELIMITER+convertToUnderBarSymbol(car.getCarPosition()));
         }
     }
 
-   private String convertToUnderBarSymbol(int pointInTimeCarPostion) {
-        StringBuffer stringBuffer = new StringBuffer(UNDER_BAR_SYMBOL);
+   private static String convertToUnderBarSymbol(int pointInTimeCarPostion) {
+        StringBuilder stringBuilder = new StringBuilder(UNDER_BAR_SYMBOL);
         for (int i = INIT_ZERO; i < pointInTimeCarPostion; i++){
-            stringBuffer.append(UNDER_BAR_SYMBOL);
+            stringBuilder.append(UNDER_BAR_SYMBOL);
         }
-        return stringBuffer.toString();
+        return stringBuilder.toString();
+    }
+
+    public static void printChampion(List<String> champions) {
+        String champion = convertListToString(champions);
+        System.out.println(champion+CHAMPION_RESULT_MESSAGE);
+    }
+
+    private static String convertListToString(List<String> champions) {
+        return champions.stream()
+                        .map(Object::toString)
+                        .collect(Collectors.joining(", "));
     }
 }
