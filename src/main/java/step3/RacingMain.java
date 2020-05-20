46775acae5c958c4c71567c3bbed5ffcbc7ea8ad
Jun <p_illusion@naver.com>
2020-05-21 02:12:23 +0900

    [feat] 기능 추가에 따른 메인 변경

--- src/main/java/step3/RacingMain.java
+++ src/main/java/step3/RacingMain.java
@@ -1,24 +1,22 @@
 package step3;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class RacingMain {
     private static final String INPUT_TIMES = "시도할 회수는 몇 회 인가요";
-    private static final String INPUT_CARS = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_CARS = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 
     public static void main(String[] args) {
         RacingInput racingInput = new RacingInput();
-        int carNumber = racingInput.InputInfo(INPUT_CARS);
-        int gameCoin = racingInput.InputInfo(INPUT_TIMES);
+        String[] carNames = racingInput.inputForString(INPUT_CARS);
+        int gameCoin = racingInput.inputForInt(INPUT_TIMES);
 
-        RacingGame racingGame = new RacingGame(carNumber);
+        RacingGame racingGame = new RacingGame(carNames);
 
         for(int i=0; i<gameCoin; i++) {
             racingGame.moveCars();
-            RacingView.print(racingGame.getRacingCarList());
+            RacingOutput.printRacing(racingGame.getRacingCarList());
         }
+
+        RacingOutput.printWinner(racingGame.findWinner());
     }
 }
 
