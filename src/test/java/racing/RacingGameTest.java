25de96bbbdc336731439a11a36fc51f3154ce0e6
bluehoped <bluehoped@hanmail.net>
2020-05-21 01:48:46 +0900

    수정 RacingGameTest 테스트 수정

--- src/test/java/racing/RacingGameTest.java
+++ src/test/java/racing/RacingGameTest.java
@@ -11,22 +11,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RacingGameTest {
 
     @Test
-    @DisplayName("게임 초기화 테스트")
-    void initRacingGameTest() {
+    @DisplayName("round를 입력받아 생성된 RacingGame의 private round 필드 값을 확인하는 테스트")
+    void racingGameConstructorTest() {
         try {
             int carCount = 3;
             int round = 5;
             RacingGame racingGame = new RacingGame(carCount, round);
+
             Class<? extends RacingGame> clazz = racingGame.getClass();
             Field privateRoundField = clazz.getDeclaredField("round");
             privateRoundField.setAccessible(true);
             assertThat(privateRoundField.get(racingGame)).isEqualTo(round);
-            //FIXME:carList size 호출해서 비교하는부분 추가
-            Field privateCarListField = clazz.getDeclaredField("carList");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    @DisplayName("playGame 메소드의 실행 결과를 확인할 수 있는 필드에 접근할 수 없어 테스트 할 수 없는 테스트")
+    void playGameTest() {
+        RacingGame racingGame = new RacingGame(3, 4);
+        racingGame.playGame();
     }
 
 
