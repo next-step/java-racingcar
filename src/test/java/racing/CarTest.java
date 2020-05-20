cde3d0305eb4eec50f86b43bd6405c0daa753927
bo0216 <bo216@naver.com>
2020-05-20 22:53:36 +0900

    feat: add moveTest()

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,5 +1,29 @@
 package racing;
 
-public class CarTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CarTest {
+    private Car car;
+    private int random;
+    private RandomNumber randomNumber;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car(0, 0);
+        randomNumber = new RandomNumber();
+        random = randomNumber.getRandomNumber();
+    }
+
+    @Test
+    @DisplayName("Random값이 4이상일 때 Car 이동")
+    void moveTest() {
+        if (random >= 4) {
+            assertThat(car.move(random)).isEqualTo(1);
+        }
+    }
 }
--- src/test/java/racing/RandomNumberTest.java
+++ src/test/java/racing/RandomNumberTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RandomNumberTest {
