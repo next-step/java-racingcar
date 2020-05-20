a13a43ca2a59ab85fc5509b7c779b1ced8ed2181
bluehoped <bluehoped@hanmail.net>
2020-05-21 01:38:59 +0900

    변경 car Test 및 GameScannerTest 삭제

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -8,14 +8,16 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @DisplayName("Car 클래스 테스트")
 class CarTest {
-    private final int movePosition = 1;
-    private final int stopPosition = 0;
 
     @Test
     @DisplayName("차 이동 여부에 따라 포지션(이동:1, 정지:0)을 테스트한다.")
     void moveTest() {
+        int stopPosition = 0;
+        int movePosition = 1;
+
         Car car = new Car();
         car.move();
+
         assertThat(car.showMyPosition()).isBetween(stopPosition, movePosition);
 
     }
--- src/test/java/racing/GameScannerTest.java
+++
@@ -1,8 +0,0 @@
-package racing;
-
-import org.junit.jupiter.api.DisplayName;
-
-@DisplayName("입력받는 메소드만 존재하여 테스트할 수 없습니다.")
-class GameScannerTest {
-
-}
