15aa980fa5074f9a877e62d9c7c2fa1eea62c28a
bbbnam <bbbnam@naver.com>
2020-05-20 21:13:57 +0900

    feat : RacingGameProgress 별도 서비스 클래스 생성

---
+++ src/main/java/study/step3/RacingGameProgress.java
@@ -0,0 +1,26 @@
+package study.step3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGameProgress {
+    private final static int INT_ONE = 1;
+
+    public static List<Round> gameStart(RacingGame racingGame, CarMoveCondition moveCondition) {
+        List<Round> rounds = new ArrayList<>();
+        Round round = initRound(racingGame.getCarEntries());
+        rounds.add(round); 
+
+        int tryTimes = racingGame.getTryTime();
+
+        for (int i = INT_ONE; i < tryTimes; i++) {
+            round = round.gameResult(moveCondition);
+            rounds.add(round);
+        }
+        return rounds;
+    }
+
+    private static Round initRound(String [] carEntries) {
+        return new Round(carEntries);
+    }
+}
