0fa0eeefda39ab4127d1c818408156a131a63480
sangyounkim <505>
2020-05-21 00:24:27 +0900

    style : 불필요한 import 제거. refactor : 출력 기능을 view로 옮겨 테스트 케이스 제거.

--- src/test/java/step3/RacingGameTest.java
+++ src/test/java/step3/RacingGameTest.java
@@ -1,18 +1,10 @@
 package step3;
 
 
-import calculator.StringCalculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import racingcar.car.Car;
 import racingcar.racinggame.RacingGame;
-
-import java.util.ArrayList;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RacingGameTest {
@@ -45,7 +37,7 @@ public class RacingGameTest {
         racingGame.makeCar(3);
         int gameCount = 5;
         racingGame.setGameCount(gameCount);
-        for(int i = 0; i < gameCount; i++){
+        for(int i = 0; i < gameCount; i++) {
             racingGame.play();
         }
 
@@ -53,20 +45,4 @@ public class RacingGameTest {
         assertThat(isEnd).isEqualTo(true);
     }
 
-    @Test
-    @DisplayName("레이싱 게임 자동차 위치 표출 테스트")
-    void racingGameCarSymbolTestFunction() {
-        String symbol = "";
-
-        racingGame.makeCar(1);
-        racingGame.setGameCount(5);
-        ArrayList<Car> cars = racingGame.getCars();
-        int carMoving = cars.get(0).getPosition();
-
-        for(int i = 0; i < carMoving; i++){
-            symbol = symbol.concat("-");
-        }
-
-        assertThat(symbol).isEqualTo(racingGame.getCarTraceSymbolByCar(cars.get(0)));
-    }
 }
