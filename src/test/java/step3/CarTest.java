17196b71e202baaedb366b36468f63434ccdcda7
sangyounkim <505>
2020-05-21 00:21:10 +0900

    fix : 자동차 테스트 실패 케이스 수정 style : 불필요한 import 제거.

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -1,39 +1,31 @@
 package step3;
 
-
-import calculator.StringCalculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.car.Car;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class CarTest {
     private Car car;
 
     @BeforeEach
     void setUp() {
-        car = new Car("testCar");
+        car = new Car();
     }
 
     @Test
     @DisplayName("정상적인 자동차 이동 테스트")
     void carMoveTestFunction() {
-        car.move(0);
-
-        assertThat(car.getPosition()).isEqualTo(5);
+        car.move(5);
+        assertThat(car.getPosition()).isEqualTo(2);
     }
 
     @Test
     @DisplayName("정상적인 자동차 비 이동 테스트")
     void carStopTestFunction() {
-        car.move(10);
+        car.move(4);
         assertThat(car.getPosition()).isEqualTo(1);
     }
 }
