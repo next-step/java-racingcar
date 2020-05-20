b56eb5bf382cfdfd89d7c6db1dacac44508f554d
bluehoped <bluehoped@hanmail.net>
2020-05-21 00:56:22 +0900

    변경 car 클래스 메소드 private으로 변경

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -3,25 +3,22 @@ package racing;
 public class Car {
     private static final int MOVE_MIN_NUMBER = 4;
     private int position;
-    
-    public Car() {
-    }
-    
+
     public void drive() {
         if (isMoveTime(Dice.cast())) {
             move();
         }
     }
 
-    void move() {
+    public int showMyPosition() {
+        return position;
+    }
+
+    private void move() {
         this.position++;
     }
 
-    boolean isMoveTime(int castNumber) {
+    private boolean isMoveTime(int castNumber) {
         return castNumber >= MOVE_MIN_NUMBER;
-    }
-
-    public int showMyPosition() {
-        return position;
     }
 }
--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -18,17 +18,4 @@ class CarTest {
         car = new Car();
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"1:false", "3:false", "5:true"}, delimiter = ':')
-    @DisplayName("주사위 값에 따라 움직 여부를 정하는 메소드 테스트")
-    void isMoveTimeTest(int input, boolean expected) {
-        assertThat(car.isMoveTime(input)).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("이동이 잘되었는지 포지션 테스트")
-    void positionTest() {
-        car.move();
-        assertThat(car.showMyPosition()).isEqualTo(1);
-    }
 }
