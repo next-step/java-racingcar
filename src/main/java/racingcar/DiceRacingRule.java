e39121071c98e3f21d8641d9b2ab4d7482b48444
junpil park <iamfeel2@gmail.com>
2020-05-21 03:53:06 +0900

    refactor (DiceRacingRule) : Dice의 값이 앞으로 전진할 지 판단하는 매직넘버를 상수화

--- src/main/java/racingcar/DiceRacingRule.java
+++ src/main/java/racingcar/DiceRacingRule.java
@@ -5,8 +5,9 @@ import racingcar.race.RacingRule;
 
 public class DiceRacingRule implements RacingRule {
 
-    public static final int STOP_MOVE = 0;
-    public static final int MOVE_FORWARD_COUNT = 1;
+    private static final int STOP_MOVE = 0;
+    private static final int MOVE_FORWARD_COUNT = 1;
+    private static final int STOP_MOVE_BOUNDS = 4;
 
     private final Dice dice;
 
@@ -17,7 +18,7 @@ public class DiceRacingRule implements RacingRule {
     @Override
     public int getMoveForward() {
         int forwardCount = MOVE_FORWARD_COUNT;
-        if(dice.generate() < 4) {
+        if(dice.generate() < STOP_MOVE_BOUNDS) {
             forwardCount = STOP_MOVE;
         }
         return forwardCount;
