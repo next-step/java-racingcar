617b609f68c81063a611f99643080e70755462b6
DongHyukki <kderr2791@gmail.com>
2020-05-20 22:03:38 +0900

    changes CarTest
    Added Car Prgress Test With Random Value

--- src/test/java/CarRacing/CarRacingTest.java
+++ src/test/java/CarRacing/CarRacingTest.java
@@ -13,7 +13,19 @@ public class CarRacingTest {
     @Test
     @DisplayName("Random Value Test")
     public void randomValueTest() {
-       assertThat(RandomValue.getRandomValue()).isBetween(0,9);
+       assertThat(new RandomValue().getRandomValue()).isBetween(0,9);
+    }
+
+    @Test
+    @DisplayName("Random Min Value Test")
+    public void randomMinValueTest() {
+        assertThat(new RandomMinValue().getRandomValue()).isGreaterThan(4);
+    }
+
+    @Test
+    @DisplayName("Random Max Value Test")
+    public void randomMaxValueTest() {
+        assertThat(new RandomMaxValue().getRandomValue()).isLessThan(4);
     }
 
     @Test
