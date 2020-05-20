54f2e00014bc6e31b2769e1253663e106551a708
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-21 01:50:38 +0900

    feat(RacingCar) : create move

---
+++ src/main/java/step3/RacingCar.java
@@ -0,0 +1,15 @@
+package step3;
+
+public class RacingCar {
+
+  private int distance;
+
+  public int getDistance() {
+    return distance;
+  };
+
+  public void move() {
+    distance++;
+  }
+
+}
--- src/test/java/step3/RacingCarTest.java
+++ src/test/java/step3/RacingCarTest.java
@@ -1,10 +1,14 @@
 package step3;
 
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class RacingCarTest {
+  RacingCar racingCar = new RacingCar();
+
   @Test
   void move() {
-
+    racingCar.move();
+    assertThat(racingCar.getDistance()).isEqualTo(1);
   }
 }
