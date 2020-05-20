6e94db5dc0bf9dc2443b34066d6b9ae6d71d7f35
bo0216 <bo216@naver.com>
2020-05-20 22:39:34 +0900

    refactor: add setUp()

--- src/test/java/racing/RandomNumberTest.java
+++ src/test/java/racing/RandomNumberTest.java
@@ -1,17 +1,26 @@
 package racing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RandomNumberTest {
+    private RandomNumber randomNumber;
+
+    @BeforeEach
+    void setUp() {
+        randomNumber = new RandomNumber();
+
+    }
 
     @Test
     @DisplayName("Random값이 0-9사이 인지")
-    void addTest() {
-        RandomNumber randomNumber = new RandomNumber();
-        assertTrue(0<= randomNumber.getRandomNumber()
-                    &&randomNumber.getRandomNumber() < 10);
+    void getRandomNumberTest() {
+        assertTrue(0 <= randomNumber.getRandomNumber()
+                && randomNumber.getRandomNumber() < 10);
     }
 }
