e2a3f62743f15151d7875fa77d1ed03bf859915f
김다혜 <dahye432111@gmail.com>
2020-05-20 23:13:10 +0900

    feat: Car 객체에 이름 필드 추가

--- src/main/java/com/kimdahyeee/racing/Car.java
+++ src/main/java/com/kimdahyeee/racing/Car.java
@@ -6,9 +6,14 @@ public class Car {
     private static final int GO_STRAIGHT_LIMIT = 4;
     private static final int RANDOM_BOUND = 10;
     private int position;
+    private String name;
 
     public Integer getPosition() {
         return position;
+    }
+
+    public String getName() {
+        return name;
     }
 
     protected void move() {
