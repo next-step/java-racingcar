481a1b65ddcf7a372f6463fef75671db75405a11
junpil park <iamfeel2@gmail.com>
2020-05-20 23:49:45 +0900

    refactor (InputView) : 상수에 final 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,10 +3,10 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static String TEXT_QUESTION_CAR_NAME_JOIN_RACE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private static String TEXT_QUESTION_RACING_TIME = "시도할 회수는 몇 회 인가요?";
+    private final static String TEXT_QUESTION_CAR_NAME_JOIN_RACE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private final static String TEXT_QUESTION_RACING_TIME = "시도할 회수는 몇 회 인가요?";
 
-    private static Scanner scanner = new Scanner(System.in);
+    private final static Scanner scanner = new Scanner(System.in);
 
     private InputView() {
 
