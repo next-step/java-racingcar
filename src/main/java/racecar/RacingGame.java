24e736e06c3a909b550a47d1a659587b49ff3a48
eunji <12@>
2020-05-21 21:23:19 +0900

    게임 실행 로직 추가
    입력, 준비, 실행, 결과발표

--- src/main/java/racecar/RacingGame.java
+++ src/main/java/racecar/RacingGame.java
@@ -1,40 +1,48 @@
 package racecar;
 
 import java.util.List;
+import java.util.Random;
 
 public class RacingGame {
-    private static final int WIN_CONDITION = 4;
+
 
     public static void main(String[] args) {
-
-        int carCount = InputView.askNumberCount();
+        //입력
+        String[] carName = InputView.askCarNames();
         int repeatCount = InputView.askRepeatCount();
 
-        //자동차 생성
-        Car.generate(carCount);
-        List<Car> carList = Car.getCarList();
+        //게임 준비
+        RacingGame racingGame = new RacingGame();
+        List<Car> carList = racingGame.ready(carName);
 
         //게임 실행
-        RacingGame racingGame = new RacingGame();
         for (int i = 0; i < repeatCount; i++) {
-            racingGame.run(carList);
-            System.out.println("");
+            racingGame.start(carList);
         }
 
+        //우승자 발표
+        racingGame.announceWinner();
+
     }
 
-    public static boolean checkPass(int number) {
-        boolean result = number >= WIN_CONDITION;
-        return result;
+    public List<Car> ready(String[] carName) {
+        List<Car> carList = Car.ready(carName);
+        return carList;
     }
 
-    public void run(List<Car> carList) {
-        Randomizer randomizer = new Randomizer();
+    public void start(List<Car> carList) {
+        Random random = new Random();
         for (Car car : carList) {
-            int fuel = randomizer.generateNumber();
-            car.move(fuel);
+            car.move(random);
             ResultView.print(car);
         }
+        System.out.println();
+    }
+
+    public void announceWinner() {
+        int max = Car.getMaxPosition();
+        List<String> winners = Car.selectWinner(max);
+        ResultView.printWinner(winners);
     }
 
 }
