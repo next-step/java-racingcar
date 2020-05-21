5cf0eb01fb591944c674c4c99ddc82e05873bcaa
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-22 01:06:39 +0900

    refactor: change input type from Integer to String to get car name

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class InputView {
 
-  public static String carInputMessage = "자동차 대수는 몇 대인가요?";
+  public static String carInputMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표 및 띄어쓰기로 기준)";
   public static String attemptInputMessage = "시도할 회수는 몇 회인가요?";
 
-  public static Integer getUserInput(String message) {
+  public static Integer getUserNumInput(String message) {
     Scanner scanner = new Scanner(System.in);
     System.out.println(message);
     return scanner.nextInt();
+  }
+
+
+  public static String getUserStringInput(String message) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(message);
+    return scanner.nextLine();
   }
 }
