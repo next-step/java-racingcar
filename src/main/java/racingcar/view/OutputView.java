79cbda47692ba7fc7b26185966f327b5983bbf97
gwonsungjun <sungjunpizz@gmail.com>
2020-05-21 12:57:02 +0900

    refactor(OutputView): view 패키지의 객체가 domain 패키지 객체에 의존
    - 기존에는 GameResult 클래스에서 OutputView를 의존하였지만 리팩토링을 진행하며 OutputView 클래스가 도메인 패키지의 객체에 의존하도록 수정
    - 따라서, printOutWinner 메소드에서 winner name을 joining 하게됨

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,18 +1,26 @@
 package racingcar.view;
 
-import racingcar.domain.GameResult;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
     public static void printMessage() {
         System.out.println("\n실행 결과");
     }
 
-    public static void printOut(GameResult gameResult) {
-        gameResult.getResult().forEach(System.out::println);
-        System.out.println();
+    public static void printOutResult(List<String> gameResults) {
+        gameResults.forEach(gameResult -> System.out.println(gameResult + "\n"));
     }
 
-    public static void printOutWinner(String winners) {
-        System.out.println(winners + "가 최종 우승했습니다.");
+    public static void printOutWinner(Cars winners) {
+        String winnerNames = winners.getCars()
+                .stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(","));
+
+        System.out.println(winnerNames + "가 최종 우승했습니다.");
     }
 }
