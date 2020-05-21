0d20027da91c5847685582957d0c7827659eda44
eunji <12@>
2020-05-21 17:14:51 +0900

    이름을 입력받아 CAR객체 생성하게 수정, move 메서드 변경

--- src/main/java/racecar/Car.java
+++ src/main/java/racecar/Car.java
@@ -2,29 +2,43 @@ package racecar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Car {
     private static List<Car> carList;
-
+    private String name;
     private int position;
 
     public Car() {
-        position = 1;
+        this.position = 1;
     }
 
-    public static void generate(int carCount) {
+    public Car(String name) {
+        this.name = name;
+        this.position = 1;
+    }
+
+    public static void ready(int carCount) {
         carList = new ArrayList<>();
         for (int i = 0; i < carCount; i++)
             carList.add(new Car());
     }
 
+    public static List<Car> ready(String[] carName) {
+        carList = new ArrayList<>();
+        for (int i = 0, carSize = carName.length; i < carSize; i++)
+            carList.add(new Car(carName[i]));
+        return carList;
+    }
+
+
     public static List<Car> getCarList() {
         return carList;
     }
 
-    public int move(int fuel) {
-        if (RacingGame.checkPass(fuel))
-            this.position = this.position + 1;
+    public int move(Random random) {
+        if (random.nextInt(Rules.BOUND) >= Rules.WIN_CONDITION)
+            this.position += 1;
         return this.position;
     }
 
