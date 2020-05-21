471820667d12d9bcb92e83cf0a5572db7af20da9
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-21 16:18:41 +0900

    style : Test 파일들 띄어쓰기 수정

--- src/test/java/step3/racingcar/CarGroupsTest.java
+++ src/test/java/step3/racingcar/CarGroupsTest.java
@@ -1,18 +1,14 @@
 package step3.racingcar;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarGroupsTest {
