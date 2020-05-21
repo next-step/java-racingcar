0f9ae3c4dfb77ae27305553dbbf20e243bcd61c0
gwonsungjun <sungjunpizz@gmail.com>
2020-05-21 12:59:13 +0900

    test(GameResultTest): displayWinner TC 삭제
    - display 관련 기능이 OutputView로 이동하면서 기존 display 테스트 삭제

--- src/test/java/racingcar/domain/GameResultTest.java
+++ src/test/java/racingcar/domain/GameResultTest.java
@@ -3,10 +3,6 @@ package racingcar.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -18,38 +14,19 @@ public class GameResultTest {
     void getResult() {
         // given
         String carName1 = "테스터";
-        String carName2 = "테스터2";
 
-        Cars cars = Cars.createCars(carName1.concat(",").concat(carName2));
+        Cars cars = Cars.createCars(carName1);
         cars.carRacing(new FixedNumberStrategy());
 
         GameResult gameResult = new GameResult(cars);
 
         // when
-        List<String> actual = gameResult.getResult();
+        String actual = gameResult.getResult();
 
         // then
         assertAll(
                 () -> assertThat(actual).isNotNull(),
-                () -> assertThat(actual).hasSize(cars.getSize()),
-                () -> assertThat(actual.get(0)).isEqualTo(carName1 + " : -")
+                () -> assertThat(actual).isEqualTo(carName1 + " : -" )
         );
-    }
-
-    @Test
-    @DisplayName("displayWinner 메소드는 쉼표로 구분된 우승자 이름을 출력한다.")
-    void displayWinner() {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        String carNames = "테스터1,테스터2";
-        Cars cars = Cars.createCars(carNames);
-
-        GameResult gameResult = new GameResult(cars);
-        gameResult.displayWinner(cars);
-
-        String expectedOutput = carNames + "가 최종 우승했습니다.\n";
-
-        assertThat(expectedOutput).isEqualTo(outContent.toString());
     }
 }
