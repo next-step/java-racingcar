cd53dfe034d9479cf70bc7cc9e74508670a5b3db
eunji <12@>
2020-05-21 16:59:03 +0900

    자동차 이름 입력 메서드 추가

--- src/main/java/racecar/InputView.java
+++ src/main/java/racecar/InputView.java
@@ -3,22 +3,28 @@ package racecar;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner;
     private InputView() {
     }
 
     public static int askNumberCount() {
-        scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("자동차 대수는 몇 대 인가요?");
         int carCount = scanner.nextInt();
         return carCount;
     }
 
     public static int askRepeatCount() {
-        scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("시도할 회수는 몇 회 인가요?");
         int repeatCount = scanner.nextInt();
         return repeatCount;
     }
 
+    public static String[] askCarNames(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String input = scanner.nextLine();
+        String[] carNames = input.split(",");
+        return carNames;
+    }
 }
