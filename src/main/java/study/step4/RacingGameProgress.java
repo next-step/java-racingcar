c2424139963954a94827166ddda73f0d586c0cb7
bbbnam <bbbnam@naver.com>
2020-05-22 20:30:18 +0900

    refactor : RacingGameProgress 클래스 리팩토링

---
+++ src/main/java/study/step4/RacingGameProgress.java
@@ -0,0 +1,27 @@
+package study.step4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGameProgress {
+    private final static int INT_ZERO = 0;
+
+    private final RacingGame racingGame;
+
+    public RacingGameProgress(RacingGame racingGame) {
+        this.racingGame = racingGame;
+    }
+
+    public List<Round> gameStart(CarMoveCondition moveCondition) {
+        List<Round> rounds = new ArrayList<>();
+        Round round = new Round(racingGame.getCarEntries());
+
+        int tryTimes = racingGame.getTryTime();
+
+        for (int i = INT_ZERO; i < tryTimes; i++) {
+            round = round.gameProgress(moveCondition);
+            rounds.add(round);
+        }
+        return rounds;
+    }
+}
