5cdb890b9a6c8cf44d5330b44b8b42708a25ad98
bbbnam <bbbnam@naver.com>
2020-05-22 20:31:33 +0900

    refactor : RandomCarMoveCondition클래스 리팩토링

---
+++ src/main/java/study/step4/RandomCarMoveCondition.java
@@ -0,0 +1,14 @@
+package study.step4;
+
+import java.util.Random;
+
+public class RandomCarMoveCondition implements CarMoveCondition {
+    private final static int RANDOM_NUMBER_LIMIT = 10;
+    private final static int MOVE_CONDITION_NUMBER = 4;
+    private final static Random random = new Random();
+
+    @Override
+    public boolean isCarMoveCondition() {
+        return random.nextInt(RANDOM_NUMBER_LIMIT) >= MOVE_CONDITION_NUMBER;
+    }
+}
