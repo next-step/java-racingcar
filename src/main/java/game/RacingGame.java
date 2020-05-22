ec96a352e0fecd6021e7492dff85c49de483d6d8
하재권 <hazxz@naver.com>
2020-05-22 12:32:02 +0900

    refactoring(racingGame): RacingGame::play 함수의 로직을 메소드 분리

--- src/main/java/game/RacingGame.java
+++ src/main/java/game/RacingGame.java
@@ -3,13 +3,14 @@ package game;
 import game.ui.RacingGameInputView;
 import game.ui.RacingGameResultView;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RacingGame {
     private static final int INIT_POSITION = 0;
+    private static final int ROUND_COMMENT_OFFSET = 1;
 
     private final MovePolicy movePolicy;
     private final RacingGameInputView racingGameInputView;
@@ -22,17 +23,32 @@ public class RacingGame {
     }
 
     public void play() {
+        List<Car> carList = setUpCarList();
+        int time = setUpTime();
+
+        IntStream.range(0, time).forEach(i -> playOnce(i, carList));
+
+        announceWinner(carList);
+    }
+
+    private List<Car> setUpCarList() {
         List<CarName> carNameList = racingGameInputView.getCarNameList();
-        int time = racingGameInputView.getTime();
-        List<Car> carList = new ArrayList<>();
-        carNameList.forEach(carName -> carList.add(new Car(carName, INIT_POSITION, movePolicy)));
+        return carNameList.stream().map(carName -> {
+            return new Car(carName, INIT_POSITION, movePolicy);
+        }).collect(Collectors.toList());
+    }
 
-        for (int i = 0; i < time; i++) {
-            racingGameResultView.printRound(i + 1);
-            carList.forEach(Car::move);
-            carList.forEach(racingGameResultView::printResult);
-        }
+    private int setUpTime() {
+        return racingGameInputView.getTime();
+    }
 
+    private void playOnce(int round, List<Car> carList) {
+        racingGameResultView.printRound(round + ROUND_COMMENT_OFFSET);
+        carList.forEach(Car::move);
+        carList.forEach(racingGameResultView::printResult);
+    }
+
+    private void announceWinner(List<Car> carList) {
         int winScore = carList.stream().max(Comparator.comparingInt(Car::getPosition)).map(Car::getPosition).orElse(INIT_POSITION);
         racingGameResultView.announceWinner(carList.stream().filter(c -> c.getPosition() >= winScore).collect(Collectors.toList()));
     }
