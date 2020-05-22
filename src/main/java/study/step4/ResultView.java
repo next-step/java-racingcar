b7f5b654cbefb97f0c02160599cba3cd5e0f2b1b
bbbnam <bbbnam@naver.com>
2020-05-22 20:32:03 +0900

    refactor : ResultView 클래스 리팩토링

---
+++ src/main/java/study/step4/ResultView.java
@@ -0,0 +1,45 @@
+package study.step4;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ResultView {
+    private static final String UNDER_BAR_SYMBOL ="_";
+    private static final String CAR_NAME_DELIMITER =" : ";
+    private static final String CHAMPION_RESULT_MESSAGE ="가 최종 우승했습니다.";
+    private static final int INIT_ZERO = 0;
+
+    public static void racingResultPrint(List<Round> rounds) {
+        for (Round round : rounds){
+            carToString(round.getCars());
+            System.out.println();
+        }
+        Round lastRound = rounds.get(rounds.size()-1);
+        printChampion(new Champion(lastRound).getChampions());
+    }
+
+    private static void carToString(List<Car> cars) {
+        for (Car car: cars){
+            System.out.println(car.getCarName()+CAR_NAME_DELIMITER+convertToUnderBarSymbol(car.getCarPosition()));
+        }
+    }
+
+   private static String convertToUnderBarSymbol(int pointInTimeCarPostion) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = INIT_ZERO; i < pointInTimeCarPostion; i++){
+            stringBuilder.append(UNDER_BAR_SYMBOL);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void printChampion(List<String> champions) {
+        String champion = convertListToString(champions);
+        System.out.println(champion+CHAMPION_RESULT_MESSAGE);
+    }
+
+    private static String convertListToString(List<String> champions) {
+        return champions.stream()
+                        .map(Object::toString)
+                        .collect(Collectors.joining(", "));
+    }
+}
