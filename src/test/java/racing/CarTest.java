73fec229845422c8ef3ba4222f9113a102f79d60
bo0216 <bo216@naver.com>
2020-05-24 03:14:08 +0900

    refactor: 필요없는 import 제거

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,12 +1,8 @@
 package racing;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
