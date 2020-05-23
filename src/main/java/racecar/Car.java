984c88409edae512773910963a4d5b6fc2042e37
eunji <12@>
2020-05-23 11:22:14 +0900

    refactor Car 생성, CarList 관리 책임 분리

--- src/main/java/racecar/Car.java
+++ src/main/java/racecar/Car.java
@@ -1,12 +1,8 @@
 package racecar;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 public class Car {
-    private static List<Car> carList;
     private String name;
     private int position;
 
@@ -15,28 +11,8 @@ public class Car {
         this.position = 1;
     }
 
-    public static List<Car> ready(String[] carName) {
-        carList = new ArrayList<>();
-        for (int i = 0, carSize = carName.length; i < carSize; i++)
-            carList.add(new Car(carName[i].trim()));
-        return carList;
-    }
-
-    public static int getMaxPosition() {
-        List<Integer> positions = new ArrayList<>();
-        for (Car car : carList)
-            positions.add(car.position);
-        int max = Collections.max(positions);
-        return max;
-    }
-
-    public static List<String> selectWinner(int max) {
-        List<String> winners = new ArrayList<>();
-        for (Car car : carList) {
-            if (car.position == max)
-                winners.add(car.name);
-        }
-        return winners;
+    public static Car generate(String name){
+        return new Car(name.trim());
     }
 
     public int move(Random random) {
@@ -46,7 +22,7 @@ public class Car {
         return this.position;
     }
 
-    public String makePositionStatus() {
+    private String makePositionStatus() {
         String status = "-".repeat(position);
         return status;
     }
@@ -54,5 +30,13 @@ public class Car {
     public String printStatus() {
         String result = name + " : " + makePositionStatus();
         return result;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
--- src/main/java/racecar/RacingGame.java
+++ src/main/java/racecar/RacingGame.java
@@ -1,10 +1,11 @@
 package racecar;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class RacingGame {
-
 
     public static void main(String[] args) {
         //입력
@@ -18,31 +19,50 @@ public class RacingGame {
         //게임 실행
         for (int i = 0; i < repeatCount; i++) {
             racingGame.start(carList);
+            System.out.println();
         }
 
         //우승자 발표
-        racingGame.announceWinner();
+        racingGame.announceWinner(carList);
 
     }
 
-    public List<Car> ready(String[] carName) {
-        List<Car> carList = Car.ready(carName);
+    private List<Car> ready(String[] carName) {
+        List<Car> carList = new ArrayList<>();
+        for (String name : carName) {
+            carList.add(Car.generate(name));
+        }
         return carList;
     }
 
-    public void start(List<Car> carList) {
+    private void start(List<Car> carList) {
         Random random = new Random();
         for (Car car : carList) {
             car.move(random);
             ResultView.printStatus(car);
         }
-        System.out.println();
     }
 
-    public void announceWinner() {
-        int max = Car.getMaxPosition();
-        List<String> winners = Car.selectWinner(max);
+    private void announceWinner(List<Car> carList) {
+        int max = getWinScore(carList);
+        List<String> winners = selectWinner(carList, max);
         ResultView.printWinner(winners);
     }
 
+    private int getWinScore(List<Car> carList) {
+        List<Integer> scores = new ArrayList<>();
+        for (Car car : carList)
+            scores.add(car.getPosition());
+        int max = Collections.max(scores);
+        return max;
+    }
+
+    private List<String> selectWinner(List<Car> carList, int max) {
+        List<String> winners = new ArrayList<>();
+        for (Car car : carList) {
+            if (max == car.getPosition())
+                winners.add(car.getName());
+        }
+        return winners;
+    }
 }
