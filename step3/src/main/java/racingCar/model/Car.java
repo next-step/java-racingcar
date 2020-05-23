ad53b53f89e748cf467000a329025ca2ea820b1c
kyooho Lee <295_92@naver.com>
2020-05-23 22:52:00 +0900

    Car 구현2

--- step3/src/main/java/racingCar/model/Car.java
+++ step3/src/main/java/racingCar/model/Car.java
@@ -1,4 +1,19 @@
-package src.main.java.racingCar.model;
+package main.java.racingCar.model;
 
 public class Car {
+    private static final int DEFAULT_POSITION = 0;
+    private static final int RANDOM_CUT = 4;
+    private static final int RANDOM_BOUND = 10;
+    private int position;
+
+    public Car(){
+        position=DEFAULT_POSITION;
+    }
+    public int getPosition() {
+        return position;
+    }
+    void move(){
+        int dice =(int)(Math.random()*RANDOM_BOUND);
+        position+= dice>=RANDOM_CUT?1:0;
+    }
 }
