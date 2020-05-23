4da3bda8701e02c6783ab641dd8ba51176f9b6e0
kyooho Lee <295_92@naver.com>
2020-05-23 23:44:37 +0900

    Output Viewer 구현

--- step3/src/main/java/racingCar/view/OutputViewer.java
+++ step3/src/main/java/racingCar/view/OutputViewer.java
@@ -1,5 +1,9 @@
 package main.java.racingCar.view;
 
 public class OutputViewer {
-    void resultOutputViewer(){}
+    public static void resultOutputViewer(int position){
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        } System.out.println("");
+    }
 }
