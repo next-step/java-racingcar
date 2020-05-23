826bd6d1265a130494633279b8dcd9815c782331
김다혜 <dahye432111@gmail.com>
2020-05-23 18:50:30 +0900

    refactor: 불필요한 객체 생성 제거

--- src/main/java/com/kimdahyeee/racing/view/InputView.java
+++ src/main/java/com/kimdahyeee/racing/view/InputView.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String NAME_SEPARATOR = ",";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> getCarNames() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String names = scanner.nextLine();
 
@@ -24,7 +24,6 @@ public class InputView {
     }
 
     public static int getTryCount() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("시도할 회수는 몇 회 인가요?");
         int tryCount = scanner.nextInt();
 
