3596da0b2e4864c98d9432c7d7470d19a07d990a
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-23 12:22:21 +0900

    refactor: RacingGame to add Game before creating GameController

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import racingcar.controller.GameController;
+import racingcar.domain.Cars;
 import racingcar.domain.Game;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
@@ -11,7 +12,8 @@ public class RacingGame {
     String[] names = InputView.getUserStringInput(InputView.CAR_INPUT_MESSAGE).split(", ");
     int attemptNum = InputView.getUserNumInput(InputView.ATTEMPT_INPUT_MESSAGE);
 
-    GameController gameController = GameController.create(attemptNum, names);
+    Game beforeGame = Game.create(Cars.create(names));
+    GameController gameController = GameController.create(attemptNum, beforeGame);
     Game playedGame = gameController.proceedGame();
     ResultView resultView = ResultView.create(playedGame);
     resultView.printView();
