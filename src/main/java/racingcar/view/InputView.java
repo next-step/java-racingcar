cb99c42964d077ce9a630e88e25fe281099d5c05
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-23 09:58:08 +0900

    refactor: static messages to keep convention

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class InputView {
 
-  public static String carInputMessage = "경주할 자동차 이름을 입력하세요.(이름은 쉼표 및 띄어쓰기로 기준)";
-  public static String attemptInputMessage = "시도할 회수는 몇 회인가요?";
+  public static String CAR_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표 및 띄어쓰기로 기준)";
+  public static String ATTEMPT_INPUT_MESSAGE = "시도할 회수는 몇 회인가요?";
 
   public static Integer getUserNumInput(String message) {
     Scanner scanner = new Scanner(System.in);
