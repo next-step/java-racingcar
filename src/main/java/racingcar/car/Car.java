f31a89508f149ef7237d0608a657436662dc2400
sangyounkim <505>
2020-05-24 01:21:43 +0900

    feat : 자동차 이름 설정 기능 추가.

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -5,17 +5,20 @@ import java.util.Random;
 public class Car {
 	private int position;
 	private int moveAbility;
+	private String name;
+
 	public static final int DEFAULT_CAR_POSITION = 1;
 	public static final int DEFAULT_CAR_ABILITY = 1;
 	public static final int DEFAULT_VALID_MOVING_NUMBER = 4;
 
-	public Car() {
-		this(DEFAULT_CAR_POSITION, DEFAULT_CAR_ABILITY);
+	public Car(String name) {
+		this(DEFAULT_CAR_POSITION, DEFAULT_CAR_ABILITY, name);
 	}
 	
-	public Car(int position , int ability) {
+	public Car(int position, int ability, String name) {
 		this.position = position;
 		this.moveAbility = ability;
+		this.name = name;
 	}
 	
 	public void move(int power) {
@@ -27,4 +30,9 @@ public class Car {
 	public int getPosition() {
 		return this.position;
 	}
+
+	public String getName() {
+		return this.name;
+	}
+
 }
