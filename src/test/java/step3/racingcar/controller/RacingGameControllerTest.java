aacf4f3298e2345a1440586e76c08b074e5cb914
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-05-23 20:00:29 +0900

    test (RacingGameControllerTest) controller의 작동 순서가 올바르지 않을때 에러 테스트

---
+++ src/test/java/step3/racingcar/controller/RacingGameControllerTest.java
@@ -0,0 +1,36 @@
+package step3.racingcar.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step3.racingcar.view.InputView;
+import step3.racingcar.view.ResultView;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class RacingGameControllerTest {
+
+    @DisplayName("Controller 객체 생성 테스트")
+    @Test
+    public void makeRacingGameController() {
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        assertDoesNotThrow(() -> {
+            RacingGameController racingGameController = new RacingGameController(inputView, resultView);
+        });
+    }
+
+    @DisplayName("RacingGame 객체 없을 때 Controller Test 에러 발생")
+    @Test
+    public void throwExceptionWhenRacingGameIsNull() {
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+
+        RacingGameController racingGameController = new RacingGameController(inputView, resultView);
+        assertThatExceptionOfType()
+                .isThrownBy(() - {
+                    racingGameController.startGame();
+                });
+    }
+}
