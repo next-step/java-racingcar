f1db99177976df83396f08ca5a5e7396461b3fb9
sangyounkim <505>
2020-05-24 20:05:53 +0900

    feat : 자동차 이름 셋팅 추가. style : 변수 순서 변경 ( 상수 > 맴버 변수 )

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,26 +1,24 @@
 package racingcar.car;
 
-import java.util.Random;
-
 public class Car {
-	private int position;
-	private int moveAbility;
-	private String name;
-
 	public static final int DEFAULT_CAR_POSITION = 1;
 	public static final int DEFAULT_CAR_ABILITY = 1;
 	public static final int DEFAULT_VALID_MOVING_NUMBER = 4;
 
+	private int position;
+	private int moveAbility;
+	private String name;
+
 	public Car(String name) {
 		this(DEFAULT_CAR_POSITION, DEFAULT_CAR_ABILITY, name);
 	}
-	
+
 	public Car(int position, int ability, String name) {
 		this.position = position;
 		this.moveAbility = ability;
 		this.name = name;
 	}
-	
+
 	public void move(int power) {
 		if (power > DEFAULT_VALID_MOVING_NUMBER) {
 			position += moveAbility;
