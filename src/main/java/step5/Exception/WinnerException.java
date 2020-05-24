6e0b1fd36e05d0c3afd90f492a1374bed6d50280
junilHwang <tjsdlf4261@naver.com>
2020-05-24 22:57:34 +0900

    [Step5 - 자동차 경주 리팩토링] refactor: Exception -> RuntimeException Extend로 변경

--- src/main/java/step5/Exception/WinnerException.java
+++ src/main/java/step5/Exception/WinnerException.java
@@ -1,6 +1,6 @@
 package step5.Exception;
 
-public class WinnerException extends Exception {
+public class WinnerException extends RuntimeException {
 
   public WinnerException() {
     this("아직 경기가 종료되지 않았습니다.");
