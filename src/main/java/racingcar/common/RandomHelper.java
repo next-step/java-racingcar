7cc50f70fc9d229dbba77cfe14a0b59b9e6c72b0
sangyounkim <505>
2020-05-25 02:38:43 +0900

    fix : 랜덤 객체 스테틱 맴버 변수로 수정.

--- src/main/java/racingcar/common/RandomHelper.java
+++ src/main/java/racingcar/common/RandomHelper.java
@@ -3,9 +3,9 @@ package racingcar.common;
 import java.util.Random;
 
 public class RandomHelper {
+    static Random random = new Random();
 
     public static int getRandomNumber(int num) {
-        Random random = new Random();
         return random.nextInt(num);
     }
 }
