e55de2f1b568485afe8df257b6f58efc1c675c00
bbbnam <bbbnam@naver.com>
2020-05-24 13:06:51 +0900

    refactor : 도메인 로직 패키지 분리

---
+++ src/main/java/study/step5/domain/RacingGameProgress.java
@@ -0,0 +1,36 @@
+package study.step5.domain;
+
+import study.step5.dto.RacingGameResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGameProgress {
+    private final static int INT_ZERO = 0;
+
+    private final RacingGame racingGame;
+
+    public RacingGameProgress(RacingGame racingGame) {
+        this.racingGame = racingGame;
+    }
+
+    public List<Round> progressRound(CarMoveCondition moveCondition) {
+        List<Round> rounds = new ArrayList<>();
+        Round round = new Round(racingGame.getCarEntries());
+        int tryTimes = racingGame.getTryTime();
+
+        for (int i = INT_ZERO; i < tryTimes; i++) {
+            round = round.carsRun(moveCondition);
+            rounds.add(round);
+        }
+        return rounds;
+    }
+
+    public RacingGameResult runGames(CarMoveCondition moveCondition){
+        List<Round> roundResults = progressRound(moveCondition);
+        Round lastRound = roundResults.get(roundResults.size() - 1);
+        return new RacingGameResult(roundResults, lastRound.decideChampion());
+    }
+
+
+}
