95553af94cd093775d762c9a1bc2e044f4f3ac1e
bluehoped <bluehoped@hanmail.net>
2020-05-24 22:07:58 +0900

    refactor Car move() 리턴 타입 변경 void -> int

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -4,14 +4,12 @@ public class Car {
     private static final int MOVE_MIN_NUMBER = 4;
     private int position;
 
-    public int showMyPosition() {
-        return position;
-    }
-
-    public void move(int diceNumber) {
+    public int move(int diceNumber) {
         if (isMoveTime(diceNumber)) {
-            this.position++;
+            position++;
         }
+
+        return position;
     }
 
     private boolean isMoveTime(int diceNumber) {
