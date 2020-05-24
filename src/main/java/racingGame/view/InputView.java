a6aca7cd91230658d24a3215aba75efedb7fded6
hy43 <hyde0401@gmail.com>
2020-05-24 15:07:40 +0900

    delete StringUtils Integer.parseInt 메소드의 불필요한 모듈화 제거

--- src/main/java/racingGame/view/InputView.java
+++ src/main/java/racingGame/view/InputView.java
@@ -2,16 +2,14 @@ package racingGame.view;
 
 import java.util.Scanner;
 
-import racingGame.utils.StringUtils;
-
 public class InputView {
 	public static int inputCountOfCars(Scanner sc) {
 		System.out.println("자동차의 대수는 몇 대 인가요?");
-		return StringUtils.parseInt(sc.nextLine());
+		return Integer.parseInt(sc.nextLine());
 	}
 	
 	public static int inputCountOfTimes(Scanner sc) {
 		System.out.println("시도할 횟수는 몇 회 인가요?");
-		return  StringUtils.parseInt(sc.nextLine());
+		return Integer.parseInt(sc.nextLine());
 	}
 }
