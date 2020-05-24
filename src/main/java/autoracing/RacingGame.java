29b8f36cec0800a23286c11be3200d68fae9a22a
DominiqN <dmut7691@gmail.com>
2020-05-24 21:00:21 +0900

    feat: add basic RacingGame class

---
+++ src/main/java/autoracing/RacingGame.java
@@ -0,0 +1,26 @@
+package autoracing;
+
+import java.util.Scanner;
+
+public class RacingGame {
+    private static final String QUESTION_NUMBER_OF_CARS = "자동차 대수는 몇 대인가요?";
+    private static final String QUESTION_ROUNDS = "시도할 횟수는 몇 회인가요?";
+
+    private int numberOfCars;
+    private int rounds;
+
+    public RacingGame(int numberOfCars, int rounds) {
+        this.numberOfCars = numberOfCars;
+        this.rounds = rounds;
+    }
+
+    public static RacingGame inputFromConsole() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(QUESTION_NUMBER_OF_CARS);
+        int numberOfCars = Integer.parseInt(scan.nextLine().trim());
+        System.out.println(QUESTION_ROUNDS);
+        int rounds = Integer.parseInt(scan.nextLine().trim());
+        scan.close();
+        return new RacingGame(numberOfCars, rounds);
+    }
+}
