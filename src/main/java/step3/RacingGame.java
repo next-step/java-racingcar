dd8fae11fa0acd0cbf816a77656618e128f818b1
Ho <doitluke@naver.com>
2020-05-25 03:15:39 +0900

    refactor : outputView를 해당위치로 옮김

--- src/main/java/step3/RacingGame.java
+++ src/main/java/step3/RacingGame.java
@@ -1,24 +1,25 @@
 package step3;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class RacingGame {
 
     public static void main(String[] args) {
 
-        InputView inputView = new InputView();
-        Cars cars = new Cars();
+        InputView inputView = new InputView(new Scanner(System.in));
+        inputView.getUserInput();
+        Cars cars = new Cars(inputView.getCarCount());
 
-        inputView.scanner();
-        cars.makeCars(inputView.getCarCount());
-
+        OutputView outputView = new OutputView();
+        DiceWithRandom dice = new DiceWithRandom();
         for (int time = 0; time < inputView.getInputTime(); time++) {
-            cars.playGame();
-            System.out.println("============"+(time +1) +"실행 끝==================");
+
+            List<Car> carList = cars.playGame(dice);
+            outputView.resultPrint(carList);
+            System.out.println("============" + (time + 1) + "실행 끝==================");
         }
     }
-
 
 
 }
