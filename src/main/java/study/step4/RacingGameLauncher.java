193cadeed12d1534a8711408562275ab05cddd89
bbbnam <bbbnam@naver.com>
2020-05-24 12:52:22 +0900

    refactor : RacingGameLauncher 클래스 리팩토링

--- src/main/java/study/step4/RacingGameLauncher.java
+++ src/main/java/study/step4/RacingGameLauncher.java
@@ -4,9 +4,9 @@ public class RacingGameLauncher {
 
     public static void main(String[] args) {
         RacingGame racingGame = new RacingGame(InputView.inputCarNames() , InputView.inputTryTime());
-
         RacingGameProgress racingGameProgress = new RacingGameProgress(racingGame);
+        RacingGameResult racingGameResult = racingGameProgress.runGames(new RandomCarMoveCondition());
 
-        ResultView.racingResultPrint(racingGameProgress.gameStart(new RandomCarMoveCondition()));
+        ResultView.racingResultPrint(racingGameResult);
     }
 }
