343ab9721d94f2f00a59e773cd0aa86d2cf66437
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-25 16:53:26 +0900

    refactor(RacingCarFactory) : 주석 추가

--- src/main/java/step3/RacingCarFactory.java
+++ src/main/java/step3/RacingCarFactory.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class RacingCarFactory {
   private RacingCarFactory() {}
-
+  
+  /**
+   * numberOfCars 에 따라 자동차 인스턴스 생성
+   * @param numberOfCars : int 생성할 자동차 개수
+   * @return 생성된 자동차 인스턴스의 List 반환
+   */
   public static List<Car> createRacingCarList(int numberOfCars) {
     List<Car> racingCarList = new ArrayList<>();
 
