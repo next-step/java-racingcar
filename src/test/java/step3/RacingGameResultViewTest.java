9a2a7e7a3df9546a13887b43996cd665988e60ef
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-25 22:33:44 +0900

    feat(RacingGameResultView) : 게임 결과를 보는 기능 추가

--- src/test/java/step3/RacingGameResultViewTest.java
+++ src/test/java/step3/RacingGameResultViewTest.java
@@ -1,5 +1,27 @@
 package step3;
 
-public class RacingGameResultViewTest {
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class RacingGameResultViewTest {
+  @Test
+  void printResultMessage() {
+    RacingGameResultView.printDistance(5);
+  }
+
+  @ParameterizedTest
+  @CsvSource({"3, 20"})
+  void printDistancesOfRacingCarList(int numberOfCars, int time) {
+    List<Car> racingCarList = RacingCarFactory.createRacingCarList(numberOfCars);
+
+    for (int i = 0; i < time; i++) {
+      for (Car racingCar : racingCarList) {
+        racingCar.move();
+      }
+      RacingGameResultView.printDistancesOfRacingCarList(racingCarList);
+    }
+
+  }
 }
