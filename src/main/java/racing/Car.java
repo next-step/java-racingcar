24e5c525cf90da0440a80ba6c72eb0a2ef0e0cce
bo0216 <bo216@naver.com>
2020-05-25 16:13:52 +0900

    add: carName멤버 변수 추가

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -1,13 +1,17 @@
 package racing;
 
 public class Car {
-
+    private String carName;
     private int position;
 
     public void move(CarMoveBehavior carMoveBehavior) {
         if (carMoveBehavior.action()) {
             position++;
         }
+    }
+
+    public String getCarName() {
+        return carName;    
     }
 
     public int getPosition() {
