a42469b8d0370bc2d2cb0c9fe4ea1395da26f401
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-05-25 23:42:18 +0900

    feat(RacingGame) : 레이싱 게임 추가

--- src/test/java/step3/RacingGameTest.java
+++ src/test/java/step3/RacingGameTest.java
@@ -1,11 +1,19 @@
 package step3;
 
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RacingGameTest {
+  RacingGame racingGame;
 
   @Test
-  void move() {
-
+  @DisplayName("게임 진행 테스트")
+  void play() {
+    racingGame = new RacingGame(2, 10);
+    racingGame.play();
   }
 }
