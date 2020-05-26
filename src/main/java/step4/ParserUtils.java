556030895879bb7aa960b7ce5b6dc4084f517df7
Ho <doitluke@naver.com>
2020-05-27 02:20:40 +0900

    feat(ParserUtils): stringToArray, checkBlank, isBlank 메서드 생성

--- src/main/java/step4/ParserUtils.java
+++ src/main/java/step4/ParserUtils.java
@@ -1,4 +1,29 @@
 package step4;
 
-public class ParserUtils {
+import java.util.Optional;
+
+public class  ParserUtils {
+
+    private static final String COMMA = ",";
+
+    public static String[] stringToArray(String input) {
+        return Optional.ofNullable(input)
+                .filter(text -> !text.trim().isEmpty())
+                .map(text -> text.split(COMMA))
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public static void checkBlank(String[] nameList) {
+        for (String name: nameList) {
+            isBlank(name);
+        }
+    }
+
+    private static void isBlank(String name) {
+        if(name.isBlank()){
+            throw new IllegalArgumentException("입력문자에 공백이 있습니다.");
+        }
+    }
+
+
 }
