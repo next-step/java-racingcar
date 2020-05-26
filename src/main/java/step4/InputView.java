4e5612a52fbf8a1553c09c8137d052a1b78d1e60
Ho <doitluke@naver.com>
2020-05-27 02:21:21 +0900

    feat(InputView): 입력받을 클래스 생성

---
+++ src/main/java/step4/InputView.java
@@ -0,0 +1,33 @@
+package step4;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private int inputTime;
+    private Scanner scanner;
+    private String[] nameList;
+
+    public InputView(Scanner scan){
+        scanner = scan;
+    }
+
+    public void getUserInput() {
+
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String carNameString = this.scanner.next();
+
+        nameList = ParserUtils.stringToArray(carNameString);
+        ParserUtils.checkBlank(nameList);
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        inputTime = this.scanner.nextInt();
+
+        if (inputTime < 1) {
+            throw new IllegalArgumentException("반복횟수가 0보다 작을 수 없습니다.");
+        }
+
+
+    }
+}
