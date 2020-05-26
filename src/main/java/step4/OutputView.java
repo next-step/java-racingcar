72ae84f0e12e20d2eccb1157a7d28f90783e6812
Ho <doitluke@naver.com>
2020-05-27 02:21:55 +0900

    feat(OutputView): 출력 관련 클래스 기본틀 생성

---
+++ src/main/java/step4/OutputView.java
@@ -0,0 +1,20 @@
+package step4;
+
+import java.util.Collections;
+import java.util.List;
+
+public class OutputView {
+
+    private static String MARK = "-";
+
+    public void resultPrint(List<Integer> positionList){
+        for (Integer position : positionList){
+            drawRepeat(position);
+        }
+    }
+
+    private void drawRepeat(int times){
+        String markFullString = String.join("", Collections.nCopies(times, MARK));
+        System.out.println(markFullString);
+    }
+}
