cbe7ab3ac1b3d0e8d4d5f9e61b4626d3b06676a8
bo0216 <bo216@naver.com>
2020-05-28 04:26:45 +0900

    add: carTest를 위한 TestCarForwardBehavior클래스 추가

---
+++ src/main/java/racing/domain/car/TestCarForwardBehavior.java
@@ -0,0 +1,10 @@
+package racing.domain.car;
+
+public class TestCarForwardBehavior implements CarMoveBehavior{
+    private final boolean GO = true;
+
+    @Override
+    public boolean action(){
+        return true;
+    }
+}
