f5d832a59ece25f1cefc0b683d72bf5fc1e7d21d
bluehoped <bluehoped@hanmail.net>
2020-05-28 14:42:16 +0900

    refactor 우승자 뽑기 기능 추가

--- src/main/java/racing/domain/RacingGameResult.java
+++ src/main/java/racing/domain/RacingGameResult.java
@@ -2,6 +2,7 @@ package racing.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGameResult {
     private final List<RacingCars> allRoundRacingCars;
@@ -11,14 +12,27 @@ public class RacingGameResult {
     }
 
     public void addResult(RacingCars racingCars) {
-        allRoundRacingCars.add(racingCars);
+        allRoundRacingCars.add(racingCars.getDeepCopyRacingCars());
     }
 
     public List<RacingCars> getAllRoundRacingCars() {
         return allRoundRacingCars;
     }
 
-    public String getWinners() {
-        return null;
+    public List<Car> getWinners() {
+        int winnerPosition = getLastRacingCars().getWinnerPosition();
+        List<Car> finalRoundCars = getLastRacingCars().getCarList();
+
+        return finalRoundCars.stream().filter(car -> comparePosition(car.getPosition(), winnerPosition)).collect(Collectors.toList());
     }
+
+    private RacingCars getLastRacingCars() {
+        int finalRound = allRoundRacingCars.size() - 1;
+        return allRoundRacingCars.get(finalRound);
+    }
+
+    private boolean comparePosition(int position1, int position2) {
+        return position1 == position2;
+    }
+
 }
