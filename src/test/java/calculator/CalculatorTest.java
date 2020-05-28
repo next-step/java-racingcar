b16140a910980f94804bb6dd873caa08bf0639af
impactrudia <impactyoung@gmail.com>
2020-05-28 23:50:29 +0900

    test : @MethodSource 형태 적용

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,9 +1,11 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,10 +13,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("계산")
 class CalculatorTest {
-    @Test
+    private static Stream<String> convertStringToDouble() {
+        return Stream.of("t", "", "  ");
+    }
+
+    @ParameterizedTest
+    @MethodSource
     @DisplayName("숫자가 아닌 다른 문자가 들어왔을 때_NumberFormatException 예외처리 발생시킴")
-    void convertStringToDouble() {
-        String input = "t";
+    void convertStringToDouble(String input) {
         assertThatThrownBy(() -> Calculator.convertStringToDouble(input)).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("연산하기위해 숫자를 입력해주세요.");
     }
