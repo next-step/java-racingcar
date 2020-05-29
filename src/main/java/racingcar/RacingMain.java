6782cb9f40d3a5858ff5b7ed6c6551212ccb576a
impactrudia <impactyoung@gmail.com>
2020-05-30 00:06:56 +0900

    feat : Main클래스에 InputView 적용

---
+++ src/main/java/racingcar/RacingMain.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class RacingMain {
+    public static void main(String[] args){
+        int carNum = InputView.carNum();
+        int tryNum = InputView.tryTime();
+
+    }
+}
