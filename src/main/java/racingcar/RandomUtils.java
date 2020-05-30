477c54e629b43300f95435d2fa9b859155df1482
impactrudia <impactyoung@gmail.com>
2020-05-30 22:59:18 +0900

    refactor : static final 상수 네이밍은 대문자

--- src/main/java/racingcar/RandomUtils.java
+++ src/main/java/racingcar/RandomUtils.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class RandomUtils {
     private static final int RANDOM_NUMBER_BOUNDARY = 10;
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
 
     public static int generateRandomNum() {
-        return random.nextInt(RANDOM_NUMBER_BOUNDARY);
+        return RANDOM.nextInt(RANDOM_NUMBER_BOUNDARY);
     }
 }
