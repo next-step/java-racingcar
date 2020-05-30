bf5d52e811ca1a4bdd1a5336c34ae4d88f4b9c57
impactrudia <impactyoung@gmail.com>
2020-05-31 00:33:42 +0900

    refactor : Main 입력기 없이 테스트 하는것 주석해제

--- src/main/java/racingcar/RacingMain.java
+++ src/main/java/racingcar/RacingMain.java
@@ -2,11 +2,10 @@ package racingcar;
 
 public class RacingMain {
     public static void main(String[] args){
-//        int carNum = InputView.carNum();
-//        int tryNum = InputView.tryTime();
+        int carNum = InputView.carNum();
+        int tryNum = InputView.tryTime();
 
-//        Racing racing = new Racing(carNum, tryNum);
-        Racing racing = new Racing(2, 4);
+        Racing racing = new Racing(carNum, tryNum);
         ResultView resultView = racing.startRace();
         resultView.results();
     }
