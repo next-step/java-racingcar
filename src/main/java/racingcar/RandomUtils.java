d2c0dac53c4fd60532d6ff18788efa140077b9d8
impactrudia <impactyoung@gmail.com>
2020-05-30 22:52:38 +0900

    refactor : 랜덤객체 한 번만 생성되도록 수정

--- src/main/java/racingcar/RandomUtils.java
+++ src/main/java/racingcar/RandomUtils.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class RandomUtils {
     private static final int RANDOM_NUMBER_BOUNDARY = 10;
+    private static final Random random = new Random();
 
     public static int generateRandomNum() {
-        return new Random().nextInt(RANDOM_NUMBER_BOUNDARY);
+        return random.nextInt(RANDOM_NUMBER_BOUNDARY);
     }
 }
