427d1daa15607462ff9bdcc8cabf2576b03fa4be
impactrudia <impactyoung@gmail.com>
2020-05-30 18:12:04 +0900

    refactor : static final시 대문자로 네이밍하기

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,15 +3,15 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static int carNum() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 
     public static int tryTime() {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 }
