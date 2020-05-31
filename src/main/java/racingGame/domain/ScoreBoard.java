4cb1eb8477b8602ead6e416f0c8bb7e9aa5e9102
hy43 <hyde0401@gmail.com>
2020-05-31 14:33:34 +0900

    refact ScoreBoard getWinner 메소드 내에서 콤마 추가 로직 Stream의 Collectors.joining 메소드 사용하도록 수정

--- src/main/java/racingGame/domain/ScoreBoard.java
+++ src/main/java/racingGame/domain/ScoreBoard.java
@@ -2,39 +2,42 @@ package racingGame.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.assertj.core.util.Arrays;
 
 public class ScoreBoard {
 	private List<Round> round;
 	private List<RacingCar> racingCars;
-	private String winners;
-	
+	private List<String> winners;
+
 	public ScoreBoard(String[] carNames, int countOfTime) {
 		round = new ArrayList<Round>();
-		for(int i = 0; i < countOfTime; i++) {
+		for (int i = 0; i < countOfTime; i++) {
 			round.add(new Round(carNames));
 		}
-		winners = "";
+		winners = new ArrayList<String>();
 	}
-	
+
 	public void registScore(List<RacingCar> racingCars, int nRound) {
 		round.get(nRound).registNRoundScore(racingCars, nRound);
 		this.racingCars = racingCars;
 	}
-	
-	public String[][] makeScoreBoard(){
+
+	public String[][] makeScoreBoard() {
 		int countOfCar = round.get(0).getCarNames().length;
 		String[][] scores = new String[countOfCar][round.size()];
-		for(int i = 0; i < countOfCar; i++) {
-			scores[i] = makeNRoundScoreBoard(i); 
+		for (int i = 0; i < countOfCar; i++) {
+			scores[i] = makeNRoundScoreBoard(i);
 		}
 		return scores;
 	}
 
 	private String[] makeNRoundScoreBoard(int orderOfCar) {
 		String[] scores = new String[round.size()];
-		for(int j = 0; j < round.size(); j++) {
+		for (int j = 0; j < round.size(); j++) {
 			scores[j] = round.get(j).getScore()[orderOfCar];
-			if(j == 0) {
+			if (j == 0) {
 				scores[j] = (round.get(j).getCarNames()[orderOfCar] + " : " + scores[j]);
 			}
 		}
@@ -43,26 +46,24 @@ public class ScoreBoard {
 
 	public String getWinner() {
 		int winnerPosition = getWinnerPosition();
-		
-		for(int i = 0; i < racingCars.size(); i++) {
-			winners += addComma(compareWinnerPosition(winnerPosition, i), i);
+
+		for (int i = 0; i < racingCars.size(); i++) {
+			registWinner(compareWinnerPosition(winnerPosition, i));
 		}
-		
-		return winners;
+
+		return winners.stream()
+						.map(String::valueOf)
+						.collect(Collectors.joining(","));
 	}
 
-	private String addComma(String winner, int i) {
-		if(!winner.isEmpty() 
-				&& i != 0 
-				&& !winners.isEmpty()) {
-			return "," + winner;
+	private void registWinner(String winner) {
+		if (!winner.isEmpty()) {
+			winners.add(winner);
 		}
-		
-		return "" + winner;
 	}
 
 	private String compareWinnerPosition(int winnerPosition, int orderOfCar) {
-		if(racingCars.get(orderOfCar).getCountOfPosition() == winnerPosition) {
+		if (racingCars.get(orderOfCar).getCountOfPosition() == winnerPosition) {
 			return racingCars.get(orderOfCar).getCarName();
 		}
 		return "";
@@ -70,7 +71,7 @@ public class ScoreBoard {
 
 	private int getWinnerPosition() {
 		int winnerPosition = 0;
-		for(int i = 0; i < racingCars.size(); i++) {
+		for (int i = 0; i < racingCars.size(); i++) {
 			int targetPosition = racingCars.get(i).getCountOfPosition();
 			winnerPosition = registWinnerPosition(winnerPosition, targetPosition);
 		}
