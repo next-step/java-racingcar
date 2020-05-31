5aa109bdaeaa10838918253b2ec9c4659e75ebd8
JihyeHwang09 <raye9053@gmail.com>
2020-06-01 00:35:05 +0900

    test: 사칙 연산의 테스트를 작성한다

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,8 +3,10 @@ package calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class StringCalculatorTest {
@@ -20,7 +22,35 @@ class StringCalculatorTest {
     @NullAndEmptySource
     void isBlank(String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            cal.add(input);
+            cal.apply(input);
         });
+    }
+
+    @ParameterizedTest
+    @DisplayName("덧셈한 결과를 반환한다")
+    @CsvSource(value = {"2 + 1 = 3", "999 + 1 = 1000", "0 + 1 = 1"}, delimiter = '=')
+    void add(String input, int expected) {
+        assertThat(cal.apply(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("뺄셈한 결과를 반환한다")
+    @CsvSource(value = {"2 - 1 = 1", "999 - 1 = 998", "0 - 1 = -1"}, delimiter = '=')
+    void subtract(String input, int expected) {
+        assertThat(cal.apply(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("곱셈한 결과를 반환한다")
+    @CsvSource(value = {"2 * 1 = 2", "-1 * 1 = -1", "0 * 2 = 0", "-2 * 0 = 0"}, delimiter = '=')
+    void multiply(String input, int expected) {
+        assertThat(cal.apply(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("나눗셈한 결과를 반환한다")
+    @CsvSource(value = {"2 / 1 = 2", "1 / 1 = 1", "0 / 1 = 1"}, delimiter = '=')
+    void divide(String input, int expected) {
+        assertThat(cal.apply(input)).isEqualTo(expected);
     }
 }
