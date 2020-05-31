f8fc219b0664aaed5ea74cc0944ccc346fbeb157
DominiqN <dmut7691@gmail.com>
2020-06-01 00:43:52 +0900

    feat: implement InputView

---
+++ src/main/java/autoracing/InputView.java
@@ -0,0 +1,34 @@
+package autoracing;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class InputView {
+    private static final String QUESTION_NUMBER_OF_CARS = "자동차 대수는 몇 대인가요?";
+    private static final String QUESTION_TOTAL_ROUNDS = "시도할 횟수는 몇 회인가요?";
+    public static InputView takeInput(InputStream inputStream) {
+        Scanner scan = new Scanner(inputStream);
+        System.out.println(QUESTION_NUMBER_OF_CARS);
+        int numberOfCars = Integer.parseInt(scan.nextLine().trim());
+        System.out.println(QUESTION_TOTAL_ROUNDS);
+        int totalRounds = Integer.parseInt(scan.nextLine().trim());
+        scan.close();
+        return new InputView(numberOfCars, totalRounds);
+    }
+
+    private final int numberOfCars;
+    private final int totalRounds;
+
+    public InputView(int numberOfCars, int totalRounds) {
+        this.numberOfCars = numberOfCars;
+        this.totalRounds = totalRounds;
+    }
+
+    public int getNumberOfCars() {
+        return numberOfCars;
+    }
+
+    public int getTotalRounds() {
+        return totalRounds;
+    }
+}
