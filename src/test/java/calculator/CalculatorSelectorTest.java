092e73a8a4e8dcf1b096111e19cda9543727ca00
JihyeHwang09 <raye9053@gmail.com>
2020-06-01 02:01:11 +0900

    test: 사칙연산 기호가 아닌 경우의 테스트 코드를 작성한다

--- src/test/java/calculator/CalculatorSelectorTest.java
+++ src/test/java/calculator/CalculatorSelectorTest.java
@@ -1,21 +1,15 @@
 package calculator;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorSelectorTest {
 
@@ -33,5 +27,14 @@ class CalculatorSelectorTest {
     @MethodSource("selectCalculator")
     void getCalculator(String operator, Calculator expected) {
         assertThat(CalculatorSelector.getCalculator(operator)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("사칙연산의 구분자로 입력된 값에 매핑되는 Enum 클래스의 상수가 아닐 경우 IllegalArgumentException을 발생시킨다")
+    @ValueSource(strings = {"!", "@", "#", "$"})
+    void isNotOperator(String input) {
+        assertThatThrownBy(() -> CalculatorSelector.getCalculator(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("사칙연산의 구분자가 아닙니다");
     }
 }
