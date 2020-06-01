2a2e6147a7328ac15200752066fe73005ef31a65
impactrudia <impactyoung@gmail.com>
2020-06-02 02:13:13 +0900

    feat : Scanner 경주할 자동차 이름 입력

--- src/main/java/racingcar/application/RacingMain.java
+++ src/main/java/racingcar/application/RacingMain.java
@@ -6,11 +6,10 @@ import racingcar.view.ResultView;
 
 public class RacingMain {
     public static void main(String[] args) {
-//        int carNum = InputView.carNum();
-//        int tryNum = InputView.tryTime();
+        String carNames = InputView.carNames();
+        int tryNum = InputView.tryTime();
 
-//        Racing racing = new Racing(carNum, tryNum);
-        Racing racing = new Racing("pobi,crong,honux", 4);
+        Racing racing = new Racing(carNames, tryNum);
 
         ResultView.results(racing.getRacingCars(), racing.startRace());
         ResultView.whoIsWinner(racing.getRacingCars());
