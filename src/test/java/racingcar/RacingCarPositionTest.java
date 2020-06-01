71bd84c8d9be7183e5f7f833d2b5bdc674339ef1
impactrudia <impactyoung@gmail.com>
2020-06-01 23:07:54 +0900

    test : RacingCarPositionTest setup 생성

--- src/test/java/racingcar/RacingCarPositionTest.java
+++ src/test/java/racingcar/RacingCarPositionTest.java
@@ -1,10 +1,10 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.application.RacingCarPosition;
-import racingcar.domain.RacingCar;
 import racingcar.utils.Const;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,13 +12,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("자동차 경주에 쓰여지는 자동차 상태")
 class RacingCarPositionTest {
+    RacingCarPosition racingCarPosition;
+
+    @BeforeEach
+    void setUp() {
+        racingCarPosition = new RacingCarPosition(Const.INITIAL_NUM);
+    }
+
     @ParameterizedTest
     @CsvSource(value = {"false:0", "true:1"}, delimiter = ':')
     @DisplayName("전진시 위치가 증가하고 멈춤시 위치가 변함이 없음을 확인한다.")
     void move(boolean isMove, int expected) {
         //given
-        RacingCarPosition racingCarPosition = new RacingCarPosition(Const.INITIAL_NUM);
-
         //when
         int result = racingCarPosition.move(() -> isMove);
 
@@ -31,8 +36,6 @@ class RacingCarPositionTest {
     @DisplayName("4이상일 경우엔 위치가 증가하고 4이하시에 위치가 변함이 없음을 확인한다.")
     void isMove(int movingNumber, int expected) {
         //given
-        RacingCarPosition racingCarPosition = new RacingCarPosition(Const.INITIAL_NUM);
-
         //when
         boolean isMoving = racingCarPosition.isMove(movingNumber);
 
