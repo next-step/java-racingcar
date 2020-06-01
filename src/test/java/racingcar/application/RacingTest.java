163045a1bb9e0c18cbe3f490af0cc17bef7511ad
impactrudia <impactyoung@gmail.com>
2020-06-02 02:08:31 +0900

    refactor : 테스트 메서드 네이밍 변경

--- src/test/java/racingcar/application/RacingTest.java
+++ src/test/java/racingcar/application/RacingTest.java
@@ -15,7 +15,7 @@ class RacingTest {
     @ParameterizedTest
     @CsvSource(value = {"pobi,crong,honux=3"}, delimiter = '=')
     @DisplayName("경주할 자동자를 split로 분리한 후 명수가 맞는지 확인한다.")
-    void name(String inputCarNames, int expected) {
+    void splitComma(String inputCarNames, int expected) {
         Racing racing = new Racing(inputCarNames, Const.INITIAL_NUM);
         assertThat(racing.getRacingCars().size()).isEqualTo(expected);
     }
