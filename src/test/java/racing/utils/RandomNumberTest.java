1a1e48f9361b9499006f5840cf98fc3cbcf768bb
bo0216 <bo216@naver.com>
2020-06-02 23:32:46 +0900

    test: 실패하는 테스트 수정

--- src/test/java/racing/utils/RandomNumberTest.java
+++ src/test/java/racing/utils/RandomNumberTest.java
@@ -2,8 +2,7 @@ package racing.utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import racing.utils.RandomNumber;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,10 +10,8 @@ public class RandomNumberTest {
 
     @ParameterizedTest
     @DisplayName("Random값이 0-bound 사이 인지")
-    @CsvSource(value = {"10,10,true","10,4,false"}, delimiter = ',')
-    void getRandomNumberTest(int bound, int max, boolean expected) {
-        assertThat(0<= RandomNumber.getRandomNumber(bound) &&
-                RandomNumber.getRandomNumber(bound) < max).isEqualTo(expected);
-
+    @ValueSource(ints = {3, 4, 10})
+    void getRandomNumberTest(int bound) {
+        assertThat(RandomNumber.getRandomNumber(bound)).isBetween(0, bound);
     }
 }
