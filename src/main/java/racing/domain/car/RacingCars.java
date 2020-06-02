d2f3d5ad3c89474e9325eb836b75d1cf457951a7
bo0216 <bo216@naver.com>
2020-06-02 23:11:32 +0900

    refactor: 변수 선언 위치 변경

--- src/main/java/racing/domain/car/RacingCars.java
+++ src/main/java/racing/domain/car/RacingCars.java
@@ -3,8 +3,9 @@ package racing.domain.car;
 import java.util.*;
 
 public class RacingCars {
-    private final List<Car> carPositions;
     private static final String DELIMITER = ",";
+
+    private final List<Car> carPositions;
 
     public RacingCars(String inputName) {
         this.carPositions = prepareCars(inputName);
