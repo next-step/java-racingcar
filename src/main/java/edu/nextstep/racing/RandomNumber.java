5f256b5e05def508bbe862f7e4a8f15a9e7e21ba
sangz1888 <sangz1888@gmail.com>
2020-06-03 23:14:56 +0900

    <feat>(<edu.nextstep.racing>): 난수 생성로직 구현

--- src/main/java/edu/nextstep/racing/RandomNumber.java
+++ src/main/java/edu/nextstep/racing/RandomNumber.java
@@ -1,4 +1,10 @@
 package edu.nextstep.racing;
 
+import java.util.Random;
+
 public class RandomNumber {
+    public int generator() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
 }
