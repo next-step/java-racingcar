a712ea7d9f0ab2ebe8eb651f715ed5ad665e600e
DominiqN <dmut7691@gmail.com>
2020-06-04 00:46:34 +0900

    refactor: extract racing game integration test from the unit test for RacingGame.class

--- src/test/java/autoracing/RacingGameTest.java
+++ src/test/java/autoracing/RacingGameTest.java
@@ -1,13 +1,10 @@
 package autoracing;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.List;
 
@@ -36,17 +33,5 @@ public class RacingGameTest {
         totalRoundsMember.setAccessible(true);
         int savedRounds = (int) totalRoundsMember.get(game);
         assertThat(savedRounds).isEqualTo(totalRounds);
-    }
-
-    @DisplayName("이건 그냥 main 함수 안 쓰려고 만든 거에요")
-    public void racingGame() {
-        Integer numberOfCars = 3;
-        Integer totalRounds = 4;
-        String inputValues = numberOfCars.toString() + "\n" + totalRounds.toString() + "\n";
-        InputStream inputStream = new ByteArrayInputStream(inputValues.getBytes());
-        InputView input = InputView.takeInput(inputStream);
-        RacingGame game = new RacingGame(input.getNumberOfCars(), input.getTotalRounds(), rule);
-        game.start();
-        game.replayResult();
     }
 }
