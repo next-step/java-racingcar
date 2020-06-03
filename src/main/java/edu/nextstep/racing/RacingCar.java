a942d96ee4be659261da590e0265884db697094c
sangz1888 <sangz1888@gmail.com>
2020-06-03 23:20:46 +0900

    <feat>(<edu.nextstep.racing>): 자동차 도메인 클래스 생성

--- src/main/java/edu/nextstep/racing/RacingCar.java
+++ src/main/java/edu/nextstep/racing/RacingCar.java
@@ -1,4 +1,21 @@
 package edu.nextstep.racing;
 
 public class RacingCar {
+    RandomNumber randomNumber = null;
+    int carPosition = 0;
+
+    RacingCar() {
+        randomNumber = new RandomNumber();
+    }
+
+    public void move() {
+        int randomNum = randomNumber.generator();
+        if(randomNum >= 4) {
+            this.carPosition++;
+        }
+    }
+
+    public int getStatus() {
+        return this.carPosition;
+    }
 }
