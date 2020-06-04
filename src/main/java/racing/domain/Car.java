4f91f78410f12727d259200c823d25cc1ead94cb
bluehoped <bluehoped@hanmail.net>
2020-06-04 20:55:32 +0900

    fix validation 추가

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,12 +1,21 @@
 package racing.domain;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Car {
     private static final int MOVE_MIN_NUMBER = 4;
     private String name;
     private int position;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String carName) {
+        if (StringUtils.isBlank(carName)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void move(int diceNumber) {
