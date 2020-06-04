43b126da4771955e34f1513f6f9929c8678970aa
Ho <doitluke@naver.com>
2020-06-05 03:30:58 +0900

    test : 추가된 로직에 대한 테스트, 누락된 메서드에 대한 테스트 추가

--- src/test/java/step4/CarsTest.java
+++ src/test/java/step4/CarsTest.java
@@ -6,17 +6,22 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
 
     private Cars cars;
+    private Cars positionCars;
     private String[] nameList = {"HO", "SOO", "RAM"};
+    private int[] positionList = {3,3,5};
 
     @BeforeEach
     public void setUp() {
         cars = new Cars(nameList);
+        positionCars = new Cars(nameList,positionList);
+
     }
 
     @DisplayName("DiceWithInput을 사용,  4보다 작을때 ")
@@ -46,5 +51,20 @@ class CarsTest {
         assertThat(result).contains(1, 1, 1);
 
 
+    }
+
+    @DisplayName("모두가 0일때 승자 출력")
+    @Test
+    public void getWinner() {
+        List<String> winner = cars.getWinner();
+        assertThat(winner.size()).isEqualTo(3);
+    }
+
+    @DisplayName("포지션이 각자 다를때 출력")
+    @Test
+    public void getWinnerPostionSet(){
+        List<String> winner = positionCars.getWinner();
+        assertThat(winner.size()).isEqualTo(1);
+        assertThat(winner).contains("RAM");
     }
 }
--- src/test/java/step4/ParserUtilsTest.java
+++ src/test/java/step4/ParserUtilsTest.java
@@ -1,5 +1,26 @@
+package step4;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParserUtilsTest {
 
+    @DisplayName("정상적인 데이터")
+    @Test
+    void checkBlank() {
+        String[] nameList = {"HO", "SOO", "RAM"};
+        ParserUtils.checkBlank(nameList);
+    }
+
+    @DisplayName("비정상적인 데이터")
+    @Test
+    void checkBlankError() {
+        String[] nameList = {"HO", "SOO", ""};
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> ParserUtils.checkBlank(nameList));
+    }
 }
