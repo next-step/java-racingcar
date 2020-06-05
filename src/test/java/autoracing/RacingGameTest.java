b061c452bfd37114e64fd15ab63055dafcb1fcd2
DominiqN <dmut7691@gmail.com>
2020-06-06 01:28:24 +0900

    test: remove reflection

--- src/test/java/autoracing/RacingGameTest.java
+++ src/test/java/autoracing/RacingGameTest.java
@@ -1,11 +1,9 @@
 package autoracing;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,24 +12,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RacingGameTest {
     private RacingRule rule;
 
-    @BeforeAll
-    public void init() {
-        this.rule = new RandomRacingRule(10, 4);
-    }
-
-    @ParameterizedTest
-    @CsvSource({"3,5", "2,7"})
-    public void shouldCreateCars(int numberOfCars, int totalRounds) throws NoSuchFieldException, IllegalAccessException {
+    @Test
+    public void shouldCreateCars() {
+        int numberOfCars = 4;
+        int totalRounds = 5;
+        RacingRule rule = () -> true;
         RacingGame game = new RacingGame(numberOfCars, totalRounds, rule);
 
-        Field participantsMember = RacingGame.class.getDeclaredField("participants");
-        participantsMember.setAccessible(true);
-        List<Car> participants = (List<Car>) participantsMember.get(game);
-        assertThat(participants.size()).isEqualTo(numberOfCars);
+        assertThat(game).isNotNull()
+                .extracting("totalRounds")
+                .containsExactly(totalRounds);
 
-        Field totalRoundsMember = RacingGame.class.getDeclaredField("totalRounds");
-        totalRoundsMember.setAccessible(true);
-        int savedRounds = (int) totalRoundsMember.get(game);
-        assertThat(savedRounds).isEqualTo(totalRounds);
+        assertThat(game).extracting("participants")
+                .flatExtracting((cars) -> (List<Car>) cars)
+                .doesNotContainNull()
+                .hasSize(numberOfCars)
+                .filteredOn("rule", rule)
+                .hasSize(numberOfCars);
     }
 }
