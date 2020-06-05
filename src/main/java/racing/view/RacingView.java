18a8d15e94ac65dac7661aa50519bdea38709b8e
wyh951753 <wyh951753@gmail.com>
2020-06-06 00:10:28 +0900

    Implements - 우승자 출력 기능 추가

--- src/main/java/racing/view/RacingView.java
+++ src/main/java/racing/view/RacingView.java
@@ -1,6 +1,8 @@
 package racing.view;
 
+import racing.domain.Car;
 import racing.domain.RacingResult;
+import racing.domain.Winners;
 import racing.util.Printer;
 
 import java.util.List;
@@ -8,14 +10,16 @@ import java.util.List;
 public class RacingView {
 
     public void print(final RacingResult racingResult) {
-        for (int i = 0; i < racingResult.getRacingRoundSize(); i++) {
+        for (int i = 1; i <= racingResult.getRacingRoundSize(); i++) {
             printRoundResult(racingResult.getRoundResult(i));
         }
+        Printer.printWinner(Winners.getWinners(racingResult.getLastRoundResult()));
     }
 
-    private void printRoundResult(final List<Integer> roundResult) {
-        for (int carLocation : roundResult) {
-            printCarMovement(carLocation);
+    private void printRoundResult(final List<Car> roundResult) {
+        for (Car car : roundResult) {
+            Printer.printName(car.getName());
+            printCarMovement(car.getLocation());
         }
         Printer.printBorder();
     }
