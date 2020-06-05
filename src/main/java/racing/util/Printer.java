8cf3f944580507f59d07485eb55f167be25db43a
wyh951753 <wyh951753@gmail.com>
2020-06-06 00:12:23 +0900

    Refactor - Util 함수 상속 및 생성자 초기화 방지

--- src/main/java/racing/util/Printer.java
+++ src/main/java/racing/util/Printer.java
@@ -1,6 +1,16 @@
 package racing.util;
 
-public class Printer {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class Printer {
+    private Printer() {}
+
+    public static void printName(final String name) {
+        String result = String.format("%s : ", name);
+
+        System.out.print(result);
+    }
 
     public static void printMovement() {
         System.out.print("- ");
@@ -8,5 +18,12 @@ public class Printer {
 
     public static void printBorder() {
         System.out.println();
+    }
+
+    public static void printWinner(final List<String> winners) {
+        String winner = winners.stream().map(Object::toString).collect(Collectors.joining(", "));
+        String result = String.format("%s가 최종 우승했습니다.", winner);
+
+        System.out.println(result);
     }
 }
