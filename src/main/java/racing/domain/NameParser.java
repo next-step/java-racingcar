07f99b91eafde8075e86053d0e35565866af1873
wyh951753 <wyh951753@gmail.com>
2020-06-07 02:17:41 +0900

    Refactor - 자동차 리스트를  생성하는 로직과 입력값을 처리하는 로직을 분리

--- src/main/java/racing/domain/NameParser.java
+++ src/main/java/racing/domain/NameParser.java
@@ -1,19 +1,16 @@
 package racing.domain;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+public final class NameParser {
+    private NameParser() {}
 
-public class NameParser {
-
-    public List<Car> parse(final String nameString) {
-        if (!isValid(nameString)) throw new IllegalArgumentException();
-
-        List<String> names = Arrays.asList((nameString).split(","));
-        return names.stream().map(Car::new).collect(Collectors.toList());
+    public static String[] parse(final String nameString) {
+        if (!isValid(nameString)) {
+            throw new IllegalArgumentException();
+        }
+        return nameString.split(",");
     }
 
-    private boolean isValid(final String nameString) {
+    private static boolean isValid(final String nameString) {
         return (nameString != null && nameString.trim().length() != 0);
     }
 }
