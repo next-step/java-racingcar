878ef3088922400d29e0759bf5f85649100b8315
sangyounkim <505>
2020-06-09 00:31:39 +0900

    refactor : - remove set function on Presenter - remove constructor - relocated to end logic - relocated to print current game status

--- src/main/java/racingcar/racinggame/RacingGamePresenter.java
+++ src/main/java/racingcar/racinggame/RacingGamePresenter.java
@@ -12,55 +12,44 @@ public class RacingGamePresenter implements RacingGameContract.Presenter {
 	private RacingGameContract.View view;
 	private RacingGame racingGame;
 
-	public RacingGamePresenter() {
-		this.racingGame = new RacingGame();
-	}
-
 	@Override
 	public void setView(RacingGameContract.View view) {
 		this.view = view;
 	}
 
 	@Override
-	public void start() {
+	public void start(int gameCount,String[] carNames) {
+		this.racingGame = new RacingGame(gameCount ,carNames);
+		view.println(MessageType.INFO.message(SUCCESS_MAKE_CAR_MSG));
 		view.println(MessageType.INFO.message(GAME_START_MSG));
-
 		while (!racingGame.isEnd()) {
 			racingGame.play();
-			view.println(MessageType.INFO.message(racingGame.getPlayCount()+PLAY_START_MSG));
-			racingGame.getCars().forEach((car) -> {
-						view.print(car.getName());
-						view.print(" :: ");
-						view.printCarPositionBySymbol(car.getPosition());
-					}
-			);
+			view.println(MessageType.INFO.message(racingGame.getPlayCount() + PLAY_START_MSG));
+			printCurrGameStatus();
 			view.println(MessageType.INFO.message(racingGame.getPlayCount() + PLAY_END_MSG));
 			view.println(MessageType.EMPTY.message(""));
 		}
+		end();
+	}
+
+	private void printCurrGameStatus() {
+		racingGame.getCars().forEach((car) -> {
+					view.print(car.getName());
+					view.print(" :: ");
+					view.printCarPositionBySymbol(car.getPosition());
+				}
+		);
+	}
+
+	private void end() {
 		List<Car> winners = racingGame.getWinner();
 		String winnerCarMsg = "";
-		for( int i = 0; i < winners.size(); i++) {
-			winnerCarMsg = winnerCarMsg.concat(winners.get(i).getName());
-			if(i != winners.size() -1) {
-				winnerCarMsg = winnerCarMsg.concat(",");
-			}
+		for (int i = 0; i < winners.size(); i++) {
+			winnerCarMsg = i != winners.size() - 1 ? winnerCarMsg.concat(winners.get(i).getName()+',') : winnerCarMsg.concat(winners.get(i).getName());
 		}
 		view.println(MessageType.INFO.message(winnerCarMsg+ WINNER_MSG));
 		view.println(MessageType.INFO.message(GAME_END_MSG));
 	}
 
-	@Override
-	public void setGameNum(String gameCount) {
-		int count = Integer.parseInt(gameCount);
-		racingGame.setGameCount(count);
-	}
-
-	@Override
-	public void addCars(String carNameString) {
-		String[] carNameArr = carNameString.split(",");
-		boolean isMakeCar = racingGame.makeCar(carNameArr);
-		String msg = isMakeCar ? SUCCESS_MAKE_CAR_MSG : FAIL_MAKE_CAR_MSG;
-		view.println(MessageType.INFO.message(msg));
-	}
 }
 
