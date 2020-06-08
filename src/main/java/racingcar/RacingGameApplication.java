f2833a948e3c1e06bb9c517a4d93444f774957da
sangyounkim <505>
2020-06-09 00:42:42 +0900

    feat : - add code for input layout function.

--- src/main/java/racingcar/RacingGameApplication.java
+++ src/main/java/racingcar/RacingGameApplication.java
@@ -7,6 +7,8 @@ public class RacingGameApplication {
     public static void main(String[] args) {
         RacingGameView racingGameview = new RacingGameView();
         racingGameview.init();
+        racingGameview.layoutCarQuestion();
+        racingGameview.layoutGameTryQuestion();
     }
 
 }
