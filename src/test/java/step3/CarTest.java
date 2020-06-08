a249b0259046574237aacc69e053f78fcab50875
sangyounkim <505>
2020-06-09 00:36:29 +0900

    test : - add car test case - refactor RacingGameTest

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -12,7 +12,7 @@ public class CarTest {
 
     @BeforeEach
     void setUp() {
-        car = new Car("testCar");
+        car = new Car("TestCar");
     }
 
     @Test
@@ -27,5 +27,61 @@ public class CarTest {
     void carStopTestFunction() {
         car.move(4);
         assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 설정 테스트")
+    void carNameTestFunction() {
+        String carName = car.getName();
+        assertThat(carName).isEqualTo("TestCar");
+    }
+
+    @Test
+    @DisplayName("자동차 위치 동일 한지 확인하는 함수 테스트")
+    void carIsEqualsPositionTestFunction() {
+        car.move(5);
+        car.move(5);
+        car.move(5);
+
+        assertThat(car.isEqualPosition(4)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("자동차 위치 클 경우 함수 테스트")
+    void compareCarPositionAtBiggerTestFunction() {
+        car.move(5);
+        car.move(5);
+        car.move(5);
+
+        Car otherCar = new Car("otherCar");
+        otherCar.move(5);
+        otherCar.move(5);
+
+        assertThat(car.compareTo(otherCar)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동차 위치 작을 경우 함수 테스트")
+    void compareCarPositionAtSmallerTestFunction() {
+        car.move(5);
+
+        Car otherCar = new Car("otherCar");
+        otherCar.move(5);
+        otherCar.move(5);
+
+        assertThat(car.compareTo(otherCar)).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("자동차 위치 동일한 경우 함수 테스트")
+    void compareCarPositionAtSameTestFunction() {
+        car.move(5);
+        car.move(5);
+
+        Car otherCar = new Car("otherCar");
+        otherCar.move(5);
+        otherCar.move(5);
+
+        assertThat(car.compareTo(otherCar)).isEqualTo(0);
     }
 }
