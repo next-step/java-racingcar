5f08e184f13b5f009cf3d7d888ea600329284d7d
HSR <tradlinx@DESKTOP-MVDAAFK>
2020-06-09 14:16:22 +0900

    생성자 입력값 유효성확인 코드 추가 및 변수 위치 수정

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -2,8 +2,9 @@ package racing;
 
 public class Car {
 
-    private int position;
     private final static int MINIMUM_CONDITIONS = 4;
+
+    private int position;
     private String carName;
 
     public Car(String inputCarName) {
@@ -11,6 +12,15 @@ public class Car {
     }
 
     public Car(String inputCarName, int position) {
+
+        if(inputCarName.trim().isEmpty()){
+            throw new IllegalArgumentException("이름이 입력되지 않았습니다.");
+        }
+
+        if(position < 0){
+            throw new IllegalArgumentException("위치값이 0보다 작을 수 없습니다.");
+        }
+
         this.carName = inputCarName;
         this.position = position;
     }
