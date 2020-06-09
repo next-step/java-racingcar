909bf4ede8dae575e9369f21484f3ca8e8fa255d
sangz1888 <sangz1888@gmail.com>
2020-06-09 23:18:59 +0900

    <feat>(<edu.nextstep.racing>): 자동차 이름관련 UI로직

--- src/main/java/edu/nextstep/racing/InputView.java
+++ src/main/java/edu/nextstep/racing/InputView.java
@@ -3,8 +3,17 @@ package edu.nextstep.racing;
 import java.util.Scanner;
 
 public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String[] splitCarName() {
+        String input = scanner.nextLine();
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("input data is empty");
+        }
+        return input.split(",");
+    }
+
     public static int inputer() {
-        Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
         return number;
--- src/main/java/edu/nextstep/racing/RacingGame.java
+++ src/main/java/edu/nextstep/racing/RacingGame.java
@@ -10,36 +10,36 @@ public class RacingGame<T> {
     }
 
     public void setUp(MovingCheck movingCheck) {
-        ResultView.view("자동차 대수는 몇 대 인가요?");
-        int carNum = InputView.inputer();
+        ResultView.view("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)");
+        String[] carNames = InputView.splitCarName();
 
         ResultView.view("시도할 회수는 몇 회 인가요?");
         int gameNum = InputView.inputer();
 
-        startGame(movingCheck, carNum, gameNum);
+        startGame(movingCheck, carNames, gameNum);
     }
 
-    public void setUpTest(MovingCheck movingCheck, int carNum, int gameNum) {
-        startGame(movingCheck, carNum, gameNum);
+    public void setUpTest(MovingCheck movingCheck, String[] carNames, int gameNum) {
+        startGame(movingCheck, carNames, gameNum);
     }
 
-    public void startGame(MovingCheck movingCheck, int carNum, int gameNum) {
-        if (carNum < 0 || gameNum < 0) {
+    public void startGame(MovingCheck movingCheck, String[] carNames, int gameNum) {
+        if (gameNum < 0) {
             throw new IllegalArgumentException("Input Data Error");
         }
 
-        for (int i = 0; i < carNum; i++) {
-            carList.add(new RacingCar());
+        for (int i = 0; i < carNames.length; i++) {
+            carList.add(new RacingCar(carNames[i]));
         }
 
         for (int i = 0; i < gameNum; i++) {
-            for (int j = 0; j < carNum; j++) {
+            for (int j = 0; j < carNames.length; j++) {
                 RacingCar car = (RacingCar)carList.get(j);
                 car.move(movingCheck);
             }
         }
 
-        resultGame(carNum);
+        resultGame(carNames.length);
     }
 
     public void resultGame(int carNum) {
