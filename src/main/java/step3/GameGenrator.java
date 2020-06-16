271a0bfdc44dea6aa1c0cde7cebe1d94faa47800
eunjeong <cs801458j@gmail.com>
2020-06-16 21:59:13 +0900

    [feat] GameGenerator class 추가

--- src/main/java/step3/GameGenrator.java
+++ src/main/java/step3/GameGenrator.java
@@ -1,4 +1,22 @@
 package step3;
 
-public class GameGenrator {
+//  게임 생성 함수
+class GameGenerator {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+
+        int gameCount = inputView.numberOfGame();
+        int carCount = inputView.numberOfCar();
+
+        RacingGame racingGame = new RacingGame(carCount);
+        ResultView resultView = new ResultView();
+
+        for (int i = 0; i < gameCount; i++) {
+            racingGame.playRacingGame();
+            resultView.showResult(racingGame.getRacingCarList());
+        }
+
+    }
+
 }
