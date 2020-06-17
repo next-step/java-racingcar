bfcc395f50762f829972e02ec5042bbaa8a1cdca
eunjeong <cs801458j@gmail.com>
2020-06-17 21:53:02 +0900

    [fix] ResultView 출력 수정

--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -3,17 +3,16 @@ import java.util.List;
 
 public class ResultView {
 
-    public ResultView() {
-        System.out.println("실행결과");
-    }
 
     public void printPosition(int positions){
         for (int i = 0; i < positions; i++) {
-            System.out.println("-");
+            System.out.print("-");
         }
+        System.out.println();
     }
 
     public void showResult(List<RacingCar> racingCars){
+        System.out.println("실행결과");
         for (RacingCar car : racingCars){
             printPosition(car.currentPosition());
         }
