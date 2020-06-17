f0dc3402c9fa0c5140518dde74394be3690fdd98
eunjeong <cs801458j@gmail.com>
2020-06-17 21:50:24 +0900

    [fix] RacingGame 클래스 구조 변경. RacingGame클래스에서 init과 game을 수행하도록 구현

--- src/main/java/step3/RacingGame.java
+++ src/main/java/step3/RacingGame.java
@@ -1,36 +1,54 @@
 package step3;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.List;
 
 public class RacingGame {
-    //  게임 진행하는 클래스 https://github.com/next-step/java-racingcar/pull/909/commits/db637e88d70bc34ed8f1c854c55e8ce82eb3a19c
-    private List<RacingCar> RacingCars;
-    private Random distance = new Random();
 
-    //  게임 진행 생성자
-    public RacingGame(int count) {
-        for (int i = 0; i < count; i++){
-            RacingCars.add(new RacingCar());
+    private List<RacingCar> racingCars = new ArrayList<>();
+    private Random distance = new Random();
+    private InputView inputView = new InputView();
+    private ResultView resultView = new ResultView();
+
+    private void setRacingGame(int carCount) {
+        for (int i = 0; i < carCount; i++){
+            racingCars.add(new RacingCar());
         }
     }
 
-    //  거리 생성하는 랜덤함수
     public int randomDistance() {
         return distance.nextInt(10);
     }
 
-    //  게임 실행하는 부분
     public List<RacingCar> playRacingGame() {
-        for (RacingCar car: RacingCars){
+        for (RacingCar car: racingCars){
             if(randomDistance() >= 4){
                 car.move();
             }
         }
-        return RacingCars;
+        return racingCars;
     }
 
     public List<RacingCar> getRacingCarList() {
-        return RacingCars;
+        return racingCars;
     }
+
+    public void initRacingGame(){
+        int carCount = inputView.numberOfCar();
+        setRacingGame(carCount);
+    }
+
+    public void startRacingGame(){
+        int gameCount = inputView.numberOfGame();
+        for (int i = 0; i < gameCount; i++) {
+            playRacingGame();
+            getRacingGameResult();
+        }
+    }
+
+    public void getRacingGameResult(){
+        resultView.showResult(getRacingCarList());
+    }
+
 }
