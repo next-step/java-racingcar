2f5a7430231fad6dde9f519749efc05cd5b4b4c4
eunjeong <cs801458j@gmail.com>
2020-06-17 21:54:38 +0900

    [fix] RacingGame 구조 변경으로 인한 GameGenertor 구조 변경

--- src/main/java/step3/GameGenerator.java
+++ src/main/java/step3/GameGenerator.java
@@ -4,19 +4,13 @@ package step3;
 class GameGenerator {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-
-        int gameCount = inputView.numberOfGame();
-        int carCount = inputView.numberOfCar();
-
-        RacingGame racingGame = new RacingGame(carCount);
-        ResultView resultView = new ResultView();
-
-        for (int i = 0; i < gameCount; i++) {
-            racingGame.playRacingGame();
-            resultView.showResult(racingGame.getRacingCarList());
-        }
+        //  racingGame 내부의 함수를 실행시키는 일만 수행
+        //  1. 게임 선언
+        RacingGame racingGame = new RacingGame();
+        //  2. 게임 설정
+        racingGame.initRacingGame();
+        //  3. 게임 실행
+        racingGame.startRacingGame();
 
     }
-
 }
