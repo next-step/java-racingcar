e7d82e0a160e353dbb2ab81f95181ce1aab7ecf2
ohjuntaek <201211734@pukyong.ac.kr>
2020-07-28 21:20:10 +0900

    [TEST] numbers: Set의 사이즈가 3인지 확인한다.

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("numbers: Set의 사이즈가 3인지 확인한다.")
+    void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+}
