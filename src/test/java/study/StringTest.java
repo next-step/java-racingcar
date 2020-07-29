29a992336819b9e546697efd052a42181cc48427
HAGA <haga.try@gmail.com>
2020-07-29 22:04:02 +0900

    미션 1 1단계

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,25 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void split() {
         String[] result = "1,2".split( ",");
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substring() {
+        String result = "1,2".substring("(1,2)");
+        assertThat(result).contains("1,2");
+    }
+
+    @Test
+    public void charAt() {
+        assertThatThrownBy(() -> {
+           "abc".charAt(2);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
