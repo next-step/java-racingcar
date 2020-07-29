602fd5105babc81c564c56846b202a4a6427541e
monds <suahnn@gmail.com>
2020-07-29 22:10:51 +0900

    Refactor: Arrays.asList 를 이용한 값 초기화

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,11 +20,7 @@ public class SetTest {
 
     @BeforeEach
     void setUp() {
-        numbers = new HashSet<>();
-        numbers.add(1);
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
+        numbers = new HashSet<>(Arrays.asList(1,1,2,3));
     }
 
     @Test
