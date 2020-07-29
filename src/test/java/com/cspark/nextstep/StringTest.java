0632fee4f7c6e3b26ac3414105d77ccc88431727
Chanseok Park <pcspapa@inzido.co.kr>
2020-07-29 23:09:05 +0900

    step1-1

--- src/test/java/com/cspark/nextstep/StringTest.java
+++ src/test/java/com/cspark/nextstep/StringTest.java
@@ -6,15 +6,26 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class StringSplitTest {
+public class StringTest {
+    private final String COMMA_DELIMITER = ",";
 
     @DisplayName("요구사항 1")
     @Test
     void split() {
-        String[] strings = "1,2".split(",");
+        String[] strings = "1,2".split(COMMA_DELIMITER);
         assertThat(strings).contains("1");
         assertThat(strings).contains("2");
         assertThat(strings).containsExactly("1", "2");
+
+        String[] ones = "1".split(COMMA_DELIMITER);
+        assertThat(ones).contains("1");
+    }
+
+    @DisplayName("요구사항 2")
+    @Test
+    void substring() {
+        String context = "(1,2)".substring(1, 4);
+        assertThat(context).isEqualTo("1,2");
     }
 
     @DisplayName("요구사항 3")
