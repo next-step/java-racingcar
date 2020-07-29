accb5b16584d62e7a894ef29ec6ceb974eb43a43
Hojun Choi <32973617+yameriong@users.noreply.github.com>
2020-07-29 22:14:11 +0900

    학습 테스트 실습 (#997)
    * Add StringTest
    Remove empty.txt
    
    * create (SetTest)

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,40 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+@DisplayName("String 클래스에 대한 학습 테스트")
+public class StringTest {
+    @Test
+    @DisplayName("test split")
+    public void testSplit() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    @DisplayName("test substring")
+    public void testSubstring() {
+        String str = "(1,2)";
+        int startBracket = str.indexOf("(");
+        int lastBracket = str.lastIndexOf(")");
+        assertThat("1,2".equals(str.substring(++startBracket,lastBracket))).isTrue();
+    }
+
+    @Test
+    @DisplayName("Test charAt")
+    public void charAt() {
+        String str = "abc";
+        // when
+        Throwable thrown = catchThrowable(() -> {
+            str.charAt(787);
+        });
+        assertThat(thrown)
+            .isInstanceOf(IndexOutOfBoundsException.class)
+            .hasMessageMatching("String index out of range: \\d+");
+    }
+}
