32537695049bb3ab27c4ddc35dc66f5b8cf42e78
hJJANG <dacapolife87@gmail.com>
2020-07-29 21:58:49 +0900

    1. Exception 발생 테스트 케이스 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,12 +3,13 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+
 
 public class StringTest {
 
     @Test
-    @DisplayName("String split()기능을 테스트")
+    @DisplayName("요구사항1 : String split()기능을 테스트")
     void splitTest(){
         String splitExampleInput = "1,2";
 
@@ -25,13 +26,52 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("String substring() 기능 테스트")
+    @DisplayName("요구사항2 : String substring() 기능 테스트")
     void substringTest(){
         String subStringExampleInput = "(1,2)";
 
         String subString = subStringExampleInput.substring(1, 4);
 
         assertThat(subString).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("요구사항3-1 : String charAt() 기능테스트")
+    void charAtTest(){
+        String charAtExampleInput = "abc";
+        char targetChar = charAtExampleInput.charAt(1);
+
+        assertThat(targetChar).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("요구사항3-2 : String charAt() OutOfBoundExample 테스트(assertThatThrownBy사용)")
+    void charAtIndexOutofBoundsExample1Test(){
+        assertThatThrownBy(() -> {
+            String charAtExampleInput = "abc";
+            charAtExampleInput.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+    @Test
+    @DisplayName("요구사항3-3 : String charAt() OutOfBoundExample 테스트(assertThatThrownBy사용-문구포함)")
+    void charAtIndexOutofBoundsExample2Test(){
+        assertThatThrownBy(() -> {
+            String charAtExampleInput = "abc";
+            charAtExampleInput.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+
+    @Test
+    @DisplayName("요구사항3-4 : String charAt() OutOfBoundExample 테스트(assertThatExceptionOfType)")
+    void charAtIndexOutofBoundsExample3Test(){
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+            .isThrownBy(() -> {
+                String charAtExampleInput = "abc";
+                charAtExampleInput.charAt(3);
+            }).withMessageMatching("String index out of range: 3");
     }
 
 }
