1c2c2663d1e85a892dcacc554a96c71470929fe6
junwoo <33277588+mike6321@users.noreply.github.com>
2020-07-29 22:14:22 +0900

    test study

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Project : java-racingcar
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:36 오후
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
