289334b9e780ba3a49d53e2bb6b32859ae3a7a01
jaeseon_hwang <jshwang@deleo.co.kr>
2020-07-29 21:19:11 +0900

    delete old_tdd_study data

--- src/main/java/calculator/StringCalculatorResultViewer.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-public class StringCalculatorResultViewer {
-
-    static void viewResult(int result) {
-        System.out.println(result);
-    }
-
-}
--- src/main/java/calculator/view/OutputView.java
+++
@@ -1,9 +0,0 @@
-package calculator.view;
-
-public class OutputView {
-
-    public static void printResult(int result) {
-        System.out.println(result);
-    }
-
-}
