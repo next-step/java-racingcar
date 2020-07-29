07364f04a7b584ff3c76b7356b5dc6fcd80e5d32
jaden.park <qkrwhdgh1007@gmail.com>
2020-07-29 22:20:54 +0900

    test (#1004)

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,22 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    public void split() {
+        String[] values = "1,2".split(",");
+        assertThat(values).containsExactly("1", "2");
+    }
+
+    @Test
+    public void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
