7b1725c53ffd4134c366d967fe8f5c6b5f888f1c
Yongdae <7942yongdae@gmail.com>
2020-07-30 02:00:06 +0900

    test study (#1017)

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers).hasSize(3);
+        assertThat(numbers.isEmpty()).isFalse();
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsWithExpect(int value, boolean expect) {
+        assertThat(numbers.contains(value)).isEqualTo(expect);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    void splitTest() {
+        String[] values1 = "1,2".split(",");
+        assertThat(values1).contains("1", "2");
+
+        String[] values2 = "1".split(",");
+        assertThat(values2).containsExactly("1");
+    }
+
+    @Test
+    void substringTest() {
+        String value = "(1,2)";
+        String result = value.substring(1, value.length() - 1);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException Test")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
