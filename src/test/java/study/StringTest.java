37325ab18611c28c58bb7bf57e0755798c61d80c
Hee Su Lee <hesue615@gmail.com>
2020-07-29 23:35:42 +0900

    Test Study (#1016)

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+
+public class StringTest {
+    @Test
+    void splitTest() {
+        String[] data = "1,2".split(",");
+
+        assertThat(data).contains("1", "2");
+        // 순서까지 보장하고 싶은 경우 containsExactly
+        assertThat(data).containsExactly("1", "2");
+    }
+
+    @Test
+    void subStrTest() {
+        String input = new String("(1,2)");
+
+        String subStr = input.substring(1, 4);
+
+        assertThat(subStr.equals("1,2")).isTrue();
+    }
+
+    @Test
+    void charAtTest() {
+        String input = new String("abc");
+
+        assertThatThrownBy(() -> {
+            input.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
+
