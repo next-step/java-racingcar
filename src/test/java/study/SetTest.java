b105afdaa1afb2dc8ba237bdde4fa4c708605617
sefide <sefide@naver.com>
2020-07-29 22:10:27 +0900

    test: test study

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,55 @@
+package study;
+
+import org.assertj.core.util.Strings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    public void size() {
+        assertThat(numbers).hasSize(3);
+        assertThat(numbers.isEmpty()).isFalse();
+        assertThat(numbers).isNotEmpty();
+    }
+
+    @Test
+    public void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    public void contains_parameterized(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "3:true", "4:false"}, delimiter = ':')
+    public void contains(int number, boolean result){
+        assertThat(numbers.contains(number)).isEqualTo(result);
+    }
+
+}
