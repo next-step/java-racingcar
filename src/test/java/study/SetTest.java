1d8e04cde5110089e7e416d4bb99337acd366ef2
jhPark <qkrwhdgh1007@gmail.com>
2020-07-29 22:01:00 +0900

    test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,36 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "3:true", "4:false"}, delimiter = ':')
+    void contains(int number, boolean result) {
+        assertThat(numbers.contains(number)).isEqualTo(result);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers).hasSize(3);
+        assertThat(numbers.isEmpty()).isFalse();
+    }
+}
