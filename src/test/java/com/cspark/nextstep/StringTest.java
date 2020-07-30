34efe162708926c92e289ffe8f0f4cf424d6add6
Chanseok Park <pcspapa@inzido.co.kr>
2020-07-30 09:46:41 +0900

    String 클래스에 대한 학습 테스트

--- src/test/java/com/cspark/nextstep/StringTest.java
+++ src/test/java/com/cspark/nextstep/StringTest.java
@@ -1,37 +1,48 @@
 package com.cspark.nextstep;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class StringTest {
-    private final String COMMA_DELIMITER = ",";
 
-    @DisplayName("요구사항 1")
-    @Test
-    void split() {
-        String[] strings = "1,2".split(COMMA_DELIMITER);
-        assertThat(strings).contains("1");
-        assertThat(strings).contains("2");
-        assertThat(strings).containsExactly("1", "2");
+  private final String COMMA_DELIMITER = ",";
 
-        String[] ones = "1".split(COMMA_DELIMITER);
-        assertThat(ones).contains("1");
-    }
+  @DisplayName("요구사항 1: split")
+  @Test
+  void split() {
+    String[] strings = "1,2".split(COMMA_DELIMITER);
+    assertThat(strings).contains("1");
+    assertThat(strings).contains("2");
+    assertThat(strings)
+        .as("모든 배열 순서 검증")
+        .containsExactly("1", "2");
 
-    @DisplayName("요구사항 2")
-    @Test
-    void substring() {
-        String context = "(1,2)".substring(1, 4);
-        assertThat(context).isEqualTo("1,2");
-    }
+    String[] ones = "1".split(COMMA_DELIMITER);
+    assertThat(ones)
+        .as("")
+        .contains("1");
+  }
 
-    @DisplayName("요구사항 3")
-    @Test
-    void charAt() {
-        assertThatThrownBy(() -> "abc".charAt(3))
+  @DisplayName("요구사항 2: substring")
+  @Test
+  void substring() {
+    String context = "(1,2)".substring(1, 4);
+    assertThat(context)
+        .as("beginIndex: inclusive, endIndex: exclusive")
+        .isEqualTo("1,2");
+  }
+
+  @DisplayName("요구사항 3: charAt")
+  @Test
+  void charAt() {
+    assertThat("abc".charAt(0)).isEqualTo('a');
+    assertThat("abc".charAt(1)).isEqualTo('b');
+    assertThat("abc".charAt(2)).isEqualTo('c');
+
+    assertThatThrownBy(() -> "abc".charAt(3))
         .isInstanceOf(StringIndexOutOfBoundsException.class);
-    }
+  }
 }
