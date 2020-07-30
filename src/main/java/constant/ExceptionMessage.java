b64319db6f418abbc850c010652e547495f7d54f
jhPark <qkrwhdgh1007@gmail.com>
2020-07-31 02:03:42 +0900

    [feat] 4칙연산을 위한 enum class 구현 및 테스트

---
+++ src/main/java/constant/ExceptionMessage.java
@@ -0,0 +1,6 @@
+package constant;
+
+public final class ExceptionMessage {
+    public static final String DIVIDE_BY_ZERO = "0으로 나눌 수 없습니다.";
+    public static final String SYMBOL_NOT_ARITHMETIC = "사칙 연산의 기호가 아닙니다.";
+}
---
+++ src/main/java/domain/Operation.java
@@ -0,0 +1,37 @@
+package domain;
+
+import constant.ExceptionMessage;
+
+import java.util.Arrays;
+import java.util.function.IntBinaryOperator;
+
+public enum Operation {
+    ADDITION("+", (prev, next) -> prev + next),
+    SUBTRACTION("-", (prev, next) -> prev - next),
+    MULTIPLICATION("*", (prev, next) -> prev * next),
+    DIVISION("/", (prev, next) -> {
+        if (next == 0) {
+            throw new ArithmeticException(ExceptionMessage.DIVIDE_BY_ZERO);
+        }
+        return prev / next;
+    });
+
+    private final String symbol;
+    private final IntBinaryOperator binaryOperator;
+
+    Operation(String symbol, IntBinaryOperator binaryOperator) {
+        this.symbol = symbol;
+        this.binaryOperator = binaryOperator;
+    }
+
+    public int compute(int prev, int next) {
+        return binaryOperator.applyAsInt(prev, next);
+    }
+
+    public static Operation findOperation(String symbol) {
+        return Arrays.stream(values())
+                .filter(operation -> operation.symbol.equals(symbol))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.SYMBOL_NOT_ARITHMETIC));
+    }
+}
