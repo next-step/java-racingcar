2c12ad4728fb96b8bf55d1b794c79db2f1bf5cc1
monds <suahnn@gmail.com>
2020-07-30 20:32:38 +0900

    Refactor: 미사용 코드 삭제

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -68,10 +68,4 @@ public final class StringCalculator {
 
         return result;
     }
-
-    public static void main(String[] args) {
-        StringCalculator calculator = new StringCalculator();
-        System.out.println(calculator.calculate("1 / 2 / 3"));
-
-    }
 }
