dc1fd10106a67c04fad81a92be22dd6cb27aef0a
heesunjeong <jhswit@naver.com>
2020-07-30 22:11:40 +0900

    feat(calculator): add calculate

--- src/main/java/study/StringCalculator.java
+++ src/main/java/study/StringCalculator.java
@@ -2,7 +2,41 @@ package study;
 
 public class StringCalculator {
 
-    public int calcuate(String expression) {
-        return 0;
+    public int calculate(String expression) {
+        String[] splitExpression = expression.split(" ");
+        int result = 0;
+
+        for (int i = 2; i < splitExpression.length; i += 2) {
+            if (i == 2) {
+                result = Integer.parseInt(splitExpression[i - 2]);
+            }
+
+            int targetNumber = Integer.parseInt(splitExpression[i]);
+            String symbol = splitExpression[i - 1];
+
+            result = calculateWithSymbol(result, targetNumber, symbol);
+        }
+
+        return result;
+    }
+
+    private int calculateWithSymbol(int value1, int value2, String symbol) {
+        if ("+".equals(symbol)) {
+            return value1 + value2;
+        }
+
+        if ("-".equals(symbol)) {
+            return value1 - value2;
+        }
+
+        if ("*".equals(symbol)) {
+            return value1 * value2;
+        }
+
+        if ("/".equals(symbol)) {
+            return value1 / value2;
+        }
+
+        throw new IllegalArgumentException("invalid calculate symbol");
     }
 }
--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -1,18 +1,32 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringCalculatorTest {
 
+    @DisplayName("문자열 값에 따라 사칙연산을 수행")
     @Test
     void calculate() {
         String expression = "2 + 3 * 4 / 2";
 
         StringCalculator calculator = new StringCalculator();
-        int result = calculator.calcuate(expression);
+        int result = calculator.calculate(expression);
 
         assertThat(result).isEqualTo(10);
+    }
+
+    @DisplayName("사칙연산 기호가 아닌 경우 exception 발생")
+    @Test
+    void calculateWithInvalidSymbol() {
+        String expression = "2 + 3 * 4 % 2";
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            StringCalculator calculator = new StringCalculator();
+            calculator.calculate(expression);
+        });
     }
 }
