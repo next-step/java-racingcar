fbd8784e439a41ceedb33a4c917b54168f0cf9d8
hJJANG <dacapolife87@gmail.com>
2020-07-30 12:33:46 +0900

    Calculator.java 추가: 사칙연산메소드만 추가

---
+++ src/main/java/step2/Calculator.java
@@ -0,0 +1,20 @@
+package step2;
+
+public class Calculator {
+
+    public int add(int firstVal, int secondVal){
+        return firstVal + secondVal;
+    }
+
+    public int minus(int firstVal, int secondVal){
+        return firstVal - secondVal;
+    }
+
+    public int multiply(int firstVal, int secondVal){
+        return firstVal * secondVal;
+    }
+
+    public int devide(int firstVal, int secondVal){
+        return firstVal / secondVal;
+    }
+}
