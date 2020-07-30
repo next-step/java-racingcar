38f3d5c7a962d4189ee1305bd68325f0040e30fd
mocha121212 <mocha121212@gmail.com>
2020-07-30 23:51:29 +0900

    test study

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,12 +3,25 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
-    void split() {
+    public void split() {
         String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
         assertThat(result).containsExactly("1","2");
+    }
 
+    @Test
+    public void substring() {
+        String str = "(1,2)".substring(1,4);
+    }
+
+    @Test
+    public void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
