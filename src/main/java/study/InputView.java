572211d4cc61d2f8c0e4ce4f390f0c48bf79944a
ap-jylee <e200uk@gmail.com>
2020-07-31 00:23:58 +0900

    feat(InputView): 사용자로부터 설정값 입력받기
    1. 자동차 갯수 입력
    2. 실행 횟수 입력
    3. 입력받을 값을 맵에 넣어서 리턴

---
+++ src/main/java/study/InputView.java
@@ -0,0 +1,25 @@
+package study;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class InputView {
+
+    static Map<String, Integer> getConfiguration() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int numberOfCars = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int numberOfAttempts = scanner.nextInt();
+        return makeConfiguration(numberOfCars, numberOfAttempts);
+    }
+
+    private static Map<String, Integer> makeConfiguration(int numberOfCars, int numberOfAttempts) {
+        Map<String, Integer> configuration = new HashMap<>();
+        configuration.put("numberOfCars", numberOfCars);
+        configuration.put("numberOfAttempts", numberOfAttempts);
+        return configuration;
+    }
+
+}
