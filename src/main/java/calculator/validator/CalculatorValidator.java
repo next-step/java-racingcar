96cf66a2f254f4de05144cfc6477e814312f1228
John <bass0223@naver.com>
2020-08-01 02:09:35 +0900

    Exception 클래스 분리에 따른 수정

--- src/main/java/calculator/validator/CalculatorValidator.java
+++ src/main/java/calculator/validator/CalculatorValidator.java
@@ -5,47 +5,51 @@ import java.util.stream.IntStream;
 
 import org.apache.commons.lang3.StringUtils;
 
-import calculator.exception.CalculatorException;
-import calculator.operation.Operation;
-import calculator.util.OperationOperandSeparator;
+import calculator.exception.IllegalInputException;
+import calculator.exception.IllegalOperandException;
+import calculator.operation.Operator;
+import calculator.util.UserInputUtil;
 
 public class CalculatorValidator {
 
-	public static void validateUserInput(String userInput) {
+	public void validate(String userInput) {
+		this.validateInputValue(userInput);
+		this.validateOperand(userInput);
+		this.validateOperation(userInput);
+	}
+
+	private void validateInputValue(String userInput) {
 		if (StringUtils.isBlank(userInput)) {
-			throw new CalculatorException("계산해야 할 내용을 입력해주세요.");
+			throw new IllegalInputException("계산해야 할 내용을 입력해주세요.");
 		}
 
 		String[] splittedValueOfUserInput = userInput.split("\\s+");
 		if (splittedValueOfUserInput.length == 1 || splittedValueOfUserInput.length % 2 != 1) {
-			throw new CalculatorException("계산에 필요한 입력이 유효하지 않습니다.");
+			throw new IllegalInputException("계산에 필요한 입력이 유효하지 않습니다.");
 		}
-
-		validateOperand(splittedValueOfUserInput);
-		validateOperation(splittedValueOfUserInput);
 	}
 
-	private static void validateOperand(String[] splittedValueOfUserInput) {
-		List<String> operands = OperationOperandSeparator.getOperands(splittedValueOfUserInput);
+	private void validateOperand(String userInput) {
+		List<String> operands = UserInputUtil.getOperands(userInput);
 
 		for (String operand : operands) {
-			validateDigit(operand);
+			this.checkDigit(operand);
 		}
 	}
 
-	private static void validateDigit(String operand) {
+	private void validateOperation(String userInput) {
+		List<String> signs = UserInputUtil.getSigns(userInput);
+		for (String sign : signs) {
+			Operator.validateOperation(sign);
+		}
+	}
+
+	private void checkDigit(String operand) {
 		boolean noDigit = IntStream.range(0, operand.length())
 								   .anyMatch(i -> !Character.isDigit(operand.charAt(i)));
 
 		if (noDigit) {
-			throw new CalculatorException("계산에 필요한 피연산자가 입력되지 않습니다.");
-		}
-	}
-
-	private static void validateOperation(String[] splittedValueOfUserInput) {
-		List<String> operations = OperationOperandSeparator.getOperations(splittedValueOfUserInput);
-		for (String operation : operations) {
-			Operation.validateOperation(operation);
+			throw new IllegalOperandException("계산에 필요한 피연산자가 입력되지 않습니다.");
 		}
 	}
 }
