cb7aa295f8041d8b55bb0cdc66478ea3907db74e
ap-jylee <e200uk@gmail.com>
2020-08-01 13:21:51 +0900

    test: 자동차 생성 테스트, 자동차 경주 게임 테스트
    1. 생성할 자동차 갯수를 입력하고 결과값 비교
    2. 자동차 경주 게임 시작
    3. 자동차 위치 기록

---
+++ src/test/java/study/step3/CarRacingGameTest.java
@@ -0,0 +1,31 @@
+package study.step3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step3.factory.CarFactory;
+import step3.service.CarRacingGame;
+
+@DisplayName("자동차 경주 게임 테스트")
+public class CarRacingGameTest {
+
+    private CarRacingGame carRacingGame;
+
+    @BeforeEach
+    public void setUp() {
+        carRacingGame = new CarRacingGame(CarFactory.create(5), 3);
+    }
+
+    @Test
+    @DisplayName("자동차 경주 게임 시작")
+    public void start() {
+        carRacingGame.start();
+    }
+
+    @Test
+    @DisplayName("스코어 보드에 기록")
+    public void recordScoreboard() {
+        carRacingGame.recordScoreboard();
+    }
+
+}
