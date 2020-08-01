f6ba028eb0644abf58c47883b4d66b080d13cc4d
hanogo <trullo393@gmail.com>
2020-08-01 16:17:00 +0900

    계산기 구현 및 테스트 코드구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,60 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Calculator {
-    public Calculator() {
+    private int result;
 
+    public Calculator(String inputText) {
+        String[] arrSplitText = inputText.split(" ");
+        AtomicInteger i = new AtomicInteger(0);
+        AtomicInteger total = new AtomicInteger(0);
+
+        if ("".equals(inputText) || inputText == null) {
+            throw new IllegalArgumentException(ErrorMsg.IS_NULL_OR_BLANK.msg);
+        }
+
+        Arrays.stream(arrSplitText).forEach(s -> {
+            if (!isNumber(s)) {
+                if (!isOperationSymbol(s)) {
+                    throw new IllegalArgumentException(s.concat(" : " + ErrorMsg.NOT_OPERATION_SYMBOL.msg));
+                }
+                Arrays.stream(Operator.values()).forEach(operator -> {
+                    Operator op = Operator.valueOf(operator.name());
+                    if (s.equals(op.valueOf(operator.name()).getName())) {
+                        total.set(op.calculate(total.get() == 0 ? arrSplitText[i.get() - 1] : total.toString(), arrSplitText[i.get() + 1]));
+                    }
+                });
+            }
+            i.getAndIncrement();
+        });
+        setResult(total.get());
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public boolean isNumber(String text) {
+        try {
+            Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public boolean isOperationSymbol(String text) {
+        AtomicBoolean inOperator = new AtomicBoolean(false);
+        Arrays.stream(Operator.values()).forEach(operator -> {
+            if (operator.getName().equals(text)) inOperator.set(true);
+        });
+        return inOperator.get();
     }
 }
--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -15,8 +15,34 @@ public enum Operator {
         this.name = name;
         this.expression = expression;
     }
+//    public Integer calculate(int num1, int num2) {
+//        return this.expression.apply(num1, num2);
+//    }
+//    public Integer calculate(String num1, String num2) {
+//        return this.expression.apply(Integer.parseInt(num1), Integer.parseInt(num2));
+//    }
 
-    public Integer calculate(Integer num1, Integer num2) {
-        return this.expression.apply(num1, num2);
+    public <T> Integer calculate(T num1, T num2) {
+        Integer intNum1 = 0;
+        Integer intNum2 = 0;
+
+        if (num1 instanceof String) {
+            intNum1 = Integer.parseInt((String) num1);
+        }
+        if (num1 instanceof Integer) {
+            intNum1 = (Integer) num1;
+        }
+        if (num2 instanceof String) {
+            intNum2 = Integer.parseInt((String) num2);
+        }
+        if (num2 instanceof Integer) {
+            intNum2 = (Integer) num2;
+        }
+
+        return this.expression.apply(intNum1, intNum2);
+    }
+
+    public String getName() {
+        return name;
     }
 }
