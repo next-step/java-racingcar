06392a42c9beefe40c1e5744d41a198b68ee8bcd
ap-jylee <e200uk@gmail.com>
2020-08-01 12:56:41 +0900

    feat: 자동차 파워 구하기
    1. 0 ~ 입력받은 값 사이의 랜덤값으로 파워를 구한다.

---
+++ src/main/java/step3/model/Car.java
@@ -0,0 +1,17 @@
+package step3.model;
+
+import java.util.Random;
+
+public class Car {
+
+    private static final Random PICKER = new Random();
+
+    public int getPower(int max) {
+        return PICKER.nextInt(max);
+    }
+
+    public int getForwardDistance(int power) {
+        return 0;
+    }
+
+}
