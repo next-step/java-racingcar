fbd7c8dbfd51485342989a5cf364901fc19f6ee1
ap-jylee <e200uk@gmail.com>
2020-08-01 12:40:57 +0900

    feat: 자동차 생성 기능
    1. 생성할 자동차 갯수를 입력받아서 자동차를 생성한다.

---
+++ src/main/java/step3/factory/CarFactory.java
@@ -0,0 +1,18 @@
+package step3.factory;
+
+import step3.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+
+    public static List<Car> create(int numberOfCars) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < numberOfCars; i++) {
+            cars.add(new Car());
+        }
+        return cars;
+    }
+
+}
