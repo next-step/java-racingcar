a3c60f92989a6ce2b561c0443f3f7da6a3496e9c
yeGenieee <cyj95428@gmail.com>
2020-08-02 00:11:12 +0900

    Add Set Collection에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,31 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
-    void split() {
+    public void split() {
         String[] result = "1,2".split(",");
+        assertThat(result).contains("1", "2");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    public void splitSubstring() {
+        String[] result = "(1,2)".substring(1,4).split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 가져오기")
+    public void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
