34c6ca83073232e23262ffacd35b0756ba6f2c3b
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-02 21:08:01 +0900

    feat(condition): Add RangeCondition class
    Add RangeCondition class
        - Checks source is inside of given range.

---
+++ src/main/java/cc/oak/racing/condition/RangeCondition.java
@@ -0,0 +1,27 @@
+package cc.oak.racing.condition;
+
+public class RangeCondition implements Condition<Integer> {
+    private final int min;
+    private final int max;
+
+    public RangeCondition(int min, int max) {
+        if (max < min) {
+            throw new IllegalArgumentException("min is larger than a max argument.");
+        }
+
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public boolean isMeetCondition(Integer source) {
+        if (source == null) {
+            throw new IllegalArgumentException("source is null!");
+        }
+        return checkNumberRange(source);
+    }
+
+    public boolean checkNumberRange(int source) {
+        return min <= source && source <= max;
+    }
+}
