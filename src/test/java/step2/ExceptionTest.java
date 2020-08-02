22c47069b724c6082063c8f668e8c4bd9e4ce8f9
John Jeong <john@teamo2.kr>
2020-08-02 18:16:23 +0900

    step2 complete

--- src/test/java/step2/ExceptionTest.java
+++ src/test/java/step2/ExceptionTest.java
@@ -1,4 +1,57 @@
 package step2;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class ExceptionTest {
+    final String[] ARITHMETIC_EXPRESSION = {"+", "-", "/", "*"};
+
+    boolean isStringEmpty(String testingValue) {
+        try {
+            if (testingValue.isBlank()) {
+                throw new IllegalArgumentException("문자열이 공백 혹은 비어있습니다.");
+            }
+
+            return false;
+        } catch (IllegalArgumentException exception) {
+
+            System.err.println(exception.getMessage());
+            return true;
+        }
+    }
+
+    boolean isArithmeticExpression(String testingValue) {
+        boolean result = false;
+
+        try {
+            for (String expression : ARITHMETIC_EXPRESSION) {
+                if (testingValue.equals(expression)) {
+                    result = true;
+                    break;
+                }
+            }
+            if (!result) {
+                throw new IllegalArgumentException("사칙연산이 아니에요.");
+            }
+
+            return true;
+        } catch (IllegalArgumentException exception) {
+
+            System.err.println(exception.getMessage());
+            return false;
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("예외 테스트")
+    @ValueSource(strings = {"+", " ", "^"})
+    void exceptionTest(String inputValue) {
+        assertThat(isStringEmpty(inputValue)).isFalse();
+        assertThat(isArithmeticExpression(inputValue)).isTrue();
+    }
 }
--- src/test/java/step2/UserInput.java
+++ src/test/java/step2/UserInput.java
@@ -1,4 +1,17 @@
 package step2;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserInput {
+
+    String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        return input;
+    }
 }
