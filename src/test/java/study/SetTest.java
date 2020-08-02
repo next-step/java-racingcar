8e48f666eef7bdef583217c292e1523b39e52240
jungmisu <jungmisu@tmon.co.kr>
2020-08-02 21:34:33 +0900

    test: set test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,2 +1,44 @@
-package study;public class SetTest {
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set의 크기 확인")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set에 1,2,3이 존재하는지 확인")
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("Set에 값이 존재하는 경우, 존재하지 않는 경우")
+    void contains(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
 }
