785b153517d66d6a7b78d4ba9c792b294d553c08
ap-jylee <e200uk@gmail.com>
2020-08-02 18:32:10 +0900

    feat: 자동차들의 이동결과 값을 기록하는 객체 생성

---
+++ src/main/java/step3/domain/CarRacingScoreboard.java
@@ -0,0 +1,23 @@
+package step3.domain;
+
+import step3.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarRacingScoreboard {
+
+    private List<List<Integer>> scoreboard = new ArrayList<>();
+
+    public void recordScoreboard(List<Car>cars) {
+        scoreboard.add(cars.stream()
+                .map(car -> car.getPosition())
+                .collect(Collectors.toList()));
+    }
+
+    public List<List<Integer>> getScoreboard() {
+        return scoreboard;
+    }
+
+}
