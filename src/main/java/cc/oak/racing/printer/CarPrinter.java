31a94b1e0911b6404f8c7267ad4122263317e756
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-02 21:25:03 +0900

    feat(printer): CarPrinter class
    Add CarPrinter class
        - CarPrinter is inherited by StringPrinter.
        - As StringPrinter was made for ensuring the string print, This should be always prints String.
        - Accepts Car instance to print.

---
+++ src/main/java/cc/oak/racing/printer/CarPrinter.java
@@ -0,0 +1,34 @@
+package cc.oak.racing.printer;
+
+import cc.oak.racing.Car;
+
+public class CarPrinter extends StringPrinter<Car> {
+    private static final String DISTANCE_SYMBOL = "-";
+
+    public CarPrinter() {
+        super();
+    }
+
+    public CarPrinter(Printer<String> stringPrinter) {
+        super(stringPrinter);
+    }
+
+    @Override
+    public void print(Car source) {
+        if (source == null) {
+            throw new IllegalArgumentException("source is null!");
+        }
+
+        String carString = createCarString(source);
+        stringPrinter.print(carString);
+    }
+
+    private String createCarString(Car car) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < car.getMovedDistance(); i++) {
+            builder.append(DISTANCE_SYMBOL);
+        }
+        builder.append("\n");
+        return builder.toString();
+    }
+}
