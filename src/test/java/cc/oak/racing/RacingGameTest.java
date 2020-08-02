5b9714bba7f8cc73ef9fe6c8078e1c3fdaf0798b
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-03 00:26:34 +0900

    feat(test): RacingGame class unit test
    Add RacingGameTest class
        - Checks createRound's argument is below 0.

---
+++ src/test/java/cc/oak/racing/RacingGameTest.java
@@ -0,0 +1,23 @@
+package cc.oak.racing;
+
+import cc.oak.racing.condition.CarForwardCondition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class RacingGameTest {
+    @Test
+    @DisplayName("createRound 인자가 0 미만일 때 예외발생")
+    public void createRound_ShouldThrow_IllegalArgumentException_OnBelowZero() {
+        RacingGame racingGame = createDummyRacingGame();
+
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(-1, 5));
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(1, -5));
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(-1, -5));
+    }
+
+    private RacingGame createDummyRacingGame() {
+        return new RacingGame(new CarForwardCondition<>(c -> true, c -> c));
+    }
+}
