0903958060aca13b0faa1af787df7ada6a318e65
hyeoni90 <hyeona743@gmail.com>
2020-08-02 23:03:07 +0900

    feat: Add Calculator test, Refactor validate zero of DivideOperator

--- src/main/java/calculator/DivideOperator.java
+++ src/main/java/calculator/DivideOperator.java
@@ -4,6 +4,9 @@ public class DivideOperator implements Operator {
 
     @Override
     public int operate(final int val1, final int val2) {
+        if(val2 == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
         return val1 / val2;
     }
 }
