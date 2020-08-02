9d03c1ab06c104f8f6033f6dc956729eb6ddb33a
heesunjeong <jhswit@naver.com>
2020-08-02 21:03:29 +0900

    refactor(racingGame): add InputView

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -1,25 +1,21 @@
 package domain;
 
+import view.InputView;
+
 import java.util.Random;
-import java.util.Scanner;
 
 public class RacingGame {
     private final Random random = new Random();
 
     public void start() {
-        Scanner scanner = new Scanner(System.in);
+        int numberOfCars = InputView.inputNumberOfCar();
+        int time = InputView.inputTime();
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int carNumber = scanner.nextInt();
-
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int time = scanner.nextInt();
-
-        int[] cars = new int[carNumber];
+        int[] cars = new int[numberOfCars];
 
         System.out.println("실행 결과");
         for(int i = 0; i < time; i++) {
-            for (int j = 0; j < carNumber; j++) {
+            for (int j = 0; j < numberOfCars; j++) {
                 cars[j] += this.move();
                 System.out.println(print(cars[j]));
             }
