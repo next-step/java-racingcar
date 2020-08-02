e1d034be9eb7ebcfea3db4372bf2137c61cc4934
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-02 21:11:40 +0900

    feat(converter): CarRandomIntConverter class
    Add CarRandomIntConverter class
        - It returns random integer from 0 to 9.

---
+++ src/main/java/cc/oak/racing/converter/CarRandomIntConverter.java
@@ -0,0 +1,20 @@
+package cc.oak.racing.converter;
+
+import cc.oak.racing.Car;
+
+import java.util.Random;
+
+public class CarRandomIntConverter implements Converter<Car, Integer> {
+    public static final int RANDOM_BOUNDS = 10;
+
+    Random random;
+
+    public CarRandomIntConverter() {
+        this.random = new Random();
+    }
+
+    @Override
+    public Integer convert(Car car) {
+        return random.nextInt(RANDOM_BOUNDS);
+    }
+}
