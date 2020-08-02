87cd90dddc1a0a48cc4bc7428facd34686e11c03
monds <suahnn@gmail.com>
2020-08-02 14:43:24 +0900

    refactor: 자동차 경주 관련 속성 값 클래스 분리

---
+++ src/test/java/racing/CarRacingProperty.java
@@ -0,0 +1,10 @@
+package racing;
+
+public final class CarRacingProperty {
+
+    public static final int MIN_RANDOM_DISTANCE = 0;
+
+    public static final int MAX_RANDOM_DISTANCE = 9;
+
+    public static final int MIN_MOVABLE_DISTANCE = 4;
+}
