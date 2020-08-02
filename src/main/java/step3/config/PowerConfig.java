32f0f54ecc04e721e07ea087159a2673d86ac9eb
ap-jylee <e200uk@gmail.com>
2020-08-02 16:15:51 +0900

    refactor: 파워 상수값에 final 추가

--- src/main/java/step3/config/PowerConfig.java
+++ src/main/java/step3/config/PowerConfig.java
@@ -2,7 +2,7 @@ package step3.config;
 
 public class PowerConfig {
 
-    public static int MAX = 10;
-    public static int CONDITION = 4;
+    public static final int MAX = 10;
+    public static final int CONDITION = 4;
 
 }
