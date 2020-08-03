4f1ca0b7ffacd56fc586e9b36dccec88c97f5043
bora <cbora852@gmail.com>
2020-08-04 07:31:22 +0900

    Add : String Calculator

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+         calculator = new StringCalculator();
+    }
+
+
+    @DisplayName("")
+    @Test
+    void name() {
+//        calculator.
+//        calculator
+    }
+}
