e63e3f9eba693e55fb568f1f7c7796c295ba3b7c
seul <eeseul.p@gmail.com>
2020-08-04 01:05:40 +0900

    Revert "Step2 2 (#1080)"
    This reverts commit 832a249847c75097b78c0fe8f6e56aad2525248e.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,74 +1,14 @@
 package study;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
     void split() {
-        String result[] = "1,2".split(",");
+       String result[] = "1,2".split(",");
 
-        assertThat(result).contains("1");
-        assertThat(result).contains("2");
-        assertThat(result).containsExactly("1","2");
-        assertThat(result[0]).isEqualTo("1");
-    }
-
-    @Test
-    void substring() {
-        String input ="(1,2)";
-
-        String result = input.substring(1,input.length()-1);
-
-        assertThat(result).isEqualTo("1,2");
-        assertThat(result).hasSize(3);
-    }
-
-    @Test
-    @DisplayName("charAt()테스트")
-    void charAt() {
-        String input ="abc";
-
-        assertThatThrownBy(()->{
-            char c=  "abc".charAt(3);
-
-        }).isInstanceOf(StringIndexOutOfBoundsException.class)
-                 .hasMessageContaining("out of range");
-
-        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
-                .isThrownBy(()->{
-                    char c = "temp".charAt(4);
-                });
-
-        assertThatThrownBy(()->{
-                List<String> list = Arrays.asList("String one","String two");
-                list.get(2);
-        }).isInstanceOf(IndexOutOfBoundsException.class)
-                 .hasMessageContaining("2");
-
-    }
-
-    class Dog{
-        private String name;
-        private double weigth;
-
-        public Dog(String name, double weigth) {
-            this.name = name;
-            this.weigth = weigth;
-        }
-    }
-
-    @Test
-    void testDog() {
-        Dog fido = new Dog("Fido",5.25);
-        Dog fidosClone = new Dog("Fido",5.25);
-//        assertThat(fido).isEqualTo(fidosClone);
-        assertThat(fido).isEqualToComparingFieldByFieldRecursively(fidosClone);
+       assertThat(result).contains("1");
     }
 }
-
