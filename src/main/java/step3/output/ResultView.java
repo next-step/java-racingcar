15313decba9c901171edd04fe362a8452a5a8fcd
ap-jylee <e200uk@gmail.com>
2020-08-03 12:29:29 +0900

    refactor: 결과값을 주입받도록 수정

--- src/main/java/step3/output/ResultView.java
+++ src/main/java/step3/output/ResultView.java
@@ -1,24 +1,32 @@
 package step3.output;
 
+import step3.domain.Car;
+
 import java.util.List;
 
 public class ResultView {
 
     private static final Character MARK = '-';
 
-    public static void printResult(List<List<Integer>> scoreboard) {
-        System.out.println("실행 결과");
-        scoreboard.forEach(ResultView::printPositions);
+    private List<List<Car>> rapResults;
+
+    public ResultView(List<List<Car>> rapResults) {
+        this.rapResults = rapResults;
     }
 
-    private static void printPositions(List<Integer> positions) {
-        positions.forEach(ResultView::printPositionOfAttempt);
+    public void printResult() {
+        System.out.println("실행 결과");
+        rapResults.forEach(this::printPositions);
+    }
+
+    private void printPositions(List<Car> cars) {
+        cars.forEach(this::printPositionOfAttempt);
         System.out.println();
     }
 
-    private static void printPositionOfAttempt(int position) {
+    private void printPositionOfAttempt(Car car) {
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < position; i++) {
+        for (int i = 0; i < car.getPosition(); i++) {
             builder.append(MARK);
         }
         System.out.println(builder.toString());
