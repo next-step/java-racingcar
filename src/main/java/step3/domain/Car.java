5ccba544136dcf3daf1196ed5ac333c7063d5145
LimHanGyeol <dlagksruf19@naver.com>
2020-08-03 23:11:24 +0900

    feat : 자동차 전진 / 정지

---
+++ src/main/java/step3/domain/Car.java
@@ -0,0 +1,47 @@
+package step3.domain;
+
+public class Car {
+
+    private static String ACCELATE_SIGN = "-";
+    private static String BRAKE_SIGN = "";
+
+
+    private int carId;
+    private String mileAge;
+
+    public Car(int carId) {
+        this.carId = carId;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public String getMileAge() {
+        return mileAge;
+    }
+
+    public void setMileAge(String mileAge) {
+        this.mileAge = mileAge;
+    }
+
+    public String accelerate() {
+        return ACCELATE_SIGN;
+    }
+
+    public String brake() {
+        return BRAKE_SIGN;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carId=" + carId +
+                ", mileAge='" + mileAge + '\'' +
+                '}';
+    }
+}
