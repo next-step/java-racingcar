b32b3db083cbea57ee60bf6289ae7e46691179c3
ap-jylee <e200uk@gmail.com>
2020-08-03 12:25:09 +0900

    refactor: 자동차 생성시 이동전략 주입

--- src/main/java/step3/factory/CarFactory.java
+++ src/main/java/step3/factory/CarFactory.java
@@ -2,19 +2,27 @@ package step3.factory;
 
 import step3.domain.BasicForwardStrategy;
 import step3.domain.Car;
+import step3.domain.ForwardStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CarFactory {
 
+    private static ForwardStrategy forwardStrategy;
+
+    static {
+        forwardStrategy = new BasicForwardStrategy();
+    }
+
     public static List<Car> create(int numberOfCars) {
         List<Car> cars = new ArrayList<>();
         for (int i = 0; i < numberOfCars; i++) {
-            cars.add(new Car(new BasicForwardStrategy()));
+            cars.add(new Car(forwardStrategy));
         }
         return cars;
     }
+
     private CarFactory() {
         throw new AssertionError();
     }
