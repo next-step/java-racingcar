3f4f23447269b35b2d4b18551323e1cf5779c8d7
John Jeong <john@teamo2.kr>
2020-08-05 01:52:50 +0900

    feat 자동차 대수, 시도 횟수 입력 기능 제작

--- src/main/java/step3/view/input/InputScanner.java
+++ src/main/java/step3/view/input/InputScanner.java
@@ -1,4 +1,12 @@
 package step3.view.input;
 
+import java.util.Scanner;
+
 public class InputScanner {
+
+    static public int scanInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextInt();
+    }
 }
--- src/main/java/step3/view/output/OutputView.java
+++ src/main/java/step3/view/output/OutputView.java
@@ -1,4 +1,21 @@
 package step3.view.output;
 
+import java.util.stream.Stream;
+
 public class OutputView {
+
+    final private String MOVED_PRINTING = "_";
+
+    public void printingMovement(int movementCount) {
+        Stream.iterate(0, n -> n)
+                .limit(movementCount)
+                .peek(n -> {
+                    System.out.print(MOVED_PRINTING);
+                })
+                .skip(movementCount - 1)
+                .findFirst()
+                .ifPresent(n -> {
+                    System.out.println(MOVED_PRINTING);
+                });
+    }
 }
