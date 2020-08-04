88a3651ccb952ec643f53d215bd8005e87c38459
John Jeong <john@teamo2.kr>
2020-08-05 01:52:50 +0900

    feat 자동차 대수, 시도 횟수 입력 기능 제작

--- src/test/java/step3/view/output/OutputViewTest.java
+++ src/test/java/step3/view/output/OutputViewTest.java
@@ -1,5 +1,19 @@
+package step3.view.output;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OutputViewTest {
 
+    @Test
+    @DisplayName("자동차 경주 경로 화면 출력")
+    public void printingMovement() {
+        OutputView outputView = new OutputView();
+
+        outputView.printingMovement(4);
+        /* TODO: System.out 으로 사용하는 화면 출력은 어떻게 유닛 테스트를 제작할까?*/
+    }
 }
