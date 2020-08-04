0826f148f8491cb10282e1051d3af7a3ab2404bb
Yongdae <7942yongdae@gmail.com>
2020-08-04 10:28:36 +0900

    Step3 - 자동차 경주 (#1092)
    * step1 단계에서 진행한 내용 이동 작업
    
    * docs: 기능 목록 정리
    
    * feat: 입력 받는 기능
    
    * feat: 자동차
    
    * feat: 게임
    
    * style

---
+++ src/main/java/step3/Car.java
@@ -0,0 +1,53 @@
+package step3;
+
+import java.util.Random;
+
+/***
+ * 자동차 게임에서 이용되는 자동차 클래스
+ * 전진 or 정지 기능이 있다.
+ */
+public class Car {
+
+    private Random engine = new Random();
+
+    private int position = 0;
+
+    /**
+     * 0 ~ 9 의 값중에 임의의 값(출력)을 반환한다.
+     *
+     * @return
+     */
+    private int getPower() {
+        return engine.nextInt(10);
+    }
+
+    /**
+     * 순간 출력을 내어 전진한다.
+     * 출력이 4보다 작은 경우 움직이지 않는다.
+     */
+    public void go() {
+        this.go(getPower());
+    }
+
+    /**
+     * 전진한다.
+     * 출력이 4보다 작은 경우 움직이지 않는다.
+     *
+     * @param power
+     */
+    public void go(int power) {
+        if (power < 4)
+            return;
+
+        position++;
+    }
+
+    /**
+     * 현재 위치를 반환한다.
+     *
+     * @return
+     */
+    public int getCurrentPosition() {
+        return position;
+    }
+}
---
+++ src/main/java/step3/InputScanner.java
@@ -0,0 +1,28 @@
+package step3;
+
+import java.util.Scanner;
+
+/***
+ *  사용자의 입력을 받아 입력한 값을 반환해주는 작업을 처리하는 클래스
+ */
+public class InputScanner {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void printMessage(String message) {
+        System.out.printf(message);
+    }
+
+    public static int getInt() {
+        return scanner.nextInt();
+    }
+
+    public static int getInputNumberOfCar() {
+        printMessage("자동차 대수는 몇 대 인가요?");
+        return getInt();
+    }
+
+    public static int getInputNumberOfTry() {
+        printMessage("시도할 회수는 몇 회 인가요?");
+        return getInt();
+    }
+}
