ede4ee56e32e142e6e20e923930111d6a582a276
LimHanGyeol <dlagksruf19@naver.com>
2020-08-05 00:18:10 +0900

    test : 레이싱 기능 테스트

--- src/test/java/study/step3/RacingCarTest.java
+++ src/test/java/study/step3/RacingCarTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import step3.RacingCar;
 import step3.domain.Car;
 
 import java.util.HashMap;
@@ -47,6 +49,36 @@ public class RacingCarTest {
         car.setMileAge(carState);
         carInfoMap.put(car.getCarId(), car);
         assertThat(car.getMileAge()).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @DisplayName("참여하는 자동차 대수 확인")
+    @ValueSource(ints = 3)
+    void preparedCarTest(int racingCarNumber) {
+        Map<Integer, Car> map = RacingCar.preparedCar(racingCarNumber);
+        assertThat(map.size()).isEqualTo(3);
+        assertThat(map).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("레이싱 기능")
+    @CsvSource(value = {"1:5"}, delimiter = ':')
+    void racingTest(int racingCarNumber, int gameCount) {
+        carInfoMap = RacingCar.preparedCar(racingCarNumber);
+        String carState = "";
+
+        for (int i = 1; i <= racingCarNumber; i++) {
+            Car car = carInfoMap.get(i);
+            if (car.getCarId() == i) {
+                for (int j = 1; j <= gameCount; j++) {
+                    int randomCondition = RacingCar.getAccelateNumber();
+                    carState += RacingCar.racing(car, randomCondition);
+                }
+                car.setMileAge(carState);
+                carInfoMap.put(car.getCarId(), car);
+            }
+        }
+        System.out.println(carState);
     }
 
 
