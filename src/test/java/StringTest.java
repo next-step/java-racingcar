0e2860bd2ef96e362400becf85ad7195092ad5c7
Jay <beginin15@gmail.com>
2020-08-04 22:49:04 +0900

    [step1] String 클래스 학습테스트 요구사항 3

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,6 +1,7 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
@@ -22,5 +23,21 @@ public class StringTest {
 
         String result = value.substring(beginIndex + 1, endIndex);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("String의 charAt 메소드 테스트")
+    public void charAt() {
+        char result = "abc".charAt(1);
+        assertThat(result).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("IndexOutOfBoundsException 발생 테스트")
+    public void handleCharAtException() {
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char result = "abc".charAt(3);
+                }).withMessageMatching("String index out of range: \\d+");
     }
 }
