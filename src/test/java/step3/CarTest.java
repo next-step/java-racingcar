bedbe45a744a2fd2c0762c922fd637fd90b8e0e3
yongdae <7942yongdae@gmail.com>
2020-08-04 19:55:42 +0900

    refactor: 경계값 테스트로 수정

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -1,25 +1,16 @@
 package step3;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
-
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3})
-    void goByFalse(int power) {
+    @CsvSource(value = {"0 = 0", "1 = 0", "3 = 0", "4 = 1", "8 = 1", "9 = 1"}, delimiter = '=')
+    void go(int power, int expect) {
         Car car = new Car();
         car.go(power);
-        assertThat(car.getCurrentPosition()).isEqualTo(0);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    void goByTrue(int power) {
-        Car car = new Car();
-        car.go(power);
-        assertThat(car.getCurrentPosition()).isEqualTo(0);
+        assertThat(car.getCurrentPosition()).isEqualTo(expect);
     }
 }
