4c2d1a44db7c790e982b741409171ce2d0787911
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-06 02:12:39 +0900

    fix(test): RacingGame unit test belowzero and onzerosize
    fix createRound_ShouldThrow_IllegalArgumentException_OnBelowZero wrong test case
    add createRound_ShouldThrow_IllegalArgumentException_OnZeroSize test

--- src/test/java/cc/oakk/racing/RacingGameTest.java
+++ src/test/java/cc/oakk/racing/RacingGameTest.java
@@ -4,6 +4,7 @@ import cc.oakk.racing.predicate.CarForwardCondition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,14 +12,23 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 public class RacingGameTest {
     @Test
+    @DisplayName("carName 인자의 크기가 0 이하일 때 예외발생")
+    public void createRound_ShouldThrow_IllegalArgumentException_OnZeroSize() {
+        RacingGame racingGame = createDummyRacingGame();
+        List<String> dummyList = new ArrayList<>();
+
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, 5));
+    }
+
+    @Test
     @DisplayName("createRound 인자가 0 이하일 때 예외발생")
     public void createRound_ShouldThrow_IllegalArgumentException_OnBelowZero() {
         RacingGame racingGame = createDummyRacingGame();
         List<String> dummyList = Collections.singletonList("a");
 
-        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, 5));
-        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, -5));
-        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, -5));
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, 0));
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, -1));
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, -10));
     }
 
     private RacingGame createDummyRacingGame() {
