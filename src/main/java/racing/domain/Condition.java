d9c39dcf69c9639f93339f9570dc6bfa71cab3ac
hanogo <trullo393@gmail.com>
2020-08-05 20:26:18 +0900

    [refeactor] Condition클래스 변수 수정

--- src/main/java/racing/domain/Condition.java
+++ src/main/java/racing/domain/Condition.java
@@ -1,9 +1,7 @@
 package racing.domain;
 
-import java.util.Random;
-
 public class Condition {
-    static final int VALUE_MIN_NUM = 4;
+    private static final int VALUE_MIN_NUM = 4;
 
     public static boolean checkMove(int randomNum) {
         return randomNum >= VALUE_MIN_NUM;
