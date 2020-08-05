b3f1c79c98e9d1c5b28947d7b1e962f41c2dbba4
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-06 01:45:44 +0900

    fix(test): createRound test
    modify createRound_ShouldThrow_IllegalArgumentException by below zero.
        - the method signature has changed.

--- src/test/java/cc/oakk/racing/RacingGameTest.java
+++ src/test/java/cc/oakk/racing/RacingGameTest.java
@@ -4,6 +4,9 @@ import cc.oakk.racing.predicate.CarForwardCondition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class RacingGameTest {
@@ -11,10 +14,11 @@ public class RacingGameTest {
     @DisplayName("createRound 인자가 0 이하일 때 예외발생")
     public void createRound_ShouldThrow_IllegalArgumentException_OnBelowZero() {
         RacingGame racingGame = createDummyRacingGame();
+        List<String> dummyList = Collections.singletonList("a");
 
-        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(0, 5));
-        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(1, -5));
-        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(-1, -5));
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, 5));
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, -5));
+        assertThatIllegalArgumentException().isThrownBy(() -> racingGame.createRound(dummyList, -5));
     }
 
     private RacingGame createDummyRacingGame() {
