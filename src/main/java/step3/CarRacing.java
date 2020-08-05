31506f83468560ae06a85dc909eac8bedca07e2f
Zorba's Company <kulkulzaza@gmail.com>
2020-08-05 19:43:47 +0900

    test( UnitTest impl)

--- src/main/java/step3/CarRacing.java
+++ src/main/java/step3/CarRacing.java
@@ -1,82 +1,81 @@
 package step3;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
+
+public class CarRacing {
+
+   static int[][] doRacing(final int numOfCars,final int numberOfTries){
+        int[][] move = new int[numberOfTries+1][numOfCars];
+        Random random = new Random();
+
+        for(int i=1; i <= numberOfTries;i++) {
+            for(int car=0; car < numOfCars; car++){
+                int randomValue= random.nextInt(10);
+
+                if (randomValue >= 4 ){
+                    move[i][car] = move[i-1][car]+1;
+                    continue;
+                }
+                move[i][car] = move[i-1][car];
+            }
+        }
+        return move;
+   }
+}
 
 class FotStartRacing{
     public static void main(String[] args) {
         InputView.initShow();
 
-        int numberOfCars = InputView.cars;
-        int numberOfTries = InputView.tries;
+        int[][] result = CarRacing.doRacing(InputView.cars,InputView.tries);
 
-        CarRacing.doRacing(numberOfCars,numberOfTries);
+        ResultView.resultShow(result);
     }
 }
 
-public class CarRacing {
-
-   static void doRacing(final int cars,final int tries){
-        int[] carMove = new int[cars];
-        StringBuilder[] carMove2= new StringBuilder[cars];
-
-        for(int i= 0; i< carMove2.length;i++){
-            carMove2[i] = new StringBuilder("0");
-        }
-
-        Random random = new Random();
-        for(int i=0;i< tries;i++) {
-            for(int j=0; j< cars; j++){
-                int randomValue= random.nextInt(10);
-
-                carMove2[j].append(0);
-//                System.out.println(carMove2[j].toString());
-                if (randomValue >= 4 ){
-                    int endChar = carMove2[j].length()-1;
-                    char s = carMove2[j].charAt(endChar);
-
-                    int temp = Integer.valueOf(s-'0');
-                    int move= temp+1;
-
-
-                    carMove2[j].replace(endChar-1,endChar,String.valueOf(move));
-                    System.out.println(carMove2[j].toString());
-
-//                    carMove[j]++;
-                }
-            }
-//            ResultView.show(carMove);
-
-        }
-   }
-
-}
-
-class InputView{
+class InputView {
     static int cars;
     static int tries;
 
     static void initShow() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("자동차 대수는 몇 대 인가요?");
-        cars = sc.nextInt();
+        cars = inputCome();
 
         System.out.println("시도할 회수는 몇 회 인가요?");
-        tries = sc.nextInt();
+        tries = inputCome();
 
         System.out.println("\n실행 결과");
     }
+
+    static int inputCome(){
+        Scanner sc = new Scanner(System.in);
+        try{
+           return sc.nextInt();
+        }catch (InputMismatchException e){
+           e.printStackTrace();
+           inputCome();
+           return 0;
+        }
+    }
 }
 
-class ResultView{
-    static void show(int[] move){
-        for(int distance : move){
-            for(int i=0; i< distance;i++){
-                System.out.print("-");
+class ResultView {
+    static void resultShow(int[][] move){
+        int numOfTries = move.length;
+        int numOfCars = move[0].length;
+
+        for(int i=1; i< numOfTries; i++){
+            for(int j=0; j< numOfCars;j++){
+                int far = move[i][j];
+                for(int k=0; k< far; k++){
+                    System.out.print("-");
+                }
+                System.out.println();
             }
             System.out.println();
         }
-        System.out.println();
     }
 }
 
--- src/test/java/step3/CarRacingTest.java
+++ src/test/java/step3/CarRacingTest.java
@@ -1,11 +1,34 @@
 package step3;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarRacingTest {
 
+    @DisplayName("scanner 출력 검사")
+    @Test
+    void inputCome(){
+        InputView inputView = new InputView();
+
+        assertThatThrownBy(()->{
+
+        });
+        assertThat(InputView.inputCome()).;
+
+
+    }
+
+
     @Test
     void doRacing(){
+    }
+
+    @Test
+    void resultShow(){
+
     }
 
 }
