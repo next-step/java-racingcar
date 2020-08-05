9aa6f4e34bcb449e51c4d18e48856b21ed55f9d8
Jay <beginin15@gmail.com>
2020-08-06 02:47:58 +0900

    refactor: StringCalculator 제거, Operator에서 연산까지 수행하도록 변경
    - 기존 StringCalculatorTest의 테스트 내용을 Operator.compute 메소드로 테스트 재진행

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -3,14 +3,21 @@ package calculator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 public enum Operator {
-    PLUS,
-    MINUS,
-    MULTIPLY,
-    DIVIDE;
+    PLUS((x, y) -> x + y),
+    MINUS((x, y) -> x - y),
+    MULTIPLY((x, y) -> x * y),
+    DIVIDE((x, y) -> x / y);
 
     private static final Map<String, Operator> BY_SYMBOL = new HashMap<>();
+
+    private final BiFunction<Integer, Integer, Integer> function;
+
+    Operator(BiFunction<Integer, Integer, Integer> function) {
+        this.function = function;
+    }
 
     static {
         BY_SYMBOL.put("+", PLUS);
@@ -22,5 +29,9 @@ public enum Operator {
     public static Operator of(String symbol) {
         return Optional.ofNullable(BY_SYMBOL.get(symbol))
                 .orElseThrow(() -> new IllegalArgumentException("사칙연산 기호가 아닙니다."));
+    }
+
+    public Integer compute(int x, int y) {
+        return this.function.apply(x, y);
     }
 }
--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class StringCalculatorTest {
+class OperatorTest {
 
     private int x;
     private int y;
@@ -20,28 +20,28 @@ class StringCalculatorTest {
     @Test
     @DisplayName("덧셈 연산 테스트")
     void add() {
-        int result = StringCalculator.add(x, y);
+        int result = Operator.PLUS.compute(x, y);
         assertEquals(result, 5);
     }
 
     @Test
     @DisplayName("뺄셈 연산 테스트")
     void minus() {
-        int result = StringCalculator.minus(x, y);
+        int result = Operator.MINUS.compute(x, y);
         assertEquals(result, 1);
     }
 
     @Test
     @DisplayName("곱셈 연산 테스트")
     void multiply() {
-        int result = StringCalculator.multiply(x, y);
+        int result = Operator.MULTIPLY.compute(x, y);
         assertEquals(result, 6);
     }
 
     @Test
     @DisplayName("나눗셈 연산 테스트")
     void divide() {
-        int result = StringCalculator.divide(x, y);
+        int result = Operator.DIVIDE.compute(x, y);
         assertEquals(result, 1);
     }
 }
