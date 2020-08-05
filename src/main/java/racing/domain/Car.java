fa714e07d7d9f1c4fc3d72ebfe7d1fbc994b118f
hanogo <trullo393@gmail.com>
2020-08-05 18:49:21 +0900

    [feat] Car클래스 구현

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,5 +1,34 @@
 package racing.domain;
 
+import java.util.Random;
+
 public class Car {
-    int move;
+    static final int RANDOM_MAX_NUM = 10;
+    int location;
+
+    Car() {
+        this.location = 0;
+    }
+
+    public void run() {
+        if (isCondition(getRandomNum())) {
+            move();
+        }
+    }
+
+    public int getRandomNum() {
+        return new Random().nextInt(RANDOM_MAX_NUM);
+    }
+
+    public boolean isCondition(int randomNum) {
+        return Condition.checkMove(randomNum);
+    }
+
+    public void move() {
+        this.location++;
+    }
+
+    public int getLocation() {
+        return location;
+    }
 }
