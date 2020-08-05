3b410624db263a6081ec8666cf5c72598470f43d
Jay <beginin15@gmail.com>
2020-08-05 22:10:51 +0900

    feat: 입력 값이 null이거나 빈 공백 문자열인 경우, IllegalArgumentException 발생하도록 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.Objects;
+
 public class StringCalculator {
 
     public static int add(int x, int y) {
@@ -16,5 +18,12 @@ public class StringCalculator {
 
     public static int divide(int x, int y) {
         return x / y;
+    }
+
+    public static String[] splitExpression(String expression) {
+        if (Objects.isNull(expression) || expression.isEmpty()) {
+            throw new IllegalArgumentException("null 또는 빈 공백의 문자열입니다.");
+        }
+        return null;
     }
 }
