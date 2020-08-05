d74fb01df1ebe33d71dcb7c7f8b0d285568ef47d
Kim Daeyong <dev.kimdaeyong@gmail.com>
2020-08-06 01:51:04 +0900

    feature(view): ResultView printRanking method
    Add printRanking method to ResultView for printing Ranking and its implementation.

--- src/main/java/cc/oakk/racing/view/ResultView.java
+++ src/main/java/cc/oakk/racing/view/ResultView.java
@@ -1,9 +1,11 @@
 package cc.oakk.racing.view;
 
 import cc.oakk.racing.Car;
+import cc.oakk.racing.Ranking;
 import cc.oakk.racing.printer.Printable;
 
 public interface ResultView {
     void printResultHeader();
     void printCar(Printable<Car> car);
+    void printRanking(Printable<Ranking> ranking);
 }
--- src/main/java/cc/oakk/racing/view/ResultViewImpl.java
+++ src/main/java/cc/oakk/racing/view/ResultViewImpl.java
@@ -1,6 +1,7 @@
 package cc.oakk.racing.view;
 
 import cc.oakk.racing.Car;
+import cc.oakk.racing.Ranking;
 import cc.oakk.racing.printer.Printable;
 import cc.oakk.racing.printer.Printer;
 import cc.oakk.racing.printer.StandardPrinter;
@@ -8,15 +9,18 @@ import cc.oakk.racing.printer.StringPrinter;
 
 public class ResultViewImpl implements ResultView {
     private final StringPrinter<Car> carPrinter;
+    private final StringPrinter<Ranking> rankingPrinter;
     private final Printer<String> stringPrinter;
 
-    public ResultViewImpl(StringPrinter<Car> carPrinter) {
+    public ResultViewImpl(StringPrinter<Car> carPrinter, StringPrinter<Ranking> rankingPrinter) {
         this.carPrinter = carPrinter;
+        this.rankingPrinter = rankingPrinter;
         this.stringPrinter = new StandardPrinter();
     }
 
-    public ResultViewImpl(StringPrinter<Car> carPrinter, Printer<String> stringPrinter) {
+    public ResultViewImpl(StringPrinter<Car> carPrinter, StringPrinter<Ranking> rankingPrinter, Printer<String> stringPrinter) {
         this.carPrinter = carPrinter;
+        this.rankingPrinter = rankingPrinter;
         this.stringPrinter = stringPrinter;
     }
 
@@ -28,6 +32,12 @@ public class ResultViewImpl implements ResultView {
     @Override
     public void printCar(Printable<Car> car) {
         car.print(carPrinter);
+        stringPrinter.print("\n");
+    }
+
+    @Override
+    public void printRanking(Printable<Ranking> ranking) {
+        ranking.print(rankingPrinter);
         stringPrinter.print("\n");
     }
 }
