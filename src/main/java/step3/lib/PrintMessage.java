ba36f4d4b2f7acb56b5dd1cb3b4f95511afe3d63
yongdae <7942yongdae@gmail.com>
2020-08-05 16:41:05 +0900

    docs: PrintMessage 주석 추가

--- src/main/java/step3/lib/PrintMessage.java
+++ src/main/java/step3/lib/PrintMessage.java
@@ -1,21 +1,40 @@
 package step3.lib;
 
+/**
+ * 출력에 관한 Util 클래스
+ */
 public class PrintMessage {
 
     private PrintMessage() {}
 
+    /**
+     * 메세지 출력
+     * @param message
+     */
     public static void print(String message) {
         System.out.printf(message);
     }
 
+    /**
+     * 메세지 출력
+     * @param format
+     * @param message
+     */
     public static void print(String format, String message) {
         System.out.printf(format, message);
     }
 
+    /**
+     * 메세지 출력
+     */
     public static void println() {
         System.out.println();
     }
 
+    /**
+     * 메세지 출력
+     * @param message
+     */
     public static void println(String message) {
         System.out.println(message);
     }
