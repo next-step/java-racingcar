1df5727100f2b7d5bd8b1788d68e7ef0db591427
Jay <beginin15@gmail.com>
2020-08-06 02:30:04 +0900

    test: Operator의 of 메소드 테스트
    - 사칙연산 기호가 아닌 경우, IllegalArgumentException을 발생시키지는지 테스트

--- src/test/java/calculator/ExpressionProcessorTest.java
+++ src/test/java/calculator/ExpressionProcessorTest.java
@@ -1,9 +1,11 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class ExpressionProcessorTest {
@@ -14,6 +16,23 @@ class ExpressionProcessorTest {
     void splitExpression(String expression) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             String[] arguments = ExpressionProcessor.splitExpression(expression);
+        });
+    }
+
+    @Test
+    @DisplayName("사칙연산 기호 테스트")
+    void isOperator() {
+        assertThat(Operator.of("+")).isEqualTo(Operator.PLUS);
+        assertThat(Operator.of("-")).isEqualTo(Operator.MINUS);
+        assertThat(Operator.of("*")).isEqualTo(Operator.MULTIPLY);
+        assertThat(Operator.of("/")).isEqualTo(Operator.DIVIDE);
+    }
+
+    @Test
+    @DisplayName("사친연산 기호가 아닌 경우")
+    void isNotOperator() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Operator operator = Operator.of("!");
         });
     }
 }
