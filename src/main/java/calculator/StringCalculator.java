1589b412ace3a850647b6d0ed5abed4c9c433d2a
Jay <beginin15@gmail.com>
2020-08-05 21:33:39 +0900

    feat: 사칙 연산 메소드 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class StringCalculator {
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int minus(int x, int y) {
+        return x - y;
+    }
+
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public static int divide(int x, int y) {
+        return x / y;
+    }
+}
