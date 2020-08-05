d7364fa3ad5c843c731eb08a725600c6ddefd8e8
John <bass0223@naver.com>
2020-08-05 13:44:01 +0900

    게임을 인터페이스를 이용하여 처리함

--- src/main/java/simpleracing/execute/CarRacingGame.java
+++ src/main/java/simpleracing/execute/CarRacingGame.java
@@ -1,30 +1,18 @@
 package simpleracing.execute;
 
-import static java.util.stream.Collectors.toList;
-
-import java.util.List;
 import java.util.Random;
-import java.util.stream.IntStream;
 
-import simpleracing.input.CarRacingInitValue;
-
-public class CarRacingGame {
+public class CarRacingGame implements CarGame {
 
 	private static final int RANDOM_MAX_VALUE = 10;
 	private Random randomValueGenerator;
-	private CarRacingInitValue initValue;
 
-	public CarRacingGame(CarRacingInitValue initValue) {
-		this.initValue = initValue;
+	public CarRacingGame() {
 		this.randomValueGenerator = new Random();
 	}
 
-	public List<Car> play() {
-		return IntStream.range(0, initValue.getCarCount())
-						.mapToObj(carNumber -> new Car(carNumber,
-													   IntStream.range(0, initValue.getTryCount())
-																.mapToObj(tryCount -> randomValueGenerator.nextInt(RANDOM_MAX_VALUE))
-																.collect(toList())))
-						.collect(toList());
+	@Override
+	public void play(Car car) {
+		car.addGameResult(randomValueGenerator.nextInt(RANDOM_MAX_VALUE));
 	}
 }
