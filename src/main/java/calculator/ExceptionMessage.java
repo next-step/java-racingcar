bda8e3dfeace4dd3dbbb11a65e09fc8d7e2afc7e
김다희 <sefide36@tmon.co.kr>
2020-08-05 10:25:42 +0900

    refactor: ExceptionMessage private 생성자 추가(생성 제한)

--- src/main/java/calculator/ExceptionMessage.java
+++ src/main/java/calculator/ExceptionMessage.java
@@ -1,6 +1,9 @@
 package calculator;
 
 public class ExceptionMessage {
+
+    private ExceptionMessage() {}
+
     public static final String INVALID_INPUT_STRING = "입력값은 필수입니다.";
     public static final String INVALID_OPERATION_SYMBOL = "입력한 값이 사칙연산 기호에 해당되지 않습니다.";
     public static final String INVALID_INPUT_NUMBER = "은 숫자가 아닙니다. 적절한 숫자를 입력해주세요.";
