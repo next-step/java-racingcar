5559a2cdaee83f8f11210bf4840cad00b9caa56e
yongdae <7942yongdae@gmail.com>
2020-08-05 16:37:46 +0900

    refactor: 메세지 정의 담당 분리 - 사용자로부터 입력시 출력되는 메세지

--- src/main/java/step3/Main.java
+++ src/main/java/step3/Main.java
@@ -5,8 +5,8 @@ import step3.view.InputScanner;
 
 public class Main {
     public static void main(String[] args) {
-        String inputNamesOfCars = InputScanner.getInputNamesOfCars();
-        int inputNumberOfTry = InputScanner.getInputNumberOfTry();
+        String inputNamesOfCars = InputScanner.getString("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        int inputNumberOfTry = InputScanner.getInt("시도할 회수는 몇 회 인가요?");
 
         RacingGame racingGame = new RacingGame(
                 inputNamesOfCars,
--- src/main/java/step3/view/InputScanner.java
+++ src/main/java/step3/view/InputScanner.java
@@ -4,7 +4,7 @@ import step3.lib.PrintMessage;
 
 import java.util.Scanner;
 
-/***
+/**
  *  사용자의 입력을 받아 입력한 값을 반환해주는 작업을 처리하는 클래스
  */
 public class InputScanner {
@@ -13,13 +13,23 @@ public class InputScanner {
 
     private InputScanner() {}
 
-    public static int getInputNumberOfTry() {
-        PrintMessage.println("시도할 회수는 몇 회 인가요?");
+    /**
+     * message를 받아 출력과 동시에 사용자 int 형 입력 값을 받는다.
+     * @param message
+     * @return
+     */
+    public static int getInt(String message) {
+        PrintMessage.println(message);
         return SCANNER.nextInt();
     }
 
-    public static String getInputNamesOfCars() {
-        PrintMessage.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    /**
+     * message를 받아 출력과 동시에 사용자 String 형 입력 값을 받는다.
+     * @param message
+     * @return
+     */
+    public static String getString(String message) {
+        PrintMessage.println(message);
         return SCANNER.next();
     }
 }
