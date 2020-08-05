0aa4b03414c68cc1c91b0b5848071bacf2c72b4f
John <bass0223@naver.com>
2020-08-05 13:44:55 +0900

    리펙토링 결과에 따라 실행기로 바뀜

--- src/main/java/simpleracing/execute/CarRacingExecutor.java
+++ src/main/java/simpleracing/execute/CarRacingExecutor.java
@@ -1,27 +1,47 @@
 package simpleracing.execute;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
+import java.util.stream.IntStream;
 
 import simpleracing.input.CarRacingInitValue;
+import simpleracing.input.CarRacingInput;
 import simpleracing.input.InputView;
-import simpleracing.output.OutputView;
+import simpleracing.output.CarRacingOutput;
+import simpleracing.view.OutputView;
 
 
 public class CarRacingExecutor {
 
+	InputView carRacingInput;
+	private CarGame racingGame;
+	private OutputView carRacingOutput;
+
+	public CarRacingExecutor() {
+		this.carRacingInput = new CarRacingInput();
+		this.racingGame = new CarRacingGame();
+		this.carRacingOutput = new CarRacingOutput();
+	}
+
 	public void execute() {
-		CarRacingInitValue initValue = InputView.input();
-		List<Car> cars = this.playGame(initValue);
-		this.render(cars);
+		CarRacingInitValue initValue = carRacingInput.input();
+
+		List<Car> cars = IntStream.range(0, initValue.getCarCount())
+								  .mapToObj(carNumber -> new Car(carNumber, this.racingGame))
+								  .collect(toList());
+
+		this.playGame(initValue, cars);
+		carRacingOutput.render();
+
 	}
 
-	private List<Car> playGame(CarRacingInitValue initValue) {
-		CarRacingGame game = new CarRacingGame(initValue);
-		return game.play();
-	}
-
-	private void render(List<Car> cars) {
-		OutputView outputView = new OutputView(cars);
-		outputView.render();
+	private void playGame(CarRacingInitValue initValue, List<Car> cars) {
+		IntStream.range(0, initValue.getTryCount())
+				 .forEach(round -> {
+					 cars.stream()
+						 .forEach(car -> car.play());
+					 this.carRacingOutput.addRenderingView(cars);
+				 });
 	}
 }
