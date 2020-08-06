b11039385c27157950d588a827e367c29fdf686f
hanogo <trullo393@gmail.com>
2020-08-07 00:57:55 +0900

    [feat] 메세지 추가

--- src/main/java/racing/message/ViewMsg.java
+++ src/main/java/racing/message/ViewMsg.java
@@ -4,7 +4,8 @@ public enum ViewMsg {
     START_MSG_CAR("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)"),
     START_MSG_REPEAT("시도할 회수는 몇 회 인가요?"),
     RESULT_VIEW_START("실행 결과"),
-    END_LINE("****************************");
+    END_LINE("****************************"),
+    VICTORY_MSG("가 최종 우승했습니다.");
 
 
     String msg;
