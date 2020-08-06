dd303df8e2041663bf7b9edce7544440d795bc92
hanogo <trullo393@gmail.com>
2020-08-06 22:48:43 +0900

    [feat] checkName기능 추가

--- src/main/java/racing/domain/Condition.java
+++ src/main/java/racing/domain/Condition.java
@@ -2,9 +2,13 @@ package racing.domain;
 
 public class Condition {
     private static final int VALUE_MIN_NUM = 4;
+    private static final int VALUE_MAX_NAME = 5;
 
     public static boolean checkMove(int randomNum) {
         return randomNum >= VALUE_MIN_NUM;
     }
 
+    public static boolean checkName(String name) {
+        return name.length() <= VALUE_MAX_NAME;
+    }
 }
