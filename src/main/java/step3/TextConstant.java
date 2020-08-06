3cd8792837b4e8c9b6d9d6e01d4f2efd34a60d61
hJJANG <dacapolife87@gmail.com>
2020-08-06 23:57:02 +0900

    feat : 경기에 참가자 수 및 경기진행할 라운드를 입력받은 InputView
    InputView를 통하여 경기의 참가자수 및 경기 진행할 라운드를 입력받고 그내용을 Competition객체에 저장하여 리턴.
    입력받을때 문구는 상수로 저장하여 출력되도록 하였습니다.

---
+++ src/main/java/step3/TextConstant.java
@@ -0,0 +1,8 @@
+package step3;
+
+public class TextConstant {
+
+    public static final String QUESTION_HOW_MANY_CAR = "자동차 대수는 몇 대 인가요?";
+
+    public static final String QUESTION_HOW_MANY_ROUND = "시도할 회수는 몇 회 인가요?";
+}
