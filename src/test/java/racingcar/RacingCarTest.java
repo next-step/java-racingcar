899a5f099c41b07d4277cf4944c7b9995e5e7041
sefide <sefide@naver.com>
2020-08-07 00:43:18 +0900

    test: RacingCar 테스트코드 작성

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,12 +1,43 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class RacingCarTest {
+    private static final int ATTEMPT_NUMBER = 4;
+    private static Random random;
 
+    @BeforeAll
+    static void setUp() {
+        random = new Random();
+    }
+
+    @DisplayName("race 메소드 실행 전 테스트")
     @Test
-    void race() {
+    void race_before_test() {
+        RacingCar racingCar = RacingCar.create(random);
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> racingCar.getRaceRecord().getBy(0))
+                .withMessageMatching("Index: \\d+, Size: \\d+");
+    }
+
+    @DisplayName("race 메소드 실행 후 테스트")
+    @Test
+    void race_after_test() {
+        RacingCar racingCar = RacingCar.create(random);
+
+        racingCar.race(ATTEMPT_NUMBER);
+
+        // racingCar의 raceRecord 생성 여부 확인
+        assertThat(racingCar.getRaceRecord()).isNotNull();
+        // racingCar의 raceRecord가 마지막 attempt까지 잘 생성되었는지 확인
+        assertThat(racingCar.getRaceRecord().getBy(ATTEMPT_NUMBER - 1)).isNotNull();
     }
 }
