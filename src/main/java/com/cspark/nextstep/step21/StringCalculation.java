165da1753b3900b368934e0a1fc68303fbcd37b9
Chanseok Park <pcspapa@inzido.co.kr>
2020-08-06 20:14:18 +0900

    refactor: 메소드 추출

--- src/main/java/com/cspark/nextstep/step21/StringCalculation.java
+++ src/main/java/com/cspark/nextstep/step21/StringCalculation.java
@@ -5,27 +5,34 @@ import java.util.Objects;
 public class StringCalculation {
 
   public static int calculate(String sentence) {
+    inputValidate(sentence);
+
+    String[] values = sentence.split(" ");
+
+    return calculate(values[1], toInt(values[0]), toInt(values[2]));
+  }
+
+  private static void inputValidate(String sentence) {
     if (Objects.isNull(sentence) || sentence.trim().length() == 0) {
       throw new IllegalArgumentException("잘못된 입력값입니다.");
     }
+  }
 
-    String[] values = sentence.split(" ");
-    String operator = values[1];
-
+  private static Integer calculate(String operator, int first, int second) {
     if (operator.equals("+")) {
-      return toInt(values[0]) + toInt(values[2]);
+      return first + second;
     }
 
     if (operator.equals("-")) {
-      return toInt(values[0]) - toInt(values[2]);
+      return first - second;
     }
 
     if (operator.equals("*")) {
-      return toInt(values[0]) * toInt(values[2]);
+      return first * second;
     }
 
     if (operator.equals("/")) {
-      return toInt(values[0]) / toInt(values[2]);
+      return first / second;
     }
 
     throw new IllegalArgumentException("지원하지 않는 연산입니다.");
