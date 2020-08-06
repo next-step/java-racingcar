86065bf860604e09fbb3b4d8e02ca43af5199f1f
sefide <sefide@naver.com>
2020-08-07 00:15:52 +0900

    test: RacingResult 테스트코드 작성

---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingCarTest {
+
+    @Test
+    void race() {
+    }
+}
---
+++ src/test/java/racingcar/RacingGameTest.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingGameTest {
+
+}
