e84e86cd63ac227580c92708b149efa0cf8c47f6
hJJANG <dacapolife87@gmail.com>
2020-08-07 07:55:10 +0900

    feat : 차량의 참가번수를 출력해주는기능
    Round별 차량의 위치를 표시할떄 맽앞에 차량의 참가번호를 나타내는기능입니다.

--- src/main/java/step3/dto/Car.java
+++ src/main/java/step3/dto/Car.java
@@ -4,6 +4,9 @@ public class Car {
     private int entryNumber;
     private int position;
 
+    private final String NAME_TAG_SEPERATOR_SEMICOLLON = "|";
+    private final String NAME_TAG_EMTPY_SPACE = "    ";
+
     private Car(int entryNumber, int position) {
         this.entryNumber = entryNumber;
         this.position = position;
@@ -21,4 +24,21 @@ public class Car {
         return position;
     }
 
+    public String getEntryNameTag(){
+
+        String entryNumber = getEntryNumberToString();
+
+        StringBuilder nameTag = new StringBuilder();
+        int length = entryNumber.length();
+
+        String newPrefix = NAME_TAG_EMTPY_SPACE.substring(length);
+        nameTag.append(entryNumber).append(newPrefix);
+        nameTag.append(NAME_TAG_SEPERATOR_SEMICOLLON);
+
+        return nameTag.toString();
+    }
+
+    private String getEntryNumberToString() {
+        return String.valueOf(entryNumber);
+    }
 }
