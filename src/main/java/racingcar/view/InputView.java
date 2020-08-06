9ff0d552360f49b00778b23e82e91d7c37db69d2
sefide <sefide@naver.com>
2020-08-06 23:37:35 +0900

    feat: 초기 설정값 입력 기능

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,54 @@
+package racingcar.view;
+
+import racingcar.RacingGame;
+import util.StringUtils;
+
+import java.util.Scanner;
+
+/**
+ * @author daheekim
+ * @version : 0.0.0
+ */
+public class InputView {
+
+    private Scanner scanner;
+
+    public InputView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public RacingGame scanRacingGameInfo() {
+        int carNumber = getCarNumber();
+        int attemptNumber = getAttemptNumber();
+
+        RacingGame racingGame = RacingGame.of(carNumber, attemptNumber);
+
+        scanner.close();
+        return racingGame;
+    }
+
+    private int getCarNumber() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        String value = scanner.nextLine();
+
+        for (int i = 0; i < 3; i++) {
+            if (StringUtils.validateNumber(value)) {
+                return Integer.parseInt(value);
+            }
+        }
+        return 0;
+    }
+
+    private int getAttemptNumber() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        String value = scanner.nextLine();
+
+        for (int i = 0; i < 3; i++) {
+            if (StringUtils.validateNumber(value)) {
+                return Integer.parseInt(value);
+            }
+        }
+        return 0;
+    }
+
+}
