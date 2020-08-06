ae023a23c25bc10bc0d2c37e6f437e48c9fe2d3b
pjhsk113 <pjhsk113@naver.com>
2020-08-06 20:18:22 +0900

    [feat] Cars, CarsTest, Random 값 생성기 추가

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,43 @@
+package racingcar.domain;
+
+import racingcar.utils.RandomValueGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 경주에 참여하는 자동차들의 정보를 담당
+ * Car를 바탕으로 경주에 참여하는 그룹을 만든다.
+ * 각 자동차들은 랜덤값을 받아 현재 위치를 List 형태로 저장하게 된다.
+ */
+public class Cars {
+    private static final int MIN_INPUT_CAR_NUMBER = 1;
+    private List<Car> cars;
+
+    public Cars(int inputCarCount) {
+        validate(inputCarCount);
+        createCars(inputCarCount);
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void createCars(int inputCarCount) {
+        cars = new ArrayList<>();
+        for (int i = 0; i< inputCarCount; i++) {
+            cars.add(new Car());
+        }
+    }
+    public void moveCars() {
+        for (Car car : cars) {
+            car.moveCount(RandomValueGenerator.createRandomValue());
+        }
+    }
+
+    public void validate(int inputCarCount) {
+        if (inputCarCount < MIN_INPUT_CAR_NUMBER) {
+            throw new IllegalArgumentException("자동차는 1대 이상 입력되어야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/racingcar/utils/RandomValueGeneratorTest.java
@@ -0,0 +1,13 @@
+package racingcar.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RandomValueGeneratorTest {
+
+    @DisplayName("지정 범위에서 값이 잘 생성되는지 테스트")
+    @Test
+    public void 랜덤값은_0부터_9까지_생성된다() {
+        // 어떻게 테스트를 해야할지 잘 모르겠습니다...ㅠㅠ
+    }
+}
