ed0c57fed5d32476db86a92c6ed9773c46f99bf3
hJJANG <dacapolife87@gmail.com>
2020-08-07 07:38:24 +0900

    feat : 라운드 진행시 차량들의 이동을 처리하는 객체
    라운드별로 차량들의 이동여부를 관리하는 기능입니다.

---
+++ src/main/java/step3/RacingRound.java
@@ -0,0 +1,22 @@
+package step3;
+
+import step3.dto.Car;
+
+import java.util.List;
+
+public class RacingRound {
+
+    public void playRound(List<Car> entryList) {
+        for(Car car : entryList){
+            goAndStopThisCar(car);
+        }
+    }
+
+    private void goAndStopThisCar(Car car){
+        if(MoveCar.moveCondition()) return;
+
+        car.movePosition();
+    }
+
+
+}
