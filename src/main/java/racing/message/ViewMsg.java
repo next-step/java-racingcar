08deb0ebc5d9ff7ce62481c41d09db1aff3761b7
hanogo <trullo393@gmail.com>
2020-08-06 20:54:13 +0900

    [refactor] 시작 메세지 수정

--- src/main/java/racing/message/ViewMsg.java
+++ src/main/java/racing/message/ViewMsg.java
@@ -1,7 +1,7 @@
 package racing.message;
 
 public enum ViewMsg {
-    START_MSG_CAR("자동차 대수는 몇 대 인가요?"),
+    START_MSG_CAR("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)"),
     START_MSG_REPEAT("시도할 회수는 몇 회 인가요?"),
     RESULT_VIEW_START("실행 결과"),
     END_LINE("****************************");
