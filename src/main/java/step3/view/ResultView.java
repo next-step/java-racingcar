b85328c22d263c98a953471cdcf415656bdcad61
hJJANG <dacapolife87@gmail.com>
2020-08-07 00:17:02 +0900

    feat : 참가자의 위치를 표시하는 기능
    참가자의 리스트를 전달받아 참여자의 이동 경로를"-"기호를 사용하여 출력을 해줍니다.

---
+++ src/main/java/step3/view/ResultView.java
@@ -0,0 +1,28 @@
+package step3.view;
+
+import step3.Car;
+
+import java.util.List;
+
+public class ResultView {
+
+    private final String CAR_MOVING_PATH = "-";
+
+    public void showCompeetitionPosition(List<Car> cars){
+        for(Car car : cars){
+            printThisCarPosition(car);
+        }
+    }
+
+    private void printThisCarPosition(Car car){
+        int nowPosition = car.nowPosition();
+
+        StringBuilder movePosition = new StringBuilder();
+
+        for(int i = 0; i < nowPosition; i++){
+            movePosition.append(CAR_MOVING_PATH);
+        }
+
+        System.out.println(movePosition.toString());
+    }
+}
