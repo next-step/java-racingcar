e17658f49eff5c10ff7ed4f8f79f4b24b20783a7
jungmisu <jungmisu@tmon.co.kr>
2020-08-07 20:01:21 +0900

    refactor: 코드 리뷰 반영

--- src/main/java/stringcalculator/OperatorType.java
+++ src/main/java/stringcalculator/OperatorType.java
@@ -1,31 +1,23 @@
 package stringcalculator;
 
 import java.util.Arrays;
-import java.util.Map;
 import java.util.function.BiFunction;
-import java.util.stream.Collectors;
 
 public enum OperatorType {
-    plus("+", (n1, n2) -> n1 + n2),
-    minus("-", (n1, n2) -> n1 - n2),
-    multiply("*", (n1, n2) -> n1 * n2),
-    divide("/", (n1, n2) -> n1 / n2);
+    PLUS("+", (n1, n2) -> n1 + n2),
+    MINUS("-", (n1, n2) -> n1 - n2),
+    MULTIPLY("*", (n1, n2) -> n1 * n2),
+    DIVIDE("/", (n1, n2) -> n1 / n2);
 
     private String operator;
     private BiFunction<Integer, Integer, Integer> expression;
-    private final static Map<String, OperatorType> calculateMap;
-
-    static {
-        calculateMap = Arrays.stream(OperatorType.values())
-                .collect(Collectors.toMap(OperatorType::getOperator, o->o));
-    }
 
     OperatorType(String operator, BiFunction<Integer, Integer, Integer> expression) {
         this.operator = operator;
         this.expression = expression;
     }
 
-    public String getOperator() {
+    private String getOperator() {
         return operator;
     }
 
@@ -34,11 +26,9 @@ public enum OperatorType {
     }
 
     public static OperatorType getOperatorType(String key) {
-        if (!calculateMap.containsKey(key)) {
-            throw new IllegalArgumentException("사칙 연산 기호가 아닙니다.");
-        }
-        return calculateMap.get(key);
+        return Arrays.stream(values())
+                .filter(o -> o.getOperator().equals(key))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("사칙 연산 기호가 아닙니다."));
     }
-
-
 }
