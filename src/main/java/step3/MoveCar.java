d91c793f46a0aab7e4cba382f46382831fe3d08d
hJJANG <dacapolife87@gmail.com>
2020-08-07 22:47:30 +0900

    refactor : 의미가 있는 숫자들의 상수화
    랜덤값의 범위인 10과 이동기준이되는 4를 상수화하였습니다.

--- src/main/java/step3/MoveCar.java
+++ src/main/java/step3/MoveCar.java
@@ -4,11 +4,15 @@ import java.util.Random;
 
 public class MoveCar {
 
-    public static boolean moveCondition() {
-        int randomVal = new Random().nextInt(10);
+    private static final int RANDOM_MAXIMUM_BOUND = 10;
+    private static final int MOVE_CRITERION_UNDER_4 = 4;
 
-        if(randomVal < 4) return false;
+    public static boolean moveCondition() {
+        int randomVal = new Random().nextInt(RANDOM_MAXIMUM_BOUND);
+
+        if(randomVal < MOVE_CRITERION_UNDER_4) return false;
 
         return true;
     }
+
 }
