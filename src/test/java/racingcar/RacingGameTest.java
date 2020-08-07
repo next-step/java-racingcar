1ba078d88a5a14b45aace6fe79a1b0bbd5263b12
slamdunk7575 <slamdunk7575@gmail.com>
2020-08-08 04:28:20 +0900

    (test) 테스트 코드 수정

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,47 +1,61 @@
 package racingcar;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import racingcar.model.RacingRecord;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.model.RacingRound;
+import racingcar.view.ResultView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class RacingGameTest {
 
-    private RacingGameStub racingGameStub;
-
-    @BeforeAll
-    void setUp() {
-        int racingCarNumber = 3;
-        int racingCount = 5;
-        racingGameStub = new RacingGameStub(racingCarNumber, racingCount);
-    }
-
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"YANG,KYUNG,JUN"})
     @DisplayName("자동차 경주 게임 동작 테스트")
-    void startTest() {
+    void startTest(String carNames) {
+
         // given
-        String expectedMark1 = "-\n-\n-";
-        String expectedMark2 = "--\n--\n--";
-        String expectedMark3 = "---\n---\n---";
-        String expectedMark4 = "----\n----\n----";
-        String expectedMark5 = "-----\n-----\n-----";
+        String expectedMark1 = "YANG : -\nKYUNG : -\nJUN : -\n";
+        String expectedMark2 = "YANG : --\nKYUNG : --\nJUN : --\n";
+        String expectedMark3 = "YANG : ---\nKYUNG : ---\nJUN : ---\n";
 
         // when
-        RacingRecord racingRecord = racingGameStub.start();
+        List<String> carNameList = Arrays.asList(carNames.split(","));
+        RacingGameStub racingGameStub = new RacingGameStub(carNameList, carNameList.size());
+        racingGameStub.start();
+        List<RacingRound> racingResults = racingGameStub.getRacingResults();
+
+        ResultView resultView = new ResultView(racingResults);
+
+        List<String> displayResult = racingResults.stream()
+                .map(racingRound -> resultView.printRoundResult(racingRound))
+                .collect(Collectors.toList());
 
         // then
         assertAll(
-                () -> assertThat(racingRecord.getAllRecord()).hasSize(5),
-                () -> assertThat(racingRecord.getAllRecord()).contains(expectedMark1),
-                () -> assertThat(racingRecord.getAllRecord()).contains(expectedMark2),
-                () -> assertThat(racingRecord.getAllRecord()).contains(expectedMark3),
-                () -> assertThat(racingRecord.getAllRecord()).contains(expectedMark4),
-                () -> assertThat(racingRecord.getAllRecord()).contains(expectedMark5)
+                () -> assertThat(displayResult).hasSize(3),
+                () -> assertThat(displayResult).contains(expectedMark1),
+                () -> assertThat(displayResult).contains(expectedMark2),
+                () -> assertThat(displayResult).contains(expectedMark3)
         );
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"YANG,KYUNG,JUN7575"})
+    @DisplayName("자동차 이름 유효성 검사 테스트")
+    public void validate_car_names(String carNames) {
+        List<String> carNameList = Arrays.asList(carNames.split(","));
+        RacingGameStub racingGameStub = new RacingGameStub(carNameList, carNameList.size());
+        assertEquals(Boolean.FALSE, racingGameStub.validateCarName(carNameList));
     }
 }
