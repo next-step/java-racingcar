7369d235c3552fe240d46324f05111ae7f933c53
John <bass0223@naver.com>
2020-08-07 14:52:44 +0900

    OutputView 인터페이스 삭제와 우승자 출력을 위한 메서드 추가

--- src/main/java/simpleracing/output/CarRacingOutput.java
+++ src/main/java/simpleracing/output/CarRacingOutput.java
@@ -1,12 +1,13 @@
 package simpleracing.output;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import simpleracing.execute.Car;
+import simpleracing.execute.CarGame;
 import simpleracing.execute.Direction;
-import simpleracing.view.OutputView;
 
-public class CarRacingOutput implements OutputView {
+public class CarRacingOutput {
 
 	private String result;
 
@@ -14,7 +15,6 @@ public class CarRacingOutput implements OutputView {
 		result = Direction.STOP.getSign();
 	}
 
-	@Override
 	public void addRenderingView(List<Car> cars) {
 		cars.stream()
 			.forEach(car -> result += car.getName() + " : " + car.getLocation() + "\n");
@@ -22,7 +22,15 @@ public class CarRacingOutput implements OutputView {
 		result += "\n";
 	}
 
-	@Override
+	public void addWinner(CarGame racingGame) {
+		result += "우승자는 " +
+				  racingGame.getWinner()
+							.stream()
+							.map(car -> car.getName())
+							.collect(Collectors.joining("와 "))
+				  + "입니다.";
+	}
+
 	public void render() {
 		System.out.println("실행 결과");
 		System.out.println(result);
