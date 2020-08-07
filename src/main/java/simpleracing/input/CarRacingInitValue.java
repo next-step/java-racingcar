06251fd459f41de1b0d70bf9ac7df45a3f990452
John <bass0223@naver.com>
2020-08-07 14:55:57 +0900

    변수가 사용하지 않는 경우도 있기 때문에 wrapper 객체인 Integer로 변경하였고, carCount 값도 상황에 따라 다르게 가져오도록 처리함

--- src/main/java/simpleracing/input/CarRacingInitValue.java
+++ src/main/java/simpleracing/input/CarRacingInitValue.java
@@ -8,12 +8,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CarRacingInitValue {
-	private int carCount;
-	private int tryCount;
+	private Integer carCount;
+	private Integer tryCount;
 	private List<String> names;
 
 	public void setNames(List<String> names) {
 		this.names = names;
-		this.carCount = names.size();
+	}
+
+	public int getCarCount() {
+		if (carCount != null) {
+			return carCount;
+		}
+		return names == null || names.size() == 0 ? 0 : names.size();
 	}
 }
