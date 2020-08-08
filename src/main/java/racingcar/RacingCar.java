977c2c8675eb488624bfd8f9ee6cd0b050816fc6
sefide <sefide@naver.com>
2020-08-09 00:06:47 +0900

    fix: 시도 횟수 조건 누락에 따른 이슈 픽스

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,7 +3,13 @@ package racingcar;
 public class RacingCar extends RacingVehicle {
     private static final int FIRST_ATTEMPT = 0;
 
-    public void race(int attempt) {
+    public void race(int attemptNumber) {
+        for (int attempt = 0; attempt < attemptNumber; attempt++) {
+            move(attempt);
+        }
+    }
+
+    private void move(int attempt) {
         int move = (attempt == FIRST_ATTEMPT)
                 ? 0
                 : raceRecord.getBy(attempt - 1);
