568aaede7790c0239f19adde0e275af955f1d76f
hJJANG <dacapolife87@gmail.com>
2020-08-08 23:06:14 +0900

    test : 랜덤값에 따른 차량이동 결과 확인 테스트
    랜덤값을 처리하는 메소드에서 차량이동 처리 로직 수정에 대한 테스트를 추가하였습니다.

--- src/test/java/step3/dto/CarTest.java
+++ src/test/java/step3/dto/CarTest.java
@@ -3,13 +3,14 @@ package step3.dto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import step3.MoveCar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"1:'1   |'","2:'2   |'","3:'3   |'"}, delimiter = ':')
+    @CsvSource(value = {"1:'CAR1   |'","2:'CAR2   |'","3:'CAR3   |'"}, delimiter = ':')
     @DisplayName("참가자의 이름 Tag가 제대로 출력되는지 확인")
     void carNameTagTest(int entryNumber,String nameTag){
         Car car = Car.participateRacing(entryNumber,0);
@@ -25,5 +26,20 @@ class CarTest {
         car.movePosition();
         int nowPosition = car.nowPosition();
         assertThat(nowPosition).isEqualTo(endPosition);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:2","2:1","3:0"}, delimiter = ':')
+    @DisplayName("차량이 랜덤함수의 결과대로 이동했는지 확인")
+    void carMovePosition2Test(int entryNumber,int startPositionString){
+        Car car = Car.participateRacing(entryNumber,startPositionString);
+        boolean moveCondition = MoveCar.moveCondition(car);
+
+        if(moveCondition){
+            assertThat(car.nowPosition()).isEqualTo(startPositionString+1);
+        }else{
+            assertThat(car.nowPosition()).isEqualTo(startPositionString);
+        }
+
     }
 }
