a922b2d391f4fcb0bd2657e9f373e523cf2c4e04
rlxodus1(기태연) <rlxodus1@gmail.com>
2020-08-08 15:30:50 +0900

    1. 요구사항에 맞춰 작성해보려하였으나 TDD가 처음이라 맞는 방법인지 모르겠씁니다.
    2. 빈값확인 isBlank_ShouldReturnTrueForNullOrBlankStrings 은 정상 작동하는데
       숫자, 사칙연산 확인 isNumber isCalculate 은
       java.lang.AssertionError:
       Expecting code to raise a throwable.
       이 발생하고 있는데 구글링해도 해결법을 잘모르겠습니다..ㅠㅠ
    
    확인부탁드립니다..!

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,2 +1,38 @@
-package study;public class StringTest {
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    /*
+     * 1단계 - 학습테스트 실습
+     * String 클래스에 대한 학습 테스트
+     */
+
+    // 요구사항 1
+    @Test
+    //Alt+Insert > method auto create
+    public void split() {
+        String[] values = "1,2".split(",");     //regex ?
+        assertThat(values) .contains("1", "2");  //Alt+Enter > Library Import
+    }
+
+    // 요구사항 2
+    @Test
+    public void substring() {
+        String value = "(1,2)";
+        String substrValue = value.substring(1,4);
+        System.out.println(substrValue);
+    }
+
+    // 요구사항 3
+    @Test
+    public void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(2);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
 }
