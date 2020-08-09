f89d3a9a15bb7a0c4ab0caa54880c15f6024a09c
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-09 17:19:29 +0900

    (feat) Car.move() takes (Condition, RaceStrategy) returns (position)

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,10 @@
 package racingcar.domain;
 
 import racingcar.strategy.DoRace;
+import racingcar.strategy.RaceCondition;
 
 public class Car {
-    //추상화 클래스? private 생성자 통한 인스턴스화 방지 가능? private Car(){}
-    public Car(){}
     private int position;
-    private DoRace doRace;
 
     public Car(int position) {
         this.position = position;
@@ -16,16 +14,9 @@ public class Car {
         return position;
     }
 
-    public void setDoRace(DoRace doRace) {
-        this.doRace = doRace;
-    }
-
-    /* Test Case 예시
-       car.setDoRace(new DoOneForward());
-       car.move();
-       : OneForward()만큼 움직이는지 확인
-     */
-    public int move() {
-        return doRace.move();
+    public int move(RaceCondition raceCondition, DoRace doRace) {
+        if (raceCondition.pass()) {
+            return position += doRace.race();
+        } return position;
     }
 }
