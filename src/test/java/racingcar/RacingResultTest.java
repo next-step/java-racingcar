2abba512b0d00245ceb3a7d62eaf62eb65634577
sefide <sefide@naver.com>
2020-08-09 23:18:47 +0900

    test: RacingResultTest 테스트코드 작성

--- src/test/java/racingcar/RacingResultTest.java
+++ src/test/java/racingcar/RacingResultTest.java
@@ -3,31 +3,39 @@ package racingcar;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingResultTest {
     private static final int ATTEMPT_NUMBER = 5;
+    private static final CarMover RANDOM_NUMBER_CAR_MOVER = new RandomNumberMover();
 
-    private List<RacingVehicle> racingCars;
+    private RacingCars racingCars;
 
     @BeforeEach
     void setUp() {
-        racingCars = new ArrayList<>();
-        Random random = new Random();
+        List<RacingCar> racingCarList = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++) {
-            RacingCar racingCar = new RacingCar();
-            racingCar.setCarMover(new RandomNumberMover());
-            racingCar.race(ATTEMPT_NUMBER);
+        RacingCar racingCar1 = new RacingCar("DD");
+        racingCar1.race(ATTEMPT_NUMBER, RANDOM_NUMBER_CAR_MOVER);
+        racingCarList.add(racingCar1);
 
-            racingCars.add(racingCar);
-        }
+        RacingCar racingCar2 = new RacingCar("LALA");
+        racingCar2.race(ATTEMPT_NUMBER, RANDOM_NUMBER_CAR_MOVER);
+        racingCarList.add(racingCar2);
+
+        RacingCar racingCar3 = new RacingCar("MOMO");
+        racingCar3.race(ATTEMPT_NUMBER, RANDOM_NUMBER_CAR_MOVER);
+        racingCarList.add(racingCar3);
+
+        racingCars = new RacingCars(racingCarList);
     }
 
     @DisplayName("aggregate 메소드 테스트")
@@ -37,6 +45,8 @@ class RacingResultTest {
 
         assertThat(racingResult).isNotNull();
         assertThat(racingResult.getAttempt()).isEqualTo(ATTEMPT_NUMBER);
+        assertThat(racingResult.getResultByAttempt(ATTEMPT_NUMBER - 1)).hasSize(3);
+        assertThat(racingResult.getWinner(ATTEMPT_NUMBER - 1)).isNotBlank();
     }
 
     @DisplayName("getResultByAttempt 메소드 테스트")
@@ -44,13 +54,32 @@ class RacingResultTest {
     void getResultByAttempt_test() {
         int testAttempt = 1;
 
-        List<Integer> isExpectedResultByAttempt = racingCars.stream()
-                .map(RacingVehicle::getRaceRecord)
-                .map(raceRecord -> raceRecord.getBy(testAttempt))
-                .collect(Collectors.toList());
+        List<String> isExpectedResultByAttempt = racingCars.getRaceRecords()
+                .getResultByAttempt(testAttempt);
 
         RacingResult racingResult = RacingResult.aggregate(ATTEMPT_NUMBER, racingCars);
         assertThat(racingResult.getResultByAttempt(testAttempt)).hasSameSizeAs(isExpectedResultByAttempt);
         assertThat(racingResult.getResultByAttempt(testAttempt).get(0)).isEqualTo(isExpectedResultByAttempt.get(0));
+    }
+
+    @DisplayName("getWinner 메소드 테스트")
+    @ParameterizedTest
+    @MethodSource("provideCarNames")
+    void getWinner_test(Stream<String> names, boolean result) {
+        RacingResult racingResult = RacingResult.aggregate(ATTEMPT_NUMBER, racingCars);
+
+        assertThat(racingResult.getWinner(ATTEMPT_NUMBER - 1)).isNotBlank();
+
+        assertThat(names.anyMatch(name -> racingResult
+                .getWinner(ATTEMPT_NUMBER - 1)
+                .contains(name))).isEqualTo(result);
+
+    }
+
+    private static Stream<Arguments> provideCarNames() {
+        return Stream.of(
+                Arguments.of(Stream.of("DD", "LALA", "MOMO"), true),
+                Arguments.of(Stream.of("TOTO"), false)
+        );
     }
 }
