875a6820d97026a0127f76cfc1a46f512e9b294a
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-09 17:20:24 +0900

    (test) CarTest, Mocking test

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,29 +1,43 @@
 package racingcar.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Constants;
+
+import static racingcar.Constants.*;
 import racingcar.strategy.DoOneForward;
+import racingcar.strategy.RaceCondition;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
 
-    @BeforeEach
-    Car car = new Car();
+    Car car = new Car(0);
 
     //핵심로직/UI로직 테스트와 무관한 테스트 꼭 확인
     @DisplayName("Car 초기 position 0에서 시작")
     @Test
     public void assertCarInitPositionIsZero() {
-        assertThat(car.getPosition()).isEqualTo(Constants.INIT_POSITION);
+        assertThat(car.getPosition()).isEqualTo(INIT_POSITION);
     }
 
-    @DisplayName("Car 가능한 Strategy 모두 전진 1 or 멈춤 0")
+    /**
+     * Mocking Test (random 추상화)
+     */
+
+    @DisplayName("Car 전진 1 가능")
     @Test
-    public void assertCarStrategyOnlyForwardOrStop() {
-        car.setDoRace(new DoOneForward());
-       assertThat(car.move()).is(0 || 1);
+    public void assertCarCanOneForward() {
+        RaceCondition alwaysOneForwardCond = () -> true;
+        DoOneForward doOneForward = new DoOneForward();
+        assertThat(car.move(alwaysOneForwardCond, doOneForward))
+                .isEqualTo(ONE_FORWARD);
     }
+
+    @DisplayName("Car 전진 0 가능")
+    @Test
+    public void assertCarCanZeroForward() {
+        RaceCondition alwaysZeroForwardCond = () -> false;
+        DoOneForward doOneForward = new DoOneForward();
+        assertThat(car.move(alwaysZeroForwardCond, doOneForward))
+                .isEqualTo(ZERO_FORWARD);  }
 }
