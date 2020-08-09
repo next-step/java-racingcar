0bc31147d4a2701305cb701952fa3e72e49f05cc
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-10 02:58:06 +0900

    (feat) Cars Stream API, 1st Collection ISSUE

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,24 +1,46 @@
 package racingcar.domain;
+import racingcar.strategy.DoRace;
+import racingcar.strategy.RaceCondition;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Cars {
+/**
+ * 일급 콜렉션, 콜렉션 wrapping하며 그 콜렉션 외에 다른 변수는 없는 상태
+ * 해당 콜렉션이 아닌 단순 배열/리스트는 ..List ...Arr 로 해서 헷갈리지 않게
+ */
 
-    private int carCounts;
-    private int racingCounts;
+public class RacingCars {
 
-    private List<Car> cars;
+    private List<Car> carList;
 
-    public Cars(List<Car> cars) {
-        this.cars = cars;
+    public RacingCars(List<Car> carList) {
+        this.carList = carList;
     }
 
-    public List<Integer> allDoRace() {
-        return cars.stream()
-                .mapToInt(c ->new Car(c.getPosition(), ))
-                .list
+    public List<Car> getCarList() {
+        return carList;
     }
+
+    public List<Integer> allDoRace(int carCounts, int racingCounts) {
+        this.carList = new ArrayList<>(carCounts);
+        List<Integer> resultList = new ArrayList<>( );
+
+        if (racingCounts == 0) {
+            return resultList;
+        }
+        resultList = carList.stream( )
+                .map(Car::getPosition)
+                .collect(Collectors.toList( ));
+        resultList.add(0);
+        racingCounts -= 1;
+        resultList.addAll(allDoRace(carCounts, racingCounts));
+        return resultList;
+    }
+}
+
+
 /*
     public List<Integer> raceCars(List<Car> cars, int racingCounts) {
         List<Car> carList = new ArrayList<Car>();
@@ -28,5 +50,4 @@ public class Cars {
             }
         }
     }
-*/
-}
+*
--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -2,32 +2,38 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Constants;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static racingcar.Constants.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class CarsTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @DisplayName("n : 자동차 대수 입력값 없거나 0일 경우")
-    @Test
-    public void whenZeroOrBlankCarCount() {
-        assertThatThrownBy(() ->
-                Cars.func())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(CAR_COUNT_ZERO_OR_BLANK);
+public class RacingCarsTest {
+
+    @DisplayName("이동 시행 횟수 입력값 없거나 0일 경우")
+    @ParameterizedTest
+    @CsvSource(value = {"1:0","3:0"}, delimiter = ':')
+    public void whenZeroOrBlankRacingCounts(int carCounts, int racingCounts) {
+        new Car(0);
+        List<Car> carList = new ArrayList<>();
+        RacingCars racingCars = new RacingCars(carList);
+        List<Integer> expected = Arrays.asList(0);
+        assertEquals(expected, racingCars.allDoRace(carCounts, racingCounts));
     }
 
-    @DisplayName("m :이동 시행 횟수 입력값 없거나 0일 경우")
+    @DisplayName("자동차 대수 입력값 없거나 0일 경우")
     @Test
-    public void whenZeroOrBlankRacingCount() {
-        assertThatThrownBy(() ->
-                Cars.func())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(RACING_COUNT_ZERO_OR_BLANK);
+    public void whenZeroOrBlankCarCounts() {
+
     }
 
+
+    @DisplayName("자동차 대수 : 2, 이동 시행 횟수 : 1 ")
+    @Test
+    public void whenTwoCarsRaceOneTime(){
 
     }
 }
