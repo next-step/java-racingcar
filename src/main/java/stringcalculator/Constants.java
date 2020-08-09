e3330727f06ae6769ac1154412a18f8c1e5efa92
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-10 03:41:35 +0900

    (refactor) UI view split, Constants

--- src/main/java/stringcalculator/Constants.java
+++ src/main/java/stringcalculator/Constants.java
@@ -1,11 +1,15 @@
 package stringcalculator;
 
 public final class Constants {
-
+    /* MAIN */
     public static final String PROTECTED_UTIL_CLASS = "StringCalculator 클래스는 인스턴스화할 수 없습니다";
     public static final String INPUT_VALUE_IS_EMPTY = "입력 값이 공백 또는 없는 오류입니다.";
     public static final String DIVIDE_BY_ZERO = "0으로 나눌 수 없는 오류입니다.";
     public static final String WRONG_OPERATOR_SYMBOL = "연산자(+,-,*,/)에 해당하지 않는 오류입니다.";
 
+    /* UI VIEW (Optional) */
+    public static final String INSTRUCTION = "Please use Blank between every number & operator";
+    public static final String INPUT_CONSOLE = "You want to Calculate :  ";
+    public static final String OUTPUT_CONSOLE = "The Answer is : ";
 }
 
