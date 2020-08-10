a92ba0447d6d4a4b603b285070537721ee68d8b3
skid901 <tlrudejr1993@naver.com>
2020-08-11 03:07:42 +0900

    step4

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class Controller {
 
@@ -10,22 +9,18 @@ public class Controller {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
 
-        int cars = inputView.nextInt("자동차 대수는 몇 대 인가요?");
-        int rounds = inputView.nextInt("시도할 회수는 몇 회 인가요?");
+        String namesRequest = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+        String roundsRequest = "시도할 회수는 몇 회 인가요?";
+
+        String[] names = inputView.nextStrings(namesRequest);
+        int rounds = inputView.nextInt(roundsRequest);
         inputView.close();
 
-        RacingCar[] entries = new RacingCar[cars];
-        Arrays.setAll(entries, entry -> new RacingCar());
+        RacingCar[] entries = Arrays.stream(names)
+                .map(RacingCar::new)
+                .toArray(RacingCar[]::new);
 
-        resultView.changeLines();
-        resultView.printlnMessage("실행결과");
-
-        IntStream.range(0, rounds).forEach(round -> {
-            Arrays.stream(entries)
-                    .peek(RacingCar::race)
-                    .map(RacingCar::record)
-                    .forEach(resultView::printlnLaps);
-            resultView.changeLines();
-        });
+        resultView.printlnRace(entries, rounds);
+        resultView.printlnWinners(entries);
     }
 }
