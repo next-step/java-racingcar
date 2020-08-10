eb7485e6d85e8251e1c11a146517df06d9b4dce6
jaeseon_hwang <jshwang@deleo.co.kr>
2020-08-10 18:55:53 +0900

    Refactor 미사용 method 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,12 +3,8 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner = new Scanner(System.in);
 
-    public static int getCountOfCars() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return Integer.valueOf(scanner.nextLine());
-    }
+    private static Scanner scanner = new Scanner(System.in);
 
     public static String getCarNames() {
         DisplayText.INPUT_CAR_NAMES.printMessage();
