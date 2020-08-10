43796239b5fa587ad3a0e0cc30918e15822a7818
yeGenieee <cyj95428@gmail.com>
2020-08-11 01:12:26 +0900

    test: 입력 값이 null 또는 공백인 경우에 대해 테스트 추가

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -1,7 +1,39 @@
 package stringcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("입력 값이 null 인 경우")
+    public void nullInputTest() {
+        assertThatThrownBy(() -> {
+            calculator.calculate(null);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력 값이 공백 문자열이거나 null 입니다.");
+    }
+
+    @Test
+    @DisplayName("입력 값이 공백 경우")
+    public void blankInputTest() {
+        assertThatThrownBy(() -> {
+            calculator.calculate(" ");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력 값이 공백 문자열이거나 null 입니다.");
+    }
+
+
 
 }
