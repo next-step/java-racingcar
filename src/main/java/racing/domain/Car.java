ff96d27f1435ae25ae40d459ffcd29df63970a56
hyeoni90 <hyeona743@gmail.com>
2020-08-11 23:27:44 +0900

    feat: Add getRacingWinner

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -5,17 +5,28 @@ import racing.behavior.Movable;
 
 public class Car {
     private static final int INITIAL_LOCATION = 0;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final String INVALID_CAR_NAME_LENGTH = "자동차 이름의 길이를 확인해 주세요.";
 
+    private final String name;
     private int location;
     private final Movable movable;
 
-    public Car(final int location, final Movable movable) {
-        this.location = location;
+    public Car(final String name, final Movable movable) {
+        this.name = validateName(name);
+        this.location = INITIAL_LOCATION;
         this.movable = movable;
     }
 
-    public static Car of(final CarMovable movable) {
-        return new Car(INITIAL_LOCATION, movable);
+    public static Car of(final String name, final CarMovable movable) {
+        return new Car(name, movable);
+    }
+
+    public static String validateName(final String name) {
+        if(name.isEmpty() || name.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH);
+        }
+        return name;
     }
 
     public void move() {
@@ -26,5 +37,9 @@ public class Car {
 
     public int getLocation() {
         return location;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/main/java/racing/domain/RacingCars.java
+++ src/main/java/racing/domain/RacingCars.java
@@ -3,9 +3,12 @@ package racing.domain;
 import racing.behavior.CarMovable;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCars {
+    private static final int INITIAL_LOCATION = 0;
 
     private final List<Car> cars;
 
@@ -13,16 +16,30 @@ public class RacingCars {
         this.cars = cars;
     }
 
-    public static RacingCars of(final int numberOfCar) {
+    public static RacingCars of(final List<String> carNames) {
         final List<Car> cars = new ArrayList<>();
-        for(int i = 0; i < numberOfCar; i++) {
-            cars.add(Car.of(new CarMovable()));
+        for (final String name : carNames) {
+            cars.add(Car.of(name, new CarMovable()));
         }
         return new RacingCars(cars);
     }
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public int getMaxLocation() {
+        return this.cars.stream()
+                .max(Comparator.comparingInt(Car::getLocation))
+                .map(Car::getLocation)
+                .orElse(INITIAL_LOCATION);
+    }
+
+    public List<String> getWinners() {
+        return this.cars.stream()
+                .filter(car -> car.getLocation() == getMaxLocation())
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 
     public List<Car> raceOfCars(final List<Car> cars) {
