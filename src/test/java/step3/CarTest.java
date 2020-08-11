01c5f3664b592bdb3905db10068a5aa3ee9f8878
Dami <fdfdsa123@hanmail.net>
2020-08-12 02:14:24 +0900

    test($step4): 우승자 구하는 기능 테스트 작성

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -5,20 +5,24 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class CarTest {
 
 	@DisplayName(value = "조건에 따른 자동차의 전진 또는 정지 테스트")
 	@ParameterizedTest
 	@CsvSource(value = {"3:0", "1:0", "4:1", "6:1", "8:1"}, delimiter = ':')
-	void moveForward(int value) {
+	void moveForward(int value, int expected) {
 		Car expect = new Car();
 		expect.moveForward(value);
 
 		Car actual = new Car();
 		actual.moveForward(value);
 
-		assertThat(actual).isEqualTo(expect);
+		assertAll(
+				() -> assertThat(actual).isEqualTo(expect)
+				, () -> assertThat(actual.getRecord()).isEqualTo(expected)
+		);
 
 	}
 
