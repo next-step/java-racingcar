b565e9a7f66f7b8a23a0112eaa2bc8e1e2ff65d4
bonyeon <bonyeon0605@gmail.com>
2020-08-12 01:57:52 +0900

    4단계에 맞게 테스트 코드 재구성

--- src/test/java/CarRacing/CarsTest.java
+++ src/test/java/CarRacing/CarsTest.java
@@ -1,19 +1,37 @@
 package CarRacing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.Car;
 import racingcar.Cars;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
-    @DisplayName("INPUT값만큼 차가 생성되었는지")
-    @ParameterizedTest
-    @ValueSource(ints = {2, 4, 6})
-    void makeCar(int count) {
-        int expectValue = count;
-        Cars cars = new Cars(count);
-        assertThat(cars.carList().size()).isEqualTo(expectValue);
+    private List<String> carName;
+
+    @BeforeEach
+    void init() {
+        carName = new ArrayList<>();
+    }
+
+    @DisplayName("입력한 자동차 이름 숫자만큼 객체 생성")
+    @Test
+    void createCars() {
+        //given
+        carName.add("tony");
+        carName.add("tom");
+        carName.add("brown");
+        //when
+        Cars cars = new Cars(carName);
+        //then
+        assertThat(cars).isNotNull();
+        assertThat(cars.carList().size()).isEqualTo(3);
     }
 }
