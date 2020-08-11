40594b1d0bd3ee4ef26713d14adcb31dc8a90661
Dami <fdfdsa123@hanmail.net>
2020-08-12 01:31:07 +0900

    refactor($step3): 불필요 매개변수 제거

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -1,20 +1,17 @@
 package step3;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class CarTest {
 
 	@DisplayName(value = "조건에 따른 자동차의 전진 또는 정지 테스트")
 	@ParameterizedTest
 	@CsvSource(value = {"3:0", "1:0", "4:1", "6:1", "8:1"}, delimiter = ':')
-	void moveForward(int value, int expected) {
+	void moveForward(int value) {
 		Car expect = new Car();
 		expect.moveForward(value);
 
