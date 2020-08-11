15e3724fbdc969724732dd517812755da31525ef
Jxxn0ch <jychoi@deleo.co.kr>
2020-08-11 22:15:46 +0900

    step 4 - 스캐너 입력 시 출력되는 메시지를 inputView 클래스 내에서 처리

--- src/main/java/racingcar/RacingCarMain.java
+++ src/main/java/racingcar/RacingCarMain.java
@@ -2,13 +2,13 @@ package racingcar;
 
 
 import static racingcar.racing.Racing.racingStart;
-import static racingcar.view.InputView.getSystemInInt;
-import static racingcar.view.InputView.getSystemInString;
+import static racingcar.view.InputView.getInputCarsName;
+import static racingcar.view.InputView.getInputTryCount;
 
 public class RacingCarMain {
     public static void main(String[] args) {
-        String carsName = getSystemInString("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        int tryCount = getSystemInInt("시도할 회수는 몇 회 인가요?");
+        String carsName = getInputCarsName();
+        int tryCount = getInputTryCount();
 
         racingStart(carsName, tryCount);
     }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,18 +2,16 @@ package racingcar.view;
 
 import java.util.Scanner;
 
-import static java.lang.System.out;
-
 public class InputView {
     static Scanner scanner = new Scanner(System.in);
 
-    public static int getSystemInInt(String message) {
-        out.println(message);
-        return scanner.nextInt();
+    public static String getInputCarsName() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine();
     }
 
-    public static String getSystemInString(String message) {
-        out.println(message);
-        return scanner.nextLine();
+    public static int getInputTryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
     }
 }
