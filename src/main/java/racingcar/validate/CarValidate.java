4c6414552844c1971912344cdc5ad37472876a1d
Jxxn0ch <jychoi@deleo.co.kr>
2020-08-11 22:17:02 +0900

    step 4 - validateCarName 접근 제어자를 private으로 조정

--- src/main/java/racingcar/validate/CarValidate.java
+++ src/main/java/racingcar/validate/CarValidate.java
@@ -9,7 +9,7 @@ public class CarValidate {
         return carsArray;
     }
 
-    public static void validateCarName(String carName) {
+    private static void validateCarName(String carName) {
         if(carName.length() > 5 || carName.length() < 1) {
             throw new IllegalArgumentException("자동차 이름은 최소 1자 이상 5자 이하여야 합니다");
         }
