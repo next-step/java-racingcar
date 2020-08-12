fec840eb6a0eebcf417f7665b53980dc005e2206
김효린 <hyorin97@naver.com>
2020-08-12 18:15:20 +0900

    feat: 이동 또는 멈춤 기능 수정

--- src/main/java/racingcarbasic/RacingCar.java
+++ src/main/java/racingcarbasic/RacingCar.java
@@ -4,15 +4,23 @@ import java.util.Random;
 
 public class RacingCar {
     public int[] getMoveCount (int[] carMoveNum) {
-        Random random = new Random();
-        for (int i=0; i< carMoveNum.length; i++) {
-            int randomNum = random.nextInt(10);
 
-            if(randomNum>=4) {
+        for (int i=0; i< carMoveNum.length; i++) {
+            if(move()) {
                 carMoveNum[i]++;
             }
         }
 
         return carMoveNum;
+    }
+
+    public boolean move() {
+        Random random = new Random();
+        int randomNum = random.nextInt(10);
+
+        if(randomNum>=4) {
+            return true;
+        }
+        return false;
     }
 }
---
+++ src/test/java/racingcarbasic/RacingCarTest.java
@@ -0,0 +1,10 @@
+package racingcarbasic;
+
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+    @Test
+    void 전진() {
+
+    }
+}
