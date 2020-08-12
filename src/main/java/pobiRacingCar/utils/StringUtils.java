fcc48a252206a0d9c78db2638e8a0803a9778d59
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-13 06:51:11 +0900

    [refac] (d) repeat variables rename

--- src/main/java/pobiRacingCar/utils/StringUtils.java
+++ src/main/java/pobiRacingCar/utils/StringUtils.java
@@ -6,10 +6,10 @@ public class StringUtils {
         return value == null || value.trim().isEmpty();
     }
     //StringBuffer : 비동기 추천, StringBuilder : 저용량 메모리
-    public static String repeat(String s, int i) {
+    public static String repeat(String value, int repeatCount) {
         StringBuilder sb = new StringBuilder();
-        for (int j = 0 ; j < i ; j++) {
-            sb.append(s);
+        for (int j = 0 ; j < repeatCount ; j++) {
+            sb.append(value);
         }
         return sb.toString();
     }
