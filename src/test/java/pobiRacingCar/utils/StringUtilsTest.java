a5622c45564de14d80998e4fd3b1749361ac1db2
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-13 06:55:09 +0900

    (refac) repeat test

--- src/test/java/pobiRacingCar/utils/StringUtilsTest.java
+++ src/test/java/pobiRacingCar/utils/StringUtilsTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringUtilsTest {
     @Test
     void repeat() {
-        String result = StringUtils.repeat("-", 5);
-        assertThat(result).isEqualTo("-----");
+        assertThat(StringUtils.repeat("-", 5)).isEqualTo("-----");
+        assertThat(StringUtils.repeat("-", 3)).isEqualTo("---");
     }
 }
