e9e4b5348157989d2853e90f02dd1f76e92fe853
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-13 06:48:14 +0900

    (feat) (d) repeat dash - StringBuilder

--- src/main/java/pobiRacingCar/utils/StringUtils.java
+++ src/main/java/pobiRacingCar/utils/StringUtils.java
@@ -5,7 +5,7 @@ public class StringUtils {
     public static boolean isNullOrBlank(String value) {
         return value == null || value.trim().isEmpty();
     }
-
+    //StringBuffer : 비동기 추천, StringBuilder : 저용량 메모리
     public static String repeat(String s, int i) {
         StringBuilder sb = new StringBuilder();
         for (int j = 0 ; j < i ; j++) {
