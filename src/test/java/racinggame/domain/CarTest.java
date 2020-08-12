b7b46f0eef67e237e0cbc8599d432c70efb27a51
포비 <pobi@woowahan.com>
2020-08-12 13:25:43 +0900

    feat: add move, stop to CarTest

---
+++ src/test/java/racinggame/domain/CarTest.java
@@ -0,0 +1,21 @@
+package racinggame.domain;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+    @Test
+    public void 이동() {
+        Car car = new Car("pobi");
+        car.move();
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    public void 정지() {
+        Car car = new Car("pobi");
+        car.move();
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
