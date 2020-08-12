9cac6d7abf201592ef305014ebec144a503b0f50
김효린 <hyorin97@naver.com>
2020-08-12 18:28:50 +0900

    test: 단위 테스트

--- src/test/java/racingcarbasic/RacingCarTest.java
+++ src/test/java/racingcarbasic/RacingCarTest.java
@@ -1,10 +1,23 @@
 package racingcarbasic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarTest {
-    @Test
-    void 전진() {
+    RacingCar racingCar = new RacingCar();
 
+    @ParameterizedTest
+    @CsvSource(value = {"4, 5, 6, 7, 8, 9"}, delimiter = ',')
+    void 전진(int num) {
+        assertThat(racingCar.move(num)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0, 1, 2, 3"}, delimiter = ',')
+    void 멈춤(int num) {
+        assertThat(racingCar.move(num)).isFalse();
     }
 }
