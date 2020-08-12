5febb6530f75277e518432067ab0cfe4b31e9603
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-13 06:17:29 +0900

    [feat] NullorBlank확인 util

---
+++ src/main/java/pobiRacingCar/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package pobiRacingCar.utils;
+
+public class StringUtils {
+    
+    public static boolean isNullOrBlank(String value) {
+        return value == null || value.trim().isEmpty();
+    }
+}
--- src/main/java/pobiRacingCar/utils/ValidationUtils.java
+++ src/main/java/pobiRacingCar/utils/ValidationUtils.java
@@ -2,6 +2,9 @@ package pobiRacingCar.utils;
 
 public class ValidationUtils {
     public static boolean validName(String name) {
+        if (StringUtils.isNullOrBlank(name)) {
+           return false;
+        }
         return true;
     }
 }
