56b0edd04d8a814192608c3b7e12f39717065a88
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-13 06:45:24 +0900

    [test] (d) repeat dash

--- src/main/java/pobiRacingCar/utils/StringUtils.java
+++ src/main/java/pobiRacingCar/utils/StringUtils.java
@@ -5,4 +5,12 @@ public class StringUtils {
     public static boolean isNullOrBlank(String value) {
         return value == null || value.trim().isEmpty();
     }
+
+    public static String repeat(String s, int i) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0 ; j < i ; j++) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
 }
--- src/test/java/pobiRacingCar/utils/StringUtilsTest.java
+++ src/test/java/pobiRacingCar/utils/StringUtilsTest.java
@@ -1,2 +1,13 @@
-package pobiRacingCar.utils;public class StringUtilsTest {
+package pobiRacingCar.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringUtilsTest {
+    @Test
+    void repeat() {
+        String result = StringUtils.repeat("-", 5);
+        assertThat(result).isEqualTo("-----");
+    }
 }
