10e27ae6354e4ac56d5ab25f2881a8ccb1cb841e
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-13 07:26:40 +0900

    [fix] Car equal&hashCode

--- src/main/java/pobiRacingCar/domain/Car.java
+++ src/main/java/pobiRacingCar/domain/Car.java
@@ -1,2 +1,30 @@
-package pobiRacingCar.domain;public class Car {
+package pobiRacingCar.domain;
+
+import java.util.Objects;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(final String name) { this.name = name.trim();
+    }
+
+    public int getPosition() { return position; }
+
+    public String getName() { return name; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass( ) != o.getClass( )) return false;
+        Car car = (Car) o;
+        return position == car.position &&
+                name.equals(car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
+    }
 }
+
