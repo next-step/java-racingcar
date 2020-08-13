5ebe1a7002eb4469a4a05ef6834a937e31fed51d
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-14 06:16:35 +0900

    [feat] CarName equals hashCode const

--- src/test/java/pobiRacingCar/domain/CarNameTest.java
+++ src/test/java/pobiRacingCar/domain/CarNameTest.java
@@ -7,8 +7,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarNameTest {
     @Test
     void doCreateCarName() {
-        CarName carName = new CarName("pobi");
+        CarName name = new CarName("pobi");
         // assertThat(carName.getName()).isEqualTo("pobi"); getter사용 ㄴㄴ 객체로 비교
-        assertThat(carName).isEqualTo(new CarName("pobi"));
+        assertThat(name).isEqualTo(new CarName("pobi"));
     }
 }
