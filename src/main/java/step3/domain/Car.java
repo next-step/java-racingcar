89ef87fa9f16b663e29b869743715997a8ca91ad
SEONBEENLEE <seonbeen@skoopmedia.co.kr>
2020-08-13 20:51:18 +0900

    refactor : Car 객체 수정

--- src/main/java/step3/domain/Car.java
+++ src/main/java/step3/domain/Car.java
@@ -1,36 +1,19 @@
 package step3.domain;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class Car {
 
-    private static final String BLANK_EXCEPTION = "잘못된 인자값 입니다.";
-
-    private String move;
     private int status;
 
     public Car() {
         this.status = 0;
     }
 
-    public Car(String move) {
-        moveArgException(move);
-
-    }
-
-    public int moveCar() {
+    public int getStatus() {
         return status;
     }
 
-    private void moveArgException(String number) throws IllegalArgumentException {
-
-        try {
-            if (!StringUtils.isNumeric(number) || StringUtils.isBlank(number)) {
-                throw new IllegalArgumentException(BLANK_EXCEPTION);
-            }
-        } catch (IllegalArgumentException e) {
-            throw e;
-        }
-
+    public int moveCar(boolean move) {
+        return move ? this.status++ : this.status;
     }
+
 }
