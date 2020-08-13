567655ae03727ec5192c52577e4f0f8dab2449e5
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-13 23:50:30 +0900

    [test] oneForward or zero, move() by int

--- src/main/java/pobiRacingCar/domain/Car.java
+++ src/main/java/pobiRacingCar/domain/Car.java
@@ -1,8 +1,11 @@
 package pobiRacingCar.domain;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class Car {
+    public static final int MAX_BOUND = 10;
+    public static final int FORWARD_NUM = 4;
     private final String name;
     private int position = 0;
 
@@ -12,6 +15,23 @@ public class Car {
     public int getPosition() { return position; }
 
     public String getName() { return name; }
+
+    //move() getRandom 즉 Random 객체에 의존 : 아예 외부에서 고정값(int 등)전달받게 하라
+    public void move(int randomNo) {
+        if (randomNo >= FORWARD_NUM)
+            this.position++;
+    }
+    /*
+    public void move() {
+        if (getRandomNo() >= FORWARD_NUM)
+            this.position++;
+    }
+    */
+
+    private int getRandomNo() {
+        Random random = new Random();
+        return random.nextInt(MAX_BOUND);
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -27,4 +47,6 @@ public class Car {
         return Objects.hash(name, position);
     }
 }
+
+
 
---
+++ src/test/java/pobiRacingCar/domain/CarTest.java
@@ -0,0 +1,22 @@
+package pobiRacingCar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+    @Test
+    void oneForwardTrue() {
+        Car car = new Car("pobi");
+        car.move(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void zeroForwardTrue() {
+        Car car = new Car("pobi");
+        car.move(3);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
+
