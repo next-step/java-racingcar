6b16e7e3c9a39eda189d3b7ebfb93c3a288fc04a
yeGenieee <cyj95428@gmail.com>
2020-08-13 21:17:10 +0900

    feat: 뺄셈, 곱셈, 나눗셈에 대한 단위테스트 및 연산 메소드 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,7 +5,21 @@ public class StringCalculator {
         String[] values = value.split(" ");
         int first = toInt(values[0]);
         int second = toInt(values[2]);
+        String operator = values[1];
 
+        return calculate(first, second, operator);
+    }
+
+    private static int calculate(int first, int second, String operator) {
+        if (operator.equals("-")) {
+            return first - second;
+        }
+        if (operator.equals("*")) {
+            return first * second;
+        }
+        if (operator.equals("/")) {
+            return first / second;
+        }
         return first + second;
     }
 
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static calculator.StringCalculator.calculate;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
 
@@ -12,5 +13,23 @@ public class StringCalculatorTest {
     @DisplayName("덧셈에 대한 단위테스트")
     void 덧셈() {
         assertThat(calculate("3 + 1")).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("뺄셈에 대한 단위테스트")
+    void 뺄셈() {
+        assertThat(calculate("3 - 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("곱셈에 대한 단위테스트")
+    void 곱셈() {
+        assertThat(calculate("3 * 1")).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("나눗셈에 대한 단위테스트")
+    void 나눗셈() {
+        assertThat(calculate("6 / 2")).isEqualTo(3);
     }
 }
