95e0127a18b865de4d2ba8785832e8e164c7d938
pjhsk113 <pjhsk113@naver.com>
2020-08-14 23:25:12 +0900

    [feat] 상수, exceptionMessage, ExceptionHandler 추가

---
+++ src/main/java/racingcar/constant/ExceptionMessage.java
@@ -0,0 +1,9 @@
+package racingcar.constant;
+
+public final class ExceptionMessage {
+    private ExceptionMessage() { }
+
+    public static final String NAME_CAN_NOT_EXCEED_FIVE_CHARACTERS = "이름은 5자를 초과할 수 없습니다.";
+    public static final String INPUT_CAN_BE_NOT_EMPTY = "공백은 입력할 수 없습니다.";
+    public static final String INPUT_CAN_BE_NOT_NULL = "Null 값은 입력할 수 없습니다.";
+}
