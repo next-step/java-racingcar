715b52a3e97a136089ef8ad11d17eeadf503b64b
rftengineer <nightpw1@naver.com>
2020-08-15 18:40:09 +0900

    test(step5): ParticipantsTest 내용 추가

--- src/test/java/step5/model/ParticipantsTest.java
+++ src/test/java/step5/model/ParticipantsTest.java
@@ -1,7 +1,9 @@
 package step5.model;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,31 +11,37 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("일급 컬렉션 Participants 테스트")
 public class ParticipantsTest {
 
-    @Test
-    void check_first_collection_creation() {
+    @ParameterizedTest
+    @CsvSource(value = {"2"})
+    @DisplayName("초기화 후 제대로 Car 객체 생성됐는지 확인")
+    void check_first_collection_creation(int expected) {
         List<Car> cars = new ArrayList<>(Arrays.asList(new Car("John"), new Car("Kim")));
         Participants partc = new Participants(cars);
-        assertThat(partc.getNumberOfCars()).isEqualTo(2);
+        assertThat(partc.getNumberOfCars()).isEqualTo(expected);
     }
 
-    @Test
-    void check_first_collection_getMaximumScore() {
+    @ParameterizedTest
+    @CsvSource(value = {"2"})
+    @DisplayName("전략에 맞춰 첫 번째 차량만 전진 시켰을 때, 그 크기가 맞는지 확인")
+    void check_first_collection_getMaximumScore(int expected) {
         List<Car> cars = new ArrayList<>(Arrays.asList(new Car("John"), new Car("Kim")));
         cars.get(0).goForward();
         cars.get(0).goForward();
         Participants partc = new Participants(cars);
-        assertThat(partc.getCurrentMaximumScoreFromParticipants()).isEqualTo(2);
+        assertThat(partc.getCurrentMaximumScoreFromParticipants()).isEqualTo(expected);
     }
 
-    @Test
-    void check_first_collection_getWinnerName() {
+    @ParameterizedTest
+    @CsvSource(value = {"John"})
+    void check_first_collection_getWinnerName(String expected) {
         List<Car> cars = new ArrayList<>(Arrays.asList(new Car("John"), new Car("Kim")));
         cars.get(0).goForward();
         cars.get(0).goForward();
         Participants partc = new Participants(cars);
-        assertThat(partc.getWinnerMember().get(0)).isEqualTo("John");
+        assertThat(partc.getWinnerMember().get(0)).isEqualTo(expected);
     }
 
     @Test
@@ -43,4 +51,13 @@ public class ParticipantsTest {
         assertThat(partc.getMemberOfParticipants()).containsExactly("John", "Kim", "Jayce");
     }
 
+    @Test
+    void check_first_collection_moveCarWithStrategy() {
+        List<Car> cars = new ArrayList<>(Arrays.asList(new Car("John"), new Car("Kim")));
+        Participants partc = new Participants(cars);
+        partc.moveCarWithStrategy(() -> true);
+        partc.moveCarWithStrategy(() -> true);
+
+        assertThat(partc.getMemberOfParticipants()).contains("John", "Kim");
+    }
 }
