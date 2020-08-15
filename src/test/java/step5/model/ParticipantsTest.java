504cb1329c38cd4f9000c0557b57b18877e4bc95
rftengineer <nightpw1@naver.com>
2020-08-15 16:51:34 +0900

    feat(step5): Participants - Car의 일급 컬렉션 제작

---
+++ src/test/java/step5/model/ParticipantsTest.java
@@ -0,0 +1,38 @@
+package step5.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ParticipantsTest {
+
+    @Test
+    void check_first_collection_creation() {
+        List<Car> cars = new ArrayList<>(Arrays.asList(new Car("John"), new Car("Kim")));
+        Participants partc = new Participants(cars);
+        assertThat(partc.getNumberOfCars()).isEqualTo(2);
+    }
+
+    @Test
+    void check_first_collection_getMaximumScore() {
+        List<Car> cars = new ArrayList<>(Arrays.asList(new Car("John"), new Car("Kim")));
+        cars.get(0).goForward();
+        cars.get(0).goForward();
+        Participants partc = new Participants(cars);
+        assertThat(partc.getCurrentMaximumScoreFromParticipants()).isEqualTo(2);
+    }
+
+    @Test
+    void check_first_collection_getWinnerName() {
+        List<Car> cars = new ArrayList<>(Arrays.asList(new Car("John"), new Car("Kim")));
+        cars.get(0).goForward();
+        cars.get(0).goForward();
+        Participants partc = new Participants(cars);
+        assertThat(partc.getWinnerMember().get(0)).isEqualTo("John");
+    }
+}
