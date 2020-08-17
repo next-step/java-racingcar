91b6978c3e883ec76b896259fbe32e761a14cd99
Jay <beginin15@gmail.com>
2020-08-18 00:55:55 +0900

    test: 테스트 코드 리팩토링

--- src/test/java/racing/core/RacingGameTest.java
+++ src/test/java/racing/core/RacingGameTest.java
@@ -6,7 +6,7 @@ import racing.core.domain.RacingGame;
 import racing.core.dto.TrackInfo;
 import racing.core.dto.Trial;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,11 +20,10 @@ class RacingGameTest {
     @DisplayName("모든 차가 전혀 이동하지 경우 테스트")
     void runNever() {
         // given
-        List<Trial> expected = Arrays.asList(
-                new Trial(Arrays.asList(new TrackInfo(0), new TrackInfo(0), new TrackInfo(0))),
-                new Trial(Arrays.asList(new TrackInfo(0), new TrackInfo(0), new TrackInfo(0))),
-                new Trial(Arrays.asList(new TrackInfo(0), new TrackInfo(0), new TrackInfo(0)))
-        );
+        List<Trial> expected = new ArrayList<>(numberOfTrials);
+        for (int i = 0; i < numberOfTrials; i++) {
+            expected.add(new Trial(getTracksNeverMoved()));
+        }
 
         // when
         List<Trial> actual = new RacingGame(namesOfCars, expected.size()).run(() -> false);
@@ -37,16 +36,31 @@ class RacingGameTest {
     @DisplayName("모든 차가 항상 이동한 경우 테스트")
     void runAlways() {
         // given
-        List<Trial> expected = Arrays.asList(
-                new Trial(Arrays.asList(new TrackInfo(1), new TrackInfo(1), new TrackInfo(1))),
-                new Trial(Arrays.asList(new TrackInfo(2), new TrackInfo(2), new TrackInfo(2))),
-                new Trial(Arrays.asList(new TrackInfo(3), new TrackInfo(3), new TrackInfo(3)))
-        );
+        List<Trial> expected = new ArrayList<>(numberOfTrials);
+        for (int i = 0; i < numberOfTrials; i++) {
+            expected.add(new Trial(getTracksAlwaysMoved(i + 1)));
+        }
 
         // when
         List<Trial> actual = new RacingGame(namesOfCars, expected.size()).run(() -> true);
 
         // then
         assertEquals(expected, actual);
+    }
+
+    private List<TrackInfo> getTracksNeverMoved() {
+        List<TrackInfo> tracks = new ArrayList<>(namesOfCars.length);
+        for (int i = 0; i < namesOfCars.length; i++) {
+            tracks.add(new TrackInfo(namesOfCars[i], 0));
+        }
+        return tracks;
+    }
+
+    private List<TrackInfo> getTracksAlwaysMoved(int position) {
+        List<TrackInfo> tracks = new ArrayList<>(namesOfCars.length);
+        for (int i = 0; i < namesOfCars.length; i++) {
+            tracks.add(new TrackInfo(namesOfCars[i], position));
+        }
+        return tracks;
     }
 }
