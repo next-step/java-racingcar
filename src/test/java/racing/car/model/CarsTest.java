0a73f89f5d6a7db97f82ba47db21c5072c870b65
misudev <jungmisu801@gmail.com>
2020-08-17 19:12:18 +0900

    test: cars 테스트 수정

--- src/test/java/racing/car/model/CarsTest.java
+++ src/test/java/racing/car/model/CarsTest.java
@@ -2,26 +2,67 @@ package racing.car.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racing.car.MovableRule;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
 
-    private Cars cars;
+    private static Stream<Arguments> getCarsArguments() {
+        return Stream.of(
+                        Arrays.asList(new Car("lee", 0),
+                                new Car("kim", 1),
+                                new Car("hong", 1)),
 
-    @BeforeEach
-    void setUp() {
-        cars = new Cars(Arrays.asList("park", "lee", "toto", "bibi"), 0);
+                        Arrays.asList(new Car("tom", 3),
+                                new Car("tony", 0),
+                                new Car("toto", 1)),
+
+                        Arrays.asList(new Car("min", 3),
+                                new Car("mimi", 5),
+                                new Car("kang", 6),
+                                new Car("kim", 1),
+                                new Car("park", 6))
+        ).map(Arguments::of);
+    }
+
+    private static Stream<Arguments> getWinnerArguments() {
+        return Stream.of(
+                Arguments.of(
+                        Arrays.asList(new Car("lee", 0),
+                                new Car("kim", 1),
+                                new Car("hong", 1)),
+                        Arrays.asList(new Car("kim", 1),
+                                new Car("hong", 1))),
+                Arguments.of(
+                        Arrays.asList(new Car("tom", 3),
+                                new Car("tony", 0),
+                                new Car("toto", 1)),
+                        Arrays.asList(new Car("tom", 3))),
+                Arguments.of(
+                        Arrays.asList(new Car("min", 3),
+                                new Car("mimi", 5),
+                                new Car("kang", 6),
+                                new Car("kim", 1),
+                                new Car("park", 6)),
+                        Arrays.asList(new Car("kang", 6),
+                                new Car("park", 6)))
+        );
     }
 
     @Test
-    @DisplayName("레이싱 한 경기 후 결과 비교하")
+    @DisplayName("레이싱 한 경기 후 결과 비교하기")
     void racingOneGame() {
         // given
+        Cars cars = new Cars(Arrays.asList("park", "lee", "toto", "bibi"), 0);
         MovableRule movableRule = () -> true;
 
         // when
@@ -31,17 +72,13 @@ class CarsTest {
         assertThat(cars).isNotEqualTo(new Cars(Arrays.asList("park", "lee", "toto", "bibi"), 0));
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("getCarsArguments")
     @DisplayName("trackrecords 생성하기")
-    void getTrackRecords() {
+    void getTrackRecords(List<Car> carList) {
         // given
-        racingOneGame();
-        Map<String, Integer> expectedTrackRecords = new LinkedHashMap<>();
-        expectedTrackRecords.put("park", 1);
-        expectedTrackRecords.put("lee", 1);
-        expectedTrackRecords.put("toto", 1);
-        expectedTrackRecords.put("bibi", 1);
-
+        Cars cars = new Cars(carList);
+        Map<String, Integer> expectedTrackRecords = getExpectedTrackRecords(carList);
 
         // when
         Map<String, Integer> trackRecords = cars.getTrackRecords();
@@ -51,20 +88,27 @@ class CarsTest {
 
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("getWinnerArguments")
     @DisplayName("우승자 찾기")
-    void findWinner() {
+    void findWinner(List<Car> carList, List<Car> expectedWinners) {
         // given
-        List<Car> expectedWinners = new ArrayList<>();
-        expectedWinners.add(new Car("park", 0));
-        expectedWinners.add(new Car("lee", 0));
-        expectedWinners.add(new Car("toto", 0));
-        expectedWinners.add(new Car("bibi", 0));
+        Cars cars = new Cars(carList);
 
         // when
-        List<Car> winners =  cars.findWinner();
+        List<Car> winners = cars.findWinner();
 
         // then
         assertThat(winners).isEqualTo(expectedWinners);
     }
+
+    private Map<String, Integer> getExpectedTrackRecords(List<Car> carList) {
+        Map<String, Integer> expectedTrackRecords = new LinkedHashMap<>();
+        for (Car car : carList) {
+            expectedTrackRecords.put(car.getName(), car.getLocation());
+        }
+
+        return expectedTrackRecords;
+    }
+
 }
