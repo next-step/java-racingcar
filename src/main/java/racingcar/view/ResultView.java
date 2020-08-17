79184609d2e3d48451b3a43639b4172c76f03bb2
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-17 21:06:21 +0900

    [refac] show Position apart from rolls

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,15 +1,25 @@
 package racingcar.view;
 
 import racingcar.domain.Car;
+import racingcar.strategy.DoOneForward;
+import racingcar.strategy.OneForwardCondition;
+import racingcar.strategy.RaceCondition;
+import racingcar.utils.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static racingcar.Constants.*;
+import static racingcar.utils.StringUtils.repeat;
 
-public class ResultView {
+public class ResultView{
     public static void printCars(List<Car> cars) {
         System.out.println(SAY_VIEW_RESULT);
+        cars.forEach(ResultView::dashPosition);
     }
 
+    public static void dashPosition(Car car) {
+        repeat("-", car.getPosition());
+    }
 
 }
