971d148fa8da0bb325e7e97b815783b506c1518a
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-17 20:10:29 +0900

    [feat] UI View(Input,Result)create

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 import static racingcar.Constants.*;
 
 public class InputView {
+    static Scanner scanner = new Scanner(System.in);
+    private static int carCounts;
+    private static int racingCounts;
 
-    public void main(String[] args) {
-        int carCounts = 0;
-        int racingCounts = 0;
-
-        Scanner scanner = new Scanner(System.in);
-
+    public static int getCarCounts() {
         System.out.println(ASK_CAR_COUNT);
-        carCounts = scanner.nextInt();
-        System.out.println(ASK_RACING_COUNT);
-        racingCounts = scanner.nextInt();
+        return carCounts = scanner.nextInt( );
     }
 
+    public static int getRacingCounts() {
+        System.out.println(ASK_RACING_COUNT);
+        return racingCounts = scanner.nextInt( );
+    }
 }
+
--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -2,12 +2,16 @@ package racingcar.view;
 
 import racingcar.domain.Car;
 
+import java.util.List;
+
 import static racingcar.Constants.*;
 
 public class ResultView {
-    public void result(){
-        System.out.println(SAY_VIEW_RESULT);
+    public static void printCars(List<Car> cars) {
+    }
 
+    public static void printResults() {
+        System.out.println(SAY_VIEW_RESULT);
     }
 
 }
