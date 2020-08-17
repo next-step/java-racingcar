1afe81b954fca24aa11efb1698bb550f6ed8558d
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-17 17:19:06 +0900

    [feat] repeat dashes by racingCounts

--- src/main/java/racingcar/utils/StringUtils.java
+++ src/main/java/racingcar/utils/StringUtils.java
@@ -1,2 +1,12 @@
-package racingcar;public class StringUtils {
+package racingcar.utils;
+
+
+public class StringUtils {
+    public static String repeat(String step, int counts) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < counts; j++) {
+            sb.append(step);
+        }
+        return sb.toString();
+    }
 }
