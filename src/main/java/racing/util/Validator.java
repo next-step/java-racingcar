8d5cf79c7c1976d6a93d5a012b6faca5d253554b
EddyJo <jkjk0600@gamil.com>
2020-08-18 17:05:28 +0900

    feat Validator 수정

--- src/main/java/racing/util/Validator.java
+++ src/main/java/racing/util/Validator.java
@@ -1,11 +1,23 @@
 package racing.util;
 
+import java.util.Arrays;
+
 public class Validator {
     private static Integer MAX_LENGTH = 5;
-    public static boolean validate(String str) {
+
+    public static String[] validate(String[] strArr) {
+        for(String str:strArr) {
+            validate(str);
+        }
+        return strArr;
+    }
+
+    public static void validate(String str) {
         if(null == str || str.length() == 0) {
             throw new IllegalArgumentException("이름에는 공백 또는 null이 들어갈 수 없습니다.");
         }
-        return str.length()<=MAX_LENGTH?true:false;
+        if(str.length()>MAX_LENGTH) {
+            throw new IllegalArgumentException("이름은 최대 5자리까지 가능합니다.");
+        }
     }
 }
--- src/test/java/racing/util/ValidatorTest.java
+++ src/test/java/racing/util/ValidatorTest.java
@@ -1,20 +1,34 @@
 package racing.util;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class ValidatorTest {
 
+    @DisplayName("이름이 다섯글자이하일 시 예외 미발생 테스트")
     @ParameterizedTest
-    @CsvSource(value = {"메롱:true", "다섯글자넘어:false"}, delimiter = ':')
-    public void 이름이다섯자이하인지체크(String name, boolean expected) {
-        assertThat(Validator.validate(name)).isEqualTo(expected);
+    @ValueSource(strings = {"여섯자리글"})
+    public void 이름이다섯자이하인지체크_다섯글자이하_예외미발생하면_통과(String name) {
+        assertThatCode(
+                () -> Validator.validate(name)
+        ).doesNotThrowAnyException();
     }
 
+    @DisplayName("이름이 다섯글자 초과시 예외 발생 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"여섯자리글자"})
+    public void 이름이다섯자이하인지체크_여섯글자(String name) {
+        assertThatThrownBy(
+                () -> Validator.validate(name)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Null or 빈값이 예외발생 테스트")
     @ParameterizedTest
     @NullAndEmptySource
     public void 이름_null또는Empty값_체크(String input) {
