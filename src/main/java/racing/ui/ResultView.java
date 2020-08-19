1a1ff4aef9fc7911fe6dd832f5f7d72489dfcfd3
Jay <beginin15@gmail.com>
2020-08-19 19:15:39 +0900

    feat: DTO의 값을 이용하여 출력하는 로직 추가 (피드백 반영)
    - 기존 DTO(Trial, TrackInfo)에서 수행하던 메소드를 뷰 레이어에서 처리하도록 변경

--- src/main/java/racing/ui/ResultView.java
+++ src/main/java/racing/ui/ResultView.java
@@ -1,9 +1,11 @@
 package racing.ui;
 
+import racing.core.dto.TrackInfo;
 import racing.core.dto.Trial;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ResultView {
 
@@ -21,9 +23,23 @@ public class ResultView {
     }
 
     public void printResult() {
-        for (Trial each : trials) {
-            System.out.println(each);
-        }
+        trials.stream()
+                .map(this::parseString)
+                .forEach(System.out::println);
         System.out.println(winners + "가 최종 우승했습니다.");
+    }
+
+    private String parseString(Trial trial) {
+        return trial.getTracks()
+                .stream()
+                .map(this::mapTrackInfoToString)
+                .reduce("", String::concat);
+    }
+
+    private String mapTrackInfoToString(TrackInfo trackInfo) {
+        String track = IntStream.range(0, trackInfo.getPosition())
+                .mapToObj(position -> "-")
+                .reduce("", String::concat);
+        return String.format("%s : %s\n", trackInfo.getCarName(), track);
     }
 }
