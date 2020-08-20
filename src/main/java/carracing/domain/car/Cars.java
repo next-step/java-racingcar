0bdc738026a0b7683da4054aa3bf49edabc61ee9
jsoraso <clsrn63@naver.com>
2020-08-21 00:29:12 +0900

    fix : Cars 생성자 defensive copy 적용

--- src/main/java/carracing/domain/car/Cars.java
+++ src/main/java/carracing/domain/car/Cars.java
@@ -1,12 +1,14 @@
 package carracing.domain.car;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Cars {
     private List<Car> cars;
 
     public Cars(List<Car> cars) {
-        this.cars = cars;
+        this.cars = Collections.unmodifiableList(cars); // defensive copy : 생성자의 인자로 받은 객체를 복사.
+
     }
 
     public List<Car> getCars() {
