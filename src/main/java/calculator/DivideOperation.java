2b7513d68c4e0abb057b3c6e1649b392e5a25cd9
yeGenieee <cyj95428@gmail.com>
2020-08-22 00:26:48 +0900

    refactor: 0으로 나누는 경우에 대해 예외 처리 추가

--- src/main/java/calculator/DivideOperation.java
+++ src/main/java/calculator/DivideOperation.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import stringcalculator.Constants;
+
 public class DivideOperation implements Operation {
     @Override
     public boolean isSupport(String operator) {
@@ -8,6 +10,9 @@ public class DivideOperation implements Operation {
 
     @Override
     public int operate(int x, int y) {
+        if (y == 0) {
+            throw new ArithmeticException(Constants.DIVIDE_BY_ZERO);
+        }
         return x / y;
     }
 }
