35d42ffcbf20b1d78e24fb02e1b9398bcdcdc1c2
김효린 <hyorin97@naver.com>
2020-08-22 01:56:06 +0900

    feat: Test 방식 수정

--- src/test/java/racingcarbasic/RacingCarTest.java
+++ src/test/java/racingcarbasic/RacingCarTest.java
@@ -1,5 +1,6 @@
 package racingcarbasic;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -7,17 +8,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarTest {
 
-    @ParameterizedTest
-    @CsvSource(value = {"0, 1, 2, 3, 4, 5, 6, 7, 8, 9"}, delimiter = ',')
+   @Test
     void 전진(int num) {
-        MoveStrategy functionalTest = () -> num >= 0;
-        assertThat(functionalTest.move()).isEqualTo(true);
+        RacingCars racingCars = RacingCars.of(5, () -> true);
+        racingCars.moveCars();
+
+        assertThat(racingCars.getCarList()).allMatch(car-> car.getStep() == 1);
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"0, 1, 2, 3, 4, 5, 6, 7, 8, 9"}, delimiter = ',')
-    void 멈춤(int num) {
-        MoveStrategy functionalTest = () -> num >= 10;
-        assertThat(functionalTest.move()).isEqualTo(false);
+    @Test
+    void 멈춤() {
+        RacingCars racingCars = RacingCars.of(5, () -> false);
+        racingCars.moveCars();
+
+        assertThat(racingCars.getCarList()).allMatch(car -> car.getStep() == 0);
     }
 }
