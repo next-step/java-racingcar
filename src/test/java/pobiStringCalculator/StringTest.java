2e34e95574b61180240a59fdd965c15295fb6167
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-21 23:16:16 +0900

    [test] StringsplitTest

--- src/test/java/pobiStringCalculator/StringTest.java
+++ src/test/java/pobiStringCalculator/StringTest.java
@@ -5,5 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
-
+    @Test
+    void split() {
+        String[] values = "3 + 4".split(" ");
+        assertThat(values).containsExactly("3","+","4");
+    }
 }
