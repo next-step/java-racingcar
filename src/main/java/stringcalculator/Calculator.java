ea8f01601fb0465841a0b0b7bc108dd506f5a496
yeGenieee <cyj95428@gmail.com>
2020-08-22 01:44:53 +0900

    refactor: Operator를 enum으로 분리

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -1,19 +1,10 @@
 package stringcalculator;
 
-import calculator.*;
+import stringcalculator.*;
 
 import java.util.*;
 
 public class Calculator {
-    private static List<Operation> operations = new ArrayList<>();
-    static {
-        operations.add(new PlusOperation());
-        operations.add(new MinusOperation());
-        operations.add(new MultipleOperation());
-        operations.add(new DivideOperation());
-    }
-
-
     public int calculate(String input) {
         if (isNullOrBlank(input)) {
             throw new IllegalArgumentException(Constants.INPUT_VALUE_IS_NULL_OR_EMPTY);
@@ -34,31 +25,8 @@ public class Calculator {
     }
 
     private int calculate(int firstOperand, int secondOperand, String operator) {
-        // 찾은 연산자
-        Optional<Operation> maybeOperation = operations.stream().filter(o -> o.isSupport(operator)).findFirst();
-
-        Operation operation = maybeOperation.orElseThrow(() -> new IllegalArgumentException(Constants.IS_NOT_VALID_OPERATOR));
-
-        return operation.operate(firstOperand, secondOperand);
-    }
-
-    private int add(int firstOperand, int secondOperand) {
-        return firstOperand + secondOperand;
-    }
-
-    private int subtract(int firstOperand, int secondOperand) {
-        return firstOperand - secondOperand;
-    }
-
-    private int multiple(int firstOperand, int secondOperand) {
-        return firstOperand * secondOperand;
-    }
-
-    private int divide(int firstOperand, int secondOperand) {
-        if (secondOperand == 0) {
-            throw new ArithmeticException(Constants.DIVIDE_BY_ZERO);
-        }
-        return firstOperand / secondOperand;
+        Operator op = Operator.getOperator(operator);
+        return op.operate(firstOperand, secondOperand);
     }
 
 }
