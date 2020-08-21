6032e4930e48db7f865781d435521b45203ee69b
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-21 23:23:42 +0900

    [feat] plus by split parseInt

--- src/main/java/pobiStringCalculator/StringCalculator.java
+++ src/main/java/pobiStringCalculator/StringCalculator.java
@@ -2,5 +2,10 @@ package pobiStringCalculator;
 
 public class StringCalculator {
     public static int calculate(String s) {
+        String[] values = s.split(" ");
+        int first = Integer.parseInt(values[0]);
+        int second = Integer.parseInt(values[2]);
+        return first + second;
+
     }
 }
