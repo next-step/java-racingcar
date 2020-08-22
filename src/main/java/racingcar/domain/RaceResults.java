e5b09ff3ef2d14149726b133e000cf27a37865e3
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-23 05:38:58 +0900

    [chore] raceResults equals hashcode

--- src/main/java/racingcar/domain/RaceResults.java
+++ src/main/java/racingcar/domain/RaceResults.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class RaceResults {
 
@@ -19,8 +20,16 @@ public class RaceResults {
         return raceResults;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass( ) != o.getClass( )) return false;
+        RaceResults that = (RaceResults) o;
+        return Objects.equals(raceResults, that.raceResults);
+    }
 
-
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(raceResults);
+    }
 }
