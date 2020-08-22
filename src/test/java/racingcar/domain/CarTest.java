3050a2e0e54a620854a70e105c4c4832a600e0ce
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-23 01:51:43 +0900

    [test] new Car position starts from 0

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,19 +3,14 @@ package racingcar.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static racingcar.Constants.*;
-import racingcar.strategy.DoOneForward;
-import racingcar.strategy.RaceCondition;
-
 import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
 
+    @DisplayName("Car 최초 생성 시 위치 0에서 출발 확인")
     @Test
     public void assertCarInitPositionIsZero() {
-
-        assertThat(new Car(new Position(0)).getPosition()).isEqualTo(INIT_POSITION);
-
+        assertThat(new Car().getPosition()).isEqualTo(0);
     }
     /**
      * Mocking Test (random 추상화) : PositionTest으로 이관
