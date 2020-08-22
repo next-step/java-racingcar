f1aa05fb716cb27fdf0dac129034c6d1659f36f7
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-23 01:59:33 +0900

    [test] Strategy oneForward, zeroForward Mock test

--- src/test/java/racingcar/strategy/StrategyTest.java
+++ src/test/java/racingcar/strategy/StrategyTest.java
@@ -1,27 +1,31 @@
-package racingcar.domain;
+package racingcar.strategy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.strategy.DoOneForward;
-import racingcar.strategy.RaceCondition;
+import racingcar.domain.Car;
+import racingcar.domain.Position;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+public class StrategyTest {
 
-public class PositionTest {
-
+    @DisplayName("1만큼 전진 전략 확인")
     @Test
     void OneForwardPosition() {
         Car car1 = new Car(new Position(3));
         RaceCondition alwaysOneForward = () -> true;
         car1.move(alwaysOneForward, new DoOneForward());
-        assertThat(car1.getPosition()).isEqualTo(4);
+        int actual = car1.getPosition();
+        assertThat(actual).isEqualTo(4);
     }
 
+    @DisplayName("0만큼 전진, 즉 멈춤 전략 확인")
     @Test
     void ZeroForwardPosition() {
         Car car2 = new Car(new Position(3));
         RaceCondition alwaysZeroForward = () -> false;
         car2.move(alwaysZeroForward, new DoOneForward());
-        assertThat(car2.getPosition()).isEqualTo(3);
+        int actual = car2.getPosition();
+        assertThat(actual).isEqualTo(3);
     }
 }
