4a596c19af0561d793167e8afdad18d09c55d41b
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-24 01:11:28 +0900

    [feat] ResultView constants

--- src/main/java/racingcar/Constants.java
+++ src/main/java/racingcar/Constants.java
@@ -10,10 +10,11 @@ public final class Constants {
     public static final int RANDOM_BOUND = 10;
 
     /** UI View **/
-    public static final String ASK_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
-    public static final String ASK_RACING_COUNT = "시도할 회수는 몇 회 인가요?";
+    public static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)";
+    public static final String ASK_RACING_COUNT = "시도할 회수는 몇회인가요?";
     public static final String SAY_VIEW_RESULT = "실행 결과";
     public static final String PRINT_GO = "-";
     public static final String EMPTY_LINE = "";
+    public static final String SAY_WINNERS_NAMES = "가 최종 우승했습니다."; //pobi, honux가 최종 우승했습니다.
 
 }
