6bd2e2f3d97e1667c5faeaf3721f95ab1fb38091
yeGenieee <cyj95428@gmail.com>
2020-08-23 18:31:03 +0900

    feat: 사용자 입력 메시지 상수화

---
+++ src/main/java/racingcar/Constants.java
@@ -0,0 +1,6 @@
+package racingcar;
+
+public class Constants {
+    public static final String INPUT_CAR_NUMBER_COMMANDS = "자동차 대수는 몇 대 인가요?";
+    public static final String INPUT_ROUND_NUMBER_COMMANDS = "시도할 횟수는 몇 회 인가요?";
+}
