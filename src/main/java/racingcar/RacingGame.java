b8b651a542e37251e61f398d36bacf91d3260ebc
yeGenieee <cyj95428@gmail.com>
2020-08-24 00:28:40 +0900

    feat: 자동차 경주 게임 로직 추가

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,45 @@
+package racingcar;
+
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RacingGame {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        inputView.input();
+
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < inputView.getCarNumber(); i++) {
+            cars.add(new Car());
+        }
+        gameStart(cars, inputView.getRound());
+    }
+
+    public static void gameStart(List<Car> cars, int round) {
+        System.out.println(Constants.RESULT_MESSAGE);
+        ResultView resultView = new ResultView();
+        for (int i = 0; i < round; i++) {
+            for (int j = 0; j < cars.size(); j++) {
+                if (isMove()) {
+                    cars.get(j).position++;
+                }
+                resultView.printResult(cars.get(j).position);
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean isMove() {
+        return getRandomNumber() >= Constants.MOVE_CONDITION;
+    }
+
+    private static int getRandomNumber() {
+        Random rand = new Random();
+        return rand.nextInt(10);
+    }
+
+}
