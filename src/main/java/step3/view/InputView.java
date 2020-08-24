4c0fd58e7a1cb391d95a615e3f7c9f8fd9ac2359
mocha121212 <mocha121212@gmail.com>
2020-08-24 23:30:46 +0900

    feat: test4  입력 UI

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -38,4 +38,11 @@ public class InputView {
         return result;
     }
 
+    public String inputNameView(String data) {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String result = inputString(data);
+        System.out.println(result);
+        return result;
+    }
+
 }
