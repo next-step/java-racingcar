b57c02fb1a2eefb5b1d87df4b43bcb9eaef7ba7d
Jiyeon Rhea Park <rheazen@gmail.com>
2020-08-25 23:58:27 +0900

    [refac] FEEDBACK : util InputView ResultView

--- src/main/java/racingcar/RacingCarMain.java
+++ src/main/java/racingcar/RacingCarMain.java
@@ -1,22 +1,17 @@
 package racingcar;
 
-import racingcar.domain.RacingGame;
-import racingcar.domain.RacingGamesFactory;
-import racingcar.view.InputChannel;
-import racingcar.view.InputView;
-import racingcar.view.OutputChannel;
-import racingcar.view.ResultView;
-
 import static racingcar.domain.RacingGamesFactory.createRacingGames;
+import static racingcar.view.InputView.*;
+import static racingcar.view.ResultView.*;
+
 
 public class RacingCarMain {
     public static void main(String[] args) {
-        InputView inputView = new InputView(InputChannel.createSystemIn( ), OutputChannel.createSystemOut( ));
-        createRacingGames(inputView.getCarNames(), inputView.getRacingCounts());
-        ResultView resultView = new ResultView(OutputChannel.createSystemOut( ));
-        resultView.printStartResult();
+        createRacingGames(getCarNames(), getRacingCounts());
+        printStartResult();
+        printEmptyLine();
         for (int i = 0 ; i < racingGames.getRacingCounts(); i++) {
-            resultView.printCars(racingGame.recordRacing());
+            printCars(racingGame.recordRacing());
             resultView.printEmptyLine();
        // }
         resultView.printWinners(racingGame.recordWinnerCars());
