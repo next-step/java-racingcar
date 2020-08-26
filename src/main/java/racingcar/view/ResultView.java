5bc409a2bdb923705aca8b55ed77f2d50d889b40
Taehee9 <dhstoalfh9509@naver.com>
2020-08-26 14:27:08 +0900

    feat: 우승자 추가에 따른 input,result view 변화

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,11 +1,13 @@
 package racingcar.view;
 
 import racingcar.Car;
+import racingcar.Winner;
 
 import java.util.List;
 
 public class ResultView {
     private final String MOVEMENT = "-";
+    private Winner winner = new Winner();
 
     public void printResultMessage() {
         System.out.println("실행 결과");
@@ -15,11 +17,15 @@ public class ResultView {
         cars.stream()
                 .forEach(car -> {
                     int position = car.getPosition();
+                    System.out.print(car.getName() + " : ");
                     for(int i = 0; i < position; i++){
                         System.out.print(MOVEMENT);
                     }
                     System.out.println();
                 });
         System.out.println();
+    }
+    public void printWinner(List<Car> cars) {
+        System.out.println(winner.findWinner(cars) + "가 최종 우승했습니다.");
     }
 }
