2e71928b15697eb09ee263c7ee1834b706e7fe8e
Jiyeon Rhea Park <rheazen@gmail.com>
2020-09-02 02:53:52 +0900

    [refac] CarsList result, carList getCarListView, car printPosition

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,33 +1,53 @@
 package racingcar.view;
 
+import racingcar.domain.RaceResults;
 import racingcar.domain.car.Car;
+import racingcar.domain.car.Cars;
 import racingcar.domain.car.WinnerCars;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static racingcar.utils.Constants.*;
+import static racingcar.utils.StringUtils.getNames;
 import static racingcar.utils.StringUtils.repeat;
 
-public class ResultView{
+public class ResultView {
 
-    private static OutputChannel outputChannel = OutputChannel.createSystemOut();
+    private static OutputChannel outputChannel = OutputChannel.createSystemOut( );
 
-    private ResultView(){}
+    private ResultView() {
+    }
 
     public static void printStartResult() {
-        outputChannel.printLine("\n"+SAY_VIEW_RESULT);
+        outputChannel.printLine("\n" + SAY_VIEW_RESULT);
     }
 
-    public static void printCars(List<Car> cars) {
-        cars.forEach(ResultView::printPosition);
+    public void printRaceResult(List<Cars> carsList) {
+        carsList.stream()
+                .map(Cars::getCars)
+                .forEach(ResultView::getCarListForNewLine);
     }
 
-    public static void printPosition(Car car) {
-        outputChannel.printLine(car.getCarName() + SAY_CAR_NAMES + repeat(PRINT_GO, car.getPosition()));
+    private static void getCarListForNewLine(List<Car> carList) {
+        carList.stream()
+                .forEach(ResultView::printResultPerCar);
+        printEmptyLine();
     }
+
+    private static void printResultPerCar(Car car) {
+           outputChannel.printLine(car.getCarName( ) + SAY_CAR_NAMES + repeat(PRINT_GO, car.getPosition( )));
+       }
+
     public static void printEmptyLine() {
         outputChannel.printLine(EMPTY_LINE);
     }
 
-    public static void printWinners(String winnerCarsNames) {outputChannel.printLine(winnerCarsNames + SAY_WINNER_CARS_NAMES); }
+    public static void printWinners(String winnerNames) {
+        outputChannel.printLine(winnerNames + SAY_WINNER_CARS_NAMES);
+    }
+
+
+
 }
