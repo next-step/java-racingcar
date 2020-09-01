605934c265d2bf8bdd1c48ecb9c77ae34224ed75
EddyJo <jkjk0600@gamil.com>
2020-09-01 22:49:54 +0900

    feat 테스트 코드 및 우승자 구하기 함수 리팩토리

--- src/main/java/racing/Winners.java
+++ src/main/java/racing/Winners.java
@@ -2,6 +2,7 @@ package racing;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,15 +10,20 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toCollection;
 
 public class Winners {
-    
+
     public static ArrayList<String> getWinner(RacingCars cars) {
         ArrayList<RacingCar> carList = (ArrayList)cars.getRecingCarList();
-        int max = carList.stream().max(Comparator.comparing(RacingCar::getRacingResult))
-                                  .map(RacingCar::getRacingResult)
-                                  .get();
+        int max = getMaxRecord(carList);
 
         return carList.stream().filter(car -> car.getRacingResult() == max)
                                .map(car -> car.getName())
                                .collect(toCollection(ArrayList::new));
+    }
+
+
+    public static int getMaxRecord(List<RacingCar> carList) {
+        return carList.stream().max(Comparator.comparing(RacingCar::getRacingResult))
+                               .map(RacingCar::getRacingResult)
+                               .get();
     }
 }
--- src/test/java/racing/WinnersTest.java
+++ src/test/java/racing/WinnersTest.java
@@ -1,7 +1,12 @@
 package racing;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racing.strategy.DefaultMoveStategy;
+import racing.strategy.MoveStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,26 +19,55 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinnersTest {
 
-    @Test
-    public void 우승자찾기() {
+    @DisplayName("우승자리스트 찾기")
+    @ParameterizedTest
+    @MethodSource("getCars")
+    public void 우승자찾기(String[] inputs, int roundOfRacing, MoveStrategy strategy, ArrayList<String> expected) {
 
-        String[] inputs = Stream.<String>builder()
-                                .add("경현")
-                                .add("에디")
-                                .add("슈마허")
-                                .build()
-                                .toArray(String[]::new);
+        RacingCars cars = RacingCars.of(inputs);
+        cars.startRacing(roundOfRacing, strategy);
 
-        ArrayList<String> excpected = Stream.<String>builder()
-                .add("경현")
+        assertThat(Winners.getWinner(cars)).isEqualTo(expected);
+    }
+
+    @DisplayName("최대갑 구하기")
+    @ParameterizedTest
+    @MethodSource("getCarsWithMaxRecord")
+    public void 최대값구하기(String[] inputs, int roundOfRacing, MoveStrategy strategy, int expected) {
+
+        RacingCars cars = RacingCars.of(inputs);
+        cars.startRacing(roundOfRacing, strategy);
+
+        assertThat(Winners.getMaxRecord(cars.getRecingCarList())).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> getCars() {
+        String[] inputs = Stream.<String>builder().add("경현")
+                .add("에디")
+                .add("슈마허")
+                .build()
+                .toArray(String[]::new);
+
+        ArrayList<String> excpected = Stream.<String>builder().add("경현")
                 .add("에디")
                 .add("슈마허")
                 .build()
                 .collect(toCollection(ArrayList<String>::new));
 
-        RacingCars cars = RacingCars.of(inputs);
-        cars.startRacing(3, new DefaultMoveStategy());
+        return Stream.of(
+                Arguments.of(inputs, 3, new DefaultMoveStategy(), excpected)
+        );
+    }
+    static Stream<Arguments> getCarsWithMaxRecord() {
+        String[] inputs = Stream.<String>builder().add("경현")
+                .add("에디")
+                .add("슈마허")
+                .build()
+                .toArray(String[]::new);
 
-        assertThat(Winners.getWinner(cars)).isEqualTo(excpected);
+
+        return Stream.of(
+                Arguments.of(inputs, 3, new DefaultMoveStategy(), 3)
+        );
     }
 }
