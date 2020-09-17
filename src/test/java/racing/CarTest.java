f488d58e9d2f0bda51672118a649d8f263ac3b33
bora <cbora852@gmail.com>
2020-09-17 18:59:49 +0900

    feat : step3 피드백 반영 - 자동차가 멈춰있는 경우 테스트 추가

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -2,9 +2,7 @@ package racing;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import racing.domain.Car;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,10 +17,15 @@ class CarTest {
 
     @Test
     public void moveTest() {
-        final int possibleMovingDistance = 5;
-
+        final int possibleMovingDistance = 4;
         car.run(possibleMovingDistance);
-
         assertThat(car.getLocation()).isEqualTo(1);
+    }
+
+    @Test
+    public void stopTest() {
+        final int possibleMovingDistance = 3;
+        car.run(possibleMovingDistance);
+        assertThat(car.getLocation()).isEqualTo(0);
     }
 }
