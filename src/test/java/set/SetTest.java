670493a19887045a8d901b2a49bfa416731617a3
siyoon <siyoon210@gmail.com>
2020-10-28 07:02:28 +0900

    test: Set 학습테스트 요구사항2 - contains
    @ParameterizedTest, @ValueSource 활용

--- src/test/java/set/SetTest.java
+++ src/test/java/set/SetTest.java
@@ -3,6 +3,8 @@ package set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,11 @@ public class SetTest {
     @DisplayName("Set Collection에 대한 학습 테스트 요구사항1 - size")
     void setSizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void setContainsTest(int value) {
+        assertThat(numbers.contains(value)).isTrue();
     }
 }
