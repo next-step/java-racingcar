bfda09ea73804316be7d17307130280c35355d55
kriery <jongchan.lee@lmep.kr>
2020-10-28 00:02:55 +0900

    added: SetTest {size, contains, contains(w/false cases)}

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static java.lang.Integer.parseInt;
+import static java.lang.Boolean.parseBoolean;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("test for `Set.contains` using `ParameterizedTest`")
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    @DisplayName("test for `Set.contains` using `ParameterizedTest` with false cases")
+    void containsWithFalseCases(String input, String expected) {
+        assertThat(numbers.contains(parseInt(input))).isEqualTo(parseBoolean(expected));
+    }
+}
