a9cc8937058deae1e551260d23bfa0770b18fda6
oen142 <oen142@naver.com>
2020-10-28 19:43:59 +0900

    요구사항 2 (1,2) 가 주어졌을때 ()를 제외한 1,2가 들어있는지 확인을 한다.

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -2,6 +2,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class StringTest {
 
     @Test
@@ -12,7 +14,18 @@ class StringTest {
         //when
         String[] result = input.split(",");
         //then
-        Assertions.assertThat(result).contains("1","2");
+        assertThat(result).contains("1","2");
 
+    }
+
+    @Test
+    @DisplayName("(1,2) 가 주어졌을때 ()를 제외한 1,2가 들어있는지 확인을 한다.")
+    void 요구사항_2(){
+        //given
+        String input = "(1,2)";
+        //when
+        String result = input.substring(1 , input.length() -1);
+        //then
+        assertThat(result).isEqualTo("1,2");
     }
 }
