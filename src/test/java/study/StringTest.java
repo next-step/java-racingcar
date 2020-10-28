fb790df152b22f00bf55ebcb1728aa9910ce9172
kit(김광수) <kit.t@kakaocorp.com>
2020-10-28 22:50:40 +0900

    feat: charAt, 에외발생 학습테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -14,6 +15,18 @@ public class StringTest {
     @Test
     void substring() {
         assertThat(stripBracket("(1,2)")).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("String#charAt 의 학습테스트")
+    void charAt() {
+        String input = "abc";
+        assertThat(input.charAt(0)).isEqualTo('a');
+
+        assertThatThrownBy(() -> {
+            input.charAt(input.length() + 1);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class) //
+                .hasMessage("String index out of range: 4");
     }
 
     private String stripBracket(String input) {
