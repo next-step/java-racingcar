e1697a39a0ef5b64728724fa2db727d2928fb644
naru <hsik0225@gmail.com>
2020-10-29 03:37:47 +0900

    feat : Calculator 추상 클래스 구현

--- src/main/java/calc/Calculator.java
+++ src/main/java/calc/Calculator.java
@@ -1,5 +1,22 @@
 package calc;
 
-public class Calculator {
+public abstract class Calculator {
 
+    protected abstract int calculate();
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return  a / b;
+    }
 }
