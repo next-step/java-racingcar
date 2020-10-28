b199eee196542cf6ab1f34ddd583d7dde752114a
oen142 <oen142@naver.com>
2020-10-28 22:34:39 +0900

    feat : 요구사항 3 1,2,3는 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false가 반환된다.

--- src/test/java/step1/SetApiTest.java
+++ src/test/java/step1/SetApiTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -17,22 +18,33 @@ class SetApiTest {
     private Set<Integer> numbers;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         numbers = new HashSet<>();
         numbers.add(1);
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
     }
+
     @Test
     @DisplayName("Set Collection Size를 확인한다.")
-    void 요구사항_1(){
+    void 요구사항_1() {
         assertThat(numbers.size()).isEqualTo(3);
     }
+
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3})
+    @ValueSource(ints = {1, 2, 3})
     @DisplayName("Set contains() 메소드를 활용해 1,2,3의 값이 존재하는지를 확인한다.")
-    void 요구사항_2(int number){
+    void 요구사항_2(int number) {
         assertThat(numbers.contains(number)).isTrue();
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"} , delimiter = ':')
+    @DisplayName("1,2,3값은 contains메소드 실행 결과 true 4,5값을 넣으면 false를 반환한다.")
+    void 요구사항_3(int input, boolean expected) {
+
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
 }
