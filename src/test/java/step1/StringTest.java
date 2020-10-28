c169e6ca774e0156978c94326aef4fd59ee009c6
Wani <46095454+oen142@users.noreply.github.com>
2020-10-28 23:51:22 +0900

    Step1 학습 테스트 실습 리뷰 부탁드립니다! (#1283)
    * empty.txt delete
    
    * 요구사항 1
    String 1,2가 주어졌을경우 1 2가 있는지 확인을 한다.
    
    * 요구사항 2
    (1,2) 가 주어졌을때 ()를 제외한 1,2가 들어있는지 확인을 한다.
    
    * 요구사항 1
    Set Collection size함수로 갯수를 확인한다
    
    * 패키지 스텝 나누기
    
    * doc : 학습목표와 코드를 짜야할때  클린코드를 구현하는 방법을
    정리한다.
    
    * feat : 요구사항 2
    Set의 contains() 메소드를 활용해 1,2,3의 값이 존재하는지 확인
    한다.
    JUnit의 ParameterizedTest를 활용해 중복을 제거한다.
    
    * feat : 요구사항 3
    1,2,3는 contains 메소드 실행결과 true,
    4, 5 값을 넣으면 false가 반환된다.

---
+++ src/test/java/step1/StringTest.java
@@ -0,0 +1,48 @@
+package step1;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class StringTest {
+
+    @Test
+    @DisplayName("1,2가 주어졌을때 1 2가 배열로 있는지 확인을 한다.")
+    void 요구사항_1(){
+        //given
+        String input = "1,2";
+        //when
+        String[] result = input.split(",");
+        //then
+        assertThat(result).contains("1","2");
+
+    }
+
+    @Test
+    @DisplayName("(1,2) 가 주어졌을때 ()를 제외한 1,2가 들어있는지 확인을 한다.")
+    void 요구사항_2(){
+        //given
+        String input = "(1,2)";
+        //when
+        String result = input.substring(1 , input.length() -1);
+        //then
+        assertThat(result).isEqualTo("1,2");
+    }
+    @Test
+    @DisplayName("String abe가 주어졌을 경우 String 클래스의 charAt(n) 메소드를 확인해서 n이 범위를 넘을 경우 익셉션이 발생한다.")
+    void 요구사항_3(){
+        //given
+        String input = "abc";
+
+        //then
+        assertThat(input.charAt(0)).isEqualTo('a');
+        assertThat(input.charAt(1)).isEqualTo('b');
+        assertThat(input.charAt(2)).isEqualTo('c');
+        assertThatThrownBy(()-> input.charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class);
+        assertThatThrownBy(()-> input.charAt(-1)).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+    }
+}
