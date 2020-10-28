a3048272e733bce46cb9b2c43462c38d77c06e6a
joongs <luck1004.kim@gmail.com>
2020-10-28 23:50:40 +0900

    add: apply parameterized test case (requirement 2)

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,11 +3,14 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -36,5 +39,15 @@ public class SetTest {
         assertThat(numbers.size())
                 .isNotEqualTo(4)
                 .isLessThan(4);
+    }
+
+    @ParameterizedTest
+    @DisplayName("JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.")
+    @ValueSource(ints = {1,2,3})
+    void contains(int input_number) {
+//        assertThat(numbers.contains(1)).isTrue();
+//        assertThat(numbers.contains(2)).isTrue();
+//        assertThat(numbers.contains(3)).isTrue();
+        assertTrue(numbers.contains(input_number));
     }
 }
