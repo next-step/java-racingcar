b255dd5224ffe55ca2d2e10c52e591a1f05400b8
naru <hsik0225@gmail.com>
2020-10-29 04:53:53 +0900

    style : Calculator 클래스의 상수 a, b의 변수 명을 pre, post로 변경

--- src/main/java/calc/Calculator.java
+++ src/main/java/calc/Calculator.java
@@ -1,22 +1,20 @@
 package calc;
 
-public abstract class Calculator {
+public class Calculator {
 
-    protected abstract int calculate();
-
-    public static int add(int a, int b) {
-        return a + b;
+    public static int add(int pre, int post) {
+        return pre + post;
     }
 
-    public static int subtract(int a, int b) {
-        return a - b;
+    public static int subtract(int pre, int post) {
+        return pre - post;
     }
 
-    public static int multiply(int a, int b) {
-        return a * b;
+    public static int multiply(int pre, int post) {
+        return pre * post;
     }
 
-    public static int divide(int a, int b) {
-        return  a / b;
+    public static int divide(int pre, int post) {
+        return  pre / post;
     }
 }
