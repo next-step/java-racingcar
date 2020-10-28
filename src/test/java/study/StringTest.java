4a4c8e81cb354536e03369853084463c805f4020
고정완 (Can) <jw.gho@ecubelabs.com>
2020-10-28 21:41:54 +0900

    [refactor]: 한 줄에 점을 하나만 찍도록 수정

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -25,7 +25,10 @@ public class StringTest {
     void substring() {
         // (1,2)" 값이 주어졌을 때 String 의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현
         String result = "(1,2)";
-        assertThat(result.substring(1, result.length() - 1)).isEqualTo("1,2");
+        assertThat(result.substring(
+                1,
+                result.length() - 1
+        )).isEqualTo("1,2");
     }
 
     @Test
@@ -34,16 +37,21 @@ public class StringTest {
         String abc = "abc";
 
         // "abc" 값이 주어졌을 때 String 의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트
-        assertThat(abc.charAt(0)).isEqualTo('a');
-        assertThat(abc.charAt(1)).isEqualTo('b');
-        assertThat(abc.charAt(2)).isEqualTo('c');
+        assertThat(abc.charAt(0))
+                .isEqualTo('a');
+        assertThat(abc.charAt(1))
+                .isEqualTo('b');
+        assertThat(abc.charAt(2))
+                .isEqualTo('c');
 
         // String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트
         assertThatThrownBy(() -> {
             abc.charAt(3);
-        }).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("String index out of range: 3");
-        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
-            abc.charAt(3);
-        }).withMessageMatching("String index out of range: 3");
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    abc.charAt(3);
+                }).withMessageMatching("String index out of range: 3");
     }
 }
