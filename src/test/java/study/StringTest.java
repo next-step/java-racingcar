6297f116d052f294a9268ece5702afde5b7e2a00
rlawls1991 <rlawls1991@gmail.com>
2020-10-28 22:35:55 +0900

    split study test

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,11 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+    @Test
+    void split(){
+        String[] result = "1, 2".split(",");
+        assertThat(result).con
+    }
+}
