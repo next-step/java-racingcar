426619fb0a6771bcf9bcdb6ac911f7b0cfa74dc3
apalsl <apalsl123@gmail.com>
2020-10-28 22:58:45 +0900

    String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,6 @@
 package study;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -8,9 +8,45 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
 
     @Test
-    void split() {
+    @DisplayName(value = "String split test")
+    void split() throws Exception {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    @DisplayName(value = "String subString test")
+    void subString() throws Exception {
+        //given
+        String input = "(1,2)";
+
+        //when
+        int firstBracket = input.indexOf('(');
+        int lastBracket = input.indexOf(')');
+        String result = input.substring(firstBracket, lastBracket);
+
+        //then
+        assertThat(result).contains("1,2");
+    }
+
+    @Test
+    @DisplayName(value = "String charAt, IndexOutOfBoundsException test")
+    public void charAt() throws Exception {
+        //given
+        String input = "abc";
+
+        //when
+        int index = 3;
+
+        //then
+        assertThatThrownBy(() -> input.charAt(index))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: " + index);
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> input.charAt(index))
+                .withMessageMatching("String index out of range: " + index);
+    }
+
 }
