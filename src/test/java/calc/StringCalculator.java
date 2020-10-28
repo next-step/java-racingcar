89fa6300be4a60a559d4c198ea3b6a8464a9200d
naru <hsik0225@gmail.com>
2020-10-29 01:04:38 +0900

    test : 음수만 있는 문자열 파싱 테스트 코드 구현

--- src/test/java/calc/StringCalculator.java
+++ src/test/java/calc/StringCalculator.java
@@ -12,12 +12,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringCalculator {
 
-    private static final Pattern pattern = Pattern.compile("^([\\d])([+-/*]\\d)*$");
+    private static final Pattern pattern = Pattern.compile("^(-?[\\d])([+-/*]\\d)*$");
 
     @Test
-    @DisplayName("숫자만 있는 문자열을 정규표현식에 맞게 자릅니다")
+    @DisplayName("양수만 있는 문자열을 정규표현식에 맞게 자릅니다")
     public void 양수만_있는_식_파싱_테스트() {
         final String expression = "1";
+        final Matcher matcher = pattern.matcher(expression);
+
+        assertTrue(matcher.matches());
+
+        // 숫자와 null 두 개의 그룹으로 나뉩니다
+        assertThat(matcher.groupCount()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("음수만 있는 문자열을 정규표현식에 맞게 자릅니다")
+    public void 음수만_있는_식_파싱_테스트() {
+        final String expression = "-1";
         final Matcher matcher = pattern.matcher(expression);
 
         assertTrue(matcher.matches());
