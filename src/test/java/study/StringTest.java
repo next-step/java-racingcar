3453a5fd7f8534444883c485c145823f497b121e
kit(김광수) <kit.t@kakaocorp.com>
2020-10-28 22:27:49 +0900

    feat: substring을 이용한 괄호제거

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,10 @@ public class StringTest {
         assertThat("1,2".split(",")).contains("1", "2");
         assertThat("1".split(",")).containsExactly("1");
     }
+    @Test
+    void substring() {
+        String string = "(1,2)";
+        String result = string.substring(string.indexOf("(") + 1, string.indexOf(")"));
+        assertThat(result).isEqualTo("1,2");
+    }
 }
