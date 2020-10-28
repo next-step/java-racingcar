4b99cf89aca19b82dd6ad831a0281465bf9ede68
naru <hsik0225@gmail.com>
2020-10-29 03:38:09 +0900

    feat : 주어진 문자열을 숫자, 사칙연산 부호로 나누는 Separator 클래스 구현

---
+++ src/main/java/calc/Separator.java
@@ -0,0 +1,20 @@
+package calc;
+
+public final class Separator {
+
+    private static final String OPERATION_REG = "[+\\-*/]";
+
+    private static final String NUMBER_REG = "\\d";
+
+    private Separator() {}
+
+    // 주어진 식을 숫자 배열로 스플릿합니다
+    private static String[] splitNumbers(final String expression) {
+        return expression.split(OPERATION_REG);
+    }
+
+    // 주어진 식을 사칙연산 배열로 스플릿합니다
+    private static String[] splitOperations(final String expression) {
+        return expression.split(NUMBER_REG);
+    }
+}
