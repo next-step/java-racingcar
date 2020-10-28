7088eced0dd6e88b60b05a99584b815d6863a71d
naru <hsik0225@gmail.com>
2020-10-29 01:01:08 +0900

    test : 부호가 있는 식 파싱 테스트 구현

--- src/test/java/calc/StringCalculator.java
+++ src/test/java/calc/StringCalculator.java
@@ -5,12 +5,14 @@ import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringCalculator {
 
-    private static final Pattern pattern = Pattern.compile("^\\d+$");
+    private static final Pattern pattern = Pattern.compile("^([\\d])([+-/*]\\d)*$");
 
     @Test
     @DisplayName("숫자만 있는 문자열을 정규표현식에 맞게 자릅니다")
@@ -19,6 +21,20 @@ public class StringCalculator {
         final Matcher matcher = pattern.matcher(expression);
 
         assertTrue(matcher.matches());
+
+        // 숫자와 null 두 개의 그룹으로 나뉩니다
+        assertThat(matcher.groupCount()).isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("부호가 같이 있는 문자열을 정규표현식에 맞게 자릅니다")
+    public void 부호가_있는_식_파싱_테스트() {
+        final String expression = StringUtils.replaceWhitespaceCharacters("1 + 2", "");
+        final Matcher matcher = pattern.matcher(expression);
+
+        assertTrue(matcher.matches());
+
+        assertThat(matcher.group(1)).isEqualTo("1");
+        assertThat(matcher.group(2)).isEqualTo("+2");
+    }
 }
