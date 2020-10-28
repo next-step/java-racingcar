0f959ad22cd9ae18102b37603450cf02844287ce
oen142 <oen142@naver.com>
2020-10-28 22:30:20 +0900

    feat : 요구사항 2 Set의 contains() 메소드를 활용해 1,2,3의 값이 존재하는지 확인 한다. JUnit의 ParameterizedTest를 활용해 중복을 제거한다.

--- src/test/java/step1/SetApiTest.java
+++ src/test/java/step1/SetApiTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -26,5 +28,11 @@ class SetApiTest {
     @DisplayName("Set Collection Size를 확인한다.")
     void 요구사항_1(){
         assertThat(numbers.size()).isEqualTo(3);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("Set contains() 메소드를 활용해 1,2,3의 값이 존재하는지를 확인한다.")
+    void 요구사항_2(int number){
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
