45cd14b2df128b1937d14bd4b99749a337aae1df
Sunghyeob.Baek <baek9652@naver.com>
2020-10-28 23:05:51 +0900

    Add additional string test
    요구사항에 명세된 String 관련 unittest 추가
    
     * subString()
      - 처음 인덱스와 마지막 인덱스에 해당되는 Charater 제거하여 기댓값과 비교
    
     * charAt()
      - 정상적인 index를 벗어난 접근에 대한 예외 발생 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,71 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
+
     @Test
-    void split() {
-        String[] result = "1,2".split(",");
-        assertThat(result).containsExactly("1", "2");
+    @DisplayName("split() 동작 테스트")
+    void splitString() {
+        String input = "1,2";
+
+        String[] expected = input.split(",");
+
+        assertThat(expected)
+                .containsExactly("1", "2")
+                .contains("1")
+                .contains("2");
+    }
+
+    @Test
+    @DisplayName("Substring() 동작 테스트")
+    void subString() {
+        String input = "(1,2)";
+        int beginIndex = 1;
+        int lastIndex = input.length() - 1;
+
+        String expected = input.substring(beginIndex, lastIndex);
+
+        assertThat(expected)
+                .isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("올바른 인덱싱에서의 charAt() 동작 테스트")
+    void charAtWithValidIndex() {
+        String input = "abc";
+        Character[] results = {'a', 'b', 'c'};
+
+        for (int i = 0; i < input.length(); i++) {
+            Character expected = input.charAt(i);
+            assertThat(expected)
+                    .isEqualTo(results[i]);
+        }
+
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 인덱싱에서의 charAt() 동작 테스트")
+    void charAtWithInvalidIndex() {
+        String input = "abc";
+        int invalidSmallIndex = -1;
+        int invalidBigIndex = input.length();
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(
+                        () -> {
+                            input.charAt(invalidSmallIndex);
+                        }
+                );
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(
+                        () -> {
+                            input.charAt(invalidBigIndex);
+                        }
+                );
     }
 }
