8ed6f66e1e71c83e40046cfe1cd8277d33f19f33
Kaeum <oror1024@gmail.com>
2020-10-29 01:10:09 +0900

    add new test files

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,4 +1,50 @@
 package study;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 테스트")
+    void sizeTest(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("ValueSource 어노테이션을 활용하여 컬렉션이 값을 포함하는지 테스트")
+    void containsValue_WithValueSourceTest(int input){
+        assertThat(numbers).contains(input);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true","2:true","3:true","4:false","5:false"}, delimiter = ':')
+    @DisplayName("CsvSource 어노테이션을 활용하여 컬렉션에 값 포함 여부 테스트")
+    void containsValue_ShouldReturnBooleanValueWithCsvSourceTest(String intInput, String booleanInput){
+        boolean actual = numbers.contains(Integer.parseInt(intInput));
+        boolean expected = Boolean.parseBoolean(booleanInput);
+        assertThat(actual).isEqualTo(expected);
+
+    }
+
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,52 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
     @Test
-    void split(){
+    @DisplayName("split() 테스트")
+    void splitTest(){
+
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
+
+    }
+
+    @Test
+    @DisplayName("substring() 테스트")
+    void substringTest(){
+
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+
+    }
+
+    @Test
+    @DisplayName("charAt() 테스트")
+    void charAtTest(){
+        assertThat("abc".charAt(0)).isEqualTo('a');
+        assertThat("abc".charAt(1)).isEqualTo('b');
+        assertThat("abc".charAt(2)).isEqualTo('c');
+
+
+
+    }
+
+    @Test
+    @DisplayName("charAt() 테스트. 예외 시 IOOB Exception 발생")
+    void charAtTest_ShouldThrowIndexOutOfBoundsExceptionTest(){
+
+        assertThatThrownBy( () -> {
+            "abc".charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+          .hasMessageContaining("out of range");
+
     }
 }
