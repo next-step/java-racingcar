c97d6126cdb9ba1ccb0c740fa1c19ad109af2ab2
oen142 <oen142@naver.com>
2020-10-29 00:26:23 +0900

    feat : Calculator 테스트 추가

---
+++ src/test/java/step2/domain/CalculatorTest.java
@@ -0,0 +1,40 @@
+package step2.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    @DisplayName("덧셈을 테스트 한다.")
+    void plus(){
+
+    }
+
+    @Test
+    @DisplayName("곱셈을 테스트 한다.")
+    void multiply(){
+
+    }
+    @Test
+    @DisplayName("나눗셈을 테스트 한다.")
+    void division(){
+
+    }
+    @Test
+    @DisplayName("입력값이 null일경우 IllegalArgumentException throw를 반환한다.")
+    void isNullThrowIllegalArgumentException(){
+
+    }
+    @Test
+    @DisplayName("입력값이 빈 공백 문자열일 경우 IllegalArgumentException throw를 반환한다.")
+    void isEmptyStringThrowIllegalArgumentException(){
+
+    }
+    @Test
+    @DisplayName("입력값이 사칙연산이 아닐 경우 IllegalArgumentException throw를 반환한다.")
+    void isNotInputValueOperatorThrowIllegalArgumentException(){
+
+    }
+}
