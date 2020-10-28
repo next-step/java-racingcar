862244c730a87e3515864744e22c3229d9a64b36
eyabc <bey4314@naver.com>
2020-10-29 01:53:59 +0900

    [자동차 경주-단위테스트] 1단계 - 학습 테스트 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,36 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
-    void name() {
-         String[] result = "1,2".split(",");
-         assertThat(result).containsExactly("1", "2");
+    @DisplayName("split , substring 테스트")
+    void stringClass() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1", "2");
+
+        String input2 = "(1,2)";
+        String expect2 = "1,2";
+
+        String result2 = input2.substring(1, input2.length() - 1);
+        assertThat(result2).isEqualTo(expect2);
     }
+
+    @Test
+    @DisplayName("charAt 의 StringIndexOutOfBoundsException 테스트")
+    void stringIndexOutOfBoundsException() {
+        String input = "abc";
+        int outOfBoundInteger = 4;
+
+        assertThatThrownBy(() -> {
+            input.charAt(outOfBoundInteger);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range");
+    }
+
 }
