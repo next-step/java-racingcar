f08378a1c0b9c40b718e08ba207bcd5b89e17df3
naru <hsik0225@gmail.com>
2020-10-28 22:39:52 +0900

    refactor : 테스트에 @DisplayName 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -27,17 +28,20 @@ public class SetTest {
     }
 
     @Test
+    @DisplayName("HashSet 의 크기 테스트")
     public void sizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
     @ParameterizedTest
+    @DisplayName("중복 코드를 제거한 HashSet 이 갖고 있는 값 테스트")
     @ValueSource(ints = {1, 2, 3})
     public void containsTest(final int number) {
         assertTrue(numbers.contains(number));
     }
 
     @ParameterizedTest
+    @DisplayName("CsvSource를 이용한 입력 값에 따른 결과 값 테스트")
     @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
     public void size1Test(final int number, final String bool) {
         assertEquals(Boolean.parseBoolean(bool), numbers.contains(number));
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class StringTest {
 
     @Test
+    @DisplayName("기본 문자열 스플릿 테스트")
     public void splitTest() {
         final String input = "1,2";
         final String[] strings = input.split(",");
@@ -16,6 +18,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("구분자가 없는 문자열 스플릿 테스트")
     public void splitTestHasNoDelimiter() {
         final String input = "1";
         final String[] strings = input.split(",");
@@ -24,6 +27,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("subString 테스트")
     public void subStringTest() {
         String input = "(1,2)";
         input = input.substring(1, input.length() - 1);
@@ -32,6 +36,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("charAt 테스트")
     public void charAtTest() {
         final String input = "abc";
         final char a = input.charAt(0);
@@ -40,6 +45,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("인덱스 범위 밖 charAt 테스트")
     public void charAtExceptionTest() {
         final String input = "abc";
 
