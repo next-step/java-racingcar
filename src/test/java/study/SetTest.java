8e993748d5700a5cd1a0cdc1401b1b316b8bfd8e
kit(김광수) <kit.t@kakaocorp.com>
2020-10-28 22:55:49 +0900

    feat: Set#size 학습 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,13 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -17,6 +21,12 @@ public class SetTest {
         numbers.add(3);
     }
 
-
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인한다")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+        numbers.clear();
+        assertThat(numbers.size()).isEqualTo(0);
+    }
 }
 
