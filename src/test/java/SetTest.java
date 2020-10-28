bdc2e5fc27073bd8ea82ae33a8edbaf51fe83c76
catsbi <killfine198@gmail.com>
2020-10-29 01:01:44 +0900

    [자동차 경주]1단계 - 학습 테스트 실습

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,48 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+
+    @Test
+    @DisplayName("Set의 크기 확인 테스트")
+    public void sizeTest(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Set의 파라미터 확인 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void containsTest(int input){
+        assertTrue(numbers.contains(input));
+    }
+
+    @DisplayName("입력값에 따라 true뿐 아니라 false도 검사 가능한 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    public void containsByConditionTest(int inputNumber, boolean inputBool){
+        assertThat(numbers.contains(inputNumber)).isEqualTo(inputBool);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("문자열 분리 테스")
+    void split(){
+        //요구사항 1
+        //"1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+        //"1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+        String oneAndTwo = "1,2";
+        String one = "1";
+        String[] splitOneAndTwo = oneAndTwo.split(",");
+        String[] splitOne = one.split(",");
+
+        assertThat(splitOneAndTwo).contains("1");
+        assertThat(splitOneAndTwo).containsExactly("1","2");
+        assertThat(splitOne).containsOnly("1");
+        
+    }
+    
+    @Test
+    @DisplayName("문자열 substring 테스")
+    public void substringTest() throws Exception{
+        //요구사항 2
+        //"(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
+        String oneAndTwoWithParentheses = "(1,2)";
+        String substring = oneAndTwoWithParentheses.substring(1, oneAndTwoWithParentheses.length()-1);
+        assertThat(substring).isEqualTo("1,2");
+    }
+    
+    @Test
+    @DisplayName("charAt Exception 테스트")
+    public void charAtExceptionTest() throws Exception{
+        //요구사항 3
+        //"abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+        //String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+        //JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+
+        String abc = "abc";
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(()->abc.charAt(4))
+                .withMessageMatching("String index out of range: \\d+");
+    }
+}
