ca38822f19e146777b2a24a32770f3b034d62d55
joongs <luck1004.kim@gmail.com>
2020-10-28 23:29:08 +0900

    add: check size of set(requirment 1)

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,40 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
+    void check_size_of_set_is_equal_to() {
+        assertThat(numbers.size())
+                .isEqualTo(3)
+                .isGreaterThan(2);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
+    void check_size_of_set_with_is_not_equal_to() {
+        assertThat(numbers.size())
+                .isNotEqualTo(4)
+                .isLessThan(4);
+    }
+}
