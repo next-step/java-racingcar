2305dd009b19e63530a0c2239f47c56dd24c20e4
naru <hsik0225@gmail.com>
2020-10-29 02:15:46 +0900

    test : 음수만 있는 문자열 스플릿 테스트 코드 구현

--- src/test/java/calc/StringCalculator.java
+++ src/test/java/calc/StringCalculator.java
@@ -12,7 +12,7 @@ public class StringCalculator {
     public static final String NUMBER_REG = "\\d";
 
     /*
-    1. 숫자 배열을 생성하도록 split 한다. numbers = String#split("+-*")
+    1. 숫자 배열을 생성하도록 split 한다. numbers = String#split("[+\\-/*]")
     2. 부호 배열을 생성하도록 split 한다. operations = String#split("\\d")
     3. 주어진 식의 맨 앞이 - 로 시작하는지 확인한다. String#startswith("-")
         3-1. 만약 - 로 시작하면
@@ -24,7 +24,7 @@ public class StringCalculator {
      */
 
     @Test
-    @DisplayName("양수만_있는_문자열_스플릿_테스트")
+    @DisplayName("양수만 있는 문자열 스플릿 테스트")
     public void 양수만_있는_문자열_스플릿_테스트() {
 
         // given
@@ -35,6 +35,27 @@ public class StringCalculator {
 
         // then
         assertThat(numbers).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("음수만 있는 문자열 스플릿 테스트")
+    public void 음수만_있는_문자열_스플릿_테스트() {
+
+        // given
+        final String expression = "-1";
+
+        // when
+        final String[] numbers = splitNumbers(expression);
+
+        // then
+        // 사칙연산 정규표현식과 일치하는 첫 번째 문자열은 ""로 바뀐다
+        assertThat(numbers).containsExactly("", "1");
+    }
+
+    @Test
+    @DisplayName("부호가 1개 있는 문자열 스플릿 테스트")
+    public void 부호가_1개_있는_문자열_스플릿_테스트() {
+
     }
 
     private static String[] splitNumbers(final String expression) {
