8cf8f4086729456714b5dc96f4feb34679fb577e
siyoon <siyoon210@gmail.com>
2020-10-29 08:17:53 +0900

    refactor: 메서드 추출, 메서드명 rename

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -7,15 +7,32 @@ import java.util.List;
 public class StringCalculator {
     private static final String DELIMITER = " ";
 
-    public Integer calculator(String input) {
+    public Integer calculate(String input) {
+        validateInput(input);
+
+        List<Integer> numbers = new ArrayList<>();
+        List<Operator> operators = new ArrayList<>();
+        initArithmeticLists(input, numbers, operators);
+
+        Iterator<Integer> numberIterator = numbers.iterator();
+        Iterator<Operator> operatorIterator = operators.iterator();
+        Integer result = numberIterator.next();
+        while (numberIterator.hasNext() && operatorIterator.hasNext()) {
+            Integer nextNumber = numberIterator.next();
+            Operator nextOperator = operatorIterator.next();
+            result = nextOperator.calculate(result, nextNumber);
+        }
+        return result;
+    }
+
+    private void validateInput(String input) {
         if (input == null || input.equals("")) {
             throw new IllegalArgumentException("Input can not be null or empty.");
         }
+    }
 
+    private void initArithmeticLists(String input, List<Integer> numbers, List<Operator> operators) {
         String[] inputSplits = input.split(DELIMITER);
-        List<Integer> numbers = new ArrayList<>();
-        List<Operator> operators = new ArrayList<>();
-
         for (int i = 0; i < inputSplits.length; i++) {
             if (isNumberIndex(i)) {
                 numbers.add(Integer.valueOf(inputSplits[i]));
@@ -26,19 +43,6 @@ public class StringCalculator {
                 operators.add(Operator.findBy(inputSplits[i]));
             }
         }
-
-        Iterator<Integer> numberIterator = numbers.iterator();
-        Iterator<Operator> operatorIterator = operators.iterator();
-
-        Integer result = numberIterator.next();
-        while (numberIterator.hasNext() && operatorIterator.hasNext()) {
-            Integer nextNumber = numberIterator.next();
-            Operator nextOperator = operatorIterator.next();
-
-            result = nextOperator.calculate(result, nextNumber);
-        }
-
-        return result;
     }
 
     private boolean isNumberIndex(int i) {
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -5,11 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.in;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class StringCalculatorTest {
@@ -24,9 +22,9 @@ class StringCalculatorTest {
     @DisplayName("입력한 문자열 값에 따라 사칙연산을 수행")
     void calculator() {
         assertAll(
-                () -> assertThat(stringCalculator.calculator("2 + 3 * 4 / 2")).isEqualTo(10),
-                () -> assertThat(stringCalculator.calculator("1 + 2 - 3")).isEqualTo(0),
-                () -> assertThat(stringCalculator.calculator("3 / 2 - 30")).isEqualTo(-29)
+                () -> assertThat(stringCalculator.calculate("2 + 3 * 4 / 2")).isEqualTo(10),
+                () -> assertThat(stringCalculator.calculate("1 + 2 - 3")).isEqualTo(0),
+                () -> assertThat(stringCalculator.calculate("3 / 2 - 30")).isEqualTo(-29)
         );
     }
 
@@ -35,6 +33,6 @@ class StringCalculatorTest {
     @DisplayName("입력값이 null 이거나 빈 공백 문자일 경우 IllegalArgumentException throw")
     void calculator_failingCase_nullOrEmptyInput(String input) {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> stringCalculator.calculator(input));
+                .isThrownBy(() -> stringCalculator.calculate(input));
     }
 }
