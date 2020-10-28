29b7d724fa74dd4f5693881b533224fea6cdc61b
siyoon <siyoon210@gmail.com>
2020-10-28 19:30:37 +0900

    feat: 문자열로 Operator 찾는 findBy() 메서드 구현

--- src/main/java/stringcalculator/Operator.java
+++ src/main/java/stringcalculator/Operator.java
@@ -1,5 +1,7 @@
 package stringcalculator;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiFunction;
 
 enum Operator {
@@ -8,11 +10,29 @@ enum Operator {
     MULTIPLY("*", (num1, num2) -> num1 * num2),
     DIVIDE("/", (num1, num2) -> num1 / num2);
 
+    private static final Map<String, Operator> stringOperatorMap = new HashMap<>();
+
+    static {
+        for (Operator operator : Operator.values()) {
+            stringOperatorMap.put(operator.strValue, operator);
+        }
+    }
+
     private final String strValue;
     private final BiFunction<Integer, Integer, Integer> calculate;
 
     Operator(String strValue, BiFunction<Integer, Integer, Integer> calculate) {
         this.strValue = strValue;
         this.calculate = calculate;
+    }
+
+    private static Operator findBy(String strValue) {
+        Operator operator = stringOperatorMap.get(strValue);
+
+        if (operator == null) {
+            throw new IllegalArgumentException("Unexpected strValue to find Operator '" + strValue + "'");
+        }
+
+        return operator;
     }
 }
