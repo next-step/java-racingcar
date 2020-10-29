98610fe71e1c088f25c30ec7d5f3ebdde1d2dcd6
oen142 <oen142@naver.com>
2020-10-30 01:08:16 +0900

    fix : 안쓰게 되는 코드 삭제

--- src/main/java/step2/exception/BlankException.java
+++
@@ -1,7 +0,0 @@
-package step2.exception;
-
-public class BlankException extends IllegalArgumentException{
-    public BlankException() {
-        super("빈 공백을 입력하셨습니다.");
-    }
-}
--- src/main/java/step2/exception/NotOperatorException.java
+++
@@ -1,7 +0,0 @@
-package step2.exception;
-
-public class NotOperatorException extends IllegalArgumentException{
-    public NotOperatorException() {
-        super("맞지 않은 연산자가 입력되었습니다.");
-    }
-}
