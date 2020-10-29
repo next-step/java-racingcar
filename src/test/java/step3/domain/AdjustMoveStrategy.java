968c29b371fdc4a67184f715645a468324691056
oen142 <oen142@naver.com>
2020-10-30 00:53:36 +0900

    test : Cars클래스 테스트 케이스들 준비

---
+++ src/test/java/step3/domain/AdjustMoveStrategy.java
@@ -0,0 +1,8 @@
+package step3.domain;
+
+public class AdjustMoveStrategy implements MoveStrategy {
+    @Override
+    public boolean move(int condition) {
+        return true;
+    }
+}
---
+++ src/test/java/step3/domain/CarsTest.java
@@ -0,0 +1,21 @@
+package step3.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarsTest {
+
+    @Test
+    @DisplayName("Cars에 있는 List를 외부에서 건들수 없는지 확인을 한다.")
+    void touchNotCars(){
+
+    }
+    @Test
+    @DisplayName("Cars에 있는 Car들이 잘 움직이는지 확인한다.")
+    void moveCars(){
+
+    }
+
+}
