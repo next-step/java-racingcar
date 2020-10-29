783fede8a6a35fa3513d445c7eabfc730d977921
oen142 <oen142@naver.com>
2020-10-29 22:20:37 +0900

    test : 자동차 클래스 테스트

---
+++ src/test/java/step3/domain/CarTest.java
@@ -0,0 +1,28 @@
+package step3.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @ParameterizedTest
+    @DisplayName("자동차가 움직여서 포지션이 맞는지 확인")
+    @ValueSource(ints = { 1 , 2 , 3})
+    void getCarPosition(int moveCount){
+        Car car = Car.of();
+        for(int i =0 ; i< moveCount;i++){
+            car.move();
+        }
+        assertThat(car.getCarPosition()).isEqualTo(moveCount);
+    }
+
+}
