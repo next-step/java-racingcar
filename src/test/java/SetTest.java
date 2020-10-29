05dd6e1044931247caeadae648978f35429fe9b0
changgunyee <minyoung403@naver.com>
2020-10-29 15:42:48 +0900

    set사이즈 확인 테스트

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,7 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -13,5 +17,11 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("set의 사이즈 확인")
+    void checkSetSize() {
+        assertThat(numbers.size()).isEqualTo(3);
     }
 }
