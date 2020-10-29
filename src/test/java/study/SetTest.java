cf0e2c6010e6177c36b90f6de6085e1211a72c3e
Byeon-SungMun <201401339hs@gmail.com>
2020-10-29 23:52:41 +0900

    step1 misson

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,17 +1,17 @@
 package study;
 
 
-import org.assertj.core.util.Strings;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class SetTest {
@@ -28,9 +28,23 @@ public class SetTest {
     }
 
     @Test
+    @DisplayName("size() 메소드를 활용해 Set의 크기를 확인")
     public void size () {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
+    @ParameterizedTest
+    @DisplayName("1, 2, 3의 값이 존재하는지를 확인 및 중복 코드를 제거")
+    @ValueSource(ints =  {1,2,3})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("1, 2, 3의 값이 존재하면 true 4,5값이 존재하면 false")
+    @CsvSource(value = {"1:true", "2:true","3:true","4:false","5:false"},delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(int input, boolean bool) {
+        assertThat(numbers.contains(input)).isEqualTo(bool);
+    }
 
 }
