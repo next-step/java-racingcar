11b05741b060cd662b7ce4b1f7183716566f93a5
oen142 <oen142@naver.com>
2020-10-29 19:52:38 +0900

    feat : BlankException 정의

---
+++ src/test/java/step2/exception/BlankExceptionTest.java
@@ -0,0 +1,18 @@
+package step2.exception;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BlankExceptionTest {
+
+
+    @Test
+    @DisplayName("문자열이 비어있을때 예외가 발생한다.")
+    void 문자열이_비어있을_때_예외가_발생한다()
+    {
+
+    }
+}
