9e62407758ff817584b4aa8206b62d708e9cdb3b
oen142 <oen142@naver.com>
2020-10-29 19:53:35 +0900

    feat : 계산기 클래스 작성

---
+++ src/main/java/step2/domain/Calculator.java
@@ -0,0 +1,34 @@
+package step2.domain;
+
+import org.apache.commons.lang3.StringUtils;
+import step2.exception.BlankException;
+
+import static java.lang.Integer.parseInt;
+
+public class Calculator {
+
+    public int calculate(String input) {
+        validNull(input);
+        String[] splits = splitBlank(input);
+        int loopSize = splits.length;
+        int operateResult = parseInt(splits[0]);
+        for (int i = 1; i < loopSize; i+=2) {
+            String operator = splits[i];
+            int nextNumber = parseInt(splits[i+1]);
+            operateResult = Operator.operate(operateResult , nextNumber , operator);
+        }
+        return operateResult;
+    }
+
+
+    private String[] splitBlank(String input) {
+        return input.split(" ");
+    }
+
+    private static void validNull(String input) {
+        if (StringUtils.isBlank(input)) {
+            throw new BlankException();
+        }
+    }
+
+}
