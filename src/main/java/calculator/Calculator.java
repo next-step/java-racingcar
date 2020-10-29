84db8eefaa55144b8690018cab9dc5dd768b7279
고정완 (Can) <jw.gho@ecubelabs.com>
2020-10-29 16:53:06 +0900

    [refactor]: Split class; Validator.class from Calcuator.class

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -12,6 +12,10 @@ public class Calculator {
         this.operator = operator;
     }
 
+    private String[] parseInput(String input) {
+        return input.split(Const.SPLIT_REGEX);
+    }
+
     int calculate(int operand1, int operand2, String operatorStr) {
         if (operatorStr.equals(Const.ADDITION)) {
             return operator.add(operand1, operand2);
@@ -28,35 +32,11 @@ public class Calculator {
         throw new IllegalArgumentException(Const.BAD_OPERATOR_ERROR_MSG);
     }
 
-    private void validateInput(String input) {
-        if (input == null) {
-            throw new IllegalArgumentException(Const.NULL_ERROR_MSG);
-        }
-        if (input.length() < 1) {
-            throw new IllegalArgumentException(Const.EMPTY_ERROR_MSG);
-        }
-    }
-
-    private void validateArray(String[] array) {
-        // 연산 가능한 최소한의 length 는 3이다. (e.g: 1 + 2)
-        if (array.length < 3) {
-            throw new IllegalArgumentException(Const.LESS_THAN_3_ERROR_MSG);
-        }
-
-        // 마지막이 숫자로 끝나려면, array 의 length 가 홀수여야 한다.
-        if (array.length % 2 == 0) {
-            throw new IllegalArgumentException(Const.ODD_ERROR_MSG);
-        }
-    }
-
-    private String[] parseInput(String input) {
-        return input.split(Const.SPLIT_REGEX);
-    }
-
     public int calculate(String input) {
-        validateInput(input);
+        Validator validator = Validator.getInstance();
+        validator.validateInput(input);
         String[] array = parseInput(input);
-        validateArray(array);
+        validator.validateArray(array);
 
         int accumulatedVal = Integer.parseInt(array[0]);
         for (int i = 1; i < array.length; i++) {
