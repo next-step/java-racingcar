048403d5d353444b24a1b08e6ce83ff77c383f96
siyoon <siyoon210@gmail.com>
2020-10-29 09:14:29 +0900

    refactor: 문자열을 산술 표현식으로 바꾸고 계산하는 클래스 추출

---
+++ src/main/java/stringcalculator/ArithmeticExpression.java
@@ -0,0 +1,70 @@
+package stringcalculator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class ArithmeticExpression {
+    private static final String DELIMITER = " ";
+
+    private final List<Integer> numbers;
+    private final List<Operator> operators;
+
+    ArithmeticExpression(String input) {
+        numbers = new ArrayList<>();
+        operators = new ArrayList<>();
+
+        String[] inputSplits = input.split(DELIMITER);
+
+        for (int i = 0; i < inputSplits.length; i++) {
+            addArithmeticNode(inputSplits[i], i);
+        }
+    }
+
+    void addArithmeticNode(String inputSplit, int index) {
+        if (isNumberIndex(index)) {
+            numbers.add(Integer.valueOf(inputSplit));
+            return;
+        }
+
+        if (isOperatorIndex(index)) {
+            operators.add(Operator.findBy(inputSplit));
+            return;
+        }
+
+        throw new IllegalArgumentException("Invalid index. " + index);
+    }
+
+    private boolean isNumberIndex(int i) {
+        return i % 2 == 0;
+    }
+
+    private boolean isOperatorIndex(int i) {
+        return i % 2 != 0;
+    }
+
+    Integer evaluate() {
+        validateArithmeticNodes();
+
+        Iterator<Integer> numbersIterator = numbers.iterator();
+        Iterator<Operator> operatorIterator = operators.iterator();
+
+        Integer result = numbersIterator.next();
+        while (numbersIterator.hasNext() && operatorIterator.hasNext()) {
+            Integer number = numbersIterator.next();
+            Operator operator = operatorIterator.next();
+            result = operator.calculate(result, number);
+        }
+        return result;
+    }
+
+    private void validateArithmeticNodes() {
+        if (numbers.isEmpty()) {
+            throw new IllegalStateException("There are no numbers in the expression.");
+        }
+
+        if (numbers.size() != operators.size()  + 1) {
+            throw new IllegalStateException("Number must be one more than operator.");
+        }
+    }
+}
--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,55 +1,15 @@
 package stringcalculator;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class StringCalculator {
-    private static final String DELIMITER = " ";
-
     public Integer calculate(String input) {
         validateInput(input);
-
-        List<Integer> numbers = new ArrayList<>();
-        List<Operator> operators = new ArrayList<>();
-        initArithmeticLists(input, numbers, operators);
-
-        Iterator<Integer> numberIterator = numbers.iterator();
-        Iterator<Operator> operatorIterator = operators.iterator();
-        Integer result = numberIterator.next();
-        while (numberIterator.hasNext() && operatorIterator.hasNext()) {
-            Integer nextNumber = numberIterator.next();
-            Operator nextOperator = operatorIterator.next();
-            result = nextOperator.calculate(result, nextNumber);
-        }
-        return result;
+        ArithmeticExpression arithmeticExpression = new ArithmeticExpression(input);
+        return arithmeticExpression.evaluate();
     }
 
     private void validateInput(String input) {
         if (input == null || input.equals("")) {
             throw new IllegalArgumentException("Input can not be null or empty.");
         }
-    }
-
-    private void initArithmeticLists(String input, List<Integer> numbers, List<Operator> operators) {
-        String[] inputSplits = input.split(DELIMITER);
-        for (int i = 0; i < inputSplits.length; i++) {
-            if (isNumberIndex(i)) {
-                numbers.add(Integer.valueOf(inputSplits[i]));
-                continue;
-            }
-
-            if (isOperatorIndex(i)) {
-                operators.add(Operator.findBy(inputSplits[i]));
-            }
-        }
-    }
-
-    private boolean isNumberIndex(int i) {
-        return i % 2 == 0;
-    }
-
-    private boolean isOperatorIndex(int i) {
-        return i % 2 != 0;
     }
 }
