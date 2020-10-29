09725a5e851b4363667740cc1f748dd6d702ebba
Byeon-SungMun <201401339hs@gmail.com>
2020-10-29 21:23:26 +0900

    Set Set Collection 요구사항1까지 했습니다 요구사항2 외부라이브러리 안사용하고 해볼라는데 감이 안잡힙니다 어떻게 해할까요 그리고jupiter-params 의존성 주입했는데도 Strings가 안됩니다 ㅠㅠ

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,36 @@
+package study;
+
+
+import org.assertj.core.util.Strings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp () {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    public void size () {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,12 +3,26 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
     @Test
-    void split(){
+    public void split(){
         String[] result = "1,2".split(",");
-        assertThat(result).contains("1");
+        assertThat(result).contains("1").contains("2");
+        assertThat(result).containsExactly("1","2");
+    }
+    @Test
+    public void subString(){
+        String result = "(1,2)".substring(1,5);
+        assertThat(result).contains("1,2");
+    }
+    @Test
+    public void chatAt(){
+        String result = "abc";
+        assertThatThrownBy(() -> {
+            result.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
     }
 }
