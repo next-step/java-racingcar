a7d07eb2f459aa947d4827176aa5c85130996712
oen142 <oen142@naver.com>
2020-10-29 19:59:18 +0900

    test : Calculator 테스트 완성

--- src/test/java/step2/domain/CalculatorTest.java
+++ src/test/java/step2/domain/CalculatorTest.java
@@ -1,10 +1,14 @@
 package step2.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import step2.exception.BlankException;
+import step2.exception.NotOperatorException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorTest {
 
@@ -15,28 +19,28 @@ class CalculatorTest {
     @CsvSource(value = {"2 + 3 + 4:" + (2 + 3 + 4), "5 + 6 + 1:" + (5 + 6 + 1)}, delimiter = ':')
     @DisplayName("덧셈을 테스트 한다.")
     void plus(String input, int expected) {
-        Assertions.assertThat(calculator.calculate(input)).isEqualTo(expected);
+        assertThat(calculator.calculate(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"2 - 3 - 4:" + (2 - 3 - 4), "5 - 6 - 1:" + (5 - 6 - 1)}, delimiter = ':')
     @DisplayName("뺄셈을 테스트 한다.")
-    void minus(String input , int expected) {
-        Assertions.assertThat(calculator.calculate(input)).isEqualTo(expected);
+    void minus(String input, int expected) {
+        assertThat(calculator.calculate(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"2 * 3 * 4:" + (2 * 3 * 4), "5 * 6 * 1:" + (5 * 6 * 1)}, delimiter = ':')
     @DisplayName("곱셈을 테스트 한다.")
     void multiply(String input, int expected) {
-        Assertions.assertThat(calculator.calculate(input)).isEqualTo(expected);
+        assertThat(calculator.calculate(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"2 / 3 / 4:" + (2 / 3 / 4), "5 / 6 / 1:" + (5 / 6 / 1)}, delimiter = ':')
     @DisplayName("나눗셈을 테스트 한다.")
     void division(String input, int expected) {
-        Assertions.assertThat(calculator.calculate(input)).isEqualTo(expected);
+        assertThat(calculator.calculate(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
@@ -44,25 +48,20 @@ class CalculatorTest {
     @DisplayName("혼합된 연산자를 테스트 한다.")
     void mixedOperator(String input, int expected) {
 
-        System.out.println("expected = " + expected);
-        Assertions.assertThat(calculator.calculate(input)).isEqualTo(expected);
+        assertThat(calculator.calculate(input)).isEqualTo(expected);
     }
 
-    @Test
-    @DisplayName("입력값이 null일경우 IllegalArgumentException throw를 반환한다.")
-    void isNullThrowIllegalArgumentException() {
-
-    }
 
     @Test
     @DisplayName("입력값이 빈 공백 문자열일 경우 IllegalArgumentException throw를 반환한다.")
-    void isEmptyStringThrowIllegalArgumentException() {
-
+    void isBlankStringThrowIllegalArgumentException() {
+        assertThatThrownBy( () -> calculator.calculate(" ")).isInstanceOf(BlankException.class);
     }
 
     @Test
     @DisplayName("입력값이 사칙연산이 아닐 경우 IllegalArgumentException throw를 반환한다.")
     void isNotInputValueOperatorThrowIllegalArgumentException() {
 
+        assertThatThrownBy( () -> calculator.calculate("1 : 2")).isInstanceOf(NotOperatorException.class);
     }
 }
