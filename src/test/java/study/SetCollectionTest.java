4c81f05659bb6fd902619475581429bc49c22fb9
mimimjin <69135527+mimimjin@users.noreply.github.com>
2020-10-30 20:50:34 +0900

    자동차 경주 step1 (#1339)
    * string split test
    
    * Set Collection Test Study
    
    * StringTest
    
    * Exception 처리 test 수정

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,51 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+    }
+    @Test
+    void getSetSize(){
+        assertThat(numbers.size()).isNotZero();
+    }
+
+    @Test
+    void contains(){
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contains2(int input){
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    void contains3(int input, boolean isTrue){
+        boolean isContains = numbers.contains(input);
+        assertEquals(isTrue, isContains);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,32 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+    @Test
+    void split(){
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1","2");
+    }
+    @Test
+    void split2(){
+        String[] result2 = "1".split(",");
+        assertThat(result2).containsExactly("1");
+    }
+    @Test
+    void substring(){
+        String result3 = "(1,2)";
+        result3 = result3.substring(result3.indexOf("(")+1, result3.indexOf(")"));
+        assertThat(result3).isEqualTo("1,2");
+    }
+    @DisplayName("String charAt Test")
+    @Test
+    void chatAt(){
+        assertThatThrownBy(() ->{
+            char result4 = "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class).hasMessageContaining("String index out of range: 3");
+    }
+}
