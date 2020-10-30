c4a572b8dc98423a6bfc22f47992f5e427785609
naru <hsik0225@gmail.com>
2020-10-30 17:10:03 +0900

    feat : GameMachine 클래스 구현

---
+++ src/main/java/racing/GameMachine.java
@@ -0,0 +1,9 @@
+package racing;
+
+public class GameMachine {
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        racingGame.setGame();
+        racingGame.startGame();
+    }
+}
