6c1473486603d2ecbd1b21aa66629a631941dce0
eyabc <bey4314@naver.com>
2020-10-30 19:27:13 +0900

    [자동차 경주-단위테스트] 2단계 - 문자열 계산기 / Operator 리팩토링

--- src/main/java/step02/Calculator.java
+++ src/main/java/step02/Calculator.java
@@ -1,20 +1,8 @@
 package step02;
 
 import exception.EmptyException;
-import exception.InValidOperatorException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Calculator {
-
-    private static final Map<String, Operation> OPERATIONS = new HashMap<>();
-
-    static {
-        for (Operator operator : Operator.values()) {
-            OPERATIONS.put(operator.getOperatorType(), operator);
-        }
-    }
 
     private boolean isNullOrEmpty(String input) {
         return input == null || input.length() == 0;
@@ -30,31 +18,17 @@ public class Calculator {
         }
     }
 
-    private void validateOperatorType(String operatorType) {
-        if (!OPERATIONS.containsKey(operatorType))
-            throw new InValidOperatorException();
-    }
-
-    public int calculate(final String input) {
-
+    public  int calculate(final String input) {
         validateInputHasContents(input);
-
         String[] rowData = input.split(" ");
-
         int calculateResult = Integer.parseInt(rowData[0]);
-
         for (int i = 1; i < rowData.length - 1; i += 2) {
-            String operatorType = rowData[i];
-
-            validateOperatorType(operatorType);
-
-            int rightOperand = Integer.parseInt(rowData[i + 1]);
-            int leftOperand = calculateResult;
-
-            Operation operator = OPERATIONS.get(operatorType);
-            calculateResult = operator.calculate(leftOperand, rightOperand);
+            calculateResult = Operator.executeOf(
+                    rowData[i],
+                    Integer.parseInt(rowData[i + 1]),
+                    calculateResult
+            );
         }
-
         return calculateResult;
     }
 }
--- src/main/java/step02/Operator.java
+++ src/main/java/step02/Operator.java
@@ -1,49 +1,35 @@
 package step02;
 
-import exception.DividedByZero;
+import exception.InValidOperatorException;
 
-public enum Operator implements Operation {
+import java.util.Arrays;
+import java.util.function.BiFunction;
 
-    PLUS("+") {
-        @Override
-        public int calculate(int first, int second) {
-            return first + second;
-        }
-    },
+public enum Operator {
 
-    MINUS("-") {
-        @Override
-        public int calculate(int first, int second) {
-            return first - second;
-        }
-    },
-
-    DIVIDE("/") {
-        @Override
-        public int calculate(int first, int second) {
-            if (second == 0) {
-                throw new DividedByZero();
-            }
-            return first / second;
-        }
-    },
-
-    MULTIPLE("*") {
-        @Override
-        public int calculate(int first, int second) {
-            return first * second;
-        }
-    };
-
+    PLUS("+", Math::addExact),
+    MINUS("-", Math::subtractExact),
+    MULTIPLE("*", Math::multiplyExact),
+    DIVIDE("/", (x, y) -> x / y);
 
     private final String operatorType;
+    private final BiFunction<Integer, Integer, Integer> operator;
 
-    Operator(String operatorType) {
+    Operator(String operatorType, BiFunction<Integer, Integer, Integer> operator) {
         this.operatorType = operatorType;
+        this.operator = operator;
     }
 
-    public String getOperatorType() {
-        return operatorType;
+    public int calculate(int x, int y) {
+        return operator.apply(x, y);
+    }
+
+    public static int executeOf(String type, int x, int y) {
+        return Arrays.stream(values())
+                     .filter(operator -> operator.operatorType.equals(type))
+                     .findFirst()
+                     .orElseThrow(InValidOperatorException::new)
+                     .calculate(x, y);
     }
 
 }
