a8b01461ea2ce6e452e90b180061290016a7e4c5
kim-daeyong <kdy2353@gmail.com>
2020-10-30 23:09:54 +0900

    split 테스트

--- src/main/java/JavaRacingCar.java
+++
@@ -1,5 +0,0 @@
-public class JavaRacingCar {
-    public static void main(String[] args) {
-        
-    }
-}
--- src/test/java/stringtest/StringTest.java
+++ src/test/java/stringtest/StringTest.java
@@ -1,13 +1,16 @@
 package stringtest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     
     @Test
-    void splitTest(){
+    void requirementTest1(){
         String[] strArray = "1,2".split(",");
         assertThat(strArray).contains("1", "2");
         assertThat(strArray).contains("1", strArray[0]);
@@ -15,6 +18,26 @@ public class StringTest {
 
         String[] strArray2 = strArray[0].split(",");
         assertThat(strArray2).contains("1", strArray2[0]);
-        
+    }
+
+    @Test
+    void requirementTest2(){
+        String str = "(1,2)";
+        String subStr = str.substring(1,4);
+        assertEquals("1,2", subStr);
+    }
+
+    @Test
+    @DisplayName("요구사항 3을 충족한다.")
+    void requirementTest3(){
+        String str = "a,b,c";
+        assertThat(str.charAt(0)).isEqualTo("a");
+        assertThat(str.charAt(2)).isEqualTo("c");
+
+        assertThatThrownBy(()->{
+            assertThat(str.charAt(3)).isEqualTo("c");
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+        .hasMessageContaining("Index: 3, Size 2, index out of range");
+
     }
 }
