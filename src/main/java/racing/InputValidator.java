f744e9579ebedf62f38a07133ef0f0b75b0051c5
naru <hsik0225@gmail.com>
2020-10-30 17:35:50 +0900

    feat : Input 값 validation 기능 구현

---
+++ src/main/java/racing/InputValidator.java
@@ -0,0 +1,22 @@
+package racing;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidator {
+
+    // 1 이상 ~ 20 미만의 숫자만 입력할 수 있습니다
+    public static final Pattern PATTERN = Pattern.compile("^[1-9]|1\\d$");
+
+    public void validate(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException(NOT_NULL);
+        }
+
+        Matcher matcher = PATTERN.matcher(input);
+        
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(INVALID_VALUE);
+        }
+    }
+}
