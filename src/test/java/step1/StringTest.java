19bb7fe0c29dc6d706252890d40b779e7a84e1cb
Byeon-SungMun <201401339hs@gmail.com>
2020-10-30 21:14:17 +0900

    test

--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -1,9 +1,7 @@
-package study;
+package step1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
