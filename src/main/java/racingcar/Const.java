815a31721b957e62c89a7db8fe0fc29881689934
고정완 (Can) <jw.gho@ecubelabs.com>
2020-10-30 16:12:10 +0900

    [feat]: Add Const.java

---
+++ src/main/java/racingcar/Const.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class Const {
+    private Const() {};
+
+    public static String HOW_MANY_CARS_STR = "자동차 대수는 몇 대 인가요?";
+    public static String HOW_MANY_TRIES_STR = "시도할 회수는 몇 회 인가요?";
+    public static String EXECUTION_RESULT_STR = "실행 결과";
+}
