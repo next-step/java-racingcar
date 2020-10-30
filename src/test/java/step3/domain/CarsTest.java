7031ed2006ae036fdb589f906a4f8c0023188498
oen142 <oen142@naver.com>
2020-10-30 21:29:19 +0900

    test : 차 List가 주어진 값에 잘 움직이는지 확인을 합니다.

--- src/test/java/step3/domain/CarsTest.java
+++ src/test/java/step3/domain/CarsTest.java
@@ -1,21 +1,37 @@
 package step3.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarsTest {
 
-    @Test
-    @DisplayName("Cars에 있는 List를 외부에서 건들수 없는지 확인을 한다.")
-    void touchNotCars(){
+    private Cars cars;
 
+    @BeforeEach
+    void setUp() {
+        List<Car> carList = Arrays.asList(new Car(), new Car());
+        cars = new Cars(carList);
     }
+
     @Test
     @DisplayName("Cars에 있는 Car들이 잘 움직이는지 확인한다.")
-    void moveCars(){
-
+    void moveCars() {
+        //given
+        MoveStrategy moveStrategy = new MustMoveStrategy();
+        //when
+        cars.moveCars(moveStrategy);
+        List<Integer> carsPositions = cars.getCarsPosition();
+        //then
+        carsPositions.forEach(carsPosition -> assertThat(carsPosition).isEqualTo(1));
     }
 
 }
