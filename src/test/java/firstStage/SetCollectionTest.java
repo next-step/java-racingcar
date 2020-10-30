cb8c0f305eca5ad0b056b191e233d43807e5691e
monkeyDugi <53487385+monkeyDugi@users.noreply.github.com>
2020-10-31 01:50:49 +0900

    자동차 경주 : 2단계 문자열계산기 (#1345)
    * docs : README.md 수정
    
    * (feat) 1단계: 문자열 계산기

--- src/test/java/firstStage/SetCollectionTest.java
+++ src/test/java/firstStage/SetCollectionTest.java
@@ -1,4 +1,4 @@
-package study;
+package firstStage;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
--- src/test/java/firstStage/StringTest.java
+++ src/test/java/firstStage/StringTest.java
@@ -1,4 +1,4 @@
-package study;
+package firstStage;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
