6f4f55635d43dbfdded85b875a1d0e7f5ee6eae8
naru <hsik0225@gmail.com>
2020-10-30 17:37:05 +0900

    style : Calculator 때 사용한 ErrorMessage 를 재사용하기 위하여 common 패키지로 이동

---
+++ src/main/java/common/ErrorMessage.java
@@ -0,0 +1,10 @@
+package common;
+
+public final class ErrorMessage {
+
+    public static final String INVALID_VALUE = "invalid value";
+
+    public static final String NOT_NULL = "input must not be null";
+
+    private ErrorMessage() {}
+}
