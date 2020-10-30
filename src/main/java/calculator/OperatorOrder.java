4c1dfe5f163fa4bb278abafacc49a1c426aed6bb
rlawls1991 <rlawls1991@gmail.com>
2020-10-30 14:38:05 +0900

    (feat) : create operator object

---
+++ src/main/java/calculator/OperatorOrder.java
@@ -0,0 +1,27 @@
+package calculator;
+
+
+/**
+ * 문자열 중에서 연산자가 존재하는 index와 해당되는 문자열을
+ */
+public class OperatorOrder {
+
+    char operatorText; // 연산자
+    int index;  // 연산자가 존재하는 inex
+
+    public char getOperatorText() {
+        return operatorText;
+    }
+
+    public void setOperatorText(char operatorText) {
+        this.operatorText = operatorText;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
