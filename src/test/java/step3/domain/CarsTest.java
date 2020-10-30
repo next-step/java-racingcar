14c4594fada2b19751157598ce596578cf0fb0b0
oen142 <oen142@naver.com>
2020-10-30 21:42:26 +0900

    test : 주어진 자동차의 갯수가 1개 이하일경우 OutBoundCarListSizeException 발생

--- src/test/java/step3/domain/CarsTest.java
+++ src/test/java/step3/domain/CarsTest.java
@@ -1,34 +1,43 @@
 package step3.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step3.exception.OutBoundCarListSizeException;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
 
-    private Cars cars;
-
-    @BeforeEach
-    void setUp() {
-        List<Car> carList = Arrays.asList(new Car(), new Car());
-        cars = new Cars(carList);
-    }
 
     @Test
     @DisplayName("Cars에 있는 Car들이 잘 움직이는지 확인한다.")
     void moveCars() {
         //given
+        List<Car> carList = Arrays.asList(new Car(), new Car());
+        Cars cars = new Cars(carList);
         MoveStrategy moveStrategy = new MustMoveStrategy();
         //when
         cars.moveCars(moveStrategy);
         List<Integer> carsPositions = cars.getCarsPosition();
         //then
         carsPositions.forEach(carsPosition -> assertThat(carsPosition).isEqualTo(1));
+    }
+
+    @Test
+    @DisplayName("자동차 갯수가 1개 미만일때 익셉션 확인")
+    void throwExceptionMinCarCount() {
+        //given
+        List<Car> carList = Collections.emptyList();
+        Cars cars;
+        //then
+        assertThatThrownBy(()-> new Cars(carList)).isInstanceOf(OutBoundCarListSizeException.class);
     }
 
 }
