0f5a3109ea70839d6dae61dfc13867f0e33ff68b
PeonyF <1621139@gmail.com>
2020-11-01 02:50:14 +0900

    문자열 계산기 테스트에 따른 문자열 계산기 수정(2)

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,38 +1,51 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.function.BiFunction;
 
 public enum Operator {
-    PLUS("+", (num1, num2) -> num1 + num2),
-    MINUS("-", (num1, num2) -> num1 - num2),
-    MULTIPLY("*", (num1, num2) -> num1 * num2),
+
+    ADDITION("+", Integer::sum),
+    SUBTRACTION("-", (val1, val2) -> val1 - val2),
+    MULTIPLY("*", (val1, val2) -> val1 * val2),
     DIVIDE("/", (num1, num2) -> {
-        checkParameterIsZero(num1,num2);
+        checkParameterIsZero(num1, num2);
         return num1 / num2;
     });
 
+    private static final int ZERO = 0;
+    private static final String CANT_DIVIDE_ZERO = "0으로 나눌 수 없습니다.";
+    private static final String EMPTY_EXPRESSION = "계산 문자열이 잘못 되었습니다.";
+    private static final String WRONG_OPERATOR = "연산자가 잘못 되었습니다.";
+
     private static void checkParameterIsZero(int num1, int num2) {
-        if (num1 == 0 || num2 == 0) {
-            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        if (num1 == ZERO || num2 == ZERO) {
+            throw new ArithmeticException(CANT_DIVIDE_ZERO);
         }
     }
 
-    private String operator;
-    private BiFunction<Integer,Integer,Integer> expression;
+    private final String operator;
+    private final BiFunction<Integer, Integer, Integer> expression;
 
-    Operator(String operator, BiFunction<Integer,Integer,Integer> expression){
+    Operator(final String operator, final BiFunction<Integer, Integer, Integer> expression) {
         this.operator = operator;
         this.expression = expression;
     }
 
-    public static int calculate(int num1, String operator, int num2){
-        return getOperator(operator).expression.apply(num1,num2);
+    public int calculate(final int num1, final int num2) {
+        return expression.apply(num1, num2);
     }
 
-    private static Operator getOperator(String operator){
-        return Arrays.stream(values())
-                .filter(o -> o.operator.equals(operator))
-                .findFirst().orElseThrow(() -> new IllegalArgumentException("올바른 연산자가 아닙니다."));
+    public static Operator getOperator(final String type) {
+        if (Objects.isNull(type) || type.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_EXPRESSION);
+        }
+
+        return Arrays.stream(Operator.values())
+                .filter(it -> type.contains(it.operator))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(WRONG_OPERATOR));
     }
 }
+
--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -1,4 +1,46 @@
 package step2;
 
+import calculator.StringCalculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.*;
+
 public class StringCalculatorTest {
+    StringCalculator stringCalculator = new StringCalculator();
+
+    @DisplayName("Testing for the four arithmetical operations")
+    @ParameterizedTest
+    @CsvSource(value  ={"2 + 3 * 4 / 2 : 10"},delimiter = ':')
+    void calculator(final String str, final int result){
+        assertThat(stringCalculator.calculate(str)).isEqualTo(result);
+    }
+
+    @DisplayName("Testing for value is null or blank")
+    @ParameterizedTest
+    @ValueSource(strings = {""," ","null"})
+    void shouldExceptionForNullOrEmptyStrings(String str){
+        assertThatIllegalArgumentException()
+                .isThrownBy(()->stringCalculator.calculate(str));
+    }
+
+    @DisplayName("Testing for if it isn't operator")
+    @ParameterizedTest
+    @ValueSource(strings = {"2 & 3 $ 4"})
+    void shouldExceptionByWrongsStrings(final String str){
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> stringCalculator.calculate(str));
+    }
+
+    @DisplayName("Testing for dividing zero test")
+    @ParameterizedTest
+    @ValueSource(strings = "5 + 3 * 4 / 0")
+    void shouldExceptionDivideByZero(final String str){
+        assertThatExceptionOfType(ArithmeticException.class)
+                .isThrownBy(()->stringCalculator.calculate(str))
+                .isInstanceOf(ArithmeticException.class)
+                .withMessage("0으로 나눌 수 없습니다.");
+
+    }
 }
