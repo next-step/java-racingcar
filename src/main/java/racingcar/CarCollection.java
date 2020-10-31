086fda0468d79fc07420f685711b506c9aa65296
고정완 (Can) <jw.gho@ecubelabs.com>
2020-11-01 02:54:08 +0900

    [style]: Fix typo

--- src/main/java/racingcar/CarCollection.java
+++ src/main/java/racingcar/CarCollection.java
@@ -9,7 +9,7 @@ public class CarCollection {
      * NOTE: 기본 배열을 사용한 이유
      * ArrayList, LinkedList, HashMap, Set 등등 자바의 자료구조를 공부했으나,
      * 현재 요구사항에서는 자동차의 추가, 수정, 삭제 및 검색 기능이 필요치 않으므로,
-     * 성능 향상을 위해 기본 배열만으로 충분하고 판단했다.
+     * 성능 향상을 위해 기본 배열만으로 충분하다고 판단했다.
      */
     private Car[] carArr;
 
