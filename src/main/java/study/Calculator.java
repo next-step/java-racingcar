fcb5f86737b584cff92cae41163c62eb3f120bb7
최종원 <lastone@choejong-won-ui-MacBookPro.local>
2020-11-01 01:11:17 +0900

    feat : Calculator.java

---
+++ src/main/java/study/Calculator.java
@@ -0,0 +1,120 @@
+package study;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    private String input;
+    private ArrayList<Double> numberList = new ArrayList<>();
+    private ArrayList<String> operatorList = new ArrayList<>();
+
+    public Calculator() {
+
+    }
+
+    public Calculator(String input) {
+        this.input = input;
+
+        // 1. 공백체크
+        checkIsBlank();
+        // 2. 숫자, 연산자 세팅
+        setNumberAndOperatorList();
+        // 3. 잘못된 연산자 체크
+        checkOperator();
+    }
+
+    double sum(double a, double b) {
+        return a + b;
+    }
+
+    double subtract(double a, double b) {
+        return a - b;
+    }
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        if (b <= 0) {
+            throw new ArithmeticException("ArithmeticException");
+        }
+        return a / b;
+    }
+
+    // 공백제거
+    String removeWhitespace(String string) {
+        return string.replaceAll(" ", "");
+    }
+
+    // null, 빈 공백 체크
+    // 입력이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw
+    void checkIsBlank() {
+        if (input == null || removeWhitespace(input).isEmpty()) {
+            throw new IllegalArgumentException("IllegalArgumentException");
+        }
+    }
+
+    String getOperate() {
+        String operator = operatorList.get(0);
+        operatorList.remove(0);
+        return operator;
+    }
+
+    // 연산
+    double operate(double a, double b) {
+        double result = 0;
+
+        String operator = getOperate();
+
+        switch(operator) {
+            case "+":
+                result = sum(a, b);
+                break;
+            case "-":
+                result = subtract(a, b);
+                break;
+            case "*":
+                result = multiply(a, b);
+                break;
+            case "/":
+                result = divide(a, b);
+                break;
+        }
+        return result;
+    }
+
+    // 숫자, 문자 구분
+    void setNumberAndOperatorList() {
+        String[] arrInputSplit = input.split(" ");
+        for (String inputSplit : arrInputSplit) {
+            try {
+                Double parseDouble = Double.parseDouble(inputSplit);
+                numberList.add(parseDouble);
+            } catch (NumberFormatException e) {
+                operatorList.add(inputSplit);
+            }
+        }
+    }
+
+    // 기호체크
+    void checkOperator() {
+        // 기호가 하나이상 입력되어있거나, 사칙연산이 아닌경우
+        for (String operator : operatorList) {
+            if (operator.toCharArray().length > 1
+                    || !"+-*/".contains(operator)) {
+                throw new IllegalArgumentException("IllegalArgumentException");
+            }
+        }
+    }
+
+    Double getFirstNumber() {
+        double firstNumber = numberList.get(0);
+        numberList.remove(0);
+        return firstNumber;
+    }
+    // 계산
+    double calculate() {
+        double firstNumber = getFirstNumber();
+        return numberList.stream().reduce(firstNumber, (a, b) -> operate(a, b));
+    }
+}
---
+++ src/test/java/study/CalculatorTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import java.util.Scanner;
+
+public class Calculator {
+    int sum() {
+        return 0;
+    }
+
+    int sub() {
+        return 0;
+    }
+
+    int gob() {
+        return 0;
+    }
+
+    int nanu() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String testInput = sc.nextLine();
+        System.out.println(testInput);
+
+        testInput
+    }
+}
