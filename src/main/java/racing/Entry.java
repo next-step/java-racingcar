e19f4f3fd69a2d4b4f87c8835b227652b6aecaf7
naru <hsik0225@gmail.com>
2020-10-31 18:29:06 +0900

    feat : 경기에 출전하는 차들의 정보를 담은 Entry 클래스 생성

---
+++ src/main/java/racing/Entry.java
@@ -0,0 +1,16 @@
+package racing;
+
+public class Entry {
+
+    private Car[] cars;
+
+    private int carCount;
+
+    public void enterCars() {
+        cars =
+    }
+
+    public void setCarCount(int carCount) {
+        this.carCount = carCount;
+    }
+}
