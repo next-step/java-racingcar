7bd080c569b5e5ecf38c2733d0af3fbd7e06423e
최종원 <lastone@choejong-won-ui-MacBookPro.local>
2020-11-01 01:12:05 +0900

    test : CalculatorTest

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,29 +1,67 @@
 package study;
 
-import java.util.Scanner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class Calculator {
-    int sum() {
-        return 0;
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    @DisplayName("sum 테스트")
+    void sumTest() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.sum(1, 2)).isEqualTo(3);
     }
 
-    int sub() {
-        return 0;
+    @Test
+    @DisplayName("subtract 테스트")
+    void subtractTest() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.subtract(2, 1)).isEqualTo(1);
     }
 
-    int gob() {
-        return 0;
+    @Test
+    @DisplayName("divide 테스트")
+    void divideTest() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.divide(4, 2)).isEqualTo(2.0);
     }
 
-    int nanu() {
-        return 0;
+    @Test
+    @DisplayName("divide 0 나누기 테스트")
+    void failDivideTest() {
+        Calculator calculator = new Calculator();
+        assertThatExceptionOfType(ArithmeticException.class).isThrownBy(() -> {
+            calculator.divide(4, 0);
+        });
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String testInput = sc.nextLine();
-        System.out.println(testInput);
+    @Test
+    @DisplayName("multiply 테스트")
+    void multiplyTest() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.multiply(2, 2)).isEqualTo(4.0);
+    }
 
-        testInput
+    @DisplayName("성공 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"2 + 3 * 4 / 2:10.0", "2 + 3 * 4 - 2:18.0"}, delimiter = ':')
+    void successCalculateTest(String input, Double result) {
+        Calculator calculator = new Calculator(input);
+
+        assertThat(calculator.calculate()).isEqualTo(result);
+    }
+
+    @DisplayName("예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "2 + 3 a 1"})
+    void failCalculateTest(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Calculator calculator = new Calculator(input);
+            calculator.calculate();
+        });
     }
 }
