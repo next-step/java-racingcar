cdfe5ff051944ffe5b71b3db22a5fbdc28740046
naru <hsik0225@gmail.com>
2020-10-31 18:17:56 +0900

    style : set에서 read로 메소드 이름 변경

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
         this.inputValidator = new InputValidator();
     }
 
-    public int setCarCount() {
+    public int readCarCount() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("자동차 대수는 몇 대 인가요?");
@@ -23,7 +23,7 @@ public class InputView {
         return Integer.parseInt(carCount);
     }
 
-    public int setRound() {
+    public int readRound() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("시도할 회수는 몇 회 인가요?");
