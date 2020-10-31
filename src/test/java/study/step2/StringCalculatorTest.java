f32cfc6fbc26ec5df0cc38bcef9a67a4146e876c
Jung-Soo Ahn <staringstar@gmail.com>
2020-10-31 22:30:02 +0900

    step2 - 테스트 코드 assertThatIllegalArgumentException 으로 변경

--- src/test/java/study/step2/StringCalculatorTest.java
+++ src/test/java/study/step2/StringCalculatorTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.*;
 
 class StringCalculatorTest {
 
@@ -44,7 +43,7 @@ class StringCalculatorTest {
     @DisplayName(" 비어있는 입력 테스트")
     void 비어있는_입력_테스트() {
 
-        assertThatExceptionOfType(IllegalArgumentException.class)
+        assertThatIllegalArgumentException()
                 .isThrownBy(() -> {
                     assertThat(sc.calculateStringInput("")).isEqualTo(9);
                 }).withMessageContaining("입력값이 비어있습니다.");
