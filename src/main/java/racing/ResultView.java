77fa2ec8e3bcd0822cd6cf5d2bb9253020fde366
naru <hsik0225@gmail.com>
2020-10-31 18:55:47 +0900

    feat : 라운드 결과 출력 기능 구현

--- src/main/java/racing/ResultView.java
+++ src/main/java/racing/ResultView.java
@@ -5,4 +5,12 @@ public class ResultView {
     public void viewMessage() {
         System.out.println("실행 결과");
     }
+
+    public void viewRoundResult(Entry entry) {
+        for (Car car : entry.getCars()) {
+            System.out.println(car.getDisplacement());
+        }
+
+        System.out.println();
+    }
 }
