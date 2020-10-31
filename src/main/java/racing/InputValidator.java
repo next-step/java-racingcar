01149f62d33a292ac96cb52b2cdedcf30e40a1eb
naru <hsik0225@gmail.com>
2020-10-31 19:21:01 +0900

    refactor : InputValidator. Util 클래스로 변경

--- src/main/java/racing/InputValidator.java
+++ src/main/java/racing/InputValidator.java
@@ -6,12 +6,14 @@ import java.util.regex.Pattern;
 import static common.ErrorMessage.INVALID_VALUE;
 import static common.ErrorMessage.NOT_NULL;
 
-public class InputValidator {
+public final class InputValidator {
 
     // 1 이상 ~ 20 미만의 숫자만 입력할 수 있습니다
     public static final Pattern PATTERN = Pattern.compile("^[1-9]|1\\d$");
 
-    public void validate(final String input) {
+    private InputValidator() {}
+
+    public static void validate(final String input) {
         if (input == null) {
             throw new IllegalArgumentException(NOT_NULL);
         }
