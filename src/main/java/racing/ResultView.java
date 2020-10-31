830905e34c82df1e102b097c71c9316c29a2f652
naru <hsik0225@gmail.com>
2020-10-31 19:20:11 +0900

    refactor : ResultView 를 Util 클래스로 변경

--- src/main/java/racing/ResultView.java
+++ src/main/java/racing/ResultView.java
@@ -1,12 +1,16 @@
 package racing;
 
-public class ResultView {
+public final class ResultView {
 
-    public void viewMessage() {
-        System.out.println("실행 결과");
+    public static final String RACING_RESULT = "실행 결과";
+
+    private ResultView() {}
+
+    public static void viewMessage() {
+        System.out.println(RACING_RESULT);
     }
 
-    public void viewRoundResult(final RacingCars racingCars) {
+    public static void viewRoundResult(final RacingCars racingCars) {
         for (final Car car : racingCars.getCars()) {
             System.out.println(car.getDisplacement());
         }
