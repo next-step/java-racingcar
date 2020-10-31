24b0387cc920f2b8d0b637d9daa64e2eaf7822c2
이병무 <am003507@gmail.com>
2020-10-31 15:53:07 +0900

    test - 피연산자 유효성검증 단위 테스트

--- src/main/java/step2/Operand.java
+++ src/main/java/step2/Operand.java
@@ -5,18 +5,15 @@ public class Operand implements ExpressionFactor {
     private final int value;
 
     public Operand(int value) {
-        if (value >= 0 || value <= 9) {
-            throw new IllegalArgumentException();
-        }
         this.value = value;
     }
 
-
     public Operand(String value) {
-        if (value == "ss") {
-            throw new IllegalArgumentException();
+        try {
+            this.value = Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("피연산자가 아님", e);
         }
-        this.value = Integer.parseInt(value);
     }
 
     public int getValue() {
---
+++ src/test/java/step2/OperandTest.java
@@ -0,0 +1,21 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class OperandTest {
+
+    @DisplayName("피연산자 유효성 검증 단위테스트")
+    @ParameterizedTest
+    @ValueSource(strings = { "as", "x", "!"})
+    public void test(String factor) {
+        assertThatThrownBy(() -> {
+            new Operand(factor);
+        })
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
