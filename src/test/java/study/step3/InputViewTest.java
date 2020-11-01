18996552f535dc45eec1cfd550764999d843345b
kit(김광수) <kit.t@kakaocorp.com>
2020-11-01 22:29:18 +0900

    feat: InputView 입력결과가 존재한다
    지금까지 구현에서는 RacingInfoProvider 가 사용자 입력을 전달하는 역할을 하였다.
    새로운 요구사항에서 입력값에 대한 파싱/검증 로직이 추가되었다.
    기존 객체는 최소한의 입력전달만 하였으므로 테스트가 불필요 하였으나 새로운 요구사항에서는 테스트 해야할 필요가 생겼다고 판단,
    InputView 에서 그 역할을 담당하도록 하였다.

---
+++ src/test/java/study/step3/InputViewTest.java
@@ -0,0 +1,16 @@
+package study.step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputViewTest {
+    @Test
+    @DisplayName("request() 은 입력 결과를 응답한다.")
+    public void request() {
+        InputView inputView = new InputView();
+        Circuit circuit = inputView.request();
+        assertThat(circuit).isNotNull();
+    }
+}
