a4d1b961f19f53eff2f2e14d2f20638b72bb067f
kit(김광수) <kit.t@kakaocorp.com>
2020-11-01 21:36:37 +0900

    refactor: 누락된 테스트 이름 완성

--- src/test/java/study/step3/RealCarTest.java
+++ src/test/java/study/step3/RealCarTest.java
@@ -2,14 +2,13 @@ package study.step3;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import study.step3.RealCar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RealCarTest {
     @Test
-    @DisplayName("RealCar 객체가 생성되면 ")
-    void instantiation() {
+    @DisplayName("RealCar 객체가 생성되면 임의의 아이디를 갖는다.")
+    void instantiation(){
         RealCar realCar = new RealCar();
         assertThat(realCar.getId()).isNotNull();
     }
