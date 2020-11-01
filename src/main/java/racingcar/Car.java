0812eeaf791255440c007b33edb47a0c1c6dcd40
changgunyee <minyoung403@naver.com>
2020-11-01 12:03:48 +0900

    feat: add toString of Car

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Car {
     private int position;
+    private static final String DISTANCE_UNIT = "-";
 
     public int getPosition() {
         return this.position;
@@ -9,5 +13,12 @@ public class Car {
 
     public void move() {
         this.position += 1;
+    }
+
+    @Override
+    public String toString() {
+        return IntStream.range(0, position)
+                .mapToObj(i -> DISTANCE_UNIT)
+                .collect(Collectors.joining());
     }
 }
