f3e422ddb1cf43a040c6bb567d802e4afd7ae240
eyabc <bey4314@naver.com>
2020-11-02 00:40:37 +0900

    refactor: Validator 의 인스턴스를 생성하지 못하게 변경

--- src/main/java/step03/Validator.java
+++ src/main/java/step03/Validator.java
@@ -3,6 +3,7 @@ package step03;
 import step03.exception.NotPositiveIntegerException;
 
 public class Validator {
+    private Validator() {}
 
     public static void validatePositiveNumber(int number) {
         if (number < 1) throw new NotPositiveIntegerException();
