0f6f1fcaf4f992e0e83c987c63776b7e23b0e764
KyeongMin <kyeongmin.jake.lee@gmail.com>
2020-11-01 20:07:46 +0900

    [Step2] 문자열 계산기 구현 (#1369)
    * Make Calculator methods
    
    * Check Input Validation
    
    * Implement String Calculator
    
    * Refactoring - 연산파트 분리

---
+++ src/test/java/step2/OperatorTest.java
@@ -0,0 +1,48 @@
+package step2;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperatorTest {
+
+    @Test
+    @DisplayName("덧셈 연산 테스트")
+    void checkPlusTest() {
+        assertThat(Operator.calculate(4, "+", 3))
+                .isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("뺄셈 연산 테스트")
+    void checkMinusTest() {
+        assertThat(Operator.calculate(4, "-", 3))
+                .isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("곱셈 연산 테스트")
+    void checkMultipleTest() {
+        assertThat(Operator.calculate(4, "*", 3))
+                .isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("나눗셈 연산 테스트")
+    void checkDivideTest() {
+        assertThat(Operator.calculate(4, "/", 3))
+                .isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("나눗셈 연산시 0으로 나눌 때 에러 테스트")
+    void checkDivideZeroErrorTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> Operator.calculate(4, "/", 0));
+    }
+
+}
