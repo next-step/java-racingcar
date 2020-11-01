423fd659d65256ed5dc8e0af121732afa35e7be1
changgunyee <minyoung403@naver.com>
2020-11-01 12:06:37 +0900

    feat: create move to move cars

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -5,9 +5,25 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class GameManager {
+
+    private final Discriminator discriminator;
+
+    public GameManager(Discriminator discriminator) {
+        this.discriminator = discriminator;
+    }
+
     public List<Car> createCars(int carNum) {
         return IntStream.range(0, carNum)
                 .mapToObj(i -> new Car())
                 .collect(Collectors.toList());
+    }
+
+    public void move(List<Car> cars) {
+        cars.stream().forEach(car -> move(car));
+    }
+
+    private void move(Car c) {
+        if (discriminator.discriminate())
+            c.move();
     }
 }
--- src/test/java/racingcar/GameManagerTest.java
+++ src/test/java/racingcar/GameManagerTest.java
@@ -4,18 +4,49 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameManagerTest {
+
     @Test
     @DisplayName("Car객체 개수만큼 생성")
     void createCars() {
         int carNum = 4;
 
-        GameManager gameManager = new GameManager();
+        Discriminator discriminator = new Discriminator(new Random());
+        GameManager gameManager = new GameManager(discriminator);
         List<Car> cars = gameManager.createCars(carNum);
 
         assertThat(cars).hasSize(carNum);
+    }
+
+    @Test
+    @DisplayName("car를 모두 move하게 하여 테스트")
+    void move_fullMovement() {
+        Discriminator discriminator = new Discriminator(DiscriminatorTest.getMockRandom(4));
+        GameManager gameManager = new GameManager(discriminator);
+        List<Car> cars = gameManager.createCars(3);
+
+        gameManager.move(cars);
+
+        cars.stream().forEach(car ->
+                assertThat(car).hasToString("-")
+        );
+    }
+
+    @Test
+    @DisplayName("car를 모두 move하지 못하게 하여 테스트")
+    void move_noMovement() {
+        Discriminator discriminator = new Discriminator(DiscriminatorTest.getMockRandom(3));
+        GameManager gameManager = new GameManager(discriminator);
+        List<Car> cars = gameManager.createCars(3);
+
+        gameManager.move(cars);
+
+        cars.stream().forEach(car ->
+                assertThat(car).hasToString("")
+        );
     }
 }
