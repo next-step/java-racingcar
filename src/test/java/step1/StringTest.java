10211e6d99c8af0405820307e64c6e576832f2cd
dbeod2 <70149382+dbeod2@users.noreply.github.com>
2020-11-01 20:32:43 +0900

    [2단계 - 문자열 계산기 구현] (#1352)
    * [1단계 - 학습 테스트 실습]
    
    * step2 문자열 계산기 1차 구현
    
    * step2 사칙연산 예외 처리 코드 수정
    
    * 미사용 코드 제거
    
    * step2 피드백 반영
    
    * step2 2번째 피드백 반영
    
    * step2 오류수정
    
    * step2 테스트 코드 수정

---
+++ src/test/java/step1/StringTest.java
@@ -0,0 +1,44 @@
+package step1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+  @Test
+  public void split() {
+    String[] strings = "1,2".split(",");
+    assertThat(strings).containsExactly("1", "2");
+
+    String[] string = "1".split(",");
+    assertThat(string).contains("1");
+  }
+
+  @Test
+  @DisplayName("String 특정 문자 테스트")
+  public void stringSubstring() {
+    String string = "(1,2)";
+    String answer = string.substring(1, string.indexOf(")"));
+    System.out.println(answer);
+  }
+
+  @Test
+  @DisplayName("charAt 메소드를 활용해 특정 위치의 문자를 가져오는 테스트")
+  public void charAt() {
+    String text = "abc";
+    assertThat(text.charAt(0)).isEqualTo('a');
+    assertThat(text.charAt(1)).isEqualTo('b');
+    assertThat(text.charAt(2)).isEqualTo('c');
+  }
+
+  @Test
+  @DisplayName("특정 위치의 문자를 가져올 때 위치 값을 벗어나면 처리하는 예외 테스트")
+  public void stringIndexOutOfBoundsException() {
+    String text = "abc";
+    assertThatExceptionOfType(IndexOutOfBoundsException.class)
+            .isThrownBy(() -> text.charAt(text.length()))
+            .withMessage("String index out of range: %s", text.length());
+  }
+}
