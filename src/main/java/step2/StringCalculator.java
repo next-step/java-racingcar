52976c30b291885c6793d24a60e21fe5b1734634
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-11-01 10:57:22 +0900

    Implement String Calculator

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -5,7 +5,38 @@ public class StringCalculator {
     public int calculate(String data) {
         checkInputValidation(data);
 
-        return 0;
+        String[] splitData = data.split(" ");
+
+        int result = checkNumberValue(splitData[0]);
+        for(int i = 1; i < splitData.length; i+=2) {
+            result = getResult(result, splitData[i], checkNumberValue(splitData[i+1]));
+        }
+
+        return result;
+    }
+
+    private int getResult(int first, String operator, int second) {
+        switch (operator) {
+            case "+":
+                return plus(first, second);
+            case "-":
+                return minus(first, second);
+            case "*":
+                return multiple(first, second);
+            case "/":
+                return divide(first, second);
+            default:
+                throw new IllegalArgumentException("+,-,*,/ 중 연산자를 입력해주세요");
+        }
+    }
+
+
+    private int checkNumberValue(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException ignored) {
+            throw new IllegalArgumentException("연산대상이 숫자가 아닙니다.");
+        }
     }
 
     private void checkInputValidation(String data) {
--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -3,6 +3,8 @@ package step2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -68,5 +70,25 @@ class StringCalculatorTest {
                 () -> calculator.calculate(" "));
     }
 
+    @Test
+    @DisplayName("입력값 중 숫자가 아닌 경우 예외처리 테스트")
+    void checkNumberInputTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> calculator.calculate("d + 5"));
+    }
+
+    @Test
+    @DisplayName("사칙연산 기호가 아닌 경우 예외처리 테스트")
+    void checkOperatorInputTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> calculator.calculate("3 ^ 7"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2 + 3 * 4 / 2"})
+    @DisplayName("사칙연산 테스트")
+    void checkCalculateTest(String data) {
+        assertThat(calculator.calculate(data)).isEqualTo(10);
+    }
 
 }
