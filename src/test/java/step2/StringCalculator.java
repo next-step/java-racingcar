cf0d53e106603fdd7648f0ef2df03cb54d97c7d4
catsbi <killfine198@gmail.com>
2020-11-01 18:27:42 +0900

    [refactor] apply feedback step 2 - StringCalculator

--- src/test/java/step2/StringCalculator.java
+++ src/test/java/step2/StringCalculator.java
@@ -3,37 +3,46 @@ package step2;
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntFunction;
 
 public class StringCalculator {
     private static final String DELIMETER = " ";
     private static final String OPERATOR_PATTERN = "[+\\-*/]";
     private static final String NUMBER_PATTERN = "(^\\d*)$";
-    private static final Map<String, IntBinaryOperator> functionByOperator = new HashMap<String, IntBinaryOperator>() {{
+    private static final Map<String, IntBinaryOperator> functionByOperator = new HashMap<>() {{
         put("+", Integer::sum);
         put("-", (i, j) -> i - j);
         put("*", (i, j) -> i * j);
         put("/", (i, j) -> i / j);
     }};
-    private static final Function<Integer, Boolean> isEven = (i)-> i % 2 == 0;
-    private static final Function<Integer, Boolean> isOdd = (i)-> i % 2 == 1;
+    private static final IntFunction<Boolean> isEven = (i)-> i % 2 == 0;
+    private static final IntFunction<Boolean> isOdd = (i)-> i % 2 == 1;
+    private final List<Integer> numbers = new ArrayList<>();
+    private final Queue<String> operators = new LinkedList<>();
 
+    public void filtering(int index, String value){
+        numbers.clear();
+        operators.clear();
+        Boolean even = isEven.apply(index);
+        if(even){
+            numbers.add(Integer.parseInt(value));
+        }
+        if (!even) {
+            operators.offer(value);
+        }
+    }
 
     /**
-     * @ImplSpec
+     * @implSpec
      * 인자값으로 문자열을 받아 DELEMITER기준으로 분할하여 문자열의 첫 번째 요소부터 순회하며 사칙연산을 수행하는 메소드
      */
     protected Integer calculate(String input) {
         isEmpty(input);
-        List<Integer> numbers = new ArrayList<>();
-        Queue<String> operators = new LinkedList<>();
-        String[] strings = input.split(" ");
+        String[] strings = input.split(DELIMETER);
         for (int i = 0; i < strings.length; i++) {
             String currentValue = strings[i];
             isValid(currentValue);
-            if(isEven.apply(i))
-                    numbers.add(Integer.parseInt(currentValue));
-            if(isOdd.apply(i))
-                    operators.offer(currentValue);
+            filtering(i, currentValue);
         }
         Optional<Integer> reduce = numbers.stream()
                 .reduce((prev, after) -> functionByOperator.get(operators.poll()).applyAsInt(prev, after));
