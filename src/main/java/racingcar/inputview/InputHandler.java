ea2acc822b4f044f19ec19afeb69fb3a55c42f9b
siyoon <siyoon210@gmail.com>
2020-11-01 17:24:51 +0900

    fix: NUMBER_OF_COUNT_TO_TRY_QUERYING_MSG 메시지 수정

--- src/main/java/racingcar/inputview/InputHandler.java
+++ src/main/java/racingcar/inputview/InputHandler.java
@@ -4,7 +4,7 @@ import racingcar.dto.Input;
 
 public class InputHandler {
     private static final String NUMBER_OF_CAR_QUERYING_MSG = "자동차 대수는 몇 대 인가요?";
-    private static final String NUMBER_OF_COUNT_TO_TRY_QUERYING_MSG = "자동차 대수는 몇 대 인가요?";
+    private static final String NUMBER_OF_COUNT_TO_TRY_QUERYING_MSG = "시도할 회수는 몇 회 인가요?";
 
     private final InputView inputView;
     private final InputDevice inputDevice;
