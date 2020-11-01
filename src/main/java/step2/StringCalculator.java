a163abd9eb5b780f2d7b3084bf03c8dcf00272d6
dbeod2 <darung.0211@gmail.com>
2020-11-01 20:55:51 +0900

    Indent 수정

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -8,40 +8,39 @@ import java.util.stream.IntStream;
 
 public class StringCalculator {
 
- public int calculator(String data) {
-  if (data.equals("") && data == null) {
-   throw new IllegalArgumentException("값을 입력해주세요.");
+  public int calculator(String data) {
+    if (data.equals("") && data == null) {
+      throw new IllegalArgumentException("값을 입력해주세요.");
+    }
+
+    String replaceData = data.replaceAll(" ", "");
+    List<String> datas = IntStream.range(0, replaceData.length())
+            .mapToObj(i -> String.valueOf(replaceData.charAt(i)))
+            .collect(Collectors.toList());
+
+    if (!this.isNumeric(datas.get(0))) {
+      throw new IllegalArgumentException("첫번째 값이 유효하지 않습니다.");
+    }
+    int value = Integer.parseInt(datas.get(0));
+    for (int i = 1; i <= datas.size() - 1; i++) {
+      if (i == datas.size() - 1 && this.isOperator(datas.get(datas.size() - 1))) {
+        continue;
+      }
+      if (!this.isOperator(datas.get(i)) || !this.isNumeric(datas.get(i + 1))) {
+        continue;
+      }
+      value = Calculator.calculate(datas.get(i), value, Integer.parseInt(datas.get(i + 1)));
+    }
+
+    return value;
   }
 
-  String replaceData = data.replaceAll(" ", "");
-  List<String> datas = IntStream.range(0, replaceData.length())
-          .mapToObj(i -> String.valueOf(replaceData.charAt(i)))
-          .collect(Collectors.toList());
-
-  if (!this.isNumeric(datas.get(0))) {
-   throw new IllegalArgumentException("첫번째 값이 유효하지 않습니다.");
+  public boolean isNumeric(String data) {
+    return Pattern.compile("(^[0-9]*$)").matcher(data).matches();
   }
 
-  int value = Integer.parseInt(datas.get(0));
-  for (int i = 1; i <= datas.size() - 1; i++) {
-   if (i == datas.size() - 1 && this.isOperator(datas.get(datas.size() - 1))) {
-    continue;
-   }
-   if (!this.isOperator(datas.get(i)) || !this.isNumeric(datas.get(i + 1))) {
-    continue;
-   }
-   value = Calculator.calculate(datas.get(i), value, Integer.parseInt(datas.get(i + 1)));
+  public boolean isOperator(String data) {
+    List<String> operators = Arrays.asList("+", "-", "*", "/");
+    return operators.contains(data);
   }
-  return value;
- }
-
-
- public boolean isNumeric(String data) {
-  return Pattern.compile("(^[0-9]*$)").matcher(data).matches();
- }
-
- public boolean isOperator(String data) {
-  List<String> operators = Arrays.asList("+", "-", "*", "/");
-  return operators.contains(data);
- }
 }
