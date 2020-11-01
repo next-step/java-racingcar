cc3b2a520e15cad734831821c91681d4c7f4bd13
catsbi <killfine198@gmail.com>
2020-11-01 18:31:06 +0900

    [refactor] 자동차 전진조건 기준점(4) 상수로 분리및 실행 코드 작성

--- src/main/java/step3/Main.java
+++ src/main/java/step3/Main.java
@@ -1,16 +1,23 @@
 package step3;
 
+import step3.strategy.MoveStrategy;
+import step3.strategy.PrintMarkStrategy;
 import step3.game.RacingGameImpl;
 import step3.game.RacingInfomation;
 import step3.view.InputView;
+import step3.view.ResultView;
+
+import java.util.Random;
 
 public class Main {
+
     public static void main(String[] args) {
         RacingGameImpl game = new RacingGameImpl();
         InputView inputView = InputView.getInstance();
+        ResultView resultView = ResultView.getInstance();
         RacingInfomation racingInfomation = inputView.execute();
-
-        game.execute(racingInfomation);
+        game.execute(racingInfomation, MoveStrategy.defaultStrategy(new Random()));
+        resultView.viewAll(game.getResultAll(), new PrintMarkStrategy("-"));
 
     }
 }
