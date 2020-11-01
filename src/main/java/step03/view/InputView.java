3540a008664ab75e85e81a9a393c00cae3b6be4b
eyabc <bey4314@naver.com>
2020-11-02 00:38:15 +0900

    refactor: 문자열의 상수화

--- src/main/java/step03/view/InputView.java
+++ src/main/java/step03/view/InputView.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String NUMBER_OF_CARS_QUESTION = "자동차 대수는 몇 대 인가요?";
+    private static final String NUMBER_OF_MOVES_QUESTION = "시도할 회수는 몇 회 인가요?";
 
     public static int scanNumberOfCars() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
+        System.out.println(NUMBER_OF_CARS_QUESTION);
         return scanner.nextInt();
     }
 
     public static int scanNumberOfMoves() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
+        System.out.println(NUMBER_OF_MOVES_QUESTION);
         return scanner.nextInt();
     }
 }
