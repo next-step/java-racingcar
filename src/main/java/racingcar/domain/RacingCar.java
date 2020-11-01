1c4180d25f8a8298965abc6dd86fe324a8482128
siyoon <siyoon210@gmail.com>
2020-11-02 07:27:34 +0900

    feat: 자동차의 name 필드 추가
    자동차별 이름을 할당한다.

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -8,15 +8,18 @@ public class RacingCar {
     private static final int MIN_MOVABLE_NUM = 4;
 
     private final RandomUtil randomUtil;
+    private final String name;
     private int moveCount;
 
-    public RacingCar() {
+    public RacingCar(String name) {
         this.randomUtil = JavaUtilRandom.getInstance();
+        this.name = name;
         moveCount = 0;
     }
 
-    public RacingCar(RandomUtil randomUtil) {
+    public RacingCar(RandomUtil randomUtil, String name) {
         this.randomUtil = randomUtil;
+        this.name = name;
         moveCount = 0;
     }
 
