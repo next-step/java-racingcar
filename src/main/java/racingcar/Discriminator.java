3eece1b1daf27facc0e231afcfa9f7dac01dced8
changgunyee <minyoung403@naver.com>
2020-11-01 11:00:28 +0900

    refactor: extract constants

--- src/main/java/racingcar/Discriminator.java
+++ src/main/java/racingcar/Discriminator.java
@@ -4,12 +4,14 @@ import java.util.Random;
 
 public class Discriminator {
     private final Random random;
+    private static final int THRESHOLD = 4;
+    private static final int BOUND = 10;
 
     public Discriminator(Random random) {
         this.random = random;
     }
 
     public boolean discriminate() {
-        return random.nextInt(10) > 3;
+        return random.nextInt(BOUND) >= THRESHOLD;
     }
 }
