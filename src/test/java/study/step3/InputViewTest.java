8ce0d0f6e6293111f23c98c3092155d1315afe92
kit(김광수) <kit.t@kakaocorp.com>
2020-11-01 22:36:08 +0900

    feat: 입력결과가 존재하지 않으면 오류메시지를 응답한다

--- src/test/java/study/step3/InputViewTest.java
+++ src/test/java/study/step3/InputViewTest.java
@@ -14,6 +14,17 @@ public class InputViewTest {
         assertThat(circuit).isNotNull();
     }
 
+    @Test
+    @DisplayName("입력결과가 없으면 오류메시지를 출력한다.")
+    public void whenCarNamesIsEmpty() {
+        TestingInputView inputView = new TestingInputView();
+        inputView.setConsoleInput("");
+
+        inputView.request();
+
+        assertThat(inputView.recentErrorMessage()).isEqualTo("쉼표로 구분된 자동차 이름을 입력해주세요.");
+    }
+
     private static class InputView {
         public Circuit request() {
             return new Circuit();
