0f0b51e2b4ea5186a9a840ff766709079038cbf6
이병무 <am003507@gmail.com>
2020-11-02 22:19:23 +0900

    refactor - 유틸성 메소드를 static 으로~

--- src/main/java/step3/controller/InputController.java
+++ src/main/java/step3/controller/InputController.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 public class InputController {
 
-    private final String CAR_COUNT_QUESTION = "자동차 대수는 몇 대 인가요?";
-    private final String MOVE_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
+    private static final String CAR_COUNT_QUESTION = "자동차 대수는 몇 대 인가요?";
+    private static final String MOVE_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
 
-    public RacingSpec enterInput() {
+    public static RacingSpec enterInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(CAR_COUNT_QUESTION);
         int carCount = scanner.nextInt();
