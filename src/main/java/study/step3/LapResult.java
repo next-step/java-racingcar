a526ccfa5117c804800dcf1260deae71a2df7c1a
kit(김광수) <kit.t@kakaocorp.com>
2020-11-02 10:26:20 +0900

    refactor: delete unused

--- src/main/java/study/step3/LapResult.java
+++ src/main/java/study/step3/LapResult.java
@@ -1,22 +1,12 @@
 package study.step3;
 
 class LapResult {
-    private final Long id;
-    private String name;
+    private final String name;
     private final Boolean moved;
 
-    public LapResult(Long id, Boolean moved) {
-        this.id = id;
-        this.moved = moved;
-    }
-
     public LapResult(String name, Boolean moved) {
-        this(0L, moved);
+        this.moved = moved;
         this.name = name;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public Boolean isMoved() {
