a2c3528d379eea4dd9fcfc10c14097db4aa1d1c0
dbeod2 <darung.0211@gmail.com>
2020-11-03 00:58:05 +0900

    step2 코드수정

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -9,9 +9,7 @@ import java.util.stream.IntStream;
 public class StringCalculator {
 
   // TO-DO refactor list
-  // 1-1. 입력받은 문자열 계산 마지막이 연산자인 경우 유효하지 않은 식이라고 예외를 던진다.
   // 1-2. 유효한 문자열을 피연산자와 연산자로 나눠서 활용하도록 한다.
-  // 1-3. 메서드당 depth 는 1depth로 한다.
 
   public int calculator(String data) {
     if (data.equals("") && data == null) {
@@ -23,20 +21,14 @@ public class StringCalculator {
             .mapToObj(i -> String.valueOf(replaceData.charAt(i)))
             .collect(Collectors.toList());
 
-    if (!this.isNumeric(datas.get(0))) {
-      throw new IllegalArgumentException("첫번째 값이 유효하지 않습니다.");
-    }
+    validate(datas);
+
     int value = Integer.parseInt(datas.get(0));
     for (int i = 1; i <= datas.size() - 1; i++) {
-      if (i == datas.size() - 1 && this.isOperator(datas.get(datas.size() - 1))) {
-        continue;
+      if (this.isOperator(datas.get(i)) && this.isNumeric(datas.get(i + 1))) {
+        value = Calculator.calculate(datas.get(i), value, Integer.parseInt(datas.get(i + 1)));
       }
-      if (!this.isOperator(datas.get(i)) || !this.isNumeric(datas.get(i + 1))) {
-        continue;
-      }
-      value = Calculator.calculate(datas.get(i), value, Integer.parseInt(datas.get(i + 1)));
     }
-
     return value;
   }
 
@@ -47,5 +39,15 @@ public class StringCalculator {
   public boolean isOperator(String data) {
     List<String> operators = Arrays.asList("+", "-", "*", "/");
     return operators.contains(data);
+  }
+
+  public void validate(List<String> datas) {
+    if (!this.isNumeric(datas.get(0))) {
+      throw new IllegalArgumentException("첫번째 값이 유효하지 않습니다.");
+    }
+
+    if (this.isOperator(datas.get(datas.size() - 1))) {
+      throw new IllegalArgumentException("유효하지 않은 연산입니다.");
+    }
   }
 }
