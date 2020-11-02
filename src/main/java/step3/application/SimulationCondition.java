5236e594fc2864dff76f11e1aa53d288ae74f68d
홍성민 <honggasm@gmail.com>
2020-11-03 01:30:07 +0900

    refactor: simulation condition test 작성 및 에러 메시지 추가

--- src/main/java/step3/application/SimulationCondition.java
+++ src/main/java/step3/application/SimulationCondition.java
@@ -1,10 +1,35 @@
 package step3.application;
 
+import common.util.Message;
+import step2.calculator.domain.Expression;
+
+import static common.util.Preconditions.checkArgument;
+import static step3.application.SimulationCondition.ErrorMessage.NUMBER_OF_ATTEMPTS_MUST_MORE_THEN_ONE;
+import static step3.application.SimulationCondition.ErrorMessage.NUMBER_OF_CAR_MUST_MORE_THEN_ONE;
+
 public class SimulationCondition {
+    public enum ErrorMessage implements Message {
+        NUMBER_OF_CAR_MUST_MORE_THEN_ONE(Expression.class.getName() + "'s numberOfCar must more then 1"),
+        NUMBER_OF_ATTEMPTS_MUST_MORE_THEN_ONE(Expression.class.getName() + "'s numberOfAttempts must more then 1"),
+        ;
+
+        private final String message;
+
+        public String getMessage() {
+            return message;
+        }
+
+        ErrorMessage(String message) {
+            this.message = message;
+        }
+    }
+
     private final int numberOfCar;
     private final int numberOfAttempts;
 
     public SimulationCondition(final int numberOfCar, final int numberOfAttempts) {
+        checkArgument(numberOfCar >= 1, NUMBER_OF_CAR_MUST_MORE_THEN_ONE);
+        checkArgument(numberOfAttempts >= 1, NUMBER_OF_ATTEMPTS_MUST_MORE_THEN_ONE);
         this.numberOfCar = numberOfCar;
         this.numberOfAttempts = numberOfAttempts;
     }
