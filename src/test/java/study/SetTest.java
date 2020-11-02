9d2a30ab09eabeb3bc16c2def7919dab9a19a26d
Ji Eun Jun <jjin.dev@gmail.com>
2020-11-02 22:55:26 +0900

    Refactor [] @DisplayName을 이용, 테스트 코드의 기능 강화

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -25,20 +26,23 @@ public class SetTest {
         numbers.add(3);
     }
 
+    @DisplayName("크기 테스트")
     @Test
     void checkSize() {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
+    @DisplayName("int 숫자열에 대한 순차적 테스트")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     void checkHasInteger(int number) {
         assertTrue(numbers.contains(number));
     }
 
+    @DisplayName("csv 형태의 데이터에 대해 테스트")
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+    void checkWithCsvSource(String input, String expected) {
         int actualValue = Integer.parseInt(input);
         boolean result = numbers.contains(actualValue);
         assertEquals(expected, Boolean.toString(result));
