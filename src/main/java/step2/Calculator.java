753ef9d6599215eb268717d261e17ba03b343e20
Byeon-SungMun <201401339hs@gmail.com>
2020-11-03 00:51:29 +0900

    step2 null값 체크

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -2,7 +2,6 @@ package step2;
 
 public class Calculator {
 
-
     public int addition (int num1, int num2) {
         return num1 + num2;
     }
@@ -16,7 +15,14 @@ public class Calculator {
     }
 
     public int division (int num1, int num2) {
+        if (num2 <= 0) {
+            throw new ArithmeticException("0으로는 나눌수 없습니다");
+        }
         return num1 / num2;
     }
-
+    public void vaildate(String input){
+        if(input.trim().isEmpty() || input == null){
+            throw new IllegalArgumentException("에러");
+        }
+    }
 }
--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -4,11 +4,12 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
-
 
     private final static Calculator calculator = new Calculator();
 
@@ -23,15 +24,23 @@ public class CalculatorTest {
     public void sub () {
         assertThat(calculator.subtraction(3, 2)).isEqualTo(1);
     }
+
     @Test
     @DisplayName("곱하기 테스트")
     public void mul () {
         assertThat(calculator.multiplication(3, 2)).isEqualTo(6);
     }
+
     @Test
     @DisplayName("나누기 테스트")
     public void div () {
-        assertThat(calculator.division(4, 2)).isEqualTo(2);
+        assertThat(calculator.division(2,1)).isEqualTo(2);
     }
 
+    @DisplayName("문자열이 비어있으면 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void vaildate (String data) {
+        assertThrows(IllegalArgumentException.class, () -> calculator.vaildate(data));
+    }
 }
