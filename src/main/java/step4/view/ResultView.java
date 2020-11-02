010598617323e3a776778576407371f109568adc
genos.lee <genos.lee@kakaocorp.com>
2020-11-02 18:15:48 +0900

    feature: 전진하는 자동차 출력시 이름도 함께 출력, 우승자 출력

---
+++ src/main/java/step4/view/ResultView.java
@@ -0,0 +1,33 @@
+package step4.view;
+
+import step4.RecordDto;
+
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+
+public class ResultView {
+    private final String SYMBOL = "-";
+    private final PrintWriter output;
+
+    public ResultView(PrintWriter output) {
+        this.output = output;
+    }
+
+    public void show(List<List<RecordDto>> records) {
+        output.println("실행 결과");
+        records.forEach(this::showAtStep);
+    }
+
+    private void showAtStep(List<RecordDto> recordAtStep) {
+        recordAtStep.forEach(recordDto -> output.println(format("%s : %s", recordDto.getName(), String.join("", Collections.nCopies(recordDto.getRecord(), SYMBOL)))));
+        output.println();
+    }
+
+    public void showWinner(List<RecordDto> winnerRecord) {
+        output.print(format("%s가 최종 우승했습니다.", winnerRecord.stream().map(RecordDto::getName).collect(Collectors.joining(", "))));
+    }
+}
