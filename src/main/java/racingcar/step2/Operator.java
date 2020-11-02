3ca29b71487b805dfa6b3382f81cab4de548791f
asda004 <asda00400@gmail.com>
2020-11-02 20:39:47 +0900

    step2 (fix) enum 변경

--- src/main/java/racingcar/step2/Operator.java
+++ src/main/java/racingcar/step2/Operator.java
@@ -1,28 +1,34 @@
 package racingcar.step2;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
 /**
  * Created : 2020-10-30 오전 10:44
  * Developer : Seo
  */
 public enum Operator {
-//    PLUS(value -> value + val);
-//
-//
-//    private int sum = 0;
-//    Function<Integer, Integer> add1 = (value) -> sum = value;
-//    Function<Integer, Integer> add2 = (value) -> sum + value;
-//
-//    Function<Integer, Integer> add3 = add2.compose(add1);
-//    Integer result1 = add3.apply(3);
-//
-//    private Function<String, String> expression;
-//
-//    Operator(Function<String, String> expression) {
-//        this.expression = expression;
-//    }
-//
-//    public int calculate(int sum, String value, String operator) {
-//        return expression.apply()
-//    }
+    PLUS("+", (x, y) -> x + y),
+    MINUS("-", (x, y) -> x - y),
+    MULTIPLY("*", (x, y) -> x * y),
+    DIVISION("/", (x, y) -> x / y);
 
+    private String operator;
+    private BiFunction<Integer, Integer, Integer> expression;
+
+    Operator(String operator, BiFunction<Integer, Integer, Integer> expression) {
+        this.operator = operator;
+        this.expression = expression;
+    }
+
+    public int calculate(Integer sum, String next) {
+        return expression.apply(sum, Integer.parseInt(next));
+    }
+
+    public static Operator getOperator(String s) {
+        return Arrays.stream(Operator.values())
+                .filter(o -> o.operator.equals(s))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
