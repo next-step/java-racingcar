bd31d1b76746da33a5b7681c194950c03de27bd1
mand2 <nayeon.kim1123@gmail.com>
2020-11-03 02:30:30 +0900

    feat: step2 문자열계산기 - 최대한 인덴트 없이. - 한 메서드 내에 15줄 내로 구현하기가 힘들다 😱 - 크게 연산을 직접 하는 부분과 연산입력을 받는 부분으로 나눔. - enum을 stream 형태로 연결해서 써보기 - 입력값이 두개인 function은 BiFunction이다. - 실제 입력값을 validation 하는 클래스를 따로 만들까 싶었지만, 아직은 작은 단위라서 같이 만듬.😂 - 현재 개발한 계산기는 일회용X/O?   추측만 하자면, A 계산식을 입력 후 B 계산식을 연달아 입력 가능할..듯..? A 의 결과값을 첫번째 값으로 세팅하면 될 듯하다.

---
+++ src/main/java/stringcalculator/Operator.java
@@ -0,0 +1,55 @@
+package stringcalculator;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+/**
+ * Created By mand2 on 2020-11-03.
+ * 계산식 처리 :enum으로 받아서 처리해도 될 것 같은데?
+ */
+public enum  Operator {
+    PLUS("+", (first, second) -> first + second),
+    MINUS("-", (first, second) -> first - second),
+    MULTIPLY("*", (first, second) -> first * second),
+    DIVIDE("/", (first, second) -> first / second)
+    ;
+
+    private String operator;
+    private BiFunction<Integer, Integer, Integer> expression;
+
+    Operator(String operator, BiFunction<Integer, Integer, Integer> expression) {
+        this.operator = operator;
+        this.expression = expression;
+    }
+
+    public static int calculate(int first, String operator, int second){
+        try {
+            return getOperator(operator).expression.apply(first, second);
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
+    }
+
+    private static Operator getOperator(String operator) {
+        return Arrays.stream(values())
+                .filter(opr -> opr.operator.equals(operator))
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("사칙연산 부호가 아닙니다."));
+    }
+
+    //    public static int plus(String first, String second) {
+//        return NumberUtils.toInt(first) + NumberUtils.toInt(second);
+//    }
+//
+//    public static int minus(String first, String second) {
+//        return NumberUtils.toInt(first) - NumberUtils.toInt(second);
+//    }
+//
+//    public static int multiply(String first, String second) {
+//        return NumberUtils.toInt(first) * NumberUtils.toInt(second);
+//    }
+//
+//    public static int divide(String first, String second) {
+//        return NumberUtils.toInt(first) / NumberUtils.toInt(second);
+//    }
+
+}
---
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -0,0 +1,78 @@
+package stringcalculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.logging.Logger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created By mand2 on 2020-11-03.
+ */
+class CalculatorTest {
+
+    @ParameterizedTest
+    @DisplayName("계산 실제로 되는가?")
+    @ValueSource(strings = {"2 + 3 * 4 / 2"})
+    public void 입력값_테스트_0(String input) {
+        Calculator cal = new Calculator(input);
+        assertThat(cal.calculate()).isEqualTo(10);
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력값이 null")
+    @NullSource
+    public void 입력값_테스트_1(String input) {
+        assertThatThrownBy(() -> new Calculator(input).calculate())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력값이 빈칸")
+    @ValueSource(strings = {""})
+    public void 입력값_테스트_2(String input) {
+        assertThatThrownBy(() -> new Calculator(input).calculate())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자가 아닐 때")
+    @ValueSource(strings = {"a + 1"})
+    public void 입력값_테스트_3(String input) {
+        assertThatThrownBy(() -> new Calculator(input).calculate())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("주어진 기호가 아닐 때")
+    @ValueSource(strings = {"2 . 4"})
+    public void 입력값_테스트_4(String input) {
+        assertThatThrownBy(() -> new Calculator(input).calculate())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력값 마무리가 안되었을 때")
+    @ValueSource(strings = {"2 + 4 -"})
+    public void 입력값_테스트_5(String input) {
+        assertThatThrownBy(() -> new Calculator(input).calculate())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("나눗셈 오류")
+    @ValueSource(strings = {"2 + 4 / 0"})
+    public void 입력값_테스트_6(String input) {
+        assertThatThrownBy(() -> new Calculator(input).calculate())
+                .isInstanceOf(ArithmeticException.class);
+    }
+
+}
