d306cc00f2c5eed793b68b3468e1c33e784f57fd
이병무 <am003507@gmail.com>
2020-11-02 22:18:47 +0900

    refactor - 클래스 내에서 사용되는 String 상수화

--- src/main/java/step3/controller/InputController.java
+++ src/main/java/step3/controller/InputController.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 
 public class InputController {
 
+    private final String CAR_COUNT_QUESTION = "자동차 대수는 몇 대 인가요?";
+    private final String MOVE_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
+
     public RacingSpec enterInput() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
+        System.out.println(CAR_COUNT_QUESTION);
         int carCount = scanner.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
+        System.out.println(MOVE_COUNT_QUESTION);
         int moveCount = scanner.nextInt();
         RacingSpec spec = new RacingSpec(carCount, moveCount);
         return spec;
