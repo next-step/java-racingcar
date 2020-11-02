4ba7b857de2dd51c8238d9d47f00c9049e75a7b9
DongHwan Kim <bkuekany@gmail.com>
2020-11-03 01:17:33 +0900

    refactor: Refactor 랜더링 뷰 테스트확인을 위한 변수추가

--- src/main/java/step3/view/RenderingView.java
+++ src/main/java/step3/view/RenderingView.java
@@ -5,6 +5,11 @@ import step3.model.RacingCars;
 
 public class RenderingView implements RaceGameContract.View {
 
+    private int executeCount;
+
+    public RenderingView(){
+        System.out.println("실행결과");
+    }
 
     @Override
     public void renderView(RacingCars cars) {
@@ -12,5 +17,10 @@ public class RenderingView implements RaceGameContract.View {
             System.out.println(cars.getParticipantCar(i).getCommands());
         }
         System.out.println("");
+        executeCount++;
+    }
+
+    public int getExecuteCount() {
+        return executeCount;
     }
 }
