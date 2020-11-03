cab5b57c9b4bafe937e86523e674a92efd4b3550
이병무 <am003507@gmail.com>
2020-11-04 00:05:40 +0900

    feat - 객체 생성 예외 처리추가

--- src/main/java/step3/ErrorMessage.java
+++ src/main/java/step3/ErrorMessage.java
@@ -7,4 +7,5 @@ public class ErrorMessage {
 
     public static final String INVALID_RANDOM_VALUE = "invalid random value";
     public static final String MOVE_STRATEGY_IS_NULL = "move strategy is null";
+    public static final String INVALID_RACING_SPEC = "invalid racing spec";
 }
--- src/main/java/step3/RacingSpec.java
+++ src/main/java/step3/RacingSpec.java
@@ -5,7 +5,10 @@ public class RacingSpec {
     private final Integer carCount;
     private final Integer moveCount;
 
-    public RacingSpec(Integer carCount, Integer moveCount) {
+    public RacingSpec(int carCount, int moveCount) {
+        if (carCount <= 0 || moveCount <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RACING_SPEC);
+        }
         this.carCount = carCount;
         this.moveCount = moveCount;
     }
