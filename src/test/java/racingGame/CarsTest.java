efaab8f7423105d8b5b71d6ae9525881ec86fdc0
SungHyoeb Baek <baek9652@naver.com>
2020-11-03 21:24:25 +0900

    test 빠져있던 Cars 테스트 추가

--- src/test/java/racingGame/CarsTest.java
+++ src/test/java/racingGame/CarsTest.java
@@ -1,7 +1,29 @@
 package racingGame;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.Vector;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class CarsTest {
+
+    @DisplayName("차량 초기상태 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    void testNumCars(int input) {
+
+        List<Integer> results = new Vector<>(input);
+        for (int i = 0; i < input; i++) {
+            results.add(0);
+        }
+
+        assertThat(Cars.of(input).getPositions()).isEqualTo(results);
+
+    }
 
 }
