5a42d955c11ce5125782d1da53c39fbffda9b24a
siyoon <siyoon210@gmail.com>
2020-11-03 21:13:25 +0900

    feat: numberOfCountToTry이 0보다 작을 경우 throw IllegalStateException

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -22,17 +22,25 @@ public class RacingCarGame {
         Input input = inputHandler.getInput();
         List<RacingCar> racingCars = input.toRacingCars();
 
-        if (racingCars.isEmpty()) {
-            return;
-        }
+        runRacing(input, racingCars);
+
+        List<RacingCar> winners = getWinners(racingCars);
+        resultView.printWinners(winners);
+    }
+
+    private void runRacing(Input input, List<RacingCar> racingCars) {
+        validateNumberOfCountToTry(input);
 
         for (int i = 0; i < input.numberOfCountToTry; i++) {
             tryToMoveRacingCars(racingCars);
             resultView.printProgress(racingCars);
         }
+    }
 
-        List<RacingCar> winners = getWinners(racingCars);
-        resultView.printWinners(winners);
+    private void validateNumberOfCountToTry(Input input) {
+        if (input.numberOfCountToTry <= 0) {
+            throw new IllegalStateException("The 'numberOfCountToTry' must be greater than 0.");
+        }
     }
 
     private void tryToMoveRacingCars(List<RacingCar> racingCars) {
