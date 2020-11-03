c4e585a637d4f0fc32a997d42124d4ea217dc46d
이병무 <am003507@gmail.com>
2020-11-04 00:22:36 +0900

    feat - 메소드 분리

--- src/main/java/step3/controller/ResultController.java
+++ src/main/java/step3/controller/ResultController.java
@@ -6,17 +6,31 @@ import java.util.stream.IntStream;
 public class ResultController {
 
     private static final String RACING_DASH = "-";
+    private static final String NEW_LINE = "\n";
 
     public void printRacingLog(List<Integer> racingStepResult) {
-        racingStepResult
-                .stream()
-                .forEach(this::printLocation);
-        System.out.println();
+        System.out.println(makeRacingResult(racingStepResult));
     }
 
-    public void printLocation(Integer location) {
+    public String makeRacingResult(List<Integer> racingStepResult) {
         StringBuilder sb = new StringBuilder();
-        IntStream.range(0, location).forEach(i -> sb.append(RACING_DASH));
-        System.out.println(sb);
+        racingStepResult
+                .stream()
+                .forEach(
+                        location -> {
+                            sb.append(makeLocationString(location));
+                            sb.append(NEW_LINE);
+                        }
+                );
+        return sb.toString();
     }
+
+    public String makeLocationString(int location) {
+        StringBuilder sb = new StringBuilder();
+        IntStream
+                .range(0, location)
+                .forEach(i -> sb.append(RACING_DASH));
+        return sb.toString();
+    }
+
 }
