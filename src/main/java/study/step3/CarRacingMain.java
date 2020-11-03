517ace3f136f93aefa125d878a47739443c587fc
karian7(김광수) <karian7@gmail.com>
2020-11-03 23:06:45 +0900

    feat: 엔트리 포인트 변경

--- src/main/java/study/step3/CarRacingMain.java
+++ src/main/java/study/step3/CarRacingMain.java
@@ -1,23 +1,10 @@
 package study.step3;
 
 
-import study.step3.domain.CarRacing;
-import study.step3.domain.Circuit;
-import study.step3.view.InputView;
-import study.step3.view.ResultView;
-
 public class CarRacingMain {
     public static void main(String[] args) {
-        Circuit request = requestInput();
-        CarRacing carRacing = new CarRacing(request);
-        carRacing.start();
-        ResultView resultView = new ResultView(carRacing);
-        resultView.report();
-    }
-
-    private static Circuit requestInput() {
         try {
-            return new InputView().request();
+            new CarRacingController().invoke();
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
             System.exit(-1);
