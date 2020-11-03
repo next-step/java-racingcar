526c7aac7cacf6787e39d696629441f2f22ef250
karian7(김광수) <karian7@gmail.com>
2020-11-03 19:59:24 +0900

    refactor: 경기기록을 저장하는 RaceRecord 객체의 초기상태를 테스트한다
    CarRacing 객체는 경기기록을 관리하고 있다.
    Map<String, List<Boolean>> 으로 관리하던 경기기록을 일급컬렉션으로 교체한다.
    이를 목적으로 RaceRecord 객체가 만들어졌으며 객체내 상태를 확인하는 테스트로 시작한다.

---
+++ src/test/java/study/step3/RaceRecordTest.java
@@ -0,0 +1,21 @@
+package study.step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RaceRecordTest {
+    // TODO 레코드 상태 존재
+    // TODO 이동 기록 저장
+    // TODO 가장 많이 이동한 기록 반환
+    // TODO 기록횟수 반환
+    // TODO 이름으로 정렬된 기록 반환
+
+    @Test
+    @DisplayName("RaceRecord는 생성후 비어있는 상태이다.")
+    void initState() {
+        RaceRecord raceRecord = new RaceRecord();
+        assertThat(raceRecord.isEmpty()).isTrue();
+    }
+}
