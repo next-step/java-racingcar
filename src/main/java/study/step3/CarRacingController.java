76a9ef2bc029fdfa340e9e3ecd5b95d77bb02a1c
karian7(김광수) <karian7@gmail.com>
2020-11-03 23:06:18 +0900

    feat: make it pass

---
+++ src/main/java/study/step3/CarRacingController.java
@@ -0,0 +1,28 @@
+package study.step3;
+
+import study.step3.domain.CarRacing;
+import study.step3.domain.Circuit;
+import study.step3.view.InputView;
+import study.step3.view.ResultView;
+
+public class CarRacingController {
+    void invoke() {
+        Circuit circuit = makeInputView().request();
+
+        makeRacing(circuit).start();
+
+        makeResultView(makeRacing(circuit)).report();
+    }
+
+    protected InputView makeInputView() {
+        return new InputView();
+    }
+
+    protected ResultView makeResultView(CarRacing carRacing) {
+        return new ResultView(carRacing);
+    }
+
+    protected CarRacing makeRacing(Circuit request) {
+        return new CarRacing(request);
+    }
+}
