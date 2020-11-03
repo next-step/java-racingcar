bf6d02afb90eaa4f69e9c5acdb889bb2ceee013b
dbeod2 <darung.0211@gmail.com>
2020-11-04 01:09:50 +0900

    2단계 문자열 계산기 피드백 반영

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -8,9 +8,6 @@ import java.util.stream.IntStream;
 
 public class StringCalculator {
 
-  // TO-DO refactor list
-  // 1-2. 유효한 문자열을 피연산자와 연산자로 나눠서 활용하도록 한다.
-
   public int calculator(String data) {
     if (data.equals("") && data == null) {
       throw new IllegalArgumentException("값을 입력해주세요.");
@@ -24,11 +21,10 @@ public class StringCalculator {
     validate(datas);
 
     int value = Integer.parseInt(datas.get(0));
-    for (int i = 1; i <= datas.size() - 1; i++) {
-      if (this.isOperator(datas.get(i)) && this.isNumeric(datas.get(i + 1))) {
-        value = Calculator.calculate(datas.get(i), value, Integer.parseInt(datas.get(i + 1)));
-      }
+    for (int i = 0; i < datas.size() - 2; i += 2) {
+      value = Calculator.calculate(datas.get(i + 1), value, Integer.parseInt(datas.get(i + 2)));
     }
+
     return value;
   }
 
