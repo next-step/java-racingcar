25c905fe41c2292161dbb746dc2ebbe03b4cebcf
rlawls1991 <rlawls1991@gmail.com>
2020-11-04 02:11:22 +0900

    에러 메세지 관리 클래스 생성

---
+++ src/main/java/race/car/ErrorMessage.java
@@ -0,0 +1,24 @@
+package race.car;
+
+public class ErrorMessage {
+    private static String NULL_EXCEPTION = "Null을 입력하였습니다. 숫자를 입력해주세요";
+    private static String EMPTY_EXCEPTION = "입력하지 않으셨습니다. 숫자를 입력해주세요";
+    private static String NUMBERFORMATEXCEPTION = "문자를 입력하였습니다. 숫자를 입력해주세요";
+    private static String ARRAYINDEXOUTOFBOUNDSEXCEPTION = "자동차의 갯수를 1개이상으로 입력해주세요";
+
+    public static String getNullException() {
+        return NULL_EXCEPTION;
+    }
+
+    public static String getEmptyException() {
+        return EMPTY_EXCEPTION;
+    }
+
+    public static String getNUMBERFORMATEXCEPTION() {
+        return NUMBERFORMATEXCEPTION;
+    }
+
+    public static String getARRAYINDEXOUTOFBOUNDSEXCEPTION() {
+        return ARRAYINDEXOUTOFBOUNDSEXCEPTION;
+    }
+}
