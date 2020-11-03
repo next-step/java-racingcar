15ae7900a83a854299533ef008469f2e860b40de
morriskim <kdy0573@naver.com>
2020-11-03 12:02:55 +0900

    Step1 (#1323)
    * String split test study
    
    * racing-step1
    
    * java-racing setp1 코드 리뷰 피드백 수정
    
    Co-authored-by: morriskim <kdy0573@daum.net>

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,24 +1,22 @@
 package study;
 
-import org.assertj.core.internal.Integers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import java.util.HashSet;
 import java.util.Set;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         numbers = new HashSet<>();
         numbers.add(1);
         numbers.add(1);
@@ -28,43 +26,40 @@ public class SetTest {
 
     @DisplayName("Require01 SetSize Check")
     @Test
-    void setSizeTest(){
+    void setSizeTest() {
         int expectSetSize = 3;
         assertThat(expectSetSize).isEqualTo(numbers.size());
     }
 
     @DisplayName("Require01 invalid SetSize Check")
     @Test
-    void invalidSetSizeTest(){
-        int expectSetSize = numbers.size()+1;
+    void invalidSetSizeTest() {
+        int expectSetSize = numbers.size() + 1;
         assertThat(expectSetSize).isNotEqualTo(numbers.size());
     }
 
     @DisplayName("Require02 Contain Test")
     @Test
-    void containTest(){
-        assertThat(numbers.contains(1)).isTrue();
-        assertThat(numbers.contains(2)).isTrue();
-        assertThat(numbers.contains(3)).isTrue();
+    void containTest() {
+        assertAll(
+                () -> assertThat(numbers.contains(1)).isTrue(),
+                () -> assertThat(numbers.contains(2)).isTrue(),
+                () -> assertThat(numbers.contains(3)).isTrue()
+        );
     }
 
     @DisplayName("Require02 Contain Parameterized Test")
     @ParameterizedTest
-    @ValueSource(ints = {1,1,2})
-    void containParameterizedTest(int element){
+    @ValueSource(ints = {1, 1, 2})
+    void containParameterizedTest(int element) {
         assertTrue(numbers.contains(element));
     }
 
     @DisplayName("Require03 Contain Method Test")
     @ParameterizedTest
-    @CsvSource(value = {"1:true","2:true","3:true","4:false","5:false"})
-    void containMethodTest(String input, String expect){
-        assertThat(numbers.contains(input))
-                .isEqualTo(
-                        Boolean.parseBoolean(expect)
-                );
-
-
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containMethodTest(int input, String expect) {
+        assertThat(numbers.contains(input)).isEqualTo(Boolean.parseBoolean(expect));
     }
 
 
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,40 +2,42 @@ package study;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
     @Test
-    void splitString(){
+    void splitString() {
         String[] split = "1,2".split(",");
         assertThat(split).contains("1");
         assertThat(split).contains("2");
-        assertThat(split).containsExactly("1","2");
+        assertThat(split).containsExactly("1", "2");
     }
+
     @Test
-    void substringTest(){
+    void substringTest() {
         String data = "(1,2)";
         String substring = data.substring(1, 4);
         assertThat(substring).isEqualTo("1,2");
     }
 
-    @Test
     @DisplayName("String charAt Method Test")
-    void charAtMethodTest(){
+    @Test
+    void charAtMethodTest01() {
         String data = "abc";
         char c = data.charAt(0);
         assertThat(c).isEqualTo('a');
-
-        int findCharIndex = 5;
-        assertThatThrownBy(()->{
-            data.charAt(findCharIndex);
-        }).isInstanceOf(StringIndexOutOfBoundsException.class)
-                .hasMessageContaining("String index out of range: "+findCharIndex);
     }
 
-
-
+    @DisplayName("String charAt Method IndexOfBound Exception Test")
+    @Test
+    void charAtMethodExceptionTest() {
+        String data = "abc";
+        int findCharIndex = 5;
+        assertThatThrownBy(() -> {
+            data.charAt(findCharIndex);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: " + findCharIndex);
+    }
 }
