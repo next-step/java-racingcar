e507d0af6ab9b8922d12cc5c8f8e83ec89294f29
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-04 21:54:57 +0900

    feat : 자동자 대수 세팅 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -2,8 +2,13 @@ package racingcar;
 
 public class Game {
     private static int gameCount;
+    private static int carCount;
 
     public static void setGameCount(int count){
         gameCount = count;
+    }
+
+    public static void setCarCount(int count){
+        carCount = count;
     }
 }
