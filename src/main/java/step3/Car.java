bd0d07aa754b6f85ec620117c6ea7ad14e5deaf2
dbeod2 <darung.0211@gmail.com>
2020-11-05 02:39:57 +0900

    미사용 import 제거

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -1,9 +1,6 @@
 package step3;
 
 import java.util.Random;
-import java.util.StringJoiner;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Car {
   private int move;
