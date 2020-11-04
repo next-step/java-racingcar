8859f7e5870a02a091fdc61ef4f620b765c9b607
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-04 23:05:07 +0900

    refector : 인스턴스 메서드 클래스 메서드로 변경

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -2,11 +2,11 @@ package racingcar;
 
 public class InputView {
 
-    public void viewCountCarInputMessage(){
+    public static void viewCountCarInputMessage(){
         System.out.println("자동차 대수는 몇 대인가요?");
     }
 
-    public void viewConutGameInputMessage(){
+    public static void viewConutGameInputMessage(){
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
 }
