affc398aecedfc8f7c4d50a7fadede7375b03b48
HRLEE <apalsl123@gmail.com>
2020-11-04 21:21:26 +0900

    Step2 - 문자열 계산기 (#1416)
    * Add Calculator, Add isBlank
    
    * Fix init Method
    
    값을 입력하라는 간단한 설명 추가
    
    * Add Calculator
    
    * Fix CalculatorTest, CalculatorSignTest
    
    사용하지 않는 테스트코드 제거, DisplayName 추가, 메서드 이름 변경

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import calculator.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.run();
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import org.assertj.core.util.Strings;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class CalculatorTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "        "})
+    @DisplayName(value = "String이 null or 공백 데이터일 경우 isBlack true")
+    public void isBlank_ShouldReturnTrueForNullOrBlankStrings(String str) {
+        assertThat(str == null || str.trim().isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"*", "/" , "+", "-"})
+    @DisplayName(value = "지원하는 계산식일 경우 true를 반환함")
+    public void isMatches_ShouldReturnTrueForCalculatorSign(String str) {
+        assertThat(str.matches("^[*/+-]+$")).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "9", "19", "9", "1232"})
+    @DisplayName(value = "숫자일 경우 true를 반환함")
+    public void isMatches_ShouldReturnTrueInteger(String str) {
+
+        assertThat(str.matches("^[0-9]+$")).isTrue();
+    }
+
+}
