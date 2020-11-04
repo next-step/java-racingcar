d15cfdf10771923111ea2473375bc209350eaef3
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-04 18:22:37 +0900

    feat : 시도할 횟수 요청 문구 출력 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -6,4 +6,7 @@ public class InputView {
         System.out.println("자동차 대수는 몇 대인가요?");
     }
 
+    public void viewConutGameInputMessage(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
 }
