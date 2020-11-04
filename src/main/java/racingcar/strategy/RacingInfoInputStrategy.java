ccd4750c6ab4cc4b5580c50f62c45ecd4f965249
catsbi <hansolsolsol@mosition.co.kr>
2020-11-04 15:31:44 +0900

    [refactor] InputStrategy 구현체 네이밍 변경

--- src/main/java/racingcar/strategy/RacingInfoInputStrategy.java
+++ src/main/java/racingcar/strategy/RacingInfoInputStrategy.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import static racingcar.Constant.QUESTION_JOIN_CAR_NAME_LIST;
 import static racingcar.Constant.QUESTION_PLAY_ROUND;
 
-public class CarNameInputStrategy implements InputStrategy {
+public class RacingInfoInputStrategy implements InputStrategy {
 
     private int questionPlayCount(Scanner scanner) {
         System.out.println(QUESTION_PLAY_ROUND);
