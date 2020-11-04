1128048efd53644692249d70f3193124bc2f88ab
naru <hsik0225@gmail.com>
2020-11-04 20:28:48 +0900

    style : RacingGameTest @DisplayName 변경

--- src/test/java/racing/RacingGameTest.java
+++ src/test/java/racing/RacingGameTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RacingGameTest {
 
     @Test
-    @DisplayName("4 이상의 숫자가 나왔을 때 움직이는지 확인합니다")
+    @DisplayName("항상 움직이는지 확인합니다")
     public void moveTest() {
         RacingCars racingCars = new RacingCars(3);
         racingCars.race(new FixedRoulette(4));
@@ -18,7 +18,7 @@ public class RacingGameTest {
     }
 
     @Test
-    @DisplayName("4 미만의 숫자가 나왔을 때 안 움직이는지 확인합니다")
+    @DisplayName("항상 안 움직이는지 확인합니다")
     public void doNotMoveTest() {
         RacingCars racingCars = new RacingCars(3);
         racingCars.race(new FixedRoulette(0));
