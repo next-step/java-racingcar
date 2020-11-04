8acbe960b20af147a983e176d1e784cd7cc5bff6
sehan <sehan.choi@gmail.com>
2020-11-04 14:48:50 +0900

    2단계 - 문자열 계산기
    > Calculator 인테페이스
    > - expression 을 받아서 계산하고 int 값을 return 하는 calculate() 를 가집니다.
    > - 기본구현체로 Calculator.Default 를 제공합니다.
    >
    > Test
    > - Calculator.calculator() 메소드를 테스트 합니다.
    > - DefaultCalculatorTest 는 Calculator.Default 에 대한 Test 입니다

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,117 @@
+import java.util.*;
+import java.util.function.BiFunction;
+
+public interface Calculator {
+
+    int calculate(String expression);
+
+    class Default implements Calculator {
+
+        private List<Operator> supportedOperators = Arrays.asList(Operator.Plus, Operator.Minus, Operator.Multiple, Operator.Divide);
+
+        enum Operator {
+            Plus("+", (value1, value2) -> value1 + value2),
+            Minus("-", (value1, value2) -> value1 - value2),
+            Multiple("*", (value1, value2) -> value1 * value2),
+            Divide("/", (value1, value2) -> value1 / value2);
+
+            private String sign;
+
+            private BiFunction<Integer, Integer, Integer> executor;
+
+            Operator(String sign, BiFunction<Integer, Integer, Integer> executor) {
+                this.sign = sign;
+                this.executor = executor;
+            }
+
+            public static Operator valueOfSign(String sign) {
+                Operator[] ops = Operator.values();
+                for (int i = 0; i < ops.length; i++) {
+                    if (ops[i].sign.equals(sign)) return ops[i];
+                }
+                throw new EnumConstantNotPresentException(Operator.class, sign);
+            }
+        }
+
+        @Override
+        public int calculate(String expression) {
+            validateExpression(expression);
+
+            Deque<Integer> operands = new LinkedList<>();
+            Queue<Operator> operators = new LinkedList<>();
+
+            parseExpression(expression, operands, operators);
+
+            Integer result = 0;
+            Iterator<Operator> it = operators.iterator();
+
+            while (it.hasNext()) {
+                result = executeOperator(it.next(), operands.poll(), operands.poll());
+                operands.addFirst(result);
+            }
+
+            return result;
+        }
+
+        private Integer executeOperator(Operator op, Integer value1, Integer value2) {
+            return op.executor.apply(value1, value2);
+        }
+
+        private void parseExpression(String expression, Queue<Integer> operands, Queue<Operator> operators) {
+            String[] parts = expression.split(" ");
+
+            for (int i = 0; i < parts.length; i++) {
+                if (isOperator(parts[i])) {
+                    operators.offer(Operator.valueOfSign(parts[i]));
+                } else if (isNumeric(parts[i])) {
+                    operands.offer(Integer.valueOf(parts[i]));
+                } else {
+                    throw new IllegalArgumentException(expression);
+                }
+            }
+        }
+
+        private boolean isNumeric(String value) {
+            try {
+                Integer.parseInt(value);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+        private boolean isOperator(String value) {
+            try {
+                return supportedOperators.contains(Operator.valueOfSign(value));
+            } catch (EnumConstantNotPresentException e) {
+                return false;
+            }
+        }
+
+        private void validateExpression(String expression) {
+            if (isNullOrBlank(expression) || !isStartByNumericOperand(expression)
+            ) throw new IllegalArgumentException(expression);
+        }
+
+        private boolean isNullOrBlank(String expression) {
+            if (expression == null) return true;
+            if (expression.isEmpty()) return true;
+            for (int i = 0; i < expression.length(); i++) {
+                if (!Character.isWhitespace(expression.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private boolean isStartByNumericOperand(String expression) {
+            try {
+                Integer.parseInt(String.valueOf(expression.split(" ")[0]));
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+abstract class CalculatorTest {
+
+    Calculator calculator;
+
+    @DisplayName("expression 에 사칙연산 기호가 아닌값이 포함되면 IllegalArgumentException 을 던진다")
+    @ParameterizedTest
+    @ValueSource( strings = {"1 @ 2", "1 % 2", "1 ^ 2", "1 & 2"})
+    void requirement1(String expression){
+        assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculate(expression));
+    }
+
+    @DisplayName("expression 이 null 이거나 빈 공백 문자일 경우 IllegalArgumentException 을 던진다")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void requirement2(String expression){
+        assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculate(expression));
+    }
+
+    @DisplayName("expression 이 숫자로 시작하지 않으면 IllegalArgumentException 을 던진다")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "+ 1 * 3",
+            "일 + 2",
+            "# + 2",
+            "A + 2"})
+    void requirment3(String expression){
+        assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculate(expression));
+    }
+
+    @DisplayName("사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정된다")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1 + 2 * 3=9",
+            "1 + 2 + 3 / 2=3",
+            "2 + 3 * 4 / 2=10",
+            "8 * 4 - 10 / 2=11"}, delimiter = '=')
+    void calculate(String expression, Integer expected){
+        assertThat(calculator.calculate(expression)).isEqualTo(expected);
+    }
+
+}
+
+class DefaultCalculatorTest extends CalculatorTest{
+
+    @BeforeEach
+    void setUp(){
+        calculator = new Calculator.Default();
+    }
+
+}
