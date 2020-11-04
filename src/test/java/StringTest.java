42cfc28dbd588ebe7e53d2a313780ba93b279153
nio.d <nio.d@kakaocorp.com>
2020-11-04 09:58:55 +0900

    Set Collection에 대한 학습 테스트
    > 추가. 코드정리

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,9 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class StringTest {
 
