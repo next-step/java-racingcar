b88caea0d491296c449872e18c8b49ae76380103
DongHwan Kim <bkuekany@gmail.com>
2020-11-05 00:27:51 +0900

    refactor: 출력을 위한 일부 메서드 추가
    - 출력을 담당하는 로직이 현재 클래스 내부에 있음. 향후 개선필요
    - 생각보다 UI / 비즈니스 로직 분리의 어려움

--- src/main/java/step3/model/RacingCars.java
+++ src/main/java/step3/model/RacingCars.java
@@ -1,28 +1,34 @@
 package step3.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class RacingCars {
 
     private List<RacingCar> participantCars;
+    private int totalRound;
 
-    public RacingCars(List<RacingCar> participantCars) {
+    public RacingCars(List<RacingCar> participantCars, int totalRound) {
         this.participantCars = participantCars;
+        this.totalRound = totalRound;
     }
 
-    public void moveForward(int index){
+    public void moveForward(int index) {
         participantCars.get(index).executeMoveTrack();
     }
 
-
-    public int[] targetRacingCarRoundResult(int index) {
-        int size  = participantCars.get(index).getMoveTrack();
-        int[] roundResult = new int[size];
-        return roundResult;
-
+    public int size() {
+        return participantCars.size();
     }
 
-    public int size(){
-        return participantCars.size();
+    public int getTotalRound() {
+        return this.totalRound;
+    }
+
+    public void roundResult() {
+        for (int i = 0; i < participantCars.size(); i++) {
+            System.out.println(String.join("", Collections.nCopies(participantCars.get(i).movePosition(), "-")));
+        }
+        System.out.println("");
     }
 }
