018ee29453ad2eb06821b9bf3119bbbb2f43cf6d
changgunyee <minyoung403@naver.com>
2020-11-04 19:30:35 +0900

    refactor: test addRecord by calling once

--- src/test/java/racingcar/RecordsTest.java
+++ src/test/java/racingcar/RecordsTest.java
@@ -3,20 +3,16 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.IntStream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RecordsTest {
     @Test
     @DisplayName("레코드 저장하기")
     void addRecord() {
-        int recordNum = 3;
-        Records records = new Records(recordNum);
+        Records records = new Records(0);
 
-        IntStream.range(0, recordNum)
-                .forEach(idx -> records.addRecord(null));
+        records.addRecord(null);
 
-        assertThat(records.getRecordList()).hasSize(3);
+        assertThat(records.getRecordList()).hasSize(1);
     }
 }
