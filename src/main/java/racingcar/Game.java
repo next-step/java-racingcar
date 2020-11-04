bac9926ab780b4643a6ac4faeb44c658d5b7d5dd
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-04 22:59:08 +0900

    feat : 라운드 시작 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,14 +1,23 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Game {
-    private static int gameCount;
+    private static int roundCount;
     private static Car[] cars;
+    private static Random random = new Random();
 
     public static void setGameCount(int count){
-        gameCount = count;
+        roundCount = count;
     }
 
     public static void setCars(int count){
         cars = new Car[count];
+    }
+
+    public static void playRound(){
+        for (int i = 0; i < cars.length; i++) {
+            if(random.nextInt(10) >= 4) cars[i].foward();
+        }
     }
 }
