9c93510779b35ee983bbc07dbd776e488ad7ac27
monkeyDugi <lbd4946@gmail.com>
2020-11-05 00:21:20 +0900

    (refactor) : operate(), calculate() 병합

--- src/main/java/stringCalculator/Operate.java
+++ src/main/java/stringCalculator/Operate.java
@@ -5,21 +5,18 @@ import java.util.List;
 public class Operate {
 
     private int result = 0;
-    // 연산자의 개수(초기 값 1)
-    // 연산자의 개수와 피연산자의 첫 자리만 빼면 개수는 같다.
-    private int operatorCnt = 1;
 
     public Operate(List<Integer> operands, List<String> operators) {
         result = operands.get(0); // 첫 자리 값 셋팅;
+        int operatorCnt = 1;
+
         for (String operator : operators) {
-            operate(operands, operator);
+            // 연산자의 개수(초기 값 1)
+            // 연산자의 개수와 피연산자의 첫 자리만 빼면 개수는 같다.
+            int operand = operands.get(operatorCnt);
+            calculate(operand, operator);
             operatorCnt++;
         }
-    }
-
-    private void operate(List<Integer> operands, String operator) {
-        int operand = operands.get(operatorCnt);
-        calculate(operand, operator);
     }
 
     private void calculate(int operand, String operator) {
