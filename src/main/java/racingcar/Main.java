d33fda2537d117a74f41844e551c31b04bcfaae2
catsbi <hansolsolsol@mosition.co.kr>
2020-11-04 15:27:24 +0900

    [refactor] 공백 삽입으로 가독성 향상

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -2,13 +2,12 @@ package racingcar;
 
 import racingcar.game.RacingGameImpl;
 import racingcar.game.RacingInfomation;
-import racingcar.strategy.CarNameInputStrategy;
 import racingcar.strategy.MoveStrategy;
 import racingcar.strategy.PrintMarkStrategy;
+import racingcar.strategy.RacingInfoInputStrategy;
+import racingcar.util.UtilContainer;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
-
-import java.util.Random;
 
 public class Main {
 
@@ -16,9 +15,12 @@ public class Main {
         RacingGameImpl game = new RacingGameImpl();
         InputView inputView = InputView.getInstance();
         ResultView resultView = ResultView.getInstance();
-        RacingInfomation racingInfomation = inputView.execute(new CarNameInputStrategy());
-        game.execute(racingInfomation, MoveStrategy.defaultStrategy(new Random()));
-        resultView.viewAll(game.getGameHistory(), new PrintMarkStrategy("-"));
 
+
+        RacingInfomation racingInfomation = inputView.execute(new RacingInfoInputStrategy());
+
+        game.execute(racingInfomation, MoveStrategy.defaultStrategy(UtilContainer.getRandom()));
+
+        resultView.viewAll(game.getGameHistory(), new PrintMarkStrategy("-"));
     }
 }
