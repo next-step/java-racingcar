752fdb25c88e275b2af8979f335ae5b9b4403fc0
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-05 11:03:39 +0900

    [feat] : ResultView 구현 CarRacing main 클래스 작성

---
+++ src/main/java/racing/CarRacing.java
@@ -0,0 +1,7 @@
+package racing;
+
+public class CarRacing {
+    public static void main(String[] args) {
+        new ResultView().play(new Racing().start());
+    }
+}
---
+++ src/main/java/racing/ResultView.java
@@ -0,0 +1,17 @@
+package racing;
+
+public class ResultView {
+
+    private static final String EXECUTE_RESULT = "실행 결과";
+
+    public void play(Grid grid) {
+        System.out.printf("\n");
+        System.out.printf("%s \n",EXECUTE_RESULT);
+        grid.getRaceRecord().stream().forEach(e -> print(e));
+    }
+
+    public void print(String input) {
+        System.out.printf("%s",input);
+    }
+
+}
