7ad49e2a637f44fd932e5e027cf02d79280032bc
고정완 (Can) <jw.gho@ecubelabs.com>
2020-11-06 08:48:57 +0900

    [refactor]: mementoList 가 read only 되도록 함

--- src/main/java/racingcar/History.java
+++ src/main/java/racingcar/History.java
@@ -11,13 +11,34 @@ public class History {
         mementoList = new LinkedList<>();
     }
 
+    private History(List<Memento> mementoList) {
+        this.mementoList = mementoList;
+    }
+
     public void add(Memento memento) {
         mementoList.add(memento);
     }
 
+    public List<Memento> cloneMementoList() {
+        List<Memento> mementoList = new LinkedList<>();
+        for (Memento memento : mementoList) {
+            mementoList.add(
+                    memento.clone()
+            );
+        }
+        return mementoList;
+    }
+
     public List<Memento> getMementoList() {
+        List<Memento> mementoList = cloneMementoList();
         return Collections.unmodifiableList(
-          new LinkedList(mementoList)
+                new LinkedList(mementoList)
         );
+    }
+
+    @Override
+    public History clone() {
+        List<Memento> mementoList = cloneMementoList();
+        return new History(mementoList);
     }
 }
