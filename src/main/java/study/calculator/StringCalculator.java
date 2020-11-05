22a907e175ea2d01c5fc1964edecd38183d411f8
mimimjin <babysodapop21@hotmail.com>
2020-11-05 21:57:09 +0900

    refactor: 문자열 입력 검증 메소드 분리

--- src/main/java/study/calculator/StringCalculator.java
+++ src/main/java/study/calculator/StringCalculator.java
@@ -1,73 +1,72 @@
 package study.calculator;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StringCalculator {
     private static final String CALCULATOR_DELIMITER = " ";
+    private static final String OPERATOR_PATTERN = "([+\\-*\\/])";
+    private static final String OPERAND_PATTERN = "(-?\\d+)";
 
     public static String getCalculationResult(String toCalculateValue){
-        isEmptyInput(toCalculateValue);
 
-        List<Integer> operand = new ArrayList<Integer>();
-        List<String> operator = new ArrayList<String>();
+        String[] calculateArray = splitCalculateValue(toCalculateValue);
 
-        splitCalculateValue(toCalculateValue, operand, operator);
+        int calculationResult = Integer.parseInt(calculateArray[0]);
 
-        int calculationResult = 0;
-        int operandSize = operand.size();
-        int operatorSize = operator.size();
-
-
-        if(operandSize != operatorSize+1){
-            throw new IllegalArgumentException(CustomErrorMessage.UNCOMPLETED_INPUT.getErrorMessage());
+        for (int i = 1; i < calculateArray.length; i += 2) {
+            String operatorValue = calculateArray[i];
+            int rightValue = Integer.parseInt(calculateArray[i + 1]);
+            CalculateSet calculateSet = new CalculateSet(calculationResult, rightValue);
+            calculationResult = Operator.calculate(operatorValue,calculateSet);
         }
 
-        for(int j=0; j<operandSize; j++){
-            if(j == operandSize - 1){
-                break;
-            }else{
-                CalculateSet calculateSet;
-                if(j == 0){
-                    calculateSet = new CalculateSet(operand.get(j), operand.get(j+1));
-                }else{
-                    calculateSet = new CalculateSet(calculationResult, operand.get(j+1));
-                }
-                String operatorValue = operator.get(j);
-                calculationResult = Operator.calculate(operatorValue,calculateSet);
-                
-            }
-        }
-        
         return String.valueOf(calculationResult);
     }
 
-    private static void splitCalculateValue(String toCalculateValue, List<Integer> operand, List<String> operator) {
+
+    private static String[] splitCalculateValue(String toCalculateValue) {
+        isEmptyInput(toCalculateValue);
+
         String[] input = toCalculateValue.split(CALCULATOR_DELIMITER);
-        int inputLength = input.length;
 
-        for(int i=0; i<inputLength; i++){
-            if(i%2 == 0){
-                isNumericString(input[i]);
-                operand.add(Integer.parseInt(input[i]));
-            }else{
-                isCorrectOperator(input[i]);
-                operator.add(input[i]);
+        isCorrectOperator(input);
 
-            }
+        isNumericString(input);
+
+        isUncompletedInput(input);
+
+        return input;
+    }
+
+    private static void isUncompletedInput(String[] input) {
+        if(input[input.length-1].matches(OPERATOR_PATTERN)){
+            throw new IllegalArgumentException(CustomErrorMessage.UNCOMPLETED_INPUT.getErrorMessage());
         }
     }
 
-    private static void isCorrectOperator(String input) {
-        if(!input.matches("([+\\-*\\/])")){
-            throw new IllegalArgumentException(CustomErrorMessage.ILLEGAL_OPERATOR.getErrorMessage());
-        }
+    private static void isCorrectOperator(String[] input) {
+        IntStream.range(0, input.length)
+                .filter(i -> i%2 != 0)
+                .mapToObj(i -> input[i])
+                .filter(j -> j.matches(OPERATOR_PATTERN))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(CustomErrorMessage.ILLEGAL_OPERATOR.getErrorMessage()));
     }
 
-    private static void isNumericString(String input) {
-        if(!input.matches("-?\\d+")){
-            throw new IllegalArgumentException(CustomErrorMessage.ILLEGAL_OPERAND.getErrorMessage());
-        }
+    private static void isNumericString(String[] input) {
+         List<String> operands = IntStream.range(0, input.length)
+                .filter(a -> a%2 == 0)
+                .mapToObj(a -> input[a]).collect(Collectors.toList());
+
+         operands.stream().forEach(x -> {
+             if(!x.matches(OPERAND_PATTERN)){
+                 throw new IllegalArgumentException(CustomErrorMessage.ILLEGAL_OPERAND.getErrorMessage());
+             }
+         });
     }
 
 
@@ -77,3 +76,4 @@ public class StringCalculator {
         }
     }
 }
+
