06f1f097e70fca244605507961291824377c781e
홍성민 <honggasm@gmail.com>
2020-11-05 10:43:27 +0900

    test: 다수의 우승자 선정 테스트 추가

--- src/test/java/step3/domain/RacingMapTest.java
+++ src/test/java/step3/domain/RacingMapTest.java
@@ -1,6 +1,7 @@
 package step3.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -10,18 +11,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingMapTest {
     @DisplayName("우승자 선정")
-    @Test
-    void selectWinnerNames() {
-        // given
-        final List<RacingCar> racingCars = RacingCarFactory.createCars(Arrays.asList("CAR1", "CAR2", "CAR3"), () -> true);
-        final RacingMap racingMap = new RacingMap(racingCars);
-        final RacingCar car1 = racingCars.get(0);
+    @Nested
+    class SelectWinnerNames {
+        @DisplayName("한명")
+        @Test
+        void single() {
+            // given
+            final List<RacingCar> racingCars = RacingCarFactory.createCars(Arrays.asList("CAR1", "CAR2", "CAR3"), () -> true);
+            final RacingMap racingMap = new RacingMap(racingCars);
+            final RacingCar car1 = racingCars.get(0);
 
-        // when
-        racingMap.move(car1, 1);
+            // when
+            racingMap.move(car1, 1);
 
-        // then
-        final List<String> winners = racingMap.selectWinnerNames();
-        assertThat(winners.get(0)).isEqualTo(car1.getName());
+            // then
+            final List<String> winners = racingMap.selectWinnerNames();
+            assertThat(winners.get(0)).isEqualTo(car1.getName());
+        }
+
+        @DisplayName("두명 이상")
+        @Test
+        void selectWinnerNames() {
+            // given
+            final List<RacingCar> racingCars = RacingCarFactory.createCars(Arrays.asList("CAR1", "CAR2", "CAR3"), () -> true);
+            final RacingMap racingMap = new RacingMap(racingCars);
+            final RacingCar car1 = racingCars.get(0);
+            final RacingCar car2 = racingCars.get(1);
+
+            // when
+            racingMap.move(car1, 1);
+            racingMap.move(car2, 1);
+
+            // then
+            final List<String> winners = racingMap.selectWinnerNames();
+            winners.sort(String::compareTo);
+            assertThat(winners).isEqualTo(Arrays.asList(car1.getName(), car2.getName()));
+        }
     }
 }
