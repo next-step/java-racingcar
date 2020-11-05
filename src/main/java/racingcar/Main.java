3d7680d9f6a9900fcb9e595790b71bf79cdd6568
고정완 (Can) <jw.gho@ecubelabs.com>
2020-11-05 12:00:34 +0900

    [style]: Main 클래스 인덴트 수정

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -4,20 +4,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
         InputView inputView = InputView.getInstance();
-
         String nameCsv = inputView.askCars();
         int maxRaceNum = inputView.askMaxRaceNum();
+
         RacingGame game = new RacingGame(nameCsv, maxRaceNum);
+        game.play();
 
         RacingView racingView = RacingView.getInstance();
         racingView.printResultMsg();
 
-        game.play();
-
         List<List<Car>> history = game.getHistory();
         racingView.printHistory(history);
-        racingView.printWinners(game.getWinners());
+
+        List<Car> winners = game.getWinners();
+        racingView.printWinners(winners);
     }
 }
