837fab78491c9e96f8747cdadaaee1af4b0f4718
Jung-Soo Ahn <staringstar@gmail.com>
2020-11-05 23:47:29 +0900

    step3 - test fix typo

--- src/test/java/study/step3/CarRacingTest.java
+++ src/test/java/study/step3/CarRacingTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 class CarRacingTest {
 
     @Test
-    @DisplayName("자동차 경주는 경주결과를 출력하는 ResultView를 받을 수 있다.")
+    @DisplayName("자동차 레이싱 결과에 에러가 없다.")
     void acceptableResultView() {
         CarRacing carRacing = new CarRacing(new RacingInput(3, 3), new ResultView());
         assertDoesNotThrow(() -> carRacing.run());
