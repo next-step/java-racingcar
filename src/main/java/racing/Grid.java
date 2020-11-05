829e001cf13147837bc1e431cc5e3a992ce18968
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-05 10:56:44 +0900

    [feat] : 그리드 객체 추가, 레이스 라인업, 스탠딩 스타드, 레이싱 기록 Builder 패턴 구현

---
+++ src/main/java/racing/Grid.java
@@ -0,0 +1,46 @@
+package racing;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Grid {
+
+    private List<Car> statingGrid;
+    private final LinkedList<String> raceRecording;
+    private static int round = 1;
+
+    public Grid(LinkedList<String> raceRecording) {
+        this.raceRecording = raceRecording;
+    }
+
+    public Grid lineUp(int numberOfCars) {
+        round = 1;
+        this.statingGrid = IntStream.range(0,numberOfCars)
+                        .mapToObj(pitOut -> new Car())
+                        .collect(Collectors.toList());
+        return this;
+    }
+
+    public Grid standingStart(int rounds) {
+        while(round <= rounds) {
+            statingGrid = statingGrid.stream()
+                    .peek(Car::move)
+                    .peek(Car -> raceRecording.add(Car.skidMark()))
+                    .collect(Collectors.toList());
+            round++;
+            raceRecording.add("\n");
+        }
+        return this;
+    }
+
+    public LinkedList<String> getRaceRecord() {
+        return raceRecording;
+    }
+
+    public Grid finished() {
+        return new Grid(raceRecording);
+    }
+}
