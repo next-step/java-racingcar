e5f63a8f7de05ae198a67878422c9fc6d74db116
kaeum <oror1024@gmail.com>
2020-11-05 09:19:28 +0900

    refactor: 의미가 모호했던 Constants의 변수명 변경

--- src/main/java/racingcar/common/Constants.java
+++ src/main/java/racingcar/common/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
 
     public static final String ASK_NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
     public static final String ASK_NUMBER_OF_TRIES = "시도할 회수는 몇 회 인가요?";
-    public static final String PRINT_PREFIX = "실행 결과";
+    public static final String PRINT_RESULT = "실행 결과";
 }
--- src/main/java/racingcar/ui/ResultView.java
+++ src/main/java/racingcar/ui/ResultView.java
@@ -5,7 +5,7 @@ import racingcar.common.Constants;
 public class ResultView {
 
     public void print(String str) {
-        System.out.println(Constants.PRINT_PREFIX);
+        System.out.println(Constants.PRINT_RESULT);
         System.out.print(str);
     }
 }
