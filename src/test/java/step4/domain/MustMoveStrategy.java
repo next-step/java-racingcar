fa8f099876a08702a2bf9061bdaa7c791743e1ff
oen142 <oen142@naver.com>
2020-11-05 23:59:19 +0900

    test fix : 경계값 테스트

--- src/test/java/step4/domain/MustMoveStrategy.java
+++ src/test/java/step4/domain/MustMoveStrategy.java
@@ -3,6 +3,11 @@ package step4.domain;
 
 public class MustMoveStrategy implements MoveStrategy {
     @Override
+    public boolean isMoved(int number) {
+        return true;
+    }
+
+    @Override
     public boolean isMoved() {
         return true;
     }
--- src/test/java/step4/domain/NotMoveStrategy.java
+++ src/test/java/step4/domain/NotMoveStrategy.java
@@ -4,6 +4,11 @@ package step4.domain;
 public class NotMoveStrategy implements MoveStrategy {
 
     @Override
+    public boolean isMoved(int number) {
+        return true;
+    }
+
+    @Override
     public boolean isMoved() {
         return false;
     }
