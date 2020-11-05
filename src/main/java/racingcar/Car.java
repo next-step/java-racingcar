acd1d2f411696aabd5d12eccfc3245e7a972db72
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-06 01:30:58 +0900

    feat : 0-9 사이의 랜덤값 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,9 +1,16 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Car {
     private final boolean[] moves;
 
     public Car(int attempts) {
         this.moves = new boolean[attempts];
+    }
+
+    private int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(10);
     }
 }
