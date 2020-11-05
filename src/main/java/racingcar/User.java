4b090394547647c2d52d45d8ca6e06958353199f
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-05 17:30:25 +0900

    test : 전체 로직 테스트

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class User {
     public static void InputCarCount(){
-        Game.setCars(new Scanner(System.in).nextInt());
+        Game.setCars(Integer.parseInt(new Scanner(System.in).nextLine()));
     }
 
     public static void InputRoundCount(){
-        Game.setRoundCount(new Scanner(System.in).nextInt());
+        Game.setRoundCount(Integer.parseInt(new Scanner(System.in).nextLine()));
     }
 }
--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -1,2 +1,30 @@
-package racingcar;public class GameTest {
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.Assert.assertTrue;
+
+public class GameTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"3,5"})
+    public void 게임플레이(int carCount, int roundCount){
+        System.out.println(carCount);
+        System.out.println(roundCount);
+        InputView.viewCountCarInputMessage();
+        Game.setCars(carCount);
+
+        InputView.viewCoutRoundInputMessage();
+        Game.setRoundCount(roundCount);
+
+        ResultView.viewResultMessage();
+
+        for (int i = 0; i < roundCount; i++) {
+            Game.playRound();
+        }
+
+    }
 }
