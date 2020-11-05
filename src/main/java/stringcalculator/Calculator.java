ce8814e938612d6987dd39c4067a5ef264884791
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-05 11:19:33 +0900

    refactor : 코드 리뷰 반영

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -5,8 +5,7 @@ public class Calculator {
     public int calculate(String input) {
         Expression expression = new Expression(input);
         int result = expression.getInteger(0);
-        int expressionLength = expression.getLength() - 1;
-        for (int i = 1; i < expressionLength ; i++) {
+        for (int i = 1, length = expression.getLength() -1 ; i < length ; i++) {
             Operator operator = Operator.getOperator(expression.get(i));
             result = operator.calculate(result, expression.getInteger(++i));
         }
