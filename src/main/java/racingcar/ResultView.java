9f150c15f7b88434909bef5772eb88f5f6c99498
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-05 14:37:53 +0900

    feat : 결과페이지 공백 추가

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -3,6 +3,7 @@ package racingcar;
 public class ResultView {
     public static void viewResultMessage() {
         System.out.println("실행 결과");
+        System.out.println();
     }
 
     public static void drawTravelDistance() {
