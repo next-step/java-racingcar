eedc3dbcfe3b874c461888770cc84a9de22ff1f3
dbeod2 <darung.0211@gmail.com>
2020-11-05 22:24:08 +0900

    Refactor : RacingCar 클래스명 변경 -> CarService

---
+++ src/main/java/step3/CarService.java
@@ -0,0 +1,37 @@
+package step3;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CarService {
+    public void run(int car, int move) {
+        validate(car, move);
+
+        Set<Car> cars = IntStream.range(0, car)
+                .mapToObj(i -> new Car())
+                .collect(Collectors.toSet());
+
+
+        System.out.println("실행결과");
+        IntStream.range(0, move)
+                .mapToObj(i -> cars)
+                .forEach(this::move);
+    }
+
+    public void move(Set<Car> cars) {
+        cars.forEach(Car::move);
+        ResultView.resultPrint(cars);
+    }
+
+    private void validate(int car, int move) {
+        if (car == 0) {
+            throw new IllegalArgumentException("자동차 수가 0입니다.");
+        }
+
+        if (move == 0) {
+            throw new IllegalArgumentException("이동횟수가 0입니다.");
+        }
+    }
+
+}
