97cb4ef674195ab870a064615582c05ddda624c8
kaeum <oror1024@gmail.com>
2020-11-05 10:33:33 +0900

    refactor: RandomNumberGenerator에 인스턴스 제공 로직 제거 - static method인 generate()만 제공하는 클래스로 변경

--- src/main/java/racingcar/common/RandomNumberGenerator.java
+++ src/main/java/racingcar/common/RandomNumberGenerator.java
@@ -5,16 +5,6 @@ import java.util.Random;
 public class RandomNumberGenerator {
     private static final int MAX_BOUNDARY = 10;
     private static Random random = new Random();
-    private static RandomNumberGenerator instance;
-
-    private RandomNumberGenerator(){}
-
-    public static RandomNumberGenerator getInstance(){
-        if(instance == null)
-            instance = new RandomNumberGenerator();
-
-        return instance;
-    }
 
     public static int generate(){
         return random.nextInt(MAX_BOUNDARY);
