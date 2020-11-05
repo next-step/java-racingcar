8c642d060ac514c63e837e96ca706682fe4e6af1
MJ Choi <1621139@gmail.com>
2020-11-05 10:23:15 +0900

    "style: Delete omit lines and formatting "

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,19 +1,15 @@
 package racingcar.domain;
 
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import racingcar.behavior.MovingStrategy;
-import racingcar.util.ErrorMessage;
 
 public class RacingCars {
-    private static final int MIN_NUM_OF_CARS = 0;
 
-    private List<Car> cars;
-    private MovingStrategy movingStrategy;
+    private  List<Car> cars;
+    private  MovingStrategy movingStrategy;
 
     public RacingCars(List<Car> cars, MovingStrategy movingStrategy) {
         this.cars = cars;
@@ -22,23 +18,8 @@ public class RacingCars {
 
     public static RacingCars of(int numOfCar, MovingStrategy movingStrategy) {
         List<Car> initCars = createCars(numOfCar);
-//        for(int i=0; i<numOfCar; i++){
-//            initCars.add(new Car());
-//        }
         return new RacingCars(initCars, movingStrategy);
     }
-
-//    private RacingCars(final int numOfCar, final MovingStrategy movingStrategy) {
-//        validateInput(numOfCar, movingStrategy);
-//        this.movingStrategy = movingStrategy;
-//        createCars(numOfCar);
-//    }
-
-//    private void validateInput(final int numOfCar, final MovingStrategy movingStrategy) {
-//        if (numOfCar <= MIN_NUM_OF_CARS || movingStrategy == null) {
-//            throw new IllegalArgumentException(ErrorMessage.INVALID_PARAMETER);
-//        }
-//    }
 
     public static List<Car> createCars(int numOfCar) {
         return Stream.generate(Car::new)
