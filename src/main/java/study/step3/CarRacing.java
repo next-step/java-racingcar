811184ee86c548a657afeed87c2e15cf3e9de40c
Jung-Soo Ahn <staringstar@gmail.com>
2020-11-05 23:33:32 +0900

    step3 - refactor cars 를 내부에서 관리하도록 수정

--- src/main/java/study/step3/CarRacing.java
+++ src/main/java/study/step3/CarRacing.java
@@ -4,7 +4,16 @@ import java.util.*;
 
 public class CarRacing {
 
-    public List<Integer> initRacing(int cars) {
+    private final List<Integer> cars;
+    private ResultView printer;
+    private int round = 0;
+
+    public CarRacing(int countOfCars, ResultView printer) {
+        this.cars = initRacing(countOfCars);
+        this.printer = printer;
+    }
+
+    private List<Integer> initRacing(int cars) {
         return new ArrayList<>(Collections.nCopies(cars, 0));
     }
 
@@ -13,25 +22,28 @@ public class CarRacing {
         InputView inputView = new InputView();
         RacingInput racingInput = inputView.getRacingInput();
 
-        CarRacing cr = new CarRacing();
+        CarRacing cr = new CarRacing(racingInput.getCountOfCar(), new ResultView());
         List<Integer> cars = cr.initRacing(racingInput.getCountOfCar());
-
-        ResultView resultView = new ResultView();
 
         int round = 0;
 
         while (round++ < racingInput.getCountOfRound()) {
-            cr.processRound(cars);
-            resultView.printResult(round, cars);
+            cr.processRound();
+            cr.printResult();
         }
     }
 
-    public void processRound(List<Integer> cars) {
+    public void printResult() {
+        printer.printResult(round, cars);
+    }
+
+    public void processRound() {
         for (int i = 0; i < cars.size(); i++) {
             if (getRandomNumber() >= 4) {
                 cars.set(i, cars.get(i) + 1);
             }
         }
+        round++;
     }
 
     private int getRandomNumber() {
