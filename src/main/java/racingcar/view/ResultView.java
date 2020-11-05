11e1b2b05516c855d21d9b4473e290f1500f6bbe
MJ Choi <1621139@gmail.com>
2020-11-05 09:14:51 +0900

    "refactor: ADD Input, Result View"

---
+++ src/main/java/racingcar/view/ResultView.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import java.util.List;
+
+import racingcar.domain.Car;
+
+public class ResultView {
+    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String NEW_LINE = "\n";
+    private static final String POSITION_MARK = "-";
+
+    public static void printResultMessage() {
+        System.out.println(NEW_LINE + RESULT_MESSAGE);
+    }
+
+    public static void printResult(final List<Car> cars) {
+        System.out.println();
+        cars.forEach(ResultView::printPosition);
+    }
+
+    public static String printPosition(final Car car) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < car.getPosition(); i++) {
+            stringBuilder.append(POSITION_MARK);
+        }
+        return stringBuilder.toString();
+    }
+}
