d9f406f680db4c86b9463ee0e5ba1df99f524eef
belle.bonjour <belle.bonjour@kakaocorp.com>
2020-11-05 17:14:36 +0900

    Refactor : RacingCarApplication 생성

---
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -0,0 +1,44 @@
+package racingcar;
+
+import racingcar.external.InputView;
+import racingcar.external.ResultView;
+import racingcar.model.Car;
+import racingcar.model.RaceReadyValue;
+
+import java.util.List;
+
+public class RacingCarApplication {
+
+    private static InputView inputView = new InputView();
+    private static ResultView resultView = new ResultView();
+
+    private static final String INPUT_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
+    private static final String RACE_TREM = "********************";
+
+
+    public static void main(String[] args) {
+        RaceReadyValue raceReadyValue = input();
+
+        List<Car> cars = RacingCarService.initialCars(raceReadyValue.getCarCount());
+
+        for (int x = 0; x < raceReadyValue.getTryCount(); x++) {
+            RacingCarService.startRaceByStep(cars);
+            output(cars);
+        }
+    }
+
+    public static RaceReadyValue input() {
+        int carCount = inputView.input(INPUT_CAR_COUNT);
+        int tryCount = inputView.input(INPUT_TRY_COUNT);
+
+        return new RaceReadyValue(carCount, tryCount);
+    }
+
+    public static void output(List<Car> cars) {
+        for (Car car : cars) {
+            resultView.out(car);
+        }
+        resultView.out(RACE_TREM);
+    }
+}
