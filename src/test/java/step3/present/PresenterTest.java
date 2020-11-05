4e3eb3d617376e283fd97be1b85eb3b5289d0cd9
DongHwan Kim <bkuekany@gmail.com>
2020-11-05 23:48:57 +0900

    test: Refactor 소스 변경에 따른 테스트 코드 수정

--- src/test/java/step3/present/PresenterTest.java
+++ src/test/java/step3/present/PresenterTest.java
@@ -3,30 +3,18 @@ package step3.present;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import step3.RaceGameContract;
 import step3.model.GameRound;
 import step3.model.RaceGame;
 import step3.model.RandomCommander;
 import step3.view.RenderingView;
 
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PresenterTest {
 
-
-    private RenderingView renderingView = new RenderingView();
-    private Presenter presenter = new Presenter(renderingView, new RandomCommander());
-
-    @DisplayName("RaceGame 인스턴스 생성 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"3, 5", "5, 3", "10, 2", "2, 6"})
-    public void createRaceGameTest(int participantCar, int round) {
-        //Given & When
-        RaceGame game = presenter.createGame(new GameRound(participantCar, round));
-
-        //Then
-        assertThat(game).isNotNull();
-
-    }
 
 
     @DisplayName("레이스 게임 시작 및 종료 확인을 위한 테스트")
@@ -34,13 +22,18 @@ public class PresenterTest {
     @CsvSource(value = {"3, 5", "5, 3", "10, 2", "2, 6"})
     public void gameStartTest(int participantCar, int round) {
         //Given
-        RaceGame game = presenter.createGame(new GameRound(participantCar, round));
+        RenderingView renderingView = new RenderingView();
+        RaceGame game = new RaceGame(new GameRound(participantCar, round), new RandomCommander());
+        RaceGameContract.Presenter presenter = new Presenter(renderingView, game);
+
 
         //When
-
+        IntStream.range(0, round)
+                .forEach(i -> presenter.gameStart());
+        int result = renderingView.getExecuteCount();
 
         //Then
-        assertThat(renderingView.getExecuteCount()).isEqualTo(round);
+        assertThat(result).isEqualTo(round);
 
     }
 
