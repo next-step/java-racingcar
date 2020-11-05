200ff17aa1429a82d901811e03b961268fb3d7ee
MJ Choi <1621139@gmail.com>
2020-11-05 15:39:08 +0900

    "style: DELETE omit test lines(random moving strategy)"

--- src/test/java/step3/RandomMovingTest.java
+++ src/test/java/step3/RandomMovingTest.java
@@ -11,14 +11,6 @@ import racingcar.behavior.RandomNumber;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomMovingTest {
-//    @DisplayName("0 부터 양의정수 입력값 이하의 랜덤 정수값을 반환하는지 테스트")
-//    @ParameterizedTest
-//    @ValueSource(ints = {3, 10, 9, 100})
-//    public void shakeRandomBoundTest (int input) {
-//        int result = new RandomMovingStrategy().random();
-//        assertThat(result).isBetween(0, input + 1);
-//    }
-
     @DisplayName("0부터 양의 정수 입력값 이하의 랜덤 정수값을 반환 여부")
     @ParameterizedTest
     @CsvSource(value = {"10:0:9", "15:0:14"}, delimiter = ':')
