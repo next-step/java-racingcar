e87ec9c1091fad4bba34446405448c33ff813627
Jung-Soo Ahn <staringstar@gmail.com>
2020-11-05 22:43:09 +0900

    [step3] - feat Input 값을 담는 객체 생성

---
+++ src/main/java/study/step3/RacingInput.java
@@ -0,0 +1,20 @@
+package study.step3;
+
+public class RacingInput {
+
+    private final int countOfCar;
+    private final int countOfRound;
+
+    public RacingInput(int countOfCar, int countOfRound) {
+        this.countOfCar = countOfCar;
+        this.countOfRound = countOfRound;
+    }
+
+    public int getCountOfCar() {
+        return countOfCar;
+    }
+
+    public int getCountOfRound() {
+        return countOfRound;
+    }
+}
