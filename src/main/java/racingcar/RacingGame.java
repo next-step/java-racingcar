ef48b36d94beb7ffa4c948af6f0cb8a67801f3b7
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-06 01:21:24 +0900

    feat : RacingGame 클래스 추가

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class RacingGame {
+    private final int cars;
+    private final int attempts;
+    private Car car;
+
+    public RacingGame(int cars, int attempts) {
+        this.cars = cars;
+        this.attempts = attempts;
+    }
+}
