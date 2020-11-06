009afbbebc25c13616a201e36147fba47072b8aa
yeGenieee <cyj95428@gmail.com>
2020-11-06 21:45:41 +0900

    사칙 연산 기호가 아닌 경우 및 계산기 기능 테스트 코드 추가

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -36,4 +37,20 @@ class CalculatorTest {
                 .hasMessageContaining("입력값이 null이거나 빈 문자열");
     }
 
+    @ParameterizedTest
+    @DisplayName("사칙 연산 기호가 아닌 경우")
+    @ValueSource(strings = {"4 @ 7", "6 ) 2"})
+    void input_not_arithmetic_operator(String input) {
+        assertThatThrownBy(() -> {
+            calculator.calculate(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("사칙연산 기호가 아님");
+    }
+
+    @ParameterizedTest
+    @DisplayName("계산기 기능 테스트")
+    @CsvSource(value = {"2 + 3 * 4 / 2=10", "4 - 1 * 8 / 4=6"}, delimiter = '=')
+    void calculate(String input, int expected) {
+        assertEquals(calculator.calculate(input), expected);
+    }
 }
