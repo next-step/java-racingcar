8c8df923540f169bbc06bbdff5656b208bb8869f
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-06 23:48:47 +0900

    feat : 입력값만큼 자동차 생산하는 기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,13 +1,28 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
-    private final int cars;
     private final int attempts;
-    private Car car;
+    private final List<Car> carList;
 
     public RacingGame(int cars, int attempts) {
-        this.cars = cars;
         this.attempts = attempts;
-        this.car = new Car(attempts);
+        this.carList = makeCars(cars);
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public List<Car> makeCars(int cars) {
+        List<Car> carList = new ArrayList<>();
+        for (int i = 0; i < cars ; i++) {
+            Car car = new Car(attempts);
+            car.generateMoves();;
+            carList.add(car);
+        }
+        return carList;
     }
 }
