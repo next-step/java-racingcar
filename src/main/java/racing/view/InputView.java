69f2b18656d0f1f6c609a3d10dca52a18e844c9c
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-06 19:33:19 +0900

    [refactor] : InputView 상태값 저장한 전역 변수 제거, 정적 메소드 호출 시 값을 리턴받도록 수정

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -1,58 +1,52 @@
 package racing.view;
 
 import java.util.Scanner;
+import static racing.view.InputViewMessage.*;
 
 public class InputView {
 
-    private static int manyCars = 0;
-    private static int tryCount = 0;
     private static final String numberRegex = "^[0-9]*$";
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public InputView() {
-        Scanner scanner = new Scanner(System.in);
+    private InputView() {}
 
+    public static int getManyCars() {
+        int manyCars;
         do {
-            System.out.print(InputViewMessage.FIRST_QUESTION);
-            manyCars = inputValidationCheck(scanner.next());
-        } while (manyCars <= 0);
-
-        do {
-            System.out.print(InputViewMessage.SECOND_QUESTION);
-            tryCount = inputValidationCheck(scanner.next());
-        } while (tryCount <= 0 );
-
-        scanner.close();
+            System.out.print(FIRST_QUESTION);
+        } while ((manyCars = inputValidationCheck(scanner.next())) <= 0);
+        return manyCars;
     }
 
-    private int inputValidationCheck(String input) {
+    public static int getLabs() {
+        int labs;
+        do {
+            System.out.print(SECOND_QUESTION);
+        } while ((labs = inputValidationCheck(scanner.next())) <= 0 );
+        return labs;
+    }
+
+    private static int inputValidationCheck(String input) {
         if(numberFormatCheck(input) == 0) {
             return 0;
         }
         return minimumValueCheck(input);
     }
 
-    private int minimumValueCheck(String input) {
+    private static int minimumValueCheck(String input) {
         int value = Integer.parseInt(input);
         if(value <= 0)
-            System.out.print(InputViewMessage.INPUT_VALIDATION_ERROR);
+            System.out.print(INPUT_VALIDATION_ERROR);
 
         return value;
     }
 
-    private int numberFormatCheck(String input) {
+    private static int numberFormatCheck(String input) {
         if(!input.matches(numberRegex)) {
-            System.out.print(InputViewMessage.INPUT_FORMAT_VALIDATION_ERROR);
+            System.out.print(INPUT_FORMAT_VALIDATION_ERROR);
             return 0;
         }
         return minimumValueCheck(input);
-    }
-
-    public static int getManyCars() {
-        return manyCars;
-    }
-
-    public static int getTryCount() {
-        return tryCount;
     }
 
 }
