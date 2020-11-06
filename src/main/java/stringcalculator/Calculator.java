3b8c9ee4eba4d20081a7818d549e05e41de5b3c4
yeGenieee <cyj95428@gmail.com>
2020-11-06 21:45:06 +0900

    계산기 기능 구현 완료

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -2,13 +2,35 @@ package stringcalculator;
 
 public class Calculator {
 
-    public void input(String input) {
+    public int calculate(String input) {
         if (isNullOrBlank(input)) {
             throw new IllegalArgumentException("입력값이 null이거나 빈 문자열");
         }
+        String [] expression = input.split(" ");
+        int firstOperand = Integer.parseInt(expression[0]);
+
+        for (int i=1; i < expression.length; i = i+2) {
+            firstOperand = calculate(firstOperand, Integer.parseInt(expression[i+1]), expression[i]);
+        }
+
+        return firstOperand;
     }
 
-    boolean isNullOrBlank(String input){
+    private boolean isNullOrBlank(String input){
         return input == null || input.trim().isEmpty();
+    }
+
+    private int calculate(int firstOperand, int secondOperand, String operator) {
+        if (operator.equalsIgnoreCase("+")) {
+            return firstOperand + secondOperand;
+        } else if (operator.equalsIgnoreCase("-")) {
+            return firstOperand - secondOperand;
+        } else if (operator.equalsIgnoreCase("*")) {
+            return firstOperand * secondOperand;
+        } else if (operator.equalsIgnoreCase("/")) {
+            return firstOperand / secondOperand;
+        }
+
+        throw new IllegalArgumentException("사칙연산 기호가 아님");
     }
 }
