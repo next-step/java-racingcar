c51225e702abf4759eb3b824e9339df71ab11757
Jung-Soo Ahn <staringstar@gmail.com>
2020-11-06 23:25:10 +0900

    step3 - refactor input 값의 validation 추가

--- src/main/java/study/racing/view/RacingInput.java
+++ src/main/java/study/racing/view/RacingInput.java
@@ -6,8 +6,24 @@ public class RacingInput {
     private final int countOfRound;
 
     public RacingInput(int countOfCar, int countOfRound) {
+
+        validateCountOfCar(countOfCar);
+        validateCountOfRound(countOfRound);
+
         this.countOfCar = countOfCar;
         this.countOfRound = countOfRound;
+    }
+
+    private void validateCountOfCar(int countOfCar) {
+        if (countOfCar <= 0) {
+            throw new IllegalArgumentException("자동차 수는 0 보다 커야합니다.");
+        }
+    }
+
+    private void validateCountOfRound(int countOfRound) {
+        if (countOfRound <= 0) {
+            throw new IllegalArgumentException("라운드 횟수는 0 보다 커야합니다.");
+        }
     }
 
     public int getCountOfCar() {
--- src/test/java/study/racing/RacingInputTest.java
+++ src/test/java/study/racing/RacingInputTest.java
@@ -1,30 +1,40 @@
 package study.racing;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import study.racing.view.RacingInput;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class RacingInputTest {
-    private RacingInput input;
-
-    @BeforeEach
-    void setUp() {
-        input = new RacingInput(5, 3);
-    }
 
     @Test
     @DisplayName("정상적으로 자동차 수 반환되는지 확인")
-    void checkCountOfCars() {
+    void 자동차_수_확인() {
+
+        RacingInput input = new RacingInput(5, 3);
+
         assertThat(input.getCountOfCar()).isEqualTo(5);
     }
 
     @Test
     @DisplayName("정상적으로 라운드 수 반환되는지 확인")
-    void checkCountOfRound() {
+    void 라운드_수_확인() {
+
+        RacingInput input = new RacingInput(5, 3);
+
         assertThat(input.getCountOfRound()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("자동차 수가 0보다 작다면 예외발생")
+    void 예외가_발생()
+    {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    RacingInput input = new RacingInput(-1, 3);
+                }).withMessageContaining("자동차 수는 0 보다 커야합니다.");
     }
 
 }
