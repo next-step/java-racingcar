74132d5ad711d8b037371f551b9f6bcca7c73372
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-06 23:45:50 +0900

    feat : 랜덤값에 따라 배열에 boolean 저장

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,13 +4,25 @@ import java.util.Random;
 
 public class Car {
     private final boolean[] moves;
+    private final static int MOVE_POINT = 4;
+    private final static int RANDOM_NUMBER_BOUND = 10;
 
     public Car(int attempts) {
         this.moves = new boolean[attempts];
     }
 
+    public boolean[] getMoves() {
+        return moves;
+    }
+
     private int getRandomNumber() {
         Random random = new Random();
-        return random.nextInt(10);
+        return random.nextInt(RANDOM_NUMBER_BOUND);
+    }
+
+    public void generateMoves() {
+        for (int i = 0; i <moves.length ; i++) {
+            moves[i] = getRandomNumber() >= MOVE_POINT;
+        }
     }
 }
