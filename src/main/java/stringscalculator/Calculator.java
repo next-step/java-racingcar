382d31fe1d342f4cdc7b7de5e8c65d794b074e18
yeGenieee <cyj95428@gmail.com>
2020-11-06 21:56:18 +0900

    문자열 계산기 패키지명 변경

--- src/main/java/stringscalculator/Calculator.java
+++ src/main/java/stringscalculator/Calculator.java
@@ -1,4 +1,4 @@
-package stringcalculator;
+package stringscalculator;
 
 public class Calculator {
 
--- src/test/java/stringscalculator/CalculatorTest.java
+++ src/test/java/stringscalculator/CalculatorTest.java
@@ -1,4 +1,4 @@
-package stringcalculator;
+package stringscalculator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
