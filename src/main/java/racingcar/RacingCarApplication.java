8a9bb117323f1e877d69ff601157791e47448965
belle.bonjour <belle.bonjour@kakaocorp.com>
2020-11-07 17:24:28 +0900

    refactor : 불필요한 RacingCarService 클래스 삭제

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -1,47 +1,30 @@
 package racingcar;
 
-import racingcar.external.InputView;
-import racingcar.external.ResultView;
-import racingcar.model.Car;
 import racingcar.dto.RaceReadyValue;
+import racingcar.external.InputChannel;
+import racingcar.external.InputView;
+import racingcar.external.OuputChannel;
+import racingcar.external.ResultView;
+import racingcar.model.Cars;
 import racingcar.model.Motor;
 import racingcar.model.RandomMove;
 
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class RacingCarApplication {
 
-    private static InputView inputView = new InputView();
-    private static ResultView resultView = new ResultView();
-
-    private static final String INPUT_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
-    private static final String INPUT_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
-    private static final String RACE_TREM = "********************";
-
+    private static final int RANGE_START_NUM = 0;
 
     public static void main(String[] args) {
-        RaceReadyValue raceReadyValue = input();
+        RaceReadyValue raceReadyValue = InputView.input(new InputChannel());
         Motor motor = new RandomMove();
 
-        List<Car> cars = RacingCarService.initialCars(raceReadyValue.getCarCount(), motor);
+        Cars cars = Cars.of(raceReadyValue.getCarCount(), motor);
 
-        for (int x = 0; x < raceReadyValue.getTryCount(); x++) {
-            RacingCarService.startRaceByStep(cars);
-            output(cars);
-        }
-    }
-
-    public static RaceReadyValue input() {
-        int carCount = inputView.input(INPUT_CAR_COUNT);
-        int tryCount = inputView.input(INPUT_TRY_COUNT);
-
-        return new RaceReadyValue(carCount, tryCount);
-    }
-
-    public static void output(List<Car> cars) {
-        for (Car car : cars) {
-            resultView.out(car);
-        }
-        resultView.out(RACE_TREM);
+        IntStream.range(RANGE_START_NUM, raceReadyValue.getTryCount())
+                .forEach(i -> {
+                    cars.run();
+                    ResultView.output(new OuputChannel(), cars);
+                });
     }
 }
