d5f04c57cdd12dc65c91d70765470dabf77634f9
rlawls1991 <rlawls1991@gmail.com>
2020-11-08 02:57:30 +0900

    refactor 1. random값을 주는 부분과 이동여부 클래스 분리

--- src/main/java/race/car/Car.java
+++ src/main/java/race/car/Car.java
@@ -1,24 +1,33 @@
 package race.car;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
 public class Car {
+
     private static Random random = new Random();
 
     /**
-     * 자동차가 이동할 것인지 체크
-     * @param paramValue
-     *
+     * 랜덤값 return
      * @return
      */
-    public static int moveCarCheck(int paramValue) {
-        int randomValue = random.nextInt(10);
+    public static int getMovePower() {
+        return random.nextInt(10);
+    }
 
-        if (randomValue >= 4) {
-            return paramValue + 1;
+    /**
+     * 랜덤값을 확인하여 이동거리 +1
+     *
+     * @param moveDistance     이동거리
+     * @param inputRandomValue 받은 랜덤한 값
+     * @return
+     */
+    public static int moveCarCheck(int moveDistance, int carPower) {
+        if (carPower >= 4) {
+            return moveDistance + 1;
         }
-        return paramValue;
+        return moveDistance;
     }
 
 }
