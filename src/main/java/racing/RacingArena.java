88d1b4ae835ae14325a3a24d359b3c4b9ddcb72f
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-07 12:23:58 +0900

    [refactor] : 레이싱 결과 UI 로직 수정, main 클래스 수정

--- src/main/java/racing/RacingArena.java
+++ src/main/java/racing/RacingArena.java
@@ -1,13 +1,18 @@
 package racing;
 
+import racing.domain.Car;
 import racing.view.InputView;
 import racing.view.ResultView;
 
+import java.util.List;
+
 public class RacingArena {
     public static void main(String[] args) {
-        new InputView();
-        int cars = InputView.getManyCars();
-        int rounds = InputView.getTryCount();
-        new ResultView().racingResults(new CarRacing(cars,rounds).start());
+        ResultView resultView = new ResultView();
+
+        int vehicleCount = InputView.getVehicleCount();
+        int maxCheckPoint = InputView.getMaxCheckPoint();
+        List<Car> cars = new CarRacing(vehicleCount,maxCheckPoint).getRacingCars();
+        resultView.racingResults(cars,maxCheckPoint);
     }
 }
--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -1,19 +1,45 @@
 package racing.view;
 
-import racing.domain.Grid;
+import racing.domain.Car;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static racing.domain.CarConfig.*;
 
 public class ResultView {
 
     private static final String EXECUTE_RESULT = "실행 결과";
+    private static final String SKID_MARK = "-";
 
-    public void racingResults(Grid grid) {
+
+    private Map<String,StringBuilder> map = new HashMap<>();
+
+    public void racingResults(List<Car> cars,int maxLaps) {
         System.out.print("\n");
         System.out.printf("%s \n",EXECUTE_RESULT);
-        grid.getRaceRecord().forEach(this::print);
+        System.out.print("\n");
+
+        int laps = 0;
+        do {
+            for(Car car:cars) {
+                parseSkidMark(car.getEcu().findRecord(laps));
+            }
+            System.out.println();
+            laps++;
+        }while (laps < maxLaps);
     }
 
-    public void print(String input) {
-        System.out.printf("%s",input);
+    public void parseSkidMark(String raceRecord) {
+        raceRecord = Pattern.compile(ECU_RECORD_STRING_SEPARATOR)
+                .splitAsStream(raceRecord)
+                .filter(str -> !"0".contains(str))
+                .collect(Collectors.joining());
+        System.out.println(raceRecord.replaceAll(ECU_NORMAL_MOVE_CODE,SKID_MARK));
     }
 
 }
