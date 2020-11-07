0dcf9a6efe98932b584775e5350a2efed8b46f75
SungHyeob Baek <baek9652@naver.com>
2020-11-07 22:34:42 +0900

    [feat] Cars 변경 및 테스트 추가

--- src/main/java/racingGame/Cars.java
+++ src/main/java/racingGame/Cars.java
@@ -1,11 +1,13 @@
 package racingGame;
 
-import racingGame.racingGameException.IllegalCarsInputRange;
-
 import java.util.List;
 import java.util.Vector;
 
 public class Cars {
+
+  /**
+   * 게임에 사용되는 Car 들을 List<Car>의 형태로 가지고 있는 객체. 게임에 사용되는 Car 전체의 상태와 각각의 Car에 접근 가능하게 함
+   */
 
   private final List<Car> cars;
 
@@ -13,37 +15,33 @@ public class Cars {
     this.cars = cars;
   }
 
-  public static Cars of(List<Car> cars) {
+  public static Cars of(List<String> names) {
+    List<Car> cars = new Vector<>();
+    for (int i = 0; i < names.size(); i++) {
+      cars.add(Car.of(names.get(i)));
+    }
+
     return new Cars(cars);
   }
 
-  public static Cars of(int numCar) {
-    if (numCar < 1) {
-      throw new IllegalCarsInputRange();
-    }
-
-    List<Car> cars = new Vector<>();
-    for (int i = 0; i < numCar; i++) {
-      cars.add(new Car());
-    }
-
-    return of(cars);
-  }
-
-  public void moves(ScoreGenerator ScoreGenerator) {
+  // Car가 어떻게 움직일 지 만드는 기능.
+  // 아.. Car의 position 상태 변경에 영향을 줌.
+  // 심지어 외부 Game Client에서 받아서 사용하고 있음
+  // position을 변경하려면 3군대에서 고쳐야 됨. 일단은 Cars 안으로만..
+  // 이후 Car 객체 생성할 때 move를 어떻게 할지 지정하는 것으로 하면 될 듯
+  // Car가 알아서 움직이게 해야됨
+  public void moves() {
+    ScoreGenerator scoreGenerator = new RandomScoreGenerator();
     for (Car car : cars) {
-      car.move(ScoreGenerator.generateScore());
+      car.move(scoreGenerator.generateScore());
     }
   }
 
-  public List<Integer> getPositions() {
-    List<Integer> positions = new Vector<>(cars.size());
-
-    for (int i = 0; i < cars.size(); i++) {
-      positions.add(i, cars.get(i).getPosition());
-    }
-
-    return positions;
+  public Car get(int index) {
+    return cars.get(index);
   }
 
+  public int size() {
+    return this.cars.size();
+  }
 }
--- src/test/java/racingGame/CarsTest.java
+++ src/test/java/racingGame/CarsTest.java
@@ -1,27 +1,50 @@
 package racingGame;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.Vector;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CarsTest {
 
-  @DisplayName("차량 초기상태 확인")
-  @ParameterizedTest
-  @ValueSource(ints = {1, 2, 3, 4})
-  void testNumCars(int input) {
-    List<Integer> results = new Vector<>(input);
-    for (int i = 0; i < input; i++) {
-      results.add(0);
-    }
+  private Cars sampleCars;
+  private List<String> names;
 
-    assertThat(Cars.of(input).getPositions()).isEqualTo(results);
+  @BeforeEach
+  void setUp() {
+    names = new Vector<>();
+    names.add("a");
+    names.add("b");
+    names.add("c");
+    names.add("d");
+    names.add("e");
+
+    this.sampleCars = Cars.of(names);
   }
 
+  @Test
+  @DisplayName("Car의 갯수 확인")
+  void CarsInitializedStatus() {
+    assertThat(sampleCars.size()).isEqualTo(this.names.size());
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {0, 1, 2, 3, 4})
+  @DisplayName("Car의 초기 상태 확인(위치)")
+  void initialPositions(int location) {
+    assertThat(this.sampleCars.get(location).getPosition()).isEqualTo(0);
+  }
+
+  @Test
+  @DisplayName("Car들의 초기 상태 확인")
+  void initialNames() {
+    for (int i = 0; i < this.sampleCars.size(); i++) {
+      assertThat(this.sampleCars.get(i).getName()).isEqualTo(names.get(i));
+    }
+  }
 }
