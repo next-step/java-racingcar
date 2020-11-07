e392c13f49fdcdb4c6e52e2149bc4bdffb10a660
eunji <carrot225@naver.com>
2020-11-07 19:25:03 +0900

    의견내용 수정 및 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -34,12 +38,35 @@ public class SetTest {
         assertThat(numbers.contains(2)).isTrue();
         assertThat(numbers.contains(3)).isTrue();
     }
+    @Test
+    @DisplayName("assertAll 사용 ,assertAll()은 여러 검증을 하나로 묶어서 테스트 할 수 있게 해준다.")
+    void assertAllTest() {
+        assertAll(
+                //A Case
+                () -> assertThat(numbers.contains(1)).isTrue(),
+
+                //B Case
+                () -> assertThat(numbers.contains(2)).isTrue(),
+
+                //C Case
+                () -> assertThat(numbers.contains(3)).isTrue()
+                );
+    }
 
     @DisplayName("ParameterizedTest를 활용해 중복 코드를 제거")
     @ParameterizedTest
     @ValueSource(ints = { 1, 2, 3 })
     void parameterizedTest(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+
+    @DisplayName("ParameterizedTest를 활용해 예측 값도 Parameterized")
+    @ParameterizedTest
+    @CsvSource({"test,TEST", "tEst,TEST", "Java,JAVA"})
+    void toUpperCase_ShouldGenercontainsateTheExpectedUppercaseValue(String input, String expected) {
+        String actualValue = input.toUpperCase();
+        assertEquals(expected, actualValue);
     }
 
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,18 +10,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
     //@Test 메서드가 호출할 때 마다 새로운 인스턴스를 생성하여 독립적인 테스트가 이루어지게 한다.
     @Test
-    @DisplayName("split 테스트 ")
+    @DisplayName("split 테스트 " +
+            ", contains: 순서와 상관 없이 실제 그룹이 주어진 값들을 포함하고 있는지를 테스트, " +
+            "containsExactly: 순서까지 고려해서 실제 그룹이 주어진 값들을 포함하고 있는지를 테스트 정확하게 일치해야함")
     void split(){
         String[] result = "1,2".split(",");
-        assertThat(result).contains("1");//순서와 상관 없이 실제 그룹이 주어진 값들을 포함하고 있는지를 테스트
-        assertThat(result).containsExactly("1,2"); //순서까지 고려해서 실제 그룹이 주어진 값들을 포함하고 있는지를 테스트 정확하게 일치해야함
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1,2"); //
     }
     @Test
-    @DisplayName("substring 테스트")
+    @DisplayName("substring 테스트" +
+            "isEqualTo :값이 같은지 검증하기(주소값 아님)")
     void substring(){
         String str = "(1,3)";
         String result = str.substring(1,str.length()-1);
-        assertThat(result).isEqualTo("1,2");//값이 같은지 검증하기(주소값 아님)
+        assertThat(result).isEqualTo("1,2");
     }
     @Test
     @DisplayName("charAt 테스트")
