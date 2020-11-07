701905408f001dbf452fac3ab707e7a566d45278
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-07 14:15:14 +0900

    feat:input으로 받는값은 숫자인지 체크하는 로직 및 테스트 추가

--- src/test/java/nextstep/step3/view/InputViewTest.java
+++ src/test/java/nextstep/step3/view/InputViewTest.java
@@ -1,5 +1,6 @@
 package nextstep.step3.view;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,10 +19,19 @@ public class InputViewTest {
 	}
 
 	@Test
-	@DisplayName("자동차 레이싱게임 게임시도 횟 입력메시지 테스트")
+	@DisplayName("자동차 레이싱게임 게임시도 횟수 입력메시지 테스트")
 	public void inputMessageTryNumberTest() {
 		InputView underTest = new InputView(new Scanner("5"));
 		Integer carNumber = underTest.getInputMessageTryNumber();
 		assertThat(carNumber).isEqualTo(5);
+	}
+
+	@Test
+	@DisplayName("자동차 레이싱게임 차와 시도횟수는 숫자만 입력가능하다. ")
+	public void inputValidateNumberTest() {
+		InputView underTest = new InputView(new Scanner("abc"));
+		Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+				.isThrownBy(() -> underTest.getInputMessageCarNumber())
+				.withMessage("자동차수와 시도횟수는 숫자만 가능합니다.");
 	}
 }
