922ec5ec4ae78238586ee59eac66bc6755274dc3
rlawls1991 <rlawls1991@gmail.com>
2020-11-08 03:01:22 +0900

    refactor 자동차의 이름을 토대로 경주 결과를 저장할 수 있게 수정

--- src/main/java/race/car/RunCarRace.java
+++ src/main/java/race/car/RunCarRace.java
@@ -1,37 +1,64 @@
 package race.car;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 public class RunCarRace {
 
-    private static String HOW_MANY_CARS = "자동차 댓수는 몇대인가요?";
+    private static String INPUT_CAR_RACE_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static String HOW_MANY_ATTEMPTS = "시도할 회수는 몇 회 인가요?";
 
-
-    public static void inputValue() {
-        System.out.println(HOW_MANY_CARS);
-        int carCount = NumberInput.inputCount();
+    public static void start() {
+        System.out.println(INPUT_CAR_RACE_NAMES);
+        String[] raceCarNames = InputCarData.checkName(InputCarData.inputValue());
         System.out.println(HOW_MANY_ATTEMPTS);
-        int tryCount = NumberInput.inputCount();
+        int raceTryCount = InputCarData.checkTryCount(InputCarData.inputValue());
 
-        run(carCount, tryCount);
+        RaceResultPrint.runPrintRaceResult(new CarRaceInfo(raceCarNames, runRace(raceCarNames, raceTryCount), raceTryCount));
     }
 
-    public static CarRaceInformation run(int carCount, int tryCount) {
-        return repeatTryMoveCar(new CarRaceInformation(carCount, tryCount));
-    }
+    /**
+     * 경주
+     *
+     * @param raceCarNames
+     * @param raceTryCount
+     * @return
+     */
+    public static HashMap<String, List<CarRaceMoveInfo>> runRace(String[] raceCarNames, int raceTryCount) {
+        HashMap<String, List<CarRaceMoveInfo>> returnCarReacInfoMap = new HashMap<>();
 
-    private static CarRaceInformation repeatTryMoveCar(CarRaceInformation carRaceInformation) {
-        for (int i = 0; i < carRaceInformation.getTryCount(); i++) {
-            repeatCar(carRaceInformation);
-
+        for (String carName : raceCarNames) {
+            returnCarReacInfoMap.put(carName, carRaceMove(raceTryCount, carName));
         }
-        return carRaceInformation;
+
+        return returnCarReacInfoMap;
     }
 
-    private static void repeatCar(CarRaceInformation carRaceInformation) {
-        for (int i = 0; i < carRaceInformation.getCarCount(); i++) {
-            carRaceInformation.getCarMoveCountArr()[i] = Car.moveCarCheck(carRaceInformation.getCarMoveCountArr()[i]);
+    /**
+     * 자동차 레이스 결과를 기록하기
+     *
+     * @param raceTryCount
+     * @param carName
+     */
+    private static List<CarRaceMoveInfo> carRaceMove(int raceTryCount, String carName) {
+        CarRaceMoveInfo carRaceMoveInfo;
+        List<CarRaceMoveInfo> carRaceMoveInfoList = new ArrayList<>();
+
+        // 첫 레이스일때
+        if (carRaceMoveInfoList.size() == 0) {
+            carRaceMoveInfo = new CarRaceMoveInfo(carName, Car.moveCarCheck(0, Car.getMovePower()));
+            carRaceMoveInfoList.add(carRaceMoveInfo);
         }
-        ResultPrint.drawMoveCar(carRaceInformation.getCarMoveCountArr());
-        System.out.print("\n");
+
+        // 두번재레이스 이후
+        for (int i = 1; i < raceTryCount; i++) {
+            carRaceMoveInfo = new CarRaceMoveInfo(carName, Car.moveCarCheck(carRaceMoveInfoList.get(i - 1).getMoveDistance(), Car.getMovePower()));
+            carRaceMoveInfoList.add(carRaceMoveInfo);
+        }
+        return carRaceMoveInfoList;
     }
+
+
 }
