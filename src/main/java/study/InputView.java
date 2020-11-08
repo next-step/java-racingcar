e471e6490e91212b996fa805eefc8de10ce324ee
joongs <luck1004.kim@gmail.com>
2020-11-08 21:16:09 +0900

    update: remove variables of instance and delete test case of InputView

--- src/main/java/study/InputView.java
+++ src/main/java/study/InputView.java
@@ -1,43 +1,13 @@
 package study;
 import java.util.Scanner;
 
-public class InputView {
-    String message;
-    int counts;
-
-    public InputView() {
-        message = "";
-        counts = 0;
-    }
-
-    public InputView(String message) {
-        this.message = message;
-        this.counts = 0;
-    }
-
-    public InputView(String message, int counts) {
-        this.message = message;
-        this.counts = counts;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
-
-    public int getCounts() {
-        return this.counts;
-    }
-
-    public void setCounts(int counts) {
-        this.counts = counts;
-    }
-
-    public void displayMessage() {
+public final class InputView {
+    public static void displayMessage(String message) {
         System.out.println(message);
     }
 
-    public void getUserInput() {
+    public static int getUserInput() {
         Scanner scanner = new Scanner(System.in);
-        setCounts(scanner.nextInt());
+        return scanner.nextInt();
     }
 }
--- src/main/java/study/RacingCarGameManager.java
+++ src/main/java/study/RacingCarGameManager.java
@@ -2,18 +2,25 @@ package study;
 
 public class RacingCarGameManager {
     public static void main(String[] args) {
-        InputView numberOfCar = new InputView("자동차 대수는 몇 대 인가요?");
-        numberOfCar.displayMessage();
-        numberOfCar.getUserInput();
+        InputView.displayMessage("자동차 대수는 몇 대 인가요?");
+        int numberOfCars = InputView.getUserInput();
 
-        InputView numberOfRound = new InputView("시도할 회수는 몇 회 인가요?");
-        numberOfRound.displayMessage();
-        numberOfRound.getUserInput();
+        InputView.displayMessage("시도할 회수는 몇 회 인가요?");
+        int numberOfRounds = InputView.getUserInput();
 
-        RacingCarGame game = new RacingCarGame(numberOfCar.getCounts(), numberOfRound.getCounts());
+        RacingCarGame game = new RacingCarGame(numberOfCars, numberOfRounds);
         game.run();
 
         ResultView resultView = new ResultView();
         resultView.displayResult(game);
     }
 }
+
+/*
+* 하지만 message, counts 같은 class 변수가 필요할 까요??
+사용자로부터 입력을 받아 그 입력을 사용하려고 하는 객체에게 값을 전달하면
+이 객체의 역할은 끝이 납니다 😄
+굳이 InputView가 그 값을 가지고 있을 이유는 많이 없어 보입니다 (자신이 쓰는 곳이 없습니다)
+
+한번 개선 검토 부탁 드립니다 🙇
+* */
