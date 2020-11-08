247481d80f4358c6ef7dc76313cd7fcdb6cfeb4e
jhoon.bak <wecube.1@gmail.com>
2020-11-08 19:21:03 +0900

    feat(InputView): 자동차 대수, 시도할 횟수를 사용자가 입력 할 수 있다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,16 @@
-package racingcar;
+package racingcar.view;
+
+import java.util.Scanner;
 
 public class InputView {
+    Integer carCount;
+    Integer racingCount;
+
+    public void insertCount() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        carCount = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        racingCount = scanner.nextInt();
+    }
 }
