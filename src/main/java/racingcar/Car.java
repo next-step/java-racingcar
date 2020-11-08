22ad64b59b6eb6f05aff38e650c4671ee780b151
changgunyee <minyoung403@naver.com>
2020-11-08 23:37:35 +0900

    refactor: rearrange car max name feature to Car

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,14 +1,29 @@
 package racingcar;
 
+import java.util.InputMismatchException;
+
 public class Car {
 
     public static final int INITIAL_POSITION = 0;
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
     private int position;
 
     public Car(String name) {
+        checkNameLength(name);
+
         this.position = INITIAL_POSITION;
         this.name = name;
+    }
+
+
+    private static void checkNameLength(String name) {
+        if (isNameOverMaxLength(name))
+            throw new InputMismatchException("MAX NAME LENGTH: " + MAX_NAME_LENGTH + ", NAME: " + name);
+    }
+
+    private static boolean isNameOverMaxLength(String name) {
+        return name.length() > MAX_NAME_LENGTH;
     }
 
     public void move(RuleStrategy ruleStrategy) {
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,7 +2,6 @@ package racingcar.view;
 
 import java.io.PrintStream;
 import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,7 +11,6 @@ public class InputView {
     private static final String CAR_NAMES_QUESTION = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String ROUND_NUM_QUESTION = "시도할 회수는 몇 회 인가요?";
     private static final String SPLIT_UNIT = ",";
-    private static final int MAX_NAME_LENGTH = 5;
 
     private InputView() {
     }
@@ -20,24 +18,8 @@ public class InputView {
     public static List<String> getCarNames() {
         out.println(CAR_NAMES_QUESTION);
         String input = scanner.nextLine();
-        List<String> names = splitWithRest(input);
 
-        checkNamesLength(names);
-
-        return names;
-    }
-
-    private static void checkNamesLength(List<String> names) {
-        names.forEach(InputView::checkNameLength);
-    }
-
-    private static void checkNameLength(String name) {
-        if (isNameOverMaxLength(name))
-            throw new InputMismatchException("MAX NAME LENGTH: " + MAX_NAME_LENGTH + ", NAME: " + name);
-    }
-
-    private static boolean isNameOverMaxLength(String name) {
-        return name.length() > MAX_NAME_LENGTH;
+        return splitWithRest(input);
     }
 
     private static List<String> splitWithRest(String input) {
