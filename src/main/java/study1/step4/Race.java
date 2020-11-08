ae53f560957cefaab4413e75b7ef9d095f4931fb
sjh <sjh@revhat.com>
2020-11-08 16:25:07 +0900

    [Step4] Race class 반영

--- src/main/java/study1/step4/Race.java
+++ src/main/java/study1/step4/Race.java
@@ -1,14 +1,32 @@
 package study1.step4;
 
-public class Race {
+import java.util.ArrayList;
 
-    public void race(){
-        String racerListString = InputView.getRacerList();
+public class Race {
+    private final int roundCnt;
+    private final Cars cars = new Cars();
+
+    public Race(){
         Tokenizer tokenizer = new Tokenizer();
 
+        String racerListString = InputView.getRacerList();
         tokenizer.TokenListValidate(racerListString);
-        String[] racerList = tokenizer.getList(racerListString);
 
-        int dummy = 1;
+        cars.initializeCars(tokenizer.getList(racerListString));
+        roundCnt = InputView.getRoundCnt();
+    }
+
+    public void race(){
+        Decision moveDecisionMaker = new MoveDecision();
+
+        ResultView.startRace(cars.getCars());
+        for(int i = 0; i < roundCnt; i++){
+            cars.race(moveDecisionMaker);
+            ResultView.show(cars.getCars());
+        }
+
+        Award award = new Award();
+        ArrayList<String> winnerList = award.getWinnerList(cars);
+        ResultView.printWinner(winnerList);
     }
 }
