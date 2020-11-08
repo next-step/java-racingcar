e5caa36c041187e28cd4c50f3f7d950d34c8c9b0
eyabc <bey4314@naver.com>
2020-11-08 19:09:05 +0900

    feat: Main

--- src/main/java/step04/Main.java
+++ src/main/java/step04/Main.java
@@ -1,5 +1,7 @@
 package step04;
 
+import step03.strategy.RandomMoveStrategy;
+
 public class Main {
     public static void main(String[] args) {
         final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
@@ -8,5 +10,13 @@ public class Main {
         final String INPUT_NUMBER_OF_MOVES_MESSAGE = "시도할 회수는 몇회인가요?";
         final int numberOfMoves = InputView.readInteger(INPUT_NUMBER_OF_MOVES_MESSAGE);
 
+        final int position = 1;
+        final String carKind = "-";
+
+        Cars cars = Cars.of(carNames, position, RandomMoveStrategy.of());
+        Race race = Race.of(numberOfMoves, cars);
+        ResultView resultView = ResultView.of(carKind);
+
+        race.operate(resultView);
     }
 }
