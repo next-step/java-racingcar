d3597936e4daacf0989cdf4681ac634783cf5d28
jhoon.bak <wecube.1@gmail.com>
2020-11-08 23:35:16 +0900

    feat(ResultView): 각 자동차별 이동거리를 출력한다

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,2 +1,21 @@
-package racingcar.view;public class ResultView {
+package racingcar.view;
+
+import racingcar.domain.Car;
+
+public class ResultView {
+
+    public void printDistanceOfCar(Car[] cars) {
+        for (Car car : cars) {
+            print(car.getDistance());
+            System.out.println("");
+        }
+    }
+
+    public void print(int distance) {
+        for (int i = 0; i < distance; i++) {
+            System.out.print("-");
+        }
+    }
+
+
 }
