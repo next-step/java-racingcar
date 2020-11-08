f89f47ddff7072886009320c2233a66963f20215
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-08 15:10:51 +0900

    [refactor] : 클래스/메소드 네이밍 변경, 레이싱 기록 리팩토링

--- src/main/java/racing/domain/CarConfig.java
+++ src/main/java/racing/domain/CarConfig.java
@@ -5,9 +5,9 @@ public final class CarConfig {
     public static final int CAR_MIN_MOVE_CONDITION = 4;
     public static final int CAR_MAX_RANDOM_NUMBER_SETTING = 10;
 
-    public static final String ECU_NORMAL_MOVE_CODE = "1";
-    public static final String ECU_STOP_MOVE_CODE = "0";
-    public static final String ECU_RECORD_STRING_SEPARATOR = ",";
+    public static final String CAR_NORMAL_MOVE_CODE = "1";
+    public static final String CAR_STOP_MOVE_CODE = "0";
+    public static final String CAR_RECORD_STRING_SEPARATOR = ",";
 
     private CarConfig() {}
 }
---
+++ src/main/java/racing/domain/CarSetInRace.java
@@ -0,0 +1,49 @@
+package racing.domain;
+
+import java.util.*;
+
+import static racing.domain.CarConfig.*;
+
+public class CarSetInRace {
+
+
+    private final List<StringJoiner> setRace;
+
+    public CarSetInRace() {
+        this.setRace = new LinkedList<>();
+    }
+
+    public void stop() {
+        if(setRace.size() == 0) {
+            setRace.add(new StringJoiner(CAR_RECORD_STRING_SEPARATOR).add(CAR_STOP_MOVE_CODE));
+            return;
+        }
+        StringJoiner preRecord = setRace.get(setRace.size() - 1);
+        setRace.add(new StringJoiner(CAR_RECORD_STRING_SEPARATOR).merge(preRecord).add(CAR_STOP_MOVE_CODE));
+    }
+
+    public void move() {
+        if(setRace.size() == 0) {
+            setRace.add(new StringJoiner(CAR_RECORD_STRING_SEPARATOR).add(CAR_NORMAL_MOVE_CODE));
+            return;
+        }
+        StringJoiner preRecord = setRace.get(setRace.size() - 1);
+        setRace.add(new StringJoiner(CAR_RECORD_STRING_SEPARATOR).merge(preRecord).add(CAR_NORMAL_MOVE_CODE));
+    }
+
+    public String findRecord(int index) {
+        return setRace.get(index).toString();
+    }
+
+    public List<StringJoiner> getRecord() {
+        return setRace;
+    }
+
+    public int getMoveSize() {
+        return findRecord(setRace.size() - 1)
+                .replaceAll(CAR_RECORD_STRING_SEPARATOR,"")
+                .replaceAll(CAR_STOP_MOVE_CODE,"")
+                .length();
+    }
+
+}
