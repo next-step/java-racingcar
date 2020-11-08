144fd9e18f06e34db421de25d716335d78ad1264
DongHwan Kim <bkuekany@gmail.com>
2020-11-08 20:54:05 +0900

    refactor: Refactor 추가 요구사항에 따른 메서드 리팩토링

--- src/main/java/racingcar/model/RaceGame.java
+++ src/main/java/racingcar/model/RaceGame.java
@@ -4,43 +4,54 @@ import racingcar.exception.AlreadyTerminateRaceGameException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RaceGame {
 
-    private List<RacingCar> racingCars;
-    private GameRound round;
+    private RacingCars racingCars;
+    private GameRound gameRound;
+    private Commander commander;
+    private int participantCars;
 
-    public RaceGame(GameRound round, Commander commander) {
-        this.round = round;
-        createParticipantRacingCar(commander);
+    public RaceGame(String[] carsNames, int totalRound, Commander commander) {
+        this.gameRound = new GameRound(totalRound);
+        this.participantCars = carsNames.length;
+        this.commander = commander;
+        createSetupRaceGame(carsNames);
     }
 
-    private void createParticipantRacingCar(Commander commander) {
-        racingCars = new ArrayList<>();
-        IntStream.range(0, round.getParticipantCar())
-                .forEach(i -> racingCars.add(new RacingCar(commander)));
+    private void createSetupRaceGame(String[] carNames) {
+        List<RacingCar> cars = new ArrayList<>();
+        IntStream.range(0, carNames.length)
+                .forEach(i -> cars.add(new RacingCar(carNames[i])));
+        racingCars = new RacingCars(cars);
     }
 
-    public void start() {
-        if(round.isAllRoundFinish()) {
+    public RaceGame start() {
+        if (gameRound.isAllRoundFinish()) {
             throw new AlreadyTerminateRaceGameException();
         }
-        play();
-
+        roundPlay();
+        return this;
     }
 
-    private void play() {
-        for (int i = 0; i < round.getParticipantCar(); i++) {
-            racingCars.get(i).executeMoveTrack();
+    private void roundPlay() {
+        for (int i = 0; i < participantCars; i++) {
+            racingCars.roundExecute(i, commander);
         }
-        round.roundFinish();
+        gameRound.roundFinish();
     }
 
-    public List<Integer> getMoveTracks() {
-        return racingCars.stream()
-                .map(RacingCar::movePosition)
-                .collect(Collectors.toList());
+
+    public List<Integer> getRoundResult(){
+        return racingCars.getResult();
+    }
+
+    public List<String> getCarNames(){
+        return racingCars.getCarName();
+    }
+
+    public List<String> getWinner() {
+        return racingCars.getWinner();
     }
 }
