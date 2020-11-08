d1d021b5625f754c7e35f8b11106d9077f2830ab
belle.bonjour <belle.bonjour@kakaocorp.com>
2020-11-09 00:18:01 +0900

    feature : 자동차 경주 플레이를 위한 Service 클래스 생성

---
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -0,0 +1,31 @@
+package racingcar.service;
+
+import racingcar.dto.RaceReadyValue;
+import racingcar.dto.RaceResultValue;
+import racingcar.dto.StepByCar;
+import racingcar.model.Cars;
+import racingcar.model.Motor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class RacingCarService {
+
+    private static final int RANGE_START_NUM = 0;
+
+    public static RaceResultValue play(RaceReadyValue raceReadyValue, Motor motor) {
+        List<StepByCar> stepByCar = new ArrayList<>();
+        int stepCount = raceReadyValue.getTryCount();
+
+        Cars cars = Cars.of(raceReadyValue.getCarNames(), motor);
+
+        IntStream.range(RANGE_START_NUM, stepCount)
+                .forEach(i -> {
+                    cars.run();
+                    stepByCar.addAll(StepByCar.of(cars));
+                });
+
+        return RaceResultValue.of(stepCount, stepByCar);
+    }
+}
