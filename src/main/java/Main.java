c4deb35d4c97b6bd41965aad72156a09776a1ecd
apalsl <apalsl123@gmail.com>
2020-11-08 16:01:28 +0900

    del step3
    step3 자동차 경주 삭제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,6 @@
-import racingcar.RacingStadium;
-
 public class Main {
 
     public static void main(String[] args) {
-        //step3
-        RacingStadium racingStadium = new RacingStadium();
-        racingStadium.run();
 
-        //step2
-//        Calculator calculator = new Calculator();
-//        calculator.run();
     }
 }
--- src/main/java/racingcar/MovableStrategy.java
+++
@@ -1,5 +0,0 @@
-package racingcar;
-
-public interface MovableStrategy {
-    int move();
-}
