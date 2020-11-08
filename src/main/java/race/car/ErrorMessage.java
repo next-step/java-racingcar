96b84a6d14b4e2c78151392f95a9ca6da0f5ef96
rlawls1991 <rlawls1991@gmail.com>
2020-11-08 22:18:12 +0900

    feat ARRAYINDEXOUTOFBOUNDSEXCEPTION 에러 문구 추가

--- src/main/java/race/car/ErrorMessage.java
+++ src/main/java/race/car/ErrorMessage.java
@@ -6,10 +6,11 @@ public class ErrorMessage {
     private static String EMPTY_EXCEPTION = "입력하지 않으셨습니다. 숫자를 입력해주세요";
     private static String NUMBERFORMATEXCEPTION = "문자를 입력하였습니다. 숫자를 입력해주세요";
     private static String ARRAYINDEXOUTOFBOUNDSEXCEPTION = "자동차의 갯수를 1개이상으로 입력해주세요";
+    private static String INDEXOUTOFBOUNDSEXCEPTION = "이름 길이가 5초과하였습니다.";
 
-   private ErrorMessage(){
-       
-   }
+    private ErrorMessage() {
+
+    }
 
     public static String getNullException() {
         return NULL_EXCEPTION;
@@ -25,5 +26,9 @@ public class ErrorMessage {
 
     public static String getARRAYINDEXOUTOFBOUNDSEXCEPTION() {
         return ARRAYINDEXOUTOFBOUNDSEXCEPTION;
+    }
+
+    public static String getINDEXOUTOFBOUNDSEXCEPTION() {
+        return INDEXOUTOFBOUNDSEXCEPTION;
     }
 }
