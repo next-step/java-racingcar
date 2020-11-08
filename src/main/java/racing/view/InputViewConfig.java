c79e85c2cefcbec50b0829546f59c34cbe06b1e4
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-08 22:22:53 +0900

    [feat] : InputView Config 유틸 클래스 추가

---
+++ src/main/java/racing/view/InputViewConfig.java
@@ -0,0 +1,10 @@
+package racing.view;
+
+public class InputViewConfig {
+
+    public static final String NUMBER_REGEX = "^[0-9]*$";
+    public static final String INPUT_STRING_ARRAY_SEPARATOR = ",";
+    public static final int INPUT_STRING_LIMIT_SIZE = 5;
+
+    private InputViewConfig() {}
+}
