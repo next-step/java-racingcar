89692e9dbe0b9ccdc037670b0a8507809e91ca61
kriery <jongchan.lee@lmep.kr>
2020-11-08 22:35:26 +0900

    feat(ui): added InputView, OutputView

---
+++ src/main/java/step3/ui/OutputView.java
@@ -0,0 +1,28 @@
+package step3.ui;
+
+import step3.logic.Car;
+
+import java.io.PrintStream;
+import java.util.List;
+
+public class OutputView {
+    private static final String POSITION_MARKER = "-";
+
+    protected final PrintStream out;
+
+    public OutputView() {
+        this(System.out);
+    }
+
+    public OutputView(PrintStream out) {
+        this.out = out;
+    }
+
+    public void showStatus(Car car) {
+        out.println(POSITION_MARKER.repeat(car.getCurrentPosition() + 1));
+    }
+
+    public void showStatus(List<Car> cars) {
+        cars.forEach(this::showStatus);
+    }
+}
