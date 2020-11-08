6f42426ffad1e15de46851bcb29d0b11f65b61cd
MJ Choi <1621139@gmail.com>
2020-11-08 17:38:40 +0900

    refactor: DELETE validateCarName'function

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,9 +9,7 @@ public class InputView {
     public static final String NUM_OF_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String LOOP_COUNT_MESSAGE = "시도할 횟수는 몇 회 인가요?";
     private static final int MIN_INPUT_VALUE = 0;
-    private static final int MAX_INPUT_NAME_VALUE = 5;
     private static final String COMMA = ",";
-    private static final String BLANK = " ";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -25,10 +23,9 @@ public class InputView {
     public static String[] getNameOfCars(String content) {
         System.out.println(content);
         String inputName = SCANNER.next();
-        String[] names = splitValues(inputName);
-        int numOfNames = names.length;
-        validateInputNum(numOfNames);
-        return names;
+        //        int numOfNames = names.length;
+//        validateInputNum(numOfNames);
+        return splitValues(inputName);
     }
 
     private static String[] splitValues(String inputName) {
@@ -38,23 +35,32 @@ public class InputView {
                 .toArray(String[]::new);
     }
 
-    public static void validInputName(String inputName) {
-        if (isValidInputName(inputName)) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_NAME_ERROR);
-        }
-    }
 
-    public static boolean isValidInputName(String inputName) {
-        return isNullOrEmpty(inputName) || isFiveLetterWords(inputName);
-    }
+//    static void validInputName(String [] carNames) {
+//        Arrays.stream(carNames)
+//                .filter()
+//                .forEach(s -> isValidInputName(s))
+//
+//                //.orElseThrow(s -> new IllegalArgumentException(ErrorMessage.INPUT_NAME_ERROR));
+//
+////       for(String carName : carNames){
+////           if (isValidInputName(carName)) {
+////               throw new IllegalArgumentException(ErrorMessage.INPUT_NAME_ERROR);
+////           }
+////       }
+//    }
 
-    public static boolean isNullOrEmpty(String name) {
-        return name.isEmpty() || name.equals(BLANK);
-    }
-
-    public static boolean isFiveLetterWords(String name) {
-        return name.length() > MAX_INPUT_NAME_VALUE;
-    }
+//    static boolean isValidInputName(String inputName) {
+//        return isNullOrEmpty(inputName) || isFiveLetterWords(inputName);
+//    }
+//
+//    static boolean isNullOrEmpty(String name) {
+//        return name.isEmpty() || name.equals(BLANK);
+//    }
+//
+//    static boolean isFiveLetterWords(String name) {
+//        return name.length() > MAX_INPUT_NAME_VALUE;
+//    }
 
     public static int inputLoopCount() {
         System.out.println(LOOP_COUNT_MESSAGE);
