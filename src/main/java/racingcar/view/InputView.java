2dbe7adb28dcd72bddd8247a84a0f707ac06318f
jhoon.bak <wecube.1@gmail.com>
2020-11-08 23:33:09 +0900

    refactor(InpuView): 입력기능 간소화

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,14 @@
 package racingcar.view;
 
+import lombok.Getter;
+
 import java.util.Scanner;
 
+@Getter
 public class InputView {
-    Integer carCount;
-    Integer racingCount;
 
-    public void insertCount() {
+    public int insertCount() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        carCount = scanner.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        racingCount = scanner.nextInt();
+        return scanner.nextInt();
     }
 }
