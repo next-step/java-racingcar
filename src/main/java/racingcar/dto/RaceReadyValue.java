dd91bda0ef24ba673d36178579c7562dede04516
belle.bonjour <belle.bonjour@kakaocorp.com>
2020-11-09 00:08:16 +0900

    feature : 자동차 이름을 입력받게 하며, 자동차 이름이 5자 초과될 때 에러 발생

--- src/main/java/racingcar/dto/RaceReadyValue.java
+++ src/main/java/racingcar/dto/RaceReadyValue.java
@@ -1,17 +1,41 @@
 package racingcar.dto;
 
+import java.util.Arrays;
+
 public class RaceReadyValue {
 
-    private final int carCount;
+    private static final String REGEX_COMMA = ",";
+    private static final int CAR_NAME_MAXIMUM_LENGTH = 5;
+    private static final String ERROR_MESSAGE_OVER_SIZE_CAR_NAME = "자동차 이름이 사이즈를 초과했습니다.";
+
+    private final String[] carNames;
     private final int tryCount;
 
-    public RaceReadyValue(int carCount, int tryCount) {
-        this.carCount = carCount;
+    RaceReadyValue(String carNameList, int tryCount) {
+        this.carNames = parseCarName(carNameList);
         this.tryCount = tryCount;
     }
 
-    public int getCarCount() {
-        return this.carCount;
+    public static RaceReadyValue of(String carNameList, int tryCount) {
+        validate(carNameList);
+
+        return new RaceReadyValue(carNameList, tryCount);
+    }
+
+    private static void validate(String carNameList) {
+        Arrays.stream(parseCarName(carNameList))
+                .forEach(v -> {
+                    if (v.length() > CAR_NAME_MAXIMUM_LENGTH)
+                        throw new IllegalArgumentException(ERROR_MESSAGE_OVER_SIZE_CAR_NAME);
+                });
+    }
+
+    private static String[] parseCarName(String carNameList) {
+        return carNameList.split(REGEX_COMMA);
+    }
+
+    public String[] getCarNames() {
+        return this.carNames;
     }
 
     public int getTryCount() {
--- src/main/java/racingcar/external/InputView.java
+++ src/main/java/racingcar/external/InputView.java
@@ -4,13 +4,13 @@ import racingcar.dto.RaceReadyValue;
 
 public class InputView {
 
-    private static final String INPUT_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
 
     public static RaceReadyValue input(InputChannel inputChannel) {
-        int carCount = inputChannel.input(INPUT_CAR_COUNT);
-        int tryCount = inputChannel.input(INPUT_TRY_COUNT);
+        String carNames = inputChannel.inputString(INPUT_CAR_NAMES);
+        int tryCount = inputChannel.inputNumber(INPUT_TRY_COUNT);
 
-        return new RaceReadyValue(carCount, tryCount);
+        return RaceReadyValue.of(carNames, tryCount);
     }
 }
