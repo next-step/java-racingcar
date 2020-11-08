451b23b9e4d65e59159c846546cf2dcd0cb48675
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-08 11:19:27 +0900

    refactor:input 메시지 개행 제거

--- src/main/java/nextstep/step3/view/InputView.java
+++ src/main/java/nextstep/step3/view/InputView.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class InputView {
 
-	private static final String INPUT_MESSAGE_CAR_NUM = "자동차 대수는 몇 대 인가요?\n";
-	private static final String INPUT_MESSAGE_TRY_NUM = "시도할 횟수는 몇 회 인가요?\n";
+	private static final String INPUT_MESSAGE_CAR_NUM = "자동차 대수는 몇 대 인가요?";
+	private static final String INPUT_MESSAGE_TRY_NUM = "시도할 횟수는 몇 회 인가요?";
 	private static final String WRONG_INPUT_COUNT = "자동차수와 시도횟수는 숫자만 가능합니다.";
 	private Scanner scanner;
 
