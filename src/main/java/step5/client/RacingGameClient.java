43a614d1b13a3eda663dbdc27090267391c033af
홍성민 <honggasm@gmail.com>
2020-11-08 17:41:41 +0900

    refactor: renaming inputview to RacingGameInputView

--- src/main/java/step5/client/RacingGameClient.java
+++ src/main/java/step5/client/RacingGameClient.java
@@ -3,13 +3,13 @@ package step5.client;
 import step5.client.view.output.Printer;
 import step5.worker.interfaces.controller.RacingGameController;
 import step5.worker.application.SimulationRequest;
-import step5.client.view.input.InputView;
+import step5.client.view.input.RacingGameInputView;
 
 public class RacingGameClient {
     private static final RacingGameController controller = new RacingGameController();
 
     public static void main(String[] args) {
-        final SimulationRequest request = InputView.receiveInputs();
+        final SimulationRequest request = RacingGameInputView.receiveInputs();
         final String result = controller.showSimulationResult(request);
         Printer.print(result);
     }
--- src/main/java/step5/client/view/input/RacingGameInputView.java
+++ src/main/java/step5/client/view/input/RacingGameInputView.java
@@ -5,8 +5,8 @@ import step5.client.view.output.Printer;
 
 import java.util.Scanner;
 
-public class InputView {
-    private InputView() {
+public class RacingGameInputView {
+    private RacingGameInputView() {
         throw new AssertionError();
     }
     private static final String ENTER_THE_NAME_OF_THE_CARS = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
