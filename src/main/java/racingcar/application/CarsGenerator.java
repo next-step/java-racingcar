3718f10e738942ada025a553533fbe2f966df7fd
hoonick <wecube.1@gmail.com>
2020-11-09 08:51:47 +0900

    feat(CarsGenerator): 사용자가 입력한 자동차의 이름을 가진 Car 배열을 생성한다

--- src/main/java/racingcar/application/CarsGenerator.java
+++ src/main/java/racingcar/application/CarsGenerator.java
@@ -3,11 +3,19 @@ package racingcar.application;
 import racingcar.domain.Car;
 
 public class CarsGenerator {
-    public Car[] generateCars(int countOfCar) {
-        Car[] cars = new Car[countOfCar];
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car();
+    public Car[] generateCars(String[] nameOfCars) {
+        Car[] cars = new Car[nameOfCars.length];
+        for (int i = 0; i < nameOfCars.length; i++) {
+            String name = nameOfCars[i];
+            validateName(name);
+            cars[i] = new Car(name, 0);
         }
         return cars;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("the name must be less than five characters.");
+        }
     }
 }
--- src/test/java/racingcar/application/CarsGeneratorTest.java
+++ src/test/java/racingcar/application/CarsGeneratorTest.java
@@ -8,22 +8,33 @@ import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CarsGeneratorTest {
 
-    CarsGenerator carsGenerator;
+    private CarsGenerator carsGenerator;
 
     @BeforeEach
     public void setUp() {
         carsGenerator = new CarsGenerator();
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = 5)
-    @DisplayName("주어진 수만큼 자동차 배열을 생성한다.")
-    public void should_create_cars(int countOfCar) {
-        Car[] cars = carsGenerator.generateCars(countOfCar);
-        assertThat(cars.length).isEqualTo(countOfCar);
+    @Test
+    @DisplayName("입력된 이름의 수 만큼 자동차들을 생성한다.")
+    public void should_create_cars() {
+        String[] nameOfCars = {"git", "pron", "abc"};
+        Car[] cars = carsGenerator.generateCars(nameOfCars);
+        assertThat(cars.length).isEqualTo(nameOfCars.length);
+    }
+
+    @Test
+    @DisplayName("입력된 이름이 5글자 이상이면 IllegalArgumentException을 던진다.")
+    public void should_throw_IllegalArgumentException_when_name_is_more_than_5_characters() {
+        String[] nameOfCars = {"hoonick", "pron", "abc"};
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> carsGenerator.generateCars(nameOfCars))
+                .withMessage("the name must be less than five characters.");
     }
 
 }
