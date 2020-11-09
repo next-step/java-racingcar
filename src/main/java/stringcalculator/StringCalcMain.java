ffd82e7082c33e1450225a2cccaafa66d2c5a0b6
kim-daeyong <kdy2353@gmail.com>
2020-11-09 23:49:15 +0900

    요구사항 구현

--- src/main/java/stringcalculator/StringCalcMain.java
+++ src/main/java/stringcalculator/StringCalcMain.java
@@ -2,12 +2,21 @@ package stringcalculator;
 
 import java.util.Scanner;
 
-public class StringCalcMain {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+import org.junit.platform.commons.util.StringUtils;
 
-        String value = sc.next();
+public class StringCalcMain {
+
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+
+        // String value = sc.next();
+        String value = "2 + 3 * 4 / 2";
+        if(value == null || value.equals("") ){
+            throw new IllegalArgumentException("Input is null or empty, Please check your input");
+        }
+
         StringCalculator strCalc = new StringCalculator();
         strCalc.calculator(value);
     }
+
 }
--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -4,44 +4,94 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringCalculator {
-    //분류
-    //입력값이 공백이거나  null 일 경우 illegalArgumentException
+
+    List<String> numbers = new ArrayList<>();
+    List<String> symbol = new ArrayList<>();
     //사칙연산 기호가 아닌 경우 illegalArgumentException
     public int calculator(String value){
-        String[] stringInput = value.split("");
-        List<String> numbers = new ArrayList<>();
-        List<String> symbol = new ArrayList<>();
+        int count = 0;
+        int result = 0;
+        String[] stringInput = value.split(" ");
+        boolean isSort = sortByNumAndSymbol(stringInput);
+        if(isSort == true){
+            count = 0;
+            for(String sym : symbol){
+                int num1 = 0;
+                int num2 = 0;
+                if(count == 0){
+                    num1 = Integer.parseInt(numbers.get(0));
+                    num2 = Integer.parseInt(numbers.get(count+1));
+                    result = symbolSelector(num1, num2, sym);
+                    count ++;
+                }else{
+                    num1 = result;
+                    num2 = Integer.parseInt(numbers.get(count+1));
+                    result = symbolSelector(num1, num2, sym);
+                    count ++;
+                }
+                if(count > numbers.size()){
+                    break;
+                }
+            }
+        }
+        System.out.println(result);
+        return result;
+
+    }
+
+    public boolean sortByNumAndSymbol(String[] stringInput){
+        boolean result = false;
 
         for(String str : stringInput){
-            if(str.equals())
+            char temp = str.charAt(0);
+            if(Character.isDigit(temp)){
+                numbers.add(str);
+            }else{
+                symbol.add(str);
+            }
+            result = true;
         }
 
-        return 0;
+        return result;
+
     }
 
-    //덧셈
+    public int symbolSelector(int num1, int num2, String symbol){
+        int calResult = 0;
+        switch(symbol){
+            case "+":
+                calResult = add(num1, num2);
+                break;
+            case "-":
+                calResult = sub(num1, num2);
+                break;
+            case "*":
+                calResult = multiple(num1, num2);
+                break;
+            case "/":
+                calResult = div(num1, num2);
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect symbol, Please check your symbol");
+        }
+
+        return calResult;
+    }
+
     public int add(int num1, int num2){
-
-        return 0;
+        return num1+num2;
     }
 
-    //뺄셈
     public int sub(int num1, int num2){
-
-        return 0;
+        return num1-num2;
     }
 
-    //곱셈
     public int multiple(int num1, int num2){
-
-        return 0;
+        return num1*num2;
     }
 
-    //나눗셈
     public int div(int num1, int num2){
-
-        return 0;
+        return num1/num2;
     }
-
 
 }
