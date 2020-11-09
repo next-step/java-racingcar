4f9844f19e599575b4e26cf1f0f39c791d1bd913
최종원 <90lastone@gmail.com>
2020-11-09 12:50:03 +0900

    feat: 자동차 수 입기 받기 함수 추가

--- src/main/java/study/step3/InputView.java
+++ src/main/java/study/step3/InputView.java
@@ -3,6 +3,11 @@ package study.step3;
 import java.util.Scanner;
 
 public class InputView {
+    /* 자동차 수 입력 */
+    public int inputCarCount() {
+        return input("자동차 대수는 몇 대 인가요?");
+    }
+
     /**
      * input 입력 받기위한 안내 문구 및 입력 받기
      * @param message
