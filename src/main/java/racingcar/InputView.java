55f45524c04c729432b2b7562725ef0f960aeedf
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-09 23:13:56 +0900

    refector : 메소드 분할

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,17 +4,29 @@ import java.util.Scanner;
 
 public class InputView {
 
-    static Game game = new Game();
+    public static void viewCountCarInputMessage(){
+        System.out.println("자동차 대수는 몇 대인가요?");
+    }
 
     public static void viewCountCarInput(){
-        System.out.println("자동차 대수는 몇 대인가요?");
-        game.setCars(Integer.parseInt(new Scanner(System.in).nextLine()));
-        System.out.println(game.cars.getSize());
+        Game.setCars(Integer.parseInt(new Scanner(System.in).nextLine()));
+    }
+
+    public static void viewCountCar(){
+        System.out.println(Game.cars.getSize());
+    }
+
+    public static void viewCountRoundInputMessage(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
     public static void viewCountRoundInput(){
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        game.setRoundCount(Integer.parseInt(new Scanner(System.in).nextLine()));
-        System.out.println(game.getRoundCount());
+        Game.setRoundCount(Integer.parseInt(new Scanner(System.in).nextLine()));
     }
+
+    public static void viewCountRound(){
+        System.out.println(Game.getRoundCount());
+    }
+
+
 }
