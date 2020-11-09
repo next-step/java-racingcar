037202a0843f40d55ed71afd01f65d1ac0a4635a
belle.bonjour <belle.bonjour@kakaocorp.com>
2020-11-10 02:46:57 +0900

    코드 리뷰 반영 : RaceResultValue 일급 컬렉션 수정

--- src/main/java/racingcar/dto/RaceResultValue.java
+++ src/main/java/racingcar/dto/RaceResultValue.java
@@ -1,8 +1,6 @@
 package racingcar.dto;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class RaceResultValue {
 
@@ -10,54 +8,37 @@ public class RaceResultValue {
     private static final String ERROR_MESSAGE_NOT_EXIST_MAX_SCORE = "우승스코어가 존재하지 않습니다.";
 
     private final int step;
-    private final Map<String, List<StepByCar>> raceStepValueByCarName;
+    private final List<StepResult> stepResultList;
 
-    private RaceResultValue(int tryCount, Map<String, List<StepByCar>> raceStepValueByCarName) {
+    private RaceResultValue(int tryCount, List<StepResult> stepResultList) {
         this.step = tryCount;
-        this.raceStepValueByCarName = raceStepValueByCarName;
+        this.stepResultList = stepResultList;
     }
 
-    public static RaceResultValue of(int tryCount, List<StepByCar> stepByCars) {
-        Map<String, List<StepByCar>> raceStepValueByCarName = stepByCars.stream()
-                .collect(Collectors.groupingBy(StepByCar::getName));
-
-        validate(tryCount, raceStepValueByCarName);
-
-        return new RaceResultValue(tryCount, raceStepValueByCarName);
+    public static RaceResultValue of(int tryCount, List<StepResult> stepResultList) {
+        validate(tryCount, stepResultList);
+        return new RaceResultValue(tryCount, stepResultList);
     }
 
-    private static void validate(int tryCount, Map<String, List<StepByCar>> raceStepValueByCarName) {
-        raceStepValueByCarName.values()
-                .forEach(v -> {
-                    if (tryCount != v.size()) {
-                        throw new IllegalArgumentException(ERROR_MESSAGE_NOT_MATCH_RACE);
-                    }
-                });
+    private static void validate(int tryCount, List<StepResult> stepResultList) {
+        if (tryCount != stepResultList.size()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_MATCH_RACE);
+        }
     }
 
     public int getStepCount() {
         return step;
     }
 
+    public List<StepResult> getStepResults(){
+        return this.stepResultList;
+    }
+
     public int getLastIndex() {
         return step - 1;
     }
 
-    public Map<String, List<StepByCar>> getRaceStepValueByCarName() {
-        return raceStepValueByCarName;
-    }
-
-    public int getMaxScore() {
-        return raceStepValueByCarName.values().stream()
-                .mapToInt(raceStepValues -> raceStepValues.get(getLastIndex()).getPosition())
-                .max()
-                .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE_NOT_EXIST_MAX_SCORE));
-    }
-
-    public List<String> getWinnerName() {
-        return raceStepValueByCarName.entrySet().stream()
-                .filter(v -> v.getValue().get(getLastIndex()).getPosition() == getMaxScore())
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
+    public List<String> getFinalWinnerName() {
+        return stepResultList.get(getLastIndex()).getWinnerName();
     }
 }
--- src/test/java/racingcar/dto/RaceResultValueTest.java
+++ src/test/java/racingcar/dto/RaceResultValueTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.model.Car;
+import racingcar.model.Cars;
 import racingcar.model.Motor;
 import racingcar.model.TesterMotor;
 
@@ -19,97 +20,67 @@ public class RaceResultValueTest {
     private static final int TRY_COUNT = 3;
 
     private Motor motor;
-    List<StepByCar> stepByCars;
+    List<StepResult> stepResults = new ArrayList<>();
 
     @BeforeEach
     void setup() {
         motor = new TesterMotor();
 
-        // car1 : 1,1,2
-        // car2 : 0,1,1
-        // car3 : 1,2,3
-
         Car car1 = new Car("dong", motor);
         Car car2 = new Car("woong", motor);
         Car car3 = new Car("hwi", motor);
 
+        Cars cars = new Cars(Arrays.asList(car1, car2, car3));
+
+        // car1 : 1,1,2
+        // car2 : 0,1,1
+        // car3 : 1,2,3
+
         car1.move();
         car3.move();
 
-        List<StepByCar> step1 = Arrays.asList(
-                StepByCar.of(car1),
-                StepByCar.of(car2),
-                StepByCar.of(car3));
+        StepResult stepResult1 = StepResult.of(cars);
 
         car2.move();
         car3.move();
 
-        List<StepByCar> step2 = Arrays.asList(
-                StepByCar.of(car1),
-                StepByCar.of(car2),
-                StepByCar.of(car3));
+        StepResult stepResult2 = StepResult.of(cars);
 
         car1.move();
         car3.move();
 
-        List<StepByCar> step3 = Arrays.asList(
-                StepByCar.of(car1),
-                StepByCar.of(car2),
-                StepByCar.of(car3));
+        StepResult stepResult3 = StepResult.of(cars);
 
-        stepByCars = new ArrayList<>();
-        stepByCars.addAll(step1);
-        stepByCars.addAll(step2);
-        stepByCars.addAll(step3);
+        stepResults.add(stepResult1);
+        stepResults.add(stepResult2);
+        stepResults.add(stepResult3);
     }
 
     @DisplayName("RaceResultValue() 생성자 테스트")
     @Test
     void RaceResultValue1() {
-        Car car1 = new Car("belle", motor);
-        Car car2 = new Car("jin", motor);
-        int tryCount = 2;
+        RaceResultValue raceResultValue = RaceResultValue.of(TRY_COUNT, stepResults);
 
-        List<StepByCar> stepByCarList = Arrays.asList(
-                StepByCar.of(car1),
-                StepByCar.of(car1),
-                StepByCar.of(car2),
-                StepByCar.of(car2));
-
-        assertThat(RaceResultValue.of(tryCount, stepByCarList))
-                .isEqualToComparingFieldByField(RaceResultValue.of(tryCount, stepByCarList));
+        assertThat(raceResultValue)
+                .isEqualToComparingFieldByField(raceResultValue);
     }
 
     @DisplayName("RaceResultValue() 생성자 테스트 : 실패케이스 -> 시도 횟수와 자동차경주 횟수가 일치하지 않는 경우 에러")
     @Test
     void RaceResultValue2() {
-        Car car1 = new Car("belle", motor);
-        Car car2 = new Car("jin", motor);
         int tryCount = 2;
 
-        List<StepByCar> stepByCarList = Arrays.asList(
-                StepByCar.of(car1),
-                StepByCar.of(car2));
-
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            RaceResultValue.of(tryCount, stepByCarList);
+            RaceResultValue.of(tryCount, stepResults);
         });
     }
 
-    @DisplayName("getMaxScore() 테스트 : 경주 MaxScore 확인")
-    @Test
-    void getMaxScore() {
-        RaceResultValue raceResultValue = RaceResultValue.of(TRY_COUNT, stepByCars);
-
-        assertThat(raceResultValue.getMaxScore()).isEqualTo(3);
-    }
-
-    @DisplayName("RaceResultValue() 테스트 : 우승자 확인")
+    @DisplayName("getWinnerName() 테스트 : 우승자 확인")
     @Test
     void getWinnerName() {
-        RaceResultValue raceResultValue = RaceResultValue.of(TRY_COUNT, stepByCars);
+        RaceResultValue raceResultValue = RaceResultValue.of(TRY_COUNT, stepResults);
 
-        assertThat(raceResultValue.getWinnerName().size()).isEqualTo(1);
-        assertThat(raceResultValue.getWinnerName()).contains("hwi");
+        assertThat(raceResultValue.getFinalWinnerName().size()).isEqualTo(1);
+        assertThat(raceResultValue.getFinalWinnerName()).contains("hwi");
     }
 }
