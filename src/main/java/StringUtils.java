8c5d5e478233064a7fa895d27a6244f1791ef589
sehan <sehan.choi@gmail.com>
2020-11-09 13:37:12 +0900

    chore: merge 과정에 restore 된 파일삭제

--- src/main/java/StringUtils.java
+++
@@ -1,8 +0,0 @@
-public class StringUtils {
-
-    public static boolean isNullOrBlank(String value) {
-        if (value == null) return true;
-        if (value.isEmpty()) return true;
-        return !value.chars().anyMatch( ch -> !Character.isWhitespace(ch) );
-    }
-}
