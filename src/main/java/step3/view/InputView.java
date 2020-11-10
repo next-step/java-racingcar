c13d4905bcaa040254486ef00bbfafc541f07c3c
mand2 <nayeon.kim1123@gmail.com>
2020-11-10 23:36:14 +0900

    refactor: InputView 안쓰는 메서드 삭제

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -11,12 +11,6 @@ public class InputView {
     private final static String ROUND_NUMBER = "시도할 회수는 몇 회 인가요?";
     private final static String CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 
-    public static int askCarNumber() {
-        System.out.println(CAR_NUMBER);
-        String participants = Controller.SCANNER.nextLine();
-        return NumberUtils.toInt(participants);
-    }
-
     public static int askRoundNumber() {
         System.out.println(ROUND_NUMBER);
         String rounds = Controller.SCANNER.nextLine();
