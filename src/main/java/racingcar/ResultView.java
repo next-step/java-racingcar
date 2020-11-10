e2dce5cbb7d01e2d9074dd3b54e7ef0b803a5dd9
MaroKim <dkgell6@gmail.com>
2020-11-11 01:05:19 +0900

    1. Car class recordDistance 타입 변경에 따른 기능 정리 2. Car class에서 printing 했던 기능 ResultView로 이동

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -14,17 +14,28 @@ public class ResultView {
     }
 
     //시도 횟수별 자동차 이동경로 값 가져오기
-    public void showResult(Cars cars){
+    public void showResult(List<Car> carList){
         System.out.println("실행 결과");
         for(int i=0;i<this.attempts;i++){
-            printDistance(cars.getCars(), i);
+            printDistance(carList, i);
         }
+    }
+    public String intToStr(int distance){
+        String distanceStr="";
+
+        while(distance >0){
+            distanceStr+="-";
+            distance-=1;
+        }
+
+        return distanceStr;
     }
 
     //이동 결과 그리기
     public void printDistance(List<Car> carList, int num){
         for(int i=0;i<this.counts;i++){
-            System.out.println(carList.get(i).recordDistance.get(num));
+            Car afterRacingCar  = carList.get(i);
+            System.out.println(this.intToStr(afterRacingCar.recordList.get(num)));
         }
         System.out.println();
 
