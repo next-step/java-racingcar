3bf0707b32c6489433d3604d4f918abfc5599d54
MaroKim <dkgell6@gmail.com>
2020-11-11 00:00:26 +0900

    recordDistance 메소드 추가 : list에 distance 삽입 메소드 move 메소드 기능 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,24 +6,20 @@ import java.util.Random;
 
 public class Car {
     public int distance;
-    public List<String> recordDistance;
+    public List<Integer> recordList;
 
     public Car(){
         this.distance = 0;
-        this.recordDistance = new ArrayList<String>();
+        this.recordList = new ArrayList<Integer>();
     }
 
-
-    //난수 발생 결과에 따른 이동 결과 저장
+    //자동차 이동
     public void move(){
-        int tempDistance = this.distance;
-        String distanceToStr="";
-
-        while(tempDistance!=0){
-            distanceToStr+="-";
-            tempDistance-=1;
-        }
-        recordDistance.add(distanceToStr);
+        this.distance +=1;
+    }
+    //자동차 이동 기록
+    public void recordDistance(){
+        this.recordList.add(this.distance);
     }
 
 }
