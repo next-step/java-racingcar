ba0d27ea9da1d85ec3aded9a58b475d7100bcb44
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-11-10 12:47:10 +0900

    test - 우승자 이름 구하기 및 우승자 구하기 테스트

--- src/test/java/racingcar/CarControllerTest.java
+++ src/test/java/racingcar/CarControllerTest.java
@@ -31,11 +31,84 @@ class CarControllerTest {
     @DisplayName("우승자 점수 계산 테스트")
     void winnerScoreTest() {
         int winnerScore = 5;
+        int looserScore = 3;
+        Car car1 = Car.carDtoMapper(new CarDto("a", winnerScore));
+        Car car2 = Car.carDtoMapper(new CarDto("b", looserScore));
+        List<Car> cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        CarList carList = CarList.from(cars);
+
+        assertThat(controller.getWinnerScore(carList)).isEqualTo(winnerScore);
+    }
+
+    @Test
+    @DisplayName("우승자 이름 구하기 테스트 - 1명일 때")
+    void winnerOnlyOneNameTest() {
+        List<String> winners = new ArrayList<>();
+        int winnerScore = 3;
+        Car car1 = Car.carDtoMapper(new CarDto("a", winnerScore));
+
+        controller.getWinnerName(winnerScore, car1, winners);
+
+        assertThat(winners).containsOnly(car1.getName());
+    }
+
+    @Test
+    @DisplayName("우승자 이름 구하기 테스트 - 여려명일 때")
+    void winnerPeopleNameTest() {
+        List<String> winners = new ArrayList<>();
+        int winnerScore = 3;
+        int looserScore = 2;
+        Car car1 = Car.carDtoMapper(new CarDto("a", winnerScore));
+        Car car2 = Car.carDtoMapper(new CarDto("b", winnerScore));
+        Car car3 = Car.carDtoMapper(new CarDto("b", looserScore));
+
+        controller.getWinnerName(winnerScore, car1, winners);
+        controller.getWinnerName(winnerScore, car2, winners);
+        controller.getWinnerName(winnerScore, car3, winners);
+
+        List<String> compareWinner = new ArrayList<>();
+        compareWinner.add(car1.getName());
+        compareWinner.add(car2.getName());
+
+        assertThat(winners).isEqualTo(compareWinner);
+    }
+
+    @Test
+    @DisplayName("우승자 구하기 테스트 - 1명일 때")
+    void winnerOnlyOneTest() {
+        int winnerScore = 5;
         Car car1 = Car.carDtoMapper(new CarDto("a", winnerScore));
         List<Car> cars = new ArrayList<>();
         cars.add(car1);
         CarList carList = CarList.from(cars);
 
-        assertThat(controller.getWinnerScore(carList)).isEqualTo(5);
+        List<String> winner = controller.getWinner(carList);
+
+        assertThat(winner).containsOnly(car1.getName());
+    }
+
+    @Test
+    @DisplayName("우승자 구하기 테스트 - 여러명일 때")
+    void winnerPeopleTest() {
+        int winnerScore = 5;
+        int looserScore = 3;
+        Car car1 = Car.carDtoMapper(new CarDto("a", winnerScore));
+        Car car2 = Car.carDtoMapper(new CarDto("b", looserScore));
+        Car car3 = Car.carDtoMapper(new CarDto("c", winnerScore));
+        List<Car> cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        CarList carList = CarList.from(cars);
+
+        List<String> winner = controller.getWinner(carList);
+
+        List<String> compareWinner = new ArrayList<>();
+        compareWinner.add(car1.getName());
+        compareWinner.add(car3.getName());
+
+        assertThat(winner).isEqualTo(compareWinner);
     }
 }
