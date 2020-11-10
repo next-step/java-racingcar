c11602488c38d7945ecb599bc397c97dcfaf0af9
Sunghyeob.Baek <baek9652@naver.com>
2020-11-10 22:31:35 +0900

    [test] RacingOperator의 테스트케이스 추가

--- src/test/java/racingGame/RacingOperatorTest.java
+++ src/test/java/racingGame/RacingOperatorTest.java
@@ -1,14 +1,53 @@
 package racingGame;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class RacingOperatorTest {
 
   private RacingOperator racingOperator;
+  private Cars cars;
 
   @BeforeEach
   void setUp() {
     String rawInput = "a,b,c,d";
-    racingOperator = RacingOperator.of(Cars.of(rawInput));
+    ScoreGenerator scoreGenerator = new DummyScoreGenerator();
+    this.cars = Cars.of(rawInput);
+    this.racingOperator = RacingOperator.of(this.cars, scoreGenerator);
+  }
+
+
+  @DisplayName("move 테스트")
+  @Test
+  void testMoveExactly() {
+    this.racingOperator.moves();
+    List<Integer> expected = new Vector<>();
+    expected.add(1);
+    expected.add(0);
+    expected.add(1);
+    expected.add(0);
+
+    assertThat(this.racingOperator.getPositions())
+        .isEqualTo(expected);
+  }
+
+  @DisplayName("우승자 테스트")
+  @Test
+  void whoIsWinner() {
+    this.racingOperator.moves();
+    List<String> expected = new Vector<>();
+    expected.add("a");
+    expected.add("c");
+
+    assertThat(this.racingOperator.extractWinners()).isEqualTo(expected);
   }
 }
