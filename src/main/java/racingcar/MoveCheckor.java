ce2e2be5dc4c75431dc07fa5d06d6bf12f2960c2
MaroKim <dkgell6@gmail.com>
2020-11-11 00:04:52 +0900

    메소드 이름 변경 및 객체 캡슐화

--- src/main/java/racingcar/MoveCheckor.java
+++ src/main/java/racingcar/MoveCheckor.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class MoveCheckor{
 
-    //난수 확인
-    public boolean checkNumber(){
+    //기준치 이상 여부 확인
+    public boolean checkOverBaseline(){
         boolean canMove = false;
         Random random = new Random();
         int randomNum = random.nextInt(10);
@@ -16,14 +16,14 @@ public class MoveCheckor{
         return canMove;
     }
 
-    //난수에 따른 이동 및 정지 결정
+    //난수에 따른 이동 및 기록
     public void moveAndStop(Car car, int attempts){
 
         for(int i=0;i<attempts;i++){
-            if(this.checkNumber()){
-                car.distance+=1;
+            if(this.checkOverBaseline()){
+                car.move();
             }
-            car.move();
+            car.recordDistance();
         }
 
     }
