e1773ac9bb0ed27d33fb23b04f055d1546fefb0d
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-11-12 07:46:00 +0900

    refactor - InputView class 변수 이름의 상수화

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
-    private final String questionCarNames = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private final String questionTryNumbers = "시도할 회수는 몇 회 인가요?";
+    private final String QUESTION_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private final String QUESTION_TRY_NUMBERS = "시도할 회수는 몇 회 인가요?";
     private final String COLON = " : ";
 
     public String[] carNames() {
-        System.out.println(questionCarNames);
+        System.out.println(QUESTION_CAR_NAMES);
         return scanner.nextLine().split(COLON);
     }
 
     public int tryCount() {
-        System.out.println(questionTryNumbers);
+        System.out.println(QUESTION_TRY_NUMBERS);
         return scanner.nextInt();
     }
 
