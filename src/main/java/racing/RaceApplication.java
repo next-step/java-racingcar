42ebd495a6e28b7d1a9f47d841b48bb0da2198e5
monkeyDugi <lbd4946@gmail.com>
2020-11-12 06:42:32 +0900

    feat : 레이싱의 모든 단계를 진행하는 RacingGame 클래스 생성

--- src/main/java/racing/RaceApplication.java
+++ src/main/java/racing/RaceApplication.java
@@ -1,17 +1,9 @@
 package racing;
 
-import racing.ui.InputView;
-import racing.ui.ResultView;
-
-import java.util.List;
-
 public class RaceApplication {
 
     public static void main(String[] args) {
-        int participationCars = InputView.inputParticipationCars();
-        int numberOfParticipationTry = InputView.inputNumberOfTry();
-
-        Cars cars = new Cars(participationCars);
-        ResultView.output(numberOfParticipationTry, cars);
+        RacingGame racingGame = new RacingGame();
+        racingGame.race();
     }
 }
--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -1,2 +1,15 @@
-package racing;public class RacingGame {
+package racing;
+
+import racing.ui.InputView;
+import racing.ui.ResultView;
+
+public class RacingGame {
+
+    public void race() {
+        int participationCars = InputView.inputParticipationCars();
+        int numberOfAttempt = InputView.inputNumberOfAttempt();
+
+        Cars cars = new Cars(participationCars);
+        ResultView.output(numberOfAttempt, cars);
+    }
 }
