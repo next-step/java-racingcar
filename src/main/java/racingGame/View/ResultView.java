4ca3f54d2a4459635c288529c0ff72d989bdc368
Sunghyeob.Baek <baek9652@naver.com>
2020-11-12 00:15:32 +0900

    [refactor] ResultView 추상화 레벨 변경

--- src/main/java/racingGame/View/ResultView.java
+++ src/main/java/racingGame/View/ResultView.java
@@ -9,11 +9,12 @@ public class ResultView {
   private static final String NAME_SCORE_DELIMITER = ": ";
   private static final String WINNERS_NAME_DELIMITER = ",";
   private static final String SCORE_CHARACTER = "-";
+  private static final String CLOSING_COMMENTS = "이(가) 최종 우승했습니다.";
 
   private ResultView() {
   }
 
-  static public void printResultMessage() {
+  public static void printResultMessage() {
     System.out.println(RESULT_MESSAGE);
   }
 
@@ -21,31 +22,43 @@ public class ResultView {
     for (Integer position : positions) {
       printDash(position);
     }
-    System.out.println();
+    splitLine();
   }
 
   public static void printCurrentStatusWithName(List<Pair<String, Integer>> nameAndPosition) {
     for (Pair<String, Integer> position : nameAndPosition) {
-      System.out.print(position.getKey());
-      System.out.print(NAME_SCORE_DELIMITER);
+      printNameWithDelimiter(position.getKey());
       printDash(position.getValue());
     }
-    System.out.println();
+    splitLine();
   }
 
-  static private void printDash(int numDash) {
+  private static void printNameWithDelimiter(String key) {
+    System.out.print(key);
+    System.out.print(NAME_SCORE_DELIMITER);
+  }
+
+  private static void printDash(int numDash) {
     for (int i = 0; i < numDash; i++) {
       System.out.print(SCORE_CHARACTER);
     }
-    System.out.println();
+    splitLine();
   }
 
-  static public void printWinner(List<String> winners) {
+  public static void printWinner(List<String> winners) {
     System.out.print(winners.get(0));
     for (int i = 1; i < winners.size(); i++) {
-      System.out.print(WINNERS_NAME_DELIMITER);
-      System.out.print(winners.get(i));
+      printSplitterAndName(winners.get(i));
     }
-    System.out.println("이(가) 최종 우승했습니다.");
+    System.out.println(CLOSING_COMMENTS);
+  }
+
+  private static void printSplitterAndName(String name) {
+    System.out.print(WINNERS_NAME_DELIMITER);
+    System.out.print(name);
+  }
+
+  private static void splitLine() {
+    System.out.println();
   }
 }
