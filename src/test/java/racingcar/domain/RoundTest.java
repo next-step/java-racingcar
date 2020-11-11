1199d678b042f38ac2e12b9a488ec5c99b016a25
MJ Choi <1621139@gmail.com>
2020-11-11 13:44:30 +0900

    feature: ADD RoundTest

---
+++ src/test/java/racingcar/domain/RoundTest.java
@@ -0,0 +1,47 @@
+package racingcar.domain;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RoundTest {
+    private final String[] nameOfCars = {"car1", "car2", "car3", "car4"};
+    private RacingCars racingCars;
+    private static final int FINAL_ROUND = 0;
+
+    @DisplayName("라운드 정상 진행 여부")
+
+    @Test
+    void roundTest() {
+        Round round = new Round(FINAL_ROUND);
+        racingCars = RacingCars.of(nameOfCars, () -> true, round);
+        racingCars.run();
+        assertThat(round.nextRound()).isEqualTo(2);
+    }
+
+    @DisplayName("마지막 라운드 시 true 반환 여부")
+    @Test
+    void finalRoundTest() {
+        Round round = new Round(5);
+        racingCars = RacingCars.of(nameOfCars, () -> true, round);
+        for (int i = 0; i < 5; i++) {
+            racingCars.run();
+        }
+        assertThat(round.isLastRound()).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("게임 라운드 정상 진행 여부")
+    @ValueSource(ints = {10, 25, 30})
+    void gameRoundTest(int roundCount) {
+        Round round = new Round(roundCount);
+        racingCars = RacingCars.of(nameOfCars, () -> true, round);
+        round.progressRound(racingCars);
+        assertThat(round.getRound()).isEqualTo(roundCount);
+    }
+
+}
