f4fb631b64ad62d74d9b6b7c3b4a1c79f4849a6d
devyoon91 <kimbyungyoun91@gmail.com>
2020-11-12 00:16:50 +0900

    [refactor] : 불필요 메소드 제거, 전역 변수 제거

--- src/main/java/racing/view/InputViewMessage.java
+++ src/main/java/racing/view/InputViewMessage.java
@@ -1,7 +1,6 @@
 package racing.view;
 
 public class InputViewMessage {
-    public static final String FIRST_QUESTION = "자동차 대수는 몇 대 인가요?\n";
     public static final String SECOND_QUESTION = "시도할 회수는 몇 회 인가요?\n";
     public static final String INPUT_VALIDATION_ERROR = "오류:: 입력한 개수가 0보다 커야 합니다.\n\n";
     public static final String INPUT_FORMAT_VALIDATION_ERROR = "오류:: 숫자만 입력 가능합니다.\n\n";
