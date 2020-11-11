91a1bd99bc77bff7907fe0d082dc17f4becf445f
DongHwan Kim <bkuekany@gmail.com>
2020-11-12 00:24:28 +0900

    refactor : Change Winner 제네릭 String 타입 -> RacingCar 타입으로 변경

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -36,10 +36,10 @@ public class RacingCars {
                 .collect(Collectors.toList());
     }
 
-    public List<String> getWinner() {
+    public List<RacingCar> getWinner() {
         return  IntStream.range(0, racingCars.size())
                 .filter(i -> racingCars.get(i).getCurrentPosition() == winnerPosition)
-                .mapToObj(i -> racingCars.get(i).getCarName())
+                .mapToObj(i -> racingCars.get(i))
                 .collect(Collectors.toList());
     }
 }
--- src/main/java/racingcar/view/RenderingView.java
+++ src/main/java/racingcar/view/RenderingView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import racingcar.domain.RacingCar;
 import racingcar.domain.RacingCars;
 
 import java.util.Collections;
@@ -27,8 +28,16 @@ public class RenderingView {
     }
 
     public void renderWinner(RacingCars racingCars) {
-        String winners = racingCars.getWinner().stream().collect(joining(WINNER_POSTFIX_EXPRESSION));
-        System.out.print(winners + "가 우승했습니다.");
+        List<RacingCar> winners = racingCars.getWinner();
+        String renderWinner = parseWinnerToString(winners);
+
+        System.out.println(renderWinner + "가 우승했습니다.");
+    }
+
+    private String parseWinnerToString(List<RacingCar> winners) {
+       return winners.stream()
+                .map(racingCar -> racingCar.getCarName())
+                .collect(joining(WINNER_POSTFIX_EXPRESSION));
     }
 
 
