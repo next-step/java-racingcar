b05ba81a8c96446dc31512497cd2d899850d657b
mand2 <nayeon.kim1123@gmail.com>
2020-11-11 21:37:55 +0900

    refactor: 사용하지 않는 변수 삭제

--- src/main/java/step3/domain/Car.java
+++ src/main/java/step3/domain/Car.java
@@ -13,7 +13,6 @@ public class Car {
     private final String name;
     private int step; //자동차가 전진한 횟수
     private final int MAXIMUM_CAR_NAME_LENGTH = 5;
-    private final int MOVE_JUDGE_NUMBER = 4;
 
     public Car(String name) {
         this(name, 0);
@@ -35,7 +34,7 @@ public class Car {
     public int getStep() {
         return this.step;
     }
- 
+
     public String getName() {
         return name;
     }
