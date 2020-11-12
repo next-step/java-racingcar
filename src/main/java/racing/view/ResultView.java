992497ce0bf0dc0f7e34b6891e9c18123ff455a8
hanogo <trullo393@gmail.com>
2020-11-13 02:52:17 +0900

    [feat] main 및 view 구현

--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -7,17 +7,22 @@ import java.util.stream.Stream;
 
 public class ResultView {
     private static final String POSITION = "-";
+    private static final String CONNECT_CHAR = " : ";
 
     private ResultView() {
     }
 
-    public static void printCarPosition(Cars cars) {
-        cars.getCarList().forEach(car -> System.out.println(repeat(car.getPosition())));
+    public static void printCarNameAndPosition(Cars cars) {
+        cars.getCarList().forEach(car -> System.out.println(car.getName() + CONNECT_CHAR + repeat(car.getPosition())));
     }
 
     private static String repeat(int count) {
         return Stream.generate(() -> POSITION)
                 .limit(count)
                 .collect(Collectors.joining());
+    }
+
+    public static void printVictoryCarNames(String victoryCarNames) {
+        System.out.println(victoryCarNames + "가 최종우승 했습니다.");
     }
 }
