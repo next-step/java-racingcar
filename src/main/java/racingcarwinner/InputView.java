88418dd245a7d3193864dce173a905481fa19492
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-12 23:29:33 +0900

    feat : InputView 클래스 구현

--- src/main/java/racingcarwinner/InputView.java
+++ src/main/java/racingcarwinner/InputView.java
@@ -1,2 +1,24 @@
-package racingcarwinner;public class InputView {
+package racingcarwinner;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private InputView(){}
+
+    public static void inputCarsName(){
+        System.out.println("경주 할 자동차의 이름을 입력해주세요(이름은 쉽표(,)로 구분).");
+        String carsName = new Scanner(System.in).nextLine();
+        System.out.println(carsName);
+
+        Cars.createCar(carsName);
+    }
+
+    public static void inputRoundCount(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        int roundCount = new Scanner(System.in).nextInt();
+        System.out.println(roundCount);
+
+        GameController.createGame(roundCount);
+    }
 }
