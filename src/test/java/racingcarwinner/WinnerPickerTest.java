7635a2180cda74a70e0aa34d6ba995b0c89589bf
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-12 11:47:26 +0900

    test : WinnerPicker 테스트케이스 추가

--- src/test/java/racingcarwinner/WinnerPickerTest.java
+++ src/test/java/racingcarwinner/WinnerPickerTest.java
@@ -1,2 +1,24 @@
-package racingcarwinner;public class WinnerPicker {
+package racingcarwinner;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class WinnerPickerTest {
+
+    @Test
+    @DisplayName("우승자의 이동거리가 가장 큰 것이 맞는가")
+    public void winnertravelDistanceTest(){
+        int largestTravelDistance = 0;
+        for (int i = 0; i < Cars.getCars().length; i++) {
+            if(Cars.getCars().getTravelDistance() > largestTravelDistance){
+                largestTravelDistance = Cars.getCars().getTravelDistance();
+            }
+        }
+
+        for (int i = 0; i < winner.length; i++) {
+            assertThat(winner[i].getTravelDistance()).isEquals(largestTravelDistance);
+        }
+
+    }
+
 }
