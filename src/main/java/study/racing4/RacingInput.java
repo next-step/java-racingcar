7c868021253921a44bf5fef863c1d8c4c20bf1a5
Jung-Soo Ahn <staringstar@gmail.com>
2020-11-12 22:20:58 +0900

    step4 - feat racing input 추가

---
+++ src/main/java/study/racing4/RacingInput.java
@@ -0,0 +1,38 @@
+package study.racing4;
+
+public class RacingInput {
+
+    private final String[] cars;
+    private final int countOfRound;
+
+    public RacingInput(String rawCarsString, int countOfRound) {
+        validateBlank(rawCarsString);
+        validateCountOfRound(countOfRound);
+        this.cars = parseCarsString(rawCarsString);
+        this.countOfRound = countOfRound;
+    }
+
+    private void validateBlank(String rawCarsString) {
+        if(rawCarsString.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름 입력값이 비어 있습니다.");
+        }
+
+    }
+    private void validateCountOfRound(int countOfRound) {
+        if (countOfRound <= 0) {
+            throw new IllegalArgumentException("라운드 수는 0 보다 커야합니다.");
+        }
+    }
+
+    private String[] parseCarsString(String rawCarsString) {
+        return rawCarsString.split(",");
+    }
+
+    public String[] getCars() {
+        return cars;
+    }
+
+    public int getCountOfRound() {
+        return countOfRound;
+    }
+}
