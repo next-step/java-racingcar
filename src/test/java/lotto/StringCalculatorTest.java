d1229eb180345394c3a15c7cf74f85d87017f2bf
이병무 <am003507@gmail.com>
2020-11-12 09:41:48 +0900

    styles - 테스트 코드 @DisplayName 변경

--- src/test/java/lotto/StringCalculatorTest.java
+++ src/test/java/lotto/StringCalculatorTest.java
@@ -65,7 +65,7 @@ class StringCalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다")
     @ParameterizedTest
     @CsvSource(value = {
             "1,2-3",
