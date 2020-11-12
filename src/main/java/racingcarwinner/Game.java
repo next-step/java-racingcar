fe94b9162cb7ed26a762a6c35ee553ac17aa97d7
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-12 23:28:51 +0900

    feat : 게임 객체 구현

--- src/main/java/racingcarwinner/Game.java
+++ src/main/java/racingcarwinner/Game.java
@@ -1,2 +1,33 @@
-package racingcarwinner;public class GameTest {
+package racingcarwinner;
+
+public class Game {
+
+    private int roundCount;
+    private final int ENABLE_MOVE_NUMBER = 4;
+
+    private Game(){}
+
+    public Game(int roundCount){
+        this.roundCount = roundCount;
+    }
+
+    public int getRoundCount(){
+        return this.roundCount;
+    }
+
+    public boolean isOrAboveEnableNumber(int randomNumber){
+        return randomNumber >= this.ENABLE_MOVE_NUMBER;
+    }
+
+    public void randomMove(Car car){
+        if(this.isOrAboveEnableNumber(RandomGenerator.getRandomNumber())){
+            car.move();
+        }
+    }
+
+    public static String[] splitCarsName(String StringCarsName){
+        return StringCarsName.split(",");
+    }
+
+
 }
