715d30da5c96e7f3c95e8044cc9770d34ea1a5a7
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-12 23:28:30 +0900

    feat : 자동차 일급컬렉션 구현

--- src/main/java/racingcarwinner/Cars.java
+++ src/main/java/racingcarwinner/Cars.java
@@ -1,2 +1,29 @@
-package racingcarwinner;public class Cars {
+package racingcarwinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private static List<Car> cars = new ArrayList();
+
+    public static void createCar(String carsName){
+
+        String[] carsNameArray = Game.splitCarsName(carsName);
+
+        for (int i = 0; i < carsNameArray.length; i++) {
+            cars.add(new Car(carsNameArray[i]));
+        }
+    }
+
+    public static Car getCar(int index){
+        return cars.get(index);
+    }
+
+    public static int getCarsCount(){
+        return cars.size();
+    }
+
+
+
 }
