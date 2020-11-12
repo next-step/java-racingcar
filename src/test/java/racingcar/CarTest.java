3a6b61e989e73ee6765953d716d0ce13149f1ede
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-13 08:27:04 +0900

    test : Car 테스트 코드 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -8,17 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
     private Car car;
-    private static final int ATTEMPTS = 5;
+    final static int START_LOCATION = 0;
 
     @BeforeEach
     public void setUp() {
-        car = new Car(ATTEMPTS);
+        car = new Car(START_LOCATION);
     }
 
     @Test
-    @DisplayName("moves 배열 검증")
-    public void generateMoves() {
-        car.generateMoves();
-        assertThat(car.getMoves().length).isEqualTo(ATTEMPTS);
+    @DisplayName("자동차가 움직이면 위치가 변하는지 확인")
+    public void move_test() {
+        car.move();
+        assertThat(car.getLocation()).isEqualTo(START_LOCATION +1);
     }
 }
