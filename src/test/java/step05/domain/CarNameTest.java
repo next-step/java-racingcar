02913376b6bf67efd2d4369d95365cc8f42ce97c
eyabc <bey4314@naver.com>
2020-11-12 19:12:20 +0900

    test: 이름이 5자 이하일 때 에러 없음

--- src/test/java/step05/domain/CarNameTest.java
+++ src/test/java/step05/domain/CarNameTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class CarNameTest {
@@ -35,13 +36,30 @@ public class CarNameTest {
     @DisplayName("이름이 5자 초과일때 에러")
     @ParameterizedTest
     @MethodSource("provideOver5LengthCarName")
-    public void isInBoundLength__over5LengthCarName_throwError(String outOfBoundCarName) {
+    public void isInBoundLength__over5LengthCarName_throwException(String outOfBoundCarName) {
         assertThatExceptionOfType(OutOfBoundCarName.class)
                 .isThrownBy(() -> CarName.of(outOfBoundCarName));
     }
 
+    private static Stream<Arguments> provideUnder5LengthCarName() {
+        return Stream.of(
+                Arguments.of(
+                        "12345",
+                        "asd",
+                        "a"
+                )
+        );
+    }
+
+    @DisplayName("이름이 5자 이하일 때 에러 없음")
+    @ParameterizedTest
+    @MethodSource("provideUnder5LengthCarName")
+    public void isInBoundLength__under5LengthCarName_NotThrowException(String InBoundCarName) {
+        assertThatCode(() -> CarName.of(InBoundCarName))
+                .doesNotThrowAnyException();
+    }
+
     /*
-        - 이름이 5자 이하인지 검사 private void isInBoundLength
         - 이름 객체 반환 public CarName getName
      */
 }
