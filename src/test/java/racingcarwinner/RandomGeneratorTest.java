b796bf3055968f248b4813bbb28ccbec56b0bf7e
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-12 11:47:01 +0900

    test : RandomGenerator 테스트케이스 추가

--- src/test/java/racingcarwinner/RandomGeneratorTest.java
+++ src/test/java/racingcarwinner/RandomGeneratorTest.java
@@ -1,2 +1,16 @@
-package racingcarwinner;public class RandomGeneratorTest {
+package racingcarwinner;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.assertTrue;
+
+public class RandomGeneratorTest {
+
+    @Test
+    @DisplayName("랜덤 값이 0과 같거나 크고 10보다 작은가?")
+    public void EnableRandomNumber(){
+        assertTrue(RandomGenerator.getRandomNumber() >= 0);
+        assertTrue(RandomGenerator.getRandomNumber() < 10);
+    }
 }
