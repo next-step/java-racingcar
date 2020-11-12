b63d8c95fd63b3c3d4ceaaf57a3cc97d48f94c49
hanogo <trullo393@gmail.com>
2020-11-13 00:37:51 +0900

    [feat] inputView inputNumberOfCars -> inputNameOfCars

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -8,9 +8,9 @@ public class InputView {
     private InputView() {
     }
 
-    public static int inputNumberOfCars() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return sc.nextInt();
+    public static String inputNameOfCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return sc.nextLine();
     }
 
     public static int inputNumberOfRound() {
