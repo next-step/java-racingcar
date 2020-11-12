389451a7691d4d47eb91dd1cab6ffba4235a8bf9
이병무 <am003507@gmail.com>
2020-11-12 15:30:08 +0900

    feat - null or 빈 문자열이 들어왔을때 처리

--- src/main/java/lotto/StringCalculator.java
+++ src/main/java/lotto/StringCalculator.java
@@ -3,6 +3,9 @@ package lotto;
 public class StringCalculator {
 
     public Integer enterExpression(String expression) {
+        if (expression == null || expression.isEmpty()) {
+            return 0;
+        }
         return -1;
     }
 }
