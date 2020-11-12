838b00a8d6b323bab5407bac7e49636700f92bd0
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-13 00:09:44 +0900

    test : 자동차 객체 생성 테스트 추가

--- src/test/java/racingcarwinner/CarsTest.java
+++ src/test/java/racingcarwinner/CarsTest.java
@@ -4,19 +4,18 @@ import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class CarsTest {
 
     @Test
     @DisplayName("자동차 객체가 제대로 생성되는가?")
     public void createCarTest(){
-        String[] strings = {"포비","코난","베니"};
+        String strings = "포비,코난,베니";
 
-        Cars cars = new Cars(strings);
-        assertThat(cars.getCar(0).getName()).isEquals("포비");
-        assertThat(cars.getCar(1).getName()).isEquals("코난");
-        assertThat(cars.getCar(2).getName()).isEquals("베니");
+        Cars.createCar(strings);
+        assertThat(Cars.getCar(0).getName()).isEqualTo("포비");
+        assertThat(Cars.getCar(1).getName()).isEqualTo("코난");
+        assertThat(Cars.getCar(2).getName()).isEqualTo("베니");
     }
 
 }
