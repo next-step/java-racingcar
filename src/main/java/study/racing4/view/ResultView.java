f81f18ea4fda1ee338a4ef6df0ea24075bcc69e9
Jung-Soo Ahn <staringstar@gmail.com>
2020-11-12 22:23:16 +0900

    step4 - feat result view 에 winner 출력 추가

--- src/main/java/study/racing4/view/ResultView.java
+++ src/main/java/study/racing4/view/ResultView.java
@@ -17,4 +17,19 @@ public class ResultView {
         System.out.println();
     }
 
+    public void printWinner(List<Car> cars) {
+
+        int lastCar = cars.size();
+        int count = 1;
+        for(Car car: cars) {
+
+            System.out.print(car.getName());
+            if(count++ < lastCar) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println("가 최종 우승했습니다.");
+    }
+
 }
