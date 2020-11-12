6dfeb04dc51dfa77307746f694bdb2a8ecdf2be2
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-13 00:10:39 +0900

    test : 경기 수 입력 테스트 추가

--- src/test/java/racingcarwinner/GameControllerTest.java
+++ src/test/java/racingcarwinner/GameControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
-public class GameTest {
+public class GameControllerTest {
 
     @Test
     @DisplayName("차 이름 문자열 쪼개기")
@@ -20,8 +20,7 @@ public class GameTest {
     @Test
     @DisplayName("경기 수가 제대로 입력 되었는가")
     public void countRoundTest(){
-        InputView.inputRoundCount(5);
-
-        assertTrue(racingcarwinner.Game.getRound() == 1);
+        GameController.createGame(5);
+        assertTrue(GameController.getGame().getRoundCount() == 5);
     }
 }
