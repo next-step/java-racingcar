a2c0afef89bba6c6b8bb0ec565ded26dee80b25e
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-12 23:31:52 +0900

    feat : 우승자 선택 클래스 구현

--- src/main/java/racingcarwinner/WinnerPicker.java
+++ src/main/java/racingcarwinner/WinnerPicker.java
@@ -1,2 +1,25 @@
-package racingcarwinner;public class WinnerPicker {
+package racingcarwinner;
+
+public class WinnerPicker {
+
+    private WinnerPicker() {}
+
+    public static void pickWinner(){
+        Winner.addWinner(Cars.getCar(0).getName());
+
+        for (int i = 1; i < Cars.getCarsCount(); i++) {
+            figureOutWinner(Cars.getCar(i-1), Cars.getCar(i));
+        }
+
+    }
+
+    public static void figureOutWinner(Car firstCar, Car secondCar){
+        if(firstCar.getTravelDistance() < secondCar.getTravelDistance()){
+            Winner.clearWinner();
+            Winner.addWinner(secondCar.getName());
+        }
+        if(firstCar.getTravelDistance() == secondCar.getTravelDistance()){
+            Winner.addWinner(secondCar.getName());
+        }
+    }
 }
