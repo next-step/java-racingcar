e3b7c2541dd53d84ecb4d37975d3807fcd6f6f50
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-12 11:46:40 +0900

    test : Game 테스트케이스 추가

--- src/test/java/racingcarwinner/GameTest.java
+++ src/test/java/racingcarwinner/GameTest.java
@@ -1,2 +1,27 @@
-package racingcarwinner;public class GameTest {
+package racingcarwinner;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import racingcar.Game;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class GameTest {
+
+    @Test
+    @DisplayName("차 이름 문자열 쪼개기")
+    public void splitCarNameTest(){
+        String[] CarNames = {"포비", "코난", "베니"};
+        assertThat(Game.splitCarName("포비,코난,베니")).
+                isEquals(CarNames);
+    }
+
+    @Test
+    @DisplayName("경기 수가 제대로 입력 되었는가")
+    public void countRoundTest(){
+
+        InputView.inputRoundCount(1);
+        assertTrue(racingcarwinner.Game.getRound() == 1);
+    }
 }
