99072be6bb087574b54975844c71dfbb335ea7a7
rlawls1991 <rlawls1991@gmail.com>
2020-11-13 16:59:07 +0900

    style

---
+++ src/main/java/carRace/step5/domain/util/ErrorMessage.java
@@ -0,0 +1,34 @@
+package carRace.step5.domain.util;
+
+public class ErrorMessage {
+
+    private static String NULL_EXCEPTION = "Null을 입력하였습니다. 숫자를 입력해주세요";
+    private static String EMPTY_EXCEPTION = "입력하지 않으셨습니다. 숫자를 입력해주세요";
+    private static String NUMBERFORMATEXCEPTION = "문자를 입력하였습니다. 숫자를 입력해주세요";
+    private static String ARRAYINDEXOUTOFBOUNDSEXCEPTION = "자동차의 갯수를 1개이상으로 입력해주세요";
+    private static String INDEXOUTOFBOUNDSEXCEPTION = "이름 길이가 5초과하였습니다.";
+
+    private ErrorMessage() {
+
+    }
+
+    public static String getNullException() {
+        return NULL_EXCEPTION;
+    }
+
+    public static String getEmptyException() {
+        return EMPTY_EXCEPTION;
+    }
+
+    public static String getNUMBERFORMATEXCEPTION() {
+        return NUMBERFORMATEXCEPTION;
+    }
+
+    public static String getARRAYINDEXOUTOFBOUNDSEXCEPTION() {
+        return ARRAYINDEXOUTOFBOUNDSEXCEPTION;
+    }
+
+    public static String getINDEXOUTOFBOUNDSEXCEPTION() {
+        return INDEXOUTOFBOUNDSEXCEPTION;
+    }
+}
