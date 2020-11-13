7b692b46dd6282ccc461571b51f954d4bcf13e91
Jung-Soo Ahn <staringstar@gmail.com>
2020-11-14 00:50:32 +0900

    step4 - refactor resultView carCollection 기반으로 변경

--- src/main/java/study/racing4/view/ResultView.java
+++ src/main/java/study/racing4/view/ResultView.java
@@ -1,6 +1,7 @@
 package study.racing4.view;
 
 import study.racing4.Car;
+import study.racing4.CarCollection;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,13 +11,14 @@ public class ResultView {
 
     private static final char POSITION_MARK = '-';
 
-    public void printCarPosition(List<Car> cars) {
-        for (Car car : cars) {
+    public void printCarPosition(CarCollection cars) {
+
+        cars.apply(car -> {
             System.out.print(car.getName() + " : " + POSITION_MARK);
             IntStream.range(0, car.getPosition())
                     .forEach(t -> System.out.print(POSITION_MARK));
             System.out.println();
-        }
+        });
         System.out.println();
     }
 
