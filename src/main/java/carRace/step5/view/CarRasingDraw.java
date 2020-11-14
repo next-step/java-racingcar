2d6498b9c2dcdd179ef768f621b816a7c8a76357
rlawls1991 <rlawls1991@gmail.com>
2020-11-14 23:46:45 +0900

    feat 자동차 경주기록, 우승자 그려주는 객체

---
+++ src/main/java/carRace/step5/view/CarRasingDraw.java
@@ -0,0 +1,29 @@
+package carRace.step5.view;
+
+import carRace.step5.domain.util.CarName;
+
+import java.util.List;
+
+public class CarRasingDraw {
+
+    public static void printCarDistance(String carName, int moveDistance){
+        System.out.println(carName + " : " + changeNumberToChar(moveDistance));
+    }
+
+    public static void printWinner(List<String> winnerList){
+        System.out.print("우승자 : " + winnerList.get(0));
+        for (int i = 1; i < winnerList.size(); i++) {
+            System.out.print(", " + winnerList.get(i));
+        }
+    }
+
+    private static String changeNumberToChar(int count) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < count; i++) {
+            sb.append("-");
+        }
+
+        return sb.toString();
+    }
+}
