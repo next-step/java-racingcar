ffefdf1714108610fce9474607a2c25fe36885b7
MJ Choi <1621139@gmail.com>
2020-11-15 01:51:51 +0900

    fix: DELETE round class

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -2,7 +2,6 @@ package racingcar.controller;
 
 import racingcar.behavior.RandomMovingStrategy;
 import racingcar.domain.RacingCars;
-import racingcar.domain.Round;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
@@ -10,7 +9,6 @@ public class RacingController {
     public static void main(String[] args) {
         String[] nameOfCars = InputView.inputNameOfCars();
         int loopCount = InputView.inputLoopCount();
-        Round round = new Round(loopCount);
 
         RacingCars racingCars = RacingCars.of(nameOfCars, new RandomMovingStrategy(), round);
         ResultView.printResultMessage();
