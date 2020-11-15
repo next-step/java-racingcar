5424f6e619bf1d4e65af494fe1472d334ff8aa52
rlawls1991 <rlawls1991@gmail.com>
2020-11-16 02:29:18 +0900

    refactor 데이터 입력받는 부분 view로 옮김

--- src/main/java/carRace/step5/controller/CarRaceGameController.java
+++ src/main/java/carRace/step5/controller/CarRaceGameController.java
@@ -2,27 +2,17 @@ package carRace.step5.controller;
 
 
 import carRace.step5.domain.CarRacingGame;
-import carRace.step5.domain.util.CarName;
-import carRace.step5.domain.util.TryCount;
-
-import java.util.Scanner;
+import carRace.step5.domain.util.CarNamesCheck;
+import carRace.step5.domain.util.TryCountCheck;
+import carRace.step5.view.RaceInputView;
 
 public class CarRaceGameController {
 
-    private static String INPUT_CAR_RACE_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private static String HOW_MANY_ATTEMPTS = "시도할 회수는 몇 회 인가요?";
-
-    private static Scanner scanner = new Scanner(System.in);
-
-
     public static void main(String[] args) {
+        String[] carNames = CarNamesCheck.checkCarNames(RaceInputView.inputRaceNames());
+        int raceTryCount = TryCountCheck.checkNumberValue(RaceInputView.inputTryCount());
 
-        System.out.println(INPUT_CAR_RACE_NAMES);
-        String[] carNames = CarName.checkCarNames(scanner.nextLine());
-        System.out.println(HOW_MANY_ATTEMPTS);
-        int raceTryCount = TryCount.checkNumberValue(scanner.nextLine());
-
-        CarRacingGame carRacingGame = new CarRacingGame(raceTryCount);
-        carRacingGame.RunPlayGame(carNames);
+        CarRacingGame carRacingGame = new CarRacingGame(raceTryCount, carNames);
+        carRacingGame.RunPlayGame();
     }
 }
