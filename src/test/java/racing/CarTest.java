4ec1571fe5eaa4eb0509bab187a9e298d0a826a6
monkeyDugi <lbd4946@gmail.com>
2020-11-16 02:32:54 +0900

    test : 테스트 추가

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -3,20 +3,23 @@ package racing;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CarTest {
 
     @DisplayName(value = "랜덤 값이 4이상이면, 전진")
-    @Test
-    void forwardMove() {
+    @ParameterizedTest
+    @CsvSource(value = {"0,1, 1,2, 2,3"})
+    void forwardMove(int oldPosition, int newPosition) {
         // given
-        Car car = new Car(0);
+        Car car = new Car(oldPosition);
 
         // when
         car.move(() -> true);
 
         // then
-        Assertions.assertThat(car.getPosition()).isEqualTo(1);
+        Assertions.assertThat(car.getPosition()).isEqualTo(newPosition);
     }
 
     @DisplayName(value = "랜덤 값이 4미만이면, 정지")
