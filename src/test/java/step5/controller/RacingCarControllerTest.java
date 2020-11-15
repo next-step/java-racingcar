937d3961b5f98e4f83a63132f5bf570dc265096c
dbeod2 <darung.0211@gmail.com>
2020-11-15 16:04:15 +0900

    refactor : 게임 진행 이동 횟수 정상 테스트 코드 작성

--- src/test/java/step5/controller/RacingCarControllerTest.java
+++ src/test/java/step5/controller/RacingCarControllerTest.java
@@ -3,6 +3,8 @@ package step5.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import step5.domain.Car;
 
 import java.util.List;
@@ -12,16 +14,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RacingCarControllerTest {
 
     RacingCarController racingCarController;
+    List<Car> cars;
+    String[] carNames = "zu,cu,du".split(",");
 
     @BeforeEach
     void setUp() {
         racingCarController = new RacingCarController();
+        racingCarController.start(carNames);
     }
 
     @Test
     @DisplayName("입력한 자동차 갯수 일치 확인")
     public void 입력한_자동차_갯수_일치_확인() {
-        String[] carNames = "zu,cu,du".split(",");
         List<Car> cars = racingCarController.start(carNames);
         assertEquals(carNames.length, cars.size());
     }
@@ -29,10 +33,16 @@ public class RacingCarControllerTest {
     @Test
     @DisplayName("입력한 자동차 값이 정상 등록되는지")
     public void 등록_자동차_확인() {
-        String[] carNames = "zu,cu,du".split(",");
-        List<Car> cars = racingCarController.start(carNames);
         assertEquals(cars.get(0).getCarName(), "zu");
         assertEquals(cars.get(1).getCarName(), "cu");
         assertEquals(cars.get(2).getCarName(), "du");
+    }
+
+    @ParameterizedTest
+    @DisplayName("게임 진행 이동 횟수 정상확인")
+    @ValueSource(ints = {1, 3, 5})
+    public void 게임_진행_이동횟수_정상확인(int values) {
+        int moveCount = racingCarController.carMove(values);
+        assertEquals(moveCount, values);
     }
 }
