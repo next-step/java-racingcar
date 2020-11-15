3ef06679fe557e6e7bdca4158e496fcf291bdd76
apalsl <apalsl123@gmail.com>
2020-11-15 19:07:35 +0900

    add DisplayName in Test

--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -1,17 +1,17 @@
 package racingcar.domain;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-class RacingCarListTest {
+class RacingCarsTest {
 
     @Test
+    @DisplayName("여러개의 Car 중 Position값이 가장 높은 객체의 int값을 찾는다.")
     public void findMaxPosition() {
         Car car1 = new Car("bus", 5);
         Car car2 = new Car("car", 4);
@@ -20,11 +20,12 @@ class RacingCarListTest {
         carList.add(car1);
         carList.add(car2);
 
-        RacingCarList racingCarList = new RacingCarList(carList);
-        assertThat(racingCarList.findMaxPosition()).isEqualTo(5);
+        RacingCars racingCars = new RacingCars(carList);
+        assertThat(racingCars.findMaxPosition()).isEqualTo(5);
     }
 
     @Test
+    @DisplayName("여러대의 Car 중 Position값이 가장 큰 객체들을 반환한다.")
     public void findWinners() {
         Car car1 = new Car("bus",3);
         Car car2 = new Car("car",4);
@@ -33,15 +34,12 @@ class RacingCarListTest {
         carList.add(car1);
         carList.add(car2);
 
-        RacingCarList racingCarList = new RacingCarList(carList);
-        List<CarDTO> winners = racingCarList.findWinners();
-
+        RacingCars racingCars = new RacingCars(carList);
+        List<Car> winners = racingCars.findWinners();
         assertThat(winners.size()).isEqualTo(1);
 
-        for (CarDTO dto : winners) {
-            assertThat(dto.getName()).isEqualTo("car");
+        for (Car car : winners) {
+            assertThat(car).isEqualTo(car2);
         }
-
     }
-
 }
