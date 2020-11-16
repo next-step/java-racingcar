327b8b37b3089060384bf40e7025760c4644264e
sehan <sehan.choi@gmail.com>
2020-11-16 20:48:22 +0900

    refactor: 이동조건 로직을 MoveCondition 구현체 내부로 이동

--- src/main/java/step3/domain/CarRacing.java
+++ src/main/java/step3/domain/CarRacing.java
@@ -2,22 +2,26 @@ package step3.domain;
 
 public class CarRacing {
 
-    private final RaceRoulette DEFAULT_RACE_ROULETTE = RaceRoulette.simple(9);
-
     private Cars cars;
-
     private int totalTries;
 
-    private RaceRoulette raceRoulette;
+    private MoveCondition moveCondition = new MoveCondition() {
+        static final int MIN_FOR_MOVE = 4;
+
+        private RaceRoulette raceRoulette = RaceRoulette.simple(9);
+        @Override
+        public boolean match() {
+            return raceRoulette.spin() >= MIN_FOR_MOVE;
+        }
+    };
 
     public CarRacing(String carNames, int totalTries) {
         this.cars = new Cars(carNames);
         this.totalTries = totalTries;
-        this.raceRoulette = DEFAULT_RACE_ROULETTE;
     }
 
     public void tryMovingAllCars() {
-        this.cars.tryMoving(() -> raceRoulette.spin() >= 4);
+        this.cars.tryMoving(moveCondition);
     }
 
     public boolean isInCarRacing(){
