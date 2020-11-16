365068794cdbbce90b9bd06d988de03be69281fb
dbeod2 <darung.0211@gmail.com>
2020-11-16 20:12:09 +0900

    refactor : 변경된 코드로 인한 테스트 코드 수정

--- src/test/java/step5/controller/RacingCarControllerTest.java
+++ src/test/java/step5/controller/RacingCarControllerTest.java
@@ -1,48 +1,43 @@
 package step5.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import step5.domain.Car;
 
-import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RacingCarControllerTest {
 
-    RacingCarController racingCarController;
-    List<Car> cars;
     String[] carNames = "zu,cu,du".split(",");
-
-    @BeforeEach
-    void setUp() {
-        racingCarController = new RacingCarController();
-        racingCarController.start(carNames);
-    }
 
     @Test
     @DisplayName("입력한 자동차 갯수 일치 확인")
     public void 입력한_자동차_갯수_일치_확인() {
-        List<Car> cars = racingCarController.start(carNames);
-        assertEquals(carNames.length, cars.size());
+        RacingCarController racingCarController = RacingCarController.of(carNames, 4);
+        racingCarController.gameProgress();
+        assertEquals(carNames.length, racingCarController.getCars().size());
     }
 
     @Test
     @DisplayName("입력한 자동차 값이 정상 등록되는지")
     public void 등록_자동차_확인() {
-        assertEquals(cars.get(0).getCarName(), "zu");
-        assertEquals(cars.get(1).getCarName(), "cu");
-        assertEquals(cars.get(2).getCarName(), "du");
+        RacingCarController racingCarController = RacingCarController.of(carNames, 4);
+        assertEquals(racingCarController.getCars().get(0).getCarName(), "zu");
+        assertEquals(racingCarController.getCars().get(1).getCarName(), "cu");
+        assertEquals(racingCarController.getCars().get(2).getCarName(), "du");
     }
 
     @ParameterizedTest
-    @DisplayName("게임 진행 이동 횟수 정상확인")
-    @ValueSource(ints = {1, 3, 5})
-    public void 게임_진행_이동횟수_정상확인(int values) {
-        int moveCount = racingCarController.carMove(values);
-        assertEquals(moveCount, values);
+    @DisplayName("마지막 게임인 경우 true")
+    @ValueSource(ints = {1, 3, 7})
+    public void 마지막_게임_확인(int values) {
+        RacingCarController racingCarController = RacingCarController.of(carNames, values);
+        IntStream.range(0, values)
+                .forEach(i -> racingCarController.gameProgress());
+        assertTrue(racingCarController.isFinish());
     }
 }
