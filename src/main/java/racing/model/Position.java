7a9113b3b17ebb61c3246b85c0c2991288343c15
HoJin <dudu016@naver.com>
2020-11-16 14:47:43 +0900

    refactor: 차의 위치 값을 position Class 로 분리 (#1541)

---
+++ src/main/java/racing/model/Position.java
@@ -0,0 +1,13 @@
+package racing.model;
+
+public class Position {
+    private int position = 0;
+
+    public void increase(){
+        position++;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+}
