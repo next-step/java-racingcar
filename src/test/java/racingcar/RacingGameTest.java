8ac5f5a063e6aec123d9974df9e3a629f784e8bd
kim-daeyong <kdy2353@gmail.com>
2020-11-17 23:46:03 +0900

    test 작성

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class RacingGameTest {
-    RacingGame racingGame;
+    RacingGame racingGame = new RacingGame();
 
     @Test
-    void setInRacingTest(){
-        
+    void placetForRacingTest(){
+        List<Integer> list =new ArrayList<Integer>(Arrays.asList(3,5));
+        Scoreboard board = racingGame.placetForRacing(list);
+        assertThat(board.getCars().size()).isEqualTo(3);
+        assertThat(board.getMove()).isEqualTo(5);
     }
     
 }
