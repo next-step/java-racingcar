663129acab0d6e503502b8bc6182297a6662fe38
hoonick <wecube.1@gmail.com>
2020-11-17 20:19:06 +0900

    refactor(RacingGame): Racing의 전체 진행 책임을 RacingGame 클래스로 변경

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -4,20 +4,48 @@ import racingcar.strategy.MovingStrategy;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.Name;
 import racingcar.domain.car.Cars;
+import racingcar.strategy.RandomMovingStrategy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingGame {
 
-    public Cars createCars(List<String> carNames) {
+    private final String racingCarNames;
+    private final int racingCount;
+    private final static String BLANK = "";
+    private final static String BLANK_STRING = " ";
+    private static final String SPLIT_LETTER = ",";
+
+    public RacingGame(String racingCarNames, int racingCount) {
+        validRacingCount(racingCount);
+        validRacingCarNames(racingCarNames);
+        this.racingCarNames = racingCarNames;
+        this.racingCount = racingCount;
+    }
+
+    public Cars createCars() {
+        List<String> carNames = Arrays.stream(racingCarNames.split(SPLIT_LETTER))
+                .collect(Collectors.toList());
+
         List<Car> cars = new ArrayList<>();
         for (String carName : carNames) {
             Name name = new Name(carName);
             cars.add(new Car(name));
         }
         return new Cars(cars);
+    }
+
+    public List<Cars> start() {
+        List<Cars> records = new ArrayList<>();
+        Cars cars = createCars();
+        for (int i = 0; i < racingCount; i++) {
+            Cars postRaceCars = race(cars, new RandomMovingStrategy());
+            records.add(postRaceCars);
+        }
+        return records;
     }
 
     public Cars race(Cars cars, MovingStrategy movableGenerator) {
@@ -28,7 +56,6 @@ public class RacingGame {
     }
 
     public Cars getWinners(Cars cars) {
-
         int winnerPosition = cars.getValue().stream()
                 .mapToInt(car -> car.getPosition().getValue())
                 .max()
@@ -39,5 +66,17 @@ public class RacingGame {
                 .collect(Collectors.toList());
 
         return new Cars(carList);
+    }
+
+    private void validRacingCount(int racingCount) {
+        if (racingCount < 0) {
+            throw new IllegalArgumentException("racing count is not available");
+        }
+    }
+
+    private void validRacingCarNames(String racingCarNames) {
+        if (racingCarNames.equals(BLANK) || racingCarNames.equals(BLANK_STRING) || racingCarNames.isEmpty()) {
+            throw new IllegalArgumentException("racing car name is wrong");
+        }
     }
 }
