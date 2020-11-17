592c2ada9d672a2ae049b2cb98f8d3c741d027c3
HRLEE <apalsl123@gmail.com>
2020-11-17 20:45:42 +0900

    add RacingGameMain (#1542)
    1. RacingGameMain에서 inputView를 통하여 사용자가 입력한 값을 받아서 Controller로 전송
    2. Controller에서 입력받은 데이터로 RacingGame (Service)를 호출하여 작업 후 도메인 리턴받음
    3. Controller에서 리턴받은 도메인을 DTO로 변경 후 요청자인 RacingGameMain으로 리턴
    4. RacingGameMain에서 결과를 보여주기 위한 ResultView로 DTO 전송
    5. DTO를 기반으로 데이터를 파싱 후 프린트

--- src/main/java/racingcar/ui/ResultView.java
+++ src/main/java/racingcar/ui/ResultView.java
@@ -3,15 +3,23 @@ package racingcar.ui;
 import racingcar.domain.CarDTO;
 
 import java.util.List;
+import java.util.Map;
 
 public class ResultView {
     private static final int FIRST = 0;
-    public void showRacingResult(List<CarDTO> racingResult) {
-        for (CarDTO carDTO : racingResult) {
+
+    public void showRacingResult(Map<Integer, List<CarDTO>> racingResult) {
+        for (List<CarDTO> result  : racingResult.values()) {
+            showCarPosition(result);
+        }
+    }
+
+    private void showCarPosition(List<CarDTO> result) {
+        for (CarDTO carDTO : result) {
             System.out.println(
                     new StringBuilder().append(carDTO.getName())
-                    .append(" : ")
-                    .append(carDTO.getPosition())
+                            .append(" : ")
+                            .append(carDTO.getPosition())
             );
         }
         System.out.println();
