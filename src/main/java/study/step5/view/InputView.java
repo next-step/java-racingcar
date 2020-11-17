0d40461bec0d3828a269a5c1cf144ad33c248603
최종원 <90lastone@gmail.com>
2020-11-17 23:07:26 +0900

    feat: InputView 구현

---
+++ src/main/java/study/step5/view/InputView.java
@@ -0,0 +1,39 @@
+package study.step5.view;
+
+import study.step4.ResultView;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
+    /* 자동차 이름 입력 */
+    public static String inputCars() {
+        return inputString("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+
+    /**
+     * inputString 입력 받기위한 안내 문구 및 입력 받기
+     * @param message
+     * @return 입력받은 문자열
+     */
+    private static String inputString(String message) {
+        ResultView.printlnMessage(message);
+        return scanner.next();
+    }
+
+    /* 시도 회수 입력 */
+    public static int inputRacingCount() {
+        return inputInt("시도할 회수는 몇회인가요?");
+    }
+
+    /**
+     * inputInt 입력 받기위한 안내 문구 및 입력 받기
+     * @param message
+     * @return 입력받은 숫자
+     */
+    private static int inputInt(String message) {
+        Scanner scanner = new Scanner(System.in);
+        ResultView.printlnMessage(message);
+        return scanner.nextInt();
+    }
+}
