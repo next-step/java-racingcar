01218464f25f791e2f8db71e8d32ea8a9b38cef1
yeGenieee <cyj95428@gmail.com>
2020-11-19 00:26:05 +0900

    feat: 입력 UI를 통해 RacingGame에서 자동차 대수와 시도 횟수 입력 받아 관리할 기능 구현

--- src/main/java/step3/RacingGame.java
+++ src/main/java/step3/RacingGame.java
@@ -8,4 +8,15 @@ public class RacingGame {
     private int round;
     private List<Car> carList;
 
+    public void startGame() {
+        InputView inputView = new InputView();
+
+        inputView.init();
+        inputView.showInputCarMessage();
+        carNumber = inputView.inputCarNumber();
+
+        inputView.showInputRoundMessage();
+        round = inputView.inputRound();
+    }
+
 }
