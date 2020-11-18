7f3804b966566625de10a6afa084b2b270c5371d
Jaehee Park <jaeheepark0145@gmail.com>
2020-11-19 01:27:46 +0900

    test : RacingGameTest 수정

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -12,9 +12,7 @@ public class RacingGameTest {
     @Test
     @DisplayName("시도 횟수만큼 기록이 저장되는지 확인")
     public void play_test() {
-        List<String> carNames = new ArrayList();
-        carNames.add("car1");
-        carNames.add("car2");
+        String carNames = "car1,car2";
         RacingGame racingGame = new RacingGame(carNames, 5);
         racingGame.play();
         assertThat(racingGame.getRaceRecords().size()).isEqualTo(5);
