43282eee1303462cdf630eacad42fc29bb202611
kim-daeyong <kdy2353@gmail.com>
2020-11-21 19:29:00 +0900

    [ADD] input string으로 수정

--- src/main/java/racingcar/RacingMain.java
+++ src/main/java/racingcar/RacingMain.java
@@ -12,7 +12,8 @@ public class RacingMain {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         RacingGame racingGame = new RacingGame(new Racing());
-        List<Integer> input = inputView.inputRacing();
+        // List<Integer> input = inputView.inputRacingByCarNum();
+        
         List<Integer> result = racingGame.setInRacing(input);
         ResultView resultView = new ResultView();
         resultView.racingResult(result);
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    public List<Integer> inputRacing(){
+    public List<Integer> inputRacingByCarNum(){
         Scanner sc = new Scanner(System.in);
 
         List<Integer> racingRule = new ArrayList<>();
@@ -14,6 +14,21 @@ public class InputView {
         racingRule.add(sc.nextInt());
         System.out.println("시도할 횟수는 몇회 인가요?");
         racingRule.add(sc.nextInt());
+
+        sc.close();
+        
+        return racingRule;
+    }
+
+    public List<String> inputRacingByName(){
+        Scanner sc = new Scanner(System.in);
+
+        List<String> racingRule = new ArrayList<>();
+    
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        racingRule.add(sc.nextLine());
+        System.out.println("시도할 횟수는 몇회 인가요?");
+        racingRule.add(sc.nextLine());
 
         sc.close();
         
