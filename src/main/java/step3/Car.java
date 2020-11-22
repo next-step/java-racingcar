df467b76528b21ef77ac0ede8e77ddf5395aeda0
yeGenieee <cyj95428@gmail.com>
2020-11-22 21:58:08 +0900

    refactor: Car 클래스 내에서 자동차의 전진 여부를 검사할 수 있도록 변경

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -3,9 +3,12 @@
  * */
 package step3;
 
+import java.util.Random;
+
 public class Car {
 
     public int position;
+    private static Random random = new Random();
 
     public Car(int position) {
         this.position = position;
@@ -15,10 +18,28 @@ public class Car {
      * 자동차의 위치를 변경하는 메소드
      */
     public void move() {
-        this.position++;
+        if (isMovable()) {
+            this.position++;
+        }
     }
 
     public int getPosition() {
         return this.position;
     }
+
+
+    /**
+     * 랜덤으로 생성된 숫자가 전진 조건인지 아닌지 판별하는 메소드
+     */
+    public static boolean isMovable() {
+        return getRandomNumber() >= Constants.MOVE_CONDITION;
+    }
+
+    /**
+     * 0에서 9사이의 랜덤값을 생성하는 메소드
+     */
+    private static int getRandomNumber() {
+        return random.nextInt(Constants.RANDOM_RANGE);
+    }
+
 }
