29e6dec73dbdf9db11098eb7218ee279ba72d2ed
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-22 23:11:37 +0900

    test : 이름 객체 테스트 수정

--- src/test/java/racingcarwinner/NameTest.java
+++ src/test/java/racingcarwinner/NameTest.java
@@ -1,2 +1,24 @@
-package racingcarwinner;public class NameTest {
+package racingcarwinner;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.*;
+
+public class NameTest {
+
+    @Test
+    @DisplayName("이름 객체 생성 테스트")
+    public void create(){
+        Car car = new Car("베니");
+        Name name = new Name("베니");
+        assertTrue(car.getName().equals(name));
+    }
+
+    @Test
+    @DisplayName("이름 객체의 값 반환이 잘 되는가")
+    public void getString(){
+        Name name = new Name("베니베니");
+        assertEquals("베니베니",name.getString());
+    }
 }
