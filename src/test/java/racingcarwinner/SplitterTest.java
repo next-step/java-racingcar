d6c3ea165f721e3b303a65de10c6826ef10125b6
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-23 00:25:15 +0900

    문자열 분리 객체 테스트

--- src/test/java/racingcarwinner/SplitterTest.java
+++ src/test/java/racingcarwinner/SplitterTest.java
@@ -1,4 +1,18 @@
 package racingcarwinner;
 
-public class SpliterTest {
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.assertEquals;
+
+public class SplitterTest {
+
+    @Test
+    @DisplayName("문자열 분리 테스트")
+    public void split(){
+        String[] splitString = Splitter.splitName("포비,코난,베니");
+        assertEquals(splitString[0],"포비");
+        assertEquals(splitString[1],"코난");
+        assertEquals(splitString[2],"베니");
+    }
 }
