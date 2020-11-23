782e34a744762e882725122d9e40505314b1eed3
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-23 22:08:16 +0900

    Cars 객체 테스트 수정

--- src/test/java/racingcarwinner/CarsTest.java
+++ src/test/java/racingcarwinner/CarsTest.java
@@ -3,21 +3,17 @@ package racingcarwinner;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CarsTest {
 
     @Test
-    @DisplayName("자동차 리스트 생성이 잘 되는가")
+    @DisplayName("자동차 객체 리스트 생성이 잘 되는가")
     public void create(){
         Cars cars = Cars.createCars("포비,코난,베니");
-        String[] splitNames = "포비,코난,베니".split(",");
-
-        for (int i = 0; i < splitNames.length; i++) {
-            Name name = new Name(splitNames[i]);
-            assertTrue(cars.getCar(i).getName().equals(name));
-        }
+        assertTrue(cars.getCar(0).equals(new Car("포비")));
+        assertTrue(cars.getCar(0).equals(new Car("코난")));
+        assertTrue(cars.getCar(0).equals(new Car("베니")));
     }
 
     @Test
@@ -32,7 +28,6 @@ public class CarsTest {
     @DisplayName("자동차 댓수 반환을 잘 하는가")
     public void getCount(){
         Cars cars = Cars.createCars("포비,코난,베니,베니2");
-
         assertTrue(cars.getCount() == 4);
     }
 }
