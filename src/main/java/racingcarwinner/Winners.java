c3db0e91455b3a09b4459d2ecc78f6bf9e86a017
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-24 17:35:10 +0900

    feat : 우승자 일급컬렉션 구현

--- src/main/java/racingcarwinner/Winners.java
+++ src/main/java/racingcarwinner/Winners.java
@@ -1,2 +1,25 @@
-package racingcarwinner;public class Winners {
+package racingcarwinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winners {
+    private final List<Winner> winners = new ArrayList();
+
+    public Winner getWinner(int index){
+        return winners.get(index);
+    }
+
+    public void addWinner(String name){
+        winners.add(new Winner(name));
+    }
+
+    public int getCount() {
+        return winners.size();
+    }
+
+    public void clearWinners(){
+        winners.clear();
+    }
+
 }
