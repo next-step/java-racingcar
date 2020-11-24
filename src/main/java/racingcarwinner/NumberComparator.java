0bdb2dcc9878d2cd0bbf2e73070300f159e4f46c
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-24 17:32:26 +0900

    feat : 랜덤값 비교 객체 구현

--- src/main/java/racingcarwinner/NumberComparator.java
+++ src/main/java/racingcarwinner/NumberComparator.java
@@ -1,4 +1,10 @@
 package racingcarwinner;
 
-public class Comparator {
+public class NumberComparator {
+
+    private static final int VAlUE = 4;
+
+    public static boolean isAndAboveValue(int value){
+        return value >= VAlUE;
+    }
 }
