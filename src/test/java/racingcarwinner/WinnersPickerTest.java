0cce2f284515b9f736fb59e16d87f1656cf7acda
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-24 17:27:14 +0900

    test : 우승자 고르는 객체 테스트

--- src/test/java/racingcarwinner/WinnersPickerTest.java
+++ src/test/java/racingcarwinner/WinnersPickerTest.java
@@ -3,7 +3,7 @@ package racingcarwinner;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class WinnersPickerTest {
 
@@ -11,15 +11,16 @@ public class WinnersPickerTest {
     @DisplayName("우승자를 제대로 골라내는가")
     public void winnersPick(){
 
-        Cars cars = Cars.createCars("포비,코난,베니");
+        Cars cars = new Cars("포비,코난,베니");
         cars.getCar(0).move();
         cars.getCar(0).move();
         cars.getCar(1).move();
         cars.getCar(1).move();
         Winners winners = WinnersPicker.winnersPick(cars);
 
-        assertTrue(winners.getWinner(0).equals("포비"));
-        assertTrue(winners.getWinner(1).equals("코난"));
+        assertEquals(winners.getWinner(0).getName().getString(),"포비");
+        assertEquals(winners.getWinner(1).getName().getString(),"코난");
 
     }
+
 }
