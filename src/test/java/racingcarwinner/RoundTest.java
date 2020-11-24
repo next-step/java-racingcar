53536829a270ec9a36a02547afcb5af43f7df491
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-24 17:12:43 +0900

    test : 할 수 있는 테스트를  찾이 못함

--- src/test/java/racingcarwinner/RoundTest.java
+++ src/test/java/racingcarwinner/RoundTest.java
@@ -1,2 +1,11 @@
-package racingcarwinner;public class RoundTest {
+package racingcarwinner;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.assertTrue;
+
+public class RoundTest {
+
+    // 어떤 테스트를 해야 할지 감이 잡히지 않음
 }
