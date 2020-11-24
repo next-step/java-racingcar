892d2360f5cbf1579bf45fee128ff62630046a25
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-24 17:12:56 +0900

    test : 할 수 있는 테스트를  찾이 못함

--- src/test/java/racingcarwinner/GameTest.java
+++ src/test/java/racingcarwinner/GameTest.java
@@ -1,2 +1,13 @@
-package racingcarwinner;public class GameTest {
+package racingcarwinner;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import racingcar.RandomGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GameTest {
+    // 어떤 테스트를 해야 할지 감이 잡히지 않음
 }
