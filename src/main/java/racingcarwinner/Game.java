bb9360e0c4a9f4693119c5ebe0265f611c700a86
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-24 17:29:50 +0900

    refector : 게임 실행 객체 구현

--- src/main/java/racingcarwinner/Game.java
+++ src/main/java/racingcarwinner/Game.java
@@ -1,2 +1,15 @@
-package racingcarwinner;public class Game {
+package racingcarwinner;
+
+
+
+public class Game {
+
+    public void play(){
+        Cars cars = new Cars(InputView.inputCarsName());
+        Rounds rounds = new Rounds(InputView.inputRoundCount());
+
+        rounds.play(cars);
+
+        ResultView.winnersView(WinnersPicker.winnersPick(cars));
+    }
 }
