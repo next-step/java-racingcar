f4b19289b41f3ee8c7211ba6198f408b3d72ea9e
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-24 17:35:17 +0900

    feat : 우승자 객체 구현

--- src/main/java/racingcarwinner/Winner.java
+++ src/main/java/racingcarwinner/Winner.java
@@ -1,2 +1,31 @@
-package racingcarwinner;public class Winner {
+package racingcarwinner;
+
+public class Winner {
+
+    private Name name;
+
+    private Winner(){}
+
+    public Winner(String name){
+        this.name = new Name(name);
+    }
+
+    public Name getName(){
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Winner winner = (Winner) o;
+
+        return name.equals(winner.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
