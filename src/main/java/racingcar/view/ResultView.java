185f9281a5d35935065b6fd9a38c625836d8dbce
kim-daeyong <kdy2353@gmail.com>
2020-11-24 23:41:58 +0900

    refactor depth 줄이기

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -14,14 +14,20 @@ public class ResultView {
         for(int i = 0; i < list.size(); i++){
             printMove(list.get(i));
             System.out.println();
-            if(list.get(i).equals("")){
-                count++;
-            }
-            if(count == move){
-                printWinner(list);
-            }
+            count = branch(i, list, move, count);
         }
         System.out.println();
+    }
+
+    public int branch(int i ,List<String> list, int move, int count){
+        int result = count;
+        if(list.get(i).equals("")){
+            result++;
+        }
+        if(result == move){
+            printWinner(list);
+        }
+        return result;
     }
 
     public void printMove(String board){
@@ -29,34 +35,53 @@ public class ResultView {
             String[] temp = board.split(" : ");
             System.out.print(temp[0] + " : ");
             int move = Integer.valueOf(temp[1]);
-            for(int j = 0; j < move; j++){
-                System.out.print("-");
-            }
+            printLoacation(move);
+        }
+    }
+
+    public void printLoacation(int move){
+        for(int j = 0; j < move; j++){
+            System.out.print("-");
         }
     }
 
     public void printWinner(List<String> list){
         List<Integer> tempList = new ArrayList<>();
         for(int k = 0; k < list.size(); k++){
-            if((!list.get(k).equals(""))){
-                String[] temp = list.get(k).split(" : ");
-                tempList.add(Integer.valueOf(temp[1]));
-            }
+            tempList.add(Integer.valueOf(checkScore(list.get(k))));
         }
         HashSet<String> winer = getWinner(list, tempList);
         System.out.println(winer + " 가 최종 우승했습니다.");
     }
 
+    public String checkScore(String score){
+        String temp = null;
+        if(!score.equals("")){
+            temp = score.split(" : ")[1];
+        }
+        if(score.equals("")){
+            temp = "0";
+        }
+        return temp;
+    }
+
     public HashSet<String> getWinner(List<String> list, List<Integer> tempList){
-        int temp = Collections.max(tempList);
+        int max = Collections.max(tempList);
         List<String> winnerList = new ArrayList<>(); 
         for(int l = 0; l < list.size(); l++){
-            if(!list.get(l).equals("") && list.get(l).split(" : ")[1].contains(String.valueOf(temp))){
-                winnerList.add(list.get(l).split(" : ")[0]);
-            }
+            winnerList.add(chooseWinner(list.get(l), max));
         }
+        while (winnerList.remove(null));
         HashSet<String> dedupl = new HashSet<String>(winnerList);
         return dedupl;
+    }
+
+    public String chooseWinner(String racer, int score){
+        String winner = null;
+        if(!racer.equals("") && racer.split(" : ")[1].contains(String.valueOf(score))){
+            winner = racer.split(" : ")[0];
+        }
+        return winner;
     }
 
 }
