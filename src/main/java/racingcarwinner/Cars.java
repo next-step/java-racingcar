cee10509f68a8412822a5c979d82d56e3885597e
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-24 17:30:38 +0900

    refector : 자동차 일급컬렉션 구현

--- src/main/java/racingcarwinner/Cars.java
+++ src/main/java/racingcarwinner/Cars.java
@@ -1,2 +1,40 @@
-package racingcarwinner;public class Cars {
+package racingcarwinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private final List<Car> cars = new ArrayList();
+
+    private Cars(){}
+
+    public Cars(String names){
+        for (String name : Splitter.splitName(names)) {
+            cars.add(new Car(name));
+        }
+    }
+
+    public Car getCar(int index) {
+        return cars.get(index);
+    }
+
+    public int getCount(){
+        return cars.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cars cars1 = (Cars) o;
+
+        return cars.equals(cars1.cars);
+    }
+
+    @Override
+    public int hashCode() {
+        return cars.hashCode();
+    }
 }
