1d1d2c92fa695a6795c0ab2272b011f20ffa8bc3
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-24 17:33:19 +0900

    refector : 랜덤값 반환 객체 구현

--- src/main/java/racingcarwinner/RandomGenerator.java
+++ src/main/java/racingcarwinner/RandomGenerator.java
@@ -1,2 +1,12 @@
-package racingcarwinner;public class RandomGenerator {
+package racingcarwinner;
+
+import java.util.Random;
+
+public class RandomGenerator {
+
+    private static final int BOUND = 10;
+
+    public static int getRandomNumber(){
+        return new Random().nextInt(BOUND);
+    }
 }
