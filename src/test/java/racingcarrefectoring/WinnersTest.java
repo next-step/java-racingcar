bbe446b113b988d2fc865ef6d684752f01fee0b3
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-25 20:21:34 +0900

    test : 우승자들 객체 테스트

--- src/test/java/racingcarrefectoring/WinnersTest.java
+++ src/test/java/racingcarrefectoring/WinnersTest.java
@@ -2,6 +2,11 @@ package racingcarrefectoring;
 
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import racingcarrefectoring.model.Name;
+import racingcarrefectoring.model.Winner;
+import racingcarrefectoring.model.Winners;
+
+import static org.junit.Assert.assertTrue;
 
 public class WinnersTest {
 
@@ -9,7 +14,8 @@ public class WinnersTest {
     @DisplayName("우승자 객체를 제대로 추가하고 가져오는가")
     public void addWinner(){
         Winners winners = new Winners();
-        winners.addWinner("베니");
-        assertTrue(winners.getWinner(0).Equals(new Winner("베니")));
+
+        winners.addWinner(new Name("베니"));
+        assertTrue(winners.getWinner(0).equals(new Winner(new Name("베니"))));
     }
 }
