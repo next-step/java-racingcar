b4e57a934fc5ca6873ec81b66c3f437715c7677e
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-25 20:29:22 +0900

    feat : 이름이 비었을 때에 대한 커스텀 예외처리 구현

---
+++ src/main/java/racingcarrefectoring/exception/NameEmptyException.java
@@ -0,0 +1,8 @@
+package racingcarrefectoring.exception;
+
+public class NameEmptyException extends IllegalArgumentException {
+
+    public NameEmptyException(String message){
+        super(message);
+    }
+}
