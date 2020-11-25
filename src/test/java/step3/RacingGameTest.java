ecd35a5ca53f8b980de4bfd626167cd9d5300993
yeGenieee <cyj95428@gmail.com>
2020-11-26 01:21:12 +0900

    fix: RacingGame 내 round 필드 삭제됨에 따라 테스트 코드 수정

--- src/test/java/step3/RacingGameTest.java
+++ src/test/java/step3/RacingGameTest.java
@@ -17,7 +17,7 @@ class RacingGameTest {
     @DisplayName("입력한 자동차 대수와 같은 자동차 리스트가 생성되었는지 및 초기 위치 테스트")
     @ValueSource(ints = {3,5,10})
     void is_equal_to_car_number(int carNumber) {
-        RacingGame racingGame = new RacingGame(carNumber,5);
+        RacingGame racingGame = new RacingGame(carNumber);
         List<Car> cars = racingGame.makeCarList(carNumber);
         assertThat(cars.size()).isEqualTo(carNumber);
         cars.forEach(car -> assertThat(car.getPosition()).isEqualTo(0));
