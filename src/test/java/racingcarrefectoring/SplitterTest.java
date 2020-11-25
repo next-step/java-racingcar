dafbf452a6c5466db410e1a6f8c58b5891d46e47
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-25 20:20:58 +0900

    test : 문자열 분리 객체 테스트

--- src/test/java/racingcarrefectoring/SplitterTest.java
+++ src/test/java/racingcarrefectoring/SplitterTest.java
@@ -2,8 +2,9 @@ package racingcarrefectoring;
 
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import racingcarrefectoring.model.Splitter;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SplitterTest {
@@ -11,10 +12,9 @@ public class SplitterTest {
     @Test
     @DisplayName("문자열 분리가 잘 되는가")
     public void splitName(){
-        Splitter splitter = new Splitter();
-        String[] strings = splitter("포비,코난,베니");
+        String[] strings = Splitter.splitName("포비,코난,베니");
         String[] splitName = "포비,코난,베니".split(",");
-        assertTrue(strings.equals(splitName));
+        assertArrayEquals(strings, splitName);
 
     }
 }
