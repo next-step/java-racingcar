04d2fb1582d262a8c6bb84874b3ee3c514da704e
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-25 20:29:44 +0900

    feat : 음수에 대한 커스텀 예외처리 구현

---
+++ src/main/java/racingcarrefectoring/exception/NegativeException.java
@@ -0,0 +1,8 @@
+package racingcarrefectoring.exception;
+
+public class NegativeException extends IllegalArgumentException{
+
+    public NegativeException(String message){
+        super(message);
+    }
+}
