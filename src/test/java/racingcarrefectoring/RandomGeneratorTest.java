61fdf4eac809f74a20f38cab92cc32cdd1ed115e
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-25 14:01:57 +0900

    test : 랜덤값 생성 객체 테스트 구현

---
+++ src/test/java/racingcarrefectoring/RandomGeneratorTest.java
@@ -0,0 +1,18 @@
+package racingcarrefectoring;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.assertTrue;
+
+public class RandomGeneratorTest {
+
+    @Test
+    @DisplayName("랜덤값을 제대로 반환하는가")
+    public void getRandomNumber(){
+        int randomNumber = RandomGenerator.getRandomNumber();
+        for (int i = 0; i < 100; i++) {
+            assertTrue(0 <= randomNumber  || randomNumber < 10)
+        }
+    }
+}
