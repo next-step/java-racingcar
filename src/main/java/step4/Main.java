bd955e6b5a88a3856d378a25d0f79d9e03e7d7b1
yeGenieee <cyj95428@gmail.com>
2020-11-27 00:04:17 +0900

    chore: step3 패키지 step4로 그대로 복사

---
+++ src/main/java/step4/Main.java
@@ -0,0 +1,25 @@
+package step4;
+
+public class Main {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        RacingGame racingGame;
+
+        inputView.init();
+        inputView.showInputCarMessage();
+        int carNumber = inputView.inputCarNumber();
+
+        inputView.showInputRoundMessage();
+        int round = inputView.inputRound();
+
+        racingGame = new RacingGame(carNumber);
+
+        resultView.showResultMessage();
+
+        for (int i = 0; i < round; i++) {
+            racingGame.playRace();
+            resultView.printRace(racingGame.getCars());
+        }
+    }
+}
---
+++ src/test/java/step4/RacingGameTest.java
@@ -0,0 +1,23 @@
+package step4;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingGameTest {
+
+    @ParameterizedTest
+    @DisplayName("입력한 자동차 대수와 같은 자동차 리스트가 생성되었는지 및 초기 위치 테스트")
+    @ValueSource(ints = {3,5,10})
+    void is_equal_to_car_number(int carNumber) {
+        RacingGame racingGame = new RacingGame(carNumber);
+        List<Car> cars = racingGame.makeCarList(carNumber);
+        assertThat(cars.size()).isEqualTo(carNumber);
+        cars.forEach(car -> assertThat(car.getPosition()).isEqualTo(0));
+    }
+
+}
