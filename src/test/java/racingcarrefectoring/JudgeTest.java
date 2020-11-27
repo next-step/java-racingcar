573b47156713b688937764abcd2e262bdbf332ad
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-27 16:59:28 +0900

    test : 마무리

--- src/test/java/racingcarrefectoring/JudgeTest.java
+++ src/test/java/racingcarrefectoring/JudgeTest.java
@@ -2,13 +2,11 @@ package racingcarrefectoring;
 
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import racingcarrefectoring.model.Judge;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class JudgeTest {
 
-    @Test
-    @DisplayName("기준 이상의 숫자일 때 True를 반환하는가")
-    public void andAbove(){
-        assertTrue(Judge.andAbove(4,4));
-        assertFalse(Judge.andAbove(3,4));
-    }
 }
--- src/test/java/racingcarrefectoring/RoundsTest.java
+++ src/test/java/racingcarrefectoring/RoundsTest.java
@@ -5,10 +5,4 @@ import org.junit.jupiter.api.DisplayName;
 
 public class RoundsTest {
 
-    @Test
-    @DisplayName("경기들이 잘 생성되는가")
-    public void createRounds(){
-        Rounds rounds = Rounds.create(3);
-        assertEquals(rounds.getRoundCount(),3);
-    }
 }
