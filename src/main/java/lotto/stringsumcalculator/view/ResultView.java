97b6cf22ace6c682b4109c6b3c9bcec94e331313
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-30 19:26:14 +0900

    feat : 결과 뷰 객체 구현

---
+++ src/main/java/lotto/stringsumcalculator/view/ResultView.java
@@ -0,0 +1,8 @@
+package lotto.stringsumcalculator.view;
+
+public class ResultView {
+
+    public static void viewResult(int result){
+        System.out.println("문자열 안에 수의 합은 " + result + "입니다");
+    }
+}
