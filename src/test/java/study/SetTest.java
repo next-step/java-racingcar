247f4ef9646f621d26533be3bfbfd7b6bf06892c
jaenyeong <jaenyeong.dev@gmail.com>
2020-11-30 22:54:14 +0900

    test(SetTest): write a contains test method for requirement 3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -35,5 +36,12 @@ public class SetTest {
     @DisplayName("[요구사항 2] Set 컬렉션의 contains() 메서드를 사용하여 특정 원소가 포함되어 있는지 확인")
     void validateContainsSpecificValueInSetWhenUseContainsMethod(final int expectedSize) {
         assertThat(numbers.contains(expectedSize)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:false", "1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    @DisplayName("[요구사항 3] 요구사항 2를 파라미터에 따라 결과 값도 다르게 테스트")
+    void validateContainsDifferentValueInSetWhenUseContainsMethodWithDifferentValueForEachParam(final int givenSize, final boolean expected) {
+        assertThat(numbers.contains(givenSize)).isEqualTo(expected);
     }
 }
