6dc469d7181a418479a6d0ef783eb45f312ea841
shk3029 <shk30290@naver.com>
2020-11-30 20:05:47 +0900

    test: substring test study

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,7 +2,7 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
@@ -11,5 +11,12 @@ public class StringTest {
         assertThat(result).contains("1");
         assertThat(result).contains("2");
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substring() {
+        String input = "(1,2)";
+        String result = input.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
     }
 }
