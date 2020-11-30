459fe9281058077fd600313e266aef9206e344d2
ccf05017 <ccf05017@gmail.com>
2020-11-30 22:45:35 +0900

    refactor(StringTest): change test description of split method
    - 테스트의 의도를 좀 더 잘 드러낼 수 있도록 테스트 설명문을 수정

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
-    @DisplayName("split 메서드를 통해 \"1,2\"를 \"1\", \"2\"로 나눌 수 있는지 확인하는 학습 테스트")
+    @DisplayName("split 메서드를 통해 주어진 문자열을 구분자로 나눌 수 있는지 확인하는 학습 테스트")
     @Test
     void splitOneAndTwoTest() {
         String target = "1,2";
