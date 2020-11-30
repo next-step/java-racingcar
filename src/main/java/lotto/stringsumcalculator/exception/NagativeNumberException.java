cf5d716dd1e7e2a2e59d41033f5b9c46a509d232
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-30 19:26:58 +0900

    feat : 음수 커스텀 예외처리 구현

---
+++ src/main/java/lotto/stringsumcalculator/exception/NagativeNumberException.java
@@ -0,0 +1,8 @@
+package lotto.stringsumcalculator.exception;
+
+public class NagativeNumberException extends RuntimeException{
+
+    public NagativeNumberException(String message){
+        super(message);
+    }
+}
