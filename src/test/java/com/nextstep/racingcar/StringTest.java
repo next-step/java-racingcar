0aefa0ccb6406e9869b30a1bcdc7a383ef2aedac
lazy-moon <magyeon@naver.com>
2020-11-30 23:38:35 +0900

    test: split 테스트

---
+++ src/test/java/com/nextstep/racingcar/StringTest.java
@@ -0,0 +1,23 @@
+package com.nextstep.racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    public void split(){
+        String source = "1,2";
+        String[] result = source.split(",");
+
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    public void split2(){
+        String source = "1";
+        String[] result = source.split(",");
+
+        assertThat(result).contains("1");
+    }
+}
