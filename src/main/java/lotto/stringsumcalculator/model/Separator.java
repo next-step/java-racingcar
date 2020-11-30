b5c5bd46f400454c2fd6fbdac210859ee1b9192a
odongjin <odongjin@sayongjaui-MacBook-Pro.local>
2020-11-30 19:24:59 +0900

    feat : 구분자 객체 구현

---
+++ src/main/java/lotto/stringsumcalculator/model/Separator.java
@@ -0,0 +1,14 @@
+package lotto.stringsumcalculator.model;
+
+public class Separator {
+
+    private final String string;
+    public Separator(String string){
+        this.string = string;
+    }
+
+    public String getString(){
+        return this.string;
+    }
+
+}
