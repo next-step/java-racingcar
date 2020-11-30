6970ab3c55fec62a0e0f83e5428312ef180ebb44
jaenyeong <jaenyeong.dev@gmail.com>
2020-11-30 22:45:51 +0900

    test(StringTest): write a charAt test method for requirement 3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("학습 테스트 실습 1단계 - Junit, AssertJ 사용법 숙지")
 public class StringTest {
@@ -33,5 +33,26 @@ public class StringTest {
         final String resultStr = givenInput.substring(startPoint, givenInput.length() - 1);
 
         assertThat(resultStr).isEqualTo(oneAndTwo);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "abc")
+    @DisplayName("[요구사항 3-1] 'abc' 문자열을 charAt 메서드 사용시 유효한 위치값을 벗어나 " +
+        "StringIndexOutOfBoundsException 예외 발생시 assertThatThrownBy 메서드를 사용하여 확인")
+    void validateReturnIndexOutOfBoundsExceptionWhenUseCharAtMethod(final String givenInput) {
+        assertThatThrownBy(
+            () -> givenInput.charAt(givenInput.length()))
+            .isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range: 3");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = "abc")
+    @DisplayName("[요구사항 3-2] 'abc' 문자열을 charAt 메서드 사용시 유효한 위치값을 벗어나 " +
+        "StringIndexOutOfBoundsException 예외 발생시 assertThatExceptionOfType 메서드를 사용하여 확인")
+    void validateReturnIndexOutOfBoundsExceptionWhenUseCharAtMethod2(final String givenInput) {
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> givenInput.charAt(givenInput.length()))
+            .withMessageMatching("String index out of range: 3");
     }
 }
