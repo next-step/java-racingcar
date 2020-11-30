6af05199095b5a253d0bab5f2c1311457c2ae24e
lazy-moon <magyeon@naver.com>
2020-11-30 23:39:34 +0900

    test: charAt 테스트

--- src/test/java/com/nextstep/racingcar/StringTest.java
+++ src/test/java/com/nextstep/racingcar/StringTest.java
@@ -1,8 +1,10 @@
 package com.nextstep.racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -29,5 +31,17 @@ public class StringTest {
         String result = source.substring(beginIdx, endIdx);
 
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 동작 및 예외처리 테스트")
+    public void charAt(){
+        String source = "abc";
+        char result = source.charAt(0);
+
+        assertThat(result).isEqualTo('a');
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> source.charAt(3))
+                .withMessageMatching("String index out of range: 3");
     }
 }
