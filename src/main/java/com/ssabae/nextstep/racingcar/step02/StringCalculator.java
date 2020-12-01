007c71f344d4554bdbf111ad90a46690c3b63f8f
Ssabae <llsb156@gmail.com>
2020-12-02 01:59:51 +0900

    feat: Step02 계산기 구현

---
+++ src/main/java/com/ssabae/nextstep/racingcar/step02/StringCalculator.java
@@ -0,0 +1,78 @@
+package com.ssabae.nextstep.racingcar.step02;
+
+import java.util.Scanner;
+
+/**
+ * @author : leesangbae
+ * @project : java-racingcar
+ * @since : 2020-12-01
+ */
+public class StringCalculator implements Calculator {
+
+    private static final String SEPARATOR = " ";
+
+    private final Validator<String[]> validator;
+
+    public StringCalculator(Validator<String[]> validator) {
+        this.validator = validator;
+    }
+
+    @Override
+    public int calculate(String input) {
+        if (isBlank(input)) {
+            throw new IllegalArgumentException("입력 값이 없습니다.");
+        }
+        String[] split = input.split(SEPARATOR);
+        validator.validate(split);
+        return calculating(split);
+    }
+
+    protected boolean isBlank(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+
+    private int calculating(String[] split) {
+        int lastValue = 0;
+        String lastOperation = "+";
+        for (int i = 0; i < split.length; i++) {
+            if (i % 2 == 0) {
+                int value = Integer.parseInt(split[i]);
+                lastValue = operate(lastValue, lastOperation, value);
+            } else {
+                lastOperation = split[i];
+            }
+        }
+        return lastValue;
+    }
+
+    protected int operate(int val1, String operator, int val2) {
+        int returnValue = 0;
+        switch (operator) {
+            case "+":
+                returnValue = val1 + val2;
+                break;
+            case "-":
+                returnValue = val1 - val2;
+                break;
+            case "*":
+                returnValue = val1 * val2;
+                break;
+            case "/":
+                returnValue = val1 / val2;
+                break;
+        }
+        return returnValue;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        Validator<String[]> validator = new StringCalculateValidator();
+        Calculator calculator = new StringCalculator(validator);
+        int result = calculator.calculate(input);
+
+        System.out.println("result = " + result);
+    }
+}
