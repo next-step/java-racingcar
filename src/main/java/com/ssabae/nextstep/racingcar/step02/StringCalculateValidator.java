ff54976948eb760101185600ac7d3161c51249ee
Ssabae <llsb156@gmail.com>
2020-12-02 02:10:29 +0900

    refactor: 에러 메시지 변수처리

--- src/main/java/com/ssabae/nextstep/racingcar/step02/StringCalculateValidator.java
+++ src/main/java/com/ssabae/nextstep/racingcar/step02/StringCalculateValidator.java
@@ -6,14 +6,18 @@ package com.ssabae.nextstep.racingcar.step02;
  * @since : 2020-12-02
  */
 public class StringCalculateValidator implements Validator<String[]> {
+
+    public static final String INVALID_PAIR_MESSAGE = "연산자와 숫자의 조합이 올바르지 않습니다.";
+    public static final String INVALID_OPERATION_MESSAGE = "사칙연산 기호가 아닌 문자열은 포함 할 수 없습니다.";
+
     @Override
     public boolean validate(String[] strings) {
         if (strings.length % 2 == 0) {
-            throw new IllegalArgumentException("연산자와 숫자의 조합이 올바르지 않습니다.");
+            throw new IllegalArgumentException(INVALID_PAIR_MESSAGE);
         }
         for (int i = 1; i < strings.length; i += 2) {
             if (!isOperator(strings[i])) {
-                throw new IllegalArgumentException("사칙연산 기호가 아닌 문자열은 포함 할 수 없습니다.");
+                throw new IllegalArgumentException(INVALID_OPERATION_MESSAGE);
             }
         }
         return true;
