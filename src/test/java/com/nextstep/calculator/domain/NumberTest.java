beb97f2091f07e41bdf9a0b3a4acb94484d58ce1
ccf05017 <ccf05017@gmail.com>
2020-12-01 23:07:44 +0900

    test(NumberTest): check negative number
    - 문자열 계산기의 정확성을 위해 음수로 Number 객체 생성이 가능한지 확인하는 테스트 추가

--- src/test/java/com/nextstep/calculator/domain/NumberTest.java
+++ src/test/java/com/nextstep/calculator/domain/NumberTest.java
@@ -4,19 +4,30 @@ import com.nextstep.calculator.domain.exceptions.InvalidNumberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NumberTest {
     @DisplayName("문자열을 인자로 받아서 객체를 생성할 수 있다.")
-    @Test
-    void createTest() {
-        Number number = Number.of("1");
-        Number expectedValue = Number.of("1");
+    @ParameterizedTest
+    @MethodSource("createTestResource")
+    void createTest(String value, Number expectedNumber) {
+        Number number = Number.of(value);
 
-        assertThat(number).isEqualTo(expectedValue);
+        assertThat(number).isEqualTo(expectedNumber);
+    }
+    public static Stream<Arguments> createTestResource() {
+        return Stream.of(
+                Arguments.of("1", Number.of("1")),
+                Arguments.of("0", Number.of("0")),
+                Arguments.of("-1", Number.of("-1"))
+        );
     }
 
     @DisplayName("인자로 전달된 문자열을 숫자로 변환할 수 없는 경우 예외 발생")
