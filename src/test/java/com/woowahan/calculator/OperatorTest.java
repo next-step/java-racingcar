427805816ee7fbd93357c397e8a7f42865500c84
cionman <topspin1278@naver.com>
2020-12-01 20:35:52 +0900

    test: 테스트 코드 추가

---
+++ src/test/java/com/woowahan/calculator/OperatorTest.java
@@ -0,0 +1,96 @@
+package com.woowahan.calculator;
+
+import static org.assertj.core.api.BDDAssertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class OperatorTest {
+
+	@DisplayName("Operator PLUS 항목의 operator필드는 덧셈기능을 하는 인터페이스 구현체다.")
+	@ParameterizedTest
+	@CsvSource(value = {"1:2:3", "3:5:8", "7:9:16"}, delimiter = ':')
+	public void operatorPlus(long value1, long value2, long expected) {
+
+		//when
+		long result = Operator.PLUS.getOperator().applyAsLong(value1, value2);
+
+		//then
+		then(result).isEqualTo(expected);
+	}
+
+	@DisplayName("Operator MINUS 항목의 operator필드는 뺄셈기능을 하는 인터페이스 구현체다.")
+	@ParameterizedTest
+	@CsvSource(value = {"3:2:1", "5:3:2", "9:6:3"}, delimiter = ':')
+	public void operatorMinus(long value1, long value2, long expected) {
+
+		//when
+		long result = Operator.MINUS.getOperator().applyAsLong(value1, value2);
+
+		//then
+		then(result).isEqualTo(expected);
+	}
+
+	@DisplayName("Operator MULTIPLY 항목의 operator필드는 곱셈기능을 하는 인터페이스 구현체다.")
+	@ParameterizedTest
+	@CsvSource(value = {"3:2:6", "5:3:15", "9:6:54"}, delimiter = ':')
+	public void operatorMultiply(long value1, long value2, long expected) {
+
+		//when
+		long result = Operator.MULTIPLY.getOperator().applyAsLong(value1, value2);
+
+		//then
+		then(result).isEqualTo(expected);
+	}
+
+	@DisplayName("Operator DIVIDE 항목의 operator필드는 나눗셈기능을 하는 인터페이스 구현체다.")
+	@ParameterizedTest
+	@CsvSource(value = {"15:3:5", "12:4:3", "25:5:5"}, delimiter = ':')
+	public void operatorDivde(long value1, long value2, long expected) {
+
+		//when
+		long result = Operator.DIVIDE.getOperator().applyAsLong(value1, value2);
+
+		//then
+		then(result).isEqualTo(expected);
+	}
+
+	@DisplayName("Operator getOperator 메서드에 사칙 연산 기호 문자열을 전달하면 알맞는 Operator를 반환한다.")
+	@ParameterizedTest
+	@MethodSource("argGetOperator")
+	public void operatorGetOperator(String value, Operator expected) {
+
+		//when
+		Operator result = Operator.getOperator(value);
+
+		//then
+		then(result).isEqualTo(expected);
+	}
+
+	public static Stream<Arguments> argGetOperator() {
+		return Stream.of(
+			Arguments.of("+", Operator.PLUS),
+			Arguments.of("-", Operator.MINUS),
+			Arguments.of("*", Operator.MULTIPLY),
+			Arguments.of("/", Operator.DIVIDE)
+		);
+	}
+
+	@DisplayName("Operator getOperator 메서드에 사칙 연산 기호가 아닌 문자열을 전달하면 IllegalArgumentException이 발생한다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"a", "bc", "!", "%"})
+	public void operatorGetOperatorThrow(String value) {
+
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> {
+				Operator.getOperator(value);
+			}).withMessage(Validator.MSG_NOT_FOUND_OPERATOR);
+	}
+
+}
--- src/test/java/com/woowahan/calculator/StringCalculatorTest.java
+++ src/test/java/com/woowahan/calculator/StringCalculatorTest.java
@@ -5,22 +5,52 @@ import static org.assertj.core.api.BDDAssertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class StringCalculatorTest {
 
 	@DisplayName("요구조건에 맞는 입력 연산")
 	@ParameterizedTest
 	@CsvSource(value = {
+		"5 + 3 =8",
+		"5 - 3 =2",
+		"5 * 3 =15",
+		"6 / 3 =2",
 		"2 + 3 * 4 / 2=10",
 		"2 + 3 * 4 + 5 - 3 * 2=44",
 	}, delimiter = '=')
-	void normalTest(String input, long expected) {
+	void calculate(String value, long expected) {
 
 		//when
-		long result = StringCalculator.calculate(input);
+		long result = StringCalculator.calculate(value);
 
 		//then
 		then(result).isEqualTo(expected);
+
+	}
+
+	@DisplayName("입력 값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException이 발생한다.")
+	@ParameterizedTest
+	@NullAndEmptySource
+	void calculateNullOrEmpty(String value) {
+
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> {
+				StringCalculator.calculate(value);
+			}).withMessageMatching(Validator.MSG_NULL_OR_EMPTY);
+
+	}
+
+	@DisplayName("사칙연산 기호가 아닌 경우 IllegalArgumentException이 발생한다.")
+	@ParameterizedTest
+	@ValueSource(strings = {"2 ! 3 @ 4 / 2", "3 ^ 3 ) 6"})
+	void calculateNotOperator(String value) {
+
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> {
+				StringCalculator.calculate(value);
+			}).withMessageMatching(Validator.MSG_NOT_FOUND_OPERATOR);
 
 	}
 
