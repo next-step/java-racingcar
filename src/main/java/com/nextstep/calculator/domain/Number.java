2807bdab18f764e01aaad3a81d87bfa079caea61
ccf05017 <ccf05017@gmail.com>
2020-12-01 23:01:05 +0900

    refactor(Number): validation check
    - 계산에 사용될 숫자 객체의 안정성을 위해 정합성을 판단하는 기능 추가
    - 숫자로 변환할 수 없는 문자열을 인자로 전달받은 경우 예외 발생

--- src/main/java/com/nextstep/calculator/domain/Number.java
+++ src/main/java/com/nextstep/calculator/domain/Number.java
@@ -1,9 +1,19 @@
 package com.nextstep.calculator.domain;
 
+import com.nextstep.calculator.domain.exceptions.InvalidNumberException;
+
 public class Number {
     private final int value;
 
-    public Number(final String number) {
-        this.value = Integer.parseInt(number);
+    Number(final int value) {
+        this.value = value;
+    }
+
+    public static Number of(final String number){
+        try {
+            return new Number(Integer.parseInt(number));
+        } catch (Exception e) {
+            throw new InvalidNumberException("숫자로 적합하지 않은 문자열입니다.");
+        }
     }
 }
--- src/test/java/com/nextstep/calculator/domain/NumberTest.java
+++ src/test/java/com/nextstep/calculator/domain/NumberTest.java
@@ -1,16 +1,27 @@
 package com.nextstep.calculator.domain;
 
+import com.nextstep.calculator.domain.exceptions.InvalidNumberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NumberTest {
     @DisplayName("문자열을 인자로 받아서 객체를 생성할 수 있다.")
     @Test
     void createTest() {
-        Number one = new Number("1");
-        
+        Number one = Number.of("1");
+
         assertThat(one).isNotNull();
+    }
+
+    @DisplayName("인자로 전달된 문자열을 숫자로 변환할 수 없는 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", " ", "", "a1", "12cb"})
+    void creatFailTest(String invalidValue) {
+        assertThatThrownBy(() -> Number.of(invalidValue)).isInstanceOf(InvalidNumberException.class);
     }
 }
