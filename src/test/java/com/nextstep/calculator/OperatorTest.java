93e9f84f9679025a892a89af8c0b581836db34b6
lazy-moon <magyeon@naver.com>
2020-12-01 23:40:22 +0900

    test: Operator 객체 획득 시 연산자가 올바르지 않은 경우 null 반환 테스트 추가

--- src/test/java/com/nextstep/calculator/OperatorTest.java
+++ src/test/java/com/nextstep/calculator/OperatorTest.java
@@ -9,7 +9,14 @@ import static org.assertj.core.api.Assertions.*;
 
 public class OperatorTest {
 
-    @DisplayName("연산자에 따른 Operator 획득 및 계산 결과 확인")
+    @DisplayName("연산자가 올바르지 않은 경우 Operator null 반환")
+    @Test
+    public void getOperator(){
+        Operator operator = Operator.select("#");
+        assertThat(operator).isNull();
+    }
+
+    @DisplayName("연산자에 따른 Operator 획득 및 계산 결과")
     @ParameterizedTest
     @CsvSource({"1,+,2,3", "3,-,1,2", "4,*,3,12", "10,/,2,5"})
     public void calculate(int a, String symbol, int b, int expected){
@@ -19,7 +26,7 @@ public class OperatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    @DisplayName("나누어 떨어지지 않는 숫자가 주어진 경우 예외처리 확인")
+    @DisplayName("나누어 떨어지지 않는 숫자가 주어진 경우 예외처리")
     @Test
     public void divisionException(){
         Operator operator = Operator.DIVISION;
