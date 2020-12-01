90d155386da9f5b13060e602381f5ca2e9c40259
HanYonggu <hyyg111@naver.com>
2020-12-01 22:28:18 +0900

    test : Set size Test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,35 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size 메소드 정상적으로 반환하는지 확인")
+    @Test
+    void sizeTest() {
+        final int expectedSize = 3;
+
+        int size = numbers.size();
+
+        assertThat(size).isEqualTo(expectedSize);
+    }
+
+}
