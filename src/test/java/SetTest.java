6b6498ee5bbdf85391dcfb411a00d06e5c772e32
Do Yeon Kim <chikeem90@gmail.com>
2020-12-01 11:05:09 +0900

    test: Set Collection에 대한 학습 테스트

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,52 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("Set test - 요구사항 1")
+	@Order(1)
+	public void Set_Size_Test() {
+	    assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("Set test - 요구사항 2")
+	@Order(2)
+	public void Set_Contains_Test(int input) {
+		assertThat(numbers.contains(input)).isTrue();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	@DisplayName("Set test - 요구사항 3")
+	@Order(3)
+	public void Set_Contains_Test(int input, boolean expectedResult) {
+		assertThat(numbers.contains(input)).isEqualTo(expectedResult);
+	}
+
+}
