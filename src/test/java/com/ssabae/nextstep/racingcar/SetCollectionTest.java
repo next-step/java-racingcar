7d61f37e7ea4613a58e730c565f59694b6c9ee56
Ssabae <llsb156@gmail.com>
2020-12-01 18:28:22 +0900

    test: 🚀 1단계 - 학습 테스트 실습 (#1575)

---
+++ src/test/java/com/ssabae/nextstep/racingcar/SetCollectionTest.java
@@ -0,0 +1,55 @@
+package com.ssabae.nextstep.racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author : leesangbae
+ * @project : java-racingcar
+ * @since : 2020-11-30
+ */
+@DisplayName("Set Collection에 대한 학습 테스트")
+public class SetCollectionTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 Size를 구하는 Test")
+    void setSizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set에 add한 값들이 있는지 확인하는 Test (with ValueSource)")
+    @ValueSource(ints = {1, 2, 3})
+    void containsTest(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("Set에 add한 값들이 있는지 확인하는 Test (with CsvSource")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
+}
---
+++ src/test/java/com/ssabae/nextstep/racingcar/StringClassTest.java
@@ -0,0 +1,93 @@
+package com.ssabae.nextstep.racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * @author : leesangbae
+ * @project : java-racingcar
+ * @since : 2020-11-30
+ */
+@DisplayName("String 클래스에 대한 학습 테스트")
+public class StringClassTest {
+
+    @Test
+    @DisplayName("쉼표를 이용한 String Split Test")
+    void splitTest() {
+        // given
+        String str = "1,2";
+
+        // when
+        String[] split = str.split(",");
+
+        // then
+        assertThat(split).contains("1");
+        assertThat(split).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("쉼표를 이용한 String Split Test (단일 항목)")
+    void splitTestWithSingleItem() {
+        // given
+        String str = "1";
+
+        // when
+        String[] split = str.split(",");
+
+        // then
+        assertThat(split).contains("1");
+    }
+
+    @Test
+    @DisplayName("쉼표를 이용한 String Split Test (괄호 있는 문자열)")
+    void splitTestWithBracketString() {
+        // given
+        String str = "(1,2)";
+        int startIndex = 0;
+        int endIndex = str.length();
+
+        // when
+        if (str.startsWith("(")) {
+            startIndex++;
+        }
+        if (str.endsWith(")")) {
+            endIndex--;
+        }
+        str = str.substring(startIndex, endIndex);
+        String[] split = str.split(",");
+
+        // then
+        assertThat(split).contains("1");
+        assertThat(split).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("문자열의 특정 위치에 대한 문자 반환 Test")
+    void stringCharAtTest() {
+        // given
+        String str = "abc";
+
+        // when
+        char c = str.charAt(1);
+
+        // then
+        assertThat(c).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("문자열의 특정 위치에 대한 문자 반환 예외 Test (IndexOutOfBoundsException)")
+    void stringCharAtTestWithIndexOutOfBoundsException() {
+        // given
+        String str = "ab";
+
+        // when, then
+        assertThatThrownBy(() -> {
+            char c = str.charAt(2);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 2");
+    }
+
+}
