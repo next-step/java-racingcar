5c7033bdfa501fd3ac1fdd76923a9019b7a9a547
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-02 00:17:15 +0900

    test(OpearatorTest): 더하기, 빼기, 곱하기, 나누기, 사칙연산모두 테스트 추가

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -1,15 +1,13 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OperatorTest {
 
@@ -24,5 +22,41 @@ public class OperatorTest {
 			stringCaculator.validate(input);
 		});
 	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,2", "24,56", "100,1", "2314,235"}, delimiter = ',')
+	@DisplayName("더하기 테스트")
+	void plus(Integer frontNumber, Integer rearNumber) {
+		assertEquals(stringCaculator.plus(frontNumber, rearNumber), frontNumber + rearNumber);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,2", "24,56", "100,1", "2314,235"}, delimiter = ',')
+	@DisplayName("빼기 테스트")
+	void minus(Integer frontNumber, Integer rearNumber) {
+		assertEquals(stringCaculator.minus(frontNumber, rearNumber), frontNumber - rearNumber);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,2", "24,56", "100,1", "2314,235"}, delimiter = ',')
+	@DisplayName("곱하기 테스트")
+	void mutiple(Integer frontNumber, Integer rearNumber) {
+		assertEquals(stringCaculator.multiply(frontNumber, rearNumber), frontNumber * rearNumber);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1,2", "24,56", "100,1", "2314,235"}, delimiter = ',')
+	@DisplayName("나누기 테스트")
+	void divide(Integer frontNumber, Integer rearNumber) {
+		assertEquals(stringCaculator.divide(frontNumber, rearNumber), (Integer) frontNumber / rearNumber);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"2 + 3 * 4 / 2"})
+	@DisplayName("사칙 연산을 모두 포함하는 기능 테스트")
+	void calculate(String input) {
+		assertEquals(stringCaculator.calculate(input), 10);
+	}
+
 
 }
