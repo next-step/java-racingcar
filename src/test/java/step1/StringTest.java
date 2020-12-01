e99bd6419947ee824be21df0f4e72dad60d43ab2
chicken-kitchen <hijh1224@gmail.com>
2020-12-02 01:15:12 +0900

    test(String): add charAt test

--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -7,7 +7,6 @@ import java.util.Collection;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -37,5 +36,26 @@ public class StringTest {
 		String result = input.substring(input.indexOf("(") + 1, input.indexOf(")"));
 
 		assertThat(result).isEqualTo("1,2");
+	}
+
+	@TestFactory
+	@DisplayName("요구사항 3")
+	Collection<DynamicTest> charAtTest() {
+		return Arrays.asList(
+			DynamicTest.dynamicTest(
+				"\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.",
+				() -> {
+					assertThat("abc".charAt(0)).isEqualTo('a');
+					assertThat("abc".charAt(1)).isEqualTo('b');
+					assertThat("abc".charAt(2)).isEqualTo('c');
+				}
+			),
+			DynamicTest.dynamicTest(
+				"String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.",
+				() -> assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+					.isThrownBy(() -> "abc".charAt(10))
+					.withMessageMatching("String index out of range: \\d+")
+			)
+		);
 	}
 }
