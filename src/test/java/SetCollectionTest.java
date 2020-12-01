b4265c2b9deefb7c57c2d9419c438382d10d335f
Donghwan Noh <namul.bap@gmail.com>
2020-12-01 21:19:36 +0900

    String과 Set Collection에 대한 학습테스트 작성

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,50 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("Set Collection에 대한 학습 테스트")
+public class SetCollectionTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@DisplayName("size : set의 크기를 확인함")
+	@Test
+	void requireOne() {
+		int result = numbers.size();
+		assertThat(result).isEqualTo(3);
+	}
+
+	@DisplayName("contains : 1, 2, 3의 값이 존재하는지를 확인")
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void requireTwo(int input) {
+		boolean result = numbers.contains(input);
+		assertThat(result).isTrue();
+	}
+
+	@DisplayName("contains : 1, 2, 3, 4, 5의 값이 존재하는지를 확인했을 때 올바른 결과가 나옴")
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void requireThree(int input, boolean expected) {
+		boolean result = numbers.contains(input);
+		assertThat(result).isEqualTo(expected);
+	}
+
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,45 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("String 클래스에 대한 학습 테스트")
+public class StringTest {
+
+	@DisplayName("split : '1,2'을 ,로 split한 결과 배열이 1과 2를 순서대로 포함함")
+	@Test
+	void requireOne1() {
+		String[] result = "1,2".split(",");
+		assertThat(result)
+			.contains("1")
+			.contains("2")
+			.containsExactly("1", "2");
+	}
+
+	@DisplayName("split : '1'을 ,로 split한 결과 배열이 1만 포함함")
+	@Test
+	void requireOne2() {
+		String[] result = "1".split(",");
+		assertThat(result)
+			.contains("1")
+			.containsExactly("1");
+	}
+
+	@DisplayName("substring: (1,2) 값이 주어졌을 때 ()을 제거하고 1,2를 반환함")
+	@Test
+	void requireTwo() {
+		String result = "(1,2)".substring(1, 4);
+		assertThat(result).isEqualTo("1,2");
+	}
+
+	@DisplayName("charAt: 특정 위치의 문자를 가져올 수 없으면 예외를 발생시킴")
+	@Test
+	void requireThree() {
+		assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+			.isThrownBy(() -> {
+				"abc".charAt(-1);
+			})
+			.withMessageMatching("String index out of range: -1");
+	}
+
+}
