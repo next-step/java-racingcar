9d66acc53452a12392a91e4d8e717320b721f931
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-01 23:59:02 +0900

    Test(OperatorTest): 입력값 예외처리 테스트 추가

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -1,8 +1,28 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OperatorTest {
+
+	StringCaculator stringCaculator = new StringCaculator();
+
+	@ParameterizedTest
+	@NullSource
+	@ValueSource(strings = {"", "  ", "1 A 3"})
+	@DisplayName("입력값이 잘못됐을 경우 예외처리 테스트")
+	void throwException_WhenInvalidInput(String input) {
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			stringCaculator.validate(input);
+		});
+	}
+
 }
