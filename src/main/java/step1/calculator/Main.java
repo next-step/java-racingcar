4659fd33dfbebb11b773901fe45ca3c1275a6180
byungkyu-ju <noel1227@gmail.com>
2020-12-01 09:50:55 +0900

    feat: [STEP1] String클래스에 대한 학습테스트 요구사항1

--- src/main/java/step1/calculator/Main.java
+++
@@ -1,9 +0,0 @@
-package step1.calculator;
-
-public class Main {
-
-  public static void main(String[] args) {
-    Long result = Calculator.run("2 + 3 * 4 / 2");
-    System.out.println(result);
-  }
-}
---
+++ src/test/java/step1/StringTest.java
@@ -0,0 +1,39 @@
+package step1;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.MethodOrderer.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/**
+ * @author : byungkyu
+ * @date : 2020/11/30
+ * @description : String 클래스에 대한 학습 테스트
+ **/
+@TestMethodOrder(OrderAnnotation.class)
+class StringTest {
+
+	@Order(1)
+	@DisplayName("1.1. \"1,2\"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
+	@Test
+	void split_two_value_is_correct() {
+		String arg = "1,2";
+		String[] splitValue = arg.split(",");
+		assertThat(splitValue.length).isEqualTo(2);
+		assertThat(splitValue).contains("1", "2");
+	}
+
+	@Order(2)
+	@DisplayName("1.2. \"1,2\"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
+	@Test
+	void split_one_value_with_comma_is_correct() {
+		String arg = "1,";
+		String[] splitValue = arg.split(",");
+		assertThat(splitValue.length).isEqualTo(1);
+		assertThat(splitValue).containsExactly("1");
+	}
+
+}
