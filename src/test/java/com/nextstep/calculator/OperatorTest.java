76c42508e37cc99b74becea98e756084adfcf0f5
lazy-moon <magyeon@naver.com>
2020-12-01 23:14:45 +0900

    feature: 사칙연산 기능 추가
    - 사칙연산 기능 Operator 추가
    - Operator 유닛 테스트 추가

---
+++ src/test/java/com/nextstep/calculator/OperatorTest.java
@@ -0,0 +1,31 @@
+package com.nextstep.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class OperatorTest {
+
+    @DisplayName("연산자에 따른 Operator 획득 및 계산 결과 확인")
+    @ParameterizedTest
+    @CsvSource({"1,+,2,3", "3,-,1,2", "4,*,3,12", "10,/,2,5"})
+    public void calculate(int a, String symbol, int b, int expected){
+        Operator operator = Operator.select(symbol);
+        int result = operator.apply(a,b);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("나누어 떨어지지 않는 숫자가 주어진 경우 예외처리 확인")
+    @Test
+    public void divisionException(){
+        Operator operator = Operator.DIVISION;
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> operator.apply(3,5)).withMessage("3 can't divided into 5");
+    }
+
+}
