734d849ebb635c973e078ad24bacfaec98cba37d
Yeoseon Yun <devyyskr@gmail.com>
2020-12-02 11:42:22 +0900

    test(StringTest): charAt 테스트 추가

--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
 
@@ -33,5 +34,25 @@ public class StringTest {
         String string = "(1,2)";
 
         assertThat(string.substring(1, string.length() - 1)).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt을 이용해 입력한 숫자의 인덱스에 위치한 문자를 가져와야 한다.")
+    void charAt() {
+        String string = "abc";
+
+        assertThat(string.charAt(0)).isEqualTo('a');
+        assertThat(string.charAt(string.length() - 1)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("charAt을 이용해 위치의 범위를 벗어난 인덱스를 지정했을 경우 StringIndexOutOfBoundsException이 발생해야 한다.")
+    void charAt_index_exception() {
+        String string = "abc";
+
+        assertThatThrownBy(() -> {
+            string.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+        .hasMessageMatching("String index out of range: \\d+");
     }
 }
