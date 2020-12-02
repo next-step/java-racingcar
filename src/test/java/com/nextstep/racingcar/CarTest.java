4f661d44b3c39b14bc4681d557c42d9b13ef4f75
lazy-moon <magyeon@naver.com>
2020-12-02 22:26:52 +0900

    feature: 경주에 사용 될 Car 클래스 생성
    - Car의 현재 상태 저장 및 반환을 담당하는 클래스 추가
    - Car의 기능을 테스트하는 CarTest 유닛테스트 추가

---
+++ src/test/java/com/nextstep/racingcar/CarTest.java
@@ -0,0 +1,38 @@
+package com.nextstep.racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+    private Car car;
+
+    @BeforeEach
+    public void init(){
+        car = new Car(5);
+    }
+
+    @DisplayName("move 호출 시 position이 1씩 증가하는지 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    public void move(int count) {
+        for ( int ix = 0 ; ix < count ; ix ++ ) {
+            car.move();
+        }
+        assertThat(car.getPosition()).isEqualTo(count);
+    }
+
+    @DisplayName("현재 position에 따른 finished 체크")
+    @ParameterizedTest
+    @CsvSource({"3, false", "4, false", "5, true"})
+    public void isFinished(int count, boolean expected) {
+        for ( int ix = 0 ; ix < count ; ix ++ ) {
+            car.move();
+        }
+        assertThat(car.isFinished()).isEqualTo(expected);
+    }
+}
