29e9c7b3e99ead570b475fe45ea463da3e1005c1
Mingyu Lee <leemingyu05@gmail.com>
2020-12-02 18:45:54 +0900

    feat: Set 요구사항 1: Set 콜렉션이 중복을 허용하지 않는지 테스트

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,27 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	void given_set_added_duplication_when_size_then_return_size_of_set_without_duplication() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+}
