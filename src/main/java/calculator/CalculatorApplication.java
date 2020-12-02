aa25716f630d1ef9c8154eeb771b5646fbf29f84
byungkyu-ju <noel1227@gmail.com>
2020-12-02 21:42:25 +0900

    feat: [STEP2] 문자열 계산기 입력 및 출력 UI 구현

---
+++ src/main/java/calculator/CalculatorApplication.java
@@ -0,0 +1,12 @@
+package calculator;
+
+/**
+ * @author : byungkyu
+ * @date : 2020/12/02
+ * @description : 문자열 계산기 실행
+ **/
+public class CalculatorApplication {
+	public static void main(String[] args) {
+		View.input();
+	}
+}
---
+++ src/main/java/calculator/View.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import java.util.Scanner;
+
+/**
+ * @author : byungkyu
+ * @date : 2020/12/02
+ * @description : UI
+ **/
+public class View {
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static String input() {
+		return scanner.next();
+	}
+
+	public static void printResult(String arg) {
+		System.out.println(arg);
+	}
+}
