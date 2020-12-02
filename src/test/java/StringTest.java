3fe6dad8fe33b519d17f4c094719ca3d6fcbf031
Mingyu Lee <leemingyu05@gmail.com>
2020-12-02 18:24:23 +0900

    refactor: 힌트 적용, 공통으로 사용하는 콤마 상수를 인스턴스 변수화

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -4,27 +4,24 @@ import org.junit.jupiter.api.Test;
 
 public class StringTest {
 
+	final String COMMA = ",";
+
 	@Test
 	void given_string_when_split_then_return_array() {
-		final String COMMA = ",";
 		final String commaSeparatedNumbers = "1,2";
-		final String[] expectedArray = new String[] {"1", "2"};
 
 		final String[] splitStringArray = commaSeparatedNumbers.split(COMMA);
 
-		assertThat(splitStringArray).isEqualTo(expectedArray);
+		assertThat(splitStringArray).contains("1", "2");
 	}
 
 	@Test
 	void given_only_one_number_string_when_split_then_return_array_with_only_one_number() {
-		final String COMMA = ",";
 		final String onlyOneNumber = "1";
-		final String[] expectedArray = new String[] {"1"};
 
 		final String[] splitStringArray = onlyOneNumber.split(COMMA);
 
-		assertThat(splitStringArray).isEqualTo(expectedArray);
-		assertThat(splitStringArray.length).isEqualTo(1);
+		assertThat(splitStringArray).containsExactly("1");
 	}
 
 }
