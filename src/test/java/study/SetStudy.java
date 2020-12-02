4d39f9ddf06b74b5328d8ee390fac4ff557969e5
sang5c <skah321@gmail.com>
2020-12-02 23:36:19 +0900

    feat(SetStudy): size 학습 테스트 추가

---
+++ src/test/java/study/SetStudy.java
@@ -0,0 +1,31 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetStudy {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size 학습 테스트")
+    @Test
+    public void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+}
