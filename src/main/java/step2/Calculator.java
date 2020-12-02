83a9d94b8116841e1ed792a15a9934142dd58ebf
shk3029 <shk30290@naver.com>
2020-12-02 12:40:39 +0900

    feat: add Calculator class

---
+++ src/main/java/step2/Calculator.java
@@ -0,0 +1,48 @@
+package step2;
+
+public class Calculator {
+
+    private double val;
+    private Operation operation;
+
+    public Calculator() {
+        this.val = 0;
+    }
+
+    public double displayResult(String input) {
+        if (input == null || input.trim().isEmpty()) throw new IllegalArgumentException();
+        for (String s : input.split(" ")) calculate(s);
+        return this.val;
+    }
+
+    private void calculate(String input) {
+        if (!isNumeric(input)) {
+            saveOperation(input);
+            return;
+        }
+        if (this.operation != null) {
+            operate(Double.parseDouble(input));
+            return;
+        }
+        initVal(Double.parseDouble(input));
+    }
+
+    boolean isNumeric(String input) {
+        boolean isNumeric = true;
+        for (char c : input.toCharArray())
+            isNumeric = Character.isDigit(c) ? true : false;
+        return isNumeric;
+    }
+
+    private void saveOperation(String input) {
+        this.operation = Operation.getOperation(input);
+    }
+
+    private void operate(double input) {
+        this.val = operation.calculate(this.val, input);
+    }
+
+    private void initVal(double input) {
+        this.val = input;
+    }
+}
