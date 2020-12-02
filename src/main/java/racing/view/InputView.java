bc873eaa942f7671e582b9e27ded6224da85f48b
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-02 23:16:08 +0900

    fix(InputView): playCount 셋팅안되던 버그 수정

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -32,16 +32,17 @@ public class InputView {
 		this.playCarCount = playCarCount;
 	}
 
-	public void inputUI(){
+	public void inputUI() {
 		System.out.println("자동차 대수는 몇 대 인가요?");
 		int playCarCount = scanner.nextInt();
 		System.out.println(playCarCount);
-		this.setPlayCount(playCount);
+		this.setPlayCarCount(playCarCount);
 
 		System.out.println("시도할 회수는 몇 회 인가요?");
 		int playCount = scanner.nextInt();
 		System.out.println(playCount);
-		this.setPlayCarCount(playCarCount);
+		this.setPlayCount(playCount);
+
 	}
 
 	@Override
