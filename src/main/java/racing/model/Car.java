31cc2637a8a3d2b82a6661daeb91365bc5b89bfa
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-02 23:13:31 +0900

    Add(Car): 자동차 클래스
    (cherry picked from commit c3b009b0849ff7cb380bafffe36966de2aeeaa34)

--- src/main/java/racing/model/Car.java
+++ src/main/java/racing/model/Car.java
@@ -1,2 +1,34 @@
-package racing.model;public class Car {
+package racing.model;
+
+public class Car {
+	private int id;
+	private int forwardCount;
+
+	public Car() {
+	}
+
+	public Car(int id, int forwardCount) {
+		this.id = id;
+		this.forwardCount = forwardCount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getForwardCount() {
+		return forwardCount;
+	}
+
+	public void setForwardCount(int forwardCount) {
+		this.forwardCount = forwardCount;
+	}
+
+	public void forward() {
+		this.forwardCount += 1;
+	}
 }
