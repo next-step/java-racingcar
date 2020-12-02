3ed6ed9ea06918698b869e7d3a85187d2ae7227b
mkkim90 <mkzzang0928@gmail.com>
2020-12-02 22:15:45 +0900

    refactor : 중복테스트 제거 및 중첩테스트 관계 형성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,16 +1,15 @@
 package study;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("String 클래스에 대한 학습 테스트")
@@ -41,23 +40,45 @@ public class StringTest {
                 .startsWith("(")
                 .contains(expected)
                 .endsWith(")");
-        assertThat(input.substring(input.indexOf("(") + 1, input.lastIndexOf(")")))
+        assertThat(input.substring(1, input.length() - 1))
                 .isEqualTo(expected);
     }
 
-    @Test
-    @DisplayName("요구사항 3 \"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현")
-    void charAt() {
-        String input = "abc";
-        assertAll(
-                () ->assertThat(input.charAt(0)).isEqualTo('a'),
-                () ->assertThat(input.charAt(1)).isEqualTo('b'),
-                () ->assertThat(input.charAt(2)).isEqualTo('c'),
-                () ->assertThatThrownBy(() -> {
-                    input.charAt(input.length());
-                }).isInstanceOf(StringIndexOutOfBoundsException.class)
-                        .hasMessageContaining("String index out of range:")
-                );
+    @Nested
+    @DisplayName("요구사항 3")
+    class charAtTest {
+        private String value;
+
+        @BeforeEach
+        void setUp() {
+            this.value = "abc";
+        }
+
+        @DisplayName("String의 chartAt() 메소드 학습 테스트를 구현")
+        @ParameterizedTest(name = "{0} 위치의 문자 ''{1}''를 반환하는지 확인")
+        @CsvSource(value = {"0, a", "1, b", "2, c"})
+        void charAt(int index, char expected) {
+            assertThat(value.charAt(index))
+                    .isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현")
+        void shouldStringIndexOutOfBoundsExceptionByCharAt() {
+            // given
+            int outOfIndex = value.length();
+
+            // when
+            Throwable thrown = catchThrowable(() -> {
+                value.charAt(outOfIndex);
+            });
+
+            // then
+            assertThat(thrown).isInstanceOf(StringIndexOutOfBoundsException.class)
+                    .hasMessageStartingWith("String index")
+                    .hasMessageContaining("out of range:")
+                    .hasMessageEndingWith(String.valueOf(outOfIndex));
+        }
     }
 
 }
