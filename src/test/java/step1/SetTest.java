53753c3a3f12a9d7863b0ae98637d092b5dca7aa
Yeoseon Yun <devyyskr@gmail.com>
2020-12-02 11:47:45 +0900

    test(SetTest): SetTest생성 및 size테스트 추가

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,31 @@
+package step1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set는 중복되는 요소는 제외하고 add되어야 한다.")
+    void size() {
+         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+
+}
