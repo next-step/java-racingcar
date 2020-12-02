72125937618787da50f7f5303c0be326c23249bb
Yeoseon Yun <devyyskr@gmail.com>
2020-12-02 11:56:41 +0900

    test(SetTest): Set contains 테스트 추가

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -3,6 +3,9 @@ package step1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -27,5 +30,23 @@ public class SetTest {
          assertThat(numbers.size()).isEqualTo(3);
     }
 
+    @Test
+    @DisplayName("Set의 요소들이 잘 들어갔는지 테스트 한다.")
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3}) // six numbers
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    void contains(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
 }
