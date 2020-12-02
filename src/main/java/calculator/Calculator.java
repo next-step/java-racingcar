234634fb5ce99a8a918569fc21db1f604b5e48c4
louisJu <rokmctkd6@gmail.com>
2020-12-02 10:00:42 +0900

    refactor: 계산 로직 메소드 캡슐화    * public static -> private 으로 변경(내부에서만 호출되도록 처리)

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,14 +6,16 @@ public class Calculator {
     public Calculator(Expression expression) {
         this.expression = expression;
     }
+
     public String calculate() {
         String result = expression.of(0);
-        for(int i = 1; i < expression.length() - 1; i=i+2) {
-            result = calculate(expression.of(i), result, expression.of(i+1));
+        for(int i = 1; i < expression.length() - 1; i = i + 2) {
+            result = calculate(expression.of(i), result, expression.of(i + 1));
         }
         return result;
     }
-    public static String calculate(String operator, String num1, String num2) {
+
+    private String calculate(String operator, String num1, String num2) {
         if(operator.equals("+")) {
             return addition(num1, num2);
         }
@@ -29,19 +31,19 @@ public class Calculator {
         throw new IllegalArgumentException( operator + " is not operator");
     }
 
-    private static String addition(String num1, String num2) {
+    private String addition(String num1, String num2) {
         return String.valueOf(Integer.parseInt(num1) + Integer.parseInt(num2));
     }
 
-    private static String subtraction(String num1, String num2) {
+    private String subtraction(String num1, String num2) {
         return String.valueOf(Integer.parseInt(num1) - Integer.parseInt(num2));
     }
 
-    private static String multiplication(String num1, String num2) {
+    private String multiplication(String num1, String num2) {
         return String.valueOf(Integer.parseInt(num1) * Integer.parseInt(num2));
     }
 
-    private static String division(String num1, String num2) {
+    private String division(String num1, String num2) {
         return String.valueOf(Integer.parseInt(num1) / Integer.parseInt(num2));
     }
 }
