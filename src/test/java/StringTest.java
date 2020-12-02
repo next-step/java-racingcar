42547c8ef3802ef844681c126b06adcb79dc0b87
Mingyu Lee <leemingyu05@gmail.com>
2020-12-02 18:20:32 +0900

    feat: "12" 문자열 split 수행시 1만을 포함하는 배열이 반환

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -15,4 +15,16 @@ public class StringTest {
 		assertThat(splitStringArray).isEqualTo(expectedArray);
 	}
 
+	@Test
+	void given_only_one_number_string_when_split_then_return_array_with_only_one_number() {
+		final String COMMA = ",";
+		final String onlyOneNumber = "1";
+		final String[] expectedArray = new String[] {"1"};
+
+		final String[] splitStringArray = onlyOneNumber.split(COMMA);
+
+		assertThat(splitStringArray).isEqualTo(expectedArray);
+		assertThat(splitStringArray.length).isEqualTo(1);
+	}
+
 }
