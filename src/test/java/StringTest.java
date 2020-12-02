a7aee0a1832ad47dd659fcff707a55bd8b7e2fa1
Mingyu Lee <leemingyu05@gmail.com>
2020-12-02 22:20:56 +0900

    1단계 - 학습 테스트 실습 (#1605)
    * feat: "1,2" 문자열 split 분리 테스트
    
    * feat: "12" 문자열 split 수행시 1만을 포함하는 배열이 반환
    
    * refactor: 힌트 적용, 공통으로 사용하는 콤마 상수를 인스턴스 변수화
    
    * feat: 요구사항2 > substring() 메서드를 활용해 괄호 제거 테스트
    
    * feat: 요구사항3: charAt() 반환결과와 익셉션 테스트
    
    * feat: Set 요구사항 1: Set 콜렉션이 중복을 허용하지 않는지 테스트
    
    * feat: Set 요구사항 2: Set contains() 메서드 테스트
    
    * feat: Set 요구사항 3: Set contains() 메서드 false를 포함한 테스트

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,57 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+	final String COMMA = ",";
+
+	@Test
+	void given_string_when_split_then_return_array() {
+		final String commaSeparatedNumbers = "1,2";
+
+		final String[] splitStringArray = commaSeparatedNumbers.split(COMMA);
+
+		assertThat(splitStringArray).contains("1", "2");
+	}
+
+	@Test
+	void given_only_one_number_string_when_split_then_return_array_with_only_one_number() {
+		final String onlyOneNumber = "1";
+
+		final String[] splitStringArray = onlyOneNumber.split(COMMA);
+
+		assertThat(splitStringArray).containsExactly("1");
+	}
+
+	@Test
+	void given_comma_string_with_bracket_when_substring_then_return_string_without_bracket() {
+		final String openBracket = "(";
+		final String closeBracket = ")";
+		final String commaSeparatedNumbersWithBracket = "(1,2)";
+		final String expectedString = "1,2";
+
+		final String commaSeparatedNumbersWithoutBracket = commaSeparatedNumbersWithBracket
+			.substring(commaSeparatedNumbersWithBracket.indexOf(openBracket) + 1,
+				commaSeparatedNumbersWithBracket.indexOf(closeBracket));
+
+		assertThat(commaSeparatedNumbersWithoutBracket).isEqualTo(expectedString);
+	}
+
+	@DisplayName("String charAt()이 특정 위치의 문자를 가져오고 위치가 인덱스 범위를 벗어날 경우 익셉션 발생 테스트")
+	@Test
+	void given_string_when_chatAt_with_index_then_return_char_at_index() {
+		final String givenString = "abc";
+		final int indexA = 0;
+		final int exceptionIndex = 9;
+
+		assertThat(givenString.charAt(indexA)).isEqualTo('a');
+
+		assertThatThrownBy(() -> givenString.charAt(exceptionIndex))
+			.isInstanceOf(StringIndexOutOfBoundsException.class)
+			.hasMessage("String index out of range: %s", exceptionIndex);
+
+	}
+
+}
