f2564addac06d6d095ca309b8fdc983cff4eb5ca
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-02 23:13:07 +0900

    feat(RacingCar): 전진 여부를 구하는 기능

--- src/main/java/racing/RacingCar.java
+++ src/main/java/racing/RacingCar.java
@@ -1,15 +1,45 @@
 package racing;
 
+import racing.model.Car;
 import racing.view.InputView;
+import racing.view.ResultView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class RacingCar {
+
+	Random random = new Random();
 
 	public void main() {
 		InputView inputView = new InputView();
 		inputView.inputUI();
 
+		List<Car> carList = createRacingCar(inputView.getPlayCarCount());
 
+		for (int i = 0; i < inputView.getPlayCount(); i++) {
+			playRacing(carList);
+		}
+	}
+
+	private void playRacing(List<Car> carList) {
+		for (int i = 0; i < carList.size(); i++) {
+			int result = random.nextInt(10);
+			if (result >= 4) {
+				Car car = carList.get(i);
+				car.forward();
+			}
+		}
+	}
+
+	private List<Car> createRacingCar(int playCarCount) {
+		List<Car> carList = new ArrayList<>();
+		for (int i = 0; i < playCarCount; i++) {
+			Car car = new Car(i, 0);
+			carList.add(car);
+		}
+		return carList;
 	}
 }
