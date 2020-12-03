3664e4daaccba35cbf328adc94e670f20a300b0d
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-04 01:13:55 +0900

    refactor(Input): extract scanner input function to Input class

--- src/main/java/com/jaenyeong/mission01/stringcalculator/Main.java
+++ src/main/java/com/jaenyeong/mission01/stringcalculator/Main.java
@@ -1,28 +1,14 @@
 package com.jaenyeong.mission01.stringcalculator;
 
-import java.util.Scanner;
-
 public class Main {
-    private static final Scanner SC = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("사칙 연산을 수행할 문자열을 입력해주세요");
-        final String inputExpression = inputExpression();
-        final int result = getCalculationResult(inputExpression);
+        Input input = new Input();
+        final String expression = input.inputString();
 
-        System.out.println(result);
-    }
-
-    private static String inputExpression() {
-        final String inputExpression = SC.nextLine();
-        SC.close();
-
-        return inputExpression;
-    }
-
-    private static int getCalculationResult(String expression) {
         final StringCalculator calculator = new StringCalculator();
 
-        return calculator.calculate(expression);
+        System.out.println(calculator.calculate(expression));
     }
 }
