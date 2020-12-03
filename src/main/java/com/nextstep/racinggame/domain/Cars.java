7dccf8cf089c241f40893646f324a8b92c0ba3e6
ccf05017 <ccf05017@gmail.com>
2020-12-03 13:59:37 +0900

    refactor(Cars): defensive array copy
    - 좀 더 안전한 일급 컬렉션 구현을 위해 객체 생성 시 방어적 복사를 수행하도록 기능 개선

--- src/main/java/com/nextstep/racinggame/domain/Cars.java
+++ src/main/java/com/nextstep/racinggame/domain/Cars.java
@@ -1,5 +1,6 @@
 package com.nextstep.racinggame.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,15 +9,15 @@ public class Cars {
     private final List<Car> cars;
 
     Cars(final List<Car> cars) {
-        this.cars = cars;
+        this.cars = new ArrayList<>(cars);
     }
 
     public static Cars of(final int carDemand) {
-        List<Car> cars = IntStream.range(0, carDemand)
+        return new Cars(
+                IntStream.range(0, carDemand)
                 .mapToObj(number -> Car.of())
-                .collect(Collectors.toList());
-
-        return new Cars(cars);
+                .collect(Collectors.toList())
+        );
     }
 
     public int size() {
