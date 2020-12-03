bc9049383c4d156d40ab812837308ab7a3dc447e
ccf05017 <ccf05017@gmail.com>
2020-12-03 14:14:00 +0900

    feat(Cars): can move all cars
    - 자동차 경주에서 모든 차량을 동시에 움직이고 결과를 얻기 위한 기능 구현

--- src/main/java/com/nextstep/racinggame/domain/Cars.java
+++ src/main/java/com/nextstep/racinggame/domain/Cars.java
@@ -2,6 +2,7 @@ package com.nextstep.racinggame.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -22,5 +23,26 @@ public class Cars {
 
     public int size() {
         return this.cars.size();
+    }
+
+    public Cars move(GasStation gasStation) {
+        List<Car> movedCars = this.cars.stream()
+                .map(car -> car.move(gasStation))
+                .collect(Collectors.toList());
+
+        return new Cars(movedCars);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cars cars1 = (Cars) o;
+        return cars.equals(cars1.cars);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cars);
     }
 }
--- src/test/java/com/nextstep/racinggame/domain/CarsTests.java
+++ src/test/java/com/nextstep/racinggame/domain/CarsTests.java
@@ -2,6 +2,12 @@ package com.nextstep.racinggame.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,5 +20,30 @@ class CarsTests {
 
         assertThat(cars).isNotNull();
         assertThat(cars.size()).isEqualTo(carDemand);
+    }
+
+    @DisplayName("속한 차량들을 한번에 움직일 수 있다.")
+    @ParameterizedTest
+    @MethodSource("moveAllTestResource")
+    void moveAllTest(GasStation gasStation, Cars expectedCars) {
+        int carDemand = 3;
+        Cars cars = Cars.of(carDemand);
+
+        Cars movedCars = cars.move(gasStation);
+
+        assertThat(movedCars)
+                .isEqualTo(expectedCars);
+    }
+    public static Stream<Arguments> moveAllTestResource() {
+        return Stream.of(
+                Arguments.of(
+                        new FourFuelGasStation(),
+                        new Cars(Arrays.asList(new Car(1), new Car(1), new Car(1)))
+                ),
+                Arguments.of(
+                        new ThreeFuelGasStation(),
+                        new Cars(Arrays.asList(new Car(0), new Car(0), new Car(0)))
+                )
+        );
     }
 }
