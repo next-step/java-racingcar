ca36588fe1689d7d573ac58735f20744c1777fa0
ccf05017 <ccf05017@gmail.com>
2020-12-03 15:44:16 +0900

    refactor(RacingGameRunner): extract method
    - 코드 가독성을 높이기 위해 게임 진행 부분과 게임 입력 부분을 메서드로 분리

--- src/main/java/com/nextstep/racinggame/application/RacingGameRunner.java
+++ src/main/java/com/nextstep/racinggame/application/RacingGameRunner.java
@@ -9,18 +9,30 @@ import java.util.Scanner;
 public class RacingGameRunner {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final GasStation GAS_STATION = new RandomGasStation();
+
     private static final String CAR_DEMAND_COMMENT = "자동차 대수는 몇 대 인가요?";
-    private static final String RESULT_HEADER = "실행 결과";
+    private static final String RACE_DEMAND_COMMENT = "시도할 회수는 몇 회 인가요?";
+    private static final String RESULT_HEADER = "\n실행 결과";
 
     public static void main(String[] args) {
         System.out.println(CAR_DEMAND_COMMENT);
         int carDemand = SCANNER.nextInt();
+        System.out.println(RACE_DEMAND_COMMENT);
+        int raceDemand = SCANNER.nextInt();
 
+        run(carDemand, raceDemand);
+    }
+
+    private static void run(final int carDemand, final int raceDemand) {
         Cars cars = Cars.of(carDemand);
 
-        Cars racedCars = RacingGame.race(cars, GAS_STATION);
-
         System.out.println(RESULT_HEADER);
-        System.out.println(RacingGameResultDrawer.drawGameResult(racedCars));
+        for (int i = 0; i < raceDemand; i++) {
+            Cars racedCars = RacingGame.race(cars, GAS_STATION);
+
+            System.out.println(RacingGameResultDrawer.drawGameResult(racedCars));
+
+            cars = racedCars;
+        }
     }
 }
