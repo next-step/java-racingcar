9f632fc98220e762a7a813a15f3482c9e6928eb7
shk3029 <shk30290@naver.com>
2020-12-04 00:34:03 +0900

    feat: 전진이 가능한지 체크 TDD

---
+++ src/main/java/step3/Checkor.java
@@ -0,0 +1,10 @@
+package step3;
+
+public class Checkor {
+
+    private final int ADVANCE_MIN_NUMBER = 4;
+
+    public boolean check(int input) {
+        return input >= ADVANCE_MIN_NUMBER;
+    }
+}
