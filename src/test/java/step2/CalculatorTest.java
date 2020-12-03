91309aa6d9a74b5a4b7e8998791a582683ee35fb
shk3029 <shk30290@naver.com>
2020-12-03 23:37:51 +0900

    test: 계산기 테스트 케이스 4가지로 나눠서 검증하도록 변경 (덧셈, 뺄셈, 곱셈, 나눗셈)

--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
@@ -17,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class CalculatorTest {
 
     Calculator calculator;
+    double result;
 
     @BeforeEach
     void setUp() {
@@ -24,25 +24,48 @@ class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"12:true","1:true","+:false","-:false","52:true"}, delimiter = ':')
+    @CsvSource(value = {"12:true", "1:true", "+:false", "-:false", "52:true"}, delimiter = ':')
     @DisplayName("현재 입력된 문자열이 숫자인지 확인하는 테스트")
     public void isNumericTest(String input, boolean expected) {
         assertThat(calculator.isNumeric(input)).isEqualTo(expected);
     }
 
+
     @ParameterizedTest
-    @MethodSource("generateCalculatorData")
-    @DisplayName("계산기 테스트")
-    public void calculator(String input, double expected) {
-        assertThat(calculator.displayResult(input)).isEqualTo(expected);
+    @CsvSource(value = {"5 + 5:10", "0 + 5:5", "100 + 10:110"}, delimiter = ':')
+    @DisplayName("덧셈 문자열을 입력하면, 합을 반환하는 테스트")
+    public void calculator_plus(String input, double expected) {
+        assertThat(calculate(input)).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"5 - 5:0", "0 - 5:-5", "100 - 10:90"}, delimiter = ':')
+    @DisplayName("뺄셈 문자열을 입력하면, 차를 반환하는 테스트")
+    public void calculator_minus(String input, double expected) {
+        assertThat(calculate(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"5 * 5:25", "0 * 5:0", "100 * 10:1000"}, delimiter = ':')
+    @DisplayName("곱셈 문자열을 입력하면, 곱셈 결과를 반환하는 테스트")
+    public void calculator_times(String input, double expected) {
+        assertThat(calculate(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"5 / 5:1", "0 / 5:0", "100 / 10:10"}, delimiter = ':')
+    @DisplayName("나눗셈 문자열을 입력하면, 나눗셈 결과를 반환하는 테스트")
+    public void calculator_divide(String input, double expected) {
+        assertThat(calculate(input)).isEqualTo(expected);
+    }
+
 
     @ParameterizedTest
     @ValueSource(strings = {" ", "   ", "      "})
     @DisplayName("input이 공백이면 IllegalArgumentException")
     public void empty_input_illegalArgumentExceptionTest(String input) {
         assertThatThrownBy(() -> {
-            calculator.displayResult(input);
+            calculator.calculate(result, input);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -50,8 +73,16 @@ class CalculatorTest {
     @DisplayName("input이 null이면 IllegalArgumentException")
     public void null_input_illegalArgumentExceptionTest() {
         assertThatThrownBy(() -> {
-            calculator.displayResult(null);
+            calculator.calculate(result, null);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    private double calculate(String input) {
+        for (String s : input.split(" ")) {
+            result = calculator.calculate(result, s);
+        }
+        return result;
     }
 
     static Stream<Arguments> generateCalculatorData() {
