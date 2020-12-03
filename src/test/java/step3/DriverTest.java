aca77f561f45968455f14313832b179f1e45e50a
shk3029 <shk30290@naver.com>
2020-12-04 01:58:26 +0900

    test: 불필요한 import 제거

--- src/test/java/step3/DriverTest.java
+++ src/test/java/step3/DriverTest.java
@@ -2,7 +2,6 @@ package step3;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,8 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class DriverTest {
 
