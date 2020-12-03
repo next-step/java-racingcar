6359385bf10f3b201b1dd9594c2450ac3d469654
shk3029 <shk30290@naver.com>
2020-12-04 01:57:52 +0900

    fix: staic, final 위치 수정

--- src/main/java/step3/RandomGenerator.java
+++ src/main/java/step3/RandomGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomGenerator {
 
-    private final static Random random = new Random();
+    private static final Random random = new Random();
 
     public static int generate() {
         return random.nextInt(10);
