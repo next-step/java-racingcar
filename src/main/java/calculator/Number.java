7f9dce7cd4d7b18b0cc397d7404e9da91454c5e8
정연화 <yhjeong1009@gmail.com>
2020-12-04 02:32:25 +0900

    refactor : 유효성 검증 로직 변경, 연산함수 enum에서 처리

---
+++ src/main/java/calculator/Number.java
@@ -0,0 +1,19 @@
+package calculator;
+
+public class Number {
+    private final static String NOT_NUMBER_REGEX = "//D+";
+    private final Integer item;
+    public Number(String input) {
+        // 유효성 검증
+        if (input == null || input.isEmpty() || input.matches(NOT_NUMBER_REGEX)) {
+            throw new IllegalArgumentException();
+        }
+        this.item = Integer.parseInt(input);
+    }
+    public Number(int input) {
+        this.item = input;
+    }
+    public Integer getItem() {
+        return item;
+    }
+}
