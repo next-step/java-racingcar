9e2304b276da432ec4dc1ccbec33adf6c6af6c86
shk3029 <shk30290@naver.com>
2020-12-03 23:36:45 +0900

    fix: Calculator 클래스 내 val 인스턴스 변수 제거

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -2,25 +2,24 @@ package step2;
 
 public class Calculator {
 
-    private double val;
     private Operation operation;
 
-    public double displayResult(String input) {
-        if (input == null || input.trim().isEmpty()) throw new IllegalArgumentException();
-        for (String s : input.split(" ")) calculate(s);
-        return this.val;
-    }
+    public double calculate(double result, String input) {
+        if (isEmptyOrNull(input)) throw new IllegalArgumentException();
 
-    private void calculate(String input) {
         if (!isNumeric(input)) {
             saveOperation(input);
-            return;
+            return result;
         }
         if (this.operation != null) {
-            operate(Integer.parseInt(input));
-            return;
+            return operation.calculate(result, Integer.parseInt(input));
         }
-        initVal(Integer.parseInt(input));
+
+        return Integer.parseInt(input);
+    }
+
+    private boolean isEmptyOrNull(String input) {
+        return input == null || input.trim().isEmpty();
     }
 
     boolean isNumeric(String input) {
@@ -34,11 +33,4 @@ public class Calculator {
         this.operation = Operation.getOperation(input);
     }
 
-    private void operate(double input) {
-        this.val = operation.calculate(this.val, input);
-    }
-
-    private void initVal(double input) {
-        this.val = input;
-    }
 }
