a2ce4be43ae11eebcf96574ea9dd3129a68e9c26
cionman <topspin1278@naver.com>
2020-12-03 19:23:44 +0900

    feat: 게임 결과를 출력하는 기능 추가

--- src/main/java/com/woowahan/racing/view/ResultView.java
+++ src/main/java/com/woowahan/racing/view/ResultView.java
@@ -7,7 +7,20 @@ import com.woowahan.racing.model.GameResult;
 public class ResultView {
 
 	public static void printResult(List<GameResult> gameResultList) {
-		throw new RuntimeException("결과 출력 구현");
+		printResultHeader();
+		for (GameResult gameResult : gameResultList) {
+			printEachResult(gameResult);
+		}
+	}
+
+	public static void printResultHeader() {
+		System.out.println("실행 결과:");
+	}
+
+	public static void printEachResult(GameResult gameResult) {
+		gameResult.getCarList()
+			.forEach(car -> System.out.println(car.getDistance()));
+		System.out.println();
 	}
 
 }
