262da3002109828a1ee6fdf9e1e4739f64dcb260
louisJu <rokmctkd6@gmail.com>
2020-12-03 11:09:20 +0900

    refactor: 피드백 반영   * Expression 클래스 인스턴스 변수명 변경.

--- src/main/java/calculator/Expression.java
+++ src/main/java/calculator/Expression.java
@@ -3,22 +3,20 @@ package calculator;
 import java.util.Objects;
 
 public class Expression {
-    private final String[] expression;
+    private final String[] operators;
 
     public Expression(String expression) {
         if(Objects.isNull(expression) || expression.trim().isEmpty()) {
             throw new IllegalArgumentException("input data is empty");
         }
-        this.expression = expression.split(" ");
+        this.operators = expression.split(" ");
     }
 
-    public String of(int num) {
-        return expression[num];
+    public String of(int idx) {
+        return operators[idx];
     }
 
     public int length() {
-        return expression.length;
+        return operators.length;
     }
-
-
 }
