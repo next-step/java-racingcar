d7e5ab939299ecfefdecf3f69eab9cea543953b8
cionman <topspin1278@naver.com>
2020-12-03 23:46:43 +0900

    refactor: InputResult 객체 생성시 validation 추가

--- src/main/java/com/woowahan/racing/model/InputResult.java
+++ src/main/java/com/woowahan/racing/model/InputResult.java
@@ -1,11 +1,16 @@
 package com.woowahan.racing.model;
 
+import com.woowahan.racing.constant.Message;
+
 public class InputResult {
 
 	private final int carCount;
 	private final int tryCount;
 
 	private InputResult(int carCount, int tryCount) {
+		if (carCount <= 0 || tryCount <= 0) {
+			throw new IllegalArgumentException(Message.MSG_POSITIVE_NUMBER);
+		}
 		this.carCount = carCount;
 		this.tryCount = tryCount;
 	}
