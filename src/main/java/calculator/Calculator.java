492acf83a19e79691713f7fc08ab464bc483df7e
sang5c <skah321@gmail.com>
2020-12-04 21:14:58 +0900

    코드 초기화

--- src/main/java/calculator/Calculator.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-public class Calculator {
-
-    public static long calculate(long left, long right, String operator) {
-        return Operator.calculate(left, right, operator);
-    }
-
-}
