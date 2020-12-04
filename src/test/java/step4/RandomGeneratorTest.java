c40905d76f1748512bb77d447d9df227d8fee13c
shk3029 <shk30290@naver.com>
2020-12-04 13:10:54 +0900

    test: import 수정

--- src/test/java/step4/RandomGeneratorTest.java
+++ src/test/java/step4/RandomGeneratorTest.java
@@ -3,6 +3,7 @@ package step4;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class RandomGeneratorTest {
