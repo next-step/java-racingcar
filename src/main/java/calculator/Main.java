1423205fa1941e461e03a87488b175d90a968d48
hyeji <khji1210@naver.com>
2020-12-04 15:57:18 +0900

    feat: main 함수 구현

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,8 +1,13 @@
 package calculator;
 
+import calculator.domain.StringCalculator;
+
+import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        StringCalculator stringCalculator = new StringCalculator(scanner.nextLine());
+        System.out.println(stringCalculator.calculate());
     }
 }
