5ec8baab812d0e4ba01169b061c84c233377ff14
jiho <jhhj424@naver.com>
2020-12-05 00:27:37 +0900

    refactor: CalculatorFormula에서 구조적으로 숫자, 연산자를 먼저 분류하도록 수정, 이때 입력값에 대한 검증을 함

--- src/main/java/calculator/CalculatorFormula.java
+++ src/main/java/calculator/CalculatorFormula.java
@@ -1,60 +1,56 @@
 package calculator;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CalculatorFormula {
     private static final String WRONG_INPUT = "입력값이 잘못되었습니다.";
-    private final List<String> formula;
+    private final List<Integer> numbers = new ArrayList<>();
+    private final List<Operator> operators = new ArrayList<>();;
 
-    public CalculatorFormula(String input) {
-        inputValidation(input);
-        formula = Arrays.asList(input.split(" "));
+    public CalculatorFormula(String[] inputArray) {
+        inputValidation(inputArray);
+        for (int i = 0; i < inputArray.length; i += 2) {
+            numbers.add(Integer.parseInt(inputArray[i]));
+            int operatorsIndex = i + 1;
+            if (operatorsIndex != inputArray.length) {
+                operators.add(Operator.getOperator(inputArray[operatorsIndex]));
+            }
+        }
     }
 
     public int getNumber(int index) {
-        int numberIndex = index * 2;
-        return Integer.parseInt(formula.get(numberIndex));
+        return numbers.get(index);
     }
-    public String getOperator(int index) {
-        int operatorIndex = index * 2 -1;
-        return formula.get(operatorIndex);
+    public Operator getOperator(int index) {
+        return operators.get(index);
     }
 
-    private void inputValidation(String input) {
-        if (CalculatorFormula.isNull(input)) {
-            throw new IllegalArgumentException(WRONG_INPUT);
-        }
-        if (CalculatorFormula.isEmpty(input)) {
-            throw new IllegalArgumentException(WRONG_INPUT);
-        }
-        if (CalculatorFormula.isNotConvertToInt(input)) {
-            throw new IllegalArgumentException(WRONG_INPUT);
-        }
-        if (CalculatorFormula.isNotAble(input)) {
+    private void inputValidation(String[] input) {
+        if (CalculatorFormula.isNull(input) || CalculatorFormula.isEmpty(input) ||
+            CalculatorFormula.isNotConvertToInt(input) || CalculatorFormula.isNotAble(input)) {
             throw new IllegalArgumentException(WRONG_INPUT);
         }
     }
 
-    private static boolean isNull(String input) {
-        return input == null;
+    private static boolean isNull(String[] inputArray) {
+        return inputArray == null;
     }
 
-    private static boolean isEmpty(String input) {
-        return input.trim().isEmpty();
+    private static boolean isEmpty(String[] inputArray) {
+        return inputArray.length == 0;
     }
 
-    private static boolean isNotConvertToInt(String input) {
-        for (int i = 0; i < input.length(); i += 4) {
-            if(!Character.isDigit(input.charAt(i))){
+    private static boolean isNotConvertToInt(String[] inputArray) {
+        for (int i = 0; i < inputArray.length; i += 2) {
+            if(!Character.isDigit(inputArray[i].charAt(0))){
                 return true;
             }
         }
         return false;
     }
 
-    private static boolean isNotAble(String input) {
-        String[] strings = input.split(" ");
-        return strings.length % 2 == 0;
+    private static boolean isNotAble(String[] inputArray) {
+        return inputArray.length % 2 == 0;
     }
 }
--- src/test/java/calculator/CalculatorFormulaTest.java
+++ src/test/java/calculator/CalculatorFormulaTest.java
@@ -10,16 +10,14 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class CalculatorFormulaTest {
     private static final String WRONG_INPUT = "입력값이 잘못되었습니다.";
+    private static final String BLANK = " ";
 
     @Test
     @DisplayName("입력값이 null일 경우 IllegalArgumentException throw")
     public void nullCheck(){
-        //given
-        String NULL = null;
-
         //when then
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new CalculatorFormula(NULL))
+                .isThrownBy(() -> new CalculatorFormula(null))
                 .withMessageMatching(WRONG_INPUT);
     }
 
@@ -27,11 +25,11 @@ public class CalculatorFormulaTest {
     @DisplayName("입력값이 빈 공백 문자일 경우 IllegalArgumentException throw")
     public void blankCheck(){
         //given
-        String BLANK = " ";
+        String blank = " ";
 
         //when then
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new CalculatorFormula(BLANK))
+                .isThrownBy(() -> new CalculatorFormula(blank.split(BLANK)))
                 .withMessageMatching(WRONG_INPUT);
     }
 
@@ -43,7 +41,7 @@ public class CalculatorFormulaTest {
 
         //when then
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new CalculatorFormula(notOperator))
+                .isThrownBy(() -> new CalculatorFormula(notOperator.split(BLANK)))
                 .withMessageMatching(WRONG_INPUT);
     }
 
@@ -52,20 +50,20 @@ public class CalculatorFormulaTest {
     @DisplayName("순번에 맞는 숫자를 반환하는지 테스트")
     public void numberCheck(String input, int index, int expected){
         //when
-        CalculatorFormula formula = new CalculatorFormula(input);
+        CalculatorFormula formula = new CalculatorFormula(input.split(BLANK));
 
         //then
         assertThat(formula.getNumber(index)).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1 + 2 + 3, 1, +","4 * 2 / 1, 1, *","3 * 5 - 9 + 2, 3, +"})
+    @CsvSource(value = {"1 + 2 + 3, 1, +","4 * 2 / 1, 0, *","3 * 5 - 9 + 2, 2, +"})
     @DisplayName("순번에 맞는 연산자를 반환하는지 테스트")
     public void operatorCheck(String input, int index, String expected){
         //when
-        CalculatorFormula formula = new CalculatorFormula(input);
+        CalculatorFormula formula = new CalculatorFormula(input.split(BLANK));
 
         //then
-        assertThat(formula.getOperator(index)).isEqualTo(expected);
+        assertThat(formula.getOperator(index).getOperator()).isEqualTo(expected);
     }
 }
