17e6857df1d4d0b9c32d0883ff69f7fdf59b4fe8
ccf05017 <ccf05017@gmail.com>
2020-12-04 21:43:52 +0900

    refactor(RacingGameRunner): can play game by car names
    - 변경된 요구사항이 반영됐는지 확인하기 위해 쉼표로 구분된 문자열로 기존처럼 레이싱 게임을 진행할 수 있도록 기능 변경

--- src/main/java/com/nextstep/racinggame/application/RacingGameRunner.java
+++ src/main/java/com/nextstep/racinggame/application/RacingGameRunner.java
@@ -1,6 +1,7 @@
 package com.nextstep.racinggame.application;
 
 import com.nextstep.racinggame.domain.*;
+import com.nextstep.racinggame.view.InputView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,30 +11,31 @@ public class RacingGameRunner {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final MovePolicy GAS_STATION = new RandomMovePolicy();
 
-    private static final String CAR_DEMAND_COMMENT = "자동차 대수는 몇 대 인가요?";
+    private static final String CAR_DEMAND_COMMENT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String RACE_DEMAND_COMMENT = "시도할 회수는 몇 회 인가요?";
     private static final String RESULT_HEADER = "\n실행 결과";
 
     public static void main(String[] args) {
         System.out.println(CAR_DEMAND_COMMENT);
-        int carDemand = SCANNER.nextInt();
+        String userInput = SCANNER.nextLine();
         System.out.println(RACE_DEMAND_COMMENT);
         int raceDemand = SCANNER.nextInt();
 
-        run(carDemand, raceDemand);
+        run(userInput, raceDemand);
     }
 
-    private static void run(final int carDemand, final int raceDemand) {
-//        // TODO: 임시방편 Cars 수정 후 수정 필요
-//        Cars cars = new Cars(new ArrayList<>());
-//
-//        System.out.println(RESULT_HEADER);
-//        for (int i = 0; i < raceDemand; i++) {
-//            Cars racedCars = RacingGame.race(cars, GAS_STATION);
-//
-//            System.out.println(RacingGameResultDrawer.drawGameResult(racedCars));
-//
-//            cars = racedCars;
-//        }
+    private static void run(final String userInput, final int raceDemand) {
+        InputView inputView = new InputView(userInput);
+
+        Cars cars = CarsFactory.createCars(inputView.parseToNames());
+
+        System.out.println(RESULT_HEADER);
+        for (int i = 0; i < raceDemand; i++) {
+            Cars racedCars = RacingGame.race(cars, GAS_STATION);
+
+            System.out.println(RacingGameResultDrawer.drawGameResult(racedCars));
+
+            cars = racedCars;
+        }
     }
 }
