a7fcb002c8d1c333fa80af67451c1012c1953c77
cionman <topspin1278@naver.com>
2020-12-04 14:01:10 +0900

    refactor: GameRandom 상수 private으로 변경

--- src/main/java/com/woowahan/racing/util/GameRandom.java
+++ src/main/java/com/woowahan/racing/util/GameRandom.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class GameRandom {
 
-	public static final int BOUND = 10;
-	public static final int MIN_NUMBER_FOR_MOVE = 4;
+	private static final int BOUND = 10;
+	private static final int MIN_NUMBER_FOR_MOVE = 4;
 
 	public static boolean isGameWin() {
 		return new Random().nextInt(BOUND) >= MIN_NUMBER_FOR_MOVE;
