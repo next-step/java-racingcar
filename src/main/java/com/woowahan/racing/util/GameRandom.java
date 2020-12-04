112376906fe657c7cd4644cdc5dbe4772d73ff6b
cionman <topspin1278@naver.com>
2020-12-04 14:11:35 +0900

    refactor: GameRandom, 도메인 요구사항이 드러나도록 BOUND 상수 제거 및 MAX_NUMBER_FOR_MOVE 값으로 대체

--- src/main/java/com/woowahan/racing/util/GameRandom.java
+++ src/main/java/com/woowahan/racing/util/GameRandom.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public class GameRandom {
 
-	private static final int BOUND = 10;
+	private static final int MAX_NUMBER_FOR_MOVE = 9;
 	private static final int MIN_NUMBER_FOR_MOVE = 4;
 
 	public static boolean isGameWin() {
-		return new Random().nextInt(BOUND) >= MIN_NUMBER_FOR_MOVE;
+		int randomBound = MAX_NUMBER_FOR_MOVE + 1;
+		return new Random().nextInt(randomBound) >= MIN_NUMBER_FOR_MOVE;
 	}
 
 }
