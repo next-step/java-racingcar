23b631d948e642775d88cb7343b8a2fd9741cec8
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-05 02:44:02 +0900

    코멘트 반영

--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -1,21 +1,9 @@
 package racing.view;
 
-import racing.model.Car;
-
-import java.util.List;
-
 public class ResultView {
-	public void printResult(List<Car> carList){
-		System.out.println("Ready.. Go!");
-		for (int i = 0; i < carList.size(); i++) {
-			Car car = carList.get(i);
-			print(car.getForwardCount());
-		}
-		System.out.println("End!");
-	}
 
-	public void print(int forwardCount){
-		for (int i = 0; i < forwardCount; i++) {
+	public void print(int forwardRecult) {
+		for (int i = 0; i < forwardRecult; i++) {
 			System.out.print("-");
 		}
 		System.out.println();
