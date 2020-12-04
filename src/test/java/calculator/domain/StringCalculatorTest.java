f8d6555ffa4c7f9db052740ac1b0a05ef3c6c5b2
hyeji <khji1210@naver.com>
2020-12-04 13:48:54 +0900

    feat: 뺄셈 기능 구현

--- src/test/java/calculator/domain/StringCalculatorTest.java
+++ src/test/java/calculator/domain/StringCalculatorTest.java
@@ -1,17 +1,27 @@
 package calculator.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
+    private StringCalculator stringCalculator = new StringCalculator();
 
     @DisplayName("덧셈")
-    @Test
-    void add() {
-        StringCalculator stringCalculator = new StringCalculator();
-        double result = stringCalculator.calculate("+", 1, 2);
-        assertThat(result).isEqualTo(3);
+    @ParameterizedTest
+    @CsvSource(value = {"1:2:3", "4:5:9"}, delimiter = ':')
+    void add(double num1, double num2, double expected) {
+        double result = stringCalculator.calculate("+", num1, num2);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("뺄셈")
+    @ParameterizedTest
+    @CsvSource(value = {"1:2:-1", "9:3:6"}, delimiter = ':')
+    void subtract(double num1, double num2, double expected) {
+        double result = stringCalculator.calculate("-", num1, num2);
+        assertThat(result).isEqualTo(expected);
     }
 }
