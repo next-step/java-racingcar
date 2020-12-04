767fcac217f602dd88c7f32c5354dd21cb989ef7
cionman <topspin1278@naver.com>
2020-12-04 22:19:17 +0900

    refactor: InputView 질문 메세지, Message 클래스의 상수로 이동

--- src/main/java/com/woowahan/racing/constant/Message.java
+++ src/main/java/com/woowahan/racing/constant/Message.java
@@ -2,4 +2,6 @@ package com.woowahan.racing.constant;
 
 public class Message {
 	public static final String MSG_POSITIVE_NUMBER = "0보다 큰 숫자를 입력해주세요.";
+	public static final String MSG_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
+	public static final String MSG_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
 }
--- src/main/java/com/woowahan/racing/view/InputView.java
+++ src/main/java/com/woowahan/racing/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static int getCarCount() {
-		System.out.println("자동차 대수는 몇 대 인가요?");
+		System.out.println(Message.MSG_CAR_COUNT);
 		String input = scanner.nextLine();
 		if (!ValidationUtil.validate(input)) {
 			System.out.println(Message.MSG_POSITIVE_NUMBER);
@@ -20,7 +20,7 @@ public class InputView {
 	}
 
 	public static int getTryCount() {
-		System.out.println("시도할 회수는 몇 회 인가요?");
+		System.out.println(Message.MSG_TRY_COUNT);
 		String input = scanner.nextLine();
 		if (!ValidationUtil.validate(input)) {
 			System.out.println(Message.MSG_POSITIVE_NUMBER);
