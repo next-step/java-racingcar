77963b9f3105493b847e8b3c66637b1178a5ed93
orgojy <giveawesome@gmail.com>
2020-12-05 17:07:26 +0900

    feat: 계산에 필요한 전위/중위/후위 표기법 변환 유틸 추가

---
+++ src/main/java/util/NotationUtils.java
@@ -0,0 +1,46 @@
+package util;
+
+import stringcalculator.arithmeticcomponent.ArithmeticOperator;
+
+import java.util.Stack;
+
+/**
+ * 표기법 관련 Util
+ */
+public class NotationUtils {
+    public static StringBuilder convertInfixIntoPostfix(String[] infixValues) {
+        Stack<String> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for (String value : infixValues) {
+            if (ArithmeticOperator.isContained(value)) {
+                while (!stack.isEmpty()) {
+                    result.append(stack.pop());
+                }
+                stack.push(value);
+                continue;
+            }
+            result.append(value);
+        }
+
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        return result;
+    }
+
+    public static Stack<String> convertPostfixStack(String[] infixValues) {
+        StringBuilder postfixExpression = NotationUtils.convertInfixIntoPostfix(infixValues);
+        String reversePostfixExpression = postfixExpression.reverse().toString();
+        return convertStack(reversePostfixExpression);
+    }
+
+    private static Stack<String> convertStack(String expression) {
+        Stack<String> stack = new Stack<>();
+        for (char value : expression.toCharArray()) {
+            stack.push(String.valueOf(value));
+        }
+        return stack;
+    }
+}
