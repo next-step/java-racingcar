e3c8e8793ceba744201dbcd769e6ef65f1904637
bss <baekss0409@gmail.com>
2020-12-05 22:05:06 +0900

    feat: 자동차 전진 기능 구현

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -4,11 +4,28 @@ public class Car {
 
     private Dashboard dashboard;
 
+    private final int MOVEMENT_CONDITION = 3;
+    private final int MOVEMENT_DISTANCE = 1;
+
     public Car() {
         this.dashboard = new Dashboard();
     }
 
     public Dashboard getDashboard() {
         return dashboard;
+    }
+
+    public void accelerate(int number) {
+        if (isMovable(number)) {
+            move(MOVEMENT_DISTANCE);
+        }
+    }
+
+    private void move(int distance) {
+        getDashboard().addMileage(distance);
+    }
+
+    private boolean isMovable(int number) {
+        return number > MOVEMENT_CONDITION;
     }
 }
--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -3,6 +3,8 @@ package step3;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -20,5 +22,13 @@ public class CarTest {
     void createCarTest() {
         assertThat(car.getDashboard()).isNotNull();
         assertThat(car.getDashboard().getMileage()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,0", "2,0", "3,0", "4,1", "7,1", "9,1"})
+    @DisplayName("자동차 전진 테스트")
+    void acceleratorTest(int input, int expected) {
+        car.accelerate(input);
+        assertThat(car.getDashboard().getMileage()).isEqualTo(expected);
     }
 }
