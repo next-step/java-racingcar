2aa23636a012d1242af2482b8a024c52f59a7b28
hhlyn0328 <hhlyn0328@daum.net>
2020-12-06 00:52:51 +0900

    feat: 시도 횟수 입력받아 횟수만큼 반복 이동하도록 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,10 +3,16 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
+
+	private static Scanner scanner = new Scanner(System.in);
+
 	public static int totalCarCount() {
 		System.out.println("자동차 대수는 몇 대 인가요?");
+		return scanner.nextInt();
+	}
 
-		Scanner scanner = new Scanner(System.in);
+	public static int totalTryCount() {
+		System.out.println("시도할 횟수는 몇 회 인가요?");
 		return scanner.nextInt();
 	}
 }
--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -6,7 +6,13 @@ public class RacingCar {
 	public static void run() {
 		int carCount = InputView.totalCarCount();
 		Car[] cars = initCarArray(carCount);
-		moveCars(cars);
+
+		int tryCount = InputView.totalTryCount();
+		System.out.println("실행결과");
+		for (int i = 0; i < tryCount; i++) {
+			moveCars(cars);
+			System.out.println();
+		}
 	}
 
 	private static void moveCars(Car[] cars) {
