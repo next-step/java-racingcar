ab33463efd0a9bddb6f3a80122a55a5766b4e7fb
Sang Uk Ahn <suahnn@gmail.com>
2020-12-05 14:55:05 +0900

    test: Set의 contains 테스트 (입력 값에 따라 결과 값이 다른 경우)

--- src/test/java/string/SetTest.java
+++ src/test/java/string/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -35,5 +36,12 @@ public class SetTest {
     @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인")
     void contains(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    @DisplayName("Set의 contains() 메소드의 입력 값에 따라 결과 값이 다른 경우")
+    void contains_ShouldReturnTrueOrFalseForInputs(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
