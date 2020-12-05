164528a7127068e400c603703639017b30596bbd
jiho <jhhj424@naver.com>
2020-12-05 15:46:19 +0900

    refactor: 실행 테스트용 클래스를 CalculatorApplication으로 이름 변경

--- src/main/java/calculator/CalculatorApplication.java
+++ src/main/java/calculator/CalculatorApplication.java
@@ -2,12 +2,13 @@ package calculator;
 
 import java.util.Scanner;
 
-public class CalculatorMain {
+public class CalculatorApplication {
     public static void main(String[] args) {
         System.out.println("계산 할 문자열을 입력하세요.");
         Scanner sc = new Scanner(System.in);
-        String formula = sc.nextLine();
-        int result = Calculator.calculate(formula);
+        String inputValue = sc.nextLine();
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(inputValue);
         System.out.println("계산결과 : " + result);
     }
 }
