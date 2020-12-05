49f3dbfb54755cbe006aafadc3147c49e84cac9d
lazy-moon <magyeon@naver.com>
2020-12-05 22:31:01 +0900

    refactor: 구분자 "," 상수로 변경

--- src/main/java/com/nextstep/racingcar/Parser.java
+++ src/main/java/com/nextstep/racingcar/Parser.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
+    private final static String DELIMITER = ",";
+    
     public static List<CarName> parseCarNames(String line) {
         List<CarName> carNames = new ArrayList<>();
-        String[] names = line.split(",");
+        String[] names = line.split(DELIMITER);
 
         for (String name : names ) {
             name = name.trim();
