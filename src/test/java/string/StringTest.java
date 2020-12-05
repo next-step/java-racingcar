6a03ad238b63951f6655486cae62fa7097c5b313
Sang Uk Ahn <suahnn@gmail.com>
2020-12-05 14:32:23 +0900

    test: String 클래스의 substring 테스트

--- src/test/java/string/StringTest.java
+++ src/test/java/string/StringTest.java
@@ -2,6 +2,8 @@ package string;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,5 +24,13 @@ public class StringTest {
         String[] result = "1".split(",");
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1");
+    }
+
+    @ParameterizedTest
+    @DisplayName("\"(1,2)\"값이 주어졌을 때 String의 substring() 메소드를 활용해 \"()\"을 제거하고 \"1,2\" 반환")
+    @ValueSource(strings = {"(1,2)"})
+    void substring(String str) {
+        String result = str.substring(str.indexOf("(") + 1, str.indexOf(")"));
+        assertThat(result).isEqualTo("1,2");
     }
 }
