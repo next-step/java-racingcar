78c7486c5ff7a2f4fb178f46503151a496b26a48
coderhglee <goodna17@gmail.com>
2020-12-05 20:46:36 +0900

    feat($input): add user input
    - 사용자에게 대수, 회수를 입력받는 기능

---
+++ src/main/java/race/InputView.java
@@ -0,0 +1,23 @@
+package race;
+
+import java.util.Scanner;
+
+public class InputView {
+	private static final String numberOfCarMessage = "자동차 대수는 몇 대 인가요?";
+	private static final String numberOfTryTimesMessage = "시도할 회수는 몇 회 인가요?";
+
+	private final Scanner in = new Scanner(System.in);
+
+	public GameProcessor settingGame() {
+		int numberOfCar = 0;
+		int numberOfTryTimes = 0;
+
+		System.out.println(numberOfCarMessage);
+		numberOfCar = in.nextInt();
+
+		System.out.println(numberOfTryTimesMessage);
+		numberOfTryTimes = in.nextInt();
+
+		return new GameProcessor(numberOfCar, numberOfTryTimes, new RandomMovePolicy());
+	}
+}
