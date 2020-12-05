9706f439c180710043d44250349df9edddde9d75
orgojy <giveawesome@gmail.com>
2020-12-05 17:07:18 +0900

    feat: 1회 연산 결과를 저장할 값 정의

---
+++ src/main/java/stringcalculator/arithmeticcomponent/ArithmeticResult.java
@@ -0,0 +1,16 @@
+package stringcalculator.arithmeticcomponent;
+
+/**
+ * 1회 연산의 숫자 결과
+ */
+public class ArithmeticResult {
+    private int number;
+
+    public ArithmeticResult(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
