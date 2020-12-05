3040486080bec432681e4195e636ec16c3e7aa77
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-05 21:54:24 +0900

    (refactor) 문자열 더하기 테스트 수정

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,41 +1,31 @@
 package calculator;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StringCalculatorTest {
 
     private StringCalculator calculator = new StringCalculator();
 
-    @BeforeAll
+    @BeforeEach
     void setUp() {
         calculator = new StringCalculator();
     }
 
     @ParameterizedTest
-    @DisplayName("입력 문자열 공백으로 나누는 테스트")
+    @DisplayName("문자열 계산기 테스트")
     @CsvSource(value = {
-            "2 + 3 - 4 * 8 / 2=9",
-            "2 + 3 * 4 / 2=7",
-            "7 + 5 * 9=5",
-            "9 + 6=3",
-            "7=1",
+            "2 + 3 * 4 / 2=10",
+            "2 + 3 - 4 * 5 / 2=2",
+            "7 + 5 * 9 / 6=18",
+            "9 + 6 * 3 / 5 + 1=10",
+            "7 + 7 * 4 / 8=7",
     }, delimiter = '=')
-    void input_split_test(String text, int expected) {
-        // when
-        List<String> result = calculator.splitText(text);
-
-        // then
-        assertThat(result.size()).isEqualTo(expected);
+    void string_calculate_test(String text, int expected) {
+        assertEquals(expected, calculator.calculate(text));
     }
 }
-
