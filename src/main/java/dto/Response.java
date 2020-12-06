41bb7f5da9913a584209939271e05d6d77635283
yohan <good_1411@naver.com>
2020-12-07 02:29:15 +0900

    feat: 출력 화면(ResultView)에 대한 로직을 추가 구현합니다.
    - 자동차 이름 출력.
    - 우승자 출력.

--- src/main/java/dto/Response.java
+++ src/main/java/dto/Response.java
@@ -4,20 +4,25 @@ import racing.domain.GameReport;
 import racing.domain.RoundReport;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Response {
 
-    private final List<RoundReport> roundReports;
+    private final GameReport gameReport;
 
-    public Response(final List<RoundReport> roundReports) {
-        this.roundReports = roundReports;
+    public Response(final GameReport gameReport) {
+        this.gameReport = Objects.requireNonNull(gameReport);
     }
 
     public static Response of(GameReport gameReport) {
-        return new Response(gameReport.getRoundReports());
+        return new Response(gameReport);
     }
 
     public List<RoundReport> getRoundReports() {
-        return roundReports;
+        return gameReport.getRoundReports();
+    }
+
+    public List<String> getWinners() {
+        return gameReport.winner();
     }
 }
