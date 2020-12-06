8d775037b06b8a63c449db5e471b1c641957cc3d
orgojy <giveawesome@gmail.com>
2020-12-07 01:39:10 +0900

    feat(CarRacingGame.java): 자동차 경주 게임을 구성하는 `CarRacingGame` 구현

---
+++ src/main/java/carracing/Application.java
@@ -0,0 +1,8 @@
+package carracing;
+
+public class Application {
+    public static void main(String[] args) {
+        CarRacingGame carRacingGame = new CarRacingGame();
+        carRacingGame.run();
+    }
+}
