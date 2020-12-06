2bbcff28152a4c0ea6fbc5abdacb911b841093e6
shk3029 <shk30290@naver.com>
2020-12-06 16:21:05 +0900

    feat: 자동차 이름을 받아서 자동차를 생산해주는 공장 추가

---
+++ src/main/java/step4/domain/CarFactory.java
@@ -0,0 +1,16 @@
+package step4.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+
+    public static List<Car> produceCars(String carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames.split(",")) {
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
+
+}
