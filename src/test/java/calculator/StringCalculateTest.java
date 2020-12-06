57ad72f0fa03f1c6d7c61f9a98d2ee264eb48b3f
Sang Uk Ahn <suahnn@gmail.com>
2020-12-06 13:50:51 +0900

    test: 입력값이 null 이거나 empty인 경우 예외 발생 테스트

--- src/test/java/calculator/StringCalculateTest.java
+++ src/test/java/calculator/StringCalculateTest.java
@@ -3,8 +3,10 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculateTest {
 
@@ -15,5 +17,13 @@ public class StringCalculateTest {
     @DisplayName("입력한 문자열 계산 정상 수행")
     public void calculate(String input, int result) {
         assertThat(StringCalculate.calculate(input)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("null 또는 empty 입력 값 전달 시 예외")
+    public void calculateWithNullAndEmpty(String input) {
+        assertThatThrownBy(() -> StringCalculate.calculate(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
