97430da179f527d4825b01dd01b424a50b5c32e2
chh <chh2644@gmail.com>
2020-12-06 13:55:52 +0900

    refactor : 상수 치환 및 정규 표현식

--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -1,6 +1,10 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class InputValidator {
+    private static final String BLANK = " ";
+    private static final Pattern NUMERIC = Pattern.compile("^[0-9]+$");
     String input;
 
     public InputValidator(String input) {
@@ -8,16 +12,18 @@ public class InputValidator {
     }
 
     public void validInput() {
-        String[] inputArr = input.split(" ");
+        String[] inputArr = input.split(BLANK);
 
         if(input.isEmpty() || (inputArr.length > 2 && inputArr.length % 2 == 0))
             throw new IllegalArgumentException("올바르게 입력해주세요.");
+
+        for(int i=0; i< inputArr.length; i+=2) {
+            validNumber(inputArr[i]);
+        }
     }
 
     public void validNumber(String num) {
-        String regex = "^[0-9]+$";
-
-        if(!num.matches(regex))
+        if(!NUMERIC.matcher(num).find())
             throw new NumberFormatException("숫자를 입력해주세요");
     }
 }
