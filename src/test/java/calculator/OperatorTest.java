8361cdb519a5d789fbbd36e3d30a24b96da7ece5
Sang Uk Ahn <suahnn@gmail.com>
2020-12-06 12:41:36 +0900

    test: 뺄셈, 곱셈, 나눗셈 연산자 테스트 추가

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -8,8 +8,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class OperatorTest {
 
     @Test
-    @DisplayName("덧셈 테스트")
+    @DisplayName("덧셈")
     public void plus() {
         assertThat(Operator.PLUS.apply(1, 2)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("뺄셈")
+    public void minus() {
+        assertThat(Operator.MINUS.apply(1, 2)).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("곱셈")
+    public void multiply() {
+        assertThat(Operator.MULTIPLY.apply(1, 2)).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("나눗셈")
+    public void divide() {
+        assertThat(Operator.DIVIDE.apply(2, 1)).isEqualTo(2);
     }
 }
