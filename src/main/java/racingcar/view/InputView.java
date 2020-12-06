77d4725afd81576448b48793af804ff581b46020
kim-daeyong <kdy2353@gmail.com>
2020-12-06 18:26:06 +0900

    refactor : validateNameLength 사용하도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -13,9 +13,13 @@ public class InputView {
     
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String[] racer = sc.nextLine().split(",");
+
+        validateNameLength(racer);
         map.put(0, racer);
+
         System.out.println("시도할 횟수는 몇회 인가요?");
         String[] move = {sc.nextLine()};
+
         map.put(1, move);
 
         sc.close();
@@ -25,7 +29,7 @@ public class InputView {
     public void validateNameLength(String[] racer){
         Stream.of(racer).forEach(s ->   {
             if (s.length() > 5){
-                throw new RuntimeException(s + "의 이름이 5글자 이상입니다.");
+                throw new IllegalArgumentException(s + "의 이름이 5글자 이상입니다.");
             }
         });
     }
