9592fb67c319e7c1c978ce29381e233e9680a511
Sang Uk Ahn <suahnn@gmail.com>
2020-12-06 12:42:52 +0900

    feat: 뺄셈, 곱셈, 나눗셈 연산자 구현

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -4,7 +4,12 @@ import java.util.function.IntBinaryOperator;
 
 public enum Operator {
 
-    PLUS(Integer::sum);
+    PLUS(Integer::sum),
+    MINUS((left, right) -> left - right),
+    MULTIPLY((left, right) -> left * right),
+    DIVIDE((left, right) -> left / right)
+
+    ;
 
     private final IntBinaryOperator operator;
 
