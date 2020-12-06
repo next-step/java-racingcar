2878e52cfd67f79c5389a307b7b944db3d7cd324
Sang Uk Ahn <suahnn@gmail.com>
2020-12-06 13:30:33 +0900

    feat: 연산자 계산 기능 구현, 연산자 심볼 캐싱

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,23 +1,41 @@
 package calculator;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.IntBinaryOperator;
 
 public enum Operator {
 
-    PLUS(Integer::sum),
-    MINUS((left, right) -> left - right),
-    MULTIPLY((left, right) -> left * right),
-    DIVIDE((left, right) -> left / right)
+    PLUS('+', Integer::sum),
+    MINUS('-', (left, right) -> left - right),
+    MULTIPLY('*', (left, right) -> left * right),
+    DIVIDE('/', (left, right) -> left / right)
 
     ;
 
+    private static final Map<Character, Operator> OPERATOR_BY_SYMBOL;
+
+    static {
+        OPERATOR_BY_SYMBOL = new HashMap<>();
+        for (Operator operator : values()) {
+            OPERATOR_BY_SYMBOL.put(operator.symbol, operator);
+        }
+    }
+
+    private final char symbol;
     private final IntBinaryOperator operator;
 
-    Operator(IntBinaryOperator operator) {
+    Operator(char symbol, IntBinaryOperator operator) {
+        this.symbol = symbol;
         this.operator = operator;
     }
 
     public int apply(int left, int right) {
         return operator.applyAsInt(left, right);
+    }
+
+    public static int calculate(char symbol, int left, int right) {
+        return OPERATOR_BY_SYMBOL.get(symbol)
+                .apply(left, right);
     }
 }
