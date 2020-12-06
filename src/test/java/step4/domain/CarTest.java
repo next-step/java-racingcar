08e58c92aac0282b11ab61b1bd22f057c8410da2
shk3029 <shk30290@naver.com>
2020-12-06 16:42:01 +0900

    test: MovingStrategy 목킹제거 후, 람다표현식으로 수정

--- src/test/java/step4/domain/CarTest.java
+++ src/test/java/step4/domain/CarTest.java
@@ -3,24 +3,16 @@ package step4.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step4.domain.Car;
-import step4.domain.strategy.MovingStrategy;
-import step4.domain.strategy.RandomMovingStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class CarTest {
 
     Car car;
-    MovingStrategy movingStrategy;
-
     @BeforeEach
     void setUp() {
         car = new Car("jack");
-        movingStrategy = mock(RandomMovingStrategy.class);
     }
 
     @Test
@@ -34,16 +26,14 @@ public class CarTest {
     @Test
     @DisplayName("전진 조건에 만족하면 전진")
     void move() {
-        when(movingStrategy.isMove()).thenReturn(true);
-        car.move(movingStrategy);
+        car.move(()->true);
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
     @Test
     @DisplayName("전진 조건에 만족하지 않으면 스탑")
     void stop() {
-        when(movingStrategy.isMove()).thenReturn(false);
-        car.move(movingStrategy);
+        car.move(()->false);
         assertThat(car.getPosition()).isEqualTo(0);
     }
 
