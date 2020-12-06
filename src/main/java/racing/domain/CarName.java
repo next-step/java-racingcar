2ef9e34bb3f574cf69021f45e51dd847eb730249
yohan <good_1411@naver.com>
2020-12-07 01:19:00 +0900

    feat: 자동차 이름(CarName)에 대한 로직을 구현합니다.

---
+++ src/main/java/racing/domain/CarName.java
@@ -0,0 +1,39 @@
+package racing.domain;
+
+import java.util.Objects;
+
+public class CarName {
+
+    private static final int MAX_LENGTH = 5;
+
+    private final String value;
+
+    private CarName(final String value) {
+        this.value = Objects.requireNonNull(value);
+    }
+
+    public static CarName of(final String value) {
+        if (Objects.isNull(value) || value.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (value.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+
+        return new CarName(value);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CarName)) return false;
+        final CarName carName = (CarName) o;
+        return Objects.equals(value, carName.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/racing/domain/CarNameTest.java
@@ -0,0 +1,46 @@
+package racing.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+트
+@DisplayName("자동차 이름(CarName) 테스")
+public class CarNameTest {
+
+    @DisplayName("5자 이내의 이름을 허용한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"쇼", "미더", "머니?", "산화머니", "자동차이름"})
+    void create(String value) {
+        // when
+        CarName carName = CarName.of(value);
+
+        // then
+        assertThat(carName).isNotNull();
+    }
+
+    @DisplayName("null 값은 허용하지 않는다.")
+    @Test
+    void createNotNull() {
+        // when / then
+        assertThrows(IllegalArgumentException.class, () -> CarName.of(null));
+    }
+
+    @DisplayName("빈 값은 허용하지 않는다.")
+    @Test
+    void createNotEmpty() {
+        // when / then
+        assertThrows(IllegalArgumentException.class, () -> CarName.of(""));
+    }
+
+    @DisplayName("5자를 초과할 수 없다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"여섯글자이름", "일곱그을자이름"})
+    void createLengthOver(String value) {
+        // when / then
+        assertThrows(IllegalArgumentException.class, () -> CarName.of(value));
+    }
+}
