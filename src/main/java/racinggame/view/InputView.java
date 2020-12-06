e0983091ae9dc1b7b064a1534fd116df356af093
hyeji <khji1210@naver.com>
2020-12-07 05:52:23 +0900

    feat(InputView) : 자동차 이름 입력 기능 추가

--- src/main/java/racinggame/view/InputView.java
+++ src/main/java/racinggame/view/InputView.java
@@ -1,18 +1,17 @@
 package racinggame.view;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String SPLIT_NAME_TEXT = ",";
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int carCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        try {
-            return scanner.nextInt();
-        } catch (InputMismatchException e) {
-            throw new InputMismatchException("숫자로 입력해주세요.");
-        }
+    public static List<String> carNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return Arrays.asList(scanner.nextLine().split(SPLIT_NAME_TEXT));
     }
 
     public static int gameCount() {
