67d8b54f2be3721d600f4bad50503748115ce54a
kim-daeyong <kdy2353@gmail.com>
2020-12-06 18:36:27 +0900

    input view

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,20 +6,15 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class InputView {
+    Scanner sc = new Scanner(System.in);
     public Map<Integer, String[]> inputRacingByName(){
-        Scanner sc = new Scanner(System.in);
 
         Map<Integer, String[]> map = new HashMap<>();
-    
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String[] racer = sc.nextLine().split(",");
 
-        validateNameLength(racer);
+        String[] racer = getRacerNames();
         map.put(0, racer);
 
-        System.out.println("시도할 횟수는 몇회 인가요?");
-        String[] move = {sc.nextLine()};
-
+        String[] move = getNumberOfRacing();
         map.put(1, move);
 
         sc.close();
@@ -32,6 +27,19 @@ public class InputView {
                 throw new IllegalArgumentException(s + "의 이름이 5글자 이상입니다.");
             }
         });
+    }
+
+    public String[] getRacerNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String[] racer = sc.nextLine().split(",");
+        validateNameLength(racer);
+        
+        return racer;
+    }
+
+    public String[] getNumberOfRacing() {
+        System.out.println("시도할 횟수는 몇회 인가요?");
+        return sc.nextLine().split(",");
     }
     
 }
