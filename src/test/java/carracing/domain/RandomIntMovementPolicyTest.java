a338dcce640e497fce6212a66f309b0460cf5f98
orgojy <giveawesome@gmail.com>
2020-12-06 23:31:53 +0900

    refactor(RandomIntMovementPolicyTest.java): 자동차 전진을 위한 정책의 케이스 생성 방식 변경

--- src/test/java/carracing/domain/RandomIntMovementPolicyTest.java
+++ src/test/java/carracing/domain/RandomIntMovementPolicyTest.java
@@ -3,7 +3,12 @@ package carracing.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -11,6 +16,10 @@ import static org.mockito.Mockito.when;
 
 @DisplayName("자동차 전진을 위한 정책을 위한 테스트")
 class RandomIntMovementPolicyTest {
+
+    private static final int POLICY_BASE_NUMBER = RandomIntMovementPolicy.getPolicyBaseNumber();
+    private static final int MAX_POSITION_NUMBER = 9;
+
     private RandomGenerator randomGenerator;
     private MovementPolicy movementPolicy;
 
@@ -22,7 +31,7 @@ class RandomIntMovementPolicyTest {
 
     @DisplayName("정책에서 자동차 전진이 가능한 경우 확인")
     @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @MethodSource("provideMovableCases")
     void checkMovableCasesInPolicyTest(int movableNumber) {
         // When
         when(randomGenerator.generateZeroToNineInt())
@@ -32,9 +41,17 @@ class RandomIntMovementPolicyTest {
         assertThat(movementPolicy.isMovable()).isTrue();
     }
 
+    private static Stream<Arguments> provideMovableCases() {
+        List<Arguments> arguments = new ArrayList<>();
+        for (int i = POLICY_BASE_NUMBER; i <= MAX_POSITION_NUMBER; i++) {
+            arguments.add(Arguments.of(i));
+        }
+        return arguments.stream();
+    }
+
     @DisplayName("정책에서 자동차 전진이 불가능한 경우 확인")
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
+    @MethodSource("provideUnmovableCases")
     void checkUnmovableCasesInPolicyTest(int unmovableNumber) {
         // When
         when(randomGenerator.generateZeroToNineInt())
@@ -42,5 +59,13 @@ class RandomIntMovementPolicyTest {
 
         // Then
         assertThat(movementPolicy.isMovable()).isFalse();
+    }
+
+    private static Stream<Arguments> provideUnmovableCases() {
+        List<Arguments> arguments = new ArrayList<>();
+        for (int i = 0; i < POLICY_BASE_NUMBER; i++) {
+            arguments.add(Arguments.of(i));
+        }
+        return arguments.stream();
     }
 }
