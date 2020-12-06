5607921f159bac86c645e16125caff4833d87843
ssovely <74448524+ssovely@users.noreply.github.com>
2020-12-06 14:05:35 +0900

    [자동차경주 - 2단계] 리뷰요청드립니다. (#1657)
    * feat(Operator): add Operator enum
    
    Added defined operator and execute function
    
    * feat(Operator): add function to find operator
    
    Added map to find operator with sign key
    Added function to find operator in map by key
    - undefined operator throw IllegalArgumentException
    
    * feat(Operand): add operand model
    
    Model with operand values
    Validation that input value is numeric
    
    * refactor(Operator): introduce constant
    
    * feat(Calculator): add calculator
    
    Added StringCalculator
    - classify input strings into operators and operands
    - execute calculation
    Added OperationElement
    - enum defining elements required for operation
    Added QueueUtils
    - convert list into queue
    
    * feat(starter): add starter
    
    * refactor(calculator): first class collection

---
+++ src/main/java/step2/Operator.java
@@ -0,0 +1,40 @@
+package step2;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Operator {
+	PLUS("+", (left, right) -> left + right),
+	MINUS("-", (left, right) -> left - right),
+	MULTIPLY("*", (left, right) -> left * right),
+	DIVIDE("/", (left, right) -> left / right);
+
+	private final String sign;
+	private final IntBinaryOperator operator;
+
+	Operator(String sign, IntBinaryOperator operator) {
+		this.sign = sign;
+		this.operator = operator;
+	}
+
+	private static final Map<String, Operator> operators = Collections.unmodifiableMap(
+		Stream.of(Operator.values())
+			.collect(
+				Collectors.toMap(operator -> operator.sign, Function.identity())
+			)
+	);
+
+	public static Operator valueOfSign(String sign) {
+		return Optional.ofNullable(operators.get(sign))
+			.orElseThrow(() -> new IllegalArgumentException(Message.UNDEFINED_OPERATOR));
+	}
+
+	public Operand operate(Operand left, Operand right) {
+		return new Operand(operator.applyAsInt(left.getValue(), right.getValue()));
+	}
+}
---
+++ src/test/java/step2/StringCalculatorTest.java
@@ -0,0 +1,113 @@
+package step2;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class StringCalculatorTest {
+	@Test
+	@DisplayName("입력 값이 null이면 IllegalArgumentException이 발생한다.")
+	void validateInput_ThrowIllegalArgumentExceptionWhenNull() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringCalculator.validateInput(null))
+			.withMessage(Message.INPUT_STRING);
+	}
+
+	@Test
+	@DisplayName("입력 값이 빈 공백이면 IllegalArgumentException이 발생한다.")
+	void validateInput_ThrowIllegalArgumentExceptionWhenEmpty() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringCalculator.validateInput(""))
+			.withMessage(Message.INPUT_STRING);
+	}
+
+	@Test
+	@DisplayName("입력 값이 연산에 필요한 최소 문자열 갯수보다 작으면 IllegalArgumentException이 발생한다.")
+	void splitBySpace_ThrowIllegalArgumentExceptionWhenLessThanMinSize() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringCalculator.splitBySpace("1 *"))
+			.withMessage(Message.INPUT_WRONG_STRING);
+
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringCalculator.splitBySpace("1 2"))
+			.withMessage(Message.INPUT_WRONG_STRING);
+	}
+
+	@Test
+	@DisplayName("입력 값이 최소 문자열 갯수보단 많지만 수식을 만들수 없는 문자열일 경우 IllegalArgumentException이 발생한다.")
+	void splitBySpace_ThrowIllegalArgumentExceptionWhenNotSizeToMakeFormula() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringCalculator.splitBySpace("1 * 4 /"))
+			.withMessage(Message.INPUT_WRONG_STRING);
+
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringCalculator.splitBySpace("1 * 4 / 2 3"))
+			.withMessage(Message.INPUT_WRONG_STRING);
+	}
+
+	@Test
+	@DisplayName("입력 값이 최소 문자열 갯수보다 많고 홀수 갯수이면 공백으로 문자열을 분리한다.")
+	void splitBySpace() {
+		assertThat(StringCalculator.splitBySpace("1 * 2"))
+			.containsExactly("1", "*", "2");
+
+		assertThat(StringCalculator.splitBySpace("1 * 2 / 2"))
+			.containsExactly("1", "*", "2", "/", "2");
+
+		assertThat(StringCalculator.splitBySpace("1 * 2 3 4"))
+			.containsExactly("1", "*", "2", "3", "4");
+	}
+
+	@Test
+	@DisplayName("입력 값을 index에 따라 operand와 operator로 분류한다.")
+	void groupingByClassTypeTest() {
+		String[] inputStrings = {"1", "*", "2", "/", "2"};
+		Map<OperationElement, List<String>> classTypeGroups = StringCalculator.groupingByClassType(inputStrings);
+
+		assertThat(classTypeGroups.size()).isEqualTo(2);
+		assertThat(classTypeGroups.get(OperationElement.OPERAND))
+			.containsExactly("1", "2", "2");
+		assertThat(classTypeGroups.get(OperationElement.OPERATOR))
+			.containsExactly("*", "/");
+	}
+
+	@Test
+	@DisplayName("연산자 자리에 이상한 값이 들어갈 경우 IllegalArgumentException이 발생한다.")
+	void create_ThrowIllegalArgumentExceptionWhenWrongOperator() {
+
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringCalculator.create("1 * 2 3 4"))
+			.withMessage(Message.UNDEFINED_OPERATOR);
+	}
+
+	@Test
+	@DisplayName("연산자 자리에 이상한 값이 들어갈 경우 IllegalArgumentException이 발생한다.")
+	void create_ThrowIllegalArgumentExceptionWhenWrongOperand() {
+
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringCalculator.create("1 * 2 / +"))
+			.withMessage(Message.INVALID_STRING_FOR_OPERAND);
+	}
+
+	@Test
+	@DisplayName("정상적으로 StringCalculator를 생성한다.")
+	void createTest() {
+		StringCalculator stringCalculator = StringCalculator.create("1 * 2 / 2");
+
+		assertThat(stringCalculator).isInstanceOf(StringCalculator.class)
+			.isNotNull();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"2 + 3 * 4 / 2,10", "13 * 5 - 2 / 3,21"})
+	void calculateTest(String input, int excepted) {
+		assertThat(StringCalculator.create(input).calculate())
+			.isEqualTo(excepted);
+	}
+}
