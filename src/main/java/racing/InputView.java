44690af44e8728da428cb5617cc4a07a98d2d0a2
jiho <jhhj424@naver.com>
2020-12-08 01:50:37 +0900

    feat: 자동차 이름은 쉼표(,)를 기준으로 구분하여 입력 받는다

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -1,19 +1,27 @@
 package racing;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
+    private static final String CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String TRY_TIMES = "시도할 회수는 몇 회 인가요?";
+    private static final String COMMA = ",";
 
-    public static int numberOfCars() {
-        System.out.println(NUMBER_OF_CARS);
-        return SCANNER.nextInt();
+
+    public static List<String> carNamesMessage() {
+        System.out.println(CAR_NAMES_MESSAGE);
+        return inputSplit(SCANNER.nextLine());
     }
 
     public static int tryTimes() {
         System.out.println(TRY_TIMES);
         return SCANNER.nextInt();
+    }
+
+    public static List<String> inputSplit(String inputString) {
+        return Arrays.asList(inputString.split(COMMA));
     }
 }
--- src/test/java/racing/CarsTest.java
+++ src/test/java/racing/CarsTest.java
@@ -4,27 +4,35 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CarsTest {
+    private static final String COMMA = ",";
+    private final String TEST_CAR_NAME = "TEST_CAR";
+    private final int ALWAYS_TRUE_CONDITION = 9;
 
     @DisplayName("경기에 참가하는 자동차의 대수는 사용자가 정한다")
     @ParameterizedTest
-    @CsvSource(value = {"3, 3", "5, 5", "7, 7"})
-    public void numberOfCars(int count, int expected) {
+    @CsvSource(value = {"A,B,C: 3", "D,E: 2", "Q,W,E,R,T: 5"}, delimiter = ':')
+    public void numberOfCars(String carNamesInput, int expected) {
         //given
-        Cars cars = new Cars(count, new RandomCondition());
+        List<String> carNames = Arrays.asList(carNamesInput.split(COMMA));
+        Cars cars = new Cars(carNames, new RandomCondition());
 
         //then
         assertEquals(expected, cars.getCars().size());
+        assertEquals(carNames.size(), cars.getCars().size());
     }
 
     @DisplayName("여러대의 자동차가 이동 요구사항을 만족하면서 이동한다")
     @ParameterizedTest
-    @CsvSource(value = {"3, 3, 0", "5, 5, 1", "7, 7, 1"})
-    public void expectedMove(int count, int condition, int expectedPosition) {
+    @CsvSource(value = {"3, 0", "5, 1", "7, 1"})
+    public void expectedMove(int RandomCondition, int expectedPosition) {
         //given
-        Cars cars = new Cars(count, new FixedCondition(condition));
+        Cars cars = new Cars(Arrays.asList(TEST_CAR_NAME, TEST_CAR_NAME), new FixedCondition(RandomCondition));
 
         //when
         cars.move();
@@ -33,5 +41,17 @@ public class CarsTest {
         for (Car car : cars.getCars()) {
             assertEquals(expectedPosition, car.getPosition());
         }
+    }
+
+    @DisplayName("자동차의 이름은 쉼표(,)를 기준으로 구분한다")
+    @ParameterizedTest
+    @CsvSource(value = {"A,B,C:,:true", "D,E:#:false", "Q,W,E,R,T:,:true"}, delimiter = ':')
+    public void SeparateWithCommas(String carNamesInput, String Separator, boolean expected) {
+        // when
+        List<String> expectedCarNames = Arrays.asList(carNamesInput.split(Separator));
+        List<String> inputCarNames = InputView.inputSplit(carNamesInput);
+
+        //then
+        assertEquals(expected, expectedCarNames.containsAll(inputCarNames));
     }
 }
