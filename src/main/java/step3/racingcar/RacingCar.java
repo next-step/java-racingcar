371830ee301e83d4593ecf8bcc2a629dcd562e67
Mingyu Lee <leemingyu05@gmail.com>
2020-12-07 20:36:22 +0900

    feat: 값이 4 이상일 경우 자동차 전진

---
+++ src/main/java/step3/racingcar/RacingCar.java
@@ -0,0 +1,21 @@
+package step3.racingcar;
+
+public class RacingCar {
+	private int moveDistance;
+
+	public void move(final int value) {
+		if (value < 4) {
+			return;
+		}
+
+		this.increaseMoveDistance();
+	}
+
+	private void increaseMoveDistance() {
+		this.moveDistance += 1;
+	}
+
+	public int getMoveDistance() {
+		return this.moveDistance;
+	}
+}
---
+++ src/test/java/step3/racingcar/RacingCarTest.java
@@ -0,0 +1,21 @@
+package step3.racingcar;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+
+	@DisplayName("4 이상의 정수 값일 경우 자동차 움직임")
+	@Test
+	void given_integer_greater_or_equal_four_when_car_move_then_move_distance_increase() {
+		RacingCar car = new RacingCar();
+		car.move(4);
+		car.move(3);
+		car.move(1);
+		car.move(5);
+		assertThat(car.getMoveDistance()).isEqualTo(2);
+	}
+
+}
