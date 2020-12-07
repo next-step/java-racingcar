e69eb4a74a456b751dbd7960b4ca196e803f8102
Mingyu Lee <leemingyu05@gmail.com>
2020-12-07 21:14:58 +0900

    feat: 시도할 횟수 입력

--- src/main/java/step3/racingcar/ui/InputView.java
+++ src/main/java/step3/racingcar/ui/InputView.java
@@ -17,6 +17,13 @@ public class InputView {
 		return participateCarCount;
 	}
 
+	private int racingExecuteCountInput() {
+		System.out.println("시도할 회수는 몇 회 인가요?");
+		int racingExecuteCount = inputInteger();
+		validateGreaterThenZero(racingExecuteCount);
+		return racingExecuteCount;
+	}
+
 	private int inputInteger() {
 		try {
 			return Integer.parseInt(scanner.nextLine());
