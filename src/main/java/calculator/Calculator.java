75721bd25db1ce8bf329d6392362ace5970c2fa7
byungkyu-ju <noel1227@gmail.com>
2020-12-07 11:06:15 +0900

    refactor: 입력값 상수 체크 로직 개선 입력값 전체를 검증하고 계산프로세스를 진행하던 방식을 계산을 하면서 값을 체크하는 방식으로 변경함으로써 체크과정을 개선

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -16,11 +16,19 @@ public class Calculator {
 		checkUserInput(arg);
 		String[] splitValue = arg.split(" ");
 
-		result = Integer.parseInt(splitValue[0]);
+		result = stringParser(splitValue[0]);
 		for (int i = 1; i < splitValue.length; i += 2) {
-			result = Operator.of(splitValue[i]).operate(result, Integer.parseInt(splitValue[i + 1]));
+			result = Operator.of(splitValue[i]).operate(result, stringParser(splitValue[i + 1]));
 		}
 		return result;
+	}
+
+	private int stringParser(String s) {
+		try {
+			return Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("계산식이 옳바르지 않습니다. 숫자의 위치가 옳바르지 않습니다.");
+		}
 	}
 
 	private void checkUserInput(String arg) {
@@ -36,26 +44,10 @@ public class Calculator {
 	private void checkUserInputValidPattern(String arg) {
 		String[] splitValue = arg.split(" ");
 		checkUserInputValidPatternOnLength(splitValue);
-		checkUserInputValidPatternOnPosition(splitValue);
 	}
 
 	private void checkUserInputValidPatternOnLength(String[] splitValue) {
 		if (splitValue.length == 1 || splitValue.length % 2 == 0)
 			throw new IllegalArgumentException("계산식이 옳바르지 않습니다. 계산식이 없거나 계산식이 옳바르게 종료되지 않았습니다.");
 	}
-
-	private void checkUserInputValidPatternOnPosition(String[] splitValue) {
-		for (int i = 0; i < splitValue.length; i += 2) {
-			checkNumeric(splitValue[i]);
-		}
-	}
-
-	private void checkNumeric(String s) {
-		try {
-			int parseInt = Integer.parseInt(s);
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("계산식이 옳바르지 않습니다. 숫자의 위치가 옳바르지 않습니다.");
-		}
-	}
-
 }
