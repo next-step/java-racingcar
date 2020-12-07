50529ee936f7e5dac0ceacf405c469ca9219f8da
jiho <jhhj424@naver.com>
2020-12-08 02:19:57 +0900

    feat: 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다
    이동거리가 0 인 자동차는 출력 안함 -> 이후 이동거리가 0 을 넘어가면 이름 과 이동거리 출력

--- src/main/java/racing/ResultView.java
+++ src/main/java/racing/ResultView.java
@@ -3,18 +3,33 @@ package racing;
 public class ResultView {
     private static final String EXECUTION_RESULT = "실행 결과";
     private static final String CAR_SYMBOL = "-";
+    private static final String CAR_NAME_SEPARATOR = " : ";
 
     public static void printResultMessage() {
         System.out.println(EXECUTION_RESULT);
     }
 
     public static void printRacingStatus(Cars cars) {
-        cars.getCars().forEach(car -> {
-            for (int i = 0; i < car.getPosition(); i++) {
-                System.out.print(CAR_SYMBOL);
-            }
-            printLineBreak();
-        });
+        cars.getCars().forEach(ResultView::printCarStatus);
+    }
+    public static void printCarStatus(Car car) {
+        printCarName(car);
+        for (int i = 0; i < car.getPosition(); i++) {
+            printCarSymbol(car);
+        }
+        printLineBreak();
+    }
+
+    public static void printCarName(Car car) {
+        if (car.getPosition() > 0) {
+            System.out.print(car.carName() + CAR_NAME_SEPARATOR);
+        }
+    }
+
+    public static void printCarSymbol(Car car) {
+        if (car.getPosition() > 0) {
+            System.out.print(CAR_SYMBOL);
+        }
     }
 
     public static void printLineBreak() {
