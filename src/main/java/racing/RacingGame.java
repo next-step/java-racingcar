c892e3c2e30d026997a5bbf2eb30cc77f01bf260
tearight <tearightss@gmail.com>
2020-12-07 21:20:35 +0900

    feat(RacingGame): 차량 댓수를 입력받고 그 차량수 만큼 자동차 경주를 진행

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -4,16 +4,18 @@ public class RacingGame {
 	private final int turn;
 	private final RacingNotifier racingNotifier;
 
-	public RacingGame(int turn, RacingNotifier racingNotifier) {
+	public RacingGame(int carNum, int turn, RacingNotifier racingNotifier) {
+		this.carList = IntStream.range(0, carNum).mapToObj(value -> createDefaultCar()).collect(Collectors.toList());
 		this.turn = turn;
 		this.racingNotifier = racingNotifier;
 	}
 
 	public RacingStatus start() {
 		for (int i = 0; i < turn; i++) {
-			racingNotifier.notify(new RacingStatus());
+			carList.forEach(Car::getMoveForwardChance);
+			racingNotifier.notify(new RacingStatus(this.carList));
 		}
 
-		return new RacingStatus();
+		return new RacingStatus(this.carList);
 	}
 }
--- src/test/java/racing/RacingGameTest.java
+++ src/test/java/racing/RacingGameTest.java
@@ -9,13 +9,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RacingGameTest {
 
 	@ParameterizedTest
+	@ValueSource(ints = {0, 2, 5})
+	@DisplayName("차량 대수를 입력받고 그 차량 수 만큼 자동차 경주를 진행")
+	void start_carNum(int carNum) {
+		// given
+		RacingGame racingGame = new RacingGame(carNum, 5, racingStatus -> {});
+
+		// when
+		RacingStatus racingStatus = racingGame.start();
+
+		// then
+		assertThat(racingStatus.getCarList()).hasSize(carNum);
+	}
+
+	@ParameterizedTest
 	@ValueSource(ints = {0, 5, 9})
 	@DisplayName("매 진행이 끝날때마다 자동차 경주상황을 전달받아야 한다.")
 	void start_notifyGameStatus(int turn) {
 		// given
 		final NotifyCounter notifyCounter = new NotifyCounter();
 		RacingNotifier racingNotifier = racingStatus -> notifyCounter.addCount();
-		RacingGame racingGame = new RacingGame(turn, racingNotifier);
+		RacingGame racingGame = new RacingGame(0, turn, racingNotifier);
 
 		// when
 		racingGame.start();
