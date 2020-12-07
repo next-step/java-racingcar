3ef6357e2697bd9246f77899db24339dd0e424e3
ycc4477 <ycc4477@gmail.com>
2020-12-07 23:00:46 +0900

    feat: 뺄셈 기능 추가

--- src/test/java/study/calculator/OperatorTest.java
+++ src/test/java/study/calculator/OperatorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class OperatorTest {
 
 	@Test
-	@DisplayName("덧셈 연산자 테스트")
+	@DisplayName("덧셈 연산자 테스")
 	public void addition_test() throws Exception {
 		//Given
 		Addition addition = new Addition();
@@ -16,5 +16,16 @@ public class OperatorTest {
 		int calculate = addition.calculate(1, 0);
 		//Then
 		assertThat(calculate).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("뺄셈 연산자 테스트")
+	public void subtraction_test() throws Exception {
+		//Given
+		Subtraction subtraction = new Subtraction();
+		//When
+		int calculate = subtraction.calculate(1, 3);
+		//Then
+		assertThat(calculate).isEqualTo(-2);
 	}
 }
