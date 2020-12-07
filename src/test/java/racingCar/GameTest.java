24918370687bd581cf10298d755e8b9ca5aa9452
byungkyu-ju <noel1227@gmail.com>
2020-12-07 20:39:35 +0900

    feat: 자동차 수는 1보다 작을 수 없다.

--- src/test/java/racingCar/GameTest.java
+++ src/test/java/racingCar/GameTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -23,5 +24,16 @@ public class GameTest {
 	void userEnterCarCount(int carCount){
 		Game game = new Game(carCount);
 		assertThat(game.getCarCount()).isEqualTo(carCount);
+	}
+
+	@Order(2)
+	@DisplayName("1.1.자동차 수는 1보다 작을 수 없다.")
+	@Test
+	void validateUserEnterCarCount(){
+		int carCount = 0;
+		assertThatThrownBy(() -> {
+			Game game = new Game(carCount);
+		}).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("자동차 수는 1보다 작을 수 없습니다.");
 	}
 }
