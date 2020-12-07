30078e2dc326edc9c4ce553d1b3cc8d4dfbd5dc6
Donghwan Noh <namul.bap@gmail.com>
2020-12-07 20:21:09 +0900

    feat(game): 자동차가 한칸 전진함
    Car 클래스에 메소드 추가
    - getLocation : 자동차의 현재 위치를 가져올 수 있음. 최초 위치는 0.
    - tryToMove : 자동차의 이동을 시도할 수 있음. 시도가 성공하면 자동차의 위치가 1 증가함.

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -1,8 +1,19 @@
 package step3;
 
 public class Car {
+	private int location;
 
 	public boolean checkIfMove() {
 		return NumberUtil.generateRandomDigit() >= 4;
+	}
+
+	public void tryToMove() {
+		if (checkIfMove()) {
+			location++;
+		}
+	}
+
+	public int getLocation() {
+		return location;
 	}
 }
--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -3,6 +3,7 @@ package step3;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Car : 자동차의 상태, 이동로직 등을 각각 객체화하기 위한 클래스")
@@ -20,5 +21,26 @@ class CarTest {
 		assertThat(moveCount)
 			.isGreaterThan(55000)
 			.isLessThan(65000);
+	}
+
+	@DisplayName("getLocation : 자동차의 현재 위치를 가져올 수 있음. 최초 위치는 0.")
+	@Test
+	void getLocation() {
+		assertThat(new Car().getLocation()).isEqualTo(0);
+	}
+
+	@DisplayName("tryToMove : 자동차의 이동을 시도할 수 있음. 시도가 성공하면 자동차의 위치가 1 증가함.")
+	@RepeatedTest(value = 10)
+	void tryToMove() {
+		int moveTryCount = 5;
+		Car car = new Car();
+		for (int i = 0; i < moveTryCount; i++) {
+			car.tryToMove();
+		}
+		int result = car.getLocation();
+		System.out.printf("자동차의 이동시도횟수 : %d, 최초위치 : %d, 현재위치 : %d \n", moveTryCount, 0, result);
+		assertThat(result)
+			.isGreaterThanOrEqualTo(0)
+			.isLessThanOrEqualTo(moveTryCount);
 	}
 }
