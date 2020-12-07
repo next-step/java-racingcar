1d13cfd8c2f5e2b42c4484ceb7a88a26ebc0313a
HanYonggu <hyyg111@naver.com>
2020-12-07 20:56:29 +0900

    refactor : 자동차가 움직이는지 확인하는 테스트 리팩토링.

--- src/test/java/race/CarTest.java
+++ src/test/java/race/CarTest.java
@@ -1,18 +1,19 @@
 package race;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import race.domain.Car;
-import race.domain.RandomMovePolicy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
+    @DisplayName("자동차가 거리를 움직이는지 확인한다.")
     @Test
     void moveTest() {
-        Car car = new Car(new RandomMovePolicy());
+        Car car = new Car(() -> 1);
         car.move();
 
-        assertThat(car.getMovedDistance()).isBetween(0, 1);
+        assertThat(car.getMovedDistance()).isEqualTo(1);
     }
 }
