755940d9774df408829e566a475b315444c4ad2d
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-08 17:32:54 +0900

    (refactor) 자동차 이름 입력값 검증 수정

--- src/main/java/racingcar/RacingApp.java
+++ src/main/java/racingcar/RacingApp.java
@@ -5,12 +5,14 @@ import racingcar.view.InputValidator;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
+import java.util.List;
+
 public class RacingApp {
     public static void main(String[] args) {
-        int countOfRacingCar = InputValidator.validateInput(InputView.getCountOfRacingCar());
+        List<String> carNames = InputValidator.validateCarNames(InputView.getInputCarName());
         int countOfRacingRound = InputValidator.validateInput(InputView.getCountOfRacingRound());
 
-        RacingGame racingGame = new RacingGame(countOfRacingCar, countOfRacingRound);
+        RacingGame racingGame = new RacingGame(carNames, countOfRacingRound);
         racingGame.start();
 
         ResultView.displayRacing(racingGame.getRacingResult());
--- src/main/java/racingcar/view/InputValidator.java
+++ src/main/java/racingcar/view/InputValidator.java
@@ -1,16 +1,58 @@
 package racingcar.view;
 
+import racingcar.domain.RacingCar;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InputValidator {
-    public static final String INVALID_INPUT_ERROR_MESSAGE = "입력한 숫자는 0 보다 커야 합니다.";
+
+    public static final String INVALID_NAME_ERROR_MESSAGE = "자동차 이름은 숫자와 문자 조합이고 5자를 초과할 수 없습니다.";
+    public static final String INVALID_ROUND_ERROR_MESSAGE = "입력한 숫자는 0 보다 커야 합니다.";
+    private static final String NO_NAME_ERROR_MESSAGE = "자동차 이름 입력값이 없습니다.";
+    private static final String COMMA = ",";
+    private static final String BLANK = "";
+    private static final String SPACE = " ";
     public static final int MIN_VALUE = 1;
 
     private InputValidator() {
     }
 
+    public static List<String> validateCarNames(String inputCarName) {
+
+        List<String> carNames = convertList(inputCarName);
+
+        boolean isCorrectCarName = carNames.stream()
+                .allMatch(carName -> RacingCar.validateCarName(carName));
+
+        if(!isCorrectCarName) {
+            throw new IllegalArgumentException(INVALID_NAME_ERROR_MESSAGE);
+        }
+
+        return carNames;
+    }
+
+    private static List<String> convertList(String text) {
+        if (isEmpty(text)) {
+            throw new IllegalArgumentException(NO_NAME_ERROR_MESSAGE);
+        }
+        text = removeBlank(text.trim());
+        return Arrays.asList(text.split(COMMA));
+    }
+
+    private static String removeBlank(String input) {
+        return input.replace(SPACE, BLANK);
+    }
+
+    private static boolean isEmpty(String text) {
+        return null == text || text.isEmpty();
+    }
+
     public static int validateInput(int count) {
         if(count < MIN_VALUE) {
-            throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
+            throw new IllegalArgumentException(INVALID_ROUND_ERROR_MESSAGE);
         }
         return count;
     }
+
 }
