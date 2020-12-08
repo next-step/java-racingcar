bb048066aba0e0ad745fbef565765a3eb6b2f120
Do Yeon Kim <chikeem90@gmail.com>
2020-12-09 00:20:38 +0900

    test: 차 이동 테스트 추가

---
+++ src/test/java/carrace/CarTest.java
@@ -0,0 +1,28 @@
+package carrace;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CarTest {
+	Car car;
+
+	@BeforeEach
+	public void init() {
+		car = new Car();
+	}
+	
+	@ParameterizedTest
+	@CsvSource("{2:2, 3:3, 4:4}")
+	public void 차_이동_테스트(int step, int expectedResult) throws Exception {
+	    // given
+		// when
+		car.move(step);
+		// then
+		int actualResult = car.getPosition();
+	    assertThat(actualResult).isEqualTo(expectedResult);
+	}
+}
