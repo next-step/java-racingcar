ad9e63bff829dcca675da3337019394d051901d6
orgojy <giveawesome@gmail.com>
2020-12-09 02:24:42 +0900

    feat(CarRacingGame.java): 자동차 경주 게임을 구성하는 `CarRacingGame` 구현
    - `InputConsole`와 `OutputConsole`를 활용하여 콘솔을 구성한다.
    - `Cars`를 초기화하여 자동차 경주 게임을 시작한다.

--- src/main/java/carracing/CarRacingGame.java
+++ src/main/java/carracing/CarRacingGame.java
@@ -2,10 +2,12 @@ package carracing;
 
 import carracing.console.InputConsole;
 import carracing.console.OutputConsole;
-import carracing.domain.Cars;
-import carracing.domain.MovementPolicy;
-import carracing.domain.RandomIntMovementPolicy;
+import carracing.domain.*;
 import carracing.util.RandomGenerator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 자동차 경주 게임을 구현한 클래스
@@ -15,14 +17,24 @@ public class CarRacingGame {
     private final MovementPolicy movementPolicy = new RandomIntMovementPolicy(randomGenerator);
 
     public void run() {
-        int numberOfCars = InputConsole.readNumberOfCars();
+        CarNames carNames = new CarNames(convertCarNames(InputConsole.readCarNames()));
         int numberOfTrials = InputConsole.readNumberOfTrials();
-
         OutputConsole.printResultMessage();
-        Cars cars = new Cars(numberOfCars);
+        initiate(carNames, numberOfTrials);
+    }
+
+    private void initiate(CarNames carNames, int numberOfTrials) {
+        Cars cars = new Cars(carNames);
         for (int i = 0; i < numberOfTrials; i++) {
             cars.moveCars(movementPolicy);
             OutputConsole.printStatusOfRacing(cars);
         }
+        OutputConsole.printWinningCarsOfRacing(cars);
+    }
+
+    private List<CarName> convertCarNames(String carNames) {
+        return Arrays.stream(carNames.split(","))
+                .map(CarName::new)
+                .collect(Collectors.toList());
     }
 }
