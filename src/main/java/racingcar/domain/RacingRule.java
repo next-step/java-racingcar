a2825639b66e559faae9ddb00181230d78011806
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-08 14:48:23 +0900

    (refactor) 지난 피드백 반영 (도메인 요구사항이 한 눈에 들어날 수 있게 변수 분리 및 수정)

--- src/main/java/racingcar/domain/RacingRule.java
+++ src/main/java/racingcar/domain/RacingRule.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class RacingRule implements MoveRule {
     private static final int MOVE_CONDITION = 4;
-    private static final int RANDOM_NUMBER_RANGE = 10;
+    private static final int RANDOM_NUMBER_RANGE = 9;
 
     private Random random = new Random();
 
     @Override
     public boolean checkMovement() {
-        return MOVE_CONDITION <= random.nextInt(RANDOM_NUMBER_RANGE);
+        int randomNumberRange = RANDOM_NUMBER_RANGE + 1;
+        return MOVE_CONDITION <= random.nextInt(randomNumberRange);
     }
 }
