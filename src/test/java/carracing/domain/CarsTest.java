9679b11c68adaa41ea64c6c87f1cebe0e71f5ecd
orgojy <giveawesome@gmail.com>
2020-12-09 02:15:45 +0900

    feat: 자동차 경주를 같이하는 자동차 그룹 `Cars` 구현
    - `Cars`를 초기화하기 위한 이름 `CarNames`를 전달 받아 `Cars`의 `Car`를 초기화한다.
    - 전달 받은 `CarNames`는 비어있지 않아야한다.
    - 경주에서 우승한 `Car` 또는 `Car`들을 검색이 가능하다.

--- src/test/java/carracing/domain/CarsTest.java
+++ src/test/java/carracing/domain/CarsTest.java
@@ -3,17 +3,19 @@ package carracing.domain;
 import carracing.util.RandomGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -27,6 +29,9 @@ class CarsTest {
     private static final int BASE_POSITION_NUMBER = RandomIntMovementPolicy.BASE_POSITION_NUMBER;
     private static final int MAX_POSITION_NUMBER = RandomIntMovementPolicy.MAX_POSITION_NUMBER;
     private static final int MIN_POSITION_NUMBER = RandomIntMovementPolicy.MIN_POSITION_NUMBER;
+    private static final int MOVABLE_POSITION_NUMBER = BASE_POSITION_NUMBER;
+    private static final String TEST_CAR_NAMES = "pobi,crong,honux";
+    private static final String DELIMITER = ",";
 
     private RandomGenerator randomGenerator;
     private MovementPolicy movementPolicy;
@@ -37,22 +42,50 @@ class CarsTest {
         movementPolicy = new RandomIntMovementPolicy(randomGenerator);
     }
 
-    @DisplayName("자동차들의 생성여부 확인")
-    @Test
-    void createCarsTest() {
+    @DisplayName("조건에 맞는 자동차 이름을 전달했을 때, 자동차들의 생성여부 확인")
+    @ParameterizedTest
+    @MethodSource("provideValidCarNames")
+    void createCarsTest(CarNames carNames) {
         // When
-        Cars cars = new Cars(NUMBER_OF_CARS);
+        Cars cars = new Cars(carNames);
 
         // Then
         assertThat(cars.getCars().size()).isEqualTo(NUMBER_OF_CARS);
     }
 
+    @DisplayName("비어있는 자동차들의 이름을 전달했을 때, 자동차 생성 예외 발생 여부 확인")
+    @ParameterizedTest
+    @NullSource
+    void checkExceptionWithInvalidCarNamesTest(CarNames carNames) {
+        // When && Then
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new Cars(carNames)
+        );
+    }
+
+    @DisplayName("우승한 자동차 검색이 가능한지 확인")
+    @ParameterizedTest
+    @MethodSource("provideValidCarNames")
+    void retrieveWinningCarsTest(CarNames carNames) {
+        // Given
+        Cars cars = new Cars(carNames);
+
+        // When
+        when(randomGenerator.generateZeroOrPositiveNumber(MAX_POSITION_NUMBER))
+                .thenReturn(MOVABLE_POSITION_NUMBER);
+        cars.moveCars(movementPolicy);
+        String actual = convertWinningCarNames(cars);
+
+        // Then
+        assertThat(actual).isEqualTo(TEST_CAR_NAMES);
+    }
+
     @DisplayName("자동차들의 이동이 가능한 경우, 이동했을 때 실제 이동하였는지 확인")
     @ParameterizedTest
     @MethodSource("provideMovableCases")
-    void checkMovableCarsTest(int movableNumber) {
+    void checkMovableCarsTest(int movableNumber, CarNames carNames) {
         // Given
-        Cars cars = new Cars(NUMBER_OF_CARS);
+        Cars cars = new Cars(carNames);
 
         // When
         when(randomGenerator.generateZeroOrPositiveNumber(MAX_POSITION_NUMBER))
@@ -69,9 +102,9 @@ class CarsTest {
     @DisplayName("자동차들의 이동이 불가능 경우, 이동했을 때 실제 이동하였는지 확인")
     @ParameterizedTest
     @MethodSource("provideUnmovableCases")
-    void checkUnmovableCarsTest(int unmovableNumber) {
+    void checkUnmovableCarsTest(int unmovableNumber, CarNames carNames) {
         // Given
-        Cars cars = new Cars(NUMBER_OF_CARS);
+        Cars cars = new Cars(carNames);
 
         // When
         when(randomGenerator.generateZeroOrPositiveNumber(MAX_POSITION_NUMBER))
@@ -85,6 +118,14 @@ class CarsTest {
                 .containsOnly(INITIATION_POSITION_NUMBER);
     }
 
+    private String convertWinningCarNames(Cars cars) {
+        return cars.retrieveWinningCars()
+                .stream()
+                .map(Car::getName)
+                .map(CarName::getName)
+                .collect(Collectors.joining(DELIMITER));
+    }
+
     private List<Integer> convertPositionOfCars(Cars cars) {
         return cars.getCars()
                 .stream()
@@ -93,10 +134,24 @@ class CarsTest {
                 .collect(Collectors.toList());
     }
 
+    private static Stream<Arguments> provideValidCarNames() {
+        List<Arguments> arguments = new ArrayList<>();
+        arguments.add(Arguments.of(convertTestCarNames()));
+        return arguments.stream();
+    }
+
+    private static CarNames convertTestCarNames() {
+        String[] testCarNames = TEST_CAR_NAMES.split(DELIMITER);
+        List<CarName> carNames = Arrays.stream(testCarNames)
+                .map(CarName::new)
+                .collect(Collectors.toList());
+        return new CarNames(carNames);
+    }
+
     private static Stream<Arguments> provideMovableCases() {
         List<Arguments> arguments = new ArrayList<>();
         for (int i = BASE_POSITION_NUMBER; i <= MAX_POSITION_NUMBER; i++) {
-            arguments.add(Arguments.of(i));
+            arguments.add(Arguments.of(i, convertTestCarNames()));
         }
         return arguments.stream();
     }
@@ -104,7 +159,7 @@ class CarsTest {
     private static Stream<Arguments> provideUnmovableCases() {
         List<Arguments> arguments = new ArrayList<>();
         for (int i = MIN_POSITION_NUMBER; i < BASE_POSITION_NUMBER; i++) {
-            arguments.add(Arguments.of(i));
+            arguments.add(Arguments.of(i, convertTestCarNames()));
         }
         return arguments.stream();
     }
