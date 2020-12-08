d329f6ab072253f82b7f40c86b301c2c53db8fb6
charllossdev <charlloss.dev@gmail.com>
2020-12-08 11:25:18 +0900

    refactor(RacingCarController): 개발 소스 코드 depth 1 조건 준수 수정

--- src/main/java/racing/RacingController.java
+++ src/main/java/racing/RacingController.java
@@ -8,36 +8,37 @@ public class RacingController {
 
 	public static void main(String[] args) {
 
-		racingGameStart();
-	}
-
-	private static void racingGameStart() {
-
 		try {
 
-			// 유저: 주행할 차량 댓수 입력
-			int carRaceNumber = userInputPlayCarNumber();
-
-			// 유저: 레이싱 주행 횟수 입력
-			int tryRaceNumber = userInputPlayTryNumber();
-
-			// 경주 차량 객체 생성
-			RacingCars cars = new RacingCars(carRaceNumber);
-
-			// 입력한 주행 횟수 만큼 반복
-			for (int i = 0; i < tryRaceNumber; i++) {
-
-				// 레이싱 차량들 랜덤 주행 시작
-				cars.start();
-
-				// 주행 결과 출력
-				UserOutput.printCarLocation(cars.getRacingCars());
-			}
-
+			// 레이싱 시작
+			racingGameStart();
 		} catch (IllegalArgumentException illegalArgumentException) {
 
 			// 유저 입력 에서 시, 에러 결과 출력
 			UserOutput.printUserErrorMsg();
+		}
+
+	}
+
+	private static void racingGameStart() throws IllegalArgumentException {
+
+		// 유저: 주행할 차량 댓수 입력
+		int carRaceNumber = userInputPlayCarNumber();
+
+		// 유저: 레이싱 주행 횟수 입력
+		int tryRaceNumber = userInputPlayTryNumber();
+
+		// 경주 차량 객체 생성
+		RacingCars cars = new RacingCars(carRaceNumber);
+
+		// 입력한 주행 횟수 만큼 반복
+		for (int i = 0; i < tryRaceNumber; i++) {
+
+			// 레이싱 차량들 랜덤 주행 시작
+			cars.start();
+
+			// 주행 결과 출력
+			UserOutput.printCarLocation(cars.getRacingCars());
 		}
 	}
 
@@ -46,7 +47,7 @@ public class RacingController {
 	 * @return 주행할 차량 수
 	 * @throws IllegalArgumentException : 유저 입력 숫자가 아닐 시 에러 Throw
 	 */
-	private static int userInputPlayTryNumber() throws IllegalArgumentException {
+	private static int userInputPlayTryNumber() {
 
 		UserOutput.printUserInputTryNumberMsg();
 
@@ -58,7 +59,7 @@ public class RacingController {
 	 * @return 레이싱 주행 횟수
 	 * @throws IllegalArgumentException : 유저 입력 숫자가 아닐 시 에러 Throw
 	 */
-	private static int userInputPlayCarNumber() throws IllegalArgumentException {
+	private static int userInputPlayCarNumber() {
 
 		UserOutput.printUserInputCarNumberMsg();
 
