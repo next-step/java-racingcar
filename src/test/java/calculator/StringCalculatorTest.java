cd21d6ef887274390785acea6a4df2836835c250
nawhew <we_hwan@naver.com>
2020-12-08 15:39:09 +0900

    refactor: simplify test code

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringCalculatorTest {
 
@@ -59,13 +58,8 @@ public class StringCalculatorTest {
     @ValueSource(strings = {"", "1 % 2", "a * 3", "123+10"})
     @DisplayName("요구사항에 위배되는 값을 입력하여 오류가 발생하는지 테스트")
     public void wrongDataOccurredException(String input) {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            try {
-                new StringCalculator(InputView.splitInput(input)).calculate();
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-                throw e;
-            }
+        assertThatThrownBy(() -> {
+            new StringCalculator(InputView.splitInput(input)).calculate();
         });
     }
 
