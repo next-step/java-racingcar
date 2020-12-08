af9a83cb1418170633013348ae6979b560e7b2b5
orgojy <giveawesome@gmail.com>
2020-12-09 02:22:30 +0900

    feat: 자동차 경주 현황을 화면에 출력하는 콘솔 `OutputConsole` 구현
    - 각 회차 `Cars`의 각 `Car`의 `CarName`과 `CarPosition` 현황을 출력한다.

--- src/main/java/carracing/console/OutputConsole.java
+++ src/main/java/carracing/console/OutputConsole.java
@@ -1,8 +1,11 @@
 package carracing.console;
 
 import carracing.domain.Car;
+import carracing.domain.CarName;
 import carracing.domain.CarPosition;
 import carracing.domain.Cars;
+
+import java.util.stream.Collectors;
 
 /**
  * 자동차 경주 현황을 출력하기위한 콘솔
@@ -10,23 +13,34 @@ import carracing.domain.Cars;
 public class OutputConsole {
     private static final String RESULT_MESSAGE = "실행결과";
     private static final String POSITION_NOTATION = "-";
+    private static final String WINNER_RESULT_MESSAGE = "가 최종 우승했습니다.";
 
-    private OutputConsole() {
+    public static void printWinningCarsOfRacing(Cars cars) {
+        String winningCars = cars.retrieveWinningCars()
+                .stream()
+                .map(Car::getName)
+                .map(CarName::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println(winningCars + WINNER_RESULT_MESSAGE);
     }
 
     public static void printStatusOfRacing(Cars cars) {
         cars.getCars()
-                .stream()
-                .map(Car::getPosition)
-                .forEach(carPosition -> System.out.println(convertStatusOfCar(carPosition)));
+                .forEach(car -> System.out.println(convertStatusOfCar(car)));
         System.out.println();
+    }
+
+    private static String convertStatusOfCar(Car car) {
+        String nameOfCar = car.getName().getName();
+        String positionOfCar = convertPositionOfCar(car.getPosition());
+        return nameOfCar + ":" + positionOfCar;
     }
 
     public static void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
     }
 
-    private static String convertStatusOfCar(CarPosition carPosition) {
+    private static String convertPositionOfCar(CarPosition carPosition) {
         int positionNumber = carPosition.getNumber();
         StringBuilder positionNotation = new StringBuilder(positionNumber);
         for (int i = 0; i < positionNumber; i++) {
