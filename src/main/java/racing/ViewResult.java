4ea2518c8117ed766ee366346f0dc221d2377c6e
himj131@gmail.com <himj131@gmail.com>
2020-12-09 00:30:30 +0900

    feat: 자동차 경주 결과에 이름 출력

--- src/main/java/racing/ViewResult.java
+++ src/main/java/racing/ViewResult.java
@@ -1,8 +1,12 @@
 package racing;
 
 public class ViewResult {
+    public static final String DELIMITER_SEMICOLON = " : ";
+    private static final String MOVE_BAR = "-";
+
     public void view(Cars cars) {
         for (Car car : cars.getCars()) {
+            System.out.print(car.getName() + DELIMITER_SEMICOLON);
             StringBuilder results = createResultMessage(car);
             System.out.println(results);
         }
@@ -12,7 +16,7 @@ public class ViewResult {
     private StringBuilder createResultMessage(Car car) {
         StringBuilder results = new StringBuilder("");
         for (int i = 0; i < car.getMoveCount(); i++) {
-            results.append("-");
+            results.append(MOVE_BAR);
         }
         return results;
     }
