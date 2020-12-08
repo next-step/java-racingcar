f4af8d0ba1c6a6cd94be4031aee14671f5899d94
Do Yeon Kim <chikeem90@gmail.com>
2020-12-08 19:52:12 +0900

    feat: 음수 처리 추가

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -2,18 +2,31 @@ package step2;
 
 import java.util.Arrays;
 
+import sun.rmi.runtime.Log;
+
 public class Calculator {
 	public long calculate(String expression) {
 		// 입력값 null이거나 빈 공백문자열인지 체크
 		validateInput(expression);
 		String[] expressionArray = expression.split(" ");
-		long result = Long.parseLong(expressionArray[0]);
+		long result = getLongNumber(expressionArray[0]);
 		for (int i = 1; i < expressionArray.length; i+=2) {
 			Operator operator = validateOperator(expressionArray[i]);
-			long second = Long.parseLong(expressionArray[i+1]);
+			long second = getLongNumber(expressionArray[i+1]);
 			result = operator.operation(result, second);
 		}
 		return result;
+	}
+
+	private long getLongNumber(String number) {
+		try {
+			if (number.startsWith("-")) {
+				return Long.parseLong(number.substring(1)) * -1;
+			}
+			return Long.parseLong(number);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("숫자가 아닙니다.");
+		}
 	}
 
 	private Operator validateOperator(String symbol) {
