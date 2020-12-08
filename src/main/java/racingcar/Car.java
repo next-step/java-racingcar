31baf8b68ca382d1615e128bc7fbe58d9c05d43e
chh <chh2644@gmail.com>
2020-12-08 22:00:31 +0900

    refactor : 메소드명 변경

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,20 +4,18 @@ public class Car {
     private static final int INITIAL_LOCATION = 1;
     private static final int MOVE_CONDITION = 4;
     int location;
-    int state;
 
     public Car() {
         this.location = INITIAL_LOCATION;
     }
 
-    public int getLocation() {
-        return this.location;
+    public void setLocation(int randomNumber) {
+        if(randomNumber >= MOVE_CONDITION) {
+            this.location += 1;
+        }
     }
 
-    public int getLocation(int randomNumber) {
-        if(randomNumber >= MOVE_CONDITION) {
-            return this.location += 1;
-        }
+    public int getLocation() {
         return this.location;
     }
 }
