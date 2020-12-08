e662b14b62efa29ede2d98e7a2458e6822f74354
HanYonggu <hyyg111@naver.com>
2020-12-08 18:35:42 +0900

    refactor : 자동차가 움직이는지 확인하는 테스트 ParameterizedTest 변경 리팩토링.

--- src/test/java/race/CarTest.java
+++ src/test/java/race/CarTest.java
@@ -1,19 +1,21 @@
 package race;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import race.domain.Car;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
-    @DisplayName("자동차가 거리를 움직이는지 확인한다.")
-    @Test
-    void moveTest() {
-        Car car = new Car(() -> 1);
+    @DisplayName("자동차가 해당 거리만큼 움직이는지 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2})
+    void moveTest(int moveDistance) {
+        Car car = new Car(() -> moveDistance);
         car.move();
 
-        assertThat(car.getMovedDistance()).isEqualTo(1);
+        assertThat(car.getMovedDistance()).isEqualTo(moveDistance);
     }
 }
