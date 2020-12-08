1e671fe05c01ed0c32dc3c32cfaed32a9fba44d1
Do Yeon Kim <chikeem90@gmail.com>
2020-12-09 01:20:38 +0900

    feat: UI 로직 분리

---
+++ src/main/java/carrace/ResultView.java
@@ -0,0 +1,19 @@
+package carrace;
+
+public class ResultView {
+	public void printResultText() {
+		System.out.println("실행 결과");
+	}
+
+	public void printCarPosition(int position) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < position; i++) {
+			sb.append("-");
+		}
+		System.out.println(sb.toString());
+	}
+
+	public void nextLine() {
+		System.out.println();
+	}
+}
