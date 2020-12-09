1195b3fbedc76321bd3a899de9afffaf895bb21d
Donghwan Noh <namul.bap@gmail.com>
2020-12-10 04:41:25 +0900

    feat(view): 플레이어로부터 쉼표로 구분된 최대 5글자의 자동차 이름들을 입력받음
    InputView 클래스에 다음 메소드 추가
    - inputNameOfCars: 플레이어로부터 문자열을 입력받아 유효성을 검증한 후 쉼표로 split하여 리턴.
    - isValidInput: 자동차 이름에 대한 입력 문자열의 유효성을 검증해, 올바르면 true를 리턴.

---
+++ src/test/java/edu/nextstep/racing/InputViewTest.java
@@ -0,0 +1,27 @@
+package edu.nextstep.racing;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputViewTest {
+
+	@DisplayName("isValidInput : 입력된 문자열이 쉼표로 구분되는 최대 다섯글자의 공백이 아닌 패턴을 만족하면 true를 리턴")
+	@ParameterizedTest
+	@ValueSource(strings = {"53fg", "가나a,2144,dh4", "가,나,abc", "!@#$%,^&*(),_+=|,<>/?',;:\""})
+	void isValidInput_ShouldTrue(String input) {
+		assertThat(InputView.isValidInput(input)).isTrue();
+	}
+
+	@DisplayName("isValidInput : 입력된 문자열이 쉼표로 구분되는 최대 다섯글자의 공백이 아닌 패턴을 만족하지 못하면 false를 리턴")
+	@ParameterizedTest
+	@NullAndEmptySource
+	@ValueSource(strings = {",", ",,", "d,gga33d,sdfe", ",,3357,", "abc-,,d"})
+	void isValidInput_ShouldFalse(String input) {
+		assertThat(InputView.isValidInput(input)).isFalse();
+	}
+
+}
