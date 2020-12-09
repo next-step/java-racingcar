ca4596d3824faedddd6e9ee52143c848c987c398
chh <chh2644@gmail.com>
2020-12-09 11:20:59 +0900

    refactor : 메소드명 변경
    - 메소드명 변경
    - 랜덤수 생성을 메소드로 이동

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,19 +3,18 @@ package racingcar;
 public class Car {
     private static final int INITIAL_LOCATION = 1;
     private static final int MOVE_CONDITION = 4;
-    int location;
+    private static final int MOVE = 1;
 
-    public Car() {
-        this.location = INITIAL_LOCATION;
-    }
-
-    public void setLocation(int randomNumber) {
-        if(randomNumber >= MOVE_CONDITION) {
-            this.location += 1;
-        }
-    }
+    private int location = INITIAL_LOCATION;
+    private final RandomGenerator generator = new RandomGenerator();
 
     public int getLocation() {
         return this.location;
+    }
+
+    public void move() {
+        if(generator.getRandom() >= MOVE_CONDITION) {
+            this.location += MOVE;
+        }
     }
 }
