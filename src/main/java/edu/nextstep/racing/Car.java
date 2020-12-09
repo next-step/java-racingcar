c75bf96ff6ab145aa1b06f9e712879d63f8c72f3
Donghwan Noh <namul.bap@gmail.com>
2020-12-09 23:04:20 +0900

    refactor: 리뷰내용 반영. 자동차 전진 로직 개선
    Car 클래스에 변수 및 생성자 추가
      - numberGenerator: 자동차 전진 여부 판단에 사용될 숫자 생성 함수형 인터페이스
      - 생성자: 생성자를 통해 nuberGenerator 할당가능. 미지정 시, default는 랜덤 생성.
    
    변경된 로직에 맞게 테스트 수정

--- src/main/java/edu/nextstep/racing/Car.java
+++ src/main/java/edu/nextstep/racing/Car.java
@@ -3,16 +3,26 @@ package edu.nextstep.racing;
 import java.util.function.Supplier;
 
 public class Car {
+	public static final int STOP_LIMIT_NUMBER = 4;
+	private Supplier<Integer> numberGenerator;
 	private int location;
 
-	public boolean checkIfMove() {
-		return NumberUtil.generateRandomDigit() >= 4;
+	public Car() {
+		this(NumberUtil::generateRandomDigit);
+	}
+
+	public Car(Supplier<Integer> numberGenerator) {
+		this.numberGenerator = numberGenerator;
 	}
 
 	public void tryToMove() {
 		if (checkIfMove()) {
 			location++;
 		}
+	}
+
+	public boolean checkIfMove() {
+		return numberGenerator.get() >= STOP_LIMIT_NUMBER;
 	}
 
 	public int getLocation() {
--- src/test/java/edu/nextstep/racing/CarTest.java
+++ src/test/java/edu/nextstep/racing/CarTest.java
@@ -3,24 +3,19 @@ package edu.nextstep.racing;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("Car : 자동차의 상태, 이동로직 등을 각각 객체화하기 위한 클래스")
 class CarTest {
 
-	@DisplayName("checkIfMove : 자동차가 움직일 수 있으면 true, 없으면 false를 반환함. true의 비율은 약 60%.")
-	@Test
-	void checkIfMove() {
-		int totalCount = 100000;
-		int moveCount = 0;
-		Car car = new Car();
-		for (int i = 0; i < totalCount; i++) {
-			moveCount += car.checkIfMove() ? 1 : 0;
-		}
-		assertThat(moveCount)
-			.isGreaterThan(55000)
-			.isLessThan(65000);
+	@DisplayName("checkIfMove : Car객체에 정의된 numberGenerator가 생성한 값이 STOP_LIMIT_NUMBER인 4 이상이면 true 리턴.")
+	@ParameterizedTest
+	@CsvSource(value = {"3, false", "4, true", "5, true"})
+	void checkIfMove(int number, boolean result) {
+		Car car = new Car(() -> number);
+		assertThat(car.checkIfMove()).isEqualTo(result);
 	}
 
 	@DisplayName("getLocation : 자동차의 현재 위치를 가져올 수 있음. 최초 위치는 0.")
@@ -30,17 +25,11 @@ class CarTest {
 	}
 
 	@DisplayName("tryToMove : 자동차의 이동을 시도할 수 있음. 시도가 성공하면 자동차의 위치가 1 증가함.")
-	@RepeatedTest(value = 10)
-	void tryToMove() {
-		int moveTryCount = 5;
-		Car car = new Car();
-		for (int i = 0; i < moveTryCount; i++) {
-			car.tryToMove();
-		}
-		int result = car.getLocation();
-		System.out.printf("자동차의 이동시도횟수 : %d, 최초위치 : %d, 현재위치 : %d \n", moveTryCount, 0, result);
-		assertThat(result)
-			.isGreaterThanOrEqualTo(0)
-			.isLessThanOrEqualTo(moveTryCount);
+	@ParameterizedTest
+	@CsvSource(value = {"3, 0", "4, 1", "5, 1"})
+	void tryToMove(int number, int result) {
+		Car car = new Car(() -> number);
+		car.tryToMove();
+		assertThat(car.getLocation()).isEqualTo(result);
 	}
 }
