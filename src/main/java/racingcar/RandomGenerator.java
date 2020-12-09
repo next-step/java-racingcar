ad241c3cb676e6dce0cfba4320bbb3621b41bed1
chh <chh2644@gmail.com>
2020-12-10 00:15:09 +0900

    refactor : 변수 접근 제어자 추가

--- src/main/java/racingcar/RandomGenerator.java
+++ src/main/java/racingcar/RandomGenerator.java
@@ -3,7 +3,7 @@ package racingcar;
 import java.util.Random;
 
 public class RandomGenerator {
-    private static final int RANDOM_MAX = 10;
+    private final int RANDOM_MAX = 10;
     private final Random random = new Random();
 
     public int getRandom() {
