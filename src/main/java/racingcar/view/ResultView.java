72ed2805ff1d65b54cac9d5cffa013774621aa0e
chh <chh2644@gmail.com>
2020-12-10 00:16:01 +0900

    refactor : 비즈니스 로직 분리

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,39 +1,22 @@
 package racingcar.view;
 
-import racingcar.Car;
-import racingcar.Cars;
-
-import java.util.List;
+import racingcar.Result;
 
 public class ResultView {
     private static final String MOVE_COUNT_VIEW = "-";
-    private List<Car> carList;
 
-    public void resultTitle() {
-        System.out.println("\n실행결과");
-    }
-
-    public void resultRacing(int playCount, int numberOfCar) {
-        resultTitle();
-
-        carList = Cars.ready(numberOfCar);
-        for (int i = 0; i < playCount; i++) {
-            positionOfCar(carList);
-        }
-    }
-
-    public void positionOfCar(List<Car> cars) {
-        for (Car car : cars) {
-            locationCarView(car);
+    public static void print(Result result) {
+        for (int i = 0; i < result.getCarLocation().size(); i++) {
+            makeLocationView(result, i);
         }
         System.out.println();
     }
 
-    public void locationCarView(Car car) {
-        for (int i = 0; i < car.getLocation(); i++) {
-            System.out.print(MOVE_COUNT_VIEW);
+    private static void makeLocationView(Result result, int idx) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int j=0; j<result.getCarLocation().get(idx); j++) {
+            stringBuilder.append(MOVE_COUNT_VIEW);
         }
-        car.move();
-        System.out.println();
+        System.out.println(stringBuilder);
     }
 }
