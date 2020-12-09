fe75b8e332dfb70cd8d70b39b80c65c752ff792d
chh <chh2644@gmail.com>
2020-12-09 11:16:42 +0900

    refactor : 접근제어자 추가

--- src/main/java/racingcar/RandomGenerator.java
+++ src/main/java/racingcar/RandomGenerator.java
@@ -4,8 +4,7 @@ import java.util.Random;
 
 public class RandomGenerator {
     private static final int RANDOM_MAX = 10;
-
-    Random random = new Random();
+    private final Random random = new Random();
 
     public int getRandom() {
         return random.nextInt(RANDOM_MAX);
