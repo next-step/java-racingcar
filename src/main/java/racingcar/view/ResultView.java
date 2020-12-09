ac3d46fad3958b06b7846ff40697a4023d8eee94
chh <chh2644@gmail.com>
2020-12-09 11:24:44 +0900

    refactor : 출력 로직, 비즈니스 로직 이동

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,26 +1,39 @@
 package racingcar.view;
 
 import racingcar.Car;
-import racingcar.RandomGenerator;
+import racingcar.Cars;
 
 import java.util.List;
 
 public class ResultView {
     private static final String MOVE_COUNT_VIEW = "-";
+    private List<Car> carList;
 
-    public void resultRacing(List<Car> cars) {
+    public void resultTitle() {
+        System.out.println("\n실행결과");
+    }
+
+    public void resultRacing(int playCount, int numberOfCar) {
+        resultTitle();
+
+        carList = Cars.ready(numberOfCar);
+        for (int i = 0; i < playCount; i++) {
+            positionOfCar(carList);
+        }
+    }
+
+    public void positionOfCar(List<Car> cars) {
         for (Car car : cars) {
-            moveCarView(car);
+            locationCarView(car);
         }
         System.out.println();
     }
 
-    public void moveCarView(Car car) {
-        RandomGenerator generator = new RandomGenerator();
+    public void locationCarView(Car car) {
         for (int i = 0; i < car.getLocation(); i++) {
             System.out.print(MOVE_COUNT_VIEW);
         }
-        car.setLocation(generator.getRandom());
+        car.move();
         System.out.println();
     }
 }
