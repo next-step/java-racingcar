6c7bf811f24d3ce937a44afb07e913b95df41c57
Donghwan Noh <namul.bap@gmail.com>
2020-12-10 06:12:20 +0900

    fix (bug fix): 확률적인 테스트를 확정적인 테스트로 수정함.
    버그사유 :
    ResultView의 viewCarLocation 메소드 테스트 시,
    각 자동차의 현재 위치를 출력하는 테스트를 확률적으로 수행했었음.
    자동차의 위치가 0일 때, 출력값.split(" : ")[1]에서 ArrayIndexOutOfBoundsException 발생.
    
    BREAKING CHANGE:
      - ResultViewTest에서 확률적인 부분을 없애고, 문자열을 확정적으로 비교하도록 수정.

--- src/test/java/edu/nextstep/racing/ResultViewTest.java
+++ src/test/java/edu/nextstep/racing/ResultViewTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,17 +12,22 @@ import org.junit.jupiter.api.Test;
 @DisplayName("ResultView : 실행 결과 표시 UI 로직을 담당하는 View 클래스")
 class ResultViewTest {
 
+	private final int ALWAYS_MOVE = 9;
+	private final int NEVER_MOVE = 1;
+
 	@DisplayName("viewCarLocation : 자동차의 이름과 자동차의 위치만큼의 '-'를, ' : '으로 붙여서 출력함.")
 	@Test
 	void viewCarLocation() {
 		List<Car> carList = new ArrayList<>();
-		Car car1 = new Car();
+		Car car1 = new Car(() -> ALWAYS_MOVE);
 		car1.setName("pobi");
 		carList.add(car1);
-		Car car2 = new Car();
+		Car car2 = new Car(() -> NEVER_MOVE);
 		car2.setName("crong");
 		carList.add(car2);
-		Car car3 = new Car();
+
+		AtomicInteger count = new AtomicInteger(2); // 두번만 이동함
+		Car car3 = new Car(() -> count.getAndDecrement() > 0 ? ALWAYS_MOVE : NEVER_MOVE);
 		car3.setName("honux");
 		carList.add(car3);
 
@@ -30,11 +36,9 @@ class ResultViewTest {
 			game.play();
 		}
 
-		assertThat(carList).allSatisfy(car -> {
-			String[] texts = ResultView.viewCarLocation(car).split(" : ");
-			assertThat(texts[0]).isEqualTo(car.getName());
-			assertThat(texts[1]).isSubstringOf("-----");
-			assertThat(texts[1].length()).isEqualTo(car.getLocation());
-		});
+		assertThat(ResultView.viewCarLocation(car1)).isEqualTo("pobi : -----");
+		assertThat(ResultView.viewCarLocation(car2)).isEqualTo("crong : ");
+		assertThat(ResultView.viewCarLocation(car3)).isEqualTo("honux : --");
+
 	}
 }
