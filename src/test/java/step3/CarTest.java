fa81c07ee4c0ad39c34e89cf358ceecf717e18a8
eversong0723 <eversong0723@gmail.com>
2020-12-09 19:18:45 +0900

    feat: 자동차 경주 게임 시작
    Read.md 수정
    Car 구현

---
+++ src/test/java/step3/CarTest.java
@@ -0,0 +1,31 @@
+package step3;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CarTest {
+
+    private static final String SEPARATOR = ",";
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "0" + SEPARATOR + " ",
+            "1" + SEPARATOR + "-",
+            "2" + SEPARATOR + "--",
+            "3" + SEPARATOR + "---",
+            "10" + SEPARATOR + "----------"
+    })
+    public void 차_이동_테스트(String input) {
+        int moveCount = Integer.parseInt(input.split(SEPARATOR)[0]);
+        String expected = input.split(SEPARATOR)[1];
+        Car car = new Car();
+        for (int i = 0; i < moveCount; i++) {
+            car.move();
+        }
+        assertThat(car.getWayResult()).isEqualTo(expected);
+    }
+
+
+}
