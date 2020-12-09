4d5edbf3a1ee60eceabcfbdca79c22ccadbfa7f1
chh <chh2644@gmail.com>
2020-12-09 11:27:45 +0900

    refactor : 메인메소드 로직 구현

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,8 +1,17 @@
 package racingcar;
 
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
 public class Racing {
     public static void main(String[] args) {
-        Race race = new Race();
-        race.play();
+        InputView inputView = new InputView();
+        inputView.input();
+
+        int numberOfCar = inputView.getNumber();
+        int playCount = inputView.getCount();
+
+        ResultView resultView = new ResultView();
+        resultView.resultRacing(playCount, numberOfCar);
     }
 }
