e4d292bda376fe8ca79b6430bd2fa485d8c480d5
Do Yeon Kim <chikeem90@gmail.com>
2020-12-10 17:11:16 +0900

    feat: 우승자 판단 로직 추가 및 carCount 대신 carNames를 받아서 처리하도록 변경

--- src/main/java/carracinggame/CarRacingGame.java
+++ src/main/java/carracinggame/CarRacingGame.java
@@ -2,12 +2,14 @@ package carracinggame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarRacingGame {
-	public void start(int carCount, int moveCount, ResultView resultView) {
+	public void start(String carNames, int moveCount, ResultView resultView) {
+		String[] carNamesArray = carNames.split(",");
 		List<Car> carList = new ArrayList<>();
-		for (int i = 0; i < carCount; i++) {
-			carList.add(new Car(new RandomMoveStrategy()));
+		for (String carName : carNamesArray) {
+			carList.add(new Car(carName, new RandomMoveStrategy()));
 		}
 
 		resultView.nextLine();
@@ -15,13 +17,31 @@ public class CarRacingGame {
 		for (int i = 0; i < moveCount; i++) {
 			startOneRound(carList, resultView);
 		}
+
+		resultView.printWinnerNames(getWinnerNames(carList));
 	}
 
 	public void startOneRound(List<Car> carList, ResultView resultView) {
 		for (Car car : carList) {
 			car.move();
-			resultView.printCarPosition(car.getPosition());
+			resultView.printCarInfo(car);
 		}
 		resultView.nextLine();
+	}
+
+	public String getWinnerNames(List<Car> carList) {
+		int maxPosition = getMaxPosition(carList);
+		return 	carList.stream()
+			.filter(car -> car.getPosition() == maxPosition)
+			.map(Car::getName)
+			.collect(Collectors.joining(","));
+	}
+
+	private int getMaxPosition(List<Car> carList) {
+		int maxPosition = 0;
+		for (Car car : carList) {
+			maxPosition = Math.max(maxPosition, car.getPosition());
+		}
+		return maxPosition;
 	}
 }
--- src/main/java/carracinggame/ResultView.java
+++ src/main/java/carracinggame/ResultView.java
@@ -13,7 +13,21 @@ public class ResultView {
 		System.out.println(sb.toString());
 	}
 
+	public void printCarInfo(Car car) {
+		int position = car.getPosition();
+		StringBuilder sb = new StringBuilder();
+		sb.append(car.getName()).append(" : ");
+		for (int i = 0; i < position; i++) {
+			sb.append("-");
+		}
+		System.out.println(sb.toString());
+	}
+
 	public void nextLine() {
 		System.out.println();
+	}
+
+	public void printWinnerNames(String winnerNames) {
+		System.out.println(winnerNames + "가 최종 우승했습니다.");
 	}
 }
