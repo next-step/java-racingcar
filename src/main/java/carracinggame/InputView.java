81912e38a485bd7e2766bf2ba12c65473707ca65
Do Yeon Kim <chikeem90@gmail.com>
2020-12-10 17:05:28 +0900

    feat: 자동차 이름을 입력받는 method 추가

--- src/main/java/carracinggame/InputView.java
+++ src/main/java/carracinggame/InputView.java
@@ -7,15 +7,16 @@ public class InputView {
 
 	public int printAndGetCarCount() {
 		System.out.println("자동차 대수는 몇 대 인가요?");
-		return getInputValue();
+		return sc.nextInt();
+	}
+
+	public String printAndGetCarName() {
+		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+		return sc.nextLine();
 	}
 
 	public int printAndGetMoveCount() {
 		System.out.println("시도할 회수는 몇 회 인가요?");
-		return getInputValue();
-	}
-
-	private int getInputValue() {
 		return sc.nextInt();
 	}
 }
