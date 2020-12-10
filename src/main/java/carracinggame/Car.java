bb042903dc11c64c278b61f4f6a5f2c74602ef2c
Do Yeon Kim <chikeem90@gmail.com>
2020-12-10 17:05:56 +0900

    refactor: Car name 필드 추가

--- src/main/java/carracinggame/Car.java
+++ src/main/java/carracinggame/Car.java
@@ -3,14 +3,21 @@ package carracinggame;
 public class Car {
 	private int position = 1;
 
+	private String name;
+
 	private final MoveStrategy moveStrategy;
 
-	public Car(MoveStrategy moveStrategy) {
+	public Car(String name, MoveStrategy moveStrategy) {
+		this.name = name;
 		this.moveStrategy = moveStrategy;
 	}
 
 	public int getPosition() {
 		return position;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public void move() {
