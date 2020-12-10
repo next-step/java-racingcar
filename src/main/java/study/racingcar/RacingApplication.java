4ee70f4fd4cc6ac827545f52cb6d90fe42c4af06
ycc4477 <ycc4477@gmail.com>
2020-12-10 16:26:36 +0900

    feat: 사용자 입력 기능
    feat: 자동차 전진 기능 추가

---
+++ src/main/java/study/racingcar/RacingApplication.java
@@ -0,0 +1,11 @@
+package study.racingcar;
+
+public class RacingApplication {
+
+	public static void main(String[] args) {
+		InputView inputView = new InputView();
+
+		inputView.inputRacingLab();
+		inputView.inputRacingParticipant();
+	}
+}
