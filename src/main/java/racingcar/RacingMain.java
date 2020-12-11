3ef1712b2d647e0fb8bd100e825d4ec3817c286b
chh <chh2644@gmail.com>
2020-12-11 14:42:11 +0900

    feat : 게임 시작 메소드 추가로 메인 메소드 수정

--- src/main/java/racingcar/RacingMain.java
+++ src/main/java/racingcar/RacingMain.java
@@ -1,20 +1,22 @@
 package racingcar;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.RacingGame;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
+import java.util.List;
+
 public class RacingMain {
+    private static Cars cars;
+
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        inputView.input();
+        int playCount = inputView.inputCountOfPlay();
+        int carCount = inputView.inputCountOfCar();
 
-        int count = inputView.getCount();
-        int number = inputView.getNumber();
-
-        RacingGame game = new RacingGame(count, number);
-        while(game.isNotEnd()) {
-            Result result = game.move();
-            ResultView.print(result);
-        }
+        RacingGame game = new RacingGame(playCount, carCount);
+        game.start();
     }
 }
