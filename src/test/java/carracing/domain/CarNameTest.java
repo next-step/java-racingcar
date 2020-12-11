d6cdcb9182b581fb258983fc3f9485d86a23d571
orgojy <giveawesome@gmail.com>
2020-12-11 13:02:27 +0900

    refactor(CarNamesTest.java): `CarName`의 유효한 생성 사이즈를 넘어가는 경우 테스트 수정

--- src/test/java/carracing/domain/CarNameTest.java
+++ src/test/java/carracing/domain/CarNameTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,7 +16,8 @@ class CarNameTest {
     private static final int MIN_NAME_SIZE = CarName.MIN_NAME_SIZE;
     private static final int MAX_NAME_SIZE = CarName.MAX_NAME_SIZE;
     private static final String TEST_NAME_LETTER = "T";
-    private static final int INVALID_NAME_SIZE = MAX_NAME_SIZE + 1;
+    private static final int INVALID_MAX_BOUND_NAME_SIZE = MAX_NAME_SIZE + 1;
+    private static final int INVALID_MIN_BOUND_NAME_SIZE = MIN_NAME_SIZE - 1;
 
     @DisplayName("유효한 자동차 이름 정보가 생성되는지 확인")
     @ParameterizedTest
@@ -43,7 +45,7 @@ class CarNameTest {
     }
 
     private static List<String> provideInvalidNames() {
-        return provideNames(INVALID_NAME_SIZE, INVALID_NAME_SIZE + 10);
+        return Arrays.asList(provideName(INVALID_MIN_BOUND_NAME_SIZE), provideName(INVALID_MAX_BOUND_NAME_SIZE));
     }
 
     private static List<String> provideNames(int startSize, int endSize) {
