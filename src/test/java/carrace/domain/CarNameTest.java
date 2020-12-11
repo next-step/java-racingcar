a263cfbc40558c597bccd6cd4dea2d7a783b1ff5
Pilhwan Kim <pilhwankim03@gmail.com>
2020-12-12 08:52:51 +0900

    docs: 테스트의 displayName 추가

--- src/test/java/carrace/domain/CarNameTest.java
+++ src/test/java/carrace/domain/CarNameTest.java
@@ -1,5 +1,6 @@
 package carrace.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,12 +8,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarNameTest {
     @Test
+    @DisplayName("자동차 이름 출력")
     void getResultTest() {
         CarName carName = new CarName("leon");
         assertThat(carName.getResult()).isEqualTo("leon : ");
     }
 
     @Test
+    @DisplayName("자동차 이름 길이 6자 제한 초과")
     void validationTest() {
         assertThatThrownBy(() -> {
             new CarName("leon15");
--- src/test/java/carrace/domain/PositionTest.java
+++ src/test/java/carrace/domain/PositionTest.java
@@ -1,5 +1,6 @@
 package carrace.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,12 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class PositionTest {
 
     @Test
+    @DisplayName("자동차 위치가 제대로 저장되는가")
     void getPosition() {
         Position position = new Position(5);
         assertThat(position.getPosition()).isEqualTo(5);
     }
 
     @Test
+    @DisplayName("자동차 위치 이동시 1칸 전진하는가")
     void forward() {
         Position position = new Position(5);
         position.forward();
@@ -21,6 +24,7 @@ class PositionTest {
     }
 
     @Test
+    @DisplayName("자동차 위치 출력 테스트")
     void getResult() {
         Position position = new Position(5);
         assertThat(position.getResult()).isEqualTo("-----");
