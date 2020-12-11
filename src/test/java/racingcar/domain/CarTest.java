1918e45b100d3e053b2c20a12b59fe3aa05c8ae9
chh <chh2644@gmail.com>
2020-12-11 14:27:40 +0900

    test : 랜덤 수에 따른 이동, 정지 테스트

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import racingcar.utils.RandomGenerator;
+
+class CarTest {
+
+    @Test
+    void move() {
+        Car car = new Car();
+        RandomGenerator randomGenerator = new RandomGenerator() {
+            @Override
+            public boolean isMovable() {
+                return true;
+            }
+        };
+        car.move();
+        assertThat(car.getLocation()).isEqualTo(2);
+    }
+
+    @Test
+    void stop() {
+        Car car = new Car();
+        RandomGenerator randomGenerator = new RandomGenerator() {
+            @Override
+            public boolean isMovable() {
+                return false;
+            }
+        };
+        car.move();
+        assertThat(car.getLocation()).isEqualTo(1);
+    }
+}
