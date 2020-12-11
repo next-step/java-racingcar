27d0a723aa736e90d240ce40f131ab43b81ae603
Sang Uk Ahn <suahnn@gmail.com>
2020-12-11 21:50:00 +0900

    feat: main 실행 로직 구현

---
+++ src/main/java/racing/CarRacingMain.java
@@ -0,0 +1,18 @@
+package racing;
+
+import static racing.view.CarRacingInputView.countOfCars;
+import static racing.view.CarRacingInputView.countOfRace;
+import static racing.view.CarRacingResultView.*;
+
+public class CarRacingMain {
+    public static void main(String[] args) {
+        Cars cars = new Cars(countOfCars());
+        CarRacing carRacing = new CarRacing(cars, countOfRace());
+
+        printRacingResultHeader();
+        while (!carRacing.isComplete()) {
+            carRacing.race(printRacingResult());
+            printRaceFooter();
+        }
+    }
+}
