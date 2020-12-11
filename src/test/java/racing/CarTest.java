77a9087ca3c0bb9de3c7da9caf2161d9cee25454
Sang Uk Ahn <suahnn@gmail.com>
2020-12-11 14:50:41 +0900

    feat: 자동차 기본 클래스 생성, 이동 기능 구현 및 테스트

---
+++ src/test/java/racing/CarTest.java
@@ -0,0 +1,20 @@
+package racing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("자동차 전진")
+    void testCarMove() {
+        // given
+        Car car = new Car();
+        // when
+        car.move();
+        // then
+        assertThat(car.getMovedDistance()).isEqualTo(1);
+    }
+}
