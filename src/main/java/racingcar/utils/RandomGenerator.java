ac36f48ed74848a6d0b15c8ccb7b100825beba8e
chh <chh2644@gmail.com>
2020-12-11 14:31:23 +0900

    refactor : 패키지 변경 및 이동 여부 기능 구현

---
+++ src/main/java/racingcar/utils/RandomGenerator.java
@@ -0,0 +1,17 @@
+package racingcar.utils;
+
+import java.util.Random;
+
+public class RandomGenerator {
+    private static final int RANDOM_MAX = 10;
+    private static final int MOVE_CONDITION = 4;
+    private final Random random = new Random();
+
+    public int getRandom() {
+        return random.nextInt(RANDOM_MAX);
+    }
+
+    public boolean isMovable() {
+        return getRandom() >= MOVE_CONDITION;
+    }
+}
