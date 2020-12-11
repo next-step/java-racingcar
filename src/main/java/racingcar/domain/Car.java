7ec47680eb1c9ddae8342593615b307c248f2f4d
chh <chh2644@gmail.com>
2020-12-11 14:28:37 +0900

    refactor : Car 객체 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+import racingcar.utils.RandomGenerator;
+
+public class Car {
+    private static final int INITIAL_LOCATION = 1;
+    private int location;
+
+    private RandomGenerator randomGenerator = new RandomGenerator();
+
+    public Car() {
+        this(INITIAL_LOCATION);
+    }
+
+    public Car(int location) {
+        this.location = location;
+    }
+
+    public int getLocation() {
+        return this.location;
+    }
+
+    public void move() {
+        if(randomGenerator.isMovable()) {
+            this.location++;
+        }
+    }
+}
