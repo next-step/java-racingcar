12b6049894776f8f1952707bf606598e5e912a85
chh <chh2644@gmail.com>
2020-12-11 14:30:56 +0900

    refactor : 결과 UI 수정

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,22 +1,31 @@
 package racingcar.view;
 
-import racingcar.Result;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class ResultView {
     private static final String MOVE_COUNT_VIEW = "-";
 
-    public static void print(Result result) {
-        for (int i = 0; i < result.getCarLocation().size(); i++) {
-            makeLocationView(result, i);
-        }
+    public static void readyPrint(Cars cars) {
+        System.out.println("\n실행결과");
+
+        List<Car> carList = cars.getCars();
+        carList.forEach(car -> makeLocationView(car.getLocation()));
         System.out.println();
     }
 
-    private static void makeLocationView(Result result, int idx) {
+    public static void print(Cars cars) {
+        List<Car> carList = cars.getCars();
+        carList.forEach(car -> makeLocationView(car.getLocation()));
+        System.out.println();
+    }
+
+    private static void makeLocationView(int location) {
         StringBuilder stringBuilder = new StringBuilder();
-        for(int j=0; j<result.getCarLocation().get(idx); j++) {
-            stringBuilder.append(MOVE_COUNT_VIEW);
-        }
+        IntStream.range(0, location).forEach(i -> stringBuilder.append(MOVE_COUNT_VIEW));
         System.out.println(stringBuilder);
     }
 }
