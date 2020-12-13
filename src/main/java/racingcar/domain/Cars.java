b82a45ffab687e7b0685d503af95e7eda48a9e8d
chh <chh2644@gmail.com>
2020-12-13 23:37:04 +0900

    refactor : 변수명 변경

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -4,33 +4,32 @@ import racingcar.utils.RandomGenerator;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Cars {
     private static final int DEFAULT_VALUE = 0;
-    private final List<Car> carList = new ArrayList<>();
+    private final List<Car> participants = new ArrayList<>();
 
     public Cars(String[] carNameArr) {
-        Arrays.stream(carNameArr).map(Car::new).forEach(carList::add);
+        Arrays.stream(carNameArr).map(Car::new).forEach(participants::add);
     }
 
     public List<Car> getCars() {
-        return carList;
+        return participants;
     }
 
     public void move() {
-        carList.forEach(car -> car.move(new RandomGenerator()));
+        participants.forEach(car -> car.move(new RandomGenerator()));
     }
 
     public int getMaxLocation() {
-        return this.carList.stream()
+        return this.participants.stream()
                 .mapToInt(Car::getLocation)
                 .max()
                 .orElse(DEFAULT_VALUE);
     }
 
     public String getWinnerCarNames() {
-        return this.carList.stream()
+        return this.participants.stream()
                 .filter(car -> car.getLocation() == getMaxLocation())
                 .map(Car::getName)
                 .collect(Collectors.joining(", "));
