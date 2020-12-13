a3617a3d607c69d0ec4fc6852689da75f3140c37
Sang Uk Ahn <suahnn@gmail.com>
2020-12-13 20:35:41 +0900

    test: Car 테스트 케이스 추가

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -2,10 +2,31 @@ package racing.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarTest {
+
+    @Test
+    @DisplayName("자동차 생성")
+    void testCreateCar() {
+        // given
+        String name = "monds";
+        // when
+        Car car = new Car(name);
+        // then
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 5자 초과 시 예외")
+    void testCreateCarWithCarNameOver5Length() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car("mondss"))
+                .withMessageMatching("car name cannot exceed 5 characters: \\S+");
+    }
 
     @Test
     @DisplayName("자동차 전진")
@@ -16,5 +37,12 @@ public class CarTest {
         car.move();
         // then
         assertThat(car.getMovedDistance()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("자동차 이름이 null 또는 empty 일 경우 예외")
+    void testCarNameWithNullOrEmpty(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car(input));
     }
 }
