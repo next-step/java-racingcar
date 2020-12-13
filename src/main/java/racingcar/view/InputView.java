c1135835f5b49a5eeed1a6982b2abb13918b4e1e
chh <chh2644@gmail.com>
2020-12-13 14:42:21 +0900

    feat : 입력 기능 구현
    - 자동차 이름 입력
    - 자동차 이름 입력 예외 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,22 +4,40 @@ import racingcar.domain.Car;
 
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class InputView {
+    private static final String SPLIT_TEXT = ",";
     private final Scanner scanner;
 
     public InputView() {
         this.scanner = new Scanner(System.in);
     }
 
-    public int inputCountOfCar() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
+    public String[] inputNameOfCar() {
+        out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String[] carName = scanner.nextLine().split(SPLIT_TEXT);
+
+        inputNameValid(carName);
+
+        return carName;
+    }
+
+    public int inputCountOfPlay() {
+        out.println("시도할 회수는 몇회인가요?");
 
         return scanner.nextInt();
     }
 
-    public int inputCountOfPlay() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
+    public void inputNameValid(String[] names) {
+        for (String carName : names) {
+            if (carName.trim().length() == 0) {
+                throw new IllegalArgumentException("이름을 입력해주세요.");
+            }
 
-        return scanner.nextInt();
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
+            }
+        }
     }
 }
