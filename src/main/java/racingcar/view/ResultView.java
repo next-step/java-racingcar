0d1ababe589522e51da5ca13e2bd99e29206ec4d
chh <chh2644@gmail.com>
2020-12-13 14:40:00 +0900

    feat : 최종 우승자 출력 기능 구현

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -6,22 +6,34 @@ import racingcar.domain.Cars;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import static java.lang.System.out;
+
 public class ResultView {
     private static final String MOVE_COUNT_VIEW = "-";
+    private static final int INITIAL_LOCATION = 1;
 
-    public static void readyPrint() {
-        System.out.println("\n실행결과");
+    public static void readyPrint(Cars cars) {
+        out.println("\n실행결과");
+
+        cars.getCars().forEach(car -> makeLocationView(car.getName(), INITIAL_LOCATION));
+        out.println();
     }
 
     public static void print(Cars cars) {
         List<Car> carList = cars.getCars();
-        carList.forEach(car -> makeLocationView(car.getLocation()));
-        System.out.println();
+        carList.forEach(car -> makeLocationView(car.getName(), car.getLocation()));
+        out.println();
     }
 
-    private static void makeLocationView(int location) {
+    private static void makeLocationView(String carName, int location) {
         StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(carName).append(" : ");
         IntStream.range(0, location).forEach(i -> stringBuilder.append(MOVE_COUNT_VIEW));
-        System.out.println(stringBuilder);
+        out.println(stringBuilder);
+    }
+
+
+    public static void winnersPrint(Cars cars) {
+        out.println(cars.getWinnerCarNames() + "가 최종 우승했습니다.");
     }
 }
