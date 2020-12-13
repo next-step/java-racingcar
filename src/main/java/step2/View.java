888d2510914246239bf92fefc5312561768d0c05
Yeoseon Yun <devyyskr@gmail.com>
2020-12-14 08:56:02 +0900

    feat(View): View 구현

---
+++ src/main/java/step2/View.java
@@ -0,0 +1,17 @@
+package step2;
+
+import java.util.Scanner;
+
+public class View {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public String readExpression() {
+        System.out.println("수식을 입력하세요.");
+
+        return scanner.nextLine();
+    }
+
+    public void printResult(int result) {
+        System.out.println("결과: " + result);
+    }
+}
--- src/test/java/step2/OperatorTest.java
+++ src/test/java/step2/OperatorTest.java
@@ -3,7 +3,8 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class OperatorTest {
 
