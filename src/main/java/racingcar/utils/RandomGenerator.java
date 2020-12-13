3d6e000b26f3554f358ed57e10264b61be0db492
chh <chh2644@gmail.com>
2020-12-13 14:47:29 +0900

    feat : 랜덤수 발생기 전략패턴 기능 구현

--- src/main/java/racingcar/utils/RandomGenerator.java
+++ src/main/java/racingcar/utils/RandomGenerator.java
@@ -1,17 +1,21 @@
 package racingcar.utils;
 
+import racingcar.domain.MoveStrategy;
+
 import java.util.Random;
 
-public class RandomGenerator {
+public class RandomGenerator implements MoveStrategy {
     private static final int RANDOM_MAX = 10;
     private static final int MOVE_CONDITION = 4;
+
     private final Random random = new Random();
 
     private int getRandom() {
         return random.nextInt(RANDOM_MAX);
     }
 
+    @Override
     public boolean isMovable() {
-        return getRandom() <= MOVE_CONDITION;
+        return getRandom() >= MOVE_CONDITION;
     }
 }
