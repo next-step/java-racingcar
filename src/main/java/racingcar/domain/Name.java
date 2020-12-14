2eb5923e9e3164cc41f9d4c2a49425e5edb98e9b
chh <chh2644@gmail.com>
2020-12-15 00:30:29 +0900

    refactor : validation 삭제
    - inputView 클래스에서 이름 입력에 대한 밸리데이션을 체크하고 있으므로
      Name 클래스에서 밸리데이션 체크 삭제

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -7,23 +7,11 @@ public class Name {
     private final String name;
 
     public Name(String name) {
-        inputNameValid(name);
-
         this.name = name;
     }
 
     public String getNameValue() {
         return this.name;
-    }
-
-    public void inputNameValid(String name) {
-        if (name.trim().length() == 0) {
-            throw new IllegalArgumentException("이름을 입력해주세요.");
-        }
-
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
-        }
     }
 
     @Override
