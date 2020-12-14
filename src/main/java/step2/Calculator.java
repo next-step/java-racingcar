1421ad761b2c6347ec9cf2a02062b51694b05622
Yeoseon Yun <devyyskr@gmail.com>
2020-12-14 09:06:23 +0900

    feat(Calculator): 입력받은 수식을 Space를 기준으로 쪼개는 기능 추가

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -2,10 +2,16 @@ package step2;
 
 public class Calculator {
     public static final String NULL_OR_EMPTY_STRING_EXCEPTION_MESSAGE = "빈 값이 입력되었습니다.";
+    public static final String SPACE = " ";
 
     public static void assertNullOrEmpty(String string) {
         if(Utils.isNullOrEmpty(string)) {
             throw new IllegalArgumentException(NULL_OR_EMPTY_STRING_EXCEPTION_MESSAGE);
         }
+    }
+
+    public static String[] splitExpression(String expression) {
+
+        return expression.split(SPACE);
     }
 }
--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -3,6 +3,7 @@ package step2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static step2.Calculator.NULL_OR_EMPTY_STRING_EXCEPTION_MESSAGE;
 
@@ -22,5 +23,13 @@ public class CalculatorTest {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             Calculator.assertNullOrEmpty("");
         }).withMessage(NULL_OR_EMPTY_STRING_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("입력받은 수식이 공백을 기준으로 잘 쪼개지는지 테스트한다.")
+    void splitExpression() {
+        String expression = "2 + 3 * 4 / 2";
+
+        assertThat(Calculator.splitExpression(expression)).containsExactly("2", "+", "3", "*", "4", "/", "2");
     }
 }
