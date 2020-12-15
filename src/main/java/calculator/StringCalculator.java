349eab20e56a408e023be8c896e0c400f1898a71
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-12-15 14:13:17 +0900

    test, refactor (compile, test) - 리뷰어 피드백 반영 및 리팩토
    세부사항 :
    나눗셈 0으로 나눌때 예외처리
    빈 공백 문자열 상수로 변경
    접근제어자 변경

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 public class StringCalculator {
 
+    public static final String BLANK_SPACE = " ";
+
+
     public int calculation(String input) {
         nullAndEmptyCheck(input);
-        List<String> inputArray = Arrays.asList(input.split(" "));
+        List<String> inputArray = Arrays.asList(input.split(BLANK_SPACE));
         int result = parseInt(inputArray.get(0));
 
-        for(int i=1; i < inputArray.size(); i += 2) {
+        for(int i= 1; i < inputArray.size(); i += 2) {
             result = Operation.arithmeticOperationCheck(inputArray.get(i)).operation(result, parseInt(inputArray.get(i+1)));
         }
         return result;
@@ -20,7 +23,7 @@ public class StringCalculator {
         return Integer.parseInt(input);
     }
 
-    void nullAndEmptyCheck(String input) {
+    protected void nullAndEmptyCheck(String input) {
         if(input == null || input.isEmpty())
             Exception.IllegalArgumentException("null 또는 빈값을 허용하지 않습니다.");
     }
