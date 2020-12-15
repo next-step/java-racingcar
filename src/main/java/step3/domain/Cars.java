1a58f04ccb5b7ce055f0646ce84d1a36facea25b
Yeoseon Yun <devyyskr@gmail.com>
2020-12-15 16:35:00 +0900

    refactor(Cars): depth 1로 줄이기 -> 일급콜렉션 Cars생성

---
+++ src/main/java/step3/domain/Cars.java
@@ -0,0 +1,22 @@
+package step3.domain;
+
+import step3.domain.rule.MoveRule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars = new ArrayList<>();
+
+    public Cars(int carCount) {
+        for(int i = 0; i < carCount; i++) {
+            cars.add(new Car());
+        }
+    }
+
+    public void moveAll(int roundTime, MoveRule moveRule) {
+        for(Car car : cars) {
+            car.move(roundTime, moveRule.canMove());
+        }
+    }
+}
