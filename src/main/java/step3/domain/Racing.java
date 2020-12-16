9b6638aa27248384499e419f6dcc08c2628c5339
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-12-16 14:54:24 +0900

    feat, test - 차량이동 루프 구현 및 테스트 코드

--- src/main/java/step3/domain/Racing.java
+++ src/main/java/step3/domain/Racing.java
@@ -4,15 +4,27 @@ import step3.domain.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Racing {
 
     private final List<Car> car = new ArrayList<>();
 
+    private int randomNumber() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
+
     protected List<Car> carRegistration(int count) {
         for(int i = 0; i<count; i++)
             car.add(new Car());
 
+        return car;
+    }
+
+
+    public List<Car> carsMove(List<Car> car) {
+        car.forEach(s -> s.move(randomNumber()));
         return car;
     }
 }
--- src/test/java/step3/domain/RacingTest.java
+++ src/test/java/step3/domain/RacingTest.java
@@ -1,19 +1,27 @@
 package step3.domain;
 
+import org.assertj.core.api.ListAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingTest {
 
     private Racing racing;
+    private List<Car> carList;
 
     @BeforeEach
     void setUp() {
         racing = new Racing();
+        carList = new ArrayList<>();
     }
 
     @ParameterizedTest
@@ -21,5 +29,16 @@ public class RacingTest {
     @DisplayName("차량 객체 생성 테스트 코드")
     void carRegistration(int count) {
         assertThat(racing.carRegistration(count)).size().isEqualTo(count);
+    }
+
+    @Test
+    @DisplayName("자동차 대수 만큼 전진 로직 루프 테스트 코드")
+    void carsMove() {
+        carList = Arrays.asList(new Car(), new Car(), new Car());
+        racing.carsMove(carList);
+        List<Integer> moveList = new ArrayList<>();
+        for(Car car : carList)
+            moveList.add(car.getPosition());
+        assertThat(moveList).containsOnly(0,1);
     }
 }
