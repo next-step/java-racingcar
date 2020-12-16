8f9f7b88769388309dc85ea73129dae73e55a340
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-12-16 15:36:50 +0900

    feat, test - 전진시도 횟수 만큼 전진하는 코드 및 테스트 코드

--- src/main/java/step3/domain/Racing.java
+++ src/main/java/step3/domain/Racing.java
@@ -1,14 +1,14 @@
 package step3.domain;
 
-import step3.domain.Car;
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class Racing {
 
     private final List<Car> car = new ArrayList<>();
+    public static int INPUT_NUMBER_OF_MOVE = 0;
 
     private int randomNumber() {
         Random random = new Random();
@@ -22,9 +22,14 @@ public class Racing {
         return car;
     }
 
-
     public List<Car> carsMove(List<Car> car) {
         car.forEach(s -> s.move(randomNumber()));
         return car;
+    }
+
+    public int numberOfCarMove(Car car) {
+        for(int i = 0; i< INPUT_NUMBER_OF_MOVE; i++)
+            carsMove(Collections.singletonList(car));
+        return car.getPosition();
     }
 }
--- src/test/java/step3/domain/RacingTest.java
+++ src/test/java/step3/domain/RacingTest.java
@@ -1,6 +1,5 @@
 package step3.domain;
 
-import org.assertj.core.api.ListAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +14,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingTest {
 
-    private Racing racing;
+    private Racing racing = new Racing() {
+
+    };
     private List<Car> carList;
 
     @BeforeEach
@@ -40,5 +41,19 @@ public class RacingTest {
         for(Car car : carList)
             moveList.add(car.getPosition());
         assertThat(moveList).containsAnyOf(0,1);
+    }
+
+    @Test
+    @DisplayName("전진 횟수 만큼 전진 로직 루프 테스트 코드")
+    void numberOfCarMove() {
+        Racing.INPUT_NUMBER_OF_MOVE = 5;
+        Car car = new Car() {
+            @Override
+            public void move(int randomNum) {
+                super.move(5);
+            }
+        };
+        assertThat(racing.numberOfCarMove(car)).isEqualTo(5);
+
     }
 }
