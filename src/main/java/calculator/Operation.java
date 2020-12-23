f6cfbb6f6cd7caf00c073bc24c97a5354e6b67cf
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-12-23 15:14:07 +0900

    feat, test, refactor - 자동차 이름 객체 생성 및 Exception Class util 패키지로 변경

--- src/main/java/calculator/Operation.java
+++ src/main/java/calculator/Operation.java
@@ -1,12 +1,7 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import java.util.function.IntBinaryOperator;
-
-import static calculator.Exception.*;
 
 public enum Operation {
     PLUS("+", Integer::sum),
