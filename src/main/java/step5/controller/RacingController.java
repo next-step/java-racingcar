3bbc5b693a87f5bf34835745f71605c2c4b84f43
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-12-26 14:33:05 +0900

    refactor - InputView 메세지 표기 상수로 변경

--- src/main/java/step5/controller/RacingController.java
+++ src/main/java/step5/controller/RacingController.java
@@ -3,13 +3,14 @@ package step5.controller;
 import step5.service.Racing;
 import step5.view.InputView;
 import step5.view.ResultView;
+import util.Message;
 
 public class RacingController {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
 
-        Racing racing = new Racing(inputView.inputString("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분 합니다."), inputView.inputInt("시도할 회수는 몇 회 인가요?"));
+        Racing racing = new Racing(inputView.inputString(Message.INPUT_CAR_NAME), inputView.inputInt(Message.INPUT_COUNT_OF_RACE));
 
         resultView.carPositionPrints(racing.getCarList());
         resultView.winnerNamePrints(racing.getWinners());
---
+++ src/main/java/util/Message.java
@@ -0,0 +1,6 @@
+package util;
+
+public class Message {
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분 합니다.";
+    public static final String INPUT_COUNT_OF_RACE = "시도할 회수는 몇 회 인가요?";
+}
