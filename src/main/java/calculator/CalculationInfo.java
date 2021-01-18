dbb26717e3ccdded4a9c5f593d5df2e096d7dcee
jeongdaeun98 <uneap@sk.com>
2021-01-18 20:56:33 +0900

    feat: create CaculationInfo

--- src/main/java/calculator/CalculationInfo.java
+++ src/main/java/calculator/CalculationInfo.java
@@ -6,6 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CalculationInfo {
+    private int dividend;
+    private int divisor;
+    private Operator operator;
+    private boolean resumeInputRequest;
 
     private int dividend;
     private int divisor;
