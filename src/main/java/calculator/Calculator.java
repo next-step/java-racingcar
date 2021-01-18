c0d967d1512672dd829091b7c2df7aaae4ff7a3c
KimBB <tree9295@gmail.com>
2021-01-19 00:32:35 +0900

    feat: Arithmethic operation(4 rules) calculation methods 구현
    - add : add two numbers
    - sub : subtract two numbers
    - mul : multiply two numbers
    - div : divide two numbers

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,25 @@
 package calculator;
 
 public class Calculator {
+    public Double add(Double num1, Double num2) {
+        return num1 + num2;
+    }
+
+    public Double sub(Double num1, Double num2) {
+        return num1 - num2;
+    }
+
+    public Double mul(Double num1, Double num2) {
+        return num1 * num2;
+    }
+
+    public Double div(Double num1, Double num2) throws ArithmeticException {
+        Double divResult = num1 / num2;
+        if (Double.isInfinite(divResult) || Double.isNaN(divResult)){
+            throw new ArithmeticException("0 또는 0.0 으로 나눌 수 없습니다.");
+        }
+        return num1 / num2;
+    }
+
 
 }
