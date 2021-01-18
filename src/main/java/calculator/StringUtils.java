443b6b6e00d56602e85fe5f30e7b0cf222c99f1d
jeongdaeun98 <uneap@sk.com>
2021-01-18 21:00:13 +0900

    feat: create splitInput in StringUtils

--- src/main/java/calculator/StringUtils.java
+++ src/main/java/calculator/StringUtils.java
@@ -1,5 +1,11 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class StringUtils {
-    
+
+    public static String[] splitInput(String input){
+        String[] inputArray = input.split(" ");
+        return inputArray;
+    }
 }
