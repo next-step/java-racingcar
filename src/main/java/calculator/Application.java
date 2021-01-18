b896d27c74661ff592e3b193e0985fd6045a3082
KimBB <tree9295@gmail.com>
2021-01-18 22:42:25 +0900

    feat: input method 생성
    - userInput: 유저로부터 입력받아 ArrayDeque 반환
    - stringToDeque: String을 Split하여 ArrayDeque 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
 
-public class Application {
-    public void start(){
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.List;
 
+public class Application {
+    public void start() {
+    }
+
+    public ArrayDeque<String> userInput() {
+        ArrayDeque<String> dequeInput = null;
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in))) {
+            String input = br.readLine();
+            dequeInput = stringToDeque(input);
+            return dequeInput;
+        } catch (IOException e) {
+            System.out.println("Input Array");
+            return dequeInput;
+        }
+    }
+
+    public ArrayDeque<String> stringToDeque(String input) {
+        List<String> stringList = Arrays.asList(input.split(" "));
+        return new ArrayDeque<>(stringList);
     }
 }
