a80c95acfa858d63cc6e41d96386b00e161ae903
d36choi <yuio1126@gmail.com>
2021-01-18 12:21:20 +0900

    feat: Calculator.java 생성 및 사칙연산 메소드 생성

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package calculator;
+
+public class Calculator {
+
+    public double add(int a, int b) {
+        return a + b;
+    }
+
+    public double substract(int a, int b) {
+        return  a - b;
+    }
+    public double multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        return  a / b;
+    }
+}
