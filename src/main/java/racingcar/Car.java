f0870cb84633b7e99cc5ee76853622b424515334
donghee youn <qeadzc9503@naver.com>
2021-01-19 22:19:33 +0900

    feat : Add asCarsList in Car
    Add
     - asCarsList : 차들의 이름 string을 받아 car객체 ArrayList를 반환

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,22 @@
 package racingcar;
 
+import java.util.ArrayList;
+
 public class Car {
     private String name;
     private int distance;
+
     public Car(String name) {
         this.name = name;
         this.distance = 0;
+    }
+
+    public static ArrayList<Car> asCarsList(String carNames) {
+        String[] names = carNames.split(",");
+        ArrayList<Car> carsList = new ArrayList<>();
+        for (String name : names) {
+            carsList.add(new Car(name));
+        }
+        return carsList;
     }
 }
