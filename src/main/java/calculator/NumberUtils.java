0decb45d92e50a9c74aa5b5a5e1156e1aaaca9dc
jeongdaeun98 <uneap@sk.com>
2021-01-19 17:57:08 +0900

    feat: create isInteger function

--- src/main/java/calculator/NumberUtils.java
+++ src/main/java/calculator/NumberUtils.java
@@ -5,9 +5,9 @@ public class NumberUtils {
     public static boolean isInteger(String str) {
         try {
             Integer.parseInt(str);
+            return true;
         } catch (NumberFormatException e) {
             return false;
         }
-        return true;
     }
 }
