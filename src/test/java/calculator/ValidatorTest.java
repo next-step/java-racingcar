0ff94d5dde6676b92f606b1b701f1fd0aeefa3e8
KimBB <tree9295@gmail.com>
2021-01-19 17:25:42 +0900

    feat : Creaete checkExpressionTest in ValidatorTest
    add
    - checkExpressionTest: 올바른 식인지 확인하는 checkExpression method 테스트

--- src/test/java/calculator/ValidatorTest.java
+++ src/test/java/calculator/ValidatorTest.java
@@ -2,7 +2,13 @@ package calculator;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -23,4 +29,32 @@ class ValidatorTest {
         assertThatThrownBy(() -> Validator.checkEmptyInput(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
+    // a static method that returns a Stream of Arguments
+    private static Stream<Arguments> provideArrayDequeForWrongExpression() { // argument source method
+        String[] expression = {"2", "+"};
+        ArrayDeque<String> expressionDeque = new ArrayDeque<>(Arrays.asList(expression));
+
+        String[] expression2 = {"3", "3", "/"};
+        ArrayDeque<String> expressionDeque2 = new ArrayDeque<>(Arrays.asList(expression2));
+
+        String[] expression3 = {"5", "*", "-"};
+        ArrayDeque<String> expressionDeque3 = new ArrayDeque<>(Arrays.asList(expression3));
+
+        String[] expression4 = {"5", ".", "7"};
+        ArrayDeque<String> expressionDeque4 = new ArrayDeque<>(Arrays.asList(expression4));
+
+        return Stream.of(
+                Arguments.of(expressionDeque),
+                Arguments.of(expressionDeque2),
+                Arguments.of(expressionDeque3),
+                Arguments.of(expressionDeque4)
+        );
+    }
+
+    @DisplayName("checkExpressionTest: 식 확인")
+    @ParameterizedTest
+    @MethodSource("provideArrayDequeForWrongExpression")
+    void checkExpressionTest(ArrayDeque<String> expression) {
+        assertThatThrownBy(() -> Validator.checkExpression(expression)).isInstanceOf(IllegalArgumentException.class);
+    }
 }
