186e45b3fe5bc1f8cc72c9cb7422b846a0ed1417
donghee youn <qeadzc9503@naver.com>
2021-01-19 22:11:53 +0900

    feat : Add class Car
    Add
     - String name
     - int distance
     - constructor

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int distance;
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+}
