7a46e6093212937634d7982ec9fabf87b3c0f60b
d36choi <yuio1126@gmail.com>
2021-01-19 21:02:20 +0900

    fix: 메인함수의 계산기 진입함수를 getUserInput() 에서 start()로 수정함.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        calculator.getUserInput();
+        calculator.start();
     }
 }
