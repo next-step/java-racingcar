84cdcfdfc6671f8e61fe4deab608164a24287a3e
donghee youn <qeadzc9503@naver.com>
2021-01-19 22:21:43 +0900

    fix : change variable name
    change
     - String carsList to carNames in Main

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -2,8 +2,8 @@ package racingcar;
 
 public class Main {
     public static void main(String[] args) {
-        String carsList = InputOutput.inputCarsName();
-        if (carsList != null) {
+        String carNames = InputOutput.inputCarsName();
+        if (carNames != null) {
         }
     }
 }
