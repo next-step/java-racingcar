55a3d9485ac96de29b01463c7c642e8a29e701c3
d36choi <yuio1126@gmail.com>
2021-01-19 21:32:42 +0900

    feat: 연산자 에러테스트 테스트코드를 작성하였음.

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,18 +4,34 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+
 public class CalculatorTest {
-    Calculator calculator;
+    private static Calculator calculator;
+    private static final String errMsg = "계산식을 입력해주세요.\n올바른 계산식을 입력해주세요.\n";
     @BeforeAll
-    void initAll () {
+    static void initAll() {
         calculator = new Calculator();
     }
 
     @Test
     void getUserInput() {
-        calculator.getUserInput();
+        calculator.start();
         String[] testStrings = {"1", "+", "2"};
 //        Assertions.assertThat(calculator.inputStrings).containsAll(testStrings);
+    }
+
+    @Test
+    void 연산자_에러테스트() {
+
+        String input = "1 + 2 k 3";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        calculator.start();
+        Assertions.assertThat(errMsg).isEqualTo(out.toString());
+
     }
 
 }
