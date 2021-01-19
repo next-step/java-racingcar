b9fb6cf21379f61b06f477d1f7c726915040912e
hakzzang <mbh930312@naver.com>
2021-01-19 22:36:43 +0900

    feat : RacingCar go 메소드 구현

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,21 +1,28 @@
 package racingcar;
 
+import java.util.Random;
+
 public class RacingCar implements Car {
-    RacingCar(String name) {
+    private int position = 0;
+    private String name;
+
+    public RacingCar(String name) {
         this.name = name;
     }
-    private int position;
-    private String name;
+
     @Override
     public void go() {
-
+        Random rand = new Random();
+        int score = rand.nextInt(10);
+        if(score >= 4){
+            position++;
+        }
     }
 
     @Override
     public void print() {
         StringBuilder stringBuilder = new StringBuilder(name + ":");
-
-        for (int i=0; i<position; i++) {
+        for (int i = 0; i < position; i++) {
             stringBuilder.append("-");
         }
         System.out.println(stringBuilder);
