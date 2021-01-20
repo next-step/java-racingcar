822d9c35b46149c59ca33fd8a08e8e3f198b42bd
d36choi <yuio1126@gmail.com>
2021-01-20 17:31:02 +0900

    feat: RacingCar.java 에 Getter/Setter 및 movingForward 메소드 생성함

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,5 +4,36 @@ public class RacingCar {
     private String name;
     private int status;
 
+    public RacingCar(String name, int status) {
+        this.name = name;
+        this.status = status;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "RacingCar{" +
+                "name='" + name + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+    public void movingForward() {
+        this.status++;
+    }
 }
