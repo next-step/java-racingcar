d2f8b3221e1023d8b83b28e43ea854401c4d5381
munkyung <munkyung0324@gmail.com>
2021-01-20 18:11:49 +0900

    feat: create PrintUtils

---
+++ src/main/java/calculator/PrintUtils.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class PrintUtils {
+    public static void printInputRequest() {
+        System.out.println("계산할 문구를 넣어주세요. (ex. 2 + 3 * 4 / 2)");
+    }
+
+    public static void printResultOutput(int result) {
+        System.out.println("결과값은 " + result + "입니다.");
+    }
+}
