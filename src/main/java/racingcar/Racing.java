7b4a4fa440fba0127c54e17d8bf7ab6b6ad0fafc
KimBB <tree9295@gmail.com>
2021-01-20 16:45:46 +0900

    feat: getRandomNumber method 구현
    - 0~9 무작위 수 반환 메서드

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Racing {
     private ArrayList<Car> carsList;
@@ -9,5 +10,10 @@ public class Racing {
     public Racing(ArrayList<Car> carsList, int stages){
         this.carsList = carsList;
         this.stages = stages;
+    }
+
+    public int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(10);
     }
 }
