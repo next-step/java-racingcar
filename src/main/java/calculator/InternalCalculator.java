50d20c5e27e9a553016788cddf4fd6091cdaa67f
munkyung <munkyung0324@gmail.com>
2021-01-20 18:14:11 +0900

    refactor: divide CalculationUtils

--- src/main/java/calculator/InternalCalculator.java
+++ src/main/java/calculator/InternalCalculator.java
@@ -32,35 +32,4 @@ public class InternalCalculator {
                 throw new IllegalArgumentException();
         }
     }
-
-    /*
-    public int startCalculator() {
-        String[] inputArray = requestInput.askUserForSuitableInput();
-        //TODO : 받아온 문자열을 연산하는 클래스함수 호출?
-        return getTotal();
-    }*/
-
-    private int getTotal(ArrayList<CalculationInfo> calculationUserInfo) {
-        int total = 0;
-        for (int i = 0; i < calculationUserInfo.size(); i++) {
-            total += calculate(calculationUserInfo.get(i));
-        }
-        return total;
-    }
-
-    private int calculate(CalculationInfo calculationInfo) {
-        Operator operator = calculationInfo.getOperator();
-        int dividend = calculationInfo.getDividend();
-        int divisor = calculationInfo.getDivisor();
-        switch (operator) {
-            case ADD:
-                return dividend + divisor;
-            case SUBTRACT:
-                return dividend - divisor;
-            case MULTIPLY:
-                return dividend * divisor;
-            default:
-                return dividend / divisor;
-        }
-    }
 }
