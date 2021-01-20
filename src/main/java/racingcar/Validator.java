5ea6e18869f74a7ead7c2bfa3ec2fb816a446ea6
KimBB <tree9295@gmail.com>
2021-01-20 17:52:58 +0900

    feat: checkDuplicateName method 구현
    - 차량 이름 중 중복된 이름 없는지 확인

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,11 +1,16 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Validator {
     public static boolean validateCarsName(String carsName) {
         try {
             checkEmptyElement(carsName);
             checkLastValueComma(carsName);
             checkCarNameLength(carsName);
+            checkDuplicateName(carsName);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
             return false;
@@ -31,6 +36,28 @@ public class Validator {
         }
     }
 
+
+    public static void checkCarNameLength(String input) throws IllegalArgumentException{
+        String[] carsNames = input.split(",");
+        for(String carName : carsNames) {
+            checkGreaterThan5(carName);
+        }
+    }
+
+    public static void checkGreaterThan5(String carName) throws IllegalArgumentException{
+        if(carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
+    }
+
+    public static void checkDuplicateName(String input) throws IllegalArgumentException{
+        String[] carsNames = input.split(",");
+        Set<String> carsUniqueName = new HashSet<>(Arrays.asList(carsNames));
+        if(carsNames.length != carsUniqueName.size()){
+            throw new IllegalArgumentException("중복된 자동차 이름이 있습니다. 다시 입력해주세요.");
+        }
+    }
+
     public static boolean validateStages(String stages) {
         try {
             Validator.checkNumber(stages);
@@ -46,19 +73,6 @@ public class Validator {
             Integer.parseInt(input);
         } catch (NumberFormatException e){
             throw new IllegalArgumentException("숫자가 아닙니다. 다시 입력해주세요.");
-        }
-    }
-
-    public static void checkCarNameLength(String input) throws IllegalArgumentException{
-        String[] carsNames = input.split(",");
-        for(String carName : carsNames) {
-            checkGreaterThan5(carName);
-        }
-    }
-
-    public static void checkGreaterThan5(String carName) throws IllegalArgumentException{
-        if(carName.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
     }
 }
