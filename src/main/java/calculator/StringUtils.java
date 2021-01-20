b462f0d09b080274a150d9d719f2a15062767695
munkyung <munkyung0324@gmail.com>
2021-01-20 20:03:18 +0900

    style: add blank for java convention

--- src/main/java/calculator/StringUtils.java
+++ src/main/java/calculator/StringUtils.java
@@ -1,10 +1,8 @@
 package calculator;
 
-import java.util.ArrayList;
-
 public class StringUtils {
 
-    public static String[] splitInput(String input){
+    public static String[] splitInput(String input) {
         String[] inputArray = input.split(" ");
         return inputArray;
     }
