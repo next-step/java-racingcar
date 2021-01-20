bc9e688cceba4ad48f99893da2cf7edec7a38e10
gyumin-kim <starkying@gmail.com>
2021-01-20 23:32:20 +0900

    feat: `Cars` 일급 컬렉션 추가
    * `Car`의 컬렉션을 갖는 `Cars` 구현
    * 관련 테스트 추가

---
+++ src/test/java/racingcar/CarsTest.java
@@ -0,0 +1,50 @@
+package racingcar;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @DisplayName("doRound() 호출하면 움직인 후의 위치 출력")
+    @Test
+    void doRound() {
+        // given
+        List<String> carStrings = List.of(
+                "car1",
+                "car2",
+                "car3"
+        );
+        Cars cars = new Cars(carStrings, new LookLikeRandomMoveChecker());
+
+        // when
+        cars.doRound();
+
+        // then
+        assertThat(outputStreamCaptor.toString()).isEqualTo(
+                "car1 : -\n" +
+                        "car2 : \n" +
+                        "car3 : \n\n"
+        );
+    }
+}
