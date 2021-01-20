ebb77568587698580f52d4aaa8c8c8b1b8a762d0
munkyung <munkyung0324@gmail.com>
2021-01-20 18:14:11 +0900

    refactor: divide CalculationUtils

--- src/main/java/calculator/InternalCalculator.java
+++ src/main/java/calculator/InternalCalculator.java
@@ -1,44 +1,38 @@
 package calculator;
 
-
-import java.util.ArrayList;
+import static calculator.Operator.*;
 
 public class InternalCalculator {
 
     private static RequestInput requestInput = new RequestInput();
 
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    /*
-    public int startCalculator() {
+    public void startCalculator() {
         String[] inputArray = requestInput.askUserForSuitableInput();
-        //TODO : 받아온 문자열을 연산하는 클래스함수 호출?
-        return getTotal();
-    }*/
-
-    private int getTotal(ArrayList<CalculationInfo> calculationUserInfo) {
-        int total = 0;
-        for (int i = 0; i < calculationUserInfo.size(); i++) {
-            total += calculate(calculationUserInfo.get(i));
+        CalculationInfo calculationInfo = requestInput
+            .convertToCalculationInfo(inputArray, 0, Integer.parseInt(inputArray[0]));
+        for (int i = 2; i < inputArray.length - 2; i += 2) {
+                calculationInfo = requestInput
+                    .convertToCalculationInfo(inputArray, i, calculate(calculationInfo));
         }
-        return total;
+        PrintUtils.printResultOutput(calculate(calculationInfo));
     }
 
-    private int calculate(CalculationInfo calculationInfo) {
+    public int calculate(CalculationInfo calculationInfo) {
         Operator operator = calculationInfo.getOperator();
         int dividend = calculationInfo.getDividend();
         int divisor = calculationInfo.getDivisor();
-        switch (operator) {
-            case ADD:
-                return dividend + divisor;
-            case SUBTRACT:
-                return dividend - divisor;
-            case MULTIPLY:
-                return dividend * divisor;
-            default:
-                return dividend / divisor;
+        if (operator.equals(ADD)) {
+            return CalculatorUtils.add(dividend, divisor);
         }
+        if (operator.equals(SUBTRACT)) {
+            return CalculatorUtils.subtract(dividend, divisor);
+        }
+        if (operator.equals(MULTIPLY)) {
+            return CalculatorUtils.multiply(dividend, divisor);
+        }
+        if (operator.equals(DIVIDE)) {
+            return CalculatorUtils.divide(dividend, divisor);
+        }
+        return 0;
     }
 }
