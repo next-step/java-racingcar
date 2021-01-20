4b7b8e1c1221be3d7f8f27472d34d7153c6f47da
d36choi <yuio1126@gmail.com>
2021-01-20 16:16:20 +0900

    feat: 입력_순서_에러테스트() test method를 추가함.

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,9 +9,14 @@ import java.io.*;
 public class CalculatorTest {
     private static Calculator calculator;
     private static final String errMsg = "계산식을 입력해주세요.\n올바른 계산식을 입력해주세요.\n";
+    OutputStream out;
+    InputStream in;
+
     @BeforeAll
     static void initAll() {
         calculator = new Calculator();
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
     }
 
     @Test
@@ -25,13 +30,23 @@ public class CalculatorTest {
     void 연산자_에러테스트() {
 
         String input = "1 + 2 k 3";
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         calculator.start();
         Assertions.assertThat(errMsg).isEqualTo(out.toString());
 
+    }
+
+    @Test
+    void 입력_순서_에러테스트() {
+
+        String input = "+ 1 +";
+
+        in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        calculator.start();
+        Assertions.assertThat(errMsg).isEqualTo(out.toString());
     }
 
 }
