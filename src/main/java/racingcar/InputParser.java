9066aa0de0a7d29b44e85dc1575cf729d211868a
gyumin-kim <starkying@gmail.com>
2021-01-20 17:52:43 +0900

    feat: 입력값을 파싱하는 기능 구현
    `InputParser` 생성 및 테스트 추가

---
+++ src/main/java/racingcar/InputParser.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import java.util.List;
+
+public class InputParser {
+
+    private InputParser() {
+    }
+
+    private static final String DELIMITER = ",";
+    private static final int MAX_CAR_NAME_LENGTH = 5;
+
+    public static List<String> parseCarNames(String input) {
+        validateNonNull(input);
+        String[] carNames = input.split(DELIMITER);
+        for (String carName : carNames) {
+            validateLength(carName);
+        }
+        return List.of(carNames);
+    }
+
+    private static void validateNonNull(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+    }
+
+    private static void validateLength(final String carName) {
+        if (carName.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
+    }
+}
