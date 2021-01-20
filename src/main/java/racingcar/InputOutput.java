64178ba642f57fd0e4a559a7bfda5ec4be453a82
KimBB <tree9295@gmail.com>
2021-01-20 17:41:55 +0900

    refactor: Validator 클래스로 validate methods 이동
    - validateCarsName: InputOuput class -> Validator class
    - validateStages: InputOutput class -> Validator class

--- src/main/java/racingcar/InputOutput.java
+++ src/main/java/racingcar/InputOutput.java
@@ -11,20 +11,9 @@ public class InputOutput {
         boolean isValidCarsName = false;
         while (!isValidCarsName) {
             carsName = scanner.next();
-            isValidCarsName = validateCarsName(carsName);
+            isValidCarsName = Validator.validateCarsName(carsName);
         }
         return carsName;
-    }
-    private static boolean validateCarsName(String carsName) {
-        try {
-            Validator.checkEmptyElement(carsName);
-            Validator.checkLastValueComma(carsName);
-            Validator.checkCarNameLength(carsName);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
     }
 
     public static int inputStages() {
@@ -32,19 +21,9 @@ public class InputOutput {
         boolean isValidStages = false;
         while (!isValidStages) {
             stages = scanner.next();
-            isValidStages = validateStages(stages);
+            isValidStages = Validator.validateStages(stages);
         }
         return Integer.parseInt(stages);
-    }
-
-    private static boolean validateStages(String stages) {
-        try {
-            Validator.checkNumber(stages);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
     }
 
     public static void printCurrentCarsState(ArrayList<Car> carsList){
--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,10 +1,16 @@
 package racingcar;
 
 public class Validator {
-    public static void checkEmptyInput(String input) throws IllegalArgumentException {
-        if (input == null || input.trim().isEmpty()) {
-            throw new IllegalArgumentException("공백이거나 빈 데이터가 있습니다. 다시 입력해 주세요.");
+    public static boolean validateCarsName(String carsName) {
+        try {
+            checkEmptyElement(carsName);
+            checkLastValueComma(carsName);
+            checkCarNameLength(carsName);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return false;
         }
+        return true;
     }
 
     public static void checkEmptyElement(String input) throws IllegalArgumentException {
@@ -13,10 +19,26 @@ public class Validator {
         }
     }
 
+    public static void checkEmptyInput(String input) throws IllegalArgumentException {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("공백이거나 빈 데이터가 있습니다. 다시 입력해 주세요.");
+        }
+    }
+
     public static void checkLastValueComma(String input) throws IllegalArgumentException {
         if (input.charAt(input.length()-1) == ','){
             throw new IllegalArgumentException("마지막 값이 제대로 입력되지 않았습니다. 다시 입력해 주세요.");
         }
+    }
+
+    public static boolean validateStages(String stages) {
+        try {
+            Validator.checkNumber(stages);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     public static void checkNumber(String input) throws IllegalArgumentException{
@@ -34,7 +56,7 @@ public class Validator {
         }
     }
 
-    private static void checkGreaterThan5(String carName) throws IllegalArgumentException{
+    public static void checkGreaterThan5(String carName) throws IllegalArgumentException{
         if(carName.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
