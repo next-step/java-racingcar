a800386558aef305b138d9b1f3b008fac93d8ade
d36choi <yuio1126@gmail.com>
2021-01-20 17:57:12 +0900

    feat: RacingGameController.java 에 getRound() 메소드를 작성함.

--- src/main/java/racingcar/RacingGameController.java
+++ src/main/java/racingcar/RacingGameController.java
@@ -1,15 +1,19 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class RacingGameController {
-    ArrayList<RacingCar> cars = new ArrayList<>();
+    private ArrayList<RacingCar> cars = new ArrayList<>();
+    private final Scanner sc = new Scanner(System.in);
+    private int numberOfRound;
+
 
     public void getCarsName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
-        Scanner sc = new Scanner(System.in);
+
         String input = sc.nextLine();
 
         if (input == null || input.trim().isEmpty()) {
@@ -23,10 +27,27 @@ public class RacingGameController {
     }
 
     public void getRound() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        String input = sc.nextLine();
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        numberOfRound = sc.nextInt();
 
     }
 
     public void findWinner() {
+
+    }
+
+    public int getRandomNumber() {
+        Random rand = new Random();
+
+        return rand.nextInt(10);
+    }
+
+    public void playRound() {
 
     }
 }
