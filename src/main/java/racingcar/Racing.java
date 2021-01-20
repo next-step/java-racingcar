032207e5bc098240d2216720e999f25e9f7a2465
KimBB <tree9295@gmail.com>
2021-01-20 16:39:05 +0900

    feat: Racing class 구현
    생성자
    - ArrayList<Car> carsList: 레이싱 경주 참여 자동차 리스트
    - int stages: 레이싱 경주 횟수

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+public class Racing {
+    private ArrayList<Car> carsList;
+    private int stages;
+
+    public Racing(ArrayList<Car> carsList, int stages){
+        this.carsList = carsList;
+        this.stages = stages;
+    }
+}
