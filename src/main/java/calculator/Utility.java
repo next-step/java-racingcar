859af0ecffd917119b81b9f1dae100cff07f43ec
Changhwan Lee <chlee1252@gmail.com>
2021-01-21 23:36:31 +0900

    refactor: refactor string calculator and add Main class.
    Divide functions from Calculator and add Main function

---
+++ src/main/java/calculator/Utility.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Utility {
+    /**
+     * Split string formula by using regex.
+     * @param formula String of math equations
+     * @param regex String of regular expression.
+     * @return equation, Array of String.
+     */
+    public static ArrayList<String> splitFormula(String formula, String regex) {
+        ArrayList<String> equation = new ArrayList<>(Arrays.asList(formula.split(regex)));
+        equation.remove("");   // 리스트안에 있는 빈 공백을 지운다.
+        return equation;
+    }
+
+    public static ArrayList<Double> stringToDouble(ArrayList<String> array) {
+        ArrayList<Double> result = new ArrayList<>(array.size());
+        for (int i = 0; i < array.size(); i++) {
+            result.add(i, Double.parseDouble(array.get(i)));
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/calculator/Validator.java
@@ -0,0 +1,25 @@
+package calculator;
+
+import java.util.ArrayList;
+
+public class Validator {
+    public static void validateFormula(ArrayList<String> operands, ArrayList<String> operators) {
+        validateOperand(operands);
+        validateOperator(operators, operands);
+    }
+
+    private static void validateOperand(ArrayList<String> operands) {
+        for(String operand : operands) {
+            boolean isDigit = operand.chars().allMatch( Character::isDigit );
+            if (!isDigit) {
+                throw new IllegalArgumentException("잘못된 식입니다.");
+            }
+        }
+    }
+
+    private static void validateOperator(ArrayList<String> operators, ArrayList<String> operands) {
+        if (operators.size() == operands.size() + 1) {
+            throw new IllegalArgumentException("잘못된 식입니다.");
+        }
+    }
+}
