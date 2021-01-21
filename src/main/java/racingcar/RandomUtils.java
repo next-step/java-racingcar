227a9f24e539188fe4eec47a1d60f7d523eb491f
munkyung <munkyung0324@gmail.com>
2021-01-21 20:47:05 +0900

    feat: create RandomUtils

---
+++ src/main/java/racingcar/RandomUtils.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import java.util.Random;
+
+public class RandomUtils {
+    private static final Random RANDOM = new Random();
+
+    private RandomUtils() {
+    }
+
+    public static int nextInt(final int startInclusive, final int endInclusive) {
+        if (startInclusive > endInclusive) {
+            throw new IllegalArgumentException();
+        }
+
+        if (startInclusive < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if (startInclusive == endInclusive) {
+            return startInclusive;
+        }
+
+        return startInclusive + RANDOM.nextInt(endInclusive - startInclusive + 1);
+    }
+}
