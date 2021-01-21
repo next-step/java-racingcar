3f58066fb64ed99411ab2aa140a068530463b8c0
koeonyack <covenant.ko@kakao.com>
2021-01-21 18:11:35 +0900

    feat: add racingcar test code
    - game test code
    - inputHandler test code

---
+++ src/test/java/racingcar/GameTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+public class GameTest {
+
+
+    @Test
+    @DisplayName("getWinners 테스트")
+    public void getWinnersTest() {
+        final Game game = new Game();
+        List<Car> test = GenerateCar.ofCars("a,b,c");
+        assertThat(game.getWinners(test)).isEqualTo("a,b,c");
+    }
+
+}
