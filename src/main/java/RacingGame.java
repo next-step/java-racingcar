29c071aada3f732714536993fa0e14771e8ed25b
hakzzang <mbh930312@naver.com>
2021-01-21 17:35:51 +0900

    refactoring : RacingGame 로직 변경

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,20 +1,29 @@
-import racingcar.*;
+import racingcar.di.CarFactory;
+import racingcar.di.CarFactoryImpl;
+import racingcar.domain.Car;
+import racingcar.utils.RacingTrack;
+import racingcar.utils.RacingTrackImpl;
+import racingcar.utils.RankPicker;
+import racingcar.utils.RankPickerImpl;
+import racingcar.view.RacingGameView;
+import racingcar.view.RacingGameViewImpl;
 
 import java.util.List;
 
 public class RacingGame {
-    public static void main(String[] args){
-        InputView inputView = new InputView();
-        ResultView resultView = new ResultView();
-        String[] names = inputView.returnNames();
-
+    public static void main(String[] args) {
         CarFactory carFactory = new CarFactoryImpl();
-        List<Car> cars = carFactory.makeCars(names);
+        RacingGameView racingGameView = new RacingGameViewImpl();
+        RankPicker rankPicker = new RankPickerImpl();
+        String[] carNames = racingGameView.inputCarNames();
+        List<Car> cars = carFactory.makeCars(carNames);
 
-        int loopCount = inputView.getTryNo();
+        int playCount = racingGameView.inputPlayCount();
 
-        PlayGround.play(loopCount,cars);
-        resultView.printWinner(cars);
-   }
+        RacingTrack racingTrack = new RacingTrackImpl();
+        racingTrack.run(playCount, () -> racingGameView.printCars(cars));
 
+        List<Car> topRankers = rankPicker.pickTopRankers(cars);
+        racingGameView.printWinner(topRankers);
+    }
 }
