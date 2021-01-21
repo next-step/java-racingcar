dd7de67e807d897d48861be43eafaa35028ddef4
hakzzang <mbh930312@naver.com>
2021-01-21 19:02:15 +0900

    feat: 이름을 테스트하는 코드 추가
    - 이름의 길이가 없을 때, 테스트 코드 추가
    - 이름의 길이가 길 때, 테스트 코드 추가

--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -5,6 +5,8 @@ import racingcar.di.CarFactory;
 import racingcar.di.CarFactoryImpl;
 import racingcar.domain.Car;
 import racingcar.domain.MockCar;
+import racingcar.utils.NameChecker;
+import racingcar.utils.NameCheckerImpl;
 import racingcar.utils.RankPicker;
 import racingcar.utils.RankPickerImpl;
 
@@ -12,8 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class RacingGameTest {
+    NameChecker nameChecker = new NameCheckerImpl();
     CarFactory carFactory = new CarFactoryImpl();
     RankPicker rankPicker = new RankPickerImpl();
     static List<Car> mockCars = new ArrayList<>();
@@ -29,6 +33,24 @@ public class RacingGameTest {
         mockCars.add(new MockCar("5th-kotlin", 4));
     }
 
+    @DisplayName("자동차 이름이 없으면 안 되는 테스트")
+    @Test
+    void testEmptyName(){
+        assertThatThrownBy(() -> {
+            nameChecker.checkAvailableCarName("");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Car's name must be at least 1 letter.");
+    }
+
+    @DisplayName("자동차 이름이 6글자 이상이면 안 되는 테스트")
+    @Test
+    void testLongCarNameLength(){
+        assertThatThrownBy(() -> {
+            nameChecker.checkAvailableCarName("12356123");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Car's name must be 5 characters or less.");
+    }
+
     @DisplayName("자동차가 잘 만들어졌는 지, 하는 테스트")
     @Test
     void testIsMakeCarList(){
@@ -37,7 +59,7 @@ public class RacingGameTest {
         assertThat(cars).extracting("name").contains("hello","new", "java");
     }
 
-    @DisplayName("가장 빠른 자동차를 골라내는 테스")
+    @DisplayName("가장 빠른 자동차를 골라내는 테스트")
     @Test
     void testPickTopRankers(){
         List<Car> rankers = rankPicker.pickTopRankers(mockCars);
