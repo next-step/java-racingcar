d14f7156bfc6a11060e6449e14a4f8e0a7f4e13b
gyumin-kim <starkying@gmail.com>
2021-01-22 00:31:15 +0900

    refactor: `RacingGame` 생성자 내 validate 메서드 추가
    `RacingGame` 생성자 내에서 사용하는 validate 메서드 추가
    * 관련 테스트 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,9 +7,31 @@ public class RacingGame {
     private final int rounds;
 
     public RacingGame(List<String> carNames, int rounds) {
-        // TODO: validate()
+        validateSize(carNames);
+        validateNonBlankNames(carNames);
+        validateRounds(rounds);
         this.cars = new Cars(carNames);
         this.rounds = rounds;
+    }
+
+    private void validateSize(final List<String> carNames) {
+        if (carNames == null || carNames.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름이 비어 있습니다.");
+        }
+    }
+
+    private void validateNonBlankNames(final List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName == null || carName.isBlank()) {
+                throw new IllegalArgumentException("이름이 없는 자동차가 있습니다.");
+            }
+        }
+    }
+
+    private void validateRounds(final int rounds) {
+        if (rounds < 1) {
+            throw new IllegalArgumentException("라운드는 최소 1 이상이어야 합니다.");
+        }
     }
 
     public void play() {
