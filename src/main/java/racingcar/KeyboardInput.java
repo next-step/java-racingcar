442d19b43304428d5a226dbb0b89a47934641a97
gyumin-kim <starkying@gmail.com>
2021-01-22 01:45:18 +0900

    chore: TODO 주석 정리

--- src/main/java/racingcar/KeyboardInput.java
+++ src/main/java/racingcar/KeyboardInput.java
@@ -13,16 +13,18 @@ public class KeyboardInput implements Input {
 
     @Override
     public List<String> getCarNames() {
-        // TODO: 사용자에게 출력할 메시지는 인자로 받도록
+        // TODO: 사용자에게 출력할 메시지를 담고 있는 별도 클래스 생성
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        // TODO: 중복된 이름이 존재하는 경우에 대한 예외 처리
         String carNames = scanner.nextLine();
         return InputParser.parseCarNames(carNames);
     }
 
     @Override
     public int getRounds() {
-        // TODO: 사용자에게 출력할 메시지는 인자로 받도록
+        // TODO: 사용자에게 출력할 메시지를 담고 있는 별도 클래스 생성
         System.out.println("시도할 횟수는 몇 회인가요?");
+        // TODO: 숫자가 아닌 값을 입력한 경우에 대한 예외 처리
         return scanner.nextInt();
     }
 }
--- src/main/java/racingcar/RacingCarGameApplication.java
+++ src/main/java/racingcar/RacingCarGameApplication.java
@@ -6,13 +6,8 @@ public class RacingCarGameApplication {
 
     public static void main(String[] args) {
         Input input = new KeyboardInput(System.in);
-
-        // TODO: carNames와 rounds를 여기서 일일이 받지 말고,
-        //  내부에서 처리하도록 하는 무언가가 있으면 좋겠다.
-        //  + carNames별 length 체크까지 내부에서 다 하게끔
         List<String> carNames = input.getCarNames();
         int rounds = input.getRounds();
-
         RacingGame game = new RacingGame(carNames, rounds);
         game.play();
     }
