4725334018f89d9a2441d8197dd5ab9c302ecaef
d36choi <yuio1126@gmail.com>
2021-01-22 22:54:35 +0900

    fix: import 문의 wildcard를 제거함

--- src/main/java/racingcar/RacingGameController.java
+++ src/main/java/racingcar/RacingGameController.java
@@ -1,6 +1,10 @@
 package racingcar;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class RacingGameController {
 
