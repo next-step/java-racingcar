6d88443aec763f3961ff3bba287918bb9b905856
quadam36 <quadam36@gmail.com>
2021-01-22 12:54:19 +0900

    refactor: Remove unnecessary test
    - Change display name with more details

--- src/test/java/racingcar/CarsFactoryTest.java
+++ src/test/java/racingcar/CarsFactoryTest.java
@@ -19,15 +19,6 @@ class CarsFactoryTest {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             CarsFactory.createCars(null, new TrueMoveChecker());
         });
-        assertThat(exception.getMessage()).isEqualTo("입력값이 없습니다.");
-    }
-
-    @DisplayName("자동차의 이름이 null일 경우 exception 발생")
-    @Test
-    void carNameIsNull() {
-        assertThatThrownBy(
-            () -> CarsFactory.createCars("car1,,car3", new TrueMoveChecker())
-        ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -38,16 +29,15 @@ class CarsFactoryTest {
         ).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("각 자동차 이름의 길이가 5자를 초과하면 exception 발생")
+    @DisplayName("자동차의 이름이 5자를 초과하면 exception 발생")
     @Test
     void carNameLengthLongerThanFive() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             CarsFactory.createCars("car1,gyumin,sanghyun", new TrueMoveChecker());
         });
-        assertThat(exception.getMessage()).isEqualTo("자동차 이름은 5자를 초과할 수 없습니다.");
     }
 
-    @DisplayName("자동차 이름에 따라서 자동차들 객체를 잘 생성하는지 확인")
+    @DisplayName("자동차들의 이름에 따라서 자동차들 객체를 잘 생성하는지 확인")
     @Test
     void createCars() {
         MoveChecker moveChecker = new TrueMoveChecker();
