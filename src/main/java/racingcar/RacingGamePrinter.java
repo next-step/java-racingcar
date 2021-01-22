f5030d6e0febbec85a43b357feff3c6fdc796806
d36choi <yuio1126@gmail.com>
2021-01-22 23:50:56 +0900

    feat: printAllCarStatus() 를 가져와 모든 출력문 역할을 가져옴

--- src/main/java/racingcar/RacingGamePrinter.java
+++ src/main/java/racingcar/RacingGamePrinter.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class RacingGamePrinter {
 
-    public static void printEachCarStatus(String name, int status) {
+    private static void printEachCarStatus(String name, int status) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(name);
         stringBuilder.append(":");
@@ -14,9 +14,19 @@ public class RacingGamePrinter {
         System.out.println(stringBuilder.toString());
     }
 
+    public static void printAllCarStatus(List<RacingCar> cars) {
+
+        for (RacingCar car : cars) {
+            String name = car.getName();
+            int status = car.getStatus();
+            printEachCarStatus(name, status);
+        }
+        System.out.println();
+    }
+
     public static void printWinners(List<String> winners) {
         System.out.print("최종 우승자: ");
         String result = String.join(", ", winners);
-        System.out.println(result+"\n");
+        System.out.println(result);
     }
 }
