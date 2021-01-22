a9c7e7384767d9a61863fef43776f3730c0e8df6
d36choi <yuio1126@gmail.com>
2021-01-22 22:12:37 +0900

    test: 사칙연산의 정상동작을 테스트하는 코드를 @MethodSource를 통해 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,9 +2,14 @@ package calculator;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.*;
+import java.util.stream.Stream;
 
 public class CalculatorTest {
     private static Calculator calculator;
@@ -20,20 +25,21 @@ public class CalculatorTest {
         calculator = new Calculator();
     }
 
-    @Test
-    void 사칙연산_테스트() {
+    private static Stream<Arguments> provideExpressionAndAnswer() {
+        return Stream.of(
+                Arguments.of(1,"+",12, 13.0),
+                Arguments.of(1,"-", 2, -1.0),
+                Arguments.of(1,"*",5, 5.0),
+                Arguments.of(1,"/",2, 0.5)
+        );
+    }
 
-        String[] input = {"1 + 2", "1 - 2", "1 * 2", "1 / 2"};
-        String[] output = {"3.0", "-1.0", "2.0", "0.5"};
+    @DisplayName("정상적인 계산식의 답안을 테스트")
+    @ParameterizedTest
+    @MethodSource("provideExpressionAndAnswer")
+    void 사칙연산_테스트(double a,String op,double b,double result) {
 
-        for (int i = 0; i < NUMBER_OF_TESTCASE; i++) {
-            in = new ByteArrayInputStream(input[i].getBytes());
-            System.setIn(in);
-            out = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(out));
-            calculator.start();
-            Assertions.assertThat(inputMsg + output[i]).isEqualTo(out.toString());
-        }
+        Assertions.assertThat(calculator.calculate(a, op, b)).isEqualTo(result);
     }
 
     @Test
