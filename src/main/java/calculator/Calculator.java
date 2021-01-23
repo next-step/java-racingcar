d62b6b3c5e9fad105533e00f06f7944e399694fa
koeonyack <covenant.ko@kakao.com>
2021-01-23 21:04:32 +0900

    refactor: Calculator class
    - ArrayList를 List로 변경
    - 로직의 흐름에 맞추어 method 위치 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,47 +2,24 @@ package calculator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Calculator {
-    private ArrayList<String> operators;
-    private ArrayList<String> operands;
-
-    private void splitFormula(String formula) {
-        String eq = formula.replaceAll(" ", "");
-        operators = new ArrayList<>(Arrays.asList(eq.split("[0-9]+")));
-        operands = new ArrayList<>(Arrays.asList(eq.split("[-+*/()]")));
-    }
+    private List<String> operators;
+    private List<String> operands;
 
     public double getAnswer(String formula) {
         splitFormula(formula);
         validateFormula();
         sanitizeOperator(operators);
-        ArrayList<Double> doubleOperands = stringToDouble(operands);
+        List<Double> doubleOperands = stringToDouble(operands);
         return calculateFormula(operators, doubleOperands);
     }
 
-    private ArrayList<Double> stringToDouble(ArrayList<String> array) {
-        ArrayList<Double> result = new ArrayList<>(array.size());
-        for (int i = 0; i < array.size(); i++) {
-            result.add(i, Double.parseDouble(array.get(i)));
-        }
-        return result;
-    }
-
-    /**
-     * Calculate mathematics equation.
-     * @param operators, The list of double.
-     * @param operands The list of String (+, -, *, /).
-     * @return result Double, calculated result
-     */
-    private double calculateFormula(ArrayList<String> operators, ArrayList<Double> operands) {
-        double result = operands.get(0);
-        int j = 0;
-        for (int i = 1; i < operands.size(); i++) {
-            result = Arithmetic.calculate(operators.get(j), result, operands.get(i));
-            j++;
-        }
-        return result;
+    private void splitFormula(String formula) {
+        String equation = formula.replaceAll(" ", "");
+        operators = Arrays.asList(equation.split("[0-9]+"));
+        operands = Arrays.asList(equation.split("[-+*/()]"));
     }
 
     private void validateFormula() {
@@ -50,7 +27,15 @@ public class Calculator {
         validateOperator(operators, operands);
     }
 
-    private void validateOperand(ArrayList<String> operands) {
+    private List<Double> stringToDouble(List<String> array) {
+        List<Double> result = new ArrayList<>(array.size());
+        for (int i = 0; i < array.size(); i++) {
+            result.add(i, Double.parseDouble(array.get(i)));
+        }
+        return result;
+    }
+
+    private void validateOperand(List<String> operands) {
         for(String operand:operands) {
             System.out.println(">>" + operand);
             boolean isDigit = operand.chars().allMatch( Character::isDigit );
@@ -60,14 +45,30 @@ public class Calculator {
         }
     }
 
-    private void validateOperator(ArrayList<String> operators, ArrayList<String> operands) {
+    private void validateOperator(List<String> operators, List<String> operands) {
         sanitizeOperator(operands);
         if (operators.size() == operands.size() + 1) {
             throw new IllegalArgumentException("잘못된 식입니다.");
         }
     }
 
-    private void sanitizeOperator(ArrayList<String> operators) {
+    private void sanitizeOperator(List<String> operators) {
         operators.remove("");
+    }
+
+    /**
+     * Calculate mathematics equation.
+     * @param operators, The list of double.
+     * @param operands The list of String (+, -, *, /).
+     * @return result Double, calculated result
+     */
+    private double calculateFormula(List<String> operators, List<Double> operands) {
+        double result = operands.get(0);
+        int j = 0;
+        for (int i = 1; i < operands.size(); i++) {
+            result = Arithmetic.calculate(operators.get(j), result, operands.get(i));
+            j++;
+        }
+        return result;
     }
 }
