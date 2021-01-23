6419129fb28364a3e35250e48ae2f82f8ef8f6c5
Changhwan Lee <chlee1252@gmail.com>
2021-01-24 01:28:39 +0900

    style: edit styles
    InputHandler: removed unused import. Utility: add code convention.

--- src/main/java/racingcar/domain/Utility.java
+++ src/main/java/racingcar/domain/Utility.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Utility {
+    
     public static ArrayList<String> nameStringToArray(String nameString) {
         nameString = removeEmptySpaceFromName(nameString);
         return new ArrayList<String>(Arrays.asList(nameString.split(",")));
--- src/main/java/racingcar/view/InputHandler.java
+++ src/main/java/racingcar/view/InputHandler.java
@@ -1,6 +1,5 @@
 package racingcar.view;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputHandler {
