79171b18527c08d42967df9aa479f8d85772fa33
quadam36 <quadam36@gmail.com>
2021-01-24 16:40:30 +0900

    fix: Fix output location print format
    - Fix output location print format
    - Extract round start msg from printRound method

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -9,24 +9,34 @@ import java.util.List;
 public class Output {
     private static final String WINNER_MSG = "최종 우승자: ";
     private static final String WINNER_DELIMITER = ",";
-    private static final String ROUND_MSG = "실행 결과";
+    private static final String ROUND_START_MSG = "실행 결과";
     private static final String CAR_DELIMITER = " : ";
+    private static final String CAR_LOCATION_SIGN = "-";
 
     public void printWinners(ResultWinners result) {
         List<String> winners = result.getWinners();
         System.out.println(WINNER_MSG + String.join(WINNER_DELIMITER, winners));
     }
 
+    public void printRoundStart() {
+        System.out.println(ROUND_START_MSG);
+    }
+
     public void printRound(ResultRound result) {
-        System.out.println(ROUND_MSG);
         for(ResultCar resultCar : result.getResultCars()) {
             printResultCar(resultCar);
         }
+        System.out.println();
     }
 
     private void printResultCar(ResultCar result) {
+        StringBuilder formattedLocation = new StringBuilder();
+        for (int i = 0; i < result.getLocation(); i++) {
+            formattedLocation.append(CAR_LOCATION_SIGN);
+        }
+
         System.out.println(
-            result.getCarName() + CAR_DELIMITER + result.getLocation()
+            result.getCarName() + CAR_DELIMITER + formattedLocation.toString()
         );
     }
 }
