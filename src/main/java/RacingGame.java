7c26f4f15a20f7f6823d787152a04d752d128b0c
hakzzang <mbh930312@naver.com>
2021-01-24 20:54:59 +0900

    refactoring : RacingGame 클래스에 컨트롤러 연결

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,29 +1,12 @@
-import racingcar.di.CarFactory;
-import racingcar.di.CarFactoryImpl;
-import racingcar.domain.Car;
-import racingcar.utils.RacingTrack;
-import racingcar.utils.RacingTrackImpl;
-import racingcar.utils.RankPicker;
-import racingcar.utils.RankPickerImpl;
-import racingcar.view.RacingGameView;
-import racingcar.view.RacingGameViewImpl;
-
-import java.util.List;
+import racingcar.controller.RacingGameController;
+import racingcar.controller.RacingGameControllerImpl;
 
 public class RacingGame {
     public static void main(String[] args) {
-        CarFactory carFactory = new CarFactoryImpl();
-        RacingGameView racingGameView = new RacingGameViewImpl();
-        RankPicker rankPicker = new RankPickerImpl();
-        String[] carNames = racingGameView.inputCarNames();
-        List<Car> cars = carFactory.makeCars(carNames);
+        RacingGameController racingGameController = new RacingGameControllerImpl();
 
-        int playCount = racingGameView.inputPlayCount();
-
-        RacingTrack racingTrack = new RacingTrackImpl();
-        racingTrack.run(playCount, () -> racingGameView.printCars(cars));
-
-        List<Car> topRankers = rankPicker.pickTopRankers(cars);
-        racingGameView.printWinner(topRankers);
+        racingGameController.initialize();
+        racingGameController.play();
+        racingGameController.showResult();
     }
 }
