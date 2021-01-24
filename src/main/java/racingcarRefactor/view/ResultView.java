7bfe68f859e32fbceff55d3367d93d9a94d975f8
koeonyack <covenant.ko@kakao.com>
2021-01-24 18:41:22 +0900

    refactor: reformat resultView

--- src/main/java/racingcarRefactor/view/ResultView.java
+++ src/main/java/racingcarRefactor/view/ResultView.java
@@ -5,23 +5,27 @@ public class ResultView {
     private final static String NEW_LINE = "\n";
     private final static String WINNER_ANNOUNCE = "최종 우승자 : ";
 
-    public static void displayRound(String driver, int distance) {
-        displayDriver(driver);
+    public static void displayRound(String carName, int distance) {
+        displayCarName(carName);
         displayDrivingDistance(distance);
     }
 
-    public static void displayDriver(String driver) {
-        System.out.print(driver + " : ");
-    }
-
-    private static void displayDrivingDistance(int distance) {
-        for (int i=0; i<=distance; i++) {
-            System.out.print(DRIVING_MARK);
-        }
+    public static void endOfRound() {
         System.out.print(NEW_LINE);
     }
 
-    public static void displayWinner(String winner) {
-        System.out.print(WINNER_ANNOUNCE + winner);
+    public static void displayWinner(String winners) {
+        System.out.printf(WINNER_ANNOUNCE + " %s%n", winners);
+    }
+
+    private static void displayCarName(String carName) {
+        System.out.print(carName + " : ");
+    }
+
+    private static void displayDrivingDistance(int distance) {
+        for (int i = 0; i <= distance; i++) {
+            System.out.print(DRIVING_MARK);
+        }
+        System.out.print(NEW_LINE);
     }
 }
