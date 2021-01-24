52d10ee4cac91b754448a79470868bf8d8988acd
koeonyack <covenant.ko@kakao.com>
2021-01-24 15:49:44 +0900

    refactor: change method name "get->input"

--- src/main/java/racingcarRefactor/view/InputView.java
+++ src/main/java/racingcarRefactor/view/InputView.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class InputView {
     public static Scanner scanner = new Scanner(System.in);
 
-    public static String getCarNames() {
+    public static String inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return scanner.nextLine();
     }
 
-    public static int getTotalLaps() {
+    public static int inputTotalLaps() {
         System.out.println("시도할 횟수를 입력해주세");
         return scanner.nextInt();
     }
