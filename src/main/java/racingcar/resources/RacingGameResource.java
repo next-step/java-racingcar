dbb60c17b0a445443eb4c8078a27fd4475db9bba
hakzzang <mbh930312@naver.com>
2021-01-24 20:53:14 +0900

    feat: RacingGameResource 추가
    - 프로젝트 내에서 관리하는 문자열 클래스 추가

---
+++ src/main/java/racingcar/resources/RacingGameResource.java
@@ -0,0 +1,7 @@
+package racingcar.resources;
+
+public class RacingGameResource {
+    public static final String INPUT_CAR_NAME_HINT = "경주할 자동차 이름을 입력하세요:";
+    public static final String INPUT_PLAY_COUNT_HINT = "시도할 횟수는 몇 회인가요?";
+    public static final String FINAL_WINNER_DECORATION = "최종 우승자: ";
+}
