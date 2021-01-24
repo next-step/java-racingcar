910795857333136681904c6e21938f87b6d6848e
hanseokhyeon <sah0322@naver.com>
2021-01-24 17:57:37 +0900

    feat: isEnd 함수 추가

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -5,9 +5,15 @@ import java.util.ArrayList;
 public class RacingGame {
     private ArrayList<Car> cars;
     private int tryNo;
+    private int nowNo;
 
     public RacingGame(String carNames, int tryNo){
         cars = CarFactory.getCars(carNames);
         this.tryNo = tryNo;
+        this.nowNo = 0;
+    }
+
+    public Boolean isEnd() {
+        return nowNo == tryNo;
     }
 }
