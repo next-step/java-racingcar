6e85a637e9d134cd815516f383a73ff990fba7e8
quadam36 <quadam36@gmail.com>
2021-01-24 18:09:08 +0900

    refactor: Extract constant msg from input class

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -3,6 +3,8 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class Input {
+    private static final String GET_CAR_NAMES_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String GET_TRY_COUNT_MSG = "시도할 횟수는 몇 회인가요?";
     private final Scanner scanner;
 
     public Input(Scanner scanner) {
@@ -10,12 +12,12 @@ public class Input {
     }
 
     public String getCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        System.out.println(GET_CAR_NAMES_MSG);
         return scanner.nextLine();
     }
 
     public int getTryCount() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(GET_TRY_COUNT_MSG);
         return scanner.nextInt();
     }
 }
