704cf410a36d1063fffe9422e37ba89a293fb707
Changhwan Lee <chlee1252@gmail.com>
2021-01-24 17:14:19 +0900

    refactor: changed test methods using parameterizedTest

--- src/test/java/racingcar/NameValidatorTest.java
+++ src/test/java/racingcar/NameValidatorTest.java
@@ -1,22 +1,25 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import racingcar.domain.NameValidator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.controller.NameValidator;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class NameValidatorTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "'asd', 'aaaaaa'",
+            "'asd', 'asdkfjasdlkfj'"
+    })
     @DisplayName("잘못된 이름 테스트")
-    public void carNameCheckerTest() {
-        ArrayList<String> test = new ArrayList<>(Arrays.asList("asd", "aaaaaa"));
-        ArrayList<String> test2 = new ArrayList<>(Arrays.asList("asd", "aaa"));
+    public void carNameCheckerTest(String input1, String input2) {
+        ArrayList<String> test = new ArrayList<>(Arrays.asList(input1, input2));
         assertThat(NameValidator.checkCarName(test)).isFalse();
-        assertThat(NameValidator.checkCarName(test2)).isTrue();
     }
 }
