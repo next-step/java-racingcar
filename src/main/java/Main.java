32c4bea76832ba21dc3b74f9be63a48dbe51dbfd
hanseokhyeon <sah0322@naver.com>
2021-01-24 20:28:51 +0900

    feat: empty.txt 제거

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import racinggame.RacingGameController;
-
-public class Main {
-    public static void main(String[] args) {
-        RacingGameController rgc = new RacingGameController();
-        rgc.start();
-
-    }
-}
