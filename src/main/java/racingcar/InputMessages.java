b1fd199e70eea07eebdf2e9b4fa9bae97bc837b2
gyumin-kim <starkying@gmail.com>
2021-01-24 20:42:26 +0900

    refactor: `InputMessages` 추가
    유저에게 입력을 받기 위해 출력하는 문자열을 `InputMessages`로 추출

---
+++ src/main/java/racingcar/InputMessages.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class InputMessages {
+
+    static final String ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    static final String ENTER_ROUNDS = "시도할 횟수는 몇 회인가요?";
+}
