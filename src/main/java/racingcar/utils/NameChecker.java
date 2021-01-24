a26b3202f5f822fd5bbf0c360d3a4d6423f1fe25
hakzzang <mbh930312@naver.com>
2021-01-24 20:47:28 +0900

    refactoring: NameChecker에 split 기능 분리

--- src/main/java/racingcar/utils/NameChecker.java
+++ src/main/java/racingcar/utils/NameChecker.java
@@ -1,6 +1,5 @@
 package racingcar.utils;
 
 public interface NameChecker {
-    String[] splitName(String inputLine);
     boolean checkAvailableCarName(String carName);
 }
--- src/main/java/racingcar/utils/NameCheckerImpl.java
+++ src/main/java/racingcar/utils/NameCheckerImpl.java
@@ -1,20 +1,15 @@
 package racingcar.utils;
 
 public class NameCheckerImpl implements NameChecker {
-
-    @Override
-    public String[] splitName(String inputLine) {
-        return inputLine.split(",");
-    }
-
     @Override
     public boolean checkAvailableCarName(String carName) {
-        if(isEmptyName(carName)){
+        if (isEmptyName(carName)) {
             throw new IllegalArgumentException("Car's name must be at least 1 letter.");
         }
         if (isTooLongCarNameLength(carName)) {
             throw new IllegalArgumentException("Car's name must be 5 characters or less.");
         }
+
         return true;
     }
 
@@ -22,7 +17,7 @@ public class NameCheckerImpl implements NameChecker {
         return carName.length() > 5;
     }
 
-    private boolean isEmptyName(String carName){
+    private boolean isEmptyName(String carName) {
         return carName.isEmpty();
     }
 }
