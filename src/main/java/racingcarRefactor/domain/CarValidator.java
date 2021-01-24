9557024b71d9bb61015eb1a5ee9a8b65812c2321
koeonyack <covenant.ko@kakao.com>
2021-01-24 18:43:27 +0900

    refactor: change argument type
    - ArrayList<String> -> String

--- src/main/java/racingcarRefactor/domain/CarValidator.java
+++ src/main/java/racingcarRefactor/domain/CarValidator.java
@@ -1,18 +1,18 @@
 package racingcarRefactor.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CarValidator {
     private static int MAX_CAR_NAME_LENGTH = 5;
     private static String INVALID_CAR_NAME_MESSAGE = "자동차 이름은 5글자가 넘을 수 없습니다.";
 
-    public static void carNameChecker(ArrayList<String> carNames) {
-        Boolean validNames = carNames.stream()
+    public static void carNameChecker(String carNames) {
+        Boolean validNames = Arrays.stream(carNames.split(","))
                 .allMatch(name -> name.length() <= MAX_CAR_NAME_LENGTH);
 
         if (!validNames) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_MESSAGE);
         }
     }
-
 }
