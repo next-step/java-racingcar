16ce807b15e82380559f7879a2d16c9e2c3c52c9
gyumin-kim <starkying@gmail.com>
2021-01-25 00:23:48 +0900

    test: `ConsoleOutput` 테스트 추가

---
+++ src/test/java/racingcar/view/ConsoleOutputTest.java
@@ -0,0 +1,41 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConsoleOutputTest {
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @DisplayName("")
+    @Test
+    void printOutput() {
+        // given
+        Output output = new ConsoleOutput();
+        String message = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+
+        // when
+        output.printOutput(message);
+
+        // then
+        assertThat(outputStreamCaptor.toString().trim()).isEqualTo(message);
+    }
+}
