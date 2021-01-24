febc1ea6013ba529d690bd9be775c205b077b3df
koeonyack <covenant.ko@kakao.com>
2021-01-24 15:50:12 +0900

    feat: create CarValidator

---
+++ src/main/java/racingcarRefactor/domain/CarValidator.java
@@ -0,0 +1,18 @@
+package racingcarRefactor.domain;
+
+import java.util.ArrayList;
+
+public class CarValidator {
+    private static int MAX_CAR_NAME_LENGTH = 5;
+    private static String INVALID_CAR_NAME_MESSAGE = "자동차 이름은 5글자가 넘을 수 없습니다.";
+
+    public static void carNameChecker(ArrayList<String> carNames) {
+        Boolean validNames = carNames.stream()
+                .allMatch(name -> name.length() <= MAX_CAR_NAME_LENGTH);
+
+        if (!validNames) {
+            throw new IllegalArgumentException(INVALID_CAR_NAME_MESSAGE);
+        }
+    }
+
+}
