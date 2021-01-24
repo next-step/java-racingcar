9af778202c35fe85e20de00037d30e571b9eed50
munkyung <munkyung0324@gmail.com>
2021-01-24 18:15:18 +0900

    refactor: create RacingGame class for manage Game status

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,2 +1,15 @@
-package racingcar.domain;public class Game {
+package racingcar.domain;
+
+public class RacingGame {
+
+    private int tryCnt = 0;
+
+    public RacingGame(int tryCnt) {
+        this.tryCnt = tryCnt;
+    }
+
+    public int getTryCnt() {
+        return tryCnt;
+    }
+    
 }
