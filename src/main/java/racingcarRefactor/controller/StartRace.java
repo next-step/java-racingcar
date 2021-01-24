4ef790a7442bb2fc61ec654b3ac46a84c7c89e32
koeonyack <covenant.ko@kakao.com>
2021-01-24 18:43:52 +0900

    feat: create StartRace

---
+++ src/main/java/racingcarRefactor/controller/StartRace.java
@@ -0,0 +1,31 @@
+package racingcarRefactor.controller;
+
+import racingcarRefactor.domain.Car;
+import racingcarRefactor.domain.CarValidator;
+import racingcarRefactor.domain.Game;
+import racingcarRefactor.domain.GenerateCar;
+import racingcarRefactor.view.InputView;
+import racingcarRefactor.view.ResultView;
+
+import java.util.List;
+
+public class StartRace {
+
+    public static void main(String[] args) {
+        String carNames = InputView.inputCarNames();
+        CarValidator.carNameChecker(carNames);
+        int laps = InputView.inputTotalLaps();
+        List<Car> cars = GenerateCar.ofCars(carNames);
+
+        Game game = new Game();
+        for (int i = 0; i < laps; i++) {
+            game.round(cars);
+            for (Car car : cars) {
+                ResultView.displayRound(car.getName(), car.getPosition());
+            }
+            ResultView.endOfRound();
+        }
+
+        ResultView.displayWinner(game.getWinners(cars));
+    }
+}
