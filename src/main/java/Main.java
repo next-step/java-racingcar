a5220da66f6cc682bc110cbc10985cea0a9c86b9
d36choi <yuio1126@gmail.com>
2021-01-24 15:32:17 +0900

    fix: MVC 패턴 리팩토링을 위한 메인 함수 설정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
 import racingcar.RacingGameController;
+import racingcar.view.RacingGameInputView;
+import racingcar.view.RacingGamePrinter;
 
 public class Main {
+
     public static void main(String[] args) {
-        RacingGameController racingGameController = new RacingGameController();
-        racingGameController.start();
+        String carNames = RacingGameInputView.getCarsName();
+        int round = RacingGameInputView.getRound();
+
+        RacingGameController racingGame = new RacingGameController(carNames, round);
+        while (!racingGame.isEnd()) {
+            racingGame.playRound();
+            RacingGamePrinter.printAllCarStatus(racingGame.getCars());
+        }
+        int maxStatus = racingGame.getMaxStatusAmongPlayers();
+        racingGame.findWinnerWithMaxStatus(maxStatus);
+        RacingGamePrinter.printWinners(racingGame.getWinners());
 
     }
 }
