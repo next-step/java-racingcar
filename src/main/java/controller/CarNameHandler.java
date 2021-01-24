7b8d45cd481029eaf6016f3f165c25a08609b22b
hanseokhyeon <sah0322@naver.com>
2021-01-24 18:12:03 +0900

    feat: CarNameHandler 클래스 작성

--- src/main/java/controller/CarNameHandler.java
+++ src/main/java/controller/CarNameHandler.java
@@ -1,4 +1,20 @@
 package controller;
 
 public class CarNameHandler {
+
+    public String[] splitCarNames(String carNames) {
+        return carNames.split(",");
+    }
+
+    public void checkCarNames(String[] carNames) {
+        for (String carName : carNames) {
+            checkEachCarName(carName);
+        }
+    }
+
+    private void checkEachCarName(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없다.");
+        }
+    }
 }
--- src/main/java/domain/CarFactory.java
+++ src/main/java/domain/CarFactory.java
@@ -1,10 +1,17 @@
 package domain;
 
+import controller.CarNameHandler;
+
 import java.util.ArrayList;
 
 public class CarFactory {
     public static ArrayList<Car> getCars(String carNames) {
         ArrayList<Car> cars = new ArrayList<>();
+
+        CarNameHandler carNameHandler = new CarNameHandler();
+        String[] carNamesSplit = carNameHandler.splitCarNames(carNames);
+        carNameHandler.checkCarNames(carNamesSplit);
+
         return cars;
     }
 }
