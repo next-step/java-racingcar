baf182dadb7f9f5a397d272dfd2b942ea2c88cc2
hakzzang <mbh930312@naver.com>
2021-01-24 20:45:40 +0900

    refactoring: Car를 만드는 로직을 Stream으로 변경

--- src/main/java/racingcar/di/CarFactoryImpl.java
+++ src/main/java/racingcar/di/CarFactoryImpl.java
@@ -1,22 +1,19 @@
 package racingcar.di;
 
 import racingcar.domain.Car;
-import racingcar.domain.Engine;
 import racingcar.domain.NormalEngine;
 import racingcar.domain.RacingCar;
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarFactoryImpl implements CarFactory{
-
     @Override
     public List<Car> makeCars(String[] names) {
-        List<Car> cars = new ArrayList<>();
-        Engine normalEngine = new NormalEngine();
-        for (String name : names) {
-            Car car = new RacingCar(name, normalEngine);
-            cars.add(car);
-        }
-        return cars;
+        return Arrays
+                .stream(names)
+                .map(name-> new RacingCar(name, new NormalEngine()))
+                .collect(Collectors.toList());
     }
 }
