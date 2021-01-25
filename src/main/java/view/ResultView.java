5aa6aec87f97c4d462adeb819562afc0d68ae074
hanseokhyeon <sah0322@naver.com>
2021-01-25 09:50:41 +0900

    fix: view에서 사용하던 상수들 static final로 변경

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -5,8 +5,12 @@ import domain.Car;
 import java.util.ArrayList;
 
 public class ResultView {
+    private static final String BEFORE_START_SIGN = "실행 결과";
+    private static final String STATE_CHARACTER = "-";
+    private static final String RESULT_SENTENCE = "최종 우승자: ";
+
     public static void printStartSign() {
-        System.out.println("실행 결과");
+        System.out.println(BEFORE_START_SIGN);
     }
 
     public static void printCars(ArrayList<Car> cars) {
@@ -19,13 +23,13 @@ public class ResultView {
     public static void printEachCar(Car car) {
         System.out.print(car.getName()+" : ");
         for (int i=0; i<car.getStatus(); i++) {
-            System.out.print("-");
+            System.out.print(STATE_CHARACTER);
         }
         System.out.println();
     }
 
     public static void printWinners(ArrayList<String> winners) {
-        System.out.println("최종 우승자: ");
+        System.out.println(RESULT_SENTENCE);
         String result = String.join(", ", winners);
         System.out.println(result);
     }
