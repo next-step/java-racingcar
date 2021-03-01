97eb189381a473e66947900c62db4813b9d45a3b
koola97620 <koola976@gmail.com>
2021-03-02 01:47:50 +0900

    feat: request3
    when charAt out of range string length
    then StringIndexOutOfBoundsException

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,14 @@ public class StringTest {
   void substring() {
     String result = "(1,2)".substring(1,4);
     assertThat(result).isEqualTo("1,2");
+  }
+
+  @DisplayName("요구사항3: charAt")
+  @Test
+  void charAt() {
+    String str = "abc";
+    assertThatThrownBy(() -> str.charAt(3))
+        .isInstanceOf(StringIndexOutOfBoundsException.class);
   }
 
 }
