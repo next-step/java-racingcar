7ddab76dac82027a9ce4e67241c8c28358de6aeb
cptbong <cptbbong@gmail.com>
2021-03-01 19:53:47 +0900

    덧셈테스트추가.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,5 @@
 public class StringCalculator {
+    public int add(int left, int right) {
+        return 0;
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,5 +1,39 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class StringCalculatorTest {
+    StringCalculator calculator;
+    @Test
+    @DisplayName("덧셈")
+    void add() {
+        assertThat(calculator.add(2,3)).isEqualTo(5);
+        assertThat(calculator.add(0,9)).isEqualTo(9);
+        assertThat(calculator.add(0,11)).isEqualTo(11);
+        assertThat(calculator.add(13,1)).isEqualTo(14);
+    }
 
+    @Test
+    @DisplayName("뺄셈")
+    void subtract() {
+    }
+
+    @Test
+    @DisplayName("곱셈")
+    void multiply() {
+
+    }
+
+    @Test
+    @DisplayName("나눗셈")
+    void divide() {
+
+    }
+
+    @BeforeEach
+    void createInstance() {
+        calculator = new StringCalculator();
+    }
 }
