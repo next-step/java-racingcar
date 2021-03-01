4d4aea87b1cdcd53ea08546117953110c432aa9f
wkdehdlr <wkdehdlr@gmail.com>
2021-03-01 22:04:13 +0900

    Racing step1 (#1829)
    * StringTest
    
    * SetTest

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("set 크기는 3")
+    void size() {
+        int setSize = numbers.size();
+        assertEquals(3, setSize);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("set에 해당 요소가 있으면 true 반환")
+    void contains(int input) {
+        assertTrue(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    @DisplayName("set에 해당 요소가 있으면 true, 없으면 false")
+    void contains(int input, boolean exp) {
+        boolean contain = numbers.contains(input);
+        assertEquals(exp, contain);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,56 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("'1,2'를 ','로 split하면 '1''2'를 반환")
+    void splitSeveralElements() {
+        assertSplit("1,2", Arrays.array("1", "2"), ",");
+    }
+
+    @Test
+    @DisplayName("'1'을 ','로 split하면 '1'을 반환")
+    void splitOneElement() {
+        assertSplit("1", Arrays.array("1"), ",");
+    }
+
+    @Test
+    @DisplayName("'(1,2)'에서 괄호를 제거하면 '1,2'를 반환")
+    void subString() {
+        String input = "(1,2)";
+        String substring = input.substring(1, 4);
+        assertEquals("1,2", substring);
+    }
+
+    @Test
+    @DisplayName("'abc'에서 각 인덱스 문자를 반환")
+    void chartAt() {
+        assertChartAt("abc", 'a', 0);
+        assertChartAt("abc", 'b', 1);
+        assertChartAt("abc", 'c', 2);
+    }
+
+    @Test
+    @DisplayName("문자열 인덱스를 벗어나면 StringIndexOutOfBoundsException 발생")
+    void charAtOutOfBounds() {
+        assertThatThrownBy(() -> {
+            assertChartAt("abc",'a',4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+    private void assertSplit(String input, String[] expStr, String delimiter) {
+        String[] splits = input.split(delimiter);
+        assertThat(splits).containsExactly(expStr);
+    }
+
+    private void assertChartAt(String input, char expChar, int index) {
+        char charAt = input.charAt(index);
+        assertEquals(expChar, charAt);
+    }
+}
