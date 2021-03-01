13347580b48032d73b970b4f2482e6463b542025
koola97620 <koola976@gmail.com>
2021-03-02 01:50:56 +0900

    feat: setTest request1
    assert set size

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,35 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author choijaeyong on 2021/03/02.
+ * @project java-racingcar
+ * @description
+ */
+public class SetTest {
+
+  private Set<Integer> numbers;
+
+  @BeforeEach
+  void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+
+  @DisplayName("요구사항1: size")
+  @Test
+  void size() {
+    assertThat(numbers.size()).isEqualTo(3);
+  }
+
+}
