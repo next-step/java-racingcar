fc07a37aa51af91e78231639050111effc3bd05e
Dion <ksundong@gmail.com>
2021-03-02 00:39:58 +0900

    refactor: 문자열 분리의 책임 분리

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
       throw new IllegalArgumentException("식을 입력해주세요.");
     }
 
-    String[] tokens = expression.split(" ");
+    String[] tokens = split(expression);
     String operator = tokens[1];
     int x = Integer.parseInt(tokens[0]);
     int y = Integer.parseInt(tokens[2]);
@@ -26,6 +26,10 @@ public class Calculator {
     }
 
     throw new IllegalArgumentException("해당 연산자는 지원하지 않습니다.");
+  }
+
+  private String[] split(String expression) {
+    return expression.split(" ");
   }
 
   private boolean isBlankString(String string) {
