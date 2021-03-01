f49017c1bafbf1056d62b914f644eb441bed3d0d
Dion <ksundong@gmail.com>
2021-03-02 00:10:20 +0900

    refactor: 중복 제거 및 읽기좋은 코드를 위해 변수명 추출

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -4,11 +4,14 @@ public class Calculator {
 
   public int calculate(String expression) {
     String[] tokens = expression.split(" ");
+    String operator = tokens[1];
+    int x = Integer.parseInt(tokens[0]);
+    int y = Integer.parseInt(tokens[2]);
 
-    if ("+".equals(tokens[1])) {
-      return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[2]);
+    if ("+".equals(operator)) {
+      return x + y;
     }
 
-    return Integer.parseInt(tokens[0]) - Integer.parseInt(tokens[2]);
+    return x - y;
   }
 }
