9b65530d627580649a1c5c87de75f0a86517fc97
juniq <juniqlim@gmail.com>
2021-03-01 18:54:01 +0900

    Set Collection study test : Requirement 1

---
+++ src/test/java/im/juniq/studytest/SetTest.java
@@ -0,0 +1,29 @@
+package im.juniq.studytest;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("Set 크기를 확인한다")
+	void testSize() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+}
