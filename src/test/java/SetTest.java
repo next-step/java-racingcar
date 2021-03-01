a8106f629aea9cab3310e0e5f15c355ca39e0ddd
cptbong <cptbbong@gmail.com>
2021-03-01 17:08:49 +0900

    테스트완료 - set테스트 완료.

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,7 +1,15 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -16,5 +24,26 @@ public class SetTest {
     }
 
     // Test Case 구현
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.\n")
+    void require1() {
+        assertEquals(4-1, numbers.size());
+    }
 
+    @ParameterizedTest
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.\n" +
+            "구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.\n" +
+            "JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.\n")
+    @ValueSource(ints = {1,2,3})
+    void require2(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.\n" +
+            "예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.\n")
+    @CsvSource(value= {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void require3(int num, boolean bool) {
+        assertEquals(numbers.contains(num), bool);
+    }
 }
