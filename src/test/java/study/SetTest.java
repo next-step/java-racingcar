5954a38e55487ddbf2a2a77091fc309a3f4d1bbf
yonghwa <qhdl0224@gmail.com>
2021-03-01 20:32:17 +0900

    displayname 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -25,6 +26,7 @@ public class SetTest {
     }
 
     @Test
+    @DisplayName("set 컬렉션 사이즈가 3인지 확인")
     void setTest() {
         assertThat(numbers.size()).isEqualTo(3);
     }
@@ -38,12 +40,14 @@ public class SetTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1,2,3})
+    @DisplayName("set 데이터들이 1,2,3인지 확인")
     void isSetContainsNumber(int number){
         assertThat(numbers.contains(number)).isTrue();
     }
 
     @ParameterizedTest
     @CsvSource(value={"1:true","2:true","3:true","4:false","5:false"},delimiter = ':')
+    @DisplayName("set 데이터가 1,2,3이면 true 4,5면 false인지 확인")
     void setContainsValueTrueAndFalse(int number,boolean expected){
         assertEquals(numbers.contains(number),expected);
     }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,12 +9,14 @@ public class StringTest {
 
 
     @Test
+    @DisplayName(",로 문자열 구분")
     void split() {
         String[] data = "1,2".split(",");
         assertThat(data).contains("1");
         assertThat(data).containsExactly("1","3");
     }
     @Test
+    @DisplayName("()를 제외한 문자열만 남기기")
     void substring() {
         String data = "(1,2)".substring(1,4);
         assertThat(data).contains("1,2");
