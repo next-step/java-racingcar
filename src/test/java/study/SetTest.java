f1d2a7138cba600d1b01a45787d33b65148c71ff
Dion <ksundong@gmail.com>
2021-03-01 18:48:22 +0900

    test: set에 4개의 요소를 넣고 size가 3인지 확인
    중복되는 요소를 집어넣음으로써 Set이 생각한대로 동작하는지 확인합니다.

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+
+  Set<Integer> numbers;
+
+  @BeforeEach
+  void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+
+  @Test
+  @DisplayName("만들어진 numbers의 크기는 3이 맞는지 확인")
+  void numbersSize() {
+    assertThat(numbers.size()).isEqualTo(3);
+  }
+}
