3ee6647cd9b620d6d56f6703f47e972e5e0a9d45
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-02 03:39:18 +0900

    TEST : add String charAt() and Exception Test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -15,5 +16,21 @@ public class StringTest {
     void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        char data = "abc".charAt(2);
+        assertThat(data).isEqualTo('c');
+    }
+
+    @DisplayName("charAt() 특정범위를 벗어날 경우")
+    @Test
+    void StringIndexOutOfBoundsExceptionTest() {
+
+        assertThatThrownBy(() -> {
+            char data = "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
