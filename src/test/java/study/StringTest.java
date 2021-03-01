20a192ec54b0d4e0f0fc057f7fe4f15e42f0d362
yonghwa <qhdl0224@gmail.com>
2021-03-01 19:25:42 +0900

    set collection study add

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,34 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
+
+
     @Test
-    void splitTest() {
+    void split() {
         String[] data = "1,2".split(",");
-        //assertThat(data).contains("1");
+        assertThat(data).contains("1");
         assertThat(data).containsExactly("1","3");
     }
+    @Test
+    void substring() {
+        String data = "(1,2)".substring(1,4);
+        assertThat(data).contains("1,2");
+    }
+
+    @Test
+    @DisplayName("String Index out of bounds exception test")
+    void charAt(){
+        String data = "abc";
+        assertThatThrownBy(()->{
+            data.charAt(1);
+            data.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+    }
+
 }
