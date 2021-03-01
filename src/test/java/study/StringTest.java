d20a62cc07fdace72b5d42c82ad382225463bb3b
Dion <ksundong@gmail.com>
2021-03-01 18:25:13 +0900

    refactor: assertAll을 이용한 테스트 통합으로 중복 제거

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,23 +9,21 @@ import org.junit.jupiter.api.Test;
 class StringTest {
 
   @Test
-  @DisplayName("actual이 1과 2를 포함하는지 테스트")
+  @DisplayName("1,2를 split한 값이 1과 2를 포함하는지, 순서대로 포함하는지 확인")
   void splitContains() {
     String[] actual = "1,2".split(",");
-    assertThat(actual).contains("1", "2");
-  }
 
-  @Test
-  @DisplayName("actual이 1과 2를 순서대로 포함하고 있는지 확인하는 테스트")
-  void splitContainsExactly() {
-    String[] actual = "1,2".split(",");
-    assertThat(actual).containsExactly("1", "2");
+    assertAll(
+        () -> assertThat(actual).contains("1", "2"),
+        () -> assertThat(actual).containsExactly("1", "2")
+    );
   }
 
   @Test
   @DisplayName("actual이 1을 포함하는지 테스트")
   void splitContainsOne() {
     String[] actual = "1".split(",");
+
     assertThat(actual).contains("1");
   }
 }
