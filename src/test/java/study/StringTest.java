1214d92689acdb72f76fc5daea42f128bdabd6f7
koola97620 <koola976@gmail.com>
2021-03-02 01:40:07 +0900

    feat: request2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,10 +11,18 @@ import org.junit.jupiter.api.Test;
  * @description
  */
 public class StringTest {
+  @DisplayName("요구사항1: split")
   @Test
   void split() {
     String[] result = "1,2".split(",");
     assertThat(result).containsExactly("1","2");
+  }
+
+  @DisplayName("요구사항2: substring")
+  @Test
+  void substring() {
+    String result = "(1,2)".substring(1,4);
+    assertThat(result).isEqualTo("1,2");
   }
 
 }
