f4a406aa1946015fdf98af4d0188265a0eb86e81
Gyunny <wjdrbs966@naver.com>
2021-03-01 23:01:26 +0900

    Test String split and substring test

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,22 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+
+    @Test
+    void substring() {
+        String name = "next step".substring(0, 4);
+        assertThat(name).isEqualTo("next");
+    }
+}
