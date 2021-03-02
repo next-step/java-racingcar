4cb3f9746a344cdb558f85c8e6567e16ef439c6c
Donghui-Lee <ehdgml3206@naver.com>
2021-03-02 19:05:28 +0900

    Requirements_2 commit

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -20,4 +20,11 @@ public class StringTest {
         assertThat(result).containsExactly("1");
     }
 
+    @Test
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현")
+    void substring() {
+        String testStr = "(1,2)";
+        String result = testStr.substring(1, testStr.length() - 1);
+        assertThat(result).contains("1,2");
+    }
 }
