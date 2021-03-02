308aed0752e25e2acd6ebf2544aff72451ebec3d
Donghui-Lee <ehdgml3206@gmail.com>
2021-03-02 23:34:35 +0900

    [Step1] 학습테스트 코드 리뷰 요청 드립니다. (#1852)
    * empty file remove
    
    * String Class Add
    
    * Requirements_1 commit
    
    * Requirements_2 commit
    
    * Requirements_3 commit
    
    * SetTest Class Add
    
    * Requirements_1 SetTest Class commit
    
    * Requirements_2 SetTest Class commit
    
    * Requirements_3 SetTest Class commit

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3 })
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현")
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false", "6:false"}, delimiter = ':')
+    @DisplayName("입력 값에 따라 결과 값이 다른 경우에 대한 테스트 구현")
+    void contains_VariousInputs(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
