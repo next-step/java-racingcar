6e33cbfb71973156b351e1c05853578005095e95
HeeSeok Noh <heeseoknoh@HeeSeoks-MacBook-Pro.local>
2021-03-02 19:02:48 +0900

    split study test

---
+++ src/test/java/test/StringTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StringTest {
+
+    @Test
+    void split(){
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1","2");
+
+    }
+
+}
