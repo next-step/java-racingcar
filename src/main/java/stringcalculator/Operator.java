7b28d129c7ea7f5420fc4f3082958a07c8517e8c
JuJinPark <wnwls1216@gmail.com>
2021-03-02 22:03:58 +0900

    [refactor] - Operator enum에 symbol에 맞는 적당한 operator찾는 책임 추가 - 불필요한 공백및 띄어쓰기 추가 - 매직넘버 제거 - 상수에 static 추가

--- src/main/java/stringcalculator/Operator.java
+++ src/main/java/stringcalculator/Operator.java
@@ -1,5 +1,9 @@
 package stringcalculator;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Operator {
 
     ADDITION("+"),
@@ -12,16 +16,25 @@ public enum Operator {
 
     private final String symbol;
 
+    private static final Map<String, Operator> lookup = new HashMap<String, Operator>();
+
+    static {
+        Arrays.stream(Operator.values())
+                .forEach(operator -> lookup.put(operator.getSymbol(),operator));
+    }
+
     Operator(String symbol) {
         this.symbol = symbol;
     }
-
 
     public String getSymbol() {
         return symbol;
     }
 
+    public static Operator getBySymbol(String symbol) {
 
+       if(!lookup.containsKey(symbol)) throw new IllegalArgumentException(symbol+" is not valid symbol");
 
-
+       return lookup.get(symbol);
+    }
 }
