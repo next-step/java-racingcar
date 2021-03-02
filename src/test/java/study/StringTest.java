0ecfd2c0ba2aa7ef93ef9a5399ef72f61bb9ae09
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-03-02 22:07:46 +0900

    1단계 요구사항 1 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,10 +7,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
-    @DisplayName("String 클래스 학습 테스트")
-    void split() {
-        String data = "1,2";
-        String[] result = data.split(",");
+    @DisplayName("요구사항 1")
+    void 요구사항1() {
+        String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
+
+        String[] result2 = "1".split(",");
+        assertThat(result2).contains("1");
     }
+
 }
