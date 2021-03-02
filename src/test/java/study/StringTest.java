1dd1cc54c003b730fc18800f3c9bc50785770249
Donghui-Lee <ehdgml3206@naver.com>
2021-03-02 19:32:29 +0900

    Requirements_3 commit

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,5 +27,24 @@ public class StringTest {
         String testStr = "(1,2)";
         String result = testStr.substring(1, testStr.length() - 1);
         assertThat(result).contains("1,2");
+    }
+
+    @Test
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현")
+    void charAt() {
+        String testStr = "abc";
+        assertAll(() -> assertThat(testStr.charAt(0)).isEqualTo('a'),
+                  () -> assertThat(testStr.charAt(1)).isEqualTo('b'),
+                  () -> assertThat(testStr.charAt(2)).isEqualTo('c'));
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현")
+    void charAt_StringIndexOutOfBoundsException() {
+        String testStr = "abc";
+
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> testStr.charAt(7))
+            .withMessageMatching("String index out of range: \\d+");
     }
 }
