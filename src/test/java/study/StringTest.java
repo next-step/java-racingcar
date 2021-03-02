d11857a29eb4a4026fd1470b52708424ea928117
Donghui-Lee <ehdgml3206@naver.com>
2021-03-02 18:52:05 +0900

    Requirements_1 commit

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,23 @@
 package study;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class StringTest {
+    @Test
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현")
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1","2");
+    }
+
+    @Test
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현")
+    void splitExactlyContains() {
+        String[] result = "1,".split(",");
+        assertThat(result).containsExactly("1");
+    }
 
 }
