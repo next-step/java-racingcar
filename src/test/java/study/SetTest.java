90b21c73df6c535610e20c5b626e431def47eff0
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-02 21:08:57 +0900

    학습 테스트 실습 완료

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,2 +1,50 @@
-package study;public class SetTest {
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("요구사항 1 - Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
+    void size() {
+       assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "Set 안에 {0}가 있는지 확인하는 테스트")
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("요구사항 2 - JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.")
+    void contains(int param) {
+        assertThat(numbers.contains(param)).isTrue();
+    }
+
+    @ParameterizedTest(name = "Set 안에 {0}가 있는지 확인하는 테스트")
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    @DisplayName("요구사항 3 - 요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다." +
+            "예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.")
+    void contains(Integer input, boolean expected) {
+        boolean actualValue = numbers.contains(input);
+        assertThat(actualValue).isEqualTo(expected);
+    }
+
+
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,2 +1,62 @@
-package study;public class StringTest {
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class StringTest {
+    @Test
+    @DisplayName("요구사항1 - 1,2을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
+    void splitToComma() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+        assertThat(result).contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("요구사항1 - 1을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.")
+    void split() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    @DisplayName("요구사항2 - (1,2) 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현한다.")
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("요구사항3 - abc 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
+    void charAt() {
+        assertThat("abc".charAt(0)).isEqualTo('a');
+        assertThat("abc".charAt(1)).isEqualTo('b');
+        assertThat("abc".charAt(2)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("요구사항 3 - String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면" +
+            " StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
+    void exceptionTest() {
+        String abc = "abc";
+        int Index = 5;
+
+        //assertThatThrownBy 사용시
+        assertThatThrownBy(() -> {
+            abc.charAt(Index);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: " + Index);
+
+        //assertThatExceptionOfType 사용시
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    abc.charAt(Index);
+                }).withMessageMatching("String index out of range: " + Index);
+    }
+
+
 }
