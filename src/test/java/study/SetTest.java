b87e026e5fa5bb32d1a60628bae724d0e2275d80
Donghui-Lee <ehdgml3206@naver.com>
2021-03-02 19:48:15 +0900

    Requirements_2 SetTest Class commit

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -26,5 +28,12 @@ public class SetTest {
     @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현")
     void size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3 })
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현")
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
