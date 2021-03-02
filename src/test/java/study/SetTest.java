8e68de12ba2a13adaadf211c09ec314bb0aa6b4c
Donghui-Lee <ehdgml3206@naver.com>
2021-03-02 19:41:06 +0900

    Requirements_1 SetTest Class commit

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,13 @@
 package study;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -15,5 +19,12 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
     }
 }
