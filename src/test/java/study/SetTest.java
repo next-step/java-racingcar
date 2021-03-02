fde681570cd6e230c13d434ce2d602d319b39280
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-03-03 00:38:46 +0900

    DisplayName 수정

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -27,13 +27,13 @@ public class SetTest {
     }
 
     @Test
-    @DisplayName("요구사항 1")
+    @DisplayName("size() 사용하여 Set 크기 확인 테스트")
     void 요구사항1() {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
     @Test
-    @DisplayName("요구사항 2")
+    @DisplayName("contains() 사용하여 1,2,3의 값이 존재하는지 테스트")
     void 요구사항2() {
         assertThat(numbers.contains(1)).isTrue();
         assertThat(numbers.contains(2)).isTrue();
@@ -42,14 +42,12 @@ public class SetTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1,2,3})
-    @DisplayName("요구사항 2-2")
     void 요구사항2_2(int num) {
         assertTrue(numbers.contains(num));
     }
 
     @ParameterizedTest
     @CsvSource(value={"1:true","2:true","3:true","4:false","5:false"}, delimiter = ':')
-    @DisplayName("요구사항 3")
     void 요구사항3(int num, boolean expected) {
         assertThat(numbers.contains(num)).isEqualTo(expected);
     }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,17 +7,21 @@ import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
-    @DisplayName("요구사항 1")
-    void 요구사항1() {
+    @DisplayName("1,2를 ,로 split 하여 잘 분리되는지 테스트")
+    void 요구사항1_1() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
+    }
 
+    @Test
+    @DisplayName("1을 ,로 split 했을때 1만을 포함하는 배열이 반환되는지 테스트")
+    void 요구사항1_2() {
         String[] result2 = "1".split(",");
         assertThat(result2).contains("1");
     }
 
     @Test
-    @DisplayName("요구사항 2")
+    @DisplayName("(1,2) 주어졌을때, substring() 활용하여 1,2로 반환되는지 테스트")
     void 요구사항2() {
         String data = "(1,2)";
         String result = data.substring(0,data.length());
@@ -25,7 +29,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("요구사항 3-1")
+    @DisplayName("abc 값이 주어졌을때, charAt() 활용하여 특정 위치 문자 가져오는 테스트")
     void 요구사항3_1() {
         String data = "abc";
         char result = data.charAt(0);
@@ -33,7 +37,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("요구사항 3-2")
+    @DisplayName("StringIndexOutOfBoundsException 발생하는 테스트")
     void 요구사항3_2() {
         String data = "abc";
         assertThatThrownBy(() -> {
