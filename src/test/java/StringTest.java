b8c719f8b9ce3449be29b8f28a9cc087c7432da5
keke0123 <keke0123@naver.com>
2021-03-02 20:51:09 +0900

    요구사항1 완료

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 // 
 import static org.assertj.core.api.Assertions.*;
@@ -5,9 +6,18 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
 
   @Test
-  public void splitTest() {
+  @DisplayName("'1,2' 로 split 시 1, 2로 잘 분리되는지 테스트")
+  public void splitTest1() {
     String[] str = "1,2".split(",");
     assertThat(str).contains("1");
     assertThat(str).containsExactly("1", "2");
+  }
+
+  @Test
+  @DisplayName("'1' 로 split 시 1만 포함하는 배열이 반환되는지 테스트")
+  public void splitTest2() {
+    String[] str = "1".split(",");
+    assertThat(str).contains("1");
+    assertThat(str).doesNotContain("2");
   }
 }
