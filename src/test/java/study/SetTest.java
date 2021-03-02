0affd5972ce8376fe5ef54ece5595fe1e22285a8
wooyonghwa <60569346+wooyonghwa@users.noreply.github.com>
2021-03-02 11:27:53 +0900

    안녕하세요 (#1841)
    * split study test
    
    * set collection study add
    
    * displayname 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,54 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("set 컬렉션 사이즈가 3인지 확인")
+    void setTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    void contains(){
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("set 데이터들이 1,2,3인지 확인")
+    void isSetContainsNumber(int number){
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value={"1:true","2:true","3:true","4:false","5:false"},delimiter = ':')
+    @DisplayName("set 데이터가 1,2,3이면 true 4,5면 false인지 확인")
+    void setContainsValueTrueAndFalse(int number,boolean expected){
+        assertEquals(numbers.contains(number),expected);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,36 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+
+    @Test
+    @DisplayName(",로 문자열 구분")
+    void split() {
+        String[] data = "1,2".split(",");
+        assertThat(data).contains("1");
+        assertThat(data).containsExactly("1","3");
+    }
+    @Test
+    @DisplayName("()를 제외한 문자열만 남기기")
+    void substring() {
+        String data = "(1,2)".substring(1,4);
+        assertThat(data).contains("1,2");
+    }
+
+    @Test
+    @DisplayName("String Index out of bounds exception test")
+    void charAt(){
+        String data = "abc";
+        assertThatThrownBy(()->{
+            data.charAt(1);
+            data.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+    }
+
+}
