8d30c50a6b4ff43384f2979410e7f37f06022e39
Beenie93 <seongbeen93@naver.com>
2021-03-04 01:07:31 +0900

    test: check if a set contains number with @ParameterizedTest

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,12 @@ public class SetTest {
     @DisplayName("요구사항 1 : Set의 size() 메소드를 활용해 Set의 크기를 확인하는 테스트")
     void checkSizeOfSet() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("요구사항 2 : Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 테스트")
+    void containNumbers(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
