44dd0a35c27d92220ec5b9e0f93ead0c159a6232
Younghyun <y.jo@navercorp.com>
2021-03-03 12:57:46 +0900

    test:DisplayName 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
+    @DisplayName("문자열(String)과 구분자(Delim)이 있을 때, 문자열이 구분자 기준으로 잘 분리 되는지 테스트")
     void Given_StrAndDelim_When_Split_Then_StrSplitted() {
         //given
         String s = "1,2";
@@ -21,6 +23,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("괄호()에 쌓여진 문자열에서 괄호가 없어지는지 테스트")
     void Given_BracketStr_When_Substring_Then_BracketDeleted() {
         //givein
         String s = "(1,2)";
@@ -33,6 +36,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열에서 특정 위치의 문자가 잘 반환 되는지 테스트")
     void Given_Str_Abc_ValidIndex_When_CharAt_Then_ChatAtSpecificPosition() {
         //given
         String s = "abc";
@@ -46,6 +50,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열에서 인덱스 밖의 문자에 접근했을 때 StringIndexOutOfBoundsException 발생 테스트")
     void Given_Str_Abd_InvalidIndex_When_CharAt_Then_StringIndexOutOfBoundsException() {
         //given
         String s = "abc";
