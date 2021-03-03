15f2d5e2852b4d996d5b012ffacbe97b8c344e66
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-04 00:44:17 +0900

    TEST : 사칙연산별 테스트 코드 추가 및 수정

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -16,19 +16,71 @@ public class StringCalculatorTest {
     int result = 0;
     int temp = 0;
 
+    @DisplayName("연산에 필요한 문자열 자르기")
+    @BeforeEach
+    @Test
+    void split_user_input() {
+        for (int i = 0; i < data.length; i++) {
+            if (!Character.isDigit(data[i].charAt(0))) {
+                result = Integer.parseInt(data[i - 1]) + Integer.parseInt(data[i + 1]);
+            }
+        }
+    }
 
     @DisplayName("단순 덧셈 테스트")
     @Test
     void simple_addition_test() {
-        String[] data = "2 + 5".split(" ");
-        int result = 0;
+        String[] data = "5 + 3 ".split(" ");
 
+        int addResult = 0;
         for (int i = 0; i < data.length; i++) {
             if (!Character.isDigit(data[i].charAt(0))) {
-                result = Integer.parseInt(data[i-1]) + Integer.parseInt(data[i+1]);
+                addResult = Integer.parseInt(data[i - 1]) + Integer.parseInt(data[i + 1]);
             }
         }
-        assertThat(result).isEqualTo(7);
+        assertThat(addResult).isEqualTo(8);
+    }
+
+    @DisplayName("단순 뺄셈 테스트")
+    @Test
+    void simple_substraction_test() {
+        String[] data = "5 - 3 ".split(" ");
+
+        int subResult = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (!Character.isDigit(data[i].charAt(0))) {
+                subResult = Integer.parseInt(data[i - 1]) - Integer.parseInt(data[i + 1]);
+            }
+        }
+        assertThat(subResult).isEqualTo(2);
+    }
+
+    @DisplayName("단순 곱하기 테스트")
+    @Test
+    void simple_multiplication_test() {
+        String[] data = "5 * 3 ".split(" ");
+
+        int mulResult = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (!Character.isDigit(data[i].charAt(0))) {
+                mulResult = Integer.parseInt(data[i - 1]) * Integer.parseInt(data[i + 1]);
+            }
+        }
+        assertThat(mulResult).isEqualTo(15);
+    }
+
+    @DisplayName("단순 나누기 테스트")
+    @Test
+    void simple_division_test() {
+        String[] data = "10 / 2 ".split(" ");
+
+        int divResult = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (!Character.isDigit(data[i].charAt(0))) {
+                divResult = Integer.parseInt(data[i - 1]) / Integer.parseInt(data[i + 1]);
+            }
+        }
+        assertThat(divResult).isEqualTo(5);
     }
 
     @Test
@@ -39,7 +91,7 @@ public class StringCalculatorTest {
 
         for (int i = 0; i < data.length; i++) {
             if (!Character.isDigit(data[i].charAt(0))) {
-                result = Integer.parseInt(data[i-1]) + Integer.parseInt(data[i+1]);
+                result = Integer.parseInt(data[i - 1]) + Integer.parseInt(data[i + 1]);
             }
         }
         assertThat(result).isEqualTo(173);
@@ -53,7 +105,7 @@ public class StringCalculatorTest {
 
         for (int i = 0; i < data.length; i++) {
             if (!Character.isDigit(data[i].charAt(0))) {
-                result = Integer.parseInt(data[i-1]) / Integer.parseInt(data[i+1]);
+                result = Integer.parseInt(data[i - 1]) / Integer.parseInt(data[i + 1]);
             }
         }
         assertThat(result).isEqualTo(10);
@@ -67,7 +119,7 @@ public class StringCalculatorTest {
 
         for (int i = 0; i < data.length; i++) {
             if (!Character.isDigit(data[i].charAt(0))) {
-                operation(data[i], data[i+1]);
+                operation(data[i], data[i + 1]);
             }
         }
         assertThat(result).isEqualTo(5);
@@ -91,12 +143,12 @@ public class StringCalculatorTest {
             case "/":
                 temp /= Integer.parseInt(value2);
                 break;
+            default:
+                throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
         }
 
         result = temp;
 
     }
-
-
 
 }
