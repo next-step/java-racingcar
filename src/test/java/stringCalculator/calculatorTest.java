0eaedad9cba989007b044f1d7e0dbb327b5ad53a
koola97620 <koola976@gmail.com>
2021-03-04 00:22:56 +0900

    test: 문자열계산기 - 덧셈 테스트 케이스 추가

--- src/test/java/stringCalculator/calculatorTest.java
+++ src/test/java/stringCalculator/calculatorTest.java
@@ -3,21 +3,20 @@ package stringCalculator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-/**
- * @author choijaeyong on 2021/03/03.
- * @project java-racingcar
- * @description
- */
+
 public class calculatorTest {
 
   @DisplayName("덧셈 테스트")
-  @Test
-  void plus() {
+  @ParameterizedTest
+  @CsvSource(value = {"1,2:3", "2,3:5", "3,4:7", "6,8:14", "12,15:27", "11,20:31"}, delimiter = ':')
+  void plus(String input, String expected) {
     Calculator calculator = new Calculator();
-    int result = calculator.plus(2, 3);
-    assertThat(result).isEqualTo(5);
+    String[] inputNumbers = input.split(",");
+    int result = calculator.plus(Integer.parseInt(inputNumbers[0]), Integer.parseInt(inputNumbers[1]));
+    assertThat(result).isEqualTo(Integer.parseInt(expected));
   }
 
 }
