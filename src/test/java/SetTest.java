46dc326c806da0a4b312c26fc42855a375adf239
Hoony(JeongByeongHun) <jbh1024@gmail.com>
2021-03-03 19:41:49 +0900

    Step1 (#1872)
    * String Test Commit
    
    * Set Test Commit
    
    Co-authored-by: Jeong Byeong Hun <bhjeong@rsupport.com>

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    @DisplayName("Set Size check Test")
+    void size(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Set contails number test(1,2,3) with parameterized")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contains(int input) {
+        assertTrue(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
