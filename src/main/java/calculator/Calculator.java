eae0dec278d0036832bc96bd5adcfb6e26de4a65
meme2367 <meme91322367@gmail.com>
2021-03-03 12:48:52 +0900

    step2 Calculator 클래스 코드 작성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,47 @@
 package calculator;
 
+import java.util.*;
+
 public class Calculator {
 
     public int calculate(String[] data) {
-        /*String[] data = {"4","+","2"};
-        assertEquals(6,calculator.calculate(data));
-        */
 
-        data[1];
+        int numberOne = Integer.parseInt(data[0]);
+        for(int i = 1; i< data.length; i = i + 2) {
+            String operator = data[i];
+            int numberTwo = Integer.parseInt(data[i+1]);
+            numberOne = detailCalculate(numberOne,operator,numberTwo);
+        }
 
-        return 0;
+        return numberOne;
     }
+
+    private int detailCalculate(int numberOne, String operator, int numberTwo) {
+        int answer = 0;
+        switch(operator) {
+            case "+" : answer = plusCalculate(numberOne,numberTwo);
+            case "-" : answer = minusCalculate(numberOne,numberTwo);
+            case "*" : answer = multiplyCalculate(numberOne,numberTwo);
+            case "/" : answer = devideCalculate(numberOne,numberTwo);
+        }
+
+        return answer;
+    }
+
+    private int devideCalculate(int numberOne, int numberTwo) {
+        return numberOne / numberTwo;
+    }
+
+    private int multiplyCalculate(int numberOne, int numberTwo) {
+        return numberOne * numberTwo;
+    }
+
+    private int minusCalculate(int numberOne, int numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    private int plusCalculate(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+
 }
