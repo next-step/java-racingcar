7f6cfc0822f6c32cec04bc1af55f321ea3abc24b
HeeSeok Noh <heeseoknoh@HeeSeoks-MacBook-Pro.local>
2021-03-03 19:31:54 +0900

    Test미션 완료

--- src/test/java/test/StringTest.java
+++ src/test/java/test/StringTest.java
@@ -1,8 +1,11 @@
 package test;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 
 public class StringTest {
 
@@ -11,6 +14,24 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
 
+    }
+
+    @Test
+    void removeParenthesis(){
+        String str = "(1,2)";
+        String result = str.substring(1,4);
+        assertThat(result).contains("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() Method 범위 테스트")
+    void getChar(){
+        assertThatThrownBy(() -> {
+            String str = "abc";
+            char result = str.charAt(10);
+            assertThat(result).isEqualTo('b');
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 10");
     }
 
 }
