097d50bd4d1a3e63542016254775000f8b98e397
juniq <juniqlim@gmail.com>
2021-03-03 23:02:42 +0900

    리팩토링

--- src/main/java/im/juniq/stringcalculator/Calculator.java
+++ src/main/java/im/juniq/stringcalculator/Calculator.java
@@ -6,7 +6,12 @@ public class Calculator {
 
 	public Calculator(String input) {
 		this.input = input;
+	}
+
+	public int run() {
 		validInputIsNullAndBlank();
+		String[] terms = extractToTerms(input);
+		return calculateEachTerm(terms);
 	}
 
 	private void validInputIsNullAndBlank() {
@@ -15,28 +20,61 @@ public class Calculator {
 		}
 	}
 
-	public int run() {
-		String[] terms = input.split(SEPARATOR);
-		int result = Integer.valueOf(terms[0]);
+	private String[] extractToTerms(String input) {
+		return input.split(SEPARATOR);
+	}
+
+	private int calculateEachTerm(String[] terms) {
+		int criterion = Integer.valueOf(terms[0]);
 		for (int i = 1; i < terms.length - 1; i += 2) {
-			result = calculate(result, terms[i], Integer.valueOf(terms[i + 1]));
+			criterion = calculate(criterion, terms[i], Integer.valueOf(terms[i + 1]));
 		}
-		return result;
+		return criterion;
 	}
 
 	private int calculate(int leftNumber, String operator, int rightNumber) {
-		if ("+".equals(operator)) {
-			return Integer.valueOf(leftNumber) + Integer.valueOf(rightNumber);
+		return Operator.findByNotation(operator).calculate(leftNumber, rightNumber);
+	}
+
+	public enum Operator {
+		addition("+"){
+			@Override
+			public int calculate(int leftNumber, int rightNumber) {
+				return leftNumber + rightNumber;
+			}
+		},
+		subtraction("-"){
+			@Override
+			public int calculate(int leftNumber, int rightNumber) {
+				return leftNumber - rightNumber;
+			}
+		},
+		multiplication("*"){
+			@Override
+			public int calculate(int leftNumber, int rightNumber) {
+				return leftNumber * rightNumber;
+			}
+		},
+		division("/"){
+			@Override
+			public int calculate(int leftNumber, int rightNumber) {
+				return leftNumber / rightNumber;
+			}
+		};
+
+		private String notation;
+		public abstract int calculate(int leftNumber, int rightNumber);
+
+		Operator(String notation) {
+			this.notation = notation;
 		}
-		if ("-".equals(operator)) {
-			return Integer.valueOf(leftNumber) - Integer.valueOf(rightNumber);
+
+		public static Operator findByNotation(String notation) {
+			for (Operator operator: Operator.values()) {
+				if(operator.notation.equals(notation))
+					return operator;
+			}
+			throw new IllegalArgumentException();
 		}
-		if ("*".equals(operator)) {
-			return Integer.valueOf(leftNumber) * Integer.valueOf(rightNumber);
-		}
-		if ("/".equals(operator)) {
-			return Integer.valueOf(leftNumber) / Integer.valueOf(rightNumber);
-		}
-		throw new IllegalArgumentException();
 	}
 }
