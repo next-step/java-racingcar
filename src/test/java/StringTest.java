c417becf6b16012a258d91f6ede60a72b1ee7574
keke0123 <40752137+keke0123@users.noreply.github.com>
2021-03-03 17:46:45 +0900

    Step1 (#1855)
    * version 문제로 다른 버전 사용
    
    * gitignore 에 추가 / history, vscode 관련
    
    * step1 init
    
    * 요구사항1 완료
    
    * 요구사항 1, 2, 3 완료

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+// 
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+  @Test
+  @DisplayName("'1,2' 로 split 시 1, 2로 잘 분리되는지 테스트")
+  public void splitTest1() {
+    String[] str = "1,2".split(",");
+    assertThat(str).contains("1");
+    assertThat(str).containsExactly("1", "2");
+  }
+
+  @Test
+  @DisplayName("'1' 로 split 시 1만 포함하는 배열이 반환되는지 테스트")
+  public void splitTest2() {
+    String[] str = "1".split(",");
+    assertThat(str).contains("1");
+    assertThat(str).doesNotContain("2");
+  }
+
+  @Test
+  @DisplayName("'(1,2)' 값이 주어졌을 때 String 의 substring() 매소드를 활용해 () 을 제거하고 '1,2' 를 반환하도록 테스트")
+  public void substringTest1() {
+    String str = "(1,2)".substring(1, 4);
+    assertThat(str).contains("1, 2");
+  }
+
+  @Test
+  @DisplayName("abc 값이 주어졌을때 String 의 CharAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습을 테스트")
+  public void charAtTest1() {
+    char c = "abc".charAt(0);
+    assertThat(c).isEqualTo('a');
+  }
+
+  @Test
+  @DisplayName("charAt() 메소드 사용시 위치 값을 벗어나면 StringIndexOutOfBoundsException 이 발생하는 부분 테스트")
+  public void charAtTest2() {
+    assertThatThrownBy(() -> {
+      "abc".charAt(3);
+    }).isInstanceOf(StringIndexOutOfBoundsException.class).hasMessageContaining("String index out of range");
+
+  }
+}
