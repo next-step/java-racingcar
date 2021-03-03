0fc44f8b5f3daebea49ed58149c51377c6b9bea0
JuJinPark <wnwls1216@gmail.com>
2021-03-04 00:04:36 +0900

    [feat] added RandomNumberGenerator to implement RacingCar

---
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import java.util.Random;
+
+public class RandomNumberGenerator implements NumberGenerator{
+
+    private int maxValue;
+
+    private int minValue;
+
+    public RandomNumberGenerator(int maxValue, int minValue) {
+        this.maxValue = maxValue;
+        this.minValue = minValue;
+    }
+
+    @Override
+    public int generate() {
+        return new Random().nextInt(maxValue+1-minValue) + minValue;
+    }
+}
