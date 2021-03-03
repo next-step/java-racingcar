7c2d0454c8b999140364a6ade1b4a1d1628eeeaf
seokrae <kslbsh@gmail.com>
2021-03-03 13:21:28 +0900

    [feat] - init StringCalculator

---
+++ src/main/java/step2/StringCalculator.java
@@ -0,0 +1,9 @@
+package step2;
+
+/**
+ * 문자열 사칙 연산 계산기
+ *
+ */
+public class StringCalculator {
+
+}
---
+++ src/test/java/step2/StringCalculatorTest.java
@@ -0,0 +1,15 @@
+package step2;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * 문자열 사칙 연산 계산기 테스트
+ */
+class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+}
