a27d326fdab8f9a46207932e9046788a1ba59f56
seokrae <kslbsh@gmail.com>
2021-03-04 14:08:32 +0900

    [refactor] - 사칙연산 외에 문자열 예외처리 변경 중에 코드 구조 변경전 커밋

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -1,7 +1,6 @@
 package step2;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * 문자열 사칙 연산 계산기
@@ -9,49 +8,142 @@ import java.util.List;
 public class StringCalculator {
 
     private static final String NUMBERS_REGEXP = "[0-9]";
+    private static final String OPERATOR_REGEXP = "[+|-|*|/]";
     private static final String EMPTY_SPACE = "";
+    private static final String WHITESPACE_DELIMITER = "\\s";
 
-    List<String> operations = Arrays.asList("+", "-", "*", "/");
+    private static final List<String> FOUR_RULE_CALCULATION = Arrays.asList("+", "-", "*", "/");
 
+    public static final String PLUS_EXPRESSION = "+";
+    public static final String SUBT_EXPRESSION = "-";
+    public static final String MULT_EXPRESSION = "*";
+    public static final String DIVI_EXPRESSION = "/";
+
+    public static final String DIVIDE_BY_ZERO = "divide by zero";
+    public static final String NOT_MATCH_FOUR_RULE_CALCULATION = "사칙 연산 문자열에 포함하지 않은 문자열이 존재합니다.";
+
+    private final Queue<Integer> operationValue = new LinkedList<>();
+    private final Queue<String> operationExpressions = new LinkedList<>();
+
+    private static final Integer ZERO = 0;
+
+    /**
+     * 유효성 검사 -> 사용자 입력 데이터 가공 -> 반복적인 연산 -> 결과
+     *
+     * @param input 사용자의 데이터 입력 값
+     * @return 사칙연산 최종 결과 값
+     */
     public int apply(String input) {
-        // 입력시 유효성 검사 체크
-        if (isBlank(input) || !isValidSymbol(input)) {
+        if (isBlank(input)) {
             throw new IllegalArgumentException();
         }
 
-        // 데이터 가공을 할건데
-        String[] split = input.split("");
-        for( String item : split) {
-            System.out.println(item);
+        dataExtractAndConvertProcess(input);
+
+        // 연속적인 연산 과정 처리
+        Integer result = operationValue.poll();
+
+        while (!operationValue.isEmpty()) {
+            int nextValue = operationValue.poll();
+            String operation = operationExpressions.poll();
+
+            result = forLoopOperation(result, nextValue, operation);
+        }
+        return result;
+    }
+
+    private void dataExtractAndConvertProcess(String input) {
+        String[] operationValues = getSplit(input, OPERATOR_REGEXP, EMPTY_SPACE, WHITESPACE_DELIMITER);
+        String[] operationExpressions = getSplit(input, NUMBERS_REGEXP, EMPTY_SPACE, WHITESPACE_DELIMITER);
+
+        operationValues = deleteEmptyBucket(operationValues);
+        operationExpressions = deleteEmptyBucket(operationExpressions);
+
+        for (String s : operationExpressions) {
+            this.operationExpressions.offer(s);
         }
 
-        return 0;
+        for (String s : operationValues) {
+            this.operationValue.offer(Integer.parseInt(s));
+        }
+    }
+
+    private String[] deleteEmptyBucket(String[] originData) {
+        List<String> tmpList = new ArrayList<>();
+        for (String item : originData) {
+            tmpList.add(isEmpty(item));
+        }
+        return tmpList.toArray(new String[ZERO]);
+    }
+
+    // 연산자
+    private int forLoopOperation(Integer result, Integer operationValue, String operator) {
+        if(!FOUR_RULE_CALCULATION.contains(operator)) {
+            throw new IllegalArgumentException("사칙 연산 외에 문자가 포함되어 있습니다.");
+        }
+        if (PLUS_EXPRESSION.equals(operator)) {
+            result = plus(result, operationValue);
+        }
+        if (SUBT_EXPRESSION.equals(operator)) {
+            result = subtract(result, operationValue);
+        }
+        if (MULT_EXPRESSION.equals(operator)) {
+            result = multiply(result, operationValue);
+        }
+        if (DIVI_EXPRESSION.equals(operator)) {
+            result = divide(result, operationValue);
+        }
+        return result;
     }
 
     // operation
-    public int plus(int a, int b) {
+    public Integer plus(Integer a, Integer b) {
         return a + b;
     }
-
-    public int subtract(int minuend, int subtrahend) {
+    public Integer subtract(Integer minuend, Integer subtrahend) {
         return minuend - subtrahend;
     }
-
-    public int multiply(Integer multiplicand, Integer multiplier) {
+    public Integer multiply(Integer multiplicand, Integer multiplier) {
         return multiplicand * multiplier;
     }
 
-    public int divide(Integer dividend, Integer divisor) {
+    public Integer divide(Integer dividend, Integer divisor) {
+        if (dividend.equals(ZERO)) {
+            throw new ArithmeticException(DIVIDE_BY_ZERO);
+        }
         return dividend / divisor;
     }
 
+    // 배열 데이터 Split 처리
+    public String[] getSplit(String origin, String regex, String completed, String delimiter) {
+        return origin.replaceAll(regex, completed).split(delimiter);
+    }
+
     // validation
-    public boolean isValidSymbol(String input) {
-        String s = input.replaceAll(NUMBERS_REGEXP, EMPTY_SPACE);
-        return operations.contains(s.trim());
+    public boolean isContainsSymbol(String input) { // 주어진 문자열의 문자가 사칙연산에 속하는지 여부 판단
+        String[] s = getSplit(input, NUMBERS_REGEXP, EMPTY_SPACE, WHITESPACE_DELIMITER);
+        for (String item : s) {
+            checkOperator(item);
+        }
+        return true;
+    }
+
+    // 문자열 값만 확인하여 사칙연산자에 포함되어 있는지 확인
+    private String checkOperator(String item) {
+        if(!isBlank(item) && !FOUR_RULE_CALCULATION.contains(item)) {
+            throw new IllegalArgumentException(NOT_MATCH_FOUR_RULE_CALCULATION);
+        }
+        return item;
     }
 
     public boolean isBlank(String input) {
-        return input == null || input.trim().isEmpty();
+        return Objects.isNull(input) || input.trim().isEmpty();
+    }
+
+    private String isEmpty(String item) {
+        if(!item.isEmpty()) {
+            return item;
+        }
+        return null;
     }
 }
--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -2,7 +2,7 @@ package step2;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -11,18 +11,42 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 /**
  * 문자열 사칙 연산 계산기 테스트
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StringCalculatorTest {
 
-    private static Stream<Arguments> blankStrings() {
+    private static final String NUMBERS_REGEXP = "[0-9]";
+    private static final String NOT_NUMBERS_REGEXP = "[+|-|*|/]";
+    private static final String EMPTY_SPACE = "";
+    private static final String WHITESPACE_DELIMITER = "\\s";
+
+    private Stream<Arguments> blankStrings() {
         return Stream.of(
-                Arguments.of(null, true),
-                Arguments.of("", true),
-                Arguments.of(" ", true)
+                Arguments.of(null, true), Arguments.of("", true), Arguments.of(" ", true)
+        );
+    }
+
+    private Stream<String> invalidSymbolStrings() {
+        return Stream.of("2 ^ 1", "3 & 2", "4 % 2", "6 $ 1");
+    }
+
+    private Stream<Arguments> validSymbolStrings() {
+        return Stream.of(
+                Arguments.of("2 + 3 * 4 / 2", 10),
+                Arguments.of("2 * 3 / 4 - 2", -1),
+                Arguments.of("2 + 3 / 5 * 8", 8),
+                Arguments.of("2 - 3 + 4 * 9", 27)
+        );
+    }
+
+    private Stream<Arguments> splitStrings() {
+        return Stream.of(
+                Arguments.of("2 + 3 / 5 * 6", NUMBERS_REGEXP, EMPTY_SPACE, WHITESPACE_DELIMITER, 6),
+                Arguments.of("2 + 3 / 5 * 2", NOT_NUMBERS_REGEXP, EMPTY_SPACE, WHITESPACE_DELIMITER, 7)
         );
     }
 
@@ -31,6 +55,14 @@ class StringCalculatorTest {
     @BeforeEach
     void setUp() {
         calculator = new StringCalculator();
+    }
+
+    @DisplayName("주어진 문자열을 숫자와 연산자로 분리하는 테스트")
+    @ParameterizedTest
+    @MethodSource(value = "splitStrings")
+    void splitTest(String given, String regExp, String replacement, String delimitExpression, int tokenSize) {
+        String[] split = calculator.getSplit(given, regExp, replacement, delimitExpression);
+        assertThat(split.length).isEqualTo(tokenSize);
     }
 
     @DisplayName("덧셈 테스트")
@@ -65,22 +97,35 @@ class StringCalculatorTest {
         assertThat(result).isEqualTo(dividend / divisor);
     }
 
-    @DisplayName("입력 값이 `null`이거나 `빈 공백` 문자인 경우 IllegalArgumentException throw")
-    @ParameterizedTest(name = "입력 값이 `{0}` 인 경우 IllegalArgumentException throw")
+    @DisplayName("입력 값이 `null`이거나 `빈 공백`에 대한 조건 메서드 검증 테스트")
+    @ParameterizedTest(name = "입력 값이 `{0}` 인 경우 {1}")
     @MethodSource("blankStrings")
     void isBlank_ShouldReturnTrueForNullOrBlankStringsOneArgument(String input, boolean expected) {
-        assertEquals(calculator.isBlank(input), expected);
+        assertThat(calculator.isBlank(input)).isEqualTo(expected);
     }
 
-    @DisplayName("사칙연산 기호가 아닌 경우의 예외처리 테스트")
-    @ParameterizedTest(name = "입력된 문자열에 사칙연산 값이 아닌 `{0}` 경우 예외처리 테스트")
-    @CsvSource(value = {"2 + 2:true", "2 * 2:true", "3 & 2:false", "6 / 1:true"}, delimiter = ':')
-    void isValidSymbol_FourRuleCalculationsExceptionTest(String given, boolean expected) {
-        assertThat(calculator.isValidSymbol(given)).isEqualTo(expected);
+    @DisplayName("입력 값이 `null`이거나 `빈 공백` 문자인 경우 IllegalArgumentException 예외 발생")
+    @ParameterizedTest(name = "입력 값이 `{0}` 인 경우 IllegalArgumentException 예외 발생")
+    @MethodSource(value = "blankStrings")
+    void isBlank_ThrowsIllegalArgumentException(String given) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> calculator.apply(given));
     }
 
-    @Test
-    void testCase() {
-        calculator.apply("2 + 2");
+    @DisplayName("사칙연산 기호가 아닌 문자열이 입력된 경우의 예외처리 테스트")
+    @ParameterizedTest(name = "입력된 문자열에 사칙연산 값이 아닌 `{0}` 경우 IllegalArgumentException 예외 테스트")
+    @MethodSource(value = "invalidSymbolStrings")
+    void invalidSymbol_FourRuleCalculationsExceptionTest(String given) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> {
+                    calculator.isContainsSymbol(given);
+                });
+    }
+
+    @DisplayName("사칙 연산을 모두 포함 연산이 가능한 기능 구현 테스트")
+    @ParameterizedTest(name = "`{0}`의 연산 결과 값은 : {1}")
+    @MethodSource(value = "validSymbolStrings")
+    void fourRuleCalculationSerialTest(String given, Integer expected) {
+        assertThat(calculator.apply(given)).isEqualTo(expected);
     }
 }
