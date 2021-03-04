c5e9a80cbbf91f729c2168175b0eacc3562ecf0a
cptbong <cptbbong@gmail.com>
2021-03-04 20:35:37 +0900

    test(add): 난수값에따른 전진테스트추가.

--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,6 +43,20 @@ class CarTest {
         assertThat(cars.get(2).getPosition()).isEqualTo(5);
         assertThat(cars.get(3).getPosition()).isEqualTo(5);
         assertThat(cars.get(4).getPosition()).isEqualTo(5);
+    }
 
+    @Test
+    @DisplayName("전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다.")
+    void asd() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(10);
+        if (randomNumber >= 4) {
+            Car car = new NextStepCar();
+            assertThat(car.moveForward()).isEqualTo(1);
+        }
+        if (randomNumber < 4) {
+            Car car = new NextStepCar();
+            assertThat(car.moveForward()).isEqualTo(0);
+        }
     }
 }
