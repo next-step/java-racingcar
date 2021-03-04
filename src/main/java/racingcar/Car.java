289eeb1e4ddd8231f47ab0bf2b9db2e509be54c2
jhLim97 <wnsgur2749@naver.com>
2021-03-04 18:32:19 +0900

    feat: 자동차의 정보를 담고 있는 클래스 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class Car {
+
+    private int cntMove;
+
+    public int getCntMove() {
+        return cntMove;
+    }
+
+    public void setCntMove(int cntMove) {
+        this.cntMove = cntMove;
+    }
+}
--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,2 +1,16 @@
-package racingcar;public class InputView {
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carNumber = scanner.nextInt();
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int tryNumber = scanner.nextInt();
+    }
 }
