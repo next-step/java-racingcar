3d252c98b6b4f0a2812762d2b0e48928dbcea4b9
Dion <ksundong@gmail.com>
2021-03-05 00:58:56 +0900

    refactor: 입력받은 만큼 자동차를 생성하는 기능 구현

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,12 +1,24 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Game {
 
-  public void initializeCars(int size) {
+  private List<Car> cars;
 
+  public void initializeCars(int size) {
+    List<Car> newCars = new ArrayList<>();
+
+    for (int i = 0; i < size; i++) {
+      newCars.add(new Car());
+    }
+
+    this.cars = Collections.unmodifiableList(newCars);
   }
 
   public int getCarSize() {
-    return 0;
+    return cars.size();
   }
 }
