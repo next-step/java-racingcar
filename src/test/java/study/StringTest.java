e3fe992f6c0939a6da798031058ccd788de1a3fe
yun gyeong heo <fullmoon3021@gamil.com>
2021-03-04 23:29:22 +0900

    step1 commit test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,37 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
 	
 	@Test
+	@DisplayName("step1.split test")
 	void split() {
 		String[] result = "1,2".split(",");
 		assertThat(result).contains("1");
+		assertThat(result).containsExactly("1", "2");
+	}
+	
+	@Test
+	@DisplayName("step1.substring test")
+	void substring() {
+		String result = "(1,2)".substring(1, 4);
+		assertThat(result).isEqualTo("1,2");
+	}
+	
+	@Test
+	@DisplayName("step1.charAt Exception test")
+	void charAt() {
+		assertThatThrownBy(() -> { "abc".charAt(5); }).isInstanceOf(StringIndexOutOfBoundsException.class)
+													.hasMessageContaining("index");
+		
+		assertThatExceptionOfType(Throwable.class).isThrownBy(() -> { "abc".charAt(5); })
+												.withMessageContaining("range")
+												.withMessageMatching("String index out of range: [0-9]*");
 	}
 }
