3e033d4d6ca5146bdfbd6c6e4923052f64f3d962
seokrae <kslbsh@gmail.com>
2021-03-04 21:23:30 +0900

    [feat] - 상수 관리 클래스 선언

---
+++ src/main/java/step2/Constants.java
@@ -0,0 +1,13 @@
+package step2;
+
+import java.util.function.Supplier;
+
+public final class Constants {
+
+    // 숫자와 사칙 연산 사이의 구분을 빈 공백으로 가정
+    public static final String SPLIT_DELIMITER = "\\s";
+
+    public static final Supplier<IllegalArgumentException> ILLEGAL_ARGUMENT_EXCEPTION_SUPPLIER =
+            () -> new IllegalArgumentException("사칙 연산에 해당하는 문자열이 존재하지 않습니다.");
+
+}
