7e024b9781bd3fe8355fa8ae8ddad2d4d08f5ec8
ddak-ddakong <79402787+ddak-ddakong@users.noreply.github.com>
2021-03-05 00:49:17 +0900

    Step1 (#1909)
    * step1 test
    
    * step1 commit test
    
    Co-authored-by: yun gyeong heo <fullmoon3021@gamil.com>

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetCollectionTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+	
+	@Test
+	@DisplayName("step1.Test Set Collection size(size > 2, size <= 3)")
+	void size() {
+		assertThat(numbers).size().isGreaterThan(2)
+								  .isLessThanOrEqualTo(3);
+	}
+	
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("step1.Test Set Collection contains value using @ParameterizedTest")
+    void contains(int testNumber) {
+        assertThat(numbers.contains(testNumber)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("step1.Test Set Collection contains value using @CsvSource")
+    void contains(int testNumber, boolean expect) {
+        assertThat(numbers.contains(testNumber)).isEqualTo(expect);
+    }
+}
