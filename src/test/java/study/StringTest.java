17cf74ef6fa6faf79c8cee379c366fae55ee2b85
minhee.son <minhee4735@gmail.com>
2021-03-04 23:25:00 +0900

    [step01-1] StringTest, SetTest 구현 완료 (#1902)

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,65 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    /*
+    * 요구사항 1
+        "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+        "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+    * */
+
+    @Test
+    public void splitTwoString() {
+        String str = "1,2";
+        String[] strs = str.split(",");
+
+        assertThat(strs).contains("1");
+        assertThat(strs).contains("2");
+    }
+
+    @Test
+    public void splitOneString() {
+        String str = "1";
+        String[] strs = str.split(",");
+
+        assertThat(strs).containsExactly("1");
+    }
+
+
+    /*요구사항 2
+    "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
+    */
+
+    @Test
+    public void removeBrackets() {
+        String str = "(1,2)";
+        String strWithoutBrackets = str.substring(1, 4);
+
+        assertThat(strWithoutBrackets).isEqualTo("1,2");
+    }
+
+
+    /* 요구사항 3
+    "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+    String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+    JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+    */
+
+    @Test
+    @DisplayName("Test for charAt() StringIndexOutOfBoundsException")
+    public void getCharacterAt() {
+        String str = "abc";
+
+        assertThatThrownBy(() -> {
+            str.charAt(3);
+
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+
+}
