4527fbf24d5c6e1f4baf7082b49d1ab8b5ce5ee2
suhyunsim <cnc1106@naver.com>
2021-03-04 18:16:55 +0900

    test: split test 추가

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,17 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("1,2를 1과 2로 잘 분리되는지 확인")
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+    
+}
