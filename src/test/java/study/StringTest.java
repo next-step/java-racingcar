9d56828b6d5dff2a4d78a7bdca0b02609c01b551
suhyunsim <cnc1106@naver.com>
2021-03-04 20:58:12 +0900

    test: splitValueOne test 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -14,4 +14,12 @@ public class StringTest {
 
         assertThat(actual).containsExactly("1", "2");
     }
+
+    @Test
+    @DisplayName("1,를 1만 포함하는 배열로 분리되는지 확인")
+    void splitOneValue() {
+        String[] actual = "1,".split(",");
+        
+        assertThat(actual).contains("1");
+    }
 }
