433eb8735cd86f13f9ad6117fd8b0374fd8d6d67
Jake <cocahack@naver.com>
2021-03-04 23:12:51 +0900

    test: Operator enum 의 lambda 기능 테스트

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -2,9 +2,6 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -28,4 +25,16 @@ class OperatorTest {
     void throwIllegalArgumentExceptionIfStringIsNotValidOperator() {
         assertThatThrownBy(() -> Operator.createOperator("%")).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("Operator enum 의 연산 기능을 적용한다.")
+    void applyOperator() {
+        assertAll(
+            () -> assertThat(Operator.ADDITION.calculation.apply(1, 2)).isEqualTo(3),
+            () -> assertThat(Operator.SUBTRACTION.calculation.apply(4, 7)).isEqualTo(-3),
+            () -> assertThat(Operator.MULTIPLICATION.calculation.apply(10, 0)).isZero(),
+            () -> assertThat(Operator.DIVISION.calculation.apply(121, 11)).isEqualTo(11)
+        );
+    }
+
 }
