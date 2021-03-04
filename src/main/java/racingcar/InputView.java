633b8b31c79e0f0ba918643ddec6c5275f8d2419
jhLim97 <wnsgur2749@naver.com>
2021-03-05 02:03:45 +0900

    refactor: 입력 단위 테스트를 위한 메서드 분리

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -5,15 +5,26 @@ import java.util.Scanner;
 public class InputView {
 
     private static RacingGame racingGame = new RacingGame();
+    private static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+    public int inputCarNumber() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         int countCar = scanner.nextInt();
 
+        return countCar;
+    }
+
+    public int inputTryCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         int countTry = scanner.nextInt();
+
+        return countTry;
+    }
+
+    public void start() {
+        int countCar = inputCarNumber();
+
+        int countTry = inputTryCount();
 
         racingGame.startRacing(countCar, countTry);
     }
