5ef4e5b98c7aef925f2068c9ca515f50cc0941e6
gmoon <gmoon@rsupport.com>
2021-03-04 23:54:30 +0900

    Step1 피드백 반영 1. String 학습 테스트 누락된 요구사항 반영 2. 테스트 코드 가독성   2.1. @DisplayName 활용   2.2. given, when, then 순으로 작성하여 테스트 코드 의도성을 명확히

--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -1,4 +1,4 @@
-package study;
+package step1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -6,20 +6,31 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class StringTest {
 
   @Test
-  @DisplayName("요구사항 1 split")
+  @DisplayName("요구 사항1, split")
   void split() {
-    String[] result = getArrays("1,2", ",");
-    assertThat(result).contains("1");
-    assertThat(result).contains("1", "2");
-    assertThat(result).containsExactly("1", "2");
+    // given
+    final String TEST_CASE1 = "1,2";
+    final String TEST_CASE2 = "1";
+
+    // when
+    final String[] RESULT1 = getArrays(TEST_CASE1, ",");
+    final String[] RESULT2 = getArrays(TEST_CASE2, ",");
+
+    // then
+    assertAll("split 학습 테스트"
+            ,() -> assertThat(RESULT1).contains("1")
+            ,() -> assertThat(RESULT1).containsExactly("1", "2")
+            ,() -> assertThat(RESULT2).containsExactly("1")
+     );
   }
 
   @Test
-  @DisplayName("요구사항 2 substring")
+  @DisplayName("요구 사항2, substring")
   void substring() {
     String str = "(1,2)";
     String result = str.substring(1, str.length() - 1);
@@ -31,7 +42,7 @@ public class StringTest {
   }
 
   @Test
-  @DisplayName("요구사항 3 charAt")
+  @DisplayName("요구 사항3, charAt")
   void charAt() {
     String str = "abc";
     assertThat(getChar(str, 0)).isEqualTo('a');
@@ -40,7 +51,7 @@ public class StringTest {
   }
 
   @Test
-  @DisplayName("요구사항 3 charAt exception test")
+  @DisplayName("요구 사항3, charAt exception test")
   void charAtOfThrow() {
     String str = "abc";
     assertThatThrownBy(() -> getChar(str, 3))
