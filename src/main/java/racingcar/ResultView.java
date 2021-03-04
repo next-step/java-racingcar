fd0ba4808b6c38d25337158ebde427c1ce8f6b18
jhLim97 <wnsgur2749@naver.com>
2021-03-05 01:12:24 +0900

    refactor: 한 단계의 들여쓰기 규칙 지키기

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -2,13 +2,19 @@ package racingcar;
 
 public class ResultView {
 
+    public void printEachCar(Car car) {
+        for (int i = 0; i < car.getCountMove(); i++) {
+            System.out.print("-");
+        }
+    }
+
     public void printResult(Car[] cars) {
         for (Car car : cars) {
-            for (int i = 0; i < car.getCountMove(); i++) {
-                System.out.print("-");
-            }
+            printEachCar(car);
+
             System.out.println();
         }
+
         System.out.println();
     }
 }
