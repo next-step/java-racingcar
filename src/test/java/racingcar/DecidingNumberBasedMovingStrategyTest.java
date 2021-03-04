0e51d5717f311585c23fceddce3c12f6ddfe8762
JuJinPark <wnwls1216@gmail.com>
2021-03-04 21:28:58 +0900

    [refactor] removed class only for tests - stub class로 대체

--- src/test/java/racingcar/DecidingNumberBasedMovingStrategyTest.java
+++ src/test/java/racingcar/DecidingNumberBasedMovingStrategyTest.java
@@ -4,17 +4,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.domain.DecidingNumberBasedMovingStrategy;
-import racingcar.domain.FixedNumberGenerator;
+import racingcar.domain.NumberGenerator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DecidingNumberBasedMovingStrategyTest {
 
+    private NumberGenerator createFixedNumberGenerator(int number) {
+        return new NumberGenerator() {
+            @Override
+            public int generate() {
+                return number;
+            }
+        };
+    }
     @ParameterizedTest
     @CsvSource(value = {"3:false", "2:false", "3:false","4:true","7:true"}, delimiter = ':')
     @DisplayName("생성된 값이 4이상의 값은 movable하고 4미만의 값은 movable하지 않다를 테스트")
     void isMovable(int number,boolean expected) {
-        FixedNumberGenerator fixedNumberGenerator = new FixedNumberGenerator(number);
+        NumberGenerator fixedNumberGenerator = createFixedNumberGenerator(number);
         DecidingNumberBasedMovingStrategy movingStrategy = new DecidingNumberBasedMovingStrategy(fixedNumberGenerator);
         assertThat(movingStrategy.isMovable()).isEqualTo(expected);
     }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -3,21 +3,27 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import racingcar.domain.FixedMovingStrategy;
+import racingcar.domain.MovingStrategy;
 import racingcar.domain.RacingCar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarTest {
 
+    private MovingStrategy createFixedMovingStrategy(boolean movable) {
+        return new MovingStrategy() {
+            @Override
+            public boolean isMovable() {
+                return movable;
+            }
+        };
+    }
     @ParameterizedTest
-    @CsvSource(value = {"2:3","1:4","0:4"}, delimiter = ':')
+    @CsvSource(value = {"true:1","true:1","false:0"}, delimiter = ':')
     @DisplayName("자동차 move 테스트")
-    void move(int movableCount,int totalMovingTryCount) {
-        RacingCar test = new RacingCar(new FixedMovingStrategy(movableCount, totalMovingTryCount - movableCount));
-        for (int i = 0; i < totalMovingTryCount; i++) {
-            test.move();
-        }
-        assertThat(test.getTravelledDistance()).isEqualTo(movableCount);
+    void move(boolean movable,int expectedTravelDistance ) {
+        RacingCar test = new RacingCar(createFixedMovingStrategy(movable));
+        test.move();
+        assertThat(test.getTravelledDistance()).isEqualTo(expectedTravelDistance);
     }
 }
