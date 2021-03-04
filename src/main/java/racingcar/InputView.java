003c6c823ed1fd68abfc7a1645a6cec822871d60
jhLim97 <wnsgur2749@naver.com>
2021-03-04 18:35:58 +0900

    feat: 변수명 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -8,9 +8,9 @@ public class InputView {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("자동차 대수는 몇 대 인가요?");
-        int carNumber = scanner.nextInt();
+        int cntCar = scanner.nextInt();
 
         System.out.println("시도할 회수는 몇 회 인가요?");
-        int tryNumber = scanner.nextInt();
+        int cntTry = scanner.nextInt();
     }
 }
