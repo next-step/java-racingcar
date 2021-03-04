df65489939c8e0c3b3be6158c06d408549cf39d4
Kim Woojae <ujae@Kimui-MacBookPro.local>
2021-03-05 01:42:22 +0900

    feat:덧셈 실패 테스트 케이스 작성

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public int calculate(String express){
+        return 0;
+    }
+
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+
+
+    @DisplayName("문자열로 들어온 덧셈 연산식을 계산하는 테스트")
+    @Test
+    void addTest(){
+        // given
+        String express = "2 + 3";
+        Calculator calculator = new Calculator();
+
+        // when
+        int result = calculator.calculate(express);
+
+        // then
+        assertThat(result).isEqualTo(5);
+
+    }
+
+}
