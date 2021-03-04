6b7e0a1759d8712ad483fb5d3c8689d24f51cbc5
Dion <ksundong@gmail.com>
2021-03-05 00:46:32 +0900

    refactor: 테스트 코드 중복 제거

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,6 +3,7 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -10,6 +11,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class CarTest {
+
+  Car car;
 
   static Stream<Arguments> distances() {
     return Stream.of(
@@ -20,12 +23,15 @@ class CarTest {
     );
   }
 
+  @BeforeEach
+  void setUp() {
+    car = new Car();
+  }
+
   @ParameterizedTest
   @DisplayName("자동차는 4이상이면 전진하고, 미만이면 정지한다.")
   @CsvSource({"3,false", "0,false", "4,true", "9,true"})
   void move(int randomNumber, boolean expected) {
-    Car car = new Car();
-
     assertThat(car.move(randomNumber)).isEqualTo(expected);
   }
 
@@ -33,8 +39,6 @@ class CarTest {
   @DisplayName("해당 횟수만큼 전진한 자동차의 상태를 확인한다. 이동 거리는 '-'로 표시한다.")
   @MethodSource("distances")
   void status(int distance, String expected) {
-    Car car = new Car();
-
     for (int i = 0; i < distance; i++) {
       car.move(4);
     }
