e6300855e66ab5e61eb6e46f5e646d555956970e
ad7549 <ad7549@gmail.com>
2021-03-05 05:43:51 +0900

    feat: Add IntegerUtil

---
+++ src/main/java/calculator/IntegerUtil.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class IntegerUtil {
+    public static boolean isInteger(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
