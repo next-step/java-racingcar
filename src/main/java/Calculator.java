a58c4ed349e615175ee825c572819b7dcd00003e
dailyworker <workingssu@gmail.com>
2021-03-05 19:10:58 +0900

    refactor : Calculator 클래스 리팩토링 및 Main 클래스 추가 1. enteredFormulaLength에서 보다 명시적으로 보기위해 LAST_INDEX로 변수명 변경 2. splitUserInput() 함수 내부에서 split 처리와 ArrayList 변환까지 동시에 하게끔 리팩토링함 3. validateUserInput() 함수 부분에서 올바르게 split 되는 문자열인지와 ** ++ '' 와 같은 이상한 문자값이 들어가있는지 검증로직 추가 4. Exception 발생 시 테스트 assertTrue()를 통하여 message contain() 비교를 위해 ERROR 문구 추가 5. 나눗셈이 정수만 리턴하기 때문에 나누면서 버리는 Math.floorDiv 사용

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,16 +1,15 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Queue;
-import java.util.Set;
 
 public class Calculator {
 
   private String enteredFormula;
-  private int enteredFormulaLength;
+  private List<String> splitFormulas = new ArrayList<>();
 
   private static final int FIRST_INDEX = 0;
+  private int LAST_INDEX;
+
   public enum Operator {
     PLUS("+"),
     MINUS("-"),
@@ -29,40 +28,61 @@ public class Calculator {
     }
   }
 
-  //TODO : 사용자 문자열 입력
+  //사용자 문자열 입력
   public void enterFormula(String userInput) {
     this.enteredFormula = userInput;
-    isValidFormula();
+    validateUserInput();
   }
 
-  //TODO : 입력 Split
-  private String[] splitUserInput() {
-    return this.enteredFormula.split(" ");
+  public int calculate() {
+    return calculateFormula(this.splitFormulas);
   }
 
-  //TODO : 사용자 문자열 validate 구현
-  private void isValidFormula() {
-    String[] splitFormula = splitUserInput();
-    List<String> formulas = toList(splitFormula);
-    this.enteredFormulaLength = splitFormula.length;
-
-    isValidateFirstAndLastElementInList(formulas);
-    isValidateHasNoRedundancy(formulas);
-    calculateFormula(formulas);
+  //입력 Split
+  private List<String> splitUserInput() {
+    return toList(this.enteredFormula.split(" "));
   }
 
-  //TODO : 문자열 List화
-  public List<String> toList(String[] splitFormula) {
+  //문자열 List화
+  private List<String> toList(String[] splitFormula) {
     return Arrays.asList(splitFormula);
   }
 
-  //TODO : 계산식의 처음과 마지막이 숫자로 시작하는지 연산자로 시작하는지
-  private void isValidateFirstAndLastElementInList(List<String> formulas) {
-    if(isOperator(formulas.get(FIRST_INDEX)) && isOperator(formulas.get(enteredFormulaLength))){
-      throw new IllegalArgumentException("잘못된 계산식입니다.");
+  //사용자 문자열 validate 구현
+  private void validateUserInput() {
+    this.splitFormulas = splitUserInput();
+    this.LAST_INDEX = this.splitFormulas.size() -1;
+    isValidateProperlySplit();
+    isValidateHasStrangeCharacter();
+    isValidateFirstAndLastElementInList(this.splitFormulas);
+    isValidateHasNoRedundancy(this.splitFormulas);
+  }
+
+  private void isValidateProperlySplit() {
+    if(LAST_INDEX <= 0) {
+      throw new IllegalArgumentException("ERROR : 잘못된 계산식입니다.");
     }
   }
 
+  private void isValidateHasStrangeCharacter() {
+    for(String str : splitFormulas){
+      isStrangeString(str);
+    }
+  }
+
+  private void isStrangeString(String str) {
+    if(!isNumeric(str) && str.length() > 1)
+      throw new IllegalArgumentException("ERROR : 연산자 길이는 1을 초과할 수 없습니다.");
+  }
+
+  //계산식의 처음과 마지막이 숫자로 시작하는지 연산자로 시작하는지
+  private void isValidateFirstAndLastElementInList(List<String> formulas) {
+    if(isOperator(formulas.get(FIRST_INDEX)) && isOperator(formulas.get(LAST_INDEX))){
+      throw new IllegalArgumentException("ERROR : 잘못된 계산식입니다.");
+    }
+  }
+
+  //연산자가 맞는지 검증하는 함수
   private boolean isOperator(String symbol) {
     boolean result = false;
     for (Operator op : Operator.values()) {
@@ -72,14 +92,14 @@ public class Calculator {
     return result;
   }
 
-  //TODO : 문자와 숫자가 서로 중복되서 들어가있는지 않은지 (e.g 2 2 + 2 3 , + + 2 + 3)
+  //문자와 숫자가 서로 중복되서 들어가있는지 않은지 (e.g 2 2 + 2 3 , + + 2 + 3)
   private void isValidateHasNoRedundancy(List<String> formulas) {
-    for(int i = 0; i < enteredFormulaLength-1; i++){
+    for(int i = 0; i < LAST_INDEX; i++){
       isRedundancy(formulas.get(i), formulas.get(i + 1));
     }
   }
 
-  //TODO : 계산의 이전값과 다음값을 통해서 숫자가 중복되있거나 연산자가 중복되어 있는 경우 체
+  //계산의 이전값과 다음값을 통해서 숫자가 중복되있거나 연산자가 중복되어 있는 경우
   private void isRedundancy(String prev, String next) {
     isPrevAndNextIsNumeric(prev, next);
     isPrevAndNextIsOperator(prev, next);
@@ -89,7 +109,7 @@ public class Calculator {
     boolean isPrevNumeric = isNumeric(prev);
     boolean isNextNumeric = isNumeric(next);
     if(isPrevNumeric == isNextNumeric) {
-      throw new IllegalArgumentException("잘못된 계산식을 입력하셨습니다.");
+      throw new IllegalArgumentException("ERROR: 잘못된 계산식을 입력하셨습니다.");
     }
   }
 
@@ -101,17 +121,17 @@ public class Calculator {
     boolean isPrevOperator = isOperator(prev);
     boolean isNextOperator = isOperator(next);
     if(isPrevOperator == isNextOperator) {
-      throw new IllegalArgumentException("잘못된 계산식을 입력하셨습니다.");
+      throw new IllegalArgumentException("ERROR: 잘못된 계산식을 입력하셨습니다.");
     }
   }
 
-  //TODO : 계산수행 2,*,3,+,1
-  private void calculateFormula(List<String> formulas) {
+  //계산수행
+  private int calculateFormula(List<String> formulas) {
     int result = Integer.parseInt(formulas.get(0));
-    for(int i = FIRST_INDEX; i < enteredFormulaLength -2; i+=2) {
+    for(int i = FIRST_INDEX; i < LAST_INDEX -1; i+=2) {
       result = calculate(result, formulas.get(i+1), Integer.parseInt(formulas.get(i+2)));
     }
-    System.out.println(result);
+    return result;
   }
 
   private int calculate(int prev, String operator, int next) {
@@ -123,7 +143,7 @@ public class Calculator {
       return multiple(prev, next);
     if(operator.equals("/"))
       return divide(prev,next);
-    throw new IllegalArgumentException("잘못된 계산식 입니다.");
+    throw new IllegalArgumentException("ERROR : 잘못된 연산자가 입력되었습니다.");
   }
 
   private int multiple(int prev, int next) {
@@ -131,7 +151,7 @@ public class Calculator {
   }
 
   private int divide(int prev, int next) {
-    return prev / next;
+    return Math.floorDiv(prev, next);
   }
 
   private int sub(int prev, int next) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
   public static void main(String[] args) {
       Calculator calculator = new Calculator();
-      calculator.enterFormula("2 + 3 * 3");
+      calculator.enterFormula("1 / 0");
+      calculator.calculate();
   }
 }
