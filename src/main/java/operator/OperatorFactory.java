10419e07c06bd556500b3e751214a7a414139f49
Kim Woojae <ujae@Kimui-MacBookPro.local>
2021-03-05 13:05:28 +0900

    feat:곱셈 성공 테스트 케이스 작성

--- src/main/java/operator/OperatorFactory.java
+++ src/main/java/operator/OperatorFactory.java
@@ -7,6 +7,7 @@ public class OperatorFactory {
     public static Optional<Operator> getOperator(String operator){
         if(operator.equals("+")) return Optional.of(new PlusOperator());
         if(operator.equals("-")) return Optional.of(new MinusOperator());
+        if(operator.equals("*")) return Optional.of(new MultiplicationOperator());
         return Optional.empty();
     }
 
