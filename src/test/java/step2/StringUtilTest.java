583ed11b60fdd3999fe86bee55d864a672b32c1d
lee.jaesung <lee.jaesung@joins.com>
2021-03-06 07:59:11 +0900

    test: StringUtil 단위 테스트 구현

---
+++ src/test/java/step2/StringUtilTest.java
@@ -0,0 +1,33 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+public class StringUtilTest {
+
+    @DisplayName("입력 문자열이 공백값을 기준으로 배열로 변환된다")
+    @Test
+    public void splitBySpace_ShouldReturnArray() {
+        String inputString = "10 + 15 - 7 * 3 / 9";
+
+        StringUtil stringUtil = new StringUtil(inputString);
+
+        assertThat(stringUtil.splitBySpace()).containsExactly("10", "+", "15", "-", "7", "*", "3", "/", "9");
+    }
+
+    @DisplayName("입력 문자열 null 이거나 빈 공백 문자일 경우 IllegalArgumentException 를 던진다")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   "})
+    public void splitBySpace_ShouldThrowIllegalArgumentExceptionForNullAndEmptyStrings(String inputString) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new StringUtil(inputString))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
