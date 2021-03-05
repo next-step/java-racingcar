bf8d5912d04650659270c327591979db7386c20e
meme2367 <meme91322367@gmail.com>
2021-03-05 13:59:15 +0900

    feat : 입출력 기능 추가

--- src/main/java/carRacing/controller/RacingController.java
+++ src/main/java/carRacing/controller/RacingController.java
@@ -1,8 +1,38 @@
 package carRacing.controller;
 
-public class RacingController {
-    public void runGame() {
+import carRacing.view.InputView;
+import carRacing.view.ResultView;
 
+public class RacingController {
+    private InputView inputView;
+    private ResultView resultView;
+
+    public RacingController() {
+        inputView = new InputView();
+        resultView = new ResultView();
+    }
+    public void runGame() {
+        int numberOfCar = drawNumberOfCarAndReturn();
+        int carRacingCount = drawCarCountAndReturn();
+
+        
     }
 
+    private void drawExecutionResult(int position) {
+        resultView.printExecutionResult(position);
+    }
+
+    private void drawLiterallyExecutionResult() {
+        resultView.printLiterallyExecutionResult();
+    }
+
+    private int drawCarCountAndReturn() {
+        resultView.printCarCountQuestion();
+        return inputView.inputNumber();
+    }
+
+    private int drawNumberOfCarAndReturn() {
+        resultView.printNumberOfCarQuestion();
+        return inputView.inputNumber();
+    }
 }
