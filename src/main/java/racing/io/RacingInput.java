4e0ed4ec6d4481e43c82db132312930dcf6dba01
Jake <cocahack@naver.com>
2021-03-05 16:06:10 +0900

    feat: 사용자 입력 기능 구현

---
+++ src/main/java/racing/io/RacingInput.java
@@ -0,0 +1,20 @@
+package racing.io;
+
+public class RacingInput {
+
+    private final int carSize;
+    private final int moves;
+
+    public RacingInput(int carSize, int moves) {
+        this.carSize = carSize;
+        this.moves = moves;
+    }
+
+    public int getCarSize() {
+        return carSize;
+    }
+
+    public int getMoves() {
+        return moves;
+    }
+}
