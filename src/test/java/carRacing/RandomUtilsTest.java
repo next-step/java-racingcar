d8378b27d4369655b572cf413a9532ace468bb34
meme2367 <meme91322367@gmail.com>
2021-03-05 15:29:35 +0900

    fix : RandomUtilsTest 수정

--- src/test/java/carRacing/RandomUtilsTest.java
+++ src/test/java/carRacing/RandomUtilsTest.java
@@ -2,6 +2,7 @@ package carRacing;
 
 import carRacing.utils.RandomUtils;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,8 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomUtilsTest {
 
-    @ParameterizedTest
-    @CsvSource(value = {"0,9"})
+    @Test
     @DisplayName("0~9 사이의 숫자를 랜덤함수로 생성하는지 확인")
     public void isCorrectRandomValue() {
         assertThat(RandomUtils.nextInt(9)).isBetween(0,9);
