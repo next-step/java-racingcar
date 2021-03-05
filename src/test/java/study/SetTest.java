874f484ce1b8b6c5419ce8bdf7a32c511175279c
tenmillionwon <tenmillionwon@gmail.com>
2021-03-06 08:44:44 +0900

    Step1 리뷰 요청 드립니다. (#1929)
    * string split,substring,chatAt test
    
    * set size,contains test
    
    * @DisplayName 위치 수정

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("Set size() 테스트")
+	void sizeTest() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("Set contains() 테스트")
+	void containsTest(int inputNumber) {
+		assertTrue(numbers.contains(inputNumber));
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	@DisplayName("Parameterized true/false 반환값 테스트")
+	void containsTrueFalseTest(int inputNumber, boolean expected) {
+		assertEquals(numbers.contains(inputNumber), expected);
+	}
+}
