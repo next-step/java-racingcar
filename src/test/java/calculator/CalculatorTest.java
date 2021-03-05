bcce731ccb421baf511eeb2e67ffbd8f67db55ea
limdingdong <goldbat9554@naver.com>
2021-03-05 12:38:11 +0900

    modify CalculatorTest class ParameterizedTest 활용

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,59 +2,53 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CalculatorTest {
 
-    @Test
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
     @DisplayName("입력값 검증테스트 - 빈 문자열")
-    void validateInput_isEmpty() {
+    void validateInput_isEmpty(String input) {
         // given
         Calculator calculator = new Calculator();
 
         // when, then
         assertThatIllegalArgumentException()
-                .isThrownBy( () -> calculator.validateInput(null) )
-                .withMessageMatching("수식을 입력해 주세요");
-
-        assertThatIllegalArgumentException()
-                .isThrownBy( () -> calculator.validateInput("") )
+                .isThrownBy( () -> calculator.validateInput(input) )
                 .withMessageMatching("수식을 입력해 주세요");
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"2 2 4", "- 2 4", "- & 4"})
     @DisplayName("입력값 검증테스트 - 사칙연산 레이아웃")
-    void validateInput_isValidOperationLayout() {
+    void validateInput_isValidOperationLayout(String input) {
         // given
         Calculator calculator = new Calculator();
 
         // when, then
         assertThatIllegalArgumentException()
-                .isThrownBy( () -> calculator.validateInput("2 2 4") )
-                .withMessageMatching("수식의 구성이 올바르지 않습니다. 다시 입력해 주세요");
-
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> calculator.validateInput("- 2 4") )
-                .withMessageMatching("수식의 구성이 올바르지 않습니다. 다시 입력해 주세요");
-
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> calculator.validateInput("- & 4") )
+                .isThrownBy( () -> calculator.validateInput(input) )
                 .withMessageMatching("수식의 구성이 올바르지 않습니다. 다시 입력해 주세요");
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"2 * 8 - 4:12", "3 / 2 + 1:2"}, delimiter = ':')
     @DisplayName("calculate 메소드 테스트")
-    void calculate() throws Exception {
+    void calculate(String formula, int expected) throws Exception {
         // given
         Calculator calculator = new Calculator();
-
         // when
-        String formula = "2 * 8 - 4";
         int formulaResult = calculator.calculate(formula);
-
         // then
-        assertThat(formulaResult).isEqualTo(12);
+        assertThat(formulaResult).isEqualTo(expected);
     }
 }
