86b06b62157c1789d5a8f8eb69c73e095dd5ebde
Kim Woojae <ujae@Kimui-MacBookPro.local>
2021-03-05 09:23:50 +0900

    refactor: 덧셈 연산 리펙터링

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,31 +1,42 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
 public class Calculator {
 
-    public Calculator() { }
+    public Calculator() {
+    }
 
-    public int calculate(String express){
-        if(isBlank(express)) throw new IllegalArgumentException();
-        String[] data = express.split(" ");
-        int sum =  0;
-        for(int i=0; i < data.length-2; i+=2){
-            if(!isPlus(data[i+1])) throw new IllegalArgumentException();
-            sum += (Integer.parseInt(data[i]) + Integer.parseInt(data[i+2]));
+    public int calculate(String express) {
+        if (isBlank(express)) throw new IllegalArgumentException();
+        return getResult(splitBlank(express));
+    }
+
+    private boolean isBlank(String express) {
+        if (express == null) return true;
+        if (express.equals(" ")) return true;
+        if (express.contains("  ")) return true;
+        return false;
+    }
+
+    private String[] splitBlank(String express) {
+        return express.split(" ");
+    }
+
+    private int getResult(String[] data) {
+        int sum = 0;
+        for (int i = 0; i < data.length - 2; i += 2) {
+            if (!isPlus(data[i + 1])) throw new IllegalArgumentException();
+            sum += add(Integer.parseInt(data[i]),Integer.parseInt(data[i + 2]));
         }
         return sum;
     }
 
-    private boolean isBlank(String express){
-        if(express.equals(" ") || express.contains("  ") || express == null) return true;
+    private boolean isPlus(String operation) {
+        if (operation.equals("+")) return true;
         return false;
     }
 
-    private boolean isPlus(String operation){
-        if(operation.equals("+")) return true;
-        return false;
+    private int add(int firstValue, int secondValue) {
+        return firstValue + secondValue;
     }
 
 }
