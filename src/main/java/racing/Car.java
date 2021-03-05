b58b1ab633874ab8c014a3df11122733906da64e
Jake <cocahack@naver.com>
2021-03-05 21:05:54 +0900

    feat: 자동차 이동경로를 반환하는 메소드 추가

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -3,6 +3,7 @@ package racing;
 public class Car {
 
     private int location = 0;
+    protected static final String SYMBOL = "-";
 
     public int getLocation() {
         return location;
@@ -11,4 +12,13 @@ public class Car {
     public void move() {
         location++;
     }
+
+    public String getTrack() {
+        if (location == 0) {
+            return "";
+        }
+
+        return new String(new char[location]).replace("\0", SYMBOL);
+    }
+
 }
