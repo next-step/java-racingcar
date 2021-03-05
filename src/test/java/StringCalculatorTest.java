0072e60addf10b12f668b9a37ebbf78934aab6d9
shpark <shpark@shparkui-MacBookAir.local>
2021-03-05 22:12:30 +0900

    [step2] Add exception test and calculate method test

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,23 +1,29 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class StringCalculatorTest {
 
-    @Test @DisplayName("덧셈 구현 테스트")
+    @Test
+    @DisplayName("덧셈 구현 확인")
     void add() throws Exception {
         // given
         StringCalculator calculator = new StringCalculator();
 
         // when
         int result = calculator.add(1, 2);
+        int result2 = calculator.add(10, 40);
+        int result3 = calculator.add(130, 560);
 
         // then
         assertThat(result).isEqualTo(3);
+        assertThat(result2).isEqualTo(50);
+        assertThat(result3).isEqualTo(690);
     }
 
-    @Test @DisplayName("뺄셈 구현 테스트")
+    @Test
+    @DisplayName("뺄셈 구현 확인")
     void subtract() throws Exception {
         // given
         StringCalculator calculator = new StringCalculator();
@@ -29,7 +35,8 @@ class StringCalculatorTest {
         assertThat(result).isEqualTo(2);
     }
 
-    @Test @DisplayName("곱셈 구현 테스트")
+    @Test
+    @DisplayName("곱셈 구현 확인")
     void multiply() throws Exception {
         // given
         StringCalculator calculator = new StringCalculator();
@@ -42,7 +49,8 @@ class StringCalculatorTest {
 
     }
 
-    @Test @DisplayName("나눗셈 구현 테스트")
+    @Test
+    @DisplayName("나눗셈 구현 확인")
     void divide() throws Exception {
         // given
         StringCalculator calculator = new StringCalculator();
@@ -52,5 +60,52 @@ class StringCalculatorTest {
 
         // then
         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("입력값이 null일 때 예외처리 확인")
+    void checkNull() {
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    StringCalculator calculator = new StringCalculator(null);
+                });
+
+    }
+
+    @Test
+    @DisplayName("입력값이 빈 문자열일 때 예외처리 확인")
+    void checkEmptyString() {
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    StringCalculator calculator = new StringCalculator("");
+                });
+
+    }
+
+    @Test
+    @DisplayName("사칙연산 기호가 아닌 경우 예외처리 확인")
+    void checkOperator() {
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    StringCalculator calculator = new StringCalculator("1 $ 2");
+                    calculator.calculate();
+                });
+
+    }
+
+    @Test
+    @DisplayName("사친 연산 모두 확인")
+    void calculate() throws Exception {
+        // given
+        StringCalculator calculator = new StringCalculator("1 + 2 / 1 * 2 + 3 - 4");
+
+        // when
+        int result = calculator.calculate();
+
+        // then
+        assertThat(result).isEqualTo(5);
     }
 }
