97c67b23a27e1861f52c36163ec5e697762bde05
wkdehdlr <wkdehdlr@gmail.com>
2021-03-05 22:40:22 +0900

    [feat]null입력 구현

--- src/test/java/step2/Calculator.java
+++ src/test/java/step2/Calculator.java
@@ -3,21 +3,33 @@ package step2;
 public class Calculator {
 
     public static int plus(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
         String[] operation = input.split(" ");
         return Integer.parseInt(operation[0]) + Integer.parseInt(operation[2]);
     }
 
     public static int minus(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
         String[] operation = input.split(" ");
         return Integer.parseInt(operation[0]) - Integer.parseInt(operation[2]);
     }
 
     public static int multiply(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
         String[] operation = input.split(" ");
         return Integer.parseInt(operation[0]) * Integer.parseInt(operation[2]);
     }
 
     public static int divide(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
         String[] operation = input.split(" ");
         return Integer.parseInt(operation[0]) / Integer.parseInt(operation[2]);
     }
