fe1978240571e127ccf708626b58a9e42bc953d0
meme2367 <meme91322367@gmail.com>
2021-03-05 14:04:16 +0900

    fix : RacingController 게임 play 기능 추가

--- src/main/java/carRacing/controller/RacingController.java
+++ src/main/java/carRacing/controller/RacingController.java
@@ -1,11 +1,14 @@
 package carRacing.controller;
 
+import carRacing.model.Car;
+import carRacing.model.CarRacingInformation;
 import carRacing.view.InputView;
 import carRacing.view.ResultView;
 
 public class RacingController {
     private InputView inputView;
     private ResultView resultView;
+    private CarRacingInformation carRacingInformation;
 
     public RacingController() {
         inputView = new InputView();
@@ -14,8 +17,21 @@ public class RacingController {
     public void runGame() {
         int numberOfCar = drawNumberOfCarAndReturn();
         int carRacingCount = drawCarCountAndReturn();
+        carRacingInformation = new CarRacingInformation(numberOfCar,carRacingCount);
+        drawLiterallyExecutionResult();
+        playGame();
 
+    }
 
+    private void playGame() {
+        while(carRacingInformation.checkCarRacingCount()) {
+            carRacingInformation.decideMovable();
+            for(Car car : carRacingInformation.getCarList()) {
+                drawExecutionResult(car.getPoisition());
+            }
+            System.out.print("\n");
+            carRacingInformation.minusCarRacingCount();
+        }
     }
 
     private void drawExecutionResult(int position) {
