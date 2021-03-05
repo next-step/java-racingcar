1e32a6b0274ab9bdc0dbd25ba7521731bd87aad9
wkdehdlr <wkdehdlr@gmail.com>
2021-03-05 23:06:59 +0900

    [fix]메소드분리 리팩토링

--- src/test/java/step2/Calculator.java
+++ src/test/java/step2/Calculator.java
@@ -3,58 +3,55 @@ package step2;
 public class Calculator {
 
     public static int plus(String input) {
-        if (input == null || input.trim().isEmpty()) {
+        if (isEmpty(input)) {
             throw new IllegalArgumentException();
         }
         String[] operation = input.split(" ");
-        if (operation[1].compareTo("+") != 0 &&
-            operation[1].compareTo("*") != 0 &&
-            operation[1].compareTo("-") != 0 &&
-            operation[1].compareTo("/") != 0) {
+        if (isOperator(operation[1])) {
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(operation[0]) + Integer.parseInt(operation[2]);
     }
 
     public static int minus(String input) {
-        if (input == null || input.trim().isEmpty()) {
+        if (isEmpty(input)) {
             throw new IllegalArgumentException();
         }
         String[] operation = input.split(" ");
-                if (operation[1].compareTo("+") != 0 &&
-            operation[1].compareTo("*") != 0 &&
-            operation[1].compareTo("-") != 0 &&
-            operation[1].compareTo("/") != 0) {
+        if (isOperator(operation[1])) {
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(operation[0]) - Integer.parseInt(operation[2]);
     }
 
     public static int multiply(String input) {
-        if (input == null || input.trim().isEmpty()) {
+        if (isEmpty(input)) {
             throw new IllegalArgumentException();
         }
         String[] operation = input.split(" ");
-                if (operation[1].compareTo("+") != 0 &&
-            operation[1].compareTo("*") != 0 &&
-            operation[1].compareTo("-") != 0 &&
-            operation[1].compareTo("/") != 0) {
+        if (isOperator(operation[1])) {
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(operation[0]) * Integer.parseInt(operation[2]);
     }
 
     public static int divide(String input) {
-        if (input == null || input.trim().isEmpty()) {
+        if (isEmpty(input)) {
             throw new IllegalArgumentException();
         }
         String[] operation = input.split(" ");
-                if (operation[1].compareTo("+") != 0 &&
-            operation[1].compareTo("*") != 0 &&
-            operation[1].compareTo("-") != 0 &&
-            operation[1].compareTo("/") != 0) {
+        if (isOperator(operation[1])) {
             throw new IllegalArgumentException();
         }
         return Integer.parseInt(operation[0]) / Integer.parseInt(operation[2]);
+    }
+
+    public static boolean isEmpty(String input) {
+        return input == null || input.trim().isEmpty();
+    }
+
+    public static boolean isOperator(String operator) {
+        char op = operator.charAt(0);
+        return !(op == '+' || op == '-' || op == '*' || op == '/');
     }
 }
