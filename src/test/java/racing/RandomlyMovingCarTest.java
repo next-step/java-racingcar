73e9c076a0304f3158b465caed234de0983f293e
Jake <cocahack@naver.com>
2021-03-05 19:05:36 +0900

    test: 무작위로 움직이는 자동차의 테스트 케이스 추가

---
+++ src/test/java/racing/RandomlyMovingCarTest.java
@@ -0,0 +1,31 @@
+package racing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomlyMovingCarTest {
+    @Test
+    @DisplayName("자동차가 무작위로 움직인다.")
+    void moveOrStay() {
+        Car car = new RandomlyMovingCar();
+        int lastLocation = car.getLocation();
+
+        boolean isMoved = false;
+        boolean isStayed = false;
+
+        for (int i = 0; i < 100; i++) {
+            car.move();
+
+            int currentLocation = car.getLocation();
+
+            isMoved |= lastLocation != currentLocation;
+            isStayed |= lastLocation == currentLocation;
+
+            lastLocation = currentLocation;
+        }
+
+        assertThat(isMoved && isStayed).isTrue();
+    }
+}
