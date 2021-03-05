2dc79464b574a21d5c0cfc9b7687f09c8746c225
Kim Woojae <ujae@Kimui-MacBookPro.local>
2021-03-05 09:59:12 +0900

    feat: 뺄셈과 덧셈 성공 테스트 케이스 작성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
 
     public Calculator() {
@@ -24,19 +26,27 @@ public class Calculator {
     private int getResult(String[] data) {
         int sum = 0;
         for (int i = 0; i < data.length - 2; i += 2) {
-            if (!isPlus(data[i + 1])) throw new IllegalArgumentException();
-            sum += add(Integer.parseInt(data[i]),Integer.parseInt(data[i + 2]));
+            // 유효성 검사에서 연산자임을 이미 검증했다 가정하고 코딩을 시작
+            if(data[i+1].equals("+")){
+                sum += plus(Integer.parseInt(data[i]), Integer.parseInt(data[i+2]));
+            }
+            if(data[i+1].equals("-")){
+                sum += minus(Integer.parseInt(data[i]), Integer.parseInt(data[i+2]));
+            }
         }
         return sum;
     }
 
-    private boolean isPlus(String operation) {
-        if (operation.equals("+")) return true;
-        return false;
-    }
-
-    private int add(int firstValue, int secondValue) {
+    private int plus(int firstValue, int secondValue) {
         return firstValue + secondValue;
     }
+
+    private int minus(int firstValue, int secondValue) {
+        return firstValue - secondValue;
+    }
+
+
+    // private boolean isPatternMatches(String express){
+    // return !(Pattern.matches("^[0-9\\+\\-*/\\s=]*$", express));}
 
 }
