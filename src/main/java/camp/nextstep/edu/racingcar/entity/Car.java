ee22298b11273d297a531f5e15d14bedd8d5cea9
jeonyeonbin <qtrgor@gmail.com>
2021-03-05 22:07:35 +0900

    feat: Car object 이름추가, stepWithName 추가, TC 추가

--- src/main/java/camp/nextstep/edu/racingcar/entity/Car.java
+++ src/main/java/camp/nextstep/edu/racingcar/entity/Car.java
@@ -3,11 +3,21 @@ package camp.nextstep.edu.racingcar.entity;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import camp.nextstep.edu.exception.UserException;
+
 public class Car {
 	private int step;
-	private final static int MOVE_CONDITION = 4;
+	private String name;
 
-	public Car() {
+	private final static int MOVE_CONDITION = 4;
+	private final static int MAX_NAME_SIZE = 5;
+
+	public Car(String name) {
+		if (name.length() > MAX_NAME_SIZE) {
+			throw new UserException();
+		}
+
+		this.name = name;
 		this.step = 0;
 	}
 
@@ -17,13 +27,21 @@ public class Car {
 		}
 	}
 
+	private String getStepString() {
+		return IntStream.range(0, step)
+			.mapToObj(i -> "-")
+			.collect(Collectors.joining(""));
+	}
+
 	public int getStep() {
 		return step;
 	}
 
-	public String getStepString() {
-		return IntStream.range(0, step)
-			.mapToObj(i -> "-")
-			.collect(Collectors.joining(""));
+	public String getCarName() {
+		return this.name;
+	}
+
+	public String getStepStringWithName() {
+		return this.name + " : " + this.getStepString();
 	}
 }
