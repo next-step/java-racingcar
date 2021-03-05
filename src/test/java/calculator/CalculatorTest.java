6cbb5f2e4d8bc5c618bf4799382dc47e19cf00a5
Kim Woojae <ujae@Kimui-MacBookPro.local>
2021-03-05 09:28:07 +0900

    feat: 뺄셈 실패 테스트케이스 작성

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,19 +9,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
 
     @DisplayName("문자열로 들어온 덧셈 연산식을 계산하는 테스트")
     @Test
-    void addTest(){
+    void plusTest(){
         // given
         String express = "2 + 3";
-        Calculator calculator = new Calculator();
 
         // when
         int result = calculator.calculate(express);
 
         // then
         assertThat(result).isEqualTo(5);
+    }
+
+    @DisplayName("문자열로 들어온 뺄샘 연산식을 계산하는 테스트")
+    @Test
+    void minusTest(){
+        // given
+        String express = "5 - 2";
+
+        // when
+        int result = calculator.calculate(express);
+
+        // then
+        assertThat(result).isEqualTo(3);
     }
 
 }
