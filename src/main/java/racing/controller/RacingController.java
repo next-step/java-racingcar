1313e7d7e6910e3b65fe415906f68a5cfe1564eb
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-06 12:42:23 +0900

    feat:레이싱 컨트롤러 추가

--- src/main/java/racing/controller/RacingController.java
+++ src/main/java/racing/controller/RacingController.java
@@ -1,57 +1,47 @@
 package racing.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import racing.model.RacingCar;
+import racing.model.RacingGame;
 import racing.view.InputView;
 import racing.view.ResultView;
 
 public class RacingController {
 
-  private final int racingCarCount;
-  private final int turnCount;
-  private List<RacingCar> carList;
-  Random random = new Random();
+  private RacingGame racingGame;
+  private ResultView resultView;
+  private InputView inputView;
+  private int gameTrun;
+
 
   public RacingController() {
-    InputView inputView = new InputView();
-    ResultView resultView = new ResultView();
-    int[] carCountAndTurnCount = inputView.setUp();
-    this.racingCarCount = carCountAndTurnCount[0];
-    this.turnCount = carCountAndTurnCount[1];
-    setUpGame();
-    startGame(resultView);
+    play(setUpGame());
   }
 
-  public void setUpGame() {
-    carList = new ArrayList<>();
-    for (int i = 0; i < racingCarCount; i++) {
-      carList.add(new RacingCar(0));
+  /**
+   * InputView 에서 자동차 갯수와 게임 실행 횟수를 입력받아 게임을 설정함
+   */
+  public List<RacingCar> setUpGame() {
+    inputView = new InputView();
+    racingGame = new RacingGame();
+
+    int[] carCountAndTurnCount = inputView.init();
+    this.gameTrun = carCountAndTurnCount[1];
+    return racingGame.setup(carCountAndTurnCount[0]);
+  }
+
+  /**
+   * 게임 시작 메서드 1 턴씩 진행하며 게임이 끝날때까지 반복된다.
+   */
+  public void play(List<RacingCar> carList) {
+    resultView = new ResultView();
+    for (int i = 0; i < gameTrun; i++) {
+      List<Integer> randomValue = racingGame.createRandomValue();
+      List<RacingCar> movedCarList = racingGame.moveAndStop(randomValue);
+      resultView.turnResultView(movedCarList);
     }
   }
 
-  public List<Integer> createRandomValue() {
-    List<Integer> randomValue = new ArrayList<>();
-    for (int i = 0; i < racingCarCount; i++) {
-      randomValue.add(random.nextInt());
-    }
-    return randomValue;
-  }
-
-  public void moveAndStop() {
-    List<Integer> randomValue = createRandomValue();
-    for (int i = 0; i < carList.size(); i++) {
-      carList.get(i).move(randomValue.get(i));
-    }
-  }
-
-  public void startGame(ResultView resultView) {
-    for (int count = 0; count < turnCount; count++) {
-      moveAndStop();
-      resultView.moveResult(carList);
-    }
-  }
 
   public static void main(String[] args) {
     new RacingController();
