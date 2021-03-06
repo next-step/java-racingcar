b989d37cf5314d4674ddadbe64a402991cd5b02e
PandaHun <gnsgnsek@gmail.com>
2021-03-06 11:35:50 +0900

    feat: Create Input number of rounds
    - 게임 횟수 입력 기능 추가

--- src/main/java/racingcar/RacingMachine.java
+++ src/main/java/racingcar/RacingMachine.java
@@ -4,8 +4,10 @@ import racingcar.util.InputView;
 
 public class RacingMachine {
     private static String numberOfCars;
+    private static String numberOfRounds;
 
     public static void main(String[] args) {
         numberOfCars = InputView.getNumberOfCar();
+        numberOfRounds = InputView.getNumberOfRound();
     }
 }
--- src/main/java/racingcar/util/InputView.java
+++ src/main/java/racingcar/util/InputView.java
@@ -10,4 +10,9 @@ public class InputView {
         System.out.println("자동차 대수는 몇 대 인가요?");
         return scanner.nextLine();
     }
+
+    public static String getNumberOfRound() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextLine();
+    }
 }
