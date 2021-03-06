69ec3a85e04f5d96cab9bfed484d48f373859dae
JeongByeongHun <jbh1024@gmail.com>
2021-03-06 23:24:54 +0900

    Step2 String Calculator method & test case

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -1,4 +1,57 @@
 package stringCalculator;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class StringCalculator {
+    final String DELIMITER = " ";
+    final HashSet<String> FOUR_RULE_OPERATOR_SET = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
+
+    public int calculate(String input) {
+        String[] splitedInput = checkParam(input);
+        int result = Integer.parseInt(splitedInput[0]);
+        for (int i = 1; i < splitedInput.length; i+=2) {
+            result = operation(splitedInput[i], result, Integer.parseInt(splitedInput[i+1]));
+        }
+        return result;
+    }
+
+    private int operation(String operator, int number1, int number2) {
+        switch (operator){
+            case "+":
+                return number1 + number2;
+            case "-":
+                return number1 - number2;
+            case "*":
+                return number1 * number2;
+            case "/":
+                return number1 / number2;
+        }
+        return 0;
+    }
+
+    private String[] checkParam(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("input is null or empty");
+        }
+        String[] validateResult = input.split(DELIMITER);
+        if (validateResult.length % 2 == 0) {
+            throw new IllegalArgumentException("calculating param counts is not odd number");
+        }
+        for (int i = 0; i < validateResult.length; i++) {
+            if (i % 2 == 0) {
+                try {
+                    Integer.parseInt(validateResult[i]);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("calculating param is wrong : contained not number or wrong opterator");
+                }
+            } else if (!FOUR_RULE_OPERATOR_SET.contains(validateResult[i])) {
+                throw new IllegalArgumentException("calculating param is wrong : contained not number or wrong opterator");
+            } else if ("/".equals(validateResult[i]) && Integer.parseInt(validateResult[i+1])==0) {
+                throw new IllegalArgumentException("calculating param is wrong : can't divide by 0");
+            }
+        }
+        return validateResult;
+    }
 }
+
--- src/test/java/stringCalculator/StringCalculatorTest.java
+++ src/test/java/stringCalculator/StringCalculatorTest.java
@@ -1,11 +1,13 @@
 package stringCalculator;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class StringCalculatorTest {
 
@@ -20,62 +22,85 @@ class StringCalculatorTest {
     @ValueSource(strings = "2000 + 21")
     @DisplayName("Adding Test")
     void adddingTest(String input){
-
+        int result = stringCalculator.calculate(input);
+        assertThat(result).isEqualTo(2021);
     }
 
     @ParameterizedTest
     @ValueSource(strings = "2021 - 20")
     @DisplayName("Subtraction Test")
     void SubtractionTest(String input){
-
+        int result = stringCalculator.calculate(input);
+        assertThat(result).isEqualTo(2001);
     }
 
     @ParameterizedTest
     @ValueSource(strings = "12 * 23")
     @DisplayName("Multiplication  Test")
     void multiplicationTest(String input){
-
+        int result = stringCalculator.calculate(input);
+        assertThat(result).isEqualTo(276);
     }
 
     @ParameterizedTest
     @ValueSource(strings = "2020 / 10")
     @DisplayName("division Test")
     void divisionTest(String input){
-
+        int result = stringCalculator.calculate(input);
+        assertThat(result).isEqualTo(202);
     }
 
     @ParameterizedTest
     @ValueSource(strings = "2 + 3 * 4 /")
     @DisplayName("Invalid parameter Test - wrong param")
     void invalidParamTest1(String input){
-
+        assertThatIllegalArgumentException()
+                .isThrownBy(()->stringCalculator.calculate(input))
+                .withMessage("calculating param counts is not odd number");
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "T E S + T")
+    @ValueSource(strings = {"T E S + T", "2 + 3 % 4 / 2"})
     @DisplayName("Invalid parameter Test - not number")
     void invalidParamTest2(String input){
-
+        assertThatIllegalArgumentException()
+                .isThrownBy(()->stringCalculator.calculate(input))
+                .withMessage("calculating param is wrong : contained not number or wrong opterator");
     }
 
     @ParameterizedTest
-    @ValueSource(strings = "2 + 3 % 4 / 2")
-    @DisplayName("Invalid parameter Test - not four rule calcultation")
-    void invalidParamTest3(){
-
+    @ValueSource(strings = {"2 + 3 * 4 / 0", "20 / 0 * 4 / 5"})
+    @DisplayName("Invalid parameter Test - divide by 0")
+    void invalidParamTest3(String input){
+        assertThatIllegalArgumentException()
+                .isThrownBy(()->stringCalculator.calculate(input))
+                .withMessage("calculating param is wrong : can't divide by 0");
     }
 
-    @Test
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"\t", "\n"})
     @DisplayName("Invalid parameter Test - null or blank")
-    void invalidParamTest4(){
-
+    void invalidParamTest4(String input){
+        assertThatIllegalArgumentException()
+                .isThrownBy(()->stringCalculator.calculate(input))
+                .withMessage("input is null or empty");
     }
 
     @ParameterizedTest
     @ValueSource(strings = "2 + 3 * 4 / 2")
-    @DisplayName("Valid parameter Test - wrong String")
+    @DisplayName("Valid parameter Test")
     void validParamTest(String input){
+        int result = stringCalculator.calculate(input);
+        assertThat(result).isEqualTo(10);
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = "2 + 3 - 5 * 0 / 2 + 2")
+    @DisplayName("Valid parameter Test")
+    void validParamTest2(String input){
+        int result = stringCalculator.calculate(input);
+        assertThat(result).isEqualTo(2);
     }
 
 
