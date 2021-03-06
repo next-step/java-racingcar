0ab36c645a003f6dee8289b24fb1da3c3bca5084
Jake <cocahack@naver.com>
2021-03-07 02:13:36 +0900

    refactor: ResultView 의 출력 책임 위임 및 Racing 결합 제거

---
+++ src/main/java/racing/car/Car.java
@@ -0,0 +1,15 @@
+package racing.car;
+
+public class Car {
+
+    private int location = 0;
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void move() {
+        location++;
+    }
+
+}
