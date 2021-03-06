9ba67cbf74e9cdfa6a7a9e989333d6589381a17a
PandaHun <gnsgnsek@gmail.com>
2021-03-06 13:06:18 +0900

    feat: Create RacingMachin to start racing
    -게임 실행 객체 생성

--- src/main/java/racingcar/RacingMachine.java
+++ src/main/java/racingcar/RacingMachine.java
@@ -1,13 +1,27 @@
 package racingcar;
 
+import racingcar.domain.Cars;
 import racingcar.util.InputView;
+import racingcar.util.ResultView;
 
 public class RacingMachine {
-    private static String numberOfCars;
-    private static String numberOfRounds;
+    private static final int ROUND_END = 0;
+
+    private static int numberOfCars;
+    private static int numberOfRounds;
+    private static Cars cars;
 
     public static void main(String[] args) {
+        runMachine();
+    }
+
+    private static void runMachine() {
         numberOfCars = InputView.getNumberOfCar();
         numberOfRounds = InputView.getNumberOfRound();
+        cars = new Cars(numberOfCars);
+        ResultView.startGame();
+        while (numberOfRounds-- > ROUND_END) {
+            cars.move();
+        }
     }
 }
