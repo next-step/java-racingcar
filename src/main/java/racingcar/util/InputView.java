92397de4ab40f788b686e1c750729c898b336f64
PandaHun <gnsgnsek@gmail.com>
2021-03-06 13:04:26 +0900

    feat: Update Input string to integer
    - 입력을 문자열에서 숫자로 변경

--- src/main/java/racingcar/util/InputView.java
+++ src/main/java/racingcar/util/InputView.java
@@ -5,14 +5,32 @@ import java.util.Scanner;
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String DIGIT_REGEX = "^[-]?\\d+";
 
-    public static String getNumberOfCar() {
+    public static int getNumberOfCar() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.nextLine();
+        String input = scanner.nextLine();
+        validateInput(input);
+        return Integer.parseInt(input);
     }
 
-    public static String getNumberOfRound() {
+    public static int getNumberOfRound() {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        return scanner.nextLine();
+        String input = scanner.nextLine();
+        validateInput(input);
+        return Integer.parseInt(input);
     }
+
+    private static void validateInput(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("숫자를 입력해주세요");
+        }
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("숫자를 입력해주세요");
+        }
+        if (!input.matches(DIGIT_REGEX)) {
+            throw new IllegalArgumentException("입력한 " + input + "는 숫자가 아닙니다");
+        }
+    }
+
 }
