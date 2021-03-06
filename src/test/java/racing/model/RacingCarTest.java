32051ab4094e2f95285f4712e04b1a76b1b79699
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-06 23:40:36 +0900

    test: 테스트용 자동차 생성 로직 수정

--- src/test/java/racing/model/RacingCarTest.java
+++ src/test/java/racing/model/RacingCarTest.java
@@ -10,37 +10,36 @@ import org.junit.jupiter.api.Test;
 
 class RacingCarTest {
 
-  private RacingCar racingCar;
+    private RacingCar racingCar;
 
-  @BeforeEach
-  void init() {
-    racingCar = new RacingCar(0);
+    @BeforeEach
+    void init() {
+        racingCar = new RacingCar(1L, "BOOM", 0);
 
-  }
+    }
 
-  @DisplayName("자동차 이동 테스트")
-  @Test
-  void moveTest() {
-    //given
-    int moveCount = 5;
+    @DisplayName("자동차 이동 테스트")
+    @Test
+    void moveTest() {
+        //given
+        int moveCount = 5;
 
-    //when
-    racingCar.move(moveCount);
+        //when
+        racingCar.move(moveCount);
 
-    //then
-    assertEquals(5, racingCar.getPosition());
-  }
+        //then
+        assertEquals(5, racingCar.getPosition());
+    }
 
-  @DisplayName("자동차 이동거리가 4이상이면 전진하라는 신호를 보내는 메서드 테스트")
-  @Test
-  void isForward() {
-    assertTrue(racingCar.isForward(5));
-  }
+    @DisplayName("자동차 이동거리가 4이상이면 전진하라는 신호를 보내는 메서드 테스트")
+    @Test
+    void isForward() {
+        assertTrue(racingCar.isForward(5));
+    }
 
-  @DisplayName("자동차 이동거리가 4이상이면 전진하라는 신호를 보내는 메서드 테스트")
-  @Test
-  void isStop() {
-    assertFalse(racingCar.isForward(0));
-  }
-
+    @DisplayName("자동차 이동거리가 4이상이면 전진하라는 신호를 보내는 메서드 테스트")
+    @Test
+    void isStop() {
+        assertFalse(racingCar.isForward(0));
+    }
 }
