ec94c90d0405150d56bc92e49d90748261353112
ad7549 <ad7549@gmail.com>
2021-03-06 20:54:57 +0900

    test: Add CarTest

---
+++ src/test/java/racing/CarTest.java
@@ -0,0 +1,13 @@
+package racing;
+
+import calculator.StringCalculator;
+import org.junit.jupiter.api.BeforeEach;
+
+public class CarTest {
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car();
+    }
+}
