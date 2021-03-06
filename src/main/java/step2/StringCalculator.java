d0e2fc2cb857f06882537ec74aa522c84b94ec4a
Seonhyeong Park <44048622+seon54@users.noreply.github.com>
2021-03-06 10:28:36 +0900

    [step2] 문자열 계산기 리뷰 요청 드립니다 (#1910)
    * [step2] Add StringCaculator class
    
    * [step2] Update StringCalculator with handling exceptions
    
    * [step2] Add getResult method on StringCalculator
    
    * [step2] Update calculation methods
    
    * [step2] Add basic calculation tests
    
    * [step2] Add exception test and calculate method test
    
    * refactor: Add checkText, checkOperator, setText methods and update caculate method
    
    * test: Add test cases for basic caculation method tests
    
    * test: Update exception tests
    
    * fix: Fix calculate method
    
    * test: Update calculate test
    
    * test: Fix typo
    
    Co-authored-by: shpark <shpark@shparkui-MacBookAir.local>

---
+++ src/main/java/step2/StringCalculator.java
@@ -0,0 +1,88 @@
+package step2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringCalculator {
+
+    private static final String DELIMITER = " ";
+    private Set<String> opSet = new HashSet<>();
+    private String text;
+    private String[] textArray;
+    private int result;
+
+    public void checkText(String text) {
+        if (text == null || text.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void checkOperator(String[] textArray) {
+        for (int i = 1; i < textArray.length; i += 2) {
+            if (!opSet.contains(textArray[i])) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void setText(String text) {
+        checkText(text);
+        checkOperator(text.split(DELIMITER));
+        this.text = text;
+        this.textArray = text.split(DELIMITER);
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public StringCalculator() {
+        opSet.add("+");
+        opSet.add("-");
+        opSet.add("*");
+        opSet.add("/");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int calculate() {
+        result = Integer.parseInt(textArray[0]);
+
+        for (int i = 1; i < textArray.length; i += 2) {
+            int temp = 0;
+            int nextNum = Integer.parseInt(textArray[i + 1]);
+
+            switch (textArray[i]) {
+                case "+":
+                    temp = add(result, nextNum);
+                    break;
+                case "-":
+                    temp = subtract(result, nextNum);
+                    break;
+                case "*":
+                    temp = multiply(result, nextNum);
+                    break;
+                case "/":
+                    temp = divide(result, nextNum);
+                    break;
+            }
+            result = temp;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/step2/StringCalculatorTest.java
@@ -0,0 +1,128 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class StringCalculatorTest {
+
+    @Test
+    @DisplayName("덧셈 구현 확인")
+    void add() throws Exception {
+        // given
+        StringCalculator calculator = new StringCalculator();
+
+        // when
+        int result = calculator.add(1, 2);
+        int result2 = calculator.add(10, 40);
+        int result3 = calculator.add(130, 560);
+
+        // then
+        assertThat(result).isEqualTo(3);
+        assertThat(result2).isEqualTo(50);
+        assertThat(result3).isEqualTo(690);
+    }
+
+    @Test
+    @DisplayName("뺄셈 구현 확인")
+    void subtract() throws Exception {
+        // given
+        StringCalculator calculator = new StringCalculator();
+
+        // when
+        int result = calculator.subtract(3, 1);
+        int result2 = calculator.subtract(13, 21);
+        int result3 = calculator.subtract(400, 201);
+
+        // then
+        assertThat(result).isEqualTo(2);
+        assertThat(result2).isEqualTo(-8);
+        assertThat(result3).isEqualTo(199);
+    }
+
+    @Test
+    @DisplayName("곱셈 구현 확인")
+    void multiply() throws Exception {
+        // given
+        StringCalculator calculator = new StringCalculator();
+
+        // when
+        int result = calculator.multiply(3, 4);
+        int result2 = calculator.multiply(10, 14);
+        int result3 = calculator.multiply(201, 123);
+
+        // then
+        assertThat(result).isEqualTo(12);
+        assertThat(result2).isEqualTo(140);
+        assertThat(result3).isEqualTo(24723);
+
+    }
+
+    @Test
+    @DisplayName("나눗셈 구현 확인")
+    void divide() throws Exception {
+        // given
+        StringCalculator calculator = new StringCalculator();
+
+        // when
+        int result = calculator.divide(12, 2);
+        int result2 = calculator.divide(180, 20);
+        int result3 = calculator.divide(150, 23);
+
+        // then
+        assertThat(result).isEqualTo(6);
+        assertThat(result2).isEqualTo(9);
+        assertThat(result3).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("입력값이 null일 때 예외처리 확인")
+    void checkNull() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    calculator.setText(null);
+                });
+    }
+
+    @Test
+    @DisplayName("입력값이 빈 문자열일 때 예외처리 확인")
+    void checkEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    calculator.setText("");
+                });
+    }
+
+    @Test
+    @DisplayName("사칙연산 기호가 아닌 경우 예외처리 확인")
+    void checkOperator() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    calculator.setText("1 + 2 & 3 & 9");
+                });
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2 + 4 * 2,12", "4 - 2 * 3 * 2 / 3 + 10 - 23,-9", "100 - 50 / 40 * 12 + 35,47"})
+    @DisplayName("사칙 연산 모두 확인")
+    void calculate(String input, int expected) {
+        // given
+        StringCalculator calculator = new StringCalculator();
+
+        // when
+        calculator.setText(input);
+        int result = calculator.calculate();
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+}
