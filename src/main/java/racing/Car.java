435301e1a20516820937ce8600134fd77918b8e5
ad7549 <ad7549@gmail.com>
2021-03-06 21:33:59 +0900

    feat: Add getDistance(), move() to Car

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -1,4 +1,17 @@
 package racing;
 
 public class Car {
+    private int distance;
+
+    public Car() {
+        distance = 0;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void move() {
+        distance++;
+    }
 }
