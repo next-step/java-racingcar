4de73e31851632d9e0a39140c586e92d545ff170
Jake <cocahack@naver.com>
2021-03-06 20:06:50 +0900

    refactor: 리팩토링 후 main 메소드 수정

--- src/main/java/racing/RacingApplication.java
+++ src/main/java/racing/RacingApplication.java
@@ -1,26 +1,29 @@
 package racing;
 
+import racing.view.IntegerInputView;
+import racing.view.InteractiveInputView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class RacingApplication {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
+        InteractiveInputView<Integer> carSizeInputView = new IntegerInputView("자동차 대수는 몇 대 인가요?");
+        InteractiveInputView<Integer> movesInputView = new IntegerInputView("시도할 회수는 몇 회 인가요?");
         ResultView resultView = new ResultView();
 
-        RacingInput userInput = inputView.getUserInput();
+        int carSize = carSizeInputView.getUserInput();
+        int moves = movesInputView.getUserInput();
 
-        List<Car> cars = new ArrayList<>();
+        List<Car> carList = new ArrayList<>();
 
-        for (int i = 0; i < userInput.getCarSize(); i++) {
-            cars.add(new RandomlyMovingCar());
+        for (int i = 0; i < carSize; i++) {
+            carList.add(new RandomlyMovingCar());
         }
 
-        for (int i = 0; i < userInput.getMoves(); i++) {
-            cars.forEach(Car::move);
-            resultView.printCar(cars);
-        }
+        Racing racing = new Racing(carList, resultView, moves);
+        racing.start();
     }
 
 }
