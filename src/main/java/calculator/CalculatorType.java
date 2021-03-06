4dafab04dfa33f43fece6e544ca6b7ebe9edd58e
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-07 04:27:52 +0900

    feat : 기호별 타입 포함시킴

--- src/main/java/calculator/CalculatorType.java
+++ src/main/java/calculator/CalculatorType.java
@@ -2,23 +2,36 @@ package calculator;
 
 public enum CalculatorType {
 
-    ADDITION {
+    ADD {
         int calculate(int value, int value2) { return value + value2; }
     },
-    SUBSTRACTION {
+    SUBTRACT {
         int calculate(int value, int value2) {
             return value - value2;
         }
     },
-    MULTIPLICATION {
+    MULTIPLY {
         int calculate(int value, int value2) { return value * value2; }
     },
-    DIVISION {
+    DIVIDE {
         int calculate(int value, int value2) {
             return value / value2;
         }
     };
 
+    static CalculatorType calculatorType(String operator) {
+        switch (operator) {
+            case "+":
+                return CalculatorType.ADD;
+            case "-":
+                return CalculatorType.SUBTRACT;
+            case "*":
+                return CalculatorType.MULTIPLY;
+            case "/":
+                return CalculatorType.DIVIDE;
+        }
+        throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
+    }
 
     abstract int calculate(int value, int value2);
 
