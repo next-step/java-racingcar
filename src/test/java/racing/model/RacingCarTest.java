a2c3bed84732f4575bde43b118b4155148b4e39c
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-06 14:17:52 +0900

    test: RacingGame ,RacingCar 단위테스트 수정

--- src/test/java/racing/model/RacingCarTest.java
+++ src/test/java/racing/model/RacingCarTest.java
@@ -2,6 +2,7 @@ package racing.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,18 +10,37 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class RacingCarTest {
 
+  private RacingCar racingCar;
+
+  @BeforeEach
+  void init() {
+    racingCar = new RacingCar(0);
+
+  }
+
   @DisplayName("자동차 이동 테스트")
   @Test
   void moveTest() {
     //given
-    RacingCar car = new RacingCar(0);
     int moveCount = 5;
 
     //when
-    car.move(moveCount);
+    racingCar.move(moveCount);
 
     //then
-    assertEquals(5, car.getPosition());
+    assertEquals(5, racingCar.getPosition());
+  }
+
+  @DisplayName("자동차 이동거리가 4이상이면 전진하라는 신호를 보내는 메서드 테스트")
+  @Test
+  void isForward() {
+    assertTrue(racingCar.isForward(5));
+  }
+
+  @DisplayName("자동차 이동거리가 4이상이면 전진하라는 신호를 보내는 메서드 테스트")
+  @Test
+  void isStop() {
+    assertFalse(racingCar.isForward(0));
   }
 
 }
---
+++ src/test/java/racing/model/RacingGameTest.java
@@ -0,0 +1,85 @@
+package racing.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RacingGameTest {
+
+  private RacingGame racingGame;
+
+  List<RacingCar> init() {
+    racingGame = new RacingGame();
+    return racingGame.setUp(3);
+  }
+
+  @DisplayName("레이싱 게임 초기화 테스트")
+  @Test
+  void setUp() {
+    List<RacingCar> racingCar = init();
+
+    //then
+    assertEquals(3, racingCar.size());
+  }
+
+  @DisplayName("랜덤값이 자동차 갯수 만큼 생성되는지 테스트")
+  @Test
+  void createRandomValue() {
+    //given
+    List<RacingCar> racingCar = init();
+
+    //when
+    List<Integer> randomValue = racingGame.createRandomValue();
+
+    //then
+    assertEquals(3, randomValue.size());
+  }
+
+
+  @DisplayName("이동거리가 4이상일때 자동차 이동 테스트")
+  @Test
+  void move() {
+    //given
+    List<RacingCar> racingCar = init();
+    List<Integer> randomValue = createMoveValue(5, 7, 8);
+
+    //when
+    racingGame.moveAndStop(randomValue);
+
+    //then
+    assertEquals(5, racingCar.get(0).getPosition());
+    assertEquals(7, racingCar.get(1).getPosition());
+    assertEquals(8, racingCar.get(2).getPosition());
+  }
+
+  @DisplayName("이동거리가 4미만일때 자동차 정지 테스트")
+  @Test
+  void stop() {
+    //given
+    List<RacingCar> racingCar = init();
+    List<Integer> randomValue = createMoveValue(2, 3, 4);
+
+    //when
+    racingGame.moveAndStop(randomValue);
+
+    //then
+    assertEquals(1, racingCar.get(0).getPosition());
+    assertEquals(1, racingCar.get(1).getPosition());
+    assertEquals(1, racingCar.get(2).getPosition());
+  }
+
+  private List<Integer> createMoveValue(int firstCarMovePosition, int secondCarMovePosition,
+      int thirdCarMovePosition) {
+    List<Integer> randomValue = new ArrayList<>();
+    randomValue.add(firstCarMovePosition);
+    randomValue.add(secondCarMovePosition);
+    randomValue.add(thirdCarMovePosition);
+    return randomValue;
+  }
+
+
+}
