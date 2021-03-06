14fe2203669c2341cc7b7ee9fec8c9c53b91ac3d
yonghwa <qhdl0224@gmail.com>
2021-03-07 04:55:01 +0900

    [refactor] 연산 기호 값을 Map에 저장된 값에서 찾도록 수정

--- src/main/java/study/Operations.java
+++ src/main/java/study/Operations.java
@@ -1,5 +1,7 @@
 package study;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiFunction;
 
 public enum Operations {
@@ -10,24 +12,24 @@ public enum Operations {
 
     private String symbol;
     private BiFunction<Integer,Integer,Integer> biFunction;
-
+    private static Map<String,String> operators = new HashMap<>();
+    static {
+        for(Operations op : Operations.values()){
+            operators.put(op.symbol,op.name());
+        }
+    }
     Operations(String symbol,BiFunction<Integer,Integer,Integer> biFunction){
         this.symbol = symbol;
         this.biFunction = biFunction;
     }
-
     public static String findSymbol(String operation){
-        for(Operations oper : Operations.values()){
-            if(oper.symbol.equals(operation)){
-                return oper.name();
-            }
+        String findResult = operators.get(operation);
+        if(findResult != null){
+            return findResult;
         }
         throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
     }
-
     public Integer calculate(Integer leftOperand,Integer rightOperand){
         return this.biFunction.apply(leftOperand,rightOperand);
     }
-
-
 }
