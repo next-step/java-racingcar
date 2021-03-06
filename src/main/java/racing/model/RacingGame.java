67caf6f2306f06bc11b4c5babb3708781fde67fd
dhdydtn2019 <dhdydtn2019@gmail.com>
2021-03-06 12:45:32 +0900

    feat:레이싱게임 객체 추가
    레이싱 게임 객체:주요 비지니스로직를 담당하고 컨트롤러에서 넘어온 동작을 수행하는 객체

---
+++ src/main/java/racing/model/RacingGame.java
@@ -0,0 +1,46 @@
+package racing.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RacingGame {
+
+  private List<RacingCar> carList;
+
+  private Random random = new Random();
+
+  /**
+   * 입력 받은 값으로 자동차와 게임 턴 설정 메서드
+   */
+  public List<RacingCar> setup(int carCount) {
+    carList = new ArrayList<>();
+    for (int i = 0; i < carCount; i++) {
+      carList.add(new RacingCar(1));
+    }
+    return carList;
+  }
+
+  /**
+   * 자동차가 이동할 랜덤값 생성 메서드
+   */
+  public List<Integer> createRandomValue() {
+    List<Integer> randomValue = new ArrayList<>();
+    for (int i = 0; i < carList.size(); i++) {
+      randomValue.add(random.nextInt(10));
+    }
+    return randomValue;
+  }
+
+  /**
+   * 조건에 따라 자동차를 이동시켜주는 메서드
+   */
+  public List<RacingCar> moveAndStop(List<Integer> randomValue) {
+    for (int i = 0; i < carList.size(); i++) {
+      carList.get(i).move(randomValue.get(i));
+    }
+    return carList;
+  }
+
+
+}
