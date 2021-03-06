c644402816ebe53dada13ed8096367a385451a6a
Beenie93 <seongbeen93@naver.com>
2021-03-06 20:50:17 +0900

    feat: add a valid method for division by zero

--- src/main/java/calculator/enums/Operator.java
+++ src/main/java/calculator/enums/Operator.java
@@ -7,8 +7,12 @@ public enum Operator {
     ADD("+", (number1, number2) -> number1 + number2),
     SUBTRACT("-", (number1, number2) -> number1 - number2),
     MULTIPLY("*", (number1, number2) -> number1 * number2),
-    DIVIDE("/", (number1, number2) -> number1 / number2);
+    DIVIDE("/", (number1, number2) -> {
+        validateDivisionByZero(number2);
+        return number1 / number2;
+    });
 
+    private static int ZERO = 0;
     private String symbol;
     private BiFunction<Integer, Integer, Integer> expression;
 
@@ -22,6 +26,12 @@ public enum Operator {
                 .filter(operator -> symbol.equals(operator.getSymbol()))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("사칙연산 기호를 확인해주세요."));
+    }
+
+    private static void validateDivisionByZero(int number) {
+        if (number == ZERO) {
+            throw new IllegalArgumentException("\"/\"이 0으로 나누어지는지 확인해주세요.");
+        }
     }
 
     public String getSymbol() {
