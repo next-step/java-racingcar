ac57e67003543d198984235778e8dd0506e826bc
keke0123 <keke0123@naver.com>
2021-03-06 22:47:37 +0900

    요구사항 3 완성

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -10,10 +10,12 @@ import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTest {
   private Set<Integer> numbers;
@@ -42,5 +44,35 @@ public class SetTest {
   private static Stream<Arguments> generateNum() {
     return Stream.of(Arguments.of(1), Arguments.of(2), Arguments.of(3));
     // return null;
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = { "1:true", "true:false", "2:true", "3:true", "4:false", "5:false" }, delimiter = ':')
+  @DisplayName("1, 2, 3 이 들어갈때 true 값을, true, 4, 5 값이 들어갈땐 false 값을 통과하는 테스트")
+  void sizeTest2(Object obj, Boolean bool) {
+    //
+    // System.out.println("obj: " + obj);
+    // System.out.println("bool: " + bool);
+    // System.out.println("numbers.contains(obj): " + numbers.contains(obj));
+
+    // true 값이 들어올 경우 exception
+    if (!isInteger((String) obj)) {
+      assertThatThrownBy(() -> {
+        Integer.parseInt((String) obj);
+      }).isInstanceOf(NumberFormatException.class);
+      return;
+    }
+    int num = Integer.parseInt((String) obj);
+    // System.out.println("num: " + num);
+    assertEquals(numbers.contains(num), bool);
+  }
+
+  private Boolean isInteger(String str) {
+    try {
+      Integer.parseInt(str);
+      return true;
+    } catch (NumberFormatException e) {
+      return false;
+    }
   }
 }
