ac89c2eacff534331bd6c00072a6d72d59b57af5
ad7549 <ad7549@gmail.com>
2021-03-06 21:29:13 +0900

    test: Add test about distance, move to CarTest

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,7 +1,10 @@
 package racing;
 
-import calculator.StringCalculator;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
     private Car car;
@@ -9,5 +12,29 @@ public class CarTest {
     @BeforeEach
     void setUp() {
         car = new Car();
+    }
+
+    @Test
+    @DisplayName("distance의 초기값은 0이어야 한다.")
+    void should_ReturnZeroToGetDistance_When_Init() {
+        final int expected = 0;
+
+        final int distance = car.getDistance();
+
+        assertThat(distance).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("distance는 move한 횟수와 같은 값이어야 한다.")
+    void should_EqualGetDistanceToMoveCount() {
+        final int expected = 4;
+
+        for (int i = 0; i < expected; i++) {
+            car.move();
+        }
+
+        final int distance = car.getDistance();
+
+        assertThat(distance).isEqualTo(expected);
     }
 }
