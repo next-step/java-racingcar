2623ba231fe5ab25f112172986619c4f1081857d
cptbong <cptbbong@gmail.com>
2021-03-06 14:49:06 +0900

    docs(todo) - InputView리팩토링.

--- TodoRefactor.md
+++ TodoRefactor.md
@@ -4,8 +4,8 @@
     * ~~반복되는 행위는 이전 미션에서 테스트하실 때 사용하신 어노테이션들을 사용하여 중복 코드를 제거~~
         * Q : 랜덤값 테스트도... ParameterizedTest가 되려나?
 * Improve InputView
-    * InputView에서 이름 그대로 입력에 대한 책임을 가지고 수행하면 더 응집력있는 객체가 될 것 같아요!
-      Scanner를 InputView에서 관리하도록 하면 어떨까요 :)
+    * ~~InputView에서 이름 그대로 입력에 대한 책임을 가지고 수행하면 더 응집력있는 객체가 될 것 같아요!
+      Scanner를 InputView에서 관리하도록 하면 어떨까요 :)~~
 * Add First Class Collection
     * Main에서 너무 많은 기능을 수행하여 OCP, DIP 위배. Client수정 없도록 바꿔보자. 여러 자동차를 관리하는 Cars 객체를 만들며, 일급컬렉션 이해하기.
 
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -8,19 +8,17 @@ import racingcar.view.ResultView;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Racing {
     public static void main(String[] args) {
         List<Car> cars = new ArrayList<>();
 
-        Scanner scanner = new Scanner(System.in);
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
         inputView.print("자동차 대수는 몇 대 인가요?");
-        int carCount = scanner.nextInt();
+        int carCount = inputView.scanner.nextInt();
         inputView.print("시도할 회수는 몇 회 인가요?");
-        int forwartCount = scanner.nextInt();
+        int forwartCount = inputView.scanner.nextInt();
         Random random = new Random();
 
         for (int i = 0; i < carCount; i++) {
