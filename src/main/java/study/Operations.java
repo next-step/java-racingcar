f2ef6d7a5226241d642664ba90de51eda99c5e84
yonghwa <qhdl0224@gmail.com>
2021-03-06 13:18:35 +0900

    [style] 코드의 가독성을 위해 띄어쓰기, 줄바꿈 변경

--- src/main/java/study/Operations.java
+++ src/main/java/study/Operations.java
@@ -3,13 +3,14 @@ package study;
 import java.util.function.BiFunction;
 
 public enum Operations {
-    PLUS("+",(a,b)->a+b),
-    MINUS("-",(a,b)->a-b),
-    MULTIPLY("*",(a,b)->a*b),
-    DIVIDE("/",(a,b)->a/b);
+    PLUS("+" , (a , b)-> a + b),
+    MINUS("-", (a , b)-> a - b),
+    MULTIPLY("*", (a , b)-> a * b),
+    DIVIDE("/", (a , b)-> a / b);
 
     private String symbol;
     private BiFunction<Integer,Integer,Integer> biFunction;
+
     Operations(String symbol,BiFunction<Integer,Integer,Integer> biFunction){
         this.symbol = symbol;
         this.biFunction = biFunction;
