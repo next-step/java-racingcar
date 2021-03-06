15fd205de1a656fd635ed0d8e4cec318d8b7a449
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-03-06 10:29:13 +0900

    feat: 이동 할 횟수 입력 기능
    이동 할 횟수 입력 기능 추가

--- src/main/java/study/step3/RacingCar.java
+++ src/main/java/study/step3/RacingCar.java
@@ -5,11 +5,11 @@ public class RacingCar {
     public void start() {
 
         InputView inputView = new InputView();
+
         int numberOfCars = inputView.enterNumberOfCars();
+        int numberOfAttempts = inputView.enterNumberOfAttempts();
+
         System.out.println("입력 한 자동차 대수는 : " + numberOfCars);
-
+        System.out.println("입력 한 이동할 횟수는 : " + numberOfAttempts);
     }
-
-
-
 }
