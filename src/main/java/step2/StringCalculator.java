aebb5f53ea3dae084698946eec5cd13daeebd49f
shkwon <oper912@naver.com>
2021-03-07 01:31:42 +0900

    feat: 문자열 수식 계산 구현

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -2,8 +2,10 @@ package step2;
 
 public class StringCalculator {
 
-    public static void calculate(String input) {
+    public static long calculate(String input) {
         validate(input);
+        String[] arr = input.split(" ");
+        return getResult(arr);
     }
 
     private static void validate(String input) {
@@ -13,5 +15,14 @@ public class StringCalculator {
         if (input.isEmpty()) {
             throw new IllegalArgumentException(CalculatorException.INPUT_BLANK);
         }
+    }
+
+    private static long getResult(String[] arr) {
+        long result = Long.parseLong(arr[0]);
+        for (int i = 1; i < arr.length; i += 2) {
+            Operator operator = Operator.findOperator(arr[i]);
+            result = operator.calculate(result, Long.parseLong(arr[i + 1]));
+        }
+        return result;
     }
 }
