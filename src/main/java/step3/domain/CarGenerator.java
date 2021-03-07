ff4c0f2da7d6240f4225d72ee9e52eb997ba068e
seokrae <kslbsh@gmail.com>
2021-03-07 21:21:11 +0900

    refactor: for문을 IntStream으로 변경

--- src/main/java/step3/domain/CarGenerator.java
+++ src/main/java/step3/domain/CarGenerator.java
@@ -2,6 +2,9 @@ package step3.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static step3.util.Constants.START_IDX;
 
 /**
  * 자동차의 생성을 관리할 클래스
@@ -12,9 +15,8 @@ public final class CarGenerator {
 
     public static List<Car> of(int carSize) {
         List<Car> cars = new ArrayList<>();
-        for(int i = 0 ; i < carSize ; i++) {
-            cars.add(Car.of());
-        }
+        IntStream.range(START_IDX, carSize)
+                .forEach(value -> cars.add(Car.of()));
         return cars;
     }
 }
