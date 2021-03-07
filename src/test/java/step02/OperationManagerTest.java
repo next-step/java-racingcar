7fc0cb2e598f10780e4e513fbe50be2c97ccab32
sonAjin <sonajin11@gmail.com>
2021-03-07 20:13:18 +0900

    (test) OperationManager test 코드 추가

---
+++ src/test/java/step02/OperationManagerTest.java
@@ -0,0 +1,35 @@
+package step02;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OperationManagerTest {
+
+    @ParameterizedTest
+    @MethodSource("OperationManagerTestValue")
+    @DisplayName("OperationManager 사칙연산 대한 테스트")
+    void num(String symbol, int first, int second, int expected) {
+        assertThat(OperationManager.calculate(symbol, first, second)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> OperationManagerTestValue() {
+        return Stream.of(
+                Arguments.of("+", 1, 2, 3),
+                Arguments.of("-", 1, 2, -1),
+                Arguments.of("-", 5, 1, 4),
+                Arguments.of("*", 1, 2, 2),
+                Arguments.of("*", 0, 5, 0),
+                Arguments.of("*", 0, 3, 0),
+                Arguments.of("*", -3, 6, -18),
+                Arguments.of("/", 1, 3, 0),
+                Arguments.of("/", 5, 3, 1),
+                Arguments.of("/", 0, 1, 0)
+        );
+    }
+}
