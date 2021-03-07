060c70e7a1c93de32b9d358abf0a444fff486f25
Donghui-Lee <ehdgml3206@naver.com>
2021-03-07 21:43:57 +0900

    refactor: 한 클래스내에서만 사용하여 상수 클래스 제거

--- src/main/java/study/step3/RacingService.java
+++ src/main/java/study/step3/RacingService.java
@@ -1,10 +1,15 @@
 package study.step3;
 
-import study.step3.constant.RacingConst;
 import study.step3.ui.InputView;
 import study.step3.utils.Printer;
 
 public class RacingService {
+    public String MESSAGE_INPUT_CAR = "자동차 대수는 몇 대 인가요?";
+    public String MESSAGE_INPUT_ROUND = "시도할 회수는 몇 회 인가요?";
+    public String MESSAGE_RACING_START = "자동차 경주를 시작합니다.";
+    public String MESSAGE_ROUND = "라운드";
+    public String PRINT_TOKEN = "-";
+
     private InputView inputView;
 
     public RacingService() {
@@ -12,17 +17,17 @@ public class RacingService {
     }
 
     public void play() {
-        int carCount = inputView.inputCount(RacingConst.MESSAGE_INPUT_CAR);
-        int round = inputView.inputCount(RacingConst.MESSAGE_INPUT_ROUND);
+        int carCount = inputView.inputCount(MESSAGE_INPUT_CAR);
+        int round = inputView.inputCount(MESSAGE_INPUT_ROUND);
 
         Racing racing = new Racing();
         racing.setUp(carCount);
 
-        Printer.println(RacingConst.MESSAGE_RACING_START);
+        Printer.println(MESSAGE_RACING_START);
         for(int i=0; i<round; i++) {
-            Printer.println((i+1) + RacingConst.MESSAGE_ROUND);
+            Printer.println((i+1) + MESSAGE_ROUND);
             racing.run();
-            racing.printRacingResult(RacingConst.PRINT_TOKEN);
+            racing.printRacingResult(PRINT_TOKEN);
         }
     }
 }
