049c123a1a473e00c52f06d3906dd904f278fada
yonghwa <qhdl0224@gmail.com>
2021-03-07 16:12:57 +0900

    [feat] 사칙연산 enum class 수정, 연산자 찾는 기능 추가

--- src/main/java/study/Operations.java
+++ src/main/java/study/Operations.java
@@ -1,7 +1,6 @@
 package study;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
 public enum Operations {
@@ -12,24 +11,18 @@ public enum Operations {
 
     private String symbol;
     private BiFunction<Integer,Integer,Integer> biFunction;
-    private static Map<String,String> operators = new HashMap<>();
-    static {
-        for(Operations op : Operations.values()){
-            operators.put(op.symbol,op.name());
-        }
-    }
+
     Operations(String symbol,BiFunction<Integer,Integer,Integer> biFunction){
         this.symbol = symbol;
         this.biFunction = biFunction;
     }
-    public static String findSymbol(String operation){
-        String findResult = operators.get(operation);
-        if(findResult != null){
-            return findResult;
-        }
-        throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
+    public static Operations getOperator(String operation){
+        return Arrays.stream(Operations.values())
+                .filter(op->op.symbol.equals(operation))
+                .findAny()
+                .orElseThrow(()->new IllegalArgumentException("사칙연산 기호가 아닙니다."));
     }
     public Integer calculate(Integer leftOperand,Integer rightOperand){
-        return this.biFunction.apply(leftOperand,rightOperand);
+        return biFunction.apply(leftOperand,rightOperand);
     }
 }
