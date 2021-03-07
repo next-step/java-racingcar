5f5b96252a40d006e2074a348e0edba394e713fc
yonghwa <qhdl0224@gmail.com>
2021-03-07 21:30:23 +0900

    [feat] ResultView 클래스 추가

---
+++ src/main/java/study/step3/ResultView.java
@@ -0,0 +1,17 @@
+package study.step3;
+
+import java.util.List;
+
+public class ResultView {
+    public static void output(List<Car> cars){
+        for(Car car : cars){
+            int position = car.move();
+            printPosition(position);
+        }
+    }
+    public static void printPosition(int position){
+        for(int i=0;i<position;i++){
+            System.out.print("-");
+        }
+    }
+}
