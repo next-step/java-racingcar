b6f436a73e304cba0539bb5a45e07d5797197ccb
koola97620 <koola976@gmail.com>
2021-03-08 01:25:58 +0900

    feat: 자동차경주 - 입력 로직 기능 구현

---
+++ src/test/java/racingCar/InputView.java
@@ -0,0 +1,11 @@
+package racingCar;
+
+import java.util.Scanner;
+
+public class InputView {
+  public int inputNumber() {
+    Scanner scanner = new Scanner(System.in);
+    return scanner.nextInt();
+  }
+
+}
