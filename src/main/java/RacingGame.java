480b9368b13ca084381b0d48c7a81a6c0c5f5b28
weleek <weleek@gmail.com>
2021-03-07 12:57:49 +0900

    refactor: 게임 시작 함수 예외처리 추가

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -8,17 +8,23 @@ public class RacingGame {
         scanner = new Scanner(System.in);
     }
 
+    public void end() {
+        if (scanner != null) scanner.close();
+    }
+
     private int inputIntArgument(String description){
         System.out.println(description);
         return scanner.nextInt();
     }
 
     public void start() {
-        int numberOfCars = inputIntArgument("자동차 대수는 몇 대 인가요?");
-        int numberOfAttempts = inputIntArgument("시도할 회수는 몇 회 인가요?");
+        try {
+            int numberOfCars = inputIntArgument("자동차 대수는 몇 대 인가요?");
+            int numberOfAttempts = inputIntArgument("시도할 회수는 몇 회 인가요?");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            end();
+        }
     }
 
-    public void end() {
-        scanner.close();
-    }
 }
