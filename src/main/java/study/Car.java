1cd2bba14ffd48582a7179828249c1e85666c7f4
yonghwa <qhdl0224@gmail.com>
2021-03-07 20:09:54 +0900

    [feat] 자동차 클래스 및 랜덤숫자 체크 클래스 추가

---
+++ src/main/java/study/Car.java
@@ -0,0 +1,14 @@
+package study;
+
+public class Car {
+    private int position;
+    public Car(){
+        this.position = 1;
+    }
+    public void move(){
+        if(NumberChecker.checkCondition()){
+            position = position + 1;
+        }
+
+    }
+}
---
+++ src/main/java/study/NumberChecker.java
@@ -0,0 +1,13 @@
+package study;
+
+import java.util.Random;
+
+public class NumberChecker {
+    public static boolean checkCondition() {
+        Random random = new Random();
+        if(random.nextInt(10) >= 4){
+            return true;
+        }
+        return false;
+    }
+}
