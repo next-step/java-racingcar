aaf6eb8dae54206464e7e7414675929c571ef1b5
wkdehdlr <wkdehdlr@gmail.com>
2021-03-07 20:38:46 +0900

    [feat]Car도메인 전진조건 테스트작성

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -1,19 +1,62 @@
 package step3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Random;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.powermock.reflect.Whitebox;
 
 public class CarTest {
 
+    private final Random random = mock(Random.class);
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        this.car = new Car();
+    }
+
     @DisplayName("자동차 이동 테스트")
     @ParameterizedTest
-    @CsvSource(value = {"4,4", "5,5"})
-    void move(int distance, int exp) {
-        Car car = new Car();
-        car.move(distance);
+    @CsvSource(value = {"true, 1", "false, 0"})
+    void move(boolean isMove, int exp) {
+        car.move(isMove);
         assertEquals(exp, car.getPosition());
+    }
+
+    @DisplayName("random 값이 4 이상일 경우만 전진")
+    @ParameterizedTest
+    @CsvSource(value = {"4", "5", "6", "7", "8", "9"})
+    void go(int input) {
+        mockRandom(input);
+        int randomValue = RandomUtil.nextInt(input);
+
+        assertTrue(car.isMove(randomValue));
+        verify(random).nextInt(input);
+    }
+
+    @DisplayName("random 값이 4 미만일 경우 멈춤")
+    @ParameterizedTest
+    @CsvSource(value = {"0", "1", "2", "3"})
+    void stop(int input) {
+        mockRandom(input);
+        int randomValue = RandomUtil.nextInt(input);
+
+        assertFalse(car.isMove(randomValue));
+        verify(random).nextInt(input);
+    }
+
+    private void mockRandom(int input) {
+        when(random.nextInt(anyInt())).thenReturn(input);
+        Whitebox.setInternalState(RandomUtil.class, random);
     }
 }
