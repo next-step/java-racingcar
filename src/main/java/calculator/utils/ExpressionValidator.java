62c54d43e970a8f6bb8441af9904f15b2ee1d5e3
Beenie93 <seongbeen93@naver.com>
2021-03-07 14:04:08 +0900

    refactor: change method name "validateLength" into "validateMinimumLength"

--- src/main/java/calculator/utils/ExpressionValidator.java
+++ src/main/java/calculator/utils/ExpressionValidator.java
@@ -4,6 +4,7 @@ public class ExpressionValidator {
 
     private static final int EMPTY = 0;
     private static final int LENGTH_MIN = 3;
+    private static final int EVEN_NUMBER = 0;
 
     private ExpressionValidator() {
     }
@@ -20,9 +21,9 @@ public class ExpressionValidator {
         }
     }
 
-    public static void validateLength(String[] expression) {
+    public static void validateMinimumLength(String[] expression) {
         if (expression.length < LENGTH_MIN) {
-            throw new IllegalArgumentException("입력 값의 길이를 확인해주세요.");
+            throw new IllegalArgumentException("입력 수식의 길이가 3이상인지 확인해주세요.");
         }
     }
 }
