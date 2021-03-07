67f018fdb56865f912b19417dff47b4f9fb226d5
abcdefg <choiyurim@naver.com>
2021-03-07 21:03:35 +0900

    feat(ResultView): add ResultView

---
+++ src/main/java/step3/ResultView.java
@@ -0,0 +1,15 @@
+package step3;
+
+public class ResultView {
+
+    public static void print(int move) {
+        for (int i = 0; i < move; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public static void print(String a) {
+
+    }
+}
