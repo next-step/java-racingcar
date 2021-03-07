3323733fb1faccac5222b83035afb4c5231b5eaf
Donghui-Lee <ehdgml3206@naver.com>
2021-03-07 20:33:14 +0900

    feat: 메시지를 출력하는 Util 클래스 추가

---
+++ src/main/java/study/step3/utils/Printer.java
@@ -0,0 +1,11 @@
+package study.step3.utils;
+
+public class Printer {
+    public final static void println(String message) {
+        System.out.println(message);
+    }
+
+    public final static void print(String message) {
+        System.out.print(message);
+    }
+}
