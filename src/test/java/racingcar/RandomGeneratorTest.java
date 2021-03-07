bb68fea8ce4d326add1caf95ae266339dbd96e0f
PandaHun <gnsgnsek@gmail.com>
2021-03-07 20:48:41 +0900

    test: Create Random Generator test
    -난수 발생기 테스트 작성

---
+++ src/test/java/racingcar/RandomGeneratorTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RandomGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomGeneratorTest {
+
+    @DisplayName("0 ~ 9 사이의 난수를 발생시킨다")
+    @Test
+    void testRandomGenerator() {
+        assertThat(RandomGenerator.getRandomValue()).isBetween(0, 9);
+    }
+}
