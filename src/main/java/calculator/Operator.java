e52da08c61674210cf4b4773e81902b6f3db47df
Jake <cocahack@naver.com>
2021-03-07 20:02:03 +0900

    refactor: Operator 생성 과정에서 switch 문 제거

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,33 +1,28 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.function.BinaryOperator;
 
 public enum Operator {
 
-    ADDITION((x, y) -> x + y),
-    SUBTRACTION((x, y) -> x - y),
-    MULTIPLICATION((x, y) -> x * y),
-    DIVISION((x, y) -> x / y);
+    ADDITION("+", (x, y) -> x + y),
+    SUBTRACTION("-", (x, y) -> x - y),
+    MULTIPLICATION("*", (x, y) -> x * y),
+    DIVISION("/", (x, y) -> x / y);
 
+    String symbol;
     BinaryOperator<Integer> calculation;
 
-    Operator(BinaryOperator<Integer> calculation) {
+    Operator(String symbol, BinaryOperator<Integer> calculation) {
+        this.symbol = symbol;
         this.calculation = calculation;
     }
 
     public static Operator createOperator(String str) {
-        switch (str) {
-            case "+":
-                return ADDITION;
-            case "-":
-                return SUBTRACTION;
-            case "*":
-                return MULTIPLICATION;
-            case "/":
-                return DIVISION;
-            default:
-                throw new IllegalArgumentException("지원하지 않는 연산자입니다. 사칙 연산자를 사용해주세요.");
-        }
+        return Arrays.stream(Operator.values())
+                     .filter(operator -> operator.symbol.equals(str))
+                     .findFirst()
+                     .orElseThrow(() -> new IllegalArgumentException("지원하지 않는 연산자입니다. 사칙 연산자를 사용해주세요."));
     }
 
 }
