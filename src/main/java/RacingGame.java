f92c5ca5aec93801872bec350f6b6363827fd802
weleek <weleek@gmail.com>
2021-03-07 12:55:59 +0900

    feat: 게임 시작 조건 입력 기능 구현

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,7 +1,24 @@
 import java.util.Scanner;
 
 public class RacingGame {
+
+    private Scanner scanner;
+
+    RacingGame () {
+        scanner = new Scanner(System.in);
+    }
+
+    private int inputIntArgument(String description){
+        System.out.println(description);
+        return scanner.nextInt();
+    }
+
     public void start() {
-        Scanner scanner = new Scanner(System.in);
+        int numberOfCars = inputIntArgument("자동차 대수는 몇 대 인가요?");
+        int numberOfAttempts = inputIntArgument("시도할 회수는 몇 회 인가요?");
+    }
+
+    public void end() {
+        scanner.close();
     }
 }
