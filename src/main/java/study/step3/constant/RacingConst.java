419a1d4f90e120789557bef4892ffaced830cbff
Donghui-Lee <ehdgml3206@naver.com>
2021-03-07 20:36:16 +0900

    feat: 상수 클래스 추가

---
+++ src/main/java/study/step3/constant/RacingConst.java
@@ -0,0 +1,9 @@
+package study.step3.constant;
+
+public class RacingConst {
+	public static final String MESSAGE_INPUT_CAR = "자동차 대수는 몇 대 인가요?";
+	public static final String MESSAGE_INPUT_ROUND = "시도할 회수는 몇 회 인가요?";
+	public static final String MESSAGE_RACING_START = "자동차 경주를 시작합니다.";
+	public static final String MESSAGE_ROUND = "라운드";
+ 	public static final String PRINT_TOKEN = "-";
+}
