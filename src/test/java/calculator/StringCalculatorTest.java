0892ea1e3dddf5e64f76f272e66741a3446037e1
Beenie93 <seongbeen93@naver.com>
2021-03-07 14:46:28 +0900

    refactor: apply @ParameterizedTest instead of @Test

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,18 +1,20 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class StringCalculatorTest {
 
-    @Test
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource(value = {"5 + 5 + 10 : 20", "1 + 2 + 3 + 4 : 10", "5 + 2 + 4 : 11"}, delimiter = ':')
     @DisplayName("덧셈")
-    public void add() throws Exception {
+    public void add(String expression, int expected) throws Exception {
         //given
-        String expression = "5 + 5 + 10";
 
         //when
         Expression splitExpression = new Expression(expression);
@@ -20,14 +22,14 @@ class StringCalculatorTest {
         int result = stringCalculator.calculate(splitExpression);
 
         //then
-        assertThat(result).isEqualTo(20);
+        assertThat(result).isEqualTo(expected);
     }
-
-    @Test
-    @DisplayName("뺼셈")
-    public void subtract() throws Exception {
+    
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource(value = {"5 - 5 - 10 : -10", "1 - 2 - 3 - 4 : -8", "5 - 2 - 4 : -1"}, delimiter = ':')
+    @DisplayName("뺄셈")
+    public void subtract(String expression, int expected) throws Exception {
         //given
-        String expression = "5 - 100 - 20";
 
         //when
         Expression splitExpression = new Expression(expression);
@@ -35,14 +37,14 @@ class StringCalculatorTest {
         int result = stringCalculator.calculate(splitExpression);
 
         //then
-        assertThat(result).isEqualTo(-115);
+        assertThat(result).isEqualTo(expected);
     }
 
-    @Test
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource(value = {"5 * 5 * 10 : 250", "3 * 3 * 3 : 27", "5 * -2 * 10 : -100"}, delimiter = ':')
     @DisplayName("곱셈")
-    public void multiply() throws Exception {
+    public void multiply(String expression, int expected) throws Exception {
         //given
-        String expression = "5 * 100 * 20";
 
         //when
         Expression splitExpression = new Expression(expression);
@@ -50,14 +52,14 @@ class StringCalculatorTest {
         int result = stringCalculator.calculate(splitExpression);
 
         //then
-        assertThat(result).isEqualTo(10000);
+        assertThat(result).isEqualTo(expected);
     }
 
-    @Test
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource(value = {"5 / 5 : 1", "27 / 3 / 2 : 4", "5 / -2 :-2"}, delimiter = ':')
     @DisplayName("나눗셈")
-    public void divide() throws Exception {
+    public void divide(String expression, int expected) throws Exception {
         //given
-        String expression = "100 / 20 / 2";
 
         //when
         Expression splitExpression = new Expression(expression);
@@ -65,14 +67,14 @@ class StringCalculatorTest {
         int result = stringCalculator.calculate(splitExpression);
 
         //then
-        assertThat(result).isEqualTo(2);
+        assertThat(result).isEqualTo(expected);
     }
 
-    @Test
+    @ParameterizedTest(name = "{0}에 올바르지 않는 사칙 연산 포함")
+    @ValueSource(strings = {"5 * 3 ! 1", "2 / 2 @ 1", "4 + 2 # 1", "4 - 1 $ 1"})
     @DisplayName("올바르지 않은 사칙 연산")
-    public void validateSymbol() throws Exception {
+    public void validateSymbol(String expression) throws Exception {
         //given
-        String expression = "10 ^ 20 / 2";
 
         //when
 
@@ -81,11 +83,11 @@ class StringCalculatorTest {
                 .isThrownBy(() -> new Expression(expression));
     }
 
-    @Test
+    @ParameterizedTest(name = "{0} = {1}")
+    @CsvSource(value = {"5 + 5 / 2 * 3 : 15", "27 / 3 + 2 * 2 : 22", "5 / -2 + 2 * 100 :0"}, delimiter = ':')
     @DisplayName("모든 사칙 연산")
-    public void validateAllSymbol() throws Exception {
+    public void validateAllSymbol(String expression, int expected) throws Exception {
         //given
-        String expression = "10 + 20 / 2 * 2";
 
         //when
         Expression splitExpression = new Expression(expression);
@@ -93,6 +95,6 @@ class StringCalculatorTest {
         int result = stringCalculator.calculate(splitExpression);
 
         //then
-        assertThat(result).isEqualTo(30);
+        assertThat(result).isEqualTo(expected);
     }
 }
