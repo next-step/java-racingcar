5513d1b1622206283307270307b0579895077999
PandaHun <gnsgnsek@gmail.com>
2021-03-08 23:12:43 +0900

    refactor: update car test
    - 고정 값을 갖는 엔진 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,6 +3,8 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+import racingcar.domain.FixedEngine;
+import racingcar.domain.RandomEngine;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,15 +13,23 @@ public class CarTest {
     @DisplayName("자동차를 생성한다")
     @Test
     void createCarTest() {
-        Car car = new Car();
-        assertThat(car.showStatus()).isEqualTo("");
+        Car car = new Car(new RandomEngine());
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
-    @DisplayName("자동차는 이동하거나 이동하지 않는다")
+    @DisplayName("자동차는 이동한다")
     @Test
     void moveCarTest() {
-        Car car = new Car();
+        Car car = new Car(new FixedEngine(5));
         car.move();
-        assertThat(car.showStatus()).isIn("", "-");
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("자동차는 이동하지 않는다")
+    @Test
+    void moveCarTestSecond() {
+        Car car = new Car(new FixedEngine(3));
+        car.move();
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
