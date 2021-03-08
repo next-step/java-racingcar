67472f108784810072a6968f8cd649260c7c526f
Jake <cocahack@naver.com>
2021-03-08 20:44:40 +0900

    refactor: 매직 넘버 제거

--- src/main/java/racing/car/RandomMovement.java
+++ src/main/java/racing/car/RandomMovement.java
@@ -7,9 +7,12 @@ public class RandomMovement implements Movement {
     private static final Random RANDOM = new Random();
     private static final int RANDOM_BOUND = 10;
     private static final int MOVE_THRESHOLD = 4;
+    private static final int STAY = 0;
+    private static final int MOVE = 1;
+
 
     @Override
     public int move() {
-        return RANDOM.nextInt(RANDOM_BOUND) < MOVE_THRESHOLD ? 0 : 1;
+        return RANDOM.nextInt(RANDOM_BOUND) < MOVE_THRESHOLD ? STAY : MOVE;
     }
 }
