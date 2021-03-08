415830860d77f0a0ad5c12af5c925b5538b785fe
HJ-Woo <hjwoou@gmail.com>
2021-03-09 00:32:20 +0900

    refactor(Calculator): input이 empty인지 확인 및 split하는 로직 분리

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,10 @@
 public class Calculator {
 
+    public String[] splitInput(String input){
+        Util.checkEmpty(input);
+        return input.split(" ");
+    }
+
     /*
     * 문자열을 입력받아 각 연산자와 피연산자로 구분하여 계산하고 결과값을 반환한다.
     * @param input 입력받을 문자; 비어있지 않아야한다.
@@ -7,8 +12,7 @@ public class Calculator {
     * @throws IllegalArgumentException 비어있는 문자열을 받을시 발생한다.
     * */
     public int calculate(String input){
-        Util.checkEmpty(input);
-        String[] numArr = input.split(" ");
+        String[] numArr = splitInput(input);
         int result = Util.checkOperand(numArr[0]);
         for(int i=1; i<numArr.length; i+=2){
             int operand = Util.checkOperand(numArr[i+1]);
