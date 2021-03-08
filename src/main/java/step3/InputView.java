9e50805203b22e70a50d6906cb447ed12bf8df52
shpark <shpark@shparkui-MacBookAir.local>
2021-03-08 23:18:22 +0900

    feat: Add InputView class

---
+++ src/main/java/step3/InputView.java
@@ -0,0 +1,19 @@
+package step3;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public InputView() {}
+
+    public int[] getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int car = scanner.nextInt();
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int trial = scanner.nextInt();
+
+        return new int[]{car, trial};
+    }
+}
--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -1,18 +1,29 @@
 package step3;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class RacingCar {
 
     public RacingCar() {}
 
     void start() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int car = scanner.nextInt();
+        InputView inputView = new InputView();
+        int car = inputView.getInput()[0];
+        int trial = inputView.getInput()[1];
 
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int trial = scanner.nextInt();
+        System.out.println("\n실행 결과");
+        Random random = new Random();
+
+        while (0 < trial) {
+            trial--;
+
+            for (int i = 0; i < car; i++) {
+                int number = random.nextInt(10);
+                System.out.println(String.format("%" + random.nextInt(10) + "s", "-")
+                        .replace(" ", "-"));
+            }
+            System.out.println();
+        }
 
     }
 
