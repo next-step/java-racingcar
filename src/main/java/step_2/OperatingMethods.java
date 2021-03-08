d59e435e90eba4be67a818a89dc62fc5fcd9eebb
n0roo <n0roo@upstair.io>
2021-03-08 19:16:02 +0900

    refactor: Java Racing Car - Step2 Review Apply
    - OperatingMethods.java  Detach `valueOfCalculate` to `valueOfString`
    and `calculate`
    - StringASMDCalculator calculateSentence Code Modified

--- src/main/java/step_2/OperatingMethods.java
+++ src/main/java/step_2/OperatingMethods.java
@@ -1,18 +1,17 @@
 package step_2;
 
 import java.util.Arrays;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.BiFunction;
 
 public enum OperatingMethods {
-    ADD("+", (primary, secondary) -> Long.sum(primary, secondary.longValue())),
-    SUB("-", (primary, secondary) -> Long.sum(primary, -secondary.longValue())),
-    MUL("*", (primary, secondary) -> {
+    ADDITION("+", (primary, secondary) -> Long.sum(primary, secondary.longValue())),
+    SUBTRACTION("-", (primary, secondary) -> Long.sum(primary, -secondary.longValue())),
+    MULTIPLY("*", (primary, secondary) -> {
         primary *= secondary;
         return primary;
     }),
-    DIV("/", (primary, secondary) -> {
+    DIVISION("/", (primary, secondary) -> {
         if (secondary == 0) MessageConstants.DIVIDE_BY_ZERO.sendViolation();
         return primary / secondary;
     });
@@ -26,14 +25,14 @@ public enum OperatingMethods {
     }
 
 
-    private static Optional<OperatingMethods> valueOfString(String symbolText) {
+    public static Optional<OperatingMethods> valueOfString(String symbolText) {
         return Arrays.stream(OperatingMethods.values())
                 .filter(method -> method.operatorString.equalsIgnoreCase(symbolText))
                 .distinct()
                 .findFirst();
     }
 
-    public static Long valueOfCalculate(String symbolText, Long currentValue, Integer nextValue) {
-        return valueOfString(symbolText).orElseThrow(NoSuchElementException::new).expression.apply(currentValue, nextValue);
+    public Long calculate(Long currentValue, Integer nextValue) {
+        return expression.apply(currentValue, nextValue);
     }
 }
