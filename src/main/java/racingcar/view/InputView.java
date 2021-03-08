ed7a8537e5a79f833f08b726218afb902055be56
Beenie93 <seongbeen93@naver.com>
2021-03-09 01:19:45 +0900

    feat: implement a method to get number of attempts

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,11 +8,22 @@ public enum InputView {
 
     INSTANCE;
 
+    private final String INPUT_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇 회 인가요?";
     private final String INPUT_HOW_MANY_CARS = "자동차 대수는 몇 대 인가요?";
     private final Scanner scanner = new Scanner(System.in);
 
     public int InputNumberOfCars() {
         System.out.println(INPUT_HOW_MANY_CARS);
+        String number = scanner.nextLine().trim();
+        InputValidator.validateNull(number);
+        InputValidator.validateEmpty(number);
+        InputValidator.validateInteger(number);
+        InputValidator.validateLessThanZero(number);
+        return Integer.parseInt(number);
+    }
+
+    public int InputNumberOfAttempts() {
+        System.out.println(INPUT_NUMBER_OF_ATTEMPTS);
         String number = scanner.nextLine().trim();
         InputValidator.validateNull(number);
         InputValidator.validateEmpty(number);
