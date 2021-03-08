f052b6391999d79f52b53a682b866e8c0a0edf62
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-03-08 18:38:55 +0900

    step 2 : 피드백 수렴하여 리팩토링. - 불필요한 메서드 제거 - 인덴트 depth 줄이기 (가급적 1단계까지만) - Test 코드는 로직이 아니라 실제 테스트 대상의 클래스로 테스트.
    Github PR(closed) #1975

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -1,49 +1,29 @@
 package step2;
 
-// TODO 사칙연산 수행하는 계산기 구현
-/*
- - 반드시 빈 공객 문자열있다고 가정
- - 나눗셈의 경우 결과 값을 정수로 떨어지는 값으로 한정
- - 문자열 계산기는 계산 우선순위가 아닌 입력값에 따라 계산순서가 결정
- - 사칙연산의 계산 우선순위가 아니라 입력값에 따라 계산 순서가 결정되는것으로 결정
- */
 public class Calculator {
-    public static void main(String[] args) {
-        String data = "2 + 3 * 4 / 2";
-        System.out.println(calculator(data));
-    }
-
     public static int calculator(String data) {
         String[] arr = data.split(" ");
         int result = Integer.parseInt(arr[0]);
         if(hasNotNullOrNotEmpty(arr)){
-            for (int i = 1; i < arr.length; i=i+2) {
-                switch(arr[i]){
-                    case "+":
-                        result = add(result, arr[i+1]);
-                        break;
-                    case "-":
-                        result = minus(result, arr[i+1]);
-                        break;
-                    case "*":
-                        result = multiple(result, arr[i+1]);
-                        break;
-                    case "/":
-                        result = divide(result, arr[i+1]);
-                        break;
-                    default:
-                        throw new IllegalArgumentException(i+"번째 요소가 기호가 아닙니다.");
-                }
-            }
+            result = getResult(arr);
+        }
+        return result;
+    }
+
+    public static int getResult(String[] arr){
+        int result = Integer.parseInt(arr[0]);
+        for (int i = 1; i < arr.length; i=i+2) {
+            if(arr[i].equals("+")) result = add(result, arr[i+1]);
+            if(arr[i].equals("-")) result = minus(result, arr[i+1]);
+            if(arr[i].equals("*")) result = multiple(result, arr[i+1]);
+            if(arr[i].equals("/")) result = divide(result, arr[i+1]);
         }
         return result;
     }
 
     public static boolean hasNotNullOrNotEmpty(String[] arr){
         for (String s : arr) {
-            if(s==null || s.equals(" ")){
-                throw new IllegalArgumentException("Null 값 또는 빈 값을 갖고있습니다.");
-            }
+            if(s==null || s.equals(" ")) throw new IllegalArgumentException("Null 값 또는 빈 값을 갖고있습니다.");
         }
         return true;
     }
--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -2,6 +2,7 @@ package step2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step2.Calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,22 +19,19 @@ class CalculatorTest {
         int actual = Calculator.calculator(data);
 
         // then
-        assertEquals(actual, 10);
+        int expected = (2 + 3) * 4 / 2;
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("빈값이거나 null 일때 IllegalArgumentException 발생 테스트")
     void hasNotNullOrNotEmpty() {
         // given
-        String[] arr = {"2","*","3"," "};
+        String[] arr = {"2","*","3", " "};
 
         // when & then
         assertThrows(IllegalArgumentException.class, () -> {
-            for (String s : arr) {
-                if(s.equals(" ")){
-                    throw new IllegalArgumentException("exception !!");
-                }
-            }
+            Calculator.hasNotNullOrNotEmpty(arr);
         });
     }
 
@@ -41,59 +39,59 @@ class CalculatorTest {
     @DisplayName("add 메서드 테스트")
     void add() {
         // given
-        int result = 5;
-        String givenNum = "5";
+        int num1 = 5;
+        String num2 = "5";
 
         // when
-        int num = Integer.parseInt(givenNum);
-        result += num;
+        int actual = Calculator.add(num1, num2);
 
         // then
-        assertEquals(result, 5+5);
+        int expected = num1+Integer.parseInt(num2);
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("minus 메서드 테스트")
     void minus() {
         // given
-        int result = 10;
-        String givenNum = "5";
+        int num1 = 10;
+        String num2 = "5";
 
         // when
-        int num = Integer.parseInt(givenNum);
-        result -= num;
+        int actual = Calculator.minus(num1, num2);
 
         // then
-        assertEquals(result, 10-5);
+        int expected = num1-Integer.parseInt(num2);
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("multiple 메서드 테스트")
     void multiple() {
         // given
-        int result = 5;
-        String givenNum = "5";
+        int num1 = 5;
+        String num2 = "5";
 
         // when
-        int num = Integer.parseInt(givenNum);
-        result *= num;
+        int actual = Calculator.multiple(num1, num2);
 
         // then
-        assertEquals(result, 5*5);
+        int expected = num1*Integer.parseInt(num2);
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("divide 메서드 테스트")
     void divide() {
         // given
-        int result = 5;
-        String givenNum = "5";
+        int num1 = 5;
+        String num2 = "5";
 
         // when
-        int num = Integer.parseInt(givenNum);
-        result /= num;
+        int actual = Calculator.divide(num1, num2);
 
         // then
-        assertEquals(result, 5/5);
+        int expected = num1/Integer.parseInt(num2);
+        assertEquals(expected, actual);
     }
 }
