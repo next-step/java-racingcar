989a154ebf53a08fc5be3b8e2f3972d375982555
yonghwa <qhdl0224@gmail.com>
2021-03-08 21:38:19 +0900

    [fix] 매직 넘버 수정

--- src/main/java/study/step3/RandomRaceCar.java
+++ src/main/java/study/step3/RandomRaceCar.java
@@ -1,9 +1,10 @@
 package study.step3;
 
 public class RandomRaceCar implements Car{
+    private static final int START_POSITION = 1;
     private int position;
     public RandomRaceCar(){
-        this.position = 1;
+        this.position = START_POSITION;
     }
     @Override
     public int move(){
