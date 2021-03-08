da4d9dd8967bc17654bc0f14edafb43f2e631f09
abcdefg <choiyurim@naver.com>
2021-03-08 21:11:26 +0900

    feat(CarRacingTest, InputViewTest): add test case

---
+++ src/test/java/step3/InputViewTest.java
@@ -0,0 +1,45 @@
+package step3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class InputViewTest {
+
+    private InputView inputView;
+
+    @BeforeAll
+    void init() {
+        inputView = new InputView();
+    }
+
+    @DisplayName("자동차 댓수 입력 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1","2","3","4","5"})
+    void amountOfCarTest(String number) {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(number.getBytes(StandardCharsets.UTF_8));
+        System.setIn(byteArrayInputStream);
+
+        Assertions.assertEquals(inputView.getAmountOfCar() , Integer.parseInt(number));
+    }
+
+    @DisplayName("레이싱 시도 횟수 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1","2","3","4","5"})
+    void countOfCarRacingTest(String number) {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(number.getBytes(StandardCharsets.UTF_8));
+        System.setIn(byteArrayInputStream);
+
+        Assertions.assertEquals(inputView.getCountOfCar() , Integer.parseInt(number));
+    }
+
+}
