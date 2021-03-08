11cc2836dbab04edb65b918183ecd0068635ab98
shpark <shpark@shparkui-MacBookAir.local>
2021-03-08 22:34:01 +0900

    feat: Add tiral input

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -10,6 +10,10 @@ public class RacingCar {
         Scanner scanner = new Scanner(System.in);
         System.out.println("자동차 대수는 몇 대 인가요?");
         int car = scanner.nextInt();
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int trial = scanner.nextInt();
+
     }
 
 }
