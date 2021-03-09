991f511ce5f3e5786f3fa5cfd16f39e1d514a59c
shpark <shpark@shparkui-MacBookAir.local>
2021-03-10 00:12:37 +0900

    feat: add car, trial variables and setter methods on RacingCar class

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -2,13 +2,20 @@ package step3;
 
 public class RacingCar {
 
+    private int car;
+    private int trial;
+
     public RacingCar() {}
 
-    void start() {
-        InputView inputView = new InputView();
-        int car = inputView.getInput()[0];
-        int trial = inputView.getInput()[1];
+    public void setCar(int car) {
+        this.car = car;
+    }
 
+    public void setTrial(int trial) {
+        this.trial = trial;
+    }
+
+    void start() {
         ResultView resultView = new ResultView();
         resultView.printResult(car, trial);
     }
