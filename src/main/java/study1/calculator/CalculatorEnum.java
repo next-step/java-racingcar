0fbfb841f1c20556e1df4aa3bd4c4fb45ff698a2
keke0123 <keke0123@naver.com>
2021-03-09 20:53:28 +0900

    가독성 향상 정리작업

--- src/main/java/study1/calculator/CalculatorEnum.java
+++ src/main/java/study1/calculator/CalculatorEnum.java
@@ -3,7 +3,9 @@ package study1.calculator;
 import java.util.Arrays;
 
 public enum CalculatorEnum {
-  ADD("+", (a, b) -> a + b), SUBSTRACT("-", (a, b) -> a - b), MULTIPLY("*", (a, b) -> a * b),
+  ADD("+", (a, b) -> a + b),
+  SUBSTRACT("-", (a, b) -> a - b),
+  MULTIPLY("*", (a, b) -> a * b),
   DIVIDE("/", (a, b) -> a / b);
 
   private final String mark;
@@ -19,8 +21,11 @@ public enum CalculatorEnum {
   }
 
   public static CalculatorEnum fromMark(String mark) {
-    return Arrays.stream(CalculatorEnum.values()).filter(calculator -> calculator.mark.equals(mark)).findAny()
-        .orElseThrow(() -> new IllegalArgumentException("not mark error"));
+    return Arrays
+      .stream(CalculatorEnum.values())
+      .filter(calculator -> calculator.mark.equals(mark))
+      .findAny()
+      .orElseThrow(() -> new IllegalArgumentException("not mark error"));
   }
 
   public int calculate(int a, int b) {
