2efb1ebfffc013046179a3397731569062714303
shpark <shpark@shparkui-MacBookAir.local>
2021-03-10 00:16:50 +0900

    feat: add results array

--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -9,13 +9,14 @@ public class ResultView {
     public void printResult(int car, int trial) {
         System.out.println("\n실행 결과");
         Random random = new Random();
+        int[] results = new int[car];
 
-        while (0 < trial) {
-            trial--;
+        while (0 < trial--) {
 
             for (int i = 0; i < car; i++) {
                 int number = random.nextInt(10);
-                System.out.println(String.format("%" + random.nextInt(10) + "s", "-")
+                results[i] += number;
+                System.out.println(String.format("%" + results[i] + "s", "-")
                         .replace(" ", "-"));
             }
             System.out.println();
