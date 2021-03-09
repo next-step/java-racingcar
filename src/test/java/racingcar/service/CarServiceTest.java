7bcd90d2402e8c730dd585ac602bad56d6bfbb22
Beenie93 <seongbeen93@naver.com>
2021-03-10 02:32:56 +0900

    test: findCars()

--- src/test/java/racingcar/service/CarServiceTest.java
+++ src/test/java/racingcar/service/CarServiceTest.java
@@ -31,4 +31,21 @@ class CarServiceTest {
         assertEquals(1, currentSize, "추가한 만큼 크기가 증가해야 한다.");
         assertEquals(car.hashCode(), foundCar.hashCode(), "추가한 자동차와 꺼낸 자동차가 같아야 한다.");
     }
+
+    @Test
+    @DisplayName("모든 자동차 찾는 로직 검증 테스트")
+    public void findCars() throws Exception {
+        //given
+        CarRepository carRepository = new CarRepository();
+        carRepository.save(new Car());
+        carRepository.save(new Car());
+        carRepository.save(new Car());
+        CarService carService = new CarService(carRepository);
+
+        //when
+        List<Car> cars = carService.findCars();
+
+        //then
+        assertEquals(3, cars.size(), "찾은 자동차의 수가 추가한 자동차의 수와 같아야 한다.");
+    }
 }
