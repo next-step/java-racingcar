4d354bb23d070dd9799d21e1ef5d311010ca2fca
Jake <cocahack@naver.com>
2021-03-09 21:12:28 +0900

    feat: 레이싱 결과를 표현하는 클래스 생성
    Signed-off-by: Jake <cocahack@naver.com>

--- src/main/java/racing/dto/RacingReport.java
+++ src/main/java/racing/dto/RacingReport.java
@@ -7,15 +7,15 @@ import java.util.List;
 
 public class RacingReport {
 
-    private final List<RoundResult> racingHistories = new ArrayList<>();
+    private final List<RoundResult> roundResults = new ArrayList<>();
     private final List<String> winners = new ArrayList<>();
 
     public void saveRoundResult(List<? extends ReadonlyCar> carList) {
-        racingHistories.add(new RoundResult(carList));
+        roundResults.add(new RoundResult(carList));
     }
 
-    public List<RoundResult> getRacingHistories() {
-        return racingHistories;
+    public List<RoundResult> getRoundResults() {
+        return roundResults;
     }
 
     public void addWinner(String name) {
