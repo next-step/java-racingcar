528ade620e64b0a294e88b1b2501971f91e4e0c5
Beenie93 <seongbeen93@naver.com>
2021-03-10 00:42:00 +0900

    feat: implement GameRound Object

---
+++ src/main/java/racingcar/domain/GameRound.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class GameRound {
+
+    private int round;
+
+    public GameRound(int round) {
+        this.round = round;
+    }
+
+    public int getRound() {
+        return round;
+    }
+}
