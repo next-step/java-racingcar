16158450fb8b13e49ba7536475622f8c4e3dd066
Jake <cocahack@naver.com>
2021-03-09 22:24:12 +0900

    refactor: 변경된 설계에 맞게 객체를 조립하도록 main 메소드 수정
    Signed-off-by: Jake <cocahack@naver.com>

--- src/main/java/racing/RacingApplication.java
+++ src/main/java/racing/RacingApplication.java
@@ -1,25 +1,23 @@
 package racing;
 
-import racing.car.Car;
-import racing.car.RandomMovement;
-import racing.view.InteractiveInputView;
+import racing.io.ConsoleOutputDriver;
+import racing.io.OutputDriver;
+import racing.view.InputView;
+import racing.view.ResultView;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class RacingApplication {
 
+
     public static void main(String[] args) {
-        List<String> carNameList =
-            InteractiveInputView.getListOfString("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).", ",");
-        int moves = InteractiveInputView.getInt("시도할 회수는 몇 회 인가요?");
+        InputView inputView = new InputView();
 
-        List<Car> carList = carNameList.stream()
-                                       .map(name -> new Car(new RandomMovement(), name))
-                                       .collect(Collectors.toList());
+        String carSymbol = "-";
+        OutputDriver outputDriver = new ConsoleOutputDriver();
+        ResultView resultView = new ResultView(outputDriver, carSymbol);
 
-        Racing racing = new Racing(carList, moves);
-        racing.run();
+        RacingController racingController = new RacingController(inputView, resultView);
+        racingController.run();
     }
 
 }
