0e85503bbe4bf3c4424a913b623c1097e4a80962
Jake <cocahack@naver.com>
2021-03-09 21:19:46 +0900

    feat: ResultView 에 이름과 우승자를 출력하는 기능 추가
    Signed-off-by: Jake <cocahack@naver.com>

--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -1,28 +1,63 @@
 package racing.view;
 
-import racing.car.Car;
+import racing.car.ReadonlyCar;
+import racing.dto.RacingReport;
+import racing.dto.RoundResult;
+import racing.io.OutputDriver;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
+    private static final String NEW_LINE = "\n";
+    private static final String EMPTY_CHAR = "\0";
+    private static final String NAME_DELIMITER = ", ";
+
+    private final OutputDriver driver;
     private final String carSymbol;
 
-    public ResultView(String carSymbol) {
+    public ResultView(OutputDriver driver, String carSymbol) {
+        this.driver = driver;
         this.carSymbol = carSymbol;
     }
 
-    public void printRacingState(List<Car> carList) {
-        carList.stream()
-               .map(Car::getLocation)
-               .forEach(location -> System.out.println(generateTrack(location)));
+    public void printRacingReport(RacingReport racingReport) {
+        driver.println("실행 결과");
+        printRacingHistory(racingReport.getRoundResults());
+        printWinners(racingReport.getWinners());
+    }
 
-        System.out.println();
+    private void printWinners(List<String> winners) {
+        driver.println(String.join(NAME_DELIMITER, winners) + "가 최종 우승했습니다.");
+    }
+
+    private void printRacingHistory(List<RoundResult> roundResults) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        roundResults.forEach(roundResult -> {
+            List<String> racingStates = makeRacingState(roundResult);
+            stringBuilder.append(String.join(NEW_LINE, racingStates));
+            stringBuilder.append(NEW_LINE);
+            stringBuilder.append(NEW_LINE);
+        });
+
+        driver.print(stringBuilder.toString());
+    }
+
+    private List<String> makeRacingState(RoundResult roundResult) {
+        return roundResult.getCarList()
+                          .stream()
+                          .map(this::makeRacingStateOfUser)
+                          .collect(Collectors.toList());
+    }
+
+    private String makeRacingStateOfUser(ReadonlyCar car) {
+        return String.format("%s : %s", car.getName(), generateTrack(car.getLocation()));
     }
 
     private String generateTrack(int location) {
-        return new String(new char[location]).replace("\0", this.carSymbol);
+        return new String(new char[location]).replace(EMPTY_CHAR, this.carSymbol);
     }
-
 
 }
