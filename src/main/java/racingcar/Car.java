369cca04012e1e2691950cd432798674b1243421
10M <tenmillionwon@gmail.com>
2021-03-09 21:39:35 +0900

    refactor: remove unused variable, method

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,7 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Car {
 	private int position;
-	private List<Integer> status = new ArrayList();
 
 	public int getPosition() {
 		return this.position;
@@ -13,13 +9,5 @@ public class Car {
 
 	public void move() {
 		this.position += 1;
-	}
-
-	public void save() {
-		status.add(getPosition());
-	}
-
-	public List<Integer> getStatus() {
-		return this.status;
 	}
 }
