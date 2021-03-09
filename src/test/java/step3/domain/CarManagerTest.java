2e8699f4a4b6454347ff0e2460b5ca794fea356c
seokrae <kslbsh@gmail.com>
2021-03-09 23:41:29 +0900

    refactor: CarManager run 메서드로 RacingRound 값 테스트

--- src/test/java/step3/domain/CarManagerTest.java
+++ src/test/java/step3/domain/CarManagerTest.java
@@ -1,11 +1,15 @@
 package step3.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import step3.move.MoveStrategy;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
  * 자동차 관리 클래스 테스트
  */
 class CarManagerTest {
+
+    public static final MoveStrategy MOVE_STRATEGY = () -> true;
 
     private static Stream<Arguments> getCarManagement() {
         return Stream.of(
@@ -32,5 +38,38 @@ class CarManagerTest {
         List<Car> cars = given.getCars();
         // then
         assertThat(cars.size()).isEqualTo(carSize);
+    }
+
+    @DisplayName("자동차 라운드별 run 수행 후 RacingRound 값 테스트")
+    @ParameterizedTest(name = "레이싱 참여자 {0} 명, 한 라운드 실행한 자동차 수 {1}")
+    @CsvSource(value = {"3, 3", "4, 4"})
+    void carManager_RunningCarsTest(int participants, int expected) {
+        // given
+        CarManager carManager = new CarManager(participants, MOVE_STRATEGY);
+
+        // when
+        RacingRound run = carManager.run();
+        List<Car> cars = run.getCars();
+        int racingResult = cars.size();
+
+        // then
+        assertThat(racingResult).isEqualTo(expected);
+    }
+
+    @DisplayName("한 라운드 레이싱 후 결과 RacingRound의 자동차 위치 확인 테스트")
+    @ParameterizedTest(name = "레이싱 참여자 {0} 명, 한 라운드 실행한 자동차 수 {1}")
+    @CsvSource(value = {"3, 1", "4, 1"})
+    void carManager_moveCarTest(int participants, int expected) {
+        // given
+        CarManager carManager = new CarManager(participants, MOVE_STRATEGY);
+        // when
+        RacingRound run = carManager.run();
+        List<Car> cars = run.getCars();
+        List<Integer> collect = cars.stream()
+                .mapToInt(Car::getPosition).boxed()
+                .collect(Collectors.toList());
+
+        // then
+        collect.forEach(integer -> assertThat(integer).isEqualTo(expected));
     }
 }
