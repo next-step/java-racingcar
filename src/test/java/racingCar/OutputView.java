2a5e3e4f50bf3ca7c83959f73ca2392ae7a593e9
koola97620 <koola976@gmail.com>
2021-03-09 21:16:22 +0900

    refactor: 자동차경주 - reformat

--- src/test/java/racingCar/OutputView.java
+++ src/test/java/racingCar/OutputView.java
@@ -2,21 +2,26 @@ package racingCar;
 
 public class OutputView {
 
+  private static final String CAR_QUESTION = "자동차 대수는 몇 대 인가요?";
+  private static final String TRY_QUESTION = "시도할 회수는 몇 회 인가요?";
+  private static final String RESULT = "실행결과";
+  private static final String HYPHEN = "-";
+
   public static void printCarNumber() {
-    System.out.println("자동차 대수는 몇 대 인가요?");
+    System.out.println(CAR_QUESTION);
   }
 
   public static void printTryCount() {
-    System.out.println("시도할 회수는 몇 회 인가요?");
+    System.out.println(TRY_QUESTION);
   }
 
   public static void printResult() {
-    System.out.println("실행 결과");
+    System.out.println(RESULT);
   }
 
   public static void printCarDistance(int distance) {
     for (int i = 0; i < distance; i++) {
-      System.out.print("-");
+      System.out.print(HYPHEN);
     }
     System.out.println();
   }
