fbcb4d94c7dd2e154d139201c7d5aa05ab20e3ed
623nana <6230nana@gmail.com>
2021-03-09 20:56:51 +0900

    refactor: 실행 질문 InputView 클래스에서 선언 되도록 수정

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -5,10 +5,8 @@ import racingcar.view.InputView;
 public class Main {
 
   public static void main(String[] args) {
-
     RacingGame racingGame = new RacingGame();
-    racingGame.readyGame(InputView.inputCount("자동차 대수는 몇 대 인가요?"));
-    racingGame.run(InputView.inputCount("시도할 회수는 몇 회 인가요?"));
+    racingGame.readyGame(InputView.inputNumOfCar());
+    racingGame.run(InputView.inputNumOfAttempt());
   }
-
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 
 public class InputView {
 
-  public static int inputCount(String message) {
-    System.out.println(message);
+  public static int inputNumOfCar() {
+    System.out.println("자동차 대수는 몇 대 인가요?");
+    return new Scanner(System.in).nextInt();
+  }
+
+  public static int inputNumOfAttempt() {
+    System.out.println("시도할 회수는 몇 회 인가요?");
     return new Scanner(System.in).nextInt();
   }
 }
