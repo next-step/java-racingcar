1a1e1b6e3164461653b4b842d96d08f245d7e4ff
623nana <6230nana@gmail.com>
2021-03-09 21:03:19 +0900

    test: 입력 받은 댓수 만큼 자동차를 생성하는 테스트

---
+++ src/test/java/racingcar/RacingGameTest.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RacingGameTest {
+
+  private RacingGame racingGame;
+
+  @BeforeEach
+  void setUp() {
+    racingGame = new RacingGame();
+  }
+
+  @Test
+  @DisplayName("[RacingGame] 입력 받은 댓수 만큼 자동차 생성 테스트")
+  void readyGameTest() {
+    assertThat(racingGame.cars.size()).isZero();
+    racingGame.readyGame(4);
+    assertThat(racingGame.cars.size()).isEqualTo(4);
+  }
+
+}
