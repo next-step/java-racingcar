1d0783a451ebb9581a962e81318ce80854cd5d9e
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-09 23:43:20 +0900

    feat : 사용자의 입력을 받는 InputView 클래스 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,2 +1,22 @@
-package racingcar;public class InputView {
+package racingcar;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("자동차 대수는 몇 대 인가요? ");
+        int cars = scanner.nextInt();
+
+        System.out.println("시도할 회수는 몇 회 인가요? ");
+        int count = scanner.nextInt();
+
+        RacingCar racingCar = new RacingCar(cars, count);
+        List<Car> list = racingCar.settingCars(cars);
+        racingCar.moveToN(list, count);
+        racingCar.carStatus(list);
+
+    }
 }
