94f8dc00ba9daba8d90eba8494da0425899de695
keke0123 <keke0123@naver.com>
2021-03-09 21:23:19 +0900

    타입을 변수 네이밍 규칙에서 삭제

--- src/main/java/study1/calculator/CalculatorService.java
+++ src/main/java/study1/calculator/CalculatorService.java
@@ -5,22 +5,22 @@ import java.util.List;
 
 public class CalculatorService {
 
-  private List<String> strList;
+  private List<String> letters;
 
   public CalculatorService(String str) {
     CalculatorValidationEnum.fromIndex(1).check(str);
-    this.strList = Arrays.asList(str.split(" "));
+    this.letters = Arrays.asList(str.split(" "));
     int index = 0;
-    for (String s : this.strList) {
+    for (String s : this.letters) {
       CalculatorValidationEnum.fromIndex(index).check(s);
       index++;
     }
   }
 
   public int calculateAll() {
-    int total = Integer.parseInt(strList.get(0));
-    for(int i = 1; i < strList.size(); i += 2) {
-      total = CalculatorEnum.fromMark(strList.get(i)).calculate(total, Integer.parseInt(strList.get(i+1)));
+    int total = Integer.parseInt(letters.get(0));
+    for(int i = 1; i < letters.size(); i += 2) {
+      total = CalculatorEnum.fromMark(letters.get(i)).calculate(total, Integer.parseInt(letters.get(i+1)));
     }
     return total;
   }
