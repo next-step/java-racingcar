d49e9b27616e8268509f0669dc9be527c73bc5a1
seokrae <kslbsh@gmail.com>
2021-03-10 18:26:47 +0900

    feat: 자동차 이름을 받기 위한 유효성 검사

--- src/main/java/step3/util/Validator.java
+++ src/main/java/step3/util/Validator.java
@@ -1,13 +1,21 @@
 package step3.util;
 
+import java.util.regex.Pattern;
+
 /**
  * 입력 문자열 유효성 검사 유틸 클래스
  */
 public final class Validator {
 
+    private static final String VALID_USERNAMES = "[a-zA-Z|,]*";
+
     private Validator() { }
 
     public static boolean isNullAndIsBlank(String input) {
         return input == null || input.trim().isEmpty();
+    }
+
+    public static boolean isValidNames(String participantNames) {
+        return Pattern.matches(VALID_USERNAMES, participantNames);
     }
 }
--- src/test/java/step3/util/ValidatorTest.java
+++ src/test/java/step3/util/ValidatorTest.java
@@ -1,14 +1,18 @@
 package step3.util;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static step3.util.Validator.isNullAndIsBlank;
+import static step3.util.Validator.isValidNames;
 
 /**
  * 입력 문자열 유효성 검사 테스트
@@ -28,5 +32,18 @@ public class ValidatorTest {
     @MethodSource("blankStrings")
     void isNullAndIsBlank_ShouldReturnTrueOrFalse(String given, boolean expected) {
         assertThat(isNullAndIsBlank(given)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> validNames() {
+        return Stream.of(
+                Arguments.of("seok,rae", true),
+                Arguments.of("seok.rae", false)
+
+        );
+    }
+    @ParameterizedTest(name = "{0} 값을 입력 시 {1}")
+    @MethodSource(value = "validNames")
+    void testCase(String given, boolean expected) {
+        assertThat(isValidNames(given)).isEqualTo(expected);
     }
 }
