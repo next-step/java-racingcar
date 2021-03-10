36cf4e778a7632e2a220404dce4635df40f17d28
kwj1270 <kwj1270@naver.com>
2021-03-11 02:02:57 +0900

    test:RacingCarRequestDto의 Amount와 Round 값에 대한 테스트케이스 작성

--- src/test/java/racingcar/domain/dto/RacingCarRequestDtoTest.java
+++ src/test/java/racingcar/domain/dto/RacingCarRequestDtoTest.java
@@ -1,4 +1,37 @@
+package racingcar.domain.dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.domain.request.Amount;
+import racingcar.domain.request.Round;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class RacingCarRequestDtoTest {
-  
+
+    @DisplayName("RacingCarRequestDto 가 알맞은 값을 가지고 반환하는지에 대한 테스트")
+    @CsvSource(value = {"2:3", "1:4", "2:5"}, delimiter = ':')
+    @ParameterizedTest(name = "현재 반복 : {index} / 들어가는 값 : {arguments}")
+    void test(int amountExpected, int roundExpected){
+
+        // given
+        Amount amount = Amount.getInstance(amountExpected);
+        Round round = Round.getInstance(roundExpected);
+        RacingCarRequestDto requestDto = RacingCarRequestDto.of(amount, round);
+
+        // when
+        int amountActual = requestDto.getAmountValue();
+        int roundActual = requestDto.getRoundValue();
+
+        // then
+        assertAll(
+                () -> assertThat(amountActual).isEqualTo(amountExpected),
+                () -> assertThat(roundActual).isEqualTo(roundExpected)
+        );
+
+    }
+
+
 }
