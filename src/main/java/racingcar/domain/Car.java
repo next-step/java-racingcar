dbbbd79b2495e694d4df5536c4ff5250dfdfa6a9
Beenie93 <seongbeen93@naver.com>
2021-03-10 22:16:46 +0900

    feat: implement a method to validate random number

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 public class Car {
     private static final int INIT_VALUE = 0;
     private static final int CONDITION_FOR_MOVEMENT = 4;
+    private static final int MAX_INCLUSIVE = 9;
+    private static final int MIN_INCLUSIVE = 0;
     private int movementRange;
     private String name;
 
@@ -11,6 +13,7 @@ public class Car {
     }
 
     public void move(int number) {
+        validateRandomNumber(number);
         if (number >= CONDITION_FOR_MOVEMENT) {
             movementRange++;
         }
@@ -26,5 +29,11 @@ public class Car {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    private void validateRandomNumber(int randomNumber) {
+        if (randomNumber > MAX_INCLUSIVE || randomNumber < MIN_INCLUSIVE) {
+            throw new IllegalArgumentException("이동 범위의 조건은 0 ~ 9이여야만 합니다.");
+        }
     }
 }
