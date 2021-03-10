c93d9fb0c975b9f23c36c1680021d54d2522306b
suhyunsim <cnc1106@naver.com>
2021-03-11 00:32:24 +0900

    refactor:  메소드를 Validator로 분리하고 접근 제한자를 private으로 변경

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,20 +1,17 @@
 package stringcalculator;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static stringcalculator.Message.INPUT_BLANK;
-import static stringcalculator.Message.INPUT_NULL;
+import static stringcalculator.Validator.isInteger;
 
 public class StringCalculator {
 
     private static final String DELIMITER = " ";
 
-    public static int calculate(String input) {
-        validate(input);
+    public int calculate(String input) {
+        Validator.validateInput(input);
         String[] inputArray = split(input);
         List<Integer> numbers = getNumbers(inputArray);
         List<Operator> operators = getOperators(inputArray);
@@ -27,39 +24,21 @@ public class StringCalculator {
         return result;
     }
 
-    public static void validate(String input) {
-        if (input == null) {
-            throw new IllegalArgumentException(INPUT_NULL);
-        }
-        if (StringUtils.isBlank(input)) {
-            throw new IllegalArgumentException(INPUT_BLANK);
-        }
-    }
-
-    public static String[] split(String input) {
+    private String[] split(String input) {
         return input.split(DELIMITER);
     }
 
-    public static List<Integer> getNumbers(String[] inputArray) {
+    private List<Integer> getNumbers(String[] inputArray) {
         return Arrays.stream(inputArray)
-                .filter(StringCalculator::isInteger)
+                .filter(Validator::isInteger)
                 .map(Integer::valueOf)
                 .collect(Collectors.toList());
     }
 
-    public static List<Operator> getOperators(String[] inputArray) {
+    private List<Operator> getOperators(String[] inputArray) {
         return Arrays.stream(inputArray)
                 .filter(value -> !isInteger(value))
                 .map(Operator::operateOf)
                 .collect(Collectors.toList());
-    }
-
-    public static boolean isInteger(String value) {
-        try {
-            Integer.parseInt(value);
-            return true;
-        } catch (NumberFormatException ne) {
-            return false;
-        }
     }
 }
