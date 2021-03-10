7a495f04f23ac711608fd95acb27523e26845208
suhyunsim <cnc1106@naver.com>
2021-03-10 19:21:14 +0900

    feat: INPUT 값 관련 예외처리 메세지 추가

--- src/main/java/stringcalculator/Message.java
+++ src/main/java/stringcalculator/Message.java
@@ -1,6 +1,8 @@
 package stringcalculator;
 
 public class Message {
+    public static final String INPUT_NULL = "입력값이 NULL입니다.";
+    public static final String INPUT_BLANK = "입력값이 공백입니다.";
     public static final String UNSUPPORTED_OPERATOR = "지원하지 않는 연산자 입니다.";
     public static final String DIVIDE_ZERO = "0으로 나눌 수 없습니다.";
 }
