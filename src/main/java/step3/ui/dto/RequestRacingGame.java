3269b27aeb0fbf09bd415410d539298f88174b59
seokrae <kslbsh@gmail.com>
2021-03-10 22:57:12 +0900

    refactor: 참여자 이름으로 요청하기 위한 dto 클래스 필드 타입 수정

--- src/main/java/step3/ui/dto/RequestRacingGame.java
+++ src/main/java/step3/ui/dto/RequestRacingGame.java
@@ -1,16 +1,14 @@
 package step3.ui.dto;
 
-import java.util.Objects;
-
 /**
  * 레이스 요청 파라미터 관리 클래스
  */
 public class RequestRacingGame {
 
-    private final int participants;
+    private final String[] participants;
     private final int attemptCount;
 
-    public RequestRacingGame(int participants, int attemptCount) {
+    public RequestRacingGame(String[] participants, int attemptCount) {
         this.participants = participants;
         this.attemptCount = attemptCount;
     }
@@ -19,20 +17,7 @@ public class RequestRacingGame {
         return attemptCount;
     }
 
-    public int getParticipants() {
+    public String[] getParticipants() {
         return participants;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RequestRacingGame that = (RequestRacingGame) o;
-        return getParticipants() == that.getParticipants() && getAttemptCount() == that.getAttemptCount();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getParticipants(), getAttemptCount());
     }
 }
