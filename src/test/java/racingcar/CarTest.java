881186e7bf143442427e3da43212e603aa985b82
623nana <6230nana@gmail.com>
2021-03-10 21:39:54 +0900

    test: 입력 받은 자동차 이름에 대한 검증 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,10 +1,16 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import calculator.InputValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.exception.CarNameException;
 import racingcar.model.Car;
 
 public class CarTest {
@@ -13,7 +19,7 @@ public class CarTest {
 
   @BeforeEach
   void setUp() {
-    car = new Car();
+    car = new Car("nana");
   }
 
   @Test
@@ -22,5 +28,13 @@ public class CarTest {
     assertThat(car.getLocation()).isEqualTo(0);
     car.move();
     assertThat(car.getLocation()).isEqualTo(1);
+  }
+
+  @ParameterizedTest(name = "[Car] validateName 테스트 - {index}")
+  @ValueSource(strings = {"", "abcde", "abcdef"})
+  void validateNameTest(String input) {
+    assertThatThrownBy(() -> {
+      car.validateName(input);
+    }).isInstanceOf(CarNameException.class);
   }
 }
