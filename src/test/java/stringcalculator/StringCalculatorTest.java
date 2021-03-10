7a52ba12c8776ac202a03d09bb271183a3d00d2c
suhyunsim <cnc1106@naver.com>
2021-03-11 00:33:39 +0900

    test: 연산 기능을 확인하는 테스트 추가

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,36 +1,67 @@
 package stringcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringCalculatorTest {
 
-    @DisplayName("null 입력 시 확인")
+    StringCalculator calculator;
+    int x;
+    int y;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+        x = 4;
+        y = 2;
+    }
+
+    @DisplayName("더하기 기능 동작")
     @Test
-    void inputNullTest() {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            StringCalculator.validate(null);
-        });
+    void add() {
+        int actual = calculator.calculate(x + " + " + y);
+        assertThat(actual).isEqualTo(x + y);
     }
 
-    @DisplayName("공백 입력 시 확인")
-    @ParameterizedTest
-    @ValueSource(strings = {"", " "})
-    void inputNullOrBlankTest(String input) {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            StringCalculator.validate(input);
-        });
+    @DisplayName("빼기 기능 동작")
+    @Test
+    void minus() {
+        int actual = calculator.calculate(x + " - " + y);
+        assertThat(actual).isEqualTo(x - y);
     }
 
-    @DisplayName("사칙 연산을 모두 포함하는 기능 구현")
+    @DisplayName("곱하기 기능 동작")
+    @Test
+    void multiply() {
+        int actual = calculator.calculate(x + " * " + y);
+        assertThat(actual).isEqualTo(x * y);
+    }
+
+    @DisplayName("나누기 기능 동작")
+    @Test
+    void divide() {
+        int actual = calculator.calculate(x + " / " + y);
+        assertThat(actual).isEqualTo(x / y);
+    }
+
+    @DisplayName("0으로 나눌 때 예외 발생")
     @ParameterizedTest
-    @ValueSource(strings = {"1 + 3 - 2 / 2"})
+    @ValueSource(strings = {"2 / 0", "4 / 0"})
+    void checkZeroTest(String input) {
+        assertThatThrownBy(() -> calculator.calculate(input))
+                .isInstanceOf(ArithmeticException.class);
+    }
+
+    @DisplayName("사칙 연산을 모두 포함하는 기능")
+    @ParameterizedTest
+    @ValueSource(strings = {"1 + 3 - 2 / 2 * 4"})
     void allOperatorTest(String input) {
-        assertThat(StringCalculator.calculate(input)).isEqualTo(1);
+        assertThat(calculator.calculate(input)).isEqualTo(4);
     }
 }
