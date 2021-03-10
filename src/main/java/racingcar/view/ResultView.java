67757d224bd46e1777b804f2c00f7b54c120cc40
623nana <6230nana@gmail.com>
2021-03-10 21:44:13 +0900

    feat: 트랙과 자동차의 이름을 함께 출력하도록 수정

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -7,7 +7,7 @@ import racingcar.model.Car;
 public class ResultView {
 
   public static void showRacingResult(List<Car> cars) {
-    cars.stream().map(car -> Movement.trackingMovement(car, "-"))
+    cars.stream().map(car -> car.getName() + " : " + Movement.trackingMovement(car, "-"))
         .forEach(System.out::println);
     System.out.println();
   }
