1364ab5f8a13f8782a65b1f59d1244e083c2605d
623nana <6230nana@gmail.com>
2021-03-10 22:40:13 +0900

    feat: 우승자를 판정하는 기능

---
+++ src/main/java/racingcar/controller/Winner.java
@@ -0,0 +1,21 @@
+package racingcar.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.model.Car;
+
+public class Winner {
+
+  private int getMaxLocation(List<Car> cars) {
+    return cars.stream().mapToInt(Car::getLocation).max().orElse(0);
+  }
+
+  private boolean isWinner(Car car, int maxLocation) {
+    return car.getLocation() == maxLocation;
+  }
+
+  public List<Car> getWinner(List<Car> cars) {
+    int maxLocation = getMaxLocation(cars);
+    return cars.stream().filter(car -> isWinner(car, maxLocation)).collect(Collectors.toList());
+  }
+}
