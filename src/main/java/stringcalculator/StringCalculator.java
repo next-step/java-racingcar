11f2fbf8761536e6365b7b9d23f3166a04dd4062
suhyunsim <cnc1106@naver.com>
2021-03-10 19:23:18 +0900

    feat: 배열에서 정수와 연산자 분리하는 메소드 추가

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -2,6 +2,10 @@ package stringcalculator;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static stringcalculator.Message.INPUT_BLANK;
 import static stringcalculator.Message.INPUT_NULL;
 
@@ -12,6 +16,10 @@ public class StringCalculator {
     public static int calculate(String input) {
         validate(input);
         String[] inputArray = split(input);
+        List<Integer> numbers = getNumbers(inputArray);
+        List<Operator> operators = getOperators(inputArray);
+        int result = numbers.get(0);
+        return result;
     }
 
     public static void validate(String input) {
@@ -27,4 +35,26 @@ public class StringCalculator {
         return input.split(DELIMITER);
     }
 
+    public static List<Integer> getNumbers(String[] inputArray) {
+        return Arrays.stream(inputArray)
+                .filter(StringCalculator::isInteger)
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Operator> getOperators(String[] inputArray) {
+        return Arrays.stream(inputArray)
+                .filter(value -> !isInteger(value))
+                .map(Operator::operateOf)
+                .collect(Collectors.toList());
+    }
+
+    public static boolean isInteger(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException ne) {
+            return false;
+        }
+    }
 }
