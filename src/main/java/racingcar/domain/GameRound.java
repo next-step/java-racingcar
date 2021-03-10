2e51111edd2bd58b90d3800bc16187fbb192cc39
Beenie93 <seongbeen93@naver.com>
2021-03-11 00:44:56 +0900

    feat: implement a method to validate a number less than 0

--- src/main/java/racingcar/domain/GameRound.java
+++ src/main/java/racingcar/domain/GameRound.java
@@ -2,13 +2,21 @@ package racingcar.domain;
 
 public class GameRound {
 
+    private static final int ZERO = 0;
     private int round;
 
     public GameRound(int round) {
+        validateLessThanZero(round);
         this.round = round;
     }
 
     public int getRound() {
         return round;
+    }
+
+    public void validateLessThanZero(int number) {
+        if (number <= ZERO) {
+            throw new IllegalArgumentException("0이하의 값인지 확인해주세요.");
+        }
     }
 }
