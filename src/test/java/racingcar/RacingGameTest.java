06013474de6902bd20ee1732bc7703b5c35b4454
623nana <6230nana@gmail.com>
2021-03-10 21:42:57 +0900

    refactor: 자동차 이름 속성 추가에 따른 테스트 수정

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -3,8 +3,8 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.controller.RacingGame;
 
 public class RacingGameTest {
@@ -16,12 +16,11 @@ public class RacingGameTest {
     racingGame = new RacingGame();
   }
 
-  @Test
-  @DisplayName("[RacingGame] 입력 받은 댓수 만큼 자동차 생성 테스트")
-  void readyGameTest() {
-    assertThat(racingGame.cars.size()).isZero();
-    racingGame.readyGame(4);
-    assertThat(racingGame.cars.size()).isEqualTo(4);
+  @ParameterizedTest(name = "[RacingGame] 입력 받은 댓수 만큼 자동차 생성 테스트")
+  @ValueSource(strings = {"pobi,crong,honux"})
+  void readyGameTest(String input) {
+    racingGame.readyGame(input);
+    assertThat(racingGame.cars.size()).isEqualTo(3);
   }
 
 }
