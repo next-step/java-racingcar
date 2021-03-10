ab610c68523f97021d149fa199813e9d67ac36ce
623nana <6230nana@gmail.com>
2021-03-10 21:26:20 +0900

    feat: 입력 받은 자동차 이름에 대한 검증 및 예외 처리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,11 +1,14 @@
 package racingcar.model;
 
+import racingcar.exception.CarNameException;
+
 public class Car {
 
   private int location;
   private String name;
 
   public Car(String name) {
+    validateName(name);
     this.name = name;
     this.location = 0;
   }
@@ -22,4 +25,20 @@ public class Car {
     return name;
   }
 
+  public void validateName(String name) {
+    isProper(name);
+    isEmpty(name);
+  }
+
+  private void isProper(String name) {
+    if (name.length() > 5) {
+      throw new CarNameException("자동차 이름은 5자를 초과할 수 없습니다.");
+    }
+  }
+
+  private void isEmpty(String name) {
+    if ("".equals(name)) {
+      throw new CarNameException("자동차 이름을 입력하세요.");
+    }
+  }
 }
