26f6b9e07f45bd720d54c1287420e6d1df88aa03
0hun <whdudgns2654@gmail.com>
2021-03-10 22:15:35 +0900

    Step1 학습테스트 리뷰 요청드립니다. (#2002)
    * String 클래스에 대한 학습 테스트 추가
    
    * Set Collection에 대한 학습 테스트 추가
    
    Co-authored-by: younghun <younghun@paycurx.co.kr>

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,65 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("Set Collection에 대한 학습 테스트")
+public class SetCollectionTest {
+
+  private Set<Integer> numbers;
+
+  @BeforeEach
+  void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+
+  @DisplayName("요구사항 1, Set의 크기 확인")
+  @Test
+  void setCheckSize() {
+    assertThat(numbers.size()).isEqualTo(3);
+  }
+
+  @DisplayName("요구사항 2, Set의 1,2,3값 확인")
+  @Test
+  void containsNumber() {
+    assertTrue(numbers.contains(1));
+    assertTrue(numbers.contains(2));
+    assertTrue(numbers.contains(3));
+  }
+
+  @DisplayName("요구사항 2, Set의 1,2,3값 확인, ParameterizedTest로 중복코드 제거")
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 3})
+  void someNumbers_IsContains(int number) {
+    assertTrue(numbers.contains(number));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 3, 4, 5 })
+  void someNumbers_IsContainsAndNotContains(int number) {
+    assertTrue(numbers.contains(number));
+  }
+
+  @DisplayName("요구사항 3, Set의 1,2,3값 확인, ParameterizedTest로 입력 값이 다른 경우")
+  @ParameterizedTest
+  @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+  void intValue_IsContainsExpectedBooleanValue(int input, boolean expected) {
+    assertThat(numbers.contains(input)).isEqualTo(expected);
+  }
+
+
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,51 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("String 클래스에대한 학습 테스트")
+public class StringTest {
+
+  @DisplayName("String 요구사항 1-1, 1과2로 분리")
+  @Test
+  void multipleSplit() {
+    String[] multipleSplit = "1,2".split(",");
+    assertThat(multipleSplit).containsExactly("1", "2");
+
+    String[] singleSplit = "1".split(",");
+    assertThat(singleSplit).containsExactly("1");
+    assertThat(singleSplit.length).isEqualTo(1);
+  }
+
+  @DisplayName("String 요구사항 1-2, 1만 포함하는지")
+  @Test
+  void singleSplit() {
+    String[] singleSplit = "1".split(",");
+    assertThat(singleSplit).containsExactly("1");
+    assertThat(singleSplit.length).isEqualTo(1);
+  }
+
+  @DisplayName("String 요구사항 2, 괄호 제거하여 1,2 반환")
+  @Test
+  void substringSplit() {
+    String[] substringSplit = "(1,2)".substring(1, 4).split(",");
+    assertThat(substringSplit).containsExactly("1", "2");
+  }
+
+  @DisplayName("String 요구사항 3, 특정위치 문자를 가져오기")
+  @Test
+  void stringCharAt() {
+    String result = "abc";
+
+    assertThat(result.charAt(0)).isEqualTo('a');
+
+    assertThatThrownBy(() -> {
+      result.charAt(3);
+    }).isInstanceOf(IndexOutOfBoundsException.class);
+
+  }
+
+}
