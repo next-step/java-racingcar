1aa2984bb93f3c90275203c784c0dc0ebcbad90d
Beenie93 <seongbeen93@naver.com>
2021-03-11 01:09:41 +0900

    refactor: apply @BeforeEach

--- src/test/java/racingcar/controller/RacingGameControllerTest.java
+++ src/test/java/racingcar/controller/RacingGameControllerTest.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,20 +8,27 @@ import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.repository.CarRepository;
 import racingcar.service.CarService;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RacingGameControllerTest {
 
+    private CarRepository carRepository;
+    private CarService carService;
+    private RacingGameController racingGameController;
+
+    @BeforeEach
+    public void setUp() {
+        this.carRepository = new CarRepository();
+        this.carService = new CarService(this.carRepository);
+        this.racingGameController = new RacingGameController(this.carService);
+    }
+
     @ParameterizedTest(name = "입력한 대수가 {0}일 경우")
     @ValueSource(ints = {1, 2, 3, 4, 5, 6})
     @DisplayName("입력받은 대수만큼 자동차 생성하는 테스트")
     public void createCars(int numberOfCars) throws Exception {
-        //given
-        CarRepository carRepository = new CarRepository();
-        CarService carService = new CarService(carRepository);
-        RacingGameController racingGameController = new RacingGameController(carService);
-
         //when
         racingGameController.createCars(numberOfCars);
         int sizeOfCars = carRepository.findAll().size();
@@ -32,14 +40,6 @@ class RacingGameControllerTest {
     @Test
     @DisplayName("입력받은 시도 회수를 저장하는 테스트")
     public void createGameRound() throws Exception {
-        //given
-        CarRepository carRepository = new CarRepository();
-        CarService carService = new CarService(carRepository);
-        RacingGameController racingGameController = new RacingGameController(carService);
-
-        //when
-
-        //then
         assertThatIllegalArgumentException().isThrownBy(() -> racingGameController.createGameRound(-1)).withMessageContaining("0이하의 값인지 확인");
     }
 }
