fc15b35da1959c5ab895a6c804c6efd0c4cf30ef
kwj1270 <kwj1270@naver.com>
2021-03-11 04:05:26 +0900

    test:Car 클래스에 움직일 수 있는 조건을 넣었을 때 실제 움직이는지 테스트

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,23 @@
+package racingcar.domain.car;
+
+public class Car {
+    private int INITIAL_VALUE = 0;
+    private int status;
+
+    public Car() {
+        status = INITIAL_VALUE;
+    }
+
+    // 이 부분 질문 있습니다! : 생성자에 넣는 것이 좋을가요? 메서드에 넣는 것이 좋을까요?
+    // 이전에 생성자에 만든 코드가 있는데 클래스를 여러개 생성할 때마다 주입해줘야 하는 작업을 했습니다.
+    public void move(MoveStrategy moveStrategy) {
+        if (moveStrategy.isMovable()) {
+            status++;
+        }
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+}
---
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -0,0 +1,25 @@
+package racingcar.domain.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @DisplayName("Car 클래스의 move()에 true 값이 주어졌을 때 값이 증가되는지 테스트")
+    @RepeatedTest(value = 10, name = "현재 Loop : {0}")
+    void test() {
+        // given
+        Car car = new Car();
+        car.move(() -> true);
+
+        // when
+        int actual = car.getStatus();
+
+        // then
+        assertThat(actual).isEqualTo(1);
+
+    }
+
+}
