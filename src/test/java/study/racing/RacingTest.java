8d7deee02e4e64e8ed7b909d032a3f867c31e47b
cyr9210 <jipsyer@naver.com>
2021-03-11 10:48:43 +0900

    test 설명 추가

--- src/test/java/study/racing/RacingTest.java
+++ src/test/java/study/racing/RacingTest.java
@@ -5,17 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RacingTest {
 
   @Test
+  @DisplayName("car 객체 생성 시도횟수만큼 번호 할당")
   void createCar() {
     Car car = new Car(4);
     assertEquals(car.getNumbers().size(), 4);
   }
 
   @Test
+  @DisplayName("전진유무 파악")
   void car_moveCheck() {
     List<Integer> numbers = Arrays.asList(1, 4, 2, 6);
     Car car = new Car(numbers);
