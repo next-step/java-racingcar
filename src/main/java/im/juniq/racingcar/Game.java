d4ecf35d4a5b70e527a6fd85a8ff756193badb25
juniq <juniqlim@gmail.com>
2021-03-11 23:28:01 +0900

    feat: 게임 실행

--- src/main/java/im/juniq/racingcar/Game.java
+++ src/main/java/im/juniq/racingcar/Game.java
@@ -6,6 +6,24 @@ import java.util.List;
 public class Game {
 	private List<RacingCar> cars = new ArrayList<>();
 
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.play();
+	}
+
+	public void play() {
+		InputView.scan();
+		createCars(InputView.getCarCount());
+		racing(InputView.getTryCount());
+	}
+
+	private void racing(int moveCount) {
+		for (int i = 0; i < moveCount; i++) {
+			moveCars();
+			printCarsMoveCount();
+		}
+	}
+
 	public void createCars(int carCount) {
 		for (int i = 0; i < carCount; i++) {
 			cars.add(new RacingCar(i));
