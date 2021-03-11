43c453593981d36d18354334e0629e6c42ca5206
lisaimeee <lisaimeee@gmail.com>
2021-03-11 23:40:23 +0900

    chore(RacingCarAppication): 코드 가독성을 위해 빈 줄 추가

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -5,13 +5,15 @@ import racingcar.view.InputView;
 import racingcar.view.PrintView;
 
 public class RacingCarApplication {
-
     public static void main(String[] args) {
         InputView inputView = new InputView();
         PrintView printView = new PrintView();
+
         int numberOfCars = inputView.getNumberOfCars();
         int numberOfTurns = inputView.getNumberOfTurns();
+
         Racing racing = Racing.withCondition(numberOfCars, numberOfTurns);
+
         printView.printResultTitle();
         printView.printCarStatus(racing.getCars());
 
