548b98c3eea03321d1f65b1275ddf6206b20fc7f
ad7549 <ad7549@gmail.com>
2021-03-11 10:46:05 +0900

    test: Add RacingTest

---
+++ src/test/java/racing/RacingTest.java
@@ -0,0 +1,25 @@
+package racing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingTest {
+    @Test
+    @DisplayName("주어진 횟수만큼 경기를 진행한다.")
+    void racing() {
+        final int carsCount = 3;
+        final int attemptsCount = 5;
+
+        final Racing racing = new Racing(carsCount, attemptsCount);
+
+        int raceCount = 0;
+        while (racing.hasNext()) {
+            racing.runRace();
+            raceCount++;
+        }
+
+        assertThat(raceCount).isEqualTo(attemptsCount);
+    }
+}
