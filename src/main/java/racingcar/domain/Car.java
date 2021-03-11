e8d0fe0f6563da214881b545bfffe636f1ba31e7
dailyworker <workingssu@gmail.com>
2021-03-11 20:43:57 +0900

    refactor : Rule에서 Car로 책임을 이동 - moveCar(int generatedRandomNumber) : 난수 발생기에서 발생된 난수를 토대로 움직일지 말지 정함 - isMoveable(int generatedRandomNumber) : 발생된 난수가 4이상인지 아닌지 판단 - positionToLine() : OutputView에서 처리했던 책임을 자신의 포지션을 토대로 자신의 위치를 String으로 리턴하게끔 함

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,9 @@
 package racingcar.domain;
 
 public class Car {
+
+  private static final int MOVEABLE_CRITERIA = 4;
+
   private int position = 0;
 
   public static Car createCar() {
@@ -11,7 +14,20 @@ public class Car {
     return position;
   }
 
-  public void moveCar() {
-    this.position++;
+  public void moveCar(int generatedRandomNumber) {
+    if(isMoveable(generatedRandomNumber))
+      this.position++;
+  }
+
+  private boolean isMoveable(int generatedRandomNumber) {
+    return generatedRandomNumber >= MOVEABLE_CRITERIA;
+  }
+
+  public String positionToLine() {
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i <= position; i++){
+      sb.append('-');
+    }
+    return sb.toString();
   }
 }
