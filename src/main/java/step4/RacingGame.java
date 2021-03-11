67a3a1521397f2992ff49daca3d475a555994dad
hwangjeonghyeon <deert101@gmail.com>
2021-03-12 01:15:10 +0900

    [feat] 4단계 자동 경주 구현

--- src/main/java/step4/RacingGame.java
+++ src/main/java/step4/RacingGame.java
@@ -1,2 +1,98 @@
-package step4;public class RacingGame {
+package step4;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RacingGame {
+
+    private static final int RANDOM_SCOPE = 10;
+    private final List<Car> cars;
+    List<Car> list;
+    private final int attempt;
+    private Random random = new Random();
+
+    public RacingGame(InputView inputView) {
+        String names = inputView.inputNames();
+        int attempt = inputView.inputAttempt();
+        this.cars = createCars(names);
+        this.attempt = attempt;
+    }
+
+    private List<Car> createCars(final String names) {
+        final List<Car> cars = new ArrayList<>();
+        for(String name : names.split(",")) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+    public void start() {
+        int round = attempt;
+
+        while (round-- != 0) {
+            move();
+        }
+        System.out.println(winner() + "가 최종 우승했습니다.");
+    }
+
+    private void move() {
+        list = new ArrayList<>();
+        for (Car car : cars) {
+            car.move(random.nextInt(RANDOM_SCOPE));
+            new ResultView(car).printEachCar();
+            list.add(car);
+        }
+        roundWinner();
+        println();
+    }
+
+    private boolean isGreaterOrEquals(int n, int advance) {
+        return advance >= n;
+    }
+
+    Map<String, Integer> map = new HashMap<>();
+    private List<String> roundWinner() {
+        List<String> carList = list.stream()
+                .filter(it -> isGreaterOrEquals(maxOfList(), it.getAdvance()))
+                .sorted(Comparator.comparing(Car::getAdvance).reversed())
+                .map(Car::getName)
+                .collect(Collectors.toList());
+
+        for (String name : carList) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        return carList;
+    }
+
+    private String winner() {
+        List<String> winner = new ArrayList<>();
+        int max = map.values()
+                .stream()
+                .mapToInt(x -> x)
+                .max()
+                .getAsInt();
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String k = entry.getKey();
+            Integer v = entry.getValue();
+            if (max <= v) winner.add(k);
+
+        }
+        return winner.stream().collect(Collectors.joining(","));
+    }
+
+    private int maxOfList() {
+        int n = list.stream()
+                .map(Car::getAdvance)
+                .max(Comparator.comparing(x -> x))
+                .get();
+
+        return n;
+    }
+
+    private void println() {
+        System.out.println();
+    }
 }
--- src/test/java/step4/RacingTests.java
+++ src/test/java/step4/RacingTests.java
@@ -1,2 +1,35 @@
-package step4;public class RacingTests {
+package step4;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingTests {
+    Car car;
+    @BeforeEach
+    public void before() {
+        Car car = new Car();
+    }
+
+    @DisplayName("전진 잘 되는지 테스트")
+    @Test
+    public void test1() {
+        assertThat(car.move(5)).isEqualTo(1);
+    }
+
+    @DisplayName("인자가 두개인 생성자 일 때 전진, 이름 출력 잘 되는지 확인")
+    @Test
+    public void test2() {
+
+    }
+
 }
