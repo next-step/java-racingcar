9ed0fb93eb29dd581037a3373fc12ca300bd9294
Donghui-Lee <ehdgml3206@naver.com>
2021-03-11 20:59:56 +0900

    feat: main application 추가

---
+++ src/main/java/study/step4/RacingApplication.java
@@ -0,0 +1,8 @@
+package study.step4;
+
+public class RacingApplication {
+    public static void main(String[] args) {
+        RacingService racingService = new RacingService();
+        racingService.play();
+    }
+}
