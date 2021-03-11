93e3b160a0eacfc216c7719385337609b52b1ff3
lisaimeee <lisaimeee@gmail.com>
2021-03-11 23:41:43 +0900

    refactor(RandomUtil): 매직넘버 제거 - 상수로 명시적으로 표현하기

--- src/main/java/racingcar/util/RandomUtil.java
+++ src/main/java/racingcar/util/RandomUtil.java
@@ -4,8 +4,12 @@ import java.util.Random;
 
 public class RandomUtil {
     private static Random random = new Random();
+    private static final int BOUND_TEN = 10;
+
+    private RandomUtil() {
+    }
 
     public static int getRandomIntBetweenZeroToNine() {
-        return random.nextInt(10);
+        return random.nextInt(BOUND_TEN);
     }
 }
