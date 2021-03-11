90d9bc1f34089c814debe1a852eee549718ee9ea
lee.jaesung <lee.jaesung@joins.com>
2021-03-11 10:15:19 +0900

    test: 테스트 방법을 CsvSource로 변경

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -1,31 +1,19 @@
 package step3;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CarTest {
 
-    @ParameterizedTest(name = "입력 숫자 {0}은 0을 반환한다.")
-    @ValueSource(ints = {0, 1, 2, 3})
-    public void moveForward_ShouldReturnZero(int inputValue) {
-        int expectedValue = 0;
-
+    @ParameterizedTest(name = "입력 숫자 {0}은 {1}을 반환한다.")
+    @CsvSource(value = {"0:0", "1:0", "2:0", "3:0", "4:1", "5:1", "6:1", "7:1", "8:1", "9:1"}, delimiter = ':')
+    public void moveForward_ShouldReturnZero(int inputValue, int expectedValue) {
         Car car = new Car();
         car.moveForward(inputValue);
 
         assertThat(car.getCurrentPosition()).isEqualTo(expectedValue);
     }
 
-    @ParameterizedTest(name = "입력 숫자 {0}은 1을 반환한다.")
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    public void moveForward_ShouldReturnOne(int inputValue) {
-        int expectedValue = 1;
-
-        Car car = new Car();
-        car.moveForward(inputValue);
-
-        assertThat(car.getCurrentPosition()).isEqualTo(expectedValue);
-    }
 }
