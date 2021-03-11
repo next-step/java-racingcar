c02e9e541c99a41d2835af02a2c584f17d474c4b
juniq <juniqlim@gmail.com>
2021-03-11 23:16:24 +0900

    feat: InputView에 getter 추가
    입력한 정보에 접근하기 위해 차대수, 게임횟수에 대한 getter 메소드 추가

--- src/main/java/im/juniq/racingcar/InputView.java
+++ src/main/java/im/juniq/racingcar/InputView.java
@@ -3,14 +3,22 @@ package im.juniq.racingcar;
 import java.util.Scanner;
 
 public class InputView {
+	private static int carCount;
+	private static int playCount;
+
 	public static void scan() {
 		System.out.println("자동차 대수는 몇 대 인가요?");
 		Scanner scanner = new Scanner(System.in);
-		String value = scanner.nextLine();
+		carCount = scanner.nextInt();
 		System.out.println("시도할 회수는 몇 회 인가요?");
-		int number = scanner.nextInt();
+		playCount = scanner.nextInt();
+	}
 
-		System.out.println("number = " + number);
-		System.out.println("value = " + value);
+	public static int getCarCount() {
+		return carCount;
+	}
+
+	public static int getPlayCount() {
+		return playCount;
 	}
 }
