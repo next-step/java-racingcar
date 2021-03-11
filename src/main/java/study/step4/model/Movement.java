42ce61a7fea0ea90f7c4640ad99693fa91e58672
Donghui-Lee <ehdgml3206@naver.com>
2021-03-11 19:49:57 +0900

    feat: 이동거리를 제한하는 클래스 구현

---
+++ src/main/java/study/step4/model/Movement.java
@@ -0,0 +1,24 @@
+package study.step4.model;
+
+import java.util.Random;
+
+/**
+ * Movement Class
+ *
+ * 이동을 제한하는 클래스
+ */
+public class Movement {
+    private static final int RANDOM_RANGE = 10;
+    private static final int MOVE_RESTRICT = 4;
+    private static final Random random = new Random();
+
+    public Movement() {
+    }
+
+    public int stopAndMove() {
+        if(random.nextInt(RANDOM_RANGE) >= MOVE_RESTRICT) {
+            return 1;
+        }
+        return 0;
+    }
+}
