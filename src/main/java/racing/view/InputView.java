f0cc4494d1e8aee4d8817399e96565b279b90cd0
povia <47127607+povia@users.noreply.github.com>
2021-03-11 13:28:06 +0900

    fix: 기능 구현 목록 업데이트 및 입력 클래스 방식 변경

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -1,23 +1,25 @@
 package racing.view;
 
-import racing.model.RequestDto;
-
+import java.util.Random;
 import java.util.Scanner;
 
-/** View 전용 입력 관리 클래스 */
+/** 
+ * View 전용 입력 관리 클래스
+ * 인스턴스가 생성되는 것을 막기 위해 static method들로만 구성함
+ */
 public class InputView {
-  Scanner scanner;
+  private static Scanner scanner;
 
-  public InputView(){
+  private InputView(){
     scanner = new Scanner(System.in);
   }
 
-  public int inputCarCount(){
+  public static int inputCarCount(){
     System.out.println("자동차의 대수는 몇 대 인가요?");
     return scanner.nextInt();
   }
 
-  public int inputPeriod(){
+  public static int inputPeriod(){
     System.out.println("시도할 회수는 몇 회 인가요?");
     return scanner.nextInt();
   }
