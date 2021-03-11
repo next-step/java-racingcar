e2c964fdb1b6ced0a7733853c0b49afd716bd0ae
jhLim97 <wnsgur2749@naver.com>
2021-03-11 21:23:31 +0900

    refacotr: 매직넘버 상수화

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,12 +9,13 @@ import java.util.Scanner;
 public class InputView {
 
     private static Scanner scanner = new Scanner(System.in);
+    private static final String SEPARATOR = ",";
 
     public String[] inputNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.");
         System.out.println("<주의> 자동차 이름은 5자를 초과할 수 없습니다.");
 
-        return scanner.nextLine().split(",");
+        return scanner.nextLine().split(SEPARATOR);
     }
 
     public int inputCountRound() {
