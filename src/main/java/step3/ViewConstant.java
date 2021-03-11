3661f04c277e5e83d577b826c3ea2e354759dc37
wkdehdlr <wkdehdlr@gmail.com>
2021-03-11 23:47:17 +0900

    [feat]InputView 이름길이조건 구현

--- src/main/java/step3/ViewConstant.java
+++ src/main/java/step3/ViewConstant.java
@@ -4,6 +4,8 @@ public class ViewConstant {
 
     public static final String ENTER_CAR_COUNT = "자동차 대수는 몇대 인가요?";
     public static final String ENTER_ROUND_COUNT = "시도할 횟수는 몇대 인가요?";
+    public static final String ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String RESULT = "실행 결과";
     public static final String TRACK = "-";
+    public static final String DELIMITER = ",";
 }
