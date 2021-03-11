9c78125e24809d57b8c54d9f74738db3bd6b793c
Sooncheol <ryxhdrnrmf@gmail.com>
2021-03-11 18:33:07 +0900

    feat: Car 구현

---
+++ src/main/java/step3/Car.java
@@ -0,0 +1,16 @@
+package step3;
+
+public class Car {
+    private int distance;
+    private MoveStrategy moveStrategy;
+
+    Car(MoveStrategy moveStrategy){
+        this.distance = 0;
+        this.moveStrategy = moveStrategy;
+    }
+
+    void move(){
+        this.distance += this.moveStrategy.getDistance();
+    }
+
+}
