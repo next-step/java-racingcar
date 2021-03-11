0428abc77c5787ad341fd5cd46cf53f8583b3292
juniq <juniqlim@gmail.com>
2021-03-11 23:20:43 +0900

    repactor: InputView의 맴버변수명 변경
    더 어울리는 변수명으로 변

--- src/main/java/im/juniq/racingcar/InputView.java
+++ src/main/java/im/juniq/racingcar/InputView.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class InputView {
 	private static int carCount;
-	private static int playCount;
+	private static int tryCount;
 
 	public static void scan() {
 		System.out.println("자동차 대수는 몇 대 인가요?");
 		Scanner scanner = new Scanner(System.in);
 		carCount = scanner.nextInt();
 		System.out.println("시도할 회수는 몇 회 인가요?");
-		playCount = scanner.nextInt();
+		tryCount = scanner.nextInt();
 	}
 
 	public static int getCarCount() {
 		return carCount;
 	}
 
-	public static int getPlayCount() {
-		return playCount;
+	public static int getTryCount() {
+		return tryCount;
 	}
 }
