7d5d19233e1c0c13b51ed84ae4d30bf2eea705eb
dailyworker <workingssu@gmail.com>
2021-03-11 20:46:29 +0900

    feat : Cars 기능 추가 - updateAll(RandomNumberGenerator randomNumberGenerator) : 난수 발생기를 주입받아서 실제 Car에게 움직일지 말지 판단하게함. - currentCarsPosition() : OutputView가 실질적으로 Car에 접근하는 걸 막기위해서 currentCarsPosition()으로 접근하고 내부로직은 Cars에서 처리

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,8 +1,10 @@
 package racingcar.domain;
 
 import java.util.List;
+import racingcar.util.RandomNumberGenerator;
 
 public class Cars {
+
   private final List<Car> cars;
 
   public Cars(List<Car> cars) {
@@ -11,5 +13,19 @@ public class Cars {
 
   public List<Car> getCars() {
     return cars;
+  }
+
+  public void updateAll(RandomNumberGenerator randomNumberGenerator){
+    for(Car car : cars) {
+      car.moveCar(randomNumberGenerator.generateRandomNumber());
+    }
+  }
+
+  public String currentCarsPosition() {
+    StringBuilder sb = new StringBuilder();
+    for(Car car : cars) {
+      sb.append(car.positionToLine()).append("\n");
+    }
+    return sb.toString();
   }
 }
