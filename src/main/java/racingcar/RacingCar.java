d8242fb697b8bae1477613dfbfe134937dc7dd0b
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-12 14:31:22 +0900

    실질적인 Car 클래스 움직임에 관여하는 코드 작성

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,36 +1,30 @@
 package racingcar;
 
-import java.util.List;
+// 자동차의 움직임을 관여한다.
+
 import java.util.Random;
 
-// 자동차의 움직임을 관여한다.
-public class MoveCar {
+public class RacingCar {
 
-    // n번만큼 이동하기
-    public void moveToN(List<Car> carList, int count) {
-        for (int i = 0; i < carList.size(); i++) {
-            carList.get(i).setLocation(isGo(random()));
-            System.out.println("");
-        }
+    private Car car;
+
+    public RacingCar(Car car) {
+        this.car = car;
     }
 
-    // 전진하기
-    private String moveForward() {
-        return "-";
+    public void move() {
+        car.setLocation(isGo(random()));
     }
 
-    // 멈춤
-    private String stop() {
+    public String status() {
+        return car.getLocation();
+    }
+
+    public String isGo(int number) {
+        if (number >= 4) return "-";
         return "";
     }
 
-    // 앞으로 가는 조건
-    public String isGo(int number) {
-        if (number >= 4) return moveForward();
-        return stop();
-    }
-
-    // 랜덤함수 출력
     public int random() {
         Random random = new Random();
         int value = random.nextInt(10);
