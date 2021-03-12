38121cee4959dbd897f0f44b152ce8cc8c455fb7
623nana <6230nana@gmail.com>
2021-03-12 14:15:17 +0900

    refactor: 자동차 이름 유효성 클래스 메소드로 분리해서 사용 되도록 수정

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -2,19 +2,20 @@ package racingcar.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.exception.CarNameException;
 import racingcar.model.Car;
-import racingcar.util.CarNameValidator;
 import racingcar.view.ResultView;
 
 public class RacingGame {
 
+  private static final int MAX_CAR_NAME_LENGTH = 5;
   public List<Car> cars = new ArrayList<>();
 
   public void readyGame(String inputName) {
     String[] names = inputName.split(",");
 
     for (int i = 0; i < names.length; i++) {
-      CarNameValidator.validateName(names[i]);
+      validateName(names[i]);
       cars.add(new Car(names[i]));
     }
   }
@@ -29,5 +30,22 @@ public class RacingGame {
     }
 
     ResultView.showWinnerName(cars);
+  }
+
+  public void validateName(String name) {
+    isMaxLength(name);
+    isEmpty(name);
+  }
+
+  private void isMaxLength(String name) {
+    if (name.length() > MAX_CAR_NAME_LENGTH) {
+      throw new CarNameException("자동차 이름은 " + MAX_CAR_NAME_LENGTH + "자를 초과할 수 없습니다.");
+    }
+  }
+
+  private void isEmpty(String name) {
+    if ("".equals(name)) {
+      throw new CarNameException("자동차 이름을 입력하세요.");
+    }
   }
 }
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.controller.RacingGame;
+import racingcar.exception.CarNameException;
 
 public class RacingGameTest {
 
@@ -23,4 +25,11 @@ public class RacingGameTest {
     assertThat(racingGame.cars.size()).isEqualTo(3);
   }
 
+  @ParameterizedTest(name = "[RacingGame] validateName 테스트 - {index}")
+  @ValueSource(strings = {"", "abcdef", "abcdefg"})
+  void validateNameTest(String input) {
+    assertThatThrownBy(() -> {
+      racingGame.validateName(input);
+    }).isInstanceOf(CarNameException.class);
+  }
 }
