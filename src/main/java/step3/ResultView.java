5e31cb67ee6213d2a67f873a1c31e406ecaf9551
lee.jaesung <lee.jaesung@joins.com>
2021-03-12 10:37:10 +0900

    refactor: StringUtil 의 하이픈으로 출력하는 함수 refactoring

--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 public class ResultView {
 
+    private final String PRINT_EXPRESSION_STR = "-";
+
     public ResultView() {
         System.out.println("실행 결과");
     }
-    
+
     public void printOutResult(List<Car> carList) {
         for (Car car : carList) {
-            System.out.println(StringUtil.printToHyphen(car.getCurrentPosition()));
+            System.out.println(StringUtil.repeat(PRINT_EXPRESSION_STR, car.getCurrentPosition()));
         }
-        System.out.println("");
+        System.out.println();
     }
 }
--- src/main/java/step3/StringUtil.java
+++ src/main/java/step3/StringUtil.java
@@ -2,11 +2,11 @@ package step3;
 
 public class StringUtil {
 
-    public static String printToHyphen(int printCount) {
+    public static String repeat(String string, int printCount) {
         StringBuffer stringBuffer = new StringBuffer();
 
         for (int i = 0; i < printCount; i++) {
-            stringBuffer.append("-");
+            stringBuffer.append(string);
         }
 
         return stringBuffer.toString();
