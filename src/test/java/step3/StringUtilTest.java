6741a42cb5c27bf892a62bc09eec670963e2097e
lee.jaesung <lee.jaesung@joins.com>
2021-03-12 10:37:53 +0900

    test: StringUtil 의 하이픈 출력 함수 refactoring에 의한 테스트 코드 수정

--- src/test/java/step3/StringUtilTest.java
+++ src/test/java/step3/StringUtilTest.java
@@ -7,13 +7,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class StringUtilTest {
 
-    @DisplayName("입력받은 숫자 만큼 하이픈 문자열을 반환한다.")
+    @DisplayName("하이픈 문자와 5개의 출력해야할 숫자를 입력받아 하이픈 5개의 문자열을 반환한다.")
     @Test
-    public void printToHyphen_ShouldReturnHyphenString() {
+    public void repeat_ShouldReturnHyphenString() {
         int inputNumber = 5;
+        String printExpressionStr = "-";
         String expectedValue = "-----";
 
-        String actual = StringUtil.printToHyphen(inputNumber);
+        String actual = StringUtil.repeat(printExpressionStr, inputNumber);
 
         assertThat(actual).isEqualTo(expectedValue);
     }
