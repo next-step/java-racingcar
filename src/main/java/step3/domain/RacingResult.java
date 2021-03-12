3199542565af4fc0219fd8d362ce6ee30afec7e2
seokrae <kslbsh@gmail.com>
2021-03-12 14:59:45 +0900

    test: RacingResult 메서드 분리로 의미 있는 테스트 코드 작성해보기

--- src/main/java/step3/domain/RacingResult.java
+++ src/main/java/step3/domain/RacingResult.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * 컬렉션 정보를 관리하는 레이싱 결과를 담는 클래스
@@ -20,25 +23,41 @@ public class RacingResult {
         this.racingRounds.add(racingRound);
     }
 
-    public List<RacingRound> getRacingRounds() {
-        return Collections.unmodifiableList(racingRounds);
-    }
+    // 레이싱 결과를 담는 클래스에 우승자를 배열 타입으로 반환
+    public String[] findWinners() {
+        List<Car> cars = getLastRound();
 
-    // 레이싱 결과를 담는 클래스에 승자를 출력하기 위한 메서드
-    public String[] getWinner() {
-        RacingRound lastRound = getRacingRounds().get(racingRounds.size() - 1);
-        List<Car> cars = lastRound.getCars();
-
-        int maxPosition = getMaxPosition(cars);
-        return cars.stream()
-                .filter(car -> car.getPosition() == maxPosition)
-                .map(Car::getName)
+        Position maxPosition = getMaxPosition(cars);
+        List<Name> names = findWinnerNames(cars, maxPosition);
+        return names.stream()
+                .map(Name::getName)
                 .toArray(String[]::new);
     }
 
-    private int getMaxPosition(List<Car> cars) {
+    // 메서드 내에 기능을 분리 -> cars 리스트에서 maxPosition 에 해당하는 Name 리스트를 반환
+    private List<Name> findWinnerNames(List<Car> cars, Position maxPosition) {
         return cars.stream()
-                .max(Comparator.comparing(Car::getPosition))
-                .get().getPosition();
+                .filter(car -> car.isWinner(maxPosition))
+                .map(Car::getName)
+                .collect(toList());
+    }
+
+    // TODO 마지막 라운드를 가져오는 좋은 방법 찾기
+    private List<Car> getLastRound() {
+        return getRacingRounds()
+                .get(racingRounds.size() - 1)
+                .getCars();
+    }
+
+    // 자동차 리스트 -> Position 리스트 -> 가장 큰 값으로 반환
+    protected Position getMaxPosition(List<Car> cars) {
+        return cars.stream()
+                .map(Car::getPosition)
+                .reduce(Position::greaterThan)
+                .orElseGet(Position::new);
+    }
+
+    public List<RacingRound> getRacingRounds() {
+        return Collections.unmodifiableList(racingRounds);
     }
 }
