95b06890bbd21b02f12c7e000a7c1339f6b62b2d
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-12 14:36:22 +0900

    refactor : 움직이는 RacingCar에 대한 초기 설정과, 결과값을 반환하기 위해 수정

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,63 +2,42 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class RacingGame {
     private int cars, count;
 
-    // 몇대의 차, 몇번의 이동 초기화
     public RacingGame(int cars, int count) {
         this.cars = cars;
         this.count = count;
     }
 
-    // n개 만큼 Car 클래스 생성하기
-    public List<Car> settingCars(int cars) {
+    public List<RacingCar> settingRacingCars(int cars) {
 
-        ArrayList<Car> list = new ArrayList<>();
+        ArrayList<RacingCar> list = new ArrayList<>();
 
         for (int i = 0; i < cars; i++) {
-            Car car = new Car();
-            list.add(car);
+            RacingCar racingCar = new RacingCar(new Car());
+            list.add(racingCar);
         }
 
         return list;
     }
 
-    // n번만큼 이동하기
-    public void moveToN(List<Car> carList, int count) {
+    public void gameStart(List<RacingCar> carList, int count) {
+        numberingGameTimes(carList, count);
+    }
+
+    private void numberingGameTimes(List<RacingCar> carList, int count) {
         for (int i = 0; i < count; i++) {
-            carStatus(carList);
-            System.out.println("");
+            numberingCars(carList);
+            System.out.print("\n");
         }
     }
 
-    // n개의 차의 대한 상태 출력
-    public void carStatus(List<Car> carList) {
-        for (int i = 0; i < carList.size(); i++) {
-            carList.get(i).setLocation(isGo(random()));
-            new ResultView().result(carList.get(i).getLocation());
+    private void numberingCars(List<RacingCar> carList) {
+        for (int j = 0; j < carList.size(); j++) {
+            carList.get(j).move();
+            System.out.println(carList.get(j).status());
         }
-    }
-
-    // 전진하기
-    private String moveForward() {
-        return "-";
-    }
-
-    // 앞으로 가는 조건
-    public String isGo(int number) {
-        if (number >= 4) return moveForward();
-        return "";
-    }
-
-    // 랜덤함수 출력
-    public int random() {
-
-        Random random = new Random();
-        int value = random.nextInt(10);
-
-        return value;
     }
 }
