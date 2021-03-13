3ead6f6e31fac8cb57aa67afa6224dc16d3601ac
juniq <juniqlim@gmail.com>
2021-03-14 00:20:47 +0900

    test: 난수 이동 전략 테스트 추가

--- src/test/java/im/juniq/racingcar/RandomNumberMovingStrategyTest.java
+++ src/test/java/im/juniq/racingcar/RandomNumberMovingStrategyTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 class RandomNumberMovingStrategyTest {
 	@DisplayName("발생된 난수가 5일때는 이동한다")
 	@Test
-	void moveWhenRandomNumberNine() {
+	void moveWhenRandomNumberFive() {
 		RandomNumberMovingStrategy randomNumberMovingStrategy = new RandomNumberMovingStrategy();
 		randomNumberMovingStrategy.setRandomNumber(new RandomNumberZeroToNine(new Random() {
 			@Override
@@ -20,5 +20,19 @@ class RandomNumberMovingStrategyTest {
 		}));
 
 		assertThat(randomNumberMovingStrategy.isMoving()).isEqualTo(true);
+	}
+
+	@DisplayName("발생된 난수가 4일때는 이동하지 않는다")
+	@Test
+	void stopWhenRandomNumberFour() {
+		RandomNumberMovingStrategy randomNumberMovingStrategy = new RandomNumberMovingStrategy();
+		randomNumberMovingStrategy.setRandomNumber(new RandomNumberZeroToNine(new Random() {
+			@Override
+			public int nextInt(int bound) {
+				return 4;
+			}
+		}));
+
+		assertThat(randomNumberMovingStrategy.isMoving()).isEqualTo(false);
 	}
 }
