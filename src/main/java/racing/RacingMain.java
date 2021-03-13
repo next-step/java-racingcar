f1e0e40605d1f6f710ef921c2df164aec38ab831
ad7549 <ad7549@gmail.com>
2021-03-14 04:03:16 +0900

    feat: Fix InputView to be created only once

--- src/main/java/racing/RacingMain.java
+++ src/main/java/racing/RacingMain.java
@@ -2,8 +2,9 @@ package racing;
 
 public class RacingMain {
     public static void main(String[] args) {
-        final int carsCount = new InputView().show("자동차 대수는 몇 대인가요?");
-        final int attemptsCount = new InputView().show("시도할 횟수는 몇 회인가요?");
+        final InputView inputView = new InputView();
+        final int carsCount = inputView.show("자동차 대수는 몇 대인가요?");
+        final int attemptsCount = inputView.show("시도할 횟수는 몇 회인가요?");
 
         final Racing racing = new Racing(carsCount, attemptsCount);
         RacingUtil.start(racing);
