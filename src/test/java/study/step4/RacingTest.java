7440475e38630df39ceb60d53c3a5d0f40b99a39
Donghui-Lee <ehdgml3206@naver.com>
2021-03-13 18:48:23 +0900

    refactor: 사용하지 않은 import문 제거

--- src/test/java/study/step4/RacingTest.java
+++ src/test/java/study/step4/RacingTest.java
@@ -2,6 +2,7 @@ package study.step4;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
