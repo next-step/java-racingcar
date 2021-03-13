1dd933c4f5b4ebb34077eae22e2efc8a74c54a0b
abcdefg <choiyurim@naver.com>
2021-03-13 21:04:23 +0900

    refactor(CarTest): carTestMethod 변경

--- src/test/java/step4/CarTest.java
+++ src/test/java/step4/CarTest.java
@@ -1,39 +1,39 @@
 package step4;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
+    @DisplayName("car 생성자 테스트 - 4자 이하")
     @Test
-    void test001() {
+    void carTest() {
+        Car car = new Car("aaaa");
+        Assertions.assertEquals(car.getName(), "aaaa");
+    }
+
+    @DisplayName("car 생성자 테스트 - 5자 이상")
+    @Test
+    void carNameSizeOverTest() {
         Assertions.assertThrows(
                 IllegalArgumentException.class, () -> new Car("aaaaa"));
     }
 
+    @DisplayName("car 이동 테스트 - 이동했을 경우")
     @Test
-    void test002() {
-        Assertions.assertDoesNotThrow(() -> new Car("aaaa"));
-    }
-
-    @Test
-    void test003() {
+    void carMoveTest() {
         Car car = new Car("aaaa");
         car.move(4);
         Assertions.assertEquals(car.getPosition(), 2);
     }
 
+    @DisplayName("car 이동 테스트 - 멈췄을경우")
     @Test
-    void test004() {
+    void catStopTest() {
         Car car = new Car("aaaa");
         car.move(3);
         Assertions.assertEquals(car.getPosition(), 1);
-    }
-
-    @Test
-    void test005() {
-        Car car = new Car("aaaa");
-        Assertions.assertEquals(car.getName(), "aaaa");
     }
 
 
