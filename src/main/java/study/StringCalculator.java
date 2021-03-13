42119519183a41c68d0ecd116b8dcb71645a7c9d
0hun <whdudgns2654@gmail.com>
2021-03-13 23:16:02 +0900

    feat(feature)  :  입력값이 null 또는 공백체크
    입력값이 null 또는 공백체크
    - 입력값이 null 또는 공백일 경우 예외 throw

--- src/main/java/study/StringCalculator.java
+++ src/main/java/study/StringCalculator.java
@@ -9,6 +9,9 @@ public class StringCalculator {
 
     for (int i = 1; i < tokens.length; i += 2) {
       String operator = tokens[i];
+
+      isEmptyOrNull(operator);
+
       double number = toDouble(tokens[i + 1]);
       result = Operation.calculate(operator, result, number);
     }
@@ -17,10 +20,14 @@ public class StringCalculator {
   }
 
   private static double toDouble(String value) {
+    isEmptyOrNull(value);
+    return Double.parseDouble(value);
+  }
+
+  public static void isEmptyOrNull(String value) {
     if (value == null || "".equals(value)) {
       throw new IllegalArgumentException();
     }
-    return Double.parseDouble(value);
   }
 
 }
