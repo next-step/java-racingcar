1ef25670aff2084fdbc39626cf39275b8c634cc5
jhLim97 <wnsgur2749@naver.com>
2021-03-13 23:32:37 +0900

    test: 주어진 횟수동안 수행하는지 테스트하는 코드 추가

---
+++ src/test/java/racingcar/contoller/RacingCarControllerTest.java
@@ -0,0 +1,29 @@
+package racingcar.contoller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.domain.Round;
+import racingcar.dto.InputManagement;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RacingCarControllerTest {
+
+    @ParameterizedTest
+    @CsvSource({"5, 5"})
+    @DisplayName("주어진 횟수 동안 경주 게임을 진행할 수 있다.")
+    void carsCanGoInGivenCount(int countRound, int finish) {
+        String[] carNames = {"pobi", "jhLim97"};
+        InputManagement inputManagement = new InputManagement(carNames, countRound);
+
+        RacingCarController racingCarController = new RacingCarController(null, inputManagement);
+
+        Round round = new Round();
+        while (racingCarController.hasNextRound(round)) {
+            round.update();
+        }
+
+        assertThat(countRound).isEqualTo(finish);
+    }
+}
