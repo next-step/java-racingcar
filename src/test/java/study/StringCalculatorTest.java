4b076a9bb20e00bd72b60f836f82779f58cbfbf2
0hun <whdudgns2654@gmail.com>
2021-03-13 23:26:07 +0900

    test(when expressionParameterizedGreaterThanZero test)  :  입력값이 여러개일 경우 파라미터 테스트를 통해 0보다 큰지 테스트
    입력값이 여러개일 경우 파라미터 테스트를 통해 0보다 큰지 테스트

--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class StringCalculatorTest {
 
@@ -34,6 +36,17 @@ class StringCalculatorTest {
 
     // then
     assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @DisplayName("입력 값이 여러개 일때, 묶어서 테스트하여 0보다 큰지")
+  @ParameterizedTest
+  @ValueSource(strings = {"2 + 3 * 4 / 2", "3 + 5 * 2 / 4"})
+  void expressionParameterizedGreaterThanZero(String calculateExpression) {
+    //when
+    double result = StringCalculator.calculate(calculateExpression);
+
+    // then
+    assertThat(result).isGreaterThan(0);
   }
 
 }
