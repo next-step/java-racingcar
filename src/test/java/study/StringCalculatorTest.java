67abf1f30314e0f8a2c937525504eb4dbc512b00
0hun <whdudgns2654@gmail.com>
2021-03-13 23:17:49 +0900

    test(when isNullOrEmptyException test)  :  입력값이 null 또는 공백일때 예외 테스트
    입력값이 null 또는 공백일때 예외 테스트

--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,11 +14,26 @@ class StringCalculatorTest {
     // given
     String calculateExpression = "2 + 3 * 4 / 2";
 
-    // then
+    // when
     double result = StringCalculator.calculate(calculateExpression);
 
     // then
     assertThat(result).isEqualTo(10);
+  }
+
+  @DisplayName("입력 값이 null이거나, 공백일때 예외테스트")
+  @Test
+  void expressionIsEmptyOrIsNull() {
+    // given
+    String calculateExpression = "2  3 * 4 / 2";
+
+    // when
+    Throwable thrown = catchThrowable(() -> {
+      StringCalculator.calculate(calculateExpression);
+    });
+
+    // then
+    assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
   }
 
 }
