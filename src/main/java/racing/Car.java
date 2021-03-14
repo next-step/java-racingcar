0078c341477a07c860293e25cda4fb47018660be
ad7549 <ad7549@gmail.com>
2021-03-14 11:17:19 +0900

    refactor: Add final keyword to constant values

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -3,12 +3,12 @@ package racing;
 import java.util.function.Supplier;
 
 public class Car implements Cloneable {
-    public static int MIN_BOUNDARY_VALUE = 0;
-    public static int MAX_BOUNDARY_VALUE = 9;
-    public static int MIN_MOVABLE_VALUE = 4;
     private static final int MAX_NAME_LENGTH = 5;
-
     private static final String DEFAULT_NAME = "empty";
+
+    public static final int MIN_BOUNDARY_VALUE = 0;
+    public static final int MAX_BOUNDARY_VALUE = 9;
+    public static final int MIN_MOVABLE_VALUE = 4;
 
     private final String name;
 
