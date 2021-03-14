5fcdff37c63de8475bf1b6a270b80c47bb09e3f6
hwangjeonghyeon <deert101@gmail.com>
2021-03-14 20:02:35 +0900

    [test] 자동차경주 테스트코드 추가

--- src/test/java/step5/CarNameTest.java
+++ src/test/java/step5/CarNameTest.java
@@ -1,2 +1,32 @@
-package step5;public class CarNameTest {
+package step5;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step5.domain.Car;
+
+public class CarNameTest {
+
+    Car car1;
+    Car car2;
+    @BeforeEach
+    public void before() {
+        car1 = new Car("poby", 2);
+        car2 = new Car("pobyss", 2);
+    }
+
+    @DisplayName("5글자 초과")
+    @Test
+    void 이상() {
+        assertThat(car2.isExceedWord()).isEqualTo(true);
+    }
+
+    @DisplayName("5글자 미만")
+    @Test
+    void 미만() {
+        assertThat(car1.isExceedWord()).isEqualTo(false);
+    }
 }
--- src/test/java/step5/CarPositionTest.java
+++ src/test/java/step5/CarPositionTest.java
@@ -1,2 +1,55 @@
-package step5;public class CarPositionTest {
+package step5;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step5.domain.Car;
+import step5.domain.Cars;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarPositionTest {
+
+    Car car;
+    @BeforeEach
+    public void before() {
+        car = new Car("poby", 2);
+    }
+
+    @DisplayName("전진 잘 되는지 테스트")
+    @Test
+    void 전진_O() {
+        Car car = new Car("qq",0) {
+            @Override
+            protected int generateRandomValue() {
+                return 4;
+            }
+        };
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("전진하지않는 테스트")
+    @Test
+    void 전진_X() {
+        Car car = new Car("qq",0) {
+            @Override
+            protected int generateRandomValue() {
+                return 3;
+            }
+        };
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @DisplayName("우승자 구하기")
+    @Test
+    public void 우승자() {
+        Car poby = new Car("poby",2);
+        Car byby = new Car("byby",2);
+        Car ruby = new Car("ruby",1);
+
+        Cars carList = new Cars(Arrays.asList(poby, byby, ruby));
+        assertThat(carList.findWinner()).isEqualTo(Arrays.asList(poby,byby));
+    }
 }
