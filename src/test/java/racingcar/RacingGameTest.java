765ab1284b8c5edb4eff7df2657c70f2b23ad00c
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-14 23:34:58 +0900

    test : 게임종료 테스트 메소드 추가,  규칙 7으로 인한 테스트 코드 수정

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,21 +15,35 @@ public class RacingGameTest {
     @DisplayName("0~9까지 랜덤 출력")
     @Test
     void random_test() {
-        System.out.println(new RacingCar(new Car()).random());
+        System.out.println(new RacingCar(new Position()).random());
     }
 
 
     @DisplayName("주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.")
     @ParameterizedTest
     @CsvSource(value = {"1:1", "2:1 ", "3:5"}, delimiter = ':')
-        void cars_can_moves_given_number_test(int cars, int count) {
+    void cars_can_moves_given_number_test(int cars, int count) {
 
-        RacingGame racingGame = new RacingGame(cars, count);
-        List<RacingCar> list = racingGame.settingRacingCars(cars);
+        GameSet gameSet = new GameSet(cars, count);
+        RacingGame racingGame = new RacingGame(gameSet);
+
+        List<RacingCar> list = racingGame.settingRacingCars(gameSet.getCars());
         racingGame.gameStart(list, count);
-        assertThat(list.size()).isEqualTo(cars);
-        assertThat(count).isEqualTo(racingGame.getRound());
+        assertThat(list.size()).isEqualTo(gameSet.getCars());
+        assertThat(count).isEqualTo(gameSet.getGivenRound());
 
     }
-    
+
+    @DisplayName("게임 종료")
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:1 ", "3:5"}, delimiter = ':')
+    void game_over(int cars, int count) {
+        GameSet gameSet = new GameSet(cars, count);
+        RacingGame racingGame = new RacingGame(gameSet);
+
+        List<RacingCar> list = racingGame.settingRacingCars(gameSet.getCars());
+        racingGame.gameStart(list, count);
+        assertThat(racingGame.gameOver()).isTrue();
+    }
+
 }
