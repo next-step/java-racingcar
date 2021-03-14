ba76f18009438f75657f2c6aa10ab22d00885248
Sooncheol <ryxhdrnrmf@gmail.com>
2021-03-14 18:51:00 +0900

    feat: InputView 구현

---
+++ src/main/java/step4/InputView.java
@@ -0,0 +1,37 @@
+package step4;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class InputView {
+
+    static String getInput(){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        checkInputNull(input);
+        return input;
+    }
+
+    static void checkInputNull(String input){
+        if(input == null || input.trim().isEmpty()){
+            throw new NoSuchElementException("Input cannot be blank or null");
+        }
+    }
+
+    static String[] splitInput(String input){
+        String[] carNames = input.split(CarRacingConstant.COMMA);
+        checkCarNamesLength(carNames);
+        return carNames;
+    }
+
+    static void checkCarNamesLength(String[] carNames){
+        Arrays.stream(carNames)
+                .filter(name -> name.length() > CarRacingConstant.LIMIT_OF_NAME_LENGTH)
+                .findAny()
+                .ifPresent(present -> {
+                    throw new IllegalArgumentException();
+                });
+    }
+
+}
