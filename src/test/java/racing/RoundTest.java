1dfa1646914b21b8922d1f9c92cc39e76aafb7b8
ad7549 <ad7549@gmail.com>
2021-03-14 12:09:46 +0900

    test: Add test for winners to RoundTest

--- src/test/java/racing/RoundTest.java
+++ src/test/java/racing/RoundTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,5 +28,53 @@ public class RoundTest {
             assertThat(roundCars.get(i)).isNotEqualTo(cars.get(i));
             assertThat(roundCars.get(i).getDistance()).isEqualTo(cars.get(i).getDistance());
         }
+    }
+
+    void mustMove(Car car, int count) {
+        final int mustMoveValue = 9;
+
+        for (int i = 0; i < count; i++) {
+            car.tryMove(() -> mustMoveValue);
+        }
+    }
+
+    @Test
+    @DisplayName("우승자(distance가 가장 큰 Car 객체)를 구한다.")
+    void getWinners() {
+        final Car pobi = new Car("pobi");
+        final Car crong = new Car("crong");
+        final Car honux = new Car("honux");
+
+        mustMove(pobi, 5);
+        mustMove(crong, 3);
+        mustMove(honux, 1);
+
+        final Round round = Round.createFrom(Arrays.asList(pobi, crong, honux));
+
+        final List<String> expectedNames = Stream.of(pobi).map(Car::getName).collect(Collectors.toList());
+        final List<String> winnerNames = round.getWinners().stream().map(e -> e.getName()).collect(Collectors.toList());
+
+        // assert
+        assertThat(winnerNames).containsAll(expectedNames);
+    }
+
+    @Test
+    @DisplayName("우승자(distance가 가장 큰 Car 객체)를 구한다. 한 명 이상일 수 있다.")
+    void getWinners2() {
+        final Car pobi = new Car("pobi");
+        final Car crong = new Car("crong");
+        final Car honux = new Car("honux");
+
+        mustMove(pobi, 5);
+        mustMove(crong, 5);
+        mustMove(honux, 1);
+
+        final Round round = Round.createFrom(Arrays.asList(pobi, crong, honux));
+
+        final List<String> expectedNames = Stream.of(pobi, crong).map(Car::getName).collect(Collectors.toList());
+        final List<String> winnerNames = round.getWinners().stream().map(e -> e.getName()).collect(Collectors.toList());
+
+        // assert
+        assertThat(winnerNames).containsAll(expectedNames);
     }
 }
