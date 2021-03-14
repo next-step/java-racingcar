8ae266621e246de6eca8c8d7834682bb4c6495fd
dailyworker <workingssu@gmail.com>
2021-03-14 20:52:06 +0900

    feat : Car name 상태 추가 및 Cars 우승자 출력 로직 추가 - Car 1. 상태 값으로 name을 갖게하고, name을 통해서 생성되게 로직 변경 - Cars 1. currentCarsPosition() : 자동차의 이름과 함께 현재 위치를 출력하게 변경 2. getWinner() : 현재 가장 높은 위치를 가진 자동차들의 이름을 가져와서 우승자를 출력하게끔 함 3. getMaxPositionInCars(int max) : 현재 가장 높은 위치를 알아내는 함수 4. getWinnerNames() : getMaxPositionInCars() 결과값을 토대로 우승자의 이름을 추출하는 함수

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,42 @@
+package racingcar.domain;
+
+public class Car {
+
+  private static final int MOVEABLE_CRITERIA = 4;
+
+  private int position = 0;
+  private final String name;
+
+  public Car(String name) {
+    this.name = name;
+  }
+
+  public static Car createCar(String name) {
+    return new Car(name);
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void moveCar(int generatedRandomNumber) {
+    if(isMoveable(generatedRandomNumber))
+      this.position++;
+  }
+
+  private boolean isMoveable(int generatedRandomNumber) {
+    return generatedRandomNumber >= MOVEABLE_CRITERIA;
+  }
+
+  public String positionToLine() {
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i <= position; i++){
+      sb.append('-');
+    }
+    return sb.toString();
+  }
+}
---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,57 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.util.RandomNumberGenerator;
+
+public class Cars {
+
+  private final List<Car> cars;
+
+  public Cars(List<Car> cars) {
+    this.cars = cars;
+  }
+
+  public List<Car> getCars() {
+    return cars;
+  }
+
+  public void updateAll(RandomNumberGenerator randomNumberGenerator){
+    for(Car car : cars) {
+      car.moveCar(randomNumberGenerator.generateRandomNumber());
+    }
+  }
+
+  public String currentCarsPosition() {
+    StringBuilder sb = new StringBuilder();
+    for(Car car : cars) {
+      sb.append(car.getName()).append(" : ").append(car.positionToLine()).append("\n");
+    }
+    return sb.toString();
+  }
+
+  public List<String> getWinner() {
+    int max = 0;
+    List<String> winnerNames = new ArrayList<>();
+
+    getWinnerNames(winnerNames, getMaxPositionInCars(max));
+
+    return winnerNames;
+  }
+
+  private void getWinnerNames(List<String> winnerNames, int max) {
+    for(Car car : cars) {
+      if(car.getPosition() == max) {
+        winnerNames.add(car.getName());
+      }
+    }
+  }
+
+  private int getMaxPositionInCars(int max) {
+    for(Car car : cars) {
+      if(max < car.getPosition())
+        max = car.getPosition();
+    }
+    return max;
+  }
+}
