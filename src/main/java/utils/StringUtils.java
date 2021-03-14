d09d21fcc660da9540adccf7a15a704b1b0ae5e1
김우재 <kwj1270@naver.com>
2021-03-14 18:02:26 +0900

    test:입력된 값을 ,를 기준으로 분리하는지 검증하는 테스트 작성

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -2,6 +2,7 @@ package utils;
 
 public class StringUtils {
     private static final int ZERO = 0;
+    private static final String COMMA = ",";
 
     private static class SettingsHolder {
         public static final StringUtils utils = new StringUtils();
@@ -14,19 +15,20 @@ public class StringUtils {
         return SettingsHolder.utils;
     }
 
-    public static boolean isNullAndBlank(String expression) {
-        if (isNull(expression)) return true;
-        if (isBlank(expression)) return true;
+    public static boolean isNullAndBlank(String sentence) {
+        if (isNull(sentence)) return true;
+        if (isBlank(sentence)) return true;
         return false;
     }
 
-    private static boolean isNull(String expression) {
-        return expression == null;
+    private static boolean isNull(String sentence) {
+        return sentence == null;
     }
 
-    private static boolean isBlank(String expression) {
-        return expression.trim().length() == ZERO;
+    private static boolean isBlank(String sentence) {
+        return sentence.trim().length() == ZERO;
     }
+
 
 
 }
