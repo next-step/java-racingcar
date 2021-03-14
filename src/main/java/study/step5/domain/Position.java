5bfd7ea3fc5d1451faf5a5aab39130506ad46692
Donghui-Lee <ehdgml3206@naver.com>
2021-03-15 02:21:12 +0900

    refactor: Position 원시값 포장, 실패하는 테스트 추가

---
+++ src/main/java/study/step5/domain/Position.java
@@ -0,0 +1,10 @@
+package study.step5.domain;
+
+public class Position {
+	public Position(int i) {
+	}
+
+	public int getPosition() {
+		return 0;
+	}
+}
---
+++ src/test/java/study/step5/PositionTest.java
@@ -0,0 +1,18 @@
+package study.step5;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import study.step5.domain.Position;
+
+public class PositionTest {
+
+    @Test
+    @DisplayName("Position 클래스 생성 테스트")
+    void create() {
+        Position position = new Position(3);
+        assertThat(position.getPosition()).isEqualTo(3);
+    }
+}
