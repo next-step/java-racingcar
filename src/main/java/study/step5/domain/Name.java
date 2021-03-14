fc599050db696a6c1675b33249768f2bf2838524
Donghui-Lee <ehdgml3206@naver.com>
2021-03-15 03:18:38 +0900

    refactor: Name 리펙터링 및 유효성 테스트 추가

--- src/main/java/study/step5/domain/Name.java
+++ src/main/java/study/step5/domain/Name.java
@@ -6,7 +6,22 @@ public class Name {
     private String name;
 
     public Name(String name) {
+        this.validEmpty(name);
+        this.validLength(name);
+
         this.name = name;
+    }
+
+    private void validEmpty(String name) {
+        if(name == "" || name == null) {
+            throw new IllegalArgumentException("공백으로 자동차 이름을 등록할 수 없습니다.");
+        }
+    }
+
+    private void validLength(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 
     @Override
