65e74f378dc7a8a822ef1ab345736c7333af42c6
dailyworker <workingssu@gmail.com>
2021-03-14 21:01:02 +0900

    test : UserInputValidatorTest 추가 및 클래스 변경으로 인한 테스트코드 변경 CarsTest - 생성하는 부분 Name을 전달해서 생성하는 테스트로 변경 - 우승자 이름을 출력하는 로직 테스트 추가
    Car
    - 생성하는 부분 Name 전달해서 생성하는 테스트로 변경
    
    UserInputValidatorTest
    - 발생 가능한 모든 익셉션에 대한 테스트 수행
    
    refactor : 다른 클래스들의 변경 점에 따른 리팩토링 수행
    InputView - getNumberOfCars() -> getNameOfCars()로 메소드 명 변경
    
    test : UserInputValidatorTest 추가 및 클래스 변경으로 인한 테스트코드 변경 CarsTest - 생성하는 부분 Name을 전달해서 생성하는 테스트로 변경 - 우승자 이름을 출력하는 로직 테스트 추가

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,43 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.util.TestRandomNumberGenerator;
+
+class CarTest {
+
+  private final TestRandomNumberGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(System.currentTimeMillis());
+
+  @ParameterizedTest
+  @ValueSource(strings = {"aaa","bbb"})
+  @DisplayName("static 메소드를 통해서 Car를 생성할 수 있는가")
+  public void createCarUsingStaticMethod(String name) {
+    //given
+    Car expectedCar = Car.createCar(name);
+
+    //when
+    Car actualCar = new Car(name);
+
+    //then
+    assertEquals(expectedCar.getName(), actualCar.getName());
+  }
+
+  @Test
+  @DisplayName("moveCar() 메소드를 통해서 현재 위치 상태를 변경할 수 있는가")
+  public void changePositionUsingMethod() {
+    //given
+    Car car = Car.createCar("aaa");
+    int initPosition = car.getPosition();
+
+    //when
+    car.moveCar(testRandomNumberGenerator.generateRandomNumber());
+    int movedPosition = car.getPosition();
+
+    //then
+    assertEquals(initPosition+1, movedPosition);
+  }
+}
---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,112 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.util.TestRandomNumberGenerator;
+
+class CarsTest {
+
+  private Cars cars;
+  private final TestRandomNumberGenerator testRandomNumberGenerator = new TestRandomNumberGenerator(System.currentTimeMillis());
+
+  @BeforeEach
+  public void createCars() {
+    List<Car> carList = new ArrayList<>();
+
+    Car car1 = Car.createCar("aaa");
+    Car car2 = Car.createCar("bbb");
+    Car car3 = Car.createCar("ccc");
+
+    carList.add(car1);
+    carList.add(car2);
+    carList.add(car3);
+
+    cars = new Cars(carList);
+  }
+
+  @Test
+  @DisplayName("여러개의 Car로 Cars를 생성할 수 있으며, 생성된 Cars 내부의 Car가 생성자로 넘긴 Car와 동일한가")
+  public void createCarsByManyCar() {
+    //given
+    List<Car> carList = new ArrayList<>();
+
+    Car actualCar1 = Car.createCar("aaa");
+    Car actualCar2 = Car.createCar("bbb");
+    Car actualCar3 = Car.createCar("ccc");
+
+    carList.add(actualCar1);
+    carList.add(actualCar2);
+    carList.add(actualCar3);
+
+    //when
+    Cars cars = new Cars(carList);
+
+    //then
+    List<Car> expectedCars = cars.getCars();
+    assertAll(
+        () -> assertEquals(expectedCars.get(0), actualCar1),
+        () -> assertEquals(expectedCars.get(1), actualCar2),
+        () -> assertEquals(expectedCars.get(2), actualCar3)
+    );
+  }
+
+  @Test
+  @DisplayName("현재 자동차들의 전체 위치를 출력할 수 있는가")
+  public void printAllCarsPosition() {
+
+    //when
+    String expectString = cars.currentCarsPosition();
+
+    //then
+    String actualString = "aaa : " + "-" + "\n" + "bbb : " + "-" + "\n" + "ccc : " +  "-" + "\n";
+
+    assertEquals(expectString, actualString);
+  }
+
+  @Test
+  @DisplayName("현재 자동차들의 전체 위치를 변경할 수 있는가")
+  public void updateAllCarsPosition() {
+
+    //when
+    cars.updateAll(testRandomNumberGenerator);
+
+    //then
+    List<Car> expectedCars = cars.getCars();
+    assertAll(
+        () -> assertEquals(expectedCars.get(0).getPosition(), 1),
+        () -> assertEquals(expectedCars.get(1).getPosition(), 1),
+        () -> assertEquals(expectedCars.get(2).getPosition(), 1)
+    );
+  }
+
+  @Test
+  @DisplayName("제일 높은 position을 통해서 우승자의 이름을 뽑아낼 수 있는가")
+  public void getWinnerNameByPosition() throws Exception {
+    //given
+    cars.updateAll(testRandomNumberGenerator);
+
+    //when
+    List<String> expectedWinners = cars.getWinner();
+    List<String> actualWinners = new ArrayList<>();
+    actualWinners.add("aaa");
+    actualWinners.add("bbb");
+    actualWinners.add("ccc");
+
+    //then
+    List<Car> expectedCars = cars.getCars();
+    assertAll(
+        () -> assertEquals(expectedWinners, actualWinners),
+        () -> assertEquals(expectedCars.get(0).getPosition(), cars.getCars().get(0).getPosition()),
+        () -> assertEquals(expectedCars.get(1).getPosition(), cars.getCars().get(1).getPosition()),
+        () -> assertEquals(expectedCars.get(2).getPosition(), cars.getCars().get(2).getPosition())
+    );
+  }
+
+}
