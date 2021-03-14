6a318cc9a29ca5f65e210e86f89afc65df2fc6d3
Donghui-Lee <ehdgml3206@naver.com>
2021-03-15 02:39:31 +0900

    refactor: Position 객체 유효성, move 메서드 추가

--- src/main/java/study/step5/domain/Position.java
+++ src/main/java/study/step5/domain/Position.java
@@ -7,7 +7,14 @@ public class Position {
     private int position;
 
     public Position(int position) {
+        if(position < 0) {
+            throw new IllegalArgumentException("음수 값을 가질 수 없습니다.");
+        }
         this.position = position;
+    }
+
+    public Position move() {
+        return new Position(position + 1);
     }
 
     @Override
--- src/test/java/study/step5/PositionTest.java
+++ src/test/java/study/step5/PositionTest.java
@@ -15,4 +15,17 @@ public class PositionTest {
         Position position = new Position(3);
         assertThat(position).isEqualTo(new Position(3));
     }
+
+    @Test
+    @DisplayName("유효성 테스트")
+    void valid() {
+        assertThatThrownBy(() -> new Position(-1)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("움직임 테스트")
+    void move() {
+        Position position = new Position(3);
+        assertThat(position.move()).isEqualTo(new Position(4));
+    }
 }
