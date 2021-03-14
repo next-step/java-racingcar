b56269de74a2bfc425776caa4a3daa06150c2527
keke0123 <keke0123@naver.com>
2021-03-14 12:18:53 +0900

    main 로직 제작

--- src/main/java/study1/racing/Main.java
+++ src/main/java/study1/racing/Main.java
@@ -1,25 +1,13 @@
 package study1.racing;
 
 public class Main {
-  /*
-    설계
-      - Main.java
-        - 로직 실행
-      - Race.java
-        - field
-          - participant / 자동차대수
-          - round / 몇회
-        - method
-          - run / 레이스 시작
-          - checkDistance / 차 거리 측정
-          - print / 거리 print
-      - Car.java
-        - field 
-          - distance / 주행거리
-        - method
-          - move / 랜덤 이동
-  */
+
   public static void main(String[] args) {
-    
+    InputView inputView = new InputView();
+    int participant = inputView.getParticipants();
+    int round = inputView.getRound();
+
+    Race race = new Race(participant, round);
+    race.run();
   }
 }
