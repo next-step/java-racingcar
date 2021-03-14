a7025b1a5297c109d59883f7f01c0bb403e17436
Beenie93 <seongbeen93@naver.com>
2021-03-15 02:36:48 +0900

    refactor: replace a logic that creates cars by number with a logic by name

--- src/test/java/racingcar/controller/RacingGameControllerTest.java
+++ src/test/java/racingcar/controller/RacingGameControllerTest.java
@@ -31,16 +31,16 @@ class RacingGameControllerTest {
         this.racingGameController = new RacingGameController(this.carService, this.gameRoundService);
     }
 
-    @ParameterizedTest(name = "입력한 대수가 {0}일 경우")
-    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
-    @DisplayName("입력받은 대수만큼 자동차 생성하는 테스트")
-    public void createCars(int numberOfCars) throws Exception {
+    @Test
+    @DisplayName("자동차 생성하는 테스트")
+    public void createCars() throws Exception {
         //when
-        //racingGameController.createCars(numberOfCars);
+        String[] names = {"one", "two", "three"};
+        racingGameController.createCars(names);
         int sizeOfCars = carRepository.findAll().size();
 
         //then
-        assertEquals(numberOfCars, sizeOfCars, "입력한 대수와 생성된 자동차의 수는 같아야한다.");
+        assertEquals(names.length, sizeOfCars, "입력한 대수와 생성된 자동차의 수는 같아야한다.");
     }
 
     @Test
