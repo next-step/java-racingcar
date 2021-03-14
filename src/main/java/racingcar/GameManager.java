d64e4a981feaf31fc86bcb7795ac544c387abc89
suhyunsim <cnc1106@naver.com>
2021-03-15 03:15:25 +0900

    feat: 전체 경기를 작동하는 메인 클래스 구현

---
+++ src/main/java/racingcar/GameManager.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import racingcar.domain.RacingGame;
+import racingcar.dto.Round;
+import racingcar.strategy.RandomMoveStrategy;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
+import java.util.List;
+
+public class GameManager {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        int carCount = inputView.inputCarCount();
+        int roundCount = inputView.inputRoundCount();
+
+        RacingGame game = new RacingGame(carCount, roundCount);
+        List<Round> rounds = game.runGame(new RandomMoveStrategy());
+        ResultView resultView = new ResultView(rounds);
+        resultView.printResult();
+    }
+}
