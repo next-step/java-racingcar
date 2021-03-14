7f8d4937940b93d983dda6b7fe77769f893d59f0
ad7549 <ad7549@gmail.com>
2021-03-14 11:14:24 +0900

    feat: Add feature for name to Car

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -6,11 +6,28 @@ public class Car implements Cloneable {
     public static int MIN_BOUNDARY_VALUE = 0;
     public static int MAX_BOUNDARY_VALUE = 9;
     public static int MIN_MOVABLE_VALUE = 4;
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private static final String DEFAULT_NAME = "empty";
+
+    private final String name;
 
     private int distance;
 
+    public Car(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.distance = 0;
+    }
+
     public Car() {
-        distance = 0;
+        this(DEFAULT_NAME);
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getDistance() {
