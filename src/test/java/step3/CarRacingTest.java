796533ad1517f0b539ef90b7ace95edcc8f5a868
jaystar13 <astarisk2@gmail.com>
2021-03-14 22:30:14 +0900

    test: 자동차 이름 부여 기능 및 validation 체크 기능 테스트

--- src/test/java/step3/CarRacingTest.java
+++ src/test/java/step3/CarRacingTest.java
@@ -10,10 +10,10 @@ public class CarRacingTest {
     @DisplayName("입력받은 숫자만큼의 자동차를 생성한다.")
     @Test
     public void makeRacingCar_ShouldReturnCreatedCarsCount() {
-        int inputCarsCount = 5;
+        String[] inputCarsName = {"test1", "test2", "test3", "test4", "test5"};
         int expectedCarsCount = 5;
 
-        CarRacing carRacing = new CarRacing(inputCarsCount);
+        CarRacing carRacing = new CarRacing(inputCarsName);
 
         assertThat(carRacing.getCarList().size()).isEqualTo(expectedCarsCount);
     }
@@ -21,11 +21,11 @@ public class CarRacingTest {
     @DisplayName("생성된 자동차는 전진하거나 그자리에 머무른다.")
     @Test
     public void moveForward_ShouldReturnGoOrStop() {
-        int inputCarsCount = 5;
+        String[] inputCarsName = {"test1", "test2", "test3", "test4", "test5"};
         int go = 1;
         int stop = 0;
 
-        CarRacing carRacing = new CarRacing(inputCarsCount);
+        CarRacing carRacing = new CarRacing(inputCarsName);
         carRacing.moveForward();
 
         assertThat(carRacing.getCarList()).allSatisfy(car -> {
