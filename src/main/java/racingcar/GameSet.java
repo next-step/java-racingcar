05474fcac27bf6df4dbdcc98ec1f4306f2680098
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-14 23:27:19 +0900

    feat : 기존에 Car 클래스 인스턴스 변수를 분리후 별도의 클래스로 생성

--- src/main/java/racingcar/GameSet.java
+++ src/main/java/racingcar/GameSet.java
@@ -1,2 +1,20 @@
-package racingcar;public class GameSet {
+package racingcar;
+
+public class GameSet {
+
+    private int cars;
+    private int givenRound;
+
+    public GameSet(int cars, int givenRound) {
+        this.cars = cars;
+        this.givenRound = givenRound;
+    }
+
+    public int getCars() {
+        return cars;
+    }
+
+    public int getGivenRound() {
+        return givenRound;
+    }
 }
