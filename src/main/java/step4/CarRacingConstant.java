06f054a7e3fca7e292ae3910d91b2f41f1ac6543
Sooncheol <ryxhdrnrmf@gmail.com>
2021-03-14 18:30:22 +0900

    feat: CarRacingConstant 구현

---
+++ src/main/java/step4/CarRacingConstant.java
@@ -0,0 +1,11 @@
+package step4;
+
+public class CarRacingConstant {
+    static final String NAMES_OF_CARS = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    static final String NUMBER_OF_ROUNDS = "시도할 회수는 몇회인가요?";
+    static final String EXECUTION_RESULTS = "실행 결과";
+    static final String DASH = "-";
+    static final String WIN = "가 최종 우승했습니다.";
+    static final String COMMA = ",";
+    static final int LIMIT_OF_NAME_LENGTH = 5;
+}
