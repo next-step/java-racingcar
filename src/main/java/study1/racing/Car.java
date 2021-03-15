c4113bf7ad010d9fed02a90058d54a8a45be0ab9
keke0123 <keke0123@naver.com>
2021-03-15 13:46:17 +0900

    distance 측정하기 위해서 getDistance 추가

--- src/main/java/study1/racing/Car.java
+++ src/main/java/study1/racing/Car.java
@@ -5,6 +5,10 @@ import java.util.Random;
 public class Car {
   private int distance = 0;
 
+  public int getDistance() {
+    return distance;
+  }
+
   public void setDistance(int movedDistance) {
     this.distance += movedDistance;
   }
@@ -14,7 +18,7 @@ public class Car {
 
   public int move() {
     setDistance(movedDistance(1));
-    return distance;
+    return getDistance();
   }
 
   // 이동한 거리 / min: 0, max: maxDistance
