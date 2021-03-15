01c7059c33c5135771174d4d208b36dca8109c15
cyr9210 <jipsyer@naver.com>
2021-03-15 17:35:09 +0900

    test 전략테스트 @DisplayName 추가

--- src/test/java/study/racing/NumberConditionMoveStrategyTest.java
+++ src/test/java/study/racing/NumberConditionMoveStrategyTest.java
@@ -2,6 +2,7 @@ package study.racing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -10,6 +11,7 @@ public class NumberConditionMoveStrategyTest {
 
   @ParameterizedTest
   @CsvSource(value = {"1,false", "2,false", "4,true", "5,true"})
+  @DisplayName("전진전략 테스트, 4이상 일시에만 true")
   void isMove(int number, boolean result) {
     NumberConditionMoveStrategy strategy = new NumberConditionMoveStrategy();
     boolean move = strategy.isMove(number);
