aa5f2a71c693ad2858ce4d6711117c4c21d166b7
keke0123 <keke0123@naver.com>
2021-03-15 19:10:09 +0900

    race test 제작

--- src/test/java/study1/racing/RacingTest.java
+++ src/test/java/study1/racing/RacingTest.java
@@ -2,9 +2,13 @@ package study1.racing;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import study1.racing.moveStrategy.TestMove;
 
@@ -26,5 +30,17 @@ public class RacingTest {
     car.setMoveStrategy(new TestMove());
     int moved = car.move();
     assertThat(moved).isEqualTo(1);
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {"5:3"}, delimiter = ':')
+  @DisplayName("round 와 participant 가 지정되었을때 반환되는 RaceRound size 와 RaceRound 안에 car 사이즈 체크")
+  public void checkStart(int participant, int round) {
+    Race race = new Race(participant, round);
+    List<RaceRound> rounds = race.start();
+    assertThat(rounds.size()).isEqualTo(round);
+    for(RaceRound raceRound : rounds) {
+      assertThat(raceRound.getResult().size()).isEqualTo(participant);
+    } 
   }
 }
