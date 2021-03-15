c77221cb65f94c22bc7a4055a2e1e7418ee01a1b
keke0123 <keke0123@naver.com>
2021-03-15 13:30:51 +0900

    테스트 가능하도록 class 다시 세팅 move 는 운영 moveDistance 는 테스트 setDistance 는 테스트

--- src/main/java/study1/racing/Car.java
+++ src/main/java/study1/racing/Car.java
@@ -3,13 +3,22 @@ package study1.racing;
 import java.util.Random;
 
 public class Car {
-  private int distance = 1;
+  private int distance = 0;
+
+  public void setDistance(int movedDistance) {
+    this.distance += movedDistance;
+  }
 
   public Car() {
   }
 
   public int move() {
-    distance += new Random().nextInt(2);
+    setDistance(movedDistance(1));
     return distance;
+  }
+
+  // 이동한 거리 / min: 0, max: maxDistance
+  public int movedDistance(int maxDistance) {
+    return new Random().nextInt(maxDistance + 1);
   }
 }
