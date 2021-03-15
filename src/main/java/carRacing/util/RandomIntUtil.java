5c369aa01f9c7c2cdd54c3ba330980a0410a18ce
JeongByeongHun <jbh1024@gmail.com>
2021-03-16 00:24:08 +0900

    feat (feature) : create RandomIntUtil.

--- src/main/java/carRacing/util/RandomIntUtil.java
+++ src/main/java/carRacing/util/RandomIntUtil.java
@@ -1,4 +1,10 @@
 package carRacing.util;
 
+import java.util.Random;
+
 public class RandomIntUtil {
+    public static int getRandomInt(){
+        Random random = new Random();
+        return random.nextInt(10);
+    }
 }
