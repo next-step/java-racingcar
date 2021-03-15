38216de23ff09c451578fa8818b3459cbc6e939e
HeeSeok Noh <heeseoknoh@HeeSeoks-MacBook-Pro.local>
2021-03-15 12:53:01 +0900

    feat: isKeepGoung,MoveCar method 작성

--- src/main/java/ch01/step2/RacingGame.java
+++ src/main/java/ch01/step2/RacingGame.java
@@ -4,9 +4,37 @@ import java.util.Random;
 
 public class RacingGame {
 
+    int attemptCount = 0;
+    int carCount = 0;
+    String[] progressArr;
     void execute(){
         InputView iv = new InputView();
+        this.attemptCount = iv.getAttemptCount();
+        this.carCount = iv.getCarCount();
+        this.progressArr = iv.getProgressArr();
+        while(attemptCount-- > 0){
+            racing();
+        }
+    }
 
+    public void racing(){
+        int randomNumber = 0;
+        int progressArrSize = progressArr.length;
+        for(int i=0; i< progressArrSize; i++){
+            randomNumber = getRandomNumber();
+
+
+            if(isKeepGoing(randomNumber)){
+                progressArr[i] += "-";
+            }
+        }
+    }
+
+    public void moveCar(int index){
+        progressArr[index] +="-";
+    }
+    public boolean isKeepGoing(int randomNumber){
+        return randomNumber > 3;
     }
 
     public int getRandomNumber(){
--- src/test/java/test/ch01/step2/RacingGameTest.java
+++ src/test/java/test/ch01/step2/RacingGameTest.java
@@ -5,26 +5,32 @@ import ch01.step2.RacingGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashSet;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RacingGameTest {
 
     RacingGame racingGame;
-    
+
     @BeforeEach
     void setUp() {
         racingGame = new RacingGame();
     }
-    
+
+    @ParameterizedTest
+    @DisplayName("숫자 입력받아서 전진할것인지 결정하는 method")
+    @CsvSource(value = {"1:false", "2:false", "3:false","4:true","5:true"}, delimiter = ':')
+    void isKeepGoingTest(int inputNum,boolean expect){
+        racingGame.isKeepGoing(inputNum);
+        assertThat(racingGame.isKeepGoing(inputNum)).isEqualTo(expect);
+    }
+
     @Test
     @DisplayName("Random 숫자 나오는지 확인")
     void getRandomNumberTest(){
         int randomNumber = racingGame.getRandomNumber();
-//        assertThat(randomNumber).has(0,1,2,3,4,5,6,7,8,9);
         assertThat(randomNumber).isBetween(0,9);
     }
     
