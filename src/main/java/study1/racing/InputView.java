b725a98d9570bea61889d5b845da4dfee5c7b029
keke0123 <keke0123@naver.com>
2021-03-15 18:02:22 +0900

    코딩컨벤션 준수

--- src/main/java/study1/racing/InputView.java
+++ src/main/java/study1/racing/InputView.java
@@ -3,18 +3,23 @@ package study1.racing;
 import java.util.Scanner;
 
 public class InputView {
+
   private final Scanner scanner;
+
   public InputView() {
     this.scanner = new Scanner(System.in);
   }
-  int getParticipants() {
+
+  public int getParticipants() {
     System.out.println("자동차 대수는 몇 대 인가요?");
     int participant = Integer.parseInt(scanner.nextLine());
     return participant;
   }
-  int getRound() {
+
+  public int getRound() {
     System.out.println("시도할 회수는 몇 회 인가요?");
     int round = Integer.parseInt(scanner.nextLine());
     return round;
   }
+
 }
