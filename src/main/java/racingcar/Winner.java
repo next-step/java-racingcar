994990186d35f84081a72d987b3687823b23a0d5
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-16 02:19:59 +0900

    우승자에 대한 정보를 저장하는 클래스 추가

--- src/main/java/racingcar/Winner.java
+++ src/main/java/racingcar/Winner.java
@@ -1,2 +1,14 @@
-package racingcar;public class Winner {
+package racingcar;
+
+public class Winner {
+
+    private String winner;
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
 }
