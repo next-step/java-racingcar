f5ec98e99893414b16488a0e6106eaf2a59a05a4
JeongByeongHun <jbh1024@gmail.com>
2021-03-16 00:22:37 +0900

    feat (feature) : create Car Class, move method. override toString

--- src/main/java/carRacing/model/Car.java
+++ src/main/java/carRacing/model/Car.java
@@ -1,4 +1,22 @@
 package carRacing.model;
 
 public class Car {
+    private int position;
+
+    public Car(){
+        this.position = 0;
+    }
+
+    public void move(){
+        this.position += 1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < position; i++) {
+            result.append("-");
+        }
+        return result.toString();
+    }
 }
