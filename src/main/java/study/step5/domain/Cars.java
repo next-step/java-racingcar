cf26641855adfab954fa11c9be5ddc209d4ada10
Donghui-Lee <ehdgml3206@naver.com>
2021-03-15 11:54:32 +0900

    refactor: Cars 일급 컬렉션 포장, 실패 테스트 추가

---
+++ src/main/java/study/step5/domain/Cars.java
@@ -0,0 +1,15 @@
+package study.step5.domain;
+
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Cars> findWinners() {
+        return null;
+    }
+}
---
+++ src/test/java/study/step5/CarsTest.java
@@ -0,0 +1,26 @@
+package study.step5;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import study.step5.domain.Car;
+import study.step5.domain.Cars;
+
+public class CarsTest {
+    @Test
+    @DisplayName("우승자 테스트")
+    void findWinners() {
+        Car test1 = new Car("test1", 3);
+        Car test2 = new Car("test2", 3);
+        Car test3 = new Car("test3", 2);
+
+        List<Car> original = Arrays.asList(test1, test2, test3);
+        Cars cars = new Cars(original);
+        assertThat(cars.findWinners()).containsExactly((Cars)Arrays.asList(test1, test2));
+    }
+}
