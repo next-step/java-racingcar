cca6a81f2503120a0517de67c98ca5be39572644
shkwon <oper912@naver.com>
2021-03-15 21:21:05 +0900

    refactor: 컨트롤러 통해서 게임 흐름 진행되도록 구조 변경

--- src/main/java/step3/RacingCarApplication.java
+++ src/main/java/step3/RacingCarApplication.java
@@ -1,15 +1,11 @@
 package step3;
 
 import step3.controller.GameController;
-import step3.view.InputView;
 
 public class RacingCarApplication {
 
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        int countOfCars = inputView.getCountOfCars();
-        int countOfTry = inputView.getCountOfTry();
-
         GameController controller = GameController.newInstance();
+        controller.run();
     }
 }
--- src/main/java/step3/controller/GameController.java
+++ src/main/java/step3/controller/GameController.java
@@ -2,12 +2,13 @@ package step3.controller;
 
 import step3.domain.Car;
 import step3.service.GameService;
+import step3.view.InputView;
+import step3.view.ResultView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
-
     private GameService gameService;
     private List<Car> cars;
 
@@ -18,5 +19,22 @@ public class GameController {
 
     public static GameController newInstance() {
         return new GameController();
+    }
+
+    public void run() {
+        makeCars();
+        moveCars();
+    }
+
+    private void makeCars() {
+        int countOfCars = InputView.getCountOfCars();
+        this.cars = gameService.makeCars(countOfCars);
+    }
+
+    private void moveCars() {
+        int countOfTry = InputView.getCountOfTry();
+        for (int i = 0; i < countOfTry; i++) {
+            this.cars = gameService.moveCars(cars);
+        }
     }
 }
