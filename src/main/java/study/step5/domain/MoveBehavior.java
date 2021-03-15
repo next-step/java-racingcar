77842de91bddad545405eda2cdf590529e831cdb
hwangyunho <hwang9yh@gmail.com>
2021-03-16 00:38:45 +0900

    feat: Move 행동전략 추가

---
+++ src/main/java/study/step5/domain/MoveBehavior.java
@@ -0,0 +1,6 @@
+package study.step5.domain;
+
+public interface MoveBehavior {
+
+    boolean isMoved();
+}
---
+++ src/main/java/study/step5/domain/OneLocationMoveBehavior.java
@@ -0,0 +1,9 @@
+package study.step5.domain;
+
+public class OneLocationMoveBehavior implements MoveBehavior {
+
+    @Override
+    public boolean isMoved() {
+        return true;
+    }
+}
