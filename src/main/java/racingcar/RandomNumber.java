652733767eca479f50fbb8b0ee956fade77d88c4
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-16 02:19:39 +0900

    feat : 랜덤값을 추출하고 기준이 되는 값을 따로 분리하는 클래스 추가

--- src/main/java/racingcar/RandomNumber.java
+++ src/main/java/racingcar/RandomNumber.java
@@ -1,2 +1,14 @@
-package racingcar;public class RandomNumber {
+package racingcar;
+
+import java.util.Random;
+
+public class RandomNumber {
+
+    // TODO 이 둘은 난수 관련 클래스로 따로 만들어서 분리한다.
+    static final int MINIMUM_NUMBER = 4;
+    private static Random random = new Random();
+
+    public static int random() {
+        return random.nextInt(10);
+    }
 }
