35b6ba3f9930da702cde66546f288a8edf2807f7
kor-Chipmunk <rhj4862@gmail.com>
2021-03-16 00:49:55 +0900

    :white_check_mark: 어떻게든 동작하는 코드를 만듭니다.

--- src/main/java/study/calculator/Calculator.java
+++ src/main/java/study/calculator/Calculator.java
@@ -2,19 +2,19 @@ package study.calculator;
 
 public class Calculator {
     public int add(int leftOperand, int rightOperand) {
-        return 15;
+        return leftOperand + rightOperand;
     }
 
     public int subtract(int leftOperand, int rightOperand) {
-        return 5;
+        return leftOperand - rightOperand;
     }
 
     public int multiply(int leftOperand, int rightOperand) {
-        return 50;
+        return leftOperand * rightOperand;
     }
 
     public int divide(int leftOperand, int rightOperand) {
-        return 2;
+        return leftOperand / rightOperand;
     }
 
     public int calculate(String expressionString) {
@@ -32,6 +32,95 @@ public class Calculator {
             throw new IllegalArgumentException();
         }
 
-        return 10;
+        boolean isNextLeftOperand = true;
+        boolean isNextOperator = false;
+        boolean isNextRightOperand = false;
+
+        int leftOperand = 0;
+        int rightOperand = 0;
+        String operator = "";
+
+        int result = 0;
+
+        String[] splitExpressions = expressionString.split(" ");
+
+        for (int i = 0; i < splitExpressions.length; ++i) {
+            String token = splitExpressions[i];
+
+            if (isNextOperator == false) {
+                if (Character.isDigit(token.charAt(0)) == false) {
+                    throw new IllegalArgumentException();
+                }
+
+            } else {
+                if (Character.isDigit(token.charAt(0)) == true) {
+                    throw new IllegalArgumentException();
+                }
+
+                if (token.equals("+")) {
+
+                }
+                else if (token.equals("-")) {
+
+                }
+                else if (token.equals("*")) {
+
+                }
+                else if (token.equals("/")) {
+
+                } else {
+                    throw new IllegalArgumentException();
+                }
+
+                operator = token;
+
+                isNextLeftOperand = false;
+                isNextOperator = false;
+                isNextRightOperand = true;
+                continue;
+            }
+
+            if (isNextLeftOperand == true) {
+                leftOperand = Integer.parseInt(token);
+
+                isNextLeftOperand = false;
+                isNextOperator = true;
+                isNextRightOperand = false;
+                continue;
+            }
+
+            if (isNextRightOperand == true) {
+                rightOperand = Integer.parseInt(token);
+
+                if (operator.equals("+")) {
+                    result = this.add(leftOperand, rightOperand);
+                }
+
+                if (operator.equals("-")) {
+                    result = this.subtract(leftOperand, rightOperand);
+                }
+
+                if (operator.equals("*")) {
+                    result = this.multiply(leftOperand, rightOperand);
+                }
+
+                if (operator.equals("/")) {
+                    result = this.divide(leftOperand, rightOperand);
+                }
+
+                isNextLeftOperand = false;
+                isNextOperator = true;
+                isNextRightOperand = false;
+
+                leftOperand = result;
+                operator = "";
+                rightOperand = 0;
+
+                continue;
+            }
+
+        }
+
+        return result;
     }
 }
