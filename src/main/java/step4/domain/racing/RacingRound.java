00a28005988435711dd92f7e32bd1a03817daf3f
김우재 <kwj1270@naver.com>
2021-03-15 20:14:54 +0900

    feat: 레이싱 라운드를 세기 위한 RacingRound 클래스 생성

---
+++ src/main/java/step4/domain/racing/RacingRound.java
@@ -0,0 +1,27 @@
+package step4.domain.racing;
+
+public class RacingRound {
+
+    private static final int START_VALUE = 0;
+
+    private int now;
+    private int finish;
+
+    public static RacingRound from(int finish) {
+        return new RacingRound(finish);
+    }
+
+    private RacingRound(int finish) {
+        this.finish = finish;
+        this.now = START_VALUE;
+    }
+
+    public boolean hasNext() {
+        return now < finish;
+    }
+
+    public void nextRound() {
+        now++;
+    }
+
+}
