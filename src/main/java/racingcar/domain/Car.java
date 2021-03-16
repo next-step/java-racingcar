4fdb0c065fc7f7b2275373d375d072c967aaeefe
lisaimeee <lisaimeee@gmail.com>
2021-03-17 01:40:26 +0900

    refactor(Car): 자동차 이름 밸리데이션 로직 Car 로 이동 - CarValidator 삭제

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,18 +1,26 @@
 package racingcar.domain;
 
 public class Car {
-    public final static int INITIAL_POSITION = 1;
+    private static final int NAME_MAX_LENGTH = 5;
+    public static final int INITIAL_POSITION = 1;
+
     private int position = INITIAL_POSITION;
     private String name;
 
     public Car(String name) {
-        CarValidator.verifyNameLength(name);
+        verifyNameLength(name);
         this.name = name;
     }
 
     public void moveOrStay(int value) {
         if (MovingForwardCondition.isSatisfied(value)) {
             move();
+        }
+    }
+
+    private void verifyNameLength(String name) {
+        if (name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(String.format("자동차 이름은 %d자를 초과할 수 없습니다.", NAME_MAX_LENGTH));
         }
     }
 
