6b379f9ab4c27f432687115c134cebc1f7af3fbe
sonAjin <sonajin11@gmail.com>
2021-03-17 00:42:29 +0900

    (refactor) ResultView static으로 변경

--- src/main/java/step03/ResultView.java
+++ src/main/java/step03/ResultView.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 public class ResultView {
 
-    private void showRacingResult(int moveRange) {
+    private ResultView() {}
+
+    private static void showRacingResult(int moveRange) {
         for (int i = 0; i < moveRange; i++) {
             System.out.print("-");
-            System.out.println();
         }
     }
 
-    public void showRacingResult(List<RacingCar> cars) {
+    public static void showRacingResult(List<RacingCar> cars) {
         for (RacingCar car : cars) {
             showRacingResult(car.getMovingRange());
+            System.out.println();
         }
     }
 
