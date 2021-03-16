061ca9a6807a53391097da626e927cca139b5266
sonAjin <sonajin11@gmail.com>
2021-03-17 01:27:41 +0900

    (refactor) Mock을 이용하여 가상객체 만들어 move 검증

--- src/test/java/step03/RacingCarTest.java
+++ src/test/java/step03/RacingCarTest.java
@@ -1,27 +1,42 @@
 package step03;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
 
 public class RacingCarTest {
 
-    RacingCar racingCar = new RacingCar();
+    @Mock
+    private MoveStrategy moveStrategy;
+    private RacingCar racingCar;
 
-    @ParameterizedTest
-    @DisplayName("random 값이 3 이하일때 값이 정상적으로 동작하는지 확인")
-    @ValueSource(ints = {1, 2, 3})
-    void tryToMoveZero(int randomRange) {
-        racingCar.tryToMove(randomRange);
-        assertThat(racingCar.getMovingRange()).isEqualTo(0);
+    @BeforeEach
+    void setRacingCar() {
+        MockitoAnnotations.initMocks(this);
+        racingCar = new RacingCar(moveStrategy);
     }
 
     @ParameterizedTest
-    @DisplayName("random 값이 4 이상일때 값이 정상적으로 동작하는지 확인")
-    @ValueSource(ints = {4, 5, 6})
-    void tryToMove(int randomRange) {
-        assertThat(racingCar.tryToMove(randomRange)).isEqualTo(1);
+    @DisplayName("무조건 전진하는 차로 tryToMove가 제대로 동작하는지 테스트")
+    @ValueSource(ints = {1, 2, 3})
+    void moveTest(int tryCount) {
+        //given
+        given(moveStrategy.move()).willReturn(true);
+
+        //when
+        for (int i = 0; i < tryCount; i++) {
+            racingCar.tryToMove();
+        }
+
+        //then
+        assertThat(racingCar.getMovingRange()).isEqualTo(tryCount);
+
     }
 }
