8560564578c361a46ad4aa023e4354966a186b6a
Beenie93 <seongbeen93@naver.com>
2021-03-17 04:58:20 +0900

    refactor: update test cases with newest version

--- src/test/java/racingcar/controller/RacingGameControllerTest.java
+++ src/test/java/racingcar/controller/RacingGameControllerTest.java
@@ -5,17 +5,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.Car;
 import racingcar.repository.CarRepository;
 import racingcar.repository.GameRoundRepository;
 import racingcar.service.CarService;
 import racingcar.service.GameRoundService;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RacingGameControllerTest {
 
+    private List<Car> cars;
     private CarRepository carRepository;
     private CarService carService;
     private GameRoundRepository gameRoundRepository;
@@ -24,7 +28,8 @@ class RacingGameControllerTest {
 
     @BeforeEach
     public void setUp() {
-        this.carRepository = new CarRepository();
+        this.cars = new ArrayList<>();
+        this.carRepository = new CarRepository(cars);
         this.carService = new CarService(this.carRepository);
         this.gameRoundRepository = new GameRoundRepository();
         this.gameRoundService = new GameRoundService(this.gameRoundRepository);
@@ -35,17 +40,18 @@ class RacingGameControllerTest {
     @DisplayName("자동차 생성하는 테스트")
     public void createCars() throws Exception {
         //when
-        String[] names = {"one", "two", "three"};
+        String[] names = {"sean", "pobi", "bibi"};
         racingGameController.createCars(names);
-        int sizeOfCars = carRepository.findAll().size();
+        List<Car> cars = carRepository.findAll();
 
         //then
-        assertEquals(names.length, sizeOfCars, "입력한 대수와 생성된 자동차의 수는 같아야한다.");
+        assertThat(cars).hasSize(3);
     }
 
     @Test
     @DisplayName("입력받은 시도 회수가 정수가 아닐 경우 에러")
     public void createGameRound() throws Exception {
-        assertThatIllegalArgumentException().isThrownBy(() -> racingGameController.createGameRound(-1)).withMessageContaining("0이하의 값인지 확인");
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> racingGameController.createGameRound(-1)).withMessageContaining("0이하의 값인지 확인");
     }
 }
