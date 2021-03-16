360c8ee4d58f749375ffdb62fd43d2a46cdc56ff
Beenie93 <seongbeen93@naver.com>
2021-03-17 04:49:28 +0900

    refactor: add List<Car>

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,19 @@
 package racingcar;
 
 import racingcar.controller.RacingGameController;
+import racingcar.domain.Car;
 import racingcar.repository.CarRepository;
 import racingcar.repository.GameRoundRepository;
 import racingcar.service.CarService;
 import racingcar.service.GameRoundService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        CarService carService = new CarService(new CarRepository());
+        List<Car> cars = new ArrayList<>();
+        CarService carService = new CarService(new CarRepository(cars));
         GameRoundService gameRoundService = new GameRoundService(new GameRoundRepository());
         RacingGameController racingGameController = new RacingGameController(carService, gameRoundService);
         racingGameController.run();
