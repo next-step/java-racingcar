16151e1d1cea4cc0643737a5575811f8de1bd22e
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-17 02:59:42 +0900

    feat : 게임 종료 기능과 우승자 추출 기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -6,18 +6,18 @@ import java.util.List;
 public class RacingGame {
 
     private GameSet gameSet;
-    private int currentRound = 0;
 
     public RacingGame(GameSet gameSet) {
         this.gameSet = gameSet;
     }
 
-    public List<RacingCar> settingRacingCars(int cars) {
+    public List<RacingCar> settingRacingCars(String owners) {
 
         ArrayList<RacingCar> list = new ArrayList<>();
+        String[] owner = owners.split(",");
 
-        for (int i = 0; i < cars; i++) {
-            RacingCar racingCar = new RacingCar(new Position());
+        for (int i = 0; i < owner.length; i++) {
+            RacingCar racingCar = new RacingCar(owner[i], new Position());
             list.add(racingCar);
         }
 
@@ -28,22 +28,26 @@ public class RacingGame {
         numberingGameTimes(carList, count);
     }
 
+    public void gameOver(List<RacingCar> carList) {
+        Winner winner = new Winner();
+        winner.showResult(winner.whoisWin(carList, gameSet));
+    }
+
+
     private void numberingGameTimes(List<RacingCar> carList, int count) {
         for (int i = 0; i < count; i++) {
             numberingCars(carList);
-            currentRound++;
             System.out.println("");
         }
+        gameOver(carList);
     }
+
 
     private void numberingCars(List<RacingCar> carList) {
         for (int i = 0; i < carList.size(); i++) {
             carList.get(i).move();
-            System.out.println(carList.get(i).status());
+            System.out.println(carList.get(i).getOwner() + " : " + carList.get(i).status());
         }
     }
 
-    public boolean gameOver() {
-        return currentRound == gameSet.getGivenRound();
-    }
 }
