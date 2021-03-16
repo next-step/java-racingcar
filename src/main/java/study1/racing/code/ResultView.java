08d95ed56c17d9c55c189b5fce1c08c82426662d
keke0123 <keke0123@naver.com>
2021-03-16 22:18:58 +0900

    refactor: 매직넘버 상수화

--- src/main/java/study1/racing/code/ResultView.java
+++ src/main/java/study1/racing/code/ResultView.java
@@ -4,13 +4,15 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class ResultView {
+  private static final String RUN_MARK = "-";
+
   public static void print(List<Integer> distances) {
     distances.stream().forEach(ResultView::printMinus);
     System.out.print("\n");
   }
 
   public static void printMinus(int num) {
-    IntStream.range(0, num).forEach((i) -> System.out.print("-"));
+    IntStream.range(0, num).forEach((i) -> System.out.print(RUN_MARK));
     System.out.print("\n");
   }
 }
