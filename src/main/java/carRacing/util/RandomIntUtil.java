7671effca2bc5bf07bdcaf7a417f169dd5230d52
JeongByeongHun <jbh1024@gmail.com>
2021-03-17 02:05:06 +0900

    refactor  : RandomIntUtil refactoring.

--- src/main/java/carRacing/util/RandomIntUtil.java
+++ src/main/java/carRacing/util/RandomIntUtil.java
@@ -3,8 +3,14 @@ package carRacing.util;
 import java.util.Random;
 
 public class RandomIntUtil {
-    public static int getRandomInt(){
-        Random random = new Random();
-        return random.nextInt(10);
+    private static final int RANDOM_INT_BOUNDARY = 10;
+
+    private static Random random = new Random();
+
+    private RandomIntUtil() {
+    }
+
+    public static int getRandomInt() {
+        return random.nextInt(RANDOM_INT_BOUNDARY);
     }
 }
