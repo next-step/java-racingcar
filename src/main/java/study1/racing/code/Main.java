cf98a52b2a89b8776d2668e1309ab8f10130f6d7
keke0123 <keke0123@naver.com>
2021-03-16 23:05:59 +0900

    refactor: scanner Inputview 에 넣음

--- src/main/java/study1/racing/code/Main.java
+++ src/main/java/study1/racing/code/Main.java
@@ -1,19 +1,16 @@
 package study1.racing.code;
 
-import java.util.Scanner;
-
 public class Main {
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int participant = InputView.getParticipants(scanner);
-    int round = InputView.getRound(scanner);
+    int participant = InputView.getParticipants();
+    int round = InputView.getRound();
 
     Race race = new Race(participant, round);
     race.start().stream()
       .map(RaceRound::getResult)
       .forEach(ResultView::print);
 
-    scanner.close();
+    InputView.close();
   }
 }
