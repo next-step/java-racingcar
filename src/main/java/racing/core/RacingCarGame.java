5a4e56c24e4fefa59a2499b5c8aa70820da78b99
gmoon <gmoon@rsupport.com>
2021-03-17 02:52:51 +0900

    feat step3 자동차 경주 게임 - 경기 결과 기록 목적만 되도록 기능 수정 isMove 메서드 이동
    todo test 코드 작성
    - Random 메서드 전략 패턴을 사용하여 테스트하기 어려운 메소드 stub 데이터로 반환하도록 수정

--- src/main/java/racing/core/RacingCarGame.java
+++ src/main/java/racing/core/RacingCarGame.java
@@ -6,10 +6,13 @@ import racing.vo.RacingCarGamePlayInfo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 public class RacingCarGame {
 
+  public static final int LIMIT_RANDOM_NUMBER = 10;
+  public static final int MORE_THAN_NUMBER_FOR_CAR_MOVE = 4;
   private final RacingCarGamePlayInfo info;
 
   private RacingCarGame(RacingCarGamePlayInfo info) {
@@ -23,13 +26,24 @@ public class RacingCarGame {
   public List<RacingRound> play() {
     int totalRound = info.getTotalRound();
     List<RacingCar> racingCars = info.getPlayers();
-    List<RacingRound> result = new ArrayList<>(totalRound);
+
+    List<RacingRound> racingRounds = new ArrayList<>(totalRound);
     for (int i = 0; i < totalRound; i++) {
-      RacingRound round = new RacingRound();
-      round.record(racingCars);
-      result.add(round);
+      for (RacingCar car : racingCars) {
+        if (isMove())
+          car.move();
+      }
+      racingRounds.add(RacingRound.createRecord(racingCars));
     }
-    return result;
+    return racingRounds;
+  }
+
+  private boolean isMove() {
+    return getRandomNumber() >= MORE_THAN_NUMBER_FOR_CAR_MOVE;
+  }
+
+  private int getRandomNumber() {
+    return new Random().nextInt(LIMIT_RANDOM_NUMBER);
   }
 
 }
--- src/main/java/racing/domain/RacingRound.java
+++ src/main/java/racing/domain/RacingRound.java
@@ -2,36 +2,28 @@ package racing.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class RacingRound {
 
-  public static final int LIMIT_RANDOM_NUMBER = 10;
-
-  public static final int MORE_THAN_NUMBER_FOR_CAR_MOVE = 4;
-
   private List<RacingCar> racingCars;
 
-  public void record(List<RacingCar> racingCars) {
-    for (RacingCar car : racingCars) {
-      if (isMove()) {
-        car.move();
-      }
+  private RacingRound() {
+  }
 
+  public static RacingRound createRecord(List<RacingCar> racingCars) {
+    RacingRound round = new RacingRound();
+    round.record(racingCars);
+    return round;
+  }
+
+  private void record(List<RacingCar> racingCars) {
+    for (RacingCar car : racingCars) {
       capture(car);
     }
   }
 
-  public void capture(RacingCar racingCar) {
+  private void capture(RacingCar racingCar) {
     getRacingCars().add(RacingCar.copy(racingCar));
-  }
-
-  private boolean isMove() {
-    return getRandomNumber() >= MORE_THAN_NUMBER_FOR_CAR_MOVE;
-  }
-
-  private int getRandomNumber() {
-    return new Random().nextInt(LIMIT_RANDOM_NUMBER);
   }
 
   public List<RacingCar> getRacingCars() {
