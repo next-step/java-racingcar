fcf1c1766db270fbff542a8b74ddb867ef5b523e
김우재 <kwj1270@naver.com>
2021-03-18 00:08:55 +0900

    refactor:Name 인스턴스의 유효성 검사 리팩터링

--- src/main/java/step4/domain/car/name/Name.java
+++ src/main/java/step4/domain/car/name/Name.java
@@ -1,14 +1,18 @@
 package step4.domain.car.name;
 
 public final class Name {
-
+    private final int STANDARD = 5;
     private final String name;
 
     public Name(String name) {
-        if(name.length() > 5) {
+        if(isInvalid(name)) {
             throw new IllegalArgumentException("유효하지 않은 이름을 사용했습니다.");
         }
         this.name = name;
+    }
+
+    private final boolean isInvalid(String name) {
+        return name.length() > STANDARD;
     }
 
     public final String name() {
