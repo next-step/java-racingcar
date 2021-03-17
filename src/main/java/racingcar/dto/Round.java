a32c7ad174052c24b428633493e9253f004f96f1
suhyunsim <cnc1106@naver.com>
2021-03-17 11:59:44 +0900

    refactor: Round에서 전체 라운드의 결과들을 정수 리스트로 반환하도록 수정

--- src/main/java/racingcar/dto/Round.java
+++ src/main/java/racingcar/dto/Round.java
@@ -1,7 +1,10 @@
 package racingcar.dto;
 
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Round {
@@ -13,6 +16,15 @@ public class Round {
 
     public Cars getAllCars() {
         return cars;
+    }
+
+    public static List<Integer> roundResult(Round round) {
+        List<Integer> result = new ArrayList<>();
+        List<Car> cars = round.getAllCars().getCarsAtRound();
+        for (Car car : cars) {
+            result.add(car.getPosition());
+        }
+        return result;
     }
 
     @Override
