94ca0236c79a8f893875522afcc5b727690dd528
gummybearr <ehrudghl123@naver.com>
2021-03-17 16:34:20 +0900

    rebase

--- src/main/java/carRacing/service/CarRacingService.java
+++ src/main/java/carRacing/service/CarRacingService.java
@@ -1,20 +1,20 @@
 package carRacing.service;
 
-import carRacing.domain.Car;
-import carRacing.domain.Game;
-import carRacing.domain.Judge;
-import carRacing.domain.View;
+import carRacing.domain.*;
 import carRacing.dto.UserInput;
 
 import java.util.List;
 
 public class CarRacingService {
 
+    private final CarFactory carFactory = new CarFactory();
     private final View view = new View();
     private final Judge judge = new Judge();
     private final Game game = new Game();
 
-    public void run(UserInput userInput, List<Car> cars) {
+    public void run() {
+        UserInput userInput = view.getUserInput();
+        List<Car> cars = carFactory.initCars(userInput);
         view.declareRace();
         game.runRace(userInput, cars);
         view.printWinners(judge.findWinners(cars));
