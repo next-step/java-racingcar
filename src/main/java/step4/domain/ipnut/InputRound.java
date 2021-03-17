0072d0347e7ffba6ab0fdc9b67e5e0bc960b7467
김우재 <kwj1270@naver.com>
2021-03-18 01:23:23 +0900

    refactor:InputRound에 부적절한 값을 입력시 예외 검증기능 메서드 분리

--- src/main/java/step4/domain/ipnut/InputRound.java
+++ src/main/java/step4/domain/ipnut/InputRound.java
@@ -1,14 +1,17 @@
 package step4.domain.ipnut;
 
 public final class InputRound {
-
+    private final static int MINIMUM = 0;
     private final int round;
 
     public InputRound(int round) {
-        if(round < 0) {
+        if (isInvalid(round)) {
             throw new IllegalArgumentException("유효하지 않은 값을 사용했습니다.");
         }
         this.round = round;
     }
 
+    private final boolean isInvalid(int round) {
+        return round < MINIMUM;
+    }
 }
