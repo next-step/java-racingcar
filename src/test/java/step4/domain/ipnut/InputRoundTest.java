559e78bd694c721384dc796ea1695cfcb2d1961e
김우재 <kwj1270@naver.com>
2021-03-18 01:19:20 +0900

    test:InputRound에 부적절한 값 입력시 예외 발생 여부 테스트 작성

--- src/test/java/step4/domain/ipnut/InputRoundTest.java
+++ src/test/java/step4/domain/ipnut/InputRoundTest.java
@@ -2,8 +2,10 @@ package step4.domain.ipnut;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step4.domain.car.name.Name;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputRoundTest {
 
@@ -18,5 +20,19 @@ class InputRoundTest {
 
         // then
         assertThat(inputRound).isNotNull();
+    }
+
+    @DisplayName("InputRound 인스턴스에 부적절한 값 입력시 예외 발생 여부 테스트")
+    @Test
+    void 검증() {
+        // given
+        int round = -1;
+
+        // when and then
+        assertThatThrownBy(() -> {
+            new InputRound(round);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 값을 사용했습니다.");
+
     }
 }
