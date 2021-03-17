9a7bd4d16d5b4287004c680bed99034b0d86bf28
김우재 <kwj1270@naver.com>
2021-03-17 22:53:14 +0900

    test:Car 객체 생성 가능한지 여부에 대한 테스트 작성

---
+++ src/test/java/step4/domain/car/CarTest.java
@@ -0,0 +1,23 @@
+package step4.domain.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @DisplayName("Car 인스턴스 생성 여부 테스트")
+    @Test
+    void 생성() {
+        // given
+        String name = "test";
+
+        // when
+        Car car = new Car("test");
+
+        // test
+        assertThat(car).isNotNull();
+    }
+
+}
