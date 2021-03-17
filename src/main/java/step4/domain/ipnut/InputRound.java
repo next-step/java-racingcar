4156bb958eb5e172abc41adba922789e5435b709
김우재 <kwj1270@naver.com>
2021-03-18 01:20:35 +0900

    feat:InputRound에 부적절한 값을 입력시 예외 발생 기능 구현

--- src/main/java/step4/domain/ipnut/InputRound.java
+++ src/main/java/step4/domain/ipnut/InputRound.java
@@ -5,6 +5,9 @@ public final class InputRound {
     private final int round;
 
     public InputRound(int round) {
+        if(round < 0) {
+            throw new IllegalArgumentException("유효하지 않은 값을 사용했습니다.");
+        }
         this.round = round;
     }
 
