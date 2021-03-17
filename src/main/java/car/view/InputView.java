aafaf30ecb56ab435648a6b46254410c3c50d0cc
yoontae8 <yoontae8@hotmail.com>
2021-03-17 16:04:53 +0900

    refactor: extract method for checking input is valid

--- src/main/java/car/view/InputView.java
+++ src/main/java/car/view/InputView.java
@@ -10,17 +10,21 @@ public class InputView {
     public static UserInput receiveInput() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         int carCount = scanner.nextInt();
-        if (carCount <= 0) {
+        if (isInvalid(carCount)) {
             throw new IllegalArgumentException("Car count must be positive");
         }
         System.out.println("시도할 횟수는 몇 회 인가요?");
         int totalRound = scanner.nextInt();
-        if (totalRound <= 0) {
+        if (isInvalid(totalRound)) {
             throw new IllegalArgumentException("Total round must be positive");
         }
 
         System.out.println();
         System.out.println("실행 결과");
         return new UserInput(carCount, totalRound);
+    }
+
+    private static boolean isInvalid(int input) {
+        return input <= 0;
     }
 }
