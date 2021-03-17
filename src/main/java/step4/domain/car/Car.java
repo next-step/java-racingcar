ad2426b76ec3a62957dee81782590c9e62104b8a
김우재 <kwj1270@naver.com>
2021-03-17 23:18:17 +0900

    feat:Car 인스턴스가 움직이는 기능 구현

--- src/main/java/step4/domain/car/Car.java
+++ src/main/java/step4/domain/car/Car.java
@@ -5,6 +5,7 @@ import step4.domain.car.name.Name;
 public final class Car {
 
     private final Name name;
+    private int position;
 
     public Car(String name) {
         this(new Name(name));
@@ -12,7 +13,17 @@ public final class Car {
 
     public Car(Name name) {
         this.name = name;
+        this.position = 0;
     }
 
+    public final void move(int activation) {
+        if (activation >= 4 && activation <= 9) {
+            position++;
+        }
+    }
+
+    public final int position() {
+        return position;
+    }
 
 }
