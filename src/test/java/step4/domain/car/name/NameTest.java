a21eeb98d63645a576b9ab1e7fda7797d8481781
김우재 <kwj1270@naver.com>
2021-03-18 00:01:58 +0900

    test:Name 인스턴스의 부적절한 이름시 예외처리 확인 테스트 작성

--- src/test/java/step4/domain/car/name/NameTest.java
+++ src/test/java/step4/domain/car/name/NameTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
 
-    @DisplayName("Name 클래스의 인스턴스 생성 여부 테스트")
+    @DisplayName("Name 인스턴스 생성 여부 테스트")
     @Test
     void 생성() {
         // given
@@ -18,5 +19,18 @@ class NameTest {
 
         // then
         assertThat(name).isNotNull();
+    }
+
+    @DisplayName("Name 인스턴스의 부적절한 이름시 예외처리 확인 테스트")
+    @Test
+    void 검증() {
+        // given
+        String invalidName = "kwj1270";
+
+        assertThatThrownBy(() -> {
+            new Name(invalidName);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 이름을 사용했습니다.");
+
     }
 }
