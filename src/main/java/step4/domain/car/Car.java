a00f933c61f007daa1f5f3a95925b1ca1f5e4748
김우재 <kwj1270@naver.com>
2021-03-17 23:04:32 +0900

    refactor:Car의 이름 필드를 Name 클래스로 리팩터링

--- src/main/java/step4/domain/car/Car.java
+++ src/main/java/step4/domain/car/Car.java
@@ -1,11 +1,16 @@
 package step4.domain.car;
 
+import step4.domain.car.name.Name;
+
 public final class Car {
 
-    private final String name;
+    private final Name name;
 
     public Car(String name) {
-        this.name = name;
+        this(new Name(name));
     }
 
+    public Car(Name name) {
+        this.name = name;
+    }
 }
