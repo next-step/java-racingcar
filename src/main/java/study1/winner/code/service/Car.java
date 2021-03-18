60fd5b875fb25f2d2fd2315a30ba7779ea03e30c
keke0123 <keke0123@naver.com>
2021-03-18 19:15:52 +0900

    (feat) car 와 cartest 제작

--- src/main/java/study1/winner/code/service/Car.java
+++ src/main/java/study1/winner/code/service/Car.java
@@ -1,5 +1,38 @@
 package study1.winner.code.service;
 
 public class Car {
+  private static final int INIT_DISTANCE = 0;
+  private static final int INCREASE_DISTANCE = 1;
   
+  private final Name name;
+  private int distance;
+
+
+  public Car(Name name, int distance) {
+    this.name = name;
+    this.distance = distance;
+  }
+
+  public Car(String name) {
+    this(new Name(name), INIT_DISTANCE);
+  }
+
+  public Car(String name, int distance) {
+    this(new Name(name), distance);
+  }
+
+  public Car move(MoveStrategy moveStrategy) {
+    if(moveStrategy.move()) {
+      distance += INCREASE_DISTANCE;
+    }
+    return new Car(new Name(name.name()), distance);
+  }
+
+  public String driver() {
+    return name.name();
+  }
+
+  public int distance() {
+    return distance;
+  }
 }
