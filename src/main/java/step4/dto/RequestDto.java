d8bb631cdf48ff1e30c69db5c75c25483890f1dd
김우재 <kwj1270@naver.com>
2021-03-18 22:44:47 +0900

    feat:RequestDto 인스턴스 생성 기능 구현

--- src/main/java/step4/dto/RequestDto.java
+++ src/main/java/step4/dto/RequestDto.java
@@ -1,4 +1,16 @@
 package step4.dto;
 
-public class RequestDto {
+import step4.domain.ipnut.InputNames;
+import step4.domain.ipnut.InputRound;
+
+public final class RequestDto {
+
+    private final InputNames inputNames;
+    private final InputRound inputRound;
+
+    public RequestDto(InputNames inputNames, InputRound inputRound) {
+        this.inputNames = inputNames;
+        this.inputRound = inputRound;
+    }
+
 }
