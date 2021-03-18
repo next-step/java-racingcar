324a08aed647af5a5aa2b673c1085d712676d8e0
김우재 <kwj1270@naver.com>
2021-03-18 23:00:45 +0900

    feat:InputNmaes가 포함한 값 반환하는 기능 구현

--- src/main/java/step4/domain/ipnut/InputNames.java
+++ src/main/java/step4/domain/ipnut/InputNames.java
@@ -7,17 +7,21 @@ import java.util.List;
 
 public class InputNames {
 
-    private List<String> inputNames;
+    private final List<String> inputNames;
 
     public InputNames(String inputNames) {
         this(StringUtils.splitByCommaToList(inputNames));
     }
 
     public InputNames(List<String> inputNames) {
-        if(ListStringUtils.isNullOrSizeZero(inputNames)) {
+        if (ListStringUtils.isNullOrSizeZero(inputNames)) {
             throw new IllegalArgumentException("유효하지 않은 값을 사용했습니다.");
         }
         this.inputNames = inputNames;
+    }
+
+    public final List<String> inputNames() {
+        return inputNames;
     }
 
 }
