356f010c4fe31e1cd6217d19412c2f38b1a8d0a4
yoontae8 <yoontae8@hotmail.com>
2021-03-18 20:35:53 +0900

    feat: Add main class for actual play

---
+++ src/main/java/car/Main.java
@@ -0,0 +1,8 @@
+package car;
+
+public class Main {
+    public static void main(String[] args) {
+        CarRacingGame carRacingGame = new CarRacingGame();
+        carRacingGame.play();
+    }
+}
