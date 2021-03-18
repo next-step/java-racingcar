329b265eea135f5a02051976f9b821f373d9d560
김우재 <kwj1270@naver.com>
2021-03-19 00:40:34 +0900

    test:Round에 부적절한 값 주입시 예외처리 발생 여부 테스트 작성

--- src/test/java/step4/domain/racing/RoundTest.java
+++ src/test/java/step4/domain/racing/RoundTest.java
@@ -2,8 +2,12 @@ package step4.domain.racing;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RoundTest {
 
@@ -19,4 +23,18 @@ class RoundTest {
         assertThat(round).isNotNull();
 
     }
+
+    @DisplayName("Round 인스턴스에 부적절한 값 주입시 예외처리 발생 여부 테스트")
+    @ParameterizedTest(name = "돌아가고 있는 횟수 : {index} , 들어가는 값 : {arguments}")
+    @ValueSource(ints = {-1, 0})
+    void 검증(int inputValue) {
+
+        // when and then
+        assertThatThrownBy(() ->{
+            new Round(inputValue);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 값을 사용했습니다.");
+
+    }
+
 }
