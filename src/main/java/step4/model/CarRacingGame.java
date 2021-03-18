ad5225bc1798113040bbcdb9af6f7a938f952f3b
Sooncheol <ryxhdrnrmf@gmail.com>
2021-03-18 22:49:40 +0900

    refactor: CarRacingGame 구현

---
+++ src/main/java/step4/model/CarRacingGame.java
@@ -0,0 +1,26 @@
+package step4.model;
+
+public class CarRacingGame {
+    private final Cars cars;
+    private final Round round;
+
+    public CarRacingGame(String[] carNames, int round) {
+        this.cars = new Cars(carNames);
+        this.round = new Round(round);
+    }
+
+    public void race(MoveStrategy moveStrategy) {
+        round.race();
+        cars.move(moveStrategy);
+    }
+
+    public boolean racing() {
+        return round.racing();
+    }
+    
+    public Winners Winners() {
+        return cars.Winners();
+    }
+
+
+}
