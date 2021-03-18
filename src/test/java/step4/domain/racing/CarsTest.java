d2a727f42abaaee61033a7dab25247ffbcdfb597
김우재 <kwj1270@naver.com>
2021-03-19 00:16:54 +0900

    test:Cars 인스턴스 부적절한 값 주입시 예외처리 발생 여부 테스트 작성

--- src/test/java/step4/domain/racing/CarsTest.java
+++ src/test/java/step4/domain/racing/CarsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import step4.domain.ipnut.InputNames;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
 
@@ -21,4 +22,20 @@ class CarsTest {
         assertThat(cars).isNotNull();
 
     }
+
+    @DisplayName("Cars 인스턴스 부적절한 값 주입시 예외처리 발생 여부 테스트")
+    @Test
+    void 검증() {
+
+        // given
+        InputNames inputNames = null;
+
+        // when
+        assertThatThrownBy(()-> {
+            new Cars(inputNames);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 값을 사용했습니다.");
+
+    }
+
 }
