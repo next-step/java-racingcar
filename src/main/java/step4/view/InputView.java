2829d74be5cb41536ca8cb812784f67f3952c9fb
김우재 <kwj1270@naver.com>
2021-03-18 23:51:08 +0900

    feat:InputView 구현

---
+++ src/main/java/step4/view/InputView.java
@@ -0,0 +1,33 @@
+package step4.view;
+
+import step4.dto.RequestDto;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static InputView instance;
+
+    public static InputView getInstance() {
+        if (instance == null) {
+            instance = new InputView();
+        }
+        return instance;
+    }
+
+    private final String inputNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.next();
+    }
+
+    private final int round() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
+    }
+
+    public final RequestDto requestDto() {
+        return new RequestDto(inputNames(), round());
+    }
+
+}
