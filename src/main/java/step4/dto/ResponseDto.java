66a6d8997fca9e5dd3cf0b52396c131bcd5f9c69
김우재 <kwj1270@naver.com>
2021-03-19 01:47:28 +0900

    feat:ResponseDto 인스턴스 생성 기능 구현

--- src/main/java/step4/dto/ResponseDto.java
+++ src/main/java/step4/dto/ResponseDto.java
@@ -1,4 +1,11 @@
 package step4.dto;
 
+import step4.domain.racing.Cars;
+
 public class ResponseDto {
+    private final Cars cars;
+
+    public ResponseDto(Cars cars) {
+        this.cars = cars;
+    }
 }
--- src/test/java/step4/dto/ResponseDtoTest.java
+++ src/test/java/step4/dto/ResponseDtoTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import step4.domain.racing.Cars;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ResponseDtoTest {
 
