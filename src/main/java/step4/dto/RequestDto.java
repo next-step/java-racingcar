957df5fcc4c7eff61e20ca60713d7d44546ee17d
김우재 <kwj1270@naver.com>
2021-03-18 22:47:06 +0900

    refactor:RequestDto 인스턴스 다양한 생성자 추가

--- src/main/java/step4/dto/RequestDto.java
+++ src/main/java/step4/dto/RequestDto.java
@@ -2,11 +2,22 @@ package step4.dto;
 
 import step4.domain.ipnut.InputNames;
 import step4.domain.ipnut.InputRound;
+import utils.StringUtils;
+
+import java.util.List;
 
 public final class RequestDto {
 
     private final InputNames inputNames;
     private final InputRound inputRound;
+
+    public RequestDto(String inputNames, int round) {
+        this(new InputNames(StringUtils.splitByCommaToList(inputNames)), new InputRound(round));
+    }
+
+    public RequestDto(List<String> inputNames, int round) {
+        this(new InputNames(inputNames), new InputRound(round));
+    }
 
     public RequestDto(InputNames inputNames, InputRound inputRound) {
         this.inputNames = inputNames;
