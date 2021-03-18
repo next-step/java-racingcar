4897ed29decdd0f6ed6ea7522f9a4ebb3c83e0c9
김우재 <kwj1270@naver.com>
2021-03-18 23:57:33 +0900

    feat:Cars 인스턴스 생성 기능 구현

--- src/main/java/step4/domain/racing/Cars.java
+++ src/main/java/step4/domain/racing/Cars.java
@@ -1,4 +1,21 @@
 package step4.domain.racing;
 
+import step4.domain.car.Car;
+import step4.domain.ipnut.InputNames;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(InputNames inputNames) {
+        this(inputNames.inputNames().stream().map(Car::new).collect(Collectors.toList()));
+    }
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
 }
