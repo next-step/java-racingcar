d911a0d9a902528d94575e3cb84fffd6eb45828a
yoontae8 <yoontae8@hotmail.com>
2021-03-18 21:15:02 +0900

    feat: Split line with more than one dot

--- src/main/java/car/view/ResultView.java
+++ src/main/java/car/view/ResultView.java
@@ -8,7 +8,10 @@ public class ResultView {
 
     public static void printStatus(List<RacingCar> racingCars) {
         for (RacingCar racingCar : racingCars) {
-            System.out.println(new String(new char[racingCar.getMoveCount()]).replace("\0", "-"));
+            System.out.println(
+                    new String(new char[racingCar.getMoveCount()])
+                            .replace("\0", "-")
+            );
         }
         System.out.println();
     }
