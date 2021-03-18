38dbe0eebaf499c619f4ad73d87bdaff1049e70e
keke0123 <keke0123@naver.com>
2021-03-18 18:37:15 +0900

    (feat) Name class 제작 및 테스트 코드 작성

--- src/main/java/study1/winner/code/service/Name.java
+++ src/main/java/study1/winner/code/service/Name.java
@@ -3,4 +3,17 @@ package study1.winner.code.service;
 import org.junit.platform.commons.util.StringUtils;
 
 public class Name {  
+  private final String name;
+
+  public Name(String name) {
+    if(StringUtils.isBlank(name)) {
+      throw new IllegalArgumentException("participants should not null or empty");
+    }
+
+    this.name = name.trim();
+  }
+
+  public String name() {
+    return name;
+  }
 }
--- src/test/java/study1/winner/code/service/NameTest.java
+++ src/test/java/study1/winner/code/service/NameTest.java
@@ -9,5 +9,23 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class NameTest {
 
+  @ParameterizedTest
+  @CsvSource(value = {" pobi:pobi", "tim :tim"}, delimiter = ':')
+  @DisplayName("참가자의 이름에 공백이 있는 값과 없는 값의 결과값이 같은지 테스트")
+  public void autoTrim(String notTrim, String trim) {
+    Name nameOfNotTrim = new Name(notTrim);
+    Name nameOfTrim = new Name(trim);
 
+    assertThat(nameOfNotTrim.name()).isEqualTo(nameOfTrim.name());
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  @DisplayName("참가자 String 값이 null or 공백인 경우 에러를 리턴하는지 테스트")
+  public void isEmpty(String name) {
+    assertThatThrownBy(() -> {
+      new Name(name);
+    }).isInstanceOf(IllegalArgumentException.class);
+  }
+  
 }
