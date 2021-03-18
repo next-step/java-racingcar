79612ca3af71e61046d669129704ed7e04287345
ad7549 <ad7549@gmail.com>
2021-03-18 14:06:47 +0900

    refactor: Fix typo

--- src/main/java/racing/views/InputView.java
+++ src/main/java/racing/views/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String carNames() {
-        System.out.println(("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."));
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return scanner.nextLine();
     }
 
