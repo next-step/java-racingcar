613e1c108df1d0bc3c3c620ac2a7b1096968de6c
keke0123 <keke0123@naver.com>
2021-03-18 19:15:34 +0900

    (feat) MoveStrategy interface 및 상속받은 클레스 정의

---
+++ src/main/java/study1/winner/code/service/MoveStrategy.java
@@ -0,0 +1,6 @@
+package study1.winner.code.service;
+
+@FunctionalInterface
+public interface MoveStrategy {
+  public Boolean move();
+}
--- src/main/java/study1/winner/code/service/RaceMove.java
+++ src/main/java/study1/winner/code/service/RaceMove.java
@@ -1,5 +1,13 @@
 package study1.winner.code.service;
 
-public class RaceMove {
-  
+import java.util.Random;
+
+public class RaceMove implements MoveStrategy {
+  @Override
+  public Boolean move() {
+    if(new Random().nextInt(10) > 4) {
+      return true;
+    }
+    return false;
+  }
 }
