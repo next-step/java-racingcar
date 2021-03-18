69f13a43eedb29c051b9907c8956c23c8b272e9e
김우재 <kwj1270@naver.com>
2021-03-18 21:01:34 +0900

    test:StringUtils 문자열을 콤마를 기준으로 나누어 List로 반환 여부 테스트 작성

--- src/main/java/step4/domain/ipnut/InputNames.java
+++ src/main/java/step4/domain/ipnut/InputNames.java
@@ -1,4 +1,19 @@
 package step4.domain.ipnut;
 
+import utils.StringUtils;
+
+import java.util.List;
+
 public class InputNames {
+
+    private List<String> inputNames;
+
+    /*
+    public InputNames(String inputNames) {
+        this(StringUtils.splitByComma(inputNames));
+    }
+    public InputNames(List<String> inputNames) {
+        this.inputNames = inputNames;
+    }
+    */
 }
--- src/test/java/utils/StringUtilsTest.java
+++ src/test/java/utils/StringUtilsTest.java
@@ -22,5 +22,19 @@ class StringUtilsTest {
 
     }
 
+    @DisplayName("StringUtils 의 splitByCommaToList()가 ,를 기준으로 알맞는 리스트를 만드는지 확인하는 테스트")
+    @ParameterizedTest(name = "splitByComma 대상이 되는 값: {0}, 예상되는 결과값: {1}")
+    @CsvSource(value = {"1,2,3:3", "123:1", ",:0"}, delimiter = ':')
+    void splitByCommaToListTest(String sentence, int expected) {
+
+        // when
+        String[] dividedSentences = StringUtils.splitByCommaToList(sentence);
+        int actual = dividedSentences.length;
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+
+    }
+
 
 }
