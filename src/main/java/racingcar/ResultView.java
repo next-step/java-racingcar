d1e74e792cd0fae059eefeebc400e961e4dacfa0
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-19 13:31:38 +0900

    feat : 결과물을 따로 저장하는 클래스 추가

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -1,2 +1,16 @@
-package racingcar;public class ResultView {
+package racingcar;
+
+public class ResultView {
+
+    private String result = "";
+
+    public void setResult(String result) {
+        this.result += result;
+    }
+
+    public void showResult() {
+        System.out.println(result);
+    }
+
+
 }
