11b497247553bd7b0c0a0d81cfa4a4072ece5e20
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-19 13:35:00 +0900

    test : 우승자가 없을 경우에 대한 테스트 코드, 우승자 있을 경우에 대한 테스트 코드 리펙토링

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -2,24 +2,14 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
-import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.setMaxElementsForPrinting;
 
 public class RacingGameTest {
-
-    @DisplayName("0~9까지 랜덤 출력")
-    @Test
-    void random_test() {
-        System.out.println(RandomNumber.random());
-    }
-
 
     @DisplayName("주어진 횟수 동안 pobi, crong, honux의 자동차는 전진 또는 멈출 수 있다.")
     @ParameterizedTest
@@ -37,37 +27,54 @@ public class RacingGameTest {
     }
 
 
-    @DisplayName("우승자가 있을경우 - pobi")
+    @DisplayName("우승자가 한명일 경우와 주어진 라운드가 1인 경우 ")
     @Test
-    void winner() {
+    void winner_one() {
+
         GameSet gameSet = new GameSet("pobi", 1);
         RacingGame racingGame = new RacingGame(gameSet);
 
-        RacingCar racingCar = new RacingCar("pobi", null);
-        racingCar.isGo(5);
+        List<RacingCar> list = racingGame.settingRacingCars(gameSet.getOwners());
+
+        RacingCar pobiCar = list.get(0);
+        pobiCar.move();
+
+        assertThat(1).isEqualTo(pobiCar.status().length());
+    }
+
+    @DisplayName("우승자가 두명일 경우와 주어진 라운드가 1인 경")
+    @Test
+    void winner_two() {
+
+        GameSet gameSet = new GameSet("pobi,crong", 1);
+        RacingGame racingGame = new RacingGame(gameSet);
 
         List<RacingCar> list = racingGame.settingRacingCars(gameSet.getOwners());
-        racingGame.gameStart(list, 1);
 
-        Winner winner = new Winner();
-        String name = winner.whoisWin(list, gameSet);
+        RacingCar pobiCar = list.get(0);
+        RacingCar crongCar = list.get(1);
 
+        pobiCar.move();
+        crongCar.move();
 
-        assertThat("pobi가 최종 우승했습니다.").isEqualTo(winner.showResult(name));
+        assertThat(1).isEqualTo(pobiCar.status().length());
+        assertThat(1).isEqualTo(crongCar.status().length());
     }
 
 
     @DisplayName("우승자가 없을 경우")
-    @Test
-    void no_winner() {
-        GameSet gameSet = new GameSet("pobi", 1);
+    @ParameterizedTest
+    @CsvSource(value = {"pobi:1", "crong:2 ", "honux:5"}, delimiter = ':')
+    void no_winner(String owners, int count) {
+        GameSet gameSet = new GameSet(owners, count);
         RacingGame racingGame = new RacingGame(gameSet);
 
-        RacingCar racingCar = new RacingCar("pobi", null);
+        List<RacingCar> list = racingGame.settingRacingCars(gameSet.getOwners());
 
-        List<RacingCar> list = racingGame.settingRacingCars(racingCar.getOwner());
-
-        assertThat("").isEqualTo(new Winner().whoisWin(list, gameSet));
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).stop();
+            assertThat(0).isEqualTo(list.get(i).status().length());
+        }
 
     }
 }
