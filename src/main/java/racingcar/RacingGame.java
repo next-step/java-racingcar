ad65c61ac0df704399c3f8c5ce8c6eb4d570b6e4
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-19 13:32:36 +0900

    refactor : 기존에 sout으로 찍었던 결과들을 ResultView에 추가하는 방식으로 수정

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class RacingGame {
 
     private GameSet gameSet;
+    private ResultView resultView;
 
     public RacingGame(GameSet gameSet) {
         this.gameSet = gameSet;
+        resultView = new ResultView();
     }
 
     public List<RacingCar> settingRacingCars(String owners) {
@@ -37,16 +39,17 @@ public class RacingGame {
     private void numberingGameTimes(List<RacingCar> carList, int count) {
         for (int i = 0; i < count; i++) {
             numberingCars(carList);
-            System.out.println("");
+            resultView.setResult("\n");
         }
+        resultView.showResult();
         gameOver(carList);
     }
 
 
     private void numberingCars(List<RacingCar> carList) {
         for (int i = 0; i < carList.size(); i++) {
-            carList.get(i).move();
-            System.out.println(carList.get(i).getOwner() + " : " + carList.get(i).status());
+            carList.get(i).isGo(RandomNumber.random());
+            resultView.setResult(carList.get(i).getOwner() + " : " + carList.get(i).status() + "\n");
         }
     }
 
