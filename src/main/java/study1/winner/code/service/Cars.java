fb1d695020257850a2825ff03370fc87659b865b
keke0123 <keke0123@naver.com>
2021-03-19 20:22:25 +0900

    (docs) 불필요한 주석 제거

--- src/main/java/study1/winner/code/service/Cars.java
+++ src/main/java/study1/winner/code/service/Cars.java
@@ -1,6 +1,5 @@
 package study1.winner.code.service;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
--- src/test/java/study1/winner/code/service/CarsTest.java
+++ src/test/java/study1/winner/code/service/CarsTest.java
@@ -7,10 +7,8 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class CarsTest {
