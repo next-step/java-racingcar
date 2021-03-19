a488181a62c56ef1ff7e4d3d92b9070e502d03fb
김우재 <kwj1270@naver.com>
2021-03-19 16:24:00 +0900

    test:RacingCarGame 인스턴스가 Winners 인스턴스 반환 여부 테스트 작성

--- src/test/java/step4/domain/racing/RacingCarGameTest.java
+++ src/test/java/step4/domain/racing/RacingCarGameTest.java
@@ -2,7 +2,11 @@ package step4.domain.racing;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step4.domain.car.Car;
+import step4.domain.ipnut.InputNames;
 import step4.dto.ResponseDto;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -68,6 +72,22 @@ class RacingCarGameTest {
 
         // then
         assertThat(responseDto).isNotNull();
+    }
+
+    @DisplayName("RacingCarGame 우승자 반환 여부 테스트")
+    @Test
+    void 우승자_반환(){
+
+        // given
+        Cars cars = new Cars("a,b,c");
+        Round round = new Round(1);
+        RacingCarGame racingCarGame = new RacingCarGame(cars, round);
+
+        // when
+        Winners winners = racingCarGame.winners();
+
+        // then
+        assertThat(winners).isNotNull();
     }
 
 }
