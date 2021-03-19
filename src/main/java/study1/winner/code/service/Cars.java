1ff5e8b5717ccb80bff3ceaeefd92b38afac6510
keke0123 <keke0123@naver.com>
2021-03-19 18:32:58 +0900

    (feat) Cars 제작 및 CarsTest 제작

--- src/main/java/study1/winner/code/service/Cars.java
+++ src/main/java/study1/winner/code/service/Cars.java
@@ -1,5 +1,41 @@
 package study1.winner.code.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Cars {
-  
+  private List<Car> cars;
+
+  public Cars(List<Car> cars) {
+    this.cars = cars;
+  }
+
+  public Cars(Cars cars) {
+    this.cars = cars.cars;
+  }
+
+  public Cars move() {
+    return new Cars(
+      cars.stream()
+      .map((car) -> car.move(new RaceMove()))
+      .collect(Collectors.toList())
+    );
+	}
+
+  public List<String> winners() {
+    int maxDistance = maxDistance();
+    return cars.stream()
+      .filter((car) -> car.distance() == maxDistance)
+      .map((car) -> car.driver())
+      .collect(Collectors.toList());
+  }
+
+  public int maxDistance() {
+    return cars.stream()
+      .map((car) -> car.distance())
+      .max(Integer::compare)
+      .orElse(-1);
+  }
+
 }
