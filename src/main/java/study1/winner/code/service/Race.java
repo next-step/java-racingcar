84d26efcac3704b12a5a954e44543d553a5275a2
keke0123 <keke0123@naver.com>
2021-03-19 19:26:51 +0900

    (feat) Race 및 RaceTest 제작

--- src/main/java/study1/winner/code/service/Race.java
+++ src/main/java/study1/winner/code/service/Race.java
@@ -1,5 +1,45 @@
 package study1.winner.code.service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Race {
-  
+  private static final int START_NUM = 0;
+  private final int round;
+  private Cars cars;
+
+  public Race(int round, Cars cars) {
+    this.round = round;
+    this.cars = cars;
+  }
+
+  public Race(int round, String participants) {
+    this(
+      round, 
+      new Cars(Arrays.asList(participants.split(","))
+        .stream()
+        .map((name) -> new Car(name))
+        .collect(Collectors.toList()))
+    );
+  }
+
+  public List<Cars> start() {
+    return IntStream
+      .range(START_NUM, round)
+      .boxed()
+      .map((i) -> cars.move())
+      .map((cars) -> roundCheck(cars))
+      .collect(Collectors.toList());
+  }
+
+  public Cars roundCheck(Cars cars) {
+    this.cars = new Cars(cars);
+    return cars;
+  }
+
+  public List<String> winners() {
+    return cars.winners();
+  }
 }
--- src/test/java/study1/winner/code/service/RaceTest.java
+++ src/test/java/study1/winner/code/service/RaceTest.java
@@ -1,5 +1,22 @@
 package study1.winner.code.service;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class RaceTest {
-  
+
+  @ParameterizedTest
+  @CsvSource(value = {"pobi,tim:5"}, delimiter = ':')
+  @DisplayName("race 시 round 가 제대로 세팅되는지 테스트")
+  public void round(String participants, int round) {
+    Race race = new Race(round, participants);
+    List<Cars> raceRound = race.start();
+
+    assertThat(raceRound.size()).isEqualTo(round);
+  }
 }
