348bd736ffa1d639d4823ac70c03db49e64830b3
keke0123 <keke0123@naver.com>
2021-03-19 20:21:52 +0900

    (feat) Main 메소드 제작

--- src/main/java/study1/winner/code/Main.java
+++ src/main/java/study1/winner/code/Main.java
@@ -1,5 +1,22 @@
 package study1.winner.code;
 
+import study1.winner.code.common.InputView;
+import study1.winner.code.common.ResultView;
+import study1.winner.code.service.Cars;
+import study1.winner.code.service.Race;
+
 public class Main {
-  
+  public static void main(String[] args) {
+    String participants = InputView.getParticipants();
+    int round = InputView.getRound();
+    System.out.println("\n");
+    Race race = new Race(round, participants);
+
+    race.start().stream()
+      .map(Cars::result)
+      .forEach(ResultView::result);
+
+    InputView.close();
+  }
+
 }
