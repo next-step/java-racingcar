b30a02912fae96a7296df734b38c2e0414f2a449
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-19 13:33:22 +0900

    test : 랜덤과 관련된 테스트 코드를 따로 분리함

--- src/test/java/racingcar/RandomNumberTest.java
+++ src/test/java/racingcar/RandomNumberTest.java
@@ -1,2 +1,13 @@
-package racingcar;public class RandomNumberTest {
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RandomNumberTest {
+
+    @DisplayName("0~9까지 랜덤 출력")
+    @Test
+    void random_test() {
+        System.out.println(RandomNumber.random());
+    }
 }
