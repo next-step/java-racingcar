9dfb46b3db8b5a4121b51cda8d196d8383805e74
ad7549 <ad7549@gmail.com>
2021-03-19 13:40:32 +0900

    refactor: Add RacingController

--- src/main/java/racing/RacingMain.java
+++ src/main/java/racing/RacingMain.java
@@ -1,25 +1,9 @@
 package racing;
 
-import racing.domain.RacingSimulator;
-import racing.domain.Round;
-import racing.utils.StringUtil;
-import racing.views.InputView;
-import racing.views.ResultView;
+import racing.controllers.RacingController;
 
 public class RacingMain {
     public static void main(String[] args) {
-        final String namesInput = InputView.carNames();
-        final int attemptsCount = InputView.attemptsCount();
-
-        final RacingSimulator simulator = new RacingSimulator(StringUtil.splitCommas(namesInput), attemptsCount);
-        simulator.start();
-
-        System.out.println("\n실행 결과");
-        for (final Round round : simulator.getRounds()) {
-            ResultView.show(round);
-            System.out.println();
-        }
-
-        ResultView.show(simulator.getWinnerList());
+        new RacingController().run();
     }
 }
