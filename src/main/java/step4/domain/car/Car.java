3189bcac531cebbe6eaa5d55d5acd2de46401ad5
김우재 <kwj1270@naver.com>
2021-03-20 17:12:49 +0900

    feat:Car 인스턴스 객체끼리 비교 기능 구현

--- src/main/java/step4/domain/car/Car.java
+++ src/main/java/step4/domain/car/Car.java
@@ -38,4 +38,16 @@ public final class Car {
         return (position.position() == winnerPosition);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(name, car.name) && Objects.equals(position, car.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
+    }
 }
