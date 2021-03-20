b90d1ea95ec0a6a4443c003e097d89db350ac5c7
shkwon <oper912@naver.com>
2021-03-20 19:09:29 +0900

    refactor: [코드리뷰반영] 출력 메시지 상수로 변환

--- src/main/java/step4/view/ResultView.java
+++ src/main/java/step4/view/ResultView.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 public class ResultView {
     private static final String RESULT_START_MESSAGE = "실행 결과";
+    private static final String POSITION_MESSAGE = "%s : %s";
     private static final String POSITION_CHARACTER = "-";
+    private static final String WINNERS_MESSAGE = "%s가 최종 우승 했습니다.";
+    private static final String WINNER_SEPARATOR = ", ";
 
     public static void printResultStartMessage() {
         System.out.println("");
@@ -26,11 +29,11 @@ public class ResultView {
         for (int i = 0; i < car.getPosition(); i++) {
             builder.append(POSITION_CHARACTER);
         }
-        return String.format("%s : %s", car.getName(), builder.toString());
+        return String.format(POSITION_MESSAGE, car.getName(), builder.toString());
     }
 
     public static void printWinners(final Cars cars) {
         List<String> winners = cars.getWinnerNames();
-        System.out.println(String.format("%s가 최종 우승 했습니다.", String.join(", ", winners)));
+        System.out.println(String.format(WINNERS_MESSAGE, String.join(WINNER_SEPARATOR, winners)));
     }
 }
