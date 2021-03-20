126a292b824596ff705bdc978859ecc5107ba5a0
juniq <juniqlim@gmail.com>
2021-03-20 22:26:42 +0900

    repactor: 사용하지 않는 메소드 삭제

--- src/main/java/im/juniq/racingcar/domain/Position.java
+++ src/main/java/im/juniq/racingcar/domain/Position.java
@@ -1,7 +1,5 @@
 package im.juniq.racingcar.domain;
 
-import java.util.Objects;
-
 public class Position implements Comparable<Position> {
 	private int position;
 
@@ -15,21 +13,6 @@ public class Position implements Comparable<Position> {
 
 	public void increase() {
 		position++;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Position position1 = (Position)o;
-		return position == position1.position;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(position);
 	}
 
 	@Override
