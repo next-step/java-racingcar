919806bb6340667ff3ea2d136915c52ae58c4a4e
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-22 00:22:13 +0900

    test : 0 ~ 9 까지에 대한 유효성 체크 테스트 코드 추가

--- src/test/java/racingcar/RandomNumberTest.java
+++ src/test/java/racingcar/RandomNumberTest.java
@@ -3,11 +3,14 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RandomNumberTest {
 
-    @DisplayName("0~9까지 랜덤 출력")
+    @DisplayName("0~9사이의 값을 사용하는가?")
     @Test
     void random_test() {
-        System.out.println(RandomNumber.random());
+        int number = RandomNumber.random();
+        assertThat(number).isGreaterThan(0).isLessThan(10);
     }
 }
