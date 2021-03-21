3909c03e2fe468bc474f61f633b93f9af5d79ead
sonAjin <sonajin11@gmail.com>
2021-03-21 17:07:47 +0900

    (feature) moveCar 관련 메소드 테스트 코드 작성

--- src/test/java/step03/RacingCarFactoryTest.java
+++ src/test/java/step03/RacingCarFactoryTest.java
@@ -1,19 +1,30 @@
 package step03;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarFactoryTest {
 
-    @ParameterizedTest
-    @DisplayName("입력한 수만큼 차가 만들어 졌는지")
-    @CsvSource(value = {"car1,car2|2"}, delimiter = '|')
-    void countMakeCars(String carName, int carCount) {
-        RacingEventManager manager = new RacingEventManager(carName, 4);
-        manager.startRandomRacing();
-        assertThat(manager.getRacingResult().size()).isEqualTo(carCount);
+    RacingCarFactory racingCarFactory;
+
+    @BeforeEach
+    void setRacingCarFactory() {
+        racingCarFactory = new RacingCarFactory("test1,test2,test3");
+    }
+
+    @Test
+    @DisplayName("RacingCarFactory moveCars 이동한 차들 모두 전진하는지 테스트")
+    void moveCars() {
+        // when
+        racingCarFactory.moveCars(() -> true);
+
+        // then
+        for (RacingCar racingCar : racingCarFactory.getRacingCars()) {
+            assertThat(racingCar.getRacingCarData().getMovingRange()).isEqualTo(1);
+        }
+
     }
 }
--- src/test/java/step03/RacingCarTest.java
+++ src/test/java/step03/RacingCarTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RacingCarTest {
 
     RacingCar racingCar;
@@ -14,13 +16,12 @@ public class RacingCarTest {
     }
 
     @Test
-    @DisplayName("무조건 전진")
+    @DisplayName("car tryToMove 무조건 전진 테스트")
     void tryToMove() {
-
         // when
         racingCar.tryToMove(() -> true);
 
-
-
+        // then
+        assertThat(racingCar.getRacingCarData().getMovingRange()).isEqualTo(1);
     }
 }
