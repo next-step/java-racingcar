4635f62368680aa03bfaeaaef88776ce379e3d09
sonAjin <sonajin11@gmail.com>
2021-03-21 16:29:24 +0900

    (refactor) RacingEventManager 전역변수 carNames 제거 (전역변수로 관리할 필요 없어짐)

--- src/main/java/step03/RacingEventManager.java
+++ src/main/java/step03/RacingEventManager.java
@@ -5,12 +5,10 @@ import java.util.List;
 public class RacingEventManager {
 
     private int tryCount = 0;
-    private String carNames;
     private RacingCarFactory racingCarFactory;
 
     public RacingEventManager(String carNames, int tryCount) {
         racingCarFactory = new RacingCarFactory(carNames);
-        this.carNames = carNames;
         this.tryCount = tryCount;
     }
 
