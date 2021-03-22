d885c833d51e1eb74c55660ab530781dd74d5ede
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-22 23:26:09 +0900

    refactor : 매직넘버 리펙토링

--- src/main/java/racingcar/RandomNumber.java
+++ src/main/java/racingcar/RandomNumber.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class RandomNumber {
 
-    // TODO 이 둘은 난수 관련 클래스로 따로 만들어서 분리한다.
-    public static final int MINIMUM_NUMBER = 4;
+    public static final int MAXIMUM_NUMBER = 10;
     private static Random random = new Random();
 
     public static int random() {
-        return random.nextInt(10);
+        return random.nextInt(MAXIMUM_NUMBER);
     }
+
 }
