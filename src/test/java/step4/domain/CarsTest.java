114d6baf746ebda3ff70d55defaf992d18693fe9
shkwon <oper912@naver.com>
2021-03-23 03:34:53 +0900

    test: 리팩토링에 따른 테스트코드 수정

--- src/test/java/step4/domain/CarsTest.java
+++ src/test/java/step4/domain/CarsTest.java
@@ -1,6 +1,7 @@
 package step4.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step4.strategy.MovableStrategy;
 import step4.strategy.TestMovableStrategy;
@@ -8,20 +9,20 @@ import step4.strategy.TestMovableStrategy;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class CarsTest {
     private Cars cars;
     private MovableStrategy strategy;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         this.cars = Cars.from(Arrays.asList("kwon", "soon", "ho"));
         this.strategy = new TestMovableStrategy();
     }
 
     @Test
-    public void move() {
+    void move() {
         cars.move(strategy);
         for (Car car : cars.getCars()) {
             assertThat(car.getPosition()).isEqualTo(1);
@@ -29,15 +30,31 @@ class CarsTest {
     }
 
     @Test
-    public void getWinners() {
+    void getWinners() {
         // given
         cars.move(strategy);
+
         // when
         List<String> winners = cars.getWinnerNames();
+
         // then
         assertThat(winners.size()).isEqualTo(3);
         assertThat(winners.get(0)).isEqualTo("kwon");
         assertThat(winners.get(1)).isEqualTo("soon");
         assertThat(winners.get(2)).isEqualTo("ho");
+    }
+
+    @DisplayName("UnmodifiableList를 수정하면 UnsupportedOperationException 발생한다.")
+    @Test
+    void getCars() {
+        assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> {
+            List<Car> cars = this.cars.getCars();
+            cars.add(Car.from("test"));
+        });
+
+        assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> {
+            List<Car> cars = this.cars.getCars();
+            cars.remove(0);
+        });
     }
 }
