f61bcee07221ad430c4e74a0e8f672b34b92a0f9
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-22 23:27:52 +0900

    refactor : 우승자 리스트를 반환할 수 있게끔 리펙토링

--- src/main/java/racingcar/Winner.java
+++ src/main/java/racingcar/Winner.java
@@ -1,43 +1,26 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Winner {
 
-    private final String NO_WINNER = "";
-    private final int NO_WINNER_LENGTH = 0;
+    private List<RacingCar> winnerList;
 
-    public String whoisWin(List<RacingCar> carList, GameSet gameSet) {
-        String names = "";
+    public List<RacingCar> whoisWin(List<RacingCar> carList, GameSet gameSet) {
+
+        winnerList = new ArrayList<>();
 
         for (int i = 0; i < carList.size(); i++) {
-            names += winner(carList.get(i), gameSet);
+            isWin(carList.get(i), gameSet);
         }
 
-        return names;
+        return winnerList;
     }
 
-    private String winner(RacingCar racingCar, GameSet gameSet) {
-        if (racingCar.status().length() == gameSet.getGivenRound()) {
-            return racingCar.getOwner() + ",";
+    private void isWin(RacingCar racingCar, GameSet gameSet) {
+        if (racingCar.getCarLocation().length() == gameSet.getGivenRound()) {
+            winnerList.add(racingCar);
         }
-        return NO_WINNER;
-    }
-
-    public void showResult(String name) {
-
-        String result = "";
-
-        if (name.length() == NO_WINNER_LENGTH) {
-            System.out.println(result);
-        }
-
-        char lastChar = name.charAt(name.length()-1);
-
-        if (lastChar == ',') {
-            result = name.replaceFirst(".$", "") + "가 최종 우승했습니다.";
-        }
-
-        System.out.println(result);
     }
 }
