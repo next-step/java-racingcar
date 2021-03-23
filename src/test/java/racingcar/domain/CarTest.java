1a695d87a999d0aa00bec839868235dc50d17f44
suhyunsim <cnc1106@naver.com>
2021-03-23 23:09:51 +0900

    test: 차 이름을 입력 받고 전진, 멈춤, 생성 테스트를 추가

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -6,39 +6,54 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.strategy.MoveStrategy;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static racingcar.exception.Message.MAX_LENGTH_ERROR;
+import static racingcar.exception.Message.NULL_OR_EMPTY_ERROR;
 
 class CarTest {
+    private final static String carName = "test";
     Car car;
+
+    static Stream<Arguments> distances() {
+        return Stream.of(
+                Arguments.of(new MoveStrategy[]{() -> false, () -> false, () -> false}, new Car(carName, 0)),
+                Arguments.of(new MoveStrategy[]{() -> true, () -> true, () -> true}, new Car(carName, 3)),
+                Arguments.of(new MoveStrategy[]{() -> true, () -> false, () -> true}, new Car(carName, 2))
+        );
+    }
+
+    static Stream<Arguments> namingError() {
+        String wrongName = "abcdef";
+        return Stream.of(
+                arguments(null, NULL_OR_EMPTY_ERROR),
+                arguments("", NULL_OR_EMPTY_ERROR),
+                arguments(wrongName, MAX_LENGTH_ERROR)
+        );
+    }
 
     @BeforeEach
     void setCar() {
-        car = new Car();
+        car = new Car(carName);
     }
 
     @Test
     @DisplayName("전진 테스트")
     void moveTrue() {
-        assertThat(car.move(() -> true)).isEqualTo(new Car(1));
+        assertThat(car.move(() -> true)).isEqualTo(new Car(carName, 1));
     }
+
     @Test
     @DisplayName("멈춤 테스트")
     void moveFalse() {
-        assertThat(car.move(() -> false)).isEqualTo(new Car(0));
-    }
-
-
-    static Stream<Arguments> distances() {
-        return Stream.of(
-                Arguments.of(new MoveStrategy[] {() -> false, () -> false, () -> false}, new Car(0)),
-                Arguments.of(new MoveStrategy[] {() -> true, () -> true, () -> true}, new Car(3)),
-                Arguments.of(new MoveStrategy[] {() -> true, () -> false, () -> true}, new Car(2))
-        );
+        assertThat(car.move(() -> false)).isEqualTo(new Car(carName, 0));
     }
 
     @ParameterizedTest
@@ -49,5 +64,22 @@ class CarTest {
             car = car.move(moveStrategy);
         }
         assertEquals(expected, car);
+    }
+
+    @ParameterizedTest
+    @DisplayName("5자 이하의 이름을 가진 자동차 생성 테스트")
+    @ValueSource(strings = {"pobi", "crong", "honux"})
+    void createCarWithName(String name) {
+        car = new Car(name);
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @ParameterizedTest
+    @DisplayName("공백, 5자 초과 시 예외 리턴 테스트")
+    @MethodSource("namingError")
+    void createCarWithWrongName(String name, String errorMessage) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Car(name))
+                .withMessage(errorMessage);
     }
 }
