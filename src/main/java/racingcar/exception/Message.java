aab9dbe5900cdd05bbb37be3c4d83f93533f8a5c
suhyunsim <cnc1106@naver.com>
2021-03-24 00:10:02 +0900

    refactor: 메세지 이름을 더 명확하도록 수정

--- src/main/java/racingcar/exception/Message.java
+++ src/main/java/racingcar/exception/Message.java
@@ -1,7 +1,7 @@
 package racingcar.exception;
 
 public class Message {
-    public static final String BAD_CARS_INPUT = "경주에 참여하는 차가 없습니다.";
+    public static final String EMPTY_CARS_ERROR = "경주에 참여하는 차가 없습니다.";
     public static final String MAX_LENGTH_ERROR = "자동차 이름은 5자를 초과할 수 없습니다.";
     public static final String NULL_OR_EMPTY_ERROR = "자동차 이름은 공백일 수 없습니다.";
 }
