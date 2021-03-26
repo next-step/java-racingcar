93660c1203ca6a280d8e55b2f0b11225fec3fb0f
shpark <shpark@shparkui-MacBookAir.local>
2021-03-26 23:26:35 +0900

    feat: add moveCount field & move method

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -4,28 +4,11 @@ import java.util.Random;
 
 public class RacingCar {
 
-    private int numberOfCar;
-    private int numberOfTrial;
+    Random random = new Random();
+    private int moveCount;
 
-    public void setInput(InputDto inputDto) {
-        this.numberOfCar = inputDto.getNumberOfCar();
-        this.numberOfTrial = inputDto.getNumberOfTrial();
+    public int move() {
+        moveCount += random.nextInt(10);
+        return moveCount;
     }
-
-    public int[][] getRandomValue() {
-        Random random = new Random();
-        int[][] results = new int[numberOfTrial][numberOfCar];
-
-        for (int i = 0; i < numberOfTrial; i++) {
-            for (int j = 0; j < numberOfCar; j++) {
-                results[i][j] = random.nextInt(10);
-
-                if (0 < i) {
-                    results[i][j] += results[i - 1][j];
-                }
-            }
-        }
-        return results;
-    }
-
 }
