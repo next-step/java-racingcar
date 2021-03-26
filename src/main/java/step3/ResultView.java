62acce18a5a4f0eea6abf110d185fe253302072c
shpark <shpark@shparkui-MacBookAir.local>
2021-03-26 23:27:14 +0900

    feat: add printStart, printRacintCar method

--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -4,16 +4,16 @@ public class ResultView {
 
     public ResultView() {}
 
-    public void printResult(int[][] results, InputDto inputDto) {
+    public void printStart() {
         System.out.println("\n실행 결과");
-
-        for (int i = 0; i < inputDto.getNumberOfTrial(); i++) {
-            for (int j = 0; j < inputDto.getNumberOfCar(); j++) {
-                int number = results[i][j];
-                String format = number > 0 ? "%" + number + "s" : "%s";
-                System.out.println(String.format(format, "-").replace(" ", "-"));
-            }
-            System.out.println();
-        }
     }
+
+    public void printRacingCar(int moveCount) {
+        while (moveCount > 0) {
+            System.out.print("-");
+            moveCount--;
+        }
+        System.out.println();
+    }
+
 }
