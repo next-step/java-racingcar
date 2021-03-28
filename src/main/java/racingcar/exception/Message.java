525de0b313ca3af5b3008dae7bdbcf3d6b4b03d4
suhyunsim <cnc1106@naver.com>
2021-03-28 16:09:54 +0900

    feat: 위치값 예외처리 메세지 추가

--- src/main/java/racingcar/exception/Message.java
+++ src/main/java/racingcar/exception/Message.java
@@ -5,4 +5,5 @@ public class Message {
     public static final String EMPTY_CARS_ERROR = "경주에 참여하는 차가 없습니다.";
     public static final String MAX_LENGTH_ERROR = "자동차 이름은 5자를 초과할 수 없습니다.";
     public static final String NULL_OR_EMPTY_ERROR = "자동차 이름은 공백일 수 없습니다.";
+    public static final String POSITION_VALUE_ERROR = "위치 값은 0 이상이어야 합니다.";
 }
