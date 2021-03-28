2446a4119ba746cea0f7b9d41a2d56f717566b98
suhyunsim <cnc1106@naver.com>
2021-03-28 16:09:04 +0900

    feat: 위치를 관리하는 Position 객체 생성

---
+++ src/main/java/racingcar/domain/Position.java
@@ -0,0 +1,40 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+import static racingcar.exception.Message.POSITION_VALUE_ERROR;
+
+public class Position {
+
+    public static final int DEFAULT = 0;
+
+    private final int position;
+
+    public Position() {
+        this(DEFAULT);
+    }
+
+    public Position(int position) {
+        if (position < DEFAULT) {
+            throw new IllegalArgumentException(POSITION_VALUE_ERROR);
+        }
+        this.position = position;
+    }
+
+    public Position increase() {
+        return new Position(this.position + 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
