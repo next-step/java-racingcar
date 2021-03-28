8c951bbe6bd032a4f97e4048daeb63243456eee4
suhyunsim <cnc1106@naver.com>
2021-03-29 02:31:59 +0900

    test: RacingGameTest에 필요 없는 테스트 삭제

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -10,15 +10,12 @@ import racingcar.dto.Round;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
-import static racingcar.exception.Message.EMPTY_CARS_ERROR;
 
 class RacingGameTest {
 
@@ -36,49 +33,24 @@ class RacingGameTest {
     @Test
     @DisplayName("생성자 테스트")
     void createRacingGame() {
-        racingGame = new RacingGame(Arrays.asList(carNames), roundCount);
-        assertNotNull(racingGame);
-    }
 
-    @Test
-    @DisplayName("생성자 예외 테스트")
-    void createEmptyRacingGame() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new RacingGame(Collections.emptyList(), roundCount)).withMessage(EMPTY_CARS_ERROR);
+        racingGame = new RacingGame(Arrays.asList(carNames), roundCount);
+
+        assertNotNull(racingGame);
     }
 
     @ParameterizedTest
     @DisplayName("모든 차가 모든 라운드에 멈춘 게임 테스트")
     @MethodSource("carList")
     void runRoundAllStop(List<Car> carList) {
+
         List<Round> expected = new ArrayList<>(roundCount);
         for (int i = 0; i < roundCount; i++) {
             expected.add(new Round(Cars.of(carList)));
         }
 
         List<Round> actual = new RacingGame(Arrays.asList(carNames), expected.size()).runGame(() -> false);
+
         assertEquals(expected, actual);
     }
-
-    @Test
-    @DisplayName("모든 차가 모든 라운드에 전진한 게임 테스트")
-    void runRoundAllMove() {
-        List<Round> expected = new ArrayList<>();
-        for (int i = 0; i < roundCount; i++) {
-            Round round = new Round(getCars(i + 1));
-            expected.add(round);
-        }
-        List<Round> actual = new RacingGame(Arrays.asList(carNames), expected.size()).runGame(() -> true);
-        assertEquals(expected, actual);
-    }
-
-    private Cars getCars(int position) {
-        List<Car> cars = new ArrayList<>();
-        for (String carName : carNames) {
-            Car car = new Car(carName, position);
-            cars.add(car);
-        }
-        return Cars.of(cars);
-    }
-
 }
