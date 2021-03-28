f1aac5ac1dc3a6f53c1021c575c84d719f75b24b
suhyunsim <cnc1106@naver.com>
2021-03-29 02:32:31 +0900

    refactor: View에 수정사항 반영

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -10,44 +10,29 @@ public class ResultView {
     private static final String RESULT_MESSAGE = "실행 결과";
     private static final String RACE_TRACK = "-";
     private static final String CAR_NAME_DELIMITER = ": ";
-    private static final String WINNER_DELIMITER = ", ";
-
-    private List<Round> rounds;
-
-    public ResultView(List<Round> rounds) {
-        this.rounds = rounds;
-    }
 
     public static void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
     }
 
-    public void printGameResult() {
+    public static void printGameResult(List<Round> rounds) {
         for (Round round : rounds) {
             printRound(round);
         }
-        printGameWinners(rounds);
     }
 
-    public void printGameWinners(List<Round> rounds) {
-        String winners = winnersToString(rounds);
-        System.out.println(winners + "가 최종 우승했습니다.");
+    public static void printGameWinners(String winnersList) {
+        System.out.println(winnersList + "가 최종 우승했습니다.");
     }
 
-    private String winnersToString(List<Round> rounds) {
-        Map<String, Integer> finalRound = Round.roundResult(rounds.get(rounds.size() - 1));
-        List<String> winnerList = Round.winners(finalRound);
-        return String.join(WINNER_DELIMITER, winnerList);
-    }
-
-    private void printRound(Round round) {
+    public static void printRound(Round round) {
         for (Map.Entry<String, Integer> stringIntegerEntry : Round.roundResult(round).entrySet()) {
             printTrack(stringIntegerEntry);
         }
         System.out.println();
     }
 
-    private void printTrack(Map.Entry<String, Integer> roundResult) {
+    public static void printTrack(Map.Entry<String, Integer> roundResult) {
         StringBuilder sb = new StringBuilder();
         sb.append(roundResult.getKey()).append(CAR_NAME_DELIMITER);
         for (int i = 0; i < roundResult.getValue(); i++) {
