3d1a163aa1f3b1bd58198ce5062572c58f5d60c6
shpark <shpark@shparkui-MacBookAir.local>
2021-03-30 00:10:26 +0900

    feat: update move method

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -8,8 +8,10 @@ public class RacingCar {
     private int moveCount;
 
     public int move() {
-        int temp = random.nextInt(10);
-        if (temp >= 4) this.moveCount += temp;
+        int randomNumber = random.nextInt(10);
+        if (randomNumber >= 4) {
+            this.moveCount ++;
+        }
         return this.moveCount;
     }
 }
