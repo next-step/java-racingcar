a3c8772c81e4648e2f17976e3885ab89f8bea71e
shpark <shpark@shparkui-MacBookAir.local>
2021-03-30 00:18:28 +0900

    feat: add carName field

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class RacingCar {
 
     private static Random random = new Random();
+    private String carName;
     private int moveCount;
 
     public int move() {
