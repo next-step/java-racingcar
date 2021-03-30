9bb94d9c49a3e0f3c8b4ec7f906428efded5bd6d
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-30 23:28:23 +0900

    test : RacingGame 프로덕션 코드의 리펙토링으로 인한 테스트 코드 수정

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import racingcar.domain.Location;
+import racingcar.domain.Owner;
 import racingcar.domain.RacingCar;
 import racingcar.domain.RacingCars;
-import racingcar.utils.GameSet;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,14 +18,15 @@ public class RacingGameTest {
 
     @DisplayName("주어진 횟수 동안 pobi, crong, honux의 자동차는 전진 또는 멈출 수 있다.")
     @ParameterizedTest
-    @CsvSource(value = {"pobi:1", "crong:2 ", "honux:5"}, delimiter = ':')
+    @CsvSource(value = {"pobi:1", "crong:3 ", "honux:5"}, delimiter = ':')
     void cars_can_moves_given_number_test(String owners, int count) {
 
-        GameSet gameSet = new GameSet(owners, count);
-        RacingGame racingGame = new RacingGame();
+        RacingCar racingCar = new RacingCar(new Owner(owners), new Location(0));
+        for (int i = 0; i < count; i++) {
+            racingCar.move(4);
+        }
 
-        List<RacingCar> list = racingGame.settingRacingCars(gameSet.getOwners());
-        racingGame.gameStart(list, count);
+        assertThat(racingCar.getLocation()).isEqualTo(count);
 
     }
 
@@ -33,45 +35,31 @@ public class RacingGameTest {
     @Test
     void winner_one() {
 
-        GameSet gameSet = new GameSet("pobi", 1);
-        RacingGame racingGame = new RacingGame();
+        RacingCar pobiCar = new RacingCar(new Owner("pobi"), new Location(0));
 
-        List<RacingCar> list = racingGame.settingRacingCars(gameSet.getOwners());
-
-        RacingCar pobiCar = list.get(0);
         pobiCar.move(5);
-
         List<RacingCar> winnerList = Arrays.asList(pobiCar);
 
         RacingCars racingCars = new RacingCars(winnerList);
-
         assertThat(racingCars.findWinners(winnerList)).contains(pobiCar);
-        
+
 
     }
 
-    @DisplayName("우승자가 2명일 경우와 주어진 라운드가 1인 경우")
+    @DisplayName("우승자가 2명일 경우와 주어진 라운드가 3인 경우")
     @Test
     void winner_two() {
 
-        GameSet gameSet = new GameSet("pobi,crong", 1);
-        RacingGame racingGame = new RacingGame();
+        RacingCar pobiCar = new RacingCar(new Owner("pobi"), new Location(0));
+        RacingCar crongCar = new RacingCar(new Owner("crong"), new Location(0));
 
-        List<RacingCar> list = racingGame.settingRacingCars(gameSet.getOwners());
-
-        RacingCar pobiCar = list.get(0);
-        RacingCar crongCar = list.get(1);
-
-        pobiCar.move(5);
-        crongCar.move(5);
+        for (int i = 0; i < 3; i++) {
+            pobiCar.move(5);
+            crongCar.move(5);
+        }
 
         List<RacingCar> winnerList = Arrays.asList(pobiCar, crongCar);
-
-        assertThat(1).isEqualTo(pobiCar.getLocation());
-        assertThat(1).isEqualTo(crongCar.getLocation());
-
         RacingCars racingCars = new RacingCars(winnerList);
-
         assertThat(racingCars.findWinners(winnerList)).contains(pobiCar, crongCar);
     }
 
