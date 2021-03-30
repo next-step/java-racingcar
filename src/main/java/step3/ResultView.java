9af86df400cd6c153cb2e27a230dfc2ff462d402
shpark <shpark@shparkui-MacBookAir.local>
2021-03-31 00:46:59 +0900

    feat: update printRacingCar method and add getMaxMoveCount, getWinnerList, lastPrint methods

--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -1,5 +1,10 @@
 package step3;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ResultView {
 
     public ResultView() {}
@@ -8,7 +13,8 @@ public class ResultView {
         System.out.println("\n실행 결과");
     }
 
-    public void printRacingCar(int moveCount) {
+    public void printRacingCar(String carName, int moveCount) {
+        System.out.print(carName + " : ");
         while (moveCount > 0) {
             System.out.print("-");
             moveCount--;
@@ -16,4 +22,27 @@ public class ResultView {
         System.out.println();
     }
 
+    private int getMaxMoveCount(ArrayList<RacingCar> carList) {
+        return carList
+                .stream()
+                .max(Comparator.comparing(RacingCar::getMoveCount))
+                .get().getMoveCount();
+    }
+
+    private String getWinnerList(ArrayList<RacingCar> carList) {
+        int maxMoveCount = getMaxMoveCount(carList);
+
+        List<String> winnerList = carList
+                .stream()
+                .filter(racingCar -> racingCar.getMoveCount() == maxMoveCount)
+                .collect(Collectors.toList())
+                .stream().map(RacingCar::getCarName).collect(Collectors.toList());
+
+        return String.join(", ", winnerList);
+    }
+
+    public void lastPrint(ArrayList<RacingCar> carList) {
+        String names = getWinnerList(carList);
+        System.out.println(names + "가 최종 우승했습니다.");
+    }
 }
