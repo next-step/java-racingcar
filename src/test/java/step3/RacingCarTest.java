f1c746d6d09e91e001118eaf652b0cb891b17f05
shpark <shpark@shparkui-MacBookAir.local>
2021-03-31 00:47:37 +0900

    test: update move test and add checkCarName test

--- src/test/java/step3/RacingCarTest.java
+++ src/test/java/step3/RacingCarTest.java
@@ -10,11 +10,19 @@ class RacingCarTest {
     @Test
     @DisplayName("move의 결과값 확인하기")
     void move() {
-        RacingCar racingCar = new RacingCar();
+        RacingCar racingCar = new RacingCar("Ron");
 
         int moveCount = racingCar.move();
 
-        assertThat(moveCount).isBetween(0, 10);
+        assertThat(moveCount).isBetween(0, 1);
+        assertThat(moveCount).isEqualTo(racingCar.getMoveCount());
     }
 
+    @Test
+    @DisplayName("차 이름 확인하기")
+    void checkCarName() {
+        RacingCar racingCar = new RacingCar("Hermione");
+
+        assertThat(racingCar.getCarName()).isEqualTo("Hermione");
+    }
 }
