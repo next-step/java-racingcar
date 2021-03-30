18dcd309ddab3f7fc846ce0dc12a2ecab3c0489f
LeeJeongSeok <ljs0429777@gmail.com>
2021-03-30 23:27:29 +0900

    test : 기본 자로형으로 선언된 owner를 클래스로 변형하면서 그에 따른 테스트 코드 추가

--- src/test/java/racingcar/OwnerTest.java
+++ src/test/java/racingcar/OwnerTest.java
@@ -1,15 +1,35 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Owner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class OwnerTest {
 
+    @DisplayName("Owner 객체가 생성이 잘 되는지")
     @Test
     void create() {
         Owner owner = new Owner("pobi");
         assertThat(owner).isNotNull();
     }
+
+    @DisplayName("Owner의 이름이 빈값인 경우")
+    @Test
+    void null_check() {
+        assertThatThrownBy(() -> {
+            new Owner("");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("Owner의 이름이 5글자가 넘는 경우")
+    @Test
+    void over_length() {
+        assertThatThrownBy(() -> {
+            new Owner("pobipobi");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
