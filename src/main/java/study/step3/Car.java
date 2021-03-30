62078f04f21f797784415abbeb219f8f50396f4a
0hun <whdudgns2654@gmail.com>
2021-03-30 22:55:19 +0900

    docs(feat): 자동차의 현재 위치를 저장하기 위한 클래스 추가
    자동차의 현재 위치를 저장하기 위한 클래스 추가:

---
+++ src/main/java/study/step3/Car.java
@@ -0,0 +1,15 @@
+package study.step3;
+
+public class Car {
+
+  private int position;
+
+  public Car(int position) {
+    this.position = position;
+  }
+
+  public int curPosition() {
+    return position;
+  }
+
+}
