95874f6d1309fb63ba2c6faa53752d51976c9434
0hun <whdudgns2654@gmail.com>
2021-03-30 22:54:14 +0900

    docs(test): 자동차의 현재 위치를 테스트
    자동차의 현재 위치를 테스트:
    - 자동차의 현재 위치를 입력하여 현재 위치가 맞는지 확인하는 테스트

---
+++ src/test/java/study/step3/CarTest.java
@@ -0,0 +1,23 @@
+package study.step3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+  @DisplayName("자동차의 위치 입력")
+  @Test
+  void stringCalculate() {
+    // given
+    int position = 2;
+
+    // when
+    Car car = new Car(position);
+
+    // then
+    assertThat(car.curPosition()).isEqualTo(2);
+  }
+
+}
