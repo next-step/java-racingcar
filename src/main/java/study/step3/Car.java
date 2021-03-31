84f273a8695b08b39fd0fbdd6b88b3a3ca943b0b
0hun <whdudgns2654@gmail.com>
2021-03-31 23:18:55 +0900

    docs(feature): 자동차의 위치를 이동시키기는 메소드 추가
    자동차의 위치를 이동시키기는 메소드 추가:
    - 자동차의 위치를 이동저장하는 메소드 추가
    - 자동차 클래스를 생성하기 위한 기본 생성자 추가

--- src/main/java/study/step3/Car.java
+++ src/main/java/study/step3/Car.java
@@ -4,12 +4,18 @@ public class Car {
 
   private int position;
 
+  public Car() {};
+
   public Car(int position) {
     this.position = position;
   }
 
   public int curPosition() {
     return position;
+  }
+
+  public void move(int position) {
+    this.position = position;
   }
 
 }
