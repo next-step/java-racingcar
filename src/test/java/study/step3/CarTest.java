3454c6499d02ed77ee812b59c6ef6399573d699a
0hun <whdudgns2654@gmail.com>
2021-03-31 23:17:36 +0900

    docs(test): 자동차의 위치를 이동하는 테스트
    자동차의 위치를 이동하는 테스트:
    - 자동차를 생성하고 위치를 입력하여 입력한 위치만큼 이동했는지 확인하는 테스트 추가
    -자동차의 처음 위치를 입력하여 테스트하는 테스트 이름 수정

--- src/test/java/study/step3/CarTest.java
+++ src/test/java/study/step3/CarTest.java
@@ -9,12 +9,26 @@ public class CarTest {
 
   @DisplayName("자동차의 위치 입력")
   @Test
-  void stringCalculate() {
+  void carInitPositionTest() {
     // given
     int position = 2;
 
     // when
     Car car = new Car(position);
+
+    // then
+    assertThat(car.curPosition()).isEqualTo(2);
+  }
+
+  @DisplayName("자동차의 위치 이동")
+  @Test
+  void catMove() {
+    // given
+    int position = 2;
+
+    // when
+    Car car = new Car();
+    car.move(position);
 
     // then
     assertThat(car.curPosition()).isEqualTo(2);
