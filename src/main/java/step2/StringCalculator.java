205f9d82cb823736645d8c91835f2ce61b13d44a
shpark <shpark@shparkui-MacBookAir.local>
2021-04-02 22:51:16 +0900

    fix: fix conflict

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -7,6 +7,7 @@ public class StringCalculator {
 
     private static final String DELIMITER = " ";
     private static final Set<String> OPERATORS = new HashSet<>();
+    private String text;
     private String[] textArray;
 
     public void setText(String text) {
