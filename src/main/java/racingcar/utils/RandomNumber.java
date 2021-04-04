654cafe3cb559894c053033927be30bf9f18c847
LeeJeongSeok <ljs0429777@gmail.com>
2021-04-04 17:58:50 +0900

    refactor : enum 클래스의 추가로 상수 부분 리펙토링

--- src/main/java/racingcar/utils/RandomNumber.java
+++ src/main/java/racingcar/utils/RandomNumber.java
@@ -1,14 +1,15 @@
 package racingcar.utils;
 
+import racingcar.command.NumberType;
+
 import java.util.Random;
 
 public class RandomNumber {
 
-    private static final int MAXIMUM_NUMBER = 10;
     private static Random random = new Random();
 
     public static int random() {
-        return random.nextInt(MAXIMUM_NUMBER);
+        return random.nextInt(NumberType.TEN.value());
     }
 
 }
