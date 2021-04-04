21a14b2d23d7f9a2b6d91e2922c32f0c2dd6d78c
LeeJeongSeok <ljs0429777@gmail.com>
2021-04-04 19:16:00 +0900

    refactor : Location 값이 10이상인 경우에 대한 예외 코드 추가

--- src/main/java/racingcar/domain/Location.java
+++ src/main/java/racingcar/domain/Location.java
@@ -9,8 +9,12 @@ public class Location {
     private int location;
 
     public Location(int location) {
-        if (location < NumberType.ZERO.value()) {
+        if (location < NumberType.ZERO.value())  {
             throw new IllegalArgumentException("Location의 값은 0보다 작은 수가 들어올 수 없습니다.");
+        }
+
+        if (location >= NumberType.TEN.value()) {
+            throw new IllegalArgumentException("Location의 값은 10 이상일 순 없습니다.");
         }
         this.location = location;
     }
