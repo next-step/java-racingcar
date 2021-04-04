69e671d68923f201ccb8cd4d6247fac6d705dc23
LeeJeongSeok <ljs0429777@gmail.com>
2021-04-04 17:55:19 +0900

    feat : 상수와 관련된 enum 클래스 추가

--- src/main/java/racingcar/command/NumberType.java
+++ src/main/java/racingcar/command/NumberType.java
@@ -1,2 +1,16 @@
-package racingcar.command;public enum NumberType {
+package racingcar.command;
+
+public enum NumberType {
+
+    ZERO(0), ONE(1), FOUR(4), FIVE(5), TEN(10);
+
+    NumberType(int value) {
+        this.value = value;
+    }
+
+    private final int value;
+
+    public int value() {
+        return value;
+    }
 }
