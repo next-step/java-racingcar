4e382a7b4a51946839f06b88c5644852e132b87e
kangmin-gi <bming0227@gmail.com>
2021-04-05 01:22:05 +0900

    [feat] Racing CarNameList 추가
    Racing 객체 CarList에 Name 추가

--- src/main/java/racingcar/racing/Racing.java
+++ src/main/java/racingcar/racing/Racing.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import racingcar.car.Car;
 import racingcar.car.strategy.MovingStrategy;
 import racingcar.exception.TryMovingCarException;
+import racingcar.result.CarResultView;
 
 public class Racing {
 
@@ -15,8 +16,9 @@ public class Racing {
   public Racing(RacingRule racingRule, MovingStrategy movingStrategy) {
     this.cars = new ArrayList<>();
     int carCount = racingRule.getCarCount();
+    List<String> carList = racingRule.getCarNameList();
     for (int i = 0; i < carCount; ++i) {
-      cars.add(new Car(movingStrategy));
+      cars.add(new Car(movingStrategy, carList.get(i)));
     }
     this.remainTryCount = racingRule.getTryCount();
   }
@@ -40,9 +42,20 @@ public class Racing {
     return this.remainTryCount;
   }
 
-  public List<Integer> currentSituation() {
+  public List<CarResultView> currentSituation() {
     return this.cars.stream()
+        .map(CarResultView::new)
+        .collect(Collectors.toList());
+  }
+
+  public List<String> getWinnerCarName() {
+    int maxPosition = this.cars.stream()
         .map(Car::getPosition)
+        .max(Integer::compare)
+        .orElseThrow(() -> new IllegalArgumentException("자동차 리스트가 비어 있습니다."));
+    return this.cars.stream()
+        .filter(car -> car.getPosition() == maxPosition)
+        .map(Car::getCarName)
         .collect(Collectors.toList());
   }
 }
