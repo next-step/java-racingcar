9ca01cffe404cb50cdb397da36372bd3b97fe454
0hun <whdudgns2654@gmail.com>
2021-04-07 00:25:11 +0900

    docs(feature): 자동차의 위치를 입력받아 입력 받은 크기 만큼 문자열 경로 생성하는 메소드 추가
    자동차의 위치를 입력받아 입력 받은 크기 만큼 문자열 경로 생성하는 메소드 추가
    - 자동차의 입력받은 위치가 4이상일 경우에만 입력 받은 위치만큼 StringBuilder를 이용하여 문자열 경로 생성 후
    이동 경로를 저장하는 변수에 저장

--- src/main/java/study/step3/Car.java
+++ src/main/java/study/step3/Car.java
@@ -1,8 +1,12 @@
 package study.step3;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Car {
 
   private int position;
+
+  private String path;
 
   public Car() {};
 
@@ -14,18 +18,31 @@ public class Car {
     return position;
   }
 
+  public String curPath() {
+    return this.path;
+  }
+
   public void move(int position) {
     if (isMove(position)) {
       this.position = position;
+      makePath(position);
     }
   }
 
   public boolean isMove(int randomPosition) {
-    if (randomPosition > 4) {
+    if (randomPosition > 3) {
       return true;
     }
 
     return false;
+  }
+
+  public void makePath(int position) {
+    this.path = StringUtils.isEmpty(this.path) ? "" : this.path;
+
+    for (int i = 0; i < position; i++) {
+      this.path = path + "-";
+    }
   }
 
 }
