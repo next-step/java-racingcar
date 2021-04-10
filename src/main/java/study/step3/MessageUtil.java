2ce62d6f60dc60e25070d3e33d73ef063425d614
0hun <whdudgns2654@gmail.com>
2021-04-10 17:49:38 +0900

    feat (feature) : 입력 및 출력에 관한 메세지 유틸 클래스 추가
    입력 및 출력에 관한 메세지 유틸 클래스 추가
    - 메세지를 한 곳의 클래스에서 관리하게 하도록 추가

---
+++ src/main/java/study/step3/MessageUtil.java
@@ -0,0 +1,8 @@
+package study.step3;
+
+public class MessageUtil {
+
+  public static final String carNumberMessage = "자동차 대수는 몇 대 인가요?";
+  public static final String carTimesMessage = "시도할 회수는 몇 회 인가요?";
+  public static final String carRacingEndMessage = "실행결과";
+}
