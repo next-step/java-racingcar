964ae9a36e1909f1b508b459dbaf7e821ab9336b
miha.kim <miha.kim@wadiz.kr>
2021-04-11 01:12:01 +0900

    test 테스트추가

--- src/test/java/step4/RacingTest.java
+++ src/test/java/step4/RacingTest.java
@@ -1,15 +1,17 @@
 package step4;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static step4.InputPrint.getSplitInputs;
 
-class InputPrintTest {
+class RacingTest {
 
     @Test
     public void 이름입력_테스트() {
@@ -35,6 +37,30 @@ class InputPrintTest {
     public void game() {
         Racing racing = new Racing(new String[]{"iu", "moon", "shine", "v"}, 5);
 
+    }
+
+    @Test
+    @DisplayName("0에서 9 사이에서 random 값을 구한다")
+    void 랜덤조건확인() {
+        int num = new RacingRule().castDice();
+        assertThat(num).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("자동차는 전진 또는 멈출 수 있다")
+    void 자동차_전진() {
+        Car car = new Car("moon");
+        new RacingRule().go(car);
+        assertThat(car.getPosition()).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("자동차는 4 이상일때만 전진한다.")
+    @CsvSource(value = {"4,1", "3,0", "5,1"}, delimiter = ',')
+    void 자동차_전진(int dice, int expected) {
+        Car car = new Car("IU");
+        new RacingRule().go(car, dice);
+        assertThat(car.getPosition()).isEqualTo(expected);
     }
 
 }
