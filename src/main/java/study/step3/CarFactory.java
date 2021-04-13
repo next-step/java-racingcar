7eb6d39d7efd89e66b10fc30bd4862c3d4fcc82e
0hun <whdudgns2654@gmail.com>
2021-04-13 21:17:32 +0900

    chore (maintain) : 자동차 입력 갯수를 확인 하는 부분 중복 제거
    자동차 입력 갯수를 확인 하는 부분 중복 제거
    - inputView와 CarFactory에서 자동차 입력 갯수를 확인하는 부분이
    중복되어 inputView에서 확인했으면, 다른 곳에서 확인하지 않도록 수정

--- src/main/java/study/step3/CarFactory.java
+++ src/main/java/study/step3/CarFactory.java
@@ -5,14 +5,7 @@ import java.util.List;
 
 public class CarFactory {
 
-  private static final int CAR_NUMBER_CHECK = 0;
-
   public static List<Car> makeCar(int carNumber) {
-
-    if (carNumber == CAR_NUMBER_CHECK) {
-      throw new IllegalArgumentException("0보다 큰 값을 입력해야합니다.");
-    }
-
     List<Car> cars = new ArrayList<>();
 
     for (int i = 0; i < carNumber; i++) {
