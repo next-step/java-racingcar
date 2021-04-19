585c713ddac043cada2807595076fecb484332dc
0hun <whdudgns2654@gmail.com>
2021-04-20 02:43:11 +0900

    test : 우승자 찾는 실패 테스트 추가
    우승자 찾는 실패 테스트 추가
    - 자동차 객체가 완주하지 못했을 때, 우승자를 못찾는지 테스트 추가

--- src/test/java/study/step3/RecordTest.java
+++ src/test/java/study/step3/RecordTest.java
@@ -40,4 +40,20 @@ public class RecordTest {
     assertThat(winners).isEqualTo(names[0]);
   }
 
+  @DisplayName("자동차 이름과 반복횟수를 입력하고 완주를 못했을 때, 우승자를 못찾는지 테스트")
+  @Test
+  void findWinnersFail() {
+    // given
+    int times = 4;
+    String[] names = new String[]{"pobi", "crong", "honux"};
+    List<Car> carList = Arrays.asList(new Car(0, names[0]), new Car(3, names[1]));
+    Record record = new Record(times);
+
+    // when
+    String winners = record.findWinners(carList);
+
+    // then
+    assertThat("".equals(winners)).isTrue();
+  }
+
 }
