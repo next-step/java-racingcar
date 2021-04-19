11dd497f4b40b4d80387d16737118c2486f8ad81
0hun <whdudgns2654@gmail.com>
2021-04-20 01:09:41 +0900

    refactor : 자동차 이름 입력 함수 추가
    자동차 이름 입력 함수 추가
    - 자동차 입력 메세지 출력
    - 입력한 자동차 이름 validation

--- src/main/java/study/step3/view/InputView.java
+++ src/main/java/study/step3/view/InputView.java
@@ -12,7 +12,9 @@ public class InputView {
 
   public static int inputCarNumber() {
     System.out.println(MessageUtil.carNumberMessage);
+
     int carNumber = SCANNER.nextInt();
+
     Validator.validatorNumber(carNumber);
 
     return carNumber;
@@ -20,10 +22,24 @@ public class InputView {
 
   public static int inputTimes() {
     System.out.println(MessageUtil.carTimesMessage);
+
     int times = SCANNER.nextInt();
+
     Validator.validatorNumber(times);
 
     return times;
+  }
+
+  public static String[] inputNames() {
+    System.out.println(MessageUtil.carNamesMessage);
+
+    String inputName = SCANNER.nextLine();
+
+    String[] names = inputName.split(",");
+
+    Validator.validatorNames(names);
+
+    return names;
   }
 
 }
