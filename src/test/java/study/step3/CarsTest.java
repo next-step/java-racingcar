fa7fb68b368e1b8751780f08186854bb743b62c6
0hun <whdudgns2654@gmail.com>
2021-04-19 22:11:58 +0900

    docs (documentation) : Cars.move 함수 테스트 추가
    Cars.move 함수 테스트 추가
    - 함수형 인터페이스를 이용하여 Cars.move 성공, 실패 테스트 추가

--- src/test/java/study/step3/CarsTest.java
+++ src/test/java/study/step3/CarsTest.java
@@ -4,39 +4,75 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import study.step3.domain.Car;
-import study.step3.domain.CarMovableStrategy;
 import study.step3.domain.Cars;
 import study.step3.domain.MovableStrategy;
 
 public class CarsTest {
 
-  List<Car> carList = new ArrayList<>();
 
-  int carNumber = 2;
+  public static Cars makeCars(int carNumber, int randomNumber) {
+    List<Car> carList = new ArrayList<>();
 
-  @BeforeEach
-  void createCarList() {
-    MovableStrategy movableStrategy = new CarMovableStrategy();
+    MovableStrategy movableStrategy = () -> randomNumber >= 4;
 
     for (int i = 0; i < carNumber; i++) {
       Car newCar = new Car(movableStrategy);
 
       carList.add(newCar);
     }
+
+    return new Cars(carList);
   }
 
   @DisplayName("입력한 숫자 수만큼의 car list를 Cars객체를 생성했을 때, cars의 car객체 숫자 테스트")
   @Test
   void createCars() {
+    //given
+    int carNumber = 2;
+    int randomNumber = (int) (Math.random() * 10 + 4);
+
     // when
-    Cars cars = new Cars(carList);
+    Cars cars = makeCars(carNumber, randomNumber);
 
     // then
     assertThat(cars.getCars().size()).isEqualTo(carNumber);
+  }
+
+  @DisplayName("랜덤 숫자를 4이상 입력했을 때, cars의 car가 움직이는지 테스트")
+  @Test
+  void moveCars_success() {
+    //given
+    int carNumber = 1;
+    int randomNumber = (int) (Math.random() * 10 + 4);
+    Cars cars = makeCars(carNumber, randomNumber);
+    Car car = new Car(1);
+
+    // when
+    cars.move();
+
+    // then
+    Car movedCar = cars.getCars().get(0);
+    assertThat(movedCar).isEqualTo(car);
+  }
+
+  @DisplayName("랜덤 숫자를 4미만 입력했을 때, cars의 car가 움직이는지 않는지 테스트")
+  @Test
+  void moveCars_fail() {
+    //given
+    int carNumber = 1;
+    int randomNumber = (int) (Math.random() + 3);
+    Cars cars = makeCars(carNumber, randomNumber);
+    Car car = new Car(0);
+
+    // when
+    cars.move();
+
+    // then
+    Car movedCar = cars.getCars().get(0);
+    assertThat(movedCar).isEqualTo(car);
   }
 
 }
