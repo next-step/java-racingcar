95df64a37c0646d371751018295c1df8fc7ba627
shpark <shpark@shparkui-MacBookAir.local>
2021-04-24 18:54:30 +0900

    feat: add RandomNoImpl

--- src/main/java/step5/domain/RandomNoImpl.java
+++ src/main/java/step5/domain/RandomNoImpl.java
@@ -2,9 +2,9 @@ package step5.domain;
 
 import java.util.Random;
 
-public class RandomNoImpl implements RandomNo{
+public class RandomNoImpl implements RandomNo {
 
-    private static Random random;
+    private static Random random = new Random();
 
     public boolean isMovable() {
         return 4 <= random.nextInt(10);
