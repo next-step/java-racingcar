22cc3fdf57726ed39893e6365423a2391230e3eb
정진우 <jwjung@upchain.kr>
2021-04-26 20:55:08 +0900

    feat: Set Collection 학습테스트 요구사항2 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,9 +2,13 @@ package study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -22,5 +26,11 @@ public class SetTest {
     void getNumbersSize() {
         int numbersSize = numbers.size();
         System.out.println(numbersSize);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void isContained_ShouldReturnTrueForNumbers(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
