5183b34890a2dd9c03f4ee57940231c9e8c5fe4c
정진우 <jwjung@upchain.kr>
2021-04-26 22:21:30 +0900

    feat: Set Collection 학습테스트 요구사항3 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -32,5 +33,13 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void isContained_ShouldReturnTrueForNumbers(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+        Boolean expectedValue = Boolean.valueOf(expected);
+        Boolean actualValue = numbers.contains(Integer.valueOf(input));
+        assertThat(actualValue.equals(expectedValue)).isTrue();
     }
 }
