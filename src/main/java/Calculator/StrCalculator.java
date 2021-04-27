391bd3f3458589a607e4416935daff82b226f11d
boggydoggy <zhd401@naver.com>
2021-04-27 15:39:40 +0900

    chore: add Calculator package and move StrCalculator.java

--- src/main/java/Calculator/StrCalculator.java
+++ src/main/java/Calculator/StrCalculator.java
@@ -1,3 +1,5 @@
+package Calculator;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
--- src/test/java/Calculator/StrCalculatorTest.java
+++ src/test/java/Calculator/StrCalculatorTest.java
@@ -1,3 +1,5 @@
+package Calculator;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
