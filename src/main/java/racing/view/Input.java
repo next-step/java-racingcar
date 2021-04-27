f0bc23e0788f359578cef008bc80e88f6a498e2f
Miot2J <ck96117@gmail.com>
2021-04-27 16:58:32 +0900

    feat: Input 기능 구현

---
+++ src/main/java/racing/view/Input.java
@@ -0,0 +1,15 @@
+package racing.view;
+
+import java.util.Scanner;
+
+public class Input {
+    public String makeCars() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public String count() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
