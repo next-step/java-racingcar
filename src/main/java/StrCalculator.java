83beac613b508464ea6669c1e4bbf2231f0e4de3
boggydoggy <zhd401@naver.com>
2021-04-27 11:26:05 +0900

    feat: add sub, multi method and test

--- src/main/java/StrCalculator.java
+++ src/main/java/StrCalculator.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class StrCalculator {
+import java.util.Arrays;
+
+public class StrCalculator {
+    String sum(String a, String b) {
+        int intA = Integer.parseInt(a), intB = Integer.parseInt(b);
+        int result = intA + intB;
+
+        return Integer.toString(result);
+    }
+
+    String sub(String a, String b) {
+        int intA = Integer.parseInt(a), intB = Integer.parseInt(b);
+        int result = intA - intB;
+
+        return Integer.toString(result);
+    }
+
+    String multi(String a, String b) {
+        int intA = Integer.parseInt(a), intB = Integer.parseInt(b);
+        int result = intA * intB;
+
+        return Integer.toString(result);
+    }
+
+    String div(String a, String b) {
+        int intA = Integer.parseInt(a), intB = Integer.parseInt(b);
+
+        if (intB == 0) {
+            throw new IllegalArgumentException("divided by 0.");
+        }
+        int result = intA / intB;
+
+        return Integer.toString(result);
+    }
+
+    void checkExpression(String expr) {
+        if (expr == null || expr.isEmpty()) {
+            throw new IllegalArgumentException("expression is empty");
+        }
+    }
+
+    String[] separation(String[] list) {
+        return Arrays.copyOfRange(list, 0, 3);
+    }
+
+    void calOneByOne(String[] subList) {
+
+    }
 }
--- src/test/java/StrCalculatorTest.java
+++ src/test/java/StrCalculatorTest.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class StrCalculatorTest {
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StrCalculatorTest {
+
+    @Test
+    void sumTest() {
+        StrCalculator cal = new StrCalculator();
+        String a = "1", b = "2";
+        String result = cal.sum(a, b);
+
+        assertThat(result).isEqualTo("3");
+    }
+    @Test
+    void subTest() {
+        StrCalculator cal = new StrCalculator();
+        String a = "1", b = "2";
+        String result = cal.sub(a, b);
+
+        assertThat(result).isEqualTo("-1");
+    }
+    @Test
+    void multiTest() {
+        StrCalculator cal = new StrCalculator();
+        String a = "2", b = "3";
+        String result = cal.multi(a, b);
+
+        assertThat(result).isEqualTo("6");
+    }
+    @Test
+    void divTest() {
+        StrCalculator cal = new StrCalculator();
+        String a = "12", b = "2";
+        String result = cal.div(a, b);
+
+        assertThat(result).isEqualTo("6");
+    }
+
+    @Test
+    void emptyExpressionTest() {
+        assertThatThrownBy(() -> {
+            StrCalculator cal = new StrCalculator();
+            String expr = "";
+            cal.checkExpression(expr);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("expression is empty");
+    }
+
+    @Test
+    void separationTest() {
+        String[] test = {"1", "+", "2", "+", "3"};
+        StrCalculator cal = new StrCalculator();
+
+        String [] subList = cal.separation(test);
+
+        assertThat(subList).containsExactly("1","+","2");
+    }
 }
