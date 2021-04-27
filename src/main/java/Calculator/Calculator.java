731a377bc13b2c0fcdd76c6ccd5a9d0815c7412a
h1un <h1unha@naver.com>
2021-04-27 15:50:05 +0900

    calculator

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,26 @@
+package Calculator;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+public class Calculator {
+
+    public static int calculate(String str) {
+
+        Queue<String> tokens = new ArrayDeque<>(Arrays.asList(str.split(" ")));
+
+        int accumulator = Integer.parseInt(tokens.remove());
+        while (!tokens.isEmpty()) {
+            Operator operator = Operator.getOperator(tokens.remove());
+            int rhs = Integer.parseInt(tokens.remove());
+
+            accumulator = operator.calculation.apply(accumulator, rhs);
+        }
+
+        return accumulator;
+    }
+
+
+
+}
---
+++ src/test/java/study/CalculatorTest.java
@@ -0,0 +1,52 @@
+package study;
+
+import Calculator.Calculator;
+import Calculator.Operator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void set() {
+        calculator = new Calculator();
+    }
+
+    //연산자 확인
+    @Test
+    void getOperator() {
+        assertThat(Operator.getOperator("+")).isEqualTo(Operator.ADDITION);
+        assertThat(Operator.getOperator("-")).isEqualTo(Operator.SUBTRACTION);
+        assertThat(Operator.getOperator("*")).isEqualTo(Operator.MULTIPLICATION);
+        assertThat(Operator.getOperator("/")).isEqualTo(Operator.DIVISION);
+
+    }
+
+    //지원하지 않는 연산자 예외 처리
+    @Test
+    void throwIllegalArgumentException() {
+        assertThatThrownBy(() -> Operator.getOperator("%")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void throwIllegalArgumentException2() {
+        String expression = "2 % 3";
+
+        assertThatThrownBy(() -> calculator.calculate(expression)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    //문자열 계산기 검사
+    @ParameterizedTest
+    @CsvSource({"1 + 2,3", "2 * 3 / 3,2", "-14 / 2,-7"
+    })
+    void calculateExpression(String input, int expected) {
+        assertThat(calculator.calculate(input)).isEqualTo(expected);
+    }
+}
