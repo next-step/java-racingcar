dcf9782a4f2fe39635cabaa2cbd3531a3b7b7032
zuge3927 <zuge3927@naver.com>
2021-04-30 08:42:42 +0900

    feat : 문자열 계산기 메인 코드 구현

---
+++ src/main/java/calculator/CalculMain.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class CalculMain {
+
+    public static void main(String[] args) throws IllegalAccessException {
+
+        Calcul calcul = new Calcul();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("input : ");
+        String input = scanner.nextLine();
+        String[] split = input.split(" ");
+
+        int result = Integer.parseInt(split[0]);
+
+        for (int i = 0; i < split.length; i++) {
+
+            if (split[i].equals("+")) {
+
+                result = calcul.add(result, Integer.parseInt(split[i + 1]));
+            } else if (split[i].equals("-")) {
+
+                result = calcul.sub(result, Integer.parseInt(split[i + 1]));
+            } else if (split[i].equals("*")) {
+
+                result = calcul.multi(result, Integer.parseInt(split[i + 1]));
+            } else if (split[i].equals("/")) {
+
+                result = calcul.division(result, Integer.parseInt(split[i + 1]));
+            }
+        }
+
+        calcul.nullCheck(input);
+        calcul.operCheck(input);
+
+        System.out.println("결과값 : " + result);
+    }
+}
