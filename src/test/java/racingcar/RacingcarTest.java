a6ce0112fc74b71ac08a8058a1358284eede1750
DESKTOP-JH4A8SV\ceo <siryugubin@naver.com>
2021-04-29 16:20:17 +0900

    feat : 자동차 이름 예외처리 테스트

--- src/test/java/racingcar/RacingcarTest.java
+++ src/test/java/racingcar/RacingcarTest.java
@@ -1,4 +1,22 @@
 package racingcar;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 public class RacingcarTest {
+    RacingCar racingCar = new RacingCar();
+    NonTestRacingCar nonTestRacingCar = new NonTestRacingCar();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"gyubin", "sangkoo", "kyeonghwan"})
+    void nameCheckTest(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            racingCar.nameCheck(input);
+        });
+    }
 }
