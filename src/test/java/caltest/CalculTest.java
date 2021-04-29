2e2b7a122fbb722f784a4bec52a29f9a280141ba
zuge3927 <zuge3927@naver.com>
2021-04-30 08:43:01 +0900

    feat : 문자열 계산기 테스트 코드 구현

---
+++ src/test/java/caltest/CalculTest.java
@@ -0,0 +1,56 @@
+package caltest;
+
+import calculator.Calcul;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculTest {
+
+    private Calcul calcul = new Calcul();
+
+    @Test
+    void addTest(){
+
+        assertThat(5).isEqualTo(calcul.add(2, 3));
+    }
+
+    @Test
+    void subTest(){
+
+        assertEquals(5, calcul.sub(8, 3));
+    }
+
+    @Test
+    void multiTest(){
+
+        assertEquals(6, calcul.multi(2, 3));
+    }
+
+    @Test
+    void divisionTest(){
+
+        assertEquals(2, calcul.division(6, 3));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"88", " "})
+    void nullCheckTest(String input) {
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            calcul.nullCheck(input);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void operCheckTest(String input) {
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            calcul.operCheck(input);
+        });
+    }
+}
