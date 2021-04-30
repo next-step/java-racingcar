8271bbef5bf6798aed25692dc8a4bb18e0a9af46
rnthd2 <rnthd2@naver.com>
2021-04-30 14:00:15 +0900

    feat(Cars): 자동차 게임의 우승자를 구하는 기능을 추가합니다
    사용자에게 입력받을 거리를 각 자동차의 distance(주행거리)와 비교하여 우승자를 리스트로 가져옴

--- src/main/java/Cars.java
+++ src/main/java/Cars.java
@@ -17,4 +17,9 @@ public class Cars {
     public boolean hasCar(Car car) {
         return cars.contains(car);
     }
+
+    //todo 요구조건에 따라 stream 사용 할 수 없어 수정필요
+    public List<Car> getWinningCar(int distance) {
+        return cars.stream().filter(car -> car.distance == distance).collect(Collectors.toList());
+    }
 }
--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -23,5 +23,29 @@ class CarsTest {
         assertTrue(cars.hasCar(car));
     }
 
-    
+    @Test
+    void getWinningCar(){
+        Car car1 = new Car("바나나차차");
+        Car car2 = new Car("딸기차차");
+        Car car3 = new Car("토마토차차");
+
+        car1.run();
+        car2.run();
+        car3.stop();
+
+        car1.run();
+        car2.run();
+        car3.stop();
+
+        cars.addCar(car3);
+        cars.addCar(car2);
+        cars.addCar(car1);
+
+        List<Car> winningCar = cars.getWinningCar(2);
+        assertTrue(winningCar.contains(car2));
+        assertTrue(winningCar.contains(car1));
+    }
+
+
+
 }
