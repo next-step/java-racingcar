0abb35dcc1af1769fa0f18eb4eb568f622d42855
yhback <yhback@upchain.kr>
2021-04-30 18:10:07 +0900

    -fix 변수네임수정 1.함수와 변수 이름이 같아 수정

--- src/main/java/racegame/RacingName.java
+++ src/main/java/racegame/RacingName.java
@@ -7,13 +7,13 @@ public class RacingName {
 
     String[] nameInput() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        boolean nameCheck = true;
+        boolean Check = true;
         String str = "";
-        while (nameCheck) {
+        while (Check) {
             Scanner scan = new Scanner(System.in);
             str = scan.nextLine();
             if (nameCheck(str)) {
-                nameCheck = false;
+                Check = false;
             }
         }
         String[] nameList = str.replaceAll(" ", "").split(",");
