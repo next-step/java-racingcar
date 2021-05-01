457e7226a8a9b4880c4ffd0664abf955ef615efb
rnthd2 <rnthd2@naver.com>
2021-05-01 23:55:31 +0900

    feat(Cars): 각 자동차는 사용자에게 입력받은 이름을 기준으로 생성하는 기능을 추가합니다
    각 자동차는 5자 이하의 이름이 있다.
    이름이 조건에(5자 이하의 이름) 맞지 않을 때, 해당 이름을 제외하고 자동차를 생성한다.
    이름 조건에 공백에 관한 내용은 없어 공백의 이름을 사용자가 입력하는 경우 공백인 이름의 자동차가 생성되어야한다.

--- src/main/java/Cars.java
+++ src/main/java/Cars.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 public class Cars {
-    List<Car> cars;
+    private List<Car> cars;
 
     public Cars() {
         cars = new ArrayList<>();
@@ -20,6 +20,20 @@ public class Cars {
 
     //todo 요구조건에 따라 stream 사용 할 수 없어 수정필요
     public List<Car> getWinningCar(int distance) {
-        return cars.stream().filter(car -> car.distance == distance).collect(Collectors.toList());
+        return cars.stream().filter(car -> car.matchDistance(distance)).collect(Collectors.toList());
+    }
+
+    //todo 요구조건에 따라 stream 사용 할 수 없어 수정필요
+    public Car getCar(String name) throws NoSuchElementException {
+        return cars.stream().filter(car -> car.matchName(name)).findFirst().orElseThrow(NoSuchElementException::new);
+    }
+
+    //todo ","쉼표로 나눈 이름으로 Car를 생성하고 Cars에 add한다
+    //todo 이름이 조건에(5자 이하의 이름) 맞지 않을 때, 해당 이름을 제외하고 자동차를 생성한다.
+    public void setCarsByNames(List<String> names) {
+        for (String name : names) {
+            Car car = Car.createCar(name);
+            cars.add(car);
+        }
     }
 }
--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -1,6 +1,8 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,8 +10,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName(value = "자동차 일급 컬렉션 테스트")
 class CarsTest {
-    Cars cars = Setup.cars;
-    Car car = Setup.car;
+    Cars cars;
+    Car car;
+
+    final String CAR_NAME = "바나나차";
+    final String CARS_NAMES = "바나나차차,딸기차차,토마토차차,바나나차차차";
+    final int CARS_SIZE = 3;
+
+    @BeforeEach
+    void setup(){
+        cars = new Cars();
+        car = Car.createCar(CAR_NAME);
+    }
 
     @Test
     void addCar() {
@@ -18,10 +30,28 @@ class CarsTest {
     }
 
     @Test
+    void getCar(){
+        cars.addCar(this.car);
+        Car car = cars.getCar(CAR_NAME);
+        assertTrue(car.matchName(CAR_NAME));
+    }
+
+    @Test
+    void setCarsByNames(){
+        List<String> nameArrays = Arrays.asList(CARS_NAMES.split(","));
+        cars.setCarsByNames(nameArrays);
+        long count = nameArrays.stream()
+                .map(name -> Car.createCar(name))
+                .filter(car -> !cars.hasCar(car))
+                .count();
+        assertTrue(count == CARS_SIZE);
+    }
+
+    @Test
     void getWinningCar(){
-        Car car1 = new Car("바나나차차");
-        Car car2 = new Car("딸기차차");
-        Car car3 = new Car("토마토차차");
+        Car car1 = Car.createCar("바나나차차");
+        Car car2 = Car.createCar("딸기차차");
+        Car car3 = Car.createCar("토마토차차");
 
         car1.run();
         car2.run();
@@ -31,14 +61,15 @@ class CarsTest {
         car2.run();
         car3.stop();
 
-        cars.addCar(car3);
-        cars.addCar(car2);
         cars.addCar(car1);
+        cars.addCar(car2);
+        cars.addCar(car3);
 
         List<Car> winningCar = cars.getWinningCar(2);
         assertTrue(winningCar.contains(car2));
         assertTrue(winningCar.contains(car1));
     }
+
 
 
 
