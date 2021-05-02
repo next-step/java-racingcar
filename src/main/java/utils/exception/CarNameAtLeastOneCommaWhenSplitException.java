8cbb36efb677f97d91b14bc3fb28ded6460e0b90
mskangg <funnycoding91@gmail.com>
2021-05-03 00:07:56 +0900

    refactor: Exception 정의한 클래스 안쓰는 생성자 제거

--- src/main/java/utils/exception/CarNameAtLeastOneCommaWhenSplitException.java
+++ src/main/java/utils/exception/CarNameAtLeastOneCommaWhenSplitException.java
@@ -4,8 +4,4 @@ public class CarNameAtLeastOneCommaWhenSplitException extends RuntimeException {
     public CarNameAtLeastOneCommaWhenSplitException() {
         super("입력시 (,)를 최소 1번 이상 입력하셔야 합니다.");
     }
-
-    public CarNameAtLeastOneCommaWhenSplitException(String message) {
-        super(message);
-    }
 }
--- src/main/java/utils/exception/CarNameExceedFiveCharacterException.java
+++ src/main/java/utils/exception/CarNameExceedFiveCharacterException.java
@@ -4,8 +4,4 @@ public class CarNameExceedFiveCharacterException extends RuntimeException{
     public CarNameExceedFiveCharacterException() {
         super("자동차 이름은 5글자 이하여야만 합니다.");
     }
-
-    public CarNameExceedFiveCharacterException(String message) {
-        super(message);
-    }
 }
