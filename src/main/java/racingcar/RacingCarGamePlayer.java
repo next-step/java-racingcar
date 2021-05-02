e3ed60294db7705f9ea1b38613df1e26e92dc1c6
iia <kha2464@gmail.com>
2021-05-02 19:09:41 +0900

    feat(RacingCarGamePlayer): 경주 스코어 안내

--- src/main/java/racingcar/RacingCarGamePlayer.java
+++ src/main/java/racingcar/RacingCarGamePlayer.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class RacingCarGamePlayer {
@@ -8,6 +9,9 @@ public class RacingCarGamePlayer {
 		String carNames = askCarNames(scan);
 		int roundCount = askRoundCount(scan);
 		scan.close();
+
+		RacingCarGame game = new RacingCarGame(carNames, roundCount);
+		printAllRoundScore(game);
 	}
 
 	private static String askCarNames(Scanner scan) {
@@ -18,5 +22,31 @@ public class RacingCarGamePlayer {
 	private static int askRoundCount(Scanner scan) {
 		System.out.println("시도할 횟수는 몇 회 인가요?");
 		return scan.nextInt();
+	}
+
+	private static void printAllRoundScore(RacingCarGame game) {
+		StringBuffer sb = new StringBuffer("");
+		sb.append("\n실행 결과");
+		for (Map<String, Integer> scoresOfRound : game.getAllRoundScore()) {
+			sb.append(toScoreInformation(scoresOfRound) + "\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static String toScoreInformation(Map<String, Integer> scoresOfRound) {
+		StringBuffer sb = new StringBuffer("");
+		String carNames[] = scoresOfRound.keySet().toArray(new String[0]);
+		for (int i = 0; i < carNames.length; i++) {
+			sb.append("\n" + carNames[i] + " : " + toDash(scoresOfRound.get(carNames[i])));
+		}
+		return sb.toString();
+	}
+
+	private static String toDash(int number) {
+		StringBuffer sb = new StringBuffer("");
+		for (int i = 0; i < number; i++) {
+			sb.append("-");
+		}
+		return sb.toString();
 	}
 }
