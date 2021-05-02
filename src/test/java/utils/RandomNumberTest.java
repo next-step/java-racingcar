8280fbb210236946ad6b1e98e8a512891b8e300d
mskangg <funnycoding91@gmail.com>
2021-05-02 11:45:54 +0900

    test: 0-9 까지 랜덤으로 생성되는지 추가

--- src/test/java/utils/RandomNumberTest.java
+++ src/test/java/utils/RandomNumberTest.java
@@ -1,17 +1,21 @@
 package utils;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class RandomNumberTest {
 
     @Test
-    @DisplayName("0-9까지 숫자 생성")
+    @DisplayName("0-9까지 숫자 랜덤 생성")
     void generate_random_number_0_to_9() {
-        int number = RandomNumber.generateNumber();
-        assertThat(number)
-                .isBetween(0, 9);
+        int number1 = RandomNumber.generateNumber();
+        int number2 = RandomNumber.generateNumber();
+        assertThat(number1).isBetween(0, 9);
+        assertThat(number2).isBetween(0, 9);
+        assertNotEquals(number1, number2);
     }
 }
