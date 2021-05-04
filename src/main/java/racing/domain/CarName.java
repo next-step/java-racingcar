a031dbf25834e243fd3bc89067b0f6df4bcb151f
조원진 <>
2021-05-04 17:32:10 +0900

    refactor(domain:CarName): add validate method
    validate 메소드를 생성하여 팩토리 메소드에서 검증하는 기능 분리

--- src/main/java/racing/domain/CarName.java
+++ src/main/java/racing/domain/CarName.java
@@ -1,21 +1,25 @@
 package racing.domain;
 
 public class CarName {
-    private static int MAX_LENGTH = 5;
-    private String name;
+    private static final int MAX_LENGTH = 5;
+    private final String name;
 
     private CarName(String name) {
         this.name = name;
     }
 
     public static CarName of(String name) {
+        validate(name);
+        return new CarName(name);
+    }
+
+    private static void validate(String name) {
         if(name.trim().isEmpty()){
-            throw new IllegalArgumentException("차 이름은 empty 또는 공백일 수 없습니다.");
+            throw new IllegalArgumentException("차 이름은 공백일 수 없습니다.");
         }
         if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("차 이름이 5자보다 깁니다.");
         }
-        return new CarName(name);
     }
 
     public String getName() {
