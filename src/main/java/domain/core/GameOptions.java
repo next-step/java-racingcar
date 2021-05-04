8e1209836f6654abe765a65ea2b21f25f601f148
mskangg <funnycoding91@gmail.com>
2021-05-04 22:43:56 +0900

    feat: GameOptions 상수 클래스 추가
    - RacingCarGame을 진행하면서 필요한 상수 클래스 작성
    - 전진 or 정지 정하는 기준
    - 최소 랜덤값
    - 최대 랜덤값
    - 자동차 이름 최대 길이
    - 이동거리 출력 글자

---
+++ src/main/java/domain/core/GameOptions.java
@@ -0,0 +1,9 @@
+package domain.core;
+
+public class GameOptions {
+    public static final int MIN_POWER = 4; // 전진 or 정지 정하는 기준
+    public static final int MIN_RANDOM = 0; // 최소 랜덤값
+    public static final int MAX_RANDOM = 9; // 최대 랜덤값
+    public static final int MAX_DIGITS = 5; // 자동차 이름 최대 길이
+    public static final String DISTANCE_PRINT = "-"; // 이동거리 출력 글자
+}
