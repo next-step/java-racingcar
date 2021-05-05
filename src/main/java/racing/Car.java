fc0794a0afe683791968fd09c3152ea500d32dad
Miot2J <ck96117@gmail.com>
2021-05-05 18:24:35 +0900

    refactor: Car.java 의 생성자에 있던 길이 검증을 메서드화
    validateLength() 메서드 추가

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -5,18 +5,22 @@ public class Car {
     private int moveCount;
 
     public Car(String name) {
-        if(name.length() > 5){
-            throw new IllegalArgumentException("이름은 5자 이내로 입력하세요.");
-        }
+        validateLength(name);
         this.name = name;
     }
-
 
     public void move(int randomNumber) {
         if (randomNumber >= 4) {
             this.moveCount++;
         }
     }
+
+    private void validateLength(String name) {
+        if(name.length() > 5){
+            throw new IllegalArgumentException("이름은 5자 이내로 입력하세요.");
+        }
+    }
+
 
     public String getName() {
         return name;
