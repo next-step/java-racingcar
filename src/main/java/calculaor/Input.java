b14e12099e601883017ac90e75985c63c4ec12e0
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-05-09 02:16:41 +0900

    "(코드)Calculator" : Input 클래스의 빈값(null 값 or " ") 판단하는 기능 구현
    1. mian
    - 빈값 (null 값 or 공백) 판단하기 : Input#checkBlank
    2. test
    - 빈값 테스트 : InputTest#checkBlank
    - " "은 통과하는데, null 값은 통과 안 됨

--- src/main/java/calculaor/Input.java
+++ src/main/java/calculaor/Input.java
@@ -1,2 +1,15 @@
-package calculaor;public class Input {
+package calculaor;
+
+public class Input {
+    private final String splitValue = " ";
+
+    // 빈값 (null 값 or " ") 판단하기
+    public Boolean checkBlank(String blankValue) {
+        if (blankValue.equals(" ") || blankValue == null) {
+        } return true;
+    }
+
+
+
+
 }
--- src/test/java/calculaor/InputTest.java
+++ src/test/java/calculaor/InputTest.java
@@ -3,19 +3,30 @@ package calculaor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class CalculatorTest {
+public class InputTest {
+//    private String input;
+    private String blankInput;
 
-}
-
-
-/*
-    @Test
-    void inputTest() {
-        Calculator ca = new Calculator();
-        String input = ca.input();
-        System.out.println(input);
+    @BeforeEach
+    void setUP() {
+//        input = "2 + 3 * 4 / 2";
+        blankInput = " ";
     }
 
- */
+    // 빈값 (null 값 or " ") 판단하기
+    // null 값은 통과 못 함
+    @Test
+    void checkBlank() {
+        // Given
+        Input input = new Input();
+
+        // when
+        Boolean actual = input.checkBlank(blankInput);
+
+        //then
+        assertThat(actual).isTrue();
+    }
+}
+
