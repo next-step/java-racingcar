a734ba87d0bee22ac93453a5edd60db579f6a4bb
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-05-09 21:15:39 +0900

    "(코드 수정) Operator" : 연산기능 구현 및 테스트
    1. main
    - 사칙연산 기능 구현
    - 나누기 기능에 예외 처리 구현
    2. test
    - 사칙연산 기능 테스트
    - 나누기 예외 상황 테스트

--- src/main/java/calculaor/Operator.java
+++ src/main/java/calculaor/Operator.java
@@ -1,6 +1,6 @@
 package calculaor;
 
-public class Calculator {
+public class Operator {
 
     // 사칙연산 기능
     public int add(int number1, int number2) {
@@ -17,9 +17,9 @@ public class Calculator {
 
     public int div(int number1, int number2) {
         try {
-            int divValue = number1 / number2;
+            return number1 / number2;
         } catch (ArithmeticException e) {
-            System.out.println("0으로는 나눌 수 없습니다.");
-        } return number1 / number2;
+            throw new IllegalArgumentException("0으로는 나눌 수 없습니다.");
+        }
     }
 }
---
+++ src/test/java/calculaor/OperatorTest.java
@@ -0,0 +1,92 @@
+package calculaor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class OperatorTest {
+    private int number1;
+    private int number2;
+
+    @BeforeEach
+    void Setup() {
+        number1 = 4;
+        number2 = 2;
+    }
+
+    // 사칙연산 테스트
+    @Test
+    void addTest() {
+        // given
+        Operator operator = new Operator();
+
+        // when
+        int expected = 6;
+        int actual = operator.add(number1, number2);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void subTest() {
+        // given
+        Operator operator = new Operator();
+
+        // when
+        int expected = 2;
+        int actual = operator.sub(number1, number2);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void mulTest() {
+        // given
+        Operator operator = new Operator();
+
+        // when
+        int expected = 8;
+        int actual = operator.mul(number1, number2);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    // 나누기 기능 테스트
+    @ParameterizedTest
+    @CsvSource(value = {"4,2","4,3"})
+    void divTest(int num1, int num2) {
+        // given
+        Operator operator = new Operator();
+
+        // when
+        int expected = 2;
+        int actual = operator.div(num1, num2);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    // 나누기 예외 상황(0으로 나누기) 테스트
+    @Test
+     void divExceptionTest() {
+        // given
+        int num1 = 1;
+        int num2 = 0;
+
+        // when
+        Operator operator = new Operator();
+
+        // then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> operator.div(num1, num2));
+    }
+
+}
