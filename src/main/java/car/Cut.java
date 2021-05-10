c1ad84688fc2dee0291df16901e0d4cd574d50f2
정진우 <jwjung@upchain.kr>
2021-05-10 10:49:48 +0900

    refactor: 변수명 수정
    Cut클래스 getCar함수 내에서 변수명 c를 car로 변경

--- src/main/java/car/Cut.java
+++ src/main/java/car/Cut.java
@@ -4,11 +4,12 @@ public class Cut {
 
     public Car getCar(String carName) {
         int length = carName.length();
-        if(length > 5)
+        if(length > 5) {
             length = 5;
-        Car c = new Car();
+        }
+        Car car = new Car();
         carName = carName.substring(0, length);
-        c.setName(carName);
-        return c;
+        car.setName(carName);
+        return car;
     }
 }
