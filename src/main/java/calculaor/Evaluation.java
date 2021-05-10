362fa0a7c77480551447a0f2c3410bdb98d7e744
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-05-10 13:02:40 +0900

    "(코드)Calculator" : 연산부호 평가(Evaluation) 클래스 메소드명 변경
    1. mian
    - (메소드명 변경) : checkOperator
    - (메소드명 변경) : isOperator
    2. test
    - (메소드명 변경)메인클래스 메소드명 변경에 따른 메소드명 변경
    - (코드추가)checkOperator 기능 테스트 추가

--- src/main/java/calculaor/Evaluation.java
+++ src/main/java/calculaor/Evaluation.java
@@ -10,25 +10,24 @@ public class Evaluation {
     }
 
     // 사칙연산 부호 평가하기
-    public int operatorEvaluation(int firstNum, String operatorValue, int secondNum) {
+    public int checkOperator(int firstNum, String operatorValue, int secondNum) {
         Operator operator = new Operator();
-        Map operatorList = new HashMap<String, Integer>();
-        int operatorResult;
+        Map<String, Integer> operatorList = new HashMap<>();
 
         operatorList.put("+", operator.add(firstNum, secondNum));
         operatorList.put("-", operator.sub(firstNum, secondNum));
         operatorList.put("*", operator.mul(firstNum, secondNum));
         operatorList.put("/", operator.div(firstNum, secondNum));
 
-        operatorResult = (int) operatorList.get(operatorValue);
+        int operatorResult = operatorList.get(operatorValue);
 
         return operatorResult;
     }
 
     // 사칙연산 부호 예외처리 ex) $ # ^
-    public int operatorException(int firstNum, String operatorValue, int secondNum) {
+    public int isOperator(int firstNum, String operatorValue, int secondNum) {
         try {
-            return operatorEvaluation(firstNum, operatorValue, secondNum);
+            return checkOperator(firstNum, operatorValue, secondNum);
         } catch (NullPointerException e) {
             throw new IllegalArgumentException("연산기호가 아닙니다.");
         }
--- src/test/java/calculaor/EvaluationTest.java
+++ src/test/java/calculaor/EvaluationTest.java
@@ -21,7 +21,7 @@ class EvaluationTest {
     // 문자형을 숫자형으로 변환 테스트
     @ParameterizedTest
     @CsvSource(value = {"1,1","2,2","3,3"})
-    void ConvertToNumberTest(String stringInput, int intInput) {
+    void convertToNumberTest(String stringInput, int intInput) {
         // given
         Evaluation evaluation = new Evaluation();
 
@@ -32,29 +32,44 @@ class EvaluationTest {
         assertThat(actual).isEqualTo(intInput);
     }
 
-    // 사칙연산 부호 평가하기 테스트
-    @ParameterizedTest
-    @CsvSource(value = {"+,6","-,2","*,8","/,2"})
-    void operatorEvaluationTest(String operatorValue, int expected) {
+    // 사칙연산 부호 평가기능 테스트
+    @Test
+    void checkOperatorTest() {
         // given
         Evaluation evaluation = new Evaluation();
 
         // when
-        int actual = evaluation.operatorEvaluation(num1,operatorValue,num2);
+        int expected = 6;
+        int actual = evaluation.checkOperator(num1,"+",num2);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+
+    // 정상 케이스: 사칙연산 부호 평가
+    @ParameterizedTest
+    @CsvSource(value = {"+,6","-,2","*,8","/,2"})
+    void isOperatorTest(String operatorValue, int expected) {
+        // given
+        Evaluation evaluation = new Evaluation();
+
+        // when
+        int actual = evaluation.isOperator(num1,operatorValue,num2);
 
         // then
         assertThat(actual).isEqualTo(expected);
     }
     
-    // 사칙연산 부호 예외처리 테스트
+    // 예외 케이스 : 사칙연산 부호 아닌 경우
     @ParameterizedTest
     @CsvSource(value = {"&","%","@"})
-    void operatorExceptionTest(String exceptionValue) {
+    void isOperatorExceptionTest(String exceptionValue) {
         // given
         Evaluation evaluation = new Evaluation();
 
         //then
         assertThatIllegalArgumentException().isThrownBy(()
-                -> evaluation.operatorException(num1, exceptionValue, num2));
+                -> evaluation.isOperator(num1, exceptionValue, num2));
     }
 }
