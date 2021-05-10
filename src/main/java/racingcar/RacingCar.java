2c5f13a9c6d8bce4d5971604d12cd626182f4018
DESKTOP-JH4A8SV\ceo <siryugubin@naver.com>
2021-05-10 10:51:39 +0900

    refactor : carNames 수정

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,9 +3,9 @@ package racingcar;
 public class RacingCar {
 
     public void nameCheck(String carName) {
-        String carNameArr[] = carName.split(",");
-        for (int i = 0; i < carNameArr.length; i++) {
-            if (carNameArr[i].length() > 5) {
+        String[] carNames = carName.split(",");
+        for (int i = 0; i < carNames.length; i++) {
+            if (carNames[i].length() > 5) {
                 throw new IllegalArgumentException("자동차 이름이 5자 이상입니다");
             }
         }
