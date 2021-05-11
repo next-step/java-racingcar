75c7e81b650b0419de5b5239431128659ed762b2
이예림 <dulfla_5@naver.com>
2021-05-12 02:54:52 +0900

    문자열 계산기 및 자동차경주게임 리뷰반영 3차 (mvc 패턴에 맞춰 리팩터링)

--- src/main/java/RacingCarView.java
+++ src/main/java/RacingCarView.java
@@ -1,3 +1,19 @@
-public class RacingCarView {
+import java.util.Scanner;
 
+public class RacingCarView {
+    Scanner scanner = new Scanner(System.in);
+
+    public String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine();
+    }
+
+    public int inputRoundNumber() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return scanner.nextInt();
+    }
+
+    public void outputWinners(String winners) {
+        System.out.println("최종 우승자:" + winners);
+    }
 }
