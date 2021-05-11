ebd9fd41e14977ff9eff43a182e38607fbdd7d46
Data-ssung <ssungmi2012@gmail.com>
2021-05-11 23:06:55 +0900

    "(코드) Calculator" : Calculation 클래스 불필요한 주석 제거

--- src/main/java/calculaor/Calculation.java
+++ src/main/java/calculaor/Calculation.java
@@ -1,15 +1,11 @@
 package calculaor;
 
 public class Calculation {
-
     public static int calculation(String inputValue) {
         Input input = new Input();
         Evaluation evaluation = new Evaluation();
-
-        // input 받기
         String[] splitValue = input.isSplit(inputValue);
 
-        // Calculation : 연산자 수 만큼 계산
         for (int i = 1; i < splitValue.length; i += 2) {
             int firstNum = evaluation.convertToNumber(splitValue[i-1]);
             String operatorValue = splitValue[i];
@@ -21,5 +17,4 @@ public class Calculation {
 
         return evaluation.convertToNumber(splitValue[splitValue.length - 1]);
     }
-
 }
