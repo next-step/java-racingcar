c22e70e5d7dd8104f6a5d3487daac29af00409c4
정진우 <jwjung@upchain.kr>
2021-05-11 21:38:44 +0900

    자동차 경주 게임 excute 책임 분리

--- src/main/java/car/Game.java
+++ src/main/java/car/Game.java
@@ -3,27 +3,46 @@ package car;
 import java.util.*;
 
 public class Game {
+    private static final int MINIMUM_CAR_AMOUNT = 2;
     private Input input = new Input();
     private Cut cut = new Cut();
 
     public void excute() {
+        String[] carInputs = inputCarNames();
 
+        List<Car> cars = createCars(carInputs);
+
+        if (cars.size() < MINIMUM_CAR_AMOUNT) {
+            return;
+        }
+
+        String count = inputTryCount();
+
+        outputRacingCars(count, cars);
+    }
+
+    private String[] inputCarNames() {
         String carInput = input.carNameinput();
-
         String[] carInputs = carInput.split(",");
 
+        return carInputs;
+    }
+
+    private List<Car> createCars(String[] carInputs) {
         List<Car> cars = new ArrayList<>();
 
         for (String carName : carInputs) {
             cars.add(cut.getCar(carName));
         }
 
-        if (cars.size() < 2) {
-            return;
-        }
+        return cars;
+    }
 
-        String count = input.countInput();
+    private String inputTryCount() {
+        return input.countInput();
+    }
 
+    private void outputRacingCars(String count, List<Car> cars) {
         Output output = new Output(cars);
 
         output.carRacing(Integer.valueOf(count));
