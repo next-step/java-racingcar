dea55cbecf5f250d0f6e3a8223939241171a2691
Data-ssung <ssungmi2012@gmail.com>
2021-05-11 23:07:47 +0900

    "(코드) Racing Car" : input 클래스 main 메소드 실행 코드 제거

--- src/main/java/racingCar/Input.java
+++ src/main/java/racingCar/Input.java
@@ -2,24 +2,16 @@ package racingCar;
 
 import java.util.Scanner;
 
-public class input {
+public class Input {
     // 자동차 이름
-    public static String carName() {
+    public String carName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return scanner.nextLine();    }
 
-    public static int gameNumber() {
+    public int gameNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("시도할 횟수는 몇 회인가요?");
        return scanner.nextInt();
-    }
-
-    public static void main(String[] args) {
-        String carName = carName();
-        int gameNumber = gameNumber();
-
-        System.out.println(carName);
-        System.out.println(gameNumber);
     }
 }
