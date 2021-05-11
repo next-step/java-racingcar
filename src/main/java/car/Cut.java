674bbca52f02620d7489623c268d58933b634094
정진우 <jwjung@upchain.kr>
2021-05-11 21:16:16 +0900

    refactor: 자동차 경주 게임 괄호 정렬

--- src/main/java/car/Cut.java
+++ src/main/java/car/Cut.java
@@ -4,7 +4,7 @@ public class Cut {
 
     public Car getCar(String carName) {
         int length = carName.length();
-        if(length > 5) {
+        if (length > 5) {
             length = 5;
         }
         Car car = new Car(carName.substring(0, length), 0);
