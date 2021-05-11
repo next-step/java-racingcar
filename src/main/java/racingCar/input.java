abb98d2e20e1fdc004bfc66d3413a8ff59feb075
Data-ssung <ssungmi2012@gmail.com>
2021-05-11 14:17:55 +0900

    "(코드수정) RacingCar" : input 클래스 불필요한 코드 정리
    1. carName 메소드
    - carname 변수 제거하고 바로 return 받기
    2. gameNumber 메소드
    - gamenumber 변수 제거하고 바로 return 받기

--- src/main/java/racingCar/input.java
+++ src/main/java/racingCar/input.java
@@ -7,24 +7,19 @@ public class input {
     public static String carName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String carName = scanner.nextLine();
-        return carName;
-    }
+        return scanner.nextLine();    }
 
     public static int gameNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("시도할 횟수는 몇 회인가요?");
-        int gameNumber = scanner.nextInt();
-        return gameNumber;
+       return scanner.nextInt();
     }
 
     public static void main(String[] args) {
-        // 불러오기
-        String carnames = carName();
-        int gamenumber = gameNumber();
+        String carName = carName();
+        int gameNumber = gameNumber();
 
-        // 실행하기
-        System.out.println(carnames);
-        System.out.println(gamenumber);
+        System.out.println(carName);
+        System.out.println(gameNumber);
     }
 }
