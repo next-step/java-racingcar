c465635655bd99cc48ee84762c466d126a16cdb9
hailey <wjdguswls55@gmail.com>
2021-05-12 17:09:54 +0900

    feat (MoveService.class) : 매직 넘버 수정
    + 숫자로 작성했던 코드를 final 형식으로 지정해 주어 수정해보았습니다.

--- src/main/java/racingcar/service/MoveService.java
+++ src/main/java/racingcar/service/MoveService.java
@@ -3,11 +3,13 @@ package racingcar.service;
 import java.util.Random;
 
 public class MoveService {
+    private static final int RANDOM_NUMBER = 10;
+    private static final int MOVABLE_NUMBER = 4;
 
     public String isMove() {
         Random random = new Random();
-        int number = random.nextInt(10);
-        if (number >= 4) {
+        int number = random.nextInt(RANDOM_NUMBER);
+        if (number >= MOVABLE_NUMBER) {
             return "1";
         }
         return "0";
