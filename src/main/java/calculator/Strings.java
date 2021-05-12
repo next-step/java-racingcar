afa4d7359a372fcfd4a368c88fac08557311a565
hailey <wjdguswls55@gmail.com>
2021-05-12 17:15:35 +0900

    feat (Strings.class) : 구글 컨벤션 적용, 주석 제공, '{','}' 추가, checkSymbol 메서드 수정
    + isBlank() 메서드에 '{','}' 를 추가하였습니다.
    + checkSymbol 메서드에 for문 기능을 수정하였습니다.

--- src/main/java/calculator/Strings.java
+++ src/main/java/calculator/Strings.java
@@ -2,50 +2,52 @@ package calculator;
 
 
 public class Strings {
-    // + - / *
-
 
     public static boolean isBlank(String input) {
-        if(input == null || input.trim().isEmpty()) throw new IllegalArgumentException("input is null or blank");
-        else return true;
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("input is null or blank");
+        } else {
+            return true;
+        }
     }
 
-    public static boolean checkSymbol(String[] splitBlank){
+    public static boolean checkSymbol(String[] splitBlank) {
         String pattern = "[\\d\\+\\-\\*\\/]";
-        boolean check = false;
-        for(int i=0;i<splitBlank.length;i++){
-            check = splitBlank[i].matches(pattern);
-            if(!check) throw new IllegalArgumentException("symbol error");
+        for (int i = 0; i < splitBlank.length; i++) {
+            if (splitBlank[i].matches(pattern)) {
+                return true;
+            }
         }
-        return check;
+        throw new IllegalArgumentException("symbol error");
+
     }
 
     public static String[] cutByOperation(String input) {
-        String [] strings = input.split("");
-        String [] returnString = new String[3];
-        for (String s: strings) {
+        String[] strings = input.split("");
+        String[] returnString = new String[3];
+        for (String s : strings) {
             if (s.equals("+")) {
                 returnString[0] = input.substring(0, input.indexOf(s));
                 returnString[1] = "+";
-                returnString[2] = input.substring(input.indexOf(s)+1, input.length());
+                returnString[2] = input.substring(input.indexOf(s) + 1, input.length());
                 break;
             }
             if (s.equals("-")) {
                 returnString[0] = input.substring(0, input.indexOf(s));
                 returnString[1] = "-";
-                returnString[2] = input.substring(input.indexOf(s)+1, input.length());
+                returnString[2] = input.substring(input.indexOf(s) + 1, input.length());
                 break;
             }
             if (s.equals("*")) {
                 returnString[0] = input.substring(0, input.indexOf(s));
                 returnString[1] = "*";
-                returnString[2] = input.substring(input.indexOf(s)+1, input.length());
+                returnString[2] = input.substring(input.indexOf(s) + 1, input.length());
                 break;
             }
             if (s.equals("/")) {
                 returnString[0] = input.substring(0, input.indexOf(s));
                 returnString[1] = "/";
-                returnString[2] = input.substring(input.indexOf(s)+1, input.length());
+                returnString[2] = input.substring(input.indexOf(s) + 1, input.length());
                 break;
             }
         }
