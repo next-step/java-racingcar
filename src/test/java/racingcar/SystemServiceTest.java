3939a5b909c47ca8e684ed93b8d1c79723a054eb
hailey <wjdguswls55@gmail.com>
2021-05-14 18:10:16 +0900

    test (SystemServiceTest) : invalidedName() 테스트 코드 작성
    - 자동차 이름이 5자를 초과하는 경우 예외가 발생한다.

--- src/test/java/racingcar/SystemServiceTest.java
+++ src/test/java/racingcar/SystemServiceTest.java
@@ -6,8 +6,33 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.view.RacingCarView;
+import racingcar.vo.Car;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SystemServiceTest {
 
+    private List<Car> carInfo;
+
+    private static final int RANDOM_NUMBER = 10;
+    private static final int MOVABLE_NUMBER = 4;
+
+    @BeforeEach
+    public void init() {
+        carInfo = Arrays.asList(
+                new Car("jeongggg", 2),
+                new Car("hyeon", 3),
+                new Car("jin", 1)
+        );
+    }
+
+    @DisplayName("자동차 이름이 5자를 초과하는 경우 예외가 발생한다.")
+    @Test
+    public void invalidedName() {
+
+        carInfo.stream().filter(s->s.getName().length() > 5).findAny().orElseThrow(IllegalArgumentException::new);
+    }
 
 }
