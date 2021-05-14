a1712ef98c626b5c0d561434447f474bf3583861
hailey <wjdguswls55@gmail.com>
2021-05-14 17:43:59 +0900

    test (SystemServiceTest) : domain에 해당하는 클래스를 test로 분류
    - View에 해당하는 Test코드 삭제

---
+++ src/test/java/racingcar/SystemServiceTest.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.view.RacingCarView;
+
+public class SystemServiceTest {
+
+
+}
