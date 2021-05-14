f13c88eaf0176d63c95f1af79e915aea47330586
정진우 <jwjung@upchain.kr>
2021-05-14 19:53:23 +0900

    feat: 자동차 경주 게임 Copy 테스트 코드 추가

--- src/test/java/car/CopyTest.java
+++ src/test/java/car/CopyTest.java
@@ -1,4 +1,32 @@
-import static org.junit.jupiter.api.Assertions.*;
+package car;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CopyTest {
-  
+    private List<Car> cars;
+
+    @BeforeEach
+    void setUp() {
+        cars = new ArrayList<>();
+
+        cars.add(new Car("java", 1));
+        cars.add(new Car("php", 2));
+        cars.add(new Car("python", 3));
+    }
+
+    @Test
+    void createCopiedCars() {
+        Copy copy = new Copy(cars);
+
+        List<Car> copiedCars = copy.createCopiedCars();
+
+        assertThat(cars).isEqualTo(copiedCars);
+    }
+
 }
