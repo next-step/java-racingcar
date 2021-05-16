ae9bc8cfe9691cdda36a89b6d1805bdea0f85669
h1un <h1unha@naver.com>
2021-05-16 15:49:52 +0900

    refactor(RacingGame) : getCars(), getCarName() 메소드 추가

--- src/main/java/racing/RacingMain.java
+++ src/main/java/racing/RacingMain.java
@@ -1,23 +1,23 @@
 package racing;
 
 import racing.domain.RacingGame;
-import racing.view.InputVeiw;
+import racing.view.InputView;
 import racing.view.ResultView;
 
 public class RacingMain {
-    static InputVeiw inputVeiw = new InputVeiw();
+    static InputView inputView = new InputView();
     static ResultView resultView = new ResultView();
 
 
     public static void main(String[] args) {
-        String carNames = inputVeiw.getCarNames();
-        int tryNo = inputVeiw.getTryNo();
+        String carNames = inputView.getCarNames();
+        int tryNo = inputView.getTryNo();
 
         RacingGame racingGame = new RacingGame(carNames, tryNo);
-        while (!racingGame.isEnd()) {
-            racingGame.race();
-            resultView.printCars(racingGame.getCars());
-        }
-        resultView.printWinners(racingGame.getWinners());
+//        while (!racingGame.isEnd()) {
+//            racingGame.race();
+//            resultView.printCars(racingGame.getCars());
+//        }
+//        resultView.printWinners(racingGame.getWinners());
     }
 }
--- src/main/java/racing/domain/RacingGame.java
+++ src/main/java/racing/domain/RacingGame.java
@@ -1,6 +1,43 @@
 package racing.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RacingGame {
+    static final int NAME_LENGTH = 5;
+    static final int MOVE_NUMBER = 4;
+    static final int MAX_RANDOM = 10;
+    static final int WINNER_NUMBER = 0;
+    static final String CAR_NAME_SPLIT = ",";
+    static Random random = new Random();
+
+    private List<Car> cars;
+    private int tryNo;
+
+    public RacingGame(String carNames, int tryNo) {
+
+        List<Car> cars = getCars(getCarName(carNames));
+        this.cars = cars;
+        this.tryNo = tryNo;
+
+    }
+
+    private List<Car> getCars(String[] carNames) {
+        List<Car> cars = new ArrayList<Car>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
+
+    public String[] getCarName(String carNamesStr) {
+        String[] carNames = carNamesStr.split(CAR_NAME_SPLIT);
+        for (String carName : carNames) {
+            if (carName.length() > NAME_LENGTH) throw new IllegalArgumentException("이름은 5자 이내로 .");
+        }
+        return carNames;
+    }
 
 
 }
