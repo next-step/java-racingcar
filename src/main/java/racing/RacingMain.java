f02b12f1bd9bf73adeb243040406da11827bffcc
h1un <h1unha@naver.com>
2021-05-16 15:25:50 +0900

    refactor(RacingMain) : 힌트 | MVC 패턴으로 리팩터링 후의 main 메서드 예시 추가

--- src/main/java/racing/RacingMain.java
+++ src/main/java/racing/RacingMain.java
@@ -1,6 +1,23 @@
 package racing;
 
+import racing.domain.RacingGame;
+import racing.view.InputVeiw;
+import racing.view.ResultView;
 
 public class RacingMain {
+    static InputVeiw inputVeiw = new InputVeiw();
+    static ResultView resultView = new ResultView();
 
+
+    public static void main(String[] args) {
+        String carNames = inputVeiw.getCarNames();
+        int tryNo = inputVeiw.getTryNo();
+
+        RacingGame racingGame = new RacingGame(carNames, tryNo);
+        while (!racingGame.isEnd()) {
+            racingGame.race();
+            resultView.printCars(racingGame.getCars());
+        }
+        resultView.printWinners(racingGame.getWinners());
+    }
 }
