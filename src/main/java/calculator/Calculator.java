0a751c9f8c152bf9588037b4ef1367fb4cbb7c64
박소연 <sycap01@gmail.com>
2021-05-17 17:23:26 +0900

    계산하기

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import java.util.List;
+
 public class Calculator {
     public int calculate(String sentence) {
-        return 0;
+        BlankEraser blankEraser = new BlankEraser();
+        String[] strings = blankEraser.erase(sentence);
+
+        Extractor extractor = new Extractor();
+        List<Integer> numbers = extractor.extractNumbers(strings);
+        List<String> operators = extractor.extractOperators(strings);
+
+        int result = 0;
+        Operator operator = new Operator("+");
+        for (int i = 0; i < numbers.size(); i++) {
+            result = operator.operate(result, numbers.get(i));
+
+            if (i != numbers.size() - 1) {
+                operator = new Operator(operators.get(i));
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,37 @@
 package calculator;
 
-public class CalculatorTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    @Test
+    void name1() {
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate("3");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void name() {
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate("3 + 2");
+        assertThat(result).isEqualTo(5);
+
+        int result2 = calculator.calculate("3 - 2");
+        assertThat(result2).isEqualTo(1);
+
+        int result3 = calculator.calculate("3 * 2");
+        assertThat(result3).isEqualTo(6);
+
+        int result4 = calculator.calculate("3 / 2");
+        assertThat(result4).isEqualTo(1);
+
+        int result5 = calculator.calculate("3 * 2 + 4 / 2 - 3");
+        assertThat(result5).isEqualTo(2);
+
+        int result6 = calculator.calculate("3 / 2 + 4 - 2 * 3");
+        assertThat(result6).isEqualTo(9);
+
+    }
 }
