f324702c0ed793a126cbae33b31627567eba6dc9
hailey <wjdguswls55@gmail.com>
2021-05-17 17:45:08 +0900

    test (SystemServiceTest) : 사용하지 않는 코드 제거

--- src/test/java/racingcar/SystemServiceTest.java
+++ src/test/java/racingcar/SystemServiceTest.java
@@ -3,21 +3,14 @@ package racingcar;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.view.RacingCarView;
 import racingcar.vo.Car;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class SystemServiceTest {
 
     private List<Car> carInfo;
-
-    private static final int RANDOM_NUMBER = 10;
-    private static final int MOVABLE_NUMBER = 4;
 
     @BeforeEach
     public void init() {
@@ -32,7 +25,7 @@ public class SystemServiceTest {
     @Test
     public void invalidedName() {
 
-        carInfo.stream().filter(s->s.getName().length() > 5).findAny().orElseThrow(IllegalArgumentException::new);
+        carInfo.stream().filter(s -> s.getName().length() > 5).findAny().orElseThrow(IllegalArgumentException::new);
     }
 
 }
