b593d0e93a4991ba77acc627f643b28d90d5f21d
xxhyxxnjxn <hyunjin077@naver.com>
2021-05-17 23:18:21 +0900

    test (CalculatorServiceTest.class) : 빈칸 체크와 기호 체크와 연산식을 수행하는 메소드를 테스트를 하기 위해 테스트 코드를 작성함
    +blankTest -> 빈칸 확인 테스트
    +checkSymbolTset -> 올바른 기호인지 확인하는 테스트
    +calculateSplitedString -> 올바른 결과값이 도출 되는지 확인하는 테스트

---
+++ src/test/java/calculator/CalculatorServiceTest.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CalculatorServiceTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    public void blankTest(String input) {
+        assertThat(StringsService.isBlank(input)).isFalse();
+    }
+
+    @Test
+    public void checkSymbolTest() {
+        String[] splitBlank = {"2", "+", "3", "*", "4", "/", "2"};
+        assertThat(StringsService.checkSymbol(splitBlank)).isTrue();
+    }
+
+    @Test
+    public void calculateSplitedString() {
+        String[] splitBlank = {"2", "+", "3", "*", "4", "/", "2"};
+        int result = StringsService.calculateSplitedString(splitBlank);
+        assertThat(result).isEqualTo(10);
+    }
+}
