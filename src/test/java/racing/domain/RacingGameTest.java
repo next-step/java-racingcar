f46baf02201973fff9fcc57150cc5cce6972e05b
h1un <h1unha@naver.com>
2021-05-18 10:15:50 +0900

    refactor(Winners) : WinnersTest

--- src/test/java/racing/domain/RacingGameTest.java
+++ src/test/java/racing/domain/RacingGameTest.java
@@ -1,5 +1,6 @@
 package racing.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,18 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RacingGameTest {
-    RacingGame racing = new RacingGame("1,2,3",7);
-
+    RacingGame racing = new RacingGame("1,2,3", 7);
 
     @DisplayName("차 이름 리스트에 담기")
     @Test
     void getCarName() {
         assertThat(racing.getCarName("chloe,chloe")).containsExactly("chloe", "chloe");
     }
+
     @DisplayName("차 이름 5자가 넘으면 예외처리")
     @Test
     void throwIllegalArgumentException() {
         assertThatThrownBy(() -> racing.getCarName("chloe,chloeJ")).isInstanceOf(IllegalArgumentException.class);
     }
-
 }
---
+++ src/test/java/racing/domain/WinnersTest.java
@@ -0,0 +1,31 @@
+package racing.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinnersTest {
+
+
+    @DisplayName("우승자 뽑기")
+    @Test
+    void getWinner() {
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new Car("1", 4));
+        cars.add(new Car("2", 3));
+        cars.add(new Car("3", 2));
+        cars.add(new Car("4", 1));
+
+        Winners winners = new Winners(cars);
+
+        assertThat(winners.winners()).contains("1");
+
+    }
+
+}
