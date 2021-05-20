386046c3d36b54d0eac778fb6a76534f68aedb06
hailey <wjdguswls55@gmail.com>
2021-05-20 23:34:25 +0900

    feat (Winner.class) : racing 게임에서 우승자 이름을 저장하는 객체 생성

---
+++ src/main/java/racingcar/domain/Winner.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class Winner {
+    private String name;
+
+    public Winner(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
