f6856e1804464397cd862c2c0d8c6cf4e49a0ed1
hailey <wjdguswls55@gmail.com>
2021-05-20 23:34:03 +0900

    feat (Util.class) : final 변수 선언

--- src/main/java/racingcar/domain/Util.java
+++ src/main/java/racingcar/domain/Util.java
@@ -1,8 +1,10 @@
-package racingcar.service;
+package racingcar.domain;
 
 public class Util {
+    private static final int CAR_LENGTH = 5;
+
     public boolean invalidedName(String name) {
-        if (name.length() > 5) {
+        if (name.length() > CAR_LENGTH) {
             throw new IllegalArgumentException("자동차의 이름은 5자를 초과할 수 없다.");
         }
         return true;
