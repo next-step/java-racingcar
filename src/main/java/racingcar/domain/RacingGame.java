0d45dd4115042cf5038f2bc460b7ec0d481e8c04
hailey <wjdguswls55@gmail.com>
2021-05-20 23:31:37 +0900

    feat (RacingGame) : 각각의 기능 분산
    + moveCar() : Car 객체 클래스에서 수행하고자 Random 클래스와 상수는 따로 random 클래스를 만들어주어 그 안에 실행하게 하였습니다.
    + checkWinner(), selectWinnerName() : 우승자를 찾는 클래스를 따로 생성하여 그 클래스 안에서 새로 리팩토링 하였습니다.

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+
+    private Util util = new Util();
+
+    private List<Car> carInfo = new ArrayList<>();
+
+    public List<Car> splitString(String input) {
+        String[] result = input.split(",");
+        for (int i = 0; i < result.length; i++) {
+            if (util.invalidedName(result[i])) {
+                Car car = new Car(result[i], 0);
+                carInfo.add(car);
+            }
+        }
+        return carInfo;
+    }
+}
