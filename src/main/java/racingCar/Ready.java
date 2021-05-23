26ce2e848e877499509c42d0c748d7c62a6e36ec
Data-ssung <67546948+Data-ssung@users.noreply.github.com>
2021-05-23 16:06:40 +0900

    (코드) Ready 클래스 : isGreaterThanFive 구현

--- src/main/java/racingCar/Ready.java
+++ src/main/java/racingCar/Ready.java
@@ -1,21 +1,14 @@
 package racingCar;
 
-import java.util.List;
-
 public class Ready {
-    private final String SPERATE_VALUE = ",";
+    private final String SPITE_VALUE = ",";
 
     public String[] createPlayerList(String name) {
-        return name.split(SPERATE_VALUE);
+        return name.split(SPITE_VALUE);
     }
 
-    public List[] createCars(String[] playerList) {
-       List[] cars = new List[playerList.length];
-       int index = 0;
-       for (String s : playerList) {
-           cars[index] = (List) new Car(s, 0);
-           index++;
-       }
-       return cars;
+    public boolean isGreaterThanFive(int number) {
+        return number > 5;
     }
+
 }
--- src/test/java/racingCar/ReadyTest.java
+++ src/test/java/racingCar/ReadyTest.java
@@ -3,14 +3,20 @@ package racingCar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ReadyTest {
+    private Ready ready = new Ready();
+
     @Test
     @DisplayName("선수 목록 만들기")
     void TestCreatePlayerList() {
         // when
-        Ready ready = new Ready();
         String name = "s,h,j";
 
         // given
@@ -20,4 +26,31 @@ public class ReadyTest {
         // then
         assertThat(actual).isEqualTo(expected);
     }
+    
+    @Test
+    @DisplayName("숫자가 5이상 인지 확인")
+    void TestisGreaterThanFive() {
+        // when
+        int number = 5;
+
+        // given
+        boolean actual = ready.isGreaterThanFive(number);
+
+        // then
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("숫자가 5이하 인지 확인")
+    void TestisNotMoreThanFive() {
+        // when
+        int number = 3;
+
+        // given
+        boolean actual = ready.isGreaterThanFive(number);
+
+        // then
+        assertFalse(actual);
+    }
+
 }
