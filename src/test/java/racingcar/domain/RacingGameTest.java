b6822398f96fa1d464e69fef2a813d8149fca45a
hailey <wjdguswls55@gmail.com>
2021-05-24 11:17:54 +0900

    refactor (RacingGameTest) : 정렬

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -18,7 +19,7 @@ public class RacingGameTest {
     @ParameterizedTest
     @ValueSource(strings = {"jeonggggg,hyeon,jin"})
     public void splitString(String input) {
-        assertThrows(IllegalArgumentException.class,()->util.invalidedName(input));
+        assertThrows(IllegalArgumentException.class, () -> util.invalidedName(input));
     }
 
 }
