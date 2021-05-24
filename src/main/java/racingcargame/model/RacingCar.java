667cd29455de7cf79e519e6d43ecfec2321533e2
이예림 <dulfla_5@naver.com>
2021-05-24 13:51:33 +0900

    feat : 자동차 이름을 입력하지 않았을 때 예외처리 기능 추가

--- src/main/java/racingcargame/model/RacingCar.java
+++ src/main/java/racingcargame/model/RacingCar.java
@@ -10,8 +10,11 @@ public class RacingCar {
     }
 
     public void checkCarNameLength() {
-        if (this.name.length() > 5) {
-            throw new NullPointerException("자동차 이름은 5자를 초과할 수 없다.");
+        if (name == null || name.equals("")) {
+            throw new IllegalArgumentException("자동차 이름을 입력하세요.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없다.");
         }
     }
 
