8d64b382e8a1444ec7a2eb4e7a6905c4a5c21401
hailey <wjdguswls55@gmail.com>
2021-05-24 10:56:16 +0900

    refactor (RacingRandom) : 전체적인 코드 수정
    + isMove()함수에 있던 random함수를 필드로 선언해주었습니다.
    + 지역 변수명을 lowerCameCase방식으로 변경시켰습니다.

--- src/main/java/racingcar/domain/RacingRandom.java
+++ src/main/java/racingcar/domain/RacingRandom.java
@@ -3,16 +3,14 @@ package racingcar.domain;
 import java.util.Random;
 
 public class RacingRandom {
+    private Random random = new Random();
+
     private static final int RANDOM_NUMBER = 10;
     private static final int MOVABLE_NUMBER = 4;
 
     public boolean isMove() {
-        Random random = new java.util.Random();
-        int random_number = random.nextInt(RANDOM_NUMBER);
-        if (random_number >= MOVABLE_NUMBER) {
-            return true;
-        }
-        return false;
+        int randomNumber = random.nextInt(RANDOM_NUMBER);
+        return randomNumber >= MOVABLE_NUMBER;
     }
 
 }
