d015d51cce160df162f3ae5732fcb567c1c9cf1b
박소연 <sycap01@gmail.com>
2021-05-24 13:53:33 +0900

    refactor(calculator):test내용명시

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -7,6 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
+    @DisplayName("3을 넣었을 때는 에러가 아니라 0+3개념으로 나오는지")
     @Test
     void name1() {
         Calculator calculator = new Calculator();
@@ -14,9 +16,10 @@ public class CalculatorTest {
         assertThat(result).isEqualTo(3);
     }
 
+    @DisplayName("상황별로 결과값이 맞게 나오는지")
     @ParameterizedTest
     @CsvSource({"3 + 2, 5", "3 - 2, 1", "3 * 2 + 4 / 2 - 3, 2", "3 / 2 + 4 - 2 * 3, 9"})
-    void name(String sentence, int expected) {
+    void situationTest(String sentence, int expected) {
         Calculator calculator = new Calculator();
         int result = calculator.calculate(sentence);
         assertThat(result).isEqualTo(expected);
