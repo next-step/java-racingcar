36bb18a066d98bae757bdc22574c74f6ce1f7797
이예림 <dulfla_5@naver.com>
2021-05-25 22:04:45 +0900

    refactor : 변하지 않는 값은 상수화하기

--- src/main/java/racingcargame/view/RacingCarGameOutputView.java
+++ src/main/java/racingcargame/view/RacingCarGameOutputView.java
@@ -5,6 +5,8 @@ import racingcargame.model.RacingCar;
 import java.util.List;
 
 public class RacingCarGameOutputView {
+    private static final String STEP = "-";
+
     public void outputRound(List<RacingCar> cars) {
         for (RacingCar car : cars) {
             System.out.println(car.getName() + " : " + showStep(car.getStep()));
@@ -15,7 +17,7 @@ public class RacingCarGameOutputView {
     private String showStep(int step) {
         StringBuilder bar = new StringBuilder();
         for (int i = 0; i < step; i++) {
-            bar.append("-");
+            bar.append(STEP);
         }
         return bar.toString();
     }
