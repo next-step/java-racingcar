3a62c4a6ba8d2d49d4911027e5840c271e0aa869
이예림 <dulfla_5@naver.com>
2021-05-25 22:07:26 +0900

    refactor : 메서드명 변경

--- src/main/java/racingcargame/view/RacingCarGameOutputView.java
+++ src/main/java/racingcargame/view/RacingCarGameOutputView.java
@@ -9,12 +9,12 @@ public class RacingCarGameOutputView {
 
     public void outputRound(List<RacingCar> cars) {
         for (RacingCar car : cars) {
-            System.out.println(car.getName() + " : " + showStep(car.getStep()));
+            System.out.println(car.getName() + " : " + stackUpSteps(car.getStep()));
         }
         System.out.println();
     }
 
-    private String showStep(int step) {
+    private String stackUpSteps(int step) {
         StringBuilder bar = new StringBuilder();
         for (int i = 0; i < step; i++) {
             bar.append(STEP);
