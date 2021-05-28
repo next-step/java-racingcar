2202bbfa83ed55efdabff3f6df9758a9733e54b1
DESKTOP-5DMQS7B\FinTech <ssungmi2012@gmail.com>
2021-05-28 13:35:27 +0900

    (코드) 코드 재작성

--- src/test/java/racingcar/StadiumTest.java
+++
@@ -1,9 +0,0 @@
-package racingCar2;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class StadiumTest {
-
-}
