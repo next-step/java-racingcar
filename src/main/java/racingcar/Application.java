ce2360ab9b74dd80aa97b173776e141b5c2fb686
DESKTOP-5DMQS7B\FinTech <ssungmi2012@gmail.com>
2021-05-28 17:23:18 +0900

    (코드) 프로그램 프로세스 적기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,5 +7,11 @@ public class Application {
         // input
         String carName = input.carName();
         int gameCount = input.gameCount();
+
+        // Ready : 자동차 목록 생성(input 값 받아서 자동차 만들기)
+
+        // Stadium : 경기장 생성, 게임하기
+
+        // Awards : 시상식(우승자 발표)
     }
 }
