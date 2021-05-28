ceacd537114b5cdd17f56280e97c1cd3e16b8340
DESKTOP-5DMQS7B\FinTech <ssungmi2012@gmail.com>
2021-05-28 17:21:04 +0900

    (코드) validateName 구현, Car 생성할 때 검증 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,20 @@
 package racingcar;
 
 public class Car {
+    private static final int NAME_LENGTH_LIMIT_VALUE = 5;
     private final String name;
     int position;
 
     public Car(String name, int position) {
+        validateName(name);
         this.name = name;
         this.position = position;
+    }
+
+    private void validateName(String name){
+        if (name.length() >= NAME_LENGTH_LIMIT_VALUE) {
+            throw new IllegalArgumentException("이름 길이가 5를 초과합니다.");
+        }
     }
 
     public String getName() {
