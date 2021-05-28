b05af65c5e4766fb45feee721e7158ab9000f6ef
DESKTOP-5DMQS7B\FinTech <ssungmi2012@gmail.com>
2021-05-28 16:17:05 +0900

    (코드) carName, gameCount 메서드 구현

---
+++ src/main/java/racingcar/Application.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Application {
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        // input
+        String carName = input.carName();
+        int gameCount = input.gameCount();
+    }
+}
---
+++ src/main/java/racingcar/Input.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class Input {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String carName() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine();
+    }
+
+    public int gameCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return scanner.nextInt();
+    }
+}
