399dbb32225564c1fab5977fe10eafebe5eebc1d
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-05-29 21:11:54 +0900

    (코드) playRace 구현

--- src/main/java/racingcar/Stadium.java
+++ src/main/java/racingcar/Stadium.java
@@ -7,6 +7,17 @@ public class Stadium {
     private List<Car> cars;
     private int gameCount;
 
+    public List<Car> playRace(List<Car> cars, int gameCount) {
+        for (int i = 0; i < gameCount; i++){
+            for (Car car : cars){
+                int number = drawNumber();
+                car.go(number);
+                System.out.println(car.getName() + " : " + car.goingProcess(number));
+            }
+        }
+        return cars;
+    }
+
     public Stadium(List<Car> cars, int gameCount) {
         this.cars = cars;
         this.gameCount = gameCount;
