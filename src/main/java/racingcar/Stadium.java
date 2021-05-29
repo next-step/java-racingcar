82f0b73e0c1ff9f97c13d8c05860e3dee9d22c27
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-05-29 21:05:58 +0900

    (코드) drawNumber 구현

--- src/main/java/racingcar/Stadium.java
+++ src/main/java/racingcar/Stadium.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.List;
+import java.util.Random;
 
 public class Stadium {
     private List<Car> cars;
@@ -9,5 +10,9 @@ public class Stadium {
     public Stadium(List<Car> cars, int gameCount) {
         this.cars = cars;
         this.gameCount = gameCount;
+    }
+
+    private int drawNumber(){
+        return new Random().nextInt(10);
     }
 }
