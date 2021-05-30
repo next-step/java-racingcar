8b7387a853ec47ea0b3cdc4ecce3c6985c779674
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-05-30 21:48:44 +0900

    (리팩터링) 필요없는 주석 제거 및 코드 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,20 +7,13 @@ public class Application {
         Input input = new Input();
         Ready ready = new Ready();
 
-        // input
-        String carName = input.carName();
         int gameCount = input.gameCount();
+        List<Car> cars = ready.createCars(input.carName());
 
-        // Ready : 자동차 목록 생성(input 값 받아서 자동차 만들기)
-        List<Car> cars = ready.createCars(carName);
-
-        // Stadium : 경기장 생성, 게임하기
         Stadium stadium = new Stadium(cars, gameCount);
         stadium.playRace();
 
-        // Award : 시상식(우승자 발표)
         List<Car> result = stadium.award();
         System.out.println("최종 우승자 : " + stadium.winnerIS(result));
-
     }
 }
