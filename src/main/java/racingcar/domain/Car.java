a5ba95a2e247f7df5713716934d3bcd593170115
xxhyxxnjxn <hyunjin077@naver.com>
2021-06-01 23:28:29 +0900

    feat : Car 객체를 생성하여 일급컬랙션 활용

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,15 +1,20 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Car {
     private List<Cars> cars;
 
     public Car(){
+        this.cars = new ArrayList<>();
     }
 
-    public List<Cars> add(String carName){
+    public void add(String carName){
         cars.add(new Cars(carName));
-        return this.cars;
+    }
+
+    public List<Cars> getCars() {
+        return cars;
     }
 }
