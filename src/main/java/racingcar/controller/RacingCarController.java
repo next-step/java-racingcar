2514d4ca1893108fc059ca8f92d5b9b63054c21c
xxhyxxnjxn <hyunjin077@naver.com>
2021-06-01 23:29:03 +0900

    refactor : static을 사용

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,5 +1,6 @@
 package racingcar.controller;
 
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.RacingGame;
 import racingcar.view.RacingCarView;
@@ -8,16 +9,15 @@ import java.util.List;
 
 public class RacingCarController {
     public static void main(String[] args) {
-        RacingCarView racingCarView = new RacingCarView();
-        String carNames = racingCarView.inputCarInfo();
+        String carNames = RacingCarView.inputCarInfo();
 
         RacingGame racingGame = new RacingGame();
-        List<Cars> carsInfos = racingGame.createCarInformation(carNames);
+        Car car = racingGame.createCarInformation(carNames);
 
-        racingCarView.inputCycle();
-        racingCarView.showResult(carsInfos);
+        RacingCarView.inputCycle();
+        RacingCarView.showResult(car);
 
         List<Cars> winnerNames = racingGame.selectWinners();
-        racingCarView.showWinner(winnerNames);
+        RacingCarView.showWinner(winnerNames);
     }
 }
