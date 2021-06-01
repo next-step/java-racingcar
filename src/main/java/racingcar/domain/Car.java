efc3a6010bfb3c6438c502f6cf5ac1981f190587
hailey <wjdguswls55@gmail.com>
2021-06-01 18:11:24 +0900

    refactor : 유효성 검증 코드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,17 +2,28 @@ package racingcar.domain;
 
 public class Car {
     private RacingRandom racingRandom = new RacingRandom();
+    private static final int CAR_LENGTH = 5;
     private String name;
     private int location;
 
     public Car(String name, int location) {
+        invalidedName(name);
+
         this.name = name;
         this.location = location;
     }
 
     public Car(String name) {
+        invalidedName(name);
+
         this.name = name;
         this.location = 0;
+    }
+
+    public void invalidedName(String name) {
+        if (name.length() > CAR_LENGTH) {
+            throw new IllegalArgumentException("자동차의 이름은 5자를 초과할 수 없다.");
+        }
     }
 
     public void move() {
@@ -21,7 +32,7 @@ public class Car {
         }
     }
 
-    public boolean compareWinnerScore(int winnerScore){
+    public boolean compareWinnerScore(int winnerScore) {
         return location == winnerScore;
     }
 
