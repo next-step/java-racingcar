2097126f89d55ee1a4f6b224884a8efa4d6d50dd
xxhyxxnjxn <hyunjin077@naver.com>
2021-06-01 23:29:11 +0900

    refactor : static을 사용

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
 import java.util.List;
@@ -8,28 +9,28 @@ import java.util.Scanner;
 
 public class RacingCarView {
 
-    private Scanner scan = new Scanner(System.in);
-    private int cycle = 0;
+    private static final Scanner scan = new Scanner(System.in);
+    private static int cycle = 0;
 
-    public String inputCarInfo() {
+    public static String inputCarInfo() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = scan.next();
         return input;
     }
 
-    public void inputCycle() {
+    public static void inputCycle() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         cycle = scan.nextInt();
     }
 
-    public void showResult(List<Cars> carsInfo) {
+    public static void showResult(Car car) {
         System.out.println("실행결과");
         for (int i = 0; i < cycle; i++) {
-            cycleCarInformation(carsInfo);
+            cycleCarInformation(car.getCars());
         }
     }
 
-    private void cycleCarInformation(List<Cars> carsInfo) {
+    private static void cycleCarInformation(List<Cars> carsInfo) {
         for (int i = 0; i < carsInfo.size(); i++) {
             carsInfo.get(i).move();
 
@@ -39,13 +40,13 @@ public class RacingCarView {
         }
     }
 
-    private void showCarLocationInfo(int moveCount) {
+    private static void showCarLocationInfo(int moveCount) {
         for (int i = 0; i < moveCount; i++) {
             System.out.print("-");
         }
     }
 
-    public void showWinner(List<Cars> winnerNames) {
+    public static void showWinner(List<Cars> winnerNames) {
         System.out.print("최종 우승자 : ");
         winnerNames.stream().filter(Objects::nonNull).forEach(winner -> System.out.print(winner.getName()));
     }
