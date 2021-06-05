93d8340c63ef6ca7be7489d8a5b33b1eeb60f64e
이예림 <dulfla_5@naver.com>
2021-06-06 00:17:51 +0900

    test : createRandomNumber 테스트 추가

---
+++ src/test/java/RacingCarGameTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcargame.model.RandomNumber;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingCarGameTest {
+    @DisplayName(value = "랜덤수는 1부터 10까지이다.")
+    @Test
+    void is_between_1_and_10() {
+        RandomNumber randomNumber = new RandomNumber();
+        int v = randomNumber.createRandomNumber();
+        assertThat(v >= 1 && v <= 10).isTrue();
+    }
+}
