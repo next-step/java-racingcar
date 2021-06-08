7dd45ddd4f50aafc2dbb403a698386f0bd308099
Data-ssung <67546948+Data-ssung@users.noreply.github.com>
2021-06-08 22:10:30 +0900

    (refactor) 상수명 WHITE_SPACE 변경, 메소드 순서 변경

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -1,26 +1,20 @@
 package calculator;
 
 public class Input {
-    private static final String SPLIT_VALUE= " ";
+    private static final String WHITE_SPACE= " ";
 
-    // 빈값 (null 값 or " ") 판단하기
-    public Boolean checkBlank(String blankValue) {
-        if (blankValue == null || blankValue.equals(" ") || blankValue.equals("")) {
-            return true;
-        }
-        return false;
-    }
-
-    //  input값 " "기준으로 split하기
-    public String[] splitter(String input) {
-        return input.split(SPLIT_VALUE);
-    }
-
-    // splitter 테스트할 때, 예외 처리
     public String[] isSplit(String testInput) {
         if (checkBlank(testInput)) {
             throw new IllegalArgumentException();
         }
         return splitter(testInput);
+    }
+
+    public String[] splitter(String input) {
+        return input.split(WHITE_SPACE);
+    }
+
+    private Boolean checkBlank(String blankValue) {
+        return (blankValue == null || blankValue.equals(" ") || blankValue.equals(""));
     }
 }
