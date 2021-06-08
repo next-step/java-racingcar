b14762509da4caf4e69dce853f21070ab3b45b85
Data-ssung <67546948+Data-ssung@users.noreply.github.com>
2021-06-08 22:19:05 +0900

    (refactor) checkBlank 메소드 테스트 삭제

--- src/test/java/calculator/InputTest.java
+++ src/test/java/calculator/InputTest.java
@@ -1,14 +1,15 @@
 package calculator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InputTest {
+    private final Input input = new Input();
     private String testInput;
 
     @BeforeEach
@@ -16,41 +17,10 @@ public class InputTest {
         testInput = "2 + 3";
     }
 
-    // null값 판단 테스트
     @Test
-    void nullTest() {
-        // given
-        String nullValue = null;
-        Input input = new Input();
-
-        // when
-        Boolean actual = input.checkBlank(nullValue);
-
-        //then
-        assertTrue(actual);
-    }
-
-    // 빈값 ("" or " ") 판단 테스트
-    @ParameterizedTest
-    @ValueSource(strings = {"", " "})
-    void checkBlankTest(String blankInput) {
-        // given
-        Input input = new Input();
-
-        // when
-        Boolean actual = input.checkBlank(blankInput);
-
-        //then
-        assertTrue(actual);
-    }
-
-    // split 정상 케이스 테스트
-    @Test
+    @DisplayName("splitterTest 정상 케이스")
     void splitterTest() {
         // given
-        Input input = new Input();
-
-        // when
         String[] expected = {"2", "+", "3"};
         String[] actual = input.isSplit(testInput);
 
@@ -58,13 +28,10 @@ public class InputTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    // split 예외상황(빈값 들어왔을 때) 테스트
     @ParameterizedTest
     @ValueSource(strings = {"", " "})
+    @DisplayName("splitterTest 예외 케이스")
     void isSplitExceptionTest(String testValue) {
-        // given
-        Input input = new Input();
-
         //then
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> input.isSplit(testValue));
