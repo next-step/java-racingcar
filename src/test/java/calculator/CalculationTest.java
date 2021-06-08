04840a2cdd07559a9d09bae106f0a6cdd8d4e3cc
Data-ssung <67546948+Data-ssung@users.noreply.github.com>
2021-06-08 22:28:28 +0900

    (refactor) 각 테스트에 DisplayName 추가

--- src/test/java/calculator/CalculationTest.java
+++ src/test/java/calculator/CalculationTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -8,9 +9,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CalculationTest {
-    // 정상 케이스 테스트
+
     @ParameterizedTest
     @CsvSource(value = {"2 + 3 * 4 / 2,10","2 + 3 * 4,20","3 * 4 / 2,6"})
+    @DisplayName("정상 케이스 테스트")
     void calculationTest(String inputValue, int expected) {
         // given
         Calculation calculation = new Calculation();
@@ -25,6 +27,7 @@ public class CalculationTest {
     // 예외 테스트
     @ParameterizedTest
     @ValueSource(strings = {" ","","^","%"})
+    @DisplayName("예외 케이스 테스트")
     void calculationExceptionTest(String exceptionValue) {
         // given
         Calculation calculation = new Calculation();
