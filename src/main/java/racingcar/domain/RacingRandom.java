29e34fcae59ebf712b59ac62553d6669326a4e9a
hailey <wjdguswls55@gmail.com>
2021-06-09 13:21:02 +0900

    refactor : 상수 이름 변경

--- src/main/java/racingcar/domain/RacingRandom.java
+++ src/main/java/racingcar/domain/RacingRandom.java
@@ -3,14 +3,14 @@ package racingcar.domain;
 import java.util.Random;
 
 public class RacingRandom {
-    private static final int RANDOM_NUMBER = 10;
-    private static final int MOVABLE_NUMBER = 4;
+    private static final int RACING_NUMBER = 10;
+    private static final int MOVABLE_CONDITION = 4;
 
     private static final Random random = new Random();
 
     public boolean isMove() {
-        int randomNumber = random.nextInt(RANDOM_NUMBER);
-        return randomNumber >= MOVABLE_NUMBER;
+        int randomNumber = random.nextInt(RACING_NUMBER);
+        return randomNumber >= MOVABLE_CONDITION;
     }
 
 }
