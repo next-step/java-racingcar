51cb7d5faa16dd0122c6ea117425b3ab0053e18c
DESKTOP-JH4A8SV\ceo <siryugubin@naver.com>
2021-06-09 14:50:09 +0900

    refactor : Assertion import 시키기

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SetTest {
     private Set<Integer> numbers;
 
@@ -30,12 +32,12 @@ public class SetTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     void isBlank_ShouldReturnTrueForNullOrBlankStrings(int input) {
-        Assertions.assertTrue(numbers.contains(input));
+        assertTrue(numbers.contains(input));
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
     void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(int input, boolean expected) {
-        Assertions.assertEquals(numbers.contains(input), expected);
+        assertEquals(numbers.contains(input), expected);
     }
 }
