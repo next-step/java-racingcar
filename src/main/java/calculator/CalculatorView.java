d9f57b039d6ad56f94d0cd72dbf299d1e96798be
hailey <wjdguswls55@gmail.com>
2021-06-15 10:40:16 +0900

    refactor : 상수 변수명 변경

--- src/main/java/calculator/CalculatorView.java
+++ src/main/java/calculator/CalculatorView.java
@@ -3,12 +3,12 @@ package calculator;
 import java.util.Scanner;
 
 public class CalculatorView {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static String inputString() {
         System.out.println("문자열을 입력하세요.");
 
-        return scanner.nextLine();
+        return SCANNER.nextLine();
     }
 
     public static void showResult(int result) {
