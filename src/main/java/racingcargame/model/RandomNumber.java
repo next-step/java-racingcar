2e20f07966f41ff2ba3dba020fda8f4e58f3eebd
이예림 <dulfla_5@naver.com>
2021-06-17 00:30:55 +0900

    refactor : RandomNumber 클래스를 생성할 때 random 변수 선언하기

--- src/main/java/racingcargame/model/RandomNumber.java
+++ src/main/java/racingcargame/model/RandomNumber.java
@@ -3,7 +3,11 @@ package racingcargame.model;
 import java.util.Random;
 
 public class RandomNumber {
-    private Random random = new Random();
+    private Random random;
+
+    public RandomNumber() {
+        random = new Random();
+    }
 
     public int createRandomNumber() {
         return random.nextInt(10);
