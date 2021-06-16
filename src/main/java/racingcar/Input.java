0cdc8668f90a8758aea59b1a4de6bbab0578e54a
DESKTOP-5DMQS7B\FinTech <ssungmi2012@gmail.com>
2021-06-16 11:25:28 +0900

    (refactor) nextline을 next을 바꾸기

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -7,11 +7,16 @@ public class Input {
 
     public String carName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return scanner.nextLine();
+        return scanner.next();
     }
 
     public int gameCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         return scanner.nextInt();
+    }
+
+    public static void main(String[] args) {
+        String name = new Input().carName();
+        System.out.println(name);
     }
 }
