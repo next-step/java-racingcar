4069d992fbeab091893fbb720e2701dc6c171c3a
DESKTOP-5DMQS7B\FinTech <ssungmi2012@gmail.com>
2021-06-16 14:19:15 +0900

    (feature) Award 기능 추가(Stadium에서 시상식 기능 분리)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         Ready ready = new Ready();
         Input input = new Input();
+        Award award = new Award();
 
         List<Car> cars = ready.createCars(input.carName());
         int gameCount = input.gameCount();
@@ -13,7 +14,7 @@ public class Application {
         Stadium stadium = new Stadium(cars, gameCount);
         stadium.playRace();
 
-        List<Car> result = stadium.award();
-        System.out.println("최종 우승자 : " + stadium.winnerIS(result));
+        List<Car> winner= award.findWinner(stadium.getCars());
+        System.out.println("최종 우승자 : " + award.winnerIS(winner));
     }
 }
