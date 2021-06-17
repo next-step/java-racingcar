f45d73feb3444e5ae41a777fe62c7937c3e13125
이예림 <dulfla_5@naver.com>
2021-06-17 17:04:10 +0900

    refactor : RacingCarGameTest 에서 RandomNumberTest 로 테스트 내용에 맞게 클래스명 변경

--- src/test/java/racingcargame/RandomNumberTest.java
+++ src/test/java/racingcargame/RandomNumberTest.java
@@ -6,7 +6,7 @@ import racingcargame.model.RandomNumber;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RacingCarGameTest {
+public class RandomNumberTest {
     @DisplayName(value = "랜덤수는 1부터 10까지이다.")
     @Test
     void random_number_is_between_1_and_10() {
