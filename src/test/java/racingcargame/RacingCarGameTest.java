c944e91f32511e1d57b4ad83cc3a2a0716ca45fe
이예림 <dulfla_5@naver.com>
2021-06-17 17:03:02 +0900

    refactor : RacingCarGameTest 패키지 나누기 및 RacingCarGameTest#random_number_is_between_1_and_10 테스트명 변경

--- src/test/java/racingcargame/RacingCarGameTest.java
+++ src/test/java/racingcargame/RacingCarGameTest.java
@@ -1,3 +1,5 @@
+package racingcargame;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcargame.model.RandomNumber;
@@ -7,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RacingCarGameTest {
     @DisplayName(value = "랜덤수는 1부터 10까지이다.")
     @Test
-    void is_between_1_and_10() {
+    void random_number_is_between_1_and_10() {
         RandomNumber randomNumber = new RandomNumber();
         int v = randomNumber.createRandomNumber();
         assertThat(v >= 1 && v <= 10).isTrue();
