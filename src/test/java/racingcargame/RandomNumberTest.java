a9af33f650f233db8c9ec7a100ddac630c4b107c
이예림 <dulfla_5@naver.com>
2021-06-28 19:13:51 +0900

    refactor : RacingCarNumberTest#random_number_is_between_1_and_10 테스트 코드 더 간결하게 수정

--- src/test/java/racingcargame/RandomNumberTest.java
+++ src/test/java/racingcargame/RandomNumberTest.java
@@ -10,8 +10,6 @@ public class RandomNumberTest {
     @DisplayName(value = "랜덤수는 1부터 10까지이다.")
     @Test
     void random_number_is_between_1_and_10() {
-        RandomNumber randomNumber = new RandomNumber();
-        int v = randomNumber.createRandomNumber();
-        assertThat(v >= 1 && v <= 10).isTrue();
+        assertThat(RandomNumber.createRandomNumber()).isBetween(1,10);
     }
 }
