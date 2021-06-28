51c0efa0018d39be02fa1e672be49afac986217f
이예림 <dulfla_5@naver.com>
2021-06-28 21:21:40 +0900

    feat : Histories 객체 생성 (History 객체의 일급 컬렉션)

---
+++ src/main/java/racingcargame/model/Histories.java
@@ -0,0 +1,15 @@
+package racingcargame.model;
+
+import java.util.List;
+
+public class Histories {
+    private List<History> histories;
+
+    public Histories(List<History> histories) {
+        this.histories = histories;
+    }
+
+    public List<History> getHistories() {
+        return histories;
+    }
+}
