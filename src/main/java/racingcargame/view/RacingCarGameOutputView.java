a71a10b0f242e57ad40de6165b5616cdadaa3a0e
이예림 <dulfla_5@naver.com>
2021-06-28 21:30:51 +0900

    refactor : RacingCarGameOutputView#outputGame 라운드별 history 출력 feat : RacingCarGameOutputView#outputRound 자동차별 상태 출력 refactor : RacingCarGameOutputView#stackUpBars 출력에 관한 기능이므로 outputview 에 맡기기

--- src/main/java/racingcargame/view/RacingCarGameOutputView.java
+++ src/main/java/racingcargame/view/RacingCarGameOutputView.java
@@ -1,13 +1,33 @@
 package racingcargame.view;
 
+import racingcargame.model.History;
+
 import java.util.List;
+import java.util.Map;
 
 public class RacingCarGameOutputView {
-    public void outputGame(List<String> histories) {
+    private static final String BAR = "-";
+
+    public void outputGame(List<History> histories) {
         System.out.println("\n실행 결과");
-        for (String history : histories) {
-            System.out.println(history);
+        for (History history : histories) {
+            outputRound(history.getHistory());
+            System.out.println();
         }
+    }
+
+    private void outputRound(Map<String, Integer> history) {
+        for (Map.Entry<String, Integer> entry : history.entrySet()) {
+            System.out.println(entry.getKey() + " : " + stackUpBars(entry.getValue()));
+        }
+    }
+
+    private String stackUpBars(int step) {
+        StringBuilder bar = new StringBuilder();
+        for (int i = 0; i < step; i++) {
+            bar.append(BAR);
+        }
+        return bar.toString();
     }
 
     public void outputWinners(String winners) {
