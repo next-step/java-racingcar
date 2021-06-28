ba2402d8056507fc81ddee240e3fbd15335385e6
이예림 <dulfla_5@naver.com>
2021-06-28 19:14:35 +0900

    refactor : RacingCarGameOutputView#outputGame 향상된 for문 사용

--- src/main/java/racingcargame/view/RacingCarGameOutputView.java
+++ src/main/java/racingcargame/view/RacingCarGameOutputView.java
@@ -3,9 +3,9 @@ package racingcargame.view;
 import java.util.List;
 
 public class RacingCarGameOutputView {
-    public void outputGame(List<String> history) {
-        for (int i = 0; i < history.size(); i++) {
-            System.out.println(history.get(i));
+    public void outputGame(List<String> histories) {
+        for (String history : histories) {
+            System.out.println(history);
         }
     }
 
