54577e4156b95cd3d264c2548b36b11eb76ff1cf
이예림 <dulfla_5@naver.com>
2021-06-28 21:54:22 +0900

    test : RacingCar 객체에 대한 테스트 추가

---
+++ src/test/java/racingcargame/RacingCarTest.java
@@ -0,0 +1,41 @@
+package racingcargame;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcargame.model.RacingCar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingCarTest {
+    RacingCar racingCar;
+
+    @BeforeEach
+    void setUp() {
+        racingCar = new RacingCar("yerim");
+    }
+
+    @DisplayName(value = "자동차는 움직인다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6})
+    void move(final int number) {
+        racingCar.move(number);
+        assertThat(racingCar.getStep()).isEqualTo(1);
+    }
+
+    @DisplayName(value = "자동차는 움직이지 않는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void stop(final int number) {
+        racingCar.move(number);
+        assertThat(racingCar.getStep()).isEqualTo(0);
+    }
+
+    @DisplayName(value = "자동차가 우승자이다.")
+    @Test
+    void is_winner() {
+        assertThat(racingCar.isWinner(0)).isTrue();
+    }
+}
