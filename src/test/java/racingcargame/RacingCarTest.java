0f82df843d1be60ca8b9a6cb011c5ed2ef5544dd
이예림 <dulfla_5@naver.com>
2021-07-05 13:53:16 +0900

    refactor : RacingCarTest#move RacingCarTest#stop 움직임 조건 테스트 분리

--- src/test/java/racingcargame/RacingCarTest.java
+++ src/test/java/racingcargame/RacingCarTest.java
@@ -3,36 +3,29 @@ package racingcargame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import racingcargame.model.MoveStrategy;
 import racingcargame.model.RacingCar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarTest {
     RacingCar racingCar;
-    MoveStrategy moveStrategy;
 
     @BeforeEach
     void setUp() {
         racingCar = new RacingCar("yerim");
-        moveStrategy = new MoveStrategy();
     }
 
     @DisplayName(value = "자동차는 움직인다.")
-    @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @Test
     void move(final int number) {
-        racingCar.move(moveStrategy.isMovable(number));
+        racingCar.move(true);
         assertThat(racingCar.getStep()).isEqualTo(1);
     }
 
     @DisplayName(value = "자동차는 움직이지 않는다.")
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3})
+    @Test
     void stop(final int number) {
-        racingCar.move(moveStrategy.isMovable(number));
+        racingCar.move(false);
         assertThat(racingCar.getStep()).isEqualTo(0);
     }
 
