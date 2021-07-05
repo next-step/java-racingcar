e7eff9a0e4a2e0e3e96d00b27c91ed2d29a7638d
이예림 <dulfla_5@naver.com>
2021-07-05 13:13:24 +0900

    refactor : RandomNumberTest#random_number_is_between_0_and_9 요구사항에 맞게 메서드명과 테스트 범위 변경

--- src/test/java/racingcargame/RandomNumberTest.java
+++ src/test/java/racingcargame/RandomNumberTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RandomNumberTest {
     @DisplayName(value = "랜덤수는 1부터 10까지이다.")
     @Test
-    void random_number_is_between_1_and_9() {
-        assertThat(RandomNumber.createRandomNumber()).isBetween(1,10);
+    void random_number_is_between_0_and_9() {
+        assertThat(RandomNumber.createRandomNumber()).isBetween(0, 9);
     }
 }
