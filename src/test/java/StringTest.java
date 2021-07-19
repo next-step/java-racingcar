d86eab92de9a4682d7193eea500e5df5cc89b689
jspark940321 <jspark940321@gmail.com>
2021-07-19 23:55:58 +0900

    feat: 요구사항1
    String "1,2"과 "1"을 ','문자로 split시 잘 분리되는지 확인하는 테스트코드
    assertj의 contains(), containsExactly()를 이용하여 확인

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    static String[] splitedStrings;
+
+    @BeforeEach
+    public void setSplitStrings(){
+        splitedStrings = "1,2".split(",");
+    }
+
+    // contains는 배열시 순서상관 없음
+    @Test
+    public void 요구사항1_contains1to2(){
+        assertThat(splitedStrings).contains("1","2");
+    }
+    @Test
+    public void 요구사항1_contains2to1(){
+        assertThat(splitedStrings).contains("2","1");
+    }
+    
+    //containsExactly의 경우 순서 상관 있음
+    @Test
+    public void 요구사항1_containsExactly1to2(){
+        assertThat(splitedStrings).containsExactly("1","2");
+    }
+    @Test
+    public void 요구사항1_containsExactly2to1(){
+        assertThat(splitedStrings).containsExactly("2","1");
+    }
+
+    @Test
+    public void 요구사항1_containsOnly1(){
+        splitedStrings = "1".split(",");
+        assertThat(splitedStrings).containsExactly("1");
+    }
+}
