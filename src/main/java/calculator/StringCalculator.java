d32c9702f819be7320ed90623b341fb49c4712d4
ckdgus08 <ckdgus0808@naver.com>
2021-07-20 03:26:38 +0900

    feat: 문자열 사칙연산 계산 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -9,4 +9,12 @@ public class StringCalculator {
             throw new IllegalArgumentException("입력 값은 null 이거나 빈 공백 문자가 올 수 없습니다.");
         return new Elements(input.split(" "));
     }
+
+    public int calculate(Elements elements) {
+        return elements.calculate();
+    }
+
+    public int calculate(String input) {
+        return splitWithSpace(input).calculate();
+    }
 }
