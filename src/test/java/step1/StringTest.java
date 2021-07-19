4c793b780f32899987e47dad003c4d6ee8682907
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-07-20 08:58:17 +0900

    step1 - 피드백 반영

--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -1,3 +1,5 @@
+package step1;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,34 +9,37 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class StringTest {
 
     @Test
-    void split_배열에_1과_2가_존재하는가() {
-        String result[] = "1,2".split(",");
+    @DisplayName("split_배열에_1과_2가_존재하는가")
+    void split_exists_one_two() {
+        String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
 
     @Test
-    void split_배열에_1이_존재하는가() {
+    @DisplayName("split_배열에_1이_존재하는가")
+    void split_exists_one() {
         final String one = "1";
-        String result[] = one.split(",");
+        String[] result = one.split(",");
         assertThat(result).containsExactly(one);
     }
 
     @Test
-    void substring_기능_테스트() {
+    @DisplayName("substring_기능_테스트")
+    void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
     }
 
     @Test
     @DisplayName("특정 문자를 charAt()으로 특정 위치 문자 가져오는 테스트")
-    void charAt_기능_테스트() {
+    void charAt() {
         char ch = "abc".charAt(2);
         assertThat(ch).isEqualTo('c');
     }
 
     @Test
     @DisplayName("charAt() 범위 초과되어 StringIndexOutOfBoundsException 발생")
-    void charAt_범위_초과_StringIndexOutOfBoundsException_발생_확인() {
+    void charAt_index_out_of_range() {
         final int index = 4;
         final String word = "abc";
         assertThatThrownBy(() -> word.charAt(index))
