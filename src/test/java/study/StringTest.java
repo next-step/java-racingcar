e6c0f53568f019b3ce8558c6a366cfc506a3df56
jung0930 <wjddlqkqh57@naver.com>
2021-07-19 23:26:25 +0900

    test : [String 클래스에 대한 학습 테스트] split 테스트 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,24 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+
     @Test
-    void split() {
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 분리되는지 테스트")
+    void split1() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 테스트")
+    void split2() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
 }
