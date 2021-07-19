6d3026b04966c47528f417b7658b2e45db1a109a
jung0930 <wjddlqkqh57@naver.com>
2021-07-20 00:20:22 +0900

    test : [Set 클래스에 대한 학습 테스트] @CsvSource을 이용하여 true, false값 모두 테스트가능한 테스트코드 작성

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -32,8 +34,15 @@ public class SetTest {
     @DisplayName("contains() 메소드를 사용해 1,2,3 값이 존재하는지 테스트")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void containsTest(int input) {
+    void containsTest1(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @DisplayName("1, 2, 3 값은 contains() 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsTest2(int input, boolean result) {
+        assertEquals(numbers.contains(input), result);
     }
 
 }
