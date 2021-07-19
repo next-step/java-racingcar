e0720b67c16afb30bc577cf01aaaba5e9de09492
jung0930 <wjddlqkqh57@naver.com>
2021-07-19 23:59:22 +0900

    refactor : 테스트코드 중 @DisplayName 위치 변경

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,22 +9,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
 
-    @Test
     @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 분리되는지 테스트")
+    @Test
     void split1() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
 
-    @Test
     @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 테스트")
+    @Test
     void split2() {
         String[] result = "1".split(",");
         assertThat(result).containsExactly("1");
     }
 
-    @Test
     @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환되는지 테스트")
+    @Test
     void substring() {
         String str = "(1,2)";
 
@@ -36,9 +36,9 @@ public class StringTest {
         assertThat(str).contains("1");
     }
 
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 사용해 특정 위치의 문자를 가져오는지 테스트")
     @ParameterizedTest
     @CsvSource({"0,a", "1,b", "2,c"})
-    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 사용해 특정 위치의 문자를 가져오는지 테스트")
     void charAt1(int index, char ch) {
         String str = "abc";
         assertThat(str.charAt(index)).isEqualTo(ch);
