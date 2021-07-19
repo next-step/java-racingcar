40f94322b0a1e171353bf4c03ce6cfd72ff7f28c
anbin <idealbin@naver.com>
2021-07-19 23:41:42 +0900

    [test] : SetTest, StringTest 테스트코드 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,36 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
+    @DisplayName("문자열 분리를 성공한다.")
     void split() {
         String[] result = "1,2".split(",");
-        assertThat(result).containsExactly("1", "2");
+        assertThat(result).contains("1", "2");
+
+        String[] result2 = "1".split(",");
+        assertThat(result2).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("문자열 자르기를 성공한다.")
+    void substring() {
+        String result = "(1,2)".substring(1,4);
+        assertEquals(result,"1,2");
+    }
+
+    @Test
+    @DisplayName("IndexOutOfBoundsException 테스트")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
