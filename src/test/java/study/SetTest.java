07e951b7038d3bb84e74614d7c79ba18325fca82
jung0930 <wjddlqkqh57@naver.com>
2021-07-20 00:11:57 +0900

    test : [Set 클래스에 대한 학습 테스트] size() 메소드에 대한 테스트코드 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,2 +1,30 @@
-package study;public class SetTest {
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size() 메소드가 3을 반환하는지 테스트")
+    @Test
+    void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
 }
