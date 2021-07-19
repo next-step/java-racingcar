e638ab2bcd190e6d3e9550ba4a642d12e3b411af
shirohoo <hch4102@naver.com>
2021-07-19 11:23:13 +0900

    feat: Create MessageFactory
    - creating message factories for String instance reuse
    - createing MessageFactoryTest

---
+++ src/main/java/type/MessageFactory.java
@@ -0,0 +1,9 @@
+package type;
+
+public class MessageFactory {
+    private static final String INPUT_ERROR_MESSAGE = "입력값이 올바르지 않습니다.";
+
+    public static String getInputErrorMessage() {
+        return INPUT_ERROR_MESSAGE;
+    }
+}
