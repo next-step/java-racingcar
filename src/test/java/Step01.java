2edab8db03e7658911461f5fb1caed33985e5cb0
rkdals213 <rkdals213@naver.com>
2021-07-19 12:33:03 +0900

    test: String 클래스에 대한 학습 테스트

---
+++ src/test/java/Step01.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class Step01 {
+
+    @Test
+    public void 문자열_분리_테스트() {
+        //given
+        String text = "1,2";
+        String[] expected = {"1", "2"};
+
+        //when
+        String[] split = text.split(",");
+
+        //then
+        assertThat(split).contains(text);
+        assertThat(split).isEqualTo(expected);
+    }
+
+    @Test
+    public void 문자열_분리_테스트2() {
+        //given
+        String text = "1";
+        String[] expected = {"1"};
+
+        //when
+        String[] split = text.split(",");
+
+        //then
+        assertThat(split).contains(text);
+        assertThat(split).isEqualTo(expected);
+    }
+
+    @Test
+    public void 문자열_SubString_테스트() {
+        //given
+        String text = "(1,2)";
+        String expected = "1,2";
+
+        //when
+        String substring = text.substring(1, text.length() - 1);
+
+        //then
+        assertThat(substring).isEqualTo(expected);
+    }
+
+    @Test
+    public void 문자열_특정위치_문자_가져오기_테스트_실패() {
+        //given
+        String text = "abc";
+
+        //when, then
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+            text.charAt(3);
+        }).withMessageMatching("String index out of range: \\d+");
+    }
+
+    @Test
+    public void 문자열_특정위치_문자_가져오기_테스트_실패2() {
+        //given
+        String text = "abc";
+
+        //when, then
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+            text.charAt(-1);
+        }).withMessageMatching("String index out of range: -\\d+");
+    }
+}
