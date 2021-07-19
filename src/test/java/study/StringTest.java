8918e9df8055114f2f3d5e04645f539fbf9606dd
janghoonseob <arpy1228@gmail.com>
2021-07-20 00:18:39 +0900

    test: step1 요구사항 2, 3 테스트케이스 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
@@ -10,6 +11,25 @@ public class StringTest {
     void split() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substringTest() {
+        String result = "(1,2)".substring(1,4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("String Exception Test")
+    @Test
+    void stringExceptionTest() {
+        String str = "abc";
+        assertThatThrownBy(() -> str.charAt(3))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range:");
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> str.charAt(3))
+                .withMessageMatching("String index out of range: \\d+");
     }
 
 }
