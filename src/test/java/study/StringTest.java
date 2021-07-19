9d98b7fb157fd3720d4bff0d35c8e738bf40e39c
jung0930 <wjddlqkqh57@naver.com>
2021-07-20 00:08:53 +0900

    refactor : 테스트코드 메소드 명 변경

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -13,21 +13,21 @@ public class StringTest {
 
     @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 분리되는지 테스트")
     @Test
-    void split1() {
+    void splitTest1() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
 
     @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 테스트")
     @Test
-    void split2() {
+    void splitTest2() {
         String[] result = "1".split(",");
         assertThat(result).containsExactly("1");
     }
 
     @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환되는지 테스트")
     @Test
-    void substring() {
+    void substringTest() {
         String str = "(1,2)";
 
         int beginIndex = str.indexOf("(") + 1;
@@ -41,7 +41,7 @@ public class StringTest {
     @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 사용해 특정 위치의 문자를 가져오는지 테스트")
     @ParameterizedTest
     @CsvSource({"0,a", "1,b", "2,c"})
-    void charAt1(int index, char ch) {
+    void charAtTest1(int index, char ch) {
         String str = "abc";
         assertThat(str.charAt(index)).isEqualTo(ch);
     }
@@ -49,7 +49,7 @@ public class StringTest {
     @DisplayName("charAt() 메소드를 사용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException 익셉션 발생하는지 테스트")
     @ParameterizedTest
     @ValueSource(ints = {-1, 3, 4})
-    void charAt2(int index) {
+    void charAtTest2(int index) {
         String str = "abc";
 
         assertThatThrownBy(() -> {
