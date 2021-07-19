c67c6f26db306e64063c2b83d85709744948255c
ckdgus08 <ckdgus0808@naver.com>
2021-07-20 01:55:29 +0900

    feat: 공백기준으로 문자열 분리 함수 구현

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+
+    StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @ParameterizedTest(name = "{displayName} [{index}] {0} ")
+    @ValueSource(strings = {"2 + 3 * 4 / 2"})
+    @DisplayName("공백 단위로 문자열을 분리한다.")
+    void splitWithSpace(String input) {
+        String[] result = stringCalculator.splitWithSpace(input);
+        assertThat(result).containsExactly("2", "+", "3", "*", "4", "/", "2");
+    }
+}
