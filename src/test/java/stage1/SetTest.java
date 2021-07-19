2dfe138ddb1a75f7af63c5f6ea4d17a8f5dc7f61
김민석 <englosia62@gmail.com>
2021-07-19 23:10:28 +0900

    패키지명 수정

--- src/test/java/stage1/SetTest.java
+++ src/test/java/stage1/SetTest.java
@@ -1,4 +1,4 @@
-package Stage1;
+package stage1;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
--- src/test/java/stage1/StringTest.java
+++ src/test/java/stage1/StringTest.java
@@ -1,4 +1,4 @@
-package Stage1;
+package stage1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
