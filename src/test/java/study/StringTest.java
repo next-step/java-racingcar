e365a999fea3b3ba18fd0b24adef15cc41e99291
김남준 <namjun88@gmail.com>
2021-07-19 22:33:46 +0900

    substring test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,20 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static  org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringTest {
     @Test
-    void split() {
+    void split () {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
     }
+
+    @Test
+    void subString_요구사항2 () {
+        String testDate = "(1,2)";
+        String result = testDate.substring(1,4);
+        assertEquals("1,2", result);
+    }
+    
 }
