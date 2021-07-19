7b87efd2bc6f67eda12e8f6900939e57a7dc1b0a
lsj8367 <lsj8367@naver.com>
2021-07-19 09:42:21 +0900

    feat: StringTest 요구사항 2, 3번 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,15 +1,35 @@
 package study;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
-    void split(){
+    void splitTest(){
         String[] result = "1,2".split(",");
-        assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void subStringTest(){
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).contains("1,2");
+    }
+
+    @Test
+    @DisplayName("특정위치 문자 가져오는 테스트")
+    void charAtGetTest(){
+        String text = "abc";
+        assertThat(text.charAt(0)).isEqualTo('a');
+        assertThat(text.charAt(1)).isEqualTo('b');
+        assertThat(text.charAt(2)).isEqualTo('c');
+
+        assertThatThrownBy(() -> {
+            text.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+          .hasMessageContaining("String index out of range");
     }
 }
