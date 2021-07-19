069fcc319b0552765cf20ed4a0f9fe1e4b9ed022
shirohoo <hch4102@naver.com>
2021-07-19 16:16:50 +0900

    feat: RacingGameRunner
    RacingGameRunner is facade

---
+++ src/main/java/racingcar/RacingGameRunner.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class RacingGameRunner {
+    public static void main(String[] args) {
+        RacingGamePrinter.print(
+                RacingGameCore.of(
+                        RacingGameScanner.enterCarCount(),
+                        RacingGameScanner.enterStageCount()
+                ).run()
+        );
+    }
+}
