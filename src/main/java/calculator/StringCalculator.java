b0710f76f49834e588d53b1412ff001762c3c4a3
ckdgus08 <ckdgus0808@naver.com>
2021-07-20 03:35:52 +0900

    refactor: elements 지역변수 제거

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,6 @@ package calculator;
 
 public class StringCalculator {
 
-    private Elements elements;
-
     public Elements splitWithSpace(String input) {
         if (input == null || input.isEmpty())
             throw new IllegalArgumentException("입력 값은 null 이거나 빈 공백 문자가 올 수 없습니다.");
