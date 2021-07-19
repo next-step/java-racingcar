67a7c87f43f72f3cd7ab282420c113ef054a0fb9
jung0930 <wjddlqkqh57@naver.com>
2021-07-19 23:10:48 +0900

    split study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,2 +1,13 @@
-package study;public class StringTest {
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
 }
