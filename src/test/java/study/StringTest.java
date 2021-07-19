7b5a76a02cf28daf31e75c12ca23ce120909b4c1
PIH6585 <pih6585@naver.com>
2021-07-19 15:14:30 +0900

    1단계 string 관련 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,15 @@
 package study;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
     @Test
+    @DisplayName("요구사항1 - 1,2을 ','로 split 값 확인 ")
     void split(){
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
@@ -15,9 +17,29 @@ public class StringTest {
     }
 
     @Test
-    void split_requireStep2(){
-        String result = "1,2".substring(0,3);
+    @DisplayName("요구사항1 - 1을 ','로 split 값 확인 ")
+    void split_requireStep1_1(){
+        String[] result = "1".split(",");
+        assertThat(result).contains("1");
+        assertThat(result[0]).isEqualTo("1");
+    }
+
+    @Test
+    @DisplayName("요구사항2 -'(1,2)' -> ()을 제고하고 '1,2'를 반환 ")
+    void string_requireStep2(){
+        String result = "(1,2)".substring(1, 4);
+
         assertThat(result).isEqualTo("1,2");
+        assertThat(result).contains("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자를 가져오기")
+    void string_requireStep3(){
+        String result = "abc";
+        int index = 2;
+        assertThatThrownBy(() -> {result.charAt(index);}).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("Index: 4, Size: 4");
     }
 
 }
