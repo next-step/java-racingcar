97de39cd89aed06a0b60f21c4316a42bb4cff6ca
김남준 <namjun88@gmail.com>
2021-07-19 23:01:16 +0900

    charAt test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
+import static  org.assertj.core.api.Assertions.*;
 import static  org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,5 +19,23 @@ public class StringTest {
         String result = testDate.substring(1,4);
         assertEquals("1,2", result);
     }
-    
+
+    @Test
+    void charAt_요구사항3 () {
+
+        char result = "abc".charAt(0);
+        assertThat(result).isEqualTo('a');
+
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("index out of range: 3");
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    "abc".charAt(3);
+                }).withMessageMatching("String index out of range: 3");
+
+    }
+
 }
