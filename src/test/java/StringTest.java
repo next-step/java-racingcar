edea1c2e28483b0f9b4da126ee7f1a88181798c0
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-07-19 21:18:01 +0900

    step1 - String 클래스에 대한 학습 테스트 (요구사항 3)

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,6 +1,8 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -21,5 +23,22 @@ public class StringTest {
     void substring_기능_테스트() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 문자를 charAt()으로 특정 위치 문자 가져오는 테스트")
+    void charAt_기능_테스트() {
+        char ch = "abc".charAt(2);
+        assertThat(ch).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("charAt() 범위 초과되어 StringIndexOutOfBoundsException 발생")
+    void charAt_범위_초과_StringIndexOutOfBoundsException_발생_확인() {
+        final int index = 4;
+        final String word = "abc";
+        assertThatThrownBy(() -> word.charAt(index))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining(String.format("String index out of range: %d", index));
     }
 }
