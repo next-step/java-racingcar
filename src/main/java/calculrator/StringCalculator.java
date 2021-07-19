a657ac672da88c73a67cb605de9313a150e543aa
PIH6585 <pih6585@naver.com>
2021-07-19 22:07:11 +0900

    2단계  문자열 계산기 구현

---
+++ src/main/java/calculrator/StringCalculator.java
@@ -0,0 +1,66 @@
+package calculrator;
+
+
+/**
+ * 사용자가 입력한 문자열 값에 따라 사칙연산을 수행할 수 있는 계산기를 구현해야 한다.
+ * 입력 문자열의 숫자와 사칙 연산 사이에는 반드시 빈 공백 문자열이 있다고 가정한다.
+ * 나눗셈의 경우 결과 값을 정수로 떨어지는 값으로 한정한다.
+ * 문자열 계산기는 사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정된다. 즉, 수학에서는 곱셈, 나눗셈이 덧셈, 뺄셈 보다 먼저 계산해야 하지만 이를 무시한다.
+ * 예를 들어 2 + 3 * 4 / 2와 같은 문자열을 입력할 경우 2 + 3 * 4 / 2 실행 결과인 10을 출력해야 한다.
+ * <p>
+ * 메소드가 너무 많은 일을 하지 않도록 분리하기 위해 노력해 본다.
+ * 테스트할 수 있는 단위로 나누어 구현 목록을 만든다.
+ * 덧셈
+ * 뺄셈
+ * 곱셈
+ * 나눗셈
+ * 입력 값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw
+ * 사칙연산 기호가 아닌 경우 IllegalArgumentException throw
+ * 사칙 연산을 모두 포함하는 기능 구현
+ * 공백 문자열을 빈 공백 문자로 분리하려면 String 클래스의 split(" ") 메소드를 활용한다.
+ * 반복적인 패턴을 찾아 반복문으로 구현한다.
+ */
+public class StringCalculator {
+
+    private CalculatorException calculratorException;
+
+    StringCalculator() {
+        calculratorException = new CalculatorException();
+    }
+
+    public int calculate(String value) {
+        String[] valueArr = value.split(" ");
+        int result =  Integer.parseInt(valueArr[0]);
+        for (int i = 0; i < valueArr.length - 2; i += 2) {
+            calculratorException.checkException( valueArr[i + 1], Integer.parseInt(valueArr[i + 2]));
+            result = calculate(result, valueArr[i + 1], Integer.parseInt(valueArr[i + 2]));
+        }
+        return result;
+    }
+
+    private int calculate(int firstValue, String expression, int lastValue) {
+        if (expression.equals("+")) return plus(firstValue, lastValue);
+        if (expression.equals("-")) return minus(firstValue, lastValue);
+        if (expression.equals("*")) return multi(firstValue, lastValue);
+        if (expression.equals("/")) return divide(firstValue, lastValue);
+        throw new IllegalArgumentException("올바른 연산자가 아닙니다.");
+    }
+
+    public int plus(int firstValue, int lastValue) {
+        return firstValue + lastValue;
+    }
+
+    public int minus(int firstValue, int lastValue) {
+        return firstValue - lastValue;
+    }
+
+    public int multi(int firstValue, int lastValue) {
+        return firstValue * lastValue;
+    }
+
+    public int divide(int firstValue, int lastValue) {
+        return firstValue / lastValue;
+    }
+
+
+}
---
+++ src/test/java/calculrator/StringCalculratorTest.java
@@ -0,0 +1,79 @@
+package calculrator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class StringCalculatorTest {
+
+    private StringCalculator stringCalculrator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculrator = new StringCalculator();
+    }
+
+    @Test
+    @DisplayName("숫자2개 덧셈기능")
+    public void calcul_plus() {
+        int plus = stringCalculrator.plus(2, 4);
+
+        assertThat(plus).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("숫자2개 뺄셈기능")
+    public void calcul_minus() {
+        int minus = stringCalculrator.minus(5, 3);
+
+        assertThat(minus).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("숫자2개 곱셈기능")
+    public void calcul_multi() {
+        int multi = stringCalculrator.multi(2, 4);
+
+        assertThat(multi).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("숫자2개 나눗셈기능")
+    public void calcul_divide() {
+        int divide = stringCalculrator.divide(4, 2);
+
+        assertThat(divide).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("입력값 null 체크 기능")
+    public void null_check() {
+        String checkValue = "2  3 +  + 1 / 2";
+
+        assertThatThrownBy(() -> stringCalculrator.calculate(checkValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("연산 수식이 빈값입니다.");
+    }
+
+    @Test
+    @DisplayName("사칙연산이 아닌 다른값 체크 기능")
+    public void notExpression_check() {
+        String checkValue = "2 ) 3 + 1 / 2";
+
+        assertThatThrownBy(() -> stringCalculrator.calculate(checkValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("올바른 연산자가 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("사칙연산을 모두 포함하는 기능")
+    public void calculator_operate_check() {
+        String checkValue = "1 + 2 * 5 / 3";
+        int result = stringCalculrator.calculate(checkValue);
+        assertThat(result).isEqualTo(5);
+    }
+
+}
