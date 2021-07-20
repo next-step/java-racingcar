07659f69c0dfcefb0b15804b1d42dffb43a8ab7c
byeongjaelee <littleprince0717@gmail.com>
2021-07-21 01:05:57 +0900

    feat: Test Learning for String Classes

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    void split_test1() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void split_test2() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    void substring_test1() {
+        String inputStr = "(1,2)";
+        String result = inputStr.substring(inputStr.indexOf('(') + 1, inputStr.indexOf(')'));
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("String Index OutOfBounds Exception Test")
+    @Test
+    void test() {
+        String inputStr = "abc";
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> inputStr.charAt(inputStr.length()))
+                .withMessageMatching("String index out of range: \\d+");
+    }
+}
