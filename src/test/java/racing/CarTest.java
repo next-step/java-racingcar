9afbcc7e3badc4bf4338e31e22e0d14690c2fe20
PIH6585 <pih6585@naver.com>
2021-07-20 15:39:53 +0900

    test : 경주 레이싱 거리 및 랜덤함수 추가에 따른 테스트 진행

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -11,8 +11,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
+    private static Car car;
+
     @BeforeAll
      static void setUp(){
+        car = new Car();
         String carNumber = "4\n";
         InputStream in = new ByteArrayInputStream(carNumber.getBytes());
         System.setIn(in);
@@ -21,10 +24,27 @@ public class CarTest {
     @Test
     @DisplayName("경주에 참가할 자동차 대수를 입력하면 대수만큼 자동차가 생성된다.")
     public void create_carModel(){
-        Car car = new Car();
         car.car_racing();
 
         assertThat(car.getCarList()).extracting("carNumber").containsExactly(1, 2, 3, 4);
         assertThat(car.getCarList().size()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("랜덤 숫자 함수를 실행하면 0이상 10미만의 숫자가 생성된다.")
+    public void create_random_number(){
+        int result = car.randomIntValue();
+
+        assertThat(result).isGreaterThan(0).isLessThan(10);
+    }
+
+    @Test
+    @DisplayName("랜덤 숫자가 4이상이면 경주에 사용되는 자동차가 전진한다.")
+    public void racing_random_number_check(){
+        String raceDistance1 = car.getRaceDistance(4);
+        String raceDistance2 = car.getRaceDistance(3);
+
+        assertThat(raceDistance1).isEqualTo("-");
+        assertThat(raceDistance2).isEqualTo("");
     }
 }
