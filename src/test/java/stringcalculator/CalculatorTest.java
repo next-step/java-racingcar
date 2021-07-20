76362a8070816ad6a1643aafd11a378412103eb7
YONGJU <odetsuo@gmail.com>
2021-07-20 21:16:14 +0900

    refactor calculator test

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -1,24 +1,23 @@
 package stringcalculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CalculatorTest {
 
-	@Test
+	@ParameterizedTest(name = "문자열에 값이 없는 경우 테스트 {index} [{arguments}]")
+	@EmptySource
+	@NullSource
 	@DisplayName("문자열에 값이 없는 경우 테스트")
-	void blank_string_formula() throws Exception {
-		//given
-		String nill = null;
-		String formula = " ";
-
+	void blank_string_formula(String formula) throws Exception {
 		//when
 		Calculator calculator = new Calculator();
 
 		//then
-		assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculate(nill));
 		assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculate(formula));
 	}
 
