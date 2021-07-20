ee395eca814c57baca8323efd379a8fc5808e495
ckdgus08 <ckdgus0808@naver.com>
2021-07-20 16:36:44 +0900

    refactor: Elements depth 2 -> 1 수정

--- src/main/java/calculator/Elements.java
+++ src/main/java/calculator/Elements.java
@@ -1,30 +1,35 @@
 package calculator;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Elements {
 
-    private final String[] elements;
+    private final List<String> elements;
 
     public Elements(String[] elements) {
-        this.elements = new String[elements.length];
+        this.elements = IntStream.range(0, elements.length)
+                .filter(index -> valid(elements, index))
+                .mapToObj((index) -> elements[index])
+                .collect(Collectors.toList());
+    }
 
-        for (int i = 0; i < elements.length; i++) {
-            String element = elements[i];
-            if (i % 2 == 0 && !isNumber(element))
-                throw new IllegalArgumentException("홀수번째 문자는 숫자만 올 수 있습니다.");
-            if (i % 2 == 1 && !isOperator(element))
-                throw new IllegalArgumentException("짝수번째 문자는 사칙 연산자만 올 수 있습니다.");
-            this.elements[i] = element;
-        }
+    private boolean valid(String[] elements, int index) {
+        if (index % 2 == 0 && !isNumber(elements[index]))
+            throw new IllegalArgumentException("홀수번째 문자는 숫자만 올 수 있습니다.");
+        if (index % 2 == 1 && !isOperator(elements[index]))
+            throw new IllegalArgumentException("짝수번째 문자는 사칙 연산자만 올 수 있습니다.");
+        return true;
     }
 
     public int calculate() {
 
-        int result = Integer.parseInt(elements[0]);
+        int result = Integer.parseInt(elements.get(0));
 
-        for (int i = 1; i < elements.length; i = i + 2) {
-            result = Operator.calculate(elements[i], result, Integer.parseInt(elements[i + 1]));
+        for (int i = 1; i < elements.size(); i = i + 2) {
+            result = Operator.calculate(elements.get(i), result, Integer.parseInt(elements.get(i + 1)));
         }
         return result;
     }
@@ -42,11 +47,11 @@ public class Elements {
         if (this == o) return true;
         if (!(o instanceof Elements)) return false;
         Elements elements1 = (Elements) o;
-        return Arrays.equals(elements, elements1.elements);
+        return Objects.equals(elements, elements1.elements);
     }
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(elements);
+        return Objects.hash(elements);
     }
 }
