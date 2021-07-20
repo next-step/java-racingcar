b303afaa29c9b161a6fa6c9083cfe93d53e131a5
ckdgus08 <ckdgus0808@naver.com>
2021-07-20 22:26:39 +0900

    feat: 자동차 수와 반복횟수 validation

--- src/main/java/racingGame/Game.java
+++ src/main/java/racingGame/Game.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 public class Game {
 
+    public static final int MIN_CAR_SIZE = 0;
+    public static final int MIN_REPEAT_COUNT = 0;
     private final List<Car> cars = new ArrayList<>();
     private final int repeatCount;
 
     public Game(int carSize, int repeatCount) {
+        valid(carSize, repeatCount);
+
         for (int i = 0; i < carSize; i++) {
             cars.add(new Car());
         }
@@ -21,5 +25,20 @@ public class Game {
 
     public int getRepeatCount() {
         return repeatCount;
+    }
+
+    private void valid(int carSize, int repeatCount) {
+        validCarSize(carSize);
+        validRepeatCount(repeatCount);
+    }
+
+    private void validCarSize(int carSize) {
+        if (carSize <= MIN_CAR_SIZE)
+            throw new IllegalArgumentException("자동차 수는 0 혹은 음수가 될 수 없습니다.");
+    }
+
+    private void validRepeatCount(int repeatCount) {
+        if (repeatCount <= MIN_REPEAT_COUNT)
+            throw new IllegalArgumentException("반복 횟수는 0 혹은 음수가 될 수 없습니다.");
     }
 }
--- src/test/java/racingGame/GameTest.java
+++ src/test/java/racingGame/GameTest.java
@@ -2,8 +2,11 @@ package racingGame;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GameTest {
 
@@ -13,5 +16,13 @@ public class GameTest {
         Game game = new Game(3, 5);
         assertThat(game.getCarSize()).isEqualTo(3);
         assertThat(game.getRepeatCount()).isEqualTo(5);
+    }
+
+    @ParameterizedTest(name = "{displayName} [{index}]")
+    @CsvSource(value = {"3:0", "3:-1", "0:5", "-1:5"}, delimiter = ':')
+    @DisplayName("자동차 수와 반복횟수는 0 혹은 음수가 될 수 없다.")
+    void valid(int carSize, int repeatSize) {
+        assertThatThrownBy(() -> new Game(carSize, repeatSize))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
