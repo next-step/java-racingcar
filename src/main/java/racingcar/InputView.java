a9509333cfb37c9628bb4f0db789ac64d4641146
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-07-20 19:54:08 +0900

    feat: 값을 입력받는 UI 기능 구현

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int numberOfCars = Integer.parseInt(scanner.nextLine());
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int numberOfAttempts = Integer.parseInt(scanner.nextLine());
+    }
+}
