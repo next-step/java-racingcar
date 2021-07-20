11bf6f6f2d00558f4991d9526ebdcc7434a368dd
shpark <bingbingpa@gmail.com>
2021-07-20 23:38:14 +0900

    feat: 자동차 경주 게임을 진행한다.

---
+++ src/main/java/racinggame/RacingGame.java
@@ -0,0 +1,26 @@
+package racinggame;
+
+import racinggame.ui.ResultView;
+import racinggame.util.RandomNumber;
+
+public class RacingGame {
+
+    private final Cars cars;
+    private final Count num;
+    private final RandomNumber randomNumber;
+
+    public RacingGame(Cars cars, Count num) {
+        this.cars = cars;
+        this.num = num;
+        this.randomNumber = new RandomNumber();
+    }
+
+    public ResultView play() {
+        int count = this.num.getCount();
+        for (int i = 0; i < count; i++) {
+            cars.race(randomNumber);
+        }
+
+        return new ResultView(cars, count);
+    }
+}
