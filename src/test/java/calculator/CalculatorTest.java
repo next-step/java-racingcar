380d883ae2f935bbc977bf06a5a622c3bcbf7a9e
hjjangdev <ju0398@hanmail.net>
2021-07-20 22:13:01 +0900

    refactor : Validation 객체 생성 수정과 테스트코드 수정 및 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,50 +9,64 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
     private Calculator calculator;
-    private Validation validation;
 
     @BeforeEach
     void init() {
         calculator = new Calculator();
-        validation = new Validation();
     }
 
     @Test
-    @DisplayName("덧셈")
+    @DisplayName("덧셈 성공 테스트")
     void plusTest() {
         int result = calculator.plus(1, 2);
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    @DisplayName("뺄셈")
+    @DisplayName("뺄셈 성공 테스트")
     void minusTest() {
         int result = calculator.minus(10, 1);
         assertThat(result).isEqualTo(9);
     }
 
     @Test
-    @DisplayName("곱셈")
+    @DisplayName("곱셈 성공 테스트")
     void multipleTest() {
         int result = calculator.multiplication(1, 2);
         assertThat(result).isEqualTo(2);
     }
 
     @Test
-    @DisplayName("나눗셈")
-    void devisionTest() {
+    @DisplayName("나눗셈 성공 테스트")
+    void devisionSuccessTest() {
         int result = calculator.devision(6, 3);
         assertThat(result).isEqualTo(2);
     }
 
     @Test
-    @DisplayName("사칙연산 성공")
+    @DisplayName("나눗셈 실패(0이 나눠졌을때) 테스트")
+    void devisionFailTest() {
+        assertThatThrownBy(() -> calculator.devision(0,3))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("0으로는 연산이 불가합니다.");
+    }
+
+    @Test
+    @DisplayName("나눗셈 실패(0으로 나눴을때) 테스트")
+    void devisionByZeroTest() {
+        assertThatThrownBy(() -> calculator.devision(3,0))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("0으로는 연산이 불가합니다.");
+    }
+
+    @Test
+    @DisplayName("사칙연산 성공 테스트")
     void calculateTest() {
         assertThat(calculator.calculate("2 + 3 * 4 / 2")).isEqualTo(10);
     }
 
     @Test
-    @DisplayName("입력 값이 Null")
+    @DisplayName("입력 값이 null일 때 발생하는 오류 테스트")
     void inputNull() {
         assertThatThrownBy(() -> calculator.calculate(null))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -60,7 +74,7 @@ public class CalculatorTest {
     }
 
     @Test
-    @DisplayName("입력 값에 빈 값이 존재")
+    @DisplayName("입력 값에 빈 값이 존재할 때 발생하는 오류 테스트")
     void inputBlank() {
         assertThatThrownBy(() -> calculator.calculate("1  + 2 - 3 * 4 / 2"))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -68,9 +82,17 @@ public class CalculatorTest {
     }
 
     @Test
-    @DisplayName("사칙연산 기호가 아닌 경우")
+    @DisplayName("사칙연산 기호가 아닌 경우에 발생하는 오류 테스트")
     void inputOperator() {
         assertThatThrownBy(() -> calculator.calculate("1 & 2 - 3 * 4 / 2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사칙연산 기호가 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("연산자->피연산자 순서로 입력했을때 발생하는 오류 테스트")
+    void userInputInvalid() {
+        assertThatThrownBy(() -> calculator.calculate("+ 1 + 2 - 3"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사칙연산 기호가 아닙니다.");
     }
