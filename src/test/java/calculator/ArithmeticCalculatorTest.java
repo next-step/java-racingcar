489e93bbb2b0f9a6b5ccd9d6375963076527073c
shirohoo <hch4102@naver.com>
2021-07-20 23:16:05 +0900

    refactor: 테스트 설명에 대한 컨벤션을 일관성있게 변경한다
    @DisplayName의 컨벤션이 조금 더 일관성있게 변경

--- src/test/java/calculator/ArithmeticCalculatorTest.java
+++ src/test/java/calculator/ArithmeticCalculatorTest.java
@@ -27,13 +27,13 @@ class ArithmeticCalculatorTest {
     }
 
     @Test
-    @DisplayName("3 / 2 = 1.5 이지만 int로 선언하였으므로 1이 나올 것이다")
+    @DisplayName("계산_결과는_1.5_지만_int로_선언하였으므로_1이_나올_것이다")
     void divide() throws Exception {
         assertThat(apply("/", 3, 2)).isEqualTo(1);
     }
 
     @Test
-    @DisplayName("0으로 나누면 ArithmeticException이 발생할 것이다")
+    @DisplayName("0으로_나누면_ArithmeticException이_발생할_것이다")
     void divideByZero() throws Exception {
         Assertions.assertThatThrownBy(() -> apply("/", 3, 0))
                   .isInstanceOf(ArithmeticException.class);
