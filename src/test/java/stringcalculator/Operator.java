2ccfde1af725ba82b446d48beb479a7302b55227
YONGJU <odetsuo@gmail.com>
2021-07-20 20:42:27 +0900

    fix operator

--- src/test/java/stringcalculator/Operator.java
+++ src/test/java/stringcalculator/Operator.java
@@ -1,10 +1,17 @@
 package stringcalculator;
 
+import java.util.Arrays;
 import java.util.function.IntBinaryOperator;
 
 public enum Operator {
 
-	PLUS("+", Integer::sum);
+	PLUS("+", Integer::sum),
+	MINUS("-", (x, y) -> x - y),
+	TIMES("*", (x, y) -> x * y),
+	DIVISION("/", (x, y) -> x / y);
+
+	private static final String WRONG_SIGN_MESSAGE = "잘못된 연산자입니다.";
+	private static final String DIVIDED_BY_ZERO_MESSAGE = "0 으로 나눌 수 없습니다.";
 
 	private final String sign;
 	private final IntBinaryOperator intBinaryOperator;
@@ -14,8 +21,26 @@ public enum Operator {
 		this.intBinaryOperator = intBinaryOperator;
 	}
 
-	public int calculate(int x, int y) {
-		return intBinaryOperator.applyAsInt(x, y);
+	/**
+	 * 연산자 문자로 Operator 조회
+	 *
+	 * 요구사항 - 사칙연산 기호가 아닌 경우 IllegalArgumentException throw
+	 *
+	 * @param sign
+	 * @return 연산자 기호에 따른 Operator
+	 */
+	public static Operator from(String sign) {
+		return Arrays.stream(Operator.values())
+		             .filter(operator -> operator.sign.equals(sign))
+		             .findFirst()
+		             .orElseThrow(() -> new IllegalArgumentException(WRONG_SIGN_MESSAGE));
+	}
+
+	public int calculate(int leftOperand, int rightOperand) {
+		if (sign.equals(DIVISION.sign) && rightOperand == 0) {
+			throw new IllegalArgumentException(DIVIDED_BY_ZERO_MESSAGE);
+		}
+		return intBinaryOperator.applyAsInt(leftOperand, rightOperand);
 	}
 
 
--- src/test/java/stringcalculator/OperatorTest.java
+++ src/test/java/stringcalculator/OperatorTest.java
@@ -1,29 +1,70 @@
 package stringcalculator;
 
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OperatorTest {
 
-	@ParameterizedTest(name = "더하기 테스트 {index} [{arguments}]")
+
+	@ParameterizedTest(name = "사칙연산 테스트 {index} [{arguments}]")
 	@CsvSource(value = {
-			"1,1,2",
-			"10,10,20",
-			"-1,1,0"
+			"1,+,1,2",
+			"10,-,10,0",
+			"-1,*,1,-1",
+			"24,/,7,3"
 	})
-	@DisplayName("더하기 테스트")
-	void add(int x, int y, int sum) throws Exception {
+	@DisplayName("사칙연산 테스트")
+	void calculate(int leftOperand, String sign, int rightOperand, int sum) throws Exception {
 		//given
-		Operator operator = Operator.PLUS;
+		Operator operator = Operator.from(sign);
 
 		//when
-		int calculated = operator.calculate(x, y);
+		int solution = operator.calculate(leftOperand, rightOperand);
 
 		//then
-		assertThat(calculated).isEqualTo(sum);
+		assertThat(solution).isEqualTo(sum);
+	}
+
+	@Test
+	@DisplayName("잘못된 연산자를 입력한 경우 테스트")
+	void wrong_sign() throws Exception {
+		//given
+		String sign = "sign";
+		String exceptionMessage = "잘못된 연산자입니다.";
+
+		//when
+		ThrowingCallable throwingCallable = () -> Operator.from(sign);
+
+		//then
+		assertException(exceptionMessage, throwingCallable);
+
+	}
+
+	@Test
+	@DisplayName("나누기 분모가 0인 경우")
+	void divided_by_zero() throws Exception {
+		//given
+		Operator operator = Operator.from("/");
+		String exceptionMessage = "0 으로 나눌 수 없습니다.";
+
+		//when
+		ThrowingCallable throwingCallable = () -> operator.calculate(100, 0);
+
+		//then
+		assertException(exceptionMessage, throwingCallable);
+
+	}
+
+	private void assertException(String exceptionMessage, ThrowingCallable throwingCallable) {
+		assertThatThrownBy(throwingCallable)
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage(exceptionMessage);
 	}
 
 
