b3f3b2033f96c5e4a8f9a4c3ed915a0fcd4388be
PIH6585 <pih6585@naver.com>
2021-07-20 17:40:11 +0900

    refactor : setup mehtod 분리

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,11 +1,9 @@
 package racing;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,7 +12,7 @@ public class CarTest {
 
     private static Car car;
 
-    void initTestSetup(String inputValue){
+    void initTestSetup(String inputValue) {
         System.setIn(new ByteArrayInputStream(inputValue.getBytes()));
         car = new Car();
     }
@@ -32,6 +30,7 @@ public class CarTest {
     @Test
     @DisplayName("랜덤 숫자 함수를 실행하면 0이상 10미만의 숫자가 생성된다.")
     public void create_random_number() {
+        initTestSetup("");
         int result = car.randomIntValue();
 
         assertThat(result).isGreaterThan(0).isLessThan(10);
@@ -40,6 +39,7 @@ public class CarTest {
     @Test
     @DisplayName("랜덤 숫자가 4이상이면 경주에 사용되는 자동차가 전진한다.")
     public void racing_random_number_check() {
+        initTestSetup("");
         String raceDistance1 = car.getRaceDistance(4);
         String raceDistance2 = car.getRaceDistance(3);
 
@@ -49,7 +49,7 @@ public class CarTest {
 
     @Test
     @DisplayName("입력값에 숫자가 아닌 문자를 입력시 예외가 발생된다.")
-    public void inputValue_notNumber_check(){
+    public void inputValue_notNumber_check() {
         initTestSetup("ㅁ\n");
         assertThatThrownBy(() -> car.car_racing())
                 .isInstanceOf(IllegalArgumentException.class)
@@ -59,7 +59,8 @@ public class CarTest {
     @Test
     @DisplayName("경주 시도 횟수만큼 참가한 차는 경주를 진행한다.")
     public void racingByCar() {
+        initTestSetup("3\n6\n");
         car.car_racing();
-        
+
     }
 }
