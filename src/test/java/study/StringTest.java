f82fc38841a44b4d2ddf1924d19094c93c11a9ba
jaehong0721 <newbie302@gmail.com>
2021-07-20 21:12:14 +0900

    String 클래스 학습테스트 - 요구사항3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,14 +2,13 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void split() {
         String[] result1 = getSplitByComma("1,2");
-        assertThat(result1).contains("1");
-        assertThat(result1).contains("2");
+        assertThat(result1).contains("1").contains("2");
 
         String[] result2 = getSplitByComma("1");
         assertThat(result2).containsExactly("1");
@@ -23,5 +22,26 @@ public class StringTest {
     void subString() {
         String result = "(1,2)".substring(1,4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    void getChar() {
+        char result;
+        result = getCharAt(0);
+        assertThat(result).isEqualTo('a');
+
+        result = getCharAt(1);
+        assertThat(result).isEqualTo('b');
+
+        result = getCharAt(2);
+        assertThat(result).isEqualTo('c');
+
+        assertThatThrownBy(() -> {
+            char c = getCharAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("String index out of range");
+    }
+
+    private char getCharAt(int i) {
+        return "abc".charAt(i);
     }
 }
