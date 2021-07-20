fac5ad9d5e11a436e9d521a53d8ec7fdf4112047
LAPTOP-I4NE0UIJ\yongs <smilesigma@gmail.com>
2021-07-20 19:51:10 +0900

    Make skeleton for step2

---
+++ src/main/java/stingcalculator/StringCalculator.java
@@ -0,0 +1,9 @@
+package stingcalculator;
+
+public class StringCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("여기서부터 시작.");
+    }
+
+}
---
+++ src/test/java/stingcalculator/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+package stingcalculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringCalculatorTest {
+
+    @Test
+    void skeleton() {
+        System.out.println("여기서부터 시작");
+    }
+}
