3eb81eb1d51394e60ad2972991623e6772567611
jayyhkwon <jayyhkwon@gmail.com>
2021-07-21 02:06:24 +0900

    Step1 #time 1h 1단계 미션

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
+    @Test
+    void demandOne() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현한다.")
+    @ValueSource(ints = {1, 2, 3})
+    void demandTwo(int input) {
+        assertThat(numbers).contains(input);
+    }
+
+    @ParameterizedTest
+    @DisplayName("1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.")
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void demandThree(int input, boolean expected) {
+        boolean actual = numbers.contains(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+
+}
