248075c244ae5b1329bada6bcfa2c000c59f57f2
PIH6585 <pih6585@naver.com>
2021-07-20 17:23:18 +0900

    test : 입력 문자열 숫자타입 예외처리 테스트 진행 refactor : setup method 수정

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -8,27 +8,21 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
 
     private static Car car;
 
-    @BeforeAll
-    static void setUp() {
-        String carNumber = "4\n3\n";
-        InputStream in1 = new ByteArrayInputStream(carNumber.getBytes());
-        System.setIn(in1);
-
-       /* String tryRacingNumber = "3\n";
-        InputStream in2 = new ByteArrayInputStream(tryRacingNumber.getBytes());
-        System.setIn(in2);*/
-
+    void initTestSetup(String inputValue){
+        System.setIn(new ByteArrayInputStream(inputValue.getBytes()));
         car = new Car();
     }
 
     @Test
     @DisplayName("경주에 참가할 자동차 대수를 입력하면 대수만큼 자동차가 생성된다.")
     public void create_carModel() {
+        initTestSetup("4\n");
         car.car_racing();
 
         assertThat(car.getCarList()).extracting("carNumber").containsExactly(1, 2, 3, 4);
@@ -51,6 +45,15 @@ public class CarTest {
 
         assertThat(raceDistance1).isEqualTo("-");
         assertThat(raceDistance2).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("입력값에 숫자가 아닌 문자를 입력시 예외가 발생된다.")
+    public void inputValue_notNumber_check(){
+        initTestSetup("ㅁ\n");
+        assertThatThrownBy(() -> car.car_racing())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 문자는 숫자만 사용 가능합니다.");
     }
 
     @Test
