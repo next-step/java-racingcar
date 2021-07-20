179e0d92af78db32650a19c4dcdc39c3c1cbbce6
SSONG22 <35327776+SSONG22@users.noreply.github.com>
2021-07-21 01:30:19 +0900

    Step1 - 학습테스트  실습 (#2275)
    * feat: String 클래스 학습테스트 요구사항 구현
    
    * feat: Set Collection 학습테스트 요구사항 구현
    
    * refactor: 에러 메세지 상수처리

---
+++ src/main/java/study/StringUtil.java
@@ -0,0 +1,27 @@
+package study;
+
+public class StringUtil {
+    private StringUtil() {
+    }
+    private static final String INPUT_ERROR = "올바른 값을 입력해주세요.";
+
+    public static String[] split(String input, String divider) {
+        if (input.isEmpty() || divider.isEmpty() || !divider.equals(","))
+            throw new IllegalArgumentException(INPUT_ERROR);
+        return input.split(divider);
+    }
+
+    public static String deleteBracket(String input) {
+        if (!input.contains("(") || !input.contains(")")) {
+            throw new IllegalArgumentException(INPUT_ERROR);
+        }
+        return input.substring(1, input.length() - 1);
+    }
+
+    public static Character getChar(String input, int index) {
+        if (input.length() <= index || index < 0) {
+            throw new IndexOutOfBoundsException(String.format("Index: %d, Size: %d", index, input.length()));
+        }
+        return input.charAt(index);
+    }
+}
