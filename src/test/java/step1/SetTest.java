c4f2b2239ef760e145d71154724bbbc2422c3e83
woung717 <woung717@gmail.com>
2021-07-21 08:03:31 +0900

    문자열 계산기 테스트코드 뼈대 작성

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -1,12 +1,10 @@
-package study;
+package step1;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
---
+++ src/test/java/step2/FIFOCalculatorTest.java
@@ -0,0 +1,43 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FIFOCalculatorTest {
+
+    @Test
+    @DisplayName("덧셈 연산 테스트")
+    void addOperationTest() {
+
+    }
+
+    @Test
+    @DisplayName("뺄셈 연산 테스트")
+    void subtractOperationTest() {
+
+    }
+
+    @Test
+    @DisplayName("곱셈 연산 테스트")
+    void multiplyOperationTest() {
+
+    }
+
+    @Test
+    @DisplayName("나눗셈 연산 테스트")
+    void divideOperationTest() {
+
+    }
+
+    @Test
+    @DisplayName("입력 값이 null 이거나 공백 문자인 경우 테스트")
+    void nullOrBlankInputTest() {
+
+    }
+
+    @Test
+    @DisplayName("입력 값이 허용되지 않은 기호를 포함하는 경우 테스트")
+    void unexpectedInputStringTest() {
+
+    }
+}
