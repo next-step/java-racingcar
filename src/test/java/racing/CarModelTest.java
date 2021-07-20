17bd9484bc491fae9a42cb8f0335d18bed02c798
PIH6585 <pih6585@naver.com>
2021-07-20 17:22:11 +0900

    fix : 불필요한 import 삭제

--- src/test/java/racing/CarModelTest.java
+++ src/test/java/racing/CarModelTest.java
@@ -1,11 +1,9 @@
 package racing;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarModelTest {
 
