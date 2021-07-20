a8e63bb16cc38d1acab221fe5baecda383c151bd
ckdgus08 <ckdgus0808@naver.com>
2021-07-20 22:01:30 +0900

    feat: 매개변수에 따라 자동차 이동 or 정지 (4이상 일때 이동)

---
+++ src/main/java/racingGame/Car.java
@@ -0,0 +1,42 @@
+package racingGame;
+
+import java.util.Objects;
+
+public class Car {
+
+    public static final int MOVABLE_LIMIT_NUMBER = 4;
+    private final int distance;
+
+    public Car() {
+        this(0);
+    }
+
+    public Car(int distance) {
+        this.distance = distance;
+    }
+
+    public Car move(int random) {
+        if (random >= MOVABLE_LIMIT_NUMBER) {
+            return new Car(distance + 1);
+        }
+        return this;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return distance == car.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
+    }
+
+}
