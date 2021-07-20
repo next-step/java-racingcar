06ac38cbcc47e896b151822198ef42938bffe307
seondongpyo <seondongpyo@gmail.com>
2021-07-20 23:10:38 +0900

    feat: 문자열 계산을 위한 연산자 enum 추가

---
+++ src/main/java/calculator/StringOperator.java
@@ -0,0 +1,37 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+
+public enum StringOperator {
+
+	ADD("+", (number1, number2) -> number1 + number2),
+	SUBTRACT("-", (number1, number2) -> number1 - number2),
+	MULTIPLY("*", (number1, number2) -> number1 * number2),
+	DIVIDE("/", (number1, number2) -> {
+		if (number2 == 0) {
+			throw new IllegalArgumentException(ExceptionMessage.DIVIDE_BY_ZERO.text());
+		}
+		return number1 / number2;
+	});
+
+	private final String operator;
+	private final BinaryOperator<Integer> calculator;
+
+	StringOperator(String operator, BinaryOperator<Integer> calculator) {
+		this.operator = operator;
+		this.calculator = calculator;
+	}
+
+	public int calculate(int number1, int number2) {
+		return calculator.apply(number1, number2);
+	}
+
+	public static StringOperator of(String inputOperator) {
+		return Arrays.stream(values())
+			.filter(value -> value.operator.equals(inputOperator))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.INVALID_OPERATOR.text()));
+	}
+
+}
