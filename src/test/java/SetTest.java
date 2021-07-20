693ccb20ea8e3cfd487cab7f49ae948b367f6f11
조원진 <>
2021-07-20 22:39:36 +0900

    String method, Set Class Test

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class SetTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setup() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size 메소드 정상동작 테스트")
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("setup 메소드에서 초기화한 값을 포함하고 있는지 테스트")
+    @ParameterizedTest(name = "{index} {displayName}")
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        assertThat(numbers).contains(input);
+    }
+
+    @DisplayName("setup 메소드에서 초기화한 값을 포함하면 true, 그 외 값의 경우 false 리턴")
+    @ParameterizedTest(name = "{index} {displayName}")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_ShouldSetupValueReturnTrueAndOthersReturnFalse(int input, boolean expected) {
+        boolean actualValue = numbers.contains(input);
+        assertThat(actualValue).isEqualTo(expected);
+    }
 }
--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class StringTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class StringTest {
+
+    @DisplayName("split 메소드가 정상 동작하는지 확인")
+    @Test
+    void split() {
+        String[] split = "1,2".split(",");
+        assertThat(split).containsExactly("1", "2");
+    }
+
+    @DisplayName("substring 메소드가 정상 동작하는지 확인")
+    @Test
+    void substring() {
+        String target = "(1,2)";
+        String substring = target.substring(1, target.length() - 1);
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt 메소드에서 특정 문자 위치를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException 발생")
+    @Test
+    void charAt_throwStringIndexOutOfBoundsException() {
+        String target = "abc";
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> target.charAt(target.length()));
+    }
 }
