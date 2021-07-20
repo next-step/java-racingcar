b908dcf28618d53eedfc0364d46f780a0fe11e2e
rkdals213 <rkdals213@naver.com>
2021-07-21 00:37:00 +0900

    feat: 전체 로직 스크립트형태로 작성

---
+++ src/main/java/step3/App.java
@@ -0,0 +1,30 @@
+package step3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carCount = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int count = scanner.nextInt();
+
+        int[] car = new int[carCount];
+
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < carCount; j++) {
+                int randomNumber = random.nextInt(10);
+                if (randomNumber >= 4) car[j]++;
+                for (int k = 0; k < car[j]; k++) {
+                    System.out.print("-");
+                }
+                System.out.println();
+            }
+            System.out.println("======================================");
+        }
+    }
+
+}
