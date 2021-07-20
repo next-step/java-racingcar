56a5d388f6d56751c977b4afdeb40cdfd7b9e2a4
PIH6585 <pih6585@naver.com>
2021-07-20 14:07:46 +0900

    숫자에 문자 입력 예외처리 및 테스트

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,9 +3,9 @@ package calculator;
 public class StringCalculator {
 
     public int calculate(String value) {
-        String[] valueArr = stringBySplit(value);
-        validValueArraySize(valueArr.length);
-        int result = valueArrForResult(valueArr);
+        String[] valueArray = stringBySplit(value);
+        validValueArraySize(valueArray.length);
+        int result = valueArrForResult(valueArray);
         return result;
     }
 
@@ -26,10 +26,18 @@ public class StringCalculator {
     }
 
     public int toInt(String value) {
+        validByNumberCheck(value);
         return Integer.parseInt(value);
     }
 
-    public void validValueArraySize(int arraySize) {
+    private void validByNumberCheck(String value) {
+        String regExp = "^[0-9]+$";
+        if(!value.matches(regExp)){
+            throw new IllegalArgumentException("해당 문자는 숫자만 사용 가능합니다.");
+        }
+    }
+
+    private void validValueArraySize(int arraySize) {
         if (arraySize < 3) {
             throw new IllegalStateException("배열의 길이는 최소 3이상 이어야 합니다.");
         }
