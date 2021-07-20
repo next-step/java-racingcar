1c9f2f6c784dd852c32cb9d934b7b58ace77394a
jaeyoungjang2 <lion85488@gmail.com>
2021-07-20 20:13:00 +0900

    split study test3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -13,14 +14,19 @@ public class StringTest {
         assertThat(result).containsExactly("1","2");
     }
 
-    @DisplayName("substring 활용")
+    @DisplayName("(1,2) 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 1,2를 반환")
     @Test
     void split2() {
         String result = "(1,2)".substring(1,4);
         assertThat(result).isEqualTo("1,2");
     }
 
-
-
-
+    @DisplayName("String의 charAt() 메소드를 이용하여 특정 위치의 문자열을 가져오는 테스트를 진행")
+    @Test
+    void split3() {
+        assertThatThrownBy(()->{
+            char result = "abc".charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+    }
 }
