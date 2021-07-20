c167e0dacba5537328479ff49936e77c420b614b
shirohoo <hch4102@naver.com>
2021-07-20 23:38:41 +0900

    test: 문자열계산기에 테스트 케이스를 추가한다
    검증이 더 필요하다고 판단되어 테스트 케이스를 추가하였음

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,6 +2,8 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -22,10 +24,14 @@ public class CalculatorTest {
                 .hasMessage("숫자와 사칙 연산자만 입력 가능하며, 모든 글자 사이에는 공백이 존재해야 합니다.");
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"2 + 3 * 4 / 2:10",
+                        "9 / 5 * 6 + 2:8",
+                        "7 * 3 / 2 + 3:13"},
+               delimiter = ':')
     @DisplayName("계산기가_정상적인_결과를_반환하는지_확인한다")
-    void calculate() throws Exception {
-        int actual = Calculator.from("2 + 3 * 4 / 2").calculate();
-        assertThat(actual).isEqualTo(10);
+    void calculate(String condition, int expected) throws Exception {
+        int actual = Calculator.from(condition).calculate();
+        assertThat(actual).isEqualTo(expected);
     }
 }
