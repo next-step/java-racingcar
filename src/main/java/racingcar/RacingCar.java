7c47aba4d97a3cae4f209f400465a2811ef2f8fc
nokchax <nokchax@gmail.com>
2021-07-20 16:25:37 +0900

    feat: race cars and check race over implements and test

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,13 +1,16 @@
 package racingcar;
 
 import racingcar.car.Cars;
+import racingcar.dto.CarDto;
 import racingcar.strategy.MoveStrategy;
+
+import java.util.List;
 
 public class RacingCar {
     private static final int MINIMUM_ROUND = 1;
     private final int totalRound;
-    private final int currentRound;
     private final Cars cars;
+    private int currentRound;
 
     private RacingCar(int numberOfCars, int totalRound, MoveStrategy moveStrategy) {
         validate(totalRound);
@@ -25,5 +28,22 @@ public class RacingCar {
 
     public static RacingCar init(int numberOfCars, int totalRound, MoveStrategy moveStrategy) {
         return new RacingCar(numberOfCars, totalRound, moveStrategy);
+    }
+
+    public void race() {
+        if (isRaceOver()) {
+            throw new IllegalStateException("Round can't exceed total round");
+        }
+
+        currentRound++;
+        cars.moveCars();
+    }
+
+    public boolean isRaceOver() {
+        return currentRound >= totalRound;
+    }
+
+    public List<CarDto> currentState() {
+        return cars.getCarDtos();
     }
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.dto.CarDto;
 import racingcar.helper.Fixture;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,4 +29,38 @@ class RacingCarTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("레이스를 진행하면 차들이 한 번 움직임을 시도한다.")
+    @Test
+    void raceMakeCarsTryMove() {
+        RacingCar racingCar = RacingCar.init(2, 1, Fixture.alwaysMoveStrategy());
+        List<CarDto> carDtos = racingCar.currentState();
+        carDtos.forEach(carDto -> assertThat(carDto.getPosition()).isEqualTo(0));
+
+        racingCar.race();
+
+        carDtos = racingCar.currentState();
+        carDtos.forEach(carDto -> assertThat(carDto.getPosition()).isEqualTo(1));
+    }
+
+    @DisplayName("현재 라운드 수는 총 라운드 수를 넘길 수 없다.")
+    @Test
+    void currentRoundCantExceedTotalRound() {
+        RacingCar racingCar = RacingCar.init(10, 1, Fixture.alwaysMoveStrategy());
+
+        racingCar.race();
+
+        assertThatThrownBy(racingCar::race).isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("경주 종료 여부를 리턴한다.")
+    @Test
+    void isRaceOver() {
+        RacingCar racingCar = RacingCar.init(2, 1, Fixture.alwaysMoveStrategy());
+
+        assertThat(racingCar.isRaceOver()).isFalse();
+
+        racingCar.race();
+
+        assertThat(racingCar.isRaceOver()).isTrue();
+    }
 }
