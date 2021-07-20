6e32dff0c89aac7c32c2fca4df3b280108783fdf
nokchax <nokchax@gmail.com>
2021-07-20 16:12:21 +0900

    feat: init racing car and validate input

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -4,13 +4,26 @@ import racingcar.car.Cars;
 import racingcar.strategy.MoveStrategy;
 
 public class RacingCar {
+    private static final int MINIMUM_ROUND = 1;
+    private final int totalRound;
+    private final int currentRound;
     private final Cars cars;
 
-    private RacingCar(int numberOfCars, MoveStrategy moveStrategy) {
+    private RacingCar(int numberOfCars, int totalRound, MoveStrategy moveStrategy) {
+        validate(totalRound);
+
+        this.totalRound = totalRound;
+        this.currentRound = 0;
         this.cars = Cars.of(numberOfCars, moveStrategy);
     }
 
-    public static RacingCar init(int numberOfCars, MoveStrategy moveStrategy) {
-        return new RacingCar(numberOfCars, moveStrategy);
+    private void validate(int totalRound) {
+        if (totalRound < MINIMUM_ROUND) {
+            throw new IllegalArgumentException("Total round can't be under zero");
+        }
+    }
+
+    public static RacingCar init(int numberOfCars, int totalRound, MoveStrategy moveStrategy) {
+        return new RacingCar(numberOfCars, totalRound, moveStrategy);
     }
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -12,17 +12,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @DisplayName("레이싱 카 게임 테스트")
 class RacingCarTest {
 
-    @DisplayName("레이싱 카 게임은 차의 숫자와 MoveStrategy 를 가지고 초기화 한다.")
+    @DisplayName("레이싱 카 게임은 차의 숫자와 라운드 수, MoveStrategy 를 가지고 초기화 한다.")
     @Test
     void init() {
-        assertThat(RacingCar.init(10, Fixture.alwaysMoveStrategy())).isNotNull();
+        assertThat(RacingCar.init(10, 5, Fixture.alwaysMoveStrategy())).isNotNull();
     }
 
-    @DisplayName("레이싱 카 게임은 차의 숫자가 음수일 수 없다.")
+    @DisplayName("총 라운드 수는 1 이상이다.")
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
-    void numberOfCarsShouldBeOverZero(int numberOfCars) {
-        assertThatThrownBy(() -> RacingCar.init(numberOfCars, Fixture.neverMoveStrategy())).isInstanceOf(IllegalArgumentException.class);
+    void roundShouldBeOverZero(int totalRound) {
+        assertThatThrownBy(() -> RacingCar.init(10, totalRound, Fixture.alwaysMoveStrategy()))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
