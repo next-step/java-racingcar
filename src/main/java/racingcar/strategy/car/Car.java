c8c7ad2993544dba6e04aad27356866ee63b3825
nokchax <nokchax@gmail.com>
2021-07-20 10:56:23 +0900

    feat: create car class and test it

---
+++ src/main/java/racingcar/strategy/car/Car.java
@@ -0,0 +1,33 @@
+package racingcar.strategy.car;
+
+import racingcar.strategy.MoveStrategy;
+
+import java.util.Objects;
+
+public class Car {
+    private int position;
+    private final MoveStrategy moveStrategy;
+
+    public Car(MoveStrategy moveStrategy) {
+        validate(moveStrategy);
+
+        this.moveStrategy = moveStrategy;
+    }
+
+    private void validate(MoveStrategy moveStrategy) {
+        if (Objects.isNull(moveStrategy)) {
+            throw new IllegalArgumentException("MoveStrategy can't be a null");
+        }
+    }
+
+    public int currentPosition() {
+        return position;
+    }
+
+    // move(MoveStrategy moveStrategy); 같은 방식은 어떨까..?
+    public void move() {
+        if (moveStrategy.isMovable()) {
+            position++;
+        }
+    }
+}
---
+++ src/test/java/racingcar/strategy/car/CarTest.java
@@ -0,0 +1,47 @@
+package racingcar.strategy.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("자동차 클래스 테스트")
+class CarTest {
+
+    @DisplayName("자동차는 MoveStrategy 를 가지고 초기화 한다.")
+    @Test
+    void initWithMoveStrategy() {
+        assertThatThrownBy(() -> new Car(() -> true)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("자동차는 MoveStrategy 없이 초기화 할 수 없다.")
+    @Test
+    void initWithOutMoveStrategy() {
+        assertThatThrownBy(() -> new Car(null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("MoveStrategy 가 true 일 경우 자동차는 +1 만큼 이동한다.")
+    @Test
+    void carMoveWhenMoveStrategyReturnTrue() {
+        Car car = new Car(() -> true);
+
+        assertThat(car.currentPosition()).isEqualTo(0);
+
+        car.move();
+
+        assertThat(car.currentPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("MoveStrategy 가 false 일 경우 움직이지 않는다.")
+    @Test
+    void carDoesNotMoveWhenMoveStrategyReturnFalse() {
+        Car car = new Car(() -> false);
+
+        assertThat(car.currentPosition()).isEqualTo(0);
+
+        car.move();
+
+        assertThat(car.currentPosition()).isEqualTo(0);
+    }
+}
