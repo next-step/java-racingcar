a2ddc554c596aa1dff14da42bdbfac2e276333fd
YONGJU <odetsuo@gmail.com>
2021-07-20 22:51:05 +0900

    feature calculator

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -2,9 +2,11 @@ package stringcalculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CalculatorTest {
@@ -20,5 +22,23 @@ class CalculatorTest {
 		//then
 		assertThatIllegalArgumentException().isThrownBy(() -> calculator.calculate(formula));
 	}
+
+	@ParameterizedTest
+	@CsvSource(value = {
+			"1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10,55",
+			"2 + 3 * 4 / 2,10",
+			"34 * 4 - 376 / 45 + 6,1"
+	})
+	void test(String formula, int solution) throws Exception {
+		//given
+		Calculator calculator = new Calculator();
+
+		//when
+		int result = calculator.calculate(formula);
+
+		//then
+		assertThat(result).isEqualTo(solution);
+	}
+
 
 }
