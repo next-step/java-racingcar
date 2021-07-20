2212ef135128f40802900b4cbf1bdcf221f44306
jaehong0721 <newbie302@gmail.com>
2021-07-20 21:45:11 +0900

    Set Collection 학습테스트 - 요구사항3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -32,5 +33,11 @@ public class SetTest {
     @ValueSource(ints = { 1, 2, 3 })
     void contains(int e) {
         assertThat(numbers).contains(e);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsTrueOrFalse(int e, boolean expected) {
+        assertThat(numbers.contains(e)).isEqualTo(expected);
     }
 }
