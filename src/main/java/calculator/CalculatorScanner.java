7f323a4df64cd787115c972b7f8ced5f2ceec064
shirohoo <hch4102@naver.com>
2021-07-20 22:48:34 +0900

    feat: 입력기를 작성한다
    입력을 받는 싱글톤 객체를 작성하였다

---
+++ src/main/java/calculator/CalculatorScanner.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import java.util.Scanner;
+
+class CalculatorScanner {
+    private static final CalculatorScanner calculatorScanner = new CalculatorScanner();
+
+    private CalculatorScanner() {
+    }
+
+    public static CalculatorScanner getInstance() {
+        return calculatorScanner;
+    }
+
+    public String enter() {
+        try(Scanner scanner = new Scanner(System.in)) {
+            return scanner.nextLine().trim();
+        }
+    }
+}
