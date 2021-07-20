5737008b513cfb4e6865520d701dd9136d68aa0b
jaeyoungjang2 <lion85488@gmail.com>
2021-07-20 20:01:51 +0900

    split study test2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,10 +3,24 @@ package study;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class StringTest {
+    @DisplayName("String 1과 2를 split으로 나누고 결과 값이 예상한 결과와 일치하는지 확인")
     @Test
-    void name() {
+    void split1() {
         String[] result = "1,2".split(",");
-        Assertions.assertThat(result).containsExactly("1","2");
+        assertThat(result).containsExactly("1","2");
     }
+
+    @DisplayName("substring 활용")
+    @Test
+    void split2() {
+        String result = "(1,2)".substring(1,4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+
+
+
 }
