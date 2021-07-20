822b5bb55be46c940a14d265eb21cd4b3a2f3667
ChangSubKwak <paradise1732@gmail.com>
2021-07-21 01:52:06 +0900

    initial commit

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,6 +8,23 @@ public class StringTest {
 	@Test
 	void split() {
 		String[] result = "1,2".split(",");
+		assertThat(result).contains("1");
+		assertThat(result).contains("2");
 		assertThat(result).containsExactly("1", "2");
+	}
+
+	@Test
+	void substring() {
+		String input = "(1,2)";
+		assertThat(input.substring(1, input.length() - 1)).isEqualTo("1,2");
+	}
+
+	@Test
+	void charAt() {
+		String input = "abc";
+		assertThatThrownBy(() -> {
+			input.charAt(input.length());
+		}).isInstanceOf(StringIndexOutOfBoundsException.class)
+		  .hasMessageContaining("String index out of range: " + input.length());
 	}
 }
