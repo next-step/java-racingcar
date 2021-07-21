3550e66a1925cd92ca3cfe5cae43f259f276d584
shirohoo <hch4102@naver.com>
2021-07-21 20:12:47 +0900

    feat: IO를 담당하는 객체를 작성한다

---
+++ src/main/java/racing/RacingGamePrinter.java
@@ -0,0 +1,9 @@
+package racing;
+
+class RacingGamePrinter {
+    private static final String END_MESSAGE_FORMAT = "\n실행 결과\n";
+
+    public static void print(String str) {
+        System.out.print(END_MESSAGE_FORMAT + str);
+    }
+}
---
+++ src/main/java/racing/RacingGameScanner.java
@@ -0,0 +1,25 @@
+package racing;
+
+import java.util.Scanner;
+
+class RacingGameScanner {
+    public static int[] enter() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대인가요?");
+        int carCount = sc.nextInt();
+
+        if(carCount <= 1) {
+            throw new IllegalArgumentException("자동차는 2대 이상이어야 합니다");
+        }
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int stageCount = sc.nextInt();
+
+        if(stageCount <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다");
+        }
+
+        sc.close();
+        return new int[]{carCount, stageCount};
+    }
+}
