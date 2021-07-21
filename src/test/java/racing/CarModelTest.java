854c0ed9795b240dd99f2e7a67077a80f5627cdb
PIH6585 <pih6585@naver.com>
2021-07-21 11:33:10 +0900

    test: 변경건에 대한 test 진행

--- src/test/java/racing/CarModelTest.java
+++ src/test/java/racing/CarModelTest.java
@@ -8,15 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CarModelTest {
 
     @Test
-    @DisplayName("CarModel class에 setter에 값을 주입시키면 getter로 값을 받을수 있다.")
+    @DisplayName("CarModel class에 전진/정지 값을 true, false로 넣으면 전진 및 정지를 한다.")
     public void SetterInDataAndGetterOutData_check() {
         CarModel carModel1 = new CarModel(1);
-        carModel1.setRaceDistance("--");
+        carModel1.carRacingAct(true);
+        String model1Distance = carModel1.carRacingAct(true);
 
         CarModel carModel2 = new CarModel(2);
-        carModel2.setRaceDistance("---");
+        carModel2.carRacingAct(true);
+        String model2Distance = carModel2.carRacingAct(false);
 
-        assertThat(carModel1.getRaceDistance()).isEqualTo("--");
-        assertThat(carModel2.getRaceDistance()).isEqualTo("---");
+        assertThat(model1Distance).isEqualTo("--");
+        assertThat(model2Distance).isEqualTo("-");
     }
 }
--- src/test/java/racing/CarRacingMainTest.java
+++ src/test/java/racing/CarRacingMainTest.java
@@ -1,37 +1,59 @@
 package racing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarRacingMainTest {
+    
 
-    private static CarRacingMain car;
+    @Test
+    @DisplayName("스캐너로 받는 입력값이 숫자일경우 그값을 반환한다.")
+    public void scannerValueToIntValue() {
+        int result1 = CarRacingMain.intValueByScanner("3");
+        int result2 = CarRacingMain.intValueByScanner("5");
 
-    void initTestSetup(String inputValue) {
-        System.setIn(new ByteArrayInputStream(inputValue.getBytes()));
-        car = new CarRacingMain();
+        assertThat(result1).isEqualTo(3);
+        assertThat(result2).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("입력받은 문자열을 숫자형태로 반환한다.")
+    public void stringValueToIntValue() {
+        int result1 = CarRacingMain.toInt("2");
+        int result2 = CarRacingMain.toInt("5");
+
+        assertThat(result1).isEqualTo(2);
+        assertThat(result2).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("입력값에 숫자가 아닌 문자를 입력시 예외가 발생된다.")
+    public void inputValue_notNumber_check() {
+        assertThatThrownBy(() -> CarRacingMain.validByNumberCheck("D!2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 문자는 숫자만 사용 가능합니다.");
     }
 
     @Test
     @DisplayName("경주에 참가할 자동차 대수를 입력하면 대수만큼 자동차가 생성된다.")
     public void create_carModel() {
-        initTestSetup("4\n");
-        car.car_racing();
+        List<CarModel> carModelList = CarRacingMain.createCarByNumber(4);
 
-        assertThat(car.getCarList()).extracting("carNumber").containsExactly(1, 2, 3, 4);
-        assertThat(car.getCarList().size()).isEqualTo(4);
+        assertThat(carModelList).extracting("carNumber").containsExactly(1, 2, 3, 4);
+        assertThat(carModelList.size()).isEqualTo(4);
     }
 
     @Test
     @DisplayName("랜덤 숫자 함수를 실행하면 0이상 10미만의 숫자가 생성된다.")
     public void create_random_number() {
-        initTestSetup("");
-        int result = car.randomIntValue();
+        int result = CarRacingMain.randomValue();
 
         assertThat(result).isGreaterThan(0).isLessThan(10);
     }
@@ -39,28 +61,13 @@ public class CarRacingMainTest {
     @Test
     @DisplayName("랜덤 숫자가 4이상이면 경주에 사용되는 자동차가 전진한다.")
     public void racing_random_number_check() {
-        initTestSetup("");
-        String raceDistance1 = car.getRaceDistance(4);
-        String raceDistance2 = car.getRaceDistance(3);
+        boolean result1 = CarRacingMain.getRaceDistance(3);
+        boolean result2 = CarRacingMain.getRaceDistance(8);
 
-        assertThat(raceDistance1).isEqualTo("-");
-        assertThat(raceDistance2).isEqualTo("");
+        assertThat(result1).isFalse();
+        assertThat(result2).isTrue();
     }
 
-    @Test
-    @DisplayName("입력값에 숫자가 아닌 문자를 입력시 예외가 발생된다.")
-    public void inputValue_notNumber_check() {
-        initTestSetup("ㅁ\n");
-        assertThatThrownBy(() -> car.car_racing())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("해당 문자는 숫자만 사용 가능합니다.");
-    }
 
-    @Test
-    @DisplayName("자동차별 경주 method 실행시 경주 결과가 화면이 보여준다.")
-    public void racingByCar() {
-        initTestSetup("3\n6\n");
-        car.car_racing();
 
-    }
 }
