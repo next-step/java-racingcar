3d76e14cfecc90c3b40a8d70a59b06925672f426
nokchax <nokchax@gmail.com>
2021-07-21 10:51:25 +0900

    test: car move test with random number

--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -2,7 +2,14 @@ package racingcar.car;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.helper.Fixture;
+import racingcar.strategy.RandomMoveStrategy;
+
+import java.util.Random;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -46,5 +53,60 @@ class CarTest {
         Car car = Car.newInstance();
 
         assertThatThrownBy(() -> car.move(null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다.")
+    @ParameterizedTest
+    @MethodSource
+    void moveTestWithMoveStrategy(Random random, boolean moved) {
+        boolean notMoved = !moved;
+        Car car = Car.newInstance();
+        RandomMoveStrategy randomMoveStrategy = RandomMoveStrategy.from(random);
+
+        int positionBeforeMove = car.currentPosition();
+        car.move(randomMoveStrategy);
+        int positionAfterMove = car.currentPosition();
+
+        assertThat(positionBeforeMove + 1 == positionAfterMove).isEqualTo(moved);
+        assertThat(positionBeforeMove == positionAfterMove).isEqualTo(notMoved);
+    }
+
+    private static Stream<Arguments> moveTestWithMoveStrategy() {
+        return Stream.of(
+                Arguments.of(new Random() {
+                    @Override
+                    public int nextInt(int bound) {
+                        return 0;
+                    }
+                }, false),
+
+                Arguments.of(new Random() {
+                    @Override
+                    public int nextInt(int bound) {
+                        return 3;
+                    }
+                }, false),
+
+                Arguments.of(new Random() {
+                    @Override
+                    public int nextInt(int bound) {
+                        return 4;
+                    }
+                }, true),
+
+                Arguments.of(new Random() {
+                    @Override
+                    public int nextInt(int bound) {
+                        return 9;
+                    }
+                }, true),
+
+                Arguments.of(new Random() {
+                    @Override
+                    public int nextInt(int bound) {
+                        return 10;
+                    }
+                }, false)
+        );
     }
 }
