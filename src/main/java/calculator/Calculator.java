7e865d77644bdba9ec8e438794f812bc17f08e8b
shirohoo <hch4102@naver.com>
2021-07-21 10:16:12 +0900

    fix: 문자열 계산기의 알고리즘을 변경한다
    계산 함수를 중복 실행할 경우 기존 데이터가 누적계산되는 버그를 고쳤음.
    생성자로 객체의 불변 데이터를 초기화하고, 매 계산마다 이를 재사용하는 방식으로 변경함.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,48 +1,59 @@
 package calculator;
 
-import java.util.regex.Pattern;
-
 import static java.lang.Integer.parseInt;
+
+import java.util.regex.Pattern;
 
 class Calculator {
     private static final Pattern NORMAL_PATTERN = Pattern.compile("^[\\d]+(\\s[\\d+*\\-/])*.\\d$");
+    private static final Pattern OPERATOR_PATTERN = Pattern.compile("[+*\\-/]");
+
+    private final String equation;
 
     private String operator;
-    private String string;
-
+    private String data;
     private int reduce;
 
     private Calculator(String string) {
-        isValid(string);
-
-        this.operator = "+";
-        this.string = string;
-        this.reduce = 0;
+        verify(string);
+        equation = string;
     }
 
     public static Calculator from(String string) {
         return new Calculator(string);
     }
 
-    private void isValid(String string) {
-        if(!NORMAL_PATTERN.matcher(string).find()) {
+    private void verify(String string) {
+        if (!isPatternMatch(NORMAL_PATTERN, string)) {
             throw new IllegalArgumentException("숫자와 사칙 연산자만 입력 가능하며, 모든 글자 사이에는 공백이 존재해야 합니다.");
         }
     }
 
     public int calculate() {
-        String[] split = this.string.split(" ");
-        for(String s : split) {
-            calculate(s);
+        init();
+
+        String[] split = this.data.split(" ");
+        for (String string : split) {
+            calculate(string);
         }
         return reduce;
     }
 
-    private void calculate(String s) {
-        if(!s.matches("[+*\\-/]")) {
-            this.reduce = ArithmeticCalculator.apply(operator, reduce, parseInt(s));
+    private void init() {
+        data = equation;
+        operator = "+";
+        reduce = 0;
+    }
+
+    private void calculate(String string) {
+        if (!isPatternMatch(OPERATOR_PATTERN, string)) {
+            this.reduce = ArithmeticCalculator.apply(operator, reduce, parseInt(string));
             return;
         }
-        this.operator = s;
+        this.operator = string;
+    }
+
+    private boolean isPatternMatch(Pattern pattern, String source) {
+        return pattern.matcher(source).find();
     }
 }
