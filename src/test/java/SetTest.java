948a6f4a643a15978d7d9258afd2eabc3dca4533
jspark940321 <jspark940321@gmail.com>
2021-07-21 23:33:31 +0900

    feat: 요구사항2
    Set의 contains() 메소드를 활용해 값이 존재하는지를 확인하는 테스트
    ParameterizedTest를 활용

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashSet;
@@ -23,5 +27,27 @@ public class SetTest {
     void 요구사항1(){
         int setSize = numbers.size();
         assertThat(setSize).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName(("1,2,3 존재 유무 확인"))
+    void 요구사항2(){
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName(("1,2,3 존재 유무 확인 ParameterizedTest 사용"))
+    void 요구사항2_ParameterizedTest(int number){
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/setTest.csv")
+    @DisplayName(("1,2,3 존재 유무 확인 ParameterizedTest 사용"))
+    void 요구사항2_ParameterizedTest_csv(int number, boolean actualValue){
+        assertThat(numbers.contains(number)).isEqualTo(actualValue);
     }
 }
