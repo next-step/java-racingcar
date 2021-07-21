413095179c22564c6523494cc70ac89791aa51f3
SONG22 <ansongi527@gmail.com>
2021-07-21 16:34:17 +0900

    refactor(step1): 피드백 반영

--- src/main/java/study/StringUtil.java
+++ src/main/java/study/StringUtil.java
@@ -1,18 +1,22 @@
 package study;
 
+import java.util.regex.Pattern;
+
 public class StringUtil {
     private StringUtil() {
     }
+
     private static final String INPUT_ERROR = "올바른 값을 입력해주세요.";
 
     public static String[] split(String input, String divider) {
-        if (input.isEmpty() || divider.isEmpty() || !divider.equals(","))
+        if (input.isEmpty() || divider.isEmpty() || !divider.equals(",")) {
             throw new IllegalArgumentException(INPUT_ERROR);
+        }
         return input.split(divider);
     }
 
     public static String deleteBracket(String input) {
-        if (!input.contains("(") || !input.contains(")")) {
+        if (!Pattern.matches("^\\([0-9]+,[0-9]+\\)$", input)) {
             throw new IllegalArgumentException(INPUT_ERROR);
         }
         return input.substring(1, input.length() - 1);
