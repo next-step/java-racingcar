8615ec124cc51db3c87ef718683b378dab997bd6
abcms031 <mjkim031@gmail.com>
2021-07-21 19:53:22 +0900

    refactor : 외부라이브러리 제거

--- src/main/java/calculator/utils/StringUtil.java
+++ src/main/java/calculator/utils/StringUtil.java
@@ -1,11 +1,9 @@
 package calculator.utils;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class StringUtil {
 
     public static void checkBlank(String expression) {
-        if(StringUtils.isBlank(expression)){
+        if(expression == null || expression.trim().length() == 0){
             throw new IllegalArgumentException("입력값이 NULL 혹은 빈 공백 문자입니다.");
         }
     }
