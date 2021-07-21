81072705c18f0527a3d38f5420f3c50bfcb95b5a
jaeyoungjang2 <lion85488@gmail.com>
2021-07-21 23:16:27 +0900

    feat: 계산코드 작성 예정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,2 +1,11 @@
-package calculator;public class Calculator {
+package calculator;
+
+import java.util.Arrays;
+
+public class Calculator {
+    public Calculator(String cmd) {
+        System.out.println("계산 시작해볼까??");
+    }
 }
+
+
