523cd8220dd5342c91acd0a2501aa2e2f57c6561
shirohoo <hch4102@naver.com>
2021-07-21 20:13:49 +0900

    feat: 자동차 게임 호출자를 작성한다

---
+++ src/main/java/racing/RacingGameRunner.java
@@ -0,0 +1,10 @@
+package racing;
+
+import static racing.RacingGamePrinter.print;
+import static racing.RacingGameScanner.*;
+
+public class RacingGameRunner {
+    public static void main(String[] args) {
+        print(RacingGame.from(enter()).run());
+    }
+}
