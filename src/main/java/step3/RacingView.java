a32cc455e227dec249423fab786aaeeda0e27074
newchk610@gmail.com <gprms@1208>
2021-07-21 11:10:46 +0900

    feat:make RacingView

--- src/main/java/step3/RacingView.java
+++ src/main/java/step3/RacingView.java
@@ -1,10 +1,27 @@
 package step3;
 
+import java.util.Scanner;
+
 public class RacingView {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+//          자동차 대수는 몇 대 인가요?
+//          3
+//          시도할 회수는 몇 회 인가요?
+//          5
+//          실행 결과
+        int carCounts = 0, raceTimes = 0;
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        carCounts = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        raceTimes = scanner.nextInt();
 
+        Race race = new Race(carCounts);
 
+        for (int i = 0; i < raceTimes; i++) {
+            race.racing();
 
+        }
     }
 }
