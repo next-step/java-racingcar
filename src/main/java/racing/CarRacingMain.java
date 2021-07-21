ff75aa13e173fd602fb27756cce629956d267417
PIH6585 <pih6585@naver.com>
2021-07-21 11:31:19 +0900

    refactor: 불필요한 method 삭제

--- src/main/java/racing/CarRacingMain.java
+++ src/main/java/racing/CarRacingMain.java
@@ -10,11 +10,11 @@ public class CarRacingMain {
     private static final String firstRequest = "자동차 대수는 몇 대 인가요?";
     private static final String secondRequest = "시도할 회수는 몇 회 인가요?";
     private static final int randomValue = 10;
-    private static List<CarModel> carModelList;
+    private static final int racingConditionValue = 3;
+    private static List<CarModel> carModelList = new ArrayList<>();
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        carModelList = new ArrayList<>();
         System.out.println(firstRequest);
         if (scanner.hasNext()) {
             createCarByNumber(intValueByScanner(scanner.next()));
@@ -44,11 +44,12 @@ public class CarRacingMain {
         }
     }
 
-    public static void createCarByNumber(int totalCarNumber) {
+    public static List<CarModel> createCarByNumber(int totalCarNumber) {
         for (int i = 0; i < totalCarNumber; i++) {
             CarModel carModel = new CarModel(i + 1);
             carModelList.add(carModel);
         }
+        return carModelList;
     }
 
     public static void tryRacingByCar(int tryraceNumber) {
@@ -59,18 +60,17 @@ public class CarRacingMain {
 
     public static void racing() {
         for (CarModel carModel : carModelList) {
-            carModel.carRacingAct(getRaceDistance());
+            carModel.carRacingAct(getRaceDistance(randomValue()));
         }
         System.out.println();
     }
 
-
-    public static boolean getRaceDistance() {
-        Random random = new Random();
-        return random.nextInt(randomValue) > 3 ? true : false;
+    public static int randomValue(){
+        return new Random().nextInt(randomValue);
     }
 
-    public List<CarModel> getCarList() {
-        return carModelList;
+    public static boolean getRaceDistance(int randomValue) {
+        return randomValue > racingConditionValue;
     }
+
 }
