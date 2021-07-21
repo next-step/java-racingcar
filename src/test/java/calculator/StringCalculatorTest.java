59ca941e1b70aa071d7f071885ae5592de87cf3b
javajigi <javajigi@gmail.com>
2021-07-21 11:29:52 +0900

    refactor: rename add to splitAnsSum

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -4,27 +4,27 @@ import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
     @Test
-    public void add_숫자하나() throws Exception {
+    public void splitAndSum_숫자하나() throws Exception {
 
     }
 
 //    @Test
-//    public void add_쉼표구분자() throws Exception {
+//    public void splitAndSum_쉼표구분자() throws Exception {
 //
 //    }
 //
 //    @Test
-//    public void add_쉼표_또는_콜론_구분자() throws Exception {
+//    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
 //
 //    }
 //
 //    @Test
-//    public void add_null_또는_빈문자() {
+//    public void splitAndSum_null_또는_빈문자() {
 //
 //    }
 //
 //    @Test(expected = RuntimeException.class)
-//    public void add_negative() throws Exception {
+//    public void splitAndSum_negative() throws Exception {
 //
 //    }
 }
