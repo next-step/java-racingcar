62c0d3212a65a449a63eb22efa845c6009bd71a8
채윤병 <ybchae0801@naver.com>
2021-07-22 01:49:45 +0900

    feat: 사칙연산 기호가 아닌 경우 예외 처리

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
@@ -44,6 +45,14 @@ class CalculatorTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> calculator.calculate(input))
                 .withMessageContaining(String.format("입력 값이 유효하지 않습니다. %s", input));
+    }
+
+    @ParameterizedTest(name = "사칙연산 기호가 아닌 경우 예외가 발생한다.")
+    @ValueSource(strings = {"1 % 2", "2 ^ 2"})
+    public void invalidOperatorTest(String input) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> calculator.calculate(input))
+                .withMessageContaining("존재하지 않는 연산자입니다.");
     }
 
 }
