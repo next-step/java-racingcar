cc86289ed90fa3e1bcccd3998dd7c2846ca9999c
jayce6190 <jayce6190gmail.com>
2021-07-21 22:43:29 +0900

    refactor: scanner 재사용

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -3,6 +3,7 @@ package racing.view;
 import java.util.Scanner;
 
 public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_NUMBER_OF_CAR = "자동차 대수는 몇 대 인가요?";
     private static final String INPUT_NUMBER_OF_COUNT = "시도할 회수는 몇 회 인가요?";
 
@@ -20,6 +21,6 @@ public class InputView {
     }
 
     private static int getNextInt() {
-        return new Scanner(System.in).nextInt();
+        return scanner.nextInt();
     }
 }
