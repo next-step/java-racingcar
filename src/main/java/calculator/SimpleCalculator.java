da672085df6d3b4d2da6a3cda072becacc37020b
ChangSubKwak <paradise1732@gmail.com>
2021-07-22 06:54:04 +0900

    feat: 빼기 기능 추가

--- src/main/java/calculator/SimpleCalculator.java
+++ src/main/java/calculator/SimpleCalculator.java
@@ -4,4 +4,8 @@ public class SimpleCalculator {
 	public String plus(String s) {
 		return Integer.valueOf(s.split(" ")[0]) + Integer.valueOf(s.split(" ")[2]) + "";
 	}
+
+	public String minus(String s) {
+		return Integer.valueOf(s.split(" ")[0]) - Integer.valueOf(s.split(" ")[2]) + "";
+	}
 }
