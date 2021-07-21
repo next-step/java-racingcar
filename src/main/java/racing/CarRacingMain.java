0b962f1a8241258e452d476808bc633da69ff647
PIH6585 <pih6585@naver.com>
2021-07-21 10:22:21 +0900

    fix : main method add

--- src/main/java/racing/CarRacingMain.java
+++ src/main/java/racing/CarRacingMain.java
@@ -7,28 +7,13 @@ import java.util.Scanner;
 
 public class CarRacingMain {
 
-    private final Scanner scanner;
+    private static final String firstRequest = "자동차 대수는 몇 대 인가요?";
+    private static final String secondRequest = "시도할 회수는 몇 회 인가요?";
+    private static List<CarModel> carModelList;
 
-    private final String firstRequest = "자동차 대수는 몇 대 인가요?";
-    private final String secondRequest = "시도할 회수는 몇 회 인가요?";
-    private List<CarModel> carModelList;
-
-    public CarRacingMain() {
-        this.scanner = new Scanner(System.in);
-        this.carModelList = new ArrayList<>();
-    }
-
-    private void printByRequest(String request) {
-        System.out.println(request);
-    }
-
-    private int intValueByScanner(String scannerValue) {
-        int intValue = toInt(scannerValue);
-        printByRequest(scannerValue);
-        return intValue;
-    }
-
-    public void car_racing() {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        carModelList = new ArrayList<>();
         printByRequest(firstRequest);
         if (scanner.hasNext()) {
             createCarByNumber(intValueByScanner(scanner.next()));
@@ -40,35 +25,44 @@ public class CarRacingMain {
         scanner.close();
     }
 
-    private int toInt(String inputValue) {
+    private static int intValueByScanner(String scannerValue) {
+        int intValue = toInt(scannerValue);
+        printByRequest(scannerValue);
+        return intValue;
+    }
+
+    private static void printByRequest(String request) {
+        System.out.println(request);
+    }
+
+    private static int toInt(String inputValue) {
         validByNumberCheck(inputValue);
         return Integer.parseInt(inputValue);
     }
 
-    private void validByNumberCheck(String inputValue) {
+    private static void validByNumberCheck(String inputValue) {
         String regExp = "^\\d+$";
         if (!inputValue.matches(regExp)) {
             throw new IllegalArgumentException("해당 문자는 숫자만 사용 가능합니다.");
         }
     }
 
-
-    private void createCarByNumber(int totalCarNumber) {
+    private static void createCarByNumber(int totalCarNumber) {
         for (int i = 0; i < totalCarNumber; i++) {
             CarModel carModel = new CarModel(i + 1);
             carModelList.add(carModel);
         }
     }
 
-    private void tryRacingByCar(int tryraceNumber) {
+    private static void tryRacingByCar(int tryraceNumber) {
         for (int i = 0; i < tryraceNumber; i++) {
             racing();
         }
     }
 
-    private void racing() {
+    private static void racing() {
         int carModelNumber = carModelList.size();
-        for(CarModel carModel : carModelList){
+        for (CarModel carModel : carModelList) {
             String carRaceResult = calculatorDistance(carModel.getRaceDistance());
             carModel.setRaceDistance(carRaceResult);
             printByRequest(carModel.getRaceDistance());
@@ -76,17 +70,17 @@ public class CarRacingMain {
         System.out.println();
     }
 
-    private String calculatorDistance(String raceDistance) {
+    private static String calculatorDistance(String raceDistance) {
         int randomNUmber = randomIntValue();
         String result = nullToBlank(raceDistance) + getRaceDistance(randomNUmber);
         return result.trim();
     }
 
-    private String nullToBlank(String raceDistance) {
+    private static String nullToBlank(String raceDistance) {
         return raceDistance == null ? "" : raceDistance;
     }
 
-    public String getRaceDistance(int randomNUmber) {
+    public static String getRaceDistance(int randomNUmber) {
         String distance = "";
         if (randomNUmber > 3) {
             distance = "-";
@@ -95,7 +89,7 @@ public class CarRacingMain {
         return distance;
     }
 
-    public int randomIntValue() {
+    public static int randomIntValue() {
         Random random = new Random();
         return random.nextInt(10);
     }
