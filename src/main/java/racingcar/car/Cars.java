5783f2f8eb4460c05131133d839a076fc772dbb3
nokchax <nokchax@gmail.com>
2021-07-21 11:47:38 +0900

    [G] feat: throw exception when names are null

--- src/main/java/racingcar/car/Cars.java
+++ src/main/java/racingcar/car/Cars.java
@@ -5,6 +5,7 @@ import racingcar.strategy.MoveStrategy;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -44,6 +45,10 @@ public class Cars {
     }
 
     private void validate(String... names) {
+        if (Objects.isNull(names)) {
+            throw new IllegalArgumentException("Car names can't be null");
+        }
+
         if (names.length < MINIMUM_NUMBER_OF_CARS) {
             throw new IllegalArgumentException("Number of cars can't be under 1");
         }
