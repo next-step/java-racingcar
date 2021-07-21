7221e407ae12a583c4a67d5c1e160fd22e62f5e4
harris <harrisleesh@gmail.com>
2021-07-21 17:39:01 +0900

    parameterizedTest with csvSource test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,12 +3,14 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -32,5 +34,11 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void csvSource(String input, String expected) {
+        assertEquals(numbers.contains(Integer.parseInt(input)), Boolean.parseBoolean(expected));
     }
 }
