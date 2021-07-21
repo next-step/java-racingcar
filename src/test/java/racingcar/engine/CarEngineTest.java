dcb4cc89e7bababea9d955450c62b1082b4ff3fa
choibyunghyeon <choi8608@gmail.com>
2021-07-21 19:39:02 +0900

    test: 자동차 엔진 작동 테스트 추가

--- src/test/java/racingcar/engine/CarEngineTest.java
+++ src/test/java/racingcar/engine/CarEngineTest.java
@@ -1,6 +1,7 @@
 package racingcar.engine;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,7 +10,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 @DisplayName("[Step3] 엔진")
 class CarEngineTest {
 
-    private Engine engine;
+    private Engine engine = new CarEngine();
+    ;
 
     @DisplayName("[성공] 작동 - 자동차 엔진")
     @ParameterizedTest
@@ -22,12 +24,26 @@ class CarEngineTest {
     )
     public void operate_carEngine(int input, int expected) {
         // given
-        engine = new CarEngine();
 
         // when
         int distance = engine.operate(input);
 
         // then
         assertThat(distance).isEqualTo(expected);
+    }
+
+    @DisplayName("[실패] 작동 - 입력 범위를 벗어남")
+    @ParameterizedTest
+    @CsvSource(value = {
+        "-1",
+        "10"}
+    )
+    public void operate_outOfRange(int input) {
+        // given
+
+        // when
+        assertThrows(IllegalArgumentException.class, () -> engine.operate(input));
+
+        // then
     }
 }
