8a3654cfe740295ab93dc291feeb3c3c98127f39
rkdals213 <rkdals213@naver.com>
2021-07-21 13:19:59 +0900

    fix: ResultView 결과 출력 로직 수정 Cars 패키지에서 실행하던 출력 로직 옮겨옴

--- src/main/java/step3/view/ResultView.java
+++ src/main/java/step3/view/ResultView.java
@@ -1,14 +1,35 @@
 package step3.view;
 
-import step3.model.CarGroup;
+import step3.game.Game;
+import step3.model.Cars;
+
+import java.util.List;
 
 public class ResultView {
-    static public void showResultOfTurn(CarGroup resultOfTurn) {
-        resultOfTurn.show();
-        printBorder();
+    static public void showResult(Cars resultOfTurn, int count) {
+        for (int i = 0; i < count; i++) {
+            List<Integer> pointOfCarsByTime = resultOfTurn.getPointOfCarsByTime(i);
+            pointOfCarsByTime.forEach(ResultView::printDistance);
+            printBorder();
+        }
+    }
+
+    static void printDistance(int distance) {
+        for (int i = 0; i < distance; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
     static private void printBorder() {
         System.out.println("==========================");
+    }
+
+    public static void showResult(Game game) {
+        for (int i = 0; i < game.countOfGame(); i++) {
+            List<Integer> pointOfCarsByTime = game.cars().getPointOfCarsByTime(i);
+            pointOfCarsByTime.forEach(ResultView::printDistance);
+            printBorder();
+        }
     }
 }
