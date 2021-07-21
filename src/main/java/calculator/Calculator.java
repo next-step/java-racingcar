25a0593d35317e60c18a3c08c2cd33a763542080
shryu <>
2021-07-21 14:32:52 +0900

    refactor : 리뷰 반영 Enum 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,37 +1,23 @@
 package calculator;
 
+import static calculator.CalculatorSign.*;
+
 public class Calculator {
 
     public static int calculate(int result, int number, String sign) {
         if(sign.equals("+")) {
-            return add(result, number);
+            return SUM.calculate(result,number);
         }
         if(sign.equals("*")) {
-            return multiply(result, number);
+            return MULTIPLY.calculate(result,number);
         }
         if(sign.equals("-")) {
-            return minus(result, number);
+            return MINUS.calculate(result,number);
         }
         if(sign.equals("/")) {
-            return divide(result, number);
+            return DIVIDE.calculate(result,number);
         }
 
         throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
-    }
-
-    private static int divide(int result, int number) {
-        return result/number;
-    }
-
-    private static int minus(int result, int number) {
-        return result-number;
-    }
-
-    private static int multiply(int result, int number) {
-        return result*number;
-    }
-
-    private static int add(int result, int number) {
-        return result+number;
     }
 }
