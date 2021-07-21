9f25f9a43527b92c845d63cadea751510bd64161
harris <harrisleesh@gmail.com>
2021-07-21 17:07:07 +0900

    substring study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -6,9 +6,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
-    void split(){
+    void split() {
         String[] split = "1,2".split(",");
         assertThat(split).contains("1");
         assertThat(split).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring(){
+        String substring = "(1,2)".substring(1, 4);
+        assertThat(substring).isEqualTo("1,2");
     }
 }
