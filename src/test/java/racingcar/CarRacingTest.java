9655c0ef2369f6e4ff2fcf5e2a16c7b390db031a
nokchax <nokchax@gmail.com>
2021-07-21 13:24:03 +0900

    chore: code optimization

--- src/test/java/racingcar/CarRacingTest.java
+++ src/test/java/racingcar/CarRacingTest.java
@@ -3,17 +3,13 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.dto.CarDto;
 import racingcar.helper.Fixture;
 import racingcar.param.RacingCarInitParam;
-import racingcar.strategy.MoveStrategy;
 
 import java.util.List;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
