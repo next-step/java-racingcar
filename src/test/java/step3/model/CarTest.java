11517ed523d2e579bc7690cc961db19fc6ae8b90
newchk610@gmail.com <gprms@1208>
2021-07-21 16:55:25 +0900

    test:carTest

--- src/test/java/step3/model/CarTest.java
+++ src/test/java/step3/model/CarTest.java
@@ -1,22 +1,35 @@
 package step3.model;
 
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CarTest {
 
-    static Car car;
-    @BeforeAll
-    static void create(){
+    Car car;
+
+    @BeforeEach
+    void create(){
         car = new Car();
     }
 
     @DisplayName("자동차 초기값 확인")
     @Test
     void initPositionTest(){
-
+        assertThat(car.getPosition()).isEqualTo(0);
     }
+
+    @DisplayName("차 위치 변경")
+    @Test
+    void moveCar(){
+        car.move(true);
+        assertThat(car.getPosition()).isEqualTo(1);
+        car.move(false);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
 
 }
