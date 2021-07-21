e87e1751925c569cc69e700432fa456c5ee63cf5
rkdals213 <rkdals213@naver.com>
2021-07-21 13:20:23 +0900

    test: 테스트 코드 수정

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import step3.model.Car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class CarTest {
     private Car car;
@@ -24,7 +25,7 @@ public class CarTest {
         int expected = 1;
 
         //when
-        car.move();
+        car.move(5);
 
         //then
         assertThat(car.now()).isEqualTo(expected);
@@ -32,7 +33,7 @@ public class CarTest {
 
     @ParameterizedTest(name = "{displayName} / value = {0}")
     @CsvSource(value = {"1:0", "2:0", "3:0", "4:1", "5:1", "6:1"}, delimiter = ':')
-    @DisplayName("Car Move If")
+    @DisplayName("Car MoveIf")
     public void carMoveIf(int num, int expected) {
         //given, when
         car.moveIf(num);
@@ -40,4 +41,16 @@ public class CarTest {
         //then
         assertThat(car.now()).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"10", "11", "-1"})
+    @DisplayName("Car MoveIf Fail")
+    public void carMoveWithException(int num) {
+        //given, when, then
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+            car.moveIf(num);
+        });
+    }
+
+
 }
--- src/test/java/step3/CarsTest.java
+++ src/test/java/step3/CarsTest.java
@@ -3,20 +3,20 @@ package step3;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import step3.model.CarGroup;
+import step3.model.Cars;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CarGroupTest {
+public class CarsTest {
 
     @ParameterizedTest(name = "{displayName} / value = {0}")
     @CsvSource(value = {"1:1", "2:2", "3:3", "4:4", "5:5"}, delimiter = ':')
     @DisplayName("CarGroup 사이즈 테스트")
     public void CarGroupSize(int num, int expected) {
         //given
-        CarGroup carGroup = new CarGroup(num);
+        Cars cars = Cars.of(num);
 
         //when, then
-        assertThat(carGroup.countOfCars()).isEqualTo(expected);
+        assertThat(cars.countOfCars()).isEqualTo(expected);
     }
 }
