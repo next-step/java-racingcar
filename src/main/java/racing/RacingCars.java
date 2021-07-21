a71bfc56a55336ee37b689ee3b7c7e55ec8cd4c8
shirohoo <hch4102@naver.com>
2021-07-21 20:13:12 +0900

    feat: 자동차를 표현할 객체를 작성한다

---
+++ src/main/java/racing/RacingCars.java
@@ -0,0 +1,36 @@
+package racing;
+
+import java.util.ArrayList;
+
+class RacingCars {
+    private static final String DISPLAY_MARK = "-";
+
+    private final ArrayList<String> cars;
+
+    private RacingCars(ArrayList<String> cars) {
+        this.cars = cars;
+    }
+
+    public static RacingCars from(ArrayList<String> cars) {
+        return new RacingCars(cars);
+    }
+
+    public int size() {
+        return this.cars.size();
+    }
+
+    public void goForward(int index, int randomNumber) {
+        if(!isNumberMoreThanFour(randomNumber)) {
+            return;
+        }
+        cars.set(index, cars.get(index) + DISPLAY_MARK);
+    }
+
+    public String get(int index) {
+        return cars.get(index);
+    }
+
+    private boolean isNumberMoreThanFour(int number) {
+        return number >= 4;
+    }
+}
---
+++ src/test/java/racing/RacingCarsTest.java
@@ -0,0 +1,55 @@
+package racing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingCarsTest {
+    private ArrayList<String> list;
+
+    @BeforeEach
+    void setUp() {
+        list = new ArrayList<>();
+        list.add("-");
+        list.add("-");
+        list.add("-");
+    }
+
+    @Test
+    @DisplayName("정적_팩토리가_인스턴스를_생성하는지_확인한다")
+    void from() throws Exception {
+        assertThat(RacingCars.from(list)).isInstanceOf(RacingCars.class);
+    }
+
+    @Test
+    @DisplayName("조회_테스트")
+    void get() throws Exception {
+        assertThat(RacingCars.from(list).get(0)).isEqualTo("-");
+    }
+
+    @Test
+    @DisplayName("전진_테스트_조건은_참")
+    void goForward() throws Exception {
+        RacingCars racingCars = RacingCars.from(list);
+        racingCars.goForward(0, 4);
+        assertThat(racingCars.get(0)).isEqualTo("--");
+    }
+
+    @Test
+    @DisplayName("전진_테스트_조건은_거짓")
+    void goForwardNot() throws Exception {
+        RacingCars racingCars = RacingCars.from(list);
+        racingCars.goForward(0, 3);
+        assertThat(racingCars.get(0)).isEqualTo("-");
+    }
+
+    @Test
+    @DisplayName("사이즈_조회_테스트")
+    void size() throws Exception {
+        assertThat(RacingCars.from(list).size()).isEqualTo(3);
+    }
+}
