0b62075f28f9d4f8eecf7548b77efd9be3ad4b45
sunghuna32 <sunghuna32@gmail.com>
2021-07-21 19:46:24 +0900

    [2021.07.22]RacingCar_Step1_Request_2

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,49 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        int result = numbers.size();
+        assertThat(result).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2,3})
+    void contains(int input) {
+        assertTrue(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource (value = {"1:TRUE", "2:TRUE", "3:TRUE", "4:FALSE","5:FALSE"}, delimiter = ':')
+    void containBool(int input, boolean expected) {
+        assertEquals(expected, numbers.contains(input));
+    }
+
+
+
+
+}
