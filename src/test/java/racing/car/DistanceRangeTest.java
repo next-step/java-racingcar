dc850079475d06e733a599a06217167fd38b435d
darkant99 <darkant99@naver.com>
2021-07-23 01:36:43 +0900

    [TEST] 잘못된 테스트 검증 오류 해결

--- src/test/java/racing/car/DistanceRangeTest.java
+++ src/test/java/racing/car/DistanceRangeTest.java
@@ -9,9 +9,14 @@ class DistanceRangeTest {
     @CsvSource({
             "-1,BACK_MOVEMENT",
             "0,NOT_MOVEMENT",
-            "1,MIN_MOVEMENT",
+            "1,NOT_MOVEMENT",
+            "2,NOT_MOVEMENT",
+            "3,NOT_MOVEMENT",
+            "4,MIN_MOVEMENT",
+            "5,MIN_MOVEMENT",
+            "6,MIN_MOVEMENT",
             "10,MAX_MOVEMENT",
-            "15,MIN_MOVEMENT"
+            "15,MAX_MOVEMENT"
     })
     @ParameterizedTest
     public void ofTest(int distance, String objName) {
