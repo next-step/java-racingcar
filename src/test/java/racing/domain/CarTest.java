d98e4fca64593cb559963944c2ee1cb72e7c5d40
0n1dev <all_mind@naver.com>
2021-07-22 18:49:29 +0900

    refactor : Car 테스트 순서 보장 제거
    - BeforeEach 어노테이션을 통해 테스트 케이스마다 객체 생성

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -3,27 +3,29 @@ package racing.domain;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-// 순서 보장 필요
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CarTest {
 
-	private Car car = new Car();
+	private Car car;
+
+	@BeforeEach
+	void carInit() {
+		car = new Car();
+	}
 
 	@Test
-	@Order(1)
 	@DisplayName("Car 오브젝트 초기 move 값 확인")
 	void init() {
 		assertThat(car.toString()).isEqualTo("");
 	}
 
 	@Test
-	@Order(2)
 	@DisplayName("Car 오브젝트 초기 move 값 확인 실패 케이스")
 	void failInit() {
 		assertThat(car.toString()).isNotEqualTo("-");
@@ -31,7 +33,6 @@ class CarTest {
 
 
 	@Test
-	@Order(3)
 	@DisplayName("move() 메서드 호출 시 move 값 확인")
 	void move() {
 		car.move();
@@ -39,10 +40,9 @@ class CarTest {
 	}
 
 	@Test
-	@Order(4)
 	@DisplayName("move() 메서드 호출 시 move 값 확인 실패 케이스")
 	void failMove() {
 		car.move();
-		assertThat(car.toString()).isEqualTo("-");
+		assertThat(car.toString()).isNotEqualTo("--");
 	}
 }
