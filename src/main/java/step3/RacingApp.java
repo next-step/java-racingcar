a34c8de307a83923fae75b2a7663ed1354ba179d
newchk610@gmail.com <gprms@1208>
2021-07-22 15:34:17 +0900

    refactor:메서드 분리

--- src/main/java/step3/RacingApp.java
+++ src/main/java/step3/RacingApp.java
@@ -8,12 +8,18 @@ public class RacingApp {
     public static final int MOVE_BOUNDARY = 4;
     public static void main(String[] args) {
 
-        carCounts = InputView.inputCountCar();
-        raceTimes = InputView.inputRaceTimes();
+        startApp();
+    }
 
+    private static void startApp() {
+        input();
         ResultView.startRace(carCounts,raceTimes);
     }
 
+    private static void input() {
+        carCounts = InputView.inputCountCar();
+        raceTimes = InputView.inputRaceTimes();
+    }
 
 
 }
