57c3b731907db5a02178afe8b7fa311c80b6f4bb
jung0930 <wjddlqkqh57@naver.com>
2021-07-22 21:15:41 +0900

    feat : 자동차 도메인 작업, 테스트코드 작성

---
+++ src/main/java/racingCar/domain/Car.java
@@ -0,0 +1,28 @@
+package racingCar.domain;
+
+import racingCar.util.RandomNumber;
+
+public class Car {
+
+    private final Distance distance;
+
+    public Car() {
+        this.distance = new Distance();
+    }
+
+    public Car(Distance distance) {
+        this.distance = distance;
+    }
+
+    public Car move(boolean command) {
+        if(command) {
+            return new Car(this.distance.addDistance());
+        }
+        return this;
+    }
+
+    public int getCarDistance() {
+        return distance.getDistance();
+    }
+
+}
