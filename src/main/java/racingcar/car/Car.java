5671a51a2e3ce3a71b626e260d4e088edc02fb21
seondongpyo <seondongpyo@gmail.com>
2021-07-22 17:53:39 +0900

    refactor: [피드백 반영] 자동차 위치를 문자열로 표현하는 로직을 ResultView로 이동

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -3,7 +3,6 @@ package racingcar.car;
 public class Car {
 
     private static final int MOVABLE_MINIMUM_NUMBER = 4;
-    private static final String SYMBOL = "-";
     private int position;
 
     public void move(int number) {
@@ -14,14 +13,6 @@ public class Car {
 
     public int getPosition() {
         return position;
-    }
-
-    public String getPositionAsSymbol() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < position; i++) {
-            stringBuilder.append(SYMBOL);
-        }
-        return stringBuilder.toString();
     }
 
 }
