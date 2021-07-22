b7e3722d9607422d9b1f3ebc3ef6d9d67d436071
0n1dev <all_mind@naver.com>
2021-07-22 16:37:51 +0900

    feat : Car 클래스 move 메서드 호출시 테스트 케이스 작성
    - 테스트 케이스 순서 보장 적용

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -9,19 +9,40 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+// 순서 보장 필요
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CarTest {
 
 	private Car car = new Car();
 
 	@Test
+	@Order(1)
 	@DisplayName("Car 오브젝트 초기 move 값 확인")
 	void init() {
 		assertThat(car.toString()).isEqualTo("");
 	}
 
 	@Test
+	@Order(2)
 	@DisplayName("Car 오브젝트 초기 move 값 확인 실패 케이스")
 	void failInit() {
 		assertThat(car.toString()).isNotEqualTo("-");
+	}
+
+
+	@Test
+	@Order(3)
+	@DisplayName("move() 메서드 호출 시 move 값 확인")
+	void move() {
+		car.move();
+		assertThat(car.toString()).isEqualTo("-");
+	}
+
+	@Test
+	@Order(4)
+	@DisplayName("move() 메서드 호출 시 move 값 확인 실패 케이스")
+	void failMove() {
+		car.move();
+		assertThat(car.toString()).isEqualTo("-");
 	}
 }
