e29c0c3e5495d57595c491282315cf74fc246a65
shineoov <shineoov88@gmail.com>
2021-07-22 23:20:07 +0900

    Operator Test 파일 사용하지 않는 import 제거

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("사칙 연산 테스트")
