54725a79eff004be54c866ebc2b7c1cafd7239f8
0n1dev <all_mind@naver.com>
2021-07-22 15:07:44 +0900

    feat : 입력 받는 UI 구현
    - RacingMain 클래스 추가
    - InputView 클래스 추가

---
+++ src/main/java/racing/RacingMain.java
@@ -0,0 +1,11 @@
+package racing;
+
+import racing.view.InputView;
+
+public class RacingMain {
+
+	public static void main(String[] args) {
+		int carCount = InputView.inputCarCount();
+		int tryCount = InputView.inputTryCount();
+	}
+}
