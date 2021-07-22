187e1271222df543c0cc5de2c91130fe104b2875
ckdgus08 <ckdgus0808@naver.com>
2021-07-22 21:04:20 +0900

    refactor: 매직넘버 -> 상수화

--- src/main/java/racingGame/RandomNumber.java
+++ src/main/java/racingGame/RandomNumber.java
@@ -4,8 +4,10 @@ import java.util.Random;
 
 public class RandomNumber {
 
+    public static final int RANDOM_NUMBER_BOUND = 10;
+
     public static int makeRandomNumber() {
         Random random = new Random();
-        return random.nextInt(10);
+        return random.nextInt(RANDOM_NUMBER_BOUND);
     }
 }
