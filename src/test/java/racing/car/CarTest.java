17727e66e9b4c2561ffbbfa746941392c6c6fa91
darkant99 <darkant99@naver.com>
2021-07-23 03:21:55 +0900

    [TEST] Car 클래스 변경에 따른 Test 수정

--- src/test/java/racing/car/CarTest.java
+++ src/test/java/racing/car/CarTest.java
@@ -2,34 +2,31 @@ package racing.car;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static racing.car.DistanceRange.*;
 
 class CarTest {
-    @ValueSource(ints = {
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    @CsvSource({
+            "FULL,1",
+            "EMPTY,0"
     })
     @DisplayName("Move 테스트")
     @ParameterizedTest
-    public void moveTest(int distanceValue) {
-        // Distance 관련 (후진, 최대 이동 가능 거리 초과) 테스트는 DistanceTest 클래스에서 진행
+    public void moveTest(String fuelName, int distance) {
+        Fuel fuel = Fuel.valueOf(fuelName);
+
         Car car = new Car();
         Location beforeLocation = car.getLocation();
-        Distance distance = Distance.newInstance(distanceValue);
 
-        car.move(distance);
+        car.move(fuel);
 
         assertThat(
                 car.getLocation()
                         .min(beforeLocation)
                         .getValue()
         ).withFailMessage("지정한 횟수만큼 이동하지 않았습니다.")
-                .isEqualTo(
-                        distance.getDistanceRange() == NOT_MOVEMENT ?
-                                DistanceRange.NOT_MOVEMENT_VALUE :
-                                distanceValue
-                );
+                .isEqualTo(distance);
     }
 }
