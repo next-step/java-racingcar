8b49c3ef9e11acb27cc906f2a6059b5ae7acf8e4
lsj8367 <lsj8367@naver.com>
2021-07-22 10:58:36 +0900

    feat: 우승자 선발기능 추가

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,19 +1,50 @@
 package racingcar;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class RacingGame {
+    private static final int MAX_VALUE = 10;
+
     public static void main(String[] args) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
 
-        int carNumber = inputView.getNumberOfCars();
+        String[] carNames = inputView.insertCarName();
+        int carNumber = carNames.length;
         int tryNumber = inputView.getNumberOfTry();
-        List<Car> cars = Car.createCars(carNumber);
+
+        List<Car> cars = Car.createCars(carNames, carNumber);
 
         for(int i = 0; i < tryNumber; i++) {
-            resultView.printAll(cars);
+            printAll(cars);
             System.out.println();
+        }
+
+        Collections.reverse(cars);
+
+        int first = cars.stream().map(Car::getMoveSpace)
+                                 .max(Integer::compareTo)
+                                 .get();
+
+        List<Car> winners = cars.stream()
+                                .filter(car -> car.getMoveSpace() == first)
+                                .collect(Collectors.toList());
+
+        resultView.getWinner(winners);
+    }
+
+    public static void printAll(List<Car> cars) {
+        ResultView resultView = new ResultView();
+        for(Car car: cars) {
+            Random random = new Random();
+
+            int randomNumber = random.nextInt(MAX_VALUE);
+            int moveNumber = car.move(randomNumber);
+
+            resultView.print(car.getName(), moveNumber);
         }
     }
 }
--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -1,29 +1,25 @@
 package racingcar;
 
 import java.util.List;
-import java.util.Random;
 
 /*
  * 자동차 경주 결과 View
  */
 public class ResultView {
-
-    private static final int MAX_VALUE = 10;
-
-    public void print(int move) {
+    public void print(String carname, int move) {
+        System.out.print(carname + ":");
         for (int i = 0; i < move; i++) {
             System.out.print("-");
         }
         System.out.println();
     }
 
-    public void printAll(List<Car> cars) {
-        for(Car car: cars) {
-            Random random = new Random();
-            int randomNumber = random.nextInt(MAX_VALUE);
-            int moveNumber = car.move(randomNumber);
-            //3회
-            print(moveNumber);
+    public void getWinner(List<Car> winners) {
+        StringBuilder winnerText = new StringBuilder();
+
+        for (Car winner : winners) {
+            winnerText.append(winner.getName()).append(" ");
         }
+        System.out.println("우승자는 " + winnerText + "입니다.");
     }
 }
