9a4a84d6e66d69ddac195187d4877269ada1facd
ChangSubKwak <paradise1732@gmail.com>
2021-07-22 09:59:57 +0900

    feat: 나눗셈시 결과 값이 정수가 아닐 경우에 대한 예외처리 및 예외클래스 추가

--- src/main/java/calculator/SimpleCalculator.java
+++ src/main/java/calculator/SimpleCalculator.java
@@ -17,7 +17,12 @@ public class SimpleCalculator {
 		return (selectOperand(s, 0) * selectOperand(s, 2)) + "";
 	}
 
-	public String divide(String s) {
+	public String divide(String s) throws NotEqualRemainderZero {
+		int firstOperand  = selectOperand(s, 0);
+		int secondOperand = selectOperand(s, 2);
+		if (firstOperand % secondOperand != 0) {
+			throw new NotEqualRemainderZero();
+		}
 		return (selectOperand(s, 0) / selectOperand(s, 2)) + "";
 	}
 }
