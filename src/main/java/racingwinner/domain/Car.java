369c9d9f6499c38c4c466d2a3f5c757fdc4105a4
YONGJU <odetsuo@gmail.com>
2021-07-22 23:39:26 +0900

    feat : 자동차 이름의 길이 최대 5자로 제한

--- src/main/java/racingwinner/domain/Car.java
+++ src/main/java/racingwinner/domain/Car.java
@@ -7,6 +7,9 @@ public class Car {
 	private final String name;
 
 	public Car(String name) {
+		if (name.length() > 5) {
+			throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+		}
 		this.name = name;
 	}
 
--- src/test/java/racingwinner/domain/CarTest.java
+++ src/test/java/racingwinner/domain/CarTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
 
@@ -43,6 +44,19 @@ class CarTest {
 
 		//then
 		assertThat(car.getForwardCount()).isNotZero();
+	}
+
+	@Test
+	@DisplayName("자동차의 이름이 제한 길이를 초과하는 경우")
+	void wrong_name_car() throws Exception {
+		//given
+		String longName = "long name";
+
+		//when, then
+		assertThatThrownBy(() -> new Car(longName))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("자동차 이름은 5자를 초과할 수 없습니다.");
+
 	}
 
 }
