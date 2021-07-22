112979007f83e92d611304ade5c0a147cb6856f1
sk1737030 <sk1737030@naver.com>
2021-07-22 22:23:25 +0900

    test: 테스트코드 리팩토링 setup으로 추출

--- src/test/java/step3/CarStadiumTest.java
+++ src/test/java/step3/CarStadiumTest.java
@@ -1,5 +1,6 @@
 package step3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
@@ -7,16 +8,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarStadiumTest {
 
+    private int givenLimitTime;
+    private int givenCars;
+    private Referee referee;
+    private CarStadium carStadium;
+
+    @BeforeEach
+    void setUp() {
+        givenLimitTime = 10;
+        givenCars = 5;
+
+        referee = new Referee(givenLimitTime);
+        carStadium = new CarStadium(referee, new ArrayList<>());
+    }
+
     @Test
     void 스타디움의_입장한_자동차_수와_총_몇번_움직일_수_있는지_알_수_있어야한다() {
         // given
-        int givenLimitTime = 10;
-        int givenCars = 5;
-
-        Referee referee = new Referee(givenLimitTime);
-
-        CarStadium carStadium = new CarStadium(referee, new ArrayList<>());
-
         for (int i = 0; i < givenCars; i++) {
             carStadium.enterCar(new Car(new CarEngine()));
         }
