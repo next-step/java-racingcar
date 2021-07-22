b9084be5e5d21b6d05fbce5036ab652c5904cb1e
darkant99 <darkant99@naver.com>
2021-07-22 21:28:14 +0900

    [REFACTOR] moveTest 피드백 반영

--- src/main/java/racing/car/Car.java
+++ src/main/java/racing/car/Car.java
@@ -1,44 +1,24 @@
 package racing.car;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class Car {
-    private static Random random = new Random();
+    private static final int MIN_MOVEMENT_DISTANCE = 4;
 
-    private Location lastMoveDistance;
     private Location location;
-    private int moveCount;
 
     public Car() {
         this.location = Location.EMPTY;
-        moveCount = 0;
     }
 
     public Location getLocation() {
         return location;
     }
 
-    public Location getLastMoveDistance() {
-        return lastMoveDistance;
-    }
-
-    public int getMoveCount() {
-        return moveCount;
-    }
-
-    public void move() {
-        // 전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다.
-        int randomValue = random.nextInt(9);
-        Location distance = new Location(
-                randomValue >= 4 ? randomValue : 0
-        );
-        move(distance);
-    }
-
-    private void move(Location distance) {
-        ++moveCount;
-
+    public void move(Location distance) {
+        if (Objects.isNull(distance) || distance.getValue() < MIN_MOVEMENT_DISTANCE)
+            return;
         this.location = location.add(distance);
-        this.lastMoveDistance = distance;
     }
 }
