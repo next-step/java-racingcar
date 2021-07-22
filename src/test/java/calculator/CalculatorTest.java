1d1f4ea2a3f6d69182773b547a51a7f7b7eacb63
HueWoon Kim <kho0307@gmail.com>
2021-07-23 00:07:17 +0900

    test: 사칙연산 테스트 케이스 구현

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -36,9 +36,9 @@ public class CalculatorTest {
 	void multiplication() {
 		CalculatorNumber three = new CalculatorNumber(3);
 		CalculatorNumber two = new CalculatorNumber(2);
-		CalculatorOperator subOperator =  CalculatorOperatorFactory.create(CalculatorOperator.MULTIPLY);
+		CalculatorOperator mulOperator =  CalculatorOperatorFactory.create(CalculatorOperator.MULTIPLY);
 
-		CalculatorNumber result = subOperator.operate(three, two);
+		CalculatorNumber result = mulOperator.operate(three, two);
 
 		assertThat(result.toInteger()).isEqualTo(6);
 	}
@@ -48,10 +48,31 @@ public class CalculatorTest {
 	void division() {
 		CalculatorNumber three = new CalculatorNumber(3);
 		CalculatorNumber two = new CalculatorNumber(2);
-		CalculatorOperator subOperator =  CalculatorOperatorFactory.create(CalculatorOperator.DIVIDE);
+		CalculatorOperator divOperator =  CalculatorOperatorFactory.create(CalculatorOperator.DIVIDE);
 
-		CalculatorNumber result = subOperator.operate(three, two);
+		CalculatorNumber result = divOperator.operate(three, two);
 
 		assertThat(result.toInteger()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("사칙연산")
+	void fourFundamentalArithmeticOperations() {
+		CalculatorNumber two = new CalculatorNumber(2);
+		CalculatorNumber three = new CalculatorNumber(3);
+		CalculatorNumber four = new CalculatorNumber(4);
+		CalculatorOperator addOperator =  CalculatorOperatorFactory.create(CalculatorOperator.PLUS);
+		CalculatorOperator subOperator =  CalculatorOperatorFactory.create(CalculatorOperator.MINUS);
+		CalculatorOperator mulOperator =  CalculatorOperatorFactory.create(CalculatorOperator.MULTIPLY);
+		CalculatorOperator divOperator =  CalculatorOperatorFactory.create(CalculatorOperator.DIVIDE);
+
+		// 2 + 3 * 4 / 2 - 2 = 8
+		CalculatorNumber result = subOperator.
+			operate(divOperator.
+				operate(mulOperator.
+					operate(addOperator.
+						operate(two, three), four), two), two);
+
+		assertThat(result.toInteger()).isEqualTo(8);
 	}
 }
