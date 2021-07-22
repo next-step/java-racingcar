42a8861966847686d78c87594dcc1b38df4ec17a
minyul <ggomjae@gmail.com>
2021-07-22 20:20:53 +0900

    특정 위치의 문자를 갖고 오는 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -16,5 +18,19 @@ public class StringTest {
 	void substring() {
 		String result = "(1,2)".substring(1,4);
 		assertThat(result).isEqualTo("1,2");
+	}
+
+	@DisplayName("특정 위치의 문자를 갖고 오는 테스트")
+	@Test
+	void charAt() {
+
+		String str = "abc";
+		char target_1 = 'b';
+		assertThat(target_1).isEqualTo(str.charAt(1));
+
+		assertThatThrownBy(() -> {
+			char target_2 = str.charAt(3);
+		}).isInstanceOf(StringIndexOutOfBoundsException.class)
+						.hasMessageContaining("String index out of range: 3");
 	}
 }
