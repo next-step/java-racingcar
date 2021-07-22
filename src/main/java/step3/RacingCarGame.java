bdb5b2cfbeee24b9f03eaa4d152eb476c03a2439
sk1737030 <sk1737030@naver.com>
2021-07-22 20:43:29 +0900

    Docs: 사용자는 몇대의 자동차로 몇 번의 이동을 할 것인지 입력할 수 있어야한다.

---
+++ src/main/java/step3/RacingCarGame.java
@@ -0,0 +1,16 @@
+package step3;
+
+import java.util.Scanner;
+
+public class RacingCarGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carCount = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int tryCount = scanner.nextInt();
+
+
+    }
+}
