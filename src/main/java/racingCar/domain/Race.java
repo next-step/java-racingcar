f772d397bb24b6582c3a504a7bf2feb3c2c7591e
jung0930 <wjddlqkqh57@naver.com>
2021-07-22 21:36:31 +0900

    test : 자동차 대수가 1대 이상일 경우 정상적으로 객체 생성됐는지 확인하는 테스트코드 추가

--- src/main/java/racingCar/domain/Race.java
+++ src/main/java/racingCar/domain/Race.java
@@ -1,2 +1,21 @@
-package racingCar.domain;public class Race {
+package racingCar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+
+    private final List<Car> cars = new ArrayList<>();
+    private final static String IS_NOT_DUAL_NUMBER_ERROR_MESSAGE = "자동차 대수는 1대 이상이어야 합니다.";
+
+    public Race(int count) {
+        validateInput(count);
+    }
+
+    private void validateInput(int count) {
+        if(count < 1) {
+            throw new IllegalArgumentException(IS_NOT_DUAL_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
 }
