8aceb48dfb72ed5a041ec33851d3c781b559faaa
ChangSubKwak <paradise1732@gmail.com>
2021-07-22 10:46:11 +0900

    feat: 수식 문자열 입력을 계산하는 기능 추가

--- src/main/java/calculator/SimpleCalculator.java
+++ src/main/java/calculator/SimpleCalculator.java
@@ -1,5 +1,9 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SimpleCalculator {
 	public Integer selectOperand(String s, int idx) {
 		return Integer.valueOf(s.split(" ")[idx]);
@@ -24,5 +28,36 @@ public class SimpleCalculator {
 			throw new NotEqualRemainderZero();
 		}
 		return (selectOperand(s, 0) / selectOperand(s, 2)) + "";
+	}
+
+	public String calc(String s) throws NotEqualRemainderZero {
+		List<String> tempList = new ArrayList<>(Arrays.asList(s.split(" ")));
+
+		while (tempList.size() != 1) {
+			String result = null;
+			result = callFunctionByOperator(tempList);
+			for (int i = 0 ; i <= 2 ; i++) {
+				tempList.remove(0);
+			}
+			tempList.add(0, result);
+		}
+		return tempList.get(0);
+	}
+
+	private String callFunctionByOperator(List<String> tempList) throws NotEqualRemainderZero {
+		String result = null;
+		if ("+".equals(tempList.get(1))) {
+			result = plus(tempList.get(0) + " + " + tempList.get(2));
+		}
+		if ("-".equals(tempList.get(1))) {
+			result = minus(tempList.get(0) + " - " + tempList.get(2));
+		}
+		if ("*".equals(tempList.get(1))) {
+			result = multiply(tempList.get(0) + " * " + tempList.get(2));
+		}
+		if ("/".equals(tempList.get(1))) {
+			result = divide(tempList.get(0) + " / " + tempList.get(2));
+		}
+		return result;
 	}
 }
