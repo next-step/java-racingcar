a9f7297379561465b87ce2503a30dd3db27e2802
YONGJU <odetsuo@gmail.com>
2021-07-22 10:35:53 +0900

    refactor : 랜덤 값 생성 클래스 분리

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,15 +1,13 @@
 package racing.domain;
 
-import java.util.Random;
-
 public class Car {
 
 	private int forwardCount = 0;
 	private static final int FORWARD_STANDARD = 4;
-	private static final int RANDOM_BOUND = 10;
+
 
 	public void moveForward() {
-		int randomValue = new Random().nextInt(RANDOM_BOUND);
+		int randomValue = RandomGenerator.generate();
 		if (canMoveForward(randomValue)) {
 			forwardCount++;
 		}
