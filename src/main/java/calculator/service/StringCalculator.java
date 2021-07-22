d4c13b1daab4f7d35ddfa1f9ef0f81d5c53b52b5
abcms031 <mjkim031@gmail.com>
2021-07-22 20:39:18 +0900

    refactor : 한 메서드에 한단계의 들여쓰기만 하도록 수정, 매직넘버 수정

--- src/main/java/calculator/service/StringCalculator.java
+++ src/main/java/calculator/service/StringCalculator.java
@@ -11,53 +11,50 @@ public class StringCalculator {
 
     private static final Pattern OPERAND_REGEX = Pattern.compile("[0-9]");
     private static final Pattern OPERATOR_REGEX = Pattern.compile("[+\\-*/]");
+    private static final Integer EXPRESSION_SEPARATOR = 2;
 
     private ArrayList<Integer> operandList = new ArrayList<>();
     private ArrayList<String> operatorList = new ArrayList<>();
 
 
     public void checkValidity(String[] expressions) {
-        if(expressions.length % 2 == 0){
+        if(expressions.length % EXPRESSION_SEPARATOR == 0){
             throw new IllegalArgumentException("유효한 연산식이 아닙니다.");
         }
 
         for (int i = 0; i < expressions.length; i++) {
-
-            if (i % 2 == 0) {
-                if(!isOperand(expressions[i])){
-                    throw new IllegalArgumentException("유효한 피연산자 형식이 아닙니다.");
-                }
-                operandList.add(Integer.parseInt(expressions[i]));
-            }
-
-            if (i % 2 == 1) {
-                if(!isOperator(expressions[i])) {
-                    throw new IllegalArgumentException("유효한 연산자 형식이 아닙니다.");
-                }
-                operatorList.add(expressions[i]);
-
-            }
+            separateExpressions(i,expressions[i]);
         }
     }
 
-    public boolean isOperand(String operand) {
+    public void separateExpressions(int index, String splitExpression){
+
+        if (index % EXPRESSION_SEPARATOR == 0) {
+            isOperand(splitExpression);
+        }
+        if (index % EXPRESSION_SEPARATOR == 1) {
+            isOperator(splitExpression);
+        }
+    }
+
+    public void isOperand(String operand) {
         Matcher matches = OPERAND_REGEX.matcher(operand);
 
-        if (matches.find()) {
-            return true;
+        if (!matches.find()) {
+            throw new IllegalArgumentException("유효한 피연산자 형식이 아닙니다.");
         }
 
-        return false;
+        operandList.add(Integer.parseInt(operand));
     }
 
-    public boolean isOperator(String operator) {
+    public void isOperator(String operator) {
         Matcher matches = OPERATOR_REGEX.matcher(operator);
 
-        if (matches.find()) {
-            return true;
+        if (!matches.find()) {
+            throw new IllegalArgumentException("유효한 연산자 형식이 아닙니다.");
         }
 
-        return false;
+        operatorList.add(operator);
     }
 
     public int calculateExpression() {
--- src/test/java/calculator/service/StringCalculatorTest.java
+++ src/test/java/calculator/service/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,19 +13,21 @@ class StringCalculatorTest {
     private StringCalculator stringCalculator = new StringCalculator();
 
     @ParameterizedTest
-    @ValueSource(strings = {"1", "2", "3", "4", "5"})
-    @DisplayName("피연산자 정규식 테스트")
+    @ValueSource(strings = {"*", "+", "-", "/"})
+    @DisplayName("피연산자 정규식 예외 테스트")
     public void isOperand(String operand) {
-        boolean checkOperand = stringCalculator.isOperand(operand);
-        assertThat(checkOperand).isEqualTo(true);
+        assertThatThrownBy(
+                () ->   stringCalculator.isOperand(operand)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"+", "*", "-", "/"})
-    @DisplayName("연산자 정규식 테스트")
+    @ValueSource(strings = {"^", "@", "!"})
+    @DisplayName("연산자 정규식 예외 테스트")
     public void isOperator(String operator) {
-        boolean checkOperator = stringCalculator.isOperator(operator);
-        assertThat(checkOperator).isEqualTo(true);
+        assertThatThrownBy(
+                () ->   stringCalculator.isOperand(operator)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
