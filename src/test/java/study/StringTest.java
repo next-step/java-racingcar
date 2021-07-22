ff8a0b4bbc1c44903a09198469dc85fb94bfb083
Jade <j2de.dev@gmail.com>
2021-07-23 01:55:27 +0900

    feat: substring test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,10 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    void substring() {
+        String input = "(1,2)";
+        assertThat(input.substring(1, input.length() - 1)).isEqualTo("1,2");
+    }
 }
