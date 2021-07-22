05dccc1707dad37d62582d8299801933f70b5f4a
rladowl92 <rladowl92@naver.com>
2021-07-22 23:58:21 +0900

    test: 테스트 메소드명에서 TEST 제거

--- src/test/java/practice/StringClassTest.java
+++ src/test/java/practice/StringClassTest.java
@@ -10,7 +10,7 @@ class StringClassTest {
 
     @DisplayName("콤마(,)로 분리한 문자가 모두 배열에 저장되어야 한다.")
     @Test
-    void TEST_split_1(){
+    void split_1(){
         String str = "1,2";
         String[] arr = str.split(",");
 
@@ -19,7 +19,7 @@ class StringClassTest {
 
     @DisplayName("분리할 문자가 없어도 한개의 값이 배열에 저장되어야 한다.")
     @Test
-    void TEST_split_2(){
+    void split_2(){
         String str = "1";
         String[] arr = str.split(",");
 
@@ -28,7 +28,7 @@ class StringClassTest {
 
     @DisplayName("주어진 문자열에서 ()을 제거하여 반환해야한다.")
     @Test
-    void TEST_substring(){
+    void substring(){
         String str = "(1,2)";
         String result = str.substring(1,4);
 
@@ -37,7 +37,7 @@ class StringClassTest {
 
     @DisplayName("특정 위치의 문자를 가져와야한다.")
     @Test
-    void TEST_charAt_1(){
+    void charAt_1(){
         String str = "abc";
         char result = str.charAt(0);
 
@@ -46,7 +46,7 @@ class StringClassTest {
 
     @DisplayName("인덱스를 벗어나면 에러가 발생해야한다.")
     @Test
-    void TEST_charAt_2(){
+    void charAt_2(){
         String str = "abc";
 
         assertThatExceptionOfType(IndexOutOfBoundsException.class)
