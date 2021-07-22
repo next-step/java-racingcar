983a8d87fdb0cb80288779866f0f6cfe62e4d135
harris <harrisleesh@gmail.com>
2021-07-22 16:36:41 +0900

    feat : 반복문 + 이항연산 함수로 다항연산 구현

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,6 +1,25 @@
 package stringcalculator;
 
 public class StringCalculator {
+    public static int calculate(String input) {
+        verifyInput(input);
+        String[] split = input.split(" ");
+        int result = Integer.parseInt(split[0]);
+        for (int i = 1; i < split.length; i += 2) {
+            int firstNumber = result;
+            char operator = split[i].charAt(0);
+            int secondNumber = Integer.parseInt(split[i + 1]);
+            result = operate(firstNumber, operator, secondNumber);
+        }
+        return result;
+    }
+
+    private static void verifyInput(String input) {
+        if (input == null)
+            throw new IllegalArgumentException();
+        if (input.isEmpty())
+            throw new IllegalArgumentException();
+    }
 
     private static int operate(int firstNumber, char operator, int secondNumber) {
         if (operator == '-')
