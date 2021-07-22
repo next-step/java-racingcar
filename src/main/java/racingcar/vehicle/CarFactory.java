07e5c0c58f0f1b944a7dec70ac2d599ee14f5ccb
choibyunghyeon <choi8608@gmail.com>
2021-07-23 02:27:27 +0900

    bug: 잘못된 상수 사용

--- src/main/java/racingcar/vehicle/CarFactory.java
+++ src/main/java/racingcar/vehicle/CarFactory.java
@@ -6,10 +6,10 @@ import racingcar.vehicle.engine.Engine;
 public class CarFactory {
 
     private static final Engine DEFAULT_ENGINE = new CarEngine();
-    private static final Distance DEFAULT_DISTANCE = new Distance(0);
+    private static final int DEFAULT_DISTANCE = 0;
 
     public static Car create(String name) {
-        return new Car(DEFAULT_ENGINE, name, DEFAULT_DISTANCE);
+        return new Car(DEFAULT_ENGINE, name, new Distance(DEFAULT_DISTANCE));
     }
 
     public static Car create(String name, int distance) {
