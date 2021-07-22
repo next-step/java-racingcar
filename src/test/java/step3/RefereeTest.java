b44b35ca0d8480a52690e292a3a563b46fc15bef
sk1737030 <sk1737030@naver.com>
2021-07-22 22:15:32 +0900

    test: RefereeTest 추가

---
+++ src/test/java/step3/RefereeTest.java
@@ -0,0 +1,39 @@
+package step3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RefereeTest {
+
+    @Test
+    void 주어진_5초가_지나면_더_이상_움직이면_안된다() {
+        // given
+        int givenTime = 5;
+        Referee referee = new Referee(givenTime);
+
+        // when
+        for (int i = 0; i < 5; i++) {
+            referee.hasPassedTime();
+        }
+
+        // then
+        assertThat(referee.isStopMove()).isTrue();
+    }
+
+    @Test
+    void 주어진_5초가_지나지않았다면_움직일_수_있다() {
+        // given
+        int givenTime = 5;
+        Referee referee = new Referee(givenTime);
+
+        // when
+        for (int i = 0; i < 2; i++) {
+            referee.hasPassedTime();
+        }
+
+        // then
+        assertThat(referee.isStopMove()).isFalse();
+    }
+
+}
