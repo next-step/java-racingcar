e4bef86ad3a1363e45f7e690a813238bb496a2ef
darkant99 <darkant99@naver.com>
2021-07-22 21:28:05 +0900

    [TEST] moveTest 피드백 반영

--- src/test/java/racing/car/CarTest.java
+++ src/test/java/racing/car/CarTest.java
@@ -8,40 +8,24 @@ import racing.car.Car;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
-    /*
-    @DisplayName("Move 테스트")
-    @Test
-    public void moveTest() {
-        int beforeLocationValue = car.getLocation().getValue();
-        for (int moveValue = 0; moveValue < 100; moveValue++) { // moveValue만큼 이동 시킨 후 실제로 Location의 값이 변경 되었는지 테스트
-            car.move(moveValue);
-            assertThat(car.getLocation().getValue() - beforeLocationValue)
-                    .isEqualTo(moveValue);
-            beforeLocationValue = car.getLocation().getValue();
-        }
-    }
-     */
+    private static final int NOT_MOVEMENT_VALUE = 0;
 
     @ValueSource(ints = {
-            100, 1000, 10000, 100000, 10000000
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
     })
-    @DisplayName("Random Move 테스트")
+    @DisplayName("Move 테스트")
     @ParameterizedTest
-    public void randomMoveTest(int limitMoveCount) {
-        int emptyMoveCount = 0;
-
+    public void moveTest(int distance) {
         Car car = new Car();
-        for (int i = 0; i < limitMoveCount; i++) {
-            car.move();
-            if (!car.getLastMoveDistance().hasValue())
-                ++emptyMoveCount;
-        }
-        assertThat(car.getMoveCount())
-                .withFailMessage("지정한 횟수만큼 이동하지 않았습니다.")
-                .isEqualTo(limitMoveCount);
+        Location beforeLocation = car.getLocation();
 
-        assertThat(emptyMoveCount)
-                .withFailMessage("0만큼 이동한 횟수가 없습니다.")
-                .isNotEqualTo(0);
+        car.move(new Location(distance));
+
+        assertThat(
+                car.getLocation()
+                        .min(beforeLocation)
+                        .getValue()
+        ).withFailMessage("지정한 횟수만큼 이동하지 않았습니다.")
+                .isEqualTo(distance < 4 ? NOT_MOVEMENT_VALUE : distance);
     }
 }
