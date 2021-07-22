cdc292baf6489d71cbde836e0c1cac17893302d6
rladowl92 <rladowl92@naver.com>
2021-07-22 22:25:58 +0900

    test: Set Collection에 대한 학습 테스트 - 요구사항 2 추가

--- src/test/java/practice/SetCollectionTest.java
+++ src/test/java/practice/SetCollectionTest.java
@@ -3,6 +3,8 @@ package practice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,12 @@ class SetCollectionTest {
     @Test
     void TEST_size(){
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("set에 1,2,3 값이 포함되어야한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void TEST_contains(int number){
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
