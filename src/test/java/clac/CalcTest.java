f7fe225992a2598f06047ed9e7183c1756eacf1e
mewookim <k011215@gmail.com>
2021-07-23 01:08:52 +0900

    refactor : 테스트클래스 추가

--- src/test/java/clac/CalcTest.java
+++ src/test/java/clac/CalcTest.java
@@ -62,30 +62,6 @@ public class CalcTest {
             calculation.calculate(3, 5, "%");
         }).withMessageMatching("not support operation");
     }
-
-    @Test
-    @DisplayName("Operation type PLUS test")
-    void operationAddTest() {
-        assertThat(Operation.PLUS.calc(3, 5)).isEqualTo(8);
-    }
-
-    @Test
-    @DisplayName("Operation type MINUS test")
-    void operationSubTest() {
-        assertThat(Operation.MINUS.calc(3, 5)).isEqualTo(-2);
-    }
-
-    @Test
-    @DisplayName("Operation type MULTIPLY test")
-    void operationMulTest() {
-        assertThat(Operation.MULTIPLY.calc(2, 5)).isEqualTo(10);
-    }
-
-    @Test
-    @DisplayName("Operation type DIVIDE test")
-    void operationDivTest() {
-        assertThat(Operation.DIVIDE.calc(10, 5)).isEqualTo(2);
-    }
-
+    
 }
 
