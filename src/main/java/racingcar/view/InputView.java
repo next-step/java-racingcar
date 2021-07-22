0133b5e9b312b6c7b41b155204fb32ba8758222b
seondongpyo <seondongpyo@gmail.com>
2021-07-22 09:11:32 +0900

    feat: 사용자로부터 경주 회수를 입력받는다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,9 +6,19 @@ public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String MESSAGE_INPUT_NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
+    private static final String MESSAGE_INPUT_TRY_COUNT = "시도할 회수는 몇 회인가요?";
 
     public static int getNumberOfCars() {
         System.out.println(MESSAGE_INPUT_NUMBER_OF_CARS);
+        return inputNumberFromUser();
+    }
+    
+    public static int getTryCount() {
+        System.out.println(MESSAGE_INPUT_TRY_COUNT);
+        return inputNumberFromUser();
+    }
+
+    private static int inputNumberFromUser() {
         return SCANNER.nextInt();
     }
 
