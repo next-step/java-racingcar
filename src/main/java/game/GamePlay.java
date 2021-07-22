ae4d7734e83cdafb4c4bb3aae0d663a726670656
PIH6585 <pih6585@naver.com>
2021-07-22 16:01:12 +0900

    feat : random method add

--- src/main/java/game/GamePlay.java
+++ src/main/java/game/GamePlay.java
@@ -2,15 +2,18 @@ package game;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GamePlay {
 
     private final List<Player> players;
     private final Validation validation;
+    private final Random random;
 
     public GamePlay() {
         players = new ArrayList<>();
         validation = new Validation();
+        random = new Random();
     }
 
     public List<Player> createPlayer(String playerListValue) {
@@ -35,6 +38,11 @@ public class GamePlay {
 
     public String stringByTrim(String value) {
         return value.trim();
+    }
+
+    public int randomValue() {
+        int RANDOM_VALUE = 10;
+        return random.nextInt(RANDOM_VALUE);
     }
 }
 
