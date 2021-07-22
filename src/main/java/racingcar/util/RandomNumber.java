9a5b86696124e3563f865845099f675adf3f983a
seondongpyo <seondongpyo@gmail.com>
2021-07-22 09:37:26 +0900

    refactor: 임의의 숫자를 생성하는 nextInt() 메서드 인자를 상수로 추출하여 선언

--- src/main/java/racingcar/util/RandomNumber.java
+++ src/main/java/racingcar/util/RandomNumber.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class RandomNumber {
 
     private static final Random RANDOM = new Random();
+    public static final int BOUNDARY = 10;
 
     public static int pick() {
-        return RANDOM.nextInt(10);
+        return RANDOM.nextInt(BOUNDARY);
     }
 
 }
