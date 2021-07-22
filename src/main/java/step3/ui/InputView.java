8565233ffed329c7b3c644922255a05350c5507a
LAPTOP-I4NE0UIJ\yongs <smilesigma@gmail.com>
2021-07-23 07:22:32 +0900

    feat (ui): 자동차 질주 시도 회수를 묻는 UI 로직 추가.

--- src/main/java/step3/ui/InputView.java
+++ src/main/java/step3/ui/InputView.java
@@ -3,6 +3,10 @@ package step3.ui;
 public class InputView {
 
     public void askHowManyCars(){
-        System.out.print("자동차 대수는 몇 대 인가요? ");
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    public void askHowManyTryCounts(){
+        System.out.println("시도할 회수는 몇 회 인가요?");
     }
 }
--- src/test/java/step3/ui/InputViewTest.java
+++ src/test/java/step3/ui/InputViewTest.java
@@ -1,12 +1,12 @@
 package step3.ui;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
@@ -25,11 +25,21 @@ class InputViewTest {
     }
 
     @Test
+    @DisplayName("게임에 참여할 자동차 대수를 묻는 UI 텍스트 검증하기")
     void askHowManyCars() {
         InputView inputView = new InputView();
         inputView.askHowManyCars();
 
-        assertThat(outputStream.toString()).isEqualTo("자동차 대수는 몇 대 인가요? ");
+        assertThat(outputStream.toString().trim()).isEqualTo("자동차 대수는 몇 대 인가요?");
+    }
+
+    @Test
+    @DisplayName("게임의 시도회수를 묻는 UI 텍스트 검증하기")
+    void askHowManyTryCounts() {
+        InputView inputView = new InputView();
+        inputView.askHowManyTryCounts();
+
+        assertThat(outputStream.toString().trim()).isEqualTo("시도할 회수는 몇 회 인가요?");
     }
 
 
