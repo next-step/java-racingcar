5c857b86bf14edff31ede8c692158e163f769667
PIH6585 <pih6585@naver.com>
2021-07-22 16:23:35 +0900

    refactor : utility성 method 분리

--- src/main/java/game/Validation.java
+++ src/main/java/game/Validation.java
@@ -2,6 +2,12 @@ package game;
 
 public class Validation {
 
+    private Utils utils;
+
+    public Validation(Utils utils) {
+        this.utils = utils;
+    }
+
     public void validStringEmpty(String value) {
         if (value.isEmpty()) {
             throw new IllegalArgumentException("자동차는 최소 1대 이상이어야 한다.");
@@ -9,12 +15,10 @@ public class Validation {
     }
 
     public void validStringLength(String value, int checkLength) {
-        if (StringLength(value) > checkLength) {
+        if (utils.stringLength(value) > checkLength) {
             throw new IllegalArgumentException("자동차 이름은 5글자를 초과할수 없습니다.");
         }
     }
 
-    public int StringLength(String value) {
-        return value.length();
-    }
+
 }
