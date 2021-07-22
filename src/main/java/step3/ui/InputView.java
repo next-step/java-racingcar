dfc9905f48a6b9e37c398afd59e8203d947da47c
LAPTOP-I4NE0UIJ\yongs <smilesigma@gmail.com>
2021-07-23 07:03:14 +0900

    feat (ui): 몇대의 차를 쓸건지 물어보기

--- src/main/java/step3/ui/InputView.java
+++ src/main/java/step3/ui/InputView.java
@@ -2,4 +2,7 @@ package step3.ui;
 
 public class InputView {
 
+    public void askHowManyCars(){
+        System.out.print("자동차 대수는 몇 대 인가요? ");
+    }
 }
--- src/test/java/step3/ui/InputViewTest.java
+++ src/test/java/step3/ui/InputViewTest.java
@@ -1,7 +1,36 @@
 package step3.ui;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class InputViewTest {
+
+    private final PrintStream standOut = System.out;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standOut);
+    }
+
+    @Test
+    void askHowManyCars() {
+        InputView inputView = new InputView();
+        inputView.askHowManyCars();
+
+        assertThat(outputStream.toString()).isEqualTo("자동차 대수는 몇 대 인가요? ");
+    }
+
 
 }
