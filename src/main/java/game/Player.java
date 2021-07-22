f279b674a796d9e8469f40d35d1ccd367a5cc684
PIH6585 <pih6585@naver.com>
2021-07-22 16:12:01 +0900

    feat : 입력숫자에 따른 주행거리 이동 추가

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -3,8 +3,13 @@ package game;
 public class Player {
 
     private String playerName;
+    private int raceDistance;
 
     public Player(String playerName) {
         this.playerName = playerName;
+    }
+
+    public int playToMove(int value) {
+        return value > 3 ? ++raceDistance : raceDistance;
     }
 }
