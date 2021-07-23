ccf716acadc0cc76de170bad96d7f3e3d2dc41da
darkant99 <darkant99@naver.com>
2021-07-24 00:53:15 +0900

    [DOCS] 요구사항 작업 사항 반영

--- src/main/java/racing/car/RandomFuel.java
+++ src/main/java/racing/car/RandomFuel.java
@@ -3,6 +3,7 @@ package racing.car;
 import util.RandomUtils;
 
 public class RandomFuel extends Fuel {
+    @Override
     public boolean isMovable(int requireFuelValue) {
         return RandomUtils.nextInt(Fuel.FULL.value) >= requireFuelValue;
     }
