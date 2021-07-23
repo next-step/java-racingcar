153b004f52937159bdebddd62c7c2c416f4623fa
longbeom <wowow1859@naver.com>
2021-07-23 14:42:31 +0900

    fix : @DisplayName 추가

--- src/test/java/step2/ValidatorTest.java
+++ src/test/java/step2/ValidatorTest.java
@@ -3,16 +3,19 @@ package step2;
 import static org.assertj.core.api.Assertions.*;
 import static step2.Validator.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ValidatorTest {
+	@DisplayName(value = "빈 문자열 테스트")
 	@Test
-	void validateExpressionTest() {
-		assertThatThrownBy(() -> validateExpression(" "))
+	void validateEmptyTest() {
+		assertThatThrownBy(() -> validateEmpty(" "))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining("입력 값이 비어있어요!");
 	}
 
+	@DisplayName(value = "연산 기호 검증 테스트")
 	@Test
 	void validateOperatorTest() {
 		assertThatThrownBy(() -> validateOperator("&"))
