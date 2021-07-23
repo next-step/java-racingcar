26bdfdfb7151e3e52a059f2f0cf1950ec6551803
jspark940321 <jspark940321@gmail.com>
2021-07-23 17:03:36 +0900

    feat: 더하기 구현
    다중 더하기등이 가능하게 구현

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,9 +1,27 @@
+
 public class StringCalculator {
     public int calculate(String s) {
         if(s == null) throw new IllegalArgumentException("null값을 입력받았습니다.");
         String[] splitedString = s.split(" ");
         if(splitedString.length == 0) throw new IllegalArgumentException("빈값을 입력받았습니다.");
-        if(splitedString.length == 1) return Integer.parseInt(s);
-        return Integer.parseInt(splitedString[0]) + Integer.parseInt(splitedString[2]);
+        int output = Integer.parseInt(splitedString[0]);
+        if(splitedString.length == 1) return output;
+        int size = splitedString.length;
+        for(int i=1;i<size;i+=2){
+            if(i+1 == size) throw new IllegalArgumentException("숫자가 부족합니다.");
+            int rightNum = 0;
+            try {
+                rightNum = Integer.parseInt(splitedString[i+1]);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자가 아닌 수를 입력받았습니다.");
+            }
+            if(splitedString[i].equals("+")) {
+                output += rightNum;
+            }
+        }
+
+        return output;
     }
+
+
 }
