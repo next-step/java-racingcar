1d41e203106d362e78ab7dc404b64dec7b36a723
권혜주 <2k3m54@gmail.com>
2021-07-24 01:11:23 +0900

    test: setTest complete

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,18 @@
 package study;
 
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.assertj.core.internal.Strings;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -18,6 +27,29 @@ public class SetTest {
     }
 
     // Test Case 구현
+    @Test
+    void checkSize(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
 
+    @Test
+    void checkContains(){
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void checkContainsParameterizedTestUsingValueSource(int number){
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void checkContainsParameterizedTestUsingCsvSource(int input, boolean expected){
+        boolean result = numbers.contains(input);
+        assertThat(result).isEqualTo(expected);
+    }
 
 }
