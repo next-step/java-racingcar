5628877adda45cb489c6a10c1107fbf9a6e5b298
shirohoo <hch4102@naver.com>
2021-07-23 13:33:16 +0900

    refactor: 테스트 초기화를 조금 더 진행

--- src/test/java/racing/RacersTest.java
+++ src/test/java/racing/RacersTest.java
@@ -12,33 +12,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RacersTest {
     private final ForwardConditionStrategy mockStrategy = BDDMockito.mock(NumberMoreThanFourConditionStrategy.class);
 
-    private ArrayList<String> list;
+    private Racers racers;
 
     @BeforeEach
     void setUp() {
-        list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
         list.add("-");
         list.add("-");
         list.add("-");
+        racers = Racers.from(list);
     }
 
     @Test
     @DisplayName("정적_팩토리가_인스턴스를_생성하는지_확인한다")
     void from() throws Exception {
-        assertThat(Racers.from(list)).isInstanceOf(Racers.class);
+        assertThat(racers).isInstanceOf(Racers.class);
     }
 
     @Test
     @DisplayName("조회_테스트")
     void get() throws Exception {
-        assertThat(Racers.from(list).get(0)).isEqualTo("-");
+        assertThat(racers.get(0)).isEqualTo("-");
     }
 
     @Test
     @DisplayName("전진_테스트_조건은_참")
     void goForward() throws Exception {
         BDDMockito.given(mockStrategy.judgeCondition()).willReturn(false);
-        Racers racers = Racers.from(list);
         racers.turnAround(mockStrategy, new StringBuilder());
         assertThat(racers.get(0)).isEqualTo("--");
     }
@@ -47,7 +47,6 @@ public class RacersTest {
     @DisplayName("전진_테스트_조건은_거짓")
     void goForwardNot() throws Exception {
         BDDMockito.when(mockStrategy.judgeCondition()).thenReturn(true);
-        Racers racers = Racers.from(list);
         racers.turnAround(mockStrategy, new StringBuilder());
         assertThat(racers.get(0)).isEqualTo("-");
     }
@@ -55,6 +54,6 @@ public class RacersTest {
     @Test
     @DisplayName("사이즈_조회_테스트")
     void size() throws Exception {
-        assertThat(Racers.from(list).size()).isEqualTo(3);
+        assertThat(racers.size()).isEqualTo(3);
     }
 }
