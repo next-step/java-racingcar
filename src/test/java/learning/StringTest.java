8e60acdf1cf68ac83502fbeb5fd5f89a606bf0b5
sangw0804 <sangw0804@likelion.org>
2021-07-23 23:30:00 +0900

    feat(StringTest): 요구사항 2
    - "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록
    구현한다.

--- src/test/java/learning/StringTest.java
+++ src/test/java/learning/StringTest.java
@@ -9,6 +9,7 @@ public class StringTest {
     @Test
     public void split_with_comma() {
         String rawString = "1,2";
+
         String[] results = rawString.split(",");
 
         assertThat(results).contains("1", "2");
@@ -17,8 +18,18 @@ public class StringTest {
     @Test
     public void split_without_comma() {
         String rawString = "1";
+
         String[] results = rawString.split(",");
 
         assertThat(results).containsExactly("1");
+    }
+
+    @Test
+    public void uncover_bracket() {
+        String rawString = "(1,2)";
+
+        String result = rawString.substring(1, rawString.length() - 1);
+
+        assertThat(result).isEqualTo("1,2");
     }
 }
