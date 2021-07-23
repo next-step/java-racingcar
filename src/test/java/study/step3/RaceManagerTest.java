6b7cfbd1a42d5067ab62fa6f8f1947093b2dc221
iiaii <jhun9409@naver.com>
2021-07-23 23:53:30 +0900

    [step3] test: RaceManager(경주 관리자) carNumber, roundNumber 는 모두 int 값으로 받는 테스트로 제거 및 수정

--- src/test/java/study/step3/RaceManagerTest.java
+++ src/test/java/study/step3/RaceManagerTest.java
@@ -8,46 +8,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RaceManagerTest {
 
-    RaceManager raceManager = new RaceManager();
-
-    @ParameterizedTest(name = "입력값이 null 인 경우 | {arguments}")
-    @CsvSource(value = {":3", "3:", ":"}, delimiter = ':')
-    public void nullInput(String carNumber, String roundNumber) {
-        // given
-        String message = RaceManager.NULL_INPUT;
-
-        // when
-        ThrowingCallable throwingCallable = () -> raceManager.register(carNumber, roundNumber);
-
-        // then
-        assertThatThrownBy(throwingCallable)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(message);
-    }
-
-    @ParameterizedTest(name = "입력값이 숫자가 아닌 경우 | {arguments}")
-    @CsvSource(value = {"2:*:\"*\"", "%:12:\"%\"", "42:1,000:\"1,000\""}, delimiter = ':')
-    public void notNumber(String carNumber, String roundNumber, String notNumber) {
-        // given
-        String message = "For input string: " + notNumber;
-
-        // when
-        ThrowingCallable throwingCallable = () -> raceManager.register(carNumber, roundNumber);
-
-        // then
-        assertThatThrownBy(throwingCallable)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(message);
-    }
+    CarManager carManager = new CarManager();
+    DriverRecruiter driverRecruiter = new DriverRecruiter();
+    RoundManager roundManager = new RoundManager();
+    RaceRecorder raceRecorder = new RaceRecorder();
+    RaceManager raceManager = new RaceManager(carManager, driverRecruiter, roundManager, raceRecorder);
 
     @ParameterizedTest(name = "입력값이 숫자가 0 보다 같거나 작은 경우 | {arguments}")
-    @CsvSource(value = {"-1:3", "0:3", "2:-3"}, delimiter = ':')
-    public void lteZero(String carNumber, String roundNumber) {
+    @CsvSource(value = {"-1:3:-1", "0:3:0", "2:-3:-3"}, delimiter = ':')
+    public void lteZero(int carNumber, int roundNumber, int illegalNumber) {
         // given
-        String message = RaceManager.LESS_THAN_OR_EQUAL_ZERO;
+        String message = RaceManager.LESS_THAN_OR_EQUAL_ZERO + illegalNumber;
 
         // when
-        ThrowingCallable throwingCallable = () -> raceManager.register(carNumber, roundNumber);
+        ThrowingCallable throwingCallable = () -> raceManager.manageRace(carNumber, roundNumber);
 
         // then
         assertThatThrownBy(throwingCallable)
