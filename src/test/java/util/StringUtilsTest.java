55d18668eaa032630964622623a1f59ce14d6e4d
darkant99 <darkant99@naver.com>
2021-07-24 00:05:23 +0900

    [TEST] StringUtils 테스트 코드 추가

---
+++ src/test/java/util/StringUtilsTest.java
@@ -0,0 +1,69 @@
+package util;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringUtilsTest {
+    @ValueSource(strings = {
+            "123", "121", "1421421421421412412412412", "1"
+    })
+    @ParameterizedTest
+    public void isNumber_Success(String str) {
+        assertThat(
+                StringUtils.isNumber(str)
+        ).isTrue();
+    }
+
+    @ValueSource(strings = {
+            "", "1q1", " 123", "123 ", "1 23"
+    })
+    @ParameterizedTest
+    public void isNumber_Fail(String str) {
+        assertThat(
+                StringUtils.isNumber(str)
+        ).isFalse();
+    }
+
+    @ValueSource(strings = {
+            "", " ", "  ", "", "          "
+    })
+    @ParameterizedTest
+    public void isEmpty_Success(String str) {
+        assertThat(
+                StringUtils.isEmpty(str)
+        ).isTrue();
+    }
+
+    @ValueSource(strings = {
+            "!", "1", " ~"
+    })
+    @ParameterizedTest
+    public void isEmpty_Fail(String str) {
+        assertThat(
+                StringUtils.isEmpty(str)
+        ).isFalse();
+    }
+
+    @ValueSource(strings = {
+            "A", "ABD", "ERG", "arw", "aaabsasAAW"
+    })
+    @ParameterizedTest
+    public void isEng_Success(String str) {
+        assertThat(
+                StringUtils.isEng(str)
+        ).isTrue();
+    }
+
+    @ValueSource(strings = {
+            " A", "A ", "A A", "a A", "jaewon1"
+    })
+    @ParameterizedTest
+    public void isEng_Fail(String str) {
+        assertThat(
+                StringUtils.isEng(str)
+        ).isFalse();
+    }
+}
