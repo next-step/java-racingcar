efc2655911ae6fb261e7e5fdc4232e33fe5962e0
sk1737030 <sk1737030@naver.com>
2021-07-23 23:50:26 +0900

    test: 테스트메소드 명 변경

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -7,19 +7,19 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 class CarTest {
 
     @Test
-    void 주어진_숫자가_4이상이면_자동차는_전진한다() {
-        int randomNumbers = 4;
+    void 주어진_숫자가_특정RPM이상이면_자동차의_moveCount는_증가한다() {
+        int givenRpm = 4;
         Car car = new Car(new CarEngine());
-        car.move(randomNumbers);
+        car.move(givenRpm);
 
         assertThat(car.getMovedCount()).isEqualTo(1);
     }
 
     @Test
-    void 주어진_숫자가_3이하이면_자동차는_할_수_없다() {
-        int randomNumbers = 3;
+    void 주어진_숫자가_특정RPM_이하이면_자동차의_moveCount는_변동이_없다() {
+        int givenRpm = 3;
         Car car = new Car(new CarEngine());
-        car.move(randomNumbers);
+        car.move(givenRpm);
 
         assertThat(car.getMovedCount()).isEqualTo(0);
     }
