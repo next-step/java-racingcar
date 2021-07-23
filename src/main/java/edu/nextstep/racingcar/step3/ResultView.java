25a1574151c362769beab238e61881f8be8421df
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-07-23 16:52:00 +0900

    feat: 자동차 이동 View 추가

--- src/main/java/edu/nextstep/racingcar/step3/ResultView.java
+++ src/main/java/edu/nextstep/racingcar/step3/ResultView.java
@@ -1,4 +1,41 @@
 package edu.nextstep.racingcar.step3;
 
+import edu.nextstep.racingcar.step3.app.Vehicle;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class ResultView {
+
+    private final List<Vehicle> vehicles;
+    private final Integer numberOfAttempts;
+
+    public ResultView(List<Vehicle> vehicles, int numberOfAttempts) {
+        this.vehicles = vehicles;
+        this.numberOfAttempts = numberOfAttempts;
+    }
+
+    public void play() {
+        IntStream.range(0, numberOfAttempts).forEach(idx -> attempt());
+    }
+
+    private void attempt() {
+        vehicles.forEach(vehicle -> {
+            calculateDistance(vehicle);
+            printDistance(vehicle);
+        });
+
+        System.out.print("\n");
+    }
+
+    private void printDistance(Vehicle vehicle) {
+        IntStream.range(0, vehicle.getDistance()).forEach(distanceIdx -> System.out.print("-"));
+        System.out.print("\n");
+    }
+
+    private void calculateDistance(Vehicle vehicle) {
+        if (vehicle.isMove()) {
+            vehicle.setDistance(vehicle.getDistance() + 1);
+        }
+    }
 }
