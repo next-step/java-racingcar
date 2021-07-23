d99d6db2eb028ee95c22935366a1e751f5d99a15
rladowl92 <rladowl92@naver.com>
2021-07-23 22:36:37 +0900

    feat(calc): 문자열로 입력받도록 수정

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,19 +1,42 @@
 package calculator;
 
 public class StringCalculator {
-    public static int add(int a, int b) {
+    public static int calc(String str){
+        String[] arr = str.split(" ");
+        int num1 = Integer.parseInt(arr[0]);
+        String operator = arr[1];
+        int num2 = Integer.parseInt(arr[2]);
+        int result = 0;
+
+        switch (operator){
+            case "+":
+                result = add(num1,num2);
+                break;
+            case "-":
+                result = sub(num1,num2);
+                break;
+            case "*":
+                result = mul(num1,num2);
+                break;
+            case "/":
+                result = div(num1,num2);
+        }
+        return result;
+    }
+
+    private static int add(int a, int b) {
         return a + b;
     }
 
-    public static int sub(int a, int b) {
+    private static int sub(int a, int b) {
         return a - b;
     }
 
-    public static int mul(int a, int b) {
+    private static int mul(int a, int b) {
         return a * b;
     }
 
-    public static int div(int a, int b) {
+    private static int div(int a, int b) {
         return a / b;
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -8,25 +8,25 @@ class StringCalculatorTest {
 
     @Test
     void add(){
-        int result = StringCalculator.add(1,2);
+        int result = StringCalculator.calc("1 + 2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     void sub(){
-        int result = StringCalculator.sub(3,1);
+        int result = StringCalculator.calc("3 - 1");
         assertThat(result).isEqualTo(2);
     }
 
     @Test
     void mul(){
-        int result = StringCalculator.mul(3,2);
+        int result = StringCalculator.calc("3 * 2");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     void div(){
-        int result = StringCalculator.div(6,2);
+        int result = StringCalculator.calc("6 / 2");
         assertThat(result).isEqualTo(3);
     }
 }
