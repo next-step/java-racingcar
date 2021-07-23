116b3cc24a74f2961237862ec3bebf23e3e965df
shirohoo <hch4102@naver.com>
2021-07-23 13:32:52 +0900

    refactor: 관심사를 분리한다

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -9,6 +9,9 @@ class RacingGame {
     private static final String END_MESSAGE_FORMAT = "실행 결과";
     private static final String NEW_LINE = System.lineSeparator();
     private static final String BLANK = "";
+    
+    private static final int LIMIT_CARS = 1;
+    private static final int LIMIT_TRY = 1;
 
     private final ForwardConditionStrategy strategy;
     private final Racers racers;
@@ -16,6 +19,14 @@ class RacingGame {
     private int stageCount;
 
     private RacingGame(ForwardConditionStrategy strategy, int stageCount, int carCount) {
+        if (carCount <= LIMIT_CARS) {
+            throw new IllegalArgumentException("자동차는 2대 이상이어야 합니다");
+        }
+
+        if (stageCount <= LIMIT_TRY) {
+            throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다");
+        }
+
         this.strategy = strategy;
         this.stageCount = stageCount;
         this.racers = Racers.from(range(0, carCount)
--- src/main/java/racing/RacingGameScanner.java
+++ src/main/java/racing/RacingGameScanner.java
@@ -8,18 +8,10 @@ class RacingGameScanner {
         System.out.println("자동차 대수는 몇 대인가요?");
         int carCount = sc.nextInt();
 
-        if (carCount <= 1) {
-            throw new IllegalArgumentException("자동차는 2대 이상이어야 합니다");
-        }
-
         System.out.println("시도할 횟수는 몇 회인가요?");
         int stageCount = sc.nextInt();
 
         System.out.println();
-
-        if (stageCount <= 0) {
-            throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다");
-        }
 
         sc.close();
         return new int[]{stageCount, carCount};
