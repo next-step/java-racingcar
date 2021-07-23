2577ebfb223558269c41c85bfa9aa7794c495f8f
darkant99 <darkant99@naver.com>
2021-07-23 23:47:03 +0900

    [TEST] Fuel 테스트 추가

--- src/test/java/racing/car/FuelTest.java
+++ src/test/java/racing/car/FuelTest.java
@@ -1,10 +1,31 @@
 package racing.car;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class FuelTest {
+    @ValueSource(ints = {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    })
+    @ParameterizedTest
+    public void newTest(int fuelValue) {
+        Fuel.newInstance(fuelValue);
+    }
+
+    @ValueSource(ints = {
+            -1, -99, 10, 11, 15
+    })
+    @ParameterizedTest
+    public void newIllegalArgumentExceptionTest(int fuelValue) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() ->
+                    Fuel.newInstance(fuelValue));
+    }
+
     @Test
     public void emptyTest() {
         assertThat(Fuel.EMPTY.isMovable(Car.REQUIRED_FUEL_VALUE))
