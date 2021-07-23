63a68d66cd30c7893c72cf6833314e6ebe85fefa
shineoov <50814622+shineoov@users.noreply.github.com>
2021-07-23 12:35:10 +0900

    Step2 - 문자열 계산기 (#2384)
    * 사용하지않는 empty.txt 파일 삭제
    
    * 산술연산 클래스
    
    + 테스트작성
    
    * 산술연산 - Lambda 식 적용
    
    + 0 으로 나누었을 때 에외처리
    
    * 산술계산 객체 가져올 때 없을경우 null 리턴
    
    * 문자열 사칙 연산 계산기 구현
    
    * 문자열 사칙 연산 계산기 구현
    
    * Operator  테스트 코드 변경
    
    - Operator 가져올 때 exception 이 아닌 null 리턴으로 변경되어 테스트 코드도 변경
    
    * Operator Test 파일 사용하지 않는 import 제거

---
+++ src/main/java/calculator/Operator.java
@@ -0,0 +1,42 @@
+package calculator;
+
+import java.util.function.BiFunction;
+
+public enum Operator {
+
+    PLUS("+", (num1, num2) -> num1 + num2),
+    MINUS("-", (num1, num2) -> num1 - num2),
+    MULTIPLY("*", (num1, num2) -> num1 * num2),
+    DIVIDE("/", (num1, num2) -> {
+        if (num1 == 0 || num2 == 0) {
+            return 0;
+        }
+        return num1 / num2;
+    });
+
+    private final String symbol;
+    private final BiFunction<Integer, Integer, Integer> bf;
+
+    Operator(String symbol, BiFunction<Integer, Integer, Integer> bf) {
+        this.symbol = symbol;
+        this.bf = bf;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int exec(int num1, int num2) {
+        return bf.apply(num1, num2);
+    }
+
+    public static Operator of(String symbol) {
+        Operator[] operators = Operator.values();
+        for (Operator operator : operators) {
+            if(operator.getSymbol().equals(symbol)){
+                return operator;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,87 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("문자열 사칙 연산 계산기 테스트")
+class StringCalculatorTest {
+
+    StringCalculator stringCalculator = new StringCalculator();
+
+    @DisplayName("덧셈 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"5 + 10 = 15", "10 + -5 = 5", "1 + 3 + 6 = 10", "10 + 100 + 500 = 610"}, delimiter = '=')
+    void calculatorPlusTest(String input, int expected) {
+        int actual = stringCalculator.arithmeticOperation(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("뺼셈 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"10 - 3 = 7", "10 - -5 = 15", "10 - 3 - 4 = 3", "1000 - 500 - 499 = 1"}, delimiter = '=')
+    void calculatorMinusTest(String input, int expected) {
+        int actual = stringCalculator.arithmeticOperation(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("곱셈 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"10 * 3 = 30", "6 * -6 = -36", "10 * 3 * 4 = 120", "1000 * 10 * 10 = 100000"}, delimiter = '=')
+    void calculatorMultiplyTest(String input, int expected) {
+        int actual = stringCalculator.arithmeticOperation(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("나눗셈 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"10 / 2 = 5", "39 / 3 = 13", "1000 / 10 / 25 = 4", "1000 / 10 / 10 = 10"}, delimiter = '=')
+    void calculatorDivideTest(String input, int expected) {
+        int actual = stringCalculator.arithmeticOperation(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("입력 값이 null 이거나 빈 공백 문자일 때 IllegalArgumentException 발생 ")
+    void calculatorEmptyExceptionTest() {
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            stringCalculator.arithmeticOperation(null);
+        });
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            stringCalculator.arithmeticOperation("");
+        });
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            stringCalculator.arithmeticOperation("  ");
+        });
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            stringCalculator.arithmeticOperation(" ");
+        });
+    }
+
+    @DisplayName("사칙연산 기호가 아닌경우 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"20 + +", "10 + *", "+ 10", "ADD 10", "10 X 20", "10", "+"})
+    void calculatorExceptionTest(String param) {
+        assertThatThrownBy(() -> stringCalculator.arithmeticOperation(param))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("invalid value");
+    }
+
+    @DisplayName("사칙 연산 모두 포함하는 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"10 + 5 - 3 * 2 / 2 = 12", "20 * 6 / 6 - 10 = 10", "10 + 10 / 5 - 7 * 2  = -6"}, delimiter = '=')
+    void calculatorCombineTest(String input, int expected) {
+        int actual = stringCalculator.arithmeticOperation(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
