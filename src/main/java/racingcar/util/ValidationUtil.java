d0ece86e5261c8558a50162285abf9d1aee901a7
hjjangdev <ju0398@hanmail.net>
2021-07-23 13:27:18 +0900

    feat : 유효성 체크 util 추가

---
+++ src/main/java/racingcar/util/ValidationUtil.java
@@ -0,0 +1,34 @@
+package racingcar.util;
+
+import racingcar.exception.InvalidUserInputException;
+
+public class ValidationUtil {
+
+    private final static String INPUT_STRING_MESSAGE = "숫자만 입력 가능합니다.";
+    private final static String INPUT_ZERO_MESSAGE = "0은 입력이 불가합니다.";
+    private final static String INPUT_CAR_NUMBER_MESSAGE = "2개 이상의 자동차를 입력해야 시작 가능합니다.";
+
+    public static int checkUserInput(String input) {
+        int inputValue;
+
+        try {
+            inputValue = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(INPUT_STRING_MESSAGE);
+        }
+
+        if (inputValue == 0) {
+            throw new InvalidUserInputException(INPUT_ZERO_MESSAGE);
+        }
+
+        return inputValue;
+    }
+
+    public static void checkNumberOfCar(int number) {
+        if (number <= 1) {
+            throw new InvalidUserInputException(INPUT_CAR_NUMBER_MESSAGE);
+        }
+    }
+
+
+}
