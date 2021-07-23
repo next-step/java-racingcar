c53e5ed14ebdd31b9de5594bad1713c62b4ffc7c
darkant99 <darkant99@naver.com>
2021-07-24 02:11:32 +0900

    [REFACTOR] Fuel 유효성 검사 Lazy

--- src/main/java/racing/car/Fuel.java
+++ src/main/java/racing/car/Fuel.java
@@ -1,32 +1,21 @@
 package racing.car;
 
-import util.RandomUtils;
-
 public class Fuel {
-    public static Fuel EMPTY = new Fuel(0, true);
-    public static Fuel FULL = new Fuel(9, true);
+    public static Fuel EMPTY = new Fuel(0);
+    public static Fuel FULL = new Fuel(9);
 
     private static final String OVER_FULL_MESSAGE = "최대 충전 가능한 연료를 넘었습니다.";
     private static final String UNDER_EMPTY_MESSAGE = "연료는 음수르 충전할 수 없습니다.";
 
     protected final int value;
 
-    private Fuel(int value, boolean skipValidate) {
-        // init 문제를 막기 위한 생성자
-        if (!skipValidate)
-            validate(value);
-        this.value = value;
-    }
-
     public Fuel(int value) {
-        validate(value);
         this.value = value;
     }
 
     protected Fuel() {
         this.value = EMPTY.value;
     }
-
 
     private static void validate(int value) {
         if (value < 0)
@@ -36,6 +25,7 @@ public class Fuel {
     }
 
     public boolean isMovable(int requireFuelValue) {
+        validate(value); // 유효성 검사 Lazy
         return this.value >= requireFuelValue;
     }
 }
