d5d3907ac4723abb4a76731ad719a48e168f990e
PIH6585 <pih6585@naver.com>
2021-07-23 11:04:10 +0900

    refactor : method name 변경

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -12,7 +12,7 @@ class CarTest {
     @ParameterizedTest
     @DisplayName("입력받은 숫자가 4이상이면 증가하고 그렇지 않으면 정지한다.")
     @CsvSource(value = {"4,1","3,0","7,1"})
-    public void playerMove(int randomValue, int moveValue) {
+    public void carRacingCondition_check(int randomValue, int moveValue) {
         Car car = new Car("car");
         int result = car.carRacing(randomValue);
 
@@ -22,7 +22,7 @@ class CarTest {
     @ParameterizedTest
     @DisplayName("이름을 입력하면 해당 이름이 반환된다.")
     @CsvSource(value = {"car1","car2","car3"})
-    public void getPlayerName(String value){
+    public void returnCarName(String value){
         Car car = new Car(value);
         String playerName = car.getCarName();
 
