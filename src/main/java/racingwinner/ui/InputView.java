b0fcdf94d571876f1ca3b2f07440a9b8c56ebde5
YONGJU <odetsuo@gmail.com>
2021-07-23 13:58:54 +0900

    fix : 메세지 상수 접근제어자 변경

--- src/main/java/racingwinner/ui/InputView.java
+++ src/main/java/racingwinner/ui/InputView.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 
 public class InputView {
 
-
-	public static final String WRONG_NAME_LENGTH_MESSAGE = "자동차 이름은 5자를 초과할 수 없습니다. 다시 입력 해주세요.";
-	public static final String NAME_SEPARATOR = ",";
+	private static final String WRONG_NAME_LENGTH_MESSAGE = "자동차 이름은 5자를 초과할 수 없습니다. 다시 입력 해주세요.";
+	private static final String NAME_SEPARATOR = ",";
 	private static final String QUESTION_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String QUESTION_REPEAT_COUNT = "시도할 회수는 몇 회 인가요?";
 	private String[] carNames;
