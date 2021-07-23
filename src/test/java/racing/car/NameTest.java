2909f74458c527c3d28036f3f82a7f79be06ad6d
darkant99 <darkant99@naver.com>
2021-07-24 00:08:32 +0900

    [TEST] Name Test

---
+++ src/test/java/racing/car/NameTest.java
@@ -0,0 +1,26 @@
+package racing.car;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class NameTest {
+    @ValueSource(strings = {
+            "A", "B", "abc", "jaewon"
+    })
+    @ParameterizedTest
+    public void ctorTest(String strName) {
+        new Name(strName);
+    }
+
+    @ValueSource(strings = {
+            "A1", "B2", "a a", "스포츠카"
+    })
+    @ParameterizedTest
+    public void ctorIllegalArgumentExceptionTest(String strName) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() ->
+                        new Name(strName));
+    }
+}
