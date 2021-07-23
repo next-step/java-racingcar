aef5a9a376bd65dfc7b2353daf55ffd136acd0b5
darkant99 <darkant99@naver.com>
2021-07-23 23:07:08 +0900

    [REFACTOR] Fuel 생성자에서 유효성 검사

--- src/main/java/racing/car/Fuel.java
+++ src/main/java/racing/car/Fuel.java
@@ -8,10 +8,21 @@ public class Fuel {
     public static Fuel EMPTY = new Fuel(0);
     public static Fuel FULL = new Fuel(9);
 
+    private static final String OVER_FULL_MESSAGE = "최대 충전 가능한 연료를 넘었습니다.";
+    private static final String UNDER_EMPTY_MESSAGE = "연료는 음수르 충전할 수 없습니다.";
+
     private int value;
 
     private Fuel(int value) {
+        validate(value);
         this.value = value;
+    }
+
+    private void validate(int value) {
+        if (value < 0)
+            throw new IllegalArgumentException(UNDER_EMPTY_MESSAGE);
+        if (value > FULL.value)
+            throw new IllegalArgumentException(OVER_FULL_MESSAGE);
     }
 
     public static Fuel newInstance(int value) {
