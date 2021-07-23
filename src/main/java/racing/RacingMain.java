b51424ad4305158011b6c958542074b69ce56163
0n1dev <all_mind@naver.com>
2021-07-23 21:31:01 +0900

    refactor : mvc 패턴 고려한 InputView 리팩토링
    - InputView에서 받아오는 매개변수 제거

--- src/main/java/racing/RacingMain.java
+++ src/main/java/racing/RacingMain.java
@@ -5,9 +5,8 @@ import racing.view.InputView;
 public class RacingMain {
 
 	public static void main(String[] args) {
-		int carCount = InputView.inputCarCount();
-		int tryCount = InputView.inputTryCount();
-
-		InputView.startRacing(carCount, tryCount);
+		InputView.inputCarCount();
+		InputView.inputTryCount();
+		InputView.startRacing();
 	}
 }
--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -9,23 +9,26 @@ public class InputView {
 	private static final Scanner scanner = new Scanner(System.in);
 	private static final RacingController racingController = new RacingController();
 
+	private static int carCount;
+	private static int tryCount;
+
 	private InputView() {
 
 	}
 
-	public static int inputCarCount() {
+	public static void inputCarCount() {
 		System.out.println("자동차 대수는 몇 대 인가요?");
 
-		return scanner.nextInt();
+		carCount = scanner.nextInt();
 	}
 
-	public static int inputTryCount() {
+	public static void inputTryCount() {
 		System.out.println("시도할 회수는 몇 회 인가요?");
 
-		return scanner.nextInt();
+		tryCount = scanner.nextInt();
 	}
 
-	public static void startRacing(int carCount, int tryCount) {
+	public static void startRacing() {
 		racingController.start(carCount, tryCount);
 	}
 }
