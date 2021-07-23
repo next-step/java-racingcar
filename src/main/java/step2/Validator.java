b663f4c35de8aef795fc1423a15be16c1cfaca06
longbeom <wowow1859@naver.com>
2021-07-23 14:36:56 +0900

    feat : 숫자 형식 검증 기능 추가

--- src/main/java/step2/Validator.java
+++ src/main/java/step2/Validator.java
@@ -2,11 +2,13 @@ package step2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Validator {
 	private static List<String> operators = Arrays.asList("+", "-", "*", "/");
+	private static String numberRegex = "^[0-9]*$";
 
-	public static void validateExpression(String expression) {
+	public static void validateEmpty(String expression) {
 		if (expression == null || expression.trim().isEmpty()) {
 			throw new IllegalArgumentException("입력 값이 비어있어요!");
 		}
@@ -15,6 +17,16 @@ public class Validator {
 	public static void validateOperator(String operator) {
 		if (!operators.contains(operator)) {
 			throw new IllegalArgumentException("연산 기호가 잘못됐어요!");
+		}
+	}
+
+	public static void validateNumber(String number) {
+		if (number == null || number.isEmpty()) {
+			throw new NumberFormatException("숫자 형식이 잘못됐어요!");
+		}
+
+		if (!Pattern.matches(numberRegex, number)) {
+			throw new NumberFormatException("숫자 형식이 잘못됐어요!");
 		}
 	}
 }
