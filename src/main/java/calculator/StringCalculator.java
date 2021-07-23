2061e0b6a53c931e4a962bafabbd90ac2581bcf1
rladowl92 <rladowl92@naver.com>
2021-07-23 22:59:20 +0900

    feat(calc): 공백 입력 예외처리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,11 @@
 package calculator;
 
 public class StringCalculator {
-    public static int calc(String str){
-        String[] arr = str.split(" ");
+    public static int calc(String input){
+        String[] arr = input.split(" ");
+        if(arr.length == 0){
+            throw new IllegalArgumentException();
+        }
         int num1 = Integer.parseInt(arr[0]);
         String operator = arr[1];
         int num2 = Integer.parseInt(arr[2]);
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,32 +1,45 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class StringCalculatorTest {
 
     @Test
-    void add(){
+    void calc_add(){
         int result = StringCalculator.calc("1 + 2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    void sub(){
+    void calc_sub(){
         int result = StringCalculator.calc("3 - 1");
         assertThat(result).isEqualTo(2);
     }
 
     @Test
-    void mul(){
+    void calc_mul(){
         int result = StringCalculator.calc("3 * 2");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    void div(){
+    void calc_div(){
         int result = StringCalculator.calc("6 / 2");
         assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("입력 값이 null 이거나 공백문자일 경우 에러")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void calc_error_empty(String input){
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            StringCalculator.calc(input);
+        });
     }
 }
