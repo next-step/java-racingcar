53a08a7578f58144ad2e15aefb611385d822e451
jspark940321 <jspark940321@gmail.com>
2021-07-24 00:54:33 +0900

    refactor: calculateSplitedString 리팩토링
    https://github.com/next-step/java-racingcar/pull/2398#discussion_r675437618
    calculateSplitedString 메소드를 getNextNumber와 enum Operator로 가독성을 좋게 만들어보았습니다.

--- src/main/java/Calculator/StringCalculator.java
+++ src/main/java/Calculator/StringCalculator.java
@@ -37,27 +37,77 @@ public class StringCalculator {
     private void prepareCalculate(String s) {
         splitedString = s.split(" ");
         output = Integer.parseInt(splitedString[0]);
-        splitedStringSize = s.length();
+        splitedStringSize = splitedString.length;
     }
 
-    void calculateSplitedString(int index) {
-        if (index + 1 == splitedStringSize) throw new IllegalArgumentException("숫자가 부족합니다.");
+    private int getNextNumber(int index) {
+        if (index + 1 == splitedStringSize) {
+            throw new IllegalArgumentException("숫자가 부족합니다.");
+        }
+
         int rightNum = 0;
         try {
             rightNum = Integer.parseInt(splitedString[index + 1]);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자가 아닌 수를 입력받았습니다.");
         }
-        if (splitedString[index].equals("+")) {
-            output += rightNum;
-        } else if (splitedString[index].equals("-")) {
-            output -= rightNum;
-        } else if (splitedString[index].equals("*")) {
-            output *= rightNum;
-        } else if (splitedString[index].equals("/")) {
-            output /= rightNum;
-        } else {
-            throw new IllegalArgumentException("사칙연산이 잘 못 되었습니다..");
+        return rightNum;
+    }
+
+    void calculateSplitedString(int index) {
+        String operator = splitedString[index];
+        int rightNum = getNextNumber(index);
+
+        if(operator.equals(Operator.PLUS.value)){
+            plus(rightNum);
+            return;
         }
+
+        if(operator.equals(Operator.MINUS.value)){
+            minus(rightNum);
+            return;
+        }
+
+        if(operator.equals(Operator.MULTIPLY.value)){
+            multiply(rightNum);
+            return;
+        }
+
+        if(operator.equals(Operator.DIVIDE.value)){
+            divide(rightNum);
+            return;
+        }
+
+        throw new IllegalArgumentException("사칙연산이 잘 못 되었습니다..");
+    }
+
+    private enum Operator {
+        PLUS("+"),
+        MINUS("-"),
+        MULTIPLY("*"),
+        DIVIDE("/");
+
+        private final String value;
+
+        Operator(String value) {
+            this.value = value;
+        }
+
+    }
+
+    private void plus(int rightNum) {
+        output = output + rightNum;
+    }
+
+    private void minus(int rightNum) {
+        output = output - rightNum;
+    }
+
+    private void multiply(int rightNum) {
+        output = output * rightNum;
+    }
+
+    private void divide(int rightNum) {
+        output = output / rightNum;
     }
 }
