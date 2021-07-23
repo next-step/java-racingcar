44b43479a7ce54c70f391f21f6d9cb4a459306fe
sangw0804 <sangw0804@likelion.org>
2021-07-23 23:46:43 +0900

    feat(SetTest): 요구사항 1
    - Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.

--- src/test/java/learning/SetTest.java
+++ src/test/java/learning/SetTest.java
@@ -1,9 +1,13 @@
 package learning;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -18,5 +22,9 @@ public class SetTest {
     }
 
     // Test Case 구현
-
+    @Test
+    @DisplayName("size 로 Set 크기 구하기")
+    public void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
 }
