7672b8d27e89131e7597d4b19029a0c4da1e6846
longbeom <wowow1859@naver.com>
2021-07-23 14:43:44 +0900

    refactor : 계산기 테스트 수정

--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,55 +16,57 @@ public class CalculatorTest {
 	}
 
 	@DisplayName(value = "덧셈 테스트")
-	@CsvSource(value = {"1:2:3", "-1:0:-1", "-1:-1:-2"}, delimiter = ':')
+	@CsvSource(value = {"1:2:3", "1:0:1", "3:10:13"}, delimiter = ':')
 	@ParameterizedTest
 	void plus(int number1, int number2, int expect) {
-		int result = calculator.plus(number1, number2);
-
-		assertThat(result).isEqualTo(expect);
+		assertThat(getResult(number1, number2, "+")).isEqualTo(expect);
 	}
 
 	@DisplayName(value = "뺄셈 테스트")
-	@CsvSource(value = {"1:2:-1", "-1:0:-1", "-1:-1:0"}, delimiter = ':')
+	@CsvSource(value = {"1:2:-1", "5:2:3", "3:3:0"}, delimiter = ':')
 	@ParameterizedTest
 	void minus(int number1, int number2, int expect) {
-		int result = calculator.minus(number1, number2);
-
-		assertThat(result).isEqualTo(expect);
+		assertThat(getResult(number1, number2, "-")).isEqualTo(expect);
 	}
 
 	@DisplayName(value = "곱셈 테스트")
-	@CsvSource(value = {"1:2:2", "-1:0:0", "-1:-1:1"}, delimiter = ':')
+	@CsvSource(value = {"1:2:2", "1:0:0", "2:10:20"}, delimiter = ':')
 	@ParameterizedTest
 	void multiply(int number1, int number2, int expect) {
-		int result = calculator.multiply(number1, number2);
-
-		assertThat(result).isEqualTo(expect);
+		assertThat(getResult(number1, number2, "*")).isEqualTo(expect);
 	}
 
 	@DisplayName(value = "나눗셈 테스트")
-	@CsvSource(value = {"1:2:0", "0:-1:0", "-1:-1:1"}, delimiter = ':')
+	@CsvSource(value = {"1:2:0", "0:1:0", "5:2:2"}, delimiter = ':')
 	@ParameterizedTest
 	void divide(int number1, int number2, int expect) {
-		int result = calculator.divide(number1, number2);
-
-		assertThat(result).isEqualTo(expect);
+		assertThat(getResult(number1, number2, "/")).isEqualTo(expect);
 	}
 
 	@DisplayName(value = "0으로 나눌 때 실패 테스트")
-	@Test
-	void failDivide() {
-		assertThatThrownBy(() -> calculator.divide(1, 0))
+	@CsvSource(value = {"1:0", "3:0", "0:0"}, delimiter = ':')
+	@ParameterizedTest
+	void failDivide(int number1, int number2) {
+		assertThatThrownBy(() -> getResult(number1, number2, "/"))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("0으로 나눌 수 없습니다.");
+			.hasMessageContaining("0으로 나눌 수 없어요!");
 	}
 
 	@DisplayName(value = "문자열 계산기 테스트")
 	@CsvSource(value = {"1 + 2 / 1 * 3:9", "3 - 5 + 2 * 1 / 2:0"}, delimiter = ':')
 	@ParameterizedTest
 	void calculate(String input, int expect) {
-		int result = calculator.calculate(input);
+		int result = calculator.execute(input);
 
 		assertThat(result).isEqualTo(expect);
+	}
+
+	private int getResult(int number1, int number2, String operator) {
+		String expression = formatToExpression(number1, number2, operator);
+		return calculator.execute(expression);
+	}
+
+	private String formatToExpression(int number1, int number2, String operator) {
+		return String.format("%d %s %d", number1, operator, number2);
 	}
 }
