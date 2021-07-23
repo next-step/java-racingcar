5e09d92f2783baa55010959d48f0e756426c370c
YONGJU <odetsuo@gmail.com>
2021-07-23 13:33:03 +0900

    feat : 자동차 이름 예외 클래스 생성

---
+++ src/main/java/racingwinner/domain/CarNameException.java
@@ -0,0 +1,11 @@
+package racingwinner.domain;
+
+public class CarNameException extends RuntimeException {
+
+	private static final String DEFAULT_MESSAGE = "자동차 이름은 5자를 초과할 수 없습니다.";
+
+	public CarNameException() {
+		super(DEFAULT_MESSAGE);
+	}
+
+}
