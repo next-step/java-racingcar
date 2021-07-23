fd154b733ff901d53611628ed300d504b5482482
shirohoo <hch4102@naver.com>
2021-07-23 11:29:11 +0900

    refactor: 클래스 이름을 변경한다

---
+++ src/main/java/racing/Racers.java
@@ -0,0 +1,41 @@
+package racing;
+
+import java.util.List;
+
+import static java.util.stream.IntStream.range;
+
+class Racers {
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String DISPLAY_MARK = "-";
+
+    private final List<String> cars;
+
+    private Racers(List<String> cars) {
+        this.cars = cars;
+    }
+
+    public static Racers from(List<String> cars) {
+        return new Racers(cars);
+    }
+
+    public int size() {
+        return this.cars.size();
+    }
+
+    public String get(int index) {
+        return cars.get(index);
+    }
+
+    public void turnAround(ForwardConditionStrategy strategy, StringBuilder sb) {
+        range(0, size()).forEach(index -> turnAround(strategy, sb, index));
+    }
+
+    private void turnAround(ForwardConditionStrategy strategy, StringBuilder sb, int index) {
+        if (strategy.judgeCondition()) {
+            sb.append(get(index)).append(NEW_LINE);
+            return;
+        }
+        cars.set(index, cars.get(index) + DISPLAY_MARK);
+        sb.append(get(index)).append(NEW_LINE);
+    }
+}
--- src/test/java/racing/RacersTest.java
+++ src/test/java/racing/RacersTest.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RacingCarsTest {
+public class RacersTest {
     private final ForwardConditionStrategy mockStrategy = BDDMockito.mock(NumberMoreThanFourConditionStrategy.class);
 
     private ArrayList<String> list;
@@ -25,36 +25,36 @@ public class RacingCarsTest {
     @Test
     @DisplayName("정적_팩토리가_인스턴스를_생성하는지_확인한다")
     void from() throws Exception {
-        assertThat(RacingCars.from(list)).isInstanceOf(RacingCars.class);
+        assertThat(Racers.from(list)).isInstanceOf(Racers.class);
     }
 
     @Test
     @DisplayName("조회_테스트")
     void get() throws Exception {
-        assertThat(RacingCars.from(list).get(0)).isEqualTo("-");
+        assertThat(Racers.from(list).get(0)).isEqualTo("-");
     }
 
     @Test
     @DisplayName("전진_테스트_조건은_참")
     void goForward() throws Exception {
         BDDMockito.given(mockStrategy.judgeCondition()).willReturn(false);
-        RacingCars racingCars = RacingCars.from(list);
-        racingCars.goForward(0, mockStrategy);
-        assertThat(racingCars.get(0)).isEqualTo("--");
+        Racers racers = Racers.from(list);
+        racers.turnAround(mockStrategy, new StringBuilder());
+        assertThat(racers.get(0)).isEqualTo("--");
     }
 
     @Test
     @DisplayName("전진_테스트_조건은_거짓")
     void goForwardNot() throws Exception {
         BDDMockito.when(mockStrategy.judgeCondition()).thenReturn(true);
-        RacingCars racingCars = RacingCars.from(list);
-        racingCars.goForward(0, mockStrategy);
-        assertThat(racingCars.get(0)).isEqualTo("-");
+        Racers racers = Racers.from(list);
+        racers.turnAround(mockStrategy, new StringBuilder());
+        assertThat(racers.get(0)).isEqualTo("-");
     }
 
     @Test
     @DisplayName("사이즈_조회_테스트")
     void size() throws Exception {
-        assertThat(RacingCars.from(list).size()).isEqualTo(3);
+        assertThat(Racers.from(list).size()).isEqualTo(3);
     }
 }
