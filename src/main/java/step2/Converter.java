ff2a5a41a35fdba6f8f71085e303d52e0b886545
longbeom <wowow1859@naver.com>
2021-07-23 14:37:44 +0900

    refactor : 문자열 숫자 변환 검증 로직 추가

--- src/main/java/step2/Converter.java
+++ src/main/java/step2/Converter.java
@@ -1,8 +1,11 @@
 package step2;
 
+import static step2.Validator.*;
+
 public class Converter {
 
 	public static int stringToInt(String number) {
+		validateNumber(number);
 		return Integer.parseInt(number);
 	}
 }
