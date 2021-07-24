467680ed24da54a3945274dbdb78f8166ffd34bc
darkant99 <darkant99@naver.com>
2021-07-25 01:47:43 +0900

    [REFACTOR] BasicCar protected Location -> private Location 수정

--- src/main/java/racing/domain/car/BasicCar.java
+++ src/main/java/racing/domain/car/BasicCar.java
@@ -7,7 +7,7 @@ import racing.domain.fuel.Fuel;
 public class BasicCar implements Car {
     private static final int REQUIRED_FUEL_VALUE = 4; // 자동차가 움직이기 위한 최소 연료
     private final Name name;
-    protected Location location;
+    private Location location;
 
     public BasicCar(Name name) {
         this.name = name;
--- src/main/java/racing/domain/car/FakeCar.java
+++ src/main/java/racing/domain/car/FakeCar.java
@@ -6,13 +6,20 @@ import racing.domain.fuel.Fuel;
 
 // Fake 객체
 public final class FakeCar extends BasicCar {
+    private Location location;
     public FakeCar(Name name) {
         super(name);
+        location = Location.empty();
+    }
+
+    @Override
+    public Location location() {
+        return location;
     }
 
     @Override
     public void move(Fuel fuel) {
-        super.location = location().add(
+        this.location = location().add(
                 Location.oneBlock()
         );
     }
