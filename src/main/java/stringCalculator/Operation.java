da14c437945073e27080ae5ef3ee44f6cd91b2ad
beadoer1 <beadoer1@gmail.com>
2021-07-24 15:27:55 +0900

    람다 적용을 통한 enum 별 apply()(@Override) 메서드 정의 간소화

--- src/main/java/stringCalculator/Operation.java
+++ src/main/java/stringCalculator/Operation.java
@@ -2,43 +2,30 @@ package stringCalculator;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BinaryOperator;
 
 public interface Operation {
     String apply(String a, String b);
 }
 
 enum FourOperation implements Operation {
-    PLUS("+"){
-        public String apply(String a, String b) {
-            return (Integer.parseInt(a) + Integer.parseInt(b))+"";
-        }
-    },
-    MINUS("-"){
-        public String apply(String a, String b) {
-            return (Integer.parseInt(a) - Integer.parseInt(b))+"";
-        }
-    },
-    MULT("*"){
-        public String apply(String a, String b) {
-            return (Integer.parseInt(a) * Integer.parseInt(b))+"";
-        }
-    },
-    DIVIDE("/"){
-        public String apply(String a, String b) {
-            return (Integer.parseInt(a) / Integer.parseInt(b))+"";
-        }
-    };
+    PLUS("+", (a,b) -> (Integer.parseInt(a)+Integer.parseInt(b))+""),
+    MINUS("-", (a,b) -> (Integer.parseInt(a)-Integer.parseInt(b))+""),
+    MULT("*", (a,b) -> (Integer.parseInt(a)*Integer.parseInt(b))+""),
+    DIVIDE("/",(a,b) -> (Integer.parseInt(a)/Integer.parseInt(b))+"");
 
-    private final String operator;
+    private final String symbol;
+    private final BinaryOperator<String> operator;
 
-    FourOperation(String operator) {
+    FourOperation(String symbol,BinaryOperator<String> operator) {
+        this.symbol = symbol;
         this.operator = operator;
     }
 
     private static final Map<String,FourOperation> OP_MAP = new HashMap<>();
     static {
         for (FourOperation f : FourOperation.values()) {
-            OP_MAP.put(f.operator,f);
+            OP_MAP.put(f.symbol,f);
         }
     }
 
@@ -49,4 +36,8 @@ enum FourOperation implements Operation {
         return OP_MAP.get(operator);
     }
 
+    @Override
+    public String apply(String a, String b) {
+        return operator.apply(a, b);
+    }
 }
