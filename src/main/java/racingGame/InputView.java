2a354700e61298b564f9f2cea0449590efde6797
ckdgus08 <ckdgus0808@naver.com>
2021-07-25 03:12:15 +0900

    refactor: , 상수화

--- src/main/java/racingGame/InputView.java
+++ src/main/java/racingGame/InputView.java
@@ -7,12 +7,14 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
-    public static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String INPUT_REPEAT_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    public static final String CAR_NAMES_DELIMITER = ",";
+
     private final static Scanner scanner = new Scanner(System.in);
 
     public static List<Car> inputCarNames() {
-        System.out.println(INPUT_CAR_NAME_MESSAGE);
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
         return splitStringWithCommaToList(scanner.nextLine());
     }
 
@@ -22,7 +24,7 @@ public class InputView {
     }
 
     public static List<Car> splitStringWithCommaToList(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(CAR_NAMES_DELIMITER))
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
