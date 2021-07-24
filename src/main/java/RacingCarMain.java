0195f32933fbac3adc8f57e46b72e3b8539b32fc
jaeyoungjang2 <lion85488@gmail.com>
2021-07-24 18:10:15 +0900

    feat: RacingCar 실행 클래스 추가

--- src/main/java/RacingCarMain.java
+++ src/main/java/RacingCarMain.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class RacingCarMain {
+import racing.RacingCarInput;
+
+import java.util.Scanner;
+
+public class RacingCarMain {
+    public static void main(String[] args) {
+        RacingCarMain rcm = new RacingCarMain();
+        rcm.run();
+    }
+
+    private void run() {
+        RacingCarInput racingCarInput = new RacingCarInput();
+        try {
+            racingCarInput.requestInput();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
