2dc452bcedf87641a3b21c18227acf70900df8e7
namhyun.sung <namhyun.sung@yanolja.com>
2021-07-24 17:31:13 +0900

    refactor : input validation 추가

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -3,6 +3,8 @@ package step2;
 public class StringCalculator {
 
     public int calculate(String input) {
+
+        validateInput(input);
         String[] result = input.split(" ");
 
         int rst = toInt(result[0]);
@@ -10,7 +12,14 @@ public class StringCalculator {
         for (int i = 1; i < result.length; i+=2) {
             rst = calculate(rst, toInt(result[i+1]), result[i]);
         }
+
         return rst;
+    }
+
+    private void validateInput(String input) {
+        if(input.trim().equals("") || input == null) {
+            throw new IllegalArgumentException("입력 값이 비어있습니다.");
+        }
     }
 
     private int calculate(int firstOperand, int secondOperand, String operator) {
