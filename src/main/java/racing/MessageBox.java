589fc301d954cacd1e1e982c9853d85f8dabf41d
PIH6585 <pih6585@naver.com>
2021-07-24 11:58:06 +0900

    feat : 실행결과 메세지 추가

--- src/main/java/racing/MessageBox.java
+++ src/main/java/racing/MessageBox.java
@@ -4,6 +4,7 @@ public class MessageBox {
 
 	private static final String FIRST_REQUEST = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String SECOND_REQUEST = "시도할 회수는 몇회인가요?";
+	private static final String RESULT_MESSAGE = "실행_결과";
 	private static final String WINNER_MESSAGE = " 가 최종 우승했습니다.";
 
 	public void firstRequestMessage() {
@@ -12,6 +13,10 @@ public class MessageBox {
 
 	public void secondRequestMessage() {
 		System.out.println(SECOND_REQUEST);
+	}
+
+	public void resultMessage() {
+		System.out.println(RESULT_MESSAGE);
 	}
 
 	public void winnerMessageBox(String winner) {
@@ -27,10 +32,10 @@ public class MessageBox {
 	}
 
 	private String getRacingResultByString(int racingResult) {
-		String result = "";
+		StringBuilder result = new StringBuilder(racingResult);
 		for (int i = 0; i < racingResult; i++) {
-			result = result.concat("-");
+			result.append("-");
 		}
-		return result;
+		return result.toString();
 	}
 }
