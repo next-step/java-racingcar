620e20aa3df13ec432d6e683ed267d90665077c3
Giwan Kim <giwankim@gmail.com>
2021-07-24 15:57:02 +0900

    feat(racingcar): Add Position class
    Position class is wrapper for the car position integer value.

---
+++ src/main/java/racingcar/domain/car/Position.java
@@ -0,0 +1,40 @@
+package racingcar.domain.car;
+
+import java.util.Objects;
+
+public class Position {
+
+    private final int position;
+
+    public Position() {
+        this(0);
+    }
+
+    public Position(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("자동차의 위치는 양수이어야 합니다.");
+        }
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Position)) {
+            return false;
+        }
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+
+    public Position move() {
+        return new Position(position + 1);
+    }
+}
