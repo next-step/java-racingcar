5d181b252245a4e418b9518eb5c28aeff97558ed
ChangSubKwak <paradise1732@gmail.com>
2021-07-24 18:51:49 +0900

    feat: 위치 변수(pos), get/set 함수 추가, 전진함수 구현

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+public class RacingCar {
+	public int pos;
+
+	public RacingCar() {
+		this.pos = 0;
+	}
+
+	public int getPos() {
+		return this.pos;
+	}
+
+	public void setPos(int pos) {
+		this.pos = pos;
+	}
+
+	public void forward() {
+		pos = ( Util.generateForwardValue() >= 4 ) ? 1 : 0;
+	}
+}
