42ca32d0889e02bc4dd9d64fe5134ae75b7e6b55
longbeom <wowow1859@naver.com>
2021-07-24 13:18:39 +0900

    refactor : 결과 값 변수 number1 -> result에 할당

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -4,31 +4,31 @@ import static step2.Converter.*;
 import static step2.Validator.*;
 
 public class Calculator {
+	public int execute(String expression) {
+		String[] elements = splitExpression(expression);
 
-	public String[] splitExpression(String expression) {
+		int number1 = stringToInt(elements[0]);
+		int result = number1;
+
+		for (int i = 1; i < elements.length; i += 2) {
+			int number2 = stringToInt(elements[i + 1]);
+			String operator = elements[i];
+
+			result = calculate(operator, number1, number2);
+			number1 = result;
+		}
+
+		return result;
+	}
+
+	private String[] splitExpression(String expression) {
 		validateEmpty(expression);
 
 		return expression.trim()
 			.split(" ");
 	}
 
-	public int execute(String expression) {
-		String[] elements = splitExpression(expression);
-
-		int number1 = stringToInt(elements[0]);
-		for (int i = 1; i < elements.length; i += 2) {
-			int number2 = stringToInt(elements[i + 1]);
-			String operator = elements[i];
-
-			number1 = calculate(operator, number1, number2);
-		}
-
-		return number1;
-	}
-
 	private int calculate(String operator, int number1, int number2) {
-		validateOperator(operator);
-
 		OperatorType operatorType = OperatorType.getByOperator(operator);
 
 		return operatorType.calculateByOperator(number1, number2);
