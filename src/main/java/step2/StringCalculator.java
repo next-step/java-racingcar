403a9f87b2ecf0e38b9235c266ef000f31236cfa
조원진 <>
2021-07-24 10:59:38 +0900

    feat(StringCalculator) : 복합 계산 기능 완료

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -1,16 +1,24 @@
 package step2;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+    private static final Pattern pattern = Pattern.compile("^[0-9]*$");
+
     public static int calculate(Formulas input) {
         String[] formulas = input.getFormulas();
-        String operator = formulas[1];
-        OperatorType operatorType = OperatorType.find(operator);
-
-        return operatorType.operate(getInt(formulas[0]), getInt(formulas[2]));
-
+        int result = getInt(formulas[0]);
+        for (int i = 1; i < formulas.length; i += 2) {
+            OperatorType operatorType = OperatorType.find(formulas[i]);
+            result = operatorType.operate(result, getInt(formulas[i + 1]));
+        }
+        return result;
     }
 
     private static int getInt(String formula) {
-        return Integer.parseInt(formula);
+        if (pattern.matcher(formula).find()) {
+            return Integer.parseInt(formula);
+        }
+        throw new IllegalArgumentException();
     }
 }
--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -2,36 +2,45 @@ package step2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringCalculatorTest {
 
-    @DisplayName("덧셈 테스트")
-    @Test
-    void plus() {
-        int result = StringCalculator.calculate(new Formulas("2 + 3"));
-        assertThat(result).isEqualTo(5);
+    @DisplayName("4칙 연산 테스트")
+    @ParameterizedTest(name = "{index} {displayName} argument: {arguments}")
+    @CsvSource(value = {"2 + 3,5", "5 - 2,3", "3 * 6,18", "4 / 2,2"}, delimiter = ',')
+    void simple_calculate(String formulas, int expected) {
+        int result = StringCalculator.calculate(new Formulas(formulas));
+        assertThat(result).isEqualTo(expected);
     }
 
-    @DisplayName("뺄셈 테스트")
+    @DisplayName("복합 계산 테스트")
     @Test
-    void minus() {
-        int result = StringCalculator.calculate(new Formulas("5 - 2"));
-        assertThat(result).isEqualTo(3);
+    void complex_calculate() {
+        int result = StringCalculator.calculate(new Formulas("2 + 3 * 4 / 2"));
+        assertThat(result).isEqualTo(10);
     }
 
-    @DisplayName("곱셈 테스트")
-    @Test
-    void multiply() {
-        int result = StringCalculator.calculate(new Formulas("3 * 6"));
-        assertThat(result).isEqualTo(18);
+    @DisplayName("4칙 연산자가 아닌 값이 입력 될 경우 IllegalArgumentException 발생")
+    @ParameterizedTest(name = "{index} {displayName} argument: {arguments}")
+    @ValueSource(strings = {"2 + 3 @ 4 - 2", "2 6 7 - 3"})
+    void calculate_throwsIllegalArgumentException_IfOperatorIsNot4Rules(String formulas) {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> StringCalculator.calculate(new Formulas(formulas))
+        );
     }
 
-    @DisplayName("나눗셈 테스트")
-    @Test
-    void divide() {
-        int result = StringCalculator.calculate(new Formulas("4 / 2"));
-        assertThat(result).isEqualTo(2);
+    @DisplayName("숫자가 아닌 값이 입력 될 경우 IllegalArgumentException 발생")
+    @ParameterizedTest(name = "{index} {displayName} argument: {arguments}")
+    @ValueSource(strings = {"% + 3 * 4 / 2", "  + 7 - 3"})
+    void calculate_throwsIllegalArgumentException_IfOperandIsNotNumber(String formulas) {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> StringCalculator.calculate(new Formulas(formulas))
+        );
     }
 }
