6c230f0e973b0b60d322f690a2ed8b1458d391e1
jspark940321 <jspark940321@gmail.com>
2021-07-25 02:02:45 +0900

    feat: Input 예외부분 구현
    Input 예외부분 구현 및 test코드 잘못된 부분 수정

--- src/main/java/Car/RacingCar.java
+++ src/main/java/Car/RacingCar.java
@@ -1,5 +1,6 @@
 package Car;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,10 +15,17 @@ public class RacingCar {
 
     public void gameStart() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        carCount = scanner.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        gameCount = scanner.nextInt();
+        try{
+            System.out.println("자동차 대수는 몇 대 인가요?");
+            carCount = scanner.nextInt();
+            System.out.println("시도할 회수는 몇 회 인가요?");
+            gameCount = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            throw new IllegalArgumentException("숫자 값을 입력해주세요.");
+        }
+        if(carCount <= 0 || gameCount <= 0){
+            throw new IllegalArgumentException("0보다 큰 값을 입력해주세요.");
+        }
 
 
     }
