598982ffaf155607d1bb4f1597f2ecf73a66e569
darkant99 <darkant99@naver.com>
2021-07-24 14:50:27 +0900

    [TEST] 경계값 분석 ?!

--- src/main/java/racing/car/Fuel.java
+++ src/main/java/racing/car/Fuel.java
@@ -1,27 +1,30 @@
 package racing.car;
 
 public class Fuel {
-    public static Fuel EMPTY = new Fuel(0);
-    public static Fuel FULL = new Fuel(9);
+    protected static int MIN_VALUE = 0;
+    protected static int MAX_VALUE = 9;
 
     private static final String OVER_FULL_MESSAGE = "최대 충전 가능한 연료를 넘었습니다.";
     private static final String UNDER_EMPTY_MESSAGE = "연료는 음수르 충전할 수 없습니다.";
 
-    protected final int value;
+    public static Fuel EMPTY = new Fuel(MIN_VALUE);
+    public static Fuel FULL = new Fuel(MAX_VALUE);
+
+    private final int value;
 
     public Fuel(int value) {
+        validate(value);
         this.value = value;
     }
 
-    private static void validate(int value) {
-        if (value < 0)
-            throw new IllegalArgumentException(UNDER_EMPTY_MESSAGE);
-        if (value > FULL.value)
-            throw new IllegalArgumentException(OVER_FULL_MESSAGE);
+    public int value() {
+        return value;
     }
 
-    public boolean isMovable(int requireFuelValue) {
-        validate(value); // 유효성 검사 Lazy
-        return this.value >= requireFuelValue;
+    private void validate(int value) {
+        if (value < MIN_VALUE)
+            throw new IllegalArgumentException(UNDER_EMPTY_MESSAGE);
+        if (value > MAX_VALUE)
+            throw new IllegalArgumentException(OVER_FULL_MESSAGE);
     }
 }
