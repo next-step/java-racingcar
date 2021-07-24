8c95533b9448d6553490f033b2091fab9ae2072d
jung0930 <wjddlqkqh57@naver.com>
2021-07-24 20:24:18 +0900

    feat : Race 도메인, 테스트 코드 추가

--- src/main/java/racingCar/domain/Race.java
+++ src/main/java/racingCar/domain/Race.java
@@ -1,49 +1,69 @@
 package racingCar.domain;
 
-import racingCar.util.RandomNumber;
+import racingCar.domain.strategy.MoveStrategy;
+import racingCar.domain.strategy.RacingMoveStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Race {
 
-    private final List<Car> cars;
-    private final int FORWARD_CRITERIA = 4;
-    private final static String IS_NOT_DUAL_NUMBER_ERROR_MESSAGE = "자동차 대수는 1대 이상이어야 합니다.";
+    private static final List<Round> rounds = new ArrayList<>();
+    private MoveStrategy moveStrategy = new RacingMoveStrategy();
+    private final int MIN_VALUE_BY_CAR = 1;
+    private final static String IS_NOT_DUAL_CAR_COUNT_ERROR_MESSAGE = "자동차 대수는 1대 이상이어야 합니다.";
+    private final static String IS_NOT_DUAL_ROUND_COUNT_ERROR_MESSAGE = "라운드는 1라운드 이상이어야 합니다.";
+    private final int ZERO = 0;
 
-    public Race(int count) {
-        validateInput(count);
-        this.cars = addCar(count);
+    public Race(int carCount, int roundCount) {
+        validateInput(carCount, roundCount);
+        lineUp(carCount);
+        startRace(roundCount);
     }
 
-    public Race(List<Car> cars) {
-        this.cars = cars;
-    }
-
-    private void validateInput(int count) {
-        if(count < 1) {
-            throw new IllegalArgumentException(IS_NOT_DUAL_NUMBER_ERROR_MESSAGE);
+    private void validateInput(int carCount, int roundCount) {
+        if(isCarCountOverZero(carCount)) {
+            throw new IllegalArgumentException(IS_NOT_DUAL_CAR_COUNT_ERROR_MESSAGE);
+        }
+        if(isRoundCountOverZero(roundCount)) {
+            throw new IllegalArgumentException(IS_NOT_DUAL_ROUND_COUNT_ERROR_MESSAGE);
         }
     }
 
-    private List<Car> addCar(int count) {
-        List<Car> cars = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            cars.add(new Car());
-        }
-        return cars;
+    private boolean isCarCountOverZero(int carCount) {
+        return carCount < MIN_VALUE_BY_CAR;
     }
 
-    public Race moveCar() {
-        List<Car> movedCars = new ArrayList<>();
-        for (Car car : cars) {
-            movedCars.add(car.move(RandomNumber.getRandomNumber() >= FORWARD_CRITERIA));
-        }
-        return new Race(movedCars);
+    private boolean isRoundCountOverZero(int roundCount) {
+        return roundCount < MIN_VALUE_BY_CAR;
     }
 
-    public List<Car> getCars() {
-        return cars;
+    private void lineUp(int carCount) {
+        this.rounds.add(new Round(
+                Stream.generate(Car::new)
+                        .limit(carCount)
+                        .collect(toList())
+        ));
+    }
+
+    private void startRace(int roundCount) {
+        int roundNumber = ZERO;
+        while (roundNumber < roundCount){
+            Round round = rounds.get(roundNumber).startRound(moveStrategy);
+            this.rounds.add(round);
+            roundNumber++;
+        }
+    }
+
+    public Round getRoundResult(int roundNumber) {
+        return rounds.get(roundNumber);
+    }
+
+    public int getRoundCount() {
+        return rounds.size();
     }
 
 }
--- src/test/java/racingCar/domain/RaceTest.java
+++ src/test/java/racingCar/domain/RaceTest.java
@@ -2,36 +2,46 @@ package racingCar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class RaceTest {
 
-    Race race;
-
-    @DisplayName("자동차 대수가 0대 이하일 경우 IllegalArgumentException throw 테스트")
+    @DisplayName("자동차 대수, 경기수가 1 이상일때 객체생성")
     @ParameterizedTest
-    @ValueSource(ints = {0, -1, -5, -7})
-    void inputNotDualNumberTest(int input) {
+    @CsvSource(value = {"1:1", "5:3", "10:2"}, delimiter = ':')
+    void testInput(int carCount, int roundCount) {
+        assertThat(new Race(carCount,roundCount)).isInstanceOf(Race.class);
+    }
+
+    @DisplayName("자동차 대수가 1대 미만일때 IllegalArgumentException throw")
+    @ParameterizedTest
+    @CsvSource(value = {"0:5", "-10:10", "-20:15"}, delimiter = ':')
+    void testInputNegativeCarsCount(int carCount, int roundCount) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            race = new Race(input);
+            new Race(carCount, roundCount);
         }).withMessageMatching("자동차 대수는 1대 이상이어야 합니다.");
     }
 
-    @DisplayName("자동차 대수가 1대 이상일 경우 객체 생성되는지 테스트")
+    @DisplayName("라운드가 1경기 미만일때 IllegalArgumentException throw")
     @ParameterizedTest
-    @ValueSource(ints = {1, 5, 10})
-    void inputDualNumberTest(int input) {
-        assertThat(new Race(input)).isInstanceOf(Race.class);
+    @CsvSource(value = {"5:0", "10:-10", "15:-20"}, delimiter = ':')
+    void testInputNegativeRoundCount(int carCount, int roundCount) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Race(carCount, roundCount);
+        }).withMessageMatching("라운드는 1라운드 이상이어야 합니다.");
     }
 
-    @DisplayName("입력값과 생성된 리스트의 길이가 같은지 테스트")
+    @DisplayName("원하는 라운드의 결과를 가져온다.")
     @ParameterizedTest
-    @ValueSource(ints = {1, 5, 10})
-    void carsLengthTest(int input) {
-        assertThat(new Race(input).getCars().size()).isEqualTo(input);
+    @CsvSource(value = {"15:20"}, delimiter = ':')
+    void testGetRound(int carCount, int roundCount) {
+        Race race = new Race(carCount, roundCount);
+        Round round = race.getRoundResult(1);
+        assertThat(round).isInstanceOf(Round.class);
+        assertThat(round.getCarsSize()).isEqualTo(carCount);
     }
 
 }
