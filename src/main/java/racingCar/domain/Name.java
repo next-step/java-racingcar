40fdbb232ce0c6e6a972e6a30bc88b7461649b15
jung0930 <wjddlqkqh57@naver.com>
2021-07-24 20:56:36 +0900

    feat : 이름값 null, 빈 문자열 검사, 테스트 코드 추가

--- src/main/java/racingCar/domain/Name.java
+++ src/main/java/racingCar/domain/Name.java
@@ -1,11 +1,26 @@
 package racingCar.domain;
 
+import java.util.Objects;
+
 public class Name {
 
     private final String name;
+    private final int MIN_LENGTH_BY_NAME = 1;
+    private final int MAX_VALUE_BY_NAME = 5;
+    private final static String IS_NULL_OR_EMPTY_ERROR_MESSAGE = "자동차의 이름을 입력해주세요.";
+    private final static String IS_NOT_BETWEEN_ONE_AND_FIVE_ERROR_MESSAGE = "자동차의 이름은 1~5글자 사이로 입력해야 합니다.";
 
     public Name(String name) {
+        validateNull(name);
         this.name = name;
     }
+
+    private void validateNull(String name) {
+        if(Objects.isNull(name) || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(IS_NULL_OR_EMPTY_ERROR_MESSAGE);
+        }
+    }
+
+    // validateNameLength
 
 }
--- src/test/java/racingCar/domain/NameTest.java
+++ src/test/java/racingCar/domain/NameTest.java
@@ -1,18 +1,51 @@
 package racingCar.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.NullString;
+
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class NameTest {
 
     @DisplayName("이름이 1글자~5글자 사이일 경우 객체생성")
     @ParameterizedTest
     @ValueSource(strings = {"바나나", "파인애플", "감", "천도복숭아"})
-    void testCreateName(String input){
+    void testCreateName(String input) {
         assertThat(new Name(input)).isInstanceOf(Name.class);
     }
+
+    @DisplayName("이름이 null일 경우 IllegalArgumentException throw")
+    @Test
+    void testInputNull() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Name(null);
+        }).withMessageMatching("자동차의 이름을 입력해주세요.");
+    }
+
+    @DisplayName("이름이 비었을 경우 IllegalArgumentException throw")
+    @ParameterizedTest
+    @ValueSource(strings = {"  ", ""})
+    void testInputEmpty(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Name(input);
+        }).withMessageMatching("자동차의 이름을 입력해주세요.");
+    }
+
+    /*
+    @DisplayName("이름이 1글자~5글자 사이가 아닐 경우 IllegalArgumentException throw")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "레인보우샤베트", "엄마는외계인", "쿠키앤크림치즈"})
+    void testInputNonSpecifiedName(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Name(input);
+        }).withMessageMatching("자동차의 이름은 1~5글자 사이로 입력해야 합니다.");
+    }
+    */
 
 }
