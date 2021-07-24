171ea6c5e2e5683bc259c85a3f44f23325f69bcc
YisooKim <45457001+gashe-soo@users.noreply.github.com>
2021-07-24 19:42:10 +0900

    Step2 - 문자열 계산기 (#2412)
    * docs:요구 구현사항 정리
    
    * feat:덧셈 기능 구현
    
    * feat:뺄셈 기능 구현
    
    * feat:곱셈 기능 구현
    
    * feat:나눗셈 기능 구현
    
    * fix:메서드 접근제한자 변경
    
    * feat:입력값 검증 기능 구현
    
    * feat:사칙연산 기호가 아닌 경우 오류 기능 구현
    
    * test:복합 사칙연산 테스트 추가
    
    * style:static 메소드 제거
    
    * refactor:중간 연산 메소드 추출

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,63 @@
+package calculator;
+
+import java.util.function.BinaryOperator;
+
+public class StringCalculator {
+
+    private int result;
+    private String[] splitExpression;
+
+    public int calculate(String expression) {
+        validateInput(expression);
+
+        splitExpression = expression.split(" ");
+        result = Integer.parseInt(splitExpression[0]);
+
+        for (int idx = 1; idx < splitExpression.length; idx += 2) {
+            calculateOneByOne(idx);
+        }
+
+        return result;
+    }
+
+    private void calculateOneByOne(int idx) {
+        BinaryOperator<Integer> calculateFunction = distinguishOperator(splitExpression[idx]);
+        int operand = Integer.parseInt(splitExpression[idx + 1]);
+
+        result = calculateFunction.apply(result, operand);
+    }
+
+    private void validateInput(String expression) {
+        if (expression == null || expression.isEmpty())
+            throw new IllegalArgumentException("입력은 공백이면 안됩니다.");
+    }
+
+    private BinaryOperator<Integer> distinguishOperator(String operator) {
+        if (operator.equals("+"))
+            return add();
+        if (operator.equals("-"))
+            return subtract();
+        if (operator.equals("*"))
+            return multiply();
+        if (operator.equals("/"))
+            return divide();
+        throw new IllegalArgumentException("사칙 연산 기호가 아닙니다.");
+    }
+
+    private BinaryOperator<Integer> add() {
+        return (a, b) -> a + b;
+    }
+
+    private BinaryOperator<Integer> subtract() {
+        return (a, b) -> a - b;
+    }
+
+    private BinaryOperator<Integer> multiply() {
+        return (a, b) -> a * b;
+    }
+
+    private BinaryOperator<Integer> divide() {
+        return (a, b) -> a / b;
+    }
+
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,81 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+
+@SuppressWarnings("NonAsciiCharacters")
+public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void 환경설정(){
+        stringCalculator = new StringCalculator();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 2,3", "7 + 8 + 5,20"})
+    void 덧셈_기능_확인(String expression, int expected) {
+        assertThat(stringCalculator.calculate(expression)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 - 2,-1", "5 - 3,2", "10 - 1 - 4,5"})
+    void 뺄셈_기능_확인(String expression, int expected) {
+        assertThat(stringCalculator.calculate(expression)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1 * 2,2", "5 * 3,15", "10 * 2 * 3,60"})
+    void 곱셈_기능_확인(String expression, int expected) {
+        assertThat(stringCalculator.calculate(expression)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4 / 2,2", "10 / 2,5", "12 / 2 / 3,2"})
+    void 나눗셈_기능_확인(String expression, int expected) {
+        assertThat(stringCalculator.calculate(expression)).isEqualTo(expected);
+    }
+
+    @Test
+    void 입력값이_빈공백_대한_오류검증(){
+        assertThatIllegalArgumentException().isThrownBy(
+                ()->stringCalculator.calculate("")
+        ).withMessage("입력은 공백이면 안됩니다.");
+    }
+
+    @Test
+    void 입력값이_null_대한_오류검증(){
+        assertThatIllegalArgumentException().isThrownBy(
+                ()->stringCalculator.calculate(null)
+        ).withMessage("입력은 공백이면 안됩니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3 % 1", "5 & 2"})
+    void 사칙연산기호가_아닌_경우_오류검증(String expression){
+        assertThatIllegalArgumentException().isThrownBy(
+                ()->stringCalculator.calculate(expression)
+        ).withMessage("사칙 연산 기호가 아닙니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1 + 2 / 3, 1"
+            , "4 - 2 + 10 * 2,24"
+            , "1 + 3 / 2 * 5,10"
+            , "1 - 5 * 2,-8"
+    })
+    void 사칙연산_모두_포함_기능_검증(String expression, int expected){
+        assertThat(stringCalculator.calculate(expression)).isEqualTo(expected);
+    }
+
+
+}
