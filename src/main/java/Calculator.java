2cb156a21372281e3c71b9e9b79efe734d943e86
joo-pe <joohyung1002@naver.com>
2021-07-24 23:48:24 +0900

    :sparkles: 문자열 파싱

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,63 @@
+import com.sun.org.apache.bcel.internal.generic.SWITCH;
+
+import static com.sun.tools.doclint.Entity.minus;
+
+public class Calculator {
+
+    public static final String ADD = "+";
+    public static final String MINUS = "-";
+    public static final String DIVISION = "/";
+    public static final String MULTIPLICATION = "*";
+
+    public static final int result = 0;
+
+    private void decide(String inputStr) {
+
+        String[] splitStr = inputStr.split(" ");
+        for (int i = 0; i < splitStr.length; i++) {
+            int leftInt = Integer.parseInt(splitStr[i - 1]);
+            int rightInt = Integer.parseInt(splitStr[i + 1]);
+            switch (splitStr[i]) {
+                case ADD:
+                    add(leftInt, rightInt);
+                    break;
+                case MINUS:
+                    minus(leftInt, rightInt);
+                    break;
+                case DIVISION:
+                    devision(leftInt, rightInt);
+                    break;
+                case MULTIPLICATION:
+                    mutiplication(leftInt, rightInt);
+                    break;
+            }
+
+        }
+    }
+
+    private void mutiplication(int leftInt, int rightInt) {
+    }
+
+    private void devision(int leftInt, int rightInt) {
+    }
+
+    private void minus(int leftInt, int rightInt) {
+    }
+
+    private void add(int leftInt , int rightInt) {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        byte[] str = new byte[256];
+        System.in.read(str, 0, 256);
+        String readStr = new String(str);
+
+        Calculator calculator = new Calculator();
+        calculator.decide(readStr);
+
+
+
+    }
+}
