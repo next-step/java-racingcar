876213c9f772dc928453e78d6336c81f4852f39b
ckdgus08 <ckdgus0808@naver.com>
2021-07-25 03:20:34 +0900

    style: 테스트 의도를 명확하게 나타내도록 네이밍 변경

--- src/test/java/racingGame/InputViewTest.java
+++ src/test/java/racingGame/InputViewTest.java
@@ -12,7 +12,7 @@ public class InputViewTest {
 
     @ParameterizedTest
     @ValueSource(strings = "pobi,crong,honux")
-    @DisplayName("입력받은 String을 , 단위로 분리하여 리스트로 만든다.")
+    @DisplayName("입력받은 String 을 , 단위로 분리하여 List<car>을 반환한다.")
     void splitStringWithCommaToList(String input) {
         List<Car> result = InputView.splitStringWithCommaToList(input);
         assertThat(result).containsExactly(
