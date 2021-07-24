5860a948d2676b2cb9fa04c62dfeed8c22a90414
jung0930 <wjddlqkqh57@naver.com>
2021-07-24 21:09:51 +0900

    feat : 자동차 이름 길이 체크, 이름 반환 메소드, 테스트 코드 추가

--- src/main/java/racingCar/domain/Name.java
+++ src/main/java/racingCar/domain/Name.java
@@ -5,22 +5,43 @@ import java.util.Objects;
 public class Name {
 
     private final String name;
-    private final int MIN_LENGTH_BY_NAME = 1;
-    private final int MAX_VALUE_BY_NAME = 5;
+    private final static int MIN_LENGTH_BY_NAME = 1;
+    private final static int MAX_VALUE_BY_NAME = 5;
     private final static String IS_NULL_OR_EMPTY_ERROR_MESSAGE = "자동차의 이름을 입력해주세요.";
     private final static String IS_NOT_BETWEEN_ONE_AND_FIVE_ERROR_MESSAGE = "자동차의 이름은 1~5글자 사이로 입력해야 합니다.";
 
     public Name(String name) {
-        validateNull(name);
+        validateName(name);
         this.name = name;
     }
 
-    private void validateNull(String name) {
-        if(Objects.isNull(name) || name.trim().isEmpty()) {
+    public void validateName(String name) {
+        validateEmpty(name);
+        validateNameLength(name);
+    }
+
+    private void validateEmpty(String name) {
+        if(isNameEmpty(name)) {
             throw new IllegalArgumentException(IS_NULL_OR_EMPTY_ERROR_MESSAGE);
         }
     }
 
-    // validateNameLength
+    private boolean isNameEmpty(String name) {
+        return Objects.isNull(name) || name.trim().isEmpty();
+    }
+
+    private void validateNameLength(String name) {
+        if(isNameOverSize(name.trim())) {
+            throw new IllegalArgumentException(IS_NOT_BETWEEN_ONE_AND_FIVE_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean isNameOverSize(String name) {
+        return name.length() < MIN_LENGTH_BY_NAME || name.length() > MAX_VALUE_BY_NAME;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 
 }
--- src/test/java/racingCar/domain/NameTest.java
+++ src/test/java/racingCar/domain/NameTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.NullString;
-
-import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -37,15 +34,20 @@ class NameTest {
         }).withMessageMatching("자동차의 이름을 입력해주세요.");
     }
 
-    /*
     @DisplayName("이름이 1글자~5글자 사이가 아닐 경우 IllegalArgumentException throw")
     @ParameterizedTest
-    @ValueSource(strings = {"", "레인보우샤베트", "엄마는외계인", "쿠키앤크림치즈"})
-    void testInputNonSpecifiedName(String input) {
+    @ValueSource(strings = {"레인보우샤베트", "엄마는외계인", "쿠키앤크림치즈"})
+    void testInputLongName(String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             new Name(input);
         }).withMessageMatching("자동차의 이름은 1~5글자 사이로 입력해야 합니다.");
     }
-    */
+    
+    @DisplayName("입력한 이름을 반환하는지 확인")
+    @ParameterizedTest
+    @ValueSource(strings = {"원숭이", "코끼리", "푸마", "불독"})
+    void testGetName(String input) {
+        assertThat(new Name(input).getName()).isEqualTo(input.trim());
+    }
 
 }
