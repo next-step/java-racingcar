43a7b6e4b47932a0ee7d7a44e33f6692defb170e
PIH6585 <pih6585@naver.com>
2021-07-24 13:19:50 +0900

    test: 기능 분리에 따른 test 진행

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,17 +1,17 @@
 package racing;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
     @ParameterizedTest
     @DisplayName("입력받은 숫자가 4이상이면 증가하고 그렇지 않으면 정지한다.")
     @CsvSource(value = {"4,1","3,0","7,1"})
-    public void carRacingCondition_check(int randomValue, int moveValue) {
+    public void carRacingConditionCheck(int randomValue, int moveValue) {
         Car car = new Car("car1");
         int result = car.carRacing(randomValue);
 
@@ -27,4 +27,32 @@ class CarTest {
 
         assertThat(playerName).isEqualTo(value);
     }
+
+    @ParameterizedTest
+    @DisplayName("이름이 5글자 이상이면 예외처리가 발생된다.")
+    @CsvSource(value = {"smith","tester","freeze"})
+    public void nameLengthCheck(String value){
+        assertThatThrownBy(() -> new Car(value))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("자동차 이름은 5글자를 초과할수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("경주시 움직인 값과 결과값이 동일하다.")
+    @CsvSource(value = {"car1,1,5,7","car2,2,4,8","car3,0,9,1"})
+    public void returnCarResultRacing(String value,int race1, int race2, int race3){
+        Car car = new Car(value);
+        int result1 = car.carRacing(race1);
+
+        assertThat(result1).isEqualTo(car.resultRacing());
+
+        int result2 = car.carRacing(race2);
+
+        assertThat(result2).isEqualTo(car.resultRacing());
+
+        int result3 = car.carRacing(race3);
+
+        assertThat(result3).isEqualTo(car.resultRacing());
+    }
+
 }
