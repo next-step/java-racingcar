1c1451acb839c5b74a6263e45336a9a62b1a3b98
hjjangdev <ju0398@hanmail.net>
2021-07-24 22:33:43 +0900

    refactor : 상수명 수정

--- src/main/java/racingcar/util/ValidationUtil.java
+++ src/main/java/racingcar/util/ValidationUtil.java
@@ -4,27 +4,27 @@ import racingcar.exception.InvalidUserInputException;
 
 public class ValidationUtil {
 
-    private static final String INPUT_STRING_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
-    private static final String INPUT_CAR_COUNT_ERROR_MESSAGE = "2개 이상의 자동차를 입력해야 시작 가능합니다.";
-    private static final String INPUT_GAME_COUNT_ERROR_MESSAGE = "1회 이상의 게임 횟수를 입력해야 시작 가능합니다.";
+    private static final String INVALID_STRING_MESSAGE = "숫자만 입력 가능합니다.";
+    private static final String INVALID_CAR_COUNT_MESSAGE = "2개 이상의 자동차를 입력해야 시작 가능합니다.";
+    private static final String INVALID_GAME_COUNT_MESSAGE = "1회 이상의 게임 횟수를 입력해야 시작 가능합니다.";
 
     public static void checkUserInput(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new NumberFormatException(INPUT_STRING_ERROR_MESSAGE);
+            throw new NumberFormatException(INVALID_STRING_MESSAGE);
         }
     }
 
     public static void checkCountOfCar(int number) {
         if (number <= 1) {
-            throw new InvalidUserInputException(INPUT_CAR_COUNT_ERROR_MESSAGE);
+            throw new InvalidUserInputException(INVALID_CAR_COUNT_MESSAGE);
         }
     }
 
     public static void checkCountOfGame(int number) {
         if (number <= 0) {
-            throw new InvalidUserInputException(INPUT_GAME_COUNT_ERROR_MESSAGE);
+            throw new InvalidUserInputException(INVALID_GAME_COUNT_MESSAGE);
         }
     }
 }
