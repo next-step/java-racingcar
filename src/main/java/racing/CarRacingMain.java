8f4c7a832449503887a10b1c773fe87fd80e1265
PIH6585 <pih6585@naver.com>
2021-07-24 11:27:50 +0900

    refactor : message 기능은 message 클래스로 이관

--- src/main/java/racing/CarRacingMain.java
+++ src/main/java/racing/CarRacingMain.java
@@ -4,18 +4,15 @@ import java.util.Scanner;
 
 public class CarRacingMain {
 
-	private static final String FIRST_REQUEST = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-	private static final String SECOND_REQUEST = "시도할 회수는 몇회인가요?";
-
 	public static void main(String[] args) {
 		MessageBox messageBox = new MessageBox();
 		Racing racing = new Racing(messageBox);
 		Scanner scanner = new Scanner(System.in);
 
-		messageBox.commonMessageBox(FIRST_REQUEST);
+		messageBox.firstRequestMessage();
 		racing.createPlayer(scanner.next());
 
-		messageBox.commonMessageBox(SECOND_REQUEST);
+		messageBox.secondRequestMessage();
 		racing.playingGame(scanner.next());
 
 		scanner.close();
