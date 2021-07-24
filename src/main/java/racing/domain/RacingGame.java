14f2d37d86e0d52503567160b00401d29bb8dde9
shirohoo <hch4102@naver.com>
2021-07-24 23:29:52 +0900

    refactor: 경주에 대한 설정을 내부 클래스로 변경

--- src/main/java/racing/domain/RacingGame.java
+++ src/main/java/racing/domain/RacingGame.java
@@ -1,12 +1,15 @@
 package racing.domain;
 
+import racing.model.Car;
 import racing.model.Racer;
 import racing.model.Racers;
-import racing.model.UnnamedCar;
+import racing.strategy.ForwardConditionStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
+import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.IntStream.range;
 
@@ -17,12 +20,12 @@ public class RacingGame {
 
     private int stageCount;
 
-    private RacingGame(ForwardConditionStrategy strategy, int racerCount, int stageCount) {
-        this.config = new RacingConfig(strategy, racerCount, stageCount);
+    private RacingGame(ForwardConditionStrategy strategy, String names, int stageCount) {
+        this.config = new RacingConfig(strategy, names, stageCount);
     }
 
-    public static RacingGame of(ForwardConditionStrategy strategy, int racerCount, int stageCount) {
-        return new RacingGame(strategy, racerCount, stageCount);
+    public static RacingGame init(ForwardConditionStrategy strategy, String names, int stageCount) {
+        return new RacingGame(strategy, names, stageCount);
     }
 
     public String run() {
@@ -33,7 +36,9 @@ public class RacingGame {
             turnAround(resultDataBuilder);
             resultDataBuilder.append(NEW_LINE);
         }
-        return resultDataBuilder.toString().trim();
+        return resultDataBuilder
+                .append(config.racers.winner())
+                .toString().trim();
     }
 
     private boolean isStageGreaterThanZero() {
@@ -45,28 +50,48 @@ public class RacingGame {
     }
 
     private class RacingConfig {
+        private static final String DELIMITER = ",";
+
         private static final int LIMIT_RACER = 2;
         private static final int LIMIT_TRY = 1;
+
+        private final Pattern normalPattern = Pattern.compile("(\\s,\\s)|(\\s,)|(,\\s)");
 
         private final ForwardConditionStrategy strategy;
         private final Racers racers;
 
         private int stageCount;
 
-        private RacingConfig(ForwardConditionStrategy strategy, int racerCount, int stageCount) {
-            if (racerCount < LIMIT_RACER) {
-                throw new IllegalArgumentException("자동차는 " + LIMIT_RACER + "대 이상이어야 합니다");
+        private RacingConfig(ForwardConditionStrategy strategy, String string, int stageCount) {
+            verify(string, stageCount);
+
+            String[] names = string.split(DELIMITER);
+            int size = names.length;
+
+            this.strategy = strategy;
+            this.stageCount = stageCount;
+            this.racers = Racers.from((List<? extends Racer>) range(0, size)
+                    .mapToObj(index -> Car.of(names[index], ""))
+                    .collect(toCollection(ArrayList::new)));
+        }
+
+        private void verify(String names, int stageCount) {
+            if (names == null || !names.contains(DELIMITER) || names.equals(DELIMITER) || isPatternMatching(names)) {
+                throw new IllegalArgumentException("자동차 이름은 " + LIMIT_RACER + "개 이상이어야 하고, 공백이 있으면 안되며 쉼표(" + DELIMITER + ")로 구분되어야 합니다.");
             }
 
             if (stageCount < LIMIT_TRY) {
                 throw new IllegalArgumentException("시도 횟수는 " + LIMIT_TRY + "회 이상이어야 합니다");
             }
 
-            this.strategy = strategy;
-            this.stageCount = stageCount;
-            this.racers = Racers.from((List<? extends Racer>) range(0, racerCount)
-                    .mapToObj(it -> UnnamedCar.emptyCar())
-                    .collect(toCollection(ArrayList::new)));
+            String[] strings = names.split(",");
+            if (strings.length != stream(strings).distinct().count()) {
+                throw new IllegalArgumentException("자동차 이름은 중복 될 수 없습니다.");
+            }
+        }
+
+        private boolean isPatternMatching(String names) {
+            return normalPattern.matcher(names).find();
         }
     }
 }
--- src/test/java/racing/domain/RacingGameTest.java
+++ src/test/java/racing/domain/RacingGameTest.java
@@ -2,8 +2,7 @@ package racing.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racing.domain.NumberMoreThanFourConditionStrategy;
-import racing.domain.RacingGame;
+import racing.strategy.NumberMoreThanFourConditionStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,29 +13,61 @@ public class RacingGameTest {
     @Test
     @DisplayName("정적_팩토리가_인스턴스를_생성하는지_확인한다")
     void from() throws Exception {
-        RacingGame game = RacingGame.of(strategy, 3, 3);
+        RacingGame game = RacingGame.init(strategy, "pobi,crong,honux", 3);
         assertThat(game).isNotNull().isInstanceOf(RacingGame.class);
     }
 
     @Test
     @DisplayName("자동차와_시도할_회수는_3회씩_결과_문자열은_1글자_이상일_것이다")
     void run() throws Exception {
-        String actual = RacingGame.of(strategy, 3, 3).run();
+        String actual = RacingGame.init(strategy, "pobi,crong,honux", 3).run();
         assertThat(actual.length()).isGreaterThanOrEqualTo(1);
     }
 
     @Test
     @DisplayName("자동차는_반드시_2대_이상이어야_한다")
-    void racerCountException() throws Exception {
-        assertThatThrownBy(() -> RacingGame.of(strategy, 1, 2))
+    void racerCountExceptionV1() throws Exception {
+        assertThatThrownBy(() -> RacingGame.init(strategy, "pobi", 2))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차는 2대 이상이어야 합니다");
+                .hasMessage("자동차 이름은 2개 이상이어야 하고, 공백이 있으면 안되며 쉼표(,)로 구분되어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("자동차는_반드시_2대_이상이어야_한다_아무것도_입력하지_않은_경우")
+    void racerCountExceptionV2() throws Exception {
+        assertThatThrownBy(() -> RacingGame.init(strategy, null, 2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 2개 이상이어야 하고, 공백이 있으면 안되며 쉼표(,)로 구분되어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("쉼표만_입력한_경우")
+    void racerCountExceptionV3() throws Exception {
+        assertThatThrownBy(() -> RacingGame.init(strategy, ",", 2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 2개 이상이어야 하고, 공백이 있으면 안되며 쉼표(,)로 구분되어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("공백이_입력된_경우")
+    void racerCountExceptionV4() throws Exception {
+        assertThatThrownBy(() -> RacingGame.init(strategy, " , ", 2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 2개 이상이어야 하고, 공백이 있으면 안되며 쉼표(,)로 구분되어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("자동차_이름이_중복된_경우")
+    void racerCountExceptionV5() throws Exception {
+        assertThatThrownBy(() -> RacingGame.init(strategy, "pobi,pobi", 2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 중복 될 수 없습니다.");
     }
 
     @Test
     @DisplayName("스테이지는_반드시_1_이상이어야_한다")
     void stageCountException() throws Exception {
-        assertThatThrownBy(() -> RacingGame.of(strategy, 2, 0))
+        assertThatThrownBy(() -> RacingGame.init(strategy, "pobi,crong,honux", 0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("시도 횟수는 1회 이상이어야 합니다");
     }
