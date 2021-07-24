18895cc8a48ac02bf3289cbaa369ccb71e92518d
jaeyoungjang2 <lion85488@gmail.com>
2021-07-24 18:09:43 +0900

    move: step2결과물 위치 이동

--- src/main/java/step2/calculator/Solution.java
+++ src/main/java/step2/calculator/Solution.java
@@ -7,7 +7,6 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.run();
-
     }
     private void run(){
         /*
