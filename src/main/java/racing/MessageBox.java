529c66bfe2f0d0344670234337839d08a31f887f
PIH6585 <pih6585@naver.com>
2021-07-24 11:27:17 +0900

    refactor : message 기능 추가

--- src/main/java/racing/MessageBox.java
+++ src/main/java/racing/MessageBox.java
@@ -2,28 +2,35 @@ package racing;
 
 public class MessageBox {
 
+	private static final String FIRST_REQUEST = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+	private static final String SECOND_REQUEST = "시도할 회수는 몇회인가요?";
+	private static final String WINNER_MESSAGE = " 가 최종 우승했습니다.";
+
+	public void firstRequestMessage() {
+		System.out.println(FIRST_REQUEST);
+	}
+
+	public void secondRequestMessage() {
+		System.out.println(SECOND_REQUEST);
+	}
+
+	public void winnerMessageBox(String winner) {
+		System.out.println(winner + WINNER_MESSAGE);
+	}
+
 	public void commonMessageBox(String message) {
 		System.out.println(message);
 	}
 
 	public void racingResultMessage(String playerName, int racingResult) {
-		String result = stringBufferToString(getRacingResultByString(racingResult));
-		commonMessageBox(playerName + result);
+		commonMessageBox(playerName + getRacingResultByString(racingResult));
 	}
 
-	private StringBuffer getRacingResultByString(int racingResult) {
-		StringBuffer result = new StringBuffer(racingResult);
+	private String getRacingResultByString(int racingResult) {
+		String result = "";
 		for (int i = 0; i < racingResult; i++) {
-			result.append("-");
+			result = result.concat("-");
 		}
 		return result;
-	}
-
-	private String stringBufferToString(StringBuffer stringBufferValue) {
-		return stringBufferValue.toString();
-	}
-
-	public void commonMessageBox(String winPlayers, String winner_message) {
-		commonMessageBox(winPlayers + winner_message);
 	}
 }
