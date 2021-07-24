37b7bd23f018033245d7ba54a217c3fbd3678fdf
longbeom <wowow1859@naver.com>
2021-07-24 13:17:35 +0900

    refactor : BinaryOperator -> IntBinaryOperator 변경, 나누기 검증 분리

--- src/main/java/step2/OperatorType.java
+++ src/main/java/step2/OperatorType.java
@@ -1,23 +1,32 @@
 package step2;
 
+import static step2.Validator.*;
+
 import java.util.Arrays;
-import java.util.function.BinaryOperator;
+import java.util.function.IntBinaryOperator;
 
 public enum OperatorType {
-	PLUS("+", (number1, number2) -> number1 + number2),
+	PLUS("+", Integer::sum),
 	MINUS("-", (number1, number2) -> number1 - number2),
 	MULTIPLY("*", (number1, number2) -> number1 * number2),
-	DIVIDE("/", (number1, number2) -> number1 / number2);
+	DIVIDE("/", (number1, number2) -> {
+		if (number2 == 0) {
+			throw new IllegalArgumentException("0으로 나눌 수 없어요!");
+		}
+		return number1 / number2;
+	});
 
 	private String operator;
-	private BinaryOperator<Integer> expression;
+	private IntBinaryOperator calculationMethod;
 
-	OperatorType(String operator, BinaryOperator<Integer> expression) {
+	OperatorType(String operator, IntBinaryOperator calculationMethod) {
 		this.operator = operator;
-		this.expression = expression;
+		this.calculationMethod = calculationMethod;
 	}
 
 	public static OperatorType getByOperator(String operator) {
+		validateOperator(operator);
+
 		return Arrays.stream(values())
 			.filter(operatorType -> operatorType.operator.equals(operator))
 			.findFirst()
@@ -25,9 +34,6 @@ public enum OperatorType {
 	}
 
 	public int calculateByOperator(int number1, int number2) {
-		if (operator.equals(DIVIDE.operator) && number2 == 0) {
-			throw new IllegalArgumentException("0으로 나눌 수 없어요!");
-		}
-		return expression.apply(number1, number2);
+		return calculationMethod.applyAsInt(number1, number2);
 	}
 }
