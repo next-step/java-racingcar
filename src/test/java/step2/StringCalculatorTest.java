7ffba1b9101c726a804e9652cf658316635c72f9
조원진 <>
2021-07-24 10:26:57 +0900

    refactor(StringCalculator) : calculate 메소드 refactoring

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -10,28 +10,28 @@ public class StringCalculatorTest {
     @DisplayName("덧셈 테스트")
     @Test
     void plus() {
-        int result = StringCalculator.calculate("2 + 3");
+        int result = StringCalculator.calculate(new Formulas("2 + 3"));
         assertThat(result).isEqualTo(5);
     }
 
     @DisplayName("뺄셈 테스트")
     @Test
     void minus() {
-        int result = StringCalculator.calculate("5 - 2");
+        int result = StringCalculator.calculate(new Formulas("5 - 2"));
         assertThat(result).isEqualTo(3);
     }
 
     @DisplayName("곱셈 테스트")
     @Test
     void multiply() {
-        int result = StringCalculator.calculate("3 * 6");
+        int result = StringCalculator.calculate(new Formulas("3 * 6"));
         assertThat(result).isEqualTo(18);
     }
 
     @DisplayName("나눗셈 테스트")
     @Test
     void divide() {
-        int result = StringCalculator.calculate("4 / 2");
+        int result = StringCalculator.calculate(new Formulas("4 / 2"));
         assertThat(result).isEqualTo(2);
     }
 }
