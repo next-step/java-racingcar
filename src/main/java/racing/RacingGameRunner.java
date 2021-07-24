e5b1c2db635c02df3d05096ed905395c1642d7a3
shirohoo <hch4102@naver.com>
2021-07-24 23:31:36 +0900

    refactor: 호출자 변경

--- src/main/java/racing/RacingGameRunner.java
+++ src/main/java/racing/RacingGameRunner.java
@@ -1,16 +1,16 @@
 package racing;
 
-import racing.domain.NumberMoreThanFourConditionStrategy;
+import racing.strategy.NumberMoreThanFourConditionStrategy;
 import racing.domain.RacingGame;
 
 import static racing.util.RacingGamePrinter.print;
-import static racing.util.RacingGameScanner.enter;
+import static racing.util.RacingGameScanner.enterNames;
+import static racing.util.RacingGameScanner.enterStageCount;
 
 public class RacingGameRunner {
     public static void main(String[] args) {
         NumberMoreThanFourConditionStrategy strategy = NumberMoreThanFourConditionStrategy.getInstance();
-        int[] enter = enter();
-        RacingGame game = RacingGame.of(strategy, enter[0], enter[1]);
+        RacingGame game = RacingGame.init(strategy, enterNames(), enterStageCount());
         print(game.run());
     }
 }
