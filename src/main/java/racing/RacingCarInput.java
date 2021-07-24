15b70dc2084bf072229d02a2b5d59b22cd5b953f
jaeyoungjang2 <lion85488@gmail.com>
2021-07-24 18:08:58 +0900

    feat: Input 관련 기능 추가 Input을 받아들이고, 받은 Input이 range안에 존재하는 값인지 확인하는 기능 추가

--- src/main/java/racing/RacingCarInput.java
+++ src/main/java/racing/RacingCarInput.java
@@ -1,2 +1,29 @@
-package racing;public class RacingCarInput {
+package racing;
+
+import java.util.Scanner;
+
+public class RacingCarInput {
+    public void requestInput() throws IllegalArgumentException{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        validInputCheck_NumOfCar(sc.nextInt());
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        validInputCheck_CycleOfRacing(sc.nextInt());
+    }
+
+    public void validInputCheck_NumOfCar(int cnt) {
+        final String errorMessage = "자동차의 수가 너무 적거나 많습니다. (range: 1~5)";
+        final int threthold = 5;
+        if (0 >= cnt || threthold < cnt ) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    public void validInputCheck_CycleOfRacing(int cnt) {
+        final String errorMessage = "시도할 회수가 너무 적거나 많습니다. (range: 1~10)";
+        final int threthold = 10;
+        if (0 >= cnt || threthold < cnt ) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
 }
