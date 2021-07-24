2b55ef8590fcafe778f3f719c001b3183f963b87
Giwan Kim <giwankim@gmail.com>
2021-07-24 17:05:12 +0900

    feat(racingcar): Add Car class
    Car class represents a car.

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,44 @@
+package racingcar.domain.car;
+
+import java.util.Objects;
+
+public class Car {
+
+    private Position position;
+
+    public Car() {
+        this(0);
+    }
+
+    public Car(int position) {
+        this.position = new Position(position);
+    }
+
+    public Car(Position position) {
+        this.position = position;
+    }
+
+    public Car move(MoveStrategy moveStrategy) {
+        if (moveStrategy.canMove()) {
+            return new Car(position.move());
+        }
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Car)) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(position, car.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
