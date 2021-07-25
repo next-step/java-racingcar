6e1e42a21c771b47844bed3bff9e0809d316a39c
yook0328 <yook0328@gmail.com>
2021-07-25 20:03:23 +0900

    test: String 클래스 요구사항 2

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,6 +1,9 @@
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+    @Test
     public void split01() {
         //given
         String given = "1,2";
@@ -10,6 +13,7 @@ public class StringTest {
         assertThat(split).containsExactly("1", "2");
     }
 
+    @Test
     public void split02() {
         //given
         String given = "1";
@@ -17,6 +21,17 @@ public class StringTest {
         String[] split = given.split(",");
         //then
         assertThat(split).contains("1");
+    }
+
+    @Test
+    public void substring() {
+        //given
+        String given = "(1,2)";
+        //when
+        String substring = given.substring(1, 4);
+
+        //then
+        assertThat(substring).isEqualTo("1,2");
 
     }
 
