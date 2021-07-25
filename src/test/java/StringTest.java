301fdde9c6003c1426bb9679b4a5112bbf7445d6
yook0328 <yook0328@gmail.com>
2021-07-25 20:09:38 +0900

    test: String 클래스 요구사항 3

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,9 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
+    @DisplayName("문자열 분리 테스트 01")
     public void split01() {
         //given
         String given = "1,2";
@@ -14,6 +17,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열 분리 테스트 02")
     public void split02() {
         //given
         String given = "1";
@@ -24,6 +28,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("문자열 자르기 테스트")
     public void substring() {
         //given
         String given = "(1,2)";
@@ -35,4 +40,26 @@ public class StringTest {
 
     }
 
+    @Test
+    @DisplayName("문자열 꺼내기 테스트")
+    public void charAt01() {
+        //given
+        String given = "abc";
+
+        //then
+        assertThat(given.charAt(0)).isEqualTo('a');
+        assertThat(given.charAt(1)).isEqualTo('b');
+        assertThat(given.charAt(2)).isEqualTo('c');
+
+    }
+
+    @Test
+    @DisplayName("문자열 범위 밖 꺼낼 시 Exception 테스트")
+    public void charAt02() {
+        //given
+        String given = "abc";
+
+        assertThatThrownBy(() -> given.charAt(3))
+                .isExactlyInstanceOf(StringIndexOutOfBoundsException.class);
+    }
 }
