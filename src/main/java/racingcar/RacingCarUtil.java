60d08fd8853baf7a9b28ab94ddd2340426ec884f
ChangSubKwak <paradise1732@gmail.com>
2021-07-25 21:58:26 +0900

    fix: 함수명 한글로 변경, boolean(Wrapper Type에서 Primitive Type 변경)

--- src/main/java/racingcar/RacingCarUtil.java
+++ src/main/java/racingcar/RacingCarUtil.java
@@ -2,9 +2,9 @@ package racingcar;
 
 import java.util.Random;
 
-public class Util {
-	public static int MIN_VALUE = 0;
-	public static int MAX_VALUE = 9;
+public class RacingCarUtil {
+	public static final int MIN_VALUE = 0;
+	public static final int MAX_VALUE = 9;
 
 	private static Random random = new Random();
 
