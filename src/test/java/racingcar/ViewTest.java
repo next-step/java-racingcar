69b97ccf7d2648af49a7504e4b2b68d668222895
shryu <>
2021-07-25 16:35:11 +0900

    test : 우승자 로직 수정으로 인한 테스트 불가로 삭제

--- src/test/java/racingcar/ViewTest.java
+++ src/test/java/racingcar/ViewTest.java
@@ -19,18 +19,4 @@ public class ViewTest {
         Cars cars = new Cars(carStrArr);
         ResultView.printMovingCars(cars);
     }
-
-    @Test
-    @DisplayName("우승자 출력")
-    void printWinners(){
-        String[] carStrArr = Arrays.array("pobi", "crong", "honux");
-        Cars cars = new Cars(carStrArr);
-        List<Car> carsList = cars.getCars();
-        Car winnerCar = carsList.get(0);
-        for(int i=0; i<5; i++){
-            winnerCar.move(4);
-        }
-        Winners winners = new Winners(cars);
-        ResultView.printWinners(winners.getWinnerNames());
-    }
 }
