a626eb473482a7a550ca14571d41419385642597
darkant99 <darkant99@naver.com>
2021-07-25 17:28:01 +0900

    [TEST] Turn 객체 테스트 생성

--- src/test/java/racing/domain/game/vo/turn/TurnTest.java
+++ src/test/java/racing/domain/game/vo/turn/TurnTest.java
@@ -1,7 +1,25 @@
 package racing.domain.game.vo.turn;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class TurnTest {
+    @DisplayName("Turn ctor Test")
+    @Test
+    public void ctorTest() {
+        assertThat(new Turn().isWaiting())
+                .isTrue();
+    }
 
+    @DisplayName("Turn Finish Test")
+    @Test
+    public void finishTest() {
+        Turn turn = new Turn();
+        turn.finish();
+        assertThat(turn.isWaiting())
+                .isFalse();
+    }
 }
