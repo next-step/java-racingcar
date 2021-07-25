05956995b458960f094629e025e2745b57ba7a4c
abcms031 <mjkim031@gmail.com>
2021-07-25 11:38:20 +0900

    refactor: ui - 입력 수정

--- src/main/java/racing/ui/InputView.java
+++ src/main/java/racing/ui/InputView.java
@@ -3,23 +3,21 @@ package racing.ui;
 import java.util.Scanner;
 
 public class InputView {
-
     private static final String RACING_CAR_MESSAGE = "자동차 대수는 몇 대 인가요?";
     private static final String RACING_GAME_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private final Scanner RACING_SCANNER = new Scanner(System.in);
+
 
     private int racingCar;
-
     private int racingRound;
 
-
     public InputView() {
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println(RACING_CAR_MESSAGE);
-        this.racingCar = scanner.nextInt();
+        this.racingCar = RACING_SCANNER.nextInt();
 
         System.out.println(RACING_GAME_MESSAGE);
-        this.racingRound = scanner.nextInt();
+        this.racingRound = RACING_SCANNER.nextInt();
     }
 
     public int getRacingCar() {
