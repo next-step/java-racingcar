2dda7e7a14e928c27f3258014fcf2be924f3123a
sangw0804 <sangw0804@likelion.org>
2021-07-25 17:19:34 +0900

    fix(StringCalculator): 덧셈 테스트 성공

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -2,6 +2,13 @@ package stringcalculator;
 
 public class StringCalculator {
     public int calculate(String input) {
-        return 0;
+        String[] args = input.split(" ");
+
+        int result = Integer.parseInt(args[0]);
+        for (int i = 1; i < args.length; i += 2) {
+            result += Integer.parseInt(args[i + 1]);
+        }
+
+        return result;
     }
 }
