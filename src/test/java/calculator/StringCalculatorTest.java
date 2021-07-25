d8c1f3637bdafaa6f08e08ee207845c44542684c
sixthou <justyuk@naver.com>
2021-07-25 20:58:34 +0900

    feat : 통합 테스트 구현

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -6,33 +6,33 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class StringCalcuratorTest {
+public class StringCalculatorTest {
 
     @DisplayName("덧셈")
     @Test
     void 덧셈() {
-        int result = StringCalcurator.cal("2 + 3");
+        int result = StringCalculator.cal("2 + 3");
         assertThat(result).isEqualTo(5);
     }
 
     @DisplayName("뺄셈")
     @Test
     void 뺄셈() {
-        int result = StringCalcurator.cal("5 - 2");
+        int result = StringCalculator.cal("5 - 2");
         assertThat(result).isEqualTo(3);
     }
 
     @DisplayName("곱셈")
     @Test
     void 곱셈() {
-        int result = StringCalcurator.cal("5 * 2");
+        int result = StringCalculator.cal("5 * 2");
         assertThat(result).isEqualTo(10);
     }
 
     @DisplayName("나눗셈")
     @Test
     void 나눗셈() {
-        int result = StringCalcurator.cal("10 / 2");
+        int result = StringCalculator.cal("10 / 2");
         assertThat(result).isEqualTo(5);
     }
 
@@ -40,26 +40,26 @@ public class StringCalcuratorTest {
     @Test
     void 입력_값_null_체크() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> StringCalcurator.cal(null));
+                .isThrownBy(() -> StringCalculator.cal(null));
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> StringCalcurator.cal(""));
+                .isThrownBy(() -> StringCalculator.cal(""));
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> StringCalcurator.cal(" "));
+                .isThrownBy(() -> StringCalculator.cal(" "));
     }
 
     @DisplayName("사칙연산 기호 체크")
     @Test
     void 사칙연산_기호_체크() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(()->StringCalcurator.cal("10 ? 2"));
+                .isThrownBy(()-> StringCalculator.cal("10 ? 2"));
     }
 
-    // 사칙 연산을 모두 포함하는 기능 구현
+    @DisplayName("통합 기능 테스트")
     @Test
     void 모든_기능() {
-
+        assertThat(StringCalculator.cal("2 + 3 * 4 / 2")).isEqualTo(10);
     }
 
 }
