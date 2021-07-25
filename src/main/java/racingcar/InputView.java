5b13a6d06b3533bea67862422a84d64c1e8a409b
ChangSubKwak <paradise1732@gmail.com>
2021-07-26 00:03:09 +0900

    fix: 함수를 static에서 일반함수로 변경, scanner변수를 한 번만 생성하도록 변경, scannerClose함수 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,18 +3,23 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
-	private static int inputIntValue() {
-		Scanner scanner = new Scanner(System.in);
+	private static Scanner scanner = new Scanner(System.in);
+
+	private int inputIntValue() {
 		return scanner.nextInt();
 	}
 
-	public static int inputRacingCarNum() {
+	public int inputRacingCarNum() {
 		System.out.println("자동차 대수는 몇 대 인가요?");
 		return inputIntValue();
 	}
 
-	public static int inputTryCount() {
+	public int inputTryCount() {
 		System.out.println("시도할 회수는 몇 회 인가요?");
 		return inputIntValue();
+	}
+
+	public void scannerClose() {
+		scanner.close();
 	}
 }
