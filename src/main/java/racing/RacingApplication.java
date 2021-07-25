2c8d5145192fa2fbb77581ef6fc91ca57e37c8d3
abcms031 <mjkim031@gmail.com>
2021-07-25 12:44:12 +0900

    refactor : 메인 메소드 수정

--- src/main/java/racing/RacingApplication.java
+++ src/main/java/racing/RacingApplication.java
@@ -1,14 +1,24 @@
 package racing;
 
+import racing.domain.Racing;
+import racing.service.RacingService;
 import racing.ui.InputView;
 import racing.ui.ResultView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingApplication {
+    private static List<Racing> racingList = new ArrayList<>();
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
-        resultView.result(inputView.getRacingRound(), inputView.getRacingCar());
 
+        int racingCar = inputView.getRacingCar();
+        int racingRound = inputView.getRacingRound();
+
+        racingList = RacingService.exec(racingRound, racingCar);
+        resultView.result(racingList, racingCar);
     }
 }
