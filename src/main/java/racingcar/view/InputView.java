26df0abfac650f2a31d093942988963b9f14a305
harris <harrisleesh@gmail.com>
2021-07-25 17:10:31 +0900

    feat : make InputView

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+public class InputView {
+    public void requestCarNumber() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    public void requestTryNumber() {
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+    }
+}
