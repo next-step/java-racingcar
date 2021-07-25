8cf751023c904e6decc144e9a0984e2e3f3d7264
abcms031 <mjkim031@gmail.com>
2021-07-25 12:26:36 +0900

    refactor : Random 정적 객체로 변경 및 입력값 유효성 체크 기능 추가, 테스트코드 경계값 분석 적용

--- src/main/java/racing/utils/RacingUtils.java
+++ src/main/java/racing/utils/RacingUtils.java
@@ -6,17 +6,25 @@ public class RacingUtils {
 
     public static final int RANDOM_NUMBER_RULE = 4;
     public static final int RANDOM_NUMBER_LIMIT = 10;
-
+    public static final int INPUT_VALUE_RULE = 0;
+    private static final Random RANDOM_NUMBER = new Random();
 
     public static int randomNumber(){
-        return new Random().nextInt(RANDOM_NUMBER_LIMIT);
+        return RANDOM_NUMBER.nextInt(RANDOM_NUMBER_LIMIT);
     }
 
     public static boolean compareNumbers(int number){
-        if(number < RANDOM_NUMBER_RULE){
-            return false;
-        }
-        return true;
+        return number >= RANDOM_NUMBER_RULE;
     }
+
+    public static void checkInputValue(int round, int car){
+        if(round <= INPUT_VALUE_RULE){
+            throw new IllegalArgumentException("경주 횟수는 0 이상으로 입력되어야합니다.");
+        }
+        if(car <= INPUT_VALUE_RULE){
+            throw new IllegalArgumentException("경주 횟수는 0 이상으로 입력되어야합니다.");
+        }
+    }
+
 
 }
--- src/test/java/racing/utils/RacingUtilsTest.java
+++ src/test/java/racing/utils/RacingUtilsTest.java
@@ -1,7 +1,9 @@
 package racing.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import calculator.operator.Operator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,9 +20,18 @@ class RacingUtilsTest {
 
     @ParameterizedTest
     @DisplayName("해당 숫자가 경주 규칙-숫자가 4이상-에 따라 전진이 가능한지 확인")
-    @CsvSource(value = {"1:false", "2:false", "4:true", "10:true"}, delimiter = ':')
+    @CsvSource(value = {"3:false", "4:true", "5:true"}, delimiter = ':')
     void compareNumbers(int number, boolean expected) {
         boolean result = RacingUtils.compareNumbers(number);
         assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("경주 횟수 및 자동차 대 수 입력값의 예외 출력 확인")
+    @CsvSource(value = {"0:1", "1:0","0:0"}, delimiter = ':')
+    void checkInputValue(int round, int car) {
+        assertThatThrownBy(
+                () ->  RacingUtils.checkInputValue(round, car)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
