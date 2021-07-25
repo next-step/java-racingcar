970f4b008cc7eeb8bc0c79ef3ad869c8e17e4a29
shryu <>
2021-07-25 15:06:27 +0900

    refactor : 필요없는 메소드 삭제

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,7 +1,5 @@
 package racingcar.domain;
 
-import java.util.Objects;
-
 public class CarName {
     private String name;
 
@@ -14,18 +12,5 @@ public class CarName {
 
     public String getName() {
         return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CarName carName = (CarName) o;
-        return Objects.equals(name, carName.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 }
