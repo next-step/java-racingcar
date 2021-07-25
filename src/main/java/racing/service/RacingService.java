f3169cb1a67c184b3dd8d84e3625245d4b3a2dbf
abcms031 <mjkim031@gmail.com>
2021-07-25 12:42:28 +0900

    feat : racingservice 추가

---
+++ src/main/java/racing/service/RacingService.java
@@ -0,0 +1,34 @@
+package racing.service;
+
+import racing.domain.Racing;
+import racing.utils.RacingUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingService {
+
+    public static boolean randomPlay() {
+        int randomNumber = RacingUtils.randomNumber();
+        return RacingUtils.compareNumbers(randomNumber);
+    }
+
+    public static boolean[] race(int cars) {
+        boolean[] racingResult = new boolean[cars];
+        for (int i = 0; i < cars; i++) {
+            racingResult[i] = randomPlay();
+        }
+        return racingResult;
+    }
+
+    public static List<Racing> exec(int racingRound, int racingCar){
+
+        RacingUtils.checkInputValue(racingRound, racingCar);
+        List<Racing> racingList = new ArrayList<>();
+        for (int i = 0; i < racingRound; i++) {
+            Racing racing = new Racing(race(racingCar));
+            racingList.add(i, racing);
+        }
+        return racingList;
+    }
+}
