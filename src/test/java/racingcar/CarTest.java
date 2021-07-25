e0c4bc7502ff4d6bd7902ed9669b43869dea6261
shineoov <shineoov88@gmail.com>
2021-07-25 12:52:51 +0900

    test: 자동차 경주 테스트 추가 및 displayname 설정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,13 +1,16 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("자동차 테스트")
 class CarTest {
 
     @Test
-    void carAdeTest() {
+    @DisplayName("자동차 전진 테스트")
+    void carMoveTest() {
         Car car = new Car();
 
         car.move();
--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -4,21 +4,38 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("레이싱 테스트")
+@DisplayName("자동차 경주 테스트")
 class RacingTest {
 
     private Racing racing;
+    private ArrayList<ArrayList<RacingRecord>> racingHistory;
 
     @BeforeEach
     void setUp() {
         racing = new Racing(3);
+        racing.start(5);
+        racingHistory = racing.getRacingHistory();
     }
 
     @Test
     @DisplayName("자동차 경주에 참여하는 자동차 대수 확인")
-    void racingCarCheck() {
+    void racingCarCountCheckTest() {
         assertThat(racing.getCars().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("자동차 경주에서 주어진 횟수 만큼 경주내역이 기록되는지 확인")
+    void racingHistoryCheckTest() {
+        assertThat(racingHistory.size()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("자동차 경주내역에 올바른 자동차 대수가 기록되는지 확인")
+    void racingHistoryCheckForCarCountTest() {
+        assertThat(racingHistory.get(0).size()).isEqualTo(3);
     }
 }
