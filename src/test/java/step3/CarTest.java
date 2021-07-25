676192ce4409247bf1db641f2d49f08d997463f2
sk1737030 <sk1737030@naver.com>
2021-07-25 15:07:32 +0900

    test: parameterized추가로 중복 코드 제거

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -1,26 +1,19 @@
 package step3;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 class CarTest {
 
-    @Test
-    void 주어진_숫자가_특정RPM이상이면_자동차의_moveCount는_증가한다() {
-        int givenRpm = 4;
+    @CsvSource(value = {"4:1", "3:0"}, delimiter = ':')
+    @ParameterizedTest(name = "주어진_숫자가_{0}RPM이면_자동차의_moveCount는_{1}이_된다")
+    void 주어진_숫자가_특정RPM이상이면_자동차의_moveCount는_증가한다(int givenRpm, int givenMoveCount) {
         Car car = new Car(new CarEngine());
         car.move(givenRpm);
 
-        assertThat(car.getMovedCount()).isEqualTo(1);
+        assertThat(car.getMovedCount()).isEqualTo(givenMoveCount);
     }
 
-    @Test
-    void 주어진_숫자가_특정RPM_이하이면_자동차의_moveCount는_변동이_없다() {
-        int givenRpm = 3;
-        Car car = new Car(new CarEngine());
-        car.move(givenRpm);
-
-        assertThat(car.getMovedCount()).isEqualTo(0);
-    }
 }
