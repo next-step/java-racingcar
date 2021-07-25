27f38175b87357ebb85f7f89d0929ee29da3467c
abcms031 <mjkim031@gmail.com>
2021-07-25 17:51:41 +0900

    refactor : 안쓰는 import 제거

--- src/test/java/racing/service/RacingServiceTest.java
+++ src/test/java/racing/service/RacingServiceTest.java
@@ -1,6 +1,5 @@
 package racing.service;
 
-import calculator.service.StringCalculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
