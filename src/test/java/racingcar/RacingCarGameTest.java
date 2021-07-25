896e9aa36ee4651c6ed59f01fe87dfba1a22b412
harris <harrisleesh@gmail.com>
2021-07-25 16:43:43 +0900

    refactor : Add setUp() Initialize RacingCars by carNumber

--- src/test/java/racingcar/RacingCarGameTest.java
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import racingcar.entity.RacingCar;
 
@@ -12,13 +14,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RacingCarGameTest {
     private final List<RacingCar> racingCars = new ArrayList<>();
+    public static final int carNumber = 3;
+    public static final int tryNumber = 5;
+
+    @BeforeEach
+    void setUp() {
+        RacingCarGame.initializeRacingCars(racingCars, carNumber);
+    }
 
     @Test
     @DisplayName("레이싱 카의 차량대수로 초괴화를 했을 때 리스트의 사이즈가 변하는 것을 확인한다.")
     void initializeRacingCars() {
-        assertThat(racingCars.size()).isZero();
-        int carNumber = 5;
-        RacingCarGame.initializeRacingCars(racingCars, carNumber);
         assertThat(racingCars.size()).isEqualTo(carNumber);
     }
 
