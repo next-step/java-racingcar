1d33418096657e3c660e98a81ee5d82f3e3464f4
jspark940321 <jspark940321@gmail.com>
2021-07-25 14:01:40 +0900

    feat: 입력, 출력 객체 추가 및 분리

--- src/main/java/Car/RacingCar.java
+++ src/main/java/Car/RacingCar.java
@@ -14,27 +14,39 @@ public class RacingCar {
     private final int MINMOVENUMBER = 4;
 
     public void gameStart() {
-        getInput();
+        new RacingCarInput().getInput();
         setCarsResult();
         for (int i = 1; i < gameCount; i++) {
             doGame();
             setGameResults(i);
         }
-        printGameResult();
+        new RacingCarOutput().printGameResult();
     }
 
-    private void getInput() {
-        Scanner scanner = new Scanner(System.in);
-        try {
-            System.out.println("자동차 대수는 몇 대 인가요?");
-            carCount = scanner.nextInt();
-            System.out.println("시도할 회수는 몇 회 인가요?");
-            gameCount = scanner.nextInt();
-        } catch (InputMismatchException e) {
-            throw new IllegalArgumentException("숫자 값을 입력해주세요.");
+    private class RacingCarInput{
+        private void getInput() {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                System.out.println("자동차 대수는 몇 대 인가요?");
+                carCount = scanner.nextInt();
+                System.out.println("시도할 회수는 몇 회 인가요?");
+                gameCount = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                throw new IllegalArgumentException("숫자 값을 입력해주세요.");
+            }
+            if (carCount <= 0 || gameCount <= 0) {
+                throw new IllegalArgumentException("0보다 큰 값을 입력해주세요.");
+            }
         }
-        if (carCount <= 0 || gameCount <= 0) {
-            throw new IllegalArgumentException("0보다 큰 값을 입력해주세요.");
+    }
+
+    private class RacingCarOutput{
+        private void printGameResult() {
+            System.out.println();
+            System.out.println("실행 결과");
+            for (int i = 0; i < gameCount; i++) {
+                System.out.println(gameResults[i]);
+            }
         }
     }
 
@@ -69,14 +81,6 @@ public class RacingCar {
         gameResults[index] = "";
         for (int i = 0; i < carCount; i++) {
             gameResults[index] += (cars[i].getStatus() + "\n");
-        }
-    }
-
-    private void printGameResult() {
-        System.out.println();
-        System.out.println("실행 결과");
-        for (int i = 0; i < gameCount; i++) {
-            System.out.println(gameResults[i]);
         }
     }
 }
