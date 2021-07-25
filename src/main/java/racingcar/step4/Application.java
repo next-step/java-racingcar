905101f1b4225deecce78dca1ef4122d0e101e7c
gunny6026 <gunny6026@naver.com>
2021-07-25 19:27:02 +0900

    feat : 입력 UI 클래스 구현

---
+++ src/main/java/racingcar/step4/Application.java
@@ -0,0 +1,8 @@
+package racingcar.step4;
+
+public class Application {
+    public static void main(String[] args) {
+        String[] carNames = InputView.InputCarNames().split(",");
+        int tryCount = InputView.InputTryCount();
+    }
+}
---
+++ src/main/java/racingcar/step4/Car.java
@@ -0,0 +1,4 @@
+package racingcar.step4;
+
+public class Car {
+}
