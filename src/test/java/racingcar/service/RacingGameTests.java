45414c558ca2e90231322442dcb25793896a08ff
syd9538 <syd9538@gmail.com>
2021-07-25 16:17:55 +0900

    test : RacingGame test 코드 추가

--- src/test/java/racingcar/service/RacingGameTests.java
+++ src/test/java/racingcar/service/RacingGameTests.java
@@ -2,7 +2,8 @@ package racingcar.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.dto.Board;
 import racingcar.dto.RacingInfo;
 import racingcar.model.Cars;
@@ -11,25 +12,37 @@ import racingcar.strategy.impl.RandomBoundMovingStrategy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTests {
-    RacingInfo racingInfo;
-    Cars cars;
-    RacingGame racingGame;
 
-    @BeforeEach
-    void setup() {
-        racingInfo = new RacingInfo(3, 5);
-        cars = Cars.of(racingInfo.numberOfCar, new RandomBoundMovingStrategy());
-
-        racingGame = new RacingGame(racingInfo, cars);
-    }
-
-
-    @Test
     @DisplayName("자동차 경주에서 주어진 횟수 만큼 경주내역이 기록되는지 확인")
-    void racingHistoryCheckTest() {
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5})
+    void racingHistoryCheckTest(int raceTrialCount) {
+        RacingInfo racingInfo = new RacingInfo(3, raceTrialCount);
+
+        Cars cars = Cars.of(racingInfo.numberOfCar, new RandomBoundMovingStrategy());
+
+        RacingGame racingGame = new RacingGame(racingInfo, cars);
+
         Board board = racingGame.gameStart();
 
-        assertThat(board.getAllRecords().size()).isEqualTo(5);
+        assertThat(board.getAllRecords().size()).isEqualTo(raceTrialCount);
     }
+
+    @DisplayName("자동차 경주에서 주어진 자동차 대수 만큼 경주하는지 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5})
+    void racingCarsCountCheckTest(int numberOfCar) {
+        RacingInfo racingInfo = new RacingInfo(numberOfCar, 1);
+
+        Cars cars = Cars.of(racingInfo.numberOfCar, new RandomBoundMovingStrategy());
+
+        RacingGame racingGame = new RacingGame(racingInfo, cars);
+
+        Board board = racingGame.gameStart();
+
+        assertThat(board.getAllRecords().get(0).size()).isEqualTo(numberOfCar);
+
+    }
+
 
 }
