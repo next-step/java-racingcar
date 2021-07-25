766b0580547cbec348f8efcb48c1c5c1a6f59bba
ChangSubKwak <paradise1732@gmail.com>
2021-07-25 22:06:32 +0900

    fix: 함수명 한글로 변경, JUnit5 부터 적용되는 접근제어자 설정에 따른 public/private 제거

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,9 +4,9 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-public class StringTest {
+class StringTest {
 	@Test
-	void split() {
+	void split함수에_컴마를_인자로_받으면_컴마를_기준으로_나누어진다() {
 		String[] result = "1,2".split(",");
 		assertThat(result).contains("1");
 		assertThat(result).contains("2");
@@ -14,13 +14,13 @@ public class StringTest {
 	}
 
 	@Test
-	void substring() {
+	void substring함수에_시작인덱스와_길이에서_1뺀값을_넣으면_첫번째와_마지막문자가_제거된다() {
 		String input = "(1,2)";
 		assertThat(input.substring(1, input.length() - 1)).isEqualTo("1,2");
 	}
 
 	@Test
-	void charAt() {
+	void charAt함수_인자에_범위를_벗어나는_값을_넣으면_StringIndexOutOfBoundsException을_던진다() {
 		String input = "abc";
 		assertThatThrownBy(() -> {
 			input.charAt(input.length());
