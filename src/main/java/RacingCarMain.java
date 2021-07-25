738b7b7ee84641409680c55864b145e992483c89
jaeyoungjang2 <lion85488@gmail.com>
2021-07-25 16:49:58 +0900

    refactor: 코드 초기화 수정보다 처음부터 코드를 짜는것이 용이할 것으로 판단함

--- src/main/java/RacingCarMain.java
+++ src/main/java/RacingCarMain.java
@@ -4,21 +4,13 @@ import racing.RacingCarOperator;
 import java.util.Scanner;
 
 public class RacingCarMain {
+
     public static void main(String[] args) {
         RacingCarMain rcm = new RacingCarMain();
         rcm.run();
     }
 
     private void run() {
-        RacingCarInput racingCarInput = new RacingCarInput();
 
-        try {
-            int[] racingInputInfo = racingCarInput.requestInput();
-            RacingCarOperator racingCarOperator = new RacingCarOperator(racingInputInfo);
-            racingCarOperator.Run();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println("프로그램을 종료합니다.");
     }
 }
