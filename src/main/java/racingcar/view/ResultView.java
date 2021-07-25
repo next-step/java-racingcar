9b75459d683d548f744c427c0cf15e380577e846
ChangSubKwak <paradise1732@gmail.com>
2021-07-26 02:54:33 +0900

    feat: '실행 결과' 문자열 변수 선언, 출력하는 함수 추가

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -6,10 +6,16 @@ import java.util.stream.Collectors;
 import racingcar.model.RacingCar;
 
 public class ResultView {
+	private static final String EXECUTE_RESULT = "실행 결과";
+
 	public void outputResult(List<RacingCar> racingCars) {
 		String result = racingCars.stream().map(racingCar ->
 			racingCar.getRacingCarDashString()
 		).collect(Collectors.joining("\n"));
 		System.out.println(result + "\n");
+	}
+
+	public void outputStatement() {
+		System.out.println("\n" + EXECUTE_RESULT);
 	}
 }
