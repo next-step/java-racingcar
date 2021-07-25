e026466c79150822de969bf6aa2cf3e7cbb8b83c
jayyhkwon <55421900+jayyhkwon@users.noreply.github.com>
2021-07-25 19:38:00 +0900

    Step2 문자열 계산기 리뷰 요청드립니다. (#2419)
    * Step2 #time 1h 올바르지 않은 input validation
    
    * Step2 #time 10m Operator 인터페이스 추가
    
    * Step2 #time 30m Operator 인터페이스 리팩토링
    
    * Step2 #time 1h Calculator 리팩토링
    
    * Step2 #time 1h 코드리뷰 반영
     -1. Calculator.calculate() 인텐트 줄이기
     -2. Operator 인터페이스 -> Enum으로 변경

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,72 @@
+import enums.Operator;
+
+import java.util.Objects;
+
+
+public class Calculator {
+
+    public int calculate(String input) {
+        String[] inputSplitByBlank = splitByBlank(input);
+        Validator.valid(inputSplitByBlank);
+        return calculate(inputSplitByBlank);
+    }
+
+    private int calculate(String[] inputs) {
+        int calculated = Integer.parseInt(inputs[0]);
+        int length = inputs.length;
+
+        for (int i = 0; i < length; i++)
+            calculated = calculate(calculated, inputs, i);
+
+        return calculated;
+    }
+
+    private int calculate(int calculated, String[] inputs, int idx) {
+        if (isLastIdx(inputs, idx))
+            return calculated;
+
+        String input = inputs[idx];
+        String nextValue = inputs[idx + 1];
+
+        if (Operator.isOperator(input)) {
+            Operator operator = Operator.findOperatorByString(input);
+            return operator.calculate(calculated, Integer.parseInt(nextValue));
+        }
+        return calculated;
+    }
+
+    private boolean isLastIdx(String[] inputs, int idx) {
+        return inputs.length == idx + 1;
+    }
+
+    private String[] splitByBlank(String input) {
+        if (Objects.isNull(input) || input.isEmpty())
+            throw new IllegalArgumentException();
+
+        return input.split("\\s");
+    }
+
+    private static class Validator {
+
+        static void valid(String[] inputs) {
+            for (String input : inputs) {
+                if (!isValidInput(input))
+                    throw new IllegalArgumentException();
+            }
+        }
+
+        private static boolean isValidInput(String input) {
+            return isInteger(input) || Operator.isOperator(input);
+        }
+
+        private static boolean isInteger(String input) {
+            try {
+                Integer.parseInt(input);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @ParameterizedTest
+    @DisplayName("입력 값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw")
+    @NullSource
+    @EmptySource
+    void inputValidation(String input) {
+        assertThrows(IllegalArgumentException.class, () -> calculator.calculate(input));
+    }
+
+    @ParameterizedTest
+    @DisplayName("사칙연산 기호가 아닌 경우 IllegalArgumentException throw")
+    @ValueSource(strings = {"2 ^ 3", "2 & 3", "2 ( 3"})
+    void invalidOperationSymbolValidation(String input) {
+        assertThrows(IllegalArgumentException.class, () -> calculator.calculate(input));
+    }
+
+    @ParameterizedTest
+    @DisplayName("사칙연산 기호인 경우 예외가 발생하지 않는다")
+    @ValueSource(strings = {"2 + 3", "2 - 3", "2 * 3 ", "2 / 3"})
+    void validOperationSymbolValidation(String input) {
+        assertDoesNotThrow(() -> calculator.calculate(input));
+    }
+
+    @ParameterizedTest
+    @DisplayName("실제 입력과 결과를 검증한다")
+    @CsvSource({"2 + 3 * 4 / 2,10", "5 * 20 / 2 + 3,53"})
+    void validOperationSymbolValidation(String input, int expected) {
+        int actual = calculator.calculate(input);
+        assertEquals(expected, actual);
+    }
+
+}
