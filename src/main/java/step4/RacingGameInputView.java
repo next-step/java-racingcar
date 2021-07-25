4e58713a2f395d2378561d63f5d3654d653dc7e3
woung717 <woung717@gmail.com>
2021-07-25 15:51:53 +0900

    feat: 입력된 자동차 이름을 구분자로 분리

--- src/main/java/step4/RacingGameInputView.java
+++ src/main/java/step4/RacingGameInputView.java
@@ -1,8 +1,14 @@
 package step4;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class RacingGameInputView {
+    private static List<String> parseNames(String namesString) {
+        return Arrays.asList(namesString.split(","));
+    }
+
     public static RacingGameConfiguration getRacingGameConfigurationWithPrompt() {
         try (Scanner standardInput = new Scanner(System.in)) {
             System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
@@ -14,7 +20,7 @@ public class RacingGameInputView {
             System.out.println("시도할 회수는 몇 회 인가요?");
             int nTurns = standardInput.nextInt();
 
-            return new RacingGameConfiguration(carNames, nCars, nTurns);
+            return new RacingGameConfiguration(parseNames(carNames), nCars, nTurns);
         }
     }
 }
