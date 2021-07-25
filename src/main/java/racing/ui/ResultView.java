4720f4de7f4785c42a4b0f9ee71553570dd885d4
abcms031 <mjkim031@gmail.com>
2021-07-25 12:43:49 +0900

    refactor : ui -result 수정

--- src/main/java/racing/ui/ResultView.java
+++ src/main/java/racing/ui/ResultView.java
@@ -1,21 +1,43 @@
 package racing.ui;
 
-import java.util.HashMap;
-import racing.service.RacingCarService;
+import java.util.Arrays;
+import java.util.List;
+
+import racing.domain.Racing;
 
 public class ResultView {
 
-
     private static final String RACING_RESULT_MESSAGE = "실행결과";
+    private static final String RACING_SIGN = "-";
 
-    public void result(int racingRound, int racingCar){
+    private String[] racingResult;
+
+    public void result(List<Racing> racingList, int racingCar){
+
         System.out.println(RACING_RESULT_MESSAGE);
 
-        HashMap<Integer, StringBuffer> racingList = RacingCarService.initialRacingCar(racingCar);
+        racingResult = new String[racingCar];
+        Arrays.fill(racingResult, "");
 
-        for (int i = 0; i < racingRound; i++) {
-            RacingCarService.race(racingList);
+        for (Racing racing : racingList) {
+            getRacingResult(racing);
+            System.out.println();
         }
+    }
+
+    private void getRacingResult(Racing racing) {
+        int length = racing.getRacingLength();
+        for (int car = 0; car < length; car++) {
+            racingResult[car] = checkIsMoved(racing, car, racingResult[car]);
+            System.out.println(racingResult[car]);
+        }
+    }
+
+    public String checkIsMoved(Racing racing, int car, String result){
+        if(racing.getRacingResult(car)){
+            result += RACING_SIGN;
+        }
+        return result;
     }
 
 }
