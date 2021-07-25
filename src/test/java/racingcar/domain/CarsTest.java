1a98eaced56b82fe0aeb3e1665df9d3af6009880
조원진 <>
2021-07-25 18:56:57 +0900

    feat(Cars) : Cars 객체 생성

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,19 @@
+package racingcar.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.List;
+
+public class CarsTest {
+
+    @DisplayName("입력된 차 List가 null이거나 empty일 경우 IllegalArgumentExcpetion 발생")
+    @ParameterizedTest(name = "{index} {displayName} {arguments}")
+    @NullAndEmptySource
+    void create_ThrowsIllegalArgumentException_IfCarListIsNullOrEmpty(List<Car> cars) {
+        Assertions.assertThatIllegalArgumentException().isThrownBy(
+                () -> new Cars(cars));
+    }
+}
