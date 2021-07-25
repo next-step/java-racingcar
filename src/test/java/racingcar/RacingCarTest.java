caf482ca60af175da555dcb50a5398af5fef21d7
ChangSubKwak <paradise1732@gmail.com>
2021-07-26 00:12:47 +0900

    fix: 랜덤함수를 테스트 하지 않고, 함수의 기능 테스트만 하도록 변경

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,26 +2,39 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RacingCarTest {
 	RacingCar racingCar;
 
 	@BeforeEach
 	void setUp() {
-		racingCar = new RacingCar();
+		racingCar = new RacingCar(RacingCar.INITIAL_POSITION);
 	}
 
-	@Test
-	@DisplayName("forward함수를 n번 호출 했을 때, 자동차의 위치는 0과 같거나 크고 n보다 같거나 작다")
-	void forwardTest() {
-		int n = 100;
-		for (int i = 0 ; i < n ; i++) {
-			racingCar.forward();
-			assertThat(racingCar.getPos()).isGreaterThanOrEqualTo(0);
-			assertThat(racingCar.getPos()).isLessThanOrEqualTo(n);
-		}
+	@ParameterizedTest
+	@CsvSource(value = { "0:false", "2:false", "4:true", "6:true", "8:true"}, delimiter = ':')
+	void 주어진_인수를_입력받았을_때_4이상이면_전진가능하다(int forwardValue, boolean expected) {
+		assertThat(racingCar.movable(forwardValue)).isEqualTo(expected);
 	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 1, 2, 3 })
+	void 주어진_인수를_입력받았을_때_그_수_만큼_전진하다(int forwardValue) {
+		for (int i = 0; i < forwardValue; i++ ) {
+			racingCar.forward();
+		}
+		assertThat(racingCar.getRacingCarDashString()).isEqualTo(new String(new char[forwardValue]).replace("\0", "-"));
+	}
+
 }
