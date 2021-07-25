4c5c257647cd6a38913ce5f767964ba5c164e7fa
shryu <>
2021-07-25 13:55:13 +0900

    feat : Scanner,close 기능 개발

--- src/main/java/racingcar/MainApplication.java
+++ src/main/java/racingcar/MainApplication.java
@@ -9,6 +9,7 @@ public class MainApplication {
     public static void main(String[] args) {
         String carNames = InputView.inputCarNames();
         int moveCount = InputView.inputMoveCount();
+        InputView.closeScanner();
         List<Car> winners = RacingCar.play(carNames, moveCount);
         ResultView.printWinners(winners);
     }
