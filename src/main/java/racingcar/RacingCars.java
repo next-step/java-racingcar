cf39e7098fc31b1c6c46f974478d5ba22029c39f
ChangSubKwak <paradise1732@gmail.com>
2021-07-26 00:19:07 +0900

    feat: 자동차들을 관리하는 클래스 추가

---
+++ src/main/java/racingcar/RacingCars.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCars {
+	List<RacingCar> racingCars = new ArrayList<>();
+
+	public List<RacingCar> getCars() {
+		return racingCars;
+	}
+
+	public RacingCars(int carNum) {
+		for (int i = 0 ; i < carNum ; i++) {
+			racingCars.add(new RacingCar(RacingCar.INITIAL_POSITION));
+		}
+	}
+
+	public void forwardCars() {
+		racingCars.stream()
+			.filter(racingCar -> racingCar.movable(RacingCarUtil.generateForwardValue()))
+			.forEach(racingCar -> {
+				racingCar.forward();
+			});
+	}
+}
