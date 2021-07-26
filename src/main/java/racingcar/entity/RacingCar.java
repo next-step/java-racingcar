18b630714870daf4cdc49e8f2cfadeb4653ec65d
harris <harrisleesh@gmail.com>
2021-07-26 23:14:34 +0900

    refactor : Add moveStrategy dependency on RacingCar - RacingCar 생성자에서 moveStrategy를 주입 - isMovable 함수를 moveStrategy를 통해 구현

--- src/main/java/racingcar/entity/RacingCar.java
+++ src/main/java/racingcar/entity/RacingCar.java
@@ -1,29 +1,26 @@
 package racingcar.entity;
 
 
-import racingcar.util.RandomNumberGenerator;
+import racingcar.strategy.MoveStrategy;
 
 public class RacingCar {
     public static final int BASE_POSITION = 0;
     public static final int ONE_STEP = 1;
-    public static final int SEPARATE_NUMBER = 4;
+    private MoveStrategy moveStrategy;
+
     private int position;
 
     public int getPosition() {
         return position;
     }
 
-    public RacingCar() {
+    public RacingCar(MoveStrategy moveStrategy) {
         this.position = BASE_POSITION;
+        this.moveStrategy = moveStrategy;
     }
 
     public void moveIfMovable() {
-        if (isMovable())
+        if (moveStrategy.isMovable())
             position += ONE_STEP;
     }
-
-    private boolean isMovable() {
-        return RandomNumberGenerator.generate() >= SEPARATE_NUMBER;
-    }
-
 }
