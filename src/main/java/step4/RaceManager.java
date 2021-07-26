3ac0d1a382e30df703d692ec2e1a44eb93331274
jaeyoungjang2 <lion85488@gmail.com>
2021-07-26 23:46:15 +0900

    feat: race manager 추가 주사위를 던져 4이상만 앞으로 전진

--- src/main/java/step4/RaceManager.java
+++ src/main/java/step4/RaceManager.java
@@ -1,2 +1,17 @@
-package step4;public class RaceManager {
+package step4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RaceManager {
+    private static final int DICE_THRETHOLD = 4;
+    private static Random rd = new Random();
+
+    public boolean judge() {
+        int rdNumber = rd.nextInt(10);
+        if (rdNumber >= 4) {
+            return true;
+        }
+        return false;
+    }
 }
