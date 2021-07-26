e1a7dd4594c5ef9303fd4e5449b589c37bc94d96
yook0328 <yook0328@gmail.com>
2021-07-26 23:25:23 +0900

    feat: 나눗셈 구현

--- src/main/java/com/racingcar/calculator/StringCalculator.java
+++ src/main/java/com/racingcar/calculator/StringCalculator.java
@@ -5,9 +5,11 @@ public class StringCalculator {
         String[] values = input.split(" ");
         if ("+".equals(values[1])) {
             return add(Integer.parseInt(values[0]), Integer.parseInt(values[2]));
+        } else if ("-".equals(values[1])) {
+            return subtract(Integer.parseInt(values[0]), Integer.parseInt(values[2]));
         }
 
-        return subtract(Integer.parseInt(values[0]), Integer.parseInt(values[2]));
+        return divide(Integer.parseInt(values[0]), Integer.parseInt(values[2]));
     }
 
     private static int add(int value1, int value2) {
@@ -16,5 +18,9 @@ public class StringCalculator {
 
     private static int subtract(int value1, int value2) {
         return value1 - value2;
+    }
+
+    private static int divide(int value1, int value2) {
+        return value1 / value2;
     }
 }
