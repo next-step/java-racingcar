947c1b07c114ab5b7a2939891385a86979feb0fe
jaeyoungjang2 <lion85488@gmail.com>
2021-07-26 23:42:33 +0900

    feat: Main 클래스 생성

--- src/main/java/RacingCarMain.java
+++ src/main/java/RacingCarMain.java
@@ -1,34 +1,21 @@
-import racing.RacingCarInput;
-import racing.RacingCarOperator;
-import step3.controller.RaceManager;
-import step3.view.InputView;
-import step3.view.ResultView;
+import step4.Race;
 
 import java.util.Scanner;
 
 public class RacingCarMain {
-    static int numOfCar;
-    static int numOfCycle;
-
     public static void main(String[] args) {
-        RacingCarMain rcm = new RacingCarMain();
-        rcm.prepareInput();
+        Race race = new Race();
+        int[] raceInfo = race.requestInput();
+        int numOfCar = raceInfo[0];
+        int numOfCycle = raceInfo[1];
+
+        race.prepareCars();
+        for (int i = 0; i < numOfCycle; i++) {
+            race.printResult();
+            race.playRace();
+            System.out.println();
+        }
+        race.printResult();
+
     }
-
-    private void prepareInput() {
-        InputView iv = new InputView();
-        numOfCar = iv.getNumOfCar();
-        numOfCycle = iv.getNumOfCycle();
-
-        RaceManager rm = new RaceManager(numOfCar, numOfCycle);
-        raceStart(rm);
-    }
-
-    private void raceStart(RaceManager rm) {
-        rm.inputCheck();
-        rm.makeCarAry();
-        rm.raceByCycle();
-    }
-
-
 }
