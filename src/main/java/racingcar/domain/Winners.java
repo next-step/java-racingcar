f66237356b5f63ad5197e31cf3203263ef579750
shryu <>
2021-07-26 13:08:05 +0900

    refactor : 중복 코드 삭제후 substring으로 대체

--- src/main/java/racingcar/domain/Winners.java
+++ src/main/java/racingcar/domain/Winners.java
@@ -5,29 +5,26 @@ import java.util.List;
 
 public class Winners {
     private List<Car> winnerCars = new ArrayList<>();
-    private int winnerPosition;
 
     public Winners(Cars cars) {
-        winnerPosition = cars.getWinnerPosition();
         getWinners(cars);
     }
 
     public String getWinnerNames(){
         StringBuffer sb = new StringBuffer();
-        for(int i=0; i<winnerCars.size()-1; i++){
+        for(int i=0; i<winnerCars.size(); i++){
             sb.append(winnerCars.get(i).getName()).append(", ");
         }
-        sb.append(winnerCars.get(winnerCars.size()-1).getName());
-        return sb.toString();
+        return sb.substring(0,sb.length()-2);
     }
 
     private void getWinners(Cars cars){
         for (Car car : cars.getCars()) {
-            addWinner(car);
+            addWinner(car, cars.getWinnerPosition());
         }
     }
 
-    private void addWinner(Car car) {
+    private void addWinner(Car car, int winnerPosition) {
         if(car.isMoreThan(winnerPosition)){
             winnerCars.add(car);
         }
