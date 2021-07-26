525d5ac994becb1376fc9b5db3f4f43b57e3234d
sk1737030 <sk1737030@naver.com>
2021-07-26 22:34:16 +0900

    Docs: 스텝4 완성

---
+++ src/test/java/step4/CarStadiumTest.java
@@ -0,0 +1,12 @@
+package step4;
+
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarStadiumTest {
+
+
+}
