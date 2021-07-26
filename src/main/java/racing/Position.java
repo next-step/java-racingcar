564a52bbf0479f02deaf8f2dca305201441c0719
gashe-soo <gashe7@gmail.com>
2021-07-27 00:56:10 +0900

    feat:전진 기능 구현

---
+++ src/main/java/racing/Position.java
@@ -0,0 +1,34 @@
+package racing;
+
+import java.util.Objects;
+
+public final class Position {
+
+    private int pos;
+
+    public Position(int pos) {
+        this.pos = pos;
+    }
+
+    public Position add(int distance) {
+        return new Position(this.pos + distance);
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return pos == position.pos;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pos);
+    }
+
+}
---
+++ src/test/java/racing/CarTest.java
@@ -0,0 +1,30 @@
+package racing;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 9})
+    void 랜덤숫자_4이상_9이하일경우_전진(int number) {
+        Car car = new Car(0);
+        car.move(number);
+
+        assertThat(car.getNowPosition()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 10})
+    void 랜덤숫자_4미만_9초과일경우_전진_실패(int number) {
+        int position = 5;
+        Car car = new Car(position);
+        car.move(number);
+
+        assertThat(car.getNowPosition()).isEqualTo(position);
+    }
+
+
+}
