3822b7bb5cc7b7008ae10bfff02a833ad8ad4faf
jaeyoungjang2 <lion85488@gmail.com>
2021-07-26 23:43:52 +0900

    feat: Race 경주 추가

--- src/main/java/step4/Race.java
+++ src/main/java/step4/Race.java
@@ -1,2 +1,38 @@
-package step4;public class Race {
+package step4;
+
+import java.util.Random;
+
+public class Race {
+    private int numOfCar;
+    private int numOfCycle;
+    private InputView inputView = new InputView();
+    private ResultView resultView = new ResultView();
+    private CarManager[] carManagers;
+
+    public int[] requestInput() {
+        numOfCar = inputView.getNumOfCar();
+        numOfCycle = inputView.getNumOfCycle();
+        return new int[]{numOfCar, numOfCycle};
+    }
+
+    public void prepareCars() {
+        carManagers = new CarManager[numOfCar];
+        for (int i = 0; i < numOfCar; i++) {
+            Car car = new Car(1);
+            carManagers[i] = new CarManager(car);
+        }
+    }
+
+    public void playRace() {
+        for (int i = 0; i < numOfCar; i++) {
+            carManagers[i].CanIGo();
+        }
+    }
+
+    public void printResult() {
+        for (int i = 0; i < numOfCar; i++) {
+            resultView.showCurrCarPosition(carManagers[i].getCurrCarPosition());
+        }
+
+    }
 }
