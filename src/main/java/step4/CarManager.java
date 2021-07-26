a66a8064e0e0faaf812a91622eee6d9a3a33c5c0
jaeyoungjang2 <lion85488@gmail.com>
2021-07-26 23:44:13 +0900

    feat: 운전을 담당하는 사람 추가

--- src/main/java/step4/CarManager.java
+++ src/main/java/step4/CarManager.java
@@ -1,2 +1,20 @@
-package step4;public class CarManager {
+package step4;
+
+public class CarManager {
+    private Car car;
+    private RaceManager raceManager = new RaceManager();
+
+    public CarManager(Car car) {
+        this.car = car;
+    }
+
+    public void CanIGo() {
+        if (raceManager.judge()) {
+            car.MoveForward();
+        }
+    }
+
+    public int getCurrCarPosition() {
+        return car.getPosition();
+    }
 }
