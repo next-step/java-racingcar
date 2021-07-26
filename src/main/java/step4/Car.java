47a92434edb0429b5dc8ca37268870e6fddde024
jaeyoungjang2 <lion85488@gmail.com>
2021-07-26 23:44:54 +0900

    feat: car domain 추가

--- src/main/java/step4/Car.java
+++ src/main/java/step4/Car.java
@@ -1,2 +1,17 @@
-package step4;public class Car {
+package step4;
+
+public class Car {
+    private int position;
+
+    public Car(int position) {
+        this.position = position;
+    }
+
+    public void MoveForward() {
+        this.position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
