0bd6a2cf68fe9e673a5837b431f6b7bef6fe8582
jspark940321 <jspark940321@gmail.com>
2021-07-26 23:25:41 +0900

    test: RacingCar 수정으로 인한 테스트 수정
    RacingCar에서 gameCount를 제거함에 따라 테스트 또한 수정

--- src/test/java/Car/RacingCarTest.java
+++ src/test/java/Car/RacingCarTest.java
@@ -4,48 +4,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-
 import static org.assertj.core.api.Assertions.*;
-
 
 class RacingCarTest {
     RacingCar racingCar;
 
     @BeforeEach
     void setUp() {
-        racingCar = new RacingCar(3, 5);
+        racingCar = new RacingCar(3);
     }
 
     @ParameterizedTest
     @CsvSource({
-            "3, 5, 3, 5",
-            "1, 4, 1, 4"
+            "3, 3",
+            "1, 1"
     })
-    void 자동차대수_테스트(int inputCarCount, int inputGameCount, int carCountAnswer, int gameCountAnswer) {
-        racingCar = new RacingCar(inputCarCount, inputGameCount);
-
-        int carCount = racingCar.getCarCount();
-        assertThat(carCount).isEqualTo(carCountAnswer);
-
-        int gameCount = racingCar.getGameCount();
-        assertThat(gameCount).isEqualTo(gameCountAnswer);
-
+    void 자동차대수_테스트(int inputCarCount, int carCountAnswer) {
+        racingCar = new RacingCar(inputCarCount);
         int carsSize = racingCar.getCars().size();
         assertThat(carsSize).isEqualTo(carCountAnswer);
     }
 
     @ParameterizedTest
     @CsvSource({
-            "0, 5, 0보다 큰 값을 입력해주세요.",
-            "-1, 4, 0보다 큰 값을 입력해주세요.",
-            "3, 0, 0보다 큰 값을 입력해주세요.",
-            "6, -1, 0보다 큰 값을 입력해주세요.",
+            "0, 0보다 큰 값을 입력해주세요.",
+            "-1, 0보다 큰 값을 입력해주세요.",
     })
-    void 자동차대수_테스트_예외발생(int carCount, int gameCount, String errMessage) throws IllegalAccessException {
-        assertThatThrownBy(() -> new RacingCar(carCount, gameCount))
+    void 자동차대수_테스트_예외발생(int carCount, String errMessage) throws IllegalAccessException {
+        assertThatThrownBy(() -> new RacingCar(carCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(errMessage);
     }
-
-
 }
