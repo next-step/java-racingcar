e2c639e297f3f67ae42f52618a80d4476e0eebe4
YooInKeun <keun0390@naver.com>
2021-07-26 13:19:13 +0900

    test: NumberTest, Number 객체 생성시 주입받은 숫자 값이 변하지 않는지 검사하는 테스트 코드 추가

--- src/test/java/racing/model/NumberTest.java
+++ src/test/java/racing/model/NumberTest.java
@@ -1,13 +1,28 @@
 package racing.model;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("숫자 테스트")
 public class NumberTest {
+
+    @DisplayName("Number 객체 생성시 주입받은 숫자 값을 그대로 갖는다.")
+    @Test
+    public void unchangedNumberValueTest() {
+        // given
+        int givenNumber = 3;
+
+        // when
+        Number number = new Number(givenNumber);
+
+        // then
+        assertEquals(number.getNumber(), givenNumber);
+    }
 
     @DisplayName("0에서 9 사이의 숫자가 아니라면, IllegalArgumentException이 발생한다")
     @ParameterizedTest
