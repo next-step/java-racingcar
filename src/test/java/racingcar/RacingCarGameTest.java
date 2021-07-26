32edb7472c03ad4846bcec6c636706a1c5a1e0c5
harris <harrisleesh@gmail.com>
2021-07-26 23:56:18 +0900

    refactor : Change constant with 대문자, 언더스코어

--- src/test/java/racingcar/RacingCarGameTest.java
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -16,28 +16,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RacingCarGameTest {
     private final List<RacingCar> randomMoveRacingCars = new ArrayList<>();
     private final List<RacingCar> alwaysMoveRacingCars = new ArrayList<>();
-    public static final int carNumber = 3;
-    public static final int tryNumber = 5;
+    public static final int CAR_NUMBER = 5;
+    public static final int TRY_NUMBER = 10;
 
     @BeforeEach
     void setUp() {
-        RacingCarGame.initializeRacingCars(randomMoveRacingCars, carNumber, new RandomMoveStrategy());
-        RacingCarGame.initializeRacingCars(alwaysMoveRacingCars, tryNumber, new AlwaysMoveStrategy());
+        RacingCarGame.initializeRacingCars(randomMoveRacingCars, CAR_NUMBER, new RandomMoveStrategy());
+        RacingCarGame.initializeRacingCars(alwaysMoveRacingCars, CAR_NUMBER, new AlwaysMoveStrategy());
     }
 
     @Test
     @DisplayName("레이싱 카의 차량대수로 초괴화를 했을 때 리스트의 사이즈가 변하는 것을 확인한다.")
     void initializeRacingCars() {
-        assertThat(randomMoveRacingCars.size()).isEqualTo(carNumber);
-        assertThat(alwaysMoveRacingCars.size()).isEqualTo(carNumber);
+        assertThat(randomMoveRacingCars.size()).isEqualTo(CAR_NUMBER);
+        assertThat(alwaysMoveRacingCars.size()).isEqualTo(CAR_NUMBER);
     }
 
     @RepeatedTest(10)
     @DisplayName("랜덤무브 레이싱카 게임의 race()를 완료했을 때 racingCar position 은 0 이상 tryNumber 이하 임을 확인한다.")
     void randomMoveRace() {
-        RacingCarGame.race(randomMoveRacingCars, tryNumber);
+        RacingCarGame.race(randomMoveRacingCars, TRY_NUMBER);
         for (RacingCar racingCar : randomMoveRacingCars) {
-            assertThat(racingCar.getPosition()).isLessThanOrEqualTo(tryNumber);
+            assertThat(racingCar.getPosition()).isLessThanOrEqualTo(TRY_NUMBER);
             assertThat(racingCar.getPosition()).isGreaterThanOrEqualTo(RacingCar.BASE_POSITION);
         }
     }
@@ -55,9 +55,9 @@ class RacingCarGameTest {
     @Test
     @DisplayName("항상 움직이는 전략을 사용한 레이싱카 게임의 race()를 완료했을 때 racingCar position 은 모두 tryNumber 임을 확인한다.")
     void alwaysMoveRace(){
-        RacingCarGame.race(alwaysMoveRacingCars, tryNumber);
+        RacingCarGame.race(alwaysMoveRacingCars, TRY_NUMBER);
         for(RacingCar racingCar : alwaysMoveRacingCars){
-            assertThat(racingCar.getPosition()).isEqualTo(tryNumber);
+            assertThat(racingCar.getPosition()).isEqualTo(TRY_NUMBER);
         }
     }
 
