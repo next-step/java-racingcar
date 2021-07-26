dbf3340ac45842d2ece2d21e752e1522f2c6f353
yook0328 <yook0328@gmail.com>
2021-07-27 01:22:55 +0900

    refactor: 연산하는 부분 인터페이스로 따로 추출.

--- src/main/java/com/racingcar/calculator/StringCalculator.java
+++ src/main/java/com/racingcar/calculator/StringCalculator.java
@@ -1,45 +1,41 @@
 package com.racingcar.calculator;
 
+import com.racingcar.calculator.operator.Operator;
+import com.racingcar.calculator.operator.StringOperator;
+
 public class StringCalculator {
+    private static final String ADD = "+";
+    private static final String SUBTRACT = "-";
+    private static final String MULTIPLY = "*";
+    private static final String DIVIDE = "/";
+
+    private static final Operator operator = new StringOperator();
+
     public static int calculate(String input) {
         String[] values = input.split(" ");
 
         int result = Integer.parseInt(values[0]);
 
         for (int i = 1; i < values.length; i += 2) {
-            String operator = values[i];
-            int operatedValue = Integer.parseInt(values[i+1]);
-            if ("+".equals(operator)) {
-                 result = add(result, operatedValue);
-                 continue;
-            } else if ("-".equals(operator)) {
-                result = subtract(result, operatedValue);
-                continue;
-            } else if("/".equals(operator)) {
-                result = divide(result, operatedValue);
-                continue;
-            }
-
-            result = multiply(result, operatedValue);
+            String operatorSign = values[i];
+            int operatedValue = Integer.parseInt(values[i + 1]);
+            result = calculate(operatorSign, result, operatedValue);
         }
-
 
         return result;
     }
 
-    private static int add(int value1, int value2) {
-        return value1 + value2;
-    }
+    private static int calculate(String operatorSign, int value01, int value02) {
+        if (ADD.equals(operatorSign)) {
+            return operator.add(value01, value02);
+        } else if (SUBTRACT.equals(operatorSign)) {
+            return operator.subtract(value01, value02);
+        } else if (MULTIPLY.equals(operatorSign)) {
+            return operator.multiply(value01, value02);
+        } else if (DIVIDE.equals(operatorSign)) {
+            return operator.divide(value01, value02);
+        }
 
-    private static int subtract(int value1, int value2) {
-        return value1 - value2;
-    }
-
-    private static int divide(int value1, int value2) {
-        return value1 / value2;
-    }
-
-    private static int multiply(int value1, int value2) {
-        return value1 * value2;
+        throw new IllegalArgumentException("Invalid Operator - target is " + operatorSign);
     }
 }
