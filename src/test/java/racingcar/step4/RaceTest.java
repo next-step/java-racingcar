b00cea734f69811fafc1611d0c7a6161acfd3c75
gunny6026 <gunny6026@naver.com>
2021-07-26 22:50:08 +0900

    test : 우승자 출력 테스트 케이스

---
+++ src/test/java/racingcar/step4/RaceTest.java
@@ -0,0 +1,23 @@
+package racingcar.step4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RaceTest {
+
+    @Test
+    @DisplayName("우승자 출력 테스트")
+    void 우승자출력(){
+        String [] carNames = {"pobi","crong","honux"};
+        List<Car> cars = Car.createCar(carNames);
+        List<Car> racingCars = Race.startRaceGame(cars);
+        assertThat(racingCars.size()).isBetween(0,carNames.length);
+    }
+
+}
