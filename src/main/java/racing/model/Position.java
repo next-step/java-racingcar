573c4dcdbdf1ed2ef96acd5e9614779b0bbee435
YooInKeun <keun0390@naver.com>
2021-07-26 12:26:56 +0900

    refactor: position 값, 불필요한 초기화 제거

--- src/main/java/racing/model/Position.java
+++ src/main/java/racing/model/Position.java
@@ -1,14 +1,9 @@
 package racing.model;
 
 public class Position {
-    private static final int INITIAL_POSITION = 0;
     private static final int ONE_SPACE = 1;
 
     private int position;
-
-    Position() {
-        this.position = INITIAL_POSITION;
-    }
 
     void increaseOne() {
         position += ONE_SPACE;
