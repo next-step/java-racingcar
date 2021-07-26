775dd3596276053f5af451ccee07858ed5190c22
gashe-soo <gashe7@gmail.com>
2021-07-27 01:32:18 +0900

    feat:racing 라운드 진행 기능 구현

---
+++ src/main/java/racing/Racing.java
@@ -0,0 +1,35 @@
+package racing;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Racing {
+
+    private int totalRound;
+    private int nowRound;
+    private List<Car> cars;
+    private RandomNumberGenerator numberGenerator;
+
+
+    public Racing(int carCount, int totalRound) {
+        this.totalRound = totalRound;
+        this.nowRound = 0;
+        this.cars = IntStream.range(0, carCount).mapToObj(a -> new Car()).collect(Collectors.toList());
+        this.numberGenerator = new RandomNumberGenerator();
+    }
+
+    public boolean isEnd() {
+        return nowRound == totalRound;
+    }
+
+    public CarsDTO play() {
+        this.nowRound++;
+        CarsDTO carsDTO = new CarsDTO();
+        for(Car car : cars){
+            car.move(numberGenerator.generate());
+            carsDTO.add(car.getNowPosition());
+        }
+        return carsDTO;
+    }
+}
