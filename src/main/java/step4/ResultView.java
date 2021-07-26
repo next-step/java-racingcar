df798b7b08afddd5c8a91abcc5372eb7ef0aecaa
jaeyoungjang2 <lion85488@gmail.com>
2021-07-26 23:45:37 +0900

    feat: result view 추가

--- src/main/java/step4/ResultView.java
+++ src/main/java/step4/ResultView.java
@@ -1,2 +1,13 @@
-package step4;public class ResultView {
+package step4;
+
+import step3.model.Car;
+
+public class ResultView {
+    public void showCurrCarPosition(int location) {
+        for (int i = 0; i < location; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+
+    }
 }
