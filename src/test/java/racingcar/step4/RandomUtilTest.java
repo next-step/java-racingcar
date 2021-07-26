94b8496f867be0a34f7a43774a577d8317b9850c
gunny6026 <gunny6026@naver.com>
2021-07-26 19:20:19 +0900

    test : 난수 생성 테스트 케이스

---
+++ src/test/java/racingcar/step4/RandomUtilTest.java
@@ -0,0 +1,19 @@
+package racingcar.step4;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomUtilTest {
+
+    @Test
+    @DisplayName("난수 생성하기")
+    void 난수생성(){
+        for (int i =0; i<10; i++) {
+            Assertions.assertThat(RandomUtil.generateRandomNumber()).isBetween(0,9);
+        }
+    }
+
+}
