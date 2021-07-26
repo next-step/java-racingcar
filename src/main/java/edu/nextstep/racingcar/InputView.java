bb5cc8911252752f46d2d31394c788df381e67f9
beadoer1 <beadoer1@gmail.com>
2021-07-26 14:25:41 +0900

    feat(InputView.java) : 자동차 대수, 시도 횟수 입력 로직 구현
    구현 메서드
    - inputCarNum() : 질문 출력 및 자동차 대수 입력
    - inputMoveNum() : 질문 출력 및 시도 횟수 입력

--- src/main/java/edu/nextstep/racingcar/InputView.java
+++ src/main/java/edu/nextstep/racingcar/InputView.java
@@ -12,11 +12,15 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static int inputCarNum() {
-        return 0;
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return scanner.nextInt();
     }
 
     public static int inputMoveNum() {
-        return 0;
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
     }
 }
