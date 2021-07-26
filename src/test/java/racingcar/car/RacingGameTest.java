ee295aea792f0c4c22d9f72d5d0742158a451e0f
seondongpyo <seondongpyo@gmail.com>
2021-07-26 10:43:58 +0900

    test: 자동차 경주의 우승자를 구하는 테스트

---
+++ src/test/java/racingcar/car/RacingGameTest.java
@@ -0,0 +1,22 @@
+package racingcar.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.strategy.AlwaysMoveStrategy;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingGameTest {
+
+    @DisplayName("우승자는 여러명일 수 있다.")
+    @Test
+    void raceWinners() {
+        RacingGame racingGame = new RacingGame(Cars.of("pobi,crong,honux"), new AlwaysMoveStrategy());
+        racingGame.race();
+        List<Car> winners = racingGame.getRaceWinners();
+        assertThat(winners).hasSize(3);
+    }
+
+}
