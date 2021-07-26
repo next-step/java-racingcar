7a5c17986557e4c5a043ed2323e940b6a2f23296
jspark940321 <jspark940321@gmail.com>
2021-07-26 23:30:37 +0900

    fix: 요청사항 수정
    Car의 Random을 RacingCar에서 관리하게 적용 및 gameResults, carCount, gameCount 제거
    https://github.com/next-step/java-racingcar/pull/2437#discussion_r676160816
    https://github.com/next-step/java-racingcar/pull/2437#discussion_r676160907

--- src/main/java/Car/RacingCar.java
+++ src/main/java/Car/RacingCar.java
@@ -4,32 +4,18 @@ package Car;
 import java.util.*;
 
 public class RacingCar {
+    static final int RANDOM_NUMBER_BOUND = 10;
+    static final Random random = new Random();
+
     private List<Car> cars;
-    private List<List<Integer>> gameResults;
-    private int carCount;
-    private int gameCount;
 
     public List<Car> getCars() {
         return cars;
     }
 
-    public int getCarCount() {
-        return carCount;
-    }
+    public RacingCar(int carCount) {
+        validate(carCount);
 
-    public int getGameCount() {
-        return gameCount;
-    }
-
-    public List<List<Integer>> getGameResults() {
-        return gameResults;
-    }
-
-    public RacingCar(int carCount, int gameCount) {
-        validate(carCount, gameCount);
-        this.carCount = carCount;
-        this.gameCount = gameCount;
-        gameResults = new ArrayList<>();
         cars = new ArrayList<>();
         for (int i = 0; i < carCount; i++) {
             cars.add(new Car());
@@ -37,26 +23,13 @@ public class RacingCar {
     }
 
     public void gameStart() {
-        saveResult();
-        for (int i = 1; i < gameCount; i++) {
-            cars.stream()
-                    .forEach(car -> car.move());
-            saveResult();
-        }
-    }
-
-    private void saveResult() {
-        List<Integer> result = new ArrayList<>();
         cars.stream()
-                .forEach(car -> result.add(car.getMoveCount()));
-        gameResults.add(result);
+                .forEach(car -> car.move(random.nextInt(RANDOM_NUMBER_BOUND)));
     }
 
-    private void validate(int carCount, int gameCount) {
-        if (carCount <= 0 || gameCount <= 0) {
+    private void validate(int carCount) {
+        if (carCount <= 0) {
             throw new IllegalArgumentException("0보다 큰 값을 입력해주세요.");
         }
     }
-
-
 }
