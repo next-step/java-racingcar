44315eed101fa7b8ad4d55c778cb209df113c7ff
khcho902 <khcho902@gmail.com>
2021-07-27 02:39:52 +0900

    feat: create checkOperatorValid method

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -6,10 +6,26 @@ public class Calculator {
         return input != null && !input.equals("");
     }
 
+    private static boolean checkOperatorValid(String operator) {
+        return operator.equals("+") || operator.equals("-") ||
+                operator.equals("*") || operator.equals("/");
+    }
+
     public static long calculate(String input){
-        if (checkInputValid(input))
+        if (!checkInputValid(input))
             throw new IllegalArgumentException();
 
-        return 0;
+        String[] tokens = input.split(" ");
+
+        long result = Long.parseLong(tokens[0]);
+        for (int i = 1; i < tokens.length; i += 2) {
+            String operator = tokens[i];
+            Long operand = Long.parseLong(tokens[i + 1]);
+
+            if (!checkOperatorValid(operator))
+                throw new IllegalArgumentException();
+
+        }
+        return result;
     }
 }
