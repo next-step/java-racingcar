1fbe1dc2a26d2175c8d831b1df2927db5df74666
0n1dev <all_mind@naver.com>
2021-07-26 22:47:09 +0900

    fix : InputView 클래스 next() 메서드 호출 변경
    - nextLine() 메서드 호출로 변경

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -17,7 +17,7 @@ public class InputView {
 	public static String[] inputCarNames() {
 		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
-		String input = scanner.next();
+		String input = scanner.nextLine();
 		String[] names = input.split(",");
 
 		return Arrays.stream(names)
