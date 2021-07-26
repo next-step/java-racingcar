ea901ecad9f01fd7961db65c6534b13787cbb5df
jspark940321 <jspark940321@gmail.com>
2021-07-26 22:57:17 +0900

    fix: Random 제거 및 자바 코딩 컨벤션 미적용 부분 수정
    Random 제거 및 자바 코딩 컨벤션이 미적용된 상수 위치, 메소드 위치 적용
    https://github.com/next-step/java-racingcar/pull/2437#discussion_r676159990
    https://github.com/next-step/java-racingcar/pull/2437#discussion_r676160043
    https://github.com/next-step/java-racingcar/pull/2437#discussion_r676160816

--- src/main/java/Car/Car.java
+++ src/main/java/Car/Car.java
@@ -3,30 +3,22 @@ package Car;
 import java.util.Random;
 
 public class Car {
-    private int moveCount;
-    private Random random;
-
-    private static final int NUMBER_BOUND = 10;
     private static final int MIN_MOVE_NUMBER = 4;
+
+    private int moveCount;
+
+    public Car() {
+        moveCount = 0;
+    }
 
     public int getMoveCount() {
         return moveCount;
     }
 
-    public Car() {
-        moveCount = 0;
-        random = new Random();
-    }
-
-    public void move() {
-        int randomNumber = getRandomNumber();
-        if (isMove(randomNumber)) {
+    public void move(int inputNumber) {
+        if (isMove(inputNumber)) {
             moveCount++;
         }
-    }
-
-    private int getRandomNumber() {
-        return random.nextInt(NUMBER_BOUND);
     }
 
     private boolean isMove(int randomNumber) {
