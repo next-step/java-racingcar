0b703cd8345fad1e5fce27734a22268e74085fdc
yook0328 <yook0328@gmail.com>
2021-07-26 23:20:50 +0900

    feat: 뺄셈 구현

--- src/main/java/com/racingcar/calculator/StringCalculator.java
+++ src/main/java/com/racingcar/calculator/StringCalculator.java
@@ -3,10 +3,18 @@ package com.racingcar.calculator;
 public class StringCalculator {
     public static int calculate(String input) {
         String[] values = input.split(" ");
-        return add(Integer.parseInt(values[0]), Integer.parseInt(values[2]));
+        if ("+".equals(values[1])) {
+            return add(Integer.parseInt(values[0]), Integer.parseInt(values[2]));
+        }
+
+        return subtract(Integer.parseInt(values[0]), Integer.parseInt(values[2]));
     }
 
     private static int add(int value1, int value2) {
         return value1 + value2;
+    }
+
+    private static int subtract(int value1, int value2) {
+        return value1 - value2;
     }
 }
