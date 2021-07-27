06a238c91b5bbc62a8737efa5dd8b7924a28e837
harris <harrisleesh@gmail.com>
2021-07-27 22:03:09 +0900

    feat : Change requestCarNumbers to requestCarNames in InputView

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class InputView {
     public static final Scanner scanner = new Scanner(System.in);
-    public int requestCarNumber() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.nextInt();
+    public static final String SPACE_SEPARATOR = " ";
+
+    public String[] requestCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine().split(SPACE_SEPARATOR);
     }
 
     public int requestTryNumber() {
