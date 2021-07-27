0ec2bfea88b4c36e8843767935f2901e502050e0
jaeyoungjang2 <lion85488@gmail.com>
2021-07-27 20:49:10 +0900

    refactor: 한번만 초기화 될 수 있도록 정적 상수로 선언

--- src/main/java/step4/InputView.java
+++ src/main/java/step4/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
     private static final int MIN_CAR = 0;
     private static final String CYCLE_ERROR_MESSAGE = "시도할 횟수는 0보다 커야 합니다.";
     private static final String CAR_ERROR_MESSAGE = "자동차 대수는 1대 이상이어야 합니다.";
-    private Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public int getNumOfCar() {
         System.out.println(HOW_MANY_CAR);
