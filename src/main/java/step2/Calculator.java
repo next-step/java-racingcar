5369cde0baa6a7539c0e223f54d26ecd6a16d89d
sangnyyy <dltkdals2202@gmail.com>
2021-07-28 01:46:45 +0900

    refactor : 덧셈, 뺄셈, 곱셈, 나눗셈에 대한 코드 리팩토링

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -3,24 +3,40 @@ package step2;
 public class Calculator {
     public static int calculate(String value) {
         String[] values = value.split(" ");
+        String operator = values[1];
         int first = toInt(values[0]);
         int second = toInt(values[2]);
-        String operator = values[1];
         return calculate(operator, first, second);
     }
 
     private static int calculate(String operator, int first, int second) {
         if ("+".equals(operator)) {
-            return first + second;
+            return sum(first, second);
         } else if ("-".equals(operator)) {
-            return first - second;
+            return sub(first, second);
         } else if ("*".equals(operator)) {
-            return first * second;
+            return mul(first, second);
         }
+        return div(first, second);
+    }
+
+    private static int sum(int first, int second) {
+        return first + second;
+    }
+
+    private static int sub(int first, int second) {
+        return first - second;
+    }
+
+    private static int mul(int first, int second) {
+        return first * second;
+    }
+
+    private static int div(int first, int second) {
         return first / second;
     }
 
-    public static int toInt(String value) {
+    private static int toInt(String value) {
         return Integer.parseInt(value);
     }
 }
--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CalculatorTest {
 
     @Test
-    public void 덧셈(){
+    public void 덧셈() {
         //given
         String given = "2 + 3";
         //when
@@ -17,7 +17,7 @@ public class CalculatorTest {
     }
 
     @Test
-    public void 뺄셈(){
+    public void 뺄셈() {
         //given
         String given = "3 - 2";
         //when
@@ -27,7 +27,7 @@ public class CalculatorTest {
     }
 
     @Test
-    public void 곱셈(){
+    public void 곱셈() {
         //given
         String given = "3 * 2";
         //when
@@ -37,7 +37,7 @@ public class CalculatorTest {
     }
 
     @Test
-    public void 나눗셈(){
+    public void 나눗셈() {
         //given
         String given = "4 / 2";
         //when
