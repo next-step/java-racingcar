16d4a018cd91b344e6e921114022d453ade91533
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-07-27 13:13:19 +0900

    test: 기능 분리로 인한 테스트 코드 수정

--- src/test/java/edu/nextstep/racingcar/step4/domain/CarsTest.java
+++ src/test/java/edu/nextstep/racingcar/step4/domain/CarsTest.java
@@ -13,13 +13,13 @@ class CarsTest {
 
     @BeforeAll
     static void getCars() {
-        cars = App.make("AAA,BBB,CCC,DDD,EEE", 10, 4);
+        cars = Cars.make("AAA,BBB,CCC,DDD,EEE", 10, 4);
     }
 
     @DisplayName("우승자 출력 테스트")
     @Test
     void getWInners() {
-        cars.play(5);
+        cars.play(5, CarStrategy.getMoveStrategy(10, 4));
         assertThat(cars.getWinners()).isNotEmpty();
     }
 }
