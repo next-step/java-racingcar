f57dabb62a73fadc461d08773f26ce3fd06eda4b
ChangSubKwak <paradise1732@gmail.com>
2021-07-27 15:44:03 +0900

    fix: 전략패턴 도입에 따른 테스트 케이스 변경 및 수정

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -3,24 +3,68 @@ package racingcar;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import racingcar.model.RacingCar;
+import racingcar.strategy.AlwaysMovableStrategy;
+import racingcar.strategy.RandomlyMovableStrategy;
+
+import java.util.Random;
+import java.util.stream.Stream;
 
 class RacingCarTest {
 	RacingCar racingCar;
 
 	@BeforeEach
 	void setUp() {
-		racingCar = new RacingCar();
+		racingCar = new RacingCar(new AlwaysMovableStrategy());
+	}
+
+	@Test
+	void AlwaysMovable_전략을_입력받았을_항상_전진가능하다() {
+		racingCar.forward();
+		assertThat(racingCar.getRacingCarDashString()).isEqualTo("-");
 	}
 
 	@ParameterizedTest
-	@CsvSource(value = { "0:false", "2:false", "4:true", "6:true", "8:true"}, delimiter = ':')
-	void 주어진_인수를_입력받았을_때_4이상이면_전진가능하다(int forwardValue, boolean expected) {
-		assertThat(racingCar.isMovable(forwardValue)).isEqualTo(expected);
+	@MethodSource
+	void RandomlyMovable_전략을_입력받았을_때_랜덤값이_4이상_일때에만_전진가능하다(Random random, String expected) {
+		racingCar = new RacingCar(RandomlyMovableStrategy.initRandomSeed(random));
+		racingCar.forward();
+		assertThat(racingCar.getRacingCarDashString()).isEqualTo(expected);
+	}
+
+	private static Stream<Arguments> RandomlyMovable_전략을_입력받았을_때_랜덤값에_따라_전진가능하다() {
+		return Stream.of(
+			Arguments.of(new Random() {
+				@Override
+				public int nextInt(int bound) {
+					return 0;
+				}
+			}, ""),
+			Arguments.of(new Random() {
+				@Override
+				public int nextInt(int bound) {
+					return 3;
+				}
+			}, ""),
+			Arguments.of(new Random() {
+				@Override
+				public int nextInt(int bound) {
+					return 4;
+				}
+			}, "-"),
+			Arguments.of(new Random() {
+				@Override
+				public int nextInt(int bound) {
+					return 9;
+				}
+			}, "-")
+		);
 	}
 
 	@ParameterizedTest
