d7fe9a609bbd89078bf2862c694eda95d12775af
PIH6585 <pih6585@naver.com>
2021-07-27 14:00:22 +0900

    refactor : cars 일급 컬렉션 적용에 따른 리팩토링 test : 리팩토링에 따른 테스트

--- src/main/java/racing/domain/Racing.java
+++ src/main/java/racing/domain/Racing.java
@@ -1,112 +1,33 @@
 package racing.domain;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
-import racing.domain.Car;
-import racing.view.MessageBox;
 
 public class Racing {
 
-	public static final int RANDOM_VALUE = 10;
-	private final List<Car> cars;
-	private final Random random;
-	private final MessageBox messageBox;
-	private int MAX_VALUE = 0;
+	private final Cars cars;
 
-	public Racing(MessageBox messageBox) {
-		cars = new ArrayList<>();
-		random = new Random();
-		this.messageBox = messageBox;
+	private Racing(String carsName) {
+		cars = new Cars(carsName);
 	}
 
-	public List<Car> createPlayer(String playerListValue) {
-		String[] playerArray = playerListValue.split(",");
-		validStringEmpty(playerListValue.trim());
-		return setupPlayer(playerArray);
+	private Racing(Cars cars) {
+		this.cars = cars;
 	}
 
-	private void validStringEmpty(String value) {
-		if (value.isEmpty()) {
-			throw new IllegalArgumentException("자동차는 최소 1대 이상이어야 한다.");
-		}
+	public static Racing raceStart(String carsName) {
+		return new Racing(carsName);
 	}
 
-	private List<Car> setupPlayer(String[] playerArray) {
-		for (String playerName : playerArray) {
-			Car car = new Car(playerName);
-			cars.add(car);
-		}
-		return cars;
+	public static Racing raceStart(Cars car) {
+		return new Racing(car);
 	}
 
-	public void playingGame(String inputValue) {
-		int tryGameCount = toInt(inputValue);
-		messageBox.resultMessage();
-		carRacing(tryGameCount);
-		messageBox.winnerMessageBox(winnerPlayer().substring(0, winnerPlayer().length() - 1));
+	public Cars carsRaceForResult() {
+		return cars.racing(new RandomMoving());
 	}
 
-	public int toInt(String value) {
-		validNumberCheck(value);
-		return Integer.parseInt(value);
+	public List<Car> winnerPlayer() {
+		return cars.winnerCars();
 	}
 
-	private void validNumberCheck(String value) {
-		String regExp = "^\\d+$";
-		if (!value.matches(regExp)) {
-			throw new IllegalArgumentException("해당 문자는 숫자만 사용 가능합니다.");
-		}
-	}
-
-	public void carRacing(int tryGameCount) {
-		for (int i = 0; i < tryGameCount; i++) {
-			carsRaceForResult();
-			messageBox.MessageBoxNextLine();
-		}
-	}
-
-	private void carsRaceForResult() {
-		for (Car car : cars) {
-			int resultRacing = car.carMove(randomValue());
-			maxValueCheck(resultRacing);
-			messageBox.racingResultMessage(car.getCarName(), getRacingResultByString(resultRacing));
-		}
-	}
-
-	public String getRacingResultByString(int racingResult) {
-		StringBuilder result = new StringBuilder(racingResult);
-		for (int i = 0; i < racingResult; i++) {
-			result.append("-");
-		}
-		return result.toString();
-	}
-
-	private void maxValueCheck(int resultValue) {
-		if (MAX_VALUE < resultValue) {
-			MAX_VALUE = resultValue;
-		}
-	}
-
-	public int randomValue() {
-		return random.nextInt(RANDOM_VALUE);
-	}
-
-	public String winnerPlayer() {
-		StringBuilder winnerResult = new StringBuilder();
-		for (Car car : cars) {
-			winnerResult.append(searchWinner(car.resultRacing(), car.getCarName()));
-		}
-		return winnerResult.toString();
-	}
-
-	public String searchWinner(int carRacingResult, String carName) {
-		StringBuilder winnerName = new StringBuilder();
-		if (carRacingResult == MAX_VALUE) {
-			winnerName.append(carName);
-			winnerName.append(",");
-		}
-		return winnerName.toString();
-	}
 }
--- src/test/java/racing/domain/RacingTest.java
+++ src/test/java/racing/domain/RacingTest.java
@@ -1,84 +1,50 @@
 package racing.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import racing.domain.Car;
-import racing.domain.Racing;
-import racing.view.MessageBox;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class RacingTest {
 
-    private Racing racing;
+    @Test
+    @DisplayName("경주에 경기 경과를 알 수 있다.")
+    public void returnRacingResult() {
+        Cars cars = racingMovedResult();
+        Racing racing = Racing.raceStart(cars);
+        Cars result = racing.carsRaceForResult();
 
-    @BeforeEach
-    void setUp() {
-        MessageBox messageBox = new MessageBox();
-        racing = new Racing(messageBox);
+        Assertions.assertThat(result.getInformation()).extracting("position.position").containsExactly(2,3,2);
     }
 
     @Test
-    @DisplayName("자동차는 입력값의 , 에따라 생성된다.")
-    public void createPayerBySplit() {
-        List<Car> cars = racing.createPlayer("car1,car2,car3");
+    @DisplayName("경주에 최종승자를  알 수 있다.")
+    public void returnWinners() {
+        Cars cars = racingMovedResult();
+        Racing racing = Racing.raceStart(cars);
+        List<Car> winners = racing.winnerPlayer();
 
-        assertThat(cars.size()).isEqualTo(3);
+        Assertions.assertThat(winners).extracting("name.name").containsExactly("car2");
     }
 
+    private Cars racingMovedResult(){
+        Car car1 = new dummyCar("car1",2);
+        Car car2 = new dummyCar("car2",3);
+        Car car3 = new dummyCar("car3",2);
 
-    @Test
-    @DisplayName("자동차의 이름은 5글자를 초과하면 예외가 발생된다.")
-    public void createPayerNameLimit_check() {
-        assertThatThrownBy(() -> racing.createPlayer("naming,para,you"))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("자동차 이름은 5글자를 초과할수 없습니다.");
+        return new Cars(car1,car2,car3);
     }
 
-    @Test
-    @DisplayName("자동차는 최소 1대이상 아니면 예외가 발생된다.")
-    public void createPayerAtListOne_check() {
-        assertThatThrownBy(() -> racing.createPlayer(" "))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("자동차는 최소 1대 이상이어야 한다.");
-    }
+    private static class dummyCar extends Car {
+        public dummyCar(String carName, int position) {
+            super(carName, position);
+        }
 
-
-    @Test
-    @DisplayName("랜덤함수는 0~9사이의 값을 가지고 있다.")
-    public void randomValue_check() {
-        int firstValue = 0;
-        int lastValue = 10;
-
-        int result1 = racing.randomValue();
-
-        betweenCheck(result1, firstValue, lastValue);
-
-        int result2 = racing.randomValue();
-
-
-        betweenCheck(result2, firstValue, lastValue);
-    }
-
-    private void betweenCheck(int checkValue, int firstValue, int lastValue) {
-        assertThat(checkValue).isGreaterThanOrEqualTo(firstValue).isLessThan(lastValue);
-    }
-
-    @ParameterizedTest
-    @DisplayName("경주의 결과만큼 '-'로 반환된다.")
-    @CsvSource(value = {"1,-","2,--","3,---","4,----","5,-----"})
-    public void intResultToStringResult(int intResult, String stringResult){
-        racing.createPlayer("car1,car2,car3");
-        String racingResultByString = racing.getRacingResultByString(intResult);
-
-        assertThat(racingResultByString).isEqualTo(stringResult);
+        @Override
+        public void move(boolean moveResult) {
+            super.move(false);
+        }
     }
 
 }
