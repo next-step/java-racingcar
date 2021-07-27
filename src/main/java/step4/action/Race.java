2c9342bdd8f11c4aa8afa9b9b74ad4a4fc6fa919
newchk610@gmail.com <gprms@1208>
2021-07-27 13:05:21 +0900

    refactor: remove 사용하지 않는 로직 제거

--- src/main/java/step4/action/Race.java
+++ src/main/java/step4/action/Race.java
@@ -9,43 +9,14 @@ import java.util.List;
 
 public class Race {
 
-    private int size;
     private Cars cars;
 
     public Race(List<Car> carList) {
         this.cars = new Cars(carList,new CarMoveAndArrangeStrategy());
-        this.size = carList.size();
     }
-//
-//    private Race(String[] names){
-//        this.cars = new Cars(new CarMoveAndArrangeStrategy());
-//        cars.makeCars(names);
-//        this.size = cars.getCarList().size();
-//    }
-//
-//    public static Race of(List<Car> cars){
-//        return new Race(cars);
-//    }
-//    public static Race of(String[] names){
-//        return new Race(names);
-//    }
+
     public void doRace(){
         cars.moveCars();
-    }
-
-    public int getCarsSize() {
-        return this.size;
-    }
-
-    public List<Integer> getPositions() {
-        if(size <= 0){
-            throw new IllegalStateException("주행할 자동차가 없습니다.");
-        }
-        List<Integer> result = new ArrayList<>();
-        for (Car car : cars.getCarList()) {
-            result.add(car.getPosition());
-        }
-        return result;
     }
 
     public List<Car> getCars() {
@@ -61,13 +32,13 @@ public class Race {
         List<Car> winners = new ArrayList<Car>();
         int longestLength = cars.get(0).getPosition();
         winners.add(cars.get(0));
-        for(int idx=1; idx < cars.size(); idx++){
-            findWinner(winners, cars.get(idx),longestLength);
+        for(int idx = 1; idx < cars.size(); idx++){
+            findCoWinner(winners, cars.get(idx),longestLength);
         }
         return winners;
     }
 
-    private void findWinner(List<Car> winners, Car car, int target) {
+    private void findCoWinner(List<Car> winners, Car car, int target) {
         if(car.getPosition() == target){
             winners.add(car);
         }
--- src/main/java/step4/model/Cars.java
+++ src/main/java/step4/model/Cars.java
@@ -17,20 +17,6 @@ public class Cars {
         this.carsMethod = carsMethod;
     }
 
-    public void makeCars(int carCount) {
-        carList = new ArrayList<Car>();
-        for (int i = 0; i < carCount; i++) {
-            carList.add(new Car());
-        }
-    }
-
-    public void makeCars(String[] cars) {
-        carList = new ArrayList<Car>();
-        for (String name : cars) {
-            carList.add(new Car(name));
-        }
-    }
-
     protected void makeCars(Car... cars) {
         carList = new ArrayList<Car>();
         for (Car car : cars) {
@@ -50,7 +36,7 @@ public class Cars {
 
     public void stateCheck() {
         if (carList == null || carList.size() == 0) {
-            throw new IllegalStateException("자동차를 먼저 생성해 주세요.");
+            throw new IllegalStateException("주행할 자동차가 없습니다.");
         }
     }
 
