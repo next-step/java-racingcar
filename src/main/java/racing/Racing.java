ac42d45824b8981e85702d5e16b00114a3627e47
김남준 <namjun88@gmail.com>
2021-07-27 17:11:49 +0900

    test code

--- src/main/java/racing/Racing.java
+++ src/main/java/racing/Racing.java
@@ -1,22 +1,97 @@
 package racing;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Racing {
 
+    static Boolean isShow = false;
+
     public static void main(String args[]) {
 
         System.out.println("자동차 대수는 몇 대 인가요?");
-
-        Scanner carNumInput = new Scanner(System.in);
-        String carNumValue = carNumInput.nextLine();
-        int carNum = carNumInput.nextInt();
-
+        Integer carNum = setNumber();
         System.out.println("시도할 회수는 몇 회 인가요?");
-
-        Scanner tryNumInput = new Scanner(System.in);
-        String tryNumValue = tryNumInput.nextLine();
-        int tryNum = tryNumInput.nextInt();
+        Integer tryNum = setNumber();
+        playGame(carNum, tryNum);
 
     }
+
+    public static Integer setNumber() {
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
+    }
+
+    public static void playGame(Integer carNum, Integer tryNum) {
+
+        Integer[] carList = gameInitiate(carNum);
+
+        while(isGameOver(carList, tryNum)) {
+            carList = setCarMove(carList);
+            showGame(carList);
+        }
+    }
+
+    public static Boolean isGameOver(Integer[] carList, Integer tryNum) {
+        for(int idx = 0; idx < carList.length ; idx++) {
+            if (tryNum.equals(carList[idx])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static Integer[] gameInitiate(int carNum) {
+        Integer[] carList = new Integer[carNum];
+        for(int idx = 0; idx < carNum; idx++) {
+            carList[idx] = 0;
+        }
+        return carList;
+    }
+
+    public static void showGame(Integer[] carList) {
+        if(!isShow) {
+            return;
+        }
+
+        System.out.println("");
+
+        for(int idx = 0; idx < carList.length; idx++) {
+            String movement = "";
+            for(int cnt = 0; cnt < carList[idx]; cnt++) {
+                movement = movement + "-";
+            }
+            System.out.println(movement);
+        }
+    }
+
+    public static Integer[] setCarMove(Integer[] carList) {
+        isShow = false;
+        for(int idx = 0; idx < carList.length; idx++) {
+
+            if(isMove(makeRanNum())) {
+                carList[idx] = carList[idx] + 1;
+            }
+        }
+        return carList;
+    }
+
+    public static Integer makeRanNum() {
+        Random random = new Random(); //랜덤 객체 생성(디폴트 시드값 : 현재시간)
+        random.setSeed(System.currentTimeMillis()); //시드값 설정을 따로 할수도 있음
+        return random.nextInt(10);
+    }
+
+    public static Boolean isMove(Integer number) {
+
+
+        if(number >= 4) {
+            isShow =  true;
+            return true;
+        }
+        return false;
+    }
+
+
 }
--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -1,13 +1,84 @@
 package racing;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import step2.Calculator;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RacingTest {
 
+
+    public static Racing racing = new Racing();
+
     @Test
-    public void racing() {
+    public void isGameOverTest() {
 
+        Integer tryNum = 5;
 
+        Integer[] carList1 = new Integer[3];
+        Integer[] carList2 = new Integer[3];
+        carList1[0] = 1;
+        carList1[1] = 2;
+        carList1[2] = 3;
+
+        carList2[0] = 0;
+        carList2[1] = 5;
+        carList2[2] = 0;
+
+        Boolean result1 = racing.isGameOver(carList1, tryNum);
+        Boolean result2 = racing.isGameOver(carList2, tryNum);
+
+        assertEquals(result1,true);
+        assertEquals(result2,false);
 
     }
+
+    @Test
+    public void gameInitiateTest() {
+        Integer carNum = 3;
+
+        Integer[] result = racing.gameInitiate(carNum);
+
+        for(int idx = 0; idx < carNum; idx++) {
+            assertEquals(0,result[idx]);
+        }
+
+    }
+
+    @Test
+    public void isMoveTest() {
+
+        Boolean result1 = racing.isMove(1);
+        Boolean result2 = racing.isMove(5);
+
+        assertEquals(result1,false);
+        assertEquals(result2,true);
+    }
+
+    @Test
+    public void makeRanNumTest() {
+
+        Integer result1 = racing.makeRanNum();
+
+        String regExp = "^[0-9]+$";
+
+        assertEquals(result1.toString().matches(regExp), true);
+    }
+
+    @Test
+    public void setCarMoveTest() {
+
+        Integer[] carList1 = new Integer[3];
+        carList1[0] = 1;
+        carList1[1] = 2;
+        carList1[2] = 3;
+
+        racing.setCarMove(carList1);
+
+        //??
+    }
+
 }
