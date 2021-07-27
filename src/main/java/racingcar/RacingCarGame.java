575efc66f6001df7f37d6dee1c4a3da1f46865b4
harris <harrisleesh@gmail.com>
2021-07-27 21:26:09 +0900

    refactor : Change strategy dependency into moveIfMovable() function

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -18,13 +18,13 @@ public class RacingCarGame {
         List<RacingCar> racingCars = new ArrayList<>();
         int carNumber = inputView.requestCarNumber();
         int tryNumber = inputView.requestTryNumber();
-        initializeRacingCars(racingCars, carNumber, new RandomMoveStrategy());
+        initializeRacingCars(racingCars, carNumber);
         race(racingCars, tryNumber);
     }
 
-    public static void initializeRacingCars(List<RacingCar> racingCars, int carNumber, MoveStrategy moveStrategy) {
+    public static void initializeRacingCars(List<RacingCar> racingCars, int carNumber) {
         for (int i = 0; i < carNumber; i++) {
-            racingCars.add(new RacingCar(moveStrategy));
+            racingCars.add(new RacingCar());
         }
     }
 
@@ -38,7 +38,7 @@ public class RacingCarGame {
 
     public static void raceOneStep(List<RacingCar> racingCars) {
         for (int i = 0; i < racingCars.size(); i++) {
-            racingCars.get(i).moveIfMovable();
+            racingCars.get(i).moveIfMovable(new RandomMoveStrategy());
         }
     }
 }
