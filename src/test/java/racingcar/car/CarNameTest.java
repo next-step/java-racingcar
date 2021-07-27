15aa89c147b01e835526031d94ab153a8a2edb70
seondongpyo <seondongpyo@gmail.com>
2021-07-27 10:09:18 +0900

    test: 자동차 이름 클래스인 CarName에 대한 테스트 작성

---
+++ src/test/java/racingcar/car/CarNameTest.java
@@ -0,0 +1,26 @@
+package racingcar.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class CarNameTest {
+
+    @DisplayName("자동차 이름이 null 또는 공백일 경우 예외가 발생한다.")
+    @ParameterizedTest(name = "자동차 이름 : {0}")
+    @NullAndEmptySource
+    void nullOrEmpty(String name) {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new CarName(name));
+    }
+
+    @DisplayName("자동차 이름의 길이는 5를 초과할 수 없다.")
+    @Test
+    void maxLength() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new CarName("newcar"));
+    }
+}
