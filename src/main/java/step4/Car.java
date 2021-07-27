2f708d4cdd04aaad16178e13be3feb514e91bb81
sk1737030 <sk1737030@naver.com>
2021-07-27 09:39:51 +0900

    Refactor: 매직상수로 변경

--- src/main/java/step4/Car.java
+++ src/main/java/step4/Car.java
@@ -1,7 +1,7 @@
 package step4;
 
 public class Car {
-
+    private static final int LIMIT_NAME_LENGTH = 5;
     private final String name;
 
     private final CarEngine carEngine;
@@ -29,7 +29,7 @@ public class Car {
     }
 
     protected String checkCarNameLength(String carName) {
-        if (carName.length() > 5) {
+        if (carName.length() > LIMIT_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자리 초과 할 수 없습니다.");
         }
 
