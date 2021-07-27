2252ebd1241e5a4c848b51ea1f8c08533bac4132
ChangSubKwak <paradise1732@gmail.com>
2021-07-27 09:39:30 +0900

    refactor: 코드 컨벤션에 맞도록 변수 선언 위치를 변경하였음

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class RacingCar {
-	public int position;
+	// https://velog.io/@kwj1270/JAVA-%EC%BD%94%EB%93%9C-%EC%BB%A8%EB%B2%A4%EC%85%98
+	// 4. 클래스 변수(static) public -> protected -> package(default) -> private
 	public static final int INITIAL_POSITION = 0;
 	private static final int MOVABLE_MINIMUM_VALUE = 4;
+
+	// 5. 일반 변수 public -> protected -> package(default) -> private
+	public int position;
 
 	public RacingCar(int position) {
 		this.position = position;
