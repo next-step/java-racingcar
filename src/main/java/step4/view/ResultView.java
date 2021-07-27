ea595f8b9ba1f0ff1bb981c553f5789c9165513b
newchk610@gmail.com <gprms@1208>
2021-07-27 10:09:56 +0900

    refactor: 안쓰는 메서드 정리

--- src/main/java/step4/view/ResultView.java
+++ src/main/java/step4/view/ResultView.java
@@ -1,21 +1,10 @@
 package step4.view;
 
-import step4.action.Race;
 import step4.model.Car;
 
 import java.util.List;
 
 public class ResultView {
-    public static void startRaceStep3(int carCounts, int raceTimes) {
-        Race race = Race.of(carCounts);
-        Print.print("실행 결과");
-        for(int i=0; i<raceTimes; i++){
-            Print.print(race.getCars());
-            race.doRace();
-            System.out.println();
-        }
-    }
-
     public static void printResult(List<Car> cars) {
         Print.print("실행 결과");
         Print.printWinner(cars);
