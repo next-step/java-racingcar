2cd42cfb89e149249a1251b1c4f8e4c0d9c5d95b
bj-lee90 <littleprince0717@gmail.com>
2021-07-27 10:00:10 +0900

    Step1 - 학습 테스트 실습  (#2416)
    * feat: Test Learning for String Classes
    
    * feat(test): added test disaply name
    
    added test disaply name
    
    * feat(test): step1 - test set collection size
    
    step1 - test set collection size
    
    * feat(test): step1 - test set collection contains
    
    step1 - test set collection contains with JUnit ParameterizedTest
    
    * feat(test): step1 - test set collection contains
    
    step1 - test set collection contains with JUnit ParameterizedTest and CsvSource
    
    Co-authored-by: BJ_LEE <bj.lee@navercorp.com>

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,18 +7,21 @@ import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
+    @DisplayName("String split test")
     @Test
     void split_test1() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
 
+    @DisplayName("Short string split test")
     @Test
     void split_test2() {
         String[] result = "1".split(",");
         assertThat(result).containsExactly("1");
     }
 
+    @DisplayName("String substring test")
     @Test
     void substring_test1() {
         String inputStr = "(1,2)";
