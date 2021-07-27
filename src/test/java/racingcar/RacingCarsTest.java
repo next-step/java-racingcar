7419e9a9f70b6f53f538d91496703d3bb47baf33
ChangSubKwak <paradise1732@gmail.com>
2021-07-27 15:44:56 +0900

    fix: 전략패턴 도입에 따른 테스트 케이스 변경 및 수정

--- src/test/java/racingcar/RacingCarsTest.java
+++ src/test/java/racingcar/RacingCarsTest.java
@@ -8,20 +8,21 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import racingcar.model.RacingCars;
+import racingcar.strategy.AlwaysMovableStrategy;
 
 class RacingCarsTest {
 	RacingCars racingCars;
 
 	@BeforeEach
 	void setUp() {
-		racingCars = new RacingCars(3);
+		racingCars = new RacingCars(3, new AlwaysMovableStrategy());
 	}
 
 
 	@ParameterizedTest
 	@ValueSource(ints = { 1, 2, 4 })
 	void 생성자를_호출하였을_때_초기값_0으로_위치값이_세팅된다(int carNum) {
-		racingCars = new RacingCars(carNum);
+		racingCars = new RacingCars(carNum, new AlwaysMovableStrategy());
 		racingCars.getCars().stream().forEach(racingCar -> {
 			assertThat(racingCar.getRacingCarDashString()).isEqualTo("");
 		});
@@ -30,7 +31,6 @@ class RacingCarsTest {
 	@Test
 	void 모든_자동차들의_전진함수가_호출된다면_모든_자동차들은_한_칸씩_전진한다() {
 		racingCars.getCars().stream()
-			.filter(racingCar -> true)				// 무조건 전진한다고 가정
 			.forEach(racingCar -> {
 				racingCar.forward();
 			});
