8b16bcb850c2f8e8cd7a2ab58392db772389ff0a
shineoov <shineoov88@gmail.com>
2021-07-27 11:53:24 +0900

    refactor: ResultView ident depth가 2가 넘지 않게 수정

--- src/main/java/racingcar/ui/ResultView.java
+++ src/main/java/racingcar/ui/ResultView.java
@@ -9,32 +9,48 @@ import java.util.NoSuchElementException;
 
 public class ResultView {
 
-    private static final String RESULT_PRE_MESSAGE = "실행결과\n";
+    private static final String RESULT_MESSAGE_PRE = "실행결과\n";
+    private static final String RESULT_MESSAGE_WINNER_POST = "가 최종 우승했습니다.";
     private static final String RACING_DISPLAY = "-";
+    private static final String CAR_SEPARATOR = " : ";
+    private static final String WINNER_SEPARATOR = ", ";
+    private static final String LINE_BREAK = "\n";
 
     private ResultView() {
     }
 
     public static void showRacingInfo(List<List<RacingRecord>> racingHistory) {
-        System.out.print(RESULT_PRE_MESSAGE);
+        System.out.print(RESULT_MESSAGE_PRE);
         for (List<RacingRecord> records : racingHistory) {
-            for (RacingRecord record : records) {
-                System.out.print(record.getCar().getName() + " : ");
-                for (int i = 0; i < record.getPosition(); i++) {
-                    System.out.print(RACING_DISPLAY);
-                }
-                System.out.println();
-            }
-            System.out.println();
+            System.out.println(getRacingCarsInfo(records));
         }
+    }
+
+    private static StringBuffer getRacingCarsInfo(List<RacingRecord> records) {
+        StringBuffer sb = new StringBuffer();
+        for (RacingRecord record : records) {
+            sb.append(record.getCar().getName());
+            sb.append(CAR_SEPARATOR);
+            sb.append(getRacingPositionDisplay(record));
+            sb.append(LINE_BREAK);
+        }
+        return sb;
+    }
+
+    private static StringBuffer getRacingPositionDisplay(RacingRecord record) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < record.getPosition(); i++) {
+            sb.append(RACING_DISPLAY);
+        }
+        return sb;
     }
 
     public static void showRacingWinner(List<RacingRecord> finalRacingHistory) {
         List<String> winners = getRacingWinner(finalRacingHistory);
-        System.out.println(String.join(", ", winners) + "가 최종 우승했습니다.");
+        System.out.println(String.join(WINNER_SEPARATOR, winners) + RESULT_MESSAGE_WINNER_POST);
     }
 
-    public static List<String> getRacingWinner(List<RacingRecord> racingRecords) {
+    private static List<String> getRacingWinner(List<RacingRecord> racingRecords) {
         int maxPosition = getRacingMaxPosition(racingRecords);
         List<String> winners = new ArrayList<>();
         racingRecords.stream()
@@ -43,7 +59,7 @@ public class ResultView {
         return winners;
     }
 
-    public static int getRacingMaxPosition(List<RacingRecord> finalRacingHistory) {
+    private static int getRacingMaxPosition(List<RacingRecord> finalRacingHistory) {
         return finalRacingHistory.stream()
                 .max(Comparator.comparing(RacingRecord::getPosition))
                 .orElseThrow(NoSuchElementException::new)
