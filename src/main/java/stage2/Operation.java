071e9caa7fa2111b6bb30ee1d7ca2fd22acd1fea
Jade <j2de.dev@gmail.com>
2021-07-27 12:23:55 +0900

    Step1 - stage2 문자열 계산기 (#2448)
    * split study test
    
    * refactor: move StringTest
    
    * docs: Add todo list
    
    * feat: substring test
    
    * feat: chatAt, exception test
    
    * feat: set size test
    
    * feat: @ParameterizedTest
    
    * feat: @CsvSource
    
    * docs: stage2 요구사항 정리
    
    * refactor: package name 변경 study > stage1
    
    * feat: 계산기 사칙연산 구현
    
    * refactor: 연산자 분리
    
    * feat: input null, 공백 체크 추가
    
    * test: 기능 및 exception 테스트 추가
    
    * docs: todo
    
    * refactor: test 중복 제거
    
    * refactor: method name 변경
    
    * feat: 코드리뷰 피드백 반영
    
    - final 추가
    - Calculator 메소드 분리
    - Expression 생성

---
+++ src/main/java/stage2/Operation.java
@@ -0,0 +1,31 @@
+package stage2;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public enum Operation {
+    ADD("+", ((first, second) -> first + second)),
+    MINUS("-", ((first, second) -> first - second)),
+    MULTI("*", ((first, second) -> first * second)),
+    DIVIDE("/", ((first, second) -> first / second));
+
+    private final String value;
+    private final BiFunction<Integer, Integer, Integer> expression;
+
+    Operation(String value, BiFunction<Integer, Integer, Integer> expression) {
+        this.value = value;
+        this.expression = expression;
+    }
+
+    public Integer operate(Integer first, Integer second) {
+        return expression.apply(first, second);
+    }
+
+    public static Operation findOperation(String input) {
+        return Arrays.stream(Operation.values())
+                .filter(i -> i.value.equals(input))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+}
