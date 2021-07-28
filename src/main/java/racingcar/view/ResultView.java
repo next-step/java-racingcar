57b7035e59390f082c3648c34670106a15de405b
ChangSubKwak <paradise1732@gmail.com>
2021-07-28 18:14:02 +0900

    feat: 우승자 출력 문구 상수, 우승자 출력 기능 추가

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -4,15 +4,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import racingcar.model.RacingCar;
+import racingcar.model.RacingCars;
 
 public class ResultView {
 	private static final String EXECUTE_RESULT = "실행 결과";
+	private static final String OUTPUT_WINNER_STATEMENT = "가 최종 우승했습니다.";
 
 	public void outputResult(List<RacingCar> racingCars) {
 		String result = racingCars.stream().map(racingCar ->
-			racingCar.getRacingCarDashString()
+			racingCar.getCarName() + " : " + racingCar.getRacingCarDashString()
 		).collect(Collectors.joining("\n"));
 		System.out.println(result + "\n");
+	}
+
+	public void outputWinners(RacingCars racingCars) {
+		String winnersString =  racingCars.getWinners().toString();
+		System.out.println(winnersString.substring(1, winnersString.length() - 1) + OUTPUT_WINNER_STATEMENT);
 	}
 
 	public void outputStatement() {
