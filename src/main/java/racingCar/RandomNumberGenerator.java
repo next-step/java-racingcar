608e3bb35b1708e192b527e7c42d21fadd6202ca
sixthou <justyuk@naver.com>
2021-07-29 00:45:20 +0900

    fix : private 생성자 추가

--- src/main/java/racingCar/RandomNumberGenerator.java
+++ src/main/java/racingCar/RandomNumberGenerator.java
@@ -1,10 +1,16 @@
 package racingCar;
 
+import sun.jvm.hotspot.utilities.AssertionFailure;
+
 import java.util.Random;
 
 public class RandomNumberGenerator {
     static private final int THRESHOLD = 10;
     static Random random = new Random();
+
+    private RandomNumberGenerator() {
+        throw new AssertionFailure();
+    }
 
     static public int getRandomNumber(){
         return random.nextInt(THRESHOLD);
