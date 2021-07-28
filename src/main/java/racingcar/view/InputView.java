9c20a55aa51cb1ce65c1501a40687173facb4c1a
ChangSubKwak <paradise1732@gmail.com>
2021-07-29 04:59:39 +0900

    feat: 시도횟수 값(1보다 작은수 예외발생)과 자동차 이름 문자열(콤마로 시작 또는 종료일 경우 예외 발생)의 유효성 체크 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,20 +1,20 @@
 package racingcar.view;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class InputView {
 	private static Scanner scanner = new Scanner(System.in);
-	private static final String INPUT_RACING_CAR_NUM_STATEMENT = "자동차 대수는 몇 대 인가요?";
 	private static final String INPUT_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
 	private static final String INPUT_CAR_NAME_LIST = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 
 	private int inputIntValue() {
-		return scanner.nextInt();
-	}
-
-	public int inputRacingCarNum() {
-		System.out.println(INPUT_RACING_CAR_NUM_STATEMENT);
-		return inputIntValue();
+		int input = scanner.nextInt();
+		if (input <= 0) {
+			throw new NoSuchElementException("1보다 작은 값을 입력하셨습니다.");
+		}
+		return input;
 	}
 
 	public int inputTryCount() {
@@ -22,9 +22,21 @@ public class InputView {
 		return inputIntValue();
 	}
 
-	public String[] inputCarNameList() {
+	private void validateCarNames(String carNames) {
+		boolean isExistFirstComma = carNames.charAt(0) == ',';
+		boolean isExistLastComma = carNames.charAt(carNames.length() - 1) == ',';
+
+		if (isExistFirstComma || isExistLastComma) {
+			throw new InputMismatchException("콤마가 맨앞 또는 맨뒤에 위치하였습니다.");
+		}
+	}
+
+	public String[] inputCarNames() {
 		System.out.println(INPUT_CAR_NAME_LIST);
-		return scanner.nextLine().split(",");
+		String carNames = scanner.nextLine().trim();
+		validateCarNames(carNames);
+
+		return carNames.split(",");
 	}
 
 	public void scannerClose() {
