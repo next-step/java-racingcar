353a9a64fd12a3ee1b6f8e23b0d59257c193d40d
조원진 <>
2021-07-28 09:36:18 +0900

    feat(Position) : getPosition 메소드 추가

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -25,6 +25,10 @@ public class Position {
         return new Position(position + 1);
     }
 
+    public int getPosition() {
+        return position;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
