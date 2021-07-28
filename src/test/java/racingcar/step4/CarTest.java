1506f665bb7a9c48fffdc138ecce4994464fde97
gunny6026 <gunny6026@naver.com>
2021-07-28 18:02:49 +0900

    fix : Car 접근 제어자 수정에 따른 자동차 전진 테스트 수정

--- src/test/java/racingcar/step4/CarTest.java
+++ src/test/java/racingcar/step4/CarTest.java
@@ -10,21 +10,22 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
+    String carList[] = {"pobi", "coco", "crong"};
 
     @Test
     @DisplayName("자동차 객체 생성 테스트")
-    void 자동차객체생성(){
-        String []cars = {"pobi","cony","hee"};
+    void 자동차객체생성() {
+        String[] cars = {"pobi", "cony", "hee"};
         List<Car> carList = Car.createCars(cars);
         assertThat(cars.length).isEqualTo(carList.size());
     }
 
     @ParameterizedTest
     @DisplayName("자동차 전진 테스트")
-    @CsvSource(value = {"1:0","2:0","3:0","4:1","5:1","6:1","7:1","8:1","9:1"},delimiter = ':')
-    void 자동차전진(int number , int distance){
-        Car car = new Car("pobi",0);
-        car.move(number);
-        assertThat(car.getDistance()).isEqualTo(distance);
+    @CsvSource(value = {"1:0", "2:0", "3:0", "4:1", "5:1", "6:1", "7:1", "8:1", "9:1"}, delimiter = ':')
+    void 자동차전진(int number, int distance) {
+        List<Car> cars = Car.createCars(carList);
+        cars.get(0).move(number);
+        assertThat(cars.get(0).getDistance()).isEqualTo(distance);
     }
 }
