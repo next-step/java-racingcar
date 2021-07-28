d5d06d0d94793e447e2cbd7a63186a763ad9d25e
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-07-28 13:31:34 +0900

    refactor: 랜덤 상수화

--- src/main/java/edu/nextstep/racingcar/step5/domain/DefaultMoveStrategy.java
+++ src/main/java/edu/nextstep/racingcar/step5/domain/DefaultMoveStrategy.java
@@ -6,9 +6,10 @@ public class DefaultMoveStrategy implements MoveStrategy {
 
     private static final int NUMBER_OF_RANDOMS = 10;
     private static final int THRESHOLD = 4;
+    private static final Random random = new Random();
 
     @Override
     public boolean isMove() {
-        return new Random().nextInt(NUMBER_OF_RANDOMS) >= THRESHOLD;
+        return random.nextInt(NUMBER_OF_RANDOMS) >= THRESHOLD;
     }
 }
