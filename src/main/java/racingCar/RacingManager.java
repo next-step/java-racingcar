9a377ba2c84bdd2ecfbbfe90997c2e4dc858e688
sixthou <justyuk@naver.com>
2021-07-29 00:43:46 +0900

    feat : RandomNumberGenerator 생성

--- src/main/java/racingCar/RacingManager.java
+++ src/main/java/racingCar/RacingManager.java
@@ -1,7 +1,13 @@
 package racingCar;
 
+import sun.jvm.hotspot.utilities.AssertionFailure;
+
 public class RacingManager {
     static private final int THRESHOLD = 4;
+
+    private RacingManager() {
+        throw new AssertionFailure();
+    }
 
     static public boolean isMovableNumber(int number){
         return number >= THRESHOLD;
