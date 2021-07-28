7ebfc4552ccc440b781c85ec0fc69be49222b27b
darkant99 <darkant99@naver.com>
2021-07-28 14:33:23 +0900

    [REFACTOR] RacingGame과 RacingController 합치기

--- src/main/java/racing/controller/RacingController.java
+++ src/main/java/racing/controller/RacingController.java
@@ -5,9 +5,10 @@ import racing.domain.dto.GameResponse;
 import racing.domain.fuel.Fuel;
 import racing.domain.fuel.RandomFuel;
 import racing.domain.dto.GameRequest;
-import racing.service.RacingGame;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 public final class RacingController {
@@ -19,11 +20,29 @@ public final class RacingController {
 
     public GameResponse gameRun(GameRequest gameRequest) {
         Fuel fuel = new RandomFuel();
-        List<Cars> gameResults = RacingGame.getInstance().racingAll(
-                gameRequest.cars(), gameRequest.turnSize(), fuel
-        );
+        Cars cars = gameRequest.cars();
+        int turnSize = gameRequest.turnSize();
 
-        return new GameResponse(gameResults);
+        validateCars(cars);
+        validateTurnSize(turnSize);
+
+        List<Cars> carsList = new ArrayList<>();
+        for (int turn = 0; turn < turnSize; turn++) {
+            cars = cars.moveAll(fuel);
+            carsList.add(cars);
+        }
+
+        return new GameResponse(carsList);
+    }
+
+    private void validateCars(Cars cars) {
+        if (Objects.isNull(cars) || cars.isEmpty())
+            throw new IllegalArgumentException("경기를 진행할 차가 없습니다.");
+    }
+
+    private void validateTurnSize(int turnSize) {
+        if (turnSize < 1)
+            throw new IllegalArgumentException("경기를 진행할 횟수가 1보다 작습니다.");
     }
 
     private static class InnerClazz {
--- src/test/java/racing/controller/RacingControllerTest.java
+++ src/test/java/racing/controller/RacingControllerTest.java
@@ -3,10 +3,22 @@ package racing.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import racing.domain.Name;
+import racing.domain.car.Car;
+import racing.domain.car.Cars;
+import racing.domain.car.FakeBasicCar;
+import racing.domain.dto.FakeGameRequest;
 import racing.domain.dto.GameRequest;
 import racing.domain.dto.GameResponse;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class RacingControllerTest {
     @CsvSource(value = {
@@ -20,5 +32,58 @@ class RacingControllerTest {
 
         GameResponse gameResponse = RacingController.getInstance().gameRun(gameRequest);
         assertThat(gameResponse).isNotNull();
+    }
+
+
+    private Cars newCars(String carNames, Function<Name, Car> carCtor) {
+        return new Cars(Arrays.stream(carNames.split("\\|"))
+                .map(Name::new)
+                .map(carCtor)
+                .collect(Collectors.toSet()));
+    }
+
+    @DisplayName("racingAllTest")
+    @CsvSource(
+            "A|B|C|D,E|F,10"
+    )
+    @ParameterizedTest
+    public void racingAllTest(String carNames, String winnerCarNames, int turnSize) {
+
+        Cars cars = newCars(carNames, Car::new);
+        Cars fakeCars = newCars(winnerCarNames, FakeBasicCar::new);
+
+        Set<Car> newCarList = new HashSet<>();
+        for (Car iCar : cars)
+            newCarList.add(iCar);
+        for (Car iCar : fakeCars)
+            newCarList.add(iCar);
+
+        GameRequest gameRequest = new FakeGameRequest(new Cars(newCarList), turnSize);
+
+        GameResponse gameResponse = RacingController.getInstance().gameRun(gameRequest);
+        Cars lastCars = gameResponse.lastValue();
+
+        Cars winners = lastCars.findWinners();
+        for (Car iWinner : winners) {
+            System.out.println(iWinner);
+            assertThat(winnerCarNames.contains(iWinner.toString()))
+                    .withFailMessage("에상한 우승자가 포함되어 있지 않음")
+                    .isTrue();
+        }
+
+        assertThat(winners.size())
+                .withFailMessage("에상한 우승자 수와 다름")
+                .isEqualTo(winnerCarNames.split("\\|").length);
+    }
+
+    @CsvSource({
+            "A,0"
+    })
+    @DisplayName("RacingAll IllegalArgumentException Test")
+    @ParameterizedTest
+    public void racingAll_IllegalArgumentException_Test(String carNames, int turnSize) {
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                racingAllTest(carNames, "QQQQ", turnSize)
+        );
     }
 }
