6afe79d7e3c8befd91073a79ee9ca422f9a6b997
sixthou <justyuk@naver.com>
2021-07-29 01:09:52 +0900

    feat : PrintService 구현

---
+++ src/main/java/racingCar/PrintService.java
@@ -0,0 +1,18 @@
+package racingCar;
+
+public class PrintService {
+
+    static public void printDistance(int number){
+        String distance = getDistance(number);
+        System.out.println(distance);
+    }
+
+    private static String getDistance(int number) {
+        StringBuilder distance = new StringBuilder();
+        for(int i = 0; i< number; i++){
+            distance.append("-");
+        }
+        return distance.toString();
+    }
+
+}
