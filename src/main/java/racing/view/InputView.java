a88dbc06df7f72a4ee4ad8f8cf5b93ae79117982
YooInKeun <keun0390@naver.com>
2021-07-28 18:01:44 +0900

    feat: 자동차 이름 입력받는 기능 추가

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -3,8 +3,9 @@ package racing.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String RACING_CAR_COUNT_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String RACING_CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String MOVEMENT_TRIAL_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회 인가요?";
+    private static final String RACING_CAR_NAMES_DELIMITER = ",";
 
     private final Scanner scanner;
 
@@ -12,12 +13,12 @@ public class InputView {
         this.scanner = new Scanner(System.in);
     }
 
-    public void printRacingCarCountInputMessage() {
-        System.out.println(RACING_CAR_COUNT_INPUT_MESSAGE);
+    public void printRacingCarNamesInputMessage() {
+        System.out.println(RACING_CAR_NAMES_INPUT_MESSAGE);
     }
 
-    public int inputRacingCarCount() {
-        return scanner.nextInt();
+    public String[] inputRacingCarNames() {
+        return scanner.nextLine().split(RACING_CAR_NAMES_DELIMITER);
     }
 
     public void printMovementTrialCountInputMessage() {
