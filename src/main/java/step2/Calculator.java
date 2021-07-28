f6308689ab4696a06747cc47d584db26da0cda30
khcho902 <khcho902@gmail.com>
2021-07-28 13:18:07 +0900

    refactor: ArithmeticOperator enum 클래스를 만들어서 연산자별로 계산하는 로직을 분리

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -6,21 +6,6 @@ public class Calculator {
         return input != null && !input.equals("");
     }
 
-    private static boolean checkOperatorValid(String operator) {
-        return operator.equals("+") || operator.equals("-") ||
-                operator.equals("*") || operator.equals("/");
-    }
-
-    private static long calculateByTwoOperand(long operand1, String operator, long operand2) {
-        if (operator.equals("+"))
-            return operand1 + operand2;
-        if (operator.equals("-"))
-            return operand1 - operand2;
-        if (operator.equals("*"))
-            return operand1 * operand2;
-        return operand1 / operand2;
-    }
-
     public static long calculate(String input){
         if (!checkInputValid(input))
             throw new IllegalArgumentException();
@@ -29,12 +14,9 @@ public class Calculator {
 
         long result = Long.parseLong(tokens[0]);
         for (int i = 1; i < tokens.length; i += 2) {
-            String operator = tokens[i];
+            ArithmeticOperator operator = ArithmeticOperator.of(tokens[i]);
             long operand = Long.parseLong(tokens[i + 1]);
-
-            if (!checkOperatorValid(operator))
-                throw new IllegalArgumentException();
-            result = calculateByTwoOperand(result, operator, operand);
+            result = operator.operate(result, operand);
         }
         return result;
     }
