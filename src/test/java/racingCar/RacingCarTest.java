fdc6862610bb22c6fe90fbc9e4bbf033dd572d57
sixthou <justyuk@naver.com>
2021-07-29 00:44:30 +0900

    test : 랜덤 값 생성 테스트 작성

--- src/test/java/racingCar/RacingCarTest.java
+++ src/test/java/racingCar/RacingCarTest.java
@@ -13,13 +13,17 @@ public class RacingCarTest {
     @Test
     void 차량_전진_테스트() {
         RacingCar racingCar = new RacingCar();
+        assertThat(racingCar.showDrivenDistance()).isEqualTo(0);
         racingCar.goForward();
         assertThat(racingCar.showDrivenDistance()).isEqualTo(1);
     }
 
+    @DisplayName("랜덤으로 생성된 수가 0, 9 사이에 있는지 테스트")
     @Test
     void 랜덤_값_생성() {
-
+        for (int i = 0; i < 20; i++) {
+            assertThat(RandomNumberGenerator.getRandomNumber()).isBetween(0,9);
+        }
     }
 
     @DisplayName("전진 가능한 숫자인지 판단하는 테스트")
