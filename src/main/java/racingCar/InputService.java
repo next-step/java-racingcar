1028d8d4c7202708cf38e3e75561e554eace6ca5
sixthou <justyuk@naver.com>
2021-07-29 00:09:08 +0900

    feat : InputService 클래스 생성, 기능 구현

---
+++ src/main/java/racingCar/InputService.java
@@ -0,0 +1,22 @@
+package racingCar;
+
+import java.util.Scanner;
+
+public class InputService {
+
+    private InputService() {
+        throw new AssertionError();
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static public int getNumberOfCar(){
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return scanner.nextInt();
+    }
+
+    static public int getTryCount(){
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
+    }
+}
