2ea7405b2d25e80c9c4ca3cead9f08d20f74b4dc
jaeyoungjang2 <lion85488@gmail.com>
2021-07-28 22:29:59 +0900

    feat: 우승자 출력 메시지 작성

--- src/main/java/step4/ResultView.java
+++ src/main/java/step4/ResultView.java
@@ -3,11 +3,30 @@ package step4;
 import step3.model.Car;
 
 public class ResultView {
+    private static String maxCarName;
+    private static int maxCarPosition = 0;
+
     public void showCurrCarPosition(int location) {
         for (int i = 0; i < location; i++) {
             System.out.print("-");
         }
         System.out.println();
+    }
 
+    public void showCurrCarName(String name) {
+        System.out.print(name+": ");
+    }
+
+    public void findMaxPositionCarName(String carName, int carPosition) {
+        if (carPosition > maxCarPosition) {
+            maxCarName = carName;
+            maxCarPosition = carPosition;
+        } else if (carPosition == maxCarPosition) {
+            maxCarName += ", "+carName;
+        }
+    }
+
+    public void printFinalResult() {
+        System.out.println(maxCarName+"가 최종 우승했습니다.");
     }
 }
