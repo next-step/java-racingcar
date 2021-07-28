21229cabd5dc4aeb754019db8eeb4fd2ac65b57b
강태윤(Taeyoon Kang)/Bot솔루션개발팀/SKP <sundo.kang@sk.com>
2021-07-28 13:31:53 +0900

    test: 테스트 추가

--- src/test/java/edu/nextstep/racingcar/step5/domain/CarTest.java
+++ src/test/java/edu/nextstep/racingcar/step5/domain/CarTest.java
@@ -1,9 +1,6 @@
 package edu.nextstep.racingcar.step5.domain;
 
 import edu.nextstep.racingcar.common.BusinessException;
-import edu.nextstep.racingcar.step4.domain.Car;
-import edu.nextstep.racingcar.step4.domain.CarName;
-import edu.nextstep.racingcar.step4.domain.DefaultMoveStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,18 +12,26 @@ public class CarTest {
     @DisplayName("자동차 이름 5글자 초과 테스트")
     @Test
     void too_long_car_name() {
-        assertThatThrownBy(() -> new edu.nextstep.racingcar.step4.domain.Car(new edu.nextstep.racingcar.step4.domain.CarName("test12345"))).isInstanceOf(BusinessException.class);
+        assertThatThrownBy(() -> new Car(new CarName("test12345"))).isInstanceOf(BusinessException.class);
     }
 
-    @DisplayName("자동차 이동 테스트")
+    @DisplayName("자동차 이동 테스트 (이동 정책: true)")
     @Test
-    void move() {
-        edu.nextstep.racingcar.step4.domain.Car car = new Car(new CarName("test"));
+    void move_when_strategy_is_true() {
+        Car car = new Car(new CarName("test"));
 
-        do {
-            car.move(new DefaultMoveStrategy());
-        } while (car.getDistance() < 10);
+        car.move(() -> true);
 
-        assertThat(car.getDistance()).isNotEqualTo(1);
+        assertThat(car.getDistance()).isGreaterThan(1);
+    }
+
+    @DisplayName("자동차 이동 테스트 (이동 정책: false)")
+    @Test
+    void move_when_strategy_is_false() {
+        Car car = new Car(new CarName("test"));
+
+        car.move(() -> false);
+
+        assertThat(car.getDistance()).isEqualTo(1);
     }
 }
--- src/test/java/edu/nextstep/racingcar/step5/domain/CarsTest.java
+++ src/test/java/edu/nextstep/racingcar/step5/domain/CarsTest.java
@@ -1,7 +1,5 @@
 package edu.nextstep.racingcar.step5.domain;
 
-import edu.nextstep.racingcar.step4.domain.Cars;
-import edu.nextstep.racingcar.step4.domain.DefaultMoveStrategy;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,17 +8,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
 
-    private static edu.nextstep.racingcar.step4.domain.Cars cars;
+    private static Cars cars;
 
     @BeforeAll
     static void getCars() {
         cars = Cars.create("AAA,BBB,CCC,DDD,EEE");
     }
 
-    @DisplayName("우승자 출력 테스트")
+    @DisplayName("우승자 출력 테스트 (Default 전략)")
     @Test
     void getWInners() {
         cars.play(5, new DefaultMoveStrategy());
         assertThat(cars.getWinners()).isNotEmpty();
+    }
+
+    @DisplayName("우승자 출력 테스트 (전원 우승)")
+    @Test
+    void getWInners_again() {
+        cars.play(5, () -> true);
+        assertThat(cars.getWinners().size()).isEqualTo(5);
+    }
+
+    @DisplayName("Cars 객체 생성 테스트")
+    @Test
+    void create() {
+        Cars cars = Cars.create("AAA,BBB,CCC,DDD,EEE");
+        assertThat(cars.getCars().size()).isEqualTo(5);
     }
 }
