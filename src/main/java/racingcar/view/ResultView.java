3ab3ae73018ca8fbe3e5e65ba04cde4d6c66b4c6
ChangSubKwak <paradise1732@gmail.com>
2021-07-29 04:58:04 +0900

    refactor: 우승자 문자열의 앞 부분을 잘라내기 위한 상수 선언

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -9,6 +9,7 @@ import racingcar.model.RacingCars;
 public class ResultView {
 	private static final String EXECUTE_RESULT = "실행 결과";
 	private static final String OUTPUT_WINNER_STATEMENT = "가 최종 우승했습니다.";
+	private static final int WINNER_STRING_START_INDEX = 1;
 
 	public void outputResult(List<RacingCar> racingCars) {
 		String result = racingCars.stream().map(racingCar ->
@@ -19,7 +20,7 @@ public class ResultView {
 
 	public void outputWinners(RacingCars racingCars) {
 		String winnersString =  racingCars.getWinners().toString();
-		System.out.println(winnersString.substring(1, winnersString.length() - 1) + OUTPUT_WINNER_STATEMENT);
+		System.out.println(winnersString.substring(WINNER_STRING_START_INDEX, winnersString.length() - 1) + OUTPUT_WINNER_STATEMENT);
 	}
 
 	public void outputStatement() {
