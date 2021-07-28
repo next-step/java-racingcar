6982e9377125f1094093ee9a42934f1a0a56a437
sangw0804 <sangw0804@likelion.org>
2021-07-29 00:58:02 +0900

    refactor(OutputView): 메소드 분리

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -4,14 +4,22 @@ public class OutputView {
     public static void printResult(RaceResult result) {
         System.out.println("실행 결과");
         for (Lap lap : result.getLaps()) {
-            for (Distance distance : lap.getDistances()) {
-                StringBuffer sb = new StringBuffer();
-                for (int d = 0; d < distance.getDistance(); d++) {
-                    sb.append('-');
-                }
-                System.out.println(sb);
-            }
+            printLap(lap);
             System.out.println();
         }
+    }
+
+    private static void printLap(Lap lap) {
+        for (Distance distance : lap.getDistances()) {
+            printDistance(distance);
+        }
+    }
+
+    private static void printDistance(Distance distance) {
+        StringBuffer sb = new StringBuffer();
+        for (int d = 0; d < distance.getDistance(); d++) {
+            sb.append('-');
+        }
+        System.out.println(sb);
     }
 }
