79a2063c241e203c8c4551ac95b4d48e8966fe15
gashe-soo <gashe7@gmail.com>
2021-07-29 23:56:26 +0900

    feat:자동차 이름 검증 로직

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -6,13 +6,15 @@ public class Car {
     private static final int UPPER_BOUND = 9;
 
     private Position position;
+    private Name name;
 
-    public Car() {
-        this(0);
+    public Car(String name) {
+        this(0, name);
     }
 
-    public Car(int position) {
+    public Car(int position, String name) {
         this.position = new Position(position);
+        this.name = new Name(name);
     }
 
     public void move(int number) {
--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,9 +1,12 @@
 package racing;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class CarTest {
@@ -11,7 +14,7 @@ public class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {4, 9})
     void 랜덤숫자_4이상_9이하일경우_전진(int number) {
-        Car car = new Car(0);
+        Car car = new Car(0, "test");
         car.move(number);
 
         assertThat(car.getNowPosition()).isEqualTo(1);
@@ -21,11 +24,27 @@ public class CarTest {
     @ValueSource(ints = {-1, 0, 10})
     void 랜덤숫자_4미만_9초과일경우_전진_실패(int number) {
         int position = 5;
-        Car car = new Car(position);
+        Car car = new Car(position, "test");
         car.move(number);
 
         assertThat(car.getNowPosition()).isEqualTo(position);
     }
 
+    @Test
+    void 자동차_이름_5자_초과일경우_오류() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car("CarName"))
+                .withMessageContaining("자동차 이름은 5자 이하로 가능합니다.");
+    }
+
+    @Test
+    void 자동차_이름_공백일경우_오류() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car(""))
+                .withMessageContaining("자동차 이름은 공백일 수 없습니다.");
+    }
+
+    @Test
+    void 자동차_이름_5자이내일경우_오류_미발생() {
+        assertDoesNotThrow(() -> new Car("test"));
+    }
 
 }
