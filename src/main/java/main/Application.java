cbe83278defa5a7ff3560a63e745995e805258f9
HueWoon Kim <hwkim@emoney.co.kr>
2021-07-29 14:05:24 +0900

    refeator: 어플리케이션 메인 분리

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,7 +1,11 @@
-package calculator;
+package main;
+
+import calculator.Calculator;
+import calculator.CalculcatorUI;
 
 public class Application {
 	public static void main(String[] args) {
+		// 계산기 게임
 		Calculator calculator = new Calculator();
 		CalculcatorUI.printIntro();
 		int result = calculator.calculate(CalculcatorUI.getInput());
