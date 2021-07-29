9eb5f6158e9463fa3af829fbdd2f30885f678545
jaehong0721 <newbie302@gmail.com>
2021-07-29 19:24:46 +0900

    refactor: Car 클래스 매직넘버 상수 처리

--- src/main/java/carRacing/Car.java
+++ src/main/java/carRacing/Car.java
@@ -3,10 +3,13 @@ package carRacing;
 import java.util.Random;
 
 public class Car {
+    private static final int MAX_BOUNDS = 10;
+    private static final int MOVE_STANDARD = 4;
+
     private int position;
 
     private boolean isMove(Random random) {
-        return random.nextInt(10) >= 4;
+        return random.nextInt(MAX_BOUNDS) >= MOVE_STANDARD;
     }
 
     int go(Random random) {
