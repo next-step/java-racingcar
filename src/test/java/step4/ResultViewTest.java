cddd82e3c1607974e8a8d0edc4054d223c89bbb5
jaeyoungjang2 <lion85488@gmail.com>
2021-07-29 20:05:09 +0900

    test: test 코드의 구조를 변경

--- src/test/java/step4/ResultViewTest.java
+++ src/test/java/step4/ResultViewTest.java
@@ -5,26 +5,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ResultViewTest {
+    static int maxCarPosition = 0;
+    static String maxCarName = "";
     @DisplayName("최종 우승 메시지 출력이 잘 나타나는 지 확인")
     @Test
     public void findMaxPositionCarName() {
-        int maxCarPosition = 0;
-        String maxCarName = "";
-
         int[] carPositions = {1,2,3,4,3,4};
         String[] carNames = {"a","b","c","d","e","f"};
 
         for (int i = 0; i < carPositions.length; i++) {
             int carPosition = carPositions[i];
             String carName = carNames[i];
-            if (carPosition > maxCarPosition) {
-                maxCarName = carName;
-                maxCarPosition = carPosition;
-            } else if (carPosition == maxCarPosition) {
-                maxCarName += ", "+carName;
-            }
+            findMaxPositionCarName(carPosition, carName);
         }
         Assertions.assertThat(maxCarName).isEqualTo("d, f");
+    }
+
+    private void findMaxPositionCarName(int carPosition, String carName) {
+        if (carPosition > maxCarPosition) {
+            maxCarName = carName;
+            maxCarPosition = carPosition;
+            return;
+        }
+        if (carPosition == maxCarPosition) {
+            maxCarName += ", "+ carName;
+            return;
+        }
     }
 
 
