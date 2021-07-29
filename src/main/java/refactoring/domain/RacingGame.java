6a6faa14ef53295760a3b5aa42a67a48e9af2291
0n1dev <all_mind@naver.com>
2021-07-30 00:36:32 +0900

    feat : RacingGame 클래스 추가
    - RacingGame 클래스 추가

---
+++ src/main/java/refactoring/domain/RacingGame.java
@@ -0,0 +1,34 @@
+package refactoring.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import strategy.AutoMoveable;
+import strategy.Moveable;
+
+public class RacingGame {
+
+	private final Cars cars;
+	private final static Moveable MOVEABLE = new AutoMoveable();
+
+	public RacingGame(String carsNames) {
+		this.cars = Cars.of(carsNames);
+	}
+
+	public List<Car> getRecords() {
+		return cars.ofRecord()
+				   .getRecords();
+	}
+
+	public List<String> getWinnerResults() {
+		return cars.ofRecord()
+				   .getWinnerRecourds()
+				   .stream()
+				   .map(Car::getCarName)
+				   .collect(Collectors.toList());
+	}
+
+	public void move() {
+		cars.move(MOVEABLE);
+	}
+}
