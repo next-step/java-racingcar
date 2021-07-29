63df69d2a4530fb8e3f6051afa70c6f030e793e3
조원진 <>
2021-07-29 17:22:33 +0900

    refactor(InputView) : 비즈니스 검증로직 제거하고 타입만 확인하도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,33 +1,32 @@
 package racingcar.view;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final Pattern pattern = Pattern.compile("^[1-9]*$");
+    private static final String MESSAGE_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
+    private static final String MESSAGE_ROUND_COUNT = "시도할 회수는 몇 회 인가요?";
 
     public static int receiveCarCount() {
-        String input;
-        do {
-            System.out.println("자동차 대수는 몇 대 인가요?");
-            input = scanner.nextLine();
-        } while (!isNumberMoreThanZero(input));
-        return Integer.parseInt(input);
+        return receiveCount(MESSAGE_CAR_COUNT);
     }
 
     public static int receiveRoundCount() {
+        return receiveCount(MESSAGE_ROUND_COUNT);
+    }
+
+    private static int receiveCount(String message) {
         String input;
+        System.out.println(message);
         do {
-            System.out.println("시도할 회수는 몇 회 인가요?");
             input = scanner.nextLine();
-        } while (!isNumberMoreThanZero(input));
+        } while (!isNumber(input));
         return Integer.parseInt(input);
     }
 
-    private static boolean isNumberMoreThanZero(String input) {
-        if (!pattern.matcher(input).find()) {
-            System.out.println("1이상 숫자만 입력 가능합니다.");
+    private static boolean isNumber(String input) {
+        if (input == null || !input.chars().allMatch(Character::isDigit)) {
+            System.out.println("숫자만 입력 가능합니다.");
             return false;
         }
         return true;
