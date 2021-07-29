31f36c56ea4b844f1fa5325f28b84e2044ac3dd6
yook0328 <yook0328@gmail.com>
2021-07-29 23:57:04 +0900

    test: 자동차 다회차 이동 테스트.

--- src/test/java/com/racingcar/car/CarTest.java
+++ src/test/java/com/racingcar/car/CarTest.java
@@ -1,9 +1,14 @@
 package com.racingcar.car;
 
+import com.racingcar.dice.Dice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,6 +28,30 @@ public class CarTest {
         car.move(input);
 
         //then
+        assertThat(car.getDistance()).isEqualTo(expected);
+    }
+
+    @DisplayName("자동차 다회차 이동")
+    @ParameterizedTest
+    @ValueSource(ints = {10, 17, 8})
+    void multipleMove(int count) {
+        //given
+        List<Integer> diceInputs = new ArrayList<>();
+
+        for(int i = 0; i < count; i++) {
+            diceInputs.add(Dice.getNumber());
+        }
+
+        //when
+        for(Integer input : diceInputs) {
+            car.move(input);
+        }
+
+        //then
+        int expected = (int)diceInputs.stream()
+                .filter((value) -> value >= 4)
+                .count();
+
         assertThat(car.getDistance()).isEqualTo(expected);
     }
 }
