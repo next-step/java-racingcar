f7d62bfd7db8087d1aa8d3b265b9eeb733eb6acb
0n1dev <all_mind@naver.com>
2021-07-29 22:43:10 +0900

    feat : CarName 클래스 생성
    - CarName 클래스 생성

---
+++ src/main/java/refactoring/domain/CarName.java
@@ -0,0 +1,40 @@
+package refactoring.domain;
+
+public class CarName {
+
+	private final int MAX_NAME_LENGTH = 5;
+
+	private final String name;
+
+	public CarName(String name) {
+		validateName(name);
+
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	private void validateName(String name) {
+		if (isEmpty(name)) {
+			throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+		}
+
+		if (checkLength(name)) {
+			throw new IllegalArgumentException("자동차 이름은 " + MAX_NAME_LENGTH + "자 이상 초과할 수 없습니다.");
+		}
+	}
+
+	private boolean checkLength(String name) {
+		return name.length() > MAX_NAME_LENGTH;
+	}
+
+	private boolean isEmpty(String name) {
+		if (name == null || "".equals(name)) {
+			return true;
+		}
+
+		return name.isEmpty();
+	}
+}
