f6ffc61d9963579c0a887f38c47352edcd5e52bb
namhyun.sung <namhyun.sung@yanolja.com>
2021-07-31 01:54:09 +0900

    refactor : 안쓰는 import 삭제

--- src/test/java/step3/CarRacingTest.java
+++ src/test/java/step3/CarRacingTest.java
@@ -1,14 +1,9 @@
 package step3;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarRacingTest {
 
