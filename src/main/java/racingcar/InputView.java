19b8a2999a15b689357aa923605a3ef186d86292
hiro <kiryuinh32@gmail.com>
2021-07-30 15:03:47 +0900

    refactor: 상수를 네이밍 컨벤션에 맞춰서 대문자로 수정

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,22 +3,18 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String ankCarCount = "자동차 대수는 몇 대 인가요?";
-    private static final String askTryCount = "시도할 회수는 몇 회 인가요?";
+    private static final String ASK_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
+    private static final String ASK_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
 
-    private final Scanner scanner;
-
-    public InputView() {
-        scanner = new Scanner(System.in);
-    }
+    private final static Scanner scanner = new Scanner(System.in);
 
     public int askCarCount() {
-        System.out.println(ankCarCount);
+        System.out.println(ASK_CAR_COUNT);
         return Integer.parseInt(scanner.nextLine());
     }
 
     public int askTryCount() {
-        System.out.println(askTryCount);
+        System.out.println(ASK_TRY_COUNT);
         return Integer.parseInt(scanner.nextLine());
     }
 }
