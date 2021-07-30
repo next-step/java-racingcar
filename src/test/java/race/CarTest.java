be8a359cb65827cdfc5bde3db3c75d0b5da8944e
woogie <woogie@wemakeprice.com>
2021-07-30 21:39:43 +0900

    [step4] refactor CarTest, CarsTest class

--- src/test/java/race/CarTest.java
+++ src/test/java/race/CarTest.java
@@ -5,43 +5,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CarTest {
 
     private static final int DEFAULT_TRY_COUNT = 5;
+    private static final String DEFAULT_NAMES = "pobi";
 
     @DisplayName("라운드까지의 이동거리값 반환")
     @ParameterizedTest
     @CsvSource(value = {"0:0", "1:0", "2:0", "3:0", "4:0"}, delimiter = ':')
-    void getDistance(int round, int expected) {
-        Car car = Car.of(DEFAULT_TRY_COUNT);
+    void getDistanceByRound(int round, int expected) {
+        Car car = Car.of(DEFAULT_TRY_COUNT, DEFAULT_NAMES);
         car.move();
-        assertThat(car.getDistance(round)).isGreaterThanOrEqualTo(expected);
+        assertThat(car.getDistanceByRound(round)).isGreaterThanOrEqualTo(expected);
     }
 
     @DisplayName("시도할 회수만큼 자동차 이동(전진 또는 멈춤)되었는지 확인")
     @Test
     void move() {
-        Car car = Car.of(DEFAULT_TRY_COUNT);
+        Car car = Car.of(DEFAULT_TRY_COUNT, DEFAULT_NAMES);
         car.move();
-        assertThat(car.getMoveCondition().size()).isEqualTo(DEFAULT_TRY_COUNT);
-    }
-
-    @DisplayName("전진하는 조건 확인 후 moveCondition리스트에 boolean추가된 것 확인 ")
-    @Test
-    void check() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Car car = Car.of(DEFAULT_TRY_COUNT);
-        Method method = car.getClass().getDeclaredMethod("check");
-        method.setAccessible(true);
-
-        for (int i = 0; i < DEFAULT_TRY_COUNT; i++) {
-            method.invoke(car);
-        }
-        assertThat(car.getMoveCondition().size()).isEqualTo(DEFAULT_TRY_COUNT);
+        assertThat(car.getDistance().size()).isEqualTo(DEFAULT_TRY_COUNT);
     }
 
 }
--- src/test/java/race/CarsTest.java
+++ src/test/java/race/CarsTest.java
@@ -1,5 +1,6 @@
 package race;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,25 +11,32 @@ class CarsTest {
 
     private static final int DEFAULT_TRY_COUNT = 5;
     private static final int DEFAULT_CAR_COUNT = 3;
+    private static final String[] DEFAULT_CAR_NAMES = {"pobi", "crong", "honux"};
+    private static final int MINIMUM_DISTANCE = 0;
 
-    @Test
-    void moveAll() {
-        Cars cars = getCars();
-        cars.moveAll();
-
-        assertThat(cars.getCars().size()).isEqualTo(DEFAULT_CAR_COUNT);
-    }
-
+    @DisplayName("생성된 Car객체에 각 시도 횟수(라운드)만큼 이동(전진,후진)이 있는지 확인")
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3,4,5})
-    void getDistanceByRound(int round) {
+    @ValueSource(ints = {0, 1, 2})
+    void moveAll(int index) {
         Cars cars = getCars();
         cars.moveAll();
-        assertThat(cars.getDistanceByRound(round).size()).isEqualTo(DEFAULT_CAR_COUNT);
+
+        Car car = cars.getCars().get(index);
+        int distanceCount = car.getDistance().size() - 1;
+        assertThat(distanceCount).isEqualTo(DEFAULT_TRY_COUNT);
     }
 
+    @DisplayName("자동차 게임 우승자 있는지 확인. 최소 1명이 나와야한다.")
+    @Test
+    void getWinner() {
+        Cars cars = getCars();
+        cars.moveAll();
 
-    Cars getCars() {
-        return Cars.of(Condition.of(DEFAULT_CAR_COUNT, DEFAULT_TRY_COUNT));
+        assertThat(cars.getWinner(MINIMUM_DISTANCE).split(",").length).isGreaterThanOrEqualTo(1);
     }
+
+    private Cars getCars() {
+        return Cars.of(Condition.of(DEFAULT_CAR_COUNT, DEFAULT_TRY_COUNT, DEFAULT_CAR_NAMES));
+    }
+
 }
