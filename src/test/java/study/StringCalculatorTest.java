ed04a27a85a5fb5d614de2e7528c7ef838ef975f
minyul <ggomjae@gmail.com>
2021-07-31 00:09:26 +0900

    숫자와 사칙연산으로 된 문자열을 파싱하여 결과값을 내는 테스트 구현

--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -3,8 +3,11 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class StringCalculatorTest {
 
@@ -15,35 +18,15 @@ public class StringCalculatorTest {
 		stringCalculator = new StringCalculator();
 	}
 
-	@DisplayName("문자열을 파싱하여 더하기를 하는 테스트")
-	@Test
-	void plus() {
-		String str = "3 + 5";
-		Integer result = 8;
-		assertThat(stringCalculator.plus(str)).isEqualTo(result);
-	}
+	@DisplayName("숫자와 사칙연산으로 된 문자열을 파싱하여 결과값을 내는 테스트")
+	@ParameterizedTest
+	@CsvSource(value = {"1 + 3 - 5:-1", "6 / 3 * 2:4"}, delimiter = ':')
+	void calculate(String input, int result) {
+		assertThat(stringCalculator.calculate(input)).isEqualTo(result);
 
-	@DisplayName("문자열을 파싱하여 빼는 테스트")
-	@Test
-	void minus() {
-		String str = "3 - 5";
-		Integer result = -2;
-		assertThat(stringCalculator.minus(str)).isEqualTo(result);
-	}
-
-	@DisplayName("문자열을 파싱하여 곱하기를 하는 테스트")
-	@Test
-	void multiply() {
-		String str = "3 * 5";
-		Integer result = 15;
-		assertThat(stringCalculator.multiply(str)).isEqualTo(result);
-	}
-
-	@DisplayName("문자열을 파싱하여 나누기를 하는 테스트")
-	@Test
-	void divide() {
-		String str = "6 + 3";
-		Integer result = 2;
-		assertThat(stringCalculator.divide(str)).isEqualTo(result);
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			stringCalculator.calculate(null);
+			stringCalculator.calculate("1 & 3 + 2");
+		});
 	}
 }
