811e64702764478b85588201b47ad4de272012ee
khcho902 <khcho902@gmail.com>
2021-07-30 22:01:34 +0900

    feat: 랜덤값(0-9)생성 클래스 구현(RandomValueGenerator)

---
+++ src/main/java/step3/RandomValueGenerator.java
@@ -0,0 +1,16 @@
+package step3;
+
+import java.util.Random;
+
+public class RandomValueGenerator {
+
+  private static final int MAX = 9;
+  private static final Random random = new Random();
+
+  private RandomValueGenerator() {
+  }
+
+  public static int getRandomNumber() {
+    return random.nextInt(MAX);
+  }
+}
