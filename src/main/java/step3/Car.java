163212a0e7b8e2d1b492e2489a1e427341b41124
namhyun.sung <namhyun.sung@yanolja.com>
2021-07-30 20:46:35 +0900

    feat : 자동차 객체 구현

---
+++ src/main/java/step3/Car.java
@@ -0,0 +1,13 @@
+package step3;
+
+public class Car {
+    int moveDistance;
+
+    public int getMoveDistance() {
+        return moveDistance;
+    }
+
+    public void moveForward() {
+        this.moveDistance+=1;
+    }
+}
