6c33adc988fa54648cce89fc015d28b95c564610
hiro <kiryuinh32@gmail.com>
2021-07-30 15:04:59 +0900

    test: move() 테스트 추가

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.util.RandomGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+    Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car(new RandomNumberMovingStrategy(new RandomGenerator()));
+    }
+
+    @Test
+    void 랜덤값이_4이상이면_전진() {
+        car.move(() -> true);
+        int expectedCarPosition = 2;
+        assertThat(car.getPosition()).isEqualTo(expectedCarPosition);
+    }
+
+    @Test
+    void 랜덤값이_3_이하면_멈춤() {
+        car.move(() -> false);
+        int expectedCarPosition = 1;
+        assertThat(car.getPosition()).isEqualTo(expectedCarPosition);
+    }
+}
