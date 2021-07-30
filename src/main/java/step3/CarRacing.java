ec6e0fa99c194a7ebdcf0db35785ac51d750212a
khcho902 <khcho902@gmail.com>
2021-07-30 23:02:14 +0900

    feat: CarRacing 클래스 구현

---
+++ src/main/java/step3/CarRacing.java
@@ -0,0 +1,39 @@
+package step3;
+
+import java.util.ArrayList;
+
+public class CarRacing {
+
+  private final int MOVE_THRESHOLD = 4;
+
+  private final int numOfMove;
+  private final ArrayList<Car> carList = new ArrayList<>();
+
+  public CarRacing(final int numOfCars, final int numOfMove) {
+    this.numOfMove = numOfMove;
+    for (int i = 0; i < numOfCars; i++) {
+      carList.add(new Car());
+    }
+  }
+
+  public void startRacing() {
+    for (int i = 0; i < numOfMove; i++) {
+      allCarsMoveOrStopOnce();
+    }
+  }
+
+  private void allCarsMoveOrStopOnce() {
+    for (Car car : carList) {
+      if (canMove()) {
+        car.move();
+      }
+    }
+  }
+
+  private boolean canMove() {
+    if (RandomValueGenerator.getRandomNumber() >= MOVE_THRESHOLD) {
+      return true;
+    }
+    return false;
+  }
+}
