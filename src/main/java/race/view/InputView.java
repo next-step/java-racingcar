55bb01a3585392d1843dbc1a236a6fa12eb31f3f
woogie <woogie@wemakeprice.com>
2021-07-30 21:38:24 +0900

    [step4] add printed car name, printed winner

--- src/main/java/race/view/InputView.java
+++ src/main/java/race/view/InputView.java
@@ -16,12 +16,14 @@ public class InputView {
 
     private Condition input() {
         Scanner scanner = new Scanner(System.in);
-        out.println("자동차 대수는 몇 대 인가요?");
-        int carCount = scanner.nextInt();
+        out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String input = scanner.next();
+        String[] names = input.split(",");
+        int carCount = names.length;
         out.println("시도할 횟수는 몇 회 인가요?");
         int tryCount = scanner.nextInt();
 
-        return Condition.of(carCount, tryCount);
+        return Condition.of(carCount, tryCount, names);
     }
 
     public InputView() {
--- src/main/java/race/view/ResultView.java
+++ src/main/java/race/view/ResultView.java
@@ -1,31 +1,52 @@
 package race.view;
 
+import race.Car;
 import race.Cars;
-
-import java.util.List;
 
 import static java.lang.System.out;
 
 public class ResultView {
 
+    private static int HIGH_DISTANCE = -1;
+
     public void operate(Cars cars, int tryCount) {
+        int finalRound = tryCount;
         out.println("실행 결과");
-        for(int i=0; i< tryCount; i++){
-            print(cars.getDistanceByRound(i));
+        printCarsByRound(cars, finalRound);
+        printWinner(cars, tryCount);
+    }
+
+    private void printWinner(Cars cars, int finalRound) {
+        String winners = cars.getWinner(HIGH_DISTANCE);
+        out.println(winners + "가 최종 우승했습니다.");
+    }
+
+    private void printCarsByRound(Cars cars, int finalRound) {
+        for (int i = 0; i <= finalRound; i++) {
+            int round = i;
+            cars.getCars().forEach(car -> printCar(car, round));
             out.println();
         }
     }
 
-    private void print(List<Integer> distances) {
-        distances.forEach(this::printDash);
-    }
+    private void printCar(Car car, int round) {
+        String carName = car.getName();
+        int distance = car.getDistanceByRound(round);
 
-    private void printDash(int distance) {
+        if (isHighDistance(distance)) {
+            HIGH_DISTANCE = distance;
+        }
+
+        out.print(carName + " : ");
         out.print("-");
-        for(int i=0; i<distance;i++){
+        for (int i = 0; i < distance; i++) {
             out.print("-");
         }
         out.println();
+    }
+
+    private boolean isHighDistance(int distance) {
+        return HIGH_DISTANCE < distance;
     }
 
     public ResultView() {
