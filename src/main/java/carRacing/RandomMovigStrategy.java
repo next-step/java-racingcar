fc9f70a42ce66efd2043051af0b13f9964b430b1
jaehong0721 <newbie302@gmail.com>
2021-08-01 05:48:59 +0900

    refactor: 매직 넘버 제거

--- src/main/java/carRacing/RandomMovigStrategy.java
+++ src/main/java/carRacing/RandomMovigStrategy.java
@@ -6,11 +6,19 @@ public class RandomMovigStrategy implements CarMovingStrategy{
 
     private static final int MOVE_STANDARD = 4;
     private static final int MAX_BOUNDS = 10;
+    private static final int MIN = 0;
+    private static final int MAX = MAX_BOUNDS -1;
+
     private final Random random = new Random();
 
     @Override
     public boolean isMove() {
-        return getRandomNumber() >= MOVE_STANDARD;
+        int number = getRandomNumber();
+
+        if (number < MIN || number > MAX) {
+            throw new IllegalStateException("랜덤 숫자의 유효범위를 벗어납니다.");
+        }
+        return number >= MOVE_STANDARD;
     }
 
     private int getRandomNumber() {
