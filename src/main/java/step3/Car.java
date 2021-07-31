77d8584e64c732e840d075b6c358fc84ae8c18bf
namhyun.sung <namhyun.sung@yanolja.com>
2021-08-01 00:00:23 +0900

    refactor : moveStatus 제거

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -2,36 +2,25 @@ package step3;
 
 public class Car {
     private int moveDistance;
-    private String moveStatus;
 
     private static final int MOVE_CRITERION = 4;
 
     public Car() {
         this.moveDistance = 0;
-        this.moveStatus = new String();
     }
 
     public int getMoveDistance() {
         return moveDistance;
     }
 
-    public String getMoveStatus() {
-        return moveStatus;
-    }
-
     public void moveForward(int number) {
         if (checkProgressPossible(number)) {
             addMoveDistance();
-            updateMoveStatus();
         }
     }
 
     public void addMoveDistance() {
         this.moveDistance += 1;
-    }
-
-    public void updateMoveStatus() {
-        this.moveStatus += "-";
     }
 
     private boolean checkProgressPossible(int number) {
