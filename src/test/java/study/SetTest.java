d3bffcbfaadd66888a70be61b3d0fbaa4f53c3e7
sunna920619 <32083644+sunna920619@users.noreply.github.com>
2021-07-31 13:21:07 +0900

    StringTest and SetTest completed (#2542)
    Co-authored-by: haena.lee <haena.lee@bankwareglobal.com>

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,50 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void sizeTest() {
+        assertEquals(3, numbers.size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, true",
+            "2, true",
+            "3, true",
+            "4, false",
+            "5, false",
+    })
+    void contains(int number, boolean result) {
+        assertEquals(numbers.contains(number), result);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,57 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StringTest {
+
+    // contains - 중복여부, 순서에 관계 없이 값만 일치하면 테스트가 성공
+    // containsExactly - 순서를 포함해서 정확히 일치
+
+    @Test
+    void split1() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1", "2");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void split2() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    void substring1() {
+        String input = "(1,2)";
+        String result = input.substring(1, input.length() - 1);
+        assertEquals(result, "1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 성공 케이스")
+    void charAt1() {
+        String abc = "abc";
+
+        assertEquals('a', abc.charAt(0));
+        assertEquals('b', abc.charAt(1));
+        assertEquals('c', abc.charAt(2));
+    }
+
+    @Test
+    @DisplayName("charAt indexOutOfBoundsException")
+    void charAt2() {
+        String abc = "abc";
+
+        assertThatThrownBy(() -> {
+            abc.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range: 4");
+    }
+
+}
