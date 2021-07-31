24ef9e1dccdda3bfbcd08b7e01c48da5110cd03d
minyul <ggomjae@gmail.com>
2021-07-31 23:45:52 +0900

    Car를 생성한다.

---
+++ src/main/java/study/Car.java
@@ -0,0 +1,20 @@
+package study;
+
+public class Car {
+
+	private int carId;
+	private String distance;
+
+	public Car(int carId, String distance) {
+		this.carId = carId;
+		this.distance = distance;
+	}
+
+	public int getCarId() {
+		return carId;
+	}
+
+	public String getDistance() {
+		return distance;
+	}
+}
