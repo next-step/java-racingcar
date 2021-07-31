2463fb4db05cce3ce089cc3b5041a2c78271fad9
jaehong0721 <newbie302@gmail.com>
2021-08-01 04:46:06 +0900

    refactor: input 값 체크를 메인 클래스로 이동

--- src/main/java/carRacing/CarRacingMain.java
+++ src/main/java/carRacing/CarRacingMain.java
@@ -1,15 +1,36 @@
 package carRacing;
 
 
-public class CarRacingMain {
-    public static void main(String[] args) {
-        InputView.printNumberOfCars();
-        int numberOfCars = InputView.inputNumber();
+import java.util.InputMismatchException;
 
-        InputView.printNumberOfRacing();
-        int numberOfRacing = InputView.inputNumber();
+public class CarRacingMain {
+
+    public static void main(String[] args) {
+
+        int numberOfCars = requestInputNumber(InputView.InputType.CAR);
+
+        int numberOfRacing = requestInputNumber(InputView.InputType.RACING);
 
         CarRacing racing = new CarRacing();
         racing.start(numberOfCars, numberOfRacing);
+    }
+
+    static int requestInputNumber(InputView.InputType type) {
+
+        int number;
+
+        InputView.printNumberQuestion(type);
+
+        try {
+            number = InputView.inputNumber();
+            if (number <= 0) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+            InputView.printError();
+            number = requestInputNumber(type);
+        }
+
+        return number;
     }
 }
--- src/main/java/carRacing/InputView.java
+++ src/main/java/carRacing/InputView.java
@@ -1,9 +1,26 @@
 package carRacing;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
+
+    enum InputType {
+        CAR, RACING
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static void printNumberQuestion(InputType type) {
+        switch (type) {
+            case CAR:
+                printNumberOfCars();
+                break;
+            case RACING:
+                printNumberOfRacing();
+                break;
+        }
+    }
+
     static void printNumberOfCars() {
         System.out.println("자동차 대수는 몇 대 인가요?");
     }
@@ -12,23 +29,11 @@ public class InputView {
         System.out.println("시도할 회수는 몇 회 인가요?");
     }
 
+    static void printError() {
+        System.out.println("0 이상의 정수값을 입력해주세요.");
+    }
+
     static int inputNumber() {
-        Scanner scanner = new Scanner(System.in);
-        int number = -1;
-
-        do {
-            try {
-                number = scanner.nextInt();
-                if (number <= 0) {
-                    throw new InputMismatchException();
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("0 이상의 정수값을 입력해주세요.");
-                scanner.next();
-            }
-
-        } while (number <= 0);
-
-        return number;
+        return scanner.nextInt();
     }
 }
