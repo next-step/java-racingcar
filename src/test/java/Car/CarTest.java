6c04729f1bd510c8b425ba5bd2da94271ac20647
jspark940321 <jspark940321@gmail.com>
2021-08-01 19:03:21 +0900

    test: Car클래스 CarName 추가에 의한 테스트 추가

---
+++ src/test/java/Car/CarTest.java
@@ -0,0 +1,46 @@
+package Car;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CarTest {
+    Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("testCar");
+    }
+
+    @Test
+    void 자동차초기상태테스트() {
+        int carMoveCount = car.getMoveCount();
+        assertThat(carMoveCount).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 0",
+            "3, 0",
+            "4, 1",
+            "5, 1",
+            "8, 1"
+    })
+    void 자동차움직임테스트(int inputNumber, int moveAnswer) {
+        car.move(inputNumber);
+        int carMoveCount = car.getMoveCount();
+        assertThat(carMoveCount).isEqualTo(moveAnswer);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi", "crong", "honux"})
+    void 자동차이름테스트(String carName) {
+        Car car = new Car(carName);
+        assertThat(car.getCarName()).isEqualTo(carName);
+    }
+
+}
