839405802844ab084c395c0d5618167036a8fb5c
yook0328 <yook0328@gmail.com>
2021-08-01 21:30:11 +0900

    refactor: GameMaker에서 Car 객체 생성 시 id 값 넣도록 수정.

--- src/main/java/com/racingcar/game/GameMaker.java
+++ src/main/java/com/racingcar/game/GameMaker.java
@@ -2,16 +2,19 @@ package com.racingcar.game;
 
 import com.racingcar.car.Car;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameMaker {
     public static Game make(int gameRound, int numberOfCar) {
         return new Game(gameRound, makeCars(numberOfCar));
     }
 
-    private static Car[] makeCars(int numberOfCar) {
-        Car[] cars = new Car[numberOfCar];
+    private static List<Car> makeCars(int numberOfCar) {
+        List<Car> cars = new ArrayList<>();
 
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car();
+        for (int i = 0; i < numberOfCar; i++) {
+            cars.add(new Car(i));
         }
 
         return cars;
