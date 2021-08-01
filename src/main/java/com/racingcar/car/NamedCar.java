c6ebe966b01abe2643c09df548b5e41b99d552b8
yook0328 <yook0328@gmail.com>
2021-08-01 21:32:54 +0900

    refactor: NamedCar가 부모 객체의 Car에서 id가 추가된 부분 사용하도록 수정.

--- src/main/java/com/racingcar/car/NamedCar.java
+++ src/main/java/com/racingcar/car/NamedCar.java
@@ -4,12 +4,13 @@ public class NamedCar extends Car {
     private static final int NAME_MAX_LENGTH = 4;
     private String name;
 
-    public NamedCar(String name) {
+    public NamedCar(int id, String name) {
         if (name.length() > NAME_MAX_LENGTH) {
             throw new IllegalArgumentException("name must under 4 characters, name : " + name);
         }
 
         this.name = name;
+        this.id = id;
     }
 
     public String getName() {
