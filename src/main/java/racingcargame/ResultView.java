7c7944c22ae5ff63e709eb3d6f368d251d6f47ec
anbin <idealbin@naver.com>
2021-08-01 17:50:03 +0900

    <refactor> : 멤버변수 제거 및 출력 로직 수정

--- src/main/java/racingcargame/ResultView.java
+++ src/main/java/racingcargame/ResultView.java
@@ -1,21 +1,30 @@
 package racingcargame;
 
+import java.util.List;
+
 public class ResultView {
-    private String movingLine = "";
+    private static final String MOVING_LINE = "-";
 
-    public String move() {
-        movingLine += "-";
-        System.out.println(movingLine);
-        return movingLine;
+    public void showResultView(List<RacingCar> cars) {
+       for (RacingCar car : cars) {
+           System.out.println(convertDistanceToLine(car.getDistance()));
+       }
+       printNewLine();
     }
 
-    public String stop() {
-        movingLine += "";
-        System.out.println(movingLine);
-        return movingLine;
+    public void printResultMessage() {
+        System.out.println(System.lineSeparator() + "실행 결과");
     }
 
-   public void showResultView() {
-       System.out.println("\n실행 결과");
+    private String convertDistanceToLine(int distance) {
+       String line = "";
+       for (int i = 0; i < distance; i++) {
+           line += MOVING_LINE;
+       }
+       return line;
+    }
+
+    private void printNewLine() {
+       System.out.print(System.lineSeparator());
     }
 }
