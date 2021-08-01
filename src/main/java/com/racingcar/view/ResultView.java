27b74d360c75958408c723e5e6be268c82a2f7d1
yook0328 <yook0328@gmail.com>
2021-08-01 21:32:10 +0900

    refactor: ResultView에서 결과를 그릴 시 기존 int[][] 사용하는 것이 아닌 GameResult 객체를 사용하도록 수정.

--- src/main/java/com/racingcar/view/ResultView.java
+++ src/main/java/com/racingcar/view/ResultView.java
@@ -1,18 +1,23 @@
 package com.racingcar.view;
 
+import com.racingcar.game.GameResult;
+import com.racingcar.game.RoundResult;
+
+import java.util.List;
+
 public class ResultView {
-    public static void draw(int[][] result) {
+    public static void draw(GameResult result) {
         System.out.println("실행 결과");
 
-        for (int i = 0; i < result.length; i++) {
-            drawOneRound(result[i]);
+        for (int i = 0; i < result.getRoundSize(); i++) {
+            drawOneRound(result.getRoundResult(i), result.getCarIdList());
             System.out.println();
         }
     }
 
-    private static void drawOneRound(int[] distances) {
-        for (int i = 0; i < distances.length; i++) {
-            drawCar(distances[i]);
+    private static void drawOneRound(RoundResult result, List<Integer> carIds) {
+        for(int id : carIds) {
+            drawCar(result.get(id));
         }
     }
 
