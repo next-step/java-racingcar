00635f1c7426c2838ab9aa3928aaee66bdce7cfb
jspark940321 <jspark940321@gmail.com>
2021-08-01 19:12:15 +0900

    feat: Car클래스 carName 상태 추가

--- src/main/java/Car/Car.java
+++ src/main/java/Car/Car.java
@@ -4,9 +4,15 @@ public class Car {
     private static final int MIN_MOVE_NUMBER = 4;
 
     private int moveCount;
+    private String carName;
 
-    public Car() {
+    public String getCarName() {
+        return carName;
+    }
+
+    public Car(String carName) {
         moveCount = 0;
+        this.carName = carName;
     }
 
     public int getMoveCount() {
