9500b8e3650e80241d7702c896c0b57e38758453
haena.lee <haena.lee@bankwareglobal.com>
2021-08-02 01:16:31 +0900

    2021-08-02 CalculatorTest 테스트케이스 추가

--- src/test/java/study/step2/CalculatorTest.java
+++ src/test/java/study/step2/CalculatorTest.java
@@ -1,2 +1,93 @@
-package study.step2;public class CalculatorTest {
+package study.step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import step2.Calculator;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @DisplayName("calculate 메소드 테스트 : +")
+    @ParameterizedTest
+    @CsvSource({
+            "1 + 2, 3",
+            "3 + 4 + 5, 12",
+            "12 + 10 + 1200 + 500, 1722"
+    })
+    void addTest(String in, int result) {
+        assertEquals(Calculator.calculate(in), result);
+    }
+
+    @DisplayName("calculate 메소드 테스트 : -")
+    @ParameterizedTest
+    @CsvSource({
+            "1 - 2, -1",
+            "100 - 4 - 6, 90",
+            "500 - 100 - 100 - 100, 200"
+    })
+    void minusTest(String in, int result) {
+        assertEquals(Calculator.calculate(in), result);
+    }
+
+    @DisplayName("calculate 메소드 테스트 : *")
+    @ParameterizedTest
+    @CsvSource({
+            "1 * 2, 2",
+            "-2 * 100 * 200, -40000",
+            "1 * 100 * 0 * 700, 0"
+    })
+    void multiplyTest(String in, int result) {
+        assertEquals(Calculator.calculate(in), result);
+    }
+
+    @DisplayName("calculate 메소드 테스트 : /")
+    @ParameterizedTest
+    @CsvSource({
+            "400 / 2, 200",
+            "300 / 3 / 3, 33",
+            "500 / 100 / 5 / 100, 0"
+    })
+    void divideTest(String in, int result) {
+        assertEquals(Calculator.calculate(in), result);
+    }
+
+    @DisplayName("calculate 메소드 테스트 : 랜덤")
+    @ParameterizedTest
+    @CsvSource({
+            "2 + 3 * 4 / 2, 10",
+            "3 * 9 + 100 / 5 - 10, 15",
+            "5 - 20 / 3 + 120 * 10, 1150"
+    })
+    void randomTest(String in, int result) {
+        assertEquals(Calculator.calculate(in), result);
+    }
+
+    @Test
+    @DisplayName("input check 테스트 : null")
+    void inputCheckTest1() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Calculator.calculate(null))
+                .withMessageMatching("input 값이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("input check 테스트 : empty")
+    void inputCheckTest2() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Calculator.calculate(""))
+                .withMessageMatching("input 값이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("operator check 테스트")
+    void operatorCheckTest2() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Calculator.calculate("1 x 2"))
+                .withMessageMatching("+, -, *, / 외에 다른 문자가 입력 되었습니다.");
+    }
+
 }
