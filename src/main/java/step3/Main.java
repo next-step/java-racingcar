ab892a6147732fdaf72ae44f9ce0b2796c5dcd18
longbeom <wowow1859@naver.com>
2021-08-02 02:25:59 +0900

    refactor : 실행 클래스 수정

--- src/main/java/step3/Main.java
+++ src/main/java/step3/Main.java
@@ -1,8 +1,15 @@
 package step3;
 
+import step3.domain.Racing;
+import step3.view.InputView;
+import step3.view.OutputView;
+
 public class Main {
 	public static void main(String[] args) {
-		int[] answers = new InputView().answers();
-		new Racing().play(answers);
+		int cars = InputView.numberOfCars();
+		int trials = InputView.numberOfTrials();
+
+		int[][] result = new Racing(trials, cars).race();
+		OutputView.viewResult(result);
 	}
 }
