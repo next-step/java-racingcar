06b7b4a6d8b12261a2c68c577f6dd1b6ba26e7e8
yook0328 <yook0328@gmail.com>
2021-08-01 23:57:13 +0900

    refactor: ResultView 객체 추상화

--- src/main/java/com/racingcar/view/ResultView.java
+++ src/main/java/com/racingcar/view/ResultView.java
@@ -3,31 +3,24 @@ package com.racingcar.view;
 import com.racingcar.game.GameResult;
 import com.racingcar.game.RoundResult;
 
-import java.util.List;
+public abstract class ResultView {
+    protected GameResult gameResult;
 
-public class ResultView {
-    public static void draw(GameResult result) {
-        System.out.println("실행 결과");
-
-        for (int i = 0; i < result.getRoundSize(); i++) {
-            drawOneRound(result.getRoundResult(i), result.getCarIdList());
-            System.out.println();
-        }
+    public ResultView(GameResult gameResult) {
+        this.gameResult = gameResult;
     }
 
-    private static void drawOneRound(RoundResult result, List<Integer> carIds) {
-        for(int id : carIds) {
-            drawCar(result.get(id));
-        }
-    }
+    abstract void draw();
 
-    private static void drawCar(int distance) {
+    abstract void drawOneRound(RoundResult result);
+
+    protected final String getCarLine(int distance) {
         StringBuilder builder = new StringBuilder();
 
         for (int i = 0; i < distance; i++) {
             builder.append("-");
         }
 
-        System.out.println(builder.toString());
+       return builder.toString();
     }
 }
