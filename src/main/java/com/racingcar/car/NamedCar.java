17259be96c117ac0a6eca8962113b9deb4197de2
yook0328 <yook0328@gmail.com>
2021-08-02 00:04:01 +0900

    refactor: 자동차 이름명 제약 로직 빈값인 것도 제거하도록 수정.

--- src/main/java/com/racingcar/car/NamedCar.java
+++ src/main/java/com/racingcar/car/NamedCar.java
@@ -1,12 +1,13 @@
 package com.racingcar.car;
 
 public class NamedCar extends Car {
+    private static final String EMPTY = "";
     private static final int NAME_MAX_LENGTH = 4;
     private String name;
 
     public NamedCar(int id, String name) {
-        if (name.length() > NAME_MAX_LENGTH) {
-            throw new IllegalArgumentException("name must under 4 characters, name : " + name);
+        if (name.length() > NAME_MAX_LENGTH || EMPTY.equals(name.trim())) {
+            throw new IllegalArgumentException("name must 1-4 characters, name : " + name);
         }
 
         this.name = name;
