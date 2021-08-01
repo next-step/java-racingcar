d71135bc1182fa8f40b1bb7415fd46671c56292d
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-01 21:12:37 +0900

    feat: 자동차 위치 저장 기능 구현

---
+++ src/main/java/carracing/Car.java
@@ -0,0 +1,13 @@
+package carracing;
+
+public class Car {
+    private String location;
+
+    public Car() {
+        this.location = "";
+    }
+
+    public String location() {
+        return location;
+    }
+}
---
+++ src/test/java/carracing/CarTest.java
@@ -0,0 +1,22 @@
+package carracing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    @DisplayName("자동차의 위치가 자동차에 저장되어있어야 한다.")
+    void carLocationTest() {
+
+        // given
+        Car car = new Car();
+
+        // whe & then
+        assertThat(car.location()).isEqualTo("");
+    }
+
+}
