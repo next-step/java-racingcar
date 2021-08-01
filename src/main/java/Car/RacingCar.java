ae7750bb7db744ff6fd2be9baaed99958c15fbca
jspark940321 <jspark940321@gmail.com>
2021-08-01 19:13:36 +0900

    feat: Car클래스 carName 상태 추가

--- src/main/java/Car/RacingCar.java
+++ src/main/java/Car/RacingCar.java
@@ -12,12 +12,12 @@ public class RacingCar {
         return cars;
     }
 
-    public RacingCar(int carCount) {
-        validate(carCount);
+    public RacingCar(int carCount, String[] carNames) {
+        validate(carCount, carNames);
 
         cars = new ArrayList<>();
         for (int i = 0; i < carCount; i++) {
-            cars.add(new Car());
+            cars.add(new Car(carNames[i]));
         }
     }
 
@@ -26,9 +26,18 @@ public class RacingCar {
                 .forEach(car -> car.move(random.nextInt(RANDOM_NUMBER_BOUND)));
     }
 
-    private void validate(int carCount) {
+    private void validate(int carCount, String[] carNames) {
         if (carCount <= 0) {
             throw new IllegalArgumentException("0보다 큰 값을 입력해주세요.");
+        }
+        for (String carName : carNames) {
+            validateCarNameLength(carName);
+        }
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
     }
 }
