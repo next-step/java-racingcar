f0a2566ad77c7a7494996055bb8472153aa13319
조원진 <>
2021-08-01 23:29:51 +0900

    revert(Cars) : Cars 생성자 List 매개변수 버전으로 revert

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,29 +1,22 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Cars {
-    private static final String DELIMITER = ",";
-    private final List<Car> cars;
+    private List<Car> cars;
 
-    public Cars(String carNames) {
-        checkNullOrEmpty(carNames);
-        this.cars = initCars(carNames.split(DELIMITER));
+    public Cars(List<Car> cars) {
+        checkNullOrEmpty(cars);
+        this.cars = cars;
     }
 
-    private void checkNullOrEmpty(String carNames) {
-        if (Objects.isNull(carNames) || carNames.trim().isEmpty()) {
-            throw new IllegalArgumentException("carNames는 null이거나 empty일 수 없습니다.");
+    private void checkNullOrEmpty(List<Car> cars) {
+        if(Objects.isNull(cars) || cars.isEmpty()) {
+            throw new IllegalArgumentException("차는 최소 1대 이상이어야 합니다.");
         }
-    }
-
-    private List<Car> initCars(String[] carNames) {
-        return Stream.of(carNames)
-                .map(Car::new)
-                .collect(Collectors.toList());
     }
 
     public void moveCars(MovableStrategy movableStrategy) {
