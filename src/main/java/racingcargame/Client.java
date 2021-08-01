7a04ed811c85b6c6ceee9ea2693242cf70d941b1
anbin <idealbin@naver.com>
2021-08-01 17:49:28 +0900

    <refactor> : 클라이언트 코드 가독성 수정

--- src/main/java/racingcargame/Client.java
+++ src/main/java/racingcargame/Client.java
@@ -3,11 +3,11 @@ package racingcargame;
 public class Client {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        ResultView resultView = new ResultView();
         RandomOption randomOption = new RandomNumberOption();
+
         RacingCarGame racingCarGame = new RacingCarGame(inputView.inputNumberOfCars(), inputView.inputCount());
-        inputView.scanner.close();
-        resultView.showResultView();
         racingCarGame.startGame(randomOption);
+
+        inputView.close();
     }
 }
