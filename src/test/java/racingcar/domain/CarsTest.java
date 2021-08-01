dcc55725fd7b1a7e2f1f3450daf9d1a3e8e28261
조원진 <>
2021-08-01 23:30:23 +0900

    revert(CarsTest) : Cars 생성자 List 매개변수 버전으로 revert

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -11,32 +11,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
 
-    @DisplayName("입력된 차 이름목록이 Null, empty, 공백, 길이 5자 초과 차 이름을 포함할 시 IllegalArgumentExcpetion 발생")
+    @DisplayName("입력된 차 List가 null이거나 empty일 경우 IllegalArgumentExcpetion 발생")
     @ParameterizedTest(name = "{index} {displayName} {arguments}")
     @NullAndEmptySource
-    @ValueSource(strings = {"  ", "benz,porsche"})
-    void create_ThrowsIllegalArgumentException_IfCarNamesIsNullOrEmptyOrBlankOrInvolveCarNameLengthMoreThanFive(String carNames) {
+    void create_ThrowsIllegalArgumentException_IfCarListIsNullOrEmpty(List<Car> cars) {
         Assertions.assertThatIllegalArgumentException().isThrownBy(
-                () -> new Cars(carNames));
-    }
-
-    @DisplayName("생성")
-    @Test
-    void create() {
-        String carNames = "benz,bmw,audi";
-        Cars cars = new Cars(carNames);
-        assertThat(cars.getCars().size()).isEqualTo(carNames.split(",").length);
-    }
-
-    @DisplayName("이동")
-    @Test
-    void moveCars_PositionIsOne() {
-        String carNames = "benz,bmw,audi";
-        Cars cars = new Cars(carNames);
-        cars.moveCars(() -> true);
-
-        for (Car car : cars.getCars()) {
-            assertThat(car.getPosition()).isEqualTo(new Position(1));
-        }
+                () -> new Cars(cars));
     }
 }
