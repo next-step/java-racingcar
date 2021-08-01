e11eb1bed1f9b9001c33e0b1467d7f8e84c62449
ChangSubKwak <paradise1732@gmail.com>
2021-08-01 20:04:27 +0900

    fix: 예외 클래스를 Unchecked 예외로 변경에 따른 정리, 출력 메서드 제거(도메인 미포함 필수), 이름 예외 메세지 문자열에 길이를 상수값으로 대체

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -11,25 +11,17 @@ public class RacingCar {
 	private String carName;
 	private MovableStrategy movableStrategy;
 
-	public RacingCar(String carName, MovableStrategy movableStrategy) throws NameValidationException {
+	public RacingCar(String carName, MovableStrategy movableStrategy) {
 		checkNameValidation(carName);
 		this.position = INITIAL_POSITION;
 		this.carName = carName;
 		this.movableStrategy = movableStrategy;
 	}
 
-	private void checkNameValidation(String carName) throws NameValidationException {
+	private void checkNameValidation(String carName) {
 		if (carName.length() > NAME_MAX_LENGTH) {
-			throw new NameValidationException("자동차 이름의 길이가 5를 초과합니다");
+			throw new NameValidationException("자동차 이름의 길이가 " + NAME_MAX_LENGTH + "를 초과합니다");
 		}
-	}
-
-	public String getRacingCarDashString() {
-		StringBuilder stringBuilder = new StringBuilder();
-		for (int i = 0; i < this.position; i++) {
-			stringBuilder.append('-');
-		}
-		return stringBuilder.toString();
 	}
 
 	public void forward() {
