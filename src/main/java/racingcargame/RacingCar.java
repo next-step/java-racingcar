5838fbb7ba4337431a366c45838ec65a9d5e3aba
anbin <idealbin@naver.com>
2021-08-01 16:36:23 +0900

    <refactor> : resultView와 프로덕션코드를 분리

--- src/main/java/racingcargame/RacingCar.java
+++ src/main/java/racingcargame/RacingCar.java
@@ -2,20 +2,25 @@ package racingcargame;
 
 public class RacingCar {
     private static final int MOVING_VALUE = 4;
-    private ResultView resultView = new ResultView();
 
-    public String tryToMoveCar(int randomValue) {
-        if(randomValue >= MOVING_VALUE) {
+    private int distance = 0;
+
+    public int tryToMove(int randomValue) {
+        if (randomValue >= MOVING_VALUE) {
             return this.move();
         }
         return this.stop();
     }
 
-    public String move() {
-        return resultView.move();
+    private int move() {
+        return distance += 1;
     }
 
-    public String stop() {
-        return resultView.stop();
+    private int stop() {
+        return distance += 0;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
--- src/test/java/racingcargame/RacingCarTest.java
+++ src/test/java/racingcargame/RacingCarTest.java
@@ -10,24 +10,28 @@ public class RacingCarTest {
     @DisplayName("값이 4미만일 경우 멈춘다.")
     @ParameterizedTest
     @CsvSource(value = {
-            "1$''"
-            , "3$''"}
+            "1$0"
+            , "3$0"}
             , delimiter = '$')
-    void stop(int input, String expected) {
+    void stop(int input, int expected) {
         RacingCar racingCar = new RacingCar();
-        String actual = racingCar.tryToMoveCar(input);
+
+        int actual = racingCar.tryToMove(input);
+
         assertEquals(expected, actual);
     }
 
     @DisplayName("값이 4이상일 경우 전진한다.")
     @ParameterizedTest
     @CsvSource(value = {
-            "4$-"
-            , "9$-"}
+            "4$1"
+            , "9$1"}
             , delimiter = '$')
-    void move(int input, String expected) {
+    void move(int input, int expected) {
         RacingCar racingCar = new RacingCar();
-        String actual = racingCar.tryToMoveCar(input);
+
+        int actual = racingCar.tryToMove(input);
+
         assertEquals(expected, actual);
     }
 }
