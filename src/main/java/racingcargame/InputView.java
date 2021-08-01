dad1dc0c687128eccec1da1bd4a965ba8395a228
anbin <idealbin@naver.com>
2021-08-01 16:39:43 +0900

    <refactor> : 접근제한자 변경 및 close() 메소드 추가

--- src/main/java/racingcargame/InputView.java
+++ src/main/java/racingcargame/InputView.java
@@ -3,7 +3,7 @@ package racingcargame;
 import java.util.Scanner;
 
 public class InputView {
-    Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     public int inputNumberOfCars() {
         System.out.println("자동차 대수는 몇 대 인가요?");
@@ -13,5 +13,9 @@ public class InputView {
     public int inputCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         return scanner.nextInt();
+    }
+
+    public void close() {
+        scanner.close();
     }
 }
