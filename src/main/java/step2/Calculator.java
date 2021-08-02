be6a5872ca51c06bc6d6166dcc36bec89c9a2a61
haena.lee <haena.lee@bankwareglobal.com>
2021-08-03 00:16:43 +0900

    2021-08-03 Calculator 클래스 Enum 사용으로 변경

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -1,5 +1,8 @@
 package step2;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
 public class Calculator {
 
     public int calculate(String formula) {
@@ -19,43 +22,35 @@ public class Calculator {
 
         int current = Integer.parseInt(terms[0]); // 왼쪽 항
 
-        for(int i = 1; i < terms.length; i ++) {
-            switch (terms[i]) {
-                case "+" :
-                    current = add(current, Integer.parseInt(terms[i + 1]));
-                    break;
-                case "-" :
-                    current = subtract(current, Integer.parseInt(terms[i + 1]));
-                    break;
-                case "*" :
-                    current = multiply(current, Integer.parseInt(terms[i + 1]));
-                    break;
-                case "/" :
-                    current = divide(current, Integer.parseInt(terms[i + 1]));
-                    break;
-                default :
-                    throw new IllegalArgumentException("+, -, *, / 외에 다른 문자가 입력 되었습니다.");
-            }
-
-            i ++;
+        for(int i = 1; i < terms.length; i +=2) {
+            current = Operator.calculate(terms[i], current, Integer.parseInt(terms[i + 1]));
         }
 
         return current;
     }
 
-    private int add(int a, int b) {
-        return a + b;
-    }
+    enum Operator {
+        PLUS("+", (a, b) -> a + b),
+        SUBTRACT("-", (a, b) -> a - b),
+        MULTIPLY("*", (a, b) -> a * b),
+        DIVIDE("/", (a, b) -> a / b);
 
-    private int subtract(int a, int b) {
-        return a - b;
-    }
+        private String operator;
+        private BiFunction<Integer, Integer, Integer> calculation; // BiFunction - 매개변수가 두 개이며 return 값이 있는 함수형 인터페이스
 
-    private int multiply(int a, int b) {
-        return a * b;
-    }
+        Operator(String operator, BiFunction<Integer, Integer, Integer> calculation) {
+            this.operator = operator;
+            this.calculation = calculation;
+        }
 
-    private int divide(int a, int b) {
-        return a / b;
+        public static int calculate(String operator, int a, int b) {
+            return getOperator(operator).calculation.apply(a, b);
+        }
+
+        private static Operator getOperator(String operator) {
+            return Arrays.stream(values())
+                    .filter(o -> o.operator.equals(operator))
+                    .findFirst().orElseThrow(() -> new IllegalArgumentException("올바른 연산자가 아닙니다."));
+        }
     }
 }
