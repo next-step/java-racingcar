4d6227871412f44a2cb8f5cb8d2434ee4dcf3424
minyul <ggomjae@gmail.com>
2021-08-02 09:34:03 +0900

    콘솔을 위한 distance를 크기를 나타내는 자료형으로 변경한다

--- src/main/java/study/ResultView.java
+++ src/main/java/study/ResultView.java
@@ -10,8 +10,15 @@ public class ResultView {
 
 	public void viewResult(List<Car> carList) {
 		for(Car car : carList) {
-			System.out.println(car.getDistance());
+			carResult(car);
+			System.out.println();
 		}
 		System.out.println();
+	}
+
+	private String carResult(Car car) {
+		for(int i = 1 ; i <= car.getDistance(); i++) {
+			System.out.print("-");
+		}
 	}
 }
