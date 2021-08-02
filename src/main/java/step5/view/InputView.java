a6c02091f207327b50878bcb7c8d3dfa543db1c6
jaeyoungjang2 <lion85488@gmail.com>
2021-08-02 21:27:47 +0900

    feat: view 생성 사용자로 부터 자동차의 수와 round 수 받음

--- src/main/java/step5/view/InputView.java
+++ src/main/java/step5/view/InputView.java
@@ -1,2 +1,23 @@
-package step5.view;public class InputView {
+package step5.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String HOW_MANY_CAR = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String HOW_MANY_CYCLE = "시도할 횟수는 몇 회 인가요?";
+    private static final int MIN_ROUND = 0;
+    private static final int MIN_CAR = 0;
+    private static final String CYCLE_ERROR_MESSAGE = "시도할 횟수는 0보다 커야 합니다.";
+    private static final String CAR_ERROR_MESSAGE = "자동차 대수는 1대 이상이어야 합니다.";
+    private static final Scanner sc = new Scanner(System.in);
+
+    public String requestCars() {
+        System.out.println(HOW_MANY_CAR);
+        return sc.next();
+    }
+
+    public int requestRound() {
+        System.out.println(HOW_MANY_CYCLE);
+        return sc.nextInt();
+    }
 }
