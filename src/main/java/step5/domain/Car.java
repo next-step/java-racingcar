8140c52fe4179dc7c70af3c046a2a728b7a2738f
jaeyoungjang2 <lion85488@gmail.com>
2021-08-02 21:30:41 +0900

    feat: 자동차 클래스 추가 실질적으로 움직이는 자동차 클래스

--- src/main/java/step5/domain/Car.java
+++ src/main/java/step5/domain/Car.java
@@ -1,2 +1,17 @@
-package step5.domain;public class Car {
+package step5.domain;
+
+public class Car {
+    private static int position;
+
+    public Car(int position) {
+        this.position = position;
+    }
+
+    public void moveForward() {
+        position++;
+    }
+
+    public int getCurrentPosition() {
+        return position;
+    }
 }
