1c9778d5b5474d01005a5f6c8e32aaed3f112228
jaeyoungjang2 <lion85488@gmail.com>
2021-08-02 21:30:58 +0900

    test: 자동차 움직이는 코드 테스트

--- src/test/java/step5/domain/CarRacerTest.java
+++ src/test/java/step5/domain/CarRacerTest.java
@@ -1,4 +1,15 @@
+package step5.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CarRacerTest {
-  
+    @Test
+    void 자동차_앞으로_1칸_전진() {
+        Car car = new Car(1);
+        car.moveForward();
+        Assertions.assertThat(car.getCurrentPosition()).isEqualTo(2);
+    }
 }
