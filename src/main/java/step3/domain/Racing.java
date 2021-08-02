4d29e7db0e1ecf29ae8cdb650658be0cb35d3759
longbeom <wowow1859@naver.com>
2021-08-02 23:51:54 +0900

    refactor : Cars 일급컬렉션 생성

--- src/main/java/step3/domain/Racing.java
+++ src/main/java/step3/domain/Racing.java
@@ -2,49 +2,26 @@ package step3.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import step3.utils.RandomUtils;
 
 public class Racing {
 	private final int trials;
-	private final List<Car> cars = new ArrayList<>();
+	private final Cars cars;
 
 	public Racing(int trials, int cars) {
 		this.trials = trials;
-		this.cars.addAll(prepareCars(cars));
+		this.cars = new Cars(cars);
 	}
 
 	public List<RacingResult> race() {
 		List<RacingResult> result = new ArrayList<>();
 
 		for (int i = 0; i < trials; i++) {
-			raceOneTrial();
-			RacingResult racingResult = new RacingResult(trials, getResultOneTrial());
+			cars.raceOneTrial();
+			List<Integer> resultOneTrial = cars.getResultOneTrial();
+			RacingResult racingResult = new RacingResult(trials, resultOneTrial);
 			result.add(racingResult);
 		}
 
 		return result;
-	}
-
-	private List<Car> prepareCars(int numberOfCars) {
-		List<Car> cars = new ArrayList<>();
-		for (int i = 0; i < numberOfCars; i++) {
-			cars.add(new Car());
-		}
-		return cars;
-	}
-
-	private void raceOneTrial() {
-		cars.forEach(car -> {
-			int randomNumber = RandomUtils.randomNumber();
-			car.advanceOrStop(randomNumber);
-		});
-	}
-
-	private List<Integer> getResultOneTrial() {
-		return cars.stream()
-			.map(Car::getPosition)
-			.collect(Collectors.toList());
 	}
 }
