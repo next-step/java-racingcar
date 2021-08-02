9033035e82b73665a8251288b8da1b1ee911838b
devchang202 <84195676+devchang202@users.noreply.github.com>
2021-08-02 22:57:52 +0900

    Step2 - 문자열 계산기 (#2558)
    * calculator test - operator implementation
    
    * calculator implementation complete for 1st PR
    
    * calculator - add main function

---
+++ src/main/java/com/devchang202/calculator/Calculator.java
@@ -0,0 +1,112 @@
+package com.devchang202.calculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int substrate (int a, int b){
+        return a - b;
+    }
+
+    int divide (int a, int b) {
+        return a / b;
+    }
+
+    int multiply(int a, int b){
+        return a * b;
+    }
+
+    String[] parseEquation(String UserInput) {
+        String[] equation = UserInput.trim().split(" ");
+
+        for (int i = 0; i < equation.length - 1; i = i + 2) {
+            checkNumber(equation[i]);
+            checkOperator(equation[i + 1]);
+        }
+        checkNumber(equation[equation.length - 1]);
+
+        return equation;
+    }
+
+    int getResultOfCalculator(String[] parsedEquation) {
+        int result = Integer.parseInt(parsedEquation[0]);
+        for (int i = 1; i < parsedEquation.length; i = i + 2) {
+            result = calculate(parsedEquation[i], result, Integer.parseInt(parsedEquation[i + 1]));
+        }
+
+        return result;
+    }
+
+    int calculate(String op, int a, int b) {
+        switch (op) {
+            case "+":
+                return add(a, b);
+            case "-":
+                return substrate(a, b);
+            case "*":
+                return multiply(a, b);
+            case "/":
+                return divide(a, b);
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    void checkOperator(String op) {
+        if (isBlank(op) || !isOperator(op)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    void checkNumber(String num) {
+        if (!isNumebr(num)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    boolean isBlank(String input) {
+        return (input.equals("") || input.equals(" "));
+    }
+
+    boolean isOperator(String input) {
+        return (input.equals("+") || input.equals("*") || input.equals("/") || input.equals("-"));
+    }
+
+    boolean isNumebr(String input) {
+        if (isBlank(input) || isOperator(input)) {
+            return false;
+        }
+
+        try {
+            int num = Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    String inputEquation() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        while (true) {
+            try {
+                System.out.println("input equation : ");
+                System.out.println("Result : " + calculator.getResultOfCalculator(calculator.parseEquation(calculator.inputEquation())));
+            } catch (IOException e) {
+                System.out.println("try again. : " + e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("invalid equation, try again. : " + e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/test/java/com/devchang202/calculator/CalculatorTest.java
@@ -0,0 +1,189 @@
+package com.devchang202.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.ArgumentUtils;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+    public static Calculator calculator = new Calculator();
+
+    @Test
+    void add() {
+        int a = (int)(Math.random() * 100);
+        int b = (int)(Math.random() * 100);
+        int result = calculator.add(a, b);
+        assertThat(result).isEqualTo(a + b);
+    }
+
+    @Test
+    void substrate() {
+        int a = (int)(Math.random() * 100);
+        int b = (int) (Math.random() * 100);
+        int result = calculator.substrate(a, b);
+        assertThat(result).isEqualTo(a - b);
+    }
+
+    @Test
+    void divide() {
+        int a = 50;
+        int b = 2;
+        int result = calculator.divide(a, b);
+        assertThat(result).isEqualTo(25);
+        assertThatExceptionOfType(ArithmeticException.class)
+                .isThrownBy(() -> {
+                    calculator.divide(a, 0);
+                });
+    }
+
+    @Test
+    void multiply() {
+        int a = (int) (Math.random() * 100);
+        int b = (int) (Math.random() * 100);
+        int result = calculator.multiply(a, b);
+        assertThat(result).isEqualTo(a * b);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringForisOperator")
+    void isOperator_ShouldReturnTrueForOperators(String input) {
+        assertThat(calculator.isOperator(input)).isTrue();
+    }
+
+    public static Stream<Arguments> provideStringForisOperator() {
+        return Stream.of(
+            Arguments.of("+", true),
+            Arguments.of("-", true),
+            Arguments.of("*", true),
+            Arguments.of("/", true),
+            Arguments.of("1234", false),
+            Arguments.of(" ", false),
+            Arguments.of("#", false),
+            Arguments.of("@", false)
+            );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForisBlank")
+    void isBlank_ShouldReturnTrueForBlanks(String input, boolean expected) {
+        assertThat(calculator.isBlank(input)).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> provideStringsForisBlank(){
+        return Stream.of(
+                Arguments.of("", true),
+                Arguments.of(" ", true),
+                Arguments.of("blank", false),
+                Arguments.of("a", false),
+                Arguments.of("1234", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForisNumber")
+    void isNumber_ShouldReturnTrueForNumbers(String input, boolean expected) {
+        assertThat(calculator.isNumebr(input)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideStringsForisNumber() {
+        return Stream.of(
+                Arguments.of("56", true),
+                Arguments.of("", false),
+                Arguments.of("+", false),
+                Arguments.of("-", false),
+                Arguments.of("*", false),
+                Arguments.of("/", false),
+                Arguments.of(" ", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForisNumber")
+    void checkNumber(String input, boolean expected) {
+        if (expected == false) {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> {
+                        calculator.checkNumber(input);
+                    });
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringForisOperator")
+    void checkOperator(String input, boolean expected) {
+        if (expected == false) {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> {
+                        calculator.checkOperator(input);
+                    });
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideIntegerForcalculate")
+    void calculate(String op, int a, int b, int result) {
+        assertThat(calculator.calculate("+", a, b)).isEqualTo(result);
+    }
+
+    public static Stream<Arguments> provideIntegerForcalculate() {
+        return Stream.of(
+                Arguments.of("+", 36, 45, calculator.add(36, 45)),
+                Arguments.of("-", 36, 45, calculator.substrate(36, 45)),
+                Arguments.of("*", 36, 45, calculator.multiply(36, 45)),
+                Arguments.of("/", 36, 45, calculator.divide(36, 45))
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"@", "a", "8"})
+    void calculate_ShouldThrowIllegalArgumentException(String input) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    calculator.calculate(input, 1, 1);
+                    calculator.calculate(input, 1, 1);
+                    calculator.calculate(input, 1, 1);
+                });
+    }
+
+    @Test
+    void parseEquation_ShouldReturnSplitStringOfEquation() {
+        assertThat(calculator.parseEquation("3 / 2 * 5 + 6 - 7"))
+                .containsExactly("3", "/", "2", "*", "5", "+", "6", "-", "7");
+        assertThat(calculator.parseEquation("8 + 5 * 7 + 7 - 6 - 135 / 7 * 0 - 1000"))
+                .containsExactly("8", "+", "5", "*", "7", "+", "7", "-", "6", "-", "135", "/", "7", "*", "0", "-", "1000");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "+ 3 * 2",
+            "3 + 2  * 3",
+            "3 3 * 2 + 100",
+            "3 * 2 % 5"})
+    void parseEquation_ShouldThrowIllegalArgumentException(String input) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    calculator.parseEquation(input);
+                });
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideEquationForgetResultOfCalculator")
+    void getResultOfCalculator(String[] parsedEquation, int result) {
+        assertThat(calculator.getResultOfCalculator(parsedEquation)).isEqualTo(result);
+    }
+
+    public static Stream<Arguments> provideEquationForgetResultOfCalculator() {
+        return Stream.of(
+                Arguments.of(calculator.parseEquation("2 + 3 * 4 / 2"), 10),
+                Arguments.of(calculator.parseEquation("5 - 2 / 3 * 7 + 100"), 107),
+                Arguments.of(calculator.parseEquation("3 - 3 * 100 / 5 - 30"), -30),
+                Arguments.of(calculator.parseEquation("20 * 60 - 5 / 5 + 7 * 2"), 492)
+        );
+    }
+}
