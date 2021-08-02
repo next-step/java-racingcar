86a9503782434a5717386b5fd77e2cd1e727cd4f
janghoonseob <arpy1228@gmail.com>
2021-08-02 18:03:42 +0900

    feat: 우승자 판별 기능 추가 feat: 우승자 출력 기능 추가

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,43 +1,53 @@
 package controller;
 
 import domain.Car;
+import domain.Winner;
 import util.RandomNumUtil;
 import util.StringUtil;
 import view.InputView;
 import view.ResultView;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class GameController {
 
     List<Car> participant;
     private int gameCount;
+    private InputView inputView;
+    private Winner winners;
 
     public GameController() {
         participant = new ArrayList<>();
+        inputView = new InputView();
+        winners = new Winner();
     }
 
     public void init() throws IllegalArgumentException {
-        InputView inputView = new InputView();
         String[] carNames = StringUtil.participants(inputView.participantInputView());
 
         for (int count = 0; count < carNames.length; count++) {
             participant.add(new Car(carNames[count]));
         }
-
         gameCount = inputView.playInputView();
-
         for (int count = 0; count < gameCount; count++) {
             playGame();
             ResultView.racingView(participant);
         }
+        winners.createWinnersData(participant, readWinnerCarDistance());
+        ResultView.winnersView(winners.readWinners());
     }
 
     public void playGame() {
         for(int count = 0; count < participant.size(); count++) {
             participant.get(count).carMoveBehavior(RandomNumUtil.makeRandomNumber());
         }
+    }
+
+    public int readWinnerCarDistance() {
+        participant.sort(Comparator.comparing(Car::readCarDistance).reversed());
+        return participant.get(0).readCarDistance();
     }
 
 }
--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -3,6 +3,7 @@ package view;
 import domain.Car;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 public class ResultView {
 
@@ -22,6 +23,14 @@ public class ResultView {
             sb.append("-");
         }
         sb.append("\r\n");
+    }
+
+    public static void winnersView(List<Car> winners) {
+        StringJoiner stringJoiner = new StringJoiner(",");
+        for (Car winner : winners) {
+            stringJoiner.add(winner.readParticipantName());
+        }
+        System.out.println(stringJoiner.toString() + "가 최종 우승했습니다.");
     }
 
 }
