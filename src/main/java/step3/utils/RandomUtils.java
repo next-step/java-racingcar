de2bbb026d4fb9ed5cfa81c2d03c7df3eb2d86fb
longbeom <wowow1859@naver.com>
2021-08-02 23:53:45 +0900

    refactor : Random 인스턴스 재사용

--- src/main/java/step3/utils/RandomUtils.java
+++ src/main/java/step3/utils/RandomUtils.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class RandomUtils {
 	private static final int RANDOM_BOUND = 10;
+	private static final Random RANDOM = new Random();
 
 	public static int randomNumber() {
-		return new Random().nextInt(RANDOM_BOUND);
+		return RANDOM.nextInt(RANDOM_BOUND);
 	}
 }
