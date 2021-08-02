5f75e6226ca05846fcc5e5a44c0648b6f0ec6477
jaeyoungjang2 <lion85488@gmail.com>
2021-08-02 21:29:28 +0900

    feat: Race 경주 클래스 추가 자동차 경주의 주요 로직을 담당 carRacer 생성

--- src/main/java/step5/domain/Race.java
+++ src/main/java/step5/domain/Race.java
@@ -1,2 +1,19 @@
-package step5.domain;public class Race {
+package step5.domain;
+
+import com.sun.deploy.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Race {
+    public Race(String[] carNames, int round) {
+        List<CarRacer> carRacers = enroll(carNames);
+    }
+
+    private List<CarRacer> enroll(String[] carNames) {
+        return Arrays.stream(carNames)
+                .map(CarRacer::new)
+                .collect(Collectors.toList());
+    }
 }
