123c9cd8437314aac6fe255ea4112719b3999cbf
devchang202 <devchang202@gmail.com>
2021-08-03 00:04:46 +0900

    style(subtract) : 메소드 명과 괄호 사이의 공백 삭제

--- src/main/java/com/devchang202/calculator/Calculator.java
+++ src/main/java/com/devchang202/calculator/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
         return a + b;
     }
 
-    int subtract (int a, int b){
+    int subtract(int a, int b) {
         return a - b;
     }
 
@@ -17,7 +17,7 @@ public class Calculator {
         return a / b;
     }
 
-    int multiply(int a, int b){
+    int multiply(int a, int b) {
         return a * b;
     }
 
