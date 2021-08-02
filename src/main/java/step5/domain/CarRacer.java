9d796bce60092c0fb7fc34d4496d913d3ad905e6
jaeyoungjang2 <lion85488@gmail.com>
2021-08-02 21:30:20 +0900

    feat: 카레이서 클래스 추가 자동차 이름 검증을 카레이서 클래스에서 진행

--- src/main/java/step5/domain/CarRacer.java
+++ src/main/java/step5/domain/CarRacer.java
@@ -1,2 +1,17 @@
-package step5.domain;public class CarRacer {
+package step5.domain;
+
+public class CarRacer {
+    static final String VALID_CAR_ERROR = "자동차 이름의 최대 길이는 5글자입니다.";
+    static final int INIT_START_POSITION = 1;
+    static Car car;
+    public CarRacer(String carName) {
+        checkValidCar(carName);
+        car = new Car(INIT_START_POSITION);
+    }
+
+    private void checkValidCar(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException(VALID_CAR_ERROR);
+        }
+    }
 }
