9cd39bc28e8c60fad36b3dbc51311f671f02bbb4
bj-lee90 <littleprince0717@gmail.com>
2021-08-03 00:19:58 +0900

    refactor(step3): 게임 결과 출력 기능 ResultView 클래스에 위임

--- src/main/java/study/step3/model/RacingGame.java
+++ src/main/java/study/step3/model/RacingGame.java
@@ -1,22 +1,25 @@
 package study.step3.model;
 
 import study.step3.model.car.Cars;
+import study.step3.view.ResultView;
 
 public class RacingGame {
 
   private Cars cars;
+  private ResultView resultView;
 
   private int racingGameRound;
 
   public RacingGame(int carNumber, int gameRound) {
     cars = new Cars(carNumber);
     racingGameRound = gameRound;
+    resultView = new ResultView();
   }
 
   public void start() {
     for(int i=0; i<racingGameRound; i++) {
       cars.move();
-      cars.printResult();
+      resultView.printResult(cars.getRacingGameCars());
     }
   }
 
--- src/main/java/study/step3/view/ResultView.java
+++ src/main/java/study/step3/view/ResultView.java
@@ -2,9 +2,18 @@ package study.step3.view;
 
 import study.step3.model.car.Car;
 
+import java.util.List;
+
 public class ResultView {
 
     private static final String DISTANCE_SEPERATOR = "-";
+
+    public void printResult(List<Car> racingGameCars) {
+        for (Car car : racingGameCars) {
+            printCarDistance(car);
+        }
+        System.out.println();
+    }
 
     public void printCarDistance(Car car) {
         for (int i = 0; i < car.getDistance(); i++) {
@@ -12,5 +21,4 @@ public class ResultView {
         }
         System.out.println();
     }
-
 }
