622af60b4d95be7b4dbe27fc55673b5bd46e6af4
jspark940321 <jspark940321@gmail.com>
2021-08-03 22:48:02 +0900

    fix: 사용하지 않은 변수 제거
    https://github.com/next-step/java-racingcar/pull/2552#discussion_r681044109

--- src/main/java/Car/RacingCarMain.java
+++ src/main/java/Car/RacingCarMain.java
@@ -10,7 +10,6 @@ public class RacingCarMain {
         InputView inputView = new InputView();
 
         String[] carNames = inputView.getCarNames("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        int carCount = carNames.length;
         int gameCount = inputView.getInt("시도할 회수는 몇 회 인가요?");
 
         RacingCar racingCar = new RacingCar(carNames);
