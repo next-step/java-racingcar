6c7983015591977785520456faa1cbf89e89af71
harris <harrisleesh@gmail.com>
2021-08-03 19:37:02 +0900

    feat : Create 1st collection RacingCars

---
+++ src/main/java/racingcar/entity/RacingCars.java
@@ -0,0 +1,53 @@
+package racingcar.entity;
+
+import racingcar.strategy.MoveStrategy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RacingCars {
+    private List<RacingCar> racingCars;
+
+    public RacingCars(List<RacingCar> racingCars) {
+        this.racingCars = racingCars;
+    }
+
+    public void moveIfMovableAll(MoveStrategy moveStrategy) {
+        for (RacingCar racingCar : racingCars) {
+            racingCar.moveIfMovable(moveStrategy);
+        }
+    }
+
+    public int findMaxPosition() {
+        int maxPosition = 0;
+        for (RacingCar racingCar : racingCars) {
+            maxPosition = racingCar.ifBiggerThanReturnPosition(maxPosition);
+        }
+        return maxPosition;
+    }
+
+    public List<String> pickWinners(int winnerRecord) {
+        return racingCars.stream()
+                .filter(racingCar -> racingCar.getPosition() == winnerRecord)
+                .map(RacingCar::getName)
+                .collect(Collectors.toList());
+    }
+
+    public void initialize(String[] carNames) {
+        for (int i = 0; i < carNames.length; i++) {
+            racingCars.add(new RacingCar(carNames[i]));
+        }
+    }
+
+    public int size() {
+        return racingCars.size();
+    }
+
+    public RacingCar get(int i) {
+        return racingCars.get(i);
+    }
+
+    public List<RacingCar> getRacingCars() {
+        return racingCars;
+    }
+}
