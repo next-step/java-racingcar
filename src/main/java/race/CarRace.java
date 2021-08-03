e5d6bdcee7dfbc12fca08edf1213d0e545a042cb
yuhyeon <klyhyeon@gmail.com>
2021-08-03 10:58:37 +0900

    feat: 자동차 이동 메서드, 실행 메서드

--- src/main/java/race/CarRace.java
+++ src/main/java/race/CarRace.java
@@ -5,7 +5,9 @@ public class CarRace {
     private int carCount;
 
     private int gameCount;
-    
+
+    private String[] carDistance;
+
     private final String messageCarCount = "자동차 대수는 몇 대 인가요?";
 
     private final String messageGameCount = "시도할 회수는 몇 회 인가요?";
@@ -22,26 +24,60 @@ public class CarRace {
         return gameCount;
     }
 
+    public static void main(String[] args) {
+        CarRace carRace = new CarRace();
+        carRace.initGame();
+    }
+
     public void initGame() {
         setGameProfile();
+        playGame();
     }
+
 
     protected void setGameProfile() {
         carCount = setCarCount();
         gameCount = setGameCount();
+        setCarDistance();
     }
 
     protected int setCarCount() {
         printOut(messageCarCount);
-       return InputOutput.inputOutput();
-    } 
+       return InputView.inputOutput();
+    }
 
     protected int setGameCount() {
         printOut(messageGameCount);
-        return InputOutput.inputOutput();
+        return InputView.inputOutput();
     }
-    
+
+    protected void setCarDistance() {
+        carDistance = new String[carCount];
+        for (int i = 0; i < carCount; i++) {
+            carDistance[i] = "";
+        }
+    }
+
+
     private void printOut(String message) {
-        System.out.println(message);   
+        System.out.println(message);
     }
+
+    private void saveCarMovement() {
+
+    }
+
+    //TODO: 컨밴션 지켜 리팩토링
+    private void playGame() {
+        while(gameCount-- > 0) {
+            int tempCarCount = 0;
+            while (tempCarCount < carCount) {
+                if (RandomNum.getRandomNum() > 4)
+                    carDistance[tempCarCount] += "-";
+                tempCarCount++;
+            }
+            ResultView.printResult(carDistance);
+        }
+    }
+
 }
--- src/test/java/race/CarRaceTest.java
+++ src/test/java/race/CarRaceTest.java
@@ -14,15 +14,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarRaceTest {
 
+    private CarRace carRace = new CarRace();
+
     @ParameterizedTest
     @ValueSource(strings = {"5", "!"})
     @DisplayName("게임프로필 테스트")
     void setGameProfileTest(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
         System.setIn(in);
-        CarRace carRace = new CarRace();
         carRace.setGameProfile();
         assertThat(carRace.getCarCount()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("랜덤숫자 테스트")
+    void getRandomNumTest() {
+        RandomNum randomNum = new RandomNum();
+        assertThat(randomNum.getRandomNum()).isLessThanOrEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차 경주 테스트")
+    void playGameTest() {
+        carRace.initGame();
     }
 
 
