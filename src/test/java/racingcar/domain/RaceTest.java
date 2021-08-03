78dcc6550ac1e03ace15f9cb4e3ff2b04d2975af
devchang202 <devchang202@gmail.com>
2021-08-03 22:12:35 +0900

    test(Race) : throw Exception if the arguments are empty

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class RaceTest {
 
@@ -17,5 +19,15 @@ public class RaceTest {
         Race race = new Race(carNum, 1);
 
         assertThat(race.cars.size()).isEqualTo(carNum);
+    }
+
+    @DisplayName("Race() : Car 개수 입력이 잘 못될 경우 Exception 발생")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void Race_ShouldThrowIllegalArgumentException(carNum, raceNum) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    Race race = new Race(carNum, raceNum);
+                });
     }
 }
