91ab6f9f346aaab543651e544329d8b6328343c5
yook0328 <yook0328@gmail.com>
2021-08-04 22:54:38 +0900

    refactor: GameInputDto 생성 방식 변경에 따른 수정.

--- src/main/java/com/racingcar/view/InputView.java
+++ src/main/java/com/racingcar/view/InputView.java
@@ -6,17 +6,16 @@ import java.util.Scanner;
 
 public class InputView {
     public static GameInputDto drawAndParse() {
-        GameInputDto input = new GameInputDto();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        input.setNames(scanner.nextLine());
+        String names = scanner.nextLine();
 
         System.out.println("시도할 횟수는 몇 회 인가요?");
-        input.setGameRound(scanner.nextInt());
+        int gameRound = scanner.nextInt();
 
         System.out.println();
 
-        return input;
+        return new GameInputDto(names, gameRound);
     }
 }
