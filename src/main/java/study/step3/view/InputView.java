28f33b306fc50a1d1aa9c626619827adcc9497b7
bj-lee90 <littleprince0717@gmail.com>
2021-08-04 22:52:13 +0900

    refactor(step3): scanner 객체 생성 로직 개선

--- src/main/java/study/step3/view/InputView.java
+++ src/main/java/study/step3/view/InputView.java
@@ -6,19 +6,19 @@ import java.util.Scanner;
 
 public class InputView {
 
+    public static final Scanner scanner = new Scanner(System.in);
+
     public Input getUserInput() {
         return new Input(getCarNumber(), getRacingGameRound());
     }
 
     public int getCarNumber() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 
     public int getRacingGameRound() {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 }
