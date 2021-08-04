0e41666cd8c7a97d45ce76fdf19d439ff49555ee
yook0328 <yook0328@gmail.com>
2021-08-04 19:03:00 +0900

    refactor: InputView 요구사항에 맞게 변경.

--- src/main/java/com/racingcar/view/InputView.java
+++ src/main/java/com/racingcar/view/InputView.java
@@ -1,16 +1,16 @@
 package com.racingcar.view;
 
-import com.racingcar.view.model.GameInput;
+import com.racingcar.view.model.GameInputDto;
 
 import java.util.Scanner;
 
 public class InputView {
-    public static GameInput drawAndParse() {
-        GameInput input = new GameInput();
+    public static GameInputDto drawAndParse() {
+        GameInputDto input = new GameInputDto();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        input.setNumberOfCars(scanner.nextInt());
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        input.setNames(scanner.nextLine());
 
         System.out.println("시도할 횟수는 몇 회 인가요?");
         input.setGameRound(scanner.nextInt());
