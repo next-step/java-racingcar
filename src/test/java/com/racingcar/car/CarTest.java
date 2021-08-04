8a94203766f2508c04f0e88c29107cacf0c9352a
yook0328 <yook0328@gmail.com>
2021-08-04 18:36:16 +0900

    test: 테스트 값 수정.

--- src/test/java/com/racingcar/car/CarTest.java
+++ src/test/java/com/racingcar/car/CarTest.java
@@ -22,7 +22,7 @@ public class CarTest {
 
     @DisplayName("자동차 이동 테스트")
     @ParameterizedTest
-    @CsvSource({"0,0", "1,0", "2,0", "3,0", "4,1", "5,1", "6,1", "7,1", "8,1", "9,1"})
+    @CsvSource({"0,1", "1,1", "2,1", "3,1", "4,2", "5,2", "6,2", "7,2", "8,2", "9,2"})
     void move(int input, int expected) {
         //when
         car.move(input);
@@ -52,6 +52,6 @@ public class CarTest {
                 .filter((value) -> value >= 4)
                 .count();
 
-        assertThat(car.getDistance()).isEqualTo(expected);
+        assertThat(car.getDistance()).isEqualTo(expected + 1);
     }
 }
