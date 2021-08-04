65243d10ca8d69b04175da248e1db67e5655e482
wlswn3974 <86590878+wlswn3974@users.noreply.github.com>
2021-08-04 09:23:54 +0900

    문자열 계산기

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,98 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.function.BinaryOperator;
+
+import static java.lang.Double.valueOf;
+
+class StringCalculator {
+    private String[] values;
+
+    private LinkedList<Double> operands;
+    private LinkedList<String> operators;
+
+    private double result;
+
+    public StringCalculator() {
+        this(new LinkedList<>(), new LinkedList<>());
+    }
+
+    private StringCalculator(LinkedList<Double> operands, LinkedList<String> operators) {
+        this.operands = operands;
+        this.operators = operators;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void calculate() {
+        result = operands.poll();
+        while(operands.size() != 0) {
+            Operator operator = Operator.of(operators.poll());
+            result = operator.calculate(result, operands.poll());
+        }
+    }
+
+    public void enter(final String s) {
+        values = s.split(" ");
+        if(values.length < 3) {
+            throw new IllegalArgumentException("입력값이 올바르지 않습니다");
+        }
+        for(int i = 0; i < values.length; i++) {
+            validate(i, values[i]);
+            add(values[i]);
+        }
+    }
+
+    private void validate(final int idx, final String value) {
+        if(idx % 2 == 0 && !isNumeric(value)) {
+            throw new IllegalArgumentException("입력값이 올바르지 않습니다");
+        }
+        if(idx % 2 == 1 && isNumeric(value)) {
+            throw new IllegalArgumentException("입력값이 올바르지 않습니다");
+        }
+    }
+
+    private void add(final String value) {
+        if(isNumeric(value)) {
+            operands.add(valueOf(value));
+        }
+        if(!isNumeric(value)) {
+            operators.add(value);
+        }
+    }
+
+    private static boolean isNumeric(final String s) {
+        if("".equals(s)) {
+            return false;
+        }
+        return s.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    private enum Operator {
+        PLUS("+", (e1, e2)->e1 + e2),
+        MINUS("-", (e1, e2)->e1 - e2),
+        DIVISION("/", (e1, e2)->e1 / e2),
+        MULTI("*", (e1, e2)->e1 * e2);
+
+        private String operator;
+        private BinaryOperator<Double> operating;
+
+        Operator(final String operator, final BinaryOperator<Double> operating) {
+            this.operator = operator;
+            this.operating = operating;
+        }
+
+        private static Operator of(final String operator) {
+            return Arrays.stream(Operator.values())
+                    .filter(value->value.operator.equals(operator))
+                    .findFirst()
+                    .orElseThrow(()->new IllegalArgumentException("유효한 연산자 형식이 아닙니다."));
+        }
+
+        private Double calculate(final Double e1, final Double e2) {
+            return operating.apply(e1, e2);
+        }
+    }
+
+}
---
+++ src/test/java/stringCalculatorTest.java
@@ -0,0 +1,50 @@
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class stringCalculatorTest {
+
+    StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @DisplayName("입력_테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"",
+                "++*/",
+                "12345",
+                "2 3 42",
+                "2 + + * 4 / 2",
+                "+ + + + + + /,"})
+    void enter(String param) {
+        assertThatThrownBy(() -> {
+            calculator.enter(param);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력값이 올바르지 않습니다");
+    }
+
+    @DisplayName("계산_테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1 + 2:3",
+            "4 - 2:2",
+            "6 * 2:12",
+            "8 / 4:2",
+            "2 * 3 / 3 / 2:1",
+            "2 + 3 * 4 / 2:10"}, delimiter = ':')
+    void calculate(String param, double expected) {
+        calculator.enter(param);
+        calculator.calculate();
+        assertThat(calculator.getResult()).isEqualTo(expected);
+    }
+
+}
+
