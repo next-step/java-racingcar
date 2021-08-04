5075c53069ed7037cbb90aac7c53ea6d77dec64b
Jade <j2de.dev@gmail.com>
2021-08-04 22:56:07 +0900

    feat: getForwardCondition

--- src/main/java/stage3/Car.java
+++ src/main/java/stage3/Car.java
@@ -1,27 +1,16 @@
 package stage3;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class Car {
-    public void start() {
-        printNotice("자동차 대수는 몇 대 인가요?");
-        int numOfCars = input();
-        printNotice("시도할 회수는 몇 회 인가요?");
-        int numOfTries = input();
+    private int status;
+
+    public Car() {
+        status = 0;
     }
 
-    private void printNotice(String text) {
-        System.out.println(text);
-    }
-
-    private int input() {
-        Scanner sc = new Scanner(System.in);
-        int result = sc.nextInt();
-        validateInput(result);
-        return result;
-    }
-
-    void validateInput(int input) {
-        if (input < 0) throw new IllegalArgumentException("음수를 입력할 수 없습니다.");
+    public int getForwardCondition() {
+        Random random = new Random();
+        return random.nextInt(10);
     }
 }
--- src/test/java/stage3/CarTest.java
+++ src/test/java/stage3/CarTest.java
@@ -2,30 +2,34 @@ package stage3;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CarTest {
-    private Car c;
+    private CarApplication ca;
+    private Car car;
 
     @BeforeAll
     void setup() {
-        c = new Car();
+        ca = new CarApplication();
+        car = new Car();
     }
 
-    @DisplayName("input 비정상 case - 음수")
-    @ParameterizedTest
-    @ValueSource(ints = {-1, -2})
-    void invalidInputTest(int input) {
-        // when
-        Throwable throwable = catchThrowable(() -> c.validateInput(input));
-
-        // than
-        assertThat(throwable).isExactlyInstanceOf(IllegalArgumentException.class); // todo
+    @Test
+    @DisplayName("전진 조건 테스트")
+    void forwardConditionTest() {
+        int minValue = 0;
+        int maxValue = 9;
+        int result = car.getForwardCondition();
+        assertTrue(result >= minValue && result <= maxValue);
     }
+
+//    @Test
+//    @DisplayName("전진 조건 얻기")
+//    void tryTest() {
+//        car.tryForward();
+//    }
 }
