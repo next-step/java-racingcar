b03f4f80b94b8a225b40341631bd2e3edb90af5e
yook0328 <yook0328@gmail.com>
2021-08-04 18:40:46 +0900

    feat: 자동차 객체 이름 추가 밑 이름에 대한 제약조건 추가.

--- src/main/java/com/racingcar/car/Car.java
+++ src/main/java/com/racingcar/car/Car.java
@@ -3,6 +3,16 @@ package com.racingcar.car;
 public class Car {
     private int distance = 1;
     private static final int MOVE_STANDARD = 4;
+    private static final int NAME_MAX_LENGTH = 4;
+    private final String name;
+
+    public Car(String name) {
+        if (name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("name must under 4 characters, name : " + name);
+        }
+
+        this.name = name;
+    }
 
     public void move(int value) {
         if (value < MOVE_STANDARD) {
@@ -14,5 +24,9 @@ public class Car {
 
     public int getDistance() {
         return distance;
+    }
+
+    public String getName() {
+        return name;
     }
 }
