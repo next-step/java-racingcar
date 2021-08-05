449e79833799e4970db3b7e7140a9c7f198a4da3
minyul <ggomjae@gmail.com>
2021-08-05 21:19:02 +0900

    5글자를 초과시 에러를 던지는 테스트를 하다.

--- src/test/java/step4/AppTest.java
+++ src/test/java/step4/AppTest.java
@@ -5,13 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class AppTest {
 
@@ -26,21 +23,15 @@ public class AppTest {
 		race = new Race(numberContainer);
 	}
 
-//	@DisplayName("자동차가 가는지 결과를 확인하는 테스트")
-//	@ParameterizedTest
-//	@CsvSource(value = {"3:0", "4:1","5:1"}, delimiter = ':')
-//	void goTest(int randomNumber, int resultDistance) {
-//		car.move(randomNumber);
-//		int carDistance = car.getDistance();
-//		assertThat(carDistance).isEqualTo(resultDistance);
-//	}
-//
-//	@DisplayName("자동차의 이름이 제대로 부여되는지 확인하는 테스트")
-//	@Test
-//	void carNameTest() {
-//		String carName = "MinYulCar";
-//		assertThat(car.getCarName()).isEqualTo(carName);
-//	}
+	@DisplayName("자동차가 가는지 결과를 확인하는 테스트")
+	@ParameterizedTest
+	@CsvSource(value = {"3:0", "4:1","5:1"}, delimiter = ':')
+	void goTest(int randomNumber, int resultDistance) {
+		Car car = new Car("yul", 0);
+		car.move(randomNumber);
+		int carDistance = car.getDistance();
+		assertThat(carDistance).isEqualTo(resultDistance);
+	}
 
 	@DisplayName("우승자를 확실히 반환하는지 확인하는 테스트")
 	@Test
@@ -55,11 +46,11 @@ public class AppTest {
 		assertThat(winners).contains(carOne, carThree);
 	}
 
-//	@DisplayName("쉼표로 String Name을 구분했을 때 잘 구분하는 지 확인하는 테스트")
-//	@ParameterizedTest
-//	@ValueSource(strings = {"a,b,c","a,b"})
-//	void commaCarNameTest(String input) {
-//		String[] inputArray = input.split(",");
-//		assertThat(inputConsole.getCarNames()).isEqualTo(inputArray);
-//	}
+	@DisplayName("5글자 초과시 에러를 던지는 테스트")
+	@Test
+	void carNameTest() {
+		assertThatIllegalArgumentException().isThrownBy(()-> {
+			Car car = new Car("123456",0);
+		});
+	}
 }
