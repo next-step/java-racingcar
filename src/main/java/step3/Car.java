e97765f79277e3e4f7553bdb740fbcbd4a53d288
haena.lee <haena.lee@bankwareglobal.com>
2021-08-06 22:13:12 +0900

    feat : Validator 추가, ResultView Println Print로 변경

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -14,6 +14,7 @@ public class Car {
         if(randomNum >= moveNumber) {
             distance ++;
         }
+
     }
 
     public int getDistance() {
--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -1,20 +1,14 @@
 package step3;
 
-import java.util.Arrays;
-
 public class ResultView {
 
     private final String GAME_RESULT = "실행결과";
     private final String DASH = "-";
 
-    public void printGameResult(Car[] cars) {
-        Arrays.stream(cars).forEach(a -> printGameResult(a.getDistance()));
-    }
-
     public void printGameResult(int result) {
 
         for(int i = 0; i < result; i++) {
-            System.out.println(DASH);
+            System.out.print(DASH);
         }
 
         System.out.println();
