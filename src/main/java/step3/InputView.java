c61b6d55fb077da2c2b60bcf2f1d9d344c548854
haena.lee <haena.lee@bankwareglobal.com>
2021-08-06 20:29:06 +0900

    refactor : InputView 입력값 받도록 수정

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -4,18 +4,12 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public int getCarCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
+    private final Scanner SCANNER = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
+    public int getCount(String message) {
+        System.out.println(message);
 
-    public int getGameCount() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
-
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 
 }
