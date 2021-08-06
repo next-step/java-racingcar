b172d3e1bd4cb8121168827fb2cdf4953c3c4d06
yook0328 <yook0328@gmail.com>
2021-08-06 23:15:04 +0900

    test: RacingGame 비즈니스 로직 TC 구현.

--- src/test/java/com/racingcar/game/RacingGameTest.java
+++ src/test/java/com/racingcar/game/RacingGameTest.java
@@ -3,20 +3,81 @@ package com.racingcar.game;
 import com.racingcar.view.model.GameInputDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
 public class RacingGameTest {
-    @DisplayName("RacingGame 정상 동작 테스트")
+    @DisplayName("RacingGame 게임 종료 이후 플레이 요청 테스트")
     @Test
-    public void play() {
+    public void playOneRoundAfterFinish() {
         //given
         String names = "john,cart,mon,see";
-        int gameRound = 5;
+        int gameRound = 1;
+        GameInputDto input = new GameInputDto(names, gameRound);
+
+        //when
+        RacingGame racingGame = GameMaker.make(input);
+        racingGame.playOneRound();
+
+        //then
+        assertThatIllegalStateException().isThrownBy(() -> {
+            racingGame.playOneRound();
+        });
+    }
+
+    @DisplayName("RacingGame 종료 전 우승자 요청 테스트")
+    @Test
+    public void getWinnersBeforeFinish() {
+        //given
+        String names = "john,cart,mon,see";
+        int gameRound = 1;
         GameInputDto input = new GameInputDto(names, gameRound);
 
         //when
         RacingGame racingGame = GameMaker.make(input);
 
         //then
-        racingGame.play();
+        assertThatIllegalStateException().isThrownBy(() -> {
+            racingGame.getWinners();
+        });
+    }
+
+    @DisplayName("Game 종료 여부 확인 테스트")
+    @ParameterizedTest
+    @CsvSource({"3,3", "4,4", "5,5"})
+    public void isGameFinished(int gameRound, int plays) {
+        //given
+        String names = "john,cart,mon,see";
+        GameInputDto input = new GameInputDto(names, gameRound);
+
+        //when
+        RacingGame racingGame = GameMaker.make(input);
+        for (int i = 0; i < plays; i++) {
+            racingGame.playOneRound();
+        }
+
+        //then
+        assertThat(racingGame.isGameFinished()).isTrue();
+    }
+
+    @DisplayName("Game 종료가 안됬는 지 여부 확인 테스트")
+    @ParameterizedTest
+    @CsvSource({"3,2", "4,3", "5,4"})
+    public void isGameNotFinished(int gameRound, int plays) {
+        //given
+        String names = "john,cart,mon,see";
+        GameInputDto input = new GameInputDto(names, gameRound);
+
+        //when
+        RacingGame racingGame = GameMaker.make(input);
+        for (int i = 0; i < plays; i++) {
+            racingGame.playOneRound();
+        }
+
+        //then
+        assertThat(racingGame.isNotGameFinished()).isTrue();
     }
 }
