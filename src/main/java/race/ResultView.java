9f7bd3d3ed12e45cb680cdd5b979713573cd035a
yuhyeon <klyhyeon@gmail.com>
2021-08-07 15:50:41 +0900

    refactor: Car, Cars 객체 활용해 printResult 메서드 변경

--- src/main/java/race/ResultView.java
+++ src/main/java/race/ResultView.java
@@ -1,17 +1,13 @@
 package race;
 
+import java.util.List;
+
 public class ResultView {
 
-    protected static void printResult(String[] carDistance) {
-        System.out.println(saveResult(carDistance));
-    }
-
-    protected static StringBuilder saveResult(String[] carDistance) {
-        int index = 0;
-        StringBuilder sb = new StringBuilder();
-        while (index < carDistance.length) {
-            sb.append(carDistance[index++]).append("\n");
+    protected static void printResult(Cars cars) {
+        for (Car car: cars.getCarList()) {
+            System.out.println(car.getDistance());
         }
-        return sb;
+        System.out.println();
     }
 }
