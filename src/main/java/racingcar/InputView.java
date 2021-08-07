6062d2bbdca5e7f906d04f48b909c25f2a61edd6
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-07 14:49:54 +0900

    refactor: InputView 클래스를 static으로 변환

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,22 +3,22 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
-    public int getCarNumber() {
+    public static int getCarNumber() {
         printIntroMessage("자동차 대수는 몇 대 인가요?");
         return getUserInput();
     }
 
-    public int getTrialNumber() {
+    public static int getRoundNumber() {
         printIntroMessage("시도할 회수는 몇 회인가요?");
         return getUserInput();
     }
 
-    private int getUserInput() {
+    private static int getUserInput() {
         Scanner scanner = new Scanner(System.in);
         return  scanner.nextInt();
     }
 
-    private void printIntroMessage(String message){
+    private static void printIntroMessage(String message){
         System.out.println(message);
     }
 }
