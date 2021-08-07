6ec14644dd06b0e3288ef7b6993d4f68d197141e
beadoer1 <beadoer1@gmail.com>
2021-08-07 17:39:25 +0900

    feat(RacingGame, RacingGameTest) : RacingGame 실패 테스트 생성

---
+++ src/main/java/edu/nextstep/racingcarwinner/domain/RacingGame.java
@@ -0,0 +1,21 @@
+package edu.nextstep.racingcarwinner.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+    public List<Car> makeCarList(String carNames) {
+        return new ArrayList<>();
+    }
+
+    public void joinGame(List<Car> carList) {
+    }
+
+    public List<Car> getCarList() {
+        return new ArrayList<>();
+    }
+
+    public List<Car> getWinner() {
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/java/edu/nextstep/racingcarwinner/domain/RacingGameTest.java
@@ -0,0 +1,43 @@
+package edu.nextstep.racingcarwinner.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingGameTest {
+
+    @Test
+    void makeCarList(){
+        // given
+        String inputString = "hwan,hyun,kook";
+        RacingGame racingGame = new RacingGame();
+        List<Car> carList = racingGame.makeCarList(inputString);
+
+        // when
+        racingGame.joinGame(carList);
+
+        // then
+        assertThat(racingGame.getCarList()).contains(new Car("hwan"),
+                new Car("hyun"),
+                new Car("kook"));
+    }
+
+    @Test
+    void getWinner(){
+        // given
+        RacingGame racingGame = new RacingGame();
+        racingGame.joinGame(Arrays.asList(new Car("youn", 3),
+                new Car("hyun", 2),
+                new Car("kook", 3)));
+
+        // when
+        List<Car> winners = racingGame.getWinner();
+
+        // then
+        assertThat(winners).contains(new Car("hwan"),
+                new Car("kook"));
+    }
+}
