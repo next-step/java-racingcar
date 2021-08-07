e33403a75ed25c691c394a05071c431ab46a0d4d
beadoer1 <beadoer1@gmail.com>
2021-08-07 16:02:49 +0900

    feat(Car) : Test 에 맞추어 Car 클래스 구현
    구현 기능
    - 생성자, move() 메서드

--- src/main/java/edu/nextstep/racingcarwinner/domain/Car.java
+++ src/main/java/edu/nextstep/racingcarwinner/domain/Car.java
@@ -1,13 +1,35 @@
 package edu.nextstep.racingcarwinner.domain;
 
-public class Car {
-    public Car(String name) {
+import java.util.Objects;
 
+public class Car {
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
     }
 
-    public Car(String younhwan, int i) {
+    public Car(String name, int distance) {
+        this.name = name;
+        this.distance = distance;
     }
 
     public void move() {
+        this.distance++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return distance == car.distance && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, distance);
     }
 }
