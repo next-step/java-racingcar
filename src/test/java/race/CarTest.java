017c523c6f13d03dcbc4de22fc9aa014e33a271b
yuhyeon <klyhyeon@gmail.com>
2021-08-07 20:29:27 +0900

    test: Car 클래스 addDistance 테스트

---
+++ src/test/java/race/CarTest.java
@@ -0,0 +1,20 @@
+package race;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @Test
+    @DisplayName("addDistance 테스트")
+    void addDistanceTest() {
+        //given
+        Car car = new Car("");
+        car.addDistance();
+        //then
+        assertThat(car.getDistance()).contains("");
+    }
+
+}
