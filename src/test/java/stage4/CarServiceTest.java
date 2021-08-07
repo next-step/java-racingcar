3a3acecf475ea3f3fe0c58eff9a2a5c5fa1de3ec
Jade <j2de.dev@gmail.com>
2021-08-08 03:11:51 +0900

    feat: stage3 test 복사

---
+++ src/test/java/stage4/CarServiceTest.java
@@ -0,0 +1,42 @@
+package stage4;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarServiceTest {
+    @Test
+    @DisplayName("tryForward - 멈춤 테스트")
+    void tryStopTest() {
+        // given
+        final int currentStatus = 0;
+        final int impossible = 3;
+        final int expectedStatus = 0;
+        Car car = new Car(currentStatus);
+        CarService carService = new CarService(car);
+
+        // when
+        final int result = carService.tryForward(impossible);
+
+        // then
+        assertThat(expectedStatus).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("tryForward - 전진 테스트")
+    void tryForwardTest() {
+        // given
+        final int currentStatus = 0;
+        final int possible = 4;
+        final int expectedStatus = 1;
+        Car car = new Car(currentStatus);
+        CarService carService = new CarService(car);
+
+        // when
+        final int result = carService.tryForward(possible);
+
+        // then
+        assertThat(expectedStatus).isEqualTo(result);
+    }
+}
