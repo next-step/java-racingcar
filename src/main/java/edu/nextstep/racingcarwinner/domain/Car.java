6116c419f5f9c8d97577b585108dce560a2ebccc
beadoer1 <beadoer1@gmail.com>
2021-08-07 17:02:30 +0900

    feat(Car) : 생성자 및 equals override, move()_random 포함 구현 move() test 실패
    - move() 의 경우 메서드 내부에 randomNum 을 뽑는 로직을 포함하고 있어 test가 어렵다.

--- src/main/java/edu/nextstep/racingcarwinner/domain/Car.java
+++ src/main/java/edu/nextstep/racingcarwinner/domain/Car.java
@@ -1,15 +1,40 @@
 package edu.nextstep.racingcarwinner.domain;
 
-public class Car {
-    public Car(String hwan) {
+import java.util.Objects;
+import java.util.Random;
 
+public class Car {
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
     }
 
-    public Car(String hwan, int i) {
-        
+    public Car(String name, int distance) {
+        this.name = name;
+        this.distance = distance;
     }
 
     public void move() {
+        Random random = new Random();
+        int randomNum = random.nextInt(10);
+        if (randomNum > 4) {
+            this.distance++;
+        }
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return distance == car.distance && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, distance);
     }
 }
