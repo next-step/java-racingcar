fe19503db9a513b8b0789aa088870971912abd19
haena.lee <haena.lee@bankwareglobal.com>
2021-08-08 22:07:45 +0900

    refactor : 필드, 생성자로 순서 변경 / 공백 제거

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -2,23 +2,20 @@ package step3;
 
 public class Car {
 
+    private int distance = 0;
+    private int moveNumber;
+
     public Car(int moveNumber) {
         this.moveNumber = moveNumber;
     }
 
-    private int distance = 0;
-    private int moveNumber;
-
     public void move(int randomNum) {
-
         if(randomNum >= moveNumber) {
             distance ++;
         }
-
     }
 
     public int getDistance() {
         return distance;
     }
-
 }
