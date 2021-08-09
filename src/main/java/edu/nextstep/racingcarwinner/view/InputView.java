a35a5517ab902a896cc1f3a69ed25614af6c41f4
beadoer1 <beadoer1@gmail.com>
2021-08-09 14:22:23 +0900

    refactor(inputView) : 코드 간소화(변수 생략)

--- src/main/java/edu/nextstep/racingcarwinner/view/InputView.java
+++ src/main/java/edu/nextstep/racingcarwinner/view/InputView.java
@@ -13,8 +13,7 @@ public class InputView {
 
     public String inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String carName = scanner.next();
-        return carName;
+        return scanner.next();
     }
 
     public int inputRoundNum() {
