113b0dcad7fb95eef69e5eb8f62508d15444cad4
JoyDaheeCha <daheecha0612@gmail.com>
2021-08-12 19:05:04 +0900

    feat: 자동차 이름 클래스 추가

---
+++ src/main/java/racingcar/Name.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class Name {
+
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() <= 0 || name.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 1글자 이상, 5글자 이하입니다.");
+        }
+    }
+}
