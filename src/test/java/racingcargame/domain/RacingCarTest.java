b7a2b40a910abc2176097b3d1c6c6a1566a92b2b
anbin <idealbin@naver.com>
2021-08-14 23:48:07 +0900

    <refactor> : 우승한 자동차를 확인하는 테스트코드 리팩토링

--- src/test/java/racingcargame/domain/RacingCarTest.java
+++ src/test/java/racingcargame/domain/RacingCarTest.java
@@ -1,12 +1,12 @@
-package racingcargame;
+package racingcargame.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import racingcargame.fake.FakeSuccessMoveRandomOption;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RacingCarTest {
     @DisplayName("값이 4미만일 경우 멈춘다.")
@@ -48,16 +48,12 @@ public class RacingCarTest {
     @DisplayName("우승한 자동차인지 확인할 수 있다.")
     @Test
     void isWinner() {
-        RandomOption movableOption = new FakeSuccessMoveRandomOption();
-        boolean expected = true;
-        int winnerDistance = 3;
+        final int winnerDistance = 1;
         RacingCar car = new RacingCar(new RacingCarName("a"));
-        car.tryToMove(movableOption.randomValue());
-        car.tryToMove(movableOption.randomValue());
-        car.tryToMove(movableOption.randomValue());
+        car.tryToMove(5);
 
         boolean actual = car.isWinner(winnerDistance);
 
-        assertEquals(expected, actual);
+        assertTrue(actual);
     }
 }
