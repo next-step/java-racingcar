96f4c683e22a1489bf4d396593e40e3662f1356a
haena.lee <haena.lee@bankwareglobal.com>
2021-08-15 22:25:00 +0900

    refactor : 입력 클래스 추가

---
+++ src/main/java/step4/util/InputView.java
@@ -0,0 +1,29 @@
+package step4.util;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private final String GAME_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+
+    private Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String enterCarName() {
+        System.out.println(CAR_NAME_MESSAGE);
+        return scanner.next();
+    }
+
+    public int enterGameCount() {
+        System.out.println(GAME_COUNT_MESSAGE);
+
+        int count = scanner.nextInt();
+        scanner.close();
+
+        return count;
+    }
+}
