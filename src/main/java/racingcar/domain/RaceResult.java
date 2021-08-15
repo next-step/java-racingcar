f6418e65d277594e2458b86486d1c1a60331c3ff
조원진 <>
2021-08-15 19:36:26 +0900

    refactor(RaceResult) : roundCount 값 포장, findMovingRecord 매개변수 Name -> carId로 변경

--- src/main/java/racingcar/domain/RaceResult.java
+++ src/main/java/racingcar/domain/RaceResult.java
@@ -4,31 +4,30 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class RaceResult {
-    private int roundCount;
+    private RoundCount roundCount;
     private List<MovingRecord> movingRecords;
 
-    public RaceResult(int roundCount, List<Car> cars) {
+    public RaceResult(RoundCount roundCount, List<Car> cars) {
         this.roundCount = roundCount;
         movingRecords = initMovingRecords(cars);
     }
 
     private List<MovingRecord> initMovingRecords(List<Car> cars) {
-        return cars.stream().map(car -> new MovingRecord(car.getName())).collect(Collectors.toList());
+        return cars.stream().map(MovingRecord::new).collect(Collectors.toList());
     }
 
     public void report(List<Car> cars) {
         for (Car car : cars) {
-            MovingRecord movingRecord = findMovingRecord(car.getName());
+            MovingRecord movingRecord = findMovingRecord(car.getId());
             movingRecord.addPositionRecord(car.getPosition());
         }
     }
 
-    private MovingRecord findMovingRecord(Name name) {
-        MovingRecord findMovingRecord = movingRecords.stream()
-                .filter(movingRecord -> movingRecord.isSame(name))
+    private MovingRecord findMovingRecord(int carId) {
+        return movingRecords.stream()
+                .filter(movingRecord -> movingRecord.isSame(carId))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("잘못된 차 이름입니다."));
-        return findMovingRecord;
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 carId입니다."));
     }
 
     public List<String> findWinners() {
@@ -41,14 +40,14 @@ public class RaceResult {
 
     private int getMaxPosition() {
         return movingRecords.stream()
-                .max(Comparator.comparingInt(movingRecord -> movingRecord.getLastPosition()))
+                .max(Comparator.comparingInt(MovingRecord::getLastPosition))
                 .orElseThrow(() -> new NoSuchElementException("값이 없습니다."))
                 .getLastPosition();
     }
 
 
     public int getRoundCount() {
-        return roundCount;
+        return roundCount.getValue();
     }
 
     public List<MovingRecord> getMovingRecords() {
