43fce622590a6643a13ea98c69f1fb9c9099f4bc
haena.lee <haena.lee@bankwareglobal.com>
2021-08-15 22:55:10 +0900

    feature : 우승자 정하는 메소드 추가

--- src/main/java/step4/domain/Racing.java
+++ src/main/java/step4/domain/Racing.java
@@ -9,11 +9,13 @@ public class Racing {
     private final int MOVE_NUMBER = 4;
     private final String COMMA = ",";
 
+    private int winnerDistance = 0; // 우승자의 distance
+
     public List<Car> initCars(String[] names) {
         List<Car> cars = new ArrayList<>();
 
         for(int i = 0; i < names.length; i++) {
-            cars.add(new Car(names[i], MOVE_NUMBER));
+            cars.add(new Car(names[i].trim(), MOVE_NUMBER));
         }
 
         return cars;
@@ -29,6 +31,30 @@ public class Racing {
         for(Car car : cars) {
             car.move(random.nextInt(10));
         }
+    }
+
+    public List<String> getWinner(List<Car> cars) {
+        List<String> winners = new ArrayList<>();
+
+        for(Car car : cars) {
+            winners = addIfWinner(winners, car);
+        }
+
+        return winners;
+    }
+
+    public List<String> addIfWinner(List<String> winners, Car car) {
+
+        if (winnerDistance == car.getDistance().length()) {
+            // 같을 경우 이름 추가
+            winners.add(car.getName());
+        } else if (winnerDistance < car.getDistance().length()) {
+            // input 차의 거리가 더 클 경우 list 생성
+            winners = new ArrayList<>();
+            winners.add(car.getName());
+        }
+
+        return winners;
     }
 
 }
