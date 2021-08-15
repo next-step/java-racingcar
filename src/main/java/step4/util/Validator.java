da3dcc3c66510cd5ab409f564565be55e1c8023b
haena.lee <haena.lee@bankwareglobal.com>
2021-08-16 00:17:46 +0900

    refactor : 테스트 케이스 작성하며 찾은 버그 수정

--- src/main/java/step4/util/Validator.java
+++ src/main/java/step4/util/Validator.java
@@ -2,18 +2,22 @@ package step4.util;
 
 public class Validator {
 
-    private final String CAR_COUNT_ERROR = "자동차 이름은 한개 이상이어야 합니다.";
+    private final String CAR_COUNT_ERROR = "자동차 이름은 한개 이상 입력 되어야 합니다.";
     private final String GAME_COUNT_ERROR = "게임 횟수는 1번 이상이어야 합니다.";
     private final String NAME_LENGTH_MIN_ERROR = "자동차 이름은 1자 이상이어야 합니다.";
     private final String NAME_LENGTH_MAX_ERROR = "자동차 이름은 5자 이하여야 합니다.";
 
     public void checkInput(String[] names, int gameCount) {
-        checkCount(names.length, CAR_COUNT_ERROR);
-        checkCount(gameCount, GAME_COUNT_ERROR);
         checkName(names);
+        checkCount(gameCount, GAME_COUNT_ERROR);
     }
 
     private void checkName(String[] names) {
+        if (names == null || names.length == 0) {
+            throw new IllegalArgumentException(CAR_COUNT_ERROR);
+        }
+
+        checkCount(names.length, CAR_COUNT_ERROR);
         for(String name : names) {
             checkLength(name);
         }
