8ffc40afa09c3ce0e7f5a62c21b53ff1e32925ae
haena.lee <haena.lee@bankwareglobal.com>
2021-08-15 22:29:09 +0900

    refactor : 검증 클래스 추가

---
+++ src/main/java/step4/util/Validator.java
@@ -0,0 +1,37 @@
+package step4.util;
+
+public class Validator {
+
+    private final String CAR_COUNT_ERROR = "자동차 이름은 한개 이상이어야 합니다.";
+    private final String GAME_COUNT_ERROR = "게임 횟수는 1번 이상이어야 합니다.";
+    private final String NAME_LENGTH_MIN_ERROR = "자동차 이름은 1자 이상이어야 합니다.";
+    private final String NAME_LENGTH_MAX_ERROR = "자동차 이름은 5자 이하여야 합니다.";
+
+    public void checkInput(String[] names, int gameCount) {
+        checkCount(names.length, CAR_COUNT_ERROR);
+        checkCount(gameCount, GAME_COUNT_ERROR);
+        checkName(names);
+    }
+
+    private void checkName(String[] names) {
+        for(String name : names) {
+            checkLength(name);
+        }
+    }
+
+    private void checkLength(String name) {
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException(NAME_LENGTH_MIN_ERROR);
+        }
+
+        if(name.length() > 5) {
+            throw new IllegalArgumentException(NAME_LENGTH_MAX_ERROR);
+        }
+    }
+
+    private void checkCount(int count, String message) {
+        if(count <= 0) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+}
