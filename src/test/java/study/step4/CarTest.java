7ba8f70c70086702d856a3a3f3b1c39c11038593
haena.lee <haena.lee@bankwareglobal.com>
2021-08-16 00:18:38 +0900

    feature : 테스트케이스 작성

---
+++ src/test/java/study/step4/CarTest.java
@@ -0,0 +1,58 @@
+package study.step4;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import step4.domain.Car;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CarTest {
+
+    String name = "TEST";
+    Car car = new Car(name, 4);
+
+    @DisplayName("이름 getter 테스트")
+    @Test
+    void getNameTest() {
+        assertEquals(name, car.getName());
+    }
+
+    @DisplayName("4 이상의 랜덤 값을 받은 경우, 1회 전진")
+    @ParameterizedTest
+    @CsvSource({
+            "4, -",
+            "9, -",
+    })
+    void moveTest_4이상의_랜덤값(int randomNumber, String distance) {
+        car.move(randomNumber);
+        assertEquals(distance, car.getDistance());
+    }
+
+    @DisplayName("4 미만의 랜덤 값을 받은 경우, 0회 전진")
+    @Test
+    void moveTest_4미만의_랜덤값() {
+        car.move(0);
+        assertEquals("", car.getDistance());
+    }
+
+    @DisplayName("4 이상의 랜덤 값을 2회 받아 2회 전진")
+    @Test
+    void moveTest_2회_전진() {
+        car.move(4);
+        car.move(9);
+
+        assertEquals("--", car.getDistance());
+    }
+
+    @DisplayName("4 이상의 랜덤 값을 1회 받아 1회 전진")
+    @Test
+    void moveTest_1회_전진() {
+        car.move(5);
+        car.move(1);
+        car.move(2);
+        assertEquals("-", car.getDistance());
+    }
+
+}
---
+++ src/test/java/study/step4/RacingTest.java
@@ -0,0 +1,116 @@
+package study.step4;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step4.domain.Car;
+import step4.domain.Racing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RacingTest {
+
+    Racing racing = new Racing();
+
+    @DisplayName("initCars 메소드 이름 1개일 때 출력 list size와 이름 테스트")
+    @Test
+    void initCarsTest_이름_1개() {
+        String[] names = {"pobi"};
+        List<Car> cars = racing.initCars(names);
+        assertEquals(1, cars.size());
+        assertEquals(names[0], cars.get(0).getName());
+    }
+
+    @DisplayName("initCars 메소드 이름 3개일 때 출력 list size와 이름 테스트")
+    @Test
+    void initCarsTest_이름_3개() {
+        String[] names = {"pobi1", "pobi2", "pobi3"};
+        List<Car> cars = racing.initCars(names);
+
+        assertEquals(3, cars.size());
+        assertThat(names).containsOnly(names[0], names[1], names[2]);
+    }
+
+    @DisplayName("이름 한개 split 확인")
+    @Test
+    void splitNamesTest_이름_1개() {
+        String[] names = racing.splitNames("pobi");
+        assertEquals(1, names.length);
+        assertEquals("pobi", names[0]);
+    }
+
+    @DisplayName("이름 3개 split 확인")
+    @Test
+    void splitNamesTest_이름_3개() {
+        String[] names = racing.splitNames("pobi1,pobi2,pobi3");
+        assertEquals(3, names.length);
+        assertEquals("pobi1", names[0]);
+        assertEquals("pobi2", names[1]);
+        assertEquals("pobi3", names[2]);
+    }
+
+    @DisplayName("자동차 거리가 모두 다를 경우 우승자 1명 확인")
+    @Test
+    void getWinner_우승자_1명() {
+        List<Car> cars = new ArrayList<>();
+
+        // distance = 3
+        Car car = new Car("winner", 4);
+        car.move(5);
+        car.move(5);
+        car.move(5);
+        cars.add(car);
+
+        // distance = 1
+        car = new Car("pobi1", 4);
+        car.move(5);
+        cars.add(car);
+
+        // distance = 0
+        car = new Car("pobi2", 4);
+        cars.add(car);
+
+        List<String> winner = racing.getWinner(cars);
+
+        assertEquals(1, winner.size());
+        assertEquals("winner", winner.get(0));
+    }
+
+    @DisplayName("자동차 거리가 2명이 같을 경우 우승자 2명 확인")
+    @Test
+    void getWinner_우승자_2명() {
+        List<Car> cars = new ArrayList<>();
+
+        // distance = 1
+        Car car = new Car("pobi", 4);
+        car.move(5);
+        cars.add(car);
+
+        // distance = 2
+        car = new Car("winner1", 4);
+        car.move(5);
+        car.move(5);
+        cars.add(car);
+
+        // distance = 2
+        car = new Car("winner2", 4);
+        car.move(5);
+        car.move(5);
+        cars.add(car);
+
+        List<String> winners = racing.getWinner(cars);
+
+        assertEquals(2, winners.size());
+        assertThat(winners).containsExactlyInAnyOrder("winner1", "winner2");
+    }
+
+    @DisplayName("startRace 메소드 실행 테스트")
+    @Test
+    void startRaceTest() {
+        String[] names = {"pobi1", "pobi2", "pobi3"};
+        racing.startRace(racing.initCars(names));
+    }
+}
