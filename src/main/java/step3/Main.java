76c0dc3725fc3e8c87d7d2dbb6dfdac3cca42e88
longbeom <wowow1859@naver.com>
2021-08-16 18:36:58 +0900

    refactor : main 메소드 기능 분리

--- src/main/java/step3/Main.java
+++ src/main/java/step3/Main.java
@@ -1,26 +1,9 @@
 package step3;
 
-import java.util.List;
-
-import step3.domain.Racing;
-import step3.domain.RacingResults;
-import step3.view.InputView;
-import step3.view.OutputView;
+import step3.controller.RacingController;
 
 public class Main {
 	public static void main(String[] args) {
-		String nameOfCars = InputView.nameOfCars();
-		String[] cars = nameOfCars.split(",");
-
-		int trials = InputView.numberOfTrials();
-
-		List<RacingResults> results = Racing.race(trials, cars);
-		OutputView.viewResult(results);
-
-		int lastResultIndex = results.size() - 1;
-		RacingResults lastResults = results.get(lastResultIndex);
-
-		List<String> champions = lastResults.getChampions();
-		OutputView.viewChampion(champions);
+		RacingController.startRacing();
 	}
 }
