42f7005256a79a4ee816911961fa887402a999d0
khcho902 <khcho902@gmail.com>
2021-08-16 22:39:20 +0900

    fix: RandomValueGeneratorTest에 잘못된 import 수정

--- src/test/java/step45/RandomValueGeneratorTest.java
+++ src/test/java/step45/RandomValueGeneratorTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step3.RandomValueGenerator;
 
 class RandomValueGeneratorTest {
 
