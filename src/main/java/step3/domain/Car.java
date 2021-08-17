60231046bd84cd1bb2bbdbd9e53dc406ba480b10
haena.lee <haena.lee@bankwareglobal.com>
2021-08-17 21:56:47 +0900

    feature : merge conflict 잘못하여 생성자 삭제된 것 추가

--- src/main/java/step3/domain/Car.java
+++ src/main/java/step3/domain/Car.java
@@ -2,26 +2,18 @@ package step3.domain;
 
 public class Car {
 
-<<<<<<< HEAD:src/main/java/step3/Car.java
-=======
     private final String DASH = "-";
     private int moveNumber;
     private StringBuilder distance = new StringBuilder();
 
->>>>>>> 3b065af1 (refactor : StringBuilder 사용하여 미리 distance를 갖고있도록 변경):src/main/java/step3/domain/Car.java
     public Car(int moveNumber) {
         this.moveNumber = moveNumber;
     }
 
-    private int distance = 0;
-    private int moveNumber;
-
     public void move(int randomNum) {
-
         if(randomNum >= moveNumber) {
             distance.append(DASH);
         }
-
     }
 
     public String getDistance() {
