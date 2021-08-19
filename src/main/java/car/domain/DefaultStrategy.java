63f69e070d08a927d22c383250e5a2c631b94e93
jspark940321 <jspark940321@gmail.com>
2021-08-19 22:15:17 +0900

    fix: static 추가

--- src/main/java/car/domain/DefaultStrategy.java
+++ src/main/java/car/domain/DefaultStrategy.java
@@ -1,7 +1,7 @@
 package car.domain;
 
 public class DefaultStrategy implements MovingStrategy{
-    private final int MINNUMBER = 4;
+    private static final int MINNUMBER = 4;
     @Override
     public boolean isMove(int inputNumber) {
         return inputNumber >= MINNUMBER;
