7d9ddffd1085d5f2f3ac9dc42ac4f5bb38993d25
YuHyeon <klyhyeon@globalorder.co.kr>
2021-08-21 17:11:25 +0900

    refactor: 연산로직 전략클래스로 이관

--- src/main/java/race/MoveCondition.java
+++ src/main/java/race/MoveCondition.java
@@ -3,4 +3,6 @@ package race;
 public interface MoveCondition {
 
     int getValue();
+
+    void moveOrNot(Car car);
 }
---
+++ src/test/java/race/RandomNumConditionTest.java
@@ -0,0 +1,9 @@
+package race;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomNumConditionTest {
+
+    //TODO: Test 작성
+
+}
