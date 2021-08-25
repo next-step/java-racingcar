7bdd32c8be5ca6a4ba177f2cc30c0cf0743c182a
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-25 18:55:25 +0900

    refactor

--- src/test/java/carracing/domain/CarFactoryTest.java
+++ src/test/java/carracing/domain/CarFactoryTest.java
@@ -2,7 +2,6 @@ package carracing.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
