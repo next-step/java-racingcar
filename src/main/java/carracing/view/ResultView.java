743b755f174f2a6b890c80c2c351b87bd2737eeb
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-25 18:46:34 +0900

    feat: result view 변경

--- src/main/java/carracing/view/ResultView.java
+++ src/main/java/carracing/view/ResultView.java
@@ -12,19 +12,34 @@ public class ResultView {
     }
 
     public void showResult(RaceResult result) {
-        showGameResult(result.raceResult());
+        for (List<Car> cars : result.raceResult()) {
+            showGameResult(cars);
+        }
+
         showWinnerResult(result.winners());
     }
 
-    public void showGameResult(List<String> raceResults) {
-        for (String raceResult : raceResults) {
-            System.out.println(raceResult);
+    public void showGameResult(List<Car> cars) {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : cars) {
+            sb.append(car.name().value() + " : ");
+            sb.append(makePosition(car.position().value()));
+            sb.append("\n");
         }
+        System.out.println(sb.toString());
+    }
+
+    private String makePosition(int location) {
+        String result = "";
+        for (int i = 0; i < location; i++) {
+            result += "-";
+        }
+        return result;
     }
 
     private void showWinnerResult(List<Car> winners) {
         List<String> names = winners.stream()
-            .map(car -> car.name()).collect(Collectors.toList());
+            .map(car -> car.name().value()).collect(Collectors.toList());
         System.out.println(String.join(", ", names) + "가 최종 우승했습니다.");
     }
 
