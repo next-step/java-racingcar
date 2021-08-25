0de8ff7d2556ad1935251d52a2faba4273df22d0
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-25 17:17:44 +0900

    feat: Position 비교 메소드 추가 및 테스트

--- src/main/java/carracing/domain/Position.java
+++ src/main/java/carracing/domain/Position.java
@@ -26,6 +26,10 @@ public class Position {
         this.position++;
     }
 
+    public Position calBiggerPosition(Position compare) {
+        return this.position > compare.position ? this : compare;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
--- src/test/java/carracing/domain/PositionTest.java
+++ src/test/java/carracing/domain/PositionTest.java
@@ -34,4 +34,34 @@ class PositionTest {
         assertThat(position).isEqualTo(new Position(1));
     }
 
+    @Test
+    @DisplayName("다른 위치와 큰값 비교했을 때 현재 위치가 더 크면 현재 위치가 반환된다.")
+    void positionCalBiggerTest() {
+
+        // given
+        Position input = new Position(3);
+        Position comparePosition = new Position(1);
+
+        // when
+        Position result = input.calBiggerPosition(comparePosition);
+
+        // then
+        assertThat(result).isEqualTo(input);
+    }
+
+    @Test
+    @DisplayName("다른 위치와 큰값 비교했을 때 현재 위치가 더 작으면 다른 위치가 반환된다.")
+    void positionCalSmallerTest() {
+
+        // given
+        Position input = new Position(1);
+        Position comparePostion = new Position(3);
+
+        // when
+        Position result = input.calBiggerPosition(comparePostion);
+
+        // then
+        assertThat(result).isEqualTo(comparePostion);
+    }
+
 }
