b1e45294ecbf3fc1825e6036158f8d4a0f36d590
YuHyeon <klyhyeon@globalorder.co.kr>
2021-08-27 23:35:58 +0900

    refactor: convertDistanceToLine 메서드 사용

--- src/main/java/race/ResultView.java
+++ src/main/java/race/ResultView.java
@@ -1,12 +1,10 @@
 package race;
 
-import java.util.Map;
-
 public class ResultView {
 
     protected static void printResult(Cars cars) {
         for (Car car : cars.getCars()) {
-            System.out.println(car.getName() + " :" + car.getDistance());
+            System.out.println(car.getName() + " :" + convertDistanceToLine(car));
         }
         System.out.println();
     }
