7325d60ce2d56261976ca8a7f8500e73f726dcd6
jaeyoungjang2 <lion85488@gmail.com>
2021-09-04 21:42:36 +0900

    feat: position 출력 결과 기능 ㅜ가 position 1 == '-'

--- src/main/java/step6/view/ResultView.java
+++ src/main/java/step6/view/ResultView.java
@@ -7,9 +7,17 @@ public class ResultView {
     private static final String GUIDE_MESSAGE = "실행 결과";
     public static void printCurrentPosition(Cars cars) {
         for (Car car: cars.getCars()) {
-            System.out.print(car.getCarName() + ": " + car.getPosition());
-            System.out.println();
+            System.out.print(car.getCarName() + ": ");
+            printPositionInfo(car.getPosition());
         }
+        System.out.println();
+    }
+
+    private static void printPositionInfo(int position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
     public static void printResultGuideMessage() {
