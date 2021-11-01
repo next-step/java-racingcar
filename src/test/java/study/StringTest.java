49e14c138f35d5d5d62aa70115e47bd0e6bb86db
kero <jdy410@naver.com>
2021-11-01 23:31:39 +0900

    String 클래스 학습 테스트 - 요구사항2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,5 +12,13 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substring() {
+        String str = "(1,2)";
+        String result = str.substring(1, 4);
+
+        assertThat(result).isEqualTo("1,2");
     }
 }
