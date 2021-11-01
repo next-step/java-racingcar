c467b83a747732f725d28d38ed19aba31a4f9715
soo <yunsoo0510@daum.net>
2021-11-02 01:16:00 +0900

    study test : Set Collection 요구사항3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -24,15 +25,22 @@ public class SetTest {
     }
 
     @Test
-    @DisplayName("set의 사이즈 테스트")
+    @DisplayName("Set의 size() 메소드 테스트")
     void sizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    @DisplayName("")
-    void containsTest(int input) {
+    @DisplayName("Set의 contains() 메소드 테스트 (true 결과)")
+    void containsTest1(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("Set의 contains() 메소드 테스트 (false 결과 포함)")
+    void containsTest2(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
