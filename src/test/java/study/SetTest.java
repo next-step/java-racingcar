56c6acec9b138dc1b82d86b92a647112fe0b7133
JaeHan-Kim <hgal8877@gmail.com>
2021-11-01 23:55:53 +0900

    test(study) : SetTest 요구사항3 - 값을 포함하고 있는지 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -35,5 +36,18 @@ public class SetTest {
     @ValueSource(ints = {1,2,3})
     void containsTest(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("입력된 값이 존재하는지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,true",
+            "2,true",
+            "3,true",
+            "4,false",
+            "5,false",
+    })
+    void containsNumberTrueOrFalse(int number, boolean result) {
+        assertThat(numbers.contains(number)).isEqualTo(result);
     }
 }
