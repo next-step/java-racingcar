2a4445fe9e447ff9d37f9013daf3ed2dcc4dbea5
JaeHan-Kim <hgal8877@gmail.com>
2021-11-01 23:48:54 +0900

    test(study) : SetTest 요구사항1 - 사이즈 체크

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("set 사이즈 테스트")
+    @Test
+    void setSizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
