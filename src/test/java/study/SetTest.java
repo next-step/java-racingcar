b58a93ec4ff9db569805d4d3a6ad94cb5a2690c1
soo <yunsoo0510@daum.net>
2021-11-02 01:12:01 +0900

    study test : Set Collection 요구사항2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,12 @@ public class SetTest {
     @DisplayName("set의 사이즈 테스트")
     void sizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("")
+    void containsTest(int input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
