2c478126fc40e3bb629c7b3a135558ce3682d1cc
aiden.sim <aiden.sim@kakaocorp.com>
2021-11-01 13:02:29 +0900

    [test] StringTest 요구사항3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     private static final String DELIMITER = ",";
@@ -24,5 +25,13 @@ public class StringTest {
         String str = "(1,2)";
         String result = str.substring(1, str.length() - 1);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("'abc' 값이 주어졌을 때 String의 charAt() 메소드를 이용해 특정 위치의 문자를 가져올 때 위치를 벗어나면 StringIndexOutOfBoundsException 발생되는지 검증")
+    void char_at() {
+        String str = "abc";
+        assertThatThrownBy(() -> str.charAt(3)).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
