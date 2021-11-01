2f9a831ab68164c9ddfe0498050f937ed4beddf7
minjoon_lee <minjoon1995@naver.com>
2021-11-01 21:21:39 +0900

    first step

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    public void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    public void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4})
+    void contains(Integer input) {
+        assertTrue(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true","4:false","5:false"}, delimiter = ':')
+    void containsUsingCsvSource(String input, String expected) {
+        assertEquals(numbers.contains(Integer.parseInt(input)),Boolean.valueOf(expected));
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    public void split() {
+        String[] result_1 = "1".split(",");
+        assertThat(result_1).containsExactly("1");
+        String[] result_2 = "1,2".split(",");
+        assertThat(result_2).containsExactly("1", "2");
+        assertThat(result_1).containsExactly("2", "1");
+    }
+
+    @Test
+    public void substring() {
+        String str = "(1,2)";
+        String result = str.substring(1,str.length()-1);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    public void charAt() {
+        assertThatThrownBy(() -> {
+            String str = "0123456";
+            Character result = str.charAt(11);
+            assertThat(result).isEqualTo('1');
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("Index: 11, Size: 7");
+
+        assertThatThrownBy(() -> {
+            String str = "0123456";
+            Character result = str.charAt(1);
+            assertThat(result).isEqualTo('1');
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("Index: 1, Size: 7");
+    }
+}
