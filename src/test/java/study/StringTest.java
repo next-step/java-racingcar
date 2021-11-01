d716a74c45b2367faaad26d91040f9cb345134e0
ydh6226 <ydh6226@naver.com>
2021-11-01 20:36:37 +0900

    String 학습테스트 - 요구사항 2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,7 +1,5 @@
 package study;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,5 +30,19 @@ public class StringTest {
 
         //then
         assertThat(splitResults).containsExactly("1");
+    }
+
+    @Test
+    void subStringTest() throws Exception {
+        //given
+        String inputString = "(1,2)";
+        int bodyStartIdx = 1;
+        int bodyEndIdx = inputString.length() - 1;
+
+        //when
+        String subStringResult = inputString.substring(bodyStartIdx, bodyEndIdx);
+
+        //then
+        assertThat(subStringResult).isEqualTo("1,2");
     }
 }
