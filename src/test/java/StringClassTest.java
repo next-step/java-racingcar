4c30b029c0a996aec2aeb7e67f5b033cc03d7f80
doorisopen <doorwasopen@gmail.com>
2021-11-01 21:57:23 +0900

    test: String 클래스에 대한 학습 테스트 / 요구사항 3

--- src/test/java/StringClassTest.java
+++ src/test/java/StringClassTest.java
@@ -1,3 +1,5 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,6 +11,7 @@ public class StringClassTest {
     private static final String COMMA = ",";
     private static final String STRING_ONE = "1";
     private static final String STRING_TWO = "2";
+    private static final String LENGTH_3_ALPHABET_SEQUENCE = "abc";
 
     @Test
     void 문자열_1_콤마_2를_콤마로_split_하면_1과_2를_포함한다() {
@@ -38,6 +41,26 @@ public class StringClassTest {
         String subStringNumbers = numberPair.substring(1, numberPair.length() - 1);
         //then
         assertThat(subStringNumbers).isEqualTo(numbers);
+    }
+
+    @DisplayName("길이가 3인 문자열 abc 의 첫 번째 문자는 a이다")
+    @Test
+    void 길이가_3인_문자열_abc_의_첫_번째_문자는_a_이다() {
+        //given
+        //when
+        char firstCharacter = LENGTH_3_ALPHABET_SEQUENCE.charAt(0);
+        //then
+        assertThat(firstCharacter).isEqualTo('a');
+    }
+
+    @DisplayName("길이가 3인 문자열 abc 의 네 번째 문자를 가져오면 StringIndexOutOfBoundsException이 발생한다")
+    @Test
+    void 길이가_3인_문자열_abc_의_네_번째_문자를_가져오면_StringIndexOutOfBoundsException이_발생한다() {
+        //given
+        //when & then
+        Assertions.assertThatThrownBy(() -> {
+            LENGTH_3_ALPHABET_SEQUENCE.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 
 }
