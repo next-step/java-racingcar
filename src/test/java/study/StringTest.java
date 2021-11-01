e086329fe3ab82802f530b38f908ae690e45ebf2
aiden.sim <aiden.sim@kakaocorp.com>
2021-11-01 12:50:17 +0900

    [refactor] StringTest 요구사항1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,20 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+    private static final String DELIMITER = ",";
+
     @Test
+    @DisplayName("1,2을 ,로 split 했을 때 1과 2로 잘 분리되는지 검증")
     void split() {
-        String[] result = "1,2".split(",");
+        String[] result = "1,2".split(DELIMITER);
+
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
         assertThat(result).containsExactly("1", "2");
     }
 }
