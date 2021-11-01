0a322734a7efe06579b2cb479f80b3267da1c864
kdhyo <kdhyo98@gmail.com>
2021-11-01 17:08:07 +0900

    test : StringTest 요구사항2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
-
     @Test
     void split() {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)";
+        result = result.substring(1, result.length() - 1);
+        assertThat(result).isEqualTo("1,2");
     }
 }
