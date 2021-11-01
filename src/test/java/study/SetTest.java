aa66d889c1dc3008e38eaaf9c20f012e4d6b0e3d
JaeHan-Kim <hgal8877@gmail.com>
2021-11-01 23:51:44 +0900

    test(study) : SetTest 요구사항2 - 값을 포함하고 있는지 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -26,5 +28,12 @@ public class SetTest {
     @Test
     void setSizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("값을 포함하고 있는지 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void containsTest(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
