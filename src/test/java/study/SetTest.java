40d4a6191b5f067efa1dbf5353b5c3fcd52e83ee
aiden.sim <aiden.sim@kakaocorp.com>
2021-11-01 13:11:31 +0900

    [test] SetTest 요구사항2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,12 @@ public class SetTest {
     @DisplayName("Set의 size() 메소드를 활용해서 Set의 크기를 검증")
     void size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("JUnit의 ParameterizedTest를 활용해 중복 코드 제거")
+    void contains(Integer input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
