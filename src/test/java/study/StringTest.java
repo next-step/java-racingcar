f0b3440d1eecec6cb630da40683aa7ecc6618f1b
kero <jdy410@naver.com>
2021-11-02 01:04:00 +0900

    Set Collection - Set의 크기 확인

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,12 +1,31 @@
 package study;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
 
     @Test
     void split() {
@@ -35,5 +54,10 @@ public class StringTest {
         }).isInstanceOf(StringIndexOutOfBoundsException.class)
         .hasMessageContaining("4");
     }
-
+    
+    @Test
+    @DisplayName("Set 크기 확인")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
 }
