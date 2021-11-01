2c897ad187c763aced6ab194c7872c42f9b5ec20
doorisopen <doorwasopen@gmail.com>
2021-11-01 22:09:24 +0900

    test: Set Collection에 대한 학습 테스트 / 요구사항 1, 요구사항 2

--- src/test/java/SetCollectionTest.java
+++ src/test/java/SetCollectionTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -26,6 +28,40 @@ public class SetCollectionTest {
         int numbersSize = numbers.size();
         //then
         assertThat(numbersSize).isGreaterThan(0);
+    }
+
+    @Test
+    void numbers_의_크기는_3이다() {
+        //given
+        //when
+        int numbersSize = numbers.size();
+        //then
+        assertThat(numbersSize).isEqualTo(3);
+    }
+
+    @Test
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void numbers_에_정수_1_2_3이_존재한다(int number) {
+        //given
+        //when
+        //then
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6})
+    void numbers_에_정수_4_5_6이_존재하지_않는다(int number) {
+        //given
+        //when
+        //then
+        assertThat(numbers.contains(number)).isFalse();
     }
 
 }
