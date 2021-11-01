5a280c13f075c3070c941515dd8afd477678d056
ChangmoKang <dkssudckdah@gmail.com>
2021-11-01 11:03:47 +0900

    Set contains test w/CsvSource

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,13 +3,13 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -29,8 +29,8 @@ public class SetTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5})
-    void containsTest(int target) {
-        assertTrue(numbers.contains(target));
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsTest(int target, boolean expected) {
+        assertEquals(numbers.contains(target), expected);
     }
 }
