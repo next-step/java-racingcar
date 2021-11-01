9ffd612461aceeafe0506c7e49676274dffa7720
marty.ni <marty.ni@kakaocorp.com>
2021-11-01 22:07:15 +0900

    2021-11-01 String class test step2 - substring

--- src/test/java/step1/StringClassTest.java
+++ src/test/java/step1/StringClassTest.java
@@ -1,3 +1,5 @@
+package step1;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,11 +9,19 @@ public class StringClassTest {
 
     @Test
     @DisplayName("step 1 - split test")
-    void step1() {
+    void splitTest() {
         String str = "1,2";
         String[] result = str.split(",");
         assertThat(result).contains("1", "2");
 
         assertThat("1".split(",")).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("step 2 - substring test")
+    void substringTest() {
+        String str = "(1,2)";
+        String result = str.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
     }
 }
