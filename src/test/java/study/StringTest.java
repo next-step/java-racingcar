aa9f87f2534ecdc3eda45fa8a49c73a9cd4e4531
jiseok-choi <choi300300@gmail.com>
2021-11-03 00:53:02 +0900

    String class 요구사항 1 누락사항 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,25 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+
     @Test
+    @DisplayName("split-1and2")
     void split() {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("split-1")
+    void splitOne() {
+        String[] array = new String[] {"1"};
+        String[] result = "1".split(",");
+        assertThat(array[0]).isEqualTo(result[0]);
     }
 }
