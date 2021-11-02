b708a959fa4a3701f490b91339ca3d9a8a80997c
Han <hankim@tapasmedia.co>
2021-11-02 21:57:23 +0900

    Add calculate()

--- src/main/java/calculator/TextCalculator.java
+++ src/main/java/calculator/TextCalculator.java
@@ -36,14 +36,68 @@ public class TextCalculator {
         }
     }
 
+    private List<Integer> numbers = new ArrayList<>();
+    private Operator operator;
+    private int size;
+
     public int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
         String[] strings = input.split(" ");
+        this.size = strings.length;
+
+
         int result = 0;
-        for (int i = 0; i < strings.length; i++) {
+        for (int i = 0; i < size; i++) {
+            String s = strings[i];
+
+            if (s == null || s.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+
             if (i % 2 == 0) {
-                result += Integer.parseInt(strings[i]);
+                numbers.add(Integer.parseInt(strings[i]));
+            } else {
+                operator = Operator.getOperator(strings[i]);
+            }
+
+            if (numbers.size() == 2 && operator != null) {
+                result = calculate(numbers, operator);
+                numbers.clear();
+                numbers.add(result);
+                this.operator = null;
             }
         }
+        return result;
+    }
+
+    private Integer calculate(List<Integer> numbers, Operator operator) {
+        if (numbers == null || numbers.size() != 2 || operator == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Integer result = null;
+        Integer a = numbers.get(0);
+        Integer b = numbers.get(1);
+
+        switch (operator) {
+
+            case PLUS:
+                result = Math.addExact(a, b);
+                break;
+            case MINUS:
+                result = Math.subtractExact(a ,b);
+                break;
+            case MULTIPLICATION:
+                result = Math.multiplyExact(a, b);
+                break;
+            case DIVISION:
+                result = Math.floorDiv(a, b);
+                break;
+        }
+
         return result;
     }
 }
