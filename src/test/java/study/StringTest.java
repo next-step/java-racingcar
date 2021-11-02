5014081ff06c35d3aeca108fc024a3ae1e215348
kero1004 <45447832+kero1004@users.noreply.github.com>
2021-11-02 23:29:28 +0900

    1단계 - 학습 테스트 실습 (#2690)

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,78 @@
+package study;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substring() {
+        String str = "(1,2)";
+        String result = str.substring(1, 4);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 가져오기")
+    void charAt() {
+        String str = "abc";
+
+        assertThat(str.charAt(0)).isEqualTo('a');
+
+        assertThatThrownBy(() -> {
+            str.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+        .hasMessageContaining("4");
+    }
+    
+    @Test
+    @DisplayName("Set 크기 확인")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("값이 존재하는지 확인")
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value= {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("입력 값에 따라 결과 값이 다른 경우")
+    void containsFalse(int input, boolean expected) {
+        Assertions.assertEquals(numbers.contains(input), expected);
+    }
+}
