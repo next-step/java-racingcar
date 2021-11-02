2f84754c31b880cb6376485b22a8a6d8b7dc309c
sid <sid.seo@kakaocorp.com>
2021-11-02 09:51:27 +0900

    [자동차 경주 - 1단계] 학습 테스트 실습  - Set Collection test의 testContains에 negative cave 추가

--- src/test/java/edu/nextstep/camp/SetTest.java
+++ src/test/java/edu/nextstep/camp/SetTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,9 +34,13 @@ public class SetTest {
     // Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
     // 구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
     // JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
-    @ParameterizedTest(name = "check element({arguments}) contains in set")
-    @ValueSource(ints = {1, 2, 3})
-    public void testContains(int n) {
-        assertThat(numbers.contains(n)).isTrue();
+    // 요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
+    // 예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.
+    @ParameterizedTest(name = "check element contains in set: {arguments}")
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    public void testContains(String numberParam, String expectedParam) {
+        int n =  Integer.parseInt(numberParam);
+        boolean expected = Boolean.parseBoolean(expectedParam);
+        assertThat(numbers.contains(n)).isEqualTo(expected);
     }
 }
