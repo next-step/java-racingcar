6f63d36c685c4cf739cdee913b62f0af2b8243f6
marty.ni <marty.ni@kakaocorp.com>
2021-11-02 22:47:57 +0900

    Feat : ResultDto 구현
      - from(Long count, Long time) 정적 팩토리 메서드 구현
      - getCount() 구현
      - getTime() 구현

--- src/main/java/step3/RuleDto.java
+++ src/main/java/step3/RuleDto.java
@@ -5,9 +5,14 @@ public class RuleDto {
     private final Long count;
     private final Long time;
 
-    public RuleDto(Long count, Long time) {
+    private RuleDto(Long count, Long time) {
         this.count = count;
         this.time = time;
+    }
+
+    public static RuleDto from(Long count, Long time) {
+
+        return new RuleDto(count, time);
     }
 
     public Long getCount() {
--- src/test/java/step3/RuleDtoTest.java
+++ src/test/java/step3/RuleDtoTest.java
@@ -1,9 +1,41 @@
 package step3;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RuleDtoTest {
 
+    @DisplayName("getCount 는 RuleDto의 count를 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"4:3:4", "5:3:5"}, delimiter = ':')
+    void getCountTest(Long count, Long time, Long expectCount) {
+        RuleDto ruleDto = RuleDto.from(count, time);
+
+        assertThat(ruleDto.getCount()).isEqualTo(expectCount);
+    }
+
+    @DisplayName("getTime 는 RuleDto의 time을 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"4:3:3", "5:6:6"}, delimiter = ':')
+    void getTimeTest(Long count, Long time, Long expectTime) {
+        RuleDto ruleDto = RuleDto.from(count, time);
+
+        assertThat(ruleDto.getTime()).isEqualTo(expectTime);
+    }
+
+    @DisplayName("from() 메서드는 정상적으로 RuleDto를 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"4:3:4:3", "5:6:5:6"}, delimiter = ':')
+    void getTimeTest(Long count, Long time, Long expectCount, Long expectTime) {
+        RuleDto ruleDto = RuleDto.from(count, time);
+
+        assertThat(ruleDto.getCount()).isEqualTo(expectCount);
+        assertThat(ruleDto.getTime()).isEqualTo(expectTime);
+    }
 
 
 }
