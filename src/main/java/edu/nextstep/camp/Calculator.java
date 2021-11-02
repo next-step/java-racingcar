d9644f27d7a87f5fb661120a49aad164a7638865
sid <sid.seo@kakaocorp.com>
2021-11-02 23:54:24 +0900

    [2단계 - 문자열 계산기] 문자열 사칙 연산 계산기 구현  - 간단한 사칙 연산 구현

---
+++ src/main/java/edu/nextstep/camp/Calculator.java
@@ -0,0 +1,19 @@
+package edu.nextstep.camp;
+
+public class Calculator {
+    public int add(int first, int second) {
+        return first + second;
+    }
+
+    public int subtract(int first, int second) {
+        return first - second;
+    }
+
+    public int product(int first, int second) {
+        return first * second;
+    }
+
+    public int divide(int first, int second) {
+        return first / second;
+    }
+}
---
+++ src/test/java/edu/nextstep/camp/CalculatorTest.java
@@ -0,0 +1,47 @@
+package edu.nextstep.camp;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+    final Calculator cal = new Calculator();
+    final Random random = new Random();
+
+    @Test
+    public void testAdd() {
+        assertThat(cal.add(3, 5)).isEqualTo(3 + 5);
+        int m = random.nextInt();
+        int n = random.nextInt();
+        assertThat(cal.add(m, n)).isEqualTo(m + n);
+    }
+
+    @Test
+    public void testSubtract() {
+        assertThat(cal.subtract(8, 5)).isEqualTo(8 - 5);
+        int m = random.nextInt();
+        int n = random.nextInt();
+        assertThat(cal.subtract(m, n)).isEqualTo(m - n);
+    }
+
+    @Test
+    public void testProduct() {
+        assertThat(cal.product(3, 5)).isEqualTo(3 * 5);
+        int m = random.nextInt();
+        int n = random.nextInt();
+        assertThat(cal.product(m, n)).isEqualTo(m * n);
+    }
+
+    @Test
+    public void testDivide() {
+        assertThat(cal.divide(8, 5)).isEqualTo(8 / 5);
+        int m = random.nextInt();
+        int n = random.nextInt();
+        assertThat(cal.divide(m, n)).isEqualTo(m / n);
+    }
+}
