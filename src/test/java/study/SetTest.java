105b33acb70f4db7251c3428542a78fc2cff5035
soosue <54942017+soosue@users.noreply.github.com>
2021-11-02 10:40:19 +0900

    🚀 1단계 - 학습 테스트 실습 (#2669)
    * study test : 요구사항1
    
    * study test : 요구사항2
    
    * study test : 요구사항3
    
    * study test : Set Collection 요구사항1
    
    * study test : Set Collection 요구사항2
    
    * study test : Set Collection 요구사항3

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드 테스트")
+    void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set의 contains() 메소드 테스트 (true 결과)")
+    void containsTest1(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("Set의 contains() 메소드 테스트 (false 결과 포함)")
+    void containsTest2(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,58 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("\"1,2\" split 테스트")
+    void splitTest1() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("\"1\" split 테스트")
+    void splitTest2() {
+        String[] result = "1".split(",");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    @DisplayName("\"(1,2)\"에서 substring() 메소드로 ()를 제거하는 테스트")
+    void substringTest() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() 메소드 테스트")
+    void charAtTest1() {
+        String string = "abc";
+        assertThat(string.charAt(0)).isEqualTo('a');
+        assertThat(string.charAt(1)).isEqualTo('b');
+        assertThat(string.charAt(2)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("charAt() 메소드 StringIndexOutOfBoundsException 테스트")
+    void charAtTest2() {
+        String string = "abc";
+        List<Integer> indexes = new ArrayList<>();
+        indexes.add(-1);
+        indexes.add(string.length());
+
+        for (Integer index : indexes) {
+            assertThatThrownBy(() -> string.charAt(index))
+                    .isInstanceOf(StringIndexOutOfBoundsException.class)
+                    .hasMessage("String index out of range: " + index);
+        }
+    }
+}
