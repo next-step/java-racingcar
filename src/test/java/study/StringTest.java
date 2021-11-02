98c721293463ca617507ca0852268b31c302acb1
hyenny <qlwb9191@naver.com>
2021-11-02 23:50:35 +0900

    feat: String charAt 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
 
@@ -19,13 +21,23 @@ public class StringTest {
     }
 
     @Test
-    void substring(){
+    void substring() {
         String data = "(1,2)";
         String result = data.substring(1, data.length() - 1);
 
         assertThat(result).isEqualTo("1,2");
     }
 
-    
+    @DisplayName("특정 위치의 문자가 문자열 길이를 벗어나면 예외")
+    @Test
+    void charAt() {
+        String data = "abc";
+
+        assertThat(data.charAt(0)).isEqualTo('a');
+        assertThatThrownBy(() -> {
+            data.charAt(10);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+            .hasMessageContaining("index out of range: 10");
+    }
 
 }
