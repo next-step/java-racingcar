bd283fa9ce8117c2c6d4fc3c368ef12786d7d5a7
sojeongw <sojeongw.official@gmail.com>
2021-11-02 22:54:03 +0900

    step-1: 학습 테스트 실습

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,48 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 메소드를 활용해 numbers의 크기를 확인한다")
+    void sizeShouldReturnSizeOfNumbers() {
+        int size = numbers.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains() 메소드를 활용해 numbers에 1, 2, 3이 존재하는지 ParameterizedTest로 확인한다")
+    void containsShouldReturnTrueForAddedValues(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("contains() 메소드를 활용해 numbers에 1, 2, 3이 존재하면 true, 아니면 false를 반환한다")
+    void containsShouldReturnTrueForAddedValuesAndReturnFalseForAnythingElse(int value, boolean expected) {
+        assertThat(numbers.contains(value)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,55 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("문자열 1,2를 split했을 때 1과 2로 잘 분리되는지 확인한다")
+    void splitShouldReturnMultipleValues() {
+        String givenData = "1,2";
+        String[] splitData = givenData.split(",");
+
+        assertThat(splitData).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("문자열 1을 split했을 때 1만을 포함하는 배열이 반환되는지 확인한다")
+    void splitShouldReturnSingleValue() {
+        String givenData = "1";
+        String[] splitData = givenData.split(",");
+
+        assertThat(splitData).contains("1");
+    }
+
+    @Test
+    @DisplayName("문자열이 주어졌을 때 substring()을 활용해 괄호를 제거한다")
+    void subStringShouldReturnStringWithoutParentheses() {
+        String givenData = "(1,2)";
+        String result = givenData.substring(1, 4);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열이 주어졌을 때 charAt() 메소드를 활용해 특정 위치의 문자를 가져온다")
+    void charAtShouldReturnTheValueOfIndexZero() {
+        String givenData = "abc";
+        char result = givenData.charAt(0);
+
+        assertThat(result).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생한다")
+    void charAtShouldThrowStringIndexOutOfBoundsException() {
+        assertThatThrownBy(() -> {
+            String givenData = "abc";
+            givenData.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+}
