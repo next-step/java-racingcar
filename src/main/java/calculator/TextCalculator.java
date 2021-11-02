83804412e2f13afaf56b34ba4df1477a21ef0cff
Han <hankim@tapasmedia.co>
2021-11-02 21:03:42 +0900

    Add Operator

--- src/main/java/calculator/TextCalculator.java
+++ src/main/java/calculator/TextCalculator.java
@@ -1,9 +1,41 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author han
  */
 public class TextCalculator {
+
+    enum Operator {
+        PLUS("+"),
+        MINUS("-"),
+        MULTIPLICATION("*"),
+        DIVISION("/");
+
+        private String text;
+
+        Operator(String text) {
+            this.text = text;
+        }
+
+        public static Operator getOperator(String input) {
+            if (input == null || input.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+
+            Operator[] values = Operator.values();
+
+            for (Operator o : values) {
+                if (o.text.equals(input)) {
+                    return o;
+                }
+            }
+
+            throw new IllegalArgumentException();
+        }
+    }
 
     public int calculate(String input) {
         String[] strings = input.split(" ");
