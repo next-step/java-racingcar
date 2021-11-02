27e0f35ca093c089279cd8274f4c25083f1a8842
ChangmoKang <dkssudckdah@gmail.com>
2021-11-02 15:33:21 +0900

    자동차 경ìì£ - 학습 테스트 실습 습 피드백

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,17 +1,22 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
+    private static final char DELIMITER = ':';
+
     private Set<Integer> numbers;
 
     @BeforeEach
@@ -24,13 +29,28 @@ public class SetTest {
     }
 
     @Test
+    @DisplayName("요구사항1 : Set의 size() 메소드를 활용해 Set의 크기를 확인한다.")
     void sizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    void containsTest(int target, boolean expected) {
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("요구사항2 : contains() 를 활용하여 1,2,3 값이 있는지 확인한다.")
+    void containsTest(int target) {
+        assertTrue(numbers.contains(target));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1:true",
+            "2:true",
+            "3:true",
+            "4:false",
+            "5:false",
+    }, delimiter = DELIMITER)
+    @DisplayName("요구사항3 : 입력 값에 따라 결과 값이 다른 경우의 테스트")
+    void strictContainsTest(int target, boolean expected) {
         assertEquals(numbers.contains(target), expected);
     }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,29 +7,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
-    @Test
-    void splitTest() {
-        String[] result;
-        result = "1,2".split(",");
-        assertThat(result).containsExactly("1", "2");
+    private static final int PADDING = 1;
+    private static final String COMMA = ",";
 
-        result = "1".split(",");
+    @Test
+    @DisplayName("요구사항1 : ,(comma) 로 구분되어 있는 값을 split 했을 때 comma 의 개수 + 1 개의 원소로 분리되어야 한다.")
+    void splitTest1() {
+        String[] result = "1,2".split(COMMA);
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("요구사항1 : ,(comma) 로 구분되어 있지 않은 값을 split 했을 때 단일 원소만 반환해야한다.")
+    void splitTest2() {
+        String[] result = "1".split(COMMA);
         assertThat(result).containsExactly("1");
     }
 
     @Test
+    @DisplayName("요구사항2 : 문자열 양 끝의 괄호를 제거한다.")
     void substringTest() {
         String input = "(1,2)";
-        String result = input.substring(1, input.length() - 1);
+        String result = input.substring(PADDING, input.length() - PADDING);
         assertThat(result).isEqualTo("1,2");
     }
 
     @Test
-    @DisplayName("charAt OutOfBoundsException Test")
+    @DisplayName("요구사항3 : charAt() 을 사용할 때 문자열 범위를 넘어서면 예외가 발생한다.")
     void charAtExceptionTest() {
         String input = "abc";
-        assertThatThrownBy(() -> {
-            input.charAt(input.length());
-        }).isInstanceOf(IndexOutOfBoundsException.class);
+        assertThatThrownBy(() -> input.charAt(input.length()))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: " + input.length());
     }
 }
