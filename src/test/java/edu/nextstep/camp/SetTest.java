6e0b8bdd9948faf0cb753277c455d9bf81a93a28
sid <sid.seo@kakaocorp.com>
2021-11-02 09:44:38 +0900

    [자동차 경주 - 1단계] 학습 테스트 실습  - Set Collection test에 contains를 확인하는 테스트 추가

--- src/test/java/edu/nextstep/camp/SetTest.java
+++ src/test/java/edu/nextstep/camp/SetTest.java
@@ -6,6 +6,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,5 +29,14 @@ public class SetTest {
     @DisplayName("size test")
     public void testSize() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    // Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
+    // 구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
+    // JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
+    @ParameterizedTest(name = "check element({arguments}) contains in set")
+    @ValueSource(ints = {1, 2, 3})
+    public void testContains(int n) {
+        assertThat(numbers.contains(n)).isTrue();
     }
 }
