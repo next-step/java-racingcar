5308aed51b42195c63137b02672179d02209488f
pollra <5346003-pollra32@users.noreply.gitlab.com>
2021-11-02 23:43:20 +0900

    Set Collection 학습 테스트
    * 요구사항 3 구현

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -3,12 +3,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Set Collection 학습 테스트")
 public class SetTest {
@@ -35,5 +37,12 @@ public class SetTest {
     @ValueSource(ints={1, 2, 3})
     public void whetherTheValueExists(int increasingNumber) {
         assertTrue(numbers.contains(increasingNumber));
+    }
+
+    @ParameterizedTest(name="Set 에 다음의 값은 존재 -> {arguments} <- 값은 없을 때 정상 수행")
+    @CsvSource(value={"1,4", "2,5", "3, "})
+    public void checkForNonExistentValues(int withNormalCase, Integer withErrorCase) {
+        assertTrue(numbers.contains(withNormalCase));
+        assertFalse(numbers.contains(withErrorCase));
     }
 }
