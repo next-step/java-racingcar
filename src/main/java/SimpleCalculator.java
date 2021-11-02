d9c10b97736c6ad35effaa4d1dd9a2c10a018e7e
HyeongJunMin <alsgudwns007@naver.com>
2021-11-02 23:42:58 +0900

    자동차경주 2단계 - 문자열 계산기

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,45 @@
+public class SimpleCalculator {
+
+  private static final String DELIMITER = " ";
+  private static final String ADDITION = "+";
+  private static final String SUBTRACTION = "-";
+  private static final String MULTIPLICATION = "*";
+  private static final String DIVISION = "/";
+
+  private String formula;
+
+  public SimpleCalculator(String formula) {
+    checkFormulaIsBlank(formula);
+    this.formula = formula;
+  }
+
+  private void checkFormulaIsBlank(String formula) {
+    if (Strings.isBlank(formula)) {
+      throw new IllegalArgumentException(String.format("formula is blank. formula : %s", formula));
+    }
+  }
+
+  public int calculate() {
+    String[] elements = formula.split(DELIMITER);
+    int result = Integer.parseInt(elements[0]);
+
+    for (int i = 1; i < elements.length; i = i + 2) {
+      String operator = elements[i];
+      int operand = Integer.parseInt(elements[i + 1]);
+      result = calculate(result, operator, operand);
+    }
+
+    return result;
+  }
+
+  private int calculate(int firstNumber, String operator, int secondNumber) {
+    switch (operator) {
+      case ADDITION : return firstNumber + secondNumber;
+      case SUBTRACTION : return firstNumber - secondNumber;
+      case MULTIPLICATION : return firstNumber * secondNumber;
+      case DIVISION : return firstNumber / secondNumber;
+      default : throw new IllegalArgumentException(String.format("invalid operator. operator : %s", operator));
+    }
+  }
+
+}
---
+++ src/main/java/Strings.java
@@ -0,0 +1,7 @@
+public class Strings {
+
+  public static boolean isBlank(String input) {
+    return input == null || input.trim().isEmpty();
+  }
+
+}
