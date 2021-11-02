1207565c800c272fd12523a67980ac71543f7b96
pollra <5346003-pollra32@users.noreply.gitlab.com>
2021-11-02 23:31:28 +0900

    Set Collection 학습 테스트
    * 요구사항 2 구현

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -2,9 +2,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Set Collection 학습 테스트")
 public class SetTest {
@@ -25,5 +29,11 @@ public class SetTest {
     public void sizeIsNormalValue() {
         int numbersSize = numbers.size();
         Assertions.assertThat(numbersSize).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name="Set 에 {index} 데이터가 존재하면 정상 수행")
+    @ValueSource(ints={1, 2, 3})
+    public void whetherTheValueExists(int increasingNumber) {
+        assertTrue(numbers.contains(increasingNumber));
     }
 }
