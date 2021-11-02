cd92270da925437e0d17839e7ae8c79f52f7ca64
DongHyo <kdhyo98@gmail.com>
2021-11-03 01:58:24 +0900

    feat : 예외처리 추가

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -5,16 +5,39 @@ import java.util.zip.DataFormatException;
 public class Calculator {
 
     public Integer calculate(String input) throws DataFormatException {
-        String[] arr = input.split(" ");
-        // TODO arr[0]이 숫자가 아닐 경우
+        String[] arr = getSpaceSplitAndValidation(input);
         Integer result = Integer.parseInt(arr[0]);
 
         for(int i = 1; i < arr.length; i = i + 2) {
-            // TODO arr[i+1]이 숫자가 아니거나 없는 경우
-            result = EOperation.getValue(arr[i]).calculate(result, Integer.parseInt(arr[i+1]));
+            try {
+                result = EOperation.getValue(arr[i]).calculate(result, Integer.parseInt(arr[i + 1]));
+            } catch (DataFormatException e) {
+                throw new DataFormatException(e.getMessage() + " : " + arr[i]);
+            }
         }
 
         return result;
+    }
+
+    private void validationParam(String input) {
+        if(input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("입력 값이 존재하지 않습니다.");
+        }
+    }
+
+    private String[] getSpaceSplitAndValidation(String input) {
+        this.validationParam(input);
+        String[] result = input.split(" ");
+
+        if(!this.isOdd(result.length)) {
+            throw new IllegalArgumentException("수식이 올바르지 않습니다.");
+        }
+
+        return result;
+    }
+
+    private Boolean isOdd(Integer number) {
+        return number % 2 == 1;
     }
 
 }
--- src/main/java/Calculator/CalculatorMain.java
+++ src/main/java/Calculator/CalculatorMain.java
@@ -5,14 +5,21 @@ import java.util.zip.DataFormatException;
 
 public class CalculatorMain {
 
-    public static void main(String[] args) throws DataFormatException {
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        Integer result;
         Scanner sc = new Scanner(System.in);
         System.out.println("계산하고 싶은 수식을 입력해주세요.");
         System.out.println("ex) 2 + 3 * 4 / 2");
         System.out.print("입력 : ");
 
-        Integer result = calculator.calculate(sc.nextLine());
-        System.out.println("결과 : " + result);
+        try {
+            result = calculator.calculate(sc.nextLine());
+            System.out.println("결과 : " + result);
+        } catch (NumberFormatException e) {
+            System.out.println("숫자 위치에 다른 문자가 존재합니다. \n" + e.getMessage());
+        } catch (IllegalArgumentException | DataFormatException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
