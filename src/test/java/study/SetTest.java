d330ea87464e7936d2223bdfb7876782099b47e9
hyenny <qlwb9191@naver.com>
2021-11-03 00:38:39 +0900

    feat: Set contains 입력값에 따라 존재여부 확인 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,5 +35,12 @@ public class SetTest {
     @ValueSource(ints = {1,2,3})
     void contains(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @DisplayName("Set에 입력값 존재여부 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_입력값_존재여부(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
