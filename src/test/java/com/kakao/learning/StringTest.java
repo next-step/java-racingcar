b3a6dfd5229bd7b061b9805b38f97bb8aa285f5c
jyami-kim <mor2222@naver.com>
2021-11-02 20:23:38 +0900

    step1: String 학습 테스트

---
+++ src/test/java/com/kakao/learning/StringTest.java
@@ -0,0 +1,71 @@
+package com.kakao.learning;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class StringTest {
+
+    private static final int SUBSTRING_INDEX = 1;
+
+    @Test
+    @DisplayName("split 테스트")
+    void splitTest() {
+        //given
+        String input = "1,2";
+        //when
+        String[] result = input.split(",");
+        //then
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("substring 테스트")
+    void subStringTest() {
+        //given
+        String input = "(1,2)";
+        //when
+        String result = input.substring(SUBSTRING_INDEX, input.length() - SUBSTRING_INDEX);
+        //then
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @ParameterizedTest
+    @DisplayName("charAt 테스트")
+    @MethodSource("indexCharAtTestParameters")
+    void indexCharAtTest(int index, char expected) {
+        //given
+        String input = "abc";
+        //when
+        char actual = input.charAt(index);
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> indexCharAtTestParameters() {
+        return Stream.of(
+                Arguments.arguments(0, 'a'),
+                Arguments.arguments(1, 'b'),
+                Arguments.arguments(2, 'c')
+        );
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException 테스트")
+    void StringIndexOutOfBoundsExceptionTest() {
+        //given
+        String input = "abc";
+        //when
+        //then
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> input.charAt(3)).withMessageMatching("String index out of range: \\d+");
+    }
+}
