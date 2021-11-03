4ef8052fe556d0f6c23e937d06f969325b96b59d
giyeon95 <giyeon15@gmail.com>
2021-11-03 15:15:11 +0900

    style: #1 불필요 import 제거

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,8 +1,6 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
