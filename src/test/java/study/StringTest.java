9a13a21580889130ac36deccf818094e2433c044
haeuniii <qnfnqnfn9825@gmail.com>
2021-11-03 15:53:52 +0900

    [UPD] substring method 관련 학습 테스트 기능 추가
    substring method 관련 학습 테스트 기능 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -14,4 +14,12 @@ public class StringTest {
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1","2");
     }
+
+    @Test
+    @DisplayName("substring method test")
+    void substring() {
+        String str = "(1,2)";
+        String result = str.substring(1, str.length() - 1);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
