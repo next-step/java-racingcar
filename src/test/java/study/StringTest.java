58892d3cedbd92e0610ad4bff2cdb5ef23afe0c1
Lee Dongkyu <nelldklee@gmail.com>
2021-11-03 16:08:32 +0900

    test(StringTest): 문자열 subString, charAt 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,33 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
 
     @Test
     void split() {
         String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void splitExcludeParentheses() {
+        String str = "(1,2)";
+        String result = str.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자열 가져올 때, 특정 에러 확인")
+    void charAt() {
+        String str = "abc";
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class).isThrownBy(() -> {
+            str.charAt(5);
+        }).withMessageMatching("String index out of range: \\d+");
     }
 }
