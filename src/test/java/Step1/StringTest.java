ef62e1c5fa24db567ab692bd2a56ab9dac7c1075
Sangin-C <sangin3435@naver.com>
2021-11-03 17:06:42 +0900

    StringTest - 요구사항2

--- src/test/java/Step1/StringTest.java
+++ src/test/java/Step1/StringTest.java
@@ -12,4 +12,10 @@ public class StringTest {
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    void 요구사항2() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
