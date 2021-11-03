eca6425c89be1e79e186dfd07b9c9821b179c487
정명훈 <audgnssweet@naver.com>
2021-11-04 08:57:06 +0900

    initial commit (#2723)
    * StringTest 클래스 생성
    
    * StringTest 작성 완료
    
    * SetTest 메서드 작성
    
    * SetTest 작성 완료

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void sizeTest() {
+        assertEquals(numbers.size(), 3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void containsTest(int input) {
+        assertTrue(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1=true", "2=true", "3=true", "4=false", "5=false"}, delimiter = '=')
+    void dynamicContainsTest(int input, boolean expected) {
+        assertEquals(numbers.contains(input), expected);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,29 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    void splitTest() {
+        assertThat("1,2".split(",")).contains("1", "2");
+        assertThat("1,2".split(",")).containsExactly("1", "2");
+    }
+
+    @Test
+    void subStringTest() {
+        assertEquals("(1,2)".substring(1, 4), "1,2");
+    }
+
+    @DisplayName("String-charAt : 특정 문자 가져오기, IndexOutOfBounds 테스트")
+    @Test
+    void charAtTest() {
+        assertEquals("abc".charAt(0), 'a');
+        assertEquals("abc".charAt(1), 'b');
+        assertEquals("abc".charAt(2), 'c');
+        assertThatThrownBy(() -> "abc".charAt(3)).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+}
