638c72c434ed92e3ba231fdc796edc36aa722f2b
audgnssweet <audgnssweet@naver.com>
2021-11-04 00:00:25 +0900

    SetTest 메서드 작성

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void sizeTest() {
+
+    }
+
+    @Test
+    void containsTest() {
+
+    }
+
+    @Test
+    void dynamicContainsTest() {
+        
+    }
+}
