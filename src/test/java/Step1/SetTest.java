0e8039d084c9f8f81fd364ac868ed457ca75ab07
Sangin-C <sangin3435@naver.com>
2021-11-03 17:36:02 +0900

    SetTest - 요구사항1

---
+++ src/test/java/Step1/SetTest.java
@@ -0,0 +1,37 @@
+package Step1;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+
+    @Test
+    @DisplayName("Set의 크기를 확인한다.")
+    void 요구사항1() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+}
