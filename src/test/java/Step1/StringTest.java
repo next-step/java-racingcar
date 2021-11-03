55479f959fce61c1f805374cc09fac6124f29d82
Sangin-C <sangin3435@naver.com>
2021-11-03 17:07:30 +0900

    StringTest - 요구사항3

--- src/test/java/Step1/StringTest.java
+++ src/test/java/Step1/StringTest.java
@@ -1,8 +1,9 @@
 package Step1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
@@ -17,5 +18,23 @@ public class StringTest {
     void 요구사항2() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() 메서드를 사용해 특정 위치문자를 가져온다. 위치 값을 벗어나면 StringIndexOutOfBoundsException를 발생한다.")
+    void 요구사항3() {
+        String result = "abc";
+        char a = result.charAt(0);
+        assertThat(a).isEqualTo('a');
+
+        assertThatThrownBy(() -> {
+            result.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    result.charAt(10);
+                }).withMessageMatching("String index out of range: 10");
     }
 }
