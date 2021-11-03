9586517b0ac8d19a3cd9d5c910bf34e9b6aa6de2
sid <sid.seo@kakaocorp.com>
2021-11-03 23:17:03 +0900

    [2단계 - 문자열 계산기] 문자열 사칙 연산 계산기 구현  - create number class

---
+++ src/test/java/edu/nextstep/camp/NumberTest.java
@@ -0,0 +1,63 @@
+package edu.nextstep.camp;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class NumberTest {
+    final Random random = new Random();
+
+    @Test
+    public void create() {
+        final Number n = Number.of(5);
+        assertThat(n.equals(Number.of(5))).isTrue();
+        assertThat(n.equals(Number.of("5"))).isTrue();
+        assertThat(n.toInt()).isEqualTo(5);
+        assertThat(n.equals(5)).isTrue();
+        assertThat(n.equals("5")).isTrue();
+    }
+
+    @Test
+    public void createFailed() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Number.of(null));
+        assertThatIllegalArgumentException().isThrownBy(() -> Number.of(""));
+        assertThatIllegalArgumentException().isThrownBy(() -> Number.of("null")); // not-a-number test
+    }
+
+    @Test
+    public void testAdd() {
+        int m = random.nextInt();
+        int n = random.nextInt();
+        assertThat(Number.of(m).add(Number.of(n))).isEqualTo(Number.of(m + n));
+    }
+
+    @Test
+    public void testSubtract() {
+        int m = random.nextInt();
+        int n = random.nextInt();
+        assertThat(Number.of(m).subtract(Number.of(n))).isEqualTo(Number.of(m - n));
+    }
+
+    @Test
+    public void testProduct() {
+        int m = random.nextInt();
+        int n = random.nextInt();
+        assertThat(Number.of(m).product(Number.of(n))).isEqualTo(Number.of(m * n));
+    }
+
+    @Test
+    public void testDivide() {
+        int m = random.nextInt();
+        int n = random.nextInt();
+        assertThat(Number.of(m).divide(Number.of(n))).isEqualTo(Number.of(m / n));
+    }
+
+
+    @Test
+    public void testDivideBy0() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Number.of(5).divide(Number.of(0)));
+    }
+}
