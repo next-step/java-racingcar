0b9121e010777bf43bc10e838c1e638e5ea2b59e
haeuniii <qnfnqnfn9825@gmail.com>
2021-11-03 15:53:19 +0900

    [UPD] split method 관련 학습 테스트 기능 추가
    split method 관련 학습 테스트 기능 추가

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,17 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+    @Test
+    @DisplayName("split method test")
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1","2");
+    }
+}
