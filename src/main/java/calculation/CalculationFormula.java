595c550fc271d93dd4e542d73cce6c8621a9133d
user <kth4878@naver.com>
2021-11-05 04:10:44 +0900

    [Refactor] 유효성 검사 추가

--- src/main/java/calculation/CalculationFormula.java
+++ src/main/java/calculation/CalculationFormula.java
@@ -1,15 +1,22 @@
 package calculation;
 
-public class Data {
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class CalculationFormula {
 	private static final String NULL_ERROR_MESSAGE = "error : null 값은 사용할수 없습니다.";
 	private static final String EMPTY_ERROR_MESSAGE = "error : 공백은 사용할수 없습니다.";
+	private static final String INPUT_TYPE_ERROR_MESSAGE = "error : 아라비아 숫자나 사칙연산자 가 아닌 문자가 있습니다.";
+	private static final String OPERATOR_PATTERN = "[-/*+]";
+	private static final String NUMBER_PATTERN = "^[0-9]*$";
 	private static final String EMPTY = " ";
-	private final String[] data;
+	private final String[] formula;
 
-	public Data(String data) {
-		checkNull(data);
-		checkEmpty(data);
-		this.data = emptyDataSplit(data);
+	public CalculationFormula(String formula) {
+		checkNull(formula);
+		checkEmpty(formula);
+		checkIndexNumber(formula);
+		this.formula = emptyDataSplit(formula);
 	}
 
 	private void checkNull(String value) {
@@ -24,19 +31,31 @@ public class Data {
 		}
 	}
 
+	private void checkIndexNumber(String formula){
+		String[] numbers = separateNumber(emptyDataSplit(formula));
+		for(int i=0; i<numbers.length; i++){
+			checkInputType(numbers[i]);
+		}
+	}
+
+	private String[] separateNumber(String[] formula) {
+		return Arrays.stream(formula)
+			.map(String::valueOf)
+			.filter(i -> !Pattern.matches(OPERATOR_PATTERN, i))
+			.toArray(String[]::new);
+	}
+
+	private void checkInputType(String value){
+		if(!Pattern.matches(NUMBER_PATTERN, value)){
+			throw new IllegalArgumentException(INPUT_TYPE_ERROR_MESSAGE);
+		}
+	}
+
 	private String[] emptyDataSplit(String data) {
 		return data.split(EMPTY);
 	}
 
-	public int size() {
-		return data.length;
-	}
-
-	public String value(int index) {
-		return data[index];
-	}
-
-	public String[] getData() {
-		return data;
+	public String[] getFormula() {
+		return formula;
 	}
 }
