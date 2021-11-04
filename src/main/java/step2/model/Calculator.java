c5617404cbe74b94932bf61c681cbd3f4f881f21
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2021-11-04 20:54:23 +0900

    Step2 (#2711)
    * [feature] calculator feature add
    
    * [fix] step1 testcode fix
    
    * [delete] enum class delete
    
    * [fix] 바로 int 타입으로 input 받도록 수정
    
    * [delete] 전부 지우고 다시 설계
    
    * [fix] Operator 클래스에서 계산과 String을 int와 operator로 나눠주는 메소드 분리
    
    * [delete] operator delete
    
    * [feature] Mynumber, Caclculator operator method add
    
    * [feature] psvm add
    
    * [fix] getter number add in MyNumber

--- src/main/java/step2/model/Calculator.java
+++ src/main/java/step2/model/Calculator.java
@@ -2,14 +2,38 @@ package step2.model;
 
 public class Calculator {
 
-    public static Integer operator(String str) {
-        String[] StringArray = str.split(" ");
-        Integer result = new Integer(StringArray[0]);
-        for (int i = 1; i < StringArray.length; i += 2) {
-            String operator = StringArray[i];
-            Integer num1 = new Integer(StringArray[i+1]);
-            result = CalculatorEnum.findOperator(result, num1, operator);
+    public static MyNumber operator(String str) {
+        String[] value = str.split(" ");
+        MyNumber result = new MyNumber(Integer.parseInt(value[0]));
+        for (int i = 1; i < value.length; i += 2) {
+            String operator = value[i];
+            MyNumber myNumber2 = new MyNumber(Integer.parseInt(value[i+1]));
+            result = calculator(result, myNumber2, operator);
         }
         return result;
     }
+
+    public static MyNumber calculator(MyNumber num1, MyNumber num2, String o) {
+        if (o.equals("+")) {
+            return num1.add(num2);
+        }
+        if (o.equals("-")) {
+            return num1.minus(num2);
+        }
+        if (o.equals("*")) {
+            return num1.multiply(num2);
+        }
+        if (o.equals("/")) {
+            valid(num2);
+            return num1.divide(num2);
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public static void valid(MyNumber num2) {
+        if (num2.equals(new MyNumber(0))) {
+            throw new IllegalArgumentException("나눗셈에서 분모는 0이 될 수 없습니다");
+        }
+    }
+
 }
--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -1,33 +1,46 @@
 package step2;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
+import step2.model.MyNumber;
 import step2.model.Calculator;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
-    @DisplayName("Operator test")
+    @DisplayName("add method test")
     @ParameterizedTest
-    @CsvSource(value = {"1 + 1 - 5 + 10 / 7 * 100:100", "100 * 100:10000", " 100 / 100:1"}, delimiter = ':')
-    public void calculator(String str, int expect) {
-        assertEquals(Calculator.operator(str), expect);
+    @CsvSource(value = {"1 + 1 - 5 + 10:7","100 * 100:10000"},delimiter = ':')
+    public void calculator(String str, int expect ) {
+        assertTrue(Calculator.operator(str).equals(new MyNumber(expect)));
     }
 
-    @DisplayName("divide by zero exception test")
-    @ParameterizedTest
-    @CsvSource(value = {"100 / 0:1"}, delimiter = ':')
-    public void divideByZeroException(String str, int expect) {
-        assertThatThrownBy(() -> {
-            Calculator.operator(str);
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("나눗셈에서 분모는 0이 될 수 없습니다");
+    @Test
+    void add() {
+        MyNumber mynumber = new MyNumber("1").add(new MyNumber(2));
+        assertTrue(mynumber.equals(new MyNumber(3)));
     }
 
+    @Test
+    void minus() {
+        MyNumber mynumber = new MyNumber("5").minus(new MyNumber(1));
+        assertTrue(mynumber.equals(new MyNumber(4)));
+    }
 
+    @Test
+    void multiply() {
+        MyNumber mynumber = new MyNumber("12").multiply(new MyNumber(12));
+        assertTrue(mynumber.equals(new MyNumber(144)));
+    }
 
+    @Test
+    void divide() {
+        MyNumber mynumber = new MyNumber("33").add(new MyNumber(3));
+        assertTrue(mynumber.equals(new MyNumber(11)));
+    }
 }
