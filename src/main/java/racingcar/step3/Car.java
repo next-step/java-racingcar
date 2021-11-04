0f8cd818883bf43b6ae7fede2616d0aeb6c14a3d
hmoon826 <hmoon826@gmail.com>
2021-11-04 15:29:46 +0900

    (feat) Car 도메인 및 기능 작성

---
+++ src/main/java/racingcar/step3/Car.java
@@ -0,0 +1,52 @@
+package racingcar.step3;
+
+import java.util.Random;
+
+/**
+ * Car(자동차) 클래스
+ * 2021.11.04
+ * v.01
+ */
+
+public class Car {
+
+    public static final int MAX_NUM = 9;
+    public static final int MIN_NUM = 4;
+    private int id;
+    private int position;
+
+    public Car(int id) {
+        this(id, 1);
+    }
+
+    public Car(int id, int position) {
+        this.id = id;
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int number) {
+        if (isMovable(number)) {
+            position++;
+        }
+    }
+
+    public void move() {
+        int randomNum = getRandomNum();
+        if (isMovable(randomNum)) {
+            position++;
+        }
+    }
+
+    private boolean isMovable(int number) {
+        return MIN_NUM <= number & number <= MAX_NUM;
+    }
+
+    private int getRandomNum() {
+        Random random = new Random();
+        return random.nextInt(MAX_NUM);
+    }
+}
