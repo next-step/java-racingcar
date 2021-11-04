40f880fcf02398c92214a1c48a0ca1ae87864406
audgnssweet <audgnssweet@naver.com>
2021-11-04 17:46:45 +0900

    긴 사칙연산 통과하는 테스트 만들기 완료

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,21 +1,41 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Calculator {
 
     public static int calc(String s) {
-        String[] split = s.split(" ");
-        int first = Integer.parseInt(split[0]);
-        int second = Integer.parseInt(split[2]);
-        String operator = split[1];
+        List<Integer> numbers = new LinkedList<>();
+        List<String> operators = new LinkedList<>();
 
-        int ans = 0;
-        if (operator.equals("+")) {
-            ans = first + second;
-        } else if (operator.equals("-")) {
-            ans = first - second;
-        } else if (operator.equals("*")) {
-            ans = first * second;
-        } else if (operator.equals("/")) {
-            ans = first / second;
+        String[] split = s.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            if (i % 2 == 0) {
+                numbers.add(Integer.parseInt(split[i]));
+            }
+            if (i % 2 == 1) {
+                operators.add(split[i]);
+            }
         }
+
+        int ans = calc(numbers, operators);
         return ans;
     }
+
+    private static int calc(List<Integer> numbers, List<String> operators) {
+        for (int i = 0; i < operators.size(); i++) {
+            int first = numbers.remove(0);
+            int second = numbers.remove(0);
+            if (operators.get(i).equals("+")) {
+                numbers.add(0, first + second);
+            } else if (operators.get(i).equals("-")) {
+                numbers.add(0, first - second);
+            } else if (operators.get(i).equals("*")) {
+                numbers.add(0, first * second);
+            } else if (operators.get(i).equals("/")) {
+                numbers.add(0, first / second);
+            }
+        }
+        return numbers.get(0);
+    }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -27,4 +27,10 @@ public class CalculatorTest {
         int result = Calculator.calc("3 / 2");
         assertThat(result).isEqualTo(1);
     }
+
+    @Test
+    void longInput() {
+        int result = Calculator.calc("2 + 3 * 4 / 2");
+        assertThat(result).isEqualTo(10);
+    }
 }
