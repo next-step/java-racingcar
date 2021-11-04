393093b85251f215d6bdcfededda84336be4ad9f
tomo.213 <tomo.213@kakaocorp.com>
2021-11-04 18:12:50 +0900

    test - Implements Racing Car (Step 1)

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,14 +8,29 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
     @Test
-    @DisplayName("Test String Class")
+    @DisplayName("split Class")
     void split() {
         assertThat("1,2".split(",")).containsExactly("1", "2");
         assertThat("1".split(",")).containsExactly("1");
-        assertThat("(1,2)".substring(1,4)).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("substring Test")
+    void substring() {
+        assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt Test")
+    void charAt() {
         assertThat("abc".charAt(0)).isEqualTo('a');
         assertThat("abc".charAt(1)).isEqualTo('b');
         assertThat("abc".charAt(2)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("IndexOutOfBounds Test")
+    void indexOutOfBounds() {
         assertThatThrownBy(() -> "abc".charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
