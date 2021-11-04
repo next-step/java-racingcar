99c45ed1bb8f6fa8afad0a7ab4f15587adee2014
khl1154 <khl1154@naver.com>
2021-11-04 21:49:16 +0900

    test : String test 반영

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    void split() {
+        String [] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1","2");
+
+    }
+
+    @Test
+    void subString(){
+        String result = "(1,2)";
+        assertThat(result.substring(1,4)).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        String result = "abc";
+        assertThatThrownBy(() -> {
+            result.charAt(5);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("에러");
+    }
+}
