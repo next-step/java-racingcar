6ac32d7dfb643f7121546363986b57351dd387e1
minjoon_lee <minjoon1995@naver.com>
2021-11-04 22:05:45 +0900

    [refactoring] divider by zeror validation를 mynumber로 이관

--- src/main/java/step2/model/MyNumber.java
+++ src/main/java/step2/model/MyNumber.java
@@ -5,15 +5,16 @@ import java.util.Objects;
 public class MyNumber {
     private final int number;
 
-    public Integer getNumber() {
-        return number;
-    }
     public MyNumber(int number) {
         this.number = number;
     }
 
     public MyNumber(String s) {
         this(Integer.parseInt(s));
+    }
+
+    public Integer getNumber() {
+        return number;
     }
 
     public MyNumber add(MyNumber myNumber2) {
@@ -23,10 +24,16 @@ public class MyNumber {
         return new MyNumber(this.number - myNumber2.number);
     }
     public MyNumber divide(MyNumber myNumber2) {
+        valid(myNumber2);
         return new MyNumber(this.number / myNumber2.number);
     }
     public MyNumber multiply(MyNumber myNumber2) {
         return new MyNumber(this.number * myNumber2.number);
+    }
+    public static void valid(MyNumber num2) {
+        if (num2.equals(new MyNumber(0))) {
+            throw new IllegalArgumentException("나눗셈에서 분모는 0이 될 수 없습니다");
+        }
     }
     @Override
     public boolean equals(Object obj) {
@@ -36,5 +43,9 @@ public class MyNumber {
         return this.number == myNumber.number;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
 
