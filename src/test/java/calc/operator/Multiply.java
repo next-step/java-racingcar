51fd44421c5c9f460a4a195f58f1c006ebd8d375
ChangmoKang <dkssudckdah@gmail.com>
2021-11-04 13:30:10 +0900

    docs : 온라인 강의 적용

--- src/test/java/calc/operator/Multiply.java
+++
@@ -1,8 +0,0 @@
-package calc.operator;
-
-public class Multiply implements Operator{
-    @Override
-    public int operation(int left, int right) {
-        return left * right;
-    }
-}
--- src/test/java/calc/operator/Operator.java
+++
@@ -1,5 +0,0 @@
-package calc.operator;
-
-public interface Operator {
-    int operation(int left, int right);
-}
