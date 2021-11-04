f0dc6a722e93664c9fd33593ef43bddb608a6026
minjoon_lee <minjoon1995@naver.com>
2021-11-04 22:04:46 +0900

    [add] hascode test code 추가

--- src/test/java/step2/MyNumberTest.java
+++ src/test/java/step2/MyNumberTest.java
@@ -7,10 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyNumberTest {
+
     @Test
-    void create() {
+    void equal() {
         MyNumber mynumber = new MyNumber("1");
         assertTrue(mynumber.equals(new MyNumber(1)));
+    }
+
+    @Test
+    void hasCode() {
+        MyNumber myNumber = new MyNumber("1");
+        MyNumber myNumber2 = new MyNumber("1");
+        assertEquals(myNumber.hashCode(),myNumber2.hashCode());
+
     }
 
 }
