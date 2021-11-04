b798ff38011ae21d474ea050f63ac04376482e42
ChangmoKang <dkssudckdah@gmail.com>
2021-11-04 20:20:29 +0900

    test (자동차 전진, 멈춤, 상태출력)

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,42 @@
-package racingcar;public class Car {
+package racingcar;
+
+import java.util.Objects;
+
+public class Car {
+    private Status status;
+
+    public Car() {
+        this(new Status());
+    }
+
+    public Car(Status status) {
+        this.status = status;
+    }
+
+    public void moveOrNot(int randomInt) {
+        if (movable(randomInt)) {
+            this.status = new Status(this.status.value() + 1);
+        }
+    }
+
+    private boolean movable(int value) {
+        return value > 3;
+    }
+
+    public int forwardCount() {
+        return this.status.value();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return this.status.equals(car.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(status);
+    }
 }
--- src/main/java/racingcar/Status.java
+++ src/main/java/racingcar/Status.java
@@ -1,2 +1,33 @@
-package racingcar;public class Status {
+package racingcar;
+
+import java.util.Objects;
+
+public class Status {
+    private final int forwardCount;
+
+    public Status() {
+        this(0);
+    }
+
+    public Status(int forwardCount) {
+        this.forwardCount = forwardCount;
+    }
+
+    public int value() {
+        return this.forwardCount;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Status status = (Status) o;
+        return forwardCount == status.forwardCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(forwardCount);
+    }
 }
