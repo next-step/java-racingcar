70507eac9982da97439e500690199d2f1f60b821
user <kth4878@naver.com>
2021-11-05 04:16:48 +0900

    [Refactor] 상태값 사용하는 방식으로 변경

--- src/main/java/calculation/Number.java
+++ src/main/java/calculation/Number.java
@@ -1,56 +1,37 @@
 package calculation;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class Number {
-	private static final String NUMBER_TYPE_ERROR_MESSAGE = "error : 연산자 앞 뒤는 아라비아 숫자만 사용가능합니다.";
-	private static final String EMPTY = "";
 	private static final String NUMBER_PATTERN = "^[0-9]*$";
-	private static final String OPERATOR_PATTERN = "[-/*+]";
+	private final int[] number;
 
-	public Number() {
+	public Number(String[] formula) {
+		this.number = changeToStringInteger(formula);
 	}
 
-	public static int findFirstNumber(Data data, int number, int operatorIndex) {
-		if (number == 0) {
-			return firstNumber(data, operatorIndex);
-		}
-		return number;
+	private int[] changeToStringInteger(String[] formula) {
+		return Arrays.stream(numberSeparation(formula))
+			.mapToInt(Integer::parseInt)
+			.toArray();
 	}
 
-	private static int firstNumber(Data data, int operatorIndex) {
-		String number = EMPTY;
-		for (int i = 0; i < operatorIndex; i++) {
-			number += data.value(i);
-		}
-
-		checkNumberType(number);
-
-		return changeToStringInteger(number);
+	private String[] numberSeparation(String[] formula){
+		return Arrays.stream(formula)
+			.filter(i -> Pattern.matches(NUMBER_PATTERN, i))
+			.toArray(String[]::new);
 	}
 
-	public static int lastNumber(Data data, int operatorIndex) {
-		String number = EMPTY;
-		for (int i = operatorIndex + 1; i < data.size(); i++) {
-			if (Pattern.matches(OPERATOR_PATTERN, data.value(i))) {
-				break;
-			}
-			number += data.value(i);
+	public int firstNumber(int calculationValue, int index){
+		if(calculationValue == 0){
+			return number[index];
 		}
 
-		checkNumberType(number);
-
-		return changeToStringInteger(number);
+		return calculationValue;
 	}
 
-	private static void checkNumberType(String number) {
-		boolean result = !Pattern.matches(NUMBER_PATTERN, number);
-		if (result) {
-			throw new IllegalArgumentException(NUMBER_TYPE_ERROR_MESSAGE);
-		}
-	}
-
-	private static int changeToStringInteger(String number) {
-		return Integer.parseInt(number);
+	public int value(int index) {
+		return number[index];
 	}
 }
