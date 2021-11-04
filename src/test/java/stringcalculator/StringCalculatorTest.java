5b5255e549931ba459a77700f6eeb8b8a03411c1
alsqhd <kiryuinh32@gmail.com>
2021-11-04 13:08:41 +0900

    test: StringCalculator plus test

---
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,29 @@
+package stringcalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringCalculatorTest {
+
+    StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @ParameterizedTest
+    @DisplayName("plus test")
+    @CsvSource({"1 + 2, 3", "2 + 2, 4"})
+    void plusTest(String s, int expected) {
+        int actual = stringCalculator.calc(s);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+
+}
