e70d208cef3a4bbe358074f55b3f66c8d0ac89a8
Chung Beom Cho <cndqjacndqja@gmail.com>
2021-11-04 20:13:59 +0900

    String substring test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,6 +8,12 @@ public class StringTest {
     @Test
     void split() {
         String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substring(){
+        assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
     }
 }
