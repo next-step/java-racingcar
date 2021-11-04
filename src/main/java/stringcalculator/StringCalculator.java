aa5481d9fa79d2d86643d1a2bc2cb846841dfbec
alsqhd <kiryuinh32@gmail.com>
2021-11-04 16:21:45 +0900

    refactor: 변수 네이밍 수정
    Number a -> Number first

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -4,15 +4,15 @@ public class StringCalculator {
     public int calc(String input) {
         String[] split = input.split(" ");
 
-        Number a = Number.from(split[0]);
+        Number firstNum = Number.from(split[0]);
 
         for (int i = 1; i < split.length - 1; i += 2) {
-            Number b = Number.from(split[i +1]);
+            Number secondNum = Number.from(split[i +1]);
             String operator = split[i];
-            a = Operator.calc(operator, a, b);
+            firstNum = Operator.calc(operator, firstNum, secondNum);
         }
 
-        return a.getNumber();
+        return firstNum.getNumber();
     }
 
 }
