82bfe8b5680308ba5d61739dc559464412534cf8
nano.son <nano.son@kakaocorp.com>
2021-11-05 02:00:17 +0900

    simple success

--- src/main/java/calculate/Calculator.java
+++ src/main/java/calculate/Calculator.java
@@ -2,6 +2,30 @@ package calculate;
 
 public class Calculator {
     public int calculate(String s) {
-        return Integer.MAX_VALUE;
+        String[] tokens = s.split(" ");
+        Integer n1 = Integer.parseInt(tokens[0]);
+        String operator = tokens[1];
+        Integer n2 = Integer.parseInt(tokens[2]);
+
+        if(operator.equals("+")) {
+            return n1 + n2;
+        }
+
+        if(operator.equals("-")) {
+            return n1 - n2;
+        }
+
+        if(operator.equals("*")) {
+            return n1 * n2;
+        }
+
+        if(operator.equals("/")) {
+            if(n2 == 0) {
+                throw new IllegalArgumentException("분모 0");
+            }
+            return n1 / n2;
+        }
+
+        throw new IllegalArgumentException("사칙연산만 가능합니다");
     }
 }
