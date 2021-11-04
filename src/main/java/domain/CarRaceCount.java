0cd8d2c5c6b77a6bc96f48093d02042ae501c792
user <kth4878@naver.com>
2021-11-05 04:23:29 +0900

    [Feat] 자동차 경주 횟수 도메인 추가

---
+++ src/main/java/domain/CarRaceCount.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class CarRaceCount {
+	private final int carRaceCount;
+
+	public CarRaceCount(int carRaceCount) {
+		this.carRaceCount = carRaceCount;
+	}
+
+	public int count(){
+		return carRaceCount;
+	}
+
+}
