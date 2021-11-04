9687a5d1fc8431c1046957f009f07da02baaa820
minjoon_lee <minjoon1995@naver.com>
2021-11-04 22:05:22 +0900

    [fix] Test코드 깨지는 것 수정 및 0으로 나눌 때 exception test추가

--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -7,17 +7,26 @@ import org.junit.jupiter.params.provider.CsvSource;
 import step2.model.MyNumber;
 import step2.model.Calculator;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    @DisplayName("add method test")
+    @DisplayName("Operator test")
     @ParameterizedTest
-    @CsvSource(value = {"1 + 1 - 5 + 10:7","100 * 100:10000"},delimiter = ':')
+    @CsvSource(value = {"1 + 1 - 5 + 10 / 7 * 100:100","100 * 100:10000"," 100 / 100:1"},delimiter = ':')
     public void calculator(String str, int expect ) {
-        assertTrue(Calculator.operator(str).equals(new MyNumber(expect)));
+        assertEquals(Calculator.operator(str),new MyNumber(expect));
+    }
+
+    @DisplayName("divide by zero exception test")
+    @ParameterizedTest
+    @CsvSource(value = {"100 / 0:1"},delimiter = ':')
+    public void divideByZeroException(String str, int expect ) {
+        assertThatThrownBy(() -> {
+            Calculator.operator(str);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("나눗셈에서 분모는 0이 될 수 없습니다");
     }
 
     @Test
@@ -40,7 +49,7 @@ public class CalculatorTest {
 
     @Test
     void divide() {
-        MyNumber mynumber = new MyNumber("33").add(new MyNumber(3));
+        MyNumber mynumber = new MyNumber("33").divide(new MyNumber(3));
         assertTrue(mynumber.equals(new MyNumber(11)));
     }
 }
