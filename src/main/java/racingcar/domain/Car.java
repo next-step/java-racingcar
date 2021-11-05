6d381d15e5f12f6d37ed1d95c60ffad97a3a33eb
alsqhd <kiryuinh32@gmail.com>
2021-11-05 14:51:08 +0900

    feat: Car#equals 재정의

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Car {
     private static final int MOVE_VALUE = 1;
 
@@ -22,5 +24,18 @@ public class Car {
 
     public Position getPosition() {
         return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(position, car.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
