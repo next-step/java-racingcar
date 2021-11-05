6a9f599bbc43a5700b8193dc35aa3075154f14a6
csh0034 <csh0034@gmail.com>
2021-11-05 21:42:00 +0900

    chore: 기능 변경에 따른 사용하지 않는 메서드 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,11 +3,6 @@ package racingcar.view;
 import racingcar.util.ScannerUtils;
 
 public final class InputView {
-	public static int scanNumberOfCars() {
-		System.out.println("자동차 대수는 몇 대 인가요?");
-		return ScannerUtils.scanInt();
-	}
-
 	public static String scanNameOfCars() {
 		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 		return ScannerUtils.scanString();
