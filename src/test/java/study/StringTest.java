d92ff0d129df26e7339ce29929b0302747bedadf
강호진 <blackpearl1210@gmail.com>
2021-11-05 23:23:01 +0900

    [21.11.05] step1 미션 수정

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * String 클래스에 대한 학습 테스트
@@ -16,13 +18,18 @@ public class StringTest {
      *   - "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
      *
      *  힌트
-     *   - 배열로 반환하는 값의 경우 assertj의 contains()를 활용해 반환 값이 맞는지 검증한다.
      *   - 배열로 반환하는 값의 경우 assertj의 containsExactly()를 활용해 반환 값이 맞는지 검증한다.
+     *   - 배열로 반환하는 값의 경우 assertj의 contains()를 활용해 반환 값이 맞는지 검증한다.
      */
     @Test
-    @DisplayName("StringTest split()")
-    void split(){
+    @DisplayName("String에 ',' 분할 후 조회")
+    void split_first(){
         assertThat("1,2".split(",")).containsExactly("1","2");
+    }
+
+    @Test
+    @DisplayName("String에 ',' 누락되며 존재하지 않아도 조회")
+    void split_second(){
         assertThat("1".split(",")).contains("1");
     }
 
@@ -31,7 +38,7 @@ public class StringTest {
      *   - "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
      */
     @Test
-    @DisplayName("StringTest substring()")
+    @DisplayName("String에 '()' 제외한 원하는 문자열 조회")
     void substring(){
         assertThat("(1,2)".substring(1,4)).isEqualTo("1,2");
     }
@@ -53,20 +60,32 @@ public class StringTest {
      *   - assertThatNullPointerException()
      */
     @Test
-    @DisplayName("StringTest charAt()")
-    void charAt(){
+    @DisplayName("String에 1번째 위치 문자열 조회")
+    void charAt_first(){
         String param = "abc";
         assertThat(param.charAt(0)).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("String에 2번째 위치 문자열 조회")
+    void charAt_second(){
+        String param = "abc";
         assertThat(param.charAt(1)).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("String에서 3번째 위치 문자열 조회")
+    void charAt_third(){
+        String param = "abc";
         assertThat(param.charAt(2)).isEqualTo('c');
+    }
 
+    @Test
+    @DisplayName("String에서 특정 문자를 가져올 때 위치 값을 벗어난 문자 조회")
+    void charAt_outOfBounds(){
+        String param = "abc";
         assertThatThrownBy(() -> {
-            System.out.println("Exception 이전");
             param.charAt(10);
-            System.out.println("Exception 이후");
         }).isInstanceOf(StringIndexOutOfBoundsException.class);
-
-//        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
-//            .isThrownBy(() -> param.charAt(10));
     }
 }
