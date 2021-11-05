cd791c610aee28df96d3a14f58dfec35e8274fb2
alsqhd <kiryuinh32@gmail.com>
2021-11-05 12:07:40 +0900

    feat: RandomMovingStrategy

---
+++ src/main/java/racingcar/RandomMovingStrategy.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import java.util.Random;
+
+public class RandomMovingStrategy implements MovingStrategy {
+    private static final int MOVE_CRITERIA = 4;
+    private static final int MAX_VALUE = 10;
+    private static final Random RANDOM = new Random();
+
+    @Override
+    public boolean move() {
+        if (RANDOM.nextInt(MAX_VALUE) >= MOVE_CRITERIA) {
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
+    }
+}
