d7a6261afe1bbf8da895b45a30181cfab4bc4223
khl1154 <khl1154@naver.com>
2021-11-05 20:58:41 +0900

    test : setCollectionTest 반영

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetCollectionTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("Set Collection 테스트")
+    void contains(int input) {
+        assertTrue(numbers.contains(input));
+        assertTrue(numbers.contains(input));
+        assertTrue(numbers.contains(input));
+    }
+
+
+}
