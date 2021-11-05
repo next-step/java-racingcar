0328e5ff58fc933243ad2b710a2066eb18994bf0
KKambi <bok03220@gmail.com>
2021-11-05 20:47:39 +0900

    test: 타입 체크 유틸리티 테스트 추가

---
+++ src/test/java/com/kkambi/racing/util/TypeCheckerTest.java
@@ -0,0 +1,21 @@
+package com.kkambi.racing.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TypeCheckerTest {
+
+    @DisplayName("숫자 형식의 문자열을 넣으면 String -> int 체크에 성공한다")
+    @Test
+    void checkInt() {
+        assertThat(TypeChecker.isInteger("1234")).isTrue();
+    }
+
+    @DisplayName("숫자가 아닌 문자열을 넣으면 String -> int 체크에 성공한다")
+    @Test
+    void checkNotInt() {
+        assertThat(TypeChecker.isInteger("12*4n23리뷰감사합니다")).isFalse();
+    }
+}
