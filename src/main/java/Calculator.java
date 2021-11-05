cd8cc83a44594a06aa4a3652ab9ae66aed2f9881
kong.jino <kong.jino@kakaoenterprise.com>
2021-11-05 20:05:25 +0900

    2개의 인자로한 산술 기능 추가

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,6 +2,30 @@ public class Calculator {
 
 
   public static String calculate(String input) {
-    return "0";
+    String[] s = input.split(" ");
+    int first = Integer.parseInt(s[0]);
+    int second = Integer.parseInt(s[2]);
+    String operator = s[1];
+
+    return calculate(first, second, operator);
+  }
+
+  private static String calculate(int first, int second, String operator) {
+    if (operator.equals("+")) {
+      return Integer.toString(first + second);
+    }
+    if (operator.equals("-")) {
+      return Integer.toString(first - second);
+    }
+    if (operator.equals("*")) {
+      return Integer.toString(first * second);
+    }
+    if (operator.equals("/")) {
+      if (second == 0) {
+        throw new IllegalArgumentException("0으로 나눌수 없습니다.");
+      }
+      return Integer.toString(first / second);
+    }
+    throw new IllegalArgumentException("허용되지 않는 연산자입니다.");
   }
 }
