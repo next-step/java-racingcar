f5e2ffecf79a100e7ac684fcff9b24f6f6bbd031
LOUIS-Seungmin <93302978+LOUIS-Seungmin@users.noreply.github.com>
2021-11-05 23:33:12 +0900

    [2단계 - 문자열 계산기] 코드리뷰 사항 수정 (#2745)
    * [2단계 - 문자열 계산기] 요구사항 구현
    
    * [2단계 - 문자열 계산기] Operator에 toString 추가
    
    * [2단계 - 문자열 계산기] 코드리뷰 수정사항 반영
    
    * [2단계 - 문자열 계산기] Calculator와 Formula의 역할 구분 수정
    
    * [2단계 - 문자열 계산기] 리스트 뒤집기시 카피된 리스트를 사용하도록 변경
    
    Co-authored-by: louis-amu <louis.amu@kakaocorp.com>

--- src/main/java/study02/Formula.java
+++ src/main/java/study02/Formula.java
@@ -1,8 +1,13 @@
 package study02;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Formula {
 
-    private final String[] splitFormula;
+    private final List<Operand> operandList;
+    private final List<Operator> operatorList;
 
     public Formula(String formula) {
         if (formula == null) {
@@ -11,10 +16,42 @@ public class Formula {
         if (formula.trim().length() == 0) {
             throw new IllegalArgumentException("수식 문자열은 비어있을 수 없습니다");
         }
-        this.splitFormula = formula.split(" ");
+
+        String[] splitFormula = formula.split(" ");
+
+        Arrays.stream(splitFormula)
+                .filter(it -> !Operand.isValid(it) && !Operator.isValid(it))
+                .findAny()
+                .ifPresent(it -> {
+                    throw new IllegalArgumentException(String.format("연산자, 피연산자로 사용이 불가능한 문자가 있습니다. %s", it));
+                });
+
+        this.operandList = Arrays.stream(splitFormula)
+                .filter(Operand::isValid)
+                .map(Operand::new)
+                .collect(Collectors.toList());
+
+        this.operatorList = Arrays.stream(splitFormula)
+                .filter(Operator::isValid)
+                .map(Operator::of)
+                .collect(Collectors.toList());
+
+        if (operandList.isEmpty()) {
+            throw new IllegalArgumentException("피연산자가 1개 이상이 필요합니다.");
+        }
+        if (operatorList.isEmpty()) {
+            throw new IllegalArgumentException("연산자가 1개 이상이 필요합니다.");
+        }
+        if (operandList.size() - operatorList.size() != 1) {
+            throw new IllegalArgumentException("연산자와 피연산자의 개수가 올바르지 않습니다.");
+        }
     }
 
-    public String[] getSplitFormula() {
-        return splitFormula;
+    public List<Operand> getOperandList() {
+        return this.operandList;
+    }
+
+    public List<Operator> getOperatorList() {
+        return this.operatorList;
     }
 }
