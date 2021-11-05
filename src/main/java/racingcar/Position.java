13185c673d0aa79d8e231e222eb55964fe8fd433
alsqhd <kiryuinh32@gmail.com>
2021-11-05 12:12:13 +0900

    feat: Position class add equals & hashCode

--- src/main/java/racingcar/Position.java
+++ src/main/java/racingcar/Position.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.Objects;
+
 public class Position {
     private final int position;
 
@@ -13,5 +15,18 @@ public class Position {
 
     public Position move(int moveValue) {
         return new Position(this.position + moveValue);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
