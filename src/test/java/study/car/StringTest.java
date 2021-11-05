bc68d2d540d3cf232a4210e77f4ce4b9d2a46310
Ordi <rhkrgns56@naver.com>
2021-11-05 13:33:11 +0900

    [PR] 1단계 - 학습 테스트 실습 (#2739)
    * 1단계 - String 클래스에 대한 학습 테스트
    
    * 1단계 - Set Collection에 대한 학습 테스트
    
    * 리팩토링: StringTest 코멘트 -> DisplayName 으로 수정

---
+++ src/test/java/study/car/StringTest.java
@@ -0,0 +1,49 @@
+package study.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+@DisplayName("1단계 - String 클래스에 대한 학습 테스트")
+public class StringTest {
+
+    @Test
+    @DisplayName("요구사항 1 - 데이터 여러개")
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("요구사항 1 - 데이터 하나")
+    void splitOnlyOne() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("요구사항 2")
+    void splitContainingBraces() {
+        String sample = "(1,2)";
+
+        String substring = sample.substring(1, sample.length() - 1);
+        String[] result = substring.split(",");
+
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("요구사항 3 - chartAt, StringIndexOutOfBoundsException 테스트")
+    void charAt() {
+        String sample = "abc";
+
+        assertThat(sample.charAt(1))
+                .isEqualTo('b');
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    sample.charAt(sample.length() + 5);
+                });
+    }
+}
