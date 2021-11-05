1ece5181380801e59101f4caf16c50596b4e2a78
aiden.sim <aiden.sim@kakaocorp.com>
2021-11-05 18:44:27 +0900

    [feature] OuputUtils 추가

--- src/main/java/racingcar/ui/ResultCarOutputView.java
+++ src/main/java/racingcar/ui/ResultCarOutputView.java
@@ -3,29 +3,31 @@ package racingcar.ui;
 import racingcar.service.domain.Car;
 import racingcar.service.dto.Record;
 import racingcar.service.dto.RoundResult;
+import racingcar.utils.OutputUtils;
 
 public class ResultCarOutputView implements OutputView {
     private static final String DASH = "-";
+    private static final String EMPTY = "";
 
     @Override
     public void output(RoundResult roundResult) {
         roundResult.getRecords().forEach(record -> {
-            System.out.printf("%d 번째 경주%n", record.getRound());
+            OutputUtils.println(String.format("%d 번째 경주", record.getRound()));
             print(record);
         });
-        System.out.printf("%s가 최종 우승했습니다.", roundResult.getFinalWinnerName());
+        OutputUtils.println(String.format("%s가 최종 우승했습니다", roundResult.getFinalWinnerName()));
     }
 
     public void print(Record record) {
         record.getCars().forEach(ResultCarOutputView::print);
-        System.out.println();
+        OutputUtils.println(EMPTY);
     }
 
     public static void print(Car car) {
-        System.out.printf("%s : ", car.getName());
+        OutputUtils.print(String.format("%s : ", car.getName()));
         for (int i = 0; i < car.getPosition(); i++) {
-            System.out.print(DASH);
+            OutputUtils.print(DASH);
         }
-        System.out.println();
+        OutputUtils.println(EMPTY);
     }
 }
