e33f43e45407b5471c19526bb048e45ecbe9ed28
alsqhd <kiryuinh32@gmail.com>
2021-11-05 14:25:01 +0900

    test: TryCount equals & hashCode 재정의

--- src/test/java/racingcar/TryCountTest.java
+++ src/test/java/racingcar/TryCountTest.java
@@ -2,8 +2,13 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TryCountTest {
@@ -15,4 +20,26 @@ class TryCountTest {
         assertThatThrownBy(() -> TryCount.from(count)).isInstanceOf(TryCountException.class);
     }
 
+    @ParameterizedTest
+    @DisplayName("TryCount counting test")
+    @MethodSource
+    void countingTest(TryCount tryCount, TryCount expected) {
+        tryCount.counting();
+
+        assertThat(tryCount).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> countingTest() {
+        return Stream.of(
+                Arguments.of(
+                        TryCount.from(1), TryCount.from(2)
+                ),
+                Arguments.of(
+                        TryCount.from(2), TryCount.from(3)
+                ),
+                Arguments.of(
+                        TryCount.from(10), TryCount.from(11)
+                )
+        );
+    }
 }
