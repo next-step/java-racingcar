4f9e409bf2a7aef97475db95e5c29b641fc88c7a
nano.son <nano.son@kakaocorp.com>
2021-11-06 00:48:19 +0900

    Operate를 분리해낸다

--- src/main/java/calculate/Formula.java
+++ src/main/java/calculate/Formula.java
@@ -1,56 +1,73 @@
 package calculate;
 
-import java.util.Stack;
+import java.util.*;
 
 public class Formula {
     private final Stack<Integer> numberStack = new Stack<>();
-    private final Stack<String> operatorStack = new Stack<>();
+    private final Stack<Operate> operatorStack = new Stack<>();
+    private final Map<String, Operate> symbolToOperate = new HashMap<>();
+
+    {
+        symbolToOperate.put("+", Operate.ADD);
+        symbolToOperate.put("-", Operate.SUBTRACT);
+        symbolToOperate.put("*", Operate.MULTIPLY);
+        symbolToOperate.put("/", Operate.DIVIDE);
+    }
 
     public Formula(String rawFormula) {
-        String[] tokens = rawFormula.split(" ");
+        if (rawFormula == null || rawFormula.trim().isEmpty()) {
+            throw new IllegalArgumentException("formula is null or blank");
+        }
 
+        String[] tokens = rawFormula.trim().split(" ");
         for (int i = tokens.length - 1; i >= 0; i--) {
-            if (i % 2 == 0) {
-                numberStack.push(Integer.parseInt(tokens[i]));
+            classify(tokens[i]);
+        }
+    }
+
+    private void classify(String raw) {
+        if (isNumeric(raw)) {
+            numberStack.push(Integer.parseInt(raw));
+        } else {
+            Operate operate = symbolToOperate.get(raw);
+            if (operate == null) {
+                throw new IllegalArgumentException("illegal operator: " + raw);
             }
-            if (i % 2 == 1) {
-                operatorStack.push(tokens[i]);
-            }
+            operatorStack.push(operate);
+        }
+    }
+
+    private boolean isNumeric(String raw) {
+        try {
+            Integer.parseInt(raw);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 
     public int calculate() {
         Stack<Integer> numberStack = (Stack<Integer>) this.numberStack.clone();
-        Stack<String> operatorStack = (Stack<String>) this.operatorStack.clone();
+        Stack<Operate> operatorStack = (Stack<Operate>) this.operatorStack.clone();
 
         while (!operatorStack.empty()) {
             numberStack.push(
-                    calculate(numberStack.pop(), operatorStack.pop(), numberStack.pop())
+                    operatorStack.pop().calculate(numberStack.pop(), numberStack.pop())
             );
         }
         return numberStack.pop();
     }
 
-    private int calculate(int n1, String operator, int n2) {
-        if (operator.equals("+")) {
-            return n1 + n2;
-        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Formula formula = (Formula) o;
+        return numberStack.equals(formula.numberStack) && operatorStack.equals(formula.operatorStack) && symbolToOperate.equals(formula.symbolToOperate);
+    }
 
-        if (operator.equals("-")) {
-            return n1 - n2;
-        }
-
-        if (operator.equals("*")) {
-            return n1 * n2;
-        }
-
-        if (operator.equals("/")) {
-            if (n2 == 0) {
-                throw new IllegalArgumentException("분모 0");
-            }
-            return n1 / n2;
-        }
-
-        throw new IllegalArgumentException("사칙연산만 가능합니다");
+    @Override
+    public int hashCode() {
+        return Objects.hash(numberStack, operatorStack, symbolToOperate);
     }
 }
--- src/test/java/calculate/FormulaTest.java
+++ src/test/java/calculate/FormulaTest.java
@@ -1,15 +1,44 @@
 package calculate;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class FormulaTest {
+    @Test
+    void whenFormulaIsNull() {
+        assertThatThrownBy(() -> {
+            new Formula(null);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenFormulaIsBlank() {
+        assertThatThrownBy(() -> {
+            new Formula(" ");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenFormulaHasIllegalOperator() {
+        assertThatThrownBy(() -> {
+            new Formula("1 & 1");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
     @ParameterizedTest
-    @CsvSource(value = {"2 + 3 * 4 / 2:10", "2 / 3 * 4 / 2:0", "2 * 3 / 4 + 2:3"}, delimiter = ':')
-    void composite(String composite, int expectedResult) {
-        Formula formula = new Formula(composite);
+    @CsvSource(value = {"2 + 3 * 4 / 2:10", "2 / 3 * 4 / 2:0", "2 * 3 / 4 + 2:3", "-1 * -1 + 1 * 2:4"}, delimiter = ':')
+    void calculate(String rawFormula, int expectedResult) {
+        Formula formula = new Formula(rawFormula);
         assertThat(formula.calculate()).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void sameFormula() {
+        String rawFormula = "2 + 3 * 4 / 2";
+        assertThat(new Formula(rawFormula)).isEqualTo(new Formula(rawFormula));
     }
 }
