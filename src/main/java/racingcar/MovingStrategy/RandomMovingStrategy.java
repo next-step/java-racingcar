6849f3105c54407d7169daabe15b06e4e986d637
alsqhd <kiryuinh32@gmail.com>
2021-11-05 14:48:29 +0900

    refactor: RandomMovingStrategy singleton pattern 적용

--- src/main/java/racingcar/MovingStrategy/RandomMovingStrategy.java
+++ src/main/java/racingcar/MovingStrategy/RandomMovingStrategy.java
@@ -5,9 +5,17 @@ import racingcar.domain.MovingStrategy;
 import java.util.Random;
 
 public class RandomMovingStrategy implements MovingStrategy {
+    private static final RandomMovingStrategy RANDOM_MOVING_STRATEGY = new RandomMovingStrategy();
     private static final int MOVE_CRITERIA = 4;
     private static final int MAX_VALUE = 10;
     private static final Random RANDOM = new Random();
+
+    private RandomMovingStrategy() {
+    }
+
+    public static MovingStrategy getInstance() {
+        return RANDOM_MOVING_STRATEGY;
+    }
 
     @Override
     public boolean move() {
