531b5eb09e40569f4402d9b4e5ec6b48ac566d67
sid <sid.seo@kakaocorp.com>
2021-11-06 21:52:49 +0900

    feature: Race 클래스에 Car 적용
    Race 클래스에서 Car 리스트를 들고 있도록 수정
     - 몇몇 변수명을 정확한 이름으로 변경
     - 게임 진행 상황을 알려주는 메소드 추가

--- src/main/java/edu/nextstep/camp/Race.java
+++ src/main/java/edu/nextstep/camp/Race.java
@@ -1,29 +1,41 @@
 package edu.nextstep.camp;
 
-public class Race {
-    private final int turn;
-    private final int cars;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public static Race of(int cars, int turn) {
-        if (cars <= 0)
-            throw new IllegalArgumentException("invalid number of cars: " + cars);
+public class Race {
+    private final int totalTurns;
+    private final List<Car> cars;
+
+    public static Race of(int numberOfCars, int turn) {
+        if (numberOfCars <= 0)
+            throw new IllegalArgumentException("invalid number of cars: " + numberOfCars);
 
         if (turn <= 0)
             throw new IllegalArgumentException("invalid number of turns: " + turn);
 
-        return new Race(cars, turn);
+        return new Race(numberOfCars, turn);
     }
 
-    private Race(int cars, int turn) {
-        this.cars = cars;
-        this.turn = turn;
+    private Race(int numberOfCars, int totalTurns) {
+        this.cars = new ArrayList<>(numberOfCars);
+        for(int i = 0; i < numberOfCars; i++) {
+            this.cars.add(new Car());
+        }
+
+        this.totalTurns = totalTurns;
     }
 
-    public int turns() {
-        return turn;
+    public int totalTurns() {
+        return totalTurns;
     }
 
     public int numberOfCars() {
-        return cars;
+        return cars.size();
+    }
+
+    public List<Integer> gameResult() {
+        return cars.stream().map(Car::position).collect(Collectors.toList());
     }
 }
--- src/test/java/edu/nextstep/camp/RaceTest.java
+++ src/test/java/edu/nextstep/camp/RaceTest.java
@@ -1,5 +1,7 @@
 package edu.nextstep.camp;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -18,6 +20,14 @@ public class RaceTest {
     public void create(int cars, int turn) {
         final Race race = Race.of(cars, turn);
         assertThat(race.numberOfCars()).isEqualTo(cars);
-        assertThat(race.turns()).isEqualTo(turn);
+        assertThat(race.totalTurns()).isEqualTo(turn);
+    }
+
+    @Test
+    @DisplayName("get game result(initial status)")
+    public void gameResult() {
+        final int numberOfCars = 4;
+        final Race race = Race.of(numberOfCars, 1);
+        assertThat(race.gameResult()).containsExactly(0, 0, 0, 0);
     }
 }
