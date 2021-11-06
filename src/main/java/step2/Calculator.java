0d7a64a5272c45e584b0ade76e0f35361325bcd6
Lee Dongkyu <nelldklee@gmail.com>
2021-11-06 22:07:15 +0900

    refactor(MyNumber): 원시값 포장 및 역할 분리

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -4,37 +4,27 @@ public class Calculator {
 
     public static int calculate(String text) {
         final String[] values = text.split(" ");
-        int first = Integer.parseInt(values[0]);
+
+        MyNumber first = new MyNumber(values[0]);
+        MyNumber second = new MyNumber(values[2]);
         String operator = values[1];
-        int second = Integer.parseInt(values[2]);
 
+        return calculate(first, operator, second).getNumber();
+    }
+
+    private static MyNumber calculate(MyNumber first, String operator, MyNumber second) {
         if (operator.equals("+")) {
-            return Calculator.add(first, second);
-        } else if (operator.equals("-")) {
-            return Calculator.sub(first, second);
-        } else if (operator.equals("*")) {
-            return Calculator.mul(first, second);
-        } else if (operator.equals("/")) {
-            return Calculator.div(first, second);
+            return first.plus(second);
         }
-
-        return 0;
+        if (operator.equals("-")) {
+            return first.minus(second);
+        }
+        if (operator.equals("*")) {
+            return first.multiple(second);
+        }
+        if (operator.equals("/")) {
+            return first.divide(second);
+        }
+        return null;
     }
-
-    private static int div(int first, int second) {
-        return first / second;
-    }
-
-    private static int mul(int first, int second) {
-        return first * second;
-    }
-
-    private static int sub(int first, int second) {
-        return first - second;
-    }
-
-    public static int add(int first, int second) {
-        return first + second;
-    }
-
 }
---
+++ src/main/java/step2/MyNumber.java
@@ -0,0 +1,49 @@
+package step2;
+
+import java.util.Objects;
+
+public class MyNumber {
+
+    private int number;
+
+    public MyNumber(String text) {
+        this(Integer.parseInt(text));
+    }
+
+    public MyNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public MyNumber plus(MyNumber second) {
+        return new MyNumber(this.number + second.number);
+    }
+
+    public MyNumber minus(MyNumber second) {
+        return new MyNumber(this.number - second.number);
+    }
+
+    public MyNumber multiple(MyNumber second) {
+        return new MyNumber(this.number * second.number);
+    }
+
+    public MyNumber divide(MyNumber second) {
+        return new MyNumber(this.number / second.number);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MyNumber)) return false;
+        MyNumber myNumber = (MyNumber) o;
+        return number == myNumber.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
