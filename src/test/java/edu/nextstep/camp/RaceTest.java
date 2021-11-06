ca6657ed8953e46d62eb1912dd4829b7469163b0
sid <sid.seo@kakaocorp.com>
2021-11-06 13:08:15 +0900

    test: 테스트 이름 변경

--- src/test/java/edu/nextstep/camp/RaceTest.java
+++ src/test/java/edu/nextstep/camp/RaceTest.java
@@ -9,13 +9,13 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 public class RaceTest {
     @ParameterizedTest(name = "create with invalid values: {arguments}")
     @CsvSource(value = {"0, 0", "-1, 5", "7, -1"})
-    public void testFailedToCreate(int cars, int turn) {
+    public void createFail(int cars, int turn) {
         assertThatIllegalArgumentException().isThrownBy(() -> Race.of(cars, turn));
     }
 
     @ParameterizedTest(name = "create race test: {arguments}")
     @CsvSource(value = {"1, 1", "53, 15", "7000, 12000"})
-    public void testCreate(int cars, int turn) {
+    public void create(int cars, int turn) {
         final Race race = Race.of(cars, turn);
         assertThat(race.numberOfCars()).isEqualTo(cars);
         assertThat(race.turns()).isEqualTo(turn);
