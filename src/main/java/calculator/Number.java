def537ca48c7171cd2635cd2bd0df42f79354e38
jeje <las9897@gmail.com>
2021-11-06 18:19:18 +0900

    feat: Number Class 추가

---
+++ src/main/java/calculator/Number.java
@@ -0,0 +1,35 @@
+package calculator;
+
+public class Number {
+    private final int number;
+
+    public Number(int number) {
+        this.number = number;
+    }
+
+    public Number(String number) {
+        this(Integer.parseInt(number));
+    }
+
+    public Number plus(Number other) {
+        return new Number(this.number + other.number);
+    }
+
+    public Number minus(Number other) {
+        return new Number(this.number - other.number);
+    }
+
+    public Number times(Number other) {
+        return new Number(this.number * other.number);
+    }
+
+    public Number dividedBy(Number other) {
+        if (other.number == 0)
+            throw new IllegalArgumentException("분모는 0이 될 수 없습니다.");
+        return new Number(this.number / other.number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
