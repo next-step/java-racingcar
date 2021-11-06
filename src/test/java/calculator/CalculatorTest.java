54debf11dbf1a5ae5e9115f8f40a2a56bbc1b382
jeje <las9897@gmail.com>
2021-11-06 18:21:49 +0900

    test: Number class를 활용한 테스트케이스 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -39,4 +39,37 @@ public class CalculatorTest {
 
     }
 
+    @Test
+    public void plus2() {
+        int acc = Calculator.calculate2("1 + 2");
+        assertThat(acc).isEqualTo(3);
+    }
+
+    @Test
+    public void minus2() {
+        int acc = Calculator.calculate2("3 - 2");
+        assertThat(acc).isEqualTo(1);
+    }
+
+    @Test
+    public void times2() {
+
+        int acc = Calculator.calculate2("3 * 2");
+        assertThat(acc).isEqualTo(6);
+    }
+
+    @Test
+    public void dividedBy2() {
+        int acc = Calculator.calculate2("4 / 2");
+        assertThat(acc).isEqualTo(2);
+    }
+
+    @Test
+    void dividedByZero2() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            int acc = Calculator.calculate2("4 / 0");
+        }).withMessageMatching("분모는 0이 될 수 없습니다.");
+
+    }
+
 }
