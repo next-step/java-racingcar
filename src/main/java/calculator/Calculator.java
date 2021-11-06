f64ac8d9ee079cfd186261034cff46667a9b4c3e
jeje <las9897@gmail.com>
2021-11-06 17:58:22 +0900

    feat: 문자열 계산기 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,7 +4,29 @@ public class Calculator {
 
     public static int calculate(String s) {
 
-        return 0;
+        String[] values = s.split(" ");
+
+        int first = Integer.parseInt(values[0]);
+        String operator = values[1];
+        int second = Integer.parseInt(values[2]);
+
+        if ("+".equals(operator)) {
+            return first + second;
+        }
+        if ("-".equals(operator)) {
+            return first - second;
+        }
+        if ("*".equals(operator)) {
+            return first * second;
+        }
+        if ("/".equals(operator)) {
+            if (second == 0) {
+                throw new IllegalArgumentException("분모는 0이 될 수 없습니다.");
+            }
+            return first / second;
+        }
+
+        throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
     }
 }
 
