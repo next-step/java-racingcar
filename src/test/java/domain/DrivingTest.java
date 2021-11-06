2c43cf6f475a912fd92eb6d2cfd19e0eb0194ac9
user <kth4878@naver.com>
2021-11-07 03:43:58 +0900

    [Test] 파일 마지막 엔터 추가

--- src/test/java/domain/DrivingTest.java
+++ src/test/java/domain/DrivingTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class DrivingTest {
 	@Test
 	@DisplayName("0부터 9까지 의 랜덤값 한번 생성")
-	public void createRandomNumber(){
-		assertThat(Driving.drive()).isBetween(0,9);
+	public void createRandomNumber() {
+		assertThat(Driving.drive()).isBetween(0, 9);
 	}
 }
