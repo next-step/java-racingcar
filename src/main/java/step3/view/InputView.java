c8dcaad2062d87fbb26f817deafb01020cad9aee
doorisopen <doorwasopen@gmail.com>
2021-11-07 21:11:41 +0900

    refactor: step3 명칭 수정

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String CAR_COUNT_OUTPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
-    private static final String GAME_RAP_COUNT_OUTPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private static final String INPUT_CAR_COUNT_REQUEST_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_GAME_COUNT_REQUEST_MESSAGE = "시도할 회수는 몇 회 인가요?";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputCarCount() {
-        System.out.println(CAR_COUNT_OUTPUT_MESSAGE);
+        System.out.println(INPUT_CAR_COUNT_REQUEST_MESSAGE);
         return scanner.nextInt();
     }
 
     public static int inputGameRoundCount() {
-        System.out.println(GAME_RAP_COUNT_OUTPUT_MESSAGE);
+        System.out.println(INPUT_GAME_COUNT_REQUEST_MESSAGE);
         return scanner.nextInt();
     }
 
--- src/main/java/step3/view/ResultView.java
+++ src/main/java/step3/view/ResultView.java
@@ -4,10 +4,10 @@ import step3.domain.Participant;
 
 public class ResultView {
 
-    private static final String START_OUTPUT_MESSAGE = "실행 결과";
+    private static final String GAME_START_OUTPUT_MESSAGE = "실행 결과";
 
     public static void start() {
-        System.out.println(START_OUTPUT_MESSAGE);
+        System.out.println(GAME_START_OUTPUT_MESSAGE);
     }
 
     public static void result(Participant participant) {
