9cf8c2ffdaf8dd4e990d7a08bb64119869af4ded
김민혁 <kminheuk@gmail.com>
2021-11-07 20:08:56 +0900

    Feat: Add StringCalculator minus test

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,15 +1,21 @@
 package stringcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void beforeEach() {
+        calculator = new StringCalculator();
+    }
 
     @Test
     void testAddTwoNumbers() {
         // Given
-        StringCalculator calculator = new StringCalculator();
         String expression = "1 + 2";
 
         // When
@@ -22,7 +28,6 @@ public class StringCalculatorTest {
     @Test
     void testAddFourNumbers() {
         // Given
-        StringCalculator calculator = new StringCalculator();
         String expression = "1 + 2 + 5 + 5";
 
         // When
@@ -30,5 +35,29 @@ public class StringCalculatorTest {
 
         // Then
         assertThat(result).isEqualTo(13);
+    }
+
+    @Test
+    void testMinusTwoNumbers() {
+        // Given
+        String expression = "3 - 1";
+
+        // When
+        int result = calculator.calculate(expression);
+
+        // Then
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void testMinusFourNumbers() {
+        // Given
+        String expression = "5 - 2 - 1 - 1";
+
+        // When
+        int result = calculator.calculate(expression);
+
+        // Then
+        assertThat(result).isEqualTo(1);
     }
 }
