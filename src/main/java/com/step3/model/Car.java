994dd93238c60c76e685292728081d19f77d345f
haeuniii <qnfnqnfn9825@gmail.com>
2021-11-07 22:09:12 +0900

    feat: if문 괄호 추가

--- src/main/java/com/step3/model/Car.java
+++ src/main/java/com/step3/model/Car.java
@@ -32,8 +32,13 @@ public class Car {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
         Car car = (Car) o;
         return id == car.id;
     }
