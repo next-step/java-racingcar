a18d651f4be48d92843801549aebf2972a012465
jjoy <ekdnlt1995@gmail.com>
2021-11-07 16:52:32 +0900

    refactor : Car 일급 컬렉션 객체 생성, 리뷰 피드백 반영

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -1,10 +1,8 @@
 package racingcar;
 
-import racingcar.moverule.RandomMoveRule;
 import racingcar.game.RacingCarGame;
 import racingcar.gameinfo.CountInfo;
-import racingcar.gameinfo.RacingCarGameInfo;
-import racingcar.gameinfo.RacingCarGameRound;
+import racingcar.moverule.RandomMoveRule;
 import racingcar.view.InputView;
 
 public class RacingCarApplication {
@@ -12,8 +10,7 @@ public class RacingCarApplication {
     public static void main(String[] args) {
         try {
             CountInfo countInfo = InputView.getCountInfo();
-            RacingCarGameInfo info = new RacingCarGameInfo(new RacingCarGameRound(new RandomMoveRule()), countInfo);
-            RacingCarGame game = new RacingCarGame(info);
+            RacingCarGame game = new RacingCarGame(countInfo, new RandomMoveRule());
             game.start();
         } catch (IllegalArgumentException e){
             System.out.println(e.getMessage() + "게임을 종료합니다.");
--- src/test/java/racingcar/car/CarsTest.java
+++ src/test/java/racingcar/car/CarsTest.java
@@ -1,26 +1,29 @@
 package racingcar.car;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.moverule.RandomMoveRule;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class CarCreatorTest {
+public class CarsTest {
+
+    Cars cars;
 
     @ParameterizedTest
     @ValueSource(ints = {1,2,3,4,5})
     @DisplayName("전달받은 size의 리스트를 생성하는지 확인")
     void sizeTest(int size){
-        assertThat(CarCreator.create(size).size()).isEqualTo(size);
+        cars = new Cars(new RandomMoveRule(), size);
+        assertThat(cars.getCarList().size()).isEqualTo(size);
     }
 
     @Test
     @DisplayName("생성된 리스트의 Car 객체는 다 달라야한다.")
     void createDifferentObjectTest(){
-        List<Car> carList = CarCreator.create(2);
-        assertThat(carList.get(0)).isNotSameAs(carList.get(1));
+        cars = new Cars(new RandomMoveRule(), 2);
+        assertThat(cars.getCarList().get(0)).isNotSameAs(cars.getCarList().get(1));
     }
 }
