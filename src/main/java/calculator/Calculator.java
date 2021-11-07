f5302ce76dc2f4cee48d7f7e23db0642d8bd7ad7
jeje <las9897@gmail.com>
2021-11-07 14:20:33 +0900

    refactor: int형 계산기 제거

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,44 +1,16 @@
 package calculator;
 
 public class Calculator {
-
     public static int calculate(String expression) {
 
         String[] values = expression.split(" ");
-
-        int first = Integer.parseInt(values[0]);
-        String operator = values[1];
-        int second = Integer.parseInt(values[2]);
-
-        if ("+".equals(operator)) {
-            return first + second;
-        }
-        if ("-".equals(operator)) {
-            return first - second;
-        }
-        if ("*".equals(operator)) {
-            return first * second;
-        }
-        if ("/".equals(operator)) {
-            if (second == 0) {
-                throw new IllegalArgumentException("분모는 0이 될 수 없습니다.");
-            }
-            return first / second;
-        }
-
-        throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
-    }
-
-    public static int calculate2(String s) {
-
-        String[] values = s.split(" ");
         Number first = new Number(values[0]);
         Number second = new Number(values[2]);
 
-        return calculate2(first, values[1], second).getNumber();
+        return calculate(first, values[1], second).getNumber();
     }
 
-    private static Number calculate2(Number first, String operator, Number second) {
+    private static Number calculate(Number first, String operator, Number second) {
 
         if ("+".equals(operator))
             return first.plus(second);
