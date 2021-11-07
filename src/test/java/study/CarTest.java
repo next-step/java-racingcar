511c84bd1e5895a02b83ebe6b15050bd2d257058
haeuniii <qnfnqnfn9825@gmail.com>
2021-11-07 22:02:33 +0900

    test: test code DisplayName 추가

--- src/test/java/study/CarTest.java
+++ src/test/java/study/CarTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import com.step3.model.Car;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,18 +9,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
     @Test
+    @DisplayName("Car class 생성자 test")
     void create() {
         Car car = new Car(0, 0);
         assertThat(car).isEqualTo(new Car(0, 0));
     }
 
     @Test
+    @DisplayName("Car move 후 Car class로 return test")
     void move() {
         Car car = new Car(0, 0);
         assertThat(car.move(7)).isEqualTo(new Car(0,1));
     }
 
     @Test
+    @DisplayName("Car 이동 거리 print test")
     void getResult() {
         Car car = new Car(0, 3);
         assertThat(car.getResult()).isEqualTo("---");
--- src/test/java/study/RandomUtilTest.java
+++ src/test/java/study/RandomUtilTest.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.*;
 
 import com.step3.model.Car;
 import com.step3.util.RandomUtil;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class RandomUtilTest {
     @Test
+    @DisplayName("0~9 사이의 숫자 추출 test")
     void getRandomValue() {
         assertThat(RandomUtil.getRandomValue()).isGreaterThanOrEqualTo(0).isLessThan(10);
     }
