53669f8da6c49c9d141bce742e6f80629d48dae7
michael.10 <michael.10@kakaocorp.com>
2021-11-07 21:27:04 +0900

    Improved Set.contains test.

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -3,6 +3,7 @@ package step1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -30,8 +31,8 @@ public class SetTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
-    void contains(int input) {
-        assertThat(numbers.contains(input)).isTrue();
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false"})
+    void contains(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
