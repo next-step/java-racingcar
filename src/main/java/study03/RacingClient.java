5f9a46b1dd3d90f07ee03e12395438061cfb3cfe
tnals1422 <tnals1422@gmail.com>
2021-11-07 14:21:17 +0900

    feat: 1. 몇 대의 자동차로 몇 번의 이동을 할 것인지 사용자로부터 입력받기

---
+++ src/main/java/study03/RacingClient.java
@@ -0,0 +1,23 @@
+package study03;
+
+import java.util.Scanner;
+
+/*
+*
+* RacingClient
+*
+* @version 1.0.0
+*
+* 2021-11-07
+*
+* Copyright tnals1422
+*/
+public class RacingClient {
+    public static void main(String[] args) {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        Scanner scanner = new Scanner(System.in);
+        String carCount = scanner.nextLine();
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        String tryCount = scanner.nextLine();
+    }
+}
