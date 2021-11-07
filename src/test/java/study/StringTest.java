63e7846d4de7bc697c8113cded5d75a6b6013fd7
Chung Beom Cho <cndqjacndqja@gmail.com>
2021-11-08 00:32:05 +0900

    [자동차 경주] 1단계 - 학습 테스트 실습 (#2780)
    * split study test
    
    * String substring test
    
    * charAt IndexOutOfBoundException test
    
    * SetTest size test
    
    * SetTest parameterized test
    
    * SetTest CsvSource test

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substring(){
+        assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt()에서의 index가 배열의 범위에서 벗어났을 때, IndexOutOfBoundException 테스트")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range");
+    }
+}
