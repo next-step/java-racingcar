998aa070171ef504a54f79f3dbf4631294e11b5d
supniverse <kmsup2@gmail.com>
2021-11-07 23:10:55 +0900

    test: 주어진 범위 안에서 이동 여부 테스트 추가

--- src/test/java/carracing/domain/CarTest.java
+++ src/test/java/carracing/domain/CarTest.java
@@ -2,32 +2,29 @@ package carracing.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
-import static org.assertj.core.api.Assertions.in;
 
 class CarTest {
 
   private Car car;
 
-  private static final int NOW_INIT_COUNT = 0;
   private static final int TRY_INIT_COUNT = 10;
 
   @BeforeEach
   void init() {
-    car = new Car(NOW_INIT_COUNT, TRY_INIT_COUNT);
+    car = new Car(TRY_INIT_COUNT);
   }
 
   @ParameterizedTest
   @DisplayName("car의 생성자를 통해 입력한 현재위치와 시도 횟수 초기화가 이루어지는지 확인한다.")
-  @CsvSource(value = {"5:10", "2:8", "9:7"}, delimiter = ':')
+  @CsvSource(value = {"1:10", "1:8", "1:7"}, delimiter = ':')
   void createCar(int now, int tryCount) {
-    car = new Car(now, tryCount);
+    car = new Car(tryCount);
     assertThat(car.getNowStep()).isEqualTo(now);
     assertThat(car.getMoveCount()).isEqualTo(tryCount);
   }
@@ -52,10 +49,21 @@ class CarTest {
   }
 
   @ParameterizedTest
-  @DisplayName("도전 기회가 0이하 일때 기회를 소진하는 함수 호출시 IllegalStateException을 던진다.")
+  @DisplayName("도전 기회가 0이하 일때 기회를 소진하는 함수 호출 시 IllegalStateException을 던진다.")
   @ValueSource(ints = {-1, 0, -5, -21})
   void NoMoreChanceTest(int input) {
-    car = new Car(NOW_INIT_COUNT, input);
+    car = new Car(input);
     assertThatIllegalStateException().isThrownBy(() -> car.minusMoveCount());
+  }
+
+  @ParameterizedTest
+  @DisplayName("한칸씩 전진이 가능하므로 주어진 기회 이상 이동을 시도할 때 IllegalStateException을 던진다.")
+  @ValueSource(ints = {5, 10, 15})
+  void NoMoreMoveTest(int tryCount) {
+    car = new Car(tryCount);
+    for (int i = 0; i < tryCount; i++) {
+      car.plusNowStep();
+    }
+    assertThatIllegalStateException().isThrownBy(() -> car.plusNowStep());
   }
 }
