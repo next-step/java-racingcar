096ea1013c528817494f2a8e87a906ae462a4041
강호진 <blackpearl1210@gmail.com>
2021-11-07 22:22:48 +0900

    [21.11.07] step2 문자열 계산기

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -2,60 +2,40 @@ package study;
 
 public class Calculator {
     public static int calculate(String param) {
+        String[] values = validation(param);
+
+        MyNumber result = new MyNumber(values[0]);
+        for(int i = 1; i < values.length; i = i + 2){
+            result = calculate(result, values[i], new MyNumber(values[i+1]));
+        }
+        return result.getNumber();
+    }
+
+    /**
+     * 입력값 정상 여부 확인
+     * @param param
+     * @return
+     */
+    private static String[] validation(String param) {
+        if(param == null || param.isEmpty()){
+            throw new IllegalArgumentException("입력 문자열이 정상적이지 않습니다.");
+        }
         String[] values = param.split(" ");
-        int first = Integer.parseInt(values[0]);
-        String operator = values[1];
-        int second = Integer.parseInt(values[2]);
-
-        return extracted(first, operator, second);
+        if(values.length < 3 || values.length % 2 == 0){
+            throw new IllegalArgumentException("입력 문자열이 정상적이지 않습니다.");
+        }
+        return values;
     }
 
-    private static int extracted(int first, String operator, int second) {
-        if(operator.equals("+")){
-            return first + second;
-        }
-        if(operator.equals("-")) {
-            return first - second;
-        }
-        if(operator.equals("*")) {
-            return first * second;
-        }
-        if(operator.equals("/")){
-            valid(second);
-            return first / second;
-        }
-        return 0;
+    /**
+     * 사칙연산 계산
+     * @param first
+     * @param operator
+     * @param second
+     * @return
+     */
+    private static MyNumber calculate(MyNumber first, String operator, MyNumber second) {
+        return MyOperator.operator(operator).operate(first, second);
     }
 
-    public static int calculate2(String param) {
-        String[] values = param.split(" ");
-        MyNumber first = new MyNumber(values[0]);
-        MyNumber second = new MyNumber(values[2]);
-        String operator = values[1];
-
-        return calculate2(first, operator, second).getNumber();
-    }
-
-    private static MyNumber calculate2(MyNumber first, String operator, MyNumber second) {
-        if(operator.equals("+")){
-            return first.plus(second);
-        }
-        if(operator.equals("-")) {
-            return first.minus(second);
-        }
-        if(operator.equals("*")) {
-            return first.times(second);
-        }
-        if(operator.equals("/")){
-//            valid(second);
-            return first.dividedBy(second);
-        }
-        throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
-    }
-
-    private static void valid(int second) {
-        if(second == 0){
-            throw new IllegalArgumentException("분모가 0일 될 수 없습니다.");
-        }
-    }
 }
--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,6 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,7 +18,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
  * - 사용자가 입력한 문자열 값에 따라 사칙연산을 수행할 수 있는 계산기를 구현해야 한다.
  * - 입력 문자열의 숫자와 사칙 연산 사이에는 반드시 빈 공백 문자열이 있다고 가정한다.
  * - 나눗셈의 경우 결과 값을 정수로 떨어지는 값으로 한정한다.
- * - 문자열 계산기는 사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정된다. 즉, 수학에서는 곱셈, 나눗셈이 덧셈, 뺄셈 보다 먼저 계산해야 하지만 이를 무시한다.
+ * - 문자열 계산기는 사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정된다.
+ *   즉, 수학에서는 곱셈, 나눗셈이 덧셈, 뺄셈 보다 먼저 계산해야 하지만 이를 무시한다.
  * - 예를 들어 2 + 3 * 4 / 2와 같은 문자열을 입력할 경우 2 + 3 * 4 / 2 실행 결과인 10을 출력해야 한다.
  *
  * 프로그래밍 요구사항
@@ -45,31 +50,65 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
  * - https://www.baeldung.com/parameterized-tests-junit-5
  */
 public class CalculatorTest {
-    @Test
-    public void 덧셈(){
-        int result = Calculator.calculate2("1 + 2");
-        assertThat(result).isEqualTo(3);
-    }
 
+    @DisplayName("입력값이 null")
     @Test
-    public void 뺄셈(){
-        int result = Calculator.calculate2("3 - 2");
-        assertThat(result).isEqualTo(1);
-    }
-
-    @Test
-    public void 곱셈(){
-        int result = Calculator.calculate2("3 * 2");
-        assertThat(result).isEqualTo(6);
-    }
-
-    @Test
-    public void 나눗셈(){
-        int result = Calculator.calculate2("4 / 2");
-        assertThat(result).isEqualTo(2);
-
+    public void 입력값이_null(){
         assertThatThrownBy(() -> {
-            Calculator.calculate2("4 / 0");
+            Calculator.calculate(null);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력값이 빈 공백 문자")
+    @Test
+    public void 입력값이_빈_공백_문자(){
+        assertThatThrownBy(() -> {
+            Calculator.calculate("");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("비정상적인 입력값")
+    @ParameterizedTest(name="{displayName} | 요청식: {0}")
+    @ValueSource(strings = {"1 + ",  "1 + 3 +"})
+    public void 비정상적인_입력값(String expression){
+        assertThatThrownBy(() -> {
+            Calculator.calculate(expression);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사칙연산 기호 아닌 입력값")
+    @Test
+    public void 사칙연산_기호_아닌_입력값(){
+        assertThatThrownBy(() -> {
+            Calculator.calculate("1 ^ 3");
+        }).isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("분자가 0인 나눗셈")
+    @Test
+    public void 분자가_0인_나눗셈(){
+        assertThat(Calculator.calculate("0 / 10")).isEqualTo(0);
+    }
+
+    @DisplayName("분모가 0인 나눗셈")
+    @Test
+    public void 분모가_0인_나눗셈(){
+        assertThatThrownBy(() -> {
+            Calculator.calculate("4 / 0");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사칙연산 정상 확인")
+    @ParameterizedTest(name="{displayName} | 요청식: {0} | 결과값: {1}")
+    @CsvSource(value = {"1 + 2:3", "3 - 2:1", "3 * 2:6", "4 / 2:2"}, delimiter = ':')
+    void 사칙연산_정상_확인(String expression, int result) {
+        assertThat(Calculator.calculate(expression)).isEqualTo(result);
+    }
+
+    @DisplayName("다중 사칙연산 정상 확인")
+    @ParameterizedTest(name="{displayName} | 요청식: {0} | 결과값: {1}")
+    @CsvSource(value = {"-2 + 3 * 4:4", "2 + 3 * 4:20", "2 + 3 * 4 / 2:10"}, delimiter = ':')
+    void 다중_사칙연산_정상_확인(String expression, int result) {
+        assertThat(Calculator.calculate(expression)).isEqualTo(result);
     }
 }
