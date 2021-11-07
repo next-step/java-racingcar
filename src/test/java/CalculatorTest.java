464d901a0aec69d24dc3a0bccdabf99bccee7dfd
kong.jino <kong.jino@kakaoenterprise.com>
2021-11-08 01:24:46 +0900

    [refactoring] 단순 연산 테스트를 OperatorTest로 분리

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,50 +8,6 @@ import org.junit.jupiter.api.Test;
 class CalculatorTest {
 
   @Test
-  @DisplayName("덧셈")
-  void testAdd() {
-    // given
-    String input = "1 + 2";
-    // when
-    String calculate = new Calculator().calculate(input);
-    // then
-    assertThat(calculate).isEqualTo("3");
-  }
-
-  @Test
-  @DisplayName("덧셈")
-  void testSubtraction() {
-    // given
-    String input = "2 - 1";
-    // when
-    String calculate = new Calculator().calculate(input);
-    // then
-    assertThat(calculate).isEqualTo("1");
-  }
-
-  @Test
-  @DisplayName("곱셈")
-  void testMultiplication() {
-    // given
-    String input = "3 * 2";
-    // when
-    String calculate = new Calculator().calculate(input);
-    // then
-    assertThat(calculate).isEqualTo("6");
-  }
-
-  @Test
-  @DisplayName("나눗셈")
-  void testDivision() {
-    // given
-    String input = "4 / 2";
-    // when
-    String calculate = new Calculator().calculate(input);
-    // then
-    assertThat(calculate).isEqualTo("2");
-  }
-
-  @Test
   @DisplayName("0으로 나눌 경우")
   void testDivision_divideByZero() {
     // given
