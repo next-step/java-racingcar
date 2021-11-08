77dda73cbb90cc655d21b95402d1bea469741149
mj950425 <minjoon1995@naver.com>
2021-11-09 00:24:18 +0900

    [feature] main문 추가

---
+++ src/main/java/step3/Main.java
@@ -0,0 +1,17 @@
+package step3;
+
+import step3.model.CarList;
+import step3.service.MoveOrStop;
+import step3.view.Input;
+import step3.view.Output;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Input input = new Input();
+        ArrayList<String> inputCarList = new CarList().makeCarList(input.getCountOfCar());
+        Output output = new Output(inputCarList, input.getCountOfTry());
+        output.outputView();
+    }
+}
