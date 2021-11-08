161047b4f75cd264aeeda9ab431a40273055b1c9
Han <hankim@tapasmedia.co>
2021-11-08 21:37:30 +0900

    test : add forwardsWithRandom

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -3,6 +3,11 @@ package racing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,5 +29,12 @@ class CarTest {
         assertThat(car.forward(0)).isEqualTo(0);
         assertThat(car.forward(1)).isEqualTo(0);
         assertThat(car.forward(10)).isEqualTo(1);
+    }
+
+    @ParameterizedTest(name = "Random value 를 통한 전진 테스트")
+    @MethodSource("racing.NumberHelperTest#randomIntegerListProvider")
+    void forwardsWithRandom(int random) {
+        boolean isForward = random > 4;
+        assertThat(car.forward(random) == 1).isEqualTo(isForward);
     }
 }
