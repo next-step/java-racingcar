f15255f807a878fa0da683e0b3e96198df50d18f
mj950425 <minjoon1995@naver.com>
2021-11-09 00:24:07 +0900

    [feature] input과 output 추가

---
+++ src/main/java/step3/view/Input.java
@@ -0,0 +1,28 @@
+package step3.view;
+
+import step3.model.CarList;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Input {
+    private Integer countOfCar;
+    private Integer countOfTry;
+
+    public Integer getCountOfTry() {
+        return countOfTry;
+    }
+
+    public Integer getCountOfCar() { return countOfCar;}
+
+    public void Input() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        this.countOfCar = scanner.nextInt();
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        this.countOfTry = scanner.nextInt();
+    }
+}
