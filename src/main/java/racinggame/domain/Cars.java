36146681d79f894b37eeb8557d2e824bbc9c5c5f
JaeHan-Kim <hgal8877@gmail.com>
2021-11-09 00:56:35 +0900

    feat(racinggame) : 자동차들의 일급 컬렉션 Cars 추가

---
+++ src/main/java/racinggame/domain/Cars.java
@@ -0,0 +1,37 @@
+package racinggame.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Cars {
+
+    private static final Integer START_INDEX = 0;
+    private static final Integer MAX_MOVE_VALUE = 9;
+
+    private final List<Car> cars;
+    private final Random random;
+
+    public Cars(Integer carCount) {
+        random = new Random();
+        cars = IntStream.range(START_INDEX, carCount)
+                .mapToObj((idx) -> new Car())
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public void roundRacing() {
+        for (Car car : cars) {
+            car.moveCar(random.nextInt(MAX_MOVE_VALUE));
+        }
+    }
+
+    public List<Integer> getRacingHistory(int round) {
+        List<Integer> records = new ArrayList<>();
+        for (Car car : cars) {
+            records.add(car.getRecord(round));
+        }
+        return records;
+    }
+}
