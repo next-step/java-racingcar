9ad52f20710bf9ce3e95739aae6f9e6733b45b83
alsqhd <kiryuinh32@gmail.com>
2021-11-08 12:05:04 +0900

    refactor: position print 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,8 +3,6 @@ package racingcar.view;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
-import java.util.stream.IntStream;
-
 public class OutputView {
     private static final String CAR_COUNT_EXCEPTIONM_ESSAGE = "자동차 생성 숫자는 0 이상 입력";
     private static final String TRY_COUNT_EXCEPTIONM_ESSAGE = "시도할 회수는 0 이상 입력";
@@ -27,7 +25,9 @@ public class OutputView {
     }
 
     private static void printCarPosition(Car car) {
-        IntStream.rangeClosed(0, car.getPosition()).mapToObj(value -> MOVE_SYMBOL).forEach(System.out::print);
+        for (int i = 0; i < car.positionValue(); i++) {
+            System.out.print(MOVE_SYMBOL);
+        }
         System.out.println();
     }
 }
