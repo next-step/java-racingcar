60716d872f714f4b5ef5356b1a925befb1b1730b
hyenny <qlwb9191@naver.com>
2021-11-08 18:18:37 +0900

    feat: 문자열 계산기 테스트 케이스 정리

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,39 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    void 덧셈() {
+
+    }
+
+    @Test
+    void 뺄셈() {
+
+    }
+
+    @Test
+    void 곱셈() {
+
+    }
+
+    @Test
+    void 나눗셈() {
+
+    }
+
+    @DisplayName("입력값이 NULL 또는 빈 공백이면 예외 발생")
+    @Test
+    void 입력값_널또는공백() {
+
+    }
+
+    @DisplayName("사칙연산 기호가 아닌 경우 예외 발생")
+    @Test
+    void 입력값_사칙연산기호() {
+
+    }
+
+}
