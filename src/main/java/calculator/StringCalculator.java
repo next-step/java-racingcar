52299dc1b5d20a4ae4e5dee331a6785a920c84e5
Ordi <rhkrgns56@naver.com>
2021-11-09 02:12:41 +0900

    2단계 - 문자열 계산기 (#2797)
    * refactor: step1 코드 패키지 이동
    
    * refactor: step1 요구사항3 ParameterizedTest 아규먼트 타입 변경
    
    * feat: 2단계 - 문자열 계산기 테스트 및 코드 추가
    
    * test: 사칙연산 예외 기호 테스트 추가
    
    * test: 공백, null 입력 예외 테스트 추가
    
    * refactor: 필요없는 패키지 삭제

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,44 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StringCalculator {
+
+    private StringCalculator() { }
+
+    public static long calculate(String expression) {
+        if (Objects.isNull(expression) || expression.equals(""))
+            throw new IllegalArgumentException("공백 or null 표현은 계산이 불가능합니다.");
+
+        List<String> items = Arrays.asList(expression.split(" "));
+        List<Long> operands = getOperands(items);
+        List<String> operatorSymbols = getOperatorSymbols(items);
+
+        Long result = operands.get(0);
+        for (int i = 1; i < operands.size(); i++) {
+            BinomialExpression binomialExpression =
+                    new BinomialExpression(result, operands.get(i), operatorSymbols.get(i - 1));
+            result = binomialExpression.calculate();
+        }
+        return result;
+    }
+
+    private static List<Long> getOperands(List<String> items) {
+        return IntStream.range(0, items.size())
+                .filter(i -> i % 2 == 0)
+                .mapToObj(items::get)
+                .map(Long::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> getOperatorSymbols(List<String> items) {
+        return IntStream.range(0, items.size())
+                .filter(i -> i % 2 == 1)
+                .mapToObj(items::get)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,76 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayName("2단계 - 문자열 계산기")
+public class StringCalculatorTest {
+
+    @Test
+    @DisplayName("덧셈")
+    void add() {
+        String expression = "1 + 2 + 7";
+        assertThat(StringCalculator.calculate(expression))
+                .isEqualTo(10L);
+    }
+
+    @Test
+    @DisplayName("뺄셈")
+    void subtract() {
+        String expression = "6 - 7 - -2";
+        assertThat(StringCalculator.calculate(expression))
+                .isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("곱하기")
+    void multiply() {
+        String expression = "2 * 6 * 2";
+        assertThat(StringCalculator.calculate(expression))
+                .isEqualTo(24L);
+    }
+
+    @Test
+    @DisplayName("나누기")
+    void divide() {
+        String expression = "12 / 2 / 3";
+        assertThat(StringCalculator.calculate(expression))
+                .isEqualTo(2L);
+    }
+
+    @Test
+    @DisplayName("나누기 - 0으로 나누기")
+    void dividedByZero() {
+        String expression = "6 / 0";
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            StringCalculator.calculate(expression);
+        });
+    }
+
+    @Test
+    @DisplayName("여러 사칙 연산")
+    void calculateLongString() {
+        String expression = "7 + 1 - -10 * 2 / 3";
+        assertThat(StringCalculator.calculate(expression))
+                .isEqualTo(12L);
+    }
+
+    @Test
+    @DisplayName("null 입력 예외")
+    void nullExpression() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            StringCalculator.calculate(null);
+        });
+    }
+
+    @Test
+    @DisplayName("공백 입력 예외")
+    void emptyExpression() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            StringCalculator.calculate("");
+        });
+    }
+}
