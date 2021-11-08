c565497e5d9b94b38925bb465d466326be096dd8
hyenny <qlwb9191@naver.com>
2021-11-09 00:13:11 +0900

    feat: 계산 기능 구현 및 리팩토링

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,52 @@
 package calculator;
 
-public class Calculator {
+import java.util.LinkedList;
+import java.util.Queue;
 
-    public static int calculate(int num1, int num2, String operator) {
-        return Operator.calculate(operator, num1, num2);
+public class Calculator {
+    private String[] inputs;
+    private Queue<String> expressions = new LinkedList<>();
+
+    public Calculator(String input) {
+        validate(input);
+        this.inputs = input.split(" ");
+    }
+
+    private void validate(String input) {
+        if (isEmpty(input)) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+    }
+
+    private boolean isEmpty(String input) {
+        return input == null || input.length() == 0;
+    }
+
+    public int calculate() {
+        int result = 0;
+        // 값을 큐에 넣으면서 계산
+        for (int i = 0; i < inputs.length; i++) {
+            expressions.offer(inputs[i]);
+            if (expressions.size() == 3) {
+                String num1 = expressions.poll();
+                String op = expressions.poll();
+                String num2 = expressions.poll();
+                expressions.offer(calculate(op, num1, num2));
+            }
+        }
+
+        // 마지막 계산
+        while (!expressions.isEmpty() && expressions.size() == 3) {
+            String num1 = expressions.poll();
+            String op = expressions.poll();
+            String num2 = expressions.poll();
+            result = Integer.parseInt(calculate(op, num1, num2));
+        }
+        return (result == 0) ? Integer.parseInt(expressions.poll()) : result;
+    }
+
+    private String calculate(String operator, String operand1, String operand2) {
+        return String.valueOf(Operator.calculate(operator, Integer.parseInt(operand1), Integer.parseInt(operand2)));
     }
 
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,31 +12,32 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CalculatorTest {
 
-    @Test
-    void 덧셈() {
-        assertThat(Calculator.calculate(3, 5, "+")).isEqualTo(8);
+    @ParameterizedTest
+    @CsvSource(value = {"1: 1", "3 + 5: 8", "3 + 5 + 6: 14"}, delimiter = ':')
+    void 덧셈(String input, int expected) {
+        assertThat(new Calculator(input).calculate()).isEqualTo(expected);
     }
 
     @Test
     void 뺄셈() {
-        assertThat(Calculator.calculate(5, 3, "-")).isEqualTo(2);
+        assertThat(new Calculator("10 - 5 - 2").calculate()).isEqualTo(3);
     }
 
     @Test
     void 곱셈() {
-        assertThat(Calculator.calculate(5, 3, "*")).isEqualTo(15);
+        assertThat(new Calculator("10 * 5 * 2 * 1").calculate()).isEqualTo(100);
     }
 
     @Test
     void 나눗셈() {
-        assertThat(Calculator.calculate(10, 2, "/")).isEqualTo(5);
+        assertThat(new Calculator("10 / 5").calculate()).isEqualTo(2);
     }
 
     @DisplayName("나눗셈 결과가 정수가 아니면 예외 발생")
     @Test
     void 나눗셈_결과는정수() {
         assertThatThrownBy(() -> {
-            Calculator.calculate(5, 10, "/");
+            new Calculator("5 / 10").calculate();
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("결과값은 정수만 허용");
     }
@@ -44,7 +46,7 @@ public class CalculatorTest {
     @Test
     void 나눗셈_0() {
         assertThatThrownBy(() -> {
-            Calculator.calculate(10, 0, "/");
+            new Calculator("10 / 2 / 0").calculate();
         }).isInstanceOf(ArithmeticException.class)
                 .hasMessageContaining("by zero");
     }
@@ -54,7 +56,7 @@ public class CalculatorTest {
     @NullAndEmptySource
     void 입력값_널또는공백(String input) {
         assertThatThrownBy(() -> {
-            MyNumber myNumber = new MyNumber(input);
+            new Calculator(input).calculate();
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력값이 없습니다");
     }
@@ -64,7 +66,7 @@ public class CalculatorTest {
     @ValueSource(strings = {"3 ( 5", "3 + 5 = 1", "5 $ 1"})
     void 입력값_사칙연산기호(String input) {
         assertThatThrownBy(() -> {
-            MyNumber myNumber = new MyNumber(input);
+            new Calculator(input).calculate();
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("사칙연산자가 아닙니다.");
     }
