bdd4f9f975e86b6b1cf50f9688437af8c9ea1755
michael.10 <michael.10@kakaocorp.com>
2021-11-09 00:24:48 +0900

    else 문 쓰지 않도록 리팩토링

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -15,14 +15,16 @@ public class Calculator {
 
         if ("+".equals(operator)) {
             return first + second;
-        } else if ("-".equals(operator)) {
-            return  first - second;
-        } else if ("*".equals(operator)) {
-            return first * second;
-        } else if ("/".equals(operator)) {
-            return first / second;
-        } else {
-            throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
         }
+        if ("-".equals(operator)) {
+            return first - second;
+        }
+        if ("*".equals(operator)) {
+            return first * second;
+        }
+        if ("/".equals(operator)) {
+            return first / second;
+        }
+        throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
     }
 }
