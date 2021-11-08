6ad5ed151fdd2f5f0a864761f03323877dd5726d
JaeHan-Kim <hgal8877@gmail.com>
2021-11-08 23:34:41 +0900

    feat(racinggame) : 자동차의 위치를 저장하는 도메인 추가

---
+++ src/main/java/racinggame/domain/Location.java
@@ -0,0 +1,49 @@
+package racinggame.domain;
+
+import java.util.Objects;
+
+public class Location implements Comparable<Location> {
+    public static final int START_LOCATION = 0;
+
+    private int location;
+
+    public Location() {
+        this(START_LOCATION);
+    }
+
+    public Location(int location) {
+        this.location = location;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void moveLocation() {
+        location++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location1 = (Location) o;
+        return location == location1.location;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(location);
+    }
+
+    @Override
+    public int compareTo(Location o) {
+        if (this.getLocation() > o.getLocation()) {
+            return 1;
+        }
+        if (this.getLocation() == o.getLocation()) {
+            return 0;
+        }
+        return -1;
+    }
+}
