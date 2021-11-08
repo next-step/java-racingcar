716c04e24c6edad3446fa5f09d6b0446305ac802
hyenny <qlwb9191@naver.com>
2021-11-08 19:12:28 +0900

    feat: 0 나누면 예외 케이스 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CalculatorTest {
 
@@ -27,16 +28,23 @@ public class CalculatorTest {
         assertThat(Calculator.divide(10,2)).isEqualTo(5);
     }
 
+    @DisplayName("0으로 나누면 예외 발생")
+    @Test
+    void 나눗셈_0() {
+        assertThatThrownBy(() -> {
+            Calculator.divide(10,0);
+        }).isInstanceOf(ArithmeticException.class)
+        .hasMessageContaining("by zero");
+    }
+
     @DisplayName("입력값이 NULL 또는 빈 공백이면 예외 발생")
     @Test
     void 입력값_널또는공백() {
-
     }
 
     @DisplayName("사칙연산 기호가 아닌 경우 예외 발생")
     @Test
     void 입력값_사칙연산기호() {
-
     }
 
 }
