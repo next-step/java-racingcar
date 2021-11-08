e5b49673ae82c78af7e1926ffa52116538d29bfe
michael.10 <michael.10@kakaocorp.com>
2021-11-08 23:56:20 +0900

    덧셈 기능이 더 많은 케이스를 통과할 수 있도록 리팩토링

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -1,7 +1,12 @@
 package step2;
 
+import java.util.List;
+
 public class Calculator {
     public static int calculate(String expression) {
-        return 3;
+        String[] elements = expression.split(" ");
+        int first = Integer.parseInt(elements[0]);
+        int second = Integer.parseInt(elements[2]);
+        return first + second;
     }
 }
