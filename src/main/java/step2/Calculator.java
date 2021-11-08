f94868e486226721176d145c015f7da78694380d
michael.10 <michael.10@kakaocorp.com>
2021-11-09 00:23:48 +0900

    Null or empty 체크에 대하여 if문 하나만 쓰도록 리팩토링

--- src/main/java/step2/Calculator.java
+++ src/main/java/step2/Calculator.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 public class Calculator {
     public static int calculate(String expression) {
-        if (expression == null) {
-            throw new IllegalArgumentException("빈 문자열 혹은 null은 입력할 수 없습니다.");
-        }
-        if (expression.trim().length() == 0) {
+        if (expression == null || expression.trim().length() == 0) {
             throw new IllegalArgumentException("빈 문자열 혹은 null은 입력할 수 없습니다.");
         }
 
