5806ecf24cf17c89f003f29107e9e45ac698d710
michael.10 <michael.10@kakaocorp.com>
2021-11-08 23:51:00 +0900

    기능 정의

---
+++ src/test/java/step2/StringCalculatorTest.java
@@ -0,0 +1,40 @@
+package step2;
+
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+    @Test
+    void plus() {
+
+    }
+
+    @Test
+    void minus() {
+
+    }
+
+    @Test
+    void times() {
+
+    }
+
+    @Test
+    void divideBy() {
+
+    }
+
+    @Test
+    void nullOrBlankInput() {
+
+    }
+
+    @Test
+    void illegalOperationChar() {
+
+    }
+
+    @Test
+    void multipleOperation() {
+
+    }
+}
