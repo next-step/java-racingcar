d62002142516e8e451d35ab2985b8dbfae60ea13
kangyonghyun <hmoon826@gmail.com>
2021-11-08 14:22:42 +0900

    (test) 4단계 - 자동차 이름을 쉼표를 기준으로 구분 테스트 수정

--- src/test/java/racingcar/step4/RacingGameTest.java
+++ src/test/java/racingcar/step4/RacingGameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.step4.config.MoveConfig;
 import racingcar.step4.domain.Cars;
@@ -26,18 +27,17 @@ public class RacingGameTest {
     @ValueSource(ints = {0, -1})
     @DisplayName("값은 0 이하 값이 들어오면 안된다")
     void inputValidation(int value) {
-        assertThatThrownBy(() -> new RacingGame("K3", value, 3))
+        assertThatThrownBy(() -> new RacingGame("K3,k5", value, 2))
                 .isInstanceOf(IllegalArgumentException.class);
         //값 정상
-        assertThatCode(() -> new RacingGame("K3", 1, 3))
+        assertThatCode(() -> new RacingGame("K3,k5", 1, 2))
                 .doesNotThrowAnyException();
     }
 
     @Test
     @DisplayName("랜덤값 - 레이스 테스트")
     void race() {
-        Cars cars = new Cars("K3", 3 );
-        RacingGame racingGame = new RacingGame("K3", 10, 3);
+        RacingGame racingGame = new RacingGame("K3,k5", 10, 2);
         racingGame.race(moving);
         assertThat(racingGame.getCountOfTry()).isEqualTo(9);
         assertThat(racingGame.isEndGame()).isFalse();
@@ -46,8 +46,7 @@ public class RacingGameTest {
     @Test
     @DisplayName("랜덤값 - 종료 테스트")
     void endGame() {
-        Cars cars = new Cars("K3", 3);
-        RacingGame racingGame = new RacingGame("K3", 10, 3);
+        RacingGame racingGame = new RacingGame("K3,k5", 10, 2);
         for (int i = 0; i < 10; i++) {
             racingGame.race(moving);
         }
