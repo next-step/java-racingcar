a4defe4aadc0d9927a6c8ccbd355aeb9c85a389b
Chung Beom Cho <cndqjacndqja@gmail.com>
2021-11-08 22:37:57 +0900

    Calculator 계산 메서드 추가

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -1,10 +1,39 @@
 package study;
 
 public class Calculator {
+    int num = 0;
+    String[] input;
 
+    public Calculator(String input) {
+        this.input = input.split(" ");
+    }
+
+    public int calculation() {
+        num = Integer.parseInt(input[0]);
+        for (int i = 1; i < input.length; i += 2) {
+            int operatorNum = Integer.parseInt(input[i + 1]);
+            num = operator(operatorNum, input[i]);
+        }
+        return num;
+    }
+
+    private int operator(int operatorNum, String operator) {
+        switch (operator) {
+            case "+":
+                return plus(num, operatorNum);
+            case "-":
+                return minus(num, operatorNum);
+            case "*":
+                return multiplied(num, operatorNum);
+            case "/":
+                return divide(num, operatorNum);
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
 
     public int plus(int a, int b) {
-        return a+b;
+        return a + b;
     }
 
     public int minus(int a, int b) {
--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
-    Calculator calculator = new Calculator();
+    Calculator calculator = new Calculator("2 + 3 * 4 / 2");
 
     @Test
     void plus() {
@@ -24,6 +24,11 @@ public class CalculatorTest {
 
     @Test
     void divide() {
-        assertThat(calculator.divide(4, 2)).isEqualTo(6);
+        assertThat(calculator.divide(4, 2)).isEqualTo(2);
+    }
+
+    @Test
+    void operator() {
+        assertThat(calculator.calculation()).isEqualTo(10);
     }
 }
