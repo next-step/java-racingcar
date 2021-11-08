a95a11fc65e08db639a708031adf2acfeb2494b6
Han <hankim@tapasmedia.co>
2021-11-09 01:12:10 +0900

    test : add indexCarsAndResultProvider()

--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -2,10 +2,13 @@ package racing;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,6 +17,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
  * @author han
  */
 class RacingTest {
+
+    class DeterministicRandom extends Random {
+        int sequentialNum = 0;
+
+        public DeterministicRandom() {
+            super();
+        }
+
+        @Override
+        public int nextInt(int input) {
+            return sequentialNum++;
+        }
+    }
 
     @Test
     @DisplayName(value = "생성")
@@ -37,37 +53,15 @@ class RacingTest {
         cars.forEach(car -> assertThat(car.getStep()).isGreaterThanOrEqualTo(0));
     }
 
-    @Test
-    @DisplayName(value = "예상된 랜덤값을 통한 플레이")
-    void playWithExpectedRandomValue() {
-        Racing racing = new Racing(3, 5);
-        List<Car> cars = racing.getCars();
-        int attempts = racing.getAttempts();
 
-        assertThat(cars.size()).isEqualTo(3);
-        assertThat(attempts).isEqualTo(5);
-
+    @ParameterizedTest(name = "예상된 랜덤값을 통한 플레이")
+    @MethodSource("indexCarsAndResultProvider")
+    void playWithExpectedRandomValue(int i, int j, int k) {
+        Racing racing = new Racing(3, 3);
         racing.play(new DeterministicRandom());
 
-        Map<Integer, List<Car>> logs = racing.getLogs();
-
-        List<Car> cars1 = logs.get(0);
-
-        assertThat(cars1.get(0).getStep()).isEqualTo(0);
-        assertThat(cars1.get(1).getStep()).isEqualTo(0);
-        assertThat(cars1.get(2).getStep()).isEqualTo(0);
-
-        List<Car> cars2 = logs.get(1);
-
-        assertThat(cars2.get(0).getStep()).isEqualTo(0);
-        assertThat(cars2.get(1).getStep()).isEqualTo(0);
-        assertThat(cars2.get(2).getStep()).isEqualTo(1);
-
-        List<Car> cars3 = logs.get(2);
-
-        assertThat(cars3.get(0).getStep()).isEqualTo(1);
-        assertThat(cars3.get(1).getStep()).isEqualTo(1);
-        assertThat(cars3.get(2).getStep()).isEqualTo(2);
+        List<Car> list = racing.getLogs().get(i);
+        assertThat(list.get(j).getStep()).isEqualTo(k);
     }
 
     @Test
@@ -78,16 +72,17 @@ class RacingTest {
         assertThatThrownBy(() -> new Racing(0, 0)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    class DeterministicRandom extends Random {
-        int sequentialNum = 0;
-
-        public DeterministicRandom() {
-            super();
-        }
-
-        @Override
-        public int nextInt(int input) {
-            return sequentialNum++;
-        }
+    static Stream<Arguments> indexCarsAndResultProvider() {
+        return Stream.of(
+            Arguments.arguments(0,0,0),
+            Arguments.arguments(0,1,0),
+            Arguments.arguments(0,2,0),
+            Arguments.arguments(1,0,0),
+            Arguments.arguments(1,1,0),
+            Arguments.arguments(1,2,1),
+            Arguments.arguments(2,0,1),
+            Arguments.arguments(2,1,1),
+            Arguments.arguments(2,2,2)
+        );
     }
 }
