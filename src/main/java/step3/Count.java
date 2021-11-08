ea3ea870c92bb1048a04203c35fcdc5a3e152a9f
kero <jdy410@naver.com>
2021-11-09 01:57:19 +0900

    feat: 횟수를 위한 Count 클래스 작성

---
+++ src/main/java/step3/Count.java
@@ -0,0 +1,18 @@
+package step3;
+
+public class Count {
+
+    private int count;
+
+    public Count(Integer input) {
+        this.count = input;
+    }
+
+    public void plusCount() {
+        this.count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
