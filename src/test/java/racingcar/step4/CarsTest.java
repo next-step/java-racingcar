8bd42e7ef142382c5ff7ec8d2333e7cc84aabfb7
kangyonghyun <hmoon826@gmail.com>
2021-11-08 13:39:47 +0900

    (test) 4단계 - 자동차 이름 받는 컨트롤러 및 뷰 테스트 수정

--- src/test/java/racingcar/step4/CarsTest.java
+++ src/test/java/racingcar/step4/CarsTest.java
@@ -3,7 +3,7 @@ package racingcar.step4;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.step3.domain.Cars;
+import racingcar.step4.domain.Cars;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,10 +14,10 @@ public class CarsTest {
     @ValueSource(ints = {0, -1})
     @DisplayName("값은 0 이하 값이 들어오면 안된다")
     void inputValidation(int value) {
-        assertThatThrownBy(() -> new Cars(value))
+        assertThatThrownBy(() -> new Cars("K3", value))
                 .isInstanceOf(IllegalArgumentException.class);
         //값 정상
-        assertThatCode(() -> new Cars(1))
+        assertThatCode(() -> new Cars("K3", 1))
                 .doesNotThrowAnyException();
     }
 
--- src/test/java/racingcar/step4/RacingGameTest.java
+++ src/test/java/racingcar/step4/RacingGameTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.step3.config.MoveConfig;
-import racingcar.step3.domain.Cars;
-import racingcar.step3.move.Moving;
-import racingcar.step3.service.RacingGame;
+import racingcar.step4.config.MoveConfig;
+import racingcar.step4.domain.Cars;
+import racingcar.step4.move.Moving;
+import racingcar.step4.service.RacingGame;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -26,18 +26,18 @@ public class RacingGameTest {
     @ValueSource(ints = {0, -1})
     @DisplayName("값은 0 이하 값이 들어오면 안된다")
     void inputValidation(int value) {
-        assertThatThrownBy(() -> new RacingGame(value, 3))
+        assertThatThrownBy(() -> new RacingGame("K3", value, 3))
                 .isInstanceOf(IllegalArgumentException.class);
         //값 정상
-        assertThatCode(() -> new RacingGame(1, 3))
+        assertThatCode(() -> new RacingGame("K3", 1, 3))
                 .doesNotThrowAnyException();
     }
 
     @Test
     @DisplayName("랜덤값 - 레이스 테스트")
     void race() {
-        Cars cars = new Cars(3);
-        RacingGame racingGame = new RacingGame(10, 3);
+        Cars cars = new Cars("K3", 3 );
+        RacingGame racingGame = new RacingGame("K3", 10, 3);
         racingGame.race(moving);
         assertThat(racingGame.getCountOfTry()).isEqualTo(9);
         assertThat(racingGame.isEndGame()).isFalse();
@@ -46,8 +46,8 @@ public class RacingGameTest {
     @Test
     @DisplayName("랜덤값 - 종료 테스트")
     void endGame() {
-        Cars cars = new Cars(3);
-        RacingGame racingGame = new RacingGame(10, 3);
+        Cars cars = new Cars("K3", 3);
+        RacingGame racingGame = new RacingGame("K3", 10, 3);
         for (int i = 0; i < 10; i++) {
             racingGame.race(moving);
         }
