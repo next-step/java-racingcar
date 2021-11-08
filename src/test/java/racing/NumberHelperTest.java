7c7698ad840f561e78e8f4981bf4ccbf7a9f46ac
Han <hankim@tapasmedia.co>
2021-11-08 21:35:34 +0900

    test : add randomIntegerListProvider

--- src/test/java/racing/NumberHelperTest.java
+++ src/test/java/racing/NumberHelperTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,10 +16,13 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class NumberHelperTest {
 
-    NumberHelper numberHelper;
+    @ParameterizedTest
+    @MethodSource("randomIntegerListProvider")
+    void getRandomValue(int random) {
+        assertThat(random).isLessThan(10);
+    }
 
-    @Test
-    void getRandomValue() {
-        assertThat(numberHelper.getRandomValue()).isLessThan(10);
+    static Stream<Integer> randomIntegerListProvider() {
+        return IntStream.range(0, 100).mapToObj(j -> NumberHelper.getRandomValue());
     }
 }
