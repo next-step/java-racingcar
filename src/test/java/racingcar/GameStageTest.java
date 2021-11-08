814edce0ac093a66c1b5350b97590837c8685e6e
giyeon95 <giyeon15@gmail.com>
2021-11-09 01:11:42 +0900

    test 자동차 생성 및 input 라운드 검증 테스트 코드 정의

---
+++ src/test/java/racingcar/GameStageTest.java
@@ -0,0 +1,46 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.lang.reflect.Field;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class GameStageTest {
+
+
+    @ValueSource(ints = {3, 5, 10})
+    @ParameterizedTest(name = "{arguments}개의 참가 자동차가 생성되어야 한다.")
+    void initCarTest(int numberOfCars) throws NoSuchFieldException, IllegalAccessException {
+        GameStage gameStage = GameStage.start(10, numberOfCars);
+        Car[] cars = getCarsByReflection(gameStage);
+
+        assertThat(cars).hasSize(numberOfCars);
+    }
+
+
+    @ValueSource(ints = {0, -1})
+    @ParameterizedTest(name = "자동차 생성 대수가 {arguments}인 경우, IllegalArgumentException이 발생한다.")
+    void initCarIllegalArgumentExceptionTest(int numberOfCars) {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> GameStage.start(10, numberOfCars));
+    }
+
+    @ValueSource(ints = -5)
+    @ParameterizedTest(name = "시도할 횟수가 {arguments}인 경우, IllegalArgumentException이 발생한다.")
+    void initRoundIllegalArgumentExceptionTest(int numberOfRound) {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> GameStage.start(numberOfRound, 1));
+    }
+
+    private Car[] getCarsByReflection(GameStage gameStage)
+        throws NoSuchFieldException, IllegalAccessException {
+        Field carsField = GameStage.class.getDeclaredField("cars");
+        carsField.setAccessible(true);
+
+        return (Car[]) carsField.get(gameStage);
+    }
+
+
+}
