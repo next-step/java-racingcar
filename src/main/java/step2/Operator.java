b48ccf52a11b07412bf409ba0de6c94ac954b557
guswnt <guswntj1277@gmail.com>
2021-11-09 03:25:30 +0900

    코드리뷰 반영

--- src/main/java/step2/Operator.java
+++ src/main/java/step2/Operator.java
@@ -2,36 +2,23 @@ package step2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BinaryOperator;
 
 public enum Operator {
 
-    PLUS("+") {
-        int operate(int a, int b) {
-            return a + b;
-        }
+    PLUS("+", Operand::plus),
+    MINUS("-", Operand::minus),
+    TIMES("*", Operand::times),
+    DIVISION("/", Operand::dividedBy);
 
-    }, MINUS("-") {
-        int operate(int a, int b) {
-            return a - b;
-        }
 
-    }, TIMES("*") {
-        int operate(int a, int b) {
-            return a * b;
-        }
-
-    }, DIVISION("/") {
-        int operate(int a, int b) {
-            return a / b;
-        }
-
-    };
-
-    Operator(String op) {
+    Operator(String op, BinaryOperator<Operand> bo) {
         this.op = op;
+        this.bo = bo;
     }
 
     private final String op;
+    private final BinaryOperator<Operand> bo;
     private static final Map<String, Operator> map;
 
     static {
@@ -41,10 +28,15 @@ public enum Operator {
         }
     }
 
-    abstract int operate(int a, int b);
+    Operand operate(Operand a, Operand b) {
+        return bo.apply(a, b);
+
+    }
 
     public static Operator from(String op) {
-        if (!map.containsKey(op)) throw new IllegalArgumentException();
+        if (!map.containsKey(op)) {
+            throw new IllegalArgumentException();
+        }
         return map.get(op);
     }
 
