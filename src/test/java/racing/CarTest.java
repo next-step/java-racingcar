50dfc26edb437626f025e4a7b972be6a0c1069dd
Han <hankim@tapasmedia.co>
2021-11-08 22:36:08 +0900

    style : remove unused imports

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
