480a5d50cd97e71666356ee449af2456382265c8
hungrytech <xorals9448@gmail.com>
2021-11-09 00:32:10 +0900

    refactor : 생성자 위치 변경

--- src/main/java/racinggame/utils/MoveValueValidator.java
+++ src/main/java/racinggame/utils/MoveValueValidator.java
@@ -2,10 +2,10 @@ package racinggame.utils;
 
 public class MoveValueValidator {
 
+    private static final int ADVANCED_CONDITION = 4;
+
     private MoveValueValidator() {
     }
-
-    private static final int ADVANCED_CONDITION = 4;
 
     public static boolean validate(int value) {
         return value >= ADVANCED_CONDITION;
