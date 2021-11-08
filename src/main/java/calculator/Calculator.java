f407a538cd7f7e097aadf443d1515a44155902ed
hyenny <qlwb9191@naver.com>
2021-11-08 21:11:08 +0900

    refactor: 연산 calculate로 통일

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,19 +2,37 @@ package calculator;
 
 public class Calculator {
 
-    public static int plus(int num1, int num2) {
+    public static int calculate(int num1, int num2, String operator) {
+        switch (operator) {
+            case "+":
+                return plus(num1, num2);
+            case "-":
+                return minus(num1, num2);
+            case "*":
+                return times(num1, num2);
+            case "/":
+                return divide(num1, num2);
+            default:
+                throw new IllegalArgumentException("사칙연산자가 아닙니다.");
+        }
+    }
+
+    private static int plus(int num1, int num2) {
         return num1 + num2;
     }
 
-    public static int minus(int num1, int num2) {
+    private static int minus(int num1, int num2) {
         return num1 - num2;
     }
 
-    public static int times(int num1, int num2) {
+    private static int times(int num1, int num2) {
         return num1 * num2;
     }
 
-    public static int divide(int num1, int num2) {
+    private static int divide(int num1, int num2) {
+        if (num1 < num2) {
+            throw new IllegalArgumentException("나눗셈 결과값은 정수만 허용합니다.");
+        }
         return num1 / num2;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -13,31 +13,40 @@ public class CalculatorTest {
 
     @Test
     void 덧셈() {
-        assertThat(Calculator.plus(3, 5)).isEqualTo(8);
+        assertThat(Calculator.calculate(3, 5, "+")).isEqualTo(8);
     }
 
     @Test
     void 뺄셈() {
-        assertThat(Calculator.minus(5, 3)).isEqualTo(2);
+        assertThat(Calculator.calculate(5, 3, "-")).isEqualTo(2);
     }
 
     @Test
     void 곱셈() {
-        assertThat(Calculator.times(5,3)).isEqualTo(15);
+        assertThat(Calculator.calculate(5, 3, "*")).isEqualTo(15);
     }
 
     @Test
     void 나눗셈() {
-        assertThat(Calculator.divide(10,2)).isEqualTo(5);
+        assertThat(Calculator.calculate(10, 2, "/")).isEqualTo(5);
+    }
+
+    @DisplayName("나눗셈 결과가 정수가 아니면 예외 발생")
+    @Test
+    void 나눗셈_결과는정수() {
+        assertThatThrownBy(() -> {
+            Calculator.calculate(5, 10, "/");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("결과값은 정수만 허용");
     }
 
     @DisplayName("0으로 나누면 예외 발생")
     @Test
     void 나눗셈_0() {
         assertThatThrownBy(() -> {
-            Calculator.divide(10,0);
+            Calculator.calculate(10, 0, "/");
         }).isInstanceOf(ArithmeticException.class)
-        .hasMessageContaining("by zero");
+                .hasMessageContaining("by zero");
     }
 
     @DisplayName("입력값이 NULL 또는 빈 공백이면 예외 발생")
@@ -47,7 +56,7 @@ public class CalculatorTest {
         assertThatThrownBy(() -> {
             MyNumber myNumber = new MyNumber(input);
         }).isInstanceOf(IllegalArgumentException.class)
-        .hasMessageContaining("입력값이 없습니다");
+                .hasMessageContaining("입력값이 없습니다");
     }
 
     @DisplayName("사칙연산 기호가 아닌 경우 예외 발생")
@@ -57,7 +66,7 @@ public class CalculatorTest {
         assertThatThrownBy(() -> {
             MyNumber myNumber = new MyNumber(input);
         }).isInstanceOf(IllegalArgumentException.class)
-        .hasMessageContaining("사칙연산자가 아닙니다.");
+                .hasMessageContaining("사칙연산자가 아닙니다.");
     }
 
 }
