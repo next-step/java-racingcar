877b70b2ec7feccd7b8210f0ed0ae8e88a3aabb6
Chung Beom Cho <cndqjacndqja@gmail.com>
2021-11-08 20:06:57 +0900

    Calculator 사칙연산 추가

---
+++ src/main/java/study/Calculator.java
@@ -0,0 +1,21 @@
+package study;
+
+public class Calculator {
+
+
+    public int plus(int a, int b) {
+        return a+b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplied(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/study/CalculatorTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void plus() {
+        assertThat(calculator.plus(1, 2)).isEqualTo(3);
+    }
+
+    @Test
+    void minus() {
+        assertThat(calculator.minus(2, 1)).isEqualTo(1);
+    }
+
+    @Test
+    void multiply() {
+        assertThat(calculator.multiplied(2, 3)).isEqualTo(6);
+    }
+
+    @Test
+    void divide() {
+        assertThat(calculator.divide(4, 2)).isEqualTo(6);
+    }
+}
