0a732aa55de50f5bf912501d832c34b5203b1127
user <kth4878@naver.com>
2021-11-09 20:48:34 +0900

    [Refactor] result 수정 및 우승자 추가

--- src/main/java/controller/CarRaceController.java
+++ src/main/java/controller/CarRaceController.java
@@ -1,17 +1,18 @@
 package controller;
 
-import domain.CarCount;
+import domain.CarGroup;
 import domain.CarRaceCount;
 import domain.CarRaceGroup;
 import view.InputView;
 import view.OutputView;
 
 public class CarRaceController {
-	public void run() {
-		CarCount carCount = new CarCount(InputView.inputRaceCarCount());
-		CarRaceCount carRaceCount = new CarRaceCount(InputView.inputCarRaceCount());
-		CarRaceGroup carRaceGroup = new CarRaceGroup(carCount, carRaceCount);
+    public void run() {
+        CarGroup carGroup = new CarGroup(InputView.inputRaceCarName());
+        CarRaceCount carRaceCount = new CarRaceCount(InputView.inputCarRaceCount());
+        CarRaceGroup carRaceGroup = new CarRaceGroup(carGroup, carRaceCount);
 
-		OutputView.result(carCount, carRaceGroup);
-	}
+        OutputView.result(carRaceGroup);
+        OutputView.winner(carRaceGroup);
+    }
 }
