7ab9844a53f00d25d30d8e614732975371bbf757
audgnssweet <audgnssweet@naver.com>
2021-11-09 15:12:35 +0900

    표준입력 완성

--- src/main/java/step3/CarRacingGame.java
+++ src/main/java/step3/CarRacingGame.java
@@ -6,6 +6,10 @@ public class CarRacingGame {
 
     public void start() throws IOException {
         GameOutput.println("자동차는 몇 대인가요?\n");
+        int numberOfCar = GameInput.readInt();
+        GameOutput.println("몇 번 시도할 것인가요?\n");
+        int numberOfTry = GameInput.readInt();
+
     }
 
     public static void main(String[] args) throws IOException {
