938aae2501584bac0b9bca1c61eb7d6202757229
mj950425 <minjoon1995@naver.com>
2021-11-09 09:23:09 +0900

    [fix] 변수명은 카멜케이스로 변경

--- src/main/java/step2/model/Calculator.java
+++ src/main/java/step2/model/Calculator.java
@@ -2,38 +2,15 @@ package step2.model;
 
 public class Calculator {
 
-    public static MyNumber operator(String str) {
-        String[] value = str.split(" ");
-        MyNumber result = new MyNumber(Integer.parseInt(value[0]));
-        for (int i = 1; i < value.length; i += 2) {
-            String operator = value[i];
-            MyNumber myNumber2 = new MyNumber(Integer.parseInt(value[i+1]));
-            result = calculator(result, myNumber2, operator);
+
+    public static Integer operator(String str) {
+        String[] stringArray = str.split(" ");
+        Integer result = new Integer(stringArray[0]);
+        for (int i = 1; i < stringArray.length; i += 2) {
+            String operator = stringArray[i];
+            Integer num1 = new Integer(stringArray[i+1]);
+            result = CalculatorEnum.findOperator(result, num1, operator);
         }
         return result;
     }
-
-    public static MyNumber calculator(MyNumber num1, MyNumber num2, String o) {
-        if (o.equals("+")) {
-            return num1.add(num2);
-        }
-        if (o.equals("-")) {
-            return num1.minus(num2);
-        }
-        if (o.equals("*")) {
-            return num1.multiply(num2);
-        }
-        if (o.equals("/")) {
-            valid(num2);
-            return num1.divide(num2);
-        }
-        throw new IllegalArgumentException();
-    }
-
-    public static void valid(MyNumber num2) {
-        if (num2.equals(new MyNumber(0))) {
-            throw new IllegalArgumentException("나눗셈에서 분모는 0이 될 수 없습니다");
-        }
-    }
-
 }
