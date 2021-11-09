5537d1d384e6c0e645bcf716d5e55fadf6829acb
mj950425 <minjoon1995@naver.com>
2021-11-09 12:46:26 +0900

    [refactoring] 람다식과 스트림에 exception 추가

--- src/main/java/step2/model/CalculatorEnum.java
+++ src/main/java/step2/model/CalculatorEnum.java
@@ -6,31 +6,25 @@ import java.util.function.BiFunction;
 public enum CalculatorEnum {
     ADD("+",(num1,num2) -> (num1 + num2)),
     MINUS("-",(num1,num2) -> (num1 - num2)),
-    DIVIDE("/",(num1,num2)-> (num1 / num2)),
-    MULTIPLY("*",(num1,num2)-> num1 * num2);
+    MULTIPLY("*",(num1,num2)-> num1 * num2),
+    DIVIDE("/",(num1,num2)-> {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("can't divide by zero");
+        }
+        return num1 / num2;
+    });
 
     private final BiFunction<Integer,Integer,Integer> expression;
     private final String operator;
-
-    public static Integer findOperator(Integer num1, Integer num2, String operator) {
-        return Arrays.stream(CalculatorEnum.values())
-                .filter(calculatorEnum -> calculatorEnum.operator.equals(operator))
-        .findAny()
-        .orElse(checkOperator(operator)).expression.apply(num1,num2);
-    }
 
     CalculatorEnum(String operator, BiFunction<Integer, Integer, Integer> expression){
         this.operator = operator;
         this.expression = expression;}
 
-    public Integer calculate(Integer num1,Integer num2){
-        return expression.apply(num1,num2); }
-
-    private static CalculatorEnum checkOperator(String operator) {
-        try {
-            throw new RuntimeException("invalid operator");
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
+    public static Integer findOperator(Integer num1, Integer num2, String operator) {
+        return Arrays.stream(values())
+                .filter(calculatorEnum -> calculatorEnum.operator.equals(operator))
+        .findAny()
+        .orElseThrow(() -> new IllegalArgumentException("invalid operator")).expression.apply(num1,num2);
     }
 }
