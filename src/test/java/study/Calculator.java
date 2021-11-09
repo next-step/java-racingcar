de380705496051fb735c91e84994b98c938d1b00
dodeon <sojeongw.official@gmail.com>
2021-11-09 11:57:00 +0900

    refactor: CalculatorTest에 DisplayName 추가

--- src/test/java/study/Calculator.java
+++ src/test/java/study/Calculator.java
@@ -1,43 +1,17 @@
 package study;
 
 public class Calculator {
+
     public static int calculate(String s) {
-        String[] values = s.split(" ");
-        int first = Integer.parseInt(values[0]);
-        int second = Integer.parseInt(values[2]);
-        String operator = values[1];
-
-        Integer result = calculate(first, second, operator);
-
-        return result;
-    }
-
-    private static int calculate(int first, int second, String operator) {
-        if("+".equals(operator)) {
-            return first + second;
-        }
-        if("-".equals(operator)) {
-            return first - second;
-        }
-        if("*".equals(operator)) {
-            return first * second;
-        }
-        if("/".equals(operator)) {
-            return first / second;
-        }
-        return 0;
-    }
-
-    public static int calculate2(String s) {
         String[] values = s.split(" ");
         InputNumber first = new InputNumber(values[0]);
         InputNumber second = new InputNumber(values[2]);
         String operator = values[1];
 
-        return calculate2(first, second, operator).getInputNumber();
+        return calculate(first, second, operator).getInputNumber();
     }
 
-    public static InputNumber calculate2(InputNumber first, InputNumber second, String operator) {
+    public static InputNumber calculate(InputNumber first, InputNumber second, String operator) {
         if("+".equals(operator)) {
             return first.add(second);
         }
--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,26 +8,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CalculatorTest {
 
     @Test
+    @DisplayName("주어진 문자열로 덧셈을 연산한다")
     void add() {
-        int result = Calculator.calculate2("1 + 2");
+        int result = Calculator.calculate("1 + 2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("주어진 문자열로 뺄셈을 연산한다")
     void subtract() {
-        int result = Calculator.calculate2("4 - 2");
+        int result = Calculator.calculate("4 - 2");
         assertThat(result).isEqualTo(2);
     }
 
     @Test
+    @DisplayName("주어진 문자열로 곱셈을 연산한다")
     void multiply() {
-        int result = Calculator.calculate2("5 * 4");
+        int result = Calculator.calculate("5 * 4");
         assertThat(result).isEqualTo(20);
     }
 
     @Test
+    @DisplayName("주어진 문자열로 나눗셈을 연산한다")
     void divide() {
-        int result = Calculator.calculate2("20 / 2");
+        int result = Calculator.calculate("20 / 2");
         assertThat(result).isEqualTo(10);
     }
 }
