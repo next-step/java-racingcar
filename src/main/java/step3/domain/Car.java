01aa82bc77f3c489fa4ec1e406f432b8fce4793d
doorisopen <doorwasopen@gmail.com>
2021-11-09 22:05:36 +0900

    feat: step4 자동차에 이름을 부여한다

--- src/main/java/step3/domain/Car.java
+++ src/main/java/step3/domain/Car.java
@@ -4,15 +4,19 @@ import step3.domain.history.CarHistory;
 import step3.service.IntNumberGenerator;
 
 public class Car {
+    private static final int NAME_MAX_LENGTH = 5;
     private static final int MOVE_CONDITION = 4;
+    private String name;
     private int position;
 
-    private Car() {
+    private Car(String name) {
+        validateNameLength(name);
+        this.name = name;
         this.position = 0;
     }
 
-    public static Car create() {
-        return new Car();
+    public static Car create(String name) {
+        return new Car(name);
     }
 
     public CarHistory move(IntNumberGenerator generator) {
@@ -22,11 +26,21 @@ public class Car {
         return new CarHistory(this);
     }
 
+    public String getName() {
+        return name;
+    }
+
     public int getPosition() {
         return this.position;
     }
 
     private boolean isMove(IntNumberGenerator generator) {
         return generator.generate() >= MOVE_CONDITION;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름의 길이는 5자를 초과할 수 없습니다.");
+        }
     }
 }
