9cc19360b1bdc940820862eb5e16b6226574c4ec
Lee Dongkyu <nelldklee@gmail.com>
2021-11-10 00:01:31 +0900

    docs(README): 기능 목록 작성 및 step3 시작

---
+++ src/main/java/step3/RacingSystem.java
@@ -0,0 +1,12 @@
+package step3;
+
+import java.util.Scanner;
+
+public class RacingSystem {
+
+    static void start() {
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+
+    }
+}
