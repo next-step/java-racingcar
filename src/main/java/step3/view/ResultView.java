c42eebf43cd1b5ddae758839f41e8d23d4b97317
doorisopen <doorwasopen@gmail.com>
2021-11-09 22:10:15 +0900

    feat: step4 자동차 이름 및 경주 우승자를 출력한다

--- src/main/java/step3/view/ResultView.java
+++ src/main/java/step3/view/ResultView.java
@@ -4,11 +4,16 @@ import step3.domain.history.CarHistory;
 import step3.domain.history.GameHistory;
 import step3.domain.history.RoundHistory;
 
+import java.util.List;
 import java.util.Map;
 
 public class ResultView {
+    private static final String WHITE_SPACE = " ";
+    private static final String COMMA = ",";
+    private static final String COLON = ":";
     private static final String DASH = "-";
     private static final String GAME_START_OUTPUT_MESSAGE = "실행 결과";
+    private static final String GAME_WINNER_OUTPUT_MESSAGE = "가 최종 우승했습니다.";
 
     public static void start() {
         System.out.println(GAME_START_OUTPUT_MESSAGE);
@@ -17,6 +22,12 @@ public class ResultView {
     public static void result(GameHistory history) {
         showGameHistory(history.getAll());
         System.out.println();
+        showGameWinner(history);
+    }
+
+    private static void showGameWinner(GameHistory history) {
+        List<String> gameWinner = history.getGameWinner();
+        System.out.println(String.join(COMMA + WHITE_SPACE, gameWinner) + GAME_WINNER_OUTPUT_MESSAGE);
     }
 
     private static void showGameHistory(Map<Integer, RoundHistory> history) {
@@ -27,13 +38,13 @@ public class ResultView {
     }
 
     private static void showRoundHistory(RoundHistory history) {
-        for (CarHistory carHistory : history.getRoundHistories()) {
+        for (CarHistory carHistory : history.getCarHistories()) {
             showCarPosition(carHistory);
         }
     }
 
     private static void showCarPosition(CarHistory carHistory) {
-        System.out.println(repeatDash(carHistory.getPosition()));
+        System.out.println(carHistory.getName() + WHITE_SPACE + COLON + WHITE_SPACE + repeatDash(carHistory.getPosition()));
     }
 
     private static String repeatDash(int position) {
