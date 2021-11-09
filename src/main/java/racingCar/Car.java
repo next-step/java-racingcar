c551f2b1d6a3b4228954909db506741acbd5593a
minchoi9509 <minchoi9509@gmail.com>
2021-11-09 23:04:08 +0900

    [fix] 자동차 MAX_NAME_LENGTH = 4 -> 5

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -4,14 +4,14 @@ import racingCar.utils.NumberUtils;
 
 public class Car {
     private static final int COMPARE_VALUE = 4;
-    private static final int MAX_NAME_LENGTH = 4;
+    private static final int MAX_NAME_LENGTH = 5;
 
     private String name;
     private int moveCount = 0;
 
     Car(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("차 이름은 5글자 미만으로 정해주세요.");
+            throw new IllegalArgumentException("차 이름은 5글자 이하로 정해주세요.");
         }
         this.name = name;
     }
