db96dd97d269c12ca5c3ff1b6fbb4e2bab442d6a
DaehunGwak <rhkrgns56@naver.com>
2021-11-10 02:58:57 +0900

    feat: 3단계 - 자동차 경주 - 플레이 가능한 게임 메인 추가

---
+++ src/main/java/racing/CarRacingMain.java
@@ -0,0 +1,11 @@
+package racing;
+
+import racing.controller.CarRacingController;
+
+public class CarRacingMain {
+
+    public static void main(String[] args) {
+        CarRacingController carRacingController = new CarRacingController();
+        carRacingController.start();
+    }
+}
