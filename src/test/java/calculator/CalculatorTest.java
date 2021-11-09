9cb5fb3e575c30cc7e17e1708d8c02bed07e712e
sjhello <sjhello235@gmail.com>
2021-11-10 00:40:29 +0900

    refactor: ParameterizedTest를 이용하여 테스트 간소화

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,45 +2,18 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.regex.Matcher;
-
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.RegularExpression;
 
 public class CalculatorTest {
 
-	@Test
-	@DisplayName("더하기 연산을 한다")
-	void add() {
-		InputNumber input = new InputNumber("1 + 3");
-		assertThat(Calculator.calculate(input)).isEqualTo(4);
-	}
-
-	@Test
-	@DisplayName("빼기 연산을 한다")
-	void minus() {
-		InputNumber input = new InputNumber("3 - 1");
-		assertThat(Calculator.calculate(input)).isEqualTo(2);
-	}
-
-	@Test
-	@DisplayName("곱셈 연산을 한다")
-	void times() {
-		InputNumber input = new InputNumber("2 * 4");
-		assertThat(Calculator.calculate(input)).isEqualTo(8);
-	}
-
-	@Test
-	@DisplayName("나눗셈 연산을 한다")
-	void divide() {
-		InputNumber input = new InputNumber("4 / 2");
-		assertThat(Calculator.calculate(input)).isEqualTo(2);
+	@ParameterizedTest
+	@DisplayName("덧셈, 뺼셈, 곱셈, 나눗셈을 테스트 한다")
+	@CsvSource(value = {"1 + 3:4", "3 - 1:2", "2 * 4:8", "4 / 2:2"}, delimiter = ':')
+	void calculateTest(String inputString, int result) {
+		InputNumber input = new InputNumber(inputString);
+		assertThat(Calculator.calculate(input)).isEqualTo(result);
 	}
 
 	@ParameterizedTest
