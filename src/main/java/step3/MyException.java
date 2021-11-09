5dbd658cc0dcce9d68b405db8737a007f984e27f
kero <jdy410@naver.com>
2021-11-10 00:12:24 +0900

    feat: 입력 가능한 범위가 아닐때 발생시키는 예외 메세지 추가

--- src/main/java/step3/MyException.java
+++ src/main/java/step3/MyException.java
@@ -3,7 +3,9 @@ package step3;
 public enum MyException {
 
     IS_NOT_NUMBER("정수가 아닙니다."),
-    VALUE_GREATER_THAN_ONE("1 이상의 값을 입력해주세요.");
+    VALUE_GREATER_THAN_ONE("1 이상의 값을 입력해주세요."),
+
+    VALID_VALUE_IS_ZERO_TO_NINE("가능한 범위는 0 ~ 9 입니다.");
 
     private final String message;
 
