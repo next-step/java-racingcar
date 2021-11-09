de48971537150c3f710bf80ca60d20b8c72210cc
chris.jeun <chris.jeun@kakaocorp.com>
2021-11-09 18:42:34 +0900

    1단계 학습 테스트를 추가 하였습니다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,31 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
-    void split() {
+    public void split() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    public void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() 특정 위치의 문자를 가져온다")
+    public void charAt() {
+        String result = "abc";
+        assertThatThrownBy(() ->
+                result.charAt(3)
+        ).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
