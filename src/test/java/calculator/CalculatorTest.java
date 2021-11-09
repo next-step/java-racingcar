c4945d7a9eda7eb80f20ac2d5ad82b74802b694f
sjhello <sjhello235@gmail.com>
2021-11-10 00:10:49 +0900

    refactor: DisplayName 애노테이션 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -16,49 +16,35 @@ import com.sun.org.apache.xerces.internal.impl.xpath.regex.RegularExpression;
 public class CalculatorTest {
 
 	@Test
+	@DisplayName("더하기 연산을 한다")
 	void add() {
 		InputNumber input = new InputNumber("1 + 3");
 		assertThat(Calculator.calculate(input)).isEqualTo(4);
 	}
 
 	@Test
+	@DisplayName("빼기 연산을 한다")
 	void minus() {
 		InputNumber input = new InputNumber("3 - 1");
 		assertThat(Calculator.calculate(input)).isEqualTo(2);
 	}
 
 	@Test
+	@DisplayName("곱셈 연산을 한다")
 	void times() {
 		InputNumber input = new InputNumber("2 * 4");
 		assertThat(Calculator.calculate(input)).isEqualTo(8);
 	}
 
 	@Test
+	@DisplayName("나눗셈 연산을 한다")
 	void divide() {
 		InputNumber input = new InputNumber("4 / 2");
 		assertThat(Calculator.calculate(input)).isEqualTo(2);
 	}
 
-	@Test
-	@DisplayName("나눗셈 연산을 하는 경우 분모가 0일때 IllegalArgumentException을 throw 한다")
-	void divideByZero() {
-		assertThatIllegalArgumentException().isThrownBy(() -> {
-			InputNumber input = new InputNumber("4 / 0");
-			Calculator.calculate(input);
-		});
-	}
-
-
-	@Test
-	@DisplayName("사칙연산 기호가 아닌경우 IllegalArgumentException을 throw 한다")
-	void notOperandValueThrowsException() {
-		assertThatThrownBy(() -> {
-			InputNumber input = new InputNumber("1 ^ 4");
-			Calculator.calculate(input);
-		}).isInstanceOf(IllegalArgumentException.class);
-	}
-
 	@ParameterizedTest
+	@DisplayName("사칙 연산 모두를 포함하는 기능을 테스트 한다")
 	@CsvSource(value = {"1 + 2 * 3 / 3 - 1:2"}, delimiter = ':')
 	void allCalc(String input, double result) {
 		InputNumber inputNumber = new InputNumber(input);
