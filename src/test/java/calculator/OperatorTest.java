92035ae3ca17515374ac329ebaefa0f2b0a688fd
sjhello <sjhello235@gmail.com>
2021-11-10 00:40:47 +0900

    refactor: Operator enum 클래스에 대한 테스트 추가

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -1,17 +1,20 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Optional;
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class OperatorTest {
+
+	@ParameterizedTest
+	@DisplayName("Operator 클래스의 사칙 연산을 테스트한다")
+	@CsvSource(value = {"PLUS:6", "MINUS:2", "TIMES:8", "DIVIDE:2"}, delimiter = ':')
+	void arithmeticOperationTest(String operatorState, int result) {
+		assertThat(Operator.valueOf(operatorState).operate(4, 2)).isEqualTo(result);
+	}
 
 	@ParameterizedTest
 	@DisplayName("사칙 연산이 가능한 연산 기호를 검증한다")
