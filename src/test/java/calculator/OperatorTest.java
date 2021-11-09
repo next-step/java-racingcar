643240f15a0e10a6d67dd530d7ed3a2eba233025
DaehunGwak <rhkrgns56@naver.com>
2021-11-10 00:07:32 +0900

    test: 2단계 - 문자열 계산기 - public method 딘위 테스트 추가
    - Operator.of 메서드 정상 시나리오 테스트 추가
    - BinomialExpression.calculate 메서드 테스트 추가
    - StringCalculator.calculate 는 기존 테스트로 가능

--- src/test/java/calculator/OperatorTest.java
+++ src/test/java/calculator/OperatorTest.java
@@ -3,14 +3,28 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("2단계 - 문자열 계산기 - enum Operator 테스트")
 class OperatorTest {
 
     @Test
-    @DisplayName("사칙연산 예외 기호 테스트")
+    @DisplayName("연산자 정상 시나리오")
     void of() {
+        assertThat(Operator.of("+"))
+                .isEqualTo(Operator.ADDITION);
+        assertThat(Operator.of("-"))
+                .isEqualTo(Operator.SUBTRACTION);
+        assertThat(Operator.of("*"))
+                .isEqualTo(Operator.MULTIPLICATION);
+        assertThat(Operator.of("/"))
+                .isEqualTo(Operator.DIVISION);
+    }
+
+    @Test
+    @DisplayName("연산자 예외 기호 테스트")
+    void ofIllegalArgumentException() {
         assertThatIllegalArgumentException().isThrownBy(() -> {
            Operator.of("^");
         });
