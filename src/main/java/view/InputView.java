560fb577789010000c8b7a12b3ebb26617a48eca
user <kth4878@naver.com>
2021-11-09 20:44:56 +0900

    [Refactor] 차 개수 입력 -> 차 이름 입력 으로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,28 +3,28 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-	private final static Scanner scanner = new Scanner(System.in);
-	private static final String NUMBER_TYPE_ERROR_MESSAGE = "error : 아라비아 숫자만 입력할수 있습니다.";
-	private static final String RACE_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
-	private static final String CAR_RACE_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private final static Scanner scanner = new Scanner(System.in);
+    private static final String NUMBER_TYPE_ERROR_MESSAGE = "error : 아라비아 숫자만 입력할수 있습니다.";
+    private static final String RACE_CAR_COUNT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)";
+    private static final String CAR_RACE_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
 
-	public static int inputRaceCarCount() {
-		System.out.println(RACE_CAR_COUNT_MESSAGE);
+    public static String inputRaceCarName() {
+        System.out.println(RACE_CAR_COUNT_MESSAGE);
 
-		return checkNumberType();
-	}
+        return scanner.nextLine();
+    }
 
-	public static int inputCarRaceCount() {
-		System.out.println(CAR_RACE_COUNT_MESSAGE);
+    public static int inputCarRaceCount() {
+        System.out.println(CAR_RACE_COUNT_MESSAGE);
 
-		return checkNumberType();
-	}
+        return checkNumberType();
+    }
 
-	private static int checkNumberType() {
-		if (!scanner.hasNextInt()) {
-			throw new IllegalArgumentException(NUMBER_TYPE_ERROR_MESSAGE);
-		}
+    private static int checkNumberType() {
+        if (!scanner.hasNextInt()) {
+            throw new IllegalArgumentException(NUMBER_TYPE_ERROR_MESSAGE);
+        }
 
-		return scanner.nextInt();
-	}
+        return scanner.nextInt();
+    }
 }
