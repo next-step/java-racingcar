47d08113bee24a8babd4160416f3cbdf52371f76
DaehunGwak <rhkrgns56@naver.com>
2021-11-09 23:54:04 +0900

    test: 3단계 - 자동차 경주 - 기능 요구사항 빈 테스트 구조 작성

---
+++ src/test/java/racing/CarRacingFeatureTest.java
@@ -0,0 +1,44 @@
+package racing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("3단계 - 자동차 경주 - 기능 요구사항")
+public class CarRacingFeatureTest {
+
+    @Test
+    @DisplayName("자동차 전진 기능")
+    void goCar() {
+
+    }
+
+    @Test
+    @DisplayName("자동차 정지 기능")
+    void stopCar() {
+
+    }
+
+    @Test
+    @DisplayName("차 수 입력 기능")
+    void inputNumberOfCar() {
+
+    }
+
+    @Test
+    @DisplayName("이동 횟수 입력 기능")
+    void inputNumberOfMove() {
+
+    }
+
+    @Test
+    @DisplayName("자동차 랜덤 행동 이동 기능")
+    void moveRandom() {
+
+    }
+
+    @Test
+    @DisplayName("자동차 상태 출력 기능")
+    void printCarStatus() {
+
+    }
+}
