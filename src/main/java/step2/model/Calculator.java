e6ebce2626b5ae926e920a8de68a2f5fe9da947f
mj950425 <minjoon1995@naver.com>
2021-11-09 09:23:09 +0900

    [fix] 변수명은 카멜케이스로 변경

--- src/main/java/step2/model/Calculator.java
+++ src/main/java/step2/model/Calculator.java
@@ -2,18 +2,14 @@ package step2.model;
 
 public class Calculator {
 
-    private Calculator() {
-    }
 
-    public static Integer calculateAll(String str) {
+    public static Integer operator(String str) {
         String[] stringArray = str.split(" ");
-        Integer firstIndex = 0;
-        Integer result = new Integer(stringArray[firstIndex]);
+        Integer result = new Integer(stringArray[0]);
         for (int i = 1; i < stringArray.length; i += 2) {
             String operator = stringArray[i];
             Integer num1 = new Integer(stringArray[i+1]);
-            Operator operatorValue = Operator.findOperator(operator);
-            result = Operator.calculator(result, num1, operatorValue);
+            result = CalculatorEnum.findOperator(result, num1, operator);
         }
         return result;
     }
