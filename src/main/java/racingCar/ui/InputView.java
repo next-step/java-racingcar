4f6f766a11ba8348e97691cf1e74179ba259d6d2
minchoi9509 <minchoi9509@gmail.com>
2021-11-09 22:45:06 +0900

    [style] 오타 수정

--- src/main/java/racingCar/ui/InputView.java
+++ src/main/java/racingCar/ui/InputView.java
@@ -7,7 +7,7 @@ public class InputView {
     private Scanner scanner = new Scanner(System.in);
 
     public String[] inputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분함");
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분함)");
         String names = scanner.next();
         return names.split(",");
     }
