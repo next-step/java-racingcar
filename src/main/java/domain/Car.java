24e3b2efd60c0182312806bc635dfafad05d261c
user <kth4878@naver.com>
2021-11-09 20:47:53 +0900

    [Feat] 자동차 이름입력 객체 추가

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Car {
+    private final String car;
+
+    public Car(String car){
+        this.car = car;
+    }
+
+    public String getCar() {
+        return car;
+    }
+}
