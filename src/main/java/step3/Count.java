656ecab67f775db718dce936329973b6d32da82e
kero <jdy410@naver.com>
2021-11-10 00:15:07 +0900

    docs: Count 클래스 equals, hashCode 메서드 추가

--- src/main/java/step3/Count.java
+++ src/main/java/step3/Count.java
@@ -1,8 +1,14 @@
 package step3;
 
-public class Count {
+import java.util.Objects;
 
+public class Count {
+    private int ZERO = 0;
     private int count;
+
+    public Count() {
+        this.count = ZERO;
+    }
 
     public Count(Integer input) {
         this.count = input;
@@ -14,5 +20,22 @@ public class Count {
 
     public int getCount() {
         return count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Count count1 = (Count) o;
+        return count == count1.count;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count);
     }
 }
