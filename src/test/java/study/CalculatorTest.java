5a42db363ea34da1f5328d1eae8a72b90cbbcd64
dante(이산하) <dante.93@kakaocorp.com>
2021-11-10 23:04:46 +0900

    모든 테스트에 displayName 추가

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -5,11 +5,12 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
 
+    @DisplayName("덧셈 기능 테스트")
     @ParameterizedTest
     @CsvSource(value = { "1 + 2:3", "-1 + 2:1" }, delimiter = ':')
     void plus(String expression, int expected) {
@@ -17,6 +18,7 @@ public class CalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @DisplayName("뺄셈 기능 테스트")
     @ParameterizedTest
     @CsvSource(value = { "1 - 2:-1", "-1 - 2:-3" }, delimiter = ':')
     void minus(String expression, int expected) {
@@ -24,6 +26,7 @@ public class CalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @DisplayName("곱셈 기능 테스트")
     @ParameterizedTest
     @CsvSource(value = { "1 * 2:2", "-1 * 2:-2" }, delimiter = ':')
     void times(String expression, int expected) {
@@ -31,6 +34,7 @@ public class CalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @DisplayName("나눗셈 기능 테스트")
     @ParameterizedTest
     @CsvSource(value = { "4 / 2:2", "-2 / 2:-1" }, delimiter = ':')
     void devidedBy(String expression, int expected) {
@@ -38,13 +42,9 @@ public class CalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    private static String[] nullEmptySource() {
-        return new String[] { null, "" };
-    }
-
     @DisplayName("입력 값이 null이거나 빈 공백 문자일 경우 예외를 던진다")
     @ParameterizedTest
-    @MethodSource("nullEmptySource")
+    @NullAndEmptySource
     void invalidExpression(String expression) {
         assertThatThrownBy(() -> Calculator.calculate(expression)).isInstanceOf(IllegalArgumentException.class);
     }
