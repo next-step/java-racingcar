9d8819fb85b012a509e7dfe498a24b1defd15b3d
giyeon95 <giyeon15@gmail.com>
2021-11-10 18:30:49 +0900

    refactor 자동차 전진 & 유지 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,13 +4,15 @@ public class Car {
 
     private int position = 0;
 
-    public void action(int command) {
-        if (command < 0 || command > 10) {
-            throw new IllegalArgumentException("입력 값은 0~9가 와야합니다.");
-        }
+    public void action(int num) {
+        Command.of(num).accept(this);
+    }
 
-        if (command > 3) {
-            this.position++;
-        }
+    public void forward() {
+        this.position++;
+    }
+
+    public void nonForward() {
+        // Non Forward
     }
 }
---
+++ src/main/java/racingcar/Command.java
@@ -0,0 +1,44 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public enum Command {
+    FORWARD(Car::forward,
+        Arrays.asList(
+            number -> number >= 4,
+            number -> number < 10)),
+
+    NON_FORWARD(Car::nonForward,
+        Arrays.asList(
+            number -> number >= 0,
+            number -> number < 4));
+
+    private final Consumer<Car> parser;
+    private final List<Predicate<Integer>> options;
+
+
+    Command(Consumer<Car> parser,
+        List<Predicate<Integer>> options) {
+        this.parser = parser;
+        this.options = options;
+
+    }
+
+
+    public static Consumer<Car> of(int commandNumber) {
+        return Arrays.stream(values())
+            .filter(cmd -> cmd.options.parallelStream()
+                .allMatch(flag -> flag.test(commandNumber))
+            )
+            .findFirst()
+            .map(Command::getParser)
+            .orElseThrow(() -> new IllegalArgumentException("입력 값은 0~9가 와야합니다."));
+    }
+
+    public Consumer<Car> getParser() {
+        return parser;
+    }
+}
