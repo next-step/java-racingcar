501e65d0e11758108b729ff9f25a59d7f4a48142
sid <sid.seo@kakaocorp.com>
2021-11-10 12:03:02 +0900

    test: test for multiple winner
    2명 이상 우승자 확인 테스트 추가
     - 테스트 코드에서 Car 객체를 재활용해 발생하던 테스트 실패 수정

--- src/test/java/edu/nextstep/camp/RaceTest.java
+++ src/test/java/edu/nextstep/camp/RaceTest.java
@@ -17,16 +17,22 @@ import static edu.nextstep.camp.TestMovePolicy.NEVER_MOVE_POLICY;
 import static org.assertj.core.api.Assertions.*;
 
 public class RaceTest {
-    private static final List<Car> TEST_CARS = Car.of(new String[]{"test0", "test1", "test2", "test3"}, ALWAYS_MOVE_POLICY);
     private static final int TEST_TURN = 1;
-    private static final Car TEST_CAR_0 = Car.of("test0", ALWAYS_MOVE_POLICY);
-    private static final Car TEST_CAR_1 = Car.of("test1", NEVER_MOVE_POLICY);
+
+    static List<Car> getTestCars() {
+        return Car.of(new String[]{"test0", "test1", "test2", "test3"}, ALWAYS_MOVE_POLICY);
+    }
+
+    static List<Car> getTestCarAsList() {
+        return List.of(Car.of("test0", ALWAYS_MOVE_POLICY));
+    }
 
     @ParameterizedTest(name = "create race test: {arguments}")
     @ValueSource(ints = {1, 15, 12000})
     public void create(int turn) {
-        final Race race = Race.of(TEST_CARS, turn);
-        assertThat(race.numberOfCars()).isEqualTo(TEST_CARS.size());
+        final List<Car> testCars = getTestCars();
+        final Race race = Race.of(testCars, turn);
+        assertThat(race.numberOfCars()).isEqualTo(testCars.size());
         assertThat(race.totalTurns()).isEqualTo(turn);
     }
 
@@ -34,8 +40,8 @@ public class RaceTest {
         return Stream.of(
                 Arguments.of(null, TEST_TURN),
                 Arguments.of(Collections.emptyList(), TEST_TURN),
-                Arguments.of(TEST_CARS, 0),
-                Arguments.of(TEST_CARS, -1000)
+                Arguments.of(getTestCars(), 0),
+                Arguments.of(getTestCars(), -1000)
         );
     }
 
@@ -48,16 +54,17 @@ public class RaceTest {
     @Test
     @DisplayName("get game result(initial status)")
     public void gameResult() {
-        final Integer[] expected = new Integer[TEST_CARS.size()];
+        final List<Car> testCars = getTestCars();
+        final Integer[] expected = new Integer[testCars.size()];
         Arrays.fill(expected, 0);
-        final Race race = Race.of(TEST_CARS, 1);
+        final Race race = Race.of(testCars, 1);
         assertThat(race.gameResult()).isEqualTo(Arrays.asList(expected));
     }
 
     @Test
     @DisplayName("process game during given turns")
     public void process() {
-        final Race race = Race.of(List.of(TEST_CAR_0), TEST_TURN);
+        final Race race = Race.of(getTestCarAsList(), TEST_TURN);
         assertThat(race.isEnded()).isFalse();
         race.process();
         assertThat(race.gameResult()).containsExactly(TEST_TURN);
@@ -67,20 +74,32 @@ public class RaceTest {
         assertThat(race.isEnded()).isTrue();
     }
 
-    @Test
-    @DisplayName("get winner after race is over")
-    public void checkWinner() {
-        final Race race = Race.of(List.of(TEST_CAR_0, TEST_CAR_1), TEST_TURN);
+    static Stream<Arguments> parseCheckWinnerArguments() {
+        final Car TEST_CAR_0 = Car.of("test0", ALWAYS_MOVE_POLICY);
+        final Car TEST_CAR_1 = Car.of("test1", NEVER_MOVE_POLICY);
+        final Car TEST_CAR_2 = Car.of("test2", ALWAYS_MOVE_POLICY);
+        final Car TEST_CAR_3 = Car.of("test3", NEVER_MOVE_POLICY);
+        final Car TEST_CAR_4 = Car.of("test4", ALWAYS_MOVE_POLICY);
+        return Stream.of(
+            Arguments.of(List.of(TEST_CAR_0, TEST_CAR_1), TEST_TURN, List.of(TEST_CAR_0.name())),
+            Arguments.of(List.of(TEST_CAR_2, TEST_CAR_3, TEST_CAR_4), TEST_TURN, List.of(TEST_CAR_2.name(), TEST_CAR_4.name()))
+        );
+    }
+
+    @ParameterizedTest(name = "get winner after race is over with: {arguments}")
+    @MethodSource("parseCheckWinnerArguments")
+    public void checkWinner(List<Car> cars, int turn, List<String> expectedWinner) {
+        final Race race = Race.of(cars, turn);
         assertThat(race.isEnded()).isFalse();
         race.process();
         assertThat(race.isEnded()).isTrue();
-        assertThat(race.winners()).containsExactly(TEST_CAR_0.name());
+        assertThat(race.winners()).hasSameElementsAs(expectedWinner);
     }
 
     @Test
     @DisplayName("failed to get winner caused the race is not over")
     public void attemptToCheckWinnerDuringTheRace() {
-        final Race race = Race.of(List.of(TEST_CAR_0), TEST_TURN);
+        final Race race = Race.of(getTestCarAsList(), TEST_TURN);
         assertThat(race.isEnded()).isFalse();
         assertThatIllegalStateException().isThrownBy(race::winners);
     }
