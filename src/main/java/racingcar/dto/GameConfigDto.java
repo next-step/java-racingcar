0e6777927e5005633e4de5afea1f3f759503622f
giyeon95 <giyeon15@gmail.com>
2021-11-10 16:05:33 +0900

    style 변수 네이밍 수정 `maxOfCars -> numberOfCars`

--- src/main/java/racingcar/dto/GameConfigDto.java
+++ src/main/java/racingcar/dto/GameConfigDto.java
@@ -2,13 +2,20 @@ package racingcar.dto;
 
 public class GameConfigDto {
 
-    private final int maxOfCars;
+    private final int numberOfCars;
     private final int maxOfRound;
 
     public GameConfigDto(int maxOfCars, int maxOfRound) {
-        this.maxOfCars = maxOfCars;
+        this.numberOfCars = maxOfCars;
         this.maxOfRound = maxOfRound;
     }
 
+    public int getNumberOfCars() {
+        return numberOfCars;
+    }
+
+    public int getMaxOfRound() {
+        return maxOfRound;
+    }
 
 }
