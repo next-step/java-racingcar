6ce3a68e94c8d9f8c21b6737f269caaf28268ec4
mj950425 <minjoon1995@naver.com>
2021-11-10 14:45:35 +0900

    [refactoring] 바뀌지않는 객체는 final

--- src/main/java/step3/model/CarList.java
+++ src/main/java/step3/model/CarList.java
@@ -4,19 +4,15 @@ import step3.strategy.MoveStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class CarList {
 
-    private List<Car> carList;
+    private final List<Car> carList;
 
     public List<Car> getCarList() {
         return carList;
     }
 
-    public CarList() {
-        this.carList = new ArrayList<Car>();
-    }
     public CarList(Integer countOfCar) {
         List<Car> carList = new ArrayList<Car>();
         for (int i = 0; i < countOfCar; i++) {
