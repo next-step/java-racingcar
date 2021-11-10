17596731b1f4a86f7d9e6ed0c66fca308984356e
minchoi9509 <minchoi9509@gmail.com>
2021-11-11 01:07:40 +0900

    [tests] 테스트 코드 추가

--- src/test/java/racingCar/RacingCarTest.java
+++ src/test/java/racingCar/RacingCarTest.java
@@ -14,16 +14,16 @@ public class RacingCarTest {
 
     RacingCarGame racingCarGame;
     private final static int TRY_TIMES = 5;
-    private final static String CAR_NAMES = "pobi,crong,honux,min";
+    private final static String[] CAR_NAMES = "pobi,crong,honux,min".split(",");
 
     @BeforeEach
     void beforeTest() {
-        racingCarGame = new RacingCarGame(TRY_TIMES);
+        racingCarGame = new RacingCarGame(CAR_NAMES, TRY_TIMES);
     }
 
     @Test
     void 자동차_리스트_생성_테스트() {
-        List<Car> cars = racingCarGame.setRaceCar(CAR_NAMES.split(","));
+        List<Car> cars = racingCarGame.getRaceCar();
         assertThat(cars.size()).isEqualTo(4);
     }
 
@@ -37,7 +37,7 @@ public class RacingCarTest {
 
     @Test
     void 자동차_경주_이름_출력_테스트() {
-        List<Car> cars = racingCarGame.setRaceCar(CAR_NAMES.split(","));
+        List<Car> cars = racingCarGame.getRaceCar();
         ResultView resultView = new ResultView();
         resultView.printCarRace(cars);
     }
@@ -46,7 +46,7 @@ public class RacingCarTest {
     void 게임_초기화_음수_파라미터_테스트() {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> {
-                    RacingCarGame testRacingCarGame = new RacingCarGame(-1);
+                    RacingCarGame testRacingCarGame = new RacingCarGame(CAR_NAMES,-1);
                 });
     }
 
