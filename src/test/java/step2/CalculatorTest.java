3b899f7399a83136f807e8622255b76720505aad
mj950425 <minjoon1995@naver.com>
2021-11-10 14:46:48 +0900

    [refactoring] Test코드 추가

--- src/test/java/step2/CalculatorTest.java
+++ src/test/java/step2/CalculatorTest.java
@@ -1,10 +1,14 @@
 package step2;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import step2.model.Calculator;
+import step2.model.CalculatorEnum;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,17 +18,48 @@ public class CalculatorTest {
     @DisplayName("Operator test")
     @ParameterizedTest
     @CsvSource(value = {"1 + 1 - 5 + 10 / 7 * 100:100", "100 * 100:10000", " 100 / 100:1"}, delimiter = ':')
-    public void calculator(String str, int expect) {
+    void calculator(String str, int expect) {
         assertEquals(Calculator.operator(str), expect);
     }
 
     @DisplayName("divide by zero exception test")
     @ParameterizedTest
     @CsvSource(value = {"100 / 0:1"}, delimiter = ':')
-    public void divideByZeroException(String str, int expect) {
+    void divideByZeroException(String str, int expect) {
         assertThatThrownBy(() -> {
             Calculator.operator(str);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("can't divide by zero");
     }
+
+    @Test
+    void findOperatorTest() {
+        CalculatorEnum ADD = CalculatorEnum.findOperator("+");
+        assertEquals(ADD,CalculatorEnum.ADD);
+
+        CalculatorEnum MINUS = CalculatorEnum.findOperator("-");
+        assertEquals(MINUS,CalculatorEnum.MINUS);
+
+        CalculatorEnum DIVIDE = CalculatorEnum.findOperator("/");
+        assertEquals(DIVIDE,CalculatorEnum.DIVIDE);
+
+        CalculatorEnum MULTIPLY = CalculatorEnum.findOperator("*");
+        assertEquals(MULTIPLY,CalculatorEnum.MULTIPLY);
+    }
+    @Test
+    void calculatorTwoNumberTest() {
+        Integer addNum = CalculatorEnum.calculator(1, 3, CalculatorEnum.ADD);
+        assertEquals(addNum,4);
+
+        Integer multiplyNum = CalculatorEnum.calculator(1, 3, CalculatorEnum.MULTIPLY);
+        assertEquals(multiplyNum,3);
+
+        Integer minusNum = CalculatorEnum.calculator(6, 3, CalculatorEnum.MINUS);
+        assertEquals(minusNum,3);
+
+        Integer divideNum = CalculatorEnum.calculator(30, 3, CalculatorEnum.DIVIDE);
+        assertEquals(divideNum,10);
+    }
+
+
 }
--- src/test/java/step3/CarListTest.java
+++ src/test/java/step3/CarListTest.java
@@ -4,34 +4,47 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step3.model.Car;
 import step3.model.CarList;
+import step3.service.ConvertOutputView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarListTest {
     @Test
-    @DisplayName("countOfCar을 입력받아 만들어지는 carList 의 여러 car의 position test")
-    void carListTest() {
-        CarList expectedCarList = new CarList();
-        for (int i = 0; i < 5; i++) {
-            expectedCarList.getCarList().add(new Car());
-        }
-        CarList carList = new CarList(5);
-
-        for (int i = 0; i < 5; i++) {
-            assertEquals(carList.getCarList().get(i).getPosition(), expectedCarList.getCarList().get(i).getPosition());
+    @DisplayName("ConvertOutputView 가 잘 적용되는지 테스트")
+    void convertOutputViewTest() {
+        List<String> expectedList = new ArrayList<String>(Arrays.asList("-", "-", "-"));
+        CarList carList = new CarList(3);
+        for (int i = 0; i < 3; i++) {
+            assertEquals(ConvertOutputView.convertOutputView(carList.getCarList().get(i).getPosition()), expectedList.get(i));
         }
     }
 
     @Test
-    @DisplayName("carList가 RandomMoveStrategy에 따라 잘 move하는지 test")
-    void randomMoveStrategyTest() {
-        CarList carList = new CarList(5);
-        carList.movableCarList(() -> true);
-        for (int i = 0; i < 5; i++) {
-            assertEquals(carList.getCarList().get(i).getPosition(), 1);
+    @DisplayName("초기에 countOfCar을 입력받아 만들어지는 carList 의 여러 car의 position이 0으로 들어가는지 test")
+    void initialCarListTest() {
+        List<String> expectedList = new ArrayList<String>(Arrays.asList("-", "-", "-"));
+        CarList carList = new CarList(3);
+        for (int i = 0; i < 3; i++) {
+            assertEquals(ConvertOutputView.convertOutputView(carList.getCarList().get(i).getPosition()), expectedList.get(i));
+        }
+    }
+
+    @Test
+    @DisplayName("carList RandomMoveStrategy test")
+    void carListTest() {
+        List<String> expectedList = new ArrayList<String>(Arrays.asList("---", "-", "--"));
+
+        CarList carList = new CarList(3);
+        carList.getCarList().get(1).movable(()->false);
+        carList.getCarList().get(0).movable(()->true);
+        carList.getCarList().get(0).movable(()->true);
+        carList.getCarList().get(2).movable(()->true);
+        for (int i = 0; i < 3; i++) {
+            assertEquals(ConvertOutputView.convertOutputView(carList.getCarList().get(i).getPosition()), expectedList.get(i));
         }
     }
 }
