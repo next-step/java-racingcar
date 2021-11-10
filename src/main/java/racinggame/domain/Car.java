425853428e2ae1ed32b9a69a32799297468a4905
JaeHan-Kim <hgal8877@gmail.com>
2021-11-10 23:08:44 +0900

    refactor : 자동차의 기록을 가지고 있는 리스트 삭제

--- src/main/java/racinggame/domain/Car.java
+++ src/main/java/racinggame/domain/Car.java
@@ -1,34 +1,21 @@
 package racinggame.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Car {
 
-    private static final Integer MIN_MOVE_VALUE = 4;
 
     private Location location;
-    private List<Boolean> record;
 
     public Car() {
         this.location = new Location();
-        this.record = new ArrayList<>();
     }
 
-    public void move(int moveValue) {
-        Boolean move = MIN_MOVE_VALUE <= moveValue;
-
-        if (move) {
-            location.moveLocation();
+    public void move(State state) {
+        if (state == State.MOVE) {
+            location.move();
         }
-        record.add(move);
     }
 
     public Location getLocation() {
         return location;
-    }
-
-    public long getRecord(int round) {
-        return record.subList(0, round + 1).stream().filter(b -> b).count();
     }
 }
