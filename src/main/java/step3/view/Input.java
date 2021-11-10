4a9e577cf8a69d7291f4e71e1364e71c9f262765
mj950425 <minjoon1995@naver.com>
2021-11-10 14:46:17 +0900

    [refactoring] convertOutputView로직 분리 및 인스턴스 객체로 사용안되므로 static으로 메소드 접근 제어자 변경

--- src/main/java/step3/view/Input.java
+++ src/main/java/step3/view/Input.java
@@ -1,24 +1,18 @@
 package step3.view;
 
-import step3.model.CarList;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Input {
 
-    public List<Integer> inputView() {
-        List<Integer> inputList = new ArrayList<Integer>();
-        Scanner scanner = new Scanner(System.in);
-
+    public static Integer getCuntOfCar() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        inputList.add(scanner.nextInt());
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
 
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        inputList.add(scanner.nextInt());
-
-        return inputList;
+    public static Integer getCuntOfTry() {
+        System.out.println("시도할 횟수는?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
