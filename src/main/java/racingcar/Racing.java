0a8e7228805dcd75998f623f0c21f730c3386f86
las9897 <las9897@gmail.com>
2021-11-10 16:00:08 +0900

    feat: 자동차 경주 클래스 추가

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import common.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Racing {
+
+    private final ListView<Car> carListView;
+    private final int tryCount;
+
+    public Racing(int carCount, int tryCount) {
+        this.carListView = new ListView<>(makeCar(carCount));
+        this.tryCount = tryCount;
+    }
+
+    public List<Car> makeCar(int carCount) {
+        List<Car> carList = new ArrayList<>();
+        for (int i = 0; i < carCount; i++) {
+            carList.add(new Car());
+        }
+        return carList;
+    }
+    
+}
