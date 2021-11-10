b5e48915cded44c100f64910091a8da68253df57
giyeon95 <giyeon15@gmail.com>
2021-11-10 16:01:50 +0900

    feat 자동차 대수 입력 UI 기능 및 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,13 +7,25 @@ public class InputView {
 
     private final Scanner sc = new Scanner(System.in);
 
-    private InputView() {
+    private static final String NUMBER_OF_CAR_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String NUMBER_OF_GAME_ROUND_INPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
 
+
+    public GameConfigDto initConfig() {
+
+        int numberOfCar = inputFromClient(NUMBER_OF_CAR_INPUT_MESSAGE);
+        int numberOfGameRound = inputFromClient(NUMBER_OF_GAME_ROUND_INPUT_MESSAGE);
+
+        return new GameConfigDto(numberOfCar, numberOfGameRound);
     }
 
-    public static GameConfigDto initConfig() {
+    private int inputFromClient(String message) {
+        print(message);
+        return sc.nextInt();
+    }
 
-        return null;
+    private void print(String message) {
+        System.out.println(message);
     }
 
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,18 +1,32 @@
 package racingcar.view;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assertions;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.dto.GameConfigDto;
 
 class InputViewTest {
 
+
+    @BeforeEach
+    void init() throws IOException {
+        // MOCK System.in
+        String data = "3\r\n3";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+    }
+
     @Test
     @DisplayName("initConfig 메소드 호출시, GameConfigDto 객체를 반환 한다.")
     void initConfigTest() {
-        Assertions.assertThat(InputView.initConfig())
+
+        InputView inputView = new InputView();
+
+        assertThat(inputView.initConfig())
             .isInstanceOf(GameConfigDto.class);
     }
 
