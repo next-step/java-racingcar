25746bd99ffb966441ef7f513f09405c7a6014f8
haeuniii <qnfnqnfn9825@gmail.com>
2021-11-10 19:34:26 +0900

    feat: 자동차 이름 획득 및 길이 체크 테스트 코드 및 기능 구현

--- src/main/java/com/step3/model/car/Cars.java
+++ src/main/java/com/step3/model/car/Cars.java
@@ -2,6 +2,7 @@ package com.step3.model.car;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     private final List<Car> cars;
@@ -10,10 +11,27 @@ public class Cars {
         List<Car> cars = new LinkedList<>();
 
         for (int idx = 0; idx < carCount; idx++) {
-            cars.add(new Car(new CarId(idx), new CarPosition(0)));
+            cars.add(new Car(new CarId("idx"), new CarPosition(0)));
         }
 
         this.cars = cars;
+    }
+
+    public Cars(String[] carsName) {
+        List<Car> cars = new LinkedList<>();
+
+        for (String carName : carsName) {
+            cars.add(new Car(new CarId(carName), new CarPosition(0)));
+        }
+
+        this.cars = cars;
+    }
+
+    public List<Car> getWinnerCars() {
+        int maxPosition = cars.stream().mapToInt(c -> c.getPosition().getCarPosition()).max().getAsInt();
+        return cars.stream()
+                .filter(c -> c.getPosition().getCarPosition() == maxPosition)
+                .collect(Collectors.toList());
     }
 
     public List<Car> getCars() {
--- src/test/java/study/CarTest.java
+++ src/test/java/study/CarTest.java
@@ -1,29 +1,49 @@
 package study;
 
 import com.step3.model.car.Car;
+import com.step3.model.car.Cars;
 import com.step3.model.car.CarId;
 import com.step3.model.car.CarPosition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class CarTest {
     @Test
     @DisplayName("Car class 생성자 test")
     void create() {
-        Car car = new Car(new CarId(0), new CarPosition(0));
-        assertThat(car).isEqualTo(new Car(new CarId(0), new CarPosition(0)));
+        Car car = new Car(new CarId("pobi"), new CarPosition(0));
+        assertThat(car).isEqualTo(new Car(new CarId("pobi"), new CarPosition(0)));
     }
 
     @Test
     @DisplayName("Car move 후 Car class로 return test")
     void move() {
-        Car car = new Car(new CarId(0), new CarPosition(0));
-        Car movedCar = new Car(new CarId(0), new CarPosition(1));
+        Car car = new Car(new CarId("pobi"), new CarPosition(0));
+        Car movedCar = new Car(new CarId("pobi"), new CarPosition(1));
 
         assertThat(car.move(true).getPosition().getCarPosition())
                 .isEqualTo(movedCar.getPosition().getCarPosition());
+    }
+
+    @Test
+    @DisplayName("cars의 이동거리 비교 후 우승자 결과 test")
+    void getBestCars() {
+        Cars cars = new Cars("pobi,crong,honux".split(","));
+        cars.getCars().get(0).move(true);   // pobi 이동
+        cars.getCars().get(1).move(true);   // crong 이동
+        assertThat(cars.getWinnerCars().stream().map(c -> c.getId().getValue()).collect(Collectors.joining(","))).isEqualTo("pobi,crong");
+    }
+
+    @Test
+    void validateCarName() {
+        assertThatThrownBy(() -> new Cars("pobipobi,crong,honux".split(",")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름 5자 이하 필수");
     }
 }
