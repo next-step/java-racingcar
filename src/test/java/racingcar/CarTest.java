dd6bc87cd166a88d3f4547e5003daaf48e87f37d
las9897 <las9897@gmail.com>
2021-11-10 15:51:38 +0900

    test: 자동차 클래스 테스트 추가

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void setup(){
+        car = new Car();
+    }
+
+    @Test
+    @DisplayName("매개변수가 4 이상일 경우 state 1 증가 테스트")
+    void increaseState() {
+        car.increaseState(4);
+        Assertions.assertThat(car.getState()).isEqualTo(1);
+    }
+}
