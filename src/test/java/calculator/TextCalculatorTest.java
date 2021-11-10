8534cce5fd315a241fd92f58e17e8bc3d395361e
Han <hankim@tapasmedia.co>
2021-11-10 10:43:11 +0900

    refactor : TextCalculatorTest

--- src/test/java/calculator/TextCalculatorTest.java
+++ src/test/java/calculator/TextCalculatorTest.java
@@ -2,9 +2,14 @@ package calculator;
 
 import calculator.TextCalculator.Operator;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -21,63 +26,67 @@ class TextCalculatorTest {
         calculator = new TextCalculator();
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "덧셈")
     @CsvSource(value = {"2 + 2=4", "2 + 2 + 2 + 2=8", "1 + 2 + 0=3"}, delimiter = '=')
     void plus(String value, int expected) {
         assertThat(calculator.calculate(value)).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "뺄샘")
     @CsvSource(value = {"2 - 2=0", "1 - 2 - 3=-4", "10 - 2=8"}, delimiter = '=')
     void minus(String value, int expected) {
         assertThat(calculator.calculate(value)).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "곱셈")
     @CsvSource(value = {"1 * 1=1", "1 * 2 * 3=6", "0 * -999=0"}, delimiter = '=')
     void multiplication(String value, int expected) {
         assertThat(calculator.calculate(value)).isEqualTo(expected);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "나눗셈")
     @CsvSource(value = {"1 * 1=1", "1 * 2 * 3=6", "0 * -999=0"}, delimiter = '=')
     void division(String value, int expected) {
         assertThat(calculator.calculate(value)).isEqualTo(expected);
     }
 
     @Test
+    @DisplayName(value = "혼합")
     void mix() {
         String input = "2 + 3 * 4 / 2 + 1 - 6 + 10 - 11 * 191";
         assertThat(calculator.calculate(input)).isEqualTo(764);
     }
 
-    @Test
-    void operator() {
-        Operator plus = Operator.getInstance("+");
-        assertThat(plus).isEqualTo(Operator.PLUS);
+    @ParameterizedTest(name = "연산자 생성")
+    @MethodSource(value = "stringOperatorProvider")
+    void operator(String input, Operator operator) {
+        assertThat(Operator.getInstance(input)).isEqualTo(operator);
+    }
 
-        Operator minus = Operator.getInstance("-");
-        assertThat(minus).isEqualTo(Operator.MINUS);
-
-        Operator multiplication = Operator.getInstance("*");
-        assertThat(multiplication).isEqualTo(Operator.MULTIPLICATION);
-
-        Operator division = Operator.getInstance("/");
-        assertThat(division).isEqualTo(Operator.DIVISION);
+    static Stream<Arguments> stringOperatorProvider() {
+        return Stream.of(
+            Arguments.arguments("+", Operator.PLUS),
+            Arguments.arguments("-", Operator.MINUS),
+            Arguments.arguments("*", Operator.MULTIPLICATION),
+            Arguments.arguments("/", Operator.DIVISION)
+        );
     }
 
     @Test
+    @DisplayName(value = "잘못된 연산자를 입력하면 에러를 던진다.")
     void throwExceptionIfWrongOperator() {
         assertThatThrownBy(() -> Operator.getInstance("%")).isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> Operator.getInstance("* *")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
+    @DisplayName(value = "범위를 넘어선 계샨을 하면 에러를 던진다")
     void throwExceptionIfIntegerOverFlow() {
         assertThatThrownBy(() -> calculator.calculate(Integer.MAX_VALUE + " + " + "1")).isInstanceOf(ArithmeticException.class);
     }
 
     @Test
+    @DisplayName(value = "잘못된 Input값을 입력하면 에러를 던진다.")
     void throwExceptionIfInputWrongValue() {
         assertThatThrownBy(() -> calculator.calculate("1" + "+ " + "1")).isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> calculator.calculate("1" + " + " + " * " + "1")).isInstanceOf(IllegalArgumentException.class);
@@ -86,6 +95,7 @@ class TextCalculatorTest {
     }
 
     @Test
+    @DisplayName(value = "0으로 나누면 에러를 던진다")
     void throwExceptionIfDivideByZero() {
         assertThatThrownBy(() -> calculator.calculate("1 / 0")).isInstanceOf(ArithmeticException.class);
     }
