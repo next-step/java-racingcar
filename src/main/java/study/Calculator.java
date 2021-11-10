1fa3bf7f83c5eb457ed56507095085a8e375edf7
강호진 <blackpearl1210@gmail.com>
2021-11-10 22:46:39 +0900

    [21.11.10] step2 comment update

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -1,12 +1,16 @@
 package study;
 
-public class Calculator {
-    public static int calculate(String param) {
-        String[] values = validation(param);
+import java.util.Objects;
 
-        MyNumber result = new MyNumber(values[0]);
+public class Calculator {
+    private static int MIN_EXPRESS_LENGTH = 3;
+
+    public static int calculate(String param) {
+        String[] values = validateParameter(param);
+
+        Number result = new Number(values[0]);
         for(int i = 1; i < values.length; i = i + 2){
-            result = calculate(result, values[i], new MyNumber(values[i+1]));
+            result = calculate(result, values[i], new Number(values[i+1]));
         }
         return result.getNumber();
     }
@@ -16,15 +20,18 @@ public class Calculator {
      * @param param
      * @return
      */
-    private static String[] validation(String param) {
-        if(param == null || param.isEmpty()){
-            throw new IllegalArgumentException("입력 문자열이 정상적이지 않습니다.");
-        }
+    private static String[] validateParameter(String param) {
+        validationExceptionCheck(Objects.isNull(param), param.isEmpty());
+
         String[] values = param.split(" ");
-        if(values.length < 3 || values.length % 2 == 0){
+        validationExceptionCheck(values.length < MIN_EXPRESS_LENGTH, values.length % 2 == 0);
+        return values;
+    }
+
+    private static void validationExceptionCheck(boolean b, boolean b2) {
+        if (b || b2) {
             throw new IllegalArgumentException("입력 문자열이 정상적이지 않습니다.");
         }
-        return values;
     }
 
     /**
@@ -34,8 +41,8 @@ public class Calculator {
      * @param second
      * @return
      */
-    private static MyNumber calculate(MyNumber first, String operator, MyNumber second) {
-        return MyOperator.operator(operator).operate(first, second);
+    private static Number calculate(Number first, String operator, Number second) {
+        return Operator.operator(operator).operate(first, second);
     }
 
 }
--- src/main/java/study/Operator.java
+++ src/main/java/study/Operator.java
@@ -3,25 +3,25 @@ package study;
 import java.util.Arrays;
 import java.util.function.BiFunction;
 
-public enum MyOperator {
+public enum Operator {
     ADDITION("+", (first, second) -> first.plus(second)),
     SUBTRACTION("-", (first, second) -> first.minus(second)),
     MULTIPLICATION("*", (first, second) -> first.times(second)),
     DIVISION("/", (first, second) -> first.dividedBy(second));
 
     public String operator;
-    public BiFunction<MyNumber, MyNumber, MyNumber> operation;
+    public BiFunction<Number, Number, Number> operation;
 
-    MyOperator(String operator, BiFunction<MyNumber, MyNumber, MyNumber> operation){
+    Operator(String operator, BiFunction<Number, Number, Number> operation){
         this.operator = operator;
         this.operation = operation;
     }
 
-    public MyNumber operate(MyNumber first, MyNumber second){
+    public Number operate(Number first, Number second){
         return operation.apply(first, second);
     }
 
-    public static MyOperator operator(String Operator) {
+    public static Operator operator(String Operator) {
         return Arrays.stream(values())
             .filter(o -> o.operator.equals(Operator))
             .findFirst()
