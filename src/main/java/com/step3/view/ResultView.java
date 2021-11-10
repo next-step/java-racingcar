dfc96bc68bd2608236b572ddc3312a112bcd19f1
haeuniii <qnfnqnfn9825@gmail.com>
2021-11-10 19:40:11 +0900

    feat: 실행 결과UI에 자동차 이름 추가되도록 구현

--- src/main/java/com/step3/view/ResultView.java
+++ src/main/java/com/step3/view/ResultView.java
@@ -3,6 +3,8 @@ package com.step3.view;
 import com.step3.model.car.Car;
 import com.step3.model.car.Cars;
 
+import java.util.stream.Collectors;
+
 public class ResultView {
     public static void showResults(Cars cars, int attempt) {
         if (attempt == 0) {
@@ -10,7 +12,7 @@ public class ResultView {
         }
 
         for (Car car : cars.getCars()) {
-            System.out.println(getResult(car));
+            System.out.println(String.format("%s%s%s", car.getId().getValue(), ":", getResult(car)));
         }
 
         System.out.println();
