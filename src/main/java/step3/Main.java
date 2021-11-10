28806ff9b0513fa2b42d828e256fe8f6510db8d4
mj950425 <minjoon1995@naver.com>
2021-11-10 14:46:33 +0900

    [refactoring] main문 변경

--- src/main/java/step3/Main.java
+++ src/main/java/step3/Main.java
@@ -1,28 +1,18 @@
 package step3;
 
-import step3.model.Car;
 import step3.model.CarList;
 import step3.strategy.RandomMoveStrategyImpl;
 import step3.view.Input;
 import step3.view.Output;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        Input input = new Input();
-        List<Integer> inputList = input.inputView();
-        Integer countOfCar = inputList.get(0);
-        Integer countOfTry = inputList.get(1);
+
+        Integer countOfCar = Input.getCuntOfCar();
+        Integer countOfTry = Input.getCuntOfTry();
 
         CarList carList = new CarList(countOfCar);
 
-        Output output = new Output();
-        output.outputView(carList,new RandomMoveStrategyImpl(),countOfTry);
-
-
-
-
+        Output.outputView(carList,new RandomMoveStrategyImpl(),countOfTry);
     }
 }
