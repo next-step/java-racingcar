22b3db97f17b96a8880dc567026fa91c6d399a9b
JaeHan-Kim <hgal8877@gmail.com>
2021-11-10 23:32:14 +0900

    refactor : 자동차 게임 리펙터링

--- src/main/java/racinggame/domain/RacingGame.java
+++ src/main/java/racinggame/domain/RacingGame.java
@@ -1,23 +1,25 @@
 package racinggame.domain;
 
+import racinggame.view.ResultView;
+
 public class RacingGame {
 
     private final Cars cars;
     private final Laps laps;
+    private final CarStateGenerator carStateGenerator;
 
-    public RacingGame(Integer carCount, Laps laps) {
+    public RacingGame(Integer carCount, Laps laps, CarStateGenerator carStateGenerator) {
         this.cars = new Cars(carCount);
         this.laps = laps;
+        this.carStateGenerator = carStateGenerator;
     }
 
     public void startRacing() {
+        System.out.print("실행 결과");
         while (!laps.isGoal()) {
-            cars.roundRacing();
+            cars.roundRacing(carStateGenerator);
+            ResultView.currentRecord(cars);
             laps.passStartLine();
         }
-    }
-
-    public Cars resultRacing() {
-        return cars;
     }
 }
