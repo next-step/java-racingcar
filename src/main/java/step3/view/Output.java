6f4a0caa7106d673070d1784a1a7895c52c1d005
mj950425 <minjoon1995@naver.com>
2021-11-10 14:46:17 +0900

    [refactoring] convertOutputView로직 분리 및 인스턴스 객체로 사용안되므로 static으로 메소드 접근 제어자 변경

--- src/main/java/step3/view/Output.java
+++ src/main/java/step3/view/Output.java
@@ -1,27 +1,18 @@
 package step3.view;
 
-
 import step3.model.CarList;
+import step3.service.ConvertOutputView;
 import step3.strategy.MoveStrategy;
 
 public class Output {
 
-    public void outputView(CarList carList, MoveStrategy moveStrategy, Integer countOfTry) {
+    public static void  outputView(CarList carList, MoveStrategy moveStrategy, Integer countOfTry) {
         System.out.println("실행 결과");
-
         for (int i = 0; i < countOfTry; i++) {
             carList.movableCarList(moveStrategy);
-            carList.getCarList().stream().map(car -> convertOutputView(car.getPosition())).forEach(System.out :: println);
+            carList.getCarList().stream().map(car -> ConvertOutputView.convertOutputView(car.getPosition())).forEach(System.out :: println);
             System.out.println("");
         }
 
-    }
-
-    public String convertOutputView(Integer position) {
-        String convertResult = "-";
-        for (int i = 0; i < position; i++) {
-            convertResult += "-";
-        }
-        return convertResult;
     }
 }
