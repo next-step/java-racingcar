f018ef393ef9e166c896eaade921db58f1658d03
jiseok-choi <choi300300@gmail.com>
2021-11-11 01:21:19 +0900

    refactor: 리뷰 반영  - ResultView 클래스 기능 분리  - Car 클래스 추가  - 규칙 5 축약 금지  - 배열을 리스트로 변경

--- src/main/java/carracing/CarRacing.java
+++ src/main/java/carracing/CarRacing.java
@@ -1,5 +1,8 @@
 package carracing;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -13,11 +16,11 @@ import java.util.Scanner;
  */
 public class CarRacing {
 
-    private static InputView inputView;
-    private static ResultView resultView;
-
     private static final String QUESTION_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
     private static final String QUESTION_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
+
+    private static final int RANDOM_RANGE = 10;     /* 랜덤 숫자 범위 */
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -27,17 +30,36 @@ public class CarRacing {
         System.out.println(QUESTION_TRY_COUNT);
         String tryCount = scanner.nextLine();
 
-        inputView = new InputView();
-        inputView.valid(carCount, tryCount);
-        resultView = new ResultView(inputView.getCarCount());
-        process();
+        process(carCount, tryCount);
     }
 
-    public static void process() {
+    /**
+     * 메인 기능 수행하는 메소드
+     */
+    public static void process(String carCount, String tryCount) {
+
+        List<Car> carList = new ArrayList<>();          /* 주행할 자동차 목록 */
+        InputView inputView = new InputView();          /* 주행 입력 */
+        inputView.valid(carCount, tryCount);            /* 입력 검증 */
+        ResultView resultView = new ResultView();       /* 출력 클래스 생성 */
+
+        for (int i = 0; i < inputView.getCarCount(); i++) {
+            carList.add(new Car());                     /* 자동차 초기화 */
+        }
 
         for (int i = 0; i < inputView.getTryCount(); i++) {
-            resultView.racing();        /* 자동차 주행 */
-            resultView.printRacing();   /* 주행 이력 출력 */
+            racing(carList, inputView);                 /* 자동차 주행 */
+            resultView.printRacing(carList);            /* 주행 이력 출력 */
+        }
+    }
+
+    /**
+     * 차량 주행 기능 메소드
+     */
+    public static void racing(List<Car> RACING_CAR, InputView inputView) {
+        for (int i = 0; i < inputView.getCarCount(); i++) {
+            int randomNum = new Random().nextInt(RANDOM_RANGE);
+            RACING_CAR.get(i).driving(randomNum);
         }
     }
 
--- src/main/java/carracing/ResultView.java
+++ src/main/java/carracing/ResultView.java
@@ -1,6 +1,7 @@
 package carracing;
 
-import java.util.Random;
+import java.util.List;
+
 
 /*
  * ResultView
@@ -13,44 +14,13 @@ import java.util.Random;
  */
 public class ResultView {
 
-    private final String[] racingCar;               /* 주행할 자동차 목록 */
-
-    private static final int RANDOM_RANGE = 10;     /* 랜덤 숫자 범위 */
-    private static final String RACING_INIT = "";   /* 차량 움직임 초기화 */
-    private static final String MOVED = "-";        /* 차량 움직임 */
-    private static final int MOVE_CONDITION = 4;    /* 차량 주행 조건 */
-
-    public ResultView(int carCount) {
-
-        racingCar = new String[carCount];
-    }
-
-    /**
-     * 차량 주행 기능 메소드
-     */
-    public void racing() {
-        Random random;
-        for (int i = 0; i < racingCar.length; i++) {
-            random = new Random();
-            int randomNum = random.nextInt(RANDOM_RANGE);
-
-            if (racingCar[i] == null) {
-                racingCar[i] = RACING_INIT;
-            }
-
-            if (randomNum >= MOVE_CONDITION) {
-                racingCar[i] = racingCar[i] + MOVED;
-            }
-        }
-    }
-
     /**
      * 차량 주행 출력 메소드
      */
-    public void printRacing() {
+    public void printRacing(List<Car> carList) {
         System.out.println();
-        for (String s : racingCar) {
-            System.out.println(s);
+        for (Car car : carList) {
+            System.out.println(car.getDrivingHistory());
         }
     }
 
