d275c90cb6bc503e5c7a919e34e6bb86d1fa7e85
las9897 <las9897@gmail.com>
2021-11-10 16:06:37 +0900

    refactor: 자동차 전진 시도에 랜덤값 적용

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -4,15 +4,18 @@ import common.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Racing {
 
     private final ListView<Car> carListView;
     private final int tryCount;
+    private final Random random;
 
     public Racing(int carCount, int tryCount) {
         this.carListView = new ListView<>(makeCar(carCount));
         this.tryCount = tryCount;
+        this.random = new Random(System.currentTimeMillis());
     }
 
     public List<Car> makeCar(int carCount) {
@@ -25,10 +28,9 @@ public class Racing {
 
     public void goingTry() {
         for (Car car : carListView.getViewList()) {
-            car.increaseState(10);
+            car.increaseState(random.nextInt(10));
         }
     }
-
 
     public List<Car> getCarList() {
         return carListView.getViewList();
