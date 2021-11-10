9a4f8d90f04346266085e95a2113b57a114ed468
JaeHan-Kim <hgal8877@gmail.com>
2021-11-10 23:29:55 +0900

    refector : random에 의존하지 않고 상태를 받아올 수 있도록 수정

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -1,37 +1,28 @@
 package racinggame.domain;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Cars {
 
     private static final Integer START_INDEX = 0;
-    private static final Integer MAX_MOVE_VALUE = 9;
 
     private final List<Car> cars;
-    private final Random random;
 
     public Cars(Integer carCount) {
-        random = new Random();
         cars = IntStream.range(START_INDEX, carCount)
                 .mapToObj((idx) -> new Car())
                 .collect(Collectors.toList());;
     }
 
-    public void roundRacing() {
-        for (Car car : cars) {
-            car.move(random.nextInt(MAX_MOVE_VALUE));
-        }
+    public List<Car> getCars() {
+        return cars;
     }
 
-    public List<Long> getRacingHistory(int round) {
-        List<Long> records = new ArrayList<>();
+    public void roundRacing(StateGenerator stateGenerator) {
         for (Car car : cars) {
-            records.add(car.getRecord(round));
+            car.move(stateGenerator.getState());
         }
-        return records;
     }
 }
