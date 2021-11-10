445990288e687bcae533eead071f60feffad565a
nano.son <nano.son@kakaocorp.com>
2021-11-11 01:49:37 +0900

    test: filter winner

---
+++ src/main/java/race/FilterWinners.java
@@ -0,0 +1,10 @@
+package race;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class FilterWinners {
+    public Collection<Car> filter(Collection<Car> cars) {
+        return Collections.EMPTY_LIST;
+    }
+}
