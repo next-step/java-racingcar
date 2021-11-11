0ca24a82c37dabe6f0b6d2bf984fb91a58e60377
audgnssweet <audgnssweet@naver.com>
2021-11-11 23:47:06 +0900

    feat: 우승자 결과출력 로직 main에 추가

--- src/main/java/step4/CarRacingGame.java
+++ src/main/java/step4/CarRacingGame.java
@@ -27,4 +27,8 @@ public class CarRacingGame {
             car.move();
         }
     }
+
+    public Car[] getWinners() {
+        return Referee.getWinners(this.cars);
+    }
 }
--- src/main/java/step4/Main.java
+++ src/main/java/step4/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("경주할 자동차의 이름을 입력하세요(이름은 쉼표 (,)를 기준으로 구분)");
         carNames = GameInput.readCarNames();
         System.out.println("몇 번 시도할 것인가요?");
-        roundCount = GameInput.readInt();
+        roundCount = GameInput.readRoundCount();
 
         System.out.println("실행 결과");
         carRacingGame = new CarRacingGame(carNames);
@@ -19,5 +19,7 @@ public class Main {
             GameOutput.printCarPaths(carRacingGame);
             System.out.println();
         }
+
+        GameOutput.printWinners(carRacingGame.getWinners());
     }
 }
