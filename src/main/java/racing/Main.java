869afc94d01f798a5dd26291110cf15d5c744269
mj950425 <minjoon1995@naver.com>
2021-11-11 19:06:33 +0900

    [refacotring] 바뀐 로직에 맞게 input, ouput, main 수정

---
+++ src/main/java/racing/Main.java
@@ -0,0 +1,18 @@
+package racing;
+
+import racing.model.Cars;
+import racing.strategy.RandomMoveStrategyImpl;
+import racing.view.Input;
+import racing.view.Output;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> nameList = Input.getNameList();
+        Integer countOfTry = Input.getCuntOfTry();
+        Cars cars = new Cars(nameList);
+        Integer criteriaNumber = 4;
+        Output.outputView(cars,new RandomMoveStrategyImpl(),countOfTry,criteriaNumber);
+    }
+}
