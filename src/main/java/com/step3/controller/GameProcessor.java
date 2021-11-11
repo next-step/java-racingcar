a3caaaf62915c41577b7b8e7868f73adaae2fa1d
haeuniii <qnfnqnfn9825@gmail.com>
2021-11-11 14:26:30 +0900

    refactor: CarList move메소드를 Cars 객체로 이동

--- src/main/java/com/step3/controller/GameProcessor.java
+++ src/main/java/com/step3/controller/GameProcessor.java
@@ -7,27 +7,13 @@ import com.step3.util.RandomUtil;
 import com.step3.view.ResultView;
 
 public class GameProcessor {
-    private static final int MOVE_POSSIBLE_BOUND = 4;
-
     public void moveCars(InputNumber myNumber) {
         Cars cars = new Cars(myNumber.getCarsName());
 
         for (int attempt = 0; attempt < myNumber.getAttemptCount(); attempt++) {
-            ResultView.showResults(moveCars(cars), attempt);
+            ResultView.showResults(cars.moveCars(), attempt);
         }
 
         ResultView.printWinnerResult(cars);
-    }
-
-    private Cars moveCars(Cars cars) {
-        for (Car car : cars.getCars()) {
-            car.move(isMovePossible(RandomUtil.getRandomValue()));
-        }
-
-        return cars;
-    }
-
-    private boolean isMovePossible(int randomValue) {
-        return randomValue >= MOVE_POSSIBLE_BOUND;
     }
 }
--- src/main/java/com/step3/model/car/Cars.java
+++ src/main/java/com/step3/model/car/Cars.java
@@ -1,5 +1,8 @@
 package com.step3.model.car;
 
+import com.step3.util.RandomUtil;
+
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,14 +20,28 @@ public class Cars {
         this.cars = cars;
     }
 
-    public List<Car> getWinnerCars() {
-        int maxPosition = cars.stream().mapToInt(c -> c.getPosition().getValue()).max().getAsInt();
-        return cars.stream()
-                .filter(c -> c.getPosition().getValue() == maxPosition)
-                .collect(Collectors.toList());
+    private Cars(List<Car> cars) {
+        this.cars = cars;
     }
 
-    public List<Car> getCars() {
-        return cars;
+    public Cars moveCars() {
+        for (Car car : this.cars) {
+            car.move(Car.isMovePossible(RandomUtil.getRandomValue()));
+        }
+
+        return new Cars(cars);
+    }
+
+    public Cars getWinnerCars() {
+        int maxPosition = cars.stream().mapToInt(c -> c.getPosition().getValue()).max().getAsInt();
+        List<Car> winnerCars = cars.stream()
+                .filter(c -> c.getPosition().getValue() == maxPosition)
+                .collect(Collectors.toList());
+
+        return new Cars(winnerCars);
+    }
+
+    public List<Car> getList() {
+        return Collections.unmodifiableList(cars);
     }
 }
