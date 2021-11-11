88e017442f2443cf6525c77a91fbf11e058400cf
Han <hankim@tapasmedia.co>
2021-11-11 20:50:14 +0900

    feat : add List<String> users

--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,7 +38,7 @@ class RacingTest {
     @Test
     @DisplayName(value = "생성")
     void create() {
-        Racing racing = new Racing(1, 2);
+        Racing racing = new Racing(Arrays.asList("1"), 2);
         assertAll(() -> assertThat(racing.getCars().size()).isEqualTo(1),
             () -> assertThat(racing.getAttempts()).isEqualTo(2));
     }
@@ -43,7 +46,7 @@ class RacingTest {
     @Test
     @DisplayName(value = "플레이")
     void play() {
-        Racing racing = new Racing(NumberHelper.getRandomValue(3) + 1, NumberHelper.getRandomValue(5) + 1);
+        Racing racing = new Racing(getUsers(NumberHelper.getRandomValue(3) + 1), NumberHelper.getRandomValue(5) + 1);
         List<Car> cars = racing.getCars();
 
         racing.play(new Random());
@@ -54,7 +57,7 @@ class RacingTest {
     @ParameterizedTest(name = "예상된 랜덤값을 통한 플레이")
     @MethodSource("indexCarsAndResultProvider")
     void playWithExpectedRandomValue(int i, int j, int k) {
-        Racing racing = new Racing(3, 3);
+        Racing racing = new Racing(getUsers(3), 3);
         racing.play(new DeterministicRandom());
 
         List<Car> list = racing.getLogs().get(i);
@@ -64,7 +67,7 @@ class RacingTest {
     @ParameterizedTest(name = "잘못 생성 시, 에러를 던진다")
     @MethodSource("carSAndAttemptsProvider")
     void throwExceptionWhenCreated(int cars, int attempts) {
-        assertThatThrownBy(() -> new Racing(cars ,attempts)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Racing(getUsers(cars) ,attempts)).isInstanceOf(IllegalArgumentException.class);
     }
 
     static Stream<Arguments> indexCarsAndResultProvider() {
@@ -87,5 +90,11 @@ class RacingTest {
             Arguments.arguments(1, -1),
             Arguments.arguments(0, 0)
         );
+    }
+
+    List<String> getUsers(int number) {
+        return IntStream.rangeClosed(1, number)
+            .mapToObj(String::valueOf)
+            .collect(Collectors.toList());
     }
 }
