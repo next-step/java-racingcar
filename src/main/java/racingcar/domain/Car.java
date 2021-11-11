f9bf667d9b05981206862fe4bc0b366b51edce0a
las9897 <las9897@gmail.com>
2021-11-11 14:27:14 +0900

    feat: 자동차 클래스에 이름 속성 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,9 +5,15 @@ public class Car {
 
     private static final int UPPER_LIMIT = 4;
     private int state;
+    private String name;
 
     public Car() {
         this.state = 0;
+    }
+
+    public Car(String name) {
+        this.state = 0;
+        this.name = name;
     }
 
     public void increaseState(int random) {
@@ -26,5 +32,9 @@ public class Car {
 
     public int getState() {
         return state;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,17 +1,18 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
     private Car car;
 
     @BeforeEach
-    void setup(){
+    void setup() {
         car = new Car();
     }
 
@@ -19,6 +20,13 @@ public class CarTest {
     @DisplayName("매개변수가 4 이상일 경우 state 1 증가 테스트")
     void increaseState() {
         car.increaseState(4);
-        Assertions.assertThat(car.getState()).isEqualTo(1);
+        assertThat(car.getState()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("이름있는 자동차 생성 테스트")
+    void createCarWithName() {
+        Car car = new Car("jeje");
+        assertThat(car.getName()).isEqualTo("jeje");
     }
 }
