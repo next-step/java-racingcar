42f80ad8866218b70b373561edd2ad542662992c
dante(이산하) <dante.93@kakaocorp.com>
2021-11-12 00:21:36 +0900

    feature (이동할 횟수와 이동할 자동차 대수 입력)

---
+++ src/main/java/study/car/race/InputView.java
@@ -0,0 +1,21 @@
+package study.car.race;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static int acceptCarCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static int acceptTryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
