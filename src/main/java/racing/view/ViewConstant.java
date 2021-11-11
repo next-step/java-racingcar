5667503af33dc6c59be6f126b25f1d6308e1776a
Han <hankim@tapasmedia.co>
2021-11-11 20:27:16 +0900

    feat : update car message

--- src/main/java/racing/view/ViewConstant.java
+++ src/main/java/racing/view/ViewConstant.java
@@ -5,7 +5,7 @@ package racing.view;
  */
 public class ViewConstant {
 
-    public static final String CAR_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    public static final String CAR_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String ATTEMPT_MESSAGE = "시도할 회수는 몇 회 인가요?";
 
     private ViewConstant() {
