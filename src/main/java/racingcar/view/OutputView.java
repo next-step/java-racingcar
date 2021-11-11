8779a02f4716b31295d6f32f40a8bc177ba8248d
las9897 <las9897@gmail.com>
2021-11-11 11:22:25 +0900

    feat: 출력 역할을 하는 Output View 클래스 생성

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public static void print(){
+        System.out.println();
+    }
+    public static void print(String message){
+        System.out.println(message);
+    }
+}
