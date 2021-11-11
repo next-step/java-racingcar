4a6ccd040556fde18e489f22744a56750ed6d778
mj950425 <minjoon1995@naver.com>
2021-11-11 19:02:48 +0900

    [refactoring] CalculatorEnum -> Operator로 이름 변경

--- src/main/java/step2/model/Calculator.java
+++ src/main/java/step2/model/Calculator.java
@@ -1,18 +1,19 @@
 package step2.model;
 
 public class Calculator {
+
     private Calculator() {
     }
 
-    public static Integer operator(String str) {
+    public static Integer calculateAll(String str) {
         String[] stringArray = str.split(" ");
         Integer firstIndex = 0;
         Integer result = new Integer(stringArray[firstIndex]);
         for (int i = 1; i < stringArray.length; i += 2) {
             String operator = stringArray[i];
             Integer num1 = new Integer(stringArray[i+1]);
-            CalculatorEnum operatorValue = CalculatorEnum.findOperator(operator);
-            result = CalculatorEnum.calculator(result, num1, operatorValue);
+            Operator operatorValue = Operator.findOperator(operator);
+            result = Operator.calculator(result, num1, operatorValue);
         }
         return result;
     }
