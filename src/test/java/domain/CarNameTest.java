88a044d79c0098e10148339ece86f136ebd95391
user <kth4878@naver.com>
2021-11-12 03:04:45 +0900

    [Remove] 불필요 클래스 삭제 및 삭제로 인한 테스트 이름변경

--- src/test/java/domain/CarNameTest.java
+++ src/test/java/domain/CarNameTest.java
@@ -10,25 +10,25 @@ class CarGroupTest {
     @Test
     @DisplayName("경주용 자동차 생성")
     public void createRaceCar() {
-        CarGroup carGroup = new CarGroup("BMW,KIA,HONDA");
-        assertThat(carGroup.size()).isEqualTo(3);
+        CarNames carNames = new CarNames("BMW,KIA,HONDA");
+        assertThat(carNames.size()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("자동차 개수 2이상 확인")
     public void checkTwoCars(){
-        CarGroup carGroup = new CarGroup("BMW,KIA");
-        assertThat(carGroup.size()).isEqualTo(2);
+        CarNames carNames = new CarNames("BMW,KIA");
+        assertThat(carNames.size()).isEqualTo(2);
     }
 
     @Test
     @DisplayName("경주용 자동차 이름확인")
     public void checkRaceCarName() {
-        CarGroup carGroup = new CarGroup("BMW,KIA,HONDA");
+        CarNames carNames = new CarNames("BMW,KIA,HONDA");
         assertAll(
-                () -> assertThat(carGroup.findCarName(0)).isEqualTo("BMW"),
-                () -> assertThat(carGroup.findCarName(1)).isEqualTo("KIA"),
-                () -> assertThat(carGroup.findCarName(2)).isEqualTo("HONDA")
+                () -> assertThat(carNames.carName(0)).isEqualTo("BMW"),
+                () -> assertThat(carNames.carName(1)).isEqualTo("KIA"),
+                () -> assertThat(carNames.carName(2)).isEqualTo("HONDA")
         );
     }
 }
