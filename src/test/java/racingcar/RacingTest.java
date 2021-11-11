939b02f3dab549f5df3af4928281893aafa71ec6
las9897 <las9897@gmail.com>
2021-11-11 14:52:46 +0900

    style: assertThat static으로 변경

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -1,6 +1,5 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +7,9 @@ import racingcar.domain.Car;
 import racingcar.domain.Racing;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class RacingTest {
 
@@ -22,21 +24,21 @@ public class RacingTest {
     @DisplayName("List<Car> 생성 테스트")
     void makeCarTest() {
         List<Car> carList = racing.makeCar(5);
-        Assertions.assertThat(carList.size()).isEqualTo(5);
+        assertThat(carList.size()).isEqualTo(5);
     }
 
     @Test
     @DisplayName("random 값이 10일 경우, 자동차들이 1번 움직였을 경우 테스트.")
-    void goingTryTest(){
+    void goingTryTest() {
         racing.goingTry(10);
         for (Car car : racing.getCarList()) {
-            Assertions.assertThat(car.getState()).isEqualTo(1);
+            assertThat(car.getState()).isEqualTo(1);
         }
     }
 
     @Test
     @DisplayName("자동차 경주 통합 테스트")
-    void racingStart(){
+    void racingStart() {
         racing.start();
     }
 
