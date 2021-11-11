30d45fad8c4378e337d5633aa27aee5adf0a4e04
Han <hankim@tapasmedia.co>
2021-11-11 10:01:14 +0900

    refactor : add static create()

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -8,12 +8,16 @@ import static racing.RacingConstant.*;
 public class Car {
     private final int step;
 
-    public Car(int step) {
+    private Car(int step) {
         this.step = step;
     }
 
     public int getStep() {
         return this.step;
+    }
+
+    public static Car create() {
+        return new Car(0);
     }
 
     public Car getInstanceByForward(int random) {
