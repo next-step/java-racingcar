d3b7fe83cf95a0ed133ab3e1f3a7889cca25a764
Han <hankim@tapasmedia.co>
2021-11-11 21:09:35 +0900

    feat : add printWinner()

--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -4,12 +4,14 @@ import racing.Car;
 import racing.Racing;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author han
  */
 public class ResultView {
     private String message = "실행 결과";
+    private String subFix = "가 최종 우승했습니다.";
 
     public void print(Racing racing) {
         System.out.println(this.message);
@@ -18,6 +20,7 @@ public class ResultView {
             List<Car> cars = racing.getLogs().get(i);
             printStep(cars);
         }
+        printWinner(racing);
     }
 
     private void printStep(List<Car> cars) {
@@ -39,5 +42,23 @@ public class ResultView {
             sb.append('-');
         }
         return sb.toString();
+    }
+
+    private void printWinner(Racing racing) {
+        List<Car> winners = racing.getWinners();
+        System.out.println(createWinner(winners));
+    }
+
+    private String createWinner(List<Car> cars) {
+        StringBuilder sb = new StringBuilder();
+        String winnerNames = getWinnerNames(cars);
+        sb.append(winnerNames + subFix);
+        return sb.toString();
+    }
+
+    private String getWinnerNames(List<Car> cars) {
+        return cars.stream()
+            .map(Car::getName)
+            .collect(Collectors.joining(","));
     }
 }
