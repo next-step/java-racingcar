bbd8ff988e71ea9be2b5ec2797753855e91260b1
soosue <yunsoo0510@daum.net>
2021-11-11 10:23:58 +0900

    refactor : racing car를 생성하는 방법 변경

--- src/main/java/racingcargame/repository/RacingCarFactory.java
+++ src/main/java/racingcargame/repository/RacingCarFactory.java
@@ -5,18 +5,14 @@ import racingcargame.domain.RacingCar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RacingCarRepository {
+public class RacingCarFactory {
 
-    private List<RacingCar> racingCars;
-
-    public RacingCarRepository(int carCount) {
-        racingCars = new ArrayList<>();
+    public static List<RacingCar> makeCars(int carCount) {
+        List<RacingCar> racingCars = new ArrayList<>();
         while (carCount-- > 0) {
             racingCars.add(new RacingCar());
         }
-    }
-
-    public List<RacingCar> getRacingCars() {
         return racingCars;
     }
+
 }
