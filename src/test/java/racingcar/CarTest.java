d8c6c753c17da500b38e570b519951f9fb06f587
giyeon95 <giyeon15@gmail.com>
2021-11-13 00:40:32 +0900

    refactor 코드리뷰 반영 - Command 제거 / 매직 넘버 제거

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -49,7 +49,6 @@ class CarTest {
 
         assertThat(afterPosition).isEqualTo(beforePosition + 1);
     }
-
     @ValueSource(ints = {10, 5, 3, 1})
     @ParameterizedTest(name = "[{arguments}] 자동차의 position에 따라, 알맞는 거리를 반환한다.")
     void getDistanceByPositionTest(int position) {
