7e1e240b165b56f0851bc11afafe01aa4936f46f
dodeon <sojeongw.official@gmail.com>
2021-11-12 16:38:45 +0900

    feat: 0과 9 사이의 랜덤 값을 구한다

--- src/test/java/study/step_3/RacingCarTest.java
+++ src/test/java/study/step_3/RacingCarTest.java
@@ -1,4 +1,20 @@
 package study.step_3;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RacingCarTest {
+
+    @Test
+    @DisplayName("0과 9 사이의 랜덤 값을 구한다")
+    void shouldReturnRandomNumber() {
+        Random random = new Random();
+        int result = random.nextInt(10);
+
+        assertThat(result).isLessThan(10);
+    }
 }
