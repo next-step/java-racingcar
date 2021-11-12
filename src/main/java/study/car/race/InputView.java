67719f0d24de004b7d1d6ac0f77077a6fba07762
dante(이산하) <dante.93@kakaocorp.com>
2021-11-13 00:54:35 +0900

    refactor (extract magic literal)

--- src/main/java/study/car/race/InputView.java
+++ src/main/java/study/car/race/InputView.java
@@ -2,16 +2,19 @@ package study.car.race;
 
 public class InputView {
 
+    private static final String QUETION_ABOUT_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
+    private static final String QUETION_ABOUT_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
+
     private InputView() {
     }
 
     public static int acceptCarCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
+        System.out.println(QUETION_ABOUT_CAR_COUNT);
         return ScannerUtils.nextInt();
     }
 
     public static int acceptTryCount() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
+        System.out.println(QUETION_ABOUT_TRY_COUNT);
         return ScannerUtils.nextInt();
     }
 }
