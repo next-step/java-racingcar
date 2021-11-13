80d6f806e0eab69ff6066881a85beec7716bddf2
MoonJiHyun <boo74153@naver.com>
2021-11-13 13:07:28 +0900

    test 리팩토링

--- src/test/java/racingCar/RacingCarTest.java
+++ src/test/java/racingCar/RacingCarTest.java
@@ -3,20 +3,22 @@ package racingCar;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RacingCarTest {
 
-  @Test
-  public void moveRacingCarTest() {
-    RacingCar car = new RacingCar(0);
-    car.tryMove(8);
-    assertThat(car.getMoveCount()).isEqualTo(1);
-  }
+  @ParameterizedTest
+  @CsvSource(value = {"1,2,3:0", "5,8:1"}, delimiter = ':')
+  public void moveRacingCarTest(String input, String expected) {
+    String[] values = input.split(",");
+    int expectedMoveCount = Integer.parseInt(expected);
 
-  @Test
-  public void notMoveRacingCarTest() {
-    RacingCar car = new RacingCar(0);
-    car.tryMove(2);
-    assertThat(car.getMoveCount()).isEqualTo(0);
+    for (String value : values) {
+      RacingCar car = new RacingCar(0);
+      car.tryMove(Integer.parseInt(value));
+      assertThat(car.getMoveCount()).isEqualTo(expectedMoveCount);
+    }
   }
 }
