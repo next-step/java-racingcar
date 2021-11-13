5d4a5338ace2047c9345a3cdd9796fc0b5a8b2ab
JaeHan-Kim <hgal8877@gmail.com>
2021-11-13 13:36:41 +0900

    test : Names 테스트 케이스 추가

---
+++ src/test/java/racinggame/domain/NameTest.java
@@ -0,0 +1,31 @@
+package racinggame.domain;
+
+import calculator.Calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameTest {
+
+    @DisplayName("Name 객체 생성 테스트")
+    @Test
+    void createName() throws Exception {
+        String excepted = "jae";
+        Name name = new Name(excepted);
+
+        assertThat(name).isNotNull();
+        assertThat(name.getName()).isEqualTo(excepted);
+    }
+
+    @DisplayName("이름이 5글자를 초과하면 객체가 예외가 발생한다.")
+    @Test
+    void createNameFail() throws Exception {
+        String name = "jaehan";
+
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(Exception.class)
+                .hasMessageContaining("[ERROR] 자동차의 이름은 5글자 이하여야 합니다.");
+    }
+}
