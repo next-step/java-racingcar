66c3987ac6a7afb2df202b577416b9ecf2244fa1
DongHyo <kdhyo98@gmail.com>
2021-11-13 21:17:04 +0900

    feat : car 클래스 구현

---
+++ src/main/java/racingcar/domain/model/Car.java
@@ -0,0 +1,26 @@
+package racingcar.domain.model;
+
+import racingcar.domain.strategy.MoveStrategy;
+
+public class Car {
+
+    private final Name name;
+    private MoveHistory moveHistory;
+
+    private Car(String name) {
+        this.name = Name.from(name);
+        this.moveHistory = MoveHistory.create();
+    }
+
+    public static Car from(String name) {
+        return new Car(name);
+    }
+
+    public void move(MoveStrategy moveStrategy) {
+        moveHistory.addHistory(moveStrategy.isMove());
+    }
+
+    public long getHistorySuccessCount(int toIndex) {
+        return moveHistory.getSuccessCount(toIndex);
+    }
+}
---
+++ src/main/java/racingcar/domain/strategy/MoveStrategy.java
@@ -0,0 +1,7 @@
+package racingcar.domain.strategy;
+
+public interface MoveStrategy {
+
+    boolean isMove();
+
+}
