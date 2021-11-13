861b2d445e34cd385bc6c2e3d159a5ead59acafb
jiseok-choi <choi300300@gmail.com>
2021-11-13 21:03:14 +0900

    feat: 리뷰 반영 - 주행 조건 exception 로직 추가 및 테스트 코드 추가

--- src/main/java/carracing/Car.java
+++ src/main/java/carracing/Car.java
@@ -13,15 +13,27 @@ public class Car {
 
     private int drivingHistory;
 
-    private static final int MOVE_CONDITION = 4;     /* 주행 조건 */
+    private static final int MOVE_CONDITION = 4;         /* 주행 조건 */
+    private static final int MOVE_CONDITION_MIN = 0;     /* 최소 주행 범위 */
+    private static final int MOVE_CONDITION_MAX = 9;     /* 최대 주행 범위 */
+
+    String ERR_MSG_CONDITION_RANGE = "주행 조건은 0 ~ 9 사이 값입니다.";
 
     public boolean driving(int driveCondition) {
         boolean isDrive = false;
+
+        valid(driveCondition);
         if (driveCondition >= MOVE_CONDITION) {
             this.drivingHistory++;
             isDrive = true;
         }
         return isDrive;                              /* 주행 여부 반환 */
+    }
+
+    public void valid(int driveCondition) {
+        if (MOVE_CONDITION_MAX < driveCondition || MOVE_CONDITION_MIN > driveCondition) {
+            throw new NumberFormatException(ERR_MSG_CONDITION_RANGE);
+        }
     }
 
     public int getDrivingHistory() {
--- src/test/java/carracing/CarTest.java
+++ src/test/java/carracing/CarTest.java
@@ -4,16 +4,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class CarTest {
 
     @Test
-    @DisplayName("전진하는 조건은 0에서 9사이 4이상")
+    @DisplayName("전진하는 조건은 0에서 9사이")
     void drivingCondition() {
-        Car car = new Car();
-        for (int i = 0; i < 10; i++) {
-            car.driving(i);
-        }
-        Assertions.assertThat(car.getDrivingHistory()).isEqualTo(6);
+        assertThatThrownBy(() -> {
+            Car car = new Car();
+            car.driving(10);
+        }).isInstanceOf(NumberFormatException.class);
     }
 
     @Test
