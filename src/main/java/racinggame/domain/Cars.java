efdf2409bfec9e7ab789981ddfa2a4d5cc81ff80
JaeHan-Kim <hgal8877@gmail.com>
2021-11-13 13:54:39 +0900

    fix : 우숭자를 구할 수 있는 로직 추가

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -1,5 +1,6 @@
 package racinggame.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,14 +11,16 @@ public class Cars {
 
     private List<Car> cars;
 
-    public static Cars from(Integer carCount) {
-        return Cars.of(carCount, Location.START_LOCATION);
+    public static Cars from(Names names) {
+        return Cars.of(names, Location.START_LOCATION);
     }
 
-    public static Cars of(Integer carCount, Integer location) {
-        List<Car> entryCars = (IntStream.range(START_INDEX, carCount)
-                .mapToObj((idx) -> new Car(location))
-                .collect(Collectors.toList()));
+    public static Cars of(Names names, Integer location) {
+
+        List<Car> entryCars = names.getNames()
+                                    .stream()
+                                    .map(name -> new Car(name, location))
+                                    .collect(Collectors.toList());
         return new Cars(entryCars);
     }
 
@@ -32,6 +35,29 @@ public class Cars {
     public void roundRacing(StateGenerator stateGenerator) {
         for (Car car : cars) {
             car.move(stateGenerator);
+        }
+    }
+
+    public Location inFormFirstRecord() {
+        Car car = cars.get(0);
+        Location location = car.getLocation();
+        for (int i = 1; i < cars.size(); i++) {
+            location = location.compareToLocation(cars.get(i).getLocation());
+        }
+        return location;
+    }
+
+    public Names inFormWinners(Location firstRecord) {
+        List<Name> names = new ArrayList<>();
+        for (Car car : cars) {
+            compareToRecord(names, car, firstRecord);
+        }
+        return new Names(names);
+    }
+
+    private void compareToRecord(List names, Car car, Location firstRecord) {
+        if (firstRecord.equals(car.getLocation())) {
+            names.add(car.getName());
         }
     }
 }
