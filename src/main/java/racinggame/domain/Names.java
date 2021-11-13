f2a7a8c734c5268863205aa82389204f3f355b2f
JaeHan-Kim <hgal8877@gmail.com>
2021-11-13 13:32:23 +0900

    feat : 이름의 목록을 저장하는 일급 컬렉션 추가

---
+++ src/main/java/racinggame/domain/Names.java
@@ -0,0 +1,36 @@
+package racinggame.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Names {
+    private static final String CAR_NAME_SEPARATORS = ",";
+
+    private final List<Name> names;
+
+    public static Names from(String text) throws Exception {
+        String[] nameArr = text.split(CAR_NAME_SEPARATORS);
+        List<Name> names = new ArrayList<>();
+        for (String name : nameArr) {
+            names.add(new Name(name));
+        }
+        return new Names(names);
+    }
+
+    Names(List<Name> names) {
+        this.names = names;
+    }
+
+    public List<Name> getNames() {
+        return names;
+    }
+
+    public String printNames() {
+        StringBuilder printName = new StringBuilder();
+        for (Name name : names) {
+            printName.append(name.getName());
+            printName.append(",");
+        }
+        return printName.substring(0, printName.length() - 1);
+    }
+}
