bec48d77143ffb0b5b00ec74a7ff579c19b56090
mj950425 <minjoon1995@naver.com>
2021-11-13 17:20:35 +0900

    Delete NameCheck.java

--- src/main/java/racing/service/NameCheck.java
+++
@@ -1,9 +0,0 @@
-package racing.service;
-
-import java.util.List;
-
-public class NameCheck {
-    public static boolean nameCheck(List<String> nameList) {
-        return nameList.stream().anyMatch(i -> i.length() > 5);
-    }
-}
