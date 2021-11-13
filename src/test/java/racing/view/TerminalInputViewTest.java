a14df983ee66d8e602dd6ad4ca852c8da4327adc
DaehunGwak <rhkrgns56@naver.com>
2021-11-13 17:40:14 +0900

    refactor: TerminalOutputViewTest 에서 출력 라인 수 테스트 및 정리
    - 출력까지는 테스트 하고 있지 않아 출력된 라인 수를 통한 테스트
    - BeforeEach, AfterEach 를 통한 setUp 은 테스트 로직에서 분
    https://github.com/next-step/java-racingcar/pull/2835#discussion_r747110270

--- src/test/java/racing/view/TerminalInputViewTest.java
+++ src/test/java/racing/view/TerminalInputViewTest.java
@@ -1,44 +1,57 @@
 package racing.view;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("3단계 - 자동차 경주 - TerminalInputView 단위 테스트")
 class TerminalInputViewTest {
 
+    InputStream stdin = System.in;
+    PrintStream stdout = System.out;
+    OutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(stdout);
+        System.setIn(stdin);
+    }
+
     @Test
     @DisplayName("차 수 입력 기능")
     void inputNumberOfCar() {
         String data = "3";
-        InputStream stdin = System.in;
-        System.setIn(
-                new ByteArrayInputStream(data.getBytes()));
+        setUpDataInputStream(data);
 
         int numberOfCar = TerminalInputView.inputNumberOfCar();
 
         assertThat(numberOfCar).isEqualTo(3);
-
-        System.setIn(stdin);
     }
 
     @Test
     @DisplayName("이동 횟수 입력 기능")
     void inputNumberOfMove() {
         String data = "5";
-        InputStream stdin = System.in;
-        System.setIn(
-                new ByteArrayInputStream(data.getBytes()));
+        setUpDataInputStream(data);
 
         int numberOfMove = TerminalInputView.inputNumberOfMove();
 
         assertThat(numberOfMove).isEqualTo(5);
-
-        System.setIn(stdin);
     }
 
+    private void setUpDataInputStream(String data) {
+        System.setIn(
+                new ByteArrayInputStream(data.getBytes()));
+    }
 }
--- src/test/java/racing/view/TerminalOutputViewTest.java
+++ src/test/java/racing/view/TerminalOutputViewTest.java
@@ -1,14 +1,35 @@
 package racing.view;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racing.model.Car;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DisplayName("3단계 - 자동차 경주 - TerminalOutputView 단위 테스트")
 class TerminalOutputViewTest {
+
+    PrintStream stdout = System.out;
+    OutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(stdout);
+    }
 
     @Test
     @DisplayName("자동차 상태 출력 기능")
@@ -21,5 +42,10 @@ class TerminalOutputViewTest {
             cars.forEach(Car::moveRandom);
             TerminalOutputView.printCars(cars);
         }
+
+        String[] lines = outputStream.toString()
+                .split(System.lineSeparator());
+        assertThat(lines.length)
+                .isEqualTo((cars.size() + 1) * numberOfMove);
     }
 }
