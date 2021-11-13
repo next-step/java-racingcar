0fac33d34721a16b8dee6fd1e80ea76b438cac99
DongHyo <kdhyo98@gmail.com>
2021-11-14 02:06:43 +0900

    refactor : 코드정리

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -11,9 +11,9 @@ public class ResultView {
 
     private static final int START_FROM_INDEX = 1;
     private static final String DASH = "-";
+    private static final String EMPTY = "";
     private static final String RACING_FORMAT = "%s : %s";
     private static final String WINNER_FORMAT = "%s 가 최종 우승했습니다.";
-    private static final String EMPTY = "";
 
     private final Cars cars;
 
@@ -30,10 +30,10 @@ public class ResultView {
         outputWinners();
     }
 
-    private void outputCarHistory(int i) {
+    private void outputCarHistory(int toIndex) {
         for (Car car : cars.getCars()) {
             String name = car.getName();
-            int count = (int) car.getHistorySuccessCount(i);
+            int count = (int) car.getHistorySuccessCount(toIndex);
             String move = String.join(EMPTY, Collections.nCopies(count, this.DASH));
 
             System.out.println(String.format(RACING_FORMAT, name, move));
@@ -49,7 +49,6 @@ public class ResultView {
         return winners.getCars().stream()
             .map(Car::getName)
             .collect(Collectors
-                .toCollection(ArrayList::new)
-            );
+                .toCollection(ArrayList::new));
     }
 }
