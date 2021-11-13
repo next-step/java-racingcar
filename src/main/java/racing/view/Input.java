24c358d9f913dbb0697311622df955a9b9d68582
mj950425 <minjoon1995@naver.com>
2021-11-13 17:21:33 +0900

    [refactoring] while문의 validation기능 InputBuilder로 옮김

--- src/main/java/racing/view/Input.java
+++ src/main/java/racing/view/Input.java
@@ -1,6 +1,6 @@
 package racing.view;
 
-import racing.service.ConvertInput;
+import racing.service.InputBuilder;
 
 import java.util.List;
 import java.util.Scanner;
@@ -10,23 +10,18 @@ public class Input {
     private Input() {
     }
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static List<String> getNameList() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        Scanner scanner = new Scanner(System.in);
         String nameString = scanner.nextLine();
-        ConvertInput convertInput = new ConvertInput();
-        List<String> nameList = convertInput.convertInput(nameString);
-        while (convertInput.nameLengthCheck(nameList)) {
-            System.out.println("길이가 5를 초과하는 이름이 있습니다, 다시 입력해주세요");
-            nameString = scanner.nextLine();
-            nameList = convertInput.convertInput(nameString);
-        }
+        InputBuilder inputBuilder = new InputBuilder(nameString);
+        List<String> nameList = inputBuilder.getNameList();
         return nameList;
     }
 
     public static Integer getCuntOfTry() {
         System.out.println("시도할 횟수는?");
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 }
