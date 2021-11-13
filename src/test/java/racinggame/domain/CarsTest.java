cedda1dc8099f34b3aec195353fd14f00439ebaf
JaeHan-Kim <hgal8877@gmail.com>
2021-11-13 13:55:04 +0900

    fix : 최고기록과 우승자 테스트 케이스 추가

--- src/test/java/racinggame/domain/CarsTest.java
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -1,5 +1,6 @@
 package racinggame.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,32 +8,67 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
 
-    private static final Integer ENTRY_CAR_COUNT = 2;
+    private static final String ENTRY_CAR_NAMES = "jae,han";
 
     @DisplayName("경주에 참가한 자동차들이 이동한지 확인")
     @Test
-    void carsMoveTest() {
+    void carsMoveTest() throws Exception {
         StateGenerator stateGenerator = () -> State.MOVE;
         int actual = 2;
-        int excepted = 3;
-        Cars cars = Cars.of(ENTRY_CAR_COUNT, actual);
+        Location excepted = new Location(3);
+        Names names = Names.from(ENTRY_CAR_NAMES);
+
+        Cars cars = Cars.of(names, actual);
         cars.roundRacing(stateGenerator);
 
         for (Car car : cars.getCars()) {
-            assertThat(car.getLocation().getLocation()).isEqualTo(excepted);
+            assertThat(car.getLocation()).isEqualTo(excepted);
         }
     }
 
     @DisplayName("경주에 참가한 자동차들이 정지한지 확인")
     @Test
-    void carsStopTest() {
+    void carsStopTest() throws Exception {
         StateGenerator stateGenerator = () -> State.STOP;
         int location = 2;
-        Cars cars = Cars.of(ENTRY_CAR_COUNT, location);
+        Names names = Names.from(ENTRY_CAR_NAMES);
+
+        Cars cars = Cars.of(names, location);
         cars.roundRacing(stateGenerator);
 
         for (Car car : cars.getCars()) {
             assertThat(car.getLocation().getLocation()).isEqualTo(location);
         }
+    }
+
+    @DisplayName("경주에 참가한 자동차들이 전진 했을 때 최고기록의 위치를 테스트 ")
+    @Test
+    void carsFirstRecordTest() throws Exception {
+        StateGenerator stateGenerator = () -> State.MOVE;
+        int location = 2;
+        Names names = Names.from(ENTRY_CAR_NAMES);
+        Location excepted = new Location(3);
+
+        Cars cars = Cars.of(names, location);
+        cars.roundRacing(stateGenerator);
+
+        assertThat(cars.inFormFirstRecord()).isEqualTo(excepted);
+    }
+
+    @DisplayName("경주에 참가한 자동차들의 우승자를 확인")
+    @Test
+    void carsWinnerTest() throws Exception {
+        StateGenerator stateGenerator = () -> State.MOVE;
+        int actual = 2;
+        Location excepted = new Location(3);
+        Names names = Names.from(ENTRY_CAR_NAMES);
+
+        Cars cars = Cars.of(names, actual);
+        cars.roundRacing(stateGenerator);
+
+        Location firstRecord = cars.inFormFirstRecord();
+        Names winner = cars.inFormWinners(firstRecord);
+
+        assertThat(winner.printNames()).isEqualTo(ENTRY_CAR_NAMES);
     }
 }
