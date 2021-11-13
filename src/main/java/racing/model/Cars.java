53970baa4ab8360b955acaee92cacdbc604a20b9
mj950425 <minjoon1995@naver.com>
2021-11-13 14:41:34 +0900

    [refactoring] 차 객체 만드는 메소드 분리 및 movableCarList이름변경

--- src/main/java/racing/model/Cars.java
+++ src/main/java/racing/model/Cars.java
@@ -3,25 +3,29 @@ package racing.model;
 import racing.strategy.MoveStrategy;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Cars {
 
     private final List<Car> carList;
 
-    public List<Car> getCarList() {
-        return carList;
+    public Cars(List<String> nameList) {
+        this.carList = makeCarList(nameList);
     }
 
-    public Cars(List<String> nameList) {
+    public List<Car> getCarList() {
+        return Collections.unmodifiableList(carList);
+    }
+
+    public List<Car> makeCarList(List<String> nameList) {
         List<Car> carList = new ArrayList<Car>();
         for (String name : nameList) {
             carList.add(new Car(name));
         }
-        this.carList =  carList;
+        return carList;
     }
-
-    public void movableCarList(MoveStrategy moveStrategy) {
+    public void playRacing(MoveStrategy moveStrategy) {
         for (Car car : carList) {
             car.movable(moveStrategy);
         }
