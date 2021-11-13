8ee40e471b40c5adae53e4e8822ab4064e68ea53
JaeHan-Kim <hgal8877@gmail.com>
2021-11-13 13:38:12 +0900

    fix : 우승자를 출력하는 기능 추가

--- src/main/java/racinggame/view/ResultView.java
+++ src/main/java/racinggame/view/ResultView.java
@@ -3,26 +3,35 @@ package racinggame.view;
 import racinggame.domain.Car;
 import racinggame.domain.Cars;
 import racinggame.domain.Location;
+import racinggame.domain.Names;
 
 public class ResultView {
 
     private static final String DASH = "-";
+    private static final String MESSAGE_PLAYER_LOCATION = "%s : %s";
+    private static final String MESSAGE_RACE_WINNER = "\n%s가 최종 우승했습니다.";
 
     private ResultView() {
     }
 
     public static void currentRecord(Cars cars) {
         for (Car car : cars.getCars()) {
-            locationResult(car.getLocation());
+            locationResult(car);
         }
         System.out.println();
     }
 
-    public static void locationResult(Location location) {
+    public static void locationResult(Car player) {
         StringBuilder racingRecord = new StringBuilder();
-        for (int i = 0; i < location.getLocation(); i++) {
+        for (int i = 0; i < player.getLocation().getLocation(); i++) {
             racingRecord.append(DASH);
         }
-        System.out.print("\n" + racingRecord);
+        System.out.print("\n" + String.format(MESSAGE_PLAYER_LOCATION, player.getName().getName(), racingRecord));
+    }
+
+    public static void awardRacing(Cars cars) {
+        Location firstRecord = cars.inFormFirstRecord();
+        Names winners = cars.inFormWinners(firstRecord);
+        System.out.print(String.format(MESSAGE_RACE_WINNER, winners.printNames()));
     }
 }
