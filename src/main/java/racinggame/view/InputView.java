d5c11478caf5bc96c89a403bb9f0b8c42d6cbb90
JaeHan-Kim <hgal8877@gmail.com>
2021-11-13 13:37:30 +0900

    fix : 기존 자동차의 숫자 입력에서 이름 입력으로 변경

--- src/main/java/racinggame/view/InputView.java
+++ src/main/java/racinggame/view/InputView.java
@@ -1,10 +1,14 @@
 package racinggame.view;
 
 import racinggame.domain.Laps;
+import racinggame.domain.Names;
 
 import java.util.Scanner;
 
 public class InputView {
+
+    private static String MESSAGE_ENTRY_CARS_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static String MESSAGE_TYR_COUNT = "시도할 회수는 몇회인가요?";
 
     private final Scanner scanner;
 
@@ -12,13 +16,14 @@ public class InputView {
         scanner = new Scanner(System.in);
     }
 
-    public Integer inputCarCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.nextInt();
+    public Names inputEntryCarNames() throws Exception {
+        System.out.println(MESSAGE_ENTRY_CARS_NAME);
+
+        return Names.from(scanner.nextLine());
     }
 
     public Laps inputLaps() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
+        System.out.println(MESSAGE_TYR_COUNT);
         return new Laps(scanner.nextInt());
     }
 }
