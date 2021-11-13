57b7d3a5240b9c3e6cf5520d8eeadf7971462b2f
michael.10 <michael.10@kakaocorp.com>
2021-11-13 17:25:59 +0900

    Test Car stay.

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -11,4 +11,11 @@ public class CarTest {
         car.play(new MovingStrategy());
         assertThat(car.location).isEqualTo(1);
     }
+
+    @Test
+    void stay() {
+        final Car card = new Car();
+        card.play(new MovingStrategy());
+        assertThat(card.location).isEqualTo(0);
+    }
 }
