e4c1ae4b66553d5db0ebb0ba401b942e56875c00
sid <sid.seo@kakaocorp.com>
2021-11-14 16:33:20 +0900

    refactor: Position

--- src/test/java/edu/nextstep/camp/racinggame/domain/car/PositionTest.java
+++ src/test/java/edu/nextstep/camp/racinggame/domain/car/PositionTest.java
@@ -21,15 +21,9 @@ public class PositionTest {
     @Test
     public void move() {
         Position position = Position.ofZero();
-        assertThat(position.toInt()).isEqualTo(0);
-        position.forward();
-        assertThat(position.toInt()).isEqualTo(1);
-        position.forward();
-        assertThat(position.toInt()).isEqualTo(2);
-        position.forward();
-        assertThat(position.toInt()).isEqualTo(3);
-        position.forward();
-        assertThat(position.toInt()).isEqualTo(4);
+        Position newPosition = position.forward();
+        assertThat(position).isEqualTo(Position.ofZero());
+        assertThat(newPosition).isEqualTo(Position.of(1));
     }
 
     @Test
