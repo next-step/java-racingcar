55da3720d765006efcbae25361ea5f546a1505c4
kero <jdy410@naver.com>
2021-11-15 01:54:52 +0900

    feat: main 함수 추가

---
+++ src/main/java/step3/RaceMain.java
@@ -0,0 +1,10 @@
+package step3;
+
+public class RaceMain {
+
+    public static void main(String[] args) {
+        RaceController raceController = new RaceController();
+
+        raceController.gameStart();
+    }
+}
