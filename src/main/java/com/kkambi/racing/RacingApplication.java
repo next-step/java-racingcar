7c10d8caf4a53397043250a860b3c896eaeaca3a
kkambee <bok03220@gmail.com>
2021-11-14 18:10:54 +0900

    refactor(step3): 필요 없는 클래스 제거
    - Racer, Bound 제거
    - 별도의 Controller 클래스 대신 main()만 사용

--- src/main/java/com/kkambi/racing/RacingApplication.java
+++ src/main/java/com/kkambi/racing/RacingApplication.java
@@ -1,10 +1,29 @@
 package com.kkambi.racing;
 
-import com.kkambi.racing.controller.RacingController;
+import com.kkambi.racing.domain.Car;
+import com.kkambi.racing.util.Dice;
+import com.kkambi.racing.view.InputView;
+import com.kkambi.racing.view.TotalView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RacingApplication {
 
     public static void main(String[] args) {
-        new RacingController().start();
+        TotalView totalView = new TotalView();
+        List<Car> carList = new ArrayList<>();
+        InputView.InputCommand inputCommand = totalView.getInput();
+
+        for (int i=0; i<inputCommand.getNumberOfCars(); i++) {
+            carList.add(new Car());
+        }
+
+        for (int times = 1; times <= inputCommand.getNumberOfAttempts(); times++) {
+            for (Car car : carList) {
+                car.tryToMove(Dice.roll(10));
+            }
+            totalView.printLocation(times, carList);
+        }
     }
 }
