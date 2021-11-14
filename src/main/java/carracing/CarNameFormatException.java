0dc11d4f97c939ee90865715fb8831ed996d9fc0
jiseok-choi <choi300300@gmail.com>
2021-11-14 09:47:09 +0900

    feat: 자동차 이름 Exception 클래스 추가

---
+++ src/main/java/carracing/CarNameFormatException.java
@@ -0,0 +1,11 @@
+package carracing;
+
+public class CarNameFormatException extends Exception {
+
+    public CarNameFormatException() {
+    };
+
+    public CarNameFormatException(String message) {
+        super(message);
+    }
+}
