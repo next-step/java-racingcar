970514ce1e60dc44003bd424e2186e181b8e1722
giyeon95 <giyeon15@gmail.com>
2021-11-14 14:21:55 +0900

    feature 이름 입력 UI 변경에 따른, InputView 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,6 +2,8 @@ package racingcar.view;
 
 import static racingcar.utils.PrintUtils.print;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import racingcar.dto.GameConfig;
 
@@ -10,21 +12,30 @@ public class InputView {
 
     private final Scanner sc = new Scanner(System.in);
 
-    private static final String NUMBER_OF_CAR_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String JOIN_CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String NUMBER_OF_GAME_ROUND_INPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
 
 
     public GameConfig initConfig() {
 
-        int numberOfCar = inputFromClient(NUMBER_OF_CAR_INPUT_MESSAGE);
-        int numberOfGameRound = inputFromClient(NUMBER_OF_GAME_ROUND_INPUT_MESSAGE);
+        print(JOIN_CAR_NAMES_INPUT_MESSAGE);
+        List<String> joinCarNames = inputCarNames(sc.next());
 
-        return new GameConfig(numberOfCar, numberOfGameRound);
+        print(NUMBER_OF_GAME_ROUND_INPUT_MESSAGE);
+        int numberOfGameRound = sc.nextInt();
+
+        return new GameConfig(joinCarNames, numberOfGameRound);
     }
 
-    private int inputFromClient(String message) {
-        print(message);
-        return sc.nextInt();
-    }
+    public List<String> inputCarNames(String input) {
+        String[] joinCarNames = input.split(",");
 
+        for (String carName : joinCarNames) {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+            }
+        }
+
+        return Arrays.asList(joinCarNames);
+    }
 }
