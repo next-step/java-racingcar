1ffdf2d2f490e480a22de77dd3ef699686440e0b
giyeon95 <giyeon15@gmail.com>
2021-11-14 14:21:55 +0900

    feature 이름 입력 UI 변경에 따른, InputView 수정

--- src/main/java/racingcar/dto/GameConfig.java
+++ src/main/java/racingcar/dto/GameConfig.java
@@ -2,19 +2,21 @@ package racingcar.dto;
 
 import static racingcar.utils.IntegerUtils.*;
 
+import java.util.List;
+
 
 public class GameConfig {
 
-    private final int numberOfCars;
+    private final List<String> joinCarNames;
     private final int maxOfRound;
 
-    public GameConfig(int maxOfCars, int maxOfRound) {
-        this.numberOfCars = isPositiveNumber(maxOfCars);
+    public GameConfig(List<String> joinCarNames, int maxOfRound) {
+        this.joinCarNames = joinCarNames;
         this.maxOfRound = isPositiveNumber(maxOfRound);
     }
 
     public int getNumberOfCars() {
-        return numberOfCars;
+        return joinCarNames.size();
     }
 
     public int getMaxOfRound() {
