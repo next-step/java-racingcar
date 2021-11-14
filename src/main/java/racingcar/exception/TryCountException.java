55ce98ba1e66f552566bc6e5ee0934058b0cba10
alsqhd <kiryuinh32@gmail.com>
2021-11-14 23:00:13 +0900

    remove: 사용하지 않는 TryCountException 클래스 삭제

--- src/main/java/racingcar/exception/TryCountException.java
+++
@@ -1,9 +0,0 @@
-package racingcar.exception;
-
-public class TryCountException extends RuntimeException {
-    private static final String MESSAGE = "시도 횟수(%d)를 잘못 입력했습니다.";
-
-    public TryCountException(int tryCount) {
-        super(String.format(MESSAGE, tryCount));
-    }
-}
