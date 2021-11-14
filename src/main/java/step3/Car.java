931dd5b6ee3344753ed779e956cabc6bef27b634
Lee Dongkyu <nelldklee@gmail.com>
2021-11-14 23:06:40 +0900

    feat(Car): 자동차 이름 부여, 제한사항 추가, 테스트 구현

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -4,14 +4,33 @@ import java.util.Objects;
 
 public class Car {
 
-    private final static int MOVABLE_COUNT = 4;
-    private int location;
+    public static final int MOVABLE_COUNT = 4;
+    public static final int VALID_CAR_SIZE_VALUE = 5;
+    public static final int DEFAULT_CAR_LOCATION_VALUE = 0;
+    public static final String DEFAULT_CAR_NAME = "이름없음";
+    public static final String VALID_MSG_CAR_NAME_SIZE = "자동차 이름은 5자를 초과할 수 없습니다.";
 
-    public Car(int location) {
-        this.location = location;
-    }
+    private int location;
+    private String name;
 
     public Car() {
+        this(DEFAULT_CAR_NAME, DEFAULT_CAR_LOCATION_VALUE);
+    }
+
+    public Car(String name) {
+        this(name, DEFAULT_CAR_LOCATION_VALUE);
+    }
+
+    public Car(int location) {
+        this(DEFAULT_CAR_NAME, location);
+    }
+
+    public Car(String name, int location) {
+        if (name.length() > VALID_CAR_SIZE_VALUE) {
+            throw new IllegalArgumentException(VALID_MSG_CAR_NAME_SIZE);
+        }
+        this.name = name;
+        this.location = location;
     }
 
     public int getLocation() {
@@ -30,12 +49,11 @@ public class Car {
         if (this == o) return true;
         if (!(o instanceof Car)) return false;
         Car car = (Car) o;
-        return location == car.location;
+        return location == car.location && Objects.equals(name, car.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(location);
+        return Objects.hash(location, name);
     }
-
 }
--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -3,19 +3,35 @@ package step3;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static step3.Car.VALID_MSG_CAR_NAME_SIZE;
 
 public class CarTest {
 
     @Test
-    void 자동차_4이상_앞으로_이동() {
-        assertThat(new Car().move(5)).isEqualTo(new Car(1));
-        assertThat(new Car().move(4)).isEqualTo(new Car(1));
+    void 자동차_이동_4이상() {
+        assertThat(new Car(0).move(5)).isEqualTo(new Car(1));
+        assertThat(new Car(0).move(4)).isEqualTo(new Car(1));
         assertThat(new Car(6).move(4)).isEqualTo(new Car(7));
     }
     @Test
-    void 자동차_4미만_제자리() {
+    void 자동차_이동_4미만() {
         assertThat(new Car(3).move(3)).isEqualTo(new Car(3));
-        assertThat(new Car().move(2)).isEqualTo(new Car());
+        assertThat(new Car(0).move(2)).isEqualTo(new Car());
         assertThat(new Car(5).move(1)).isEqualTo(new Car(5));
+    }
+
+    @Test
+    void 자동차_이름_생성() {
+        assertThat(new Car("abcd")).isEqualTo(new Car("abcd"));
+        assertThat(new Car("nell")).isEqualTo(new Car("nell"));
+        assertThat(new Car("dasom")).isEqualTo(new Car("dasom"));
+    }
+
+    @Test
+    void 자동차_이름_5자_초과() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Car("nelldklee");
+        }).withMessageMatching(VALID_MSG_CAR_NAME_SIZE);
     }
 }
