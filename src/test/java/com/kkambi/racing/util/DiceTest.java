1f20b8c6a5ee0c7e2e7a6ca45cf305e932100c01
kkambee <bok03220@gmail.com>
2021-11-14 18:36:49 +0900

    refactor(step3): Bound 클래스 제거에 따라 Dice 유틸리티 변경
    - Enum 대신 int 파라미터를 받도록 변경

--- src/test/java/com/kkambi/racing/util/DiceTest.java
+++ src/test/java/com/kkambi/racing/util/DiceTest.java
@@ -1,19 +1,18 @@
 package com.kkambi.racing.util;
 
-import com.kkambi.racing.domain.Bound;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DiceTest {
 
     @DisplayName("주사위의 값이 Bound 내에 있는지?")
-    @EnumSource(value = Bound.class)
+    @ValueSource(ints = {1, 4, 10})
     @ParameterizedTest
-    void withinBound(Bound bound) {
+    void withinBound(int bound) {
         // when, then
-        assertThat(Dice.roll(bound)).isLessThan(bound.getValue());
+        assertThat(Dice.roll(bound)).isLessThan(bound);
     }
 }
