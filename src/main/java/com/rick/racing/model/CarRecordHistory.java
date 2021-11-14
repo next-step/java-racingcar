4eb405e75f72b76dc28ab5ba900cbd898209ae97
gyuyeol <gyuyeol.lee@live.com>
2021-11-14 18:01:25 +0900

    refactor(logic): 삼항 연산자 제거

--- src/main/java/com/rick/racing/model/CarRecordHistory.java
+++ src/main/java/com/rick/racing/model/CarRecordHistory.java
@@ -1,18 +1,31 @@
 package com.rick.racing.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class CarRecordHistory extends ArrayList<Integer> {
+public class CarRecordHistory {
 
     public static final int GO_VALUE = 1;
     public static final int STOP_VALUE = 0;
 
+    final private List<Integer> positions = new ArrayList<>();
+
     public void addHistory(boolean isGo) {
-        final int lastRecord = this.size() == 0 ? 0 : this.get(this.size() - 1);
-        this.add(lastRecord + (isGo ? GO_VALUE : STOP_VALUE));
+        int lastTryRecord = 0;
+
+        if(!positions.isEmpty()) {
+            lastTryRecord = positions.get(positions.size() - 1);
+        }
+
+        if(isGo) {
+            positions.add(lastTryRecord + GO_VALUE);
+            return;
+        }
+
+        positions.add(lastTryRecord + STOP_VALUE);
     }
 
     public int getPosition(int index) {
-        return this.get(index);
+        return positions.get(index);
     }
 }
