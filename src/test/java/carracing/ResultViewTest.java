d796493f9edf67c3a97dcfe96baa695168a7d966
jiseok-choi <choi300300@gmail.com>
2021-11-14 13:31:41 +0900

    test: 우승 자동차 출력 테스트 코드 추가 및 기존 코드 변경

--- src/test/java/carracing/ResultViewTest.java
+++ src/test/java/carracing/ResultViewTest.java
@@ -10,13 +10,13 @@ public class ResultViewTest {
 
     @Test
     @DisplayName("정상 출력 확인")
-    void printRacing() {
+    void printRacing() throws CarNameFormatException {
         ResultView resultView = new ResultView();
 
         List<Car> carList = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
-            carList.add(new Car());
+            carList.add(new Car(String.valueOf(i)));
         }
 
         resultView.printRacing(carList);
@@ -27,5 +27,15 @@ public class ResultViewTest {
     void printCarName() throws CarNameFormatException {
         ResultView resultView = new ResultView();
         resultView.print(new Car("name"));
+    }
+
+    @Test
+    @DisplayName("우승 자동차 출력 확인")
+    void printWinner() {
+        ResultView resultView = new ResultView();
+        List<String> winnerCars = new ArrayList<>();
+        winnerCars.add("car1");
+        winnerCars.add("car2");
+        resultView.printWinner(winnerCars);
     }
 }
