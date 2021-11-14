2031fac3c0d6cec2ce21427d27fff6bc3b156f6f
JaeHan-Kim <hgal8877@gmail.com>
2021-11-14 13:03:49 +0900

    fix : 자동차의 이름과 위치를 테스트 할 수 있도록 수정

--- src/test/java/racinggame/domain/CarTest.java
+++ src/test/java/racinggame/domain/CarTest.java
@@ -3,19 +3,17 @@ package racinggame.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racinggame.domain.fixture.CarFixture;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
-    private static final String ENTRY_CAR_NAME = "jae";
-
     private Car car;
 
     @BeforeEach
     void setUp() throws Exception {
-        Name name = new Name(ENTRY_CAR_NAME);
-        car = new Car(name);
+        car = CarFixture.createCar();
     }
 
     @DisplayName("자동차가 전진 하는지 테스트")
@@ -36,6 +34,22 @@ class CarTest {
         Location expected = new Location();
 
         car.move(stateGenerator);
+
+        assertThat(car.getLocation()).isEqualTo(expected);
+    }
+
+    @DisplayName("자동차의 이름이 맞는지 동등성 테스트")
+    @Test
+    void checkCarName() throws Exception {
+        Name expected = new Name(CarFixture.ENTRY_CAR_NAME);
+
+        assertThat(car.getName()).isEqualTo(expected);
+    }
+
+    @DisplayName("자동차의 위치가 같은지 동등성 테스트")
+    @Test
+    void checkCarLocation() {
+        Location expected = new Location(Location.START_LOCATION);
 
         assertThat(car.getLocation()).isEqualTo(expected);
     }
