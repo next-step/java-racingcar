58efc9fbc513beaa706594e17dad58375c6bc629
alsqhd <kiryuinh32@gmail.com>
2021-11-14 22:09:56 +0900

    remove: TryCount의 사용하지 않는 메서드 제거

--- src/main/java/racingcar/domain/TryCount.java
+++ src/main/java/racingcar/domain/TryCount.java
@@ -44,10 +44,6 @@ public class TryCount {
         }
     }
 
-    public int currentRound() {
-        return finishRound - tryCount;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
