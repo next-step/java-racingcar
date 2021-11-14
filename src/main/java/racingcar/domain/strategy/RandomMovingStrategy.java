d2a90c9c5aaae2a243a1e30a84315db42c18e763
ssibongee <ssibongee@gmail.com>
2021-11-14 14:27:43 +0900

    feat: 이동 전략(MovingStrategy) 및 랜덤 이동 전략(RandomMovingStrategy) 클래스 구현

---
+++ src/main/java/racingcar/domain/strategy/RandomMovingStrategy.java
@@ -0,0 +1,22 @@
+package racingcar.domain.strategy;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomMovingStrategy implements MovingStrategy {
+
+    public static final int MOVE_CONSTRAINTS = 4;
+    private static final int MAX_RANDOM_RANGE = 10;
+    private static final MovingStrategy RANDOM_MOVING_STRATEGY = new RandomMovingStrategy();
+
+    private RandomMovingStrategy() {
+    }
+
+    public static MovingStrategy getInstance() {
+        return RANDOM_MOVING_STRATEGY;
+    }
+
+    @Override
+    public boolean movable() {
+        return ThreadLocalRandom.current().nextInt(MAX_RANDOM_RANGE) >= MOVE_CONSTRAINTS;
+    }
+}
