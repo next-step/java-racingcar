bf76a90c7ed6415dd9f8ce2778be97da7e5af51e
JaeHan-Kim <hgal8877@gmail.com>
2021-11-14 12:43:11 +0900

    fix : equal, hashcode 추가

--- src/main/java/racinggame/domain/Name.java
+++ src/main/java/racinggame/domain/Name.java
@@ -1,5 +1,7 @@
 package racinggame.domain;
 
+import java.util.Objects;
+
 public class Name {
     private static final int CAR_NAME_MAX_LENGTH = 5;
     private static final String ERROR_CAR_NAME_MAX_LENGTH = "[ERROR] 자동차의 이름은 5글자 이하여야 합니다.";
@@ -19,5 +21,18 @@ public class Name {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
