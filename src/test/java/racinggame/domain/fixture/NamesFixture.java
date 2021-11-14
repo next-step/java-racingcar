18c0ed3b9f59e21bf67a95c4c52b879ecee30d7c
JaeHan-Kim <hgal8877@gmail.com>
2021-11-14 12:59:06 +0900

    feat : test에 자주 사용되는 Fixture 추가

---
+++ src/test/java/racinggame/domain/fixture/NamesFixture.java
@@ -0,0 +1,15 @@
+package racinggame.domain.fixture;
+
+import racinggame.domain.Names;
+
+public class NamesFixture {
+    private static final String ENTRY_CAR_NAMES = "jae,han";
+
+    public static Names createNames() throws Exception {
+        return createNames(ENTRY_CAR_NAMES);
+    }
+
+    public static Names createNames(String text) throws Exception {
+        return Names.from(text);
+    }
+}
