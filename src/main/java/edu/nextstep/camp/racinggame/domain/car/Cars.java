c9da6aca2d3719585f1d66eeb18ab3b1c5150132
sid <sid.seo@kakaocorp.com>
2021-11-15 19:45:41 +0900

    refactor: domain, view 패키지 분리
    기존 model 패키지를 domain으로 변경
    - 스탭별 패키지 분리
    - view 클래스 인스턴스화 방지

---
+++ src/main/java/edu/nextstep/camp/racinggame/domain/car/Cars.java
@@ -0,0 +1,71 @@
+package edu.nextstep.camp.racinggame.domain.car;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import edu.nextstep.camp.racinggame.domain.car.policy.MovePolicy;
+
+public class Cars implements Iterable<Car> {
+    private static final int MINIMUM_NUMBER_OF_CAR = 1;
+
+    private final Collection<Car> cars;
+
+    private Cars(Collection<Car> cars) {
+        this.cars = Collections.unmodifiableCollection(cars);
+    }
+
+    public static Cars of(Collection<Name> names, MovePolicy movePolicy) {
+        if (names == null) {
+            throw new IllegalArgumentException("Name of cars cannot be null.");
+        }
+
+        if (names.size() < MINIMUM_NUMBER_OF_CAR) {
+            throw new IllegalArgumentException("invalid number of cars must be larger than 1 but 0");
+        }
+
+        return new Cars(names.stream()
+                .map(name -> Car.of(name, movePolicy))
+                .collect(Collectors.toList()));
+    }
+
+    public static Cars of(Car... cars) {
+        if (cars == null) {
+            throw new IllegalArgumentException("Name of cars cannot be null.");
+        }
+
+        if (cars.length < MINIMUM_NUMBER_OF_CAR) {
+            throw new IllegalArgumentException("invalid number of cars: " + cars.length);
+        }
+
+        return new Cars(Arrays.asList(cars));
+    }
+
+    public static Cars of(Collection<Car> cars) {
+        if (cars == null || cars.isEmpty()) {
+            throw new IllegalArgumentException("invalid list of cars: " + cars);
+        }
+
+        return new Cars(cars);
+    }
+
+    public Collection<Car> collect() {
+        return cars;
+    }
+
+    public Stream<Car> stream() {
+        return cars.stream();
+    }
+
+    public int size() {
+        return cars.size();
+    }
+
+    @Override
+    public Iterator<Car> iterator() {
+        return cars.iterator();
+    }
+}
