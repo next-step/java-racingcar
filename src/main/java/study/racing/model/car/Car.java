96b2016de0d9f5a3b8462cc80f8bf9dfa9db28ef
dante(이산하) <dante.93@kakaocorp.com>
2021-11-15 22:41:27 +0900

    refactor (change block)

--- src/main/java/study/racing/model/car/Car.java
+++ src/main/java/study/racing/model/car/Car.java
@@ -33,8 +33,12 @@ public class Car {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {return true;}
-        if (o == null || getClass() != o.getClass()) {return false;}
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Car car = (Car) o;
         return distance == car.distance && Objects.equals(name, car.name);
     }
