1e1e0b3e23628c714ba15768c5f6dd744aea687f
sid <sid.seo@kakaocorp.com>
2021-11-15 10:04:41 +0900

    refactor: 변하지 않는 값을 불변 객체로
    Turn을 Turn과 Turns로 분리
     - value class로서의 Turn
     - Turns는 턴이 돌아가는 로직을 책임
     - Turns가 Turn을 들고있는 Iterator를 구현

--- src/test/java/edu/nextstep/camp/racinggame/domain/race/TurnTest.java
+++ src/test/java/edu/nextstep/camp/racinggame/domain/race/TurnTest.java
@@ -1,69 +1,23 @@
 package edu.nextstep.camp.racinggame.domain.race;
 
-import java.util.stream.Stream;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class TurnTest {
-    private static final int INITIAL_TURN = 0;
-
-    static Stream<Arguments> parseValidTurnArguments() {
-        return Stream.of(
-                Arguments.of(1),
-                Arguments.of(10),
-                Arguments.of(40),
-                Arguments.of(100)
-        );
+    @Test
+    @DisplayName("create turn")
+    public void create() {
+        final int testTurn = 0;
+        assertThat(Turn.of(testTurn).toInt()).isEqualTo(testTurn);
+        assertThat(Turn.of(testTurn)).isEqualTo(Turn.of(testTurn));
     }
 
-    @ParameterizedTest(name = "create turn: {arguments}")
-    @MethodSource("parseValidTurnArguments")
-    public void create(int turn) {
-        assertThat(Turn.of(turn).total()).isEqualTo(turn);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, -1, -1000})
-    public void createFailed(int turns) {
-        assertThatIllegalArgumentException().isThrownBy(() -> Turn.of(turns));
-    }
-
-    @ParameterizedTest(name = "get current turn: {arguments}")
-    @MethodSource("parseValidTurnArguments")
-    public void current(int turn) {
-        Turn testTurn = Turn.of(turn);
-        assertThat(testTurn.current()).isEqualTo(INITIAL_TURN);
-    }
-
-    @ParameterizedTest(name = "go to next turn: {arguments}")
-    @MethodSource("parseValidTurnArguments")
-    public void next(int turn) {
-        Turn testTurn = Turn.of(turn);
-        for (int i = 0; i < turn; i++) {
-            assertThat(testTurn.current()).isEqualTo(i);
-            testTurn.next();
-            assertThat(testTurn.current()).isEqualTo(i + 1);
-        }
-    }
-
-
-    @ParameterizedTest(name = "check end: {arguments}")
-    @MethodSource("parseValidTurnArguments")
-    public void isEnded(int turn) {
-        Turn testTurn = Turn.of(turn);
-        for (int i = 0; testTurn.hasNext(); i++) {
-            assertThat(testTurn.hasNext()).isTrue();
-            assertThat(testTurn.current()).isEqualTo(i);
-            testTurn.next();
-            assertThat(testTurn.current()).isEqualTo(i + 1);
-        }
-
-        assertThat(testTurn.hasNext()).isFalse();
+    @Test
+    @DisplayName("creation failed")
+    public void createFailed() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Turn.of(-1));
     }
 }
