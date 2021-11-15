c35f11fb735d7e2c9f83ecb3e0bf14d118049a16
hyenny <qlwb9191@naver.com>
2021-11-15 11:41:38 +0900

    test: 테스트 목록 정리

---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+
+    @Test
+    void 입력_자동차대수_시도할회수() {
+
+    }
+
+    @Test
+    void 전진또는멈춤() {
+
+    }
+
+    @Test
+    void 출력_자동차상태() {
+        
+    }
+
+}
