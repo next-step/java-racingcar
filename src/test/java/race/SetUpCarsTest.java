1b641369c740f1c143438f4d4593063e2a9b2810
nano.son <nano.son@kakaocorp.com>
2021-11-16 00:38:12 +0900

    refactor: FilterWinnersTest에도 NullAndEmptySource 적용 & 불필요한 import제거 & new line

--- src/test/java/race/SetUpCarsTest.java
+++ src/test/java/race/SetUpCarsTest.java
@@ -1,11 +1,9 @@
 package race;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 import race.domain.Car;
 import race.domain.CarName;
 
