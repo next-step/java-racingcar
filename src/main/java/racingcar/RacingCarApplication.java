2d6ac67c9bdf6975aa513c410c43a82f509f2e6f
ssibongee <ssibongee@gmail.com>
2021-11-16 01:39:22 +0900

    refactor: 불필요한 상수 선언 제거 및 실제 게임 진행 로직을 RacingGameController 클래스로 분리

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -1,31 +1,25 @@
 package racingcar;
 
-import racingcar.domain.*;
-import racingcar.domain.strategy.RandomMovingStrategy;
+import racingcar.domain.RacingGameController;
+import racingcar.domain.Records;
 import racingcar.ui.RacingCarInputView;
 import racingcar.ui.RacingCarOutputView;
 
 public class RacingCarApplication {
 
-    private static final RacingGameController RACING_GAME_CONTROLLER = RacingGameController.getInstance();
-    private static final RacingCarInputView INPUT_VIEW = RacingCarInputView.getInstance();
-    private static final RacingCarOutputView OUTPUT_VIEW = RacingCarOutputView.getInstance();
-
     public static void main(String[] args) {
-        INPUT_VIEW.carInputPrompt();
-        int counts = INPUT_VIEW.input();
+        RacingGameController controller = RacingGameController.getInstance();
+        RacingCarInputView inputView = RacingCarInputView.getInstance();
+        RacingCarOutputView outputView = RacingCarOutputView.getInstance();
 
-        RacingCars racingCars = RacingCars.from(counts, RandomMovingStrategy.getInstance());
-        Records records = Records.EMPTY_RECORDS;
+        inputView.carInputPrompt();
+        int counts = inputView.input();
 
-        INPUT_VIEW.lapInputPrompt();
-        int laps = INPUT_VIEW.input();
+        inputView.lapInputPrompt();
+        int laps = inputView.input();
 
-        for (int i = 1; i <= laps; i++) {
-            racingCars = RACING_GAME_CONTROLLER.race(racingCars);
-            records = records.record(Lap.from(i), Record.from(racingCars));
-        }
+        Records result = controller.execute(counts, laps);
 
-        OUTPUT_VIEW.outputPrompt(records);
+        outputView.outputPrompt(result);
     }
 }
