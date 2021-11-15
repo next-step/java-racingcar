499f3962704bb55a1813f433d1ed5b4edf846662
hyenny <qlwb9191@naver.com>
2021-11-15 12:03:23 +0900

    feat:Input 기능 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,11 +2,32 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class RacingCarTest {
 
-    @Test
-    void 입력_자동차대수_시도할회수() {
+    private static InputStream generateUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
 
+    @Test
+    void 입력_자동차대수() {
+        InputStream in = generateUserInput("3");
+        System.setIn(in);
+        InputView inputView = new InputView(new Scanner(System.in));
+        assertThat(inputView.input("자동차 대수는 몇 대 인가요?")).isEqualTo(3);
+    }
+
+    @Test
+    void 입력_시도횟수() {
+        InputStream in = generateUserInput("5");
+        System.setIn(in);
+        InputView inputView = new InputView(new Scanner(System.in));
+        assertThat(inputView.input("시도할 횟수는 몇 회 인가요?")).isEqualTo(5);
     }
 
     @Test
@@ -16,7 +37,7 @@ public class RacingCarTest {
 
     @Test
     void 출력_자동차상태() {
-        
+
     }
 
 }
