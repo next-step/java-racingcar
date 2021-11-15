d897c1f47c43e7635dd4620069168f29128da84a
kkambee <bok03220@gmail.com>
2021-11-15 22:50:49 +0900

    refactor(step4): Car 클래스의 원시값 및 문자열 포장
    - 위치와 이름을 별도의 클래스로 포장
    - validation 및 메소드에 대한 단위 테스트

--- src/test/java/com/kkambi/racing/domain/CarTest.java
+++ src/test/java/com/kkambi/racing/domain/CarTest.java
@@ -1,22 +1,25 @@
 package com.kkambi.racing.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CarTest {
 
     @DisplayName("자동차 전진 시도")
     @MethodSource("getRandomValueWhenTryToMove")
     @ParameterizedTest
-    void tryToMove(int randomValue, int expectedLocation) {
+    void tryToCarMove(int randomValue, Car.Location expectedLocation) {
         // given
-        Car car = new Car();
+        Car car = new Car(0, "pobi");
 
         // when
         car.tryToMove(randomValue);
@@ -27,9 +30,40 @@ class CarTest {
 
     static Stream<Arguments> getRandomValueWhenTryToMove() {
         return Stream.of(
-                Arguments.of(1, 0),
-                Arguments.of(4, 1),
-                Arguments.of(9, 1)
+                Arguments.of(1, new Car.Location(0)),
+                Arguments.of(4, new Car.Location(1)),
+                Arguments.of(9, new Car.Location(1))
         );
+    }
+
+    @DisplayName("자동차 위치는 음수일수 없다")
+    @ValueSource(ints = {-5, -3, -1})
+    @ParameterizedTest
+    void locationMustNotBeMinus(int locationValue) {
+        // when
+        // then
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car.Location(locationValue));
+    }
+
+    @DisplayName("자동차 위치 증가")
+    @Test
+    void locationMove() {
+        // given
+        Car.Location location = new Car.Location(0);
+
+        // when
+        Car.Location locationAfterMove = location.move();
+
+        // then
+        assertThat(locationAfterMove.getValue()).isEqualTo(location.getValue() + 1);
+    }
+
+    @DisplayName("자동차 이름은 5글자를 초과할 수 없다")
+    @ValueSource(strings = {"123456", "coding"})
+    @ParameterizedTest
+    void nameMustNotOver5(String name) {
+        // when
+        // then
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car.Name(name));
     }
 }
