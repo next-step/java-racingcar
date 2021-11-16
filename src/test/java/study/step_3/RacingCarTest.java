4e72940af5c9cdef2227804156a41326e7b15c2e
sojeongw <sojeongw.official@gmail.com>
2021-11-16 22:25:03 +0900

    refactor: RacingCar 테스트를 리팩토링 한다

--- src/test/java/study/step_3/RacingCarTest.java
+++ src/test/java/study/step_3/RacingCarTest.java
@@ -5,43 +5,36 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarTest {
 
-    @Test
-    @DisplayName("0에서 9 사이의 랜덤 값을 생성한다")
-    void shouldGenerateRandomNumberBetweenZeroAndNine() {
-        RandomNumber randomNumber = new RandomNumber();
-
-        assertThat(randomNumber.generate()).isBetween(0, 9);
-    }
-
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    @DisplayName("랜덤 값이 4 이상인 경우 횟수를 센다")
+    @DisplayName("랜덤 값이 4 이상인 경우 이동 거리를 추가한다")
     void shouldCountIfGreaterThanFour(int randomNumber) {
-        Car car = new Car();
+        RacingCar racingCar = new RacingCar();
 
-        if (car.canMove(randomNumber)) {
-            car.move();
+        if (racingCar.canMove(randomNumber)) {
+            racingCar.move();
         }
 
-        assertThat(car.getNumberOfMoves()).isEqualTo(1);
+        assertThat(racingCar.getDistance()).isEqualTo(1);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
-    @DisplayName("랜덤 값이 4 미만인 경우 횟수를 세지 않는다")
+    @DisplayName("랜덤 값이 4 미만인 경우 이동 거리를 추가하지 않는다")
     void shouldNotCountIfLessThanFour(int randomNumber) {
-        Car car = new Car();
+        RacingCar racingCar = new RacingCar();
 
-        if (car.canMove(randomNumber)) {
-            car.move();
+        if (racingCar.canMove(randomNumber)) {
+            racingCar.move();
         }
 
-        assertThat(car.getNumberOfMoves()).isEqualTo(0);
+        assertThat(racingCar.getDistance()).isEqualTo(0);
     }
 }
