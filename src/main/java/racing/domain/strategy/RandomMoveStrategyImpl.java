26b97493f3ad4dc46176894244d5d7d32a973e73
minjoon_lee <minjoon1995@naver.com>
2021-11-16 15:35:52 +0900

    [refactoring] Random은 static으로 single톤으로 관리

--- src/main/java/racing/domain/strategy/RandomMoveStrategyImpl.java
+++ src/main/java/racing/domain/strategy/RandomMoveStrategyImpl.java
@@ -3,9 +3,13 @@ package racing.domain.strategy;
 import java.util.Random;
 
 public class RandomMoveStrategyImpl implements MoveStrategy {
+
+    private final static Random random = new Random();
+
     @Override
     public boolean movable() {
         Integer criteriaNumber = 4;
-        return new Random().nextInt(10) > criteriaNumber;
+        Integer boundNumber = 10;
+        return random.nextInt(boundNumber) > criteriaNumber;
     }
 }
