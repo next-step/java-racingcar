e986fb67407984afdca133984415cd36f8cf87eb
scott.lee <scott.lee@kakaocorp.com>
2021-11-17 22:30:14 +0900

    테스트 통과하는 코드 작성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,42 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
     public static int compute(String s) {
-        return 0;
+        if (s == null || s.isEmpty()) {
+            throw new IllegalArgumentException("Input must not null or blank");
+        }
+
+        List<String> list = new ArrayList<>(Arrays.asList(s.split(" ")));
+
+        while (list.size() != 1) {
+            int left = Integer.parseInt(list.remove(0));
+            String operator = list.remove(0);
+            int right = Integer.parseInt(list.remove(0));
+
+            int result;
+            switch (operator) {
+                case "+":
+                    result = left + right;
+                    break;
+                case "-":
+                    result = left - right;
+                    break;
+                case "*":
+                    result = left * right;
+                    break;
+                case "/":
+                    result = left / right;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid operator");
+            }
+            list.add(0, Integer.toString(result));
+        }
+
+        return Integer.parseInt(list.get(0));
     }
 }
