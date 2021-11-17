61441c0b064a245ef66527ce85419605a8bf4771
scott.lee <scott.lee@kakaocorp.com>
2021-11-17 22:41:28 +0900

    refactoring: indent 줄이기, naming 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,38 +5,37 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Calculator {
-    public static int compute(String s) {
-        if (s == null || s.isEmpty()) {
+    public static int compute(String input) {
+        if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException("Input must not null or blank");
         }
 
-        List<String> list = new ArrayList<>(Arrays.asList(s.split(" ")));
+        List<String> elements = new ArrayList<>(Arrays.asList(input.split(" ")));
 
-        while (list.size() != 1) {
-            int left = Integer.parseInt(list.remove(0));
-            String operator = list.remove(0);
-            int right = Integer.parseInt(list.remove(0));
+        while (elements.size() != 1) {
+            int left = Integer.parseInt(elements.remove(0));
+            String operator = elements.remove(0);
+            int right = Integer.parseInt(elements.remove(0));
 
-            int result;
-            switch (operator) {
-                case "+":
-                    result = left + right;
-                    break;
-                case "-":
-                    result = left - right;
-                    break;
-                case "*":
-                    result = left * right;
-                    break;
-                case "/":
-                    result = left / right;
-                    break;
-                default:
-                    throw new IllegalArgumentException("Invalid operator");
-            }
-            list.add(0, Integer.toString(result));
+            int result = operate(left, operator, right);
+            elements.add(0, Integer.toString(result));
         }
 
-        return Integer.parseInt(list.get(0));
+        return Integer.parseInt(elements.get(0));
+    }
+
+    private static int operate(int left, String operator, int right) {
+        switch (operator) {
+            case "+":
+                return left + right;
+            case "-":
+                return left - right;
+            case "*":
+                return left * right;
+            case "/":
+                return left / right;
+            default:
+                throw new IllegalArgumentException("Invalid operator");
+        }
     }
 }
