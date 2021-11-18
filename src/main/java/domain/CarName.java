c80d4e51a4c8f51f333a68db409ecc6e800ce686
user <kth4878@naver.com>
2021-11-19 01:56:00 +0900

    [Refactor] equals, hashCode 추가후 테스트 변경

--- src/main/java/domain/CarName.java
+++ src/main/java/domain/CarName.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class CarName {
     private static final String NAME_LENGTH_ERROR_MESSAGE = "error : 이름 최대 길이를 초과했습니다.";
     private static final int NAME_LENGTH_MAX = 5;
@@ -18,5 +20,18 @@ public class CarName {
 
     public String value() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
