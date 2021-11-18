12ab309a0e03c25e136547527b028647972e28b5
ljlwpwp <leejaelin@gmail.com>
2021-11-18 23:18:01 +0900

    불필요 부분 삭제

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,18 +1,13 @@
 package racingcar.car;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Car {
     private static final int DEFAULT_POSITION = 1;
     private static final int MOVABLE_CONDITION = 4;
 
     private int position;
-    private List<Integer> history;
 
     public Car() {
         this.position = DEFAULT_POSITION;
-        this.history = new ArrayList<>();
     }
 
     public int move(int random) {
@@ -24,9 +19,5 @@ public class Car {
 
     public int getPosition() {
         return this.position;
-    }
-
-    public int getPositionHistory(int lapIndex) {
-        return this.history.get(lapIndex);
     }
 }
