8443be6ab8c99dfc70806cfd12e0b8ea0f2bdab1
hyenny <qlwb9191@naver.com>
2021-11-18 16:44:10 +0900

    feat: 자동차 이름 객체 생성 및 예외 처리

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,11 +4,17 @@ public class Car {
     private int id;
     private int distance;
     private Movable movable;
+    private CarName name;
 
     public Car(int id, Movable movable) {
+        this(id, movable, new CarName(""));
+    }
+
+    public Car(int id, Movable movable, CarName name) {
         this.id = id;
         this.distance = 1;
         this.movable = movable;
+        this.name = name;
     }
 
     public int getDistance() {
---
+++ src/test/java/racingcar/CarNameTest.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class CarNameTest {
+
+    @DisplayName("자동차 이름이 5자를 초과하면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"helloo", "modernjava", "testtest"})
+    void 자동차이름_5자초과_예외(String input) {
+        assertThatThrownBy(() -> {
+            new CarName(input);
+        }).hasMessageContaining("자동차 이름은 5자를 초과할 수 없습니다.");
+    }
+
+    @DisplayName("자동차 이름을 입력하지 않으면 예외가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 자동차이름_없을때_예외(String input) {
+        assertThatThrownBy(() -> {
+            new CarName(input);
+        }).hasMessageContaining("자동차 이름을 입력해야 합니다.");
+    }
+
+}
