aaac66f502ad5b4d47ddd7ff480a27629d90433b
scott.lee <scott.lee@kakaocorp.com>
2021-11-19 04:30:43 +0900

    feat Track, Car 기능 구현

--- src/main/java/carracing/Car.java
+++ src/main/java/carracing/Car.java
@@ -2,10 +2,25 @@ package carracing;
 
 public class Car {
 
-    private int step;
+    private static final int MAX_RANDOM = 9;
+    private static final int MIN_RANDOM = 0;
+    private static final int MIN_FORWARD = 4;
+
+    private int step = 0;
 
     public void stepForwardByRandomNumber(int random) {
+        if (validateRandom(random)) {
+            throw new IllegalArgumentException(
+                    String.format("Random must great or equal %d and less or equale than %d", MIN_RANDOM, MAX_RANDOM));
+        }
 
+        if (random >= MIN_FORWARD) {
+            step += 1;
+        }
+    }
+
+    private boolean validateRandom(int random) {
+        return random < MIN_RANDOM || MAX_RANDOM < random;
     }
 
     public int getStep() {
--- src/main/java/carracing/Track.java
+++ src/main/java/carracing/Track.java
@@ -1,17 +1,29 @@
 package carracing;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Track {
-    public Track(int cars) {
+    private final List<Car> cars;
 
+    public Track(int cars) {
+        this.cars = new ArrayList<>(cars);
+        for (int i = 0; i < cars; i++) {
+            this.cars.add(new Car());
+        }
     }
 
     public List<Integer> getSteps() {
-        return null;
+        return cars.stream()
+                .map(Car::getStep)
+                .collect(Collectors.toList());
     }
 
     public void forward() {
-
+        cars.forEach(car -> {
+            int random = (int)(Math.random() * 10);
+            car.stepForwardByRandomNumber(random);
+        });
     }
 }
