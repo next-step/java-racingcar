d9c2b526036b9e10797acee515c0f5be34cf15ff
guswnt <guswntj1277@gmail.com>
2021-11-19 10:52:16 +0900

    feat: step4  자동차 이름은 5자를 초과할 수 없다.

--- src/main/java/step4/domain/Car.java
+++ src/main/java/step4/domain/Car.java
@@ -1,11 +1,14 @@
-package step4;
+package step4.domain;
 
+
+import step4.exception.RacingGameInputException;
 
 public class Car {
     private String name;
     private int currentPosition;
 
-    Car(String name) {
+    public Car(String name) {
+        validateCarName(name);
         this.name = name;
         this.currentPosition = 0;
     }
@@ -20,5 +23,11 @@ public class Car {
 
     public String getName(){
         return name;
+    }
+
+    private void validateCarName(String carName) {
+        if (carName.length() > 5) {
+            throw new RacingGameInputException("5자 이하의 자동차 이름을 입력하세요");
+        }
     }
 }
---
+++ src/main/java/step4/exception/RacingGameInputException.java
@@ -0,0 +1,9 @@
+package step4.exception;
+
+public class RacingGameInputException extends RuntimeException {
+
+    public RacingGameInputException(String message) {
+        super(message);
+    }
+
+}
