a288a86759434ea98b01a72e1ecebcea0c46c4d7
guswnt <guswntj1277@gmail.com>
2021-11-19 10:38:47 +0900

    feat: step4  0에서 9 사이 random 값을 구한 후 random값이 4이상일 경우 전진한다.

---
+++ src/main/java/step4/domain/cargameStrategy/RandomCarRacingGameStrategy.java
@@ -0,0 +1,26 @@
+package step4.domain.cargameStrategy;
+
+import java.util.Random;
+
+public class RandomCarRacingGameStrategy implements CarRacingGameStrategy {
+    private static final int MAX_RANDOM_VALUE = 10;
+    private static final int BOUND = 4;
+    private static final Random random = new Random();
+
+    @Override
+    public int playGetDistanceToMove() {
+        int random = random(MAX_RANDOM_VALUE);
+        return getDistanceToMove(random);
+    }
+
+    private int getDistanceToMove(int randomNumber) {
+        if (randomNumber >= BOUND) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private static int random(int bound) {
+        return random.nextInt(bound);
+    }
+}
---
+++ src/test/java/step4/domain/gameStrategy/RandomCarGameStrategyTest.java
@@ -0,0 +1,51 @@
+package step4.domain.gameStrategy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import step4.domain.cargameStrategy.RandomCarRacingGameStrategy;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomCarGameStrategyTest {
+
+    RandomCarRacingGameStrategy randomCarGameStrategy = new RandomCarRacingGameStrategy();
+
+    @DisplayName("4이상의 수를 입력하면 1을 리턴한다.")
+    @ParameterizedTest()
+    @ValueSource(ints = {4, 5})
+    void getDistanceToMove(int value) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = RandomCarRacingGameStrategy.class
+                .getDeclaredMethod("getDistanceToMove", int.class);
+        method.setAccessible(true);
+        int result = (int) method.invoke(randomCarGameStrategy, value);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @DisplayName("4미만의 수를 입력하면 0을 리턴한다.")
+    @ParameterizedTest()
+    @ValueSource(ints = {3, 2})
+    void getDistanceToMove2(int value) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = RandomCarRacingGameStrategy.class
+                .getDeclaredMethod("getDistanceToMove", int.class);
+        method.setAccessible(true);
+        int result = (int) method.invoke(randomCarGameStrategy, value);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @DisplayName("게임 결과 항상 1 또는 0의 값을 리턴한다.")
+    @Test
+    void playGetDistanceToMove() {
+        int distanceToMove = randomCarGameStrategy.playGetDistanceToMove();
+        assertThat(isZeroOrOne(distanceToMove)).isEqualTo(true);
+    }
+
+    boolean isZeroOrOne(int value) {
+        return value == 1 || value == 0;
+    }
+
+}
