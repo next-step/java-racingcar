56d119a74b4c21f38ee1f86db2564cd31b98bb8a
김민혁 <kminheuk@gmail.com>
2021-11-21 01:27:45 +0900

    fix: Fix RandomNumber constructor with Random

--- src/main/java/racingcar/RandomNumber.java
+++ src/main/java/racingcar/RandomNumber.java
@@ -1,8 +1,16 @@
 package racingcar;
 
+import java.util.Random;
+
 public class RandomNumber {
     private static final int STANDARD_TO_GO = 4;
+    private static final int BOUND_RANDOM_NUMBER = 10;
+    private static final Random RANDOM = new Random();
     private final int number;
+
+    public RandomNumber() {
+        this.number = RANDOM.nextInt(BOUND_RANDOM_NUMBER);
+    }
 
     public RandomNumber(int number) {
         this.number = number;
