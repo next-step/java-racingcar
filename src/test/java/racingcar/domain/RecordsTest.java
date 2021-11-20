dba409bf3d691ff5050e1a7241768d7b88592e12
ssibongee <ssibongee@gmail.com>
2021-11-20 13:37:14 +0900

    test: 우승자 이름 조회 테스트 작성

--- src/test/java/racingcar/domain/RecordsTest.java
+++ src/test/java/racingcar/domain/RecordsTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.fixture.RacingCarFixture;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -25,6 +28,23 @@ class RecordsTest {
                 () -> assertThat(beforeRecord.getValues()).size().isEqualTo(0),
                 () -> assertThat(afterRecord.getValues()).size().isEqualTo(1)
         );
+    }
 
+    @DisplayName("우승자 이름을 조회할 수 있다.")
+    @Test
+    void getWinner() {
+        // given
+        Record record = new Record(Arrays.asList(
+                new Score(new Name("a"), Position.from(10)),
+                new Score(new Name("b"), Position.from(20)),
+                new Score(new Name("c"), Position.from(30))
+        ));
+        List<Name> expected = Arrays.asList(new Name("c"));
+
+        // when
+        List<Name> winners = record.getWinner();
+
+        // then
+        assertThat(winners).isEqualTo(expected);
     }
 }
