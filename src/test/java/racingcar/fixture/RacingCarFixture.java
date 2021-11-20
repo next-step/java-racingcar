cd489acbd3ab2a5cf7e656a6b733f141cf07012e
ssibongee <ssibongee@gmail.com>
2021-11-20 13:26:08 +0900

    refactor: Name 속성 추가에 따른 경주용 자동차 클래스 및 테스트 코드 대응

--- src/test/java/racingcar/fixture/RacingCarFixture.java
+++ src/test/java/racingcar/fixture/RacingCarFixture.java
@@ -1,5 +1,7 @@
 package racingcar.fixture;
 
+import racingcar.domain.Name;
+import racingcar.domain.Position;
 import racingcar.domain.RacingCar;
 import racingcar.domain.RacingCars;
 import racingcar.domain.strategy.MockRandomMovingStrategy;
@@ -7,15 +9,23 @@ import racingcar.domain.strategy.MockRandomMovingStrategy;
 import java.util.Arrays;
 import java.util.Collections;
 
+import static racingcar.domain.strategy.MockRandomMovingStrategy.MOVABLE_RANDOM_MOVE_STRATEGY;
+
 public class RacingCarFixture {
 
+    public static final Name PARTICIPANT_NAME = new Name("ahn");
+
     public static final RacingCar MOVABLE_RACING_CAR =
-            new RacingCar(MockRandomMovingStrategy.MOVABLE_RANDOM_MOVE_STRATEGY);
+            new RacingCar(PARTICIPANT_NAME, MOVABLE_RANDOM_MOVE_STRATEGY);
 
     public static final RacingCar IMMOVABLE_RACING_CAR =
-            new RacingCar(MockRandomMovingStrategy.IMMOVABLE_RANDOM_MOVE_STRATEGY);
+            new RacingCar(PARTICIPANT_NAME, MockRandomMovingStrategy.IMMOVABLE_RANDOM_MOVE_STRATEGY);
 
 
     public static final RacingCars RACING_CARS =
             RacingCars.from(Collections.unmodifiableList(Arrays.asList(MOVABLE_RACING_CAR, IMMOVABLE_RACING_CAR)));
+
+    public static RacingCar 특정_위치의_경주용_자동차를_생성(String name, int value) {
+        return new RacingCar(new Name(name), Position.from(value), MOVABLE_RANDOM_MOVE_STRATEGY);
+    }
 }
