4ebc5111ddabe18a3be81bd1cfb723417c4f1d3f
orgojy <giveawesome@gmail.com>
2021-11-20 21:27:01 +0900

    feat(CarPosition.java): 자동차 위치를 비교하기 위한 equals, hashCode 구현

--- src/main/java/carracing/domain/CarPosition.java
+++ src/main/java/carracing/domain/CarPosition.java
@@ -1,5 +1,7 @@
 package carracing.domain;
 
+import java.util.Objects;
+
 /**
  * 자동차의 위치를 저장하는 클래스
  */
@@ -27,5 +29,18 @@ public class CarPosition {
 
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarPosition that = (CarPosition) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
