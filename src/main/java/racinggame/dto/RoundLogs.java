bbe50a2c11867bf7ca7a963c408e9e4e38f823ff
JaeHan-Kim <hgal8877@gmail.com>
2021-11-20 19:01:54 +0900

    fix : DTO의 생성자에서 자동차의 리스트로 로그르 만들도록 변경

--- src/main/java/racinggame/dto/RoundLogs.java
+++ src/main/java/racinggame/dto/RoundLogs.java
@@ -1,16 +1,20 @@
 package racinggame.dto;
 
+import racinggame.domain.Car;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class RoundLogs {
-    List<RoundLog> roundLogs;
+    List<RoundLog> roundLogs = new ArrayList<>();;
 
-    public RoundLogs() {
-        this.roundLogs = new ArrayList<>();
+    public RoundLogs(List<Car> cars) {
+        for (Car car : cars) {
+            this.roundLogs.add(RoundLog.from(car));
+        }
     }
 
-    public void addRoundLog(RoundLog roundLog){
+    private void addRoundLog(RoundLog roundLog){
         this.roundLogs.add(roundLog);
     }
 
