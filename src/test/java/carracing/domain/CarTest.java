41c7e9e6fcfe4e7d078837c332a2f2dbf732bedc
orgojy <giveawesome@gmail.com>
2021-11-21 23:47:57 +0900

    refactor(CarTest.java): FunctionalInterface를 이용하여 mock 사용 로직 대체

--- src/test/java/carracing/domain/CarTest.java
+++ src/test/java/carracing/domain/CarTest.java
@@ -1,38 +1,15 @@
 package carracing.domain;
 
-import carracing.util.RandomGenerator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 
 @DisplayName("자동차 테스트")
 class CarTest {
 
     private static final int INITIATION_POSITION_NUMBER = 0;
     private static final int FIRST_MOVING_POSITION_NUMBER = INITIATION_POSITION_NUMBER + 1;
-    private static final int BASE_POSITION_NUMBER = RandomIntMovementPolicy.BASE_POSITION_NUMBER;
-    private static final int MAX_POSITION_NUMBER = RandomIntMovementPolicy.MAX_POSITION_NUMBER;
-    private static final int RANDOM_BOUND_NUMBER = RandomIntMovementPolicy.RANDOM_BOUND_NUMBER;
-
-    private RandomGenerator randomGenerator;
-    private MovementPolicy movementPolicy;
-
-    @BeforeEach
-    void setUp() {
-        randomGenerator = mock(RandomGenerator.class);
-        movementPolicy = new RandomIntMovementPolicy(randomGenerator);
-    }
 
     @DisplayName("유효한 자동차 생성 확인")
     @Test
@@ -45,46 +22,24 @@ class CarTest {
     }
 
     @DisplayName("자동차가 움직일 수 있는 경우, 자동자의 위치가 변경되는지 확인")
-    @ParameterizedTest
-    @MethodSource("provideMovableCases")
-    void moveCarTest(int movableNumber) {
+    @Test
+    void moveCarTest() {
         // given
         Car car = new Car();
-        given(randomGenerator.generateZeroOrPositiveNumber(RANDOM_BOUND_NUMBER))
-                .willReturn(movableNumber);
         // when
-        car.move(movementPolicy);
+        car.move(() -> true);
         // then
         assertThat(car.getPosition()).isEqualTo(new CarPosition(FIRST_MOVING_POSITION_NUMBER));
     }
 
     @DisplayName("자동차가 움직일 수 없는 경우, 자동자의 위치가 변경되는지 확인")
-    @ParameterizedTest
-    @MethodSource("provideUnmovableCases")
-    void checkUnmovableCarTest(int unmovableNumber) {
+    @Test
+    void checkUnmovableCarTest() {
         // given
         Car car = new Car();
-        given(randomGenerator.generateZeroOrPositiveNumber(RANDOM_BOUND_NUMBER))
-                .willReturn(unmovableNumber);
         // when
-        car.move(movementPolicy);
+        car.move(() -> false);
         // then
         assertThat(car.getPosition()).isEqualTo(new CarPosition(INITIATION_POSITION_NUMBER));
-    }
-
-    private static Stream<Arguments> provideMovableCases() {
-        List<Arguments> arguments = new ArrayList<>();
-        for (int i = BASE_POSITION_NUMBER; i <= MAX_POSITION_NUMBER; i++) {
-            arguments.add(Arguments.of(i));
-        }
-        return arguments.stream();
-    }
-
-    private static Stream<Arguments> provideUnmovableCases() {
-        List<Arguments> arguments = new ArrayList<>();
-        for (int i = 0; i < BASE_POSITION_NUMBER; i++) {
-            arguments.add(Arguments.of(i));
-        }
-        return arguments.stream();
     }
 }
