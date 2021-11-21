1c922833ed33685f532a666d144d033fe54719b4
orgojy <giveawesome@gmail.com>
2021-11-22 00:37:34 +0900

    feat(Cars.java): 일급컬렉션 내부 리스트 필드에 불변성 적용

--- src/main/java/carracing/domain/Cars.java
+++ src/main/java/carracing/domain/Cars.java
@@ -1,19 +1,16 @@
 package carracing.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * 자동차 경주를 같이하는 자동차들을 표현한 클래스
  */
 public class Cars {
-    private List<Car> cars;
+    private final List<Car> cars;
 
     public Cars(int numberOfCars) {
-        initiateCars(numberOfCars);
-    }
-
-    private void initiateCars(int numberOfCars) {
         cars = new ArrayList<>(numberOfCars);
         for (int i = 0; i < numberOfCars; i++) {
             cars.add(new Car());
@@ -25,6 +22,6 @@ public class Cars {
     }
 
     public List<Car> getCars() {
-        return cars;
+        return Collections.unmodifiableList(cars);
     }
 }
