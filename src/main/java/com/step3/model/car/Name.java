e6fdeeac366a07ff825029c441a8d102e93c9485
haeuniii <qnfnqnfn9825@gmail.com>
2021-11-21 11:53:27 +0900

    refactor: 매직넘버 상수로 선언

--- src/main/java/com/step3/model/car/Name.java
+++ src/main/java/com/step3/model/car/Name.java
@@ -1,6 +1,10 @@
 package com.step3.model.car;
 
+import java.util.Objects;
+
 public class Name {
+    private static final int CAR_NAME_LENGTH_BOUND = 5;
+
     private String name;
 
     public Name(String name) {
@@ -13,8 +17,16 @@ public class Name {
     }
 
     private void validateCarName(String name) {
-        if (name.length() > 5) {
+        if (name.length() > CAR_NAME_LENGTH_BOUND) {
             throw new IllegalArgumentException("자동차 이름 5자 이하 필수");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name1 = (Name) o;
+        return Objects.equals(name, name1.name);
     }
 }
--- src/main/java/com/step3/model/car/Position.java
+++ src/main/java/com/step3/model/car/Position.java
@@ -1,6 +1,8 @@
 package com.step3.model.car;
 
-public class Position {
+import java.util.Comparator;
+
+public class Position implements Comparable<Position> {
     private int position;
 
     public Position(int position) {
@@ -9,5 +11,31 @@ public class Position {
 
     public int getValue() {
         return position;
+    }
+
+    public Position add() {
+        this.position += 1;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int compareTo(Position p) {
+        if (this.equals(p)) {
+            return 0;
+        }
+
+        if (p.position < this.position) {
+            return 1;
+        }
+
+        return -1;
     }
 }
