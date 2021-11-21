3c7b16aadf7dbba71d6dc0a3bcc5667750a515e8
sojeongw <sojeongw.official@gmail.com>
2021-11-22 00:26:24 +0900

    feat: 실행 결과를 출력한다

---
+++ src/main/java/study/step_3/OutputView.java
@@ -0,0 +1,10 @@
+package study.step_3;
+
+public class OutputView {
+    public static void result(int position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+}
