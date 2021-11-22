f3fd2e191721c865e09ff29dcf860e15a9991575
jiseok-choi <choi300300@gmail.com>
2021-11-22 22:32:10 +0900

    refactor: 리뷰 반영 미사용 코드 제거

--- src/main/java/carracing/controller/CarRacing.java
+++ src/main/java/carracing/controller/CarRacing.java
@@ -1,6 +1,9 @@
 package carracing.controller;
 
-import carracing.domain.*;
+import carracing.domain.Car;
+import carracing.domain.Cars;
+import carracing.domain.RandomCondition;
+import carracing.domain.Winners;
 import carracing.exception.CarNameFormatException;
 import carracing.view.InputInfo;
 import carracing.view.InputView;
@@ -27,9 +30,8 @@ public class CarRacing {
         return new InputInfo(inputView.getCarNames(), inputView.getTryCount());
     }
 
-    public RaceResult racingStart(InputInfo inputInfo) throws CarNameFormatException {
+    public void racingStart(InputInfo inputInfo) throws CarNameFormatException {
 
-        RaceResult raceResult = new RaceResult();
         ResultView resultView = new ResultView();
         List<Car> carList = new ArrayList<>();
 
@@ -39,13 +41,10 @@ public class CarRacing {
         for (int i = 0; i < inputInfo.getTryCount(); i++) {
             racing(cars, inputInfo);                 /* 시도 횟수 만큼 주행 */
             resultView.printRacing(cars);            /* 시도 횟수 만큼 출력 */
-            raceResult.roundCount++;
         }
 
         Winners winners = cars.getWinner();
         resultView.printWinner(winners);
-
-        return raceResult;
     }
 
     private void createCarNameList(InputInfo inputInfo, List<Car> carList) throws CarNameFormatException {
