fdccf81b78078da9e79dc0973b413d131f9a8016
pollra <5346003-pollra32@users.noreply.gitlab.com>
2021-11-22 23:07:30 +0900

    refactor($step3): 코드리뷰 적용
    - 기름보단 위치가 어울려요
    - 배열보단 리스트

--- src/main/java/racing/basic/domain/Car.java
+++ src/main/java/racing/basic/domain/Car.java
@@ -5,15 +5,15 @@ public class Car {
     private final static int FORWARD_CONDITION = 4;
     private final static int OIL = 1;
 
-    private int fuel;
+    private int position;
 
     protected void moveOrStop(int diceNumber) {
         if(FORWARD_CONDITION <= diceNumber) {
-            fuel += OIL;
+            position += OIL;
         }
     }
 
     protected int getDrivingDistance() {
-        return fuel;
+        return position;
     }
 }
