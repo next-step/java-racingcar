590ee5d59da6873e66224c865fecd544aa775c71
Richard JEON <ztzy1907@gmail.com>
2021-11-22 12:09:33 +0900

    Set Collection 요구사항 1 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,13 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
 
@@ -16,6 +20,12 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 크기 확인")
+    void checkSizeOfSet() {
+        assertThat(numbers.size()).isEqualTo(3);
     }
 
 }
