06762a6d68053850574e2984050f79ba85b6ce3d
haeuniii <qnfnqnfn9825@gmail.com>
2021-11-22 19:50:05 +0900

    refactor<Cars>: moveCars 메서드 테스트 코드 구현
    메서드 안에 randomValue를 구하는 메서드가 들어가 있어
    테스트 코드를 구현하기 어려워 개선함.

--- src/test/java/study/CarsTest.java
+++ src/test/java/study/CarsTest.java
@@ -4,7 +4,6 @@ import com.step3.model.car.Car;
 import com.step3.model.car.Cars;
 import com.step3.model.car.Name;
 import com.step3.model.car.Position;
-import com.step3.model.car.strategy.CarMoveStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,53 +12,54 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarsTest {
     @Test
     @DisplayName("차 List를 이용한 생성자 Test")
     void createFromList() {
         List<Car> cars = new ArrayList() {{
-            add(new Car(new Name("test1"), new Position(0), new CarMoveStrategy()));
+            add(new Car(new Name("test1"), new Position(0)));
         }};
 
-        assertThat(Cars.createFromList(cars))
-                .isEqualTo(Cars.createFromList(new ArrayList() {{
-                    add(new Car(new Name("test1"), new Position(0), new CarMoveStrategy()));
-                }}));
+        Cars fromList = Cars.createFromList(cars);
+
+        assertThat(fromList.getList()).containsExactly(new Car(new Name("test1"), new Position(0)));
     }
 
     @Test
     @DisplayName("차 이름을 이용한 생성자 Test")
     void createFromName() {
         List<Car> cars = new ArrayList<>();
-        cars.add(new Car(new Name("test1"), new Position(0), new CarMoveStrategy()));
-        cars.add(new Car(new Name("test2"), new Position(0), new CarMoveStrategy()));
-        cars.add(new Car(new Name("test3"), new Position(0), new CarMoveStrategy()));
+        cars.add(new Car(new Name("test1"), new Position(0)));
+        cars.add(new Car(new Name("test2"), new Position(0)));
+        cars.add(new Car(new Name("test3"), new Position(0)));
 
-        assertThat(Cars.createFromName(new String[]{"test1", "test2", "test3"}, new CarMoveStrategy()))
+        assertThat(Cars.createFromName(new String[]{"test1", "test2", "test3"}))
                 .isEqualTo(Cars.createFromList(cars));
+    }
+
+    @Test
+    void moveCars() {
+        Cars cars = Cars.createFromName("pobi,crong,honux".split(","));
+        List<Car> moveCars = new ArrayList(){{
+            new Car(new Name("pobi"), new Position(1));
+            new Car(new Name("crong"), new Position(1));
+            new Car(new Name("honux"), new Position(1));
+        }};
+
+        assertThat(cars.moveCars(() -> true).getList()).containsAll(moveCars);
     }
 
     @Test
     @DisplayName("cars의 이동거리 비교 후 우승자 결과 test")
     void getBestCars() {
-        Cars cars = Cars.createFromName("pobi,crong,honux".split(","), new CarMoveStrategy());
-        cars.getList().get(0).move(true);   // pobi 이동
-        cars.getList().get(1).move(true);   // crong 이동
+        Cars cars = Cars.createFromName("pobi,crong,honux".split(","));
+        cars.getList().get(0).move(() -> true);   // pobi 이동
+        cars.getList().get(1).move(() -> true);   // crong 이동
         assertThat(cars.getWinnerCars().getList()
                 .stream()
                 .map(c -> c.getName().get())
                 .collect(Collectors.joining(","))
         ).isEqualTo("pobi,crong");
-    }
-
-
-    @Test
-    @DisplayName("자동차 이름 5자 이상 예외처리 test")
-    void validateCarName() {
-        assertThatThrownBy(() -> Cars.createFromName("pobipobi,crong,honux".split(","), new CarMoveStrategy()))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름 5자 이하 필수");
     }
 }
