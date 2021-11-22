e1b3eb3dd696f9164acd1aa5a9de73cebaf92cb4
Richard JEON <ztzy1907@gmail.com>
2021-11-22 13:05:53 +0900

    Set Collection 요구사항 3 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -32,9 +33,16 @@ public class SetTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    @DisplayName("Set Collection 내 값 존재 확인")
+    @DisplayName("Set Collection 내 1, 2, 3 존재 확인")
     void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"-1:false", "0:false", "1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    @DisplayName("Set Collection 내 있는 값이면 true, 없는 값이면 false 반환 확인")
+    void contains2(int number, boolean contains) {
+        assertThat(contains).isEqualTo(numbers.contains(number));
     }
 
 }
