fa81f75a4a023ed192e4e888a237dec7f751bde3
Richard JEON <ztzy1907@gmail.com>
2021-11-22 12:06:22 +0900

    String클래스 요구사항3 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
@@ -17,6 +18,24 @@ public class StringTest {
         String result = "(1,2)";
         result = result.substring(1, result.length()-1);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열의 특정 위치 문자 가져오기")
+    void getCharAt() {
+        String result = "abc";
+
+        assertThatThrownBy(() -> result.charAt(-1))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: -1");
+
+        result.charAt(0);
+        result.charAt(1);
+        result.charAt(2);
+
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> result.charAt(3))
+                .withMessageMatching("String index out of range: \\d+");
     }
 
 }
