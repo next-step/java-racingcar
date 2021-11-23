42f6bf8b819fa58be38a57d25b1b91763e06e6f6
Richard JEON <ztzy1907@gmail.com>
2021-11-23 18:41:19 +0900

    피드백 반영 - 매직 넘버 상수로 정의

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -5,6 +5,11 @@ import java.util.Objects;
 public class StringCalculator {
 
     private static final String IAE_MESSAGE = "사칙연산 기호를 확인해주세요 :(";
+    private static final int INDEX_TO_GET_FIRST_VALUE = 0;
+    private static final int INDEX_TO_START_CALCULATE = 1;
+    private static final int NUMBER_OF_UNITS_TO_CALCULATE = 2;
+    private static final int ADDITIONAL_INDEX_TO_GET_VALUE = 1;
+
     private int result = 0;
 
     public void setResult(int result) {
@@ -16,10 +21,10 @@ public class StringCalculator {
 
     public int calculate(String data) {
         String[] splitData = data.split(" ");
-        result = Integer.parseInt(splitData[0]);
+        result = Integer.parseInt(splitData[INDEX_TO_GET_FIRST_VALUE]);
 
-        for (int i = 1; i < splitData.length; i += 2) {
-            operate(getOperation(splitData[i]), splitData[i + 1]);
+        for (int i = INDEX_TO_START_CALCULATE; i < splitData.length; i += NUMBER_OF_UNITS_TO_CALCULATE) {
+            operate(getOperation(splitData[i]), splitData[i + ADDITIONAL_INDEX_TO_GET_VALUE]);
         }
 
         return result;
