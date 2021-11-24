3417bc7d4708be448ebc1f5519cf6c1e5ec859f3
kero <jdy410@naver.com>
2021-11-25 02:39:10 +0900

    refactor: 불필요한 import 제거

--- src/main/java/step4/service/RacingGameService.java
+++ src/main/java/step4/service/RacingGameService.java
@@ -1,13 +1,10 @@
 package step4.service;
 
-import step4.common.MyException;
 import step4.dto.*;
 
 import java.util.List;
 
 public class RacingGameService {
-    private static final int ZERO = 0;
-
     public RacingGameService() {}
 
     public RaceRecordGroup raceStart(List<String> carsName, TrialCount trialCount) {
@@ -26,6 +23,4 @@ public class RacingGameService {
 
         return raceRecordGroup;
     }
-
-
 }
