6af7f0c11d1dc095eeb0a825850bd7d85d4be17e
김민혁 <kminheuk@gmail.com>
2021-11-25 00:15:25 +0900

    fix: Fix Car to have name field

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,11 +1,30 @@
 package racingcar.model;
 
-public class Car {
+import java.util.Objects;
 
+public class Car {
+    private String name;
     private int position;
 
     public Car() {
         this.position = 1;
+    }
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return position == car.position && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 
     public int getPosition() {
