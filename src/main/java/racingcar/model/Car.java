a1e91564a1bbde1514afdf2d937c080d398c34ce
김민혁 <kminheuk@gmail.com>
2021-11-25 23:44:07 +0900

    style: fix parameter name

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -31,8 +31,8 @@ public class Car {
         return this.position;
     }
 
-    public void run(MoveStrategy randomMoveStrategy) {
-        if (randomMoveStrategy.isMovable()) {
+    public void run(MoveStrategy moveStrategy) {
+        if (moveStrategy.isMovable()) {
             this.position += 1;
         }
     }
