5c079c52a493cf7eae29a5e0e5b812af5f9276d7
김민혁 <kminheuk@gmail.com>
2021-11-26 00:24:24 +0900

    refactor: Refactoring RacingGameTest

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,22 +1,12 @@
 package racingcar.model;
 
-import racingcar.view.InputResult;
-
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class RacingGame {
     private final List<Car> cars;
     private final int lastRound;
-
-    public RacingGame(InputResult inputResult) {
-        cars = new ArrayList<>();
-        IntStream.range(0, inputResult.getNumberOfCars())
-                .forEach((int i) -> cars.add(new Car()));
-        this.lastRound = inputResult.getNumberOfTries();
-    }
 
     public RacingGame(List<Car> cars, int lastRound) {
         this.cars = cars;
@@ -39,5 +29,9 @@ public class RacingGame {
         return cars.stream()
                 .map(Car::getPosition)
                 .collect(Collectors.toList());
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
     }
 }
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,35 +1,29 @@
 package racingcar;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 import racingcar.model.RacingGame;
-import racingcar.view.InputResult;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingGameTest {
 
-    @ParameterizedTest(name = "({0}, {1})의 input을 받으면 {0}대의 차, {1}번 시도하는 게임이 생성되어야 한다")
-    @MethodSource("racingGameInputs")
-    void testPlay_given_3_cars_5_rounds(int numberOfCars, int numberOfTries) {
-        // Given & When
-        RacingGame racingGame = new RacingGame(new InputResult(numberOfCars, numberOfTries));
+    @Test
+    @DisplayName("3대의 차, 5번 시도 횟수를 입력으로 받으면, 3대의 차, last round가 5인 게임이 만들어진다")
+    void testRacingGame_given_3_cars_5_rounds() {
+        // Given
+        List<Car> cars = Arrays.asList(new Car("test1"), new Car("test2"), new Car("test3"));
+        int lastRound = 5;
+
+        // When
+        RacingGame racingGame = new RacingGame(cars, lastRound);
 
         // Then
-        assertThat(racingGame.getNumberOfCars()).isEqualTo(numberOfCars);
-        assertThat(racingGame.getLastRound()).isEqualTo(numberOfTries);
-    }
-
-    static Stream<Arguments> racingGameInputs() {
-        return Stream.of(
-                Arguments.of(1, 1),
-                Arguments.of(2, 2),
-                Arguments.of(3, 3),
-                Arguments.of(4, 4),
-                Arguments.of(5, 6)
-        );
+        assertThat(racingGame.getCars().size()).isEqualTo(cars.size());
+        assertThat(racingGame.getLastRound()).isEqualTo(lastRound);
     }
 }
