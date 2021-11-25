bd6c78cc14680fd96f309dc5f1aef3c7b169547d
dodeon <sojeongw.official@gmail.com>
2021-11-25 16:48:35 +0900

    refactor: Car에 이름 세팅하는 로직을 생성자로 수정한다

--- src/main/java/study/Car.java
+++ src/main/java/study/Car.java
@@ -3,21 +3,21 @@ package study;
 public class Car {
     public static final int LENGTH_LIMIT = 5;
 
-    private String name;
+    private final String name;
     private int position;
 
-    public void move(boolean movable) {
-        if (movable) {
-            this.position++;
-        }
-    }
-
-    public void name(String name) {
+    public Car(String name) {
         if (name.length() > LENGTH_LIMIT) {
             throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
 
         this.name = name;
+    }
+
+    public void move(boolean movable) {
+        if (movable) {
+            this.position++;
+        }
     }
 
     public int getPosition() {
--- src/test/java/study/CarTest.java
+++ src/test/java/study/CarTest.java
@@ -10,10 +10,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
 
+    private final String name = "hazel";
+
     @Test
     @DisplayName("움직일 수 있는 조건이 주어지면 전진한다")
     void shouldIncreasePosition() {
-        Car car = new Car();
+        Car car = new Car(name);
         car.move(true);
 
         assertThat(car.getPosition()).isEqualTo(1);
@@ -22,7 +24,7 @@ class CarTest {
     @Test
     @DisplayName("움직일 수 있는 조건을 만족하지 않으면 전진하지 않는다")
     void shouldHaveSamePosition() {
-        Car car = new Car();
+        Car car = new Car(name);
         car.move(false);
 
         assertThat(car.getPosition()).isEqualTo(0);
@@ -31,9 +33,7 @@ class CarTest {
     @Test
     @DisplayName("5자 이하의 자동차 이름을 자동차에 부여한다")
     void shouldEqualInputName() {
-        String name = "hazel";
-        Car car = new Car();
-        car.name(name);
+        Car car = new Car(name);
 
         assertThat(car.getName()).isEqualTo(name);
         assertThat(car.getName().length()).isLessThanOrEqualTo(5);
@@ -42,10 +42,9 @@ class CarTest {
     @Test
     @DisplayName("자동차 이름이 5자를 초과하면 예외를 던진다")
     void shouldThrowExceptionWhenNameIsOverFiveCharacters() {
-        String name = "monica";
-        Car car = new Car();
+        String illegalName = "monica";
 
-        assertThatThrownBy(() -> car.name(name))
+        assertThatThrownBy(() -> new Car(illegalName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 5자를 초과할 수 없습니다.");
     }
