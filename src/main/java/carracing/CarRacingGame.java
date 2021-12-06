c6a8fc3dd6aa52fd291efd7b8d8dd1f111b99db9
orgojy <giveawesome@gmail.com>
2021-12-06 19:20:31 +0900

    feat(CarRacingGame.java): 자동차 경주 게임에서 자동차 이름을 입력받는 로직 추가

--- src/main/java/carracing/CarRacingGame.java
+++ src/main/java/carracing/CarRacingGame.java
@@ -2,6 +2,7 @@ package carracing;
 
 import carracing.console.InputConsole;
 import carracing.console.OutputConsole;
+import carracing.domain.CarNames;
 import carracing.domain.Cars;
 import carracing.domain.MovementPolicy;
 import carracing.domain.RandomIntMovementPolicy;
@@ -18,11 +19,11 @@ public class CarRacingGame {
     private final MovementPolicy movementPolicy = new RandomIntMovementPolicy(randomGenerator);
 
     public void run() {
-        int numberOfCars = InputConsole.readNumberOfCars();
+        String carNames = InputConsole.readCarNames();
         int numberOfTrials = InputConsole.readNumberOfTrials();
 
         OutputConsole.printResultMessage();
-        Cars cars = new Cars(numberOfCars);
+        Cars cars = new Cars(new CarNames(carNames));
         List<Cars> carsGroup = new ArrayList<>(numberOfTrials);
         for (int i = 0; i < numberOfTrials; i++) {
             cars = cars.moveCars(movementPolicy);
