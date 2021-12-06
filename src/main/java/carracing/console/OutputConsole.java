e80ba1f8366bb27e4d7e30403916a27184409b23
orgojy <giveawesome@gmail.com>
2021-12-06 19:17:43 +0900

    feat(OutputConsole.java): 자동차 경주 현황과 승리자를 화면에 출력하는 로직 추가

--- src/main/java/carracing/console/OutputConsole.java
+++ src/main/java/carracing/console/OutputConsole.java
@@ -5,34 +5,52 @@ import carracing.domain.CarPosition;
 import carracing.domain.Cars;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 /**
- * 자동차 경주 현황을 출력하기위한 콘솔
+ * 자동차 경주 현황을 출력하기 위한 콘솔
  */
 public class OutputConsole {
     private static final String RESULT_MESSAGE = "실행결과";
     private static final String POSITION_NOTATION = "-";
-
-    private OutputConsole() {
-    }
+    private static final String WINNER_RESULT_MESSAGE = "가 최종 우승했습니다.";
 
     public static void printResultOfRacingGame(List<Cars> carsGroup) {
         carsGroup.forEach(OutputConsole::printStatusOfRacing);
+        printWinningCarsOfRacing(lastCars(carsGroup));
     }
 
-    private static void printStatusOfRacing(Cars cars) {
+    public static void printStatusOfRacing(Cars cars) {
         cars.getCars()
-                .stream()
-                .map(Car::getPosition)
-                .forEach(carPosition -> System.out.println(convertStatusOfCar(carPosition)));
+                .forEach(car -> System.out.println(statusOfCar(car)));
         System.out.println();
+    }
+
+    public static void printWinningCarsOfRacing(Cars cars) {
+        String winningCars = cars.retrieveWinningCars()
+                .stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println(winningCars + WINNER_RESULT_MESSAGE);
     }
 
     public static void printResultMessage() {
         System.out.println(RESULT_MESSAGE);
     }
 
-    private static String convertStatusOfCar(CarPosition carPosition) {
+    private static Cars lastCars(List<Cars> carsGroup) {
+        return carsGroup.stream()
+                .skip(Math.min(carsGroup.size(), 1))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("last Cars is not found."));
+    }
+
+    private static String statusOfCar(Car car) {
+        return String.format("%s : %s", car.getName(), notationOfpositionOfCar(car.getPosition()));
+    }
+
+    private static String notationOfpositionOfCar(CarPosition carPosition) {
         int positionNumber = carPosition.getNumber();
         StringBuilder positionNotation = new StringBuilder(positionNumber);
         for (int i = 0; i < positionNumber; i++) {
