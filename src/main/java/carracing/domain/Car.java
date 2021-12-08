0c8a0f7ffcd56a6f2b6d3f531e55d0649de7acaa
orgojy <giveawesome@gmail.com>
2021-12-09 01:02:49 +0900

    refactor: `Car`에서 지정했던 초기 위치 로직을 `CarPosition`으로 위임

--- src/main/java/carracing/domain/Car.java
+++ src/main/java/carracing/domain/Car.java
@@ -4,14 +4,12 @@ package carracing.domain;
  * 자동차 정보를 가지는 자동차 클래스
  */
 public class Car {
-    private static final int INITIATION_POSITION_NUMBER = 0;
-
     private final CarName name;
     private final CarPosition position;
 
     Car(CarName name) {
         this.name = name;
-        this.position = new CarPosition(INITIATION_POSITION_NUMBER);
+        this.position = new CarPosition();
     }
 
     private Car(CarName name, CarPosition position) {
