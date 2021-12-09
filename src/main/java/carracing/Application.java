78f33295ed3430b9ccb78acdc63492365966a192
orgojy <giveawesome@gmail.com>
2021-12-09 23:07:00 +0900

    refactor(Application.java): Model, View, Controller를 분리하여 Application 구성

--- src/main/java/carracing/Application.java
+++ src/main/java/carracing/Application.java
@@ -1,10 +1,12 @@
 package carracing;
 
 import carracing.controller.CarRacingGameController;
+import carracing.view.InputView;
+import carracing.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         CarRacingGameController carRacingGameController = new CarRacingGameController();
-        carRacingGameController.run();
+        OutputView.printResultOfRacingGame(carRacingGameController.run(InputView.readCarNames(), InputView.readNumberOfTrials()));
     }
 }
