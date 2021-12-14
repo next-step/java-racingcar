3c2e205268126e5699cfeb86714fbaafc0f49a6d
90jam <90woozz@gmail.com>
2021-12-14 15:50:18 +0900

    feat 자동차 이름은 5자를 초과할 수 없다는 요구사항 구현

--- src/main/java/racingcar/domain/RacingCarName.java
+++ src/main/java/racingcar/domain/RacingCarName.java
@@ -2,9 +2,15 @@ package racingcar.domain;
 
 public class RacingCarName {
 
+    private static final int NAME_LENGTH_LIMIT = 5;
+    private static final String NAME_LENGTH_LIMIT_EXCCEED_MESSAGE = "자동차 이름은 5자를 초과할 수 없습니다.";
+
     private final String name;
 
     public RacingCarName(String name) {
+        if (name.length() > NAME_LENGTH_LIMIT) {
+            throw new racingcar.exception.RacingGameException(NAME_LENGTH_LIMIT_EXCCEED_MESSAGE);
+        }
         this.name = name;
     }
 
