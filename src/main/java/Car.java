0fd31b03cd832fbe934c3778063b39ed8435f7cf
yyy96 <dlduswo1247@gmail.com>
2021-12-28 12:42:49 +0900

    게임 우승자 발표

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 public class Car {
     public static List<String> cars = new ArrayList<>();
     public static List<String> carsMoving = new ArrayList<>();
-    static final int CAR_NAME_LENGTH_BOUND =5;
-    static final int CAR_MOVE =4;
+    public static List<String> winner = new ArrayList<>();
+    static final int CAR_NAME_LENGTH_BOUND = 5;
+    static final int CAR_MOVE = 4;
 
     Scanner scanner = new Scanner(System.in);
 
-    public void enterCarsName(){
+    public void enterCarsName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String carsName = scanner.next();
         splitCarsName(carsName);
     }
 
-    public void splitCarsName(String carsName){
+    public void splitCarsName(String carsName) {
         String[] carsList = carsName.split(",");
-        for(int i=0; i<carsList.length; i++){
+        for (int i = 0; i < carsList.length; i++) {
             validateNameLengthCheck(carsList[i]);
             cars.add(carsList[i]);
             carsMoving.add("");
@@ -26,15 +27,44 @@ public class Car {
     }
 
     public void validateNameLengthCheck(final String carName) {
-        if (carName.length()>CAR_NAME_LENGTH_BOUND) {
+        if (carName.length() > CAR_NAME_LENGTH_BOUND) {
             throw new IllegalArgumentException("car name is too long");
         }
     }
 
-    public void carsMove(int carIndex, int randomNumber){
-        if(randomNumber>=CAR_MOVE){
-            String newMove = carsMoving.get(carIndex) +"-";
+    public void carsMove(int carIndex, int randomNumber) {
+        if (randomNumber >= CAR_MOVE) {
+            String newMove = carsMoving.get(carIndex) + "-";
             carsMoving.set(carIndex, newMove);
+        }
+    }
+
+    public int findMaxDistance() {
+        int maxDistance = 0;
+        for (int i = 0; i < cars.size(); i++) {
+            int distance = carsMoving.get(i).length();
+            maxDistance = checkMaxDistance(maxDistance, distance);
+        }
+        return maxDistance;
+    }
+
+    public int checkMaxDistance(int maxDistance, int distance) {
+        if (distance > maxDistance) {
+            return distance;
+        }
+        return maxDistance;
+    }
+
+    public void findMaxDistanceCar(int maxDistance) {
+        for (int i = 0; i < cars.size(); i++) {
+            int distance = carsMoving.get(i).length();
+            isMaxDistanceCar(maxDistance, distance, i);
+        }
+    }
+
+    public void isMaxDistanceCar(int maxDistance, int distance, int carIndex) {
+        if (distance == maxDistance) {
+            winner.add(cars.get(carIndex));
         }
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,3 @@
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,36 +7,53 @@ public class Game {
 
     Scanner scanner = new Scanner(System.in);
 
-    void gameStart(){
+    void gameStart() {
         car.enterCarsName();
         enterGameTurn();
         System.out.println("실행결과");
-        for(int i=0; i<gameTurn; i++){
+        for (int i = 0; i < gameTurn; i++) {
             racing();
         }
+        findWinner();
     }
 
-    void enterGameTurn(){
+    void enterGameTurn() {
         System.out.println("시도할 회수는 몇회인가요?");
         gameTurn = scanner.nextInt();
         System.out.println();
     }
 
-    void racing(){
+    void racing() {
         Random random = new Random();
-        for(int i=0; i<Car.cars.size(); i++){
+        for (int i = 0; i < Car.cars.size(); i++) {
             int randomNumber = random.nextInt(10);
             car.carsMove(i, randomNumber);
         }
-        printResult();
+        printTurnResult();
     }
 
-    void printResult(){
-        for(int i=0; i<Car.cars.size(); i++){
+    void printTurnResult() {
+        for (int i = 0; i < Car.cars.size(); i++) {
             String carName = Car.cars.get(i);
             String carMoving = Car.carsMoving.get(i);
-            System.out.println(carName+" : "+carMoving);
+            System.out.println(carName + " : " + carMoving);
         }
         System.out.println();
+    }
+
+    void findWinner() {
+        int maxDistance = car.findMaxDistance();
+        car.findMaxDistanceCar(maxDistance);
+        printWinnerResult();
+    }
+
+    void printWinnerResult() {
+        for (int i = 0; i < Car.winner.size(); i++) {
+            if (i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(Car.winner.get(i));
+        }
+        System.out.println("가 최종 우승했습니다");
     }
 }
