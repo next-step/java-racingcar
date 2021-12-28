d46ee5d5bd5f55ba13c90081564aa842b15d89d9
yyy96 <dlduswo1247@gmail.com>
2021-12-28 10:28:41 +0900

    몇 회 이동할지 입력받기

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -4,9 +4,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Car {
-    List<String> car = new ArrayList<>();
-    Scanner scanner = new Scanner(System.in);
+    static List<String> car = new ArrayList<>();
     static final int CAR_NAME_LENGTH_BOUND =5;
+
+    Scanner scanner = new Scanner(System.in);
 
     public void enterCarsName(){
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Game {
     Car car = new Car();
+    static int gameTurn;
+
+    Scanner scanner = new Scanner(System.in);
 
     void gameStart(){
         car.enterCarsName();
+        enterGameTurn();
+    }
+
+    void enterGameTurn(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        gameTurn = scanner.nextInt();
     }
 }
