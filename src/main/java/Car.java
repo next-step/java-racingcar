0ec1c8aa89d99f644c655d41374d327f31031a94
yyy96 <dlduswo1247@gmail.com>
2021-12-28 10:23:44 +0900

    자동차 이름 글자제한(5자)

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Car {
     List<String> car = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
+    static final int CAR_NAME_LENGTH_BOUND =5;
 
     public void enterCarsName(){
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
@@ -16,7 +17,14 @@ public class Car {
     public void splitCarsName(String carsName){
         String[] cars = carsName.split(",");
         for(int i=0; i<cars.length; i++){
+            validateNameLengthCheck(cars[i]);
             car.add(cars[i]);
+        }
+    }
+
+    public void validateNameLengthCheck(final String carName) {
+        if (carName.length()>CAR_NAME_LENGTH_BOUND) {
+            throw new IllegalArgumentException("car name is too long");
         }
     }
 }
