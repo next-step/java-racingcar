b1af5dad5198afbc6819be72465577eb6bc13111
smc5236 <smc5236@gmail.com>
2022-01-17 18:05:04 +0900

    feat: 사용자 입력값 추출기
    - 공백을 기준

--- src/test/java/common/UserInput.java
+++ src/test/java/common/UserInput.java
@@ -3,11 +3,19 @@ package common;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserInput {
 
-    public String userInput() throws IOException {
+    public UserInput() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        return bufferedReader.readLine();
+        String userInput = bufferedReader.readLine();
+        splitStr(userInput);
+    }
+
+    private List<String> splitStr(final String userInput) {
+        return Arrays.stream(userInput.split(" ")).collect(Collectors.toList());
     }
 }
