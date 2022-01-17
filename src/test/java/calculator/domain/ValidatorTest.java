b777a0e6fa08c186d67495b65a880b61f82a5d60
kimtaejun97 <kim_taejun@naver.com>
2022-01-17 23:31:42 +0900

    test: validate() Test
    - 공백("", " ")인지
    - 짝수번째가 숫자인지
    - 홀수번째가 연산자인지
      - +, - , *, / 인지

--- src/test/java/calculator/domain/ValidatorTest.java
+++ src/test/java/calculator/domain/ValidatorTest.java
@@ -3,57 +3,69 @@ package calculator.domain;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTest {
 
     @Test
-    void 홀수번째는_숫자이다_성공() {
-        // given
-        String userInput = "1 + 3";
-        String[] split = userInput.split(" ");
+    void 입력으로_공백이_들어옴_실패(){
 
+        //given
+        String userInput = " ";
 
         // then
-        assertDoesNotThrow(()->Validator.oddIndexIsNumber(split));
+        assertThrows(IllegalArgumentException.class, ()->Validator.validateUserInput(userInput));
+    }
+
+    @Test
+    void 홀수번째는_숫자이다_성공() {
+
+        // given
+        String userInput = "1 + 3";
+
+        // then
+        assertDoesNotThrow(()->Validator.validateUserInput(userInput));
     }
 
     @Test
     void 홀수번째는_숫자이다_실패_숫자가아님() {
+
         // given
         String userInput = "/ + 3";
-        String[] split = userInput.split(" ");
 
         // then
-        assertThrows(IllegalArgumentException.class, ()->Validator.oddIndexIsNumber(split));
+        assertThrows(IllegalArgumentException.class, ()->Validator.validateUserInput(userInput));
     }
 
     @Test
     void 홀수번째는_숫자이다_실패_공백여러개() {
+
         // given
         String userInput = "1 +  3";
-        String[] split = userInput.split(" ");
-        System.out.println(split);
 
         // then
-        assertThrows(IllegalArgumentException.class, ()->Validator.oddIndexIsNumber(split));
+        assertThrows(IllegalArgumentException.class, ()->Validator.validateUserInput(userInput));
     }
 
-    @Test
-    void 짝수번째는_연산자이다_성공() {
+    @ValueSource(strings = {"+", "-", "*", "/"})
+    @ParameterizedTest
+    void 짝수번째는_연산자이다_성공(String op) {
+
         // given
-        String userInput = "1 + 3";
-        String[] split = userInput.split(" ");
+        String userInput = "1 " + op + " 3";
 
-        assertDoesNotThrow((()->Validator.evenIndexIsOperator(split)));
+        assertDoesNotThrow((()->Validator.validateUserInput(userInput)));
     }
+
 
     @Test
     void 짝수번째는_연산자이다_실패_연산자가_아님() {
+
         // given
         String userInput = "1 2 3";
-        String[] split = userInput.split(" ");
 
         // then
-        assertThrows(IllegalArgumentException.class, ()->Validator.evenIndexIsOperator(split));
+        assertThrows(IllegalArgumentException.class, ()->Validator.validateUserInput(userInput));
     }
 }
