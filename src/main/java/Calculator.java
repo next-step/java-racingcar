f49f88b5b44691b42ce66e8e9f5ad04762ca6406
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-17 17:54:30 +0900

    feat: sub() 함수 추가

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,4 +6,8 @@ public class Calculator {
     public int add(int num1, int num2) {
         return num1 + num2;
     }
+
+    public int sub(int num1, int num2) {
+        return num1 - num2;
+    }
 }
