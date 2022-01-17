06a2bd4871c6adb98a084c89a2673829a797be43
kimtaejun97 <kim_taejun@naver.com>
2022-01-17 23:30:33 +0900

    feat: 입력으로 공백이 들어왔을 때 예외처리 기능 구현
    isBlank() 메서드 구현, "" | " " 예외 처리

--- src/main/java/calculator/domain/Validator.java
+++ src/main/java/calculator/domain/Validator.java
@@ -2,14 +2,22 @@ package calculator.domain;
 
 public class Validator {
 
-    public static void validate(String userInput){
+    public static void validateUserInput(String userInput){
+        checkIsBlack(userInput);
+
         String[] split = userInput.split(" ");
 
         oddIndexIsNumber(split);
         evenIndexIsOperator(split);
     }
 
-    public static void oddIndexIsNumber(String[] split) {
+    private static void checkIsBlack(String userInput) {
+        if(userInput.equals("") || userInput.equals(" ")){
+            throw new IllegalArgumentException("[Error] 입력 형식이 맞지 않습니다.");
+        }
+    }
+
+    private static void oddIndexIsNumber(String[] split) {
         for (int i = 0; i < split.length; i+=2) {
             isNumber(split[i]);
         }
@@ -17,15 +25,15 @@ public class Validator {
 
     private static void isNumber(String target) {
         if(target.length() == 0 || !Character.isDigit(target.charAt(0))){
-            throw new IllegalArgumentException("[ERROR] 입력형식이 맞지 않습니다.");
+            throw new IllegalArgumentException("[ERROR] 입력 형식이 맞지 않습니다.");
         }
     }
 
 
-    public static void evenIndexIsOperator(String[] split) {
+    private static void evenIndexIsOperator(String[] split) {
         for(int i=1; i< split.length; i+=2){
             if(!isOperator(split[i])){
-                throw new IllegalArgumentException("[ERROR] 입력형식이 맞지 않습니다.");
+                throw new IllegalArgumentException("[ERROR] 입력 형식이 맞지 않습니다.");
             }
         }
     }
