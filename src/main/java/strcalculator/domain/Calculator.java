ce7aae53dcfbed056b83c06ec1f1e36ed7cf9cbd
xxell8 <cell8pepper@gmail.com>
2022-01-17 18:07:17 +0900

    feat: Create Calculator and Write Function

---
+++ src/main/java/strcalculator/domain/Calculator.java
@@ -0,0 +1,22 @@
+package strcalculator.domain;
+
+public class Calculator {
+    public Calculator() {
+    }
+
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int subtract(int number1, int number2) {
+        return number1 - number2;
+    }
+
+    public int multiply(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public int divide(int number1, int number2) {
+        return number1 / number2;
+    }
+}
