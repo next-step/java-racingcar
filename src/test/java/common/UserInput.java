5a01595daa251553b8a212787bc307ab013c2a8b
smc5236 <smc5236@gmail.com>
2022-01-17 18:29:40 +0900

    feat: 사용자 입력값 검증 함수 구현
    - null 체크
    - 공백 체크

--- src/test/java/common/UserInput.java
+++ src/test/java/common/UserInput.java
@@ -3,11 +3,20 @@ package common;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class UserInput {
+    private static final String ERROR_MESSAGE_NULL = "[ERROR] 입력값은 NULL일 수 없습니다.";
+    private static final String ERROR_MESSAGE_BLANK = "[ERROR] 입력값은 공백일 수 없습니다.";
+    private static final String ERROR_MESSAGE_SIGN = "[ERROR] 입력값이 사칙 연산 기호가 아닙니다.";
+    private static final List<String> SINGS = new ArrayList<>(Arrays.asList("+", "-", "/", "*"));
+
+    private List<Integer> numbers = new ArrayList<>();
+    private List<String> signs = new ArrayList<>();
 
     public UserInput() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -17,5 +26,32 @@ public class UserInput {
 
     private List<String> splitStr(final String userInput) {
         return Arrays.stream(userInput.split(" ")).collect(Collectors.toList());
+    }
+
+    private void segregateSignAndNumber(final List<String> splits) {
+        for (String split : splits) {
+            if (SINGS.contains(split)) {
+                signs.add(split);
+                continue;
+            }
+            numbers.add(Integer.valueOf(split));
+        }
+    }
+
+
+    private void validateNull(final String userInput) {
+        if (Objects.isNull(userInput)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_NULL);
+        }
+    }
+
+    private void validateBlank(final String userInput) {
+        if (userInput.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BLANK);
+        }
+    }
+
+    private void validateSign(final String userInput) {
+
     }
 }
