8f12e04ea1e7e631e08e377fcf2a3c4814641c18
Hoya-kim <1203play@gmail.com>
2022-01-18 17:04:39 +0900

    feat: Implement Calculator for operation, add, sub, mul, div

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,5 +1,19 @@
 package calculator.domain;
 
 public class Calculator {
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
 
+    public static int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int mul(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static int div(int num1, int num2) {
+        return num1 / num2;
+    }
 }
