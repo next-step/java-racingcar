3cd74aa63d4797dc18028acd397f4f8c101b01d4
0JUUU <clleo97@naver.com>
2022-01-18 23:31:15 +0900

    feat: 사칙연산 로직 구현

--- src/main/java/calculator/domain/Validator.java
+++ src/main/java/calculator/domain/Validator.java
@@ -1,11 +1,25 @@
 package calculator.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
-    
+
     public static boolean checkValidate(String expression) {
 
         String pattern ="^\\d+(\\s*[-+*/]\\s*\\d+)+$";
 
-        return expression.matches(pattern);
+        if(!expression.matches(pattern))
+            return false;
+
+        Pattern patternDivideZero = Pattern.compile("[/]\\s*[0]\\s*[-+*/]*");
+        Matcher matcher = patternDivideZero.matcher(expression);
+
+        if(matcher.find()) {
+            return false;
+        }
+
+        return true;
+
     }
 }
