26742429c9674c150912beda40e16b2d4ea17854
0JUUU <clleo97@naver.com>
2022-01-18 17:37:28 +0900

    feat: 예외사항 검사(0으로 나눌 때 제외)

--- src/main/java/calculator/domain/Validator.java
+++ src/main/java/calculator/domain/Validator.java
@@ -1,14 +1,11 @@
 package calculator.domain;
 
-import java.util.StringTokenizer;
-
 public class Validator {
-
-    private static final String[] OPERATORS = {"+", "-", "*", "/"};
-
-    private boolean isValidate;
-
+    
     public static boolean checkValidate(String expression) {
-        return false;
+
+        String pattern ="^\\d+(\\s*[-+*/]\\s*\\d+)+$";
+
+        return expression.matches(pattern);
     }
 }
