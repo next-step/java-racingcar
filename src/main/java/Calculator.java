73bef69cabc09ab9f767fe5faca7d435447924c6
hyoenju <l67378810@gmail.com>
2022-01-19 00:15:45 +0900

    feat(calc): 연산자 타입 별 이항연산 수행
    - "+" : add() func
    - "-" : sub() func
    - "*" : mul() func
    - "/" : div() func

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -18,4 +18,18 @@ public class Calculator {
     public float div(int num1, int num2) {
         return num1 / num2;
     }
+
+    public float doBinaryOperation(int num1, int num2, String ope) {
+        switch (ope) {
+            case "+":
+                return add(num1, num2);
+            case "-":
+                return sub(num1, num2);
+            case "*":
+                return mul(num1, num2);
+            case "/":
+                return div(num1, num2);
+        }
+        return 0;
+    }
 }
