a2f76c76c1fd3587ebb0f154fe9fd4f26a55161a
jihyunhillpark <girl9505@naver.com>
2022-01-18 17:36:43 +0900

    feat: 연산 쪼개기 기능 구현

--- src/main/java/calculator/util/FormulaUtil.java
+++ src/main/java/calculator/util/FormulaUtil.java
@@ -1,5 +1,14 @@
 package calculator.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FormulaUtil {
 
+    private FormulaUtil(){}
+
+    public static List<String> split(String formular) {
+        return new ArrayList<>(Arrays.asList(formular.split(" ")));
+    }
 }
---
+++ src/test/java/calculator/util/FormulaUtilTest.java
@@ -0,0 +1,21 @@
+package calculator.util;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FormulaUtilTest {
+
+    @Test
+    void 연산쪼깨기(){
+        List<String> result = FormulaUtil.split("6 / 2 + 3");
+
+        assertThat(result.size()).isEqualTo(5);
+        assertThat(result.get(0)).isEqualTo("6");
+        assertThat(result.get(1)).isEqualTo("/");
+        assertThat(result.get(2)).isEqualTo("2");
+        assertThat(result.get(3)).isEqualTo("+");
+        assertThat(result.get(4)).isEqualTo("3");
+    }
+}
