678254c3ec6cc0192a074cf6f04dc79e128f36b4
jihyunhillpark <girl9505@naver.com>
2022-01-18 18:18:26 +0900

    feat: 입력값 null 혹은 공백 여부 확인

--- src/main/java/calculator/ui/FormulaInput.java
+++ src/main/java/calculator/ui/FormulaInput.java
@@ -6,15 +6,22 @@ import java.io.InputStreamReader;
 
 public class FormulaInput {
 
-    public String getFormula() {
+    public static String getFormula() {
         String input = "";
         System.out.println("계산할 연산식을 입력해주세요.");
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             input = bufferedReader.readLine();
+            isBlankOrNot(input);
         } catch (IllegalArgumentException | IOException e) {
             System.out.println("[ERROR]");
             getFormula();
         }
         return input;
+    }
+
+    private static void isBlankOrNot(String input) {
+        if (input == null || "".equals(input)) {
+            throw new IllegalArgumentException("입력값이 Null이거나 빈 공백 문자입니다!");
+        }
     }
 }
