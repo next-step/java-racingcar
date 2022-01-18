39c116bfa0c25ee2ce2f6da596c25b32203a99e2
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-18 18:06:28 +0900

    feat(validate): Number Class에 들어온 입력이 숫자인지 검증

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -9,8 +9,11 @@ public class Number {
         numbers = new ArrayList<>();
     }
 
-    public void addNumber(int num) {
-        numbers.add(num);
+    public void addNumber(String num) {
+        if (!Character.isDigit(num.charAt(0))) {
+            throw new NumberFormatException("숫자가 아닙니다");
+        }
+        numbers.add(Integer.valueOf(num));
     }
 
     public void print() {
