1af8c24dd485d1e6e0aceeb66b052a39651c6596
0JUUU <clleo97@naver.com>
2022-01-18 17:06:04 +0900

    feat: to create Validator.java

---
+++ src/main/java/calculator/domain/Validator.java
@@ -0,0 +1,14 @@
+package calculator.domain;
+
+import java.util.StringTokenizer;
+
+public class Validator {
+
+    private static final String[] OPERATORS = {"+", "-", "*", "/"};
+
+    private boolean isValidate;
+
+    public static boolean checkValidate(String expression) {
+        return false;
+    }
+}
