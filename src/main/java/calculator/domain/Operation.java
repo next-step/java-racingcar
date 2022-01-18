abd0de45a3bdbcdf6f738e3f10cfe9a399ec1e66
0JUUU <clleo97@naver.com>
2022-01-18 17:05:44 +0900

    feat: to create Operation.java

---
+++ src/main/java/calculator/domain/Operation.java
@@ -0,0 +1,11 @@
+package calculator.domain;
+
+public class Operation {
+
+    private double result = 0;
+
+    public void setInitiateNumber(int first) {
+        result = (double) first;
+    }
+
+}
