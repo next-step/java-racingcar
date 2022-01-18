37362489dcbe760c3d9a53de376a08e79fc1942d
Hoya-kim <1203play@gmail.com>
2022-01-18 17:00:47 +0900

    test: Add test case. add, sub, mul, div
    - add, sub, mul, div

--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -1,7 +1,34 @@
 package calculator.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class CalculatorTest {
+
+    @Test
+    void add() {
+        int result = Calculator.add(1, 3);
+        assertThat(result).isEqualTo(4);
+    }
+
+    @Test
+    void sub() {
+        int result = Calculator.sub(3, 1);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void mul() {
+        int result = Calculator.mul(2, 3);
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    void div() {
+        int result = Calculator.div(6, 3);
+        assertThat(result).isEqualTo(2);
+    }
 
 }
