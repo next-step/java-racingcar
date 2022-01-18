e4a47310f80a87be3db65699b95830eb5b6edbec
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-18 17:29:09 +0900

    feat: Operator 클래스를 추가
    - 유저 인풋의 split된 두 개의 피연산자 값을 받아와 Operator 클래스에서 계산
    - +, -, *, / 외에 문자 들어오면 exception 처리

---
+++ src/main/java/domain/Operator.java
@@ -0,0 +1,48 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public enum Operator {
+    PLUS("+", (number, operand) ->  Operator.add(number, operand)),
+    MINUS("-", (number, operand) -> Operator.sub(number, operand)),
+    DIVIDE("/", (number, operand) -> Operator.divide(number, operand)),
+    MULTIPLE("*", (number, operand) -> Operator.multiple(number, operand));
+
+    private static final String ERROR_MESSAGE_SIGN = "[ERROR] 입력값이 사칙 연산 기호가 아닙니다.";
+
+    private String sign;
+    private BiFunction<Integer, Integer, Integer> operate;
+
+    Operator(final String sign, final BiFunction<Integer, Integer, Integer> operate) {
+        this.sign = sign;
+        this.operate = operate;
+    }
+
+    public static Operator of(final String inputSign) {
+        return Arrays.stream(Operator.values())
+            .filter(operator -> operator.sign.equals(inputSign))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(ERROR_MESSAGE_SIGN));
+    }
+
+    private static int add(final int number, final int operand) {
+        return number + operand;
+    }
+
+    private static int sub(final int number, final int operand) {
+        return number - operand;
+    }
+
+    private static int divide(final int number, final int operand) {
+        return number / operand;
+    }
+
+    private static int multiple(final int number, final int operand) {
+        return number * operand;
+    }
+
+    public int operate(final int number, final int operand) {
+        return this.operate.apply(number, operand);
+    }
+}
