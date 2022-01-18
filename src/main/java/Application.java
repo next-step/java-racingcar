c3b5129df314a84d7e15b25d51cd5c8d3694e6e0
Ji In Lee <loveted0225@naver.com>
2022-01-18 16:49:11 +0900

    feat(Application): 계산기 시작 기능 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        int result;
+        User user = new User();
+        final List<String> userInput= user.getUserInput();
+        System.out.println(userInput);
+        Calculator calculator= new Calculator();
+        result= calculator.calculateByOperator(userInput);
+        System.out.println(result);
+    }
+}
