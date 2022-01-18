2439e6a59c24a40d8ca81b307158e013f54cb51d
hyoenju <l67378810@gmail.com>
2022-01-19 00:22:07 +0900

    fix(calc) : Change Data Type(int => float)

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,19 +3,19 @@ public class Calculator {
 
     }
 
-    public int add(int num1, int num2) {
+    public float add(float num1, float num2) {
         return num1 + num2;
     }
 
-    public int sub(int num1, int num2) {
+    public float sub(float num1, float num2) {
         return num1 - num2;
     }
 
-    public int mul(int num1, int num2) {
+    public float mul(float num1, float num2) {
         return num1 * num2;
     }
 
-    public float div(int num1, int num2) {
+    public float div(float num1, float num2) {
         return num1 / num2;
     }
 
