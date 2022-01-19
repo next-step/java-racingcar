791de7a81cda983196bf23aa732eea8fd5c20e93
Tommy <g1.tommy.jeon@gmail.com>
2022-01-19 21:36:30 +0900

    feat: Application에서 Game 실행
    - 입력 및 게임 실행 구현
    
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,26 @@
 import calculator.Calculator;
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.Game;
+import racingcar.domain.Car;
+import racingcar.utils.Console;
 
 public class Application {
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println("결과값은 " + calc.value() + "입니다.");
+//        Calculator calc = new Calculator();
+//        System.out.println("결과값은 " + calc.value() + "입니다.");
+
+        List<String> carNames = Console.getCarNames();
+        int carCount = carNames.size();
+        int turn = Console.getTurn();
+
+        // Initialize Car Objects and Run Game
+        List<Car> cars = new ArrayList<>();
+        for (int index = 0; index < carCount; index++) {
+            cars.add(new Car(carNames.get(index)));
+        }
+
+        new Game(cars, turn).play();
     }
 }
