d156de7f3e6e489bc6c1b7d245c536d27d8e64e9
0JUUU <clleo97@naver.com>
2022-01-19 16:42:38 +0900

    feat: 입력값이 null이거나 빈 공백일 경우, 유효성 검사

--- src/test/java/calculator/domain/ValidatorTest.java
+++ src/test/java/calculator/domain/ValidatorTest.java
@@ -1,7 +1,16 @@
 package calculator.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTest {
 
+    @ValueSource(strings = {"", "  "})
+    @ParameterizedTest
+    void 입력값_null_or_빈칸(String input) {
+        assertThat(Validator.checkValidate(input)).isFalse();
+    }
 }
