688fd335f430249d57a2e63a821c1a930e447fb3
HanSeoungjin <kktmdwls1000@konkuk.ac.kr>
2022-01-19 18:09:26 +0900

    feat: 사용자 입력 클래스 추가

---
+++ src/main/java/racing/Application.java
@@ -0,0 +1,7 @@
+package racing;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/racing/domain/UserInput.java
@@ -0,0 +1,10 @@
+package racing.domain;
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void getCarNames() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+    }
+}
