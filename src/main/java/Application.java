e775ea27a22107e2fde12097ff2694462615adc3
Hae-Riri <ehl3288@naver.com>
2022-01-19 17:57:49 +0900

    feat: 연산 실행 기능 추가
    - 사칙연산 메소드 호출을 통한 전체 연산 실행

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,18 @@
-import com.sun.org.apache.xpath.internal.operations.Bool;
+import domain.Calculator;
 import domain.Validator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Application {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Validator validator = new Validator();
-        validator.stringCheck(br.readLine());
+        String[] input = validator.stringCheck(br.readLine());
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.execute(input));
     }
 }
---
+++ src/test/java/domain/CalculatorTest.java
@@ -0,0 +1,51 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void 나눗셈_0으로_나눌_때() {
+        String[] data = {"5", "/", "0"};
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> calculator.execute(data));
+    }
+
+    @Test
+    void 덧셈() {
+        String[] data = {"5", "+", "9"};
+        Assertions.assertThat(calculator.execute(data)).isEqualTo(14);
+    }
+
+    @Test
+    void 뺼셈_결과_음수() {
+        String[] data = {"1", "-", "3"};
+        Assertions.assertThat(calculator.execute(data)).isEqualTo(-2);
+    }
+
+    @Test
+    void 곱셈() {
+        String[] data = {"5", "*", "3"};
+        Assertions.assertThat(calculator.execute(data)).isEqualTo(15);
+    }
+
+    @Test
+    void 나눗셈_결과_2자리_이상() {
+        String[] data = {"1", "/", "3"};
+        double result = calculator.execute(data);
+        Assertions.assertThat(result).isEqualTo(0.33);
+    }
+}
