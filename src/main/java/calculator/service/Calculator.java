380634c3abde1f26d523c7d16f10883974b30bd1
jihyunhillpark <girl9505@naver.com>
2022-01-19 18:51:42 +0900

    feat:  다항식 계산 기능 추가

--- src/main/java/calculator/service/Calculator.java
+++ src/main/java/calculator/service/Calculator.java
@@ -1,8 +1,39 @@
 package calculator.service;
 
+import calculator.util.Operation;
+import java.util.List;
+import java.util.Stack;
+
 public class Calculator {
 
     private Calculator() {
+    }
+
+    public static int run(List<String> formula){
+        Stack<String> formulaStack = new Stack<>();
+
+        for(int i = formula.size()-1; i>=0; i--){
+            formulaStack.push(formula.get(i));
+        }
+        while(formulaStack.size() > 1){
+            int prev = Integer.parseInt(formulaStack.pop());
+            Operation operator = Operation.fromString(formulaStack.pop()).get();
+            int next = Integer.parseInt(formulaStack.pop());
+
+            int result = 0;
+            if(Operation.PLUS == operator){
+                result = add(prev, next);
+            }else if(Operation.MINUS == operator){
+                result = sub(prev, next);
+            }else if(Operation.TIMES == operator){
+                result = multiply(prev, next);
+            }else if(Operation.DIVDE == operator){
+                result = divide(prev, next);
+            }
+            formulaStack.push(String.valueOf(result));
+        }
+
+        return Integer.parseInt(formulaStack.pop());
     }
 
     public static int add(int prev, int next) {
