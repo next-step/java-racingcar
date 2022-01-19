f3d505bd2f6d095ebb67ddce7a69d523f0495dc5
kimtaejun97 <kim_taejun@naver.com>
2022-01-19 20:38:55 +0900

    test: 불필요한 공백제거 기능 테스트

--- src/test/java/racinggame/domain/InputTest.java
+++ src/test/java/racinggame/domain/InputTest.java
@@ -30,4 +30,34 @@ class InputTest {
 
         assertThat(trial).isEqualTo(5);
     }
+
+    @Test
+    void 시도횟수_입력값에_불필요한_공백이_들어왔을_때_제거() throws IOException {
+        InputStream inputStream = new ByteArrayInputStream("1 0".getBytes());
+        System.setIn(inputStream);
+
+        assertDoesNotThrow(() -> Input.getTrial());
+    }
+
+    @Test
+    void 시도횟수_입력값에_불필요한_공백이_들어왔을_때_제거_값_잘들어오는지() throws IOException {
+
+        InputStream inputStream = new ByteArrayInputStream("1 0".getBytes());
+        System.setIn(inputStream);
+
+        int input = Input.getTrial();
+        assertThat(input).isEqualTo(10);
+    }
+
+    @Test
+    void carName_입력값에_불필요한_공백이_들어왔을_때_제거() throws IOException {
+        InputStream inputStream = new ByteArrayInputStream("car1, car2, c ar3".getBytes());
+        System.setIn(inputStream);
+
+        String[] carNames = Input.getCarNames();
+
+        assertThat(carNames[0]).isEqualTo("car1");
+        assertThat(carNames[1]).isEqualTo("car2");
+        assertThat(carNames[2]).isEqualTo("car3");
+    }
 }
