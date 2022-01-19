1bb84b54b95c71a22454202583f093c478763e1d
Hoya-kim <1203play@gmail.com>
2022-01-19 18:29:04 +0900

    feat: 참가자 2명 이상 여부 검증 로직 추가

--- src/main/java/racinggame/domain/Validator.java
+++ src/main/java/racinggame/domain/Validator.java
@@ -2,4 +2,9 @@ package racinggame.domain;
 
 public class Validator {
 
+    public static void validatePossibleToStart(String[] carNames) {
+        if (carNames.length < 2) {
+            throw new IllegalArgumentException("참가자는 2명 이상입니다.");
+        }
+    }
 }
