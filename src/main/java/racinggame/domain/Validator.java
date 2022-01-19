f7a93598d9198d5128ba3d43b9843f5fda4ca41c
Hoya-kim <1203play@gmail.com>
2022-01-19 18:41:27 +0900

    feat: 시도 횟수 입력값 숫자인지 확인하는 로직 구현`

--- src/main/java/racinggame/domain/Validator.java
+++ src/main/java/racinggame/domain/Validator.java
@@ -13,4 +13,12 @@ public class Validator {
             throw new IllegalArgumentException("자동차 이름의 글자 수는 5자 이하입니다.");
         }
     }
+
+    public static void isNumber(String trial) {
+        try {
+            Integer.parseInt(trial);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도횟수는 숫자로 입력해주세요.");
+        }
+    }
 }
