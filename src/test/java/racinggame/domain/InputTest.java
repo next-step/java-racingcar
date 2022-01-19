87c8e26d6e83bbccc05f77dbaeb5de3f207673a9
kimtaejun97 <kim_taejun@naver.com>
2022-01-19 19:12:59 +0900

    test: 시도횟수 입력 받기 테스트.
    - 정상 입력을 받았을 때 제대로된 값을 반환하는지 테스트

--- src/test/java/racinggame/domain/InputTest.java
+++ src/test/java/racinggame/domain/InputTest.java
@@ -20,4 +20,14 @@ class InputTest {
         assertThat(carNames[0]).isEqualTo("car1");
         assertThat(carNames[1]).isEqualTo("car2");
     }
+
+    @Test
+    void 시도횟수_정상_입력() throws IOException {
+        InputStream inputStream = new ByteArrayInputStream("5".getBytes());
+        System.setIn(inputStream);
+
+        int trial = Input.getTrial();
+
+        assertThat(trial).isEqualTo(5);
+    }
 }
