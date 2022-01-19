0f5f1389e62ffaf3c21f299d2a020d59f6869a53
0JUUU <clleo97@naver.com>
2022-01-19 17:04:16 +0900

    feat: 유효한 식을 계산할 클래스 구현
    1. 식에 포함된 공백 제거
    2. 연산자와 피연산자 구분
    3. 초기값 설정
    4. 사칙연산
    	1) 0으로 나눌 경우 예외 던지기
    5. 결과 출력

--- src/main/java/calculator/domain/Expression.java
+++ src/main/java/calculator/domain/Expression.java
@@ -1,7 +1,56 @@
 package calculator.domain;
 
-public class Expression {
-    public Expression(String expression) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Expression {
+
+    private List<String> numbers = new ArrayList<>();
+    private List<String> operators = new ArrayList<>();
+
+    private double result = 0;
+
+    public Expression(String expression) {
+        expression = expression.replaceAll(" ", "");
+        numbers = Arrays.asList(expression.split("[-+*/]"));
+        operators = Arrays.asList(expression.split("[0-9]+"));
+        result = Integer.parseInt(numbers.get(0));
+    }
+
+    public void calculate() {
+        for (int i = 1, size = numbers.size(); i < size; i++) {
+            int operand = Integer.parseInt(numbers.get(i));
+            String operator = operators.get(i);
+
+            if (operator.equals("/") && operand == 0) {
+                throw new ArithmeticException();
+            }
+
+            if (operator.equals("+")) {
+                result = Operation.addition(result, operand);
+                continue;
+            }
+
+            if (operator.equals("-")) {
+                result = Operation.subtraction(result, operand);
+                continue;
+            }
+
+            if (operator.equals("*")) {
+                result = Operation.multiplication(result, operand);
+                continue;
+            }
+
+            if (operator.equals("/")) {
+                result = Operation.division(result, operand);
+                continue;
+            }
+        }
+    }
+
+    public void printResult() {
+        System.out.println("----------결과----------");
+        System.out.println(result);
     }
 }
