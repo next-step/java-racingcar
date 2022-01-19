99d1f629af70b03055f097be573af9093c68cb08
Hoya-kim <1203play@gmail.com>
2022-01-19 18:40:43 +0900

    test: 시도횟수 검증하는 테스트 케이스 추가
    - 시도 횟수 입력값은 숫자 이어야 한다.

--- src/test/java/racinggame/domain/ValidatorTest.java
+++ src/test/java/racinggame/domain/ValidatorTest.java
@@ -8,7 +8,6 @@ class ValidatorTest {
 
     @Test
     void 참여자는_두_명_이상이다_실패() {
-
         String[] carNames = "car1".split(",");
 
         assertThrows(IllegalArgumentException.class,
@@ -17,7 +16,6 @@ class ValidatorTest {
 
     @Test
     void 참여자는_두_명_이상이다_성공() {
-
         String[] carNames = "car1,car2".split(",");
 
         assertDoesNotThrow(() -> Validator.validatePossibleToStart(carNames));
@@ -32,9 +30,22 @@ class ValidatorTest {
 
     @Test
     void 자동차의_이름_글자수_5자_이하_성공() {
-
         String carName = "car1";
 
         assertDoesNotThrow(() -> Validator.validateNameLength(carName));
+    }
+
+    @Test
+    void 시도횟수는_숫자인지_검증하는_로직_실패() {
+        String trial = "a";
+
+        assertThrows(IllegalArgumentException.class, () -> Validator.isNumber(trial));
+    }
+
+    @Test
+    void 시도횟수는_숫자인지_검증하는_로직_성공() {
+        String trial = "5";
+
+        assertDoesNotThrow(() -> Validator.isNumber(trial));
     }
 }
