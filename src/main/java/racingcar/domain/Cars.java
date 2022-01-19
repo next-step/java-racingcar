2b00c85583c8897773dd908541c4a6887e8de15c
smc5236 <smc5236@gmail.com>
2022-01-19 21:21:36 +0900

    🔥 feat(racingCar): 일급 컬렉션 내의 drive 함수 구현
    - 난수값을 통해 각 Car객체의 step을 증가시킬지를 결정

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -2,7 +2,6 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Cars {
@@ -14,5 +13,13 @@ public class Cars {
 
     public static Cars of() {
         return new Cars();
+    }
+
+    public void drive() {
+        IntStream.range(0, cars.size())
+                .forEach(idx -> {
+                    if (Racing.stopOrMoveForward())
+                        cars.get(idx).moveForward();
+                });
     }
 }
