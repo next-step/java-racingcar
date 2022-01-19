660f1aba80a5e52c7eb0a0768d7243635be84be0
Hoya-kim <1203play@gmail.com>
2022-01-19 21:40:58 +0900

    test: RacingCar  현재 위치 출력 테스트 코드 작성

--- src/test/java/racinggame/domain/RacingCarTest.java
+++ src/test/java/racinggame/domain/RacingCarTest.java
@@ -3,24 +3,50 @@ package racinggame.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 
 class RacingCarTest {
 
     @Test
     void 자동차_객체_생성() {
-        RacingCar car1 = new RacingCar("car1");
+        RacingCar car = new RacingCar("car");
 
-        assertThat(car1.getCarName()).isEqualTo("car1");
-        assertThat(car1.getPosition()).isEqualTo(0);
+        assertThat(car.getCarName()).isEqualTo("car");
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @Test
     void 한_칸_전진() {
-        RacingCar car1 = new RacingCar("car1");
+        RacingCar car = new RacingCar("car");
 
-        car1.forward();
+        car.forward();
 
-        assertThat(car1.getPosition()).isEqualTo(1);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 자동차_이름과_위치출력() {
+        RacingCar car = new RacingCar("car");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        car.printCurrPosition();
+
+        assertEquals("car  : ", out.toString());
+    }
+
+    @Test
+    void 자동차_이름과_위치출력_한칸_전진() {
+        RacingCar car = new RacingCar("car");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        car.forward();
+        car.printCurrPosition();
+        assertEquals("car  : -", out.toString());
     }
 }
