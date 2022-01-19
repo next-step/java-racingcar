b587db72f1d5ccb0d07402f566a6a871f0422d61
smc5236 <smc5236@gmail.com>
2022-01-19 17:49:02 +0900

    feat: 사용자 입력에 따라 계산기 종료 여부 구현
    - 1로 입력받으면 계속 작업을 수행하고 그 외 숫자는 작업 종료

--- src/main/java/stringcalculator/domain/CalculatorInit.java
+++ src/main/java/stringcalculator/domain/CalculatorInit.java
@@ -7,6 +7,8 @@ import java.io.*;
 public class CalculatorInit {
 
     private static final String repeatPhrase = "계속하려면 1번을 눌러주세요.";
+    private static final int NUMBER_ONE = 1;
+    private static int judgeContinue = 0;
 
     private void continueCalculator() {
 
@@ -23,6 +25,26 @@ public class CalculatorInit {
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+    }
+
+    private boolean repeatCalculator(){
+
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
+        try{
+            log.write(repeatPhrase+'\n');
+            log.flush();
+            judgeContinue = Integer.parseInt(bufferedReader.readLine());
+
+        }catch(IOException exception){
+            exception.printStackTrace();
+        }
+
+        if(judgeContinue == NUMBER_ONE) {
+            return true;
+        }
+        return false;
     }
 
     public void start() {
