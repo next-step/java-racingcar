a9ebbfd2eeb82ced44d3b9d15f97ddd92075ff3f
hyojeong <qkrgywjd456@naver.com>
2022-01-19 18:38:22 +0900

    feat: create validate to playerName

--- src/main/java/racinggame/domain/GameInit.java
+++ src/main/java/racinggame/domain/GameInit.java
@@ -6,12 +6,36 @@ public class GameInit {
 
 
     public String[] inputPlayerName(){
-        Scanner scanner = new Scanner(System.in);
-        String playerNames = scanner.nextLine();
-        String[] playerNameList = playerNames.split(",");
+        String playerNames="";
+        String[] playerNameList;
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            playerNames = scanner.nextLine();
+            playerNameList = playerNames.split(",");
+            boolean isValidate = validationStringLength(playerNameList);
+            if(isValidate==true){
+                break;
+            }
+        }
+
         return playerNameList;
     }
 
+    public int inputTryNumber(){
+        Scanner scanner = new Scanner(System.in);
+        int tryNumber = scanner.nextInt();
+        return tryNumber;
+    }
 
+    private boolean validationStringLength(String[] playerNameList ){
+        boolean isValidate=true;
+        for (String playerName: playerNameList) {
+            if(playerName.length()>5){
+                System.out.println("자동차의 이름은 5자를 초과할 수 없습니다.");
+                isValidate=false;
+            }
+        }
+        return isValidate;
+    }
 
 }
