5fa977267ae5a1d1f933747e278734db81436465
Gal2o <hunmin8788@gmail.com>
2022-01-19 21:36:33 +0900

    feat: 매직넘버 상수를 위한 Constant class 추가

---
+++ src/main/java/calculator/Constant.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class Constant {
+    public static final char ADD = '+';
+    public static final char SUB = '-';
+    public static final char MULTIPLY = '*';
+    public static final char DIVIDE = '/';
+}
