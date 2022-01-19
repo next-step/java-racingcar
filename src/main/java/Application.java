79de887e81d1aafc312c7f3b631c9841b4e0c218
parkminsang <minsang0850@naver.com>
2022-01-19 17:00:19 +0900

    feat: Create IOclass and print function

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,13 @@
 import calculatorFunction.Calculator;
 import calculatorFunction.GetInput;
+import racingcar.IOclass;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("문자열 계산기를 사용하시려면 1, 자동차 경주를 사용하시려면 2를 눌러주세요.");
-        int flag=1;
+        int flag=sc.nextInt();
         if(flag==1) {
             System.out.println("수식을 입력하시오: ");
             String str = GetInput.inputString();
@@ -14,7 +17,7 @@ public class Application {
             calculator.calculate(stringArray);
         }
         if(flag==2){
-
+            IOclass.getName();
         }
     }
 }
---
+++ src/main/java/racingcar/IOclass.java
@@ -0,0 +1,7 @@
+package racingcar;
+
+public class IOclass {
+    public static void getName(){
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+}
