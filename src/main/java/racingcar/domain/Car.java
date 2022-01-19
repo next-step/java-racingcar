d5f6c6db725293a609f654a01a812b2602f6ee37
smc5236 <smc5236@gmail.com>
2022-01-19 18:32:12 +0900

    🔥 feat(racingCar): 자동차 클래스 기본 골격 구현
    - 자동차의 이름을 저장할 name
    - 자동차의 상태를 저장할 step

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,16 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String name;
+    private int step;
+
+    private Car(final String name, final int step) {
+        this.name = name;
+        this.step = 0;
+    }
+
+    public static Car of(final String name, final int step) {
+        return new Car(name, step);
+    }
+}
