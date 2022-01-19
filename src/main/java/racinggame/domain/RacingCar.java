d5c66191b35de31f165fb2079656913bde7d5f9a
Hoya-kim <1203play@gmail.com>
2022-01-19 21:02:02 +0900

    feat: RacingCar 클래스 생성
    - 테스트용 getter 생성
    - getCarName, getPosition

--- src/main/java/racinggame/domain/RacingCar.java
+++ src/main/java/racinggame/domain/RacingCar.java
@@ -2,4 +2,20 @@ package racinggame.domain;
 
 public class RacingCar {
 
+    private String carName;
+    private int carPosition = 0;
+
+    private RacingCar() {}
+
+    public RacingCar(String carName) {
+        this.carName = carName;
+    }
+
+    public String getCarName() {
+        return this.carName;
+    }
+
+    public int getPosition() {
+        return this.carPosition;
+    }
 }
