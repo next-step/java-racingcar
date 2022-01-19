03c108296e3adf53bed11004172cc06e12e009b1
Hoya-kim <1203play@gmail.com>
2022-01-19 18:36:43 +0900

    feat: 자동차 이름 글자 수 검증 로직 구현

--- src/main/java/racinggame/domain/Validator.java
+++ src/main/java/racinggame/domain/Validator.java
@@ -7,4 +7,10 @@ public class Validator {
             throw new IllegalArgumentException("참가자는 2명 이상입니다.");
         }
     }
+
+    public static void validateNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름의 글자 수는 5자 이하입니다.");
+        }
+    }
 }
