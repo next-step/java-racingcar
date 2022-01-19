f41faf077c20e59f6af91a7fd2de96bf64a6fbb5
hyojeong <qkrgywjd456@naver.com>
2022-01-19 18:28:04 +0900

    refactor: rename function to getPlayerName

--- src/main/java/racinggame/domain/GameInit.java
+++ src/main/java/racinggame/domain/GameInit.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class GameInit {
 
-    public String[] getPlayerName(){
+
+    public String[] inputPlayerName(){
         Scanner scanner = new Scanner(System.in);
         String playerNames = scanner.nextLine();
         String[] playerNameList = playerNames.split(",");
         return playerNameList;
     }
+
+
 
 }
