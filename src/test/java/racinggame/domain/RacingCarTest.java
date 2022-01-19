597bf1e4febba383b30b32858ee1efccb9a7d7aa
Hoya-kim <1203play@gmail.com>
2022-01-19 21:01:06 +0900

    test: 자동차 객체 생성 검증 테스트코드 추가
    - RacingCar 객체 생성
    - name, position 값 일치 확인

--- src/test/java/racinggame/domain/RacingCarTest.java
+++ src/test/java/racinggame/domain/RacingCarTest.java
@@ -1,7 +1,17 @@
 package racinggame.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class RacingCarTest {
 
+    @Test
+    void 자동차_객체_생성() {
+        RacingCar car1 = new RacingCar("car1");
+
+        assertThat(car1.getCarName()).isEqualTo("car1");
+        assertThat(car1.getPosition()).isEqualTo(0);
+    }
 }
