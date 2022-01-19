26badd3f6bd72804da6768d133ae2509ff743180
kimtaejun97 <kim_taejun@naver.com>
2022-01-19 19:02:45 +0900

    feat: getCarNames 기능 구현
    - 경주할 자동차 목록 입력
    - splitNameByComma(): ','를 기준으로 이름 분할
    - validateNameLengthFromCarNames(): 자동차 이름 길이 검증 실행.

--- src/main/java/racinggame/domain/Input.java
+++ src/main/java/racinggame/domain/Input.java
@@ -1,5 +1,33 @@
 package racinggame.domain;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Input {
 
+    private Input() {}
+
+    public static String[] getCarNames() throws IOException {
+        System.out.println("경주할 자동차 이름을 ','로 구분해서 입력하세요.(2대 이상)");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String inputCarNames = br.readLine().trim();
+        String[] carNames = splitNameByComma(inputCarNames);
+
+        Validator.validatePossibleToStart(carNames);
+        validateNameLengthFromCarNames(carNames);
+
+        return carNames;
+    }
+
+    private static String[] splitNameByComma(String inputCarNames) {
+        return inputCarNames.split(",");
+    }
+
+    private static void validateNameLengthFromCarNames(String[] carNames) {
+        for (String carName : carNames) {
+            Validator.validateNameLength(carName);
+        }
+    }
 }
