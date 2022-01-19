46b1a90c95f5249e9b49deb92bc11c9779b71549
Hoya-kim <1203play@gmail.com>
2022-01-19 18:28:00 +0900

    test: 참가자 예외 사항 테스트 코드 추가
    - 게임의 참가자는 두 명 이상이어야 한다.

--- src/test/java/racinggame/domain/ValidatorTest.java
+++ src/test/java/racinggame/domain/ValidatorTest.java
@@ -1,7 +1,24 @@
 package racinggame.domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
 
+    @Test
+    void 참여자는_두_명_이상이다_실패() {
+
+        String[] carNames = "car1".split(",");
+
+        assertThrows(IllegalArgumentException.class, () ->Validator.validatePossibleToStart(carNames));
+    }
+
+    @Test
+    void 참여자는_두_명_이상이다_성공() {
+
+        String[] carNames = "car1,car2".split(",");
+
+        assertDoesNotThrow(() ->Validator.validatePossibleToStart(carNames));
+    }
 }
