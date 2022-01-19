1924c14ab42d14acb22ae74e6350798ec82dd1fe
hyeonho8453 <lhh5863@dgu.ac.kr>
2022-01-19 18:38:33 +0900

    test(Round) : test Round
    - 라운드 생성 기능 테스트

---
+++ src/test/java/racingcar/domain/RoundTest.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RoundTest {
+
+    @Test
+    void 라운드_생성_테스트 () {
+        Round round = new Round("12");
+        assertThat(round.getRound()).isEqualTo(12);
+    }
+
+    @Test
+    void 라운드_생성_예외_테스트(){
+        assertThatThrownBy(() -> new Round("pan"))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Round("-2"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
