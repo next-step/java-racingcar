a9fd05acddc332831b21164b78c32444b09d53ba
kimtaejun97 <kim_taejun@naver.com>
2022-01-19 18:59:59 +0900

    test: Input.getCarNames 정상입력 테스트
    정상 입력이 들어왔을 때, 이름 목록을 잘 반환하는지 테스트

--- src/test/java/racinggame/domain/InputTest.java
+++ src/test/java/racinggame/domain/InputTest.java
@@ -1,7 +1,23 @@
 package racinggame.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.jupiter.api.Test;
 
 class InputTest {
 
+    @Test
+    void 정상_입력() throws IOException {
+        InputStream inputStream = new ByteArrayInputStream("car1,car2".getBytes());
+        System.setIn(inputStream);
+
+        String[] carNames = Input.getCarNames();
+
+        assertThat(carNames[0]).isEqualTo("car1");
+        assertThat(carNames[1]).isEqualTo("car2");
+    }
 }
