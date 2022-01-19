34db3180574235f8fdb9413c4461c9f310bce065
0JUUU <clleo97@naver.com>
2022-01-19 17:02:12 +0900

    feat: 계산기 실행 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,25 +1,11 @@
 package calculator;
 
-import calculator.domain.Expression;
-import calculator.domain.Validator;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import calculator.domain.Calculator;
 
 public class Application {
 
-    public static void main(String[] args){
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        String expression = br.readLine();
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-
-        while (!Validator.checkValidate(str)) {
-            System.out.println(str);
-            str = scanner.nextLine();
-        }
-        // d
-        // 유효성 검사 -> 통과하면 Expression 에 넣어서 계산 처리
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.startCalculate();
     }
-
 }
