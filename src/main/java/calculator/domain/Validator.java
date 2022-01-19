b5f3a5cd5e2977dcc18aa1c2fc7de6cfaf55469f
0JUUU <clleo97@naver.com>
2022-01-19 16:30:20 +0900

    refactor: 입력값 유효검사 return문 구조 변경

--- src/main/java/calculator/domain/Validator.java
+++ src/main/java/calculator/domain/Validator.java
@@ -1,25 +1,9 @@
 package calculator.domain;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Validator {
 
     public static boolean checkValidate(String expression) {
-
-        String pattern ="^\\d+(\\s*[-+*/]\\s*\\d+)+$";
-
-        if(!expression.matches(pattern))
-            return false;
-
-//        Pattern patternDivideZero = Pattern.compile("[/]\\s*[0]\\s*[-+*/]*");
-//        Matcher matcher = patternDivideZero.matcher(expression);
-//
-//        if(matcher.find()) {
-//            return false;
-//        }
-
-        return true;
-
+        String pattern = "^\\d+(\\s*[-+*/]\\s*\\d+)+$";
+        return expression.matches(pattern);
     }
 }
