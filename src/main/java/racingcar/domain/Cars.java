c73238f486f8cd9abe80da9ecf41f47d20763dfb
smc5236 <smc5236@gmail.com>
2022-01-19 21:20:20 +0900

    🔥 feat(racingCar): Cars 일급 컬렉션 생성

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,2 +1,18 @@
-package racingcar.domain;public class Cars {
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Cars {
+
+    private final List<Car> cars = new ArrayList<>();
+
+    private Cars() {
+    }
+
+    public static Cars of() {
+        return new Cars();
+    }
 }
