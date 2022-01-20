f7b452321ff1a214d05a6d2ae5863b81f19e88e4
kimtaejun97 <kim_taejun@naver.com>
2022-01-20 17:00:56 +0900

    test: RacingCar 생성자 테스트
    생성자로 전달받은 RacingCar 리스트와 trial 값이 잘 설정되는지 테스트

--- src/test/java/racinggame/domain/RacingGameTest.java
+++ src/test/java/racinggame/domain/RacingGameTest.java
@@ -1,7 +1,23 @@
 package racinggame.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class RacingGameTest {
 
+    @Test
+    void RacingCar_리스트를_받아_RacingGame_객체_생성(){
+        String[] carNames = {"car1", "car2"};
+        List<RacingCar> racingCars = RacingCar.createRacingCarFromCarNames(carNames);
+        int trial = 5;
+
+        RacingGame racingGame = new RacingGame(racingCars, 5);
+
+        assertTrue(racingGame.getRacingCars().contains(racingCars.get(0)));
+        assertTrue(racingGame.getRacingCars().contains(racingCars.get(1)));
+        assertThat(racingGame.getTrial()).isEqualTo(trial);
+    }
 }
