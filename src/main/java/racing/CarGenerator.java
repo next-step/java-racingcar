24b0f7078297a1fa71576056ead559b8e902ccf3
Hae-Riri <ehl3288@naver.com>
2022-01-20 17:20:33 +0900

    feat: CarGenerator 클래스 생성
    - 자동차 이름 입력 받는 기능 추가
    - 자동차 리스트 생성 후 반환

---
+++ src/main/java/racing/CarGenerator.java
@@ -0,0 +1,24 @@
+package racing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarGenerator {
+    public List<Car> createCars() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String[] names = br.readLine().split(",");
+
+        List<Car> cars = new ArrayList<>();
+        for(int i = 0; i < names.length; i++) {
+            cars.add(new Car(names[i]));
+        }
+
+        return cars;
+    }
+}
