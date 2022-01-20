f67e654ccb38899fdca754b9892d78aee1dd30ad
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-20 16:50:34 +0900

    refactor(racingcar): 일급 컬렉션 기능을 Racing 클래스로 통합
    - Cars 클래스 삭제

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -1,10 +1,41 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Racing {
 
     private static int generateRandomNumber() {
         return (int) (Math.random() * 10);
     }
+
+    private final List<Car> cars = new ArrayList<>();
+
+    private Racing() {
+    }
+
+    public static Racing of() {
+        return new Racing();
+    }
+
+    public void drive() {
+        IntStream.range(0, cars.size())
+            .forEach(idx -> {
+                if (Racing.stopOrMoveForward()) {
+                    cars.get(idx).moveForward();
+                }
+            });
+    }
+
+    public void registerCar(final Car car) {
+        cars.add(car);
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
 
     public static boolean stopOrMoveForward() {
         if (generateRandomNumber() >= 4) {
