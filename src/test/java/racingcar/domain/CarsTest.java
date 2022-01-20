95adf5beae6ee0cc33181c83df82cf215cb0db8f
hyeonho8453 <lhh5863@dgu.ac.kr>
2022-01-20 17:35:12 +0900

    test(Cars): Testing Cars
    - Cars 테스트 작성

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,30 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CarsTest {
+
+    @Test
+    void 참가자_생성_테스트() {
+        Cars cars = new Cars(new String[]{"pobi"}, new FixedEngine(true));
+        assertThat(cars.cars()
+            .get(0)
+            .name()).isEqualTo(new Name("pobi"));
+    }
+
+    @Test
+    void 참가자_생성_예외_테스트() {
+        assertThatThrownBy(() -> new Cars(new String[]{"jasons"}, new FixedEngine(false)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 참가자_움직임_테스트() {
+        Cars cars = new Cars(new String[]{"pobi"}, new FixedEngine(true));
+        cars.moveAll();
+        assertThat(cars.cars().get(0).position()).isEqualTo(1);
+    }
+}
