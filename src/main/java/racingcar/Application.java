0b44a02323e749b491b5152bb8a3e66d411adaa9
jihyunhillpark <girl9505@naver.com>
2022-01-20 19:07:30 +0900

    feat: 자동차 게임 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,21 @@
 package racingcar;
 
+
+import java.util.List;
+import racingcar.service.GameLauncher;
+import racingcar.ui.RacingCarInput;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        List<String> carNameList = RacingCarInput.getCarName();
+        int raceCount = RacingCarInput.getRaceCount();
+        GameLauncher gameLauncher = new GameLauncher(carNameList);
+        while (0 < raceCount) {
+            gameLauncher.moveForwardAll();
+            gameLauncher.printRacing();
+            raceCount--;
+        }
+        gameLauncher.printWinner();
     }
 }
