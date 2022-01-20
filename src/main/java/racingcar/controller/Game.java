964c3fedef67a861697cf47f94c91c1404cc50c5
Ji In Lee <loveted0225@naver.com>
2022-01-20 16:56:32 +0900

    feat(Game): 자동차 게임 진행 구현

---
+++ src/main/java/racingcar/controller/Game.java
@@ -0,0 +1,45 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import racingcar.model.Car;
+import racingcar.model.User;
+import racingcar.util.Message;
+import racingcar.view.OutputView;
+
+public class Game {
+    public void play(){
+        ArrayList<String> carNameList ;
+        String [] carNames; //입력 받은 자동차 이름들
+        int tryCount;
+        ArrayList<Car> carList = new ArrayList<>(); //경주를 하는 car 모음
+        //ArrayList<Car> winnerList = new ArrayList<>();
+        User user = new User();
+
+        System.out.println(Message.INPUT_GUIDE_MESSAGE);
+        carNames=user.getCarName();
+        System.out.println(Message.ASK_TRY_COUNT);
+        tryCount=user.getTryCount();
+
+        for (int i=0; i<carNames.length; i++){
+            carList.add(new Car(carNames[i])); //car 객체 생성
+        }
+        System.out.println(Message.GAME_RESULT_MESSAGE);
+        for (int i=0; i<tryCount; i++){
+            System.out.println(tryCount+1+"회");
+            moveForwardByCount(carList);
+        }
+
+
+    }
+
+    public void moveForwardByCount(ArrayList<Car> carList){
+        OutputView outputView = new OutputView();
+        for (Car car:carList){
+            car.moveForward();
+            outputView.printResult(car.getCarName(),car.getPosition());
+        }
+        System.out.println();
+    }
+
+}
