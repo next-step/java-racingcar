7f8d1bce3557444f68ecc376aa886684874f978b
kimtaejun97 <kim_taejun@naver.com>
2022-01-20 17:01:46 +0900

    feat: RacingCar Class 생성자 구현
    RacingCar List와 시도횟수 trial을 받는 생성자 구현

--- src/main/java/racinggame/domain/RacingGame.java
+++ src/main/java/racinggame/domain/RacingGame.java
@@ -1,5 +1,24 @@
 package racinggame.domain;
 
+import java.util.List;
+
 public class RacingGame {
 
+    private List<RacingCar> racingCars;
+    private int trial;
+
+    public RacingGame() {}
+
+    public RacingGame(List<RacingCar> racingCars, int trial) {
+        this.racingCars = racingCars;
+        this.trial = trial;
+    }
+
+    public List<RacingCar> getRacingCars() {
+        return this.racingCars;
+    }
+
+    public int getTrial(){
+        return this.trial;
+    }
 }
