4b3ff4581711a7b15479706ae7b0865d9c0acccf
hyojeong <qkrgywjd456@naver.com>
2022-01-20 16:42:45 +0900

    feat: create function moving cars

--- src/main/java/racinggame/domain/PlayRacingGame.java
+++ src/main/java/racinggame/domain/PlayRacingGame.java
@@ -1,2 +1,26 @@
-package racinggame.domain;public class PlayRacingGame {
+package racinggame.domain;
+
+import java.util.List;
+import java.util.Random;
+
+public class PlayRacingGame {
+
+    public int makeRandomNumber(){
+        Random random = new Random();
+        int randomValue=random.nextInt(9);
+        return randomValue;
+    }
+
+    public void move(List<Car> cars){
+        for (Car car:cars) {
+            int randomNumber = makeRandomNumber();
+            if(randomNumber>=4){
+                int location = car.getLocation();
+                location++;
+                car.setLocation(location);
+            }
+        }
+
+    }
+
 }
