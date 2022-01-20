2708384968ca6959d19f14f6b7bf3994df00507b
hyojeong <qkrgywjd456@naver.com>
2022-01-20 17:01:14 +0900

    feat: create Application.class
    main is gameLogic

---
+++ src/main/java/racinggame/Application.java
@@ -0,0 +1,32 @@
+package racinggame;
+
+import java.util.List;
+import racinggame.domain.Car;
+import racinggame.domain.Cars;
+import racinggame.domain.GameInit;
+import racinggame.domain.PlayRacingGame;
+
+public class Application {
+
+    public static void main(String[] args) {
+        //게임 초기화
+        GameInit gameInit = new GameInit();
+        String[] carNames = gameInit.inputPlayerName();
+        int playGameNumber = gameInit.inputTryNumber();
+
+        Cars cars = new Cars();
+        List<Car> carList = cars.createCarList(carNames);
+        //게임 시작
+        PlayRacingGame playRacingGame = new PlayRacingGame();
+        for(;playGameNumber>0;playGameNumber--){
+            playRacingGame.move(carList);
+            playRacingGame.printCarsLocation(carList);
+
+        }
+        List<String> winner = playRacingGame.findWinner(carList);
+        playRacingGame.printWinner(winner);
+    }
+
+
+
+}
