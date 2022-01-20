1d88d61165fa260b3f5dd7a3d2041750d167c0d9
Dami Kim <dami.k.dev@gmail.com>
2022-01-20 20:20:41 +0900

    feat: main 함수 내 console 클래스 생성자 생성 기능 추가

--- src/main/java/racingcar/ApplicationRacingCar.java
+++ src/main/java/racingcar/ApplicationRacingCar.java
@@ -1,5 +1,11 @@
 package racingcar;
 
+import racingcar.Console;
+import java.util.List;
+
 public class ApplicationRacingCar {
+    public static void main(String[] args) {
+        Console console = new Console();
+    }
 
 }
