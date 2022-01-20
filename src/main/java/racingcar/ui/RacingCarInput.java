b33d94a0e6516dd77f1d250331b04f06e8b7d0ab
jihyunhillpark <girl9505@naver.com>
2022-01-20 18:45:37 +0900

    feat: 사용자 입력을 입력받는 기능 구현
    1. 경주할 자동차 이름 입력받는 기능
    2. 사용자가 경주를 몇 번 시도할지 구현

--- src/main/java/racingcar/ui/RacingCarInput.java
+++ src/main/java/racingcar/ui/RacingCarInput.java
@@ -1,5 +1,33 @@
 package racingcar.ui;
 
+import calculator.util.FormulaUtil;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
 public class RacingCarInput {
 
+    public static List<String> getCarName() {
+        String input = "";
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)");
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            input = bufferedReader.readLine();
+        } catch (IllegalArgumentException | IOException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
+        return Arrays.asList(input.split(","));
+    }
+
+    public static int getRaceCount() {
+        int input = 0;
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            input = Integer.parseInt(bufferedReader.readLine());
+        } catch (IllegalArgumentException | IOException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
+        return input;
+    }
 }
