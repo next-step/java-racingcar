34fbfd754ee39ee3a8f2180661c91402eb45c5f5
hyeonho8453 <lhh5863@dgu.ac.kr>
2022-01-20 17:13:55 +0900

    refactor(Name): Extract exception message in validateName
    - 매직넘버 추출

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 public class Name {
 
     private static final int NAME_LIMIT_LENGTH = 5;
+    private static final String NAME_VALIDATION_MESSAGE = "[ERROR] 이름은 5 글자 이하입니다.";
 
     private String name;
 
@@ -17,7 +18,7 @@ public class Name {
 
     private void validateName(String name) {
         if (name == null || name.isEmpty() || name.length() > NAME_LIMIT_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NAME_VALIDATION_MESSAGE);
         }
     }
 }
