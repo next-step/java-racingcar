0f4aface02a8e024ea8c23660235504af982d209
Dami Kim <dami.k.dev@gmail.com>
2022-01-20 20:46:28 +0900

    feat: main 함수에서 racingCarGame 클래스를 생성하는 생성자 추가

--- src/main/java/racingcar/ApplicationRacingCar.java
+++ src/main/java/racingcar/ApplicationRacingCar.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class ApplicationRacingCar {
     public static void main(String[] args) {
         Console console = new Console();
+
+        RacingCarGame racingCarGame = new RacingCarGame(console.getTryCount(),
+            console.getCarNames());
     }
 
 }
