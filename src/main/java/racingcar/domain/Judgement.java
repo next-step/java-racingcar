c9d5c135e02bbda7d36114dbdd4dc9631f207eb6
smc5236 <smc5236@gmail.com>
2022-01-20 21:25:10 +0900

    🔥 refactor(racingCar): Judgement 클래스 매직 넘버 제거 및 StringBuilder 사용
    - StringBuilder를 인스턴스 필드로 변경하고 judge 메소드에서도 사용
    - max가 0이면 아무도 전진을 못했으므로 우승자는 없음

--- src/main/java/racingcar/domain/Judgement.java
+++ src/main/java/racingcar/domain/Judgement.java
@@ -3,20 +3,35 @@ package racingcar.domain;
 import java.util.List;
 
 import static racingcar.common.SystemMessage.FINAL_WINNERS_INFO;
+import static racingcar.common.SystemMessage.NO_WINNER_INFO;
 
 public class Judgement {
 
     private static final int ZERO = 0;
     private static final String STEP = "-";
+    private static final String COMMA = ",";
+
+    private StringBuilder stringBuilder = new StringBuilder();
 
     public void judge(final List<Car> cars) {
+        stringBuilder.setLength(ZERO);
         int max = maxDriveLength(cars);
+
+        if (max == 0) {
+            System.out.println(NO_WINNER_INFO);
+            return;
+        }
+
+        stringBuilder.append(FINAL_WINNERS_INFO);
 
         for (Car car : cars) {
             if (max == car.getStep()) {
-                System.out.print(FINAL_WINNERS_INFO + car.getName());
+                stringBuilder.append(car.getName());
+                stringBuilder.append(COMMA);
             }
         }
+        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(COMMA));
+        System.out.println(stringBuilder);
     }
 
     private int maxDriveLength(List<Car> cars) {
@@ -27,7 +42,6 @@ public class Judgement {
     }
 
     private String carPosition(final Car car) {
-        StringBuilder stringBuilder = new StringBuilder();
         for (int step = ZERO; step < car.getStep(); step++) {
             stringBuilder.append(STEP);
         }
