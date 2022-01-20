c8b20f301ba159f01e34be047ba19771149b57cf
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-21 00:38:14 +0900

    feat(unit-test): 난수가 4 이상이어야 앞으로 전진하는 단위테스트 구현

--- src/test/java/racingcar/domain/car/MoveTest.java
+++ src/test/java/racingcar/domain/car/MoveTest.java
@@ -1,10 +1,11 @@
 package racingcar.domain.car;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.random.NoMoveGen;
+import racingcar.domain.random.RandomFactory;
+import racingcar.domain.random.MoveGen;
 
 @DisplayName("자동차는 난수 값에 의해 정확하게 제어된다.")
 class MoveTest {
@@ -12,25 +13,27 @@ class MoveTest {
     @Test
     void move() {
         //given
-        Car oldCar = Car.of("jason", 5);
+        final int BOUND = 4;
+        RandomFactory randomFactory = new RandomFactory();
 
         //when
-        Car newCar = Move.isSatisfiedMoveCondition(oldCar, 4);
+        int random = randomFactory.getRandom(new MoveGen());
 
         //then
-        Assertions.assertThat(newCar).isNotEqualTo(oldCar);
+        Assertions.assertThat(random >= BOUND).isTrue();
     }
 
     @DisplayName("랜덤 값 4 미만은 전지하지 않는다.")
     @Test
     void notMove() {
         //given
-        Car oldCar = Car.of("jason", 5);
+        final int BOUND = 4;
+        RandomFactory randomFactory = new RandomFactory();
 
         //when
-        Car newCar = Move.isSatisfiedMoveCondition(oldCar, 3);
+        int random = randomFactory.getRandom(new NoMoveGen());
 
         //then
-        Assertions.assertThat(newCar).isEqualTo(oldCar);
+        Assertions.assertThat(random >= BOUND).isFalse();
     }
 }
