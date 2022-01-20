88244a9e5a2e8fc4e28e3ff9aff81a23d9de58d8
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-20 18:27:52 +0900

    feat(racingcar): 자동차를 등록하기 위한 helper 클래스 작성

---
+++ src/main/java/racingcar/domain/Helper.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+import java.util.List;
+import racingcar.common.UserInput;
+
+public class Helper {
+    public void runner(final List<String> input, final Racing racing){
+
+        for (String carName: input) {
+            racing.registerCar(Car.of(carName));
+        }
+    }
+}
