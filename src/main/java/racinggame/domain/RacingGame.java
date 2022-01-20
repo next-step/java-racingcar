195291ba7409b9e3709ef2af18b963799445bb53
kimtaejun97 <kim_taejun@naver.com>
2022-01-20 18:24:13 +0900

    feat: start 중 progress() 구현
    racingCars 리스트를 순회하며 랜덤 숫자값을 받아. 조건에 맞으면 전진
    조건: 랜덤값이 4이상.

--- src/main/java/racinggame/domain/RacingGame.java
+++ src/main/java/racinggame/domain/RacingGame.java
@@ -7,15 +7,33 @@ public class RacingGame {
     private List<RacingCar> racingCars;
     private int trial;
 
-    public RacingGame() {
-    }
+    public RacingGame() {}
 
     public RacingGame(List<RacingCar> racingCars, int trial) {
         this.racingCars = racingCars;
         this.trial = trial;
     }
 
-    private int getRandomValue() {
+    public void start() {
+        for (int i = 0; i < trial; i++) {
+            progress();
+
+            //TODO 계속 진행 묻기
+            System.out.println("그만 하시려면 q, 계속하시려면 아무키나 입력해 주세요.");
+        }
+    }
+
+    private void progress() {
+        racingCars.forEach(car -> {
+            int randomValue = getRandomValue();
+            if (canForward(randomValue)) {
+                car.forward();
+            }
+            car.printCurrPosition();
+        });
+    }
+
+    public int getRandomValue() {
         int random = (int) Math.floor(Math.random() * (9 - 0 + 1));
         return random;
     }
