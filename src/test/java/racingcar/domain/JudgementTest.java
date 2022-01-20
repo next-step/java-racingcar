6c56bbff636add9ddf8acd4d6bd3dd9009b9092d
smc5236 <smc5236@gmail.com>
2022-01-20 22:42:18 +0900

    🔥 test(racingCar): 동점자 테스트

--- src/test/java/racingcar/domain/JudgementTest.java
+++ src/test/java/racingcar/domain/JudgementTest.java
@@ -1,16 +1,13 @@
 package racingcar.domain;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class JudgementTest {
     List<Car> cars = new ArrayList<>();
@@ -25,7 +22,14 @@ class JudgementTest {
     @Test
     void winnerTest() {
         Judgement judgement = Judgement.of();
-        Assertions.assertThat(judgement.judge(cars)).isEqualTo("최종 우승자: java");
+        assertThat(judgement.judge(cars)).isEqualTo("최종 우승자: java");
+    }
+
+    @Test
+    void tieBreakerTest() {
+        Judgement judgement = Judgement.of();
+        cars.add(new Car("jUnit", 4));
+        assertThat(judgement.judge(cars)).isEqualTo("최종 우승자: java,jUnit");
     }
 
 }
