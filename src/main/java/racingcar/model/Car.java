9c8d41b09a5286dba8d413b6c3e866acc2e504a9
Ji In Lee <loveted0225@naver.com>
2022-01-20 16:53:11 +0900

    feat(Car): 랜덤값에 따라 car 이동

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -10,6 +10,16 @@ public class Car {
         this.position="";
     }
 
+    public void moveForward(){
+        RandomGenerator randomGenerator = new RandomGenerator();
+        int randomNumber=randomGenerator.generateRandomNumber();
+        //System.out.println(randomNumber);
+        if(randomNumber>=4){
+            this.position+="-";
+        }
+
+    }
+
     public String getCarName() {
         return carName;
     }
