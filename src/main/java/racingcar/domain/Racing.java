c9a0964f95b65d3162015ea7e6f765127cf5e9de
smc5236 <smc5236@gmail.com>
2022-01-20 21:02:24 +0900

    🔥 refactor(racingCar): Racing 클래스 매직 넘버 제거 및 코드 스타일 적용

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -6,8 +6,11 @@ import java.util.stream.IntStream;
 
 public class Racing {
 
+    private static final int RANGE_OF_RANDOM_NUMBER = 10;
+    private static final int STANDARD_NUMBER_OF_MOVE_FORWARD = 4;
+
     private static int generateRandomNumber() {
-        return (int) (Math.random() * 10);
+        return (int) (Math.random() * RANGE_OF_RANDOM_NUMBER);
     }
 
     private final List<Car> cars = new ArrayList<>();
@@ -21,11 +24,11 @@ public class Racing {
 
     public void drive() {
         IntStream.range(0, cars.size())
-            .forEach(idx -> {
-                if (Racing.stopOrMoveForward()) {
-                    cars.get(idx).moveForward();
-                }
-            });
+                .forEach(idx -> {
+                    if (Racing.stopOrMoveForward()) {
+                        cars.get(idx).moveForward();
+                    }
+                });
     }
 
     public void registerCar(final Car car) {
@@ -38,7 +41,7 @@ public class Racing {
 
 
     public static boolean stopOrMoveForward() {
-        if (generateRandomNumber() >= 4) {
+        if (generateRandomNumber() >= STANDARD_NUMBER_OF_MOVE_FORWARD) {
             return true;
         }
         return false;
