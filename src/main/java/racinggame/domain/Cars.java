a9673579220a8fa620364e952cc62900dc073591
hyojeong <qkrgywjd456@naver.com>
2022-01-20 16:34:10 +0900

    feat: create Cars to manage Car.class

---
+++ src/main/java/racinggame/domain/Cars.java
@@ -0,0 +1,19 @@
+package racinggame.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> cars=new ArrayList<>();
+
+    public List<Car> createCarList(String[]carNames ){
+        //Car들 모임
+        List<Car> cars=new ArrayList<>();
+        for(String carName:carNames){
+            cars.add(new Car(carName));
+        }
+        return cars;
+    }
+
+}
