f6dc89cdff3d926506af4a29ae4055ff3d87264f
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-20 17:05:27 +0900

    feat(input-validate): 사용자가 입력한 횟수 조건 판단 기능 구현
    아래와 같은 경우 False 및 재입력 요청
    - 빈 값("")
    - 숫자가 아닌 경우
    - 1 보다 작은 경우

--- src/main/java/racingcar/UserInput.java
+++ src/main/java/racingcar/UserInput.java
@@ -14,6 +14,7 @@ public class UserInput {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final String NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String TURN_MESSAGE = "시도할 횟수는 몇번인가요?";
+    private static final String ERROR_LOG = "[ERROR] ";
 
     public static Participants createCars() {
         String[] names = getInputValueWithMessage(NAME_MESSAGE).split(",");
@@ -26,13 +27,22 @@ public class UserInput {
                 cars.add(car);
             }
         } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_LOG + e.getMessage());
             return createCars();
         }
         return Participants.getInstance(cars);
     }
 
     public static int setTurn() {
-        return Integer.parseInt(getInputValueWithMessage(TURN_MESSAGE));
+        String turn;
+        try {
+            turn = getInputValueWithMessage(TURN_MESSAGE).trim();
+            validateTurn(turn);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_LOG + e.getMessage());
+            return setTurn();
+        }
+        return Integer.parseInt(turn);
     }
 
     public static String getInputValueWithMessage(String message) {
@@ -41,6 +51,7 @@ public class UserInput {
             String line = br.readLine().trim();
             return line;
         } catch (IOException e) {
+            System.out.println(ERROR_LOG + e.getMessage());
             return getInputValueWithMessage(message);
         }
     }
@@ -48,6 +59,16 @@ public class UserInput {
     private static void validateCarName(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름이 5자를 초과합니다.");
+        }
+    }
+
+    private static void validateTurn(String turn) {
+        if (turn.trim().equals("")) {
+            throw new IllegalArgumentException("횟수를 입력해주세요.");
+        } else if (!turn.matches("[+-]?\\d*(\\.\\d+)?")) {
+            throw new NumberFormatException("입력한 값은 숫자가 아닙니다.");
+        } else if (Integer.parseInt(turn) < 1) {
+            throw new IllegalArgumentException("시도할 횟수는 1 이상이어야 합니다.");
         }
     }
 }
