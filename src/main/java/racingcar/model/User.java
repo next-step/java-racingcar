485f5e552d44262a152b1ae667b8588294e4ac98
Ji In Lee <loveted0225@naver.com>
2022-01-20 16:50:27 +0900

    feat(User): 사용자 입력값 처리 기능 구현

---
+++ src/main/java/racingcar/model/User.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import racingcar.util.Message;
+
+public class User {
+    public String []  getCarName(){
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        return (userInput.split(","));
+    }
+    public int getTryCount(){
+        Scanner scanner = new Scanner(System.in);
+        int tryCount=scanner.nextInt();
+
+        return tryCount;
+    }
+}
