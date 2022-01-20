5070794f6c02fb2b2837ca97b532aebb41b29ec4
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-20 17:17:58 +0900

    feat(racingcar): 자동차 이름, 시도할 횟수를 입력받도록 구성
    - UserInput 클래스 작성
    - Application 클래스 작성, UserInput 호출

---
+++ src/main/java/racingcar/Application.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import racingcar.common.UserInput;
+
+public class Application {
+
+    public static void main(String[] args){
+        new UserInput();
+    }
+}
--- src/main/java/racingcar/common/UserInput.java
+++ src/main/java/racingcar/common/UserInput.java
@@ -1,5 +1,7 @@
 package racingcar.common;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import racingcar.common.exception.InputValidationException;
 
 import java.io.BufferedReader;
@@ -11,25 +13,52 @@ import java.util.stream.Collectors;
 
 public class UserInput {
 
+
     private static final String DELIMITER = ",";
     private List<String> splitUserInput;
-    private String userInput;
+    private String carNameInput;
+    private int racingTryCounter;
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public UserInput() {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        while (parsingCarName())
+            ;
+        while (parsingRacingTry())
+            ;
+    }
+
+    private boolean parsingCarName() {
         try {
-            this.userInput = bufferedReader.readLine();
-            splitUserInput = splitStr(userInput);
+            log.write("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+            log.flush();
+            this.carNameInput = bufferedReader.readLine();
+            splitUserInput = splitStr(carNameInput);
             validateLengthLimit(splitUserInput);
             validateDuplicateCar(splitUserInput);
-        } catch (IOException exception) {
+        } catch (IOException | InputValidationException exception) {
             exception.printStackTrace();
+            return true;
         }
+        return false;
+    }
+
+    private boolean parsingRacingTry() {
+        try {
+            log.write("시도할 횟수는 몇 회인가요?");
+            log.flush();
+            this.racingTryCounter = Integer.parseInt(bufferedReader.readLine());
+
+        } catch (IOException | InputValidationException exception) {
+            exception.printStackTrace();
+            return true;
+        }
+        return false;
     }
 
     private void validateLengthLimit(final List<String> splitUserInput) {
         boolean match = splitUserInput.stream()
-                .allMatch(input -> input.length() <= 5 && input.length() >= 1);
+            .allMatch(input -> input.length() <= 5 && input.length() >= 1);
 
         if (!match) {
             throw new InputValidationException("자동차 이름은 5글자를 넘을 수 없습니다.");
