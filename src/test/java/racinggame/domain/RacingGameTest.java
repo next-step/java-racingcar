7edca5fca168f5ff38841fc465b2acb44bb5f9c4
kimtaejun97 <kim_taejun@naver.com>
2022-01-20 17:27:31 +0900

    test: canForward 전진 가능한지 확인하는 기능 테스트
    int 값 하나를 받아 4이상이면 true, 그보다 작다면 false 반환

--- src/test/java/racinggame/domain/RacingGameTest.java
+++ src/test/java/racinggame/domain/RacingGameTest.java
@@ -3,13 +3,15 @@ package racinggame.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class RacingGameTest {
 
     @Test
-    void RacingCar_리스트를_받아_RacingGame_객체_생성(){
+    void RacingCar_리스트를_받아_RacingGame_객체_생성() {
         String[] carNames = {"car1", "car2"};
         List<RacingCar> racingCars = RacingCar.createRacingCarFromCarNames(carNames);
         int trial = 5;
@@ -19,5 +21,32 @@ class RacingGameTest {
         assertTrue(racingGame.getRacingCars().contains(racingCars.get(0)));
         assertTrue(racingGame.getRacingCars().contains(racingCars.get(1)));
         assertThat(racingGame.getTrial()).isEqualTo(trial);
+    }
+
+    @Test
+    void canForward_전진실패()
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        int randomValue = 1;
+
+        RacingGame racingGame = new RacingGame();
+        Method canForward = RacingGame.class.getDeclaredMethod("canForward", int.class);
+        canForward.setAccessible(true);
+        boolean result = (boolean) canForward.invoke(racingGame, randomValue);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void canForward_전진가능()
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        int randomValue = 5;
+
+        RacingGame racingGame = new RacingGame();
+        Method canForward = RacingGame.class.getDeclaredMethod("canForward", int.class);
+        canForward.setAccessible(true);
+        boolean result = (boolean) canForward.invoke(racingGame, randomValue);
+
+        assertTrue(result);
     }
 }
