ecd3b4d1fe7de1c8d3a08594b754860564d485d3
Dami Kim <dami.k.dev@gmail.com>
2022-01-20 20:45:38 +0900

    feat: 0부터 9사이의 무작위 값을 구하는 기능 추가

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,5 +1,33 @@
 package racingcar;
 
+import java.util.List;
+import java.util.Random;
+
 public class RacingCarGame {
 
+    private int tryCount;
+    private List<String> racingCarList;
+    private String[] printResult;
+
+    private static final Random RANDOM = new Random();
+    private final int MIN_MOVE = 0;
+    private final int MAX_MOVE = 9;
+    private final int MOVABLE = 4;
+
+    public RacingCarGame(int tryCount, List<String> racingCarList) {
+        this.tryCount = tryCount;
+        this.racingCarList = racingCarList;
+    }
+
+    private int getRandomMove() {
+        int move = MIN_MOVE + RANDOM.nextInt(MAX_MOVE - MIN_MOVE + 1);
+
+        checkMovable(move);
+
+        return move;
+    }
+
+    private boolean checkMovable(int move) {
+        return MOVABLE <= move;
+    }
 }
