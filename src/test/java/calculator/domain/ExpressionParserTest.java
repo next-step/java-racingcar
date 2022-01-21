5022fd95c5f22f697d7548690ea921bc9dd03b6c
kimtaejun97 <kim_taejun@naver.com>
2022-01-21 19:39:29 +0900

    test: 사용자 입력을 받아 Expression을 반환하는 parseExpression 기능 테스트
    - ExpressionParser.parseExpression()

--- src/test/java/calculator/domain/ExpressionParserTest.java
+++ src/test/java/calculator/domain/ExpressionParserTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 class ExpressionParserTest {
-  
+
+    @Test
+    void parseExpression_사용자_입력을_받아_Expression_반환() {
+
+        //given
+        String[] userInput = "1 + 2 * 3".split("\\s+");
+
+        // when
+        ExpressionParser parser = new ExpressionParser();
+        Expression expression = parser.parseExpression(userInput);
+
+        // then
+        assertThat(expression.getOperators().size()).isEqualTo(2);
+        assertThat(expression.getTerms().size()).isEqualTo(3);
+    }
 }
