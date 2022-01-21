5261e03bf2f5c0c76d8562c1d44114f7ab77cb44
kimtaejun97 <kim_taejun@naver.com>
2022-01-21 20:12:39 +0900

    test: 접근 지정자가 public -> private로 변경되어 리플렉션을 이용한 테스트로 변경.
    식의 계산을 Expression에서 Calculator로 옮기면서 접근 지정자 변경.

--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -2,31 +2,48 @@ package calculator.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
     @Test
-    void add() {
-        long result = Calculator.add(1, 3);
+    void add() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        Method add = Calculator.class.getDeclaredMethod("add", long.class, long.class);
+        add.setAccessible(true);
+        long result = (long)add.invoke(null,1,3);
+
         assertThat(result).isEqualTo(4);
     }
 
     @Test
-    void sub() {
-        long result = Calculator.subtract(3, 1);
+    void subtract() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        Method subtract = Calculator.class.getDeclaredMethod("subtract", long.class, long.class);
+        subtract.setAccessible(true);
+        long result = (long)subtract.invoke(new Calculator(), 3,1);
         assertThat(result).isEqualTo(2);
     }
 
     @Test
-    void mul() {
-        long result = Calculator.multiply(2, 3);
+    void multiply() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+
+        Method multiply = Calculator.class.getDeclaredMethod("multiply", long.class, long.class);
+        multiply.setAccessible(true);
+        long result = (long)multiply.invoke(new Calculator(), 2,3);
+
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    void div() {
-        long result = Calculator.divide(6, 3);
+    void divide() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        Method divide = Calculator.class.getDeclaredMethod("divide", long.class, long.class);
+        divide.setAccessible(true);
+        long result = (long)divide.invoke(new Calculator(), 4,2);
+
         assertThat(result).isEqualTo(2);
     }
 
