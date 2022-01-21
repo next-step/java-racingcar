4957619afd8fbf7ed5494ff24138bc9eaab25f7a
PandaHun <gnsgnsek@gmail.com>
2022-01-21 10:14:05 +0900

    typo(Calculator): Modify variable name, method name
    - 변수명 수정
    - method 오타 수정

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -11,43 +11,43 @@ public class Calculator {
     private static final int START_OPERATION_INDEX = 1;
     private static final int INTERVAL_OF_NUMBER_INDEX = 1;
 
-    private List<Operand> operandList;
-    private List<Operation> operationList;
+    private List<Operand> operands;
+    private List<Operation> operations;
 
     public Calculator() {
-        operandList = new ArrayList<>();
-        operationList = new ArrayList<>();
+        operands = new ArrayList<>();
+        operations = new ArrayList<>();
     }
 
     public int calculate(String expression) {
         String[] splitExpression = StringUtils.splitExpression(expression);
         validateIdleExpression(splitExpression);
         extractOperand(splitExpression);
-        extractOPeration(splitExpression);
+        extractOperation(splitExpression);
         calculateExpression();
-        return operandList.get(operandList.size() - 1)
+        return operands.get(operands.size() - 1)
             .getNumber();
     }
 
     private void calculateExpression() {
-        Operand left = operandList.get(START_OPERAND_INDEX);
-        for (int index = START_OPERAND_INDEX; index < operationList.size(); index++) {
-            Operation operation = operationList.get(index);
-            Operand right = operandList.get(index + INTERVAL_OF_NUMBER_INDEX);
+        Operand left = operands.get(START_OPERAND_INDEX);
+        for (int index = START_OPERAND_INDEX; index < operations.size(); index++) {
+            Operation operation = operations.get(index);
+            Operand right = operands.get(index + INTERVAL_OF_NUMBER_INDEX);
             left = new Operand(operation.operate(left, right));
         }
-        operandList.add(left);
+        operands.add(left);
     }
 
-    private void extractOPeration(String[] splitExpression) {
+    private void extractOperation(String[] splitExpression) {
         for (int index = START_OPERATION_INDEX; index < splitExpression.length; index += INTERVAL_OF_INDEX) {
-            operationList.add(new Operation(splitExpression[index]));
+            operations.add(new Operation(splitExpression[index]));
         }
     }
 
     private void extractOperand(String[] splitExpression) {
         for (int index = 0; index < splitExpression.length; index += INTERVAL_OF_INDEX) {
-            operandList.add(new Operand(splitExpression[index]));
+            operands.add(new Operand(splitExpression[index]));
         }
     }
 
