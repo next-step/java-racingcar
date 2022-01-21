a6211bc5441f98c937b56ea661c7ab84541100f0
PandaHun <gnsgnsek@gmail.com>
2022-01-21 10:53:31 +0900

    refactor(Operand): Modify Operand with using interger constructor
    - Operand 정수형 생성자 추가 반영

--- src/main/java/calculator/domain/Operand.java
+++ src/main/java/calculator/domain/Operand.java
@@ -8,6 +8,10 @@ public class Operand {
         this.number = toInt(number);
     }
 
+    public Operand(int number) {
+        this.number = number;
+    }
+
     public int getNumber() {
         return number;
     }
