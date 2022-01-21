4f1acb4b32788cc8d36f526aef6112367f1a800f
kimtaejun97 <kim_taejun@naver.com>
2022-01-21 17:09:25 +0900

    feat: 0으로 나누었을 때 예외처리
    catch ArithmeticException and pring Error Message

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,10 +10,15 @@ public class Application {
     public static void main(String[] args) throws IOException {
         while (true) {
             String[] splitUserInput;
+            long result;
 
             try {
                 System.out.print("식을 입력해 주세요(그만하려면 q): ");
                 splitUserInput = Input.getUserInput();
+
+                Expression expression = new Expression(splitUserInput);
+                result = expression.evaluate();
+
             } catch (NumberFormatException e) {
                 System.out.println("[ERROR] 해당 자리에는 값(숫자)이 위치해야 합니다.(v op v op v ..)");
                 continue;
@@ -23,10 +28,11 @@ public class Application {
             } catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 continue;
+            } catch (ArithmeticException e){
+                System.out.println("[ERROR] 0으로 나눌 수 없습니다.");
+                continue;
             }
 
-            Expression expression = new Expression(splitUserInput);
-            long result = expression.evaluate();
             System.out.println(result);
         }
     }
