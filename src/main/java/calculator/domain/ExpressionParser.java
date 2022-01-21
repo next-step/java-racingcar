4de7d0eaed652330f21bf3f4820480bdcaf39abf
kimtaejun97 <kim_taejun@naver.com>
2022-01-21 19:40:40 +0900

    feat: 사용자의 입력을 받아 파싱해 Expression 객체를 반환하는 기능 구현.
    Expression 객체는 terms 와operators를 가진다.

--- src/main/java/calculator/domain/ExpressionParser.java
+++ src/main/java/calculator/domain/ExpressionParser.java
@@ -1,2 +1,28 @@
-package calculator;public class ExpressionParser {
+package calculator.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpressionParser {
+
+    public ExpressionParser() {}
+
+    public Expression parseExpression(String[] splitUserInput) {
+        List<Long> terms = new ArrayList<>();
+        List<String> operators = new ArrayList<>();
+
+        for (int pos = 0; pos < splitUserInput.length; pos++) {
+            if (isTerm(pos)) {
+                terms.add(Long.parseLong(splitUserInput[pos]));
+                continue;
+            }
+            operators.add(splitUserInput[pos]);
+        }
+
+        return new Expression(terms, operators);
+    }
+
+    private boolean isTerm(int pos) {
+        return pos % 2 == 0;
+    }
 }
