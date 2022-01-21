a7c1edaacee8408e9cecc40c72027891273704a4
kimtaejun97 <kim_taejun@naver.com>
2022-01-21 20:32:34 +0900

    test: Culculator.evaluate() 테스트
    - Expression 객체를 받아 연산 값을 반환한다.

--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -4,7 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Currency;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CalculatorTest {
 
@@ -13,26 +19,28 @@ class CalculatorTest {
 
         Method add = Calculator.class.getDeclaredMethod("add", long.class, long.class);
         add.setAccessible(true);
-        long result = (long)add.invoke(null,1,3);
+        long result = (long) add.invoke(null, 1, 3);
 
         assertThat(result).isEqualTo(4);
     }
 
     @Test
-    void subtract() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    void subtract()
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
         Method subtract = Calculator.class.getDeclaredMethod("subtract", long.class, long.class);
         subtract.setAccessible(true);
-        long result = (long)subtract.invoke(null, 3,1);
+        long result = (long) subtract.invoke(null, 3, 1);
         assertThat(result).isEqualTo(2);
     }
 
     @Test
-    void multiply() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    void multiply()
+            throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
 
         Method multiply = Calculator.class.getDeclaredMethod("multiply", long.class, long.class);
         multiply.setAccessible(true);
-        long result = (long)multiply.invoke(null, 2,3);
+        long result = (long) multiply.invoke(null, 2, 3);
 
         assertThat(result).isEqualTo(6);
     }
@@ -42,9 +50,23 @@ class CalculatorTest {
 
         Method divide = Calculator.class.getDeclaredMethod("divide", long.class, long.class);
         divide.setAccessible(true);
-        long result = (long)divide.invoke(null, 4,2);
+        long result = (long) divide.invoke(null, 4, 2);
 
         assertThat(result).isEqualTo(2);
     }
 
+    @Test
+    void evaluate() {
+
+        // given
+        List<Long> terms = new ArrayList<>(Arrays.asList(1L, 2L, 3L, 10L, 2L, 2L));
+        List<String> operators = new ArrayList<>(Arrays.asList("+", "+", "*", "-", "/"));
+        Expression expression = new Expression(terms, operators);
+
+        // when
+        long result = Calculator.evaluate(expression);
+
+        // then
+        assertThat(result).isEqualTo(29L);
+    }
 }
