878f4d9ff82d09b66976ac295e38aba1377ebe60
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-22 01:48:26 +0900

    refactor(racingCar): Application 클래스 수정
    - throws IOException 삭제

--- src/main/java/stringcalculator/Application.java
+++ src/main/java/stringcalculator/Application.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         new CalculatorInit().start();
     }
