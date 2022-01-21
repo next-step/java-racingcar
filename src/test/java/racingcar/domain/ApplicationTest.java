3152cc668292b73c235673c416f345e91f29a49d
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-22 01:24:14 +0900

    feat(racingCar): userInput 테스트 목적의 코드 작성
    - ApplicationTest 클래스작성

--- src/test/java/racingcar/domain/ApplicationTest.java
+++ src/test/java/racingcar/domain/ApplicationTest.java
@@ -1,2 +1,48 @@
-package racingcar.domain;public class ApplicationTest {
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static racingcar.common.info.RacingGameErrorInfo.*;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import org.assertj.core.api.AbstractBooleanAssert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.common.UserInput;
+import racingcar.common.info.RacingGameErrorInfo;
+
+public class ApplicationTest {
+
+    private UserInput userInput;
+
+    @DisplayName("객체 초기화")
+    @BeforeEach
+    void init(){
+        userInput = new UserInput();
+    }
+
+    @DisplayName("유저_입력_글자길이_테스트")
+    @Test
+    void userInputLengthTest() {
+        userInput.setCarNameInput("CarNameIsLong");
+        assertTrue(userInput.parsingCarName() == true);
+    }
+
+    @DisplayName("유저_입력_중복_테스트")
+    @Test
+    void userInputDuplicationTest(){
+        userInput.setCarNameInput("Benz,Kia,Kia");
+        assertThat(userInput.parsingCarName() == true);
+    }
+
+    @DisplayName("입력_횟수_범위_테스트")
+    @Test
+    void userInputRacingTry() {
+        userInput.setRacingTryCounter(10);
+        assertThat(userInput.parsingRacingTry() == true);
+    }
 }
