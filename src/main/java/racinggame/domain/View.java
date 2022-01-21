248ab5077f730f22d84b20b11a82081cc6ae9ba0
kimtaejun97 <kim_taejun@naver.com>
2022-01-21 18:30:16 +0900

    feat: 우승자를 출력하는 로직을 View 클래스에 구현.
    - View.printWinners
    - View.getCarPositions
    - View.getHighScore
    - View.getWinners

--- src/main/java/racinggame/domain/View.java
+++ src/main/java/racinggame/domain/View.java
@@ -1,8 +1,13 @@
 package racinggame.domain;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class View {
+
+    private static final String DELIMITER = ", ";
+
     private List<RacingCar> racingCars;
 
     private View() {}
@@ -17,5 +22,30 @@ public class View {
 
     public void printProgress() {
         racingCars.forEach(RacingCar::printCurrPosition);
+    }
+
+    public void printWinners() {
+        int max = getHighScore();
+        List<String> winners = getWinners(max);
+
+        String result = winners.stream().collect(Collectors.joining(DELIMITER));
+        System.out.println("최종 우승자: " + result);
+    }
+
+    private int getHighScore() {
+        return Collections.max(getCarPositions());
+    }
+
+    private List<Integer> getCarPositions() {
+        return racingCars.stream()
+                .map(car -> car.getPosition())
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getWinners(int max) {
+        return racingCars.stream()
+                .filter(car -> car.getPosition() == max)
+                .map(car -> car.getCarName())
+                .collect(Collectors.toList());
     }
 }
