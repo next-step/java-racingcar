e5d98c06577bf3feffec7137ae467307b8c08d6a
PandaHun <gnsgnsek@gmail.com>
2022-01-21 09:56:10 +0900

    refactor(Calculator): Modify method naming validate to extract
    - 추출 메서드 명 변경

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class Calculator {
 
+    private static final int INTERVAL_OF_INDEX = 2;
+    private static final int START_OPERAND_INDEX = 0;
+    private static final int START_OPERATION_INDEX = 1;
+    private static final int INTERVAL_OF_NUMBER_INDEX = 1;
+
     private List<Operand> operandList;
     private List<Operation> operationList;
 
@@ -17,31 +22,31 @@ public class Calculator {
     public int calculate(String expression) {
         String[] splitExpression = StringUtils.splitExpression(expression);
         validateIdleExpression(splitExpression);
-        validateOperand(splitExpression);
-        validateOperation(splitExpression);
+        extractOperand(splitExpression);
+        extractOPeration(splitExpression);
         calculateExpression();
         return operandList.get(operandList.size() - 1)
             .getNumber();
     }
 
     private void calculateExpression() {
-        Operand left = operandList.get(0);
-        for (int index = 0; index < operationList.size(); index++) {
+        Operand left = operandList.get(START_OPERAND_INDEX);
+        for (int index = START_OPERAND_INDEX; index < operationList.size(); index++) {
             Operation operation = operationList.get(index);
-            Operand right = operandList.get(index + 1);
+            Operand right = operandList.get(index + INTERVAL_OF_NUMBER_INDEX);
             left = new Operand(operation.operate(left, right));
         }
         operandList.add(left);
     }
 
-    private void validateOperation(String[] splitExpression) {
-        for (int index = 1; index < splitExpression.length; index += 2) {
+    private void extractOPeration(String[] splitExpression) {
+        for (int index = START_OPERATION_INDEX; index < splitExpression.length; index += INTERVAL_OF_INDEX) {
             operationList.add(new Operation(splitExpression[index]));
         }
     }
 
-    private void validateOperand(String[] splitExpression) {
-        for (int index = 0; index < splitExpression.length; index += 2) {
+    private void extractOperand(String[] splitExpression) {
+        for (int index = 0; index < splitExpression.length; index += INTERVAL_OF_INDEX) {
             operandList.add(new Operand(splitExpression[index]));
         }
     }
