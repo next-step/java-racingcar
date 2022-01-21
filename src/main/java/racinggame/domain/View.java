1b5419e2e443ffefb8b1e4b3217c6f4bd1cb02cd
kimtaejun97 <kim_taejun@naver.com>
2022-01-21 17:53:04 +0900

    feat: View 객체 생성자 구현
    - RacingCar List를 받아 생성.

--- src/main/java/racinggame/domain/View.java
+++ src/main/java/racinggame/domain/View.java
@@ -1,2 +1,17 @@
-package racinggame.domain;public class View {
+package racinggame.domain;
+
+import java.util.List;
+
+public class View {
+    private List<RacingCar> racingCars;
+
+    private View() {}
+
+    public View(List<RacingCar> racingCars) {
+        this.racingCars = racingCars;
+    }
+
+    public List<RacingCar> getRacingCars() {
+        return racingCars;
+    }
 }
