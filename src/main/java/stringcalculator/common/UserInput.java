37ddd236029cf3402cffb6a6a1bc7d1cc9475497
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-22 05:21:21 +0900

    feat(stringCalculator): UserInput 클래스 수정
    - setter 함수 생성
    - 입력과 검증 부분을 메소드로 나누어 구현
    - 입력포맷에 대한 검증 기능 구현
    - " "에 대한 검증 로직 추가

--- src/main/java/stringcalculator/common/UserInput.java
+++ src/main/java/stringcalculator/common/UserInput.java
@@ -1,49 +1,99 @@
 package stringcalculator.common;
 
+import static stringcalculator.common.ErrorMessage.*;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import racingcar.common.exception.InputValidationException;
 
 public class UserInput {
 
-    private static final String DELIMITER = " ";
+    private static final String DELIMITER_BLANK = " ";
+    private static final String DELIMITER_NO_BLANK = "";
+    private static final String INPUT_OPERATION_PHRASE = "사칙 연산을 입력해주세요.\n";
+
+    private final BufferedReader bufferedReader;
+    private final BufferedWriter log;
+
     private List<String> splitUserInput;
     private String userInput;
 
     public UserInput() {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            this.userInput = bufferedReader.readLine();
-            validateNull(userInput);
-            validateBlank(userInput);
-            splitUserInput = splitStr(userInput);
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+        log = new BufferedWriter(new OutputStreamWriter(System.out));
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        splitUserInput = new ArrayList<>();
+        userInput = "";
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    public boolean userInputRun(){
+        userInput();
+        return parsingUserInput();
     }
 
     public List<String> getSplitUserInput() {
         return splitUserInput;
     }
 
-    private List<String> splitStr(final String userInput) {
-        return Arrays.stream(userInput.split(DELIMITER)).collect(Collectors.toList());
+    public boolean parsingUserInput(){
+        try {
+            validateNull(userInput);
+            validateBlank(userInput);
+            splitUserInput = splitStr(userInput);
+            validateFormat(splitUserInput);
+        }catch(IllegalArgumentException exception){
+            exception.printStackTrace();
+            return true;
+        }
+        return false;
     }
 
-
-    private void validateNull(final String userInput) {
-        if (Objects.isNull(userInput)) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_NULL);
+    private void userInput(){
+        try {
+            log.write(INPUT_OPERATION_PHRASE);
+            log.flush();
+            this.userInput = bufferedReader.readLine();
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 
-    private void validateBlank(final String userInput) {
-        if (userInput.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_MESSAGE_BLANK);
+    private List<String> splitStr(String userInput) {
+        return Arrays.stream(userInput.replace(DELIMITER_BLANK, DELIMITER_NO_BLANK).split(DELIMITER_NO_BLANK)).collect(Collectors.toList());
+    }
+
+    public void validateFormat(final List<String> userInput){
+        IntStream.range(0, userInput.size())
+            .forEach(idx -> {
+                if(idx%2==0 && !Character.isDigit(userInput.get(idx).charAt(0))){
+                    throw new IllegalArgumentException(ERROR_MESSAGE_FORMAT);
+                }else if(idx%2==1 && !"-*/+".contains(userInput.get(idx))){
+                    throw new IllegalArgumentException(ERROR_MESSAGE_FORMAT);
+                }
+            });
+    }
+
+    public void validateNull(final String userInput) {
+        if (Objects.isNull(userInput)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_NULL);
+        }
+    }
+
+    public void validateBlank(final String userInput) {
+        if (userInput.isEmpty() || userInput.equals(" ")) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BLANK);
         }
     }
 
