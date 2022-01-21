170dc8cd261a622e482215f5e02ce6cac63f38eb
smc5236 <smc5236@gmail.com>
2022-01-21 16:35:02 +0900

    🔥 feat(racingCar) : SystemMessage 클래스 private 생성자 추가

--- src/main/java/racingcar/common/SystemMessage.java
+++ src/main/java/racingcar/common/SystemMessage.java
@@ -2,6 +2,9 @@ package racingcar.common;
 
 public class SystemMessage {
 
+    private SystemMessage() {
+    }
+
     public final static String CAR_NAME_INPUT_INFO = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public final static String RACING_TIME_INPUT_INFO = "시도할 횟수는 몇 회인가요?";
     public final static String FINAL_WINNERS_INFO = "최종 우승자: ";
