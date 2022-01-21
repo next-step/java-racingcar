14f939b4897b064f5fa04f5d6e8ac47dc5c5e332
kimtaejun97 <kim_taejun@naver.com>
2022-01-21 19:27:49 +0900

    test: Expression은 operators와 terms를 받아 생성된다.
    - operators: List<String>
    - terms: List<Long>

--- src/test/java/calculator/domain/ExpressionTest.java
+++ src/test/java/calculator/domain/ExpressionTest.java
@@ -2,16 +2,25 @@ package calculator.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ExpressionTest {
 
     @Test
-    void 계산값이_올바른가() {
-        String[] splitUserInput = "1 + 2 + 3 * 5".split("\\s++");
+    void Terms와_Operators를_받아_생성() {
 
-        Expression expression = new Expression(splitUserInput);
-        long result = expression.evaluate();
-        assertThat(result).isEqualTo(30);
+        // given
+        ArrayList<Long> terms = new ArrayList<>(Arrays.asList(1L, 2L));
+        ArrayList<String> operators = new ArrayList<>(Arrays.asList("+"));
+
+        // when
+        Expression expression = new Expression(terms, operators);
+
+        // then
+        assertThat(expression.getTerms()).isEqualTo(terms);
+        assertThat(expression.getOperators()).isEqualTo(operators);
     }
 }
