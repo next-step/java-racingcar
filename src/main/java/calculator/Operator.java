05a6c99af5403658c85bd7252bebce1ae8ca76f3
hyoenju <l67378810@gmail.com>
2022-01-22 17:04:41 +0900

    refactor(operator): Change Access Modifier(public => private)

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Operator {
 
     private final static List<String> SYMBOLS = new ArrayList<>(Arrays.asList("+", "-", "*", "/"));
-    public final List<String> operators;
+    private List<String> operators;
 
     public Operator() {
         operators = new ArrayList<>();
@@ -18,5 +18,9 @@ public class Operator {
             throw new IllegalArgumentException("연산기호가 아닙니다.");
         }
         operators.add(ope);
+    }
+
+    public List<String> getOperators() {
+        return operators;
     }
 }
