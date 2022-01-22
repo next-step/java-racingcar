b5bf2b58f9e77ef747e22f9e98fae8ab5de631fc
kimtaejun97 <kim_taejun@naver.com>
2022-01-22 23:31:29 +0900

    test: Operators.nextOperator() 테스트
    - 다음 연산자를 잘 가져오는지 테스트

--- src/test/java/calculator/domain/OperatorsTest.java
+++ src/test/java/calculator/domain/OperatorsTest.java
@@ -1,6 +1,7 @@
 package calculator.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,11 +11,20 @@ import org.junit.jupiter.api.Test;
 class OperatorsTest {
 
     @Test
-    void 연산자_리스트를_받아_생성(){
+    void 연산자_리스트를_받아_생성() {
         List<String> ops = new ArrayList<>(Arrays.asList("+", "-"));
 
         Operators operators = new Operators(ops);
 
         assertThat(operators.getOperators().size()).isEqualTo(2);
+    }
+
+    @Test
+    void nextOperator() {
+        List<String> ops = new ArrayList<>(Arrays.asList("+", "-"));
+        Operators operators = new Operators(ops);
+
+        assertThat(operators.nextOperator()).isEqualTo("+");
+        assertThat(operators.nextOperator()).isEqualTo("-");
     }
 }
