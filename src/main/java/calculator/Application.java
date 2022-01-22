4c967b057fa119bc85c7f20fa9f935a6a64b9990
hyoenju <l67378810@gmail.com>
2022-01-22 16:28:49 +0900

    feat(print): Add Input, Output Message

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,14 +3,17 @@ package calculator;
 import java.io.IOException;
 
 public class Application {
+    static String INPUT_MESSAGE = "계산식을 입력하세요 : ";
+    static String OUTPUT_MESSAGE = "결과값 : ";
 
     public static void main(String[] args) throws IOException {
+        System.out.print(INPUT_MESSAGE);
         UserInput userInput = new UserInput();
         Seperator seperator = new Seperator();
         seperator.seperate(userInput.inputList);
         Calculator calculator = new Calculator();
 
         double result = calculator.doMultipleOperation(seperator.number.numbers, seperator.operator.operators);
-        System.out.println(result);
+        System.out.println(OUTPUT_MESSAGE + result);
     }
 }
