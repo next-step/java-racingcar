085d17d619e3f38c9038d25635fff23ef777b1d8
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-22 18:34:25 +0900

    feat(RacingController): 재할당이 필요없는 인스턴스 변수에 final 부여

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -8,10 +8,10 @@ import racingcar.domain.random.RandomGenerator;
 public class RacingController {
 
     private static final String HEAD_MESSAGE = "\n실행 결과";
-    private Participants participants;
+    private final Participants participants;
     private final int turnCount;
     private final int participantCount;
-    private Move move;
+    private final Move move;
 
     private RacingController(Participants participants, int turn) {
         this.participants = participants;
