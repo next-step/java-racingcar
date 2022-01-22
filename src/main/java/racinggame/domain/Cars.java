7894f2b289e3d26ea87889018feb0be36f0332d7
hyojeong <qkrgywjd456@naver.com>
2022-01-22 18:44:28 +0900

    refactor: Making a car object list in the cars class

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -7,12 +7,22 @@ public class Cars {
 
     private List<Car> cars = new ArrayList<>();
 
+    public Cars(String[] carNames) {
+        for (int carNum = 0; carNum < carNames.length; carNum++) {
+            cars.add(new Car(carNames[carNum]));
+        }
+    }
+
     public List<Car> createCarList(String[] carNames) {
         //Car들 모임
         List<Car> cars = new ArrayList<>();
         for (String carName : carNames) {
             cars.add(new Car(carName));
         }
+        return cars;
+    }
+
+    public List<Car> getCars(){
         return cars;
     }
 
