c6c6f59666805781e16e02bc6ab7d5c230293aa7
hyojeong <qkrgywjd456@naver.com>
2022-01-22 23:30:01 +0900

    refactor: change the magic number 5 to a constant

--- src/main/java/racinggame/domain/GameInit.java
+++ src/main/java/racinggame/domain/GameInit.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class GameInit {
 
+    private static final int MAX_CAR_NAME_LENGTH=5;
 
     public String[] inputPlayerName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
@@ -29,10 +30,10 @@ public class GameInit {
         return tryNumber;
     }
 
-    public boolean validationStringLength(String[] playerNameList) {
+     boolean validationStringLength(String[] playerNameList) {
         boolean isValidate = true;
         for (String playerName : playerNameList) {
-            if (playerName.length() > 5) {
+            if (playerName.length() > MAX_CAR_NAME_LENGTH) {
                 System.out.println("자동차의 이름은 5자를 초과할 수 없습니다.");
                 isValidate = false;
             }
