af6deaa49b15d02d9132a53f5d7dd939255429fc
hyojeong <qkrgywjd456@naver.com>
2022-01-22 11:44:25 +0900

    style: delete unnecessary comment

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -9,14 +9,13 @@ import racinggame.domain.PlayRacingGame;
 public class Application {
 
     public static void main(String[] args) {
-        //게임 초기화
         GameInit gameInit = new GameInit();
         String[] carNames = gameInit.inputPlayerName();
         int playGameNumber = gameInit.inputTryNumber();
 
         Cars cars = new Cars();
         List<Car> carList = cars.createCarList(carNames);
-        //게임 시작
+
         PlayRacingGame playRacingGame = new PlayRacingGame();
         System.out.println("실행 결과");
         for (; playGameNumber > 0; playGameNumber--) {
