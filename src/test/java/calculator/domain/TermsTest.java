f02bc96620799060567db173baaac083c49af51a
kimtaejun97 <kim_taejun@naver.com>
2022-01-22 23:45:48 +0900

    test: Terms.hasNext() 기능 테스트
    - 다음 값이 있을 때 -> true
    - 다음 값이 없을 때 -> false

--- src/test/java/calculator/domain/TermsTest.java
+++ src/test/java/calculator/domain/TermsTest.java
@@ -12,7 +12,7 @@ class TermsTest {
 
     @Test
     void 값_리스트를_받아_생성() {
-        List<Double> values = new ArrayList<>(Arrays.asList(1D,2D));
+        List<Double> values = new ArrayList<>(Arrays.asList(1D, 2D));
 
         Terms terms = new Terms(values);
 
@@ -21,10 +21,29 @@ class TermsTest {
 
     @Test
     void nextTerm() {
-        List<Double> values = new ArrayList<>(Arrays.asList(1D,2D));
+        List<Double> values = new ArrayList<>(Arrays.asList(1D, 2D));
         Terms terms = new Terms(values);
 
         assertThat(terms.nextTerm()).isEqualTo(1);
         assertThat(terms.nextTerm()).isEqualTo(2);
+    }
+
+    @Test
+    void hasNext_true() {
+        List<Double> values = new ArrayList<>(Arrays.asList(1D, 2D));
+        Terms terms = new Terms(values);
+
+        assertTrue(terms.hasNext());
+    }
+
+    @Test
+    void hasNext_false() {
+        List<Double> values = new ArrayList<>(Arrays.asList(1D, 2D));
+        Terms terms = new Terms(values);
+
+        terms.nextTerm();
+        terms.nextTerm();
+
+        assertFalse(terms.hasNext());
     }
 }
