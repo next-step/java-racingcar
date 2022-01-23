baa75077e4400367707945f089222e103b980ce0
hyojeong <qkrgywjd456@naver.com>
2022-01-24 05:09:12 +0900

    style: delete semicolon

--- src/main/java/strcalculator/domain/StringProcessor.java
+++ src/main/java/strcalculator/domain/StringProcessor.java
@@ -58,7 +58,7 @@ public class StringProcessor {
 
     public String[] parseInputNumber(){
         String input = userInput.replaceAll(" ", "");
-        String []numbers=input.split("[+\\-x/]");;
+        String []numbers=input.split("[+\\-x/]");
         return numbers;
     }
 
