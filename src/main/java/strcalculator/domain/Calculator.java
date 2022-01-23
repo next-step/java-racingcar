ec22fc932411ec1887232fbf10c24d5852c16f03
hyojeong <qkrgywjd456@naver.com>
2022-01-24 05:36:57 +0900

    refactor: modify calculator parameter modify List type to String[]

--- src/main/java/strcalculator/domain/Calculator.java
+++ src/main/java/strcalculator/domain/Calculator.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Calculator {
     private List<String> inputStr;
-    private List<String> numberList;
-    private List<String> operandList;
+    private String[] numberList;
+    private String[] operandList;
     public Calculator() {
     }
 
@@ -13,12 +13,12 @@ public class Calculator {
         this.inputStr=inputStr;
     }
 
-    public Calculator(List<String> numberList,List<String>operandList) {
+    public Calculator(String[] numberList,String[]operandList) {
         this.numberList=numberList;
         this.operandList=operandList;
     }
 
-    public void calculate(String[]numberList,String[]operandList) {
+    public void calculate() {
         int total = Integer.valueOf(numberList[0]);
         for (int i = 1; i < operandList.length; i++) {
             String operator = operandList[i];
