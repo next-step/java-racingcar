1e4d8ba2a224e6b9296d84c3078b5583ed1267e9
smc5236 <smc5236@gmail.com>
2022-01-23 20:35:26 +0900

    🔥 feat(RacingCar): Racing 클래스는 경주의 역할만 하고 나머지는 일급 컬렉션에게 위임

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -6,8 +6,6 @@ import java.util.stream.IntStream;
 
 public class Racing {
 
-    private final List<Car> cars = new ArrayList<>();
-
     private Racing() {
     }
 
@@ -15,17 +13,9 @@ public class Racing {
         return new Racing();
     }
 
-    public void drive() {
+    public void drive(final List<Car> cars) {
         IntStream.range(0, cars.size())
                 .forEach(idx -> cars.get(idx).moveForward());
-    }
-
-    public void registerCar(final Car car) {
-        cars.add(car);
-    }
-
-    public List<Car> getCars() {
-        return cars;
     }
 
 }
