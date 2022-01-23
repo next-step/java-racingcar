c6aa647be830d31a632eedada91a5b197c293f16
smc5236 <smc5236@gmail.com>
2022-01-23 20:36:46 +0900

    🔥 refactor(RacingCar): 코드 스타일 적용

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-import racingcar.domain.movable.ForwardStrategy;
 import racingcar.domain.movable.MovableStrategy;
 
 import java.util.ArrayList;
--- src/main/java/racingcar/domain/movable/ForwardStrategy.java
+++ src/main/java/racingcar/domain/movable/ForwardStrategy.java
@@ -2,7 +2,7 @@ package racingcar.domain.movable;
 
 import racingcar.domain.RandomNumberGenerator;
 
-public class ForwardStrategy implements MovableStrategy{
+public class ForwardStrategy implements MovableStrategy {
 
     private static final int STANDARD_NUMBER_OF_MOVE_FORWARD = 4;
 
