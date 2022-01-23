333767c9da12e187a847f3e9392546577e04fb2c
smc5236 <smc5236@gmail.com>
2022-01-23 20:35:59 +0900

    🔥 refactor(RacingCar): 변경된 코드로 인한 깨진 코드 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,9 +2,10 @@ package racingcar;
 
 
 import racingcar.common.UserInput;
-import racingcar.domain.Helper;
+import racingcar.domain.Cars;
 import racingcar.domain.Judgement;
 import racingcar.domain.Racing;
+import racingcar.domain.movable.ForwardStrategy;
 
 public class Application {
 
@@ -14,12 +15,12 @@ public class Application {
         Racing racing = Racing.instance();
         UserInput userInput = UserInput.instance();
         Judgement judgement = Judgement.instance();
-        new Helper().runner(userInput.getSplitUserInput(), racing);
+        Cars cars = Cars.of(userInput.getSplitUserInput(), new ForwardStrategy());
         for (int time = START_FROM_ZERO; time < userInput.getRacingTime(); time++) {
-            racing.drive();
-            judgement.carsStatement(racing.getCars());
+            racing.drive(cars.getCars());
+            judgement.carsStatement(cars.getCars());
         }
-        System.out.println(judgement.judge(racing.getCars()));
+        System.out.println(judgement.judge(cars.getCars()));
     }
 
 }
