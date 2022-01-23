7bd1ee0b6d7f7f61ad021360836392c1608a4a36
smc5236 <smc5236@gmail.com>
2022-01-23 20:59:09 +0900

    🔥 feat(RacingCar): 파라미터를 통해 고정된 움직임 전략을 가지는 Strategy 구현체 생성
    - 인스턴스 변수인 isMoved를 반환하여 움직일지 멈출지를 판단한다.

---
+++ src/main/java/racingcar/domain/movable/FixedForwardStrategy.java
@@ -0,0 +1,15 @@
+package racingcar.domain.movable;
+
+public class FixedForwardStrategy implements MovableStrategy{
+
+    private final boolean isMoved;
+
+    public FixedForwardStrategy(final boolean isMoved) {
+        this.isMoved = isMoved;
+    }
+
+    @Override
+    public boolean move() {
+        return isMoved;
+    }
+}
