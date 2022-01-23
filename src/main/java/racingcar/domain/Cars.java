ede189ffc54cd83ee6a291aaf5c468fbaa773ccf
smc5236 <smc5236@gmail.com>
2022-01-23 20:34:38 +0900

    🔥 feat(RacingCar): Cars 일급 컬렉션 클래스 생성

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import racingcar.domain.movable.ForwardStrategy;
+import racingcar.domain.movable.MovableStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> cars = new ArrayList<>();
+
+    private Cars(final List<String> input, final MovableStrategy strategy) {
+        for (String carName : input) {
+            cars.add(Car.of(carName, strategy));
+        }
+    }
+
+    public static Cars of(final List<String> input, final MovableStrategy strategy) {
+        return new Cars(input, strategy);
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+}
