b4f495dfa4332b161a12102fbd1be1d636d03f7c
hyojeong <qkrgywjd456@naver.com>
2022-01-24 00:06:12 +0900

    refactor: Remove factor value of calculator function

--- src/main/java/strcalculator/Application.java
+++ src/main/java/strcalculator/Application.java
@@ -14,8 +14,8 @@ public class Application {
             try {
                 play.parseInput();
                 List<String> result = play.getResult();
-                Calculator calculator = new Calculator();
-                calculator.calculate(result);
+                Calculator calculator = new Calculator(result);
+                calculator.calculate();
                 break;
             } catch (IllegalArgumentException | ArithmeticException e) {
                 System.out.println("다시 입력해주세요.");
--- src/main/java/strcalculator/domain/Calculator.java
+++ src/main/java/strcalculator/domain/Calculator.java
@@ -11,18 +11,18 @@ public class Calculator {
         this.inputStr=inputStr;
     }
 
-    public void calculate(List<String> result) {
-        int total = Integer.valueOf(result.get(0));
-        for (int i = 1; i < result.size(); i += 2) {
-            String operator = result.get(i);
+    public void calculate() {
+        int total = Integer.valueOf(inputStr.get(0));
+        for (int i = 1; i < inputStr.size(); i += 2) {
+            String operator = inputStr.get(i);
             if (operator.equals("+")) {
-                total = add(total, Integer.valueOf(result.get(i + 1)));
+                total = add(total, Integer.valueOf(inputStr.get(i + 1)));
             } else if (operator.equals("-")) {
-                total = subtract(total, Integer.valueOf(result.get(i + 1)));
+                total = subtract(total, Integer.valueOf(inputStr.get(i + 1)));
             } else if (operator.equals("*")) {
-                total = multiply(total, Integer.valueOf(result.get(i + 1)));
+                total = multiply(total, Integer.valueOf(inputStr.get(i + 1)));
             } else if (operator.equals("/")) {
-                total = divide(total, Integer.valueOf(result.get(i + 1)));
+                total = divide(total, Integer.valueOf(inputStr.get(i + 1)));
             }
         }
         System.out.println(total);
