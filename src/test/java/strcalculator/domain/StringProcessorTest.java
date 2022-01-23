a61b6faeda1dc39e93915270a881e29771f4e7b0
hyojeong <qkrgywjd456@naver.com>
2022-01-24 05:35:11 +0900

    refactor: change test logic
    validation function throw error

--- src/test/java/strcalculator/domain/StringProcessorTest.java
+++ src/test/java/strcalculator/domain/StringProcessorTest.java
@@ -2,7 +2,7 @@ package strcalculator.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +13,8 @@ class StringProcessorTest {
     @DisplayName("숫자와 사칙연산을 제외한 문자가 들어왔는지 확인")
     public void inputValidation(){
         StringProcessor stringProcessor = new StringProcessor("1+++2+3+++");
-        boolean validation = stringProcessor.validation();
-        Assertions.assertThat(validation).isFalse();
+        Assertions.assertThrows(IllegalArgumentException.class,()->{
+            stringProcessor.validation();
+        });
     }
 }
