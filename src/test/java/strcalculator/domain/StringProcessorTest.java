9b70dd839baa6fd3ac41d25772a427adaf4232d4
hyojeong <qkrgywjd456@naver.com>
2022-01-24 01:53:45 +0900

    test: create inputValidation function

---
+++ src/test/java/strcalculator/domain/StringProcessorTest.java
@@ -0,0 +1,19 @@
+package strcalculator.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringProcessorTest {
+
+
+    @Test
+    @DisplayName("숫자와 사칙연산을 제외한 문자가 들어왔는지 확인")
+    public void inputValidation(){
+        StringProcessor stringProcessor = new StringProcessor("1+++2+3+++");
+        boolean validation = stringProcessor.validation();
+        Assertions.assertThat(validation).isFalse();
+    }
+}
