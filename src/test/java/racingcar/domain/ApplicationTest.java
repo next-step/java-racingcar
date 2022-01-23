3775b3f8ecaac60f5dcc30b569055da07f41b5c6
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-23 15:07:07 +0900

    test(racingCar): maxCheck 메소드에 대한 테스트 코드 작성
    - maxCheck - 가장 많이 전진한 Car 를 확인할 수 있는 기능 구현

--- src/test/java/racingcar/domain/ApplicationTest.java
+++ src/test/java/racingcar/domain/ApplicationTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static racingcar.common.info.RacingGameErrorInfo.*;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.AbstractBooleanAssert;
 import org.assertj.core.api.Assertions;
@@ -14,6 +16,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.common.UserInput;
 import racingcar.common.info.RacingGameErrorInfo;
+import racingcar.common.info.RacingGameInfo;
 
 public class ApplicationTest {
 
@@ -41,8 +44,31 @@ public class ApplicationTest {
 
     @DisplayName("입력_횟수_범위_테스트")
     @Test
-    void userInputRacingTry() {
+    void userInputRacingTryTest() {
         userInput.setRacingTryCounter(10);
         assertThat(userInput.parsingRacingTry() == true);
+    }
+
+    @DisplayName("우승자_확인_테스트")
+    @Test
+    public void maxCheckTest() {
+        //given
+        Car testCarA = Car.of("a");
+        Car testCarB = Car.of("aa");
+        Car testCarC = Car.of("aaa");
+        final List<Car> cars = Arrays.asList(testCarA, testCarB, testCarC);
+
+        //when
+        cars.get(0).moveForward();
+        cars.get(0).moveForward();
+        cars.get(0).moveForward();
+
+        final int max = cars.stream().map(car -> car.getStep())
+            .mapToInt(length -> Integer.valueOf(length))
+            .max()
+            .getAsInt();
+
+        //then
+        assertThat(testCarA.toString(max)).isEqualTo("a ");
     }
 }
