2c796c1482d6c9aafa448be5910e6a00dc6d1ee0
kimtaejun97 <kim_taejun@naver.com>
2022-01-24 14:53:44 +0900

    refactor: 우승자를 구하는 로직의 책임을 Judge에게 넘김.
    Judge.getWinners를 가지고 우승자를 구하고, GameView에서는 우승자를 받아 출력한다.

--- src/main/java/racinggame/view/GameView.java
+++ src/main/java/racinggame/view/GameView.java
@@ -3,6 +3,7 @@ package racinggame.view;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import racinggame.domain.Judge;
 import racinggame.domain.RacingCar;
 import racinggame.domain.Utils;
 
@@ -29,30 +30,10 @@ public class GameView {
         System.out.println(sb);
     }
 
-    private List<Integer> getCarPositions() {
-        return racingCars.stream()
-                .map(car -> car.getPosition())
-                .collect(Collectors.toList());
-    }
-
     public void printWinners() {
-        int max = getHighScore();
-        List<String> winners = getWinners(max);
+        List<String> winners = Judge.getWinners(racingCars);
 
         String result = winners.stream().collect(Collectors.joining(DELIMITER));
         System.out.println("최종 우승자: " + result);
-    }
-
-    private int getHighScore() {
-        return Collections.max(getCarPositions());
-    }
-
-
-
-    private List<String> getWinners(int max) {
-        return racingCars.stream()
-                .filter(car -> car.getPosition() == max)
-                .map(car -> car.getCarName())
-                .collect(Collectors.toList());
     }
 }
