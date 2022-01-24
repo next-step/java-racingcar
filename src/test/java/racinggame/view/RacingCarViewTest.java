6e7189338a4b1dd09c27e4f79592a282708f827b
kimtaejun97 <kim_taejun@naver.com>
2022-01-24 15:17:54 +0900

    test: RacingCarView 생성자 테스트

--- src/test/java/racinggame/view/RacingCarViewTest.java
+++ src/test/java/racinggame/view/RacingCarViewTest.java
@@ -1,4 +1,19 @@
+package racinggame.view;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.Test;
+import racinggame.domain.RacingCar;
+
 class RacingCarViewTest {
-  
+
+    @Test
+    void 생성자_테스트() throws NoSuchFieldException, IllegalAccessException {
+        RacingCarView racingCarView = new RacingCarView(new RacingCar("car1"));
+
+        Field racingCar = RacingCarView.class.getDeclaredField("racingCar");
+        racingCar.setAccessible(true);
+        assertNotNull(racingCar.get(racingCarView));
+    }
 }
