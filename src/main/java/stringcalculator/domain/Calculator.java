743c90027e9ca0a46168aa28e89cff5a63f2381a
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-25 08:06:52 +0900

    refactor(stringCalculator): 네이밍 변경
    - parsing된 유저 인풋에 대한 네이밍 변경

--- src/main/java/stringcalculator/domain/Calculator.java
+++ src/main/java/stringcalculator/domain/Calculator.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class Calculator {
 
     private int result;
-    private final List<String> userInput;
+    private final List<String> parsingInputFormat;
 
-    private Calculator(final List<String> userInput) {
-        this.userInput = userInput;
+    private Calculator(final List<String> parsingInputFormat) {
+        this.parsingInputFormat = parsingInputFormat;
         this.result = 0;
         process();
     }
@@ -18,17 +18,17 @@ public class Calculator {
     }
 
 
-    public static Calculator of(final List<String> userInput) {
-        return new Calculator(userInput);
+    public static Calculator of(final List<String> parsingInputFormat) {
+        return new Calculator(parsingInputFormat);
     }
 
     private void process() {
 
-        this.result = Integer.parseInt(userInput.get(0));
+        this.result = Integer.parseInt(parsingInputFormat.get(0));
 
-        for (int index = 1; index < userInput.size() - 1; index += 2) {
-            this.result = Operator.of(userInput.get(index))
-                .operate(this.result, Integer.parseInt(userInput.get(index + 1)));
+        for (int index = 1; index < parsingInputFormat.size() - 1; index += 2) {
+            this.result = Operator.of(parsingInputFormat.get(index))
+                .operate(this.result, Integer.parseInt(parsingInputFormat.get(index + 1)));
         }
 
 
