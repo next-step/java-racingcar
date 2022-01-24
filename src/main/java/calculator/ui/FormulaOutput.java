bb30654134988d4df312247aedd0ecfd42f99b97
YangWoo Yu <yuyw0712@naver.com>
2022-01-24 17:14:51 +0900

    feat: FormulaOutput 클래스로 출력 부분 별도 분리

---
+++ src/main/java/calculator/ui/FormulaOutput.java
@@ -0,0 +1,14 @@
+package calculator.ui;
+
+public class FormulaOutput {
+
+    private FormulaOutput() {
+
+    }
+    public static final String ERROR_MESSAGE = "[ERROR] ";
+    public static final String ERROR_NULL_OR_EMPTY = "입력값이 Null이거나 빈 공백 문자입니다!";
+    public static final String REQUIRE_FORMULA_INPUT_MESSAGE = "계산할 연산식을 입력해주세요.";
+    public static final String ERROR_NOT_OPERATOR = "사칙연산 기호가 아닙니다.";
+    public static final String ERROR_NOT_NUMBER = "피연산자가 숫자가 아닙니다.";
+
+}
