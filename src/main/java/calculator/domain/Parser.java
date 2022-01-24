af034555ba3c5f224bf8c785f004b0d298299369
xxell8 <cell8pepper@gmail.com>
2022-01-24 10:12:32 +0900

    refactor: Change Parsing Logic to regular expression
    - To reduce depth, Delete 'for' loop.
    - Instead, extract number and operator with REGEX.

---
+++ src/main/java/calculator/domain/Parser.java
@@ -0,0 +1,19 @@
+package calculator.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+    private static final Pattern NUMBER_OR_OPERATOR = Pattern.compile("[+*/-]|[0-9]+");
+
+    public static List<String> parseInput(String userInput) {
+        List<String> result = new ArrayList<>();
+        Matcher matcher = NUMBER_OR_OPERATOR.matcher(userInput);
+        while (matcher.find()) {
+            result.add(matcher.group());
+        }
+        return result;
+    }
+}
