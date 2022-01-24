3bd1372a9c2f5819f985d455fdf4b969c349c37f
PandaHun <gnsgnsek@gmail.com>
2022-01-24 09:49:36 +0900

    refactor(Calculator): Remove attributes in Calculator reducing side effects
    - 인스턴스 변수 참조를 해제

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,54 +1,30 @@
 package calculator.domain;
 
 import calculator.util.StringUtils;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Calculator {
 
     private static final int INTERVAL_OF_INDEX = 2;
     private static final int START_OPERAND_INDEX = 0;
-    private static final int START_OPERATION_INDEX = 1;
-    private static final int INTERVAL_OF_NUMBER_INDEX = 1;
-
-    private List<Operand> operands;
-    private List<Operation> operations;
+    private static final int OPERATION_INDEX = 1;
 
     public Calculator() {
-        operands = new ArrayList<>();
-        operations = new ArrayList<>();
     }
 
     public int calculate(String expression) {
         String[] splitExpression = StringUtils.splitExpression(expression);
         validateIdleExpression(splitExpression);
-        extractOperand(splitExpression);
-        extractOperation(splitExpression);
-        calculateExpression();
-        return operands.get(operands.size() - 1)
-            .getNumber();
+        return calculateExpression(splitExpression);
     }
 
-    private void calculateExpression() {
-        Operand left = operands.get(START_OPERAND_INDEX);
-        for (int index = START_OPERAND_INDEX; index < operations.size(); index++) {
-            Operation operation = operations.get(index);
-            Operand right = operands.get(index + INTERVAL_OF_NUMBER_INDEX);
+    private int calculateExpression(String[] expression) {
+        Operand left = new Operand(expression[START_OPERAND_INDEX]);
+        for (int index = START_OPERAND_INDEX; index < expression.length - INTERVAL_OF_INDEX; index += INTERVAL_OF_INDEX) {
+            Operation operation = Operation.from(expression[index + OPERATION_INDEX]);
+            Operand right = new Operand(expression[index + INTERVAL_OF_INDEX]);
             left = new Operand(operation.operate(left, right));
         }
-        operands.add(left);
-    }
-
-    private void extractOperation(String[] splitExpression) {
-        for (int index = START_OPERATION_INDEX; index < splitExpression.length; index += INTERVAL_OF_INDEX) {
-            operations.add(Operation.from(splitExpression[index]));
-        }
-    }
-
-    private void extractOperand(String[] splitExpression) {
-        for (int index = 0; index < splitExpression.length; index += INTERVAL_OF_INDEX) {
-            operands.add(new Operand(splitExpression[index]));
-        }
+        return left.getNumber();
     }
 
     private void validateIdleExpression(String[] splitExpression) {
