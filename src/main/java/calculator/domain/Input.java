f0f294be9440a601d3793070a898f4048b08efe6
xxell8 <cell8pepper@gmail.com>
2022-01-24 13:34:14 +0900

    feat: Add Default Validation
    - 공백 입력인지 확인
    - 숫자, 연산자, 공백 외 문자가 있는지 확인

--- src/main/java/calculator/domain/Input.java
+++ src/main/java/calculator/domain/Input.java
@@ -1,42 +1,14 @@
 package calculator.domain;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Input {
 
-    private String userInput;
-    private List<String> result = new ArrayList<String>();
-
-    public void getInput() {
+    public static String getInput() {
+        String userInput;
         Scanner sc = new Scanner(System.in);
         userInput = sc.nextLine();
-        Validator.isEmpty(userInput);
-    }
-
-    public void parseInput() throws IllegalArgumentException {
-        String input = userInput.replaceAll(" ", "");
-        String temp = "";
-
-        for (int i = 0; i < input.length(); i++) {
-            char element = input.charAt(i);
-            if (Character.isDigit(element)) {
-                temp += element;
-            } else if (element == '+' || element == '-' || element == '*' || element == '/') {
-                result.add(temp);
-                result.add(Character.toString(element));
-                temp = "";
-            } else {
-                throw new IllegalArgumentException("숫자와 연산자, 공백문자만 입력할 수 있습니다.");
-            }
-        }
-        if (temp.length() > 0) {
-            result.add(temp);
-        }
-    }
-
-    public List<String> getResult() {
-        return result;
+        new Validator(userInput);
+        return userInput;
     }
 }
