c002e32646a66ce5a97e6a27bfb3aa6cb6943309
hyojeong <qkrgywjd456@naver.com>
2022-01-24 13:23:45 +0900

    feat: StringProcessor parseInput 함수 제거
    parseInput 함수를 대체하는 함수 ParseInputNumber, parseInputOperands

--- src/main/java/strcalculator/domain/StringProcessor.java
+++ src/main/java/strcalculator/domain/StringProcessor.java
@@ -25,28 +25,6 @@ public class StringProcessor {
             throw new IllegalArgumentException("연산 가능한 수식을 입력해주세요.");
         }
     }
-
-    public void parseInput() throws IllegalArgumentException {
-        String input = userInput.replaceAll(" ", "");
-        String temp = "";
-
-        for (int i = 0; i < input.length(); i++) {
-            char element = input.charAt(i);
-            if (Character.isDigit(element)) {
-                temp += element;
-            } else if (element == '+' || element == '-' || element == '*' || element == '/') {
-                result.add(temp);
-                result.add(Character.toString(element));
-                temp = "";
-            } else {
-                throw new IllegalArgumentException("숫자와 연산자, 공백문자만 입력할 수 있습니다.");
-            }
-        }
-        if (temp.length() > 0) {
-            result.add(temp);
-        }
-    }
-
     public void validation(){
         boolean matches = userInput.matches("[+\\-*/]|[0-9]+");
         if(matches==false){
