a8eb0aea9a2e169121e1467d313369073940bf15
xxell8 <cell8pepper@gmail.com>
2022-01-24 11:04:36 +0900

    feat: Add Logic to Verify Input Format
    - 숫자와 연산자, 공백이 아닌 문자열이 포함되었는지 확인

--- src/main/java/calculator/domain/Validator.java
+++ src/main/java/calculator/domain/Validator.java
@@ -1,10 +1,24 @@
 package calculator.domain;
 
+import java.util.regex.Pattern;
+
 public class Validator {
+
+    public Validator(String userInput) {
+        isEmpty(userInput);
+        canCalculate(userInput);
+    }
 
     public static void isEmpty(String userInput) {
         if (userInput == null || userInput.trim().length() == 0) {
             throw new IllegalArgumentException("[ERROR] 연산 가능한 수식을 입력해주세요.");
+        }
+    }
+
+    public static void canCalculate(String userInput) {
+        String cannotCalculate = "(.*)[^\\s\\d+*/-](.*)";
+        if (userInput.matches(cannotCalculate)) {
+            throw new IllegalArgumentException("[ERROR] 숫자, 연산자, 공백 문자만 허용됩니다.");
         }
     }
 }
