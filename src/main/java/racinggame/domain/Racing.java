a2e71f0d068a0506ec7fe1c04b51531894e09faa
xxell8 <cell8pepper@gmail.com>
2022-01-24 16:45:34 +0900

    refactor: Add constructor and Change funcs managing game
    - 자동차 등록 메서드 생성
    - 결과 한 번에 출력하도록 변경

--- src/main/java/racinggame/domain/Racing.java
+++ src/main/java/racinggame/domain/Racing.java
@@ -4,61 +4,88 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class PlayRacingGame {
+public class Racing {
+    private static final int MAX_RANGE = 10;
+    private static final int FORWARD_NUMBER = 4;
 
-    public int makeRandomNumber() {
-        Random random = new Random();
-        int randomValue = random.nextInt(9);
-        return randomValue;
+    private List<Car> cars = new ArrayList<>();
+    private StringBuilder result = new StringBuilder("");
+
+    public Racing(List<String> carNames, int tryCount) {
+        registerCar(carNames);
+        while (tryCount > 0) {
+            move();
+            calculateResult();
+            tryCount--;
+        }
     }
 
-    public void move(List<Car> cars) {
+    private void registerCar(List<String> carNames) {
+        for (String carName : carNames) {
+            cars.add(Car.of(carName));
+        }
+    }
+
+    private void move() {
         for (Car car : cars) {
             int randomNumber = makeRandomNumber();
-            if (randomNumber >= 4) {
-                int location = car.getLocation();
-                location++;
-                car.setLocation(location);
+            if (randomNumber >= FORWARD_NUMBER) {
+                car.moveForward();
             }
         }
-
     }
 
-    public void printCarsLocation(List<Car> cars) {
+    private int makeRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(MAX_RANGE);
+    }
+
+    private void calculateResult() {
         for (Car car : cars) {
-            System.out.print(car.getCarName() + " : " + "");
+            result.append(car.getName() + " : ");
             for (int i = 0; i < car.getLocation(); i++) {
-                System.out.print('-');
+                result.append('-');
             }
-            System.out.println();
+            result.append("\n");
         }
-        System.out.println();
+        result.append("\n");
     }
+    
+//    public void printCarsLocation() {
+//        for (Car car : cars) {
+//            System.out.print(car.getName() + " : ");
+//            for (int i = 0; i < car.getLocation(); i++) {
+//                System.out.print('-');
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+//    }
 
 
-    public int findMaxLocation(List<Car> cars) {
-        int maxLocation = 0;
-        for (Car car : cars) {
-            maxLocation = Math.max(car.getLocation(), maxLocation);
-        }
-        return maxLocation;
-    }
-
-    public List<String> findWinner(List<Car> cars) {
-        List<String> winnerUsers = new ArrayList<>();
-        int maxLocation = findMaxLocation(cars);
-        for (Car car : cars) {
-            if (car.getLocation() == maxLocation) {
-                winnerUsers.add(car.getCarName());
-            }
-        }
-        return winnerUsers;
-    }
-
-    public void printWinner(List<String> winnerUsers) {
-        System.out.print("최종 우승자: ");
-        String winnerList = String.join(", ", winnerUsers);
-        System.out.println(winnerList);
-    }
+//    public int findMaxLocation() {
+//        int maxLocation = 0;
+//        for (Car car : cars) {
+//            maxLocation = Math.max(car.getLocation(), maxLocation);
+//        }
+//        return maxLocation;
+//    }
+//
+//    public List<String> findWinner() {
+//        List<String> winnerUsers = new ArrayList<>();
+//        int maxLocation = findMaxLocation();
+//        for (Car car : cars) {
+//            if (car.getLocation() == maxLocation) {
+//                winnerUsers.add(car.getName());
+//            }
+//        }
+//        return winnerUsers;
+//    }
+//
+//    public void printWinner(List<String> winnerUsers) {
+//        System.out.print("최종 우승자: ");
+//        String winnerList = String.join(", ", winnerUsers);
+//        System.out.println(winnerList);
+//    }
 
 }
