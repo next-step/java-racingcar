7af595461cc2c7ccff508ee3a51c2ae3466203a5
0JUUU <clleo97@naver.com>
2022-01-25 05:09:56 +0900

    feat: 자동차 이름의 길이가 제약조건보다 길 경우에 대한 테스트코드 작성

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -1,7 +1,15 @@
 package racing.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.Test;
 
 class CarTest {
 
+    @Test
+    void 자동차_이름_길이_확인() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new Car("abcdef"));
+    }
 }
