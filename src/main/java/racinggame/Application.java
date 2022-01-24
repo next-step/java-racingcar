a8cb4493a809b7c28f85b09bab610988d6fed751
xxell8 <cell8pepper@gmail.com>
2022-01-24 17:07:00 +0900

    refactor: Adjust main function according to class change

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,32 +1,20 @@
 package racinggame;
 
 import java.util.List;
-import racinggame.domain.Car;
-import racinggame.domain.Cars;
 import racinggame.domain.GameInit;
-import racinggame.domain.PlayRacingGame;
+import racinggame.domain.Racing;
+import racinggame.domain.Winner;
 
 public class Application {
 
     public static void main(String[] args) {
-        //게임 초기화
         GameInit gameInit = new GameInit();
-        String[] carNames = gameInit.inputPlayerName();
+        List<String> carNames = gameInit.inputCarName();
         int playGameNumber = gameInit.inputTryNumber();
 
-        Cars cars = new Cars();
-        List<Car> carList = cars.createCarList(carNames);
-        //게임 시작
-        PlayRacingGame playRacingGame = new PlayRacingGame();
-        System.out.println("실행 결과");
-        for (; playGameNumber > 0; playGameNumber--) {
-            playRacingGame.move(carList);
-            playRacingGame.printCarsLocation(carList);
-
-        }
-        List<String> winner = playRacingGame.findWinner(carList);
-        playRacingGame.printWinner(winner);
+        Racing game = new Racing(carNames, playGameNumber);
+        System.out.println(game.getResult());
+        Winner.print(game.getCarInfo());
     }
-
 
 }
