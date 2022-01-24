de64d7d262683d392e4fdb2dda38a6bf8b4d2063
hyojeong <qkrgywjd456@naver.com>
2022-01-24 14:22:23 +0900

    test: create cars 전진 유무 테스트

--- src/test/java/racinggame/domain/CarsTest.java
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -1,7 +1,10 @@
 package racinggame.domain;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +18,18 @@ class CarsTest {
         Cars cars = new Cars(carNames);
         List<Car> carList = cars.getCars();
         Assertions.assertThat(2).isEqualTo(carList.size());
+
+    }
+
+    @DisplayName("Cars 움직임 테스트")
+    @Test
+    public void moveCarsTest(){
+        String []carNames= {"a","b"};
+        Cars cars = new Cars(carNames);
+        List<Integer> randomValue =Arrays.asList(1, 5);
+        cars.move(randomValue);
+        Assertions.assertThat(cars.getCars().get(0).getLocation()).isEqualTo(0);
+        Assertions.assertThat(cars.getCars().get(1).getLocation()).isEqualTo(1);
 
     }
 
