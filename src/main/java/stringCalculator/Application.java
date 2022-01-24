8274bb7d0fca1b93c6ca6b6417aa780403272c79
fpal95 <snc7303@gmail.com>
2022-01-24 17:27:15 +0900

    Fix: result의 변수 선언과 초기화 수정

--- src/main/java/stringCalculator/Application.java
+++ src/main/java/stringCalculator/Application.java
@@ -3,13 +3,13 @@ package stringCalculator;
 import java.util.List;
 
 public class Application {
+
     public static void main(String[] args) {
-        int result;
         User user = new User();
-        final List<String> userInput= user.getUserInput();
+        final List<String> userInput = user.getUserInput();
         System.out.println(userInput);
-        Calculator calculator= new Calculator();
-        result= calculator.calculateByOperator(userInput);
+        Calculator calculator = new Calculator();
+        int result = calculator.calculateByOperator(userInput);
         System.out.println(result);
     }
 }
