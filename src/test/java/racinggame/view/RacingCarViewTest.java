c388fe691dc8af1f783df29be85c50f877648191
kimtaejun97 <kim_taejun@naver.com>
2022-01-24 15:36:38 +0900

    test: racingCarView.drawCurrPosition 기능 테스트
    - 현재 자동차의 위치를 그려 잘 넘겨주는지.

--- src/test/java/racinggame/view/RacingCarViewTest.java
+++ src/test/java/racinggame/view/RacingCarViewTest.java
@@ -1,10 +1,14 @@
 package racinggame.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.reflect.Field;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racinggame.domain.RacingCar;
+import racinggame.domain.Utils;
 
 class RacingCarViewTest {
 
@@ -15,5 +19,20 @@ class RacingCarViewTest {
         Field racingCar = RacingCarView.class.getDeclaredField("racingCar");
         racingCar.setAccessible(true);
         assertNotNull(racingCar.get(racingCarView));
+    }
+
+    @ValueSource(ints = {1,2,3,4,5})
+    @ParameterizedTest
+    void drawCurrPosition(int position) {
+        RacingCar car1 = new RacingCar("car1", position);
+        RacingCarView racingCarView = new RacingCarView(car1);
+
+        String currPositionView = racingCarView.drawCurrPosition();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("car1 : ")
+                .append(Utils.repeat("-", position));
+
+        assertThat(currPositionView.equals(sb.toString()));
     }
 }
