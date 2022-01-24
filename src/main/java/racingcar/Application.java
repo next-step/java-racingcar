2cceb4314fe9a5d8544e598d167e2c2ffeb2719a
hscom96 <hscom96@gmail.com>
2022-01-24 18:25:32 +0900

    refactor: gameLauncher print 메서드 역할 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,8 @@ package racingcar;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Car;
 import racingcar.service.GameLauncher;
 import racingcar.ui.RacingCarInput;
 
@@ -10,14 +12,29 @@ public class Application {
     public static void main(String[] args) {
         RacingCarInput racingCarInput = new RacingCarInput();
         List<String> carNameList = racingCarInput.getCarName();
+
         int raceCount = racingCarInput.getRaceCount();
+
+        StringBuilder gameLog = new StringBuilder();
         GameLauncher gameLauncher = new GameLauncher(carNameList);
         while (0 < raceCount) {
             gameLauncher.moveForwardAll();
-            gameLauncher.printRacing();
+            List<Car> carList = gameLauncher.getRacingCarList();
+
+            for(Car car : carList){
+                gameLog.append(car);
+                gameLog.append("\n");
+            }
+            gameLog.append("------------------------------\n");
+
             raceCount--;
-            System.out.println("------------------------------");
         }
-        gameLauncher.printWinner();
+
+        List<String> winnerList = gameLauncher.getWinner().stream()
+            .map(Car::getName)
+            .collect(Collectors.toList());
+        gameLog.append(String.join(",",winnerList));
+
+        System.out.println(gameLog);
     }
 }
--- src/test/java/racingcar/service/GameLauncherTest.java
+++ src/test/java/racingcar/service/GameLauncherTest.java
@@ -4,15 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mockStatic;
 
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import racingcar.Application;
 import racingcar.domain.Car;
-import racingcar.domain.RacingCar;
 import racingcar.domain.RandomConfigTest;
 import racingcar.util.RandomUtil;
 
