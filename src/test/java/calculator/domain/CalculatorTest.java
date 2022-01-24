f269ce0af760ca43052691d4b8602d734abeb124
Dami Kim <dami.k.dev@gmail.com>
2022-01-24 23:39:42 +0900

    feat: CalculatorTest 내 사칙연산 테스트 추가

---
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -0,0 +1,22 @@
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import calculator.util.Parser;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void 사칙연산_테스트() {
+        String expression = "1 + 2 / 3 * 4 - 3";
+        List<String> splitExpression = Parser.splitExpression(expression);
+        Calculator calculator = new Calculator(splitExpression);
+        int result = calculator.getResult();
+
+        assertThat(result).isEqualTo(1);
+    }
+
+}
