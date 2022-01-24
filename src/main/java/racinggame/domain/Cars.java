d97b13c0f582f0e5018f0547e405a66558fc44d6
hyojeong <qkrgywjd456@naver.com>
2022-01-24 16:42:47 +0900

    refactor: 매직넘버 상수치환

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Cars {
 
     private List<Car> cars = new ArrayList<>();
-
+    private static final int GO_POSSIBLE_VALUE=4;
     public Cars(String[] carNames) {
         for (int carNum = 0; carNum < carNames.length; carNum++) {
             cars.add(new Car(carNames[carNum]));
@@ -28,7 +28,7 @@ public class Cars {
 
     public void move(List<Integer> randomNumberList) {
         for (int i=0;i< randomNumberList.size() ; i++) {
-            if (randomNumberList.get(i) >= 4) {
+            if (randomNumberList.get(i) >= GO_POSSIBLE_VALUE) {
                 Car car = cars.get(i);
                 int location = car.getLocation();
                 location++;
