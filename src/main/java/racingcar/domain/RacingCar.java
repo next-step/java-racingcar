b4daae6de28449ffadc0ea1f0342226efe7b5dbc
hyeonsu <hs.kim@sk.com>
2022-01-24 18:33:23 +0900

    refactor: 변수 및 메서드 위치 수정

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -4,30 +4,17 @@ import racingcar.util.RandomUtil;
 
 public class RacingCar implements Car {
 
-    private final String name;
-
-    private int position = 0;
-
     private static final int RANDOM_START = 0;
-
     private static final int RANDOM_END = 9;
-
     private static final int THRESHOLD = 4;
-
     private static final int NAME_SIZE = 5;
 
+    private final String name;
+    private int position = 0;
 
     public RacingCar(String name) {
         isCorrectNameSize(name);
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPosition() {
-        return position;
     }
 
     public void moveForward() {
@@ -45,6 +32,14 @@ public class RacingCar implements Car {
         if (NAME_SIZE < name.length()) {
             throw new IllegalArgumentException("자동차 이름의 길이가 5를 초과했습니다!");
         }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 
     @Override
