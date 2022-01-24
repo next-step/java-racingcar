667e13877abe96e007901ad566b029739a056af3
fpal95 <snc7303@gmail.com>
2022-01-25 00:16:29 +0900

    Feat: 에러 케이스 추가
    연속으로 숫자가 나오거나, 연산자가 나오는 경우 에러 발생

--- src/main/java/stringCalculator/User.java
+++ src/main/java/stringCalculator/User.java
@@ -8,22 +8,38 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class User {
-    public List<String> getUserInput(){
-        Scanner scanner= new Scanner(System.in);
-        String []userInputTokens;
-        String userInput= scanner.nextLine();
 
-        userInputTokens=userInput.split(" ");
+    public List<String> getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        String[] userInputTokens;
+        String userInput = scanner.nextLine();
 
-        if (userInputTokens.length<3){
+        userInputTokens = userInput.split(" ");
+
+        if (userInputTokens.length < 3) {
             throw new IllegalArgumentException("공백은 허용되지 않습니다");
         }
-        for (int i=0; i<userInputTokens.length;i++){
-            if (userInputTokens[i].equals("")){
+
+        for (String userInputToken : userInputTokens) {
+            if (userInputToken.equals("")) {
                 throw new IllegalArgumentException("공백이 존재합니다");
+            }
+        }
+
+        for (int i = 0; i < userInputTokens.length; i += 2) {
+            if (!userInputTokens[i].matches("[0-9]+")) {
+                throw new IllegalArgumentException("잘못된 입력입니다");
+            }
+        }
+
+        for (int i = 1; i < userInputTokens.length; i += 2) {
+            if (!userInputTokens[i].contains("+")
+                && !userInputTokens[i].contains("-")
+                && !userInputTokens[i].contains("*")
+                && !userInputTokens[i].contains("/")) {
+                throw new IllegalArgumentException("잘못된 입력입니다");
             }
         }
         return Arrays.asList(userInputTokens);
     }
-
 }
