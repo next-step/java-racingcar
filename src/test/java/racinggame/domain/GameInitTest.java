f7c66c445dafff219e47e368acedf496efe858aa
hyojeong <qkrgywjd456@naver.com>
2022-01-24 13:34:21 +0900

    feat: 자동차 문자길이 제약조건 테스트제거

--- src/test/java/racinggame/domain/GameInitTest.java
+++ src/test/java/racinggame/domain/GameInitTest.java
@@ -8,13 +8,5 @@ import org.junit.jupiter.api.Test;
 
 class GameInitTest {
 
-    @DisplayName("각 자동차 이름은 5글자를 초과할 수 없다.")
-    @Test
-    public void validationPlayerNameLength() {
-        GameInit gameInit = new GameInit();
-        String[] strings = {"abcdef"};
-        boolean isCorrect = gameInit.validationStringLength(strings);
-        Assertions.assertThat(isCorrect).isFalse();
-    }
 
 }
