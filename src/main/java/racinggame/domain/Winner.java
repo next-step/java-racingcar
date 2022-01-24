b2d6befadd8018cdddee98babfdff2c7fbf54192
xxell8 <cell8pepper@gmail.com>
2022-01-24 17:06:02 +0900

    refactor: Separate Logic calculates and prints winner

---
+++ src/main/java/racinggame/domain/Winner.java
@@ -0,0 +1,33 @@
+package racinggame.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winner {
+    private static final String DELIMITER = ", ";
+
+    public static void print(List<Car> cars) {
+        List<String> winners = findWinner(cars);
+        String winnerList = String.join(DELIMITER, winners);
+        System.out.println("최종 우승자: " + winnerList);
+    }
+
+    private static List<String> findWinner(List<Car> cars) {
+        List<String> winnerUsers = new ArrayList<>();
+        int maxLocation = findMaxLocation(cars);
+        for (Car car : cars) {
+            if (car.getLocation() == maxLocation) {
+                winnerUsers.add(car.getName());
+            }
+        }
+        return winnerUsers;
+    }
+
+    private static int findMaxLocation(List<Car> cars) {
+        int maxLocation = 0;
+        for (Car car : cars) {
+            maxLocation = Math.max(car.getLocation(), maxLocation);
+        }
+        return maxLocation;
+    }
+}
