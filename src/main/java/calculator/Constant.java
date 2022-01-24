70764852970317c33228e76d075f483398ac313e
Dami Kim <dami.k.dev@gmail.com>
2022-01-24 23:25:29 +0900

    refactor: 연산자 index 상수 네이밍 수정

--- src/main/java/calculator/Constant.java
+++ src/main/java/calculator/Constant.java
@@ -6,6 +6,6 @@ public class Constant {
     public static final char MULTIPLY = '*';
     public static final char DIVIDE = '/';
     public static final String RESULT_TEXT = "연산의 결과는 : ";
-    public static final int INIT_ARITHMETIC = 1;
-    public static final int NEXT_ARITHMETIC = 2;
+    public static final int INIT_OPERATOR_INDEX = 1;
+    public static final int NEXT_OPERATOR_DIFFERENCE = 2;
 }
--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -8,9 +8,11 @@ public class Calculator {
     private int result;
     private final List<String> splitedInput;
 
-    public Calculator(List<String> splitedInput) {
-        this.splitedInput = splitedInput;
-        result = Integer.parseInt(splitedInput.get(0));
+    public Calculator(List<String> splitExpression) {
+        result = Integer.parseInt(splitExpression.get(0));
+
+        validateExpression(splitExpression);
+        calculateNumbers(splitExpression);
     }
 
     public int getResult() {
@@ -25,7 +27,12 @@ public class Calculator {
             char arithmetic = splitedInput.get(i).charAt(0);
             int number = Integer.parseInt(splitedInput.get(i + 1));
 
-            switch (arithmetic) {
+        for (int i = Constant.INIT_OPERATOR_INDEX; i < splitExpression.size();
+            i += Constant.NEXT_OPERATOR_DIFFERENCE) {
+            char operator = splitExpression.get(i).charAt(0);
+            int number = Integer.parseInt(splitExpression.get(i + 1));
+
+            switch (operator) {
                 case Constant.ADD:
                     addNumbers(number);
                     break;
