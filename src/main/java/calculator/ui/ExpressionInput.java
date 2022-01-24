e65433c7b5d245205b8325b4db99342a54f74fa2
jihyunhillpark <girl9505@naver.com>
2022-01-24 20:45:07 +0900

    fix: fromString() -> from():Operation
    리턴값 변화에 따른 익셉션 처리

--- src/main/java/calculator/ui/ExpressionInput.java
+++ src/main/java/calculator/ui/ExpressionInput.java
@@ -38,10 +38,7 @@ public class ExpressionInput {
 
     private static void isNotOperator(List<String> formular) {
         for (int i = 1; i < formular.size(); i += 2) {
-            Operation.fromString(formular.get(i))
-                .orElseThrow(() ->
-                    new IllegalArgumentException("사칙연산 기호가 아닙니다.")
-                );
+            Operation.from(formular.get(i));
         }
     }
 
--- src/main/java/calculator/util/Operation.java
+++ src/main/java/calculator/util/Operation.java
@@ -1,9 +1,6 @@
 package calculator.util;
 
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public enum Operation {
     PLUS("+") {
@@ -40,14 +37,13 @@ public enum Operation {
         return symbol;
     }
 
-    private static final Map<String, Operation> stringToEnum =
-        Stream.of(values()).collect(Collectors.
-            toMap(Object::toString, e -> e));
 
     /**
-     * fromString은 toString이 반환하는 문자열을 해당 열거 타입 상수로 변환해주는 기능
+     * from은 toString이 반환하는 문자열을 해당 열거 타입 상수로 변환해주는 기능
      */
-    public static Optional<Operation> fromString(String symbol) {
-        return Optional.ofNullable(stringToEnum.get(symbol));
+    public static Operation from(String symbol) {
+        return Arrays.stream(values())
+            .filter(operation -> symbol.equals(operation.symbol))
+            .findFirst().orElseThrow(() -> new IllegalArgumentException("[ERROR] 사칙 연산자가 아닙니다!"));
     }
 }
