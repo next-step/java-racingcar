c54b9cc5552210e720c206de09b221800ecd9e9d
0JUUU <clleo97@naver.com>
2022-01-25 01:48:58 +0900

    refactor: final 선언이 가능한 변수에 final 추가

--- src/main/java/racing/domain/UserInput.java
+++ src/main/java/racing/domain/UserInput.java
@@ -14,11 +14,10 @@ public class UserInput {
     }
 
     public static List<String> getCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String input = SCANNER.nextLine();
-
         try {
-            List<String> names = splitStringByComma(input);
+            System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+            final String input = SCANNER.nextLine();
+            final List<String> names = splitStringByComma(input);
             checkCarCount(names);
             return names;
         } catch (IllegalArgumentException e) {
@@ -27,27 +26,27 @@ public class UserInput {
         }
     }
 
-    public static List<String> splitStringByComma(String input) {
-        List<String> names = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(input, ",");
+    public static List<String> splitStringByComma(final String input) {
+        final List<String> names = new ArrayList<>();
+        final StringTokenizer st = new StringTokenizer(input, ",");
 
         while (st.hasMoreTokens()) {
-            String name = st.nextToken().trim();
+            final String name = st.nextToken().trim();
             names.add(name);
         }
         return names;
     }
 
-    public static void checkCarCount(List<String> names) {
+    public static void checkCarCount(final List<String> names) {
         if (names.size() < 1) {
             throw new IllegalArgumentException("[ERROR] 최소 1대 이상의 자동차가 필요합니다.");
         }
     }
 
     public static int getTryNumber() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
         try {
-            int tryNumber = Integer.parseInt(SCANNER.nextLine());
+            System.out.println("시도할 횟수는 몇 회인가요?");
+            final int tryNumber = Integer.parseInt(SCANNER.nextLine());
             checkTryNumber(tryNumber);
             return tryNumber;
         } catch (NumberFormatException e) {
@@ -59,7 +58,7 @@ public class UserInput {
         }
     }
 
-    public static void checkTryNumber(int tryNumber) {
+    public static void checkTryNumber(final int tryNumber) {
         if (tryNumber <= 0) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수는 최소 1회 이상이어야 합니다.");
         }
