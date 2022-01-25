f79c023e027c4d260b82b685ea99b9db953ac5f7
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-25 16:56:51 +0900

    style(racingCar): MVC구조 패턴을 위한 파일 변경 & 이동

--- src/main/java/racingcar/common/exception/InputValidationException.java
+++
@@ -1,9 +0,0 @@
-package racingcar.common.exception;
-
-public class InputValidationException extends RuntimeException {
-
-    public InputValidationException(String message) {
-        super(message);
-    }
-}
-
