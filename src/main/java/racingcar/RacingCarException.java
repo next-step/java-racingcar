5d3340c6d63749d7a00dfc9cced7d14e15661bdb
Gal2o <hunmin8788@gmail.com>
2022-01-25 23:47:32 +0900

    refactor(Racing): 입력 값의 예외를 공통으로 처리하도록 RacingCarException 생성

---
+++ src/main/java/racingcar/RacingCarException.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class RacingCarException extends IllegalArgumentException {
+
+    public static final String CAR_NAME_INVALID = "자동차 이름은 5글자를 넘을 수 없습니다.";
+    public static final String CAR_NAME_EMPTY = "입력 값이 존재하지 않습니다.";
+
+    public RacingCarException(String message) {
+        super(message);
+    }
+}
