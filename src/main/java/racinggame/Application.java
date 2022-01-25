9078eb6cd6b2cf058b4317cae3e0ecccc2846f7c
hyojeong <qkrgywjd456@naver.com>
2022-01-25 21:37:40 +0900

    refactor: Cars move func 수정으로 인해 makeRandomNumbers func 사용 x
    -main 에서 사용하지 않아도 되는 RandomNumber 생성로직을 Cars move func 로직에 추가
    - RandomNumber은 Car객체의 location 값 변경에만 전진 조건으로 사용됨

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -13,25 +13,21 @@ public class Application {
 
     public static void main(String[] args) {
         GameInit gameInit = new GameInit();
-        Random random = new Random();
         ResultView resultView = new ResultView();
         PlayRacingGame playRacingGame = new PlayRacingGame();
-
         String[] carNames = gameInit.inputPlayerName();
         Cars cars = new Cars(carNames);
 
         int playGameNumber = gameInit.inputTryNumber();
         int maxLocation;
         List<Car> carList = cars.getCars();
-
         System.out.println("실행 결과");
         for (; playGameNumber > 0; playGameNumber--) {
-            List<Integer> randomNumberList = playRacingGame.makeRandomNumbers(carList.size(),
-                random);
-            cars.move(randomNumberList);
-            resultView.printCarsLocation(carList);
+            cars.move();
+            resultView.printCarsLocation(cars);
 
         }
+
         maxLocation = cars.findMaxLocation();
         List<Winner> winnerList = playRacingGame.findWinner(carList, maxLocation);
         resultView.printWinner(winnerList);
