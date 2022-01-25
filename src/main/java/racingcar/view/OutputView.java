1f6e3fe0a1fc25f3b7ac1c9bf207270a0e2c51ec
Ji In Lee <loveted0225@naver.com>
2022-01-25 16:51:01 +0900

    refactor(OutputView): Message에 있떤 메세지 변수를 사용하는 위치로 이동

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,17 +1,19 @@
 package racingcar.view;
 
 import java.util.ArrayList;
-import racingcar.model.Car;
-import racingcar.util.Message;
 
 public class OutputView {
 
+    private final String GAME_RESULT_MESSAGE = "출력결과";
+    private final String WINNER_MESSAGE = "최종 우승자: ";
+
     public void printResult(String carName, String position) {
+        System.out.println(GAME_RESULT_MESSAGE);
         System.out.println(carName + ": " + position);
     }
 
     public void printWinner(ArrayList<String> winnerList) {
-        System.out.print(Message.WINNER_MESSAGE);
+        System.out.print(WINNER_MESSAGE);
         System.out.print(String.join(",", winnerList));
     }
 }
