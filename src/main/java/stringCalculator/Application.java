1e4cd40e47f6c986ddfddc6842cbd191751903fc
fpal95 <snc7303@gmail.com>
2022-01-25 16:01:28 +0900

    Fix(Application): UserInputValue.java 생성자 추가

--- src/main/java/stringCalculator/Application.java
+++ src/main/java/stringCalculator/Application.java
@@ -5,8 +5,9 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
+        UserInputValue input = new UserInputValue();
         User user = new User();
-        final List<String> userInput = user.getUserInput();
+        final List<String> userInput = user.getUserInput(input.userInput());
         System.out.println(userInput);
         Calculator calculator = new Calculator();
         int result = calculator.calculateByOperator(userInput);
