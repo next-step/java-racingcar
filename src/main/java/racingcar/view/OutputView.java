b927f6acc146dbea051e65eb90bac34d53ddd858
G1.Jeon <g1.jeon@kakao.com>
2022-01-25 18:15:42 +0900

    refactor: Delegated winner printing method
    - 우승자 문자열 출력 역할 위임 (Car -> OutputView)
    
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,14 @@
 package racingcar.view;
 
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
 
 public class OutputView {
 
     private static final String CAR_RACER_SPLITTER = " : ";
     private static final String CAR_MOVE_CHAR = "-";
+    private static final String CAR_WINNER_DELIMITER = ", ";
 
     private OutputView() {}
 
@@ -18,11 +21,18 @@ public class OutputView {
         System.out.println(sb);
     }
 
-    public static void printWhoIsWinner(final String winners) {
+    public static void printWhoIsWinner(final Cars cars) {
+        String winners = winners(cars);
         System.out.print("최종 우승자 : " + winners);
     }
 
     public static void printTurnResult() {
         System.out.println("실행 결과");
+    }
+
+    private static String winners(final Cars cars) {
+        return cars.filterWinners().stream()
+            .map(Car::name)
+            .collect(Collectors.joining(CAR_WINNER_DELIMITER));
     }
 }
