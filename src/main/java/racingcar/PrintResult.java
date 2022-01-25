1a6477ba0523e49d9b71639e935f6e26327945ba
Gal2o <hunmin8788@gmail.com>
2022-01-25 23:51:37 +0900

    refactor(Racing): 출력을 담당하는 PrintResult 클래스 생성

---
+++ src/main/java/racingcar/PrintResult.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PrintResult {
+
+    public static final String DASH = "-";
+    public static final String WINNERS_IS = "최종 우승자: ";
+    public static final String COMMA = ", ";
+
+    public static void printMoveState(List<RacingCar> racingCar) {
+        racingCar.forEach(car -> {
+            System.out.print(car.getCarName() + ": ");
+
+            printCarDist(car.getCarDist());
+        });
+
+        System.out.println();
+    }
+
+    public static void printCarDist(int dist) {
+        do {
+            System.out.print(DASH);
+            dist--;
+        } while (dist > 0);
+
+        System.out.println();
+    }
+
+    public static void printGameResult(List<RacingCar> winners) {
+        System.out.println(WINNERS_IS + winners.stream()
+            .map(RacingCar::getCarName)
+            .collect(Collectors.joining(COMMA)));
+    }
+}
