9943d188729ec1dfc4d1aa1e47a56037a2c225cb
YangWoo Yu <yuyw0712@naver.com>
2022-01-25 17:48:09 +0900

    refacor: Application.java GameLauncher 시작 코드만 존재

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,22 +2,13 @@ package racingcar;
 
 
 import java.util.List;
-import racingcar.service.GameLauncher;
+import racingcar.racing.GameLauncher;
 import racingcar.ui.RacingCarInput;
 
 public class Application {
 
     public static void main(String[] args) {
-        RacingCarInput racingCarInput = new RacingCarInput();
-        List<String> carNameList = racingCarInput.getCarName();
-        int raceCount = racingCarInput.getRaceCount();
-        GameLauncher gameLauncher = new GameLauncher(carNameList);
-        while (0 < raceCount) {
-            gameLauncher.moveForwardAll();
-            gameLauncher.printRacing();
-            raceCount--;
-            System.out.println("------------------------------");
-        }
-        gameLauncher.printWinner();
+        GameLauncher gameLauncher = new GameLauncher();
+        gameLauncher.start();
     }
 }
