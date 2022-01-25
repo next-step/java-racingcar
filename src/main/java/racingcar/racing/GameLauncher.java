007ac408578ed24ef3b048b874a2fbf840fbd339
YangWoo Yu <yuyw0712@naver.com>
2022-01-25 17:49:00 +0900

    refacor: GameLauncher.java 클래스 책임 분리 1. 응집도 향상을 위해 책임 분리

---
+++ src/main/java/racingcar/racing/GameLauncher.java
@@ -0,0 +1,11 @@
+package racingcar.racing;
+
+public class GameLauncher {
+
+    public void start() {
+        RacingGame racingGame = new RacingGame();
+        racingGame.playRacingGame();
+    }
+
+}
+
