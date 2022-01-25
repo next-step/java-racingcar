ff40aec78cef1dfc88a0de84fa61e16e009764b5
Ji In Lee <loveted0225@naver.com>
2022-01-25 16:01:22 +0900

    refactor(Game): 불필요한 변수 제거 및 선언과 초기화 동시로 수정

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -2,6 +2,7 @@ package racingcar.controller;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import racingcar.model.Car;
 import racingcar.model.User;
 import racingcar.util.Message;
@@ -10,31 +11,27 @@ import racingcar.view.OutputView;
 public class Game {
 
     public void play() {
-        ArrayList<String> carNameList;
-        String[] carNames; //입력 받은 자동차 이름들
-        int tryCount;
-        ArrayList<Car> carList = new ArrayList<>(); //경주를 하는 car 모음
-        //ArrayList<Car> winnerList = new ArrayList<>();
+        List<Car> carList = new ArrayList<>();
         User user = new User();
 
         System.out.println(Message.INPUT_GUIDE_MESSAGE);
-        carNames = user.getCarName();
-        System.out.println(Message.ASK_TRY_COUNT);
-        tryCount = user.getTryCount();
+        String[]carNames = user.getCarName();
 
         for (int i = 0; i < carNames.length; i++) {
             carList.add(new Car(carNames[i])); //car 객체 생성
         }
+        System.out.println(Message.ASK_TRY_COUNT);
+        int tryCount = user.getTryCount();
+
         System.out.println(Message.GAME_RESULT_MESSAGE);
         for (int i = 0; i < tryCount; i++) {
-            System.out.println(tryCount + 1 + "회");
             moveForwardByCount(carList);
         }
 
         checkWinner(carList);
     }
 
-    public void moveForwardByCount(ArrayList<Car> carList) {
+    public void moveForwardByCount(List<Car> carList) {
         OutputView outputView = new OutputView();
         for (Car car : carList) {
             car.moveForward();
@@ -43,7 +40,7 @@ public class Game {
         System.out.println();
     }
 
-    public void checkWinner(ArrayList<Car> carList) {
+    public void checkWinner(List<Car> carList) {
         ArrayList<String> winnerList = new ArrayList<>();
         OutputView outputView = new OutputView();
         int maxPosition = 0;
