62f461def9569982f8e4fff3a82549bdd6303e99
Ji In Lee <loveted0225@naver.com>
2022-01-25 17:53:27 +0900

    feat(ApplicationTest): 테스트 코드 작성

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,3 +1,30 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+import racingcar.util.RandomGenerator;
+
 public class ApplicationTest {
+    @Test
+
+    void 전진_불가 (){
+        Car car = new Car("Jiin");
+        car.moveForward(1);
+        assertThat(car.getPosition()).isEqualTo("");
+    }
+
+    void 전진_가능(){
+        Car car = new Car("Leah");
+        car.moveForward(5);
+        assertThat(car.getPosition()).isEqualTo("-");
+    }
+
+    void 이름_5글자_이상(){
+        assertThatIllegalArgumentException()
+            .isThrownBy(()-> new Car("LeahJiin"));
+    }
+
+
 
 }
