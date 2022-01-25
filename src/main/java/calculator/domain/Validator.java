5e8e08b332c5504d5bfbc7ba381811d3e3e9a305
HanSeoungjin <kktmdwls1000@konkuk.ac.kr>
2022-01-25 14:50:30 +0900

    refactor: Validator 클래스에서 정규식 캐싱
    - 정규식을 static 으로 선언해서 매번 새로운 정규식 인스턴스를 생성하지 않도록 함- 함수 이름을 좀 더 명확하게 변경

--- src/main/java/calculator/domain/Validator.java
+++ src/main/java/calculator/domain/Validator.java
@@ -1,9 +1,13 @@
 package calculator.domain;
 
+import java.util.regex.Pattern;
+
 public class Validator {
 
-    public static boolean checkValidate(String expression) {
-        String pattern = "^\\d+(\\s*[-+*/]\\s*\\d+)+$";
-        return expression.matches(pattern);
+    private static final Pattern EXPRESSION = Pattern.compile(
+        "^(0|[1-9][0-9]*)(\\s*(([-+*]\\s*(0|[1-9][0-9]*))|([/]\\s*[1-9][0-9]*)))+$");
+
+    public static boolean isValidExpression(String input) {
+        return EXPRESSION.matcher(input).matches();
     }
 }
