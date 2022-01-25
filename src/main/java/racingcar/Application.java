a46dece899f3d56b6555703bf535c838019f36ad
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-26 07:54:01 +0900

    feat(racingCar): Application 클래스 수정
    - racingController 메소드 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,7 +6,7 @@ import racingcar.controller.RacingController;
 public class Application {
 
     public static void main(String[] args) {
-        new RacingController();
+        RacingController.racingController();
     }
 }
 
