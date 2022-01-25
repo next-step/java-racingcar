6e62b66eb584a61574f23717266dbc0f3808e914
Ji In Lee <loveted0225@naver.com>
2022-01-25 17:26:02 +0900

    refactor(OutputView): 불필요한 출력결과 메시지 삭제

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 
 public class OutputView {
 
-    private final String GAME_RESULT_MESSAGE = "출력결과";
     private final String WINNER_MESSAGE = "최종 우승자: ";
 
     public void printResult(String carName, String position) {
-        System.out.println(GAME_RESULT_MESSAGE);
         System.out.println(carName + ": " + position);
     }
 
