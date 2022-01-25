4e44540dcc9988f5aae37adb9078709300ac3c19
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-25 17:20:25 +0900

    refactor(racingCar): Application 클래스 변경
    - Application 클래스 에서는 RacingController 호출
    - 주요 비즈니스 로직에 대한 처리를 RacingController에 전담

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 
+import racingcar.controller.RacingController;
 import racingcar.view.RacingCarUserInput;
 import racingcar.model.domain.Helper;
 import racingcar.view.RacingCarUserOutput;
@@ -8,19 +9,8 @@ import racingcar.model.domain.Racing;
 
 public class Application {
 
-    private static final int NUMBER_ZERO = 0;
-
     public static void main(String[] args) {
-        Racing racing = Racing.get();
-        RacingCarUserInput userInput = new RacingCarUserInput();
-        RacingCarUserOutput judgement = new RacingCarUserOutput();
-        userInput.userInputRun();
-        new Helper().run(userInput.getSplitUserInput(), racing);
-        for (int time = NUMBER_ZERO; time < userInput.getRacingTryCounter(); time++) {
-            racing.drive();
-            judgement.carsStatement(racing.getCars());
-        }
-        judgement.judge(racing.getCars());
+        new RacingController();
     }
 }
 
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,5 +1,22 @@
 package racingcar.controller;
 
-public class RacingController {
+import racingcar.model.domain.Racing;
+import racingcar.view.RacingCarUserInput;
+import racingcar.view.RacingCarUserOutput;
 
+public class RacingController {
+    private static final int NUMBER_ZERO = 0;
+
+    public void racingController(){
+        Racing racing = Racing.get();
+        RacingCarUserInput userInput = new RacingCarUserInput();
+        RacingCarUserOutput judgement = new RacingCarUserOutput();
+        userInput.userInputRun();
+        new Helper().run(userInput.getSplitUserInput(), racing);
+        for (int time = NUMBER_ZERO; time < userInput.getRacingTryCounter(); time++) {
+            racing.drive();
+            judgement.carsStatement(racing.getCars());
+        }
+        judgement.judge(racing.getCars());
+    }
 }
