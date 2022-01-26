1b0c7e797e18014c8f5746ccc8fac4921c58dd72
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-26 22:52:00 +0900

    Revert "feat(unit-test): 접근제한자 변경으로 인한 테스트 범위 조정"
    This reverts commit 160349c6ffb7588fc2aa93bc72b97dea8109e1d1.

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -57,7 +57,7 @@ class CarTest {
         Collections.sort(cars, new Comparator<Car>() {
             @Override
             public int compare(Car o1, Car o2) {
-                return o2.getPosition() - o1.getPosition();
+                return o2.position - o1.position;
             }
         });
 
