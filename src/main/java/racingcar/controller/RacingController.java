8dff5225f10ff4370b929795cea2b2bb196096d8
MinChul-Son <smc5236@naver.com>
2022-01-27 01:19:36 +0900

    🚀 refactor: RacingController 생성자 주입 및 메서드 이동
    - InputView를 생성자 주입으로 추가함
    - Application에서 핵심 비즈니스 로직이 노출되었던 부분을 Controller로 이관

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,20 +1,33 @@
 package racingcar.controller;
 
 import racingcar.service.RacingService;
+import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
 public class RacingController {
 
+    private static final int START_FROM_ZERO = 0;
+
     private final RacingService racingService;
+    private final InputView inputView;
     private final ResultView resultView;
 
-    public RacingController(final RacingService racingService, final ResultView resultView) {
+    public RacingController(final RacingService racingService, final InputView inputView, final ResultView resultView) {
         this.racingService = racingService;
+        this.inputView = inputView;
         this.resultView = resultView;
     }
 
+    public void setUpRacing() {
+        inputView.processConsoleInput();
+        racingService.registerCars(inputView.getSplitUserInput());
+    }
+
     public void startRacing() {
-        racingService.drive();
+        for (int time = START_FROM_ZERO; time < inputView.getRacingTime(); time++) {
+            racingService.drive();
+            carsCurrentStatements();
+        }
     }
 
     public void carsCurrentStatements() {
