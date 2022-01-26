9914e4ad715c5ab30c12a0885f73af9ecff54004
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-26 22:50:37 +0900

    Revert "feat(random-gen): Random Class는 캐싱해서 재활용"
    This reverts commit 50be5e4d98660b0cb19a3c7c907650001d673659.

--- src/main/java/racingcar/domain/random/RandomGenerator.java
+++ src/main/java/racingcar/domain/random/RandomGenerator.java
@@ -4,10 +4,9 @@ import java.util.Random;
 
 public class RandomGenerator implements RandomStrategy {
 
-    private static final Random random = new Random();
     private static final int LIMIT = 10;
 
     public int get() {
-        return random.nextInt(LIMIT);
+        return new Random().nextInt(LIMIT);
     }
 }
