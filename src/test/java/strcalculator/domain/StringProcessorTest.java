ea04ad62c5f975bd55f57e98f443ef7e1297c7ca
hyojeong <qkrgywjd456@naver.com>
2022-01-27 02:12:28 +0900

    test: 사칙연산 제외한 숫자만 남는지 확인

--- src/test/java/strcalculator/domain/StringProcessorTest.java
+++ src/test/java/strcalculator/domain/StringProcessorTest.java
@@ -2,6 +2,7 @@ package strcalculator.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,15 @@ class StringProcessorTest {
     @DisplayName("공백 or null 만 들어왔을때 오류 확인")
     public void checkNullOrSpace() {
         assertThrows(IllegalArgumentException.class, () -> new StringProcessor(" "));
+    }
+
+    @Test
+    @DisplayName("사칙연산 제외한 숫자만 남는지 확인")
+    public void inputParseNumber(){
+        StringProcessor stringProcessor = new StringProcessor("1+   2+  3");
+        stringProcessor.deleteSpaceString();
+        String[] strings = stringProcessor.parseInputNumber();
+        Arrays.stream(strings).allMatch((s)->s.matches("[0-9]+"));
+
     }
 }
