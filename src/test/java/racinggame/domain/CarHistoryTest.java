8b783bc7ea29bba7955aa8d0e818238c79dae825
hyojeong <qkrgywjd456@naver.com>
2022-01-27 00:34:08 +0900

    test: carHistory 의 car 객체와 carHistory 객체의 location 동등성 확인

--- src/test/java/racinggame/domain/CarHistoryTest.java
+++ src/test/java/racinggame/domain/CarHistoryTest.java
@@ -16,8 +16,13 @@ class CarHistoryTest {
         Assertions.assertThat(carHistory.getCarName()).isEqualTo(car.getCarName());
     }
 
-
-
+    @DisplayName("각 Car 와 CarHistory 이름 일치여부 확인")
+    @Test
+    public void getLocation() {
+        Car car = new Car("a", 7);
+        CarHistory carHistory = new CarHistory(car);
+        Assertions.assertThat(carHistory.getLocation()).isEqualTo(car.getLocation());
+    }
 
 
 }
