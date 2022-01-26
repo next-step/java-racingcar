ab7b02f4ea3fa1f45cf11ec9b849f76bffbfd00f
0JUUU <clleo97@naver.com>
2022-01-27 01:43:58 +0900

    refactor: 시도횟수에 대한 유효성 검사를 TryNumber 객체에 위임

--- src/main/java/racing/domain/UserInput.java
+++ src/main/java/racing/domain/UserInput.java
@@ -1,10 +1,7 @@
 package racing.domain;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class UserInput {
 
@@ -14,55 +11,17 @@ public class UserInput {
     }
 
     public static List<String> getCarNames() {
-        try {
-            System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-
-            final String input = SCANNER.nextLine();
-            final List<String> names = splitStringByComma(input);
-
-            checkCarCount(names);
-            return names;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return getCarNames();
-        }
-    }
-
-    public static List<String> splitStringByComma(final String input) {
-        final List<String> names = new ArrayList<>();
-        final StringTokenizer st = new StringTokenizer(input, ",");
-
-        while (st.hasMoreTokens()) {
-            final String name = st.nextToken().trim();
-            names.add(name);
-        }
-        return names;
-    }
-
-    private static void checkCarCount(final List<String> names) {
-        if (names.size() < 1) {
-            throw new IllegalArgumentException("[ERROR] 최소 1대 이상의 자동차가 필요합니다.");
-        }
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return Parser.split(SCANNER.nextLine());
     }
 
     public static int getTryNumber() {
         try {
             System.out.println("시도할 횟수는 몇 회인가요?");
-            final int tryNumber = Integer.parseInt(SCANNER.nextLine());
-            checkTryNumber(tryNumber);
-            return tryNumber;
-        } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 숫자만 입력 해주세요.");
-            return getTryNumber();
+            return new TryNumber(SCANNER.nextLine()).getTryNumber();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return getTryNumber();
-        }
-    }
-
-    private static void checkTryNumber(final int tryNumber) {
-        if (tryNumber <= 0) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 최소 1회 이상이어야 합니다.");
         }
     }
 }
