f5f73b519d4c08f8ea9a4063b77d7d8bf3675e70
0JUUU <clleo97@naver.com>
2022-01-27 01:58:34 +0900

    refactor: throw Exception 하는 부분 통일

--- src/main/java/racing/domain/TryNumber.java
+++ src/main/java/racing/domain/TryNumber.java
@@ -2,23 +2,25 @@ package racing.domain;
 
 public class TryNumber {
 
+    private static final int MIN_TRY_NUMBER = 1;
+
     private final int tryNumber;
 
     public TryNumber(String tryNumber) {
+        this.tryNumber = toInt(tryNumber);
+        isValidTryNumber(this.tryNumber);
+    }
+
+    private int toInt(String tryNumber) {
         try {
-            this.tryNumber = toInt(tryNumber);
-            checkTryNumber(this.tryNumber);
+            return Integer.parseInt(tryNumber);
         } catch (NumberFormatException nfe) {
             throw new NumberFormatException("[ERROR] 숫자만 입력 해주세요.");
         }
     }
 
-    private int toInt(String tryNumber) {
-        return Integer.parseInt(tryNumber);
-    }
-
-    private static void checkTryNumber(final int tryNumber) {
-        if (tryNumber <= 0) {
+    private static void isValidTryNumber(final int tryNumber) {
+        if (tryNumber < MIN_TRY_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수는 최소 1회 이상이어야 합니다.");
         }
     }
