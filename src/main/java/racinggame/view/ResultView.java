91129c3504b76479a7c8e278375850f5959c38a5
hyojeong <qkrgywjd456@naver.com>
2022-01-26 22:39:48 +0900

    feat: 각 라운드 별 자동차의 상태값 저장후 출력하도록 구현

--- src/main/java/racinggame/view/ResultView.java
+++ src/main/java/racinggame/view/ResultView.java
@@ -3,27 +3,56 @@ package racinggame.view;
 import java.util.ArrayList;
 import java.util.List;
 import racinggame.domain.Car;
+import racinggame.domain.Cars;
+import racinggame.domain.CarHistory;
+import racinggame.domain.RoundHistory;
 import racinggame.domain.Winner;
+import racinggame.domain.Winners;
 
 public class ResultView {
 
-    public void printCarsLocation(List<Car> cars) {
-        for (Car car : cars) {
-            System.out.print(car.getCarName() + " : " + "");
-            for (int i = 0; i < car.getLocation(); i++) {
-                System.out.print('-');
-            }
+    private List<RoundHistory> roundHistories = new ArrayList<>();
+
+    public ResultView() {
+    }
+
+    public void captureResult(Cars cars) {
+        List<CarHistory> carHistories = new ArrayList<>();
+
+        for (Car car : cars.getCars()) {
+            carHistories.add(new CarHistory(car));
+        }
+        roundHistories.add(new RoundHistory(carHistories));
+
+    }
+
+    public void printCarsLocation() {
+        for (RoundHistory roundHistory : roundHistories) {
+            roundHistory(roundHistory);
             System.out.println();
+        }
+
+    }
+
+    private void roundHistory(RoundHistory roundHistory) {
+        for (CarHistory carHistory : roundHistory.getRoundHistory()) {
+            System.out.print(carHistory.getCarName() + " : " + "");
+            printLocation(carHistory.getLocation());
+        }
+    }
+
+    private void printLocation(int location) {
+        for (int i = 0; i < location; i++) {
+            System.out.print('-');
         }
         System.out.println();
     }
 
-
-    public void printWinner(List<Winner> winners) {
+    public void printWinner(Winners winners) {
         List<String> winnerNameList = new ArrayList<>();
         String winnerNames = "";
         System.out.print("최종 우승자: ");
-        for (Winner winner : winners) {
+        for (Winner winner : winners.getWinners()) {
             winnerNameList.add(winner.getWinnerName());
         }
         winnerNames = String.join(", ", winnerNameList);
