243a1e4803cc2689d795e46cb3acc2ac1a64b6c7
Hae-Riri <ehl3288@naver.com>
2022-01-27 00:35:40 +0900

    test: Car와 Judgement 테스트 코드 추가

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,28 +1,25 @@
 package racing;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
     @Test
-    void 자동차_이름이_5글자_이상() {
+    void 자동차_이름이_5글자_이상_에러() {
         assertThatIllegalArgumentException()
             .isThrownBy(() -> {
-                new Car("안녕하세요박찬우입니다.", 5, 4);
+                new Car("안녕하세요박찬우입니다.");
             });
     }
 
     @Test
-    void 자동차_3라운드_결과() {
-        Car car = new Car("박찬우", 5, 4);
-        car.move();
-        car.move();
-        car.move();
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < car.getDistance(); i++) {
-            sb.append('-');
-        }
+    void 이동조건_판별_기능_확인() {
+        Car car = new Car("어해림");
+        car.move(3);
+        car.move(5);
+        Assertions.assertThat(car.getDistance()).isEqualTo(1);
     }
 }
