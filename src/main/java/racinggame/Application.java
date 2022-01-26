6cba23e1bfa5fb2abb1f9a0f90375b29999eb0dc
hyojeong <qkrgywjd456@naver.com>
2022-01-26 22:37:53 +0900

    refactor: Car 의 상태값 저장 후 각 라운드별 자동차 상태 한번에 출력

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -9,17 +9,18 @@ public class Application {
 
     public static void main(String[] args) {
         GameInit gameInit = new GameInit();
-        ResultView resultView = new ResultView();
         String[] carNames = gameInit.inputPlayerName();
         Cars cars = new Cars(carNames);
 
         int playGameNumber = gameInit.inputTryNumber();
         System.out.println("실행 결과");
+        ResultView resultView = new ResultView();
         for (; playGameNumber > 0; playGameNumber--) {
             cars.move();
-            resultView.printCarsLocation(cars);
+            resultView.captureResult(cars);
 
         }
+        resultView.printCarsLocation();
 
         final int maxLocation = cars.findMaxLocation();
         Winners winners = new Winners(cars, maxLocation);
