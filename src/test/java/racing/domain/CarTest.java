e4aa25d63ced1353cf4674657361d9432adbeb6b
0JUUU <clleo97@naver.com>
2022-01-26 23:29:20 +0900

    feat: Car 객체에 대한 테스트코드 작성

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -12,4 +12,20 @@ class CarTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
             .isThrownBy(() -> new Car("abcdef"));
     }
+
+    @Test
+    void 자동차_이름_출력_확인() {
+        Car car1 = new Car("abc");
+        assertThat(car1.getNameBy(0)).isEqualTo("abc");
+        assertThat(car1.getNameBy(2)).isEmpty();
+    }
+
+    @Test
+    void 자동차_움직임_여부() {
+        Car car1 = new Car("abc");
+        car1.drive(3);
+        assertThat(car1.getPosition()).isEqualTo(0);
+        car1.drive(4);
+        assertThat(car1.getPosition()).isEqualTo(1);
+    }
 }
