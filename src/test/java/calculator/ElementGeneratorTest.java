a1ba109ecbcbc8d87235de1a2441dd1948789b33
Hae-Riri <ehl3288@naver.com>
2022-01-27 00:11:15 +0900

    test: ElementGenerator 테스트 코드 추가

--- src/test/java/calculator/ElementGeneratorTest.java
+++ src/test/java/calculator/ElementGeneratorTest.java
@@ -1,15 +1,10 @@
 package calculator;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ElementGeneratorTest {
-
-    private ElementGenerator elementGenerator;
 
     @Test
     void 입력값이_Null일_때_에러() {
@@ -18,10 +13,15 @@ class ElementGeneratorTest {
     }
 
     @Test
-    void 입력값이_공백일_때_에러() {
-        String EMPTY_INPUT = "";
+    void 입력값이_Empty일_때_에러() {
         assertThatIllegalArgumentException()
-            .isThrownBy(() -> new ElementGenerator(EMPTY_INPUT));
+            .isThrownBy(() -> new ElementGenerator(""));
+    }
+
+    @Test
+    void 입력값이_Blank일_때_에러() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new ElementGenerator("       "));
     }
 
     @Test
@@ -29,18 +29,5 @@ class ElementGeneratorTest {
         String INCORRECT_INPUT = "12 $ 34 / 5";
         assertThatIllegalArgumentException()
             .isThrownBy(() -> new ElementGenerator(INCORRECT_INPUT));
-    }
-
-    @Test
-    void 정상() {
-        String CORRECT_INPUT = "12 * 3 / 2";
-        elementGenerator = new ElementGenerator(CORRECT_INPUT);
-        List<String> elements = elementGenerator.getElements();
-
-        for (int i = 0; i < elements.size(); i++) {
-            if (i % 2 == 1) {
-                assertThat(elements.get(i)).isIn(Arrays.asList("+", "-", "*", "/"));
-            }
-        }
     }
 }
