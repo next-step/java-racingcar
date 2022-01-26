635512b1e3e710291dee1bd4f002baaa5cfb2038
Hae-Riri <ehl3288@naver.com>
2022-01-26 23:22:40 +0900

    fix: 나눗셈 결과 포맷팅, 예외처리 추가
    - 무한소수 결과를 처리하기 위해 소수점 아래 두 자리수까지 출력
    - 0으로 나눌 경우 예외처리
    - number1,2가 null로 들어오는 경우를 감싸기 위해 wrapper type으로 변환

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -7,17 +7,24 @@ public enum Operator {
     ADD("+", (number1, number2) -> number1 + number2),
     SUBTRACT("-", (number1, number2) -> number1 - number2),
     MULTIPLY("*", (number1, number2) -> number1 * number2),
-    DIVIDE("/", (number1, number2) -> number1 / number2);
+    DIVIDE("/", (number1, number2) -> {
+        final int DIGIT = 2;
+        if(Double.valueOf(number1 / number2).isInfinite()) {
+            throw new IllegalArgumentException("[ERROR] 0으로 나눌 수 없습니다.");
+        }
+        return
+            Math.round((number1 / number2) * Math.pow(10, DIGIT)) / Math.pow(10, DIGIT);
+    });
 
     private final String symbol;
     private final BiFunction<Double, Double, Double> operator;
 
-    Operator(String symbol, final BiFunction<Double, Double, Double> operator) {
+    Operator(String symbol, BiFunction<Double, Double, Double> operator) {
         this.symbol = symbol;
         this.operator = operator;
     }
 
-    public double apply(double number1, double number2) {
+    public double apply(Double number1, Double number2) {
         return operator.apply(number1, number2);
     }
 
