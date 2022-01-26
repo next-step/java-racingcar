7de3854c893898d38b8d2d4404e48a9c6dac319c
SeungyeonJung <crowsy01@gmail.com>
2022-01-26 17:46:19 +0900

    refactor: Change Math class to Random class for random number generation

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,4 +1,5 @@
 package racingcar;
+import java.util.Random;
 
 public class Race {
     public static void execute() {
@@ -7,12 +8,10 @@ public class Race {
             System.out.println("자동차 이름은 5자를 초과할 수 없습니다.");
             return ;
         }
-        Race race = new Race();
         int[] cars = new int[carNames.length];
         int times = InputView.getTimes();
         repeatGame(cars,carNames,times);
         OutputView.printWinners(carNames, cars);
-        return;
     }
 
     public static void repeatGame(int[] cars, String[] carNames, int times) {
@@ -26,10 +25,11 @@ public class Race {
         }
     }
 
-    public static int getRandomNumber(){
-        int randomNumber = (int) Math.floor(Math.random()*10);
+    public static int getRandomNumber() {
+        int randomNumber = new Random().nextInt(10);
         return randomNumber;
     }
+
     public static void compareWithFour(int[] cars, int idx, int randomNumber){
         if(randomNumber>=4)
             cars[idx]++;
