76b397abbd270cd7dbe06b277f028affb78fa1ba
jihyunhillpark <girl9505@naver.com>
2022-01-27 00:50:58 +0900

    refactor: controller 클래스명 수정

--- src/main/java/racingcar/RacingMain.java
+++ src/main/java/racingcar/RacingMain.java
@@ -4,7 +4,7 @@ import racingcar.domain.RacingGame;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
-public class Application {
+public class RacingMain {
 
     public static void main(String[] args) {
         String carNames = InputView.getCarNames();
