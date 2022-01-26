70a7a8dd963403bb3d9c686b3d204141edefb2eb
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-26 20:28:54 +0900

    test(racingCar): 테스트 코드 수정
    - @ParameterizedTest 적용

--- src/test/java/racingcar/domain/ApplicationTest.java
+++ src/test/java/racingcar/domain/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
@@ -8,7 +9,11 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.controller.dto.InputDTO;
+import racingcar.model.domain.Cars;
 import racingcar.model.entity.Car;
 import racingcar.view.RacingCarUserConsole;
 
@@ -43,10 +48,11 @@ public class ApplicationTest {
     }
 
     @DisplayName("우승자_확인_테스트")
-    @Test
-    public void maxCheckTest() {
+    @ParameterizedTest
+    @CsvSource({"a,aa,aaa"})
+    public void maxCheckTest(String one, String two, String three) {
         //given
-        final List<Car> cars = Arrays.asList(Car.get("a"), Car.get("aa"), Car.get("aaa"));
+        final List<Car> cars = Arrays.asList(Car.get(one), Car.get(two), Car.get(three));
 
         //when
         cars.get(0).moveForward();
@@ -60,5 +66,22 @@ public class ApplicationTest {
 
         //then
         assertThat(cars.get(0).toString(max)).isEqualTo("a ");
+    }
+
+    @DisplayName("car클래스가 cars클래스에 Wrapping 검증")
+    @ParameterizedTest
+    @CsvSource({"a,aa,aaa"})
+    void carWrappingTest(String one, String two, String three) {
+        Cars cars = new Cars(Arrays.asList(one, two, three));
+        List<Car> carsRepo = cars.getRacing().getCars();
+        assertAll(
+            () -> assertThat(carsRepo.get(0).getName()).isEqualTo("a"),
+            () -> assertThat(carsRepo.get(1).getName()).isEqualTo("aa"),
+            () -> assertThat(carsRepo.get(2).getName()).isEqualTo("aaa")
+        );
+    }
+
+    @DisplayName("랜덤넘버가 4이상일 때 전진할 수 있는지 검증")
+    void name() {
     }
 }
