17cfa3fbb07c3144959d66288379a3252c2e2cfc
SeungyeonJung <crowsy01@gmail.com>
2022-01-27 00:46:22 +0900

    refactor: Use @BeforeEach to initialize

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -1,12 +1,14 @@
 package racingcar;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RaceTest {
+    int[] cars = new int[3];
+
     @DisplayName("0~9사이의 정수를 반환하는지 100번 확인한다.")
     @Test
     public void printRandomNumberTenTimes(){
@@ -23,13 +25,16 @@ class RaceTest {
         assertThat(sum).isEqualTo(100);
     }
 
-    @DisplayName("난수가 5일때 배열의 숫자가 증가하는지 확인한다")
-    @Test
-    public void checkWhenRandomNumberIsFive() {
-        int[] cars = new int[3];
+    @BeforeEach
+    void init() {
         cars[0]=2;
         cars[1]=2;
         cars[2]=2;
+    }
+
+    @DisplayName("난수가 5일때 배열의 숫자가 증가하는지 확인한다")
+    @Test
+    public void checkWhenRandomNumberIsFive() {
         Race.compareWithFour(cars,1,5);
         assertThat(cars[1]).isEqualTo(3);
     }
@@ -37,10 +42,6 @@ class RaceTest {
     @DisplayName("난수가 4일때 배열의 숫자가 증가하는지 확인한다")
     @Test
     public void checkWhenRandomNumberIsFour() {
-        int[] cars = new int[3];
-        cars[0]=2;
-        cars[1]=2;
-        cars[2]=2;
         Race.compareWithFour(cars,1,4);
         assertThat(cars[1]).isEqualTo(3);
     }
@@ -48,10 +49,6 @@ class RaceTest {
     @DisplayName("난수가 3일때 배열의 숫자가 증가하는지 확인한다")
     @Test
     public void checkWhenRandomNumberIsThree() {
-        int[] cars = new int[3];
-        cars[0]=2;
-        cars[1]=2;
-        cars[2]=2;
         Race.compareWithFour(cars,1,3);
         assertThat(cars[1]).isEqualTo(2);
     }
