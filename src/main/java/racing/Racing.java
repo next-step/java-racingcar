e7fbab867086855739f42c054acd410667c4eb3c
Hae-Riri <ehl3288@naver.com>
2022-01-27 00:31:23 +0900

    refactor: 생성자와 입력받는 로직 분리
    - initRoundNumber를 통해 생성자와 입력 로직 분리
    - 예외처리 에러 메시지 추가

--- src/main/java/racing/Racing.java
+++ src/main/java/racing/Racing.java
@@ -8,15 +8,22 @@ import java.util.List;
 public class Racing {
 
     private final List<Car> cars;
-    private final int roundNumber;
+    private int roundNumber;
 
-    public Racing(List<Car> cars) throws IOException {
+    public Racing(List<Car> cars){
         this.cars = cars;
+        initRoundNumber();
+    }
 
-        //TODO: Step2에서 리팩토링
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("시도할 횟수는 몇회인가요?");
-        this.roundNumber = Integer.parseInt(br.readLine());
+    private void initRoundNumber() {
+        try {
+            //TODO: Step2에서 리팩토링
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("시도할 횟수는 몇회인가요?");
+            this.roundNumber = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            System.out.println("[ERROR] 입출력 예외 발생");
+        }
     }
 
     public List<Car> race() {
@@ -30,7 +37,7 @@ public class Racing {
     private void startRound() {
         for (Car car : cars) {
             car.move(makeRandom());
-            car.printDistance(); //TODO: Step2에서 리팩토링
+            car.printDistance(); //TODO: Step2에서 입출력 분리 리팩토링 예정
         }
     }
 
