303c4efef7f4395ddf5f942d519044b905e81b66
hyojeong <qkrgywjd456@naver.com>
2022-01-26 22:53:59 +0900

    refactor: 변수, 객체 final 추가

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -8,13 +8,14 @@ import racinggame.view.ResultView;
 public class Application {
 
     public static void main(String[] args) {
-        GameInit gameInit = new GameInit();
-        String[] carNames = gameInit.inputPlayerName();
-        Cars cars = new Cars(carNames);
+
+        final GameInit gameInit = new GameInit();
+        final String[] carNames = gameInit.inputPlayerName();
+        final Cars cars = new Cars(carNames);
 
         int playGameNumber = gameInit.inputTryNumber();
         System.out.println("실행 결과");
-        ResultView resultView = new ResultView();
+        final ResultView resultView = new ResultView();
         for (; playGameNumber > 0; playGameNumber--) {
             cars.move();
             resultView.captureResult(cars);
@@ -23,7 +24,7 @@ public class Application {
         resultView.printCarsLocation();
 
         final int maxLocation = cars.findMaxLocation();
-        Winners winners = new Winners(cars, maxLocation);
+        final Winners winners = new Winners(cars, maxLocation);
         resultView.printWinner(winners);
     }
 
