9da2e4fa9475287d762f6d3b6c92bf4257b265fe
0JUUU <clleo97@naver.com>
2022-01-27 02:17:06 +0900

    refactor: 문자열 상수로 전환

--- src/main/java/calculator/domain/Expression.java
+++ src/main/java/calculator/domain/Expression.java
@@ -9,6 +9,10 @@ import java.util.stream.Collectors;
 
 public class Expression {
 
+    private static final String WHITE_SPACE = " ";
+    private static final String EMPTY = "";
+    private static final String REGEX_OPERATORS = "[-+*/]";
+    private static final String REGEX_NUMBERS = "[0-9]+";
     private static final double INITIAL_RESULT_OF_CALCULATE = 0;
 
     private final Queue<String> numbers;
@@ -17,14 +21,12 @@ public class Expression {
     private double resultCalculate;
 
     public Expression(String expression) {
-        //TODO: 방어적 복사본 사용할 것
         if (!Validator.checkValidate(expression)) {
             throw new IllegalArgumentException("[ERROR] 유효한 식이 아닙니다.");
         }
-
-        expression = expression.replaceAll(" ", "");
-        numbers = new LinkedList<>(Arrays.asList(expression.split("[-+*/]")));
-        operators = Arrays.stream(expression.split("[0-9]+"))
+        expression = expression.replaceAll(WHITE_SPACE, EMPTY);
+        numbers = new LinkedList<>(Arrays.asList(expression.split(REGEX_OPERATORS)));
+        operators = Arrays.stream(expression.split(REGEX_NUMBERS))
             .filter(operand -> !operand.isEmpty())
             .collect(Collectors.toList());
         resultCalculate = INITIAL_RESULT_OF_CALCULATE;
