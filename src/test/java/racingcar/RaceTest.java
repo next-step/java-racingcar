f4ea65fce5293b982d364e44e6c83e71c5892d09
SeungyeonJung <crowsy01@gmail.com>
2022-01-27 00:24:45 +0900

    refactor: Remove unnecessary constructor

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -10,10 +10,9 @@ class RaceTest {
     @DisplayName("0~9사이의 정수를 반환하는지 100번 확인한다.")
     @Test
     public void printRandomNumberTenTimes(){
-        Race race = new Race();
         int[] check = new int[10];
         for(int i=0; i<100; i++) {
-            int randomNumber = race.getRandomNumber();
+            int randomNumber = Race.getRandomNumber();
             System.out.println(randomNumber);
             check[randomNumber]++;
         }
@@ -27,36 +26,33 @@ class RaceTest {
     @DisplayName("난수가 5일때 배열의 숫자가 증가하는지 확인한다")
     @Test
     public void checkWhenRandomNumberIsFive() {
-        Race race = new Race();
         int[] cars = new int[3];
         cars[0]=2;
         cars[1]=2;
         cars[2]=2;
-        race.compareWithFour(cars,1,5);
+        Race.compareWithFour(cars,1,5);
         assertThat(cars[1]).isEqualTo(3);
     }
 
     @DisplayName("난수가 4일때 배열의 숫자가 증가하는지 확인한다")
     @Test
     public void checkWhenRandomNumberIsFour() {
-        Race race = new Race();
         int[] cars = new int[3];
         cars[0]=2;
         cars[1]=2;
         cars[2]=2;
-        race.compareWithFour(cars,1,4);
+        Race.compareWithFour(cars,1,4);
         assertThat(cars[1]).isEqualTo(3);
     }
 
     @DisplayName("난수가 3일때 배열의 숫자가 증가하는지 확인한다")
     @Test
     public void checkWhenRandomNumberIsThree() {
-        Race race = new Race();
         int[] cars = new int[3];
         cars[0]=2;
         cars[1]=2;
         cars[2]=2;
-        race.compareWithFour(cars,1,3);
+        Race.compareWithFour(cars,1,3);
         assertThat(cars[1]).isEqualTo(2);
     }
 }
