c60f804c9c44787019f1292b5c222261eba5d1dd
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-26 22:50:59 +0900

    Revert "feat(Pariticpants): 참가자 자동차 만드는 과정을 Stream으로 활용"
    This reverts commit 23d131e91c7c9b9fdcaa5dc7836785f687c57fe9.

--- src/main/java/racingcar/domain/Participants.java
+++ src/main/java/racingcar/domain/Participants.java
@@ -1,14 +1,16 @@
 package racingcar.domain;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.Move;
 import racingcar.domain.random.RandomGenerator;
+import racingcar.view.UserConsole;
 
 public class Participants {
 
+    private static final String NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String ERROR_LOG = "[ERROR] ";
     private final List<Car> cars;
     private final Move move;
 
@@ -25,11 +27,19 @@ public class Participants {
         return Participants.getInstance(racingResult.getResult());
     }
 
-    public static Participants createCars(String[] names) {
-        List<Car> cars;
-        cars = Arrays.stream(names)
-            .map(name -> Car.from(name.trim()))
-            .collect(Collectors.toList());
+    public static Participants createCars() {
+        String[] names = UserConsole.getConsoleTextFrom(NAME_MESSAGE).split(",");
+        List<Car> cars = new ArrayList<>();
+        try {
+            for (String name : names) {
+                name = name.trim();
+                Car car = Car.from(name);
+                cars.add(car);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(ERROR_LOG + e.getMessage());
+            return createCars();
+        }
         return Participants.getInstance(cars);
     }
 
