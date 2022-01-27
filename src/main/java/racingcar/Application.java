47ea3e4d7de55004cfe1e4aaec58de4dfca9fc1c
Dami Kim <dami.k.dev@gmail.com>
2022-01-27 23:47:56 +0900

    style: 불필요한 개행 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,8 +7,7 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
 
-        RacingCarGame racingCarGame = new RacingCarGame(inputView.getTryCount(),
-            inputView.getCarNames());
+        RacingCarGame racingCarGame = new RacingCarGame(inputView.getTryCount(), inputView.getCarNames());
 
         racingCarGame.play();
     }
