06cf41dec8be100d4f86da0974c2ef05b5c6d89b
Dami Kim <dami.k.dev@gmail.com>
2022-01-27 23:22:06 +0900

    refactor: 결과값 출력하는 로직 ResultView 클래스로 분리

---
+++ src/main/java/racingcar/view/ResultView.java
@@ -0,0 +1,25 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class ResultView {
+
+    private String getDashByMove(int move) {
+        StringBuilder dash = new StringBuilder();
+
+        for (int i = 0; i < move; i++) {
+            dash.append("-");
+        }
+        return dash.toString();
+    }
+
+    public void printGameStatus(String name, int move) {
+        String dashes = getDashByMove(move);
+        System.out.println(name + ":" + dashes);
+    }
+
+    public void printGameResult(List<String> winners) {
+        System.out.println("최종 우승자: " + String.join(", ", winners));
+    }
+
+}
