9b4a7bf3f296d4d4f5d6ffc5a637882145b4e8bc
hscom96 <hscom96@gmail.com>
2022-01-27 21:21:12 +0900

    feat: 승리 전용 객체 생성

---
+++ src/main/java/racingcar/domain/Winners.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Winners {
+
+    private List<Car> winnerCars;
+
+    public Winners(List<Car> cars){
+        this.winnerCars = filterWinners(cars);
+    }
+
+    private List<Car> filterWinners(List<Car> cars){
+        int maxPosition = cars.stream()
+            .mapToInt(Car::getPosition)
+            .max().orElseThrow(IllegalStateException::new);
+
+        return cars.stream()
+            .filter(car -> car.isSamePosition(maxPosition))
+            .collect(Collectors.toList());
+    }
+
+    public List<Car> getWinnerCars() {
+        return winnerCars;
+    }
+}
