2fefe79125c4326b1d4d24692715e7cb52315ae3
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-27 20:22:51 +0900

    feat(RacingRecord): 자동차 경주이 매턴 결과를 저장하는 클래스 추가

---
+++ src/main/java/racingcar/domain/RacingRecord.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+public class RacingRecord {
+
+    private static final String ENTER = "\n";
+    private static final String RESULT_LINE = "%s : %s" + ENTER;
+    private static final String DASH = "-";
+    private final StringBuilder raceRecord;
+
+    public RacingRecord() {
+        raceRecord = new StringBuilder();
+    }
+
+    public void appendRecord(RacingResult racingResult) {
+        racingResult.getResult().forEach(car
+            -> raceRecord.append(String.format(RESULT_LINE, car.getName(), convertPositionToDashString(car.getPosition()))));
+        raceRecord.append(ENTER);
+    }
+
+    private String convertPositionToDashString(int position) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            sb.append(DASH);
+        }
+        return sb.toString();
+    }
+
+    public String getResultRecord() {
+        return this.raceRecord.toString();
+    }
+}
