4a31fa2ff1066bfa5d1bf6356b7ed7ec8162ea36
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-27 20:50:09 +0900

    refactor: Car Class 랜덤넘버를 상수로 변경, 변수명 변경

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -5,8 +5,10 @@ import racingcar.domain.random.RandomStrategy;
 public class Car {
 
     private static final String NAME_LENGTH_ERROR_MESSAGE = "자동차 이름이 5자를 초과합니다.";
-    private static final int BOUND = 4;
+    private static final int INITIAL_POSITION = 0;
+    private static final int THRESHOLD_TO_MOVE = 4;
     private static final int MOVE_ONE_STEP = 1;
+    private static final int MAXIMUM_ALLOWED_NAME_LENGTH = 5;
 
     private final String name;
     public int position;
@@ -14,7 +16,7 @@ public class Car {
     private Car(String name) {
         validateCarName(name);
         this.name = name;
-        this.position = 0;
+        this.position = INITIAL_POSITION;
     }
 
     private Car(String name, int position) {
@@ -40,13 +42,13 @@ public class Car {
     }
 
     public void go(RandomStrategy randomStrategy) {
-        if (randomStrategy.get() > BOUND) {
+        if (randomStrategy.get() > THRESHOLD_TO_MOVE) {
             this.position += MOVE_ONE_STEP;
         }
     }
 
     private static void validateCarName(String name) {
-        if (name.length() > 5) {
+        if (name.length() > MAXIMUM_ALLOWED_NAME_LENGTH) {
             throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
         }
     }
