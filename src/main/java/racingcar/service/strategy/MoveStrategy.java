ac01eca9e19d5da52cd295caf2559a3be6c22ca6
hscom96 <hscom96@gmail.com>
2022-01-27 21:20:30 +0900

    feat: 전략패턴 인터페이스 및 구현체 추가

---
+++ src/main/java/racingcar/service/strategy/MoveStrategy.java
@@ -0,0 +1,7 @@
+package racingcar.service.strategy;
+
+@FunctionalInterface
+public interface MoveStrategy {
+
+    int createMoveNumber();
+}
---
+++ src/main/java/racingcar/service/strategy/RandomMoveStrategy.java
@@ -0,0 +1,14 @@
+package racingcar.service.strategy;
+
+import racingcar.util.RandomUtil;
+
+public class RandomMoveStrategy implements MoveStrategy{
+
+    private static final int RANDOM_START = 0;
+    private static final int RANDOM_END = 9;
+
+    @Override
+    public int createMoveNumber() {
+        return RandomUtil.pickNumberInRange(RANDOM_START, RANDOM_END);
+    }
+}
