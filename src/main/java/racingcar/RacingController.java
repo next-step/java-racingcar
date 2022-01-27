910132e6deb34f39807e2fbaeeda016785795898
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-27 19:25:01 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -2,19 +2,16 @@ package racingcar;
 
 import racingcar.domain.Participants;
 import racingcar.domain.RacingResult;
-import racingcar.domain.random.RandomGenerator;
 
 public class RacingController {
 
     private static final String HEAD_MESSAGE = "\n실행 결과";
     private Participants participants;
     private final int turnCount;
-    private final int participantCount;
 
     private RacingController(Participants participants, int turn) {
         this.participants = participants;
         this.turnCount = turn;
-        this.participantCount = participants.getParticipantCount();
     }
 
     public static RacingController getInstance(Participants participants, int turn) {
