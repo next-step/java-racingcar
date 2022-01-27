68cbd9b2a7c2a40cf34acc3eeaf4cf2de9871453
MinChul-Son <smc5236@naver.com>
2022-01-27 20:16:40 +0900

    🚀 feat: Service에 자동차별 현재 상태를 저장하는 로직 추가
    - drive의 반복문내에서 함께 수행
    - 각 상태를 dto 객체 내에서 문자열로 변경하여 반환하고 이를 List에 저장
    - getter를 통해 ResultView로 전달

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -2,51 +2,62 @@ package racingcar.service;
 
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
+import racingcar.domain.dto.OutputDto;
 import racingcar.domain.movable.RandomForwardStrategy;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RacingService {
 
-    private final Cars cars;
+  private final Cars cars;
+  private List<OutputDto> dtos = new ArrayList<>();
 
-    private RacingService(final Cars cars) {
-        this.cars = cars;
+  private RacingService(final Cars cars) {
+    this.cars = cars;
+  }
+
+  public static RacingService from(final Cars cars) {
+    return new RacingService(cars);
+  }
+
+  public void drive() {
+    IntStream.range(0, cars.getCars().size())
+        .forEach(idx -> {
+          cars.getCars().get(idx).moveForward();
+          OutputDto dto = new OutputDto(cars.getCars().get(idx));
+          dto.convertCurrentCarStatement();
+          dtos.add(dto);
+        });
+  }
+
+  public List<Car> notifyCurrentStatement() {
+    return cars.getCars();
+  }
+
+  public List<String> judgeWinners() {
+    return cars.getCars().stream()
+        .filter(car -> (car.getStep() == maxDriveLength() && car.getStep() > 0))
+        .map(car -> car.getName())
+        .collect(Collectors.toList());
+  }
+
+  private int maxDriveLength() {
+    return cars.getCars().stream()
+        .mapToInt(Car::getStep)
+        .max()
+        .getAsInt();
+  }
+
+  public void registerCars(final List<String> splitUserInput) {
+    for (String carName : splitUserInput) {
+      cars.getCars().add(Car.of(carName, new RandomForwardStrategy()));
     }
+  }
 
-    public static RacingService from(final Cars cars) {
-        return new RacingService(cars);
+    public List<OutputDto> getOutputDtos() {
+        return dtos;
     }
-
-    public void drive() {
-        IntStream.range(0, cars.getCars().size())
-                .forEach(idx -> cars.getCars().get(idx).moveForward());
-    }
-
-    public List<Car> notifyCurrentStatement() {
-        return cars.getCars();
-    }
-
-    public List<String> judgeWinners() {
-        return cars.getCars().stream()
-                .filter(car -> (car.getStep() == maxDriveLength() && car.getStep() > 0))
-                .map(car -> car.getName())
-                .collect(Collectors.toList());
-    }
-
-    private int maxDriveLength() {
-        return cars.getCars().stream()
-                .mapToInt(Car::getStep)
-                .max()
-                .getAsInt();
-    }
-
-    public void registerCars(final List<String> splitUserInput) {
-        for (String carName : splitUserInput) {
-            cars.getCars().add(Car.of(carName, new RandomForwardStrategy()));
-        }
-    }
-
 }
