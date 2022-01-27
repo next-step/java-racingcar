ef60d8d6fb86b89eafda19bcb63f3a67f8c34872
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-27 20:22:17 +0900

    feat(Pariticpants): 자동차 전진 기능을 Stream을 활용하여 변경

--- src/main/java/racingcar/domain/Participants.java
+++ src/main/java/racingcar/domain/Participants.java
@@ -3,8 +3,6 @@ package racingcar.domain;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.domain.car.Car;
-import racingcar.domain.car.Move;
-import racingcar.domain.random.RandomGenerator;
 import racingcar.view.UserConsole;
 
 public class Participants {
@@ -12,19 +10,9 @@ public class Participants {
     private static final String NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String ERROR_LOG = "[ERROR] ";
     private final List<Car> cars;
-    private final Move move;
 
-    private Participants(List<Car> cars) {
+    public Participants(List<Car> cars) {
         this.cars = cars;
-        this.move = Move.get();
-    }
-
-    public static Participants getInstance(List<Car> cars) {
-        return new Participants(cars);
-    }
-
-    public static Participants toParticipants(RacingResult racingResult) {
-        return Participants.getInstance(racingResult.getResult());
     }
 
     public static Participants createCars() {
@@ -40,22 +28,13 @@ public class Participants {
             System.out.println(ERROR_LOG + e.getMessage());
             return createCars();
         }
-        return Participants.getInstance(cars);
+        return new Participants(cars);
     }
 
     public RacingResult race() {
-        int carSize = cars.size();
-        for (int i = 0; i < carSize; i++) {
-            moveCarIfPositionChanged(i, move.isSatisfiedMoveCondition(new RandomGenerator()));
-        }
+        cars.stream().forEach(car -> car.go());
 
         return RacingResult.getInstance(cars);
-    }
-
-    public void moveCarIfPositionChanged(int index, boolean bool) {
-        if (bool) {
-            this.cars.get(index).go();
-        }
     }
 
     public List<Car> getParticipants() {
