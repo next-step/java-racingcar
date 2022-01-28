092d35362467b13a677bcd15cfda9f279a1ae3f7
hochan222 <hochan049@gmail.com>
2022-01-28 19:54:46 +0900

    refactor: carMove 람다 함수 이용
    - 이해 안되는 문법.. 드디어 찾았다
      - 람다 식 및 함수형 인터페이스: 팁 및 모범 사례: https://recordsoflife.tistory.com/557
    
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
+import racingcar.domain.movable.MovableStrategy;
 import racingcar.domain.movable.RandomMovableStrategy;
 import racingcar.utils.Parser;
 
@@ -17,22 +18,18 @@ public class CarsTest {
     @DisplayName("각 플레이어별 이름이 (5자) 지정된 글자 수 이내일 경우 통과하며, 그렇지 않을 경우 IllegalArgumentException을 발생")
     @Test
     void testNameLengthValid() {
-        // Given
         List<String> carNames = Parser.parseCarNames("OK,Invalid,A_Car");
 
-        // When
         assertThatThrownBy(() -> new Cars(carNames.stream()
             .map(Car::new)
             .collect(Collectors.toList())
         ))
-            // Then
             .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("3칸 이동하는 A와 2칸 이동하는 B가 주어졌을 때, 우승자를 필터링하면 A가 필터된다.")
     @Test
     void testCarDistancePrintingValid() {
-        // Given
         List<Boolean> aMoves = Arrays.asList(true, false, true, false, true);
         List<Boolean> bMoves = Arrays.asList(false, false, true, false, true);
 
@@ -44,19 +41,13 @@ public class CarsTest {
 
         Cars cars = new Cars(Arrays.asList(a, b));
 
-        // Then
         assertThat(cars.filterWinners())
             .isEqualTo(Arrays.asList(a));
     }
 
     private void carMove(Car car, List<Boolean> moves) {
         for (boolean carMoved: moves) {
-            car.run(new RandomMovableStrategy() {
-                @Override
-                public boolean isMovable() {
-                    return carMoved;
-                }
-            });
+            car.run(() -> carMoved);
         }
     }
 }
