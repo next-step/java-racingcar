030a062ad0eed72fc151c236bca9db3fd952201f
Hae-Riri <ehl3288@naver.com>
2022-01-28 20:52:21 +0900

    refactor: delimiter 매직넘버 상수치환

--- src/main/java/racing/CarGenerator.java
+++ src/main/java/racing/CarGenerator.java
@@ -6,11 +6,13 @@ import java.util.stream.Collectors;
 
 public class CarGenerator {
 
+    private static final String SPLIT_DELIMITER = ",";
+
     private CarGenerator() {
     }
 
     public static List<Car> createCars(String carNames) {
-        return Arrays.stream(carNames.split(","))
+        return Arrays.stream(carNames.split(SPLIT_DELIMITER))
             .map(Car::new)
             .collect(Collectors.toList());
     }
