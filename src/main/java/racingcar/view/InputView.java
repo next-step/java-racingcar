35fc72f3a5a34aec4611529e8b1182c1ffe62bdc
Ji In Lee <loveted0225@naver.com>
2022-01-28 21:26:06 +0900

    style(InputView): static final 필드 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private final String INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    private final String ASK_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+    private static final String INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String ASK_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
 
     public String[] inputCarName() {
         System.out.println(INPUT_GUIDE_MESSAGE);
