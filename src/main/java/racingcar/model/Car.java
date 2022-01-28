c07479a8450fdea2c31d186a5bc67ee57dec7853
Ji In Lee <loveted0225@naver.com>
2022-01-28 21:24:55 +0900

    style(Car): static final 필드 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,8 +4,8 @@ public class Car {
 
     private final String carName;
     private String position;
-    private final String CAR_NAME_LONGER_THAN_FIVE = "[ERROR] 자동차 이름은 5글자를 초과할 수 없습니다.";
-    private final String CAR_NAME_LESS_THAN_ONE = "[ERROR] 자동차 이름은 최소 1글자 이상이어야 합니다. ";
+    private static final String CAR_NAME_LONGER_THAN_FIVE = "[ERROR] 자동차 이름은 5글자를 초과할 수 없습니다.";
+    private static final String CAR_NAME_LESS_THAN_ONE = "[ERROR] 자동차 이름은 최소 1글자 이상이어야 합니다. ";
 
     public Car(String carName) {
         validateCarNameLength(carName);
