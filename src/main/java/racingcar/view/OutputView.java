f4373ee584ff3f0ccd4b6c9eba15fd98db332843
Ji In Lee <loveted0225@naver.com>
2022-01-28 22:29:08 +0900

    refactor(OutputView): 결과 출력 부분 리팩토링

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,16 +1,20 @@
 package racingcar.view;
 
-import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
 
 public class OutputView {
 
     private static final String WINNER_MESSAGE = "최종 우승자: ";
 
-    public void printResult(String carName, String position) {
-        System.out.println(carName + ": " + position);
+    public void printResult(List<Car> cars) {
+        for (Car car:cars){
+            System.out.println(car.getCarName() + ": " + car.getPosition());
+        }
+
     }
 
-    public void printWinner(ArrayList<String> winnerList) {
+    public void printWinner(List<String> winnerList) {
         System.out.print(WINNER_MESSAGE);
         System.out.print(String.join(",", winnerList));
     }
