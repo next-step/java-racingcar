bbc8ea7c814a435b2b16a607c1148c953d92871f
Ji In Lee <loveted0225@naver.com>
2022-01-28 23:35:58 +0900

    refactor(OutputView): position의 값 만큼 "-" 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,10 +8,18 @@ public class OutputView {
     private static final String WINNER_MESSAGE = "최종 우승자: ";
 
     public void printResult(List<Car> cars) {
-        for (Car car:cars){
-            System.out.println(car.getCarName() + ": " + car.getPosition());
+        for (Car car : cars) {
+            System.out.print(car.getCarName() + ": ");
+            printPosition(car.getPosition());
         }
+        System.out.println();
+    }
 
+    private void printPosition(int position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
     public void printWinner(List<String> winnerList) {
