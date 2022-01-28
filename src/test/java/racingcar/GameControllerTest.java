db1e5077e64ea0b79d01c9f89a8d9ef61dfb8583
hochan222 <hochan049@gmail.com>
2022-01-28 18:50:43 +0900

    test: GameController의 startGame 메서드 테스트 구현
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>

---
+++ src/test/java/racingcar/GameControllerTest.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.controller.GameController;
+import racingcar.domain.movable.RandomMovableStrategy;
+
+public class GameControllerTest {
+    @DisplayName("GameController에 ('a,b,c,d', 6)을 인자로 넣었을때, startGame 메서드가 정상 작동한다.")
+    @Test
+    void playSuccess() {
+        assertDoesNotThrow(
+            () -> new GameController("a,b,c,d", 6)
+                .startGame(RandomMovableStrategy.getInstance())
+        );
+    }
+}
