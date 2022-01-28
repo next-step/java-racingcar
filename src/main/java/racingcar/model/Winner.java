414fbbc60cfe8059b5405fb576517a39edb4f860
Ji In Lee <loveted0225@naver.com>
2022-01-28 23:39:56 +0900

    style(Winner): google java style 적용

--- src/main/java/racingcar/model/Winner.java
+++ src/main/java/racingcar/model/Winner.java
@@ -2,28 +2,31 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import racingcar.view.OutputView;
 
 public class Winner {
-    private List<Car> cars ;
-    public Winner(List<Car> car){
-        this.cars= car;
+
+    private List<Car> cars;
+
+    public Winner(List<Car> car) {
+        this.cars = car;
     }
+
     public List<String> getWinner() {
-        List<String> winnerList= new ArrayList<String>();
-        int maxPosition= getMaxPosition();
+        List<String> winnerList = new ArrayList<>();
+        int maxPosition = getMaxPosition();
         for (Car car : cars) {
-            if (car.getPosition().length() == maxPosition) {
+            if (car.getPosition() == maxPosition) {
                 winnerList.add(car.getCarName());
             }
         }
         return winnerList;
     }
-    public int getMaxPosition(){
+
+    private int getMaxPosition() {
         int maxPosition = 0;
         for (Car car : cars) {
-            if (car.getPosition().length() > maxPosition) {
-                maxPosition = car.getPosition().length();
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
 
             }
         }
