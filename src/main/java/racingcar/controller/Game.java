f9fa4e29be4a3b3b29e84f92d4b2183a3ec895b8
Ji In Lee <loveted0225@naver.com>
2022-01-28 23:34:32 +0900

    refactor(Game): RandomGenerator 대신 Random 이용

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -2,9 +2,9 @@ package racingcar.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import racingcar.model.Car;
 import racingcar.model.Winner;
-import racingcar.util.RandomGenerator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -12,14 +12,15 @@ public class Game {
 
     private static final String TRY_COUNT_LESS_THAN_ZERO = "[ERROR] 시도 횟수는 1회 이상이어야 합니다.";
     private static final String GAME_RESULT_MESSAGE = "출력결과";
-    private static final int MINIMUM_TRY_COUNT=0;
+    private static final int MINIMUM_TRY_COUNT = 0;
     private List<Car> cars = new ArrayList<>();
+
     public void play() {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         String[] carNames = inputView.inputCarName();
 
-        for (String carName: carNames) {
+        for (String carName : carNames) {
             cars.add(new Car(carName));
         }
         int tryCount = inputView.inputTryCount();
@@ -29,21 +30,21 @@ public class Game {
         for (int i = 0; i < tryCount; i++) {
             moveForwardByCarsSize();
             outputView.printResult(cars);
-            System.out.println();
         }
         Winner winner = new Winner(cars);
         outputView.printWinner(winner.getWinner());
     }
+
     private void validateTryCount(int tryCount) {
-        if (tryCount <=MINIMUM_TRY_COUNT) {
+        if (tryCount <= MINIMUM_TRY_COUNT) {
             throw new IllegalArgumentException(TRY_COUNT_LESS_THAN_ZERO);
         }
     }
 
     public void moveForwardByCarsSize() {
         for (Car car : cars) {
-            RandomGenerator randomGenerator = new RandomGenerator();
-            int moveOrNot = randomGenerator.generateRandomNumber();
+            Random random = new Random();
+            int moveOrNot = random.nextInt(9);
             car.moveForward(moveOrNot);
         }
     }
