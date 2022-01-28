3b1788367788839d718d34f6e25b48bc9870aa70
hochan222 <hochan049@gmail.com>
2022-01-28 18:34:01 +0900

    test: Parser 객체 테스트 작성하기
    - 자동차의 이름이 한 글자 이상 일때, 유효하다.
    - Jason, holee가 주어질 때, Parser는 trim을 수행 할 수 있다
    
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>

---
+++ src/test/java/racingcar/ParserTest.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import racingcar.utils.Parser;
+
+public class ParserTest {
+    static Stream<String> validCarNames() {
+        return Stream.of("A,B,C,D", "holee", "Jason, holee");
+    }
+
+    @DisplayName("자동차의 이름이 한 글자 이상 일때, 유효하다.")
+    @ParameterizedTest
+    @MethodSource("validCarNames")
+    void testCarNamesValid(String carNames) {
+        List<String> names = Parser.parseCarNames(carNames); // Given & When
+        assertTrue(names.size() >= 1); // Then
+    }
+
+    @DisplayName("'Jason, holee'가 주어질 때, Parser는 trim을 수행 할 수 있다.")
+    @Test
+    void testCarNamesTrimAfterSplitValid() {
+        List<String> names = Parser.parseCarNames("Jason, holee"); // Given & When
+        assertThat(names)
+            .isEqualTo(Arrays.asList("Jason", "holee"));
+    }
+}
