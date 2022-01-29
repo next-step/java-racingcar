a3449ff921b5c8f4fc37fa2ab4ab4c7e5ad65603
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-29 23:33:37 +0900

    refactor: Random 상수를 적절한 상수명으로 변경

--- src/main/java/racingcar/domain/random/RandomGenerator.java
+++ src/main/java/racingcar/domain/random/RandomGenerator.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class RandomGenerator implements RandomStrategy {
 
-    private static final Random random = new Random();
+    private static final Random RANDOM_GENERATOR = new Random();
     private static final int LIMIT = 10;
 
     public int get() {
-        return random.nextInt(LIMIT);
+        return RANDOM_GENERATOR.nextInt(LIMIT);
     }
 }
