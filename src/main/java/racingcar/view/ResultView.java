4ba15c0a3164a22b023eebae5e6e8bca96e69502
kim-svadoz <dhkdghehfdl@gmail.com>
2022-01-29 23:18:49 +0900

    feat(UI): ResultView에 기본 Message 출력 메소드 추가

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -6,6 +6,10 @@ public class ResultView {
     private static final String HEAD_MESSAGE = ENTER + "실행 결과";
     private static final String FINAL_WINNER_MESSAGE = "최종 우승자: ";
 
+    public static void printMessage(final String message) {
+        System.out.println(message);
+    }
+
     public static void printRacingResult(final String result) {
         System.out.println(HEAD_MESSAGE);
         System.out.print(result);
