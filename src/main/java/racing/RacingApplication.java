6ef8f228feef440b2c1fe22b37e74a7ddb45aea3
Hae-Riri <ehl3288@naver.com>
2022-01-29 17:34:18 +0900

    refactor: RacingApplication 코드 분리

--- src/main/java/racing/RacingApplication.java
+++ src/main/java/racing/RacingApplication.java
@@ -1,21 +1,16 @@
 package racing;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class RacingApplication {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String carNames = sc.nextLine();
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
-        System.out.println("최종우승자: " + String.join(", ",
-                new Judgement(
-                    new Racing(
-                        CarGenerator.createCars(carNames)
-                    ).race()
-                ).judgeWinner()
-            )
-        );
+        List<Car> cars = new Racing(CarGenerator.createCars(carNames)).race();
+        System.out.println("최종우승자: " + String.join(", ", new Judgement(cars).judgeWinner()));
     }
 }
