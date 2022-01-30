2d3032badfb10d7f52d769b20479c4b48c60fc09
MinChul-Son <smc5236@naver.com>
2022-01-30 16:01:17 +0900

    🚀 refactor: 재입력을 담당하는 객체 생성

---
+++ src/main/java/racingcar/view/ProgressingConsole.java
@@ -0,0 +1,43 @@
+package racingcar.view;
+
+import java.util.Objects;
+
+public class ProgressingConsole {
+
+    private boolean isAbNormalCarsName;
+    private boolean isAbNormalRacingTime;
+
+    public ProgressingConsole() {
+        this.isAbNormalCarsName = true;
+        this.isAbNormalRacingTime = true;
+    }
+
+    public ProgressingConsole(final boolean isAbNormalCarsName, final boolean isAbNormalRacingTime) {
+        this.isAbNormalCarsName = isAbNormalCarsName;
+        this.isAbNormalRacingTime = isAbNormalRacingTime;
+    }
+
+    public static boolean isContinue(final ProgressingConsole parser) {
+        ProgressingConsole parser1 = new ProgressingConsole(false, false);
+        return parser.equals(parser1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ProgressingConsole that = (ProgressingConsole) o;
+        return isAbNormalCarsName == that.isAbNormalCarsName
+            && isAbNormalRacingTime == that.isAbNormalRacingTime;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isAbNormalCarsName, isAbNormalRacingTime);
+    }
+
+}
