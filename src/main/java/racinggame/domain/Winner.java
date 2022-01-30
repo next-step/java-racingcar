f5ce0cdc534a038ba69d8a1c186e5919363661f0
xxell8 <cell8pepper@gmail.com>
2022-01-30 18:47:20 +0900

    refactor: Change findWinner Logic using stream()

--- src/main/java/racinggame/domain/Winner.java
+++ src/main/java/racinggame/domain/Winner.java
@@ -1,22 +1,31 @@
 package racinggame.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Winner {
 
-    public static List<String> findWinner(List<Car> cars) {
-        List<String> winnerUsers = new ArrayList<>();
-        int maxLocation = findMaxLocation(cars);
-        for (Car car : cars) {
-            if (car.getLocation() == maxLocation) {
-                winnerUsers.add(car.getName());
-            }
-        }
-        return winnerUsers;
+    private final List<Car> cars;
+
+    public Winner(List<Car> cars) {
+        this.cars = cars;
     }
 
-    private static int findMaxLocation(List<Car> cars) {
+    public List<String> getWinnersName() {
+        final int maxLocation = findMaxLocation();
+        final List<Car> winners = findWinners(maxLocation);
+        return winners.stream()
+            .map(Car::getName)
+            .collect(Collectors.toList());
+    }
+
+    private List<Car> findWinners(final int maxLocation) {
+        return cars.stream()
+            .filter(car -> car.getLocation() == maxLocation)
+            .collect(Collectors.toList());
+    }
+
+    private int findMaxLocation() {
         return cars.stream()
             .mapToInt(Car::getLocation)
             .max().getAsInt();
