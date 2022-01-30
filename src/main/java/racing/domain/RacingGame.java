c35123e38a77535f5b86f221b6333154d85fdda5
0JUUU <clleo97@naver.com>
2022-01-31 04:09:10 +0900

    refactor: Cars 객체로 로직 이동

--- src/main/java/racing/domain/RacingGame.java
+++ src/main/java/racing/domain/RacingGame.java
@@ -9,30 +9,22 @@ import racing.view.OutputView;
 
 public class RacingGame {
 
-    private static final int RANGE_OF_RANDOM_NUMBER = 10;
-
-    private final List<Car> cars;
+    private Cars cars;
     private List<List<CarHistory>> raceHistories;
 
     public RacingGame() {
-        this.cars = new ArrayList<>();
     }
 
     public void start() {
         try {
             final List<String> names = InputView.getCarNames();
-            for (String name : names) {
-                cars.add(new Car(name));
-            }
-            checkCarCount(cars);
+            cars = new Cars(names);
 
             final TryNumber tryNumber = InputView.getTryNumber();
-
             raceHistories = new ArrayList<>();
             startRacing(tryNumber);
-            final int maxPosition = getMaxPosition();
-            List<String> winners = getWinners(maxPosition);
 
+            final List<String> winners = getWinners(cars.getMaxPosition(), cars.getCars());
             OutputView.printGameResult(raceHistories);
             OutputView.printWinners(winners);
         } catch (IllegalArgumentException e) {
@@ -41,47 +33,16 @@ public class RacingGame {
         }
     }
 
-    // 차들 관리
-    private List<String> getWinners(final int maxPosition) {
+    private List<String> getWinners(final int maxPosition, List<Car> cars) {
         return cars.stream()
             .filter(car -> !Objects.isNull(car.getNameBy(maxPosition)))
-            .map(car -> car.getName())
+            .map(Car::getName)
             .collect(Collectors.toList());
-    }
-
-    // 차들 관리
-    private static void checkCarCount(List<Car> cars) {
-        if (cars.isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] 최소 1대 이상의 자동차가 필요합니다.");
-        }
     }
 
     private void startRacing(final TryNumber tryNumber) {
         while (tryNumber.nextStep()) {
-            raceHistories.add(driveCars());
+            raceHistories.add(cars.driveCars());
         }
-    }
-
-    // 차들 관리
-    private List<CarHistory> driveCars() {
-        final List<CarHistory> carHistories = new ArrayList<>();
-        for (Car car : cars) {
-            car.drive(generateRandomNumber());
-            carHistories.add(new CarHistory(car.getName(), car.getPosition()));
-        }
-        return carHistories;
-    }
-
-    private int generateRandomNumber() {
-        return (int) (Math.random() * RANGE_OF_RANDOM_NUMBER);
-    }
-
-    // 차들 관리
-    private int getMaxPosition() {
-        int maxPosition = 0;
-        for (Car car : cars) {
-            maxPosition = Math.max(maxPosition, car.getPosition());
-        }
-        return maxPosition;
     }
 }
