0018d6c05bac83f345b607583bfbbb300563a913
xxell8 <cell8pepper@gmail.com>
2022-01-30 17:11:41 +0900

    fix: Add NullPointerException Handling

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -18,7 +18,7 @@ public class Application {
                 int result = calculator.calculate(parsedInput);
                 ResultView.printResult(result);
                 break;
-            } catch(IllegalArgumentException | ArithmeticException e) {
+            } catch(IllegalArgumentException | ArithmeticException | NullPointerException e) {
                 System.out.println("[ERROR] 연산 가능한 수식을 입력해주세요. (숫자, 연산자, 공백 문자만 허용됩니다.)");
             }
         }
