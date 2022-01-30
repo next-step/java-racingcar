8efc90628e61517abb25374ff7d0fe9b20820bea
hyojeong <qkrgywjd456@naver.com>
2022-01-31 02:00:51 +0900

    feat: StringProcessor 의 내부에서만 사용하는 validation 메서드 테스트 제거

--- src/test/java/strcalculator/domain/StringProcessorTest.java
+++ src/test/java/strcalculator/domain/StringProcessorTest.java
@@ -11,15 +11,6 @@ class StringProcessorTest {
 
 
     @Test
-    @DisplayName("숫자와 사칙연산을 제외한 문자가 들어왔는지 확인")
-    public void inputValidation() {
-        StringProcessor stringProcessor = new StringProcessor("1+++2+3+++");
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            stringProcessor.validation();
-        });
-    }
-
-    @Test
     @DisplayName("공백 or null 만 들어왔을때 오류 확인")
     public void checkNullOrSpace() {
         assertThrows(IllegalArgumentException.class, () -> new StringProcessor(" "));
