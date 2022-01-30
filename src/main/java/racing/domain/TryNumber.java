ccc88d89d0d2e7012439772c5c2b74e1e370b366
0JUUU <clleo97@naver.com>
2022-01-31 07:25:48 +0900

    refactor: 연산자 변경
    - -= 1  -> --

--- src/main/java/racing/domain/TryNumber.java
+++ src/main/java/racing/domain/TryNumber.java
@@ -10,7 +10,7 @@ public class TryNumber {
         checkValidTryNumber(tryNumber);
         this.tryNumber = tryNumber;
     }
-    
+
     private void checkValidTryNumber(final int tryNumber) {
         if (tryNumber < MIN_TRY_NUMBER) {
             throw new IllegalArgumentException("[ERROR] 시도 횟수는 최소 1회 이상이어야 합니다.");
@@ -30,6 +30,6 @@ public class TryNumber {
     }
 
     private void decrease() {
-        this.tryNumber -= 1;
+        this.tryNumber--;
     }
 }
