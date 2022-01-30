1fdebd3444af0560aebbfbb14f59c7d0d63d5bf7
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-01-30 19:35:42 +0900

    refactor(racingCar): Application 클래스 수정 - Application 클래스가 Controller 역할 수행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,71 @@
 package racingcar;
 
 
-import racingcar.controller.RacingController;
+import static racingcar.model.utils.common.Parsing.parsingCarName;
+import static racingcar.model.utils.common.Parsing.parsingRacingTry;
+import static racingcar.view.RacingCarUserConsole.inputCarName;
+import static racingcar.view.RacingCarUserConsole.inputRacingTry;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.controller.dto.InputDTO;
+import racingcar.controller.dto.OutputDTO;
+import racingcar.model.domain.Cars;
+import racingcar.model.domain.Racing;
+import racingcar.model.domain.RandomNumberBehavior;
+import racingcar.model.domain.Victory;
+import racingcar.model.utils.common.StringUtils;
+import racingcar.view.RacingCarUserResult;
 
 public class Application {
 
+    private static final int NUMBER_ZERO = 0;
     public static void main(String[] args) throws Exception {
-        RacingController racingController = new RacingController();
-        racingController.run();
+        InputDTO inputDTO = startGame();
+        Cars cars = new Cars(inputDTO.getSplitUserInput());
+        Racing racing = new Racing(cars.cars(), RandomNumberBehavior.getInstance());
+        for (int idx = NUMBER_ZERO; idx < inputDTO.getRacingTryCounter(); idx++) {
+            racing.drive();
+            List<OutputDTO> result = convertCar(cars);
+            RacingCarUserResult.carStatus(result);
+        }
+        Victory victory = Victory.maxCheck(cars.cars());
+        List<OutputDTO> victoryResult = convertVictory(victory);
+        RacingCarUserResult.judge(victoryResult);
+    }
+    private static List<OutputDTO> convertVictory(final Victory victory) {
+        return victory.victory()
+            .stream()
+            .map(OutputDTO::new)
+            .collect(Collectors.toList());
+    }
+
+    private static List<OutputDTO> convertCar(final Cars cars) {
+        return cars.cars().stream().map(OutputDTO::new).collect(Collectors.toList());
+    }
+
+    private static InputDTO startGame() throws IOException {
+        String name = receiveUserCarse();
+        int count = userTry();
+        List<String> parsingName = StringUtils.splitStr(name);
+        return new InputDTO(parsingName, count);
+    }
+
+    private static int userTry() throws IOException {
+        int count = 0;
+        do {
+            count = inputRacingTry();
+        } while (parsingRacingTry(count));
+        return count;
+    }
+
+    private static String receiveUserCarse() throws IOException {
+        String name = "";
+        do {
+            name = inputCarName();
+        } while (parsingCarName(name));
+        return name;
     }
 }
 
