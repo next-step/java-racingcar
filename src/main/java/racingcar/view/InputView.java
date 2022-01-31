cd90dd511719829bb467e455dddd0d76c02a5808
Ji In Lee <loveted0225@naver.com>
2022-02-01 02:06:31 +0900

    refactor(InputView): Scanner 객체 상수명 규칙에 따라 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,12 +6,12 @@ public class InputView {
 
     private static final String INPUT_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String ASK_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
-    public String[] inputCarName() {
+    public String[] inputCarNames() {
         System.out.println(INPUT_GUIDE_MESSAGE);
 
-        String userInput = scanner.nextLine();
+        String userInput = SCANNER.nextLine();
 
         return userInput.split(",");
     }
@@ -19,7 +19,7 @@ public class InputView {
     public int inputTryCount() {
         System.out.println(ASK_TRY_COUNT);
 
-        int tryCount = scanner.nextInt();
+        int tryCount = SCANNER.nextInt();
 
         return tryCount;
     }
