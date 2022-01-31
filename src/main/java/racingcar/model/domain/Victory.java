4c8c8abcf560cb4fce7494c1968b4db2d3c17dee
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-01 00:15:15 +0900

    refactor(racingCar): Victory 클래스 수정
    - isMax 메소드에 대한 책임을 Car 클래스에 부여

--- src/main/java/racingcar/model/domain/Victory.java
+++ src/main/java/racingcar/model/domain/Victory.java
@@ -7,27 +7,23 @@ import racingcar.model.vo.Car;
 
 public class Victory {
 
-    private List<Car> victory;
+    private List<Car> victories;
     private static final String MAX_DRIVE_LENGTH = "MAX 값을 구할 수 없습니다. 비교할 수 있는 정수값인지 확인하세요";
 
     private Victory(final List<Car> cars) {
-        victory = cars;
+        victories = cars;
     }
 
     public List<Car> victory() {
-        return Collections.unmodifiableList(victory);
+        return Collections.unmodifiableList(victories);
     }
 
     public static Victory maxCheck(final List<Car> cars) {
 
         int max = maxDriveLength(cars);
         return new Victory(cars.stream()
-            .filter(car -> isMax(max, car))
+            .filter(car -> car.isMax(max, car))
             .collect(Collectors.toList()));
-    }
-
-    private static boolean isMax(final int max, final Car car) {
-        return car.getStep() == max;
     }
 
     private static int maxDriveLength(final List<Car> cars) {
--- src/main/java/racingcar/model/vo/Car.java
+++ src/main/java/racingcar/model/vo/Car.java
@@ -16,6 +16,10 @@ public class Car {
         this.step++;
     }
 
+    public boolean isMax(final int max, final Car car) {
+        return car.getStep() == max;
+    }
+
     public int getStep() {
         return step;
     }
