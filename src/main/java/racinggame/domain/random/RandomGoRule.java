9df78412c8a84ccc2966d6b9ea39a8a123143527
hyojeong <qkrgywjd456@naver.com>
2022-01-31 19:13:03 +0900

    feat: RandomGoRule class 생성 밒 isGo 라는 전진 유뮤 반환 함수 생성

---
+++ src/main/java/racinggame/domain/random/RandomGoRule.java
@@ -0,0 +1,17 @@
+package racinggame.domain.random;
+
+import java.util.Random;
+
+public class RandomGoRule implements GoRule {
+
+    private static final Random random = new Random();
+    private static final int LIMIT = 10;
+    public static final int GO_MIN_VALUE=4;
+    @Override
+    public boolean isGo() {
+        if(random.nextInt(LIMIT)>=GO_MIN_VALUE){
+            return true;
+        }
+        return false;
+    }
+}
