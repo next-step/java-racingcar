ef235a88df207c721934522686d4301d41af148a
Ji In Lee <loveted0225@naver.com>
2022-02-01 02:12:22 +0900

    refactor(RacingGame): MINIMUM_TRY_COUNT 값 1로 변경
    (cherry picked from commit 6cac851cb5fa5aca6aefde538a3a7d3663f1be62)

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -8,17 +8,18 @@ import racingcar.model.Winner;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
-public class Game {
+public class RacingGame {
 
     private static final String TRY_COUNT_LESS_THAN_ZERO = "[ERROR] 시도 횟수는 1회 이상이어야 합니다.";
     private static final String GAME_RESULT_MESSAGE = "출력결과";
-    private static final int MINIMUM_TRY_COUNT = 0;
+    private static final int MINIMUM_TRY_COUNT = 1;
+    private static final int RANDOM_BOUND_VALUE=9;
     private List<Car> cars = new ArrayList<>();
 
     public void play() {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        String[] carNames = inputView.inputCarName();
+        String[] carNames = inputView.inputCarNames();
 
         for (String carName : carNames) {
             cars.add(new Car(carName));
@@ -32,22 +33,21 @@ public class Game {
             outputView.printResult(cars);
         }
         Winner winner = new Winner(cars);
-        outputView.printWinner(winner.getWinner());
+        outputView.printWinner(winner.findWinner());
     }
 
     private void validateTryCount(int tryCount) {
-        if (tryCount <= MINIMUM_TRY_COUNT) {
+        if (tryCount < MINIMUM_TRY_COUNT) {
             throw new IllegalArgumentException(TRY_COUNT_LESS_THAN_ZERO);
         }
     }
 
     public void moveForwardByCarsSize() {
+        Random random = new Random();
         for (Car car : cars) {
-            Random random = new Random();
-            int moveOrNot = random.nextInt(9);
+            int moveOrNot = random.nextInt(RANDOM_BOUND_VALUE);
             car.moveForward(moveOrNot);
         }
     }
-
 
 }
