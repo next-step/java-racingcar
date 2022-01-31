a3ec9c6fbcc39ccd60ebc79a940cb4239bd1fa3e
0JUUU <clleo97@naver.com>
2022-01-31 16:46:51 +0900

    refactor: final로 변경

--- src/test/java/racing/domain/TryNumberTest.java
+++ src/test/java/racing/domain/TryNumberTest.java
@@ -20,13 +20,13 @@ class TryNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6})
     void 레이싱_진행할_수_있으면_참_반환(int inputNumber) {
-        TryNumber tryNumber = new TryNumber(inputNumber);
+        final TryNumber tryNumber = new TryNumber(inputNumber);
         assertThat(tryNumber.hasNextStep()).isTrue();
     }
 
     @Test
     void 레이싱_진행할_수_없으면_거짓_반환() {
-        TryNumber tryNumber = new TryNumber(1);
+        final TryNumber tryNumber = new TryNumber(1);
         tryNumber.nextStep();
         assertThat(tryNumber.hasNextStep()).isFalse();
     }
