e80714f213b98142754ee4b7f324b8abb251e653
YangWoo Yu <yuyw0712@naver.com>
2022-01-31 15:51:14 +0900

    refactor: FormulaOutput.java 의미를 명확하게 출력할 수 있도록 메서드로 변경

--- src/main/java/calculator/view/FormulaOutput.java
+++ src/main/java/calculator/view/FormulaOutput.java
@@ -8,8 +8,20 @@ public class FormulaOutput {
 
     public static final String ERROR_MESSAGE = "[ERROR] ";
     public static final String ERROR_NULL_OR_EMPTY = "입력값이 Null이거나 빈 공백 문자입니다!";
-    public static final String REQUIRE_FORMULA_INPUT_MESSAGE = "계산할 연산식을 입력해주세요.";
+    private static final String REQUEST_FORMULA_INPUT_MESSAGE = "계산할 연산식을 입력해주세요.";
     public static final String ERROR_NOT_OPERATOR = "사칙연산 기호가 아닙니다.";
     public static final String ERROR_NOT_NUMBER = "피연산자가 숫자가 아닙니다.";
+
+    public static void printRequestFormula() {
+        System.out.println(REQUEST_FORMULA_INPUT_MESSAGE);
+    }
+
+    public static void printExceptionMessage(String exceptionMessage) {
+        System.out.println(ERROR_MESSAGE + exceptionMessage);
+    }
+
+    public static void printFormulaResult(Double result) {
+        System.out.println(result);
+    }
 
 }
