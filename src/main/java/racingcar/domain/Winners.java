7102322dc55bf592c25622ad80c6698c3c45040a
YangWoo Yu <yuyw0712@naver.com>
2022-01-31 17:34:40 +0900

    refacor: Winners.java 클래스 안에 책임 분리 및 구조화

---
+++ src/main/java/racingcar/domain/Winners.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Winners {
+
+    private List<String> racingWinnerLists;
+
+    public Winners(List<RacingCar> racingCarLists) {
+        this.racingWinnerLists = getWinners(racingCarLists);
+    }
+
+    private List<String> getWinners(List<RacingCar> racingCarLists) {
+        int maxPosition = getHighScore(racingCarLists);
+
+        return racingCarLists.stream()
+            .filter(car -> car.getPosition() == maxPosition)
+            .map(RacingCar::getName)
+            .collect(Collectors.toList());
+    }
+
+    private int getHighScore(List<RacingCar> racingCarLists) {
+        return racingCarLists.stream()
+            .mapToInt(RacingCar::getPosition)
+            .max().getAsInt();
+    }
+
+    public List<String> getRacingWinnerLists() {
+        return racingWinnerLists;
+    }
+}
