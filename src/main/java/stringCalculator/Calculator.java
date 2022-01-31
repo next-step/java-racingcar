94d87666a1e797de07e4eeeaf21aa3aec2f9f731
Ji In Lee <loveted0225@naver.com>
2022-02-01 02:09:47 +0900

    feat(Calculator) : 0으로 나눴을 시 예외처리
    (cherry picked from commit 4427336da8b4184f15352ffd8a483b3716308cb8)

--- src/main/java/stringCalculator/Calculator.java
+++ src/main/java/stringCalculator/Calculator.java
@@ -30,13 +30,11 @@ public class Calculator {
         }
         return result;
     }
-
-    private void checkDivideByZero(String operand) {
-        if ("0".equals(operand)) {
+    private void checkDivideByZero(String operand){
+        if("0".equals(operand)){
             throw new IllegalArgumentException("0으로 나뉠 수 없습니다");
         }
     }
-
     private int sum(int a, int b) {
         return a + b;
     }
