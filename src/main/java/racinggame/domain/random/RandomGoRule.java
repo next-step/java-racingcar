6c9d93074db34f230c956350ca410f7152064ece
hyojeong <qkrgywjd456@naver.com>
2022-02-01 21:53:13 +0900

    refactor: if문 줄이기

--- src/main/java/racinggame/domain/random/RandomGoRule.java
+++ src/main/java/racinggame/domain/random/RandomGoRule.java
@@ -6,12 +6,10 @@ public class RandomGoRule implements GoRule {
 
     private static final Random random = new Random();
     private static final int LIMIT = 10;
-    public static final int GO_MIN_VALUE=4;
+    public static final int GO_MIN_VALUE = 4;
+
     @Override
     public boolean isGo() {
-        if(random.nextInt(LIMIT)>=GO_MIN_VALUE){
-            return true;
-        }
-        return false;
+        return random.nextInt(LIMIT) >= GO_MIN_VALUE;
     }
 }
