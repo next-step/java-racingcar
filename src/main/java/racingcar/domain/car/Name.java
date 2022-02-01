db3ede174b530a3fedd140bb8eb4668b24e84bdd
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-01 12:31:25 +0900

    feat(car#Name): 이름에 대한 최소길이 검증 로직 추가
    - 외부에서 사용하지 않는 메서드는 private 처리

--- src/main/java/racingcar/domain/car/Name.java
+++ src/main/java/racingcar/domain/car/Name.java
@@ -3,7 +3,9 @@ package racingcar.domain.car;
 public class Name {
 
     private static final int MAXIMUM_ALLOWED_NAME_LENGTH = 5;
-    private static final String NAME_LENGTH_ERROR_MESSAGE = "자동차 이름이 " + MAXIMUM_ALLOWED_NAME_LENGTH + "자를 초과합니다.";
+    private static final int MINIMUM_ALLOWED_NAME_LENGTH = 1;
+    private static final String OVER_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름이 " + MAXIMUM_ALLOWED_NAME_LENGTH + "자를 초과합니다.";
+    private static final String UNDER_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름이 " + MAXIMUM_ALLOWED_NAME_LENGTH + "자를 넘어가지 않습니다.";
 
     private final String name;
 
@@ -12,9 +14,12 @@ public class Name {
         this.name = name;
     }
 
-    public void validateName(final String name) {
+    private void validateName(final String name) {
         if (name.length() > MAXIMUM_ALLOWED_NAME_LENGTH) {
-            throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
+            throw new IllegalArgumentException(OVER_NAME_LENGTH_ERROR_MESSAGE);
+        }
+        if (name.length() <= MINIMUM_ALLOWED_NAME_LENGTH) {
+            throw new IllegalArgumentException(UNDER_NAME_LENGTH_ERROR_MESSAGE);
         }
     }
 
