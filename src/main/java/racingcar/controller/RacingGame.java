bdfc3201166754c223c483072f3a8eadfe44fed8
Ji In Lee <loveted0225@naver.com>
2022-02-01 18:09:50 +0900

    refactor(RacingGame): 시도회수에 대한 책임과 자동차 전진 역할을 TryCount와 Cars 객체로 위임

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,53 +1,35 @@
 package racingcar.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.model.TryCount;
 import racingcar.model.Winner;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingGame {
-
-    private static final String TRY_COUNT_LESS_THAN_ZERO = "[ERROR] 시도 횟수는 1회 이상이어야 합니다.";
     private static final String GAME_RESULT_MESSAGE = "출력결과";
-    private static final int MINIMUM_TRY_COUNT = 1;
-    private static final int RANDOM_BOUND_VALUE=9;
-    private List<Car> cars = new ArrayList<>();
+
+    private List<Car> raceResult;
 
     public void play() {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         String[] carNames = inputView.inputCarNames();
-
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
-        int tryCount = inputView.inputTryCount();
-        validateTryCount(tryCount);
+        Cars cars= new Cars(carNames);
+        TryCount tryCount = new TryCount(inputView.inputTryCount());
 
         System.out.println(GAME_RESULT_MESSAGE);
-        for (int i = 0; i < tryCount; i++) {
-            moveForwardByCarsSize();
-            outputView.printResult(cars);
+        int moveCount=0;
+        while(tryCount.isUnderTryCount(moveCount)){
+            raceResult=cars.moveForwardByCarsSize();
+            outputView.printResult(raceResult);
+            moveCount++;
         }
-        Winner winner = new Winner(cars);
+
+        Winner winner = new Winner(raceResult);
         outputView.printWinner(winner.findWinner());
-    }
-
-    private void validateTryCount(int tryCount) {
-        if (tryCount < MINIMUM_TRY_COUNT) {
-            throw new IllegalArgumentException(TRY_COUNT_LESS_THAN_ZERO);
-        }
-    }
-
-    public void moveForwardByCarsSize() {
-        Random random = new Random();
-        for (Car car : cars) {
-            int moveOrNot = random.nextInt(RANDOM_BOUND_VALUE);
-            car.moveForward(moveOrNot);
-        }
     }
 
 }
