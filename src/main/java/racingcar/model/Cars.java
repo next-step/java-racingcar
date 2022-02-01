3201b9712d8fd9977bff9dbb1726ca87ffe69eae
Ji In Lee <loveted0225@naver.com>
2022-02-01 18:05:08 +0900

    refactor(Cars): 자동차들을 관리하고 앞으로 전진시키는 역할 위임 (Game -> Cars)

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,26 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Cars {
+    private static final int RANDOM_BOUND_VALUE=9;
+
+    private List<Car> cars= new ArrayList<>();
+    public Cars(String [] carNames){
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+    }
+
+    public List<Car> moveForwardByCarsSize() {
+        Random random = new Random();
+        for (Car car : cars) {
+            int moveOrNot = random.nextInt(RANDOM_BOUND_VALUE);
+            car.moveForward(moveOrNot);
+        }
+        return cars;
+    }
+
+}
