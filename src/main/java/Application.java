6f35b7c57ce899f160469bfa261fe716ba0b230f
SeungyeonJung <crowsy01@gmail.com>
2022-02-02 20:35:21 +0900

    refactor: Magic number

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,14 +4,17 @@ import racingcar.Race;
 
 public class Application {
 
+    private static final int CALCULATOR = 1;
+    private static final int RACE = 2;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("문자열 계산기를 사용하시려면 1, 자동차 경주를 사용하시려면 2를 눌러주세요.");
-        int flag = sc.nextInt();
-        if (flag == 1) {
+        int gameName = sc.nextInt();
+        if (gameName == CALCULATOR) {
             Calculator.execute();
         }
-        if (flag == 2) {
+        if (gameName == RACE) {
             Race.execute();
         }
     }
