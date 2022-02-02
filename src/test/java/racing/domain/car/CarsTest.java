75ebd6a162a9cf779f3ce4f9c79b5f7f0f6962b5
0JUUU <clleo97@naver.com>
2022-02-03 04:01:51 +0900

    feat: Cars에 대한 테스트코드 작성 (drive() 제외)

--- src/test/java/racing/domain/car/CarsTest.java
+++ src/test/java/racing/domain/car/CarsTest.java
@@ -1,6 +1,7 @@
 package racing.domain.car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,10 +24,43 @@ class CarsTest {
         );
     }
 
+    private static Stream<Arguments> 중복된_이름_확인() {
+        return Stream.of(
+            Arguments.of(
+                Arrays.asList(
+                    new Car("a", 2),
+                    new Car("a", 4),
+                    new Car("C", 1)
+                )
+            )
+        );
+    }
+
+    private static Stream<Arguments> 자동차_개수_확인() {
+        return Stream.of(
+            Arguments.of(Arrays.asList())
+        );
+    }
+
     @ParameterizedTest
     @MethodSource
     void Car들_중_최대_위치_값_확인(List<Car> parameterCars) {
         final Cars cars = new Cars(parameterCars);
         assertThat(cars.getMaxPosition()).isEqualTo(4);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void 중복된_이름_확인(List<Car> parameterCars) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new Cars(parameterCars)).withMessage("[ERROR] 중복된 이름은 들어갈 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void 자동차_개수_확인(List<String> parameterCars) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new Cars(parameterCars, 0))
+            .withMessage("[ERROR] 자동차 개수는 적어도 1개 이상이어야 합니다.");
     }
 }
