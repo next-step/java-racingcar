000fa7499e13450ceaf69ea85f0a02646800821f
0JUUU <clleo97@naver.com>
2022-02-03 04:00:55 +0900

    refactor: 불필요한 import문 제거

--- src/test/java/racing/domain/RacingGameTest.java
+++ src/test/java/racing/domain/RacingGameTest.java
@@ -1,12 +1,10 @@
 package racing.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
