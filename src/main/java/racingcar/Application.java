9cf590e3b921b028933b5a64c933e43ca05d65e9
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-04 00:34:32 +0900

    feat(controller): 컨트롤러 메서드 분리 및 간소화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,38 +1,11 @@
 package racingcar;
 
 import racingcar.controller.RacingController;
-import racingcar.domain.car.Participants;
-import racingcar.domain.car.Turn;
-import racingcar.domain.dto.ParticipantsRequestDto;
-import racingcar.domain.dto.TurnRequestDto;
-import racingcar.view.InputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        Participants participants = setParticipants();
-        int turnCount = setTurn().getValue();
-
-        RacingController racingController = RacingController.getInstance(participants, turnCount);
+        RacingController racingController = new RacingController();
         racingController.start();
-        racingController.printGameResult();
-    }
-
-    public static Participants setParticipants() {
-        try {
-            return ParticipantsRequestDto.toEntity(InputView.setNames());
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            return setParticipants();
-        }
-    }
-
-    public static Turn setTurn() {
-        try {
-            return TurnRequestDto.toEntity(InputView.setTurnCount());
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            return setTurn();
-        }
     }
 }
--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,41 +1,53 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import racingcar.domain.car.Participants;
+import racingcar.domain.dto.ParticipantsRequestDto;
+import racingcar.domain.dto.TurnRequestDto;
 import racingcar.domain.racing.RacingGame;
-import racingcar.domain.racing.RacingRecord;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
+import racingcar.view.RacingRecord;
 
 public class RacingController {
 
-    private final Participants participants;
-    private final RacingRecord racingRecord;
-    private final int turnCount;
-    private String winner;
-
-    private RacingController(Participants participants, int turn) {
-        this.participants = participants;
-        this.turnCount = turn;
-        this.racingRecord = new RacingRecord();
-    }
-
-    public static RacingController getInstance(Participants participants, int turn) {
-        return new RacingController(participants, turn);
-    }
-
     public void start() {
-        RacingGame racingGame = new RacingGame(participants, turnCount);
-        winner = racingGame.gameStart(racingRecord).stream()
-            .reduce((first, second) -> second)
-            .orElseThrow(() -> new RuntimeException())
-            .getWinner();
+        RacingGame racingGame = makeRacingGame();
+
+        List<Participants> participants = racingGame.gameStart();
+
+        printRacingRecord(participants);
+        printWinner(participants);
     }
 
-    public void printGameResult() {
-        OutputView.printRacingResult(getRacingProcess());
-        OutputView.printWinner(winner);
+    private RacingGame makeRacingGame() {
+        try {
+            return new RacingGame(makeParticipants(), makeTurnCount());
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return makeRacingGame();
+        }
     }
 
-    public String getRacingProcess() {
-        return racingRecord.getResultRecord();
+    private Participants makeParticipants() {
+        ParticipantsRequestDto participantsRequestDto = InputView.setNames();
+        return participantsRequestDto.toEntity();
+    }
+
+    private int makeTurnCount() {
+        TurnRequestDto turnRequestDto = InputView.setTurnCount();
+        return turnRequestDto.toEntity().getValue();
+    }
+
+    public void printRacingRecord(final List<Participants> cars) {
+        for (Participants participants : cars) {
+            RacingRecord.appendRecord(participants.getParticipants());
+        }
+        OutputView.printRacingResult(RacingRecord.getResultRecord());
+    }
+
+    public void printWinner(final List<Participants> cars) {
+        OutputView.printWinner(cars.get(cars.size() - 1).findWinners());
     }
 }
