7ef57af0bd8caac6637e258299660d5895f344a4
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-04 00:38:43 +0900

    fix: RacingGame의 반환값을 List<Pariticpants>로 변경

--- src/main/java/racingcar/domain/racing/RacingGame.java
+++ src/main/java/racingcar/domain/racing/RacingGame.java
@@ -6,20 +6,22 @@ import racingcar.domain.car.Participants;
 
 public class RacingGame {
 
-    private final Participants participants;
     private final int turnCount;
+    private Participants participants;
 
     public RacingGame(Participants participants, int turnCount) {
         this.participants = participants;
         this.turnCount = turnCount;
     }
 
-    public List<RacingResult> gameStart(RacingRecord racingRecord) {
-        List<RacingResult> results = new ArrayList<>();
+    public List<Participants> gameStart() {
+        List<Participants> cars = new ArrayList<>();
+
         for (int i = 0; i < turnCount; i++) {
-            results.add(participants.race());
-            racingRecord.appendRecord(results.get(i));
+            participants = participants.race();
+            cars.add(participants);
         }
-        return results;
+
+        return new ArrayList<>(cars);
     }
 }
