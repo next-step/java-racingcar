1ceee66d94b9cd9932fa4208a3e4324ce1d97ba3
0JUUU <clleo97@naver.com>
2022-02-05 03:37:49 +0900

    feat: Cars에 대한 테스트코드 추가
    - driveCars()

--- src/test/java/racing/domain/car/CarsTest.java
+++ src/test/java/racing/domain/car/CarsTest.java
@@ -3,6 +3,7 @@ package racing.domain.car;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -14,32 +15,16 @@ class CarsTest {
 
     private static Stream<Arguments> Car들_중_최대_위치_값_확인() {
         return Stream.of(
-            Arguments.of(
-                Arrays.asList(
-                    new Car("a", 2),
-                    new Car("b", 4),
-                    new Car("C", 1)
-                )
-            )
-        );
+            Arguments.of(Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))));
     }
 
     private static Stream<Arguments> 중복된_이름_확인() {
         return Stream.of(
-            Arguments.of(
-                Arrays.asList(
-                    new Car("a", 2),
-                    new Car("a", 4),
-                    new Car("C", 1)
-                )
-            )
-        );
+            Arguments.of(Arrays.asList(new Car("a", 2), new Car("a", 4), new Car("C", 1))));
     }
 
     private static Stream<Arguments> 자동차_개수_확인() {
-        return Stream.of(
-            Arguments.of(Arrays.asList())
-        );
+        return Stream.of(Arguments.of(Arrays.asList()));
     }
 
     @ParameterizedTest
@@ -52,15 +37,51 @@ class CarsTest {
     @ParameterizedTest
     @MethodSource
     void 중복된_이름_확인(List<Car> parameterCars) {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> new Cars(parameterCars)).withMessage("[ERROR] 중복된 이름은 들어갈 수 없습니다.");
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
+            () -> new Cars(parameterCars)).withMessage("[ERROR] 중복된 이름은 들어갈 수 없습니다.");
     }
 
     @ParameterizedTest
     @MethodSource
     void 자동차_개수_확인(List<String> parameterCars) {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> new Cars(parameterCars, 0))
-            .withMessage("[ERROR] 자동차 개수는 적어도 1개 이상이어야 합니다.");
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
+            () -> new Cars(parameterCars, 0)).withMessage("[ERROR] 자동차 개수는 적어도 1개 이상이어야 합니다.");
+    }
+
+    private static Stream<Arguments> Car들_움직임_확인() {
+        return Stream.of(
+            Arguments.of(4, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))),
+            Arguments.of(5, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))),
+            Arguments.of(6, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))),
+            Arguments.of(7, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))),
+            Arguments.of(8, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))),
+            Arguments.of(9, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void Car들_전진_움직임_확인(int movableNumber, List<Car> parameterCars) {
+        Cars cars = new Cars(parameterCars);
+        List<Car> moveCars = new ArrayList<>();
+        for (Car car : parameterCars) {
+            moveCars.add(new Car(car.getNameValue(), car.getPositionValue() + 1));
+        }
+        assertThat(cars.driveCars(() -> movableNumber)).isEqualTo(moveCars);
+    }
+
+    private static Stream<Arguments> Car들_움직이지_않는_것_확인() {
+        return Stream.of(
+            Arguments.of(0, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))),
+            Arguments.of(1, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))),
+            Arguments.of(2, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))),
+            Arguments.of(3, Arrays.asList(new Car("a", 2), new Car("b", 4), new Car("C", 1))));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void Car들_움직이지_않는_것_확인(int movableNumber, List<Car> parameterCars) {
+        Cars cars = new Cars(parameterCars);
+        List<Car> moveCars = new ArrayList<>();
+        assertThat(cars.driveCars(() -> movableNumber)).isEqualTo(parameterCars);
     }
 }
