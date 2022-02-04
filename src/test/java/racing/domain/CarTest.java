b2f25577d60e17a07330d28596a4c1998858ae93
Hae-Riri <ehl3288@naver.com>
2022-02-05 04:37:01 +0900

    refactor: CarTest 패키지 변경

---
+++ src/test/java/racing/domain/CarTest.java
@@ -0,0 +1,31 @@
+package racing.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    private MoveRule moveRule;
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        moveRule = new MoveRule(4);
+    }
+
+    @Test
+    void 자동차_이름이_5글자_이상_에러() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Car("포르쉐입니다", moveRule));
+    }
+
+    @Test
+    void 이동전략에_따라_이동을_판단한다() {
+        car = new Car("포르쉐", 0 ,new MoveRule(4));
+        car.moveByRule(5);
+        assertThat(car.getDistance()).isEqualTo(1);
+    }
+}
