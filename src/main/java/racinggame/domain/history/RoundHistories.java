556f0d1b1e5a430bf325835fd177420649c04d05
hyojeong <qkrgywjd456@naver.com>
2022-02-05 02:42:25 +0900

    refactor: captureRoundHistory 매개변수 변경
    -Cars 타입에서 List<Car>로 변경하여,
    foreach에서 getCars()제거

--- src/main/java/racinggame/domain/history/RoundHistories.java
+++ src/main/java/racinggame/domain/history/RoundHistories.java
@@ -3,25 +3,24 @@ package racinggame.domain.history;
 import java.util.ArrayList;
 import java.util.List;
 import racinggame.domain.car.Car;
-import racinggame.domain.car.Cars;
 import racinggame.domain.winner.Winners;
 
 public class RoundHistories {
 
+    private static final int LAST_ROUND_HISTORY_SUB_NUMBER = 1;
     private final List<RoundHistory> allRoundHistory = new ArrayList<>();
-    private static final int LAST_ROUND_HISTORY_SUB_NUMBER=1;
 
-    public void captureRoundHistory(final Cars cars) {
+    public void captureRoundHistory(final List<Car> cars) {
         List<Car> carsCapture = new ArrayList<>();
-        for (Car car : cars.getCars()) {
+        for (Car car : cars) {
             carsCapture.add(new Car(car.getCarName(), car.getLocation()));
         }
         allRoundHistory.add(new RoundHistory(carsCapture));
-
     }
 
     private int findMaxLocation() {
-        RoundHistory roundHistory = allRoundHistory.get(allRoundHistory.size() - LAST_ROUND_HISTORY_SUB_NUMBER);
+        RoundHistory roundHistory = allRoundHistory.get(
+            allRoundHistory.size() - LAST_ROUND_HISTORY_SUB_NUMBER);
         int maxLocation = 0;
         for (Car car : roundHistory.getRoundHistory()) {
             maxLocation = Math.max(car.getLocation(), maxLocation);
@@ -30,7 +29,8 @@ public class RoundHistories {
     }
 
     public Winners findWinners() {
-        RoundHistory roundHistory = allRoundHistory.get(allRoundHistory.size() - LAST_ROUND_HISTORY_SUB_NUMBER);
+        RoundHistory roundHistory = allRoundHistory.get(
+            allRoundHistory.size() - LAST_ROUND_HISTORY_SUB_NUMBER);
         return new Winners(roundHistory.getRoundHistory(), this.findMaxLocation());
     }
 
