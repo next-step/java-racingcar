0817579ac7d931ff206e54fbc83ca4db0baf716c
MinChul-Son <smc5236@naver.com>
2022-02-04 10:47:04 +0900

    🚀 test: RacingGame 테스트코드 추가

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -12,25 +12,33 @@ import racingcar.view.ResultView;
 import static org.assertj.core.api.Assertions.*;
 
 class RacingGameTest {
+
     RacingGame racingGame;
+    List<String> carNames;
 
     @BeforeEach
     void setUp() {
-        List<String> carNames = new ArrayList<>(Arrays.asList("c", "java", "jpa"));
+        carNames = new ArrayList<>(Arrays.asList("c", "java", "jpa"));
         racingGame = RacingGame.from(Cars.of(carNames, new FixedForwardStrategy(true)));
     }
 
     @Test
-    void winnerTest() {
+    void 가장_많이_전진한_자동차가_우승한다() {
         racingGame.drive(3, ResultView.instance());
         assertThat(racingGame.judgeWinners().getResult()).containsExactly("c", "java", "jpa");
     }
 
     @Test
-    void noWinnerTest() {
+    void 아무도_전진하지_못했다면_우승자는_없다() {
         assertThat(racingGame.judgeWinners().getResult().size()).isEqualTo(0);
     }
 
-
+    @Test
+    void 자동차를_등록할_수_있다() {
+        Cars cars = Cars.instance();
+        racingGame = RacingGame.from(cars);
+        racingGame.registerCars(carNames);
+        assertThat(cars.getCars().size()).isEqualTo(3);
+    }
 
 }
