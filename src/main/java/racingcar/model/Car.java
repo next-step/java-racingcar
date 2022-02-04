084c2dc8f95af96a3d9f330fc8e6690d5a6e3014
fpal95 <snc7303@gmail.com>
2022-02-04 17:09:17 +0900

    Refactor(Car): checkMaxPosition 리턴 값 수정

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,9 +1,9 @@
 package racingcar.model;
 
 import racingcar.util.Message;
-import racingcar.util.RandomGenerator;
 
 public class Car {
+
     private static final int MOVING_CONDITION = 4;
     private static final int NAME_LENGTH = 5;
 
@@ -20,21 +20,20 @@ public class Car {
         if (randomNumber >= MOVING_CONDITION) {
             this.position += "-";
         }
+    }
 
+    public int checkMaxPosition(int maxPosition) {
+        return Math.max(this.position.length(), maxPosition);
+    }
+
+    public boolean isMaxPosition(int maxPosition) {
+        return this.position.length() == maxPosition;
     }
 
     private void validateCarNameLength(String carName) {
         if (carName.length() > NAME_LENGTH) {
             throw new IllegalArgumentException(Message.CAR_NAME_ERROR);
         }
-    }
-
-    public boolean checkMaxPosition(int maxPosition) {
-        return this.position.length() > maxPosition;
-    }
-
-    public boolean isMaxPosition(int maxPosition) {
-        return this.position.length() == maxPosition;
     }
 
     public String getCarName() {
