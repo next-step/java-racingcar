335cf5b1a2c302f49b79a3cd8b6ca5b6122035df
chanu94 <qkrcksdn0208@naver.com>
2022-02-06 01:50:34 +0900

    refactor : Validator 매직넘버 처리

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -7,13 +7,14 @@ public class Validator {
 
     private static final ArrayList<String> OPERATOR_CHAR = new ArrayList<>(
         Arrays.asList("+", "-", "*", "/"));
+    private static final String WHITE_SPACE = " ";
 
     public static String[] stringCheck(String input) {
         if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] Null이나 빈 공백 문자는 입력할 수 없습니다.");
         }
 
-        String[] splitInput = input.split(" ");
+        String[] splitInput = input.split(WHITE_SPACE);
         for (int i = 0; i < splitInput.length; i++) {
             if (i % 2 == 1) {
                 operatorCheck(splitInput[i]);
