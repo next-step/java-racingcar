2f430a07682d8bc22a09634523580637a48942e3
Gal2o <hunmin8788@gmail.com>
2022-02-05 22:25:41 +0900

    fix(RacingCar): 테스트 코드 리스트 중복 제거

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -17,7 +17,7 @@ class RacingCarTest {
     @BeforeEach
     @DisplayName("테스트에 필요한 자동차들을 생성한다.")
     void initCars() {
-        List<String> carNames = new ArrayList<>(Arrays.asList("aCar", "bCar", "cCar", "dCar"));
+        List<String> carNames = Arrays.asList("aCar", "bCar", "cCar", "dCar");
 
         madeRacingCars = RacingCar.setRacingCars(carNames);
     }
