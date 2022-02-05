0b3864a689f1367ff63256dc8433903fb39ace5b
Gal2o <hunmin8788@gmail.com>
2022-02-05 22:22:36 +0900

    refactor(RacingCar): 매직넘버 상수화

--- src/main/java/racingcar/Console.java
+++ src/main/java/racingcar/Console.java
@@ -8,12 +8,14 @@ public class Console {
 
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+    private static final int CAR_NAME_MAX = 5;
+    private static final String DELIMETER = ",";
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> setRacingCarNames() {
         System.out.println(INPUT_CAR_NAME);
 
-        List<String> carNames = Arrays.asList(scanner.nextLine().split(","));
+        List<String> carNames = Arrays.asList(scanner.nextLine().split(DELIMETER));
 
         validateCarNameEmpty(carNames);
 
@@ -31,7 +33,7 @@ public class Console {
     }
 
     private void validateCarNameLength(String carName) {
-        if (carName.length() > 5) {
+        if (carName.length() > CAR_NAME_MAX) {
             throw new IllegalArgumentException(RacingCarException.CAR_NAME_INVALID);
         }
     }
