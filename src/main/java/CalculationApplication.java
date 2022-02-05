44a7bbf28ac4f3fa054fc3da8aa9bb99d9bab5c1
chanu94 <qkrcksdn0208@naver.com>
2022-02-06 01:52:29 +0900

    feat : 문자열 계산기 input 안내 문구 추가

--- src/main/java/CalculationApplication.java
+++ src/main/java/CalculationApplication.java
@@ -11,6 +11,7 @@ public class CalculationApplication {
     }
 
     public static void calculate() throws IOException {
+        System.out.print("문자열을 입력해주세요 : ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = Validator.stringCheck(br.readLine());
