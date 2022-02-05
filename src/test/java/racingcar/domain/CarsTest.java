17dfd179c9d859e2e80cb91e0f55c72ec2e978d5
MinChul-Son <smc5236@naver.com>
2022-02-05 22:34:15 +0900

    🚀 test: Cars 테스트 코드 추가

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.movable.RandomForwardStrategy;
+
+class CarsTest {
+
+    @Test
+    void 파싱된_자동차_이름으로부터_Car_객체를_생성해_내부에_저장할_수_있다() {
+        List<String> input = new ArrayList<>(Arrays.asList("tdd", "lotto", "race"));
+        Cars cars = Cars.of(input, new RandomForwardStrategy());
+
+        Assertions.assertThat(cars.getCars().size()).isEqualTo(3);
+    }
+
+}
