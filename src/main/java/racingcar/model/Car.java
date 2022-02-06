a6f2cd1f8d6d2a008d435eedcdbd44c2dcb1131e
Ji In Lee <loveted0225@naver.com>
2022-02-06 17:14:59 +0900

    refactor: Car 메소드 변경 (findPositionWithMax -> isSamePosition)

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,42 +2,28 @@ package racingcar.model;
 
 public class Car {
 
-    private static final String CAR_NAME_LONGER_THAN_FIVE = "[ERROR] 자동차 이름은 5글자를 초과할 수 없습니다.";
-    private static final String CAR_NAME_LESS_THAN_ONE = "[ERROR] 자동차 이름은 최소 1글자 이상이어야 합니다. ";
-    private static final int MINIMUM_CAR_NAME_LENGTH = 1;
-    private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
     private static final int MOVE_FORWARD_CONDITION = 4;
     private static final int DEFAULT_POSITION = 0;
 
-    private final String carName;
     private int position;
+    private final Name name;
 
-    public Car(String carName) {
-        this(carName, DEFAULT_POSITION);
+    public Car(String name) {
+        this(name, DEFAULT_POSITION);
     }
 
-    public Car(String carName, int position) {
-        validateCarNameLength(carName);
-        this.carName = carName;
+    public Car(String name, int position) {
+        this.name =new Name(name);
         this.position = position;
     }
 
-    private void validateCarNameLength(String carName) {
-        if (carName.length() < MINIMUM_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException(CAR_NAME_LESS_THAN_ONE);
-        }
-        if (carName.length() > MAXIMUM_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException(CAR_NAME_LONGER_THAN_FIVE);
-        }
-    }
-
-    public void moveForward(int moveOrNot) {
-        if (moveOrNot >= MOVE_FORWARD_CONDITION) {
+    public void moveForward(int moveCondition) {
+        if (moveCondition >= MOVE_FORWARD_CONDITION) {
             this.position++;
         }
     }
 
-    public boolean findPositionWithMax(int maxPosition) {
+    public boolean isSamePosition(int maxPosition) {
         return position == maxPosition;
     }
 
@@ -46,7 +32,7 @@ public class Car {
     }
 
     public String getCarName() {
-        return carName;
+        return name.getCarName();
     }
 
     public int getPosition() {
