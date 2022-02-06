a2a20d47691fefbbda6aad644aa051b7fa43ccfe
kim-svadoz <dhkdghehfdl@gmail.com>
2022-02-06 13:45:06 +0900

    test: Car#go 메서드 테스트 로직 변경

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -7,7 +7,6 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.domain.random.MovementStrategy;
 
 class CarTest {
     @DisplayName("차량 인스턴스 생성 후 이름은 정확하게 저장된다")
@@ -22,24 +21,34 @@ class CarTest {
         assertThat(car.getName()).isEqualTo("jason");
     }
 
-    @DisplayName("go() 메서드 호출 후 차량의 위치는 그 횟수만큼 증가된다.")
     @Test
-    public void go_메서드_테스트() {
+    public void go_메서드의_인자가_5이상이면_차량의_위치는_그_횟수만큼_증가한다() {
         //given
-        Car car = Car.from("jason");
-        MovementStrategy movementGoStrategy = new MovementStrategy() {
-            @Override
-            public int generate() {
-                return 5;
-            }
-        };
+        Car prevCar = Car.from("jason");
 
         //when
-        Car car1 = car.go(movementGoStrategy.generate());
-        Car car2 = car1.go(movementGoStrategy.generate());
+        Car nextCar1 = prevCar.go(5);
+        Car nextCar2 = nextCar1.go(5);
 
         //then
-        assertThat(car2.getPosition()).isEqualTo(2);
+        assertThat(prevCar.getPosition()).isEqualTo(0);
+        assertThat(nextCar1.getPosition()).isEqualTo(1);
+        assertThat(nextCar2.getPosition()).isEqualTo(2);
+    }
+
+    @Test
+    public void go_메서드의_인자가_5미만이면_차량의_위치는_증가하지_않는다() {
+        //given
+        Car prevCar = Car.from("jason");
+
+        //when
+        Car nextCar1 = prevCar.go(4);
+        Car nextCar2 = nextCar1.go(4);
+
+        //then
+        assertThat(prevCar.getPosition()).isEqualTo(0);
+        assertThat(nextCar1.getPosition()).isEqualTo(0);
+        assertThat(nextCar2.getPosition()).isEqualTo(0);
     }
 
     @DisplayName("List<Car>를 정렬하면 위치의 내림차순으로 정렬된다.")
