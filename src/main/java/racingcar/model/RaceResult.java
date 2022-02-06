fe28d81d7d64db5d07b19e3c79ae4d463db74b4b
Ji In Lee <loveted0225@naver.com>
2022-02-06 17:16:25 +0900

    feat(RaceResult): 전진한 차들의 결과를 저장하기 위한 RaceResult 객체 생성

---
+++ src/main/java/racingcar/model/RaceResult.java
@@ -0,0 +1,16 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceResult {
+    private final List<Car> raceResultByRound;
+
+    public RaceResult(List<Car> raceResult ){
+        this.raceResultByRound=new ArrayList<>(raceResult);
+    }
+
+    public List<Car> getRaceResultByRound() {
+        return raceResultByRound;
+    }
+}
