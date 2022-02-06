f230029affa40deaf6f25ae14d5e37e9a47b0ce8
Ji In Lee <loveted0225@naver.com>
2022-02-06 17:18:33 +0900

    refactpr(OutputView): RaceResult 객체 추가에 따라 결과 출력 기능 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,13 +1,23 @@
 package racingcar.view;
 
 import java.util.List;
+import racingcar.controller.RacingGameController;
 import racingcar.model.Car;
+import racingcar.model.RaceResult;
 
 public class OutputView {
 
     private static final String WINNER_MESSAGE = "최종 우승자: ";
+    private static final String GAME_RESULT_MESSAGE = "출력결과";
 
-    public void printResult(List<Car> cars) {
+    public void printResult(List<RaceResult> totalResult) {
+        System.out.println(GAME_RESULT_MESSAGE);
+        for (RaceResult raceResult: totalResult){
+            printEachRoundResult(raceResult.getRaceResultByRound());
+        }
+    }
+
+    private void printEachRoundResult(List<Car> cars) {
         for (Car car : cars) {
             System.out.print(car.getCarName() + ": ");
             printPosition(car.getPosition());
@@ -26,4 +36,6 @@ public class OutputView {
         System.out.print(WINNER_MESSAGE);
         System.out.print(String.join(",", winners));
     }
+
+
 }
