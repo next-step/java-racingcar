00caac8cbc3209e5e00e5376aa46524c270269f6
kimtaejun97 <kim_taejun@naver.com>
2022-02-07 00:01:51 +0900

    test: 우승자가 두대 이상일 때 테스트 추가.

--- src/test/java/racinggame/domain/JudgeTest.java
+++ src/test/java/racinggame/domain/JudgeTest.java
@@ -25,4 +25,22 @@ class JudgeTest {
         assertThat(winners.size()).isEqualTo(1);
         assertThat(winners.contains("car1")).isTrue();
     }
+
+    @Test
+    void 우승자_판단_우승이_두대_이상() {
+        // given
+        final RacingCar racingCar1 = new RacingCar("car1", 2);
+        final RacingCar racingCar2 = new RacingCar("car2", 2);
+        final RacingCar racingCar3 = new RacingCar("car3", 1);
+
+        final RacingCars racingCars = new RacingCars(
+                Arrays.asList(racingCar1, racingCar2, racingCar3));
+
+        // when
+        final List<String> winners = Judge.getWinners(racingCars);
+
+        // then
+        assertThat(winners.size()).isEqualTo(2);
+        assertThat(winners.containsAll(Arrays.asList("car1", "car2"))).isTrue();
+    }
 }
