3315638b18fd7b4c122ed85e568ad43952d0eabb
kimtaejun97 <kim_taejun@naver.com>
2022-02-07 00:00:41 +0900

    test: forward 메서드에 의존하지 않도록 테스트 변경.

--- src/test/java/racinggame/domain/JudgeTest.java
+++ src/test/java/racinggame/domain/JudgeTest.java
@@ -2,6 +2,7 @@ package racinggame.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +11,12 @@ class JudgeTest {
     @Test
     void 우승자_판단() {
         // given
-        final RacingCars racingCars = new RacingCars("car1,car2,car2");
-        racingCars.get().get(0).forward();
+        final RacingCar racingCar1 = new RacingCar("car1", 2);
+        final RacingCar racingCar2 = new RacingCar("car2", 1);
+        final RacingCar racingCar3 = new RacingCar("car3", 0);
+
+        final RacingCars racingCars = new RacingCars(
+                Arrays.asList(racingCar1, racingCar2, racingCar3));
 
         // when
         final List<String> winners = Judge.getWinners(racingCars);
