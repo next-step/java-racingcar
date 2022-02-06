e979a73312187a99fccad2dee509e425529b153f
Ji In Lee <loveted0225@naver.com>
2022-02-06 17:08:48 +0900

    refactor: 기존 컨트롤러에 남아있는 비즈니스 로직 RacingGame으로 위임 후 RacingController로 이름 변경

---
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -0,0 +1,18 @@
+package racingcar.controller;
+
+import racingcar.model.RacingGame;
+import racingcar.model.Winner;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingGameController {
+
+    public void play() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RacingGame racingGame = new RacingGame(inputView.inputCarNames(), inputView.inputTryCount());
+        outputView.printResult(racingGame.startRace());
+        Winner winner = new Winner(racingGame.getFinalRoundResult());
+        outputView.printWinner(winner.findWinner());
+    }
+}
