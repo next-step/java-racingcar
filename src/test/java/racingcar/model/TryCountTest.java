03ec77295a5bcf0066cea55d1c3b2b9e0675bd52
Ji In Lee <loveted0225@naver.com>
2022-02-06 17:19:58 +0900

    test(TryCountTest): TryCount에 대한 테스트코드 추가

---
+++ src/test/java/racingcar/model/TryCountTest.java
@@ -0,0 +1,23 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class TryCountTest {
+    @Test
+    void 시도횟수는_1회_이상이어야_한다(){
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> new TryCount(0));
+    }
+
+    @Test
+    void 차를_한번씩_움직이고_시도횟수는_1씩_감소한다(){
+        TryCount tryCount= new TryCount(3);
+        for (int i=0; i<3;i++){
+            tryCount.decreaseTryCount();
+        }
+        assertThat(tryCount.isPlaying()).isEqualTo(false);
+    }
+}
