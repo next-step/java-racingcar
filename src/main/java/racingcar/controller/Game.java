a676d4ec0228e611811f0f2e4e7da8f730110604
fpal95 <snc7303@gmail.com>
2022-02-07 12:20:49 +0900

    Refactor(Game): 우승자를 판단하는 부분을 객체로 생성해 분리

--- src/main/java/racingcar/controller/Game.java
+++ src/main/java/racingcar/controller/Game.java
@@ -1,8 +1,11 @@
 package racingcar.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.model.Car;
+import racingcar.model.Winner;
 import racingcar.view.InputView;
 import racingcar.util.Message;
 import racingcar.util.RandomGenerator;
@@ -13,14 +16,12 @@ public class Game {
 
     public void play() {
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
 
         System.out.println(Message.INPUT_GUIDE_MESSAGE);
         String[] carNames = inputView.getCarName();
 
-        for (String carName:carNames) {
-            Car carInformation = new Car(carName);
-            carList.add(carInformation);
-        }
+        carList = carInformation(carNames);
 
         System.out.println(Message.ASK_TRY_COUNT);
         int tryCount = inputView.getTryCount();
@@ -29,7 +30,17 @@ public class Game {
         for (int i = 0; i < tryCount; i++) {
             moveForwardByCount();
         }
-        checkWinner();
+
+        Winner winner = new Winner(carList);
+        outputView.printWinner(winner.winnerList());
+    }
+
+    private List<Car> carInformation(String[] carNames) {
+        for (String carName: carNames){
+            Car carInformation = new Car(carName);
+            carList.add(carInformation);
+        }
+        return Arrays.stream(carNames).map(Car::new).collect(Collectors.toList());
     }
 
     public void moveForwardByCount() {
@@ -41,33 +52,5 @@ public class Game {
             outputView.printResult(car.getCarName(), car.getPosition());
         }
         System.out.println();
-    }
-
-    public void checkWinner() {
-        OutputView outputView = new OutputView();
-
-        int maxPosition = maximumDistance();
-        ArrayList<String> winners = winnerList(maxPosition);
-
-        outputView.printWinner(winners);
-    }
-
-    public ArrayList<String> winnerList(int maxPosition) {
-        ArrayList<String> winners = new ArrayList<>();
-
-        for (Car car : carList) {
-            if (car.isMaxPosition(maxPosition)) {
-                winners.add(car.getCarName());
-            }
-        }
-        return winners;
-    }
-
-    private int maximumDistance() {
-        int maxPosition = 0;
-        for (Car car : carList) {
-            maxPosition = car.checkMaxPosition(maxPosition);
-        }
-        return maxPosition;
     }
 }
