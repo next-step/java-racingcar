60047b08f5f681a95e7c35142c4e0de4977fe4f3
SeungYeonJeong <crowsy@naver.com>
2022-02-07 16:23:25 +0900

    refactor: Remove static method

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public static List<String> getName() {
+    public List<String> getName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         return Arrays.asList(str.split(","));
     }
 
-    public static int getTimes() {
+    public int getTimes() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         Scanner sc = new Scanner(System.in);
         return sc.nextInt();
