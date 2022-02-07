3a7f17d75823beb693107feb1c449a8531ec68dc
0JUUU <clleo97@naver.com>
2022-02-08 02:21:44 +0900

    refactor: 불필요한 변수 선언 제거

--- src/main/java/racing/controller/RacingController.java
+++ src/main/java/racing/controller/RacingController.java
@@ -3,7 +3,6 @@ package racing.controller;
 import java.util.List;
 import racing.domain.RacingGame;
 import racing.domain.TryNumber;
-import racing.domain.startegy.MovingStrategy;
 import racing.domain.startegy.RandomMovingStrategy;
 import racing.view.InputView;
 import racing.view.OutputView;
@@ -13,10 +12,9 @@ public class RacingController {
     public void start() {
         try {
             final List<String> names = InputView.getCarNames();
-            final MovingStrategy movingStrategy = new RandomMovingStrategy();
-            final RacingGame racingGame = RacingGame.registerCars(movingStrategy, names);
-
+            final RacingGame racingGame = RacingGame.registerCars(new RandomMovingStrategy(), names);
             final TryNumber tryNumber = InputView.getTryNumber();
+
             OutputView.printGameResult(racingGame.race(tryNumber));
             OutputView.printWinners(racingGame.decideWinners());
         } catch (IllegalArgumentException e) {
