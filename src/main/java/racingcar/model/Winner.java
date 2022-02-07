74db059e6e09fdb11dbb66bb7b81fc5668cfcb56
fpal95 <snc7303@gmail.com>
2022-02-07 12:21:51 +0900

    refactor(Winner): 우승자를 판단하는 class생성

---
+++ src/main/java/racingcar/model/Winner.java
@@ -0,0 +1,34 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winner {
+
+    private List<Car> carList;
+
+    private Winner(List<Car> car) {
+        this.carList = new ArrayList<>(car);
+    }
+
+    public ArrayList<String> winnerList() {
+        int maxPosition = maximumDistance();
+        ArrayList<String> winners = new ArrayList<>();
+
+        for (Car car : carList) {
+            if (car.isMaxPosition((maxPosition))) {
+                winners.add(car.getCarName());
+            }
+        }
+        return winners;
+    }
+
+    private int maximumDistance() {
+        int maxPosition = 0;
+        for (Car car : carList) {
+            maxPosition = car.checkMaxPosition(maxPosition);
+        }
+        return maxPosition;
+    }
+
+}
