907383f8acbc6da05566118c26559a49ed9c4b83
kimtaejun97 <kim_taejun@naver.com>
2022-02-07 18:54:11 +0900

    test: 검증 로직이 InputView로 이동.

--- src/test/java/racinggame/domain/TrialTest.java
+++ src/test/java/racinggame/domain/TrialTest.java
@@ -1,14 +1,9 @@
 package racinggame.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import racinggame.exception.InputBlankException;
 
 class TrialTest {
 
@@ -21,41 +16,5 @@ class TrialTest {
 
         // then
         assertThat(trial.getValue()).isEqualTo(5);
-    }
-
-    @ValueSource(strings = {"1 0", " 10", " 1 0 ", "10 ", "1   0  "})
-    @ParameterizedTest
-    void getTrial_불필요한_공백이_들어왔을_때_제거(final String inputTrial) {
-        // when
-        final int trial = new Trial(inputTrial).getValue();
-
-        // then
-        assertThat(trial).isEqualTo(10);
-    }
-
-    @Test
-    void 시도횟수는_숫자인지_검증하는_로직_실패() {
-        // given
-        final String input = "a";
-
-        // then
-        assertThrows(NumberFormatException.class, () -> new Trial(input));
-    }
-
-    @Test
-    void 시도횟수는_숫자인지_검증하는_로직_성공() {
-        // given
-        final String input = "5";
-
-        // then
-        assertDoesNotThrow(() -> new Trial(input));
-    }
-
-    @ValueSource(strings = {" ", "", "\n", "\t"})
-    @ParameterizedTest
-    void 시도횟수에_공백이_들어오면_예외발생(String input) {
-
-        // then
-        assertThrows(InputBlankException.class, () -> new Trial(input));
     }
 }
--- src/test/java/racinggame/view/InputViewTest.java
+++ src/test/java/racinggame/view/InputViewTest.java
@@ -1,10 +1,15 @@
 package racinggame.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewTest {
 
@@ -23,7 +28,7 @@ class InputViewTest {
     }
 
     @Test
-    void 시도횟수_정상_입력() {
+    void 시도횟수_정상_입력() throws IllegalAccessException {
         // given
         final String inputTrial = "5";
         final InputStream inputStream = new ByteArrayInputStream(inputTrial.getBytes());
@@ -34,5 +39,48 @@ class InputViewTest {
 
         // then
         assertThat(trial).isEqualTo(inputTrial);
+    }
+
+    @ValueSource(strings = {"1 0", " 10", " 1 0 ", "10 ", "1   0  "})
+    @ParameterizedTest
+    void getTrial_불필요한_공백이_들어왔을_때_제거(final String inputTrial) throws IllegalAccessException {
+        InputStream inputStream = new ByteArrayInputStream(inputTrial.getBytes());
+        System.setIn(inputStream);
+
+        // when
+        final String trial = InputView.inputTrial();
+
+        // then
+        assertThat(trial).isEqualTo("10");
+    }
+
+    @Test
+    void 시도횟수는_숫자인지_검증하는_로직_성공() {
+        // given
+        InputStream inputStream = new ByteArrayInputStream("5".getBytes());
+        System.setIn(inputStream);
+
+        // then
+        assertDoesNotThrow(() -> InputView.inputTrial());
+    }
+
+    @Test
+    void 시도횟수가_숫자가_아니면_재입력() {
+        // given
+        InputStream inputStream = new ByteArrayInputStream("a".getBytes());
+        System.setIn(inputStream);
+
+        // then
+        assertThrows(NoSuchElementException.class, () -> InputView.inputTrial());
+    }
+
+    @ValueSource(strings = {" ", "", "\n", "\t"})
+    @ParameterizedTest
+    void 시도횟수에_공백이_들어오면_재입력(String input) {
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        // then
+        assertThrows(NoSuchElementException.class, () -> InputView.inputTrial());
     }
 }
