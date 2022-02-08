2ee0ab8f66d9807906976ed39147f559f0757927
Dami Kim <dami.k.dev@gmail.com>
2022-02-08 18:28:07 +0900

    style: 불필요한 주석 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,37 +7,6 @@ import java.util.Scanner;
 public class InputView {
 
     private final static int MAX_NAME_LENGTH = 5;
-//    private static int tryCount;
-//    private static List<String> carNames;
-
-//    public InputView() {
-//
-//    }
-
-//    public static void initCarNamesAndTryCount() {
-//        setRacingCarNames();
-//        setTryCount();
-//    }
-
-//    private static void setRacingCarNames() {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-//
-//        carNames = Arrays.asList(scanner.nextLine().split(","));
-//
-//        for (String carName : carNames) {
-//            validateCarNameLength(carName);
-//        }
-//    }
-//
-//    private static void setTryCount() {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("시도할 횟수는 몇 회인가요?");
-//
-//        tryCount = Integer.parseInt(scanner.nextLine());
-//    }
 
     private static void validateCarNameLength(String carName) {
         if (carName.length() > MAX_NAME_LENGTH) {
