22cc3c2ca8be917f75989f3a7692ef9f0d26a738
xxell-8 <cell8pepper@gmail.com>
2022-02-08 16:55:45 +0900

    refactor: Change name to getCarNames

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -7,7 +7,7 @@ import racinggame.view.ResultView;
 
 public class Application {
     public static void main(String[] args) {
-        List<String> carNames = InputView.getCarName();
+        List<String> carNames = InputView.getCarNames();
         int tryNumber = InputView.getTryNumber();
 
         Racing game = new Racing();
--- src/main/java/racinggame/view/InputView.java
+++ src/main/java/racinggame/view/InputView.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String SEPARATOR = ",";
 
-    public static List<String> getCarName() {
+    public static List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         while (true) {
             Scanner scanner = new Scanner(System.in);
