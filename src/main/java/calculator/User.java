5c873d95c22f4c936fca515e393a8975fdf987d4
Jiin Lee <loveted0225@naver.com>
2022-02-09 22:21:41 +0900

    refactor(User): 매직넘버 상수 처리

--- src/main/java/calculator/User.java
+++ src/main/java/calculator/User.java
@@ -6,21 +6,23 @@ import java.util.Scanner;
 
 public class User {
 
+    private static final int MINIMUM_TOKEN_NUMBER = 3;
+    private static final String NOT_NULL_ALLOWED = "공백은 허용되지 않습니다";
+
     public List<String> getUserInput() {
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
 
         String[] userInputTokens = userInput.split(" ");
 
-        if (userInputTokens.length < 3) {
-            throw new IllegalArgumentException("공백은 허용되지 않습니다");
+        if (userInputTokens.length < MINIMUM_TOKEN_NUMBER) {
+            throw new IllegalArgumentException(NOT_NULL_ALLOWED);
         }
-        for (int i = 0; i < userInputTokens.length; i++) {
-            if (userInputTokens[i].equals("")) {
-                throw new IllegalArgumentException("공백이 존재합니다");
+        for (String token: userInputTokens) {
+            if (token.equals("")) {
+                throw new IllegalArgumentException(NOT_NULL_ALLOWED);
             }
         }
         return Arrays.asList(userInputTokens);
     }
-
 }
