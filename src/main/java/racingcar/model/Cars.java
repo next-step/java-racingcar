970e7a410a866014dcc8a4be8fb0c35189528317
Jiin Lee <loveted0225@naver.com>
2022-02-09 21:42:26 +0900

    refactor(Cars): MoveStrategy 구현체 주입

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -2,25 +2,29 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Cars {
 
-    private static final int RANDOM_BOUND_VALUE = 9;
-
-    private List<Car> cars = new ArrayList<>();
+    private final List<Car> cars;
+    private MoveStrategy moveStrategy;
 
     public Cars(List<String> parsedInput) {
+        cars = new ArrayList<>();
         for (String carName : parsedInput) {
             cars.add(new Car(carName));
         }
     }
 
+    public Cars(List<String> parsedInput, MoveStrategy moveStrategy) {
+        this(parsedInput);
+        this.moveStrategy = moveStrategy;
+    }
+
     public List<Car> moveCars() {
-        Random random = new Random();
         for (Car car : cars) {
-            int moveCondition = random.nextInt(RANDOM_BOUND_VALUE);
-            car.moveForward(moveCondition);
+            if (moveStrategy.canMove()) {
+                car.moveForward();
+            }
         }
         return cars;
     }
