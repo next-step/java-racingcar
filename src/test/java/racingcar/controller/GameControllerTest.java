e75168762615eaeaad0a65047d93598444b7dbcb
hochan222 <hochan049@gmail.com>
2022-02-09 17:57:58 +0900

    test: GameControllerTest의 메소트명 고민해보기 및 테스트 케이스 추가

--- src/test/java/racingcar/controller/GameControllerTest.java
+++ src/test/java/racingcar/controller/GameControllerTest.java
@@ -1,19 +1,51 @@
 package racingcar.controller;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import racingcar.domain.movable.RandomMovableStrategy;
 
 public class GameControllerTest {
 
-    @DisplayName("GameController에 ('a,b,c,d', 6)을 인자로 넣었을때, startGame 메서드가 정상 작동한다.")
-    @Test
-    void playSuccess() {
+    static Stream<Arguments> generateValidGameControllerParameter() {
+        return Stream.of(
+            Arguments.of("a, b, c, d", 1),
+            Arguments.of("a, b, c, d, e", 10),
+            Arguments.of("a, b, c, d, e", 20)
+        );
+    }
+
+    static Stream<Arguments> generateInvalidTurnGameControllerParameter() {
+        return Stream.of(
+            Arguments.of("a, b, c", 0),
+            Arguments.of("a, b, c, d", 0),
+            Arguments.of("a, b, c, d, e", 0)
+        );
+    }
+
+    @DisplayName("GameController에 유효한 인자로 넣었을때, startGame 메서드가 정상 작동한다.")
+    @ParameterizedTest
+    @MethodSource("generateValidGameControllerParameter")
+    void GivenValidGameControllerParameter_WhenStartGame_ThenNotThrowException(String carNames,
+        int turn) {
         assertDoesNotThrow(
-            () -> GameController.of("a,b,c,d", 6)
+            () -> GameController.of(carNames, turn)
                 .startGame(RandomMovableStrategy.getInstance())
         );
+    }
+
+    @DisplayName(value = "0으로 Price 객체를 생성할때, IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @MethodSource("generateInvalidTurnGameControllerParameter")
+    void GivenTurnZero_WhenStartGame_ThenIllegalArgumentException(String carNames, int turn) {
+        assertThatThrownBy(
+            () -> GameController.of(carNames, turn)
+                .startGame(RandomMovableStrategy.getInstance())
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
