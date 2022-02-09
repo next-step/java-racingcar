b0811baaed75494808d9d0c4eb015f6592d80e12
YangWoo Yu <yuyw0712@naver.com>
2022-02-09 17:53:04 +0900

    refactor: RacingGame.java 자동차 게임 모든 차 전진하는 도메인 기능 추가

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -3,22 +3,44 @@ package racingcar.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import racingcar.util.Movable;
+import racingcar.util.RandomUtil;
 
-public class RacingGame {
+public class RacingGame implements Movable {
 
+    private static final int RANDOM_START = 0;
+    private static final int RANDOM_END = 9;
+    private static final String CONTOUR = "----------------\n";
 
     private final List<RacingCar> racingCarLists;
+    private final int raceCount;
 
-    public RacingGame(List<String> nameList) {
+    public RacingGame(List<String> nameList, int raceCount) {
         racingCarLists = new ArrayList<>();
+        this.raceCount = raceCount;
+
         for (String name : nameList) {
             racingCarLists.add(new RacingCar(name));
         }
     }
 
+    public void runRace() {
+        for(int i = 0; i < raceCount; i++) {
+            moveForwardAll();
+        }
+    }
+
+    @Override
+    public void moveForwardAll() {
+        for (RacingCar racingCar : racingCarLists) {
+            racingCar.moveForward(RandomUtil.pickNumberInRange(RANDOM_START, RANDOM_END));
+            String currentStatus = racingCar.getName() + ":" + racingCar.getPosition() + "\n";
+            GameStatus.setStatus(currentStatus);
+        }
+        GameStatus.setStatus(CONTOUR);
+    }
+
     public List<RacingCar> getRacingCarLists() {
         return Collections.unmodifiableList(racingCarLists);
     }
-
-
 }
