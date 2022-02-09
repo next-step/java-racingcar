0d18a97c525d74b75d7fc316503032e99164e5ae
Jiin Lee <loveted0225@naver.com>
2022-02-09 21:53:31 +0900

    refactor(RaceResults): 레이스 결과를 갖고 우승자를 찾는 부분을 위임하기 위한 객체 추가

---
+++ src/main/java/racingcar/model/RaceResults.java
@@ -0,0 +1,46 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RaceResults {
+    private final List<RaceResult> raceResults;
+    private final RaceResult lastRaceResult;
+    private final List<String> winners;
+    private int maxPosition;
+
+    public RaceResults(List<RaceResult> raceResults){
+        this.raceResults=raceResults;
+        this.lastRaceResult=raceResults.get(raceResults.size()-1);
+        this.winners=new ArrayList<>();
+        this.maxPosition=0;
+    }
+
+    public List<String> findWinners(){
+        List<Car> carsAfterLastRace = lastRaceResult.getRaceResultByRound();
+        findMaxPosition(carsAfterLastRace);
+        findCarsWithMaxPosition(carsAfterLastRace);
+        return winners;
+    }
+
+    public void findMaxPosition(List<Car> carsAfterRace) {
+        for (Car car : carsAfterRace) {
+            maxPosition = car.compareMaxPosition(maxPosition);
+        }
+    }
+
+    private void findCarsWithMaxPosition(List<Car> carsAfterRace) {
+        for (Car car : carsAfterRace) {
+           findCarWithSamePosition(car);
+        }
+    }
+    private void findCarWithSamePosition(Car car){
+        if(car.isSamePosition(maxPosition)){
+            winners.add(car.getCarName());
+        }
+    }
+
+    public List<RaceResult> getRaceResults() {
+        return raceResults;
+    }
+}
