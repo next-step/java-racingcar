5ad0a7e6e8de941e2cf630d545cc97b0a130682f
Jiin Lee <loveted0225@naver.com>
2022-02-09 21:58:35 +0900

    test(RacingGameTest): RacingGame 테스트코드 작성

---
+++ src/test/java/racingcar/model/RacingGameTest.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class RacingGameTest {
+
+    @Test
+    void 시도횟수만큼_레이스를한다() {
+        RacingGame racingGame = new RacingGame(new Cars(Arrays.asList("pobi", "woni", "jun")), 3);
+        List<RaceResult> totalResult = racingGame.startRace();
+        assertThat(totalResult.size()).isEqualTo(3);
+    }
+}
