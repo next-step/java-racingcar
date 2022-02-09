62ee47bacb830511e147845e8090e451704281d4
Jiin Lee <loveted0225@naver.com>
2022-02-09 22:18:11 +0900

    refactor(Calculator): 나누기 함수 안으로 유효성 검증 로직 이동

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Calculator {
 
-    private static final String ZERO = "0";
+    private static final int ZERO = 0;
     private static final String PLUS = "+";
     private static final String MINUS = "-";
     private static final String MULTIPLY = "*";
@@ -30,19 +30,12 @@ public class Calculator {
                 continue;
             }
             if (DIVIDE.equals(userInput.get(i))) {
-                checkDivideByZero(userInput.get(i + 1));
                 result = divide(result, operand);
                 continue;
             }
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         return result;
-    }
-
-    private void checkDivideByZero(String operand) {
-        if (ZERO.equals(operand)) {
-            throw new IllegalArgumentException(CANNOT_DIVIDE_BY_ZERO);
-        }
     }
 
     private int sum(int a, int b) {
@@ -58,6 +51,9 @@ public class Calculator {
     }
 
     private int divide(int a, int b) {
+        if (b==ZERO) {
+            throw new IllegalArgumentException(CANNOT_DIVIDE_BY_ZERO);
+        }
         return a / b;
     }
 }
