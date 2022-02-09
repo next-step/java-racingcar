3f332f8a261c1d6c5e104c15fe73283751cd37e4
Jiin Lee <loveted0225@naver.com>
2022-02-09 21:44:55 +0900

    test(Car): moveForward에 대한 테스트코드 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,35 +1,16 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
-    @ValueSource(ints = {0, 1, 2, 3})
-    @ParameterizedTest
-    void 전진_불가(final int condition) {
-        Car car = new Car("leah");
-        car.moveForward(condition);
-        assertThat(car.getPosition()).isEqualTo(0);
-    }
-
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    @ParameterizedTest
-    void 전진_가능(final int condition) {
-        Car car = new Car("leah");
-        car.moveForward(condition);
-        assertThat(car.getPosition()).isEqualTo(1);
-    }
-
-    @ValueSource(strings = {"LeahJiin", "Samuel", "JiinLee"})
-    @ParameterizedTest
-    void 자동차의_이름이_5글자_넘으면_예외_발생(final String name) {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> new Car(name));
+    @Test
+    void 자동차는_전진하면_위치가_증가한다() {
+        Car car = new Car("leah", 2);
+        car.moveForward();
+        assertThat(car.getPosition()).isEqualTo(3);
     }
 
     @Test
@@ -45,5 +26,4 @@ public class CarTest {
         int result = carLeah.compareMaxPosition(2);
         assertThat(result).isEqualTo(3);
     }
-
 }
