9c70a23f6cafcad3727aefb98159ebf8f037dabf
gal2o <hunmin8788@gmail.com>
2022-02-10 17:30:16 +0900

    refactor(RacingCar): RandomGenerateTest을 더 정확하게 테스트 하기 위한 @RepetedTest로 수정

--- src/test/java/racingcar/domain/RandomGenerateTest.java
+++ src/test/java/racingcar/domain/RandomGenerateTest.java
@@ -4,11 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class RandomGenerateTest {
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("랜덤한 수의 범위는 0 ~ 9까지이다.")
     void randomValue() {
         assertThat(RandomGenerate.makeRandom()).isBetween(0, 9);
