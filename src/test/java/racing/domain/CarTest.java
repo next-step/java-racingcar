828b97eba2632c16f875c30473f50a2b7aa3f4d3
eohaerim <ehl3288@naver.com>
2022-02-10 21:52:33 +0900

    test: MoveRuleTest, CarTest에 테스트용 MoveRule 적용

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -8,25 +8,38 @@ import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
-    private MoveRule moveRule;
+    private MoveRule testMoveRule;
     private Car car;
 
     @BeforeEach
     void setUp() {
-        moveRule = new MoveRule(4);
+        testMoveRule = new MoveRule() {
+            private static final int MOVE_CONDITION = 4;
+            private static final int MOVABLE_VALUE = 5;
+
+            @Override
+            public int makeValue() {
+                return MOVABLE_VALUE;
+            }
+
+            @Override
+            public boolean isMovable() {
+                return makeValue() >= MOVE_CONDITION;
+            }
+        };
     }
 
     @Test
     void 자동차_이름이_5글자_이상_에러() {
         assertThatIllegalArgumentException()
-            .isThrownBy(() -> new Car("포르쉐입니다", moveRule))
+            .isThrownBy(() -> new Car("포르쉐입니다", testMoveRule))
             .withMessage("[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.");
     }
 
     @Test
     void 이동전략에_따라_이동을_판단한다() {
-        car = new Car("포르쉐", 0 ,new MoveRule(4));
-        car.moveByRule(5);
+        car = new Car("포르쉐", 0, testMoveRule);
+        car.move();
         assertThat(car.getDistance()).isEqualTo(1);
     }
 }
--- src/test/java/racing/domain/MoveRuleTest.java
+++ src/test/java/racing/domain/MoveRuleTest.java
@@ -8,7 +8,20 @@ class MoveRuleTest {
 
     @Test
     void 주입된_이동전략에_따라_이동_가능성을_판단한다() {
-        MoveRule moveRule = new MoveRule(7);
-        assertThat(moveRule.isMovable(6)).isFalse();
+        MoveRule moveRule = new MoveRule() {
+            private static final int MOVE_CONDITION = 4;
+            private static final int NON_MOVABLE_VALUE = 1;
+
+            @Override
+            public int makeValue() {
+                return NON_MOVABLE_VALUE;
+            }
+
+            @Override
+            public boolean isMovable() {
+                return makeValue() >= MOVE_CONDITION;
+            }
+        };
+        assertThat(moveRule.isMovable()).isFalse();
     }
 }
