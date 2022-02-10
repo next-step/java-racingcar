8da0b6d0e7b1b234721d80de16d7988e1950dcda
Dami Kim <dami.k.dev@gmail.com>
2022-02-10 23:24:19 +0900

    refactor: Racing Car ResultView 메서드 static으로 리팩터링

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 public class ResultView {
 
-    public void printGameStatus(String name, int move) {
+    public static void printGameStatus(String name, int move) {
         String dashes = getDashByMove(move);
         System.out.println(name + ":" + dashes);
     }
 
-    public void printGameResult(List<String> winners) {
+    public static void printGameResult(List<String> winners) {
         System.out.println("최종 우승자: " + String.join(", ", winners));
     }
 
-    private String getDashByMove(int move) {
+    private static String getDashByMove(int move) {
         StringBuilder dash = new StringBuilder();
 
         for (int i = 0; i < move; i++) {
