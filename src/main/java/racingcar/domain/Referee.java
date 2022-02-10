c3cdeac375a083612e0aa8b4ac4ee65a80c1bbd4
Dami Kim <dami.k.dev@gmail.com>
2022-02-10 23:21:39 +0900

    refactor: Referee 메서드 static 으로 리팩터링

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class Referee {
 
-    public List<String> getWinners(List<Car> cars) {
+    public static List<String> getWinners(List<Car> cars) {
         int maxMovingDistance = getMaxMovingDistance(cars);
 
         return cars.stream()
@@ -13,7 +13,7 @@ public class Referee {
             .collect(Collectors.toList());
     }
 
-    private int getMaxMovingDistance(List<Car> cars) {
+    private static int getMaxMovingDistance(List<Car> cars) {
         int maxMovingDistance = 0;
 
         for (Car car : cars) {
