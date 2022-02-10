cb582a8e627a663f252e8f0d8a54a755f9384e0d
Dami Kim <dami.k.dev@gmail.com>
2022-02-10 17:55:31 +0900

    refactor: 상수에 static keyword 부착

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,10 +2,9 @@ package racingcar.domain;
 
 public class Car {
 
-    private final int MIN_DISTANCE = 4;
+    private static final int MIN_DISTANCE = 4;
     private final String name;
     private int position = 0;
-
 
     public Car(String name) {
         this.name = name;
