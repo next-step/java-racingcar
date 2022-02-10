43bcb4135bbb02874c011ee4930fd66db1cc4864
Jiin Lee <loveted0225@naver.com>
2022-02-11 02:59:56 +0900

    refactor(TryCount): 매직넘버 상수 선언

--- src/main/java/racingcar/model/TryCount.java
+++ src/main/java/racingcar/model/TryCount.java
@@ -4,12 +4,12 @@ public class TryCount {
 
     private static final String TRY_COUNT_LESS_THAN_ZERO = "[ERROR] 시도 횟수는 1회 이상이어야 합니다.";
     private static final int MINIMUM_TRY_COUNT = 1;
-
+    private static final int ZERO = 0;
     private int tryCount;
 
-    public TryCount(int tryCountInput) {
-        validateTryCount(tryCountInput);
-        this.tryCount = tryCountInput;
+    public TryCount(int tryCount) {
+        validateTryCount(tryCount);
+        this.tryCount = tryCount;
     }
 
     private void validateTryCount(int tryCountInput) {
@@ -19,7 +19,7 @@ public class TryCount {
     }
 
     public boolean isPlaying() {
-        return tryCount > 0;
+        return tryCount > ZERO;
     }
 
     public void decreaseTryCount() {
