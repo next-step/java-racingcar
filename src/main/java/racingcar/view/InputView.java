74ce8270870ec0218ee0c6a4754965f33834daf3
Dami Kim <dami.k.dev@gmail.com>
2022-02-10 22:58:40 +0900

    refactor: Scanner 객체가 한 번 생성될 수 있도록 리팩터링

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class InputView {
 
     private final static int MAX_NAME_LENGTH = 5;
+    private final static Scanner scanner = new Scanner(System.in);
 
     private static void validateCarNameLength(String carName) {
         if (carName.length() > MAX_NAME_LENGTH) {
@@ -15,16 +16,12 @@ public class InputView {
     }
 
     public static int getTryCount() {
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("시도할 횟수는 몇 회인가요?");
 
         return Integer.parseInt(scanner.nextLine());
     }
 
     public static List<String> getCarNames() {
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
         List<String> carNames = Arrays.asList(scanner.nextLine().split(","));
