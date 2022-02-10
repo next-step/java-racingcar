d54cfa95c42ec5e24b09b58d3547895b6309ba5a
Dami Kim <dami.k.dev@gmail.com>
2022-02-10 18:16:14 +0900

    style: CarTest test명 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -12,7 +12,7 @@ class CarTest {
     Car car = new Car("Dami");
 
     @Test
-    void 랜덤_값이_4이상일_경우_전진하는_테스트() {
+    void 랜덤_값이_4이상일_경우_전진한다() {
         final int DISTANCE = 5;
         final int RESULT = 5;
 
@@ -25,13 +25,13 @@ class CarTest {
 
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    void 랜덤_값이_4이상일_경우_전진하는_상태를_만드는_테스트(int distance) {
+    void 랜덤_값이_4이상일_경우_전진하는_상태를_만든다(int distance) {
         assertTrue(car.isMovable(distance));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
-    void 랜덤_값이_4미만일_경우_전진하지_않는_상태를_만드는_테스트(int distance) {
+    void 랜덤_값이_4미만일_경우_전진하지_않는_상태를_만든다(int distance) {
         assertFalse(car.isMovable(distance));
     }
 
