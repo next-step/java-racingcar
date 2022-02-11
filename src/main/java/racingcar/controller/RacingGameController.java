b6e628adf59b3422b88556bc206a5754f545b83a
Jiin Lee <loveted0225@naver.com>
2022-02-11 15:05:43 +0900

    refactor(RacingGameController): private 메소드 생성하여 Cars 파라미터로 전달

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,7 +1,9 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.model.Car;
 import racingcar.model.Cars;
-import racingcar.model.MoveStrategy;
 import racingcar.model.RaceResults;
 import racingcar.model.RacingGame;
 import racingcar.model.RandomStrategy;
@@ -13,10 +15,19 @@ public class RacingGameController {
     public void play() {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        Cars cars = new Cars(inputView.inputCarNames());
-        RacingGame racingGame = new RacingGame(cars, inputView.inputTryCount(),new RandomStrategy());
+        Cars cars = new Cars(createCars(inputView.inputCarNames()));
+        RacingGame racingGame = new RacingGame(cars, inputView.inputTryCount(),
+            new RandomStrategy());
         RaceResults raceResults = racingGame.startRace();
         outputView.printResult(raceResults);
         outputView.printWinner(raceResults.findWinners());
+    }
+
+    private static List<Car> createCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
     }
 }
