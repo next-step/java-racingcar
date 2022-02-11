c29a47fb12c5c872d886b4a513367253b5cde1d8
mingsang <minsang0850@naver.com>
2022-02-11 14:27:40 +0900

    refactor: rename parseOperation to calculateIndividual

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,43 +2,28 @@ package calculator;
 
 public class Calculator {
 
-    private static final String ERROR = "ERROR";
-
     public static void execute() {
         System.out.println("수식을 입력하시오: ");
         String expression = InputView.inputString();
         String[] expressions = InputView.parsing(expression);
         Calculator calculator = new Calculator();
-        calculator.calculate(expressions);
+        int result = calculator.calculate(expressions);
+        System.out.println(result);
     }
 
-    public static void calculate(String[] strings) {
-        final int STRING_ARRAY_LENGTH = (strings.length - 1) / 2;
-        for (int i = 0; i < STRING_ARRAY_LENGTH; i++) {
-            if (ERROR.equals(strings[0])) {
-                System.out.println("ERROR: 종료합니다");
-                return;
-            }
-            if (strings[i * 2 + 1] == null) {
-                System.out.println("정답: " + Integer.parseInt(strings[i * 2]));
-                return;    //정수로 바꿔서 리턴
-            }
-            parseOperation(strings, i * 2);
+    public static int calculate(String[] expression) {
+        final int expressionLength = expression.length;
+        for (int i = 0; i < expressionLength - 1; i += 2) {
+            int firstOperand = Integer.parseInt(expression[i]);
+            String operator = expression[i + 1];
+            int secondOperand = Integer.parseInt(expression[i + 2]);
+            int result = caculateIndividual(firstOperand, secondOperand, operator);
+            expression[i + 2] = Integer.toString(result);
         }
+        return Integer.parseInt(expression[expressionLength - 1]);
     }
 
-    public static void parseOperation(String[] strings, int start) {
-        int firstOperand, secondOperand;
-
-        if (ERROR.equals(strings[start]) || ERROR.equals(strings[start + 2])) {
-            strings[0] = ERROR;
-            return;
-        }
-
-        String operator = strings[start + 1];
-        firstOperand = Integer.parseInt(strings[start]);
-        secondOperand = Integer.parseInt(strings[start + 2]);
-
+    public static int caculateIndividual(int firstOperand, int secondOperand, String operator) {
         int result = 0;
         if (operator.equals("+")) {
             result = add(firstOperand, secondOperand);
@@ -48,16 +33,14 @@ public class Calculator {
             result = mul(firstOperand, secondOperand);
         } else if (operator.equals("/")) {
             if (secondOperand == 0) {
-                strings[0] = ERROR;
-                return;
+                throw new IllegalArgumentException("Error: can't divide zero");
             } else {
                 result = div(firstOperand, secondOperand);
             }
         } else {
             throw new IllegalArgumentException("Error: need right operator");
         }
-
-        strings[start + 2] = String.valueOf(result);
+        return result;
     }
 
     public static int add(int firstOperand, int secondOperand) {
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,13 +7,12 @@ import org.junit.jupiter.api.Test;
 class CalculatorTest {
 
     @Test
-    public void parseOperationTest() {
-        String[] strings1 = {"13", "+", "5"};
-        String[] strings2 = {"10", "-", "5"};
-        int start = 0;
-        Calculator.parseOperation(strings1, start);
-        assertThat(strings1[2]).isEqualTo("18");
-        Calculator.parseOperation(strings2, start);
-        assertThat(strings2[2]).isEqualTo("5");
+    public void calculateTest() {
+        String[] expression1 = {"13", "+", "5"};
+        String[] expression2 = {"10", "-", "5"};
+        Calculator.calculate(expression1);
+        assertThat(expression1[2]).isEqualTo("18");
+        Calculator.calculate(expression2);
+        assertThat(expression2[2]).isEqualTo("5");
     }
 }
