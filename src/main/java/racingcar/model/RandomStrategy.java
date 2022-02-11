3e1e3b1e5a12a48751b995ed5d2a399516be271d
Jiin Lee <loveted0225@naver.com>
2022-02-11 15:11:20 +0900

    refactor(RandomStrategy): Random은 RANDOM-> Random 으로 변경

--- src/main/java/racingcar/model/RandomStrategy.java
+++ src/main/java/racingcar/model/RandomStrategy.java
@@ -6,13 +6,13 @@ public class RandomStrategy implements MoveStrategy {
 
     private static final int RANDOM_BOUND_VALUE = 10;
     private static final int MOVE_FORWARD_CONDITION = 4;
-    private static final Random RANDOM = new Random();
+    private static final Random Random = new Random();
 
     public boolean moveOrNot() {
         return generateRandomNumber() > MOVE_FORWARD_CONDITION;
     }
 
     private int generateRandomNumber() {
-        return RANDOM.nextInt(RANDOM_BOUND_VALUE);
+        return Random.nextInt(RANDOM_BOUND_VALUE);
     }
 }
