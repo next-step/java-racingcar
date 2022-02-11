a54c574fc5b3728f4726d188db4d458db574fe64
Jiin Lee <loveted0225@naver.com>
2022-02-11 15:01:44 +0900

    refactor(CalculatorTest): int -> double 형변환

--- src/test/java/calculator/model/CalculatorTest.java
+++ src/test/java/calculator/model/CalculatorTest.java
@@ -11,28 +11,28 @@ public class CalculatorTest {
     @Test
     void 덧셈() {
         Calculator calculator = new Calculator();
-        int result = calculator.calculateByOperator(Arrays.asList("2", "+", "3"));
+        double result = calculator.calculateByOperator(Arrays.asList("2", "+", "3"));
         assertThat(result).isEqualTo(5);
     }
 
     @Test
     void 뺄셈() {
         Calculator calculator = new Calculator();
-        int result = calculator.calculateByOperator(Arrays.asList("3", "-", "2"));
+        double result = calculator.calculateByOperator(Arrays.asList("3", "-", "2"));
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     void 곱셉() {
         Calculator calculator = new Calculator();
-        int result = calculator.calculateByOperator(Arrays.asList("2", "*", "3"));
+        double result = calculator.calculateByOperator(Arrays.asList("2", "*", "3"));
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     void 나눗셈() {
         Calculator calculator = new Calculator();
-        int result = calculator.calculateByOperator(Arrays.asList("3", "/", "2"));
-        assertThat(result).isEqualTo(1);
+        double result = calculator.calculateByOperator(Arrays.asList("3", "/", "2"));
+        assertThat(result).isEqualTo(1.5);
     }
 }
