a45ea2899b781aefa6b898e75ca52d39a212aed3
Jiin Lee <loveted0225@naver.com>
2022-02-11 14:58:02 +0900

    refactor(Calculator): 연산자를 enum클래스로 만들어 리팩토링

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,56 +4,16 @@ import java.util.List;
 
 public class Calculator {
 
-    private static final int ZERO = 0;
-    private static final String PLUS = "+";
-    private static final String MINUS = "-";
-    private static final String MULTIPLY = "*";
-    private static final String DIVIDE = "/";
-    private static final String ERROR_MESSAGE = "사칙 연산이 아닙니다";
-    private static final String CANNOT_DIVIDE_BY_ZERO = "0으로 나뉠 수 없습니다";
-
-    public int calculateByOperator(List<String> userInput) {
-        int result = Integer.parseInt(userInput.get(0));
-        int userInputSize = userInput.size();
+    public double calculateByOperator(List<String> userString) {
+        double result = Double.parseDouble(userString.get(0));
+        int userInputSize = userString.size();
         for (int i = 1; i < userInputSize - 1; i += 2) {
-            int operand = Integer.parseInt(userInput.get(i + 1));
-            if (PLUS.equals(userInput.get(i))) {
-                result = sum(result, operand);
-                continue;
-            }
-            if (MINUS.equals(userInput.get(i))) {
-                result = subtract(result, operand);
-                continue;
-            }
-            if (MULTIPLY.equals(userInput.get(i))) {
-                result = multiply(result, operand);
-                continue;
-            }
-            if (DIVIDE.equals(userInput.get(i))) {
-                result = divide(result, operand);
-                continue;
-            }
-            throw new IllegalArgumentException(ERROR_MESSAGE);
+            String symbol = userString.get(i);
+            double operand = Double.parseDouble(userString.get(i + 1));
+            Operator operator = Operator.matchOperator(symbol);
+            result = operator.apply(result, operand);
         }
         return result;
     }
 
-    private int sum(int a, int b) {
-        return a + b;
-    }
-
-    private int subtract(int a, int b) {
-        return a - b;
-    }
-
-    private int multiply(int a, int b) {
-        return a * b;
-    }
-
-    private int divide(int a, int b) {
-        if (b == ZERO) {
-            throw new IllegalArgumentException(CANNOT_DIVIDE_BY_ZERO);
-        }
-        return a / b;
-    }
 }
