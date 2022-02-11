dc1135faed6fe1dd6d3c4ce5c1c650e668dac56c
Jiin Lee <loveted0225@naver.com>
2022-02-11 15:13:04 +0900

    test(CarsTest): moveStretegy 익명클래스로 변경

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -4,33 +4,48 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
 
     @Test
     void 입력받은_이름으로_Car객체_리스트를_만든다() {
-        MoveStrategy moveStrategy = new RandomStrategy();
-        Cars cars = new Cars(Arrays.asList("pobi", "woni", "jun"),moveStrategy);
-        List<Car> movedCars = cars.moveCars();
-        assertThat(movedCars.get(0).getCarName()).isEqualTo("pobi");
-        assertThat(movedCars.get(1).getCarName()).isEqualTo("woni");
-        assertThat(movedCars.get(2).getCarName()).isEqualTo("jun");
+        MoveStrategy moveStrategy = new MoveStrategy() {
+            @Override
+            public boolean moveOrNot() {
+                if (new Random().nextInt(10) > 4) {
+                    return true;
+                }
+                return false;
+            }
+
+        };
+        Cars cars = new Cars(Arrays.asList(
+            new Car("jiin"), new Car("leah"), new Car("sam")));
+        List<Car> movedCars = cars.moveCars(moveStrategy);
+        assertThat(movedCars.get(0).getCarName()).isEqualTo("jiin");
+        assertThat(movedCars.get(1).getCarName()).isEqualTo("leah");
+        assertThat(movedCars.get(2).getCarName()).isEqualTo("sam");
     }
 
     @Test
     void 생성된_Car들이_전진한다() {
         MoveStrategy moveStrategy = new RandomStrategy();
-        Cars cars = new Cars(Arrays.asList("pobi", "woni", "jun"),moveStrategy);
-        List<Car> movedCars = cars.moveCars();
+        Cars cars = new Cars(Arrays.asList(new Car("jiin"), new Car("leah"), new Car("sam")));
+        List<Car> movedCars = cars.moveCars(() -> true);
         assertThat(movedCars.get(0).getPosition()).isNotNull();
     }
 
     @Test
     void 전진_불가_테스트() {
-        MoveStrategy moveStrategy = new NeverMoveStrategy();
-        Cars cars = new Cars(Arrays.asList("pobi", "woni", "jun"), moveStrategy);
-        List<Car> movedCars = cars.moveCars();
+        Cars cars = new Cars(Arrays.asList(new Car("jiin"), new Car("leah"), new Car("sam")));
+        List<Car> movedCars = cars.moveCars(new MoveStrategy() {
+            @Override
+            public boolean moveOrNot() {
+                return false;
+            }
+        });
         assertThat(movedCars.get(0).getPosition()).isEqualTo(0);
         assertThat(movedCars.get(1).getPosition()).isEqualTo(0);
         assertThat(movedCars.get(2).getPosition()).isEqualTo(0);
@@ -38,9 +53,8 @@ public class CarsTest {
 
     @Test
     void 전진_가능_테스트() {
-        MoveStrategy moveStrategy = new AlwaysMoveStrategy();
-        Cars cars = new Cars(Arrays.asList("pobi", "woni", "jun"), moveStrategy);
-        List<Car> movedCars = cars.moveCars();
+        Cars cars = new Cars(Arrays.asList(new Car("jiin"), new Car("leah"), new Car("sam")));
+        List<Car> movedCars = cars.moveCars(() -> true);
         assertThat(movedCars.get(0).getPosition()).isEqualTo(1);
         assertThat(movedCars.get(1).getPosition()).isEqualTo(1);
         assertThat(movedCars.get(2).getPosition()).isEqualTo(1);
