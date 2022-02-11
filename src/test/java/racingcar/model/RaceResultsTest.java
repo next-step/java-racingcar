47d39d933bafc61e86dad2fd643ed015bae73fcf
Jiin Lee <loveted0225@naver.com>
2022-02-11 15:14:17 +0900

    refactor(RaceResultsTest): RaceResult 클래스 삭제에 따라 테스트 코드 변경

--- src/test/java/racingcar/model/RaceResultsTest.java
+++ src/test/java/racingcar/model/RaceResultsTest.java
@@ -10,33 +10,29 @@ class RaceResultsTest {
 
     @Test
     void 모든_레이스의_결과를_저장한다() {
-        RaceResult firstRaceResult = new RaceResult(
-            Arrays.asList(new Car("jiin", 0), new Car("leah", 1), new Car("sam", 1))
-        );
-        RaceResult secondRaceResult = new RaceResult(
-            Arrays.asList(new Car("jiin", 1), new Car("leah", 2), new Car("sam", 1))
-        );
-        RaceResult thirdRaceResult = new RaceResult(
-            Arrays.asList(new Car("jiin", 1), new Car("leah", 3), new Car("sam", 2))
-        );
+        List<Car> firstRaceResult =
+            Arrays.asList(new Car("jiin", 0), new Car("leah", 1), new Car("sam", 1));
+        List<Car> secondRaceResult =
+            Arrays.asList(new Car("jiin", 1), new Car("leah", 2), new Car("sam", 1));
+        List<Car> thirdRaceResult =
+            Arrays.asList(new Car("jiin", 1), new Car("leah", 3), new Car("sam", 2));
         RaceResults raceResults = new RaceResults(
             Arrays.asList(firstRaceResult, secondRaceResult, thirdRaceResult));
-        List<RaceResult> totalResults = raceResults.getRaceResults();
+        List<List<Car>> totalResults = raceResults.getRaceResults();
         assertThat(totalResults.size()).isEqualTo(3);
-        assertThat(totalResults.get(0).equals(firstRaceResult.getRaceResultByRound()));
+        assertThat(totalResults.get(0).get(0).getPosition()).isEqualTo(0);
+        assertThat(totalResults.get(1).get(0).getPosition()).isEqualTo(1);
+        assertThat(totalResults.get(2).get(0).getPosition()).isEqualTo(1);
     }
 
     @Test
     void 우승자를_찾는다() {
-        RaceResult firstRaceResult = new RaceResult(
-            Arrays.asList(new Car("jiin", 0), new Car("leah", 1), new Car("sam", 1))
-        );
-        RaceResult secondRaceResult = new RaceResult(
-            Arrays.asList(new Car("jiin", 1), new Car("leah", 2), new Car("sam", 1))
-        );
-        RaceResult thirdRaceResult = new RaceResult(
-            Arrays.asList(new Car("jiin", 1), new Car("leah", 3), new Car("sam", 2))
-        );
+        List<Car> firstRaceResult =
+            Arrays.asList(new Car("jiin", 0), new Car("leah", 1), new Car("sam", 1));
+        List<Car> secondRaceResult =
+            Arrays.asList(new Car("jiin", 1), new Car("leah", 2), new Car("sam", 1));
+        List<Car> thirdRaceResult =
+            Arrays.asList(new Car("jiin", 1), new Car("leah", 3), new Car("sam", 2));
         RaceResults raceResults = new RaceResults(
             Arrays.asList(firstRaceResult, secondRaceResult, thirdRaceResult));
         assertThat(raceResults.findWinners()).containsExactly("leah");
