e107d9e59b3b714ce4fbedbc5ccfcbed1dd499d5
Jiin Lee <loveted0225@naver.com>
2022-02-11 15:02:09 +0900

    refactor(Application): 컨트롤러 코드 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,10 +6,9 @@ public class Application {
 
     public static void main(String[] args) {
         User user = new User();
-        final List<String> userInput = user.getUserInput();
-        System.out.println(userInput);
+        final List<String> userInput = user.parseUserInput();
         Calculator calculator = new Calculator();
-        int result = calculator.calculateByOperator(userInput);
+        double result = calculator.calculateByOperator(userInput);
         System.out.println(result);
     }
 }
