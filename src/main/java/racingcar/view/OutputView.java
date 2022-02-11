aa24e9bd9848e785f114d77b5659d901c42d1ff4
Jiin Lee <loveted0225@naver.com>
2022-02-11 15:07:51 +0900

    refactor(OutputView): 출력 문구 변경 및 줄바꿈 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,9 +7,10 @@ import racingcar.model.RaceResults;
 public class OutputView {
 
     private static final String WINNER_MESSAGE = "최종 우승자: ";
-    private static final String GAME_RESULT_MESSAGE = "출력결과";
+    private static final String GAME_RESULT_MESSAGE = "실행결과";
 
     public void printResult(RaceResults raceResults) {
+        System.out.println();
         System.out.println(GAME_RESULT_MESSAGE);
         for (List<Car> raceResult : raceResults.getRaceResults()) {
             printEachRoundResult(raceResult);
