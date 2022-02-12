7eaf1d8a3ff39d0dae25385558bef1bfffabe0cf
eohaerim <ehl3288@naver.com>
2022-02-13 03:55:03 +0900

    refactor: InputView의 getRacingInfo() 적용, Racing에 전달

--- src/main/java/racing/RacingApplication.java
+++ src/main/java/racing/RacingApplication.java
@@ -1,28 +1,22 @@
 package racing;
 
-import racing.domain.MoveRule;
-import racing.domain.RandomMoveRule;
-import racing.domain.RoundResults;
-import racing.dto.RacingInfo;
 import racing.domain.Judgement;
 import racing.domain.Racing;
+import racing.domain.RoundResults;
+import racing.dto.RacingInfo;
 import racing.view.InputView;
 import racing.view.ResultView;
 
 public class RacingApplication {
 
     public static void main(String[] args) {
-        String carNames = InputView.getCars();
-        int roundNumber = InputView.getTotalRoundNumber();
-        MoveRule moveRule = new RandomMoveRule();
-        RacingInfo infoDto = RacingInfo.of(carNames, roundNumber, moveRule);
+        RacingInfo info = InputView.getRacingInfo();
 
-        Racing racing = new Racing(infoDto.getCars(), infoDto.getTotalRoundNumber());
+        Racing racing = new Racing(info);
         racing.race();
-
         RoundResults results = racing.getResults();
-        Judgement judgement = new Judgement(results.getLastRawResult());
 
+        Judgement judgement = new Judgement(results.getLastRawResult());
         ResultView.printResultAndWinners(results.getRawResults(), judgement.judgeWinner());
     }
 }
--- src/main/java/racing/domain/Racing.java
+++ src/main/java/racing/domain/Racing.java
@@ -1,16 +1,16 @@
 package racing.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import racing.dto.RacingInfo;
 
 public class Racing {
 
     private final List<Car> cars;
     private final Round round;
 
-    public Racing(List<Car> cars, int totalRoundNumber) {
-        this.round = new Round(totalRoundNumber);
-        this.cars = cars;
+    public Racing(RacingInfo info) {
+        this.round = new Round(info.getTotalRoundNumber());
+        this.cars = CarGenerator.createCars(info.getCarNames(), new RandomMoveRule());
     }
 
     public void race() {
