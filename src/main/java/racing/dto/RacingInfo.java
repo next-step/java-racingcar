0551c420c5f07ba7e419b780803b522c2a5c3ecf
eohaerim <ehl3288@naver.com>
2022-02-13 03:52:34 +0900

    refactor: racingInfo를 DTO역할로 수정

--- src/main/java/racing/dto/RacingInfo.java
+++ src/main/java/racing/dto/RacingInfo.java
@@ -1,30 +1,21 @@
 package racing.dto;
 
-import java.util.List;
-import racing.domain.Car;
-import racing.domain.CarGenerator;
-import racing.domain.MoveRule;
-
 public class RacingInfo {
 
-    private final List<Car> cars;
+    private final String carNames;
     private final int totalRoundNumber;
 
-    public RacingInfo(List<Car> cars, int totalRoundNumber) {
-        this.cars = cars;
+    public RacingInfo(String carNames, int totalRoundNumber) {
+        this.carNames = carNames;
         this.totalRoundNumber = totalRoundNumber;
-    }
-
-    public static RacingInfo of(String carNames, int totalRoundNumber, MoveRule moveRule) {
-        final List<Car> cars = CarGenerator.createCars(carNames, moveRule);
-        return new RacingInfo(cars, totalRoundNumber);
-    }
-
-    public List<Car> getCars() {
-        return cars;
     }
 
     public int getTotalRoundNumber() {
         return totalRoundNumber;
     }
+
+    public String getCarNames() {
+        return carNames;
+    }
+
 }
