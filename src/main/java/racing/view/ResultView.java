0526ee5545fc85f3cf20fdfe312d5347d574d8a4
eohaerim <ehl3288@naver.com>
2022-02-13 04:05:29 +0900

    refactor: 메소드명의 의미를 명확하게 변경함

--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -5,24 +5,24 @@ import racing.domain.CarResult;
 
 public class ResultView {
 
-    public static void printResultAndWinners(List<List<CarResult>> rawRoundResults, List<String> winners) {
+    public static void printResultAndWinners(List<List<CarResult>> totalRoundResults, List<String> winners) {
         System.out.println("실행 결과");
-        for (List<CarResult> carResults : rawRoundResults) {
-            System.out.println(formatResult(carResults));
+        for (List<CarResult> roundResult : totalRoundResults) {
+            System.out.println(formatResult(roundResult));
         }
 
         System.out.println("최종 우승자: " + String.join(", ", winners));
     }
 
-    private static StringBuilder formatResult(List<CarResult> carResults) {
+    private static StringBuilder formatResult(List<CarResult> roundResult) {
         final StringBuilder formattedResult = new StringBuilder();
 
-        for(CarResult vo : carResults) {
+        for(CarResult carResult : roundResult) {
             final StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < vo.getDistance(); i++) {
+            for(int i = 0; i < carResult.getDistance(); i++) {
                 sb.append('-');
             }
-            formattedResult.append(vo.getName()).append(" : ").append(sb).append("\n");
+            formattedResult.append(carResult.getName()).append(" : ").append(sb).append("\n");
         }
 
         return formattedResult;
