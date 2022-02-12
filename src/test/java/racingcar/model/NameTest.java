c97cdc22cb60b8a2677c9c63beee6e2e22d8683b
Jiin Lee <loveted0225@naver.com>
2022-02-12 23:20:18 +0900

    test(NameTest): 자동차 이름이 정상적으로 생성되는 경우 테스트 추가

--- src/test/java/racingcar/model/NameTest.java
+++ src/test/java/racingcar/model/NameTest.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,5 +13,12 @@ class NameTest {
     void 자동차의_이름이_5글자_넘으면_예외_발생(final String name) {
         assertThatExceptionOfType(IllegalArgumentException.class)
             .isThrownBy(() -> new Car(name));
+    }
+
+    @ValueSource(strings = {"Leah", "Sam", "Jiin"})
+    @ParameterizedTest
+    void 자동차의_이름이_정상적으로_생성(final String name) {
+       Name carName = new Name(name);
+       assertThat(carName.getCarName()).isEqualTo(name);
     }
 }
