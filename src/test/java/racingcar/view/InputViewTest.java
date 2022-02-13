441c4aed1356334bdeace747c111e95dead59324
Dami Kim <dami.k.dev@gmail.com>
2022-02-14 00:21:08 +0900

    refactor: 입력한 자동차 이름이 2개 미만일 때 예외 처리 테스트 코드 구현

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -2,6 +2,8 @@ package racingcar.view;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
@@ -20,5 +22,13 @@ class InputViewTest {
         assertThatThrownBy(() -> InputView.validateTryCount(WORD))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("[ERROR] 횟수는 숫자만 허용합니다.");
+    }
+
+    @Test
+    void 자동차_이름은_2개_이상이어야_한다() {
+        final List<String> carNames = Arrays.asList("a");
+        assertThatThrownBy(() -> InputView.validateCarNameCount(carNames))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 자동차 이름은 2개 이상이어야 합니다.");
     }
 }
