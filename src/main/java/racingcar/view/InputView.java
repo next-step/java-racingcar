3e46eb9449448851b8dff6975c643068e9b58681
Dami Kim <dami.k.dev@gmail.com>
2022-02-14 00:20:53 +0900

    refactor: 입력한 자동차 이름이 2개 미만일 때 예외 처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class InputView {
 
     private final static int MAX_NAME_LENGTH = 5;
+    private final static int MIN_NAME_COUNT = 2;
     private final static Scanner scanner = new Scanner(System.in);
 
     public static int getTryCount() {
@@ -22,6 +23,7 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
         List<String> carNames = Arrays.asList(scanner.nextLine().split(","));
+        validateCarNameCount(carNames);
 
         for (String carName : carNames) {
             validateCarNameLength(carName);
@@ -29,20 +31,25 @@ public class InputView {
         return carNames;
     }
 
+    public static void validateTryCount(String tryCount) {
+        if (tryCount.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 횟수는 공백일 수 없습니다.");
+        }
+
+        if (!tryCount.matches("[0-9]")) {
+            throw new IllegalArgumentException("[ERROR] 횟수는 숫자만 허용합니다.");
+        }
+    }
+
+    public static void validateCarNameCount(List<String> carNames) {
+        if (carNames.size() <= MIN_NAME_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 2개 이상이어야 합니다.");
+        }
+    }
+
     private static void validateCarNameLength(String carName) {
         if (carName.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.");
         }
     }
-
-    private static void validateTryCount(String tryCount) {
-        if (tryCount.isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] 횟수는 공백일 수 없습니다.");
-        }
-
-        if (tryCount.matches("[^0-9]")) {
-            throw new IllegalArgumentException("[ERROR] 횟수는 숫자만 허용합니다.");
-        }
-    }
-
 }
