52ff15499537afecdd8e2324ba1b905bccd3ca25
Dami Kim <dami.k.dev@gmail.com>
2022-02-14 01:10:55 +0900

    refactor: 랜덤 movement, Cars 생성 로직 application에 적용

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.domain.RacingCarGame;
 import racingcar.domain.Referee;
 import racingcar.view.InputView;
@@ -11,14 +12,15 @@ public class Application {
 
     public static void main(String[] args) {
         List<String> carNames = InputView.getCarNames();
+        List<Car> cars = new Cars(carNames).getCars();
         int tryCount = InputView.getTryCount();
 
-        RacingCarGame racingCarGame = new RacingCarGame(carNames, tryCount);
-
+        RacingCarGame racingCarGame = new RacingCarGame(cars, tryCount);
+        System.out.println("실행 결과");
         racingCarGame.play();
 
-        List<Car> cars = racingCarGame.getCars();
-        List<String> winners = Referee.getWinners(cars);
+        List<Car> MovingCars = racingCarGame.getMovingCars();
+        List<String> winners = Referee.getWinners(MovingCars);
         ResultView.printGameResult(winners);
     }
 }
