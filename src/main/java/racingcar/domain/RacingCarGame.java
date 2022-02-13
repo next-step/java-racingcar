092ebe7b722ec618559e28d7e5c6c1e846fd5e72
Dami Kim <dami.k.dev@gmail.com>
2022-02-14 01:10:33 +0900

    refactor: 랜덤 movement, Cars 생성 로직 분리

--- src/main/java/racingcar/domain/RacingCarGame.java
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -1,57 +1,33 @@
 package racingcar.domain;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import racingcar.view.ResultView;
 
 public class RacingCarGame {
 
-    private final static int MIN_MOVE = 0;
-    private final static int MAX_MOVE = 9;
-    private final static Random RANDOM = new Random();
     private final List<Car> cars;
-    private final List<String> carNames;
     private int tryCount;
 
-    public RacingCarGame(List<String> carNames, int tryCount) {
-        this.carNames = carNames;
+    public RacingCarGame(List<Car> cars, int tryCount) {
+        this.cars = cars;
         this.tryCount = tryCount;
-        cars = makeCarsByCarNames();
     }
 
     public void play() {
-        System.out.println("실행 결과");
-
         do {
             loadGame();
 
             tryCount--;
-
         } while (tryCount > 0);
     }
 
-    public List<Car> getCars() {
+    public List<Car> getMovingCars() {
         return cars;
-    }
-
-    private List<Car> makeCarsByCarNames() {
-        List<Car> cars = new ArrayList<>();
-
-        for (String carName : carNames) {
-            Car car = new Car(carName);
-            cars.add(car);
-        }
-        return cars;
-    }
-
-    private int getRandomMove() {
-        return MIN_MOVE + RANDOM.nextInt(MAX_MOVE - MIN_MOVE + 1);
     }
 
     private void loadGame() {
         for (Car car : cars) {
-            int randomMove = getRandomMove();
+            int randomMove = Movement.getRandomMove();
             car.move(randomMove);
 
             String carName = car.getName();
