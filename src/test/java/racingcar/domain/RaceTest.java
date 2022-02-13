bc5c4398c0218c597e8015bd0d54972ffcf12e1e
SeungYeonJeong <crowsy@naver.com>
2022-02-13 18:20:50 +0900

    test(Race): Movable에 맞춰 리팩터링

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -2,23 +2,25 @@ package racingcar.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class RaceTest {
 
     @Test
-    void _0에서_9사이의_정수를_반환하는지_확인한다() {
-        int[] check = new int[10];
-        Race race = new Race(Collections.singletonList("car"), 7);
-        for (int i = 0; i < 100; i++) {
-            int randomNumber = race.getRandomNumber();
-            check[randomNumber]++;
-        }
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            sum += check[i];
-        }
-        assertThat(sum).isEqualTo(100);
+    void 움직일_수_있는_상태라면_자동차는_전진한다() {
+        Race race = new Race(new Cars(Arrays.asList(new Car("seung", 1), new Car("yeon", 5))), 5);
+        List<List<Car>> histories = race.playGames(() -> true);
+        assertThat(histories.get(histories.size() - 1).get(0).getDistance()).isEqualTo(6);
+        assertThat(histories.get(histories.size() - 1).get(1).getDistance()).isEqualTo(10);
+    }
+
+    @Test
+    void 움직일_수_없는_상태라면_자동차는_전진하지_않는다() {
+        Race race = new Race(new Cars(Arrays.asList(new Car("seung", 1), new Car("yeon", 5))), 5);
+        List<List<Car>> histories = race.playGames(() -> false);
+        assertThat(histories.get(histories.size() - 1).get(0).getDistance()).isEqualTo(1);
+        assertThat(histories.get(histories.size() - 1).get(1).getDistance()).isEqualTo(5);
     }
 }
