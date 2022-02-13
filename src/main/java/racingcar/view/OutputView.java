b3f6b20eebc95ca9064402c579609d6432b110d4
SeungYeonJeong <crowsy@naver.com>
2022-02-13 18:40:27 +0900

    refactor: Simplify printWinners

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.domain.Car;
 
@@ -10,8 +11,8 @@ public class OutputView {
     }
 
     private static void printCars(List<Car> cars) {
-        System.out.println();
         cars.forEach(OutputView::printCar);
+        System.out.println();
     }
 
     private static void printCar(Car car) {
@@ -27,16 +28,11 @@ public class OutputView {
     }
 
     public static void printWinners(List<Car> winners) {
-        StringBuilder winnerNames = new StringBuilder();
-
-        for (int i = 0; i < winners.size(); i++) {
-            winnerNames.append(winners.get(i).getName());
-            if (i != (winners.size() - 1)) {
-                winnerNames.append(", ");
-            }
+        List<String> winnerNames = new ArrayList<>();
+        for (Car winner: winners) {
+            winnerNames.add(winner.getName());
         }
-
         System.out.print("최종 우승자: ");
-        System.out.println(winnerNames);
+        System.out.println(String.join(", ", winnerNames));
     }
 }
