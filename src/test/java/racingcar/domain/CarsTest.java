b91f7ef8ff975a3e07a1a0b3640d740dcf96baed
SeungYeonJeong <crowsy@naver.com>
2022-02-13 17:29:10 +0900

    refactor: 테스트 코드 수정

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,18 +1,15 @@
 package racingcar.domain;
 
-import static java.util.Arrays.asList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
-
     @Test
     void 입력_받은_이름으로_Car_객체를_만든다() {
-        List<String> list = new ArrayList<>(asList("jack", "smith", "lana"));
-        Cars cars = new Cars(list);
+        String[] strings = new String[]{"jack", "smith", "lana"};
+        Cars cars = new Cars(strings);
         List<Car> getCars = cars.getCars();
 
         assertThat(getCars.get(0).getName()).isEqualTo("jack");
--- src/test/java/racingcar/domain/WinnerTest.java
+++ src/test/java/racingcar/domain/WinnerTest.java
@@ -1,21 +1,16 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class WinnerTest {
     @Test
     void 가장_많이_전진한_자동차가_우승한다() {
-        List<Car> cars = Arrays.asList(
-            new Car("jack", 7),
-            new Car("smith", 6),
-            new Car("lana", 4)
-        );
-        Winner winner = new Winner(cars);
-        winner.setWinners();
-        List<Car> winners = winner.getWinners();
-        assertThat(winners.get(0)).isEqualTo(cars.get(0));
+        Winner winners = new Winner(new Cars(Arrays.asList(new Car("seung", 8)
+            , new Car("yeon", 2))));
+        winners.setWinners();
+        assertThat(winners.getWinners().get(0).getName()).isEqualTo("seung");
     }
 }
