f1b2dc2b6b34c5056c2dba64ea999f603a50edfa
eohaerim <ehl3288@naver.com>
2022-02-16 02:43:34 +0900

    test: Race 테스트 추가

--- src/test/java/racing/domain/RacingTest.java
+++ src/test/java/racing/domain/RacingTest.java
@@ -3,6 +3,7 @@ package racing.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,8 +12,22 @@ class RacingTest {
     @DisplayName(value = "carNames에서 얻은 자동차 수만큼 차를 생성한다.")
     @Test
     void createCar() {
-        RacingInfo info = new RacingInfo(Arrays.asList("a", "b", "c", "d"), 0);
-        Racing racing = new Racing(info);
+        List<Car> cars = CarGenerator.createCars(Arrays.asList("a", "b", "c", "d"),
+            new RandomMoveRule());
+        Racing racing = new Racing(cars, new Round(4));
         assertThat(racing.getCars()).hasSize(4);
     }
+
+    @Test
+    void race() {
+        Round round = new Round(1);
+        Racing racing = new Racing(
+            Arrays.asList(new Car("a", () -> true), new Car("b", () -> false)), round);
+
+        racing.race();
+        List<CarResult> lastRoundResult = round.getRacingResult().getLastRoundResult();
+
+        assertThat(lastRoundResult.get(0).getName()).isEqualTo("a");
+    }
+
 }
