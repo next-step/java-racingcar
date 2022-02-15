b1ad362141b3a0f1b37ddeda066da02d94e294d9
eohaerim <ehl3288@naver.com>
2022-02-16 02:43:57 +0900

    style: 코드 정렬

--- src/main/java/racing/domain/MoveRule.java
+++ src/main/java/racing/domain/MoveRule.java
@@ -2,5 +2,6 @@ package racing.domain;
 
 @FunctionalInterface
 public interface MoveRule {
+
     boolean isMovable();
 }
--- src/test/java/racing/domain/CarGeneratorTest.java
+++ src/test/java/racing/domain/CarGeneratorTest.java
@@ -10,7 +10,8 @@ class CarGeneratorTest {
 
     @Test
     void 문자열과_이동전략을_주입받아_차를_생성한다() {
-        List<Car> cars = CarGenerator.createCars(Arrays.asList("a","b","c","d"), new RandomMoveRule());
+        List<Car> cars = CarGenerator.createCars(Arrays.asList("a", "b", "c", "d"),
+            new RandomMoveRule());
         assertThat(cars).hasSize(4);
         assertThat(cars).allSatisfy(car -> assertThat(car.getDistance()).isEqualTo(0));
     }
