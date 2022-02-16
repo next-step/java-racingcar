ba3eb892e4e3fe6db2ce2373010e6ff629a4d6ec
eohaerim <ehl3288@naver.com>
2022-02-17 00:56:28 +0900

    test: 코드 정렬, 불필요한 테스트 제거

--- src/test/java/racing/domain/RacingTest.java
+++ src/test/java/racing/domain/RacingTest.java
@@ -5,18 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RacingTest {
-
-    @DisplayName(value = "carNames에서 얻은 자동차 수만큼 차를 생성한다.")
-    @Test
-    void createCar() {
-        List<Car> cars = CarGenerator.createCars(Arrays.asList("a", "b", "c", "d"), new RandomMoveRule());
-        Racing racing = new Racing(cars, new Round(4));
-        assertThat(racing.getCars()).hasSize(4);
-    }
 
     @Test
     void race() {
