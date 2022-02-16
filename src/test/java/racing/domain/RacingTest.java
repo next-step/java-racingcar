a16f4f0fa503f03ac4a22eb90154f2dbc17a819c
eohaerim <ehl3288@naver.com>
2022-02-16 23:00:14 +0900

    empty.txt 삭제, 코드 정렬

--- src/test/java/racing/domain/RacingTest.java
+++ src/test/java/racing/domain/RacingTest.java
@@ -12,8 +12,7 @@ class RacingTest {
     @DisplayName(value = "carNames에서 얻은 자동차 수만큼 차를 생성한다.")
     @Test
     void createCar() {
-        List<Car> cars = CarGenerator.createCars(Arrays.asList("a", "b", "c", "d"),
-            new RandomMoveRule());
+        List<Car> cars = CarGenerator.createCars(Arrays.asList("a", "b", "c", "d"), new RandomMoveRule());
         Racing racing = new Racing(cars, new Round(4));
         assertThat(racing.getCars()).hasSize(4);
     }
@@ -21,13 +20,13 @@ class RacingTest {
     @Test
     void race() {
         Round round = new Round(1);
-        Racing racing = new Racing(
-            Arrays.asList(new Car("a", () -> true), new Car("b", () -> false)), round);
+        Racing racing = new Racing(Arrays.asList(new Car("a", () -> true), new Car("b", () -> false)), round);
 
         racing.race();
         List<CarResult> lastRoundResult = round.getRacingResult().getLastRoundResult();
 
         assertThat(lastRoundResult.get(0).getName()).isEqualTo("a");
+        //distance확인, b의 distance도 확인
     }
 
 }
