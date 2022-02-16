9179514880c61e9ed2951a396d92cabb21bdfd09
Dami Kim <dami.k.dev@gmail.com>
2022-02-17 02:02:51 +0900

    refactor: ResultView 문구, 기호 상수화

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,32 +1,36 @@
 package racingcar.view;
 
 import java.util.List;
-import racingcar.domain.RacingResult;
-import racingcar.domain.RacingTotalResult;
+import racingcar.domain.Car;
 
 public class ResultView {
 
-    public static void printGameStatus() {
-        List<RacingResult> racingResults = RacingTotalResult.getRacingResults();
-        racingResults.forEach(result -> {
-            String dashes = getDashByMove(result.getMovingDistance());
-            System.out.println(result.getCarName() + ":" + dashes);
+    private final static String GAME_WINNER_MESSAGE = "최종 우승자: ";
+    private final static String GAME_RESULT_MESSAGE = "실행 결과";
+    private final static String DELIMITER = ", ";
+    private final static String COLON = ":";
+    private final static String DASH = "-";
+
+    public static void printGameStatus(List<Car> cars) {
+        cars.forEach(car -> {
+            String dashes = getDashByMove(car.getPosition());
+            System.out.println(car.getName() + COLON + dashes);
         });
     }
 
     public static void printGameResult(List<String> winners) {
-        System.out.println("최종 우승자: " + String.join(", ", winners));
+        System.out.println(GAME_WINNER_MESSAGE + String.join(DELIMITER, winners));
     }
 
     public static void printGameResultMessage() {
-        System.out.println("실행 결과");
+        System.out.println(GAME_RESULT_MESSAGE);
     }
 
     private static String getDashByMove(int move) {
         StringBuilder dash = new StringBuilder();
 
         for (int i = 0; i < move; i++) {
-            dash.append("-");
+            dash.append(DASH);
         }
         return dash.toString();
     }
