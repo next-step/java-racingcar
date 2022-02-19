ae63d86e49c2350c0ead1c8b93efc6852fb2a578
chanu94 <qkrcksdn0208@naver.com>
2022-02-20 01:41:08 +0900

    feat : Car 구현
    ### Car
    - [x] 이름을 가진다.
    - [x] 현재의 위치를 가진다.
    - [x] 전진 또는 멈출 수 있다.
    - [x] 현재의 이름과 위치를 String으로 반환한다.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,5 +1,38 @@
 package domain;
 
+import java.util.Collections;
+
 public class Car {
 
+    public static final int INITIAL_DISTANCE = 0;
+    public static final String CAR_STATUS_FORMAT = "%s : %s";
+    public static final String DISTANCE_FORMAT = "-";
+
+    private final String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        distance = INITIAL_DISTANCE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void move() {
+        distance++;
+    }
+
+    public String convertStatus() {
+        return String.format(CAR_STATUS_FORMAT, name, convertDistance());
+    }
+
+    private String convertDistance() {
+        return String.join("", Collections.nCopies(distance, DISTANCE_FORMAT));
+    }
 }
---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,43 @@
+package domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    public static final String TEST_NAME = "박찬우";
+    public Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car(TEST_NAME);
+    }
+
+    @Test
+    void 이름을_가질수_있다() {
+        assertThat(car.getName()).isEqualTo(TEST_NAME);
+    }
+
+    @Test
+    void 위치의_초기값은_0이다() {
+        assertThat(car.getDistance()).isEqualTo(0);
+    }
+
+    @Test
+    void 전진할_수_있다() {
+        car.move();
+        car.move();
+        assertThat(car.getDistance()).isEqualTo(2);
+    }
+
+    @Test
+    void 현재이름_위치를_String으로_반환() {
+        car.move();
+        car.move();
+        car.move();
+        assertThat(car.convertStatus()).isEqualTo("박찬우 : ---");
+    }
+}
