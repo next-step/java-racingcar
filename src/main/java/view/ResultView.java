306b5d6c3856ca0a569527fd85c9d19dc52693c2
chanu94 <qkrcksdn0208@naver.com>
2022-02-20 21:20:20 +0900

    feat : ResultView 구현
    ### ResultView
    - printCars()
    - printWinners()

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,5 +1,21 @@
 package view;
 
+import domain.Cars;
+
 public class ResultView {
 
+    public static final String RESULT_HEAD_TEXT = "\n실행결과";
+    public static final String PRINT_WINNER_FORMAT = "최종 우승자: %s\n";
+
+    public static void printResultHead() {
+        System.out.println(RESULT_HEAD_TEXT);
+    }
+
+    public static void printCars(Cars cars) {
+        System.out.println(cars.convertCarsStatus());
+    }
+
+    public static void printWinners(String winners) {
+        System.out.printf(PRINT_WINNER_FORMAT, winners);
+    }
 }
