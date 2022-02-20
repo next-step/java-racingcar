dd7752bbb90fb7fa38b335ded2d41d4bb921a914
chanu94 <qkrcksdn0208@naver.com>
2022-02-20 21:20:43 +0900

    feat : RacingMain 구현

--- src/main/java/RacingMain.java
+++ src/main/java/RacingMain.java
@@ -1,16 +1,18 @@
+import domain.RacingGame;
 import view.InputView;
 import view.ResultView;
-import domain.RacingGame;
 
 public class RacingMain {
+
     public static void main(String[] args) {
         String carNames = InputView.getCarNames();
         int tryNo = InputView.getTryNo();
 
         RacingGame racingGame = new RacingGame(carNames, tryNo);
+        ResultView.printResultHead();
         while (!racingGame.isEnd()) {
             racingGame.race();
-            ResultView.printCars(racingGame.getCars());
+            ResultView.printCars(racingGame.getRacingCars());
         }
         ResultView.printWinners(racingGame.getWinners());
     }
