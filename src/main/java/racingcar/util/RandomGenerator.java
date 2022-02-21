9dbde30ff546591fdf415b1c33739c9bce89223a
fpal95 <snc7303@gmail.com>
2022-02-21 18:46:08 +0900

    Refactor(RandomGenerator): Random()을 static으로 수정

--- src/main/java/racingcar/util/RandomGenerator.java
+++ src/main/java/racingcar/util/RandomGenerator.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class RandomGenerator {
 
     private static final int MAX_RANGE_VALUE = 10;
+    private static final Random random = new Random();
 
-    public int generateRandomNumber() {
-        Random random = new Random();
+    public static int generateRandomNumber() {
         return random.nextInt(MAX_RANGE_VALUE);
     }
 }
