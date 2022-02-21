d2c2a7eddc6d6567f96970045893d91c56ec9928
fpal95 <snc7303@gmail.com>
2022-02-21 14:27:12 +0900

    Refactor(InputView): Scanner를 재사용 가능하도록 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,18 +3,15 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String[] getCarName() {
-        Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
 
         return userInput.split(",");
     }
 
     public static int getTryCount() {
-        Scanner scanner = new Scanner(System.in);
-        int tryCount = scanner.nextInt();
-
-        return tryCount;
+        return scanner.nextInt();
     }
 }
