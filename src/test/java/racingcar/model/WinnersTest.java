cb565206f15f6c97ed8d867256e94232650c52b8
fpal95 <snc7303@gmail.com>
2022-02-21 23:46:23 +0900

    Test(WinnersTest): 최대이동거리에 따른 우승자 리스트 생성 여부 테스트

--- src/test/java/racingcar/model/WinnersTest.java
+++ src/test/java/racingcar/model/WinnersTest.java
@@ -1,15 +1,13 @@
 package racingcar.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class WinnersTest {
     @Test
-    void 자동차_리스트가_주어졌을때_맥스포지션이_5이면_포지션이_5인_자동차만_위너리스트에_포함된다() {
+    void 자동차_리스트가_주어졌을때_가장_멀리_이동한_자동차만_위너리스트에_포함된다1() {
         Winners winners = new Winners(
             Arrays.asList(
                 new Car("wonie", 3),
@@ -17,7 +15,20 @@ class WinnersTest {
                 new Car("jonie", 5)
             )
         );
-        assertThat(winners.winnerList()).hasSize(1);
+        assertThat(winners.getRaceResults()).hasSize(1);
     }
+
+    @Test
+    void 자동차_리스트가_주어졌을때_가장_멀리_이동한_자동차만_위너리스트에_포함된다2() {
+        Winners winners = new Winners(
+            Arrays.asList(
+                new Car("wonie", 3),
+                new Car("pobie", 5),
+                new Car("jonie", 5)
+            )
+        );
+        assertThat(winners.getRaceResults()).hasSize(2);
+    }
+
 
 }
