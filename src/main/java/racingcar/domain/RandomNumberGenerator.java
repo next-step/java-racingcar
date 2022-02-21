ac3a0fec68c775d2f8764a170b53882610bd5d0f
minsang0850 <minsang0850@naver.com>
2022-02-22 01:54:29 +0900

    refactor: add pricate constructor

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -4,6 +4,9 @@ public class RandomNumberGenerator {
 
     private static final int MAX_NUMBER = 10;
 
+    private RandomNumberGenerator() {
+    }
+
     public static int getRandomNumber() {
         return (int) Math.floor(Math.random() * MAX_NUMBER);
     }
