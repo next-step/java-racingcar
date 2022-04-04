8b34ea4f93e730fa2b0525ccd3f84deda93a4ff3
august <conomeer@gmail.com>
2022-04-04 19:46:17 +0900

    test: Add String::split case of single tests

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,8 +8,15 @@ public class StringTest {
 
     @Test
     @DisplayName("\"1,2\"을 \",\"로 split 했을 때 1과 2로 분리된다.")
-    void split() {
+    void split_multiple() {
         String[] result = "1,2".split(",");
         Assertions.assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("\"1\"을 \",\"로 split 했을 때 1을 포함하는 배열이 반환된다.")
+    void split_single() {
+        String[] result = "1".split(",");
+        Assertions.assertThat(result).containsExactly("1");
     }
 }
