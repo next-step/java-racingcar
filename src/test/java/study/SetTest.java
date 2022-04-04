53efd9df3a8aaf530326c035a0721add0b7310e7
august <conomeer@gmail.com>
2022-04-04 20:43:38 +0900

    test: Add Set::contains by `@CsvSource` tests

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -31,7 +32,14 @@ public class SetTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("1,1,2,3이 추가되면 Set에 1,2,3의 값이 존재해야한다.")
-    void contains(int input) {
+    void contains_pass(int input) {
         Assertions.assertThat(numbers.contains(input)).isEqualTo(true);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("1,1,2,3이 추가되면 Set에 1,2,3의 값은 존재하고, 4,5는 존재하지 않는다.")
+    void contains_fail(int input, boolean expected) {
+        Assertions.assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
