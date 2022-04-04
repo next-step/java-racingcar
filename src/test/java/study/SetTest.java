4053aae64e7eb485670511563761a52282276c9f
August <polynomeer@gmail.com>
2022-04-04 21:21:52 +0900

    [Mission1] Step1 - 학습 테스트 실습 (#3076)
    * test: Add String::split case of multiple tests
    
    * test: Add String::split case of single tests
    
    * test: Add String::substring tests
    
    * test: Add String::charAt tests
    
    * test: Add Set::size tests
    
    * test: Add Set::contains by `@ValueSource` tests
    
    * test: Add Set::contains by `@CsvSource` tests
    
    * chore: Change to use static import for Assertions
    
    Co-authored-by: august <conomeer@gmail.com>

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setup() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("1,1,2,3이 추가되면 Set의 크기가 3이어야 한다.")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("1,1,2,3이 추가되면 Set에 1,2,3의 값이 존재해야한다.")
+    void contains_pass(int input) {
+        assertThat(numbers.contains(input)).isEqualTo(true);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("1,1,2,3이 추가되면 Set에 1,2,3의 값은 존재하고, 4,5는 존재하지 않는다.")
+    void contains_fail(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,55 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("\"1,2\"을 \",\"로 split 했을 때 1과 2로 분리된다.")
+    void split_multiple() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("\"1\"을 \",\"로 split 했을 때 1을 포함하는 배열이 반환된다.")
+    void split_single() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 ()을 제거하고 \"1,2\"를 반환한다.")
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("\"abc\" 값이 주어졌을 때 String::charAt로 특정 위치의 문자를 가져온다.")
+    void charAt() {
+        String value = "abc";
+        char first = value.charAt(0);
+        char second = value.charAt(1);
+        char third = value.charAt(2);
+
+        assertThat(first).isEqualTo('a');
+        assertThat(second).isEqualTo('b');
+        assertThat(third).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("\"abc\" 값이 주어졌을 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생한다.")
+    void charAt_exception() {
+        String value = "abc";
+
+        assertThatThrownBy(() -> value.charAt(4))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+    }
+
+}
