af3eb2b7b49aa972ef0627a88598039b85a0b1dc
kivv00ng <rldnd2637@naver.com>
2022-04-05 02:46:16 +0900

    String And Set test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,11 +4,22 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringTest {
     @Test
     void split(){
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1","2");
+    }
+    @Test
+    void substring(){
+        String result = "(1,2)".substring(1,4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() throws Exception{
+        assertThrows(StringIndexOutOfBoundsException.class,()->{char result = "abc".charAt(4);} );
     }
 }
