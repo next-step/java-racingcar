e5002986e0d52516f57f64cd6a0d996406116608
august <conomeer@gmail.com>
2022-04-04 20:45:46 +0900

    chore: Change to use static import for Assertions

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,5 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +9,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -26,20 +27,20 @@ public class SetTest {
     @Test
     @DisplayName("1,1,2,3이 추가되면 Set의 크기가 3이어야 한다.")
     void size() {
-        Assertions.assertThat(numbers.size()).isEqualTo(3);
+        assertThat(numbers.size()).isEqualTo(3);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("1,1,2,3이 추가되면 Set에 1,2,3의 값이 존재해야한다.")
     void contains_pass(int input) {
-        Assertions.assertThat(numbers.contains(input)).isEqualTo(true);
+        assertThat(numbers.contains(input)).isEqualTo(true);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
     @DisplayName("1,1,2,3이 추가되면 Set에 1,2,3의 값은 존재하고, 4,5는 존재하지 않는다.")
     void contains_fail(int input, boolean expected) {
-        Assertions.assertThat(numbers.contains(input)).isEqualTo(expected);
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -10,21 +12,21 @@ public class StringTest {
     @DisplayName("\"1,2\"을 \",\"로 split 했을 때 1과 2로 분리된다.")
     void split_multiple() {
         String[] result = "1,2".split(",");
-        Assertions.assertThat(result).containsExactly("1", "2");
+        assertThat(result).containsExactly("1", "2");
     }
 
     @Test
     @DisplayName("\"1\"을 \",\"로 split 했을 때 1을 포함하는 배열이 반환된다.")
     void split_single() {
         String[] result = "1".split(",");
-        Assertions.assertThat(result).containsExactly("1");
+        assertThat(result).containsExactly("1");
     }
 
     @Test
     @DisplayName("\"(1,2)\" 값이 주어졌을 때 ()을 제거하고 \"1,2\"를 반환한다.")
     void substring() {
         String result = "(1,2)".substring(1, 4);
-        Assertions.assertThat(result).isEqualTo("1,2");
+        assertThat(result).isEqualTo("1,2");
     }
 
     @Test
@@ -35,9 +37,9 @@ public class StringTest {
         char second = value.charAt(1);
         char third = value.charAt(2);
 
-        Assertions.assertThat(first).isEqualTo('a');
-        Assertions.assertThat(second).isEqualTo('b');
-        Assertions.assertThat(third).isEqualTo('c');
+        assertThat(first).isEqualTo('a');
+        assertThat(second).isEqualTo('b');
+        assertThat(third).isEqualTo('c');
     }
 
     @Test
@@ -45,7 +47,7 @@ public class StringTest {
     void charAt_exception() {
         String value = "abc";
 
-        Assertions.assertThatThrownBy(() -> value.charAt(4))
+        assertThatThrownBy(() -> value.charAt(4))
                 .isInstanceOf(StringIndexOutOfBoundsException.class)
                 .hasMessageContaining("String index out of range: 4");
     }
