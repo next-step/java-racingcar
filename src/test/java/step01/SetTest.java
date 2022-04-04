a1116e5c4287b3afae14306d5f0bb11c2a1a39f8
cooper <pbg0205@naver.com>
2022-04-04 16:52:04 +0900

    test: set 원소 확인 테스트

---
+++ src/test/java/step01/SetTest.java
@@ -0,0 +1,33 @@
+package step01;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("set 원소들의 포함 여부를 테스트한다")
+    void containsTest(int value, boolean hasElement) {
+        assertThat(numbers.contains(value)).isEqualTo(hasElement);
+    }
+
+}
