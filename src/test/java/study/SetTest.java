89e6fb85cbcc2309c4c0a24d5787de91099ab59d
august <conomeer@gmail.com>
2022-04-04 20:28:39 +0900

    test: Add Set::size tests

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setup() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("1,1,2,3이 추가되면 Set의 크기가 3이어야 한다.")
+    void size() {
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+}
