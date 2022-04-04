3a8691bd1fe635b2c445d3d7d01a84c1c96cfceb
august <conomeer@gmail.com>
2022-04-04 20:35:23 +0900

    test: Add Set::contains by `@ValueSource` tests

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,5 +26,12 @@ public class SetTest {
     @DisplayName("1,1,2,3이 추가되면 Set의 크기가 3이어야 한다.")
     void size() {
         Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("1,1,2,3이 추가되면 Set에 1,2,3의 값이 존재해야한다.")
+    void contains(int input) {
+        Assertions.assertThat(numbers.contains(input)).isEqualTo(true);
     }
 }
