747dbf2e9b8a45ab785ef069e737d7247e7963d1
강지수 <37580216+kang-jisu@users.noreply.github.com>
2022-04-05 01:42:02 +0900

    Step1 - 1단계 학습 테스트 실습 (#3080)
    * feat: StringTest
    
    - split test
    - substring test
    - charAt test
    - charAt exception test
    
    * feat: SetCollectionTest
    
    - size test
    - parameterizedTest를 사용한 contains test
    - csvSource를 사용한 contains test
    
    * feat: 테스트 데이터 추가
    
    * fix: hasSize() 메소드 사용

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,48 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetCollectionTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기 확인")
+    void sizeTest() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set의 contains() 메소드를 활용해 값 존재 확인")
+    void containsTest(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("Set의 contains() 메소드를 CsvSource의 값으로 테스트")
+    void containsCsvSourceTest(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
