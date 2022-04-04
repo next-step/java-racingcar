fa711721062e7ea704ace7cb9592fd8d22683124
pangnem <sprnd645@gmail.com>
2022-04-05 00:14:55 +0900

    test: StringTest
    `@Nested`어노테이션을 활용하여 계층형 테스트를 사용하였습니다.
    
    See Also:
    - https://johngrib.github.io/wiki/junit5-nested/

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,57 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    void splitTestWithTwoNumber() {
+        String string = "1,2";
+
+        String[] actual = string.split(",");
+
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void splitWithOneNumber() {
+        String string = "1,";
+
+        String[] actual = string.split(",");
+
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void subString() {
+        String string = "(1,2)";
+
+        String actual = string.substring(1, 4);
+
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Nested
+    @DisplayName("charAt 메서드는")
+    class Describe_charAt {
+        private static final String STRING = "abc";
+
+        @Nested
+        @DisplayName("벗어난 위치 값이 주어지면")
+        class Context_exceedLocationValue {
+            private final int EXCEED_LOCATION_VALUE = STRING.length() + 1;
+
+            @Test
+            @DisplayName("StringIndexOutOfBoundsException을 던진다")
+            void It_throwsStringIndexOutOfBoundsException() {
+                assertThatThrownBy(() -> STRING.charAt(EXCEED_LOCATION_VALUE))
+                        .isInstanceOf(StringIndexOutOfBoundsException.class);
+            }
+        }
+    }
+}
