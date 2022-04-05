d7ba020897beb64caed69ea2a749265304c40482
minigear <coosac@gmail.com>
2022-04-06 00:24:02 +0900

    요구사항 3 추가 - StringIndexOutOfBoundsException test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,7 +3,7 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -30,5 +30,22 @@ public class StringTest {
 
         //then
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("요구사항3: StringIndexOutOfBoundsException test")
+    void charAtTest() {
+        //given
+        String data = "abc";
+
+        //when
+        Throwable thrown = catchThrowable(() -> {
+            char a = data.charAt(3);
+        });
+
+        //then
+        assertThat(thrown).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+
     }
 }
