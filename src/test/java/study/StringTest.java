83a8f098e0e11c913342aead12521caa69a50f2f
최영훈 <c_yh0327@naver.com>
2022-04-05 22:21:55 +0900

    [refactor] displayName 수정
    1. low dash로 만들어놨던 이름들을 일반 문자열로 수정하였음.
    2. displayName을 명확히 표현.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -22,7 +22,7 @@ public class StringTest {
         }
 
         @Test
-        @DisplayName("성공 - 문자열에_분리할_문자가_없어도_배열이_반환된다.")
+        @DisplayName("성공 - 문자열에 분리할 문자가 없어도 배열이 반환된다.")
         void test2() {
             String[] result = "1".split(",");
             assertThat(result).contains("1");
@@ -35,7 +35,7 @@ public class StringTest {
     class 요구사항2 {
 
         @Test
-        @DisplayName("성공 - Substring_test")
+        @DisplayName("성공 -  start 인덱스부터 end 전 인덱스까지 문자열을 자른다.")
         void test1() {
             String testCase = "(1,2)";
             String result =testCase.substring(1, testCase.length() - 1);
@@ -48,7 +48,7 @@ public class StringTest {
     class 요구사항3 {
 
         @Test
-        @DisplayName("성공 - ChartAt으로_특정_위치의_문자를_가져온다")
+        @DisplayName("성공 - ChartAt으로 특정위치의 문자를 가져온다")
         void test1() {
             String testCase = "abc";
 
@@ -58,7 +58,7 @@ public class StringTest {
         }
 
         @Test
-        @DisplayName("실패 - CharAt으로_문자열의_범위를_넘긴다")
+        @DisplayName("실패 - CharAt으로 문자열의 범위를 넘긴다")
         void test2() {
             String testCase = "abc";
 
