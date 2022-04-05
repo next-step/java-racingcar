e4bb308b629d2de1affc5cc2ba6b55816e992863
minigear <coosac@gmail.com>
2022-04-06 00:50:19 +0900

    Set Collection에 대한 학습 테스트 - 요구사항 3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,10 +35,17 @@ public class SetTest {
         assertThat(size).isEqualTo(3);
     }
 
+    @DisplayName("요구사항2: set contains 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3})
-    @DisplayName("요구사항2: 중복제거 테스트")
+    @ValueSource(ints = {1, 2, 3})
     void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("요구사항3: set contains and not contains 테스트")
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void containsNotTest(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
