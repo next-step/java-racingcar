5f1af7fc5bc15289058fcd0b8736e3089cbc1423
김기웅 <93498749+kivv00ng@users.noreply.github.com>
2022-04-05 09:29:55 +0900

    Step1 (#3092)
    * split study test
    
    * String And Set test
    
    * String And Set test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,45 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private HashSet<Integer> numbers;
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    void size(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints={1,2,3})
+    void containsV1(int input){
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value= {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsV2(int input, Boolean expected){
+        System.out.println(expected);
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+
+    }
+}
