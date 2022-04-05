af92ad4fed0e35a34a3c6c142fdb61092b6df9b5
minigear <coosac@gmail.com>
2022-04-06 00:35:13 +0900

    Set Collection에 대한 학습 테스트 - 요구사항 2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,11 +3,13 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -30,5 +32,12 @@ public class SetTest {
 
         //then
         assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("요구사항2: 중복제거 테스트")
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
