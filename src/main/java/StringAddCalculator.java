c95545abe651a5ad1ad718658d7dbd989a505e82
hihisososo <hihisososo@naver.com>
2022-04-07 07:45:22 +0900

    2단계 - 문자열 덧셈 계산기

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,78 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StringAddCalculator {
+
+
+    private static final String OR = "|";
+    private static final String BASE_SPLIT_REGEX = ",|:";
+    private static final String CUSTOM_DELIMITER_FIND_REGEX = "^//(.*)\n(.*)";
+
+    public Integer calcSum(String text) {
+        if (isEmptyOrNull(text)) {
+            return 0;
+        }
+
+        if (containsCustomDelimeter(text)) {
+            return customDelimiterSum(text);
+        }
+
+        return normalDelimiterSum(text);
+    }
+
+    private Integer normalDelimiterSum(String text) {
+        List<Integer> splitted = split(BASE_SPLIT_REGEX, text);
+        checkPositive(splitted);
+        return sum(splitted);
+    }
+
+    private Integer customDelimiterSum(String text) {
+        Matcher customDelimiterMatch = Pattern.compile(CUSTOM_DELIMITER_FIND_REGEX).matcher(text);
+        customDelimiterMatch.find();
+
+        String customDelimeter = customDelimiterMatch.group(1);
+        String delimeterRemovedText = customDelimiterMatch.group(2);
+
+        List<Integer> splitted = split(BASE_SPLIT_REGEX + OR + customDelimeter, delimeterRemovedText);
+        checkPositive(splitted);
+        return sum(splitted);
+    }
+
+    private void checkPositive(List<Integer> nums) {
+        for (Integer num : nums) {
+            checkPositive(num);
+        }
+    }
+
+    private void checkPositive(Integer num) {
+        if (num < 0) {
+            throw new RuntimeException("number is negative");
+        }
+    }
+
+    private Integer sum(List<Integer> splittedNumber) {
+        return splittedNumber.stream().mapToInt(i -> i).sum();
+    }
+
+    private List<Integer> split(String delimiter, String text) {
+        try {
+            return Stream.of(text.split(delimiter))
+                    .mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException("input text must contains only delimiter or number");
+        }
+    }
+
+    private boolean containsCustomDelimeter(String text) {
+        return text.matches(CUSTOM_DELIMITER_FIND_REGEX);
+    }
+
+    private boolean isEmptyOrNull(String text) {
+        return text == null || text.isEmpty();
+    }
+}
---
+++ src/test/java/StringAddCalculatorTest.java
@@ -0,0 +1,80 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StringAddCalculatorTest {
+    StringAddCalculator stringAddCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringAddCalculator = new StringAddCalculator();
+    }
+
+    @Test
+    @DisplayName("빈 문자열이 들어왔을 경우, 0을 반환")
+    void emptyTest() {
+        assertThat(stringAddCalculator.calcSum("")).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("null 문자열이 들어왔을 경우, 0을 반환")
+    void nullTest() {
+        assertThat(stringAddCalculator.calcSum(null)).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("콤마, 콜론 구분자를 통해 구분된 숫자의 합을 반환")
+    @CsvSource(value = {"1,1|2", "1,2:3|6", "10:1:2|13", "0:0,0|0", "100:100:11|211", "2:2,2|6"}, delimiter = '|')
+    void addTest(String text, Integer sum) {
+        assertThat(stringAddCalculator.calcSum(text)).isEqualTo(sum);
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("커스텀 구분자를 통해 구분된 숫자의 합을 반환")
+    @MethodSource("customDelimiterParameters")
+    void customDelimiterTest(String text, Integer sum) {
+        assertThat(stringAddCalculator.calcSum(text)).isEqualTo(sum);
+    }
+
+    private static Stream<Arguments> customDelimiterParameters() {
+        return Stream.of(
+                Arguments.of("//;\n1;2;3", 6),
+                Arguments.of("//&&\n1&&2&&7", 10),
+                Arguments.of("///\n1/2/3", 6),
+                Arguments.of("//!\n1!2!7", 10),
+                Arguments.of("//'\n10'20'30", 60)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("음수가 들어왔을 경우, RuntimeException 을 반환")
+    @ValueSource(strings = {"1,-1", "-5,-10", "1,2,5,6,-1"})
+    void negativeNumberTest(String text) {
+        assertThrows(RuntimeException.class, () -> {
+            stringAddCalculator.calcSum(text);
+        });
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자가 아닌 값이 들어왔을 경우, RuntimeException 을 반환")
+    @ValueSource(strings = {"1,-asd1", "zx", "maskld,1k:123", "123mklz,123"})
+    void notNumberTest(String text) {
+        assertThrows(RuntimeException.class, () -> {
+            stringAddCalculator.calcSum(text);
+        });
+    }
+
+}
