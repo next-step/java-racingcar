5eb347d2de00e4667de932568bbf28f892d8754f
blossun <blossunn@gmail.com>
2022-04-06 21:32:57 +0900

    4. 의미있는 이름으로 변경해준다.

--- src/main/java/C.java
+++
@@ -1,5 +0,0 @@
-public class C {
-    public static int c(final String o) {
-        return 0;
-    }
-}
---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,5 @@
+public class StringAddCalculator {
+    public static int splitAndSum(final String o) {
+        return 0;
+    }
+}
