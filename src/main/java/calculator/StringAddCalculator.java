76008cf063e87907b5886a6e546e29c4ef53c8b3
polynomeer <polynomeer@gmail.com>
2022-04-06 15:39:08 +0900

    refactor: Add StringAddCalculator::sum

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -17,18 +17,21 @@ public class StringAddCalculator {
         if (m.find()) {
             String customDelimiter = m.group(1);
             String[] tokens = m.group(2).split(customDelimiter);
-
-            for (String token : tokens) {
-                if (Integer.parseInt(token) < 0) throw new RuntimeException("음수는 나올 수 없습니다.");
-                sum += Integer.parseInt(token);
-            }
+            sum = sum(tokens);
         } else {
             String[] tokens = text.split(defaultDelimiter);
+            sum = sum(tokens);
+        }
 
-            for (String token : tokens) {
-                if (Integer.parseInt(token) < 0) throw new RuntimeException("음수는 나올 수 없습니다.");
-                sum += Integer.parseInt(token);
-            }
+        return sum;
+    }
+
+    private static int sum(String[] tokens) {
+        int sum = 0;
+
+        for (String token : tokens) {
+            if (Integer.parseInt(token) < 0) throw new RuntimeException("음수는 계산할 수 없습니다.");
+            sum += Integer.parseInt(token);
         }
 
         return sum;
