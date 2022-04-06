3c11876df33da950c1f1b2b308dce3106abb2761
itdar <thesockerr@gmail.com>
2022-04-06 21:58:35 +0900

    test: 문자열 덧셈 계산기 테스트 수정

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -3,11 +3,13 @@ package step2;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringAddCalculatorTest {
+
     @Test
-    public void splitAndSum_null_또는_빈문자() {
+    public void splitAndSum_null_또는_빈문자는_0_반환한다() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
@@ -16,31 +18,46 @@ public class StringAddCalculatorTest {
     }
 
     @Test
-    public void splitAndSum_숫자하나() throws Exception {
+    public void splitAndSum_유효한_숫자하나는_그대로_반환한다() {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
+    public void splitAndSum_유효하지않은_문자하나는_RuntimeException_발생한다() {
+        assertThatThrownBy(() ->
+            StringAddCalculator.splitAndSum("r")
+        ).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitAndSum_유효하지않은_문자열은_RuntimeException_발생한다() {
+        assertThatThrownBy(() ->
+            StringAddCalculator.splitAndSum("go")
+        ).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+    public void splitAndSum_쉼표_또는_콜론_구분자() {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_custom_구분자() throws Exception {
+    public void splitAndSum_custom_구분자() {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
+    @DisplayName("음수를 전달할 경우 RuntimeException 이 발생한다.")
     @Test
-    public void splitAndSum_negative() throws Exception {
+    public void splitAndSum_negative() {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
             .isInstanceOf(RuntimeException.class);
     }
