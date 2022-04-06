a374802280cc22eb2015456a1fe220423a88a45d
devgony <devgony@gmail.com>
2022-04-06 22:17:58 +0900

    Early return with sum

--- src/main/java/calculator/StringAddCalculatorTest.java
+++ src/main/java/calculator/StringAddCalculatorTest.java
@@ -7,29 +7,26 @@ public class StringAddCalculatorTest {
     public static int splitAndSum(String text) {
         if (text == null || text.isBlank()) {
             return 0;
-
         }
-        int result = 0;
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
             String[] values = m.group(2).split(customDelimiter);
-            sum(values, result);
-        } else {
-            String[] values = text.split(",|:");
-            sum(values, result);
+            return sum(values);
         }
-        return result;
+        String[] values = text.split(",|:");
+        return sum(values);
     }
 
-    private static int sum(String[] values, int result) {
+    private static int sum(String[] values) {
+        int acc = 0;
         for (String value: values) {
             int number = Integer.parseInt(value);
             if (number < 0) {
                 throw new RuntimeException("Negative number is not accepted.");
             }
-            result += number;
+            acc += number;
         }
-        return result;
+        return acc;
     }
 }
