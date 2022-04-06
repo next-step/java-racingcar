b9640efc9b223e926dddbdf0c684d3a3b1fd2682
devgony <devgony@gmail.com>
2022-04-06 22:12:57 +0900

    Early return removes else

--- src/main/java/calculator/StringAddCalculatorTest.java
+++ src/main/java/calculator/StringAddCalculatorTest.java
@@ -5,19 +5,19 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculatorTest {
     public static int splitAndSum(String text) {
-        int result = 0;
         if (text == null || text.isBlank()) {
-            result = 0;
+            return 0;
+
+        }
+        int result = 0;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] values = m.group(2).split(customDelimiter);
+            sum(values, result);
         } else {
-            Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-            if (m.find()) {
-                String customDelimiter = m.group(1);
-                String[] values = m.group(2).split(customDelimiter);
-                sum(values, result);
-            } else {
-                String[] values = text.split(",|:");
-                sum(values, result);
-            }
+            String[] values = text.split(",|:");
+            sum(values, result);
         }
         return result;
     }
