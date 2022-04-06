df70b6e7db3d5c6684d10ae7ad571e3bc963ccf0
devyonghee <devyonghee@gmail.com>
2022-04-06 23:40:46 +0900

    feat(StringAddCalculator) : 문자열 덧셈 계산기 기능 추가

--- src/main/java/me/devyonghee/basic/StringAddCalculator.java
+++ src/main/java/me/devyonghee/basic/StringAddCalculator.java
@@ -1,28 +1,18 @@
 package me.devyonghee.basic;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 final class StringAddCalculator {
+
+    private static final StringSeparator SEPARATOR = StringSeparator.from(Pattern.compile("//(.)\n(.*)"));
 
     private StringAddCalculator() {
         throw new AssertionError();
     }
 
     static int splitAndSum(String target) {
-        return stringNumbers(target).positiveSum();
-    }
-
-    private static StringNumbers stringNumbers(String target) {
-        if (target == null || target.isBlank()) {
-            return StringNumbers.empty();
-        }
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(target);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] strings = m.group(2).split(customDelimiter);
-            return StringNumbers.from(strings);
-        }
-        return StringNumbers.from(target.split(",|:"));
+        return ZeroPositives.from(SEPARATOR.separate(target))
+                .sum()
+                .number();
     }
 }
