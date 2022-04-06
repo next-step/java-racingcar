84ba0ff3dbb12be55a98bed23097b44d60e0680e
jeongjaeeom <jeongjae.eom@gmail.com>
2022-04-06 22:39:59 +0900

    2단계 - 회사에서 강의 들으면서 1차 완료.

---
+++ src/main/java/camp/nextstep/StringAddCalculator.java
@@ -0,0 +1,51 @@
+package camp.nextstep;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+  기능 요구사항
+  쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환 (예: “” => 0, "1,2" => 3, "1,2,3" => 6, “1,2:3” => 6)
+  앞의 기본 구분자(쉼표, 콜론)외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다. 예를 들어 “//;\n1;2;3”과 같이 값을 입력할 경우 커스텀 구분자는 세미콜론(;)이며, 결과 값은 6이 반환되어야 한다.
+  문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.
+
+  프로그래밍 요구사항
+  메소드가 너무 많은 일을 하지 않도록 분리하기 위해 노력해 본다.
+*/
+public class StringAddCalculator {
+
+  public static final String DEFAULT_DELIMITER = ",|:";
+  public static final String CUSTOM_DELIMITER_REGEX = "//(.+)\n(.*)";
+
+  public static int splitAndSum(String input) {
+    if (input == null || input.length() < 1) {
+      return 0;
+    }
+
+    String[] strings = split(input);
+    return Arrays.stream(strings)
+        .mapToInt(StringAddCalculator::parseInt)
+        .sum();
+  }
+
+  private static int parseInt(String s) {
+    int number = Integer.parseInt(s);
+    if (number < 0) {
+      throw new RuntimeException("음수를 허용하지 않습니다.");
+    }
+    return number;
+  }
+
+  private static String[] split(String input) {
+    Pattern r = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+    Matcher matcher = r.matcher(input);
+    if (!matcher.find()) {
+      return input.split(DEFAULT_DELIMITER);
+    }
+
+    String delimiter = matcher.group(1);
+    return matcher.group(2).split(delimiter);
+  }
+
+}
