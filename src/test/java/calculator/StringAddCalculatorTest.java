9fc44743964b3235176182811695efcde62b5167
kang-jisu <rkdwltn0425@gmail.com>
2022-04-07 00:14:21 +0900

    test: DisplayName 수정

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -11,39 +11,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringAddCalculatorTest {
 
     @Test
-    @DisplayName("빈 문자열 또는 null 입력할 경우 0반환")
-    void blankAndNullTest() {
+    @DisplayName("빈 문자열 또는 null 입력할 경우 0 반환")
+    void nullAndEmptyTest() {
         assertThat(splitAndSum(null)).isEqualTo(0);
         assertThat(splitAndSum("")).isEqualTo(0);
     }
 
     @Test
-    @DisplayName("숫자 하나 문자열로 입력할 경우 해당 숫자 반환")
-    void singleInputTest() {
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자 반환")
+    void singleNumberTest() {
         assertThat(splitAndSum("1")).isEqualTo(1);
     }
 
     @Test
     @DisplayName("컴마 구분자일 경우 두 숫자의 합을 반환")
-    void commaTest() {
+    void commaDelimiterTest() {
         assertThat(splitAndSum("1,2")).isEqualTo(3);
     }
 
     @Test
-    @DisplayName("구분자를 컴마 이외에 콜론도 사용 가능")
-    void colonTest() {
+    @DisplayName("컴마와 콜론을 기본 구분자로 하여 합을 반환")
+    void defaultDelimiterTest() {
         assertThat(splitAndSum("1,2:3")).isEqualTo(6);
     }
 
     @Test
-    @DisplayName("//와\n사이에 커스텀 구분자 테스트")
+    @DisplayName("커스텀 구분자를 지정하여 합을 반환")
     void customDelimiterTest() {
         assertThat(splitAndSum("//;\n1;2;3")).isEqualTo(6);
     }
 
     @Test
     @DisplayName("음수를 전달할 경우 RuntimeException 예외 발생")
-    void negativeTest() {
+    void negativeIntegerFailTest() {
         Assertions.assertThrows(RuntimeException.class,
                 ()-> splitAndSum("-1,2,3"));
     }
