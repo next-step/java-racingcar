9ce85b75eb7dc52fe79a7f137cfc830e1572f81f
blossun <blossunn@gmail.com>
2022-04-06 21:42:05 +0900

    7. 테스트를 통과하기 위해 프로덕션 코드를 수정한다.

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,5 +1,14 @@
 public class StringAddCalculator {
-    public static int splitAndSum(final String o) {
-        return 0;
+    public static int splitAndSum(final String text) {
+        int result = 0;
+        if (text == null || text.isBlank()) {
+            result = 0;
+        } else {
+            String[] values = text.split(",");
+            for (String value : values) {
+                result += Integer.parseInt(value);
+            }
+        }
+        return result;
     }
 }
