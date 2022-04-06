2bad86d63c26bffb3787eab0f75ae7ac1a8dfebe
blossun <blossunn@gmail.com>
2022-04-06 21:28:35 +0900

    test: input과 output을 지정
    프로덕션코드(실제 동작하는 로직)가 아직 구현되지 않았기 때문에 처음에는 무조건 컴파일 에러가 난다.

---
+++ src/test/java/CTest.java
@@ -0,0 +1,14 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CTest {
+    //클래스명이 무엇인지 메서드명이 무엇인지 중요하지 않다. -> 나중에 설계 영역으로 넘어가도 좋다.
+    //input과 output을 잘 정하는 것이 중요하다.
+    //프로덕션코드(실제 동작하는 로직)가 아직 구현되지 않았기 때문에 처음에는 무조건 컴파일 에러가 난다.
+    @Test
+    void name() {
+        int result = C.c(null);
+        assertThat(result).isEqualTo(0);
+    }
+}
