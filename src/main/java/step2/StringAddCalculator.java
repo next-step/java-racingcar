86bd0f25f39bee8ac20eb91b16e1973f3a3eaa1c
전지형 <naoog@naver.com>
2022-04-06 22:06:15 +0900

    [NEXTSTEP] 2단계 요구사항 구현

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,35 @@
+package step2;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+  public static int spliteAndSum(String text) {
+    if (text == null || text.isBlank()) {
+      return 0;
+    }
+
+    Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+    if (m.find()) {
+      String customDelimeter = m.group(1);
+      String[] tokens = m.group(2).split(customDelimeter);
+      return sum(tokens);
+    }
+
+    String[] values = text.split(",|:");
+    return sum(values);
+  }
+
+  private static int sum(String[] values) {
+    int result = 0;
+    for (String value : values) {
+      int number = Integer.parseInt(value);;
+      if (number < 0) {
+        throw new RuntimeException("음수는 변환할 수 없습니다.");
+      }
+      result += number;
+    }
+    return result;
+  }
+}
---
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -0,0 +1,43 @@
+package step2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class StringAddCalculatorTest {
+
+  @Test
+  void 빈_문자열_공백문자_일때_0을_반환한다() {
+    assertThat(StringAddCalculator.spliteAndSum(null)).isEqualTo(0);
+    assertThat(StringAddCalculator.spliteAndSum("")).isEqualTo(0);
+  }
+
+  @Test
+  void 문자열_하나를_숫자로_반환한다() {
+    assertThat(StringAddCalculator.spliteAndSum("1")).isEqualTo(1);
+  }
+
+  @Test
+  void 쉼표_구분자를_구분하고_합친다() {
+    assertThat(StringAddCalculator.spliteAndSum("1,2")).isEqualTo(3);
+  }
+
+  @Test
+  void 콜론_구분자를_구분하고_합친다() {
+    assertThat(StringAddCalculator.spliteAndSum("1:2")).isEqualTo(3);
+  }
+
+  @Test
+  void 커스텀_구분자를_구분하고_합친다() {
+    assertThat(StringAddCalculator.spliteAndSum("//;\n1;2;3")).isEqualTo(6);
+  }
+
+  @Test
+  void 음수를_전달할경우_예외를_발생한다() {
+    assertThatThrownBy(() -> {
+      StringAddCalculator.spliteAndSum("-1,2,3s");
+    }).isInstanceOf(RuntimeException.class);
+  }
+
+}
