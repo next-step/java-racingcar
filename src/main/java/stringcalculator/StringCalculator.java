47945908aeb9b37b57d1d265af9a7c4073726084
bky4564 <bky4564@hunet.co.kr>
2022-04-07 16:50:20 +0900

    feat: 커스텀 구분자 이용 각 숫자의 합 반환

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,14 +1,35 @@
 package stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
+    public static final String COMMON_DELIMITER = "[,:]";
+    public static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+
     public int splitAndSum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        String[] values = input.split("[,:]");
+
         int result = 0;
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String[] values = matcher.group(2).split(customDelimiter);
+            result = getResult(values, result);
+        } else {
+            String[] values = input.split(COMMON_DELIMITER);
+            result = getResult(values, result);
+        }
+        return result;
+    }
+
+    private int getResult(String[] values, int result) {
         for (String value : values) {
-            result += Integer.parseInt(value);
+            int number = Integer.parseInt(value);
+            result += number;
         }
         return result;
     }
