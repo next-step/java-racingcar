13b6c54057699e0be515c94d847ddb7fa2d5f864
wlroh <wlroh.paul@gmail.com>
2022-04-07 22:15:01 +0900

    refactor: 한단계의 들여쓰기만 하기 위해 메소드 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,36 +6,49 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
 
     public static final String SEPARATOR = ",|:";
-    public static final String REGEX_PATTERN = "//(.)\n(.*)";
+    public static final String CUSTOM_SEPARATOR_REGEX_PATTERN = "//(.)\n(.*)";
 
     public static int splitAndSum(String text) {
-        if (text == null || text.isEmpty()) {
+        if (isNullOrBlank(text)) {
             return 0;
         }
 
-        int result = 0;
-        Matcher matcher = Pattern.compile(REGEX_PATTERN).matcher(text);
+        Matcher matcher = Pattern.compile(CUSTOM_SEPARATOR_REGEX_PATTERN).matcher(text);
         if (matcher.find()) {
             String customSeparator = matcher.group(1);
             String[] values = matcher.group(2).split(customSeparator);
-            for (String value : values) {
-                int number = Integer.parseInt(value);
-                if (number < 0) {
-                    throw new RuntimeException();
-                }
-                result += number;
-            }
-            return result;
+            return sum(toInts(values));
         }
 
         String[] values = text.split(SEPARATOR);
-        for (String value : values) {
-            int number = Integer.parseInt(value);
-            if (number < 0) {
-                throw new RuntimeException();
-            }
-            result += number;
+        return sum(toInts(values));
+    }
+
+    private static boolean isNullOrBlank(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private static int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = toInt(values[i]);
         }
-        return result;
+        return numbers;
+    }
+
+    private static int toInt(String value) {
+        int number = Integer.parseInt(value);
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
+    private static int sum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
