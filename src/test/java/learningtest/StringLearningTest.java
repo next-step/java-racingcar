d7fc6444cb718408a63073fa48b7019f4cecf19f
guswns1659 <guswns1659@gmail.com>
2022-04-08 07:15:12 +0900

    1week-step1 refactor: 명확한 변수명으로 변경

--- src/test/java/learningtest/StringLearningTest.java
+++ src/test/java/learningtest/StringLearningTest.java
@@ -11,24 +11,21 @@ public class StringLearningTest {
     @DisplayName("string을 , 기준으로 split했을 때 성공적으로 나눠진다.")
     @Test
     void split_StringNumberGiven_SuccessSplit() {
-        // given
-        String number = "1,2";
-
         // when
-        String[] splittedNumber = number.split(",");
+        String[] number = "1,2".split(",");
 
         // then
-        assertThat(splittedNumber).contains("1", "2");
+        assertThat(number).contains("1", "2");
     }
 
     @DisplayName("하나의 문자인 String을 split했을 때도 배열이 반환된다.")
     @Test
     void split_StringGiven_ShouldReturnArray() {
         // given, when
-        String[] split = "1".split(",");
+        String[] strings = "1".split(",");
 
         // then
-        assertThat(split).containsExactly("1");
+        assertThat(strings).containsExactly("1");
     }
 
     @DisplayName("String의 substring()을 배운다.")
