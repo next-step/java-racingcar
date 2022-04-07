25f937f34675deb25e0bbcfb2cbca98656a3f59d
guswns1659 <guswns1659@gmail.com>
2022-04-08 06:43:31 +0900

    1week-step2 feat: 커스텀 구분자가 포함되도 합계를 정상 출력하는 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,13 +1,28 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
-    public static int splitAndSum(String value) {
-        if (value == null || value.isBlank()) {
+    public static int splitAndSum(String text) {
+        if (text == null || text.isBlank()) {
             return 0;
         }
+
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String[] splitedByDelimiter = matcher.group(2).split(customDelimiter);
+            return stringArraySum(splitedByDelimiter);
+        }
+
+        String[] splitedString = text.split(",|:");
+        return stringArraySum(splitedString);
+    }
+
+    private static int stringArraySum(String[] splitedString) {
         int result = 0;
-        String[] splitedString = value.split(",|:");
         for (String each : splitedString) {
             result += Integer.parseInt(each);
         }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -7,16 +7,21 @@ import org.junit.jupiter.api.Test;
 public class StringCalculatorTest {
 
     @Test
-    void splitAndSum_NullAndEmptyStringGiven_ReturnZero() {
+    void nullAndEmptyStringGiven_ReturnZero() {
         assertThat(StringAddCalculator.splitAndSum(null)).isEqualTo(0);
         assertThat(StringAddCalculator.splitAndSum("")).isEqualTo(0);
     }
 
     @Test
-    void splitAndSum_StringWithColonOrCommaGiven_ReturnSum() {
+    void stringWithColonOrCommaGiven_ReturnSum() {
         assertThat(StringAddCalculator.splitAndSum("1,2")).isEqualTo(3);
         assertThat(StringAddCalculator.splitAndSum("1,2,3")).isEqualTo(6);
         assertThat(StringAddCalculator.splitAndSum("1,2:3")).isEqualTo(6);
     }
 
+    @Test
+    void customDelimiterGiven_ReturnSum() {
+        assertThat(StringAddCalculator.splitAndSum("//;\n1;2;3")).isEqualTo(6);
+        assertThat(StringAddCalculator.splitAndSum("//:\n1:2:3")).isEqualTo(6);
+    }
 }
