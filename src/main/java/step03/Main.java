ccd4dfc68bc47dde2e9b48eb3286b469ab3f9c95
cooper <pbg0205@naver.com>
2022-04-07 20:40:55 +0900

    feat: 레이싱카 어플리케이션 코드 작성

---
+++ src/main/java/step03/Main.java
@@ -0,0 +1,13 @@
+package step03;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConsoleReader consoleReader = new ConsoleReader();
+        OutputView outputView = new OutputView();
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        RacingCarApplication application = new RacingCarApplication(consoleReader, outputView, randomNumberGenerator);
+        application.run();
+    }
+}
