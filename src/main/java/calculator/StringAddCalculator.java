fc0602a8d9febdd7d9b92752f52477baf721c387
rumor1993 <rumor1993@naver.com>
2022-04-08 01:34:39 +0900

    2단계 문자열 덧셈 계산기

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,20 +1,56 @@
 package calculator;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
     public static int splitAndSum(String value) {
-        int result = 0;
-        if (value == null || value.isBlank()) {
+        if (isaBlank(value)) {
             return 0;
-        } else {
-            String[] numbers = value.split(",");
-            for (String number : numbers) {
-                System.out.println("number = " + number);
-                result += Integer.parseInt(number);
+        }
+
+        String[] numbers = getText(value).split(getSeparator(value));
+        return sum(numbers);
+    }
+
+    private static String getText(String value) {
+        Matcher matcher = getMatcher(value);
+
+        if (!matcher.find()) {
+            return value;
+        }
+
+        return matcher.group(2);
+    }
+
+    private static String getSeparator(String value) {
+        Matcher matcher = getMatcher(value);
+
+        if (!matcher.find()) {
+            return value;
+        }
+
+        return matcher.group(1);
+    }
+
+    private static Matcher getMatcher(String value) {
+        return Pattern.compile("//(.)\n(.*)").matcher(value);
+    }
+
+    private static int sum(String[] numbers) {
+        int result = 0;
+
+        for (String number : numbers) {
+            if (Integer.parseInt(number) < 0) {
+                throw new RuntimeException();
             }
+            result += Integer.parseInt(number);
         }
         return result;
+    }
+
+    private static boolean isaBlank(String value) {
+        return value == null || value.isBlank();
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,6 +3,8 @@ package calculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class StringAddCalculatorTest {
     @Test
     void 빈_문자열이거나_Null일_경우() {
@@ -14,5 +16,23 @@ public class StringAddCalculatorTest {
     void 빈_문자_Null이_아닌_경우() {
         Assertions.assertThat(StringAddCalculator.splitAndSum("1,2")).isEqualTo(3);
         Assertions.assertThat(StringAddCalculator.splitAndSum("1,2,3")).isEqualTo(6);
+    }
+
+    @Test
+    void 빈_문자나_Null이_아닌_경우() {
+        Assertions.assertThat(StringAddCalculator.splitAndSum("//;\n1;2;3")).isEqualTo(6);
+    }
+
+    @Test
+    void 숫자_이외의_값_또는_음수를_전달하는_경우_예외를_던진다() {
+        Assertions.assertThatThrownBy(() -> {
+            StringAddCalculator.splitAndSum("-1,2,3");
+        }).isInstanceOf(RuntimeException.class);
+
+        Assertions.assertThatThrownBy(() -> {
+            StringAddCalculator.splitAndSum("k,2,3");
+        }).isInstanceOf(RuntimeException.class);
+
+
     }
 }
