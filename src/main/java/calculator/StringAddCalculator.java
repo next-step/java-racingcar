a3e97cdc2c27223106f7900b0592ba90ec3a4e6d
redkyu <hongstar2311@gmail.com>
2022-04-07 22:37:48 +0900

    문자열 계산기 리팩토링

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,41 +6,66 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
 
     static final String DEFAULT_DELIMITER = ",|:";
-    static final String CUSTOM_DELIMITER = "//(.)\\n(.*)";
+    static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
 
     public static int splitAndSum(String text) {
         int result = 0;
 
-        if(text == null || text.isBlank()) {
-            result = 0;
-        } else {
-            Matcher m = Pattern.compile(CUSTOM_DELIMITER).matcher(text);
-
-            if(m.find()) {
-                String customDelimiter = m.group(1);
-                String[] values = m.group(2).split(customDelimiter);
-
-                for (String value : values) {
-                    int number = Integer.parseInt(value);
-                    if(number < 0) {
-                        throw new RuntimeException();
-                    }
-                    result += number;
-                }
-
-            } else {
-                String[] values = text.split(DEFAULT_DELIMITER);
-
-                for (String value : values) {
-                    int number = Integer.parseInt(value);
-                    if(number < 0) {
-                        throw new RuntimeException();
-                    }
-                    result += number;
-                }
-            }
+        if (checkNullOrEmpty(text)) {
+            return result;
         }
 
-        return result;
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER).matcher(text);
+
+        if (m.find()) {
+            String[] values = splitCustomDelimiter(m);
+
+            return sum(toInts(values));
+        }
+
+        String[] values = split(text, DEFAULT_DELIMITER);
+
+        return sum(toInts(values));
+    }
+
+    private static String[] splitCustomDelimiter(Matcher m) {
+        String customDelimiter = m.group(1);
+        return m.group(2).split(customDelimiter);
+    }
+
+    private static String[] split(String text, String delimiter) {
+        return text.split(delimiter);
+    }
+
+    private static boolean checkNullOrEmpty(String text) {
+        return text == null || text.isBlank();
+    }
+
+    private static int[] toInts(String[] strings) {
+        int[] values = new int[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            values[i] = toInt(strings[i]);
+        }
+
+        return values;
+    }
+
+    private static int sum(int[] values) {
+        int sum = 0;
+
+        for (int value : values) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    private static int toInt(String value) {
+        int number = Integer.parseInt(value);
+
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
     }
 }
