d60573c45ef016d3dab1d635fa3661c03c50eb11
bluewow <dogseen@gmail.com>
2022-04-07 23:40:21 +0900

    refactor : 1단계 - feedback 반영

--- src/test/java/study/step1/SetCollectionTest.java
+++ src/test/java/study/step1/SetCollectionTest.java
@@ -1,4 +1,4 @@
-package study;
+package study.step1;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,20 +28,27 @@ public class SetCollectionTest {
     @DisplayName("size() 를 이용하여 Set 의 크기 확인")
     @Test
     void size() {
-        assertThat(numbers.size()).isEqualTo(3);
+        assertThat(numbers).hasSize(3);
     }
 
-    @DisplayName("contains() 를 이용하여 1, 2, 3 확인")
+    @DisplayName("contains 함수에 대한 학습테스트 - 성공케이스")
     @ParameterizedTest
     @ValueSource(ints = {1, 2 ,3})
-    void contains(int input) {
+    void containsSuccessCase(int input) {
         assertThat(numbers.contains(input)).isTrue();
     }
 
-    @DisplayName("contains() 를 이용하여 1, 2, 3, 4, 5확인")
+    @DisplayName("contains 함수에 대한 학습테스트 - 실패케이스")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5})
+    void parameterizedValueTest(int input) {
+        assertThat(numbers.contains(input)).isFalse();
+    }
+
+    @DisplayName("CsvSource 에 대한 학습 테스트")
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    void contains(int input, boolean expected) {
+    void parameterizedCsvTest(int input, boolean expected) {
         assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
--- src/test/java/study/step1/StringTest.java
+++ src/test/java/study/step1/StringTest.java
@@ -1,4 +1,4 @@
-package study;
+package study.step1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,39 +7,29 @@ import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("String 클래스에 대한 학습 테스트")
 public class StringTest {
-    @DisplayName("1, 2를 ,로 split 했을 때 [1, 2] 반환")
+    @DisplayName("split 함수에 대한 학습 테스트")
     @Test
-    void split_ex1() {
-        String[] result = "1,2".split(",");
-        assertThat(result).containsExactly("1", "2");
+    void splitMultiCase() {
+        assertThat("1,2".split(",")).containsExactly("1", "2");
+        assertThat("1".split(",")).containsExactly("1");
     }
 
-    @DisplayName("1을 ,로 split 했을 때 [1] 반환")
-    @Test
-    void split_ex2() {
-        String[] result = "1".split(",");
-        assertThat(result).containsExactly("1");
-    }
 
-    @DisplayName("(1, 2)을 ,로 split 했을 때 '1,2' 반환")
+    @DisplayName("substring 함수에 대한 학습 테스트")
     @Test
     void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
     }
 
-    @DisplayName("charAt() 메소드를 활용해 StringIndexOutOfBoundsException 을 발생시켜라 Type1")
+    @DisplayName("assertj exception 에 대한 학습 테스트")
     @Test
-    void exception_ex1() {
+    void exceptionMultiCase() {
         assertThatThrownBy(() -> {
             char c = "abc".charAt(4);
         }).isInstanceOf(IndexOutOfBoundsException.class)
                 .hasMessageContaining("String index out of range: 4");
-    }
 
-    @DisplayName("charAt() 메소드를 활용해 StringIndexOutOfBoundsException 을 발생시켜라 Type2")
-    @Test
-    void exception_ex2() {
         assertThatExceptionOfType(IndexOutOfBoundsException.class)
                 .isThrownBy(() -> {
                     char c = "abc".charAt(4);
