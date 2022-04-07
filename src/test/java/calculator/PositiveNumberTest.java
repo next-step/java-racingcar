96c722f61b327a2256e680179bbcb2d6c9fdcd8d
jaden <jonghaha7@gmail.com>
2022-04-07 13:29:54 +0900

    <feat> PositiveNumberTest

---
+++ src/test/java/calculator/PositiveNumberTest.java
@@ -0,0 +1,30 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PositiveNumberTest {
+
+    private PositiveNumber positiveNumber1;
+    private PositiveNumber positiveNumber2;
+
+    @BeforeEach
+    void setUp(){
+        positiveNumber1 = new PositiveNumber(1);
+        positiveNumber2 = new PositiveNumber("2");
+    }
+
+    @Test
+    void getValue(){
+        assertThat(positiveNumber1.getValue()).isEqualTo(1);
+        assertThat(positiveNumber2.getValue()).isEqualTo(2);
+    }
+
+    @Test
+    void plus(){
+        assertThat(positiveNumber1.plus(positiveNumber2).getValue()).isEqualTo(3);
+    }
+
+}
