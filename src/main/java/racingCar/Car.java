13dd88a7ff88c091fbbbdc0d01280bb7083e8327
younghun <asqwklop12@naver.com>
2022-04-09 02:05:34 +0900

    refactor 매직 넘버 정리 및 메소드명 수정

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -5,18 +5,21 @@ import java.util.Random;
 public class Car {
 
   private int randomValue;
+  private final static int MIN_VALUE = 3;
+  private final static String STEP = "-";
+  private final static int RANDOM_BOUND_VALUE = 10;
 
   public String move() {
-    setRandomValue();
-    if (randomValue <= 3) {
+    initRandomValue();
+    if (randomValue <= MIN_VALUE) {
       return null;
     }
-    return "-";
+    return STEP;
   }
 
-  public void setRandomValue() {
+  public void initRandomValue() {
     Random random = new Random();
-    this.randomValue = random.nextInt(10);
+    this.randomValue = random.nextInt(RANDOM_BOUND_VALUE);
   }
 
   public int getRandomValue() {
