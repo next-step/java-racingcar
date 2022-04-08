7e8bed68c1977d3ce98eec0b237af0f2693f3564
SoonMyeong_notebook <soon_myeong@naver.com>
2022-04-08 20:35:04 +0900

    2단계 - 문자열 덧셈 계산기 (수정#2)

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,43 +6,45 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringAddCalculator {
-    private static final Pattern PATTERN = Pattern.compile("^//(.)\\n(.*)$");
-    private static final String DELIMITER = "[,:]";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("^//(.)\\n(.*)$");
+    private static final String COMMA_OR_COLON_DELIMITER = "[,:]";
+    private static final int NULL_AND_BLANK_VALUE = 0;
+    private static final int FOR_CHECK_NEGATIVE_NUMBER = 0;
     private static final int MATCHER_CUSTOM_DELIMITER = 1;
     private static final int MATCHER_TEXT = 2;
 
     public static int splitAndSum(String text) {
-        if(text == null || text.isBlank()) {
-            return 0;
+        if(checkNullAndBlank(text)) {
+            return NULL_AND_BLANK_VALUE;
         }
 
-        Matcher matcher = PATTERN.matcher(text);
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
         if(matcher.find()) {
-            return sum(toIntegerList(splitCustomDelimiter(
-                    matcher.group(MATCHER_CUSTOM_DELIMITER),matcher.group(MATCHER_TEXT))));
+            String matcherGroupDelimiter = matcher.group(MATCHER_CUSTOM_DELIMITER);
+            return sum(toIntegerList(splitDelimiter(matcherGroupDelimiter, matcher.group(MATCHER_TEXT))));
         }
 
-        return sum(toIntegerList(splitDelimiter(text)));
+        return sum(toIntegerList(splitDelimiter(COMMA_OR_COLON_DELIMITER,text)));
     }
 
-    private static List<String> splitCustomDelimiter(String customDelimiter, String text) {
+    private static boolean checkNullAndBlank(String text) {
+        return text == null || text.isBlank();
+    }
+
+    private static List<String> splitDelimiter(String customDelimiter, String text) {
         return List.of(text.split(customDelimiter));
-    }
-
-    private static List<String> splitDelimiter(String text) {
-        return List.of(text.split(DELIMITER));
     }
 
     private static List<Integer> toIntegerList(List<String> list) {
         return list.stream()
-                .map(StringAddCalculator::stringToInteger)
+                .map(StringAddCalculator::toInteger)
                 .collect(Collectors.toList());
     }
 
-    private static Integer stringToInteger(String text) {
+    private static Integer toInteger(String text) {
         try {
             int number = Integer.parseInt(text);
-            if(number < 0) {
+            if(number < FOR_CHECK_NEGATIVE_NUMBER) {
                 throw new IllegalArgumentException("음수는 불가능 합니다.");
             }
             return number;
@@ -52,6 +54,8 @@ public class StringAddCalculator {
     }
 
     private static int sum(List<Integer> numbers) {
-        return numbers.stream().mapToInt(integer->integer).sum();
+        return numbers.stream()
+                .mapToInt(integer->integer)
+                .sum();
     }
 }
