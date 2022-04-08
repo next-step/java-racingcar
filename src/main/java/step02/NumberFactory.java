4a31b94b5f632e5aaef23d27a4472358fb0cef19
cooper <pbg0205@naver.com>
2022-04-08 18:48:44 +0900

    refactor: NumberFactory 불필요 메서드 제거

--- src/main/java/step02/NumberFactory.java
+++ src/main/java/step02/NumberFactory.java
@@ -1,36 +1,17 @@
 package step02;
 
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-public enum NumberFactory {
-
-    POSITIVE(PositiveNumber::new),
-    ZERO(number -> new Zero());
-
-    private final Function<Integer, Number> numberFunction;
-
-    NumberFactory(Function<Integer, Number> numberFunction) {
-        this.numberFunction = numberFunction;
-    }
+public class NumberFactory {
 
     public static Number from(int number) {
         if (number > 0) {
-            return POSITIVE.numberFunction.apply(number);
+            return new PositiveNumber(number);
         }
 
         if (number == 0) {
-            return ZERO.numberFunction.apply(number);
+            return new Zero();
         }
 
         throw new IllegalArgumentException("음수 값을 포함하고 있습니다.");
-    }
-
-    public static List<Number> of(List<Integer> numbers) {
-        return numbers.stream()
-                .map(NumberFactory::from)
-                .collect(Collectors.toList());
     }
 
 }
--- src/test/java/step02/NumberFactoryTest.java
+++ src/test/java/step02/NumberFactoryTest.java
@@ -2,15 +2,9 @@ package step02;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class NumberFactoryTest {
 
@@ -31,29 +25,6 @@ class NumberFactoryTest {
     void fromExceptionTest() {
         //when, then
         assertThatIllegalArgumentException().isThrownBy(() -> NumberFactory.from(-1));
-    }
-
-    @ParameterizedTest
-    @DisplayName("정상적인 객체 생성을 확인한다")
-    @MethodSource("numberProvider")
-    void ofTest(List<Integer> sampleNumbers, List<Number> expectedNumbers) {
-        //when
-        List<Number> numbers = NumberFactory.of(sampleNumbers);
-
-        //then
-        assertThat(numbers.containsAll(expectedNumbers)).isTrue();
-    }
-
-    static Stream<Arguments> numberProvider() {
-        return Stream.of(
-                arguments(
-                        List.of(0, 1, 2),
-                        List.of(new Zero(), new PositiveNumber(1), new PositiveNumber(2))
-                ), arguments(
-                        List.of(1, 2, 3),
-                        List.of(new PositiveNumber(1), new PositiveNumber(2), new PositiveNumber(3))
-                )
-        );
     }
 
 }
