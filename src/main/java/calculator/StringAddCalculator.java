64ba3c78ef7bc2c891efa12ac7a6f8be14776b41
hihisososo <hihisososo@naver.com>
2022-04-08 14:12:08 +0900

    2단계 - 문자열 덧셈 계산기 (#3141)
    * step1 코드리뷰 코멘트 참고하여 수정
    
    * 2단계 - 문자열 덧셈 계산기
    
    * chore: 사용하지 않는 import 제거
    
    * style: 구글 코드 포맷 스타일 설정 및 소스 포매팅 적용
    
    * refactor: 문자열 계산기 메서드 내부 호출 순서 변경
    
    * refactor: Pattern.compile() 결과 매번 생성하지 않도록 static 변수로 변경
    
    * chore: delimiter 오타 수정
    
    * refactor: SetTest 파라미터의  Wrapper Class -> primitive type 으로 변경
    
    * test: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환하는 테스트 추가
    
    * test: AssertJ, jupiter 검증 -> AssertJ 로 사용하도록 변경
    
    * chore: 단계별로 자바 소스  패키지 이동

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,80 @@
+package calculator;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StringAddCalculator {
+
+  private static final String OR = "|";
+  private static final String BASE_SPLIT_REGEX = ",|:";
+  private static final String CUSTOM_DELIMITER_FIND_REGEX = "^//(.*)\n(.*)";
+  private static final Pattern customDelimiterPattern = Pattern.compile(
+      CUSTOM_DELIMITER_FIND_REGEX);
+
+  public Integer calcSum(String text) {
+    if (isEmptyOrNull(text)) {
+      return 0;
+    }
+
+    if (containsCustomDelimiter(text)) {
+      return customDelimiterSum(text);
+    }
+
+    return normalDelimiterSum(text);
+  }
+
+  private boolean isEmptyOrNull(String text) {
+    return text == null || text.isEmpty();
+  }
+
+  private boolean containsCustomDelimiter(String text) {
+    return text.matches(CUSTOM_DELIMITER_FIND_REGEX);
+  }
+
+  private Integer customDelimiterSum(String text) {
+    Matcher customDelimiterMatch = customDelimiterPattern.matcher(text);
+    customDelimiterMatch.find();
+
+    String customDelimiter = customDelimiterMatch.group(1);
+    String delimiterRemovedText = customDelimiterMatch.group(2);
+
+    List<Integer> splitted = split(BASE_SPLIT_REGEX + OR + customDelimiter, delimiterRemovedText);
+    checkPositive(splitted);
+    return sum(splitted);
+  }
+
+  private List<Integer> split(String delimiter, String text) {
+    try {
+      return Stream.of(text.split(delimiter))
+          .mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+    } catch (Exception e) {
+      throw new RuntimeException("input text must contains only delimiter or number");
+    }
+  }
+
+  private void checkPositive(List<Integer> nums) {
+    for (Integer num : nums) {
+      checkPositive(num);
+    }
+  }
+
+  private void checkPositive(Integer num) {
+    if (num < 0) {
+      throw new RuntimeException("number is negative");
+    }
+  }
+
+  private Integer sum(List<Integer> splittedNumber) {
+    return splittedNumber.stream().mapToInt(i -> i).sum();
+  }
+
+  private Integer normalDelimiterSum(String text) {
+    List<Integer> splitted = split(BASE_SPLIT_REGEX, text);
+    checkPositive(splitted);
+    return sum(splitted);
+  }
+
+}
