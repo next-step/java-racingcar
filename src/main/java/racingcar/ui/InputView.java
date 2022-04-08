189e26091d092ced4732573a507ca2d613a15008
imcool2551 <imcool2551@gmail.com>
2022-04-08 22:10:23 +0900

    refactor: InputView
    InputView의 Scanner 인스턴스를 상수화했습니다.

--- src/main/java/racingcar/ui/InputView.java
+++ src/main/java/racingcar/ui/InputView.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {
         throw new AssertionError();
@@ -12,11 +12,11 @@ public class InputView {
 
     public static int promptCarCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 
     public static int promptRounds() {
         System.out.println("시도할 횟수는 몇 회 인가요?");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 }
