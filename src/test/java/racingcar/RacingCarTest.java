abc4a48d08a68ab1653d8910c7b4ffe562cafd7b
DahamLeee <dlek1011@gmail.com>
2022-04-08 10:39:18 +0900

    refactor remove unnecessary test resource

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -17,31 +17,9 @@ public class RacingCarTest {
         racingCar = new RacingCar();
     }
 
-    @DisplayName("경주용 차 난수가 4이상일 경우 전진")
+    @DisplayName("CsvSource 어노테이션 사용하여 움직임 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    void moveRacingCarIfRandomNumberIsFourOrMore(int randomNumber) {
-        racingCar.moveOrStop(randomNumber);
-
-        assertThat(racingCar)
-                .extracting("distance")
-                .isEqualTo(1);
-    }
-
-    @DisplayName("경주용 차 난수가 3이하일 경우 멈춤")
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
-    void stopRacingCarIfRandomNumberIsThreeOrLess(int randomNumber) {
-        racingCar.moveOrStop(randomNumber);
-
-        assertThat(racingCar)
-                .extracting("distance")
-                .isEqualTo(0);
-    }
-
-    @DisplayName("4이상, 3이하 CsvSource 어노테이션 사용하여 움직임 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"1:0", "2:0", "3:0", "4:1", "5:1", "6:1", "7:1", "8:1", "9:1"}, delimiter = ':')
+    @CsvSource(value = {"3:0", "4:1"}, delimiter = ':')
     void moveOrStopByRandomGenerator(int randomNumber, int distance) {
         racingCar.moveOrStop(randomNumber);
 
