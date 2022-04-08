5bd5f2f096ce0b83ae28b24688e864fb27db90bd
Anthologia <pyoungjy@gmail.com>
2022-04-08 16:44:26 +0900

    refactor: StringAddCalculator 클래스 리팩토링
    - StringAddCalculator 클래스의 인스턴스 생성자 제한
    - 매직 넘버 추출
    - 인스턴스 생성 비용이 비싼 Pattern 객체 정적화
    - 기능들의 메서드 분리
    - 오타 수정

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -4,22 +4,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private static String DEFAULT_REGEX_DELIMITER = ",|:";
-    private static String CUSTOM_REGEX_DELIMITER = "//(.)\n(.*)";
+    private static final int DEFAULT_NUMBER = 0;
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int CUSTOM_TEXT_INDEX = 2;
+    private static final String DEFAULT_REGEX_DELIMITER = ",|:";
+    private static final String CUSTOM_REGEX_DELIMITER = "//(.)\n(.*)";
+    private static final Pattern pattern = Pattern.compile(CUSTOM_REGEX_DELIMITER);
+
+    private StringAddCalculator() {
+        throw new AssertionError(String.format("객체를 생성할 수 없는 클래스(%s)입니다.\n", this.getClass()));
+    }
 
     public static int splitAndSum(String text) {
         if (isBlank(text)) {
-            return 0;
+            return DEFAULT_NUMBER;
         }
 
-        Matcher m = Pattern.compile(CUSTOM_REGEX_DELIMITER).matcher(text);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] splits = m.group(2).split(customDelimiter);
+        Matcher matcher = getMatcher(text);
+        if (hasCustomDelimiter(matcher)) {
+            String[] splits = split(getCustomText(matcher), getCustomDelimiter(matcher));
             return sum(toPositives(splits));
         }
 
-        String[] splits = text.split(DEFAULT_REGEX_DELIMITER);
+        String[] splits = split(text, DEFAULT_REGEX_DELIMITER);
         return sum(toPositives(splits));
     }
 
@@ -27,18 +34,38 @@ public class StringAddCalculator {
         return text == null || text.isEmpty();
     }
 
+    private static Matcher getMatcher(String text) {
+        return pattern.matcher(text);
+    }
+
+    private static boolean hasCustomDelimiter(Matcher matcher) {
+        return matcher.find();
+    }
+
+    private static String getCustomText (Matcher matcher) {
+        return matcher.group(CUSTOM_TEXT_INDEX);
+    }
+
+    private static String getCustomDelimiter(Matcher matcher) {
+        return matcher.group(CUSTOM_DELIMITER_INDEX);
+    }
+
+    private static String[] split(String text, String delimiter) {
+        return text.split(delimiter);
+    }
+
     private static Positive[] toPositives(String[] values) {
-        Positive[] arr = new Positive[values.length];
+        Positive[] positives = new Positive[values.length];
         for (int i = 0; i < values.length; i++) {
-            arr[i] = new Positive(values[i]);
+            positives[i] = new Positive(values[i]);
         }
-        return arr;
+        return positives;
     }
 
     private static int sum(Positive[] positives) {
         int sum = 0;
-        for (Positive positive : positives) {;
-            sum += positive.value;
+        for (Positive positive : positives) {
+            sum = positive.add(sum);
         }
         return sum;
     }
