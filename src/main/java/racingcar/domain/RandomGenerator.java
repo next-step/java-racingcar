ac1b8fe666c00cd10251f68201b158d8a53aafe5
imcool2551 <imcool2551@gmail.com>
2022-04-08 22:55:13 +0900

    refactor: RandomGenerator 인스턴스화 방지

--- src/main/java/racingcar/domain/RandomGenerator.java
+++ src/main/java/racingcar/domain/RandomGenerator.java
@@ -7,6 +7,10 @@ public class RandomGenerator {
     private static final Random RANDOM_GENERATOR = new Random();
     private static final int RANDOM_UPPER_BOUND = 10;
 
+    private RandomGenerator() {
+        throw new AssertionError();
+    }
+
     public static int generateRandom() {
         return RANDOM_GENERATOR.nextInt(RANDOM_UPPER_BOUND);
     }
