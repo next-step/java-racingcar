680c791137404478a2a70c5ac0ef898d3dc802a5
DahamLeee <dlek1011@gmail.com>
2022-04-08 12:11:20 +0900

    feat racingcar can have name

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,9 +3,22 @@ package racingcar;
 public class RacingCar {
 
     private static final int MOVABLE_BOUNDARY_NUMBER = 4;
+    private static final int MAX_CAR_NAME_SIZE = 5;
+    private String carName;
     private int distance;
 
     public RacingCar() { }
+
+    public RacingCar(String carName) {
+        if (invalidCarName(carName)) {
+            carName = carName.substring(0, MAX_CAR_NAME_SIZE);
+        }
+        this.carName = carName;
+    }
+
+    private boolean invalidCarName(String carName) {
+        return carName.length() > MAX_CAR_NAME_SIZE;
+    }
 
     public void moveOrStop(int randomNumber) {
         if (randomNumber >= MOVABLE_BOUNDARY_NUMBER) {
