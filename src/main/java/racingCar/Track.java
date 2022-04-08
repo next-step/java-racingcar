31851c73eb5d9c47caa14bda9bc50bbad811d073
younghun <asqwklop12@naver.com>
2022-04-09 02:20:57 +0900

    test 몇 대의 차량과 경주를 할지 테스트한다.

--- src/main/java/racingCar/Track.java
+++ src/main/java/racingCar/Track.java
@@ -1,2 +1,17 @@
-package racingCar;public class Track {
+package racingCar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Track {
+
+
+
+  public List<Car> vehiclesCount(int carCount) {
+    List<Car> cars = new ArrayList<>();
+    for (int i = 0; i < carCount; i++) {
+      cars.add(new Car());
+    }
+    return cars;
+  }
 }
--- src/test/java/racingCar/TrackTest.java
+++ src/test/java/racingCar/TrackTest.java
@@ -1,4 +1,25 @@
+package racingCar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class TrackTest {
-  
+
+  private Track track;
+  @BeforeEach
+  void init() {
+    track = new Track();
+  }
+
+  @Test
+  @DisplayName("몇 대의 차량과 경주를 할지 테스트한다.")
+  void countTest() {
+    List<Car> cars = track.vehiclesCount(3);
+    assertThat(cars.size()).isEqualTo(3);
+  }
 }
