4d5d7fef4195ec548f5e6b2796c4ede760ff242b
hjeom <nara1030@naver.com>
2022-04-09 18:36:57 +0900

    feat] 자동차는 전진하거나 정지할 수 있다.

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,4 +1,16 @@
 package racing;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racing.domain.Car;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarTest {
+    @ParameterizedTest
+    @CsvSource({"0,0", "1,0", "2,0", "3,0", "4,1", "5,1", "6,1", "7,1", "8,1", "9,1"})
+    void 차가_전진_혹은_정지(int input, int expected) {
+        Car car = new Car();
+        assertThat(car.moveForwardOrStop(input)).isEqualTo(expected);
+    }
 }
