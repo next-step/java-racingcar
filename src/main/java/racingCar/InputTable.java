611d98e284bc1507935d63cb7787fcbaaef4c746
younghun <asqwklop12@naver.com>
2022-04-09 12:07:33 +0900

    bug 타이핑 버그 수정

--- src/main/java/racingCar/InputTable.java
+++ src/main/java/racingCar/InputTable.java
@@ -7,7 +7,7 @@ public final class InputTable {
   final static Track TRACK = new Track();
   private final static Scanner SCANNER = new Scanner(System.in);
   private final static String HOW_MANY_CARS = "자동차 대수는 몇 대 인가요?";
-  private final static String HOW_MANY_GAMES = "자동차 대수는 몇 대 인가요?";
+  private final static String HOW_MANY_GAMES = "시도할 회수는 몇 회 인가요?";
 
   private InputTable() {
   }
