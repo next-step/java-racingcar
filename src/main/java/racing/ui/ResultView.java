d789bcd4ad75ba7cc806a0fdd7b02d0cfc4afcd4
hjeom <nara1030@naver.com>
2022-04-09 18:38:51 +0900

    feat] 자동차의 상태를 화면에 출력한다(출력 시점 제약 없음).

--- src/main/java/racing/ui/ResultView.java
+++ src/main/java/racing/ui/ResultView.java
@@ -1,4 +1,11 @@
 package racing.ui;
 
+import racing.domain.Competition;
+
 public class ResultView {
+    public static void print(final Competition competition) {
+        System.out.println("실행 결과");
+
+        competition.progressCompetition();
+    }
 }
--- src/test/java/racing/CompetitionTest.java
+++ src/test/java/racing/CompetitionTest.java
@@ -1,4 +1,15 @@
 package racing;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import racing.domain.Competition;
+import racing.ui.ResultView;
+
 public class CompetitionTest {
+    @ParameterizedTest
+    @CsvSource({"1,1", "1,2", "2,3", "3,5"})
+    void print(int unit, int count) {
+        Competition competition = new Competition(unit, count);
+        ResultView.print(competition);
+    }
 }
