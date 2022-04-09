33dcce97e3dd92a6993be593a410c100295af9d1
younghun <asqwklop12@naver.com>
2022-04-09 12:00:17 +0900

    style input output 상수 선언

--- src/main/java/racingCar/InputTable.java
+++ src/main/java/racingCar/InputTable.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public final class InputTable {
   final static Track TRACK = new Track();
   private final static Scanner SCANNER = new Scanner(System.in);
+  private final static String HOW_MANY_CARS = "자동차 대수는 몇 대 인가요?";
+  private final static String HOW_MANY_GAMES = "자동차 대수는 몇 대 인가요?";
   private InputTable() {
   }
 
   public static void howManyCar() {
-    System.out.println("자동차 대수는 몇 대 인가요? ");
+    System.out.println(HOW_MANY_CARS);
     TRACK.addCar(SCANNER.nextInt());
   }
 
   public static void howManyGame() {
-    System.out.println("시도할 회수는 몇 회 인가요? ");
+    System.out.println(HOW_MANY_GAMES);
     TRACK.setGameCount(SCANNER.nextInt());
   }
 
--- src/main/java/racingCar/OutputTable.java
+++ src/main/java/racingCar/OutputTable.java
@@ -2,10 +2,11 @@ package racingCar;
 
 public final class OutputTable {
 
+  private final static String OUTPUT_RESULT = "결과: ";
   public static void main(String[] args) {
     InputTable.howManyCar();
     InputTable.howManyGame();
-    System.out.println("결과: ");
+    System.out.println(OUTPUT_RESULT);
     InputTable.TRACK.play();
   }
 
