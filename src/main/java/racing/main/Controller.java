ec1cbc87df4780c20cc1b29a84421700b7928114
hjeom <nara1030@naver.com>
2022-04-09 18:39:28 +0900

    feat] 사용자는 자동차 대수와 이동 횟수를 입력할 수 있다.

--- src/main/java/racing/main/Controller.java
+++ src/main/java/racing/main/Controller.java
@@ -1,4 +1,12 @@
 package racing.main;
 
+import racing.domain.Competition;
+import racing.ui.InputView;
+import racing.ui.ResultView;
+
 public class Controller {
+    public static void main(String[] args) {
+        Competition competition = InputView.input();
+        ResultView.print(competition);
+    }
 }
