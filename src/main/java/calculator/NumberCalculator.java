7b17c877972555dbd9bb537e1ef4b024baa58997
zieunx <wldms_1217@naver.com>
2022-04-10 00:44:07 +0900

    step2: 합계를 구하는 방식을 for 에서 stream sum() 으로 변경

--- src/main/java/calculator/NumberCalculator.java
+++ src/main/java/calculator/NumberCalculator.java
@@ -5,10 +5,8 @@ import java.util.List;
 public class NumberCalculator {
 
 	public static int sum(List<Integer> numbers) {
-		int sum = 0;
-		for (int number : numbers) {
-			sum += number;
-		}
-		return sum;
+		return numbers.stream()
+			.mapToInt(number -> number)
+			.sum();
 	}
 }
