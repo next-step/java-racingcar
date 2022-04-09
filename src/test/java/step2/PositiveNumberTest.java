1b2c084b58f6f005c11353ac09ab1e5df0783a44
itdar <thesockerr@gmail.com>
2022-04-09 20:21:03 +0900

    refactor: 리뷰어님 step2 리뷰사항 적용 커밋
    - 클래스 네이밍 수정
    - 스트림 적용

--- src/test/java/step2/PositiveNumberTest.java
+++ src/test/java/step2/PositiveNumberTest.java
@@ -5,18 +5,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
-class PositiveTest {
+class PositiveNumberTest {
 
     @Test
     void 정상생성_테스트한다() {
-        Positive positive = new Positive(1);
-        assertThat(positive.getPositiveNumber()).isEqualTo(1);
+        PositiveNumber positiveNumber = new PositiveNumber(1);
+        assertThat(positiveNumber.getPositiveNumber()).isEqualTo(1);
     }
 
     @Test
     void 음수생성_실패한다() {
         assertThatThrownBy(() -> {
-            new Positive(-1);
+            new PositiveNumber(-1);
         }).isInstanceOf(RuntimeException.class);
     }
 
