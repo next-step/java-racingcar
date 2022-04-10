bf69fbab0ad9a9a88f47a9c7eee73d7bdf6970fd
양경희 <alicesister1@gmail.com>
2022-04-10 22:00:31 +0900

    docs: public 인터페이스 문서화

--- src/main/java/racing/model/CarMoveStrategy.java
+++ src/main/java/racing/model/CarMoveStrategy.java
@@ -1,5 +1,8 @@
 package racing.model;
 
+/**
+ * 자동차가 이동하는 규칙을 정의하기 위한 인터페이스
+ */
 public interface CarMoveStrategy {
     boolean isMovable();
 }
--- src/main/java/racing/model/CustomRandom.java
+++ src/main/java/racing/model/CustomRandom.java
@@ -1,5 +1,8 @@
 package racing.model;
 
+/**
+ * 범위를 지정하여 랜덤 생성하는 방법을 정의하기 위한 인터페이스
+ */
 public interface CustomRandom {
     int getRandomRange(int bound);
 }
