fbc09a715379d6e687d76a5912e8e1814c3b50ce
guswns1659 <guswns1659@gmail.com>
2022-04-11 07:37:21 +0900

    step3 feat: 레이싱 게임 동작하는 코드 구현

---
+++ src/main/java/RacingController.java
@@ -0,0 +1,30 @@
+import racingcar.RacingCar;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RacingController {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("레이싱에 참여할 자동차 대수를 입력해주세요.");
+        int countOfCar = scanner.nextInt();
+        System.out.println("시도할 횟수는 몇 번인가요?");
+        int maxAttempts = scanner.nextInt();
+
+        int[] cars = new int[countOfCar];
+
+        System.out.println("실행 결과");
+
+        for (int attempt = 0; attempt < maxAttempts; attempt++) {
+            for (int carIndex = 0; carIndex < cars.length; carIndex++) {
+                cars[carIndex] += RacingCar.move(new Random().nextInt(10));
+                for (int print = 0; print < cars[carIndex]; print++) {
+                    System.out.print("-");
+                }
+                System.out.println();
+                Thread.sleep(500);
+            }
+            System.out.println("<----------> 회차종료");
+        }
+    }
+}
