d5852faecc86355d283b96044002f4155ad3aa2e
minigear <coosac@gmail.com>
2022-04-10 12:36:01 +0900

    2단계 - 문자열 덧셈 계산기 : method 분리

--- src/main/java/calculator/StringAndCalculator.java
+++ src/main/java/calculator/StringAndCalculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,23 +16,36 @@ public class StringAndCalculator {
         if (m.find()) {
             String customDelimiter = m.group(1);
             String[] tokens = m.group(2).split(customDelimiter);
-            return sumValues(tokens);
+            List<Integer> integers = stringsToIntegers(tokens);
+            return sumValues(integers);
         } else {
             String[] tokens = text.split("[,:]");
-            return sumValues(tokens);
+            List<Integer> integers = stringsToIntegers(tokens);
+            return sumValues(integers);
         }
     }
 
-    private static int sumValues(String[] values) {
+    private static int sumValues(List<Integer> values) {
         int result = 0;
-        for (String value : values) {
-
-            int temp = Integer.parseInt(value);
-            if (temp < 0) {
-                throw new RuntimeException();
-            }
-            result += temp;
+        for (Integer value : values) {
+            result += value;
         }
         return result;
+    }
+
+    private static List<Integer> stringsToIntegers(String[] values) {
+        List<Integer> integers = new ArrayList<>();
+        for (String value : values) {
+            integers.add(stringToInt(value));
+        }
+        return integers;
+    }
+
+    private static int stringToInt(String value) {
+        int intValue = Integer.parseInt(value);
+        if (intValue < 0) {
+            throw new RuntimeException();
+        }
+        return intValue;
     }
 }
