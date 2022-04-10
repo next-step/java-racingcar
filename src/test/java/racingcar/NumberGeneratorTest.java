146b20d68207bb30e5335c75b3c6d922ae071b50
hahoho87 <bbaing4564@gmail.com>
2022-04-10 23:47:35 +0900

    refactor: 난수 생성 테스트 리팩토링

--- src/test/java/racingcar/NumberGeneratorTest.java
+++ src/test/java/racingcar/NumberGeneratorTest.java
@@ -1,35 +1,34 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * The type Number generator test.
+ */
 class NumberGeneratorTest {
+
+    NumberGenerator numberGenerator;
+
+    @BeforeEach
+    void setUp() {
+        numberGenerator = new NumberGenerator();
+    }
 
     @RepeatedTest(30)
     @DisplayName("0 - 9 난수 생성")
     void generateRandomNumberZeroToNine() {
-        // given
-        NumberGenerator numberGenerator = new NumberGenerator();
-        // when
-        int generatedNumber = numberGenerator.generateRandomNumber();
-        // then
-        assertThat(generatedNumber).isBetween(0, 9);
+        assertThat(numberGenerator.generateRandomNumber()).isBetween(0, 9);
     }
 
     @Test
     @DisplayName("input의 길이 만큼 난수 리스트 생성")
     void generateRandomNumberList() {
-        // given
-        NumberGenerator numberGenerator = new NumberGenerator();
-        // when
         int roundCount = 5;
-        List<Integer> randomNumbers = numberGenerator.generateRandomNumbers(5);
-        // then
-        assertThat(randomNumbers).hasSize(roundCount);
+        assertThat(numberGenerator.generateRandomNumbers(roundCount)).hasSize(roundCount);
     }
 }
