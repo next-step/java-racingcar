59dfd90c47c4ac5c9ac25bd36de7153e539fdcb7
wlroh <wlroh.paul@gmail.com>
2022-04-11 01:57:00 +0900

    refactor: 한 단계의 들여쓰기 위해 메소드 분리 클래스 분리

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -10,24 +10,28 @@ import java.util.List;
 
 public class RacingGame {
 
-    private static final String MESSAGE_RESULT = "실행결과";
-
     private InputCarCount carCount;
     private InputRoundCount roundCount;
     private List<Car> cars;
 
     public RacingGame(InputCarCount carCount, InputRoundCount roundCount) {
-        if (carCount == null || roundCount == null) {
+        if (isNull(carCount, roundCount)) {
             throw new IllegalArgumentException();
         }
+        init(carCount, roundCount);
+    }
 
+    private void init(InputCarCount carCount, InputRoundCount roundCount) {
         this.carCount = carCount;
         this.roundCount = roundCount;
         this.cars = new ArrayList<>();
-
         for (int i = 0; i < carCount.getCarCount(); i++) {
             this.cars.add(new Car());
         }
+    }
+
+    private boolean isNull(InputCarCount carCount, InputRoundCount roundCount) {
+        return carCount == null || roundCount == null;
     }
 
     public List<Car> getCars() {
@@ -38,10 +42,7 @@ public class RacingGame {
         int tryCount = 0;
         for (int i = 0; i < roundCount.getRoundCount(); i++) {
             cars.forEach(car -> car.stopOrGo(ConditionGenerator.getInstance().generateRandomCondition()));
-            if (tryCount == 0) {
-                System.out.println(MESSAGE_RESULT);
-            }
-            ResultView.renderResultView(cars);
+            ResultView.renderResultView(cars, tryCount);
             tryCount++;
         }
         return tryCount;
--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -6,10 +6,19 @@ import java.util.List;
 
 public class ResultView {
 
-    public static void renderResultView(List<Car> cars) {
+    private static final String MESSAGE_RESULT = "실행결과";
+
+    public static void renderResultView(List<Car> cars, int tryCount) {
+        if (isFirstResult(tryCount)) {
+            System.out.println(MESSAGE_RESULT);
+        }
         for (Car car : cars) {
             System.out.println(car.getPositionView());
         }
         System.out.println("");
+    }
+
+    private static boolean isFirstResult(int tryCount) {
+        return tryCount == 0;
     }
 }
