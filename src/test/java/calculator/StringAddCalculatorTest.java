9f9217797f1f7981fe2e7cfd9c92948e7ed3dbb2
zieunx <wldms_1217@naver.com>
2022-04-11 01:36:17 +0900

    step2: 메소드명을 의미있는 영어로 변경하고 @DisplayName() 어노테이션을 사용하여 의미를 더 쉽게 전달

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -2,61 +2,72 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringAddCalculatorTest {
+	@DisplayName("빈문자열이거나 Null인 경우 0")
 	@Test
-	void 빈문자열이거나_Null인_경우() {
+	void When_BlackOrNull_Expect_Zero() {
 		assertThat(StringAddCalculator.splitAndSum("")).isEqualTo(0);
 		assertThat(StringAddCalculator.splitAndSum(null)).isEqualTo(0);
 	}
 
+	@DisplayName("쉼표 구분자")
 	@Test
-	void 쉼표_구분자() {
+	void When_commaSeparator_Then_Sum() {
 		assertThat(StringAddCalculator.splitAndSum("1,2")).isEqualTo(3);
 	}
 
+	@DisplayName("숫자가 하나인 경우")
 	@Test
-	void 숫자_하나의_문자열() {
+	void When_OnlyOneNumber_Then_ReturnNumber() {
 		assertThat(StringAddCalculator.splitAndSum("1")).isEqualTo(1);
 	}
 
+	@DisplayName("콜론 구분자")
 	@Test
-	void 콜론_구분자() {
+	void When_ColonSeparator_Then_Sum() {
 		assertThat(StringAddCalculator.splitAndSum("2:3")).isEqualTo(5);
 	}
 
+	@DisplayName("custom 구분자")
 	@Test
-	void custom_구분자() {
+	void When_CustomSeparator_Then_Sum() {
 		assertThat(StringAddCalculator.splitAndSum("//-\n1-2-3")).isEqualTo(6);
 	}
 
+	@DisplayName("구분자_연속사용_RuntimeException_발생")
 	@Test
-	void 구분자_연속사용_RuntimeException_발생() {
+	void When_SeriesOfSeparator_Then_RunTimeException() {
 		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("1::2"))
 			.isInstanceOf(RuntimeException.class);
 	}
 
+	@DisplayName("custom_구분자포맷이_아닌_구분자_RuntimeException_발생")
 	@Test
-	void custom_구분자포맷이_아닌_구분자_RuntimeException_발생() {
+	void When_UndeclaredSeparator_Then_RunTimeException() {
 		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("1;2:3"))
 			.isInstanceOf(RuntimeException.class);
 	}
 
+	@DisplayName("음수_RuntimeException_발생")
 	@Test
-	void 음수_RuntimeException_발생() {
+	void When_NegativeNumber_Then_RunTimeException() {
 		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1"))
 			.isInstanceOf(RuntimeException.class);
 	}
 
+	@DisplayName("음수와_custom_구분자_RuntimeException_발생")
 	@Test
-	void 음수와_custom_구분자_RuntimeException_발생() {
+	void When_NegativeNumberAndCustomSeparator_Then_RunTimeException() {
 		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("//;\n-1;2;3"))
 			.isInstanceOf(RuntimeException.class);
 	}
 
+	@DisplayName("음수와_custom_구분자가_마이너스일때_RuntimeException_발생")
 	@Test
-	void 음수와_custom_구분자가_마이너스일때_RuntimeException_발생() {
+	void When_NegativeNumberAndMinusSeparator_Then_RunTimeException() {
 		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("//-\n1--2-3"))
 			.isInstanceOf(RuntimeException.class);
 	}
