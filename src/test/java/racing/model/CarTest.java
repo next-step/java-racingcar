b9ee43fd74e24c43498a98f2b14bfa8b97cc8759
양경희 <alicesister1@gmail.com>
2022-04-10 22:02:28 +0900

    test: 불필요한 BooleanConverter 제거

--- src/test/java/racing/model/CarTest.java
+++ src/test/java/racing/model/CarTest.java
@@ -1,11 +1,7 @@
 package racing.model;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.converter.ArgumentConversionException;
-import org.junit.jupiter.params.converter.ArgumentConverter;
-import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,22 +11,8 @@ class CarTest {
     @DisplayName("전략에 따라 자동차 전진 또는 멈춤")
     @ParameterizedTest
     @CsvSource(value = {"true,1", "false,0"}, delimiter = ',')
-    void carMoveForward(@ConvertWith(BooleanConverter.class) Boolean isMovable, int expected) {
+    void carMoveForward(Boolean isMovable, int expected) {
         CarMoveStrategy moveStrategy = () -> isMovable;
         assertThat(new Car().run(moveStrategy)).isEqualTo(expected);
-    }
-
-    private static class BooleanConverter implements ArgumentConverter {
-        @Override
-        public Object convert(Object source, ParameterContext context) throws ArgumentConversionException {
-            if (!(source instanceof String)) {
-                throw new IllegalArgumentException("The argument should be a String: " + source);
-            }
-            try {
-                return Boolean.parseBoolean((String) source);
-            } catch (Exception e) {
-                throw new IllegalArgumentException("Failed to convert", e);
-            }
-        }
     }
 }
