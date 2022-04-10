912be8da1c7029d447e44d3bb47d73d184dd1294
minigear <coosac@gmail.com>
2022-04-10 22:25:44 +0900

    3단계 - 자동차 경주 [doc] 기능 목록 작성

---
+++ src/test/java/racing/RacingTest.java
@@ -0,0 +1,12 @@
+package racing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RacingTest {
+    @Test
+    @DisplayName("test")
+    void carTest() {
+
+    }
+}
