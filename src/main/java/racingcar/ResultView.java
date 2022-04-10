4796d5855fb7123ca97e84459452f00393227e0b
hahoho87 <bbaing4564@gmail.com>
2022-04-10 23:55:46 +0900

    feat: 결과 출력을 위한 ResultView 객체 추가

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -1,39 +1,15 @@
 package racingcar;
 
-import java.util.List;
-
 public class ResultView {
 
-    private static final String PATH_SYMBOL = "-";
-    private static final String WINNER_ANNOUNCMENT_MESSAGE = " 가 최종 우승했습니다.";
-
-
-    public void printEveryLocation(List<Car> carList) {
-        carList.forEach(c -> System.out.println(this.printNameAndLocation(c)));
-        System.out.println();
-    }
-
-    public String printNameAndLocation(Car car) {
+    // TODO 각 라운드별로 출력할 수 있게 변경 [2022.04.10 - hahoho87]
+    public void printResult(int resultLocation) {
         StringBuilder stringBuilder = new StringBuilder();
-        printCarName(car, stringBuilder);
-        printLocation(car, stringBuilder);
-        return stringBuilder.toString();
-    }
-
-    private void printLocation(Car car, StringBuilder stringBuilder) {
         int count = 0;
-        while (count < car.getCurrentLocation()) {
-            stringBuilder.append(PATH_SYMBOL);
+        while (count < resultLocation) {
+            stringBuilder.append("-");
             count++;
         }
-    }
-
-    private void printCarName(Car car, StringBuilder stringBuilder) {
-        stringBuilder.append(car.getCarName()).append(": ");
-    }
-
-    public void printWinners(Cars cars) {
-        String winners = cars.getWinners();
-        System.out.println(winners + WINNER_ANNOUNCMENT_MESSAGE);
+        System.out.println(stringBuilder.toString());
     }
 }
