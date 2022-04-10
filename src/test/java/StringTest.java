e1efb1182208f6b688a5bc36eee214c21ec3b33e
syeeuns <syeeuns@gmail.com>
2022-04-10 21:24:27 +0900

    step1 피드백 반영

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -9,20 +9,17 @@ public class StringTest {
 
   @Test
   void splitTwo() {
-    String[] result = "1,2".split(",");
-    assertThat(result).contains("1", "2");
+    assertThat("1,2".split(",")).containsExactly("1", "2");
   }
 
   @Test
   void splitOnlyOne() {
-    String[] result = "1".split(",");
-    assertThat(result).containsExactly("1");
+    assertThat("1".split(",")).containsExactly("1");
   }
 
   @Test
   void substring() {
-    String result = "(1,2)".substring(1, 4);
-    assertThat(result).isEqualTo("1,2");
+    assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
   }
 
   @DisplayName("charAt으로 특정 위치의 문자를 가져온다.")
