0972a67041bb20606d41f01eb034e144610723a4
Haeman Lee <37089983+haemanlee@users.noreply.github.com>
2022-04-10 23:53:09 +0900

    Step2 (#3184)
    * (feat) stringtest settest add.
    
    * (refector) 공통부분 BeforeEach로 리팩토링
    
    * merged
    
    * (feat) 문자열 계산기
    
    * (chore) rollback
    
    * (fix) 코드리뷰 피드백 반영
    
    Co-authored-by: 이해만Haeman Lee <haeman.lee@bespinglobal.com>

---
+++ src/main/java/charactercalculator/CharCalculator.java
@@ -0,0 +1,63 @@
+package charactercalculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CharCalculator {
+    private String text;
+    private Matcher m;
+
+//    static List<String> expressionList = Arrays.asList(":",",");
+    public CharCalculator(String input) {
+        this.text = input;
+        // 생성자 생성될 때만 한번 선언하게 리펙토링
+        if(!isEmpty(input)){
+            m = Pattern.compile("//(.)\n(.*)").matcher(this.text);
+        }
+    }
+    public CharCalculator() {
+    }
+
+
+    public int calculate() {
+        int result = 0;
+        if(isEmpty(text)){
+            return 0;
+        }
+        return getResult(result);
+    }
+
+    private boolean isEmpty(String text) {
+        return "".equals(text) || text == null;
+    }
+
+    private int getResult(int result) {
+        String customSeparator = getSeparator();
+        String[] numberStrings = this.text.split(customSeparator);
+        for (String numberString : numberStrings) {
+            result += getNumber(numberString);
+        }
+        return result;
+    }
+
+    private String getSeparator() {
+        String customSeparator = ",|:";
+        if(m.find()){
+            customSeparator = m.group(1);
+            this.text = m.group(2);
+        }
+        return customSeparator;
+    }
+
+    private int getNumber(String numberString) {
+        int result = Integer.parseInt(numberString);
+        if(isMinus(result)) {
+            throw new RuntimeException("양수를 입력해야 합니다.");
+        }
+        return result;
+    }
+
+    private boolean isMinus(int result) {
+        return result < 0;
+    }
+}
