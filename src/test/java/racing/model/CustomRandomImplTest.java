18f0e93595dfaf0b270caf9f7bd2871b77a9e251
양경희 <alicesister1@gmail.com>
2022-04-10 21:23:54 +0900

    test: 무의미한 랜덤 전략 테스트 개선

--- src/test/java/racing/model/CustomRandomImplTest.java
+++ src/test/java/racing/model/CustomRandomImplTest.java
@@ -1,5 +1,7 @@
 package racing.model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -8,14 +10,18 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class CustomRandomImplTest {
 
-    @ParameterizedTest
-    @ValueSource(ints = {2, 5, 10})
-    void testRandomRange(int bound) {
-        assertThat(new CustomRandomImpl().getRandomRange(bound)).isBetween(0, bound);
+    @DisplayName("경계값인 1, 10 범위를 반복 테스트")
+    @RepeatedTest(10)
+    void testRandomRange() {
+        int boundOne = 1;
+        int boundTen = 10;
+        assertThat(new CustomRandomImpl().getRandomRange(boundOne)).isBetween(0, 0);
+        assertThat(new CustomRandomImpl().getRandomRange(boundTen)).isBetween(0, 9);
     }
 
+    @DisplayName("랜덤 생성시 0이하 범위는 예외 발생")
     @ParameterizedTest
-    @ValueSource(ints = {-5, 0})
+    @ValueSource(ints = {-1, 0})
     void testRandomRangeThrowException(int bound) {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new CustomRandomImpl().getRandomRange(bound));
     }
