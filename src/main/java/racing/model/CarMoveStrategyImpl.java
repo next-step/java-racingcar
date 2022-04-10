1078a0f0dc60e51368720c2f5f1a8ce538295031
양경희 <alicesister1@gmail.com>
2022-04-10 21:41:52 +0900

    docs: 주석에 노출된 로직 삭제

--- src/main/java/racing/model/CarMoveStrategyImpl.java
+++ src/main/java/racing/model/CarMoveStrategyImpl.java
@@ -11,7 +11,7 @@ public class CarMoveStrategyImpl implements CarMoveStrategy {
     }
 
     /**
-     * 전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다
+     * 전진하는 조건 만족 여부 확인
      *
      * @return 전진 가능 여부
      */
