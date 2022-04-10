d981e8fde930af9d53d06d70ac5df2eec135a07e
양경희 <alicesister1@gmail.com>
2022-04-10 23:44:29 +0900

    refactor: 게임 로직 예외 및 반환값 테스트

--- src/test/java/racing/RacingCarGameTest.java
+++ src/test/java/racing/RacingCarGameTest.java
@@ -3,7 +3,7 @@ package racing;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import racing.model.CarMoveStrategy;
 import racing.model.CarMoveStrategyImpl;
 import racing.model.CustomRandomImpl;
@@ -14,10 +14,9 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 class RacingCarGameTest {
 
     @DisplayName("자동차 수를 1대 보다 작게 입력하면 예외 발생")
-    @ParameterizedTest
-    @ValueSource(ints = {-5, 0})
-    void createGameThrowException(int input) {
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new RacingCarGame(input, createMoveStrategy()));
+    @Test
+    void createGameThrowException() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new RacingCarGame(0, createMoveStrategy()));
     }
 
     private CarMoveStrategy createMoveStrategy() {
@@ -25,18 +24,17 @@ class RacingCarGameTest {
     }
 
     @DisplayName("이동횟수를 1 보다 작게 입력하면 예외 발생")
-    @ParameterizedTest
-    @ValueSource(ints = {-5, 0})
-    void runThrowException(int input) {
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new RacingCarGame(3, createMoveStrategy()).run(input));
+    @Test
+    void runThrowException() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new RacingCarGame(3, createMoveStrategy()).run(0));
     }
 
-    @DisplayName("게임 실행 결과는 null 이 아니다")
-    @Test
-    void testGameRun() {
-        RacingCarGame racingCarGame = new RacingCarGame(3, createMoveStrategy());
-        GameResult gameResult = racingCarGame.run(5);
-        assertThat(gameResult).isNotNull();
+    @DisplayName("정상 입력하여 게임 실행 시 GameResult 객체가 반환된다")
+    @ParameterizedTest
+    @CsvSource(value = {"1,1", "3,5"})
+    void testGameRun(int numberOfCars, int numberOfMoves) {
+        RacingCarGame racingCarGame = new RacingCarGame(numberOfCars, createMoveStrategy());
+        GameResult gameResult = racingCarGame.run(numberOfMoves);
         assertThat(gameResult).isInstanceOf(GameResult.class);
     }
 }
