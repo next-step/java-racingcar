10ae6a633f4fa32834f72a957a851cfe131f8c85
devyonghee <devyonghee@gmail.com>
2022-04-12 01:00:23 +0900

    test(TrackTest) : 테스트 객체 변경

--- src/test/java/me/devyonghee/racingcar/model/TrackTest.java
+++ src/test/java/me/devyonghee/racingcar/model/TrackTest.java
@@ -3,12 +3,7 @@ package me.devyonghee.racingcar.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @DisplayName("경주로")
 class TrackTest {
@@ -31,11 +26,8 @@ class TrackTest {
     @Test
     @DisplayName("다음 단계에서 자동차가 움직이면 거리는 증가")
     void movedTrack_move_increaseDistance() {
-        //given
-        Random random = mock(Random.class);
-        when(random.nextInt(anyInt())).thenReturn(4);
-        //when
-        Track track = Track.of(RacingCar.from(RandomMovementPolicy.from(random)), Distance.ZERO).movedTrack();
+        //given, when
+        Track track = Track.of(RacingCar.from(new MovementPolicy.Fake(true)), Distance.ZERO).movedTrack();
         //then
         assertThat(track.distance()).isEqualTo(Distance.ONE);
     }
@@ -44,11 +36,9 @@ class TrackTest {
     @DisplayName("다음 단계에서 자동차가 움직이지 않으면 거리는 고정")
     void movedTrack_stop_sameDistance() {
         //given
-        Random random = mock(Random.class);
-        when(random.nextInt(anyInt())).thenReturn(0);
         Distance initialDistance = Distance.ZERO;
         //when
-        Track track = Track.of(RacingCar.from(RandomMovementPolicy.from(random)), initialDistance).movedTrack();
+        Track track = Track.of(RacingCar.from(new MovementPolicy.Fake(false)), initialDistance).movedTrack();
         //then
         assertThat(track.distance()).isEqualTo(initialDistance);
     }
