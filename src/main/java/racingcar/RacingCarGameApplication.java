38ab01ed40ebf6bad8c163451e57564fb059c798
DahamLeee <dlek1011@gmail.com>
2022-04-11 12:49:09 +0900

    refactor separate resultView

--- src/main/java/racingcar/RacingCarGameApplication.java
+++ src/main/java/racingcar/RacingCarGameApplication.java
@@ -4,13 +4,19 @@ public class RacingCarGameApplication {
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
         String[] carNames = inputView.insertRacingCarName();
         int totalRacingRound = inputView.insertTotalRacingRound();
 
-        RacingGame racingGame = new RacingGame(carNames, totalRacingRound, randomNumberGenerator);
-        racingGame.raceStart();
+        RacingGame racingGame = new RacingGame(carNames);
+
+        for (int i = 0; i < totalRacingRound; i++) {
+            racingGame.roundStart(randomNumberGenerator);
+            resultView.roundLogTrace(racingGame.getRacingCars());
+        }
+
         racingGame.winnersOfTheRace();
     }
 }
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -7,17 +7,11 @@ import static java.util.Comparator.*;
 
 public class RacingGame {
 
-    private int totalRound;
     private int longestDistance;
-    private ResultView resultView;
-    private RandomNumberGenerator randomNumberGenerator;
     private List<RacingCar> racingCars = new ArrayList<>();
     private List<String> winners = new ArrayList<>();
 
-    public RacingGame(String[] carNames, int totalRound, RandomNumberGenerator randomNumberGenerator) {
-        this.totalRound = totalRound;
-        this.resultView = new ResultView();
-        this.randomNumberGenerator = randomNumberGenerator;
+    public RacingGame(String[] carNames) {
         readyRacingCars(carNames);
     }
 
@@ -27,15 +21,7 @@ public class RacingGame {
         }
     }
 
-    public void raceStart() {
-        for (int i = 0; i < totalRound; i++) {
-            roundStart();
-            resultView.roundLogTrace(racingCars);
-        }
-        findLongestDistance();
-    }
-
-    private void roundStart() {
+    public void roundStart(RandomNumberGenerator randomNumberGenerator) {
         for (RacingCar racingCar : racingCars) {
             racingCar.moveOrStop(randomNumberGenerator.generateRacingRandomNumber());
         }
@@ -50,12 +36,15 @@ public class RacingGame {
         for (RacingCar racingCar : racingCars) {
             isWinner(racingCar);
         }
-        resultView.winnerLogTrace(winners);
     }
 
     private void isWinner(RacingCar racingCar) {
         if (racingCar.getDistance() == longestDistance) {
             winners.add(racingCar.getCarName());
         }
+    }
+
+    public List<RacingCar> getRacingCars() {
+        return racingCars;
     }
 }
