4b14dbea49632a6f9ff880d3a6abef2c656a8e64
imcool2551 <imcool2551@gmail.com>
2022-04-11 15:24:51 +0900

    test: RandomMoveStrategy
    RandomMoveStrategy를 테스트했습니다. RacingCarGame에서 더 이상 이동 가능한지 여부를 판단하지 않기 때문에 필요 없어진 테스트들을 제거했습니다.

--- src/test/java/racingcar/domain/RacingCarGameTest.java
+++ src/test/java/racingcar/domain/RacingCarGameTest.java
@@ -1,105 +1,40 @@
 package racingcar.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.strategy.MoveStrategy;
+import racingcar.domain.strategy.RandomMoveStrategy;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import static org.assertj.core.api.Assertions.*;
 
 class RacingCarGameTest {
 
-    private static final Random random = new Random();
-    private static final int STOP_LOW = 0;
-    private static final int STOP_HIGH = 3;
-    private static final int MOVE_LOW = 4;
-    private static final int MOVE_HIGH = 9;
-    private final int carCount = 10;
-    private final int rounds = 10;
-    private List<Car> cars;
-    private RacingCarGame game;
-
-    @BeforeEach
-    void setUp() {
-        cars = CarFactory.generateCarsOfSize(carCount);
-        game = new RacingCarGame(cars, rounds);
-    }
+    private static final int carSize = 10;
+    private static final int rounds = 10;
 
     @Test
     void rounds_만큼_proceedRound_호출하면_정상동작() {
-
+        RacingCarGame game = generateGame(new RandomMoveStrategy());
         assertThatNoException().isThrownBy(() -> {
             for (int i = 0; i < rounds; i++) {
-                game.proceedRound(RandomGenerator.generateRandomsOfSize(carCount));
+                game.proceedRound();
             }
         });
     }
 
     @Test
     void rounds_보다_proceedRound_많이_호출하면_예외() {
-
+        RacingCarGame game = generateGame(new RandomMoveStrategy());
         assertThatThrownBy(() -> {
             for (int i = 0; i < rounds + 1; i++) {
-                game.proceedRound(RandomGenerator.generateRandomsOfSize(carCount));
+                game.proceedRound();
             }
         }).isInstanceOf(IllegalStateException.class);
     }
 
-    @DisplayName("proceedRound() 호출 시 자동차에 제공된 값이 0 ~ 3이면 정지")
-    @Test
-    void proceedRound_정지() {
-        List<Integer> stopNumbers = getStopNumbers();
-        Car stoppedCar = new Car(new Position());
-
-        game.proceedRound(stopNumbers);
-
-        cars.forEach(car -> {
-            assertThat(car).isEqualTo(stoppedCar);
-        });
-    }
-
-    @DisplayName("proceedRound() 호출 시 자동차에 제공된 값이 4 ~ 9이면 이동")
-    @Test
-    void proceedRound_이동() {
-        List<Integer> moveNumbers = getMoveNumbers();
-        Car movedCar = new Car(new Position());
-        movedCar.move();
-
-        game.proceedRound(moveNumbers);
-
-        cars.forEach(car -> {
-            assertThat(car).isEqualTo(movedCar);
-        });
-    }
-
-    private List<Integer> getStopNumbers() {
-        List<Integer> stopNumbers = new ArrayList<>();
-        for (int i = 0; i < carCount; i++) {
-            stopNumbers.add(getStopNumber());
-        }
-        return stopNumbers;
-    }
-
-    private List<Integer> getMoveNumbers() {
-        List<Integer> moveNumbers = new ArrayList<>();
-        for (int i = 0; i < carCount; i++) {
-            moveNumbers.add(getMoveNumber());
-        }
-        return moveNumbers;
-    }
-
-    private int getStopNumber() {
-        return generateRandomBetween(STOP_LOW, STOP_HIGH);
-    }
-
-    private int getMoveNumber() {
-        return generateRandomBetween(MOVE_LOW, MOVE_HIGH);
-    }
-
-    private int generateRandomBetween(int low, int high) {
-        return random.nextInt(high - low + 1) + low;
+    private RacingCarGame generateGame(MoveStrategy strategy) {
+        List<Car> cars = CarFactory.generateCarsOfSize(carSize);
+        return new RacingCarGame(cars, rounds, strategy);
     }
 }
---
+++ src/test/java/racingcar/domain/strategy/RandomMoveStrategyTest.java
@@ -0,0 +1,26 @@
+package racingcar.domain.strategy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomMoveStrategyTest {
+
+    MoveStrategy strategy = new RandomMoveStrategy();
+
+    @DisplayName("0~3은 이동 실패")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void canMove_fail(int number) {
+        assertThat(strategy.canMove(number)).isFalse();
+    }
+
+    @DisplayName("4~9는 이동 성공")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void canMove_success(int number) {
+        assertThat(strategy.canMove(number)).isTrue();
+    }
+}
