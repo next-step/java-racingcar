ffe38029d0eeb73be75a9e5e9d387d8903976b72
guswns1659 <guswns1659@gmail.com>
2022-04-13 07:34:44 +0900

    step3 refactor: RacingController 역할을 FormulaRacing에게 전달.

--- src/main/java/FormulaRacing.java
+++ src/main/java/FormulaRacing.java
@@ -1,12 +1,24 @@
-import racingcar.Prompt;
-import racingcar.RacingController;
-import racingcar.Rule;
+import racingcar.*;
+
+import java.util.List;
 
 public class FormulaRacing {
     public static void main(String[] args) {
         int countOfCar = Prompt.ask("레이싱에 참여할 자동차 대수를 입력해주세요.");
         int maxAttempts = Prompt.ask("시도할 횟수는 몇 번인가요?");
 
-        RacingController.start(new Rule(countOfCar, maxAttempts));
+        start(new Rule(countOfCar, maxAttempts));
+    }
+
+    private static void start(Rule rule) {
+        List<RacingCar> cars = rule.readyCars();
+
+        Printer.print("부릉부릉!! 레이싱 시작");
+
+        Racing racing = new Racing(rule.getMaxAttempts(), cars);
+        racing.start();
+        racing.end();
+
+        Printer.print("레이싱 종료");
     }
 }
