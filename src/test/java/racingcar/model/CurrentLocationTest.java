20e0a0c60f9d6950b8a38c9b5b77a9c835a59bf4
cooper <pbg0205@naver.com>
2022-04-12 21:49:08 +0900

    test: CurrentLocationTest 움직임 테스트 수정

--- src/test/java/racingcar/model/CurrentLocationTest.java
+++ src/test/java/racingcar/model/CurrentLocationTest.java
@@ -1,28 +1,21 @@
 package racingcar.model;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.model.CurrentLocation;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CurrentLocationTest {
 
-    @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    @DisplayName("움직임의 규칙을 만족하는지 확인한다")
-    void meetsMovementRuleTest(int number) {
+    @Test
+    void moveTest() {
+        //given
         CurrentLocation currentLocation = new CurrentLocation();
-        assertThat(currentLocation.meetsMovementRule(number)).isTrue();
-    }
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3})
-    @DisplayName("움직임의 규칙을 만족하지 않음을 확인한다")
-    void doesNotMeetMovementRuleTest(int number) {
-        CurrentLocation currentLocation = new CurrentLocation();
-        assertThat(currentLocation.meetsMovementRule(number)).isFalse();
+        //when
+        currentLocation.moveForward();
+
+        //then
+        assertThat(currentLocation.getLocationValue()).isEqualTo(2);
     }
 
 }
