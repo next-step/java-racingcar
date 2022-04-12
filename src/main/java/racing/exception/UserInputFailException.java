e64904db81b70935f6f98a5d9f1dc303e5dfca72
lyj <hihisososo@naver.com>
2022-04-12 19:29:34 +0900

    refactor: 사용하지 않는 UserInputFailException 의 생성자 제거

--- src/main/java/racing/exception/UserInputFailException.java
+++ src/main/java/racing/exception/UserInputFailException.java
@@ -2,10 +2,6 @@ package racing.exception;
 
 public class UserInputFailException extends RuntimeException {
 
-  public UserInputFailException(String message) {
-    super(message);
-  }
-
   public UserInputFailException(String message, Throwable cause) {
     super(message, cause);
   }
