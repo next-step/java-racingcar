e9ba2dc465d5a8a385e7e1635ad6ac7f4d4c2c87
Daseul Kim <nldaseul@gmail.com>
2022-04-12 09:25:37 +0900

    2단계 문자열 덧셈 계산기 (#3217)
    * 구현: splitAndSum 메서드 구현
    
    문자배열 안에 ',' 또는 ':' 구분자를 기준으로 문자열 자른 후 합계 반환
    
    * 숫자 이외의 값, 음수 포함된 문자열로 splitAndSum 메서드 호출 시 예외 반환 추가
    
    * 커스텀_구분자_기준으로_합계_반환 구현
    
    * typo: delimeter -> delimiter 오타 수정
    
    * 메서드 추출하여 문자열 덧셈 로직 1 depth로 리팩터링

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,42 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    static String DEFAULT_DELIMITER = ",|:";
+    static String CUSTOM_DELIMITER = "//(.)\n(.*)";
+
+    public static int splitAndSum(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String delimiter = DEFAULT_DELIMITER;
+
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(input);
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+            input = matcher.group(2);
+        }
+
+        Pattern pattern = Pattern.compile(delimiter);
+        return sum(pattern.split(input));
+    }
+
+    private static int sum(String[] strings) {
+        int result = 0;
+        for (String string: strings) {
+            result += parseInt(string);
+        }
+        return result;
+    }
+
+    private static int parseInt(String value) {
+        Integer num = Integer.parseInt(value);
+        if (num < 0) {
+            throw new RuntimeException("음수 문자열은 허용되지 않습니다.");
+        }
+        return num;
+    }
+}
