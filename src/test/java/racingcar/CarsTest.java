01d9e56d3b907e5ac538ca610c9dc1b2735c691b
kang-jisu <rkdwltn0425@gmail.com>
2022-04-12 20:42:27 +0900

    test: Cars 클래스 생성 테스트

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -10,10 +10,26 @@ import java.util.ArrayList;
 public class CarsTest {
 
     @Test
-    @DisplayName("ArrayList 생성시 null값이 들어오면 NullPointerException이 발생한다.")
+    @DisplayName("ArrayList 생성시 null 값이 들어오면 NullPointerException 이 발생한다.")
     void arrayListNullTest() {
         ArrayList<Car> emptyCars = null;
         Assertions.assertThatThrownBy(() -> new ArrayList<>(emptyCars))
                 .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    @DisplayName("Cars 생성시 null 값이 들어오면 NullPointerException 이 발생한다.")
+    void nullCarsTest() {
+        Assertions.assertThatThrownBy(() -> new Cars(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessageContaining("Cars is null");
+    }
+
+    @Test
+    @DisplayName("Cars 생성시 빈 리스트가 들어오면 IllegalArgumentException 이 발생한다.")
+    void emptyCarsTest() {
+        Assertions.assertThatThrownBy(() -> new Cars(new ArrayList<>()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Cars is empty");
     }
 }
