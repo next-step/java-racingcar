8a47435643cefb49ba8e53134fa2a43903a8e7ba
jisu <jisuuoo96@gmail.com>
2022-04-12 23:41:54 +0900

    feat : 몇 번의 시도할 지 묻는 askNumberOfTries() 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,4 +10,9 @@ public class InputView {
         System.out.println("자동차 대수는 몇 대 인가요?");
         return sc.nextInt();
     }
+
+    public int askNumberOfTries() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return sc.nextInt();
+    }
 }
