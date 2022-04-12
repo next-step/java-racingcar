af4fe5bdb240144b1d949ad7a9ef318adfc20d2e
chkim <cndghks0213@gmail.com>
2022-04-13 01:23:17 +0900

    refactor: 랜덤 숫자 생성 클래스에서 의미없는 변수(MIN) 제거

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -4,13 +4,12 @@ import java.util.Random;
 
 public class RandomNumberGenerator {
     private static final Random random = new Random();
-    private static final int MIN = 0;
     private static final int MAX = 9;
 
     private RandomNumberGenerator() {
     }
 
     public static int getRandomNumber() {
-        return random.nextInt(MAX - 1) + MIN;
+        return random.nextInt(MAX + 1);
     }
 }
