f7dc4362c6519b175d762048fb7333e80e25a9f2
bluewow <dogseen@gmail.com>
2022-04-13 05:56:56 +0900

    feat : UI class 생성

---
+++ src/main/java/study/step3/InputView.java
@@ -0,0 +1,11 @@
+package study.step3;
+
+public class InputView {
+    public static void carCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    public static void tryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+    }
+}
---
+++ src/main/java/study/step3/ResultView.java
@@ -0,0 +1,7 @@
+package study.step3;
+
+public class ResultView {
+    public static void result() {
+        System.out.println("실행 결과");
+    }
+}
