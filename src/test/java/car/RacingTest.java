070dea0d9ee120e22aa02fe47807f389a2524ff0
rumor1993 <rumor1993@naver.com>
2022-04-13 23:50:31 +0900

    test: 자동차 4이상인 경우 전진, 멈춤 수정

--- src/test/java/car/RacingTest.java
+++ src/test/java/car/RacingTest.java
@@ -10,18 +10,6 @@ import java.util.regex.Pattern;
 public class RacingTest {
 
     @Test
-    void 자동차_대수가_맞는지() {
-        CarRacing carRacing = new CarRacing(4, 3, 4);
-        Assertions.assertThat(carRacing.getParticipatingCarCount()).isEqualTo(4);
-    }
-
-    @Test
-    void 주어진_횟수가_맞는지() {
-        CarRacing carRacing = new CarRacing(4, 3, 4);
-        Assertions.assertThat(carRacing.getAttemptCount()).isEqualTo(3);
-    }
-
-    @Test
     void 랜덤_한_숫자_0에서_9사이_값을_구한다() {
         Random random = new Random();
         Matcher matcher = Pattern.compile("[0-9]").matcher(String.valueOf(random.nextInt(10)));
@@ -30,25 +18,14 @@ public class RacingTest {
 
     @Test
     void 랜덤_값에서_4이상_인_경우_전진한다() {
-        CarRacing carRacing = new CarRacing(4, 3, 4);
-        Car car = new Car(carRacing);
-        car.move();
-
-        if (car.isMove()) {
-            Assertions.assertThat(car.isMove()).isTrue();
-        }
+        Car car = new Car("pobi");
+        Assertions.assertThat(car.move(4)).isEqualTo("-");
     }
 
     @Test
     void 랜덤_값에서_4미만_인_경우_멈춘다() {
-        CarRacing carRacing = new CarRacing(4, 3, 4);
-        Car car = new Car(carRacing);
-        car.move();
-
-        if (!car.isMove()) {
-            Assertions.assertThat(car.isMove()).isTrue();
-        }
+        Car car = new Car("pobi");
+        Assertions.assertThat(car.move(2)).isEqualTo("");
     }
-
 }
 
