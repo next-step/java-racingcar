b570ce5ddeabcac0056a67fce58c4a08870a8857
rumor1993 <rumor1993@naver.com>
2022-04-13 23:52:47 +0900

    test: 랜덤한 숫자를 구하는 테스트 구현부 변경
    주사위 클래스를 통해서 랜덤한 숫자를 가져오도록 처리

--- src/test/java/car/RacingTest.java
+++ src/test/java/car/RacingTest.java
@@ -2,6 +2,7 @@ package car;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import step4.Dice;
 
 import java.util.Random;
 import java.util.regex.Matcher;
@@ -11,8 +12,7 @@ public class RacingTest {
 
     @Test
     void 랜덤_한_숫자_0에서_9사이_값을_구한다() {
-        Random random = new Random();
-        Matcher matcher = Pattern.compile("[0-9]").matcher(String.valueOf(random.nextInt(10)));
+        Matcher matcher = Pattern.compile("[0-9]").matcher(String.valueOf(Dice.roll()));
         Assertions.assertThat(matcher.find()).isTrue();
     }
 
