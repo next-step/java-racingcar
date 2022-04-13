9f83e35e3919bf7521a6b6662a086e3897ea4040
양경희 <alicesister1@gmail.com>
2022-04-14 00:19:43 +0900

    refactor: Car 매직리터럴 상수화, 접근제한 private

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -4,14 +4,15 @@ import racing.domain.strategies.CarMoveStrategy;
 import racing.utils.Counter;
 
 public class Car {
-    public static final int RUN_STEP = 1;
-    public static final int INIT_VALUE = 0;
+    private static final int RUN_STEP = 1;
+    private static final int INIT_VALUE = 0;
+    private static final String NAME_EXCEPTION_MESSAGE = "자동차 이름은 5글자를 초과하지 않아야합니다.";
     private final Counter counter = new Counter(INIT_VALUE);
     private final String name;
 
     public Car(String nameOfCar) {
         if (nameOfCar == null || nameOfCar.isBlank() || nameOfCar.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5글자를 초과하지 않아야합니다.");
+            throw new IllegalArgumentException(NAME_EXCEPTION_MESSAGE);
         }
         name = nameOfCar;
     }
