d09aa2fab63dac8b9d53b8bd1d257b716befedf6
양경희 <alicesister1@gmail.com>
2022-04-14 00:25:26 +0900

    test: 자동차 이름, 이동횟수 getter 테스트

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,6 +1,7 @@
 package racing;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,6 +21,21 @@ class CarTest {
     @ValueSource(strings = {"Super Car", "Porsche", "Sports SUV"})
     void carMoveForward(String name) {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Car(name));
+    }
+
+    @DisplayName("getName()은 자동차 이름을 반환")
+    @ParameterizedTest
+    @ValueSource(strings = {"carA", "carB", "car!", "1car2"})
+    void getName(String name) {
+        Car carA = new Car(name);
+        assertThat(carA.getName()).isEqualTo(name);
+    }
+
+    @DisplayName("자동차의 이동횟수 초기값은 0")
+    @Test
+    void getCounter() {
+        Car carA = new Car("carA");
+        assertThat(carA.getCounter()).isEqualTo(new Counter(0));
     }
 
     @DisplayName("전략에 따라 자동차 전진 또는 멈춤")
