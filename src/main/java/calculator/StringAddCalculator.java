341deb12f56e7f150062291dd092efa1826dc823
wonjin <wonjin1776@naver.com>
2022-04-13 10:15:22 +0900

    1. 로컬변수 제거하기   - int result = 0   - int number = toInt(value) 2. 메소드 분리에 또 분리   - 메소드는 한가지 기능만 할 것   - private static int sum(String[] values) => 합산만 할 것   - private static int toInt(String value)  => 변환만 할 것

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -10,27 +10,39 @@ public class StringAddCalculator {
 
     public static int splitAndSum(String text) {
 
-        if(text == null || text.isBlank())return 0;
+        //로컬변수 int result = 0 을 선언하고 메소드 전체 블록 안에서 사용하는 것은 올바르지 않다.
+        //Early Return 을 통해 else문을 없애라.
 
-        int result = 0;
+        if(text == null || text.isBlank())return 0;
 
         Matcher m = Pattern.compile(DELIMITER_CUSTOM_REGEXP).matcher(text);
         if(m.find()){
             String customDelimiter = m.group(1);
             String[] values = m.group(2).split(customDelimiter);
-            return sum(result, values);
+            return sum(values);
         }
 
         String[] values = text.split(DELIMITER_DEFUALT_REGEXP);
-        return sum(result, values);
+        return sum(values);
     }
 
-    private static int sum(int result, String[] values) {
+    private static int sum(String[] values) {
+        int sum = 0;
         for (String value : values) {
-            int number = Integer.parseInt(value);
-            result += number;
+//            int number = toInt(value)   한 곳에서만 사용하는 지역변수는 제거할 것
+            sum += toInt(value);
         }
-        return result;
+        return sum;
     }
+
+    private static int toInt(String value) {
+        int number = Integer.parseInt(value);
+        if(number < 0){
+            throw new RuntimeException("음수를 허용하지 않습니다.");
+        }
+        return number;
+    }
+
+
 
 }
