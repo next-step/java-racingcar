eab8027e925fc44d6188c696cca24dd4d91298b1
jinsxl <wlsthf1113@gmail.com>
2022-04-14 02:02:18 +0900

    feat: 자동차 경주 구현

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -5,6 +5,13 @@ import java.util.Random;
 public class Car {
     static Random random = new Random();
     static final int BOUND = 10;
+    int distance = 0;
+
+    public void tryMove() {
+        if (canMoveForward(getRandomValue())) {
+            distance++;
+        }
+    }
 
     public int getRandomValue() {
         return random.nextInt(BOUND);
@@ -12,5 +19,9 @@ public class Car {
 
     public boolean canMoveForward(int number) {
         return number >= 4;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
--- src/main/java/racingCar/RacingCar.java
+++ src/main/java/racingCar/RacingCar.java
@@ -4,5 +4,17 @@ public class RacingCar {
     public static void main(String[] args) {
         int numberOfCars = InputView.getInput("자동차 대수를 입력해주세요.");
         int numberOfRounds = InputView.getInput("횟수를 입력해주세요.");
+
+        Car[] cars = new Car[numberOfCars];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car();
+        }
+
+        for (int round = 0; round < numberOfRounds; round++) {
+            for (Car car : cars) {
+                car.tryMove();
+            }
+            ResultView.printEachRound(cars);
+        }
     }
 }
