5947b0bdae08a5449934257008eb6cb0fb308e65
akayj820 <akayj820@gmail.com>
2022-04-13 20:50:13 +0900

    test: add test case in RacingCarTest

--- src/main/java/newcarracing/RacingCar.java
+++ src/main/java/newcarracing/RacingCar.java
@@ -8,7 +8,7 @@ public class RacingCar {
 
     public RacingCar(String name) {
         if(name.length() > MAX_LENGTH_OF_NAME){
-            throw new IllegalCallerException("자동차의 이름은 " + MAX_LENGTH_OF_NAME + "자를 초과할 수 없습니다.");
+            throw new IllegalArgumentException("자동차의 이름은 " + MAX_LENGTH_OF_NAME + "자를 초과할 수 없습니다.");
         }
         this.name = name;
     }
--- src/test/java/newcarracing/RacingCarTest.java
+++ src/test/java/newcarracing/RacingCarTest.java
@@ -1,9 +1,11 @@
 package newcarracing;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RacingCarTest {
     @ParameterizedTest
@@ -11,5 +13,16 @@ class RacingCarTest {
     void 경주용_자동차(String input) {
         RacingCar racingCar = new RacingCar(input);
         assertThat(racingCar.getName()).isEqualTo(input);
+    }
+
+    @Test
+    void 경주용_자동차_이름_길이_제한초과() {
+        assertThatThrownBy(() -> {
+            RacingCar racingCar = new RacingCar("carname");
+        })
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("초과")
+        ;
+
     }
 }
