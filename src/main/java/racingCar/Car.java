1a8a46a27cde623a3c53762f0197bfda8aa80b70
jinsxl <wlsthf1113@gmail.com>
2022-04-14 01:47:55 +0900

    feat: 0~9 사이의 랜덤 값

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -6,6 +6,10 @@ public class Car {
     static Random random = new Random();
     static final int BOUND = 10;
 
+    public int getRandomValue() {
+        return random.nextInt(BOUND);
+    }
+
     public boolean canMoveForward(int number) {
         return number >= 4;
     }
