c3ac9ab1be0625102b75b8f33c7180017658e8fb
imcool2551 <imcool2551@gmail.com>
2022-04-13 12:03:14 +0900

    refactor: CarFactory
    CarFactory에서 차 개수 대신 CarName리스트를 기반으로 Car리스트를 생성하도록 변경했습니다. 그에따라 테스트코드도 변경했습니다.

--- src/main/java/racingcar/domain/CarFactory.java
+++ src/main/java/racingcar/domain/CarFactory.java
@@ -1,7 +1,7 @@
 package racingcar.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarFactory {
 
@@ -9,11 +9,9 @@ public class CarFactory {
         throw new AssertionError();
     }
 
-    public static List<Car> generateCarsOfSize(int carSize) {
-        List<Car> cars = new ArrayList<>();
-        for (int i = 0; i < carSize; i++) {
-            cars.add(new Car());
-        }
-        return cars;
+    public static List<Car> generateCars(List<CarName> carNames) {
+        return carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/racingcar/domain/CarFactoryTest.java
+++ src/test/java/racingcar/domain/CarFactoryTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,12 +9,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarFactoryTest {
 
+    private static final List<CarName> CAR_NAMES =
+            List.of(new CarName("pobi"), new CarName("crong"), new CarName("honux"));
+    private static final Position DEFAULT_POSITION = new Position(0);
+
+    @DisplayName("이름 리스트와 같은 순서로 위치가 0인 차 리스트 생성")
     @Test
-    void createCars() {
-        int size = 5;
+    void generateCars() {
 
-        List<Car> cars = CarFactory.generateCarsOfSize(size);
+        List<Car> cars = CarFactory.generateCars(CAR_NAMES);
 
-        assertThat(cars).hasSize(size);
+        assertThat(cars.size()).isEqualTo(CAR_NAMES.size());
+        for (int i = 0; i < cars.size(); i++) {
+            assertThat(cars.get(i))
+                    .isEqualTo(new Car(CAR_NAMES.get(i), DEFAULT_POSITION));
+        }
     }
 }
