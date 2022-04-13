a12300e101faec552e00a09374a6e2691a84853b
wonjin <wonjin1776@naver.com>
2022-04-13 10:24:05 +0900

    2가지를 하고 있는 메소드 리펙토링 - private static int sum(String[] values)   - 합산과 숫자형으로 변환 두가지 일을 하고 있음

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -13,26 +13,47 @@ public class StringAddCalculator {
         //로컬변수 int result = 0 을 선언하고 메소드 전체 블록 안에서 사용하는 것은 올바르지 않다.
         //Early Return 을 통해 else문을 없애라.
 
-        if(text == null || text.isBlank())return 0;
+        if(isBlankText(text))return 0;
 
         Matcher m = Pattern.compile(DELIMITER_CUSTOM_REGEXP).matcher(text);
         if(m.find()){
             String customDelimiter = m.group(1);
             String[] values = m.group(2).split(customDelimiter);
-            return sum(values);
+            return sum(toIntArray(values));
         }
 
         String[] values = text.split(DELIMITER_DEFUALT_REGEXP);
-        return sum(values);
+        return sum(toIntArray(values));
     }
 
-    private static int sum(String[] values) {
+    private static boolean isBlankText(String text) {
+        return text == null || text.isBlank();
+    }
+
+    private static int sumBeforeRefactoring(String[] values) {
         int sum = 0;
         for (String value : values) {
-//            int number = toInt(value)   한 곳에서만 사용하는 지역변수는 제거할 것
+            //int number = toInt(value)   한 곳에서만 사용하는 지역변수는 제거할 것
             sum += toInt(value);
         }
         return sum;
+    }
+
+    private static int sum(int[] values) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    private static int[] toIntArray(String[] values) {
+        int[] ints = new int[values.length];
+
+        for (int i = 0; i < values.length; i++) {
+            ints[i] = toInt(values[i]);
+        }
+        return ints;
     }
 
     private static int toInt(String value) {
