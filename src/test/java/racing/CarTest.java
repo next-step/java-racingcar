5e16e5d6b5164b4c014ab74c9f3cda51bb8bc22d
양경희 <alicesister1@gmail.com>
2022-04-14 00:20:30 +0900

    test: 자동차에 글자수 제한 넘을 때 예외발생 테스트

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -4,14 +4,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racing.domain.Car;
 import racing.utils.Counter;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class CarTest {
+
+    @DisplayName("이름 5글자를 초과하면 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"Super Car", "Porsche", "Sports SUV"})
+    void carMoveForward(String name) {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Car(name));
+    }
 
     @DisplayName("전략에 따라 자동차 전진 또는 멈춤")
     @ParameterizedTest
