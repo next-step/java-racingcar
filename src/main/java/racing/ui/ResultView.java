08023d931e8ee871fcbee434a7219e26ae436018
양경희 <alicesister1@gmail.com>
2022-04-14 00:02:58 +0900

    feat: 게임 결과 우승자를 표시하고 1명 이상인 경우 쉼표로 구분하여 표시

--- src/main/java/racing/ui/ResultView.java
+++ src/main/java/racing/ui/ResultView.java
@@ -4,6 +4,8 @@ import racing.dto.GameResult;
 import racing.dto.GameResult.GameRoundResult;
 import racing.utils.Counter;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class ResultView {
@@ -22,18 +24,27 @@ public class ResultView {
     }
 
     public void printResult(GameResult result) {
+        Objects.requireNonNull(result);
         System.out.println();
         System.out.println(PRINT_RESULT_MESSAGE);
         result.getRounds().forEach(this::printRoundResult);
+        List<String> winners = result.getWinners();
+        String joinWinners = String.join(",", winners);
+        System.out.println(joinWinners + "가 최종 우승했습니다.");
     }
 
     private void printRoundResult(GameRoundResult round) {
-        round.getStatusList().forEach(this::printMark);
+        round.getCarStatus().forEach(this::printMark);
         System.out.println();
     }
 
-    private void printMark(Counter status) {
-        IntStream.range(0, status.getCount()).mapToObj(i -> DISPLAY_MARK).forEach(System.out::print);
-        System.out.println();
+    private void printMark(String name, Counter status) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(name);
+        stringBuilder.append(" : ");
+        IntStream.range(0, status.getCount())
+                .mapToObj(i -> DISPLAY_MARK)
+                .forEach(stringBuilder::append);
+        System.out.println(stringBuilder);
     }
 }
