2a284483396214d8fa0106044237ec0837a04fbe
jess <official1126@naver.com>
2022-04-14 01:14:55 +0900

    feat inputView, CarRace

--- src/main/java/CarRace/CarRace.java
+++ src/main/java/CarRace/CarRace.java
@@ -1,30 +1,23 @@
 package CarRace;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class CarRace {
     int numberOfCars;
     int numberOfRaces;
+    List<List<Integer>> raceInfoBoard;
 
-    public CarRace(int numberOfCars, int numberOfRaces) {
-        this.numberOfCars = numberOfCars;
-        this.numberOfRaces = numberOfRaces;
-    }
+    public CarRace() {}
 
-    public static void main(String[] args) {
-        CarRace carRace = inputView();
-    }
-
-
-    private static CarRace inputView() {
+    public void inputView() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("자동차 대수는 몇 대 인가요?");
-        int inputNumberOfCars = scanner.nextInt();
+        this.numberOfCars = scanner.nextInt();
 
         System.out.println("시도할 횟수는 몇 회 인가요?");
-        int inputNumberOfRaces = scanner.nextInt();
+        this.numberOfRaces = scanner.nextInt();
 
-        return new CarRace(inputNumberOfCars, inputNumberOfRaces);
+        this.raceInfoBoard = new ArrayList<>();
     }
 }
---
+++ src/main/java/CarRace/race.java
@@ -0,0 +1,11 @@
+package CarRace;
+
+import static CarRace.CarRace.*;
+
+public class race {
+
+    public static void main(String[] args) {
+        CarRace carRace = new CarRace();
+        carRace.inputView();
+    }
+}
