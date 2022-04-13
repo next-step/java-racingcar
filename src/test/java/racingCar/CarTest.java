412b39900860d7bb0ae786d8571b9784d60f1216
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2022-04-14 00:31:22 +0900

    feat 자동차 이름은 공백일 수 없습니다. 추가

--- src/test/java/racingCar/CarTest.java
+++ src/test/java/racingCar/CarTest.java
@@ -12,14 +12,14 @@ class CarTest {
   @Test
   @DisplayName("4이상인 경우에는 전진한다.")
   void onlyIfFourOrHigher() {
-    Car car = new Car(null);
+    Car car = new Car("klom");
     assertThat(car.move(() -> true)).isEqualTo(1);
   }
 
   @Test
   @DisplayName("3이하인 경우에는 멈춘다.")
   void onlyIfThreeLessThen() {
-    Car car = new Car(null);
+    Car car = new Car("klom");
     assertThat(car.move(() -> false)).isEqualTo(0);
 
   }
@@ -28,7 +28,7 @@ class CarTest {
   @Test
   @DisplayName("입력한 횟수 만큼 자동차의 미터기가 작동합니다.")
   void meterTest() {
-    Car car = new Car(null);
+    Car car = new Car("klom");
     car.move(new CarMoveRandomStrategy());
     car.move(new CarMoveRandomStrategy());
     car.move(new CarMoveRandomStrategy());
