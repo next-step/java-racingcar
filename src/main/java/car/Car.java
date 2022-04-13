f27329f777a72910c7a0c9ac6fca92dce0adae9f
rumor1993 <rumor1993@naver.com>
2022-04-13 23:51:42 +0900

    refactor: 자동차 클래스 수정
    자동차의 이름을 생성자로 받도록 변경
    move() 함수 또한 랜덤 숫자를 매개변수로 받도록 변경

--- src/main/java/car/Car.java
+++ src/main/java/car/Car.java
@@ -3,35 +3,25 @@ package car;
 import java.util.Random;
 
 public class Car {
-    private int distance;
-    private CarRacing carRacing;
+    public static final int EXCESS_NUMBERS = 4;
+    private String name;
+    private StringBuilder distance;
 
-    public Car(CarRacing carRacing) {
-        this.distance = 0;
-        this.carRacing = carRacing;
+    public Car(String name) {
+        this.distance = new StringBuilder();
+        this.name = name;
     }
 
-    public int move() {
-        if (this.randomNumber() < carRacing.getMoveMinNumber()) {
-            return this.distance;
+    public String move(int number) {
+        if (number < EXCESS_NUMBERS) {
+            return "";
         }
 
-        this.distance ++;
-        return this.distance;
+        this.distance.append("-");
+        return this.distance.toString();
     }
 
-    private int randomNumber() {
-        Random random = new Random();
-        return random.nextInt(10);
-    }
+    public void print() {
 
-    public void printDistance() {
-        for (int i = 0; i < this.distance; i++) {
-            System.out.print("-");
-        }
-    }
-
-    public boolean isMove() {
-        return this.distance > 0;
     }
 }
