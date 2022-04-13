c94098931be4b5f429325b98e93a60f41a2e78fa
양경희 <alicesister1@gmail.com>
2022-04-14 00:08:58 +0900

    test: 게임 우승자 판별 테스트

--- src/test/java/racing/RacingCarGameTest.java
+++ src/test/java/racing/RacingCarGameTest.java
@@ -3,7 +3,7 @@ package racing;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racing.domain.strategies.CarMoveStrategy;
 import racing.domain.strategies.CarMoveStrategyImpl;
 import racing.domain.strategies.CustomRandomImpl;
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class RacingCarGameTest {
 
-    @DisplayName("자동차 수를 1대 보다 작게 입력하면 예외 발생")
+    @DisplayName("자동차 이름을 입력하지 않으면 예외 발생")
     @Test
     void createGameThrowException() {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new RacingCarGame(Collections.emptyList(), createMoveStrategy()));
@@ -29,19 +29,21 @@ class RacingCarGameTest {
     }
 
     @DisplayName("이동횟수를 1 보다 작게 입력하면 예외 발생")
-    @Test
-    void runThrowException() {
-        List<String> names = List.of("carA", "carB");
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new RacingCarGame(names, createMoveStrategy()).run(0));
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void runThrowException(int numberOfMoves) {
+        RacingCarGame game = new RacingCarGame(List.of("carA", "carB"), createMoveStrategy());
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> game.run(numberOfMoves));
     }
 
-    @DisplayName("정상 입력하여 게임 실행 시 GameResult 객체가 반환된다")
+    @DisplayName("정상적으로 게임 실행 시 우승 자동차 1대 이상")
     @ParameterizedTest
-    @CsvSource(value = {"1,1", "3,5"})
-    void testGameRun(int numberOfCars, int numberOfMoves) {
+    @ValueSource(ints= {1, 5})
+    void testGameRun(int numberOfMoves) {
         List<String> names = List.of("carA", "carB");
         RacingCarGame racingCarGame = new RacingCarGame(names, createMoveStrategy());
         GameResult gameResult = racingCarGame.run(numberOfMoves);
         assertThat(gameResult).isInstanceOf(GameResult.class);
+        assertThat(gameResult.getWinners()).hasSizeGreaterThanOrEqualTo(1);
     }
 }
