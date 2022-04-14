a089434cb99163c1db8df670ab61ffa478d59356
asdfg5415 <asdfg5415@gmail.com>
2022-04-14 23:10:41 +0900

    refactor: util 성격의 클래스 제거

--- src/main/java/step3/impl/RandomStrategy.java
+++ src/main/java/step3/impl/RandomStrategy.java
@@ -1,14 +1,21 @@
 package step3.impl;
 
 import step3.model.TryStrategy;
-import step3.util.RandomIntegerGeneratorUtil;
+
+import java.util.Random;
 
 public class RandomStrategy implements TryStrategy {
 
+    private static final Random RANDOM = new Random();
+    private static final int MAX = 10;
     private static final int THRESHOLD = 4;
 
     @Override
     public boolean canGoForward() {
-        return RandomIntegerGeneratorUtil.generate() >= THRESHOLD;
+        return generate() >= THRESHOLD;
+    }
+
+    private int generate() {
+        return RANDOM.nextInt(MAX);
     }
 }
