5b2ac2e99268ae2f56e1df26b0ef212109ebd706
imcool2551 <imcool2551@gmail.com>
2022-04-14 12:54:30 +0900

    feat: List<Car> 를 일급컬렉션으로 만듬
    List<Car>를 래핑하는 일급컬렉션 Cars 클래스를 만들었습니다.
    
    RacingCarGame에서 담당하던 가장 멀리 나간 차를 구하는 역할을 이 클래스의 getMostDistantCarNames() 로 옮겼습니다. 내부구현은 getter 대신 compareTo()를 이용해서 순서를 비교하는 방식으로 했습니다.
    
    각 시도마다 자동차들의 위치를 출력하는 요구사항을 위해 getCurrentPositionsByCarName() 을 통해 차들의 이름과 위치를 Map으로 반환했습니다.

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,44 @@
+package racingcar.domain;
+
+import racingcar.domain.strategy.MoveStrategy;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        validateDuplicateName(cars);
+        this.cars = cars;
+    }
+
+    private void validateDuplicateName(List<Car> cars) {
+        Set<String> names = cars.stream()
+                .map(Car::getName)
+                .collect(Collectors.toSet());
+
+        if (names.size() != cars.size()) {
+            throw new IllegalArgumentException("cars can't have same names");
+        }
+    }
+
+    public void move(MoveStrategy strategy) {
+        cars.forEach(car -> car.move(strategy));
+    }
+
+    public Map<String, Integer> getCurrentPositionsByCarName() {
+        return cars.stream()
+                .collect(Collectors.toMap(Car::getName, Car::getPosition));
+    }
+
+    public List<String> getMostDistantCarNames() {
+        Car mostDistantCar = Collections.max(cars);
+
+        return cars.stream()
+                .filter(car -> car.compareTo(mostDistantCar) == 0)
+                .map(Car::getName)
+                .collect(Collectors.toList());
+    }
+}
