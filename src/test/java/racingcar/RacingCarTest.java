6d1a4036020210a4569a31f4abc9a05bd03de98d
yuzin9712 <dbwls97@ajou.ac.kr>
2022-04-14 22:31:06 +0900

    (test) 자동차 경주 서비스 로직 테스트 클래스 생성

---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.Car;
+import racingcar.service.RacingCar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RacingCarTest {
+
+    RacingCar racingCar;
+
+    @BeforeEach
+    void setUp() {
+        racingCar = new RacingCar(1, 1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 전진조건_4이상_9이하(int number) {
+        assertThat(racingCar.moveCondition(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 정지조건_0이상_4미만(int number) {
+        assertThat(racingCar.moveCondition(number)).isFalse();
+    }
+
+}
