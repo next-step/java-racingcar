25e288cf23c32b6c9a98bbef718e5c1af4eabe8f
Choe minho <>
2022-04-14 22:45:01 +0900

    refactor: 차량대수 및 시도횟수 입력을 static으로 분리

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -3,30 +3,24 @@ package racing.view;
 import java.util.Scanner;
 
 public class InputView {
-    public static final String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
-    public static final String ATTEMPTS_COUNT_MESSAGE = "시도할 횟수는 몇 회 인가요?";
-    private final int carCount;
-    private final int attemptsCount;
+    private static final String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String ATTEMPTS_COUNT_MESSAGE = "시도할 횟수는 몇 회 인가요?";
+    private static final Scanner SCANNER = new Scanner(System.in);
 
-    public InputView() {
-        Scanner scanner = new Scanner(System.in);
-
+    public static int inputCarCount() {
         showMessage(CAR_COUNT_MESSAGE);
-        this.carCount = scanner.nextInt();
 
-        showMessage(ATTEMPTS_COUNT_MESSAGE);
-        this.attemptsCount = scanner.nextInt();
+        return SCANNER.nextInt();
     }
 
-    private void showMessage(String carCountMessage) {
+    public static int inputAttemptsCount() {
+        showMessage(ATTEMPTS_COUNT_MESSAGE);
+
+        return SCANNER.nextInt();
+    }
+
+    private static void showMessage(String carCountMessage) {
         System.out.println(carCountMessage);
     }
 
-    public int getCarCount() {
-        return carCount;
-    }
-
-    public int getAttemptsCount() {
-        return attemptsCount;
-    }
 }
