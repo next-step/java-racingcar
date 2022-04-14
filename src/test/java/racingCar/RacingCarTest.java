98da1bd347d4f75aaa8f1a1c3992521c4c7e974b
kivv00ng <rldnd2637@naver.com>
2022-04-15 02:37:49 +0900

    step3-test추가

--- src/test/java/racingCar/RacingCarTest.java
+++ src/test/java/racingCar/RacingCarTest.java
@@ -2,8 +2,18 @@ package racingCar;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static java.lang.System.lineSeparator;
+import static java.lang.System.setIn;
+
 public class RacingCarTest {
     @Test
-    void RandomNumberTest(){
+    void GameTest(){
+        String userInput = String.format("3%s5", lineSeparator(), lineSeparator());
+        setIn(new ByteArrayInputStream(userInput.getBytes()));
+        RacingCar.main(null);
     }
 }
