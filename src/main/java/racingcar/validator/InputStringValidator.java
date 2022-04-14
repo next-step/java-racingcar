c6b91d67ec6fc4f356c47d90fa6f05bf252ce673
redkyu <hongstar2311@gmail.com>
2022-04-15 02:54:50 +0900

    fix string validator 예외처리 수정

--- src/main/java/racingcar/validator/InputStringValidator.java
+++ src/main/java/racingcar/validator/InputStringValidator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class InputStringValidator {
 
     private static final Pattern onlyNumberPattern = Pattern.compile("^[0-9]*?");
-    private static final String ERROR_MESSAGE = "잘못된 입력값 입니다";
+    private static final String WRONG_INPUT_ERROR_MESSAGE = "잘못된 입력값 입니다";
 
     public static int valid(String input) {
         Matcher matcher = onlyNumberPattern.matcher(input);
@@ -15,18 +15,14 @@ public class InputStringValidator {
             return checkPositive(input);
         }
 
-        System.out.println(ERROR_MESSAGE);
-
-        //TODO: 수정 필요
-        throw new RuntimeException();
+        throw new IllegalArgumentException(WRONG_INPUT_ERROR_MESSAGE);
     }
 
     private static int checkPositive(String input) {
         int number = Integer.parseInt(input);
 
         if(number < 0) {
-            //TODO: 수정 필요
-            throw new RuntimeException();
+            throw new IllegalArgumentException(WRONG_INPUT_ERROR_MESSAGE);
         }
 
         return number;
