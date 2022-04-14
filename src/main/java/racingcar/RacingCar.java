281874f01449d9231d459a1ddc062e61c7c5828e
guswns1659 <guswns1659@gmail.com>
2022-04-15 06:26:32 +0900

    step4 refactor: (리뷰) RacingCar의 condition 상태 제거

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -2,16 +2,13 @@ package racingcar;
 
 public class RacingCar {
     private int movement;
-    private int condition;
 
     public RacingCar() {
         this.movement = 0;
-        this.condition = 0;
     }
 
     public int move(int condition) {
-        this.condition = condition;
-        this.movement += this.condition > 3 ? 1 : 0;
+        this.movement += condition > 3 ? 1 : 0;
         return movement;
     }
 }
