4e8b29f19310aca755916bd1fc61891b1cebf26f
jshans40 <jshans40@gmail.com>
2022-04-14 18:52:46 +0900

    feat : 시도할 횟수 입력 받기

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -5,6 +5,7 @@ import view.InputView;
 public class RacingGameController {
     public static void main(String args[]) {
         InputView inputView = new InputView();
-        int inputNumber = inputView.getInputCarsNumber();
+        int carsNumber = inputView.getInputCarsNumber();
+        int carMoveCount = inputView.getInputCarMoveCount();
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private final String INPUT_CARS_NUMBERS_PRINT = "자동차 대수는 몇 대 인가요?";
+    private final String INPUT_CARS_MOVE_COUNT_PRINT = "시도할 횟수는 몇 회 인가요?";
     private final Scanner scanner;
 
     public InputView() {
@@ -18,4 +19,15 @@ public class InputView {
     private void printInputCarsNumber() {
         System.out.println(INPUT_CARS_NUMBERS_PRINT);
     }
+
+
+    public int getInputCarMoveCount() {
+        printCarMoveCount();
+        return scanner.nextInt();
+    }
+
+    private void printCarMoveCount() {
+        System.out.println(INPUT_CARS_MOVE_COUNT_PRINT);
+    }
+
 }
