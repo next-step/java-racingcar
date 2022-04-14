07b1fb252e06d7e3ab936e3f49baf2e153bfa8e8
guswns1659 <guswns1659@gmail.com>
2022-04-15 07:16:27 +0900

    step4 feat: Prompt는 입력만 요청하도록 역할 최소화

--- src/main/java/racingcar/Prompt.java
+++ src/main/java/racingcar/Prompt.java
@@ -6,13 +6,11 @@ public class Prompt {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static String[] ask(String question) {
-        Printer.print(question);
         String input;
-        input = SCANNER.nextLine();
-        if (input.isBlank()) {
-            System.out.println("입력 값은 빈 값이 안됩니다. 다시 입력 해주세요.");
+        do {
+            Printer.print(question);
             input = SCANNER.nextLine();
-        }
+        } while (InputValidator.isInputWrong(input));
         return input.split(",");
     }
 
