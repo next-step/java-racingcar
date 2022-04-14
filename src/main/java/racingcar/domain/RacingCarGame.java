5bd7e7cf7e70efcaa2b20062b79e91ef2a102275
imcool2551 <imcool2551@gmail.com>
2022-04-14 12:50:26 +0900

    refactor: RacingCarGame
    우승한 Car를 구하는 역할을 RacingCarGame 에서 빼냈습니다. 또, 우승결과를 담는 GameResult 클래스를 제거했습니다.

--- src/main/java/racingcar/domain/RacingCarGame.java
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -2,19 +2,13 @@ package racingcar.domain;
 
 import racingcar.domain.strategy.MoveStrategy;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
-
 public class RacingCarGame {
 
-    private static final Comparator<Car> CAR_POSITION_COMPARATOR = Comparator.comparing(Car::getPosition);
-    private final List<Car> cars;
+    private final Cars cars;
     private int rounds;
     private final MoveStrategy moveStrategy;
 
-    public RacingCarGame(List<Car> cars, int rounds, MoveStrategy moveStrategy) {
+    public RacingCarGame(Cars cars, int rounds, MoveStrategy moveStrategy) {
         this.cars = cars;
         this.rounds = rounds;
         this.moveStrategy = moveStrategy;
@@ -22,33 +16,13 @@ public class RacingCarGame {
 
     public void proceedRound() {
         decreaseGameRound();
-        cars.forEach(car -> car.move(moveStrategy));
+        cars.move(moveStrategy);
     }
 
     private void decreaseGameRound() {
         if (rounds <= 0) {
             throw new IllegalStateException("game is already over");
         }
-
         rounds = rounds - 1;
     }
-
-    public GameResult getResult() {
-        if (rounds > 0) {
-            throw new IllegalStateException("game is still on progress");
-        }
-
-        return new GameResult(findWinners());
-    }
-
-    private List<Car> findWinners() {
-        Car winner = cars.stream()
-                .max(CAR_POSITION_COMPARATOR)
-                .orElseThrow(NoSuchElementException::new);
-
-        return cars.stream()
-                .filter(c -> c.getPosition() == winner.getPosition())
-                .collect(Collectors.toList());
-    }
-
 }
