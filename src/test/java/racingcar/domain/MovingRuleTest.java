24e3ac5572f3b2318ab124f31c55e1ce72eafc1c
zieunx <wldms_1217@naver.com>
2022-04-15 03:01:49 +0900

    feat(step3/MovingRuleTest): 이동규칙 테스트 추가

--- src/test/java/racingcar/domain/MovingRuleTest.java
+++ src/test/java/racingcar/domain/MovingRuleTest.java
@@ -1,6 +1,5 @@
 package racingcar.domain;
 
-
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,34 +8,34 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import exception.OutOfRangeException;
 
-public class CarTest {
+public class MovingRuleTest {
 
-	@DisplayName("랜덤값 4미만으로 실행시 이동X")
+	@DisplayName("랜덤값이 4미만이면 이동하지 않는다.")
 	@ValueSource(ints = {0,1,2,3})
 	@ParameterizedTest
 	public void When_lessThan4_Expected_0(int randomNumber) {
 		// given
-		Car car = new Car();
+		RandomMovingRule randomMovingRule = new RandomMovingRule(() -> randomNumber);
 
 		// when
-		int numbersOfMove = car.run(new RandomMovingRule(() -> randomNumber));
+		boolean isMoveCar = randomMovingRule.isMoveCar();
 
 		// then
-		assertThat(numbersOfMove).isEqualTo(0);
+		assertThat(isMoveCar).isEqualTo(false);
 	}
 
-	@DisplayName("랜덤값 4이상으로 실행시 1 이동")
+	@DisplayName("랜덤값이 4이상이면 이동한다.")
 	@ValueSource(ints = {4,5,6,7,8,9})
 	@ParameterizedTest
 	public void When_moreThanOrEqual4_Expected_1(int randomNumber) {
 		// given
-		Car car = new Car();
+		RandomMovingRule randomMovingRule = new RandomMovingRule(() -> randomNumber);
 
 		// when
-		int numbersOfMove = car.run(new RandomMovingRule(() -> randomNumber));
+		boolean isMoveCar = randomMovingRule.isMoveCar();
 
 		// then
-		assertThat(numbersOfMove).isEqualTo(1);
+		assertThat(isMoveCar).isEqualTo(true);
 	}
 
 	@DisplayName("랜덤이 범위를 벗어나면 RunTimeException발생")
@@ -44,10 +43,10 @@ public class CarTest {
 	@ParameterizedTest
 	public void When_RandomNumberIsOutOfRange_Expected_RunTimeException(int randomNumber) {
 		// given
-		Car car = new Car();
+		RandomMovingRule randomMovingRule = new RandomMovingRule(() -> randomNumber);
 
 		// when, then
-		assertThatThrownBy(() -> car.run(new RandomMovingRule(() -> randomNumber)))
+		assertThatThrownBy(randomMovingRule::isMoveCar)
 			.isInstanceOf(OutOfRangeException.class);
 	}
 }
