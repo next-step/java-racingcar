0460af608f43b0c4f965381b7b4bbb4808e9760f
imcool2551 <imcool2551@gmail.com>
2022-04-14 14:55:39 +0900

    refactor: RacingGameTest
    RacingGame에서 많은 책임을 제거함에따라 필요없어진 테스트를 제거했습니다.
    테스트 메서드 이름을 더 읽기 쉽도록 변경했습니다.

--- src/test/java/racingcar/domain/RacingCarGameTest.java
+++ src/test/java/racingcar/domain/RacingCarGameTest.java
@@ -1,15 +1,12 @@
 package racingcar.domain;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.strategy.MoveStrategy;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RacingCarGameTest {
 
@@ -18,9 +15,8 @@ class RacingCarGameTest {
     private static final int ROUNDS = 5;
     private static final MoveStrategy ALWAYS_MOVE_STRATEGY = () -> true;
 
-    @DisplayName("rounds 만큼 proceedRound() 호출하면 정상동작")
     @Test
-    void proceedRound() {
+    void proceedRound_DoesNotThrowException_IfRoundsIsGreaterThanZero() {
         RacingCarGame game = createRacingCarGame();
 
         assertThatNoException().isThrownBy(() -> {
@@ -30,9 +26,8 @@ class RacingCarGameTest {
         });
     }
 
-    @DisplayName("rounds 보다 proceedRound() 많이 호출하면 예외")
     @Test
-    void proceedRound_Exception() {
+    void proceedRound_ThrowsException_IfRoundsIsEqualOrLessThanZero() {
         RacingCarGame game = createRacingCarGame();
 
         assertThatThrownBy(() -> {
@@ -42,37 +37,8 @@ class RacingCarGameTest {
         }).isInstanceOf(IllegalStateException.class);
     }
 
-    @DisplayName("경주가 끝난 후 getResult()를 통해 우승한 차의 이름을 조회할 수 있다.")
-    @Test
-    void getResult() {
-        RacingCarGame game = createRacingCarGame();
-        for (int i = 0; i < ROUNDS; i++) {
-            game.proceedRound();
-        }
-
-        List<String> winnerNames = game.getResult()
-                .getWinnerNames();
-        List<String> carNames = CAR_NAMES.stream()
-                .map(CarName::getName)
-                .collect(Collectors.toList());
-
-        assertThat(winnerNames)
-                .containsExactlyInAnyOrderElementsOf(carNames);
-    }
-
-    @DisplayName("경주 진행 중에 getResult() 호출하면 예외")
-    @Test
-    void getResult_Exception() {
-        RacingCarGame game = createRacingCarGame();
-        for (int i = 0; i < ROUNDS - 1; i++) {
-            game.proceedRound();
-        }
-
-        assertThatThrownBy(game::getResult)
-                .isInstanceOf(IllegalStateException.class);
-    }
-
     private RacingCarGame createRacingCarGame() {
-        return new RacingCarGame(CarFactory.generateCars(CAR_NAMES), ROUNDS, ALWAYS_MOVE_STRATEGY);
+        Cars cars = CarFactory.generateCars(CAR_NAMES);
+        return new RacingCarGame(cars, ROUNDS, ALWAYS_MOVE_STRATEGY);
     }
 }
