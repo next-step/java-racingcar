0549dec2a9ba90cbdc78a5ec1bd7fb8e6d615b31
pangnem <sprnd645@gmail.com>
2022-04-14 19:08:46 +0900

    test: CarsFactory 자동차 이름으로 생성

--- src/test/java/racingcar/model/factory/CarsFactoryTest.java
+++ src/test/java/racingcar/model/factory/CarsFactoryTest.java
@@ -2,19 +2,48 @@ package racingcar.model.factory;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.model.Cars;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CarsFactoryTest {
 
     @Nested
     class create_메서드는 {
 
-        @Test
-        void Cars_타입을_리턴한다() {
-            assertThat(CarsFactory.create(3))
-                    .isInstanceOf(Cars.class);
+        @Nested
+        class 숫자가_주어질_경우 {
+
+            @Test
+            void Cars_타입을_리턴한다() {
+                assertThat(CarsFactory.create(3))
+                        .isInstanceOf(Cars.class);
+            }
+        }
+
+        @Nested
+        class _5자가_넘는_자동차_이름이_주어질_경우 {
+
+            @ParameterizedTest
+            @ValueSource(strings = {"pobiconan,pang,nem"})
+            void IllegalArgumentException을_던진다(String carNames) {
+                assertThatIllegalArgumentException()
+                        .isThrownBy(() -> CarsFactory.create(carNames));
+            }
+        }
+
+        @Nested
+        class _5자이하_자동차_이름이_주어질_경우 {
+
+            @ParameterizedTest
+            @ValueSource(strings = {"pobi,pang,nem"})
+            void Cars_타입을_리턴한다(String carNames) {
+                assertThat(CarsFactory.create(carNames))
+                        .isInstanceOf(Cars.class);
+            }
         }
     }
 }
