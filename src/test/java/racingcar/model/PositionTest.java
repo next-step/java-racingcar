bb4fe72171b708e8f1ea4e2f65cfe0993d1e0538
pangnem <sprnd645@gmail.com>
2022-04-14 23:18:16 +0900

    feat: 자동차 포지션 클래스 추가

---
+++ src/test/java/racingcar/model/PositionTest.java
@@ -0,0 +1,38 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PositionTest {
+    private Position position;
+
+    @BeforeEach
+    void setUp() {
+        this.position = new Position(2);
+    }
+
+    @Nested
+    class increase_메서드는 {
+
+        @Test
+        void 증가된_Position값을_리턴한다() {
+            Position actual = position.increase();
+
+            assertThat(actual).isEqualTo(new Position(3));
+        }
+    }
+
+    @Nested
+    class toString_메서드는 {
+
+        @Test
+        void 포지션만큼의_막대기_문자열을_리턴한다() {
+            String string = position.toString();
+
+            assertThat(string).isEqualTo("--");
+        }
+    }
+}
