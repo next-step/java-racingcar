15497ba3585afeaba7888bc07560f4bb68ce28c8
kang-jisu <rkdwltn0425@gmail.com>
2022-04-15 01:38:26 +0900

    refactor: 결과 출력용 리터럴 문자 상수화
    - 개행이 OS마다 다를 수 있어서 System.lineSeparator사용

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -6,6 +6,9 @@ import racingcar.dto.RoundResult;
 import java.util.List;
 
 public final class ResultView {
+    public static final String START_LINE = "|";
+    public static final String CAR_STATE = "-";
+    public static final String NEW_LINE = System.lineSeparator();
 
     private ResultView() {
         throw new AssertionError();
@@ -20,11 +23,11 @@ public final class ResultView {
     public static void printStates(List<PositiveInteger> states) {
         StringBuilder sb = new StringBuilder();
         for (PositiveInteger state : states) {
-            sb.append("|");
+            sb.append(START_LINE);
             for (int i = 0; i < state.value(); i++) {
-                sb.append("-");
+                sb.append(CAR_STATE);
             }
-            sb.append("\n");
+            sb.append(NEW_LINE);
         }
         System.out.println(sb);
     }
