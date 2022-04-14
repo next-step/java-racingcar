4c5e43818880471781983ab6fbcdc4395bf95d18
hihisososo <hihisososo@naver.com>
2022-04-14 16:18:01 +0900

    test: RandomNumberGeneratorTest 에 RepeatedTest 사용하여 테스트 하도록 변경

--- src/test/java/racing/domain/strategy/RandomNumberGeneratorTest.java
+++ src/test/java/racing/domain/strategy/RandomNumberGeneratorTest.java
@@ -3,17 +3,14 @@ package racing.domain.strategy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 class RandomNumberGeneratorTest {
 
-  @Test
-  @DisplayName("랜덤 숫자가 생성 되는지 확인")
+  @RepeatedTest(100)
+  @DisplayName("100 회동안 랜덤 숫자가 범위내에서 잘 생성 되는지 확인")
   void randomNumberTest() {
-    int loop = 100;
-    for (int i = 0; i < loop; i++) {
       RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
       assertThat(randomNumberGenerator.generate()).isBetween(0, 9);
-    }
   }
 }
