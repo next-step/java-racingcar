7d0527af6afbcea3b1f901001255e00bf55d398e
younghun <asqwklop12@naver.com>
2022-04-15 23:39:46 +0900

    style 우승자 출력문 추가

--- src/main/java/racingCar/view/OutputTable.java
+++ src/main/java/racingCar/view/OutputTable.java
@@ -12,6 +12,7 @@ public final class OutputTable {
   private static final String WHAT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
   private static final String HOW_MANY_GAMES = "시도할 회수는 몇 회 인가요?";
   private static final String OUTPUT_RESULT = "결과: ";
+  private static final String WINNER_TEXT_PRINT = "가 최종 우승했습니다.";
 
   private OutputTable() {
   }
@@ -28,7 +29,7 @@ public final class OutputTable {
     List<Car> gameResult = game.gameResult(racingHistories, rounds);
     Winner winner = game.winner(gameResult);
     String racingGameWinner = game.coWinner(winner, gameResult);
-    System.out.println(racingGameWinner);
+    System.out.println(racingGameWinner + WINNER_TEXT_PRINT);
   }
 
   private static void historyPrint(List<Car> allCars, List<RacingCarHistory> racingHistories) {
