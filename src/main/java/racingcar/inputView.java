0b6cbfbb2904176e778e24c85528a4a77d765ee3
daseulll <nldaseul@gmail.com>
2022-04-15 22:28:54 +0900

    refactor: mainView -> inputView 클래스 이름변경

--- src/main/java/racingcar/inputView.java
+++ src/main/java/racingcar/inputView.java
@@ -2,7 +2,7 @@ package racingcar;
 
 import java.util.Scanner;
 
-public class mainView {
+public class inputView {
 
     public static void main(String[] args) {
         System.out.println("자동차 대수는 몇 대 인가요?");
