e15cc2ae3fe1b07cb391c15589ccf772b83ca79b
hihisososo <hihisososo@naver.com>
2022-04-15 18:46:49 +0900

    refactor: RacingController 에서 사용할 객체를 주입 받도록 변경

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -1,11 +1,22 @@
 package racing;
 
 import racing.controller.RacingController;
+import racing.domain.strategy.MoveStrategy;
+import racing.domain.strategy.NumberCompareMoveStrategy;
+import racing.domain.strategy.RandomNumberGenerator;
+import racing.view.RacingInputView;
+import racing.view.RacingOutputView;
 
 public class RacingGame {
 
   public static void main(String[] args) {
-    new RacingController().startGame();
+    RacingInputView racingInputView = new RacingInputView();
+    RacingOutputView racingOutputView = new RacingOutputView();
+    MoveStrategy moveStrategy = new NumberCompareMoveStrategy(new RandomNumberGenerator());
+
+    RacingController racingController = new RacingController(racingInputView, racingOutputView,
+        moveStrategy);
+    racingController.startGame();
   }
 
 }
--- src/main/java/racing/controller/RacingController.java
+++ src/main/java/racing/controller/RacingController.java
@@ -1,27 +1,34 @@
 package racing.controller;
 
 import racing.domain.Cars;
-import racing.domain.strategy.NumberCompareMoveStrategy;
-import racing.domain.strategy.RandomNumberGenerator;
+import racing.domain.strategy.MoveStrategy;
 import racing.view.RacingInputView;
 import racing.view.RacingOutputView;
 
 public class RacingController {
 
+  private final RacingInputView racingInputView;
+  private final RacingOutputView racingOutputView;
+  private final MoveStrategy moveStrategy;
+
+  public RacingController(RacingInputView racingInputView,
+      RacingOutputView racingOutputView, MoveStrategy moveStrategy) {
+    this.racingInputView = racingInputView;
+    this.racingOutputView = racingOutputView;
+    this.moveStrategy = moveStrategy;
+  }
+
   public void startGame() {
-    RacingInputView racingInputView = new RacingInputView();
     int carCount = racingInputView.getRacingCarCount();
     int attemptCount = racingInputView.getRacingAttemptCount();
 
-    RacingOutputView racingOutputView = new RacingOutputView();
     racingOutputView.printResultTitle();
 
-    Cars cars = Cars.newInstance(carCount, new NumberCompareMoveStrategy(new RandomNumberGenerator()));
+    Cars cars = Cars.newInstance(carCount, moveStrategy);
 
     for (int i = 0; i < attemptCount; i++) {
       cars.attempt();
       racingOutputView.printCarDistance(cars);
     }
   }
-
 }
