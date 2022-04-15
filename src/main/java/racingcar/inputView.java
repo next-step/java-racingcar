3c2aa082453d181d2780096dfdb621ff33e7ac77
daseulll <nldaseul@gmail.com>
2022-04-15 23:17:49 +0900

    feat: 숫자 이외의 값 입력 시, 안내문구와 함께 게임 재시작

--- src/main/java/racingcar/inputView.java
+++ src/main/java/racingcar/inputView.java
@@ -1,9 +1,19 @@
 package racingcar;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class inputView {
     public static Input entry() {
+        try {
+            return scanInput();
+        } catch (InputMismatchException e) {
+            System.out.println("숫자 이외의 문자열은 입력할 수 없습니다. 다시 입력해주세요.");
+            return scanInput();
+        }
+    }
+
+    private static Input scanInput() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         Scanner scanner = new Scanner(System.in);
         int numOfCar = scanner.nextInt();
@@ -11,7 +21,6 @@ public class inputView {
         System.out.println("시도할 횟수는 몇 회 인가요?");
         int numOfRound = scanner.nextInt();
         scanner.close();
-
         return new Input(numOfCar, numOfRound);
     }
 
