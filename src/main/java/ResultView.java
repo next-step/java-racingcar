6c669a48c4e41f6a13385ce4797b34f044f6b462
daakludens <heejahn713@gmail.com>
2022-04-16 00:24:41 +0900

    feat : 자동차 경주 로직, 자동차 경주 결과 출력

---
+++ src/main/java/ResultView.java
@@ -0,0 +1,14 @@
+public class ResultView {
+
+    public void printRaceStart() {
+        System.out.println("실행 결과");
+    }
+
+    public void printEndOfTurn() {
+        System.out.println();
+    }
+
+    public void printResultOfTurn(StringBuilder result) {
+        System.out.println(result);
+    }
+}
