19a5a43ef6af12d83f6e343c4f665821d56fa373
younlll <youn.lee96@gmail.com>
2022-04-16 15:10:33 +0900

    feat: change to enter car name

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,8 +3,9 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String NUMBER_OF_CARS_PROMPT = "자동차 대수는 몇 대 인가요?";
+    private static final String NAME_OF_THE_CARS_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)을 기준으로 구분).";
     private static final String RACING_TIMES_PROMPT = "시도할 회수는 몇 회 인가요?";
+    private static final String BASIC_SPLIT_REGEX = ",";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
@@ -12,9 +13,13 @@ public class InputView {
         throw new AssertionError();
     }
 
-    public static int inputNumberOfCars() {
-        System.out.println(NUMBER_OF_CARS_PROMPT);
-        return validateInputValue(SCANNER.nextInt());
+    public static String[] inputNumberOfCars() {
+        System.out.println(NAME_OF_THE_CARS_PROMPT);
+        String[] carNames = separateInputCars(SCANNER.nextLine());
+
+        validateInputValue(carNames);
+
+        return carNames;
     }
 
     public static int inputNumberOfAttempts() {
@@ -22,11 +27,26 @@ public class InputView {
         return SCANNER.nextInt();
     }
 
-    public static int validateInputValue(int value) {
+    public static String[] separateInputCars(String value) {
+        return value.split(BASIC_SPLIT_REGEX);
+    }
+
+    public static void validateInputValue(String[] values) {
+        checkTheNumberOfInputCars(values.length);
+        for (String value : values) {
+            checkLengthCarName(value);
+        }
+    }
+
+    public static void checkTheNumberOfInputCars(int value) {
         if (value <= 0) {
             throw new IllegalStateException("자동차는 1대 이상 입력되어야 합니다.");
         }
+    }
 
-        return value;
+    public static void checkLengthCarName(String value) {
+        if(value.length() > 5) {
+            throw new IllegalStateException("자동차의 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 }
---
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -0,0 +1,37 @@
+package racingcar.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewTest {
+
+    @Test
+    @DisplayName("입력받은 자동차 이름들을 콤마(,)로 자르기")
+    void separatorOfInputCarNames() {
+        assertThat(InputView.separateInputCars("pobi,crong,honux")).contains("pobi", "crong", "honux");
+    }
+
+    @Test
+    @DisplayName("입력된 자동차의 갯수가 1 미만인 경우 예외발생")
+    void checkTheNumberOfEnteredCars() {
+        int inputCarsSize = 0;
+
+        assertThatThrownBy(() -> InputView.checkTheNumberOfInputCars(inputCarsSize))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("자동차는 1대 이상 입력되어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("입력된 자동차의 이름의 길이가 5자 초과인 경우 예외 발생")
+    void checkNameIsLongerThanFiveCharacters() {
+        String carName = "pobipobi";
+
+        assertThatThrownBy(() -> InputView.checkLengthCarName(carName))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("자동차의 이름은 5자를 초과할 수 없습니다.");
+    }
+
+}
