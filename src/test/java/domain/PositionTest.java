f8c745875f78f60e3c62537f0935d8c880248d50
chkim <cndghks0213@gmail.com>
2022-04-17 07:48:24 +0900

    test: 어노테이션 오타 수정

--- src/test/java/domain/PositionTest.java
+++ src/test/java/domain/PositionTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class PositionTest {
 
     @DisplayName("포지션 이동 테스트")
-    @Test()
+    @Test
     void goForwardTest() {
         Position position = new Position();
         int beforePosition = position.getPosition();
