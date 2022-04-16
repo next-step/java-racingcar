dfd11b928d7fbe315dcf321f1ae8ccd0127a7219
imcool2551 <imcool2551@gmail.com>
2022-04-16 22:35:23 +0900

    refactor: RacingCarGame
    최소 라운드로 사용하던 숫자0 을 정적상수로 만들었습니다.
    경주가 끝났는지 판별하는 isFinished() 메서드를 생성자 유효성 검사와 moveCars() 메서드에서 재사용했습니다.

--- src/main/java/racingcar/domain/RacingCarGame.java
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -4,32 +4,35 @@ import racingcar.domain.strategy.MoveStrategy;
 
 public class RacingCarGame {
 
+    private static final int MINIMUM_ROUND = 0;
+
     private final Cars cars;
     private int rounds;
     private final MoveStrategy moveStrategy;
 
     public RacingCarGame(Cars cars, int rounds, MoveStrategy moveStrategy) {
         this.cars = cars;
-        this.rounds = validateRounds(rounds);
+        this.rounds = rounds;
         this.moveStrategy = moveStrategy;
+        validateRounds();
     }
 
-    private int validateRounds(int rounds) {
-        if (rounds <= 0) {
+    private void validateRounds() {
+        if (isFinished()) {
             throw new IllegalArgumentException("rounds must be greater than 0");
         }
-        return rounds;
-    }
-
-    public boolean isFinished() {
-        return rounds == 0;
     }
 
     public void moveCars() {
-        if (rounds <= 0) {
+        if (isFinished()) {
             throw new IllegalStateException("game is already over");
         }
         rounds = rounds - 1;
         cars.move(moveStrategy);
     }
+
+    public boolean isFinished() {
+        return rounds <= MINIMUM_ROUND;
+    }
+
 }
