024fe7600be181caa5c1c6f8566f0b4181ae9a1a
wlroh <wlroh.paul@gmail.com>
2022-04-16 23:25:40 +0900

    feat: 자동차 이름을 받을 수 있도록 InputView 뷰 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String MESSAGE_INPUT_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
+    private static final String MESSAGE_INPUT_CAR_COUNT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)";
     private static final String MESSAGE_INPUT_ROUND_COUNT = "시도할 횟수는 몇 회 인가요?";
     private static final Scanner scanner = new Scanner(System.in);
 
