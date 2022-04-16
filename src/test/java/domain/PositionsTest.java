3f0daef8c3083f319a4550406b470850115b6f24
chkim <cndghks0213@gmail.com>
2022-04-17 07:47:27 +0900

    test: Positions 테스트 리팩터링

--- src/test/java/domain/PositionsTest.java
+++ src/test/java/domain/PositionsTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,12 +13,16 @@ class PositionsTest {
 
     private List<Position> positionList;
 
+    @DisplayName("Positions가 정상적으로 생성되는지 테스트")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 5, 10, 50, 100})
     void getPositionsTest(int count) {
         init(count);
         Positions positions = new Positions(positionList);
         assertThat(positions.getPositions()).hasSize(count);
+        for (int position : positions.getPositions()) {
+            assertThat(position).isEqualTo(0);
+        }
     }
 
     void init(int count) {
