8f72e946dd40ac86649d20e03e0d6a3929ee43e5
suwampy <tndhks123@nate.com>
2022-04-18 01:58:01 +0900

    refator: matcher로 만들어진 group이 어떤 그룹을 나타내는지 상수로 선언

--- src/main/java/calculator/StringSplit.java
+++ src/main/java/calculator/StringSplit.java
@@ -6,6 +6,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringSplit {
+    private static final int CUSTOM_DELIMITER = 1;
+    private static final int TEXT = 2;
     public static Matcher getCustomDelimiterMatcher(String text) {
         return Pattern.compile(StringCalculatorDelimiter.CUSTOM_DELIMITER_REGEXP).matcher(text);
     }
@@ -13,8 +15,7 @@ public class StringSplit {
     public static String[] split(String text) {
         Matcher matcher = getCustomDelimiterMatcher(text);
         if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            return matcher.group(2).split(customDelimiter);
+            return matcher.group(TEXT).split(matcher.group(CUSTOM_DELIMITER));
         }
         return text.split(StringCalculatorDelimiter.DEFAULT_DELIMITER);
     }
