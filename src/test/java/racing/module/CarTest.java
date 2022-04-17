4d5a86b7acfdc083ef5c3d27d368595bd92931d4
hwangdongho <ygekl8523@naver.com>
2022-04-18 00:34:14 +0900

    test: 자동차이름 확인테스트추가

--- src/test/java/racing/module/CarTest.java
+++ src/test/java/racing/module/CarTest.java
@@ -1,5 +1,7 @@
 package racing.module;
 
+import exception.CustomException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +10,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CarTest {
     private final MoveStrategy alwaysTrueStrategy = () -> true;
     private final MoveStrategy alwaysFalseStrategy = () -> false;
-    private final Car car = new Car("test");
+    private Car car;
+
+    @BeforeEach
+    void setUp() throws CustomException {
+        car = new Car("test");
+    }
 
     @Test
     @DisplayName("자동차가 움직이지 않는지 확인")
@@ -29,5 +36,11 @@ class CarTest {
     void onlyMove() {
         car.move();
         assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 맞는지 확인")
+    void checkCarName() {
+        assertThat(car.getCarName()).isEqualTo("test");
     }
 }
