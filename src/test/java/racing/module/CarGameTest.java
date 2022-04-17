6da7f330e95b171d0c18aff5ae328c295a3c179c
hwangdongho <ygekl8523@naver.com>
2022-04-18 00:54:27 +0900

    test: CarName 클래스 테스트 추가

--- src/test/java/racing/module/CarGameTest.java
+++ src/test/java/racing/module/CarGameTest.java
@@ -1,7 +1,13 @@
 package racing.module;
 
+import exception.CustomException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,14 +17,14 @@ class CarGameTest {
 
     @Test
     @DisplayName("대수가 맞게 생성되었는지 확인")
-    void carGame() {
+    void carGame() throws CustomException {
         CarGame carGame = new CarGame("a,b,c");
         assertThat(carGame.getCarList().size()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("생성된 자동차들이 모두 움직였는지 확인")
-    void alwayMovingplay() {
+    void alwayMovingplay() throws CustomException {
         CarGame carGame = new CarGame("a,b,c");
         carGame.play(alwaysTrueStrategy);
         for (Car car : carGame.getCarList()) {
@@ -29,12 +35,44 @@ class CarGameTest {
 
     @Test
     @DisplayName("생성된 자동차들이 모두 안움직였는지 확인")
-    void alwayStayplay() {
+    void alwayStayplay() throws CustomException {
         CarGame carGame = new CarGame("a,b,c");
         carGame.play(alwaysFalseStrategy);
         for (Car car : carGame.getCarList()) {
             assertThat(car.getPosition()).isEqualTo(0);
 
         }
+    }
+
+    @Test
+    @DisplayName("제일 많이 움직인 위치가 맞는지 확인")
+    void getMaxPosition() throws CustomException {
+        CarGame carGame = new CarGame("a,b,c");
+        List<Car> carList = carGame.getCarList();
+        for (Car car : carList) {
+            if(Objects.equals(car.getCarName(), "a")){
+                car.move();
+                car.move();
+            }
+
+            if(Objects.equals(car.getCarName(), "b")){
+                car.move();
+            }
+        }
+        assertThat(carGame.getMaxPosition()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("우승자가 맞는지 확인")
+    void getWinner() throws CustomException {
+        CarGame carGame = new CarGame("a,b,c");
+        List<Car> carList = carGame.getCarList();
+        for (Car car : carList) {
+            if(Objects.equals(car.getCarName(), "a")){
+                car.move();
+            }
+        }
+        assertThat(carGame.getWinner().size()).isEqualTo(1);
+        assertThat(carGame.getWinner().get(0).getCarName()).isEqualTo("a");
     }
 }
---
+++ src/test/java/racing/module/CarNameTest.java
@@ -0,0 +1,34 @@
+package racing.module;
+
+import exception.CustomException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarNameTest {
+    @Test
+    @DisplayName("이름이 맞게 생성되었는지 확인")
+    void getName() throws CustomException {
+        CarName carName = new CarName("test");
+        assertThat(carName.getName()).isEqualTo("test");
+    }
+
+    @Test
+    @DisplayName("이름이 공백일때 Exception확인")
+    void blankName() {
+        assertThatThrownBy(() -> {
+            CarName carName = new CarName("");
+        }).isInstanceOf(CustomException.class).hasMessageContaining("공백 자동차 이름은 허용하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("이름이 5글자가 넘을때 Exception확인")
+    void overFiveLetterName() {
+        assertThatThrownBy(() -> {
+            CarName carName = new CarName("test66");
+        }).isInstanceOf(CustomException.class).hasMessageContaining(String.format("자동차 이름이 적합하지 않습니다. 이름 : %s", "test66"));
+    }
+}
