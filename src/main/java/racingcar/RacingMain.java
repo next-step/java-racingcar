adf38af9c9ece22133353eb2f2ded8f093028fa2
pangnem <sprnd645@gmail.com>
2022-04-17 23:28:30 +0900

    refactor: cars 생성 RacingGame으로 이동

--- src/main/java/racingcar/RacingMain.java
+++ src/main/java/racingcar/RacingMain.java
@@ -2,23 +2,23 @@ package racingcar;
 
 import racingcar.controller.RacingGame;
 import racingcar.model.Cars;
-import racingcar.model.factory.CarsFactory;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingMain {
     public static void main(String[] args) {
         String carNames = InputView.inputCarNames();
-        Cars cars = CarsFactory.create(carNames);
-
         int tryCount = InputView.inputTryCount();
-        RacingGame racingGame = new RacingGame(tryCount);
 
+        RacingGame racingGame = new RacingGame(tryCount, carNames);
+
+        Cars cars = null;
         while (!racingGame.end()) {
-            racingGame.race(cars);
+            cars = racingGame.race();
             OutputView.outputStatus(cars);
         }
 
+        assert cars != null;
         Cars winnerCars = cars.findWinners();
         OutputView.outputWinners(winnerCars);
     }
--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,23 +1,33 @@
 package racingcar.controller;
 
 import racingcar.model.Cars;
+import racingcar.model.factory.CarsFactory;
 
 public class RacingGame {
+    private static final String DEFAULT_CAR_NAMES = "car";
     private final int tryCount;
+    private final Cars cars;
 
     private int movedCount = 0;
 
     public RacingGame(int tryCount) {
+        this(tryCount, DEFAULT_CAR_NAMES);
+    }
+
+    public RacingGame(int tryCount, String carNames) {
         this.tryCount = tryCount;
+        this.cars = CarsFactory.create(carNames);
     }
 
     public boolean end() {
         return this.movedCount >= this.tryCount;
     }
 
-    public void race(Cars cars) {
-        cars.move();
+    public Cars race() {
+        this.cars.move();
         increaseMovedCount();
+
+        return this.cars;
     }
 
     private synchronized void increaseMovedCount() {
