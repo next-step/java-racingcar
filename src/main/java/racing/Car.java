e72e6aa29842e534f71fa8b68d23aff6cf199c5e
minigear <coosac@gmail.com>
2022-04-17 19:46:44 +0900

    [refactor][3단계 - 차량 최소 이동 회수 추가

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -6,12 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Car {
+    private static final int MIN_NUMBER_OF_MOVES = 1;
     private final List<Boolean> moves;
     private final int moveCount;
 
     private final Engine engine;
 
     public Car(Engine engine, int moveCount) {
+        if (moveCount < MIN_NUMBER_OF_MOVES) {
+            throw new IllegalArgumentException();
+        }
         this.engine = engine;
         this.moves = new ArrayList<>();
         this.moveCount = moveCount;
