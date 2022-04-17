c519d57db816db65d3f24c93f157d5e746611833
suwampy <tndhks123@nate.com>
2022-04-18 01:52:44 +0900

    test: 반복적인 테스트는 @ParameterizedTest를 활용하여 작성할 수 있도록 리팩토링

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -2,6 +2,8 @@ package calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -31,17 +33,11 @@ class StringAddCalculatorTest {
         assertThat(result).isEqualTo(3);
     }
 
-    @Test
-    @DisplayName("구분자를 컴마 이외에 콜론으로 사용할 경우에도 구분된 수들의 합을 출력한다.")
-    void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
-        assertThat(result).isEqualTo(6);
-    }
-
-    @Test
-    @DisplayName("//n과 \n 문자 사이에 커스텀 구분자를 사용할 경우 구분된 수들의 합을 출력한다.")
-    void splitAndSum_custom_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2:3", "//;\n1;2;3"})
+    @DisplayName("구분자와 커스텀 문자를 사용하여 구분된 수들의 합을 출력한다.")
+    void splitAndSum_쉼표_또는_콜론_구분자_커스텀_문자(String input) throws Exception {
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(6);
     }
 
