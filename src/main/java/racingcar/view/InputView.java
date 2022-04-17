2870ba6b69e022d5cdbf269e9357ff06ea3204c1
pangnem <sprnd645@gmail.com>
2022-04-17 22:22:39 +0900

    refactor: 미사용 메서드 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,18 +4,11 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String INPUT_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
     private static final String INPUT_CAR_MOVE_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 
     private InputView() throws InstantiationException {
         throw new InstantiationException();
-    }
-
-    public static int inputCarCount() {
-        System.out.println(INPUT_CAR_COUNT_MESSAGE);
-
-        return inputInt();
     }
 
     public static int inputCarMoveCount() {
