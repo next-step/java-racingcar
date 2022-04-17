f5b7cf58ec13149b39d7d0f29d4d784b97882a50
daseulll <nldaseul@gmail.com>
2022-04-17 17:11:51 +0900

    refactor: RacingCarGameTest에서 mock사용하지 않고 테스트 수행하도록 수정

--- src/test/java/racingcar/RacingCarGameTest.java
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -4,20 +4,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.*;
-
 
 public class RacingCarGameTest {
     @Test
-    @DisplayName("랜덤숫자_조건_만족시_전진")
-    void proceedCarWhenRandomNumConditionSatisfied() {
+    @DisplayName("전진_조건_만족시_한칸_전진")
+    void proceedCarWhenConditionSatisfied() {
         int numOfRound = 3;
         RacingCar[] racingCars = {new RacingCar(), new RacingCar(), new RacingCar()};
 
-        RandomNumCondition condition = spy(new RandomNumCondition());
-        doReturn(true).when(condition).isSatisfied();
-
-        RacingCarGame racingCarGame = new RacingCarGame(racingCars, numOfRound, condition);
+        RacingCarGame racingCarGame = new RacingCarGame(racingCars, numOfRound, new SatisfiedCondition());
         racingCarGame.start();
 
         Assertions.assertThat(racingCars[0].getLocation().getX()).isEqualTo(3);
@@ -26,19 +21,28 @@ public class RacingCarGameTest {
     }
 
     @Test
-    @DisplayName("랜덤숫자_조건_만족시키지_못했을경우_전진하지_않음")
-    void notProceedCarWhenRandomNumConditionDisSatisfied() {
+    @DisplayName("전진_조건_만족시키지_못했을경우_전진하지_않음")
+    void notProceedCarWhenConditionDisSatisfied() {
         int numOfRound = 3;
         RacingCar[] racingCars = {new RacingCar(), new RacingCar(), new RacingCar()};
 
-        RandomNumCondition condition = spy(new RandomNumCondition());
-        doReturn(false).when(condition).isSatisfied();
-
-        RacingCarGame racingCarGame = new RacingCarGame(racingCars, numOfRound, condition);
+        RacingCarGame racingCarGame = new RacingCarGame(racingCars, numOfRound, new DisSatisfiedCondition());
         racingCarGame.start();
 
         Assertions.assertThat(racingCars[0].getLocation().getX()).isEqualTo(0);
         Assertions.assertThat(racingCars[1].getLocation().getX()).isEqualTo(0);
         Assertions.assertThat(racingCars[2].getLocation().getX()).isEqualTo(0);
+    }
+
+    private class SatisfiedCondition implements Condition {
+        public boolean isSatisfied() {
+            return true;
+        }
+    }
+
+    private class DisSatisfiedCondition implements Condition {
+        public boolean isSatisfied() {
+            return false;
+        }
     }
 }
