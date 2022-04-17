f5df8e5354e17562ddad3e566ba5cd020ae71e8b
0319easy <0319easy@gmail.com>
2022-04-17 19:04:42 +0900

    feat random 함수를 이용한 자동차 움직임 구현

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,6 +3,23 @@ package racingcar;
 import java.util.Random;
 
 public class RacingCar {
+    private static final int MOVE_CONDITION = 4;
+    private static final int MOVE_UNIT = 1;
+    private int position;
+
+    RacingCar() {
+        position = 0;
+    }
+
+    public int moveOrStop() {
+        int r = random();
+        if (r >= MOVE_CONDITION) {
+            position += MOVE_UNIT;
+        }
+
+        return position;
+    }
+
     public static int random() {
         return new Random().nextInt(10);
     }
---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+    RacingCar racingCar;
+
+    @BeforeEach
+    void setUp() {
+        racingCar = new RacingCar();
+    }
+
+    @Test
+    void 전진조건_테스트() {
+        assertThat(RacingCar.random()).isGreaterThanOrEqualTo(0)
+                .isLessThanOrEqualTo(9);
+    }
+
+    @Test
+    void 이동_또는_정지_테스트() {
+        assertThat(racingCar.moveOrStop()).isGreaterThanOrEqualTo(0);
+    }
+}
