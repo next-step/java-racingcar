6065ad497f4959bde9761e39f6a8cdc9b39d8e16
daseulll <nldaseul@gmail.com>
2022-04-17 10:56:49 +0900

    refactor: Condition.isSatisfied 메서드 간결화 및 리네이밍

--- src/main/java/racingcar/RandomNumCondition.java
+++ src/main/java/racingcar/RandomNumCondition.java
@@ -7,10 +7,6 @@ public class RandomNumCondition implements Condition {
     @Override
     public boolean isSatisfiedBy() {
         int randomNum = ThreadLocalRandom.current().nextInt(0, 9);
-
-        if (randomNum >= 4) {
-            return true;
-        }
-        return false;
+        return randomNum >= 4;
     }
 }
