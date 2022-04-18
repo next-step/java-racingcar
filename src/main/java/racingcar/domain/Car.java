6daa027a990d9b1c81ea739ca3db62701f449d10
jisu <jisuuoo96@gmail.com>
2022-04-18 19:43:23 +0900

    refactor : 코드 컨벤션을 적용하여 순서 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,8 +1,9 @@
 package racingcar.domain;
 
 public class Car {
-    private int movement = 0;
+
     private static final int BOUNDARYVALUE= 4;
+    private int movement = 0;
 
     public int moveOrStop(int value) {
         if (value >= BOUNDARYVALUE) {
