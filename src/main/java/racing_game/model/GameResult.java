f939786fb1c6a0093b1f499d68916b76cf02e05c
river.water.sea@gmail.com <river.water.sea@gmail.com>
2022-04-19 03:27:51 +0900

    fix: depth 요구사항 적용

--- src/main/java/racing_game/model/GameResult.java
+++ src/main/java/racing_game/model/GameResult.java
@@ -1,36 +1,29 @@
 package racing_game.model;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class GameResult {
 
-    private final List<List<String>> gameResult;
+    private final List<Map<String, String>> gameResult;
 
     public GameResult() {
         this.gameResult = new ArrayList<>();
     }
 
-    public List<List<String>> getGameResult() {
+    public List<Map<String, String>> getGameResult() {
         return this.gameResult;
     }
 
     public void record(RoundResult roundResult) {
-        this.gameResult.add(new ArrayList<>(roundResult.getRoundResult().values()));
+        this.gameResult.add(roundResult.getRoundResult());
 
         if (gameResult.size() > 1) {
-            int beforeLastIndex = this.gameResult.size() - 2;
-            int afterLastIndex = this.gameResult.size() - 1;
-            List<String> beforeLast = this.gameResult.get(beforeLastIndex);
-            List<String> afterLast = this.gameResult.get(afterLastIndex);
-
-            List<String> updateAfterLast = IntStream.range(0, beforeLast.size())
-                    .mapToObj(racingCarCount -> beforeLast.get(racingCarCount) + afterLast.get(racingCarCount))
-                    .collect(Collectors.toList());
-
-            gameResult.set(afterLastIndex, updateAfterLast);
+            this.update();
         }
     }
 
@@ -42,5 +35,18 @@ public class GameResult {
             IntStream.range(minIndex, maxIndex)
                     .forEach(index -> this.gameResult.add(gameResult.getGameResult().get(index)));
         }
+    }
+
+    private void update() {
+        int beforeLastIndex = this.gameResult.size() - 2;
+        int afterLastIndex = this.gameResult.size() - 1;
+        Map<String, String> beforeLast = this.gameResult.get(beforeLastIndex);
+        Map<String, String> afterLast = this.gameResult.get(afterLastIndex);
+
+        Map<String, String> update = afterLast
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> beforeLast.get(entry.getKey()) + entry.getValue(), (l, r) -> l, LinkedHashMap::new));
+        gameResult.set(afterLastIndex, update);
     }
 }
--- src/main/java/racing_game/view/ResultView.java
+++ src/main/java/racing_game/view/ResultView.java
@@ -2,11 +2,19 @@ package racing_game.view;
 
 import racing_game.model.GameResult;
 
+import java.util.Comparator;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
+    private static final String BLANK = "";
     private static final String EXECUTE_RESULT_MESSAGE = "실행 결과";
+    private static final BinaryOperator<String> COMBINE = (left, right) -> left + " : " + right;
+    private static final BinaryOperator<String> NEW_LINE = (before, after) -> before += ("\n" + after);
 
     private ResultView() {
     }
@@ -17,10 +25,26 @@ public class ResultView {
 
     public static void printResult(GameResult gameResult) {
         printMessage(EXECUTE_RESULT_MESSAGE);
+        combinedResult(gameResult);
+        findWinner(gameResult);
+    }
+
+    private static void combinedResult(GameResult gameResult) {
         gameResult.getGameResult().forEach(result -> {
-            AtomicReference<String> thisRoundResult = new AtomicReference<>("");
-            result.forEach(r -> thisRoundResult.getAndAccumulate(r, (before, after) -> before += ("\n" + after)));
+            AtomicReference<String> thisRoundResult = new AtomicReference<>(BLANK);
+            result.forEach((name, forward) -> thisRoundResult.getAndAccumulate(COMBINE.apply(name, forward), NEW_LINE));
             printMessage(thisRoundResult.get());
         });
+    }
+
+    private static void findWinner(GameResult gameResult) {
+        int finalRoundNumber = gameResult.getGameResult().size() - 1;
+        String winner = gameResult.getGameResult().get(finalRoundNumber)
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByKey())
+                .orElseThrow()
+                .getKey();
+        printMessage("\n" + winner);
     }
 }
