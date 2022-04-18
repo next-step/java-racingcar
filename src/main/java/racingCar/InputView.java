aa3a13cc279653e2c86ec9ca160a0cc59dfee858
jinsxl <wlsthf1113@gmail.com>
2022-04-19 01:44:52 +0900

    refactor: view에서 비즈니스 로직 분리

--- src/main/java/racingCar/InputView.java
+++ src/main/java/racingCar/InputView.java
@@ -5,18 +5,9 @@ import java.util.Scanner;
 public class InputView {
     static Scanner scanner = new Scanner(System.in);
 
-    public static int getInput(String message) {
+    public static String getInput(String message) {
         System.out.println(message);
 
-        return toInt(scanner.nextLine());
-    }
-
-    public static int toInt(String input) {
-        int number = Integer.parseInt(input);
-        if (number < 0) {
-            throw new RuntimeException();
-        }
-
-        return number;
+        return scanner.nextLine();
     }
 }
--- src/main/java/racingCar/RacingCar.java
+++ src/main/java/racingCar/RacingCar.java
@@ -2,8 +2,8 @@ package racingCar;
 
 public class RacingCar {
     public static void main(String[] args) {
-        int numberOfCars = InputView.getInput("자동차 대수를 입력해주세요.");
-        int numberOfRounds = InputView.getInput("횟수를 입력해주세요.");
+        int numberOfCars = new PositiveNumber(InputView.getInput("자동차 대수를 입력해주세요.")).getValue();
+        int numberOfRounds = new PositiveNumber(InputView.getInput("횟수를 입력해주세요.")).getValue();
 
         Car[] cars = new Car[numberOfCars];
         for (int i = 0; i < cars.length; i++) {
