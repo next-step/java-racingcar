191b9f87e58ede591341c2576dd90e80c407b53a
asdfg5415 <asdfg5415@gmail.com>
2022-04-19 01:58:03 +0900

    refactor: ResultView

--- src/main/java/step3/view/ResultView.java
+++ src/main/java/step3/view/ResultView.java
@@ -1,50 +1,19 @@
 package step3.view;
 
-import step3.model.EachTryResult;
-import step3.model.GameResult;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import step3.view.dto.GameResultDto;
 
 import static java.lang.System.out;
 
 public class ResultView {
 
     private static final String START = "[START]";
-    private static final String DISPLAY_CHARACTER = ">";
-    private static final String LINE_BREAK_PER_CAR = "\n";
-    private static final String LINE_BREAK_PER_EACH_TRY = "----------";
     private static final String END = "[END]";
 
 
-    public void printResult(GameResult gameResult) {
+    public void printResult(GameResultDto gameResultDto) {
         show(START);
-        showGameResult(gameResult);
+        show(gameResultDto.toString());
         show(END);
-    }
-
-    private void showGameResult(GameResult gameResult) {
-        List<EachTryResult> eachTryResults = gameResult.getEachTryResults();
-        eachTryResults.stream()
-                .map(this::parseToString)
-                .forEach(this::showEachResult);
-    }
-
-    private String parseToString(EachTryResult eachTryResult) {
-        List<Integer> moveCounts = eachTryResult.getMoveCounts();
-        return moveCounts.stream()
-                .map(DISPLAY_CHARACTER::repeat)
-                .collect(Collectors.joining(LINE_BREAK_PER_CAR));
-    }
-
-    private void showEachResult(String eachResult) {
-        show(eachResult);
-        lineBreak();
-    }
-
-
-    private void lineBreak() {
-        show(LINE_BREAK_PER_EACH_TRY);
     }
 
     private void show(String output) {
