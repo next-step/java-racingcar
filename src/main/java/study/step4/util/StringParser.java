0b79a80b9914f28a3d5797d03b55cd8a64cb7d5a
bluewow <dogseen@gmail.com>
2022-04-18 23:44:31 +0900

    refactor : 생성자 제한

--- src/main/java/study/step4/util/StringParser.java
+++ src/main/java/study/step4/util/StringParser.java
@@ -1,6 +1,9 @@
 package study.step4.util;
 
 public class StringParser {
+    private StringParser() {
+    }
+
     public static String[] impl(String input, String delimiter) {
         return input.split(delimiter);
     }
