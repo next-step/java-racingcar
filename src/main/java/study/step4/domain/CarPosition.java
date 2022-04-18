b1493f44b1ecde3b6c3629a1316bb2fdf0fb2527
bluewow <dogseen@gmail.com>
2022-04-18 23:39:21 +0900

    refactor : 정적 팩토리 메서드 처리

--- src/main/java/study/step4/domain/CarPosition.java
+++ src/main/java/study/step4/domain/CarPosition.java
@@ -1,10 +1,16 @@
 package study.step4.domain;
+package study.step4.domain;
 
 public class CarPosition {
+    private static final int CAR_START_POSITION = 0;
     private final int position;
 
     public CarPosition(int position) {
         this.position = position;
+    }
+
+    public static CarPosition createDefault() {
+        return new CarPosition(CAR_START_POSITION);
     }
 
     public CarPosition forward() {
@@ -14,4 +20,5 @@ public class CarPosition {
     public int getPosition() {
         return position;
     }
+
 }
