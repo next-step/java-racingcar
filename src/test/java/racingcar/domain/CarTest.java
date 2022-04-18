36c4d09d5bf237c30a8faf96b095eb9af7de7a79
hahoho87 <bbaing4564@gmail.com>
2022-04-18 23:45:13 +0900

    chore: step5 - 패키지 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,11 +1,12 @@
 
-package racingcar;
+package racingcar.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.DefaultMovingStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,4 +1,4 @@
-package racingcar;
+package racingcar.view;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
