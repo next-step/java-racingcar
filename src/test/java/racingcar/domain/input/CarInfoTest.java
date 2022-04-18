8d6fc4ee77e6457249df20f46a802eef64f5e534
wlroh <wlroh.paul@gmail.com>
2022-04-18 21:57:21 +0900

    test: 커스텀 예외 적용 관련 테스트 코드 변경

--- src/test/java/racingcar/domain/input/CarInfoTest.java
+++ src/test/java/racingcar/domain/input/CarInfoTest.java
@@ -3,6 +3,7 @@ package racingcar.domain.input;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.input.CarInfo;
+import racingcar.domain.input.exception.InvalidCarNameException;
 import racingcar.dto.InputCar;
 
 import static org.assertj.core.api.Assertions.*;
@@ -10,20 +11,18 @@ import static org.assertj.core.api.Assertions.*;
 class CarInfoTest {
 
     @Test
-    @DisplayName("문자열이 공란인 경우 IllegalArgumentException.class 예외(메시지 '최소 하나 이상의 이름을 입력해야합니다.')를 반환한다.")
+    @DisplayName("문자열이 공란인 경우 InvalidCarNameException.class 예외(메시지 '최소 하나 이상의 이름을 입력해야합니다.')를 반환한다.")
     void inputBlankTest() {
         assertThatThrownBy(() -> new CarInfo(""))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(InvalidCarNameException.class)
                 .hasMessage("최소 하나 이상의 이름을 입력해야합니다.");
     }
 
     @Test
-    @DisplayName("문자열이 null 인 경우 IllegalArgumentException.class 예외(메시지 '최소 하나 이상의 이름을 입력해야합니다.')를 반환한다.")
+    @DisplayName("문자열이 null 인 경우 InvalidCarNameException.class 예외(메시지 '최소 하나 이상의 이름을 입력해야합니다.')를 반환한다.")
     void inputNullTest() {
         assertThatThrownBy(() -> new CarInfo(null))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(InvalidCarNameException.class)
                 .hasMessage("최소 하나 이상의 이름을 입력해야합니다.");
     }
-
-
 }
