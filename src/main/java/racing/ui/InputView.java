32fab4c3bac4fd5c8a83cd95a43538a262db2851
nara1030 <nara1030@naver.com>
2022-04-19 14:33:33 +0900

    refactor: 문자열 상수 추출 등

--- src/main/java/racing/ui/InputView.java
+++ src/main/java/racing/ui/InputView.java
@@ -7,13 +7,16 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String INPUT_NUMBER_OF_ROUND_MESSAGE = "시도할 회수는 몇 회인가요?";
+    private static final String DELIMETER = ",";
 
     private InputView() {
     }
 
     public static String[] inputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String[] carNames = scanner.nextLine().split(",");
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
+        String[] carNames = scanner.nextLine().split(DELIMETER);
 
         try {
             checkCarNamesLength(carNames);
@@ -32,7 +35,7 @@ public class InputView {
     }
 
     public static int inputNumberOfRounds() {
-        System.out.println("시도할 회수는 몇 회인가요?");
+        System.out.println(INPUT_NUMBER_OF_ROUND_MESSAGE);
 
         return scanner.nextInt();
     }
