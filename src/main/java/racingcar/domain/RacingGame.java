af102063c9adde669be1e24009d1f6e77a52ecc4
jisu <jisuuoo96@gmail.com>
2022-04-19 19:57:14 +0900

    feat : 우승자를 계산하는 calMaxWinner() 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,11 +1,13 @@
 package racingcar.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class RacingGame {
 
     private static final List<Car> cars = new ArrayList<>();
+    private static final List<String> winners = new ArrayList<>();
 
     public List<Car> createCar(String[] names) {
         for (int i = 0; i < names.length; i++) {
@@ -20,4 +22,20 @@ public class RacingGame {
         }
         return cars;
     }
+
+    public List<String> calMaxWinner() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < cars.size(); i++) {
+            list.add(cars.get(i).getMovement());
+        }
+        Integer max = Collections.max(list);
+
+        for (Car car : cars) {
+            if (max == car.getMovement()) {
+                winners.add(car.getName());
+            }
+        }
+        return winners;
+    }
+
 }
