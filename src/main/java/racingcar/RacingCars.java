19fd32375b80a7ed1a62f746f6e41bf93370e072
guswns1659 <guswns1659@gmail.com>
2022-04-19 18:01:01 +0900

    step4 refactor: (리뷰) 승자구하는 로직 변경

--- src/main/java/racingcar/RacingCars.java
+++ src/main/java/racingcar/RacingCars.java
@@ -8,7 +8,7 @@ public class RacingCars {
     private static final Random RANDOM = new Random();
     private static final int BOUND = 10;
     private static final int FIRST = 0;
-    List<RacingCar> cars;
+    private List<RacingCar> cars;
 
     public RacingCars(String[] carNames) {
         this.cars = new ArrayList<>(carNames.length);
@@ -28,25 +28,16 @@ public class RacingCars {
     }
 
     public String award() {
-        List<RacingCar> winnerCars = calculateScores();
-        return selectWinners(winnerCars).toString();
-    }
-
-    private List<RacingCar> calculateScores() {
-        List<RacingCar> winnerCars = new ArrayList<>();
-        cars.sort(RacingCar::compareTo);
-        RacingCar highScoreCar = cars.get(FIRST);
-        for (RacingCar car : cars) {
-            winnerCars = car.compareHighScoreCar(highScoreCar, winnerCars);
-        }
-        return winnerCars;
-    }
-
-    private StringBuilder selectWinners(List<RacingCar> winnerCars) {
+        RacingCar highScoreCar = getHighestScoreCar();
         StringBuilder winners = new StringBuilder();
-        for (RacingCar car : winnerCars) {
-            winners.append(car.getName()).append(" ");
+        for (RacingCar car : cars) {
+            winners.append(car.compareWithHighestScoreCar(highScoreCar)).append(" ");
         }
-        return winners;
+        return winners.toString();
+    }
+
+    private RacingCar getHighestScoreCar() {
+        cars.sort(RacingCar::compareTo);
+        return cars.get(FIRST);
     }
 }
--- src/test/java/racingcar/RacingCarsTest.java
+++ src/test/java/racingcar/RacingCarsTest.java
@@ -11,4 +11,13 @@ public class RacingCarsTest {
         String[] carNames = {"car", "car2"};
         assertThat(new RacingCars(carNames).getCars().size()).isEqualTo(carNames.length);
     }
+
+    @Test
+    void carsGiven_ReturnWinners() {
+        RacingCars racingCars = new RacingCars(new String[]{"car1", "car2", "car3"});
+        for (int attempt=0; attempt < 3; attempt++) {
+            racingCars.turnAroundCircuit();
+        }
+        assertThat(racingCars.award().length()).isGreaterThan(4);
+    }
 }
