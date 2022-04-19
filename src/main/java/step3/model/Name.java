4077ee3bfe20cb27213ec1d394cef56e83cf9d78
asdfg5415 <asdfg5415@gmail.com>
2022-04-20 03:00:46 +0900

    refactor: 리뷰 반영 - 매직넘버 상수화

--- src/main/java/step3/model/Name.java
+++ src/main/java/step3/model/Name.java
@@ -2,6 +2,8 @@ package step3.model;
 
 public class Name {
 
+    private static final int NAME_LENGTH_LIMIT = 5;
+
     private final String value;
 
     public Name(String input) {
@@ -13,8 +15,9 @@ public class Name {
         if (input.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
         }
-        if (input.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없다.");
+
+        if (input.length() > NAME_LENGTH_LIMIT) {
+            throw new IllegalArgumentException("자동차 이름은 " + NAME_LENGTH_LIMIT + "자를 초과할 수 없다.");
         }
     }
 
