010df10a7aa85eef7f504fcb50477e349d604839
jisu <jisuuoo96@gmail.com>
2022-04-19 19:45:33 +0900

    feat : 자동차의 이름을 입력받는 askNameOfCars() 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,9 +6,9 @@ public class InputView {
 
     private static final Scanner sc = new Scanner(System.in);
 
-    public int askNumberOfCars() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return sc.nextInt();
+    public String[] askNameOfCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분).");
+        return sc.nextLine().split(",");
     }
 
     public int askNumberOfTries() {
