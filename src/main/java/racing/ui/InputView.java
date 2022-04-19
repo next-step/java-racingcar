95b36b1182b5d0d7d713d39fb758f2a622eb62e6
hjeom <nara1030@naver.com>
2022-04-20 00:51:07 +0900

    refactor: 상수 네이밍 대문자 수정

--- src/main/java/racing/ui/InputView.java
+++ src/main/java/racing/ui/InputView.java
@@ -6,7 +6,7 @@ import racing.exception.CarNameLengthException;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_NUMBER_OF_ROUND_MESSAGE = "시도할 회수는 몇 회인가요?";
     private static final String DELIMETER = ",";
@@ -16,7 +16,7 @@ public class InputView {
 
     public static String[] inputCarNames() {
         System.out.println(INPUT_CAR_NAME_MESSAGE);
-        String[] carNames = scanner.nextLine().split(DELIMETER);
+        String[] carNames = SCANNER.nextLine().split(DELIMETER);
 
         try {
             checkCarNamesLength(carNames);
@@ -37,6 +37,6 @@ public class InputView {
     public static int inputNumberOfRounds() {
         System.out.println(INPUT_NUMBER_OF_ROUND_MESSAGE);
 
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 }
