6677316b1c0df3817302231e44b749ce6869765a
daseulll <nldaseul@gmail.com>
2022-04-20 22:31:26 +0900

    feat: 결과 출력 시 자동차 이름도 함께 출력

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -4,15 +4,21 @@ public class ResultView {
 
     public static void showResult(RacingCar[] racingCars) {
         for (RacingCar racingCar : racingCars) {
+            showCarName(racingCar);
             showLocation(racingCar);
         }
         System.out.printf("\n");
     }
 
-    public static void showLocation(RacingCar racingCar) {
+    private static void showCarName(RacingCar racingCar) {
+        System.out.printf(racingCar.getName() + " : ");
+    }
+
+    private static void showLocation(RacingCar racingCar) {
         for (int i = 0; i < racingCar.getLocation().getX() + 1; i++) {
             System.out.printf("-");
         }
         System.out.printf("\n");
+
     }
 }
