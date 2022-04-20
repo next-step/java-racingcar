a9629a0ffac9798325e0990c1334a1cc5de326b8
chkim <cndghks0213@gmail.com>
2022-04-20 19:08:29 +0900

    test: 이름으로 자동차를 생성 및 우승자 찾기 테스트 추가

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -3,48 +3,75 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.test.util.ReflectionTestUtils;
-import util.MoveStrategy;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
     @DisplayName("자동차 대수를 입력했을 때 Cars가 정상적으로 생성되는지 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 5, 10, 20, 100})
-    void constructCarsTest(int size) {
-        Cars cars = new Cars(size);
-        assertThat((List<Car>) ReflectionTestUtils.getField(cars, "cars")).hasSize(size);
+    @ValueSource(strings = {"a,b,c", "a  ,b#, c", "abcde,12345,!@#$%", "-12#4, a1cd,!@s34", "a, b", "ad", "a,b,"})
+    void constructCarsTest(String carsName) {
+        Cars cars = new Cars(carsName);
+        List<Car> carList = (List<Car>) ReflectionTestUtils.getField(cars, "cars");
+        assertThat(carList).hasSize(carsName.split(",").length);
+    }
+
+    @DisplayName("자동차 대수를 입력했을 때 Cars가 정상적으로 생성되는지 테스트 - 실패 케이스")
+    @ParameterizedTest
+    @ValueSource(strings = {",a,b", "abcdef,a,c", "a,,a", ",12345"})
+    void constructCarsFailTest(String carsName) {
+        assertThatThrownBy(() -> new Cars(carsName))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("movable 전략에 따라 자동차가 움직이는지 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"true", "false"})
-    void constructCarsTest(boolean movable) {
-        Cars cars = new Cars(50);
-        Positions positions = cars.moveCars(TestMoveStrategy.getInstance(movable));
-        positions.getPositions()
-                .forEach(position -> assertThat(position).isEqualTo(movable ? 1 : 0));
+    @CsvSource(value = {"a,b,c:true:1", "a,b,c:false:0"}, delimiter = ':')
+    void moveCarsTest(String carsName, boolean movable, int distance) {
+        Cars cars = new Cars(carsName);
+
+        Map<String, Integer> result = cars.moveCars(TestMoveStrategy.getInstance(movable));
+
+        result.keySet()
+                .forEach(carName -> assertThat(result.get(carName)).isEqualTo(distance));
     }
 
-    static class TestMoveStrategy implements MoveStrategy {
-        private static final TestMoveStrategy testMoveStrategy = new TestMoveStrategy();
-        private static boolean moveState = false;
+    @DisplayName("우승자 찾기 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"a,b,c:true:a", "a,b,c:false:a,b", "a,b,c:false:a,b,c"}, delimiter = ':')
+    void findWinnersTest(String carsName, boolean movable, String additionalMove) {
+        Cars cars = new Cars(carsName);
+        cars.moveCars(TestMoveStrategy.getInstance(movable));
+        List<Car> carList = (List<Car>) ReflectionTestUtils.getField(cars, "cars");
 
-        private TestMoveStrategy() {
+        List<String> additionalMoveCars = Arrays.stream(additionalMove.split(","))
+                .collect(Collectors.toList());
+        for (Car car : carList) {
+            if (additionalMoveCars.contains(car.getName())) {
+                car.move(TestMoveStrategy.getInstance(true));
+            }
         }
 
-        public static TestMoveStrategy getInstance(boolean moveState) {
-            TestMoveStrategy.moveState = moveState;
-            return testMoveStrategy;
-        }
+        List<String> winners = cars.findWinners().stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+        assertThat(winners.equals(additionalMoveCars)).isTrue();
+    }
 
-        @Override
-        public boolean isMovable() {
-            return moveState;
+    @Test
+    void test() {
+        String[] split = ",a,b".split(",");
+        for (String s : split) {
+            System.out.println("[" + s + "]");
         }
     }
 }
