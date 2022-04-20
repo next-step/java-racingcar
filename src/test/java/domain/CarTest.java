88c0dd402205c2ae27eaaf82678adeaf99a6e96f
chkim <cndghks0213@gmail.com>
2022-04-20 19:08:45 +0900

    test: 자동차 생성 및 이동 테스트 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,49 +1,41 @@
 package domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.test.util.ReflectionTestUtils;
-import util.MoveStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
-    private Car car;
+    @DisplayName("자동차 생성 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "abcde","1", "12345", "#", "@#$%@", "@1#4%", "a1!@d"})
+    void createCarTest(String carName) {
+        Car car = new Car(carName);
+        assertThat(car.getName()).isEqualTo(carName);
+    }
 
-    @BeforeEach
-    void init() {
-        car = new Car();
-        ReflectionTestUtils.setField(car, "position", 5);
+    @DisplayName("자동차 생성 테스트 - 실패케이스")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "abcdef", "@#$%^&", "123456"})
+    void createCarFailTest(String carName) {
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("자동차 이동 테스트")
     @ParameterizedTest
     @CsvSource(value = {"true:1", "false:0"}, delimiter = ':')
     void carMoveTest(boolean movable, int moveDistance) {
-        int beforePosition = car.getPosition().getPosition();
+        Car car = new Car("car");
+        Position afterPosition = new Position();
+        ReflectionTestUtils.setField(afterPosition, "position", moveDistance);
 
         car.move(TestMoveStrategy.getInstance(movable));
 
-        assertThat(car.getPosition().getPosition()).isEqualTo(beforePosition + moveDistance);
-    }
-
-    static class TestMoveStrategy implements MoveStrategy {
-        private static final TestMoveStrategy testMoveStrategy = new TestMoveStrategy();
-        private static boolean moveState = false;
-
-        private TestMoveStrategy() {
-        }
-
-        public static TestMoveStrategy getInstance(boolean moveState) {
-            TestMoveStrategy.moveState = moveState;
-            return testMoveStrategy;
-        }
-
-        @Override
-        public boolean isMovable() {
-            return moveState;
-        }
+        assertThat(car.equalPosition(afterPosition)).isTrue();
     }
 }
