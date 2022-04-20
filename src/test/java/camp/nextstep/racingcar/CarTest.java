f8b45a777e18f7c31306767f598ffad4a57c999c
jeongjaeeom <jeongjae.eom@gmail.com>
2022-04-20 12:10:58 +0900

    refactor($compile): 주어진 자동차 수에 따른 자동차들 생성 기능 구현
    주어진 자동차 수에 따른 자동차들 생성 기능 구현
    - 자동차 생성 팩토리 생성
    - 자동차 생성 메소드 구현
    
    BREAKING CHANGE: 주어진 자동차 수에 따른 자동차들 생성 기능 구현

--- src/test/java/camp/nextstep/racingcar/CarTest.java
+++ src/test/java/camp/nextstep/racingcar/CarTest.java
@@ -2,6 +2,8 @@ package camp.nextstep.racingcar;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
@@ -12,4 +14,11 @@ public class CarTest {
         car.move(4);
         assertThat(car.getPosition()).isEqualTo(1);
     }
+
+    @Test
+    public void 주어진_자동차_수_따른_자동차들_생성() {
+        List<Car> cars = CarFactory.create(4);
+        assertThat(cars).hasSize(4);
+    }
+
 }
