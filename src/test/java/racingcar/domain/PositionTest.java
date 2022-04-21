36371453e7a35659f499fcc8c0d3b7a15ef86ac8
sjhello <sjhello235@gmail.com>
2022-04-21 17:21:08 +0900

    test: 이동거리 객체 테스트 작성

---
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -0,0 +1,38 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+
+    @Test
+    @DisplayName("현재 위치를 문자열로 표현한다")
+    void initPosition() {
+        Position position = new Position();
+
+        String initPosition = position.show();
+
+        assertThat(initPosition).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("현재 위치를 숫자로 표현한다")
+    void showPositionLength() {
+        Position position = new Position();
+
+        int initPosition = position.length();
+
+        assertThat(initPosition).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("이동 단위는 - 이다")
+    void movingUnit() {
+        String unit = "-";
+
+        assertThat(Position.MOVING_UNIT).isEqualTo(unit);
+    }
+}
