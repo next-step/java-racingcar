925e18d797a75d11bef0a4bbabb2a49a58f0adf1
minigear <coosac@gmail.com>
2022-04-22 20:55:23 +0900

    [refactor] 3단계 - code review : ResultView method 분리

--- src/main/java/racing/ResultView.java
+++ src/main/java/racing/ResultView.java
@@ -5,30 +5,48 @@ import com.sun.nio.sctp.IllegalReceiveException;
 import java.util.List;
 
 public class ResultView {
+    private final static String NO_MOVE = "";
+    private final static String MOVE = "-";
 
     public static void view(Circuit circuit) {
         System.out.println("실행 결과");
-        List<Car> cars = circuit.getCars();
-        for (int i = 0; i < circuit.getNumberOfRacing(); i++) {
-            System.out.printf("%d 회 \n", i + 1);
-            for (Car car : cars) {
-                printMove(car.getMoves(), i);
-            }
+        for (int roundNumber = 0; roundNumber < circuit.getNumberOfRacing(); roundNumber++) {
+            viewRound(circuit.getCars(), roundNumber);
         }
     }
 
-    private static void printMove(List<Boolean> moves, int index) {
-        if (moves.size() < index) {
+    private static void viewRound(List<Car> cars, int roundNumber) {
+        System.out.printf("%d 회 \n", roundNumber + 1);
+
+        for (Car car : cars) {
+            viewCar(car.getMoves(), roundNumber);
+        }
+    }
+
+    private static void viewCar(List<Boolean> moves, int roundNumber) {
+        validateCarMoves(moves, roundNumber);
+
+        System.out.println(getCarMoves(moves, roundNumber));
+    }
+
+    private static void validateCarMoves(List<Boolean> moves, int roundNumber) {
+        if (moves.size() < roundNumber) {
             throw new IllegalReceiveException();
         }
+    }
 
+    private static StringBuilder getCarMoves(List<Boolean> moves, int roundNumber) {
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i <= index; i++) {
-            if (moves.get(i)) {
-                result.append("-");
-            }
+        for (int round = 0; round <= roundNumber; round++) {
+            result.append(getMove(moves, round));
         }
+        return result;
+    }
 
-        System.out.println(result);
+    private static String getMove(List<Boolean> moves, int round) {
+        if (moves.get(round)) {
+            return MOVE;
+        }
+        return NO_MOVE;
     }
 }
