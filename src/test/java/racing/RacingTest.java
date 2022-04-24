0495982ad53e5f256dc7805eeaf6fa915be906c6
minigear <coosac@gmail.com>
2022-04-24 22:21:21 +0900

    [test] 3단계 - ParameterizedTest 적용

--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -2,36 +2,46 @@ package racing;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingTest {
-    @Test
-    @DisplayName("차량 생성 테스트")
-    void circuitTest() {
-        int numberOfCars = 3;
-        int numberOfMoves = 3;
-        Circuit circuit = new Circuit(numberOfCars, numberOfMoves);
+    @ParameterizedTest
+    @MethodSource("provideInputCar")
+    void circuitTest(int numberOfCar, int numberOfRacing) {
+        Circuit circuit = new Circuit(numberOfCar, numberOfRacing);
         circuit.startRacing();
-        assertThat(numberOfCars).isEqualTo(circuit.getCars().size());
+        assertThat(numberOfCar).isEqualTo(circuit.getCars().size());
 
 
         int errorMinNumberOfCar = 0;
         int errorMinNumberOfMove = 0;
-        assertThatThrownBy(() -> new Circuit(errorMinNumberOfCar, numberOfMoves))
+        assertThatThrownBy(() -> new Circuit(errorMinNumberOfCar, numberOfRacing))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Circuit(numberOfCars, errorMinNumberOfMove))
+        assertThatThrownBy(() -> new Circuit(numberOfCar, errorMinNumberOfMove))
                 .isInstanceOf(IllegalArgumentException.class);
 
 
         int errorMaxNumberOfCar = 100;
         int errorMaxNumberOfMove = 100;
-        assertThatThrownBy(() -> new Circuit(errorMaxNumberOfCar, numberOfMoves))
+        assertThatThrownBy(() -> new Circuit(errorMaxNumberOfCar, numberOfRacing))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Circuit(numberOfCars, errorMaxNumberOfMove))
+        assertThatThrownBy(() -> new Circuit(numberOfCar, errorMaxNumberOfMove))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    private static Stream<Arguments> provideInputCar() {
+        return Stream.of(
+                Arguments.of(3, 3)
+        );
+    }
+
 
     @Test
     @DisplayName("차량 이동 회수 테스트")
