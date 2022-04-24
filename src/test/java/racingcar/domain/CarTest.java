87d22f81e9b369f5dd83a7ebeceafff6d37eaed3
sjhello <sjhello235@gmail.com>
2022-04-24 21:51:05 +0900

    chore: 불필요한 import 제거

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import racingcar.domain.Car;
 import racingcar.util.generator.NumberGenerator;
 import racingcar.util.strategy.move.CarMoveStrategy;
 import racingcar.util.strategy.move.MoveStrategy;
--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -1,10 +1,9 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
 
