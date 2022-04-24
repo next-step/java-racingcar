bb28e326f74cad168ffec5ef378d8425d0531038
bluewow <dogseen@gmail.com>
2022-04-25 01:18:10 +0900

    refactor : 매직 리터럴

--- src/main/java/study/step4/view/InputView.java
+++ src/main/java/study/step4/view/InputView.java
@@ -6,7 +6,9 @@ import study.step4.util.StringParser;
 import java.util.Scanner;
 
 public class InputView {
+
     private static final String CAR_NAME_INPUT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String COMMA = ",";
     private static final String GAME_TRY_COUNT = "시도할 회수는 몇회인가요?";
     private final Scanner scanner = new Scanner(System.in);
 
@@ -17,7 +19,7 @@ public class InputView {
     }
 
     private String[] carNameParser(String input) {
-        return StringParser.impl(input, ",");
+        return StringParser.impl(input, COMMA);
     }
 
     public int gameCountSetting() {
