854e5fccfeb8a83dbdd38c02c6f9d3d8e12ec634
chkim <cndghks0213@gmail.com>
2022-04-27 23:16:00 +0900

    refactor: 서비스 계층의 시도 횟수를 클래스로 분리

--- src/main/java/domain/AttemptCount.java
+++ src/main/java/domain/AttemptCount.java
@@ -1,2 +1,54 @@
-package domain;public class AttemptCount {
+package domain;
+
+import java.util.Objects;
+
+public class AttemptCount {
+    private static final String WRONG_ATTEMPT_COUNT_MESSAGE = "시도 횟수는 1이상의 정수여야 합니다.";
+    private static final String CORRECT_INPUT_PATTERN = "[0-9]*";
+    private static final int MIN_ATTEMPT_COUNT = 1;
+    private final int count;
+
+    public AttemptCount(String count) {
+        validateAttemptCount(count);
+        this.count = Integer.parseInt(count);
+    }
+
+    public AttemptCount(int count) {
+        validateAttemptCount(count);
+        this.count = count;
+    }
+
+    public int getAttemptCount() {
+        return count;
+    }
+
+    private void validateAttemptCount(int count) {
+        if (count < MIN_ATTEMPT_COUNT) {
+            throw new IllegalArgumentException(WRONG_ATTEMPT_COUNT_MESSAGE);
+        }
+    }
+
+    private void validateAttemptCount(String count) {
+        if (count == null || !count.matches(CORRECT_INPUT_PATTERN)) {
+            throw new IllegalArgumentException(WRONG_ATTEMPT_COUNT_MESSAGE);
+        }
+        validateAttemptCount(Integer.parseInt(count));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AttemptCount that = (AttemptCount) o;
+        return count == that.count;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count);
+    }
 }
--- src/test/java/domain/AttemptCountTest.java
+++ src/test/java/domain/AttemptCountTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class AttemptCountTest {
-  
+    @DisplayName("경기 시도 횟수 생성 - 정상 케이스")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2"})
+    void createAttemptCountTest(String count) {
+        assertThat(new AttemptCount(count))
+                .isEqualTo(new AttemptCount(Integer.parseInt(count)));
+    }
+
+    @DisplayName("경기 시도 횟수 생성 - 실패 케이스")
+    @NullAndEmptySource
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0", "1회"})
+    void createAttemptCountTestFail(String count) {
+        assertThatThrownBy(() -> new AttemptCount(count))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
