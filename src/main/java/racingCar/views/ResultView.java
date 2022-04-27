bd81e753fd6292e1981bfaac21cc7fc70e75be8b
jinsxl <wlsthf1113@gmail.com>
2022-04-28 02:58:06 +0900

    feat: 자동차 이동상태 출력 시 이름을 같이 출력

--- src/main/java/racingCar/views/ResultView.java
+++ src/main/java/racingCar/views/ResultView.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class ResultView {
     public static void showRoundResult(List<Car> cars) {
         for (Car car : cars) {
-            System.out.println("-".repeat(car.getPosition()));
+            System.out.println(car.getName() + ": " + "-".repeat(car.getPosition()));
         }
         System.out.println();
     }
