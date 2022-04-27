bcecd8beb99726f63efa62d59a2b6bd512070b8d
chkim <cndghks0213@gmail.com>
2022-04-27 23:14:01 +0900

    refactor: 우승자를 찾는 기능 수정 및 equals, hashcode 재정의

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -2,36 +2,61 @@ package domain;
 
 import util.MoveStrategy;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Cars {
+    private static final String NOT_EXIST_WINNERS = "우승자가 없습니다.";
     private static final String DELIMITER = ",";
-    private final List<Car> cars;
+    private final List<Car> carList;
+
+    public Cars(List<Car> carList) {
+        this.carList = carList;
+    }
 
     public Cars(String name) {
-        cars = Arrays.stream(name.split(DELIMITER))
+        carList = Arrays.stream(name.split(DELIMITER))
                 .map(Car::new)
                 .collect(Collectors.toList());
     }
 
-    public Map<String, Integer> moveCars(MoveStrategy moveStrategy) {
-        cars.forEach(car -> car.move(moveStrategy));
-        return cars.stream()
-                .collect(Collectors.toMap(Car::getName, car -> car.getPosition().getPosition()));
+    public List<Car> moveCars(MoveStrategy moveStrategy) {
+        carList.forEach(car -> car.move(moveStrategy));
+        return carList;
     }
 
     public List<Car> findWinners() {
-        Position maxPosition = getMaxPosition();
-        return cars.stream()
-                .filter(car -> car.equalPosition(maxPosition))
+        Car maxPositionCar = findMaxPositionCar();
+        return carList.stream()
+                .filter(car -> car.isEqualPosition(maxPositionCar))
                 .collect(Collectors.toList());
     }
 
-    private Position getMaxPosition() {
-        return cars.stream()
-                .map(Car::getPosition)
-                .max(Position::compareTo)
-                .orElseThrow(() -> new IllegalArgumentException("우승자가 없습니다."));
+    private Car findMaxPositionCar() {
+        if (carList.isEmpty()) {
+            throw new IllegalArgumentException(NOT_EXIST_WINNERS);
+        }
+        return carList.stream()
+                .max(Car::compareTo)
+                .get();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Cars carList = (Cars) o;
+        return Objects.equals(this.carList, carList.carList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carList);
     }
 }
