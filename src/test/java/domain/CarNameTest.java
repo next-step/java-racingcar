5ba54d6fe9621f03d03e14387ec550609b843acb
chkim <cndghks0213@gmail.com>
2022-04-27 23:11:33 +0900

    test: 자동차 이름 관련 테스트 작성

--- src/test/java/domain/CarNameTest.java
+++ src/test/java/domain/CarNameTest.java
@@ -1,4 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class CarNameTest {
-  
+
+    @DisplayName("자동차 이름 생성 - 정상 케이스")
+    @ParameterizedTest
+    @ValueSource(strings = {"차", "자동차이름"})
+    void createCarNameTest(String name) {
+        CarName carName = new CarName(name);
+        assertThat(carName).isEqualTo(new CarName(name));
+    }
+
+    @DisplayName("자동차 이름 생성 - 실패 케이스")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"새로운자동차", "123456"})
+    void createTest(String carName) {
+        assertThatThrownBy(() -> new CarName(carName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
