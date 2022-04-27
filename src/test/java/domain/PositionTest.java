86d20e49ccb440b50159041eee7dd6d52325f9fb
chkim <cndghks0213@gmail.com>
2022-04-27 23:11:44 +0900

    test: 자동차 위치 관련 테스트 작성

--- src/test/java/domain/PositionTest.java
+++ src/test/java/domain/PositionTest.java
@@ -1,21 +1,42 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PositionTest {
+
+    @DisplayName("포지션 생성 테스트 - NoArg")
+    @Test
+    void createPositionNoArg() {
+        Position position = new Position();
+        assertThat(position).isEqualTo(new Position());
+    }
+
+    @DisplayName("포지션 생성 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 100})
+    void createPositionArg(int newPosition) {
+        Position position = new Position(newPosition);
+        assertThat(position).isEqualTo(new Position(newPosition));
+    }
+
+    @DisplayName("포지션 생성 테스트 - 실패 케이스")
+    @Test
+    void createPositionFail() {
+        assertThatThrownBy(() -> new Position(-1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("포지션 이동 테스트")
     @Test
     void goForwardTest() {
         Position position = new Position();
-        Position afterPosition = new Position();
-        ReflectionTestUtils.setField(afterPosition, "position", 1);
-
         position.goForward();
-
-        Assertions.assertThat(position.equals(position)).isTrue();
+        assertThat(position).isEqualTo(new Position(1));
     }
 }
