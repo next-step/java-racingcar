ec5aa4801cef7f8f0c8ba4c8aac2dd3b3cb2de8b
jinsxl <wlsthf1113@gmail.com>
2022-04-27 23:46:32 +0900

    feat: Car 구현

---
+++ src/main/java/racingCar/domains/Car.java
@@ -0,0 +1,18 @@
+package racingCar.domains;
+
+import java.util.Random;
+
+public class Car {
+    private int position;
+
+    public Car() {
+        this.position = 0;
+    }
+
+    public void move() {
+        Random random = new Random();
+        if (random.nextInt(10) >= 4) {
+            position++;
+        }
+    }
+}
