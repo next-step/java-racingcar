8e9241af4bd978a3c8591285cec00784fa082eb6
Latoya <71559880+daakludens@users.noreply.github.com>
2022-04-28 00:19:15 +0900

    3단계 - 자동차 경주 (#3268)

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,19 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public int getInputNumber() { return scanner.nextInt(); }
+
+    public void askNumberOfCars() {
+        System.out.println("자동차 대수는 몇 대 인가요? : ");
+    }
+
+    public void askNumberOfTurns() {
+        System.out.println("시도할 회수는 몇 회 인가요? : ");
+    }
+
+}
---
+++ src/main/java/view/ResultView.java
@@ -0,0 +1,17 @@
+package view;
+
+public class ResultView {
+
+    public void printRaceStart() {
+        System.out.println("실행 결과");
+    }
+
+    public void printEndOfTurn() {
+        System.out.println();
+    }
+
+    public void printResultOfTurn(StringBuilder result) {
+        System.out.println(result);
+    }
+
+}
