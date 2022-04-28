7d65b227b0692ecdac8a09b781f0f180ffd0c490
polynomeer <polynomeer@gmail.com>
2022-04-28 10:02:21 +0900

    chore: Remove unnecessary print statement

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -6,7 +6,6 @@ public class OutputView {
     }
 
     public static void printRound(int speed, int distance) {
-        System.out.print("speed = " + speed + ": ");
         for (int j = 0; j < distance; j++) {
             System.out.print("-");
         }
