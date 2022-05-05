6e8f6ddee4215d75d8ff622c0df43aa076729fa9
suwampy <tndhks123@nate.com>
2022-05-05 23:25:24 +0900

    refact: 자바 명세에 맞게 수정자 순서 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,9 +7,9 @@ public class InputView {
 
     }
 
-    private final static Scanner SCANNER = new Scanner(System.in);
-    private final static String INPUT_RACING_CAR_COUNT = "자동차 대수는 몇대 인가요?";
-    private final static String INPUT_ATTEMPT_COUNT = "시도할 회수는 몇 회 인가요?";
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String INPUT_RACING_CAR_COUNT = "자동차 대수는 몇대 인가요?";
+    private static final String INPUT_ATTEMPT_COUNT = "시도할 회수는 몇 회 인가요?";
 
     public static int inputCarCount() {
         System.out.println(INPUT_RACING_CAR_COUNT);
