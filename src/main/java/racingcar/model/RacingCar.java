fd547b40e4f67ab0abd728b0d73d18bfda008fcb
suwampy <tndhks123@nate.com>
2022-05-06 00:53:30 +0900

    refact: RaingCar에서 랜덤한 요소를 메소드로 추출해 테스트에 용이하도록 개선

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import java.util.Objects;
+
 public class RacingCar {
     private RacingCarPosition status;
     private final MovableStrategy movableStrategy = new RandomMovableStrategy();
@@ -13,12 +15,27 @@ public class RacingCar {
     }
 
     public void move() {
-        if (movableStrategy.isMovable()) {
-            this.status = status.add();
-        }
+        this.status = status.add();
     }
 
     public int getStatus() {
         return this.status.getValue();
+    }
+
+    public boolean isMovable() {
+        return movableStrategy.isMovable();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RacingCar racingCar = (RacingCar) o;
+        return Objects.equals(status, racingCar.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(status);
     }
 }
