3d6090eb794d1886668dfcb49c767af923b546bd
suwampy <tndhks123@nate.com>
2022-05-05 22:40:07 +0900

    test: RacingCarStatus 테스트 코드 작성

---
+++ src/test/java/model/RacingCarStatusTest.java
@@ -0,0 +1,16 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingCarStatusTest {
+    @Test
+    @DisplayName("RacingCarStatus를 생성 후 add를 호출했을 때 value가 증가하는지 확인한다.")
+    void value_add() {
+        RacingCarPosition rcp = new RacingCarPosition();
+        rcp = rcp.add();
+        assertThat(rcp.equals(new RacingCarPosition(1)));
+    }
+}
