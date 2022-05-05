d8786e5cb37d50c9511535de6da1ce7df3c7d201
suwampy <tndhks123@nate.com>
2022-05-06 00:15:45 +0900

    test: racingCars 테스트 작성

--- src/test/java/racingcar/model/RacingCarsTest.java
+++ src/test/java/racingcar/model/RacingCarsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RacingCarsTest {
 
@@ -12,5 +13,12 @@ class RacingCarsTest {
     void create_racing_cars() {
         RacingCars rc = RacingCars.create(3);
         assertThat(rc.getValue().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("RacingCars 객체를 만들 때 0 이하의 수를 입력하면 예외가 발생한다.")
+    void create_racing_cars_negative() throws Exception {
+        assertThatThrownBy(() -> RacingCars.create(0))
+                .isInstanceOf(RuntimeException.class);
     }
 }
