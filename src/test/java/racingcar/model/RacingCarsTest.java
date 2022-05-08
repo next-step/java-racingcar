afeb072c29fdefc4ddaf6098de0c317a86d8fece
suwampy <tndhks123@nate.com>
2022-05-08 23:02:11 +0900

    test: 요구사항 수정에 따라 racingCars 객체를 만들때 발생하는 오류를 테스트하는 테스트코드 수정

--- src/test/java/racingcar/model/RacingCarsTest.java
+++ src/test/java/racingcar/model/RacingCarsTest.java
@@ -16,9 +16,11 @@ class RacingCarsTest {
     }
 
     @Test
-    @DisplayName("RacingCars 객체를 만들 때 0 이하의 수를 입력하면 예외가 발생한다.")
+    @DisplayName("RacingCars 객체를 만들 때 5자 이상의 이름을 입력하면 예외가 발생한다.")
     void create_racing_cars_negative() throws Exception {
-        assertThatThrownBy(() -> RacingCars.create(0))
-                .isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> RacingCars.create("qwertyasdfg"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("자동차 이름은 5자를 초과할 수 없습니다.");
+    }
     }
 }
