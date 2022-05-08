711e4e809ba8b03bffdb0ec3bba79eb593b16290
suwampy <tndhks123@nate.com>
2022-05-08 23:02:55 +0900

    test: RacingCars 객체를 만들 때 이름 입력에 따른 예외 발생 테스트 코드 작성

--- src/test/java/racingcar/model/RacingCarsTest.java
+++ src/test/java/racingcar/model/RacingCarsTest.java
@@ -3,6 +3,8 @@ package racingcar.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -21,6 +23,14 @@ class RacingCarsTest {
         assertThatThrownBy(() -> RacingCars.create("qwertyasdfg"))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("자동차 이름은 5자를 초과할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("RacingCars 객체를 만들 때 이름을 입력하지 않으면 예외가 발생한다.")
+    void create_racing_cars_negative_blank() throws Exception {
+        assertThatThrownBy(() -> RacingCars.create(""))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("구분자는 ,를 사용해서 이름을 1개 이상 입력해주세요");
     }
     }
 }
