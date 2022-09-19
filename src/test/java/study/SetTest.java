b500418e85c4d91ca070200b2b4d0153b72b9aa8
성준혁 <36617783+nooose@users.noreply.github.com>
2022-09-19 20:32:13 +0900

    1단계 - 학습 테스트 (#3395)
    * test: String 클래스 테스트 코드 작성
    1단계 요구사항에 맞게 테스트를 작성했다.
    
    * test: Set 클래스 테스트 코드 작성
    1단계 요구사항에 맞게 테스트를 작성했다.
    
    * style: 개행 단위를 한 줄로 변경

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set 사이즈 확인")
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Set 원소 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @DisplayName("Set 원소 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
