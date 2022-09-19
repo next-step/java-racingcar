30fe533103bb8f20d0cc2429fe2a884eed7590ba
jungwoon727 <jungwoon727@gmail.com>
2022-09-19 20:26:09 +0900

    Set 클래스 size 메소드 학습 테스트

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -1,8 +1,12 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class SetCollectionTest {
     private Set<Integer> numbers;
@@ -14,5 +18,12 @@ public class SetCollectionTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 클래스 size 메소드 학습 테스트")
+    void size() {
+        int result = numbers.size();
+        assertThat(result).isEqualTo(3);
     }
 }
