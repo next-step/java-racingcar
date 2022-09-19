97132cce2f8107bb75eb58d421c78716516308e5
김건우 <kgw1457@gmail.com>
2022-09-19 20:30:52 +0900

    Add String split Test

---
+++ src/test/java/study/StringSplitTest.java
@@ -0,0 +1,14 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringSplitTest {
+
+    @Test
+    void split() {
+       String[] result = "1,2".split(",");
+       assertThat(result).containsExactly("1","2");
+    }
+}
