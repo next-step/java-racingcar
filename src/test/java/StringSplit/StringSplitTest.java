fbc6073d31858dcdd023d1a7602aa99ab3ee8063
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2022-09-19 17:44:37 +0900

    학습테스트 요구사항1 작성

---
+++ src/test/java/StringSplit/StringSplitTest.java
@@ -0,0 +1,34 @@
+package StringSplit;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringSplitTest {
+    @Test
+    void split_string_1_and_2() {
+        //given
+        String testString = "1,2";
+        //when
+        String[] splitResult = testString.split(",");
+        //then
+        assertThat(splitResult).contains("1");
+        assertThat(splitResult).contains("2");
+        assertThat(splitResult).containsExactly("1","2");
+
+    }
+
+    @Test
+    void split_string_1_return_array_contain_1(){
+        //given
+        String testString = "1";
+        //when
+        String[] splitResult = testString.split(",");
+
+        //then
+        assertThat(splitResult).contains("1");
+        assertThat(splitResult).containsExactly("1");
+    }
+}
