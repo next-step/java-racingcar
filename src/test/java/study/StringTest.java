949c2eed2456b91f52b4cb30d6368f648e77191a
doxxx <doxxx93@gmail.com>
2022-09-19 17:43:55 +0900

    String charAt study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,8 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -38,6 +40,35 @@ public class StringTest {
         void substring() {
             String result = "(1,2)".substring(1, 4);
             assertThat(result).isEqualTo("1,2");
+        }
+    }
+
+    @Nested
+    @DisplayName("요구사항 3")
+    class Requirement3 {
+
+        @Test
+        @DisplayName("abc 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트")
+        void charAt() {
+            String str = "abc";
+            assertThat(str.charAt(0)).isEqualTo('a');
+            assertThat(str.charAt(1)).isEqualTo('b');
+            assertThat(str.charAt(2)).isEqualTo('c');
+        }
+
+        @Test
+        @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트")
+        void charAt2() {
+            String str = "abc";
+            assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    str.charAt(3);
+                }).withMessageMatching("String index out of range: 3");
+
+            assertThatThrownBy(() -> {
+                str.charAt(3);
+            }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
         }
     }
 }
