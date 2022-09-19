7943532d945d731d1c31cd953f69d251f43b4f8b
doxxx <doxxx93@gmail.com>
2022-09-19 18:20:17 +0900

    Set contains study test with CsvSource

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -44,6 +45,18 @@ public class SetTest {
         @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습 테스트")
         void contains(int number) {
             assertThat(numbers.contains(number)).isTrue();
+        }
+    }
+
+    @Nested
+    @DisplayName("요구사항 3")
+    class Requirement3 {
+
+        @ParameterizedTest
+        @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+        @DisplayName("Set의 contains() 메소드를 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.")
+        void contains2(int number, boolean expected) {
+            assertThat(numbers.contains(number)).isEqualTo(expected);
         }
     }
 }
