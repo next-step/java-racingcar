705de8788d7dbb888ea50f4599f5cf07a32e414a
suchanmyoung <aud248@naver.com>
2022-09-19 21:40:44 +0900

    test: Set Collection 테스트 요구사항2

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -23,5 +25,12 @@ public class SetTest {
     @DisplayName("Set 클래스의 size() 메소드를 활용해 크기를 확인할 수 있다.")
     void calculate_set_size(){
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("Set 클래스의 contains() 메소드를 활용해 값이 존재하는지 확인할 수 있다.")
+    void set_contains_by_parametrizedTest(int number){
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
