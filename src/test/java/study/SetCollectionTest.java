4def2f0f457c9ef43004e94bf3d352ad125705ca
jungwoon727 <jungwoon727@gmail.com>
2022-09-19 20:34:30 +0900

    contains 메소드 true, false 결과를 모두 포함한 학습 테스트

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -7,6 +7,9 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetCollectionTest {
     private Set<Integer> numbers;
@@ -25,5 +28,21 @@ public class SetCollectionTest {
     void size() {
         int result = numbers.size();
         assertThat(result).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set 클래스 contains 메소드 학습 테스트")
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        boolean result = numbers.contains(input);
+        assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set 클래스 contains 메소드 true, false 결과를 모두 포함한 학습 테스트")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains(String input, String expected) {
+        boolean result = numbers.contains(Integer.parseInt(input));
+        assertThat(result).isEqualTo(Boolean.parseBoolean(expected));
     }
 }
