423e9f5fff4329af70ae651a436fd990fff95329
kureungkureung <terry9611@naver.com>
2022-09-20 01:01:01 +0900

    test: set size 테스트

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void set은_중복을_포함하지_않는_요소의_개수를_가지고_있다() {
+        assertThat(numbers).hasSize(3);
+    }
+}
