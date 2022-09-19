9d833b9d0f073101b8e5550deb0d38d9858d39e8
kureungkureung <terry9611@naver.com>
2022-09-20 01:04:50 +0900

    test: set contains 테스트

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SetTest {
     private Set<Integer> numbers;
@@ -21,5 +24,11 @@ class SetTest {
     @Test
     void set은_중복을_포함하지_않는_요소의_개수를_가지고_있다() {
         assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void set은_중복을_포함하지_않는_요소들을_가지고_있다(int number) {
+        assertTrue(numbers.contains(number));
     }
 }
