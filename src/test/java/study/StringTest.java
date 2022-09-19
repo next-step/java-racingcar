ccb6b829dcf3ae7c6d6e225cc8a347783838daff
doxxx <doxxx93@gmail.com>
2022-09-19 17:43:01 +0900

    String substring study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -28,4 +28,16 @@ public class StringTest {
             assertThat(result).containsExactly("1");
         }
     }
+
+    @Nested
+    @DisplayName("요구사항 2")
+    class Requirement2 {
+
+        @Test
+        @DisplayName("(1,2) 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 1,2를 반환하도록 구현")
+        void substring() {
+            String result = "(1,2)".substring(1, 4);
+            assertThat(result).isEqualTo("1,2");
+        }
+    }
 }
