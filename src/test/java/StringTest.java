f3e4ff1b308d6ba349e86bce787ee87a2aac43e4
kureungkureung <terry9611@naver.com>
2022-09-20 00:37:29 +0900

    test: 문자열 split 테스트

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringTest {
+    @Test
+    void 문자열을_쪼깬_배열은_쪼개진_요소들은_포함한다() {
+        String str = "1,2";
+        String[] sut = str.split(",");
+        assertThat(sut).containsExactly("1", "2");
+    }
+
+    @Test
+    void 문자열을_쪼깰_때_쪼깬_요소들을_포함한_배열이_반환된다() {
+        String str = "1";
+        String[] sut = str.split(",");
+        assertThat(sut).isEqualTo(new String[]{"1"});
+    }
+}
