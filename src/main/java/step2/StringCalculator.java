15479d6cfbb0593161e10eda544f1ed5ac210228
KimWanJoo <wjwan0915@gmail.com>
2022-09-20 01:07:13 +0900

    :sparkles: racing car step2 프로덕션 코드 작성 중

---
+++ src/main/java/step2/StringCalculator.java
@@ -0,0 +1,44 @@
+package step2;
+
+import java.util.Arrays;
+
+public class StringCalculator {
+    private final String numbers;
+
+
+    public StringCalculator(String numbers) {
+        validation(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validation(String numbers) {
+        if (numbers != null && !numbers.matches("\\d")) {
+            throw new RuntimeException("숫자가 없습니다.");
+        }
+        if (numbers != null && numbers.matches("(.*)-\\d(.*)")) {
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
+    }
+
+    public int calculate() {
+        if (numbers == null) {
+            return 0;
+        }
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        String regex = getRegex();
+
+        return Arrays.stream(numbers.replaceAll("//\\D\\n", "").split(regex))
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    private String getRegex() {
+        String regex = "[,|:]";
+        if (numbers.contains("//")) {
+            regex = String.valueOf(numbers.substring(2).charAt(0));
+        }
+        return regex;
+    }
+}
