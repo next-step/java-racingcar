3918d707a169519207af4eb71aa0185b356c7803
KimWanJoo <wjwan0915@gmail.com>
2022-09-20 13:38:01 +0900

    :sparkles: feat: 출력 클래스 작성

---
+++ src/main/java/step3/application/ConsoleOut.java
@@ -0,0 +1,24 @@
+package step3.application;
+
+import step3.domain.Cars;
+import step3.domain.NumberGenerator;
+
+public class ConsoleOut {
+    public void askCarsCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    public void askMoveCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+    }
+
+    public void result(Cars cars, NumberGenerator numberGenerator) {
+        cars.cars().forEach(car -> {
+            car.move(numberGenerator);
+            String strState = "-".repeat(car.state());
+            System.out.println(strState);
+        });
+        System.out.println();
+
+    }
+}
