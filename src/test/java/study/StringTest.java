ff44fc606a29c7ab0816c2c00159890f2629a366
Yunsik-Choi <choiys0311@gmail.com>
2022-09-20 16:21:46 +0900

    split study test

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,13 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    void split(){
+        String[] result = "1,2".split(",");
+        Assertions.assertThat(result).containsExactly("1","2");
+    }
+}
