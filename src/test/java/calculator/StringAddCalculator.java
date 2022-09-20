f70a09f1a15fa324156849c2e42669bf54b6ac1e
devcs96 <katd6@naver.com>
2022-09-20 21:40:09 +0900

    Step2 - 문자열 덧셈 계산기

---
+++ src/test/java/calculator/StringAddCalculator.java
@@ -0,0 +1,50 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final int DEFAULT_VALUE = 0;
+    private static final Pattern pattern = Pattern.compile(("//(.)\n(.*)"));
+    private static final String FIRST_DEFAULT_DELIMITER = ":";
+    private static final String SECOND_DEFAULT_DELIMITER = ",";
+    private static final String OR = "|";
+
+    public static int splitAndSum(String input) throws IllegalArgumentException {
+        if (isInvalid(input)) {
+            return DEFAULT_VALUE;
+        }
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return getSum(matcher.group(2).split(customDelimiter));
+        }
+        return getSum(input.split(FIRST_DEFAULT_DELIMITER + OR + SECOND_DEFAULT_DELIMITER));
+    }
+
+    private static boolean isInvalid(String input) {
+        return input == null || input.isEmpty();
+    }
+
+    private static Integer getSum(String[] input) {
+        return Arrays.stream(input).map(StringAddCalculator::mapToInt).reduce(Integer::sum).orElse(DEFAULT_VALUE);
+    }
+
+    private static Integer mapToInt(String input) {
+        try {
+            return isMinusNumber(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("input must be number", e);
+        }
+    }
+
+    private static int isMinusNumber(String input) {
+        int num = Integer.parseInt(input);
+        if (num < 0) {
+            throw new IllegalArgumentException("input number must be positive");
+        }
+        return num;
+    }
+}
