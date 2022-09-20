d7e172fca65ff1ab50aac714ebb4563dd297d347
Greg.Study <greg0917@naver.com>
2022-09-20 23:22:57 +0900

    step1 - 학습 테스트 실습

--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -1,7 +1,6 @@
 package step1;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
 
