f38d758817b0b13c3c76bbefebb7aa1edb830431
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2022-09-21 00:08:20 +0900

    기능 요구사항 5 테스트 코드 작성, 메서드 코드 작성

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -2,6 +2,8 @@ package StringCalculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
@@ -20,8 +22,17 @@ public class StringCalculator {
 
     private List<Integer> stringSplit(Object addValues) {
         String upcastAddValues = (String) addValues;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(upcastAddValues);
 
-        return Arrays.stream(upcastAddValues.split(",|:"))
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return Arrays.stream(m.group(2).split(customDelimiter))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList());
+        }
+
+        return Arrays.stream(upcastAddValues.split("[,:]"))
                 .mapToInt(Integer::parseInt)
                 .boxed()
                 .collect(Collectors.toList());
--- src/test/java/StringAddCalculatorTest/StringCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest/StringCalculatorTest.java
@@ -8,28 +8,30 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-
 public class StringCalculatorTest {
     private StringCalculator calculator;
+
     @BeforeEach
-    void setUp(){
-        calculator  = new StringCalculator();
+    void setUp() {
+        calculator = new StringCalculator();
     }
+
     @Test
     @DisplayName("null이나 빈 문자를 입력시 0을 반환한다")
-    void stringSum_null_또는_빈문자(){
+    void stringSum_null_또는_빈문자() {
         //given//when
-        int result =calculator.stringSum(null);
+        int result = calculator.stringSum(null);
         //then
         assertThat(result).isEqualTo(0);
         //given//when
-        result =calculator.stringSum("");
+        result = calculator.stringSum("");
         //then
         assertThat(result).isEqualTo(0);
     }
+
     @Test
     @DisplayName("문자열 1을 넣으면 1을 반환한다.")
-    void stringSum_숫자하나(){
+    void stringSum_숫자하나() {
         //given//when
         int result = calculator.stringSum("1");
         //then
@@ -38,7 +40,7 @@ public class StringCalculatorTest {
 
     @Test
     @DisplayName("문자열 1,2를 넣으면 ,를 제거하고 합 3을 반환한다.")
-    void stringSum_숫자두개(){
+    void stringSum_숫자두개() {
         //given//when
         int result = calculator.stringSum("1,2");
         //then
@@ -47,10 +49,21 @@ public class StringCalculatorTest {
 
     @Test
     @DisplayName("문자열 1,2:3를 넣으면 ,와 : 를제거하고 합 6을 반환한다.")
-    void stringSum_숫자_3개_구분자_추가_제거(){
+    void stringSum_숫자_3개_구분자_추가_제거() {
         //given//when
         int result = calculator.stringSum("1,2:3");
         //then
         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("//;\\n1;2;3 넣으면 ;를제거하고 합 6을 반환한다.")
+    void stringSum_custom_구분자_추가_제거() {
+        //given//when
+        int result = calculator.stringSum("//;\n1;2;3");
+        int result2 = calculator.stringSum("//!\n1!2!3");
+        //then
+        assertThat(result).isEqualTo(6);
+        assertThat(result2).isEqualTo(6);
     }
 }
