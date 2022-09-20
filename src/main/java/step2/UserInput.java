297ac149dbb70ce2d37de13a4f000675f00cbd5d
KimWanJoo <wjwan0915@gmail.com>
2022-09-21 01:10:32 +0900

    :recycle: refactor: class 이름 변경

--- src/main/java/step2/UserInput.java
+++ src/main/java/step2/UserInput.java
@@ -1,25 +1,25 @@
 package step2;
 
-public class Numbers {
+public class UserInput {
     private static final String DEFAULT_VALUE = "0";
     private final String numbers;
 
-    public Numbers(String numbers) {
-        this.numbers = vaildation(numbers);
+    public UserInput(String numbers) {
+        this.value = validation(numbers);
     }
 
-    public String numbers() {
-        return numbers;
+    public String value() {
+        return value;
     }
 
     private static String vaildation(String numbers) {
         if (numbers == null || numbers.isEmpty()) {
             return DEFAULT_VALUE;
         }
-        if (!numbers.matches("(.*)[\n]*[0-9](.*)")) {
+        if (!numbers.matches("(//.*\n)?.*[0-9].*")) {
             throw new RuntimeException("숫자가 없습니다.");
         }
-        if (numbers.matches("(.*)-[0-9](.*)")) {
+        if (numbers.matches("(//.*\n)?.*-[0-9].*")) {
             throw new RuntimeException("음수는 입력할 수 없습니다.");
         }
         return numbers;
