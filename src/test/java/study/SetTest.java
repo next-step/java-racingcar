1b3ae7eb8431fc0a041d7e9632705df68068f5fe
jimbaemon <jimbae@kakao.com>
2022-09-20 21:35:27 +0900

    test: set의 사이즈를 확인하는 요구사항1 테스트 생성.

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,32 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void 요구사항1_SET의_사이즈확인(){
+        int size = numbers.size();
+
+        assertThat(size).isEqualTo(3);
+    }
+
+
+}
