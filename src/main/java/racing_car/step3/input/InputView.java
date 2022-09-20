58c128bb6b4eda4f313b3e0fff5e87dfdcc18052
JunH <tjdtls690@gmail.com>
2022-09-20 18:21:07 +0900

    :sparkles: feat : 숫자가 아닌 데이터를 입력 시 다시 입력받기 기능 구현

--- src/main/java/racing_car/step3/input/InputView.java
+++ src/main/java/racing_car/step3/input/InputView.java
@@ -1,11 +1,13 @@
 package racing_car.step3.input;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     public static final String INPUT_NUMBER_OF_CARS_MESSAGE = "자동차 대수는 몇 대인가요?";
     public static final String INPUT_NUMBER_OF_TRY_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    public static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "숫자만 입력할 수 있습니다. 다시 입력해 주세요.";
     
     public static int inputNumberOfCars() {
         System.out.println(INPUT_NUMBER_OF_CARS_MESSAGE);
@@ -18,6 +20,11 @@ public class InputView {
     }
     
     private static int inputNum() {
-        return SCANNER.nextInt();
+        try {
+            return SCANNER.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println(NUMBER_FORMAT_EXCEPTION_MESSAGE);
+            return inputNum();
+        }
     }
 }
