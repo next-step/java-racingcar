7b1bbde7bce5190629e942762be90cbbf6bc3cb3
wooris32 <wooris9032@gmail.com>
2022-09-20 20:50:23 +0900

    20220920 1단계-학습테스트 실습: 요구사항2  "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.

--- src/test/java/Study/StringTest.java
+++ src/test/java/Study/StringTest.java
@@ -7,8 +7,19 @@ public class StringTest {
 
     @Test
     void split(){
-        String[] test12 = "1,2".split(",");
-        assertThat(test12).contains("1");
-        assertThat(test12).containsExactly("1","2");
+        String[] stringTest1 = "1,2".split(",");
+        assertThat(stringTest1).contains("1","2");
+
+        String[] stringTest2 = "1".split(",");
+        assertThat(stringTest2).containsExactly("1");
+    }
+
+    @Test
+    void testSubstring(){
+        assertThat(substring("(1,2)")).contains("1,2");
+    }
+
+    public String substring(String s){
+        return s.substring(1,4);
     }
 }
