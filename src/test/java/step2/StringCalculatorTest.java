efb15f1784abbff804bc830f2f9a9518ae771f99
suchan <aud248@naver.com>
2022-09-20 16:13:00 +0900

    test: splitAndSum() 메소드 커스텀 구분자 테스트 추가

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -1,16 +1,25 @@
 package step2;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
 
     @Test
     @DisplayName("add() 메소드는 쉼표 또는 콜론을 구분자를 기준으로 분리한 각 수자의 합을 반환한다.")
-    public void split_and_add(){
+    void split_and_add(){
         StringCalculator calculator = new StringCalculator();
-        Integer sum =  calculator.splitAndAdd("1:2,3");
-        Assertions.assertThat(sum).isEqualTo(6);
+        Integer sum =  calculator.splitAndSum("1:2,3");
+        assertThat(sum).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자를 지정해서 문자열을 자른 후 더할 수 있다")
+    void split_and_add_by_custom_delimiter(){
+        StringCalculator calculator = new StringCalculator();
+        Integer sum = calculator.splitAndSum("//;\n1;2;3;");
+        assertThat(sum).isEqualTo(6);
     }
 }
