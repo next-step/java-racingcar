43f0ebeeadcb61be16ea71f6400844683110f976
nooose <hd15807@gmail.com>
2022-09-20 23:36:35 +0900

    refactor: 상수 선언 및 검증 메서드 수정 무엇을 의미하는지 정확히 표현했고, 다른 검증 로직이 추가될 수 있으니 메서드를 분리했다.

--- src/main/java/calculator/PositiveNumber.java
+++ src/main/java/calculator/PositiveNumber.java
@@ -4,11 +4,15 @@ public class PositiveNumber {
     private final int number;
 
     public PositiveNumber(int number) {
+        validate(number);
+
+        this.number = number;
+    }
+
+    private static void validate(int number) {
         if (isNegative(number)) {
             throw new RuntimeException();
         }
-
-        this.number = number;
     }
 
     private static boolean isNegative(int number) {
--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -5,8 +5,11 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
-    public static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
-    public static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+    private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final int CUSTOM_DELIMITER_POSITION = 1;
+    private static final int NUMBERS_POSITION = 2;
+
 
     public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
@@ -25,8 +28,8 @@ public class StringAddCalculator {
     private static String[] splitToken(String input) {
         Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(input);
         if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            return matcher.group(2).split(customDelimiter);
+            String customDelimiter = matcher.group(CUSTOM_DELIMITER_POSITION);
+            return matcher.group(NUMBERS_POSITION).split(customDelimiter);
         }
 
         return input.split(DEFAULT_DELIMITER_REGEX);
