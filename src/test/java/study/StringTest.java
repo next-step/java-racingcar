d1dee8fabc886b550a6eeb3cf5ab1b082b03b815
charlotte.chk <charlotte.chk@kakaocorp.com>
2022-09-20 23:45:53 +0900

    요구사항 3 charAt 메서드 정상 동작 테스트 누락 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -25,9 +27,15 @@ class StringTest {
         assertThat(result).isEqualTo("1,2");
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"0,a", "1,b", "2,c"}, delimiter = ',')
+    void charAt(int index, char expected) {
+        assertThat("abc".charAt(index)).isEqualTo(expected);
+    }
+
     @DisplayName("charAt 메서드에서 위치 값이 문자열의 범위를 벗어나면 에러가 발생한다.")
     @Test
-    void charAt() {
-        assertThatThrownBy(() -> "abc".charAt(4)).isInstanceOf(StringIndexOutOfBoundsException.class);
+    void charAtOutOfIndex() {
+        assertThatThrownBy(() -> "abc".charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
