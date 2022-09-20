d0ceb89fd3a2f1f8b1b3328ab17318355065acb5
jimbaemon <jimbae@kakao.com>
2022-09-20 21:46:47 +0900

    test: Parametrized Csv Source 를 이용하여 특정값이 존재하는지 확인하는 요구사항3 테스트 생성.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,6 +35,14 @@ public class SetTest {
     @ValueSource(ints = {1,2,3})
     void 요구사항2_특정값이_존재하는지_확인(int givenValue){
         assertThat(numbers.contains(givenValue)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true","2:true","3:true","4:false","5:false"}, delimiter = ':')
+    void 요구사항3_특정값이_존재하는지_확인(String csvValue, String csvExpected){
+        Integer givenValue = Integer.valueOf(csvValue);
+        Boolean expected = Boolean.valueOf(csvExpected);
+        assertThat(numbers.contains(givenValue)).isEqualTo(expected);
     }
 
 }
