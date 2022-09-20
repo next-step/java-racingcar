480337d3070983b23b42439ee476f15ee05faa32
JunH <tjdtls690@gmail.com>
2022-09-20 18:12:53 +0900

    :sparkles: feat : 이동 시도 횟수 입력 기능 구현

--- src/main/java/racing_car/step3/domain/input/InputView.java
+++ src/main/java/racing_car/step3/domain/input/InputView.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
+    public static final String INPUT_NUMBER_OF_CARS_MESSAGE = "자동차 대수는 몇 대인가요?";
+    public static final String INPUT_TRY_NUM_MESSAGE = "시도할 회수는 몇 회 인가요?";
     
-    public static int inputNum() {
-        System.out.println("자동차 대수는 몇 대인가요?");
+    public static int inputNumberOfCars() {
+        System.out.println(INPUT_NUMBER_OF_CARS_MESSAGE);
+        return inputNum();
+    }
+    
+    public static int inputTryNum() {
+        System.out.println(INPUT_TRY_NUM_MESSAGE);
+        return inputNum();
+    }
+    
+    private static int inputNum() {
         return SCANNER.nextInt();
     }
 }
