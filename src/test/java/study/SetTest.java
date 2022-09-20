5b7e06a2a369a4187f5735ddfbffc48cac8eef46
모진혁 <gkgk246@naver.com>
2022-09-20 21:31:32 +0900

    SetTest 요구사항 1 Set의 Size 구하는 Test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+	
+	@Test
+	@DisplayName("자료구조 Set의 크기를 확인하는 Test")
+	void sizeTest() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+	
+}
