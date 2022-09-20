3ce807dec7e6e76a2c0808e4a2db2b2c6089c03e
kkywalk2 <kkywalk2@gmail.com>
2022-09-20 21:33:03 +0900

    feat: Set Collection에 대한 학습 테스트

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인")
+    void sizeTest() {
+        int expected = 3;
+
+        assertThat(numbers.size()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인")
+    void contains(int given) {
+        assertThat(numbers.contains(given)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트")
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(int given, boolean expected) {
+        assertThat(numbers.contains(given)).isEqualTo(expected);
+    }
+}
