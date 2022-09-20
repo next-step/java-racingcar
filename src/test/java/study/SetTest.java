62f7f792812675f98341cb77a27c823e9ec38b5b
Yunsik-Choi <choiys0311@gmail.com>
2022-09-20 20:22:15 +0900

    set isContains test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class SetTest {
 
@@ -33,6 +34,23 @@ public class SetTest {
     @ValueSource(ints = {1,2,3})
     void contains(int num){
         Assertions.assertThat(numbers.contains(num)).isTrue();
+    }
+
+
+    private static Stream<Arguments> providesForIsContains(){
+        return Stream.of(
+                Arguments.of(1,true),
+                Arguments.of(2,true),
+                Arguments.of(3,true),
+                Arguments.of(4,false),
+                Arguments.of(5,false)
+        );
+    }
+
+    @ParameterizedTest(name = "set이 추가한 값을 포함하고 있는지 성공하는 케이스와 실패하는 케이스를 테스트한다.")
+    @MethodSource("providesForIsContains")
+    void isContains(int num, boolean expected){
+        Assertions.assertThat(numbers.contains(num)).isEqualTo(expected);
     }
 
 }
