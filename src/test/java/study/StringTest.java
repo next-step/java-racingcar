178dd471cf3e24400e6cbd19938cc3b82dbc9cd4
jimbaemon <jimbae@kakao.com>
2022-09-20 21:34:08 +0900

    test: subString 을 통한 소괄호 제거 요구사항2 테스트 생성.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,5 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,20 +7,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 
     @Test
-    void 요구사항1_split시_1과2분리_확인(){
+    void 요구사항1_split시_1과2분리_확인() {
         String givenString = "1,2";
 
         String[] split = givenString.split(",");
 
-        assertThat(split).contains("1","2");
+        assertThat(split).contains("1", "2");
     }
 
     @Test
-    void 요구사항1_split시_1만_포함하느배열_확인(){
+    void 요구사항1_split시_1만_포함하느배열_확인() {
         String givenString = "1";
 
         String[] split = givenString.split(",");
 
         assertThat(split).containsExactly("1");
+    }
+
+    @Test
+    void 요구사항2_소괄호_제거여부_확인() {
+        String givenString = "(1,2)";
+
+        String subString = givenString.substring(1, 3);
+
+        assertThat(subString).isEqualTo("1,2");
     }
 }
