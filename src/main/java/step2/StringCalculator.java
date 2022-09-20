fbeefb2192462699523ab8338313ca90237ebe9b
KimWanJoo <wjwan0915@gmail.com>
2022-09-21 01:12:35 +0900

    :recycle: refactor: 정규식 변경 및 메서드 로직 리팩토링

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -5,18 +5,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    private static final Pattern CUSTOM_SEPARATOR = Pattern.compile(("//(.)\n(.*)"));
-    private static final String FIRST_DEFAULT_DELIMITER = ":";
-    private static final String SECOND_DEFAULT_DELIMITER = ",";
-    private static final String OR = "|";
-    private final Numbers numbers;
+    // "1:2:3:4,5"
+    private static final Pattern PATTERN = Pattern.compile(("//(.)\n(.*)"));
+    private static final String DEFAULT_SEPARATOR = ":|,";
+    private final UserInput userInput;
 
-    public StringCalculator(Numbers numbers) {
-        this.numbers = numbers;
-    }
-
-    public StringCalculator(String numbers) {
-        this(new Numbers(numbers));
+    public StringCalculator(UserInput userInput) {
+        this.userInput = userInput;
     }
 
     public int calculate() {
@@ -27,16 +22,20 @@ public class StringCalculator {
                 .sum();
     }
 
-    private String getRegex() {
-        Matcher separatorMatcher = CUSTOM_SEPARATOR.matcher(numbers.numbers());
-        String regex = FIRST_DEFAULT_DELIMITER + OR + SECOND_DEFAULT_DELIMITER;
-        if (separatorMatcher.find()) {
-            regex = separatorMatcher.group(1);
+    @SuppressWarnings("magicNumber")
+    private String extractSeparator() {
+        Matcher matcher = PATTERN.matcher(userInput.value());
+        if (matcher.find()) {
+            return matcher.group(1);
         }
-        return regex;
+        return DEFAULT_SEPARATOR;
     }
 
-    private String deleteSeparator() {
-        return numbers.numbers().replaceAll("//\\D\\n", "");
+    private String extractExpression() {
+        Matcher matcher = PATTERN.matcher(userInput.value());
+        if (matcher.find()) {
+            return matcher.group(2);
+        }
+        return userInput.value();
     }
 }
