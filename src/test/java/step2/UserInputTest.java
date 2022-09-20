0f626e748d6fa4514f69644d8e5fd2a1791742ca
KimWanJoo <wjwan0915@gmail.com>
2022-09-21 01:11:15 +0900

    :white_check_mark: test: class 이름 변경

--- src/test/java/step2/UserInputTest.java
+++ src/test/java/step2/UserInputTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class NumbersTest {
+class UserInputTest {
 
     @Test
     @DisplayName("숫자가 없을 경우 예외가 발생한다")
     void noneNumberThrowExceptionTest() {
-        assertThatThrownBy(() -> new Numbers("test"))
+        assertThatThrownBy(() -> new UserInput("test"))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("숫자가 없습니다.");
     }
@@ -19,7 +19,7 @@ class NumbersTest {
     @Test
     @DisplayName("음수를 전달할 경우 예외가 발생한다")
     void hasMinusThrowException() {
-        assertThatThrownBy(() -> new Numbers("3,-1,2"))
+        assertThatThrownBy(() -> new UserInput("3,-1,2"))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("음수는 입력할 수 없습니다.");
     }
@@ -28,7 +28,7 @@ class NumbersTest {
     @DisplayName("빈 값일 경우 0을 반환한다")
     void emptyStringTest() {
         //given
-        Numbers numbers = new Numbers("");
+        UserInput numbers = new UserInput("");
         StringCalculator stringCalculator = new StringCalculator(numbers);
 
         //when
@@ -42,7 +42,7 @@ class NumbersTest {
     @DisplayName("null 값일 경우 0을 반환한다")
     void nullStringTest() {
         //given
-        Numbers numbers = new Numbers(null);
+        UserInput numbers = new UserInput(null);
         StringCalculator stringCalculator = new StringCalculator(numbers);
 
         //when
