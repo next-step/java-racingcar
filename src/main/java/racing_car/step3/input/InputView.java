d9d9ad59f071674221e3342d3774f60dc4e6cd7a
JunH <tjdtls690@gmail.com>
2022-09-20 18:14:47 +0900

    :art: refactor : 패키지 구조 및 메서드, 메시지 상수 이름 변경

--- src/main/java/racing_car/step3/input/InputView.java
+++ src/main/java/racing_car/step3/input/InputView.java
@@ -1,20 +1,19 @@
-package racing_car.step3.domain.input;
+package racing_car.step3.input;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     public static final String INPUT_NUMBER_OF_CARS_MESSAGE = "자동차 대수는 몇 대인가요?";
-    public static final String INPUT_TRY_NUM_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    public static final String INPUT_NUMBER_OF_TRY_MESSAGE = "시도할 회수는 몇 회 인가요?";
     
     public static int inputNumberOfCars() {
         System.out.println(INPUT_NUMBER_OF_CARS_MESSAGE);
         return inputNum();
     }
     
-    public static int inputTryNum() {
-        System.out.println(INPUT_TRY_NUM_MESSAGE);
+    public static int inputNumberOfTry() {
+        System.out.println(INPUT_NUMBER_OF_TRY_MESSAGE);
         return inputNum();
     }
     
