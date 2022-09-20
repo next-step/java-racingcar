ef5f127c631b6e54692d29c7edc88e8fab8ad8af
sienna <20200803@sungshin.ac.kr>
2022-09-20 12:04:18 +0900

    step1 : String 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,7 +1,7 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,12 +9,44 @@ import org.junit.jupiter.api.Test;
 public class StringTest {
 
   @Test
-  @DisplayName("집합 분리 테스트")
-  void split() {
+  @DisplayName("#요구사항1 - 집합 분리 테스트(1)")
+  void split_one_and_two() {
 
     String[] result = "1,2".split(",");
-    assertThat(result).containsExactly("1", "2");
 
+    assertThat(result).containsExactly("1", "2");
+    assertThat(result).contains("1","2");
+
+  }
+
+  @Test
+  @DisplayName("#요구사항1 - 집합 분리 테스트(2)")
+  void split_one() {
+
+    String[] result2 = "1".split(",");
+    assertThat(result2).containsExactly("1");
+
+  }
+
+  @Test
+  @DisplayName("#요구사항2 - 괄호 제거")
+  void delete_괄호() {
+
+    String input = "(1,2)";
+    assertThat(input.substring(1,4)).isEqualTo("1,2");
+
+  }
+
+  @Test
+  @DisplayName("#요구사항3 -잘못된 인덱스 가져오기")
+  void 인덱스_가져오기() {
+
+    String input = "a,b,c";
+    int index = 10;
+
+    assertThatThrownBy(() -> {
+      input.charAt(index);
+    }).isInstanceOf(StringIndexOutOfBoundsException.class);
   }
 
 }
