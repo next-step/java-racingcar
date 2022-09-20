bf84fe8f1e80a76569a9b178e554c334fadf5648
Yunsik-Choi <choiys0311@gmail.com>
2022-09-20 20:27:29 +0900

    cleanup code style

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -31,25 +31,25 @@ public class SetTest {
     }
 
     @ParameterizedTest(name = "set이 추가한 값을 포함하고 있는지 검사한다.")
-    @ValueSource(ints = {1,2,3})
-    void contains(int num){
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int num) {
         Assertions.assertThat(numbers.contains(num)).isTrue();
     }
 
 
-    private static Stream<Arguments> providesForIsContains(){
+    private static Stream<Arguments> providesForIsContains() {
         return Stream.of(
-                Arguments.of(1,true),
-                Arguments.of(2,true),
-                Arguments.of(3,true),
-                Arguments.of(4,false),
-                Arguments.of(5,false)
+                Arguments.of(1, true),
+                Arguments.of(2, true),
+                Arguments.of(3, true),
+                Arguments.of(4, false),
+                Arguments.of(5, false)
         );
     }
 
     @ParameterizedTest(name = "set이 추가한 값을 포함하고 있는지 성공하는 케이스와 실패하는 케이스를 테스트한다.")
     @MethodSource("providesForIsContains")
-    void isContains(int num, boolean expected){
+    void isContains(int num, boolean expected) {
         Assertions.assertThat(numbers.contains(num)).isEqualTo(expected);
     }
 
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,18 +1,20 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
     @Test
     void split() {
         String[] splitArray1 = "1,2".split(",");
-        Assertions.assertThat(splitArray1).contains("1", "2");
+        assertThat(splitArray1).contains("1", "2");
 
         String[] splitArray2 = "1".split(",");
-        Assertions.assertThat(splitArray2).containsExactly("1");
+        assertThat(splitArray2).containsExactly("1");
     }
 
     @Test
@@ -21,23 +23,24 @@ public class StringTest {
 
         String result = str.substring(str.indexOf("(") + 1, str.indexOf(")"));
 
-        Assertions.assertThat(result).isEqualTo("1,2");
+        assertThat(result).isEqualTo("1,2");
     }
+
 
     @DisplayName("charAt() 메소드를 활용해 특정 위치의 문자를 가져온다. 만약 위치 값을 벗어나면 StringIndexOutOfBoundsException.class를 예외를 발생시킨다.")
     @Test
-    void charAt(){
+    void charAt() {
         String str = "abc";
 
         org.junit.jupiter.api.Assertions.assertAll(
-                () -> Assertions.assertThat(str.charAt(0)).isEqualTo('a'),
-                () -> Assertions.assertThat(str.charAt(1)).isEqualTo('b'),
-                () -> Assertions.assertThat(str.charAt(2)).isEqualTo('c')
+                () -> assertThat(str.charAt(0)).isEqualTo('a'),
+                () -> assertThat(str.charAt(1)).isEqualTo('b'),
+                () -> assertThat(str.charAt(2)).isEqualTo('c')
         );
 
-        Assertions.assertThatThrownBy(() -> {
-                    str.charAt(3);
-                }).isInstanceOf(StringIndexOutOfBoundsException.class)
+        assertThatThrownBy(() -> {
+            str.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
                 .hasMessageContaining("String index out of range");
     }
 
