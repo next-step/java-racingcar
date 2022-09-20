ca168c99cc676cd051ff1a9b611cb3b9b29bad6f
suchan <aud248@naver.com>
2022-09-20 16:13:17 +0900

    feat: splitAndSum() 메소드 커스텀 구분자 기능 추가

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -1,9 +1,30 @@
 package step2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
-    public Integer splitAndAdd(String text) {
+    public Integer splitAndSum(String text) {
+        String[] numbers = split(text);
+
+        return sum(numbers);
+    }
+
+    public String[] split(String text) {
+        String delimiter = ":|,";
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+
+        if (m.find()) {
+            delimiter = m.group(1);
+            text = m.group(2);
+        }
+
+        return text.split(delimiter);
+    }
+
+    public Integer sum(String[] numbers) {
         Integer sum = 0;
-        String[] numbers = text.split(":|,");
 
         for (String number : numbers) {
             sum += Integer.parseInt(number);
