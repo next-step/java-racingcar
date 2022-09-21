f13b98ce452f8d86a23fad7716751568fdad7efc
jungwoon727 <jungwoon727@gmail.com>
2022-09-21 14:43:34 +0900

    test: 쉼표와 콜론을 구분자하는 테스트 추가
    쉽표와 콜론을 구분자로 가지는 문자열이 입력되면 구분자를 기준으로 분리한 각 숫자의 합을 반환하는지 확인하는 테스트

--- src/test/java/string/StringAddCalculatorTest.java
+++ src/test/java/string/StringAddCalculatorTest.java
@@ -44,4 +44,11 @@ class StringAddCalculatorTest {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
+
+    @Test
+    @DisplayName("쉽표와 콜론을 구분자로 가지는 문자열이 입력되면 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
 }
