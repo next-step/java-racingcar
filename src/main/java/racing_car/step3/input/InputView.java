a5e8f7be5e72230d2b880150c561f70ebe724cc3
JunH <tjdtls690@gmail.com>
2022-09-21 20:50:45 +0900

    :art: refactor : 변수 순서 및 줄바꿈 리팩토링

--- src/main/java/racing_car/step3/input/InputView.java
+++ src/main/java/racing_car/step3/input/InputView.java
@@ -3,11 +3,12 @@ package racing_car.step3.input;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner SCANNER = new Scanner(System.in);
     public static final String INPUT_NUMBER_OF_CARS_MESSAGE = "자동차 대수는 몇 대인가요?";
     public static final String INPUT_NUMBER_OF_TRY_MESSAGE = "시도할 회수는 몇 회 인가요?";
     public static final String NUMBER_FORMAT_EXCEPTION_MESSAGE = "숫자만 입력할 수 있습니다. 다시 입력해 주세요.";
     public static final String NEGATIVE_NUMBER_EXCEPTION_MESSAGE = "음수는 입력할 수 없습니다. 다시 입력해주세요.";
+    
+    private static final Scanner SCANNER = new Scanner(System.in);
     
     public static int inputNumberOfCars() {
         System.out.println(INPUT_NUMBER_OF_CARS_MESSAGE);
