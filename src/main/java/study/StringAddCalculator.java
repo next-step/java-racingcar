6addabc277a5758ccb6b4abf124b7f25d9bf8c82
jimbaemon <jimbae@kakao.com>
2022-09-21 23:39:36 +0900

    feat: custom 구분자 처리.

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,9 +1,18 @@
 package study;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
         if(text == null || text.isEmpty()) {
             return 0;
+        }
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if(matcher.find()){
+            String customDelimiter = matcher.group(1);
+            String[] split = matcher.group(2).split(customDelimiter);
+            return sum(split);
         }
         String[] split = split(text);
         return sum(split);
@@ -20,6 +29,4 @@ public class StringAddCalculator {
         }
         return result;
     }
-
-
 }
