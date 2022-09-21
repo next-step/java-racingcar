7e37868f4f45acf2ad276a86220722ce3b29d159
sienna <20200803@sungshin.ac.kr>
2022-09-22 01:50:47 +0900

    :white_check_mark:test : canMove()는 자동차의 이동여부에 따라 True/False를 반환한다

--- src/test/java/carRacing/level3/CarRacingTest.java
+++ src/test/java/carRacing/level3/CarRacingTest.java
@@ -15,4 +15,32 @@ public class CarRacingTest {
     assertThat(CarRace.getRandomNum()).isLessThan(10);
   }
 
+  @Test
+  @DisplayName("랜덤값이 4이상이면 True를 반환한다.")
+  void isTrue() {
+
+    int random1 = 5;
+    int random2 = 4;
+    int random3 = 9;
+
+    assertThat(CarRace.canMove(random1)).isTrue();
+    assertThat(CarRace.canMove(random2)).isTrue();
+    assertThat(CarRace.canMove(random3)).isTrue();
+
+  }
+
+  @Test
+  @DisplayName("랜덤값이 4 미만이면 false를 반환한다.")
+  void isFalse() {
+
+    int random1 = 0;
+    int random2 = 3;
+    int random3 = 2;
+
+    assertThat(CarRace.canMove(random1)).isFalse();
+    assertThat(CarRace.canMove(random2)).isFalse();
+    assertThat(CarRace.canMove(random3)).isFalse();
+
+  }
+
 }
