b6e0e568ddaa21e42b7a6b1129b4107a19b59b90
jimbae <58099885+jimbaemon@users.noreply.github.com>
2022-09-21 16:29:50 +0900

    1단계 - 학습 테스트 실습 PR (#3462)
    * test: "1,2" 분리 확인, "1" 분리시 1만 포함되어있는지 확인하는 요구사항1 테스트
    
    * test: subString 을 통한 소괄호 제거 요구사항2 테스트 생성.
    
    * test: 특정문자 위치값을 벗어나는 경우 에러가 발생하는지 확인하는 요구사항3 테스트 생성.
    
    * test: set의 사이즈를 확인하는 요구사항1 테스트 생성.
    
    * test: Parametrized Value Source 를 이용하여 특정값이 존재하는지 확인하는 요구사항2 테스트 생성.
    
    * test: Parametrized Csv Source 를 이용하여 특정값이 존재하는지 확인하는 요구사항3 테스트 생성.
    
    * fix: 요구사항2 범위설정을 잘못하여 발생하는 테스트 수정.

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,48 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void 요구사항1_SET의_사이즈확인(){
+        int size = numbers.size();
+
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void 요구사항2_특정값이_존재하는지_확인(int givenValue){
+        assertThat(numbers.contains(givenValue)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true","2:true","3:true","4:false","5:false"}, delimiter = ':')
+    void 요구사항3_특정값이_존재하는지_확인(String csvValue, String csvExpected){
+        Integer givenValue = Integer.valueOf(csvValue);
+        Boolean expected = Boolean.valueOf(csvExpected);
+        assertThat(numbers.contains(givenValue)).isEqualTo(expected);
+    }
+
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,52 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    void 요구사항1_split시_1과2분리_확인() {
+        String givenString = "1,2";
+
+        String[] split = givenString.split(",");
+
+        assertThat(split).contains("1", "2");
+    }
+
+    @Test
+    void 요구사항1_split시_1만_포함하느배열_확인() {
+        String givenString = "1";
+
+        String[] split = givenString.split(",");
+
+        assertThat(split).containsExactly("1");
+    }
+
+    @Test
+    void 요구사항2_소괄호_제거여부_확인() {
+        String givenString = "(1,2)";
+
+        String subString = givenString.substring(1, 4);
+
+        assertThat(subString).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정문자 위치값을 벗어나는 경우 에러가 발생하는지 확인한다.")
+    void 요구사항3_특정문자_위치값을_벗어나는경우_에러발생() {
+        String givenString = "abc";
+        int givenIndex = 5;
+
+        assertThatThrownBy(() -> charAt(givenString, givenIndex))
+            .isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessage("String index out of range: %d", givenIndex);
+    }
+
+    private char charAt(String string, int index) {
+        return string.charAt(index);
+    }
+}
