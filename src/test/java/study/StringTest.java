4fc0aba6b2aa4e972c64ece1dbb3241a62776c33
KangBada <kangbada0728@gmail.com>
2022-09-21 16:26:55 +0900

    1단계 - 학습 테스트 실습 (#3483)
    * add: String 클래스에 대한 학습 테스트
    
    * refactor: DisplayName을 더 명시적으로 변경
    
    * add: Set Collection에 대한 학습 테스트

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    @DisplayName("split 메서드 학습 테스트")
+    void split() {
+        String[] strings = "1,2".split(",");
+        assertThat(strings).contains("1", "2");
+
+        String[] strings2 = "1".split(",");
+        assertThat(strings2).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("substring 메서드 학습 테스트")
+    void substring() {
+        String substringResult = "(1,2)".substring(1, 4);
+        assertThat(substringResult).contains("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 메서드 학습 테스트")
+    void charAt() {
+        char c = "abc".charAt(0);
+        assertThat(c).isEqualTo('a');
+        assertThatThrownBy(() -> {
+            "abc".charAt(10);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+          .hasMessageContaining("String index out of range: 10");
+    }
+}
