a3f7999d3eb191930cb4d8c5b4566c22b457b144
KimWanJoo <wjwan0915@gmail.com>
2022-09-21 10:56:10 +0900

    :recycle: refactor: 출력 관련 리팩토링
    출력에 대한 책임 가질 수 있게 리팩토링

--- src/main/java/step3/application/ConsoleOut.java
+++ src/main/java/step3/application/ConsoleOut.java
@@ -1,8 +1,6 @@
 package step3.application;
 
-import step3.domain.Car;
 import step3.domain.CarRace;
-import step3.domain.NumberGenerator;
 
 public class ConsoleOut {
     public void askCarsCount() {
@@ -13,12 +11,10 @@ public class ConsoleOut {
         System.out.println("시도할 회수는 몇 회 인가요?");
     }
 
-    public void result(CarRace carRace, NumberGenerator numberGenerator) {
-        carRace.cars().stream().map(car -> {
-            Car moveCar = car.move(numberGenerator);
-            String strState = "-".repeat(moveCar.state());
+    public void result(CarRace carRace) {
+        carRace.cars().forEach(car -> {
+            String strState = "-".repeat(car.state());
             System.out.println(strState);
-            return moveCar;
         });
         System.out.println();
 
