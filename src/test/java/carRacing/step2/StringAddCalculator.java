8aab90369b7603ea1f943c3e884647356203bdb9
bada.kang <kangbada0728@gmail.com>
2022-09-21 22:46:41 +0900

    add: 2단계 - 문자열 덧셈 계산기 (1차)

---
+++ src/test/java/carRacing/step2/StringAddCalculator.java
@@ -0,0 +1,48 @@
+package carRacing.step2;
+
+import java.util.StringTokenizer;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        StringBuilder delim = new StringBuilder(",:");
+        if(text.charAt(0) == '/') {
+            text = text.substring(getStartIdxAndDelim(text, delim));
+        }
+
+        StringTokenizer tokens = new StringTokenizer(text, delim.toString());
+        int result = 0;
+        while (tokens.hasMoreTokens()) {
+            int number = getNumber(tokens);
+            result += number;
+        }
+
+        return result;
+    }
+
+    private static int getStartIdxAndDelim(String text, StringBuilder delim) {
+        int idx = 2;
+        while(true) {
+            char c = text.charAt(idx);
+            if(c == '\n') {
+                idx += 1;
+                break;
+            }
+            delim.append(c);
+            idx += 1;
+        }
+        return idx;
+    }
+
+    private static int getNumber(StringTokenizer tokens) {
+        int number = Integer.parseInt(tokens.nextToken());
+        if(number < 0) {
+            throw new RuntimeException("음수 발생");
+        }
+        return number;
+    }
+
+}
---
+++ src/test/java/carRacing/step2/StringAddCalculatorTest.java
@@ -0,0 +1,58 @@
+package carRacing.step2;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class StringAddCalculatorTest {
+
+    @Test
+    @Order(1)
+    public void splitAndSum_null_또는_빈문자() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @Order(2)
+    public void splitAndSum_숫자하나() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @Order(3)
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @Order(4)
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @Order(5)
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @Order(6)
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
