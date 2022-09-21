49c7d5a7715038cac1d247201a067cb22fb0936a
suchan <aud248@naver.com>
2022-09-22 08:22:07 +0900

    test: splitAndSum() 메소드 문자값 입력 예외 테스트

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
 
     @Test
-    @DisplayName("add() 메소드는 쉼표 또는 콜론을 구분자를 기준으로 분리한 각 수자의 합을 반환한다.")
+    @DisplayName("splitAndAdd() 메소드는 쉼표 또는 콜론을 구분자를 기준으로 분리한 각 수자의 합을 반환한다.")
     void split_and_add(){
         StringCalculator calculator = new StringCalculator();
         Integer sum =  calculator.splitAndSum("1:2,3");
@@ -21,5 +22,14 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         Integer sum = calculator.splitAndSum("//;\n1;2;3;");
         assertThat(sum).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("splitAndAdd() 메소드는 문자값이 전달되면 NumberFormatException을 던진다")
+    void split_and_add_throw_runtimeException(){
+        StringCalculator calculator = new StringCalculator();
+        assertThatThrownBy(() -> {
+            calculator.splitAndSum("a:e:d");
+        }).isInstanceOf(NumberFormatException.class);
     }
 }
