b6e994131b3d545cc5307f1cb0a67922aac5dfb0
hasunzo <gktjsdl470@gmail.com>
2022-09-21 16:22:35 +0900

    Step1 (#3473)
    * String 클래스에 대한 학습 테스트
    
    * Set Collection에 대한 학습 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    boolean isContains(int number) {
+        return numbers.contains(number);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        assertThat(isContains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void contains(int input, boolean isContains) {
+        assertThat(isContains(input)).isEqualTo(isContains);
+    }
+}
