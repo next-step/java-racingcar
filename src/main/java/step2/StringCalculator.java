0c56eb0b2eea1eaeff5bc1ffab4e596ff9efd031
suchan <aud248@naver.com>
2022-09-22 08:38:00 +0900

    test: splitAndSum() 메소드 음수값 입력 예외 테스트

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -23,11 +23,16 @@ public class StringCalculator {
         return text.split(delimiter);
     }
 
-    public Integer sum(String[] numbers) {
+    public Integer sum(String[] numbers){
         Integer sum = 0;
 
-        for (String number : numbers) {
-            sum += Integer.parseInt(number);
+        for (String number  : numbers) {
+            int parseInt = Integer.parseInt(number);
+
+            if(parseInt < 0){
+                throw new IllegalArgumentException("음수는 포함할 수 없습니다.");
+            }
+            sum += parseInt;
         }
 
         return sum;
