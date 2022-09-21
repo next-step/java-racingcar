256048cc6aa458e5c3ed8b19c8e019bac5220085
JunH <tjdtls690@gmail.com>
2022-09-21 22:09:19 +0900

    :white_check_mark: test : CarDTO 테스트 코드 추가

---
+++ src/test/java/racing_car/step3/dto/CarDTOTest.java
@@ -0,0 +1,19 @@
+package racing_car.step3.dto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racing_car.step3.domain.Car;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarDTOTest {
+    
+    @Test
+    @DisplayName("차량의 포지션 정보 확인")
+    void getPosition() {
+        Car car = new Car(3);
+        CarDTO carDTO = car.information();
+        assertThat(carDTO.getPosition()).isEqualTo(3);
+    }
+}
