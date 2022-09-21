19485f21f0ae9b82f9aea2f404d9d616498aff8a
suchan <aud248@naver.com>
2022-09-22 08:38:38 +0900

    test: splitAndSum() 메소드 음수값 입력 예외 테스트

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -10,15 +10,15 @@ public class StringCalculatorTest {
 
     @Test
     @DisplayName("splitAndAdd() 메소드는 쉼표 또는 콜론을 구분자를 기준으로 분리한 각 수자의 합을 반환한다.")
-    void split_and_add(){
+    void split_and_add() {
         StringCalculator calculator = new StringCalculator();
-        Integer sum =  calculator.splitAndSum("1:2,3");
+        Integer sum = calculator.splitAndSum("1:2,3");
         assertThat(sum).isEqualTo(6);
     }
 
     @Test
     @DisplayName("커스텀 구분자를 지정해서 문자열을 자른 후 더할 수 있다")
-    void split_and_add_by_custom_delimiter(){
+    void split_and_add_by_custom_delimiter() {
         StringCalculator calculator = new StringCalculator();
         Integer sum = calculator.splitAndSum("//;\n1;2;3;");
         assertThat(sum).isEqualTo(6);
@@ -26,10 +26,20 @@ public class StringCalculatorTest {
 
     @Test
     @DisplayName("splitAndAdd() 메소드는 문자값이 전달되면 NumberFormatException을 던진다")
-    void split_and_add_throw_runtimeException(){
+    void split_and_add_throw_NumberFormatException() {
         StringCalculator calculator = new StringCalculator();
         assertThatThrownBy(() -> {
             calculator.splitAndSum("a:e:d");
         }).isInstanceOf(NumberFormatException.class);
+    }
+
+    @Test
+    @DisplayName("splitAndAdd() 메소드는 음수값이 전달되면 IllegalArgumentException을 던진다")
+    void split_and_add_throw_IllegalArgumentException(){
+        StringCalculator calculator = new StringCalculator();
+        assertThatThrownBy(() -> {
+            calculator.splitAndSum("-1:2:5");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음수는 포함할 수 없습니다.");
     }
 }
