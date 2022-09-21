1401e773a8448f81f4bc9404f3cf9b8bc248bfb0
jungwoon727 <jungwoon727@gmail.com>
2022-09-21 15:03:13 +0900

    feat: 커스텀 구분자를 사용하여 각 값을 더할 수 있도록 수정
    '//'과'\n' 사이에 있는 문자를 구분자로 사용하여 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.

--- src/main/java/string/StringAddCalculator.java
+++ src/main/java/string/StringAddCalculator.java
@@ -1,6 +1,8 @@
 package string;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
@@ -17,6 +19,11 @@ public class StringAddCalculator {
     }
 
     private static String[] splitText(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
         return text.split("[,:]");
     }
 
