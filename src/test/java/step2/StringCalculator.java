0e957eb558df04a88b91dedca15ba3d70419fc40
wugatama <dnr7410@naver.com>
2022-09-21 22:21:41 +0900

    refactor: 문자열 분리할 때 음수나 숫자 아닐 때 조건 추가

--- src/test/java/step2/StringCalculator.java
+++ src/test/java/step2/StringCalculator.java
@@ -16,6 +16,9 @@ public class StringCalculator {
 	public int[] convertToInt(String[] arr) {
 		int[] convert = new int[arr.length];
 		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].charAt(0) == '-' || (89 < (arr[i].charAt(0)) && (arr[i].charAt(0)) < 80)) {
+				throw new RuntimeException();
+			}
 			convert[i] = Integer.parseInt(arr[i]);
 		}
 		return convert;
