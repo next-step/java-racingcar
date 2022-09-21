7ee775094d2842f1d5e26a0f2ac0b821c31f4a5b
jungwoon727 <jungwoon727@gmail.com>
2022-09-21 14:39:55 +0900

    feat: 문자열을 쉼표로 구분하여 각값을 더하여 반환 추가
    쉼표를 구분자로 가지는 문자열이 입력되면 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.

--- src/main/java/string/StringAddCalculator.java
+++ src/main/java/string/StringAddCalculator.java
@@ -1,12 +1,29 @@
 package string;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
 
     public static int splitAndSum(String text) {
+        int result = 0;
+
         if (isEmpty(text)) {
-            return 0;
+            return result;
         }
-        return convertNumber(text);
+
+        result = sum(splitText(text));
+
+        return result;
+    }
+
+    private static String[] splitText(String text) {
+        return text.split("[,]");
+    }
+
+    private static int sum(String[] splitText) {
+        return Arrays.stream(splitText)
+            .mapToInt(StringAddCalculator::convertNumber)
+            .sum();
     }
 
     private static int convertNumber(String text) {
