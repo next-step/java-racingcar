20d72ec978c9af9c9c001668d55f4f1f115014f3
jimbaemon <jimbae@kakao.com>
2022-09-21 23:13:28 +0900

    feat: 첫번쨰 테스트 요구사항 충족.

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,7 @@
+package study;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String text) {
+        return 0;
+    }
+}
--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringAddCalculatorTest {
