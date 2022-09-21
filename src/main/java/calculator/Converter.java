1d5f16f752f145fb4db9b43bc7f65373b2b81cd2
jhwlim <jhwlim25@gmail.com>
2022-09-21 20:31:15 +0900

    기능 요구사항 추가
    - null 또는 빈 문자열을 입력할 경우, 0 반환
    - 음수를 전달할 경우, RuntimeException 예외 발생

--- src/main/java/calculator/Converter.java
+++ src/main/java/calculator/Converter.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,6 +12,10 @@ public class Converter {
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
     public List<Integer> convert(String input) {
+        if (input == null || input.isEmpty()) {
+            return Collections.emptyList();
+        }
+
         Matcher matcher = pattern.matcher(input);
         if (matcher.find()) {
             return splitWithDelimiter(matcher.group(2), matcher.group(1));
@@ -20,8 +25,16 @@ public class Converter {
 
     private List<Integer> splitWithDelimiter(String input, String delimiter) {
         return Arrays.stream(input.split(delimiter))
-                .map(Integer::parseInt)
+                .map(this::parsePositiveIntOrThrow)
                 .collect(Collectors.toList());
+    }
+
+    private int parsePositiveIntOrThrow(String str) {
+        int num = Integer.parseInt(str);
+        if (num < 0) {
+            throw new RuntimeException("음수를 전달할 수 없습니다");
+        }
+        return num;
     }
 
 }
--- src/test/java/calculator/ConverterTest.java
+++ src/test/java/calculator/ConverterTest.java
@@ -17,13 +17,13 @@ class ConverterTest {
     @DisplayName("',' 또는 ':'을 구분자로 가지는 문자열을 변환할 때, 숫자 리스트를 반환해야 한다.")
     @ParameterizedTest
     @ValueSource(strings = {
-            "1,2,3",
-            "1,2:3",
-            "1:2:3"
+            "0,1,2",
+            "0,1:2",
+            "0:1:2"
     })
     void convert_withDefaultDelimiter(String input) {
         List<Integer> actual = converter.convert(input);
-        assertThat(actual).containsExactly(1, 2, 3);
+        assertThat(actual).containsExactly(0, 1, 2);
     }
 
     @DisplayName("',' 또는 ':'이 아닌 구분자를 포함하는 문자열을 변환할 때, 예외가 발생해야 한다.")
@@ -40,6 +40,13 @@ class ConverterTest {
         String input = "//;\n1;2;3";
         List<Integer> actual = converter.convert(input);
         assertThat(actual).containsExactly(1, 2, 3);
+    }
+
+    @DisplayName("음수를 포함하는 경우, 예외가 발생해야 한다.")
+    @Test
+    void convert_containingNegativeInt() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> converter.convert("1;-2;3"));
     }
 
 }
