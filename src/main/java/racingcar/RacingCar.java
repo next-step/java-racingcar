629c72f88fc8ed88a01e872f300654939f79990a
jinukix <jinukix@styleshare.kr>
2022-09-21 16:58:19 +0900

    feat: RacingCar 기능 구현

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+import java.util.Random;
+
+public class RacingCar {
+
+    private Random random;
+    private static final int MIN_RANDOM_NUM_CAN_MOVED = 4;
+
+    private final int MOVE_LENGTH = 1;
+    private int position = 0;
+
+
+    private boolean canMove() {
+        return MIN_RANDOM_NUM_CAN_MOVED <= random.nextInt();
+    }
+
+    public void move() {
+        if (canMove()) {
+            position += MOVE_LENGTH;
+        }
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
