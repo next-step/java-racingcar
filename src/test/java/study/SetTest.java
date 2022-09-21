ae969e11be62ac839631d9fb45f0804fa468e256
hocaron <kkannu0407@gmail.com>
2022-09-22 07:48:01 +0900

    test: Set Collection 요구사항 3에 대한 테스트 코드 작성

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -38,12 +39,31 @@ public class SetTest {
 	@DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인")
 	@ParameterizedTest
 	@ValueSource(ints = {1, 2, 3})
-	void Given_Set_When_Contains_Then_CheckValue(int index){
+	void Given_Set_When_Contains_Then_ReturnTrue(int value){
 
 		// when
-		boolean result = numbers.contains(index);
+		boolean result = numbers.contains(value);
 
 		// then
 		assertThat(result).isEqualTo(true);
+	}
+
+	@DisplayName("Set의 contains() 메소드를 활용해 존재하는 값은 true, 존재하지 않으면 false를 반환한다")
+	@ParameterizedTest
+	@CsvSource(value = {
+		"1:true",
+		"2:true",
+		"3:true",
+		"4:false",
+		"5:false"
+	},
+		delimiter = ':')
+	void Given_Set_When_Contains_Then_ReturnTrueAndFalse(int value, boolean hasValue){
+
+		// when
+		boolean result = numbers.contains(value);
+
+		// then
+		assertThat(result).isEqualTo(hasValue);
 	}
 }
