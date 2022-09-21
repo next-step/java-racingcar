30586fae7c7344a109925dd988eb76180e854762
doyle.min <doyle.min@kakaocorp.com>
2022-09-21 23:03:49 +0900

    2단계 : 마무리 코드 정리

--- src/main/java/com/nextstep/javaRacing/StringAddCalculator.java
+++ src/main/java/com/nextstep/javaRacing/StringAddCalculator.java
@@ -1,20 +1,20 @@
 package com.nextstep.javaRacing;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private static final Pattern singleNumberPattern = Pattern.compile("[-+]?\\d*");
-    private static final Pattern customDelimiterPattern = Pattern.compile("//(.)\n(.*)");
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     public static int splitAndSum(String s) {
         if (s == null || s.isEmpty()) return 0;
-        String[] tokens = tokenize(s);
-        return parseAndSum(tokens);
+        String[] tokens = split(s);
+        return sum(tokens);
     }
 
-    private static String[] tokenize(String s) {
-        Matcher matcher = customDelimiterPattern.matcher(s);
+    private static String[] split(String s) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(s);
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
             String valuePattern = matcher.group(2);
@@ -23,23 +23,18 @@ public class StringAddCalculator {
         return s.split("[,:]");
     }
 
-    private static int parseAndSum(String[] tokens) {
-        int result = 0;
-        for (String token : tokens) {
-            result += parse(token);
-        }
-        return result;
+    private static int sum(String[] tokens) {
+        return Arrays.stream(tokens).mapToInt(s->parse(s)).sum();
     }
 
     private static int parse(String s) {
-        if(s.charAt(0)=='-' || !isNumeric(s)){
+        if(!isNumeric(s)){
             throw new IllegalArgumentException("input must be positive number : " + s);
         }
         return Integer.parseInt(s);
     }
 
     private static boolean isNumeric(String s) {
-        Matcher m = singleNumberPattern.matcher(s);
-        return m.matches();
+        return s.chars().allMatch(Character::isDigit);
     }
 }
