7fed37fb0c23ff2a7a1c662af8419d704a006561
nooose <hd15807@gmail.com>
2022-09-22 02:11:00 +0900

    feat: 숫자 이외의 다른 값이면 재시도하도록 기능 구현

--- src/main/java/racingcar/view/RetryView.java
+++ src/main/java/racingcar/view/RetryView.java
@@ -2,16 +2,30 @@ package racingcar.view;
 
 public class RetryView {
 
+    private static final String RETRY_MESSAGE = "1 이상의 숫자만 입력해 주세요.";
+
     private RetryView() {
     }
 
     public static int retryIfInputNegativeNumber(View inputView) {
-        int number = Integer.parseInt(inputView.input());
+        int number;
 
-        if (number < 0) {
+        try {
+            number = Integer.parseInt(inputView.input());
+        } catch (NumberFormatException e) {
+            System.out.println(RETRY_MESSAGE);
+            return retryIfInputNegativeNumber(inputView);
+        }
+
+        if (isZeroOrNegative(number)) {
+            System.out.println(RETRY_MESSAGE);
             return retryIfInputNegativeNumber(inputView);
         }
 
         return number;
+    }
+
+    private static boolean isZeroOrNegative(int number) {
+        return number <= 0;
     }
 }
