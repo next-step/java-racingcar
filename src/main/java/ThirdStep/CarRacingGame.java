874de789c96cccdbc3fd39b5d3d8064f608f4f46
yerin-158 <hanblueblue@gmail.com>
2022-09-21 19:48:32 +0900

    feature : 게임에 참여하는 자동차 수와 이동 횟수를 입력받는다.

--- src/main/java/ThirdStep/CarRacingGame.java
+++ src/main/java/ThirdStep/CarRacingGame.java
@@ -1,4 +1,19 @@
 package ThirdStep;
 
+import java.util.Scanner;
+
 public class CarRacingGame {
+
+    public static int cars;
+    public static int attempts;
+    public static void racing() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        cars = scanner.nextInt();
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        attempts = scanner.nextInt();
+    }
+
 }
