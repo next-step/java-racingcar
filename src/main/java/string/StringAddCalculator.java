36c5a4a4c8943062355eee4bc283fe229930b16a
jungwoon727 <jungwoon727@gmail.com>
2022-09-21 14:14:09 +0900

    feat: 문자 -> 숫자 변환중 예외처리 추가
    문자를 숫자로 변환중 NumberFormatException이 발생하면 IncludeNotNumberException으로 포장하여 예외를 발생시킨다.

--- src/main/java/string/StringAddCalculator.java
+++ src/main/java/string/StringAddCalculator.java
@@ -6,7 +6,17 @@ public class StringAddCalculator {
         if (isEmpty(text)) {
             return 0;
         }
-        return Integer.parseInt(text);
+        return convertNumber(text);
+    }
+
+    private static int convertNumber(String text) {
+        int result = 0;
+        try {
+            result = Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            throw new IncludeNotNumberException(text);
+        }
+        return result;
     }
 
     private static boolean isEmpty(String text) {
