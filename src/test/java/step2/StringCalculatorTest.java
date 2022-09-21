b8db9548e83778d409c4306a6106cad9e5d99176
suchan <aud248@naver.com>
2022-09-22 08:54:48 +0900

    refactor: StringCalculator 객체 Test 인스턴스 변수로 리팩토링

--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -8,20 +8,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
 
+    StringCalculator calculator = new StringCalculator();
+
     @Test
     @DisplayName("splintAndAdd() 메소드에 null 혹은 빈 문자열이 입력되면 0을 반환한다")
     void split_and_add_return_0() {
-        StringCalculator calculator = new StringCalculator();
         Integer sumByEmpty = calculator.splitAndSum("");
-        Integer sumByNull = calculator.splitAndSum(null);
         assertThat(sumByEmpty).isEqualTo(0);
+
+        Integer sumByNull = calculator.splitAndSum(null);
         assertThat(sumByNull).isEqualTo(0);
     }
 
     @Test
     @DisplayName("splitAndAdd() 메소드는 쉼표 또는 콜론을 구분자를 기준으로 분리한 각 수자의 합을 반환한다.")
     void split_and_add() {
-        StringCalculator calculator = new StringCalculator();
         Integer sum = calculator.splitAndSum("1:2,3");
         assertThat(sum).isEqualTo(6);
     }
@@ -29,7 +30,6 @@ public class StringCalculatorTest {
     @Test
     @DisplayName("커스텀 구분자를 지정해서 문자열을 자른 후 더할 수 있다")
     void split_and_add_by_custom_delimiter() {
-        StringCalculator calculator = new StringCalculator();
         Integer sum = calculator.splitAndSum("//;\n1;2;3;");
         assertThat(sum).isEqualTo(6);
     }
@@ -37,7 +37,6 @@ public class StringCalculatorTest {
     @Test
     @DisplayName("splitAndAdd() 메소드는 문자값이 전달되면 NumberFormatException을 던진다")
     void split_and_add_throw_NumberFormatException() {
-        StringCalculator calculator = new StringCalculator();
         assertThatThrownBy(() -> {
             calculator.splitAndSum("a:e:d");
         }).isInstanceOf(NumberFormatException.class);
@@ -46,7 +45,6 @@ public class StringCalculatorTest {
     @Test
     @DisplayName("splitAndAdd() 메소드는 음수값이 전달되면 IllegalArgumentException을 던진다")
     void split_and_add_throw_IllegalArgumentException(){
-        StringCalculator calculator = new StringCalculator();
         assertThatThrownBy(() -> {
             calculator.splitAndSum("-1:2:5");
         }).isInstanceOf(IllegalArgumentException.class)
