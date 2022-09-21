5fa950aa327a4303923572a94219c92f6e861cd5
jungwoon727 <jungwoon727@gmail.com>
2022-09-21 13:24:28 +0900

    test: 문자 -> 숫자 변환시 예외발생 테스트
    문자에서 숫자 변환시 NumberFormatException이 발생할 경우 IncludeNotNumberException 예외가 발생되는지 확인하는 테스트 추가

--- src/test/java/string/StringAddCalculatorTest.java
+++ src/test/java/string/StringAddCalculatorTest.java
@@ -1,7 +1,9 @@
 package string;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +24,17 @@ class StringAddCalculatorTest {
     public void splitAndSum_숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("문자열 -> 숫자 변환시 NumberFormatException이 발생할 경우 IncludeNotNumberException 예외발생한다.")
+    public void splitAndSum_숫자변환시_예외발생() throws Exception {
+        String data = "A";
+
+        ThrowingCallable result = () -> StringAddCalculator.splitAndSum(data);
+
+        assertThatThrownBy(result)
+            .isInstanceOf(IncludeNotNumberException.class)
+            .hasMessageContaining("문자를 숫자로 변환중 숫자가 아닌 문자가 포함되어 있습니다. 문제발생문자 : " + data);
     }
 }
