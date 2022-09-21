e9b6750f12776981ba317022dfb60390abc8d8b3
yoonjeong <50859560+cyj199637@users.noreply.github.com>
2022-09-22 08:36:58 +0900

    Step1 - 학습 테스트 실습 (#3515)
    * (test): String 클래스, Set Collection 학습 테스트
    
    * (refactor): 함수명 수정

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,43 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size로_Set의_크기를_확인할_수_있다")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains로_Set의_요소를_확인할_수_있다")
+    void contains(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("contains로_Set의_요소를_확인할_수_있다_2")
+    void contains_withOtherResult(int value, boolean expected) {
+        assertThat(numbers.contains(value)).isEqualTo(expected);
+    }
+}
