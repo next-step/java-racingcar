d2b98c3dfe53da29d5ff6e77a7c74aa02682fc67
devcs96 <katd6@naver.com>
2022-09-21 20:24:35 +0900

    🔧 refactor  : 메소드,변수 네이밍 변경 , 역할에 따른 클래스 분리

--- src/test/java/calculator/StringAddCalculator.java
+++ src/test/java/calculator/StringAddCalculator.java
@@ -6,22 +6,20 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
-    private static final int DEFAULT_VALUE = 0;
+    private static final int DEFAULT_RESULT_VALUE = 0;
+    private static final String DEFAULT_DELIMITER = ":|,";
     private static final Pattern pattern = Pattern.compile(("//(.)\n(.*)"));
-    private static final String FIRST_DEFAULT_DELIMITER = ":";
-    private static final String SECOND_DEFAULT_DELIMITER = ",";
-    private static final String OR = "|";
 
     public static int splitAndSum(String input) throws IllegalArgumentException {
         if (isInvalid(input)) {
-            return DEFAULT_VALUE;
+            return DEFAULT_RESULT_VALUE;
         }
         Matcher matcher = pattern.matcher(input);
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
             return getSum(matcher.group(2).split(customDelimiter));
         }
-        return getSum(input.split(FIRST_DEFAULT_DELIMITER + OR + SECOND_DEFAULT_DELIMITER));
+        return getSum(input.split(DEFAULT_DELIMITER));
     }
 
     private static boolean isInvalid(String input) {
@@ -29,21 +27,8 @@ public class StringAddCalculator {
     }
 
     private static Integer getSum(String[] input) {
-        return Arrays.stream(input).map(StringAddCalculator::mapToInt).reduce(Integer::sum).orElse(DEFAULT_VALUE);
+        return Arrays.stream(input).map(StringIntParser::mapToInt).reduce(Integer::sum).orElse(DEFAULT_RESULT_VALUE);
     }
 
-    private static Integer mapToInt(String input) {
-        try {
-            return throwExpIfMinus(Integer.parseInt(input));
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("input must be number", e);
-        }
-    }
 
-    private static int throwExpIfMinus(int num) {
-        if (num < 0) {
-            throw new IllegalArgumentException("input number must be positive");
-        }
-        return num;
-    }
 }
