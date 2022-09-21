e56b08f827f801cccb2ca0a8f38884aa3f50269b
KimWanJoo <wjwan0915@gmail.com>
2022-09-21 11:16:30 +0900

    :white_check_mark: test: Position Test

---
+++ src/test/java/step3/domain/PositionTest.java
@@ -0,0 +1,40 @@
+package step3.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step3.exception.NegativePositionException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class PositionTest {
+
+    @Test
+    @DisplayName("생성 테스트")
+    void ctorStateTest() {
+        assertDoesNotThrow(() -> new Position());
+    }
+
+    @Test
+    @DisplayName("음수 예외처리 테스트")
+    void negativeTest() {
+        assertThatThrownBy(() -> new Position(-1))
+                .isInstanceOf(NegativePositionException.class)
+                .hasMessage("음수의 전진 상태는 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("상태 +1 테스트")
+    void changeStateTest() {
+        //given
+        Position position = new Position();
+
+        //when
+        Position advancePosition = position.advance();
+
+        //then
+        assertThat(advancePosition.value()).isEqualTo(1);
+    }
+
+}
