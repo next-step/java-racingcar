bbc46471e0c6fe1fba0ef0583b45b4a02c6fa2c5
KimWanJoo <wjwan0915@gmail.com>
2022-09-21 11:18:53 +0900

    :white_check_mark: test: List<Car> 위치 변경 테스트 추가
    자동차 전체의 위치가 변하는 것을 CarRace 클래스에 구현하여 테스트 추가

--- src/test/java/step3/domain/CarRaceTest.java
+++ src/test/java/step3/domain/CarRaceTest.java
@@ -3,6 +3,7 @@ package step3.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,9 +20,9 @@ public class CarRaceTest {
     void ctorTest() {
         //given
         List<Car> carList = List.of(
-                new Car(0),
-                new Car(0),
-                new Car(0)
+                new Car(),
+                new Car(),
+                new Car()
         );
 
         //then
@@ -46,7 +47,7 @@ public class CarRaceTest {
     @DisplayName("자동차 추가 테스트")
     void addCarTest() {
         //given
-        List<Car> list = List.of(new Car(0));
+        List<Car> list = new ArrayList<>(List.of(new Car(0)));
         CarRace carRace = new CarRace(list, NumberGenerator.RandomNumberGenerator(10));
 
         //when
@@ -57,18 +58,32 @@ public class CarRaceTest {
     }
 
     @Test
-    @DisplayName("자동차 전진 테스트")
+    @DisplayName("전체 자동차 전진 테스트")
     void advanceCarTest() {
         //given
-        List<Car> list = List.of(new Car(0));
+        List<Car> list = new ArrayList<>(List.of(new Car(0)));
         CarRace carRace = new CarRace(list, advanced);
 
         //when
         carRace.move();
 
         //then
-        carRace.cars().get(0).state();
+        Car moveCar = carRace.cars().get(0);
+        assertThat(moveCar.state()).isEqualTo(1);
+    }
 
+    @Test
+    @DisplayName("전체 자동차 전진 실패 테스트")
+    void notAdvanceCarTest() {
+        //given
+        List<Car> list = new ArrayList<>(List.of(new Car(0)));;
+        CarRace carRace = new CarRace(list, notAdvanced);
+
+        //when
+        carRace.move();
+
+        //then
+        assertThat(carRace.cars().get(0).state()).isEqualTo(0);
     }
 
 
