7f18944ce482f8453030ef17b191a4e8954d1ce4
yunhwan <pyh8618@gmail.com>
2022-09-21 21:55:31 +0900

    test: 문자열 덧셈 계산기 테스트 추가

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,4 +1,7 @@
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -6,11 +9,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class StringAddCalculatorTest {
     @Test
     public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
-        assertThat(result).isEqualTo(0);
+        int resultNull = StringAddCalculator.splitAndSum(null);
+        int resultBlank = StringAddCalculator.splitAndSum("");
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(resultNull).as("null 테스트").isEqualTo(0);
+            softAssertions.assertThat(resultBlank).as("Blank 테스트").isEqualTo(0);
+        });
     }
 
     @Test
@@ -44,9 +48,10 @@ public class StringAddCalculatorTest {
                 .hasMessageContaining("음수는 들어올 수 없습니다.");
     }
 
-    @Test
-    public void splitAndSum_숫자형태가_아닌_값() throws Exception {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("1/2,2,3"))
+    @ParameterizedTest
+    @ValueSource(strings = {"1/2,2,3", "1$3,4:5", "//%\n1%%2%%3"})
+    public void splitAndSum_숫자형태가_아닌_값(String value) throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(value))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("숫자 형태가 아닌 값이 존재합니다.");
     }
