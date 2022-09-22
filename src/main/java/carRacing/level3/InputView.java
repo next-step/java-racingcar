b28cd757d3885a2b387e9e353492c04ab939738b
sienna <20200803@sungshin.ac.kr>
2022-09-22 10:49:01 +0900

    :sparkles:feat : InputView 기능

---
+++ src/main/java/carRacing/level3/InputView.java
@@ -0,0 +1,23 @@
+package carRacing.level3;
+
+import java.util.Scanner;
+
+public class InputView {
+
+  private final Scanner scanner;
+
+  public InputView() {
+    scanner = new Scanner(System.in);
+  }
+
+  public int askNumberofCars() {
+    System.out.println("자동차 대수는 몇 대 인가요?");
+    return scanner.nextInt();
+  }
+
+  public int askValueofRound() {
+    System.out.println("시도 회수는 몇회인가요?");
+    return scanner.nextInt();
+  }
+
+}
