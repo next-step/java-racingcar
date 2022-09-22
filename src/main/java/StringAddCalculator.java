d26b243908469288000312b60b849b167aba765e
shineyuno <shineyuno@gmail.com>
2022-09-22 20:24:10 +0900

    문자열 덧셈 계산기 구현

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
-        return 0;
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        String[] tokens;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            tokens = m.group(2).split(customDelimiter);
+        } else {
+            tokens = text.split(",|:");
+        }
+
+        return Arrays.stream(tokens)
+                .mapToInt(token -> {
+                    int n = Integer.parseInt(token);
+                    if (n < 0) throw new RuntimeException("contains negative number : " + n);
+                    return n;
+                })
+                .sum();
     }
 }
