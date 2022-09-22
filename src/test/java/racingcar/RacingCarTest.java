ff8eb63e48b9df9ea29880b74c2e36fb4362145e
jinukix <jinukix@styleshare.kr>
2022-09-22 17:30:11 +0900

    refator: RacingCar.move() 테스트 보완

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -23,22 +24,19 @@ public class RacingCarTest {
     }
 
     @Test
-    void move() {
+    @DisplayName("RacingCar의 move 호출 시 MoveCondition.canMove()에서 true 반환시 position 값 증가")
+    void canMove() {
         CanMoveCondition canMoveCondition = new CanMoveCondition();
-        RacingCar cantMoveRacingCar = new RacingCar();
-        then(cantMoveRacingCar.getPosition()).isZero();
-        cantMoveRacingCar.move(canMoveCondition);
-        then(cantMoveRacingCar.getPosition()).isEqualTo(1);
-        cantMoveRacingCar.move(canMoveCondition);
-        then(cantMoveRacingCar.getPosition()).isEqualTo(2);
-        cantMoveRacingCar.move(canMoveCondition);
-        then(cantMoveRacingCar.getPosition()).isEqualTo(3);
+        RacingCar canMoveRacingCar = new RacingCar();
+        then(canMoveRacingCar.getPosition()).isZero();
+        canMoveRacingCar.move(canMoveCondition);
+        then(canMoveRacingCar.getPosition()).isOne();
     }
 
     @Test
+    @DisplayName("RacingCar의 move 호출 시 MoveCondition.canMove()에서 false 반환시 position 값 변화 없음")
     void cantMove() {
         CantMoveCondition cantMoveCondition = new CantMoveCondition();
-
         RacingCar cantMoveRacingCar = new RacingCar();
         then(cantMoveRacingCar.getPosition()).isZero();
         cantMoveRacingCar.move(cantMoveCondition);
