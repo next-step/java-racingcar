13b006cba3c30b4a9d20b27f29221e3925c6a7a3
kkywalk2 <kkywalk2@gmail.com>
2022-09-23 03:18:54 +0900

    feat: StringAddCalculatorTest 네번째, 다섯번째 테스트 통과

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,17 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
+    final static String regex = "(?<=//)(.*)(?=\\n)";
+
     static int splitAndSum(String str) {
-        String delimiter = ",";
         int sum = 0;
 
         if (str == null || str.isBlank()) return sum;
 
-        String[] splitString = str.split(delimiter);
+        String customDelimiter = checkCustomDelimiter(str);
+        List<String> splitString = customDelimiter == null ? splitByDefault(str) : splitByCustom(str, customDelimiter);
 
         for (String value : splitString) {
             sum += Integer.parseInt(value);
         }
 
         return sum;
+    }
+
+    static List<String> splitByDefault(String str) {
+        List<String> result = new ArrayList();
+        String[] splitString = str.split(",");
+
+        for (String value : splitString) {
+            String[] element = value.split(":");
+            result.addAll(Arrays.asList(element));
+        }
+
+        return result;
+    }
+
+    static List<String> splitByCustom(String str, String delimiter) {
+        String target = str.split("\n")[1];
+        return Arrays.asList(target.split(delimiter));
+    }
+
+    static String checkCustomDelimiter(String str) {
+        Matcher matcher = Pattern.compile(regex, Pattern.MULTILINE).matcher(str);
+
+        if(matcher.find())
+            return matcher.group(0);
+        else
+            return null;
     }
 }
