3c041f96decb90478e3736f86ae35f4a626f916e
Yunsik-Choi <60809936+Yunsik-Choi@users.noreply.github.com>
2022-09-21 17:30:33 -0700

    Step2 - 문자열 덧셈 계산기 (#3522)
    * StringCalculator textCheck test
    
    * StringCalculator textCheckProvidesANumber test
    
    * StringCalculator addOperationSeprateByRest test
    
    * StringCalculator addOperationSeparateByRestAndColon test
    
    * StringCalculator CaddOperationSeparateWithCustomSeparator test
    
    * StringCalculator addOperation_negative test

---
+++ src/main/java/step2/StringCalculator.java
@@ -0,0 +1,43 @@
+package step2;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    public int addOperation(String text) {
+        if (text == null)
+            return 0;
+        String[] numbers = split(text);
+        int result = add(numbers);
+        return result;
+    }
+
+    private static String[] split(String text) {
+        String regex = ",|:";
+        Matcher m = Pattern.compile("//(.)\\n(.*)").matcher(text);
+        if (m.find()) {
+            String separator = m.group(1);
+            regex = regex + "|" + separator;
+            text = m.group(2);
+        }
+        return text.split(regex);
+    }
+
+    private int add(String[] textArray) {
+        return Arrays.stream(textArray).mapToInt(i -> textCheck(i)).sum();
+    }
+
+    public int textCheck(String text) {
+        if (text == null) {
+            return 0;
+        }
+        if (text.isEmpty()) {
+            return 0;
+        }
+        if (text.chars().allMatch(Character::isDigit))
+            return Integer.parseInt(text);
+        throw new RuntimeException();
+    }
+
+}
---
+++ src/test/java/step2/StringCalculatorTest.java
@@ -0,0 +1,68 @@
+package step2;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+
+public class StringCalculatorTest {
+
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    private void setStringCalculator() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @Test
+    void textCheck() {
+        org.junit.jupiter.api.Assertions.assertAll(
+                () -> Assertions.assertThat(stringCalculator.addOperation("")).isEqualTo(0),
+                () -> Assertions.assertThat(stringCalculator.addOperation(null)).isEqualTo(0)
+        );
+    }
+
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    @Test
+    void textCheckProvidesANumber() {
+        org.junit.jupiter.api.Assertions.assertAll(
+                () -> Assertions.assertThat(stringCalculator.addOperation("1")).isEqualTo(1),
+                () -> Assertions.assertThat(stringCalculator.addOperation("22")).isEqualTo(22)
+        );
+    }
+
+    @DisplayName("숫자 두개를 컴마 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @Test
+    void addOperationSeparateByRest() {
+        Assertions.assertThat(stringCalculator.addOperation("1,2")).isEqualTo(3);
+    }
+
+    @DisplayName("구분자를 컴마 이외에 콜론을 사용할 수 있다.")
+    @Test
+    void addOperationSeparateByRestAndColon() {
+        Assertions.assertThat(stringCalculator.addOperation("1,2:3")).isEqualTo(6);
+    }
+
+    @DisplayName("//와 \\n로 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @Test
+    void addOperationSeparateWithCustomSeparator() {
+        Assertions.assertThat(stringCalculator.addOperation("//;\n1;2;3"));
+    }
+
+    @DisplayName("음수 또는 숫자 이외의 값을 전달할 경우 RuntimeException 예외가 발생한다")
+    @Test
+    public void addOperation_negative() {
+        org.junit.jupiter.api.Assertions.assertAll(
+                () -> assertThatThrownBy(() -> stringCalculator.addOperation("-1,2,3"))
+                        .isInstanceOf(RuntimeException.class),
+                () -> assertThatThrownBy(() -> stringCalculator.addOperation("1,a,3"))
+                        .isInstanceOf(RuntimeException.class)
+
+        );
+    }
+}
