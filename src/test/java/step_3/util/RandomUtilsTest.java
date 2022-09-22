372a95c2dabc846e8c741952009880205a7afd40
swsong <ssw890720@gmail.com>
2022-09-23 01:01:02 +0900

    refactor: 테스트 케이스 @Disabled 메시지 추가

--- src/test/java/step_3/util/RandomUtilsTest.java
+++ src/test/java/step_3/util/RandomUtilsTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RandomUtilsTest {
 
-    @Disabled
+    @Disabled("싪패 가능성이 있는 테스트 이므로 로컬에서 1회성 실행만 할것")
     @Test
     void 랜덤_숫자가_0_9_사이로_생성된다() {
         //given
