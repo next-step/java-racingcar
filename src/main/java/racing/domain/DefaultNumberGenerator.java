761ae54af3a4ee148035d140b8426c40a6dd24d5
KimWanJoo <wjwan0915@gmail.com>
2022-09-22 23:32:54 +0900

    :recycle: refactor (DefaultNumberGenerator Class 작성)
    기본 숫자생성 클래스 명 변경 및 매직넘버 추가

--- src/main/java/racing/domain/DefaultNumberGenerator.java
+++ src/main/java/racing/domain/DefaultNumberGenerator.java
@@ -1,19 +1,25 @@
-package step3.domain;
+package racing.domain;
 
 import java.util.Random;
 
-public class RandomNumberGenerator implements NumberGenerator {
+public class DefaultNumberGenerator implements NumberGenerator {
+
+    private static int EXCEPTION_BOUND = 0;
     private final int bound;
 
     private final Random random = new Random();
 
-    protected RandomNumberGenerator(int bound) {
+    public DefaultNumberGenerator() {
+        this(10);
+    }
+
+    public DefaultNumberGenerator(int bound) {
         validation(bound);
         this.bound = bound;
     }
 
     private void validation(int bound) {
-        if (bound <= 0) {
+        if (bound <= EXCEPTION_BOUND) {
             throw new IllegalArgumentException("0이하의 랜덤 값은 만들 수 없습니다.");
         }
     }
