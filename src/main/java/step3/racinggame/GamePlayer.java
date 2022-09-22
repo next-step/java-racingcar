127cab8004d73f61fb3eef8eb875b69fd6472fb4
kingkk31 <kingkk31@naver.com>
2022-09-23 00:27:06 +0900

    feat: 게임 실행용 클래스 추가

---
+++ src/main/java/step3/racinggame/GamePlayer.java
@@ -0,0 +1,13 @@
+package step3.racinggame;
+
+import step3.racinggame.core.RacingGame;
+
+public class GamePlayer {
+
+    // 정말로 게임을 실행시켜보고 싶어서 만들었습니다. 요구사항과는 무관합니다.
+    public static void main(String[] args) {
+        RacingGame game = new RacingGame();
+        game.play();
+    }
+
+}
