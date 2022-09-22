5e766daae0ea2ca8f033a17fcd672b0062980a91
sienna <20200803@sungshin.ac.kr>
2022-09-22 10:49:14 +0900

    :sparkles:feat : OutputView 기능

---
+++ src/main/java/carRacing/level3/OutPutView.java
@@ -0,0 +1,15 @@
+package carRacing.level3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class OutPutView {
+
+  HashMap<Integer, String> map1 = new HashMap<Integer, String>();
+
+  public void showRace(int carNum) {
+    System.out.println("-");
+    map1.put(carNum, "-");
+
+  }
+}
