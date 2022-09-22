0589c980cc884473bfe864aebad9080db6c9b2f4
Cheeze <cheeze.ball@kakaocorp.com>
2022-09-22 10:24:39 +0900

    fix(CarRacing) : 사용자 입력 기능 클래스 분리
    BREAKING CHANGE
    ADD InputView
    ADD ResultView

--- src/main/java/car/CarRacing.java
+++ src/main/java/car/CarRacing.java
@@ -3,24 +3,15 @@ package car;
 import java.util.Scanner;
 
 public class CarRacing {
+    private final InputView inputView;
     public  CarRacing() {
-
+        inputView = new InputView();
     }
 
     public void start() {
-        int carNum = enterCarCount();
-        int tryNum = enterTryCount();
-    }
+        int carNum = inputView.enterCarCount();
+        int tryNum = inputView.enterTryCount();
 
-    private int enterCarCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
 
-    private int enterTryCount() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
     }
 }
---
+++ src/main/java/car/InputView.java
@@ -0,0 +1,17 @@
+package car;
+
+import java.util.Scanner;
+
+public class InputView {
+    public int enterCarCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public int enterTryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
