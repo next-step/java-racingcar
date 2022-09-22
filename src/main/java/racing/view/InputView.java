7404122e48d4b4ba3f3e21dec27655a9cd07f4c9
KimWanJoo <wjwan0915@gmail.com>
2022-09-22 23:31:05 +0900

    :recycle: refactor (InputView Class 작성)
    입력받는 클래스 작성

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -1,17 +1,20 @@
-package step3.application;
+package racing.view;
 
 import java.util.Scanner;
 
-public class ConsoleIn {
+public class InputView {
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public int numberOfCars() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
         int count = scanner.nextInt();
         scanner.nextLine();
         return count;
     }
 
     public int moveCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
         int count = scanner.nextInt();
         scanner.nextLine();
         return count;
