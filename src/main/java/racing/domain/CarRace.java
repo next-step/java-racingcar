95324c642976516aa1cc24d385a56e996536a1ff
KimWanJoo <wjwan0915@gmail.com>
2022-09-22 23:26:23 +0900

    :recycle: refactor (CarRace Class 작성)
    생성자 추가 및 매직넘버 추가

--- src/main/java/racing/domain/CarRace.java
+++ src/main/java/racing/domain/CarRace.java
@@ -1,17 +1,24 @@
-package step3.domain;
+package racing.domain;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import racing.domain.car.Car;
 
 public class CarRace {
 
+    private static int MOVE_NUMBER = 4;
     private List<Car> cars;
     private final NumberGenerator numberGenerator;
 
     public CarRace(List<Car> cars, NumberGenerator numberGenerator) {
         this.cars = cars;
         this.numberGenerator = numberGenerator;
+    }
+
+    public CarRace(List<Car> cars) {
+        this.cars = cars;
+        this.numberGenerator = new DefaultNumberGenerator();
     }
 
     public int size() {
@@ -28,12 +35,12 @@ public class CarRace {
 
     public void move() {
         this.cars = cars.stream()
-                .map(car -> move(car, numberGenerator))
-                .collect(Collectors.toList());
+            .map(car -> move(car, numberGenerator))
+            .collect(Collectors.toList());
     }
 
     private Car move(Car car, NumberGenerator numberGenerator) {
-        if (numberGenerator.randomNumber() < 4) {
+        if (numberGenerator.randomNumber() < MOVE_NUMBER) {
             return car;
         }
         return car.move();
