9c1708785227aab5e83d9665cea43135c16f5ea9
BosukNam <popman1105@gmail.com>
2022-09-22 22:15:12 +0900

    2단계 미션 추가 리팩토링 진행

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -21,39 +21,69 @@
  * 6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)
  */
 public class StringAddCalculator {
-    
+
+    public static final String SEPARATOR = ",|:";
+    public static final String CUSTOM_SEPARATOR_START= "//";
+    public static final String CUSTOM_SEPARATOR_END= "\n";
+
     public static int splitAndSum(String ogText) {
-        // 1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)
-        if(ogText == null || ogText.isEmpty()) {
+        if(isBlank(ogText)) {
             return 0;
         }
 
+        return sum(ogText);
+    }
+
+    private static int sum(String ogText) {
+        String delimiter = SEPARATOR;
+        
         // 5. “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\n1;2;3” => 6)
-        if(ogText.startsWith("//")) {
-            int indexOfSlashN = ogText.indexOf("\n");
-            String delimiter = ogText.substring(2, indexOfSlashN);
-            String subText = ogText.substring(indexOfSlashN + 1);
-            return getResultSum(subText, delimiter);
+        if(isOgTextHasCustomSeparator(ogText)) {
+            delimiter = getCustomDelimiter(ogText);
+            ogText = getTextAfterCustomDelimiter(ogText);
         }
 
-        return getResultSum(ogText, ",|:");
+        return getSum(ogText, delimiter);
+    }
+
+    private static int getIndexOfSlashN(String ogText) {
+        return ogText.indexOf(CUSTOM_SEPARATOR_END);
+    }
+
+    private static String getTextAfterCustomDelimiter(String ogText) {
+        int indexOfSlashN = getIndexOfSlashN(ogText);
+        return ogText.substring(indexOfSlashN + 1);
+    }
+
+    private static String getCustomDelimiter(String ogText) {
+        int indexOfSlashN = getIndexOfSlashN(ogText);
+        return ogText.substring(2, indexOfSlashN);
+    }
+
+    private static boolean isOgTextHasCustomSeparator(String ogText) {
+        return ogText.startsWith(CUSTOM_SEPARATOR_START);
+    }
+
+    // 1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)
+    private static boolean isBlank(String ogText) {
+        return ogText == null || ogText.isEmpty();
     }
 
     // 2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)
     // 3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)
     // 4. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)
-    private static int getResultSum(String ogText, String delimiter) {
+    private static int getSum(String ogText, String delimiter) {
         int resultSum = 0;
         String[] splTexts = ogText.split(delimiter);
         for(String splText : splTexts) {
-            int parsedVal = getParsedVal(splText);
+            int parsedVal = getPositiveInt(splText);
             resultSum += parsedVal;
         }
         return resultSum;
     }
 
     // 6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)
-    private static int getParsedVal(String splText) {
+    private static int getPositiveInt(String splText) {
         int parsedVal = Integer.parseInt(splText);
         if(parsedVal < 0) {
             throw new RuntimeException("음수가 전달되었습니다!");
