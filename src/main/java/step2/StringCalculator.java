3ad65f00e81b726c879f0aa63efdd4e649f4627c
wugatama <dnr7410@naver.com>
2022-09-23 20:24:06 +0900

    refactor: 테스트 코드, 기존 코드 리팩토링

---
+++ src/main/java/step2/StringCalculator.java
@@ -0,0 +1,42 @@
+package step2;
+
+public class StringCalculator {
+	public static final String SEPARATORS = ",|:";
+	public int calculate(String input) {
+		if (input == null || input.isBlank()) {
+			return 0;
+		}
+		return add(convertToInt(split(input)));
+	}
+
+	public String[] split(String input) {
+		return input.split(SEPARATORS);
+	}
+
+	public int[] convertToInt(String[] arr) {
+		int[] convert = new int[arr.length];
+		checkMinusOrNotNumber(arr, convert);
+		return convert;
+	}
+
+	private void checkMinusOrNotNumber(String[] arr, int[] convert) {
+		for (int i = 0; i < arr.length; i++) {
+			validMinusOrNotNumber(arr, i);
+			convert[i] = Integer.parseInt(arr[i]);
+		}
+	}
+
+	private void validMinusOrNotNumber(String[] arr, int i) {
+		if (arr[i].charAt(0) == '-' || (89 < (arr[i].charAt(0)) && (arr[i].charAt(0)) < 80)) {
+			throw new RuntimeException();
+		}
+	}
+
+	public int add(int[] arr) {
+		int sum = 0;
+		for (int i : arr) {
+			sum += i;
+		}
+		return sum;
+	}
+}
--- src/test/java/step2/StringCalculateTest.java
+++ src/test/java/step2/StringCalculateTest.java
@@ -2,18 +2,26 @@ package step2;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class StringCalculateTest {
+class StringCalculateTest {
+	private StringCalculator calculator;
+
+	@BeforeEach
+	void setUp() {
+		calculator = new StringCalculator();
+	}
 	@Test
 	@DisplayName("빈 문자열이거나 null 일 때 0 처리")
-	public void blankOrNull() throws Exception {
+	void blankOrNull() throws Exception {
 		String str1 = "";
 		String str2 = null;
-		StringCalculator calculator = new StringCalculator();
+
 		assertThat(calculator.calculate(str1)).isEqualTo(0);
 		assertThat(calculator.calculate(str2)).isEqualTo(0);
 	}
@@ -21,40 +29,48 @@ public class StringCalculateTest {
 	@ParameterizedTest
 	@DisplayName("기본 구분자로 split 하기")
 	@ValueSource(strings = {"1,2,3", "1:2:3"})
-	public void splitByOtherSeparators(String input) throws Exception {
+	void splitByOtherSeparators(String input) throws Exception {
 		StringCalculator calculator = new StringCalculator();
-		String[] expected = calculator.split(input);
-		assertThat(new String[] {"1", "2", "3"}).isEqualTo(expected);
+
+		assertThat(new String[] {"1", "2", "3"}).isEqualTo(calculator.split(input));
 	}
 
 	@Test
 	@DisplayName("분리된 문자배열을 숫자배열로 반환")
-	public void convertToIntArray() throws Exception {
+	void convertToIntArray() throws Exception {
 		StringCalculator calculator = new StringCalculator();
 		String[] arr = {"1", "2", "3"};
-		int[] expected = calculator.convertToInt(arr);
 
-		assertThat(new int[] {1, 2, 3}).isEqualTo(expected);
+		assertThat(new int[] {1, 2, 3}).isEqualTo(calculator.convertToInt(arr));
 	}
 
 	@Test
 	@DisplayName("숫자배열 더하기")
-	public void addInt() throws Exception {
+	void addInt() throws Exception {
 		StringCalculator calculator = new StringCalculator();
 		int[] arr = {1, 2, 3};
-		int expected = calculator.add(arr);
 
-		assertThat(6).isEqualTo(expected);
+		assertThat(6).isEqualTo(calculator.add(arr));
 	}
 
 	@ParameterizedTest
 	@DisplayName("음수 또는 숫자가 아닐 때 RuntimeException throw")
 	@ValueSource(strings = {"-1,2,3", "a:2:3"})
-	public void minusOrNotNumber(String input) throws Exception {
+	void minusOrNotNumber(String input) throws Exception {
 		StringCalculator calculator = new StringCalculator();
 		String[] split = calculator.split(input);
 
 		assertThatThrownBy(() -> calculator.convertToInt(split))
 			.isInstanceOf(RuntimeException.class);
+	}
+
+	@ParameterizedTest
+	@DisplayName("덧셈 테스트")
+	@CsvSource(value = {
+		"1,2,3;6",
+		"4:5,6;15"
+	}, delimiter = ';')
+	void plus(String input, int expected) {
+		assertThat(calculator.calculate(input)).isEqualTo(expected);
 	}
 }
