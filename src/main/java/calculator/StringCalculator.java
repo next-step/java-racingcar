215ab4d1999e29330a29beccbd72c82060bc527a
gmelon <hsh1769@naver.com>
2022-09-23 15:34:07 +0900

    refactor: 파싱, validation, summation 기능 PositiveNumbers 클래스로 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -14,9 +14,13 @@ public class StringCalculator {
         }
         Matcher matcher = createMatcher(text);
         if (matcher.find()) {
-            return sum(toInts(splitCustomizedText(matcher)));
+            return sum(new PositiveNumbers(splitCustomizedText(matcher)));
         }
-        return sum(toInts(split(text, DEFAULT_SEPARATOR)));
+        return sum(new PositiveNumbers(splitDefaultText(text)));
+    }
+
+    private static int sum(PositiveNumbers positive) {
+        return positive.sum();
     }
 
     private static boolean isBlank(String text) {
@@ -27,48 +31,13 @@ public class StringCalculator {
         return CUSTOM_SEPARATOR_PATTERN.matcher(text);
     }
 
-    private static int sum(int[] numbers) {
-        int sum = 0;
-        for (int number : numbers) {
-            sum += number;
-        }
-        return sum;
-    }
-
-    private static int[] toInts(String[] values) {
-        int[] numbers = new int[values.length];
-        for (int i = 0; i < values.length; i++) {
-            numbers[i] = toPositiveInt(values[i]);;
-        }
-        return numbers;
-    }
-
-    private static int toPositiveInt(String value) {
-        int number = validateValueIsNumber(value);
-        validateValueIsPositive(number);
-        return number;
-    }
-
-    private static int validateValueIsNumber(String values) {
-        int number;
-        try {
-            number = Integer.parseInt(values);
-        } catch (NumberFormatException exception) {
-            throw new RuntimeException("숫자 이의의 값은 입력될 수 없습니다.");
-        }
-        return number;
-    }
-
-    private static void validateValueIsPositive(int number) {
-        if (number < 0) {
-            throw new RuntimeException("음수는 입력될 수 없습니다.");
-        }
+    private static String[] splitDefaultText(String text) {
+        return split(text, DEFAULT_SEPARATOR);
     }
 
     private static String[] splitCustomizedText(Matcher matcher) {
         return split(matcher.group(2), matcher.group(1));
     }
-
 
     private static String[] split(String text, String separator) {
         return text.split(separator);
