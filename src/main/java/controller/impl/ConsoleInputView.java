1fa6017f1c2eb7275c632c0ca61ea412f868adc0
김찬수 <cskim96@crscube.co.kr>
2022-09-23 10:23:37 +0900

    :sparkles: feat(ui) : 사용자로부터 값 입력받는 controller
    사용자로부터 값을 입력받는 로직

---
+++ src/main/java/controller/impl/ConsoleInputView.java
@@ -0,0 +1,39 @@
+package controller.impl;
+
+import controller.InputView;
+import model.GameParam;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by P-161 at : 2022-09-23
+ *
+ * 여기에 ConsoleInputView 클래스에 대한 설명을 기술해주세요
+ *
+ * @author P-161
+ * @version 1.0
+ * @since 1.0
+ */
+public class ConsoleInputView implements InputView {
+
+    public static final String CAR_NUMBER_ASK_QUESTION = "자동차 대수는 몇 대 인가요?";
+    public static final String TRY_NUMBER_ASK_QUESTION = "시도할 회수는 몇 회 인가요?";
+    public static final String RUN_RESULT_MSG = "실행 결과";
+    public static final String RUN_FAIL_USER_MSG = "자동차 대수 , 시도할 회수는 숫자값이여만 합니다.";
+
+    @Override
+    public GameParam getInputFromUser() {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.println(CAR_NUMBER_ASK_QUESTION);
+            int carNumber = sc.nextInt();
+            System.out.println(TRY_NUMBER_ASK_QUESTION);
+            int tryNumber = sc.nextInt();
+            System.out.println(RUN_RESULT_MSG);
+            return new GameParam(carNumber,tryNumber);
+        }catch (InputMismatchException e){
+            System.out.println(RUN_FAIL_USER_MSG);
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
