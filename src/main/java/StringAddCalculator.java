bb5b5ee5ee2b41fb6502b5455c61d4bdbf8e41f9
shineyuno <shineyuno@gmail.com>
2022-09-23 20:47:19 +0900

    패턴 컴파일 효율을 위해, 커스텀구분자 찾는 정규식 재사용 가능하도록 상수로 등록

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -3,13 +3,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.*)");
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
 
         String[] tokens;
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = CUSTOM_DELIMITER.matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
             tokens = m.group(2).split(customDelimiter);
