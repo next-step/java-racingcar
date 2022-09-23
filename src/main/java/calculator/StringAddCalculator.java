0d3b3b78e41dc6b2888cfeb53f1286e22af47c9c
yoonjeong <cyj199637@gmail.com>
2022-09-23 16:20:51 +0900

    (refactor): 가이드 및 강의 기반 리팩토링 진행

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,53 +2,51 @@ package calculator;
 
 import static java.lang.Integer.parseInt;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private static final String[] SEPERATORS = {",", ":"};
-    private static final String CUSTOM_SEPERATOR_PATTERN = "//(.)\n(.*)";
+    private static final String SEPARATORS = "[,:]";
+    private static final String CUSTOM_SEPARATOR_PATTERN = "//(.)\n(.*)";
+    private static final String POSITIVE_INT_PATTERN = "[0-9]";
 
     public static int splitAndSum(String value) {
         if (value == null || value.isEmpty()) {
             return 0;
         }
 
-        if (Pattern.matches(CUSTOM_SEPERATOR_PATTERN, value)) {
-            String customSeperator = value.split("\n")[0].substring(2);
-            List<String> tokens = Arrays.asList(value.split("\n")[1].split(customSeperator));
-
-            return sum(tokens);
-        }
-
-        List<String> commaTokens = Arrays.asList(value.split(SEPERATORS[0]));
-        List<String> colonTokens = new ArrayList<>();
-        for (String token : commaTokens) {
-            colonTokens.addAll(Arrays.asList(token.split(SEPERATORS[1])));
-        }
-
-        return sum(colonTokens);
+        return sum(split(value));
     }
 
-    private static int sum(List<String> value) {
+    private static String[] split(String value) {
+        if (isUsedCustomSeparator(value)) {
+            return value.split(value.split("\n")[0].substring(2));
+        }
+
+        return value.split(SEPARATORS);
+    }
+
+    private static boolean isUsedCustomSeparator(String value) {
+        return Pattern.matches(CUSTOM_SEPARATOR_PATTERN, value);
+    }
+
+    private static int sum(String[] value) {
         int result = 0;
         for (String token : value) {
-            int parsed;
-            try {
-                parsed = parseInt(token);
-            } catch (NumberFormatException e) {
-                throw new RuntimeException("허용되지 않는 값이 존재합니다.");
-            }
-
-            if (parsed < 0) {
-                throw new RuntimeException("허용되지 않는 값이 존재합니다.");
-            }
-
-            result += parsed;
+            result += parsePositiveInt(token);
         }
 
         return result;
+    }
+
+    private static int parsePositiveInt(String value) {
+        if (isImpossibleToParse(value)) {
+            throw new RuntimeException("허용하지 않는 값이 존재합니다.");
+        }
+
+        return parseInt(value);
+    }
+
+    private static boolean isImpossibleToParse(String value) {
+        return !Pattern.matches(POSITIVE_INT_PATTERN, value);
     }
 }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -5,11 +5,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringAddCalculatorTest {
 
     @Test
     void null_or_empty() {
+        assertThat(splitAndSum(null)).isZero();
         assertThat(splitAndSum("")).isZero();
     }
 
@@ -33,10 +36,11 @@ public class StringAddCalculatorTest {
         assertThat(splitAndSum("//;\n1;2;3")).isEqualTo(6);
     }
 
-    @Test
-    void wrong_string_value() {
-        assertThatThrownBy(() -> splitAndSum("-1,2")).
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2", "#$ek", "2-9"})
+    void wrong_string_value(String input) {
+        assertThatThrownBy(() -> splitAndSum(input)).
             isInstanceOf(RuntimeException.class).
-            hasMessage("허용되지 않는 값이 존재합니다.");
+            hasMessage("허용하지 않는 값이 존재합니다.");
     }
 }
