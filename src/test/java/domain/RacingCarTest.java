bcc158a996fca17a2a560d19805ae3058b09110b
michol.lee <lovethefeel@gmail.com>
2022-09-23 16:57:34 +0900

    feat(test): 레이싱 자동차 테스트 케이스

---
+++ src/test/java/domain/RacingCarTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingCarTest {
+
+    @DisplayName("랜덤 숫자가 4이상인 경우(true) 전진하며, 4미만인 경우(false) 정지한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"false,0", "true,1"})
+    void car_move(final boolean movable, final int position) {
+
+        final RacingCar racingCar = RacingCar.init();
+        racingCar.move(() -> movable);
+
+        assertThat(racingCar.moveDistance()).isEqualTo(position);
+    }
+}
