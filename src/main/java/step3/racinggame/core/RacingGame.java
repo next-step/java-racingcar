899c078bbf4845b389b140aa0cc1d816c3d310c8
kingkk31 <kingkk31@naver.com>
2022-09-24 02:31:36 +0900

    refactor: 레이싱 게임 클래스를 게임 실행의 기능만 가지게 분리

--- src/main/java/step3/racinggame/core/RacingGame.java
+++ src/main/java/step3/racinggame/core/RacingGame.java
@@ -1,63 +1,34 @@
 package step3.racinggame.core;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import step3.racinggame.view.GamePad;
-import step3.racinggame.view.ScoreBoard;
 
 public class RacingGame {
 
-    private final GasStation gasStation;
+    private final List<Car> cars;
 
-    public RacingGame() {
-        this.gasStation = new GasStation();
+    private final FuelTank fuelTank;
+
+    public RacingGame(List<Car> cars, FuelTank fuelTank) {
+        this.cars = cars;
+        this.fuelTank = fuelTank;
     }
 
-    // 현재 구현한 '게임'에서 단위테스트는 어떻게?
-    // 단위테스트를 위해 private 메소드 일부를 public 으로 바꾼 기분...
-
-    // 게임을 실행하는 부분
-    public void play() {
-        // 차량 수를 입력받아 경기할 차량을 준비한다.
-        GamePad.printNumOfCarInputMsg();
-        List<Car> cars = readyToRace(GamePad.insertData());
-
-        // 경기 수를 입력 받아 경기를 진행한다.
-        GamePad.printNumOfGameInputMsg();
-        List<List<Integer>> scoreBoard = playGame(GamePad.insertData(), cars);
-
-        // 경기 결과를 출력한다.
-        ScoreBoard.printScoreBoard(scoreBoard);
-    }
-
-
-    public List<Car> readyToRace(int numOfCar) {
-        List<Car> cars = new ArrayList<>();
-        for (int i = 0; i < numOfCar; i++) {
-            cars.add(new Car());
-        }
-
-        return cars;
-    }
-
-    public List<List<Integer>> playGame(int numOfGame, List<Car> cars) {
-        List<List<Integer>> scoreBoard = new ArrayList<>();
+    public ScoreBoard play(int numOfGame) {
+        ScoreBoard scoreBoard = new ScoreBoard(numOfGame, cars.size());
 
         for (int i = 0; i < numOfGame; i++) {
-            scoreBoard.add(playRace(cars));
+            playRound(i, scoreBoard);
         }
 
         return scoreBoard;
     }
 
-    private List<Integer> playRace(List<Car> cars) {
-        return cars.stream()
-                .map(car -> {
-                    car.move(gasStation.sellFuel());
-                    return car.whereAmI();
-                })
-                .collect(Collectors.toUnmodifiableList());
-    }
+    private void playRound(int round, ScoreBoard scoreBoard) {
+        for (int j = 0; j < cars.size(); j++) {
+            Car car = cars.get(j);
+            car.move(fuelTank.takeFuel());
 
+            scoreBoard.setDistance(car.getDistance(), round + 1, j + 1);
+        }
+    }
 }
--- src/test/java/step3/racinggame/core/RacingGameTest.java
+++ src/test/java/step3/racinggame/core/RacingGameTest.java
@@ -1,34 +1,63 @@
 package step3.racinggame.core;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step3.racinggame.view.GamePad;
 
 class RacingGameTest {
 
-    // '모든 로직에 단위 테스트를 구현한다.' 때문에 만든 테스트 클래스인데요,
-    // 제가 제대로 요구사항을 이해한건지 확신이 서지 않습니다...
-
+    private static final int NUM_OF_GAME = 3;
+    private static final int NUM_OF_CAR = 4;
 
     @Test
-    @DisplayName("주어진 차 수만큼 차량 생성 테스트")
-    void init_cars() {
-        RacingGame game = new RacingGame();
-        List<Car> cars = game.readyToRace(3);
-        assertThat(cars.size()).isEqualTo(3);
+    @DisplayName("주어진 게임 정보에 대해 경기 후의 결과 테스트")
+    void play_with_game_info() {
+        RacingGame game = new RacingGame(initCars(), initFuelTank());
+
+        ScoreBoard resultScoreBoard = game.play(NUM_OF_GAME);
+
+        assertAll(
+                () -> assertThat(resultScoreBoard.getDistance(1, 1)).isEqualTo(0),
+                () -> assertThat(resultScoreBoard.getDistance(1, 2)).isEqualTo(0),
+                () -> assertThat(resultScoreBoard.getDistance(1, 3)).isEqualTo(1),
+                () -> assertThat(resultScoreBoard.getDistance(1, 4)).isEqualTo(1),
+                () -> assertThat(resultScoreBoard.getDistance(2, 1)).isEqualTo(0),
+                () -> assertThat(resultScoreBoard.getDistance(2, 2)).isEqualTo(1),
+                () -> assertThat(resultScoreBoard.getDistance(2, 3)).isEqualTo(2),
+                () -> assertThat(resultScoreBoard.getDistance(2, 4)).isEqualTo(1),
+                () -> assertThat(resultScoreBoard.getDistance(3, 1)).isEqualTo(1),
+                () -> assertThat(resultScoreBoard.getDistance(3, 2)).isEqualTo(2),
+                () -> assertThat(resultScoreBoard.getDistance(3, 3)).isEqualTo(2),
+                () -> assertThat(resultScoreBoard.getDistance(3, 4)).isEqualTo(1)
+        );
+
+        /*  <result>
+            [0, 0, 1, 1]
+            [0, 1, 2, 1]
+            [1, 2, 2, 1]
+         */
     }
 
-    @Test
-    @DisplayName("주어진 경기 수만큼 게임 후 점수 생성 테스트")
-    void play_game_get_scores() {
-        RacingGame game = new RacingGame();
-        List<Car> cars = game.readyToRace(3);
+    private List<Car> initCars() {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < NUM_OF_CAR; i++) {
+            cars.add(new Car());
+        }
+        return cars;
+    }
 
-        List<List<Integer>> result = game.playGame(5, cars);
-
-        assertThat(result.size()).isEqualTo(5);
+    private FuelTank initFuelTank() {
+        FuelTank fuelTank = new FuelTank();
+        for (int i = 0; i < NUM_OF_GAME * NUM_OF_CAR; i++) {
+            fuelTank.addFuel(i * 3 % 10);
+            //연료: 0, 3, 6, 9,  12, 15, 18, 21,  24, 27, 30, 33
+        }
+        return fuelTank;
     }
 
 }
