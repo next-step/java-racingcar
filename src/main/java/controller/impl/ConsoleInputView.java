ce09ebce119cdaf99f06e17dc0e4934730334d72
김찬수 <cskim96@crscube.co.kr>
2022-09-23 10:39:38 +0900

    :recycle: refactor(ui) : controller 코드 리팩토링
    enum 타입으로 메시지 분리 , builder 패턴 도입

--- src/main/java/controller/impl/ConsoleInputView.java
+++ src/main/java/controller/impl/ConsoleInputView.java
@@ -1,31 +1,36 @@
 package controller.impl;
 
 import controller.InputView;
+import enumeration.GameMessage;
 import model.GameParam;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import static enumeration.GameMessage.*;
+
 
 public class ConsoleInputView implements InputView {
 
-    public static final String CAR_NUMBER_ASK_QUESTION = "자동차 대수는 몇 대 인가요?";
-    public static final String TRY_NUMBER_ASK_QUESTION = "시도할 회수는 몇 회 인가요?";
-    public static final String RUN_RESULT_MSG = "실행 결과";
-    public static final String RUN_FAIL_USER_MSG = "자동차 대수 , 시도할 회수는 숫자값이여만 합니다.";
-
     @Override
     public GameParam getInputFromUser() {
-        try(Scanner sc = new Scanner(System.in)){
-            System.out.println(CAR_NUMBER_ASK_QUESTION);
+        try (Scanner sc = new Scanner(System.in)) {
+            printMessage(CAR_NUMBER_ASK_QUESTION);
             int carNumber = sc.nextInt();
-            System.out.println(TRY_NUMBER_ASK_QUESTION);
+            printMessage(TRY_NUMBER_ASK_QUESTION);
             int tryNumber = sc.nextInt();
-            System.out.println(RUN_RESULT_MSG);
-            return new GameParam(carNumber,tryNumber);
-        }catch (InputMismatchException e){
-            System.out.println(RUN_FAIL_USER_MSG);
+            printMessage(RUN_RESULT_MSG);
+            return new GameParam.GameParamBuilder()
+                    .carNum(carNumber)
+                    .tryNum(tryNumber)
+                    .build();
+        } catch (InputMismatchException e) {
+            printMessage(RUN_FAIL_MSG);
             throw new IllegalArgumentException(e);
         }
+    }
+
+    private void printMessage(GameMessage message) {
+        System.out.println(message.getMsg());
     }
 }
--- src/main/java/model/GameParam.java
+++ src/main/java/model/GameParam.java
@@ -2,13 +2,33 @@ package model;
 
 public class GameParam {
 
-    private final int carNum;
+    private int carNum;
 
-    private final int tryNum;
+    private int tryNum;
 
-    public GameParam(int carNum, int tryNum) {
-        this.carNum = carNum;
-        this.tryNum = tryNum;
+    public static class GameParamBuilder {
+        private int carNum;
+
+        private int tryNum;
+
+        public GameParamBuilder carNum(int carNum){
+            this.carNum = carNum;
+            return this;
+        }
+
+        public GameParamBuilder tryNum(int tryNum){
+            this.tryNum = tryNum;
+            return this;
+        }
+
+        public GameParam build(){
+            return new GameParam(this);
+        }
+    }
+
+    private GameParam(GameParamBuilder builder) {
+        this.carNum = builder.carNum;
+        this.tryNum = builder.tryNum;
     }
 
     public int getCarNum() {
