011e2bdfa788119844dcb8a2961dff324fde769c
0chil <0@chll.it>
2022-09-23 12:25:50 +0900

    refactor(calculator): 메서드 분리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,14 +2,34 @@ package calculator;
 
 public class StringCalculator {
 
-	public Integer calculate(String input) {
-		if (input == null || input.length() == 0)
-			return 0;
-		String[] split = input.split(",|:");
+	private String separator() {
+		return ",|:";
+	}
+
+	private String[] expressions(String input) {
+		if (input == null || input.isBlank())
+			return new String[] {};
+		return input.split(separator());
+	}
+
+	private int sum(int[] numbers) {
 		int sum = 0;
-		for (String number : split) {
-			sum += Integer.parseInt(number);
+		for (int number : numbers) {
+			sum += number;
 		}
 		return sum;
+	}
+
+	private int[] numbers(String[] expressions) {
+		int[] numbers = new int[expressions.length];
+		for (int i = 0; i < expressions.length; i++) {
+			numbers[i] = Integer.parseInt(expressions[i]);
+		}
+		return numbers;
+	}
+
+	public int calculate(String input) {
+		String[] expressions = expressions(input);
+		return sum(numbers(expressions));
 	}
 }
