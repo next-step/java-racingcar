96632b9ba15d3ff0f4d14f4b41cc681acc80c5d2
suchan <aud248@naver.com>
2022-09-23 14:52:35 +0900

    feat: 자동차 경기 I/O 클래스 추가

---
+++ src/main/java/step3/RacingCarInput.java
@@ -0,0 +1,16 @@
+package step3;
+
+import java.util.Scanner;
+
+public class RacingCarInput {
+
+    public Integer carCount(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public Integer tryCount(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
---
+++ src/main/java/step3/RacingCarOutput.java
@@ -0,0 +1,16 @@
+package step3;
+
+public class RacingCarOutput {
+
+    public void printCarCount(){
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    public void printTryCount(){
+        System.out.println("시도할 회수는 몇 회인가요?");
+    }
+
+    public void printRacingResult(){
+        System.out.println("-");
+    }
+}
