3061a8b417c2172c4476843faeeae6251761d073
0chil <0@chll.it>
2022-09-23 11:38:48 +0900

    feat(calculator): 숫자를 분리해 더하기

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,6 +3,13 @@ package calculator;
 public class StringCalculator {
 
 	public Integer calculate(String input) {
-		return 0;
+		if (input == null || input.length() == 0)
+			return 0;
+		String[] split = input.split(",");
+		int sum = 0;
+		for (String number : split) {
+			sum += Integer.parseInt(number);
+		}
+		return sum;
 	}
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -15,4 +15,10 @@ public class StringCalculatorTest {
 		assertThat(stringCalculator.calculate(null)).isZero();
 		assertThat(stringCalculator.calculate("")).isZero();
 	}
+
+	@DisplayName("문자열에서 숫자를 분리해 더한다")
+	@Test
+	void split_and_sum() {
+		assertThat(stringCalculator.calculate("1,2,3")).isEqualTo(6);
+	}
 }
