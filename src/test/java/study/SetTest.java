d5f5057f043a73e6b06555dd55bd5b42b7c71ec9
JopopScript <jopopscript@JopopScriptui-MacBookPro-2.local>
2022-09-24 03:53:21 +0900

    test: string split charAt / hashSet contains

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,48 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void hashSetSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    void numbersContains() {
+        assertThat(numbers).contains(1, 2, 3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void numbersContainsByParameterizedTest(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:4", "2:5", "3:"}, delimiter = ':')
+    void shouldBeNotEqual(Integer expectTrue, Integer expectFalse) {
+        assertThat(numbers.contains(expectTrue)).isTrue();
+        assertThat(numbers.contains(expectFalse)).isFalse();
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+public class StringTest {
+    @Test
+    @DisplayName("split메소드가 지정한문자를 기준으로 정상적으로 분리되어야 함")
+    void split() {
+        String givenData = "1,2";
+        String[] result = givenData.split(",");
+        assertThat(result).contains("1","2");
+        assertThat(result).containsExactly("1","2");
+    }
+
+//    @Test
+//    void substring() {
+//        String givenData = "(1,2)";
+//        String result = givenData.substring(1, 4);
+//        System.out.println(result);
+//    }
+
+    String substring(String givenData) {
+        return givenData.substring(1, 4);
+    }
+
+    @Test
+    @DisplayName("charAt메소드가 지정한 위치의 문자를 char로 반환 해야함")
+    void charAt_success() {
+        String givenData = "abc";
+        assertThat(givenData.charAt(0)).isEqualTo('a');
+        assertThat(givenData.charAt(1)).isEqualTo('b');
+        assertThat(givenData.charAt(2)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("charAt메소드가 문자열의 길이를 벗하는 index를 지정했을때 StringIndexOutOfBoundsException가 발생해야함")
+    void charAt_fail() {
+        String givenData = "abc";
+        Throwable thrown = catchThrowable(() -> { givenData.charAt(4); });
+        assertThat(thrown).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
