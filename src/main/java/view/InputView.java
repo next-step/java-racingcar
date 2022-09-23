8120172e350b20d4ebf729d5586762c5e7db5b7d
michol.lee <lovethefeel@gmail.com>
2022-09-23 16:55:10 +0900

    feat: 입력 객체 생성

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,16 @@
+package view;
+
+public class InputView {
+
+    private InputView() {}
+
+    public static void carCount() {
+
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    public static void playCount() {
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+    }
+}
