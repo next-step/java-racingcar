ca0a845f41f5ae1be4c177dfb08e67d72a55c925
KimWanJoo <wjwan0915@gmail.com>
2022-09-24 03:30:38 +0900

    :recycle: refactor(입력과 출력 컨트롤러 구분)

--- src/main/java/racing/application/Main.java
+++ src/main/java/racing/application/Main.java
@@ -1,15 +1,21 @@
 package racing.application;
 
-import racing.controller.RacingController;
-import racing.dto.CarRaceDto;
+import racing.application.controller.RacingController;
+import racing.application.service.RacingServiceImpl;
+import racing.application.view.InputView;
+import racing.application.view.View;
 
 public class Main {
 
     public static void main(String[] args) {
-        RacingController racingController = new RacingController();
+        InputView inputView = new InputView();
+        RacingController racingController = new RacingController(new RacingServiceImpl());
 
-        CarRaceDto carRaceDto = racingController.makeCars();
+        int numberOfCars = inputView.numberOfCars();
+        int moveCount = inputView.moveCount();
 
-        racingController.racingGame(carRaceDto);
+        View view = racingController.racingGame(numberOfCars, moveCount);
+
+        view.print();
     }
 }
