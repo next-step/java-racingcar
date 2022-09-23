51f0866304d8e8d4f547568f652ab82ee4907974
D <doxxx93@gmail.com>
2022-09-23 19:06:33 +0900

    step2 Positive plus test

--- src/test/java/step2/PositiveTest.java
+++ src/test/java/step2/PositiveTest.java
@@ -10,7 +10,7 @@ import step2.exception.NegativeNumberException;
 public class PositiveTest {
 
     @Test
-    @DisplayName("생성자에 ")
+    @DisplayName("문자열과 숫자를 통해 생성된 Positive 객체는 같은 객체이다.")
     void create1() {
         assertThat(new Positive("1")).isEqualTo(new Positive(1));
     }
@@ -21,5 +21,13 @@ public class PositiveTest {
         assertThatExceptionOfType(NegativeNumberException.class)
             .isThrownBy(() -> new Positive("-1"))
             .withMessageMatching("음수는 입력할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("값 객체를 더하게 되면 더한 값을 갖는 Positive 객체가 반환된다.")
+    void sum() {
+        Positive positive1 = new Positive("1");
+        Positive positive2 = new Positive("2");
+        assertThat(positive1.plus(positive2)).isEqualTo(new Positive(3));
     }
 }
