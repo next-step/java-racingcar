1d303f0f8140c8c3b6e2ce561e6b69fc6f1f4fcb
michol.lee <lovethefeel@gmail.com>
2022-09-23 17:03:28 +0900

    feat(test): 레이싱 자동차 결과 계산 테스트 케이스

---
+++ src/test/java/domain/RacingScoreTest.java
@@ -0,0 +1,22 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingScoreTest {
+
+    @DisplayName("자동차 레이싱 이동 결과를 - 로 변환한다.")
+    @Test
+    void racing_convert() {
+
+        final RacingCar racingCar = RacingCar.init();
+        racingCar.move(() -> true);
+        final RacingCars racingCars = RacingCars.from(racingCar);
+        final RacingScore racingScore = RacingScore.init();
+        racingScore.save(racingCars);
+
+        assertThat(racingScore.score().get(0)).contains("-");
+    }
+}
