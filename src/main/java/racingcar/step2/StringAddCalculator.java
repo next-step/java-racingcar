c354577561c1db82035288a4a878efb090223c0d
Myunghwan Kim <samkim2626@gmail.com>
2022-09-24 01:57:57 +0900

    기능 요구사항 3
    - 숫자 두 개를 컴마(,)로 구분해 합을 반환

--- src/main/java/racingcar/step2/StringAddCalculator.java
+++ src/main/java/racingcar/step2/StringAddCalculator.java
@@ -2,19 +2,33 @@ package racingcar.step2;
 
 public class StringAddCalculator {
 
-	public static int sum(String input) {
+	public static final String COMMA = ",";
+
+	public static int calculate(String input) {
 		if (isBlank(input)) {
 			return 0;
 		}
 
-		return parseToInt(input);
+		return sum(split(input));
+	}
+
+	private static boolean isBlank(String input) {
+		return input == null || input.isBlank();
 	}
 
 	private static int parseToInt(String input) {
 		return Integer.parseInt(input);
 	}
 
-	private static boolean isBlank(String input) {
-		return input == null || input.isBlank();
+	private static String[] split(String input) {
+		return input.split(COMMA);
+	}
+
+	private static int sum(String[] numbers) {
+		int result = 0;
+		for (String number : numbers) {
+			result += parseToInt(number);
+		}
+		return result;
 	}
 }
--- src/test/java/racingcar/step2/StringAddCalculatorTest.java
+++ src/test/java/racingcar/step2/StringAddCalculatorTest.java
@@ -3,6 +3,7 @@ package racingcar.step2;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
@@ -15,7 +16,7 @@ class StringAddCalculatorTest {
 	@NullSource
 	@ValueSource(strings = {"", " "})
 	void sumNullOrEmptyTest(String input) {
-		int result = StringAddCalculator.sum(input);
+		int result = StringAddCalculator.calculate(input);
 
 		assertThat(result).isZero();
 	}
@@ -24,8 +25,16 @@ class StringAddCalculatorTest {
 	@ParameterizedTest
 	@CsvSource(value = {"1:1", "2:2", "3:3"}, delimiter = ':')
 	void sumOneNumberTest(String input, int expected) {
-		int result = StringAddCalculator.sum(input);
+		int result = StringAddCalculator.calculate(input);
 
 		assertThat(result).isEqualTo(expected);
+	}
+
+	@DisplayName("숫자 두 개를 컴마(,)로 구분해 합을 반환")
+	@Test
+	void splitWithCommaAndSumTest() {
+		int result = StringAddCalculator.calculate("1,2");
+
+		assertThat(result).isEqualTo(3);
 	}
 }
