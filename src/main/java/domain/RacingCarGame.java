91aa91fed43126a74c995b52f98a9632df397718
michol.lee <lovethefeel@gmail.com>
2022-09-23 16:57:57 +0900

    feat: 자동차 경주 게임 메인 로직 실행

---
+++ src/main/java/domain/RacingCarGame.java
@@ -0,0 +1,38 @@
+package domain;
+
+import view.InputView;
+import view.ResultView;
+
+import java.util.Scanner;
+
+public class RacingCarGame {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        InputView.carCount();
+        final String cars = scanner.next();
+        final RacingCars racingCars = RacingCars.from(cars);
+        InputView.playCount();
+        final String attempt = scanner.next();
+        final RacingAttempt racingAttempt = RacingAttempt.from(attempt);
+        ResultView.resultTitle();
+        gamePlay(racingAttempt, racingCars);
+    }
+
+    private static void gamePlay(final RacingAttempt racingAttempt, final RacingCars racingCars) {
+
+        final RacingScore racingScore = RacingScore.init();
+        raceStart(racingAttempt, racingCars, racingScore);
+        ResultView.resultPrint(racingScore);
+    }
+
+    private static void raceStart(final RacingAttempt racingAttempt, final RacingCars racingCars, final RacingScore racingScore) {
+
+        while (racingAttempt.isAttempt()) {
+            racingAttempt.race(racingCars);
+            racingScore.save(racingCars);
+        }
+    }
+}
