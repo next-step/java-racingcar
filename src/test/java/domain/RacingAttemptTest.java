1071bd5f50d94bad1babcfeb1bc474df38239185
michol.lee <lovethefeel@gmail.com>
2022-09-23 16:56:52 +0900

    feat(test): 레이싱 시도 횟수 테스트 케이스

---
+++ src/test/java/domain/RacingAttemptTest.java
@@ -0,0 +1,30 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RacingAttemptTest {
+
+    @DisplayName("시도 횟수는 0이하인 경우 익셉션 처리한다.")
+    @Test
+    void race_zero_attempt() {
+
+        assertThatThrownBy(() -> RacingAttempt.from("0"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력값은 1 이상이어야 합니다.");
+    }
+
+    @DisplayName("시도 횟수의 문자가 포함된 경우 익셉션 처리한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"d1", "te"})
+    void race_string_attempt(final String str) {
+
+        assertThatThrownBy(() -> RacingAttempt.from(str))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자만 입력해야 합니다.");
+    }
+}
