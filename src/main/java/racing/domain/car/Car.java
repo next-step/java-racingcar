1061affb9619e2c73548b71d9ef85f52f9fd6dfa
KimWanJoo <wjwan0915@gmail.com>
2022-09-24 03:32:29 +0900

    :recycle: refactor(equals 추가)

--- src/main/java/racing/domain/car/Car.java
+++ src/main/java/racing/domain/car/Car.java
@@ -1,26 +1,45 @@
 package racing.domain.car;
 
+import java.util.Objects;
+
 public class Car {
 
-    private final CarPosition carPosition;
+    private final Position position;
 
     public Car() {
         this(0);
     }
 
-    public Car(CarPosition carPosition) {
-        this.carPosition = carPosition;
+    public Car(Position position) {
+        this.position = position;
     }
 
     public Car(int position) {
-        this(new CarPosition(position));
+        this(new Position(position));
     }
 
     public Car move() {
-        return new Car(carPosition.advance());
+        return new Car(position.advance());
     }
 
     public int position() {
-        return carPosition.value();
+        return position.value();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(position, car.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
