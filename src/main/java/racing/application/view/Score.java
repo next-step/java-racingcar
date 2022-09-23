bdbcbd17ea0d7a00202ffcb02bb249e88fb6261c
KimWanJoo <wjwan0915@gmail.com>
2022-09-24 03:22:40 +0900

    :sparkles: feat(Score Class 작성)
    출력용 데이터를 가지는 응답 클래스 작성

---
+++ src/main/java/racing/application/view/Score.java
@@ -0,0 +1,29 @@
+package racing.application.view;
+
+import java.util.List;
+import racing.domain.car.Car;
+import racing.domain.car.CarRace;
+
+public class Score {
+
+    private final List<CarRace> carRaces;
+
+    public Score(List<CarRace> carRaces) {
+        this.carRaces = carRaces;
+    }
+
+    public void print() {
+        carRaces.forEach(carRace -> {
+            carRace.cars().forEach(this::printCar);
+            System.out.println();
+        });
+    }
+
+    public int size() {
+        return carRaces.size();
+    }
+
+    private void printCar(Car car) {
+        System.out.println("-".repeat(car.position()));
+    }
+}
