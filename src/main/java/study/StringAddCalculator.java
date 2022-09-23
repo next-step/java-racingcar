19f2b5c0c51b1715886b20d3081ec15b91cdf19c
caoyu-dev <choyumin01@gmail.com>
2022-09-23 23:28:59 +0900

    [Feat] 요구사항 1 - 빈 문자열 또는 null 값 입력되는 경우 0 을 반환

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,11 @@
+package study;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String str) {
+        int result = 0;
+        if (str == null || str == "") {
+            result = 0;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -0,0 +1,18 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringAddCalculatorTest {
+    @Test
+    @DisplayName("빈 문자열 또는 null 값을 입력하는 경우에는 0 을 반환한다.")
+    public void splitAndSum_null_or_empty_value() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+}
