0e772bc911ed40da91508ce796b114fc1256bc44
KimWanJoo <wjwan0915@gmail.com>
2022-09-24 03:18:32 +0900

    :fire: refactor(class 삭제)

--- src/main/java/racing/domain/car/usecase/CreateUseCase.java
+++
@@ -1,8 +0,0 @@
-package racing.domain.car.usecase;
-
-import racing.dto.CarRaceDto;
-
-public interface CreateUseCase {
-
-    CarRaceDto makeCars(int numberOfCar);
-}
--- src/main/java/racing/domain/car/usecase/RacingUseCase.java
+++
@@ -1,8 +0,0 @@
-package racing.domain.car.usecase;
-
-import racing.dto.CarRaceDto;
-
-public interface RacingUseCase {
-
-    CarRaceDto racingGame(CarRaceDto carRaceDto);
-}
