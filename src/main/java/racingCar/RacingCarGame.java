9d51ff327ccb43e2a2a6004ecc33d5a72553f294
hiblue02 <hiblue02@gmail.com>
2022-09-24 00:13:09 +0900

    feat (make InputView Function)

--- src/main/java/racingCar/RacingCarGame.java
+++ src/main/java/racingCar/RacingCarGame.java
@@ -1,7 +1,17 @@
 package racingCar;
 
-public class RacingCarGame {
-    public static void main(String[] args) {
+import racingCar.view.InputView;
 
+public class RacingCarGame {
+
+    public static void main(String[] args) {
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.play();
+    }
+
+    public void play(){
+        InputView inputView = new InputView();
+        int numberOfCars = inputView.askHowMayCars();
+        int numberOfTimes = inputView.askHowManyTimes();
     }
 }
--- src/main/java/racingCar/view/InputView.java
+++ src/main/java/racingCar/view/InputView.java
@@ -1,4 +1,33 @@
 package racingCar.view;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class InputView {
+    private final String ASK_HOW_MANY_CARS_MESSAGE = "자동 대수는 몇 대 인가요?";
+    private final String ASK_HOW_MANY_TIMES_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private final String NOT_INTGER_TYPE ="정수 타입이 아닙니다.";
+    private final String PLEASE_ENTER_AGAIN ="다시 입력 해주세요.";
+
+    public int askHowMayCars(){
+        System.out.println(ASK_HOW_MANY_CARS_MESSAGE);
+        return scanInt();
+    }
+    public int askHowManyTimes(){
+        System.out.println(ASK_HOW_MANY_TIMES_MESSAGE);
+        return scanInt();
+    }
+
+    private int scanInt(){
+        Scanner scanner = new Scanner(System.in);
+        try{
+            int number = scanner.nextInt();
+            return number;
+        }catch (InputMismatchException e){
+            System.out.println(NOT_INTGER_TYPE);
+            System.out.println(PLEASE_ENTER_AGAIN);
+            scanInt();
+        }
+        return 0;
+    }
 }
