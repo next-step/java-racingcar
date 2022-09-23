c4d67d0953f5aefb65609cbd35fc48217cfcb85b
jimbae <58099885+jimbaemon@users.noreply.github.com>
2022-09-23 23:16:06 +0900

    2단계 - 문자열 덧셈 계산기 (#3540)
    * test: 제공된 테스트 케이스 적용.
    
    * feat: 첫번쨰 테스트 요구사항 충족.
    
    * feat: 숫자 하나 처리 :)
    
    * feat: 쉼표 하나 처리.
    
    * feat: 쉼표 또는 콜론 처리.
    
    * feat: custom 구분자 처리.
    
    * feat: 음수일 경우 Runtime Exception 발생처리.
    
    * refactor: split -> tokens 명명.
    
    * refactor: indent 한번으로 감소.
    
    * refactor: token을 가져오는 함수 분리.
    
    * refactor: Magic String 제거.
    
    * refactor: 캡슐화.
    
    * refactor: delimiter 와 text 를 가져오는 함수로 쪼개기.
    
    * refactor: 함수명에 일치하게 split과 sum 을 진행하도록 수정.
    
    * refactor: 주요 기능을 하는 함수를 상단으로 위치 변경.
    
    * refactor: 패턴 상수 처리.
    
    * refactor: 사용하지 않는 예외처리 제거.
    
    * refactor: regex 수정.
    
    * refactor: IDE 를 이용한 컨벤션 설정.
    
    * refactor: 테스트 Parameterized 로 전환하여 진행.
    
    * fix: " " blank 함수를 이용하여 스페이스 빈값 에러 처리.
    
    * refactor: 빈값에 대한 처리 함수화.
    
    * refactor: custom delimiter 에 자리값 magic int -> 상수 처리

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,62 @@
+package study;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\\n(.*)");
+    private static final int DELIMITER_LOCATION = 1;
+    private static final int TEXT_LOCATION = 2;
+
+    public static int splitAndSum(final String text) {
+        if (isNullOrBlank(text)) {
+            return 0;
+        }
+        String[] tokens = split(text);
+        return sum(tokens);
+    }
+
+    private static boolean isNullOrBlank(String text) {
+        return text == null || text.isBlank();
+    }
+
+    private static String[] split(final String text){
+        String delimiter = delimiter(text);
+        String convertedText = convertText(text);
+        return convertedText.split(delimiter);
+    }
+
+    private static int sum(final String[] token){
+        int result = 0;
+        for (String text : token) {
+            int number = parsePositiveNumber(text);
+            result += number;
+        }
+        return result;
+    }
+
+    private static String delimiter(final String text){
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if(matcher.find()){
+            return matcher.group(DELIMITER_LOCATION);
+        }
+        return DEFAULT_DELIMITER;
+    }
+
+    private static String convertText(final String text){
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if(matcher.find()){
+            return matcher.group(TEXT_LOCATION);
+        }
+        return text;
+    }
+
+    private static int parsePositiveNumber(final String text) {
+        int number = Integer.parseInt(text);
+        if(number<0){
+            throw new RuntimeException();
+        }
+        return number;
+    }
+}
