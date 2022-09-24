4bcb4fe18b6531f7f60e8b91b84473af4b43c738
wugatama <dnr7410@naver.com>
2022-09-25 00:01:58 +0900

    feat: 자동차 움직임 횟구 구현

--- src/main/java/step3/Cars.java
+++ src/main/java/step3/Cars.java
@@ -4,4 +4,8 @@ public class Cars {
 	public static int participant(String input) {
 		return Integer.parseInt(input);
 	}
+
+	public static int move(String input) {
+		return Integer.parseInt(input);
+	}
 }
