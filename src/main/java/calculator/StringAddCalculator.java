4aec51cb8e00c93e3969c23a09d2c3738111f4d7
mean-ji <winter7574@gmail.com>
2022-09-24 20:06:04 +0900

    refactor: StringAddCalculator 메서드 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,31 +4,60 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    public static final String SEPARATOR = ",|:";
+    public static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
 
     public static int splitAndAddString(String numbersText) {
-        if (numbersText == null || numbersText.isEmpty()) {
+        if (isEmptyText(numbersText)) {
             return 0;
         }
 
-        String[] numbers;
-        int result = 0;
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(numbersText);
+        return add(stringArrayToIntArray(split(numbersText)));
+    }
 
-        if (m.find()) {
-            String customDelimeter = m.group(1);
-            numbers = m.group(2).split(customDelimeter);
-        } else {
-            numbers = numbersText.split(",|:");
+    private static String[] split(String numbersText) {
+        Matcher m = Pattern.compile(CUSTOM_SEPARATOR).matcher(numbersText);
+
+        if (containCustomSeparator(m)) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
         }
 
-        for (String text : numbers) {
-            int number = Integer.parseInt(text);
-            if (number < 0) {
-                throw new RuntimeException();
-            }
+        return numbersText.split(SEPARATOR);
+    }
+
+    private static boolean containCustomSeparator(Matcher m) {
+        return m.find();
+    }
+
+    public static int[] stringArrayToIntArray (String[] numbers) {
+        int[] result = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            result[i] = textToInt(numbers[i]);
+        }
+        return result;
+    }
+
+    private static int add(int[] numbers) {
+        int result = 0;
+
+        for (int number : numbers) {
             result += number;
         }
-
         return result;
+    }
+
+    private static int textToInt(String text) {
+        int number = Integer.parseInt(text);
+
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
+    private static boolean isEmptyText(String numbersText) {
+        return numbersText == null || numbersText.isEmpty();
     }
 }
