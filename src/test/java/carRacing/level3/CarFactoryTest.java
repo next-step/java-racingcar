b9490e598a7999b603cb55c0dd5d4c44c977a95b
sienna <20200803@sungshin.ac.kr>
2022-09-24 15:23:36 +0900

    :white_check_mark:refactor : 객체 생성 리팩토링

--- src/test/java/carRacing/level3/CarFactoryTest.java
+++ src/test/java/carRacing/level3/CarFactoryTest.java
@@ -2,7 +2,6 @@ package carRacing.level3;
 
 import carRacing.level3.domain.CarFactory;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,12 +9,7 @@ public class CarFactoryTest {
 
 	public static final Integer CARNUM = 1;
 
-	CarFactory carFactory;
-
-	@BeforeEach
-	void CarFactory() {
-		CarFactory carFactory = new CarFactory();
-	}
+	CarFactory carFactory = new CarFactory();
 
 	@Test
 	@DisplayName("처음 라운드는 모두 한칸씩 이동하여 현재 이동 상태는 1이다.")
@@ -24,7 +18,5 @@ public class CarFactoryTest {
 		int status = carFactory.getCarStatus(CARNUM);
 		Assertions.assertEquals(status, 1);
 	}
-
-
 
 }
