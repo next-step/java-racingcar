54eebac4797bbc44626fb0374a93a89a985373ea
hiblue02 <hiblue02@gmail.com>
2022-09-24 23:45:23 +0900

    refactor(InputView): scanInputNumber 음수 검증 기능 추가
    scanInputNumber - return 부분에 함수 호출하도록 수정(재귀), 입력된 숫자가 양수일 때만 종료하도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,5 @@
 package racingcar.view;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
@@ -9,6 +8,10 @@ public class InputView {
     private static final String ASK_HOW_MANY_TIMES_MESSAGE = "시도할 회수는 몇 회 인가요?";
     private static final String NOT_INTEGER_TYPE_MESSAGE ="정수 타입이 아닙니다.";
     private static final String PLEASE_ENTER_AGAIN_MESSAGE = "다시 입력 해주세요.";
+
+    private InputView (){
+
+    }
 
     public static int askHowManyCars(){
         System.out.println(ASK_HOW_MANY_CARS_MESSAGE);
@@ -22,14 +25,16 @@ public class InputView {
 
     private static int scanInputNumber(){
         Scanner scanner = new Scanner(System.in);
+
         try{
             int number = scanner.nextInt();
-            return number;
-        }catch (InputMismatchException e){
+            if (number > 0){
+                return number;
+            }
+        }catch (Exception e){
             System.out.println(NOT_INTEGER_TYPE_MESSAGE);
             System.out.println(PLEASE_ENTER_AGAIN_MESSAGE);
-            scanInputNumber();
         }
-        return 0;
+        return scanInputNumber();
     }
 }
