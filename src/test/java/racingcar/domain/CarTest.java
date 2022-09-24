017151ffb6d901d4279ad9bdcc97608a8c706ddc
hiblue02 <hiblue02@gmail.com>
2022-09-25 01:19:47 +0900

    refactor(CarTest): ParameterizedTest 적용

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,7 +2,6 @@ package racingcar.domain;
 
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,9 +12,9 @@ class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     @DisplayName("랜덤값이 4이상이면 달리기")
-    void race_when_random_value_greater_then_4(int random) {
+    void race_when_random_value_greater_then_4(int runUnit) {
         //given
-        Car car = new Car(((min, max) -> random));
+        Car car = new Car(((min, max) -> runUnit));
         //when
         car.run();
         //then
@@ -25,24 +24,26 @@ class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("랜덤값이 3이하면 달리지 않기")
-    void do_not_race_when_random_value_less_than_3(int random) {
+    void do_not_race_when_random_value_less_than_3(int runUnit) {
         //given
-        Car car = new Car(((min, max) -> random));
+        Car car = new Car(((min, max) -> runUnit));
         //when
         car.run();
         //then
         assertThat(car.getDistance()).isZero();
     }
 
-    @Test
-    @DisplayName("그래프 그리기")
-    void test_distance() {
+    @ParameterizedTest
+    @ValueSource(ints = {10})
+    @DisplayName("이동거리 확인하기")
+    void check_distance(int numberOfTimes) {
         //given
-        Car car = new Car();
+        Car car = new Car((min, max) -> 9);
         //when
-        car.run();
-        car.run();
+        for (int time = 0; time < numberOfTimes; time++) {
+            car.run();
+        }
         //then
-        assertThat(car.getDistance()).isEqualTo(2);
+        assertThat(car.getDistance()).isEqualTo(numberOfTimes);
     }
 }
