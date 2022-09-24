36db372145427cc1c9aa7b50de592b374af5315e
Shin seongsu <tlstjdtn321@naver.com>
2022-09-25 00:52:31 +0900

    feat: StringAndCalculator 추가

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,53 @@
+package calculator;
+
+import exception.NegativeNumberException;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    public static final String COMMA_SEPARATOR = "[,:]";
+    public static final String PATTERN = "//(.)\n(.*)";
+
+    public static int splitAndSum(String text) {
+        if (isBlank(text)) {
+            return 0;
+        }
+        int[] numbers = toInts(split(text));
+
+        return sum(numbers);
+    }
+
+    private static boolean isBlank(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private static String[] split(String text) {
+        Matcher m = Pattern.compile(PATTERN).matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+
+        return text.split(COMMA_SEPARATOR);
+    }
+
+    private static int[] toInts(String[] values){
+        return Arrays.stream(values)
+            .mapToInt(Integer::parseInt)
+            .map(StringAddCalculator::checkPositiveNumber)
+            .toArray();
+    }
+
+    private static int checkPositiveNumber(int num) {
+        if (num < 0) {
+            throw new NegativeNumberException();
+        }
+        return num;
+    }
+
+    private static int sum(int[] numbers) {
+        return Arrays.stream(numbers).sum();
+    }
+
+}
