139a843b4e4c3ac4f04d7ad05d9d7c50fd0296fe
scoopsoup <rnrdud46@nate.com>
2022-09-24 19:36:54 +0900

    test : 요구사항1(,와 :로 구분한 후 더하기) 하나의 메서드로 구현 테스트

--- src/test/java/stringsum/StringSumTest.java
+++ src/test/java/stringsum/StringSumTest.java
@@ -30,7 +30,7 @@ public class StringSumTest {
     @DisplayName("요구사항1_기본구분자로 분리된 숫자의 합 반환하기")
     @Test
     void basicSumTest(){
-        assertThat(strSum.basicSum()).isEqualTo(24);
+        assertThat(strSum.basicSum("1:2,3:4,5,7")).isEqualTo(22);
     }
 
     @DisplayName("요구사항2_커스텀 구분자 찾기")
@@ -48,7 +48,7 @@ public class StringSumTest {
     @DisplayName("요구사항2_합구하기")
     @Test
     void customSumTest(){
-        assertThat(strSum.customSum()).isEqualTo(96);
+        assertThat(strSum.basicSum("//-\n1-23:5,67")).isEqualTo(96);
     }
 
     @DisplayName("요구사항3_계산기 예외처리")
