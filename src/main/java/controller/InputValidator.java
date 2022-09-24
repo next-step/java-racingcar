d380a14336bd508a110f19e1057f4acdb55533d5
devcs96 <katd6@naver.com>
2022-09-24 19:40:19 +0900

    :recycle: refactor(ui) : validator 검증로직 리팩토링
    for문 대신 stream으로 변경하여 불필요한 인덱스 변수 제거

--- src/main/java/controller/InputValidator.java
+++ src/main/java/controller/InputValidator.java
@@ -1,23 +1,19 @@
 package controller;
 
+import java.util.Arrays;
+
 public class InputValidator {
 
     private static final int MAX_CAR_NAME_LENGTH = 5;
 
     public void validateName(String[] names) {
-        for (int i = 0; i < names.length; i++) {
-            validateName(names[i]);
-        }
-    }
-
-    private void validateName(String name) {
-        if (isInvalid(name)) {
-            throw new IllegalArgumentException();
-        }
+        Arrays.stream(names).filter(this::isInvalid).findAny().ifPresent((name) -> {
+            throw new IllegalArgumentException(name);
+        });
     }
 
     private boolean isInvalid(String name) {
-        return name == null || name.length() == 0 || name.length() > MAX_CAR_NAME_LENGTH;
+        return name == null || name.isBlank() || name.length() > MAX_CAR_NAME_LENGTH;
     }
 
 }
