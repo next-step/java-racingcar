2971c08aabc164d41637b106e1665b96f80b2a99
조씨 <73353809+caoyu-dev@users.noreply.github.com>
2022-09-24 16:36:04 +0900

    2단계 - 문자열 덧셈 계산기 (#3600)
    * [Feat] 요구사항 1 - 빈 문자열 또는 null 값 입력되는 경우 0 을 반환
    
    * [feat] 요구사항 2 - 숫자 하나 입력되는 경우 해당 숫자 반환
    
    * [feat] 요구사항 3 - 쉼표로 구분한 숫자 두 개를 합하여 반환
    
    * [feat] 요구사항 4 - 쉼표와 콜론(:) 으로 숫자를 나누는 경우
    
    * [feat] 요구사항 5 - 문자 사이의 커스텀 구분자로 숫자를 나누는 경우
    
    * [feat] 요구사항 6 - 음수가 들어가는 경우 예외 발생

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,46 @@
+package study;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    public static String separator = ",|:";
+    public static String delimiter = "//(.)\n(.*)";
+    public static int splitAndSum(String str) {
+        if (isBlank(str)) {
+            return 0;
+        }
+        return sum(split(str));
+    }
+
+    private static String[] split(String str) {
+        Matcher m = Pattern.compile(delimiter).matcher(str);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+        return str.split(separator);
+    }
+
+    private static int sum(String[] str) {
+        return Arrays.stream(str)
+                .mapToInt(StringAddCalculator::toInt)
+                .sum();
+    }
+
+    private static int toInt(String str) {
+        if (!isNumeric(str)) {
+            throw new IllegalArgumentException("음수는 들어가면 안돼요 --> " + str);
+        }
+        return Integer.parseInt(str);
+    }
+
+    private static boolean isNumeric(String str) {
+        return str.chars().allMatch(Character::isDigit);
+    }
+
+    private static boolean isBlank(String str) {
+        return str == null || str == "";
+    }
+}
---
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -0,0 +1,54 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringAddCalculatorTest {
+    @Test
+    @DisplayName("빈 문자열 또는 null 값을 입력하는 경우에는 0 을 반환한다.")
+    void splitAndSum_null_or_empty_value() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    void splitAndSum_single_number() {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    void splitAndSum_separate_number_with_comma() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.")
+    void splitAndSum_separate_number_with_comma_and_colon() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    void splitAndSum_separate_number_with_custom_delimiter() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생해야 한다.")
+    void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
