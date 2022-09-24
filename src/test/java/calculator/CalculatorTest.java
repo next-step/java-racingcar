c5f49f001e502d9eddd2446e216a5899f5fdd2d1
mean-ji <winter7574@gmail.com>
2022-09-24 19:03:09 +0900

    style: CalculatorTest 메서드 이름 수정

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,34 +7,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
     @Test
-    public void splitAndAddString_null_또는_빈문자() {
+    public void null_또는_빈문자() {
         assertThat(StringAddCalculator.splitAndAddString(null)).isEqualTo(0);
         assertThat(StringAddCalculator.splitAndAddString("")).isEqualTo(0);
     }
 
     @Test
-    public void splitAndAddString_숫자_하나() throws Exception {
+    public void 숫자_하나() throws Exception {
         assertThat(StringAddCalculator.splitAndAddString("3")).isEqualTo(3);
     }
 
     @Test
-    public void splitAndAddString_쉼표_구분자() throws Exception {
+    public void 쉼표_구분자() throws Exception {
         assertThat(StringAddCalculator.splitAndAddString("1,2")).isEqualTo(3);
     }
 
     @Test
-    public void splitAndAddString_쉼표_또는_콜론_구분자() throws Exception {
+    public void 쉼표_또는_콜론_구분자() throws Exception {
         assertThat(StringAddCalculator.splitAndAddString("1,2:3")).isEqualTo(6);
     }
 
     @Test
-    public void splitAndAddString_custom_구분자() throws Exception {
+    public void custom_구분자() throws Exception {
         int result = StringAddCalculator.splitAndAddString("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndAddString_negative() throws Exception {
+    public void 음수_Error_발생() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndAddString("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
