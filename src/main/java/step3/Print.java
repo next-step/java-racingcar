c6d373bc7c061d1a8de118343b559de754b7d208
wugatama <dnr7410@naver.com>
2022-09-25 00:12:14 +0900

    feat: Print 용 클래스 구현

---
+++ src/main/java/step3/Print.java
@@ -0,0 +1,14 @@
+package step3;
+
+public class Print {
+	private static final String HOW_MANY_CARS = "자동차 대수는 몇 대 인가요?";
+	private static final String HOW_MANY_MOVING_COUNT = "시도할 횟수는 몇 회 인가요?";
+
+	void howManyCars() {
+		System.out.println(HOW_MANY_CARS);
+	}
+
+	void howManyMovingCount() {
+		System.out.println(HOW_MANY_MOVING_COUNT);
+	}
+}
