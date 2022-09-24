41ecddc7a30756105e3a1d82f0e78a78909b305e
sienna <20200803@sungshin.ac.kr>
2022-09-24 18:24:49 +0900

    :sparkles:feat : 자동차 이동 출력 기능

--- src/main/java/carRacing/level3/controller/RacingGame.java
+++ src/main/java/carRacing/level3/controller/RacingGame.java
@@ -1,21 +1,34 @@
 package carRacing.level3.controller;
 
+import carRacing.level3.output.OutPutView;
 import carRacing.level3.service.CarService;
 
 public class RacingGame {
 
 	CarService carService = new CarService();
+	OutPutView outPutView = new OutPutView();
 
 	public void initRace(int carSum) {
 		for (int car = 0; car < carSum; car++) {
 			carService.initRace(car);
+			showRace(car);
 		}
+		printSpace();
 	}
 
 	public void driveCar(int carSum) {
 		for (int car = 0; car < carSum; car++) {
 			carService.moveCar(car);
+			showRace(car);
 		}
+	}
+
+	public void showRace(int carNum) {
+		outPutView.showCar(carService.carStatus(carNum));
+	}
+
+	public void printSpace() {
+		outPutView.printSpace();
 	}
 
 }
--- src/main/java/carRacing/level3/output/OutPutView.java
+++ src/main/java/carRacing/level3/output/OutPutView.java
@@ -1,14 +1,12 @@
 package carRacing.level3.output;
 
-import java.util.HashMap;
-
 public class OutPutView {
 
-	HashMap<Integer, String> map1 = new HashMap<Integer, String>();
+	public void showCar(int status) {
+		System.out.println("-".repeat(status));
+	}
 
-	public void showRace(int carNum) {
-		System.out.println("-");
-		map1.put(carNum, "-");
-
+	public void printSpace() {
+		System.out.println("\n");
 	}
 }
