d8913d5d9fc906f5b375693518528672a874f06a
kureungkureung <terry9611@naver.com>
2022-09-24 20:06:10 +0900

    refactor(ParsedString): 커스텀 구분자로 문자열 분리 기능 삭제

--- src/main/java/step2/parsed/ParsedString.java
+++ src/main/java/step2/parsed/ParsedString.java
@@ -12,7 +12,7 @@ public class ParsedString implements Parsed {
 
     private static final Pattern NUMBER_JUDGMENT = Pattern.compile(".*[0-9].*");
 
-    private static final Pattern CUSTOM_DELIMITER_JUDGMENT = Pattern.compile("^(//.\\n)");
+    private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
 
     private final String stringToBeParsed;
 
@@ -24,16 +24,7 @@ public class ParsedString implements Parsed {
     public List<String> parsedValue() {
         verifyNegative();
         verifyNumber();
-
-        if (CUSTOM_DELIMITER_JUDGMENT.matcher(stringToBeParsed).find()) {
-            final String customDelimiter = customDelimiter();
-            final String stringParsedIntro = stringParsedIntro();
-            return Arrays.stream(stringParsedIntro.split(customDelimiter))
-                    .collect(toList());
-        }
-
-        final String defaultDelimiterRegex = "[,:]";
-        return Arrays.stream(stringToBeParsed.split(defaultDelimiterRegex))
+        return Arrays.stream(stringToBeParsed.split(DEFAULT_DELIMITER_REGEX))
                 .collect(toList());
     }
 
@@ -47,17 +38,6 @@ public class ParsedString implements Parsed {
         if (!NUMBER_JUDGMENT.matcher(stringToBeParsed).find()) {
             throw new RuntimeException("숫자 이외의 문자를 입력할 수 없습니다.");
         }
-    }
-
-    private String customDelimiter() {
-        final int customDelimiterIndex = 2;
-        return String.valueOf(stringToBeParsed.charAt(customDelimiterIndex));
-    }
-
-    private String stringParsedIntro() {
-        final String introToBeParsed = "//.\n";
-        final int introLength = introToBeParsed.length();
-        return stringToBeParsed.substring(introLength);
     }
 
 }
