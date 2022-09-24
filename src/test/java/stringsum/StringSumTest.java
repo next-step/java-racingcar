c4f2128261825cc867d3c7a67b7d66c6f1022790
scoopsoup <rnrdud46@nate.com>
2022-09-24 19:57:38 +0900

    test : 기본구분자로 문자 구분하는 기능 테스트 삭제 test : 커스텀문자로 문자 구분 후 합구하기 테스트성공

--- src/test/java/stringsum/StringSumTest.java
+++ src/test/java/stringsum/StringSumTest.java
@@ -9,24 +9,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOf
 public class StringSumTest {
     StringSum strSum = new StringSum();
 
-    @DisplayName("요구사항1_쉽표(,)구분자로 문자열 분리하기")
-    @Test
-    void splitRestTest(){
-        assertThat(strSum.basicSplitStr("1,2,3")).containsExactly("1", "2", "3");
-    }
-
-    @DisplayName("요구사항1_콜론(:)구분자로 문자열 분리하기")
-    @Test
-    void splitColonTest(){
-        assertThat(strSum.basicSplitStr("1:2:3")).containsExactly("1", "2", "3");
-    }
-
-    @DisplayName("요구사항1_쉽표(,)와 콜론(:)이 동시에 있을 경우 문자열 분리")
-    @Test
-    void basicSplitStrTest(){
-        assertThat(strSum.basicSplitStr("1:2,3:4,5,7")).containsExactly("1","2","3","4","5","7");
-    }
-
     @DisplayName("요구사항1_기본구분자로 분리된 숫자의 합 반환하기")
     @Test
     void basicSumTest(){
@@ -48,7 +30,7 @@ public class StringSumTest {
     @DisplayName("요구사항2_합구하기")
     @Test
     void customSumTest(){
-        assertThat(strSum.basicSum("//-\n1-23:5,67")).isEqualTo(96);
+        assertThat(strSum.customSum("//-\n1-23:5,67")).isEqualTo(96);
     }
 
     @DisplayName("요구사항3_계산기 예외처리")
