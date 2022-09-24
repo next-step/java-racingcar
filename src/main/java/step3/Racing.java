7581547f9a581da960a25123210b2f8680eae193
wugatama <dnr7410@naver.com>
2022-09-25 00:25:00 +0900

    feat: 전진하는 조건 랜덤값 구현

--- src/main/java/step3/Racing.java
+++ src/main/java/step3/Racing.java
@@ -1,2 +1,23 @@
-package step3;public class Racing {
+package step3;
+
+import java.util.Scanner;
+
+public class Racing {
+	private Print print = new Print();
+	private Scanner sc = new Scanner(System.in);
+
+	public static int random() {
+		int random = (int)(Math.random()*10);
+		return random;
+	}
+
+	public void play() {
+		print.howManyCars();
+		String cars = sc.nextLine();
+		print.howManyMovingCount();
+		String moveCount = sc.nextLine();
+
+		Cars.participant(cars);
+		Cars.move(moveCount);
+	}
 }
