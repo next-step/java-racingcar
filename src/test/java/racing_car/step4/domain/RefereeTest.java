71ff8663c0004b22a2c16315163c20307ae5a10d
JunH <tjdtls690@gmail.com>
2022-09-24 20:01:22 +0900

    :white_check_mark: test : 우승자 여러명 구하기 테스트 추가 및 구조 리팩토링

--- src/test/java/racing_car/step4/domain/RefereeTest.java
+++ src/test/java/racing_car/step4/domain/RefereeTest.java
@@ -1,5 +1,6 @@
 package racing_car.step4.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,16 +10,34 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RefereeTest {
+    private Car jun;
+    private Car young;
+    
+    @BeforeEach
+    void setUp() {
+        jun = new Car(new CarName("jun"), new Position(5));
+        young = new Car(new CarName("young"), new Position(3));
+    }
+    
     @Test
     @DisplayName("우승자 한 명 구하기")
-    void findWinner() {
-        Car jun = new Car(new CarName("jun"), new Position(5));
+    void find_one_inner() {
         Car pobi = new Car(new CarName("pobi"), new Position(4));
-        Car young = new Car(new CarName("young"), new Position(3));
         Cars cars = new Cars(Arrays.asList(jun, pobi, young));
         Referee referee = new Referee(cars);
         
         List<String> winnersName = referee.getWinnersNameList();
         assertThat(winnersName).isEqualTo(List.of("jun"));
+    }
+    
+    @Test
+    @DisplayName("우승자 여러명 구하기")
+    void find_two_winner() {
+        Car pobi = new Car(new CarName("pobi"), new Position(5));
+        Cars cars = new Cars(Arrays.asList(jun, pobi, young));
+        Referee referee = new Referee(cars);
+        
+        List<String> winnersName = referee.getWinnersNameList();
+        assertThat(winnersName).isEqualTo(List.of("jun", "pobi"));
     }
 }
