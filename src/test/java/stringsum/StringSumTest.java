ba48d058dc33b8d7737f2d3558c437b1a1f130cf
scoopsoup <rnrdud46@nate.com>
2022-09-24 23:18:36 +0900

    test : 빈문자열 또는 null값이 들어온 경우 0 반환 기능 테스트

--- src/test/java/stringsum/StringSumTest.java
+++ src/test/java/stringsum/StringSumTest.java
@@ -15,10 +15,22 @@ public class StringSumTest {
         assertThat(strSum.basicSum("1:2,3:4,5,7")).isEqualTo(22);
     }
 
+    @DisplayName("Basic 빈문자일 때 0")
+    @Test
+    void basicEmptyTset(){
+        assertThat(strSum.basicSum("")).isEqualTo(0);
+    }
+
+    @DisplayName("Basic null일 때 0")
+    @Test
+    void basicNullTset(){
+        assertThat(strSum.basicSum(null)).isEqualTo(0);
+    }
+
     @DisplayName("요구사항2_커스텀 구분자 찾기")
     @Test
     void customCharTest(){
-        assertThat(strSum.customChar("//[\n1.23:5,67")).isEqualTo("[");
+        assertThat(strSum.customChar("//]\n1-23:5,67")).isEqualTo("]");
     }
 
     @DisplayName("요구사항2_커스텀 구분자 제거 후 숫자찾기")
@@ -31,6 +43,24 @@ public class StringSumTest {
     @Test
     void customSumTest(){
         assertThat(strSum.customSum("//-\n1-23:5,67")).isEqualTo(96);
+    }
+
+    @DisplayName("null값이 들어오는 경우")
+    @Test
+    void emptyValueTest(){
+        assertThat(strSum.customSum((""))).isEqualTo(0);
+    }
+
+    @DisplayName("빈값이 들어오는 경우")
+    @Test
+    void nullValueTest(){
+        assertThat(strSum.customSum((null))).isEqualTo(0);
+    }
+
+    @DisplayName("숫자하나만 들어가는 경우")
+    @Test
+    void oneNum(){
+        assertThat(strSum.customSum("0")).isEqualTo(0);
     }
 
     @DisplayName("요구사항3_계산기 예외처리")
