bd4997f961877a10bf264717b5cf0f4841979aa9
kureungkureung <terry9611@naver.com>
2022-09-24 20:01:29 +0900

    test(ParedStringByCustomDelimiterTest): 커스텀 구분자로 문자열 분리

---
+++ src/test/java/step2/parsed/ParedStringByCustomDelimiterTest.java
@@ -0,0 +1,22 @@
+package step2.parsed;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class ParedStringByCustomDelimiterTest {
+
+    @Test
+    @DisplayName("문자열 첫 부분에 // 와 \n 사이에 문자가 있을 경우 해당 문자를 구분자로 문자열을 나눌 수 있다.")
+    void a() {
+        String stringToBeParsed = "//;\n1;2";
+        Parsed sut = new ParedStringByCustomDelimiter(stringToBeParsed);
+
+        List<String> parsedValue = sut.parsedValue();
+
+        Assertions.assertThat(parsedValue).containsExactly("1", "2");
+    }
+
+}
