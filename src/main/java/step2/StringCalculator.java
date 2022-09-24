3e245ad0ba82047afb7339bea2aba520ef1cdf7b
Yunsik-Choi <choiys0311@gmail.com>
2022-09-24 20:29:48 +0900

    refactor step2

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -5,16 +5,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+
+    private static final int EMPTY_TEXT = 0;
+    private static final String BASE_REGEX = ",|:";
+
     public int addOperation(String text) {
-        if (text == null)
-            return 0;
+        if (text == null) {
+            return EMPTY_TEXT;
+        }
         String[] numbers = split(text);
         int result = add(numbers);
         return result;
     }
 
     private static String[] split(String text) {
-        String regex = ",|:";
+        String regex = BASE_REGEX;
         Matcher m = Pattern.compile("//(.)\\n(.*)").matcher(text);
         if (m.find()) {
             String separator = m.group(1);
@@ -29,14 +34,12 @@ public class StringCalculator {
     }
 
     public int textCheck(String text) {
-        if (text == null) {
-            return 0;
+        if (text == null || text.isEmpty()) {
+            return EMPTY_TEXT;
         }
-        if (text.isEmpty()) {
-            return 0;
-        }
-        if (text.chars().allMatch(Character::isDigit))
+        if (text.chars().allMatch(Character::isDigit)) {
             return Integer.parseInt(text);
+        }
         throw new RuntimeException();
     }
 
