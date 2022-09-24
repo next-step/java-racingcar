3a744dc3641f184e80df0780e6c83a04afc74351
김남희 <40882598+snt2525@users.noreply.github.com>
2022-09-24 18:18:58 +0900

    1단계 - 학습 테스트 실습 과제 (#3563)
    * 1단계 - 학습 테스트 실습
    
    * 1단계 - 학습 테스트 실습
    
    - Set Collection에 대한 학습 테스트
    
    Co-authored-by: bella.n <bella.n@kakaocorp.com>

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("요구사항1: set 크기 검증")
+    void setCollectionSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("요구사항2: 값 존재 여부 검증")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("요구사항3: 결과 값이 다른 경우 검증")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains2(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+}
