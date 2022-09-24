357ff5524d64e7f5e7e94f48784eef2a0585ed2e
Shin seongsu <tlstjdtn321@naver.com>
2022-09-25 00:53:01 +0900

    test: 테스트 코드 추가

---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,44 @@
+package calculator;
+
+import static calculator.StringAddCalculator.splitAndSum;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.sun.jdi.NativeMethodException;
+import org.junit.jupiter.api.Test;
+
+class StringAddCalculatorTest {
+
+    @Test
+    public void splitAndSum_null_또는_빈문자() {
+        assertThat(splitAndSum(null)).isEqualTo(0);
+        assertThat(splitAndSum("")).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() {
+        assertThat(splitAndSum("1")).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() {
+        assertThat(splitAndSum("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() {
+        assertThat(splitAndSum("1,2:3")).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() {
+        assertThat(splitAndSum("//;\n1;2;3")).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() {
+        assertThatThrownBy(() -> splitAndSum("-1,2,3"))
+            .isInstanceOf(RuntimeException.class);
+    }
+}
