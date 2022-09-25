16c89d461debcc66559ca714f9fcda29e57a7349
kureungkureung <terry9611@naver.com>
2022-09-25 16:51:01 +0900

    test(RacingCarTest): 자동차 전진 가능

---
+++ src/test/java/step3/car/RacingCarTest.java
@@ -0,0 +1,21 @@
+package step3.car;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import step3.number.Number;
+
+class RacingCarTest {
+
+    @Test
+    @DisplayName("숫자가 4이상 나올 경우 자동차는 한 칸 전진한다.")
+    void a() {
+        final Number number = new Fake(4);
+        final Car sut = new RacingCar(number);
+
+        sut.useTurn();
+
+        Assertions.assertThat(sut).isEqualTo(new RacingCar(1, number));
+    }
+
+}
