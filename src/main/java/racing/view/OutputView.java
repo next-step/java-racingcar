ffdfe120e9b6f9470a582ed8378ac1389ea48c51
moonstal1506 <moonstal1506@gmail.com>
2022-09-26 08:31:37 +0900

    feat: 자동차의 상태를 화면에 출력한다.

---
+++ src/main/java/racing/view/OutputView.java
@@ -0,0 +1,23 @@
+package racing.view;
+
+import racing.model.Car;
+
+import java.util.List;
+
+public class OutputView {
+
+	public static void printCarsPosition(List<Car> cars) {
+		for (Car car : cars) {
+			printCarPosition(car);
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	private static void printCarPosition(Car car) {
+		int position = car.getPosition();
+		for (int i = 0; i < position; i++) {
+			System.out.print("-");
+		}
+	}
+}
