65e0b455713ec8ffd338381b9ebd202ba9ff387b
kureungkureung <terry9611@naver.com>
2022-09-25 17:10:04 +0900

    test(RacingCarTest): 자동차 전진x 기능

--- src/test/java/step3/car/RacingCarTest.java
+++ src/test/java/step3/car/RacingCarTest.java
@@ -19,4 +19,15 @@ class RacingCarTest {
         Assertions.assertThat(sut).isEqualTo(new RacingCar(1, number));
     }
 
+    @Test
+    @DisplayName("숫자가 4미만 나올 경우 자동차는 현재 위치에 움직이지 않는다.")
+    void b() {
+        final Number number = new Fake(3);
+        final Car sut = new RacingCar(number);
+
+        sut.useTurn();
+
+        Assertions.assertThat(sut).isEqualTo(new RacingCar(0, number));
+    }
+
 }
