eb2cd16a1ac5b7ffe3a59af1827086c0b1a69dac
kureungkureung <terry9611@naver.com>
2022-09-26 01:22:47 +0900

    feat(StringsToNumbers): 입력값 검증 기능

--- src/main/java/step2/numbers/StringsToNumbers.java
+++ src/main/java/step2/numbers/StringsToNumbers.java
@@ -3,10 +3,13 @@ package step2.numbers;
 import step2.parsed.Parsed;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static java.util.stream.Collectors.toList;
 
 public class StringsToNumbers implements Numbers {
+
+    private static final Pattern NOT_NUMBER_JUDGMENT = Pattern.compile("\\D");
 
     private final Parsed parsed;
 
@@ -16,10 +19,22 @@ public class StringsToNumbers implements Numbers {
 
     @Override
     public List<Integer> numbers() {
+        verifyCanConvertNumbers();
         List<String> parsedValues = parsed.parsedValue();
         return parsedValues.stream()
                 .map(Integer::parseInt)
                 .collect(toList());
+    }
+
+    private void verifyCanConvertNumbers() {
+        List<String> parsedValues = parsed.parsedValue();
+        parsedValues.forEach(this::verifyCanConvertNumber);
+    }
+
+    private void verifyCanConvertNumber(String parsedValue) {
+        if (NOT_NUMBER_JUDGMENT.matcher(parsedValue).find()) {
+            throw new RuntimeException("숫자가 아니거나 음수입니다.");
+        }
     }
 
 }
