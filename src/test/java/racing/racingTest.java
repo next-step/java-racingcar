7f8273bdab268a5660fd1bc194c7cf6478c84468
모진혁 <gkgk246@naver.com>
2022-09-25 21:38:45 +0900

    refactor : 레이싱 테스트 코드 작성

--- src/test/java/racing/racingTest.java
+++ src/test/java/racing/racingTest.java
@@ -2,6 +2,7 @@ package racing;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racing.model.Car;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -11,21 +12,22 @@ public class racingTest {
     private static final int NOT_START = 0;
 
     @Test
-    @DisplayName("자동차가 이동할 수 있을지 판단하고 이동할 수 있다면 위치값이 증가한다.")
+    @DisplayName("자동차가 이동할 수 있을지 판단하고 True라면 위치값이 증가한다.")
     void goStraight_when_isGoStraight_true() {
         Car car = new Car();
-        car.resetAdvanceCondition();
-        if (car.isGoStraight()) {
-            car.goStraight();
-            int currentLocation = car.getCurrentLocation();
-            assertThat(currentLocation).isEqualTo(ONE_GO_STRAIGHT);
-        }
-        if (!car.isGoStraight()) {
-            int currentLocation = car.getCurrentLocation();
-            assertThat(currentLocation).isEqualTo(NOT_START);
-        }
+        car.goStraight(() -> true);
+        final int curLocation = car.getCurrentLocation();
 
+        assertThat(curLocation).isEqualTo(ONE_GO_STRAIGHT);
     }
 
+    @Test
+    @DisplayName("자동차가 이동할 수 있을지 판단하고 False라면 위치값이 그대로 머물게 된다.")
+    void goStraight_when_isGoStraight_false() {
+        Car car = new Car();
+        car.goStraight(() -> false);
+        final int curLocation = car.getCurrentLocation();
+        assertThat(curLocation).isEqualTo(NOT_START);
+    }
 
 }
