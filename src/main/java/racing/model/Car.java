3d8ad8b8e76b57fd1b2c3508b9fa4ff4a69d814d
moonstal1506 <moonstal1506@gmail.com>
2022-09-25 23:56:41 +0900

    feat: 자동차는 random 값에 따라 전진 또는 멈출 수 있다.

--- src/main/java/racing/model/Car.java
+++ src/main/java/racing/model/Car.java
@@ -4,7 +4,21 @@ public class Car {
 
 	private int position;
 
-	public int move() {
-		return position + 1;
+	public Car() {
+		this.position = 0;
+	}
+
+	public void move(int random) {
+		if (isMovable(random)) {
+			position += 1;
+		}
+	}
+
+	private boolean isMovable(int random) {
+		return random >= 4;
+	}
+
+	public int getPosition() {
+		return position;
 	}
 }
--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,14 +1,25 @@
 package racing;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racing.model.Car;
 
 class CarTest {
 
+	@DisplayName("자동차는 random 값이 4이상일 때 움직인다.")
 	@Test
 	void move() {
 		Car car = new Car();
-		Assertions.assertThat(car.move()).isEqualTo(1);
+		car.move(4);
+		Assertions.assertThat(car.getPosition()).isEqualTo(1);
+	}
+
+	@DisplayName("자동차는 random 값이 3이하일 때 움직이지 않는다.")
+	@Test
+	void not_move() {
+		Car car = new Car();
+		car.move(3);
+		Assertions.assertThat(car.getPosition()).isEqualTo(0);
 	}
 }
