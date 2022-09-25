4ac76ba96c0c1db67d3ee28cd24ce5083df196ce
michol.lee <lovethefeel@gmail.com>
2022-09-26 00:30:36 +0900

    refactor: 입력 화면 객체의 입력 처리 메소드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,16 +1,22 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {}
 
-    public static void carCount() {
+    public static String carCount() {
 
         System.out.println("자동차 대수는 몇 대 인가요?");
+        return scanner.next();
     }
 
-    public static void playCount() {
+    public static String playCount() {
 
         System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.next();
     }
 }
