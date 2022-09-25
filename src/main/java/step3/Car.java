43531700186a4a9ba8b21a53966a4c73887b7ee8
myabcc17 <myabcc17@naver.com>
2022-09-25 16:53:23 +0900

    feat(test): add car test case

---
+++ src/main/java/step3/Car.java
@@ -0,0 +1,11 @@
+package step3;
+
+public class Car {
+    public Car move(int number) {
+        return null;
+    }
+
+    public int getPosition() {
+        return 0;
+    }
+}
---
+++ src/test/java/step3/CarTest.java
@@ -0,0 +1,23 @@
+package step3;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CarTest {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,false",
+            "3,false",
+            "4,true",
+            "9,true"
+    })
+    void Given_Integer_GreaterThanFour_When_Move_Then_MoveForward(int number, boolean expected) {
+        Car car = new Car();
+
+        Car moveCar = car.move(number);
+
+        assertThat(car.getPosition() != moveCar.getPosition()).isEqualTo(expected);
+    }
+}
