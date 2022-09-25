81c4984e77ec44b4a5a9abcb49d080f4eb6c4c10
nhs219 <heesun_z@naver.com>
2022-09-26 00:50:27 +0900

    chore(RacingCar) : 배열을 List로 변경

--- src/main/java/racing/RacingCar.java
+++ src/main/java/racing/RacingCar.java
@@ -1,31 +1,40 @@
 package racing;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RacingCar {
 
     public static void racing() {
-
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
 
         inputView.input();
-
         resultView.printTitle();
 
+        List positions = getPositions(inputView.getCars());
+
         for(int i=0 ; i< inputView.getTimes() ; i++) {
-            getResult(inputView.getCars(), inputView.getCars(), resultView);
+            getResult(positions, resultView);
             resultView.printBlank();
         }
     }
 
-    private static void getResult(int cars, int carsNumber, ResultView resultView) {
-        int[] position = new int[carsNumber];
-
-        for(int j = 0; j < cars ; j++) {
-            position[j] = plusCount(position[j], getRandomNum());
-            resultView.printResult(position[j]);
+    private static void getResult(List positions, ResultView resultView){
+        for(int j = 0; j < positions.size() ; j++) {
+            int position = plusCount((int)positions.get(j), getRandomNum());
+            positions.set(j, position);
+            resultView.printResult(position);
         }
+    }
+
+    private static List getPositions(int cars) {
+        List positions = new ArrayList();
+        for(int i = 0; i < cars; i++) {
+            positions.add(0);
+        }
+        return positions;
     }
 
     public static int plusCount(int count, int number) {
