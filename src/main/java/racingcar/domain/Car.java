5b212af2e81347fe4b8613dd33c5a37ce591a7db
이건회 <rg970604@naver.com>
2022-09-25 19:39:30 +0900

    feat(README.md) : 구현 기능 목록 정리

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+public class Car {
+
+    public String positionView = "";
+    private int criteriaNum = 4;
+
+    public Car() {
+    }
+
+    public boolean isMoveAble(int randomInt) {
+        if (randomInt >= criteriaNum) {
+            return true;
+        }
+        return false;
+    }
+
+    public String moveCar(boolean isMoveable) {
+        if (isMoveable == true) {
+            positionView += "-";
+        }
+        return positionView;
+    }
+}
