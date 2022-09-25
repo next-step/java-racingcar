e4dfa4228a7c7dd55cc1a93fb832996944bc2646
heejaeahn <heejahn713@gmail.com>
2022-09-25 11:05:28 +0900

    feat: 2단계 기능 개발

---
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -0,0 +1,37 @@
+package step2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static step2.StringAddCalculator.splitAndSum;
+
+public class StringAddCalculatorTest {
+    @Test
+    void null_또는_space() {
+        assertThat(splitAndSum("")).isEqualTo(0);
+        assertThat(splitAndSum(null)).isEqualTo(0);
+    }
+
+    @Test
+    void 문자열_하나() {
+        assertThat(splitAndSum("1")).isEqualTo(1);
+    }
+
+    @Test
+    void 문자열_쉼표_구분자() {
+        assertThat(splitAndSum("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    void 문자열_쉼표_콜론_구분자() {
+        assertThat(splitAndSum("1,2:3")).isEqualTo(6);
+    }
+
+    @Test
+    void 숫자_음수() {
+        assertThatThrownBy(() -> {
+            splitAndSum("1,2:-3");
+        }).isInstanceOf(RuntimeException.class);
+    }
+}
