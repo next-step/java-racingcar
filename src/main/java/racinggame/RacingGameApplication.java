68e0526ef515fea3a146d57adbac6f37fbd96b39
gumsu <chavi9@naver.com>
2022-09-25 11:54:53 +0900

    refactor: 출력 ui 로직 이동

--- src/main/java/racinggame/RacingGameApplication.java
+++ src/main/java/racinggame/RacingGameApplication.java
@@ -3,13 +3,16 @@ package racinggame;
 public class RacingGameApplication {
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+
         int number = inputView.inputUserCarCount();
         int tryCount = inputView.inputUserTryCount();
 
         RacingCar racingCar = new RacingCar(number);
 
+        resultView.printResultMessage();
         for (int i = 0; i < tryCount; i++) {
-            racingCar.racing();
+            resultView.printStatus(racingCar.racing());
         }
     }
 }
--- src/main/java/racinggame/ResultView.java
+++ src/main/java/racinggame/ResultView.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 public class ResultView {
 
+    public void printResultMessage() {
+        System.out.println("실행 결과");
+    }
     public void printStatus(List<Car> cars) {
         for (Car car : cars) {
             System.out.println("-".repeat(car.getStatus()));
         }
+        System.out.println();
     }
 }
