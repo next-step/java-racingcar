0418c0fd98eb7789bb4a92a7e4513c946d353845
hiblue02 <hiblue02@gmail.com>
2022-09-25 19:47:19 +0900

    refactor(InputView): ASK_HOW_MANY_CARS_MESSAGE 오타 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String ASK_HOW_MANY_CARS_MESSAGE = "자동 대수는 몇 대 인가요?";
+    private static final String ASK_HOW_MANY_CARS_MESSAGE = "자동차 대수는 몇 대 인가요?";
     private static final String ASK_HOW_MANY_TIMES_MESSAGE = "시도할 회수는 몇 회 인가요?";
-    private static final String NOT_INTEGER_TYPE_MESSAGE ="정수 타입이 아닙니다.";
+    private static final String NOT_INTEGER_TYPE_MESSAGE = "정수 타입이 아닙니다.";
     private static final String PLEASE_ENTER_AGAIN_MESSAGE = "다시 입력 해주세요.";
 
     private InputView (){
