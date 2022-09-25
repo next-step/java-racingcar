822dc6e605169ac60a4ee9f66a34da4bef4e6878
shineyuno <shineyuno@gmail.com>
2022-09-25 16:32:53 +0900

    Step2 - 문자열 덧셈 계산기 (#3566)
    * 문자열 덧셈 계산기 클래스 추가 및TestCase 소스 코드 등록
    
    * 문자열 덧셈 계산기 구현
    
    * 패턴 컴파일 효율을 위해, 커스텀구분자 찾는 정규식 재사용 가능하도록 상수로 등록
    
    * else를 없애기 위해 메소드로 빼는 리팩토링함
    
    * 입력값이 문자일때 정상적으로 동작하지 않고 예외처리되는지 확인
    
    * 익명 함수에서 하던일 의도를 좀더 알기쉽게 같은일을 하는 parseUnsignedInt 메소드 호출로 변경
    
    * 문자 분할후에 바로 리턴할수있도록 변경

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile("//(.)\n(.*)");
+    public static int splitAndSum(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        Matcher m = CUSTOM_DELIMITER.matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return sum(m.group(2).split(customDelimiter));
+        }
+        return sum(text.split(",|:"));
+
+    }
+
+    private static int sum(String[] tokens) {
+        return Arrays.stream(tokens)
+                .mapToInt(Integer::parseUnsignedInt)
+                .sum();
+    }
+
+}
