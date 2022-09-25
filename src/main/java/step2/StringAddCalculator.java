705e96d7f6a35f61121ddb174bbc194db5e4cb5b
owen.qq <owen.qq@kakaocorp.com>
2022-09-25 09:40:26 +0900

    step2 구현, input을 처리하는 별도 클래스 정의

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,32 +1,161 @@
 package step2;
 
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
-    private static final int EMPTY_VALUE = 0;
 
-    public static int splitAndSum(String input) {
-        if (isBlank(input)) {
-            return EMPTY_VALUE;
+    private static Pattern customDelimiterPattern = Pattern.compile("//(.)\n(.*)");
+
+    public static int splitAndAdd(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
         }
 
-        if (input.length() == 1) {
-            return Integer.parseInt(input);
+        return add(split(parseInputExpression(input)));
+    }
+
+    public static int add(String[] strNumbers) {
+        int sum = 0;
+
+        for (String strNumber : strNumbers) {
+            sum += parseInt(strNumber);
         }
 
-        String[] strNumbers = input.split(",");
-
-        return -1;
+        return sum;
     }
 
-    private static boolean isBlank(String value) {
-        return value == null || value.isEmpty() ? true : false;
+    public static int parseInt(String maybeNumberString) {
+        try {
+            int parsedValue = Integer.parseInt(maybeNumberString);
+
+            if (parsedValue < 0) {
+                throw new RuntimeException();
+            }
+
+            return parsedValue;
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
-    private static boolean hasCustomDelimiter(String value) {
-        return true;
+    public static int add(Integer[] numbers) {
+        int sum = 0;
+
+        for (Integer number : numbers) {
+            sum += number;
+        }
+
+        return sum;
     }
 
-    private static String extractDelimiter(String value) {
-        return "";
+    public static String[] split(Input input) {
+        return input.getInput().split(input.getDelimiter());
     }
 
+    public static String extractDelimiter(String input) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+
+        if (m.find()) {
+            return m.group(1);
+        }
+
+        return null;
+    }
+
+    public static boolean hasCustomDelimiter(String input) {
+        Matcher matcher = customDelimiterPattern.matcher(input);
+        return matcher.find();
+    }
+
+    public static String getCustomDelimiter(String input) {
+        if (hasCustomDelimiter(input)) {
+            Matcher matcher = customDelimiterPattern.matcher(input);
+            return matcher.find() ? matcher.group(1) : input;
+        }
+
+        return input;
+    }
+
+    public static String removeCustomDelimiter(String input) {
+        Matcher matcher = customDelimiterPattern.matcher(input);
+        if (matcher.find()) {
+            return matcher.group(2);
+        }
+
+        return input;
+    }
+
+    public static String getValidInput(String input) {
+        if (hasCustomDelimiter(input)) {
+            return removeCustomDelimiter(input);
+        }
+
+        return input;
+    }
+
+    public static Input parseInputExpression(String input) {
+        if (hasCustomDelimiter(input)) {
+            return new Input(input, getValidInput(input), getCustomDelimiter(input));
+        }
+
+        return Input.hasNoDelimiter(input);
+    }
+
+    public static class Input {
+        private static final String DEFAULT_DELIMITERS = ",|:";
+
+        private String originalInput;
+        private String delimiterRemovedInput;
+        private String customDelimiter;
+
+        private Input(String originalInput, String delimiterRemovedInput, String customDelimiter) {
+            this.originalInput = originalInput;
+            this.delimiterRemovedInput = delimiterRemovedInput;
+            this.customDelimiter = customDelimiter;
+        }
+
+        public static Input hasDelimiter(String input, String delimiterRemovedInput, String customDelimiter) {
+            return new Input(input, delimiterRemovedInput, customDelimiter);
+        }
+
+        public static Input hasNoDelimiter(String input) {
+            return new Input(input, input, "");
+        }
+
+        public boolean useCustomDelimiter() {
+            return customDelimiter != null && !customDelimiter.isBlank();
+        }
+
+        public String getDelimiter() {
+            return useCustomDelimiter() ? customDelimiter : DEFAULT_DELIMITERS;
+        }
+
+        public String getInput() {
+            return useCustomDelimiter() ? delimiterRemovedInput : originalInput;
+        }
+
+        public String[] split() {
+            return getInput().split(getDelimiter());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Input)) {
+                return false;
+            }
+            Input input = (Input) o;
+            return originalInput.equals(input.originalInput) && Objects.equals(delimiterRemovedInput,
+                    input.delimiterRemovedInput) && Objects.equals(customDelimiter, input.customDelimiter);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(originalInput, delimiterRemovedInput, customDelimiter);
+        }
+    }
 }
--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -7,50 +7,91 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StringAddCalculatorTest {
-
     @Test
-    public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
-        assertThat(result).isEqualTo(0);
+    void empty_or_null() {
+        assertThat(StringAddCalculator.splitAndAdd( "")).isEqualTo(0);
     }
 
     @Test
-    public void splitAndSum_숫자하나() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1");
-        assertThat(result).isEqualTo(1);
+    void 문자열분리() {
+        String input = "1,2";
+
+        assertThat(input.split(",")).contains("1", "2");
     }
 
     @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2");
+    void 문자열2개() {
+        String input = "1,2";
+        String[] strNumbers = input.split(",");
+
+        int result = StringAddCalculator.add(strNumbers);
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
-        assertThat(result).isEqualTo(6);
+    void 문자열3개() {
+        String input = "1,2,3";
+        String[] strNumbers = input.split(",");
+
+        assertThat(StringAddCalculator.add(strNumbers)).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_custom_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
-        assertThat(result).isEqualTo(6);
+    void split_쉼표와콜론을처리한다() {
+        String input = "1,2:3";
+        String[] strNumbers = StringAddCalculator.parseInputExpression(input).split();
+
+        assertThat(strNumbers).contains("1","2","3");
     }
 
     @Test
-    public void splitAndSum_negative() throws Exception {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
-                .isInstanceOf(RuntimeException.class);
+    void 커스텀구분자를추출한다() {
+        String input = "//a\n";
+
+        assertThat(StringAddCalculator.extractDelimiter(input)).isEqualTo("a");
     }
 
     @Test
-    public void isBlank_split으로체크한다() {
-        String input = "1";
+    void 커스텀구분자가들어있는경우() {
+        String input = "//;\n1;2;3";
+        String inputContainsNegative = input + ";-1";
 
-        Assertions.assertThat(input.split(","));
+        assertThat(StringAddCalculator.splitAndAdd(input)).isEqualTo(6);
+        assertThatThrownBy(() -> {
+            StringAddCalculator.splitAndAdd(inputContainsNegative);
+        }).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void parseInt_숫자이외의문자열은처리할수없다() {
+        String input = "zzz";
+
+        Assertions.assertThatThrownBy(() -> {
+            StringAddCalculator.parseInt(input);
+        }).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void parseInt_음수는처리할수없다() {
+        String input = "-1";
+
+        Assertions.assertThatThrownBy(() -> {
+            StringAddCalculator.parseInt(input);
+        }).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void hasCustomDelimiter_커스구분자를식별한다() {
+        String customDelimiter = "//;\n";
+        String input = customDelimiter + "1";
+
+        assertThat(StringAddCalculator.hasCustomDelimiter(input)).isTrue();
+    }
+
+    @Test
+    void getValidInput_커스텀구분자정의를제거한다() {
+        String customDelimiter = "//;\n";
+        String input = customDelimiter + "1;2;3";
+        assertThat(StringAddCalculator.getValidInput(input)).isEqualTo("1;2;3");
     }
 }
