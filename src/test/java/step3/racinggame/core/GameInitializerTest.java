d981012d4694fbb4fff3ef890629c36f1d65cf74
kingkk31 <kingkk31@naver.com>
2022-09-25 12:09:27 +0900

    test: GameInitializerTest 테스트 추가

---
+++ src/test/java/step3/racinggame/core/GameInitializerTest.java
@@ -0,0 +1,21 @@
+package step3.racinggame.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class GameInitializerTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5})
+    @DisplayName("주어진 값만큼 차를 생성하기")
+    void init_cars(int numOfCar) {
+        List<Car> cars = GameInitializer.initCars(numOfCar);
+
+        assertThat(cars).hasSize(numOfCar);
+    }
+
+}
