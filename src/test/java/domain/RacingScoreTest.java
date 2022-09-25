37a2809366acc59efc130511695e37a09d7a84c7
michol.lee <lovethefeel@gmail.com>
2022-09-26 00:36:56 +0900

    refactor(test): 코드리뷰 반영 - 테스트 코드 객체 명 추가 - MovingStrategy 상위로 이동하여 테스트 코드 수정

--- src/test/java/domain/RacingScoreTest.java
+++ src/test/java/domain/RacingScoreTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("레이싱 자동차 스코어 관련")
 class RacingScoreTest {
 
     @DisplayName("자동차 레이싱 이동 결과를 - 로 변환한다.")
@@ -12,7 +13,7 @@ class RacingScoreTest {
     void racing_convert() {
 
         final RacingCar racingCar = RacingCar.init();
-        racingCar.move(() -> true);
+        racingCar.move();
         final RacingCars racingCars = RacingCars.from(racingCar);
         final RacingScore racingScore = RacingScore.getInstance();
         racingScore.save(racingCars);
