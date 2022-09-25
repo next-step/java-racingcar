58100b4be910524072d44ba6da1052848101d7e0
bada.kang <kangbada0728@gmail.com>
2022-09-25 18:20:00 +0900

    refactor: 결과 출력만 담당하도록 변경
    콘솔 환경에서 웹이나 모바일 환경으로 바뀌어도 결과값을 출력할 수 있도록 ResultView#printResults에서 게임을 수행하는 기능을 제외하고 결과를 출력하는 기능만 넣음

--- src/main/java/carracing/step3/ResultView.java
+++ src/main/java/carracing/step3/ResultView.java
@@ -2,12 +2,10 @@ package carracing.step3;
 
 public class ResultView {
 
-    public void printResults(final int tryCount, final Cars cars) {
-        System.out.println("실행 결과");
-        for (int i = 0; i < tryCount; i++) {
-            cars.moveAllCarsRandomly();
-            System.out.print(cars.getAllCarsTrace());
+    public void printResults(String result, int playCount) {
+        if (playCount == 0) {
+            System.out.println("실행 결과");
         }
+        System.out.println(result);
     }
-
 }
