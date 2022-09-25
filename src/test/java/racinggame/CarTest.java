665861f846263784dd8bc5ed98700bcd55b0c9b9
gumsu <chavi9@naver.com>
2022-09-25 10:42:08 +0900

    refactor: move 메서드 이동

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -3,7 +3,10 @@ package racinggame;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
@@ -19,6 +22,30 @@ class CarTest {
         int status = car.getStatus();
 
         // then
-        Assertions.assertThat(status).isEqualTo(1);
+        assertThat(status).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 100})
+    @DisplayName("숫자가 4이상이면 이동할 수 있다.")
+    void 값이_4이상(int value) {
+        Car car = new Car();
+        int nowStatus = car.getStatus();
+
+        car.move(value);
+
+        assertThat(car.getStatus()).isGreaterThan(nowStatus);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("숫자가 4미만이면 이동할 수 없다.")
+    void 값이_4미만(int value) {
+        Car car = new Car();
+        int nowStatus = car.getStatus();
+
+        car.move(value);
+
+        assertThat(car.getStatus()).isGreaterThan(nowStatus);
     }
 }
