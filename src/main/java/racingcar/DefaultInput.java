a0dae2f4210326d01939e6ff5b06e88a7ae135ff
jungwoon727 <jungwoon727@gmail.com>
2022-09-25 22:28:31 +0900

    refactor: 콘솔에 출력되는 문장 상수화

--- src/main/java/racingcar/DefaultInput.java
+++ src/main/java/racingcar/DefaultInput.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class DefaultInput implements Input {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String CAR_QUANTITY_QUESTION = "경기에 출전할 자동차 수는 몇대인가요?";
+    private static final String MOVEMENT_COUNT_QUESTION = "자동차의 이동 횟수는 몇번인가요?";
 
     public int getCarQuantity() {
-        System.out.println("경기에 출전할 자동차 수는 몇대인가요?");
+        System.out.println(CAR_QUANTITY_QUESTION);
         return scanner.nextInt();
     }
 
     public int getMovementCount() {
-        System.out.println("자동차의 이동 횟수는 몇번인가요?");
+        System.out.println(MOVEMENT_COUNT_QUESTION);
         return scanner.nextInt();
     }
 
