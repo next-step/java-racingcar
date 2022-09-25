36c467c656ee5ee18a14103c53929955890e320e
gumsu <chavi9@naver.com>
2022-09-25 11:56:40 +0900

    test: RacingCar 테스트 수정

--- src/test/java/racinggame/RacingCarTest.java
+++ src/test/java/racinggame/RacingCarTest.java
@@ -1,13 +1,10 @@
 package racinggame;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class RacingCarTest {
 
@@ -21,20 +18,7 @@ class RacingCarTest {
         racingCar.racing();
 
         // then
-//        Assertions.assertThat(cars.get(0).getStatus()).isEqualTo();
-    }
-
-    @Test
-    void tryMove() {
-        // given
-        RacingCar racingCar = new RacingCar(3);
-        Car car = racingCar.getCars().get(0);
-
-        // when
-        racingCar.tryMove(car, 5);
-
-        // then
-        assertThat(car.getStatus()).isEqualTo(1);
+        assertThat(cars.get(0).getStatus()).isLessThanOrEqualTo(1);
     }
 
 }
