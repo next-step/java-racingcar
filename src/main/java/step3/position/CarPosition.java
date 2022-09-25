86621e4da9dce882d6b21c14edd83f310635ae95
kureungkureung <terry9611@naver.com>
2022-09-25 18:32:29 +0900

    feat(CarPosition): 한 칸 이동 기능

---
+++ src/main/java/step3/position/CarPosition.java
@@ -0,0 +1,33 @@
+package step3.position;
+
+import java.util.Objects;
+
+public final class CarPosition implements Position {
+
+    private static final int DISTANCE = 1;
+
+    private final int position;
+
+    public CarPosition(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public Position movedPosition() {
+        return new CarPosition(position + DISTANCE);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarPosition that = (CarPosition) o;
+        return position == that.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+
+}
---
+++ src/main/java/step3/position/Position.java
@@ -0,0 +1,8 @@
+package step3.position;
+
+@FunctionalInterface
+public interface Position {
+
+    Position movedPosition();
+
+}
