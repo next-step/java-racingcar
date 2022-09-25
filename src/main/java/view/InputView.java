b0f3f7ba898e3412bcbefca7a1753de9a67d1fd3
michol.lee <lovethefeel@gmail.com>
2022-09-26 01:08:11 +0900

    refactor: 상수명 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputView() {}
 
     public static String carCount() {
 
         System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.next();
+        return SCANNER.next();
     }
 
     public static String playCount() {
 
         System.out.println("시도할 회수는 몇 회 인가요?");
-        return scanner.next();
+        return SCANNER.next();
     }
 }
