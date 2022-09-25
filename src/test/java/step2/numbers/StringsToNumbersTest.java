97f1556968069b543d769d3ec7e06d355947621b
kureungkureung <terry9611@naver.com>
2022-09-26 00:39:43 +0900

    test(StringsToNumbersTest): 입력값 검증 기능

--- src/test/java/step2/numbers/StringsToNumbersTest.java
+++ src/test/java/step2/numbers/StringsToNumbersTest.java
@@ -3,10 +3,14 @@ package step2.numbers;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import step2.parsed.Parsed;
 import step2.parsed.ParsedStringByDefaultDelimiter;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 class StringsToNumbersTest {
 
@@ -20,6 +24,25 @@ class StringsToNumbersTest {
         List<Integer> numbers = sut.numbers();
 
         Assertions.assertThat(numbers).containsExactly(1, 2, 3);
+    }
+
+
+    @MethodSource
+    @ParameterizedTest
+    @DisplayName("문자열이 숫자가 아니거나 음수일 경우 예외가 발생한다")
+    void b(Parsed parsed) {
+        Numbers sut = new StringsToNumbers(parsed);
+
+        Assertions.assertThatThrownBy(() -> sut.numbers())
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("숫자가 아니거나 음수입니다.");
+    }
+
+    private static Stream<Arguments> b() {
+        return Stream.of(
+                Arguments.of(new Parsed.Fake(List.of("a"))),
+                Arguments.of(new Parsed.Fake(List.of("-1")))
+        );
     }
 
 }
