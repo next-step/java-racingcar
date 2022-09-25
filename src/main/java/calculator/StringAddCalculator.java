939ef050f4c0a3b5eb1de46d91ffd4836ff7e47f
leejihyung <nave369@naver.com>
2022-09-25 19:55:37 +0900

    라이브강의까지 커밋

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class Cal {
+    public static int cal(String s) {
+        return 0;
+    }
+}
---
+++ src/test/java/calculator/PositiveTest.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PositiveTest {
+    @Test
+    void create(){
+        assertThat(new Positive("1")).isEqualTo(new Positive(1));
+    }
+
+    @Test
+    void 덧셈(){
+        Positive positive = new Positive("1");
+        Positive result = positive.plus(2);
+        assertThat(result).isEqualTo(new Positive(3));
+    }
+
+    @Test
+    void 음수(){
+        assertThatThrownBy( ()->{
+            new Positive("-1");
+        }).isInstanceOf(RuntimeException.class);
+    }
+}
