f343440434c79ed635cf40bedfc03098c989372e
jwoh <jwoh@wemakeprice.com>
2022-09-26 22:58:10 +0900

    feat : race 실행 클래스

---
+++ src/main/java/step3/Race.java
@@ -0,0 +1,24 @@
+package step3;
+
+import step3.model.Cars;
+import step3.view.InputView;
+import step3.view.ResultView;
+
+
+public class Race
+{
+    public static void main(String[] args)
+    {
+        Race.start();
+    }
+
+    public static void start()
+    {
+        InputView inputView = new InputView();
+        Cars cars = new Cars(inputView.carNumber(), inputView.tryNumber());
+        ResultView resultView = new ResultView(cars);
+
+        resultView.draw();
+    }
+
+}
