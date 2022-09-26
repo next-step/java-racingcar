2bbf8428898dabe39d87635b9b8d8105167ccb88
o3ozzvb <ou1112@naver.com>
2022-09-26 18:54:57 +0900

    refactor : 질문 출력 함수와 입력값 입력받는 함수 병합

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -3,19 +3,12 @@ package step3;
 import java.util.Scanner;
 
 public class InputView {
-    private final String Q_CAR_NUMBER = "자동차 대수는 몇 대 인가요?";
-    private final String Q_TRY_TIMES = "시도할 회수는 몇 회 인가요?";
+    public static final String Q_CAR_NUMBER = "자동차 대수는 몇 대 인가요?";
+    public static final String Q_TRY_TIMES = "시도할 회수는 몇 회 인가요?";
     Scanner scanner = new Scanner(System.in);
 
-    public void printQ_CarNumber() {
-        System.out.println(Q_CAR_NUMBER);
-    }
-
-    public void printQ_TryTimes() {
-        System.out.println(Q_TRY_TIMES);
-    }
-
-    public int inputNumber() {
+    public int printQuestion(String question) {
+        System.out.println(question);
         return scanner.nextInt();
     }
 
