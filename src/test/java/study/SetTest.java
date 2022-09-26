d9f6fb9645fd0f94c7a88b1d829ab8ad48670419
ChaeHyeon <86863306+cohys7@users.noreply.github.com>
2022-09-26 17:03:23 +0900

    리뷰 반영 (#3628)
    - Collections.addAll 사용
    - 테스트 메서드명 변경 및 displayName 추가
    - 중복되는 테스트 ParameterizedTest로 변경
    - 테스트케이스 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,42 +1,45 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Set Collection에 대한 학습 테스트")
 class SetTest {
     private Set<Integer> numbers;
 
     @BeforeEach
     void setUp() {
         numbers = new HashSet<>();
-        numbers.add(1);
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
+        Collections.addAll(numbers, 1, 1, 2, 3);
     }
 
     @Test
-    void size() {
+    @DisplayName("Set은 중복된 값을 가지지 않는다.")
+    void hasUniqueElements() {
         assertThat(numbers).hasSize(3);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "Set에 {0}이 존재한다.")
     @ValueSource(ints = {1, 2, 3})
-    void contains(int number) {
+    @DisplayName("Set에 1, 2, 3이 존재한다.")
+    void containsInitializedValue(int number) {
         assertThat(numbers).contains(number);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "Set에 {0}가 존재한다 : {1}")
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    void contains(int number, boolean result) {
+    @DisplayName("Set에 어떤 값이 존재하는지 확인한다.")
+    void containsOnlyInitializedValue(int number, boolean result) {
         assertThat(numbers.contains(number)).isEqualTo(result);
     }
 }
