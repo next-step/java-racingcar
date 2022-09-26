670d18cb0bf10dddb3588ff41d912cbc325f8074
KimWanJoo <wjwan0915@gmail.com>
2022-09-26 19:01:37 +0900

    :recycle: refactor(controller 매개변수 수정)

--- src/main/java/racing/application/controller/RacingController.java
+++ src/main/java/racing/application/controller/RacingController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import racing.application.view.InputView;
 import racing.application.view.OutputView;
 import racing.application.view.Score;
-import racing.application.view.Winner;
 import racing.domain.car.CarFactory;
 import racing.domain.car.Labs;
 import racing.domain.car.Track;
@@ -23,7 +22,7 @@ public class RacingController {
         int moveCount = inputView.moveCount();
         Track track = new Track(new History(new ArrayList<>()), new CarFactory(inputNames).createCar());
         Track racingTrack = track.racing(new Labs(moveCount));
-        OutputView outputView = new OutputView(new Score(racingTrack.getHistory(), new Winner(racingTrack.winner())));
+        OutputView outputView = new OutputView(new Score(racingTrack));
         outputView.print();
     }
 }
