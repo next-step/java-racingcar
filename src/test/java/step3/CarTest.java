877aeb069561972f66dec0d2cbf2fc9ef4d7ca35
wugatama <dnr7410@naver.com>
2022-09-26 21:43:32 +0900

    test: Car 전진 후 정지 확인 테스트

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -10,7 +10,16 @@ public class CarTest {
 	@DisplayName("4이상 일때 전진 확인 테스트")
 	void checkCarMoving() {
 		Car car = new Car();
-		String actual = Racing.move(4);
-		assertThat(car.move(actual)).isEqualTo("-");
+		car.move(Racing.move(4));
+		assertThat(car.getStatus()).isEqualTo("-");
+	}
+
+	@Test
+	@DisplayName("4이상 일때 전진 다음 정지 테스트")
+	void checkCarMovingAndStopMoving() {
+		Car car = new Car();
+		car.move(Racing.move(4));
+		car.move(Racing.move(3));
+		assertThat(car.getStatus()).isEqualTo("-");
 	}
 }
