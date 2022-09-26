65ce5feb488d112cd5b3b47cac710b087d9afea4
김찬수 <cskim96@crscube.co.kr>
2022-09-26 14:48:17 +0900

    :recycle: refactor(service) : Position 상수를 사용핟록 수정

--- src/main/java/service/impl/MultipleWinnerStrategy.java
+++ src/main/java/service/impl/MultipleWinnerStrategy.java
@@ -9,8 +9,6 @@ import java.util.stream.Collectors;
 
 public class MultipleWinnerStrategy implements WinnerStrategy {
 
-    private static final CarPosition DEFAULT_CAR_POSITION = new CarPosition(0);
-
     @Override
     public List<Car> pickWinner(List<Car> candidates) {
         CarPosition maxPosition = getMaxPosition(candidates);
@@ -18,6 +16,6 @@ public class MultipleWinnerStrategy implements WinnerStrategy {
     }
 
     private CarPosition getMaxPosition(List<Car> candidates) {
-        return candidates.stream().map(Car::getCarPosition).max(CarPosition::compareTo).orElse(DEFAULT_CAR_POSITION);
+        return candidates.stream().map(Car::getCarPosition).max(CarPosition::compareTo).orElse(CarPosition.ZERO);
     }
 }
