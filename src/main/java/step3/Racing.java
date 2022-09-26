a6645aa13e5672630600d9bbf0f1df9c5d316678
wugatama <dnr7410@naver.com>
2022-09-26 20:19:43 +0900

    feat: 경기 참가수 또는 경기 횟수 음수 또는 문자 예외처리 구현

--- src/main/java/step3/Racing.java
+++ src/main/java/step3/Racing.java
@@ -4,4 +4,14 @@ public class Racing {
 	public static int participate(String input) {
 		return Integer.parseInt(input);
 	}
+
+	public static void checkInput(String input) {
+		if (Integer.parseInt(input) < 0 || validateCharacter(input)) {
+			throw new RuntimeException();
+		}
+	}
+
+	private static boolean validateCharacter(String input) {
+		return 89 < input.charAt(0) && input.charAt(0) < 80;
+	}
 }
