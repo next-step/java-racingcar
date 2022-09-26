73f83065c6aca116302b10ea57c3df6addc77f55
wugatama <dnr7410@naver.com>
2022-09-27 00:56:23 +0900

    feat: Print 용 클래스 구현

--- src/main/java/step3/Print.java
+++ src/main/java/step3/Print.java
@@ -1,2 +1,19 @@
-package step3;public class Print {
+package step3;
+
+public class Print {
+	public static void howManyCars() {
+		System.out.println("자동차 대수는 몇 대 인가요?");
+	}
+
+	public static void howManyMatches() {
+		System.out.println("시도할 회수는 몇 회 인가요?");
+	}
+
+	public static void resultOfRound(String status) {
+		System.out.println(status);
+	}
+
+	public static void newLine() {
+		System.out.println();
+	}
 }
