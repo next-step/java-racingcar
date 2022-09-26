dc5796d911b0f5db2d6a0971299b6de1cfdb2b92
wugatama <dnr7410@naver.com>
2022-09-26 20:50:36 +0900

    refactor: RacingTest 클래스 이름 변경

--- src/test/java/step3/RacingTest.java
+++ src/test/java/step3/RacingTest.java
@@ -2,15 +2,12 @@ package step3;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-public class RacingCarTest {
+public class RacingTest {
 	@Test
 	@DisplayName("레이싱 참가자 수 테스트")
 	void participantOfRacing() throws Exception {
