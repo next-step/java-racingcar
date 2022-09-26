5d9464f0346fa39651c33c003178d126c7228601
yunhwan <pyh8618@gmail.com>
2022-09-27 01:06:28 +0900

    test : @BeforeEach 적용

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,14 +1,19 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import racingcar.util.RandomUtil;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
+    Car car;
+    @BeforeEach
+    void setUp() {
+        car = new Car();
+    }
     @Test
     public void 조건을_만족할때_position_증가() {
-        Car car = new Car();
         int num = RandomUtil.generateRandomValue(ForwardCondition.FORWARD_CONDITION_LOW_BOUND, ForwardCondition.FORWARD_CONDITION_HIGH_BOUND);
         car.forwardCarByCondition(num);
         assertThat(car.getPosition()).isEqualTo(1);
@@ -16,7 +21,6 @@ class CarTest {
 
     @Test
     public void 조건을_만족하지_않을_때_포지션변화() {
-        Car car = new Car();
         int num = RandomUtil.generateRandomValue(0, ForwardCondition.FORWARD_CONDITION_LOW_BOUND);
         car.forwardCarByCondition(num);
         assertThat(car.getPosition()).isEqualTo(0);
@@ -24,7 +28,6 @@ class CarTest {
 
     @Test
     public void 조건을_만족하지_않을_때_포지션변화2() {
-        Car car = new Car();
         int num = RandomUtil.generateRandomValue(ForwardCondition.FORWARD_CONDITION_HIGH_BOUND + 1, ForwardCondition.HIGHEST_BOUND + 1);
         car.forwardCarByCondition(num);
         assertThat(car.getPosition()).isEqualTo(0);
