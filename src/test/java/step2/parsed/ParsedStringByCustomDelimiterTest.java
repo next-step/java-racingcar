deb3ace7a27552aa28bcd0c535fbe5e4e6bbd8ab
kureungkureung <terry9611@naver.com>
2022-09-26 21:50:50 +0900

    test(ParsedStringByCustomDelimiterTest): 문자열 유효성 검증 기능

--- src/test/java/step2/parsed/ParsedStringByCustomDelimiterTest.java
+++ src/test/java/step2/parsed/ParsedStringByCustomDelimiterTest.java
@@ -2,6 +2,7 @@ package step2.parsed;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -29,5 +30,16 @@ class ParsedStringByCustomDelimiterTest {
         );
     }
 
+    @Test
+    @DisplayName("//.\n 접두사가 없을 경우 예외가 발생한다.")
+    void b() {
+        String stringToBeParsed = "+";
 
+        Parsed sut = new ParsedStringByCustomDelimiter(stringToBeParsed);
+
+        Assertions.assertThatThrownBy(() -> sut.parsedValue())
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("유효한 문자열이 아닙니다.");
+    }
+    
 }
