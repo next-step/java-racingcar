34a74aeca8d7788e1f30e7365294a0d9dcca304d
Myunghwan Kim <samkim2626@gmail.com>
2022-09-26 21:56:51 +0900

    feat: 자동차 대수, 시도할 회수 입력 기능 구현

---
+++ src/main/java/racingcar/step3/GameAttribute.java
@@ -0,0 +1,12 @@
+package racingcar.step3;
+
+public class GameAttribute {
+
+	private final int carCount;
+	private final int tryCount;
+
+	public GameAttribute(int carCount, int tryCount) {
+		this.carCount = carCount;
+		this.tryCount = tryCount;
+	}
+}
---
+++ src/main/java/racingcar/step3/RacingCarApp.java
@@ -0,0 +1,9 @@
+package racingcar.step3;
+
+public class RacingCarApp {
+
+	public static void main(String[] args) {
+		InputView inputView = new InputView();
+		GameAttribute gameAttribute = inputView.getGameAttribute();
+	}
+}
