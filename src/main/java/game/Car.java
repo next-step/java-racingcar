605b0fd5b6a96e5f4369a3a7ab58d1fbcfb7c924
Yunsik-Choi <choiys0311@gmail.com>
2022-09-26 21:34:44 +0900

    [feat] RacingGame start

--- src/main/java/game/Car.java
+++ src/main/java/game/Car.java
@@ -1,19 +1,29 @@
 package game;
 
+
 public class Car {
 
     private int location;
+
     public int location() {
         return location;
     }
 
     public void changeLocation(int given) {
-        if(isForward(given)){
-            location+=1;
+        if (isForward(given)) {
+            location += 1;
         }
     }
 
     private static boolean isForward(int given) {
         return given >= 4;
+    }
+
+    public void showLocation() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < location(); i++) {
+            stringBuilder.append("-");
+        }
+        System.out.println(stringBuilder);
     }
 }
--- src/main/java/game/RacingGame.java
+++ src/main/java/game/RacingGame.java
@@ -1,9 +1,8 @@
 package game;
 
-import java.io.Console;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class RacingGame {
@@ -12,12 +11,26 @@ public class RacingGame {
     private Integer round;
     private final static String INPUT_CAR_GUIDE = "자동차 대수는 몇 대 인가요?";
     private final static String INPUT_ROUND_GUIDE = "시도할 회수는 몇 회 인가요?";
+    private final static String RESULT_GUIDE = "실행 결과";
+    private final static int CHANGE_LOCATION_BOUND = 10;
 
-    public static RacingGame start(){
+    public static void start() {
         RacingGame racingGame = new RacingGame();
         racingGame.inputNumberOfCar();
         racingGame.inputNumberOfRound();
-        return racingGame;
+        System.out.println(RESULT_GUIDE);
+        for (int i = 0; i < racingGame.round(); i++) {
+            racingGame.progressRound(racingGame);
+        }
+    }
+
+    private void progressRound(RacingGame racingGame) {
+        for (Car car : racingGame.cars()) {
+            Random random = new Random();
+            car.changeLocation(random.nextInt(CHANGE_LOCATION_BOUND));
+            car.showLocation();
+        }
+        System.out.println();
     }
 
     public void inputNumberOfCar() {
@@ -42,7 +55,7 @@ public class RacingGame {
             int result = sc.nextInt();
             checkPositiveNumber(result);
             return result;
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException();
         }
