dc3e92e82aeeb8cf2f99a452f7e44445e7576a1b
myabcc17 <myabcc17@naver.com>
2022-09-26 18:00:17 +0900

    feat(implement): add car class

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -1,11 +1,25 @@
 package step3;
 
 public class Car {
+    private static final int MIN_NUM_TO_MOVE = 4;
+    private final int position;
+
+    public Car() {
+        this.position = 0;
+    }
+
+    private Car(int position) {
+        this.position = position;
+    }
+
     public Car move(int number) {
-        return null;
+        if (number >= MIN_NUM_TO_MOVE) {
+            return new Car(this.position + 1);
+        }
+        return this;
     }
 
     public int getPosition() {
-        return 0;
+        return this.position;
     }
 }
