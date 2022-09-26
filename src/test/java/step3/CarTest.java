134845c4d9e84b91ca6beff16d917ec4801394ec
wugatama <dnr7410@naver.com>
2022-09-27 01:21:19 +0900

    refactor: CarTest beforeEach 추가

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -2,14 +2,21 @@ package step3;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
+	private Car car;
+
+	@BeforeEach
+	void setUp() {
+		car = new Car();
+	}
+
 	@Test
 	@DisplayName("4이상 일때 전진 확인 테스트")
 	void checkCarMoving() {
-		Car car = new Car();
 		car.move(Racing.move(4));
 		assertThat(car.getStatus()).isEqualTo("-");
 	}
@@ -17,7 +24,6 @@ public class CarTest {
 	@Test
 	@DisplayName("4이상 일때 전진 다음 정지 테스트")
 	void checkCarMovingAndStopMoving() {
-		Car car = new Car();
 		car.move(Racing.move(4));
 		car.move(Racing.move(3));
 		assertThat(car.getStatus()).isEqualTo("-");
