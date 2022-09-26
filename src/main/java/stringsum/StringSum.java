f8f75b46bfbe65b1ab368702fd49b60be92343d9
scoopsoup <rnrdud46@nate.com>
2022-09-27 01:04:49 +0900

    chore : 컨벤션 적용하기

--- src/main/java/stringsum/StringSum.java
+++ src/main/java/stringsum/StringSum.java
@@ -5,37 +5,36 @@ import java.util.regex.Pattern;
 
 public class StringSum {
 
-    public int basicSum(String testData){
-        if(testData == null || testData.isBlank() || testData.trim().isEmpty()){
+    public int basicSum(String testData) {
+        if (testData == null || testData.isBlank() || testData.trim().isEmpty()) {
             return 0;
         }
         String[] splitResult = testData.split(",|:");
         int sum = 0;
-        for(String td : splitResult){
+        for (String td : splitResult) {
             sum += Integer.parseInt(td);
         }
         return sum;
     }
 
-    public String customChar(String testData){
-        if(testData == null || testData.isBlank() || testData.trim().isEmpty()){
+    public String customChar(String testData) {
+        if (testData == null || testData.isBlank() || testData.trim().isEmpty()) {
             return "";
         }
         Pattern pattern = Pattern.compile("[/][/](.*?)[\\n]");
         Matcher matcher = pattern.matcher(testData);
         String result = "";
-        if(matcher.find() && matcher.start() == 0){
-            return testData.substring(2, matcher.end()-1); // 특정 단어 사이의 값 추출
+        if (matcher.find() && matcher.start() == 0) {
+            return testData.substring(2, matcher.end() - 1); // 특정 단어 사이의 값 추출
         }
         return result;
     }
 
-    public String[] splitStr(String testData){
+    public String[] splitStr(String testData) {
         String custom = customChar(testData);
-        if(custom == "" || custom.isBlank() || custom.trim().isEmpty()){
+        if (custom == "" || custom.isBlank() || custom.trim().isEmpty()) {
             return custom.split(",|:");
-        }
-        else if(custom.equals("+")){
+        } else if (custom.equals("+")) {
             custom = "\\+|,|:";
             return testData.substring(4).split(custom);
         }
@@ -43,22 +42,22 @@ public class StringSum {
         return testData.substring(4).split(custom);
     }
 
-    public int customSum(String testData){
+    public int customSum(String testData) {
         String[] splitResult = splitStr(testData);
-        if(splitResult[0] == ""){
+        if (splitResult[0] == "") {
             return 0;
         }
         int sum = 0;
-        for(String str : splitResult){
+        for (String str : splitResult) {
             sum += Integer.parseInt(str);
         }
         return sum;
     }
 
-    public int calculator(){
+    public int calculator() {
         String[] splitResult = splitStr("1:2:3,5,7,6,d");
         int sum = 0;
-        for(String td : splitResult){
+        for (String td : splitResult) {
             sum += Integer.parseInt(td);
         }
         return sum;
--- src/test/java/stringsum/StringSumTest.java
+++ src/test/java/stringsum/StringSumTest.java
@@ -11,61 +11,61 @@ public class StringSumTest {
 
     @DisplayName("요구사항1_기본구분자로 분리된 숫자의 합 반환하기")
     @Test
-    void basicSumTest(){
+    void basicSumTest() {
         assertThat(strSum.basicSum("1:2,3:4,5,7")).isEqualTo(22);
     }
 
     @DisplayName("Basic 빈문자일 때 0")
     @Test
-    void basicEmptyTset(){
+    void basicEmptyTset() {
         assertThat(strSum.basicSum("")).isEqualTo(0);
     }
 
     @DisplayName("Basic null일 때 0")
     @Test
-    void basicNullTset(){
+    void basicNullTset() {
         assertThat(strSum.basicSum(null)).isEqualTo(0);
     }
 
     @DisplayName("요구사항2_커스텀 구분자 찾기")
     @Test
-    void customCharTest(){
+    void customCharTest() {
         assertThat(strSum.customChar("//]\n1-23:5,67")).isEqualTo("]");
     }
 
     @DisplayName("요구사항2_커스텀 구분자 제거 후 숫자찾기")
     @Test
-    void splitStrTest(){
-        assertThat(strSum.splitStr("//-\n1-23:5,67")).containsExactly("1","23","5","67");
+    void splitStrTest() {
+        assertThat(strSum.splitStr("//-\n1-23:5,67")).containsExactly("1", "23", "5", "67");
     }
 
     @DisplayName("요구사항2_합구하기")
     @Test
-    void customSumTest(){
+    void customSumTest() {
         assertThat(strSum.customSum("//-\n1-23:5,67")).isEqualTo(96);
     }
 
     @DisplayName("null값이 들어오는 경우")
     @Test
-    void emptyValueTest(){
+    void emptyValueTest() {
         assertThat(strSum.customSum((""))).isEqualTo(0);
     }
 
     @DisplayName("빈값이 들어오는 경우")
     @Test
-    void nullValueTest(){
+    void nullValueTest() {
         assertThat(strSum.customSum((null))).isEqualTo(0);
     }
 
     @DisplayName("숫자하나만 들어가는 경우")
     @Test
-    void oneNum(){
+    void oneNum() {
         assertThat(strSum.customSum("0")).isEqualTo(0);
     }
 
     @DisplayName("요구사항3_계산기 예외처리")
     @Test
-    void calculatorTest(){
+    void calculatorTest() {
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> {
                     assertThat(strSum.calculator()).isEqualTo(24);
