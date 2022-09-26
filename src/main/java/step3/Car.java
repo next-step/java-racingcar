27e8f7c9a0c14c85e2248e358f1b6ba5cf7b6bac
wugatama <dnr7410@naver.com>
2022-09-26 20:56:10 +0900

    feat: 움직임 상태 인스턴스 변수로 구현, 전진 기능 구현

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -1,4 +1,13 @@
 package step3;
 
 public class Car {
+	private String status;
+
+	public Car() {
+		this.status = "";
+	}
+
+	public String move(String advanced) {
+		return this.status += advanced;
+	}
 }
