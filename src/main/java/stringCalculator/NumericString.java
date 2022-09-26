cbed35489d9a3815e5974735f83521e5c8b0fcb4
jiokim <33409242+jiokim@users.noreply.github.com>
2022-09-26 21:00:50 +0900

    chore: 키워드 정렬, 메소드명 수정

--- src/main/java/stringCalculator/NumericString.java
+++ src/main/java/stringCalculator/NumericString.java
@@ -7,8 +7,8 @@ import java.util.stream.IntStream;
 public class NumericString {
 
     private String numericString;
-    private final static String COMMA_OR_COLON_DELIMITER = ",|:";
-    private final static String CUSTOM_DELIMITER = "//(.)\\n(.*)";
+    private static final String COMMA_OR_COLON_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//(.)\\n(.*)";
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER);
     private static final String POSITIVE_INTEGER_REGEX = "[0-9]+";
     private static final Pattern POSITIVE_INTEGER_PATTERN = Pattern.compile(POSITIVE_INTEGER_REGEX);
@@ -45,7 +45,7 @@ public class NumericString {
         return ints;
     }
 
-    private int parseInt(String number) {
+    private int parsePositiveInt(String number) {
         isPositive(number);
         return Integer.parseInt(number);
     }
