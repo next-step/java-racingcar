b0e240160d574fb4729f1a5ff93e69801b7f1e38
swsong <ssw890720@gmail.com>
2022-09-26 22:26:15 +0900

    refactor: 랜덤유틸 테스트 케이스 수정

--- src/test/java/racinggame/util/RandomUtilsTest.java
+++ src/test/java/racinggame/util/RandomUtilsTest.java
@@ -13,16 +13,8 @@ class RandomUtilsTest {
     @Disabled("싪패 가능성이 있는 테스트 이므로 로컬에서 1회성 실행만 할것")
     @Test
     void 랜덤_숫자가_0_9_사이로_생성된다() {
-        //given
-        Set<Integer> set = new HashSet<>();
-
-        for (int i = 0; i < 1000; i++) {
-            set.add(RandomUtils.randomNumber(10));
-        }
-
-        assertThat(set)
-                .containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-                .doesNotContain(11, 12, 13, 14, 15, 16, 17);
+        int number = RandomUtils.randomNumber(10);
+        assertThat(number).isGreaterThan(0).isLessThan(9);
     }
 
 }
