e22b7b03317d012cd08c0535499f40db5681b9be
hiblue02 <hiblue02@gmail.com>
2022-09-26 22:31:40 +0900

    refactor(Car, Winner): 자동차 이동거리 그래프를 Car에서 그려주도록 수정, Winner가 우승자들의 이름을 보여주도록 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,14 +4,11 @@ import racingcar.domain.Car;
 import racingcar.domain.Winners;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class OutputView {
 
     private static final String TITLE_RACING_RESULT = "실행 결과";
     private static final String GRAPH_FIGURE = "-";
-    private static final String FORMAT_PRINT_RACING = "%s : %s \n";
 
     private OutputView() {
 
@@ -22,21 +19,11 @@ public class OutputView {
     }
 
     public static void printRacing(List<Car> cars) {
-        cars.forEach(car -> System.out.printf(FORMAT_PRINT_RACING, car.showName(), printGraph(car)));
+        cars.forEach(car -> System.out.println(car.showGraph(GRAPH_FIGURE)));
         System.out.println();
     }
 
-    private static String printGraph(Car car) {
-        StringBuilder graph = new StringBuilder();
-        IntStream.range(0, car.showDistance()).forEach(a -> graph.append(GRAPH_FIGURE));
-        return graph.toString();
-    }
-
     public static void printWinners(Winners winners) {
-        String winnerNames = winners.showWinners().stream()
-                .map(Car::showName)
-                .collect(Collectors.joining(","));
-
-        System.out.println(winnerNames+ "가 최종 우승했습니다.");
+        System.out.println(winners.showWinnerNames() + "가 최종 우승했습니다.");
     }
 }
