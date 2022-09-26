62ce4761d86086ac3bc05e3e586c34cccf947b6b
KimWanJoo <wjwan0915@gmail.com>
2022-09-26 18:55:27 +0900

    :recycle: refactor(RacingController 의존성 변경 및 Serivce 삭제)

--- src/main/java/racing/application/controller/RacingController.java
+++ src/main/java/racing/application/controller/RacingController.java
@@ -1,34 +1,29 @@
 package racing.application.controller;
 
-import racing.application.service.RacingService;
+import java.util.ArrayList;
+import racing.application.view.InputView;
 import racing.application.view.OutputView;
-import racing.application.view.View;
-import racing.domain.car.CarRace;
-import racing.domain.car.exception.MakeCarException;
-import racing.domain.car.exception.UnderOfZeroException;
+import racing.application.view.Score;
+import racing.application.view.Winner;
+import racing.domain.car.CarFactory;
+import racing.domain.car.Labs;
+import racing.domain.car.Track;
+import racing.domain.score.History;
 
 public class RacingController {
 
-    private final RacingService racingService;
+    private final InputView inputView;
 
-    public RacingController(RacingService racingService) {
-        this.racingService = racingService;
+    public RacingController(InputView inputView) {
+        this.inputView = inputView;
     }
 
-    public View racingGame(String names, int moveCount) {
-        validation(names, moveCount);
-        CarRace carRace = racingService.makeCars(names);
-
-        return new OutputView(racingService.scoreBoard(carRace, moveCount));
-    }
-
-    private void validation(String names, int moveCount) {
-        if (names == null || names.equals("") || names.isBlank()) {
-            throw new MakeCarException("자동차는 1대 이상 입력 해야 합니다.");
-        }
-
-        if (moveCount < 0) {
-            throw new UnderOfZeroException("0번 이상 움직여야 합니다.");
-        }
+    public void play() {
+        String inputNames = inputView.inputNames();
+        int moveCount = inputView.moveCount();
+        Track track = new Track(new History(new ArrayList<>()), new CarFactory(inputNames).createCar());
+        Track racingTrack = track.racing(new Labs(moveCount));
+        OutputView outputView = new OutputView(new Score(racingTrack.getHistory(), new Winner(racingTrack.winner())));
+        outputView.print();
     }
 }
