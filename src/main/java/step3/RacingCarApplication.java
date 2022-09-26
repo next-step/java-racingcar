c316fe62350cea5db9d1348c891c1682ac3eb743
Greg.Study <greg0917@naver.com>
2022-09-27 02:12:38 +0900

    입력기능 구현

---
+++ src/main/java/step3/RacingCarApplication.java
@@ -0,0 +1,10 @@
+package step3;
+
+public class RacingCarApplication {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        int numberOfCar = inputView.numberOfCar();
+        int count = inputView.count();
+    }
+}
---
+++ src/test/java/step3/CarRacingTest.java
@@ -0,0 +1,11 @@
+package step3;
+
+import org.junit.jupiter.api.Test;
+
+public class CarRacingTest {
+
+    @Test
+    public void car() {
+
+    }
+}
