4a353f45d7de005a3f92ad89f29784d357f35e5b
gkfgran5037 <gkfgran5037@naver.com>
2022-09-27 16:49:18 +0900

    Refactoring splitAndSum()
    미션 1 작성 함수 리펙토링 : 기능에 따라 isBlank(), split(), isBlank()로 메서드 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,14 +3,25 @@ package calculator;
 public class StringAddCalculator {
     public static String SEPARATOR = ",|:";
     public static int splitAndSum(String text) {
-        if (text == null || text.isBlank()) {
+        if (isBlank(text)) {
             return 0;
         }
-        String[] numbers = text.split(SEPARATOR);
+        return sum(split(text));
+    }
+
+    private static int sum(String[] numbers) {
         int sum = 0;
         for (String number : numbers) {
             sum += Integer.parseInt(number);
         }
         return sum;
+    }
+
+    private static String[] split(String text) {
+        return text.split(SEPARATOR);
+    }
+
+    private static boolean isBlank(String text) {
+        return text == null || text.isBlank();
     }
 }
