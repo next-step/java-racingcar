d9d6d9cc242d556dfe9ddfc03316725f2333639e
OhSeungCheol <sc.oh131@gmail.com>
2022-09-27 13:30:13 +0900

    [자동차 경주 3단계] refactor: 자동차 경주 클래스가 생성될 때 멤버 변수를 초기화 하도록 수정
    https://github.com/next-step/java-racingcar/pull/3670

--- src/main/java/step3/CarRacingGame.java
+++ src/main/java/step3/CarRacingGame.java
@@ -4,35 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarRacingGame {
-    private Integer numberOfCar;
-    private Integer tryCount;
-    private List<Car> carEntryList;
+    private final Integer tryCount;
+    private final List<Car> carEntryList = new ArrayList<>();
 
     CarRacingGame(int numberOfCar, int tryCount) {
-        this.numberOfCar = numberOfCar;
         this.tryCount = tryCount;
+        for (int index = 0; index < numberOfCar; index++) {
+            this.carEntryList.add(new Car(index));
+        }
     }
 
     // 게임 시작
     public void doGame() {
-        readyToCarEntry();
         for (int index = 0; index < this.tryCount; index++) {
             doRacing();
-            // 각 게임 회차마다 현황 출력
-            ResultView.displayRacingState(this.carEntryList);
         }
     }
 
     private void doRacing() {
         this.carEntryList
                 .forEach(Car::run);
-    }
-
-    private void readyToCarEntry() {
-        this.carEntryList = new ArrayList<>();
-        for (int index = 0; index < numberOfCar; index++) {
-            this.carEntryList.add(new Car(index));
-        }
     }
 
 }
