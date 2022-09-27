6a49640bbef0fa0ab73e0d15eed80aca95fdb130
swsong <ssw890720@gmail.com>
2022-09-28 01:08:41 +0900

    fix : step3 랜덤유틸 테스트 케이스 오류 수정

--- src/test/java/racinggame/util/RandomUtilsTest.java
+++ src/test/java/racinggame/util/RandomUtilsTest.java
@@ -1,20 +1,18 @@
 package racinggame.util;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RandomUtilsTest {
 
-    @Disabled("싪패 가능성이 있는 테스트 이므로 로컬에서 1회성 실행만 할것")
     @Test
     void 랜덤_숫자가_0_9_사이로_생성된다() {
-        int number = RandomUtils.randomNumber(10);
-        assertThat(number).isGreaterThan(0).isLessThan(9);
+        for (int i = 0; i < 1000; i++) {
+            int number = RandomUtils.randomNumber(10);
+            assertThat(number).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(9);
+        }
+
     }
 
 }
