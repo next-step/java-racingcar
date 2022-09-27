fa0fbf19f6580275c52d5933a0f6c28fd5e8e7ed
gumsu <chavi9@naver.com>
2022-09-27 22:20:22 +0900

    test: 우승자 반환 테스트 추가

--- src/test/java/racinggame/RacingCarTest.java
+++ src/test/java/racinggame/RacingCarTest.java
@@ -11,7 +11,7 @@ class RacingCarTest {
     @Test
     void racing() {
         // given
-        RacingCar racingCar = new RacingCar(new String[]{"a", "b", "c"});
+        RacingCar racingCar = RacingCar.of(List.of("a", "b", "c"));
         List<Car> cars = racingCar.getCars();
 
         // when
@@ -23,4 +23,21 @@ class RacingCarTest {
         }
     }
 
+    @Test
+    void 우승자_목록을_반환한다() {
+        // given
+        Car gumsu = new Car("gumsu", 5);
+        Car pobi = new Car("pobi", 3);
+        Car crong = new Car("crong", 2);
+        Car conan = new Car("conan", 5);
+        RacingCar racingCar = new RacingCar(List.of(gumsu, pobi, crong, conan));
+        List<Car> expected = List.of(gumsu, conan);
+
+        // when
+        List<Car> winners = racingCar.getWinners();
+
+        // then
+        assertThat(winners).isEqualTo(expected);
+        assertThat(winners.size()).isEqualTo(expected.size());
+    }
 }
