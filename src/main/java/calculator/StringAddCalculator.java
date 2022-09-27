4ef0a1c93f4e663b3c52059f0d82bb859bd789dd
meanji <winter7574@gmail.com>
2022-09-27 11:29:29 +0900

    racingcar step2 (#3621)
    * feat: null 또는 빈문자열인 경우 테스트
    
    * feat: 숫자 하나인 경우 테스트
    
    * feat: 컴마와 콜론 구분자로 입력할 경우 테스트
    
    * feat: custom 구분자로 입력할 경우 테스트
    
    * feat: 음수인 경우 예외 발생 처리
    
    * style: CalculatorTest 메서드 이름 수정
    
    * refactor: StringAddCalculator 메서드 분리
    
    * fix: PR comment 반영
    
    Related to: #3621

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,69 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    public static final String SEPARATOR = ",|:";
+    public static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
+    public static final int SPLIT_FIRST = 1;
+    public static final int SPLIT_SECOND = 2;
+    public static final Pattern customSeparatorPattern = Pattern.compile(CUSTOM_SEPARATOR);
+
+    public static int splitAndAddString(String numbersText) {
+        if (isEmptyText(numbersText)) {
+            return 0;
+        }
+
+        return add(stringArrayToIntArray(split(numbersText)));
+    }
+
+    private static String[] split(String numbersText) {
+        Matcher customSeparatorMatcher = customSeparatorPattern.matcher(numbersText);
+
+        if (containCustomSeparator(customSeparatorMatcher)) {
+            String customDelimiter = customSeparatorMatcher.group(SPLIT_FIRST);
+            return customSeparatorMatcher.group(SPLIT_SECOND).split(customDelimiter);
+        }
+
+        return numbersText.split(SEPARATOR);
+    }
+
+    private static boolean containCustomSeparator(Matcher m) {
+        return m.find();
+    }
+
+    public static List<Integer> stringArrayToIntArray (String[] numbers) {
+        List<Integer> result = new ArrayList<>();
+
+        for (String number : numbers) {
+            result.add(textToInt(number));
+        }
+
+        return result;
+    }
+
+    private static int add(List<Integer> numbers) {
+        int result = 0;
+
+        for (int number : numbers) {
+            result += number;
+        }
+        return result;
+    }
+
+    private static int textToInt(String text) {
+        int number = Integer.parseInt(text);
+
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
+    private static boolean isEmptyText(String numbersText) {
+        return numbersText == null || numbersText.isEmpty();
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void null_또는_빈문자() {
+        assertThat(StringAddCalculator.splitAndAddString(null)).isEqualTo(0);
+        assertThat(StringAddCalculator.splitAndAddString("")).isEqualTo(0);
+    }
+
+    @Test
+    public void 숫자_하나() {
+        assertThat(StringAddCalculator.splitAndAddString("3")).isEqualTo(3);
+    }
+
+    @Test
+    public void 쉼표_구분자() {
+        assertThat(StringAddCalculator.splitAndAddString("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    public void 쉼표_또는_콜론_구분자() {
+        assertThat(StringAddCalculator.splitAndAddString("1,2:3")).isEqualTo(6);
+    }
+
+    @Test
+    public void custom_구분자() {
+        int result = StringAddCalculator.splitAndAddString("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void 음수_Error_발생() {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndAddString("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
