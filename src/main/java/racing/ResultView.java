21cfe68b3e4179923682001860bbbd130322c9fd
min16 <minji.na@cre.ma>
2022-09-27 23:46:38 +0900

    REFACTOR - 문자열 결합 StringBuilder를 사용하자

--- src/main/java/racing/ResultView.java
+++ src/main/java/racing/ResultView.java
@@ -5,6 +5,8 @@ public class ResultView {
     private static final String ROUND = "라운드";
     private static final String RESULT_TITLE = "실행 결과";
 
+    private static StringBuilder stringBuilder;
+
     public static void printRound(int index) {
         System.out.println(ROUND + " " + index);
     }
@@ -18,19 +20,15 @@ public class ResultView {
     }
 
     private static String generateDistanceToString(int distance) {
-        String stringDistance = "";
-        if (distance == 0) { return stringDistance; }
-        if (distance == 1) { return concatDistance(stringDistance); }
+        if (distance == 0) return "";
 
+        stringBuilder = new StringBuilder();
         for (int i = 0; i < distance; i++) {
-            stringDistance = concatDistance(stringDistance);
+            stringBuilder.append(DISTANCE_CHARACTER);
+            if (distance == 1) break;
         }
 
-        return stringDistance;
-    }
-
-    private static String concatDistance(String str) {
-        return str.concat(DISTANCE_CHARACTER);
+        return stringBuilder.toString();
     }
 
     public static void printNewLine() {
