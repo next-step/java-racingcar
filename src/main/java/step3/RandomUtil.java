b7a27e0f803bf5abdd66352253a0a7ff84ad82bf
OhSeungCheol <sc.oh131@gmail.com>
2022-09-28 01:21:49 +0900

    [1-3] 디버깅용 코드 삭제
    https://github.com/next-step/java-racingcar/pull/3670

--- src/main/java/step3/RandomUtil.java
+++ src/main/java/step3/RandomUtil.java
@@ -6,8 +6,6 @@ public class RandomUtil {
     private static final Random random = new Random();
 
     public static int generateRandomNumber(int range){
-        int temp = random.nextInt(range);
-        System.out.println(temp);
-        return temp;
+        return random.nextInt(range);
     }
 }
