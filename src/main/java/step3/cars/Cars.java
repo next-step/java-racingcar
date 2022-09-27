863fc5a66a910c0919dea07c1bd45fc85203efa9
kureungkureung <terry9611@naver.com>
2022-09-27 22:15:37 +0900

    feat(RacingCars): 자동차 등록 기능

---
+++ src/main/java/step3/cars/Cars.java
@@ -0,0 +1,9 @@
+package step3.cars;
+
+import step3.car.Car;
+
+@FunctionalInterface
+public interface Cars {
+    Cars carsWithAddCar(Car car);
+
+}
---
+++ src/main/java/step3/cars/RacingCars.java
@@ -0,0 +1,42 @@
+package step3.cars;
+
+import step3.car.Car;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public final class RacingCars implements Cars {
+
+    private final List<Car> cars;
+
+    public RacingCars() {
+        this(List.of());
+    }
+
+    public RacingCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public Cars carsWithAddCar(Car car) {
+        ArrayList<Car> existedCars = new ArrayList<>(cars);
+        existedCars.add(car);
+        return new RacingCars(Collections.unmodifiableList(existedCars));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RacingCars that = (RacingCars) o;
+        return Objects.equals(cars, that.cars);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cars);
+    }
+
+}
