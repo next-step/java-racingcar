7ebab8e29080dabdb7e8026a577ba3f0ad03930b
OhSeungCheol <sc.oh131@gmail.com>
2022-09-27 14:25:14 +0900

    [자동차 경주 3단계] refactor: 미사용 멤버변수 삭제
    https://github.com/next-step/java-racingcar/pull/3670

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -3,16 +3,11 @@ package step3;
 import java.util.Random;
 
 public class Car {
-    private int id;
     private int position = 1;
 
     private static final Random random = new Random();
     private static final int RANGE_TO_RANDOM_NUMBER = 10;
     private static final int LIMIT_POINT_TO_MOVE = 4;
-
-    Car(int id) {
-        this.id = id;
-    }
 
     public void run() {
         if (isMovable()) {
--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -9,7 +9,7 @@ public class CarTest {
     @RepeatedTest(10)
     @DisplayName("Car 의 run 수행 시 position 이 변하지 않거나, 1 만큼 증가하는지 테스트")
     public void carMoveTest() {
-        Car car = new Car(0);
+        Car car = new Car();
         car.run();
         assertThat(car.getPosition()).matches(position -> position == 1 || position == 2);
     }
