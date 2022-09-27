db27f61b9e34da970aa03696af67d08eab0708ff
boradol2 <boradol0902@gmail.com>
2022-09-28 02:31:42 +0900

    test : 자동차 이동 구현 testcode 작성

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,14 +1,18 @@
-package step3.racingcar.domain;
+package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
-    @ParameterizedTest
-    @ValueSource(ints = {0,1,2,3})
-    @DisplayName("자동차 멈추는 기능 구현")
-    void randomValueTest(int input) {
-        Car car = new Car(input);
+    public static final int MOVING_POSITION = 1;
+
+    @Test
+    @DisplayName("자동차가 움직이는 기능 구현 : 랜덤값에 의해 자동차의 위치가 1 증가하였음을 생성자로 비교 확인")
+    void movingCarTest_car_positionOneIncrease() {
+        Car car = new Car();
+        car.move(() -> true);
+        assertThat(car).isEqualTo(new Car(MOVING_POSITION));
     }
 }
