af5942e5a3c3c3da4452e003de70b3ce1a677cf1
scoopsoup <rnrdud46@nate.com>
2022-09-28 02:52:15 +0900

    feat : 값 입력 기능구현

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+    Scanner sc = new Scanner(System.in);
+    public int getNumberOfCar(){
+        return sc.nextInt();
+    }
+
+    public int getCarMovingNum(){
+        return sc.nextInt();
+    }
+}
