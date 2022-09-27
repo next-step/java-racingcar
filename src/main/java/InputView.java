b34beb6bafbf7c48ec7efbc58c3c8f7d20d820b8
kkywalk2 <kkywalk2@gmail.com>
2022-09-28 00:35:02 +0900

    refactor: scanner를 try-with-resource를 사용하여 사용범위를 좀 더 명시적으로 수정

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class InputView {
 
     public static CarRacingExecutor inputCarRacingInformation() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int carCount = scanner.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int moveCount = scanner.nextInt();
-        scanner.close();
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.println("자동차 대수는 몇 대 인가요?");
+            int carCount = scanner.nextInt();
 
-        return new CarRacingExecutor(carCount, moveCount);
+            System.out.println("시도할 회수는 몇 회 인가요?");
+            int moveCount = scanner.nextInt();
+
+            return new CarRacingExecutor(carCount, moveCount);
+        }
     }
 }
