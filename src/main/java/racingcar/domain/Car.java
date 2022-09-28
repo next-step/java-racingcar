4ced1a46455568f1992093bad92b529baa3f8894
이건회 <rg970604@naver.com>
2022-09-28 17:57:22 +0900

    refactor(Car) : 역할 분리

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,23 +1,8 @@
 package racingcar.domain;
 
 public class Car {
-    public String positionView = "";
-    private int criteriaNum = 4;
 
-    public Car() {
-    }
+  public String positionView = "";
+  public int criteriaNum = 4;
 
-    public boolean isMoveAble(int randomInt) {
-        if (randomInt >= criteriaNum) {
-            return true;
-        }
-        return false;
-    }
-
-    public String moveCar(boolean isMoveable) {
-        if (isMoveable == true) {
-            positionView += "-";
-        }
-        return positionView;
-    }
 }
