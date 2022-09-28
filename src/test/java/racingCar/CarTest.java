756b819238b2a1bd26fda745ca5f1917bb5b5409
wugatama <dnr7410@naver.com>
2022-09-28 20:58:56 +0900

    test: 라운드 당 랜덤값 삽입

--- src/test/java/racingCar/CarTest.java
+++ src/test/java/racingCar/CarTest.java
@@ -2,6 +2,8 @@ package racingCar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,20 +14,6 @@ public class CarTest {
 	@BeforeEach
 	void setUp() {
 		car = new Car();
-	}
-
-	@Test
-	@DisplayName("4이상 일때 전진 확인 테스트")
-	void checkCarMoving() {
-		car.move(Racing.move(4));
-		assertThat(car.getStatus()).isEqualTo("-");
-	}
-
-	@Test
-	@DisplayName("4이상 일때 정지 테스트")
-	void checkCarMovingAndStopMoving() {
-		car.move(Racing.move(3));
-		assertThat(car.getStatus()).isEqualTo("");
 	}
 
 	@Test
@@ -40,5 +28,13 @@ public class CarTest {
 	void carNameBelowFive() {
 		car.createName("abcde");
 		assertThat(car.getName()).isEqualTo("abcde");
+	}
+
+	@Test
+	@DisplayName("라운드 당 랜덤값 상태 확인")
+	void carStatusOfRound() {
+		Random random = new Random();
+		car.movingOfRound(random.nextInt(10));
+		assertThat(car.getStatus().size()).isEqualTo(1);
 	}
 }
