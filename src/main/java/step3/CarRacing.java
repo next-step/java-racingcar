daafa203fd7fabce13300e5407a5e84fb2e9a9db
ankyeongeun <anruddms8124@gmail.com>
2022-09-29 00:53:52 +0900

    feat - [Step3] inputview add

---
+++ src/main/java/step3/CarRacing.java
@@ -0,0 +1,12 @@
+package step3;
+
+import java.util.Scanner;
+
+public class CarRacing {
+    Scanner scanner = new Scanner(System.in);
+    String value = scanner.nextLine();
+    int number = scanner.nextInt();
+
+    public static class InputView {
+    }
+}
