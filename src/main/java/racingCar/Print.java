942b650eb67c9150bf29361ea1aa1335969f6567
wugatama <dnr7410@naver.com>
2022-09-29 00:58:16 +0900

    feat: Print 경주할 자동차 추가

--- src/main/java/racingCar/Print.java
+++ src/main/java/racingCar/Print.java
@@ -1,6 +1,12 @@
 package racingCar;
 
+import java.util.List;
+
 public class Print {
+	public static void participantsOfRacing() {
+		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+	}
+
 	public static void howManyCars() {
 		System.out.println("자동차 대수는 몇 대 인가요?");
 	}
@@ -9,7 +15,7 @@ public class Print {
 		System.out.println("시도할 회수는 몇 회 인가요?");
 	}
 
-	public static void resultOfRound(String status) {
+	public static void resultOfRound(List<Integer> status) {
 		System.out.println(status);
 	}
 
