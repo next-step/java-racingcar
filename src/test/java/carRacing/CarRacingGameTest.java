6a9254ef41a5ff7e440a1b97c485c7469403d110
OhSeungCheol <sc.oh131@gmail.com>
2022-09-28 10:58:18 +0900

    [1-4] test: Car 구조 변경(이름 추가)에 따른 테스트 케이스 수정

--- src/test/java/carRacing/CarRacingGameTest.java
+++ src/test/java/carRacing/CarRacingGameTest.java
@@ -9,20 +9,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarRacingGameTest {
     private final static int DEFAULT_POSITION = 1;
+    private final static String CAR_NAMES_STRING_TO_TEST = "pobi,crong,honux";
 
     @Test
-    @DisplayName("CarRacingGame 인스턴스가 생성됐을 때 CarEntry 값이 비어있지 않음을 테스트")
+    @DisplayName("CarRacingGame 인스턴스를 생성했을 때 CarEntry 값이 올바른지 테스트")
     public void carRacingGameTest1() {
-        CarRacingGame carRacingGame = new CarRacingGame(5, 3);
+        CarRacingGame carRacingGame = new CarRacingGame(CAR_NAMES_STRING_TO_TEST , 3);
         assertThat(carRacingGame.getCarEntryList()).isNotNull();
-        assertThat(carRacingGame.getCarEntryList().size()).isNotEqualTo(0);
+        assertThat(carRacingGame.getCarEntryList().size()).isEqualTo(CAR_NAMES_STRING_TO_TEST.split(",").length);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9})
-    @DisplayName("모든 자동차 경주 게임이 끝난 후, 각 자동차가 움직인 횟수는 시합 횟수를 초과하지 않음을 테스트")
-    public void carRacingGameTest1(int testNumber) {
-        CarRacingGame carRacingGame = new CarRacingGame(testNumber, testNumber);
+    @DisplayName("[랜덤 요소 존재] 모든 자동차 경주 게임이 끝난 후, 각 자동차가 움직인 횟수는 시합 횟수를 초과하지 않음을 테스트")
+    public void carRacingGameTest2(int testNumber) {
+        CarRacingGame carRacingGame = new CarRacingGame(CAR_NAMES_STRING_TO_TEST, testNumber);
         carRacingGame.doGame();
         for (Car car : carRacingGame.getCarEntryList()) {
             assertThat(car.getPosition() - DEFAULT_POSITION).isLessThanOrEqualTo(testNumber);
--- src/test/java/carRacing/CarTest.java
+++ src/test/java/carRacing/CarTest.java
@@ -1,6 +1,5 @@
 package carRacing;
 
-import carRacing.Car;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -12,7 +11,7 @@ public class CarTest {
     @CsvSource(value = {"0:1", "1:1", "2:1", "3:1", "4:2", "5:2", "6:2", "7:2", "8:2", "9:2"}, delimiter = ':')
     @DisplayName("movePoint 가 4 이상일 때만 Car 의 position 증가하는지 테스트")
     public void carMoveTest(int movePoint, int expectedPosition) {
-        Car car = new Car();
+        Car car = new Car("testName");
         car.run(movePoint);
         assertThat(car.getPosition()).isEqualTo(expectedPosition);
     }
