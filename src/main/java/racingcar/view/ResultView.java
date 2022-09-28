c489d8e55477a0f3796ebf33820aded0ee16619d
boradol2 <boradol0902@gmail.com>
2022-09-29 01:07:47 +0900

    feat : 출력 부분 position에 따라 '-' 출력 되게 구현

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -2,12 +2,16 @@ package racingcar.view;
 
 public class ResultView {
     private static final String RESULT_MESSAGE = "실행결과";
-    private static final String MOVING_POINT = "-";
+    private static final String POSITION_MARK = "-";
 
     private ResultView() {
     }
 
     public static void printResultMessage() {
         System.out.println("\n" + RESULT_MESSAGE + "\n");
+    }
+
+    public static void printPositionMark(int position) {
+        System.out.println(POSITION_MARK.repeat(position));
     }
 }
