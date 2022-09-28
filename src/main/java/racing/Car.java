3597f3b68abeba6c2ce56c6a5ddef5fa9b3bd764
jimbaemon <jimbae@kakao.com>
2022-09-28 21:58:44 +0900

    feat: 자동차 위치 표기 함수 생성.

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -3,7 +3,9 @@ package racing;
 import java.util.Comparator;
 
 public class Car implements Comparator<Car> {
-    private int location;
+    private static final String ROAD = "-";
+
+    private int location = 1;
 
     public void move(){
         location++;
@@ -12,5 +14,11 @@ public class Car implements Comparator<Car> {
     @Override
     public int compare(Car o1, Car o2) {
         return Integer.compare(o1.location, o2.location);
+    }
+
+    public String location() {
+        StringBuilder result = new StringBuilder();
+        result.append(ROAD.repeat(Math.max(1, location)));
+        return result.toString();
     }
 }
