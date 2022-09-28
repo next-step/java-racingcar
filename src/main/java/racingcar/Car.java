1b2c136bdbbbcd9142ef9727d4ff67a79f0561dc
jungwoon727 <jungwoon727@gmail.com>
2022-09-29 02:19:59 +0900

    refactor: 사용하지 않는 equals() hashCode() 메서드 제거

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -19,19 +19,6 @@ public class Car {
         }
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Car car = (Car) o;
-        return position == car.position;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(position);
-    }
-
     public int getPosition() {
         return position;
     }
