15a44abfeeee0183f2c1dc7b293971343de51444
bella.n <bella.n@kakaocorp.com>
2022-09-29 00:08:45 +0900

    test: 자동차 이동 여부 검증 테스트

---
+++ src/test/java/racingcarGame/GameTest.java
@@ -0,0 +1,18 @@
+package racingcarGame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"3:false", "4:true", "5:true"}, delimiter = ':')
+    @DisplayName("숫자로 자동자 이동 여부 검증")
+    public void movableCar(int num, boolean expected) {
+        boolean result = Game.isMovable(num);
+        assertThat(result).isEqualTo(expected);
+    }
+}
