b3c27cc20da3220d103b15db6be6fcedc9a34ee3
김재훈 <timewarp96@naver.com>
2022-09-29 04:08:06 +0900

    [update] Car 클래스 패키지 이동 및 이동 조건 추가, position 0으로 초기화
    초기 position 1 -> 0으로 변경,
    position이 0이면 출발 라인에도 안서있는 상태이므로 무조건 이동하는 로직 추가

--- src/main/java/racing/game/Car.java
+++ src/main/java/racing/game/Car.java
@@ -1,4 +1,6 @@
-package racing;
+package racing.game;
+
+import racing.game.random.RandomNumber;
 
 public class Car {
 
@@ -6,25 +8,36 @@ public class Car {
     private static final int MOVABLE_LIMIT = 4;
 
     private final RandomNumber random;
+    private final String name;
     private int position;
 
-    public Car(RandomNumber random) {
-        this.position = 1;
+    public Car(String name, RandomNumber random) {
+        this.name = name;
+        this.position = 0;
         this.random = random;
     }
 
     public void move() {
-        if (isMovable()) {
+        if (isMovable() || isOnStartLine()) {
             moveForward();
         }
+    }
+
+    public String name() {
+        return name;
     }
 
     public int currentPosition() {
         return position;
     }
 
+
     private boolean isMovable() {
         return random.generate(BOUND) >= MOVABLE_LIMIT;
+    }
+
+    private boolean isOnStartLine() {
+        return position == 0;
     }
 
     private void moveForward() {
