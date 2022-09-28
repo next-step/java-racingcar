04b11ce2b7a90651022284a27d234eb506c5bf47
bella.n <bella.n@kakaocorp.com>
2022-09-29 01:15:00 +0900

    refactor: 불필요한 메소드 제거

--- src/test/java/racingcarGame/CarTest.java
+++ src/test/java/racingcarGame/CarTest.java
@@ -7,25 +7,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
-    private static final String POSITION_SHOW_MARK = "-";
 
     @ParameterizedTest
-    @CsvSource(value = {"1:2", "2:3"}, delimiter = ':')
+    @CsvSource(value = {"--:---", "-:--"}, delimiter = ':')
     @DisplayName("자동차 이동 검증")
-    public void moveCar(int input, int expected) {
-        Car car = new Car(getCarMovement(input));
+    public void moveCar(String input, String expected) {
+        Car car = new Car(input);
         Car result = car.move();
 
-        assertThat(result).isEqualTo(new Car(getCarMovement(expected)));
-    }
-
-    private String getCarMovement(int position) {
-        String result = "";
-
-        for (int i = 0; i < position; i++) {
-            result += POSITION_SHOW_MARK;
-        }
-
-        return result;
+        assertThat(result).isEqualTo(new Car(expected));
     }
 }
