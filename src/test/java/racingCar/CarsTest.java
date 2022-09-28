f282b7a1487e7760ee706cb6dffd56adabbd71c0
wugatama <dnr7410@naver.com>
2022-09-29 00:57:47 +0900

    test: CarsTest 추가 - , 분리된 배열을 받아서 생성

---
+++ src/test/java/racingCar/CarsTest.java
@@ -0,0 +1,15 @@
+package racingCar;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarsTest {
+	@Test
+	@DisplayName(", 로 분리된 배열을 받아서 List<Car> 생성")
+	void createCars() {
+		Cars cars = new Cars(new String[] {"pobi", "crong", "honux"});
+		assertThat(cars.getCars().size()).isEqualTo(3);
+	}
+}
