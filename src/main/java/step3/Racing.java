99c883e63faa108b0f059521a5eca720e5743303
jopopscript <lssmm1230@gmail.com>
2022-09-29 07:06:25 +0900

    feat: Racing에서 우승자 선정하는 기능 생성

--- src/main/java/step3/Racing.java
+++ src/main/java/step3/Racing.java
@@ -1,21 +1,20 @@
 package step3;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Racing {
-    final int carNumber;
+    private final String[] carNames;
 
-    final int iterate;
+    private final int iterate;
 
-    private final List<Car> cars = new ArrayList<Car>();
+    private final List<Car> cars = new ArrayList<>();
 
-    public Racing(int carNumber, int iterate) {
-        this.carNumber = carNumber;
+    public Racing(String[] carNames, int iterate) {
+        this.carNames = carNames;
         this.iterate = iterate;
-        this.createParticipants();
+        this.createCars();
     }
 
     public void start() {
@@ -24,9 +23,20 @@ public class Racing {
         }
     }
 
-    private void createParticipants() {
-        for (int i = 0; i < this.carNumber; i++) {
-            this.cars.add(new Car());
+    public List<Car> getRacingRecordOfCars() {
+        return this.cars;
+    }
+
+    public List<String> getWinners() {
+        Map<String, Integer> lastRecords = this.mapLastRecords(cars);
+        Integer winnerLastRecord = this.getWinnerLastRecord(lastRecords);
+        List<String> winners = this.filterWinners(winnerLastRecord, lastRecords);
+        return winners;
+    }
+
+    private void createCars() {
+        for (String carName : this.carNames) {
+            this.cars.add(new Car(carName));
         }
     }
 
@@ -34,7 +44,28 @@ public class Racing {
         this.cars.forEach(Car::moveByRandomOnRound);
     }
 
-    public List<Car> getRacingRecordOfCars() {
-        return this.cars;
+    private Map<String, Integer> mapLastRecords(List<Car> cars) {
+        return cars.stream()
+                .map(car -> {
+                    List<Integer> racingRecord = car.getRacingRecord();
+                    System.out.println("racingRecord = " + racingRecord);
+                    Integer lastRecord = racingRecord.get(racingRecord.size() - 1);
+                    return Map.entry(car.getName(), lastRecord);
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private Integer getWinnerLastRecord(Map<String, Integer> lastRecords) {
+        return lastRecords.entrySet().stream()
+                .mapToInt(entry -> entry.getValue())
+                .max()
+                .orElseThrow(() -> new NoSuchElementException("승자의 기록을 찾기위해서 1개이상의 기록이 필요합니다."));
+    }
+
+    private List<String> filterWinners(Integer winnerLastRecord, Map<String, Integer> lastRecords) {
+        return lastRecords.entrySet().stream()
+                .filter(entry -> entry.getValue() == winnerLastRecord)
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList());
     }
 }
