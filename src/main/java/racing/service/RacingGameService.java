b30c9e81ce1e52189d343d491a04263f8bc40b0f
Shin seongsu <tlstjdtn321@naver.com>
2022-09-29 02:04:36 +0900

    fix: service interface 제거

--- src/main/java/racing/service/RacingGameService.java
+++ src/main/java/racing/service/RacingGameService.java
@@ -1,11 +1,30 @@
 package racing.service;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import racing.domain.Car;
 import racing.domain.Cars;
 import racing.domain.MovableStrategy;
+import racing.ui.ResultView;
 
-public interface RacingGameService {
+public class RacingGameService {
 
-    Cars prepare();
+    private static final int FIRST_INDEX = 0;
 
-    void racing(Cars cars, MovableStrategy movableStrategy);
+    public Cars prepare(int totalCount) {
+        return new Cars(
+            IntStream.range(FIRST_INDEX, totalCount)
+                .mapToObj(index -> new Car())
+                .collect(Collectors.toList()));
+    }
+
+    public void racing(Cars cars, int tryCount, ResultView resultView, MovableStrategy movableStrategy) {
+        resultView.start();
+
+        IntStream.range(FIRST_INDEX, tryCount)
+            .forEach(index -> {
+                cars.move(movableStrategy);
+                cars.print(resultView);
+            });
+    }
 }
