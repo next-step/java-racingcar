9415b0076c5d8b8e526cc8de8d3dae99b112a685
jimbaemon <jimbae@kakao.com>
2022-09-28 23:26:12 +0900

    test: 경주 경기 횟수별, 자동차 댓수별 결과 반환 테스트

---
+++ src/test/java/racing/RaceTest.java
@@ -0,0 +1,29 @@
+package racing;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class RaceTest {
+
+    @Test
+    void 자동차_경주_결과_경기_횟수_확인(){
+        Race race = new Race(3, 5);
+        race.play();
+
+        ArrayList<ArrayList<String>> result = race.result();
+
+        Assertions.assertThat(result).hasSize(5);
+    }
+
+    @Test
+    void 자동차_경주_결과_자동차_댓수_확인(){
+        Race race = new Race(3, 5);
+        race.play();
+        ArrayList<ArrayList<String>> result = race.result();
+
+        Assertions.assertThat(result.get(0)).hasSize(3);
+    }
+
+}
