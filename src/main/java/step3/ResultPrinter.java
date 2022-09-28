d24e32ea7b5d366cae81305a683a19c57b4e71b9
jopopscript <lssmm1230@gmail.com>
2022-09-29 07:07:14 +0900

    feat: 경기기록 출력시 이름과 우승자 출력 기능 생성

--- src/main/java/step3/ResultPrinter.java
+++ src/main/java/step3/ResultPrinter.java
@@ -2,13 +2,18 @@ package step3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ResultPrinter {
 
     private final String PROCESS_BAR = "-";
+
     private final String END_OF_ROUND_MESSAGE = "";
+
+    private final String WINNER_SUFFIX = "가 최종 우승했습니다.";
 
     private final int iterate;
 
@@ -16,32 +21,39 @@ public class ResultPrinter {
         this.iterate = iterate;
     }
 
-    public void print(List<Car> cars) {
+    public void printRecord(List<Car> cars) {
         System.out.println("실행 결과");
-        List<List<String>> processBarRecords = this.convertProcessBar(cars);
+        Map<String, List<String>> processBarRecords = this.recordConvertToProcessBar(cars);
         for (int i = 0; i < this.iterate; i++) {
-            for (List<String> processBarRecord : processBarRecords) {
-                System.out.println(processBarRecord.get(i));
-            }
+            this.printRoundRecord(processBarRecords, i);
             this.printEndMessageOfRound(i);
         }
     }
 
-    private void printEndMessageOfRound(int round) {
-        if (!this.isLastRound(round)) {
-            System.out.println(this.END_OF_ROUND_MESSAGE);
+    public void printWinner(List<String> winners) {
+        System.out.println(String.join(", ", winners) + WINNER_SUFFIX);
+    }
+
+    private Map<String, List<String>> recordConvertToProcessBar(List<Car> cars) {
+        return cars.stream()
+                .map(car -> {
+                    List<String> processBarRecord = car.getRacingRecord().stream()
+                            .map(PROCESS_BAR::repeat)
+                            .collect(Collectors.toList());
+                    return Map.entry(car.getName(), processBarRecord);
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private void printRoundRecord(Map<String, List<String>> processBarRecords, int round) {
+        for( Map.Entry<String, List<String>> entry : processBarRecords.entrySet() ){
+            String carName = entry.getKey();
+            String processBarRecord = entry.getValue().get(round);
+            System.out.println(carName + " : " + processBarRecord);
         }
     }
 
-    private boolean isLastRound(int round) {
-        return round == this.iterate - 1;
-    }
-
-    private List<List<String>> convertProcessBar(List<Car> cars) {
-        return cars.stream().map(
-                car -> car.getRacingRecord().stream()
-                        .map(PROCESS_BAR::repeat)
-                        .collect(Collectors.toList())
-        ).collect(Collectors.toList());
+    private void printEndMessageOfRound(int round) {
+        System.out.println(this.END_OF_ROUND_MESSAGE);
     }
 }
