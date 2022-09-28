4eb7e0445c92b00f895b322b7f086be68388f8f4
suchan <aud248@naver.com>
2022-09-28 15:33:36 +0900

    test: tryCount 예외처리

--- src/main/java/step4/input/RacingCarInput.java
+++ src/main/java/step4/input/RacingCarInput.java
@@ -13,6 +13,14 @@ public class RacingCarInput {
 
     public static int tryCount() {
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        int tryCount = scanner.nextInt();
+        validateTryCount(tryCount);
+        return tryCount;
+    }
+
+    private static void validateTryCount(int tryCount) {
+        if (tryCount < 1) {
+            throw new IllegalArgumentException("음수나 0은 입력할 수 없습니다");
+        }
     }
 }
