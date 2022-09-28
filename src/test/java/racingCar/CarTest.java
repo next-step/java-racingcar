24bce54a5fb3e89aed5754736b49b5b49c32bd40
wugatama <dnr7410@naver.com>
2022-09-29 00:30:32 +0900

    refactor: Car 생성시 바로 이름 넣기

--- src/test/java/racingCar/CarTest.java
+++ src/test/java/racingCar/CarTest.java
@@ -9,30 +9,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
-	private Car car;
-
-	@BeforeEach
-	void setUp() {
-		car = new Car();
-	}
 
 	@Test
 	@DisplayName("자동차 이름 5자 초과하면 예외발생")
 	void carNameOverFive() {
-		assertThatThrownBy(() -> car.createName("abcdef"))
+		assertThatThrownBy(() -> new Car("abcdef"))
 			.isInstanceOf(RuntimeException.class);
 	}
 
 	@Test
 	@DisplayName("자동차 이름 5자 이하면 통과")
 	void carNameBelowFive() {
-		car.createName("abcde");
+		Car car = new Car("abcde");
 		assertThat(car.getName()).isEqualTo("abcde");
 	}
 
 	@Test
 	@DisplayName("라운드 당 랜덤값 상태 확인")
 	void carStatusOfRound() {
+		Car car = new Car("test");
 		Random random = new Random();
 		car.movingOfRound(random.nextInt(10));
 		assertThat(car.getStatus().size()).isEqualTo(1);
