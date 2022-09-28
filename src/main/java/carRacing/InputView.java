93ae51aa86d9caec1b8e753a49bd41741de1980e
OhSeungCheol <sc.oh131@gmail.com>
2022-09-28 10:46:39 +0900

    [1-4] feat: 요구 사항에 맞게 UI 영역 수정

--- src/main/java/carRacing/InputView.java
+++ src/main/java/carRacing/InputView.java
@@ -3,22 +3,22 @@ package carRacing;
 import java.util.Scanner;
 
 public class InputView {
-    private Integer numberOfCar;
+    private String carNamesString;
     private Integer tryCount;
 
     public InputView() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int numberOfCar = scanner.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String carNameString = scanner.nextLine();
+        System.out.println("시도할 회수는 몇회인가요?");
         int tryCount = scanner.nextInt();
 
-        this.numberOfCar = numberOfCar;
+        this.carNamesString= carNameString;
         this.tryCount = tryCount;
     }
 
-    public Integer getNumberOfCar() {
-        return numberOfCar;
+    public String getCarNamesString() {
+        return carNamesString;
     }
 
     public Integer getTryCount() {
