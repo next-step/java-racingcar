0438f9ba33833c27ad1fba1a24b64d9c1a51323d
이건회 <rg970604@naver.com>
2022-09-30 04:28:33 +0900

    refactor(ResultView.java) : 경주 진행 로직 분리

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -6,10 +6,16 @@ import java.util.List;
 
 public class ResultView {
 
-  public void printResult(int tryTimes, List<Car> cars) {
-    System.out.println("실행 결과");
-    RacingRule racingRule = new RacingRule();
-    racingRule.cycleTryTimes(tryTimes, cars);
+  private static final String positionPoint = "-";
+
+  public void printResult(List<Car> cars) {
+    for (Car car : cars) {
+      for (int j = 0; j < car.getPosition(); j++) {
+        System.out.print(positionPoint);
+      }
+      System.out.println("");
+    }
+    System.out.println();
   }
 
 }
