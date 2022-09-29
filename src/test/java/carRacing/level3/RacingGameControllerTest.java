faca9d907d5260bae0bdc7aa4b674c6c5937d709
sienna <20200803@sungshin.ac.kr>
2022-09-29 22:14:39 +0900

    :white_check_mark:Test : controller 테스트

---
+++ src/test/java/carRacing/level3/RacingGameControllerTest.java
@@ -0,0 +1,19 @@
+package carRacing.level3;
+
+
+import carRacing.level3.controller.RacingGameController;
+import carRacing.level3.service.RandomMovingStartegy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RacingGameControllerTest {
+
+	RacingGameController race = new RacingGameController();
+
+	@Test
+	@DisplayName("경주시작")
+	public void driveCar() {
+		race.gameStart(new RandomMovingStartegy());
+	}
+
+}
