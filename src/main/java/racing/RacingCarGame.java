7217ee8420d6baf0c38962b24a7ef51377e8219f
michol.lee <lovethefeel@gmail.com>
2022-09-29 21:13:59 +0900

    refactor: 레이싱 자동차 메인

---
+++ src/main/java/racing/RacingCarGame.java
@@ -0,0 +1,12 @@
+package racing;
+
+import racing.controller.RacingCarsPlay;
+
+public class RacingCarGame {
+
+    public static void main(String[] args) {
+
+        RacingCarsPlay racingCarsPlay = new RacingCarsPlay();
+        racingCarsPlay.start();
+    }
+}
