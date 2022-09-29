703e93dec83c293bb90ecf2604586bca3f6e42c5
이건회 <rg970604@naver.com>
2022-09-30 04:28:08 +0900

    refactor(RandomNumTest.java)

--- src/test/java/racingCar/RandomNumTest.java
+++ src/test/java/racingCar/RandomNumTest.java
@@ -3,14 +3,16 @@ package racingCar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.RandomNum;
+import racingcar.MovingStrategy;
+import racingcar.RandomMovingStrategy;
 
 public class RandomNumTest {
 
   @Test
   @DisplayName("랜덤값 0 이상 10 미만")
   void testRandomNum0to9() {
-    RandomNum randomNum = new RandomNum();
-    Assertions.assertThat(randomNum.createRandNum()).isBetween(0, 9);
+    MovingStrategy movingStrategy = new RandomMovingStrategy();
+    int randomNum = movingStrategy.getRandNum();
+    Assertions.assertThat(randomNum).isBetween(0, 9);
   }
 }
