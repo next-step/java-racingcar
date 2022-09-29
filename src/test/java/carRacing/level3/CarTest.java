fc696ac372b5c73189fe6937a666bd2f39f7febd
sienna <20200803@sungshin.ac.kr>
2022-09-29 10:11:28 +0900

    :recycle:refactor : carNum 변수 삭제 어차피 List라 index가 carNum을 뜻하게 되어서 carNum은 삭제해보았습니다!

--- src/test/java/carRacing/level3/CarTest.java
+++ src/test/java/carRacing/level3/CarTest.java
@@ -6,18 +6,16 @@ import org.junit.jupiter.api.Test;
 
 public class CarTest {
 	public static final Integer DEFAULT_LOCATION = 1;
-	public static final Integer TEST_CARNUM = 1;
 
 	@Test
 	void 자동차_한대_생성() {
-		Car car = new Car(TEST_CARNUM);
+		Car car = new Car();
 		Assertions.assertEquals(car.carLocation(), DEFAULT_LOCATION);
-		Assertions.assertEquals(car.carNum(), TEST_CARNUM);
 	}
 
 	@Test
 	void 자동차_전진() {
-		Car car = new Car(TEST_CARNUM);
+		Car car = new Car();
 		car.moveLocation();
 		Assertions.assertEquals(car.carLocation(), DEFAULT_LOCATION + 1);
 	}
