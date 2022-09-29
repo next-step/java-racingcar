67229a1a70ea364d5bde2212d441f527ee4b6b86
sienna <20200803@sungshin.ac.kr>
2022-09-29 12:13:56 +0900

    :recycle:refactor : 랜덤값에 따른 움직임 여부를 car에서 처리하도록 추가

--- src/main/java/carRacing/level3/domain/Car.java
+++ src/main/java/carRacing/level3/domain/Car.java
@@ -1,11 +1,12 @@
 package carRacing.level3.domain;
 
+import carRacing.level3.service.MovingStartegy;
 public class Car {
 
 	private static final Integer DEFAULT_LOCATION = 1;
 	private static final Integer MOVE_LOCATION = 1;
 	private Integer location;
-
+	private MovingStartegy movingStartegy = new MovingStartegy();
 	public Car() {
 		this.location = DEFAULT_LOCATION;
 	}
@@ -15,6 +16,9 @@ public class Car {
 	}
 
 	public void moveLocation() {
+		if(movingStartegy.isMovable())
 		this.location += MOVE_LOCATION;
 	}
+
+
 }
