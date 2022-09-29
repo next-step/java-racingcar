6f802f93473877923fd6d8981aec3cce2ea0ebb4
sylee <wnghkdcjdtns@naver.com>
2022-09-29 23:46:35 +0900

    test 시도한 횟수 및 자동차 이동 테스트 추가

--- src/test/java/study/RacingGameTest.java
+++ src/test/java/study/RacingGameTest.java
@@ -1,7 +1,5 @@
 package study;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +13,19 @@ public class RacingGameTest {
     void carCount(){
         RacingGame racingGame = new RacingGame();
         assertThat(racingGame.makeCarList(5)).hasSize(5);
+    }
+
+    @Test
+    @DisplayName("시도한 횟수")
+    void tryCount(int count){
+        /* 요부분은 고민하고있습니다 ㅜㅜ..*/
+    }
+
+    @Test
+    @DisplayName("4이상의 숫자가 들어오면 전진")
+    void carMove(){
+        Car car = new Car();
+        car.move(5);
+        assertThat(car.getMoveCount()).isEqualTo(1);
     }
 }
