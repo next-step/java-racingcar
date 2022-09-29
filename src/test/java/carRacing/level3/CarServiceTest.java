478b38634d3465de7f4f24422d2fc1482884f08f
sienna <20200803@sungshin.ac.kr>
2022-09-29 12:10:41 +0900

    :recycle:refactor : CarService 테스트-컬렉션 적용

--- src/test/java/carRacing/level3/CarServiceTest.java
+++ src/test/java/carRacing/level3/CarServiceTest.java
@@ -1,29 +1,31 @@
 package carRacing.level3;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import carRacing.level3.domain.Cars;
 import carRacing.level3.service.CarService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarServiceTest {
 
-	public static final Integer CAR_NUM = 1;
-	public static final Integer MOVE_STATUS = 1;
-
+	public static final Integer CAR_SUM = 3;
+	public static final Integer DEFAULT_LOCATION = 1;
 	CarService carService = new CarService();
 
 	@Test
-	@DisplayName("경주가 시작되면 자동차는 랜덤값에 따라 이동한다.")
-	public void moveCar() {
-		carService.initRace(CAR_NUM);
-		int current_status = carService.carMovingLocation(CAR_NUM);
-
-		carService.moveCar(CAR_NUM);
-		int new_status = carService.carMovingLocation(CAR_NUM);
-
-		assertEquals(current_status + MOVE_STATUS, new_status);
-
+	@DisplayName("경주를 위해 자동차 컬렉션을 생성한다")
+	void 경주_준비() {
+		assertThat(carService.prepare(CAR_SUM).getClass()).isEqualTo(Cars.class);
 	}
 
+	@Test
+	@DisplayName("경주를 진행한다")
+	void 경주_진행() {
+		Cars cars = carService.prepare(CAR_SUM);
+		carService.moveCar(cars);
+		for (int i = 0; i < CAR_SUM; i++) {
+			assertThat(cars.carLocation(i)).isGreaterThanOrEqualTo(DEFAULT_LOCATION);
+		}
+	}
 }
