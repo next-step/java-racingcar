be07b3ac3ace6df525c2987f21944cef529dfede
heoseungyeon <dia0312@naver.com>
2022-09-29 20:31:40 +0900

    fix: splitAndSum_null_또는_빈문자 테스트 ParameterizedTest로 변경 - NullAndEmptySource 어노테이션 활용

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,17 +3,17 @@ package calculator;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class StringAddCalculatorTest {
 
 	private StringAddCalculator stringAddCalculator = new StringAddCalculator();
 
-	@Test
-	public void splitAndSum_null_또는_빈문자() {
-		int result = stringAddCalculator.splitAndSum(null);
-		assertThat(result).isEqualTo(0);
-
-		result = stringAddCalculator.splitAndSum("");
+	@ParameterizedTest
+	@NullAndEmptySource
+	public void splitAndSum_null_또는_빈문자(String text) {
+		int result = stringAddCalculator.splitAndSum(text);
 		assertThat(result).isEqualTo(0);
 	}
 
