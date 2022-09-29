8bc0db8e2c8d1c47aa66a06d702881214d2e2188
michol.lee <lovethefeel@gmail.com>
2022-09-29 21:17:03 +0900

    feat(test): 레이싱 스코어 테스트

---
+++ src/test/java/racing/domain/RacingScoreTest.java
@@ -0,0 +1,33 @@
+package racing.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("레이싱 자동차 스코어 객체 관련 테스트")
+class RacingScoreTest {
+
+    @Test
+    @DisplayName("레이싱 결과를 저장한다.")
+    void asdasd() {
+
+        final RacingScore racingScore = RacingScore.getInstance();
+
+        final List<RacingCar> oneRound = List.of(new RacingCar(new CarName("pobi"), 0)
+                , new RacingCar(new CarName("line"), 1)
+        );
+        final RacingCars racingCars = new RacingCars(oneRound);
+
+        racingScore.save(racingCars);
+
+        assertAll(
+                () -> assertThat(racingScore.findByIndex(0)).hasSize(2),
+                () -> assertThat(racingScore.findByIndex(0)).isEqualTo(oneRound)
+        );
+
+    }
+}
