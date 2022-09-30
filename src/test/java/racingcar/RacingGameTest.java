21942574a3b334f6a16f2975ad1477aff3c321f2
yunhwan <pyh8618@gmail.com>
2022-09-30 22:22:11 +0900

    test: RoundGame 테스트
    * 시도횟수음수 생성 실패 테스트
    * racingStart 후 Record기록 테스트

---
+++ src/test/java/racingcar/RacingGameTest.java
@@ -0,0 +1,41 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.TestMovingStrategy.TestSuccessMoveStrategy;
+import racingcar.strategy.MovingStrategy;
+
+class RacingGameTest {
+
+    List<String> names;
+
+    @BeforeEach
+    void setUp() {
+        names = Arrays.asList("y1", "a2", "b3");
+    }
+
+    @Test
+    void constructorTryNumMinusFail() {
+        assertThatThrownBy(() -> new RacingGame(-1, names))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void racingStart() {
+        RacingGame racingGame = new RacingGame(3, names);
+
+        MovingStrategy movingStrategy = new TestSuccessMoveStrategy();
+        RacingRecord racingRecord = racingGame.racingStart(movingStrategy);
+
+        List<RoundCars> roundCars = racingRecord.retrieveRecord();
+        assertThat(roundCars)
+                .flatMap(RoundCars::retrieveCars)
+                .map(Car::getPosition)
+                .containsExactly(1, 1, 1, 2, 2, 2, 3, 3, 3);
+    }
+}
