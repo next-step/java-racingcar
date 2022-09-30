7e354bbe9d56ef997d4f8fe6bf8d766de034d240
mjin1220 <myeongjin1220@gmail.com>
2022-10-01 00:48:59 +0900

    refactor: 자동차 이름 최대 길이 상수를 한 곳에서 관리되도록 변경

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 public class RacingCar {
 
-    private static final int MAX_NAME_LENGTH = 5;
+    public static final int MAX_NAME_LENGTH = 5;
 
     private final String name;
     private int distance;
--- src/main/java/racingcar/exception/RacingCarNameOverSizeException.java
+++ src/main/java/racingcar/exception/RacingCarNameOverSizeException.java
@@ -1,11 +1,11 @@
 package racingcar.exception;
 
+import racingcar.RacingCar;
+
 public class RacingCarNameOverSizeException extends RuntimeException {
 
-    private static final int MAX_RACING_CAR_NAME_SIZE = 5;
-
     public RacingCarNameOverSizeException() {
-        this(String.format("자동차 이름은 최대 길이인 %d를 넘길 수 없습니다.", MAX_RACING_CAR_NAME_SIZE));
+        this(String.format("자동차 이름은 최대 길이인 %d를 넘길 수 없습니다.", RacingCar.MAX_NAME_LENGTH));
     }
 
     public RacingCarNameOverSizeException(String msg) {
