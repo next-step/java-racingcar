075fc5215021fe69cf68fe7743b8a77ed0901f23
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2022-09-30 12:01:42 +0900

    refactor: RacingRuleDefalut 안의 상수 final 선언 및 대문자로 수정

--- src/main/java/racingGame/racingRule/RacingRuleDefault.java
+++ src/main/java/racingGame/racingRule/RacingRuleDefault.java
@@ -2,12 +2,13 @@ package racingGame.racingRule;
 
 import java.util.Random;
 
-public class RacingRuleDefault implements RacingRule{
-    private static  int maxNum = 10;
-    private static  int limitNum = 4;
-    private static Random random = new Random();
+public class RacingRuleDefault implements RacingRule {
+    private static final int MAXNUM = 10;
+    private static final int LIMIT = 4;
+    private static final Random RANDOM = new Random();
+
     @Override
     public boolean createRandomResult() {
-        return random.nextInt(maxNum) >= limitNum;
+        return RANDOM.nextInt(MAXNUM) >= LIMIT;
     }
 }
