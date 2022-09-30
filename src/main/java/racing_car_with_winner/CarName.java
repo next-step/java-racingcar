2fe96fdc15834e1933dcd80fb3c23d5ad5ce8d9f
owen.qq <owen.qq@kakaocorp.com>
2022-09-30 09:52:01 +0900

    feat: 자동차 이름을 가지는 CarName 객체 분리
    - test: CarName 클래스 분리에 따른 tc 보완

---
+++ src/main/java/racing_car_with_winner/CarName.java
@@ -0,0 +1,19 @@
+package racing_car_with_winner;
+
+import racing_car_with_winner.exception.InvalidInputException;
+
+public class CarName {
+    private String value;
+
+    public CarName(String value) {
+        if (value == null || value.isBlank()) {
+            throw new InvalidInputException("자동차의 이름은 공백을 가질 수 없습니다");
+        }
+
+        if (value.length() > 5) {
+            throw new InvalidInputException("자동차의 이름은 5자를 초과할 수 없습니다");
+        }
+
+        this.value = value;
+    }
+}
---
+++ src/main/java/racing_car_with_winner/exception/InvalidInputException.java
@@ -0,0 +1,7 @@
+package racing_car_with_winner.exception;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
