896250dd527adc6ca2ca34e6401943d7f7c8bee7
Ye-Ji-Kim <kyeoji0128@naver.com>
2022-09-30 18:19:21 +0900

    test: 테스트 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,11 +3,21 @@ package racingcar;
 public class Car {
     private int position = 0;
 
+    public Car() {}
+
+    public Car(int position) {
+        this.position = position;
+    }
+
     public void forward() {
         position += 1;
     }
 
     public String getPositionString() {
         return "-".repeat(position);
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
