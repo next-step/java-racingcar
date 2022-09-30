c016cf9a3d979f9b4afb6541dc683e2af0193038
Mny <orc1226@naver.com>
2022-09-30 23:26:08 +0900

    Step2 문자열 덧셈 계산기 (#3744)
    * CsvSource 기본 delimiter 사용
    
    * 업데이트되지 않은 DisplayName 수정
    
    * feat: Positive, StringAddCalculator 구현
    
    - 정수를 담는 mutable Positive 클래스 구현
    - StringAddCalculator 구현
      - ',', ':'를 delimiter로 사용해 문자에 포함된 숫자의 합을 계산
    
    * feat: StringAddCalculator 커스텀 구분자 기능 구현
    
    * StringAddCalculator를 생성자로 만드는 방식으로 변경
    
    * input의 delimiter 처리 부분 분리
    
    Co-authored-by: mnyok <>

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,58 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    public static final String DEFAULT_DELIMITER = "[,:]";
+    public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\\n(.*)");
+    private String input;
+    private String delimiter = DEFAULT_DELIMITER;
+
+    public StringAddCalculator(String input) {
+        this.input = input;
+    }
+
+    public int calculate() {
+        if (isBlank(input)) {
+            return 0;
+        }
+
+        setCustomDelimiter();
+
+        return sum(toInts(split(input)));
+    }
+
+    private boolean isBlank(String input) {
+        return input == null || input.isBlank();
+    }
+
+    private void setCustomDelimiter() {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+        if (matcher.matches()) {
+            delimiter = matcher.group(1);
+            input = matcher.group(2);
+        }
+    }
+
+    private String[] split(String input) {
+        return input.split(delimiter);
+    }
+
+    private Positive[] toInts(String[] values) {
+        Positive[] numbers = new Positive[values.length];
+        for (int i = 0; i < values.length; i++) {
+            String value = values[i];
+            numbers[i] = new Positive(value);
+        }
+        return numbers;
+    }
+
+    private int sum(Positive[] numbers) {
+        int result = 0;
+        for (Positive number: numbers) {
+            result += number.get();
+        }
+        return result;
+    }
+}
---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,71 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class StringAddCalculatorTest {
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {""})
+    @DisplayName("빈 값이나 null을 calculate했을 때 0을 반환한다")
+    void null_or_space(String input) {
+        int result = new StringAddCalculator(input).calculate();
+
+        assertThat(result).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "4"})
+    @DisplayName("1 개의 숫자 입력시 해당 값을 그대로 반환")
+    void single_number(String input) {
+        int result = new StringAddCalculator(input).calculate();
+
+        assertThat(result).isEqualTo(Integer.parseInt(input));
+    }
+
+    @Test
+    @DisplayName("음수 입력시 예외를 던짐")
+    void minus_number() {
+        String input = "-1";
+
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> {
+                    new StringAddCalculator(input).calculate();
+                });
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2 3", "5,4 9", "11,63 74"}, delimiter = ' ')
+    @DisplayName(", 구분자로 여러 개의 숫자 입력")
+    void multi_number_(String input, int expected) {
+        int result = new StringAddCalculator(input).calculate();
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"6:4 10", "1:1 2", "0:62 62"}, delimiter = ' ')
+    @DisplayName(": 구분자로 여러 개의 숫자 입력")
+    void multi_number_colon(String input, int expected) {
+        int result = new StringAddCalculator(input).calculate();
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("입력된 delimiter를 사용해 계산")
+    void custom_delimiter() {
+        String input = "//;\n1;2;3";
+
+        int result = new StringAddCalculator(input).calculate();
+
+        assertThat(result).isEqualTo(6);
+    }
+}
