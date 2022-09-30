931eafdd62fb4355429c2517c954a40f90d5e80c
jiokim <jiokim222@gmail.com>
2022-09-30 22:18:21 +0900

    test(car): 전략패턴에 의한 동작 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,35 +1,35 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
-    @Test
-    @DisplayName("랜덤숫자가 4일때 move")
-    void car_move_test(){
-        Car car = new Car(){
-              @Override
-              protected int generateRandomNumber(){
-                  return 4;
-             }
-        };
-        car.move();
-        assertThat(car.getPosition()).isEqualTo(1);
+    private Car car;
+    private static final int INITIAL_POSITION = 0;
+
+    @BeforeEach
+    private void setUp() {
+        car = new Car();
     }
 
-    @Test
-    @DisplayName("랜덤숫자가 2일때 stop")
-    void car_not_move_test(){
-        Car car = new Car(){
-            @Override
-            protected int generateRandomNumber(){
-                return 2;
-            }
-        };
-        car.move();
-        assertThat(car.getPosition()).isEqualTo(0);
+    @ParameterizedTest
+    @DisplayName("랜덤숫자가 4이상 move")
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void car_move_test(int input) {
+        car.move(new RandomMoveStrategy(input));
+        assertThat(car.getPosition()).isEqualTo(INITIAL_POSITION + 1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("랜덤숫자가 4이하 스탑")
+    @ValueSource(ints = {1, 2, 3})
+    void car_not_move_test(int input) {
+        car.move(new RandomMoveStrategy(input));
+        assertThat(car.getPosition()).isEqualTo(INITIAL_POSITION);
     }
 }
