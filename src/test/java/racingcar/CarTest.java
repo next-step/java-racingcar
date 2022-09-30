e280493dbde6f202ccace7720d02404cbeaa9729
yunhwan <pyh8618@gmail.com>
2022-09-30 20:57:05 +0900

    test: Car 테스트코드 추가
    * 자동차 5자 제한
    * move 메서드 이동 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,22 @@ class CarTest {
     void setUp() {
         car = new Car("test");
     }
+
+    @Test
+    public void 자동차_이름_제한_예외() {
+        assertThatThrownBy(() -> new Car("abcdefg"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("5자를 초과");
+    }
+
+    @Test
+    public void 자동차_이름_제한_통과() {
+        Car car = new Car("abcde");
+        assertThat(car)
+                .extracting("name")
+                .isEqualTo("abcde");
+    }
+
     @Test
     public void position_증가() {
         testMovingStrategy = new TestSuccessMoveStrategy();
@@ -23,7 +40,7 @@ class CarTest {
     }
 
     @Test
-    public void 포지션변화_실패() {
+    public void 포지션증가_실패() {
         testMovingStrategy = new TestFailMoveStrategy();
         car.move(testMovingStrategy);
         assertThat(car.getPosition()).isEqualTo(0);
