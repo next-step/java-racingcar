3fc8c42258bbee220224503a9753bf708980e5aa
yunhwan <pyh8618@gmail.com>
2022-09-30 22:06:27 +0900

    test: RoundCar 테스트
    * 생성자 파라미터 List시 생성 테스트
    * moveCars 움직임 테스트 (With testStrategy)
    * 우승자 도출 테스트 작성
    * deepCopy 후 서로다른 인스턴스인지 테스트

--- src/test/java/racingcar/RoundCarsTest.java
+++ src/test/java/racingcar/RoundCarsTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -58,6 +59,41 @@ class RoundCarsTest {
         assertThat(cars)
                 .map(Car::getPosition)
                 .containsExactly(0, 0, 0);
+    }
+
+    @Test
+    void 우승자_도출_테스트() {
+        RacingRecord racingRecord = new RacingRecord();
+        MovingStrategy movingStrategy = new TestSuccessMoveStrategy();
+
+        roundCars.moveCars(racingRecord, movingStrategy);
+        roundCars.moveCars(racingRecord, movingStrategy);
+        List<Car> cars = roundCars.retrieveCars();
+        Car winnerCar = cars.get(1);
+        winnerCar.move(movingStrategy);
+
+        List<String> winners = roundCars.retrieveWinners();
+        assertThat(winners)
+                .containsExactly(winnerCar.getName());
+    }
+
+    @Test
+    void deepCopy_서로_다른_인스턴스인지() {
+        RoundCars copyRoundCars = roundCars.deepCopy();
+        List<Car> cars = roundCars.retrieveCars();
+        List<Car> copyCar = copyRoundCars.retrieveCars();
+
+        Assertions.assertAll(
+                () -> assertThat(roundCars)
+                        .as("서로 다른 RoundCar인스턴스 인지")
+                        .isNotEqualTo(copyRoundCars),
+                () -> assertThat(cars)
+                        .as("서로 다른 List인스턴스 인지")
+                        .isNotEqualTo(copyCar),
+                () -> assertThat(cars)
+                        .as("서로 다른 각각의 Car인스턴스 인지")
+                        .doesNotContain(copyCar.toArray(new Car[0]))
+        );
     }
 
 }
