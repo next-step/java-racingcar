0c8ecf5333ebaf311df39931f1f38116c2d3a633
gumsu <chavi9@naver.com>
2022-09-30 19:45:23 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/racinggame/domain/Car.java
+++ src/main/java/racinggame/domain/Car.java
@@ -15,11 +15,7 @@ public class Car {
     public Car(String name, int status) {
         this(name);
         this.status = status;
-    }
-
-    public void forward() {
-        this.status++;
-    }
+    }거
 
     public int getStatus() {
         return status;
