7493fba072b951f64373475e0f13316da6d115f8
gkfgran5037 <gkfgran5037@naver.com>
2022-09-30 17:30:58 +0900

    Add constant
    2단계 피드백 반영 : 상수화, 배열 -> 리스트

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,10 +1,16 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    public static String SEPARATOR = ",|:";
+    private static final Pattern TEXT_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String BASIC_SEPARATOR = ",|:";
+    private static final int CUSTOM_SEPARATOR_GROUP = 1;
+    private static final int USER_TEXT_GROUP = 2;
+
     public static int splitAndSum(String text) {
         if (isBlank(text)) {
             return 0;
@@ -13,7 +19,7 @@ public class StringAddCalculator {
     }
 
     private static String[] split(String text) {
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher matcher = TEXT_PATTERN.matcher(text);
         if (matcher.find()) {
             return customSplit(matcher);
         }
@@ -21,15 +27,15 @@ public class StringAddCalculator {
     }
 
     private static String[] customSplit(Matcher matcher) {
-        String customDelimiter = matcher.group(1);
-        return matcher.group(2).split(customDelimiter);
+        String customDelimiter = matcher.group(CUSTOM_SEPARATOR_GROUP);
+        return matcher.group(USER_TEXT_GROUP).split(customDelimiter);
     }
 
     private static String[] basicSplit(String text) {
-        return text.split(SEPARATOR);
+        return text.split(BASIC_SEPARATOR);
     }
 
-    private static int sum(int[] numbers) {
+    private static int sum(List<Integer> numbers) {
         int sum = 0;
         for (int number : numbers) {
             sum += number;
@@ -37,10 +43,10 @@ public class StringAddCalculator {
         return sum;
     }
 
-    private static int[] toInts(String[] tokens) {
-        int[] numbers = new int[tokens.length];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = positive(tokens[i]);
+    private static List<Integer> toInts(String[] tokens) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String token : tokens) {
+            numbers.add(positive(token));
         }
         return numbers;
     }
