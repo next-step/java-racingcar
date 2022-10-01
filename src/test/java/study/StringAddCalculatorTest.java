63d0321fbdc0134722054405d41310bbfbe962ee
leesoonyong <44168355+leesoonyong@users.noreply.github.com>
2022-10-01 21:44:58 +0900

    테스트 코드 수정

--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -2,19 +2,21 @@ package study;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class StringAddCalculatorTest {
 
-
     StringAddCalculator stringAddCalculator = new StringAddCalculator();
-    @Test
-    public void splitAndSum_null_또는_빈문자(){
-        int result = stringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = stringAddCalculator.splitAndSum("");
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    public void splitAndSum_null_또는_빈문자(String data){
+        int result = stringAddCalculator.splitAndSum(data);
         assertThat(result).isEqualTo(0);
     }
 
@@ -30,9 +32,10 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(3);
     }
 
-    @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception{
-        int result = stringAddCalculator.splitAndSum("1,2:3");
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2:3", "1,2,3", "1:2:3"})
+    public void splitAndSum_쉼표_또는_콜론_구분자(String data) throws Exception{
+        int result = stringAddCalculator.splitAndSum(data);
         assertThat(result).isEqualTo(6);
     }
 
@@ -44,7 +47,9 @@ public class StringAddCalculatorTest {
 
     @Test
     public void splitAndSum_음수_입력시_에러발생() throws Exception{
-        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3")).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageStartingWith("음수가 들어왔습니다");
     }
 
 
