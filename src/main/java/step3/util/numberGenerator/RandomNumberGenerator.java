ae08314ec769c722950a0be10b88a63d30209977
김남혁 <nhkim25@lotte.net>
2022-10-01 14:23:38 +0900

    refactor : 3단계 3차 리뷰에 의거한 리팩토링

--- src/main/java/step3/util/numberGenerator/RandomNumberGenerator.java
+++ src/main/java/step3/util/numberGenerator/RandomNumberGenerator.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
     private static final Random random = new Random();
+    private static final int MAX_BOUND_NUMBER = 10;
 
     @Override
-    public int generate(int maxBound) {
-        if(maxBound > 10) throw new RuntimeException("경주에 필요한 값의 생성 범위를 벗어났습니다.");
-        return random.nextInt(maxBound);
+    public int generate(int boundNumber) {
+        if(boundNumber > MAX_BOUND_NUMBER) throw new RuntimeException("경주에 필요한 값의 생성 범위를 벗어났습니다.");
+        return random.nextInt(boundNumber);
     }
 }
