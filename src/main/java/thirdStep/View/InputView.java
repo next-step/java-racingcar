6c1c36f1e6295e17a3596f4695334d9a5843ebb3
isakchoe <isakchoe@naver.com>
2022-10-01 16:13:39 +0900

    feat (InputView): add get method
    -getCarCount
    -getRaceCount

---
+++ src/main/java/thirdStep/View/InputView.java
@@ -0,0 +1,28 @@
+package thirdStep.View;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
+    private static int carCount;
+    private static int raceCount;
+
+    public static void inputCarCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        carCount = scanner.nextInt();
+    }
+
+    public static void inputRaceCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        raceCount = scanner.nextInt();
+    }
+
+    public static int getCarCount() {
+        return carCount;
+    }
+
+    public static int getRaceCount() {
+        return raceCount;
+    }
+
+}
