f65d7476f69c3c11e230a9225c9ef36e9a36102f
kureungkureung <terry9611@naver.com>
2022-10-02 08:37:25 +0900

    refactor(RandomNumber): 랜덤 숫자 생성 수정

--- src/main/java/step3/number/RandomNumber.java
+++ src/main/java/step3/number/RandomNumber.java
@@ -1,16 +1,29 @@
 package step3.number;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class RandomNumber implements Number {
 
-    private static final Random RANDOM = new Random();
+    private static final int RANDOM_BOUND = 10;
 
     @Override
     public int generatedNumber() {
-        return RANDOM.ints(0, 10)
-                .findFirst()
-                .orElse(0);
+        return ThreadLocalRandom.current()
+                .nextInt(RANDOM_BOUND);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return isRandomNumber(obj);
+    }
+
+    private boolean isRandomNumber(Object object) {
+        return object instanceof RandomNumber;
     }
 
 }
