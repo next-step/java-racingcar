b1669030f62337ae3369d8c80cf196780d2a562f
kureungkureung <terry9611@naver.com>
2022-10-02 08:51:42 +0900

    test(RacingCarsTest): 필요없는 테스트 삭제

--- src/test/java/step3/cars/RacingCarsTest.java
+++ src/test/java/step3/cars/RacingCarsTest.java
@@ -7,43 +7,21 @@ import step3.car.RacingCar;
 import step3.number.Number;
 import step3.position.CarPosition;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingCarsTest {
 
     @Test
-    @DisplayName("자동차 등록하면 등록된 자동차 리스트들이 나온다.")
-    void a() {
-        final Number number = new Number.Fake(4);
-        final Car car = new RacingCar(number);
-
-        final Cars sut = new RacingCars();
-
-        assertThat(sut.carsWithAddCar(car)).isEqualTo(new RacingCars().carsWithAddCar(car));
-    }
-
-    @Test
-    @DisplayName("자동차들을 등록 해제하면 해제된 자동차 리스트들이 나온다.")
-    void b() {
-        final Number number = new Number.Fake(4);
-        final Car car = new RacingCar(number);
-        final Cars cars = new RacingCars();
-
-        final Cars sut = cars.carsWithAddCar(car);
-
-        assertThat(sut.carsWithRemoveCar(car)).isEqualTo(new RacingCars());
-    }
-
-    @Test
     @DisplayName("자동차들에게 턴을 사용할 있게 알람을 줄 수 있다.")
-    void c() {
-        final Number number = new Number.Fake(4);
-        final Car car = new RacingCar(number);
-        final Cars cars = new RacingCars();
+    void a() {
+        Number number = new Number.Fake(4);
+        Car car = new RacingCar(number);
+        Cars sut = new RacingCars(List.of(car));
 
-        final Cars sut = cars.carsWithAddCar(car);
-
-        assertThat(sut.carsWithTurnUsed()).isEqualTo(cars.carsWithAddCar(new RacingCar(new CarPosition(1), number)));
+        Car movedCar = new RacingCar(new CarPosition(1), number);
+        assertThat(sut.carsWithTurnUsed()).isEqualTo(new RacingCars(List.of(movedCar)));
     }
 
 }
