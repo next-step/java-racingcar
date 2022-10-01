cce3a21d88bc4ef6e85eecaa5f00556f18e8c0a5
jimbaemon <jimbae@kakao.com>
2022-10-01 16:28:27 +0900

    feat: 생성자, Equals and HashCode 구현.

--- src/main/java/racing/RaceResult.java
+++ src/main/java/racing/RaceResult.java
@@ -2,11 +2,41 @@ package racing;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class RaceResult {
-    private final List<Integer> locations = new ArrayList<>();
+    private final List<Integer> carLocations;
 
-    public void addResult(final int location){
-        locations.add(location);
+    public RaceResult() {
+        this.carLocations = new ArrayList<>();
+    }
+
+    public RaceResult(List<Integer> carLocations) {
+        this.carLocations = carLocations;
+    }
+
+    public void addCarLocation(final int location){
+        carLocations.add(location);
+    }
+
+    public List<Integer> carLocations(){
+        return carLocations;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RaceResult result = (RaceResult) o;
+        return Objects.equals(carLocations, result.carLocations);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carLocations);
     }
 }
--- src/main/java/racing/ScoreBoard.java
+++ src/main/java/racing/ScoreBoard.java
@@ -2,11 +2,41 @@ package racing;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ScoreBoard {
-    private final List<RaceResult> raceResults = new ArrayList<>();
+    private final List<RaceResult> raceResults;
+
+    public ScoreBoard() {
+        this.raceResults = new ArrayList<>();
+    }
+
+    public ScoreBoard(List<RaceResult> raceResults) {
+        this.raceResults = raceResults;
+    }
 
     public void addResult(final RaceResult raceResult){
         raceResults.add(raceResult);
+    }
+
+    public List<RaceResult> raceResults(){
+        return raceResults;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ScoreBoard that = (ScoreBoard) o;
+        return Objects.equals(raceResults, that.raceResults);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(raceResults);
     }
 }
