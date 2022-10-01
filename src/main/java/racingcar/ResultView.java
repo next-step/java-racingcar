d019f19d56d07503dd11d736a075b0f47675eebf
scoopsoup <rnrdud46@nate.com>
2022-10-01 11:08:15 +0900

    feat : InputView 한번만 인스턴스화 하여 사용하기

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 public class ResultView {
     public static void main(String[] args) {
+        InputView inputView = new InputView();
         System.out.println("자동차 수를 입력하세요.");
-        int numberOfCar = new InputView().getNumberOfCar();
+        int numberOfCar = inputView.getNumberOfCar();
         System.out.println("이동횟수를 입력하세요.");
-        int carMovingNum = new InputView().getCarMovingNum();
+        int carMovingNum = inputView.getCarMovingNum();
         RacingCar racingCar = new RacingCar(numberOfCar);
         List<String> carStatus;
         for (int i = 0; i < carMovingNum; i++) {
