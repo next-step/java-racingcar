d2f9d1371c6d501f756aa7a475bd1d0aefff4deb
wugatama <dnr7410@naver.com>
2022-10-02 00:08:21 +0900

    refactor: 메소드 정리

--- src/main/java/racingcar/Print.java
+++ src/main/java/racingcar/Print.java
@@ -7,16 +7,8 @@ public class Print {
 		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 	}
 
-	public static void howManyCars() {
-		System.out.println("자동차 대수는 몇 대 인가요?");
-	}
-
 	public static void howManyMatches() {
 		System.out.println("시도할 회수는 몇 회 인가요?");
-	}
-
-	public static void resultOfRound(List<Integer> status) {
-		System.out.println(status);
 	}
 
 	public static void newLine() {
