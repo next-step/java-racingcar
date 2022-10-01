39d39fd7ec26b2ff770cfe7bef833b51457a9408
jimbaemon <jimbae@kakao.com>
2022-10-01 14:55:04 +0900

    refactor: 조건절 함수화 하여 의미부여.

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -19,6 +19,10 @@ public class Car implements Comparator<Car> {
     }
 
     private boolean isPossibleToMove() {
+        return doesRandomNumberAboveTargetNumber();
+    }
+
+    private boolean doesRandomNumberAboveTargetNumber() {
         Random random = new Random();
         return random.nextInt(RANDOM_BOUND) >= TARGET_NUMBER;
     }
