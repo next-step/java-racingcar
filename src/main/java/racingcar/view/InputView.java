bc2db5c4b22796e9a2e1fc3b209fa296c532103f
0chil <0@chll.it>
2022-10-01 15:19:27 +0900

    refactor(InputView): 양수만 허용

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,18 +8,27 @@ public class InputView {
 	private static final Scanner SCANNER = new Scanner(System.in);
 
 	public int readCarCount() {
-		return readInt();
+		return readPositive();
 	}
 
 	public int readTrialCount() {
-		return readInt();
+		return readPositive();
+	}
+
+	private int readPositive() {
+		int input = readInt();
+		if (input <= 0) {
+			throw new InputMismatchException("양수를 입력해야 합니다");
+		}
+		return input;
 	}
 
 	private int readInt() {
 		try {
 			return SCANNER.nextInt();
-		} catch (InputMismatchException exception) {
-			throw new InputMismatchException("숫자만 입력해야 합니다");
+		}
+		catch (InputMismatchException exception){
+			throw new InputMismatchException("정수를 입력해야 합니다");
 		}
 	}
 }
