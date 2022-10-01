faf79a2554ef5575d11757fdfc5a795cdb807bfd
kureungkureung <terry9611@naver.com>
2022-10-02 08:37:08 +0900

    feat(BarOutputView): 동등성 비교 기능

--- src/main/java/step3/output/BarOutputView.java
+++ src/main/java/step3/output/BarOutputView.java
@@ -8,5 +8,19 @@ public class BarOutputView implements OutputView {
     public void print(int count) {
         System.out.println(BAR.repeat(count));
     }
-    
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return isBarOutputView(obj);
+    }
+
+    private boolean isBarOutputView(Object object) {
+        return object instanceof BarOutputView;
+    }
+
 }
