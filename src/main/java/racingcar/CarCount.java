24aab8028fc5c0c87dafd7a189f93913277d3ebc
charlotte.chk <charlotte.chk@kakaocorp.com>
2022-10-02 01:38:13 +0900

    refactor(Cars): change class name
    change class name from Cars to CarCount for meaning clear

--- src/main/java/racingcar/CarCount.java
+++ src/main/java/racingcar/CarCount.java
@@ -1,10 +1,16 @@
 package racingcar;
 
-public class Cars {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarCount {
     public static final int MIN_COUNT = 1;
 
-    public Cars(int count) {
+    private final int count;
+
+    public CarCount(int count) {
         validateCountOverMinimum(count);
+        this.count = count;
     }
 
     private void validateCountOverMinimum(int count) {
