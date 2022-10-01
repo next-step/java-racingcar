956e5569d88684cf986db8346fd3c8abdb948199
min16 <minji.na@cre.ma>
2022-10-01 17:10:55 +0900

    EDIT - `#inputPositive` 양수가 아니면 에러 던지기

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String CAR_COUNT_TEXT = "자동차 대수는 몇 대 인가요?";
     private static final String ROUND_COUNT_TEXT = "시도할 회수는 몇 회 인가요?";
+    private static final String ONLY_POSITIVE_VALID = "양수만 입력 가능합니다.";
 
     private static Scanner scanner = new Scanner(System.in);
 
@@ -19,7 +20,7 @@ public class InputView {
     private static int inputPositive(String question) {
         System.out.println(question);
         int value = scanner.nextInt();
-        if (value < 1) { value = 1; }
+        if (value < 1) { throw new RuntimeException(ONLY_POSITIVE_VALID); }
 
         return value;
     }
