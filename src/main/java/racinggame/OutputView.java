0c13e63314ecf72d010b97e3b273d5d01ffe8ccc
jhwlim <jhwlim25@gmail.com>
2022-10-01 16:42:39 +0900

    feat : 자동차 경주 우승자 이름 포함하여 결과 출력하도록 수정

--- src/main/java/racinggame/OutputView.java
+++ src/main/java/racinggame/OutputView.java
@@ -1,5 +1,7 @@
 package racinggame;
 
+import java.util.List;
+
 public class OutputView {
 
     private OutputView() {}
@@ -7,6 +9,9 @@ public class OutputView {
     public static void printResult(RacingGameResult result) {
         System.out.println("실행 결과");
         System.out.println(result.getContent());
+
+        List<String> winners = result.getWinnerNames();
+        System.out.printf("%s가 최종 우승했습니다.", String.join(StringConstants.COMMA + StringConstants.SPACE, winners));
     }
 
 }
--- src/main/java/racinggame/StringConstants.java
+++ src/main/java/racinggame/StringConstants.java
@@ -4,6 +4,7 @@ public class StringConstants {
 
     public static final String NEW_LINE = "\n";
     public static final String COMMA = ",";
+    public static final String SPACE = " ";
 
     private StringConstants() {}
 
