ade10d02cdfd15a3ff9048c1e38f8e498cb9b8c9
jimbaemon <jimbae@kakao.com>
2022-10-01 16:10:11 +0900

    test: 경기결과 List 에서 객체로 변경 테스트.

--- src/test/java/racing/RaceTest.java
+++ src/test/java/racing/RaceTest.java
@@ -13,14 +13,26 @@ class RaceTest {
     void 자동차_경주_결과_테스트() {
         Race race = new Race(3, 5, new NoRule());
         race.play();
-        ArrayList<ArrayList<Integer>> result = race.result();
-        assertThat(result).isEqualTo(
-                List.of(
-                        List.of(1, 1, 1),
-                        List.of(2, 2, 2),
-                        List.of(3, 3, 3),
-                        List.of(4, 4, 4),
-                        List.of(5, 5, 5)
+        ScoreBoard scoreBoard = race.result();
+        assertThat(scoreBoard).isEqualTo(
+                new ScoreBoard(
+                        List.of(
+                                new RaceResult(
+                                        List.of(1, 1, 1)
+                                ),
+                                new RaceResult(
+                                        List.of(2, 2, 2)
+                                ),
+                                new RaceResult(
+                                        List.of(3, 3, 3)
+                                ),
+                                new RaceResult(
+                                        List.of(4, 4, 4)
+                                ),
+                                new RaceResult(
+                                        List.of(5, 5, 5)
+                                )
+                        )
                 )
         );
     }
