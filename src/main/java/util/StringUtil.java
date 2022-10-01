31ad81641086dc27894d528d34e6a19930c5bf4d
annelguplus <annelguplus@gmail.com>
2022-10-01 23:48:16 +0900

    refactor: 2단계 문자열 덧셈 계산기 리팩토링

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -2,7 +2,7 @@ package util;
 
 public class StringUtil {
     public static String parse(String string) {
-        return string.substring(1, string.length()-1);
+        return string.substring(1, string.length() - 1);
     }
 
 }
---
+++ src/test/java/util/PositiveTest.java
@@ -0,0 +1,38 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PositiveTest {
+
+    @Test
+    public void init() {
+        assertThat(new Positive(1)).isNotNull();
+
+        assertThatThrownBy(() -> new Positive(-1))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("음수값은 허용되지 않습니다.");
+    }
+
+    @Test
+    public void 덧셈_숫자() {
+        Positive numberOne = new Positive(1);
+        int numberTwo = 2;
+
+        int expected = 3;
+
+        assertThat(numberOne.plus(numberTwo)).isEqualTo(expected);
+    }
+
+    @Test
+    public void 덧셈_Positive객체() {
+        Positive numberOne = new Positive(1);
+        Positive numberTwo = new Positive(2);
+
+        Positive expected = new Positive(3);
+
+        assertThat(numberOne.plus(numberTwo)).isEqualTo(expected);
+    }
+}
