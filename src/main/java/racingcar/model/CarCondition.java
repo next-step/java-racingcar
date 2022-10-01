ec44116d2fc652643a13232af9c99a10624f845f
jungwoon727 <jungwoon727@gmail.com>
2022-10-01 11:20:26 +0900

    test: CarCondition 테스트 추가
    조건의 경계값을 테스트 하기위한 수정

--- src/main/java/racingcar/model/CarCondition.java
+++ src/main/java/racingcar/model/CarCondition.java
@@ -4,12 +4,15 @@ import java.util.Random;
 
 public class CarCondition implements Condition {
 
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
     private static final int BOUND = 10;
     private static final int CONDITION = 4;
 
     public boolean inspect() {
-        return random.nextInt(BOUND) >= CONDITION;
+        return random() >= CONDITION;
     }
 
+    protected int random() {
+        return RANDOM.nextInt(BOUND);
+    }
 }
