1fc8d7809e34d3ef6ed0a23164df4f412398707e
min16 <minji.na@cre.ma>
2022-10-01 16:53:11 +0900

    REFACTOR - `#play` -> `#start`로 rename
     ### Desc
       - 추가 수정: 더이상 사용하지 않는 rounds 인스턴스 변수 제거

--- src/main/java/racing/CarRacing.java
+++ src/main/java/racing/CarRacing.java
@@ -4,19 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarRacing {
-    private int rounds = 0;
     private List<Car> cars = new ArrayList<>();
 
     public static void main(String[] args) {
         CarRacing carRacing = new CarRacing();
-        carRacing.play();
+        carRacing.start();
     }
 
-    private void play() {
+    private void start() {
         prepare();
         int rounds = InputView.inputRounds();
-
         ResultView.printResultTitle();
+        play(rounds);
+    }
+
+    private void play(int rounds) {
         for (int i = 1; i <= rounds; i++) {
             playRound();
             announceResult(i);
