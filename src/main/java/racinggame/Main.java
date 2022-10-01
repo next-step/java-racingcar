8474aff8439998343d40042ef13812a296cdb814
jhwlim <jhwlim25@gmail.com>
2022-10-01 14:54:54 +0900

    feat : 자동차 이름을 입력 받도록 입력 화면 수정

--- src/main/java/racinggame/Main.java
+++ src/main/java/racinggame/Main.java
@@ -1,12 +1,14 @@
 package racinggame;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        int numOfCar = InputView.inputNumberOfCar();
+        List<String> carNames = InputView.inputCarNames();
         int tryCount = InputView.inputTryCount();
         RacingGame game = new RacingGame(new RandomNumberGenerator());
-        RacingGameResult result = game.run(numOfCar, tryCount);
+        RacingGameResult result = game.run(carNames.size(), tryCount);
         OutputView.printResult(result);
     }
 
--- src/main/java/racinggame/StringConstants.java
+++ src/main/java/racinggame/StringConstants.java
@@ -3,6 +3,7 @@ package racinggame;
 public class StringConstants {
 
     public static final String NEW_LINE = "\n";
+    public static final String COMMA = ",";
 
     private StringConstants() {}
 
