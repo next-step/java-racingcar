a6999ed1ce370a0d92dee9c6770a780c0ce9744d
michol.lee <lovethefeel@gmail.com>
2022-10-02 21:28:09 +0900

    feat: 코드리뷰 반영 - 메인 로직 클래스명 변경

--- src/main/java/racing/RacingCarGameApplication.java
+++ src/main/java/racing/RacingCarGameApplication.java
@@ -1,12 +1,12 @@
-package racing.controller;
+package racing;
 
 import racing.domain.*;
 import racing.view.InputView;
 import racing.view.ResultView;
 
-public class RacingCarsPlay {
+public class RacingCarGameApplication {
 
-    public void start() {
+    public static void main(String[] args) {
 
         final RacingCars racingCars = RacingCars.from(CarNames.from(InputView.carName()));
         final RacingAttempt racingAttempt = new RacingAttempt(InputView.playCount());
