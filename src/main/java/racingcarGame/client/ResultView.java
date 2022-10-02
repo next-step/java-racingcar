cad0ca4755bc26415efb0acbf5c5206aaaf8503b
bella.n <bella.n@kakaocorp.com>
2022-10-03 00:12:30 +0900

    refactor: step3의 리뷰 반영
    - pickRandomlyNumber 메소드 제거
    - 자동차 이동 테스트 코드 움직이는 경우와 움직이지 않는 경우롤 분리
    - 축약어 수정
    - String#repeat 반영

--- src/main/java/racingcarGame/client/ResultView.java
+++ src/main/java/racingcarGame/client/ResultView.java
@@ -5,7 +5,7 @@ import racingcarGame.model.Car;
 import java.util.List;
 
 public class ResultView {
-    private static final String POSITION_STR = "-";
+    private static final String POSITION_DASH = "-";
     private static final String SEPARATE_COLON = " : ";
 
     public static void show(String message) {
@@ -21,15 +21,7 @@ public class ResultView {
     }
 
     public static void showResult(List<Car> cars) {
-        cars.forEach(car -> {
-            String movement = "";
-            for (int i = 0; i < car.getPosition(); i++) {
-                movement += POSITION_STR;
-            }
-
-            show(car.getName() + SEPARATE_COLON + movement);
-        });
-
+        cars.forEach(car -> show(car.getName() + SEPARATE_COLON + POSITION_DASH.repeat(car.getPosition())));
         System.out.println();
     }
 }
--- src/main/java/racingcarGame/model/Car.java
+++ src/main/java/racingcarGame/model/Car.java
@@ -1,6 +1,6 @@
 package racingcarGame.model;
 
-import racingcarGame.util.RandomNumber;
+import java.util.Objects;
 
 public class Car {
     private static final int RANDOM_BASE_VALUE = 4;
@@ -19,10 +19,6 @@ public class Car {
         }
     }
 
-    public int pickRandomlyNumber() {
-        return RandomNumber.generate();
-    }
-
     boolean isMovable(int num) {
         return num >= RANDOM_BASE_VALUE;
     }
@@ -35,4 +31,17 @@ public class Car {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return position == car.position &&
+                Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, name);
+    }
 }
