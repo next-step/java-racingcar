cc8c6f10766093e7a04658cace5f968ae1e20f4f
leesoonyong <44168355+leesoonyong@users.noreply.github.com>
2022-10-02 14:07:45 +0900

    refactor 코드 띄어쓰기 수정

--- src/main/java/study/InputView.java
+++ src/main/java/study/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
     private static final String TRY_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+
     Scanner sc = new Scanner(System.in);
 
     public int carCount() {
