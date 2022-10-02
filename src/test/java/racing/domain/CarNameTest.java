0eda78fec0a247d1bfdffd96d60aa03e0dcda890
michol.lee <lovethefeel@gmail.com>
2022-10-02 23:49:12 +0900

    refactor(test): 기존 CarNames 메소드의 역할 이동

--- src/test/java/racing/domain/CarNameTest.java
+++ src/test/java/racing/domain/CarNameTest.java
@@ -3,8 +3,11 @@ package racing.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -37,6 +40,36 @@ class CarNameTest {
     void error_car_create2() {
 
         assertThatThrownBy(() -> new CarName("     "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 필수값입니다.");
+    }
+
+    @DisplayName("입력된 사용자 이름을 , 로 잘라서 목록으로 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"pobi:1", "pobi,:1", "pobi,crong:2", "pobi,crong,honux:3"}, delimiter = ':')
+    void input_name(final String input, final int size) {
+
+        final List<CarName> carNames = CarName.from(input);
+
+        assertThat(carNames).hasSize(size);
+    }
+
+    @DisplayName("입력된 자동차 이름이 빈 값이 포함되어 있다면 익셉션 처리한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {",pobi,", " ,pobi, "})
+    void error_input_name(final String input) {
+
+        assertThatThrownBy(() -> CarName.from(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 필수값입니다.");
+    }
+
+    @DisplayName("입력된 자동차 이름이 없으면 예외 처리를 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void error_input_null_empty(final String input) {
+
+        assertThatThrownBy(() -> CarName.from(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 필수값입니다.");
     }
