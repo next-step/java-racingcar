636cdd116ca78e760e5e64c9c4471d6679b3d37c
kkywalk2 <kkywalk2@gmail.com>
2022-10-03 02:48:19 +0900

    test: CarTest 추가

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    static int index = 0;
+
+    @ParameterizedTest
+    @CsvSource(value = {"3,4,9:2"}, delimiter = ':')
+    void carMoveTest(String given, int expected) {
+        String[] splitNumber = given.split(",");
+
+        Car car = new Car(() -> Integer.parseInt(splitNumber[index++]));
+        for (int i = 0; i < 3; i++) {
+            car.move();
+        }
+
+        assertThat(car.getCurrentPosition()).isEqualTo(expected);
+    }
+}
