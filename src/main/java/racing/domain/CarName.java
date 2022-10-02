8e42d6257c7df4a63affde52073ceeac877b437a
michol.lee <lovethefeel@gmail.com>
2022-10-02 23:49:01 +0900

    refactor: 기존 CarNames 메소드의 역할 이동

--- src/main/java/racing/domain/CarName.java
+++ src/main/java/racing/domain/CarName.java
@@ -1,6 +1,12 @@
 package racing.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CarName {
+
+    public static final String REGEX_COMMA = ",";
 
     private final String carName;
 
@@ -10,11 +16,24 @@ public class CarName {
         this.carName = carName;
     }
 
-    private void validate(final String name) {
+    public static List<CarName> from(final String name) {
+
+        validate(name);
+        return split(name);
+    }
+
+    private static void validate(final String name) {
 
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 필수값입니다.");
         }
+    }
+
+    private static List<CarName> split(final String name) {
+
+        return Arrays.stream(name.split(REGEX_COMMA))
+                .map(CarName::new)
+                .collect(Collectors.toList());
     }
 
     public String getCarName() {
