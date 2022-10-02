c281e14d0f161bd77206bafb58ce744a63e66b73
elmo-devv <ismyelmo@gmail.com>
2022-10-02 16:01:21 +0900

    feature: InputView 구현

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -1,2 +1,23 @@
-package step3;public class InputView {
+package step3;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public InputView() {
+    }
+
+    public int carNumber() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        return Integer.parseInt(value);
+    }
+
+    public int carRunningNumber() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        return Integer.parseInt(value);
+    }
 }
