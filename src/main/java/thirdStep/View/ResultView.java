0e10c7ccfd8e95549cdef3a0e44633aaa1ada0ab
isakchoe <isakchoe@naver.com>
2022-10-02 22:08:07 +0900

    refactor - ResultView
    makeStirng: 각 자동차의 정수 상태를 String 으로 바꿔주는 함수

--- src/main/java/thirdStep/View/ResultView.java
+++ src/main/java/thirdStep/View/ResultView.java
@@ -2,9 +2,17 @@ package thirdStep.View;
 
 import thirdStep.logic.Race;
 
+import java.util.stream.IntStream;
+
 public class ResultView {
 
-    public void printResult() {
+    public static void main(String[] args) {
+        InputView.inputCarCount();
+        InputView.inputRaceCount();
+        ResultView.printResult();
+    }
+
+    private static void printResult() {
         System.out.println("실행결과");
         for (int i = 0; i < InputView.getRaceCount(); i++) {
             printOneRace();
@@ -12,11 +20,17 @@ public class ResultView {
         }
     }
 
-    public void printOneRace() {
+    private static void printOneRace() {
         Race.startOneRace();
         int[] carStatus = Race.getCarStatus();
         for (int i = 0; i < carStatus.length; i++) {
-            System.out.println(carStatus[i] * '-');
+            System.out.println(makeString(carStatus[i]));
         }
+    }
+
+    private static StringBuilder makeString(int number) {
+        StringBuilder carMoveResult = new StringBuilder("-");
+        IntStream.range(0, number).mapToObj(i -> "-").forEach(carMoveResult::append);
+        return carMoveResult;
     }
 }
