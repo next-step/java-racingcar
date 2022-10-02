b30a589187ffaef36f52fd65c1cb609857cc13ba
Myunghwan Kim <samkim2626@gmail.com>
2022-10-02 21:05:14 +0900

    feat: 매 전진 시도마다 각 자동차 이름과 이동 상태 출력 기능 구현

--- src/main/java/racingcar/step3_4/Name.java
+++ src/main/java/racingcar/step3_4/Name.java
@@ -41,4 +41,9 @@ public class Name {
     public int hashCode() {
         return Objects.hash(name);
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
--- src/main/java/racingcar/step3_4/ResultView.java
+++ src/main/java/racingcar/step3_4/ResultView.java
@@ -9,6 +9,8 @@ public class ResultView {
 	public void printEachTryResult(List<Car> cars) {
 		StringBuilder stringBuilder = new StringBuilder();
 		for (Car car : cars) {
+			stringBuilder.append(car.getName());
+			stringBuilder.append(" : ");
 			stringBuilder.append(toDashMark(car.getPosition()));
 		}
 		System.out.println(stringBuilder);
