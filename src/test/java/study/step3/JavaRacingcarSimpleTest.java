84ef9350dff205be52bb57127059b117a11318ee
hocaron <kkannu0407@gmail.com>
2022-10-02 13:41:44 +0900

    test: 초간단 자동차 경주 게임 테스트 코드 작성

---
+++ src/test/java/study/step3/JavaRacingcarSimpleTest.java
@@ -0,0 +1,26 @@
+package study.step3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class JavaRacingcarSimpleTest {
+
+	final JavaRacingcarSimple javaRacingcarSimple = new JavaRacingcarSimple();
+
+	@DisplayName("random 값이 4 이상인 경우 true 를 반환한다")
+	@Test
+	void Given_MoreThen4_Then_ReturnTrue() {
+		boolean result = javaRacingcarSimple.isGoForward(4);
+		assertTrue(result);
+	}
+
+	@DisplayName("random 값이 4 미만인 경우 false 를 반환한다")
+	@Test
+	void Given_Under4_Then_ReturnFalse() {
+		boolean result = javaRacingcarSimple.isGoForward(3);
+		assertFalse(result);
+	}
+}
