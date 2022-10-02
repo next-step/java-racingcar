b307d9558f6851d8a9b81d89dea1c663b383a82a
gumsu <chavi9@naver.com>
2022-10-02 18:13:55 +0900

    style: 코드 컨벤션에 맞게 수정

--- src/main/java/racinggame/domain/RandomMovingStrategy.java
+++ src/main/java/racinggame/domain/RandomMovingStrategy.java
@@ -2,13 +2,16 @@ package racinggame.domain;
 
 import java.util.Random;
 
-public class RandomMovingStrategy implements MovingStrategy{
+public class RandomMovingStrategy implements MovingStrategy {
     private static final int MAX_POINT_TO_MOVE = 4;
     private static final int MAX_BOUND = 10;
 
     @Override
     public boolean movable() {
-        return generateRandomNumber()>= MAX_POINT_TO_MOVE;
+        if (generateRandomNumber() >= MAX_POINT_TO_MOVE) {
+            return true;
+        }
+        return false;
     }
 
     private int generateRandomNumber() {
