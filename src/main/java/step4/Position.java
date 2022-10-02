c0c375cea7732a9980966513afa9427834b03aa5
nhs219 <heesun_z@naver.com>
2022-10-02 14:20:49 +0900

    refactor : Position List -> Map으로 변경

---
+++ src/main/java/step4/Position.java
@@ -0,0 +1,27 @@
+package step4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Position {
+    private static final int INIT_NUM = 0;
+    private static final int MOVE = 1;
+
+    private Map<String, Integer> positions = new HashMap<>();
+
+    public void initPosition(List<String> names) {
+        for (int i = 0 ; i < names.size()  ; i++) {
+            positions.put(names.get(i), INIT_NUM);
+        }
+    }
+
+    public void move(String name) {
+        positions.put(name, positions.get(name) + 1);
+    }
+
+    public Map<String, Integer> getPositions() {
+        return positions;
+    }
+}
--- src/test/java/step4/PositionsTest.java
+++ src/test/java/step4/PositionsTest.java
@@ -3,31 +3,33 @@ package step4;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class PositionsTest {
-    Positions positions;
+    Position position;
 
     @BeforeEach
     void init() {
-        positions = new Positions();
-        positions.initPositions(3);
+        position = new Position();
+        position.initPosition(new ArrayList<>(List.of(new String[]{"test1", "test2"})));
     }
     @Test
     void initPositionsTest() {
 
-        List<Integer> positionsList = positions.getPositions();
+        Map<String, Integer> positions = position.getPositions();
 
-        assertThat(positionsList.size()).isEqualTo(3);
-        assertThat(positionsList.get(0)).isEqualTo(0);
+        assertThat(positions.size()).isEqualTo(3);
+        assertThat(positions.get("test1")).isEqualTo(0);
     }
 
     @Test
     void moveTest() {
-        positions.move(0);
+        position.move("test1");
 
-        assertThat(positions.getPositions().get(0)).isEqualTo(1);
+        assertThat(position.getPositions().get("test1")).isEqualTo(1);
     }
 }
