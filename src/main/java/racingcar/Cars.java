e83bba9bdb9fc706f3d0941aa2b0cbd8fd76afaf
wugatama <dnr7410@naver.com>
2022-10-04 00:36:23 +0900

    refactor: random 생성 메소드 삭제

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -2,7 +2,6 @@ package racingcar;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Cars {
 	private final List<Car> cars = new ArrayList<>();
@@ -15,12 +14,5 @@ public class Cars {
 
 	public List<Car> getCars() {
 		return cars;
-	}
-
-	public void move() {
-		for (int i = 0; i < this.cars.size(); i++) {
-			int rand = new Random().nextInt(10);
-			this.cars.get(i).movingOfRound(rand);
-		}
 	}
 }
