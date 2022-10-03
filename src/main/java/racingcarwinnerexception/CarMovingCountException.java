507eb7e41ff6e4a762ed002520519d6463fafe80
scoopsoup <rnrdud46@nate.com>
2022-10-03 15:03:18 +0900

    feat : 시도회수 예외처리를 위한 커스텀예외 클래스

---
+++ src/main/java/racingcarwinnerexception/CarMovingCountException.java
@@ -0,0 +1,12 @@
+package racingcarwinnerexception;
+
+public class CarMovingCountException extends Exception{
+
+    public CarMovingCountException(){
+        super();
+    }
+
+    public CarMovingCountException(String message){
+        super(message);
+    }
+}
