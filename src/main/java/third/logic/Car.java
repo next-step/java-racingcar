450463c46d0472ce2c816a0f831ef5ca941a2bdf
isakchoe <isakchoe@naver.com>
2022-10-03 22:34:22 +0900

    refactor : Car
    -carArrayList 변수, getter 삭제 
    -carMove 메서드명 변경
    -carLocation 변수, getter 타입 변경: StringBuilder -> int

--- src/main/java/third/logic/Car.java
+++ src/main/java/third/logic/Car.java
@@ -1,30 +1,18 @@
 package third.logic;
 
 
-import java.util.ArrayList;
-
-
 public class Car {
 
     private static final int THRESHOLD = 4;
-    private static final ArrayList<Car> carArrayList = new ArrayList<>();
-    private final StringBuilder carLocation = new StringBuilder("-");
+    private int carLocation = 1;
 
-    public Car() {
-        carArrayList.add(this);
-    }
-
-    public static ArrayList<Car> getCarArrayList() {
-        return carArrayList;
-    }
-
-    public StringBuilder getCarLocation() {
+    public int getCarLocation() {
         return carLocation;
     }
 
-    public void carMove(int number) {
+    public void Move(int number) {
         if (number >= THRESHOLD) {
-            carLocation.append("-");
+            carLocation += 1;
         }
     }
 }
