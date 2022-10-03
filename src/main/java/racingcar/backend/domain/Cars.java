e912ddd4dc06e5e489e66d1a63c4491ac334750f
jiokim <jiokim222@gmail.com>
2022-10-03 20:47:30 +0900

    feat(Cars): 객체 생성 정적 메소드 구현

--- src/main/java/racingcar/backend/domain/Cars.java
+++ src/main/java/racingcar/backend/domain/Cars.java
@@ -1,10 +1,17 @@
 package racingcar.backend.domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
-    
+
     private final List<Car> values;
+
+    public static Cars create(String[] names) {
+        return new Cars(Arrays.asList(names).stream()
+                .map(Car::new).collect(Collectors.toList()));
+    }
 
     public Cars(List<Car> values) {
         this.values = values;
@@ -13,9 +20,12 @@ public class Cars {
     public GameRecord play() {
         GameRecord gameRecord = new GameRecord();
         for (Car car : values) {
-            car.move(new RandomMoveStrategy());
-            gameRecord.add(car.getPosition());
+            gameRecord.add(car.move(new RandomMoveStrategy()));
         }
         return gameRecord;
+    }
+
+    public List<Car> getValues() {
+        return values;
     }
 }
