134bce1e29b01978d825853cd0b11df27159406d
nhs219 <heesun_z@naver.com>
2022-10-03 23:10:22 +0900

    refactor : Car test 수정

--- src/test/java/step4/CarTest.java
+++ src/test/java/step4/CarTest.java
@@ -9,15 +9,15 @@ public class CarTest {
     @Test
     void inputCarExceptionTest() {
         assertThatThrownBy(() -> {
-            new Car(new String[]{"test1111", "test1"}, 5);
+            new Car("123456", new Position());
         }).isInstanceOf(RuntimeException.class);
     }
 
     @Test
     void inputCarTest() {
-        Car car = new Car(new String[]{"test1", "test2"}, 5);
-        
-        assertThat(car.getTimes()).isEqualTo(5);
-        assertThat(car.getNames().get(0)).isEqualTo("test1");
+        Car car = new Car("1234", new Position());
+
+        assertThat(car.getName()).isEqualTo("1234");
+        assertThat(car.getPosition()).isEqualTo(new Position());
     }
 }
