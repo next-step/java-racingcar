3769ddcd153d51547aa6c07275e28f3ce4aa86f1
Myunghwan Kim <samkim2626@gmail.com>
2022-10-03 22:31:52 +0900

    refactor: foreach -> stream 사용

--- src/main/java/racingcar/step3_4/Cars.java
+++ src/main/java/racingcar/step3_4/Cars.java
@@ -1,51 +1,49 @@
 package racingcar.step3_4;
 
-import java.util.ArrayList;
+import static java.util.stream.Collectors.*;
+
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 public class Cars {
 
-	private final List<Car> cars;
+    private final List<Car> cars;
 
-	private Cars(List<Car> cars) {
-		this.cars = cars;
-	}
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
 
-	public static Cars createCars(Names carNames) {
-		List<Car> cars = new ArrayList<>();
-		for (Name name : carNames.getNames()) {
-			cars.add(new Car(name));
-		}
+    public static Cars createCars(Names carNames) {
+        return carNames.getNames()
+            .stream()
+            .map(Car::new)
+            .collect(collectingAndThen(toList(), Cars::new));
+    }
 
-		return new Cars(cars);
-	}
+    public void move(RandomValueGenerator randomValueGenerator) {
+        for (Car car : cars) {
+            car.move(randomValueGenerator);
+        }
+    }
 
-	public void move(RandomValueGenerator randomValueGenerator) {
-		for (Car car : cars) {
-			car.move(randomValueGenerator);
-		}
-	}
+    public List<Car> determineWinners() {
+        return cars.stream()
+            .filter(car -> car.getPosition() == getMaxPosition())
+            .collect(toList());
+    }
 
-	public List<Car> determineWinners() {
-		return cars.stream()
-			.filter(car -> car.getPosition() == getMaxPosition())
-			.collect(Collectors.toList());
-	}
+    private int getMaxPosition() {
+        return cars.stream()
+            .mapToInt(Car::getPosition)
+            .max()
+            .orElseThrow(() -> new NoSuchElementException("우승 위치의 자동차가 존재하지 않습니다."));
+    }
 
-	private int getMaxPosition() {
-		return cars.stream()
-			.mapToInt(Car::getPosition)
-			.max()
-			.orElseThrow(() -> new NoSuchElementException("우승 위치의 자동차가 존재하지 않습니다."));
-	}
+    public int getCarsCount() {
+        return cars.size();
+    }
 
-	public int getCarsCount() {
-		return cars.size();
-	}
-
-	public List<Car> getCars() {
-		return List.copyOf(this.cars);
-	}
+    public List<Car> getCars() {
+        return List.copyOf(this.cars);
+    }
 }
