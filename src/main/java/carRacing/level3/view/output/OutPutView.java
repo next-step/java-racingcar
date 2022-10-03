277c517c5b5432ce51d9da91c0f38cd5fb0e7392
sienna <20200803@sungshin.ac.kr>
2022-10-04 00:00:08 +0900

    :poop:잘못 올라간 주석 제거

--- src/main/java/carRacing/level3/view/output/OutPutView.java
+++ src/main/java/carRacing/level3/view/output/OutPutView.java
@@ -4,12 +4,12 @@ import carRacing.level3.model.Cars;
 
 public class OutPutView {
 
-	//cars리스트가 들어오면 안에 다 빼서 /n도 함께 출력해주기
-
 	public void showRace(Cars cars,int totalCarNum){
+
 		for(int i = 0 ; i < totalCarNum; i++){
 			printLocation(cars.carLocation(i));
 		}
+
 	}
 
 	public void printLocation(int location) {
