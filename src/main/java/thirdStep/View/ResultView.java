3e89d5943c070226c10f484cab3f7e496ce4abc7
isakchoe <isakchoe@naver.com>
2022-10-03 17:36:50 +0900

    refactor: ResultView
    - 출력을 담당하는 메서드만 남기고, 나머지 메서드는 제거  : Race.startOneRace 부분 제거

---
+++ src/main/java/thirdStep/View/ResultView.java
@@ -0,0 +1,14 @@
+package thirdstep.View;
+
+import thirdstep.logic.Car;
+import java.util.ArrayList;
+
+public class ResultView {
+
+    public static void printOneRace(ArrayList<Car> carArrayList) {
+        for (Car car : carArrayList) {
+            System.out.println(car.getCarLocation());
+        }
+        System.out.println();
+    }
+}
