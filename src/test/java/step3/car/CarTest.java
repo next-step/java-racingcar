f52964df841a27c4364fbec47cb2f2d4b741f977
kureungkureung <terry9611@naver.com>
2022-10-03 15:53:47 +0900

    test(CarTest): OutputView Fake 객체 주입

--- src/test/java/step3/car/CarTest.java
+++ src/test/java/step3/car/CarTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step3.number.Number;
+import step3.output.OutputView;
 import step3.position.Position;
 
 class CarTest {
@@ -12,18 +13,18 @@ class CarTest {
     @DisplayName("숫자가 4이상 나올 경우 자동차는 한 칸 전진한다.")
     void a() {
         final Number number = new Number.Fake(4);
-        final Car sut = new Car(number);
+        final Car sut = new Car(number, new OutputView.Fake());
 
-        Assertions.assertThat(sut.movedCar()).isEqualTo(new Car(new Position(1), number));
+        Assertions.assertThat(sut.movedCar()).isEqualTo(new Car(new Position(1, new OutputView.Fake()), number));
     }
 
     @Test
     @DisplayName("숫자가 4미만 나올 경우 자동차는 현재 위치에 움직이지 않는다.")
     void b() {
         final Number number = new Number.Fake(3);
-        final Car sut = new Car(number);
+        final Car sut = new Car(number, new OutputView.Fake());
 
-        Assertions.assertThat(sut.movedCar()).isEqualTo(new Car(new Position(0), number));
+        Assertions.assertThat(sut.movedCar()).isEqualTo(new Car(new Position(0, new OutputView.Fake()), number));
     }
 
 }
