4fff52c5a664868803ff06f75f62b03464aeb828
sangwon lee <231rollo@gmail.com>
2022-10-03 13:41:30 +0900

    refactoring(3단계 - 자동차 경주) : 상수의 이름
    - 상수의 이름 규칙은 변수와 구분 짓도록 대문자로 한다.
    
    ref : #3695

--- src/main/java/step3/car_racing/view/InputView.java
+++ src/main/java/step3/car_racing/view/InputView.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final String MESSAGE = "양수만 입력할 수 있습니다. 다시 입력해 주세요.";
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String VALIDATION_MESSAGE = "양수만 입력할 수 있습니다. 다시 입력해 주세요.";
 
     public int input() {
         return validateInput();
     }
 
     private int validateInput() {
-        while (!scanner.hasNextInt() || scanner.nextInt() < 0) {
-            System.out.println(MESSAGE);
-            scanner.next();
+        while (!SCANNER.hasNextInt() || SCANNER.nextInt() < 0) {
+            System.out.println(VALIDATION_MESSAGE);
+            SCANNER.next();
         }
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 }
