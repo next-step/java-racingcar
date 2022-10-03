0415bb71bf3bf7a3d6fe61417f090459eba4aead
wugatama <dnr7410@naver.com>
2022-10-04 00:35:43 +0900

    test: 자동차들 움직임 테스트

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +13,16 @@ public class CarsTest {
 	void createCars() {
 		Cars cars = new Cars(new String[] {"pobi", "crong", "honux"});
 		assertThat(cars.getCars().size()).isEqualTo(3);
+	}
+
+	@Test
+	@DisplayName("자동차들 움직임 테스트")
+	void move() {
+		Cars cars = new Cars(new String[] {"pobi", "wuga"});
+		cars.getCars().get(0).movingOfRound(3);
+		cars.getCars().get(1).movingOfRound(4);
+
+		assertThat(cars.getCars().get(0).getStatus()).isEqualTo(0);
+		assertThat(cars.getCars().get(1).getStatus()).isEqualTo(1);
 	}
 }
