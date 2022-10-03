ce6a7e7e5f38ed5bef5bb0ac1817ac17a5a088bf
wugatama <dnr7410@naver.com>
2022-10-04 01:39:14 +0900

    refactor: Cars 로직 테스트 추가

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -2,8 +2,6 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Random;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +22,25 @@ public class CarsTest {
 
 		assertThat(cars.getCars().get(0).getStatus()).isEqualTo(0);
 		assertThat(cars.getCars().get(1).getStatus()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("우승자 확인")
+	void findWinners() {
+		Cars cars = new Cars(new String[] {"aaa", "bbb"});
+		cars.getCars().get(0).movingOfRound(1);
+		cars.getCars().get(1).movingOfRound(4);
+
+		assertThat(cars.findWinner().size()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("우승자 위치 확인")
+	void bestPosition() {
+		Cars cars = new Cars(new String[] {"aaa", "bbb"});
+		cars.getCars().get(0).movingOfRound(1);
+		cars.getCars().get(1).movingOfRound(4);
+
+		assertThat(cars.findWinner().get(0).getStatus()).isEqualTo(1);
 	}
 }
