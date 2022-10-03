2d7c16df33beb0d3b7e43b75779373b247f0fab0
scoopsoup <rnrdud46@nate.com>
2022-10-03 15:37:33 +0900

    feat : 자동차 이름 분리 및 참가자동차 갯수 구하기

--- src/main/java/racingcarwinner/RacingCarWinner.java
+++ src/main/java/racingcarwinner/RacingCarWinner.java
@@ -4,9 +4,17 @@ public class RacingCarWinner {
 
     private String participantName;
     private int carMovingCount;
+    private String[] participantNames;
+    private int participantNumber;
 
     public RacingCarWinner(String participantName, int carMovingCount) {
         this.participantName = participantName;
         this.carMovingCount = carMovingCount;
     }
+
+    public void splitParticipantName() {
+        participantNames = participantName.split(",");
+        participantNumber = participantNames.length;
+    }
+
 }
