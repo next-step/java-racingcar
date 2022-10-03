1c7312a50bbcee8481be1b4cc9f08fbebdf22461
김남혁 <nhkim25@lotte.net>
2022-10-03 14:42:00 +0900

    refactor : 매직넘버 상수화 및 메서드 분리

--- src/main/java/step4/controller/RacingGameController.java
+++ src/main/java/step4/controller/RacingGameController.java
@@ -9,22 +9,29 @@ import step4.view.output.OutputViewImpl;
 
 public class RacingGameController {
 
-    private static final int MAX_BOUND = 10;
+	private static final int MAX_BOUND = 10;
+	private static final int MIN_TRY_CNT = 1;
+	private static final int MIN_NAME_LENGTH = 1;
 
 	public void racingGame(String nameInput, int tryCnt) {
-        if(nameInput.length() < 1 || tryCnt < 1) throw new RuntimeException("자동차 댓수 또는 시도 횟수는 적어도 양수여야 합니다.");
+		if (nameInput.length() < MIN_NAME_LENGTH || tryCnt < MIN_TRY_CNT) throw new RuntimeException("자동차 댓수 또는 시도 횟수는 적어도 양수여야 합니다.");
 
-        final Cars cars = new Cars();
-        final Winners winners = new Winners();
-        final OutputView outputView = new OutputViewImpl();
-        final NumberGenerator numberGenerator = new RandomNumberGenerator();
+		final Cars cars = new Cars();
+		final Winners winners = new Winners();
+		final OutputView outputView = new OutputViewImpl();
+		final NumberGenerator numberGenerator = new RandomNumberGenerator();
 
 		cars.init(nameInput);
 		for (int i = 0; i < tryCnt; i++) {
-			cars.getCars().forEach(car -> car.move(numberGenerator.generate(MAX_BOUND)));
+			decideMove(cars, numberGenerator);
 			outputView.printRacingResult(cars);
 		}
 		winners.checkWinners(cars, winners.getWinnerStandard(cars));
 		outputView.printWinner(winners);
+	}
+
+	private static void decideMove(Cars cars, NumberGenerator numberGenerator) {
+		cars.getCars()
+				.forEach(car -> car.move(numberGenerator.generate(MAX_BOUND)));
 	}
 }
