cd7f2e954f916ea196daf99c31d3d3a5273ee36f
kureungkureung <terry9611@naver.com>
2022-10-03 14:40:34 +0900

    refactor(RandomNumber): 동등성 비교 제거

--- src/main/java/step3/number/RandomNumber.java
+++ src/main/java/step3/number/RandomNumber.java
@@ -12,18 +12,4 @@ public final class RandomNumber implements Number {
                 .nextInt(RANDOM_BOUND);
     }
 
-    @Override
-    public int hashCode() {
-        return 0;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return isRandomNumber(obj);
-    }
-
-    private boolean isRandomNumber(Object object) {
-        return object instanceof RandomNumber;
-    }
-
 }
