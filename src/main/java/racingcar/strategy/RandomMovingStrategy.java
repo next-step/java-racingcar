10a0b8a37c2f65621eb02cffd8d3260db6efbb63
이건회 <rg970604@naver.com>
2022-10-03 22:41:54 +0900

    feat (RandomMovingStrategy.java)

---
+++ src/main/java/racingcar/strategy/RandomMovingStrategy.java
@@ -0,0 +1,16 @@
+package racingcar.strategy;
+
+import java.util.Random;
+
+public class RandomMovingStrategy implements MovingStrategy {
+
+  private static final int MIN_NUMBER = 4;
+  private static final int MAX_NUMBER = 10;
+
+  @Override
+  public boolean isMoveAble() {
+    Random random = new Random();
+    int randomNum = random.nextInt(MAX_NUMBER);
+    return MIN_NUMBER <= randomNum && randomNum < MAX_NUMBER;
+  }
+}
