6c953d69fd96151bd8fe50cb78e06529f52d9e39
heoseungyeon <dia0312@naver.com>
2022-10-03 17:46:36 +0900

    test: Car move() 테스트 구현

---
+++ src/test/java/racing/domain/CarTest.java
@@ -0,0 +1,20 @@
+package racing.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+	RandomNumber randomNumber = new RandomNumber();
+	Car car = new Car(randomNumber);
+	int movePivot = 4;
+
+	@DisplayName("move() 테스트")
+	@Test
+	void move() {
+		int preCarLocation = car.getLocation();
+		int moveDistance = car.move();
+		Assertions.assertThat(car.getLocation()).isEqualTo(preCarLocation + moveDistance);
+	}
+}
