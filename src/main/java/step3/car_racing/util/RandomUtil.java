e3b36083779c24c79ef45aecad660a0ff1c0a91e
sangwon lee <231rollo@gmail.com>
2022-10-03 13:47:35 +0900

    refactoring(3단계 - 자동차 경주) : 유틸리티 클래스 인스턴스화 방지
    - 컴파일러의 자동 기본생성자 생성 방지를 위한 private 생성자 추가
    
    ref : #3695

--- src/main/java/step3/car_racing/util/RandomUtil.java
+++ src/main/java/step3/car_racing/util/RandomUtil.java
@@ -8,6 +8,11 @@ public class RandomUtil {
     private static final Integer STANDARD_VALUE = 4;
     private static final Random random = new Random();
 
+    private RandomUtil() {
+        // 인스턴스화 방지
+        throw new AssertionError();
+    }
+
     public static Boolean isForward() {
         return random.nextInt(RANDOM_RANGE) > STANDARD_VALUE;
     }
