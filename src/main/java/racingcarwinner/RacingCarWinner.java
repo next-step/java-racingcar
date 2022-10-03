32759a41971729a00c285926dd90168bb4d3640d
scoopsoup <rnrdud46@nate.com>
2022-10-03 15:36:00 +0900

    feat : RacingCarWinner 생성자

---
+++ src/main/java/racingcarwinner/RacingCarWinner.java
@@ -0,0 +1,12 @@
+package racingcarwinner;
+
+public class RacingCarWinner {
+
+    private String participantName;
+    private int carMovingCount;
+
+    public RacingCarWinner(String participantName, int carMovingCount) {
+        this.participantName = participantName;
+        this.carMovingCount = carMovingCount;
+    }
+}
