c8bdb7b9eec0b6ae6ea7eb3705826f4b2e870ad6
scoopsoup <rnrdud46@nate.com>
2022-10-03 15:10:38 +0900

    feat : 자동차 이름, 시도횟수 입력하기

---
+++ src/main/java/racingcarwinner/ResultView.java
@@ -0,0 +1,16 @@
+package racingcarwinner;
+
+import racingcarwinnerexception.CarMovingCountException;
+import racingcarwinnerexception.NameIsEmptyNullException;
+import racingcarwinnerexception.NameOutOfLengthException;
+
+public class ResultView {
+    public static void main(String[] args) throws NameOutOfLengthException, NameIsEmptyNullException, CarMovingCountException {
+        InputView inputView = new InputView();
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String participantName = inputView.enterParticipantName();
+        System.out.println("시도할 회수는 몇번인가요?");
+        int carMovingCount = inputView.enterCarMovingCount();
+        System.out.println();
+    }
+}
