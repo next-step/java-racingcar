ff9483078ebedbc8d17f68089a41d7be3c955a36
caoyu-dev <choyumin01@gmail.com>
2022-10-03 23:06:58 +0900

    [test] CarTest 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,21 +1,34 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
+import racingcar.RandomGenerator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
     @DisplayName("Car 클래스의 move 메서드의 전진하는 조건은 0 에서 9 사이의 random 값에서 random 값이 기준으로 삼은 값 이상일 경우이다.")
-    @ParameterizedTest
-    @CsvSource(value = {"1:0", "2:0", "4:1", "5:1"}, delimiter = ':')
-    void move(int input, int expected) {
+    @Test
+    void move() {
         MoveCondition moveCondition = new RandomMoveCondition();
         Car car = new Car(moveCondition);
 
-        car.move(input);
-        assertThat(car.getPosition()).isEqualTo(expected);
+        int createNum = createRandom();
+
+        if (isMovable(moveCondition, createNum)) {
+            car.move(createNum);
+            assertThat(car.getPosition()).isEqualTo(1);
+        } else {
+            assertThat(car.getPosition()).isEqualTo(0);
+        }
+    }
+
+    private boolean isMovable(MoveCondition moveCondition, int createNum) {
+        return moveCondition.isMovable(createNum);
+    }
+
+    private int createRandom() {
+        return RandomGenerator.randomInt();
     }
 }
