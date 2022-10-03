b3434fd4524191935e71cda609312f28637bffa7
kureungkureung <terry9611@naver.com>
2022-10-03 14:55:55 +0900

    test(CarTest): Number.Fake 객체의 static import 제거

--- src/test/java/step3/car/CarTest.java
+++ src/test/java/step3/car/CarTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step3.number.Number;
-import step3.number.Number.Fake;
 import step3.position.Position;
 
 class CarTest {
@@ -12,7 +11,7 @@ class CarTest {
     @Test
     @DisplayName("숫자가 4이상 나올 경우 자동차는 한 칸 전진한다.")
     void a() {
-        final Number number = new Fake(4);
+        final Number number = new Number.Fake(4);
         final Car sut = new Car(number);
 
         Assertions.assertThat(sut.movedCar()).isEqualTo(new Car(new Position(1), number));
@@ -21,7 +20,7 @@ class CarTest {
     @Test
     @DisplayName("숫자가 4미만 나올 경우 자동차는 현재 위치에 움직이지 않는다.")
     void b() {
-        final Number number = new Fake(3);
+        final Number number = new Number.Fake(3);
         final Car sut = new Car(number);
 
         Assertions.assertThat(sut.movedCar()).isEqualTo(new Car(new Position(0), number));
