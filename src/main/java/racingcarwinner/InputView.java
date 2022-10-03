13e9f5c92b822cd5a7e7d5e00c369fd31e5793e0
scoopsoup <rnrdud46@nate.com>
2022-10-03 15:08:22 +0900

    feat : 입력값 예외처리(자동차 이름 및 이동회수)

--- src/main/java/racingcarwinner/InputView.java
+++ src/main/java/racingcarwinner/InputView.java
@@ -1,16 +1,39 @@
 package racingcarwinner;
 
+import racingcarwinnerexception.CarMovingCountException;
+import racingcarwinnerexception.NameIsEmptyNullException;
+import racingcarwinnerexception.NameOutOfLengthException;
+
 import java.util.Scanner;
 
 public class InputView {
 
     private Scanner sc = new Scanner(System.in);
+    private String[] participantNames;
 
-    public String enterParticipantName() {
-        return sc.nextLine();
+    public String enterParticipantName() throws NameOutOfLengthException, NameIsEmptyNullException {
+        String enterName = sc.nextLine();
+        participantNames = enterName.split(",");
+        for (String name : participantNames) {
+            NameLengthDiscrimination(name);
+        }
+        if (enterName == null || enterName.isBlank()) {
+            throw new NameIsEmptyNullException("자동차 이름은 빈값이 들어올 수 없습니다.");
+        }
+        return enterName;
     }
 
-    public int enterCarMovingCount() {
-        return sc.nextInt();
+    public int enterCarMovingCount() throws CarMovingCountException {
+        int enterCarMoveCount = sc.nextInt();
+        if (enterCarMoveCount < 1) {
+            throw new CarMovingCountException("시도회수는 1이상 이여야합니다.");
+        }
+        return enterCarMoveCount;
+    }
+
+    public void NameLengthDiscrimination(String name) throws NameOutOfLengthException {
+        if (name.length() > 5) {
+            throw new NameOutOfLengthException("자동차 이름은 5글자를 넘길 수 없습니다.");
+        }
     }
 }
