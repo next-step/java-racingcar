efef042c2abfd4c42d4cdc7f0b487c563515a505
heoseungyeon <dia0312@naver.com>
2022-10-03 17:24:48 +0900

    refactor: RandomNumber 클래스 수정 - value 멤버 변수 추가

--- src/main/java/racing/domain/RandomNumber.java
+++ src/main/java/racing/domain/RandomNumber.java
@@ -5,8 +5,14 @@ import java.util.Random;
 public class RandomNumber {
 	private final static Random RANDOM = new Random();
 	private final static int RANDOM_MAX_BOUND = 10;
+	private int value;
 
 	public int getRandomNumber() {
-		return RANDOM.nextInt(RANDOM_MAX_BOUND);
+		value = RANDOM.nextInt(RANDOM_MAX_BOUND);
+		return value;
+	}
+
+	public int getValue() {
+		return value;
 	}
 }
