154d39d416afb86ece32770219621eb845f0c686
isakchoe@naver.com <isakchoe@naver.com>
2022-10-03 20:10:44 +0900

    짜투리

---
+++ src/main/java/thirdStep/logic/Main.java
@@ -0,0 +1,27 @@
+package thirdstep.logic;
+
+import thirdstep.view.InputView;
+import thirdstep.view.ResultView;
+
+import java.util.ArrayList;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int raceCount = InputView.inputRaceCount();
+        int carCount = InputView.inputCarCount();
+
+        for(int i =0; i < carCount; i++) {
+            Car car = new Car();
+        }
+
+        ArrayList<Car> carArrayList = Car.getCarArrayList();
+
+        for( int i = 0; i < raceCount; i++){
+            Race.startOneRace(carArrayList, carCount);
+            ResultView.printOneRace(carArrayList);
+        }
+    }
+}
