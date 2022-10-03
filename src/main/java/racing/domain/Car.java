3a3c40f79b0be57790db141bc59a0bd8cc2e467a
heoseungyeon <dia0312@naver.com>
2022-10-03 16:37:07 +0900

    feat: 요구사항(각 자동차는 random() 을 통해 전진 or 정지가 결정된다.) 기능 구현

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,11 +1,35 @@
 package racing.domain;
 
+import java.util.Random;
+
 public class Car {
 
+	private final static int START_POINT = 0;
+	private final static int MOVE_PIVOT = 4;
+	private int location;
+
 	public Car() {
+		this(START_POINT);
+	}
+
+	public Car(int location) {
+		this.location = location;
 	}
 
 	public void move() {
+		Random random = new Random();
+		int randomNumber = random.nextInt(10);
 
+		if (randomNumber >= MOVE_PIVOT) {
+			moveForward();
+		}
+	}
+
+	private void moveForward() {
+		this.location += 1;
+	}
+
+	public int getLocation() {
+		return location;
 	}
 }
