9ba3bf0a4f63bb9814a9a57f2867f4f8594e42d3
wugatama <dnr7410@naver.com>
2022-10-04 01:32:30 +0900

    refactor: 우승자 로직 Cars객체로 이동

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -3,8 +3,8 @@ package racingcar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cars extends Condition{
-	private final List<Car> cars = new ArrayList<>();
+public class Cars extends Condition {
+	private List<Car> cars = new ArrayList<>();
 
 	public Cars(String[] cars) {
 		super(new NameCondition());
@@ -21,5 +21,30 @@ public class Cars extends Condition{
 
 	public List<Car> getCars() {
 		return cars;
+	}
+
+	public List<Car> findWinner() {
+		int max = 0;
+		List<Car> winners = new ArrayList<>();
+		for (int i = 0; i < cars.size(); i++) {
+			max = bestPosition(max, i);
+		}
+		return findWinners(max, winners);
+	}
+
+	private List<Car> findWinners(int max, List<Car> winners) {
+		for (Car car : cars) {
+			if (car.getStatus() == max) {
+				winners.add(car);
+			}
+		}
+		return winners;
+	}
+
+	private int bestPosition(int max, int i) {
+		if (max <= cars.get(i).getStatus()) {
+			max = cars.get(i).getStatus();
+		}
+		return max;
 	}
 }
--- src/main/java/racingcar/Views.java
+++ src/main/java/racingcar/Views.java
@@ -1,8 +1,5 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Views {
 
 	public static void results(Cars cars, int index) {
@@ -12,27 +9,15 @@ public class Views {
 		Print.newLine();
 	}
 
+	public static void winOfRacing(Cars cars) {
+		Print.printWinner(cars.findWinner());
+	}
+
 	public static String move(int input) {
 		String temp = "";
 		for (int i = 0; i < input; i++) {
 			temp += "-";
 		}
 		return temp;
-	}
-
-	public static void winOfRacing(Cars cars) {
-		List<Car> winners = new ArrayList<>();
-		int max = 0;
-		for (int i = 0; i < cars.getCars().size(); i++) {
-			if (max <= cars.getCars().get(i).getStatus()) {
-				max = cars.getCars().get(i).getStatus();
-			}
-		}
-		for (Car car : cars.getCars()) {
-			if (car.getStatus() == max) {
-				winners.add(car);
-			}
-		}
-		Print.printWinner(winners);
 	}
 }
