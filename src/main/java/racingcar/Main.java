872cbb76392774d902af62a4e3eb47f4d70f7296
sasha-com <sasha.com@kakaocorp.com>
2022-10-03 22:55:07 +0900

    fix: RacingCarFactory 클래스가 추가됨에 따라 Main 클래스 로직 수정, README.md 파일에 설명 추가

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -1,22 +1,12 @@
 package racingcar;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        List<RacingCar> cars = new ArrayList<>();
-        Random random = new Random();
-
         int numOfCars = InputView.getNumOfCars();
         int tryingCount = InputView.getTryCount();
 
-        for(int i=0 ; i<numOfCars ; i++) {
-            cars.add(new RacingCar(random, tryingCount));
-        }
-
-        ResultView resultView = new ResultView(cars);
+        RacingCarFactory factory = RacingCarFactory.getFactory(numOfCars, tryingCount);
+        ResultView resultView = new ResultView(factory.manufactoringCars());
         resultView.printCars();
 
     }
