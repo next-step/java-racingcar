e4b95ef7bccebc02dc904b73a2edd969e02c40ba
scoopsoup <rnrdud46@nate.com>
2022-10-03 14:59:55 +0900

    feat : 자동차 이름 길이 예외처리를 위한 커스텀에외 만들기

---
+++ src/main/java/racingcarwinnerexception/NameOutOfLengthException.java
@@ -0,0 +1,12 @@
+package racingcarwinnerexception;
+
+public class NameOutOfLengthException extends Exception{
+
+    public NameOutOfLengthException(){
+        super();
+    }
+
+    public NameOutOfLengthException(String message){
+        super(message);
+    }
+}
