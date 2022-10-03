9f686bed9aea753288bf4801130934fb5e932052
Hamstory <>
2022-10-03 13:33:53 +0900

    refactor(InputView): 상수 추출

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
     public static final String QUERY_NUMBER_OF_CARS_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String QUERY_NUMBER_OF_TRIALS_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    public static final String DELIMITER = ",";
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {
@@ -14,7 +15,7 @@ public class InputView {
     public static String[] queryCarNames() {
         System.out.println(QUERY_NUMBER_OF_CARS_MESSAGE);
         String input = scanner.nextLine();
-        return input.split(",");
+        return input.split(DELIMITER);
     }
 
     public static int queryNumberOfTrials() {
