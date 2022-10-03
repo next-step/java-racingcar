822ec0e68df384abc84c823c999428d245c2cea8
scoopsoup <rnrdud46@nate.com>
2022-10-03 15:02:05 +0900

    feat : 자동차 이름 NULL/빈값 예외처리를 위한 커스텀예외 클래스

---
+++ src/main/java/racingcarwinnerexception/NameIsEmptyNullException.java
@@ -0,0 +1,12 @@
+package racingcarwinnerexception;
+
+public class NameIsEmptyNullException extends Exception {
+
+    public NameIsEmptyNullException() {
+        super();
+    }
+
+    public NameIsEmptyNullException(String message) {
+        super(message);
+    }
+}
