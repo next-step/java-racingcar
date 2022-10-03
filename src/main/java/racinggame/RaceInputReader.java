8f0c1206b996ea625966114ba6227e44ec291587
swsong <ssw890720@gmail.com>
2022-10-03 21:07:57 +0900

    refactor: 오타수정

--- src/main/java/racinggame/RaceInputReader.java
+++ src/main/java/racinggame/RaceInputReader.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class RaceInputReader {
 
-    public static final String CAR_NAME_SEPERATOR = ",";
+    public static final String CAR_NAME_SEPARATOR = ",";
 
     private RaceInputReader() {
     }
@@ -33,6 +33,6 @@ public class RaceInputReader {
     }
 
     private static String[] split(String carNames) {
-        return carNames.replace(" ", "").split(CAR_NAME_SEPERATOR);
+        return carNames.replace(" ", "").split(CAR_NAME_SEPARATOR);
     }
 }
