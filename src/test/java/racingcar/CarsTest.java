80eb27f30ab3fbdcc4be41c9f8b683f3e0e72c2c
wugatama <dnr7410@naver.com>
2022-10-05 00:47:08 +0900

    refactor: Cars 일급 콜렉션

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -2,33 +2,45 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarsTest {
 	@Test
-	@DisplayName(", 로 분리된 배열을 받아서 List<Car> 생성")
+	@DisplayName("List<Car> 생성 - 일급 콜렉션")
 	void createCars() {
-		Cars cars = new Cars(new String[] {"pobi", "crong", "honux"});
+		Car aaa = new Car("aaa");
+		Car bbb = new Car("bbb");
+		Car ccc = new Car("ccc");
+
+		List<Car> list = Arrays.asList(aaa, bbb, ccc);
+		Cars cars = new Cars(list);
 		assertThat(cars.getCars().size()).isEqualTo(3);
 	}
 
 	@Test
 	@DisplayName("우승자 확인")
 	void findWinners() {
-		Cars cars = new Cars(new String[] {"aaa", "bbb"});
-		cars.getCars().get(0).movingOfRound(1);
-		cars.getCars().get(1).movingOfRound(4);
+		Car aaa = new Car("aaa");
+		aaa.movingOfRound(3);
+		Car bbb = new Car("bbb");
+		bbb.movingOfRound(4);
+		Cars cars = new Cars(Arrays.asList(aaa, bbb));
 
-		assertThat(cars.findWinner().get(0).getName()).isEqualTo(new Name("bbb"));
+		assertThat(cars.findWinner()).containsExactly(bbb);
 	}
 
 	@Test
 	@DisplayName("우승자 위치 확인")
 	void bestPosition() {
-		Cars cars = new Cars(new String[] {"aaa", "bbb"});
-		cars.getCars().get(0).movingOfRound(1);
-		cars.getCars().get(1).movingOfRound(4);
+		Car aaa = new Car("aaa");
+		aaa.movingOfRound(3);
+		Car bbb = new Car("bbb");
+		bbb.movingOfRound(4);
+		Cars cars = new Cars(Arrays.asList(aaa, bbb));
 
 		assertThat(cars.findWinner().get(0).getStatus()).isEqualTo(new Position(1));
 	}
