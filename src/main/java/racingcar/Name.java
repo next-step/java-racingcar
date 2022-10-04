a133d83343ac2547a0471e7332774d6905f190d8
wugatama <dnr7410@naver.com>
2022-10-04 19:47:51 +0900

    feat: Name 불변 객체로 분리

--- src/main/java/racingcar/Name.java
+++ src/main/java/racingcar/Name.java
@@ -1,2 +1,35 @@
-package racingcar;public class Name {
+package racingcar;
+
+import java.util.Objects;
+
+public class Name extends Condition {
+
+	private final String name;
+
+	public Name(String input) {
+		super(new NameCondition());
+		if (validInput(input)) {
+			throw new RuntimeException();
+		}
+		this.name = input;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Name name1 = (Name)o;
+		return Objects.equals(name, name1.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
 }
