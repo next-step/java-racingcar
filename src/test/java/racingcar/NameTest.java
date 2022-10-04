5830eea64f2da937755795df188bddfd3ac535d0
wugatama <dnr7410@naver.com>
2022-10-04 19:47:33 +0900

    test: Name 테스트

--- src/test/java/racingcar/NameTest.java
+++ src/test/java/racingcar/NameTest.java
@@ -1,2 +1,29 @@
-package racingcar;public class NameTest {
+package racingcar;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NameTest {
+	@Test
+	@DisplayName("이름 생성")
+	void create() {
+		Name name = new Name("abcde");
+		assertThat(name).isEqualTo(new Name("abcde"));
+	}
+
+	@Test
+	@DisplayName("5자 초과 예외발생")
+	void valid() {
+		assertThatThrownBy(() -> new Name("abcdef"))
+			.isInstanceOf(RuntimeException.class);
+	}
+
+	@Test
+	@DisplayName("이름 확인")
+	void checkName() {
+		Name name = new Name("abcde");
+		assertThat(name.getName()).isEqualTo("abcde");
+	}
 }
