c77020cd493659dba90a85873e68401f0926cd12
caoyu-dev <choyumin01@gmail.com>
2022-10-05 00:41:18 +0900

    [feat] 자동차 관련 도메인 생성자 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,8 +3,8 @@ package racingcar.domain;
 public class Car {
     private MoveCondition moveCondition;
     private int position;
-    private final int MOVE_POSITION = 1;
     private String name;
+    private final int MOVE_POSITION = 1;
     private final int NAME_MAX_LENGTH = 5;
 
     public Car(MoveCondition moveCondition) {
@@ -14,6 +14,7 @@ public class Car {
 
     public Car(String name) {
         checkLength(name);
+        this.position = 0;
         this.name = name;
     }
 
@@ -21,6 +22,11 @@ public class Car {
         if (NAME_MAX_LENGTH < name.length()) {
             throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없다.");
         }
+    }
+
+    public Car(int position, String name) {
+        this.position = position;
+        this.name = name;
     }
 
     public int getPosition() {
