203644ad2baacc5894daeb9218f8da39275032bb
jimbaemon <jimbae@kakao.com>
2022-10-05 00:21:47 +0900

    feat: 자동차의_이름이_5자를_초과하면_에러발생.

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -3,6 +3,8 @@ package racing.domain;
 import java.util.Comparator;
 
 public class Car implements Comparator<Car> {
+    private final static int MAX_LENGTH_OF_NAME = 5;
+
     private final MovingRule movingRule;
     private final String name;
     private int location;
@@ -11,6 +13,13 @@ public class Car implements Comparator<Car> {
     public Car(MovingRule movingRule, String name) {
         this.movingRule = movingRule;
         this.name = name;
+        validateName();
+    }
+
+    private void validateName() {
+        if (name.length() > MAX_LENGTH_OF_NAME) {
+            throw new IllegalArgumentException("자동차의 이름은 5자를 초과할수 없습니다.");
+        }
     }
 
     public void move() {
@@ -19,7 +28,7 @@ public class Car implements Comparator<Car> {
         }
     }
 
-    public String name(){
+    public String name() {
         return name;
     }
 
