ab5f0811cea4f39ff46fd8d2170a0d2e4898a084
wugatama <dnr7410@naver.com>
2022-10-04 19:58:27 +0900

    refactor: Car에서 검증된 테스트 삭제

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -7,28 +7,10 @@ import org.junit.jupiter.api.Test;
 
 public class CarsTest {
 	@Test
-	@DisplayName("이름이 5자 초과하는 자동차 예외")
-	void validCar() {
-		assertThatThrownBy(() -> new Cars(new String[] {"abcdef"}))
-			.isInstanceOf(RuntimeException.class);
-	}
-
-	@Test
 	@DisplayName(", 로 분리된 배열을 받아서 List<Car> 생성")
 	void createCars() {
 		Cars cars = new Cars(new String[] {"pobi", "crong", "honux"});
 		assertThat(cars.getCars().size()).isEqualTo(3);
-	}
-
-	@Test
-	@DisplayName("자동차들 움직임 테스트")
-	void move() {
-		Cars cars = new Cars(new String[] {"pobi", "wuga"});
-		cars.getCars().get(0).movingOfRound(3);
-		cars.getCars().get(1).movingOfRound(4);
-
-		assertThat(cars.getCars().get(0).getStatus()).isEqualTo(new Position());
-		assertThat(cars.getCars().get(1).getStatus()).isEqualTo(new Position(1));
 	}
 
 	@Test
