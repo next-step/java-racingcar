f16f012d682fbf5396e43340b70170e1c6ae7e03
Yunsik-Choi <choiys0311@gmail.com>
2022-10-05 00:41:11 +0900

    [fix] Main RacingGameInput

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,12 +1,17 @@
 package game;
 
 import game.view.input.Input;
+import game.view.input.RacingGameInput;
 
 public class Main {
 
     public static void main(String[] args) {
         RacingGameController racingGameController
-                = new RacingGameController(4, 10, Input.inputNames(), Input.inputNumber());
+                = new RacingGameController(
+                        4,
+                10,
+                RacingGameInput.inputCarNames(),
+                RacingGameInput.inputNumberOfRound());
         racingGameController.startGame();
     }
 }
