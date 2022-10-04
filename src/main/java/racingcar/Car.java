922ff9424ab9db2967879570065bf9e63751438f
wugatama <dnr7410@naver.com>
2022-10-04 15:34:08 +0900

    refactor: 상수 언더스코어 적용

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,8 +3,8 @@ package racingcar;
 import java.util.Random;
 
 public class Car {
-	private static int MAXOFRANDOM = 10;
-	private static final int MOVINGCONDITION = 4;
+	private static int MAX_OF_RANDOM = 10;
+	private static final int MOVING_CONDITION = 4;
 	private final String name;
 	private Position position;
 
@@ -14,13 +14,13 @@ public class Car {
 	}
 
 	public void movingOfRound(int random) {
-		if (random >= MOVINGCONDITION) {
+		if (random >= MOVING_CONDITION) {
 			this.position = new Position(this.position.getPosition() + 1);
 		}
 	}
 
 	public int makeRandom() {
-		return new Random().nextInt(MAXOFRANDOM);
+		return new Random().nextInt(MAX_OF_RANDOM);
 	}
 
 	public int getStatus() {
--- src/main/java/racingcar/NameCondition.java
+++ src/main/java/racingcar/NameCondition.java
@@ -1,9 +1,10 @@
 package racingcar;
 
 public class NameCondition implements ConditionStrategy {
+	private final int MAX_OF_NAME = 5;
 	@Override
 	public boolean validInput(String input) {
-		if (input.length() > 5 || input.isBlank()) {
+		if (input.length() > MAX_OF_NAME || input.isBlank()) {
 			throw new RuntimeException();
 		}
 		return true;
