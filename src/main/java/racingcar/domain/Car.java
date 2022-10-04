9a65ca8f72f881579bae532111f38bb18022aabc
caoyu-dev <choyumin01@gmail.com>
2022-10-04 22:59:48 +0900

    [feat] 자동차 이름 관련 도메인 수정 1. 자동차 이름 입력 구현 2. 자동차 이름은 5자를 초과할 수 없다

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,14 +4,31 @@ public class Car {
     private MoveCondition moveCondition;
     private int position;
     private final int MOVE_POSITION = 1;
+    private String name;
+    private final int NAME_MAX_LENGTH = 5;
 
     public Car(MoveCondition moveCondition) {
         this.position = 0;
         this.moveCondition = moveCondition;
     }
 
+    public Car(String name) {
+        checkLength(name);
+        this.name = name;
+    }
+
+    private void checkLength(String name) {
+        if (NAME_MAX_LENGTH < name.length()) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없다.");
+        }
+    }
+
     public int getPosition() {
         return position;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int move(int num) {
