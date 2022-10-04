2cd811d0aa6937bd8e0867f6ba763356e2176036
boradol2 <boradol0902@gmail.com>
2022-10-05 01:32:02 +0900

    feature : step4 전진하는 자동차를 이름 함께 출력.

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,13 +1,17 @@
 package racingcar.view;
 
 import racingcar.domain.Car;
-import racingcar.domain.Cars;
+import racingcar.dto.CarDto;
+import racingcar.dto.CarsDto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
     private static final String RESULT_MESSAGE = "실행결과";
     private static final String POSITION_MARK = "-";
+    private static final String COMMA = ",";
+    private static final String FINAL_WON_MESSAGE = " 가 최종 우승했습니다.";
 
     private ResultView() {
     }
@@ -16,12 +20,26 @@ public class ResultView {
         System.out.println("\n" + RESULT_MESSAGE + "\n");
     }
 
-    public static void print(Cars cars) {
-        List<Car> values = cars.getValues();
-        for (Car value : values) {
+    public static void printRacingCars(List<CarsDto> carsValues) {
+        for (CarsDto carsValue : carsValues) {
+            List<CarDto> values = carsValue.getValues();
+            printCarsRound(values);
+            System.out.println();
+        }
+    }
+
+    public static void printWinner(List<Car> winnerValues) {
+        System.out.println();
+        List<String> winnerCarNames = winnerValues.stream().map(Car::getName).collect(Collectors.toList());
+        System.out.print(String.join(COMMA, winnerCarNames));
+        System.out.println(FINAL_WON_MESSAGE);
+    }
+
+    private static void printCarsRound(List<CarDto> values) {
+        for (CarDto value : values) {
+            System.out.print(value.getName() + " : ");
             printPositionMark(value.getPosition());
         }
-        System.out.println();
     }
 
     private static void printPositionMark(int position) {
