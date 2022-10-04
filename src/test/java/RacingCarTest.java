e09f323bead9f84be8b509414bc80d5207c8d394
hannah33 <hannah.33@kakaocorp.com>
2022-10-04 19:54:15 +0900

    test: 자동차 이름에 최대 5자까지 허용하는지 확인하는 테스트 작성

--- src/test/java/RacingCarTest.java
+++ src/test/java/RacingCarTest.java
@@ -1,21 +1,32 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingCar.step3.RacingCar;
-import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class RacingCarTest {
-    private RacingCar car = new RacingCar(System.currentTimeMillis());
+    private RacingCar car = new RacingCar("test", System.currentTimeMillis());
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
     @ValueSource(ints = {1, 2, 3})
     void canMove_4보다_작은_수(int number) {
         assertThat(car.canMove(number)).isEqualTo(false);
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     void canMove_4보다_큰_수(int number) {
         assertThat(car.canMove(number)).isEqualTo(true);
     }
 
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @ValueSource(strings = {"a", "ab", "abc", "abcd", "abcde", "1", "세글자"})
+    void 자동차_이름_5글자_이하(String name) {
+        assertThat(new RacingCar(name, 1)).isInstanceOf(RacingCar.class);
+    }
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @ValueSource(strings = {"abcdef", "abcdeg", "123456", "다섯글자초과"})
+    void 자동차_이름_5글자_초과(String name) {
+        assertThatThrownBy(() -> new RacingCar(name, 1));
+    }
 }
