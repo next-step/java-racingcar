346ade835f69c9398def212a36b358737edd6720
sienna <20200803@sungshin.ac.kr>
2022-10-04 14:29:57 +0900

    :sparkles:feat : 자동차 이름을 입력 받는다

--- src/main/java/carRacing/level3/view/input/InputView.java
+++ src/main/java/carRacing/level3/view/input/InputView.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 public class InputView {
 
 	private final Scanner scanner;
+	private final String CAR_NAME_DELIMITER = ",";
 
 	public InputView() {
 		scanner = new Scanner(System.in);
 	}
 
-	public int askNumberCars() {
-		System.out.println("자동차 대수는 몇 대 인가요?");
-		return scanner.nextInt();
-	}
-
 	public int askValueRound() {
 		System.out.println("시도 회수는 몇회인가요?");
 		return scanner.nextInt();
+	}
+
+	public String[] askCarsName() {
+		System.out.println("경주할 자동차 이름을 입력하세요");
+		return scanner.nextLine().split(CAR_NAME_DELIMITER);
+
 	}
 
 }
