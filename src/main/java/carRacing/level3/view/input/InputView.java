ab7254d0ac8aeb2576d3ca1ab2671d50d95f39fb
sienna <20200803@sungshin.ac.kr>
2022-10-04 14:35:38 +0900

    :sparkles:feat : 이름 길이는 5자를 초과할 수 없다

--- src/main/java/carRacing/level3/view/input/InputView.java
+++ src/main/java/carRacing/level3/view/input/InputView.java
@@ -1,11 +1,15 @@
 package carRacing.level3.view.input;
 
+import carRacing.level3.infra.error.NameLengthException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputView {
 
 	private final Scanner scanner;
 	private final String CAR_NAME_DELIMITER = ",";
+	private final int MAX_LENGTH = 5;
+	public static ArrayList<String> carNames = new ArrayList<>();
 
 	public InputView() {
 		scanner = new Scanner(System.in);
@@ -20,6 +24,27 @@ public class InputView {
 		System.out.println("경주할 자동차 이름을 입력하세요");
 		return scanner.nextLine().split(CAR_NAME_DELIMITER);
 
+	}
+
+	private void validateLength(String carName) {
+		if (carName.length() >= MAX_LENGTH) {
+			throw new NameLengthException("자동차 이름이 제한 길이를 초과하였습니다");
+		}
+	}
+
+	public ArrayList<String> carNames() {
+		return carNames;
+	}
+
+	public int carSum() {
+		return carNames.size();
+	}
+
+	public void saveCarNames() throws Exception {
+		for (String carName : askCarsName()) {
+			validateLength(carName);
+			carNames.add(carName);
+		}
 	}
 
 }
