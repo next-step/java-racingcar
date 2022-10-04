30d3aa5838f8a05a7a19ddf030df756797c5ae56
caoyu-dev <choyumin01@gmail.com>
2022-10-05 00:42:37 +0900

    [feat] View Input -  자동차 이름 입력

--- src/main/java/racingcar/view/Input.java
+++ src/main/java/racingcar/view/Input.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 
 public class Input {
     private static final Scanner sc = new Scanner(System.in);
-    private static final String NUMBER_OF_CARS = "자동차 대수는 몇 대 인가요?";
+    private static final String NAME_OF_CARS = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String NUMBER_OF_ATTEMPTS = "시도할 회수는 몇 회 인가요?";
+    private static final String DELIMITER = ",";
 
-    public static int numberOfCars() {
-        System.out.println(NUMBER_OF_CARS);
-        return sc.nextInt();
+    public static String[] nameOfCars() {
+        System.out.println(NAME_OF_CARS);
+        String names = sc.next();
+        String[] name = names.split(DELIMITER);
+        return name;
     }
 
     public static int numberOfAttempts() {
