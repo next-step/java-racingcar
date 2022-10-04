fb0a1b78e379199998a3e97e90a4fc66588e6cdd
HYEONSEOK1 <gustir929@gmail.com>
2022-10-04 19:28:32 +0900

    feat: 출력기능 추가

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -4,5 +4,16 @@ import java.util.List;
 
 public class ResultView {
     public static void printCars(List<Car> cars) {
+        cars.forEach(ResultView::printBar);
+        System.out.println();
     }
+
+    private static void printBar(Car car) {
+        for (int i = 0; i < car.getPosition(); i++) {
+            System.out.print("|");
+        }
+        System.out.println();
+    }
+
+
 }
