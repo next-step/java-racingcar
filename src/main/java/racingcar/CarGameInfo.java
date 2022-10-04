bc64cb5f868953e0d394a691363b3f5eb9dbeaa7
sasha-com <sasha.com@kakaocorp.com>
2022-10-04 19:00:54 +0900

    feat: CarGameInfo 클래스 추가 (전체 경주 자동차, 우승자 관리)

---
+++ src/main/java/racingcar/CarGameInfo.java
@@ -0,0 +1,62 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CarGameInfo {
+    private final List<Car> cars;
+    private final List<StringBuilder> resultString;
+
+    public CarGameInfo(List<Car> cars) {
+        this.cars = cars;
+        this.resultString = new ArrayList<>(cars.size());
+        initResultString();
+    }
+
+    public void printCars(int indexOfCount) {
+        for (int j=0 ; j<cars.size() ; j++) {
+            Car car = cars.get(j);
+            System.out.println(car.getName() + " : " + updateDashString(car, resultString.get(j), indexOfCount));
+        }
+    }
+
+    private void initResultString() {
+        for(int i=0 ; i<cars.size() ; i++) {
+            resultString.add(new StringBuilder());
+        }
+    }
+
+    private StringBuilder updateDashString(Car car, StringBuilder str, int indexOfCount) {
+        if(car.getHistoriesByIndex(indexOfCount) == 1) {
+            str.append("-");
+        }
+        return str;
+    }
+
+    private int getMaxStraightCount() {
+        StringBuilder maxCount = resultString.get(0);
+        for(int i=1 ; i<resultString.size() ; i++) {
+            StringBuilder target = resultString.get(i);
+            if(target.length() >= maxCount.length()) {
+                maxCount = target;
+            }
+        }
+        return maxCount.length();
+    }
+
+    public List<Car> getWinnerList() {
+        List<Car> winnerList = new LinkedList<>();
+        int maxStraightCount = getMaxStraightCount();
+        for(int i=0 ; i<resultString.size() ; i++) {
+            decideIsWinnerCar(winnerList, i, maxStraightCount);
+        }
+        return winnerList;
+    }
+
+    private void decideIsWinnerCar(List<Car> winnerList, int index, int maxStraightCount) {
+        if(resultString.get(index).length() >= maxStraightCount) {
+            winnerList.add(cars.get(index));
+        }
+    }
+}
