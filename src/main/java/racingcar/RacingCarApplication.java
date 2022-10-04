0923ff50cc12aeb99f91cca98d2deaadccfb2e46
yunhwan <pyh8618@gmail.com>
2022-10-05 00:23:43 +0900

    refactor: InputView에 대한 역할과 책임 재정의
    InputView가 입력에 대한 포괄적인 의미를 담고 있다고 생각이 들어서 RacingInputView로 클래스명을 변경하였습니다.
    의미 없는 메서드 분리를 삭제하였고 Racing에 대한 입력만을 책임지는 클래스로 리팩터링 하였습니다.

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -1,21 +1,17 @@
 package racingcar;
 
 import java.util.List;
-import racingcar.view.InputView;
+import racingcar.view.RacingInputView;
 import racingcar.view.ResultView;
 
 public class RacingCarApplication {
-
-    public static final String COMMA_REGEX = ",";
-
     public static void main(String[] args) {
-        String answer = InputView.askQuestionAnswer("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분");
-        int tryNums = InputView.askQuestionAnswerInt("시도할 회수는 몇회인가요?");
-        List<String> names = InputView.split(answer, COMMA_REGEX);
+        List<String> carNames = RacingInputView.getCarNames();
+        int tryNums = RacingInputView.getTryNums();
 
-        RacingGame racingGame = new RacingGame(tryNums, names);
-
+        RacingGame racingGame = new RacingGame(tryNums, carNames);
         RacingRecord racingRecord = racingGame.racingStart();
+
         ResultView.printRoundRecord(racingRecord, "-");
     }
 }
