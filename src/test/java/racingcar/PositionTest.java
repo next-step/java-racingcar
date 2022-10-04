450005727c5d5861e90350562df64a088e039173
wugatama <dnr7410@naver.com>
2022-10-04 20:39:24 +0900

    test: 포지션 전진 테스트

--- src/test/java/racingcar/PositionTest.java
+++ src/test/java/racingcar/PositionTest.java
@@ -2,12 +2,21 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PositionTest {
 	@Test
+	@DisplayName("포지션 위치 확인")
 	void create() {
 		Position position = new Position(3);
 		assertThat(position).isEqualTo(new Position(3));
+	}
+
+	@Test
+	@DisplayName("포지션 전진")
+	void changePosition() {
+		Position position = new Position(1);
+		assertThat(position.increase()).isEqualTo(new Position(2));
 	}
 }
