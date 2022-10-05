6676115e9db1aeecb3d358bba9573bf70bb78bbd
jimbaemon <jimbae@kakao.com>
2022-10-05 12:42:21 +0900

    fix: input 메시지 출력 오류 수정.

--- src/main/java/racing/ui/InputView.java
+++ src/main/java/racing/ui/InputView.java
@@ -5,10 +5,20 @@ import java.util.Scanner;
 public class InputView {
 
     public static Form createForm() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        Scanner carScanner = new Scanner(System.in);
+        String carNames = inputCarNames();
+        int playCounts = inputPlayCounts();
+        return new Form(carNames, playCounts);
+    }
+
+    private static int inputPlayCounts() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         Scanner playScanner = new Scanner(System.in);
-        return new Form(carScanner.nextLine(), playScanner.nextInt());
+        return playScanner.nextInt();
+    }
+
+    private static String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        Scanner carScanner = new Scanner(System.in);
+        return carScanner.nextLine();
     }
 }
