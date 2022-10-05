913da374438311a28522b55f0e785cfd122aaf24
lkk6201 <lkk6201@gmail.com>
2022-10-05 22:04:41 +0900

    refactor: 2단계 코드리뷰 리팩토링 반영 - private 생성자 추가 - 커스텀 구분자 Pattern 상수 추가 - 숫자 검사용 정규식 상수 추가 - 기존 Matcher를 의미있는 이름으로 변경 - 문자열 덧셈 수행 메서드에서 숫자가 아닐 경우 RuntimeException을 throw 하는 로직 추가 - null 또는 빈 문자 테스트 케이스에 @NullAndEmptySource 어노테이션 적용

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -7,16 +7,21 @@ public class StringAddCalculator {
 
     private static final String BASE_SEPARATOR = ",|:";
     private static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile(CUSTOM_SEPARATOR);
+    private static final String ONLY_NUMBER_REGEXP = "^(-?)[0-9]+$";
+
+    private StringAddCalculator() {}
 
     public static int splitAndSum(String target) {
         if (isNullOrEmpty(target)) {
             return 0;
         }
 
-        Matcher m = Pattern.compile(CUSTOM_SEPARATOR).matcher(target);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            return getArraySum(m.group(2).split(customDelimiter));
+        Matcher customSeparatorMatcher = CUSTOM_SEPARATOR_PATTERN.matcher(target);
+
+        if (customSeparatorMatcher.find()) {
+            String customDelimiter = customSeparatorMatcher.group(1);
+            return getArraySum(customSeparatorMatcher.group(2).split(customDelimiter));
         }
 
         return getArraySum(target.split(BASE_SEPARATOR));
@@ -30,6 +35,10 @@ public class StringAddCalculator {
         int result = 0;
 
         for (String s : targetArr) {
+            if (!s.matches(ONLY_NUMBER_REGEXP)) {
+                throw new RuntimeException("숫자가 아닌 값은 포함될 수 없습니다.");
+            }
+
             int item = Integer.parseInt(s);
             if (0 > item) {
                 throw new RuntimeException("음수는 포함될 수 없습니다.");
