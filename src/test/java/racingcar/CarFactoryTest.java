1fa4c7ef22035d28809e45ac2c40bf5a66401f17
sasha-com <sasha.com@kakaocorp.com>
2022-10-05 17:24:46 +0900

    fix: CarFactoryTest 실패건 수정

--- src/test/java/racingcar/CarFactoryTest.java
+++ src/test/java/racingcar/CarFactoryTest.java
@@ -11,11 +11,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 
 public class CarFactoryTest {
-    private final String carNameList = "niro, tusan, genesis";
-
     @ParameterizedTest
     @MethodSource("getManufactoringCarsTestParams")
-    public void manufactoringCars(int numOfCars, int tryingCount) {
+    public void manufactoringCars(String carNameList, int numOfCars, int tryingCount) {
         List<Car> cars = CarFactory.manufactoringCars(carNameList, tryingCount);
         assertThat(cars.size()).isEqualTo(numOfCars);
         cars.forEach(car -> {
@@ -27,11 +25,11 @@ public class CarFactoryTest {
 
     private static Stream<Arguments> getManufactoringCarsTestParams() {
         return Stream.of(
-                Arguments.of(5, 3),
-                Arguments.of(3, 5),
-                Arguments.of(4, 4),
-                Arguments.of(10, 5),
-                Arguments.of(5, 10)
+                Arguments.of("niro,tusan,genesis,porsche,morning", 5, 3),
+                Arguments.of("niro,tusan,genesis", 3, 5),
+                Arguments.of("niro,tusan,genesis,porsche", 4, 4),
+                Arguments.of("niro,tusan,genesis,porsche,morning,niro,tusan,genesis,porsche,morning", 10, 5),
+                Arguments.of("niro,tusan,genesis,porsche,morning", 5, 10)
         );
     }
 }
