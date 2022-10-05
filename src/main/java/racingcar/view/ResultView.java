c2f9a57ab6f5a99b8355d50bc6c6a8a6ac858f99
boradol2 <boradol0902@gmail.com>
2022-10-06 00:03:06 +0900

    refactor : 개행 "\n" 처리 수정

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -9,28 +9,30 @@ import java.util.stream.Collectors;
 
 public class ResultView {
     private static final String COMMA = ",";
+    private static final String COLON = " : ";
     private static final String POSITION_MARK = "-";
     private static final String RESULT_MESSAGE = "실행결과";
     private static final String FINAL_WON_MESSAGE = " 가 최종 우승했습니다.";
+    private static final String NEW_LINE = System.lineSeparator();
 
     private ResultView() {
     }
 
     public static void printResultMessage() {
-        System.out.println("\n" + RESULT_MESSAGE + "\n");
+        System.out.println(NEW_LINE + RESULT_MESSAGE + NEW_LINE);
     }
 
     public static void printRacingCars(List<CarsDto> carsValues) {
         for (CarsDto carsValue : carsValues) {
             List<CarDto> values = carsValue.getValues();
             printCarsRound(values);
-            System.out.println();
+            System.out.println(NEW_LINE);
         }
     }
 
     public static void printWinner(List<Car> winnerValues) {
         List<String> winnerCarNames = winnerValues.stream().map(Car::getName).collect(Collectors.toList());
-        System.out.println("\n" + String.join(COMMA, winnerCarNames).concat(FINAL_WON_MESSAGE));
+        System.out.println(NEW_LINE + String.join(COMMA, winnerCarNames).concat(FINAL_WON_MESSAGE));
     }
 
     private static void printCarsRound(List<CarDto> values) {
@@ -40,7 +42,7 @@ public class ResultView {
     }
 
     private static void printCarPosition(CarDto value) {
-        System.out.print(value.getName() + " : ");
+        System.out.print(value.getName() + COLON);
         System.out.println(POSITION_MARK.repeat(value.getPosition()));
     }
 }
