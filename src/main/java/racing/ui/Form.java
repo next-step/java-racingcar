a59f56f827678bc59fa50dd81bbb0701e1f62c00
jimbaemon <jimbae@kakao.com>
2022-10-06 01:15:55 +0900

    refactor: Magic String to Constant variable

--- src/main/java/racing/ui/Form.java
+++ src/main/java/racing/ui/Form.java
@@ -1,11 +1,12 @@
 package racing.ui;
 
 public class Form {
+    public static final String CAR_NAMES_SEPARATOR = ",";
     private final String[] nameOfCars;
     private final int numberOfPlays;
 
     public Form(String nameOfCars, int numberOfPlays) {
-        this.nameOfCars = nameOfCars.split(",");
+        this.nameOfCars = nameOfCars.split(CAR_NAMES_SEPARATOR);
         this.numberOfPlays = numberOfPlays;
     }
 
