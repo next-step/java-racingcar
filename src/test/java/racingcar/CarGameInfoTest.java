8dd238aa794ea959553e60279c2e943fb28caa95
sasha-com <sasha.com@kakaocorp.com>
2022-10-05 17:33:23 +0900

    feat: CarGameInfoTest 테스트 클래스 추가

---
+++ src/test/java/racingcar/CarGameInfoTest.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class CarGameInfoTest {
+    private static Random random = new Random();
+
+    @ParameterizedTest
+    @MethodSource("getCarListParams")
+    public void init(List<Car> carList) {
+        CarGameInfo carGameInfo = new CarGameInfo(carList);
+    }
+
+    @Test
+    public void getWinnerList() {
+        List<Car> cars = List.of(new Car(random, "niro", 5), new Car(random, "tusan", 3),
+                                 new Car(random, "porsche", 7));
+        CarGameInfo carGameInfo = new CarGameInfo(cars);
+        List<Car> winners = carGameInfo.getWinnerList();
+        assertThat(winners).isNotNull();
+        assertThat(winners.size()).isGreaterThan(0);
+    }
+
+    private static Stream<Arguments> getCarListParams() {
+        return Stream.of(
+                Arguments.of(List.of(new Car(random, "niro", 5))),
+                Arguments.of(List.of(new Car(random, "niro", 5), new Car(random, "tusan", 3))),
+                Arguments.of(List.of(new Car(random, "niro", 5), new Car(random, "tusan", 3),
+                                     new Car(random, "porsche", 7))));
+    }
+}
