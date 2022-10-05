0a4e2e6aaeb64adb1b7320cb0b9f9dd654d761e0
Shin seongsu <tlstjdtn321@naver.com>
2022-10-05 23:53:16 +0900

    fix: 거리 표현 상수로 수정

--- src/main/java/racing/ui/ResultViewImpl.java
+++ src/main/java/racing/ui/ResultViewImpl.java
@@ -1,14 +1,10 @@
 package racing.ui;
 
-import java.util.StringJoiner;
 import racing.domain.Car;
 
 public class ResultViewImpl implements ResultView {
 
     private static final String BEGIN_TEXT = "실행 결과";
-    private static final String DISTANCE_DELIMETER = "-";
-    private static final String DELIMETER = " : ";
-    private static final String WINNER_TEXT = "가 최종 우승했습니다.";
 
     @Override
     public void start() {
@@ -17,21 +13,7 @@ public class ResultViewImpl implements ResultView {
 
     @Override
     public void print(Car car) {
-        StringJoiner stringJoiner = new StringJoiner(DELIMETER)
-            .add(car.getName())
-            .add(DISTANCE_DELIMETER.repeat(car.getPosition()));
-
-        System.out.println(stringJoiner);
-    }
-
-    @Override
-    public void enter() {
-        System.out.println();
-    }
-
-    @Override
-    public void winner(String winner) {
-        System.out.println(winner + WINNER_TEXT);
+        System.out.println("-".repeat(car.getDistance()));
     }
 
 }
