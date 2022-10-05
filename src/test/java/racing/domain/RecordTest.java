0d9bf956b9063e5f22b9c27421214d983c45b8d6
jimbaemon <jimbae@kakao.com>
2022-10-05 22:47:56 +0900

    test: 어떤 테스트를 만들어볼까 하다가 생성하는 테스트가 필요할듯 해서 만들어 봤습니다 :)

---
+++ src/test/java/racing/domain/RecordTest.java
@@ -0,0 +1,21 @@
+package racing.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RecordTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"붕붕이:2", "썡썡이:3", "씽씽이:4"}, delimiter = ':')
+    void 자동차의_상태를_저장하는_기록_생성(String carName, int moveCount) {
+        Car car = new Car(new AlwaysMovingRule(), carName);
+        for (int i = 0; i < moveCount; i++) {
+            car.move();
+        }
+        Record record = Record.record(car);
+
+        assertThat(record).isEqualTo(new Record(carName, moveCount));
+    }
+}
