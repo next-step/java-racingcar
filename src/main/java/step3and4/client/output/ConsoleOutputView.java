bab818a1ce5b821bee02a2ea31897aa5c408203f
kureungkureung <terry9611@naver.com>
2022-10-06 01:23:26 +0900

    feat(ConsoleOutputView): 우승자들 이름 출력

--- src/main/java/step3and4/client/output/ConsoleOutputView.java
+++ src/main/java/step3and4/client/output/ConsoleOutputView.java
@@ -1,6 +1,7 @@
 package step3and4.client.output;
 
 import step3and4.game.car.Cars;
+import step3and4.game.car.WinningCars;
 
 import java.util.List;
 
@@ -11,6 +12,18 @@ public class ConsoleOutputView implements OutputView {
         List<String> positionsAndNames = cars.positionsAndNames();
         positionsAndNames.forEach(System.out::println);
         System.out.println();
+    }
+
+    @Override
+    public void print(WinningCars winningCars) {
+        List<String> winners = winningCars.winners();
+        int numberOfWinners = winners.size();
+
+        for (int i = 0; i < numberOfWinners - 1; i++) {
+            System.out.print(winners.get(i) + ", ");
+        }
+
+        System.out.print(winners.get(numberOfWinners - 1) + "가 최종 우승했습니다.");
     }
 
 }
--- src/main/java/step3and4/client/output/OutputView.java
+++ src/main/java/step3and4/client/output/OutputView.java
@@ -1,10 +1,12 @@
 package step3and4.client.output;
 
 import step3and4.game.car.Cars;
+import step3and4.game.car.WinningCars;
 
-@FunctionalInterface
 public interface OutputView {
 
     void print(Cars cars);
+
+    void print(WinningCars winningCars);
 
 }
