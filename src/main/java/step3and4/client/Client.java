adcd270a923d553f5cfd185e48ee53655819ac94
kureungkureung <terry9611@naver.com>
2022-10-05 23:43:37 +0900

    refactor(Client): 출력 담당 클래스 명 수정

--- src/main/java/step3and4/client/Client.java
+++ src/main/java/step3and4/client/Client.java
@@ -2,7 +2,7 @@ package step3and4.client;
 
 import step3and4.client.input.ConsoleInputView;
 import step3and4.client.number.RandomNumber;
-import step3and4.client.output.BarOutputView;
+import step3and4.client.output.ConsoleOutputView;
 import step3and4.game.CarGame;
 
 import java.io.BufferedReader;
@@ -14,7 +14,7 @@ public class Client {
     private static final BufferedReader BUFFERED_READER = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        CarGame carGame = new CarGame(new RandomNumber(), new ConsoleInputView(), new BarOutputView());
+        CarGame carGame = new CarGame(new RandomNumber(), new ConsoleInputView(), new ConsoleOutputView());
         carGame.run();
         BUFFERED_READER.close();
     }
