ed09e0ce4bbb23ba82dfbfeb9637d5432d990e97
swsong <ssw890720@gmail.com>
2022-10-05 13:05:19 +0900

    refactor: CarFactory클래스 Car 초기화시 디폴트값 추가

--- src/main/java/racinggame/domain/CarFactory.java
+++ src/main/java/racinggame/domain/CarFactory.java
@@ -8,12 +8,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CarFactory {
+
+    public static final int DEFAULT_CAR_DISTANCE = 0;
+
     private CarFactory() {
     }
 
     public static RacingCars produce(RaceInputDTO dto) {
         List<Car> cars = Arrays.stream(dto.getCarNames())
-                .map(name -> new Car(name, new CarDistance(0)))
+                .map(name -> new Car(name, new CarDistance(DEFAULT_CAR_DISTANCE)))
                 .collect(Collectors.toList());
 
         return new RacingCars(cars);
