0b61d4e9a26965ef03f2347dde478fbaa4d3be3c
Shin seongsu <tlstjdtn321@naver.com>
2022-10-06 01:40:56 +0900

    feat: 이름 5자이면 에러를 반환

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,11 +1,22 @@
 package racing.domain;
 
+import java.util.Objects;
+
 public class Car {
 
+    private static final int NAME_LENGTH = 5;
+    private static final String LENGTH_ERROR_TEXT = "이름은 5자를 초과할 수 없습니다.";
+
+    private String name;
     private int distance;
 
-    public Car() {
-        this.distance = 0;
+    public Car(String name, int distance) {
+        this.name = validateName(name);
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getDistance() {
@@ -16,4 +27,28 @@ public class Car {
         distance += movableStrategy.randomMovable();
     }
 
+    private String validateName(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException(LENGTH_ERROR_TEXT);
+        }
+
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return distance == car.distance && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, distance);
+    }
 }
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -1,6 +1,7 @@
 package racing.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,7 @@ class CarTest {
     @DisplayName("앞으로 한칸 전지")
     @Test
     void 전진하는_경우_테스트() {
-        Car car = new Car();
+        Car car = new Car("pobi", 0);
         car.move(() -> 1);
 
         assertThat(car.getDistance()).isEqualTo(1);
@@ -19,10 +20,26 @@ class CarTest {
     @DisplayName("제자리 걸음")
     @Test
     void 제자리에_있는_테스트() {
-        Car car = new Car();
+        Car car = new Car("pobi", 0);
         car.move(() -> 0);
 
         assertThat(car.getDistance()).isEqualTo(0);
+    }
+
+    @DisplayName("생성자 테스트")
+    @Test
+    void 생성자_테스트() {
+        Car car = new Car("pobi",0 );
+
+        assertThat(car).isEqualTo(new Car("pobi", 0));
+    }
+
+    @DisplayName("이름이 5자 이상이면 에러를 반환")
+    @Test
+    void 이름이_5자_이상이면_에러_반환() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Car("123456", 1);
+        }).withMessage("이름은 5자를 초과할 수 없습니다.");
     }
 
 }
