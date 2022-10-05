9d08709d2d0633ad527673379c4aa88ba3e167e9
Jackson Hong <jacksonhong0111@gmail.com>
2022-10-05 22:24:30 +0900

    [delete] 불필요한 코드 삭제

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -25,12 +25,4 @@ public class StringAddCalculator {
         }
         return result;
     }
-
-    public static void main(String[] args) {
-        InputText test1 = InputText.from("jackson");
-        InputText test2 = InputText.from("jason");
-
-        System.out.println(System.identityHashCode(test1.getPattern()));
-        System.out.println(System.identityHashCode(test2.getPattern()));
-    }
 }
