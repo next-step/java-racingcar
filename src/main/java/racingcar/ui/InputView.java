a991cd009cb42db53a69d8b1d7b786a57658234f
shineyuno <shineyuno@gmail.com>
2022-10-05 21:24:09 +0900

    feat (InputView): 자동차 이름은 쉼표(,)를 기준으로 구분한다.

--- src/main/java/racingcar/ui/InputView.java
+++ src/main/java/racingcar/ui/InputView.java
@@ -2,12 +2,12 @@ package racingcar.ui;
 
 import util.NumberUtil;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class InputView {
-    private static final String CAR_DESCRIPTION = "자동차 대수는 몇 대 인가요?";
+    private static final String CAR_DESCRIPTION = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String TRY_DESCRIPTION = "시도할 횟수는 몇 회 인가요?";
 
     private static final Scanner scanner= new Scanner(System.in);
@@ -20,5 +20,20 @@ public class InputView {
     public static int getTryCount() {
         System.out.println(TRY_DESCRIPTION);
         return NumberUtil.getPositiveInteger(scanner.nextLine());
+    }
+
+    public static List<String> getCarNames() {
+        System.out.println(CAR_DESCRIPTION);
+        String inputCarNames = scanner.nextLine();
+        return convertNameList(inputCarNames);
+    }
+
+    public static List<String> convertNameList(String inputNames) {
+        if (inputNames == null || inputNames.isEmpty()) {
+            throw new RuntimeException("자동차 이름을 한글자 이상 입력해주세요.");
+        }
+        String[] names = inputNames.split(",");
+
+        return Arrays.asList(names);
     }
 }
