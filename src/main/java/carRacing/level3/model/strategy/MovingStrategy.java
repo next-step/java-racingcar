cfa5e82878a7d4bae96d0a784e70e7e8eeb7b85f
sienna <20200803@sungshin.ac.kr>
2022-10-05 12:05:52 +0900

    :recycle:refactor : 불필요한 메소드 분리 및 테스트 삭제

--- src/main/java/carRacing/level3/model/strategy/MovingStrategy.java
+++ src/main/java/carRacing/level3/model/strategy/MovingStrategy.java
@@ -4,5 +4,4 @@ public interface MovingStrategy {
 
 	Boolean isMovable();
 
-	Boolean movingStandard(int num);
 }
--- src/test/java/carRacing/level3/RandomNumStrategyTest.java
+++ src/test/java/carRacing/level3/RandomNumStrategyTest.java
@@ -2,43 +2,17 @@ package carRacing.level3;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import carRacing.level3.model.strategy.RandomMovingStrategy;
 import carRacing.level3.model.strategy.RandomNum;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class RandomNumStrategyTest {
-
-	RandomMovingStrategy movingStrategy = new RandomMovingStrategy();
 	RandomNum randomNum = new RandomNum();
 
 	@Test
 	@DisplayName("randomNum()는 0~9까지의 랜덤값을 생성해준다")
 	void getRandomNum() {
 		assertThat(randomNum.randomNum()).isLessThan(10);
-	}
-
-	@ParameterizedTest
-	@ValueSource(ints = {6, 7, 4})
-	@DisplayName("isMoving()는 랜덤값이 4이상이면 True를 반환한다.")
-	void isTrue(int value) {
-		assertThat(movingStrategy.movingStandard(value)).isTrue();
-	}
-
-	@ParameterizedTest
-	@ValueSource(ints = {1, 2, 3})
-	@DisplayName("랜덤값이 4 미만이면 false를 반환한다.")
-	void isFalse(int value) {
-		assertThat(movingStrategy.movingStandard(value)).isFalse();
-	}
-
-	@ParameterizedTest
-	@ValueSource(ints = {-5, 10})
-	@DisplayName("랜덤값이 0~10범위가 아니라면 false를 반환한다.")
-	void 랜덤값_예외_테스트(int value) {
-		assertThat(movingStrategy.movingStandard(value)).isFalse();
 	}
 
 }
