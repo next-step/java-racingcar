d3239527a5a6dbcd3b682db0f0a940588a288c7f
Shin seongsu <tlstjdtn321@naver.com>
2022-10-05 23:51:43 +0900

    fix: 상수로 명확하게 표현하기

--- src/main/java/racing/domain/CarMovableStrategy.java
+++ src/main/java/racing/domain/CarMovableStrategy.java
@@ -6,14 +6,17 @@ public class CarMovableStrategy implements MovableStrategy {
 
     private final static int RANDOM_RANGE = 10;
     private final static int RACING_CONDITION = 4;
-    private final static Random RANDOM = new Random();
+    private final static int RUN = 1;
+    private final static int STOP = 0;
+
+    private final Random RANDOM = new Random();
 
     @Override
     public int randomMovable() {
         int random = RANDOM.nextInt(RANDOM_RANGE);
         if(RACING_CONDITION <= random) {
-            return 1;
+            return RUN;
         }
-        return 0;
+        return STOP;
     }
 }
