5c2c8abe4e4e7b5b899378cb979241304b73aafa
Shin seongsu <tlstjdtn321@naver.com>
2022-10-06 01:26:44 +0900

    feat: 이름 값 추가

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,48 +1,19 @@
 package racing.domain;
 
-import java.util.Objects;
-
 public class Car {
 
-    private Name name;
-    private Position position;
+    private int distance;
 
-    public Car(String name, int position) {
-        this.name = new Name(name);
-        this.position = new Position(position);
+    public Car() {
+        this.distance = 0;
     }
 
-    public String getName() {
-        return name.getName();
-    }
-
-    public int getPosition() {
-        return position.getPosition();
+    public int getDistance() {
+        return distance;
     }
 
     public void move(MovableStrategy movableStrategy) {
-        position.move(movableStrategy);
+        distance += movableStrategy.randomMovable();
     }
 
-    public boolean equalsPosition(int maxPosition) {
-        return position.equalsPosition(maxPosition);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Car car = (Car) o;
-        return Objects.equals(name, car.name) && Objects.equals(position,
-            car.position);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, position);
-    }
 }
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -7,12 +7,22 @@ import org.junit.jupiter.api.Test;
 
 class CarTest {
 
-    @DisplayName("생성자 테스트")
+    @DisplayName("앞으로 한칸 전지")
     @Test
-    void 생성자_테스트() {
-        Car car = new Car("pobi",0 );
+    void 전진하는_경우_테스트() {
+        Car car = new Car();
+        car.move(() -> 1);
 
-        assertThat(car).isEqualTo(new Car( "pobi", 0));
+        assertThat(car.getDistance()).isEqualTo(1);
+    }
+
+    @DisplayName("제자리 걸음")
+    @Test
+    void 제자리에_있는_테스트() {
+        Car car = new Car();
+        car.move(() -> 0);
+
+        assertThat(car.getDistance()).isEqualTo(0);
     }
 
 }
