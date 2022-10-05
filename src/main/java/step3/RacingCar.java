1a702a8e6085f3bf8bf5a5cd3acdad3a8c230fd6
marco.keem <marco.keem@kakaocorp.com>
2022-10-06 00:56:47 +0900

    실행결과 화면과 동일하도록 공백라인 출력 추가

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -4,6 +4,7 @@ public class RacingCar {
     public static void main(String[] args) {
         RaceInput racingInput = new RaceInput();
         RaceController raceController = new RaceController(racingInput);
+        System.out.println("실행 결과");
         raceController.startRacing();
     }
 }
