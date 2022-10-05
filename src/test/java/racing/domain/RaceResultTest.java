a82c330581b25f13c599a3878da5082fe0bac282
jimbaemon <jimbae@kakao.com>
2022-10-06 01:08:48 +0900

    test: 경기 결과에 경기 기록 등록 테스트 추가.

--- src/test/java/racing/domain/RaceResultTest.java
+++ src/test/java/racing/domain/RaceResultTest.java
@@ -1,12 +1,30 @@
 package racing.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RaceResultTest {
+
+    @Test
+    void 레코드_등록_테스트() {
+        Car 빵빵이 = new Car(new AlwaysMovingRule(), "빵빵이");
+        빵빵이.move();
+        빵빵이.move();
+
+        RaceResult raceResult = new RaceResult();
+        raceResult.addRecord(빵빵이);
+
+        assertThat(raceResult).isEqualTo(
+            new RaceResult(
+                List.of(
+                    new Record("빵빵이", 2)
+                )
+            )
+        );
+    }
 
     @Test
     void 기록중_가장_멀리간_기록_조회() {
@@ -14,12 +32,12 @@ class RaceResultTest {
         Record 씽씽이 = new Record("씽씽이", 4);
         Record 쌩쌩이 = new Record("쌩쌩이", 5);
         RaceResult raceResult = new RaceResult(
-                List.of(빵빵이, 씽씽이, 쌩쌩이)
+            List.of(빵빵이, 씽씽이, 쌩쌩이)
         );
 
-        Assertions.assertThat(raceResult.winners())
-                .hasSize(2)
-                .containsExactly(빵빵이, 쌩쌩이);
+        assertThat(raceResult.winners())
+            .hasSize(2)
+            .containsExactly(빵빵이, 쌩쌩이);
     }
 
 }
