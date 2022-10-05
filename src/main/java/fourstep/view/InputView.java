ffc265c4f32401424e4c888c0eadfbda66e8f43c
isakchoe <isakchoe@naver.com>
2022-10-05 23:53:12 +0900

    refactor: InputView
    입력값 변화에 따른 변수명과 출력문 변경 
    inputCarCount --> inputCarName

--- src/main/java/fourstep/view/InputView.java
+++ src/main/java/fourstep/view/InputView.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String[] inputCarCount() {
+    public static String[] inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         try {
             String str = scanner.nextLine();
