b09a25cb02d7ee1ac0f71bf2484a0ccb999b187e
wugatama <dnr7410@naver.com>
2022-10-05 12:04:41 +0900

    refactor: final 키워드

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Cars {
-	private List<Car> cars;
+	private final List<Car> cars;
 
 	public Cars(final List<Car> cars) {
 		this.cars = cars;
