a4c0d04ff035ffd71d18c5712e406cfdde600cdf
kureungkureung <terry9611@naver.com>
2022-10-05 23:42:55 +0900

    feat(CarGame): 출력 기능 추가

--- src/main/java/step3and4/game/CarGame.java
+++ src/main/java/step3and4/game/CarGame.java
@@ -37,6 +37,7 @@ public class CarGame {
     private void useTurns(Cars cars, int numberOfGames) {
         for (int i = 0; i < numberOfGames; i++) {
             cars = cars.carsWithTurnUsed();
+            outputView.print(cars);
         }
     }
 
