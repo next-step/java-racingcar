f5ae42f2e21f63a5bdccc35020f4c11942a9d446
yogunsunwoo <dnaodtjs@gmail.com>
2022-10-06 10:13:42 +0900

    [cc-java-15] stage3 - add Car class and testCode

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class Car {
+
+    private int distance;
+
+    public int getDistance(){
+        return this.distance;
+    }
+
+    public void move(){
+        this.distance = this.distance + 1;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < this.getDistance(); i++) {
+            result.append("-");
+        }
+        return result.toString();
+    }
+}
---
+++ src/test/java/CarTest.java
@@ -0,0 +1,31 @@
+import domain.Car;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class CarTest {
+
+
+    @Test
+    public void 자동차_생성처리(){
+        Car car = new Car();
+        assertThat(car.getDistance()).isEqualTo(0);
+
+    }
+
+    @Test
+    public void 자동차_이동처리() {
+        Car car = new Car();
+        car.move();
+        car.move();
+        car.move();
+        assertThat(car.getDistance()).isEqualTo(3);
+    }
+
+    @Test
+    public void 자동차_이동거리출력() {
+        Car car = new Car();
+        car.move();
+        car.move();
+        assertThat(car.toString()).isEqualTo("--");
+    }
+}
