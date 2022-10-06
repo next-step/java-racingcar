0ae3124951ff5d4fa5ac187589857207f00597c8
Ye-Ji-Kim <kyeoji0128@naver.com>
2022-10-06 11:15:48 +0900

    test: 우승자 알려주는 로직 테스트

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class RacingGameTest {
@@ -21,5 +22,17 @@ public class RacingGameTest {
         racingGame.tryGame();
 
         assertThat(racingGame.getCars().get(0).getPosition()).isEqualTo(3);
+    }
+
+    @Test
+    void testGetWinnerNames() {
+        RacingGame racingGame = new RacingGame();
+        racingGame.createCars(List.of("winner1", "winner2", "loser"));
+
+        List<Car> cars = racingGame.getCars();
+        cars.get(0).forward();
+        cars.get(1).forward();
+
+        assertThat(racingGame.getWinnerNames()).containsOnly("winner1", "winner2");
     }
 }
