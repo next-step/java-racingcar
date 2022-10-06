e11c3827816276ee8c9da3b08206c893dc66c5d9
scoopsoup <rnrdud46@nate.com>
2022-10-06 22:15:51 +0900

    feat : Car클래스 필드값 변경(자동차 이름, 위치)

--- src/main/java/racingcarwinner/Car.java
+++ src/main/java/racingcarwinner/Car.java
@@ -3,13 +3,38 @@ package racingcarwinner;
 import java.util.Map;
 
 public class Car {
-    private Map<String, Integer> participant;
+//    private Map<String, Integer> participant;
+//
+//    public Map<String, Integer> getParticipant() {
+//        return this.participant;
+//    }
+//
+//    public void setParticipant(Map<String, Integer> participant) {
+//        this.participant = participant;
+//    }
 
-    public Map<String, Integer> getParticipant() {
-        return this.participant;
+    private String carName;
+    private int position;
+    private static final int DEFAULT_POSITION = 0;
+
+    public Car(String carName) {
+        this.carName = carName;
+        this.position = DEFAULT_POSITION;
     }
 
-    public void setParticipant(Map<String, Integer> participant) {
-        this.participant = participant;
+    public String getCarName() {
+        return this.carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
     }
 }
