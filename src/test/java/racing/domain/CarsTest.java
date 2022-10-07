d5f79b13efb6ad2ef7b8329f87e108dfa3f5f00f
Shin seongsu <tlstjdtn321@naver.com>
2022-10-08 01:08:42 +0900

    fix: 중복된 테스트 제거

--- src/test/java/racing/domain/CarsTest.java
+++ src/test/java/racing/domain/CarsTest.java
@@ -8,31 +8,11 @@ import org.junit.jupiter.api.Test;
 
 class CarsTest {
 
-    @DisplayName("모든 자동차들이 전진")
-    @Test
-    void 전진_테스트() {
-        Cars cars = new Cars(List.of(new Car("pobi", 0), new Car("crong", 0)));
-        cars.move(() -> 1);
-
-        assertThat(cars.getCars().stream()
-            .map(Car::getDistance)).containsExactly(1, 1);
-    }
-
-    @DisplayName("모든 자동차 제자리")
-    @Test
-    void 제자리_테스트() {
-        Cars cars = new Cars(List.of(new Car("pobi", 0), new Car("crong", 0)));
-        cars.move(() -> 0);
-
-        assertThat(cars.getCars().stream()
-            .map(Car::getDistance)).containsExactly(0, 0);
-    }
-
     @DisplayName("최종 우승자가 여러명 일떄")
     @Test
     void 최종_우승자가_여러명일떄() {
         Cars cars = new Cars(List.of(new Car("pobi", 0), new Car("crong", 0)));
-        cars.move(() -> 1);
+        cars.move(() -> true);
 
         assertThat(cars.findWinners()).isEqualTo("pobi, crong");
     }
@@ -41,7 +21,7 @@ class CarsTest {
     @Test
     void 최종_우승자가_한명일떄() {
         Cars cars = new Cars(List.of(new Car("pobi", 1), new Car("crong", 0)));
-        cars.move(() -> 1);
+        cars.move(() -> true);
 
         assertThat(cars.findWinners()).isEqualTo("pobi");
     }
