d1db4205842bd3f65a2ae4d10a6b68756c43340c
caoyu-dev <choyumin01@gmail.com>
2022-10-08 03:05:40 +0900

    [feat] Winner 우승자 객체

--- src/main/java/racingcar/domain/Winners.java
+++ src/main/java/racingcar/domain/Winners.java
@@ -1,2 +1,29 @@
-package racingcar.domain;public class Winners {
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winners {
+
+    public static List<Car> findWinners(List<Car> cars) {
+        return findWinners(cars, getMaxPosition(cars));
+    }
+
+    private static List<Car> findWinners(List<Car> cars, int maxPosition) {
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.hasMaxPosition(maxPosition)) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    private static int getMaxPosition(List<Car> cars) {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            maxPosition = car.comparePosition(maxPosition);
+        }
+        return maxPosition;
+    }
 }
