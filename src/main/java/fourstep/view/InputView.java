cbd4a395e5e31386c78455179b6cfa988ecc2e2c
isakchoe <isakchoe@naver.com>
2022-10-08 14:11:25 +0900

    refactor : InputVIew
    -inputCarName : 입력 메세지 변경, 리턴값 변경
    -에러 처리 추가

--- src/main/java/fourstep/view/InputView.java
+++ src/main/java/fourstep/view/InputView.java
@@ -4,30 +4,30 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static String[] inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         try {
-            String str = scanner.nextLine();
+            String str = scanner.next();
             String[] strArr = str.split(",");
             return strArr;
-        }
-        catch (InputMismatchException e) {
+        } catch (InputMismatchException e) {
             System.out.println("정수값만 입력해 주세요.");
+            scanner = new Scanner(System.in);
+            return inputCarName();
         }
-        return new String[1];
     }
 
     public static int inputRaceCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         try {
             return scanner.nextInt();
-        }
-        catch (InputMismatchException e) {
+        } catch (InputMismatchException e) {
             System.out.println("정수값만 입력해 주세요.");
+            scanner = new Scanner(System.in);
+            return inputRaceCount();
         }
-        return 0;
     }
 }
 
