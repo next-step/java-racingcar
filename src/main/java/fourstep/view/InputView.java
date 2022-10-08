819d82deb1a97acf6876fd3a81f6b4480378a166
isakchoe <isakchoe@naver.com>
2022-10-08 14:27:03 +0900

    refactor: InputView
    - inputCarName 에러 처리 try/catch 제거

--- src/main/java/fourstep/view/InputView.java
+++ src/main/java/fourstep/view/InputView.java
@@ -8,15 +8,10 @@ public class InputView {
 
     public static String[] inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        try {
-            String str = scanner.next();
-            String[] strArr = str.split(",");
-            return strArr;
-        } catch (InputMismatchException e) {
-            System.out.println("정수값만 입력해 주세요.");
-            scanner = new Scanner(System.in);
-            return inputCarName();
-        }
+
+        String str = scanner.next();
+        String[] strArr = str.split(",");
+        return strArr;
     }
 
     public static int inputRaceCount() {
