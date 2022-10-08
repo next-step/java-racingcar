937248321703456453a019344f044c6db6c3c0d0
jiokim <jiokim222@gmail.com>
2022-10-08 17:11:31 +0900

    refactor(RandomMoveStrategy): 숫자 생성

--- src/main/java/racingcar/backend/domain/RandomMoveStrategy.java
+++ src/main/java/racingcar/backend/domain/RandomMoveStrategy.java
@@ -5,14 +5,9 @@ import racingcar.backend.utils.RandomUtils;
 public class RandomMoveStrategy implements MoveStrategy {
 
     private static final int MOVING_CONDITION_MIN_VALUE = 4;
-    private int randomNumber;
-
-    public RandomMoveStrategy() {
-        this.randomNumber = RandomUtils.generate();
-    }
 
     @Override
     public boolean movable() {
-        return MOVING_CONDITION_MIN_VALUE <= randomNumber;
+        return MOVING_CONDITION_MIN_VALUE <= RandomUtils.generate();
     }
 }
