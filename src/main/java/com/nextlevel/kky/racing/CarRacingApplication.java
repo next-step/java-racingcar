97fe06fd1dc5ebc911148ce8dfde2d8f2ca75cbb
kkywalk2 <kkywalk2@gmail.com>
2022-10-08 21:36:51 +0900

    feat: CarRacingApplication 코드 수정

--- src/main/java/com/nextlevel/kky/racing/CarRacingApplication.java
+++ src/main/java/com/nextlevel/kky/racing/CarRacingApplication.java
@@ -1,19 +1,28 @@
 package com.nextlevel.kky.racing;
 
+import com.nextlevel.kky.racing.core.CarNameValidator;
 import com.nextlevel.kky.racing.core.CarRacingExecutor;
 import com.nextlevel.kky.racing.ui.InputView;
 import com.nextlevel.kky.racing.ui.ResultView;
 
+import java.util.Arrays;
+
 public class CarRacingApplication {
 
     public static void main(String[] args) {
-        int carCount = InputView.inputNumber("자동차 대수는 몇 대 인가요?");
+        String[] carNames = InputView.inputString("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+
+        if(!CarNameValidator.checkValidation(carNames)) {
+            System.out.println("유효하지 않은 이름 입니다!");
+            return;
+        }
+
         int roundCount = InputView.inputNumber("시도할 회수는 몇 회 인가요?");
 
-        CarRacingExecutor carRacingExecutor = new CarRacingExecutor(carCount);
+        CarRacingExecutor carRacingExecutor = new CarRacingExecutor(carNames);
 
         for (; roundCount > 0; roundCount--) {
-            ResultView.printCarRacingResult(carRacingExecutor);
+            ResultView.printCarRacingResultWithName(carRacingExecutor);
             carRacingExecutor.proceedNextRound();
         }
     }
