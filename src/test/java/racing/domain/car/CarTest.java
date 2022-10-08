62484cd1b175b0f9fc4c4a04c365b6f82154e050
김재훈 <timewarp96@naver.com>
2022-10-08 19:25:37 +0900

    [test] 테스트 수정

--- src/test/java/racing/domain/car/CarTest.java
+++ src/test/java/racing/domain/car/CarTest.java
@@ -10,20 +10,20 @@ class CarTest {
     void 자동차는_조건에_맞으면_움직일_수_있다() {
         Car car = new Car("name");
         car.move(() -> true);
-        assertThat(car.getPosition()).isEqualTo(2);
+        assertThat(car.getPosition()).isEqualTo(1);
     }
 
     @Test
     void 자동차는_조건에_안맞으면_움직이지_않는다() {
         Car car = new Car("name");
         car.move(() -> false);
-        assertThat(car.getPosition()).isEqualTo(1);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @Test
     void 앞선_위치값을_가진_자동차가_선두_그룹이다() {
-        Car leader = new Car("leader", 2);
-        Car follower = new Car("follower", 1);
+        Car leader = new Car("lead", 2);
+        Car follower = new Car("loser", 1);
         assertThat(leader.leading(follower)).isEqualTo(leader);
     }
 }
