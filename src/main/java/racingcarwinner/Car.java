fedb35190b8d9503fa56ff06d36f89277b85c5be
scoopsoup <rnrdud46@nate.com>
2022-10-08 17:26:59 +0900

    chore : 주석 제거

--- src/main/java/racingcarwinner/Car.java
+++ src/main/java/racingcarwinner/Car.java
@@ -3,15 +3,6 @@ package racingcarwinner;
 import java.util.Map;
 
 public class Car {
-//    private Map<String, Integer> participant;
-//
-//    public Map<String, Integer> getParticipant() {
-//        return this.participant;
-//    }
-//
-//    public void setParticipant(Map<String, Integer> participant) {
-//        this.participant = participant;
-//    }
 
     private String carName;
     private int position;
