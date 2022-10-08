61d401aa3989b767005c7d6725ad9f4e0ae803cb
scoopsoup <rnrdud46@nate.com>
2022-10-08 17:24:23 +0900

    feat : 불필요 인스턴스변수 제거

--- src/main/java/racingcarwinner/RacingCarWinner.java
+++ src/main/java/racingcarwinner/RacingCarWinner.java
@@ -8,7 +8,6 @@ public class RacingCarWinner {
     private static final int ADVANCE_MIN_VALUE = 4;
     private String participantName;
     private int carMovingCount;
-    private int participantNumber;
     private List<Car> carList = new ArrayList<>();
 
     public RacingCarWinner(String participantName, int carMovingCount) {
@@ -18,8 +17,7 @@ public class RacingCarWinner {
 
     public void splitParticipantName() {
         String[] participantNames = participantName.split(",");
-        participantNumber = participantNames.length;
-        for (int i = 0; i < participantNumber; i++) {
+        for (int i = 0; i < participantNames.length; i++) {
             carList.add(new Car(participantNames[i]));
         }
     }
@@ -34,7 +32,7 @@ public class RacingCarWinner {
 
     public List<Integer> racingCarMove() {
         List<Integer> carMovement = new ArrayList<>();
-        for (int i = 0; i < participantNumber; i++) {
+        for (int i = 0; i < carList.size(); i++) {
             carMovement.add(i, judgmentGoStop());
         }
         return carMovement;
@@ -43,7 +41,7 @@ public class RacingCarWinner {
     public String updateParticipantInformation() {
         List<Integer> carMovement = racingCarMove();
         int eachNumberOfMove = 0;
-        for (int i = 0; i < participantNumber; i++) {
+        for (int i = 0; i < carList.size(); i++) {
             eachNumberOfMove = carList.get(i).getPosition() + carMovement.get(i);
             carList.get(i).setPosition(eachNumberOfMove);
         }
@@ -52,7 +50,7 @@ public class RacingCarWinner {
 
     public String printGameStatus() {
         String printingStatus = "";
-        for (int i = 0; i < participantNumber; i++) {
+        for (int i = 0; i < carList.size(); i++) {
             printingStatus += carList.get(i).getCarName() + " : " + printCarBar(i) + "\n";
         }
         printingStatus += "\n";
@@ -71,7 +69,7 @@ public class RacingCarWinner {
     public String winner() {
         int max = carList.get(0).getPosition();
         String winnerParticipant = carList.get(0).getCarName();
-        for (int i = 1; i < participantNumber; i++) {
+        for (int i = 1; i < carList.size(); i++) {
             if (max == carList.get(i).getPosition()) {
                 winnerParticipant += "," + carList.get(i).getCarName();
             }
