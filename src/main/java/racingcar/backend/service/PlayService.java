86419fefbc6eb871a062b9218cd45add09647869
jiokim <jiokim222@gmail.com>
2022-10-08 16:57:29 +0900

    feat(PlayService): carRecord 생성

--- src/main/java/racingcar/backend/service/PlayService.java
+++ src/main/java/racingcar/backend/service/PlayService.java
@@ -2,13 +2,26 @@ package racingcar.backend.service;
 
 import racingcar.backend.domain.*;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class PlayService {
 
-    public GameRecords play(Cars cars, int times) {
-        GameRecords gameRecords = new GameRecords();
-        for (int round = 0; round < times; round++) {
-            gameRecords.add(cars.play());
+    public static Cars playCar(Cars cars, MoveStrategy moveStrategy) {
+        for (Car car : cars.getValues()) {
+            car.move(moveStrategy);
         }
-        return gameRecords;
+        return cars;
+    }
+
+    public static GameRecord getGameRecord(Cars cars) {
+        Map<CarName, Position> record = cars.getValues().stream()
+                .collect(Collectors.toMap(
+                        car -> new CarName(car.getName()),
+                        car -> new Position(car.getPosition()),
+                        (u, v) -> v,
+                        LinkedHashMap::new));
+        return GameRecord.create(record);
     }
 }
