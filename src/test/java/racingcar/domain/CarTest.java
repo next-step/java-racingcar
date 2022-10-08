7081e8c14945b7ec878c7770dcd7300c6773f959
boradol2 <boradol0902@gmail.com>
2022-10-09 01:00:03 +0900

    refactor(CarTest): 테스트명 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CarTest {
     @ParameterizedTest
     @ValueSource(strings = {"pobi"})
-    @DisplayName("이름을 입력하면 자동차가 생성되고, 그 자동차가 움직인다면 해당 자동차의 위치가 1 증가한다.")
+    @DisplayName("자동차가 움직인다면 해당 자동차의 위치가 1 증가한다.")
     void moveTest(String input) {
         Car car = new Car(input);
         car.moves(() -> true);
@@ -19,7 +19,7 @@ class CarTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"conan"})
-    @DisplayName("이름을 입력하면 자동차가 생성되고, 그 자동차가 정지한다면 해당 자동차의 위치가 0이다.")
+    @DisplayName("자동차가 움직이지 않는다면 해당 자동차의 위치가 0이다.")
     void stopTest(String input) {
         Car car = new Car(input);
         car.moves(() -> false);
