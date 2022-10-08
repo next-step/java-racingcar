21bb88926822103f34454712291094206a1a7b26
isakchoe <isakchoe@naver.com>
2022-10-08 17:55:19 +0900

    refactor: Race
    -winner 변수명 --> winners 로 수정 
    -게터 메서드 위치 변경 
    -isMaxLocation 메서드명 ->checkMaxCar

--- src/main/java/fourstep/logic/Race.java
+++ src/main/java/fourstep/logic/Race.java
@@ -8,11 +8,15 @@ import java.util.stream.IntStream;
 public class Race {
 
     private ArrayList<Car> cars = new ArrayList<>();
-    private ArrayList<String> winners = new ArrayList<>();
+    private final ArrayList<String> winners = new ArrayList<>();
 
     public Race(String[] carNames) {
         int carCount = carNames.length;
         IntStream.range(0, carCount).forEach(i -> cars.add(new Car(carNames[i])));
+    }
+
+    public Race(ArrayList<Car> cars) {
+        this.cars = cars;
     }
 
     public void play(int raceCount, RandomNumber random) {
@@ -21,13 +25,20 @@ public class Race {
             carsMove(random);
             ResultView.printOneRace(cars);
         }
-        getWinner();
+        getWinners();
         ResultView.printWinner(winners);
     }
 
-    public void carsMove(RandomNumber random) {
+    private void carsMove(RandomNumber random) {
         for (Car car : cars) {
             car.move(random.getRandomNumber());
+        }
+    }
+
+
+    private void checkMaxCar(Car car, int maxLocation) {
+        if (car.getLocation() == maxLocation) {
+            winners.add(car.getName());
         }
     }
 
@@ -39,17 +50,11 @@ public class Race {
         return maxLocation;
     }
 
-    public ArrayList<String> getWinner() {
+    public ArrayList<String> getWinners() {
         int maxLocation = getMaxLocation();
         for (Car car : cars) {
-            isMaxLocation(car, maxLocation);
+            checkMaxCar(car, maxLocation);
         }
         return winners;
-    }
-
-    private void isMaxLocation(Car car, int maxLocation) {
-        if (car.getLocation() == maxLocation) {
-            winners.add(car.getName());
-        }
     }
 }
