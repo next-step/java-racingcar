cd8577c24a5b4702dc836643692845573e4e4c91
sienna <20200803@sungshin.ac.kr>
2022-10-09 08:44:41 +0900

    :white_check_mark:test : location은 음수가 될 수 없다

---
+++ src/test/java/carRacing/level3/LocationTest.java
@@ -0,0 +1,21 @@
+package carRacing.level3;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import carRacing.level3.infra.error.NameLengthException;
+import carRacing.level3.model.Location;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LocationTest {
+
+
+	@Test
+	@DisplayName("위치는 음수가 될 수 없다")
+	void 위치_음수_테스트(){
+		assertThatThrownBy(() -> {
+			new Location(-1);
+		}).isInstanceOf(RuntimeException.class);
+	}
+
+}
