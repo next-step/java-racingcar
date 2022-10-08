e1d07f71e19f7b2563d8c1e11f55548493951f7d
isakchoe <isakchoe@naver.com>
2022-10-08 17:56:29 +0900

    refactor : RandomNumber
    -멤버변수 final 추가로 상수로 선언

--- src/main/java/fourstep/logic/RandomNumber.java
+++ src/main/java/fourstep/logic/RandomNumber.java
@@ -4,8 +4,7 @@ import java.util.Random;
 
 public class RandomNumber {
 
-    private Random random = new Random();
-
+    private final Random random = new Random();
     public int getRandomNumber() {
         return random.nextInt(9);
     }
