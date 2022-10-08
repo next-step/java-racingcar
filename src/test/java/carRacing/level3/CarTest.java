3a90d703e9cdfa91cc1f3b3d98993683b3dadbc9
sienna <20200803@sungshin.ac.kr>
2022-10-09 08:50:32 +0900

    :white_check_mark:test : 자동차는 전략이 참이면 움직인다

--- src/test/java/carRacing/level3/CarTest.java
+++ src/test/java/carRacing/level3/CarTest.java
@@ -1,33 +1,26 @@
 package carRacing.level3;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import carRacing.level3.infra.error.NameLengthException;
-import carRacing.level3.model.Car;
-import carRacing.level3.model.strategy.RandomMovingStrategy;
-import org.junit.jupiter.api.DisplayName;
+import carRacing.level3.model.Cars;
+import carRacing.level3.model.Location;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
-	public static final Integer DEFAULT_LOCATION = 0;
-
 	@Test
-	@DisplayName("이름은 5자를 초과하면 안된다")
-	void 자동차_이름(){
-		assertThatThrownBy(() -> {
-			new Car("testCar1");
-		}).isInstanceOf(NameLengthException.class);;
+	void 전략이_참이면_움직인다() {
+		List<String> carNameList = new ArrayList<>();
+		carNameList.add("car1");
+		carNameList.add("car2");
+
+		Cars cars = Cars.create(carNameList);
+		cars.moveCarLocation(new FixMovingStrategy());
+
+		assertThat(cars.carLocation(0)).isEqualTo(new Location(1));
+
 	}
 
-
-	@Test
-	void 자동차_전진() {
-		Car car = new Car("test");
-		car.moveLocation(new RandomMovingStrategy());
-		assertThat(car.carLocation()).isGreaterThanOrEqualTo(DEFAULT_LOCATION);
-	}
 
 }
