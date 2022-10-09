4a6e69b860b07d05d7013865f0bc4c2b4fd970ed
justzino <zino@channel.io>
2022-10-09 19:29:21 +0900

    Refactor: view 레이어 분리

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -1,25 +1,22 @@
 package racing;
 
 import java.util.List;
-import java.util.Scanner;
+import racing.view.InputView;
 import racing.view.ResultView;
 
 public class RacingGame {
-  public static final Scanner input = new Scanner(System.in);
 
   public void start() {
-    System.out.println("자동차 대수는 몇 대 인가요?");
-    int numberOfCar = input.nextInt();
-    System.out.println("시도할 횟수는 몇 회 인가요?");
-    int numberOfChance = input.nextInt();
-    System.out.println("실행 결과");
+    int numberOfCar = InputView.getNumberOfRacingCar();
+    int numberOfChance = InputView.getNumberOfChance();
+
+    ResultView.printResultTitle();
 
     Cars cars = new Cars(numberOfCar);
-
     for (int i = 0; i < numberOfChance; i++) {
       cars.move();
       List<Integer> locations = cars.getLocations();
-      ResultView.printResult(locations);
+      ResultView.printTurnResult(locations);
       System.out.println();
     }
   }
--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 public class ResultView {
 
+  public static final String RESULT_START_MESSAGE = "실행 결과";
   public static final String CAR_TRACE = "-";
 
-  public static void printResult(List<Integer> locations) {
+  public static void printResultTitle() {
+    System.out.println(RESULT_START_MESSAGE);
+  }
+
+  public static void printTurnResult(List<Integer> locations) {
     for (int location : locations) {
       System.out.println(CAR_TRACE.repeat(location));
     }
   }
-
 }
