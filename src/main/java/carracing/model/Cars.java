7f32875f89d067c9a073ccbdad9f33fa3cd2a018
owen.qq <owen.qq@kakaocorp.com>
2022-10-09 14:26:07 +0900

    refactor: cars 생성자 추가

--- src/main/java/carracing/model/Cars.java
+++ src/main/java/carracing/model/Cars.java
@@ -22,6 +22,10 @@ public class Cars {
         this.cars = cars;
     }
 
+    public Cars(Car... cars) {
+        this.cars = List.of(cars);
+    }
+
     public void goOrStop(MovableStrategy movableStrategy) {
         cars.forEach(car -> car.tryMove(movableStrategy));
     }
