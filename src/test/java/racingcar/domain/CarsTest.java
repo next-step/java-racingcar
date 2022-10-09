7e8211261e5741a928edf1a2de370857aaa4b0cb
boradol2 <boradol0902@gmail.com>
2022-10-10 01:30:37 +0900

    refactor(InputView): carNames split 역할 부여
    7. Cars 에서 carNames 입력받는 testcode 삭제

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -2,8 +2,6 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,15 +9,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
-    @ParameterizedTest
-    @ValueSource(strings = {"pobi,crong,honux"})
-    @DisplayName("입력한 자동차 이름을 ','로 구분하고, 구분한 개수만큼 cars의 사이즈가 같다.")
-    void carsCreateTest_thenIsEqualTo(String carNames) {
-        Cars cars = new Cars(carNames);
-
-        assertThat(cars).isEqualTo(new Cars(carNames));
-    }
-
     @Test
     @DisplayName("우승 자동차를 찾아낸다. 우승 자동차는 'pobi', 'honux' 이다.")
     void findWinners_thenContainWinners() {
