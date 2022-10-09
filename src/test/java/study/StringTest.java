f3ad54f72f1d755e2a8aa9506c5e9442b8539eea
sylee <wnghkdcjdtns@naver.com>
2022-10-09 16:42:19 +0900

    refactor step1 split()테스트 케이스 변경

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -12,10 +12,7 @@ public class StringTest {
     @Test
     void split(){
         String[] data = "1,2".split(",");
-        assertAll(
-                () -> assertThat(data).contains("1"),
-                () -> assertThat(data).containsExactly("1","2")
-        );
+            assertThat(data).containsExactly("1","2");
     }
 
     @Test
@@ -28,7 +25,10 @@ public class StringTest {
     @DisplayName("특정 위치 문자 가져오기")
     void charAtException(){
         String result = "abc";
-        assertThatThrownBy(() -> {result.charAt(4);}).isInstanceOf(StringIndexOutOfBoundsException.class) .hasMessageContaining("String index out of range: 4");
-        assertThatExceptionOfType(StringIndexOutOfBoundsException.class).isThrownBy(() -> {result.charAt(4);}).withMessageMatching("String index out of range: 4");
+        assertThatThrownBy(() -> {result.charAt(4);})
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class).isThrownBy(() -> {result.charAt(4);})
+                .withMessageMatching("String index out of range: 4");
     }
 }
