a51b535a4d31e3771feab688f7d5144a0fcd6d51
kureungkureung <terry9611@naver.com>
2022-10-09 19:57:01 +0900

    test(CarTest): Car 생성 방식 변경

--- src/test/java/step3and4/game/car/CarTest.java
+++ src/test/java/step3and4/game/car/CarTest.java
@@ -12,24 +12,24 @@ class CarTest {
     @DisplayName("숫자가 4이상 나올 경우 자동차는 한 칸 전진한다.")
     void a() {
         final Number number = new Number.Fake(4);
-        final Car sut = new Car(number);
+        final Car sut = new Car.Factory(number, new Position(0), "").car();
 
-        assertThat(sut.movedCar()).isEqualTo(new Car(new Position(1), number));
+        assertThat(sut.movedCar()).isEqualTo(new Car.Factory(number, new Position(1), "").car());
     }
 
     @Test
     @DisplayName("숫자가 4미만 나올 경우 자동차는 현재 위치에 움직이지 않는다.")
     void b() {
         final Number number = new Number.Fake(3);
-        final Car sut = new Car(number);
+        final Car sut = new Car.Factory(number, new Position(0), "").car();
 
-        assertThat(sut.movedCar()).isEqualTo(new Car(new Position(0), number));
+        assertThat(sut.movedCar()).isEqualTo(new Car.Factory(number, new Position(0), "").car());
     }
 
     @Test
     @DisplayName("자동차를 출력할 수 있다.")
     void c() {
-        final Car sut = new Car(new Position(1), new Number.Fake(3), new Name("12345"));
+        final Car sut = new Car.Factory(new Number.Fake(3), new Position(1), "12345").car();
 
         assertThat(sut).hasToString("12345 : -");
     }
