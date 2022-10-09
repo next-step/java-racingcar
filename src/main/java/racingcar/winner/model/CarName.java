e7fd98feed38cc12164bc9a5f3d424ccb622af4e
owen.qq <owen.qq@kakaocorp.com>
2022-10-09 15:43:17 +0900

    refactor: CarName 생성자에서 수행되는 벨리데이션 로직을 메서드 분리수행

--- src/main/java/racingcar/winner/model/CarName.java
+++ src/main/java/racingcar/winner/model/CarName.java
@@ -5,18 +5,26 @@ import java.util.Objects;
 import racingcar.winner.exception.InvalidInputException;
 
 public class CarName {
+    private static final int NAME_MAX_LENGTH = 5;
     private String value;
 
     public CarName(final String value) {
+        nameShouldNotBlank(value);
+        validateNameLength(value);
+
+        this.value = value;
+    }
+
+    private void nameShouldNotBlank(String value) {
         if (value == null || value.isBlank()) {
             throw new InvalidInputException("자동차의 이름은 공백을 가질 수 없습니다");
         }
+    }
 
-        if (value.length() > 5) {
+    private void validateNameLength(String value) {
+        if (value.length() > NAME_MAX_LENGTH) {
             throw new InvalidInputException("자동차의 이름은 5자를 초과할 수 없습니다");
         }
-
-        this.value = value;
     }
 
     @Override
