29b9947b5e9d5a89c4ed063b95f34e35f91fca98
sylee <wnghkdcjdtns@naver.com>
2022-10-09 16:34:24 +0900

    refactor Car 클래스 구조 변경 - move if문 조건에 number를 MOVE_ABLE_COUNT상수로 변경

--- src/main/java/study/Car.java
+++ src/main/java/study/Car.java
@@ -1,15 +1,16 @@
 package study;
 
 public class Car {
-    private int moveCount;
+    private int moveCount = 0;
+    private static final int MOVE_ABLE_COUNT = 4;
 
     public void move(int number){
-        if(number >= 4){
+        if(number >= MOVE_ABLE_COUNT){
             this.moveCount++;
         }
     };
 
     public int getMoveCount() {
-        return moveCount;
+        return this.moveCount;
     }
 }
