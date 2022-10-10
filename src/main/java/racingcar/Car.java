c2b0551b1278ba662f24db18cf937301eef1fc3f
sasha-com <sasha.com@kakaocorp.com>
2022-10-10 20:29:00 +0900

    fix: CarValidator 클래스 추가 (벨리데이션 책임 분리)

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,25 +9,10 @@ public class Car {
     public Car(String name, int count) {
         this.name = name;
 
-        validateCount(count);
-        validateName(name);
+        CarValidator.validateCount(count);
+        CarValidator.validateName(name);
 
         this.histories = new int[count];
-    }
-
-    private void validateCount(int count) {
-        if(count < 0 || count > 99) {
-            throw new RuntimeException("count는 1에서 99까지의 숫자만 입력가능합니다.");
-        }
-    }
-
-    private void validateName(String name) {
-        if(name == null || name.isBlank()) {
-            throw new RuntimeException("name은 필수값이고 공백문자만 사용이 불가합니다.");
-        }
-        if(name.length() > 10) {
-            throw new RuntimeException("name은 1자에서 10자까지 입력가능합니다.");
-        }
     }
 
     public int getHistoriesByIndex(int index) {
