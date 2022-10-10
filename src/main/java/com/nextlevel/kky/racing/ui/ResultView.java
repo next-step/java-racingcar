87df2bf358eb33674de79494d1dc9710a2a167aa
kkywalk2 <kkywalk2@gmail.com>
2022-10-10 19:26:05 +0900

    refactor: printCarRacingResultWithName의 2중 loop 형태 코드 제거

--- src/main/java/com/nextlevel/kky/racing/ui/ResultView.java
+++ src/main/java/com/nextlevel/kky/racing/ui/ResultView.java
@@ -10,9 +10,7 @@ public class ResultView {
     public static void printCarRacingResultWithName(CarRacingExecutor carRacingExecutor) {
         for (Car car : carRacingExecutor.carList) {
             System.out.print(car.getName() + ": ");
-            for (int i = 0; i < car.getCurrentPosition() + 1; i++) {
-                System.out.print("-");
-            }
+            System.out.print("-".repeat(car.getCurrentPosition() + 1));
             System.out.println();
         }
         System.out.println();
