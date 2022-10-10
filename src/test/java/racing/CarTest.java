d2f2c0c5db496c74fa02d878d4aad606b6f5fbdf
justzino <zino@channel.io>
2022-10-10 14:20:04 +0900

    Test: 초기 위치 검증 & 숫자 상수화

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
+  public static final int START_LOCATION = 1;
+  public static final int DISTANCE_TO_MOVE_AT_ONCE = 1;
+
+
   private static Car car;
 
   @BeforeEach
@@ -16,10 +20,15 @@ public class CarTest {
   }
 
   @Test
+  void initialLocation() {
+    assertThat(car.nowLocation()).isEqualTo(START_LOCATION);
+  }
+
+  @Test
   @DisplayName("자동차를 한칸 이동시킨다")
   void moveOnce() {
     int carLastLocation = car.nowLocation();
     car.move();
-    assertThat(car.nowLocation()).isEqualTo(carLastLocation + 1);
+    assertThat(car.nowLocation()).isEqualTo(carLastLocation + DISTANCE_TO_MOVE_AT_ONCE);
   }
 }
