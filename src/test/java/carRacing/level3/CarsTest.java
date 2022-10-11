74b6e90ab386d72453ab9f45da65daa893608680
sienna <20200803@sungshin.ac.kr>
2022-10-11 13:23:52 +0900

    :recycle:refactor : test 리팩토링

--- src/test/java/carRacing/level3/CarsTest.java
+++ src/test/java/carRacing/level3/CarsTest.java
@@ -7,6 +7,7 @@ import carRacing.level3.error.NotFoundCarsException;
 import carRacing.level3.model.Cars;
 import carRacing.level3.model.Location;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,9 +16,7 @@ public class CarsTest {
 
 	@Test
 	void 전략이_참이면_움직인다() {
-		List<String> carNameList = new ArrayList<>();
-		carNameList.add("car1");
-		carNameList.add("car2");
+		List<String> carNameList = Arrays.asList("car1","car2");
 
 		Cars cars = Cars.create(carNameList);
 		cars.moveCarLocation(new FixMovingStrategy());
@@ -28,14 +27,14 @@ public class CarsTest {
 	}
 
 	@Test
-	@DisplayName("최대위치 구하기 테스트")
-	void cars가_없으면_에러_반환() {
+	@DisplayName("최대 위치 에러 반환 테스트")
+	void 자동차가_비어있으면_에러_반환() {
 		List<String> carNameList = new ArrayList<>();
 
 		Cars cars = Cars.create(carNameList);
 
 		assertThatThrownBy(() -> {
-			cars.maxCarLocation();
+			cars.maxLocation();
 		}).isInstanceOf(NotFoundCarsException.class);
 
 
