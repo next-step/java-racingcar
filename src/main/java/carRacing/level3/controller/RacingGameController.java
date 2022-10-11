dce14587f73c21f3dc3a3396258a493a50403519
sienna <20200803@sungshin.ac.kr>
2022-10-11 13:59:33 +0900

    :recycle:refactor : 인스턴스 상수화

--- src/main/java/carRacing/level3/controller/RacingGameController.java
+++ src/main/java/carRacing/level3/controller/RacingGameController.java
@@ -9,26 +9,26 @@ import java.util.List;
 
 public class RacingGameController {
 
-	private InputView inputView = new InputView();
-	private OutPutView outPutView = new OutPutView();
-	private Winner winner = new Winner();
+	private final static InputView INPUT_VIEW = new InputView();
+	private final static OutPutView OUT_PUT_VIEW = new OutPutView();
+	private final static Winner WINNER = new Winner();
 
 	public void gameStart(MovingStrategy movingStrategy) {
 
-		List<String> carNameList = inputView.saveCarNames();
-		int gameRound = inputView.askValueRound();
+		List<String> carNameList = INPUT_VIEW.saveCarNames();
+		int gameRound = INPUT_VIEW.askValueRound();
 
 		Cars cars = Cars.create(carNameList);
 
-		outPutView.showNotice();
+		OUT_PUT_VIEW.showNotice();
 
 		for (int i = 0; i < gameRound; i++) {
 			cars.moveCarLocation(movingStrategy);
-			outPutView.showRace(cars);
+			OUT_PUT_VIEW.showRace(cars);
 		}
 
-		winner.decideWinner(cars);
-		outPutView.showWinner(winner.getWinnerList());
+		WINNER.decideWinner(cars);
+		OUT_PUT_VIEW.showWinner(WINNER.getWinnerList());
 
 	}
 
