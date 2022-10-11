cf1fb5dc6e359d7ae742f27877d3a3bb6084f035
marco.keem <marco.keem@kakaocorp.com>
2022-10-11 17:41:05 +0900

    refactor - 불필요 메소드 제거

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,12 +1,10 @@
-package step3;
+package racingcar;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.Car;
+import racingcar.model.Car;
 
 import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,11 +14,6 @@ public class CarTest {
     private final Car car = new Car();
     private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-
-    @BeforeAll
-    public static void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
 
     @Test
     @DisplayName("car_race_동작_테스트")
