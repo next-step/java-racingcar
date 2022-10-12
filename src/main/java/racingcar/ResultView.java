e40e4d215785ceeaa498ac4e52daea08e1d0cff0
HYEONSEOK1 <gustir929@gmail.com>
2022-10-12 21:36:55 +0900

    feat: 자동차 이름 출력 로직 추가

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -1,16 +1,21 @@
 package racingcar;
 
+import java.util.Formatter;
 import java.util.List;
 
 public class ResultView {
+
+    private static final String Bar = "-";
+
     public static void printCars(List<Car> cars) {
         cars.forEach(ResultView::printBar);
         System.out.println();
     }
 
     private static void printBar(Car car) {
+        System.out.print(String.format("%s : ", car.getName()));
         for (int i = 0; i < car.getPosition(); i++) {
-            System.out.print("|");
+            System.out.print(Bar);
         }
         System.out.println();
     }
