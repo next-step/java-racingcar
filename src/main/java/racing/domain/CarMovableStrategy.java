cb33425746b460cc9b441fb068dc5badd444c749
Shin seongsu <tlstjdtn321@naver.com>
2022-10-12 16:55:17 +0900

    fix: 리뷰어님께서 추천해주신 방식으로 사용

--- src/main/java/racing/domain/CarMovableStrategy.java
+++ src/main/java/racing/domain/CarMovableStrategy.java
@@ -6,17 +6,12 @@ public class CarMovableStrategy implements MovableStrategy {
 
     private static final int RANDOM_RANGE = 10;
     private static final int RACING_CONDITION = 4;
-    private static final boolean RUN = true;
-    private static final boolean STOP = false;
 
     private final Random RANDOM = new Random();
 
     @Override
     public boolean randomMovable() {
         int random = RANDOM.nextInt(RANDOM_RANGE);
-        if (RACING_CONDITION <= random) {
-            return RUN;
-        }
-        return STOP;
+        return RACING_CONDITION <= random;
     }
 }
