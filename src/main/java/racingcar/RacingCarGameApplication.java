6a9b6cc7dec9d56b4f79a7cbbb88c599affa7247
boradol2 <boradol0902@gmail.com>
2022-10-13 00:33:57 +0900

    refactor(RacingCarGameController) : 컨트롤러 사용 재정의
    1. Application 진입점으로만 사용
    2. Controller에 Application code 이동.

--- src/main/java/racingcar/RacingCarGameApplication.java
+++ src/main/java/racingcar/RacingCarGameApplication.java
@@ -1,26 +1,9 @@
 package racingcar;
 
 import racingcar.controller.RacingCarGameController;
-import racingcar.domain.RacingCarGame;
-import racingcar.dto.CarsRecord;
-import racingcar.dto.RacingRecord;
-import racingcar.view.InputView;
-import racingcar.view.ResultView;
-
-import java.util.List;
 
 public class RacingCarGameApplication {
     public static void main(String[] args) {
-        List<String> carNames = InputView.inputCarNames();
-        int tryCount = InputView.inputTryCount();
-
-        RacingCarGame racingCarGame = new RacingCarGame(carNames, tryCount);
-        RacingCarGameController racingCarGameController = new RacingCarGameController(racingCarGame);
-        RacingRecord racingCarGameRecords = racingCarGameController.play();
-        CarsRecord winners = racingCarGameController.findFinalWinner();
-
-        ResultView.printResultMessage();
-        ResultView.printRacingCars(racingCarGameRecords);
-        ResultView.printWinner(winners);
+        RacingCarGameController.game();
     }
 }
--- src/main/java/racingcar/controller/RacingCarGameController.java
+++ src/main/java/racingcar/controller/RacingCarGameController.java
@@ -4,19 +4,25 @@ import racingcar.domain.RacingCarGame;
 import racingcar.dto.CarsRecord;
 import racingcar.dto.RacingRecord;
 import racingcar.strategy.RandomValueMovingStrategy;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
+import java.util.List;
 
 public class RacingCarGameController {
-    private final RacingCarGame racingCarGame;
-
-    public RacingCarGameController(RacingCarGame racingCarGame) {
-        this.racingCarGame = racingCarGame;
+    private RacingCarGameController() {
     }
 
-    public RacingRecord play() {
-        return racingCarGame.play(new RandomValueMovingStrategy());
-    }
+    public static void game() {
+        List<String> carNames = InputView.inputCarNames();
+        int tryCount = InputView.inputTryCount();
 
-    public CarsRecord findFinalWinner() {
-        return racingCarGame.findFinalWinner();
+        RacingCarGame racingCarGame = new RacingCarGame(carNames, tryCount);
+        RacingRecord play = racingCarGame.play(new RandomValueMovingStrategy());
+        CarsRecord finalWinner = racingCarGame.findFinalWinner();
+
+        ResultView.printResultMessage();
+        ResultView.printRacingCars(play);
+        ResultView.printWinner(finalWinner);
     }
 }
