de45ed7adbaf85a9c7083b631bcf64082920f6c8
mean-ji <winter7574@gmail.com>
2022-10-13 20:39:08 +0900

    test: 자동차 객체 테스트 추가

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -1,7 +1,7 @@
 package racing;
 
 public class Car {
-    int moveCount;
+    public int moveCount;
 
     public Car () {
     }
@@ -9,12 +9,16 @@ public class Car {
     public void move () {
         int randomNumber = (int)(Math.random()*10);
         if (randomNumber >= 4) {
-            moveCount++;
+            this.moveCount++;
         }
     }
 
+    public int getMoveCount() {
+        return this.moveCount;
+    }
+
     public void printResult () {
-        for (int i = 0; i < moveCount; i++) {
+        for (int i = 0; i < this.moveCount; i++) {
             System.out.print("-");
         }
     }
