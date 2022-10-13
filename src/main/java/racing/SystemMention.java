e54bbbfe48a09135ee25f651e0f49a0e7161f678
gkfgran5037 <gkfgran5037@naver.com>
2022-10-13 11:53:19 +0900

    Add Result mention

--- src/main/java/racing/SystemMention.java
+++ src/main/java/racing/SystemMention.java
@@ -1,7 +1,10 @@
 package racing;
 
 public enum SystemMention {
-     NULL_CHECK("값을 입력해주세요"),
+    CNT_OF_CAR("자동차 대수는 몇 대 인가요?"),
+    CNT_OF_TRY("시도할 회수는 몇 회 인가요?"),
+    RESULT("실행 결과"),
+    NULL_CHECK("값을 입력해주세요"),
     NUMBER_CHECK("숫자만 입력해주세요");
 
     private final String mention;
