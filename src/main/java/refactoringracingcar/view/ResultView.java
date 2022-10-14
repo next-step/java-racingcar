c52bc5c14021d90eec3951a8eedb550bfa9dce0f
scoopsoup <rnrdud46@nate.com>
2022-10-14 23:20:09 +0900

    feat : 자동차 상태 표시("-") 및 우승자 판별 및 출력

---
+++ src/main/java/refactoringracingcar/view/ResultView.java
@@ -0,0 +1,51 @@
+package refactoringracingcar.view;
+
+import refactoringracingcar.domain.Car;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ResultView {
+    private List<Car> cars;
+
+    public ResultView(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public String printGameStatus() {
+        String carDistanceStatus = "";
+        for (int i = 0; i < cars.size(); i++) {
+            carDistanceStatus += cars.get(i).getCarName() + " : " + printStatusBar(i) + "\n";
+        }
+        carDistanceStatus += "\n";
+        return carDistanceStatus;
+    }
+
+    private String printStatusBar(int participantTurn) {
+        int moveDistance = cars.get(participantTurn).getCarPosition();
+        String carStatus = "";
+        for (int i = 0; i < moveDistance; i++) {
+            carStatus += "-";
+        }
+        return carStatus;
+    }
+
+    public String printWinner() {
+        String winnerResult = "";
+        List<Car> winnerCars = determineWinner();
+        for (Car winnerCarName : winnerCars) {
+            winnerResult += ", " + winnerCarName.getCarName();
+        }
+        return winnerResult.substring(2) + "가 최종 우승했습니다.";
+    }
+
+    private List<Car> determineWinner() {
+        Car winner = cars.stream().max(Comparator.comparing(Car::getCarPosition)).get();
+        List<Car> winnerCars = cars.stream()
+                .filter(car -> car.getCarPosition() == winner.getCarPosition())
+                .collect(Collectors.toList());
+
+        return winnerCars;
+    }
+}
