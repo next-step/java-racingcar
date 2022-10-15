7b252032302e02e0732c3d4faf4ffc8dd0515c5b
justzino <zino@channel.io>
2022-10-15 19:07:34 +0900

    Refactor: Random 객체 최초만 초기화하도록

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -6,8 +6,9 @@ public class RandomNumberGenerator {
 
   public static final int UNITS_DIGIT_BOUND = 10;
 
+  private static final Random random = new Random();
+
   public static int generateUnitsDigit() {
-    Random random = new Random();
     return random.nextInt(UNITS_DIGIT_BOUND);
   }
 }
