6097ebda64a934cb5cbc5dcb529617518a5e6a76
naseunghak <rshak8912@gmail.com>
2022-10-16 13:49:17 +0900

    feat (전략패턴) : 전략패턴 추가하여 랜덤 숫자 생성

---
+++ src/main/java/racingcar/race/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package racingcar.race;
+
+import java.util.Random;
+
+public class RandomNumberGenerator implements NumberGenerator {
+
+    private static final int RANDOM_NUMBER_UPPER_BOUND = 10;
+
+    @Override
+    public int generate() {
+        return new Random().nextInt(RANDOM_NUMBER_UPPER_BOUND);
+    }
+
+}
