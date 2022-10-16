f940ce8649314a0ef1bd04e83c6eec921b8290ff
jiokim <jiokim222@gmail.com>
2022-10-16 16:55:32 +0900

    refactor(GameRecordConverter): collectingAndThen

--- src/main/java/racingcar/backend/converter/GameRecordConverter.java
+++ src/main/java/racingcar/backend/converter/GameRecordConverter.java
@@ -4,15 +4,15 @@ import racingcar.backend.domain.Cars;
 import racingcar.backend.dto.CarDto;
 import racingcar.backend.domain.GameRecord;
 
-import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class GameRecordConverter {
 
     public static GameRecord getGameRecord(Cars cars) {
-        List<CarDto> carDtos = cars.getValues().stream()
+        return cars.getValues().stream()
                 .map(car -> CarDto.create(car.getName(), car.getPosition()))
-                .collect(Collectors.toList());
-        return new GameRecord(carDtos);
+                .collect(collectingAndThen(toList(), GameRecord::new));
     }
 }
