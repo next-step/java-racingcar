aab089f5e257fa45f5be6d78c6d100ff6d3de263
kkywalk2 <kkywalk2@gmail.com>
2022-10-16 22:35:22 +0900

    refactor: 불필요한 레이어(controller) 제거

--- src/main/java/com/nextlevel/kky/racing/CarRacingApplication.java
+++ src/main/java/com/nextlevel/kky/racing/CarRacingApplication.java
@@ -1,19 +1,19 @@
 package com.nextlevel.kky.racing;
 
-import com.nextlevel.kky.racing.controller.CarRacingController;
 import com.nextlevel.kky.racing.dto.CarRaceResultDto;
+import com.nextlevel.kky.racing.service.CarRacingService;
 import com.nextlevel.kky.racing.ui.InputView;
 import com.nextlevel.kky.racing.ui.ResultView;
 
 public class CarRacingApplication {
 
-    private static final CarRacingController carRacingController = new CarRacingController();
+    private static final CarRacingService carRacingService = new CarRacingService();
 
     public static void main(String[] args) {
         String[] carNames = InputView.inputString("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         int roundCount = InputView.inputNumber("시도할 회수는 몇 회 인가요?");
 
-        CarRaceResultDto carRaceResultDto = carRacingController.race(carNames, roundCount);
+        CarRaceResultDto carRaceResultDto = carRacingService.race(carNames, roundCount);
 
         ResultView.printCarRaceResultDto(carRaceResultDto);
     }
