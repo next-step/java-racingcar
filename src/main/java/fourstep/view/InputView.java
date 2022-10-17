e169fefe78c1a764070ec896faa2ba2af845c101
isakchoe <isakchoe@naver.com>
2022-10-17 12:45:48 +0900

    refactor: InputView
    - 자동차 이름 5글자 예외처리 추가 
    - 시도횟수 마이너스값 예외처리 추가

--- src/main/java/fourstep/view/InputView.java
+++ src/main/java/fourstep/view/InputView.java
@@ -11,15 +11,46 @@ public class InputView {
 
         String str = scanner.next();
         String[] strArr = str.split(",");
+
+        int carNameErrFlag = checkEntireName(strArr);
+
+        if (carNameErrFlag != 0) {
+            System.out.println("자동차 이름은 5 글자를 초과할 수 없습니다.");
+            scanner = new Scanner(System.in);
+            return inputCarName();
+        }
         return strArr;
+    }
+
+    public static int checkEntireName(String[] strArr) {
+        int carNameErrFlag = 0;
+        for (String s : strArr) {
+            carNameErrFlag += checkName(s);
+        }
+        return carNameErrFlag;
+    }
+
+    public static int checkName(String carName) {
+        if (carName.length() > 5) {
+            return 1;
+        }
+        return 0;
     }
 
     public static int inputRaceCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         try {
-            return scanner.nextInt();
+            int raceCount = scanner.nextInt();
+
+            if (raceCount <= 0) {
+                System.out.println("0이상 값만 입력해 주세요.");
+                scanner = new Scanner(System.in);
+                return inputRaceCount();
+            }
+
+            return raceCount;
         } catch (InputMismatchException e) {
-            System.out.println("정수값만 입력해 주세요.");
+            System.out.println("양의 정수값만 입력해 주세요.");
             scanner = new Scanner(System.in);
             return inputRaceCount();
         }
