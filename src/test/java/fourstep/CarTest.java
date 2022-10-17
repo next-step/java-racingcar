ef709ade31a59db34a2a6ff07abdad6392dc4020
isakchoe <isakchoe@naver.com>
2022-10-17 12:47:41 +0900

    Test: CarTest
    -DisplayName 추가
    - 로직 수정

--- src/test/java/fourstep/CarTest.java
+++ src/test/java/fourstep/CarTest.java
@@ -1,6 +1,7 @@
 package fourstep;
 
 import fourstep.logic.Car;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -9,9 +10,10 @@ import static org.assertj.core.api.Assertions.*;
 public class CarTest {
 
     @Test
+    @DisplayName("차 이동 테스트")
     void moveTest() {
         Car car = new Car("busung");
         car.move(4);
-        assertThat(car.getLocation()).isEqualTo(2);
+        assertThat(car.getLocation()).isEqualTo(1);
     }
 }
