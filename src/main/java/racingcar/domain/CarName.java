45fb655d02f78b92bf54080d18e9a9c96579dd23
hungryjayy <juwonson2068@naver.com>
2022-10-20 11:19:34 +0900

    feat: Add CarName class

---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,28 @@
+package racingcar.domain;
+
+public class CarName {
+    private final String name;
+
+    private static final int CAR_NAME_THRESHOLD = 5;
+
+    private static final String CAR_NAME_LENGTH_EXCEPTION = "자동차 이름은 5자를 초과할 수 없습니다.";
+
+    public CarName(String name) {
+        this.name = name;
+    }
+
+    public static CarName valueOf(String name) {
+        validateCarNames(name);
+        return new CarName(name);
+    }
+
+    private static void validateCarNames(String name) throws IllegalArgumentException {
+        if (isOverThreshold(name)) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_EXCEPTION);
+        }
+    }
+
+    private static boolean isOverThreshold(String name) {
+        return name.length() > CAR_NAME_THRESHOLD;
+    }
+}
--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -13,13 +13,13 @@ public class CarNameTest {
     void valueOf_ShouldReturnCarNameObject() {
         CarName actual = CarName.valueOf("abcde");
 
-        assertThat(actual.class).isEqualTo(CarName.class);
+        assertThat(actual).isInstanceOf(CarName.class);
     }
 
     @Test
     @DisplayName("5자 길이의 자동차 이름을 생성한다.")
     void valueOf_ShouldFailWhenANameOfNamesOver5() {
-        assertThatThrownBy(() -> CarName.valueOf("abcde"))
+        assertThatThrownBy(() -> CarName.valueOf("abcdef"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 5자를 초과할 수 없습니다.");
     }
