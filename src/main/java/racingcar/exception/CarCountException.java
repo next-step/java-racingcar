ab2bad00fb2f6260c269f23514d864d97414225f
0chil <0@chll.it>
2022-10-20 17:56:14 +0900

    typo(exception): 자동차는 '대'

--- src/main/java/racingcar/exception/CarCountException.java
+++ src/main/java/racingcar/exception/CarCountException.java
@@ -2,7 +2,7 @@ package racingcar.exception;
 
 public class CarCountException extends RuntimeException {
 
-	public static final String MESSAGE_EMPTY_CARS = "자동차는 1개 이상이어야 합니다";
+	public static final String MESSAGE_EMPTY_CARS = "자동차는 1대 이상이어야 합니다";
 
 	public CarCountException() {
 		this(MESSAGE_EMPTY_CARS);
