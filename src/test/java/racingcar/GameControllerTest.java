eeb59d94da9c56c3076fa3a35c669f4899bd2b2b
0chil <0@chll.it>
2022-10-20 21:49:00 +0900

    test(GameController): play 테스트

---
+++ src/test/java/racingcar/GameControllerTest.java
@@ -0,0 +1,79 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import racingcar.controller.GameController;
+import racingcar.dto.CarDto;
+import racingcar.dto.Result;
+import racingcar.strategy.MoveStrategy;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+class GameControllerTest {
+
+	private static final List<String> NAMES = List.of("0chil", "g");
+	private static final int TRIAL_COUNT = 2;
+	private static final MoveStrategy MOVE_STRATEGY_ALWAYS_MOVE = () -> true;
+
+	private InputView inputView;
+	private OutputView outputView;
+
+	private List<String> outputNames;
+	private List<Result> outputResults;
+
+	@BeforeEach
+	void setUp() {
+		outputNames = new ArrayList<>();
+		outputResults = new ArrayList<>();
+
+		inputView = new InputView() {
+			@Override
+			public List<String> readNames() {
+				return NAMES;
+			}
+
+			@Override
+			public int readTrialCount() {
+				return TRIAL_COUNT;
+			}
+		};
+
+		outputView = new OutputView() {
+			@Override
+			public void printWinners(List<String> names) {
+				outputNames = names;
+			}
+
+			@Override
+			public void printResults(List<Result> results) {
+				outputResults = results;
+			}
+		};
+	}
+
+	@Test
+	@DisplayName("게임 플레이를 테스트한다")
+	void play() {
+		GameController gameController = new GameController(inputView, outputView, MOVE_STRATEGY_ALWAYS_MOVE);
+		gameController.play();
+
+		assertThat(outputNames).containsAll(NAMES);
+		assertThat(outputResults).last().satisfies(result -> assertThatHasUniformPositionOf(result, TRIAL_COUNT));
+	}
+
+	private void assertThatHasUniformPositionOf(Result result, int position) {
+		result.getCarDtos()
+			.forEach(carDto -> assertThatHasPositionOf(carDto, position));
+	}
+
+	private void assertThatHasPositionOf(CarDto carDto, int position) {
+		assertThat(carDto.getPosition()).isEqualTo(position);
+	}
+}
