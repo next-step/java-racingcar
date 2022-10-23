870f7b478360fcc8717496bfe8946d621578df84
nhs219 <heesun_z@naver.com>
2022-10-23 15:33:25 +0900

    chore : SCANNER부분에 exception추가

--- src/main/java/step4/InputRacingCar.java
+++ src/main/java/step4/InputRacingCar.java
@@ -3,21 +3,29 @@ package step4;
 import java.util.Scanner;
 
 public class InputRacingCar {
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public String[] inputNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
-        return splitNames();
+        try {
+            return splitNames();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
     public int inputTimes() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return scanner.nextInt();
+        try {
+            return SCANNER.nextInt();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
     private static String[] splitNames() {
-        return scanner.next().split(",");
+        return SCANNER.next().split(",");
     }
 
 }
