e1c6e56fd251c4f1aeb7a8189695f3f65d278580
gkfgran5037 <gkfgran5037@naver.com>
2022-10-24 17:35:37 +0900

    refactor : 사용하지 않는 메서드 제거

--- src/test/java/racing/domain/WinnerTest.java
+++ src/test/java/racing/domain/WinnerTest.java
@@ -11,17 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 class WinnerTest {
 
     @Test
-    void findMaxPositionTest() {
-        Car car1 = new Car("자동차1", 3);
-        Car car2 = new Car("자동차2", 2);
-        Car car3 = new Car("자동차3", 5);
-        List<Car> carList = Arrays.asList(car1, car2, car3);
-
-        Winner winner = new Winner(carList);
-        assertThat(winner.getMaxPosition()).isEqualTo(new Position(5));
-    }
-
-    @Test
     void findWinnersTest() {
         Car car1 = new Car("자동차1", 3);
         Car car2 = new Car("자동차2", 3);
