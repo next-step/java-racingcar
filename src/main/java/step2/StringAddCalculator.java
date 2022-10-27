44d7c4fca4c257ff7b964123d1cd3f8e76c792bb
develiberta <62236332+develiberta@users.noreply.github.com>
2022-10-27 12:29:03 +0900

    Step2 피드백 반영 (#3973)
    * step2
    
    * step2 reflect feedback

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,66 +1,63 @@
 package step2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
-    private static String DELIMETER_COMMA = ",";
-    private static String DELIMETER_COLON = ":";
-    private static String SPLIT_OR = "|";
-    private static String stringProvided = null;
-    private static String delimeter = DELIMETER_COMMA + SPLIT_OR + DELIMETER_COLON;
+    private static final String DELIMITER_COMMA = ",";
+    private static final String DELIMITER_COLON = ":";
+    private static final String SPLIT_OR = "|";
+    private static final String DELIMITER = DELIMITER_COMMA + SPLIT_OR + DELIMITER_COLON;
 
     public static int splitAndSum(String string) {
-        stringProvided = string;
-        if (nullOrEmpty()) return 0;
-        processInCaseCustomDelimeter();
-        String[] stringArray = stringProvided.split(delimeter);
+        if (nullOrEmpty(string)) {
+            return 0;
+        }
+        String[] stringArray = splitByDelimiter(string);
         int[] intArray = intArrayFromStringArray(stringArray);
         return intArraySum(intArray);
     }
 
-    private static void processInCaseCustomDelimeter() {
-        if (includingCustomDelimeter(stringProvided) == false) return;
-        delimeter += SPLIT_OR + customDelimeter(stringProvided);
-        stringProvided = stringProvided.substring(4);
+    private static String[] splitByDelimiter(String string) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(string);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+        return string.split(DELIMITER);
     }
 
     private static int intArraySum(int[] intArray) {
         int sum = 0;
-        for (int i = 0; i < intArray.length; i++) sum += intArray[i];
+        for (int i = 0; i < intArray.length; i++) {
+            sum += intArray[i];
+        }
         return sum;
     }
 
     private static int[] intArrayFromStringArray(String[] stringArray) {
         int[] intArray = new int[stringArray.length];
-        for (int i = 0; i < stringArray.length; i++) intArray[i] = intFromString(stringArray[i]);
+        for (int i = 0; i < stringArray.length; i++) {
+            intArray[i] = intFromString(stringArray[i]);
+        }
         return intArray;
     }
 
     private static int intFromString(String string) {
         int result = Integer.parseInt(string);
-        if (positive(result) == false) throw new RuntimeException();
+        if (positive(result) == false) {
+            throw new RuntimeException();
+        }
         return result;
     }
 
-    private static String customDelimeter(String string) {
-        return String.valueOf(string.charAt(2));
-    }
-
-    private static boolean includingCustomDelimeter(String string) {
-        System.out.print(string.length());
-        if (string.length() < 4) return false;
-        String substring = string.substring(0,4);
-        if (substring.startsWith("//") && substring.endsWith("\n")) return true;
-        return false;
-    }
-
-    private static boolean nullOrEmpty() {
-        if (stringProvided == null || stringProvided.equals("")) return true;
-        return false;
+    private static boolean nullOrEmpty(String string) {
+        return string == null || string.isEmpty();
     }
 
     private static boolean positive(int integer) {
-        if (integer >= 0) return true;
-        return false;
+        return integer >= 0;
     }
 
 }
