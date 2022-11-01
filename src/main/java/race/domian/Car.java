35b7dc803dc6f607ceb7d478ae5232280ae4fba7
wisdom08 <wisdom08@kakao.com>
2022-11-01 10:54:39 +0900

    fix: 위치 관련 상수 추가

--- src/main/java/race/domian/Car.java
+++ src/main/java/race/domian/Car.java
@@ -5,14 +5,17 @@ import race.strategy.MoveStrategy;
 public class Car {
 
     private int position;
+    public static final int START_POSITION = 0;
+    public static final int DISTANCE_TO_MOVE_AT_ONCE = 1;
+
 
     public Car() {
-        this.position = 0;
+        this.position = START_POSITION;
     }
 
     public void move(MoveStrategy moveStrategy) {
         if (moveStrategy.movable()) {
-            ++position;
+            this.position += DISTANCE_TO_MOVE_AT_ONCE;
         }
     }
 
