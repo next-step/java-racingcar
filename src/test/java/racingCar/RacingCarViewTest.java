a289295671cd8a7619e19e87c88243b7a8f73e41
hannah33 <hannah.33@kakaocorp.com>
2022-11-01 21:49:16 +0900

    fix: 테스트코드 컴파일 에러 수정

--- src/test/java/racingCar/RacingCarViewTest.java
+++ src/test/java/racingCar/RacingCarViewTest.java
@@ -7,8 +7,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RacingCarViewTest {
     @Test
     void 자동차_이름과_포지션() {
-        assertThat(new RacingCarView("a", new Position(3)).toString()).isEqualTo("a : ---");
-        assertThat(new RacingCarView("b", new Position(1)).toString()).isEqualTo("b : -");
-        assertThat(new RacingCarView("c", new Position(0)).toString()).isEqualTo("c : ");
+        assertThat(new RacingCarView(new RacingCarName("a"), new Position(3)).toString()).isEqualTo("a : ---");
+        assertThat(new RacingCarView(new RacingCarName("b"), new Position(1)).toString()).isEqualTo("b : -");
+        assertThat(new RacingCarView(new RacingCarName("c"), new Position(0)).toString()).isEqualTo("c : ");
     }
 }
