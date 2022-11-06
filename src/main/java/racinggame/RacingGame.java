06738f3fbccf624cd0629d177a55a84ea9e01f6d
GimunLee <gimoon0226@naver.com>
2022-11-06 14:40:19 +0900

    step4 application 구현

--- src/main/java/racinggame/RacingGame.java
+++ src/main/java/racinggame/RacingGame.java
@@ -1,2 +1,36 @@
-package racinggame;public class RacingGame {
+package racinggame;
+
+import racinggame.domain.Cars;
+
+public class RacingGame {
+
+    private final Cars cars;
+
+    private final int reps;
+    private int currentRep;
+
+    public RacingGame(String[] carNames, int reps) {
+        this.cars = new Cars(carNames);
+        this.reps = reps;
+        this.currentRep = 0;
+
+    }
+
+    public void race() {
+        this.cars.moveCars();
+        ++currentRep;
+    }
+
+    public Cars getWinners() {
+        return this.cars.getWinners();
+    }
+
+    public boolean isFinished() {
+        return currentRep > reps;
+    }
+
+
+    public Cars getCars() {
+        return this.cars;
+    }
 }
--- src/main/java/racinggame/RacingGameApplication.java
+++ src/main/java/racinggame/RacingGameApplication.java
@@ -1,14 +1,21 @@
 package racinggame;
 
-public class RacingGame {
+import racinggame.view.InputView;
+import racinggame.view.ResultView;
+
+public class RacingGameApplication {
     public static void main(String[] args) {
-        int carCnt = InputView.requestNumber("자동차 대수는 몇 대 인가요?");
-        int reps = InputView.requestNumber("시도할 회수는 몇 회 인가요?");
+        String[] carNames = InputView.requestCarName("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        int reps = InputView.requestNumber("시도할 회수는 몇 회인가요?");
+        System.out.println("실행 결과");
 
-        Track track = new Track(new RacingOption(carCnt, reps));
-        track.createCars();
+        RacingGame racingGame = new RacingGame(carNames, reps);
 
-        ResultView.printTitle();
-        track.start(reps);
+        while(!racingGame.isFinished()){
+            racingGame.race();
+            ResultView.printRacingResult(racingGame.getCars());
+        }
+        ResultView.printWinners(racingGame.getWinners());
+
     }
 }
