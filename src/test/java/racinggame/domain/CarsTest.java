d6f5e85cba0617b2b370e5fc5d0ac1c80724e0a0
GimunLee <gimoon0226@naver.com>
2022-11-06 14:39:21 +0900

    step4 테스트 정의

--- src/test/java/racinggame/domain/CarsTest.java
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -1,4 +1,34 @@
-package racinggame;
+package racinggame.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
+    @Test
+    void moveCarsTest() {
+        Car car1 = new Car("자동차1", 0, () -> true);
+        Car car2 = new Car("자동차2", 0, () -> true);
+        List<Car> carList = Arrays.asList(car1, car2);
+        Cars cars = new Cars(carList);
+
+        cars.moveCars();
+        for (Car car : cars.getCars()) {
+            assertThat(car.getPosition()).isEqualTo(new Position(1));
+        }
+    }
+
+    @Test
+    void winnerTest() {
+        Car car1 = new Car("자동차1", 0, () -> true);
+        Car car2 = new Car("자동차2", 0, () -> false);
+        List<Car> carList = Arrays.asList(car1, car2);
+        Cars cars = new Cars(carList);
+
+        cars.moveCars();
+        assertThat(cars.getWinners().getCars().get(0).getCarName().toString()).isEqualTo("자동차1");
+    }
 }
