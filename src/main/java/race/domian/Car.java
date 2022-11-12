36321550622a2155f43ce38f1bc61ad6a498f681
wisdom08 <wisdom08@kakao.com>
2022-11-12 11:08:24 +0900

    feat: 자동차 이름 글자 수 검증 추가

--- src/main/java/race/domian/Car.java
+++ src/main/java/race/domian/Car.java
@@ -8,16 +8,25 @@ public class Car {
     private int position;
     public static final int START_POSITION = 0;
     public static final int DISTANCE_TO_MOVE_AT_ONCE = 1;
+    private static final String CHARACTER_LIMIT_ERROR = "이름은 5자를 초과할 수 없습니다.";
+    private static final int CHARACTER_LIMIT = 5;
 
     public Car(String name) {
         this.position = START_POSITION;
-        this.name = name;
+        this.name = validateName(name);
     }
 
     public void move(MoveStrategy moveStrategy) {
         if (moveStrategy.movable()) {
             this.position += DISTANCE_TO_MOVE_AT_ONCE;
         }
+    }
+
+    private String validateName(String name) {
+        if (name.length() > CHARACTER_LIMIT) {
+            throw new IllegalArgumentException(CHARACTER_LIMIT_ERROR);
+        }
+        return name;
     }
 
     public int getPosition() {
