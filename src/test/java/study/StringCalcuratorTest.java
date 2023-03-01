192143556e323aa9b7290cf42c398ed4eecd1d2f
givxq <coooeeeeep@gmail.com>
2023-03-01 19:42:54 +0900

    [study] 문자열 계산기 수정

--- src/test/java/study/StringCalcuratorTest.java
+++ src/test/java/study/StringCalcuratorTest.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+// import org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalcuratorTest {
 
@@ -22,12 +25,37 @@ public class StringCalcuratorTest {
                 .collect(Collectors.toList());
     }
 
+    void validateInput(String input) {
+        if (input == null || input.trim().isEmpty())
+            throw new IllegalArgumentException("입력값이 null이거나 빈 공백 문자입니다.");
+    }
+
     int calc(String strInput) {
+        validateInput(strInput);
+
         List<String> input = Arrays.asList(strInput.split(" "));
+
+        validateNumberAndSign(input);
+
         List<Integer> numbers = getNumbers(input);
         List<String> signs = getSigns(input);
 
         return initCalcurator(numbers, signs);
+    }
+
+    private void validateNumberAndSign(List<String> input) {
+
+        if (input.stream().filter(sign -> sign.matches("[+\\-/*%]")).count() + input.stream()
+                .filter(number -> number.matches("[0-9]")).count() != input.size())
+            throw new IllegalArgumentException("연산자는 +, -, *, /, % 중 입력 가능 합니다.");
+
+        if (input.stream().filter(sign -> sign.matches("[+\\-/*%]")).count() != input.stream()
+                .filter(number -> number.matches("[0-9]")).count() - 1)
+            throw new IllegalArgumentException("숫자와 연산자의 개수가 맞지 않습니다.");
+
+        if (input.stream().filter(number -> number.matches("[0-9]")).count() < 2)
+            throw new IllegalArgumentException("숫자는 최소 2개 이상이어야 합니다.");
+
     }
 
     private int initCalcurator(List<Integer> numbers, List<String> signs) {
@@ -42,31 +70,47 @@ public class StringCalcuratorTest {
                 });
     }
 
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        return a / b;
+    }
+
     int calcurator(int result, String sign, int number) {
-        switch (sign) {
-            case "+":
-                result += number;
-                break;
-            case "-":
-                result -= number;
-                break;
-            case "*":
-                result *= number;
-                break;
-            case "/":
-                result /= number;
-                break;
-            default:
-                break;
-        }
-        return result;
+        if (sign.equals("+"))
+            return add(result, number);
+        if (sign.equals("-"))
+            return subtract(result, number);
+        if (sign.equals("*"))
+            return multiply(result, number);
+        if (sign.equals("/"))
+            return divide(result, number);
+
+        return 0;
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "2 + 3 * 4 / 2 - 3", "2 + 1", "6 / 7 + 2" })
+    @ValueSource(strings = { "2 + 3 * 4 / 2 - 3", "2 + 1", "6 / 7 + 2", "2 @ 2", "" })
     void test(String input) {
 
-        int result = calc(input);
+        int result;
+
+        // assertThatThrownBy(() -> {
+        // int testResult = calc(input);
+        // System.out.println(testResult);
+        // }).isInstanceOf(IllegalArgumentException.class);
+
+        result = calc(input);
 
         System.out.println(result);
     }
