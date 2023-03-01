65db06641017a59513793089bd6229c42142497e
JEAN-20210074\테스트 <jean.calm@kakaopaysec.com>
2023-03-01 12:27:04 +0900

    study: 문자열계산기 테스트코드 작성을 위한 Calculator 클래스 작성

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package calculator;
+
+public class Calculator {
+
+    int add(int num1, int num2) {
+        return num1 + num2;
+    }
+    int subtract(int num1, int num2){
+        return num1 - num2;
+    }
+
+    int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    int divide(int num1, int num2){
+        return num1/num2;
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    public void test(){
+        //given
+
+        //when
+
+        //then
+
+    }
+
+}
