06956a221e590ead5ed128a5ad20fd1cc643cdb2
harry.93 <harry.93@kakaopaysec.com>
2023-03-04 12:23:26 +0900

    task : car racing work 3

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -2,18 +2,29 @@ package racingCar;
 
 public class Car {
 
-    String name;
-    int position;
+    String name; // 자동차 이름
+    int position; // 자동차 위치
 
+    /**
+     * 생성자
+     *
+     * @param name
+     */
     Car(String name) {
         this.name = name;
         this.position = 0;
     }
 
+    /**
+     * 전진
+     */
     void forward() {
         this.position += 1;
     }
 
+    /**
+     * 현재 위치 값 출력
+     */
     void showPosition() {
         System.out.print(this.name + " : ");
         for (int i = 0; i < this.position; i++) {
--- src/main/java/racingCar/RacingCar.java
+++ src/main/java/racingCar/RacingCar.java
@@ -6,54 +6,70 @@ import java.util.Scanner;
 
 public class RacingCar {
 
-    String inputName;
-    int inputTryNumber;
+    String inputName; // 자동차 이름 입력 값
+    int inputTryNumber; // 경주 시도 횟수 값
 
-    ArrayList<Car> garage;
+    ArrayList<Car> garage; // 차고
 
+    /**
+     * 경주 시작
+     */
     public void race() {
         inputString();
-
         System.out.println("");
         System.out.println("실행 결과");
         for (int i = 0; i < inputTryNumber; i++) {
             racing();
         }
-
         chooseWinner();
     }
 
+    /**
+     * 우승자 결정
+     */
     private void chooseWinner() {
         int max = Integer.MIN_VALUE;
         String winner = "";
-        for(Car car : garage) {
-            if(car.position > max) {
+        for (Car car : garage) {
+            if (car.position > max) {
                 max = car.position;
                 winner = car.name;
                 continue;
             }
-            if(car.position == max) {
+            if (car.position == max) {
                 winner += "," + car.name;
             }
         }
         System.out.print("최종 우승자:" + winner);
     }
 
+    /**
+     * 실제 경주 1회 수행
+     */
     private void racing() {
-        for(Car car : garage) {
+        for (Car car : garage) {
             int move = draw();
-            if(move >= 4)
+            if (move >= 4) {
                 car.forward();
+            }
             car.showPosition();
         }
         System.out.println("");
     }
 
+    /**
+     * 0-9 랜덤 뽑기
+     *
+     * @return
+     */
     private int draw() {
         Random ran = new Random();
         return ran.nextInt(9);
     }
 
+    /**
+     * 입력값 입력 받는 함수
+     */
     void inputString() {
         Scanner sc = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
@@ -67,12 +83,14 @@ public class RacingCar {
         System.out.println(inputTryNumber);
     }
 
+    /**
+     * 자동차 생성하여 차고에 넣는 함수
+     *
+     * @param inputName 입력값
+     */
     private void initCar(String inputName) {
         String[] carNameList = inputName.split(",");
-
-        //자동차 이름은 5자를 초과할 수 없다 - 초과체크 구현
         carNameValidChk(carNameList);
-
         garage = new ArrayList<>();
         for (int i = 0; i < carNameList.length; i++) {
             Car car = new Car(carNameList[i]);
@@ -80,9 +98,14 @@ public class RacingCar {
         }
     }
 
+    /**
+     * 이름 예외 처리 함수
+     *
+     * @param carNameList 자동차 이름 리스트
+     */
     private void carNameValidChk(String[] carNameList) {
-        for(int i = 0; i < carNameList.length; i++) {
-            if(carNameList[i].length() > 5) {
+        for (int i = 0; i < carNameList.length; i++) {
+            if (carNameList[i].length() > 5) {
                 throw new IllegalArgumentException(carNameList[i] + " 자동차 이름은 5자를 초과할 수 없습니다. ");
             }
         }
