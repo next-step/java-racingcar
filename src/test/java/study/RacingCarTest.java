afe81d9f81426d81531d21944df23cdeb236b3ae
givxq <coooeeeeep@gmail.com>
2023-03-04 16:31:58 +0900

    [study] 자동차 경주

--- src/test/java/study/RacingCarTest.java
+++ src/test/java/study/RacingCarTest.java
@@ -1,18 +1,39 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import service.RacingCarService;
+import view.RacingCarView;
 
 import java.io.*;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class RacingCarTest {
 
     RacingCarService racingCarService;
 
-    @Test
-    void startRacing() {
-
+    static Stream<Arguments> provideCarNamesAndTargetDistance() {
+        return Stream.of(
+                Arguments.of("pobi,woni,jun", 5),
+                Arguments.of("pobi", 5),
+                Arguments.of("pobi,woni", 5));
     }
+
+    @DisplayName("자동차 경주 테스트")
+    @ParameterizedTest
+    @MethodSource("provideCarNamesAndTargetDistance")
+    void startRacing(String input, int targetDistance) {
+        List<String> carNames = RacingCarView.getCarNames(input);
+
+        racingCarService = new RacingCarService();
+        racingCarService.startRacing(carNames, targetDistance);
+    }
+
 }
