f64cdd4c1bbb3280debca476ad9eecd2a042aaa4
ssolssolham <bruno.3000@kakaopaysec.com>
2023-03-05 17:09:02 +0900

    feat : RacingRandomTest 작성
    0 ~ 9 사이의 무작위를 반환하는 함수를 대상으로 5000회 테스트

---
+++ src/test/java/racingcar/RacingRandomTest.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingRandomTest {
+    @Test
+    void randomizeTenBegger(){
+        for(int i=0;i<5000;i++){
+            assertThat(RacingRandom.randomizeTenBegger())
+                    .isGreaterThan(-1)
+                    .isLessThan(10);
+        }
+    }
+
+}
