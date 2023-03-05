6180be56831e26ea5e6ccb864b8a95cfe24d7943
chris.tmas <chris.tmas@kakaopaysec.com>
2023-03-06 01:15:35 +0900

    test: 랜덤값이 4이상일 때만 움직이는지 확인 (주입해주는 Random 객체를 상속받아서 하드코딩된 값을 반환하게 만듦)

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -3,6 +3,8 @@ package racing;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 import java.util.Random;
@@ -47,5 +49,26 @@ class CarTest {
         List<Car> racingWinners = race.getRacingWinners();
 
         Assertions.assertThat(racingWinners).containsExactly(carC, carD);
+    }
+
+    @DisplayName("랜덤값이 4이상일때 움직인다.")
+    @ParameterizedTest
+    @CsvSource({"1, true", "2, true", "3, true", "4, false", "5, false",
+            "6, false", "7, false", "8, false", "9, false"})
+    void stopTest(int randomResult, boolean isMove) {
+        Car defaultCar = new Car("zero", 0);
+        Car moveCar = new Car("test");
+        moveCar.move(new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return randomResult;
+            }
+        });
+
+        Assertions.assertThat(isSameLocation(defaultCar, moveCar)).isEqualTo(isMove);
+    }
+
+    private boolean isSameLocation(Car defaultCar, Car moveCar) {
+        return moveCar.compareTo(defaultCar) == 0;
     }
 }
