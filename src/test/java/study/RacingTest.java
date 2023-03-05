dd5e66e53f72e3d02523a2d2ab2b95f0c2a132a8
jade.cyan <cyanjd1226@gmail.com>
2023-03-05 23:33:51 +0900

    study: learn about StringCar, RacingCar

--- src/test/java/study/RacingTest.java
+++ src/test/java/study/RacingTest.java
@@ -6,13 +6,10 @@ import RacingCar.RacingCar;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import RacingCar.Car;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class RacingTest {
