a451d6993ffd2be22db32f757a0cfda15bc10f31
eddy-20220147 <eddy-20220147@(1.5)-eddy-20220147-MacBookPro>
2023-03-06 11:00:01 +0900

    test

--- src/main/java/racingcar/racingcar.java
+++ src/main/java/racingcar/racingcar.java
@@ -1,17 +1,18 @@
 package racingcar;
+
 import java.util.Scanner;
 
 public class racingcar {
     public static void main(String input) {
-        String [] ArrayCarMembers;
-        int racingCount ;
+        String[] ArrayCarMembers;
+        int racingCount;
         Scanner scan = new Scanner(System.in);
 
 
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String StringCarMember = scan.nextLine();
         ArrayCarMembers = StringCarMember.split(",");
-        if(! validateCarlength(ArrayCarMembers)) {
+        if (!validateCarlength(ArrayCarMembers)) {
             System.out.println("자동차 이름이 너무 깁니다");
             return;
         }
@@ -24,42 +25,35 @@ public class racingcar {
         System.out.println("최종 우승자:");
 
 
-
     }
 
 
+    static boolean validateCarlength(String[] ArrayCarMember) {
 
-
-
-
-
-
-    static boolean validateCarlength(String [] ArrayCarMember) {
-
-        for(int i = 0; i < ArrayCarMember.length; i += 1) {
-            if(ArrayCarMember[i].length() > 5)
+        for (int i = 0; i < ArrayCarMember.length; i += 1) {
+            if (ArrayCarMember[i].length() > 5)
                 return false;
         }
         return true;
     }
 
 
-    static int RaceStart(String [] ArrayCarMembers, int racingCount) {
-        for(int k = 0; k <ArrayCarMembers.length; k +=1) {
+    static int RaceStart(String[] ArrayCarMembers, int racingCount) {
+        for (int k = 0; k < ArrayCarMembers.length; k += 1) {
             int Array
         }
 
-        for(int i = 0; i < racingCount; i +=1) {
-            for(int j = 0; j <ArrayCarMembers.length; j +=1) {
-                if( randomValue())
-                    System.out.println(ArrayCarMembers[i]+" : " );
+        for (int i = 0; i < racingCount; i += 1) {
+            for (int j = 0; j < ArrayCarMembers.length; j += 1) {
+                if (randomValue())
+                    System.out.println(ArrayCarMembers[i] + " : ");
 
             }
         }
 
     }
 
-    static boolean randomValue () {
+    static boolean randomValue() {
         int randomVal = (int) (Math.random() * 10);
         if (randomVal > 3) {
 
