16dbed6f7f189f3784c640a7882e131ee4f80535
ssolssolham <bruno.3000@kakaopaysec.com>
2023-03-08 21:28:36 +0900

    fix : RacingMain inputLine 등 변수명 수정

--- src/main/java/racingcar/RacingMain.java
+++ src/main/java/racingcar/RacingMain.java
@@ -6,12 +6,12 @@ public class RacingMain {
     public static void main(String[] args) {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         Scanner sc = new Scanner(System.in);
-        String inputLine = sc.nextLine();
-        String[] racingCars = inputLine.split(",");
+        String racingCarsInput = sc.nextLine();
+        String[] racingCars = racingCarsInput.split(",");
         Racing racing = new Racing(racingCars);
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String inputLine2 = sc.nextLine();
-        racing.run(Integer.parseInt(inputLine2));
+        String tryCount = sc.nextLine();
+        racing.run(Integer.parseInt(tryCount));
         racing.printFinalWinner();
     }
 }
