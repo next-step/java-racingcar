cebb8d90b19aacf349594869835c35abde29b792
ssolssolham <bruno.3000@kakaopaysec.com>
2023-03-08 21:20:43 +0900

    fix : OneLineSolver 접근 제한자 public -> private 수정

--- src/main/java/calculator/OneLineSolver.java
+++ src/main/java/calculator/OneLineSolver.java
@@ -4,33 +4,37 @@ import calculator.decision.DecisionType;
 
 public class OneLineSolver {
     public double solve(double num1, double num2, DecisionType decisionType){
-        switch(decisionType){
-            case NONE:
-                return num2;
-            case PLUS:
-                return sum(num1, num2);
-            case MINUS:
-                return minus(num1, num2);
-            case MULTIPLY:
-                return multiply(num1, num2);
-            case DIVIDE:
-                return divide(num1, num2);
+        if(decisionType.equals(DecisionType.NONE)){
+            return num2;
+        }
+        if(decisionType.equals(DecisionType.PLUS)){
+            return sum(num1, num2);
+        }
+        if(decisionType.equals(DecisionType.MINUS)){
+            return minus(num1, num2);
+        }
+        if(decisionType.equals(DecisionType.MULTIPLY)){
+            return multiply(num1, num2);
+        }
+
+        if(decisionType.equals(DecisionType.DIVIDE)){
+            return divide(num1, num2);
         }
         return 0;
     }
 
-    public double sum(double num1, double num2){
+    private double sum(double num1, double num2){
         return num1 + num2;
     }
-    public double minus(double num1, double num2){
+    private double minus(double num1, double num2){
         return num1 - num2;
     }
 
-    public double multiply(double num1, double num2){
+    private double multiply(double num1, double num2){
         return num1 * num2;
     }
 
-    public double divide(double num1, double num2){
+    private double divide(double num1, double num2){
         return num1 / num2;
     }
 }
