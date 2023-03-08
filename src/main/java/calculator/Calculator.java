79126a8719f02dfcfb0de54e3d9f1595728458ea
dh-son <89595116+dh-son@users.noreply.github.com>
2023-03-08 17:42:52 +0900

    Step1 (#4008)

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,45 @@
+package calculator;
+
+import java.util.Arrays;
+
+public class Calculator {
+
+    public int calculatorByString(final String input) {
+        String [] convertInput = input.split(" ");
+
+        int result = 0;
+        for (int i = 0; i < convertInput.length; i++) {
+            if (i == 0) {
+                result = Integer.parseInt(convertInput[i]);
+            }
+
+            if (isSigned(convertInput[i])) {
+                result = getCalc(result, Integer.parseInt(convertInput[i+1]), convertInput[i]);
+            }
+        }
+
+        return result;
+    }
+
+    private int getCalc(int i, int j, String sign) {
+        int returnVal;
+
+        switch (sign) {
+            case "*": returnVal = i * j; break;
+            case "+": returnVal = i + j; break;
+            case "-": returnVal = i - j; break;
+            case "/": returnVal = i / j; break;
+            default: returnVal = i; break;
+        }
+
+        return returnVal;
+    }
+
+    private boolean isSigned(String sign) {
+        if (sign.equals("*") || sign.equals("+") || sign.equals("-") || sign.equals("/")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,56 @@
+package study;
+
+import calculator.Calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class StringTest {
+    @Test
+    void split1() {
+        final String[] actual = "1,2".split(",");
+        //actual contains [1, 2]
+        System.out.println(Arrays.toString(actual));
+        assertThat(actual).containsExactly("1", "2", "3");
+    }
+
+    @Test
+    void split2() {
+        final String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String [] actual = "(1,2)".replace("(", "").replace(")", "").split(",");
+//        String [] actual1 = "(1,2)".substring(1, 4);
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void charAt() {
+        String actual = "abc".substring(1, 2);
+        char actual1 = "abc".charAt(2);
+        assertThat(actual1).isEqualTo("c");
+
+    }
+
+    @DisplayName("특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException 발생")
+    @Test
+    void charAt2() {
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> "abc".charAt(4));
+    }
+
+    @Test
+    void calculator() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.calculatorByString("2 + 3 * 4 / 2");
+        assertThat(result).isEqualTo(10);
+    }
+}
