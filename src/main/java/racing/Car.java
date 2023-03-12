7b26d11a7c5779dc579ce401fff994e1cca26854
KAI-20200079\kakaopaysec <kai.bk.study@gmail.com>
2023-03-12 20:42:20 +0900

    step2 init & start

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -8,11 +8,11 @@ public class Car {
 
     private int position;
 
-    public Car(String name){
+    public Car(String name) {
         this(name, 0);
     }
 
-    public Car(String name, int position){
+    public Car(String name, int position) {
         checkName(name);
 
         this.position = position;
@@ -20,30 +20,29 @@ public class Car {
         this.moveOption = 4;
     }
 
-    private void checkName(String name){
-        if(name.length() > 5){
+    private void checkName(String name) {
+        if (name.length() > 5) {
             throw new IllegalArgumentException("차이름은 5글자 미만이여야 합니다.");
         }
     }
 
-    public int move(int moveValue){
-       /* System.out.println(("move > "+moveValue));*/
-        if(moveOption <= moveValue){
+    public int move(int moveValue) {
+        if (moveOption <= moveValue) {
             position++;
         }
 
         return position;
     }
 
-    public String getPositionFormat(){
+    public String getPositionFormat() {
         return name + " : " + translatePosition(this.position);
     }
 
-    public String translatePosition(int position){
+    public String translatePosition(int position) {
 
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < position; i++){
+        for (int i = 0; i < position; i++) {
             sb.append("-");
         }
 
--- src/main/java/racing/TheGame.java
+++ src/main/java/racing/TheGame.java
@@ -9,7 +9,7 @@ public class TheGame {
     private final int round;
     private Random random;
 
-    public TheGame(String names, int round){
+    public TheGame(String names, int round) {
         this.cars = initCars(names);
         this.round = round;
         this.random = new Random();
@@ -21,7 +21,6 @@ public class TheGame {
         System.out.println("자동차 이름을 입력하세요(쉼표(,)구분");
         String names = in.nextLine();
 
-
         System.out.println("시도할 회수를 입력하세요!");
         int round = Integer.parseInt(in.nextLine());
 
@@ -30,33 +29,31 @@ public class TheGame {
         System.out.println("실행결과");
         int winnerPosition = TheGame.racing();
 
-        System.out.println("우승자: "+TheGame.getWinner(winnerPosition));
+        System.out.println("우승자: " + TheGame.getWinner(winnerPosition));
     }
 
-    public Car[] initCars(String names){
-
+    public Car[] initCars(String names) {
         String[] nameArray = names.split(",");
-
         Car[] cars = new Car[nameArray.length];
 
-        for(int i = 0; i < nameArray.length; i++){
+        for (int i = 0; i < nameArray.length; i++) {
             cars[i] = new Car(nameArray[i]);
         }
 
         return cars;
     }
 
-    void printCarPosition(){
-        for(Car car : cars){
+    void printCarPosition() {
+        for (Car car : cars) {
             System.out.println(car.getPositionFormat());
         }
     }
 
-    private int moveCars(){
+    private int moveCars() {
         int carPosistion = 0;
 
-        for(Car car : cars){
-           /* carPosistion = Math.max(car.move(random.nextInt(10)), carPosistion);*/
+        for (Car car : cars) {
+            /* carPosistion = Math.max(car.move(random.nextInt(10)), carPosistion);*/
             carPosistion = car.move(random.nextInt(10));
         }
 
@@ -64,11 +61,10 @@ public class TheGame {
     }
 
 
-
-    public int racing(){
+    public int racing() {
         int maxPosition = 0;
 
-        for(int i = 0; i < round; i++){
+        for (int i = 0; i < round; i++) {
             maxPosition = Math.max(moveCars(), maxPosition);
             printCarPosition();
         }
@@ -83,8 +79,8 @@ public class TheGame {
         for (int i = 0; i < cars.length; i++) {
 
             if (cars[i].getPosition() == maxPosition) {
-                winnerCnt ++;
-                if(winnerCnt >= 2) {
+                winnerCnt++;
+                if (winnerCnt >= 2) {
                     winnerName += ", ";
                 }
                 winnerName += cars[i].getName();
