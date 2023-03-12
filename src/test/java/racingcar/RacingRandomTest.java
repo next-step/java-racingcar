942c1d08d2459f7a37ca6b5896c8b9bda78c3fe3
ssolssolham <bruno.3000@kakaopaysec.com>
2023-03-12 20:42:54 +0900

    feat : domain 패키지 추가로 인한 import 변경

--- src/test/java/racingcar/RacingRandomTest.java
+++ src/test/java/racingcar/RacingRandomTest.java
@@ -1,7 +1,7 @@
 package racingcar;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.RacingRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
