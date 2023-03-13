771cd836db6e9cf1d40ccaf3ba8dd6068a90a539
KAITLIN-2019005\kakaopaysec <kaitlin.kpsec@gmail.com>
2023-03-13 20:35:17 +0900

    study: 문자열 계산기 1차 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,81 +3,88 @@ package calculator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public class Calculator {
-    int a = Integer.MIN_VALUE;
-    int b = Integer.MIN_VALUE;
 
-    String operand = "";
+    private int a = Integer.MIN_VALUE;
+    private int b = Integer.MIN_VALUE;
 
-    public int add(int a, int b) {
-        return a+b;
-    }
-    public int substract(int a, int b) {
-        return a-b;
-    }
-    public int multiply(int a, int b) {
-        return a*b;
-    }
-    public int divide(int a, int b) {
-        return a/b;
+    private String operand = "";
+
+    private int add(int a, int b) {
+        return a + b;
     }
 
-    public static void main(String[] args) {
-        String input = "2 + 3 / 1";
-        String[] numbers = input.split("[^0-9]");
-        String[] operands = input.split("[0-9]+");
-        for(String s : numbers) System.out.print(s);
-        System.out.println("");
-        for(String s : operands) System.out.print(s);
+    private int substract(int a, int b) {
+        return a - b;
     }
 
-    public int calulator(String input) {
-        inputValidCheck(input);
+    private int multiply(int a, int b) {
+        return a * b;
+    }
 
-        List<String> inputString = new ArrayList<>();
-        inputString.addAll(List.of(input.split(" ")));
+    private int divide(int a, int b) {
+        return a / b;
+    }
+
+
+    public int calculator(String input) {
+        inputIsNumllOrEmptyCheck(input);
+
+        List<String> inputString = getStrings(input);
+
+        return stringCalculator(inputString);
+    }
+
+    private int stringCalculator(List<String> inputString) {
         a = Integer.parseInt(inputString.get(0));
         inputString.remove(0);
-
-        while(!inputString.isEmpty()) {
+        while (!inputString.isEmpty()) {
             operand = operandValidCheck(inputString.get(0));
             inputString.remove(0);
             b = Integer.parseInt(inputString.get(0));
-            System.out.print(a + " " + operand + " " + b + " = ");
-            a = excuteCalulator(a,b,operand);
-            System.out.println(a);
+            a = excuteCalulator(a, b, operand);
             inputString.remove(0);
         }
-        System.out.println(a);
         return a;
     }
 
-    @Contract("_ -> param1")
-    private @NotNull String operandValidCheck(String operand) {
-        if(!(operand.equals("+") || operand.equals("-") || operand.equals("*") || operand.equals("/"))) {
-            throw new IllegalArgumentException("사칙연산 기호가 아닌 기호가 포함되어있습니다. ("+operand+")");
+    private List<String> getStrings(String input) {
+        List<String> inputString = new ArrayList<>();
+        inputString.addAll(List.of(input.split(" ")));
+        return inputString;
+    }
+
+    private String operandValidCheck(String operand) {
+        if (!(operand.equals("+") || operand.equals("-") || operand.equals("*") || operand.equals(
+            "/"))) {
+            throw new IllegalArgumentException("사칙연산 기호가 아닌 기호가 포함되어있습니다. (" + operand + ")");
         }
         return operand;
     }
 
     private int excuteCalulator(int a, int b, String operand) {
-        if(operand.equals("+"))
+        if (operand.equals("+")) {
             return add(a, b);
-        if(operand.equals("-"))
+        }
+        if (operand.equals("-")) {
             return substract(a, b);
-        if(operand.equals("*"))
+        }
+        if (operand.equals("*")) {
             return multiply(a, b);
-        if(operand.equals("/"))
+        }
+        if (operand.equals("/")) {
             return divide(a, b);
-        return -9;
+        }
+        return 0;
     }
 
-    private void inputValidCheck(String input) {
-        if(input.isBlank() || input.isEmpty() || input == null) {
+    private void inputIsNumllOrEmptyCheck(String input) {
+        if (input == null || input.isEmpty()) {
             throw new IllegalArgumentException("입력값이 null이거나 빈 공백 문자입니다.");
         }
     }
--- src/test/java/test/calculatorTest.java
+++ src/test/java/test/calculatorTest.java
@@ -1,65 +1,83 @@
 package test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import calculator.Calculator;
 import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class calculatorTest {
 
+    @DisplayName("입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw")
     @ParameterizedTest
-    @ValueSource(strings = {"2 + 3", "10 + 8"})
+    @NullAndEmptySource
+//    @ValueSource(strings = {""})
+    void inputValidCheckTest(String text) {
+        AssertionsForClassTypes.assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> {
+                Calculator calc = new Calculator();
+                calc.calculator(text);
+            }).isInstanceOf(IllegalArgumentException.class)
+            .withMessageMatching("입력값이 null+.*")
+            .withMessageMatching(".*빈 공백.*");
+    }
+
+    @DisplayName("덧셈 기호가 있으면 덧셈을 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"2 + 3"})
     void addTest(String input) {
         Calculator calc = new Calculator();
-        calc.calulator(input);
+        assertThat(calc.calculator(input)).isEqualTo(5);
     }
+
+    @DisplayName("뺄셈 기호가 있으면 뺄셈을 한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"2 - 3", "10 - 8"})
+    @ValueSource(strings = {"2 - 3"})
     void substractTest(String input) {
         Calculator calc = new Calculator();
-        calc.calulator(input);
+        assertThat(calc.calculator(input)).isEqualTo(-1);
     }
+
+    @DisplayName("곱셈 기호가 있으면 곱셈을 한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"2 * 3", "10 * 8"})
+    @ValueSource(strings = {"2 * 3"})
     void multipulyTest(String input) {
         Calculator calc = new Calculator();
-        calc.calulator(input);
+        assertThat(calc.calculator(input)).isEqualTo(6);
     }
+
+    @DisplayName("나눗셈 기호가 있으면 나눗셈을 한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"2 / 3", "10 / 8", "6 / 3"})
+    @ValueSource(strings = {"2 / 3"})
     void divideTest(String input) {
         Calculator calc = new Calculator();
-        calc.calulator(input);
+        assertThat(calc.calculator(input)).isEqualTo(0);
     }
+
+    @DisplayName("사칙연산 기호가 모두 있으면 사칙연산을 한다.")
     @ParameterizedTest
     @ValueSource(strings = {"2 + 3 * 4 / 2"})
-        //@ValueSource(strings = {"2 + 3 * 4 / 2", "3 * 10 - 5 / 5"})
     void calculatorTest(String input) {
         Calculator calc = new Calculator();
-        calc.calulator(input);
+        assertThat(calc.calculator(input)).isEqualTo(10);
     }
 
-    @Test
-    void inputValidCheckTest() {
+    @DisplayName("사칙연산 기호가 아닌 경우 IllegalArgumentException throw")
+    @ParameterizedTest
+    @ValueSource(strings = {"2 ++ 3 * 4 / 2"})
+    void inputOperandCheckTest(String input) {
         AssertionsForClassTypes.assertThatExceptionOfType(IllegalArgumentException.class)
             .isThrownBy(() -> {
                 Calculator calc = new Calculator();
-                calc.calulator("");
-            }).withMessageMatching("입력값이 null+.*")
-            .withMessageMatching(".*빈 공백.*");
-
-    }
-    @Test
-    void inputOperandCheckTest() {
-        AssertionsForClassTypes.assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> {
-                Calculator calc = new Calculator();
-                calc.calulator("2 ++ 3 * 4 / 2");
-            }).withMessageMatching("사칙연산+.*");
+                calc.calculator(input);
+            }).isInstanceOf(IllegalArgumentException.class)
+            .withMessageMatching("사칙연산+.*");
     }
 }
