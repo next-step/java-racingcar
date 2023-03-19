79031ffbae15631d2cfbe8b29fc48343224a9910
gyusikwon <wonks7@gmail.com>
2023-03-19 21:52:48 +0900

    자동차경주

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,2 +1,37 @@
-package study;public class SetTest {
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void chkSize() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ValueSource(ints = {1, 2, 3})
+    @ParameterizedTest
+    void contains(final int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,2 +1,48 @@
-package study;public class StringTest {
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] split = "1,2".split(",");
+        System.out.println(Arrays.toString(split));
+        assertThat(split).containsExactly("1","2","3");
+    }
+
+    @Test
+    void split2() {
+        String[] split2 = "1".split(",");
+        System.out.println(Arrays.toString(split2));
+        assertThat(split2).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        char actual = "abc".charAt(2);
+        assertThat(actual).isEqualTo('c');
+    }
+
+
+    @DisplayName("특정 위치의 문자를 가져올 때 위치 값을 벗어나면 예외가 발생한다.")
+    @Test
+    void charAt2() {
+        char actual = "abc".charAt(4);
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> "abc".charAt(4));
+    }
+
+
 }
