c2200b8b8f0f2c65e09b4922b0a9ee00139808b9
givxq <coooeeeeep@gmail.com>
2023-03-20 00:04:30 +0900

    [study] 자동차 경주 리팩터링 - develop

--- src/test/java/study/StringCalcuratorTest.java
+++ src/test/java/study/StringCalcuratorTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
