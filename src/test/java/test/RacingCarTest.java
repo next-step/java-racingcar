b882bfbcef3868708c13ea0ff273c6686a25fa38
KAITLIN-2019005\kakaopaysec <kaitlin.kpsec@gmail.com>
2023-03-23 23:38:09 +0900

    feat : CarTest 로 일부 테스트 이관, RacingGame테스트 추가

--- src/test/java/test/RacingCarTest.java
+++ src/test/java/test/RacingCarTest.java
@@ -3,115 +3,55 @@ package test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import java.io.ByteArrayInputStream;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingCar.Car;
-import racingCar.PrintCarNames;
-import racingCar.RacingGame;
-import racingCar.Winner;
+import racingCar.domain.Car;
+import racingCar.domain.RacingGame;
+import racingCar.view.InputView;
 
 public class RacingCarTest {
 
-    private RacingGame racingGame;
+    private final int MOVABLE_NUMBER = 4;
 
-    @BeforeEach
-    void setUp() {
-        racingGame = new RacingGame("pobi,woni,jun", 5);
-    }
-
-    @DisplayName("자동차경주")
+    @DisplayName("경주0바퀴")
     @Test
-    void racingTest() {
-        racingGame.race();
-    }
+    void race_0() {
+        RacingGame racingGame = new RacingGame("pobi,woni,jun");
+        racingGame.race(racingGame.getCars(), 0);
+        assertThat(racingGame.getTryCount()).isZero();
 
-    @DisplayName("자동차경주")
-    @Test
-    void racingTest2() {
-        System.setIn(new ByteArrayInputStream("pobi,woni,jun\n5".getBytes()));
-        RacingGame racingGame = new RacingGame();
-        racingGame.race();
     }
-
-    @DisplayName("자동차는 이름을 가진다.")
+    @DisplayName("경주5바퀴")
     @Test
-    void hasName() {
-        final Car car = new Car("pobi");
-        assertThat(car.getName()).isEqualTo("pobi");
-    }
+    void race_5() {
+        RacingGame racingGame = new RacingGame("pobi,woni,jun", 5);
+        racingGame.race(racingGame.getCars(), 5);
+        assertThat(racingGame.getTryCount()).isEqualTo(5);
 
-    @DisplayName("자동차 이름의 길이가 5글자를 넘으면 예외가 발생한다.")
-    @Test
-    void racingInputLengthErrorTest() {
-        assertThatThrownBy(() -> {
-            new Car("November");
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("자동차의 초기 위치는 0이다.")
-    @Test
-    void showInitPosition() {
-        final Car car = new Car("pobi");
-        assertThat(car.getPosition()).isEqualTo(0);
-    }
-
-    @DisplayName("자동차의 현재 위치를 알 수 있다.")
-    @Test
-    void getCurrentPosition() {
-        Car car = new Car("june", 10);
-        assertThat(car.getPosition()).isEqualTo(10);
-    }
-
-    @DisplayName("자동차가 4 이상일 때 전진한다.")
-    @Test
-    void move() {
-        Car car = new Car("june");
-        car.move(4);
-        assertThat(car.getPosition()).isEqualTo(1);
-    }
-
-    @DisplayName("자동차가 4 미만일 때 정지한다.")
-    @Test
-    void stop() {
-        Car car = new Car("june");
-        car.move(3);
-        assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @DisplayName("자동차들 중에서 가장 멀리 간 위치가 어디인지 알 수 있다.")
     @Test
     void far() {
-        racingGame.race();
-        System.out.println(racingGame.findMaxPosition());
-    }
+        List<Car> cars = new ArrayList<>();
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("crong");
+        Car car3 = new Car("honux");
 
-    @DisplayName("자동차 한 대가 우승한 경우")
-    @Test
-    void winner() {
-        List<Car> cars = Arrays.asList(
-            new Car("pobi", 2),
-            new Car("crong", 3),
-            new Car("honux", 1)
-        );
+        RacingGame racingGame = new RacingGame("pobi,woni,jun");
 
-        List<String> winner = new Winner().getWinner(3, cars);
-        assertThat(new PrintCarNames().toString(winner)).isEqualTo("crong");
-    }
+        racingGame.getCars().get(0).move(MOVABLE_NUMBER);
+        racingGame.getCars().get(0).move(MOVABLE_NUMBER);
+        racingGame.getCars().get(1).move(MOVABLE_NUMBER);
+        racingGame.getCars().get(1).move(MOVABLE_NUMBER);
+        racingGame.getCars().get(1).move(MOVABLE_NUMBER);
+        racingGame.getCars().get(2).move(MOVABLE_NUMBER);
+        racingGame.getCars().get(2).move(MOVABLE_NUMBER);
+        racingGame.getCars().get(2).move(MOVABLE_NUMBER);
 
-    @DisplayName("자동차 여러대가 우승한 경우")
-    @Test
-    void winners() {
-        List<Car> cars = Arrays.asList(
-            new Car("pobi", 3),
-            new Car("crong", 3),
-            new Car("honux", 1)
-        );
-
-        List<String> winners = new Winner().getWinner(3, cars);
-        assertThat(new PrintCarNames().toString(winners)).isEqualTo("pobi, crong");
+        assertThat(racingGame.findMaxPosition(racingGame.getCars())).isEqualTo(3);
     }
 }
