7e2b58579523dd1c62aebb6c2b88b4b9aed86c25
KAITLIN-2019005\kakaopaysec <kaitlin.kpsec@gmail.com>
2023-03-23 23:17:34 +0900

    feat: Name 타입 클래스 생성

---
+++ src/main/java/racingCar/domain/Name.java
@@ -0,0 +1,19 @@
+package racingCar.domain;
+
+public class Name {
+
+    private final int CAR_NAME_MAX_LENGTH = 5;
+    private final String value;
+
+    public Name(final String value) {
+
+        if (value.length() > CAR_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은" + CAR_NAME_MAX_LENGTH + "자를 초과할 수 없습니다.");
+        }
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return value;
+    }
+}
