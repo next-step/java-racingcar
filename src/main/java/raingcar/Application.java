ce9e3425f3b877519472dcc096ca718d5f3b6dbb
sycha72 <sycha72@gmail.com>
2023-03-27 21:02:24 +0900

    feat : reformat

--- src/main/java/raingcar/Application.java
+++ src/main/java/raingcar/Application.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 import java.util.concurrent.LinkedTransferQueue;
 
 public class Application {
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
         RacingInput intput = new RacingInput();
         String carNames = intput.getCars();
-        int     rap = intput.getRap();
+        int rap = intput.getRap();
 
         RacingGame game = new RacingGame(carNames);
         game.play(rap);
