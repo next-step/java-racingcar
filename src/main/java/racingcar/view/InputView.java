1747dc1deee04293b5bdbcf3dd446c2572001b37
jade.cyan <cyanjd1226@gmail.com>
2023-03-27 14:51:23 +0900

    프로그램 공백 정리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,9 +9,7 @@ import java.util.Scanner;
 public class InputView {
 
     public static List<Car> inputCars() {
-
         List<Car> cars =  new ArrayList<Car>();
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n");
         String inputValue = scanner.nextLine();
@@ -20,17 +18,13 @@ public class InputView {
         for(int i=0; i < splitValue.length; i++) {
             cars.add(new Car(splitValue[i]));
         }
-
         return cars;
     }
 
     public static int inputGameCnt() {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("시도할 횟수는 몇 회인가요?");
-
         int gameCount = Integer.parseInt(scanner.nextLine());
-
         return gameCount;
     }
 }
