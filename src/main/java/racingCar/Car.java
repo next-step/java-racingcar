142d82961f515a5f7a48c9e48b09bfbb26f5647c
hyeyeon-23 <wltinan@naver.com>
2023-03-31 17:40:07 +0900

    [feat] step1 자동차 경주

---
+++ src/main/java/racingCar/Car.java
@@ -0,0 +1,35 @@
+package racingCar;
+
+public class Car {
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final int DEFAULT_POSITION = 0;
+    private final String name;
+    private int position;
+
+    public Car(final String name){
+        this(name, DEFAULT_POSITION);
+        this.position = position;
+    }
+
+    public Car(final String name,int position){
+        if(name.length() > MAXIMUM_NAME_LENGTH){
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getPosition(){
+        return this.position;
+    }
+
+    public void move(int randomNumber){
+        if(randomNumber>=4){
+            this.position++;
+        }
+    }
+}
