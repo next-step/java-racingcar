df2782348750ef54d2aee7d8b4b8ddb2f480ea46
In-wook Hwang <inwook9@gmail.com>
2023-04-04 00:35:48 +0900

    StringTest - 요구사항1

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,21 @@
+package study;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("String 클래에 대한 학습 테스트")
+class StringTest {
+
+    @Test
+    void split() {
+        String[] result1 = "1,2".split(",");
+        String[] result2 = "1".split(",");
+        Assertions.assertAll(
+                () -> assertThat(result1).containsExactly("1", "2"),
+                () -> assertThat(result2).containsExactly("1")
+        );
+    }
+}
