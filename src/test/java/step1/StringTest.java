493a8e0f050e05ab185c812bd573e1b4fc6a5302
cohi <tkfka156@gmail.com>
2023-04-03 20:01:53 +0900

    rename: 패키지명 변경

--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -1,9 +1,8 @@
-package stringtest;
+package step1;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
