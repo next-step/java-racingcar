ad06c14cb32b45a45ee06a829e65beef1a3d5cc1
ethan.v <ethan.v@kakaocorp.com>
2023-04-03 19:07:57 +0900

    [step1] 1단계 과제

---
+++ src/test/java/step1/SetCollectionTest.java
@@ -0,0 +1,47 @@
+package step1;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("요구사항1")
+    void setSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("요구사항2")
+    @ValueSource(ints = {1, 2, 3})
+    void setContains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("요구사항3")
+    void setCsvSource(String num, String expected) {
+        assertThat(numbers.contains(Integer.parseInt(num))).isEqualTo(Boolean.parseBoolean(expected));
+    }
+}
