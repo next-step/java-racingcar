bb7d66883f11ca5bee1f643ff0ad58450664c4ad
In-wook Hwang <inwook9@gmail.com>
2023-04-04 00:56:25 +0900

    SetTest - 요구사항1

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@DisplayName("Set Collection에 대한 학습 테스트")
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드는 Set의 크기를 반환합니다.")
+    void size() {
+        Assertions.assertThat(numbers.size()).isSameAs(3);
+    }
+}
