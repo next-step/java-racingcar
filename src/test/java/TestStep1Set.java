5d8f595e7fc1fe4746d6d90c863af1024b4bcca3
jaden <36589622+jadenChoo@users.noreply.github.com>
2023-04-03 23:12:55 +0900

    Step1 (#4085)
    * test: string Class split
    
    * test: set class
    
    * refactor: rename test function to verb
    
    ---------
    
    Co-authored-by: jaden.1230 <jaden.1230@kakaocorp.com>

---
+++ src/test/java/TestStep1Set.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestStep1Set {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+
+    @Test
+    @DisplayName("req1")
+    void checkSetSize() {
+        /* 요구 1 */
+        Assertions.assertEquals(numbers.size(),3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("req2")
+    void checkContainAll(int inputNumber) {
+        /* 요구 2 */
+        Assertions.assertTrue(numbers.contains(inputNumber));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true","3:true","4:false","5:false"}, delimiter = ':')
+    @DisplayName("req3")
+    void checkIfContain(int inputNumber, boolean IsContain) {
+        /* 요구 3 */
+        Assertions.assertEquals(numbers.contains(inputNumber), IsContain);
+    }
+
+
+
+
+
+}
---
+++ src/test/java/TestStep1String.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@DisplayName("step1")
+public class TestStep1String {
+
+    @Test
+    @DisplayName("req1_1")
+    void splitByComma() {
+        /* 요구 1 */
+        final String inputData = "1,2";
+
+        final String[] expected = {"1","2"};
+
+        final String[] actual = inputData.split(",");
+
+        Assertions.assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("req1_2")
+    void splitOne() {
+        /* 요구 1 */
+        final String inputData = "1";
+
+        final String[] expected = {"1"};
+
+        final String[] actual = inputData.split(",");
+
+        Assertions.assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("req2")
+    void splitBetweenBracket() {
+        /* 요구 2 */
+        final String inputData = "(1,2)";
+
+        final String[] expected = {"1","2"};
+
+        final String[] actual = inputData.substring(1, inputData.length()-1).split(",");
+
+        Assertions.assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("req3_1")
+    void extractChar() {
+        /* 요구 3 */
+        final String inputData = "abc";
+
+        final String expected = "c";
+
+        final char actual = inputData.charAt(2);
+
+        Assertions.assertEquals(expected.charAt(0),actual);
+    }
+
+    @Test
+    @DisplayName("abc 문자열이 있을때 index 10 번째 위치의 문자를 가져오면 IndexOutOfBoundsException 발생")
+    void handelIndexOutOfBoundsException() {
+        /* 요구 3 */
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char result = "abc".charAt(9);
+                }).withMessageMatching("String index out of range: \\d+");
+    }
+
+}
+
