60ed1e23995b089fd99f436dc1af410a59229ddc
In-wook Hwang <inwook9@gmail.com>
2023-04-04 00:41:38 +0900

    StringTest - 요구사항2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -18,4 +18,10 @@ class StringTest {
                 () -> assertThat(result2).containsExactly("1")
         );
     }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
