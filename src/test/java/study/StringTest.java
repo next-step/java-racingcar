5a273047dcecb6582c12fca15aa76227081ae862
tataki26 <tataki26@gmail.com>
2023-04-03 23:24:03 +0900

    [자동차 경주] 1단계 - String 클래스 학습 테스트
    요구 사항 1-1

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,16 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    void split() {
+        // 1-1
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("2","1");
+        assertThat(result).containsExactly("1","2");
+    }
+}
