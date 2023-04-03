634b6600e0909b3ec808c1d810bfbfa94015830d
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-04-03 22:23:07 +0900

    test - charAt 활용 테스트

--- src/test/java/study/StringStudyTest.java
+++ src/test/java/study/StringStudyTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -28,5 +29,28 @@ class StringStudyTest {
         String result = parentheses.substring(position, parentheses.length()-position);
 
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 확인 테스트")
+    void charAtTest() {
+        String testString = "abc";
+
+        char a = testString.charAt(0);
+        char b = testString.charAt(1);
+        char c = testString.charAt(2);
+
+        assertThat(a).isEqualTo('a');
+        assertThat(b).isEqualTo('b');
+        assertThat(c).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("인덱스에서 벗어날 시 에러 반환 테스트")
+    void charAtErrorTest() {
+        String testString = "abc";
+
+        assertThrows(StringIndexOutOfBoundsException.class,
+                () -> testString.charAt(testString.length()));
     }
 }
