c255a61af679c00faca28ab8cf832bc4cbb4e19a
seominah <seominah95@gmail.com>
2023-04-03 22:07:58 +0900

    charAt study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -15,5 +18,14 @@ public class StringTest {
     void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 찾는 테스트, 위치를 벗어나면 StringIndexOutOfBoundsException 발생")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("index out of");
     }
 }
