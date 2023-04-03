94296469f362ed647a7bea45f2c138fa0cc44c21
lumioustone <mlicp1994@gmail.com>
2023-04-03 21:02:04 +0900

    test[StringTest]: add charAt test code

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,21 +1,40 @@
+import org.assertj.core.api.AbstractThrowableAssert;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
     @Test
+    @DisplayName("String class split test")
     void splitTest() {
         String[] actual = "1,2".split(",");
-        Assertions.assertThat(actual).containsExactly("1", "2");
+        assertThat(actual).containsExactly("1", "2");
 
         String[] actual1 = "1".split(",");
-        Assertions.assertThat(actual1).containsExactly("1");
+        assertThat(actual1).containsExactly("1");
     }
 
     @Test
-    void subStringTest() {
+    @DisplayName("String class substring test")
+    void substringTest() {
         String target = "(1,2)";
         String actual = target.substring(target.indexOf("(") + 1, target.indexOf(")"));
-        Assertions.assertThat(actual).isEqualTo("1,2");
+        assertThat(actual).isEqualTo("1,2");
     }
+
+    @Test
+    @DisplayName("String class charAt method test")
+    void charAtTest() {
+        String target = "abc";
+        char actual = target.charAt(0);
+
+        assertThat(actual).isEqualTo('a');
+
+        assertThatThrownBy(() -> target.charAt(target.length() + 1)).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+    }
+
 }
