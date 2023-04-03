4cc4455db1444f730210b8ecf33ea3725274c13d
juhyun <zzang9ha@naver.com>
2023-04-04 01:37:34 +0900

    test: Set Collection 학습 테스트 요구사항 3 추가

--- src/test/java/SetCollectionTest.java
+++ src/test/java/SetCollectionTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -31,6 +32,12 @@ public class SetCollectionTest {
     @ValueSource(ints = {1, 2, 3})
     void Set의_contains_메서드는_값이_존재하는지_확인한다(int number) {
         assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void Set의_contains_메서드는_값이_존재하면_true를_존재하지않으면_false를_반환한다(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
     }
 
 }
