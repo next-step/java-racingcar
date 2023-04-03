52eee9d5b0235f5b7bdd2fb630d812137e98bae2
Baek Jiyeon <100.paperkite@gmail.com>
2023-04-03 12:03:40 +0900

    feat: String 요구사항 2,3 구현

--- src/test/java/StringClassTest.java
+++ src/test/java/StringClassTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringClassTest {
 
@@ -26,5 +27,44 @@ public class StringClassTest {
 
         //then
         assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    public void substring_으로_괄호가_제거되어야_한다() {
+        //given
+        String test = "(1,2)";
+        
+        //when
+        String result = test.substring(1, test.length()-1);
+
+        //then
+        assertThat(result).isEqualTo("1,2");
+    }
+    
+    @Test
+    public void chatAt으로_특정_위치의_문자를_가져와야_한다() {
+        //given
+        String test = "abc";
+
+        //when
+        char a = test.charAt(0);
+        char b = test.charAt(1);
+        char c = test.charAt(2);
+
+        //then
+        assertThat(a).isEqualTo('a');
+        assertThat(b).isEqualTo('b');
+        assertThat(c).isEqualTo('c');
+    }
+    
+    @Test
+    public void charAt의_범위가_벗어나면_인덱스_예외가_나야_한다() {
+        //given
+        String test = "abc";
+        int outOfRange = test.length();
+
+        //when then
+        assertThatThrownBy(() -> test.charAt(outOfRange))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
