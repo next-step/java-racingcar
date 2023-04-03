bc44fcff3e9cf4d3ee43ed8d00e619fda4bde5f9
zuri-pong <zuri.pong@kakaocorp.com>
2023-04-04 01:16:50 +0900

    누락된 테스트 추가

--- src/test/java/study/step1/StringTest.java
+++ src/test/java/study/step1/StringTest.java
@@ -8,8 +8,11 @@ public class StringTest {
 
     @Test
     public void 요구사항1() {
-        String[] actual = "1,2".split(",");
-        assertThat(actual).containsExactly("1", "2");
+        String input1  = "1,2";
+        String input2 = "1";
+
+        assertThat(input1.split(",")).containsExactly("1", "2");
+        assertThat(input2.split(",")).containsExactly("1");
     }
 
     @Test
