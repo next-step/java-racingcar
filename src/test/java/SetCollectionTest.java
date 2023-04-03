7b251219635e695d296e79202c60195a347364de
lumioustone <mlicp1994@gmail.com>
2023-04-03 21:05:01 +0900

    test[SetCollectionTset]: add size method test code

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,28 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("SetCollection size method test")
+    void sizeTest() {
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    
+}
