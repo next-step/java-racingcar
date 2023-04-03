8133f10a14de2d5b0e3973b439cf5c8c703c40fc
highjune <highjune37@gmail.com>
2023-04-03 21:52:05 +0900

    Set 테스트 클래스 생성, 요구사항1 완성

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 Size() 메소드를 활용해 크기를 확인하는 학습테스트를 구현한다.")
+    void setSizeTest() {
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+
+}
