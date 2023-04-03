f19edd7a2e4639ab68e0522589b1ea915b2a2dab
ky-kiske <ky.kiske@kakaocorp.com>
2023-04-03 18:11:33 +0900

    feat : 요구사항 3 구현  - "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.  - String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.  - JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,10 +1,13 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
+    @DisplayName("split을 통해 콤마로 구분된 문자열을 각각의 원소로 나눌 수 있다.")
     @Test
     void splitTwoValues() {
         //GIVEN
@@ -18,6 +21,7 @@ public class StringTest {
         assertThat(result).containsExactly("1", "2");
     }
 
+    @DisplayName("split을 통해 콤마로 구분된 단일 원소를 가지고 있는 문자열로부터 원소를 추출할 수 있다.")
     @Test
     void splitOneValue() {
         //GIVEN
@@ -30,6 +34,7 @@ public class StringTest {
         assertThat(result).containsExactly("1");
     }
 
+    @DisplayName("substring을 통해 괄호로 감싸진 문자열에서 괄호를 제외한 부분의 문자열을 얻을 수 있다.")
     @Test
     void substringParentheses() {
         //GIVEN
@@ -40,5 +45,46 @@ public class StringTest {
 
         //THEN
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("chatAt을 통해 문자열로부터 각 인덱스별 문자를 얻을 수 있다.")
+    @Test
+    void chatAt() {
+        //GIVEN
+        String src = "abc";
+
+        //WHEN
+        char a = src.charAt(0);
+        char b = src.charAt(1);
+        char c = src.charAt(2);
+
+        //THEN
+        assertThat(a).isEqualTo('a');
+        assertThat(b).isEqualTo('b');
+        assertThat(c).isEqualTo('c');
+    }
+
+    @DisplayName("문자열의 길이보다 긴 인덱스로 chatAt 요청을 하면 OutOfBoundException이 발생한다.")
+    @Test
+    void charAtIndexOver() {
+        //GIVEN
+        String src = "abc";
+
+        //WHEN
+        //THEN
+        assertThatThrownBy(() -> src.charAt(5))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+    @DisplayName("음수인 인덱스로 chatAt 요청을 하면 OutOfBoundException이 발생한다.")
+    @Test
+    void charAtNegativeIndex() {
+        //GIVEN
+        String src = "abc";
+
+        //WHEN
+        //THEN
+        assertThatThrownBy(() -> src.charAt(-2))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
