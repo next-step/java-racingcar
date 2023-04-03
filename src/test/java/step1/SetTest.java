802a6807b90c039588b74de0ce71cb97cc238b81
cohi <tkfka156@gmail.com>
2023-04-03 20:05:45 +0900

    test: Set test 요구사항 1 구현

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,33 @@
+package step1;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("set size 측정")
+    @Test
+    void test1() throws Exception {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+
+
+}
