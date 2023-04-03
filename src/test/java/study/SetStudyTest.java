831740c045d6c4880fb4cf1daabc9df4844945af
kyeongminlee <kyeongmin.jake.lee@gmail.com>
2023-04-03 22:39:09 +0900

    test - contains 테스트

--- src/test/java/study/SetStudyTest.java
+++ src/test/java/study/SetStudyTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -27,6 +29,20 @@ public class SetStudyTest {
     @DisplayName("Set Size 맞을 경우 테스트")
     void setSizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 4})
+    @DisplayName("contains 테스트")
+    void containsTest(Integer input) {
+        assertThat(numbers).contains(input);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 4})
+    @DisplayName("contains 테스트")
+    void setContainsTest(Integer input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 
 }
