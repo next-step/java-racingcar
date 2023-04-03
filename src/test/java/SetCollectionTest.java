865c606cd6a96ee1cadf64f7b8b2a0d275daea7c
dkswnkk <jjoy9810@gmail.com>
2023-04-03 23:48:02 +0900

    feat: Set Collection 요구사항 1 구현

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Set Collection에 대한 학습 테스트")
+public class SetCollectionTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size()를 통해 Set의 크기를 확인할 수 있다.")
+    void size() {
+        assertEquals(3, numbers.size());
+    }
+}
