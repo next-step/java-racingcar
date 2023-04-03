3741d166ff652bc10f51eda621f276544f809a09
Gangbean <xbox2537@gmail.com>
2023-04-03 16:47:29 +0900

    String class test impl#1

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,72 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("2개 이상 요소 split 정상 테스트")
+    void splitMultipleElementsTest() {
+        // given
+        String input = "1,2";
+
+        // when
+        String[] splitInputs = input.split(",");
+
+        // then
+        assertThat(splitInputs).contains("1").contains("2");
+    }
+
+    @Test
+    @DisplayName("1개 요소 split 정상 테스트")
+    void splitSingleElementTest() {
+        // given
+        String input = "1,";
+
+        // when
+        String[] splitInputs = input.split(",");
+
+        // then
+        assertThat(splitInputs).containsOnly("1");
+    }
+
+    @Test
+    @DisplayName("substring 정상 테스트")
+    void substringTest() {
+        // given
+        String input = "(1,2)";
+
+        // when
+        String output = input.substring(1, input.length()-1);
+
+        // then
+        assertThat(output).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 정상 테스트")
+    void charAtTest() {
+        // given
+        String input = "abc";
+
+        // when
+        char output = input.charAt(0);
+
+        // then
+        assertThat(output).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("charAt 인덱스 범위 초과 exception 테스트")
+    void charAtStringIndexOutOfBoundsExceptionTest() {
+        // given
+        String input = "abc";
+
+        // then
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> input.charAt(input.length())
+        ).withMessageMatching("String index out of range: \\d+");
+    }
+}
