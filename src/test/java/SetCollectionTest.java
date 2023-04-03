8f7f99e22d8311bbf0bbd8aad16a4867bb3fcb9a
dkswnkk <jjoy9810@gmail.com>
2023-04-03 23:54:06 +0900

    feat: Set Collection 요구사항 2 구현

--- src/test/java/SetCollectionTest.java
+++ src/test/java/SetCollectionTest.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Set Collection에 대한 학습 테스트")
 public class SetCollectionTest {
@@ -25,5 +28,12 @@ public class SetCollectionTest {
     @DisplayName("size()를 통해 Set의 크기를 확인할 수 있다.")
     void size() {
         assertEquals(3, numbers.size());
+    }
+
+    @ParameterizedTest(name = "{displayName} {index}이 존재 하는지 확인")
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains()를 통해 Set에 값이 존재하는지 확인할 수 있다.")
+    void contains_01(int value) {
+        assertTrue(numbers.contains(value));
     }
 }
