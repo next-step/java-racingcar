8291727086e42498589fc864ec8e20dd72de37c4
highjune <highjune37@gmail.com>
2023-04-03 21:46:17 +0900

    앞선 테스드 2개에도 @DisplayName 적용하기

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Test;
 public class StringTest {
 
     @Test
+    @DisplayName("String을 split 했을시 배열 검증")
     void split() {
         String[] result = "1,2".split(",");
         Assertions.assertThat(result).containsExactly("1", "2");
     }
 
     @Test
+    @DisplayName("스트링에서 특정 문자 제외시켜서 밚환하기")
     void subString() {
         String problem = "(1, 2)";
         int openBracket = problem.indexOf('(');
@@ -25,8 +27,16 @@ public class StringTest {
     @DisplayName("유효하지 않은 위치의 스트링 값을 가져올 때 에러가 발생할 것이다.")
     void stringCharAt() {
         String problem = "abc";
+
+        // #1
         Assertions.assertThatThrownBy(() -> problem.charAt(problem.length()))
-                .isInstanceOf(StringIndexOutOfBoundsException.class);
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+
+        // #2
+        Assertions.assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> problem.charAt(problem.length()))
+                .withMessageMatching("String index out of range: \\d");
     }
 
 }
