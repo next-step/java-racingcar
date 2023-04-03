48ec90dc805181860cb5524a40babd698a03699c
lily-yu <lily.yu@kakaocorp.com>
2023-04-03 22:46:57 +0900

    Set Collection 테스트 - 요구사항 1

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 크기를 확인할 수 있어야 한다.")
+    void testGetSizeOfSet(){
+        Integer result = numbers.size();
+
+        assertEquals(3, result);
+    }
+}
