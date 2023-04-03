ff7930d889636032234597b42b5bf8e35a05d365
lily-yu <lily.yu@kakaocorp.com>
2023-04-03 22:35:51 +0900

    String 테스트 - 요구사항 2

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringTest {
 
@@ -11,5 +12,14 @@ public class StringTest {
         String[] result = input.split(",");
 
         assertThat(result).contains("1", "2", "3");
+    }
+
+    @Test
+    void testExtractStringFromBracket() {
+        String input = "(1,2,3)";
+
+        String result = input.substring(1, input.length() - 1);
+
+        assertEquals("1,2,3", result);
     }
 }
