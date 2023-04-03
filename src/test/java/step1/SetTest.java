995a002a6188fead68b66a9043a17e069dd662d9
cohi <tkfka156@gmail.com>
2023-04-03 20:21:47 +0900

    test: Set test 요구사항 2, 3 구현

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -4,10 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
 import java.util.Set;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SetTest {
 
@@ -28,6 +30,17 @@ class SetTest {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void test2(int input) throws Exception {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void test3(int value, boolean expected) throws Exception {
+        assertThat(numbers.contains(value)).isEqualTo(expected);
+    }
 
 
 }
