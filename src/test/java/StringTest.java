b480376686adb7ead84d2bc0ed7c4f6c053ef822
juhyun <zzang9ha@naver.com>
2023-04-04 00:11:19 +0900

    test: String 학습 테스트 요구사항 1 추가

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("String 클래스 학습 테스트")
+public class StringTest {
+
+    @Test
+    void String의_split_메서드로_콤마의_문자열을_정상적으로_분리한다() {
+        final var word = "1,2";
+
+        final var actual = word.split(",");
+
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void String의_split_메서드로_한개의_문자를_분리하면_동일한_문자를_반환한다() {
+        final var word = "1";
+
+        final var actual = word.split(",");
+
+        assertThat(actual).containsExactly("1");
+    }
+
+}
