093680d7997a4902da26aabcfc86057df4c60002
lily-yu <lily.yu@kakaocorp.com>
2023-04-03 22:57:49 +0900

    Set Collection 테스트 - 요구사항 2

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -21,9 +24,16 @@ public class SetTest {
 
     @Test
     @DisplayName("Set의 크기를 확인할 수 있어야 한다.")
-    void testGetSizeOfSet(){
+    void testGetSizeOfSet() {
         Integer result = numbers.size();
 
         assertEquals(3, result);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set 안에 특정 값이 존재하는지 확인할 수 있어야 한다.")
+    @ValueSource(ints = {1, 2, 3})
+    void testFindValueInSet(int input) {
+        assertTrue(numbers.contains(input));
     }
 }
