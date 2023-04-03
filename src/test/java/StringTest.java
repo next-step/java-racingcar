273ee521a1dc771204a8c9d94ce5abfe72e00d24
munjeongho <96mjh00@gmail.com>
2023-04-03 21:14:40 +0900

    split sutdy test

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,13 @@
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+	@Test
+	void split()
+	{
+		String[] result = "1,2".split(",");
+		assertThat(result).containsExactly("1","2");
+	}
+}
