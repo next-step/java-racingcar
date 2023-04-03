139496c0e7686bb6955f7844d7250d225207a8f5
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-03 23:54:49 +0900

    요구사항2_(1,2) 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환한다

--- src/test/java/step1/StringClassStudy.java
+++ src/test/java/step1/StringClassStudy.java
@@ -1,5 +1,6 @@
 package step1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -47,6 +48,36 @@ public class StringClassStudy {
                 assertThat(result).containsExactly(SHOULD_RETURN_ARRAY);
             }
         }
+    }
+
+    @Nested
+    @DisplayName("[요구사항2] 쉼표로 구분된 소괄호안 문자열")
+    class Describe_StringInCommaSeparatedBrackets {
+
+        private final static String SHOULD_REMOVE_SPECIAL_CHARACTERS = "[/(|/)]";
+        // apache lang StringUtils 클래스가 없어 대체합니다
+        private final static String EMPTY_STRING = "";
+
+        @Nested
+        @DisplayName("(1,2)이 주어진다면")
+        class Context_with_ONE_DELIMITER_TWO_String {
+
+            private final static String GIVEN_STRING = "(1,2)";
+            private final static String SHOULD_RETURN_STRING = "1,2";
+
+            @Test
+            @DisplayName("1,2을 반환한다")
+            void returnStringArray() {
+                String resultWithReg = GIVEN_STRING.replaceAll(SHOULD_REMOVE_SPECIAL_CHARACTERS, EMPTY_STRING);
+                String resultWithSubString = GIVEN_STRING.substring(1, 4);
+
+                Assertions.assertAll(
+                        () -> assertThat(resultWithReg).isEqualTo(SHOULD_RETURN_STRING),
+                        () -> assertThat(resultWithSubString).isEqualTo(SHOULD_RETURN_STRING)
+                );
+            }
+        }
+
     }
 
 }
