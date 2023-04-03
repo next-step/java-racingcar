87456b666eefe83411852b22a7fb02e6ff75758d
highjune <highjune37@gmail.com>
2023-04-03 21:09:39 +0900

    subString study test 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,13 @@ public class StringTest {
         String[] result = "1,2".split(",");
         Assertions.assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    void subString() {
+        String problem = "(1, 2)";
+        int openBracket = problem.indexOf('(');
+        int closeBracket = problem.indexOf(')');
+        String resultStr = problem.substring(openBracket + 1, closeBracket);
+        Assertions.assertThat(resultStr).isEqualTo("1, 2");
+    }
 }
