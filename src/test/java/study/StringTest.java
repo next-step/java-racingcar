ebf8a9c11878ec361f7d7fe7d14130dbbf5e7cf8
highjune <highjune37@gmail.com>
2023-04-03 21:44:42 +0900

    유효하지 않은 위치의 스트링 값에 접근할 시 에러 발생

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -19,4 +20,13 @@ public class StringTest {
         String resultStr = problem.substring(openBracket + 1, closeBracket);
         Assertions.assertThat(resultStr).isEqualTo("1, 2");
     }
+
+    @Test
+    @DisplayName("유효하지 않은 위치의 스트링 값을 가져올 때 에러가 발생할 것이다.")
+    void stringCharAt() {
+        String problem = "abc";
+        Assertions.assertThatThrownBy(() -> problem.charAt(problem.length()))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
 }
