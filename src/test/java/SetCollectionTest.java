f3cdd20f6cae6e8d6af53e6afbad23dec2035b27
juhyun <zzang9ha@naver.com>
2023-04-04 00:21:40 +0900

    test: Set Collection 학습 테스트 요구사항 1 추가

--- src/test/java/SetCollectionTest.java
+++ src/test/java/SetCollectionTest.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetCollectionTest {
 
@@ -14,6 +17,11 @@ public class SetCollectionTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Test
+    void Set의_size_메서드는_크기를_반환한다() {
+        assertThat(numbers).hasSize(3);
     }
 
 }
