e323e7ed3d31acceac81bc6f982395e6cac4cca1
player-geun <player_geun@naver.com>
2023-04-03 21:18:51 +0900

    [step 1] StringTest 수정
    요구사항 1에 누락된 부분이 있어서 수정

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
 
     @Test
-    @DisplayName("[요구사항 1] split() 테스트")
-    void split_함수_테스트() {
+    @DisplayName("[요구사항 1] 구분자가 문자열에 있는 경우, split() 테스트")
+    void 구분자_있는_split_함수_테스트() {
         //given
         String line = "1,2";
 
@@ -18,6 +18,19 @@ public class StringTest {
 
         //then
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("[요구사항 1] 구분자가 문자열에 없는 경우, split() 테스트")
+    void 구분자_없는_split_함수_테스트() {
+        //given
+        String line = "1";
+
+        //when
+        String[] result = line.split(",");
+
+        //then
+        assertThat(result).contains("1");
     }
 
     @Test
