ae532cce102691679bb936faad4ec776a38c3fe3
Hwang In-wook <inwook9@gmail.com>
2023-04-05 08:42:41 +0900

    Step1 (#4123)
    * StringTest - 요구사항1
    
    * StringTest - 요구사항2
    
    * StringTest - 요구사항3
    
    * SetTest - 요구사항1
    
    * SetTest - 요구사항2
    
    * SetTest - 요구사항3

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,38 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Set Collection에 대한 학습 테스트")
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드는 Set의 크기를 반환합니다.")
+    void size() {
+        assertThat(numbers.size()).isSameAs(3);
+    }
+
+    @ParameterizedTest(name = "Set의 contains() 메소드는 포함여부를 반환합니다.({0}포함? {1})")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
