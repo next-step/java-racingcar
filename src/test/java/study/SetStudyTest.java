189118c62c05ef8a7c8fdb20fc9fdb4660b431e6
KyeongMin <kyeongmin.jake.lee@gmail.com>
2023-04-04 09:54:42 +0900

    Step1 (#4108)
    * chore: upgrade java8 -> java11, gradle, junit version
    
    * test - comma로 split했을 때 잘 분리되는지 테스트
    
    * test - substring으로 () 제거 테스트
    
    * test - charAt 활용 테스트
    
    * test - size 테스트
    
    * test - contains 테스트
    
    * test - csvSource 활용 테스트
    
    * refactor - ValueSource 활용 시 Integer 타입을 int로 변경
    
    ---------
    
    Co-authored-by: javajigi <javajigi@gmail.com>

---
+++ src/test/java/study/SetStudyTest.java
@@ -0,0 +1,55 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetStudyTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(3);
+        numbers.add(4);
+    }
+
+    @Test
+    @DisplayName("Set Size 맞을 경우 테스트")
+    void setSizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 4})
+    @DisplayName("contains 테스트")
+    void containsTest(int input) {
+        assertThat(numbers).contains(input);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 4})
+    @DisplayName("contains 테스트")
+    void setContainsTest(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "3:true", "4:true", "5:false", "10:false"}, delimiter = ':')
+    @DisplayName("csvSource 활용 테스트")
+    void csvSourceTest(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
+}
---
+++ src/test/java/study/StringStudyTest.java
@@ -0,0 +1,56 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringStudyTest {
+
+    @Test
+    void splitTest() {
+        String commaString = "1,2";
+
+        String[] result = commaString.split(",");
+
+        assertThat(result.length).isEqualTo(2);
+        assertThat(result).containsExactly("1", "2");
+        assertThat(result[0]).isEqualTo("1");
+        assertThat(result[1]).isEqualTo("2");
+    }
+
+    @Test
+    void substringTest() {
+        int position = 1;
+        String parentheses = "(1,2)";
+
+        String result = parentheses.substring(position, parentheses.length()-position);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 확인 테스트")
+    void charAtTest() {
+        String testString = "abc";
+
+        char a = testString.charAt(0);
+        char b = testString.charAt(1);
+        char c = testString.charAt(2);
+
+        assertThat(a).isEqualTo('a');
+        assertThat(b).isEqualTo('b');
+        assertThat(c).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("인덱스에서 벗어날 시 에러 반환 테스트")
+    void charAtErrorTest() {
+        String testString = "abc";
+
+        assertThrows(StringIndexOutOfBoundsException.class,
+                () -> testString.charAt(testString.length()));
+    }
+}
