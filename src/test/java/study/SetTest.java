f85ab9e60d5c6da6fd015ed7b992841ba601fdaa
gette <upwyy123@gmail.com>
2023-04-04 22:22:54 +0900

    subStringTest, charAtTest, setTest

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,45 @@
+package study;
+
+import org.assertj.core.internal.Numbers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void containsNumbers_ShouldReturnTrueForValidInputs(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void inputs_ShouldGenerateTheExpectedBoolean(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,9 +1,10 @@
 package study;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
@@ -12,5 +13,25 @@ public class StringTest {
         assertThat(result).contains("1");
         //순서보장확인
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void subString() {
+        String input = "(1,2)";
+        String result = input.substring(1,4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("인수가 벗어난 값을 호출했을 때 항상 실패한다")
+    void chartAt() {
+        //given
+        String input = "abc";
+        //when
+        Throwable thrown = catchThrowable(() -> {
+            input.charAt(4);
+        });
+        // then
+        assertThat(thrown).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
