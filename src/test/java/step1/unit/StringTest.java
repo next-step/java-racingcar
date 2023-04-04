e9e368fa28fb6eb9912f9ffc185d77721fa763e1
lehdqlsl <lehdqlsl@naver.com>
2023-04-04 16:51:02 +0900

    test: 테스트 케이스 1개로 합체

--- src/test/java/step1/unit/StringTest.java
+++ src/test/java/step1/unit/StringTest.java
@@ -11,21 +11,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
 
-    @ParameterizedTest(name = "Split Test - 개별 요소 확인 (input: ''{0}'')")
+    @ParameterizedTest(name = "Split Test - 순서 및 요소 확인 (input: ''{0}'')")
     @ValueSource(strings = {"1", "1,2", "3,4", "5,6,7"})
     void input_array_check_element(String input) {
         String[] split = input.split(",");
-
-        for (String s : split) {
-            assertThat(split).contains(s);
-        }
-    }
-
-    @ParameterizedTest(name = "Split Test - 배열 순서 확인 (input: ''{0}'')")
-    @ValueSource(strings = {"1", "1,2", "3,4", "5,6,7"})
-    void input_array_check_list(String input) {
-        String[] split = input.split(",");
         List<String> list = List.of(split);
+
+        assertThat(list).contains(split);
         assertThat(list).containsExactly(split);
     }
+
 }
