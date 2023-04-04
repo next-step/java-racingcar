2a4ce9ae26b6abecf2391ce9d6f7ee67933dbd58
Sangwook Hwang <yoonseol1983@gmail.com>
2023-04-05 00:21:09 +0900

    feat:  빈문자열 또는 null 값 입력 시 0 반환 테스트 코드& 메서드 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Calculator {
+    public Integer input(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+        return null;
+    }
+}
