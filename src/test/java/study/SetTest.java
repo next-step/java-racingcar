1e7d71e97ebb184e7c5ed8db9c857b459e968d2b
Jung Sim <tlawjd99gma@gmail.com>
2023-04-05 08:47:34 +0900

    Step1 (#4131)
    * feat : 요구사항 1 기능 구현
     - "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
     - "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
    
    * feat : 요구사항 2 구현
     - "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
    
    * feat : 요구사항 3 구현
     - "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
     - String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
     - JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
    
    * feat : SetTest 생성 및 초기 설정
    
    * feat : 요구사항 1 구현
     - Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.
    
    * feat : 요구사항 2 구현
     - Set의 contains() 메소드를 활용해 값이 존재하는지 확인하는 테스트를 구현한다.
     - ParameterizedTest를 활용해 중복 코드를 제거한다.
    
    * feat : 요구사항 3 구현 및 ParameterizedTest display 변경
     - 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
    
    * refactor : ParameterizedTest 이름 변경
    
    ---------
    
    Co-authored-by: ky-kiske <ky.kiske@kakaocorp.com>

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,49 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size() 메소드를 통해 Set의 크기를 확인할 수 있다.")
+    @Test
+    void size() {
+        //GIVEN
+        //WHEN
+        int size = numbers.size();
+
+        //THEN
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "contains() 메소드를 통해 {index} 값은 존재하는 것을 확인할 수 있다.")
+    @ValueSource(ints = {1, 2, 3})
+    void containsWithInput(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest(name = "contains() 메소드를 통해 {0} 값에 대한 결과는 {1} 인 것을 확인할 수 있다.")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsWithInputAndExpected(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+}
