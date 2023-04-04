4c1d4ead838fef17a43fda5a0a2d64394a2996b7
luminous <mlicp1994@gmail.com>
2023-04-04 15:39:28 +0900

    Step1 (#4100)
    * test[StringTest]: add split test code
    
    * test[StringTest]: add substring test code
    
    * test[StringTest]: add charAt test code
    
    * test[SetCollectionTset]: add size method test code
    
    * test[SetCollectionTset]: add contains method test code used ParameterizedTest
    
    * test[SetCollectionTset]: add contains method test code used CsvSource

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,42 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("SetCollection size method test")
+    void sizeTest() {
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void containsTest(int number){
+        Assertions.assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void containsTestUsedCsvSource(int number, boolean expected){
+        Assertions.assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,40 @@
+import org.assertj.core.api.AbstractThrowableAssert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("String class split test")
+    void splitTest() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1", "2");
+
+        String[] actual1 = "1".split(",");
+        assertThat(actual1).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("String class substring test")
+    void substringTest() {
+        String target = "(1,2)";
+        String actual = target.substring(target.indexOf("(") + 1, target.indexOf(")"));
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("String class charAt method test")
+    void charAtTest() {
+        String target = "abc";
+        char actual = target.charAt(0);
+
+        assertThat(actual).isEqualTo('a');
+
+        assertThatThrownBy(() -> target.charAt(target.length() + 1)).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+    }
+
+}
