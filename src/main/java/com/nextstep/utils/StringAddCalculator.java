efd05d145dafc6b24ec7fb4e93530774903396fb
patrickYoo <yhy1045@naver.com>
2023-04-04 21:31:32 +0900

    쉼표 또는 커스텀 구분자가 있을경우 숫자 배열의 합을 구하는 요구조건 구현 완료

--- src/main/java/com/nextstep/utils/StringAddCalculator.java
+++ src/main/java/com/nextstep/utils/StringAddCalculator.java
@@ -1,14 +1,38 @@
 package com.nextstep.utils;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
-    public static Integer splitAndSum(String text) {
-        if(isEmptyText(text)) return 0;
+    private static final String CUSTOM_SEPARATOR_REGEX = "//(.)\n(.*)";
+    private static final String TEXT_SEPARATOR = ",|:";
 
-        return -1;
+    public static int splitAndSum(String text) {
+        if(isEmptyText(text)) return 0;
+        int [] integers = convertStringIntoArray(text);
+        return Arrays.stream(integers).sum();
     }
 
     private static boolean isEmptyText(String text) {
         return text == null || text.isEmpty();
     }
+
+    private static int[] convertStringIntoArray(String text) {
+        Matcher m = Pattern.compile(CUSTOM_SEPARATOR_REGEX).matcher(text);
+        if(m.find()) {
+            return Arrays
+                    .stream(m.group(2)
+                    .split(m.group(1)))
+                    .mapToInt(Integer::parseInt).toArray();
+        }
+
+        return Arrays
+                .stream(text.split(TEXT_SEPARATOR))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
 }
--- src/test/java/racingcar/step2/StringAddCalculatorTest.java
+++ src/test/java/racingcar/step2/StringAddCalculatorTest.java
@@ -3,7 +3,10 @@ package racingcar.step2;
 import com.nextstep.utils.StringAddCalculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.nextstep.utils.StringAddCalculator.splitAndSum;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class StringAddCalculatorTest {
@@ -11,10 +14,38 @@ class StringAddCalculatorTest {
     @Test
     @DisplayName("빈문자열 또는 null 값을 입력할 경우 0을 반환한다")
     void splitAndSum_null_or_emptyText() {
-        int result = StringAddCalculator.splitAndSum(null);
+        int result = splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
-        result = StringAddCalculator.splitAndSum("");
+        result = splitAndSum("");
         assertThat(result).isEqualTo(0);
+    }
+
+    @ParameterizedTest(name = "[{index}] {0} 의 정수 결과는 {0} 이다")
+    @ValueSource(strings = {"1","2","3"})
+    public void splitAndSum_single_number_str(String valueSource) {
+        int result = splitAndSum(valueSource);
+        assertThat(result).isEqualTo(Integer.parseInt(valueSource));
+    }
+
+    @Test
+    @DisplayName("쉼표 구분자 입력 두 숫자의 합을 반환한다")
+    public void splitAndSum_with_comma() throws Exception {
+        int result = splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("쉼표 구분자, 콜론 구분자 복합 입력일 경우 숫자의 합을 반환한다")
+    public void splitAndSum_comma_or_colon_complex_separator() throws Exception {
+        int result = splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자를 지정하였을 경우 숫자의 합을 반환한다")
+    public void splitAndSum_custom_separator() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
     }
 }
