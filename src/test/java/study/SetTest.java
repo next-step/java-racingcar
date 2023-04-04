2c77342d3c90b870ace4b705caff2eb660c0674c
Highjune <highjune37@gmail.com>
2023-04-04 10:43:03 +0900

    split study test (#4094)
    * split study test
    
    * subString study test 완료
    
    * 유효하지 않은 위치의 스트링 값에 접근할 시 에러 발생
    
    * 앞선 테스드 2개에도 @DisplayName 적용하기
    
    * Set 테스트 클래스 생성, 요구사항1 완성
    
    * set 요구사항 2 완성
    Set 요소들 확인하기 : 단순, @ViewSource, @CsvSource
    
    * 요구사항 3 마무리 : set 안에 포함된 요소 확인하기

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,70 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 Size() 메소드를 활용해 크기를 확인하는 학습테스트를 구현한다.")
+    void setSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Set 안에 포함된 요소들 확인하기 : 단순")
+    void containsV1() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set 안에 포함된 요소들 확인하기 : @ViewSource ")
+    @ValueSource(ints = {1, 2, 3})
+    void containsV2(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set 안에 포함된 요소들 확인하기 : @CsvSource ")
+    @CsvSource(value = {"1:true", "2:true", "3:true"}, delimiter = ':')
+    void containsV3(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set 안에 포함된 요소들 확인하기 : @CsvSource(true, false 둘 다 사용해보기)")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsV4(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set 안에 포함된 요소들 확인하기(예제) : @CsvSource(true, false 둘 다 사용해보기)")
+    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
+    void containsV4_1(String input, String expected) {
+        String actual = input.toLowerCase();
+        assertThat(actual).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,44 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("String을 split 했을시 배열 검증")
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("스트링에서 특정 문자 제외시켜서 밚환하기")
+    void subString() {
+        String problem = "(1, 2)";
+        int openBracket = problem.indexOf('(');
+        int closeBracket = problem.indexOf(')');
+        String resultStr = problem.substring(openBracket + 1, closeBracket);
+        assertThat(resultStr).isEqualTo("1, 2");
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 위치의 스트링 값을 가져올 때 에러가 발생할 것이다.")
+    void stringCharAt() {
+        String problem = "abc";
+
+        // #1
+        assertThatThrownBy(() -> problem.charAt(problem.length()))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+
+        // #2
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> problem.charAt(problem.length()))
+                .withMessageMatching("String index out of range: \\d");
+    }
+
+}
