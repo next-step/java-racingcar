3e2b422d060e48eeaa88369fb9576125cae56fdc
patrickYoo <yhy1045@naver.com>
2023-04-04 16:10:57 +0900

    Step1 학습 테스트 실습 요구사항 구현 코드 커밋

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,53 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 중복값이 제거된 사이즈를 반환 한다")
+    void setSize(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("contains 1, 2, 3 값을 포함한다")
+    void containsHardCodedTest() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest(name = "[{index}] {0} 는 Set 함수에 포함한다")
+    @ValueSource(ints = {1,2,3})
+    void containsTestWithTrueCases(int valueSource) {
+        assertThat(numbers.contains(valueSource)).isTrue();
+    }
+
+    @ParameterizedTest(name = "[{index}] {0} 는 Set 함수에 포함된다 : {1}")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    void containsTestWithAllCases(int valueSource, boolean expected) {
+        assertThat(numbers.contains(valueSource)).isEqualTo(expected);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,15 +1,43 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class StringTest {
 
     @Test
-    void splitTest() {
-        String [] result = "1,2".split(",");
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2 를 분리한다")
+    void splitTestStep1() {
+        final String [] result = "1,2".split(",");
+        assertThat(result).contains("1","2");
         assertThat(result).containsExactly("1","2");
-        
     }
+
+    @Test
+    @DisplayName("\"1\"을 ,로 split 1만을 포함하는 배열을 반환한다")
+    void splitTestStep2() {
+        final String [] result = "1".split(",");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    @DisplayName("\"(1,2)\" 값 ()을 제거하고 \"1,2\"를 반환 한다")
+    void substringTest() {
+        final String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("String 배열 범위를 벗어난 index 시 예외를 발생한다.")
+    void strWithIndexOutOfBoundsTest() {
+        final String testInitValue = "abc";
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> testInitValue.charAt(testInitValue.length()))
+                .withMessageContaining("index out of range")
+                .withMessageMatching("String index out of range: \\d+");
+    }
+
 }
