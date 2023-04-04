3f8209fec7a62902c5ddf5bab595e04c1d0a3b7f
SMJminjeong <sdwp0920@gmail.com>
2023-04-05 07:49:44 +0900

    Step1 - String test (#4141)
    * String.split() study test
    
    * String.substring() study test
    
    * String.charAt() study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,7 +3,8 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+
 
 public class StringTest {
 
@@ -23,6 +24,48 @@ public class StringTest {
 
         assertThat(result2).contains("1");
         assertThat(result2).containsExactly("1");
+    }
+
+    /**
+     * 요구사항 2
+     * "(1,2)" 값이 주어졌을 때 String.substring() 메소드를 이용하여 "1,2" 반환
+     */
+    @Test
+    @DisplayName("String.subString() test")
+    void subString() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    /**
+     * 요구사항 3
+     * "abc" 값이 주어졌을때 String.charAt() 메소드를 활용해 특정 위치 문자 가져오기
+     */
+    @Test
+    @DisplayName("String.charAt() test")
+    void charAt() {
+        String result = "abc";
+        char charResult = result.charAt(1);
+
+        assertThat(charResult).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("charAt()에서 assertThatThrownBy()을 사용하여 Exception처리")
+    void assertThatThrownByTest() {
+        String result = "abc";
+        assertThatThrownBy(() -> {
+            result.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("String index out of range: 3");
+    }
+
+    @Test
+    @DisplayName("charAt()에서 assertThatExceptionOfType()을 사용하여 Exception처리")
+    void assertThatExceptionOfTypeTest() {
+        String result = "abc";
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {result.charAt(3);})
+                .withMessageContaining("index out of")
+                .withNoCause();
     }
 
 }
