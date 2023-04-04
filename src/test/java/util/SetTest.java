d1f121994656cefc0c0e707f454b569d53beb9ba
biblekimm <biblekimm931@gmail.com>
2023-04-04 21:37:11 +0900

    1단계- 학습테스트 실습

---
+++ src/test/java/util/SetTest.java
@@ -0,0 +1,36 @@
+package util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void setSizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void setContainsTest(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+}
