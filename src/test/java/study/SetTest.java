015254c157e8387e41a5377b772adf8fd2293619
seominah <seominah95@gmail.com>
2023-04-05 00:16:52 +0900

    apply feedback

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,5 @@
 package study;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +11,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -30,21 +28,23 @@ public class SetTest {
     @DisplayName("Set의 크기 확인하는 테스트")
     void sizeOfSet() {
         int result = numbers.size();
-        assertThat(result).isEqualTo(3);
+//        assertThat(result).isEqualTo(3);
+        assertThat(numbers).hasSize(3);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("Set에 특정 값이 존재하는지 확인하는 테스트, ParameterizedTest 사용")
     void contains(int number) {
-        assertThat(numbers.contains(number)).isTrue();
+//        assertThat(numbers.contains(number)).isTrue();
+        assertThat(numbers).contains(number);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
     @DisplayName("Set에 특정 값이 존재하는지 결과를 boolean으로 반환하는 테스트, CsvSource 사용")
-    void isContains(String input, String expected) {
-        boolean actualValue = numbers.contains(Integer.valueOf(input));
-        assertThat(String.valueOf(actualValue)).isEqualTo(expected);
+    void isContains(int input, boolean expected) {
+        boolean actualValue = numbers.contains(input);
+        assertThat(actualValue).isEqualTo(expected);
     }
 }
