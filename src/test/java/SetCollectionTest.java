a749cc03e6165321c263ef33789ec60d3b278f57
LeeJuHyun <50076031+JuHyun419@users.noreply.github.com>
2023-04-04 16:45:57 +0900

    1단계 학습 테스트 실습 (#4124)
    * test: String 학습 테스트 요구사항 1 추가
    
    * test: String 학습 테스트 요구사항 2 추가
    
    * test: String 학습 테스트 요구사항 3 추가
    
    * test: Set Collection 기본 데이터 추가
    
    * test: Set Collection 학습 테스트 요구사항 1 추가
    
    * test: Set Collection 학습 테스트 요구사항 2 추가
    
    * test: Set Collection 학습 테스트 요구사항 3 추가

---
+++ src/test/java/SetCollectionTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetCollectionTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void Set의_size_메서드는_크기를_반환한다() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void Set의_contains_메서드는_값이_존재하는지_확인한다(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void Set의_contains_메서드는_값이_존재하면_true를_존재하지않으면_false를_반환한다(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("String 클래스 학습 테스트")
+public class StringTest {
+
+    @Test
+    void String의_split_메서드로_콤마의_문자열을_정상적으로_분리한다() {
+        final var word = "1,2";
+
+        final var actual = word.split(",");
+
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void String의_split_메서드로_한개의_문자를_분리하면_동일한_문자를_반환한다() {
+        final var word = "1";
+
+        final var actual = word.split(",");
+
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void String의_substring_메서드로_소괄호를_제거한다() {
+        final var word = "(1,2)";
+
+        final var actual = word.substring(1, word.length() - 1);
+
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("String의 charAt() 메서드는 특정 위치의 문자를 가져온다.")
+    void String의_charAt_메서드는_특정_위치의_문자를_반환한다() {
+        final var word = "abc";
+
+        final var actual = word.charAt(1);
+
+        assertThat(actual).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("String의 charAt() 메서드에서 문자열의 위치값을 벗어나면 StringIndexOutOfBoundsException 예외가 발생한다.")
+    void String의_charAt_메서드에서_특정_위치값을_벗어나면_예외가_발생한다() {
+        final var word = "abc";
+
+        assertThatThrownBy(
+                () -> word.charAt(10)
+        ).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+}
