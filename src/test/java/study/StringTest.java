3056d790d3228a4933b82761afc2034555f31d3b
choitaehoon <hugebigdream@gmail.com>
2023-04-04 22:41:32 +0900

    Request 3: StringIndexOutOfBoundsException Test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -15,6 +18,14 @@ public class StringTest {
     void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 문자를 가져올때 위치 값을 벗어나면 StringIndexOutOfBoundsException 이 발생.")
+    void charAt() {
+        String givenValue = "abc";
+        assertThatThrownBy(() -> givenValue.charAt(15)).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range");
     }
 
 }
