6a54cbf021e40be75ab3c36e3d18f690399b95ee
hoa0217 <gjwjdghk123@naver.com>
2023-04-05 00:22:30 +0900

    feat: "Set 요구사항 2"
    - Set의 contains를 사용하여 해당 값의 존재를 확인한다.
    - 중복 코드를 제거하기 위해 JUnit의 ParameterizedTest사용한다.
      - 이 때 int배열을 인자로 전달하므로 `@ValueSource`를 사용한다.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -21,9 +23,16 @@ public class SetTest {
     numbers.add(3);
   }
 
-  @DisplayName("size을 사용하여 Set의 크기를 확인한다.")
+  @DisplayName("size를 사용하여 Set의 크기를 확인한다.")
   @Test
   void requirement1(){
     assertThat(numbers.size()).isEqualTo(3);
+  }
+
+  @DisplayName("contains를 사용하여 해당 값의 존재를 확인한다.")
+  @ParameterizedTest
+  @ValueSource(ints = {1,2,3})
+  void requirement2(int input){
+    assertThat(numbers.contains(input)).isTrue();
   }
 }
