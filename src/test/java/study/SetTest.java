26e3a51a37e7fab5f3959e3a17b1f3a3a08e5ef0
hoa0217 <gjwjdghk123@naver.com>
2023-04-05 00:16:36 +0900

    feat: "Set 요구사항 1"
    - Set의 size를 사용하여 Set의 크기를 확인한다.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,8 +1,12 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class SetTest {
 
@@ -15,5 +19,11 @@ public class SetTest {
     numbers.add(1);
     numbers.add(2);
     numbers.add(3);
+  }
+
+  @DisplayName("size을 사용하여 Set의 크기를 확인한다.")
+  @Test
+  void requirement1(){
+    assertThat(numbers.size()).isEqualTo(3);
   }
 }
