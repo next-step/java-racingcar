10010a11fd2c23322e1ec3aa63ac9749073ec69f
dosaem <dosaem90@gmail.com>
2023-04-05 02:06:54 +0900

    fix: display name 추가, charAtTest CsvSource사용

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,9 @@ package study;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -9,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class StringTest {
 
     @Test
+    @DisplayName("split 했을 때 1과 2로 잘 분리되는지, 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트")
     void splitTest() {
         // given
         String containComma = "1,2";
@@ -24,6 +28,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하는 테스트")
     void substringTest() {
         // given
         String originalStr = "(1,2)";
@@ -35,18 +40,14 @@ public class StringTest {
         assertThat(result).isEqualTo("1,2");
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"0:a", "1:b", "2:c"}, delimiter = ':')
     @DisplayName("charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트")
-    void charAtTest() {
+    void charAtTest(int number, char string) {
         // given
         String originalStr = "abc";
-        char[] originalArr = originalStr.toCharArray();
-
         // when & then
-        for (int i = 0; i < originalStr.length(); i++) {
-            assertThat(originalStr.charAt(i)).isEqualTo(originalArr[i]);
-        }
-
+        assertThat(originalStr.charAt(number)).isEqualTo(string);
     }
 
     @Test
@@ -60,7 +61,5 @@ public class StringTest {
                 () -> originalStr.charAt(originalStr.length()))
                 .isInstanceOf(IndexOutOfBoundsException.class)
                 .hasMessageContaining("String index out of range: " + originalStr.length());
-
-
     }
 }
