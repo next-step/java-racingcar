d984ff5803570bf12f863b7444a862ff97c06440
juno-junho <ssmm0205@naver.com>
2023-04-04 21:42:50 +0900

    feat(study): set test 요구사항 3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -41,5 +42,12 @@ public class SetTest {
     @ValueSource(ints = {1,2,3})
     void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("값이 set에 존재하는지 테스트")
+    @ParameterizedTest(name = "값: {0}")
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"}, delimiter = ',')
+    void contains(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
     }
 }
