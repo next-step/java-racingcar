6002c7a43ac0ede44cbd49886f0b14b11ff6c3ec
tataki26 <tataki26@gmail.com>
2023-04-04 22:53:15 +0900

    [자동차 경주] 1단계 - String 클래스 학습 테스트 3
    요구 사항 3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,9 +1,11 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -23,5 +25,27 @@ public class StringTest {
         // 2
         String result = "(1,2)".substring(1,4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    // 3-3
+    @DisplayName("charAt result test")
+    void charAtV1() {
+        // 3-1
+        char result = "abc".charAt(1);
+        assertThat(result).isEqualTo('b');
+    }
+
+    @Test
+    // 3-3
+    @DisplayName("charAt index out of bounds exception test")
+    void charAtV2() {
+        // 3-2
+        // char data = "abc".charAt(3);
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        })
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range");
     }
 }
