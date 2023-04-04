466aac68b8689c3f601b5c4c8b286d00031c0f0e
juno-junho <ssmm0205@naver.com>
2023-04-04 19:31:34 +0900

    test(study): 요구사항 1 구현
    하나의 값만 있을 경우 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -20,4 +20,18 @@ public class StringTest {
         assertThat(splitString).contains("1", "2");
         assertThat(splitString).containsExactly("1", "2");
     }
+
+    @Test
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환 되는지 확인")
+    void commaSeparatedStringWithOneInput() {
+        // given
+        String givenString = "1";
+
+        // when
+        String[] splitString = givenString.split(",");
+
+        // then
+        assertThat(splitString).contains("1");
+        assertThat(splitString).containsExactly("1");
+    }
 }
