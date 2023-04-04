aae3d8cd43c4434da3b8ef7d4fb976b4481f8d10
yong.kwon <yong.kwno@kakaocorp.com>
2023-04-04 19:31:01 +0900

    [Step-1] [Test] 학습테스트 실습 요구사항 구현 완료

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 메소드는 중복 원소가 제외된 길이를 반환한다")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains()는 포함된 원소를 확인하고 없으면 true를 반환한다.")
+    void contains(int num) {
+        assertThat(numbers.contains(num)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false","5:false"}, delimiter = ':')
+    @DisplayName("contains()는 포함된 원소를 확인하고 없으면 true 혹은 false를 반환한다.")
+    void contains(int num, boolean expected) {
+        assertThat(numbers.contains(num)).isEqualTo(expected);
+    }
+}
