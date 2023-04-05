0c4e0150417661ceb108a4fde68c8d83fa57edda
choitaehoon1998 <51841238+choitaehoon1998@users.noreply.github.com>
2023-04-05 20:44:21 +0900

    1단계 학습 테스트 (#4158)
    * Request 1: split
    
    * Request 2: substring
    
    * Request 3: StringIndexOutOfBoundsException Test
    
    * Request 1: 1만을 포함하는 배열이 반환되는지에 대한 내용 추가.
    
    * Set Collection에 대한 학습 테스트
    
    * 메소드 명 변경

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,33 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 문자를 가져올때 위치 값을 벗어나면 StringIndexOutOfBoundsException 이 발생.")
+    void charAt() {
+        String givenValue = "abc";
+        assertThatThrownBy(() -> givenValue.charAt(15)).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range");
+    }
+
+}
