ea49d8a3bb896c65c1c8c635985afa9a33daaeaa
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-05 21:17:59 +0900

    계산기 실행하는 객체 구현

--- src/main/java/step2/StringCalculateExecutor.java
+++ src/main/java/step2/StringCalculateExecutor.java
@@ -1,13 +1,24 @@
 package step2;
 
+import java.util.List;
+
 public class StringCalculateExecutor {
 
     private static class CalculatorInstanceLazyHolder {
         private static final StringCalculateExecutor INSTANCE = new StringCalculateExecutor();
     }
 
-    public static CalculatorInstanceLazyHolder getInstance() {
-        return StringCalculateExecutor.getInstance();
+    public static StringCalculateExecutor getInstance() {
+        return CalculatorInstanceLazyHolder.INSTANCE;
+    }
+
+    public int execute(String input) {
+
+        List<String> inputToInputList = StringCalculateSeparator.getInstance().executeSeparator(input);
+
+        StringCalculateValidator.getInstance().executeValidator(inputToInputList);
+
+        return StringCalculateCalculator.getInstance().executeCalculator(inputToInputList);
     }
 
 
