aab07d980b4405a83ff60fe7e52bc3418a397739
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-06 00:49:28 +0900

    StringCalulator 리팩토링

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -15,7 +15,7 @@ class StringCalculatorTest {
 
     @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우, 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
     @Test
-    void calculate() {
+    void splitAndSum() {
         String str = "1,2;3";
         assertThat(StringCalculator.splitAndSum(str))
             .isEqualTo(6);
@@ -23,7 +23,7 @@ class StringCalculatorTest {
 
     @DisplayName("기본 구분자 외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 \"//\"와 \"\\n\" 사이에 위치하는 문자를 커스텀 구분자로 사용한다.")
     @Test
-    void customDelimiter() {
+    void set_customDelimiter() {
         String str = "//.\n1.23.4";
         assertThat(StringCalculator.splitAndSum(str))
             .isEqualTo(28);
