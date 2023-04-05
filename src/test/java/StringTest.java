0b10ac6029adf795b230c9cdb2748c4234a0ecab
조은호 <jo-eunho@jo-eunhoui-MacBookAir.local>
2023-04-05 13:42:20 +0900

    chore: 주석추가 및 @ParameterizedTest 활용

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
 public class StringTest {
     @Test
@@ -16,8 +18,13 @@ public class StringTest {
     @Test
     @DisplayName("substring() 메서드 확인")
     void substring(){
+        //given
         String numberString = "(1,2)";
+
+        //when
         String[] result = numberString.substring(1, numberString.length() - 1).split(",");
+
+        //then
         assertThat(result).containsExactly("1","2");
     }
 
@@ -25,21 +32,23 @@ public class StringTest {
     @Test
     @DisplayName("charAt() 특정위치의 문자 확인")
     void charAt(){
+        //given
         String value = "abc";
         int index = 2;
 
+        //when
         char actual = value.charAt(index);
+
+        //then
         assertThat(actual).isEqualTo('c');
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"abc,3", "abcd,4"})
     @DisplayName("charAt() IndexOutOfBoundsException")
-    void charAt_IndexOutOfBoundsException(){
-        String value = "abc";
-        int index = 3;
-
+    void charAt_IndexOutOfBoundsException(String value,int index){
         assertThatThrownBy(() -> {
-            value.charAt(index);
-        }).isInstanceOf(IndexOutOfBoundsException.class);
+            value.charAt(index);//when
+        }).isInstanceOf(IndexOutOfBoundsException.class);//then
     }
 }
