a8c34bfa0970ce06bc2c05dd3e9358443dfe7070
SeokyungKim <kskyung0624@gmail.com>
2023-04-06 01:52:31 +0900

    refactor : StringAdditionCalculator가 가지던 문자열 분할 책임을 AdditionalString으로 옮긴다

--- src/main/java/calculator/StringAdditionCalculator.java
+++ src/main/java/calculator/StringAdditionCalculator.java
@@ -1,42 +1,16 @@
 package calculator;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class StringAdditionCalculator {
 
-  private static final String DEFAULT_DELIMITER = ",|:";
-
-  private final String[] input;
-
-  public StringAdditionCalculator(final String input) {
-    this.input = parseInput(input);
-  }
-
-  public Integer sum() {
-    return Arrays.stream(input)
+  public Integer sum(final AdditionalString input) {
+    return Arrays.stream(input.split())
         .map(this::parsePositiveInteger)
         .reduce(0, Integer::sum);
   }
 
-  private String[] parseInput(String input) {
-    if (input == null || input.isEmpty()) {
-      return new String[]{};
-    }
-
-    Matcher matcher = Pattern.compile("//(.)\\n(.*)").matcher(input);
-
-    String delimiter = DEFAULT_DELIMITER;
-    if(matcher.find()) {
-      delimiter += "|" + matcher.group(1);
-      input = matcher.group(2);
-    }
-
-    return input.split(delimiter);
-  }
-
-  private Integer parsePositiveInteger(String input) {
+  private Integer parsePositiveInteger(final String input) {
     int number;
 
     try {
--- src/test/java/calculator/StringAdditionCalculatorTest.java
+++ src/test/java/calculator/StringAdditionCalculatorTest.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,46 +11,47 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringAdditionCalculatorTest {
 
+  StringAdditionCalculator calculator = new StringAdditionCalculator();
+
   @ParameterizedTest
   @CsvSource(value = {"|0", "1,2|3", "1,2,3|6", "1,2:3|6"}, delimiter = '|')
   @DisplayName("입력받은 문자열의 각 숫자의 합을 반환한다")
   void sumTest(String input, int expected) {
-    // given
-    StringAdditionCalculator calculator = new StringAdditionCalculator(input);
-
-    // when & then
-    Assertions.assertThat(calculator.sum()).isEqualTo(expected);
+    assertThat(
+        calculator.sum(
+            new AdditionalString(input)
+        ))
+        .isEqualTo(expected);
   }
 
   @Test
   @DisplayName("빈 문자열을 입력받은 경우 0을 반환한다")
   void sumEmptyTest() {
-    // given
-    StringAdditionCalculator calculator = new StringAdditionCalculator("");
-
-    // when & then
-    Assertions.assertThat(calculator.sum()).isEqualTo(0);
+    assertThat(
+        calculator.sum(
+            new AdditionalString("")
+        ))
+        .isEqualTo(0);
   }
 
   @Test
   @DisplayName("커스텀 구분자를 지정한 문자열의 각 숫자의 합을 반환한다")
   void sumCustomDelimiterTest() {
-    // given
-    StringAdditionCalculator calculator = new StringAdditionCalculator("//;\n1;2,3:4");
-
     // when & then
-    Assertions.assertThat(calculator.sum()).isEqualTo(10);
+    assertThat(
+        calculator.sum(
+            new AdditionalString("//;\n1;2,3:4")
+        )).isEqualTo(10);
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"a;2;3", "-1;2;3"})
   @DisplayName("숫자 이외의 값 또는 음수를 전달할 경우 RuntimeException 예외를 발생시킨다")
   void sumExceptionTest(String input) {
-    // given
-    StringAdditionCalculator calculator = new StringAdditionCalculator(input);
-
-    // when & then
-    Assertions.assertThatThrownBy(() -> calculator.sum())
+    Assertions.assertThatThrownBy(() ->
+            calculator.sum(
+                new AdditionalString(input))
+        )
         .isInstanceOf(RuntimeException.class);
   }
 }
