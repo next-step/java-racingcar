f828370cece535cda8b7bff8d170fbc49c4a270c
In-wook Hwang <inwook9@gmail.com>
2023-04-06 01:17:48 +0900

    feat: 기능요구사항-2 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,15 +4,49 @@ import java.util.Objects;
 
 public class StringCalculator {
 
-    public static final int DEFAULT_VALUE = 0;
-    public static final String SPLIT_REGEX = "[,:]";
+    private static final int DEFAULT_VALUE = 0;
+    private static final String DEFAULT_SPLIT_REGEX = "[,:]";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\n";
 
     public int calculate(String text) {
         if (isEmpty(text)) {
             return DEFAULT_VALUE;
         }
-        String[] splitTexts = getSplitTexts(text);
+        String delimiter = extractCustomDelimiterOrDefault(text);
+        text = removeCustomDelimiter(text, delimiter);
+        String[] splitTexts = getSplitTexts(text, delimiter);
         return calculate(splitTexts);
+    }
+
+    private boolean isEmpty(String text) {
+        if (Objects.isNull(text)) {
+            return true;
+        }
+        return text.isEmpty();
+    }
+
+    private String extractCustomDelimiterOrDefault(String text) {
+        if (!existCustomDelimiter(text)) {
+            return DEFAULT_SPLIT_REGEX;
+        }
+        return text.substring(text.indexOf(CUSTOM_DELIMITER_PREFIX) + CUSTOM_DELIMITER_PREFIX.length(),
+                              text.indexOf(CUSTOM_DELIMITER_SUFFIX));
+    }
+
+    private boolean existCustomDelimiter(String text) {
+        return text.startsWith(CUSTOM_DELIMITER_PREFIX) && text.contains(CUSTOM_DELIMITER_SUFFIX);
+    }
+
+    private String removeCustomDelimiter(String text, String delimiter) {
+        if (DEFAULT_SPLIT_REGEX.equals(delimiter)) {
+            return text;
+        }
+        return text.split(CUSTOM_DELIMITER_SUFFIX)[1];
+    }
+
+    private String[] getSplitTexts(String text, String delimiter) {
+        return text.split(delimiter);
     }
 
     private int calculate(String[] splitTexts) {
@@ -22,16 +56,5 @@ public class StringCalculator {
             result += parsedValue;
         }
         return result;
-    }
-
-    private String[] getSplitTexts(String text) {
-        return text.split(SPLIT_REGEX);
-    }
-
-    private boolean isEmpty(String text) {
-        if (Objects.isNull(text)) {
-            return true;
-        }
-        return text.isEmpty();
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -8,6 +8,15 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+/**
+ * 요구사항
+ * <pre>
+ *     1. 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
+ *     2. 앞의 기본 구분자(쉼표, 콜론)외에 커스텀 구분자를 지정할 수 있다.
+ *        커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다.
+ *        예를 들어 “//;\n1;2;3”과 같이 값을 입력할 경우 커스텀 구분자는 세미콜론(;)이며, 결과 값은 6이 반환되어야 한다.
+ * </pre>
+ */
 @DisplayName("문자열 덧셈 계산기를 통한 TDD/리팩토링 실습")
 class StringCalculatorTest {
 
@@ -16,7 +25,8 @@ class StringCalculatorTest {
                          Arguments.of("", 0),
                          Arguments.of("1,2", 3),
                          Arguments.of("1,2,3", 6),
-                         Arguments.of("1,2:3", 6));
+                         Arguments.of("1,2:3", 6),
+                         Arguments.of("//;\n1;2;3", 6));
     }
 
     @ParameterizedTest(name = "쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환({0}={1})")
