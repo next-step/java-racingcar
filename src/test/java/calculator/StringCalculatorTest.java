6c8dd4f351a9d07fe439db46c39191d4ff382591
In-wook Hwang <inwook9@gmail.com>
2023-04-06 01:58:55 +0900

    doc: 주석 및 테스트 설명 수정

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -11,13 +11,17 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 /**
- * 요구사항
+ * 기능 요구사항
  * <pre>
  *     1. 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
  *     2. 앞의 기본 구분자(쉼표, 콜론)외에 커스텀 구분자를 지정할 수 있다.
  *        커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다.
  *        예를 들어 “//;\n1;2;3”과 같이 값을 입력할 경우 커스텀 구분자는 세미콜론(;)이며, 결과 값은 6이 반환되어야 한다.
  *     3. 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.
+ * </pre>
+ * 프로그래밍 요구사항
+ * <pre>
+ *     메소드가 너무 많은 일을 하지 않도록 분리하기 위해 노력해 본다.
  * </pre>
  */
 @DisplayName("문자열 덧셈 계산기를 통한 TDD/리팩토링 실습")
@@ -34,8 +38,9 @@ class StringCalculatorTest {
                          Arguments.of("//;\n1;2;3", 6));
     }
 
-    @ParameterizedTest(name = "특정 구분자를 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환합니다.({0}={1})")
+    @ParameterizedTest(name = "input={0} / expected={1}")
     @MethodSource("provideArgumentsForSuccessTest")
+    @DisplayName("특정 구분자를 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환합니다.")
     void success_test(String text, int expected) {
         // given
         StringCalculator stringCalculator = new StringCalculator();
@@ -47,10 +52,11 @@ class StringCalculatorTest {
         assertThat(result).isSameAs(expected);
     }
 
-    @ParameterizedTest(name = "문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 합니다.({0})")
+    @ParameterizedTest(name = "input={0}")
     @ValueSource(strings = {"a,1",
                             "2,-1",
                             "adsf"})
+    @DisplayName("문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 합니다.")
     void failed_test(String text) {
         // given
         StringCalculator stringCalculator = new StringCalculator();
