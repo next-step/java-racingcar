d3bd75ee0f86e010b7d207f8b9af8eb84a1fdc83
EthanKim <hey0507@gmail.com>
2023-04-05 23:46:55 +0900

    Update : 피드백 반영 - Pattern.compile() 부분 Static 으로 선언 - i, MagicNumber 변경 -> 인자값을 구체화 - isEmpty -> isBlank() 로 변경, 메서드명 변경(세분화) - SEPARATE_STRING 을 정규식으로 표현
    - test 코드의 불필요한 Exception 부분 제거
    - test 코드 메서드의 한영혼용 변경

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -4,45 +4,54 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
-    private static final String SEPARATE_STRING = ",|:";
-    private static final String CUSTOM_SEPARATE_REGEX = "//(.)\n(.*)";
+    private static final String SEPARATE_STRING = "[,:]";
+    private static final Pattern CUSTOM_SEPARATE_REGEX_CHECKER = Pattern.compile("//(.)\n(.*)");
+
+    private static final int CUSTOM_DELIMITER = 1;
+    private static final int NUMBER_TOKENS = 2;
 
     public static int splitAndSum(String input) {
-        if(isEmpty(input)) return 0;
+        if (isNullOrEmpty(input)) {
+            return 0;
+        }
+
         int[] separateResult = separateInputToIntArray(input);
+
         negativeValidator(separateResult);
 
-        return Arrays.stream(separateResult)
+        return Arrays
+                .stream(separateResult)
                 .sum();
     }
 
-    private static void negativeValidator(int[] targets){
-        if(hasNegative(targets)){
+    private static void negativeValidator(int[] targets) {
+        if (hasNegative(targets)) {
             throw new RuntimeException();
         }
     }
 
     private static boolean hasNegative(int[] separateResult) {
-        return Arrays.stream(separateResult).anyMatch(i -> i < 0);
+        return Arrays.stream(separateResult)
+                .anyMatch(eachNumber -> eachNumber < 0);
     }
 
-    private static int[] separateInputToIntArray(String input){
+    private static int[] separateInputToIntArray(String input) {
         Matcher matcher = separateByRegex(input);
-        if(matcher.find()){
-            return Arrays.stream(matcher.group(2)
-                    .split(matcher.group(1)))
+        if (matcher.find()) {
+            return Arrays.stream(matcher.group(NUMBER_TOKENS)
+                            .split(matcher.group(CUSTOM_DELIMITER)))
                     .mapToInt(Integer::parseInt).toArray();
         }
         return Arrays.stream(input
-                .split(SEPARATE_STRING))
+                        .split(SEPARATE_STRING))
                 .mapToInt(Integer::parseInt).toArray();
     }
 
-    private static boolean isEmpty(String input){
-        return input == null || input.isEmpty();
+    private static boolean isNullOrEmpty(String input) {
+        return input == null || input.isBlank();
     }
 
     private static Matcher separateByRegex(String input) {
-        return Pattern.compile(CUSTOM_SEPARATE_REGEX).matcher(input);
+        return CUSTOM_SEPARATE_REGEX_CHECKER.matcher(input);
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,15 +1,15 @@
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("문자열 계산기 기능 테스트")
 class StringAddCalculatorTest {
 
     @Test
     @DisplayName("비거나 null 값이 들어있는 경우 0 을 반환한다.")
-    public void splitAndSum_null_또는_빈문자() {
+    public void splitAndSumNullOrEmpty() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
@@ -19,35 +19,35 @@ class StringAddCalculatorTest {
 
     @Test
     @DisplayName("숫자 하나를 입력하면, 해당 숫자를 반환한다.")
-    public void splitAndSum_숫자하나() throws Exception {
+    public void splitAndSumOneNumber() {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     @DisplayName("쉼표 구분자를 입력할 경우, 구분된 문자열의 합을 반환한다.")
-    public void splitAndSum_쉼표구분자() throws Exception {
+    public void splitAndSumWithComma() {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     @DisplayName("쉼표 또는 콜론으로 구분자를 넣을 수 있다.")
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+    public void splitAndSumWithCommaOrColon() {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     @DisplayName("\"//\"와 \"\\n\" 사이에, 구분자를 커스텀할 수 있다.")
-    public void splitAndSum_custom_구분자() throws Exception {
+    public void splitAndSum_custom_Separator() {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     @DisplayName("음수가 포함되어있으면, RuntimeException 발생")
-    public void splitAndSum_negative() throws Exception {
+    public void splitAndSum_negative() {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
