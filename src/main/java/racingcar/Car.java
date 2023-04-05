df3c673b4ec7a5eb210d9b92c3c5ced9dac0b88b
신형기 <tkaqkeldk@hanatour.com>
2023-04-05 23:13:12 +0900

    feat: 자동차 전진하는 조건 테스트

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,11 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Car {
+
+    private static final int FORWARD_NUM = 4;
+    private static final int MAX_BOUND = 10;
 
     private int position = 0;
 
@@ -8,7 +13,18 @@ public class Car {
     }
 
     public void move() {
-        position++;
+        if (getRandomNumber() >= FORWARD_NUM) {
+            this.position += 1;
+        }
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    protected int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(MAX_BOUND);
     }
 
     @Override
@@ -16,9 +32,5 @@ public class Car {
         return "Car{" +
                 "position=" + position +
                 '}';
-    }
-
-    public int getPosition() {
-        return this.position;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,11 +8,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CarTest {
 
     @Test
-    void test() {
+    @DisplayName("자동차 전진 테스트")
+    void car_move_test() {
         Car car = new Car();
         car.move();
 
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("0 에서 9 사이에서 random 값을 테스트한다.")
+    void random_number_test() {
+        Car car = new Car();
+        int randomNumber = car.getRandomNumber();
+
+        assertThat(randomNumber).isBetween(0, 9);
+    }
+
+    @Test
+    @DisplayName("자동차 는 random 값이 4이상일 경우 전진한다.")
+    void car_move_true_test() {
+        Car car = new Car() {
+            @Override
+            protected int getRandomNumber() {
+                return 4;
+            }
+        };
+        car.move();
+
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동차 는 random 값이 4 미만 일 경우 전진 하지 않은다.")
+    void car_move_false_test() {
+        Car car = new Car() {
+            @Override
+            protected int getRandomNumber() {
+                return 3;
+            }
+        };
+        car.move();
+
+        assertThat(car.getPosition()).isZero();
+    }
 }
