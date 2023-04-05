f806e93acd3344ed3fa5f8bdbdf7be90d06fddaf
choitaehoon <hugebigdream@gmail.com>
2023-04-06 01:53:22 +0900

    한메소드가 가능한한 한가지 일만 하도록 메소드 분리

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -4,36 +4,37 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    //TODO: 메소드가 너무 많은 일을 하지 않도록 분리하기 위해 노력해 본다.
     public static int splitAndSum(String givenValue) {
-        if (givenValue == null || givenValue.isEmpty()) {
+        if (isNullOrEmpty(givenValue)) {
             return 0;
         }
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(givenValue);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            System.out.println("customDelimiter = " + customDelimiter);
-            String[] tokens = m.group(2).split(customDelimiter);
-            int total = 0;
-            for (String number : tokens) {
-                total += Integer.parseInt(number);
-            }
-            return total;
+            String[] numbers = splitByGivenDelimiter(m.group(2), m.group(1));
+            return getTotal(numbers);
         }
-        String[] numbers = givenValue.split(",|:");
-        if (numbers.length == 1) {
-            return Integer.parseInt(numbers[0]);
-        }
+        String[] numbers = splitByGivenDelimiter(givenValue, ",|:");
+        return getTotal(numbers);
+    }
+
+    private static boolean isNullOrEmpty(String givenValue) {
+        return givenValue == null || givenValue.isEmpty();
+    }
+
+    private static String[] splitByGivenDelimiter(String givenString, String customDelimiter) {
+        return givenString.split(customDelimiter);
+    }
+
+    private static int getTotal(String[] numbers) {
         int total = 0;
         for (String number : numbers) {
             int parsedNumber = Integer.parseInt(number);
             if (parsedNumber < 0) {
-                throw new RuntimeException();
+                throw new IllegalArgumentException("음수를 포함하지 말아야 합니다.");
             }
             total += parsedNumber;
         }
         return total;
-
     }
 
 }
