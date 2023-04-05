0aba00388408a6c3e941450e21f3e71ac434f813
IMHYEWON <37797830+IMHYEWON@users.noreply.github.com>
2023-04-05 10:42:10 +0900

    [1단계] 학습테스트 실습 (#4168)
    * [step1] String 학습 테스트
    
    * [step1] Set 학습 테스트

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,39 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+    //    "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한트다.
+    //    "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+    @Test
+    @DisplayName("split 테스트")
+    void split() {
+        String[] splitArrayContainsTwo = "1,2".split(",");
+        String[] splitArrayContainsOne = "1,".split(",");
+
+        assertThat(splitArrayContainsTwo)
+            .contains("1").contains("2")
+            .contains("1", "2")
+            .containsExactly("1", "2");
+        assertThat(splitArrayContainsOne).containsOnly("1");            //오직하나만
+    }
+
+    //    "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
+    @Test
+    @DisplayName("substring 테스트")
+    void substring() {
+        assertThat("(1,2)".substring(1, 4)).isEqualTo("1,2");
+    }
+
+    //    "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+    //    String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+    //    JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+    @Test
+    @DisplayName("chartAt 테스트")
+    void chartAt() {
+        assertThatThrownBy(() -> "abc".charAt(3)).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+}
