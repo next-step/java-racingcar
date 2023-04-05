6dd9d0722ca7cc7ae3c72b502bfafa768bbf5c24
kth496 <taehongkim496@gmail.com>
2023-04-05 16:40:29 +0900

    feat: 기능3 구현

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Calculator {
+    public Integer sumAll(List<Integer> numbers) {
+        return numbers.stream().reduce(0, Integer::sum);
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("[3] 구분자를 기준으로 분리한 각 숫자의 합을 반환")
+    public void sumTest1() {
+        List<Integer> given = List.of(1, 2, 3, 4);
+        Integer actual = calculator.sumAll(given);
+        assertThat(actual).isEqualTo(10);
+    }
+}
