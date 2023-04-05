7b5bac6bd2e850b911a973e57c9bfea37197558b
진우 <nice7677@naver.com>
2023-04-05 21:33:22 +0900

    2단계 - 문자열 덧셈 계산기

---
+++ src/main/java/step2/exception/NotPositiveIntegerException.java
@@ -0,0 +1,9 @@
+package step2.exception;
+
+public class NotPositiveIntegerException extends RuntimeException{
+
+    public NotPositiveIntegerException(String message) {
+        super(message);
+    }
+
+}
