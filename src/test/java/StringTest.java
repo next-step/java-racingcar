51d2702e1644d23493a33540fcea9e049197cf87
kth496 <taehongkim496@gmail.com>
2023-04-05 11:55:30 +0900

    refactor: 1단계 코드 리뷰 반영

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
