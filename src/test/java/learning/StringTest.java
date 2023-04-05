981fce2e9b8630ab118a6a22b919c8df8c220156
MoonHKLee <moonhyuk.lee@lotte.net>
2023-04-05 09:57:48 +0900

    test(string) : 요구사항3 테스트 진행.

--- src/test/java/learning/StringTest.java
+++ src/test/java/learning/StringTest.java
@@ -46,4 +46,35 @@ public class StringTest {
         //assert
         assertThat(substring).isEqualTo("1,2");
     }
+
+    @Test
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져온다")
+    void testStringWithCharAt() {
+        //arrange
+        String target = "abc";
+
+        //act
+        char charAt0 = target.charAt(0);
+        char charAt1 = target.charAt(1);
+        char charAt2 = target.charAt(2);
+
+        //assert
+        assertThat(charAt0).isEqualTo('a');
+        assertThat(charAt1).isEqualTo('b');
+        assertThat(charAt2).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용할 때 값이 벗어나면 StringIndexOutOfBoundsException이 발생한다.")
+    void testStringWithCharAtOutOfIndex() {
+        //arrange
+        String target = "abc";
+
+        //assert
+        assertThatThrownBy(() -> {
+            //act
+            target.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
 }
