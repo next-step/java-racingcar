4540539936ae3e4a9858e39c1a34036b9afb1b0b
ynoolee <53856184+ynoolee@users.noreply.github.com>
2023-04-05 22:47:04 +0900

    1단계 - 학습 테스트 실습 (#4195)
    * test: String 의 split 메소드 테스트 코드 작성
    
    * test: String 의 substring 메소드 테스트코드 작성
    
    * test: String 의 charAt 메소드 테스트코드 작성
    
    * test: Set 의 메소드 테스트 코드 작성

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,58 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size 메소드는 자료구조에 실제로 들어있는 유니크한 원소의 개수를 리턴한다")
+    void size() {
+        int size = numbers.size();
+
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("contains 메소드는 해당 원소가 자료구조에 들어있을 경우 true 를 리턴한다")
+    @ValueSource(ints = {1, 2, 3})
+    void given_containingValues_whenCallingContains_thenAlwaysTrue(int inputData) {
+        assertThat(numbers.contains(inputData))
+            .isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("contains 메서드는 해당 원소가 자료구조에 들어있지 않을 경우 false 를 리턴한다")
+    @NullSource
+    @ValueSource(ints = {4, 5})
+    void given_notContainingValues_whenCallingContains_thenAlwaysTrue(Integer inputData) {
+        assertThat(numbers.contains(inputData))
+            .isFalse();
+    }
+
+    @ParameterizedTest
+    @DisplayName("contains 메소드는 해당 원소가 자료구조에 들어있을 경우 true 를, 들어있지 않을 경우 false 를 리턴한다")
+    @CsvSource(value = {"1:true", "2:true", "4:false", "5:false"}, delimiter = ':')
+    void given_multipleValues_whenCallingContains_thenSomeAreTureAndSomeAreFalse(int inputData,
+        Boolean expectedValue) {
+        assertThat(numbers.contains(inputData))
+            .isEqualTo(expectedValue);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,73 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("String 클래스의 ")
+public class StringTest {
+
+    @Nested
+    @DisplayName("split 메소드를 사용하여  ")
+    class SplitTest {
+
+        private final String delimiterComma = ",";
+
+        @Test
+        @DisplayName("콤마로 분리된 문자열 1,2 를 콤마를 중심으로 문자열 1과 2로 쪼갠다")
+        void givenStringWithComma_whenSplit_thenReturnParts() {
+            String strWithComma = "1,2";
+
+            String[] parts = strWithComma.split(delimiterComma);
+
+            Assertions.assertThat(parts).hasSize(2);
+            Assertions.assertThat(parts[0]).isEqualTo("1");
+            Assertions.assertThat(parts[1]).isEqualTo("2");
+        }
+
+        @Test
+        @DisplayName("콤마가 존재하지 않는 문자열 1 을 콤마를 중심으로 쪼개려 할 경우 원본 문자열만이 포함된 배열을 리턴한다")
+        void givenStringWithoutComma_whenSplit_thenReturnOnePart() {
+            String strWithoutComma = "1";
+
+            String[] parts = strWithoutComma.split(delimiterComma);
+
+            Assertions.assertThat(parts).hasSize(1);
+            Assertions.assertThat(parts[0]).isEqualTo("1");
+        }
+    }
+
+    @Nested
+    @DisplayName("substring 메소드를 사용할 때 ")
+    class SubStringTest {
+
+        @Test
+        @DisplayName("시작 index 와 끝 index 가 주어질 경우, 시작 index 를 포함하고 끝 index 를 제외한 범위의 문자열 일부를 추출한다")
+        void givenString_whenSubString_thenExtractPartOfString() {
+            String originString = "(1,2)";
+
+            int beforeStartIdx = originString.indexOf('(');
+            int afterEndIdx = originString.indexOf(')');
+
+            String partOfString = originString.substring(beforeStartIdx + 1, afterEndIdx);
+
+            Assertions.assertThat(partOfString).isEqualTo("1,2");
+        }
+    }
+
+    @Nested
+    @DisplayName("chatAt 메소드를 사용하여 ")
+    class CharAtTest {
+
+        private final String givenString = "abc";
+
+        @Test
+        @DisplayName("존재하지 않는 index 를 인수로 호출 할 경우 예외가 발생한다")
+        void givenInvalidIndex_whenCharAt_thenThrowException() {
+            int invalidIdx = -1;
+
+            Assertions.assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> givenString.charAt(invalidIdx))
+                .withMessageMatching("String index out of range: -?\\d+");
+        }
+    }
+}
