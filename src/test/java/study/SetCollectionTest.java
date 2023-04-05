ef306934fe3f4397fd9f3c75d7281f77f9efca8a
DeveloperKGH <65940338+DeveloperKGH@users.noreply.github.com>
2023-04-05 20:55:25 +0900

    :sparkles: "add StringTest & SetCollectionTest" (#4185)

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,55 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("1단계_SetCollectionTest_요구사항1 - size() 했을 때 Set 의 크기인 3이 반환되는지 확인")
+    @Test
+    void size() {
+        //given
+
+        //when, then
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("1단계_SetCollectionTest_요구사항2 - contains() 했을 때 1, 2, 3의 값이 존재하는지를 확인")
+    @ParameterizedTest(name = "testValue : {0}")
+    @ValueSource(ints = {1, 2, 3})
+    void contains_only_true_case(int number) {
+        //given
+
+        //when, then
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("1단계_SetCollectionTest_요구사항3 - contains() 했을 때 1, 2, 3의 값은 존재하고, 4, 5의 값은 존재하지 않음을 확인")
+    @ParameterizedTest(name = "testValue : {0}, 포함여부 : {1}")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_true_and_false_case(int number, boolean expected) {
+        //given
+
+        //when, then
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+}
