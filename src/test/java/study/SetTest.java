7ebddc5a3a8c721ebfe760f36032c9b1d6c7117b
SMJminjeong <sdwp0920@gmail.com>
2023-04-05 21:37:58 +0900

    Set.contains() study test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,11 +3,14 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
 
@@ -31,5 +34,25 @@ public class SetTest {
     void setSize() {
         int setSize = numbers.size();
         assertThat(setSize).isEqualTo(3);
+    }
+
+    /**
+     * 요구사항 2
+     * Set의 contains()메소드를 활용해 1,2,3의 값의 존재 확인
+     * JUnit의 ParameterizedTest를 활용하여 중복 코드 제거해보기
+     */
+    @Test
+    @DisplayName("contains test")
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @DisplayName("@ParameterizedTest 를 이용하여 중복 코드 제거")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void parameterizedTest(int num) {
+        assertTrue(numbers.contains(num));
     }
 }
