299add3626851f98069bd1e0577657a10e5b1890
eric.cc <eric.cc@kakaocorp.com>
2023-04-05 16:45:42 +0900

    1단계 - 학습 테스트 실습
    코드 정리

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -1,12 +1,10 @@
 package step1;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,7 +12,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 public class SetTest {
     private Set<Integer> numbers;
--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -1,12 +1,8 @@
 package step1;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
