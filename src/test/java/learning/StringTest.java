b749d53c37ca023068a3cc3e3ffb63d729d130cf
MoonHKLee <moonhyuk.lee@lotte.net>
2023-04-05 09:57:30 +0900

    test(string) : 요구사항2 테스트 진행.

--- src/test/java/learning/StringTest.java
+++ src/test/java/learning/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -18,5 +19,31 @@ public class StringTest {
 
         //assert
         assertThat(split).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("\"1\"을 , 로 split 하면 1만 포함하는 배열을 반환한다.")
+    void testStringWithOneComma() {
+        //arrange
+        String target = "1,";
+
+        //act
+        String[] split = target.split(",");
+
+        //assert
+        assertThat(split).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때, String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환한다.")
+    void testStringWithOneCommaTwoAndBracket() {
+        //arrange
+        String target = "(1,2)";
+
+        //act
+        String substring = target.substring(1, target.length() - 1);
+
+        //assert
+        assertThat(substring).isEqualTo("1,2");
     }
 }
