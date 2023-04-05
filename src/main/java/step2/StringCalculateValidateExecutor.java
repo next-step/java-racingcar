7f415ff2a1a7be22157ef7ca3d3946d676efd6ff
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-06 01:47:37 +0900

    리펙토링

--- src/main/java/step2/StringCalculateValidateExecutor.java
+++ src/main/java/step2/StringCalculateValidateExecutor.java
@@ -3,13 +3,12 @@ package step2;
 import step2.model.Input;
 import step2.validator.Validator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StringCalculateValidateExecutor {
 
-    private List<Validator> validatorList = new ArrayList<>();
+    private List<Validator> validatorList;
 
     public StringCalculateValidateExecutor(
             Validator... validator
