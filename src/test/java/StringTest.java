20dcaacd8e8584dc1471a22bc2ca8e9d19bd1c90
jerry shin <shiney798@gmail.com>
2023-04-06 00:53:04 +0900

    String 클래스에 대한 학습 테스트

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 및 Exception 발생 학습테스트")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            char result = "abc".charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+    }
+}
