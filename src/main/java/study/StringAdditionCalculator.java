54f9ec18397b12841f65d4be6f4d3a6b926f9b96
seriouskang <scdean110@gmail.com>
2023-04-05 22:22:05 +0900

    feat: 단일 숫자 문자열 입력 시, 해당 숫자를 반환

--- src/main/java/study/StringAdditionCalculator.java
+++ src/main/java/study/StringAdditionCalculator.java
@@ -6,7 +6,7 @@ public class StringAdditionCalculator {
             return 0;
         }
 
-        return 0;
+        return Integer.parseInt(input);
     }
 
     private static boolean isNullOrEmpty(String input) {
--- src/test/java/study/StringAdditionCalculatorTest.java
+++ src/test/java/study/StringAdditionCalculatorTest.java
@@ -7,10 +7,21 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.*;
 
 class StringAdditionCalculatorTest {
-    @ParameterizedTest(name = "[{index}/3] {displayName}")
-    @CsvSource(value = {"'',0", ",0", "' ',0"})
+    @ParameterizedTest(name = "[{index}/2] {displayName}")
+    @CsvSource(value = {"'',0", ",0"})
     @DisplayName("빈 문자열이나 null 값을 입력할 경우 0 반환")
-    void 빈_문자열_또는_null_덧셈_연산(String input, int expected) {
+    void null_or_빈문자(String input, int expected) {
+        // given, when
+        int result = StringAdditionCalculator.splitAndSum(input);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "[{index}/2] {displayName}")
+    @CsvSource(value = {"0,0", "100,100"})
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환")
+    void 숫자_하나(String input, int expected) {
         // given, when
         int result = StringAdditionCalculator.splitAndSum(input);
 
