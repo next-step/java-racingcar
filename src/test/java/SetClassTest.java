b68cf8b4cb7b025dcb4db9bec84d502d209a61f7
syndersonLEE <winnery93@gmail.com>
2023-04-06 10:45:36 +0900

    feat : SetClassTest

--- src/test/java/SetClassTest.java
+++ src/test/java/SetClassTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -29,5 +30,11 @@ public class SetClassTest {
 	@ValueSource(ints = {1, 2, 3})
 	void setContains(int parameters) {
 		assertThat(numbers.contains(parameters)).isTrue();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter =  ':')
+	void setContainsTestWithCsvSource(int intParameter, boolean expected) {
+		assertThat(numbers.contains(intParameter)).isEqualTo(expected);
 	}
 }
