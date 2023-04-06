61ff19abb561c359ed1625afa6852e950b464647
jaewon <jaewon9937@gmail.com>
2023-04-06 20:59:49 +0900

    2단계 - 문자열 덧셈 계산기(피드백 수정) - 주석 제서

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -11,20 +11,14 @@ public class StringAddCalculator {
 
     public int splitAndSum(String inputText) {
 
-        //  1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.
         if (checkEmpty(inputText)) {
             return 0;
         }
 
-        //  2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.
         if (inputText.length() == 1) {
             return parseFormat(inputText);
         }
 
-        //  3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.
-        //  4. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.
-        //  5. “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다.
-        //  6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다.
         return sumNumbers(splitText(inputText));
     }
 
