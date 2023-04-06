d993b85a768cea1a5295dc2cbbbbacaf2db99115
lumioustone <mlicp1994@gmail.com>
2023-04-07 05:06:53 +0900

    docs[calculator]: 기능요구사항 정리

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringCalculatorTest {
+
+
+}
