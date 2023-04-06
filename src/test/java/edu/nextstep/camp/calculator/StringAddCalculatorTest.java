e3d415401f0ad32c0bca2a469fbf94669937da73
rkd5656meta <rkd8307@naver.com>
2023-04-06 21:09:41 +0900

    Test: 테스트 파라미터 추가

--- src/test/java/edu/nextstep/camp/calculator/StringAddCalculatorTest.java
+++ src/test/java/edu/nextstep/camp/calculator/StringAddCalculatorTest.java
@@ -1,18 +1,19 @@
 package edu.nextstep.camp.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
 
-    @Test
-    public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void splitAndSum_null_또는_빈문자(String nums) {
+        int result = StringAddCalculator.splitAndSum(nums);
         assertThat(result).isEqualTo(0);
     }
 
@@ -30,19 +31,21 @@ public class StringAddCalculatorTest {
 
     @Test
     public void splitAndSum_쉼표_또는_콜론_구분자() {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
+        int result = StringAddCalculator.splitAndSum("1,2:3:4");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3", "//@\n1@2@3"})
+    public void splitAndSum_custom_구분자(String nums) {
+        int result = StringAddCalculator.splitAndSum(nums);
         assertThat(result).isEqualTo(6);
     }
 
-    @Test
-    public void splitAndSum_custom_구분자() {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
-        assertThat(result).isEqualTo(6);
-    }
-
-    @Test
-    public void splitAndSum_negative() {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2,3", "1,-2,3", "1;-2~3,200","7,1,2,fd3", "//@\n1@2:3"})
+    public void splitAndSum_negative(String nums) {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(nums))
                 .isInstanceOf(RuntimeException.class);
     }
 }
