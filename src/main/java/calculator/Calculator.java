7d8f5e0720a3d1948d8148363ddbc353c247d1d8
hoyeoon <hoyeon.backend@gmail.com>
2023-04-07 02:01:51 +0900

    refactor: 메서드 위치 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,19 +5,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Calculator {
+    public int splitAndSum(String input) {
+        if(input == null || input.isBlank()) {
+            return 0;
+        }
+        return sum(getIntegerList(input));
+    }
+
     public int sum(List<Integer> list) {
         int sum = 0;
         for (Integer value : list) {
             sum += value;
         }
         return sum;
-    }
-
-    public int splitAndSum(String input) {
-        if(input == null || input.isBlank()) {
-            return 0;
-        }
-        return sum(getIntegerList(input));
     }
 
     private static List<Integer> getIntegerList(String input) {
