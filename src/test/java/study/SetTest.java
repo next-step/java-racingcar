7778430ed1d4a459b9ad9476bb2815a9787f4d33
Dale <steady8@kakao.com>
2023-04-06 19:08:05 +0900

    1단계 - 학습 테스트 실습

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,49 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인")
+    void checkSizeAsUsingSizeMethodOfSet() {
+        int setSize = numbers.size();
+        assertThat(setSize).isEqualTo(3);
+    }
+
+
+    @ValueSource(ints = {1, 2, 3})
+    @ParameterizedTest
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인")
+    void checkContainNumberAsUsingContainsMethodOfSet(int sampleNumber) {
+        assertThat(numbers).contains(sampleNumber);
+    }
+
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @ParameterizedTest
+    public void checkIfContainsNumbers(int sample, boolean expectedResult) {
+
+        assertThat(numbers.contains(sample)).isEqualTo(expectedResult);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
 
@@ -16,5 +18,30 @@ public class StringTest {
     void splitInOrder() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void returnOnlyNumberOne() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    void removeParenthesisAndReturnNumberOneAndTwo() {
+        String value = "(1,2)";
+        String expectResult = "1,2";
+        String result = value.substring(1, 4);
+        assertThat(result).isEqualTo(expectResult);
+    }
+
+    @Test
+    @DisplayName("문자열의 특정 인덱스 위치의 문자를 추출할 때 범위를 벗어나면 StringIndexOutOfBoundsException이 발생하는지 검증")
+    public void requirement3_whenGettingCharAtSpecificPosition() {
+
+        String value = "abc";
+
+        assertThatThrownBy(() -> {
+            value.charAt(10);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
