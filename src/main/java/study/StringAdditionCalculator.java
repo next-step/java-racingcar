29dee8834ed200ecd680cade1f69ba6a0a995bf6
seriouskang <scdean110@gmail.com>
2023-04-06 23:43:41 +0900

    feat: 커스텀 구분자 로직 신규

--- src/main/java/study/StringAdditionCalculator.java
+++ src/main/java/study/StringAdditionCalculator.java
@@ -1,9 +1,12 @@
 package study;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAdditionCalculator {
     private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final String CUSTOM_DELIMITER_INPUT_PATTERN_REGEX = "//(.)\n(.*)";
 
     public static int splitAndSum(String input) {
         if(isNullOrEmpty(input)) {
@@ -15,7 +18,27 @@ public class StringAdditionCalculator {
     }
 
     private static String[] operandsOf(String input) {
+        if(isCustomDelimiterInput(input)) {
+            return splitByCustomDelimiter(input);
+        }
+
         return input.split(DEFAULT_DELIMITER_REGEX);
+    }
+
+    private static String[] splitByCustomDelimiter(String input) {
+        Matcher matcher = matcherOf(input);
+        matcher.find();
+
+        String customDelimiter = matcher.group(1);
+        return matcher.group(2).split(customDelimiter);
+    }
+
+    private static boolean isCustomDelimiterInput(String input) {
+        return matcherOf(input).matches();
+    }
+
+    private static Matcher matcherOf(String input) {
+        return Pattern.compile(CUSTOM_DELIMITER_INPUT_PATTERN_REGEX).matcher(input);
     }
 
     private static int sumOf(String[] operands) {
--- src/test/java/study/StringAdditionCalculatorTest.java
+++ src/test/java/study/StringAdditionCalculatorTest.java
@@ -1,10 +1,14 @@
 package study;
 
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.SoftAssertions.*;
 import static study.StringAdditionCalculator.*;
 
 class StringAdditionCalculatorTest {
@@ -27,5 +31,16 @@ class StringAdditionCalculatorTest {
     @DisplayName("컴마 혹은 콜론 구분자로 분리된 숫자들을 입력할 경우 해당 숫자들의 합 반환")
     void 컴마_혹은_콜론_구분자로_분리된_숫자들(String input, int expected) {
         assertThat(splitAndSum(input)).isEqualTo(expected);
+    }
+
+//    @ParameterizedTest(name = "[{index}/2] {displayName}")
+//    @CsvSource(value = {"//;\\n1;2;3-6", "//#\\n1#10-11"}, delimiter = '-')
+    @Test // TODO: @ParameterizedTest 적용 가능 여부 확인
+    @DisplayName("커스텀 구분자(“//”와 “\\n” 사이에 위치하는 문자)로 분리된 숫자들을 입력할 경우 해당 숫자들의 합 반환")
+    void 커스텀_구분자로_분리된_숫자들() {
+        assertSoftly(softly -> {
+            softly.assertThat(splitAndSum("//;\n1;2;3")).isEqualTo(6);
+            softly.assertThat(splitAndSum("//#\n1#10")).isEqualTo(11);
+        });
     }
 }
