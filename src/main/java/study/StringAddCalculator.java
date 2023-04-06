f0c0f082f0803dfcc89166b3fa97203ac709dfc0
player-geun <player_geun@naver.com>
2023-04-06 14:18:02 +0900

    [step 2] StringAddCalulator
    요구사항에 맞게 문자열 덧셈 계산기와 테스트를 구현

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,73 @@
+package study;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final String DELIMITER = ",|:";
+
+    public static int splitAndSum(String text) {
+        if (isNullOrEmpty(text)) {
+            return 0;
+        }
+
+        if (isOnlyNumber(text)) {
+            return Integer.parseInt(text);
+        }
+
+        String[] tokens = split(text);
+
+        return sum(tokens);
+    }
+
+    private static int sum(String[] tokens) {
+        return Arrays.stream(tokens)
+                .mapToInt(e -> parsePositiveInt(e))
+                .sum();
+    }
+
+    private static int parsePositiveInt(String token) {
+        int number;
+
+        try {
+            number = Integer.parseInt(token);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (number < 0) {
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
+
+        return number;
+    }
+
+    private static String[] split(String text) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        String delimiter = DELIMITER;
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            delimiter += "|" + customDelimiter;
+            text = matcher.group(2);
+        }
+
+        return text.split(delimiter);
+    }
+
+    private static boolean isOnlyNumber(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (!Character.isDigit(text.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isNullOrEmpty(String text) {
+        return Objects.isNull(text) || text.isEmpty();
+    }
+}
---
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -0,0 +1,88 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringAddCalculatorTest {
+
+    @Test
+    public void splitAndSum_null() {
+        //given
+
+        //when
+        int result = StringAddCalculator.splitAndSum(null);
+
+        //then
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_빈문자() {
+        //given
+
+        //when
+        int result = StringAddCalculator.splitAndSum("");
+
+        //then
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() throws Exception {
+        //given
+
+        //when
+        int result = StringAddCalculator.splitAndSum("1");
+
+        //then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        //given
+
+        //when
+        int result = StringAddCalculator.splitAndSum("1,2");
+
+        //then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        //given
+
+        //when
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+
+        //then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        //given
+
+        //when
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+
+        //then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_음수포함() throws Exception {
+        //given
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
