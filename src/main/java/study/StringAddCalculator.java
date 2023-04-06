90269e97a7cc84378be99dfd0e65d45bb22e6e09
juno-junho <ssmm0205@naver.com>
2023-04-06 18:08:45 +0900

    refactor(study): refactor for clean code

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -4,42 +4,61 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    
+    public static final StringBuilder REGEX = new StringBuilder("[:,]");
+
     public static int splitAndSum(String text) {
         try {
-            // null 이거나 공백일 경우
-            if (text == null) {
-                return 0;
-            }
-            if (text.isBlank()) {
-                return 0;
-            }
-            // ,나 :으로 구분자 있을 경우
-
-            int sum = 0;
-            Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-            if (m.find()) {
-                String customDelimiter = m.group(1);
-                String[] tokens = m.group(2).split(customDelimiter);
-                for (String token : tokens) {
-                    int value = Integer.parseInt(token);
-                    if (value < 0) {
-                        throw new RuntimeException();
-                    }
-                    sum += value;
-                }
-            }else {
-                String[] split = text.split("[:,]");
-                for (String s : split) {
-                    int num = Integer.parseInt(s);
-                    if (num < 0) {
-                        throw new RuntimeException();
-                    }
-                    sum += num;
-                }
-            }
-            return sum;
+            return calculatedResult(text);
         } catch (Exception e) {
-            throw new RuntimeException();
+            throw new RuntimeException(e);
         }
+    }
+
+    private static int calculatedResult(String text) {
+        if (isNullOrBlank(text)) {
+            return 0;
+        }
+        text = validatedText(text);
+        return sumUsingDelimiter(text);
+    }
+
+    private static int sumUsingDelimiter(String text) {
+        int sum = 0;
+        String[] split = text.split(REGEX.toString());
+        for (String s : split) {
+            int num = Integer.parseInt(s);
+            checkIfIsNegative(num);
+            sum += num;
+        }
+        return sum;
+    }
+
+    private static String validatedText(String text) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (matcher.find()) {
+            text = textUpdate(matcher);
+        }
+        return text;
+    }
+
+    private static String textUpdate(Matcher matcher) {
+        char customDelimiter = matcher.group(1).charAt(0);
+        addDelimiter(customDelimiter);
+        return matcher.group(2);
+    }
+
+    private static void addDelimiter(char customDelimiter) {
+        REGEX.insert(3, customDelimiter);
+    }
+
+    private static void checkIfIsNegative(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isNullOrBlank(String text) {
+        return text == null || text.isBlank();
     }
 }
