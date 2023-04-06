ba2a837386a681fd43dffcb9e43c69c8bc94707a
hoyeoon <hoyeon.backend@gmail.com>
2023-04-07 04:00:54 +0900

    feat: 숫자 이외의 값을 전달할 경우 예외 발생 기능 구현

--- src/main/java/calculator/PositiveInteger.java
+++ src/main/java/calculator/PositiveInteger.java
@@ -4,10 +4,17 @@ public class PositiveInteger {
     private final int number;
 
     public PositiveInteger(String input) {
+        if (!isNumber(input)) {
+            throw new RuntimeException("숫자 이외의 값이 포함되어 있습니다.");
+        }
         this.number = Integer.parseInt(input);
     }
 
     public int getNumber() {
         return number;
+    }
+
+    private static boolean isNumber(String input) {
+        return input != null && input.matches("[-+]?\\d*\\.?\\d+");
     }
 }
