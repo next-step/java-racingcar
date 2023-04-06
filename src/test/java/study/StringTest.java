901b5a2b15afb9198990ffbe0b7a76fa33290830
Dale <steady8@kakao.com>
2023-04-06 17:08:35 +0900

    동영상을 따라하며 코드리뷰 플로우 테스트.

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,20 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    void splitInOrder() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+}
