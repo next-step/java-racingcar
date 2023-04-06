9714d7030103a5ddf65ee2619547a64b7744350c
dkswnkk <jjoy9810@gmail.com>
2023-04-07 00:11:11 +0900

    feat: InputView 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static int inputCarCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return SCANNER.nextInt();
+    }
+
+    public static int inputTryNumber() {
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        return SCANNER.nextInt();
+    }
+}
