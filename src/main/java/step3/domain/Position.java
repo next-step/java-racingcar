a16115a1c837c191cd02c9dc0620f2891b6683f6
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-06 20:58:01 +0900

    feat: Position 도메인 생성

---
+++ src/main/java/step3/domain/Position.java
@@ -0,0 +1,14 @@
+package step3.domain;
+
+public class Position {
+
+    private int position;
+
+    public void increase() {
+        position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
