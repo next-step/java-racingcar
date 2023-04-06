cb736c9c7c69fb8366c833393b88ea1df2c87778
jaewon <jaewon9937@gmail.com>
2023-04-06 21:07:42 +0900

    2단계 - 문자열 덧셈 계산기(피드백 수정) - 정규식 강조

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -29,7 +29,7 @@ public class StringAddCalculator {
             return splitCustomPattern(text);
         }
 
-        return text.split(",|:");
+        return text.split("[,|:]");
     }
 
     private boolean isMatchCustomPattern(String text) {
