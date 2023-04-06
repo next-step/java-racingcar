674ea2c51cc45af8dcb647d4dff526be73f72b9f
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-06 21:55:28 +0900

    refactor: 테스트코드 리팩토링 1. Number 객체 생성자를 이용한 입력값 전달 처리 2. @ParameterizedTest 를 이용한 다양한 입력값 테스트 구현 3. @NullAndEmptySource 를 이용한 null 과 빈문자열 입력값 테스트

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,110 +3,88 @@ package calculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("문자열 덧셈 계산기 테스트")
 public class StringAddCalculatorTest {
 
-    @DisplayName("빈 문자열 값을 입력 할 경우 0을 반환한다")
-    @Test
-    void givenEmptyStringWhenInputReturnZero() {
-        // given
-        String text = "";
+    @DisplayName("빈 문자열과 null 을 입력하면 0을 반환한다")
+    @ParameterizedTest
+    @NullAndEmptySource()
+    @ValueSource(strings = {" "})
+    void givenEmptyStringOrNullWhenInputReturnZero(String input) {
+        Number number = new Number(input);
 
-        // when
-        int result = StringAddCalculator.splitAndSum(text);
+        int result = StringAddCalculator.sumNumbers(number);
 
-        // then
-        assertThat(result).isEqualTo(0);
-    }
-
-    @DisplayName("null 값을 입력 할 경우 0을 반환한다")
-    @Test
-    void givenNullWhenInputReturnZero() {
-        // given
-        String text = null;
-
-        // when
-        int result = StringAddCalculator.splitAndSum(text);
-
-        // then
         assertThat(result).isEqualTo(0);
     }
 
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
-    @Test
-    void givenOneNumberStringWhenInputReturnNumber() {
-        // given
-        String text = "1";
+    @ParameterizedTest
+    @CsvSource(value = {"1=1", "19=19", "2023=2023"}, delimiter = '=')
+    void givenOneNumberStringWhenInputReturnNumber(String input, Integer expected) {
+        Number number = new Number(input);
 
-        // when
-        int result = StringAddCalculator.splitAndSum(text);
+        int result = StringAddCalculator.sumNumbers(number);
 
-        // then
-        assertThat(result).isEqualTo(1);
+        assertThat(result).isEqualTo(expected);
     }
 
     @DisplayName("숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다")
-    @Test
-    void givenTwoNumberStringWithCommaWhenInputReturnSum() {
-        // given
-        String text = "1,2";
+    @ParameterizedTest
+    @CsvSource(value = {"3,4=7", "1,2,3,6=12", "3,5,7,1,12=28"}, delimiter = '=')
+    void givenTwoNumberStringWithCommaWhenInputReturnSum(String input, Integer expected) {
+        Number number = new Number(input);
 
-        // when
-        int result = StringAddCalculator.splitAndSum(text);
+        int result = StringAddCalculator.sumNumbers(number);
 
-        // then
-        assertThat(result).isEqualTo(3);
+        assertThat(result).isEqualTo(expected);
     }
 
     @DisplayName("구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다")
-    @Test
-    void givenTwoNumberStringWithColonWhenInputReturnSum() {
-        // given
-        String text = "1:2";
+    @ParameterizedTest
+    @CsvSource(value = {"3:4=7", "1:2:3:6=12", "3:5:7:1:12=28"}, delimiter = '=')
+    void givenTwoNumberStringWithColonWhenInputReturnSum(String input, Integer expected) {
+        Number number = new Number(input);
 
-        // when
-        int result = StringAddCalculator.splitAndSum(text);
+        int result = StringAddCalculator.sumNumbers(number);
 
-        // then
-        assertThat(result).isEqualTo(3);
+        assertThat(result).isEqualTo(expected);
     }
 
     @DisplayName("구분자를 쉼표(,) 와 콜론(:)을 같이 사용 할 수 있다")
-    @Test
-    void givenTwoNumberStringWithCommaAndColonWhenInputReturnSum() {
-        // given
-        String text = "1,2:3,4";
+    @ParameterizedTest
+    @CsvSource(value = {"1,2:3=6", "3,4:5=12"}, delimiter = '=')
+    void givenTwoNumberStringWithCommaAndColonWhenInputReturnSum(String input, Integer expected) {
+        Number number = new Number(input);
 
-        // when
-        int result = StringAddCalculator.splitAndSum(text);
+        int result = StringAddCalculator.sumNumbers(number);
 
-        // then
-        assertThat(result).isEqualTo(10);
+        assertThat(result).isEqualTo(expected);
     }
 
-    @DisplayName("// 와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다")
     @Test
+    @DisplayName("// 와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다")
     void givenCustomDelimiterWhenInputReturnSum() {
-        // given
-        String text = "//;\n1;2;3";
+        Number number = new Number("//;\n1;2;3");
 
-        // when
-        int result = StringAddCalculator.splitAndSum(text);
+        int result = StringAddCalculator.sumNumbers(number);
 
-        // then
         assertThat(result).isEqualTo(6);
     }
 
     @DisplayName("숫자 이외의 값 또는 음수를 전달할 경우 RuntimeException 예외를 발생시킨다")
     @Test
     void givenNotNumberStringWhenInputReturnRuntimeException() {
-        // given
-        String text = "-1,-2,3,4,5,6,7,8,9,10,11";
+        Number number = new Number("-1,-9:3,4:5,Z,@");
 
-        // when & then
-        Assertions.assertThatThrownBy(() -> StringAddCalculator.splitAndSum(text))
+        Assertions.assertThatThrownBy(() -> StringAddCalculator.sumNumbers(number))
                 .isInstanceOf(RuntimeException.class);
     }
 }
