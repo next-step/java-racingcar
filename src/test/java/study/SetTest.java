65d7c942a9cd5c4ece70860c3dc84cd77dd9f25a
MIJIN KANG <icoqwee@gmail.com>
2023-04-06 19:53:11 +0900

    [step1] 1단계 - 학습 테스트 실습 (#4250)

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,51 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size()를 이용하여 Set의 원소 갯수 구하기 - 중복 원소는 제외")
+    void size_test() {
+        int actual = numbers.size();
+        int expected = 3;
+
+        assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains()를 이용하여 포함된 원소 확인")
+    void contains_test(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false","5:false"}, delimiter = ':')
+    @DisplayName("contains()를 이용하여 포함된 원소 확인")
+    void contains_test(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,44 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StringTest {
+    @Test
+    @DisplayName("split()을 이용하여 문자열 자르기")
+    void split_test() {
+        String data = "1,2";
+        String expected = "1";
+        String[] actual = data.split(",");
+        assertThat(actual).contains(expected);
+    }
+
+    @Test
+    @DisplayName("substring()을 이용하여 첫 번째와 마지막 문자 자르기")
+    void substring_test() {
+        String data = "(1,2)";
+        String expected = "1,2";
+        String actual = data.substring(1, data.length() - 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("charAt()을 이용하여 특정 위치의 문자 가져오기")
+    void charAt_test() {
+        String data = "abc";
+        char expected = 'b';
+        char result = data.charAt(1);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("charAt()을 이용하여 특정 위치의 문자를 가져올 때, 인덱스를 벗어나는 경우 StringIndexOutOfBoundsException 발생")
+    void charAt_test_exception() {
+        String data = "abc";
+        assertThrows(StringIndexOutOfBoundsException.class, () -> data.charAt(data.length()));
+    }
+}
