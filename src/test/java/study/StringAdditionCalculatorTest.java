ba5dd84ec6eb441f65a9f951a2579aae8cb959b8
seriouskang <scdean110@gmail.com>
2023-04-06 22:39:45 +0900

    refactor: 테스트 검증 로직 간소화(inline) 및 StringAdditionCalculator static import

--- src/test/java/study/StringAdditionCalculatorTest.java
+++ src/test/java/study/StringAdditionCalculatorTest.java
@@ -5,38 +5,27 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static study.StringAdditionCalculator.*;
 
 class StringAdditionCalculatorTest {
     @ParameterizedTest(name = "[{index}/2] {displayName}")
     @CsvSource(value = {"'',0", ",0"})
     @DisplayName("빈 문자열이나 null 값을 입력할 경우 0 반환")
-    void null_or_빈문자(String input, int expected) {
-        // given, when
-        int result = StringAdditionCalculator.splitAndSum(input);
-
-        // then
-        assertThat(result).isEqualTo(expected);
+    void null_혹은_빈문자(String input, int expected) {
+        assertThat(splitAndSum(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "[{index}/2] {displayName}")
     @CsvSource(value = {"0,0", "100,100"})
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환")
     void 숫자_하나(String input, int expected) {
-        // given, when
-        int result = StringAdditionCalculator.splitAndSum(input);
-
-        // then
-        assertThat(result).isEqualTo(expected);
+        assertThat(splitAndSum(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest(name = "[{index}/4] {displayName}")
     @CsvSource(value = {"1,10-11", "1,2,3-6", "1:10-11", "1:2,3-6"}, delimiter = '-')
     @DisplayName("컴마 혹은 콜론 구분자로 분리된 숫자들을 입력할 경우 해당 숫자들의 합 반환")
     void 컴마_혹은_콜론_구분자로_분리된_숫자들(String input, int expected) {
-        // given, when
-        int result = StringAdditionCalculator.splitAndSum(input);
-
-        // then
-        assertThat(result).isEqualTo(expected);
+        assertThat(splitAndSum(input)).isEqualTo(expected);
     }
 }
