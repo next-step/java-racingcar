aaed582ebab8f8512ac0712e8a80742cb679b528
dong <dong.22@kakaoenterprise.com>
2023-04-06 21:33:20 +0900

    feat : 패키지 이름 수정

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,6 +1,5 @@
-package step2;
+package calculator;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
