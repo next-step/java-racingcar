bd1095323946dfa609263631a83efbb99f5df3cb
ethan.v <ethan.v@kakaocorp.com>
2023-04-06 21:26:41 +0900

    step2
    - stream 사용해봄

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,19 +1,24 @@
 package step2;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class StringAddCalculator {
     private int calcStringWithDel(String text, String del) {
-        int sum = 0;
-        for (String s : text.split(del)) {
-            int num = Integer.parseInt(s);
-            if (num < 0) {
-                throw new RuntimeException("음수값은 처리할 수 없습니다.");
-            }
-            sum += num;
+        Stream<String> stream = Arrays.stream(text.split(del));
+        int result = stream
+                .mapToInt(Integer::parseInt)
+                .peek(this::checkNegativeNum)
+                .sum();
+        return result;
+    }
+
+    private void checkNegativeNum(int num) {
+        if (num < 0) {
+            throw new RuntimeException("음수값은 처리할 수 없습니다.");
         }
-        return sum;
     }
 
     public int splitAndSum(String text) {
