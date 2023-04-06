7e7596b13763fc750b61f2761faa2df09ac901d1
garden-2 <1eejw@naver.com>
2023-04-06 15:56:17 +0900

    feat: String 요구사항 2 - substring 메소드를 활용해 () 제거할 수 있다.

--- src/test/java/study/StringStudyTest.java
+++ src/test/java/study/StringStudyTest.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StringStudy {
+public class StringStudyTest {
 
     @DisplayName("문자열 '1,2'를 ',' 기준으로 분리할 수 있다")
     @Test
@@ -23,5 +23,16 @@ public class StringStudy {
                 .collect(Collectors.toList());
 
         assertThat(numbers).contains(one, two);
+    }
+
+    @DisplayName("문자열 '(1,2)'를 '()'를 제거하고 '1,2'를 반환할 수 있다")
+    @Test
+    void substringTest() {
+        String numbersWithParentheses = "(1,2)";
+        String numbersWithoutParentheses = "1,2";
+
+        final String result = numbersWithParentheses.substring(1, numbersWithParentheses.length() - 1);
+
+        assertThat(result).isEqualTo(numbersWithoutParentheses);
     }
 }
