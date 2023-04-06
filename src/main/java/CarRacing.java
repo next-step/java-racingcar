7eb639ea3f571626821542f39819f78afd46cd34
KimJuHyun <rlawngus0910@naver.com>
2023-04-06 22:49:12 +0900

    feat: 자동차 수만큼 Car 클래스 생성

--- src/main/java/CarRacing.java
+++ src/main/java/CarRacing.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarRacing {
     public static void main(String[] args) {
 
         int numberOfCar = InputView.numberOfCar();
         int trial = InputView.trial();
+
+        List<Car> cars = cars(numberOfCar);
+    }
+
+    private static List<Car> cars(int number) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            cars.add(new Car());
+        }
+
+        return cars;
     }
 }
