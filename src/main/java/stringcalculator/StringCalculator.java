7b27e4db69da331026db6dadd395e2150ceb7869
garden-2 <1eejw@naver.com>
2023-04-06 18:13:46 +0900

    feat: 구분자가 없는 문자열을 입력해서 문자열계산기를 생성할 수 없다.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,10 +1,45 @@
 package stringcalculator;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("^(/{2}.\\n).*");
+    private static final String COMMA = ",";
+    private static final String COLON = ":";
+    private final Set<String> delimiters = new HashSet<>();
     private final String text;
 
     public StringCalculator(String text) {
+        initializeDelimiters();
+        delimiterValidate(text);
         this.text = text;
+    }
+
+    private void initializeDelimiters() {
+        delimiters.add(COMMA);
+        delimiters.add(COLON);
+    }
+
+    private void delimiterValidate(String text) {
+        if (containsCustomDelimiter(text)) {
+            final String customDelimiter = String.valueOf(text.charAt(2));
+            delimiters.add(customDelimiter);
+        }
+        if (containsNotAnyDelimiters(text)) {
+            throw new IllegalArgumentException("구분자가 포함되어 있지 않습니다.");
+        }
+    }
+
+    private boolean containsCustomDelimiter(String text) {
+        return CUSTOM_DELIMITER_PATTERN.matcher(text)
+                .find();
+    }
+
+    private boolean containsNotAnyDelimiters(String text) {
+        return delimiters.stream()
+                .noneMatch(text::contains);
     }
 
     public int sum() {
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -5,17 +5,31 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class StringCalculatorTest {
 
     @DisplayName("문자열을 입력해서 문자열계산기를 생성할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = "123")
+    @ValueSource(strings = {"1:2", "1,2", "1:2,3", "1,2,3", "1:2:3", "//+\n1+2+3"})
     void createStringCalculator(String text) {
-
-        assertThatCode(() -> {
-            final StringCalculator stringCalculator = new StringCalculator(text);
-        }).doesNotThrowAnyException();
+        assertThatCode(() -> new StringCalculator(text))
+                .doesNotThrowAnyException();
     }
 
+    @DisplayName("커스텀 구분자를 포함한 문자열을 입력해서 문자열계산기를 생성할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"//+\n1+2+3", "//-\n1-2-3", "//;\n1;2;3"})
+    void createStringCalculatorWithCustomDelimiter(String text) {
+        assertThatCode(() -> new StringCalculator(text))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("구분자가 없는 문자열을 입력해서 문자열계산기를 생성할 수 없다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"/+\n1+2+3", "ABC+\n22"})
+    void createStringCalculatorWithInvalidText(String text) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new StringCalculator(text)).withMessage("구분자가 포함되어 있지 않습니다.");
+    }
 }
