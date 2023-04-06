2790a1e1937f9d2839d0e52e2d87b444764f0ec8
dong <dong.22@kakaoenterprise.com>
2023-04-06 21:47:07 +0900

    feat : 기능대로 실패하는 테스트 추가

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -23,4 +23,82 @@ public class StringAddCalculatorTest {
         //when
         //then
     }
+
+    @DisplayName("null 값 입력시 0을 반환해야 한다")
+    @Test
+    public void nullStringInput() {
+        log.info("예시 : null >> 0");
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    @Test
+    public void 숫자_하나() {
+        log.info("예시 : 1 >> 1");
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("10 이상의 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    @Test
+    public void 숫자_10이상_하나() {
+        log.info("예시 : 10 >> 10");
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("자릿수가 긴 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    @Test
+    public void 숫자_큰걸로_하나() {
+        log.info("예시 : 100 >> 100");
+        //given
+        //when
+        //then
+    }
+
+
+
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)")
+    @Test
+    public void run() {
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)")
+    @Test
+    public void run() {
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\\n1;2;3” => 6)")
+    @Test
+    public void run() {
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)")
+    @Test
+    public void run() {
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("")
+    @Test
+    public void run() {
+        //given
+        //when
+        //then
+    }
 }
