418d305805486b27dbcdf5b22195ab01461717c0
ziaryu <jaryu@dozn.co.kr>
2023-04-07 15:58:15 +0900

    구체 클래스 없애고, 테스트 코드 재작성

--- src/test/java/StringClassTest.java
+++ src/test/java/StringClassTest.java
@@ -6,41 +6,33 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringClassTest {
 
+    final int BUFFER = 1;
+
     @DisplayName("1,2를 분리")
     @Test
-    void string1_1() {
-        StringClass stringClass = new StringClass();
-
-        int[] res = stringClass.split("1,2", ",");
-        assertThat(res).contains(1, 2);
+    void 문자열분리_1() {
+        String input = "1,2";
+        assertThat(input.split(",")).containsExactly("1", "2");
     }
 
     @DisplayName("1을 분리")
     @Test
-    void string1_2() {
-        StringClass stringClass = new StringClass();
-
-        int[] res = stringClass.split("1", ",");
-
-        assertThat(res).containsExactly(1);
+    void 문자열분리_2() {
+        String input = "1";
+        assertThat(input.split(",")).containsExactly("1");
     }
 
     @DisplayName("(1,2) 주어지면 `1,2`")
     @Test
-    void string2() {
-        StringClass stringClass = new StringClass();
-
-        String res = stringClass.substring("(1,2)");
-
-        assertThat(res).contains("1,2");
+    void 문자열분리_3() {
+        String input = "(1,2)";
+        assertThat(input.substring(BUFFER, input.length() - BUFFER)).contains("1,2");
     }
 
     @DisplayName("abc주어지면 charAt() 사용하여 특정 위치 문자 가져오기")
     @Test
     void string3_1() {
-
         String input = "abc";
-
         assertThat(input.charAt(0)).isEqualTo('a');
         assertThat(input.charAt(1)).isEqualTo('b');
     }
@@ -48,7 +40,6 @@ public class StringClassTest {
     @DisplayName("abc주어지면 charAt() 사용하여 특정 위치 문자 가져오기")
     @Test
     void string3_Throw() {
-
         String input = "abc";
         assertThatThrownBy(() -> input.charAt(5))
                 .isInstanceOf(StringIndexOutOfBoundsException.class);
