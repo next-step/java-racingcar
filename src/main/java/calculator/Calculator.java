4ea25c16a9de0fed96a0deb0d6373e8d3734ee95
saerang <lsg10119@gamail.com>
2023-04-07 23:51:40 +0900

    feat: 양수의 값이 들어오지 않으면 예외를 발생

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,14 +3,15 @@ package calculator;
 public class Calculator {
 
     public static final int ZERO = 0;
-    public static final String DEFAULT_DELIMITER = ",|:";
+    public static final String DEFAULT_DELIMITER = "[,:]";
+    public static final String POSITIVE_NUMBER = "[0-9]";
 
     public static int splitAndSum(String input) {
         if (isEmpty(input)) {
             return ZERO;
         }
 
-        return sum(parse(split(input)));
+        return sum(toPositiveNumbers(split(input)));
     }
 
     private static boolean isEmpty(String input) {
@@ -21,18 +22,26 @@ public class Calculator {
         return input.split(DEFAULT_DELIMITER);
     }
 
-    private static int[] parse(String[] numbers) {
+    private static int[] toPositiveNumbers(String[] numbers) {
         int[] result = new int[numbers.length];
 
         for (int i = 0; i < numbers.length; i++) {
-            result[i] = Integer.parseInt(numbers[i]);
+            result[i] = toPositiveNumber(numbers[i]);
         }
 
         return result;
     }
 
+    private static int toPositiveNumber(String number) {
+        if (!number.matches(POSITIVE_NUMBER)) {
+            throw new RuntimeException(number + "는 양수가 아닙니다.");
+        }
+
+        return Integer.parseInt(number);
+    }
+
     private static int sum(int[] numbers) {
-        int result = 0;
+        int result = ZERO;
 
         for (int number : numbers) {
             result += number;
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,9 +3,11 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorTest {
 
@@ -30,11 +32,19 @@ public class CalculatorTest {
         assertThat(result).isEqualTo(1);
     }
 
-    @ParameterizedTest(name = "숫자 두개와 구분자(, :)가 입력될 경우 두 숫자의 합을 반환한다.")
-    @ValueSource(strings = {"1,2", "1:2"})
-    void test03(String input) {
+    @ParameterizedTest(name = "숫자와 구분자(, :)가 입력될 경우 숫자의 합을 반환한다.")
+    @CsvSource(value = {"1,2|3", "1:2|3", "1,2,3|6"}, delimiter = '|')
+    void test03(String input, int expected) {
         int result = Calculator.splitAndSum(input);
 
-        assertThat(result).isEqualTo(3);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("숫자 이외의 값을 전달하는 경우 RuntimeException 에러 발생한다.")
+    @ParameterizedTest(name = "양의 숫자 이외의 값을 전달하는 경우 RuntimeException 에러 발생한다.")
+    @ValueSource(strings = {"-1", "A", "-1,5"})
+    void test04(String input) {
+        assertThatThrownBy(() -> Calculator.splitAndSum(input))
+                .isExactlyInstanceOf(RuntimeException.class).hasMessageContaining("는 양수가 아닙니다.");
     }
 }
