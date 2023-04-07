931b575bd3da0a8c34ce289efba2781db3f92d9d
garden-2 <1eejw@naver.com>
2023-04-07 10:13:19 +0900

    fix: displayName을 parameterizedTest의 name 속성으로 수정

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -8,32 +8,28 @@ import static org.assertj.core.api.Assertions.*;
 
 class StringCalculatorTest {
 
-    @DisplayName("문자열을 입력해서 문자열계산기를 생성할 수 있다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "문자열을 입력해서 문자열계산기를 생성할 수 있다.")
     @ValueSource(strings = {"1:2", "1,2", "1:2,3", "1,2,3", "1:2:3", "//+\\n1+2+3"})
     void createStringCalculator(String text) {
         assertThatCode(() -> new StringCalculator(text))
                 .doesNotThrowAnyException();
     }
 
-    @DisplayName("커스텀 구분자를 포함한 문자열을 입력해서 문자열계산기를 생성할 수 있다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "커스텀 구분자를 포함한 문자열을 입력해서 문자열계산기를 생성할 수 있다.")
     @ValueSource(strings = {"//+\\n1+2+3", "//-\\n1-2-3", "//;\\n1;2;3", "//;\\n1;2;3"})
     void createStringCalculatorWithCustomDelimiter(String text) {
         assertThatCode(() -> new StringCalculator(text))
                 .doesNotThrowAnyException();
     }
 
-    @DisplayName("구분자가 없는 문자열을 입력해서 문자열계산기를 생성할 수 없다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "구분자가 없는 문자열을 입력해서 문자열계산기를 생성할 수 없다.")
     @ValueSource(strings = {"/+\n1+2+3", "ABC+\n22"})
     void createStringCalculatorWithInvalidText(String text) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new StringCalculator(text)).withMessage("구분자를 제외하고 숫자가 아닌 문자는 입력할 수 없습니다.");
     }
 
-    @DisplayName("구분자를 제외하고 숫자가 아닌 문자는 입력할 수 없습니다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "구분자를 제외하고 숫자가 아닌 문자는 입력할 수 없습니다.")
     @ValueSource(strings = {"1,2A,3", "AA12:2:3", "//A\n1A22:3BB"})
     void createStringCalculatorWithInvalidText2(String text) {
         assertThatIllegalArgumentException()
@@ -41,8 +37,7 @@ class StringCalculatorTest {
                 .withMessage("구분자를 제외하고 숫자가 아닌 문자는 입력할 수 없습니다.");
     }
 
-    @DisplayName("입력한 문자열의 합을 구할 수 있다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "입력한 문자열의 합을 구할 수 있다.")
     @ValueSource(strings = {"1,2,3", "1:2:3", "//+\\n1+2+3", "//{\\n1{2,3", "//}\\n1}2:3", "//(\\n1(2:3", "//)\\n1)2:3", "//[\\n1[2:3", "//]\\n1]2:3", "//A\\n1A2:3"})
     void sumTest(String text) {
         int expectedSum = 6;
@@ -51,8 +46,7 @@ class StringCalculatorTest {
         assertThat(stringCalculator.sum()).isEqualTo(expectedSum);
     }
 
-    @DisplayName("음수를 입력할 수 없다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "음수를 입력할 수 없다.")
     @ValueSource(strings = {"1:-2", "-1,2", "//+\\n1+-2+3"})
     void negativeNumbersNotAvailable(String text) {
         assertThatIllegalArgumentException()
@@ -60,8 +54,7 @@ class StringCalculatorTest {
                 .withMessage("음수를 입력할 수 없습니다.");
     }
 
-    @DisplayName("null 이나 빈문자의 계산값은 0 이다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "null 이나 빈문자의 계산값은 0 이다.")
     @ValueSource(strings = {"  ", ""})
     void nullOrEmptyStringIsZero(String text) {
         int expectedSum = 0;
@@ -69,8 +62,7 @@ class StringCalculatorTest {
         assertThat(new StringCalculator(text).sum()).isEqualTo(expectedSum);
     }
 
-    @DisplayName("구분자 없이 숫자인 하나의 문자만 입력된다면 계산값은 입력한 값이다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "구분자 없이 숫자인 하나의 문자만 입력된다면 계산값은 입력한 값이다.")
     @ValueSource(strings = {"1", "20"})
     void onlyOneStringWithoutOperator(String text) {
         int expectedSum = Integer.parseInt(text);
