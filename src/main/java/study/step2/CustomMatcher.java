9bcfb9ee8353880fd0438579240a894b4f2afbdd
jm.hong <jm.hong@kakaocorp.comm>
2023-04-07 21:19:36 +0900

    refactor
    빌드패턴 생성자로 변경

--- src/main/java/study/step2/CustomMatcher.java
+++ src/main/java/study/step2/CustomMatcher.java
@@ -5,6 +5,10 @@ import java.util.regex.Matcher;
 public class CustomMatcher {
   private final Matcher matcher;
 
+  public CustomMatcher(Matcher matcher) {
+    this.matcher = matcher;
+  }
+
   public void findCustomDelimiter() {
     if (!matcher.find()) {
       throw new IllegalArgumentException("커스텀 구분자를 구별할 수 없습니다.");
@@ -14,23 +18,4 @@ public class CustomMatcher {
   public String group(int group) {
     return this.matcher.group(group);
   }
-
-  public static class Builder {
-
-    private Matcher matcher;
-
-    public Builder matcher(Matcher matcher) {
-      this.matcher = matcher;
-      return this;
-    }
-
-    public CustomMatcher build() {
-      return new CustomMatcher(this);
-    }
-  }
-
-  public CustomMatcher(Builder builder) {
-    this.matcher = builder.matcher;
-  }
-
 }
