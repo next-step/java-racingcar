27680eaa04ce83b9402e336d119692f129024e84
juhyun <zzang9ha@naver.com>
2023-04-07 21:21:22 +0900

    feat: InputView 추가

---
+++ src/main/java/step3/InputView.java
@@ -0,0 +1,25 @@
+package step3;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public static int inputCar() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+
+        return SCANNER.nextInt();
+    }
+
+    public static int inputRetryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+
+        return SCANNER.nextInt();
+    }
+
+
+}
