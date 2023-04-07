46a41bc61a30853868313f45b3053f57b62f1772
ghkdwlgns612 <ghkdwlgns612@naver.com>
2023-04-07 11:31:54 +0900

    숫자 이외의 값 테스트 코드 작성 + @MethodResource 활용

--- src/test/java/com/next/step/step2/StringAddCalculatorTest.java
+++ src/test/java/com/next/step/step2/StringAddCalculatorTest.java
@@ -1,16 +1,15 @@
 package com.next.step.step2;
 
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class StringAddCalculatorTest {
 
     @ParameterizedTest(name = "입력이 NULL 또는 빈 문자열인 경우")
@@ -42,16 +41,30 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
-    @Test
-    @DisplayName("입력에 커스텀 구분자가 포함된 경우")
-    public void splitAndSum_custom_delimiter_input() {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
-        assertThat(result).isEqualTo(6);
+    @ParameterizedTest(name = "입력에 커스텀 구분자가 포함된 경우")
+    @MethodSource("provideCustomDelimiter")
+    public void splitAndSum_custom_delimiter_input(String input, int expected) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private Stream<Arguments> provideCustomDelimiter() {
+        return Stream.of(
+                Arguments.of("//;\n1;2;3", 6),
+                Arguments.of("//~\n1~2~3", 6)
+        );
     }
 
     @ParameterizedTest(name = "입력에 음수가 포함된 경우")
-    @ValueSource(strings = {"-1,2,3","-1:2:3","-100,-200,-300"})
+    @ValueSource(strings = {"-1,2,3", "-1:2:3", "-100,-200,-300"})
     public void splitAndSum_negative_input(String input) {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(input))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest(name = "입력에 숫자이외의 값이 포함된 경우")
+    @ValueSource(strings = {"-a,2,3", "-1:b:3", "-100,-200,-c"})
+    public void splitAndSum_not_number_input(String input) {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum(input))
                 .isInstanceOf(RuntimeException.class);
     }
