6c54e908498cb50f0453a4fe72a610e0130d7790
dong <dong.22@kakaoenterprise.com>
2023-04-08 00:35:48 +0900

    test : 스텝3의 요구사항 기반의 테스트코드 껍데기만 작성

---
+++ src/test/java/carracing/race/RaceApplicationTest.java
@@ -0,0 +1,55 @@
+package carracing.race;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RaceApplicationTest {
+
+    @DisplayName("n대의 자동차는 전진 또는 멈출 수 있다")
+    @Test
+    public void carCanMoveOrStop() {
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("몇대의 자동차로 이동할수 있는지 지정할 수 있다")
+    @Test
+    public void countOfMovingCar() {
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("몇번을 이동할것인지 입력할 수 있다")
+    @Test
+    public void movingIteration() {
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("0에서 9사이에서 random값을 구한다")
+    @Test
+    public void getRandomFrom0To9() {
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("3 초과(=4이상) 일 경우 전진한다")
+    @Test
+    public void forwardOver4() {
+        //given
+        //when
+        //then
+    }
+
+    @DisplayName("4 미만(=3이하) 일 경우 정지한다")
+    @Test
+    public void stopUnder4() {
+        //given
+        //when
+        //then
+    }
+}
