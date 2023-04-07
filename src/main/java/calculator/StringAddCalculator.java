94f2d5a887916e13a888d40d1da50db03022ffa8
moto3d <ultraraptor@naver.com>
2023-04-07 13:39:53 +0900

    delete : 수업때 배운거 버리고 제로베이스 시작

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,21 +4,9 @@ public class StringAddCalculator {
 
     private static final int ZERO = 0;
 
-    private void sum() {
-
-    }
-
-    private void isBlank() {
-
-    }
-
-    private int[] toIntArray() {
-
-    }
 
 
-
-    public static int splitAndSum(String text) {
+    public int splitAndSum(String text) {
         int sum = 0;
 
         return sum;
