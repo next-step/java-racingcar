5145f85bf1627efb9e042d89aa987d174a7d2170
juhyun <zzang9ha@naver.com>
2023-04-07 18:20:52 +0900

    feat: Car 객체 생성

---
+++ src/main/java/step3/Car.java
@@ -0,0 +1,14 @@
+package step3;
+
+public class Car {
+
+    private int distance;
+
+    public Car() {
+        this.distance = 0;
+    }
+
+    public void forwardCar() {
+        this.distance++;
+    }
+}
