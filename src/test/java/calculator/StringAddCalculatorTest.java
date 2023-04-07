69b6a79db09954899de12d9c692409f10a581913
moto3d <ultraraptor@naver.com>
2023-04-07 09:18:17 +0900

    feat : 테스코드 추가 : 음수처리에 대한 예외사항 추가

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -65,34 +65,46 @@ public class StringAddCalculatorTest {
 
 
 
-    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)")
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다")
     @Test
-    public void run() {
+    public void 컴마구분자() {
         //given
         //when
         //then
     }
 
-    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)")
+    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다")
     @Test
-    public void run() {
+    public void 콜론구분자() {
         //given
         //when
         //then
     }
 
-    @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\\n1;2;3” => 6)")
+    @DisplayName("구분자를 컴마(,) 와 콜론(:)을 함께 사용할 수 있다")
     @Test
-    public void run() {
+    public void 콜론과_컴마구분자() {
         //given
         //when
         //then
     }
 
-    @DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)")
+    @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : ")
     @Test
-    public void run() {
+    public void 커스텀구분자() {
         //given
+        String dataset = "//;\n1;2;3";
+        int answer = 6;
+        //when
+        //then
+    }
+
+    @DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생해야 한다")
+    @Test
+    public void 음수예외() {
+        //given
+         String dataset = "-1,2,3";
+
         //when
         //then
     }
