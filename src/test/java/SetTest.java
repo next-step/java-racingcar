49b7e8d46c3a7993f0d2f3e1336b59c5c13e2230
hovy1994 <37677421+hovy1994@users.noreply.github.com>
2023-04-07 20:58:07 +0900

    Step1: 학습 테스트 실습 (#4114)
    * String 테스트 - 요구사항 1
    
    * String 테스트 - 요구사항 2
    
    * String 테스트 - 요구사항 3
    
    * Set Collection 테스트 - 요구사항 1
    
    * Set Collection 테스트 - 요구사항 2
    
    * Set Collection 테스트 - 요구사항 3
    
    * refactoring
    
    ---------
    
    Co-authored-by: lily-yu <lily.yu@kakaocorp.com>

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 크기를 확인할 수 있어야 한다.")
+    void testGetSizeOfSet() {
+        assertEquals(3, numbers.size());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set 안에 특정 값이 존재하는지 확인할 수 있어야 한다.")
+    @ValueSource(ints = {1, 2, 3})
+    void testFindValueInSet(int input) {
+        assertTrue(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Set 안에 특정 값이 존재하는지 안 하는지 확인할 수 있어야 한다.")
+    @CsvSource(value = {"1:TRUE", "3:TRUE", "5:FALSE"}, delimiter=':')
+    void testFindValueInSet(int input, boolean expected) {
+        assertEquals(expected, numbers.contains(input));
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("숫자로 컴마로 이루어진 문자열을 입력받아 컴마로 숫자를 분리해서 정수 배열을 반환해야 한다.")
+    void testSeparateByComma() {
+        String input = "1,2,3";
+
+        String[] result = input.split(",");
+
+        assertThat(result).contains("1", "2", "3");
+    }
+
+    @Test
+    @DisplayName("괄호로 감싸진 문자열을 입력받아 괄호가 제거된 문자열을 반환해야 한다.")
+    void testExtractStringFromBracket() {
+        String input = "(1,2,3)";
+
+        String result = input.substring(1, input.length() - 1);
+
+        assertEquals("1,2,3", result);
+    }
+
+    @Test
+    @DisplayName("문자열의 특정 위치를 입력받아 그 위치에 해당하는 문자를 반환해야 한다.")
+    void testGetCharacterAtIndex(){
+        String input = "abc";
+
+        assertEquals('c', input.charAt(2));
+        assertThrows(StringIndexOutOfBoundsException.class, ()-> input.charAt(-1));
+    }
+}
