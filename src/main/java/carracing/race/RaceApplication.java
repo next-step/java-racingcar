4c049ad03f90c3d8a0866130d13123be9510060a
dong <dong.22@kakaoenterprise.com>
2023-04-08 17:37:56 +0900

    feat : 1차 자동차경주게임 작성

--- src/main/java/carracing/race/RaceApplication.java
+++ src/main/java/carracing/race/RaceApplication.java
@@ -1,8 +1,11 @@
 package carracing.race;
 
+import carracing.race.logic.AutomobileFederation;
 import carracing.race.ui.InputView;
 import carracing.race.ui.ResultView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -21,12 +24,24 @@ public class RaceApplication {
         int participate = inputView.participate();
         int iterations = inputView.iterations();
         Map<Integer, List<Integer>> raceResults = this.racingStart(participate, iterations);
-        resultView.resultview(raceResults);
+        resultView.printResult(new AutomobileFederation(raceResults));
 
     }
 
     public Map<Integer, List<Integer>> racingStart(int participate, int iterations) {
-        return null;
+
+        Map<Integer, List<Integer>> resultMap = new HashMap<>();
+
+        for (int stage = 1; stage <= iterations; stage++) {
+            //todo : 로직이 이중포문에 더티하게 섞여있어서 매우 복잡하다 리팩토링 필요
+            List<Integer> raceRecode = new ArrayList<>();
+            for (int i = 0; i < participate; i++) {
+
+                raceRecode.add(getRandom());
+            }
+            resultMap.put(stage, raceRecode);
+        }
+        return resultMap;
     }
 
     public int getRandom() {
