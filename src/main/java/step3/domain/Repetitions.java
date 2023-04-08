63d9d5cbfdca451e831d9a32062af20398f12b5c
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-08 17:59:05 +0900

    refactor: 자동차 경기 기록 저장시 자동차 목록은 깊은 복사로

--- src/main/java/step3/domain/Repetitions.java
+++ src/main/java/step3/domain/Repetitions.java
@@ -1,12 +1,18 @@
 package step3.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Repetitions {
-    private int repetitions;
-    private List<RacingCar> racingCarList;
+public class Repetitions implements Cloneable {
 
-    public static final Repetitions _INSTANCE = new Repetitions();
+    private final int repetitions;
+    private final List<RacingCar> racingCarList;
+
+    private Repetitions(int repetitions, List<RacingCar> racingCarList) {
+        this.repetitions = repetitions;
+        this.racingCarList = racingCarList;
+    }
+
 
     public int getRepetitions() {
         return repetitions;
@@ -16,19 +22,15 @@ public class Repetitions {
         return racingCarList;
     }
 
-    public boolean isLastReps(int numberOfReps) {
-        return repetitions == numberOfReps;
+    public static Repetitions recordOf(int repetitions, List<RacingCar> racingCarList) {
+
+        return new Repetitions(repetitions, copyRacingCarList(racingCarList));
     }
 
-    public Repetitions record(List<RacingCar> racingCarList) {
-        increaseReps();
-        this.racingCarList = racingCarList;
-
-        return this;
-    }
-
-    private void increaseReps() {
-        repetitions++;
+    private static List<RacingCar> copyRacingCarList(List<RacingCar> racingCarList) {
+        return racingCarList.stream()
+                .map(RacingCar::copyOf)
+                .collect(Collectors.toList());
     }
 }
 
