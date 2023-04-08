18312d2144e12f0225685551435ba3c5380b6e95
dong <dong.22@kakaoenterprise.com>
2023-04-08 16:03:25 +0900

    test : Input ui testcode add

--- src/test/java/carracing/race/ui/InputViewTest.java
+++ src/test/java/carracing/race/ui/InputViewTest.java
@@ -24,23 +24,27 @@ class InputViewTest {
         //given
         String input = "5";
         int answer = 5;
-        inAndOutTestHelper(input);
+
         //when
+        inAndOutTestHelper(input);
         int result = inputView.participate();
+
         //then
         assertEquals(result, answer);
     }
 
+    @DisplayName("경기를 반복회는 횟수를 입력받는다")
     @Test
-    public void testSystemOutPrintln() {
-        // 출력하려는 문자열을 작성합니다.
-        String testString = "Hello, world!";
-        // 문자열을 출력합니다.
-        System.out.print(testString);
-        inputView.iterations();
-        // SystemOutRule 객체를 사용하여 출력된 문자열을 가져옵니다.
-        //String outputString = systemOutRule.getLog();
-        // 예상되는 출력 문자열과 일치하는지 확인합니다.
-        //assertEquals(testString, outputString);
+    public void iteration() {
+        //given
+        String input = "7";
+        int answer = 7;
+
+        //when
+        inAndOutTestHelper(input);
+        int result = inputView.participate();
+
+        //then
+        assertEquals(result, answer);
     }
 }
