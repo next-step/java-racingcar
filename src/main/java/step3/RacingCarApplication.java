e4bf75071cdc83e85123e3eea1b4302af3230575
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-08 18:00:47 +0900

    rafactor: 코드 리팩토링

--- src/main/java/step3/RacingCarApplication.java
+++ src/main/java/step3/RacingCarApplication.java
@@ -1,14 +1,14 @@
 package step3;
 
-import step3.strategy.RandomMoveStrategy;
+import step3.strategy.draw.UnderscoreDrawStrategy;
+import step3.strategy.move.RandomMoveStrategy;
 import step3.view.InputView;
 import step3.view.PrintView;
 
 public class RacingCarApplication {
     public static void main(String[] args) {
 
-        RacingGame racingGame = new RacingGame(new InputView(), new PrintView(), new RandomMoveStrategy());
-
+        RacingGame racingGame = RacingGame.of(InputView.getInstance(), PrintView.of(UnderscoreDrawStrategy.getInstance()), RandomMoveStrategy.getInstance());
         racingGame.execute();
     }
 
--- src/main/java/step3/RacingGame.java
+++ src/main/java/step3/RacingGame.java
@@ -1,29 +1,27 @@
 package step3;
 
-import step3.domain.Position;
 import step3.domain.RacingCar;
+import step3.domain.RacingInformation;
 import step3.domain.Repetitions;
-import step3.strategy.draw.DrawStrategy;
 import step3.strategy.move.MoveStrategy;
 import step3.view.InputView;
 import step3.view.PrintView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
 
 public class RacingGame {
+    private static final String FORCED_TERMINATION_USER_GUIDE = "사용자에 의해 프로그램이 종료되었습니다";
 
-    private static final boolean CONTINUE_FLAG = true;
-    private static final boolean FINISH_FLAG = false;
-    private static final int RACING_CAR_FIRST_INDEX = 0;
+    private static final int ZERO = 0;
     private final InputView inputView;
     private final PrintView printView;
     private final MoveStrategy moveStrategy;
-    private final DrawStrategy drawStrategy;
-    private boolean flag;
 
-
-    public RacingGame(
+    private RacingGame(
             InputView inputView
             , PrintView printView
             , MoveStrategy moveStrategy
@@ -33,51 +31,42 @@ public class RacingGame {
         this.moveStrategy = moveStrategy;
     }
 
+    public static RacingGame of(
+            InputView inputView
+            , PrintView printView
+            , MoveStrategy moveStrategy
+    ) {
+        return new RacingGame(inputView, printView, moveStrategy);
+    }
+
     public void execute() {
-        play(prepare());
+        RacingInformation racingInformation = inputView.execute();
+        List<Repetitions> repetitionsList = play(makeRacingCarList(racingInformation), racingInformation);
+        printView.printAll(repetitionsList);
     }
 
-    public List<RacingCar> prepare() {
-        showIntro();
-        switchFlag(CONTINUE_FLAG);
-        return makeRacingCarList();
-    }
+    public List<Repetitions> play(List<RacingCar> racingCarList, RacingInformation racingInformation) {
+        List<Repetitions> repetitionsList = new ArrayList<>();
+        IntStream.range(ZERO, racingInformation.getNumberOfReps())
+                .forEach(reps -> {
+                    racingCarList.forEach(RacingCar::move);
+                    repetitionsList.add(Repetitions.recordOf(reps, Collections.unmodifiableList(racingCarList)));
+                });
 
-    public void play(List<RacingCar> racingCarList) {
-
-        while (flag) {
-            racingCarList.forEach(RacingCar::move);
-            Repetitions repetitions = Repetitions._INSTANCE.record(racingCarList);
-            printView.print(repetitions);
-            isContinue(repetitions);
-        }
+        return repetitionsList;
     }
 
     public void stop() {
-        switchFlag(FINISH_FLAG);
+        throw new RuntimeException(FORCED_TERMINATION_USER_GUIDE);
     }
 
-    private void isContinue(Repetitions repetitions) {
-        if (repetitions.isLastReps(inputView.getNumberOfReps())) {
-            stop();
+    private List<RacingCar> makeRacingCarList(RacingInformation racingInformation) {
+        List<RacingCar> racingCarList = new ArrayList<>();
+
+        for (int racingCarIndex = ZERO; racingCarIndex < racingInformation.getNumberOfCars(); racingCarIndex++) {
+            racingCarList.add(RacingCar.of(moveStrategy));
         }
-    }
 
-    private void showIntro() {
-        inputView.requestNumberOfCar();
-        inputView.requestNumberOfTry();
-    }
-
-    private void switchFlag(boolean flag) {
-        this.flag = flag;
-    }
-
-    private List<RacingCar> makeRacingCarList() {
-        ArrayList<RacingCar> racingCarList = new ArrayList<>();
-
-        for (int index = RACING_CAR_FIRST_INDEX; index < inputView.getNumberOfCars(); index++) {
-            racingCarList.add(RacingCar.of(moveStrategy, Position.getInstance()));
-        }
         return racingCarList;
     }
 }
