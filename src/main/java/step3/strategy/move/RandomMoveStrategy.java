6b15dee8bf2672918935d589f10b18ee33f1a58b
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-08 16:16:59 +0900

    refactor: [코드리뷰 반영] thread safe한 ThreadLocalRandom로 변경

--- src/main/java/step3/strategy/move/RandomMoveStrategy.java
+++ src/main/java/step3/strategy/move/RandomMoveStrategy.java
@@ -1,10 +1,11 @@
 package step3.strategy.move;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomMoveStrategy implements MoveStrategy {
 
-    private final static Random RANDOM = new Random();
+    private final static ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
     private final static int RANDOM_BOUND = 10;
     private final static int THRESHOLD = 4;
 
