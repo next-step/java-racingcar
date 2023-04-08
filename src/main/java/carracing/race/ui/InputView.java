01ccb4c5483e664a609b03936400f43d6025912c
dong <dong.22@kakaoenterprise.com>
2023-04-08 16:04:05 +0900

    feat : inputUI component impl

--- src/main/java/carracing/race/ui/InputView.java
+++ src/main/java/carracing/race/ui/InputView.java
@@ -1,4 +1,30 @@
 package carracing.race.ui;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class InputView {
+
+    private final Scanner scanner;
+    private final PrintStream printStream;
+
+    public static final String PARTICIPAT = "자동차 대수는 몇 대 인가요?";
+
+    public static final String ITERATIONS = "시도할 회수는 몇 회 인가요?";
+
+    public InputView(InputStream inputStream, PrintStream printStream) {
+        this.scanner = new Scanner(inputStream);
+        this.printStream = printStream;
+    }
+
+    public int participate() {
+        printStream.println(PARTICIPAT);
+        return scanner.nextInt();
+    }
+
+    public int iterations() {
+        System.out.println(ITERATIONS);
+        return scanner.nextInt();
+    }
 }
