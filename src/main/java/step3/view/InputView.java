df18b55c02f510688f6cee55f602223328f1be78
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-08 16:04:25 +0900

    refactor: 레이싱게임 정보의 상태값 제거 및 InputView의 책임을 더 명확하게

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -1,5 +1,7 @@
 package step3.view;
 
+import step3.domain.RacingInformation;
+
 import java.util.Scanner;
 
 public class InputView {
@@ -7,36 +9,25 @@ public class InputView {
     private final static Scanner scanner = new Scanner(System.in);
     private final static String REQUEST_NUMBER_OF_CAR_GUIDE = "자동차 대수는 몇 대 인가요?";
     private final static String REQUEST_NUMBER_OF_TRY = "시도할 회수는 몇 회 인가요?";
-    private final static String ZERO_NUMBER_USER_GUIDE = "0 이상 입력해주세요";
-    private final static int ZERO = 0;
 
-    private int numberOfCars;
-    private int numberOfReps;
-
-    public void requestNumberOfCar() {
-        System.out.println(REQUEST_NUMBER_OF_CAR_GUIDE);
-        numberOfCars = scanner.nextInt();
-        validateZeroNumber(numberOfCars);
+    public int requestNumberOfCar() {
+        return showPrompt(REQUEST_NUMBER_OF_CAR_GUIDE);
     }
 
-    public void requestNumberOfTry() {
-        System.out.println(REQUEST_NUMBER_OF_TRY);
-        numberOfReps = scanner.nextInt();
-        validateZeroNumber(numberOfReps);
-
+    public int requestNumberOfTry() {
+        return showPrompt(REQUEST_NUMBER_OF_TRY);
     }
 
-    public int getNumberOfCars() {
-        return numberOfCars;
+    private int showPrompt(String requestPrompt) {
+        System.out.println(requestPrompt);
+        return scanner.nextInt();
     }
 
-    public int getNumberOfReps() {
-        return numberOfReps;
-    }
+    public RacingInformation execute() {
 
-    private void validateZeroNumber(int number) {
-        if (number == ZERO) {
-            throw new IllegalArgumentException(ZERO_NUMBER_USER_GUIDE);
-        }
+        int numberOfCars = requestNumberOfCar();
+        int numberOfReps = requestNumberOfTry();
+
+        return RacingInformation.of(numberOfCars, numberOfReps);
     }
 }
