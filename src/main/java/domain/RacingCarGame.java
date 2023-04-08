3ce909a7577984d60861d58a3d1c7c88abae2e54
dkswnkk <jjoy9810@gmail.com>
2023-04-09 01:36:38 +0900

    feat: RacingCarGame 객체 추가

---
+++ src/main/java/domain/RacingCarGame.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RacingCarGame {
+    private final List<RacingCar> cars;
+
+    public RacingCarGame(int carCount) {
+        this.cars = createCars(carCount);
+    }
+
+    private static List<RacingCar> createCars(int carCount) {
+        return Stream.generate(RacingCar::new)
+                .limit(carCount)
+                .collect(Collectors.toList());
+    }
+
+    public List<RacingCar> playAndReturnCars() {
+        for (RacingCar car : cars) {
+            car.moveForward();
+        }
+        return cars;
+    }
+}
