70ebe9da2bb8a875c06c2af83c4b8ad09b69f5ac
lumioustone <mlicp1994@gmail.com>
2023-04-08 22:12:42 +0900

    test[PositiveNumberTest]: positiveNumber 테스트 케이스 추가

--- src/main/java/calculator/PositiveNumber.java
+++ src/main/java/calculator/PositiveNumber.java
@@ -4,7 +4,7 @@ public class PositiveNumber {
     private int number;
 
     public PositiveNumber(String number) {
-        if(number == null || number.isBlank() || !number.matches("^[0-9]*$")) {
+        if(number == null || number.isBlank() || !number.matches("^([-]?[0-9]*)$")) {
             throw new RuntimeException("Is not number");
         }
         int positiveNumber = Integer.parseInt(number);
