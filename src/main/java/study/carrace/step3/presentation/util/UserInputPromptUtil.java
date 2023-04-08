26d9aa2b228ca9cf55ee37c37149ad7c68432742
seriouskang <scdean110@gmail.com>
2023-04-09 00:30:39 +0900

    feat(UserInputPromptUtil): 시도 횟수 콘솔 입력 메소드 신규
    - 시도 횟수 콘솔 입력 메소드 신규
    - 입력 받은 시도 횟수 입력값에 대한 검증 로직 추가
    - 콘솔 정수 입력 메소드 분리
    - 신규 기능에 대한 테스트 코드 추가

--- src/main/java/study/carrace/step3/presentation/util/UserInputPromptUtil.java
+++ src/main/java/study/carrace/step3/presentation/util/UserInputPromptUtil.java
@@ -5,18 +5,37 @@ import java.util.Scanner;
 import static study.carrace.step3.presentation.PromptMessage.*;
 
 public class UserInputPromptUtil {
-    private UserInputPromptUtil() { }
+    private UserInputPromptUtil() {
+    }
 
     public static int askQuantityOfCars() {
         System.out.print(CAR_QUANTITY_QUESTION_MESSAGE.getMessage());
-        int carQuantity = new Scanner(System.in).nextInt();
+        int carQuantity = getIntFromConsole();
         validateCarQuantity(carQuantity);
 
         return carQuantity;
     }
 
+    public static int askIterationCount() {
+        System.out.print(ITERATION_COUNT_QUESTION_MESSAGE.getMessage());
+        int iterationCount = getIntFromConsole();
+        validateIterationCount(iterationCount);
+
+        return iterationCount;
+    }
+
+    private static int getIntFromConsole() {
+        return new Scanner(System.in).nextInt();
+    }
+
+    private static void validateIterationCount(int iterationCount) {
+        if (iterationCount <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 한번 이상이여야 합니다: " + iterationCount);
+        }
+    }
+
     private static void validateCarQuantity(int carQuantity) {
-        if(carQuantity <= 0) {
+        if (carQuantity <= 0) {
             throw new IllegalArgumentException("자동차 대수는 한 대 이상이여야 합니다: " + carQuantity);
         }
     }
--- src/test/java/study/carrace/step3/presentation/util/UserInputPromptUtilTest.java
+++ src/test/java/study/carrace/step3/presentation/util/UserInputPromptUtilTest.java
@@ -9,30 +9,31 @@ import static study.carrace.step3.presentation.util.UserInputPromptUtil.*;
 
 class UserInputPromptUtilTest {
     @Test
-    void 자동차_대수_입력() {
-        // given
-        String input = "1";
-
-        // when
-        systemSetIn(input);
-        int carQuantity = askQuantityOfCars();
-
-        // then
-        assertThat(carQuantity).isEqualTo(1);
+    void 자동차_대수() {
+        systemSetIn("1");
+        assertThat(askQuantityOfCars()).isEqualTo(1);
     }
 
     @Test
-    void 유효하지_않은_자동차_대수_입력() {
-        // given
-        String input = "-1";
-
-        // when
-        systemSetIn(input);
-
-        // then
+    void 유효하지_않은_자동차_대수() {
+        systemSetIn("-1");
         assertThatThrownBy(() -> askQuantityOfCars())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 대수는 한 대 이상이여야 합니다: -1");
+    }
+
+    @Test
+    void 시도_횟수() {
+        systemSetIn("1");
+        assertThat(askIterationCount()).isEqualTo(1);
+    }
+
+    @Test
+    void 유효하지_않은_시도_횟수() {
+        systemSetIn("-1");
+        assertThatThrownBy(() -> askIterationCount())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("시도 횟수는 한번 이상이여야 합니다: -1");
     }
 
     private void systemSetIn(String input) {
