0f75de7065d9f52cdfeb72dc46f2a5aa982a0786
jaewon <jaewon9937@gmail.com>
2023-04-08 22:08:32 +0900

    2단계 - 문자열 덧셈 계산기(피드백 수정) - 공통 로직 @NullAndEmptySource 로 테스트 수정

--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -3,6 +3,9 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,13 +20,10 @@ public class StringAddCalculatorTest {
     }
 
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
-    @Test
-    public void splitAndSum_null_또는_빈문자() {
-        int result = stringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = stringAddCalculator.splitAndSum("");
-        assertThat(result).isEqualTo(0);
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void splitAndSum_null_또는_빈문자(String input) {
+        assertThat(stringAddCalculator.splitAndSum(input)).isEqualTo(0);
     }
 
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
