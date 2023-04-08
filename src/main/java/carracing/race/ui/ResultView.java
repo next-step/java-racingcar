a7cfbb34382c32e2ad889c6e6a4667c333edd9ab
dong <dong.22@kakaoenterprise.com>
2023-04-08 17:37:36 +0900

    feat : 1차 출력로직 작성

--- src/main/java/carracing/race/ui/ResultView.java
+++ src/main/java/carracing/race/ui/ResultView.java
@@ -1,7 +1,9 @@
 package carracing.race.ui;
 
-import java.io.InputStream;
+import carracing.race.logic.AutomobileFederation;
+
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,6 +13,38 @@ public class ResultView {
         this.printStream = printStream;
     }
 
-    public void resultview(Map<Integer, List<Integer>> raceResults) {
+    public void printResult(AutomobileFederation federation) {
+        Map<Integer, List<Integer>> raceResults = federation.getResult();
+
+        ArrayList<Integer> race = new ArrayList<>(raceResults.keySet());
+        for (int lap : race) {
+            List<Integer> raceResultThisLap = raceResults.get(lap);
+            for (int recordCurrentCar : raceResultThisLap) {
+                // printStream.print(resultEachCar);
+                printStream.print(isForward(recordCurrentCar));
+                printStream.print(" ");
+
+            }
+            printStream.println("");
+        }
+        printStream.println();
     }
+
+
+    public static boolean isForward(int recode) {
+        return recode >= 4;
+    }
+    /*
+    도메인 정리
+    Map<Integer, List<Integer>> raceResults
+    키 > Lap
+
+    1랩 >> 각 차 남바별로 성적이 출력
+
+    recode >> 각 차량이 경기당 성적 , 0~9 범위값
+
+
+     */
+
+
 }
