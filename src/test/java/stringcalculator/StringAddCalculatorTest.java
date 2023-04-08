31caca5e31392bdb0955d4ddb2bd2e9ea7bdef8a
MoonHKLee <mf311110@gmail.com>
2023-04-09 01:33:12 +0900

    test(StringAddCalculator) : 메서드명 변경

--- src/test/java/stringcalculator/StringAddCalculatorTest.java
+++ src/test/java/stringcalculator/StringAddCalculatorTest.java
@@ -2,9 +2,7 @@ package stringcalculator;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.NullSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,43 +12,43 @@ public class StringAddCalculatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     public void splitAndSum_null_또는_빈문자(String input) {
-        int result = StringAddCalculator.splitAndSum(input);
+        int result = StringAddCalculator.calculate(input);
         assertThat(result).isEqualTo(0);
     }
 
      @Test
     public void splitAndSum_숫자하나() {
-        int result = StringAddCalculator.splitAndSum("1");
+        int result = StringAddCalculator.calculate("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     public void splitAndSum_쉼표구분자() {
-        int result = StringAddCalculator.splitAndSum("1,2");
+        int result = StringAddCalculator.calculate("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void splitAndSum_쉼표_또는_콜론_구분자() {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
+        int result = StringAddCalculator.calculate("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_custom_구분자() {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        int result = StringAddCalculator.calculate("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_negative() {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+        assertThatThrownBy(() -> StringAddCalculator.calculate("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
     public void splitAndSum_not_number() {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("!@$$,12,as"))
+        assertThatThrownBy(() -> StringAddCalculator.calculate("!@$$,12,as"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
