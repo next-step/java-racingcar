0787304431171ccadd817722e8e43ee61f2a4043
jaewon <jaewon9937@gmail.com>
2023-04-08 22:22:58 +0900

    2단계 - 문자열 덧셈 계산기(피드백 수정) - static 변경

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -10,7 +10,7 @@ public class StringAddCalculator {
     private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
     private static final int DELIMITER_PATTERN_NUMBER = 1;
     private static final int INPUT_PATTERN_NUMBER = 2;
-    public int splitAndSum(String inputText) {
+    public static int splitAndSum(String inputText) {
 
         if (checkEmpty(inputText)) {
             return ZERO;
@@ -19,13 +19,13 @@ public class StringAddCalculator {
         return sumNumbers(splitText(inputText));
     }
 
-    private boolean checkEmpty(String text) {
+    private static boolean checkEmpty(String text) {
 
         //  나중에는 StringUtils.hasText() 사용하면 될 것 같다고 생각이 든다.
         return text == null || text.isEmpty();
     }
 
-    private String[] splitText(String text) {
+    private static String[] splitText(String text) {
         if (isMatchCustomPattern(text)) {
             return splitUsingCustomPattern(text);
         }
@@ -33,11 +33,11 @@ public class StringAddCalculator {
         return text.split("[,|:]");
     }
 
-    private boolean isMatchCustomPattern(String text) {
+    private static boolean isMatchCustomPattern(String text) {
         return CUSTOM_PATTERN.matcher(text).find();
     }
 
-    private String[] splitUsingCustomPattern(String text) {
+    private static String[] splitUsingCustomPattern(String text) {
         Matcher matcher = CUSTOM_PATTERN.matcher(text);
 
         if (matcher.find()) {
@@ -47,7 +47,7 @@ public class StringAddCalculator {
         return new String[0];
     }
 
-    private int sumNumbers(String[] numbers) {
+    private static int sumNumbers(String[] numbers) {
 
         if(isNegativeNumber(numbers)) {
             return Arrays.stream(numbers)
@@ -58,7 +58,7 @@ public class StringAddCalculator {
         throw new RuntimeException();
     }
 
-    private boolean isNegativeNumber(String[] numbers) {
+    private static boolean isNegativeNumber(String[] numbers) {
 
         return (Arrays.stream(numbers)
                 .allMatch(number -> Integer.parseInt(number) > 0));
