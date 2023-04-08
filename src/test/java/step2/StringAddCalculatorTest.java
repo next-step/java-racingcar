14b0ff62435e7ef1aa8af1ff5a9374cd41175be0
seongjo24 <53929443+seongjo24@users.noreply.github.com>
2023-04-09 00:18:47 +0900

    2단계 문자열 계산기 구현 (#4294)

---
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -0,0 +1,59 @@
+package step2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static step2.StringAddCalculator.getDelimeter;
+import static step2.StringAddCalculator.getStrNumbers;
+
+public class StringAddCalculatorTest {
+    @Test
+    public void splitAndSum_null_또는_빈문자() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void spliTest() throws Exception {
+        assertThat(getDelimeter("//;\n1;2;3")).isEqualTo(";");
+    }
+
+    @Test
+    public void getstrNumberTest() {
+        assertThat(getStrNumbers("//;\n1;2;3")).containsExactly("1", "2", "3");
+    }
+}
