718af669a718ae6b37e0f9440241c7e46e9e6592
dong <dong.22@kakaoenterprise.com>
2023-04-08 16:21:28 +0900

    test : 테스트코드 추가

--- src/test/java/carracing/race/RaceApplicationTest.java
+++ src/test/java/carracing/race/RaceApplicationTest.java
@@ -3,7 +3,34 @@ package carracing.race;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 class RaceApplicationTest {
+
+    private RaceApplication raceApplication;
+
+    @DisplayName("경기횟수, 참가차량수가 주어지면 경기결과를 리턴한다")
+    @Test
+    public void run() {
+        //given
+        int parti = 0;
+        int iter = 0;
+
+        //when
+        Map<Integer, List<Integer>> raceResults = raceApplication.racingStart(parti, iter);
+
+        //then
+        assertThat(raceResults.keySet()).hasSize(parti); // 경기 참가자수 검증
+        assertThat(raceResults.values()).hasSize(parti);
+        //횟수검증
+        raceResults.values().stream().forEach(s -> {
+            assertThat(s).hasSize(iter);
+        });
+    }
 
     @DisplayName("n대의 자동차는 전진 또는 멈출 수 있다")
     @Test
