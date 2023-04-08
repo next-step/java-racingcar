507c3cae794943ef168f622b9165a58cf58b6569
신형기 <tkaqkeldk@hanatour.com>
2023-04-08 19:38:47 +0900

    feat: inputdata Test 추가

--- src/test/java/racingcar/domain/InputDataTest.java
+++ src/test/java/racingcar/domain/InputDataTest.java
@@ -1,4 +1,25 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class InputDataTest {
-  
+
+        @Test
+        @DisplayName("입력받은 자동차 대수와 시도 횟수를 저장한다.")
+        void inputData_test() {
+            // Given
+            int carCount = 5;
+            int roundCount = 10;
+
+            // When
+            InputData inputData = new InputData(carCount, roundCount);
+
+            // Then
+            assertEquals(5, inputData.getCarCount());
+            assertEquals(10, inputData.getRoundCount());
+        }
+
 }
