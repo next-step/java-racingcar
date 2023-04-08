29d969b3a9f9e8db1de8345c2af5fe06e33b70d9
henry.young <henry.young@kakaocorp.com>
2023-04-08 22:58:49 +0900

    feat: 자동차 전진 구현

--- src/main/java/racing/RacingCar.java
+++ src/main/java/racing/RacingCar.java
@@ -1,6 +1,33 @@
 package racing;
 
+import java.util.Random;
+
 public class RacingCar {
+    private final int MAX_RANDOM_VALUE = 9;
+    private final int THRESHOLD_ONGOING = 4;
+
+    private final Random rnd = new Random();
+    private int numberOfOngoing;
+
+    public int getNumberOfOngoing() {
+        return numberOfOngoing;
+    }
+
     public RacingCar() {
+        this.numberOfOngoing = 0;
+    }
+
+    private int random() {
+        return rnd.nextInt(this.MAX_RANDOM_VALUE + 1);
+    }
+
+    private boolean isOngoing() {
+        return random() >= this.THRESHOLD_ONGOING;
+    }
+
+    public void ongoing() {
+        if (isOngoing()) {
+            numberOfOngoing++;
+        }
     }
 }
