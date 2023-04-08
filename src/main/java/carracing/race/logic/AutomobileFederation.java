0709884aac46be5a6b7aade00c6a9f5a8442aed3
dong <dong.22@kakaoenterprise.com>
2023-04-08 20:37:04 +0900

    refactor : 경기관련 규칙을 자동차연맹으로 이관

--- src/main/java/carracing/race/logic/AutomobileFederation.java
+++ src/main/java/carracing/race/logic/AutomobileFederation.java
@@ -1,5 +1,7 @@
 package carracing.race.logic;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -16,10 +18,38 @@ public class AutomobileFederation {
     }
 
     private Map<Integer, List<String>> scoreboardCalculation(Map<Integer, List<Integer>> raceResults) {
+        //Map<Integer, List<Integer>> raceResults = federation.getResult();
+
+        ArrayList<Integer> race = new ArrayList<>(raceResults.keySet());
+        for (int lap : race) {
+            List<Integer> raceResultThisLap = raceResults.get(lap);
+            for (int recordCurrentCar : raceResultThisLap) {
+                // printStream.print(resultEachCar);
+          //      printStream.print(isForward(recordCurrentCar));
+            //    printStream.print(" ");
+
+            }
+            //printStream.println("");
+        }
+        //printStream.println();
         return null;
     }
 
     public Map<Integer, List<Integer>> getResult() {
         return this.raceResults;
+    }
+
+    public Map<Integer, List<String>> getScoreboard() {
+        return scoreboard;
+    }
+
+    public List<Integer> allLap() {
+        ArrayList<Integer> integers = new ArrayList<>(raceResults.keySet());
+        integers.sort(Comparator.naturalOrder());
+        return integers;
+    }
+
+    public List<String> lapSituations(int lap) {
+        return null;
     }
 }
