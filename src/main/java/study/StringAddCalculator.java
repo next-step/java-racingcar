559b5dd4998700d8d47b7352c341c1d4b367eb9e
choitaehoon1998 <51841238+choitaehoon1998@users.noreply.github.com>
2023-04-09 04:51:17 +0900

    Step2 : 2단계 - 문자열 덧셈 계산기 (#4237)
    * 실패 하는 테스트 코드 생성.
    
    * 테스트 성공하도록 변경
    
    * 한메소드가 가능한한 한가지 일만 하도록 메소드 분리
    
    * gitkeep 파일 제거.
    
    * 1. String.split() -> pattern.split으로 변경 가능한 부분 변경
    2. MAGIC NUMBER 제거
    3. 메소드 분리 가능한 부분 추가 적으로 분리
    
    * 1. 매직 넘버 추가로 제거
    2. 커스텀 구분자를 위한 테스트 추가

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,55 @@
+package study;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final int ZERO = 0;
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int NUM_STRING_INDEX = 2;
+    private static final int DELIMITER_INDEX = 1;
+    private static final String GIVEN_DELIMITER = ",|:";
+    private static final Pattern GIVEN_DELIMITER_PATTERN = Pattern.compile(GIVEN_DELIMITER);
+
+    public static int splitAndSum(String givenValue) {
+        if (isNullOrEmpty(givenValue)) {
+            return ZERO;
+        }
+        String[] numbers = split(givenValue);
+        return getTotal(numbers);
+    }
+
+
+    private static boolean isNullOrEmpty(String givenValue) {
+        return givenValue == null || givenValue.isEmpty();
+    }
+
+    private static String[] split(String givenValue) {
+        Matcher customDelimiterMatcher = CUSTOM_DELIMITER_PATTERN.matcher(givenValue);
+        if (customDelimiterMatcher.find()) {
+            return splitByGivenDelimiter(customDelimiterMatcher.group(NUM_STRING_INDEX), customDelimiterMatcher.group(DELIMITER_INDEX));
+        }
+        return GIVEN_DELIMITER_PATTERN.split(givenValue);
+    }
+
+    private static String[] splitByGivenDelimiter(String givenString, String customDelimiter) {
+        return givenString.split(customDelimiter);
+    }
+
+    private static int getTotal(String[] numbers) {
+        int total = ZERO;
+        for (String number : numbers) {
+            int parsedNumber = toInteger(number);
+            total += parsedNumber;
+        }
+        return total;
+    }
+
+    private static int toInteger(String number) {
+        int parsedNumber = Integer.parseInt(number);
+        if (parsedNumber < 0) {
+            throw new IllegalArgumentException("음수를 포함하지 말아야 합니다.");
+        }
+        return parsedNumber;
+    }
+}
