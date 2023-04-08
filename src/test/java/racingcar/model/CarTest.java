d9413c7b48e95f1ca3ed743b73306dc99a900c5e
Baek Jiyeon <100.paperkite@gmail.com>
2023-04-08 18:06:04 +0900

    fix(test): 매번 초기화돼서 같은 값으로만 테스트하는 문제 수정

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,14 +1,14 @@
 package racingcar.model;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.*;
 
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("레이싱 자동차 테스트")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // seed 순서대로 나와야 하므로 병렬로 실행되면 안됨
 class CarTest {
     private final static long SEED = 42;
     private final static int MAX_RANDOM_REPEAT = 10;
@@ -16,14 +16,15 @@ class CarTest {
     private Car car;
     private Random random;
 
-    @BeforeEach
+    @BeforeAll
     public void reset() {
-        car = new Car(SEED);
         random = new Random(SEED);
+        car = new Car(SEED);
     }
-    
-    @DisplayName("랜덤 값이 4 이상일 경우 자동차는 움직여야 한다")
+
+    @Order(1)
     @RepeatedTest(MAX_RANDOM_REPEAT)
+    @DisplayName("랜덤 값이 4 이상일 경우 자동차는 움직여야 한다")
     public void go() {
         int expected = random.nextInt(10);
         CarStatus status = car.move();
@@ -33,8 +34,9 @@ class CarTest {
         }
     }
 
-    @DisplayName("랜덤 값이 4 미만일 경우 자동차는 움직여선 안된다")
+    @Order(1)
     @RepeatedTest(MAX_RANDOM_REPEAT)
+    @DisplayName("랜덤 값이 4 미만일 경우 자동차는 움직여선 안된다")
     public void stop() {
         int expected = random.nextInt(10);
         CarStatus status = car.move();
@@ -44,14 +46,16 @@ class CarTest {
         }
     }
 
-    @DisplayName("움직인다면 최대 1만큼만 움직여야 한다")
+    @Order(2)
     @RepeatedTest(MAX_RANDOM_REPEAT)
+    @DisplayName("움직인다면 1만큼만 움직여야 한다")
     public void goOneStep() {
+        int prevDistance = car.distance();
         CarStatus status = car.move();
 
         if (status == CarStatus.RUN) {
             int distance = car.distance();
-            assertThat(distance).isEqualTo(1);
+            assertThat(distance).isEqualTo(prevDistance + 1);
         }
     }
 }
