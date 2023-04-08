3f9bfc016c1556c89196c456e5f9f82e0ca793d6
dkswnkk <jjoy9810@gmail.com>
2023-04-09 01:36:04 +0900

    refactor: OutputView 메서드 분리

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,12 +5,18 @@ import java.util.List;
 import domain.RacingCar;
 
 public class OutputView {
+    private static final String POSITION_SEPARATOR = "-";
+
     public static void printCarsPositionStatus(List<RacingCar> cars) {
         for (RacingCar car : cars) {
-            for (int i = 0; i < car.getPosition(); i++) {
-                System.out.print("-");
-            }
-            System.out.println();
+            printCarPosition(car.getPosition());
+        }
+        System.out.println();
+    }
+
+    private static void printCarPosition(int position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print(POSITION_SEPARATOR);
         }
         System.out.println();
     }
