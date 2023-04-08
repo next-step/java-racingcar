27bcbc121e76f0bb89b10897df37fe0110aebfcd
bahar-j <joonie811@naver.com>
2023-04-09 00:21:09 +0900

    feat: 문자열 덧셈 계산기 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+    public static final String DEFAULT_SEPARATORS = ",:";
+    public static final String CUSTOM_SEPARATOR_REGEX = "//(.*)\\\\n";
+
+    public static int splitAndSum(String input) {
+        if (input == null || input.isBlank()) return 0;
+        return sum(removeSeparatorPhrase(input).split(makeRegex(input)));
+    }
+
+    private static int sum(String[] numbers) {
+        int result = 0;
+        for (String number : numbers) {
+            result += new Positive(number).number();
+        }
+        return result;
+    }
+
+    private static String removeSeparatorPhrase(String text) {
+        return text.replaceAll(CUSTOM_SEPARATOR_REGEX, "");
+    }
+
+    private static String makeRegex(String text) {
+        return "[" + DEFAULT_SEPARATORS + findCustomSeparators(text) + "]";
+    }
+
+    private static String findCustomSeparators(String text) {
+        String customSeparators = "";
+        Matcher matcher = Pattern.compile(CUSTOM_SEPARATOR_REGEX).matcher(text);
+        while (matcher.find()) {
+            String match = matcher.group(1);
+            customSeparators += match;
+        }
+        return customSeparators;
+    }
+}
