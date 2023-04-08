c1b0a0e2c43bc353fca6a16dd53de17d48f80aae
dong <dong.22@kakaoenterprise.com>
2023-04-08 16:04:48 +0900

    refactor : in & out stream can be changed

--- src/main/java/carracing/race/ui/ResultView.java
+++ src/main/java/carracing/race/ui/ResultView.java
@@ -1,4 +1,10 @@
 package carracing.race.ui;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+
 public class ResultView {
+    public ResultView(InputStream in, PrintStream out) {
+
+    }
 }
