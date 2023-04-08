471d9acade862d9b65f583f9d545a9ae2500def9
seongjokang <kangsj24@naver.com>
2023-04-08 15:14:42 +0900

    2단계 문자열 계산기 구현

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,46 @@
+package step2;
+
+public class StringAddCalculator {
+    static final int ZERO = 0;
+    static final int CUSTOM_DELIMETER_INDEX = 2;
+    static final int CUSTOM_INPUT_BEGIN_INDEX = 4;
+
+    public static int splitAndSum(String input) {
+        if (isNullOrBlank(input)) return ZERO;
+        String[] strNumbers = getStrNumbers(input);
+        return calculate(strNumbers);
+    }
+
+
+    public static String[] getStrNumbers(String input) {
+        return getInput(input).split(getDelimeter(input));
+    }
+
+    public static String getDelimeter(String input) {
+        if (isCustum(input)) return String.valueOf(input.charAt(CUSTOM_DELIMETER_INDEX));
+        return ",|:";
+    }
+
+    public static String getInput(String input) {
+        if (isCustum(input)) return input.substring(CUSTOM_INPUT_BEGIN_INDEX);
+        return input;
+    }
+
+    public static int calculate(String[] strNumbers) {
+        int sum = 0;
+        for (String strNumber : strNumbers) {
+            int number = Integer.parseInt(strNumber);
+            if (number < 0) throw new RuntimeException();
+            sum += number;
+        }
+        return sum;
+    }
+
+    public static boolean isCustum(String input) {
+        return input.contains("//");
+    }
+
+    public static boolean isNullOrBlank(String input) {
+        return input == null || input.isBlank();
+    }
+}
