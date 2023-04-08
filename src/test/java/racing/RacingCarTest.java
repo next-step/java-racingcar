8c7daa779be1918392157014b786b986cb4b53fc
henry.young <henry.young@kakaocorp.com>
2023-04-08 22:57:31 +0900

    test: 자동차 전진 테스트

--- src/test/java/racing/RacingCarTest.java
+++ src/test/java/racing/RacingCarTest.java
@@ -7,11 +7,26 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarTest {
+
     @Test
     @DisplayName("자동차 생성")
     void create() {
         RacingCar actual = new RacingCar();
         assertThat(actual).isNotNull();
+    }
+
+    @Test
+    @DisplayName(("자동차 전진 테스트"))
+    void ongoing() {
+        RacingCar car = new RacingCar();
+        float REPEAT_NUMBER = 10000;
+        for (int i = 0; i < REPEAT_NUMBER; i++) {
+            car.ongoing();
+        }
+
+        float actual = car.getNumberOfOngoing() / REPEAT_NUMBER;
+        System.out.println("actual = " + actual);
+        assertThat(actual >= 0.57 && actual <= 0.63).isTrue();
     }
 }
 
