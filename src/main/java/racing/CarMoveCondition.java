048395b04e7a65fe8f0c017aa626e255da8599db
soochan <tncksdl05@gmail.com>
2023-04-09 14:02:58 +0900

    feat : 자동차가 움직일 수 있는 조건 판단 객체 생성

---
+++ src/main/java/racing/CarMoveCondition.java
@@ -0,0 +1,17 @@
+package racing;
+
+/**
+ * 자동차 이동 조건 (CarMoveCondition)
+ *     - 책임
+ *       - 랜덤 생성기의 임의 값을 기반으로 Car 객체의 움직임을 제어.
+ *       - Car 객체가 앞으로 이동해야 하는지 정지해야 하는지 결정합니다.
+ */
+public class CarMoveCondition {
+
+    private final int MOVE_CONDITION = 4;
+
+    public boolean isMove(int randomNumber) {
+        return randomNumber >= MOVE_CONDITION;
+    }
+
+}
