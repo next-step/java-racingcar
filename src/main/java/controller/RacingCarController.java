9c62cde509805a44ba2e0df29e53c93980d69255
dkswnkk <jjoy9810@gmail.com>
2023-04-10 01:53:33 +0900

    feat: RacingCarController 기능 추가

--- src/main/java/controller/RacingCarController.java
+++ src/main/java/controller/RacingCarController.java
@@ -1,20 +1,27 @@
 package controller;
 
+import domain.RacingCar;
 import domain.RacingCarGame;
 import view.InputView;
 import view.OutputView;
 
+import java.util.List;
+
 public class RacingCarController {
 
     public static void main(String[] args) {
-        int createCarCount = InputView.inputCarCount();
+        String[] createCarNames = InputView.inputCarNames();
         int tryCount = InputView.inputTryCount();
 
-        RacingCarGame racingCarGame = new RacingCarGame(createCarCount);
+        RacingCarGame racingCarGame = new RacingCarGame(createCarNames);
 
         System.out.println("실행 결과");
         for (int i = 0; i < tryCount; i++) {
-            OutputView.printCarsPositionStatus(racingCarGame.playAndReturnCars());
+            List<RacingCar> racingCars = racingCarGame.playAndReturnCars();
+            OutputView.printCarsNameAndPositionStatus(racingCars);
         }
+
+        List<RacingCar> cars = racingCarGame.getCars();
+        OutputView.printWinners(RacingCarGame.getWinners(cars));
     }
 }
