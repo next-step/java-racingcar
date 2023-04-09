cc134f68a12c7a60578511a5d6e81ac062175ec1
dkswnkk <jjoy9810@gmail.com>
2023-04-10 01:52:52 +0900

    feat: 자동차 이름 입력 받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     public static final Scanner SCANNER = new Scanner(System.in);
+    private static final int MAX_NAME_LENGTH = 5;
 
     private InputView() {
     }
@@ -16,5 +17,28 @@ public class InputView {
     public static int inputTryCount() {
         System.out.println("시도할 횟수는 몇 회 인가요?");
         return SCANNER.nextInt();
+    }
+
+    public static String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String[] carNames = splitBySeparate(SCANNER.nextLine());
+        validateCarName(carNames);
+        return carNames;
+    }
+
+    public static void validateCarName(String[] carNames) {
+        for (String carName : carNames) {
+            checkCarNameLength(carName);
+        }
+    }
+
+    private static String[] splitBySeparate(String carNames) {
+        return carNames.split(",");
+    }
+
+    private static void checkCarNameLength(String carName) {
+        if (carName.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 }
