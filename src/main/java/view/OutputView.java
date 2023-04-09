301a6162b2084d500aa9895bc23b23629372b1a3
dkswnkk <jjoy9810@gmail.com>
2023-04-10 01:53:08 +0900

    feat: 우승자 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import domain.RacingCar;
 
@@ -17,8 +18,31 @@ public class OutputView {
         System.out.println();
     }
 
+    public static void printCarsNameAndPositionStatus(List<RacingCar> cars) {
+        for (RacingCar car : cars) {
+            printCarNameAndPosition(car);
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<RacingCar> winners) {
+        String winnerNames = winners.stream()
+                .map(RacingCar::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println("최종 우승자: " + winnerNames);
+    }
+
     private static void printCarPosition(int position) {
         for (int i = 0; i < position; i++) {
+            System.out.print(POSITION_SEPARATOR);
+        }
+        System.out.println();
+    }
+
+    private static void printCarNameAndPosition(RacingCar car) {
+        System.out.print(car.getName() + " : ");
+        for (int i = 0; i < car.getPosition(); i++) {
             System.out.print(POSITION_SEPARATOR);
         }
         System.out.println();
