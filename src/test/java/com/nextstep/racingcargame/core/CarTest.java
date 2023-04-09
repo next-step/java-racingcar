655ac9c06b29c827792ce49f677cca217e86c5cd
patrickYoo <yhy1045@naver.com>
2023-04-09 16:22:24 +0900

    chor(core-elements): 불필요 import 객체를 삭제한다

--- src/test/java/com/nextstep/racingcargame/core/CarTest.java
+++ src/test/java/com/nextstep/racingcargame/core/CarTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
