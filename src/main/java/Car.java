2d2c59e7640417a22b3d45dccd1173b51fc15eb7
KimJuHyun <rlawngus0910@naver.com>
2023-04-10 00:16:56 +0900

    feat: Car에 이름 필드 추가, 5자 이상은 예외처리

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,11 +1,28 @@
 public class Car {
 
+    private String name;
     private int distance;
+
+    public Car(String name, int distance) {
+        checkLengthOfName(name);
+        this.name = name;
+        this.distance = distance;
+    }
+
+    private void checkLengthOfName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름이 5자 이상이 될 수 없습니다! : " + name);
+        }
+    }
 
     public void go(boolean condition) {
         if (condition) {
             this.distance++;
         }
+    }
+
+    public String name() {
+        return this.name;
     }
 
     public int distance() {
