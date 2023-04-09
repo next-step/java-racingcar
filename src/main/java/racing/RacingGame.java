6c4550072883511ec5caef64991e26a7b8d65e77
henry.young <henry.young@kakaocorp.com>
2023-04-09 14:22:27 +0900

    feat: RacingGame Constructor에서 Input 입력받도록 구현

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -1,13 +1,22 @@
 package racing;
 
 public class RacingGame {
+    private final InputView iv;
     private final int numOfCar;
     private final int numOfCycle;
 
     public RacingGame(int numOfCar, int numOfCycle) {
+        iv = null;
         this.numOfCar = numOfCar;
         this.numOfCycle = numOfCycle;
     }
+
+    public RacingGame() {
+        iv = new InputView();
+        this.numOfCar = iv.readNumberOfCars();
+        this.numOfCycle = iv.readNumberOfCycles();
+    }
+
 
     public void info() {
         System.out.println("자동차 수: " + this.numOfCar + ", 사이클 수: " + this.numOfCycle);
