e4537afe7d9aa61f0c497eb30e3ab9d5f48182cd
henry.young <henry.young@kakaocorp.com>
2023-04-09 19:19:04 +0900

    feat: 경기 생성하고 시작하는 기능 구현

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -1,24 +1,64 @@
 package racing;
 
-public class RacingGame {
-    private final InputView iv;
-    private final int numOfCar;
-    private final int numOfCycle;
+import java.util.ArrayList;
+import java.util.List;
 
-    public RacingGame(int numOfCar, int numOfCycle) {
-        iv = null;
-        this.numOfCar = numOfCar;
-        this.numOfCycle = numOfCycle;
+public class RacingGame {
+    private final ResultView rv = new ResultView();
+    private final int numOfCars;
+    private final int numOfCycles;
+    private final List<RacingCar> gameCars;
+
+    public RacingGame(int numOfCars, int numOfCycles) {
+        this.numOfCars = numOfCars;
+        this.numOfCycles = numOfCycles;
+        this.gameCars = createCarList();
     }
 
     public RacingGame() {
-        iv = new InputView();
-        this.numOfCar = iv.readNumberOfCars();
-        this.numOfCycle = iv.readNumberOfCycles();
+        InputView iv = new InputView();
+        this.numOfCars = iv.readNumberOfCars();
+        this.numOfCycles = iv.readNumberOfCycles();
+        this.gameCars = createCarList();
+    }
+
+    private List<RacingCar> createCarList() {
+        List<RacingCar> carList = new ArrayList<>();
+        for (int i = 0; i < this.numOfCars; i++) {
+            carList.add(new RacingCar());
+        }
+        return carList;
+    }
+
+    public void start() {
+        rv.printStartMessage();
+        for (int i = 0; i < this.numOfCycles; i++) {
+            progressCycle();
+        }
+    }
+
+    private void progressCycle() {
+        ongoingEachCar();
+        rv.printCarListStatus(this.gameCars);
+    }
+
+    private void ongoingEachCar() {
+        for (int i = 0; i < this.numOfCars; i++) {
+            this.gameCars.get(i).ongoing();
+        }
+    }
+
+    public List<Integer> result() {
+        List<Integer> carStatusList = new ArrayList<>();
+        for (int i = 0; i < this.numOfCars; i++) {
+            int carStatus = this.gameCars.get(i).getNumberOfOngoing();
+            carStatusList.add(carStatus);
+        }
+        return carStatusList;
     }
 
 
     public void info() {
-        System.out.println("자동차 수: " + this.numOfCar + ", 사이클 수: " + this.numOfCycle);
+        System.out.println("자동차 수: " + this.numOfCars + ", 사이클 수: " + this.numOfCycles);
     }
 }
