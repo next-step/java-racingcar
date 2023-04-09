d862f3ed083119bcce9d981ca65a028bf18dc443
soochan <tncksdl05@gmail.com>
2023-04-09 14:05:19 +0900

    feat : 사용자 입력을 받는 객체 생성

---
+++ src/main/java/racing/InputView.java
@@ -0,0 +1,34 @@
+package racing;
+
+import java.util.Scanner;
+
+/**
+ * 사용자 입력 (InputView)
+ *     - 책임
+ *       - 자동차 대수를 입력받는다.
+ *       - 자동차 이동 횟수를 입력받는다.
+ */
+public class InputView {
+
+  private final Scanner scanner;
+
+  public InputView () {
+    scanner = new Scanner(System.in);
+  }
+
+  public int scanNumberOfCars() {
+    System.out.println("자동차 대수는 몇 대 인가요?");
+    return scanner.nextInt();
+  }
+
+  public int scanNumberOfMovements() {
+    System.out.println("시도할 회수는 몇 회 인가요?");
+    return scanner.nextInt();
+  }
+
+  public UserInput scanUserInput() {
+    int numberOfCars = scanNumberOfCars();
+    int numberOfMovements = scanNumberOfMovements();
+    return new UserInput(numberOfCars, numberOfMovements);
+  }
+}
