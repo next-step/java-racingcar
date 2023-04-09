84d990cad2d89f20ea12150e7af7ee7663e554a3
soochan <tncksdl05@gmail.com>
2023-04-09 14:02:35 +0900

    feat : 자동차 공장 객체 생성

---
+++ src/main/java/racing/CarFactory.java
@@ -0,0 +1,18 @@
+package racing;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 자동차 생성 공장 (CarFactory) - 책임 - 지정된 수의 Car 객체를 생성한다.
+ */
+public class CarFactory {
+
+  public List<Car> createCars(int numberOfCars) {
+    return IntStream.range(0, numberOfCars)
+        .mapToObj(i -> new Car())
+        .collect(Collectors.toList());
+  }
+
+}
