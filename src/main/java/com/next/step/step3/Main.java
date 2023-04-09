fbc27a1cd38183c475ce83c8ba7d1ec16dd41242
ghkdwlgns612 <ghkdwlgns612@naver.com>
2023-04-09 13:22:18 +0900

    feat : Main Class추가 및 패키지 분리.

---
+++ src/main/java/com/next/step/step3/Main.java
@@ -0,0 +1,11 @@
+package com.next.step.step3;
+
+import com.next.step.step3.view.InputView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+
+    }
+}
