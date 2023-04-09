3d2522d1ffdf2b4edb0ef3d86e7ee9d22b394fcc
dong <dong.22@kakaoenterprise.com>
2023-04-09 09:19:53 +0900

    test : 테스트코드 보완

--- src/test/java/carracing/race/ui/ResultViewTest.java
+++ src/test/java/carracing/race/ui/ResultViewTest.java
@@ -1,31 +1,123 @@
 package carracing.race.ui;
 
+import carracing.race.logic.AutomobileFederation;
+import carracing.race.logic.type.Round;
+import carracing.race.logic.type.Score;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ResultViewTest {
+    private static final Logger log = Logger.getLogger("ResultViewTest");
     private ResultView resultView;
+    private OutputStream outputStream;
+
     @BeforeEach
     public void beforeEach() {
-        this.resultView = new ResultView(new PrintStream(new PrintStream(new ByteArrayOutputStream())));
-
+        outputStream = new ByteArrayOutputStream();
+        this.resultView = new ResultView(new PrintStream(new PrintStream(outputStream)));
     }
 
-    @DisplayName("")
+    @DisplayName("경기결과가 CarIndex 와 함께 출력된다")
     @Test
-    public void iteration() {
+    public void withCarIndex() {
         //given
-        //int
+        Map<Round, List<Score>> inputRoundToScoreListMap = Map.of(
+                new Round(5), Arrays.asList(new Score(1), new Score(4), new Score(5)),
+                new Round(1), Arrays.asList(new Score(7), new Score(3), new Score(9)),
+                new Round(2), Arrays.asList(new Score(5), new Score(2), new Score(1)),
+                new Round(3), Arrays.asList(new Score(9), new Score(4), new Score(5)),
+                new Round(4), Arrays.asList(new Score(8), new Score(8), new Score(8))
+        );
+        String output = "" +
+                "1 : -\n" +
+                "2 : \n" +
+                "3 : -\n" +
+                "\n" +
+                "1 : --\n" +
+                "2 : \n" +
+                "3 : -\n" +
+                "\n" +
+                "1 : ---\n" +
+                "2 : \n" +
+                "3 : --\n" +
+                "\n" +
+                "1 : ----\n" +
+                "2 : -\n" +
+                "3 : ---\n" +
+                "\n" +
+                "1 : ----\n" +
+                "2 : -\n" +
+                "3 : ----\n" +
+                "\n";
+
         //when
-        //resultview
+        AutomobileFederation automobileFederation = new AutomobileFederation(inputRoundToScoreListMap);
+        resultView.printResultWithCarIndex(automobileFederation);
+
         //then
+        assertAll(
+                () -> assertEquals(outputStream.toString(), output),
+                () -> assertThat(automobileFederation.getRounds()).hasSize(inputRoundToScoreListMap.size()),
+                () -> log.warning("ResultView 를 테스트해야하는데, AutomobileFederation 가 실패하면 해당 테스도 실패한다"),
+                () -> log.warning("UI 컴포넌트인 ResultView 만을 순수하게 테스트할수 있도록 구조를 개선해야한다")
+        );
     }
 
+    @DisplayName("경기결과가 출력된다")
+    @Test
+    public void iteration2() {
+        //given
+        Map<Round, List<Score>> inputRoundToScoreListMap = Map.of(
+                new Round(5), Arrays.asList(new Score(1), new Score(4), new Score(5)),
+                new Round(1), Arrays.asList(new Score(7), new Score(3), new Score(9)),
+                new Round(2), Arrays.asList(new Score(5), new Score(2), new Score(1)),
+                new Round(3), Arrays.asList(new Score(9), new Score(4), new Score(5)),
+                new Round(4), Arrays.asList(new Score(8), new Score(8), new Score(8))
+
+        );
+        String output = "" +
+                "-\n" +
+                "\n" +
+                "-\n" +
+                "\n" +
+                "--\n" +
+                "\n" +
+                "-\n" +
+                "\n" +
+                "---\n" +
+                "\n" +
+                "--\n" +
+                "\n" +
+                "----\n" +
+                "-\n" +
+                "---\n" +
+                "\n" +
+                "----\n" +
+                "-\n" +
+                "----\n" +
+                "\n";
+
+        //when
+        AutomobileFederation automobileFederation = new AutomobileFederation(inputRoundToScoreListMap);
+        resultView.printResultWithoutCarIndex(automobileFederation);
+
+        //then
+        assertAll(
+                () -> assertEquals(outputStream.toString(), output),
+                () -> assertThat(automobileFederation.getRounds()).hasSize(inputRoundToScoreListMap.size())
+        );
+    }
 }
