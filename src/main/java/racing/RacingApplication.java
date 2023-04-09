1ce690ae30b023d0588cfd696f98c735ab95619e
lumioustone <mlicp1994@gmail.com>
2023-04-09 11:58:28 +0900

    restart

--- src/main/java/racing/RacingApplication.java
+++ src/main/java/racing/RacingApplication.java
@@ -6,24 +6,6 @@ import java.util.Scanner;
 
 public class RacingApplication {
     public static void main(String[] args) {
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int totalCarNumber = scanner.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int opportunity = scanner.nextInt();
-
-        List<Car> carList = Car.createCarList(totalCarNumber);
-
-        System.out.println("실행 결과");
-        for (int i = 0; i < opportunity; i++) {
-            for (Car racingCar : carList) {
-                int randomNumber = random.nextInt(10);
-                racingCar.move(randomNumber);
-                PrintUtil.drawPosition(racingCar.valueOfPosition());
-            }
-            System.out.println("");
-        }
 
     }
 }
