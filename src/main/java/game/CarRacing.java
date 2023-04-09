63e68f952f40ad38f43552722f5508aa5a2bd18d
In-wook Hwang <inwook9@gmail.com>
2023-04-10 02:32:39 +0900

    feat: 결과 출력기능 연동 및 MainApplication 추가

--- src/main/java/game/CarRacing.java
+++ src/main/java/game/CarRacing.java
@@ -5,16 +5,22 @@ import java.util.stream.IntStream;
 public class CarRacing {
     private final int racingRep;
     private final Cars cars;
+    private final ResultView resultView;
 
     public CarRacing(RacingOptions options) {
         this.racingRep = options.racingRep();
         this.cars = Cars.generate(options.carCount());
+        this.resultView = new ResultView(cars);
     }
 
 
     public void start() {
         IntStream.range(0, racingRep)
                 .forEach(rep -> cars.drive());
+    }
+
+    public void end() {
+        resultView.show();
     }
 
     public int carCount() {
