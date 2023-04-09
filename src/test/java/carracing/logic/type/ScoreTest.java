89b4265334394340f32d0be9edaebcecc20018cc
kingdong <ultraraptor@naver.com>
2023-04-09 19:40:30 +0900

    test : 동등성 검증 테스트코드 추가

--- src/test/java/carracing/logic/type/ScoreTest.java
+++ src/test/java/carracing/logic/type/ScoreTest.java
@@ -7,12 +7,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScoreTest {
 
-    @DisplayName("")
+    @DisplayName("같은 Value 를 가진 Score 가 동등한지를 검증 1 - JCF 사용을 위해 검증")
     @Test
-    public void equilty() {
+    public void equivalence() {
         //given
         Score oneA = new Score(1);
         Score oneB = new Score(1);
+        //when
+        //then
+        assertThat(oneA).isEqualTo(oneB);
+    }
+
+    @DisplayName("같은 Value 를 가진 Score 가 동등한지를 검증 2 - JCF 사용을 위해 검증")
+    @Test
+    public void equivalence2() {
+        //given
+        Score oneA = new Score(777);
+        Score oneB = new Score(777);
         //when
         //then
         assertThat(oneA).isEqualTo(oneB);
