09af6aecd17bf54e59239667555a767292be5a46
player-geun <player_geun@naver.com>
2023-04-09 19:05:06 +0900

    feat : 사용자 입력 기능 구현

---
+++ src/main/java/study/step3/view/InputView.java
@@ -0,0 +1,37 @@
+package study.step3.view;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String TEXT_INPUT_CAR = "자동차 대수는 몇 대 인가요?";
+    private static final String TEXT_INPUT_MOVE = "시도할 회수는 몇 회 인가요?";
+
+    public int inputCountOfCar() {
+        System.out.println(TEXT_INPUT_CAR);
+        return inputInt();
+    }
+
+    public int inputCountOfMove() {
+        System.out.println(TEXT_INPUT_MOVE);
+        return inputInt();
+    }
+
+    private int inputInt() {
+        try {
+            int number = scanner.nextInt();
+            validatePositiveInt(number);
+            return number;
+        } catch (InputMismatchException e) {
+            throw new RuntimeException("숫자가 아닌 것은 입력할 수 없습니다.");
+        }
+    }
+
+    private void validatePositiveInt(int number) {
+        if (number < 0) {
+            throw new IllegalStateException("음수는 입력할 수 없습니다.");
+        }
+    }
+}
