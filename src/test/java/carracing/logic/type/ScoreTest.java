bb3379c8c54e0ad20817836112ea59971dd3b52c
kingdong <ultraraptor@naver.com>
2023-04-09 20:00:32 +0900

    test : 실패하는 테스트케이스 추가

--- src/test/java/carracing/logic/type/ScoreTest.java
+++ src/test/java/carracing/logic/type/ScoreTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class ScoreTest {
 
@@ -28,4 +29,30 @@ public class ScoreTest {
         //then
         assertThat(oneA).isEqualTo(oneB);
     }
+
+    @DisplayName("범위를 초과하는 Score 의 Value 입력시 RuntimeException 을 throw 한다 : -1")
+    @Test
+    public void outOfDownRangeThrowPlz() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            new Score(-1);
+        }).isInstanceOf(RuntimeException.class);
+        //.hasMessageContaining(
+    }
+
+
+    @DisplayName("범위를 초과하는 Score 의 Value 입력시 RuntimeException 을 throw 한다 : 10")
+    @Test
+    public void outOfUpRangeThrowPlz() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            new Score(10);
+        }).isInstanceOf(RuntimeException.class);
+        //.hasMessageContaining(
+    }
+
 }
