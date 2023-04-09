9cd2293dd21c5e5ee1ba572a4a3f53d48f8b4cdc
ji lim <jilim@jiui-MacBookPro.local>
2023-04-09 21:36:19 +0900

    Feat: string test, set test 완료

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,52 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetCollectionTest {
+
+    private static Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    void test1() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void test2(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1", "2", "3", "4", "5"})
+    void test3(String testValue) {
+        boolean isTrue = numbers.contains(Integer.getInteger(testValue));
+        assertThat(numbers.contains(Integer.getInteger(testValue))).isEqualTo(isTrue);
+
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,8 +7,28 @@ import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
-    void split() {
+    void stringClass1() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void stringClass2() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).contains("1,2");
+    }
+
+    @Test
+    void stringClass3() {
+        String abc = "abc";
+        char a = abc.charAt(0);
+        char b = abc.charAt(1);
+        char c = abc.charAt(2);
+
+        assertThat(a).isEqualTo('a');
+        assertThat(b).isEqualTo('b');
+        assertThat(c).isEqualTo('c');
+
+        assertThatThrownBy( () -> abc.charAt(3)) .isInstanceOf(IndexOutOfBoundsException.class).hasMessageContainingAll("String index out of range: 3");
     }
 }
