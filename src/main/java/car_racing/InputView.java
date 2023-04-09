8dbd2febbbc72d59fde9f94a0fd7edfbdd88e5b5
hj1115hj <hyejin_one@naver.com>
2023-04-09 23:31:04 +0900

    feat: 구현 class 생성

---
+++ src/main/java/car_racing/InputView.java
@@ -0,0 +1,4 @@
+package car_racing;
+
+public class InputView {
+}
---
+++ src/test/java/car_racing/CarRacingTest.java
@@ -0,0 +1,12 @@
+package car_racing;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarRacingTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
