9bdff3139950ac0027c638c2465372d2ccae443d
PiHyoJeong <shson4007@gmail.com>
2023-04-10 01:02:40 +0900

    Refactor: 2단계 코드리뷰 반영

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -5,13 +5,11 @@ public class StringAddCalculator {
     public static final int ZERO = 0;
     public static final String DELIMITER = ",|:";
 
-    public StringAddCalculator(String[] splitText) {
-
-    }
-
     public static int splitAndSum(String text) {
-        if(isBoolean(text)) return ZERO;
-        return getSum(toInts(getSplit(text)));
+        if(isBoolean(text)) {
+            return ZERO;
+        }
+        return getSum(stringsToNumbers(getSplit(text)));
     }
 
     private static String[] getSplit(String text) {
@@ -30,18 +28,27 @@ public class StringAddCalculator {
         return result;
     }
 
-    private static int[] toInts(String[] splitText) {
+    int classNumber;
+
+    private static int[] stringsToNumbers(String[] splitText) {
         int[] numbers = new int[splitText.length];
         for (int i = 0; i < numbers.length; i++) {
-            int number = toPositiveNumber(splitText[i]);
-            numbers[i] = number;
+            StringAddCalculator stringAddCalculator = new StringAddCalculator(splitText[i]);
+            int numberTest = stringAddCalculator.classNumber;
+            numbers[i] = numberTest;
         }
         return numbers;
     }
 
     private static int toPositiveNumber(String splitText) {
         int number = Integer.parseInt(splitText);
-        if(number < 0){throw new RuntimeException("음수는 입력할 수 없습니다");}
+        if(number < 0){
+            throw new RuntimeException("음수는 입력할 수 없습니다");
+        }
         return number;
+    }
+
+    public StringAddCalculator(String splitText) {
+        this.classNumber = toPositiveNumber(splitText);
     }
 }
