662c8db179c34080c66e9e22059df1c5e1e912d1
kingdong <ultraraptor@naver.com>
2023-04-09 19:09:38 +0900

    fix : 인터페이스 추상화의 결과로 클래스의 목적을 명시

--- src/main/java/carracing/ui/ResultViewV1.java
+++ src/main/java/carracing/ui/ResultViewV1.java
@@ -5,14 +5,17 @@ import carracing.logic.AutomobileFederation;
 import java.io.PrintStream;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
+import java.util.logging.Logger;
 
 public class ResultViewV1 implements ResultView {
+    private static final Logger log = Logger.getLogger("ResultViewV1");
     private static final Boolean DISPLAY_CAR_INDEX = Boolean.TRUE;
     private static final Boolean DISPLAY_ROUND_INFORMATION = Boolean.TRUE;
 
     private final PrintStream printStream;
 
     public ResultViewV1(PrintStream printStream) {
+        log.info("ResultViewV1 에서는 DISPLAY_CAR_INDEX 만 표시여부를 결정할 수 있습니다");
         this.printStream = printStream;
     }
 
@@ -20,6 +23,7 @@ public class ResultViewV1 implements ResultView {
         return recode >= 4;
     }
 
+    @Override
     public void printResult(AutomobileFederation federation) {
         Consumer<AutomobileFederation> printer = DISPLAY_CAR_INDEX ?
                 this::printResultWithCarIndex :
