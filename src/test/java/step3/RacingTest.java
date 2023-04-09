bb751100bec77a95b089350b4da16ef83302d8e3
cohi <tkfka156@gmail.com>
2023-04-10 06:45:37 +0900

    :white_check_mark: test(CarRace): move 테스트 추가

--- src/test/java/step3/RacingTest.java
+++ src/test/java/step3/RacingTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,6 +12,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 
 class RacingTest {
+
+
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car();
+    }
 
     @DisplayName("입력받은 개수에 따라 car 객체를 생성한다.")
     @Test
@@ -33,7 +42,6 @@ class RacingTest {
     @ParameterizedTest(name = "4 이상이 나올 경우 car가 움직인다.")
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     void test1(int input) throws Exception {
-        Car car = new Car();
         car.move(input);
 
         assertThat(car.getDistance()).hasSize(1);
@@ -42,10 +50,18 @@ class RacingTest {
     @ParameterizedTest(name = "3 이하가 나올 경우 car는 움직이지 않는다.")
     @ValueSource(ints = {0, 1, 2, 3})
     void test2(int input) throws Exception {
-        Car car = new Car();
         car.move(input);
 
         assertThat(car.getDistance()).isEmpty();
+    }
+
+    @DisplayName("5번 시도 중 3번 이상 4이상이 나오면 car가 움직인 거리는 3이다.")
+    @Test
+    void test10() throws Exception {
+        int[] ints = {0,3,4,5,9};
+        testCarMoving(ints);
+
+        assertThat(car.getDistance()).hasSize(3);
     }
 
     @DisplayName("자동차 개수가 0 이하로 입력되면 예외가 발생한다.")
@@ -72,5 +88,11 @@ class RacingTest {
     @Test
     void test8() throws Exception {
         assertThatCode(() -> new Race(1, 1)).doesNotThrowAnyException();
+    }
+
+    private void testCarMoving(int[] ints) {
+        for (int anInt : ints) {
+            car.move(anInt);
+        }
     }
 }
