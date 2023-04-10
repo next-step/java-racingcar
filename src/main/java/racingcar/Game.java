c35dc03a01eea2f1aa38481a2a5df210424eca84
MoonHKLee <moonhyuk.lee@lotte.net>
2023-04-10 21:15:49 +0900

    refactor(game) : 공통기능 메서드 추출. - validate()

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -7,15 +7,19 @@ public class Game {
     private final Car[] cars;
 
     public Game(final int carAmount, final int tryAmount) {
+        validate(carAmount, tryAmount);
+        this.carAmount = carAmount;
+        this.tryAmount = tryAmount;
+        this.cars = createCars();
+    }
+
+    private void validate(int carAmount, int tryAmount) {
         if (carAmount < 1) {
             throw new IllegalArgumentException("자동차 대수는 1 이상이어야 합니다.");
         }
         if (tryAmount < 1) {
             throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
         }
-        this.carAmount = carAmount;
-        this.tryAmount = tryAmount;
-        this.cars = createCars();
     }
 
     public int getCarAmount() {
