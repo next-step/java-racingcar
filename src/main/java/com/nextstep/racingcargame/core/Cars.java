a7f3c615a3f08ce09ddd19800fb34d4db8ae7fd3
patrickYoo <yhy1045@naver.com>
2023-04-10 20:37:14 +0900

    feat(core-elements): 가장 멀리이동한 차량에 대한 이름을 추출하는 기능 작성

--- src/main/java/com/nextstep/racingcargame/core/Cars.java
+++ src/main/java/com/nextstep/racingcargame/core/Cars.java
@@ -11,6 +11,8 @@ import java.util.stream.Collectors;
 public class Cars {
 
     private static final String HAS_NEGATIVE_CAR_CREATE_REQUEST = "1 보다 작은 수의 차량은 생성될 수 없습니다.";
+
+    private static final String WINNER_JOIN_DELIMITER = ",";
     private final List<Car> cars;
 
     // root ctor
@@ -47,11 +49,22 @@ public class Cars {
         return number > 0;
     }
 
-    public Car getFurthestTraveledCar() {
+    public List<String> getWinnerNames() {
+        return this.cars.stream()
+                .filter(car -> car.getDistance() == getFurthestDistance())
+                .map(Car::getCarName)
+                .collect(Collectors.toList());
+    }
+
+    public String joinWinnerNames() {
+        return String.join(WINNER_JOIN_DELIMITER,getWinnerNames());
+    }
+
+    public Car findFirstFurthestTraveledCar() {
         return max(this.cars, Comparator.comparingInt(Car::getDistance));
     }
 
     public int getFurthestDistance() {
-        return getFurthestTraveledCar().getDistance();
+        return findFirstFurthestTraveledCar().getDistance();
     }
 }
--- src/test/java/com/nextstep/racingcargame/core/CarsTest.java
+++ src/test/java/com/nextstep/racingcargame/core/CarsTest.java
@@ -15,7 +15,7 @@ class CarsTest {
     private static final int CAR_FORCE_MOVE_NUMBER = 9;
 
     @ParameterizedTest(name = "[{index}] Cars 객체에 (,) 구분자를 기준으로 이름만큼 자동차를 생성한다.")
-    @CsvSource(value = {"rick,jack,ethan:3","ethan:1","jack,ethan:2"}, delimiter = ':')
+    @CsvSource(value = {"rick,jack,ethan:3", "ethan:1", "jack,ethan:2"}, delimiter = ':')
     void createCarsSuccessTest(String valueSource, int expectedCarSize) {
         Cars cars = new Cars(new CarNameChunk(valueSource));
         assertThat(cars.carSize()).isEqualTo(expectedCarSize);
@@ -40,15 +40,34 @@ class CarsTest {
         patCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
 
         Car rickCar = new Car("rick");
+
         Car ethanCar = new Car("ethan");
 
-        Cars cars = new Cars(Arrays.asList(patCar,rickCar,ethanCar));
-        Car f = cars.getFurthestTraveledCar();
-        assertThat(cars.getFurthestTraveledCar().getCarName()).isEqualTo("pat");
+        Cars cars = new Cars(Arrays.asList(patCar, rickCar, ethanCar));
+        assertThat(cars.findFirstFurthestTraveledCar().getCarName()).isEqualTo("pat");
     }
 
     @Test
-    @DisplayName("Cars 객체에서 가장 멀리간 자동차의 거리를 찾아낸다")
+    @DisplayName("Cars 객체에서 가장 멀리간 자동차가 2개 이상일 경우 첫번째 자동차가 선택된다.")
+    void findFirstFurthestTraveledCarTest() {
+        Car patCar = new Car("pat");
+
+        Car rickCar = new Car("rick");
+
+        rickCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
+        rickCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
+
+        Car ethanCar = new Car("ethan");
+
+        ethanCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
+        ethanCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
+
+        Cars cars = new Cars(Arrays.asList(patCar, rickCar, ethanCar));
+        assertThat(cars.findFirstFurthestTraveledCar().getCarName()).isEqualTo("rick");
+    }
+
+    @Test
+    @DisplayName("Cars 객체에서 가장 멀리간 자동차의 거리를 반환한다.")
     void getFurthestTraveledCarDistanceTest() {
         Car patCar = new Car("pat");
 
@@ -57,10 +76,15 @@ class CarsTest {
         patCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
 
         Car rickCar = new Car("rick");
+
+        rickCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
+        rickCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
+        rickCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
+
         Car ethanCar = new Car("ethan");
 
-        Cars cars = new Cars(Arrays.asList(patCar,rickCar,ethanCar));
-        Car f = cars.getFurthestTraveledCar();
-        assertThat(cars.getFurthestDistance()).isEqualTo(2);
+        Cars cars = new Cars(Arrays.asList(patCar, rickCar, ethanCar));
+        Car f = cars.findFirstFurthestTraveledCar();
+        assertThat(cars.getFurthestDistance()).isEqualTo(3);
     }
 }
