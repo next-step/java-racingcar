8c1467d38be3ee1d2e823cd2014651263199c3ed
박태영 <>
2023-04-10 20:37:15 +0900

    패키지 구조 변경

---
+++ src/test/java/study/step1/StringTest.java
@@ -0,0 +1,49 @@
+package study.step1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    @DisplayName("문자열 자르기")
+    void split() {
+        //given
+        String input = "1,2";
+        //when
+        String[] result = input.split(",");
+        //then
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("substring을 이용한 괄호제거")
+    void removeBracket() {
+        //given
+        String input = "(1,2)";
+
+        //when
+        String substring = input.substring(1, 4);
+
+        //then
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 문자 찾기, 입력값 범위 밖을 경우 String index out of range 발생")
+    void findCharacter() {
+        //given
+        String msg = "abc";
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            char c = msg.charAt(msg.length());
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range")
+                .hasMessageContaining(String.valueOf(msg.length()));
+    }
+}
+
