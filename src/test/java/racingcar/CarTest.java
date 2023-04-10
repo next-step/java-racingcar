62b7bed514a12258cd548981e76b6f7bba9f2432
MoonHKLee <moonhyuk.lee@lotte.net>
2023-04-10 20:35:18 +0900

    test(car) : 자동차 전진 테스트 작성.

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -16,4 +16,13 @@ public class CarTest {
         car.move();
         assertThat(car.getPosition()).isEqualTo(0);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("자동차가 전진한다.")
+    void test2() {
+        Car car = new Car();
+        car.move();
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
 }
