47cb399a97f8ee16cbb244dc6e0a48d42f332cdc
ayla-c <ayla@MacBook-Pro-151.local>
2023-04-10 22:42:09 +0900

    feat: add RandomBool

--- src/main/java/racingcar/RandomBool.java
+++ src/main/java/racingcar/RandomBool.java
@@ -1,2 +1,22 @@
-package racingcar;public class RandomBool {
+package racingcar;
+
+import java.util.Random;
+
+public class RandomBool {
+    private static final Random RANDOM = new Random;
+
+    public static boolean next() {
+        return toBool(RANDOM.nextInt(10));
+    }
+
+    public static boolean toBool(int number) {
+        if (number >= 0 && number < 4) {
+            return false;
+        }
+        if (number >= 4 && number <= 9) {
+            return true;
+        }
+
+        throw new RuntimeException();
+    }
 }
