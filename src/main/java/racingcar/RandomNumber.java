7fc6f12bd8d5c8fb1d1d5f0b4e53c580265ca38d
MoonHKLee <moonhyuk.lee@lotte.net>
2023-04-10 21:13:08 +0900

    refactor(randomNumber) : 변수 상수화.

--- src/main/java/racingcar/RandomNumber.java
+++ src/main/java/racingcar/RandomNumber.java
@@ -3,12 +3,13 @@ package racingcar;
 import java.util.Random;
 
 public class RandomNumber {
+    private static final int RANDOM_NUMBER_BOUND = 10;
     static Random random = new Random();
 
     private RandomNumber() {
     }
 
     public static int getNumber() {
-        return random.nextInt(10);
+        return random.nextInt(RANDOM_NUMBER_BOUND);
     }
 }
