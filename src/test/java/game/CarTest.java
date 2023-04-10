532c7f575069756a1f35168104cd29a75f3d8062
In-wook Hwang <inwook9@gmail.com>
2023-04-11 01:26:07 +0900

    test: Car 테스트케이스 추가 및 README 반영

--- src/test/java/game/CarTest.java
+++ src/test/java/game/CarTest.java
@@ -25,4 +25,35 @@ class CarTest {
         }
         assertThat(car.moveCount()).isSameAs(driveCount);
     }
+
+    @Test
+    @DisplayName("자동차는 NumberGenerator 가 4 이상을 반환하면 움직인다.")
+    void test2() {
+        Car car = new Car(new MovableNumberGenerator());
+        car.drive();
+        assertThat(car.positionOfRep(0)).isEqualTo(CarAction.MOVE);
+    }
+
+    @Test
+    @DisplayName("자동차는 NumberGenerator 가 3 이하를 반환하면 움직이지 않는다.")
+    void test3() {
+        Car car = new Car(new NonMovableNumberGenerator());
+        car.drive();
+        assertThat(car.positionOfRep(0)).isEqualTo(CarAction.STOP);
+    }
+
+
+    static class MovableNumberGenerator implements NumberGenerator {
+        @Override
+        public int generate() {
+            return 5;
+        }
+    }
+
+    static class NonMovableNumberGenerator implements NumberGenerator {
+        @Override
+        public int generate() {
+            return 3;
+        }
+    }
 }
