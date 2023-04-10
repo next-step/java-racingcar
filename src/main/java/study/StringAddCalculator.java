bc37799bfdbbf54c3ce4a96765de26285b3a9294
letitgobaby <gnu@gnuui-MacBookPro.local>
2023-04-10 23:48:43 +0900

    step2 피드백 반영

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -4,6 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+
+    static Pattern customPattern = Pattern.compile("//(.)\n(.*)");
     
     static int splitAndSum(String text) {
         int result = 0;
@@ -13,12 +15,7 @@ public class StringAddCalculator {
         
         String[] tokens = splitByPattern(text);
         for (String token : tokens) {
-            int number = Integer.parseInt(token);
-            if (number < 0) {
-                throw new RuntimeException("Negative Number !");
-            }
-
-            result += Integer.parseInt(token);
+            result += parseInt(token);
         }
 
         return result;
@@ -32,12 +29,21 @@ public class StringAddCalculator {
     
     private static String[] splitByPattern(String text) {
         String[] tokens = text.split(",|:");
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher matcher = customPattern.matcher(text);
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
             tokens = matcher.group(2).split(customDelimiter);
         }
 
         return tokens;
+    }
+
+
+    private static int parseInt(String text) {
+        int parsedNum = Integer.parseInt(text);
+        if (parsedNum < 0) {
+            throw new RuntimeException("Negative Number !");
+        }
+        return parsedNum;
     }
 }
