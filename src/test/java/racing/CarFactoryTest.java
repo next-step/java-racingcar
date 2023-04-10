6c50d0ae5b7c71ab08fc2b651d5640d3cb22cc82
soochan <tncksdl05@gmail.com>
2023-04-10 17:39:48 +0900

    test : car factory 객체에 대한 테스트 코드 작성

---
+++ src/test/java/racing/CarFactoryTest.java
@@ -0,0 +1,20 @@
+package racing;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CarFactoryTest {
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 3, 4, 5})
+  void 입력_받은_숫자만큼_자동차_객체_생성(int carCount) {
+    CarFactory carFactory = new CarFactory();
+    List<Car> cars = carFactory.createCars(carCount);
+
+    assertThat(cars.size()).isEqualTo(carCount);
+  }
+
+}
