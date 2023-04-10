99f32f3a8e24c72728deaf9531ed4eb6b69a7997
zuri-pong <zuri.pong@kakaocorp.com>
2023-04-10 19:25:14 +0900

    3단계 - 자동차 경주 로직&테스트 코드 작성

---
+++ src/main/java/study/step3/RacingGame.java
@@ -0,0 +1,33 @@
+package study.step3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static study.step3.ResultView.moveResult;
+import static study.step3.ResultView.resultMsg;
+import static study.step3.ResultView.endingRoundMsg;
+
+public class RacingGame {
+
+    private static final int DEFAULT_MIN = 0;
+    private static final int DEFAULT_MAX = 9;
+    List<Car> carList;
+
+    public void readyGame(int carCount) {
+        carList = new ArrayList<>();
+        for (int i = 0; i < carCount; i++) {
+            carList.add(new Car(0));
+        }
+    }
+
+    public void startGame(int tryCount) {
+        resultMsg();
+        for (int i = 0; i < tryCount; i++) {
+            for (Car car : carList) {
+                if (car.canMove(DEFAULT_MIN,DEFAULT_MAX)) car.move();
+                moveResult(car.getPosition());
+            }
+            endingRoundMsg(i + 1);
+        }
+    }
+}
