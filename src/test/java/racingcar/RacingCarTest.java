752c0e138fec2b96e4643c581b2c343f70e58182
hoyeoon <hoyeon.backend@gmail.com>
2023-04-10 19:18:17 +0900

    test: random 값이 4 이상이면 전진한다.

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,5 +20,20 @@ public class RacingCarTest {
 
         //then
         assertThat(randomNumber).isBetween(0, 9);
+    }
+
+    @DisplayName("random 값이 4이상이면 전진한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"0:false","1:false","2:false","3:false",
+                        "4:true","5:true","6:true","7:true","8:true","9:true"}, delimiter = ':')
+    void move(int randomNumber, boolean result) {
+        //given
+        RacingCar racingCar = new RacingCar();
+
+        //when
+        boolean moveYn = racingCar.move(randomNumber);
+
+        //then
+        assertThat(moveYn).isEqualTo(result);
     }
 }
