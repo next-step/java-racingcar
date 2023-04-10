9e41dfff81cc301bc7b571ae1e8dd9fec666755d
MoonHKLee <moonhyuk.lee@lotte.net>
2023-04-10 20:48:52 +0900

    feat(car) : 자동차 전진 기능 구현.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,10 +2,23 @@ package racingcar;
 
 public class Car {
 
-    public void move() {
+    private int position;
+
+    public void push(int number) {
+        if (movable(number)) {
+            move();
+        }
+    }
+
+    private boolean movable(int number) {
+        return number >= 4;
+    }
+
+    private void move() {
+        position++;
     }
 
     public int getPosition() {
-        return 0;
+        return position;
     }
 }
