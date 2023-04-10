404d2f45428f9df166ef9952b6220c1c64b1bd18
garden-2 <1eejw@naver.com>
2023-04-10 17:53:24 +0900

    refactor: Delimiter enum class 분리

--- src/main/java/stringcalculator/Delimiters.java
+++ src/main/java/stringcalculator/Delimiters.java
@@ -3,23 +3,9 @@ package stringcalculator;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static stringcalculator.Delimiter.*;
+
 public class Delimiters {
-    private static final Map<String, String> REGEX_ESCAPE = new HashMap<>();
-    private static final String COMMA = ",";
-    private static final String COLON = ":";
-    private static final String BLANK_STRING = " ";
-    private static final String DOUBLE_SLASH = "//";
-    private static final String CUSTOM_END_STRING = "\\n";
-    private static final String ESCAPE_CUSTOM_END_STRING = "\\\\n";
-
-    static {
-        REGEX_ESCAPE.put("+", "\\+");
-        REGEX_ESCAPE.put("{", "\\{");
-        REGEX_ESCAPE.put("(", "\\(");
-        REGEX_ESCAPE.put(")", "\\)");
-        REGEX_ESCAPE.put("[", "\\[");
-    }
-
     private final Set<String> delimiters = new HashSet<>();
 
     private Delimiters(String text) {
@@ -33,16 +19,16 @@ public class Delimiters {
 
     public List<String> split(String text) {
         String result = text;
-        if (text.contains(CUSTOM_END_STRING)) {
-            result = text.split(ESCAPE_CUSTOM_END_STRING)[1];
+        if (text.contains(CUSTOM_END_STRING.getValue())) {
+            result = text.split(ESCAPE_CUSTOM_END_STRING.getValue())[1];
         }
 
         for (String delimiter : delimiters) {
             delimiter = REGEX_ESCAPE.getOrDefault(delimiter, delimiter);
-            result = result.replaceAll(delimiter, BLANK_STRING);
+            result = result.replaceAll(delimiter, BLANK_STRING.getValue());
         }
 
-        return Arrays.stream(result.split(BLANK_STRING)).collect(Collectors.toList());
+        return Arrays.stream(result.split(BLANK_STRING.getValue())).collect(Collectors.toList());
     }
 
     private void validateDelimiter(String text) {
@@ -52,16 +38,12 @@ public class Delimiters {
         }
     }
 
-    private boolean containsCustomDelimiter(String text) {
-        return text.contains(CUSTOM_END_STRING) && text.startsWith(DOUBLE_SLASH);
-    }
-
     public boolean containsDelimiters() {
         return !delimiters.isEmpty();
     }
 
     private void initializeDelimiters() {
-        delimiters.add(COMMA);
-        delimiters.add(COLON);
+        delimiters.add(COMMA.getValue());
+        delimiters.add(COLON.getValue());
     }
 }
