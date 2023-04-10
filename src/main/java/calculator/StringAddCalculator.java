e86440fcb1228e584fd93bd65487f32eb0421303
SMJminjeong <sdwp0920@gmail.com>
2023-04-10 23:38:42 +0900

    Step2 문자열 계산기 Test

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,63 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    public static final int ZERO = 0;
+    public static final String DELIMITER = ",|:";
+    public static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+
+    public static int splitAndSum(String text) {
+        if(isBlank(text))
+        {
+            return ZERO;
+        }
+
+        return sum(toInts(split(text)));
+    }
+
+    private static String[] split(String text) {
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(text);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+
+        return text.split(DELIMITER);
+    }
+
+    private static boolean isBlank(String text){
+        return text == null || text.isBlank();
+    }
+
+
+    private static int sum(int[] numbers) {
+        int sum = ZERO;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    private static int[] toInts(String[] values) throws NumberFormatException{
+        int[] numbers = new int[values.length];
+        for(int i = 0; i < values.length; i++)
+        {
+            int number = toInt(values[i]);
+            numbers[i] = number;
+        }
+        return numbers;
+    }
+
+    private static int toInt(String values) {
+        int number = Integer.parseInt(values);
+        if(number < 0){
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
+        return number;
+    }
+
+}
