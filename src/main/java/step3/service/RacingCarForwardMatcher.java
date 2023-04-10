bb2c45edebd9f9665be1cb86eea4dff8f147ccd1
juhyun <zzang9ha@naver.com>
2023-04-11 00:01:33 +0900

    refactor: 랜덤 값 생성 로직 -> RandomValue 객체에 위임

--- src/main/java/step3/service/RacingCarForwardMatcher.java
+++ src/main/java/step3/service/RacingCarForwardMatcher.java
@@ -1,15 +1,11 @@
 package step3.service;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 public class RacingCarForwardMatcher implements GameMatcher {
 
-    private static final ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
-    private static final int RANDOM_BOUND = 10;
     public static final int CAR_FORWARD_MINIMUM_NUMBER = 4;
 
     @Override
-    public boolean matcher() {
-        return RANDOM.nextInt(RANDOM_BOUND) >= CAR_FORWARD_MINIMUM_NUMBER;
+    public boolean match(int random) {
+        return random >= CAR_FORWARD_MINIMUM_NUMBER;
     }
 }
