31745059bc3ecd86c750ba2e1a2fc2de1f053fe6
ian.ja <ian.ja@kakaocorp.com>
2023-04-10 18:16:34 +0900

    feat: InputView 구현

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+    static Scanner scanner = new Scanner(System.in);
+    static String carNumber;
+    static String tryNumber;
+
+    public static void getCarNumber() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        carNumber = scanner.nextLine();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        tryNumber = scanner.nextLine();
+    }
+}
---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+    @Test
+    void inputViewTest() {
+        InputView.getCarNumber();
+    }
+}
