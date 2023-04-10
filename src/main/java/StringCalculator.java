5e7a6622f599ea3d2fac030a3804bad41fa345b2
이재현 <izg0820@naver.com>
2023-04-10 23:28:34 +0900

    Step2 문자열 계산기

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class StringCalculator {
+    public int sum(String input) throws RuntimeException {
+        if(Util.isNullOrBlank(input)) {
+            input = "0";
+        }
+        String[] nums = getNumbers(input);
+        Util.hasMinusNumber(nums);
+        return sumArray(nums);
+    }
+
+    private String[] getNumbers(String input) {
+        if (Util.hasCustomDelimiter(input)) {
+            return Util.splitCustomDelimiter(input);
+        }
+        return Util.split(input);
+    }
+
+    private int sumArray(String[] nums) {
+        return Arrays.stream(nums)
+                .map(num -> Util.isNullOrBlank(num) ? 0 : Util.convertInput(num))
+                .reduce(0, Integer::sum);
+    }
+
+
+}
--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -4,19 +4,20 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class UtilTest {
 
     @ParameterizedTest
     @NullAndEmptySource
-    @DisplayName("input값이 null 혹은 빈문자열인지 확인하는 메소드")
+    @DisplayName("input값이 null 혹은 빈문자열인지 확인 테스트")
     public void isNullOrBlankTest(String input) {
         assertThat(Util.isNullOrBlank(input)).isTrue();
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1,1", "2,2","3,3"})
+    @DisplayName("숫자 변환 테스트")
     public void convertInputTest(String input, int result) {
         assertThat(Util.convertInput(input)).isEqualTo(result);
     }
@@ -25,6 +26,21 @@ public class UtilTest {
     @ValueSource(strings = {"1,2:3", "1:2:3", "1,2,3"})
     @DisplayName("split 테스트")
     public void splitTest(String str) {
-        assertThat(Util.split(str, ",|:").length).isEqualTo(3);
+        assertThat(Util.split(str).length).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3", "//:\n1:2:3"})
+    @DisplayName("//와 \n 문자사이에 커스텀 구분자가 있는지 판별 후 boolean 반환")
+    public void hasCustomDelimiterTest(String input) {
+        assertThat(Util.hasCustomDelimiter(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2,3", "1,-2,3"})
+    @DisplayName("음수가 있는지 확인 테스트")
+    public void hasMinusNumberTest(String input) {
+        String[] nums = Util.split(input);
+        assertThatThrownBy(() -> Util.hasMinusNumber(nums)).isInstanceOf(RuntimeException.class);
     }
 }
