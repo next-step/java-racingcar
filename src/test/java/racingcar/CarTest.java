7c153869c46c0d2866d704d4ee73e0ff37c28947
MoonHKLee <moonhyuk.lee@lotte.net>
2023-04-10 20:49:27 +0900

    refactor(car) : 자동차 전진 테스트 변경.

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -11,18 +11,18 @@ public class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
     @DisplayName("자동차가 정지한다.")
-    void test1() {
+    void test1(int number) {
         Car car = new Car();
-        car.move();
+        car.push(number);
         assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     @DisplayName("자동차가 전진한다.")
-    void test2() {
+    void test2(int number) {
         Car car = new Car();
-        car.move();
+        car.push(number);
         assertThat(car.getPosition()).isEqualTo(1);
     }
 }
