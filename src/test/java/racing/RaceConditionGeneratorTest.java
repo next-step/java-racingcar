317ec1cf077830ade0802e610b0a750730e09c42
kei.choi <kei.choi@kakaocorp.com>
2023-04-10 19:33:26 +0900

    [style] 코드 포맷 변경

--- src/test/java/racing/RaceConditionGeneratorTest.java
+++ src/test/java/racing/RaceConditionGeneratorTest.java
@@ -15,7 +15,6 @@ public class RaceConditionGeneratorTest {
 
     @Test
     void isForwardableTest() {
-
         List<Boolean> results = Lists.newArrayList();
 
         for (int i = 0; i < 10; i++) {
@@ -31,7 +30,6 @@ public class RaceConditionGeneratorTest {
     @ParameterizedTest(name = "숫자 4 기준으로 (이상은 true), (미만은 false)로 반환하는지 테스트")
     @CsvSource(value = {"4, 4, true", "4, 5, true", "4, 3, false"})
     void isGreaterThanEqualTest(Integer base, Integer value, boolean expected) {
-
         boolean result = RaceConditionGenerator.isGreaterThanEqualToBase(base, value);
 
         assertThat(result).isEqualTo(expected);
@@ -39,7 +37,6 @@ public class RaceConditionGeneratorTest {
 
     @RepeatedTest(value = 10, name = "0~10 사이 난수 생성 테스트")
     void 난수_생성_테스트() {
-
         int limit = 10;
         int randNum = RaceConditionGenerator.randPositiveOrZero(limit);
 
