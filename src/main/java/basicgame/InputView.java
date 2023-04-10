2160242dd1eda5421dde6e6ea7eb4cd472a45552
wotjd4305 <wotjd4305@naver.com>
2023-04-11 00:12:28 +0900

    feat. step3 - 입력, Input-view

---
+++ src/main/java/basicgame/InputView.java
@@ -0,0 +1,12 @@
+package basicgame;
+
+public class InputView {
+
+    public static void printCarInput(){
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    public static void printTryInput(){
+        System.out.println("시도할 회수는 몇 회 인가요?");
+    }
+}
