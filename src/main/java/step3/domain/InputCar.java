6e54743b2c2140330e8d5b1e778709dd78508d94
juhyun <zzang9ha@naver.com>
2023-04-11 01:18:56 +0900

    refactor: input 유효성 검증 로직 추가

--- src/main/java/step3/domain/InputCar.java
+++ src/main/java/step3/domain/InputCar.java
@@ -7,8 +7,16 @@ public class InputCar {
     private final int retry;
 
     public InputCar(int count, int retry) {
+        validate(count, retry);
+        
         this.count = count;
         this.retry = retry;
+    }
+
+    private void validate(int count, int retry) {
+        if (count < 0 || retry < 0) {
+            throw new IllegalArgumentException("count, retry should be positive - count: " + count + ", retry: " + retry);
+        }
     }
 
     public int getCount() {
