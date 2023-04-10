6dceca670ef5c3eddaa661777190504abfa46fce
In-wook Hwang <inwook9@gmail.com>
2023-04-11 00:08:09 +0900

    refactor: RacingOptoins 에 0 또는 음수에 대한 검증로직을 추가

--- src/main/java/game/RacingOptions.java
+++ src/main/java/game/RacingOptions.java
@@ -5,8 +5,17 @@ public class RacingOptions {
     private final int racingRep;
 
     public RacingOptions(int carCount, int racingRep) {
+        throwIfNegativeNumber(carCount);
+        throwIfNegativeNumber(racingRep);
+
         this.carCount = carCount;
         this.racingRep = racingRep;
+    }
+
+    private void throwIfNegativeNumber(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("입력은 0 보다 큰 양수만 가능합니다.");
+        }
     }
 
     public int racingRep() {
