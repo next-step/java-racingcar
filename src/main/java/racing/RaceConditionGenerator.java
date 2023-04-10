13d902969b2d20f3fcd95bc2e2a10a68b5bd1811
kei.choi <kei.choi@kakaocorp.com>
2023-04-10 18:32:31 +0900

    (feat) 자동차 전진가능여부 조건 확인하는 함수 생성

--- src/main/java/racing/RaceConditionGenerator.java
+++ src/main/java/racing/RaceConditionGenerator.java
@@ -1,19 +1,35 @@
 package racing;
 
-import java.util.Random;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class RaceConditionGenerator {
 
-    public static int randPositiveOrZero(int limit) {
-        long seed = System.currentTimeMillis();
-        Random random = new Random(seed);
+    private static final Integer RACE_CONDITION_NUM_LIMIT = 10;
+    private static final Integer BASE_VALUE = 4;
 
-        return random.nextInt(limit);
+    // 0 <= generated value < limit
+    public static Integer randPositiveOrZero(int limit) {
+        Integer num;
+        
+        try {
+            SecureRandom instanceStrong = SecureRandom.getInstanceStrong();
+            num = instanceStrong.nextInt(limit);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException("난수 생성에 실패했습니다.");
+        }
+
+        return num;
     }
 
     public static Boolean isGreaterThanEqualToBase(Integer base, Integer value) {
         int result = value.compareTo(base);
         return result >= 0;
+    }
+
+    public static Boolean isForwardable() {
+        return isGreaterThanEqualToBase(BASE_VALUE, randPositiveOrZero(RACE_CONDITION_NUM_LIMIT));
     }
 
 }
