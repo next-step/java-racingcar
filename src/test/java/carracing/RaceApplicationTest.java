9be06ba4cf153fb0e9d491c0e77321a4b76eebb6
kingdong <ultraraptor@naver.com>
2023-04-10 12:08:06 +0900

    test : 클래스의 분리로 인한 테스트 코드의 이동

--- src/test/java/carracing/RaceApplicationTest.java
+++ src/test/java/carracing/RaceApplicationTest.java
@@ -22,6 +22,7 @@ import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class RaceApplicationTest {
@@ -49,43 +50,6 @@ public class RaceApplicationTest {
         this.inputPresentation = new InputPresentation(inputViewInputStream, resultViewPrintStream);
         this.raceApplication = new RaceApplication(this.inputPresentation, this.resultPresentation);
     }
-
-    @DisplayName("경기횟수 & 참가차량수 주어지면 그에 맞는 경기결과를 리턴한다")
-    @Test
-    public void participatesAndIterations() {
-        //given
-        int participates = 9;
-        int iterations = 4;
-
-        //when
-        log.info("자동차 경주의 정보 > 참가자수=[{}명], 자동차 경주의 Round 횟수=[{}판]");
-        Map<Round, List<Score>> raceResults = raceApplication.racingStart(participates, iterations);
-
-        //then
-        assertAll(
-            () -> assertThat(raceResults.keySet()).as("경기 반복횟수 검증").hasSize(iterations),
-            () -> assertThat(raceResults.get(new Round(1))).as("경기 참가자수 검증 - 1번경가").hasSize(participates),
-            () -> raceResults.values().forEach(s -> assertThat(s).as("경기 참가자수 검증 - 모든경가").hasSize(participates))
-        );
-    }
-
-    @DisplayName("몇대의 자동차로 이동할수 있는지 지정할 수 있다")
-    @Test
-    public void countOfMovingCar() {
-        //given
-        int participate = 55;
-        int iterations = 33;
-
-        //when
-        Map<Round, List<Score>> roundListMap = raceApplication.racingStart(participate, iterations);
-
-        //then
-        assertAll(
-            () -> assertThat(roundListMap.keySet()).as("몇번 이동하는지 검증").hasSize(iterations),
-            () -> assertThat(roundListMap.get(new Round(1))).as("몇대가 움직이는지 검증").hasSize(participate)
-        );
-    }
-
     @DisplayName("문구 출력 검증 : [자동차 대수는 몇 대 인가요?] 출력된다")
     @Test
     public void participates() {
@@ -117,29 +81,14 @@ public class RaceApplicationTest {
         assertThat(resultViewOutputStream.toString().trim()).contains(answer);
     }
 
-    @DisplayName("입력한 경기수 (Round) 에 따라 해당 경기수만큼 잘 치뤄진건지 검증한다")
-    @Test
-    public void movingIteration() {
-        //given
-        int roundIterations = 22;
-
-        //when
-        Map<Round, List<Score>> roundListMap = raceApplication.racingStart(11, roundIterations);
-
-        //then
-        assertAll(
-            () -> assertThat(roundListMap.keySet()).as("Round 숫자를 검증").hasSize(roundIterations),
-            () -> assertThat(roundListMap.values()).as("List<Score> 를 검증").hasSize(roundIterations)
-        );
-    }
-
     @DisplayName("0에서 9사이에서 random값을 구한다")
     @RepeatedTest(100)
     public void getRandomFrom0To9() {
         //given
         //when
-        int random = raceApplication.randomScore();
+        //int random = raceApplication.randomScore();
         //then
-        assertThat(random).isBetween(0, 9);
+        //assertThat(random).isBetween(0, 9);
+        fail();
     }
 }
--- src/test/java/carracing/service/RacingServiceTest.java
+++ src/test/java/carracing/service/RacingServiceTest.java
@@ -12,6 +12,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class RacingServiceTest {
     private static final Logger log = Logger.getLogger("AutomobileFederationTest");
     private static final Map<Round, List<Score>> inputRoundToScoreListMap = Map.of(
@@ -22,11 +25,12 @@ public class RacingServiceTest {
         new Round(4), Arrays.asList(new Score(8), new Score(8), new Score(8))
     );
 
-    RacingService federation;
+    private RacingService racingService;
+
 
     @BeforeEach
     public void beforeEach() {
-        federation = new RacingService(new RoundRepository());
+        racingService = new RacingService(new RoundRepository());
     }
 
     @DisplayName("입력 결기경과의 수 대로 ")
@@ -40,6 +44,61 @@ public class RacingServiceTest {
         //then
         log.warning("테스트코드가 요구사항에 의존적이지 않고 구현에 의존적이다. 이렇게 되면 구현이 변경될때 테스트코드도 변경될 여지가 있다");
     }
+
+
+    @DisplayName("입력한 경기수 (Round) 에 따라 해당 경기수만큼 잘 치뤄진건지 검증한다")
+    @Test
+    public void movingIteration() {
+        //given
+        int roundIterations = 22;
+
+        //when
+        Map<Round, List<Score>> roundListMap = racingService.racingStart(11, roundIterations);
+
+        //then
+        assertAll(
+            () -> assertThat(roundListMap.keySet()).as("Round 숫자를 검증").hasSize(roundIterations),
+            () -> assertThat(roundListMap.values()).as("List<Score> 를 검증").hasSize(roundIterations)
+        );
+    }
+
+
+    @DisplayName("경기횟수 & 참가차량수 주어지면 그에 맞는 경기결과를 리턴한다")
+    @Test
+    public void participatesAndIterations() {
+        //given
+        int participates = 9;
+        int iterations = 4;
+
+        //when
+        log.info("자동차 경주의 정보 > 참가자수=[{}명], 자동차 경주의 Round 횟수=[{}판]");
+        Map<Round, List<Score>> raceResults = racingService.racingStart(participates, iterations);
+
+        //then
+        assertAll(
+            () -> assertThat(raceResults.keySet()).as("경기 반복횟수 검증").hasSize(iterations),
+            () -> assertThat(raceResults.get(new Round(1))).as("경기 참가자수 검증 - 1번경가").hasSize(participates),
+            () -> raceResults.values().forEach(s -> assertThat(s).as("경기 참가자수 검증 - 모든경가").hasSize(participates))
+        );
+    }
+
+    @DisplayName("몇대의 자동차로 이동할수 있는지 지정할 수 있다")
+    @Test
+    public void countOfMovingCar() {
+        //given
+        int participate = 55;
+        int iterations = 33;
+
+        //when
+        Map<Round, List<Score>> roundListMap = racingService.racingStart(participate, iterations);
+
+        //then
+        assertAll(
+            () -> assertThat(roundListMap.keySet()).as("몇번 이동하는지 검증").hasSize(iterations),
+            () -> assertThat(roundListMap.get(new Round(1))).as("몇대가 움직이는지 검증").hasSize(participate)
+        );
+    }
+
 
 
 }
