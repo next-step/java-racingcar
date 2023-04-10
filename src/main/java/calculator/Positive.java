a5b1912a06033f30250f9af2fb54a4b4975ab643
wotjd4305 <wotjd4305@naver.com>
2023-04-10 23:02:13 +0900

    refactor. step2 - 피드백 반영(양수 클래스 수정 및 사용)

--- src/main/java/calculator/Positive.java
+++ src/main/java/calculator/Positive.java
@@ -4,8 +4,11 @@ public class Positive {
 
     private final int number;
 
-    public Positive(String s){
-        this.number = Integer.parseInt(s);
+    public Positive(String text){
+        this.number = Integer.parseInt(text);
+        if (this.number < 0) {
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
+        }
     }
 
     public int getNumber(){
