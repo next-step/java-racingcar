347be743c5ad6a7a491f8c4d42bb2768f6a76427
In-wook Hwang <inwook9@gmail.com>
2023-04-12 02:29:40 +0900

    fix: 자동차 움직이는 조건 수정

--- src/main/java/game/domain/policy/StandardCarMovePolicy.java
+++ src/main/java/game/domain/policy/StandardCarMovePolicy.java
@@ -3,6 +3,6 @@ package game.domain.policy;
 public class StandardCarMovePolicy implements CarMovePolicy {
     @Override
     public boolean isMovable(int input) {
-        return input > 4;
+        return input >= 4;
     }
 }
