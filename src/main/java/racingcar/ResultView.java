87e16e9c7d1b2c7b1320ef11862f01387dd1777e
hoyeoon <hoyeon.backend@gmail.com>
2023-04-11 13:50:18 +0900

    feat: UI(System.out) 로직 구현

---
+++ src/main/java/racingcar/ResultView.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class ResultView {
+    public void questionCarCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    public void questionTryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+    }
+
+    public void executeResult() {
+        System.out.println("실행 결과");
+    }
+
+    public void printCarMoveStateLine(String line) {
+        System.out.println(line);
+    }
+}
