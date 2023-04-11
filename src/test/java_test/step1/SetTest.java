77af520dd8b50d4281a1bdee3872229cb64c1944
1kihong <98547689+1kihong@users.noreply.github.com>
2023-04-11 13:18:56 +0900

    Step1 (#4376)
    Co-authored-by: kihong <kihong@kihongs-MacBook-Air.local>

---
+++ src/test/java_test/step1/SetTest.java
@@ -0,0 +1,41 @@
+package java_test.step1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+    @Test
+    void sizeSet() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void valueScoreContains(int number) {
+        assertThat(numbers).contains(number);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:false", "1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    void csvSourceContains(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+}
