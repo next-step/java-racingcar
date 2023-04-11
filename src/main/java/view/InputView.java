2220815816cd84fd991d6d19511cb3838e395f7d
eohyemin <eohyemin@eohyemin-ui-MacBookPro.local>
2023-04-11 20:25:03 +0900

    feat: InputView 입력 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String CAR_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String STAGE_INPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+
+    public static int carInput() {
+        System.out.println(CAR_INPUT_MESSAGE);
+        return scanner.nextInt();
+    }
+
+    public static int stageInput() {
+        System.out.println(STAGE_INPUT_MESSAGE);
+        return scanner.nextInt();
+    }
+
+}
