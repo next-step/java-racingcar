711aa6c13cab9726d7ddbaa4eace7694a8a5704c
kingdong <ultraraptor@naver.com>
2023-04-11 21:45:04 +0900

    test : 로직을 도메인계층으로 이동

--- src/main/java/carracing/domain/Round.java
+++ src/main/java/carracing/domain/Round.java
@@ -1,12 +1,18 @@
 package carracing.domain;
 
+import carracing.repository.RoundRepository;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.IntUnaryOperator;
+
+import static carracing.RaceApplication.ioc;
 
 public class Round implements Comparable<Round> {
     private static final int FIRST_ROUND_VALUE = 1;
+    private static final AtomicInteger INDEX = new AtomicInteger(FIRST_ROUND_VALUE);
     public static final List<Score> emptyScores = new ArrayList<>();
     private static final IntUnaryOperator validate = value -> {
         if (value < FIRST_ROUND_VALUE) {
@@ -14,31 +20,46 @@ public class Round implements Comparable<Round> {
         }
         throw new RuntimeException("라운드는 1부터 시작하므로 항상 1 이상입니다");
     };
-    private int id;
+    private Integer roundId;
     private List<Score> scores;
     private List<Record> records;
 
     public Round(List<Score> scoreList) {
+        this.roundId = INDEX.getAndIncrement();
         this.scores = scoreList;
         this.records = scoreToRecord(scoreList);
     }
 
     private List<Record> scoreToRecord(List<Score> scoreList) {
+        AtomicInteger integer = new AtomicInteger(0);
         List<Record> recordList = new ArrayList<>();
         for(Score score : scoreList) {
-            recordList.add(new Record(score.toProgress() + score.prevScoreProgress()));
+            recordList.add(new Record(calRecord(integer, score)));
         }
         return recordList;
     }
 
+    private String calRecord(AtomicInteger integer, Score score) {
+        if(isFirstRound() ) {
+            return score.toProgress();
+        }
+        return score.toProgress() + prevScoreProgress(integer.getAndIncrement());
+    }
+
+    private String prevScoreProgress(int index) {
+        RoundRepository roundRepository = (RoundRepository) ioc.get(RoundRepository.class);
+        Round byId = roundRepository.findById(this.getRoundId()-  1);
+        return byId.getRecords().get(index).getRecord();
+    }
+
     public boolean isFirstRound() {
-        return this.id == FIRST_ROUND_VALUE;
+        return this.roundId <= FIRST_ROUND_VALUE;
     }
 
 
     @Override
     public int compareTo(Round other) {
-        return Integer.compare(this.getId(), other.getId());
+        return Integer.compare(this.getRoundId(), other.getRoundId());
     }
 
     @Override
@@ -46,16 +67,16 @@ public class Round implements Comparable<Round> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Round round = (Round) o;
-        return id == round.id;
+        return roundId == round.roundId;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(roundId);
     }
 
-    public int getId() {
-        return id;
+    public int getRoundId() {
+        return roundId;
     }
 
     public List<Score> getScores() {
@@ -66,7 +87,7 @@ public class Round implements Comparable<Round> {
         return records;
     }
 
-    public void setId(int i) {
-        this.id = i;
+    public void setRoundId(int i) {
+        this.roundId = i;
     }
 }
