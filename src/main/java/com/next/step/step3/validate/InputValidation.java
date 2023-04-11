cd70f30c86522f42d3047bb8547cf7cebb06f502
ghkdwlgns612 <ghkdwlgns612@naver.com>
2023-04-11 22:04:56 +0900

    feat : InputValidation클래스 변경 및 테스트 작성.

--- src/main/java/com/next/step/step3/validate/InputValidation.java
+++ src/main/java/com/next/step/step3/validate/InputValidation.java
@@ -1,6 +1,12 @@
 package com.next.step.step3.validate;
 
+import java.util.List;
+
 public class InputValidation {
+
+    private static final int MIN_CAR_NAME_LENGTH = 1;
+
+    private static final int MAX_CAR_NAME_LENGTH = 5;
 
     public void validateNotNegative(int inputNum) {
         if (inputNum < 0) {
@@ -8,15 +14,26 @@ public class InputValidation {
         }
     }
 
-    public void validateNumOfCars(int numOfCars) {
-        if (numOfCars == 0) {
-            throw new RuntimeException("최소 1대의 차량이 필요합니다.");
+    public void validateExistName(String carNames) {
+        if (carNames == null || carNames.isEmpty()) {
+            throw new RuntimeException("빈 문자열이나 NULL값은 입력할 수 없습니다.");
         }
     }
 
     public void validateNumOfAttempts(int numOfAttempts) {
         if (numOfAttempts == 0) {
             throw new RuntimeException("최소 1번의 경기는 진행되어야 합니다.");
+        }
+    }
+
+    public void validateCarNameLength(List<String> carNames) {
+        for (String carName : carNames) {
+            if (carName.length() > MAX_CAR_NAME_LENGTH) {
+                throw new RuntimeException("자동차 이름의 최대 길이는 5글자입니다.");
+            }
+            if (carName.length() < MIN_CAR_NAME_LENGTH) {
+                throw new RuntimeException("자동차 이름의 최소 길이는 1글자입니다.");
+            }
         }
     }
 }
--- src/test/java/com/next/step/step3/validate/InputValidationTest.java
+++ src/test/java/com/next/step/step3/validate/InputValidationTest.java
@@ -3,7 +3,13 @@ package com.next.step.step3.validate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -19,13 +25,42 @@ public class InputValidationTest {
                 .hasMessage("입력에 음수 값이 포함될 수 없습니다.");
     }
 
-    @Test
-    @DisplayName("자동차 대수가 0일 때 예외가 발생하는 테스트")
-    void validateNumOfCars_자동차_대수가_0인_경우_테스트() {
-        int numOfCars = 0;
-        assertThatThrownBy(() -> inputValidation.validateNumOfCars(numOfCars))
+    @ParameterizedTest(name = "자동차 이름이 NULL이나 빈 문자열일 때 예외가 발생하는 테스트")
+    @NullAndEmptySource
+    void validateNumOfCars_자동차_이름이_NULL_또는_빈_문자열인_경우_테스트(String input) {
+        assertThatThrownBy(() -> inputValidation.validateExistName(input))
                 .isInstanceOf(RuntimeException.class)
-                .hasMessage("최소 1대의 차량이 필요합니다.");
+                .hasMessage("빈 문자열이나 NULL값은 입력할 수 없습니다.");
+    }
+
+    @ParameterizedTest(name = "자동차 이름들 중 빈 문자열이 존재할 경우")
+    @MethodSource("provideLessMinLengthCarName")
+    void validateCarNameLength_자동차_이름들_가운데_빈_문자열인_경우(List<String> carNames) {
+        assertThatThrownBy(() -> inputValidation.validateCarNameLength(carNames))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("자동차 이름의 최소 길이는 1글자입니다.");
+    }
+
+    static Stream<Arguments> provideLessMinLengthCarName() {
+        return Stream.of(
+                Arguments.of(List.of("a", "", "c")),
+                Arguments.of(List.of("pobi", "crong", ""))
+        );
+    }
+
+    @ParameterizedTest(name = "자동차 이름들 중 5글자 이상이 존재할 경우")
+    @MethodSource("provideOverMaxLengthCarName")
+    void validateCarNameLength_자동차_이름들_가운데_5글자_이상의_문자열인_경우(List<String> carNames) {
+        assertThatThrownBy(() -> inputValidation.validateCarNameLength(carNames))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("자동차 이름의 최대 길이는 5글자입니다.");
+    }
+
+    static Stream<Arguments> provideOverMaxLengthCarName() {
+        return Stream.of(
+                Arguments.of(List.of("a", "b", "cccccc")),
+                Arguments.of(List.of("pobi", "crong", "martini"))
+        );
     }
 
     @Test
