a68bf32228021c176a2e7faec80dcb89baa5a4aa
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-11 12:59:34 +0900

    feat: 자동차 대수와 시도횟수 입력 기능 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class Car {
+    public boolean initParticipationNumber(int number) {
+        if (number  <= 0) {
+            throw new IllegalArgumentException("자동차 대수는 양수만 입력가능합니다");
+        }
+        return true;
+    }
+
+    public boolean initRepetitionCount(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("자동차 시도 횟수는 양수만 입력 가능합니다");
+        }
+        return true;
+    }
+}
