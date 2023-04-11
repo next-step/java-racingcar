8499927781d05e235de4bd9f523547f3989b41e0
highjune <highjune37@gmail.com>
2023-04-11 23:31:23 +0900

    두번째 요구사항 테스트 후 리팩토링

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,10 +1,21 @@
 public class StringAddCalculator {
 
     public static int splitAndSum(String val) {
-        int result = Integer.parseInt(val);
-        if (val == null || val.isBlank()) {
-            return result;
+        if (isInvalidValue(val)) {
+            return 0;
         }
+        int result = Integer.parseInt(val);
         return result;
     }
+
+    private static boolean isInvalidValue(String val) {
+        if (val == null || val == "") {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
 }
