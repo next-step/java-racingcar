680fa94dba7eb769f0823ab4215a23c2a6c7748e
kyoungil.yoon <dev.atypical@gmail.com>
2023-04-11 23:38:38 +0900

    feat: draw ui class

---
+++ src/main/java/step3/ResultView.java
@@ -0,0 +1,23 @@
+package step3;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ResultView {
+
+    public static final String DASH = "-";
+    public static void generateUI(Map<Integer, AtomicInteger> cars) {
+        for (Map.Entry<Integer, AtomicInteger> entry :cars.entrySet()) {
+            drawDash(entry.getValue().intValue());
+        }
+        System.out.print("\n");
+
+    }
+
+    private static void drawDash(int count) {
+        for (int i = 0; i <= count; i++) {
+            System.out.print(DASH);
+        }
+        System.out.print("\n");
+    }
+}
