919057ce8217135c4e95af2b8dd42c95a11064b4
lehdqlsl <lehdqlsl@naver.com>
2023-04-11 14:17:06 +0900

    test: 커스텀 구분자 기능 구현 및 테스트 코드 작성

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -2,13 +2,29 @@ package step2;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator implements Calculator {
+
+    private static final String CUSTOM_SEPARATOR_REGEX = "//(.)\n(.*)";
+    private static final String DEFAULT_SEPARATOR_REGEX = ",|:";
+    public static final int CUSTOM_DELIMITER_GROUP = 1;
+    public static final int TEXT_GROUP = 2;
+    private static Pattern pattern = Pattern.compile(CUSTOM_SEPARATOR_REGEX);
+
 
     public static int splitAndSum(String text) {
         if (validText(text)) return 0;
 
-        String[] split = text.split(",|:");
+        String[] split;
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(CUSTOM_DELIMITER_GROUP);
+            split = matcher.group(TEXT_GROUP).split(customDelimiter);
+        } else {
+            split = text.split(DEFAULT_SEPARATOR_REGEX);
+        }
 
         return Arrays.stream(split)
                 .mapToInt(Integer::parseInt)
--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -34,4 +34,11 @@ public class StringCalculatorTest {
         int result = StringCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
 }
