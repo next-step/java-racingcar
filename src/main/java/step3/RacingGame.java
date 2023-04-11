eccc4b977c37d146da632d2ea718ace820958c74
양해준 <haejun.yang@fssuniverse.com>
2023-04-11 20:36:44 +0900

    feat (사용자로부터 입력값 받기): 입력값을 받기 위한 UI 로직 InputView 생성

---
+++ src/main/java/step3/RacingGame.java
@@ -0,0 +1,11 @@
+package step3;
+
+public class RacingGame {
+    private final InputView inputView = new InputView();
+
+    public void play() {
+        int numberOfCar = inputView.getNumberOfCar();
+        int count = inputView.getCount();
+    }
+
+}
