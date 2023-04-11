43c1baa211c81ed14fddc4234bd5eb98004869ad
kyoungil.yoon <dev.atypical@gmail.com>
2023-04-11 23:38:24 +0900

    feat: iterate period

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -8,10 +8,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Car {
 
     public static final int RANDOM_NUMBER_LIMIT = 4;
+
     public static Map<Integer, AtomicInteger> generateCars(int carCount) {
         Map<Integer, AtomicInteger> cars = new HashMap<>();
-        for (int i = 1; i <= carCount; i++) {
-            cars.put(i, new AtomicInteger(1));
+        for (int i = 0; i < carCount; i++) {
+            cars.put(i, new AtomicInteger(0));
         }
         return cars;
     }
@@ -20,5 +21,33 @@ public class Car {
         Random random = new Random();
         return random.nextInt(10);
     }
-    
+
+    public static Map<Integer, AtomicInteger> iteratePeriod(int period, Map<Integer, AtomicInteger> cars) {
+        System.out.println("실행결과");
+        for (int i = 0; i < period; i++) {
+            Map<Integer, AtomicInteger> periods = tryPeriod(cars);
+        }
+
+
+        return cars;
+    }
+
+    public static boolean isRandomNumberOverFour() {
+        return generateRandomNumber() >= RANDOM_NUMBER_LIMIT;
+    }
+
+    public static Map<Integer, AtomicInteger> tryPeriod(Map<Integer, AtomicInteger> cars) {
+        ResultView.generateUI(cars);
+        for (Map.Entry<Integer, AtomicInteger> entry: cars.entrySet()) {
+            doForward(entry);
+        }
+
+        return cars;
+    }
+
+    private static void doForward(Map.Entry<Integer, AtomicInteger> entry) {
+        if (isRandomNumberOverFour()) {
+            entry.getValue().getAndIncrement();
+        }
+    }
 }
--- src/test/java/step3/RacingCarTest.java
+++ src/test/java/step3/RacingCarTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.*;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.*;
+import static step3.Car.*;
 
 public class RacingCarTest {
 
@@ -22,20 +24,27 @@ public class RacingCarTest {
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
 
-        Map<Integer, AtomicInteger> cars = Car.generateCars(i);
+        Map<Integer, AtomicInteger> cars = generateCars(i);
 
         assertThat(cars.size()).isEqualTo(i);
     }
 
     @Test
     void 랜덤_생성_숫자가_4보다_큰지_작은지() {
-        int randomNumber = Car.generateRandomNumber();
-        boolean isOverFour = randomNumber >= Car.RANDOM_NUMBER_LIMIT;
+        int randomNumber = generateRandomNumber();
+        boolean isOverFour = randomNumber >= RANDOM_NUMBER_LIMIT;
         if (isOverFour) {
-            assertThat(randomNumber).isGreaterThanOrEqualTo(Car.RANDOM_NUMBER_LIMIT);
+            assertThat(randomNumber).isGreaterThanOrEqualTo(RANDOM_NUMBER_LIMIT);
         } else {
-            assertThat(randomNumber).isLessThanOrEqualTo(Car.RANDOM_NUMBER_LIMIT);
+            assertThat(randomNumber).isLessThanOrEqualTo(RANDOM_NUMBER_LIMIT);
         }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5})
+    void 입력_숫자만큼_라운드를_반복함(int round) {
+        Map<Integer, AtomicInteger> cars = iteratePeriod(round, generateCars(round));
+        assertThat(cars.size()).isEqualTo(round);
     }
 
 }
