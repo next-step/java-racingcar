1df8f0e5194057c901c7a07d7fcffa280b82d8aa
jaewon0913 <jaewon9937@gmail.com>
2023-04-11 22:34:38 +0900

    feat(RacingCar) : 자동차 경주 게임 개발(피드백)
    - Car 모델 View 관련 로직 제거

--- src/main/java/study/racingcar/Car.java
+++ src/main/java/study/racingcar/Car.java
@@ -2,27 +2,17 @@ package study.racingcar;
 
 public class Car {
 
-    private static final int MOVE_CONDITION = 4;
-
-    private static final String MOVE_MARK = "-";
-
     private String position;
 
     public Car(String position) {
         this.position = position;
     }
 
-    public void move(int randomNum) {
-        if (isMove(randomNum)) {
-            this.position += MOVE_MARK;
-        }
-    }
-
-    private boolean isMove(int randomNum) {
-        return randomNum >= MOVE_CONDITION;
-    }
-
     public String getPosition() {
         return this.position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
     }
 }
--- src/main/java/study/racingcar/RacingGame.java
+++ src/main/java/study/racingcar/RacingGame.java
@@ -7,6 +7,9 @@ public class RacingGame {
 
     private static final Random RANDOM = new Random();
     private static final int BOUND_NUMBER = 10;
+    private static final int MOVE_CONDITION = 4;
+    private static final String MOVE_MARK = "-";
+
     public static void gameStart(List<Car> carList, int countOfGames) {
 
         ResultView.startGame();
@@ -19,7 +22,7 @@ public class RacingGame {
     private static void moveCar(List<Car> cars) {
 
         for(Car car : cars) {
-            car.move(generateRandomNumber());
+            move(generateRandomNumber(), car);
 
             ResultView.resultGame(car.getPosition());
         }
@@ -29,5 +32,15 @@ public class RacingGame {
 
     public static int generateRandomNumber() {
         return RANDOM.nextInt(BOUND_NUMBER);
+    }
+
+    public static void move(int randomNum, Car car) {
+        if (isMove(randomNum)) {
+            car.setPosition(car.getPosition() + MOVE_MARK);
+        }
+    }
+
+    private static boolean isMove(int randomNum) {
+        return randomNum >= MOVE_CONDITION;
     }
 }
