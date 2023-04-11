7f8a19cedd1e6fb88e5433392f3f88359f66b029
cohi <tkfka156@gmail.com>
2023-04-11 09:41:06 +0900

    :recycle: refactor: 불필요 메서드 제거 및 잘못된 메서드명 변경

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -1,19 +1,8 @@
 package step3;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Car {
 
-class Car {
-
-    private final List<Car> cars = new ArrayList<>();
     private final StringBuilder sb = new StringBuilder();
-
-    public Car(int count) {
-        addCars(count);
-    }
-
-    public Car() {
-    }
 
     public void move(int input) {
         if (input <= 3) {
@@ -24,15 +13,5 @@ class Car {
 
     public String getDistance() {
         return sb.toString();
-    }
-
-    public List<Car> getList() {
-        return cars;
-    }
-
-    private void addCars(int count) {
-        for (int i = 0; i < count; i++) {
-            cars.add(new Car());
-        }
     }
 }
--- src/main/java/step3/Race.java
+++ src/main/java/step3/Race.java
@@ -1,18 +1,17 @@
 package step3;
 
-import java.util.List;
 import java.util.Random;
 
 public class Race {
 
-    private final Car racingCars;
+    private final RacingCar racingCars;
     private final int trial;
     private static final Random RANDOM = new Random();
     public static final int BOUND = 10;
 
     public Race(int count, int trial) {
         validate(count, trial);
-        this.racingCars = new Car(count);
+        this.racingCars = new RacingCar(count);
         this.trial = trial;
     }
 
@@ -20,7 +19,6 @@ public class Race {
         for (int i = 0; i < this.trial; i++) {
             printRacingResult();
             System.out.println();
-            sleep();
         }
     }
 
@@ -29,32 +27,24 @@ public class Race {
     }
 
     private void validate(int count, int trial) {
-        if (isOverThan0(count, trial)) {
+        if (isLessThanZero(count, trial)) {
             throw new IllegalArgumentException("1 이상을 입력해 주세요");
         }
     }
 
-    private void sleep() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     private void printRacingResult() {
-        List<Car> cars = this.racingCars.getList();
-        for (Car car : cars) {
-            car.move(getRandomNumber());
-            System.out.println(car.getDistance());
-        }
+        this.racingCars.getList().forEach(racingCar -> {
+                racingCar.move(getRandomNumber());
+                System.out.println(racingCar.getDistance());
+            }
+        );
     }
 
     private int getRandomNumber() {
         return RANDOM.nextInt(BOUND);
     }
 
-    private boolean isOverThan0(int count, int trial) {
+    private boolean isLessThanZero(int count, int trial) {
         return count < 1 || trial < 1;
     }
 }
