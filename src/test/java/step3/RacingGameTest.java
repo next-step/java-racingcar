0f21489723e36388e0754f0848fc5494fe08e8b4
profoundsea25 <profoundsea25@gmail.com>
2023-04-11 20:36:44 +0900

    feat (사용자로부터 입력값 받기): 입력값을 받기 위한 UI 로직 InputView 생성

---
+++ src/test/java/step3/RacingGameTest.java
@@ -0,0 +1,14 @@
+package step3;
+
+import org.junit.jupiter.api.Test;
+
+public class RacingGameTest {
+
+    private final RacingGame racingGame = new RacingGame();
+
+    @Test
+    public void playTest() {
+        racingGame.play();
+    }
+
+}
