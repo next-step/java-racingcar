61b9adb01eb91633657ebc6e54a0dcf72cec974c
patrickYoo <yhy1045@naver.com>
2023-04-11 17:59:31 +0900

    test: Cars 객체에 대해 root 생성자 또한 테스트를 진행한다.

--- src/test/java/com/nextstep/racingcargame/core/CarsTest.java
+++ src/test/java/com/nextstep/racingcargame/core/CarsTest.java
@@ -3,7 +3,9 @@ package com.nextstep.racingcargame.core;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,6 +15,9 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 class CarsTest {
 
     private static final int CAR_FORCE_MOVE_NUMBER = 9;
+    private static final String APPROPRIATE_TEST_CAR_NAME = "test";
+
+    private static final int SINGLE_OBJECT_SIZE = 1;
 
     @ParameterizedTest(name = "[{index}] Cars 객체에 (,) 구분자를 기준으로 이름만큼 자동차를 생성한다.")
     @CsvSource(value = {"rick,jack,ethan:3", "ethan:1", "jack,ethan:2"}, delimiter = ':')
@@ -28,6 +33,22 @@ class CarsTest {
             new Cars(new CarNameChunk(valueSource));
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("차 이름이 임력되지 않았습니다.");
+    }
+
+    @DisplayName("주 생성자에 정상 자동차 객체 주입시 객채가 정상 생성된다.")
+    @Test
+    void createCarsSuccessTestWithRootCtor() {
+        Cars cars = new Cars(List.of(new Car(APPROPRIATE_TEST_CAR_NAME)));
+        assertThat(cars.carSize()).isEqualTo(SINGLE_OBJECT_SIZE);
+    }
+
+    @DisplayName("주 생성자에 빈 배열이 유입된 경우 예외를 발생한다.")
+    @Test
+    void createCarsSuccessWithRootCtor() {
+        assertThatThrownBy(() -> {
+            new Cars(new ArrayList<>());
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1 보다 작은 수의 차량은 생성될 수 없습니다.");
     }
 
     @Test
@@ -90,7 +111,6 @@ class CarsTest {
     void getFurthestTraveledCarDistanceTest() {
         Car patCar = new Car("pat");
 
-        // move 2 steps forward
         patCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
         patCar.moveForwardByNumber(CAR_FORCE_MOVE_NUMBER);
 
