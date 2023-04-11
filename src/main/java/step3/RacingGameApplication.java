246bbed02fb94c8c0f6d6f7c6d57cd3980160e72
bryan.ik <bryan.ik@kakaocorp.com>
2023-04-11 11:39:03 +0900

    [step3] feat : 레이싱 게임 어플리케이션 (메인) 구현

---
+++ src/main/java/step3/RacingGameApplication.java
@@ -0,0 +1,9 @@
+package step3;
+
+public class RacingGameApplication {
+
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        racingGame.run();
+    }
+}
