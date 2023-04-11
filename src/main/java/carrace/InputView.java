ff108d66dd5e8cb60afabdd57be856d68f627b41
garden-2 <1eejw@naver.com>
2023-04-12 01:12:53 +0900

    refactor: 실행 결과 문구 추가 외
    - scanner 상수로 선언

--- src/main/java/carrace/InputView.java
+++ src/main/java/carrace/InputView.java
@@ -6,6 +6,7 @@ public enum InputView {
     HOW_MANY_CARS("자동차 대수는 몇 대 인가요?"),
     HOW_MANY_LABS("시도할 회수는 몇 회 인가요?");
 
+    public static final Scanner SCANNER = new Scanner(System.in);
     private final String description;
 
     InputView(String description) {
@@ -14,7 +15,6 @@ public enum InputView {
 
     public int getAnswer() {
         System.out.println(description);
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 }
--- src/main/java/carrace/ResultView.java
+++ src/main/java/carrace/ResultView.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class ResultView {
     private static final String DASH = "-";
     public static void print(CarRace carRace) {
+        System.out.println("실행 결과");
         final List<Integer> carsPosition = carRace.getCarsPosition();
         carsPosition.stream()
                 .map(ResultView::getDash)
