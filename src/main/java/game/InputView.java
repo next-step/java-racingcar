978c1454a6bb2f33548d94c050fee156e12433f4
In-wook Hwang <inwook9@gmail.com>
2023-04-12 00:09:43 +0900

    refactor: 불필요 상수 원복

--- src/main/java/game/InputView.java
+++ src/main/java/game/InputView.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public final class InputView {
 
-    private static final String CAR_COUNT_QUESTION = "자동차 대수는 몇 대 인가요?";
-    private static final String RACING_REP_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
-
     private InputView() {
         throw new IllegalCallerException("잘못된 객체생성 입니다.");
     }
@@ -14,10 +11,10 @@ public final class InputView {
     public static RacingOptions showAndGetOptions() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(CAR_COUNT_QUESTION);
+        System.out.println("자동차 대수는 몇 대 인가요?");
         int carCount = sc.nextInt();
 
-        System.out.println(RACING_REP_COUNT_QUESTION);
+        System.out.println("시도할 회수는 몇 회 인가요?");
         int repCount = sc.nextInt();
 
         return new RacingOptions(carCount, repCount);
