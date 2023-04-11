28f4aa5ccda39214d44dabe2b2f772e65ce6a2fb
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-12 00:30:24 +0900

    refactor: 자동차 전진 가능 여부 체크 메서드 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,17 +1,15 @@
 package racingcar.domain;
 
 public class Car {
-    public boolean initParticipationNumber(int number) {
-        if (number  <= 0) {
-            throw new IllegalArgumentException("자동차 대수는 양수만 입력가능합니다");
+    private int point = 0;
+
+    public void move(RandomNumberGenerator generator) {
+        if (generator.isMovable()) {
+            this.point += 1;
         }
-        return true;
     }
 
-    public boolean initRepetitionCount(int count) {
-        if (count <= 0) {
-            throw new IllegalArgumentException("자동차 시도 횟수는 양수만 입력 가능합니다");
-        }
-        return true;
+    public int getPoint() {
+        return this.point;
     }
 }
