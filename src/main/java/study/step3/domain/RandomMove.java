42459201255dd27c47e1c0fd11909e37633d9fc5
player-geun <player_geun@naver.com>
2023-04-11 19:47:11 +0900

    refactor : 컨벤션 규칙에 맞게 수정

--- src/main/java/study/step3/domain/RandomMove.java
+++ src/main/java/study/step3/domain/RandomMove.java
@@ -2,7 +2,7 @@ package study.step3.domain;
 
 import java.util.Random;
 
-public class RandomMove implements MoveStrategy{
+public class RandomMove implements MoveStrategy {
 
     private static final Random random = new Random();
 
