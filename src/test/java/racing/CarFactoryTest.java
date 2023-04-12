d92aa682af27cfc31d1a9a6ad905e42fa6a6bd29
soochan <tncksdl05@gmail.com>
2023-04-12 16:59:29 +0900

    refactor : carFactory 를 static 으로 관리함으로써 테스트 코드 수정.

--- src/test/java/racing/CarFactoryTest.java
+++ src/test/java/racing/CarFactoryTest.java
@@ -13,8 +13,7 @@ class CarFactoryTest {
   @ParameterizedTest
   @ValueSource(ints = {1, 2, 3, 4, 5})
   void 입력_받은_숫자만큼_자동차_객체_생성(int carCount) {
-    CarFactory carFactory = new CarFactory();
-    List<Car> cars = carFactory.createCars(carCount);
+    List<Car> cars = CarFactory.createCars(carCount);
 
     assertThat(cars.size()).isEqualTo(carCount);
   }
