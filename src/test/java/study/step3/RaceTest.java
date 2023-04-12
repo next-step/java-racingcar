1f423c5923ff2c0801e519faf7ae497d9e96c627
hoa0217 <gjwjdghk123@naver.com>
2023-04-13 05:14:42 +0900

    feature: "Race클래스 생성" - 자동차들을 인스턴스 변수로 주입받아 play하는 도메인 클래스

---
+++ src/test/java/study/step3/RaceTest.java
@@ -0,0 +1,26 @@
+package study.step3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import study.step3.domain.Car;
+import study.step3.domain.Race;
+import study.step3.factory.CarFactory;
+
+public class RaceTest {
+
+  CarFactory carFactory = new CarFactory();
+
+  @DisplayName("경기 한번 당 모든 자동차들은 랜덤하게 1칸 전진하거나 전진하지 않는다.")
+  @Test
+  public void play_forwardOrStop_carList() {
+    Race race = new Race(carFactory.createCars(5));
+
+    race.play();
+
+    for (Car car : race.getCarList()) {
+      assertThat(car.getDistance()).isLessThanOrEqualTo(1);
+    }
+  }
+}
