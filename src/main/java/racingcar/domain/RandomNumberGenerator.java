eeeee135ebd1f71f8d3f954b91a55c2c3f4f872c
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-12 12:51:08 +0900

    refactor: RandomNumberGenerator 리팩토링 - 움직임 조건 숫자 상수로 명명 - 테스트 코드 위치 이동

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -6,12 +6,13 @@ public class RandomNumberGenerator {
 
     private static final Random RANDOM = new Random();
     private static final int RANDOM_RULE_MAX_NUMBER = 10;
+    public static final int MOVABLE_NUMBER_CONDITION = 4;
 
     public int generate() {
         return RANDOM.nextInt(RANDOM_RULE_MAX_NUMBER);
     }
 
     public boolean isMovable() {
-        return generate() >= 4;
+        return generate() >= MOVABLE_NUMBER_CONDITION;
     }
 }
