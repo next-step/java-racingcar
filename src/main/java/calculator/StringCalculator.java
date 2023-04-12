38771d89c62a36f960cca3dad83ae65f3c9a9a1d
Jung Sim <tlawjd99gma@gmail.com>
2023-04-12 23:32:39 +0900

    Step2 문자열 덧셈 계산기 (#4373)
    * feat : 기능 요구사항 구현
    
     - 문자열 덧셈 계산기 미션의 요구사항 구현
    
    * test : 커스텀 구분자 테스트 ParameterizedTest로 변경
    
    * refactor : ZERO 상수가 의미를 갖도록 이름을 변경
    
    * refactor : Pattern 객체 분리 및 변수 분리
    
    * feat : StringCalculatorInput 클래스 추가 및 역할 분리
    
    * feat : 숫자가 아닐 경우 예외 처리 추가

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,91 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Character.isDigit;
+
+public class StringCalculator {
+    public static final int DEFAULT_RESULT = 0;
+    public static final String DEFAULT_DELIMITER = ",|:";
+    public static final String CUSTOM_DELIMITER_REGEX_PREFIX = "//(.)\n(.*)";
+    public static final Pattern DEFAULT_PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX_PREFIX);
+
+    public static int splitAndSum(String text) {
+        if (isBlank(text)) {
+            return DEFAULT_RESULT;
+        }
+
+        String[] split = split(text);
+        int[] positives = toPositives(split);
+        return sum(positives);
+    }
+
+    private static String[] split(String text) {
+        StringCalculatorInput stringCalculatorInput = makeStringCalculatorInput(text);
+        String delimiter = stringCalculatorInput.delimiter;
+        String content = stringCalculatorInput.content;
+
+        return content.split(delimiter);
+    }
+
+    private static StringCalculatorInput makeStringCalculatorInput(String text) {
+        Matcher matcher = DEFAULT_PATTERN.matcher(text);
+
+        if (matcher.find()) {
+            return new StringCalculatorInput(matcher.group(1), matcher.group(2));
+        }
+
+        return new StringCalculatorInput(DEFAULT_DELIMITER, text);
+    }
+
+    private static class StringCalculatorInput {
+        private final String delimiter;
+        private final String content;
+
+        public StringCalculatorInput(String delimiter, String content) {
+            this.delimiter = delimiter;
+            this.content = content;
+        }
+    }
+
+    private static int[] toPositives(String[] values) {
+        int[] ints = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            ints[i] = toPositive(values[i]);
+        }
+        return ints;
+    }
+
+    private static int toPositive(String value) {
+        if (!isNumber(value)) {
+            throw new RuntimeException("Not number!");
+        }
+
+        int num = Integer.parseInt(value);
+        if (num < 0) {
+            throw new RuntimeException("Negative number!");
+        }
+        return num;
+    }
+
+    private static boolean isNumber(String value) {
+        boolean result = true;
+        for (int i = 0; i < value.length(); i++) {
+            result &= isDigit(value.charAt(i));
+        }
+        return result;
+    }
+
+    private static int sum(int[] numbers) {
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    private static boolean isBlank(String text) {
+        return text == null || text.isBlank();
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,112 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static calculator.StringCalculator.splitAndSum;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class StringCalculatorTest {
+    @ParameterizedTest(name = "{0}에 음수가 있으므로 RuntimeException이 발생한다")
+    @ValueSource(strings = {"-1,2:3", "1,-2:3", "1,2:-3"})
+    void 음수를_전달할_경우_RuntimeException이_발생한다(String input) {
+        assertThatThrownBy(() -> splitAndSum(input))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest(name = "{0}에 문자가 있으므로 RuntimeException이 발생한다")
+    @ValueSource(strings = {"a,2:3", "1,a:3", "1,2:a"})
+    void 문자를_전달할_경우_RuntimeException이_발생한다(String input) {
+        assertThatThrownBy(() -> splitAndSum(input))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest(name = "{0}에 정의된 구분자로 구분된 숫자들을 더하면 {1}이다")
+    @MethodSource("getCustomDelimiterStubs")
+    void 커스텀_구분자를_지정할_수_있다(String input, int expected) {
+        assertThat(splitAndSum(input)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> getCustomDelimiterStubs() {
+        return Stream.of(
+                Arguments.arguments("//;\n1;2;3", 6),
+                Arguments.arguments("//@\n2@3@4", 9),
+                Arguments.arguments("//~\n3~4~5", 12)
+        );
+    }
+
+    @ParameterizedTest(name = "컴마와 콜론이 포함된 {0}의 숫자들을 더하면 {1}이다")
+    @CsvSource(value = {"1,2:3/6", "2:3,4/9"}, delimiter = '/')
+    void 컴마와_콜론으로_구분된_숫자들을_더한_값을_반환한다(String input, int expected) {
+        assertThat(splitAndSum(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "컴마로 구분된 {0}의 숫자들을 더하면 {1}이다")
+    @CsvSource(value = {"1,2,3/6", "2,3/5"}, delimiter = '/')
+    void 컴마로_구분된_숫자들을_더한_값을_반환한다(String input, int expected) {
+        assertThat(splitAndSum(input)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "문자열 {0}을 입력하면 그대로 숫자 {1}이 반환된다")
+    @CsvSource(value = {"1:1", "2:2", "3:3"}, delimiter = ':')
+    void 숫자_하나를_넣으면_그대로_반환한다(String input, int expected) {
+        assertThat(splitAndSum(input)).isEqualTo(expected);
+    }
+
+    @Test
+    void null을_입력하면_0을_반환한다() {
+        assertThat(splitAndSum(null)).isEqualTo(0);
+    }
+
+    @Test
+    void 빈_문자열을_입력하면_0을_반환한다() {
+        assertThat(splitAndSum("")).isEqualTo(0);
+    }
+
+    // 이하는 요구사항에 첨부된 TestCase 소스 코드입니다.
+    @Test
+    public void splitAndSum_null_또는_빈문자() {
+        int result = splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() throws Exception {
+        int result = splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
