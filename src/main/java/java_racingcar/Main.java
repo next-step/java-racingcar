348dc33127db1d1d1cb26e2757c29f38af4a46ab
yerimmac <yerim.lee.earth@gmail.com>
2023-04-12 21:42:30 +0900

    feature: 경기 회수 입력 구현

--- src/main/java/java_racingcar/Main.java
+++ src/main/java/java_racingcar/Main.java
@@ -3,5 +3,6 @@ package java_racingcar;
 public class Main {
     public static void main(String[] args) {
         int carNumber = InputView.inputCarNumber();
+        int racingTime = InputView.inputRacingTime();
     }
 }
