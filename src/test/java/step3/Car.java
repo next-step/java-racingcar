48bac66106d12138b1a73a9ddf3891321a16920e
lehdqlsl <lehdqlsl@naver.com>
2023-04-12 16:12:12 +0900

    feat(Car) : Car 클래스 작성

---
+++ src/test/java/step3/Car.java
@@ -0,0 +1,29 @@
+package step3;
+
+public class Car {
+    private static final int MOVE_NUMBER = 4;
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+    private int location;
+
+    public Car() {
+        this.location = 0;
+    }
+
+    public void move(int input) {
+        if (isMovable(input)) {
+            this.location++;
+        }
+    }
+
+    private static boolean isMovable(int input) {
+        if (input < MIN_NUMBER || input > MAX_NUMBER) {
+            throw new RuntimeException("유효하지 않은 숫자입니다.");
+        }
+        return input >= MOVE_NUMBER;
+    }
+
+    public int location() {
+        return location;
+    }
+}
