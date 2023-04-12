06b2aa2659f53c1a5a630773697eda2f89fbf1b9
jake.b <jake.b@kakaocorp.com>
2023-04-12 18:56:26 +0900

    step2 - refactor1

--- src/main/java/StringCalculatorInput.java
+++ src/main/java/StringCalculatorInput.java
@@ -1,36 +1,72 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculatorInput {
+    public static final List<String> DEFAULT_DELIMITERS = Arrays.asList(",", ":");
     public static final String CUSTOM_DELIMITER_START_PATTERN = "//";
     public static final String CUSTOM_DELIMITER_END_PATTERN = "\\n";
-
-    private final String rawInput;
+    public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(String.format("%s(.+)%s(.*)", CUSTOM_DELIMITER_START_PATTERN, CUSTOM_DELIMITER_END_PATTERN));
+    private final List<Long> numbers;
 
     public StringCalculatorInput(String input) {
-        rawInput = input;
+        var delimiters = getAllDelimitersFromCustomDelimiters(getCustomDelimiters(input));
+        var tokenSplitWithDelimiters = split(getToken(input), delimitersToRegex(delimiters));
+        numbers = parseStringsToLongs(tokenSplitWithDelimiters);
     }
 
-    public List<String> getCustomDelimiters() {
-        var matcher = getCustomDelimiterAndTokenMatcher(rawInput);
+    private static List<String> split(String input, String regex) {
+        return List.of(input.split(regex));
+    }
+
+    private static List<String> getAllDelimitersFromCustomDelimiters(List<String> customDelimiters) {
+        var delimiters = new ArrayList<String>();
+        delimiters.addAll(DEFAULT_DELIMITERS);
+        delimiters.addAll(customDelimiters);
+        return delimiters;
+    }
+
+    private static String delimitersToRegex(List<String> delimiters) {
+        var regex = new StringBuilder();
+        for (var delimiter : delimiters) {
+            regex.append(delimiter).append('|');
+        }
+        deleteLastChar(regex);
+        return regex.toString();
+    }
+
+    private static List<Long> parseStringsToLongs(List<String> strings) {
+        var longs = new ArrayList<Long>();
+        for (var element : strings) {
+            longs.add(Long.parseLong(element));
+        }
+        return longs;
+    }
+
+    private static void deleteLastChar(StringBuilder stringBuilder) {
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+    }
+
+    public List<Long> getNumbers() {
+        return numbers;
+    }
+
+    public List<String> getCustomDelimiters(String input) {
+        var regexCustomDelimiterPart = 1;
+        var matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
         if (matcher.find()) {
-            return List.of(matcher.group(1));
+            return List.of(matcher.group(regexCustomDelimiterPart));
         }
         return new ArrayList<>();
     }
 
-    public String getToken() {
-        var matcher = getCustomDelimiterAndTokenMatcher(rawInput);
+    public String getToken(String input) {
+        var regexTokenPart = 2;
+        var matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
         if (matcher.find()) {
-            return matcher.group(2);
+            return matcher.group(regexTokenPart);
         }
-        return rawInput;
-    }
-
-    private Matcher getCustomDelimiterAndTokenMatcher(String input) {
-        var extractorRegex = String.format("%s(.+)%s(.*)", CUSTOM_DELIMITER_START_PATTERN, CUSTOM_DELIMITER_END_PATTERN);
-        return Pattern.compile(extractorRegex).matcher(input);
+        return input;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringCalculatorTest {
     @Test
+    @DisplayName("String calculator returns 0 when input is null")
     public void zeroIfNull() {
         String input = null;
         var result = StringCalculator.sumOfNumbers(input);
@@ -15,6 +17,7 @@ public class StringCalculatorTest {
     }
 
     @Test
+    @DisplayName("String calculator returns 0 when input is empty")
     public void zeroIfEmpty() {
         var input = "";
         var result = StringCalculator.sumOfNumbers(input);
@@ -23,7 +26,8 @@ public class StringCalculatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "10", "123"})
-    public void splitAndAdd(String input) {
+    @DisplayName("String calculator return same number if a single number is given")
+    public void singleInput(String input) {
         var result = StringCalculator.sumOfNumbers(input);
         var expected = Long.parseLong(input);
         assertThat(result).isEqualTo(expected);
@@ -35,19 +39,14 @@ public class StringCalculatorTest {
             "2,3,4:9",
             "12,12,23:47",
     }, delimiter = ':')
+    @DisplayName("Splits and adds input with delimiter ,:")
     public void splitAndAdd(String input, Long expected) {
         var result = StringCalculator.sumOfNumbers(input);
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void multipleDelimiters() {
-        var input = "1,2:3";
-        var result = StringCalculator.sumOfNumbers(input);
-        assertThat(result).isEqualTo(6);
-    }
-
-    @Test
+    @DisplayName("Splits and adds input with a custom delimiter")
     public void customDelimiters() {
         var input = "//;\n1;2;3";
         var result = StringCalculator.sumOfNumbers(input);
@@ -55,6 +54,7 @@ public class StringCalculatorTest {
     }
 
     @Test()
+    @DisplayName("Throws an exception when negative input is given")
     public void negativeValueInput() {
         var input = "-1,2,3";
         assertThatThrownBy(() -> StringCalculator.sumOfNumbers(input))
