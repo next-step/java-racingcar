9c3f39a20a5a7cfa5b46de61fcfa36df514d9220
right1203 <right1203@gmail.com>
2023-04-12 20:47:34 +0900

    feat: 자동차 이름 받을 수 있도록 개선, 자동차 우승자 찾기 구현, 생성자에 List<RacingCar>를 받도록 수정

--- src/main/java/racing/domain/RacingGame.java
+++ src/main/java/racing/domain/RacingGame.java
@@ -1,34 +1,20 @@
 package racing.domain;
 
-import racing.strategy.MoveStrategy;
-
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGame {
     private final List<RacingCar> gameCars;
 
-    public RacingGame(int count) {
-        this.gameCars = createCarList(count);
+    public RacingGame(List<RacingCar> gameCars) {
+        this.gameCars = gameCars;
     }
 
     public List<RacingCar> getGameCars() {
-        return gameCars;
+        return Collections.unmodifiableList(gameCars);
     }
 
-    private List<RacingCar> createCarList(int count) {
-        List<RacingCar> carList = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            carList.add(new RacingCar());
-        }
-        return carList;
-    }
-
-    public void setMoveStrategyOfCars(MoveStrategy moveStrategy) {
-        for (RacingCar gameCar : gameCars) {
-            gameCar.setMoveStrategy(moveStrategy);
-        }
-    }
 
     public void progressCycle() {
         for (RacingCar gameCar : gameCars) {
@@ -36,11 +22,10 @@ public class RacingGame {
         }
     }
 
-    public List<Integer> result() {
-        List<Integer> carStatusList = new ArrayList<>();
-        for (RacingCar gameCar : gameCars) {
-            carStatusList.add(gameCar.getPosition());
-        }
-        return carStatusList;
+    public List<RacingCar> findWinner() {
+        int maxPosition = Collections.max(gameCars).getPosition();
+        return gameCars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
     }
 }
