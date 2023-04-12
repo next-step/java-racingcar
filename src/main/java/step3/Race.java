2ae8b15ff22f8a6fa957ff3f69fa5fd757a3c029
poistargram <poi.stargram@gmail.com>
2023-04-12 23:14:33 +0900

    feat: 규칙대로 레이싱에서 자동차가 가도록 구현

--- src/main/java/step3/Race.java
+++ src/main/java/step3/Race.java
@@ -2,11 +2,13 @@ package step3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Race {
 
     private final int raceCount;
     private final List<Car> cars;
+    private final static Random random = new Random();
 
     private Race(int raceCount, List<Car> cars) {
         this.raceCount = raceCount;
@@ -29,4 +31,25 @@ public class Race {
     public List<Car> getCars() {
         return cars;
     }
+
+    public void race(){
+        for(int i = 0; i < raceCount; i++){
+            raceCar();
+        }
+    }
+
+    private void raceCar(){
+        cars.stream()
+                .filter(car -> success())
+                .forEach(Car::move);
+    }
+
+    private boolean success(){
+        int randomNumber = random.nextInt(10);
+        if(randomNumber < 4) {
+            return false;
+        }
+        return true;
+    }
+
 }
