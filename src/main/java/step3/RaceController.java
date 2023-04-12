dd36b3c4785c1db6411a9d131f744928c91eb23f
hovy <hovy1994@naver.com>
2023-04-12 12:54:03 +0900

    refactoring: 사용자 입력 값 유효성 체크 및 경주 조건 클래스 위치 변경

--- src/main/java/step3/RaceController.java
+++ src/main/java/step3/RaceController.java
@@ -1,13 +1,13 @@
 package step3;
 
 import step3.domain.Race;
-import step3.view.InputRaceCondition;
+import step3.domain.RaceCondition;
 import step3.view.RaceConditionInputView;
 
 public class RaceController {
 
     public static void main(String[] args) {
-        InputRaceCondition raceCondition = RaceConditionInputView.display();
+        RaceCondition raceCondition = RaceConditionInputView.display();
         Race race = new Race(raceCondition.carCount, raceCondition.raceRound);
         race.start();
     }
--- src/main/java/step3/view/RaceConditionInputView.java
+++ src/main/java/step3/view/RaceConditionInputView.java
@@ -1,16 +1,18 @@
 package step3.view;
 
+import step3.domain.RaceCondition;
+
 import java.util.Scanner;
 
 public class RaceConditionInputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static InputRaceCondition display() {
+    public static RaceCondition display() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         int carCount = scanner.nextInt();
         System.out.println("시도할 회수는 몇 회 인가요?");
         int raceRound = scanner.nextInt();
 
-        return new InputRaceCondition(carCount, raceRound);
+        return RaceCondition.create(carCount, raceRound);
     }
 }
