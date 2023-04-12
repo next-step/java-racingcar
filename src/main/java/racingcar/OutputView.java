b6506525a403e60bfd7a1aed4d60a224db686a48
ynoolee <heerarea129@gmail.com>
2023-04-12 22:56:12 +0900

    feat: 콘솔에 각종 정보를 출력하는 기능 추가

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public interface OutputView {
+
+    void printQuestionAboutTheNumberOfCar();
+
+    void printQuestionAboutTheNumberOfTrial();
+
+    void printCarLocation(int location);
+}
--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,12 +3,30 @@ package racingcar;
 
 import java.util.Scanner;
 
-public class RacingGame implements InputView {
+public class RacingGame implements InputView, OutputView {
 
     @Override
     public int getIntegerInput() {
         Scanner scanner = new Scanner(System.in);
 
         return scanner.nextInt();
+    }
+
+    @Override
+    public void printQuestionAboutTheNumberOfCar() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    @Override
+    public void printQuestionAboutTheNumberOfTrial() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+    }
+
+    @Override
+    public void printCarLocation(int location) {
+        for (int cnt = 0; cnt < location; cnt++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 }
