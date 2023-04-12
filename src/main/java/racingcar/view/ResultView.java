51cc159127a64f5e6517d9bf7e96f3437e58c458
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-12 12:47:24 +0900

    feat: ResultView 구현

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,24 +1,33 @@
 package racingcar.view;
 
 import racingcar.domain.Score;
+import racingcar.domain.Scores;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ResultView {
     public static final String RUN_RESULT_MESSAGE = "실행결과";
-    public static final String MOVE_RESULT = "-";
+    public static final String MOVE_RESULT_EXPRESSION = "-";
 
-    public void printResult(List<Score> scores){
+    public static void printResult(List<Scores> scores){
         System.out.println(RUN_RESULT_MESSAGE);
-        for (Score score : scores) {
+        scores.forEach(score -> {
             printScore(score);
             System.out.println();
-        }
+        });
     }
 
-    private void printScore(Score score) {
-        for (int i = 0; i < score.getPoint(); i++) {
-            System.out.print(MOVE_RESULT);
-        }
+    private static void printScore(Scores scores) {
+        scores.getScores().forEach(score -> {
+            printDistance(score);
+            System.out.println();
+        });
+    }
+
+    private static void printDistance(Score score) {
+        IntStream.range(0, score.getPoint())
+                .mapToObj(i -> MOVE_RESULT_EXPRESSION)
+                .forEach(System.out::print);
     }
 }
--- src/test/java/racingcar/ResultViewTest.java
+++ src/test/java/racingcar/ResultViewTest.java
@@ -3,10 +3,10 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Score;
+import racingcar.domain.Scores;
 import racingcar.view.ResultView;
 
 import java.util.Arrays;
-import java.util.List;
 
 @DisplayName("결과 출력 테스트")
 public class ResultViewTest {
@@ -14,8 +14,11 @@ public class ResultViewTest {
     @DisplayName("포인트 정보를 전달하면 - 가 출력된다")
     @Test
     void printTest() {
-        List<Score> scores = Arrays.asList(new Score(4), new Score(5));
+        Scores scores = new Scores();
+        scores.addScore(new Score(5));
+        Arrays.asList(scores);
         ResultView resultView = new ResultView();
-        resultView.printResult(scores);
+        resultView.printResult(Arrays.asList(scores));
+
     }
 }
