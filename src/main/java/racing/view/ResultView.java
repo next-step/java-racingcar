327ee567c429135711c51cbaf82155d3207b3e3e
right1203 <right1203@gmail.com>
2023-04-12 20:48:28 +0900

    feat: 결과 출력 UI 구현

--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -3,6 +3,9 @@ package racing.view;
 import racing.domain.RacingCar;
 import racing.domain.RacingGame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultView {
     private static final char PRINT_POSITION_CHAR = '-';
 
@@ -18,13 +21,32 @@ public class ResultView {
     }
 
     public static void printCarStatus(RacingCar car) {
+        printCarName(car);
         printOngoingStatus(car.getPosition());
+        System.out.println();
     }
 
     private static void printOngoingStatus(int count) {
         for (int i = 0; i < count; i++) {
             System.out.print(PRINT_POSITION_CHAR);
         }
-        System.out.println();
+    }
+
+    private static void printCarName(RacingCar car) {
+        System.out.print(car.getName() + " : ");
+    }
+
+    public static void printWinner(RacingGame game) {
+        List<RacingCar> winners = game.findWinner();
+        System.out.print(String.join(", ", findWinnerNames(winners)));
+        System.out.println("가 최종 우승했습니다.");
+    }
+
+    private static List<String> findWinnerNames(List<RacingCar> winners) {
+        List<String> winnerNames = new ArrayList<>();
+        for (RacingCar winner : winners) {
+            winnerNames.add(winner.getName());
+        }
+        return winnerNames;
     }
 }
