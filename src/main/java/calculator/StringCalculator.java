d2cac05f7fc2a13a6b9c08e45b38be744967103f
bahar-j <joonie811@naver.com>
2023-04-12 22:28:46 +0900

    refactor: step2 피드백 반영

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,15 +1,14 @@
 package calculator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class StringCalculator {
-    public static final String DEFAULT_SEPARATORS = ",:";
-    public static final String CUSTOM_SEPARATOR_REGEX = "//(.*)\\\\n";
 
-    public static int splitAndSum(String input) {
-        if (input == null || input.isBlank()) return 0;
-        return sum(removeSeparatorPhrase(input).split(makeRegex(input)));
+    public static final int DEFAULT_RESULT = 0;
+
+    public static int calculate(String input) {
+        if (!Splitor.isValid(input)) {
+            return DEFAULT_RESULT;
+        }
+        return StringCalculator.sum(Splitor.split(input));
     }
 
     private static int sum(String[] numbers) {
@@ -20,21 +19,4 @@ public class StringCalculator {
         return result;
     }
 
-    private static String removeSeparatorPhrase(String text) {
-        return text.replaceAll(CUSTOM_SEPARATOR_REGEX, "");
-    }
-
-    private static String makeRegex(String text) {
-        return "[" + DEFAULT_SEPARATORS + findCustomSeparators(text) + "]";
-    }
-
-    private static String findCustomSeparators(String text) {
-        String customSeparators = "";
-        Matcher matcher = Pattern.compile(CUSTOM_SEPARATOR_REGEX).matcher(text);
-        while (matcher.find()) {
-            String match = matcher.group(1);
-            customSeparators += match;
-        }
-        return customSeparators;
-    }
 }
