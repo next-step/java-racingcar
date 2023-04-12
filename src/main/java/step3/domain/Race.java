3858fda557f73b43cc0afd923df27dd9a857511c
hovy <hovy1994@naver.com>
2023-04-12 12:44:29 +0900

    refactoring: 불필요한 필드 제거

--- src/main/java/step3/domain/Race.java
+++ src/main/java/step3/domain/Race.java
@@ -6,8 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
-    private List<Car> cars;
-    private final int carCount;
+    private final List<Car> cars;
     private final int raceRound;
 
     public Race(int carCount, int raceRound) {
@@ -15,8 +14,11 @@ public class Race {
         for (int i = 0; i < carCount; i++) {
             this.cars.add(new Car());
         }
-        this.carCount = carCount;
         this.raceRound = raceRound;
+    }
+
+    public List<Car> getCars() {
+        return cars;
     }
 
     public void start() {
@@ -28,9 +30,9 @@ public class Race {
     }
 
     private void moveCars() {
-        for (int carNumber = 0; carNumber < carCount; carNumber++) {
+        for (Car car : cars) {
             if (CarRandomMovement.canMove()) {
-                cars.get(carNumber).move();
+                car.move();
             }
         }
     }
