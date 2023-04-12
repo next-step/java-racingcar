e408c031a5a163ce867eef80f8460f7ceca72f8d
jaewon0913 <jaewon9937@gmail.com>
2023-04-12 21:32:01 +0900

    Feat(RacingCar) : 자동차 경주 게임 개발(피드백)
    - 테스트 코드 수정 및 CarTest 분기

--- src/test/java/study/racingcar/CarTest.java
+++ src/test/java/study/racingcar/CarTest.java
@@ -2,20 +2,23 @@ package study.racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
-    @DisplayName("랜덤 수를 전달하여 4이상인 경우 차가 움직이는지 테스트")
+    @DisplayName("횟수 만큼 전달하여 자동차가 해당 수만큼 움직였는지 테스트")
     @ParameterizedTest
-    @CsvSource(value = {"0:0", "7:1", "2:0", "4:1"}, delimiter = ':')
-    public void generateRandomNum_범위내에_있는지_테스트(int randomNum, int length) {
+    @ValueSource(ints = {0,2,5,7})
+    public void racingGame_move(int randomNum) {
 
         Car car = new Car("");
-        //car.move(randomNum);
 
-        assertThat(car.getPosition().length()).isEqualTo(length);
+        for(int i = 0; i < randomNum; i++) {
+            RacingGame.move(car);
+        }
+
+        assertThat(car.getPosition().length()).isEqualTo(randomNum);
     }
 }
--- src/test/java/study/racingcar/RacingCarTest.java
+++ src/test/java/study/racingcar/RacingCarTest.java
@@ -3,7 +3,6 @@ package study.racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
@@ -17,7 +16,7 @@ public class RacingCarTest {
 
     @DisplayName("숫자를 입력시 자동차 대수를 반환 해야 한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1", "2", "3"})
+    @ValueSource(strings = {"2"})
     public void inputView_setCars_숫자(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
@@ -27,7 +26,7 @@ public class RacingCarTest {
 
     @DisplayName("숫자를 입력시 게임 횟수를 반환 해야 한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"4", "5", "6"})
+    @ValueSource(strings = {"5"})
     public void inputView_setGames_숫자(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
@@ -35,20 +34,20 @@ public class RacingCarTest {
         assertThat(InputView.setGames()).isEqualTo(Integer.parseInt(input));
     }
 
-    @DisplayName("차량수, 게임수 숫자가 양수인 경우 입력할 경우 True 를 반환한다.")
+    @DisplayName("차량수 숫자가 양수인 경우 입력할 경우 True 를 반환한다.")
     @ParameterizedTest
-    @CsvSource(value = {"2:3", "3:5", "3:7", "2:5"}, delimiter = ':')
-    public void settingGame_isReadyToGame_양수(int cars, int games) {
+    @ValueSource(ints = {4, 5, 6})
+    public void settingGame_createCars_양수(int cars) {
 
-        assertThat(SettingGame.isReadyToGame(cars, games)).isTrue();
+        assertThat(SettingGame.createCars(cars)).hasSize(cars);
     }
 
-    @DisplayName("차량수, 게임수 숫자가 음수 또는 0인 경우 입력할 경우 예외를 반환한다.")
+    @DisplayName("차량수 숫자가 음수 또는 0인 경우 입력할 경우 예외를 반환한다.")
     @ParameterizedTest
-    @CsvSource(value = {"-1:2", "0:3", "2:0", "4:-1"}, delimiter = ':')
-    public void settingGame_isReadyToGame_음수(int cars, int games) {
+    @ValueSource(ints = {-1, 0})
+    public void settingGame_createCars_음수(int cars) {
 
-        assertThatThrownBy(() -> SettingGame.isReadyToGame(cars, games))
+        assertThatThrownBy(() -> SettingGame.createCars(cars))
                 .isInstanceOf(InputMismatchException.class);
     }
 
@@ -62,8 +61,8 @@ public class RacingCarTest {
 
     @DisplayName("랜덤 값을 생성할 때 범위내에 존재하는지 테스트")
     @Test
-    public void racingGame_generateRandomNum_범위내에_있는지_테스트() {
-        int randomNum = RacingGame.generateRandomNumber();
-        assertThat(randomNum).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(9);
+    public void generateNumber_random_범위내에_있는지_테스트() {
+
+        assertThat(GenerateNumber.random()).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(9);
     }
 }
