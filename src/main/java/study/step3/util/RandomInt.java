744c7c1da88c6e3f6833c769bfbc40c6c8713085
hoa0217 <gjwjdghk123@naver.com>
2023-04-13 05:12:44 +0900

    feature: "Util성 Ramdom int값 반환 클래스 생성"

---
+++ src/main/java/study/step3/util/RandomInt.java
@@ -0,0 +1,12 @@
+package study.step3.util;
+
+import java.util.Random;
+
+public class RandomInt {
+
+  private static final Random RANDOM_INSTANCE = new Random();
+
+  public static int makeRandomInt(int bound) {
+    return RANDOM_INSTANCE.nextInt(bound);
+  }
+}
