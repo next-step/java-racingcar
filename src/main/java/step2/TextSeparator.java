1913dfb310c63a92a3a2458cbf4c4c86b045b3ec
lehdqlsl <lehdqlsl@naver.com>
2023-04-12 15:45:26 +0900

    fix(separator) : 접근제어자 수정

--- src/main/java/step2/TextSeparator.java
+++ src/main/java/step2/TextSeparator.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class TextSeparator {
     private static final String CUSTOM_SEPARATOR_REGEX = "//(.)\n(.*)";
     private static final String DEFAULT_SEPARATOR_REGEX = ",|:";
-    public static final int CUSTOM_DELIMITER_GROUP = 1;
-    public static final int TEXT_GROUP = 2;
+    private static final int CUSTOM_DELIMITER_GROUP = 1;
+    private static final int TEXT_GROUP = 2;
     private static Pattern pattern = Pattern.compile(CUSTOM_SEPARATOR_REGEX);
 
     public static String[] separate(String text) {
