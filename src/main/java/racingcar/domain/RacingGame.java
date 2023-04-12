0007686e6a5cfcf5028d786b1b25162c15646fb1
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-12 13:42:13 +0900

    feat: 자동차 경주 기능 클래스 구현  - 해당 클래스에 대한 단위 테스트를 어떻게 할 수 있을지 막막해서 테스트 코드는 구현하지 못했습니다  - 어떻게하면 TDD 로 설계와 구현이 가능할지 조언을 부탁드립니다

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,36 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RacingGame {
+
+    private final int carNumber;
+    private final int gameRound;
+
+    public RacingGame(InputData inputData) {
+        carNumber = inputData.getCarNumber();
+        gameRound = inputData.getGameRound();
+    }
+
+    public List<Scores> startGame(RandomNumberGenerator numberGenerator) {
+        Cars cars = new Cars(carNumber);
+        return round(cars, numberGenerator);
+    }
+
+    public List<Scores> round(Cars cars, RandomNumberGenerator numberGenerator) {
+        return IntStream.range(0, gameRound)
+                .mapToObj(i -> race(cars, numberGenerator))
+                .collect(Collectors.toList());
+    }
+
+    private Scores race(Cars cars, RandomNumberGenerator numberGenerator) {
+        Scores roundScores = new Scores();
+        cars.getCars().forEach(car -> {
+            car.move(numberGenerator);
+            roundScores.addScore(new Score(car.getPoint()));
+        });
+        return roundScores;
+    }
+}
