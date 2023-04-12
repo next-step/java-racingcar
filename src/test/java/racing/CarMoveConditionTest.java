1cdc9cf35b6141d0daeacd19a0ae523a6b404df3
soochan <tncksdl05@gmail.com>
2023-04-12 13:54:49 +0900

    refactor : CarMoveCondition 을 static 객체로 관리함으로써 test case 수정.

--- src/test/java/racing/CarMoveConditionTest.java
+++ src/test/java/racing/CarMoveConditionTest.java
@@ -11,17 +11,15 @@ class CarMoveConditionTest {
   @ParameterizedTest
   @ValueSource(ints = {0, 1, 2, 3})
   void 자동차가_움직일_수_없는_조건인_경우(int number) {
-    CarMoveCondition carMoveCondition = new CarMoveCondition();
 
-    assertThat(carMoveCondition.isMove(number)).isFalse();
+    assertThat(CarMoveCondition.isMove(number)).isFalse();
   }
 
   @ParameterizedTest
   @ValueSource(ints = {4, 5, 6, 7, 8, 9})
   void 자동차가_움직일_수_있는_조건인_경우(int number) {
-    CarMoveCondition carMoveCondition = new CarMoveCondition();
 
-    assertThat(carMoveCondition.isMove(number)).isTrue();
+    assertThat(CarMoveCondition.isMove(number)).isTrue();
   }
 
 }
