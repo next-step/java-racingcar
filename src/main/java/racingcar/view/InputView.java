6d0a3bafbbe3fa4b446866e55a894fb4c301c77b
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-12 12:58:32 +0900

    feat: 입력 값 전달 객체 InputData 수정  - 입력 값 전달 필드명 변경  - 입력 값 조회 메서드 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,16 +7,16 @@ import java.util.Scanner;
 public class InputView {
 
     public static final String CAR_PARTICIPATION_NUMBER_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
-    public static final String CAR_REPETITION_COUNT_INPUT_MESSAGE = "시도할 회수는 몇 대 인가요?";
+    public static final String CAR_REPETITION_COUNT_INPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    public static final Scanner SCANNER = new Scanner(System.in);
 
-    public InputData getUserInput() {
-        Scanner scanner = new Scanner(System.in);
+    public static InputData getUserInput() {
 
         System.out.println(CAR_PARTICIPATION_NUMBER_INPUT_MESSAGE);
-        int participationNumber = scanner.nextInt();
+        int participationNumber = SCANNER.nextInt();
 
         System.out.println(CAR_REPETITION_COUNT_INPUT_MESSAGE);
-        int repetitionCount = scanner.nextInt();
+        int repetitionCount = SCANNER.nextInt();
 
         return new InputData(participationNumber, repetitionCount);
     }
