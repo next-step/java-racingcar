ed63e6e476b55c8f60249cc573f99e911de9a2a6
MoonHKLee <moonhyuk.lee@lotte.net>
2023-04-12 21:41:06 +0900

    refactor(test) : 테스트메서드 네이밍 변경. - 테스트 의도를 더 표시하도록 변경.

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -12,7 +12,7 @@ public class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3})
     @DisplayName("자동차가 정지한다.")
-    void test1(int number) {
+    void testNonMove(int number) {
         Car car = new Car();
         car.move(number);
         assertThat(car.getPosition()).isEqualTo(0);
@@ -21,7 +21,7 @@ public class CarTest {
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     @DisplayName("자동차가 전진한다.")
-    void test2(int number) {
+    void testMove(int number) {
         Car car = new Car();
         car.move(number);
         assertThat(car.getPosition()).isEqualTo(1);
@@ -30,7 +30,7 @@ public class CarTest {
     @ParameterizedTest
     @CsvSource(value = {"3:0", "4:1"}, delimiter = ':')
     @DisplayName("자동차의 위치를 가져온다.")
-    void test3(int number, int expected) {
+    void testGetPosition(int number, int expected) {
         Car car = new Car();
         car.move(number);
         assertThat(car.getPosition()).isEqualTo(expected);
--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -12,7 +12,7 @@ public class ValidatorTest {
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
     @DisplayName("자동차 대수가 1이하면 IllegalArgumentException 예외가 발생한다.")
-    void test1(int number) {
+    void testCarLatherThanOne(int number) {
         assertThatThrownBy(()->Validator.validateCarAmount(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 대수는 1 이상이어야 합니다.");
@@ -21,14 +21,14 @@ public class ValidatorTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 10})
     @DisplayName("자동차 대수가 1 이상이면 아무 문제 없다.")
-    void test2(int number) {
+    void testCarMoreThanOne(int number) {
         assertThatNoException().isThrownBy(()->Validator.validateCarAmount(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
     @DisplayName("시도 횟수가 1이하면 IllegalArgumentException 예외가 발생한다.")
-    void test3(int number) {
+    void testTryLatherThanOne(int number) {
         assertThatThrownBy(()->Validator.validateTryAmount(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("시도 횟수는 1 이상이어야 합니다.");
@@ -37,7 +37,7 @@ public class ValidatorTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 10})
     @DisplayName("시도 횟수가 1 이상이면 아무 문제 없다.")
-    void test4(int number) {
+    void testTryMoreThanOne(int number) {
         assertThatNoException().isThrownBy(()->Validator.validateTryAmount(number));
     }
 }
