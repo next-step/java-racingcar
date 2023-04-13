4a872224425d9676e07fcb06c6745737758fcff3
eohyemin <eohyemin@gmail.com>
2023-04-14 08:28:06 +0900

    refactor(InputView): 상수명 변경 및 상수값 정리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,17 +3,15 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final String CAR_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
-    private static final String STAGE_INPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static int carInput() {
-        System.out.println(CAR_INPUT_MESSAGE);
-        return scanner.nextInt();
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return SCANNER.nextInt();
     }
 
     public static int stageInput() {
-        System.out.println(STAGE_INPUT_MESSAGE);
-        return scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return SCANNER.nextInt();
     }
 }
