7cb2319520684c857676ef70b21a47653f73cda5
dongdongKing <ultraraptor@naver.com>
2023-04-14 02:25:06 +0900

    feat : 요구사항과 같은 출력으로 표시

--- src/main/java/step3/present/OutputPresent.java
+++ src/main/java/step3/present/OutputPresent.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 public class OutputPresent {
     public void printResult(List<List<String>> collect) {
+        System.out.println("실행 결과");
         for (int i = 0; i < collect.get(0).size(); i++) {
-            System.out.println("###");
+            System.out.println("");
             for (int j = 0; j < collect.size(); j++) {
                 System.out.println(collect.get(j).get(i));
             }
