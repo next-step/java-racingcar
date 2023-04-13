ed1c51a112ee050fc88353e7d22f4bf0cb4476db
ShinYeahChan <syeahc@gmail.com>
2023-04-13 21:13:02 +0900

    style : Car클래스 move메서드 매개변수명 변경

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -3,7 +3,7 @@ package racing;
 public class Car {
     public int moveStatus;
 
-    public void move(int moveCount) {
-        moveStatus += moveCount;
+    public void move(int moveOrStop) {
+        moveStatus += moveOrStop;
     }
 }
