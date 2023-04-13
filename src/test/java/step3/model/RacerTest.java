997389e97af90fe12eccb8fc36c813c0c5a29332
dongdongKing <ultraraptor@naver.com>
2023-04-14 02:05:50 +0900

    feat : 테스트추가

--- src/test/java/step3/model/RacerTest.java
+++ src/test/java/step3/model/RacerTest.java
@@ -3,9 +3,11 @@ package step3.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class RacerTest {
 
@@ -29,6 +31,25 @@ public class RacerTest {
         assertThatThrownBy(() -> {
             Racer racer = new Racer(iterations);
         }).isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("경기결과에 따른 진행상황이 정확히 표시된다")
+    @Test
+    public void 결과계산() {
+        //given
+        Racer racer = new Racer(2);
+        //when
+        List<String> results = racer.calculateResults(List.of(7, 7, 7, 7, 7));
+        //then
+        assertAll(
+                () -> assertThat(results).hasSize(5),
+                () -> assertThat(results.get(0)).isEqualTo("-"),
+                () -> assertThat(results.get(1)).isEqualTo("--"),
+                () -> assertThat(results.get(2)).isEqualTo("---"),
+                () -> assertThat(results.get(3)).isEqualTo("----"),
+                () -> assertThat(results.get(4)).isEqualTo("-----")
+        );
+
     }
 
 }
