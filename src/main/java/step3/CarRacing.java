12031fd1124bc0fcc5a67e76d8a2cb5349ede711
dongdongKing <ultraraptor@naver.com>
2023-04-14 02:32:06 +0900

    feat : 불필요한 로컬변수 제거

--- src/main/java/step3/CarRacing.java
+++ src/main/java/step3/CarRacing.java
@@ -8,13 +8,7 @@ public class CarRacing {
     public static void main(String[] args) {
         InputPresent inputPresent = new InputPresent();
         OutputPresent outputPresent = new OutputPresent();
-
-        int iterations = inputPresent.iterations();
-        int participates = inputPresent.participates();
-        Match match = new Match(participates, iterations);
-
-
+        Match match = new Match(inputPresent.participates(), inputPresent.iterations());
         outputPresent.printResult(match.display());
-
     }
 }
