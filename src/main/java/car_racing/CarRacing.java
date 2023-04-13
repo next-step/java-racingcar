55e8680ff5c8733689da7abb6fcbe6f50ea5e379
hj1115hj <hyejin_one@naver.com>
2023-04-14 02:17:36 +0900

    feat: 우승 로직을 구현한다.

--- src/main/java/car_racing/CarRacing.java
+++ src/main/java/car_racing/CarRacing.java
@@ -2,6 +2,8 @@ package car_racing;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CarRacing {
@@ -11,19 +13,42 @@ public class CarRacing {
         return cars;
     }
 
-    public CarRacing(List<Car> cars) {
-        this.cars = cars;
-    }
-
     @VisibleForTesting
     public void setCars(List<Car> carMocks) {
         cars = carMocks;
+    }
+
+    public CarRacing(List<Car> cars) {
+        this.cars = cars;
     }
 
     public void moveCars() {
         for (int i = 0; i < cars.size(); i++) {
             cars.get(i).moveOrStop();
         }
+    }
+
+    public List<Car> getWinner() {
+        List<Integer> carMoveCounts = getCarMoveCounts();
+        int maxIndex = Collections.max(carMoveCounts);
+        return getWinCars(maxIndex);
+    }
+
+    private List<Integer> getCarMoveCounts() {
+        List<Integer> carMoveCounts = new ArrayList<>();
+        for (Car car : cars)
+            carMoveCounts.add(car.getMoveCount());
+        return carMoveCounts;
+    }
+
+    private List<Car> getWinCars(int maxIndex) {
+        List<Car> winCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getMoveCount() == maxIndex) {
+                winCars.add(car);
+            }
+        }
+        return winCars;
     }
 
 }
--- src/main/java/ui/Application.java
+++ src/main/java/ui/Application.java
@@ -5,6 +5,8 @@ import car_racing.CarRacing;
 import dto.InputDto;
 import util.RandomNumberGenerator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         InputDto inputDto;
@@ -16,6 +18,7 @@ public class Application {
         }
         CarRacing carRacing = new CarRacing(Car.generateCars(inputDto.carNames, new RandomNumberGenerator()));
         run(carRacing, inputDto.trySize);
+        List<Car> winCars = carRacing.getWinner();
     }
 
     private static void run(CarRacing carRacing, int trySize) {
