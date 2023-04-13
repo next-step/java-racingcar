ad83eb80a3832d2669400c69ea037e933e1ee469
jeenstar <jeenstar11@gmail.com>
2023-04-14 03:03:53 +0900

    step2 - StringAddCalculator, step1 - package separation

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,48 @@
+package step1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 크기 확인")
+    void size() {
+        int size = numbers.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("Set 값 존재 확인")
+    void contains(int num) {
+        assertThat(numbers.contains(num)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("결과값이 다른 경우 확인")
+    void containsAllCase(int input, String expected) {
+        String result = String.valueOf(numbers.contains(input));
+        assertThat(result).isEqualTo(expected);
+    }
+}
