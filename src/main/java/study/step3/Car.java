e65749d20449f2ed4e71004676e7a3a7b176c216
gisungPark <gisung2215@naver.com>
2023-04-14 00:40:20 +0900

    feat: 3단계, 전달 받은 값이 4 이상일 경우 자동차 전진

--- src/main/java/study/step3/Car.java
+++ src/main/java/study/step3/Car.java
@@ -4,7 +4,26 @@ import java.util.Random;
 
 public class Car {
 
+    private int round;
+    private int position;
+
     public Car() {
+        round = 0;
+        position = 0;
+    }
+
+    public boolean move() {
+        round++;
+        if (isExceedStandard(getRandomInt())) {
+            position++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isExceedStandard(int num) {
+        return num > 3;
     }
 
     public int getRandomInt() throws IllegalArgumentException {
--- src/test/java/study/step3/CarTest.java
+++ src/test/java/study/step3/CarTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
     Car car = new Car();
-    
+
     @Test
     @DisplayName("0~9 사이 무작위 값을 반환한다.")
     public void random_함수_test() throws Exception {
@@ -16,5 +16,13 @@ class CarTest {
                     .isGreaterThan(-1)
                     .isLessThan(10);
         }
+    }
+
+    @Test
+    @DisplayName("전달 받은 값이 4 이상일 경우 true를 반환한다.")
+    public void car_move_test() throws Exception {
+        assertThat(car.isExceedStandard(5)).isTrue();
+        assertThat(car.isExceedStandard(4)).isTrue();
+        assertThat(car.isExceedStandard(3)).isFalse();
     }
 }
