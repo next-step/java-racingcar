3e2627cd41c1dd770d63bc5b63310907d7c22486
juno-junho <ssmm0205@naver.com>
2023-04-14 01:36:10 +0900

    feat(racingcar): Game을 실행하는 GameController 생성

---
+++ src/main/java/study/racingcar/GameController.java
@@ -0,0 +1,8 @@
+package study.racingcar;
+
+public class GameController {
+    public static void main(String[] args) {
+        GameBoard racingCarGame = new GameBoard();
+        racingCarGame.run();
+    }
+}
