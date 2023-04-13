5f83cc16b9e5a289aebc2d6b7ff1164e374afa53
juno-junho <ssmm0205@naver.com>
2023-04-14 01:35:33 +0900

    docs(racingcar): 기능 목록 작성

---
+++ src/test/java/study/racingcar/InputViewTest.java
@@ -0,0 +1,21 @@
+package study.racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+
+    @Test
+    void 자동차_개수_입력() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+}
