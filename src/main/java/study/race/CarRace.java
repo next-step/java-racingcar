ea0211b38307d9c45a7998d206b8974aff04e1d1
letitgobaby <gnu@gnuui-MacBookPro.local>
2023-04-13 17:17:14 +0900

    feat : 자동차 개수 및 이동 횟수 테스트

---
+++ src/main/java/study/race/CarRace.java
@@ -0,0 +1,17 @@
+package study.race;
+
+public class CarRace {
+
+    private int carNum;
+    private int tryNum;
+
+    public CarRace(int carNum, int tryNum) {
+        this.carNum = carNum;
+        this.tryNum = tryNum;
+    }
+    
+    public void run() {
+
+    }
+
+}
--- src/test/java/study/race/TestCarRaceSimple.java
+++ src/test/java/study/race/TestCarRaceSimple.java
@@ -1,13 +1,16 @@
 package study.race;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestCarRaceSimple {
+
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
@@ -20,6 +23,14 @@ public class TestCarRaceSimple {
         }
 
         assertThat(car.showHistory().length()).isEqualTo(moveNum);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({"2, 4", "4, 5", "3, 6"})
+    @DisplayName("CarRace Car 개수 및 시도 횟수 테스트")
+    public void test_carrace_carNum_tryNum_input(int carNum, int tryNum) {
+        assertDoesNotThrow(() -> new CarRace(carNum, tryNum));
     }
     
 }
