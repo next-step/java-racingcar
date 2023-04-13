ccd70a467cada9acfca99aac95ba45977f290b72
ShinYeahChan <syeahc@gmail.com>
2023-04-13 19:39:03 +0900

    test : assertAll 사용

--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -8,6 +8,7 @@ import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class RacingTest {
     @Test
@@ -15,14 +16,18 @@ public class RacingTest {
         int input_car_count = 3;
         int input_try_count = 7;
         Racing racing = new Racing(input_car_count,input_try_count);
-        assertThat(racing.cars.size()).isEqualTo(3);
-        assertThat(racing.try_count).isEqualTo(7);
+        assertAll(
+                () -> assertThat(racing.cars.size()).isEqualTo(3),
+                () -> assertThat(racing.try_count).isEqualTo(7)
+        );
     }
 
     @Test
     void 자동차수_시도횟수_음수_입력() {
-        assertThatThrownBy(() -> new Racing(-3,7)).isInstanceOf(IllegalArgumentException.class).hasMessage("음수는 입력할 수 없습니다.");
-        assertThatThrownBy(() -> new Racing(3,-7)).isInstanceOf(IllegalArgumentException.class).hasMessage("음수는 입력할 수 없습니다.");
+        assertAll(
+                () -> assertThatThrownBy(() -> new Racing(-3,7)).isInstanceOf(IllegalArgumentException.class).hasMessage("음수는 입력할 수 없습니다."),
+                () -> assertThatThrownBy(() -> new Racing(3,-7)).isInstanceOf(IllegalArgumentException.class).hasMessage("음수는 입력할 수 없습니다.")
+        );
     }
 
     @ParameterizedTest
