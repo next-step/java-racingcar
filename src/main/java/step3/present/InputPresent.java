2b6a79c173bd89a4d3de879a6d0c3c8992e10d20
dongdongKing <ultraraptor@naver.com>
2023-04-14 01:12:19 +0900

    feat : 프리젠테이션 계층을 구현

---
+++ src/main/java/step3/present/InputPresent.java
@@ -0,0 +1,23 @@
+package step3.present;
+
+import java.util.Scanner;
+
+public class InputPresent {
+    public static final String PARTICIPATES = "자동차 대수는 몇 대 인가요?";
+    public static final String ITERATIONS = "시도할 회수는 몇 회 인가요?";
+
+    private final Scanner scanner;
+    public InputPresent() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int participates() {
+        System.out.println(PARTICIPATES);
+        return scanner.nextInt();
+    }
+
+    public int iterations() {
+        System.out.println(ITERATIONS);
+        return scanner.nextInt();
+    }
+}
