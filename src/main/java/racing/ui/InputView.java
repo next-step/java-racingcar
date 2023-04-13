e8b449f445282eb2bbe2cc3e455894f222e959b8
ShinYeahChan <syeahc@gmail.com>
2023-04-13 20:54:45 +0900

    refactor : InputView 클래스 Scanner 상수로 선언

--- src/main/java/racing/ui/InputView.java
+++ src/main/java/racing/ui/InputView.java
@@ -6,13 +6,14 @@ public class InputView {
     public int car_count;
     public int try_count;
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public InputView(int input_car_count, int input_try_count) {
         this.car_count = input_car_count;
         this.try_count = input_try_count;
     }
 
     public static InputView create() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("자동차 대수는 몇 대 인가요?");
         int input_car_count = scanner.nextInt();
         System.out.println("시도할 회수는 몇 회 인가요?");
