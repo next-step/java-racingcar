e5510e05f5607bd7748a1d409cedcb9d008d5991
letitgobaby <gnu@gnuui-MacBookPro.local>
2023-04-14 08:51:33 +0900

    refactor : CarTest Car객체 중복코드 처리

--- src/test/java/study/race/TestCar.java
+++ src/test/java/study/race/TestCar.java
@@ -2,6 +2,7 @@ package study.race;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,12 +10,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 
 public class TestCar {
+
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car();
+    }
     
     @Test
     @DisplayName("Car 객체의 전진 조건 테스트 - 성공")
     public void test_car_move_condition_success() {
         int successNum = 4;
-        Car car = new Car();
         car.move(successNum);
 
         assertThat(car.getScore().length()).isEqualTo(1);
@@ -25,8 +32,6 @@ public class TestCar {
     @DisplayName("Car 객체의 전진 조건 테스트 - 실패")
     public void test_car_move_condition_fail() {
         int failNum = 3;
-
-        Car car = new Car();
         car.move(failNum);
 
         assertThat(car.getScore().length()).isEqualTo(0);
@@ -38,8 +43,6 @@ public class TestCar {
     @DisplayName("Car 객체의 전진 기록 테스트")
     public void test_car_move_history(int moveNum) {
         int successNum = 4;
-
-        Car car = new Car();
         for (int i = 0; i < moveNum; i++) {
             car.move(successNum);
         }
