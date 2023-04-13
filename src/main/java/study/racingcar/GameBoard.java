8a7b248e4e1d12b8c971c6d01ac871304c91f746
juno-junho <ssmm0205@naver.com>
2023-04-14 01:45:09 +0900

    feat(racingcar): 4 이상일 경우 Car 객체의 위치 증가

--- src/main/java/study/racingcar/GameBoard.java
+++ src/main/java/study/racingcar/GameBoard.java
@@ -17,5 +17,20 @@ public class GameBoard {
         for (int i = 0; i < carNum; i++) {
             cars.add(new Car());
         }
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int tryCount = sc.nextInt();
+
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < tryCount; i++) {
+            for (Car car : cars) {
+                int randomValue = random.nextInt(10);
+                if (randomValue >= 4) {
+                    car.setMoveCount(car.getMoveCount() + 1);
+                }
+            }
+        }
+
     }
 }
