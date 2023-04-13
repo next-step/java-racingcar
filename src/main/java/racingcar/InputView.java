46b864c46eea41b325e110ab7268c1e15c57d9a9
MintKim1990 <rlaalsxo1990@gmail.com>
2023-04-13 20:32:54 +0900

    fix: 사용자가 게임내용을 입력받는 InputView 클래스 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -2,17 +2,12 @@ package racingcar;
 
 import java.util.Scanner;
 
-public class Organizer {
+public class InputView {
 
     private static final String ASK_CAR_MENT = "자동차 대수는 몇 대 인가요?";
     private static final String ASK_ROUND_MENT = "시도할 회수는 몇 회 인가요?";
 
     private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        Game game = new Game(askCar(), askRound());
-        game.play();
-    }
 
     public static int askCar() {
         System.out.println(ASK_CAR_MENT);
