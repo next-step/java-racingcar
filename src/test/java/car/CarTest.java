2851d5e1556e34ad60a591a609d5e778506f23cf
eohyemin <eohyemin@gmail.com>
2023-04-14 10:38:47 +0900

    test: 테스트 파라미터 및 테스트명 변경

--- src/test/java/car/CarTest.java
+++ src/test/java/car/CarTest.java
@@ -8,27 +8,26 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
-    private static final int DEFAULT_CAR_POSITION = 0;
 
-    @DisplayName("랜덤값이 4 이상일 경우 자동차가 이동한다")
-    @ParameterizedTest
-    @ValueSource(ints = {4,5,6,7,8,9})
-    void carRandomMoveTest(int input) {
-        Car car = new Car(DEFAULT_CAR_POSITION);
+    @DisplayName("자동차가 이동할 수 있는 경우 위치가 1 증가한다")
+    @Test
+    void move() {
+        Car car = new Car();
+        int position = car.getPosition();
 
         MoveStrategy moveStrategy = () -> true;
         car.move(moveStrategy);
-        assertThat(car.getPosition()).isEqualTo(DEFAULT_CAR_POSITION + 1);
+        assertThat(car.getPosition()).isEqualTo(position + 1);
     }
 
-    @DisplayName("랜덤값이 4 미만일 경우 자동차가 이동하지 않는다")
-    @ParameterizedTest
-    @ValueSource(ints = {0,1,2,3})
-    void carRandomNotMoveTest(int input) {
-        Car car = new Car(DEFAULT_CAR_POSITION);
+    @DisplayName("자동차가 이동할 수 없는 경우 위치가 증가하지 않는다")
+    @Test
+    void notMove() {
+        Car car = new Car();
+        int position = car.getPosition();
 
         MoveStrategy moveStrategy = () -> false;
         car.move(moveStrategy);
-        assertThat(car.getPosition()).isEqualTo(DEFAULT_CAR_POSITION);
+        assertThat(car.getPosition()).isEqualTo(position);
     }
 }
