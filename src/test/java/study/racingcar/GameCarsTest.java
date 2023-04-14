9ae967a28aa699211d47ff3b427223a86ba8932e
juno-junho <ssmm0205@naver.com>
2023-04-14 23:57:13 +0900

    test(racingcar): GameCars 일급 컬렉션 테스트

--- src/test/java/study/racingcar/GameCarsTest.java
+++ src/test/java/study/racingcar/GameCarsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class GameCarsTest {
 
@@ -16,5 +16,23 @@ class GameCarsTest {
         GameCars cars = new GameCars(carNum);
         // then
         assertThat(cars).hasSize(carNum);
+    }
+
+    @DisplayName("1보다 작은 수를 입력할 때 예외 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void exception_occur_when_input_is_less_than_one(int carNum) {
+
+        assertThatThrownBy(() -> new GameCars(carNum))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1보다 큰 수를 입력하세요");
+    }
+
+    @DisplayName("1보다 같거나 큰 수 입력시 예외 발생하지 않는다")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,10,100})
+    void exception_occur_when_input_is_eqaul_or_greater_than_one(int carNum) {
+        assertThatNoException()
+                .isThrownBy(() -> new GameCars(carNum));
     }
 }
