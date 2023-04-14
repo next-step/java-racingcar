3cf132841be95574d45abe5ebb74a6805534ede5
kingdong <ultraraptor@naver.com>
2023-04-14 21:24:55 +0900

    feat : insert name of car

--- src/main/java/step3/present/InputPresent.java
+++ src/main/java/step3/present/InputPresent.java
@@ -1,9 +1,14 @@
 package step3.present;
 
+import step3.model.Car;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputPresent {
-    public static final String PARTICIPATES = "자동차 대수는 몇 대 인가요?";
+    public static final String PARTICIPATES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)";
     public static final String ITERATIONS = "시도할 회수는 몇 회 인가요?";
 
     private final Scanner scanner;
@@ -12,9 +17,10 @@ public class InputPresent {
         this.scanner = new Scanner(System.in);
     }
 
-    public int participates() {
+    public List<Car> participates() {
         System.out.println(PARTICIPATES);
-        return scanner.nextInt();
+        return Arrays.stream(scanner.nextLine().split(",")).map(s -> new Car(s))
+            .collect(Collectors.toList());
     }
 
     public int iterations() {
