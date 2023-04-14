6d7b2e0af6696bffabd28233667b6a8e48421bae
seriouskang <scdean110@gmail.com>
2023-04-14 22:10:14 +0900

    refactor(RandomZeroAndPositiveIntegerGenerator): Random 타입 인스턴스 변수 상수화

--- src/main/java/study/carrace/step3/domain/RandomZeroAndPositiveIntegerGenerator.java
+++ src/main/java/study/carrace/step3/domain/RandomZeroAndPositiveIntegerGenerator.java
@@ -3,15 +3,14 @@ package study.carrace.step3.domain;
 import java.util.Random;
 
 public class RandomZeroAndPositiveIntegerGenerator implements RandomIntegerGenerator {
-    private final Random random;
+    private static final Random RANDOM = new Random();
     private final int maxNumber;
 
     public RandomZeroAndPositiveIntegerGenerator(int maxNumber) {
-        this.random = new Random();
         this.maxNumber = maxNumber;
     }
 
     public int generate() {
-        return random.nextInt(maxNumber + 1);
+        return RANDOM.nextInt(maxNumber + 1);
     }
 }
