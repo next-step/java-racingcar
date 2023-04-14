4994443079c6724d62bcb0cc86bac4e7bc1f2585
hoa0217 <gjwjdghk123@naver.com>
2023-04-15 01:26:02 +0900

    refactor: "RacingGame 클래스 제거"
    - InputView와 ResultView를 같은 레이어에 두기 위해 제거

--- src/main/java/study/racingcar/Application.java
+++ src/main/java/study/racingcar/Application.java
@@ -1,7 +1,9 @@
 package study.racingcar;
 
-import study.racingcar.logic.RacingGame;
+import study.racingcar.domain.Race;
+import study.racingcar.factory.RaceFactory;
 import study.racingcar.view.InputView;
+import study.racingcar.view.ResultView;
 
 public class Application {
 
@@ -9,7 +11,12 @@ public class Application {
     String[] nameOfCars = InputView.inputNameOfCars();
     int numOfRaces = InputView.inputNumOfRaces();
 
-    RacingGame racing = new RacingGame(nameOfCars, numOfRaces);
-    racing.start();
+    Race race = RaceFactory.createRace(nameOfCars);
+    for (int i = 0; i < numOfRaces; i++) {
+      race.play();
+      ResultView.printResult(i, race.getCars());
+    }
+
+    ResultView.printWinners(race.getCars());
   }
 }
