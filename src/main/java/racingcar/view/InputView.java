3a1020803b43245bcf9eefe1269f915df51d0247
이재현 <wincrane2@flow.team>
2023-04-15 00:06:54 +0900

    feat: 자동차 대수, 시도 횟수가 음수일 경우에 대한 예외처리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,12 +11,34 @@ public class InputView {
     }
 
     public int enterNumberOfCars() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.nextInt();
+        int numberOfCars = -1;
+        try {
+            System.out.println("자동차 대수는 몇 대 인가요?");
+            numberOfCars = scanner.nextInt();
+            isValidateNum(numberOfCars);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            enterNumberOfCars();
+        }
+        return numberOfCars;
     }
 
     public int enterNumberOfTry() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        return scanner.nextInt();
+        int numberOfTry = -1;
+        try {
+            System.out.println("시도할 회수는 몇 회 인가요?");
+            numberOfTry = scanner.nextInt();
+            isValidateNum(numberOfTry);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            enterNumberOfTry();
+        }
+        return numberOfTry;
+    }
+
+    private void isValidateNum(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다.\n다시 입력해주세요.");
+        }
     }
 }
