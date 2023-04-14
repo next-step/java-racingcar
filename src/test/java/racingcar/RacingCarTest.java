88672a89f50dd3e5ffa3915eb30f452c85d5f3b9
hoyeoon <hoyeon.backend@gmail.com>
2023-04-15 03:38:40 +0900

    test: 자동차의 이동 횟수 리스트 만들기

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -5,22 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RacingCarTest {
-
-    @DisplayName("0에서 9사이의 random 값을 구한다.")
-    @Test
-    void getRandomNumber() {
-        //given
-        RandomGenerator randomGenerator = new RandomGenerator();
-
-        //when
-        int randomNumber = randomGenerator.getRandomNumber();
-
-        //then
-        assertThat(randomNumber).isBetween(0, 9);
-    }
 
     @DisplayName("random 값이 4이상이면 전진한다.")
     @ParameterizedTest
@@ -37,7 +29,7 @@ public class RacingCarTest {
         assertThat(moveYn).isEqualTo(result);
     }
 
-    @DisplayName("자동차의 상태 만들기(1회 전진마다 -추가")
+    @DisplayName("자동차의 출력 상태값 만들기(1회 전진마다 -추가")
     @ParameterizedTest
     @CsvSource(value = {"0:''", "1:'-'", "2:'--'", "3:'---'", "4:'----'", "5:'-----'"}, delimiter = ':')
     void makeCarMoveStateLine(int moveCount, String result) {
@@ -49,5 +41,25 @@ public class RacingCarTest {
 
         //then
         assertThat(carMoveStateLine).isEqualTo(result);
+    }
+
+    @DisplayName("자동차의 이동 횟수 리스트 만들기")
+    @Test
+    void makeMoveCounts() {
+        //given
+        RacingCar racingCar = new RacingCar(5);
+
+        List<Integer> unmovableNumbers = Arrays.asList(1, 2);
+        List<Integer> movableNumbers = Arrays.asList(4, 5, 6);
+        List<Integer> numbers = new ArrayList<>();
+        numbers.addAll(unmovableNumbers);
+        numbers.addAll(movableNumbers);
+
+        //when
+        List<Integer> list = racingCar.makeMoveCounts(numbers);
+
+        //then
+        assertThat(unmovableNumbers.size()).isEqualTo(Collections.frequency(list, 0));
+        assertThat(movableNumbers.size()).isEqualTo(Collections.frequency(list, 1));
     }
 }
