e8f919be0f7c75441f59b1618f1682b31411b4fd
In-wook Hwang <inwook9@gmail.com>
2023-04-14 22:47:27 +0900

    refactor: 유의미한 값 상수화 2

--- src/main/java/game/domain/policy/StandardCarMovePolicy.java
+++ src/main/java/game/domain/policy/StandardCarMovePolicy.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class StandardCarMovePolicy implements CarMovePolicy {
 
     private static final int MOVABLE_THRESHOLD_NUMBER = 4;
+    private static final int RANDOM_NUMBER_MINIMUM = 0;
+    private static final int RANDOM_NUMBER_MAXIMUM = 9;
 
     @Override
     public boolean isMovable() {
@@ -14,7 +16,7 @@ public class StandardCarMovePolicy implements CarMovePolicy {
     private int generateRandomNumber() {
         return ThreadLocalRandom
                 .current()
-                .nextInt(0, 10 + 1);
+                .nextInt(RANDOM_NUMBER_MINIMUM, RANDOM_NUMBER_MAXIMUM + 1);
     }
 
 }
