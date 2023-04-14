bb75e4f26f0a9cfa2d5c95f695ee0008617d8e21
kingdong <ultraraptor@naver.com>
2023-04-14 21:22:35 +0900

    feature : feat impl

---
+++ src/main/java/step3/model/Car.java
@@ -0,0 +1,12 @@
+package step3.model;
+
+public class Car {
+    private final String name;
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
