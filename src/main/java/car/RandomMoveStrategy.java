0b5793eaf7e4b6a059a9fda6f9e33677d95c8710
eohyemin <eohyemin@gmail.com>
2023-04-14 11:10:07 +0900

    refactor(RandomMoveStrategy): 상수명 변경

--- src/main/java/car/RandomMoveStrategy.java
+++ src/main/java/car/RandomMoveStrategy.java
@@ -3,11 +3,11 @@ package car;
 import java.util.Random;
 
 public class RandomMoveStrategy implements MoveStrategy {
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
     private static final int DEFAULT_MOVABLE_VALUE = 4;
 
     @Override
     public boolean movable() {
-        return random.nextInt(10) >= DEFAULT_MOVABLE_VALUE;
+        return RANDOM.nextInt(10) >= DEFAULT_MOVABLE_VALUE;
     }
 }
