eb45a1c8db16263489fededf088c6b5049a5ad78
kingdong <ultraraptor@naver.com>
2023-04-15 22:53:22 +0900

    refactor : refactor local var name

--- src/main/java/step3/model/Match.java
+++ src/main/java/step3/model/Match.java
@@ -8,28 +8,28 @@ import java.util.stream.IntStream;
 
 public class Match {
 
-    private final List<Racer> collect;
+    private final List<Racer> racerList;
 
     public Match(List<Car> participates, int iterations) {
-        this.collect = IntStream.range(0, participates.size())
+        this.racerList = IntStream.range(0, participates.size())
             .boxed()
             .map(integer -> new Racer(iterations, participates.get(integer)))
             .collect(Collectors.toList());
     }
 
     public List<DisplayRaceVO> display() {
-        return collect.stream()
+        return racerList.stream()
             .map(racer -> new DisplayRaceVO(new Result(racer.getResults()), racer.getCar()))
             .collect(Collectors.toList());
     }
 
     public List<String> winnerDisplay() {
         //전체 점수를 가져온데
-        List<Integer> positions = collect.stream().map(racer -> racer.getFinalPosition()).collect(Collectors.toList());
+        List<Integer> positions = racerList.stream().map(racer -> racer.getFinalPosition()).collect(Collectors.toList());
         //최고득점을 가져온다
         int maxPos = positions.stream().mapToInt(pos -> pos.intValue()).max().getAsInt();
         //최고득점인 사람을 찾는다
-        List<String> collect1 = collect.stream().filter(racer -> racer.getFinalPosition() == maxPos).map(racer -> racer.getCar().getName()).collect(Collectors.toList());
+        List<String> collect1 = racerList.stream().filter(racer -> racer.getFinalPosition() == maxPos).map(racer -> racer.getCar().getName()).collect(Collectors.toList());
         return collect1;
     }
 
