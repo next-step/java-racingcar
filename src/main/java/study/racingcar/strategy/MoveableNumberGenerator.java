84baad8346c8200b0cabe9823e557a60182c272f
juno-junho <ssmm0205@naver.com>
2023-04-15 23:33:33 +0900

    refactor(racingcar): 전략패턴에 따라 클래스 및 인터페이스 생성

---
+++ src/main/java/study/racingcar/strategy/MoveableNumberGenerator.java
@@ -0,0 +1,9 @@
+package study.racingcar.strategy;
+
+public class MoveableNumberGenerator implements NumberGenerator{
+    public static final int MOVABLE_LOWER_BOUND = 4;
+    @Override
+    public int generate() {
+        return MOVABLE_LOWER_BOUND;
+    }
+}
