8b16c693269cedd3f4fa60e7c850e34721e6adad
kingdong <ultraraptor@naver.com>
2023-04-15 12:00:40 +0900

    delete : unused import remove

--- src/test/java/step3/present/InputPresentTest.java
+++ src/test/java/step3/present/InputPresentTest.java
@@ -1,7 +1,6 @@
 package step3.present;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step3.model.Car;
