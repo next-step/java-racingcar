3290193a417bbfc5eb1c661b36318340954ca0d7
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-16 01:27:29 +0900

    refactor: 게임실행

--- src/main/java/step3/RacingCarApplication.java
+++ src/main/java/step3/RacingCarApplication.java
@@ -1,15 +1,19 @@
 package step3;
 
-import step3.strategy.draw.DashDrawStrategy;
+import step3.domain.CarRacingType;
 import step3.strategy.move.RandomMoveStrategy;
-import step3.view.InputView;
-import step3.view.PrintView;
+import step3.view.UsingNameCarInputView;
 
 public class RacingCarApplication {
     public static void main(String[] args) {
 
-        RacingGame racingGame = RacingGame.of(InputView.getInstance(), PrintView.of(DashDrawStrategy.getInstance()), RandomMoveStrategy.getInstance());
-        racingGame.execute();
+      AbstractRacingCarGame abstractRacingCarGame = AbstractRacingCarGame.of(
+              CarRacingType.USING_NAME_CAR,
+              UsingNameCarInputView.getInstance(),
+              RandomMoveStrategy.getInstance()
+      );
+
+      abstractRacingCarGame.execute();
     }
 
 }
