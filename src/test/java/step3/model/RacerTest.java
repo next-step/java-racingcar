55c1e50ba720435b2be139643c0e2ce429b4bd52
kingdong <ultraraptor@naver.com>
2023-04-15 12:08:31 +0900

    test/refactor : 리팩토링에 의해 깨진 테스트코드를 복구

--- src/test/java/step3/model/RacerTest.java
+++ src/test/java/step3/model/RacerTest.java
@@ -17,7 +17,7 @@ public class RacerTest {
         //given
         int iterations = 5;
         //when
-        Racer racer = new Racer(iterations);
+        Racer racer = new Racer(iterations,new Car("hello"));
         //then
         assertThat(racer.getResults()).hasSize(iterations);
     }
@@ -29,7 +29,7 @@ public class RacerTest {
         //when
         //then
         assertThatThrownBy(() -> {
-            Racer racer = new Racer(iterations);
+            Racer racer = new Racer(iterations,new Car("hello"));
         }).isInstanceOf(RuntimeException.class);
     }
 
@@ -37,9 +37,9 @@ public class RacerTest {
     @Test
     public void 결과계산() {
         //given
-        Racer racer = new Racer(2);
+        Racer racer = new Racer(2,new Car("hello"));
         //when
-        List<String> results = racer.calculateResults(List.of(7, 1, 7, 1, 7));
+        List<String> results = racer.calculateResults(List.of(7, 1, 7, 1, 7)).getPositionList();
         //then
         assertAll(
                 () -> assertThat(results).hasSize(5),
@@ -55,10 +55,10 @@ public class RacerTest {
     @Test
     public void 전진하는4이상() {
         //given
-        Racer racer = new Racer(2);
+        Racer racer = new Racer(2,new Car("hello"));
         List<Integer> scores = List.of(4, 4, 4, 4, 4, 4, 4, 4, 4, 4);
         //when
-        List<String> results = racer.calculateResults(scores);
+        List<String> results = racer.calculateResults(scores).getPositionList();
         //then
         assertAll(
                 () -> assertThat(results).hasSize(scores.size()),
@@ -79,10 +79,10 @@ public class RacerTest {
     @Test
     public void 전진하지않는3이하다() {
         //given
-        Racer racer = new Racer(2);
+        Racer racer = new Racer(2,new Car("hello"));
         List<Integer> scores = List.of(3, 3, 3, 3, 3, 3, 3, 3, 3, 3);
         //when
-        List<String> results = racer.calculateResults(scores);
+        List<String> results = racer.calculateResults(scores).getPositionList();
         //then
         assertAll(
                 () -> assertThat(results).hasSize(scores.size()),
