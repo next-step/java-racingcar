23d0fabaf3f212a1d07741fdc12ea1ff9f0346ec
doho-ho <edh1021@gmail.com>
2023-04-15 20:49:03 +0900

    Step2 -  코드리뷰 반영

--- src/main/java/step2/StringCalculator.java
+++ src/main/java/step2/StringCalculator.java
@@ -6,38 +6,42 @@ import java.util.regex.Pattern;
 public class StringCalculator {
 
     private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int CUSTOM_DELIMITER_GROUP = 1;
+    private static final int CUSTOM_DELIMITER_OTHER_GROUP = 2;
 
-    public static long calculate(String input) {
+    public static int calculate(String input) {
         if (input == null || input.isEmpty()) {
-            return 0L;
+            return 0;
         }
-        return sum(parseLong(split(input)));
+        return sum(parsing(split(input)));
     }
 
     private static String[] split(String input) {
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
         if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            return matcher.group(2).split(customDelimiter);
+            String customDelimiter = matcher.group(CUSTOM_DELIMITER_GROUP);
+            return matcher.group(CUSTOM_DELIMITER_OTHER_GROUP).split(customDelimiter);
         }
         return input.split(DEFAULT_DELIMITER);
     }
 
-    private static long[] parseLong(String[] numbers) {
+    private static long[] parsing(String[] numbers) {
         long[] newNumbers = new long[numbers.length];
-        try {
-            for (int i = 0; i < numbers.length; i++) {
-                newNumbers[i] = Long.parseLong(numbers[i]);
-                if(newNumbers[i] < 0) {
-                    throw new RuntimeException("음수가 입력되었습니다.");
-                }
-            }
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("숫자가 아닌 값이 입력되었습니다.");
+        for (int i = 0; i < numbers.length; i++) {
+            newNumbers[i] = parseLong(numbers[i]);
         }
         return newNumbers;
     }
-    private static long sum(long[] numbers) {
+
+    private static long parseLong(String number) {
+        long newNumber = Long.parseLong(number);
+        if(newNumber < 0) {
+            throw new RuntimeException("음수가 입력되었습니다.");
+        }
+        return newNumber;
+    }
+    private static int sum(long[] numbers) {
         long sum = 0;
         for (long number : numbers) {
             sum += number;
@@ -45,6 +49,6 @@ public class StringCalculator {
         if (sum > Integer.MAX_VALUE) {
             throw new RuntimeException("합이 int 범위를 초과한 수가 입력되었습니다.");
         }
-        return sum;
+        return (int) sum;
     }
 }
--- src/test/java/step2/StringCalculatorTest.java
+++ src/test/java/step2/StringCalculatorTest.java
@@ -2,6 +2,9 @@ package step2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.*;
 import static step2.StringCalculator.*;
@@ -9,34 +12,34 @@ import static step2.StringCalculator.*;
 public class StringCalculatorTest {
     @Test
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
-    void emptyOrNull() {
+    @NullAndEmptySource
+    void emptyOrNull(String text) {
         // given & when & then
-        assertThat(calculate(null)).isEqualTo(0);
-        assertThat(calculate("")).isEqualTo(0);
+        assertThat(calculate(text)).isEqualTo(0);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
-    void oneNumber() {
+    @CsvSource(value = {"1,1", "10,10"})
+    void oneNumber(String text, int expected) {
         // given & when & then
-        assertThat(calculate("1")).isEqualTo(1);
-        assertThat(calculate("10")).isEqualTo(10);
+        assertThat(calculate(text)).isEqualTo(expected);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
-    void twoNumbers() {
+    @CsvSource(value = {"1,2|3", "10,21|31"}, delimiter = '|')
+    void twoNumbers(String input, int expected) {
         // given & when & then
-        assertThat(calculate("1,2")).isEqualTo(3);
-        assertThat(calculate("10,21")).isEqualTo(31);
+        assertThat(calculate(input)).isEqualTo(expected);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.")
-    void colon() {
+    @CsvSource(value = {"1,2:3|6", "10,21:32|63"}, delimiter = '|')
+    void colon(String input, int expected) {
         // given & when & then
-        assertThat(calculate("1,2:3")).isEqualTo(6);
-        assertThat(calculate("10,21:32")).isEqualTo(63);
+        assertThat(calculate(input)).isEqualTo(expected);
     }
 
     @Test
@@ -64,12 +67,12 @@ public class StringCalculatorTest {
                 .hasMessageContaining("숫자가 아닌 값이 입력되었습니다.");
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("구분자만 존재하는 경우 0을 반환한다.")
-    void onlyDelimiter() {
+    @CsvSource(value = {",|0",":|0"}, delimiter = '|')
+    void onlyDelimiter(String input, int expected) {
         // given & when & then
-        assertThat(calculate(",")).isEqualTo(0);
-        assertThat(calculate(":")).isEqualTo(0);
+        assertThat(calculate(input)).isEqualTo(expected);
     }
 
     @Test
