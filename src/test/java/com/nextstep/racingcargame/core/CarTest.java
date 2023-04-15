712661fe57350b130cf4f7f359bc18ab57f70b1a
patrickYoo <yhy1045@naver.com>
2023-04-15 14:33:33 +0900

    test: Distance 에서 검증해야하는 로직 CarTest 로 이관

--- src/test/java/com/nextstep/racingcargame/core/CarTest.java
+++ src/test/java/com/nextstep/racingcargame/core/CarTest.java
@@ -35,14 +35,6 @@ class CarTest {
     }
 
     @Test
-    @DisplayName("3 칸 이동한 자동차 보다 4칸을 이동한 자동차가 더 멀리간 값을 참으로 출력한다.")
-    void isLongerThanTest() {
-        Car fourMovedCar = new Car(TEST_CAR_NAME, MOVE_NUMBER);
-        Distance moveThreeStep = new Distance(STOP_NUMBER);
-        assertThat(fourMovedCar.isLongerThan(moveThreeStep)).isTrue();
-    }
-
-    @Test
     @DisplayName("Equals And Hash Code 구현에 따라 Car 객체에 동일한 값이 주입되어 생성된 경우 서로 같은 해시값을 갖는다")
     void equalsAndHashCodeTest() {
         Car group_one = new Car(TEST_CAR_NAME, new Distance(0), new RandomNumberZeroToNine());
--- src/test/java/com/nextstep/racingcargame/core/DistanceTest.java
+++ src/test/java/com/nextstep/racingcargame/core/DistanceTest.java
@@ -2,6 +2,7 @@ package com.nextstep.racingcargame.core;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +12,28 @@ class DistanceTest {
 
     private static final int MOVE_FIVE_STEP = 5;
 
+    private static final int STOP_NUMBER = 3;
+
+    private static final int MOVE_NUMBER = 4;
+
+    private static final int MOVE_STEP = 1;
+
+    private static final String TEST_CAR_NAME = "test";
+
     @Test
     @DisplayName("차량의 이동거리만큼의 길이를 문자열로 정상 출력한다")
     void distanceFormTest() {
         Distance distance = new Distance(MOVE_FIVE_STEP);
         assertThat(distance.distanceForm(CAR_DISTANCE_DISPLAY_UNIT))
                 .isEqualTo("-".repeat(MOVE_FIVE_STEP));
+    }
+
+    @Test
+    @DisplayName("3 칸 이동한 자동차 보다 4칸을 이동한 자동차가 더 멀리간 값을 참으로 출력한다.")
+    void isLongerThanTest() {
+        Car fourMovedCar = new Car(TEST_CAR_NAME, MOVE_NUMBER);
+        Distance moveThreeStep = new Distance(STOP_NUMBER);
+        Assertions.assertThat(fourMovedCar.isLongerThan(moveThreeStep)).isTrue();
     }
 
     @Test
