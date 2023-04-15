fcb711fed0761d41104f359cb580a63d4bd7f450
kingdong <ultraraptor@naver.com>
2023-04-15 13:01:08 +0900

    chore : git cherrypick

--- src/main/java/step3/model/Match.java
+++ src/main/java/step3/model/Match.java
@@ -1,5 +1,7 @@
 package step3.model;
 
+import step3.present.DisplayRaceVO;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,16 +10,27 @@ public class Match {
 
     private final List<Racer> collect;
 
-    public Match(int participates, int iterations) {
-        this.collect = IntStream.rangeClosed(1, participates)
-                .boxed()
-                .map(integer -> new Racer(iterations))
-                .collect(Collectors.toList());
+    public Match(List<Car> participates, int iterations) {
+        this.collect = IntStream.range(0, participates.size())
+            .boxed()
+            .map(integer -> new Racer(iterations, participates.get(integer)))
+            .collect(Collectors.toList());
     }
 
-    public List<List<String>> display() {
+    public List<DisplayRaceVO> display() {
         return collect.stream()
-                .map(Racer::getResults)
-                .collect(Collectors.toList());
+            .map(racer -> new DisplayRaceVO(new Result(racer.getResults()), racer.getCar()))
+            .collect(Collectors.toList());
     }
+
+    public List<String> winnerDisplay() {
+        //전체 점수를 가져온데
+        List<Integer> positions = collect.stream().map(racer -> racer.getFinalPosition()).collect(Collectors.toList());
+        //최고득점을 가져온다
+        int maxPos = positions.stream().mapToInt(pos -> pos.intValue()).max().getAsInt();
+        //최고득점인 사람을 찾는다
+        List<String> collect1 = collect.stream().filter(racer -> racer.getFinalPosition() == maxPos).map(racer -> racer.getCar().getName()).collect(Collectors.toList());
+        return collect1;
+    }
+
 }
