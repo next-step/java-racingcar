0fec96e6c07eff0ca9f1bf309f23b51ecc372c51
kingdong <ultraraptor@naver.com>
2023-04-15 22:45:35 +0900

    test : display change

--- src/test/java/step3/present/OutputPresentTest.java
+++ src/test/java/step3/present/OutputPresentTest.java
@@ -64,7 +64,7 @@ public class OutputPresentTest {
         assertThat(outputStream.toString().trim()).isEqualTo(answer);
     }
 
-    @DisplayName("우승출력 AB-TEST : 누가 우승했는지를 알려준다. 여러명일수 있다")
+    @DisplayName("우승출력 TEST : 여러명이 우승했을때는 우승한 여러명 모두를 출력한다")
     @Test
     public void multiWinner() {
         //given
@@ -76,7 +76,7 @@ public class OutputPresentTest {
         assertThat(outputStream.toString().trim()).isEqualTo(answer);
     }
 
-    @DisplayName("우승출력 AB-TEST : 우승했는지를 알려준다")
+    @DisplayName("우승출력 TEST : 단독우승시 한명만 출력된다")
     @Test
     public void winnerDisplay() {
         //given
