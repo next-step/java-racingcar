466276eb94e3aedd66751afc8b4740c5ed698568
kingdong <ultraraptor@naver.com>
2023-04-15 12:00:29 +0900

    delete : unused import remove

--- src/test/java/step3/present/OutputPresentTest.java
+++ src/test/java/step3/present/OutputPresentTest.java
@@ -62,4 +62,27 @@ public class OutputPresentTest {
         assertThat(outputStream.toString().trim()).isEqualTo(answer);
     }
 
+    @DisplayName("우승출력 AB-TEST : 누가 우승했는지를 알려준다. 여러명일수 있다")
+    @Test
+    public void multiWinner() {
+        //given
+        List<String> input = List.of("Winner", "Manner", "Checkin", "Dinner");
+        String answer = "Winner,Manner,Checkin,Dinner 가 최종 우승하였습니다";
+        //when
+        outputPresent.printWinner(input);
+        //then
+        assertThat(outputStream.toString().trim()).isEqualTo(answer);
+    }
+
+    @DisplayName("우승출력 AB-TEST : 우승했는지를 알려준다")
+    @Test
+    public void winnerDisplay() {
+        //given
+        List<String> input = List.of("Winner");
+        String answer = "Winner 가 최종 우승하였습니다";
+        //when
+        outputPresent.printWinner(input);
+        //then
+        assertThat(outputStream.toString().trim()).isEqualTo(answer);
+    }
 }
