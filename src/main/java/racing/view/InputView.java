3d97ea8e309748ce3fcf99e4f54527cf65849c25
std-freejia <std.freejia@gmail.com>
2023-04-16 22:40:43 +0900

    main 메서드에서 호출할 입력용, 출력용 view 단 클래스 정의

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
 
+<<<<<<< HEAD
     private static final String MESSAGE_NAMES_OF_UNIT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String MESSAGE_COUNT_OF_TURN = "시도할 회수는 몇 회 인가요?";
     private final static Scanner scanner = new Scanner(System.in);
@@ -14,5 +15,23 @@ public class InputView {
         System.out.println(MESSAGE_COUNT_OF_TURN);
         int count = scanner.nextInt();
         return new InputDto(nameInput, count);
+=======
+    private static final String MESSAGE_COUNT_OF_UNIT = "자동차 대수는 몇 대 인가요?";
+    private static final String MESSAGE_COUNT_OF_TURN = "시도할 회수는 몇 회 인가요?";
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int InputCountOfUnit() {
+        System.out.println(MESSAGE_COUNT_OF_UNIT);
+        return this.scanner.nextInt();
+    }
+
+    public int InputCountOfTurn() {
+        System.out.println(MESSAGE_COUNT_OF_TURN);
+        return scanner.nextInt();
+>>>>>>> 583de095d (main 메서드에서 호출할 입력용, 출력용 view 단 클래스 정의)
     }
 }
--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -1,12 +1,17 @@
 package racing.view;
 
 import racing.domain.Car;
+<<<<<<< HEAD
 import racing.domain.Cars;
+=======
+import racing.dto.GameResult;
+>>>>>>> 583de095d (main 메서드에서 호출할 입력용, 출력용 view 단 클래스 정의)
 
 import java.util.List;
 
 public class ResultView {
 
+<<<<<<< HEAD
     private static void println() {
         System.out.print("\n");
     }
@@ -49,6 +54,25 @@ public class ResultView {
         printName(car.name());
         for (int i = 0; i < car.location(); i++) {
             printDash();
+=======
+    public static void printCarsLocations(List<GameResult> results) {
+        for (int i = 0; i < results.size(); i++) { // 1회
+            printLocation(results.get(i).getResult());
+        }
+        System.out.println("\n\n");
+    }
+
+    private static void printLocation(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            printOnceCar(list.get(i));
+            System.out.println("\n");
+        }
+    }
+
+    private static void printOnceCar(int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.print("-");
+>>>>>>> 583de095d (main 메서드에서 호출할 입력용, 출력용 view 단 클래스 정의)
         }
     }
 }
