b8ca2a59262f3508e7fd0034e88765ff21cc3074
MJ_Aus <alswjd7010@gmail.com>
2023-04-17 00:54:09 +0900

    create main method

--- src/main/java/step3/RacingCarMain.java
+++ src/main/java/step3/RacingCarMain.java
@@ -1,5 +1,24 @@
 package step3;
 
+import static step3.ResultView.printRaceStep;
+
 public class RacingCarMain {
+    public static void main(String[] args) {
+        int car = step3_bak.InputView.input("자동차 대수는 몇개인가요?");
+        int count = InputView.input("시도할 회수는 몇 회 인가요?");
+
+        String[][] races = new String[car][count];
+
+        RacingCar rc = new RacingCar();
+        rc.racingCar(races);
+        printRaceStep(races);
+    }
+
+
+
+
+
+
+    //ResultView.printRaceStep(races);
 
 }
--- src/test/java/step3/RacingCarTest.java
+++ src/test/java/step3/RacingCarTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import step3.RacingCar;
 
 import java.util.stream.Stream;
 
