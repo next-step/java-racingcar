240580b09333035692f0ac1f561ecd4225ff7307
std-freejia <std.freejia@gmail.com>
2023-04-16 22:48:34 +0900

    style 개행 조금 수정

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -18,11 +18,7 @@ public class InputView {
 =======
     private static final String MESSAGE_COUNT_OF_UNIT = "자동차 대수는 몇 대 인가요?";
     private static final String MESSAGE_COUNT_OF_TURN = "시도할 회수는 몇 회 인가요?";
-    private final Scanner scanner;
-
-    public InputView() {
-        this.scanner = new Scanner(System.in);
-    }
+    private final Scanner scanner = new Scanner(System.in);
 
     public int InputCountOfUnit() {
         System.out.println(MESSAGE_COUNT_OF_UNIT);
--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -58,14 +58,15 @@ public class ResultView {
     public static void printCarsLocations(List<GameResult> results) {
         for (int i = 0; i < results.size(); i++) { // 1회
             printLocation(results.get(i).getResult());
+            System.out.print("\n");
         }
-        System.out.println("\n\n");
+        System.out.print("\n");
     }
 
     private static void printLocation(List<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             printOnceCar(list.get(i));
-            System.out.println("\n");
+            System.out.print("\n");
         }
     }
 
