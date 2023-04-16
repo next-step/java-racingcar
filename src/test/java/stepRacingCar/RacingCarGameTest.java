0d156c4d39ff0a3dbdcb1f2bf38a3ab83b506d74
SightStudio <sdm3285@gmail.com>
2023-04-16 18:55:32 +0900

    test (자동차 경주 자동차 이름 제한, 자동차 없는 게임 생성금지 테스트코드 추가하기)

--- src/test/java/stepRacingCar/RacingCarGameTest.java
+++ src/test/java/stepRacingCar/RacingCarGameTest.java
@@ -1,15 +1,19 @@
 package stepRacingCar;
 
+import java.util.Collections;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingCar.RacingCarGame;
+import racingCar.car.RacingCar;
 import racingCar.car.move.RacingCarMoveDirectionStrategy;
 import racingCar.car.move.RacingCarMoveForward;
+import racingCar.exception.InvalidRacingCarNameException;
+import racingCar.exception.NotAllowedGameSettingException;
 import racingCar.view.RacingCarGameResultView;
-import util.RandomUtils;
 
 import java.util.List;
 
@@ -22,18 +26,17 @@ public class RacingCarGameTest {
   }
 
   @DisplayName("[RacingCarGame.class] 레이싱카 게임은 사용자가 입력한 숫자만큼 레이싱카가 설정된다.")
-  @ParameterizedTest
-  @ValueSource(ints = {1, 2, 3})
-  public void 사용자_입력만큼_레이싱카_세팅(int racingCarCnt) {
+  public void 사용자_입력만큼_레이싱카_세팅() {
 
+    final List<String> 레이싱카_목록 = List.of("car1");
     // when
-    RacingCarGame racingCarGame = new RacingCarGame(racingCarCnt, new RacingCarGameResultView(), allowedMoveStrategies);
+    RacingCarGame racingCarGame = new RacingCarGame(레이싱카_목록, new RacingCarGameResultView(), allowedMoveStrategies);
 
     // then
     Assertions.assertThat(racingCarGame)
         .extracting("racingCars")
         .asList()
-        .hasSize(racingCarCnt);
+        .isEqualTo(레이싱카_목록);
   }
 
   @DisplayName("[RacingCarGame.class] 레이싱카 게임은 게임 턴마다 자동차 움직임을 기록한다.")
@@ -41,8 +44,8 @@ public class RacingCarGameTest {
   @ValueSource(ints = {1, 2, 3, 4})
   public void 레이싱카_게임은_매_이동마다_기록을_가지고_있음(int moveTryCnt) {
     // given
-    final int 레이싱카_수 = 10;
-    RacingCarGame racingCarGame = new RacingCarGame(레이싱카_수, new RacingCarGameResultView(), allowedMoveStrategies);
+    final List<String> 레이싱카_목록 = List.of("car1");
+    RacingCarGame racingCarGame = new RacingCarGame(레이싱카_목록, new RacingCarGameResultView(), allowedMoveStrategies);
 
     // when
     racingCarGame.play(moveTryCnt);
@@ -52,5 +55,16 @@ public class RacingCarGameTest {
         .extracting("resultView.snapShots")
         .asList()
         .hasSize(moveTryCnt);
+  }
+
+  @DisplayName("[RacingCarGame.class] 레이싱카가 한대도 존재하지 않으면 게임을 시작 할 수 없다.")
+  @Test
+  public void 레이싱카_게임은_최소_한대의_자동차가_요구된다() {
+    // given
+    final List<String> 레이싱카_목록 = Collections.emptyList();
+
+    // when && then
+    Assertions.assertThatThrownBy(() -> new RacingCarGame(레이싱카_목록, new RacingCarGameResultView(), allowedMoveStrategies))
+        .isInstanceOf(NotAllowedGameSettingException.class);
   }
 }
--- src/test/java/stepRacingCar/RacingCarTest.java
+++ src/test/java/stepRacingCar/RacingCarTest.java
@@ -3,6 +3,7 @@ package stepRacingCar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingCar.car.RacingCar;
@@ -11,12 +12,14 @@ import racingCar.car.move.RacingCarMoveForward;
 import racingCar.car.move.RacingCarMoveServiceLocator;
 
 import java.util.List;
+import racingCar.exception.InvalidRacingCarNameException;
 
 public class RacingCarTest {
 
-  private final long racingCarId = 1;
+  private final String racingCarName = "car1";
 
   private RacingCarMoveServiceLocator racingCarMoveServiceLocator;
+
   @BeforeEach
   void setup() {
     List<RacingCarMoveDirectionStrategy> allowedMoveStrategies = List.of(new RacingCarMoveForward());
@@ -28,7 +31,7 @@ public class RacingCarTest {
   @ValueSource(ints = {4, 5, 6, 7, 8, 9})
   public void 레이싱카는_4_이상에서_전진한다(int moveAck) {
     // given
-    RacingCar racingCar = new RacingCar(racingCarId, racingCarMoveServiceLocator);
+    RacingCar racingCar = new RacingCar(racingCarName, racingCarMoveServiceLocator);
 
     // when
     racingCar.moveIfPossible(moveAck);
@@ -44,7 +47,7 @@ public class RacingCarTest {
   @ValueSource(ints = {0, 1, 2, 3})
   public void 레이싱카는_4_미만의_수에서는_전진안한다(int moveAck) {
     // given
-    RacingCar racingCar = new RacingCar(racingCarId, racingCarMoveServiceLocator);
+    RacingCar racingCar = new RacingCar(racingCarName, racingCarMoveServiceLocator);
 
     // when
     racingCar.moveIfPossible(moveAck);
@@ -53,5 +56,16 @@ public class RacingCarTest {
     Assertions.assertThat(racingCar)
         .extracting("position")
         .isEqualTo(0);
+  }
+
+  @DisplayName("[RacingCar.class] 레이싱카의 이름은 5글자를 초과할 수 없다.")
+  @Test
+  public void 레이싱카의_이름은_5글자_초과_불가능() {
+    // given
+    final String 레이싱카_이름 = "car123";
+
+    // when & then
+    Assertions.assertThatThrownBy(() -> new RacingCar(레이싱카_이름, racingCarMoveServiceLocator))
+        .isInstanceOf(InvalidRacingCarNameException.class);
   }
 }
