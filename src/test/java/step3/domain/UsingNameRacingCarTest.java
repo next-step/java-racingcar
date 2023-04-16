94ecf7a003990228f9f6fd957aa732b103c2f3f8
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-16 21:47:17 +0900

    feat: 레이싱카 도메인 테스트

---
+++ src/test/java/step3/domain/UsingNameRacingCarTest.java
@@ -0,0 +1,48 @@
+package step3.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class UsingNameRacingCarTest {
+
+    @ParameterizedTest
+    @MethodSource("getRacingCarName")
+    @DisplayName("이름을 가지는 레이싱카 생성 테스트")
+    void 이름을_가지는_레이싱카_생성_테스트(String carName) {
+        UsingNameRacingCar usingNameRacingCar = UsingNameRacingCar.of(carName);
+
+        assertThat(usingNameRacingCar.getName()).isEqualTo(carName);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("null or 빈값 일 경우 예외를 던진다")
+    void null_or_빈값_일_경우_예외를_던진다(String carName) {
+
+        assertThatIllegalArgumentException().isThrownBy(() -> UsingNameRacingCar.of(carName))
+                .withMessage("자동차 이름을 입력해주세요");
+    }
+
+    @ParameterizedTest
+    @MethodSource("getRacingCarName")
+    @DisplayName("레이싱카 이동 테스트")
+    void 레이싱카_이동_테스트(String carName) {
+        UsingNameRacingCar usingNameRacingCar = UsingNameRacingCar.of(carName);
+
+        usingNameRacingCar.move(() -> true);
+
+        assertThat(usingNameRacingCar.getCurrentLocation()).isEqualTo(1);
+    }
+
+    static Stream<String> getRacingCarName() {
+        return Stream.of("pobi", "gang", "gwal", "gwak");
+    }
+
+}
