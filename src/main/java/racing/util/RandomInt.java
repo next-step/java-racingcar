0da4b944ec8b8bcbff6df7451be6ab6c089b1ebd
std-freejia <std.freejia@gmail.com>
2023-04-16 23:10:51 +0900

    feat 랜덤값 구한 후, "4 이상인 경우에만 자동차 이동 가능" 조건 추가

--- src/main/java/racing/util/RandomInt.java
+++ src/main/java/racing/util/RandomInt.java
@@ -7,7 +7,7 @@ public class RandomInt {
     private static final int RANDOM_INT_MAX = 8;
     private static final int RANDOM_INT_MIN = 1;
 
-    public static int makeRandomInt(){
+    public static int makeRandomInt() {
         Random random = new Random();
         int randomPower = random.nextInt(RANDOM_INT_MAX) + RANDOM_INT_MIN;
         return randomPower;
