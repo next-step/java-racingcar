cc54ef8512041da4058dfccf541dbf576d51c960
kingdong <ultraraptor@naver.com>
2023-04-16 17:18:44 +0900

    refactor : 검증메서드 분리

--- src/main/java/step3/model/Racer.java
+++ src/main/java/step3/model/Racer.java
@@ -13,12 +13,16 @@ public class Racer {
     private final Car car;
 
     public Racer(int iterations, Car car) {
-        if (iterations < 0) {
-            throw new RuntimeException("iterations 는 1 이상이어야 합니다");
-        }
+        validateIterations(iterations);
         this.scores = doRace(iterations);
         this.results = calculateResults(scores.getScoreList());
         this.car = car;
+    }
+
+    private void validateIterations(int iterations) {
+        if (iterations < 0) {
+            throw new RuntimeException("iterations 는 1 이상이어야 합니다");
+        }
     }
 
     private Score doRace(int iterations) {
