d71c65e482fd3cd0d61ae4c91c3fc3fae457aca3
std-freejia <std.freejia@gmail.com>
2023-04-16 22:44:01 +0900

    style formatting

--- src/main/java/racing/domain/Cars.java
+++ src/main/java/racing/domain/Cars.java
@@ -1,75 +1,15 @@
 package racing.domain;
 
-<<<<<<< HEAD
-=======
 import racing.domain.Car;
 import racing.dto.GameResult;
 import racing.util.RandomInt;
 
->>>>>>> e356fab2c (refactor Car 클래스를 필드로 가지는 일급컬렉션 추가)
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cars { // 일급컬렉션
     private List<Car> cars;
 
-<<<<<<< HEAD
-    public Cars(List<Car> cars) {
-        this.cars = cars;
-    }
-
-    public Car theCar(int index) {
-        return this.cars.get(index);
-    }
-
-    public int size() {
-        return this.cars.size();
-    }
-
-    public Cars playOnceTurn() {
-
-        List<Car> movedCars = new ArrayList<>();
-        RandomGameRule randomGameRule = new RandomGameRule();
-
-        for (int i = 0; i < this.cars.size(); i++) {
-            int randomNumber = randomGameRule.getPossibleInt();
-            Car car = this.cars.get(i).move(randomNumber);
-            movedCars.add(car);
-        }
-        return new Cars(movedCars);
-    }
-
-    public List<String> findWinners() {
-
-        List<String> winners = new ArrayList<>();
-        int maxLocation = maxLocation();
-
-        for (int i = 0; i < this.cars.size(); i++) {
-            addOnlyWinner(winners, maxLocation, i);
-        }
-        return winners;
-    }
-
-    private void addOnlyWinner(List<String> winners, int maxLocation, int i) {
-        if (checkWinner(maxLocation, this.cars.get(i))) {
-            winners.add(this.cars.get(i).name());
-        }
-    }
-
-    private boolean checkWinner(int maxLocation, Car car) {
-        if (car.isWinner(maxLocation)) {
-            return true;
-        }
-        return false;
-    }
-
-    public int maxLocation() {
-        int maxLocation = 0;
-        for (int i = 0; i < this.cars.size(); i++) {
-            maxLocation = this.cars.get(i).bigger(maxLocation);
-        }
-        return maxLocation;
-=======
     public Cars(List<Car> cars){
         this.cars = cars;
     }
@@ -86,6 +26,5 @@ public class Cars { // 일급컬렉션
             onceResult.add(this.cars.get(i).getLocation());
         }
         return new GameResult(onceResult);
->>>>>>> e356fab2c (refactor Car 클래스를 필드로 가지는 일급컬렉션 추가)
     }
 }
