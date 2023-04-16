275d0b619398e11b756830bd7da507077806f9b7
garden-2 <1eejw@naver.com>
2023-04-16 15:41:53 +0900

    feat: 각 자동차에 이름을 부여할 수 있다. 자동차 이름은 5자를 초과할 수 없다.

--- src/main/java/carrace/Car.java
+++ src/main/java/carrace/Car.java
@@ -1,14 +1,44 @@
 package carrace;
 
 public class Car {
+    private static final int START_POSITION = 0;
+    private static final int ZERO_INDEX = 0;
+    private static final int LIMIT_NAME_LENGTH = 5;
+    private static final String DEFAULT_DRIVER_NAME = "CAR_";
+    private String name;
     private int position;
 
-    public Car() {
-        this.position = 0;
+    public Car(int index, String name) {
+        name = getNameOrDefault(name, index);
+        validateName(name);
+        this.name = name;
+        this.position = START_POSITION;
+    }
+
+    Car(String name) {
+        name = getNameOrDefault(name, ZERO_INDEX);
+        validateName(name);
+        this.name = name;
+        this.position = START_POSITION;
     }
 
     Car(int position) {
+        this.name = DEFAULT_DRIVER_NAME;
         this.position = position;
+    }
+
+    private String getNameOrDefault(String name, int index) {
+        name = name.trim();
+        if (name == null || name.isEmpty()) {
+            return DEFAULT_DRIVER_NAME + index;
+        }
+        return name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > LIMIT_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
     }
 
     public void move(boolean movable) {
@@ -19,5 +49,9 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/main/java/carrace/InputView.java
+++ src/main/java/carrace/InputView.java
@@ -3,7 +3,7 @@ package carrace;
 import java.util.Scanner;
 
 public enum InputView {
-    HOW_MANY_CARS("자동차 대수는 몇 대 인가요?"),
+    HOW_MANY_CARS("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."),
     HOW_MANY_LABS("시도할 회수는 몇 회 인가요?");
 
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -13,8 +13,8 @@ public enum InputView {
         this.description = description;
     }
 
-    public int getAnswer() {
+    public String getAnswer() {
         System.out.println(description);
-        return SCANNER.nextInt();
+        return SCANNER.nextLine();
     }
 }
