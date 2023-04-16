5e4ce14c771634dc76dc3cdaee111e4ac14c17fb
woonys <wodns1324@gmail.com>
2023-04-16 20:08:19 +0900

    feat 차는 입력받은 조건으로 전진 혹은 멈출 수 있다.

--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -5,10 +5,9 @@ public class RacingCar {
     private int distance;
 
     private static final int ONE_TIME_DISTANCE = 1;
+
     public RacingCar() {
     }
-
-
 
     public void moveOrStop(boolean coin) {
         if (coin) {
--- src/test/java/domain/RacingCarTest.java
+++ src/test/java/domain/RacingCarTest.java
@@ -1,17 +1,11 @@
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
+package domain;
 
-import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import domain.Race;
-import domain.RacingCar;
-import domain.CarNavigator;
 
 public class RacingCarTest {
     private RacingCar racingCar;
@@ -21,10 +15,9 @@ public class RacingCarTest {
         racingCar = new RacingCar();
     }
 
-
     @DisplayName("전진하는 조건은 0에서 9 사이에서 4이상일 경우이다.")
     @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @ValueSource(ints = { 4, 5, 6, 7, 8, 9 })
     public void shouldGoOneStep_whenNumberIsUpToFour(int conditionToGo) throws Exception {
         //given
         int oneStep = 1;
@@ -37,7 +30,7 @@ public class RacingCarTest {
 
     @DisplayName("전진하지 않는 조건은 0에서 9 사이에서 4미만일 경우이다.")
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3})
+    @ValueSource(ints = { 0, 1, 2, 3 })
     public void shouldStop_whenNumberIsUnderFour(int conditionToStop) throws Exception {
         //given
         int NoStep = 0;
@@ -47,59 +40,5 @@ public class RacingCarTest {
         //then
         assertThat(racingCar.getDistance()).isEqualTo(NoStep);
     }
-
-    @DisplayName("randomNumber를 생성해 4 이상이면 참을 반환한다")
-    @Test
-    public void shouldReturnTrue_whenRandomNumberIsAboveStandard() {
-        //given
-        int randomNum = CarNavigator.getRandomNumber();
-        int standard = 4;
-
-        //when
-        boolean coin = CarNavigator.flipCoin(randomNum);
-
-        //then
-        if (randomNum >= standard) {
-            assertThat(coin).isTrue();
-        }
-    }
-
-    @DisplayName("randomNumber를 생성해 4 미만이면 거짓을 반환한다")
-    @Test
-    public void shouldReturnFalse_whenRandomNumberIsBelowStandard() {
-        //given
-        int randomNum = CarNavigator.getRandomNumber();
-        int standard = 4;
-
-        //when
-        boolean coin = CarNavigator.flipCoin(randomNum);
-
-        //then
-        if (randomNum < standard) {
-            assertThat(coin).isFalse();
-        }
-    }
-
-    @DisplayName("입력받은 차 대수만큼 차를 생성한다")
-    @Test
-    public void shouldCreateCars_whenInputNumberOfCar() throws Exception {
-        //given
-        int inputNum = 3;
-        //when
-        List<RacingCar> racingCarList = Race.createCar(inputNum);
-        //then
-        assertThat(racingCarList.size()).isEqualTo(inputNum);
-    }
-
-    @DisplayName("입력받은 경기 횟수만큼 경기를 진행한다")
-    @Test
-    public void shouldPlayGameForSpecifiedNumberOfRounds_whenNumberOfRoundsIsSpecified() throws Exception {
-        //given
-
-        //when
-
-        //then
-    }
-
 
 }
