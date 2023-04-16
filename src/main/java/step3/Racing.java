6299f276cc1f1ad87e12f8bbaa4ac5b93e2c0536
foameraserblue <foameraserblue@gmail.com>
2023-04-16 10:37:08 +0900

    feature: Racing 객체 생성, 레이스 참가엔트리와 레이스, 스코어보드 확인 기능 작성

---
+++ src/main/java/step3/Racing.java
@@ -0,0 +1,55 @@
+package step3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Racing {
+    private final List<Car> racingCars;
+    private final int matchCount;
+    private final List<List<Integer>> scoreBoard = new ArrayList<>();
+    private static final Random random = new Random();
+
+    public Racing(int carCount, int matchCount) {
+        this.racingCars = this.racingCarRegistration(carCount);
+        this.matchCount = matchCount;
+    }
+
+    private List<Car> racingCarRegistration(int carCount) {
+        final Car[] racingCars = new Car[carCount];
+
+        for (int i = 0; i < carCount; i++) {
+            racingCars[i] = new Car();
+        }
+
+        return Arrays.asList(racingCars);
+    }
+
+    public void raceStart() {
+        for (int i = 0; i < this.matchCount; i++) {
+            this.carMove();
+            this.insertScore();
+        }
+    }
+
+    private void carMove() {
+        for (Car car : this.racingCars) {
+            car.move(random.nextInt());
+        }
+    }
+
+    private void insertScore() {
+        final List<Integer> currentScore = new ArrayList<>();
+
+        for (Car car : this.racingCars) {
+            currentScore.add(car.currentLocation());
+        }
+
+        this.scoreBoard.add(currentScore);
+    }
+
+    public List<List<Integer>> scoreBoard() {
+        return this.scoreBoard;
+    }
+}
