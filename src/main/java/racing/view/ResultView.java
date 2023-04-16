583de095d79bbae576931dcfd183446f35e56c55
std-freejia <std.freejia@gmail.com>
2023-04-16 22:40:43 +0900

    main 메서드에서 호출할 입력용, 출력용 view 단 클래스 정의

---
+++ src/main/java/racing/view/ResultView.java
@@ -0,0 +1,29 @@
+package racing.view;
+
+import racing.domain.Car;
+import racing.dto.GameResult;
+
+import java.util.List;
+
+public class ResultView {
+
+    public static void printCarsLocations(List<GameResult> results) {
+        for (int i = 0; i < results.size(); i++) { // 1회
+            printLocation(results.get(i).getResult());
+        }
+        System.out.println("\n\n");
+    }
+
+    private static void printLocation(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            printOnceCar(list.get(i));
+            System.out.println("\n");
+        }
+    }
+
+    private static void printOnceCar(int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.print("-");
+        }
+    }
+}
