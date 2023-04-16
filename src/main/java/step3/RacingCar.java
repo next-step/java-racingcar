f1a374beef7c04ecf8f56d987e85f8e2ccd7cdd0
MJ_Aus <alswjd7010@gmail.com>
2023-04-16 23:37:34 +0900

    랜덤값이 4 이상인지 판단

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -1,2 +1,17 @@
-package step3;public class RacingCar {
+package step3;
+
+import java.util.Random;
+
+public class RacingCar {
+
+    public static final int LIMIT = 4;
+    public static final int RANDOM_LIMIT = 10;
+    public boolean isValueMoreThan(int value) {
+        return value >= LIMIT;
+    }
+    public int randomValue() {
+        Random random = new Random();
+        int value = random.nextInt(RANDOM_LIMIT);
+        return value;
+    }
 }
--- src/test/java/step3/RacingCarTest.java
+++ src/test/java/step3/RacingCarTest.java
@@ -1,5 +1,34 @@
 package step3;
 
-public class RacingCarTest {
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import step3.RacingCar;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class RacingCarTest {
+    RacingCar rc = new RacingCar();
+
+    int car = 3;
+    int count = 5;
+    String[][] race = new String[car][count];
+
+    @ParameterizedTest(name = "{0} is more than 4 : {1}")
+    @MethodSource("parameter")
+    void 입력값에_따른_논리결과값(int value, boolean result) {
+        assertThat(rc.isValueMoreThan(value)).isEqualTo(result);
+    }
+
+    static Stream<Arguments> parameter(){
+        return Stream.of(
+                arguments(1, false)
+                ,arguments(2, false)
+                ,arguments(4, true)
+                ,arguments(6, true)
+        );
+    }
 }
