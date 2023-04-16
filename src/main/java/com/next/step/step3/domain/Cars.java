c3e28e18a7dd253c23b630ba1010b42e1cf19b8a
ghkdwlgns612 <ghkdwlgns612@naver.com>
2023-04-16 21:42:00 +0900

    refactor : winner역할을 cars에게 역할 위임.

--- src/main/java/com/next/step/step3/domain/Cars.java
+++ src/main/java/com/next/step/step3/domain/Cars.java
@@ -2,9 +2,14 @@ package com.next.step.step3.domain;
 
 import com.next.step.step3.util.RandomNumberGenerator;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
+
+    private static final int NOT_FOUND_MAX_POSITION = 0;
 
     private List<Car> cars;
 
@@ -21,5 +26,22 @@ public class Cars {
 
     public List<Car> carElements() {
         return this.cars;
+    }
+
+    public int findMaxPosition() {
+        List<Car> cars = new ArrayList<>(this.cars);
+        cars.sort(Comparator.comparingInt(Car::position).reversed());
+
+        return cars.stream()
+                .findFirst()
+                .map(Car::position)
+                .orElse(NOT_FOUND_MAX_POSITION);
+    }
+
+    public List<String> getWinnerNames(int maxPosition) {
+        return cars.stream()
+                .filter(car -> car.position() == maxPosition)
+                .map(Car::name)
+                .collect(Collectors.toList());
     }
 }
