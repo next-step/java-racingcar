3b9dc50a7ccb87e6a50c29ed253fde017d65a384
JhyunJung <jhyun.rigel@gmail.com>
2023-04-16 14:42:07 +0900

    중복 수정 완료

--- src/test/java/RacingCarTest.java
+++ src/test/java/RacingCarTest.java
@@ -1,3 +1,5 @@
+import entity.car.Car;
+import entity.race.CarRace;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,9 +14,9 @@ public class RacingCarTest {
 
     @ParameterizedTest
     @DisplayName("레이싱 경주 초기화")
-    @CsvSource(value = {"1,2", "3,5"}, delimiter = ',')
-    public void initRacing(int num, int count) {
-        CarRace carRace = CarRace.initRace(num, count);
+    @CsvSource(value = {"1:2:test", "3:5:test1,test2,test3"}, delimiter = ':')
+    public void initRacing(int num, int count, String names) {
+        CarRace carRace = CarRace.initRace(num, count,names);
 
         assertThat(num).isEqualTo(carRace.getRacingCars().getCars().size());
         assertThat(count).isEqualTo(carRace.getRoundValue());
@@ -24,7 +26,7 @@ public class RacingCarTest {
     @DisplayName("전진_조건_랜덤_값_구하기_0 이상 9 이하")
     public void 전진_조건_랜덤_값_구하기(){
         //given
-        Car car = new Car();
+        Car car = new Car("test");
         int result = car.moveOrNot();
         //when
         assertThat(result).isGreaterThanOrEqualTo(0).isLessThan(10);
@@ -34,7 +36,7 @@ public class RacingCarTest {
     @DisplayName("레이싱_레이스_로직_구현")
     public void 레이싱_레이스_로직_구현(){
         //given
-        CarRace carRace = CarRace.initRace(3, 5);
+        CarRace carRace = CarRace.initRace(3, 5,"test1,test2,test3");
         carRace.race();
         //when
         List<Integer> moveList = carRace.getRacingCars().getCars().stream()
