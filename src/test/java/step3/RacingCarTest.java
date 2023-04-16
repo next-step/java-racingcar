1c0cced3b1160292eb575cf807132ff52fc3123f
MJ_Aus <alswjd7010@gmail.com>
2023-04-17 00:57:18 +0900

    Revert "create main method"
    This reverts commit b8ca2a59262f3508e7fd0034e88765ff21cc3074.

--- src/test/java/step3/RacingCarTest.java
+++ src/test/java/step3/RacingCarTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import step3.RacingCar;
 
 import java.util.stream.Stream;
 
