348f23e6d20e81e86fe9a7de6cc8358dfb816525
woonys <wodns1324@gmail.com>
2023-04-16 20:10:29 +0900

    feat 경기는 입력받은 조건으로 각 차마다 전진할지 혹은 멈출지를 결정한다.

--- src/main/java/domain/Race.java
+++ src/main/java/domain/Race.java
@@ -7,11 +7,37 @@ public class Race {
 
     public static List<RacingCar> racingCarList = new ArrayList<>();
 
-    public static List<RacingCar> createCar(int inputNum) {
-        for (int i = 0; i < inputNum; i++) {
+    public static void createCar(int carCount) {
+        for (int i = 0; i < carCount; i++) {
             RacingCar car = new RacingCar();
             racingCarList.add(car);
         }
-        return racingCarList;
+    }
+
+    public static void race() {
+        for (RacingCar racingCar : racingCarList) {
+            boolean coin = CarNavigator.flipCoin(CarNavigator.getRandomNumber());
+            racingCar.moveOrStop(coin);
+        }
+    }
+
+    public static String[] getEachResult() {
+        String[] result = new String[racingCarList.size()];
+        for (int i = 0; i < racingCarList.size(); i++) {
+            result[i] = "-".repeat(racingCarList.get(i).getDistance());
+        }
+        return result;
+    }
+
+    public static List<String[]> playRace(int[] input) {
+        int carCount = input[0];
+        int round = input[1];
+        List<String[]> totalResult = new ArrayList<>();
+        Race.createCar(carCount);
+        for (int i = 0; i < round; i++) {
+            Race.race();
+            totalResult.add(getEachResult());
+        }
+        return totalResult;
     }
 }
--- src/test/java/domain/RaceTest.java
+++ src/test/java/domain/RaceTest.java
@@ -1,2 +1,23 @@
-package domain;public class RaceTest {
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RaceTest {
+
+    @DisplayName("입력받은 차 대수만큼 차를 생성한다")
+    @Test
+    public void shouldCreateCars_whenInputNumberOfCar() throws Exception {
+        //given
+        int inputNum = 3;
+        List<RacingCar> racingCarList = Race.racingCarList;
+        //when
+        Race.createCar(inputNum);
+        //then
+        assertThat(racingCarList.size()).isEqualTo(inputNum);
+    }
 }
