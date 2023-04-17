04a2fafcc93483a359a870d7d11d43e5d48f6281
player-geun <player_geun@naver.com>
2023-04-17 15:43:31 +0900

    feat : 자동차의 이름을 입력하는 기능

--- src/main/java/study/step3/view/InputView.java
+++ src/main/java/study/step3/view/InputView.java
@@ -8,6 +8,9 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String TEXT_INPUT_CAR = "자동차 대수는 몇 대 인가요?";
     private static final String TEXT_INPUT_MOVE = "시도할 회수는 몇 회 인가요?";
+    private static final String DELIMINATOR = ",";
+    private static final String TEXT_INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표("
+            + DELIMINATOR + ")를 기준으로 구분).";
 
     public int inputCountOfCar() {
         System.out.print(TEXT_INPUT_CAR);
@@ -33,5 +36,18 @@ public class InputView {
         if (number < 0) {
             throw new IllegalStateException("음수는 입력할 수 없습니다.");
         }
+    }
+
+    public String[] inputCarNames() {
+        System.out.println(TEXT_INPUT_CAR_NAME);
+        return splitString();
+    }
+
+    private String[] splitString() {
+        return inputString().split(DELIMINATOR);
+    }
+
+    private String inputString() {
+        return scanner.next();
     }
 }
