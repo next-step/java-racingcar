4bf96e00f98a790f77533e05d4653afb9a3dda60
foameraserblue <foameraserblue@gmail.com>
2023-04-17 17:41:48 +0900

    feature : 우승자 출력기능 구현

--- src/main/java/race/view/ResultView.java
+++ src/main/java/race/view/ResultView.java
@@ -2,7 +2,7 @@ package race.view;
 
 import race.car.dto.CarInfoDto;
 
-import java.util.List;
+import java.util.*;
 
 public class ResultView {
     private static final String CAR_SCORE = "-";
@@ -17,6 +17,8 @@ public class ResultView {
         for (List<CarInfoDto> carInfoDtos : racingScores) {
             printScoreAtOneTime(carInfoDtos);
         }
+
+        printWinner(racingScores);
     }
 
     private static void printScoreAtOneTime(List<CarInfoDto> carInfoDtos) {
@@ -24,5 +26,30 @@ public class ResultView {
             System.out.println(carInfoDto.name() + COLON + CAR_SCORE.repeat(carInfoDto.location()));
         }
         System.out.println();
+    }
+
+    /**
+     * 우승자 출력
+     */
+    private static void printWinner(List<List<CarInfoDto>> racingScores) {
+        final List<CarInfoDto> racingResult = racingScores.get(racingScores.size() - 1);
+
+        System.out.println(String.join(", ", winnerNameFilter(racingResult)) + "가 최종 우승했습니다.");
+    }
+
+    /**
+     * 우승자 확인
+     */
+    private static List<String> winnerNameFilter(List<CarInfoDto> racingResult) {
+        final Map<Integer, List<String>> resultMap = new HashMap<>();
+
+        for (CarInfoDto carInfoDto : racingResult) {
+            final List<String> value = resultMap.getOrDefault(carInfoDto.location(), new ArrayList<>());
+            value.add(carInfoDto.name());
+
+            resultMap.put(carInfoDto.location(), value);
+        }
+
+        return resultMap.get(Collections.max(resultMap.keySet()));
     }
 }
