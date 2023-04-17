70b6c72b03684de8fdeed8c86c844affe52d12f2
ji lim <jilim@jiui-MacBookPro.local>
2023-04-18 01:07:34 +0900

    Feat : 로직 수정

--- src/main/java/car/step3/Car.java
+++ src/main/java/car/step3/Car.java
@@ -4,6 +4,11 @@ import java.util.Objects;
 
 public class Car {
     private int location = 0;
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
 
     public void go() {
         this.location++;
@@ -13,4 +18,7 @@ public class Car {
         return location;
     }
 
+    public String name(){
+        return name;
+    }
 }
--- src/test/java/car/step3/CarTest.java
+++ src/test/java/car/step3/CarTest.java
@@ -12,21 +12,21 @@ class CarTest {
     HandleCar handleCar = new HandleCar();
     @Test
     public void 차가_전진하는지_테스트() {
-        Car car = new Car();
+        Car car = new Car("new");
         car.go();
         assertThat(car.location()).isEqualTo(1);
     }
 
     @Test
     public void 랜덤값이_4이상일때_전지하는지_테스트() {
-        Car car = new Car();
+        Car car = new Car("lim");
         handleCar.goCar(car);
         assertThat(car.location()).isGreaterThanOrEqualTo(0);
     }
 
     @Test
     public void 차리스트를_조회하여_전진한다() {
-        List<Car> carList = Arrays.asList(new Car(), new Car());
+        List<Car> carList = Arrays.asList(new Car("jiyoung"), new Car("celineååååå"));
         List<Car> newCarList = handleCar.iteratorCar(carList);
         assertThat(newCarList.size()).isEqualTo(carList.size());
         assertThat(newCarList.get(0).location()).isGreaterThanOrEqualTo(carList.get(0).location());
