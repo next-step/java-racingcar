42a1e480e564d032efb05a0217df323a618219a1
lehdqlsl <lehdqlsl@naver.com>
2023-04-17 21:22:56 +0900

    test(Cars) : 승자 테스트 코드 작성.

--- src/test/java/step4/unit/CarTest.java
+++ src/test/java/step4/unit/CarTest.java
@@ -1,25 +1,28 @@
 package step4.unit;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import step3.car.game.Car;
+import step3.car.game.Cars;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.*;
 
 
 public class CarTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"t","te","tes","test","test1"})
+    @ValueSource(strings = {"t", "te", "tes", "test", "test1"})
     void 자동차생성_이름_5자이하(String input) {
         assertThatCode(() -> new Car(input))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"master","coffee","macbook","monitor","keyboard"})
+    @ValueSource(strings = {"master", "coffee", "macbook", "monitor", "keyboard"})
     void 자동차생성_이름_5자를_초과할수없음(String input) {
         assertThatThrownBy(() -> new Car(input))
                 .isInstanceOf(RuntimeException.class);
@@ -30,5 +33,22 @@ public class CarTest {
     void 자동차이름은_비어있거나_널일수없음(String input) {
         assertThatThrownBy(() -> new Car(input))
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 첫번째_자동차가_승자() {
+        Cars cars = new Cars(4);
+
+        for (int i = 0; i < cars.cars().size(); i++) {
+            moveCar(cars.cars().get(i), 5 - i);
+        }
+
+        assertThat(cars.winners()).hasSize(1);
+        assertThat(cars.winners().get(0)).isEqualTo(cars.cars().get(0).name());
+    }
+
+    private void moveCar(Car car1, int location) {
+        IntStream.range(0, location)
+                .forEach(value -> car1.move(9));
     }
 }
