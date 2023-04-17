33310b2dc8a00c876fafde6c3c0e592fb2566be2
0giri <higiri@naver.com>
2023-04-18 03:26:41 +0900

    feat: 자동차 경주를 시작하는 Main 메소드 구현

---
+++ src/main/java/step3/Main.java
@@ -0,0 +1,17 @@
+package step3;
+
+/**
+ * @author : 0giri
+ * @since : 2023/04/16
+ */
+public class Main {
+    public static void main(String[] args) {
+        int carsCount = InputView.askInteger("자동차 개수를 입력해주세요.");
+        int roundsCount = InputView.askInteger("라운드 수를 입력해주세요.");
+
+        CarRacingGame carRacingGame = new CarRacingGame(carsCount);
+        carRacingGame.start(roundsCount);
+
+        ResultView.printGameResult(carRacingGame.result());
+    }
+}
