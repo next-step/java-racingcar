fd5b758f1a90c611b40a5493ddc7688d77f233e7
hanseo-jo <hanseo.jo@navercorp.com>
2023-04-18 02:09:03 +0900

    step 2

---
+++ src/main/java/calculator/StringAdditionCalculator.java
@@ -0,0 +1,42 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAdditionCalculator {
+    private static final String POSITIVE_NUMBER_REGEX = "[^0-9].*";
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+
+    public StringAdditionCalculator() {}
+
+    public int splitAndSum(String str) throws Exception {
+        if (str.isEmpty() || str == null) {
+            return 0;
+        }
+
+        String[] tokens = parseStringToTokens(str);
+        if (!allowedTokens(tokens)) {
+            throw new Exception("include unexpected value");
+        }
+
+        return Arrays.stream(tokens).mapToInt(token -> Integer.parseInt(token)).sum();
+    }
+
+    private String[] parseStringToTokens(String str) {
+        Matcher delimiterMatcher = CUSTOM_DELIMITER_PATTERN.matcher(str);
+        if (delimiterMatcher.find()) {
+            String customDelimiter = delimiterMatcher.group(1);
+            return delimiterMatcher.group(2).split(customDelimiter);
+        }
+        return str.split(",|:");
+    }
+
+    private boolean allowedTokens(String[] strings) {
+        long notAllowedTokenCount = Arrays.stream(strings)
+                .filter(str -> str.matches(POSITIVE_NUMBER_REGEX))
+                .count();
+        return notAllowedTokenCount == 0 ? true : false;
+    }
+}
