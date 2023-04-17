b06ab89888df89de45277d393636da6a4ad0a317
zi-seong <jiseong4577@gmail.com>
2023-04-18 01:36:49 +0900

    test(StringCalculator) : 같은 메서드 테스트가 반복적으로 함수로 정의되는 부분 수정
    4개의 테스트가 매개변수만 다르게 호출하고 있어서 ParameterizedTest의 MethodSource를 사용하여 합쳤습니다.

--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -3,7 +3,11 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -11,7 +15,7 @@ public class StringCalculatorTest {
 
     private StringCalculator stringCalculator = new StringCalculator();
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "빈 문자열 또는 null 값을 입력할 경우 0 반환")
     @NullAndEmptySource
     public void checkNull(String checkStr) {
         int result = stringCalculator.numberStringSum(checkStr);
@@ -21,25 +25,27 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo(0);
     }
 
-    @Test
-    @DisplayName("하나의 숫자를 문자열로 입력할 경우 해당 숫자 반환")
-    public void onlyOneNumberString() {
-        int result = stringCalculator.numberStringSum("3");
-        assertThat(result).isEqualTo(3);
+    /**
+     * 요구사항 4개 한번에 테스트
+     * 1.하나의 숫자를 문자열로 입력할 경우 해당 숫자 반환
+     * 2.숫자 두개를 콤마 구분자로 입력할 경우 두 숫자의 합을 반환
+     * 3.콜론으로도 구분자 사용
+     * 4.// 와 \n문자 사이에 커스텀 구분자 사용
+     */
+    @ParameterizedTest(name = "numberStringSum 테스트")
+    @MethodSource("numberStringSumTest")
+    void numberStringSumTest(String checkStr,int result) {
+        int sumNum = stringCalculator.numberStringSum(checkStr);
+        assertThat(sumNum).isEqualTo(result);
     }
 
-    @Test
-    @DisplayName("숫자 두개를 콤마 구분자로 입력할 경우 두 숫자의 합을 반환")
-    public void splitAndSum() {
-        int result = stringCalculator.numberStringSum("1,2");
-        assertThat(result).isEqualTo(3);
-    }
+    static Stream<Arguments> numberStringSumTest() {
+        return Stream.of(
+                    Arguments.of("3",3),
+                    Arguments.of("1,2",3),
+                    Arguments.of("1,2:3",6),
+                    Arguments.of("//;\n1;2,3",6));
 
-    @Test
-    @DisplayName("콜론으로도 구분자 사용")
-    public void colonDelimiter() {
-        int result = stringCalculator.numberStringSum("1,2:3");
-        assertThat(result).isEqualTo(6);
     }
 
     @Test
@@ -47,12 +53,5 @@ public class StringCalculatorTest {
     public void negative() {
         assertThatThrownBy(() -> stringCalculator.numberStringSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
-    }
-
-    @Test
-    @DisplayName("// 와 \n 문자 사이에 커스텀 구분자 사용")
-    public void customDelimiter() {
-        int result = stringCalculator.numberStringSum("//;\n1;2,3");
-        assertThat(result).isEqualTo(6);
     }
 }
