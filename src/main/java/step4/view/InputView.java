7cd3c622464f16180aaf406b3b140703ffafd517
juhyun <zzang9ha@naver.com>
2023-04-18 00:29:29 +0900

    feat: InputView(입력 객체) 추가

---
+++ src/main/java/step4/view/InputView.java
@@ -0,0 +1,33 @@
+package step4.view;
+
+import step4.domain.entity.UserInput;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private InputView() {
+    }
+
+    public static UserInput input() {
+        final String names = inputCarNames();
+        final int retry = inputRetryCount();
+
+        return new UserInput(names, retry);
+    }
+
+    private static String inputCarNames() {
+        System.out.println("경주할 자동 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+
+        return SCANNER.nextLine();
+    }
+
+    private static int inputRetryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+
+        return SCANNER.nextInt();
+    }
+
+}
