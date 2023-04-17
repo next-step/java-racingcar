f9fc3f375f570bcc5283c84183aafd10797d0587
jeenstar <jeenstar11@gmail.com>
2023-04-17 21:24:01 +0900

    feat: 자동차 생성 및 자동차의 랜덤값 발생 기능 구현

--- src/main/java/step3/SimpleRacingCar.java
+++ src/main/java/step3/SimpleRacingCar.java
@@ -1,5 +1,8 @@
 package step3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleRacingCar {
 
     public static void main(String[] args) {
@@ -10,5 +13,18 @@ public class SimpleRacingCar {
 
         System.out.println("시도할 회수는 몇 회 인가요?");
         int repeatCnt = InputView.inputInfo();
+
+        //입력받은 대수의 자동차 생성 및 각 자동차의 랜덤값 발생시키기
+        List<Car> racingCarList = createCar(carCnt, repeatCnt);
+    }
+
+    public static List<Car> createCar(int carCnt, int repeatCnt) {
+        List<Car> carList = new ArrayList<Car>();
+
+        for(int num=0; num<carCnt; num++){
+            carList.add(new Car(num,repeatCnt));
+        }
+
+        return carList;
     }
 }
