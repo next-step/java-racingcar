dc8dba7db778787b10f49973dc8be3ae88c31a82
ji lim <jilim@jiui-MacBookPro.local>
2023-04-18 01:06:38 +0900

    Feat: 출력 수정

--- src/main/java/car/step3/Main.java
+++ src/main/java/car/step3/Main.java
@@ -2,7 +2,6 @@ package car.step3;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,20 +9,25 @@ public class Main {
         HandleCar handleCar = new HandleCar();
         ResultView resultView = new ResultView();
         //자동차 대수
-        int carNumber = inputView.scannerCarCertain();
+        String carNames = inputView.scannerCarCertain();
+        List<Car> cars = makeCar(carNames);
         //시도할 횟수
         int number = inputView.scannerCarNumber();
-        List<Car> carList = new ArrayList<Car>();
-
-        //입력 받은 자동차 대수만큼 차를 생성하여 리스트에 넣는다.
-        for (int i = 0; i < carNumber; i++) {
-            Car car = new Car();
-            carList.add(car);
-        }
         // 시도할 횟수만큼 시도한다. 랜덤 값이 4 이상이면 전진한다.
         for (int i = 0; i < number; i++) {
-            carList = handleCar.iteratorCar(carList);
-            resultView.result(carList);
+            cars = handleCar.iteratorCar(cars);
+            resultView.result(cars);
         }
     }
+
+    private static List<Car> makeCar(String carNames) {
+        List<Car> carList = new ArrayList<Car>();
+        String[] cars =  carNames.split(",");
+        for (String car : cars) {
+            carList.add(new Car(car));
+
+        }
+        return carList;
+    }
+
 }
--- src/main/java/car/step3/ResultView.java
+++ src/main/java/car/step3/ResultView.java
@@ -3,12 +3,11 @@ package car.step3;
 import java.util.List;
 
 public class ResultView {
-
     private final String DASH = "-";
     public void result(List<Car> carList) {
         for (Car car : carList) {
             String printCarLocation = DASH.repeat(car.location());
-            System.out.println(printCarLocation);
+            System.out.println(car.name() + " : " + printCarLocation);
         }
     }
 }
