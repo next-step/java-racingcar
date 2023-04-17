de59b1634164395d762f5ef1c2df383e7812f0f3
DeveloperKGH <developerkgh@gmail.com>
2023-04-17 21:07:46 +0900

    :sparkles: feat : 생성한 random 값이 0 ~ 9 사이의 숫자인지 확인

---
+++ src/main/java/racing/NumberUtil.java
@@ -0,0 +1,13 @@
+package racing;
+
+import java.util.Random;
+
+public class NumberUtil {
+
+    public static final Random RANDOM = new Random();
+
+    public static int generateRandomNumberFromZeroToInputNumber(int number) {
+        return RANDOM.nextInt(number);
+    }
+
+}
---
+++ src/test/java/racing/NumberUtilTest.java
@@ -0,0 +1,14 @@
+package racing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumberUtilTest {
+    @DisplayName("3단계 - 자동차 경주 - 생성한 random 값이 0 ~ 9 사이의 숫자인지 확인")
+    @Test
+    void generate_random_number_between_0_to_9() {
+        assertThat(NumberUtil.generateRandomNumberFromZeroToInputNumber(9)).isBetween(0, 9);
+    }
+}
