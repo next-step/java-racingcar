76815b5acb80c3c2729b71f6baa66e2de9832df7
patrickYoo <yhy1045@naver.com>
2023-04-18 01:52:30 +0900

    refactor : Distance 에서 더이상 차량의 이동을 관장하지 않아짐에 따라 해당 테스트 삭제처리

--- src/test/java/com/nextstep/racingcargame/core/DistanceTest.java
+++ src/test/java/com/nextstep/racingcargame/core/DistanceTest.java
@@ -23,7 +23,7 @@ class DistanceTest {
     @DisplayName("차량의 이동거리만큼의 길이를 문자열로 정상 출력한다")
     void distanceFormTest() {
         Distance distance = new Distance(MOVE_FIVE_STEP);
-        assertThat(distance.distanceForm(CAR_DISTANCE_DISPLAY_UNIT))
+        assertThat(CAR_DISTANCE_DISPLAY_UNIT.repeat(distance.distanceIntType()))
                 .isEqualTo("-".repeat(MOVE_FIVE_STEP));
     }
 
@@ -35,19 +35,4 @@ class DistanceTest {
         Assertions.assertThat(fourMovedCar.isLongerThan(moveThreeStep)).isTrue();
     }
 
-    @Test
-    @DisplayName("Random 난수 값이 3 이하인 경우 거리가 변경되지 않는다.")
-    void stopTest() {
-        Distance distance = new Distance(0);
-        distance.moveForward(STOP_NUMBER);
-        assertThat(distance).isEqualTo(new Distance(0));
-    }
-
-    @Test
-    @DisplayName("Random 난수 값이 4 이상인경우 앞으로 한칸 웁직인다")
-    void moveForwardTest() {
-        Distance distance = new Distance(0);
-        distance.moveForward(MOVE_NUMBER);
-        assertThat(distance).isEqualTo(new Distance(1));
-    }
 }
