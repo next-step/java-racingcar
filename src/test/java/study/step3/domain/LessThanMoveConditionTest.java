b69b708953b8c902a2179b9b12aca47a8b6a1608
player-geun <player_geun@naver.com>
2023-04-17 15:51:54 +0900

    fix : isPossible() 메서드 수정에 따른 테스트 변경
    isPossible()가 자체적으로 move를 만들어내기 때문에 랜덤값을 특정할 수 없어, 규칙적인 테스트가 불가능하여 삭제

--- src/test/java/study/step3/domain/LessThanMoveConditionTest.java
+++ src/test/java/study/step3/domain/LessThanMoveConditionTest.java
@@ -8,22 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LessThanMoveConditionTest {
 
-    private LessThanMoveCondition lessThanMoveCondition;
-
-    @BeforeEach
-    void init() {
-        lessThanMoveCondition = new LessThanMoveCondition(4, new RandomMove());
-    }
-
-    @ParameterizedTest(name = "[{index}] {0}라는 숫자가 4이하 인지 : {1}")
-    @CsvSource(value = {"1:false", "5:true"}, delimiter = ':')
-    void isPossible(int distance, boolean expected) {
-        //given
-
-        //when
-        boolean condition = lessThanMoveCondition.isPossible();
-
-        //then
-        assertThat(condition).isEqualTo(expected);
-    }
+//    private LessThanMoveCondition lessThanMoveCondition;
+//
+//    @BeforeEach
+//    void init() {
+//        lessThanMoveCondition = new LessThanMoveCondition(4, new RandomMove());
+//    }
+//
+//    @ParameterizedTest(name = "[{index}] {0}라는 숫자가 4이하 인지 : {1}")
+//    @CsvSource(value = {"1:false", "5:true"}, delimiter = ':')
+//    void isPossible(int distance, boolean expected) {
+//        //given
+//
+//        //when
+//        boolean condition = lessThanMoveCondition.isPossible();
+//
+//        //then
+//        assertThat(condition).isEqualTo(expected);
+//    }
 }
