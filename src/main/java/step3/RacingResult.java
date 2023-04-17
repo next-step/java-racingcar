73ee91f9c397da24df90d03a7584409c38d73cdd
0giri <higiri@naver.com>
2023-04-18 03:24:24 +0900

    fix: CarRacingGame 결과를 위한 클래스 생성

---
+++ src/main/java/step3/RacingResult.java
@@ -0,0 +1,20 @@
+package step3;
+
+import java.util.List;
+
+/**
+ * @author : 0giri
+ * @since : 2023/04/18
+ */
+public class RacingResult {
+
+    private final List<Car> carList;
+
+    public RacingResult(List<Car> carList) {
+        this.carList = carList;
+    }
+
+    public List<Car> carList() {
+        return this.carList;
+    }
+}
