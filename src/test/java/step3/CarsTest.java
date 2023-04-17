59cb0f3916bac47869fd2640bb433385c901ca81
0giri <higiri@naver.com>
2023-04-18 03:21:35 +0900

    test: unmodifiableList 테스트 Cars 테스트로 이동

--- src/test/java/step3/CarsTest.java
+++ src/test/java/step3/CarsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * @author : 0giri
@@ -25,6 +26,29 @@ public class CarsTest {
     @Test
     void 자동차_수_확인() {
         assertThat(cars.count()).isEqualTo(2);
+    }
+
+    @Test
+    void unmodifiableList_변경시_예외() {
+        List<Car> unmodifiableList = cars.unmodifiableList();
+        assertThatThrownBy(() -> unmodifiableList.add(new Car()))
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void unmodifiableList_요소_변경_가능() {
+        List<Car> unmodifiableList = cars.unmodifiableList();
+        assertThat(unmodifiableList.get(0).location()).isEqualTo(0);
+        unmodifiableList.get(0).move(5);
+        assertThat(unmodifiableList.get(0).location()).isEqualTo(5);
+    }
+
+    @Test
+    void unmodifiableList_요소_변경시_원본_리스트_요소도_함께변경() {
+        List<Car> unmodifiableList1 = cars.unmodifiableList();
+        unmodifiableList1.get(0).move(5);
+        List<Car> unmodifiableList2 = cars.unmodifiableList();
+        assertThat(unmodifiableList2.get(0).location()).isEqualTo(unmodifiableList1.get(0).location());
     }
 
     @Test
