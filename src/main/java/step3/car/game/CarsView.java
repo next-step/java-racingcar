41b0666a3d9937ed2d47d8539aa6d522eece9378
lehdqlsl <lehdqlsl@naver.com>
2023-04-17 21:23:26 +0900

    add(CarsView) : 승자 출력

--- src/main/java/step3/car/game/CarsView.java
+++ src/main/java/step3/car/game/CarsView.java
@@ -9,6 +9,7 @@ public class CarsView {
                 .stream()
                 .forEach(car -> print(car));
         System.out.println("");
+
     }
 
     private void print(Car car) {
@@ -16,5 +17,9 @@ public class CarsView {
         IntStream.range(0, car.location())
                 .forEach(value -> System.out.print("-"));
         System.out.println("");
+    }
+
+    public void winner(Cars cars) {
+        System.out.println(cars.winners() + " 가 최종우승했씁니다.");
     }
 }
