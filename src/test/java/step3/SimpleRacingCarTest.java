7870baeeaeaff0ce640cd153d81a2c2a57be8777
jeenstar <jeenstar11@gmail.com>
2023-04-17 21:25:38 +0900

    test: 자동차 생성 및 자동차 랜덤값 발생 테스트 추가

--- src/test/java/step3/SimpleRacingCarTest.java
+++ src/test/java/step3/SimpleRacingCarTest.java
@@ -3,6 +3,9 @@ package step3;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
@@ -13,5 +16,21 @@ class SimpleRacingCarTest {
     void numberInputCheck() {
         assertThatThrownBy(() -> InputView.inputCheck(-1))
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("입력한 자동차 대수와 생성된 자동차 대수 일치")
+    void numberOfCarsCheck() {
+        int inputCarCnt = 3;
+        List<Car> createCarList = SimpleRacingCar.createCar(inputCarCnt,3);
+        assertThat(createCarList.size()).isEqualTo(inputCarCnt);
+    }
+
+    @Test
+    @DisplayName("생성된 자동차의 랜덤값 발생개수와 반복회수 일치")
+    void generateRandomNumCheck() {
+        int inputRepeatCnt = 3;
+        List<Car> createCarList = SimpleRacingCar.createCar(1,inputRepeatCnt);
+        assertThat(createCarList.get(0).carSpeed.length).isEqualTo(inputRepeatCnt);
     }
 }
