b0883366fa6aaa7291fbe67fb34be2739f6e7e9f
hoyeoon <hoyeon.backend@gmail.com>
2023-04-18 00:39:10 +0900

    refactor: 상수명 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,12 +2,12 @@ package racingcar;
 
 public class Car {
 
-    public static final int CAR_NAME_MAX_LENGTH = 5;
+    public static final int NAME_MAX_LENGTH = 5;
     private String name;
     private int moveCount;
 
     public Car(String name, int moveCount) {
-        if(name.length() > CAR_NAME_MAX_LENGTH) {
+        if(name.length() > NAME_MAX_LENGTH) {
             throw new RuntimeException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
         this.name = name;
