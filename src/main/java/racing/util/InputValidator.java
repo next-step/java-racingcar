1ceead21705ea6bab9decd0d51c59d205342578a
DeveloperKGH <developerkgh@gmail.com>
2023-04-19 00:22:58 +0900

    :sparkles: feat : 자동차 이름이 5자를 초과하는 경우 IllegalArgumentException 예외가 발생하는지 확인

--- src/main/java/racing/util/InputValidator.java
+++ src/main/java/racing/util/InputValidator.java
@@ -5,5 +5,9 @@ public class InputValidator {
         if (carName == null || carName.isBlank()) {
             throw new IllegalArgumentException("입력한 값이 null 또는 공백일 수 없습니다.");
         }
+
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("입력한 값이 5글자를 초과할 수 없습니다.");
+        }
     }
 }
