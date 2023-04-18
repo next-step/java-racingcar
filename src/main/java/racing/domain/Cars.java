ba7488fefb61dc9cd189360e797782900341a523
std-freejia <std.freejia@gmail.com>
2023-04-18 22:30:21 +0900

    fix Cars 생성하여 객체 불변 보장하여 반환 하도록 수정

--- src/main/java/racing/domain/Cars.java
+++ src/main/java/racing/domain/Cars.java
@@ -2,6 +2,7 @@ package racing.domain;
 
 import racing.service.RandomGameRule;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cars { // 일급컬렉션
@@ -12,10 +13,6 @@ public class Cars { // 일급컬렉션
         this.cars = cars;
     }
 
-    private void addCar(Car car) {
-        this.cars.add(car);
-    }
-
     public Car theCar(int index) {
         return this.cars.get(index);
     }
@@ -24,14 +21,15 @@ public class Cars { // 일급컬렉션
         return this.cars.size();
     }
     public Cars playOnceTurn() {
-        Cars carsResult = new Cars();
+
+        List<Car> movedCars = new ArrayList<>();
         RandomGameRule randomGameRule = new RandomGameRule();
 
-        for (int i = 0; i < cars.size(); i++) {
+        for (int i = 0; i < this.cars.size(); i++) {
             int randomNumber = randomGameRule.getPossibleInt();
             Car car = this.cars.get(i).move(randomNumber);
-            carsResult.addCar(car);
+            movedCars.add(car);
         }
-        return carsResult;
+        return new Cars(movedCars);
     }
 }
