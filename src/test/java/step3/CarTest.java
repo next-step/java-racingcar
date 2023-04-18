564bccddc4fe51bd60f4d41e0fa9d3bd42f16bd8
dosaem <dosaem90@gmail.com>
2023-04-19 01:47:15 +0900

    fix: CarTest 실패하는 경우 수정

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -8,9 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
-    @DisplayName("random 값이 4이상일 경우에만 전진")
-    @ParameterizedTest
-    @CsvSource(value = {"0:0", "1:0", "2:0", "3:0", "4:1", "3:1", "6:2"}, delimiter = ':')
+    @DisplayName("random 값이 4이상인 경우 전진")
+    @ParameterizedTest(name = "{displayName} {index} = 랜덤값: {0} -> 결과: {1}")
+    @CsvSource(value = {"4:1", "5:1", "6:1", "7:1", "8:1", "9:1"}, delimiter = ':')
+    void test2(int input, int result) {
+        Car car = new Car();
+        assertThat(car.move(input)).isEqualTo(result);
+    }
+
+    @DisplayName("random 값이 4미만인 경우 멈춤")
+    @ParameterizedTest(name = "{displayName} {index} = 랜덤값: {0} -> 결과: {1}")
+    @CsvSource(value = {"0:0", "1:0", "2:0", "3:0"}, delimiter = ':')
     void test1(int input, int result) {
         Car car = new Car();
         assertThat(car.move(input)).isEqualTo(result);
