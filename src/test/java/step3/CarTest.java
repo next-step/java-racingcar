12396c1e03d9ed1eecaff6dc5e42575450cb09d6
dosaem <dosaem90@gmail.com>
2023-04-19 01:51:04 +0900

    refactor: CarTest test케이스 하나로 통합

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -7,18 +7,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
-
-    @DisplayName("random 값이 4이상인 경우 전진")
+    @DisplayName("random 값이 4미만인 경우 멈춤, 4이상인 경우 전진")
     @ParameterizedTest(name = "{displayName} {index} = 랜덤값: {0} -> 결과: {1}")
-    @CsvSource(value = {"4:1", "5:1", "6:1", "7:1", "8:1", "9:1"}, delimiter = ':')
-    void test2(int input, int result) {
-        Car car = new Car();
-        assertThat(car.move(input)).isEqualTo(result);
-    }
-
-    @DisplayName("random 값이 4미만인 경우 멈춤")
-    @ParameterizedTest(name = "{displayName} {index} = 랜덤값: {0} -> 결과: {1}")
-    @CsvSource(value = {"0:0", "1:0", "2:0", "3:0"}, delimiter = ':')
+    @CsvSource(value = {"0:0", "1:0", "2:0", "3:0", "4:1", "5:1", "6:1", "7:1", "8:1", "9:1"}, delimiter = ':')
     void test1(int input, int result) {
         Car car = new Car();
         assertThat(car.move(input)).isEqualTo(result);
