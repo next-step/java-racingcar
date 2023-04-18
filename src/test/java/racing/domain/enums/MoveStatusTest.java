c397d1c2c83f497419d6c3366cfddaecba925bef
DeveloperKGH <developerkgh@gmail.com>
2023-04-18 19:54:49 +0900

    :recycle: refactor : NPE 방지를 위한 MoveStatus null 반환 대신 exception 처리 - 해당 로직 변경에 따른 Unit Test 추가

--- src/test/java/racing/domain/enums/MoveStatusTest.java
+++ src/test/java/racing/domain/enums/MoveStatusTest.java
@@ -2,9 +2,9 @@ package racing.domain.enums;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racing.domain.enums.MoveStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class MoveStatusTest {
     @DisplayName("3단계 - 자동차 경주 - 자동차 전진 조건 테스트")
@@ -19,6 +19,15 @@ public class MoveStatusTest {
     void stay_condition() {
         int randomNumber = 3;
         assertThat(MoveStatus.findByNumber(randomNumber)).isEqualTo(MoveStatus.STAY);
+    }
+
+    @DisplayName("3단계 - 자동차 경주 - 자동차 움직임 조건 범위를 벗어나는 값을 입력하는 경우 RuntimeException 예외가 발생되는지 확인")
+    @Test
+    void pass_invalid_number_for_movable_condition() {
+        int randomNumber = -1;
+        assertThatThrownBy(() -> MoveStatus.findByNumber(randomNumber))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("입력한 숫자가 해당하는 범위의 code 가 없습니다.");
     }
 
 }
