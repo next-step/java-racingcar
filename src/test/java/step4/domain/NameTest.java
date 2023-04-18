e301c4015c4c42eb33dfa2d38b98ba045fc5c006
juhyun <zzang9ha@naver.com>
2023-04-18 22:23:12 +0900

    test: Name 테스트 코드 추가

---
+++ src/test/java/step4/domain/NameTest.java
@@ -0,0 +1,31 @@
+package step4.domain;
+
+import org.junit.jupiter.api.Test;
+import step4.exception.NameExceedException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class NameTest {
+
+    @Test
+    void name의_길이가_5를_초과할경우_예외를_던진다() {
+        assertThrows(
+                NameExceedException.class,
+                () -> new Name("abcdef")
+        );
+    }
+
+    @Test
+    void List의_Name을_출력형식에_맞게_파싱한다() {
+        final List<Name> names = List.of(new Name("pobi"), new Name("crong"));
+        final String expected = "pobi,crong";
+
+        final String actual = Name.parseWinnerNames(names);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
