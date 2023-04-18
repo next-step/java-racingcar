8b129536b99cb388a748d19e2db45f5b93c14ee8
SanghoCho-pixel <shxho0476@gmail.com>
2023-04-18 21:08:03 +0900

    [feat]입력받은 값에 따라 레이싱을 진행하며 결과를 출력하는 method 구현

--- src/main/java/study/step3/CarRacing.java
+++ src/main/java/study/step3/CarRacing.java
@@ -3,6 +3,21 @@ package study.step3;
 import java.util.*;
 
 public class CarRacing {
+
+    private static void racing(int carNum, int tryNum) {
+        String[] positionArray = new String[carNum];
+        Arrays.fill(positionArray, "");
+
+        for (int i = 0; i < tryNum; i++) {
+            for (int j = 0; j < carNum; j++) {
+                if (isSuccess()) {
+                    positionArray[j] += "-";
+                }
+                System.out.println(positionArray[j]);
+            }
+            System.out.println();
+        }
+    }
     private static boolean isSuccess() {
         final Random random = new Random();
         int randomNumber = random.nextInt(10);
