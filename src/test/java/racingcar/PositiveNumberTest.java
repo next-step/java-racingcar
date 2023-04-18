5f459e86931c9a653a99c72f49847e3f02ab2db4
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-18 12:47:40 +0900

    refactor: InputData Class Rename  - InputData -> PositiveNumber

--- src/test/java/racingcar/PositiveNumberTest.java
+++ src/test/java/racingcar/PositiveNumberTest.java
@@ -4,16 +4,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.domain.InputData;
+import racingcar.domain.PositiveNumber;
 
 @DisplayName("자동차 객체 테스트")
-public class InputDataTest {
+public class PositiveNumberTest {
 
     @DisplayName("자동차 대수는 양수만 입력가능하다")
     @ParameterizedTest
     @ValueSource(ints = {0, -4})
     void validateCarParticipationNumber(Integer number) {
-        Assertions.assertThatThrownBy(() -> new InputData(number, 3))
+        Assertions.assertThatThrownBy(() -> new PositiveNumber(number, 3))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 대수는 양수만 입력가능합니다");
     }
@@ -22,7 +22,7 @@ public class InputDataTest {
     @ParameterizedTest
     @ValueSource(ints = {0, -9})
     void validateCarRepetitionCount(int count) {
-        Assertions.assertThatThrownBy(() -> new InputData(4, count))
+        Assertions.assertThatThrownBy(() -> new PositiveNumber(4, count))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 시도 횟수는 양수만 입력 가능합니다");
     }
