be5ca335c480746ef35f0da07ff39cfb41ad0df3
KimWooHun <dngjs1@naver.com>
2023-04-18 16:42:33 +0900

    feat : 자동차들의 이름 입력받는 기능 구현(5자 이하, 쉼표(,)로 구분

--- src/main/java/carracing/controller/CarRacingGame.java
+++ src/main/java/carracing/controller/CarRacingGame.java
@@ -13,22 +13,9 @@ public class CarRacingGame {
     public static void main(String[] args) {
         //자동차 대수 및 이동 횟수 입력
         InputView inputView = new InputView();
-        int carNum = inputView.inputCarNum();
+        //자동차 이름 입력
+        String carNames = inputView.inputCarName();
+
         int moveNum = inputView.inputMoveNum();
-
-        //입력 대수만큼 자동차 객체 생성
-        CarCompany carCompany = new CarCompany();
-        List<Car> cars = carCompany.createCar(carNum);
-
-        ResultView resultView = new ResultView();
-        //실행결과 텍스트 출력
-        resultView.resultStart();
-
-        for (int i = 0; i < moveNum; i++) {
-            //자동차 이동
-            cars = moveCars(cars);
-            //출력
-            resultView.outputResult(cars);
-        }
     }
 }
--- src/main/java/carracing/view/InputView.java
+++ src/main/java/carracing/view/InputView.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public int inputCarNum() {
-        System.out.println("경주에 참여할 자동차 수를 입력하세요.");
-        return scanner.nextInt();
+    public String inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)");
+        return scanner.nextLine();
     }
 
     public int inputMoveNum() {
