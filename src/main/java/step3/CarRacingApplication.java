dd3e28336d167ebd9a6a190a9c436aa77bd1e256
dosaem <dosaem90@gmail.com>
2023-04-18 16:10:06 +0900

    feat: 사용자로 부터 자동차 대수, 시도할 횟수 입력받기

---
+++ src/main/java/step3/CarRacingApplication.java
@@ -0,0 +1,10 @@
+package step3;
+
+public class CarRacingApplication {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        inputView.carCount();
+        inputView.tryCount();
+        System.out.println("\n");
+    }
+}
