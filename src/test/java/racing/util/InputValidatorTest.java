9acdf3304bdf01a90b7b285f523f9bd0cf92c669
DeveloperKGH <developerkgh@gmail.com>
2023-04-19 00:40:00 +0900

    :memo: docs : InputValidatorTest.java DisplayName 오탈자 수정

--- src/test/java/racing/util/InputValidatorTest.java
+++ src/test/java/racing/util/InputValidatorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class InputValidatorTest {
-    @DisplayName("3단계 - 자동차 경주 - 자동차 이름에 null 을 입력한 경우 IllegalArgumentException 예외가 발생하는지 확인")
+    @DisplayName("4단계 - 자동차 경주(우승자) - 자동차 이름에 null 을 입력한 경우 IllegalArgumentException 예외가 발생하는지 확인")
     @Test
     void pass_null_name_value_illegal_argument_exception() {
         assertThatThrownBy(() -> InputValidator.validateCarName(null))
@@ -14,7 +14,7 @@ public class InputValidatorTest {
                 .hasMessageContaining("입력한 값이 null 또는 공백일 수 없습니다.");
     }
 
-    @DisplayName("3단계 - 자동차 경주 - 자동차 이름에 공백 을 입력한 경우 IllegalArgumentException 예외가 발생하는지 확인")
+    @DisplayName("4단계 - 자동차 경주(우승자) - 자동차 이름에 공백 을 입력한 경우 IllegalArgumentException 예외가 발생하는지 확인")
     @Test
     void pass_blank_name_value_illegal_argument_exception() {
         assertThatThrownBy(() -> InputValidator.validateCarName(""))
