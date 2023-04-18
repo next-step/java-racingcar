c4ddfcb0d6e9f8b3fa41e974e0cf8f6cc3d2ddb5
lia.s2 <lia.s2@kakaocorp.com>
2023-04-18 20:50:18 +0900

    [step3] 전략 수정

--- src/main/java/racing/strategy/MoveNumberStrategy.java
+++ src/main/java/racing/strategy/MoveNumberStrategy.java
@@ -1,14 +1,8 @@
 package racing.strategy;
 
-import java.util.Random;
-
 public class MoveNumberStrategy implements NumberGeneratorStrategy {
 
-  private static final Random RANDOM = new Random();
-
-  private static final int NUMBER_FOR_MOVE = MAX_FOR_MOVE - MIN_FOR_MOVE + 1;
-
   public int getNumber() {
-    return RANDOM.nextInt(NUMBER_FOR_MOVE) + MIN_FOR_MOVE;
+    return MAX_FOR_MOVE;
   }
 }
--- src/main/java/racing/strategy/StopNumberStrategy.java
+++ src/main/java/racing/strategy/StopNumberStrategy.java
@@ -1,12 +1,8 @@
 package racing.strategy;
 
-import java.util.Random;
-
 public class StopNumberStrategy implements NumberGeneratorStrategy {
 
-  private static final Random RANDOM = new Random();
-
   public int getNumber() {
-    return RANDOM.nextInt(MIN_FOR_MOVE);
+    return MIN_FOR_MOVE - 1;
   }
 }
