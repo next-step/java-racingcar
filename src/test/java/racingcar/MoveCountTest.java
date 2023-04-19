a0a9d48f910ad5c739a5160b7e3af0228c3a305c
hoyeoon <hoyeon.backend@gmail.com>
2023-04-20 01:41:15 +0900

    test: moveCount, maxMoveCount 비교 메서드 (>)

--- src/test/java/racingcar/MoveCountTest.java
+++ src/test/java/racingcar/MoveCountTest.java
@@ -9,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MoveCountTest {
 
     public static final int MOVE_COUNT = 1;
+    public static final int MAX_MOVE_COUNT = 2;
 
     @DisplayName("문자열 포장 객체 MoveCount 생성 확인")
     @Test
@@ -21,5 +22,19 @@ public class MoveCountTest {
 
         //then
         assertThat(moveCount).isEqualTo(MOVE_COUNT);
+    }
+
+    @DisplayName("moveCount, maxMoveCount 비교 메서드 (>)")
+    @Test
+    void isGreaterThan() {
+        //given
+        MoveCount myMoveCount = new MoveCount(MOVE_COUNT);
+
+
+        //when
+        boolean isGreater = myMoveCount.isGreaterThan(MAX_MOVE_COUNT);
+
+        //then
+        assertThat(isGreater).isFalse();
     }
 }
