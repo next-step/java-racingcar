2198c447332bd244bf7f4612d275508b6429959e
hovy <hovy1994@naver.com>
2023-04-20 01:42:17 +0900

    test: 경주가 끝난 후 자동차 위치에 대한 테스트 추가

--- src/test/java/step3/RaceTest.java
+++ src/test/java/step3/RaceTest.java
@@ -2,26 +2,36 @@ package step3;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import step3.domain.Car;
-import step3.domain.Race;
-import step3.domain.RaceRound;
-import step3.domain.RacingCars;
+import step3.domain.*;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RaceTest {
+
     @Test
-    @DisplayName("경주가 끝난 자동차의 위치는 round 수 초과이거나 음수일 수 없다")
+    @DisplayName("경주가 끝난 자동차의 위치는 round 수 초과일 수 없다")
     void testCarCanNotMoveBeyondRound() {
         RaceRound raceRound = RaceRound.of(5);
-        Race race = new Race(getMockRacingCars(), raceRound);
+        Race race = new Race(getMockRacingCars(), raceRound, ()->true);
 
         race.start();
 
         for (Car car : race.getCars()) {
             assertThat(car.getPositionValue()).isLessThanOrEqualTo(raceRound.getValue());
+        }
+    }
+
+    @Test
+    @DisplayName("경주가 끝난 자동차의 위치는 음수일 수 없다")
+    void testFinishPositionCannotBeNegative() {
+        RaceRound raceRound = RaceRound.of(5);
+        Race race = new Race(getMockRacingCars(), raceRound, ()->false);
+
+        race.start();
+
+        for (Car car : race.getCars()) {
             assertThat(car.getPositionValue()).isNotNegative();
         }
     }
