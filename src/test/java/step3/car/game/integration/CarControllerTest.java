b427b4517714f541305c6e771d6ea9ea5783277a
lehdqlsl <lehdqlsl@naver.com>
2023-04-19 12:15:20 +0900

    fix(CarController) : 통합테스트 수정

--- src/test/java/step3/car/game/integration/CarControllerTest.java
+++ src/test/java/step3/car/game/integration/CarControllerTest.java
@@ -3,12 +3,18 @@ package step3.car.game.integration;
 import org.junit.jupiter.api.Test;
 import step3.controller.CarController;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
 public class CarControllerTest {
 
 
     @Test
     void 자동차_게임_통합테스트() {
-        CarController carController = new CarController(10,20);
-        carController.start();
+        String[] carNames = new String[]{"benz","bmw","audi"};
+
+        assertThatCode(() -> {
+            CarController carController = new CarController(carNames,20);
+            carController.start();
+        }).doesNotThrowAnyException();
     }
 }
