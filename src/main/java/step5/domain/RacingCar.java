531c9b1ead58f2872164acfccefa09ae47b7e0a3
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-19 18:40:15 +0900

    [코드리뷰 반영] 개행을 통해 가독성을 높인다

--- src/main/java/step5/domain/RacingCar.java
+++ src/main/java/step5/domain/RacingCar.java
@@ -3,7 +3,7 @@ package step5.domain;
 import step5.strategy.move.MoveStrategy;
 
 public class RacingCar {
-
+    private final static int THRESHOLD = 5;
     private final String name;
     private int currentLocation;
 
@@ -12,19 +12,19 @@ public class RacingCar {
     }
 
     public static RacingCar of(String name) {
-        hasCarName(name);
-        hasLessThen5Name(name);
+        validateEmpty(name);
+        validateLessThen5Name(name);
         return new RacingCar(name);
     }
 
-    private static void hasCarName(String name) {
+    private static void validateEmpty(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("자동차 이름을 입력해주세요");
         }
     }
 
-    private static void hasLessThen5Name(String name){
-        if(name.length() > 5){
+    private static void validateLessThen5Name(String name){
+        if(name.length() > THRESHOLD){
             throw new IllegalArgumentException("5자를 초과할수 없습니다");
 
         }
