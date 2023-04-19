870f1b88fc78671bf96ea7a0e59c60696ca9da9f
DeveloperKGH <developerkgh@gmail.com>
2023-04-19 18:19:15 +0900

    :recycle: refactor : RacingCarsTest.java 에서 각 테스트마다 존재하는 자동차 객체 생성관련 로직 @BeforeEach 를 활용하여 중복 제거

--- src/test/java/racing/domain/RacingCarsTest.java
+++ src/test/java/racing/domain/RacingCarsTest.java
@@ -1,5 +1,6 @@
 package racing.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,20 +11,25 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarsTest {
-    private final String DEFAULT_CAR_NAME = "kgh";
+    private Car car1;
+    private Car car2;
+
+    @BeforeEach
+    public void beforeEach() {
+        car1 = new Car("car2");
+        car2 = new Car("car2");
+    }
 
     @DisplayName("3단계 - 자동차 경주 - 레이싱 자동차 객체가 생성되는지 확인")
     @Test
     void create_racing_cars() {
-        assertThat(RacingCars.create(Collections.singletonList(new Car(DEFAULT_CAR_NAME)))).isNotNull();
+        assertThat(RacingCars.create(Collections.singletonList(car1))).isNotNull();
     }
 
     @DisplayName("4단계 - 자동차 경주(우승자) - 레이싱 자동차 컬렉션에서 가장 멀리 간 position 값 을 반환하는지 확인")
     @Test
     void get_farthest_position() {
         //given
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
         car1.go();
         RacingCars racingCars = RacingCars.create(Arrays.asList(car1, car2));
 
@@ -38,8 +44,6 @@ public class RacingCarsTest {
     @Test
     void get_winner_cars() {
         //given
-        Car car1 = new Car("car1");
-        Car car2 = new Car("car2");
         car1.go();
         RacingCars racingCars = RacingCars.create(Arrays.asList(car1, car2));
 
