bacc0e3cb6861a605fa4857fe658c7c987368273
hoyeoon <hoyeon.backend@gmail.com>
2023-04-20 01:31:46 +0900

    feat: 문자열 포장 객체 MoveCount 생성 확인

---
+++ src/main/java/racingcar/domain/MoveCount.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class MoveCount {
+
+    private int moveCount;
+
+    public MoveCount(int moveCount) {
+        this.moveCount = moveCount;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+}
