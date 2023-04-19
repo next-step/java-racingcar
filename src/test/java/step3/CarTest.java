b23fce61a1b997e8ddbc29aaab816b3b0a4538f5
issac.park <issac.park@kakaocorp.com>
2023-04-20 00:05:37 +0900

    refactor: CarTest 멤버변수 접근제어자 - private 접근제어자 추가

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
-    SimpleCar car;
+    private SimpleCar car;
 
     @BeforeEach
     void init() {
