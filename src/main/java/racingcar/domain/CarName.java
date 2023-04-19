ed492383479dd2de7c43848fa368d9672cb51bcd
hoyeoon <hoyeon.backend@gmail.com>
2023-04-20 00:57:20 +0900

    feat: 문자열 포장 객체 CarName 생성 예외 처리(길이가 5를 초과 할 경우)

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -2,9 +2,14 @@ package racingcar.domain;
 
 public class CarName {
 
-    private String name;
+    public static final int MAX_LENGTH = 5;
+
+    private final String name;
 
     public CarName(String name) {
+        if(name.length() > MAX_LENGTH) {
+            throw new RuntimeException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
         this.name = name;
     }
 
