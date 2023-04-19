03bf6f10cade215e488aa38b170fb83251ff5ed1
DeveloperKGH <developerkgh@gmail.com>
2023-04-19 18:09:34 +0900

    :recycle: refactor : CarTest.java 에서 각 테스트마다 존재하는 자동차 객체 생성관련 로직 @BeforeEach 를 활용하여 중복 제거

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racing.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,17 +9,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarTest {
     private final String DEFAULT_CAR_NAME = "kgh";
 
+    private Car car;
+
+    @BeforeEach
+    public void beforeEach() {
+        car = new Car(DEFAULT_CAR_NAME);
+    }
+
     @DisplayName("3단계 - 자동차 경주 - 자동차 객체가 생성되는지 확인")
     @Test
     void create_car() {
-        Car car = new Car(DEFAULT_CAR_NAME);
         assertThat(car.getPosition()).isEqualTo(0);
     }
 
     @DisplayName("3단계 - 자동차 경주 - 자동차 전진 이동 테스트")
     @Test
     void go_car() {
-        Car car = new Car(DEFAULT_CAR_NAME);
         car.go();
         assertThat(car.getPosition()).isEqualTo(1);
     }
@@ -26,7 +32,6 @@ public class CarTest {
     @DisplayName("4단계 - 자동차 경주(우승자) - 자동차 객체 생성시 이름을 부여할 수 있는지 확인")
     @Test
     void create_car_with_name() {
-        Car car = new Car(DEFAULT_CAR_NAME);
         assertThat(car.getName()).isEqualTo(DEFAULT_CAR_NAME);
     }
 
