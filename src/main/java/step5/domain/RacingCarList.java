725937706eaa58b7471a2aea7626891017ae1746
Lee Byoung Seong <dev.bslee@gmail.com>
2023-04-19 17:30:25 +0900

    [코드리뷰 반영] validator메소드 애매하게 네이밍을 정함 접두사 has -> validator

---
+++ src/main/java/step5/domain/RacingCarList.java
@@ -0,0 +1,82 @@
+package step5.domain;
+
+import step5.strategy.move.MoveStrategy;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+public class RacingCarList {
+
+    private final List<RacingCar> racingCarList;
+
+    private RacingCarList(List<RacingCar> racingCarList) {
+        this.racingCarList = racingCarList;
+    }
+
+    public static RacingCarList of(String inputCarName) {
+        List<RacingCar> racingCarList = makeRacingCarList(inputCarName);
+        return new RacingCarList(racingCarList);
+    }
+
+    private static List<RacingCar> makeRacingCarList(String inputCarName) {
+        validateEmpty(inputCarName);
+        List<String> carNameList = getCarNameList(inputCarName);
+        validateDuplicate(carNameList);
+        validateHasLength(carNameList);
+
+        return carNameList.stream()
+                .map(RacingCar::of)
+                .collect(toUnmodifiableList());
+    }
+
+    private static List<String> getCarNameList(String inputCarName) {
+        return Arrays.asList(inputCarName.split(Separator.SPLIT_CAR_NAME_SEPARATOR.getSeparator()));
+    }
+
+    private static void validateEmpty(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요");
+        }
+    }
+
+    private static void validateDuplicate(List<String> carNameList) {
+        Set<String> carNameSet = new HashSet<>(carNameList);
+
+        if (carNameSet.size() != carNameList.size()) {
+            throw new IllegalArgumentException("같은 이름이 있습니다");
+        }
+    }
+
+    private static void validateHasLength(List<String> carNameList){
+        if(carNameList.isEmpty()){
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요");
+        }
+    }
+
+    public void move(MoveStrategy moveStrategy){
+        racingCarList.forEach(racingCar -> racingCar.move(moveStrategy));
+    }
+
+    public List<String> getWinner(){
+        int winnerLocation = getWinnerLocation();
+
+        return racingCarList.stream()
+                .filter(racingCar -> racingCar.getCurrentLocation() == winnerLocation)
+                .map(RacingCar::getName)
+                .collect(toUnmodifiableList());
+
+    }
+
+    public int getWinnerLocation() {
+        return racingCarList.stream()
+                .mapToInt(RacingCar::getCurrentLocation)
+                .max()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public List<RacingCar> getRacingCarList(){
+        return racingCarList;
+    }
+}
