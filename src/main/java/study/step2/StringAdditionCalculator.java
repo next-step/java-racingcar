aafc14d526abcd400894bb44785d8d5faa5b16b9
conagreen <verycona@gmail.com>
2023-04-20 00:26:30 +0900

    [step2] 2단계 - step 별로 패키지 나누기

--- src/main/java/study/step2/StringAdditionCalculator.java
+++ src/main/java/study/step2/StringAdditionCalculator.java
@@ -1,4 +1,4 @@
-package study;
+package study.step2;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
--- src/test/java/study/step2/StringAdditionCalculatorTest.java
+++ src/test/java/study/step2/StringAdditionCalculatorTest.java
@@ -1,4 +1,4 @@
-package study;
+package study.step2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
