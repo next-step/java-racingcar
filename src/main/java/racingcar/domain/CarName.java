096ac4318eeeefa0aa1f5587ffd11d9956d756bf
hoyeoon <hoyeon.backend@gmail.com>
2023-04-20 00:48:23 +0900

    feat: 문자열 포장 객체 CarName 생성 확인

---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class CarName {
+
+    private String name;
+
+    public CarName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
