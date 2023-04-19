3645039869bc3a5032c0b6e4e2e567b398803181
hoyeoon <hoyeon.backend@gmail.com>
2023-04-20 00:53:42 +0900

    test: 문자열 포장 객체 CarName 생성 예외 처리(길이가 5를 초과 할 경우)

--- src/test/java/racingcar/CarNameTest.java
+++ src/test/java/racingcar/CarNameTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import racingcar.domain.CarName;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarNameTest {
 
     @DisplayName("문자열 포장 객체 CarName 생성 확인")
     @Test
-    void createCarName() throws Exception {
+    void constructCarName() {
         //given
         String carName = "oyeon";
         CarName oyeon = new CarName(carName);
@@ -20,5 +21,14 @@ public class CarNameTest {
 
         //then
         assertThat(name).isEqualTo(carName);
+    }
+
+    @DisplayName("문자열 포장 객체 CarName 생성 예외 처리(길이가 5를 초과 할 경우)")
+    @Test
+    void constructCarNameException() {
+        assertThatThrownBy(() -> {
+            new CarName("hoyeon");
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("자동차 이름은 5자를 초과할 수 없습니다.");
     }
 }
