98e3674ae2eb26fa9c0e7e8365ebf6335574433d
hanseo-jo <hanseo.jo@navercorp.com>
2023-04-19 19:39:05 +0900

    test: add tests

--- src/test/java/carrace/CarFactoryTest.java
+++ src/test/java/carrace/CarFactoryTest.java
@@ -1,4 +1,15 @@
-import static org.junit.jupiter.api.Assertions.*;
+package carrace;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class CarFactoryTest {
-  
+
+    static final int NUMBER_OF_CARS = 5;
+
+    @Test
+    void createCars_test() {
+        assertThat(CarFactory.createCars(NUMBER_OF_CARS).size()).isEqualTo(NUMBER_OF_CARS);
+    }
 }
--- src/test/java/carrace/CarTest.java
+++ src/test/java/carrace/CarTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+package carrace;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class CarTest {
-  
+    Car car;
+
+    @BeforeEach
+    void setUp() {
+        this.car = new Car();
+    }
+
+    @Test
+    void carPosition_test() {
+        assertThat(car.getRecentRoundPosition()).isEqualTo(0);
+
+        car.setNewRoundPosition(5);
+        assertThat(car.getRecentRoundPosition()).isEqualTo(5);
+
+        assertThat(car.getPositionAtRound(0)).isEqualTo(5);
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    car.getPositionAtRound(10);
+                });
+    }
 }
