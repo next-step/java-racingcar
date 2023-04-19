2b32d65c20618c5e10b8eb98c0cbdf5c48ba1a36
bahar-j <joonie811@naver.com>
2023-04-19 21:36:38 +0900

    refactor: 이동 횟수를 카운트하는 클래스 생성

--- src/test/java/racing/RacingCarGameTest.java
+++ src/test/java/racing/RacingCarGameTest.java
@@ -2,24 +2,16 @@ package racing;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarGameTest {
 
     @Test
     void 각_자동차는_주어진_횟수만큼_이동한다() {
-        int carCount = 5;
-        int moveCount = 3;
-        Cars cars = new Cars(List.of("a", "b", "c", "d", "e"), moveCount);
-        RacingCarGame.progress(cars);
-
-        // 실제로 모든 차가 주어진 횟수만큼 이동했느지 판단할 수 있는 방법....?
-        assertThat(cars.values().size()).isEqualTo(carCount);
-        cars.values().forEach(car -> {
-            assertThat(car.position()).isBetween(0, moveCount);
-        });
+        CountingCar car = new CountingCar("june");
+        car.move(3);
+        car.move(7);
+        assertThat(car.getMoveCount()).isEqualTo(2);
     }
 
 }
