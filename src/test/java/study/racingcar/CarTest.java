038532f2086e63a9397d17e80558c8b2254a33c5
jaewon <jaewon9937@gmail.com>
2023-04-19 23:21:23 +0900

    refactor(RacingCar) : 자동차 경추 리팩토링(5단계_피드백)
    - 미작성된 테스트 코드 추가

--- src/test/java/study/racingcar/CarTest.java
+++ src/test/java/study/racingcar/CarTest.java
@@ -1,7 +1,9 @@
 package study.racingcar;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import study.racingcar.domain.Car;
 import study.racingcar.strategy.CarMoveStrategy;
@@ -16,16 +18,16 @@ public class CarTest {
 
     @DisplayName("숫자를 전달하여 자동차가 정상적으로 움직였는지 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {3, 6, 5})
+    @ValueSource(ints = {4, 6, 5})
     public void Car_move(int games) {
 
-        Car car = new Car(0, "pobbi", new CarMoveStrategy(new RandomNumber()));
+        Car car = new Car(0, "pobbi", new CarMoveStrategy(bound -> games));
 
         for(int i = 0; i < games; i++) {
             car.move();
         }
 
-        assertThat(car.currentPosition()).isBetween(0, games);
+        assertThat(car.currentPosition()).isEqualTo(games);
     }
 
     @DisplayName("자동차 이름은 5글자를 넘을 수 없다.")
@@ -37,4 +39,21 @@ public class CarTest {
                 .isInstanceOf(InputMismatchException.class);
     }
 
+    @DisplayName("자동차의 현재 위치와 최고위치가 같은 경우 TRUE 를 반환한다.")
+    @Test
+    public void Car_isPosition_TRUE_테스트() {
+
+        Car car = new Car(5, "pobbi", new CarMoveStrategy(new RandomNumber()));
+
+        assertThat(car.isPosition(5)).isEqualTo(true);
+    }
+
+    @DisplayName("자동차의 현재 위치와 최고위치가 다른 경우 FALSE 를 반환한다.")
+    @Test
+    public void Car_isPosition_FALSE_테스트() {
+
+        Car car = new Car(5, "pobbi", new CarMoveStrategy(new RandomNumber()));
+
+        assertThat(car.isPosition(10)).isEqualTo(false);
+    }
 }
--- src/test/java/study/racingcar/RacingGameTest.java
+++ src/test/java/study/racingcar/RacingGameTest.java
@@ -3,6 +3,7 @@ package study.racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import study.racingcar.domain.Car;
 import study.racingcar.domain.RacingGame;
 import study.racingcar.domain.RacingGameInputs;
 
@@ -22,5 +23,22 @@ public class RacingGameTest {
         }
 
         assertThat(racingGame.isOver()).isTrue();
+    }
+
+    @DisplayName("게임 횟수만큼 움직이고 현재 위치가 포함되는지 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {5,3,4})
+    public void RacingGame_move(int games) {
+
+        RacingGame racingGame = new RacingGame(new RacingGameInputs(games, "pobbi,crong,honux"));
+
+        while (!racingGame.isOver()) {
+            racingGame.moveCar();
+            racingGame.nextLap();
+        }
+
+        assertThat(racingGame.getCars())
+                .extracting(Car::currentPosition)
+                .hasSizeBetween(0, games);
     }
 }
