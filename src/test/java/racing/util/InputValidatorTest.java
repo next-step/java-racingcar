5feea71204dbf84153945879aea2f9051da822c9
DeveloperKGH <developerkgh@gmail.com>
2023-04-19 18:31:49 +0900

    :recycle: refactor : InputValidatorTest.java 자동차 이름에 null 또는 공백 관련 테스트 ParameterizedTest, NullAndEmptySource 를 활용하여 하나로 통합

--- src/test/java/racing/util/InputValidatorTest.java
+++ src/test/java/racing/util/InputValidatorTest.java
@@ -2,23 +2,18 @@ package racing.util;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class InputValidatorTest {
-    @DisplayName("4단계 - 자동차 경주(우승자) - 자동차 이름에 null 을 입력한 경우 IllegalArgumentException 예외가 발생하는지 확인")
-    @Test
-    void pass_null_name_value_illegal_argument_exception() {
-        assertThatThrownBy(() -> InputValidator.validateCarName(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("입력한 값이 null 또는 공백일 수 없습니다.");
-    }
-
-    @DisplayName("4단계 - 자동차 경주(우승자) - 자동차 이름에 공백 을 입력한 경우 IllegalArgumentException 예외가 발생하는지 확인")
-    @Test
-    void pass_blank_name_value_illegal_argument_exception() {
-        assertThatThrownBy(() -> InputValidator.validateCarName(""))
+    @DisplayName("4단계 - 자동차 경주(합우승자) - 자동차 이름에 null 또는 공백을 입력한 경우 IllegalArgumentException 예외가 발생하는지 확인")
+    @ParameterizedTest(name = "testValue : {0}")
+    @NullAndEmptySource
+    void pass_null_or_blank_name_value_illegal_argument_exception(String name) {
+        assertThatThrownBy(() -> InputValidator.validateCarName(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력한 값이 null 또는 공백일 수 없습니다.");
     }
