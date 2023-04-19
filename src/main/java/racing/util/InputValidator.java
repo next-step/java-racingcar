e9af59181f40e1c499bc1e7b2bbcbef31983a234
DeveloperKGH <developerkgh@gmail.com>
2023-04-19 18:03:24 +0900

    :recycle: refactor : InputValidator.java 에서 자동차 이름 글자 제한숫자에 관련한 매직넘버 상수화

--- src/main/java/racing/util/InputValidator.java
+++ src/main/java/racing/util/InputValidator.java
@@ -1,12 +1,14 @@
 package racing.util;
 
 public class InputValidator {
+    private static final int MAX_CAR_NAME_LETTERS = 5;
+
     public static String validateCarName(String carName) {
         if (carName == null || carName.isBlank()) {
             throw new IllegalArgumentException("입력한 값이 null 또는 공백일 수 없습니다.");
         }
 
-        if (carName.length() > 5) {
+        if (carName.length() > MAX_CAR_NAME_LETTERS) {
             throw new IllegalArgumentException("입력한 값이 5글자를 초과할 수 없습니다.");
         }
 
