df5f0294e675e998a21c178b912d325b266bc723
MJ_Aus <alswjd7010@gmail.com>
2023-04-20 21:51:01 +0900

    * 랜덤값을 생성

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -3,8 +3,16 @@ package step3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class RacingCar {
+
+    public static final int RANDOM_LIMIT = 10;
+    private int randomValue() {
+        Random random = new Random();
+        int value = random.nextInt(RANDOM_LIMIT);
+        return value;
+    }
 
     List<Car> initCars(int car){
         List<Car> cars = new ArrayList<>();
