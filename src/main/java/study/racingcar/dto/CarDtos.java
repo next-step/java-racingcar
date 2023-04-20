f732c5e876ca82374d772f92097d52e0966476c1
jaewon0913 <jaewon9937@gmail.com>
2023-04-20 17:03:57 +0900

    refactor(RacingCar) : 자동차 경추 리팩토링(5단계_피드백)
    - CarDto List를 가지는 클래스 생성

---
+++ src/main/java/study/racingcar/dto/CarDtos.java
@@ -0,0 +1,16 @@
+package study.racingcar.dto;
+
+import java.util.List;
+
+public class CarDtos {
+
+    private List<CarDto> carDtos;
+
+    public CarDtos(List<CarDto> carDtos) {
+        this.carDtos = carDtos;
+    }
+
+    public List<CarDto> getCarDtos() {
+        return carDtos;
+    }
+}
