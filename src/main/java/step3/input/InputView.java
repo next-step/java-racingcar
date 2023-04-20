5e727a80d59a88df310800e15dab90488a8fb5fd
MJ_Aus <alswjd7010@gmail.com>
2023-04-20 21:36:13 +0900

    입력 class 구현

--- src/main/java/step3/input/InputView.java
+++ src/main/java/step3/input/InputView.java
@@ -1,4 +1,22 @@
 package step3.input;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class InputView {
+
+    public static int input(String text) {
+
+        int inputValue = 0;
+        Scanner scanner = new Scanner(System.in);
+        try{
+            System.out.println(text);
+            inputValue = scanner.nextInt();;
+        }catch (InputMismatchException e){
+            System.out.println("숫자 외에는 입력할 수 없습니다.");
+            inputValue = input(text);
+        }
+        return inputValue;
+
+    }
 }
