958fc089a9853bb8d2f3be78cf0acfe75d9cc062
Hwang Sangwook <goodbyeyo@naver.com>
2023-04-20 16:08:50 +0900

    refactor: Score, Scores 리팩토링  - Score 자동차 이름 필드 추가  - 우승자, 우승자 이름, 우승포인트 찾기 메서드 구현

--- src/main/java/racingcar/domain/Score.java
+++ src/main/java/racingcar/domain/Score.java
@@ -2,12 +2,18 @@ package racingcar.domain;
 
 public class Score {
     private final int point;
+    private final String carName;
 
-    public Score(int point) {
-        this.point = point;
+    public Score(Car car) {
+        this.point = car.findCarPoint();
+        this.carName = car.findCarName();
     }
 
     public int findPoint() {
         return point;
+    }
+
+    public String findCarName() {
+        return carName;
     }
 }
