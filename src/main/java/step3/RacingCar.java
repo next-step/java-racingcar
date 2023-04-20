58bc51d26ca35d19e144ccdd355514023f9474ad
MJ_Aus <alswjd7010@gmail.com>
2023-04-20 22:11:01 +0900

    레이스 출력

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -9,6 +9,24 @@ public class RacingCar {
 
     public static final int RANDOM_LIMIT = 10;
     public static final int LIMIT = 4;
+    public static final String MARK = "-";
+
+    public void races(List<Car> cars, int count){
+        for(int i=0; i<count; i++){
+            for (Car car : cars) {
+                boolean goStop = isValueMoreThan(randomValue());
+                car.goOrStop(goStop);
+            }
+            for (Car car : cars) {
+                for(int j = 0; j< car.getGoCount(); j++){
+                    System.out.print(MARK);
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+        }
+    }
 
     public boolean isValueMoreThan(int value) {
         return value >= LIMIT;
@@ -20,7 +38,7 @@ public class RacingCar {
         return value;
     }
 
-    List<Car> initCars(int car){
+    static List<Car> initCars(int car){
         List<Car> cars = new ArrayList<>();
         for(int i=0; i<car; i++){
             cars.add(new Car());
---
+++ src/main/java/step3/RacingCarMain.java
@@ -0,0 +1,18 @@
+package step3;
+
+
+import java.util.List;
+
+import static step3.RacingCar.*;
+import static step3.input.InputView.input;
+
+public class RacingCarMain {
+    public static void main(String[] args) {
+        RacingCar rc = new RacingCar();
+        int car = input("자동차 대수는 몇 대 인가요?");
+        int count = input("시도할 회수는 몇 회 인가요?");
+
+        List<Car> cars = initCars(car);
+        rc.races(cars, count);
+    }
+}
