eec3b169be3657cc61bfa658ba18a05ade06cc18
std-freejia <std.freejia@gmail.com>
2023-04-21 17:06:27 +0900

    feat 자동차에 이름을 부여한다. 이름은 5자를 초과할 수 없다.

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -2,17 +2,33 @@ package racing.domain;
 
 public class Car {
     private int location;
+    private String name;
+
+    private static final int NAME_MAX_LENGTH = 5;
 
     public Car(int location) {
         this.location = location;
     }
+    public Car(int location, String name) {
+        this.location = location;
+        this.name = nameValidation(name);
+    }
+
+    public String nameValidation(String input) {
+        if (input.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
+        return input;
+    }
 
     public Car move(int distance) {
         this.location += distance;
-        return new Car(this.location);
+        return new Car(this.location, this.name);
     }
 
     public int location() {
         return this.location;
     }
+
+    public String showName() { return this.name; }
 }
