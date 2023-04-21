115306d7164f463b36c96f0713846469b8299029
std-freejia <std.freejia@gmail.com>
2023-04-21 17:06:27 +0900

    feat 자동차에 이름을 부여한다. 이름은 5자를 초과할 수 없다.

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -2,18 +2,13 @@ package racing.domain;
 
 public class Car {
     private int location;
-<<<<<<< HEAD
     private String name;
 
     private static final int NAME_MAX_LENGTH = 5;
-=======
->>>>>>> e356fab2c (refactor Car 클래스를 필드로 가지는 일급컬렉션 추가)
 
     public Car(int location) {
         this.location = location;
     }
-
-<<<<<<< HEAD
     public Car(int location, String name) {
         this.location = location;
         this.name = nameValidation(name);
@@ -31,31 +26,9 @@ public class Car {
         return new Car(this.location, this.name);
     }
 
-    public int bigger(int input) {
-        if (input > this.location) return input;
-        return this.location;
-    }
-
-    public boolean isWinner(int winnerLocation) {
-        if (this.location >= winnerLocation) return true;
-        return false;
-    }
-
     public int location() {
         return this.location;
     }
 
-    public String name() {
-        return this.name;
-    }
-=======
-    public Car move(int distance) {
-        this.location += distance;
-        return new Car(this.location);
-    }
-
-    public int location() {
-        return this.location;
-    }
->>>>>>> e356fab2c (refactor Car 클래스를 필드로 가지는 일급컬렉션 추가)
+    public String showName() { return this.name; }
 }
