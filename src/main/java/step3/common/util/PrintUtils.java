724ff5ff543e61b2398fa0de3a312542a801650b
진우 <nice7677@naver.com>
2023-04-21 13:58:39 +0900

    feat: 출력 추가

---
+++ src/main/java/step3/common/util/PrintUtils.java
@@ -0,0 +1,13 @@
+package step3.common.util;
+
+public class PrintUtils {
+
+    public static void println(String text) {
+        System.out.println(text);
+    }
+
+    public static void print(String text) {
+        System.out.print(text);
+    }
+
+}
