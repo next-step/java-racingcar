df04ee03e8af02a26eb346ae26251fa0dab5b6ed
진우 <nice7677@naver.com>
2023-04-21 14:27:16 +0900

    feat: Car, Game, RacingCarGame 추가

---
+++ src/main/java/step3/common/RacingCarGame.java
@@ -0,0 +1,10 @@
+package step3.common;
+
+public class RacingCarGame implements Game {
+
+    @Override
+    public void start() {
+
+    }
+
+}
--- src/main/java/step3/common/util/RandomUtils.java
+++ src/main/java/step3/common/util/RandomUtils.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class RandomUtils {
 
-    private static final int MAXIMUM_NUMBER = 10;
+    private static final int BOUND = 10;
     private static final Random random = new Random();
 
     public static int generate() {
-        return random.nextInt(MAXIMUM_NUMBER);
+        return random.nextInt(BOUND);
     }
 
 }
