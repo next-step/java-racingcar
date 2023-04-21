f27dd9bf8d7dd1716c2ef230a38c1fd86edd1f01
juno-junho <ssmm0205@naver.com>
2023-04-21 15:04:57 +0900

    test(racingcar): CarNameTest refactoring

--- src/test/java/study/racingcar/domain/CarNameTest.java
+++ src/test/java/study/racingcar/domain/CarNameTest.java
@@ -2,6 +2,8 @@ package study.racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -10,7 +12,7 @@ class CarNameTest {
 
     @DisplayName("자동차 이름 길이가 5이상일 경우 예외 발생")
     @Test
-    void throw_exception_when_name_length_is_six() {
+    void when_carNameOver5_Expect_ThrowsException() {
         // given
         String carName = "abcdef";
 
@@ -20,39 +22,16 @@ class CarNameTest {
 
     }
 
-    @DisplayName("자동차 이름 길이가 5일 경우 예외 발생하지 않음")
-    @Test
-    void do_not_throw_exception_when_name_length_is_five() {
-        // given
-        String carName = "abcde";
-
-        // when, then
-        assertThatNoException().isThrownBy(() -> new CarName(carName));
-    }
-
-    @DisplayName("자동차 이름이 5이하일 경우 예외 발생하지 않음")
-    @Test
-    void do_not_throw_exception_when_name_length_is_four() {
-        // given
-        String carName = "abcd";
-
-        // when, then
-        assertThatNoException().isThrownBy(() -> new CarName(carName));
-    }
-
-    @DisplayName("자동차 이름 길이 측정시 공백 제거 하는지 테스트")
-    @Test
-    void when_name_contains_blank() {
-        // given
-        String carName = "    abcde   ";
-
-        // when, then
+    @DisplayName("자동차 이름 길이가 5이하일 경우 예외 발생하지 않음")
+    @ParameterizedTest
+    @ValueSource(strings = {"abcde", "    abcde   ", "abcd", "a"})
+    void when_carNameLessThanOrEqualTo5_Expect_NoException(String carName) {
         assertThatNoException().isThrownBy(() -> new CarName(carName));
     }
 
     @DisplayName("자동차 이름 길이가 주어지지 않은 경우 예외 발생")
     @Test
-    void throw_exception_when_name_length_is_zero() {
+    void when_NoCarName_Expect_ThrowsException() {
         // given
         String carName = "     ";
 
