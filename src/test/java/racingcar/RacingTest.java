a43ce10f5bb989b836ee74a3c1942af1a3807a66
dhkimneer <kimds989@naver.com>
2023-04-23 01:38:59 +0900

    test: refactor RacingTest test

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -9,19 +9,20 @@ import static org.assertj.core.api.Assertions.*;
 
 public class RacingTest {
 
-    private static final int NUMBER_OF_MOVES = 1;
     private static final int NUMBER_OF_CARS = 3;
 
     @Test
     @DisplayName("[요구사항 1] 1회 이동 확인")
     void 요구사항_1() {
-        // given : 자동차 생성
-        Racing racing = new Racing(NUMBER_OF_CARS, NUMBER_OF_MOVES);
+        // given : 자동차 생성 및 강제 1회 이동
         List<Car> cars = Racing.create(NUMBER_OF_CARS);
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
-        // when : 1회 이동 진행
-        racing.race(cars);
+        ForceMovingStrategy forceMovingStrategy = new ForceMovingStrategy(randomNumberGenerator);
+        Racing racing = new Racing(NUMBER_OF_CARS, cars, forceMovingStrategy);
+        racing.race();
 
-        // then : TODO: 검증 방법
+        // then : 모든 차들이 1칸을 갔는지 검증
+        cars.forEach(car -> assertThat(car.getMoveCount()).isEqualTo(1));
     }
 }
