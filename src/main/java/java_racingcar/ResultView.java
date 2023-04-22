202af94223e355720ea8fd3dd7dad7bbfbdc46c1
yerimmac <yerim.lee.earth@gmail.com>
2023-04-22 16:09:33 +0900

    feature: 출력 메서드 및 클래스 구현

---
+++ src/main/java/java_racingcar/ResultView.java
@@ -0,0 +1,15 @@
+package java_racingcar;
+
+public class ResultView {
+    public static void printRaceResult() {
+        System.out.println("실행 결과");
+    }
+
+    public static void printSingleRace(String raceResult) {
+        System.out.println(raceResult);
+    }
+
+    public static void printEmptyLine() {
+        System.out.println();
+    }
+}
