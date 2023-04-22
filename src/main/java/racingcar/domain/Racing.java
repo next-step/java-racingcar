50add88c183748d4f899d0243419dc98b499bd17
ain <lovepizza132@gmail.com>
2023-04-22 14:14:37 +0900

    test : Racing recordMaxPositioin, makeCarPositionList 추가

--- src/main/java/racingcar/domain/Racing.java
+++ src/main/java/racingcar/domain/Racing.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import racingcar.strategyTest.MoveStrategy;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Racing {
@@ -12,6 +13,35 @@ public class Racing {
             racingCar.tryToMove(moveStrategy);
             updateMaxPosition(racingCar.position());
         }
+    }
+
+//    public List<String> findWinner(List<RacingCar> racingCars) {
+//        List<Integer> carPositionValues = new ArrayList<>();
+//        for (RacingCar racingCar : racingCars) {
+//            carPositionValues.add(racingCar.position());
+//        }
+////        List<Integer>Collections.max(carPositionValues);
+//    }
+    public List<Integer> makeCarPostionList(List<RacingCar> racingCars) {
+        List<Integer> carPositions = new ArrayList<>();
+
+        for(RacingCar racingcar : racingCars) {
+            carPositions.add(racingcar.position());
+        }
+
+        return carPositions;
+    }
+
+    public int recordMaxPosition(List<Integer> carPositions) {
+        return Collections.max(carPositions);
+    }
+
+    public void findWinner(List<RacingCar> racingCars) {
+        List<Integer> carPositions = makeCarPostionList(racingCars);
+
+        int maxPosition = recordMaxPosition(carPositions);
+
+
     }
 
     public void updateMaxPosition(int position) {
--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -9,6 +9,7 @@ import racingcar.strategyTest.MoveStrategy;
 import racingcar.strategyTest.RandomMoveStrategy;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -88,6 +89,45 @@ public class RacingTest {
         List<String> result = racing.makeWinnerList(racingCarList);
 
         assertThat(result.get(0)).isEqualTo("test2");
+    }
+
+    @Test
+    @DisplayName("자동차 maxPostionList 만들기")
+    public void findWinner() {
+        RacingCar racingCar = new RacingCar("test", 4);
+        RacingCar racingCar2 = new RacingCar("test2", 5);
+        RacingCar racingCar3 = new RacingCar("test3", 5);
+        RacingCar racingCar4 = new RacingCar("test4", 3);
+
+        List<RacingCar> racingCars = new ArrayList<>();
+        racingCars.add(racingCar);
+        racingCars.add(racingCar2);
+        racingCars.add(racingCar3);
+        racingCars.add(racingCar4);
+
+        // carPostionsList
+        Racing racing = new Racing();
+
+        List<Integer> carPositions = racing.makeCarPostionList(racingCars);
+
+        assertThat(carPositions.get(0)).isEqualTo(4);
+        assertThat(carPositions.get(1)).isEqualTo(5);
+        assertThat(carPositions.get(2)).isEqualTo(5);
+        assertThat(carPositions.get(3)).isEqualTo(3);
+
+    }
+
+    @Test
+    @DisplayName("자동차 maxPosition 찾기 5반환")
+    public void maxPosition_ReturnFive() {
+        List<Integer> carPositions = List.of(3,4,5,5);
+
+        Racing racing = new Racing();
+
+        int maxPosition = racing.recordMaxPosition(carPositions);
+
+        assertThat(maxPosition).isEqualTo(5);
+
     }
 
 }
