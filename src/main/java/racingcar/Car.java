71f0596403dc498cbee9d2d081b65a150cb09441
dhkimneer <kimds989@naver.com>
2023-04-23 01:34:35 +0900

    refactor: refactor move function(Car class)

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,15 +2,14 @@ package racingcar;
 
 public class Car {
 
-    private static final int THRESHOLD = 4;
     private int moveCount = 0;
 
     public int getMoveCount() {
         return moveCount;
     }
 
-    public void move(int givenNumber) {
-        if (givenNumber >= THRESHOLD) {
+    public void move(MovingStrategy movingStrategy) {
+        if (movingStrategy.isMove()) {
             moveCount++;
         }
     }
