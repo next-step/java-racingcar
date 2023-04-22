2d35061b49d00bc8d6ca8db4e822c7a0c2128ac1
kingdong <ultraraptor@naver.com>
2023-04-22 22:56:17 +0900

    test : 테스트코드 추가

--- src/test/java/carracing/model/MatchTest.java
+++ src/test/java/carracing/model/MatchTest.java
@@ -3,26 +3,28 @@ package carracing.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class MatchTest {
-    @DisplayName("전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다")
-    @Test
-    public void goOver4() {
-        //given
-        //when
-        //then
-        fail();
-    }
 
     @DisplayName("우승자는 여러명일 수 있다")
     @Test
     public void multiWinner() {
         //given
-
+        Racer mika = new Racer("mika", List.of(7, 7, 7));
+        Racer mj = new Racer("mj", List.of(7, 7, 7));
+        Racer oasis = new Racer("oasis", List.of(7, 7, 7));
+        int asnwer = 3;
+        Match match = new Match();
         //when
+        match.addRacer(mika);
+        match.addRacer(mj);
+        match.addRacer(oasis);
         //then
-        fail();
+        assertThat(match.winnerDisplay().participateCount()).isEqualTo(asnwer);
     }
 
 }
