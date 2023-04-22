9de93a283b1328c829f7c1b4167c54bbc4603dc4
soochan <tncksdl05@gmail.com>
2023-04-22 22:20:48 +0900

    refactor : controller 로직을 main으로 이동

--- src/main/java/racingrefactoring/Application.java
+++ src/main/java/racingrefactoring/Application.java
@@ -1,13 +1,39 @@
 package racingrefactoring;
 
-import racingrefactoring.controller.RacingCarController;
+import racingrefactoring.domain.CarGroup;
+import racingrefactoring.domain.MoveStrategy;
+import racingrefactoring.domain.RacingCarGame;
 import racingrefactoring.domain.RandomMoveStrategy;
+import racingrefactoring.view.InputView;
+import racingrefactoring.view.OutputView;
 
 public class Application {
 
   public static void main(String[] args) {
-    RacingCarController racingCarController = new RacingCarController(new RandomMoveStrategy());
-    racingCarController.startGame();
+    CarGroup carGroup = initCarGroup();
+    carGroup = playGames(carGroup, new RandomMoveStrategy());
+    OutputView.printWinners(carGroup.findWinners());
+  }
+
+  private static CarGroup initCarGroup() {
+    String carNames = InputView.scanCarNames();
+
+    return new CarGroup(carNames);
+  }
+
+  private static CarGroup playGames(CarGroup carGroup, MoveStrategy moveStrategy) {
+    RacingCarGame racingCarGame = new RacingCarGame(carGroup);
+
+    int round = InputView.scanRound();
+
+    OutputView.printEmptyLine();
+    OutputView.printResultMessage();
+    for (int i = 0; i < round; i++) {
+      carGroup = racingCarGame.play(moveStrategy);
+      OutputView.printCarGroup(carGroup);
+      OutputView.printEmptyLine();
+    }
+    return carGroup;
   }
 
 }
