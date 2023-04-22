e8bc15a815473bebb75b77ac21e18dd9561c7c4c
ji lim <jilim@jiui-MacBookPro.local>
2023-04-22 22:09:40 +0900

    Feat: merge 피드백 반영

--- src/main/java/car/step3/Winner.java
+++ src/main/java/car/step3/Winner.java
@@ -4,19 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Winner {
-    private int max_location;
+    private int maxLocation;
     private List<String> winners = new ArrayList<>();
 
     public List<String> findWinner(List<Car> cars) {
         for (Car car : cars) {
-            addWinner(car);
+            findWinner(car);
         }
         return this.winners;
     }
 
-    private void addWinner(Car car) {
-        if(car.location() >= this.max_location) {
-            this.max_location = car.location();
+    private void findWinner(Car car) {
+        if(car.location() == this.maxLocation) {
+            this.winners.add(car.name());
+        }
+        if(car.location() > this.maxLocation) {
+            this.maxLocation = car.location();
+            this.winners.clear();;
             this.winners.add(car.name());
         }
     }
