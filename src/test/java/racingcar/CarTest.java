4864f0ac37bc80bc1d2dd5a84120e81906b6bad2
dhkimneer <kimds989@naver.com>
2023-04-23 01:38:43 +0900

    test: refactor CarTest test

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,18 +8,21 @@ import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
 
-    private static final int ZERO = 0;
-    private static final int FOUR = 4;
+    Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car();
+    }
 
     @Test
-    @DisplayName("[요구사항 1] random 값이 4 이상일 경우 1칸 전진한다.")
+    @DisplayName("[요구사항 1] random 값이 4 이상일 경우(전진 조건 만족할 경우) 1칸 전진한다.")
     void 요구사항_1() {
         // given
-        Car car = new Car();
         final int CAR_MOVE_COUNT = car.getMoveCount();
 
         // when
-        car.move(FOUR);
+        car.move(()->true);
 
         // then
         assertThat(car.getMoveCount()).isEqualTo(CAR_MOVE_COUNT + 1);
@@ -28,11 +32,10 @@ public class CarTest {
     @DisplayName("[요구사항 2] random 값이 4 미만일 경우 멈춘다.")
     void 요구사항_2() {
         // given
-        Car car = new Car();
         final int CAR_MOVE_COUNT = car.getMoveCount();
 
         // when
-        car.move(ZERO);
+        car.move(()->false);
 
         // then
         assertThat(car.getMoveCount()).isEqualTo(CAR_MOVE_COUNT);
