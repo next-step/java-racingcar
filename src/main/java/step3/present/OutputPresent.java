8e03517e912d911f02f59d22e6c02741e12d0086
kingdong <ultraraptor@naver.com>
2023-04-22 17:23:33 +0900

    refactor : UI 클래스 수정

--- src/main/java/step3/present/OutputPresent.java
+++ src/main/java/step3/present/OutputPresent.java
@@ -1,14 +1,31 @@
 package step3.present;
 
+import step3.model.Racer;
+
 import java.util.List;
 
 public class OutputPresent {
-    public void printResult(List<DisplayRaceVO> collect) {
-        System.out.println("실행 결과");
-        throw new RuntimeException();
+    public void printResult(List<RacingDisplayVO> racingDisplayVOList) {
+         for(RacingDisplayVO racingDisplayVO :racingDisplayVOList) {
+             System.out.println(racingDisplayVO.getCarName());
+             for(String s : racingDisplayVO.getResults()) {
+                 System.out.println(s);
+             }
+         }
     }
 
-    public void printWinner(List<String> winnerList) {
-        throw new RuntimeException();
+    public void printWinner(List<RacingDisplayVO> racingDisplayVOList) {
+        String winners = winnerString(racingDisplayVOList);
+        System.out.printf("우승자는 %s 입니다", winners);
+    }
+
+    private String winnerString(List<RacingDisplayVO> racingDisplayVOList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(RacingDisplayVO racingDisplayVO : racingDisplayVOList) {
+            stringBuilder.append(racingDisplayVO.getCarName());
+            stringBuilder.append(",");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        return stringBuilder.toString();
     }
 }
