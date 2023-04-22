5e644bd6a638353f19295733a7f2cbb3f05495ca
kingdong <ultraraptor@naver.com>
2023-04-22 22:22:04 +0900

    test : 테스트코드 추가

--- src/test/java/carracing/model/PresenterTest.java
+++ src/test/java/carracing/model/PresenterTest.java
@@ -3,15 +3,16 @@ package carracing.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class PresenterTest {
 
     @DisplayName("")
     @Test
-    public void run() {
+    public void s() {
         //given
         //when
         //then
+        fail();
     }
 }
