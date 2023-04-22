fbbe9ce31072a2e0a2d5a5a871a7b7886b59cef1
kingdong <ultraraptor@naver.com>
2023-04-22 17:25:10 +0900

    refactor : 경기를 관리하는 오브젝트 수정

---
+++ src/main/java/step3/model/Match.java
@@ -0,0 +1,50 @@
+package step3.model;
+
+
+import step3.present.RacingDisplayVO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Match {
+    List<Racer> racers = new ArrayList<>();
+    public void addRacer(Racer racer) {
+        racers.add(racer);
+    }
+
+    public List<RacingDisplayVO> progressDisplay() {
+        List<RacingDisplayVO> racingDisplayVOs = new ArrayList<>();
+        for(Racer racer : racers) {
+            racingDisplayVOs.add(new RacingDisplayVO(racer));
+        }
+        return racingDisplayVOs;
+    }
+
+    public List<RacingDisplayVO> winnerDisplay() {
+        List<RacingDisplayVO> racingDisplayVOs = new ArrayList<>();
+        List<Racer> winners = findWinners(racers);
+        for(Racer winner : winners) {
+            racingDisplayVOs.add(new RacingDisplayVO(winner));
+        }
+        return racingDisplayVOs;
+    }
+
+    private List<Racer> findWinners(List<Racer> racers) {
+        List<Racer> winners = new ArrayList<>();
+        int winnerPosition = maxPosition(racers);
+        for(Racer racer : racers) {
+            if(racer.isWinner(winnerPosition)) {
+                winners.add(racer);
+            }
+        }
+        return winners;
+    }
+
+    private int maxPosition(List<Racer> racers) {
+        int maxPosition = -1;
+        for(Racer racer :racers) {
+           maxPosition = Math.max(maxPosition, racer.finalPosition());
+        }
+        return maxPosition;
+    }
+}
