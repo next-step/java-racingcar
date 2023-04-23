890b61a6ef2f8d465e5facfc669bb166fea5ff9c
kyoungil.yoon <dev.atypical@gmail.com>
2023-04-23 21:38:58 +0900

    fix: input value

--- src/main/java/step3/RacingCarMain.java
+++ src/main/java/step3/RacingCarMain.java
@@ -5,10 +5,10 @@ import step3.view.InputView;
 public class RacingCarMain {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        int carCount = inputView.inputValue("자동차 대수는 몇대인가요");
-        int periodCount = inputView.inputValue("시도할 회수는 몇 회인가요");
+        String carNames = inputView.inputValue("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        int periodCount = Integer.parseInt(inputView.inputValue("시도할 회수는 몇 회인가요"));
 
-        RacingProcess racingProcess = new RacingProcess(carCount, periodCount);
+        RacingProcess racingProcess = new RacingProcess(carNames, periodCount);
         racingProcess.racingStart();
 
     }
