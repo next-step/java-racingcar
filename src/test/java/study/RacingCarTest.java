32c8229a4205c7e02ff2ad02400531af0929ad81
zi-seong <jiseong4577@gmail.com>
2023-04-24 00:26:33 +0900

    test (RecingCar) : 사용자에게 숫자만 입력받는 기능 테스트

--- src/test/java/study/RacingCarTest.java
+++ src/test/java/study/RacingCarTest.java
@@ -1,19 +1,33 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.*;
 public class RacingCarTest {
 
     RacingCar racingCar = new RacingCar();
 
+
     @Test
     @DisplayName("사용자에게 특정 메세지를 보여주는 기능")
     void showMessage() {
-        Assertions.assertThat(racingCar.questionMessage("TRY")).isEqualTo("시도할 회수는 몇 회 인가요?");
-        Assertions.assertThat(racingCar.questionMessage("CAR")).isEqualTo("자동차 대수는 몇 대 인가요?");
-        Assertions.assertThat(racingCar.questionMessage("A")).isEqualTo("NO MESSAGE");
+        assertThat(racingCar.questionMessage("TRY")).isEqualTo("시도할 회수는 몇 회 인가요?");
+        assertThat(racingCar.questionMessage("CAR")).isEqualTo("자동차 대수는 몇 대 인가요?");
+        assertThat(racingCar.questionMessage("A")).isEqualTo("NO MESSAGE");
     }
 
+    @Test
+    @DisplayName("사용자에게 숫자만 입력받는 기능")
+    void systemSetInNumber() {
+        InputStream inputStream = new ByteArrayInputStream("3".getBytes());
+        System.setIn(inputStream);
+        Scanner scanner = new Scanner(System.in);
+        String result = scanner.next();
+        assertThat(result).isEqualTo("3");
+    }
 }
