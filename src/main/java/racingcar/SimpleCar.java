4bb3664841c7ab159bb6a41f8e7637b96df1ae62
issac.park <issac.park@kakaocorp.com>
2023-04-24 01:27:22 +0900

    STEP4-1 feat: 이름 글자수 validation

--- src/main/java/racingcar/SimpleCar.java
+++ src/main/java/racingcar/SimpleCar.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class SimpleCar {
     public static int RANDOM_BOUND = 10;
     public static int LEAST_MOVABLE_NUM = 4;
+    public static int MAX_NAME_LENGTH = 5;
 
     private int position = 0;
 
@@ -29,5 +30,9 @@ public class SimpleCar {
 
     public Integer getPosition() {
         return position;
+    }
+
+    static boolean isValidName(String name) {
+        return name != null && !name.isBlank() && name.length() <= MAX_NAME_LENGTH;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,6 +3,8 @@ package racingcar;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,5 +20,16 @@ public class CarTest {
     @DisplayName("최초 포지션은 0")
     void Test_initial_position_0() {
         assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {":false", "carname_too_long:false", "abc:true"}, delimiter = ':')
+    @DisplayName("이름 부여 5글자 제한")
+    void Test_car_name_length_validation(String inputName, boolean expected) {
+        // when
+        boolean isValid = SimpleCar.isValidName(inputName);
+
+        // then
+        assertThat(isValid).isEqualTo(expected);
     }
 }
