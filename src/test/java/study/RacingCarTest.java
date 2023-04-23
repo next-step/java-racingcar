4f7e3105ebce20ca8dfa9c342d34a7383881f7f3
zi-seong <jiseong4577@gmail.com>
2023-04-23 23:45:00 +0900

    test (RecingCar) : 사용자에게 특정 메세지를 보여주는 기능 테스트

--- src/test/java/study/RacingCarTest.java
+++ src/test/java/study/RacingCarTest.java
@@ -1,2 +1,19 @@
-package study;public class RacingCarTest {
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+
+    RacingCar racingCar = new RacingCar();
+
+    @Test
+    @DisplayName("사용자에게 특정 메세지를 보여주는 기능")
+    void showMessage() {
+        Assertions.assertThat(racingCar.questionMessage("TRY")).isEqualTo("시도할 회수는 몇 회 인가요?");
+        Assertions.assertThat(racingCar.questionMessage("CAR")).isEqualTo("자동차 대수는 몇 대 인가요?");
+        Assertions.assertThat(racingCar.questionMessage("A")).isEqualTo("NO MESSAGE");
+    }
+
 }
