f8343479e6cb19a312a233f9bcbd4979539bfcaf
zi-seong <jiseong4577@gmail.com>
2023-04-23 23:44:08 +0900

    feat (feature) : 사용자에게 특정 메세지를 보여주는 기능

---
+++ src/main/java/study/RacingCar.java
@@ -0,0 +1,18 @@
+package study;
+
+public class RacingCar {
+
+    String questionCarCountMessage = "자동차 대수는 몇 대 인가요?";
+    String questionTryCountMessage = "시도할 회수는 몇 회 인가요?";
+    String questionNoMessage = "NO MESSAGE";
+
+    public String questionMessage(String type) {
+        if (type.equals("TRY")) {
+            return questionTryCountMessage;
+        } else if (type.equals("CAR")) {
+            return questionCarCountMessage;
+        }
+        return questionNoMessage;
+    }
+
+}
