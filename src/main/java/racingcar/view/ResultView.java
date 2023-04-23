6cd0f4b7de01bc311c17258d2d630e4cfee5e37f
ryan <wsdf25867@gmail.com>
2023-04-23 13:35:59 +0900

    refactor : ResultView 변경
    * toString 구현에 의한 ResultView 클래스 변경

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,36 +1,23 @@
 package racingcar.view;
 
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
 
 import java.util.List;
 
 public class ResultView {
 
     private static final String MESSAGE_EXECUTE_RESULT = "실행결과";
-    public static final String POSITION_MAKER = "-";
-    public static final String NAME_DELIMITER = ",";
     public static final String MESSAGE_WINNER_ANNOUNCEMENT = "%s가 최종 우승했습니다.";
 
     public static void printResultHeader() {
         System.out.println(MESSAGE_EXECUTE_RESULT);
     }
 
-    public static void printCarsResult(List<Car> cars) {
-        for (Car car : cars) {
-            printCar(car);
-        }
+    public static void printCarsResult(Cars cars) {
+        System.out.println(cars.toString());
     }
-
-    private static void printCar(Car car) {
-        System.out.print(car.name() + " : ");
-        for (int i = 0; i < car.position(); i++) {
-            System.out.print(POSITION_MAKER);
-        }
-        System.out.println();
-    }
-
-    public static void printWinners(List<String> winners) {
-        String winnersString = String.join(NAME_DELIMITER, winners);
-        System.out.printf(MESSAGE_WINNER_ANNOUNCEMENT, winnersString);
+    public static void printWinners(Cars winners) {
+        System.out.printf(MESSAGE_WINNER_ANNOUNCEMENT, winners.toStringNames());
     }
 }
