9ea009954e66d77ecab8d7320750eef767a77900
ryan <wsdf25867@gmail.com>
2023-04-23 13:31:52 +0900

    refactor : CarFactory 및 CarFactoryTest 수정
    * Cars 일급콜렉션을 추가하면서 Factory 클래스도 Cars를 반환하도록 변경

--- src/test/java/racingcar/domain/CarFactoryTest.java
+++ src/test/java/racingcar/domain/CarFactoryTest.java
@@ -2,11 +2,7 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import racingcar.domain.Car;
-import racingcar.domain.CarFactory;
-
-import java.util.List;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,10 +10,10 @@ public class CarFactoryTest {
 
     @ParameterizedTest
     @DisplayName("쉼표로 구분된 이름수 만큼 자동차를 생산한다")
-    @CsvSource(value = {"짱구,철수,맹구:3", "짱구,철수:2", "짱구:1"}, delimiter = ':')
-    void create(String input, int output) {
-        List<Car> cars = CarFactory.create(input);
+    @ValueSource(strings = {"짱구,철수,맹구", "짱구,철수", "짱구"})
+    void create(String input) {
+        Cars cars = CarFactory.create(input);
 
-        assertThat(cars).hasSize(output);
+        assertThat(cars.toStringNames()).contains(input);
     }
 }
