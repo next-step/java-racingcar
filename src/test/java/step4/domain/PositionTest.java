3c2af0533b5d51ed7046fdc003f7eb98318f0a7e
juhyun <zzang9ha@naver.com>
2023-04-23 13:27:45 +0900

    test: 테스트 케이스 수정

--- src/test/java/step4/domain/PositionTest.java
+++ src/test/java/step4/domain/PositionTest.java
@@ -18,21 +18,19 @@ class PositionTest {
     }
 
     @Test
-    void 랜덤값이_4이상이면_자동차는_전진한다() {
+    void Position_객체의_초기값은_0이다() {
         final Position position = Position.create();
 
-//        position.forward(4);
-
-        assertThat(position.getDistance()).isEqualTo(1);
+        assertThat(position.getDistance()).isEqualTo(0);
     }
 
     @Test
-    void 랜덤값이_4미만이면_자동차는_멈춰있다() {
+    void 자동차가_전진하면_거리가_1_증가한다() {
         final Position position = Position.create();
 
-//        position.forward(3);
+        position.forward();
 
-        assertThat(position.getDistance()).isEqualTo(0);
+        assertThat(position.getDistance()).isEqualTo(1);
     }
 
     @Test
@@ -40,7 +38,7 @@ class PositionTest {
         Position winner = Position.create();
         Position position1 = Position.create();
         Position position2 = Position.create();
-//        winner.forward(4);
+        winner.forward();
         final List<Position> positions = List.of(winner, position1, position2);
 
         final Position actual = Position.winnerPosition(positions);
