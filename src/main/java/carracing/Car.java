f578fe34e21481ee4a27cd84a1bbe5b58a35b79b
seominah <seominah95@gmail.com>
2023-04-23 22:04:12 +0900

    Refactor: Car Class 리팩토링 및 테스트코드 추가

--- src/main/java/carracing/Car.java
+++ src/main/java/carracing/Car.java
@@ -1,19 +1,34 @@
 package carracing;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
     private static final int MINIMUM_SPEED = 4;
-    private int speed;
-    private int step;
+    private final String name;
+    private int position;
 
-    public int move(int speed) {
-        if (speed < MINIMUM_SPEED) {
-            stop();
-            return this.step;
-        }
-        return ++this.step;
+    public Car() {
+        this.name = "temp";
     }
 
-    public int stop() {
-        return 0;
+    public Car(final String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("5글자를 초과하였습니다.");
+        }
+        this.name = name;
+    }
+
+    public int move(int speed) {
+        if (speed >= MINIMUM_SPEED) {
+            this.position++;
+        }
+        return this.position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
--- src/test/java/carracing/CarTest.java
+++ src/test/java/carracing/CarTest.java
@@ -1,15 +1,63 @@
 package carracing;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * 1. 자동차는 이름을 가진다.
+ * 2. 속도가 4이상이면 움직인다.
+ * 2-1. 속도가 3이하이면 위치가 변하지 않는다.
+ * 3. 이름은 5글자 초과면 예외를 발생시킨다.
+ * 4. 첫번째 위치의 자동차의 속도가 4이상이여서 두번째 위치에 도착한다.
+ */
 public class CarTest {
-    @DisplayName("값이 4이상일 경우 전진하는 테스트")
-    @Test
-    void move() {
-        Car car = new Car();
-        assertThat(car.move(5)).isEqualTo(1);
+    public Car pobi;
+    @BeforeEach
+    void init() {
+        pobi = new Car("pobi");
     }
+
+    @DisplayName("자동차는 이름을 가진다.")
+    @Test
+    void getName() {
+        assertEquals(pobi.getName(), "pobi");
+    }
+
+    @DisplayName("속도가 4이상이면 움직인다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void move_over4(int number) {
+        assertEquals(pobi.move(number), 1);
+    }
+
+    @DisplayName("속도가 3이하이면 위치가 변하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void move_under3(int number) {
+        assertEquals(pobi.move(number), 0);
+    }
+
+    @DisplayName("이름은 5글자 초과면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdef", "abcdefg"})
+    void checkName(String 여섯글자를_넘는_이름) {
+        assertThrows(IllegalArgumentException.class, () -> new Car(여섯글자를_넘는_이름));
+    }
+
+    @DisplayName("첫번째 위치의 자동차의 속도가 4이상이여서 두번째 위치에 도착한다.")
+    @Test
+    void move_twoStepPosition() {
+        pobi.move(5);
+        pobi.move(6);
+        assertEquals(pobi.getPosition(), 2);
+    }
+
 }
