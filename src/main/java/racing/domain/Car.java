14fc0b5d543e7e2ff131a33e34e6f6ac16d6b129
bahar-j <joonie811@naver.com>
2023-04-23 21:55:54 +0900

    refactor: 상수 이름 변경

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -3,9 +3,9 @@ package racing.domain;
 public class Car {
 
     private static final Integer DEFAULT_POSITION = 0;
-    private static final Integer MAX_RANDOM = 9;
-    private static final Integer NAME_LENGTH_CRITERIA = 5;
-    private static final Integer MOVE_CRITERIA = 4;
+    private static final Integer RANDOM_MAX = 9;
+    private static final Integer NAME_LEN_MAX = 5;
+    private static final Integer MOVE_MIN = 4;
 
     private final String name;
     private int position;
@@ -23,16 +23,16 @@ public class Car {
     }
 
     private static void validateName(String name) {
-        if (name.length() > NAME_LENGTH_CRITERIA) {
+        if (name.length() > NAME_LEN_MAX) {
             throw new RuntimeException("이름은 5자를 초과할 수 없습니다.");
         }
     }
 
     public void move(int randomNumber) {
-        if (randomNumber < 0 || randomNumber > MAX_RANDOM) {
+        if (randomNumber < 0 || randomNumber > RANDOM_MAX) {
             throw new RuntimeException("랜덤값 경계를 벗어났습니다.");
         }
-        if (randomNumber >= MOVE_CRITERIA) {
+        if (randomNumber >= MOVE_MIN) {
             position += 1;
         }
     }
