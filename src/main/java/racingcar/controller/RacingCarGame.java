b2a05219f32eb13739f934e8261c83fe822a9923
ryan <wsdf25867@gmail.com>
2023-04-23 13:29:46 +0900

    refactor : RacingCarGameTest 삭제 및 RacingCarGame 수정
    * 역할을 분리하면서 Random 의존성 제거 및 상수의 이동

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -2,46 +2,40 @@ package racingcar.controller;
 
 import racingcar.domain.Car;
 import racingcar.domain.CarFactory;
-import racingcar.domain.WinnersUtil;
+import racingcar.domain.Cars;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class RacingCarGame {
-    private static final int MOVE_THRESHOLD = 4;
-    private static final int RANDOM_RANGE = 10;
     public static final String MESSAGE_NEGATIVE_NUMBER_INPUT = "음수는 입력할 수 없습니다. input: %d";
-    private List<Car> cars;
-    private List<String> winners = new ArrayList<>();
-    private int maxPosition;
-
-    public RacingCarGame() {
-        this.maxPosition = 0;
-    }
+    private Cars cars;
 
     public void start() {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
         InputView.printCarsNamesInputMessage();
         String names = inputNames(scanner);
+        cars = CarFactory.create(names);
 
         InputView.printTryCountInputMessage();
         int tryCount = inputCount(scanner);
 
-        cars = CarFactory.create(names);
-        play(tryCount, random);
-        int maxPosition = WinnersUtil.findMaxPosition(cars);
-        List<String> winners = WinnersUtil.determineWinners(cars, maxPosition);
+        play(tryCount);
+
+        end();
+    }
+
+    private void end() {
+        Car maxPositionCar = cars.findMaxPositionCar();
+        Cars winners = cars.findWinners(maxPositionCar);
 
         ResultView.printResultHeader();
         ResultView.printCarsResult(cars);
         ResultView.printWinners(winners);
     }
+
     private String inputNames(Scanner scanner) {
         return scanner.nextLine();
     }
@@ -54,28 +48,9 @@ public class RacingCarGame {
         return count;
     }
 
-    private void play(int tryCount, Random random) {
+    private void play(int tryCount) {
         for (int i = 0; i < tryCount; i++) {
-            moveCars(random);
+            cars.move();
         }
-    }
-
-    private void moveCars(Random random) {
-        for (Car car : cars) {
-            move(car, random);
-        }
-    }
-
-    private void move(Car car, Random random) {
-        if (isMovable(random)) {
-            car.move();
-        }
-    }
-
-    private boolean isMovable(Random random) {
-        return random.nextInt(RANDOM_RANGE) >= MOVE_THRESHOLD;
-    }
-    public List<String> getWinners() {
-        return winners;
     }
 }
