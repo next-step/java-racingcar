45931528723aedc4a3fbe481ea47cb7aa3c3a14d
ji lim <jilim@jiui-MacBookPro.local>
2023-04-24 20:18:28 +0900

    Feat: if 문에 괄호 추가

--- src/main/java/car/step3/domain/Car.java
+++ src/main/java/car/step3/domain/Car.java
@@ -48,7 +48,9 @@ public class Car {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        };
 
         Car car = (Car) o;
 
