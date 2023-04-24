83fde58adb57564934981cbde4e34a76dd2fa4b4
yihyeonkang <kellykang@yihyeonkangui-MacBookPro.local>
2023-04-24 22:11:57 +0900

    fix(printResult): 자동차 동시에 이동시키기

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,16 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
-    private int cars;
+    private int totalOfCars;
     private int count;
 
 
-    public void setting() {
+    public void inputGuideNumber() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("자동차 대수는 몇 대 인가요?");
-        cars = scanner.nextInt();
+        totalOfCars = scanner.nextInt();
 
         System.out.println("시도할 회수는 몇 회 인가요?");
         count = scanner.nextInt();
@@ -21,16 +23,19 @@ public class InputView {
         System.out.println();
         System.out.println("실행 결과");
 
-        for (int i = 0; i < cars; i++) {
-            final Car car = new Car();
-            System.out.println();
-            System.out.println("car = " + car);
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < totalOfCars; i++) {
+            cars.add(new Car());
+        }
 
-            for (int j = 0; j < count; j++) {
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < totalOfCars; j++) {
+                Car car = cars.get(j);
                 car.move(RandomGenerator.create());
-                System.out.println("result: " + car.print());
+
+                System.out.println(car.print());
             }
+            System.out.println();
         }
     }
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         final InputView racingCar = new InputView();
 
-        racingCar.setting();
+        racingCar.inputGuideNumber();
         racingCar.printResult();
     }
 
