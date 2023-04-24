6d6a200729f0ae11eaef9ccd18029f70450bcc56
hyeyoungs <sunzero0116@gmail.com>
2023-04-24 18:43:53 +0900

    자동차 경주 - 단위 테스트 | 2단계 - 문자열 덧셈 계산기 | 유의미하게 관리되어야 할 값들 상수 처리

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -4,20 +4,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    static final String customDelimiterPattern = "\"//(.)\\n(.*)";
+    static final String defaultDelimiter = ",|:";
+
     static public int splitAndSum(String text) {
         // null 또는 빈문자 처리
         if (isEmptyText(text)) {
             return 0;
         }
         // 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher matcher = Pattern.compile(customDelimiterPattern).matcher(text);
         if (matcher.find()) {
             String customDelimiter = matcher.group(1);
             text = matcher.group(2);
             return(sum(text.split(customDelimiter)));
         }
         // 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환
-        String defaultDelimiter = ",|:";
         return sum(text.split(defaultDelimiter));
     }
 
