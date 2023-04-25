cef2bbfa0e1a866ccd8aae9cb93a23f10eb3a0ed
dhkimneer <kimds989@naver.com>
2023-04-25 20:04:09 +0900

    feat: Add requestCarNames function and delete requestCarNums

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String SPLITTER = ",";
+    private static final String REQUEST_CAR_NAMES_INPUT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String REQUEST_MOVE_NUMS_INPUT = "시도할 회수는 몇 회인가요?";
 
-    public int requestCarNums() {
-        System.out.println("자동차 대수는 몇 대인가요?");
-        return SCANNER.nextInt();
+    public String[] requestCarNames() {
+        System.out.println(REQUEST_CAR_NAMES_INPUT);
+        return SCANNER.next().split(SPLITTER);
     }
 
     public int requestMoveNums() {
-        System.out.println("시도할 회수는 몇 회인가요?");
+        System.out.println(REQUEST_MOVE_NUMS_INPUT);
         return SCANNER.nextInt();
     }
 }
