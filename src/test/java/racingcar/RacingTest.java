777c6f9f991c8e7786334167294ae685ce283a2f
dhkimneer <kimds989@naver.com>
2023-04-25 20:07:04 +0900

    test: Update RacingTest.java by adding 요구사항_2 test

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -9,20 +11,41 @@ import static org.assertj.core.api.Assertions.*;
 
 public class RacingTest {
 
-    private static final int NUMBER_OF_CARS = 3;
+    private static final String[] RACING_CAR_EXAMPLE = new String[]{"pobi", "crong", "honux"};
+    private static final int ONE = 1;
 
     @Test
     @DisplayName("[요구사항 1] 1회 이동 확인")
     void 요구사항_1() {
         // given : 자동차 생성 및 강제 1회 이동
-        List<Car> cars = Racing.create(NUMBER_OF_CARS);
+        List<Car> cars = Racing.create(RACING_CAR_EXAMPLE);
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
         ForceMovingStrategy forceMovingStrategy = new ForceMovingStrategy(randomNumberGenerator);
-        Racing racing = new Racing(NUMBER_OF_CARS, cars, forceMovingStrategy);
+        Racing racing = new Racing(RACING_CAR_EXAMPLE, cars, forceMovingStrategy);
         racing.race();
 
         // then : 모든 차들이 1칸을 갔는지 검증
-        cars.forEach(car -> assertThat(car.getMoveCount()).isEqualTo(1));
+        cars.forEach(car -> assertThat(car.getMoveCount()).isEqualTo(ONE));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi", "crong", "honux"})
+    @DisplayName("[요구사항 2] 강제 1회 이동 후 최종 우승자 출력 확인")
+    void 요구사항_2(String carName) {
+        // given : 자동차 생성 및 강제 1회 이동
+        List<Car> cars = Racing.create(RACING_CAR_EXAMPLE);
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+        ForceMovingStrategy forceMovingStrategy = new ForceMovingStrategy(randomNumberGenerator);
+        Racing racing = new Racing(RACING_CAR_EXAMPLE, cars, forceMovingStrategy);
+        racing.race();
+
+        // when : 최대 이동 횟수 찾고, 우승자들 리스트 찾기
+        int maxMoveCount = racing.findMaxMoveCount(cars);
+        List<String> winners = racing.findWinnerCarNames(cars, maxMoveCount);
+
+        // then : 최종 우승자가 pobi, crong, honux 모두 나오는지 검증
+        assertThat(winners.contains(carName)).isTrue();
     }
 }
