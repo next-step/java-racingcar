b0b26c0a9ae7a2de24fce2cbb111b884dc6e8ad3
Dale <steady8@kakao.com>
2023-04-25 15:08:14 +0900

    2단계 - PR 수정 _바로 띄어스기 대체

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -8,49 +8,49 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 public class CalculatorTest {
 
     @Test
-    void 쉼표구분자테스트() {
+    void 쉼표_구분자_테스트() {
         String sample = "1,2,3";
         int result = StringNumberCalculator.sumPatternString(sample);
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    void 쉼표구분자테스트에빈문자열을넣으면결과0() {
+    void 쉼표_구분자_테스트에_빈_문자열을_넣으면_결과_0() {
         String sample = "";
         int result = StringNumberCalculator.sumPatternString(sample);
         assertThat(result).isEqualTo(0);
     }
 
     @Test
-    void 널테스트() {
+    void 널_테스트() {
         String sample = null;
         int result = StringNumberCalculator.sumPatternString(sample);
         assertThat(result).isEqualTo(0);
     }
 
     @Test
-    void 구분자콜론테스트() {
+    void 구분자_콜론_테스트() {
         String sample = "1:2:3:4:5";
         int result = StringNumberCalculator.sumPatternString(sample);
         assertThat(result).isEqualTo(15);
     }
 
     @Test
-    void 혼합테스트() {
+    void 혼합_테스트() {
         String sample = "1,2:3";
         int result = StringNumberCalculator.sumPatternString(sample);
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    void 커스텀구분자() {
+    void 커스텀_구분자() {
         String sample = "//b\n1b2b3";
         int result = StringNumberCalculator.sumPatternString(sample);
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    void 음수일경우예외처리() {
+    void 음수일경우_예외처리() {
         String sample = "-1:2:3";
 
         assertThatThrownBy(() -> {
