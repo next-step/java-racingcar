d1f7ab18029483b35c23d09bceebfd81567479ba
0giri <higiri@naver.com>
2023-04-25 19:51:53 +0900

    fix: Car 이동 거리를 요구사항에 맞게 변경

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -6,59 +6,54 @@ package racing.domain;
  * @since : 2023/04/15
  */
 public class Car {
-    private static final int DEFAULT_LOCATION = 0;
+    private static final int DEFAULT_POSITION = 0;
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
     private static final int MOVE_AVAILABLE_BOUND = 4;
 
     private final String name;
-    private int location;
+    private int position;
 
     public Car(String name) {
-        this(name, DEFAULT_LOCATION);
+        this(name, DEFAULT_POSITION);
     }
 
-    public Car(String name, int location) {
-        String trimmedName = name.trim();
-        validCarName(trimmedName);
-        this.name = trimmedName;
-        this.location = location;
+    public Car(String name, int position) {
+        validCarNameLength(name);
+        this.name = name;
+        this.position = position;
     }
 
     public Car(Car car) {
         this.name = car.name;
-        this.location = car.location;
+        this.position = car.position;
     }
 
-    private void validCarName(String trimmedName) {
-        if (validNameLength(trimmedName)) {
+    private void validCarNameLength(String name) {
+        if (name.length() < MIN_NAME_LENGTH || MAX_NAME_LENGTH < name.length()) {
             throw new IllegalArgumentException("앞,뒤 공백을 제외한 이름의 길이는 1~5의 길이만 가능합니다.");
         }
     }
 
-    private boolean validNameLength(String trimmedName) {
-        return trimmedName.length() < MIN_NAME_LENGTH || MAX_NAME_LENGTH < trimmedName.length();
-    }
-
-    public void move(int distance) {
-        if (canMove(distance)) {
-            changeLocation(distance);
+    public void move(int randomNo) {
+        if (canMove(randomNo)) {
+            increasePosition();
         }
     }
 
-    private boolean canMove(int distance) {
-        return MOVE_AVAILABLE_BOUND <= distance;
+    private boolean canMove(int randomNo) {
+        return MOVE_AVAILABLE_BOUND <= randomNo;
     }
 
-    private void changeLocation(int distance) {
-        this.location += distance;
+    private void increasePosition() {
+        this.position ++;
     }
 
     public String name() {
         return this.name;
     }
 
-    public int location() {
-        return this.location;
+    public int position() {
+        return this.position;
     }
 }
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -21,73 +21,52 @@ public class CarTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"0", "  0 ", "0g", "0gi", "0gir", "0giri"})
-    void 자동차이름_앞뒤_공백제외_1글자_이상_5글자_이하_성공(String value) {
+    @ValueSource(strings = {"0", "0gi", "0giri"})
+    void 이름_1글자_이상_5글자_이하_성공(String value) {
         assertThatNoException().isThrownBy(() -> new Car(value));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"", "  ", "0    0", "0girii"})
-    void 자동차이름_앞뒤_공백제외_1글자_미만_5글자_초과_실패(String value) {
+    @ValueSource(strings = {"", "0girii"})
+    void 이름_1글자_미만_5글자_초과_실패(String value) {
         assertThatThrownBy(() -> new Car(value)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 복사생성자로_생성된_복제본_변경시_원본_유지() {
-        //given
-        car.move(10);
-        Car copy = new Car(car);
-
+    void 이동_랜덤_4이상_전진() {
         //when
-        copy.move(5);
-
+        car.move(5);
         //then
-        assertThat(car.location()).isEqualTo(10);
-        assertThat(copy.location()).isEqualTo(15);
+        assertThat(car.position()).isEqualTo(1);
     }
 
     @Test
-    void 자동차_거리_4이상_전진() {
-        //when
-        car.move(5);
-
-        //then
-        assertThat(car.location()).isEqualTo(5);
-    }
-
-    @Test
-    void 자동차_거리_4미만_멈춤() {
-        //given
-        car.move(5);
-        assertThat(car.location()).isEqualTo(5);
-
+    void 이동_랜덤_4미만_정지() {
         //when
         car.move(2);
-
         //then
-        assertThat(car.location()).isEqualTo(5);
+        assertThat(car.position()).isEqualTo(0);
     }
 
     @Test
-    void 자동차_여러번_이동시_위치_누적() {
+    void 여러번_이동시_위치_누적() {
         //when
         car.move(5);
         car.move(2);
         car.move(8);
-
         //then
-        assertThat(car.location()).isEqualTo(13);
+        assertThat(car.position()).isEqualTo(2);
     }
 
     @Test
-    void 자동차_현재위치_반환() {
+    void 복사생성자_복제본_변경시_원본_유지() {
         //given
-        assertThat(car.location()).isEqualTo(0);
-
+        car.move(10);
+        Car copy = new Car(car);
         //when
-        car.move(7);
-
+        copy.move(5);
         //then
-        assertThat(car.location()).isEqualTo(7);
+        assertThat(car.position()).isEqualTo(10);
+        assertThat(copy.position()).isEqualTo(15);
     }
 }
