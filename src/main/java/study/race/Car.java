7e7dab5ef87d0202a02c393ff802bd63b04c320c
letitgobaby <gnu@gnuui-MacBookPro.local>
2023-04-25 13:16:59 +0900

    refactor : Car 객체의 이름 유효성 검사 수정

--- src/main/java/study/race/Car.java
+++ src/main/java/study/race/Car.java
@@ -2,13 +2,14 @@ package study.race;
 
 public class Car {
 
-    private String name;
+    private String carName;
     private int moved = 0;
 
     public Car() {}
 
-    public Car(String name) {
-        this.name = validateName(name);
+    public Car(String carName) {
+        this.carName = carName;
+        validateCarName(carName);
     }
 
     public void move() {
@@ -16,18 +17,23 @@ public class Car {
     }
 
     public String getName() {
-        return name;
+        return carName;
     }
 
     public int getScore() {
         return moved;
     }
 
-    private String validateName(String name) {
-        if (name == null || name.trim().isEmpty() || name.length() > 5) {
+    private void validateCarName(String carName) {
+        int carNameMaxLength = 5;
+
+        if (carName == null || carName.trim().isEmpty()) {
+            throw new IllegalStateException("자동차 이름을 입력해주세요.");
+        }
+
+        if (carName.length() > carNameMaxLength) {
             throw new IllegalStateException("자동차 이름은 1~5자만 가능합니다.");
         }
-        return name;
     }
 
 }
