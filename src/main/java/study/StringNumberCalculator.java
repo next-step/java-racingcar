6fc92d2dc500c3bdb6973a423e9ece24fbd5d396
Dale <steady8@kakao.com>
2023-04-25 11:01:48 +0900

    2단계 - PR 수정 패턴을 static 변수로 지정해서 불필요한 객체 생성을 피함.

--- src/main/java/study/StringNumberCalculator.java
+++ src/main/java/study/StringNumberCalculator.java
@@ -7,7 +7,10 @@ import java.util.regex.Pattern;
 public class StringNumberCalculator {
     public static final String DEFAULT_DELIMETER_REGEX = ",|:";
 
+    public static final Pattern CALCULATOR_NUMBER_PARSER_PATTERN = Pattern.compile("//(.)\n(.*)");
+
     public static int splitAndSum(String sample) {
+
 
         if (StringUtils.isBlank(sample)) { // 이 자체로도 해석이 가능하다고 생각, 메서드로 만들면 어떤 장점이 있을지 고민.
             return 0;
@@ -15,7 +18,7 @@ public class StringNumberCalculator {
 
         String splitRegex;
 
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(sample); // TODO:: 상수로 만들기
+        Matcher matcher = CALCULATOR_NUMBER_PARSER_PATTERN.matcher(sample);
 
         if (matcher.find()) {
             splitRegex = matcher.group(1); // TODO:: return 해야할 것이 splitRegex, sample 2개인데, 어떻게 메서드로 만들지?
