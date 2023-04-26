32b3458a80baa4c4a08153ee8c712cb505a9e109
juno-junho <ssmm0205@naver.com>
2023-04-26 21:50:09 +0900

    feat(racingcar): 게임 실행하는 Application 생성

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import study.racingcar.domain.RacingCarGame;
+
+public class Application {
+    public static void main(String[] args) {
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.run();
+    }
+}
