2a93e38bd6074cc268b25cc6caf11d2d74d5b968
seriouskang <scdean110@gmail.com>
2023-04-26 23:32:33 +0900

    refactor(CarRaceApplication): 메인 메소드 정리

--- src/main/java/study/carrace/step3/CarRaceApplication.java
+++ src/main/java/study/carrace/step3/CarRaceApplication.java
@@ -1,18 +1,23 @@
 package study.carrace.step3;
 
 import study.carrace.step3.application.RaceManager;
+import study.carrace.step3.domain.Cars;
 import study.carrace.step3.presentation.RaceMonitor;
+
+import java.util.List;
 
 import static study.carrace.step3.application.util.MoveStrategyFactory.randomMoveStrategy;
 import static study.carrace.step3.presentation.util.ConsoleInputUtil.*;
 
 public class CarRaceApplication {
     public static void main(String[] args) {
-        RaceManager raceManager = new RaceManager(askCarNames(), randomMoveStrategy(), askIterationCount());
-        RaceMonitor raceMonitor = new RaceMonitor(raceManager);
+        List<String> carNames = askCarNames();
+        int iterationCount = askIterationCount();
 
-        raceManager.startRace();
+        RaceManager raceManager = new RaceManager(carNames, randomMoveStrategy(), iterationCount);
+        Cars racedCars = raceManager.startRace();
 
+        RaceMonitor raceMonitor = new RaceMonitor(racedCars, iterationCount);
         raceMonitor.showCarsPositionHistory();
         raceMonitor.announceWinners();
     }
