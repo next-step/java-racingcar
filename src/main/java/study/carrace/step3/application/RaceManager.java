ddac92dccbbef519cc1a3c9b4c7ad2f1a23b0a08
seriouskang <scdean110@gmail.com>
2023-04-26 22:59:59 +0900

    refactor(RaceManager): startRace() 메소드 호출 시, 레이스가 완료된 Cars 객체 반환

--- src/main/java/study/carrace/step3/application/RaceManager.java
+++ src/main/java/study/carrace/step3/application/RaceManager.java
@@ -4,12 +4,10 @@ import study.carrace.step3.domain.*;
 import study.carrace.step3.domain.exception.IllegalIterationCountException;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class RaceManager {
-    private static final String LINE_BREAK = "\n";
-    private final Cars cars;
     private final int iterationCount;
+    private final Cars cars;
 
     public RaceManager(List<String> carNames, MoveStrategy moveStrategy, int iterationCount) {
         validateIterationCount(iterationCount);
@@ -18,29 +16,12 @@ public class RaceManager {
         this.iterationCount = iterationCount;
     }
 
-    public void startRace() {
-        IntStream.range(0, iterationCount)
-                .forEach(iteration -> moveOrStopCars());
-    }
-
-    public String carsPositionHistory() {
-        StringBuilder carsPositionHistory = new StringBuilder();
-        IntStream.rangeClosed(1, iterationCount)
-                .forEach(iteration -> carsPositionHistory.append(carsPositionAt(iteration)).append(LINE_BREAK));
-
-        return carsPositionHistory.toString();
+    public Cars startRace() {
+        return cars.moveOrStopCars(iterationCount);
     }
 
     public List<CarName> winners() {
         return cars.firstRankers();
-    }
-
-    private String carsPositionAt(int iteration) {
-        return cars.carsPositionAt(iteration);
-    }
-
-    private void moveOrStopCars() {
-        cars.moveOrStopCars();
     }
 
     private void validateIterationCount(int iterationCount) {
--- src/test/java/study/carrace/step3/application/RaceManagerTest.java
+++ src/test/java/study/carrace/step3/application/RaceManagerTest.java
@@ -1,7 +1,9 @@
 package study.carrace.step3.application;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import study.carrace.step3.domain.CarName;
+import study.carrace.step3.domain.Cars;
 import study.carrace.step3.domain.MoveStrategy;
 import study.carrace.step3.domain.exception.IllegalIterationCountException;
 
@@ -16,28 +18,9 @@ class RaceManagerTest {
         int iterationCount = -1;
 
         // when, then
-        assertThatThrownBy(() -> new RaceManager(null, null, iterationCount))
+        assertThatThrownBy(() -> new RaceManager(List.of("foo", "bar"), mockMoveStrategy(true), iterationCount))
                 .isInstanceOf(IllegalIterationCountException.class)
                 .hasMessage("시도 횟수는 한번 이상이여야 합니다: " + iterationCount);
-    }
-
-    @Test
-    void cars_position_history() {
-        // given
-        List<String> carNames = List.of("foo", "bar");
-        RaceManager raceManager = new RaceManager(carNames, mockMoveStrategy(true), 2);
-
-        // when
-        raceManager.startRace();
-
-        // then
-        String expected = new StringBuilder()
-                .append("foo : -\n")
-                .append("bar : -\n\n")
-                .append("foo : --\n")
-                .append("bar : --\n\n")
-                .toString();
-        assertThat(raceManager.carsPositionHistory()).isEqualTo(expected);
     }
 
     @Test
@@ -51,6 +34,24 @@ class RaceManagerTest {
 
         // then
         assertThat(raceManager.winners()).containsExactly(new CarName("foo"), new CarName("bar"));
+    }
+
+    @DisplayName("startRace() 메소드 호출 시, 레이스가 완료된 Cars 객체 반환")
+    @Test
+    void start_race() {
+        // given
+        List<String> carNames = List.of("foo", "bar");
+        MoveStrategy mockMoveStrategy = mockMoveStrategy(true);
+        int iterationCount = 1;
+
+        RaceManager raceManager = new RaceManager(carNames, mockMoveStrategy, iterationCount);
+
+        // when
+        Cars cars = raceManager.startRace();
+
+        // then
+        Cars movedOrStoppedCars = new Cars(carNames, mockMoveStrategy).moveOrStopCars(iterationCount);
+        assertThat(cars).isEqualTo(movedOrStoppedCars);
     }
 
     private MoveStrategy mockMoveStrategy(boolean movable) {
