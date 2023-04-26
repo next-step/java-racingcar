78ac76d84a6bbf28d1b152a05f0b5eaef966caac
letitgobaby <48303144+letitgobaby@users.noreply.github.com>
2023-04-26 22:23:53 +0900

    Step4 자동차 경주 (우승자) (#4588)
    * refactor : TestCarRaceSimple 클래스 테스트 설명 변경
    
    * doc : Readme에 step4 요구사항 추가
    
    * feat : Car 객체 이름 상태값 추가 & 테스트 코드
    
    * style : 코드 정리
    
    * feat : Car 이름 쉼표 구분 및 이름 같이 출력
    
    * feat : CarRace 우승자 상태값 추가
    
    * feat : ResultView 우승자 UI 추가
    
    * refactor : Car 객체의 이름 유효성 검사 수정
    
    * refactor : Car 객체 생성자 및 테스트 케이스 수정
    
    * style : 코드정리
    
    * refactor : for문 변경
    
    * refactor : CarRace 스코어, 우승자 구하는 메서드 분리
    
    * feat : Car 객체 생성 테스트 코드 추가
    
    * refactor : masScore 확인하는 메서드 이름 변경
    
    ---------
    
    Co-authored-by: letitgobaby <gnu@gnuui-MacBookPro.local>

--- src/test/java/study/race/TestCar.java
+++ src/test/java/study/race/TestCar.java
@@ -1,10 +1,13 @@
 package study.race;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 
@@ -14,18 +17,49 @@ public class TestCar {
 
     @BeforeEach
     void setUp() {
-        car = new Car();
+        car = new Car("test");
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "Car 객체의 전진 테스트")
     @ValueSource(ints = {7, 2, 3})
-    @DisplayName("Car 객체의 전진 테스트")
     public void test_car_move(int moveNum) {
         for (int i = 0; i < moveNum; i++) {
             car.move();
         }
 
         assertThat(car.getScore()).isEqualTo(moveNum);
+    }
+
+
+    @ParameterizedTest(name = "Car 객체 생성 테스트")
+    @ValueSource(strings = {"car1", "car2", "car3"})
+    public void test_car_name(String name) {
+        Car car = new Car(name);
+
+        assertThat(car.getName()).isEqualTo(name);
+    }
+
+
+    @ParameterizedTest(name = "Car 객체 생성 테스트 - 이름 Null 유효성")
+    @NullAndEmptySource
+    public void test_car_name_empty(String testCarName) {
+        Throwable emptyException = assertThrows(IllegalStateException.class, () -> {
+            new Car(testCarName);
+        });
+
+        assertThat(emptyException.getMessage()).isEqualTo("자동차 이름을 입력해주세요.");
+    }
+
+    
+    @Test
+    @DisplayName("Car 객체 생성 테스트 - 이름 최대 길이 유효성")
+    public void test_car_name_max_length() {
+        String testCarName = "123456";
+        IllegalStateException maxLengthException = assertThrows(IllegalStateException.class, () -> {
+            new Car(testCarName);
+        });
+
+        assertThat(maxLengthException.getMessage()).isEqualTo("자동차 이름은 1~5자만 가능합니다.");
     }
 
 }
--- src/test/java/study/race/TestCarRaceSimple.java
+++ src/test/java/study/race/TestCarRaceSimple.java
@@ -3,8 +3,6 @@ package study.race;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -28,10 +26,15 @@ public class TestCarRaceSimple {
         }
     }
 
+    private String[] splitForTest(String givenString) {
+        String splitChar = ",";
+        return givenString.split(splitChar);
+    }
+
 
     @Test
-    @DisplayName("CarRace 조건 미충족 테스트")
-    public void test_race_start_fail() {
+    @DisplayName("CarRace 초기화 후 ready() 함수 미실행 테스트")
+    public void test_race_ready_fail() {
         RaceConditionNumber successNumber = new SuccessCondition();
         CarRace race = new CarRace(successNumber);
 
@@ -41,34 +44,74 @@ public class TestCarRaceSimple {
     }
 
 
-    @ParameterizedTest
-    @CsvSource({"2, 4", "4, 5", "3, 6"})
-    @DisplayName("CarRace 입력 CarEntry 개수 입력 테스트")
-    public void test_carrace_carNum_tryNum_input(int entryNum, int raceNum) {
+    @ParameterizedTest(name = "CarRace 입력 CarEntry 개수 입력 테스트")
+    @CsvSource({
+        "'pobi,honux', 4", 
+        "'pobi,crong,honux,elsa', 5", 
+        "'pobi,crong,honux', 6"
+    })
+    public void test_carrace_carNum_tryNum_input(String entryNum, int raceNum) {
+        String[] entryNums = splitForTest(entryNum);
+
         RaceConditionNumber successNumber = new SuccessCondition();
         CarRace race = new CarRace(successNumber);
-        race.ready(entryNum, raceNum);
+        race.ready(entryNums, raceNum);
 
-        assertThat(race.getRaceResult().size()).isEqualTo(entryNum);
+        assertThat(race.getRaceResult().size()).isEqualTo(entryNums.length);
     }
 
 
-    @ParameterizedTest
-    @CsvSource({"2, 4", "4, 5", "3, 6"})
-    @DisplayName("CarRace 실행 테스트")
-    public void test_carrace_run(int entryNum, int raceNum) {
+    @ParameterizedTest(name = "CarRace 실행 테스트")
+    @CsvSource({
+        "'pobi,honux', 4", 
+        "'pobi,crong,honux,elsa', 5", 
+        "'pobi,crong,honux', 6"
+    })
+    public void test_carrace_run(String entryNum, int raceNum) {
+        String[] entryNums = splitForTest(entryNum);
+
         RaceConditionNumber failNumber = new FailCondition();
         CarRace race = new CarRace(failNumber);
-        race.ready(entryNum, raceNum);
-
+        race.ready(entryNums, raceNum);
         race.run();
 
-        List<CarEntry> entrys = race.getRaceResult();
-        assertThat(entrys.size()).isEqualTo(entryNum);
-
-        for (CarEntry entry : entrys) {
+        for (CarEntry entry : race.getRaceResult()) {
             assertThat(entry.getMoveRecord().size()).isEqualTo(raceNum);
         }
+    }
+
+
+    @Test
+    @DisplayName("CarRace run() 함수 미실행 우승자 테스트")
+    public void test_carrace_not_run_test() {
+        int raceNum = 4;
+        String[] entryNums = splitForTest("pobi,honux");
+
+        RaceConditionNumber successNumber = new SuccessCondition();
+        CarRace race = new CarRace(successNumber);
+        race.ready(entryNums, raceNum);
+
+        assertThrows(IllegalStateException.class, () -> {
+            race.winner();
+        });
+    }
+
+
+    @ParameterizedTest(name = "CarRace 우승자 테스트")
+    @CsvSource({
+        "'pobi,honux', 4",
+        "'pobi,crong,honux,elsa', 5", 
+        "'pobi,crong,honux', 6"
+    })
+    public void test_carrace_winner(String entryNum, int raceNum) {
+        String[] entryNums = splitForTest(entryNum);
+
+        RaceConditionNumber successNumber = new SuccessCondition();
+        CarRace race = new CarRace(successNumber);
+        race.ready(entryNums, raceNum);
+        race.run();
+
+        assertThat(race.winner().size()).isEqualTo(entryNums.length);
     }
 
 }
