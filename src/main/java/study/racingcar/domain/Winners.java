5f333c5c45327e8c4290ee9515933f4e4332bff4
juno-junho <ssmm0205@naver.com>
2023-04-26 22:01:21 +0900

    feat(racingcar): winners 불변 객체 생성

---
+++ src/main/java/study/racingcar/domain/Winners.java
@@ -0,0 +1,25 @@
+package study.racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Winners {
+    private final List<Car> winners;
+
+    public Winners() {
+        this.winners = new ArrayList<>();
+    }
+
+    public Winners(List<Car> winners) {
+        this.winners = List.copyOf(winners);
+    }
+
+    public void add(Car car) {
+        this.winners.add(car);
+    }
+
+    public List<Car> getWinners() {
+        return Collections.unmodifiableList(winners);
+    }
+}
