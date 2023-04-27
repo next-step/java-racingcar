b42a5b9dbcacd1e6d463f9cef48b6b4f1141af9e
Yeojin Kim <yeojin.dev@gmail.com>
2023-04-28 01:47:26 +0900

    fix: delete a redundant test

--- src/test/java/racing/race/GameTest.java
+++ src/test/java/racing/race/GameTest.java
@@ -9,14 +9,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class GameTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"1,1", "2,2", "3,2"})
-    public void validGame(int participantsCount, int laps) {
-        Game game = new Game(participantsCount, laps);
-        assertThat(game.participantsStream()).hasSize(participantsCount);
-        assertThat(game.lapsStream()).hasSize(laps);
-    }
-
-    @ParameterizedTest
     @CsvSource(value = {"1,0", "0,2", "-1, 0", "0, -2"})
     public void invalidGame(int participantsCount, int laps) {
         assertThatThrownBy(() -> new Game(participantsCount, laps))
