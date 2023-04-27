fa6cab02c91956dc48daafdef1d2ec9c1090387b
Yeojin Kim <yeojin.dev@gmail.com>
2023-04-28 03:48:16 +0900

    feat: add displayCarsPosition()

--- src/main/java/racing/ui/DisplayView.java
+++ src/main/java/racing/ui/DisplayView.java
@@ -1,8 +1,11 @@
 package racing.ui;
 
+import racing.vehicle.Cars;
+
 public class DisplayView implements View {
 
     public final String RACE_START_MESSAGE = "실행 결과";
+    public final String UNIT_DISPLAYING_LABS_OF_CAR = "-";
 
     @Override
     public void display(String content) {
@@ -15,5 +18,9 @@ public class DisplayView implements View {
 
     public void displayRaceStart() {
         this.display(RACE_START_MESSAGE);
+    }
+
+    public void displayCarsPosition(Cars cars) {
+        cars.getCarPositions().stream().map(UNIT_DISPLAYING_LABS_OF_CAR::repeat).forEach(this::display);
     }
 }
