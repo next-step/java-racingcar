7f3f26e5b5092becb57780347b66bd305774640b
ethan.v <ethan.v@kakaocorp.com>
2023-04-28 17:17:33 +0900

    [lotto] step2 - 피드백 적용
    - TC 수정

--- src/test/java/lotto/step2/domain/LottoCountTest.java
+++ src/test/java/lotto/step2/domain/LottoCountTest.java
@@ -15,7 +15,7 @@ class LottoCountTest {
     }
 
     @Test
-    @DisplayName("구매금액을 입력해서 몇장의 로또를 구매할 수 있는지(1000원 이하일 때)")
+    @DisplayName("1000원 이하의 구매금액을 입력하면 예외 throw")
     void failLottoCount() {
         assertThatThrownBy(() -> new LottoCount(500)).isInstanceOf(IllegalArgumentException.class);
     }
