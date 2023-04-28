096c6c4644ef23fb6175fcf683320be1a3d002c6
poistargram <poi.stargram@gmail.com>
2023-04-28 18:38:47 +0900

    [refactor] 출력 관련 로직 static으로 전환

--- src/main/java/racingcar/printer/Printer.java
+++
@@ -1,8 +0,0 @@
-package racingcar.printer;
-
-import racingcar.Race;
-
-public interface Printer {
-
-    void print(Race race);
-}
---
+++ src/main/java/racingcar/printer/RacePrinter.java
@@ -0,0 +1,31 @@
+package racingcar.printer;
+
+import racingcar.car.Car;
+
+import java.util.List;
+
+public class RacePrinter {
+
+    private static String STATE = "-";
+
+    public static void printRaceStart(){
+        System.out.println("실행결과");
+    }
+    public static void printPerRound(List<Car> cars){
+        for(Car car : cars){
+            System.out.println(car.getName()+" : "+STATE.repeat(car.getState()));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<Car> cars){
+        StringBuilder buffer = new StringBuilder();
+        for(Car car : cars){
+            buffer.append(car.getName());
+            buffer.append(", ");
+        }
+
+        buffer.delete(buffer.length()-2, buffer.length()-1);
+        System.out.println(buffer +"가 최종 우승하셨습니다.");
+    }
+}
