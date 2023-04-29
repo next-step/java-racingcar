d185a8044abf4245c679ce98bc038db095328ad7
dhkimneer <kimds989@naver.com>
2023-04-29 20:32:18 +0900

    chore: update constructor of Car domain and move its domain to domain folder

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,29 +1,20 @@
-package racingcar;
+package racingcar.domain;
 
 public class Car {
 
-    private static final int FIVE = 5;
-    private static final String CANNOT_ABOVE_FIVE_LETTERS = "자동차 이름은 5자를 초과할 수 없습니다.";
     private int moveCount = 0;
-    private String carName;
+    private final CarName carName;
 
-    public Car(String carName) {
+    public Car(CarName carName) {
         this.carName = carName;
-        isAboveFiveLetters();
     }
 
-    public String getCarName() {
+    public CarName getCarName() {
         return this.carName;
     }
 
     public int getMoveCount() {
         return moveCount;
-    }
-
-    private void isAboveFiveLetters() {
-        if (carName.length() > FIVE) {
-            throw new IllegalArgumentException(CANNOT_ABOVE_FIVE_LETTERS);
-        }
     }
 
     public void move(MovingStrategy movingStrategy) {
