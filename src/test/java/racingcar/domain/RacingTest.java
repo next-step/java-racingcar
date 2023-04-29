1ca05946a4ee668005482be4a3a6d889677b352d
dhkimneer <kimds989@naver.com>
2023-04-29 20:45:00 +0900

    test: Move RacingTest to domain folder

--- src/test/java/racingcar/domain/RacingTest.java
+++ src/test/java/racingcar/domain/RacingTest.java
@@ -1,13 +1,9 @@
-package racingcar;
+package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.domain.Car;
-import racingcar.domain.ForceMovingStrategy;
-import racingcar.domain.Racing;
-import racingcar.domain.RandomNumberGenerator;
 
 import java.util.List;
 
