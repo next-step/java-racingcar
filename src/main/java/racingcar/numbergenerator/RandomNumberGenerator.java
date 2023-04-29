81c6fc641ca5498559ff4f14ae83a90f966ed700
poistargram <poi.stargram@gmail.com>
2023-04-29 12:13:23 +0900

    refactor: Random 클래스 static으로 변경

--- src/main/java/racingcar/numbergenerator/RandomNumberGenerator.java
+++ src/main/java/racingcar/numbergenerator/RandomNumberGenerator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class RandomNumberGenerator implements NumberGenerator{
 
     private final static int BOUND_NUMBER = 10;
-    private final Random random = new Random();
+    private final static Random random = new Random();
     @Override
     public int generate() {
         return random.nextInt(BOUND_NUMBER);
