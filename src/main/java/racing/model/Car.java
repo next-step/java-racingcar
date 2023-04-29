f60152080d994db6489de0fda30dc8d910aa2ea3
KangHo-dev <tiger452003@gmail.com>
2023-04-30 02:02:02 +0900

    feat : 자동차 경주 우승자 표시

--- src/main/java/racing/model/Car.java
+++ src/main/java/racing/model/Car.java
@@ -3,35 +3,45 @@ package racing.model;
 import racing.model.move.MoveStrategy;
 import racing.util.RandomUtil;
 
-public class Car {
+public class Car implements Comparable<Car> {
 
     private String name;
-    private int position;
+    private Position position;
 
     public Car() {
+        this.position = new Position();
     }
 
     public Car(String name) {
         this.name = name;
-        this.position = 0;
+        this.position = new Position();
     }
 
     public Car(String name, int position) {
         this.name = name;
-        this.position = position;
+        this.position = new Position(position);
     }
 
     public void move(MoveStrategy moveStrategy) {
         if (moveStrategy.move()) {
-            position++;
+            position.move();
         }
     }
 
     public int getPosition() {
-        return position;
+        return position.getPosition();
     }
 
     public String getName() {
         return name;
+    }
+
+    public boolean isSamePosition(int position) {
+        return this.position.getPosition() == position;
+    }
+
+    @Override
+    public int compareTo(Car car) {
+        return (this.position.getPosition() > car.getPosition()) ? 1 : -1;
     }
 }
--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -1,6 +1,7 @@
 package racing.view;
 
 import racing.model.Car;
+import racing.model.Cars;
 
 import java.util.List;
 
@@ -21,6 +22,18 @@ public class ResultView {
         System.out.println("실행 결과");
     }
 
+    public static void printRoundWinners(Cars cars) {
+        List<Car> winners = cars.findWinners();
+
+        int count = winners.size();
+
+        for (Car winner : winners) {
+            printWinnersName(winner, --count);
+        }
+
+        printWinnersMessageSuffix();
+    }
+
     private static void printDash(Car car) {
         int num = car.getPosition();
         for (int i = 0; i < num; i++) {
@@ -31,5 +44,17 @@ public class ResultView {
     private static void printName(Car car) {
         String name = car.getName();
         System.out.print(name + " : ");
+    }
+
+    private static void printWinnersName(Car winner, int count) {
+        System.out.printf(winner.getName());
+
+        if (count != 0) {
+            System.out.printf(", ");
+        }
+    }
+
+    private static void printWinnersMessageSuffix() {
+        System.out.printf("가 우승했습니다.");
     }
 }
