718dddeff8ee680eb52d5da886e3edbf3653e7bb
poistargram <poi.stargram@gmail.com>
2023-04-29 12:12:30 +0900

    refactor: RandomNumberGenerator 바운드 넘버 상수로 변경

--- src/main/java/racingcar/numbergenerator/RandomNumberGenerator.java
+++ src/main/java/racingcar/numbergenerator/RandomNumberGenerator.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator{
 
+    private final static int BOUND_NUMBER = 10;
     private final Random random = new Random();
     @Override
     public int generate() {
-        return random.nextInt(10);
+        return random.nextInt(BOUND_NUMBER);
     }
 }
