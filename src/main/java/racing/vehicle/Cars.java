8d1bba7937314d22566f338a63f8f364d095f414
Yeojin Kim <yeojin.dev@gmail.com>
2023-04-30 15:13:23 +0900

    feat: update Cars to use CarNames

--- src/main/java/racing/vehicle/Cars.java
+++ src/main/java/racing/vehicle/Cars.java
@@ -3,34 +3,40 @@ package racing.vehicle;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Cars {
 
     private final List<Car> cars;
 
-    public Cars(int number) {
-        validateNumber(number);
-        cars = createRandomCars(number);
+    public Cars(CarNames carNames) {
+        validateCarNames(carNames);
+        cars = createRandomCars(carNames);
     }
 
-    private void validateNumber(int number) {
-        if (number <= 0) {
+    private void validateCarNames(CarNames carNames) {
+        if (carNames.getSize() == 0) {
             throw new IllegalArgumentException("The number of participants must be positive.");
         }
     }
 
-    private List<Car> createRandomCars(int number) {
-        return IntStream.range(0, number)
-                .mapToObj(i -> Car.createRandomCar())
-                .collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+    private List<Car> createRandomCars(CarNames carNames) {
+        return carNames.getNames().stream()
+                .map(Car::createRandomCar)
+                .collect(Collectors.toUnmodifiableList());
     }
 
     public void move() {
         cars.forEach(Car::move);
     }
 
-    public List<Integer> getCarPositions() {
-        return cars.stream().map(Car::getCurrentPosition).collect(Collectors.toList());
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
+    }
+
+    public List<Car> getWinners() {
+        int maxPosition = Collections.max(this.cars).getCurrentPosition();
+        return cars.stream()
+                .filter(car -> car.isAt(maxPosition))
+                .collect(Collectors.toUnmodifiableList());
     }
 }
--- src/test/java/racing/vehicle/CarsTest.java
+++ src/test/java/racing/vehicle/CarsTest.java
@@ -1,18 +1,33 @@
 package racing.vehicle;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 
-import java.util.Collections;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
+    static Stream<Arguments> carNames() {
+        return Stream.of(
+                Arguments.arguments(new CarNames("car1,car2,car3"))
+        );
+    }
 
-    @ParameterizedTest
-    @CsvSource(value = {"1", "2", "3"})
-    public void getCarPositions(int number) {
-        Cars cars = new Cars(number);
-        assertThat(cars.getCarPositions()).isEqualTo(Collections.nCopies(number, 1 ));
+    @ParameterizedTest(name = "Creating cars with carNames - {0}")
+    @MethodSource("carNames")
+    public void createCarsWithValidCarNames(CarNames carNames) {
+        Cars cars = new Cars(carNames);
+        assertThat(cars.getCars()).hasSize(carNames.getSize());
+    }
+
+    @ParameterizedTest(name = "Creating cars with null carNames - {0}")
+    @NullSource
+    public void createCarsWithNullCarNames(CarNames carNames) {
+        assertThatThrownBy(() -> new Cars(carNames))
+                .isInstanceOf(RuntimeException.class);
     }
 }
