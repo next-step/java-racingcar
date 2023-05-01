335ddf9bea95cd24d838ad7abc027b0db69c0565
Dale <steady8@kakao.com>
2023-05-01 16:52:49 +0900

    3단계 refactor: 랜덤값 4이상인 경우의 제약조건을 RacingService가 역할을 맡도록 수정.

--- src/main/java/study/RacingCar.java
+++ src/main/java/study/RacingCar.java
@@ -4,11 +4,8 @@ public class RacingCar {
 
     private int position = 1;
 
-    public void moveForward(int randomNumberZeroToNine) {
-        if (randomNumberZeroToNine >= 4) {
-            this.position++;
-        }
-
+    public void moveForward() {
+        this.position++;
     }
 
     public int position() { // getPosition의 getter이름이 아닐 뿐이지, 캡슐화를 위배하는 것은 똑같지 않나요..?
