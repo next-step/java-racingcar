bdfa599b2c4a546cf97485b4b63e5f7d543ecc40
Dale <steady8@kakao.com>
2023-05-01 16:54:53 +0900

    3단계 feat: 요구사항 코드 구현.

--- src/main/java/study/ResultView.java
+++ src/main/java/study/ResultView.java
@@ -1,32 +1,28 @@
 package study;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ResultView {
     public static void main(String[] args) {
+
+        startRacing();
+    }
+
+
+    public static void startRacing() {
+        RacingService racingService = new RacingService();
         Scanner scan = new Scanner(System.in);
 
         System.out.println("자동차의 대수는 몇 대 인가요?");
-        int carCount = scan.nextInt();
+        int carQuantity = scan.nextInt();
 
         System.out.println("시도할 회수는 몇 회 인가요?");
         int tryCount = scan.nextInt();
 
         System.out.println("실행 결과");
+        List<RacingCar> racingCars = racingService.createRacingCars(carQuantity);
 
+        racingService.startRacingGame(tryCount, racingCars);
     }
-
-
-
-//    public void startRacing() {
-//        Scanner scan = new Scanner(System.in);
-//
-//        System.out.println("자동차의 대수는 몇 대 인가요?");
-//        int carCount = scan.nextInt();
-//
-//        System.out.println("시도할 회수는 몇 회 인가요?");
-//        int tryCount = scan.nextInt();
-//
-//        System.out.println("실행 결과");
-//    }
 }
