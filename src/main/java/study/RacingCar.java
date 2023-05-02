2ced1b31012b03c4fe272f3521e2f599892a535f
Dale <steady8@kakao.com>
2023-05-02 16:04:08 +0900

    3단계 refactor: position 0으로 변경 후 전진의 역할을 RacingCar에 위임.

--- src/main/java/study/RacingCar.java
+++ src/main/java/study/RacingCar.java
@@ -2,13 +2,15 @@ package study;
 
 public class RacingCar {
 
-    private int position = 1;
-
-    public void moveForward() {
-        this.position++;
-    }
+    private int position = 0;
 
     public int position() { // getPosition의 getter이름이 아닐 뿐이지, 캡슐화를 위배하는 것은 똑같지 않나요..?
         return this.position;
+    }
+
+    public void moveForwardWhenFourHigher() {
+        if (RandomNumberUtils.getRandomNumberZeroToNine() >= 4) {
+            position++;
+        }
     }
 }
