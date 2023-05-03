d01577cbeaa9507d2574eae460983d29dad2c3d3
Dale <steady8@kakao.com>
2023-05-03 20:59:01 +0900

    3단계 refactor: private final 추가

--- src/main/java/study/RacingCars.java
+++ src/main/java/study/RacingCars.java
@@ -6,18 +6,17 @@ import java.util.stream.IntStream;
 
 public class RacingCars {
 
-    List<RacingCar> racingCars;
+    private final List<RacingCar> racingCars;
 
     public RacingCars(int quantity) {
 
-        List<RacingCar> racingCars = new ArrayList<>();
+        racingCars = new ArrayList<>();
 
         IntStream.range(0, quantity).forEach(i -> {
             RacingCar racingCar = new RacingCar();
             racingCars.add(racingCar);
         });
 
-        this.racingCars = racingCars;
     }
 
     public List<RacingCar> getRacingCars() {
