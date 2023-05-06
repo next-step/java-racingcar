ae9fbdf319506a498c70160cd2f19e76c7258572
hyeyoungs <sunzero0116@gmail.com>
2023-05-06 20:49:04 +0900

    3단계 - 자동차 경주 | feat : 자동차를 조건에 따라 전진 또는 멈춤을 위한 Car 생성

---
+++ src/test/java/step3/CarTest.java
@@ -0,0 +1,23 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class CarTest {
+    @DisplayName("각각의 차가 전진했을 때, 전진한 정도가 맞는지 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 10})
+    public void plusTraceTest(int count) {
+        Car car = new Car();
+        int expected = 0;
+        for (int i = 0; i < count; i++) {
+            car.plusTrace();
+            expected += 1;
+        }
+
+        assertThat(car.getTrace()).isEqualTo(expected);
+    }
+}
