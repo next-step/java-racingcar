58956df306fd57dc7806de5a79bae062d4588403
jm.hong <jm.hong@kakaocorp.comm>
2023-05-06 20:17:21 +0900

    refactor

--- src/main/java/study/carrace/Main.java
+++ src/main/java/study/carrace/Main.java
@@ -1,20 +1,13 @@
 package study.carrace;
 
-import study.view.Input;
 import study.view.InputImpl;
-import study.util.Randomizer;
 import study.util.RandomizerImpl;
-import study.view.Result;
 import study.view.ResultImpl;
 
 public class Main {
 
   public static void main(String[] args) {
-    Input carInput = new InputImpl();
-    Input countInput = new InputImpl();
-    Randomizer randomizer = new RandomizerImpl();
-    Result result = new ResultImpl();
-    CarRace carRace = new CarRaceImpl(carInput, countInput, randomizer, result);
-    String output = carRace.run();
+    CarRace carRace = new CarRaceImpl(new InputImpl(), new InputImpl(), new RandomizerImpl(), new ResultImpl());
+    carRace.run();
   }
 }
