cc3da3e9d42806ff8ac029e6a7ddebafb489e0ae
hyeyoungs <sunzero0116@gmail.com>
2023-05-06 20:05:41 +0900

    3단계 - 자동차 경주 | feat : 입력 받기 위한 InputView 생성

---
+++ src/main/java/step3/RacingGame.java
@@ -0,0 +1,10 @@
+package step3;
+
+public class RacingGame {
+    private final InputView inputView = new InputView();
+
+    public void play() {
+        int numOfCar = inputView.getNumOfCar();
+        int numOfTrial = inputView.getNumOfTrial();
+    }
+}
