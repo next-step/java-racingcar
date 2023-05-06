fd3a37a14bee9d82222a505132fa25e8ef21d461
hyeyoungs <sunzero0116@gmail.com>
2023-05-06 21:38:43 +0900

    feat : 3단계 - 자동차 경주 | 자동차 경주 게임 Main 기능 완성

--- src/main/java/step3/RacingGame.java
+++ src/main/java/step3/RacingGame.java
@@ -1,10 +1,45 @@
 package step3;
 
-public class RacingGame {
-    private final InputView inputView = new InputView();
+import java.util.ArrayList;
+import java.util.List;
 
-    public void play() {
+public class RacingGame {
+    private static final InputView inputView = new InputView();
+    private static final ResultView resultView = new ResultView();
+    public static final List<Car> carList = new ArrayList<>();
+
+    public static void main(String[] args) {
         int numOfCar = inputView.getNumOfCar();
         int numOfTrial = inputView.getNumOfTrial();
+
+        resultView.printResultMessage();
+
+        makeCars(numOfCar);
+        for (int i = 0; i < numOfTrial; i++) {
+            race();
+        }
+    }
+
+    public static void makeCars(int numOfCar) {
+        for (int i = 0; i < numOfCar; i++) {
+            carList.add(new Car());
+        }
+    }
+
+    public static void race() {
+        raceEachCar(carList);
+        printResultEachCar(carList);
+    }
+
+    public static void raceEachCar(List<Car> carList) {
+        for (Car car : carList) {
+            car.go();
+        }
+    }
+
+    public static void printResultEachCar(List<Car> carList) {
+        for (Car car : carList) {
+            resultView.printTraceEachCar(car.getTrace());
+        }
     }
 }
--- src/test/java/step3/RacingGameTest.java
+++ src/test/java/step3/RacingGameTest.java
@@ -1,13 +1,20 @@
 package step3;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 public class RacingGameTest {
 
     private final RacingGame racingGame = new RacingGame();
 
-    @Test
-    void playTest() {
-        racingGame.play();
+    @DisplayName("입력 받은 값 만큼 차를 생성하는지 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 10})
+    void makeCarTest(int numOfCar) {
+        RacingGame.makeCars(numOfCar);
+        assertThat(RacingGame.carList).hasSize(numOfCar);
     }
 }
