e98838263b870e1da7c8e03300bf6db1b4af3bde
Yeojin Kim <yeojin.dev@gmail.com>
2023-05-06 13:37:07 +0900

    test: Game.Builder

--- src/test/java/racing/race/GameTest.java
+++ src/test/java/racing/race/GameTest.java
@@ -1,11 +1,18 @@
 package racing.race;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import racing.ui.DisplayView;
+import racing.vehicle.Car;
+import racing.vehicle.CarName;
+import racing.vehicle.CarNames;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GameTest {
@@ -21,7 +28,31 @@ class GameTest {
     @ParameterizedTest
     @MethodSource("invalidGames")
     public void invalidGame(String carNamesMessage, int laps) {
-        assertThatThrownBy(() -> new Game(carNamesMessage, laps))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Game.builder()
+                .setCarNames(carNamesMessage)
+                .setLaps(laps)
+                .build()
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void builder() {
+        // given
+        String carNames = "car1,car2,car3";
+        int laps = 5;
+
+        // when
+        Game game = Game.builder()
+                .setCarNames(carNames)
+                .setLaps(laps)
+                .build();
+
+        // then
+        CarNames expectedCarNames = new CarNames(carNames);
+        assertThat(game.getParticipants().getCars().stream()
+                .map(Car::getName)
+                .collect(Collectors.toList())
+        ).isEqualTo(expectedCarNames.getNames());
+        assertThat(game.getLaps()).isEqualTo(laps);
     }
 }
