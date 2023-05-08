c2519ad7578d619eb186bbc2cd29321be44e67c0
ynoolee <heerarea129@gmail.com>
2023-05-08 20:33:35 +0900

    refactor: Random 객체를 단 한 번만 생성하도록 한다
    - 이전 : generate가 호출될 때 마다 새로운 Random 객체를 생성하고 있었다
    - 변경 : Random 객체는 RandomGenerator 생성 시점에 단 한번만 생성되도록 하고 이를 재사용한다. 이는 Random 객체에 대한 문서 상에서 해당 인스턴스가 스레드 세이프 하기 때문에 가능하다

--- src/main/java/racingcar/domain/random/RandomGenerator.java
+++ src/main/java/racingcar/domain/random/RandomGenerator.java
@@ -5,17 +5,17 @@ import java.util.Random;
 public class RandomGenerator {
     private final int randomMin;
     private final int randomMax;
+    private final Random random;
 
     public RandomGenerator(int randomMin, int randomMax) {
         this.randomMin = randomMin;
         this.randomMax = randomMax;
+        this.random = new Random();
     }
 
     public RandNum generate() {
-        Random random = new Random();
-
         int range = randomMax - randomMin + 1;
 
-        return new RandNum(random.nextInt(range) + randomMin);
+        return new RandNum(this.random.nextInt(range) + randomMin);
     }
 }
