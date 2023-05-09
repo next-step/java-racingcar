9872d3fac4092ed18d9203d6502bbdab1eb18461
hyeyoungs <sunzero0116@gmail.com>
2023-05-09 17:53:41 +0900

    3단계 - 자동차 경주 | refactor : 상수로 처리해야 할 값들을 변수에서 상수로 변경

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -3,9 +3,9 @@ package step3;
 import java.util.Scanner;
 
 public class InputView {
-    private Scanner scanner = new Scanner(System.in);
-    private String ASK_QUESTION_FOR_A_NUM_OF_CAR = "자동차 대수는 몇 대 인가요?";
-    private String ASK_QUESTION_FOR_A_NUM_OF_TRIAL = "시도할 회수는 몇 회 인가요?";
+    private static Scanner scanner = new Scanner(System.in);
+    private static String ASK_QUESTION_FOR_A_NUM_OF_CAR = "자동차 대수는 몇 대 인가요?";
+    private static String ASK_QUESTION_FOR_A_NUM_OF_TRIAL = "시도할 회수는 몇 회 인가요?";
 
     public int getNumOfCar() {
         System.out.println(ASK_QUESTION_FOR_A_NUM_OF_CAR);
