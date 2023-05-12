c013cb437282f0d675c2f0e335f5417bc80ed706
O_jeegu <lumirite0@gmail.com>
2023-05-12 13:08:37 +0900

    test : drive 메서드가 제대로 동작하는지 확인
    Car 클래스의 drice 메서드가 제대로 동작하는지 테스트 코드를 통해 확인.

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step3.domain.Car;
-import step3.domain.CarOperator;
 import step3.util.NumberGenerator;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+//todo : CarOperator.drivingResult 테스트 코드 어떻게 짤 지 고민하기
 public class CarTest {
 
     @Test
@@ -23,27 +23,26 @@ public class CarTest {
     @Test
     @DisplayName("랜덤 숫자가 4 이상 나왔을 때만 true 반환하는지 확인")
     public void checkMoveState() {
-        Assertions.assertTrue(CarOperator.willMoving(4));
-        Assertions.assertTrue(CarOperator.willMoving(9));
-        Assertions.assertFalse(CarOperator.willMoving(3));
-        Assertions.assertFalse(CarOperator.willMoving(0));
+        Assertions.assertTrue(Car.willMoving(9));
+        Assertions.assertTrue(Car.willMoving(4));
+        Assertions.assertFalse(Car.willMoving(3));
+        Assertions.assertFalse(Car.willMoving(0));
     }
 
 
     @Test
-    @DisplayName("조건이 true인 경우 해당 자동차에만 '-'가 추가 되었는지 확인")
+    @DisplayName("조건이 true인 경우 1이 추가되고, 그렇지 않은 경우 0이 추가되는지 확인")
     public void checkGenerateMoving() {
         int numOfCars = 5;
-        int randomNumber = NumberGenerator.makeRandomly();
         List<Car> carList = Car.factory(numOfCars);
 
-        CarOperator.drive(carList);
-
         for (Car car : carList) {
-            if (CarOperator.willMoving(randomNumber)) {
-                Assertions.assertTrue(car.getMovingList().contains("-"));
+            int randomNumber = NumberGenerator.makeRandomly();
+            car.drive(randomNumber);
+            if (Car.willMoving(randomNumber)) {
+                Assertions.assertTrue(car.getMovingList().contains(1));
             } else {
-                Assertions.assertFalse(car.getMovingList().contains("-"));
+                Assertions.assertTrue(car.getMovingList().contains(0));
             }
         }
     }
