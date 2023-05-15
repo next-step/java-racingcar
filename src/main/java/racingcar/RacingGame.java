e9c0f340ab2d878a5a7dcaa1f9c0686185055ac8
IMHYEWON <hyewon1118@gmail.com>
2023-05-15 23:54:19 +0900

    Test: 자동차 경주 클래스 테스트

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -8,7 +8,7 @@ public class RacingGame {
     private static final Random RANDOM = new Random();
     private static final int RANDOM_VALUE = 10;
 
-    private static Car[] createCars(int num) {
+    public static Car[] createCars(int num) {
         Car[] cars = new Car[num];
         for (int i = 0; i < num; i++) {
             cars[i] = new Car();
@@ -32,6 +32,7 @@ public class RacingGame {
 
         for (int i = 0; i < tryCount; i++) {
             playRound(cars);
+            // TODO : 테스트 코드를 작성하다보니 playGame 메소드 내부에서 UI 메소드를 호출하는게 메소드의 책임이 불분명해지는 것 같은데 어느시점에서 UI결과를 호출해야 할지 혼란스러워요
             ResultView.printRound(cars);
         }
     }
