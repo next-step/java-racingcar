431acebf50ab713191b07f9205b28574e63388db
MoonHKLee <mf311110@gmail.com>
2023-05-17 17:39:47 +0900

    refactor(name) : name 생성시 이름 길이 체크 추가.

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -4,6 +4,9 @@ public class Name {
     private final String name;
 
     public Name(String name) {
+        if (name==null || name.isEmpty()) {
+            throw new IllegalArgumentException("자동차의 이름을 입력하세요.");
+        }
         this.name = name;
     }
 
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,10 +3,12 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.Car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarTest {
 
@@ -35,5 +37,14 @@ public class CarTest {
         Car car = new Car("test");
         car.move(number);
         assertThat(car.getPosition()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("자동차의 위치를 가져온다.")
+    void testGetPosition(String name) {
+        assertThatThrownBy(()->new Car(name)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차의 이름을 입력하세요.");
+
     }
 }
