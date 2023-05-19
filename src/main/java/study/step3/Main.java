7b209ad758f53f2fd82ee2f7036466a41a43047e
SanghoCho-pixel <shxho0476@gmail.com>
2023-05-19 20:38:50 +0900

    [refactor] RacingGame객체와 Main객체를 분리

---
+++ src/main/java/study/step3/Main.java
@@ -0,0 +1,26 @@
+package study.step3;
+
+import study.step3.domain.Car;
+import study.step3.dto.RacingInput;
+import study.step3.move.RandomlyMoveStrategy;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        RacingInput input = inputView.enter();
+
+        RacingGame racingGame = new RacingGame(input.getCarNum(), new RandomlyMoveStrategy());
+
+        for (int i = 0; i < input.getTryNum(); i++) {
+            racingGame.play();
+            List<Car> cars = racingGame.getCarList();
+            System.out.println("\n실행 결과");
+            resultView.printCars(cars);
+        }
+
+    }
+}
