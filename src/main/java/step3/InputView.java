3c20cf6774b9c39e84408fa78abd2d1a6abfb3f6
hyeyoungs <sunzero0116@gmail.com>
2023-05-19 20:39:06 +0900

    3단계 - 자동차 경주 피드백 반영 | refactor : Scanner 상수를 대문자로 변경

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -3,13 +3,13 @@ package step3;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner SCANNER = new Scanner(System.in);
     private static String ASK_QUESTION_FOR_A_NUM_OF_CAR = "자동차 대수는 몇 대 인가요?";
     private static String ASK_QUESTION_FOR_A_NUM_OF_TRIAL = "시도할 회수는 몇 회 인가요?";
 
     public static int getNumOfCar() {
         System.out.println(ASK_QUESTION_FOR_A_NUM_OF_CAR);
-        int numOfCar = Integer.parseInt(scanner.nextLine());
+        int numOfCar = Integer.parseInt(SCANNER.nextLine());
         if (numOfCar <= 0) {
             throw new RuntimeException("자동차 대수는 양수여야 합니다.");
         }
@@ -18,7 +18,7 @@ public class InputView {
 
     public static int getNumOfTrial() {
         System.out.println(ASK_QUESTION_FOR_A_NUM_OF_TRIAL);
-        int numOfTrial = Integer.parseInt(scanner.nextLine());
+        int numOfTrial = Integer.parseInt(SCANNER.nextLine());
         if (numOfTrial <= 0) {
             throw new RuntimeException("시도할 횟수는 양수여야 합니다.");
         }
