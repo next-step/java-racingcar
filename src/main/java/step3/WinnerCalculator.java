9cb7848459ae00537257f42989966a7eb1002e03
hyeyoungs <sunzero0116@gmail.com>
2023-05-21 20:11:06 +0900

    4단계 - 자동차 경주(우승자) | feat : 우승자 계산 기능 추가

---
+++ src/main/java/step3/WinnerCalculator.java
@@ -0,0 +1,23 @@
+package step3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinnerCalculator {
+    public static List<Car> getWinners(List<Car> cars) {
+        int maxTrace = getMaxTrace(cars);
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if(car.getTrace() == maxTrace)
+                winners.add(car);
+        }
+        return winners;
+    }
+
+    public static int getMaxTrace(List<Car> cars) {
+        return cars.stream()
+                .mapToInt(Car::getTrace)
+                .max()
+                .orElse(0);
+    }
+}
