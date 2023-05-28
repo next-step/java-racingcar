e9991ad307cb5952fbad30ff54191a5aa6b65ef6
hyeyoungs <sunzero0116@gmail.com>
2023-05-29 00:16:56 +0900

    5단계 - 자동차 경주(리팩토링) | test : NameTest 추가

---
+++ src/test/java/racinggame/NameTest.java
@@ -0,0 +1,24 @@
+package racinggame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racinggame.domain.Name;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NameTest {
+    @Test
+    void create() {
+        Name name = new Name("붕붕이");
+        assertThat(name).isEqualTo(new Name("붕붕이"));
+    }
+
+    @Test
+    @DisplayName("입력받는 이름이 5자 이상이면 에러가 발생")
+    void valid() {
+        assertThatThrownBy(() -> {
+            new Name("helloWorld");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
