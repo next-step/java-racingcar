6ec57c436026be451158196ede41e71d51c576e0
rkd5656meta <rkd8307@naver.com>
2023-06-12 22:59:01 +0900

    refactor: println으로 변경 및 우승자 출력 추가

--- src/main/java/edu/nextstep/camp/racing/view/OutputView.java
+++ src/main/java/edu/nextstep/camp/racing/view/OutputView.java
@@ -1,11 +1,10 @@
 package edu.nextstep.camp.racing.view;
 
 public class OutputView {
-
     protected OutputView() {
     }
 
     public static void print(String msg){
-        System.out.printf(msg);
+        System.out.println(msg);
     }
 }
--- src/main/java/edu/nextstep/camp/racing/view/RacingOutputView.java
+++ src/main/java/edu/nextstep/camp/racing/view/RacingOutputView.java
@@ -2,20 +2,25 @@ package edu.nextstep.camp.racing.view;
 
 import edu.nextstep.camp.racing.domain.Car;
 
+import java.util.List;
+
 public class RacingOutputView extends OutputView{
 
     private static final String POSITION_BAR = "-";
-    private static final String NEXT_LINE = "\n";
+
+    private static final String WINNER_GUIDE_MSG = "(이)가 최종 우승했습니다.";
 
     public static void printCurrentPosition(Car car) {
         String name = car.getName();
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(name)
                 .append(": ")
-                .append(POSITION_BAR.repeat(car.currentPosition()))
-                .append(NEXT_LINE);
+                .append(POSITION_BAR.repeat(car.currentPosition()));
 
         print(stringBuilder.toString());
     }
 
+    public static void printWinners(List<String> winners) {
+        print(String.join(",", winners) + WINNER_GUIDE_MSG);
+    }
 }
