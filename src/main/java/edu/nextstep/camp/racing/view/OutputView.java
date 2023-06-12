79ee8ac67446eebe9eabefc9297ea75fc8a6b8ce
rkd5656meta <rkd8307@naver.com>
2023-06-12 09:50:25 +0900

    feat: OutputView 객체(프린트 단일 기능) 추가

---
+++ src/main/java/edu/nextstep/camp/racing/view/OutputView.java
@@ -0,0 +1,11 @@
+package edu.nextstep.camp.racing.view;
+
+public class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void print(String msg){
+        System.out.printf(msg);
+    }
+}
--- src/test/java/edu/nextstep/camp/testUtils/TestUtils.java
+++ src/test/java/edu/nextstep/camp/testUtils/TestUtils.java
@@ -1,6 +1,9 @@
 package edu.nextstep.camp.testUtils;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 
 public class TestUtils {
@@ -12,4 +15,10 @@ public class TestUtils {
         ByteArrayInputStream in = new ByteArrayInputStream(inputString.getBytes(StandardCharsets.UTF_8));
         System.setIn(in);
     }
+    public static OutputStream getOutputStream() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        return out;
+    }
 }
+
