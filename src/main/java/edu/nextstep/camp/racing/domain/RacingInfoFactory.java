a4c68c81d5afbd03b7062621de506bed72743356
rkd5656meta <rkd8307@naver.com>
2023-06-12 09:48:54 +0900

    feat: RacingInfoFactory 객체 추가
    1. InputView를 통해 return 받은 시도 횟수, 자동차 이름 등을 RacingInfo 객체로 반환하는 책임을 가짐
    
    2. RacingInfo 객체 생성 중 유효성이 안맞을 시 재입력 유도

---
+++ src/main/java/edu/nextstep/camp/racing/domain/RacingInfoFactory.java
@@ -0,0 +1,29 @@
+package edu.nextstep.camp.racing.domain;
+
+import edu.nextstep.camp.racing.view.InputView;
+import edu.nextstep.camp.racing.view.OutputView;
+
+public class RacingInfoFactory {
+    private static String CAR_NAME_INPUT_GUIDE_MSG = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static String MOVING_COUNT_INPUT_GUIDE_MSG = "시도할 회수는 몇회인가요?";
+    public RacingInfo createRacingInfo() {
+        try {
+            return RacingInfo.of( inputCarNames(), inputMovingCount());
+        } catch (RuntimeException e){
+            OutputView.print(e.getMessage());
+            return createRacingInfo();
+        } finally {
+            InputView.close();
+        }
+    }
+
+    private String inputCarNames(){
+        OutputView.print(CAR_NAME_INPUT_GUIDE_MSG);
+        return InputView.input();
+    }
+
+    private String inputMovingCount(){
+        OutputView.print(MOVING_COUNT_INPUT_GUIDE_MSG);
+        return InputView.input();
+    }
+}
