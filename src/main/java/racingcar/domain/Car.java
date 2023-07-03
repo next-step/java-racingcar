42d0596d844f8a8df8218889dcf93a2b15553565
devxb <dlwnsdud205@naver.com>
2023-07-03 17:48:39 +0900

    [feat] : Car 클래스 생성자 null값 검증 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Car {
 
     private final String name;
@@ -10,6 +12,9 @@ public class Car {
     }
 
     private void preAssert(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException("자동차 이름은 null값이 될 수 없습니다.");
+        }
         if (name.length() == 0 || name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 1~5자 사이로 입력해야됩니다. 입력한 값: \"" + name + "\"");
         }
