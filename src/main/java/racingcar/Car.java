4c2dfb43add45f2dc0c592926cc7980f67465f7a
impati <yongs170@naver.com>
2023-07-03 14:53:25 +0900

    feat: 자동차 이름이 5자 초과이면 에러를 던진다

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,8 +3,9 @@ package racingcar;
 public class Car {
 
     private String name;
-    
+
     public Car(String name) {
+        if(name.length() > 5) throw new IllegalArgumentException();
         this.name = name;
     }
 
