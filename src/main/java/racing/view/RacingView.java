732baf2274c8c3be23f53af97abf4ff8ceff59c3
bkRyusim <bkryusim@gmail.com>
2023-07-03 18:58:42 +0900

    feature: RacingView.getAskingCarNameView and getAskingCountView and getResultTitleView

--- src/main/java/racing/view/RacingView.java
+++ src/main/java/racing/view/RacingView.java
@@ -1,15 +1,19 @@
 package racing.view;
 
 public class RacingView {
+    private final static String ASKING_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private final static String ASKING_COUNT = "시도할 회수는 몇회인가요?";
+    private final static String RESULT_TITLE = "실행 결과";
+
     public String getAskingCarNameView() {
-        return null;
+        return ASKING_CAR_NAME;
     }
 
     public String getAskingCountView() {
-        return null;
+        return ASKING_COUNT;
     }
 
     public String getResultTitleView() {
-        return null;
+        return RESULT_TITLE;
     }
 }
