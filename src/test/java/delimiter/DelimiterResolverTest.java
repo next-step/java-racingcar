fc8489ce6714034362d71b6bc4a82d71b6e62360
DAWN <qkgk61@gmail.com>
2023-07-03 15:33:25 +0900

    :umbrella: Test: 구분자를 파싱하는 테스트 코드 작성

---
+++ src/test/java/delimiter/DelimiterResolverTest.java
@@ -0,0 +1,28 @@
+package delimiter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DelimiterResolverTest {
+
+    @Test
+    @DisplayName("커스텀 구분자가 없는 경우")
+    void nonCustomDelimiterTest() {
+        String text = "1,2:3";
+        String[] resolved = DelimiterResolver.resolve(text);
+        assertThat(resolved[0]).isEqualTo(DelimiterResolver.BASIC_DELIMITER);
+        assertThat(resolved[1]).isEqualTo(text);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자가 있는 경우")
+    void customDelimiterTest() {
+        String text = "//;\n1;2;3";
+        String[] resolved = DelimiterResolver.resolve(text);
+        assertThat(resolved[0]).isEqualTo(DelimiterResolver.BASIC_DELIMITER + "|;");
+        assertThat(resolved[1]).isEqualTo("1;2;3");
+    }
+
+}
