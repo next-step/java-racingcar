d9e7effef27939983436299032d78bab05a14a83
DAWN <qkgk61@gmail.com>
2023-07-03 15:28:56 +0900

    :sparkles: Feature: 쉼표를 구분자로 가지는 경우 프로덕션 코드 작성

---
+++ src/main/java/caculator/Calculator.java
@@ -0,0 +1,21 @@
+package caculator;
+
+import delimiter.DelimiterResolver;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public final class Calculator {
+
+    public static final Pattern NUMBER_PATTERN = Pattern.compile("(^[0-9]*$)");
+
+    private Calculator() {
+    }
+
+    public static int sum(String text) {
+        return Arrays.stream(text.split(","))
+                     .mapToInt(Integer::parseInt)
+                     .reduce(Integer::sum)
+                     .orElseThrow(RuntimeException::new);
+    }
+
+}
