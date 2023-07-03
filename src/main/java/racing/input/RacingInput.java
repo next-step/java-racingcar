86d2227dedb22f32849ea21c0a8fe6034f78628a
bkRyusim <bkryusim@gmail.com>
2023-07-03 18:33:26 +0900

    test: add RacingInputTest.inputNameTest

---
+++ src/main/java/racing/input/RacingInput.java
@@ -0,0 +1,17 @@
+package racing.input;
+
+import racing.Car;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class RacingInput {
+    
+
+    public RacingInput(InputStream inputStream) {
+    }
+
+    public List<Car> inputName() {
+        return null;
+    }
+}
---
+++ src/test/java/racing/input/RacingInputTest.java
@@ -0,0 +1,31 @@
+package racing.input;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import racing.Car;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class RacingInputTest {
+    @Test
+    void inputNameTest() {
+        //given
+        String names = "pobi,crong,honux";
+        InputStream inputStream = new ByteArrayInputStream(names.getBytes(StandardCharsets.UTF_8));
+        RacingInput racingInput = new RacingInput(inputStream);
+
+        //when
+        List<Car> cars = racingInput.inputName();
+
+        //then
+        assertThat(cars.size()).isEqualTo(3);
+        assertThat(cars.get(0).getName()).isEqualTo("pobi");
+        assertThat(cars.get(1).getName()).isEqualTo("crong");
+        assertThat(cars.get(2).getName()).isEqualTo("honux");
+    }
+}
