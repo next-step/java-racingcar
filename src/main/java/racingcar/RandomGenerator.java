61f6bd85daf86c321398902150fe83ef14839ad3
impati <yongs170@naver.com>
2023-07-03 17:18:54 +0900

    refactor: 랜덤 객체를 메서드 호출시 마다 생성하지 않도록 변경

--- src/main/java/racingcar/RandomGenerator.java
+++ src/main/java/racingcar/RandomGenerator.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class RandomGenerator {
     static final int min = 0;
     static final int max = 9;
+    static final Random random = new Random();
     public static int generateNumber() {
-        return new Random().nextInt(max + min) + min;
+        return random.nextInt(max + min) + min;
     }
 }
