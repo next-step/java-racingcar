164c5fca665075825baccbc2f65418c1ed7f7748
DAWN <qkgk61@gmail.com>
2023-07-03 15:35:24 +0900

    :umbrella: Test: 유효성 검증 테스트 코드 작성

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -41,4 +41,39 @@ class CalculatorTest {
         ).isEqualTo(6);
     }
 
+    @Test
+    @DisplayName("null, 빈 문자열, 공백을 사용하는 경우")
+    void negativeIncludedTest2() {
+        String[] texts = { null, "", "   "};
+        for (String text : texts) {
+            assertThat(
+                    Calculator.sum(text)
+            ).isZero();
+        }
+    }
+
+    @Test
+    @DisplayName("음수를 사용하는 경우")
+    void negativeIncludedTest1() {
+        assertThatThrownBy(
+                () -> Calculator.sum("-1,2")
+        ).isExactlyInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("구분자를 연속해서 사용하는 경우")
+    void negativeDelimiterTest() {
+        assertThatThrownBy(
+                () -> Calculator.sum("//-\n1--2-3")
+        ).isExactlyInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("숫자 이외의 값을 사용하는 경우")
+    void wrongTextTest() {
+        assertThatThrownBy(
+                () -> Calculator.sum("1,가,3")
+        ).isExactlyInstanceOf(RuntimeException.class);
+    }
+
 }
