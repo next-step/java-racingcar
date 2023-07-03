f6f890f1fcb24695e001633963fb69b85bc44b60
impati <yongs170@naver.com>
2023-07-03 17:29:30 +0900

    test: 값이 4 이상일 경우 전진한다

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -3,6 +3,8 @@ package racingcar;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -78,5 +80,16 @@ public class RacingCarTest {
 
         int number = RandomGenerator.generateNumber();
         assertThat(number).isBetween(0, 9);
+    }
+    
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("값이 4 이상일 경우 전진한다")
+    public void 값이_4_이상일_경우_전진한다(int number) throws Exception {
+        Car car = new Car("chan");
+
+        car.progress(number);
+
+        assertThat(car.getDistance()).isEqualTo(1);
     }
 }
