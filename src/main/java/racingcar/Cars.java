1339bed1ff34a61a57b25148a7adb79af17cbc5e
impati <yongs170@naver.com>
2023-07-03 16:53:02 +0900

    fix: add 관련 테스트 삭제
    생성자로만 Cars 객체를 초기화하여 테스트하는 것으로 변경한다.

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,6 +1,15 @@
 package racingcar;
 
+import java.util.List;
+
 public class Cars {
+
+    private List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
     public static String[] split(String str) {
         return str.split(",");
     }
--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,20 +1,6 @@
 package racingcar;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class CarsTest {
 
-    @Test
-    @DisplayName("자동자 목록에 자동차를 추가한다.")
-    public void 자동자_목록에_자동차를_추가한다() throws Exception {
-        Cars cars = new Cars();
 
-        cars.add(new Car("pobi"));
-
-        assertThat(cars.size()).isEquals(1);
-    }
 }
