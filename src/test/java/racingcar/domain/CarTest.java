21d13011c74d3ecb5c09486a0bfbc8257d7d01c7
devxb <dlwnsdud205@naver.com>
2023-07-03 17:44:28 +0900

    [test] : Car 클래스는 0자 또는 6자 이상의 값이 주어진다면, 예외를 던진다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -26,6 +27,22 @@ class CarTest {
             void ItCreated() {
                 assertThatNoException().isThrownBy(() -> new Car(minimumInput));
                 assertThatNoException().isThrownBy(() -> new Car(fullInput));
+            }
+
+        }
+
+        @Nested
+        @DisplayName("0자 또는 6자 이상의 이름이 주어진다면,")
+        class DescribeInvalidInput {
+
+            private final String blankInput = "";
+            private final String overflowInput = "123456";
+
+            @Test
+            @DisplayName("IllegalArgumentException 이 발생한다.")
+            void ItThrowIllegalArgumentException() {
+                assertThatThrownBy(() -> new Car(blankInput)).isInstanceOf(IllegalArgumentException.class);
+                assertThatThrownBy(() -> new Car(overflowInput)).isInstanceOf(IllegalArgumentException.class);
             }
 
         }
