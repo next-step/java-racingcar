cb703a990e0058c18c46de85b024839ea71c5b8e
devxb <dlwnsdud205@naver.com>
2023-07-03 17:45:24 +0900

    [feat] : Car 클래스 생성자 자동차 이름 길이 검증 로직 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,7 +5,14 @@ public class Car {
     private final String name;
 
     public Car(String name) {
+        preAssert(name);
         this.name = name;
+    }
+
+    private void preAssert(String name) {
+        if (name.length() == 0 || name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 1~5자 사이로 입력해야됩니다. 입력한 값: \"" + name + "\"");
+        }
     }
 
 }
