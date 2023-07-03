06d1b42e305e576111cc9e1ef9d23369e1314105
impati <yongs170@naver.com>
2023-07-03 14:45:56 +0900

    feat: 각 자동차에 이름을 부여할 수 있다

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class Car {
+
+    private String name;
+    
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
