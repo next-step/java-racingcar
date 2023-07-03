29471ef3a00f955ac5570c7dc8721212195146b9
DAWN <qkgk61@gmail.com>
2023-07-03 15:30:01 +0900

    :umbrella: Test: 쉼표 또는 콜론을 구분자로 가지는 경우 테스트 코드 작성

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import caculator.Calculator;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,22 @@ class CalculatorTest {
     void commaDelimiterTest() {
         assertThat(
                 Calculator.sum("1,2,3")
+        ).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("콜론을 구분자로 가지는 경우")
+    void colonDelimiterTest() {
+        assertThat(
+                Calculator.sum("1:2:3")
+        ).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("쉼표 또는 콜론을 구분자로 가지는 경우")
+    void basicDelimiterTest() {
+        assertThat(
+                Calculator.sum("1,2:3")
         ).isEqualTo(6);
     }
 
