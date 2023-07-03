398f2c98a807f630d892d1bdc99662ecd0c896cb
DAWN <qkgk61@gmail.com>
2023-07-03 17:50:16 +0900

    :hammer: Refactor: 자동차 이동 코드 리팩토링

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,23 +1,33 @@
 package racing.domain;
 
 public class Car {
+
     private String name;
+    private int criterion;
     private int position;
 
     public Car(String name) {
         this.name = name;
+        this.criterion = 4;
+        this.position = 1;
+    }
+
+    public Car(String name, int criterion) {
+        this.name = name;
+        this.criterion = criterion;
         this.position = 1;
     }
 
     public String getName() {
         return this.name;
     }
+
     public int getPosition() {
         return this.position;
     }
 
-    public void moveOrStop(int value) {
-        if (value >= 4) {
+    public void moveOneStepMoreThanCriterion(int value) {
+        if (value >= this.criterion) {
             this.position++;
         }
     }
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -1,19 +1,20 @@
 package racing.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
+
     @Test
     @DisplayName("자동차가 이동하는 경우")
     void moveTest() {
         Car car = new Car("test");
-        car.moveOrStop(4);
+        car.moveOneStepMoreThanCriterion(4);
         assertThat(
                 car.getPosition()
         ).isEqualTo(2);
     }
+
 }
