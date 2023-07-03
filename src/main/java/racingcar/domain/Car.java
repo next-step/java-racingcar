4f7cde2df8a970455609c757209c7bc05e4fa63b
devxb <dlwnsdud205@naver.com>
2023-07-03 17:50:19 +0900

    [refactor] : assert 메소드 분리

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -12,11 +12,19 @@ public class Car {
     }
 
     private void preAssert(String name) {
-        if (Objects.isNull(name)) {
-            throw new IllegalArgumentException("자동차 이름은 null값이 될 수 없습니다.");
-        }
+        assertNotNull(name);
+        assertNameLength(name);
+    }
+
+    private void assertNameLength(String name) {
         if (name.length() == 0 || name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 1~5자 사이로 입력해야됩니다. 입력한 값: \"" + name + "\"");
+        }
+    }
+
+    private void assertNotNull(String name) {
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException("자동차 이름은 null값이 될 수 없습니다.");
         }
     }
 
