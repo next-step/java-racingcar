70e3d2f0cd951965dcdb1e6e0897a41a83252997
bkRyusim <bkryusim@gmail.com>
2023-07-04 14:00:52 +0900

    feature: modify calculate with split

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,13 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
 
     public int calculate(String input) {
         if (input == null || input.isEmpty())
             return 0;
 
-        return Integer.parseInt(input);
+        return Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).sum();
     }
 }
