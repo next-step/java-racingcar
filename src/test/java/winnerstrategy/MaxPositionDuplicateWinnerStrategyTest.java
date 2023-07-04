93a23b6851386341cfa00252ea95f2c36a784ab5
건 <gunner6603@snu.ac.kr>
2023-07-04 16:52:33 +0900

    Test: 테스트 수정

--- src/test/java/winnerstrategy/MaxPositionDuplicateWinnerStrategyTest.java
+++ src/test/java/winnerstrategy/MaxPositionDuplicateWinnerStrategyTest.java
@@ -5,11 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import car.Car;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MaxPositionDuplicateWinnerStrategyTest {
 
     @Test
+    @DisplayName("position 값이 가장 큰 자동차를 우승자로 선출한다.")
     void test1() {
         List<Car> cars = List.of(
             new Car(3, "a"),
