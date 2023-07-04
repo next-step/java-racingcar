6a876c29bd70cff1bffde993b048c29dd27a26e7
impati <yongs170@naver.com>
2023-07-04 11:12:09 +0900

    fix: Car객체를 Mock으로 수정

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -11,8 +11,8 @@ public class CarsTest {
     @Test
     @DisplayName("한 턴에 대해 자동차들에게 게임 실행 메시지을 했는지 확인한다.")
     public void 한_턴에_대해_자동차들에게_게임_실행_메시지_전달한다() throws Exception {
-        Car chanCar = new Car("chan");
-        Car iptCar = new Car("ipt");
+        Car chanCar = Mockito.mock(Car.class);
+        Car iptCar = Mockito.mock(Car.class);
         Cars cars = new Cars(List.of(chanCar, iptCar));
 
         cars.move();
