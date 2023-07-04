3917299884e1ffb058bfa47092345de5f6eb11eb
woowatech3 <woowatech3@woowatech3ui-MacBookPro.local>
2023-07-04 17:25:41 +0900

    style: follow code convention

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -14,34 +14,39 @@ public class RacingCarTest {
     @ParameterizedTest
     @ValueSource(strings = {"pobi,crong,honux", "a", " a "})
     void 자동차_입력_검증_성공(String carNames) {
-        assertThat(RacingCar.isValidCarNames(carNames)).isEqualTo(true);
+        assertThat(RacingCar.isValidCarNames(carNames))
+                .isEqualTo(true);
     }
 
     @DisplayName("유효하지 않은 자동차 이름일 경우 false를 리턴한다")
     @ParameterizedTest
     @ValueSource(strings = {"pobi,croong,honux", "pobi,,honux", ",,,,"})
     void 자동차_입력_검증_실패(String carNames) {
-        assertThat(RacingCar.isValidCarNames(carNames)).isEqualTo(false);
+        assertThat(RacingCar.isValidCarNames(carNames))
+                .isEqualTo(false);
     }
 
     @DisplayName("실행 횟수가 양수인 경우 true를 리턴한다")
     @ParameterizedTest
     @ValueSource(strings = {"5", "11", " 11 ", "7 "})
     void 자동차_실행_횟수_검증_성공(String count) {
-        assertThat(RacingCar.isValidCount(count)).isEqualTo(true);
+        assertThat(RacingCar.isValidCount(count))
+                .isEqualTo(true);
     }
 
     @DisplayName("실행 횟수가 음수이거나 정수가 아닌 경우 false를 리턴한다")
     @ParameterizedTest
     @ValueSource(strings = {"5.1", "-999", " ARA ", "1 1"})
     void 자동차_실행_횟수_검증_실패(String count) {
-        assertThat(RacingCar.isValidCount(count)).isEqualTo(false);
+        assertThat(RacingCar.isValidCount(count))
+                .isEqualTo(false);
     }
 
     @DisplayName("랜덤함수가 0이상 9이하의 숫자를 리턴한다")
     @Test
     void 랜덤_함수_검증() {
         int result = RacingCar.generateRandom();
-        assertThat(result).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(9);
+        assertThat(result).isGreaterThanOrEqualTo(0)
+                .isLessThanOrEqualTo(9);
     }
 }
