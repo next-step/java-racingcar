93f566320986ea4a7ca320e63c8d20230b3ff97b
devxb <dlwnsdud205@naver.com>
2023-07-04 11:05:25 +0900

    [test] : Cars에 중복된 이름의 Car가 있다면, 예외를 던진다.

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -31,6 +32,24 @@ class RacingGameTest {
             @DisplayName("생성을 성공 한다.")
             void ItCreated() {
                 assertThatNoException().isThrownBy(() -> new RacingGame(cars));
+            }
+
+        }
+
+        @Nested
+        @DisplayName("Cars에 중복된 이름의 Car가 있다면,")
+        class DescribeDuplicateCarsNameInput {
+
+            private final Car[] duplicateNameCars = {
+                new Car("A", trueMover),
+                new Car("A", falseMover),
+                new Car("A", trueMover),
+            };
+
+            @Test
+            @DisplayName("IllegalArgumentException을 던진다.")
+            void ItThrowIllegalArgumentException() {
+                assertThatThrownBy(() -> new RacingGame(duplicateNameCars)).isInstanceOf(IllegalArgumentException.class);
             }
 
         }
