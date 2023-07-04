f6165527f6520b091f3f277a2c4b9b951de8a93b
건 <gunner6603@snu.ac.kr>
2023-07-04 16:52:18 +0900

    Feat: 입출력 메소드 분리

--- src/main/java/client/CarRacingGame.java
+++ src/main/java/client/CarRacingGame.java
@@ -6,37 +6,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import validator.StringNameValidator;
 import winnerstrategy.MaxPositionDuplicateWinnerStrategy;
 
 public class CarRacingGame {
 
-    private static String name;
+    private static List<String> names;
     private static int count;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         getUserInput();
-        String[] carNames = name.split(",");
-
-        System.out.println("\n실행 결과");
-        CarRacingField carRacingField = new CarRacingField(
-            Arrays.asList(carNames),
-            new MaxPositionDuplicateWinnerStrategy()
-        );
-        carRacingField.moveCarsByCount(count);
-        String winnerString = carRacingField.getWinners().stream()
-            .map(Car::getName)
-            .collect(Collectors.joining(", "));
-        System.out.println(winnerString + "가 최종 우승했습니다.");
+        validateCarNames();
+        printWinners(playAndSelectWinners());
     }
 
     private static void getUserInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        name = scanner.nextLine();
+        String name =  scanner.nextLine();
+        names = Arrays.asList(name.split(","));
         System.out.println("시도할 횟수는 몇 회인가요?");
         count = scanner.nextInt();
+    }
+
+    private static void validateCarNames() {
+        names.forEach(StringNameValidator::validate);
+    }
+
+    private static List<Car> playAndSelectWinners() {
+        CarRacingField carRacingField = new CarRacingField(
+            names,
+            new MaxPositionDuplicateWinnerStrategy()
+        );
+        System.out.println("\n실행 결과");
+        carRacingField.moveCarsByCount(count);
+        return carRacingField.getWinners();
+    }
+
+    private static void printWinners(List<Car> winners) {
+        String winnerString = winners.stream()
+            .map(Car::getName)
+            .collect(Collectors.joining(", "));
+        System.out.println(winnerString + "가 최종 우승했습니다.");
     }
 }
