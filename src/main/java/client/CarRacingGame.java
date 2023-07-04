15a48ea0b391728fc96a00991ee2cdcb1c407a3f
건 <gunner6603@snu.ac.kr>
2023-07-04 16:34:44 +0900

    Feat: 입출력 메소드 분리

--- src/main/java/client/CarRacingGame.java
+++ src/main/java/client/CarRacingGame.java
@@ -12,13 +12,13 @@ import winnerstrategy.MaxPositionDuplicateWinnerStrategy;
 
 public class CarRacingGame {
 
+    private static String name;
+    private static int count;
+
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String inputName = scanner.nextLine();
-        String[] carNames = inputName.split(",");
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int count = scanner.nextInt();
+
+        getUserInput();
+        String[] carNames = name.split(",");
 
         System.out.println("\n실행 결과");
         CarRacingField carRacingField = new CarRacingField(
@@ -32,4 +32,11 @@ public class CarRacingGame {
         System.out.println(winnerString + "가 최종 우승했습니다.");
     }
 
+    private static void getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        name = scanner.nextLine();
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        count = scanner.nextInt();
+    }
 }
