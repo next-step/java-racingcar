3c5d9c5336524b9f7b5cdc87e137be1dea86f954
devxb <dlwnsdud205@naver.com>
2023-07-04 11:08:29 +0900

    [feat] : RacingGame 생성자 중복이름 검증 로직 추가

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,10 +1,20 @@
 package racingcar.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RacingGame {
 
     private final Car[] cars;
 
     public RacingGame(Car... cars) {
+        Set<String> duplicatedNameCars = new HashSet<>();
+        for(Car car : cars) {
+            if(duplicatedNameCars.contains(car.getName())) {
+                throw new IllegalArgumentException("");
+            }
+            duplicatedNameCars.add(car.getName());
+        }
         this.cars = cars;
     }
 
