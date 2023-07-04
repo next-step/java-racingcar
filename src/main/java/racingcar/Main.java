17f7cd112f6b42485038c0174bd51eaef2e8829a
impati <yongs170@naver.com>
2023-07-04 21:30:46 +0900

    refactor: Main 클래스에서 입력과 출력 , 경주 진행을 제어하도록 리팩토링

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -5,11 +5,23 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try (InputManager inputManager = new InputManager()) {
-            RacingManager racingManager = RacingManager.of(inputManager.readCarNames(), inputManager.readRound());
-            racingManager.playRacing();
-            racingManager.printWinners();
+            OutputManager outputManager = new OutputManager();
+            play(inputManager, outputManager);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void play(InputManager inputManager, OutputManager outputManager) throws IOException {
+        Cars cars = Cars.from(inputManager.readCarNames());
+        int round = inputManager.readRound();
+        RacingManager racingManager = new RacingManager(cars);
+        outputManager.printExecutionResult();
+        outputManager.printCars(cars);
+        for (int i = 0; i < round; i++) {
+            cars = racingManager.playRacing();
+            outputManager.printCars(cars);
+        }
+        outputManager.printWinnerNames(cars.findWinnerNames());
     }
 }
