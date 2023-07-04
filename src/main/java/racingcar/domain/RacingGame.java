daaa3bd9e0212a001c051fc1daa25b0a6871e8df
devxb <dlwnsdud205@naver.com>
2023-07-04 11:13:33 +0900

    [refactor] : RacingGame 생성자 중복이름 검증 로직 메서드로 추출

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -8,14 +8,22 @@ public class RacingGame {
     private final Car[] cars;
 
     public RacingGame(Car... cars) {
-        Set<String> duplicatedNameCars = new HashSet<>();
-        for(Car car : cars) {
-            if(duplicatedNameCars.contains(car.getName())) {
-                throw new IllegalArgumentException("");
-            }
-            duplicatedNameCars.add(car.getName());
-        }
+        assertDuplicateCarName(cars);
         this.cars = cars;
+    }
+
+    private static void assertDuplicateCarName(Car... cars) {
+        Set<String> duplicatedName = new HashSet<>();
+        for (Car car : cars) {
+            throwIfDuplicatedCarName(duplicatedName, car);
+            duplicatedName.add(car.getName());
+        }
+    }
+
+    private static void throwIfDuplicatedCarName(Set<String> duplicatedName, Car car) {
+        if (duplicatedName.contains(car.getName())) {
+            throw new IllegalArgumentException("중복된 자동차 이름이 발견됐습니다.");
+        }
     }
 
 }
