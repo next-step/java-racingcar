43c996be4f038fc11916fbf6d31cee023e28741f
건 <gunner6603@snu.ac.kr>
2023-07-04 10:55:38 +0900

    Feat: 주석 추가, 클래스명 변경

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,15 +1,14 @@
 package calculator;
 
 import java.util.Arrays;
-import java.util.List;
 
-public class Calculator {
+public class StringCalculator {
 
     public static int sum(String text) {
         if (isNullOrEmpty(text)) return 0;
 
-        String[] tokens = TokenUtils.generateTokensFromText(text);
-        TokenUtils.validateToken(tokens);
+        String[] tokens = TokenUtils.generateTokensFromText(text); //구분자로 텍스트를 나눠서 토큰을 생성함
+        TokenUtils.validateToken(tokens); //토큰이 음수이거나 숫자가 아닌 문자를 포함하는지 검증함
 
         return Arrays.stream(tokens)
                 .mapToInt(Integer::parseInt)
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,59 +1,56 @@
 package calculator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class CalculatorTest {
+public class StringCalculatorTest {
 
     @DisplayName("null 이나 빈 문자열을 입력하면 0을 리턴한다.")
     @ParameterizedTest
     @NullAndEmptySource
     void test1(String text) {
 
-        assertThat(Calculator.sum(text)).isEqualTo(0);
+        assertThat(StringCalculator.sum(text)).isEqualTo(0);
     }
 
     @Test
     @DisplayName(", 구분자로 숫자를 구분하는 경우 숫자 합을 리턴한다.")
     void test2() {
 
-        assertThat(Calculator.sum("1,2")).isEqualTo(3);
-        assertThat(Calculator.sum("1,2,3")).isEqualTo(6);
+        assertThat(StringCalculator.sum("1,2")).isEqualTo(3);
+        assertThat(StringCalculator.sum("1,2,3")).isEqualTo(6);
     }
 
     @Test
     @DisplayName("숫자 하나가 들어오면 해당 숫자를 리턴한다.")
     void test3() {
 
-        assertThat(Calculator.sum("1")).isEqualTo(1);
+        assertThat(StringCalculator.sum("1")).isEqualTo(1);
     }
 
     @Test
     @DisplayName("; 구분자로 숫자를 구분하는 경우 숫자 합을 리턴한다.")
     void test4() {
-        assertThat(Calculator.sum("1;2;3")).isEqualTo(6);
+        assertThat(StringCalculator.sum("1;2;3")).isEqualTo(6);
     }
 
     @Test
     @DisplayName(",와 ; 구분자로 숫자를 구분하는 경우 숫자 합을 리턴한다.")
     void test5() {
-        assertThat(Calculator.sum("1,2;3")).isEqualTo(6);
+        assertThat(StringCalculator.sum("1,2;3")).isEqualTo(6);
     }
 
     @Test
     @DisplayName("커스텀 구분자로 숫자를 구분하는 경우 숫자 합을 리턴한다.")
     void test6() {
-        assertThat(Calculator.sum("//;\n1;2;3")).isEqualTo(6);
-        assertThat(Calculator.sum("//#\n1#2#3")).isEqualTo(6);
-        assertThat(Calculator.sum("//-\n1-2-3")).isEqualTo(6);
+        assertThat(StringCalculator.sum("//;\n1;2;3")).isEqualTo(6);
+        assertThat(StringCalculator.sum("//#\n1#2#3")).isEqualTo(6);
+        assertThat(StringCalculator.sum("//-\n1-2-3")).isEqualTo(6);
     }
 
     @Test
@@ -61,11 +58,11 @@ public class CalculatorTest {
     void test7() {
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> {
-                    Calculator.sum("-1,2,3");
+                    StringCalculator.sum("-1,2,3");
                 });
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> {
-                    Calculator.sum("//-\n-1--2-3");
+                    StringCalculator.sum("//-\n-1--2-3");
                 });
     }
 
@@ -74,11 +71,11 @@ public class CalculatorTest {
     void test8() {
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> {
-                    Calculator.sum("//-\n1-!2-3");
+                    StringCalculator.sum("//-\n1-!2-3");
                 });
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> {
-                    Calculator.sum("//@\n1@@@2@3");
+                    StringCalculator.sum("//@\n1@@@2@3");
                 });
     }
 }
