e2838ccd289c6d8a314d12efb268f1551253e3b8
건 <gunner6603@snu.ac.kr>
2023-07-04 17:37:38 +0900

    Test: 테스트 수정
    가독성을 높이기 위해 테스트를 수정함

--- src/test/java/carracingfield/CarRacingFieldTest.java
+++ src/test/java/carracingfield/CarRacingFieldTest.java
@@ -2,8 +2,7 @@ package carracingfield;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import car.Car;
-import java.util.ArrayList;
+import car.CarStatus;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,15 +25,19 @@ public class CarRacingFieldTest {
     @Test
     @DisplayName("레이싱 필드에서 자동차들을 이동시키면 모든 자동차가 움직이지 않거나 1칸 이동한다.")
     void test1() {
-        List<Car> cars = field.getCars();
+        List<CarStatus> cars = field.getCarStatuses();
         List<Integer> carPrevPositions = cars.stream()
-            .map(Car::getPosition)
+            .map(CarStatus::getPosition)
             .collect(Collectors.toList());
 
-        field.moveCarsByCount(1);
+        field.moveOrStopCars();
+
+        List<Integer> carCurrPositions = cars.stream()
+            .map(CarStatus::getPosition)
+            .collect(Collectors.toList());
 
         for (int i = 0; i < cars.size(); i++) {
-            int positionDiff = cars.get(i).getPosition() - carPrevPositions.get(i);
+            int positionDiff = carCurrPositions.get(i) - carPrevPositions.get(i);
             assertThat(positionDiff >= 0).isTrue();
             assertThat(positionDiff <= 1).isTrue();
         }
