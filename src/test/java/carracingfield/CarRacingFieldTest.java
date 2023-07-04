23692bf562683db11f61f2eba3a8b43253acc6bd
건 <gunner6603@snu.ac.kr>
2023-07-04 15:40:05 +0900

    Test: 테스트 리팩토링

--- src/test/java/carracingfield/CarRacingFieldTest.java
+++ src/test/java/carracingfield/CarRacingFieldTest.java
@@ -6,21 +6,32 @@ import car.Car;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import winnerstrategy.MaxPositionDuplicateWinnerStrategy;
 
 public class CarRacingFieldTest {
+
+    CarRacingField field;
+
+    @BeforeEach
+    void setup() {
+        field = new CarRacingField(
+            List.of("a", "b", "c"),
+            new MaxPositionDuplicateWinnerStrategy()
+        );
+    }
 
     @Test
     @DisplayName("레이싱 필드에서 자동차들을 이동시키면 모든 자동차가 움직이지 않거나 1칸 이동한다.")
     void test1() {
-        CarRacingField field = new CarRacingField(List.of("a", "b", "c"));
         List<Car> cars = field.getCars();
         List<Integer> carPrevPositions = cars.stream()
             .map(Car::getPosition)
             .collect(Collectors.toList());
 
-        field.moveCars();
+        field.moveCarsByCount(1);
 
         for (int i = 0; i < cars.size(); i++) {
             int positionDiff = cars.get(i).getPosition() - carPrevPositions.get(i);
@@ -32,12 +43,11 @@ public class CarRacingFieldTest {
     @Test
     @DisplayName("position 값이 가장 큰 자동차를 우승자로 선출한다.")
     void test2() {
-        CarRacingField field = new CarRacingField(List.of("a", "b", "c"));
-        List<Car> carList = new ArrayList<>();
-        carList.add(new Car(4, "a"));
-        carList.add(new Car(3, "b"));
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(4, "a"));
+        cars.add(new Car(3, "b"));
 
-        List<Car> winners = field.getWinners(carList);
+        List<Car> winners = field.getWinners();
         assertThat(winners.size()).isEqualTo(1);
         assertThat(winners.stream().map(Car::getName).collect(Collectors.toList()))
             .containsExactly("a");
