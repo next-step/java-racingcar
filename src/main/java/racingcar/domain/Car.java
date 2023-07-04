b0b91d1c025ef0f6080fbd624823c3e984edc160
devxb <dlwnsdud205@naver.com>
2023-07-04 11:54:26 +0900

    [refactor] : Car 클래스 Asserts 로직 리팩토링

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,7 +1,7 @@
 package racingcar.domain;
 
-import java.util.Objects;
 import racingcar.domain.extension.Moveable;
+import racingcar.util.Asserts;
 
 public class Car {
 
@@ -17,27 +17,10 @@ public class Car {
     }
 
     private void preAssert(String name, Moveable moveable) {
-        assertNameIsNotNull(name);
-        assertMoveableIsNotNull(moveable);
-        assertNameLength(name);
-    }
-
-    private void assertNameIsNotNull(String name) {
-        if (Objects.isNull(name)) {
-            throw new IllegalArgumentException("자동차 이름은 null값이 될 수 없습니다.");
-        }
-    }
-
-    private void assertMoveableIsNotNull(Moveable moveable) {
-        if (Objects.isNull(moveable)) {
-            throw new IllegalArgumentException("Moveable은 null값이 될 수 없습니다.");
-        }
-    }
-
-    private void assertNameLength(String name) {
-        if (name.length() == 0 || name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 1~5자 사이로 입력해야됩니다. 입력한 값: \"" + name + "\"");
-        }
+        Asserts.notNull(name, () -> "자동차 이름은 null값이 될 수 없습니다.");
+        Asserts.notNull(moveable, () -> "Moveable은 null값이 될 수 없습니다.");
+        Asserts.isTrue(name.length() > 0 && name.length() < 6,
+            () -> "자동차 이름은 1~5자 사이로 입력해야됩니다. 입력한 값: \"" + name + "\"");
     }
 
     public int getPosition() {
