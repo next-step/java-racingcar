2e3809553653f5f7fdc4bdd40dd528304decc181
bkRyusim <bkryusim@gmail.com>
2023-07-04 13:57:29 +0900

    feature: modify calculate with parseInt

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,6 +3,9 @@ package calculator;
 public class Calculator {
 
     public int calculate(String input) {
-        return 0;
+        if (input == null || input.isEmpty())
+            return 0;
+
+        return Integer.parseInt(input);
     }
 }
