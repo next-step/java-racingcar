c2e0ad55d30e5923df928668af84d6eaf829507c
DAWN <qkgk61@gmail.com>
2023-07-04 17:27:42 +0900

    :sparkles: Feature: 5자를 초과하는 자동차 이름이 입력된 경우 프로덕션 코드 작성

--- src/main/java/racing/factory/CarFactory.java
+++ src/main/java/racing/factory/CarFactory.java
@@ -8,9 +8,12 @@ import racing.model.Car;
 public class CarFactory {
 
     private static final String DEFAULT_DELIMITER = ",";
+    public static final int CAR_NAME_MAX = 5;
 
     private static class CarFactoryHandler {
+
         private static final CarFactory INSTANCE = new CarFactory();
+
     }
 
     private CarFactory() {
@@ -21,13 +24,26 @@ public class CarFactory {
     }
 
     public List<Car> manufactureCars(String text) {
-        return manufactureCars(text.split(DEFAULT_DELIMITER));
+        String[] carNames = text.split(DEFAULT_DELIMITER);
+
+        if (!validate(carNames)) {
+            throw new IllegalArgumentException("5자를 초과하는 자동차 이름은 사용할 수 없습니다.");
+        }
+
+        return manufactureCars(carNames);
     }
 
-    public List<Car> manufactureCars(String[] names) {
+    private List<Car> manufactureCars(String[] names) {
         return Arrays.stream(names)
                      .map(Car::new)
                      .collect(Collectors.toList());
+    }
+
+    private boolean validate(String[] carNames) {
+        return Arrays.stream(carNames)
+                     .filter(cur -> cur.length() > CAR_NAME_MAX)
+                     .findAny()
+                     .isEmpty();
     }
 
 }
