bbc7aa452cb15ae384cadf6e6793ae290ed5d6f3
whatasame <whatasame@gmail.com>
2023-07-04 17:51:52 +0900

    refactor: 메서드 분리 및 exception 처리
    Co-authored-by: whatasame <whatasame@gmail.com>
    Co-authored-by: ch0213 <cndghks0213@gmail.com>

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -8,18 +8,31 @@ import racingcar.view.ConsoleOutput;
 
 public class RacingCarApplication {
     public static void main(String[] args) {
-        String names = ConsoleInput.inputNames();
-        int count = ConsoleInput.inputCount();
+        try {
+            run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("[ERROR] 예상하지 못한 예외가 발생했습니다. 프로그램을 종료합니다.");
+        }
+    }
 
-        ConsoleOutput.printResultHeader();
+    private static void run() {
+        String names = ConsoleInput.readNames();
+        int count = ConsoleInput.readCount();
 
         MoveStrategy strategy = new RandomMoveStrategy();
         Cars cars = new Cars(names);
+        race(cars, count, strategy);
+
+        ConsoleOutput.printWinners(cars.winners());
+    }
+
+    private static void race(Cars cars, int count, MoveStrategy strategy) {
+        ConsoleOutput.printResultHeader();
         for (int i = 0; i < count; i++) {
             cars.move(strategy);
             ConsoleOutput.printCurrentStatus(cars.getCars());
         }
-
-        ConsoleOutput.printWinners(cars.winners());
     }
 }
