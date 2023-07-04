c2cbd7a8335885598e2139b759943709472426f5
impati <yongs170@naver.com>
2023-07-04 11:54:19 +0900

    test: 자동차들 중 가장 멀리 간 차들의 이름을 반환한다 다른 거리

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -42,4 +42,23 @@ public class CarsTest {
         assertThat(winnerNames).containsAnyOf(chanCar.getName(), iptCar.getName());
     }
 
+    @Test
+    @DisplayName("자동차들_중_가장_멀리_간_차들의_이름을_반환한다_다른_거리")
+    public void 자동차들_중_가장_멀리_간_차들의_이름을_반환한다_다른_거리() throws Exception {
+        Car chanCar = new Car("chan");
+        Car iptCar = new Car("ipt");
+        Car tommyCar = new Car("tommy");
+
+        chanCar.forward();
+        chanCar.forward();
+        iptCar.forward();
+
+        Cars cars = new Cars(List.of(chanCar, iptCar, tommyCar));
+
+        List<String> winnerNames = cars.findWinnerNames();
+
+        assertThat(winnerNames.size()).isEqualTo(1);
+        assertThat(winnerNames).containsAnyOf(chanCar.getName());
+    }
+
 }
