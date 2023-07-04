dbaf4bb5ae15fcda5c61e682d45e7d72c35f98e3
bkRyusim <bkryusim@gmail.com>
2023-07-04 14:10:19 +0900

    feature: modify Calculator.calculate with customSplitter

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,20 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
     public int calculate(String input) {
         if (input == null || input.isEmpty())
             return 0;
+
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (matcher.find()) {
+            String customSplitter = matcher.group(1);
+            return Arrays.stream(matcher.group(2).split(customSplitter)).mapToInt(Integer::parseInt).sum();
+        }
 
         return Arrays.stream(input.split("[,:]")).mapToInt(Integer::parseInt).sum();
     }
