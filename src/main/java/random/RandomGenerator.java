e24bbdd3d542732a094ec520466b07715612d04a
건 <gunner6603@snu.ac.kr>
2023-07-04 11:55:19 +0900

    Feat: Car. RandomGenerator 클래스 생성
    자동차는 랜덤값을 토대로 이동 여부를 결정
    RandomGenerator 클래스는 랜덤값을 생성

---
+++ src/main/java/random/RandomGenerator.java
@@ -0,0 +1,11 @@
+package random;
+
+import java.util.Random;
+
+public class RandomGenerator {
+
+    public static int extractRandomSingleDigit() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
+}
---
+++ src/test/java/random/RandomGeneratorTest.java
@@ -0,0 +1,16 @@
+package random;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RandomGeneratorTest {
+
+    @Test
+    @DisplayName("0 에서 9 사이의 랜덤 값을 추출한다.")
+    void test1() {
+        int random = RandomGenerator.extractRandomSingleDigit();
+        Assertions.assertThat(random >= 0).isTrue();
+        Assertions.assertThat(random <= 9).isTrue();
+    }
+}
