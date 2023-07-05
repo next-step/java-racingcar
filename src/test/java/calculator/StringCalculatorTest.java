a0b76e8486bd1dda53f0e701e0d81cf30f00704e
devxb <dlwnsdud205@naver.com>
2023-07-05 15:35:15 +0900

    [refactor] : StringCalculatorTest 테스트명 리팩토링

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -16,10 +16,9 @@ class StringCalculatorTest {
         "1,2!3",
         "1,2,3!6",
         "1,2:3!6",
-
     }, delimiter = '!')
     @DisplayName("양수 값이 들어오면, 양수 값을 더한 값을 반환한다.")
-    void test1(String value, int expected) {
+    void returnPositiveNumSumResult(String value, int expected) {
         assertThat(StringCalculator.sum(value)).isEqualTo(expected);
     }
 
@@ -27,23 +26,22 @@ class StringCalculatorTest {
     @CsvSource(value = {
         "a:1",
         "-1:1",
-
     })
     @DisplayName("양수이외의 값이 들어오면, IllegalArgumentException 을 던진다.")
-    void test2(String value) {
+    void throwIllegalArgumentExceptionWhenNotPositiveNum(String value) {
         assertThatThrownBy(() -> StringCalculator.sum(value)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("Blank, Null 값이 들어오면, 0을 반환한다.")
-    void test3(String value) {
+    void returnZeroWhenBlankOrNull(String value) {
         assertThat(StringCalculator.sum(value)).isZero();
     }
 
     @Test
     @DisplayName("커스텀 구분자를 지정하면 커스텀 구분자에 맞춰 문자열 덧셈을 실행한 값을 반환한다.")
-    void test4() {
+    void returnPositiveNumSumResultWhenCustomSeparator() {
         assertThat(StringCalculator.sum("//;\n1;2;3")).isEqualTo(6);
     }
 }
