524abe6415d4999fc7c81821256284f848be5064
devxb <dlwnsdud205@naver.com>
2023-07-05 13:54:29 +0900

    [refactor] : RandomMover 랜덤 로직 수정

--- src/main/java/racingcar/randommove/RandomMover.java
+++ src/main/java/racingcar/randommove/RandomMover.java
@@ -1,19 +1,30 @@
 package racingcar.randommove;
 
-import java.util.function.IntSupplier;
+import java.util.Random;
+import racingcar.domain.Car;
 import racingcar.domain.extension.Moveable;
 
 public class RandomMover implements Moveable {
 
-    private final IntSupplier intSupplier;
+    private final int left;
+    private final int right;
+    private final Random random;
+    private static final int DEFAULT_LEFT = 0;
+    private static final int DEFAULT_RIGHT = 9;
 
-    public RandomMover(IntSupplier intSupplier) {
-        this.intSupplier = intSupplier;
+    public RandomMover newInstance() {
+        return new RandomMover(DEFAULT_LEFT, DEFAULT_RIGHT);
+    }
+
+    RandomMover(int left, int right) {
+        this.left = left;
+        this.right = right;
+        this.random = new Random();
     }
 
     @Override
     public boolean isMove() {
-        return intSupplier.getAsInt() > 3;
+        return random.nextInt(right - left + 1) + left > Car.MOVE_RULE;
     }
 
 }
--- src/test/java/racingcar/randommove/RandomMoverTest.java
+++ src/test/java/racingcar/randommove/RandomMoverTest.java
@@ -16,29 +16,29 @@ class RandomMoverTest {
     class DescribeIsMoveMethod {
 
         @Nested
-        @DisplayName("RandomMover에 4가 들어오면,")
+        @DisplayName("RandomMover가 0에서 3사이의 랜덤값이 생성되면,")
         class ContextReturn4 {
 
-            private final Moveable randomMover = new RandomMover(() -> 4);
-
-            @Test
-            @DisplayName("true를 반환한다.")
-            void ItReturnTrue() {
-                assertThat(randomMover.isMove()).isTrue();
-            }
-
-        }
-
-        @Nested
-        @DisplayName("RandomMover에 3이 들어오면,")
-        class ContextBinding3 {
-
-            private final Moveable randomMover = new RandomMover(() -> 3);
+            private final Moveable randomMover = new RandomMover(0, 3);
 
             @Test
             @DisplayName("false를 반환한다.")
             void ItReturnFalse() {
                 assertThat(randomMover.isMove()).isFalse();
+            }
+
+        }
+
+        @Nested
+        @DisplayName("RandomMover가 4에서 9사이의 랜덤값이 생성되면,")
+        class ContextBinding3 {
+
+            private final Moveable randomMover = new RandomMover(4, 9);
+
+            @Test
+            @DisplayName("true를 반환한다.")
+            void ItReturnTrue() {
+                assertThat(randomMover.isMove()).isTrue();
             }
 
         }
