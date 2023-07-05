f8ce81da8280ba5f3d3467760b8834afa0567c79
impati <yongs170@naver.com>
2023-07-05 22:49:42 +0900

    refactor: static 변수 컨밴션 지키기

--- src/main/java/racingcar/RandomGenerator.java
+++ src/main/java/racingcar/RandomGenerator.java
@@ -3,10 +3,11 @@ package racingcar;
 import java.util.Random;
 
 public class RandomGenerator {
-    static final int min = 0;
-    static final int max = 9;
+    static final int MIN = 0;
+    static final int MAX = 9;
     static final Random random = new Random();
+
     public static int generateNumber() {
-        return random.nextInt(max + min) + min;
+        return random.nextInt(MAX + MIN) + MIN;
     }
 }
