f0fb0a3ef6419a378f0eba388ad9247e9b2513d8
DAWN <qkgk61@gmail.com>
2023-07-05 23:50:25 +0900

    :sparkles: Feature: 입력값을 파싱하여 숫자 객체로 변환하는 기능 구현

---
+++ src/main/java/calculator/parser/NumberParser.java
@@ -0,0 +1,27 @@
+package calculator.parser;
+
+import calculator.model.NonNegativeNumbers;
+
+public final class NumberParser {
+
+    private static class NumberParserHolder {
+        private static final NumberParser INSTANCE = new NumberParser();
+    }
+
+    private final TextParser textParser;
+
+    private NumberParser() {
+        this.textParser = TextParser.getInstance();
+    }
+
+    public static NumberParser getInstance() {
+        return NumberParserHolder.INSTANCE;
+    }
+
+    public NonNegativeNumbers parse(final String text) {
+        return NonNegativeNumbers.of(
+                textParser.parse(text)
+        );
+    }
+
+}
