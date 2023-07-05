c4f78473f5009d6726131fefd8b44c382e341e99
dongjoo0-0 <kdj486347@gmail.com>
2023-07-05 14:53:33 +0900

    remove all unnecessary comments

--- src/main/java/racingcar/Input.java
+++ src/main/java/racingcar/Input.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 public class Input {
     private final static Scanner scanner = new Scanner(System.in);
 
-    /** Get try count. If get exception, retry it. */
     public static int processTryCount() {
         try {
             return readTryCount();
@@ -18,7 +17,6 @@ public class Input {
         }
     }
 
-    /** Read try count. */
     private static int readTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         String input = scanner.nextLine();
@@ -28,12 +26,10 @@ public class Input {
         throw new IllegalArgumentException();
     }
 
-    /** Validate whether string can be converted to number. */
     public static boolean isValidNumber(String number) {
         return number.matches("[0-9]+");
     }
 
-    /** Get names of car. If get exception, retry it. */
     public static List<String> processCarNames() {
         try {
             return readCarNames();
@@ -43,14 +39,12 @@ public class Input {
         }
     }
 
-    /** Read car names. */
     private static List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = scanner.nextLine();
         return split(input);
     }
 
-    /** If name of car is null or more than 5 characters, throw exception. */
     public static boolean isValidCarName(String carName) {
         if (carName == null || carName.length() > 5) {
             throw new IllegalArgumentException();
@@ -58,7 +52,6 @@ public class Input {
         return true;
     }
 
-    /** Given input names, split and validate names of cars. */
     public static List<String> split(String input) {
         String[] carNames = input.split(",");
         if (carNames.length == 0)  {
--- src/main/java/racingcar/Output.java
+++ src/main/java/racingcar/Output.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 public class Output {
 
-    /** Print initial status. */
     public static void printInitialStatus(List<Car> cars) {
         System.out.println();
         System.out.println("실행 결과"); // another sout or \n in string?
         printCarStatus(cars);
     }
 
-    /** Print status of cars. */
     public static void printCarStatus(List<Car> cars) {
         cars.stream().map(Output::carStatusToString).forEach(System.out::println);
         System.out.println();
@@ -21,7 +19,6 @@ public class Output {
         return car.getName() + " : " + "-".repeat(car.getPosition());
     }
 
-    /** Print winners. */
     public static void printWinners(List<String> winnerNames) {
         System.out.println(String.join(", ", winnerNames) + "가 최종우승했습니다.");
     }
