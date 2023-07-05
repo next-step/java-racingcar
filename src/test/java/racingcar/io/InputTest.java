6247ee6a59588eb7b26d3ffcaf633a303269acb8
ProtoSeo <proto_seo@naver.com>
2023-07-05 23:49:20 +0900

    refactor: 입출력 테스트 리팩토링
    - 여러 줄의 테스트를 ParameterizedTest 애노테이션을 활용해서 리팩토링
    - 하나의 테스트에서 유효하지 않은, 유효한 케이스를 검사하던 것을 메서드로 분리

--- src/test/java/racingcar/io/InputTest.java
+++ src/test/java/racingcar/io/InputTest.java
@@ -1,15 +1,21 @@
 package racingcar.io;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class InputTest {
 
-    @Test
-    void inputTest_validateNumber() {
-        assertThat(Input.validateNumber("123")).isTrue();
-        assertThat(Input.validateNumber("ag")).isFalse();
-        assertThat(Input.validateNumber("")).isFalse();
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "14"})
+    void inputTest_validateNumber(String numbers) {
+        assertThat(Input.validateNumber(numbers)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ag", "", "-1"})
+    void inputTest_notValidateNumber(String numbers) {
+        assertThat(Input.validateNumber(numbers)).isFalse();
     }
 }
