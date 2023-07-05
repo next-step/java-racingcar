99a4d0e17e9dfe6bf66c3be4790d18346c825b38
dongjoo0-0 <kdj486347@gmail.com>
2023-07-05 13:54:54 +0900

    refactor: make unit tests of Game

--- src/test/java/racingcar/GameTest.java
+++ src/test/java/racingcar/GameTest.java
@@ -4,17 +4,25 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameTest {
 
     @Test
-    void GameBoardTest_winnerTest() {
+    void gameTest_getCarsTest() {
         List<String> carNames = List.of("pobi", "honux");
-        int tryCount = 5;
         Game game = new Game(carNames);
-        for (int i = 0; i < tryCount; i++) {
-            game.run();
-        }
+        List<String> getCarNames = game.getCars()
+                .stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
+        Assertions.assertThat(getCarNames).contains("pobi", "honux");
+    }
+
+    @Test
+    void gameTest_winnerTest() {
+        List<String> carNames = List.of("pobi", "honux");
+        Game game = new Game(carNames);
         String[] winners = game.getWinnerNames().toArray(new String[0]);
         Assertions.assertThat(carNames).contains(winners);
     }
