23b78a1507d4aac424599c36ffad4e4518b057b0
bkRyusim <bkryusim@gmail.com>
2023-07-05 19:23:58 +0900

    feature: car name should be shorter than 5

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -2,12 +2,21 @@ package racing.domain;
 
 public class Car {
     private static final int INITIAL_POSITION = 1;
+    private static final int MAX_LENGTH = 5;
     private int position;
     private String name;
 
     public Car(String name) {
         this.name = name;
         this.position = INITIAL_POSITION;
+
+        validateName();
+    }
+
+    private void validateName() {
+        if (this.name.length() > MAX_LENGTH) {
+            throw new RuntimeException();
+        }
     }
 
     public void goForward(boolean trigger) {
