1364521d3ab9e43a1728121dd6c7f529ac478ff6
김명준 <audwnswmf@naver.com>
2023-07-05 18:37:32 +0900

    chore: modify README.md and modify RacingExceptionHandler

--- src/main/java/racing/RacingExceptionHandler.java
+++ src/main/java/racing/RacingExceptionHandler.java
@@ -3,9 +3,15 @@ package racing;
 import racing.exception.RacingBaseException;
 
 public class RacingExceptionHandler {
+    private final RacingApplication racingApplication;
+
+    public RacingExceptionHandler(final RacingApplication racingApplication) {
+        this.racingApplication = racingApplication;
+    }
+
     public void exceptionHandle() {
         try {
-            new RacingApplication().run();
+            racingApplication.run();
         } catch (RacingBaseException e) {
             System.out.println(e.getMessage());
         } catch (Exception e) {
