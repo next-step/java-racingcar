e73851ae270f3eef157c53fd433b54689de31aa2
woowatech3 <woowatech3@woowatech3ui-MacBookPro.local>
2023-07-05 14:25:14 +0900

    [racingcar]feat: add print function

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class RacingGame {
     public final RacingCar[] cars;
@@ -21,4 +22,21 @@ public class RacingGame {
                 .filter(car -> car.moveCount == winnerCount)
                 .toArray(RacingCar[]::new);
     }
+
+    public void printResult() {
+        String result = Arrays.stream(cars)
+                .map(RacingCar::statusToString)
+                .collect(Collectors.joining("\n"));
+        System.out.println(result + "\n");
+    }
+
+    public void printWinner() {
+        String result = Arrays.stream(selectWinners())
+                .map(car -> car.name)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(result + "가 최종 우승했습니다.");
+    }
+
+
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -128,4 +132,5 @@ public class RacingCarTest {
 
         assertThat(result).isEqualTo("pobi : --");
     }
+
 }
