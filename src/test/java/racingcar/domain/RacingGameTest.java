f8a5f38895a0ae8eb0d4c473f71242e943a47d08
devxb <dlwnsdud205@naver.com>
2023-07-07 01:42:15 +0900

    [test] : RacingGame.playAndGetRoundResults 메소드 테스트 추가

--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.extension.Moveable;
+import racingcar.domain.response.RacingGamePlayResponse;
 
 @DisplayName("RacingGame 클래스")
 class RacingGameTest {
@@ -91,8 +92,8 @@ class RacingGameTest {
     }
 
     @Nested
-    @DisplayName("play 메소드는")
-    class DescribePlayMethod {
+    @DisplayName("playAndGetPlayResponse 메소드는")
+    class DescribePlayAndGetPlayResponseMethod {
 
         private final List<Car> cars = List.of(
             new Car("A", falseMover),
@@ -106,14 +107,13 @@ class RacingGameTest {
 
             private final int round = 1;
             private final RacingGame racingGame = new RacingGame(round, cars);
-            private final int moveCount = 1;
-            private final List<Car> expectedCars = getExpectedCars(cars, moveCount);
 
             @Test
-            @DisplayName("한판을 진행하고, 그 결과를 car에 반영한다.")
+            @DisplayName("한판을 진행하고, 결과를 응답한다.")
             void ItReturn1RoundResult() {
-                racingGame.play();
-                assertEqualAllCars(cars, expectedCars);
+                RacingGamePlayResponse result = racingGame.playAndGetRoundResults();
+
+                assertGamePlayResult(round, cars.size(), List.of("C"), result);
             }
 
         }
@@ -124,117 +124,29 @@ class RacingGameTest {
 
             private final int round = 3;
             private final RacingGame racingGame = new RacingGame(round, cars);
-            private final int moveCount = 3;
-            private final List<Car> expectedCars = getExpectedCars(cars, moveCount);
 
             @Test
             @DisplayName("세판을 진행하고, 그 결과를 car에 반영한다.")
             void ItReturn1RoundResult() {
-                racingGame.play();
+                RacingGamePlayResponse result = racingGame.playAndGetRoundResults();
 
-                assertEqualAllCars(cars, expectedCars);
+                assertGamePlayResult(round, cars.size(), List.of("C"), result);
             }
 
         }
 
-    }
+        private void assertGamePlayResult(int expectedRound, int playerCount, List<String> winnerNames,
+            RacingGamePlayResponse result) {
 
-    @Nested
-    @DisplayName("getRoundResult 메소드는")
-    class DescribeRoundResultMethod {
+            assertThat(result.getRacingGameRoundResponses()).hasSize(expectedRound);
 
-        private final List<Car> cars = List.of(
-            new Car("A", falseMover),
-            new Car("B", falseMover),
-            new Car("C", trueMover)
-        );
+            assertThat(result.getWinnerNames()).containsAll(winnerNames);
 
-        @Nested
-        @DisplayName("play가 실행된 이후 호출하면,")
-        class ContextCall {
-
-            private final int round = 1;
-            private final RacingGame racingGame = new RacingGame(round, cars);
-            private final int moveCount = 1;
-            private final List<Car> expectedCars = getExpectedCars(cars, moveCount);
-
-            @Test
-            @DisplayName("현재 라운드의 결과를 반환한다.")
-            void ItReturnRoundResult() {
-                racingGame.play();
-                List<Car> roundResult = racingGame.getRoundResult();
-                assertEqualAllCars(roundResult, expectedCars);
-            }
-
-        }
-
-        @Nested
-        @DisplayName("play가 두번 호출된 후, 호출되면")
-        class ContextCallBeforePlayTwice {
-
-            private final int round = 2;
-            private final RacingGame racingGame = new RacingGame(round, cars);
-            private final int moveCount = 2;
-            private final List<Car> expectedCars = getExpectedCars(cars, moveCount);
-
-            @Test
-            @DisplayName("두번째 라운드의 결과를 반환한다.")
-            void ItReturnTwiceRoundResult() {
-                racingGame.play();
-                List<Car> roundResult = racingGame.getRoundResult();
-                assertEqualAllCars(roundResult, expectedCars);
-            }
-
+            result.getRacingGameRoundResponses().forEach(
+                racingGameRoundResponse -> assertThat(racingGameRoundResponse.getCarPerRoundResponses())
+                    .hasSize(playerCount));
         }
 
     }
 
-    @Nested
-    @DisplayName("getWinners 메소드는")
-    class ContextGetWinnersMethod {
-
-        private final List<Car> cars = List.of(
-            new Car("win1", trueMover),
-            new Car("win2", trueMover),
-            new Car("loser", falseMover)
-        );
-
-        @Nested
-        @DisplayName("한판 진행 후 호출되면,")
-        class DescribeCall {
-
-            private final int round = 1;
-            private final RacingGame racingGame = new RacingGame(round, cars);
-            private final int moveCount = 1;
-            private final List<Car> expectedCars = getExpectedCars(cars, moveCount);
-
-            @Test
-            @DisplayName("우승자들을 반환한다.")
-            void ItReturnRoundResult() {
-                racingGame.play();
-                List<Car> roundResult = racingGame.getWinners();
-                assertEqualAllCars(roundResult, expectedCars);
-            }
-
-        }
-
-    }
-
-    private List<Car> getExpectedCars(List<Car> cars, int moveCount) {
-        List<Car> copy = List.copyOf(cars);
-        for (Car car : copy) {
-            moveCar(car, moveCount);
-        }
-        return copy;
-    }
-
-    private void moveCar(Car car, int moveCount) {
-        for (int i = 0; i < moveCount; i++) {
-            car.move();
-        }
-    }
-
-    private void assertEqualAllCars(List<Car> result, List<Car> expected) {
-        assertThat(result).containsAll(expected);
-    }
 }
