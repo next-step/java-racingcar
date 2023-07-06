90e457811752fec252f2ddb9f690b884fce3c16f
devxb <dlwnsdud205@naver.com>
2023-07-07 02:07:45 +0900

    [refactor] : ConsoleInput의 매직넘버를 없앤다

--- src/main/java/racingcar/app/input/ConsoleInput.java
+++ src/main/java/racingcar/app/input/ConsoleInput.java
@@ -7,10 +7,12 @@ import racingcar.controller.request.RacingGamePlayControllerRequest;
 
 public class ConsoleInput {
 
+    private static final String CAR_NAME_SEPERATE_EXPRESSION = ",";
+
     public RacingGamePlayControllerRequest input() {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-            List<String> carNames = Arrays.asList(scanner.next().split(","));
+            List<String> carNames = Arrays.asList(scanner.next().split(CAR_NAME_SEPERATE_EXPRESSION));
             System.out.println("시도할 회수는 몇회인가요?");
             int round = scanner.nextInt();
             return new RacingGamePlayControllerRequest(round, carNames);
