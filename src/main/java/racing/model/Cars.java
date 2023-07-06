6f5257ab3c3afb4c142528ddbee99cab45c53669
DAWOON-LEE <qkgk61@gmail.com>
2023-07-07 01:31:21 +0900

    :umbrella: Test: Cars 객체에 대한 테스트 코드 추가
    테스트 코드를 추가하며 RacingManager와 역할이 같다는 것을 깨닫고 해당 클래스를 제거하였습니다.

--- src/main/java/racing/model/Cars.java
+++ src/main/java/racing/model/Cars.java
@@ -1,46 +1,42 @@
 package racing.model;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Cars {
 
-    private final List<Car> original;
+    private final List<Car> cars;
 
-    public Cars(List<Car> original) {
-        this.original = original;
+    public Cars(final List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
     }
 
     public int size() {
-        return this.original.size();
+        return cars.size();
     }
 
-    public void moveOneStepMoreThanCriterion(int idx, int randomValue) {
-        this.original.get(idx)
-                     .moveOneStepMoreThanCriterion(randomValue);
+    public void moveCars(final Random random) {
+        cars.forEach(car -> car.move(random));
     }
 
-    public List<String> getWinnerNames() {
-        int maxPosition = getMaxPosition();
-        return this.original.stream()
-                            .filter(cur -> cur.isWinner(maxPosition))
-                            .map(Car::getName)
-                            .collect(Collectors.toList());
+    public List<String> findWinnerNames() {
+        final int maxPosition = maxPosition();
+        return cars.stream()
+                   .filter(car -> car.isWinner(maxPosition))
+                   .map(Car::getName)
+                   .collect(Collectors.toList());
     }
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        original.forEach(car -> builder.append(car).append('\n'));
-        return builder.toString();
+    private int maxPosition() {
+        return cars.stream()
+                   .mapToInt(Car::getPosition)
+                   .max()
+                   .orElseThrow(() -> new RuntimeException("Cars에 Car가 저장되지 않는 경우는 일어날 수 없습니다."));
     }
-
-    private int getMaxPosition() {
-        int maxPosition = -1;
-        for (Car car : this.original) {
-            maxPosition = Math.max(maxPosition, car.getPosition());
-        }
-        return maxPosition;
-    }
-
 }
