6a078a5e68ca10de2a3f563dbb79d8e74350bf95
woowatech3 <woowatech3@woowatech3ui-MacBookPro.local>
2023-07-06 20:57:35 +0900

    [racingcar]refactor: rename variable and field

--- src/main/java/stringcalculator/Expression.java
+++ src/main/java/stringcalculator/Expression.java
@@ -4,42 +4,42 @@ import java.util.Arrays;
 
 public class Expression {
 
-    private final String[] stringArray;
+    private final String[] elements;
 
     public Expression(String expression, String delimiter) {
-        this.stringArray = validateInputText(expression, delimiter);
+        this.elements = validateInputText(expression, delimiter);
+    }
+
+    private String[] validateInputText(String expression, String delimiter) {
+        String[] elements = parse(expression, delimiter);
+        for (String element : elements) {
+            validateNumber(element);
+        }
+
+        return elements;
     }
 
     private String[] parse(String expression, String delimiter) {
         return expression.split(delimiter);
     }
 
-    private void validateNumber(String string) {
-        if (!string.matches("^[0-9]*$")) {
+    private void validateNumber(String element) {
+        if (!element.matches("^[0-9]*$")) {
             throw new RuntimeException("유효하지 않은 값입니다");
         }
     }
 
-    private String[] validateInputText(String expression, String delimiter) {
-        String[] stringArray = parse(expression, delimiter);
-        for (String string : stringArray) {
-            validateNumber(string);
-        }
-
-        return stringArray;
+    public int sumString() {
+        return Arrays.stream(elements)
+                .mapToInt(element -> stringToInt(element))
+                .sum();
     }
 
-    private int stringToInt(String s) {
-        if (s.length() == 0) {
+    private int stringToInt(String element) {
+        if (element.isBlank()) {
             return 0;
         }
 
-        return Integer.parseInt(s);
-    }
-
-    public int sumString() {
-        return Arrays.stream(stringArray)
-                .mapToInt(s -> stringToInt(s))
-                .sum();
+        return Integer.parseInt(element);
     }
 }
