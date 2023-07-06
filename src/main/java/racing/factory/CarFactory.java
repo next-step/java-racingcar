583e51020f815b55763b634fb6afff1675dbd864
hwihwi <yjh200523@naver.com>
2023-07-06 15:48:00 +0900

    :sparkles: Feature: 요구사항에 서술되지 않은 예외 조건 프로덕션 코드 작성

--- src/main/java/racing/factory/CarFactory.java
+++ src/main/java/racing/factory/CarFactory.java
@@ -1,6 +1,7 @@
 package racing.factory;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import racing.model.Car;
@@ -8,7 +9,7 @@ import racing.model.Car;
 public class CarFactory {
 
     private static final String DEFAULT_DELIMITER = ",";
-    public static final int CAR_NAME_MAX = 5;
+    private static final int CAR_NAME_MAX = 5;
 
     private static class CarFactoryHandler {
 
@@ -26,24 +27,30 @@ public class CarFactory {
     public List<Car> manufactureCars(String text) {
         String[] carNames = text.split(DEFAULT_DELIMITER);
 
-        if (!validate(carNames)) {
-            throw new IllegalArgumentException("5자를 초과하는 자동차 이름은 사용할 수 없습니다.");
-        }
+        validate(carNames);
 
         return manufactureCars(carNames);
     }
 
     private List<Car> manufactureCars(String[] names) {
-        return Arrays.stream(names)
-                     .map(Car::new)
-                     .collect(Collectors.toList());
+        return Arrays.stream(names).map(Car::new).collect(Collectors.toList());
     }
 
-    private boolean validate(String[] carNames) {
-        return Arrays.stream(carNames)
-                     .filter(cur -> cur.length() > CAR_NAME_MAX)
-                     .findAny()
-                     .isEmpty();
+    private void validate(String[] carNames) {
+        final boolean isInvalidCarName = Arrays.stream(carNames)
+                                         .anyMatch(carName -> carName.isBlank() || isLongerThanMax(carName));
+
+        if (isInvalidCarName || isDuplicated(carNames)) {
+            throw new IllegalArgumentException("자동차 이름이 유효하지 않거나 중복된 자동차 이름이 존재합니다.");
+        }
+    }
+
+    private boolean isLongerThanMax(String carName) {
+        return carName.length() > CAR_NAME_MAX;
+    }
+
+    private boolean isDuplicated(String[] carNames) {
+        return new HashSet<>(List.of(carNames)).size() != carNames.length;
     }
 
 }
