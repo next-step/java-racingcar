9e0cc3b8a06a8f414717ba257e7ec901e52ba8fe
devxb <dlwnsdud205@naver.com>
2023-07-06 12:10:55 +0900

    [refactor] : RacingGamePlayUsecase.play 구조 변경

--- src/main/java/racingcar/usecase/RacingGamePlayUsecase.java
+++ src/main/java/racingcar/usecase/RacingGamePlayUsecase.java
@@ -13,28 +13,33 @@ public class RacingGamePlayUsecase implements RacingGamePlayable {
 
     @Override
     public RacingGamePlayResponse play(int round, RacingGamePlayRequest racingGamePlayRequest) {
-        RacingGame racingGame = getRacingGame(racingGamePlayRequest);
-        RacingGamePlayResponse response = new RacingGamePlayResponse();
-        response.addRacingGameRoundResponse(ROUND_OFFSET, racingGame.getRoundResult());
-
-        playRound(round, racingGame, response);
-
-        response.setWinner(racingGame.getWinners());
-        return response;
+        RacingGame racingGame = readyRacingGame(racingGamePlayRequest);
+        return playRacingGame(round, racingGame);
     }
 
-    private RacingGame getRacingGame(RacingGamePlayRequest racingGamePlayRequest) {
+    private RacingGame readyRacingGame(RacingGamePlayRequest racingGamePlayRequest) {
         return new RacingGame(racingGamePlayRequest.getCarRequests().stream()
             .map(cr -> new Car(cr.getName(), cr.getMoveable()))
             .collect(Collectors.toList())
         );
     }
 
-    private void playRound(int round, RacingGame racingGame, RacingGamePlayResponse response) {
+    private RacingGamePlayResponse playRacingGame(int round, RacingGame racingGame) {
+        RacingGamePlayResponse response = initialRacingGamePlayResponse(racingGame);
+
         for (int currentRound = PLAYED_ROUND_OFFSET; currentRound <= round; currentRound++) {
             racingGame.play();
             response.addRacingGameRoundResponse(currentRound, racingGame.getRoundResult());
         }
+
+        response.setWinner(racingGame.getWinners());
+        return response;
+    }
+
+    private RacingGamePlayResponse initialRacingGamePlayResponse(RacingGame racingGame) {
+        RacingGamePlayResponse response = new RacingGamePlayResponse();
+        response.addRacingGameRoundResponse(ROUND_OFFSET, racingGame.getRoundResult());
+        return response;
     }
 
 }
