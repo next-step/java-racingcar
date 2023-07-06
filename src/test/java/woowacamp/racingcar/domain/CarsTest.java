460096a103a410ad76de4ec72e658a2fda14dcc5
ch0213 <cndghks0213@gmail.com>
2023-07-06 15:09:17 +0900

    test: 길어진 테스트에 given, when, then 주석 추가

--- src/test/java/woowacamp/racingcar/domain/CarsTest.java
+++ src/test/java/woowacamp/racingcar/domain/CarsTest.java
@@ -23,10 +23,13 @@ public class CarsTest {
     @ValueSource(ints = {4, 9})
     @ParameterizedTest
     void 모든_자동차가_움직인다(int number) {
+        // given
         Cars cars = new Cars("충환,정규,충규");
 
+        // when
         cars.move(() -> number);
 
+        // then
         List<Position> positions = cars.getCars().stream()
                 .map(Car::getPosition)
                 .collect(Collectors.toList());
@@ -38,10 +41,13 @@ public class CarsTest {
     @ValueSource(ints = {0, 3})
     @ParameterizedTest
     void 모든_자동차가_움직이지_않는다(int number) {
+        // given
         Cars cars = new Cars("충환,정규,충규");
 
+        // when
         cars.move(() -> number);
 
+        // then
         List<Position> positions = cars.getCars().stream()
                 .map(Car::getPosition)
                 .collect(Collectors.toList());
@@ -52,17 +58,20 @@ public class CarsTest {
 
     @Test
     void 우승자를_구한다() {
+        // given
         Cars cars = new Cars(
                 List.of(new Car("충환", 3),
                         new Car("정규", 3),
                         new Car("충규", 1)));
 
+        // when
         List<String> winnerNames = cars.winners()
                 .stream()
                 .map(Car::getName)
                 .map(Name::getValue)
                 .collect(Collectors.toList());
 
+        // then
         assertThat(winnerNames).containsExactly("충환", "정규");
     }
 
