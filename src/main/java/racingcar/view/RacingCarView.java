f15a0f2c71942df706f24989ff0d4532dae3f136
woowatech3 <woowatech3@woowatech3ui-MacBookPro.local>
2023-07-06 15:21:43 +0900

    [racingcar]refactor: add print function

--- src/main/java/racingcar/view/RacingCarView.java
+++ src/main/java/racingcar/view/RacingCarView.java
@@ -1,12 +1,21 @@
 package racingcar.view;
 
+import racingcar.dto.Result;
+import racingcar.dto.Winner;
+
+import java.util.List;
+
 public interface RacingCarView {
 
     void printCarNamesPrompt();
 
     void printCountPrompt();
 
-    void printResult(String result);
+    void printResults(List<List<Result>> results);
+
+    void printResult(List<Result> result);
+
+    void printWinners(List<Winner> winners);
 
     String read();
 }
