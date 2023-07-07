49d575721fd2426dc943e3680eb6ce029fb13ad2
ProtoSeo <proto_seo@naver.com>
2023-07-07 20:48:51 +0900

    test: Cars winner 확인 테스트 수정

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -12,10 +12,14 @@ public class CarsTest {
     @Test
     @DisplayName("자동차들 중 우승자가 포함되어 있는지 확인한다.")
     void carsTest_getWinnerNames() {
-        List<CarName> carNames = List.of(new CarName("pobi"), new CarName("honux"));
-        Cars cars = new Cars(carNames);
+        List<Car> carList = List.of(
+                new Car(new CarName("pobi"), new Position(4)),
+                new Car(new CarName("honux"), new Position(5)),
+                new Car(new CarName("zzuni"), new Position(5))
+        );
+        Cars cars = new Cars(carList);
 
-        String[] winners = cars.getWinnerNames().toArray(new String[0]);
-        assertThat(carNames.stream().map(CarName::getValue).toArray()).contains(winners);
+        List<String> winners = cars.getWinnerNames().getNames();
+        assertThat(winners).contains("honux", "zzuni");
     }
 }
