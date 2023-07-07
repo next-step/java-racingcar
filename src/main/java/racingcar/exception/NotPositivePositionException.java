b31d614d08105d8d5d73bf855967fa9429117cfa
ProtoSeo <proto_seo@naver.com>
2023-07-07 15:52:06 +0900

    feature: 위치가 양수가 아닐 경우의 커스텀 예외 생성

---
+++ src/main/java/racingcar/exception/NotPositivePositionException.java
@@ -0,0 +1,8 @@
+package racingcar.exception;
+
+public class NotPositivePositionException extends RuntimeException {
+
+    public NotPositivePositionException(String message) {
+        super(message);
+    }
+}
