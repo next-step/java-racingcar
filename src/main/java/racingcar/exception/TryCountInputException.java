31acfd819396241ae580ef1b4533776541ddb5e2
ProtoSeo <proto_seo@naver.com>
2023-07-07 19:19:25 +0900

    refactor: 메세지를 가질 수 있도록 변경

--- src/main/java/racingcar/exception/TryCountInputException.java
+++ src/main/java/racingcar/exception/TryCountInputException.java
@@ -1,4 +1,8 @@
 package racingcar.exception;
 
 public class TryCountInputException extends RuntimeException {
+
+    public TryCountInputException(String message) {
+        super(message);
+    }
 }
