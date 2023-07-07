9a77d21a6c947477bdb618a32678db3bd1fbee22
ProtoSeo <proto_seo@naver.com>
2023-07-07 20:49:40 +0900

    refactor: CarNames 사용하도록 수정 및, playGame 로직 수정

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,19 +1,17 @@
 package racingcar.controller;
 
-import racingcar.domain.CarName;
+import racingcar.domain.CarNames;
 import racingcar.domain.GameBoard;
 import racingcar.domain.TryCount;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
-import java.util.List;
-
 public class RacingCarController {
 
     public void run() {
-        List<CarName> carNames = InputView.processCarNames();
+        CarNames carNames = InputView.processCarNames();
         TryCount tryCount = InputView.processTryCount();
-        GameBoard gameBoard = new GameBoard(carNames);
+        GameBoard gameBoard = new GameBoard(carNames.toCars());
 
         OutputView.printInitialStatus(gameBoard.getCars());
         playGame(tryCount, gameBoard);
@@ -21,8 +19,8 @@ public class RacingCarController {
     }
 
     private void playGame(TryCount tryCount, GameBoard gameBoard) {
-        for (TryCount t = tryCount; t.isZero(); t = t.decrease()) {
-            gameBoard.run();
+        for (TryCount t = tryCount; !t.isZero(); t = t.decrease()) {
+            gameBoard.play();
             OutputView.printExecutionResult(gameBoard.getCars());
         }
     }
