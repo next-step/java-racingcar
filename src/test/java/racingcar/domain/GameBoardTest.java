ebddf27dab788f963d74584a2a1832d13b644612
ProtoSeo <proto_seo@naver.com>
2023-07-07 20:47:24 +0900

    test: GameBoard winner 확인 테스트 수정

--- src/test/java/racingcar/domain/GameBoardTest.java
+++ src/test/java/racingcar/domain/GameBoardTest.java
@@ -1,22 +1,24 @@
 package racingcar.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GameBoardTest {
 
     @Test
+    @DisplayName("우승자의 이름들을 반환한다.")
     void GameBoardTest_winnerTest() {
-        List<String> carNames = List.of("pobi", "honux");
-        int tryCount = 5;
-        GameBoard gameBoard = new GameBoard(carNames.stream().map(CarName::new).collect(Collectors.toList()));
-        for (int i = 0; i < tryCount; i++) {
-            gameBoard.run();
-        }
-        String[] winners = gameBoard.getWinnerNames().toArray(new String[0]);
-        Assertions.assertThat(carNames).contains(winners);
+        List<Car> cars = List.of(
+                new Car(new CarName("pobi"), new Position(4)),
+                new Car(new CarName("honux"), new Position(5)),
+                new Car(new CarName("zzuni"), new Position(5))
+        );
+        GameBoard gameBoard = new GameBoard(cars);
+
+        List<String> winners = gameBoard.getWinnerNames().getNames();
+        Assertions.assertThat(winners).contains("honux", "zzuni");
     }
 }
