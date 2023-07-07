f1066265ac27cda0e83987cbe56ac9687eaa3176
ProtoSeo <proto_seo@naver.com>
2023-07-07 16:04:41 +0900

    test: view 패키지 이동

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -1,21 +1,21 @@
-package racingcar.io;
+package racingcar.view;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class InputTest {
+public class InputViewTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"123", "14"})
     void inputTest_validateNumber(String numbers) {
-        assertThat(Input.validateNumber(numbers)).isTrue();
+        assertThat(InputView.validateNumber(numbers)).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"ag", "", "-1"})
     void inputTest_notValidateNumber(String numbers) {
-        assertThat(Input.validateNumber(numbers)).isFalse();
+        assertThat(InputView.validateNumber(numbers)).isFalse();
     }
 }
