6f2dc8125f3a7dd374f062b77d22fd69809613db
chanwooDev <pove2019@gmail.com>
2023-07-07 16:21:18 +0900

    refactor: 불필요 테스트 제거

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.number_generator.SpecificNumberGenerator;
@@ -9,26 +8,6 @@ import racingcar.number_generator.SpecificNumberGenerator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
-
-    @Test
-    @DisplayName("각 자동차에 이름을 부여할 수 있다.")
-    public void 각_자동차에_이름을_부여할_수_있다() throws Exception {
-        String name = "chanu";
-
-        Car car = new Car(name);
-
-        assertThat(car.getName()).isEqualTo(name);
-    }
-
-    @Test
-    @DisplayName("차가 전진한다")
-    public void 차가_전진한다() throws Exception {
-        Car car = new Car("hello");
-
-        car.forward();
-
-        assertThat(car.getDistance()).isEqualTo(1);
-    }
 
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
