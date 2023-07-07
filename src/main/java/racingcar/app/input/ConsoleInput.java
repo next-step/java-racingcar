fab7ab59b75bce0ff9b5eaeaab1fa4d22ecff39f
devxb <dlwnsdud205@naver.com>
2023-07-07 14:20:09 +0900

    [refactor] : ConsoleInput 인라인 리턴으로 리팩토링

--- src/main/java/racingcar/app/input/ConsoleInput.java
+++ src/main/java/racingcar/app/input/ConsoleInput.java
@@ -18,8 +18,7 @@ public class ConsoleInput {
     public List<String> inputCarNames() {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-            List<String> carNames = Arrays.asList(scanner.next().split(CAR_NAME_SEPERATE_EXPRESSION));
-            return carNames;
+            return Arrays.asList(scanner.next().split(CAR_NAME_SEPERATE_EXPRESSION));
         }
     }
 
