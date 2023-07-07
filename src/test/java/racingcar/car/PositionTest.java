f92dd6492dfc7df2396b872b6cb5372c5b2a3835
ProtoSeo <proto_seo@naver.com>
2023-07-07 15:47:48 +0900

    test: Position 관련 테스트 작성
    - 포지션 생성 테스트
    - 0이하의 값 관련 테스트
    - 포지션의 값이 증가하는지 테스트

---
+++ src/test/java/racingcar/car/PositionTest.java
@@ -0,0 +1,41 @@
+package racingcar.car;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.exception.NotPositivePositionException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PositionTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10})
+    @DisplayName("포지션을 생성한다.")
+    public void positionTest_createPosition(int value) {
+        assertDoesNotThrow(() -> new Position(value));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    @DisplayName("0이하의 값은 예외를 던진다.")
+    public void positionTest_createPositivePosition(int value) {
+        assertThatThrownBy(() -> new Position(value))
+                .isInstanceOf(NotPositivePositionException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이동 시 포지션의 값이 1 증가한다.")
+    public void positionTest_movePosition() {
+        int beforeValue = 1;
+        Position position = new Position(beforeValue);
+
+        Position increasePosition = position.increaseValue();
+
+        assertThat(increasePosition.getValue()).isEqualTo(beforeValue + 1);
+    }
+}
