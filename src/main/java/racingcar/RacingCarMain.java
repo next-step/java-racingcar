281c0d5fa127623bb951dfcbd2159d62c4985874
dongjoo0-0 <kdj486347@gmail.com>
2023-07-07 22:43:55 +0900

    refactor: move input function to controller, split run steps

--- src/main/java/racingcar/RacingCarMain.java
+++ src/main/java/racingcar/RacingCarMain.java
@@ -1,35 +1,12 @@
 package racingcar;
 
 import racingcar.controller.RacingCarController;
-import racingcar.domain.CarName;
-import racingcar.domain.Game;
-import racingcar.domain.TryCount;
-import racingcar.view.InputView;
-
-import java.util.List;
-import java.util.function.Supplier;
 
 public class RacingCarMain {
 
     public static void main(String[] args) {
-        Game game = processInput();
-
-        RacingCarController racingCarController = new RacingCarController(game);
+        RacingCarController racingCarController = new RacingCarController();
+        racingCarController.runGame();
         racingCarController.printResult();
-    }
-
-    private static <T> T retryIfException(Supplier<T> supplier) {
-        try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return retryIfException(supplier);
-        }
-    }
-
-    private static Game processInput() {
-        List<CarName> carNames = retryIfException(InputView::readCarNames);
-        TryCount tryCount = retryIfException(InputView::readTryCount);
-        return new Game(carNames, tryCount);
     }
 }
--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,17 +1,50 @@
 package racingcar.controller;
 
+import racingcar.domain.CarName;
+import racingcar.domain.CarStatus;
 import racingcar.domain.Game;
+import racingcar.domain.TryCount;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
+
+import java.util.List;
+import java.util.function.Supplier;
 
 public class RacingCarController {
 
     private final Game game;
+    private List<List<CarStatus>> gameStatus;
+    private List<CarName> winners;
 
-    public RacingCarController(Game game) {
+    public RacingCarController() {
+        this(processInput());
+    }
+
+    private RacingCarController(Game game) {
         this.game = game;
     }
 
+    private static <T> T retryIfException(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return retryIfException(supplier);
+        }
+    }
+
+    private static Game processInput() {
+        List<CarName> carNames = retryIfException(InputView::readCarNames);
+        TryCount tryCount = retryIfException(InputView::readTryCount);
+        return new Game(carNames, tryCount);
+    }
+
+    public void runGame() {
+        gameStatus = game.run();
+        winners = game.findWinnerNames();
+    }
+
     public void printResult() {
-        OutputView.printResult(game.run(), game.findWinnerNames());
+        OutputView.printResult(gameStatus, winners);
     }
 }
