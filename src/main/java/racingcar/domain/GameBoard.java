12a7a1c49e7270c4dd44bb117e6fb12545486f2a
ProtoSeo <proto_seo@naver.com>
2023-07-07 20:46:45 +0900

    refactor: Random 멤버변수 추가 및 메서드명 변경
    - run 메서드 명 play로 수정
    - Random 멤버변수 추가

--- src/main/java/racingcar/domain/GameBoard.java
+++ src/main/java/racingcar/domain/GameBoard.java
@@ -1,24 +1,31 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.Random;
 
 public class GameBoard {
 
     private final Cars cars;
+    private final Random random;
 
-    public GameBoard(List<CarName> carNames) {
-        cars = new Cars(carNames);
+    GameBoard(List<Car> cars) {
+        this(new Cars(cars));
     }
 
-    public void run() {
-        cars.move();
+    public GameBoard(Cars cars) {
+        this.cars = cars;
+        this.random = new Random();
     }
 
-    public List<String> getWinnerNames() {
+    public void play() {
+        cars.move(random);
+    }
+
+    public CarNames getWinnerNames() {
         return cars.getWinnerNames();
     }
 
     public Cars getCars() {
-        return cars;
+        return this.cars;
     }
 }
