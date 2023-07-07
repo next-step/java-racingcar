bfa43b06380a5aabd82a85c1be4d22bcb8c10f7b
chanwooDev <pove2019@gmail.com>
2023-07-07 16:08:40 +0900

    refactor-feat: equals, hashcode, tostring 생성

--- src/main/java/racingcar/Distance.java
+++ src/main/java/racingcar/Distance.java
@@ -1,6 +1,33 @@
 package racingcar;
 
+import java.util.Objects;
+
 public class Distance {
+
+    private final int distance;
+
     public Distance(int distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Distance distance1 = (Distance) o;
+        return distance == distance1.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
+    }
+
+    @Override
+    public String toString() {
+        return "Distance{" +
+                "distance=" + distance +
+                '}';
     }
 }
