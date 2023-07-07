2fd80385876c8b11d9e18b51012c53092503cd18
ProtoSeo <proto_seo@naver.com>
2023-07-07 15:55:00 +0900

    test: Position을 활용해 원시값 포장하도록 수정

--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.car;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.MockRandom;
 
@@ -10,16 +11,18 @@ public class CarTest {
     private final int canNotMoveValue = 3;
 
     @Test
+    @DisplayName("자동차가 랜덤 값이 4이상인 경우 이동한다.")
     void carTest_move() {
         Car car = new Car(new CarName("hello"), new MockRandom(canMoveValue));
         car.move();
-        assertThat(car.getPosition()).isEqualTo(2);
+        assertThat(car.getPosition()).isEqualTo(new Position(2));
     }
 
     @Test
+    @DisplayName("자동차가 랜덤 값이 3 이하인 경우 이동할 수 없다.")
     void carTest_not_move() {
         Car car = new Car(new CarName("hello"), new MockRandom(canNotMoveValue));
         car.move();
-        assertThat(car.getPosition()).isEqualTo(1);
+        assertThat(car.getPosition()).isEqualTo(new Position(1));
     }
 }
--- src/test/java/racingcar/car/CarsTest.java
+++ src/test/java/racingcar/car/CarsTest.java
@@ -1,19 +1,21 @@
 package racingcar.car;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
 
     @Test
+    @DisplayName("자동차들 중 우승자가 포함되어 있는지 확인한다.")
     void carsTest_getWinnerNames() {
-        List<String> carNames = List.of("pobi", "honux");
-        Cars cars = new Cars(carNames.stream().map(CarName::new).collect(Collectors.toList()));
+        List<CarName> carNames = List.of(new CarName("pobi"), new CarName("honux"));
+        Cars cars = new Cars(carNames);
 
         String[] winners = cars.getWinnerNames().toArray(new String[0]);
-        Assertions.assertThat(carNames).contains(winners);
+        assertThat(carNames.stream().map(CarName::getValue).toArray()).contains(winners);
     }
 }
