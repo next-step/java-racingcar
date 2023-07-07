36c8063c4ca7fd27b5108e1eedc3fea185c8f535
devxb <dlwnsdud205@naver.com>
2023-07-07 09:47:39 +0900

    [refactor] : request에 누락된 equals, hashcode, toString을 추가한다

--- src/main/java/racingcar/controller/request/RacingGamePlayControllerRequest.java
+++ src/main/java/racingcar/controller/request/RacingGamePlayControllerRequest.java
@@ -1,6 +1,7 @@
 package racingcar.controller.request;
 
 import java.util.List;
+import java.util.Objects;
 
 public class RacingGamePlayControllerRequest {
 
@@ -20,4 +21,28 @@ public class RacingGamePlayControllerRequest {
         return carNames;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof RacingGamePlayControllerRequest)) {
+            return false;
+        }
+        RacingGamePlayControllerRequest that = (RacingGamePlayControllerRequest) o;
+        return round == that.round && Objects.equals(carNames, that.carNames);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(round, carNames);
+    }
+
+    @Override
+    public String toString() {
+        return "RacingGamePlayControllerRequest{" +
+            "round=" + round +
+            ", carNames=" + carNames +
+            '}';
+    }
 }
