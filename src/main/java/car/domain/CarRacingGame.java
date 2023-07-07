ec2d8484929e812bbf22accf9edaa76d326de8b1
Geon <gunner6603@snu.ac.kr>
2023-07-08 02:07:03 +0900

    Feat: 검증 로직 추가

--- src/main/java/car/domain/CarRacingGame.java
+++ src/main/java/car/domain/CarRacingGame.java
@@ -1,14 +1,11 @@
 package car.domain;
 
-import car.domain.Car;
-import car.domain.CarFormatter;
-import car.domain.Cars;
-import car.domain.Name;
 import car.domain.winnerstrategy.MaxPositionDuplicateWinnerStrategy;
 import car.domain.winnerstrategy.WinnerStrategy;
+import utils.random.RandomGenerator;
+
 import java.util.List;
 import java.util.stream.Collectors;
-import utils.random.RandomGenerator;
 
 public class CarRacingGame {
 
@@ -19,26 +16,41 @@ public class CarRacingGame {
 
     public CarRacingGame(final List<String> carNames, final int playCount) {
         this.cars = new Cars(carNames);
+        validatePositive(playCount);
         this.leftPlayCount = playCount;
     }
 
     public void playRaceOnce() {
+        if (isFinished()) {
+            throw new IllegalStateException("남은 실행 횟수가 0이므로 더 이상 실행할 수 없습니다.");
+        }
+
         cars.move(randomGenerator);
         leftPlayCount--;
     }
 
-    public List<String> formatCars(CarFormatter formatter) {
+    public List<String> formatCars(final CarFormatter formatter) {
         return cars.format(formatter);
+    }
+
+    public List<Name> resolveWinnerNames() {
+        final List<Car> winners = cars.selectWinners(winnerStrategy);
+        return winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.toList());
     }
 
     public boolean isNotFinished() {
         return leftPlayCount > 0;
     }
 
-    public List<Name> resolveWinnerNames() {
-        final List<Car> winners = cars.selectWinners(winnerStrategy);
-        return winners.stream()
-            .map(Car::getName)
-            .collect(Collectors.toList());
+    private boolean isFinished() {
+        return leftPlayCount == 0;
+    }
+
+    private void validatePositive(final int playCount) {
+        if (playCount <= 0) {
+            throw new IllegalArgumentException(String.format("실행 횟수는 양수여야 합니다. 입력한 데이터 : %d", playCount));
+        }
     }
 }
