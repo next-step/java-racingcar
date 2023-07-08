1d38e6a1bcae1846f6f42985e95868803ea1d4e2
rimrim990 <lim990718@naver.com>
2023-07-08 19:40:56 +0900

    Test: 테스트 리팩토링

--- src/test/java/racingcar/car/domain/NameTest.java
+++ src/test/java/racingcar/car/domain/NameTest.java
@@ -1,6 +1,7 @@
 package racingcar.car.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,14 +10,14 @@ class NameTest {
     @Test
     @DisplayName("자동차 이름은 5자를 초과할 경우 예외를 던진다.")
     void name_givenOver5Length_throwException() {
-        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+        assertThatExceptionOfType(IllegalArgumentException.class)
             .isThrownBy(() -> new Name("qwerty"));
     }
 
     @Test
     @DisplayName("자동차 이름은 5자 이하여야 한다.")
     void name_givenBelowThan5Length_doesNotThrowException() {
-        Assertions.assertThatNoException()
+        assertThatNoException()
             .isThrownBy(() -> new Name("abcde"));
     }
 }
