e645b8379ee1bd98de60224fd259ea6081bd1178
dongjoo0-0 <kdj486347@gmail.com>
2023-07-09 00:17:46 +0900

    fix: fix to print result only after run game

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -9,9 +9,11 @@ import java.util.function.Supplier;
 
 public class RacingCarController {
 
+    private static final String ERROR_GAME_NOT_RUN = "게임이 정상적으로 실행되지 않았습니다. 게임 실행 후 출력 가능합니다.";
+
     private final Game game;
-    private List<List<CarStatus>> gameStatus;
-    private List<CarName> winners;
+    private List<List<CarStatus>> gameStatus = null;
+    private List<CarName> winners = null;
 
     public RacingCarController() {
         this(processInput());
@@ -41,7 +43,27 @@ public class RacingCarController {
         winners = game.findWinnerNames();
     }
 
-    public void printResult() {
+    /** runGame() 실행 이후 실행해야 정상적으로 게임 결과를 출력합니다. */
+    public void tryPrintResult() {
+        try {
+            printResult();
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void printResult() {
+        if (isNullStatus() || isEmptyStatus()) {
+            throw new IllegalStateException(ERROR_GAME_NOT_RUN);
+        }
         OutputView.printResult(gameStatus, winners);
+    }
+
+    private boolean isNullStatus() {
+        return gameStatus == null || winners == null;
+    }
+
+    private boolean isEmptyStatus() {
+        return gameStatus.isEmpty() || winners.isEmpty();
     }
 }
