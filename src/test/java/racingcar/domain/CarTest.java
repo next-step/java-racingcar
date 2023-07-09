086fa0bc68d29f8cf6ad1fa8b1b31677b35eba8a
ProtoSeo <proto_seo@naver.com>
2023-07-09 21:16:45 +0900

    test: 이동 전략에 따라 테스트 수정

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -6,22 +6,20 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
-    private static final int CAN_MOVE_VALUE = 4;
-    private static final int CAN_NOT_MOVE_VALUE = 3;
 
     @Test
-    @DisplayName("자동차가 랜덤 값이 4이상인 경우 이동한다.")
+    @DisplayName("자동차가 조건에 맞으면 이동한다.")
     void carTest_move() {
         Car car = new Car(new CarName("hello"), new Position());
-        car.move(CAN_MOVE_VALUE);
+        car.move(() -> true);
         assertThat(car.getPosition()).isEqualTo(new Position(2));
     }
 
     @Test
-    @DisplayName("자동차가 랜덤 값이 3 이하인 경우 이동할 수 없다.")
+    @DisplayName("자동차가 조건에 맞지 않으면 이동할 수 없다.")
     void carTest_not_move() {
         Car car = new Car(new CarName("hello"), new Position());
-        car.move(CAN_NOT_MOVE_VALUE);
+        car.move(() -> false);
         assertThat(car.getPosition()).isEqualTo(new Position(1));
     }
 }
