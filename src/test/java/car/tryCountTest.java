a0f07d365f036f881ba0365cb5468100ca7ac126
worldii <bbc2788@naver.com>
2023-07-09 22:35:24 +0900

    Test : tryCount 메서드 추가

--- src/test/java/car/tryCountTest.java
+++ src/test/java/car/tryCountTest.java
@@ -35,4 +35,25 @@ public class tryCountTest {
         // when & then
         assertEquals(c1, c2);
     }
+
+    @Test
+    @DisplayName("isPositive 메소드 정상 동작 확인")
+    void isPositive_정상_동작_확인() {
+        assertEquals(new TryCount(1).isPositive(), true);
+    }
+
+
+    @Test
+    @DisplayName("decrease 메소드 정상 동작 확인")
+    void decreseNumber_메소드_정상_동작_확인() {
+        int countNum = 2;
+        int decreseNum = countNum-1;
+        TryCount count = new TryCount(countNum);
+        TryCount anotherCount = new TryCount(decreseNum);
+
+        count = count.decreaseCount();
+
+        assertEquals(count, anotherCount);
+    }
+
 }
