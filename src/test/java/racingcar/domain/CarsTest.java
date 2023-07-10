d601160d6624a43be781f6b361073905e63d7b04
dahyen0o <seeunchoi99@gmail.com>
2023-07-10 21:33:42 +0900

    test: '우승 자동차 선택' 테스트 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -16,4 +16,19 @@ class CarsTest {
         // when & then
         assertThatNoException().isThrownBy(() -> Cars.of(carNames));
     }
+
+    @Test
+    void 가장높은position을가지는자동차를선택할때_성공() {
+        // given
+        final String[] carNames = new String[]{"pobi", "crong", "honux"};
+        final Cars cars = Cars.of(carNames);
+        final Car car = cars.getCars().get(0);
+        car.moveForward();
+
+        // when
+        List<Car> maxPositionCars = cars.findMaxPositionCars();
+
+        // then
+        assertThat(maxPositionCars).hasSize(1).contains(car);
+    }
 }
