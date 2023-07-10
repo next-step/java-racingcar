cfb2e3c748955d03381fa2485894ebc525d50aa0
worldii <bbc2788@naver.com>
2023-07-11 01:01:50 +0900

    Refactor : Controller 리팩토링에 따른 메인 변경

--- src/main/java/car/Main.java
+++ src/main/java/car/Main.java
@@ -3,9 +3,10 @@ package car;
 import car.controller.RacingController;
 
 public class Main {
+
     public static void main(String[] args) {
         try {
-            new RacingController().run();
+            new RacingController().start();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
