f44198dff19b289eb1e9a2a3756ce0afa2dc6f30
rkd5656meta <rkd8307@naver.com>
2023-07-30 16:17:09 +0900

    refactor: 자동차 대수, 중복 유효성 검사 추가 및 String 배열 생성자 추가

--- src/main/java/edu/nextstep/camp/racing/domain/Cars.java
+++ src/main/java/edu/nextstep/camp/racing/domain/Cars.java
@@ -3,45 +3,67 @@ package edu.nextstep.camp.racing.domain;
 import edu.nextstep.camp.racing.view.OutputView;
 import edu.nextstep.camp.racing.view.RacingOutputView;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Cars {
+
+    private static final int CAR_MIN_COUNT = 2;
+    private static final int CAR_MAX_COUNT = 20;
 
     private final Random random = new Random();
 
     private final List<Car> cars;
 
-    private int maxPosition;
-
-    public Cars(List<String> carNames) {
-        cars = carNames.stream()
+    public Cars(String... carNames) {
+        this(Arrays.stream(carNames)
                 .map(Car::new)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
+    }
+
+    public Cars(List<Car> cars){
+        nameCountCheck(cars);
+        duplicateNameCheck(cars);
+        this.cars = cars;
+    }
+
+    private static void nameCountCheck(List<Car> carNames) {
+        int carCount = carNames.size();
+
+        if (CAR_MIN_COUNT > carCount || carCount > CAR_MAX_COUNT) {
+            throw new IllegalArgumentException("자동차 수는 2~20대로 설정 하세요.");
+        }
+    }
+
+    private static void duplicateNameCheck(List<Car> carNames) {
+        Set<Car> carNamesSet = new HashSet<>(carNames);
+
+        if (carNamesSet.size() != carNames.size()) {
+            throw new IllegalArgumentException("자동차는 중복될 수 없습니다.");
+        }
     }
 
     public void moveCars() {
-        cars.forEach(car->{
+        cars.forEach(car -> {
             car.move(randomInt());
-
-            maxPosition = car.maxPosition(maxPosition);
-
-            RacingOutputView.printCurrentPosition(car);
         });
-
-        // 개행
-        OutputView.print("");
     }
 
-    public List<String> winners(){
-        return cars.stream()
-                .filter(car->car.currentPosition() == maxPosition)
-                .map(Car::getName)
-                .collect(Collectors.toList());
+    public List<String> winners() {
+        Optional<Car> carOpt = cars.stream()
+                .max((car, compareCar) -> car.maxPosition(compareCar.currentPosition()));
+
+        return cars.stream().filter(c -> c.isWinner(carOpt.get())).map(Car::getName).collect(Collectors.toList());
     }
 
-    protected int randomInt(){
+    protected int randomInt() {
         return random.nextInt(10);
+    }
+
+    @Override
+    public String toString() {
+        return cars.stream()
+                .map(Car::toString)
+                .collect(Collectors.joining("\n"));
     }
 }
