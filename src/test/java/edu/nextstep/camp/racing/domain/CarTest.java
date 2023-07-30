3ce063cb36ef48f9fb59df9cd53bfd5850cc57fd
rkd5656meta <rkd8307@naver.com>
2023-07-30 16:09:45 +0900

    test: 이름 길이 및 공백 검증 테스트 추가

--- src/test/java/edu/nextstep/camp/racing/domain/CarTest.java
+++ src/test/java/edu/nextstep/camp/racing/domain/CarTest.java
@@ -4,18 +4,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class CarTest {
 
     private Car car;
 
     @BeforeEach
-    void init(){
+    void init() {
         car = new Car("KBC");
     }
 
     @Test
-    void 전진(){
+    void 전진() {
         car.move(4);
         int currentPosition = car.currentPosition();
 
@@ -23,10 +25,19 @@ public class CarTest {
     }
 
     @Test
-    void 정지(){
+    void 정지() {
         car.move(2);
         int currentPosition = car.currentPosition();
 
         assertThat(currentPosition).isEqualTo(0);
+    }
+
+    @Test
+    void 운전자_이름_길이_및_공백_검증() {
+        assertAll(
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> new Car("RIDER1  ")),
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> new Car("   ")),
+                () -> assertThatIllegalArgumentException().isThrownBy(() -> new Car("RIDER1234"))
+        );
     }
 }
