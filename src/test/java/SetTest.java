1537a9122a3066115a6df1fa0f3f583905e66913
sunggyupaik <melon9751@naver.com>
2023-10-31 01:29:15 +0900

    [feat] Set Collection에 대한 학습 테스트를 구현하라
    Set Collection에 대한 3가지 요구사항을 구현합니다

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	void setTest1() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void setTest2(int number) {
+		assertThat(numbers).contains(number);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false", "6:false"}, delimiter = ':')
+	void setTest3(int number, boolean contained) {
+		boolean actual = numbers.contains(number);
+		assertThat(actual).isEqualTo(contained);
+	}
+}
