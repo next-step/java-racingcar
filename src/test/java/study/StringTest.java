301d7ab676d8a68229c569191ae5210f266edd9f
yumble <hnjn1101@gmail.com>
2023-10-30 21:17:07 +0900

    request3 test
    - charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현
    - StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현
    - JUnit의 @DisplayName을 활용

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -20,5 +22,18 @@ public class StringTest {
         String str = "(1,2)";
         String result = str.substring(1, str.length()-1);
         assertThat(result).isEqualTo("1,2");
+    }
+    @Test
+    @DisplayName("charAt 함수를 이용하여 올바른 char 가져오는지 확인 테스트")
+    void charAtTest() {
+        Character result = "abc".charAt(1);
+        assertThat(result).isEqualTo('b');
+    }
+    @Test
+    @DisplayName("charAt 함수를 이용할 때, 문자 길이보다 긴 Index 입력 시, IndexOutOfBoundsException 에러가 발생하는지 테스트")
+    void charAtErrorTest() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
     }
 }
