39c21f9bd1eec37f1e6a69d56d6828e422194372
jikimee64 <jikimee64@gmail.com>
2023-10-30 17:19:12 +0900

    Test: Set contains 테스트

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -21,13 +23,21 @@ public class SetTest {
 
     @Test
     @DisplayName("성공 - 중복이 제거된 Set 크기를 확인할 수 있다.")
-    void success_set_size_test(){
+    void success_set_size_test() {
         //when
         int size = numbers.size();
         int expected = 3;
 
         //then
         assertThat(size).isEqualTo(expected);
+    }
+
+    @DisplayName("성공 - Set에 1,2,3 값이 존재 하는지 확인 한다.")
+    @ValueSource(ints = {1, 2, 3})
+    @ParameterizedTest
+    void success_set_contains_test(int given) {
+        //when & then
+        assertThat(numbers.contains(given)).isTrue();
     }
 
 }
