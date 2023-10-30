9764ba232ae50440d448c7956faf5359d5617a82
choomi1217 <whdudal1217@naver.com>
2023-10-31 00:22:49 +0900

    Set containsSuccess test

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -3,11 +3,14 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetCollectionTest {
 
@@ -26,5 +29,12 @@ public class SetCollectionTest {
     @DisplayName("Set의 크기를 측정")
     void size(){
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("값이 존재하는지 확인 - 성공 케이스")
+    @ParameterizedTest(name = "set has {0}? {1}")
+    @ValueSource(ints = {1,2,3})
+    void containsSuccess(int expected){
+        assertTrue(numbers.contains(expected));
     }
 }
