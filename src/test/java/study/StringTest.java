745f3f4f42933fdb7a09467a7c4c24519104bb33
birdieHyun <flaehdan@gmail.com>
2023-10-30 12:53:20 +0900

    test: charAt() 테스트 (#1)

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class StringTest {
@@ -36,5 +37,24 @@ class StringTest {
 
         // then
         assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() 메서드를 테스트한다.")
+    void charAt() {
+        // given
+        String data = "abc";
+
+        // when
+
+        // then
+        assertAll(
+                () -> assertThat(data.charAt(0)).isEqualTo('a'),
+                () -> assertThat(data.charAt(1)).isEqualTo('b'),
+                () -> assertThat(data.charAt(2)).isEqualTo('c'),
+                () -> assertThatThrownBy(() -> data.charAt(3))
+                        .isInstanceOf(IndexOutOfBoundsException.class)
+                        .hasMessageContaining("String index out of range: 3")
+        );
     }
 }
