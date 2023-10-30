4018f7fc0afcf27215a8d544c2dac2647cf11527
chocheolhee <whcjfgml92@naver.com>
2023-10-30 22:55:34 +0900

    1단계 요구사항 1 테스트 코드 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,32 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
     @Test
-    void split() {
+    @DisplayName("1,2을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 테스트 - contains()")
+    void split_1() {
         String[] result = "1,2".split(",");
-        Assertions.assertThat(result).containsExactly("1", "2");
+        assertThat(result).contains("1", "2");
     }
+
+    @Test
+    @DisplayName("1,2을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 테스트 - containsExactly()")
+    void split_2() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("1을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 테스트")
+    void split_3() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
 }
