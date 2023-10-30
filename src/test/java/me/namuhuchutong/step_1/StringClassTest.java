0277c250791ed5e4fa99f43662bb654cfe6bc8dd
namuhuchutong <namuhuchutong@gmail.com>
2023-10-30 20:35:19 +0900

    test: 문자열 split 학습 테스트

---
+++ src/test/java/me/namuhuchutong/step_1/StringClassTest.java
@@ -0,0 +1,31 @@
+package me.namuhuchutong.step_1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringClassTest {
+
+    @DisplayName(",가 포함된 문자열이 ,로 split했을 때 잘 분리되는지 확인")
+    @ParameterizedTest(name = "{index} - given: \"{0}\", result: {1}")
+    @MethodSource("givenStringAndExpectedArray")
+    void split_string_with_comma(String given, String[] expected) {
+        //when
+        String[] result = given.split(",");
+
+        //then
+        assertThat(result).contains(expected);
+    }
+
+    static Stream<Arguments> givenStringAndExpectedArray() {
+        return Stream.of(
+                Arguments.arguments("1,2", new String[] {"1", "2"}),
+                Arguments.arguments("1", new String[] {"1"})
+        );
+    }
+}
