0a3c94cf00c9fadb5021cc510712ffc54b45b66b
yumble <hnjn1101@gmail.com>
2023-10-30 21:10:26 +0900

    request2: (1,2)  -> 1,2로 괄호를 제거하는지 substring test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -15,4 +15,10 @@ public class StringTest {
         String[] result = "1".split(",");
         assertThat(result).containsExactly("1");
     }
+    @Test
+    void split3() {
+        String str = "(1,2)";
+        String result = str.substring(1, str.length()-1);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
