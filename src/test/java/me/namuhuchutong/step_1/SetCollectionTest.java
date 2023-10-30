d592785feaff1311fa8f5afb08076dea252064d2
namuhuchutong <namuhuchutong@gmail.com>
2023-10-30 21:56:57 +0900

    test: Set contains 학습 테스트

--- src/test/java/me/namuhuchutong/step_1/SetCollectionTest.java
+++ src/test/java/me/namuhuchutong/step_1/SetCollectionTest.java
@@ -3,6 +3,8 @@ package me.namuhuchutong.step_1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -33,5 +35,16 @@ public class SetCollectionTest {
 
         //then
         assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("주어진 numbers는 1,2,3 값이 존재하면 true를 반환")
+    @ParameterizedTest(name = "numbers contains {0}")
+    @ValueSource(ints = {1, 2, 3})
+    void return_true_when_element_contains(int element) {
+        //when
+        boolean result = numbers.contains(element);
+
+        //then
+        assertThat(result).isTrue();
     }
 }
