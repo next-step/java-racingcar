7e8e5d6b79b454d76bc1d6fc15fca0a35c868767
solchan98 <dev.sol0427@gmail.com>
2023-10-30 22:52:54 +0900

    StringTest 불필요 임포트 구문 및 주석 제거

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,11 +1,7 @@
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -75,6 +71,6 @@ class StringTest {
 
         // when then
         assertThatThrownBy(() -> value.charAt(invalidIndex))
-                .isInstanceOf(StringIndexOutOfBoundsException.class);// then
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
