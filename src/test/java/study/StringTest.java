096948a8ffee8f3baffec4342de3e94239eba6d3
yumble <hnjn1101@gmail.com>
2023-10-30 21:08:06 +0900

    split ver2 test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,9 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
     }
+    @Test
+    void split2() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
 }
