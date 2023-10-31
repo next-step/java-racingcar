3f4a0680a63c6d679274944c2b960e52ae4f5179
chunghyunleeme <chunghyun.dev@gmail.com>
2023-10-31 16:18:50 +0900

    size study test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,33 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @DisplayName("size 테스트")
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
