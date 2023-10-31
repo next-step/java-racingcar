a527aeba167f158386dbde58fab5fa26eebd9619
JongWoonLee <59693666+JongWoonLee@users.noreply.github.com>
2023-10-31 21:32:18 +0900

    1단계 - 학습 테스트 실습 (#4803)
    * initial commit
    
    * String 클래스에 대한 학습 테스트
    
    * Set Collection에 대한 학습 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,50 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인")
+    @Test
+    void setSizeTest() {
+        assertEquals(numbers.size(), 3);
+    }
+
+
+    @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현 및 코드 중복 제거")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void valueContainsTest(Integer input) {
+        assertTrue(numbers.contains(input));
+    }
+
+    @DisplayName("1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void csvValueContainsTest(int input, Boolean expected) {
+        assertEquals(numbers.contains(input), expected);
+    }
+
+}
