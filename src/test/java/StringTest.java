ca28605493032303e09917db9800e48e718af2ad
kmj990929 <kmj990929@dgist.ac.kr>
2023-10-31 22:52:20 +0900

    add substring test

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -13,4 +13,11 @@ public class StringTest {
         String[] noSplitResult = noSplitString.split(",");
         assertThat(noSplitResult).containsExactly("1");
     }
+
+    @Test
+    void substringTest() {
+        String inputString = "(1,2)";
+        String substringResult = inputString.substring(1,4);
+        assertThat(substringResult).isEqualTo("1,2");
+    }
 }
