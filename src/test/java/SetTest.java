bee1c2901a0c4fd8cb7599fb5c75b83bbb1fe5a8
Hanna Choi <44824602+gkssk4163@users.noreply.github.com>
2023-11-01 00:53:11 +0900

    test: SetTest 요구사항2 구현

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -23,5 +25,12 @@ public class SetTest {
     @DisplayName("Set 크기 확인. 값이 중복인 경우 하나만 저장")
     void Check_set_size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set에 데이터 존재여부 확인")
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
