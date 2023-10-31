e617219323971c616b297241501242b0e11114f1
hyunyyxp <qqqq_o@naver.com>
2023-10-31 14:56:12 +0900

    Feat: Set Collection에 대한 테스트 수정

--- src/test/java/SetCollectionTest.java
+++ src/test/java/SetCollectionTest.java
@@ -25,14 +25,14 @@ class SetCollectionTest {
     @Test
     @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.")
     void sizeTest(){
-        assertThat(numbers.size()).isEqualTo(3);
+        assertThat(numbers).hasSize(3);
     }
     
     @ParameterizedTest
-    @DisplayName("JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.")
     @ValueSource(ints = {1, 2, 3})
+    @DisplayName("JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.")
     void useParameterizedTest(int number) {
-        assertThat(numbers.contains(number)).isTrue();
+        assertThat(numbers).contains(number);
     }
 
     @ParameterizedTest
