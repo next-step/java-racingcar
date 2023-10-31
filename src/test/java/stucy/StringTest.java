8b22c253d419eb1620b498b8ae9ec456dfafd106
jeonghun.tech <jeonghun.tech@gmail.com>
2023-10-31 20:06:56 +0900

    split study test

---
+++ src/test/java/stucy/StringTest.java
@@ -0,0 +1,22 @@
+package stucy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    void split() {
+        String[] result = "1,2,".split(",");
+        assertThat(result).contains("1", "2");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    public void subString(){
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+}
