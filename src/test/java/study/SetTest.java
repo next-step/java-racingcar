cdb86ab8925627bc4c50be5b3557c7d71fd3de24
chocheolhee <whcjfgml92@naver.com>
2023-10-31 21:45:17 +0900

    1단계 Set 학습테스트 요구사항 1

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,33 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 메서드를 활용하여 중복된 값을 제외하고 3을 출력한다.")
+    void size() {
+        assertThat(numbers).hasSize(3);
+    }
+
+}
