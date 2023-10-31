124ab3cea9076bdbf2e3e2a551ff9a186c93702a
ms-93@hanmail.net <ms-93@hanmail.net>
2023-10-31 22:42:47 +0900

    1단계:split study test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,43 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import java.util.*;
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("요구사항1 : size 테스트")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("요구사항2 : 중복코드 제거 테스트")
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("요구사항3 : 입력값에 따라 결과가 다른 테스트")
+    void notContains(int input, boolean output) {
+        assertThat(numbers.contains(input)).isEqualTo(output);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,35 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+    @Test
+    @DisplayName("요구사항1 : split 테스트")
+    void split() {
+        String[]  result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    @DisplayName("요구사항2 : substring 테스트")
+    void substring() {
+        String given = "(1,2)";
+        String result = given.substring(1, given.length()-1);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    // 요구사항 3
+    @Test
+    @DisplayName("요구사항3 : 특정위치 문자 가져오기")
+    void charAt() {
+        String given = "abc";
+        assertThat('a').isEqualTo(given.charAt(0));
+        assertThatThrownBy(() ->{
+            given.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+}
