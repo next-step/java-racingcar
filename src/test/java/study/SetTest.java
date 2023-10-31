cf3e79ed839fe769ef9e50c88d68e23a35e8da82
pilming44 <100845449+pilming44@users.noreply.github.com>
2023-10-31 09:19:40 +0900

    1단계 학습테스트 실습입니다. (#4795)
    * spilit study test
    
    * String 클래스에 대한 학습 테스트
    
    * Set Collection에 대한 학습 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("사이즈를 확인")
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("값이 존재하는지 확인")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @DisplayName("입력 값에 따라 값이 존재하는지 확인")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsValidation(int value, boolean flag) {
+        assertThat(numbers.contains(value)).isEqualTo(flag);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,59 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @DisplayName("한 단어 split")
+    @Test
+    void splitOnlyOne() {
+        String[] result = "1".split(",");
+        assertThat(result).isEqualTo("1");
+    }
+
+    @DisplayName("처음, 끝 substring 후 split")
+    @Test
+    void splitWithSubstring() {
+        String data = "(1,2)";
+        String[] result = data.substring(1, data.length()-1).split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @DisplayName("charAt 첫 인덱스")
+    @Test
+    void charAtStartIndex() {
+        char result = "abc".charAt(0);
+        assertThat(result).isEqualTo('a');
+    }
+
+    @DisplayName("charAt 마지막 인덱스")
+    @Test
+    void charAtLastIndex() {
+        String data = "abc";
+        char result = data.charAt(data.length()-1);
+        assertThat(result).isEqualTo('c');
+    }
+
+    @DisplayName("charAt 범위 내")
+    @Test
+    void charAtInOfBound() {
+        char result = "abc".charAt(1);
+        assertThat(result).isEqualTo('b');
+    }
+
+    @DisplayName("charAt 범위 밖")
+    @Test
+    void charAtOutOfBound() {
+        assertThatThrownBy(() -> {
+            char result = "abc".charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
