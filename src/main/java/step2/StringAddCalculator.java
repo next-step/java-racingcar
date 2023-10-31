04c993417a8874c9d0ccbc631e7f1e524c9bb280
MYUNGHOON JU <myunghoonju@gmail.com>
2023-11-01 01:27:43 +0900

    init commit

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,10 @@
+package step2;
+/**
+ *  tip. 메소드가 너무 많은 일을 하지 않도록 분리하기 위해 노력해 본다.
+ * */
+public class StringAddCalculator {
+
+    public static int splitAndSum(String data) {
+        return 0;
+    }
+}
