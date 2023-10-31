182f4b447638c8c5e867ababc33e001478f13802
tgHan <hantaegune@mf.seegene.com>
2023-10-31 13:06:49 +0900

    String 클래스에 대한 학습 테스트  - 요구사항 3 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -20,5 +22,15 @@ public class StringTest {
         assertThat(str).isEqualTo("1,2");
 
 
+    }
+
+    @Test
+    @DisplayName("charAt() 테스트")
+    void charAt() {
+        String chars = "abc";
+        assertThatThrownBy(() -> {
+            chars.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range:");
     }
 }
