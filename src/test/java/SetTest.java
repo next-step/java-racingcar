a0d3d22437096aae2fc48c019793d6743f553e90
Hanna Choi <44824602+gkssk4163@users.noreply.github.com>
2023-11-01 00:44:29 +0900

    test: SetTest 요구사항1 구현

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 크기 확인. 값이 중복인 경우 하나만 저장")
+    void Check_set_size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
