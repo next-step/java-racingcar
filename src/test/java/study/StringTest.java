7dcc12059f6d6fb9c4eb93030fbc5b66ab3aa6bf
ChangHoon Choi <69202388+hoon25@users.noreply.github.com>
2023-10-31 23:33:47 +0900

    🚀 1단계 - 학습 테스트 실습 (#4817)
    * test(string): split
    
    * test(string): substring
    
    * test(string): charAt Success
    
    * test(string): charAt Fail
    
    * test(set): init
    
    * test(set): size
    
    * test(set): contains success
    
    * test(set): contains false
    
    * test(set): contains csvTest
    
    * fix(set): parameterizedTest name

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,36 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt(성공) - String의 index내로 CharAt으로 특정위치의 문자를 가져온다")
+    void charAtSuccess() {
+        char result = "abc".charAt(0);
+        assertThat(result).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("charAt(실패) - String의 index범위밖으로 CharAt을 사용하면 StringIndexOutOfBoundsException이 발생한다")
+    void charAtFail() {
+        assertThatThrownBy(() -> "abc".charAt(10))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 10");
+    }
+}
