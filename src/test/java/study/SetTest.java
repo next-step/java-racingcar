ef1ea1626874a61525485ea904e1b4774803172d
chocheolhee <whcjfgml92@naver.com>
2023-10-31 21:59:39 +0900

    1단계 Set 학습테스트 요구사항 3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -35,8 +36,14 @@ public class SetTest {
     @ParameterizedTest
     @DisplayName("contains() 메소드를 활용하여 1, 2, 3의 존재 유무를 확인한다.")
     @ValueSource(strings = {"1", "2", "3"})
-    void contains(Integer input) {
+    void contains(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_CsvSource(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 
 }
