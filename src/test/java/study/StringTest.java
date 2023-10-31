679c6747803136eeb263cc5c149f96ea9b269fa4
ByungChan Lee <lbc2415@gmail.com>
2023-11-01 00:06:11 +0900

    added string1 test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class StringTest {
 
     @Test
-    void split () {
+    void split() {
         String[] testData1 = "1,2".split(",");
 
         assertThat(testData1).contains("1");
@@ -17,6 +17,14 @@ public class StringTest {
 
         assertThat(testData2).contains("1");
         assertThat(testData2).containsExactly("1");
+    }
+
+    @Test
+    void string1() {
+        String testData1 = "(1,2)";
+        String result = testData1.substring(1,4);
+
+        assertThat(result).isEqualTo("1,2");
     }
 
 }
