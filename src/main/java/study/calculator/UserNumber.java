b83d83066e1fd602e186b1b2d3c58d28353382d3
choomi1217 <whdudal1217@naver.com>
2023-11-01 01:43:05 +0900

    feat: UserNumber 추가

--- src/main/java/study/calculator/UserNumber.java
+++ src/main/java/study/calculator/UserNumber.java
@@ -1,2 +1,29 @@
-package study.calculator;public class UserNumber {
+package study.calculator;
+
+public class UserNumber {
+
+    private final int number;
+
+    private UserNumber(int number) {
+        this.number = number;
+    }
+
+    public static UserNumber from(String userString){
+        return new UserNumber(parsing(userString));
+    }
+
+    private static int parsing(String userString) {
+        if(isBlank(userString)){
+            return 0;
+        }
+        return Integer.parseInt(userString);
+    }
+
+    private static boolean isBlank(String userString) {
+        return userString == null || userString.isEmpty() || userString.isBlank();
+    }
+
+    public int number() {
+        return this.number;
+    }
 }
--- src/test/java/study/calculator/UserNumberTest.java
+++ src/test/java/study/calculator/UserNumberTest.java
@@ -1,2 +1,33 @@
-package study.calculator;public class UserNumberTest {
+package study.calculator;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserNumberTest {
+
+    @DisplayName("empty, null은 0")
+    @NullAndEmptySource
+    @ParameterizedTest (name = " \"{0}\" is return zero")
+    void emptyAndNullMustReturnZero(String target){
+        UserNumber userNumber = UserNumber.from(target);
+        int number = userNumber.number();
+        assertThat(number).isZero();
+    }
+
+    @DisplayName("한자리면 해당 숫자를 반환")
+    @CsvSource(value = {"1,1", "10,10"})
+    @ParameterizedTest
+    void singleStringMustReturnSingleDigit(String target, String expected){
+        UserNumber userNumber = UserNumber.from(target);
+        int number = userNumber.number();
+        assertThat(number).isEqualTo(Integer.parseInt(expected));
+    }
+
+
 }
