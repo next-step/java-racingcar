36826b3c22b344c431a942d7f9fbc883bb87ee94
chocheolhee <whcjfgml92@naver.com>
2023-10-31 21:51:58 +0900

    1단계 Set 학습테스트 요구사항 2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -28,6 +30,13 @@ public class SetTest {
     @DisplayName("size() 메서드를 활용하여 중복된 값을 제외하고 3을 출력한다.")
     void size() {
         assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("contains() 메소드를 활용하여 1, 2, 3의 존재 유무를 확인한다.")
+    @ValueSource(strings = {"1", "2", "3"})
+    void contains(Integer input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 
 }
