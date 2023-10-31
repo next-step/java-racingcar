7ff80c1cf602176e052b85b73d708fedd700c0bd
hyn.xp <50633008+kyunghyun-Park@users.noreply.github.com>
2023-10-31 17:31:14 +0900

    🚀 1단계 - 학습 테스트 실습 PR (#4804)
    * Sttest:String
    
    * Test: Set Collection에 대한 테스트 코드 추가
    
    * Docs: README.md 수정
    
    * Fear: public 접근 제어자 제거
    
    * Feat: 요구사항3에 대한 테스트 코드 수정
    
    * Feat: Set Collection에 대한 테스트 수정

---
+++ src/test/java/StringClassTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringClassTest {
+
+    @Test
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
+    void split_contains() {
+        //given
+        String data = "1,2";
+
+        //when
+        String[] split = data.split(",");
+
+        //then
+        assertThat(split).contains("1");
+        assertThat(split).contains("2");
+    }
+    
+    @Test
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.")
+    void split_containsExactly() {
+        //given
+        String data = "1";
+
+        //when
+        String[] split = data.split(",");
+
+        //then
+        assertThat(split).containsExactly("1");
+    }
+    
+    @Test
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환하도록 구현한다.")
+    void substring() {
+        //given
+        String data = "(1,2)";
+
+        //when
+        String substring = data.substring(1, 4);
+
+        //then
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
+    void charAt(){
+        //given
+        String data = "abc";
+
+        //when
+        char c = data.charAt(2);
+
+        //then
+        assertThat(c).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
+    void charAt_exception(){
+        String data = "abc";
+
+        assertThatThrownBy(() -> data.charAt(3))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+}
