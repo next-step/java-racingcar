7591d568e2db8a2092a85e2da10a86a05837ad10
choomi1217 <whdudal1217@naver.com>
2023-11-01 22:24:24 +0900

    feat: StringAddCalculator 구현 완료

--- src/main/java/study/calculator/StringAddCalculator.java
+++ src/main/java/study/calculator/StringAddCalculator.java
@@ -1,8 +1,14 @@
 package study.calculator;
 
-public class StringAddCalculator {
+import java.util.List;
 
+public class StringAddCalculator {
     public static int splitAndSum(String origin) {
-        return 0;
+        if(origin == null || origin.isBlank()){
+            return 0;
+        }
+        StringDelimiter stringDelimiter = new StringDelimiter(new Delimiter(origin));
+        List<String> strings = stringDelimiter.filteredString(origin);
+        return UserNumbers.from(strings).sum();
     }
 }
