ddf4aee895ac935bb7f8685fd2cb3a15353e5480
mk_windows <16min99@gmail.com>
2023-11-01 20:45:04 +0900

    요구사항1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,6 +8,7 @@ public class StringTest {
     @Test
     void split() {
         String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
         assertThat(result).containsExactly("1","2"); // 순서보장
     }
 }
