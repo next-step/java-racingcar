4fa790e8cc766a6446fd6bffe5431111f53fae72
kimbro97 <rlagudwog@gmail.com>
2023-11-01 20:31:43 +0900

    feat: step2 TDD 문자열 덧셈 계산기 구현

---
+++ src/main/java/stringcalculator/StringAddCalculator.java
@@ -0,0 +1,32 @@
+package stringcalculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String text) {
+        int sum = 0;
+        if (isNullOrBlank(text)) return 0;
+        if (text.contains("-")) throw new RuntimeException("음수를 전달받은 경우 예외 발생");
+        for (String str : splitText(text)) {
+            sum += Integer.parseInt(str);
+        }
+        return sum;
+    }
+
+    private static boolean isNullOrBlank(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private static String[] splitText(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens= m.group(2).split(customDelimiter);
+            return tokens;
+        } else {
+            return text.split(",|:");
+        }
+    }
+}
