9b471dcd23d556c4f3ca5a7509114155b0b53d8b
sunga <eert2260@naver.com>
2023-11-01 22:20:26 +0900

    test : [Set Collection에 대한 학습 테스트] 요구사항1 - size 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+    }
+
+    @Test
+    @DisplayName("요구사항1 - size() 메소드로 Set 크기 확인 테스트")
+    void sizeTest(){
+        int numbersSize = numbers.size();
+        assertThat(numbersSize).isEqualTo(4);
+    }
+}
