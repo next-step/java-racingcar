a5161cc5ec3d23ecd1a38cab4abb05b75ae5dc06
hypernova1 <hypemova@gmail.com>
2023-11-01 17:50:23 +0900

    set test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,55 @@
+package study;
+
+import org.assertj.core.util.Strings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test()
+    @DisplayName("주어진 Set 데이터의 길이를 구하는 테스트")
+    void getSize() {
+        assertThat(this.numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("주어진 Set 내에 특정 값이 존재하는 지 확인하는 테스트")
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("파라미터 테스트를 통해서 Set 내에 값이 존재하는 지 확인하는 테스트")
+    @ValueSource(ints = {1, 2, 3})
+    void containsValueWithParameters(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("특정 값에 따라 다른 결과가 나오는지 확인하는 테스트")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsValueWithInputAndResult(int input, boolean result) {
+        assertThat(numbers.contains(input)).isEqualTo(result);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
 
     @Test
+    @DisplayName("문자열을 (,)로 split 했을 때 특정 값이 배열 내에 존재하는 지 확인하는 테스트")
     void split() {
         String[] results = "1,2".split(",");
         assertThat(results).contains("1");
@@ -14,15 +16,16 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("주어진 문자열 내에 문자를 charAt으로 찾았을 때 위치 값을 벗어나면 StringIndexOutOfBoundsException 예외가 발생하는지 확인하는 테스트")
     void substring() {
         String data = "abc";
 
         assertThatThrownBy(() -> {
             char character = data.charAt(3);
-        }).isInstanceOf(IndexOutOfBoundsException.class)
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
                 .hasMessageContaining("String index out of range: 3");
 
-        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
                 .isThrownBy(() -> {
                 char character = data.charAt(3);
         }).withMessageMatching("String index out of range: 3");
