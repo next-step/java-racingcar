b99e473a890d809a2556d360e8bb6fc96e28f669
joomin <joomin@joomins-MacBook-Pro.local>
2023-11-01 15:43:40 +0900

    subString test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,8 +9,21 @@ public class StringTest {
     @Test
     void spllit() {
         String[] result = "1,2".split(",");
+
+        assertThat(result).contains("1","2");
         assertThat(result).containsExactly("1","2");
     }
+
+    @Test
+    String subString() {
+        String result = "1,2".substring(0);
+        result = result.substring(-1);
+
+        assertThat(result).isEqualTo("1,2");
+
+        return result;
+    }
+
 
 
 }
