5e8ed68415de8b3adf3ad72a385a258a5b8a248a
mk_windows <16min99@gmail.com>
2023-11-01 20:57:20 +0900

    요구사항2 "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,26 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
     @Test
-    void split() {
+    void 요구사항1() {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
-        assertThat(result).containsExactly("1","2"); // 순서보장
+        assertThat(result).containsExactly("1", "2"); // 순서보장
+    }
+
+    @Test
+    void 요구사항2() {
+        String myString = "(1,2)";
+        String expectedResult = "1,2";
+
+        String result = myString.substring(1,4);
+
+        assertThat(result).isEqualTo(expectedResult);
     }
 }
