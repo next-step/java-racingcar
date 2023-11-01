f3f2340339fcb769d051e78a4196c2616d141471
geunnseung <bsg1017@naver.com>
2023-11-01 18:42:39 +0900

    set collection test

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,59 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetCollectionTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@DisplayName("size()메소드를 통해 크기를 확인한다")
+	@Test
+	void checkSize() {
+	    // given
+		int numbersSize = numbers.size();
+
+	    // when
+	    // then
+		assertThat(numbersSize).isEqualTo(3);
+	}
+
+	@DisplayName("contains()메소드를 통해 값이 존재하는지 확인한다")
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void checkContains(int targetNumbers) {
+	    // given
+	    // when
+	    // then
+		assertThat(numbers.contains(targetNumbers))
+				.isTrue();
+	}
+
+	@DisplayName("contains 메소드의 결과가 다른 경우")
+	@ParameterizedTest
+	@CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+	void checkContains2(int targetNumbers, boolean expectedResult) {
+	    // given
+	    // when
+	    // then
+		assertThat(numbers.contains(targetNumbers))
+				.isEqualTo(expectedResult);
+	}
+}
