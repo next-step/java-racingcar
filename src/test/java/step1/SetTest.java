d563e24b8fcc0429a8fba328de9df998ac362644
hyeongiii <hyeongiii0811@gmail.com>
2023-11-01 18:06:16 +0900

    step1: 학습 테스트 실습

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,52 @@
+package step1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setup() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @AfterEach
+    void clear() {
+        numbers.clear();
+    }
+
+    @Test
+    @DisplayName("size() 테스트")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains() 테스트 - ParameterizedTest 활용")
+    void contains(int num) {
+        assertThat(numbers.contains(num)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("contains() 테스트 - ParameterizedTest_CsvSource 활용")
+    void contains_CsvSource(int num, boolean expected) {
+        assertThat(numbers.contains(num)).isEqualTo(expected);
+    }
+}
