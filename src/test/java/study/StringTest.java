cc3c46de90998407b8fb1144984bff80af277d18
sudo-init <dep.em.k.0905@gmail.com>
2023-11-01 20:12:17 +0900

    split study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,7 +7,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
-    void split() {
+    public void split1() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+    }
+
+    @Test
+    public void split2() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
