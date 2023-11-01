0cb7bb154f4a33d6c02cd3a59133e476d94f6863
hypernova1 <hypemova@gmail.com>
2023-11-01 17:32:17 +0900

    substring study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,7 +2,7 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
@@ -11,5 +11,21 @@ public class StringTest {
         String[] results = "1,2".split(",");
         assertThat(results).contains("1");
         assertThat(results).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String data = "abc";
+
+        assertThatThrownBy(() -> {
+            char character = data.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                char character = data.charAt(3);
+        }).withMessageMatching("String index out of range: 3");
+
     }
 }
