cfb5206546819a676c3eff1d411d12a8670849aa
joomin <joomin@joomins-MacBook-Pro.local>
2023-11-01 15:26:27 +0900

    split study test

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,16 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StringTest {
+
+    @Test
+    void spllit() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1","2");
+    }
+
+
+}
