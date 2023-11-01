0b040e5cedcd4c7fa07e3e1dd6a2b429554540fa
jm <67771030+jmnl225@users.noreply.github.com>
2023-11-01 17:29:22 +0900

    step1: 학습테스트 실습 (#4834)
    * split study test
    
    * subString test
    
    * charAt test
    
    * SetTest add and test
    
    ---------
    
    Co-authored-by: joomin <joomin@joomins-MacBook-Pro.local>

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,58 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+
+        assertThat(result).contains("1","2");
+        assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    String subString() {
+        String result = "1,2".substring(0);
+        result = result.substring(-1);
+
+        assertThat(result).isEqualTo("1,2");
+
+        return result;
+    }
+
+    @Test
+    @DisplayName("요구사항3-1: charAt() 메소드를 활용해 특정 위치의 문자 가져오는 테스트")
+    void charAt(){
+        String str = "abc";
+        char result = str.charAt(1);
+
+        assertThat(result).isEqualTo('b');
+    }
+
+
+    @Test
+    @DisplayName("요구사항3-2: 위치 값을 벗어나면 StringIndexOutOfBoundsException발생 테스트")
+    void charAtException(){
+        String str = "abc";
+        int index = 5;
+
+        assertThatThrownBy(() -> {
+            char result = str.charAt(index);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("조회하는 스트링 size: "+str.length());
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    char result = str.charAt(index);
+                }).withMessageMatching("Index: "+index + " Size: "+str.length());
+    }
+
+
+}
