cf682df44e6e06b05866a95df6af1c319522a1af
taegoon <hantaegune@gmail.com>
2023-11-01 22:27:40 +0900

    중복 데이터 최소화

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -7,23 +7,16 @@ public class StringAddCalculator {
 
     public static int splitAndSum(String text){
         int sum =0;
-        boolean integerOrNot = false;
 
         // 1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)
-        if (text == null){
-            return sum;
-        }
-        if (text.isEmpty()) {
-            return sum;
-        }
+        if (text == null ||text.isEmpty()) return sum;
 
         // 6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)
         if (text.contains("-")) throw new RuntimeException("음수를 전달할 경우 RuntimeException 예외가 발생!!");
 
         // 2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)
-        integerOrNot = text.matches("-?\\d+");
-        if(text.length()==1 && integerOrNot){
-            int number = Integer.parseInt(text);
+        if(text.length()==1 && integerOrNot(text)){
+            int number = convertStringToInt(text);
             return number;
         }
 
@@ -32,15 +25,8 @@ public class StringAddCalculator {
         String[] numbers = text.split(",|:");
         // 앞 단계의 구분자가 없는 경우도 split()을 활용해 구현할 수 있는지 검토해 본다.
         for (int i = 0; i < numbers.length; i++) {
-
-            integerOrNot = numbers[i].matches("-?\\d+");
-            if(integerOrNot){
-                sum += Integer.parseInt(numbers[i]);
-            }
-
-            if(i+1==numbers.length){
-                return sum;
-            }
+            if(integerOrNot(numbers[i])) sum += convertStringToInt(numbers[i]);
+            if(i+1==numbers.length) return sum;
         }
 
 
@@ -52,17 +38,23 @@ public class StringAddCalculator {
             String[] tokens= m.group(2).split(customDelimiter);
             // 덧셈 구현
             for (int i = 0; i < tokens.length; i++) {
-                integerOrNot = tokens[i].matches("-?\\d+");
-                if(integerOrNot){
-                    sum += Integer.parseInt(tokens[i]);
-                }
-                if(i+1==tokens.length){
-                    return sum;
-                }
+                if(integerOrNot(tokens[i])) sum += convertStringToInt(tokens[i]);
+                if(i+1==tokens.length)return sum;
+
             }
         }
 
         return 0;
+    }
+
+    // 숫자 여부 판단
+    public static boolean integerOrNot(String str){
+        return str.matches("-?\\d+");
+    }
+
+    // String Convert to Number
+    public static int convertStringToInt(String str){
+        return Integer.parseInt(str);
     }
 
 }
