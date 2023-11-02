d20e57a5906557887a7dcc22324c3911a74a7545
엄태권 <akfls221@gmail.com>
2023-11-03 03:07:23 +0900

    feat : PositiveIntegers Test Code 추가

--- src/test/java/stringaddcalculator/domain/PositiveIntegersTest.java
+++ src/test/java/stringaddcalculator/domain/PositiveIntegersTest.java
@@ -1,17 +1,11 @@
 package stringaddcalculator.domain;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PositiveIntegersTest {
-    @BeforeEach
-    void init() {
-    }
 
     @Test
     @DisplayName("문자배열을 통해 생성한 PositiveIntegers의 모든 합을 구한다.")
