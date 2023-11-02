9b2fcf83f47edb83673409db88e9fcd64550e736
엄태권 <akfls221@gmail.com>
2023-11-03 02:07:05 +0900

    fix : splitter 테스트 코드 수정

--- src/test/java/stringaddcalculator/SplitterTest.java
+++ src/test/java/stringaddcalculator/SplitterTest.java
@@ -2,16 +2,17 @@ package stringaddcalculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SplitterTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3", "1:2:3"})
     @DisplayName("기본 split 조건인 콤마(,)를 구분자로 하여 입력한 문자열을 배열로 생성한다.")
-    void splitter_base_regex_comma() {
-        String inputString = "1,2,3";
-
+    void splitter_base_regex_comma(String inputString) {
         String[] result = Splitter.splitInputString(inputString);
 
         assertThat(result).hasSize(3)
@@ -20,18 +21,4 @@ class SplitterTest {
                 .contains("3");
 
     }
-
-    @Test
-    @DisplayName("기본 split 조건인 콜론(:)를 구분자로 하여 입력한 문자열을 배열로 생성한다.")
-    void splitter_base_regex_colon() {
-        String inputString = "1:2:3";
-
-        String[] result = Splitter.splitInputString(inputString);
-
-        assertThat(result).hasSize(3)
-                .contains("1")
-                .contains("2")
-                .contains("3");
-    }
-
 }
