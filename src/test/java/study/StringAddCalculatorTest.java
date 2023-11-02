5dc621ad80ffdb544eba55ea07dbf342105ebd33
jeonghuntech <jeonghun.tech@gmail.com>
2023-11-02 13:22:02 +0900

    - throws Exception 필요 없는 곳 삭제

--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
-    
+
     @Test
     @DisplayName("문자열 분리, 빈문자인 경우 0 ")
     public void split_null_또는_빈문자() {
@@ -21,7 +21,7 @@ public class StringAddCalculatorTest {
 
     @Test
     @DisplayName("문자열 분리, 숫자 하나만 있는 경우 하나만 리턴")
-    public void split_숫자하나() throws Exception {
+    public void split_숫자하나() {
         StringAddCalculator stringAddCalculator = new StringAddCalculator();
         String[] result = stringAddCalculator.split("1");
         assertThat(result).containsExactly("1");
@@ -29,7 +29,7 @@ public class StringAddCalculatorTest {
 
     @Test
     @DisplayName("문자열 분리, 쉼표를 구분자로 숫자를 분리한다")
-    public void split_쉼표구분자() throws Exception {
+    public void split_쉼표구분자() {
         StringAddCalculator stringAddCalculator = new StringAddCalculator();
         String[] result = stringAddCalculator.split("1,2");
         assertThat(result).containsExactly("1", "2");
@@ -37,7 +37,7 @@ public class StringAddCalculatorTest {
 
     @Test
     @DisplayName("문자열 분리, 쉼표 또는 콜론을 구분자로 숫자를 분리한다")
-    public void split_쉼표_또는_콜론_구분자() throws Exception {
+    public void split_쉼표_또는_콜론_구분자() {
         StringAddCalculator stringAddCalculator = new StringAddCalculator();
         String[] result = stringAddCalculator.split("1,2:3");
         assertThat(result).containsExactly("1", "2", "3");
@@ -45,7 +45,7 @@ public class StringAddCalculatorTest {
 
     @Test
     @DisplayName("문자열 분리, 커스텀 구분자로 숫자를 분리한다")
-    public void split_custom_구분자() throws Exception {
+    public void split_custom_구분자()  {
         StringAddCalculator stringAddCalculator = new StringAddCalculator();
         String[] result = stringAddCalculator.split("//;\n1;2;3");
         assertThat(result).containsExactly("1", "2", "3");
@@ -54,7 +54,7 @@ public class StringAddCalculatorTest {
 
     @Test
     @DisplayName("문자열 분리, 쉼표, 콜론, 커스텀 구분자로 숫자를 분리한다")
-    public void split_custom_구분자_또는_쉼표_콜론_구분자() throws Exception {
+    public void split_custom_구분자_또는_쉼표_콜론_구분자()  {
         StringAddCalculator stringAddCalculator = new StringAddCalculator();
         String[] result = stringAddCalculator.split("//;\n1;2;3,4:5");
         assertThat(result).containsExactly("1", "2", "3", "4", "5");
