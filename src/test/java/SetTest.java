3c7fc2556f88582a1f6b187e330a8bfe25dd8335
ike.oh <ike.oh@balancehero.com>
2023-11-02 18:38:59 +0900

    [STEP1] 학습 테스트 실습

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,51 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+    }
+
+    @Test
+    void test_requirement_1() {
+        int expecting = 4;
+        int result = numbers.size();
+
+        assertThat(result).isEqualTo(expecting);
+    }
+
+    @Test
+    void test_requirement_2_1() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3 })
+    void test_requirement_2_2(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = { "1:true", "2:true", "3:true", "4:true", "5:false" }, delimiter = ':')
+    void test_requirement_3(int input, Boolean expecting) {
+        assertThat(numbers.contains(input)).isEqualTo(expecting);
+    }
+
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,54 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+    @Test
+    @DisplayName("','를 이용하여 String을 split 한다.")
+    void test_requirement_1_1() {
+        String input = "1,2";
+
+        String[] result = input.split(",");
+        assertThat(result).contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("하나의 숫자를 ','로 split 할 때, 그 숫자가 그래도 나오는지 확인한다.")
+    void test_requirement_1_2() {
+        String input = "1";
+
+        String[] result = input.split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("'()'를 제거한다.")
+    void test_requirement_2() {
+        String input = "(1,2)";
+        String expecting = "1,2";
+
+        String result = input.replace("(", "").replace(")", "");
+        assertThat(result).isEqualTo(expecting);
+    }
+
+    @Test
+    @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다")
+    void test_requirement_3_1() {
+        String input = "abc";
+        char expecting = 'b';
+
+        char result = input.charAt(1);
+        assertThat(expecting).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.")
+    void test_requirement_3_2() {
+        String input = "abc";
+
+        assertThatThrownBy(() -> input.charAt(input.length())).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+}
