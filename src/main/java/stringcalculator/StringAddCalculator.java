8b33d87c1249b9250ff069917b01aea26372a080
kimbro97 <rlagudwog@gmail.com>
2023-11-02 18:16:21 +0900

    refactor: 음수 판별 로직 개선 작업

--- src/main/java/stringcalculator/StringAddCalculator.java
+++ src/main/java/stringcalculator/StringAddCalculator.java
@@ -9,20 +9,33 @@ public class StringAddCalculator {
     private static final String DEFAULT_DELIMITERS = ",|:";
     private static final int CUSTOM_DELIMITER_GROUP = 1;
     private static final int TOKENS_GROUP = 2;
+    private static final int ZERO = 0;
     public static int splitAndSum(String text) {
         int sum = 0;
         if (isNullOrBlank(text)) {
             return 0;
         }
-        if (text.contains("-")) throw new RuntimeException("음수를 전달받은 경우 예외 발생");
+
         for (String str : splitText(text)) {
-            sum += Integer.parseInt(str);
+            int number = Integer.parseInt(str);
+            validateNumber(number);
+            sum += number;
         }
         return sum;
     }
 
     private static boolean isNullOrBlank(String text) {
         return text == null || text.isEmpty();
+    }
+
+    private static boolean isNegativeOrPositive(int number) {
+        return number < ZERO;
+    }
+
+    public static void validateNumber(int number) {
+        if (isNegativeOrPositive(number)) {
+            throw new RuntimeException("음수를 전달받은 경우 예외 발생");
+        }
     }
 
     private static String[] splitText(String text) {
