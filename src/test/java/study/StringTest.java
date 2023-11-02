61eb83c6b41ffc3e0a7bd749b8639d8171b763a8
sudo-init <dep.em.k.0905@gmail.com>
2023-11-02 15:19:28 +0900

    step 1 practice

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,20 +1,39 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 
 public class StringTest {
     @Test
+    @DisplayName("요구사항 1 contains() 테스트")
     public void split1() {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
     }
 
     @Test
+    @DisplayName("요구사항 1 containsExactly() 테스트")
     public void split2() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("요구사항 2 substring()")
+    public void substring() {
+        String result = "(1,2)".substring(1,4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("요구사항 3  StringIndexOutOfBoundsException")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            char result = "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range");
     }
 }
