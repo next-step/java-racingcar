244d9e1feb29d36b3cd79122090235f7e939793a
h3yon <khykhy1006@gmail.com>
2023-11-02 17:58:40 +0900

    feature: solve requirement1 and change function name

--- src/test/java/study/SpringTest.java
+++ src/test/java/study/SpringTest.java
@@ -6,8 +6,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SpringTest {
     @Test
-    void split() {
+    void testRequirement1() {
         String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
     }
 }
