25faf58cc5f9db4779ee429c2d1c9b6a90389717
jeounpar <cdex6531@ajou.ac.kr>
2023-11-02 23:39:43 +0900

    step2 review-01

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -5,44 +5,47 @@ import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
-	public static int splitAndSum(String input) throws RuntimeException {
+	private static final String DEFAULT_DELIMITER = ",|:";
+	private static final Pattern PATTERN = Pattern.compile("//(.)\n(.*)");
+	private static String input;
+
+	public static int splitAndSum(String text) {
+		input = text;
 		if (isEmpty(input))
 			return 0;
 
 		if (input.contains("-"))
 			throw new RuntimeException();
 
-		Matcher matcher = patternMatcher(input);
-
-		String[] numbers;
-		if (matcher.find()) {
-			String customDelimiter = matcher.group(1);
-			String text = matcher.group(2);
-			numbers = split(text, customDelimiter);
-			return sum(numbers);
-		}
-
-		numbers = split(input, ",|:");
+		String delimiter = findDelimiter(input);
+		String[] numbers = split(input, delimiter);
 		return sum(numbers);
 	}
 
-	private static String[] split(String input, String delimiter) {
-		return input.split(delimiter);
+	private static String findDelimiter(String text) {
+		Matcher matcher = PATTERN.matcher(text);
+		if (matcher.find()) {
+			input = matcher.group(2);
+			return DEFAULT_DELIMITER + '|' + matcher.group(1);
+		}
+		return DEFAULT_DELIMITER;
 	}
 
-	private static Matcher patternMatcher(String text) {
-		return Pattern.compile("//(.)\n(.*)").matcher(text);
+
+	private static String[] split(String text, String delimiter) {
+		return text.split(delimiter);
 	}
 
 	private static int sum(String[] numbers) {
 		int result = 0;
-		for (String number : numbers)
+		for (String number : numbers) {
 			result += Integer.parseInt(number);
+		}
 		return result;
 	}
 
-	private static boolean isEmpty(String input) {
-		return input == null || "".equals(input);
+	private static boolean isEmpty(String text) {
+		return text == null || "".equals(text);
 	}
 
 }
