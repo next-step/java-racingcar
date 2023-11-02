275396872bd3e0672a00bd363a60282cba7b4cf9
hoon25 <cchoon95@hanmail.net>
2023-11-02 23:23:11 +0900

    feat: null,"" 계산

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class StringCalculator {
+    public static int calculate(String o) {
+        if (o == null || o.isEmpty()) {
+            return 0;
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+
+    @Test
+    void calculate_null() {
+        assertThat(StringCalculator.calculate(null)).isEqualTo(0);
+    }
+
+    @Test
+    void calculate_emptyString() {
+        assertThat(StringCalculator.calculate("")).isEqualTo(0);
+    }
+}
