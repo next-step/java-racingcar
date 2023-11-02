81ee77d1bb1efb73f4445727a793879cab145e00
hoon25 <cchoon95@hanmail.net>
2023-11-03 02:19:54 +0900

    refactor: 입력받은 문자를 나눠주는 Text클래스 추가

---
+++ src/main/java/calculator/Text.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Text {
+    public static final String DEFAULT_DELIMITER = ",|\\:";
+    public static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+
+    private String text;
+    private String delimiter;
+
+    private Text(String text, String delimiter) {
+        this.text = text;
+        this.delimiter = delimiter;
+    }
+
+    public static Text input(String input) {
+        return new Text(removeDelimiter(input), transferDelimiter(input));
+    }
+
+    public String[] getElements() {
+        return text.split(delimiter);
+    }
+
+    private static String transferDelimiter(String text) {
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(text);
+        if (m.find()) {
+            return m.group(1);
+        }
+        return DEFAULT_DELIMITER;
+    }
+
+    private static String removeDelimiter(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            return m.group(2);
+        }
+        return text;
+    }
+}
--- src/main/java/calculator/TextCalculator.java
+++ src/main/java/calculator/TextCalculator.java
@@ -1,40 +1,17 @@
 package calculator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static java.lang.Integer.parseInt;
 
 public class TextCalculator {
-
-    public static final String DEFAULT_DELIMITER = ",|\\:";
-
     public static int calculate(String input) {
         if (isEmpty(input)) {
             return 0;
         }
-        String[] elements = removeCustomDelimiter(input).split(getDelimiter(input));
-        return sum(transfer(elements));
+        return sum(transfer(Text.input(input).getElements()));
     }
 
     private static boolean isEmpty(String text) {
         return text == null || text.isEmpty();
-    }
-
-    private static String getDelimiter(String text) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-        if (m.find()) {
-            return m.group(1);
-        }
-        return DEFAULT_DELIMITER;
-    }
-
-    private static String removeCustomDelimiter(String text) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-        if (m.find()) {
-            return m.group(2);
-        }
-        return text;
     }
 
     private static Integer[] transfer(String[] elements) {
