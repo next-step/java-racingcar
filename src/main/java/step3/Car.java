28c17ba939152b780a492bf998a11177853d02a9
hyeonjinnoh <hyeonjin.noh@sixshop.com>
2023-11-02 23:18:34 +0900

    feat: random 값이 4 이상일 경우 자동차는 전진할 수 있다

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -5,10 +5,29 @@ import java.util.Random;
 public class Car {
 
     private static final int RANDOM_BOUND = 10;
+    private static final int MOVE_BOUND = 4;
     private static final Random random = new Random();
+
+    private int position;
 
     public int random() {
         return random.nextInt(RANDOM_BOUND);
+    }
+
+    public void move() {
+        int random = random();
+
+        if (canMove(random)) {
+            position++;
+        }
+    }
+
+    private boolean canMove(int random) {
+        return random >= MOVE_BOUND;
+    }
+
+    public int position() {
+        return this.position;
     }
 
 }
--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -8,6 +8,8 @@ public class CarTest {
 
     private static final int RANDOM_MIN = 0;
     private static final int RANDOM_MAX = 9;
+    private static final int NOT_MOVED = 0;
+    private static final int MOVED = 1;
 
     private Car car = new Car();
 
@@ -15,6 +17,12 @@ public class CarTest {
     public void 랜덤값_0부터_9사이() {
         int random = car.random();
         assertThat(random).isBetween(RANDOM_MIN, RANDOM_MAX);
+    }
+
+    @Test
+    public void 자동차_전진() {
+        car.move();
+        assertThat(car.position()).isIn(NOT_MOVED, MOVED);
     }
 
 }
