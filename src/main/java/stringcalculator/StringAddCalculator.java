6211da0cac7087c3a0144177bd73f2e574ad6383
kimbro97 <rlagudwog@gmail.com>
2023-11-02 17:51:28 +0900

    style: if문에 중괄호 추가 작업

--- src/main/java/stringcalculator/StringAddCalculator.java
+++ src/main/java/stringcalculator/StringAddCalculator.java
@@ -7,7 +7,9 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
         int sum = 0;
-        if (isNullOrBlank(text)) return 0;
+        if (isNullOrBlank(text)) {
+            return 0;
+        };
         if (text.contains("-")) throw new RuntimeException("음수를 전달받은 경우 예외 발생");
         for (String str : splitText(text)) {
             sum += Integer.parseInt(str);
