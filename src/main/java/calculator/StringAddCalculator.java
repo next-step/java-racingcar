e5bb5628a3af3062917bb0c85c4835d02439ccc5
kyunghyun-park <qqqq_o@naver.com>
2023-11-02 23:23:17 +0900

    Revert "refactor: 계산기 클래스 리팩토링"
    This reverts commit 45018c32951f6784fa7dcce1ae84dfaccbdd8315.

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,49 +4,46 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private static final String DEFAULT_DELIMITER = "[,:]";
-    public static final int CUSTOM_DELIMITER = 1;
-    public static final int TEXT = 2;
+    private static final String DELIMITER = "[,:]";
 
     public static int splitAndSum(String text) {
         if (isNullOrEmpty(text)) {
             return 0;
         }
 
-        return sum(split(text));
+        String[] split = splitText(text);
+        return sumSplitText(split);
     }
 
     private static boolean isNullOrEmpty(String text) {
         return text == null || text.isEmpty();
     }
 
-    private static String[] split(String text) {
+    private static String[] splitText(String text) {
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-
         if (m.find()) {
-            return m.group(TEXT).split(m.group(CUSTOM_DELIMITER));
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
         }
-
-        return text.split(DEFAULT_DELIMITER);
+        return text.split(DELIMITER);
     }
 
-    private static int sum(String[] split) {
+    private static int sumSplitText(String[] split) {
         int sum = 0;
-
         for (String s : split) {
-            sum += parseInt(s);
+            int parseInt = getParseInt(s);
+            sum += parseInt;
         }
 
         return sum;
     }
 
-    private static int parseInt(String s) {
+    private static int getParseInt(String s) {
         int parseInt = Integer.parseInt(s);
 
-        if (parseInt < 0) {
+        if(parseInt < 0) {
             throw new RuntimeException("음수 값이 입력됨" + parseInt);
         }
-
         return parseInt;
     }
 }
