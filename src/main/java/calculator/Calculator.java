a3fa8b64e42f1dc332ed6340da670848fa22c36d
yumble <hnjn1101@gmail.com>
2023-11-02 22:34:14 +0900

    feat: 1주차 라이브 코딩 따라가면서 기능 3/6 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,40 @@
+package calculator;
+
+public class Calculator {
+
+    public static final String DELIMITER = ",";
+    public static final int NULL_VALUE = 0;
+
+    public static int cal(String text) {
+        if (isBlank(text)) {
+            return NULL_VALUE;
+        } //else문 사용 지양을 위해 Early Return 지향하자
+
+        return sum(toInts(split(text)));
+    }
+
+    private static String[] split(String text) {
+        return text.split(DELIMITER);
+    }
+
+    private static boolean isBlank(String text) {
+        return text == null || text.isBlank();
+    } //추상화 레벨을 sum과 같이 맞추기
+
+    private static int sum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    private static int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
+    }
+
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,26 @@
+package calculator;
+
+import static calculator.Calculator.cal;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    @DisplayName("Null이거나 빈문자열의 값")
+    void null_빈문자값() {
+        assertThat(cal(null)).isEqualTo(0);
+        assertThat(cal("")).isEqualTo(0);
+    }
+
+    @Test
+    void 숫자_하나() {
+        assertThat(cal("1")).isEqualTo(1);
+    }
+
+    @Test
+    void 컴마_구분자(){
+        assertThat(cal("1,2")).isEqualTo(3);
+    }
+}
