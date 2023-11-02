1fff8aab473d341e0f22bf3d507d22feeb2330ed
chocheolhee <90247663+chocheolhee@users.noreply.github.com>
2023-11-03 07:07:46 +0900

    2단계 문자열 덧셈 계산기 (#4861)
    * 2단계 문자열 덧셈 계산기
    
    * refactor : 2단계 미션 코드리뷰 요청 반영 및 추가적인 리팩토링

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,68 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    public static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String SEPARATOR = ",|:";
+    public static final int MAGIC_NUMBER = 0;
+
+    public static int splitAndSum(String text) {
+
+        if (isNullOrEmpty(text)) {
+            return MAGIC_NUMBER;
+        }
+
+        Matcher customPattern = getMatcher(text);
+        if (checkCustomPattern(customPattern)) {
+            return sum(disuniteValue(customPattern));
+        }
+
+        return separatorSum(text);
+    }
+
+    private static int sum(String[] values) {
+        int num = 0;
+        for (String value : values) {
+            num += validate(value);
+        }
+        return num;
+    }
+
+    private static int separatorSum(String text) {
+        String[] numbers = text.split(SEPARATOR);
+        return sum(numbers);
+    }
+
+    private static boolean checkCustomPattern(Matcher customPattern) {
+        return customPattern.find();
+    }
+
+    private static String[] disuniteValue(Matcher customPattern) {
+        String customDelimiter = customPattern.group(1);
+        return customPattern.group(2).split(customDelimiter);
+    }
+
+    private static int validate(String value) {
+        int number = stringToInt(value);
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
+    private static int stringToInt(String value) {
+        return Integer.parseInt(value);
+    }
+
+    private static boolean isNullOrEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private static Matcher getMatcher(String text) {
+        return CUSTOM_PATTERN.matcher(text);
+    }
+
+}
