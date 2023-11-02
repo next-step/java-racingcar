03a6c85f8c915f4db8d8c492425b4ee3aa368efe
hoon25 <cchoon95@hanmail.net>
2023-11-03 01:26:42 +0900

    test(Step1리뷰반영): primitive type으로 변경

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,7 +1,6 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -31,19 +30,19 @@ public class SetTest {
 
     @ParameterizedTest(name="Set에 있는 값인 {0}를 contains하면 True를 반환")
     @ValueSource(ints = {1, 2, 3})
-    void containsTrue(Integer input) {
+    void containsTrue(int input) {
         assertThat(numbers.contains(input)).isTrue();
     }
 
     @ParameterizedTest(name="Set에 없는 값인 {0}를 contains하면 False를 반환")
     @ValueSource(ints = {4, 10, 12})
-    void containsFalse(Integer input) {
+    void containsFalse(int input) {
         assertThat(numbers.contains(input)).isFalse();
     }
 
     @ParameterizedTest(name="값{0}의 Set 존재여부를 contains로 확인하며 {1}를 반환")
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    void contains(Integer input, Boolean expected) {
+    void contains(int input, Boolean expected) {
         assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
