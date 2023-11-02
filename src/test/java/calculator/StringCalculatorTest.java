d25c20e45de32d8ded6bfff224d488fc008d11e5
hoon25 <cchoon95@hanmail.net>
2023-11-03 00:31:18 +0900

    test: 숫자한개일때 테스트 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -18,11 +18,17 @@ public class StringCalculatorTest {
     }
 
     @Test
+    void calculate_숫자_한개() {
+        assertThat(StringCalculator.calculate("1")).isEqualTo(1);
+    }
+
+    @Test
     void calculate_기본_구분자() {
         assertThat(StringCalculator.calculate("1,2")).isEqualTo(3);
         assertThat(StringCalculator.calculate("1,2,3")).isEqualTo(6);
         assertThat(StringCalculator.calculate("1,2:3")).isEqualTo(6);
     }
+
     @Test
     void calculate_커스텀_구분자() {
         assertThat(StringCalculator.calculate("//;\n1;2;3")).isEqualTo(6);
