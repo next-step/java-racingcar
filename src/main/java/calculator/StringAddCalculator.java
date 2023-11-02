ef8fef312b9705becb9a226c8c69bdc410aa98ca
g0rani <pjw0405@naver.com>
2023-11-02 22:48:08 +0900

    feat: 문자열 덧셈 계산기 빈문자열과 null 숫자 반환, 개산 기능 개발
    라이브 강의와 함께 진행한 내용에 대한 커밋
    - 빈 문자열 또는 null 값인 경우 0 반환
    - 숫자가 하나일 때 숫자 그대로의 값을 반환
    - 숫자를 쉼표 구분자로 계산

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,38 @@
+package calculator;
+
+public class StringAddCalculator {
+
+    public static final String DELIMITER = ",";
+
+    public static int splitAndSum(String text) {
+        if (isBlank(text)) {
+            return 0;
+        }
+        
+        return sum(toInts(split(text)));
+    }
+
+    private static String[] split(String text) {
+        return text.split(DELIMITER);
+    }
+
+    private static boolean isBlank(String text) {
+        return text == null || text.isBlank();
+    }
+
+    private static int sum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    private static int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
+    }
+}
---
+++ src/test/java/calculator/StringAddStringAddCalculatorTest.java
@@ -0,0 +1,44 @@
+package calculator;
+
+import static calculator.StringAddCalculator.splitAndSum;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class StringAddStringAddCalculatorTest {
+
+    @Test
+    void 빈_문자열_또는_null_값인_경우_0을_반환한다() {
+        int expected = 0;
+        Assertions.assertAll(
+            () -> {
+                int actual = splitAndSum(null);
+
+                assertThat(actual).isEqualTo(expected);
+            },
+            () -> {
+                int actual = splitAndSum("");
+
+                assertThat(actual).isEqualTo(0);
+            }
+        );
+    }
+
+    @Test
+    void 숫자가_하나일_때_숫자_그대로의_값을_반환한다() {
+        int actual = splitAndSum("1");
+        int expected = 1;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 숫자_두개를_컴마_구분자로_입력_할_경우_두_숫자의_합을_반환한다() {
+        int actual = splitAndSum("1,2,3");
+        int expected = 6;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
