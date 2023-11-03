3059307d5c32ea03ac34f11ae922ee4dafdb01cd
HyunggiPark <phg9587@naver.com>
2023-11-03 14:42:51 +0900

    feat: splitAndSum 메서드의 입력으로 숫자 두개이상이 컴마(,) 구분자로 구분되어 입력된 경우, 숫자들의 합을 반환

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,7 +6,21 @@ public class StringAddCalculator {
             return 0;
         }
 
-        return -1;
+        return sum(split(input));
+    }
+
+    private static String[] split(final String input) {
+        return input.split(",");
+    }
+
+    private static int sum(final String[] numbers) {
+        int sum = 0;
+
+        for (final String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
     }
 
     private static boolean isEmpty(final String input) {
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -27,4 +27,19 @@ class StringAddCalculatorTest {
         //then
         assertThat(result).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("splitAndSum 메서드의 입력으로 숫자 두개이상이 컴마(,) 구분자로 구분되어 입력된 경우, 숫자들의 합을 반환한다")
+    void splitAndSum_NumbersSeparatedByComma() {
+        //given
+        //when
+        int result1 = StringAddCalculator.splitAndSum("1,2");
+        int result2 = StringAddCalculator.splitAndSum("1,2,3");
+        int result3 = StringAddCalculator.splitAndSum("1,2,3,4");
+
+        //then
+        assertThat(result1).isEqualTo(3);
+        assertThat(result2).isEqualTo(6);
+        assertThat(result3).isEqualTo(10);
+    }
 }
