85a63ee3fc705866cabbf2c1a05188c885bbf929
KimGwangHoon <khkimykk@naver.com>
2023-11-03 23:09:15 +0900

    feat : 경주용 차량팩토리 구현

--- src/main/java/step3/CarFactory.java
+++ src/main/java/step3/CarFactory.java
@@ -1,2 +1,40 @@
-package step3;public class CarFactory {
+package step3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+    private List<Car> cars;
+    private RandomNumber randomNumber;
+    private int tryCount;
+
+    public CarFactory() {
+        this.cars = new ArrayList<>();
+        randomNumber = new RandomNumber();
+    }
+
+    public List<Car> carsInfo() {
+        return cars;
+    }
+
+    public void participants(int count) {
+        for (int addCar = 0; addCar < count; addCar++) {
+            cars.add(new Car());
+        }
+    }
+
+    public void attempts(int tryCount) {
+        this.tryCount = tryCount;
+    }
+
+    public int tryCount() {
+        return tryCount;
+    }
+
+    public void moveCars() {
+        for (Car car : cars) {
+            car.move(randomNumber.randomNumberCreate());
+        }
+    }
+
 }
