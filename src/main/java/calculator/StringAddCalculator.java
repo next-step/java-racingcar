3af95cdfb84d416be16cc21ccd69b5aa1b0657ca
HyunggiPark <phg9587@naver.com>
2023-11-03 15:34:29 +0900

    refact: 메서드가 너무 많은 일을 하지 않도록 메서드 분리 진행

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,27 +6,37 @@ public class StringAddCalculator {
             return 0;
         }
 
-        return sum(split(input));
+        return sum(toInts(split(input)));
     }
 
     private static String[] split(final String input) {
         return input.split("[,|:]");
     }
 
-    private static int sum(final String[] numbers) {
+    private static int sum(final int[] numbers) {
         int sum = 0;
 
-        for (final String number : numbers) {
-            final int eachValue = Integer.parseInt(number);
-
-            if (eachValue < 0) {
-                throw new RuntimeException("Input must be a positive integer");
-            }
-
-            sum += eachValue;
+        for (final int number : numbers) {
+            sum += number;
         }
 
         return sum;
+    }
+
+    private static int[] toInts(final String[] numbers) {
+        int[] intNumbers = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            final int parseInt = Integer.parseInt(numbers[i]);
+
+            if (parseInt < 0) {
+                throw new RuntimeException("Input must be a positive integer");
+            }
+
+            intNumbers[i] = parseInt;
+        }
+
+        return intNumbers;
     }
 
     private static boolean isEmpty(final String input) {
