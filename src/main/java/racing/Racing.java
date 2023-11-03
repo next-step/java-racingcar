9cbebbfb9d940e8183e853bf3dfff2379e4311bc
chunghyunleeme <chunghyun.dev@gmail.com>
2023-11-03 23:40:17 +0900

    chore: ParticipatingCars 클래스 변수명 변경

--- src/main/java/racing/Racing.java
+++ src/main/java/racing/Racing.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 public class Racing {
     ParticipatingCars participatingCars;
+
     public Racing() {
         this.participatingCars = new ParticipatingCars();
     }
 
     public List<Car> ready(int numberOfCar) {
-        for(int i = 0; i < numberOfCar; ++i) {
+        for (int i = 0; i < numberOfCar; ++i) {
             participatingCars.join(new Car());
         }
         return this.participatingCars();
@@ -21,14 +22,14 @@ public class Racing {
     }
 
     private static class ParticipatingCars {
-        private List<Car> cars = new ArrayList<>();
+        private List<Car> participatingCars = new ArrayList<>();
 
         private void join(Car car) {
-            this.cars.add(car);
+            this.participatingCars.add(car);
         }
 
-        private List<Car> cars(){
-            return this.cars;
+        private List<Car> cars() {
+            return this.participatingCars;
         }
     }
- }
+}
