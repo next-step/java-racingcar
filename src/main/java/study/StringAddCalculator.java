4b7bf50187959a334dd9b8a1c4adade80957efc8
beyondorder <youofsea@gmail.com>
2023-11-03 15:31:34 +0900

    리팩터링 완료

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,31 +1,77 @@
 package study;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+    static final String DEFAULT_DELIMITER_REGEX = ",|:";
+    static final String CUSTOM_DELIMITER_REGEX = "(\\/\\/)(.*)(\\n)(.*)";
+    static final int CUSTOM_DELIMITER_GROUP_NUMBER = 2;
+    static final int BODY_GROUP_NUMBER = 4;
 
     public static int add(String s) {
+        if(isBlank(s)) return 0;
 
-        int sum = 0;
+        int[] intArray = IntArrayFromStrNumbersArray(getStrNumbers(s));
 
-        if(s == null || s.equals("")){
-            return 0;
-        }
-        String[] numbers = s.split(",|:");
-        Matcher matcher = Pattern.compile("(\\/\\/)(.*)(\\n)(.*)").matcher(s);
-        if(matcher.find()) {
-            String customDelimiter = matcher.group(2);
-            numbers =matcher.group(4).split(customDelimiter);
-        }
+        throwExceptionIfMinusValueIncluded(intArray);
 
-        for(int i = 0; i < numbers.length; i++) {
-         if(Integer.parseInt(numbers[i]) < 0){
-             throw new RuntimeException("minus input error !!");
-         }
-          sum += Integer.parseInt(numbers[i]);
+        return sum(intArray);
+    }
+
+    private static String[] getStrNumbers(String s) {
+        Matcher customDelimiterRegexMatcher = customDelimiterRegexMatcher(s);
+        if(isCustomDelimiterInput(customDelimiterRegexMatcher)) {
+            String customDelimiter = getCustomDelimiter(customDelimiterRegexMatcher);
+            return strNumbersSplitedByCustomDelimiter(customDelimiterRegexMatcher, customDelimiter);
+        }else{
+            return strNumbersOfDefaultDelimiter(s);
         }
-        return sum;
+    }
+
+    private static String[] strNumbersOfDefaultDelimiter(String s) {
+        return s.split(DEFAULT_DELIMITER_REGEX);
+    }
+
+    private static String[] strNumbersSplitedByCustomDelimiter(Matcher customDelimiterRegexMatcher, String customDelimiter) {
+        return customDelimiterRegexMatcher.group(BODY_GROUP_NUMBER).split(customDelimiter);
+    }
+
+    private static String getCustomDelimiter(Matcher customDelimiterRegexMatcher) {
+        return customDelimiterRegexMatcher.group(CUSTOM_DELIMITER_GROUP_NUMBER);
+    }
+
+    private static boolean isCustomDelimiterInput(Matcher customDelimiterRegexMatcher) {
+        return customDelimiterRegexMatcher.find();
+    }
+
+    private static Matcher customDelimiterRegexMatcher(String s) {
+        return Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(s);
+    }
+
+    private static int[] IntArrayFromStrNumbersArray(String[] numbers) {
+        return Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static void throwExceptionIfMinusValueIncluded(int[] numbers) {
+        Arrays.stream(numbers).forEach(StringAddCalculator::throwExceptionIfMinusValue);
+    }
+
+    private static void throwExceptionIfMinusValue(int value) {
+        if(value < 0) throwRuntimeException("Minus Value Error!");
+    }
+
+    private static void throwRuntimeException(String message) {
+        throw new RuntimeException(message);
+    }
+
+    private static boolean isBlank(String s) {
+        return s == null || s.equals("");
+    }
+
+    private static int sum(int[] numbers) {
+        return Arrays.stream(numbers).sum();
     }
 }
