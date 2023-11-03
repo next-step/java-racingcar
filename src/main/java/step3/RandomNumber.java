1a3a1a06838beaa837a71c53c743d04545fdab48
kimgwanghoon <khkimykk@naver.com>
2023-11-03 10:15:28 +0900

    feat: 랜덤값생성)

--- src/main/java/step3/RandomNumber.java
+++ src/main/java/step3/RandomNumber.java
@@ -4,4 +4,10 @@ import java.util.Random;
 
 public class RandomNumber {
 
+    private static final int RANDOM_NUMBER_MAX = 10;
+
+    public static int randomNumberCreate() {
+        Random random = new Random();
+        return random.nextInt(RANDOM_NUMBER_MAX);
+    }
 }
