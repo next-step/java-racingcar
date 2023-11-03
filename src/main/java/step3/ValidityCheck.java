dba1492be89a0625deec7d48bdc0e751554fbd85
kimgwanghoon <khkimykk@naver.com>
2023-11-03 11:22:47 +0900

    feat: 숫자입력 유효성검사 구현

--- src/main/java/step3/ValidityCheck.java
+++ src/main/java/step3/ValidityCheck.java
@@ -1,4 +1,15 @@
 package step3;
 
 public class ValidityCheck {
+
+    public static boolean positiveNumberCheck(int number) {
+        try {
+            if(number >= 0) {
+                return true;
+            }
+            throw new IllegalArgumentException("0보다 큰 숫자를 입력해주세요.");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
 }
