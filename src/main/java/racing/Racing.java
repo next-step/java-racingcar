9e6f46d82767ed3dec98045271c0de34f0426f09
chunghyunleeme <chunghyun.dev@gmail.com>
2023-11-03 14:36:06 +0900

    feat: 참가할 수 만큼 자동차를 생성하여 경주 준비

--- src/main/java/racing/Racing.java
+++ src/main/java/racing/Racing.java
@@ -1,4 +1,15 @@
 package racing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Racing {
-}
+    private List<Car> cars = new ArrayList<>();
+
+    public List<Car> ready(int numberOfCar) {
+        for(int i = 0; i < numberOfCar; ++i) {
+            cars.add(new Car());
+        }
+        return this.cars;
+    }
+ }
--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -1,10 +1,24 @@
 package racing;
 
+import jdk.jfr.Name;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class RacingTest {
+    @Name("경주 준비 자동차 생성 테스트")
+    @Test
+    void ready() {
+        // given
+        Racing racing = new Racing();
+        int numberOfCar = 3;
+
+        // when
+        List<Car> cars = racing.ready(numberOfCar);
+
+        // then
+        assertThat(cars.size()).isEqualTo(numberOfCar);
+    }
 }
