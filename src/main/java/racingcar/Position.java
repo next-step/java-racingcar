d06a07c07a04233e8a4c8a93c77db5dbf1d5fac0
엄태권 <akfls221@gmail.com>
2023-11-04 03:46:36 +0900

    feat : Position Class 생성 및 테스트 코드 작성

---
+++ src/main/java/racingcar/Position.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+public class Position {
+
+    private static final int DEFAULT_POSITION = 0;
+    private int number;
+
+    public Position() {
+        this.number = DEFAULT_POSITION;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void move(final int moveCount) {
+        this.number += moveCount;
+    }
+}
