3c920abb2ae8617156d84721774605ea0cc4ada0
henry174 <dltmdwns812@daum.net>
2023-11-03 19:59:17 +0900

    StringAdder 추가
    빈 문자열 입력 시 0 반환 구현

---
+++ src/main/java/calculator/StringAdder.java
@@ -0,0 +1,25 @@
+package calculator;
+
+/**
+ * 문자열 안에 주어진 숫자를 더하는 연산 기능을 제공하는 유틸 클래스
+ */
+public class StringAdder {
+    /**
+     * 문자열로 정수 목록이 주어지면 정수들의 합을 계산합니다.
+     * 문자열 내에서 각 정수들은 지정된 구분자로 구분되거나
+     * 사용자가 직접 구분자를 정의할 수 있습니다.
+     * 기본 구분자는 , 또는 :입니다.
+     * 사용자가 직접 구분자를 정의하려면 //과 \n 사이에 구분자를 넣으십시오.
+     *
+     * @param integerList 구분자로 구분되는 정수들을 포함하는 문자열
+     *
+     * @return 해당 정수들의 합
+     */
+    public static int sum(String integerList) {
+        if (integerList.isEmpty()) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
---
+++ src/test/java/calculator/StringAdderTest.java
@@ -0,0 +1,16 @@
+package calculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringAdderTest {
+    @Test
+    @DisplayName("빈 문자열 입력 테스트")
+    public void inputEmptyString() {
+        assertThat(StringAdder.sum(""))
+                .isEqualTo(0);
+    }
+}
