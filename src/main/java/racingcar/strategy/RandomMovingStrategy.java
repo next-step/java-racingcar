ea34a0e778ae994b445f0164e917a0443f1bb130
엄태권 <akfls221@gmail.com>
2023-11-04 03:46:57 +0900

    feat : 자동차가 움직일 수 있는 전략추가

---
+++ src/main/java/racingcar/strategy/RandomMovingStrategy.java
@@ -0,0 +1,23 @@
+package racingcar.strategy;
+
+import java.util.Random;
+
+public class RandomMovingStrategy implements MovingStrategy{
+
+    private static final int BASE_RANDOM_BOUND = 10;
+    private static final int MOVE_CONDITION = 4;
+    private static final int MOVE_POSITION = 1;
+    private static final int DEFAULT_MOVE_POSITION = 0;
+    private final Random random = new Random();
+
+    @Override
+    public int movingCondition() {
+        int randomNumber = random.nextInt(BASE_RANDOM_BOUND);
+
+        if (randomNumber >= MOVE_CONDITION) {
+            return MOVE_POSITION;
+        }
+
+        return DEFAULT_MOVE_POSITION;
+    }
+}
