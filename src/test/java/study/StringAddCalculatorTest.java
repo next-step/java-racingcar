3a9bb49ceb3067ff1dd64d98ca78a9fbaa42ceb4
beyondorder <youofsea@gmail.com>
2023-11-03 12:12:10 +0900

    숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)

--- src/test/java/study/StringAddCalculatorTest.java
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -12,22 +12,23 @@ import static study.StringAddCalculator.*;
 public class StringAddCalculatorTest {
 
     @Test
-    @DisplayName("문자열 더하기")
-    void addString(){
-        assertThat(add("1,2")).isEqualTo(3);
-    }
-
-    @Test
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
     void emptyStringOrNull (){
         assertThat(add("")).isEqualTo(0);
         assertThat(add(null)).isEqualTo(0);
     }
-
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)")
-    @ParameterizedTest
-    @CsvSource(value = {"1:1","3:3","14:14","5:5"},delimiter = ':')
+    @ParameterizedTest(name = "input=> {0} output=> {1}")
+    @CsvSource(value = {"1,1","3,3","14,14","5,5"})
     void onlyOneInput (String input, int expected){
         assertThat(add(input)).isEqualTo(expected);
     }
+
+    @Test
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)")
+    void addString(){
+        assertThat(add("1,2")).isEqualTo(3);
+    }
+
+
 }
