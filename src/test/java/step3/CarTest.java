9bf8750d0fbd569635b0b31f1775a49331b167a9
kimgwanghoon <khkimykk@naver.com>
2023-11-03 13:22:43 +0900

    test: 자동차 동작 테스트

---
+++ src/test/java/step3/CarTest.java
@@ -0,0 +1,54 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CarTest {
+    private Car car;
+    private final int DEFULT_POSITION = 0;
+
+    public void setUp(int position) {
+        this.car = new Car(position);
+    }
+
+    @DisplayName("자동차가 움직이는지 테스트")
+    @Test
+    public void move() {
+        //given
+        setUp(DEFULT_POSITION);
+        int moveNumber = 4;
+        //when
+        car.move(moveNumber);
+        //then
+        assertThat(car.nowPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("자동차가 멈추는지 테스트")
+    @Test
+    public void stop() {
+        //given
+        setUp(DEFULT_POSITION);
+        int moveNumber = 3;
+        //when
+        car.move(moveNumber);
+        //then
+        assertThat(car.nowPosition()).isEqualTo(0);
+    }
+
+    @DisplayName("자동차의 현재위치 출력")
+    @Test
+    public void position() {
+        //given
+        setUp(DEFULT_POSITION);
+        int moveNumber = 4;
+        //when
+        car.move(moveNumber);
+        car.move(moveNumber);
+        int stopMoveNumber = 3;
+        car.move(stopMoveNumber);
+        //then
+        assertThat(car.printCar()).isEqualTo("--");
+    }
+}
