d4bff250a40007a5334006f83a6d6f15f275e3c9
chunghyunleeme <chunghyun.dev@gmail.com>
2023-11-03 14:22:04 +0900

    test: car 클래스 테스트 코드 클래스 분리

--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -7,17 +7,4 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.*;
 
 public class RacingTest {
-    @CsvSource({"0, false", "1, false", "2, false", "3, false", "4, true", "5, true", "6, true", "7, true", "8, true", "9, true"})
-    @ParameterizedTest(name = "자동차 전진 테스트")
-    void move_car(int number, boolean expected) {
-        // given
-        Car car = new Car();
-
-        // whern
-        int prevPosition = car.position();
-        car.move(number);
-
-        // then
-        assertThat(car.position() == prevPosition + 1).isEqualTo(expected);
-    }
 }
