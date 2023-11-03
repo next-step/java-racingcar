6add3471a9de98685baf5173230a23db617ec09a
simbokyung <synk98@gmail.com>
2023-11-03 09:32:43 +0900

    Fix : 1주차 강의 내용 반영(메소드 분리)

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -16,29 +15,32 @@ public class StringAddCalculator {
 
 
     public static int splitAndSum(String text) {
-        if(isNullOrEmpty(text )) return  0;
+        if(isNullOrEmpty(text)) return  0;
 
-        if(text.contains("-")) throw new  RuntimeException();
+        if(isNegativeNumber(text)) throw new  RuntimeException();
 
-        String[] array = splitText(text);
-
-        return  sum(array);
-    }
-    private static boolean isNullOrEmpty(String text){
-        return  text == null || text.isEmpty();
-    }
-
-    private static String[] splitText(String text){
         Matcher m = CUNSTOM_PATTERN.matcher(text);
 
         String delimiter = findDelimiter(m);
 
-        if(!delimiter.equals(DEFAULT_DELIMITERS)){
+        if(isCustomDelimiter(delimiter)){
             text = m.group(2);
         }
-        return  text.split(delimiter);
+
+        return  sum(splitText(text, delimiter));
     }
 
+    private static boolean isNullOrEmpty(String text){
+        return  text == null || text.isEmpty();
+    }
+
+    private static boolean isNegativeNumber(String text) {
+        return text.contains("-");
+    }
+
+    private static boolean isCustomDelimiter(String delimiter) {
+        return !delimiter.equals(DEFAULT_DELIMITERS);
+    }
 
     private static String findDelimiter(Matcher m) {
 
@@ -48,7 +50,10 @@ public class StringAddCalculator {
         return DEFAULT_DELIMITERS;
     }
 
-
+    private static String[] splitText(String text, String delimiter){
+        return  text.split(delimiter);
+    }
+    
     private static int sum(String[] textArray){
         return Stream.of(textArray).mapToInt(Integer::parseInt).sum();
     }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static calculator.StringAddCalculator.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,7 +13,7 @@ public class StringAddCalculatorTest {
     @DisplayName("빈 문자열 또는 null을 전달하는 경우 0을 반환")
     void 빈문자_null_check() {
 
-        int result = StringAddCalculator.splitAndSum("");
+        int result = splitAndSum("");
 
         assertThat(result).isEqualTo(0);
     }
@@ -21,7 +22,7 @@ public class StringAddCalculatorTest {
     @DisplayName("숫자하나를 문자열로 전달하는 경우 숫자의 합을 반환")
     void 숫자하나_split() {
 
-        int result = StringAddCalculator.splitAndSum("1");
+        int result = splitAndSum("1");
 
         assertThat(result).isEqualTo(1);
     }
@@ -30,7 +31,7 @@ public class StringAddCalculatorTest {
     @DisplayName("쉼표(,)를 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환")
     void 쉼표구분자_split() {
 
-        int result = StringAddCalculator.splitAndSum("1,2,3");
+        int result = splitAndSum("1,2,3");
 
         assertThat(result).isEqualTo(6);
     }
@@ -39,7 +40,7 @@ public class StringAddCalculatorTest {
     @DisplayName("콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환")
     void 콜론구분자_split() {
 
-        int result = StringAddCalculator.splitAndSum("1:2:3");
+        int result = splitAndSum("1:2:3");
 
         assertThat(result).isEqualTo(6);
     }
@@ -48,7 +49,7 @@ public class StringAddCalculatorTest {
     @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환")
     void 콜론과쉼표_구분자_split() {
 
-        int result = StringAddCalculator.splitAndSum("1,2:3");
+        int result = splitAndSum("1,2:3");
 
         assertThat(result).isEqualTo(6);
     }
@@ -57,7 +58,7 @@ public class StringAddCalculatorTest {
     @DisplayName("커스텀 구분자를 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환")
     void 커스텀구분자_split() {
 
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        int result = splitAndSum("//;\n1;2;3");
 
         assertThat(result).isEqualTo(6);
     }
@@ -65,7 +66,7 @@ public class StringAddCalculatorTest {
     @Test
     @DisplayName("문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외를 throw")
     void split_Exception() {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("pp")).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> splitAndSum("pp")).isInstanceOf(RuntimeException.class);
     }
 
 
