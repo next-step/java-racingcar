6b3feac784b02a00170b24c2ce409b7fbf44213e
kimgwanghoon <khkimykk@naver.com>
2023-11-03 11:04:35 +0900

    test: 입력 유효성검사 테스트 추가

--- src/main/java/step3/InOutputView.java
+++ src/main/java/step3/InOutputView.java
@@ -6,19 +6,19 @@ public class InOutputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public int inputCarCount(){
+    public int inputCarCount() {
         return scanner.nextInt();
     }
 
-    public int inputMoveCount(){
+    public int inputMoveCount() {
         return scanner.nextInt();
     }
 
-    public void outputCarPrint(){
+    public void outputCarPrint() {
         System.out.println("자동차 대수는 몇 대 인가요?");
     }
 
-    public void outputMovePrint(){
+    public void outputMovePrint() {
         System.out.println("시도할 회수는 몇 회 인가요?");
     }
 }
---
+++ src/test/java/step3/ValidityCheckTest.java
@@ -0,0 +1,52 @@
+package step3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidityCheckTest {
+
+    @Test
+    public void 입력_숫자() {
+        //given
+        int number = 2;
+        //when
+        //then
+        assertThat(ValidityCheck.positiveNumberCheck(number)).isFrue();
+    }
+
+    @Nested
+    @DisplayName("입력값이 숫자가 아닌값 테스트")
+    class ValidityCheckFaild{
+        @ParameterizedTest
+        @NullAndEmptySource
+        @DisplayName("null, 빈문자열 체크")
+        public void 입력_null_또는_빈문자(int number) {
+            //given
+            //when
+            //then
+            assertThatThrownBy(() -> ValidityCheck.positiveNumberCheck(null)).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        public void 입력_음수() {
+            //given
+            //when
+            //then
+            assertThatThrownBy(() -> ValidityCheck.positiveNumberCheck(-1)).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        public void 입력_문자() {
+            //given
+            //when
+            //then
+            assertThatThrownBy(() -> ValidityCheck.positiveNumberCheck("a")).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+}
