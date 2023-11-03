a3b99ff163b6556ee4251cf84ada5a4afea987d8
solchan98 <dev.sol0427@gmail.com>
2023-11-03 17:33:47 +0900

    refactor: 온라인 강의 참여 후, 리팩토링 진행 - stream api 제거 - 매직넘버 상수화

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,47 +1,62 @@
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+    private static final int DEFAULT_NUMBER = 0;
+
+    private static final String DELIMITER = "[,:]";
+
     private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
 
     public static int splitAndSum(String text) {
         if (isEmpty(text)) {
-            return 0;
+            return DEFAULT_NUMBER;
         }
 
-        int[] numbers = stringToNumbers(text);
-        checkHasNegative(numbers);
+        return sum(toNumbers(split(text)));
+    }
 
-        return Arrays.stream(numbers).sum();
+    private static int[] toNumbers(String[] split) {
+        int[] numbers = new int[split.length];
+
+        for (int idx = 0; idx < split.length; idx++) {
+            int number = Integer.parseInt(split[idx]);
+            checkIsNegative(number);
+            numbers[idx] = number;
+        }
+
+        return numbers;
+    }
+
+    private static String[] split(String text) {
+        Matcher m = pattern.matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+
+            return m.group(2).split(customDelimiter);
+        }
+
+        return text.split(DELIMITER);
     }
 
     private static boolean isEmpty(String text) {
         return text == null || text.isEmpty();
     }
 
-    private static int[] stringToNumbers(String text) {
-        Matcher m = pattern.matcher(text);
-
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-
-            return Arrays.stream(m.group(2).split(customDelimiter))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-        }
-
-        return Arrays.stream(text.split("[,:]"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-    }
-
-    private static void checkHasNegative(int[] numbers) {
-        boolean hasNegative = Arrays.stream(numbers).anyMatch(a -> a < 0);
-
-        if (hasNegative) {
+    private static void checkIsNegative(int number) {
+        if (number < 0) {
             throw new RuntimeException();
         }
+    }
+
+    private static int sum(int[] numbers) {
+        int sum = 0;
+
+        for (int number: numbers) {
+            sum += number;
+        }
+
+        return sum;
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
