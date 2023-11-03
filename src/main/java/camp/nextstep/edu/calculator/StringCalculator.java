db0a8bca9969dea9f06cef5a5edac30349c59082
deepredk <deepred.kim@gmail.com>
2023-11-03 11:27:33 +0900

    refactor: 메서드 분리

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -9,33 +9,12 @@ public class StringCalculator {
             return 0;
         }
 
-        String[] values = split(expression);
-        if (hasCustomDelimiter(expression)) {
-            String customDelimiter = extractCustomDelimiter(expression);
-            expression = removeCustomDelimiter(expression);
-            values = split(expression, customDelimiter);
-        }
-
-        return sum(toInts(values));
+        int[] numbers = extractNumbers(expression);
+        return sum(numbers);
     }
 
-    private static String removeCustomDelimiter(String expression) {
-        int beginIndex = expression.indexOf('\n') + 1;
-        return expression.substring(beginIndex);
-    }
-
-    private static boolean hasCustomDelimiter(String expression) {
-        return expression.startsWith("\\");
-    }
-
-    private static String extractCustomDelimiter(String expression) {
-        int beginIndex = 1;
-        int endIndex = expression.indexOf('\n');
-        return expression.substring(beginIndex, endIndex);
-    }
-
-    private static String[] split(String str, String customDelimiter) {
-        return str.split(customDelimiter);
+    private static boolean isEmpty(String str) {
+        return str == null || str.isBlank();
     }
 
     private static int sum(int[] numbers) {
@@ -46,6 +25,10 @@ public class StringCalculator {
         return sum;
     }
 
+    private static int[] extractNumbers(String expression) {
+        return toInts(extractValues(expression));
+    }
+
     private static int[] toInts(String[] values) {
         int[] ints = new int[values.length];
         for (int i = 0; i < values.length; i++) {
@@ -54,11 +37,32 @@ public class StringCalculator {
         return ints;
     }
 
-    private static String[] split(String str) {
-        return str.split(DEFAULT_DELIMITER);
+    private static String[] extractValues(String expression) {
+        String delimiter = extractDelimiter(expression);
+        expression = removeDelimiter(expression);
+        return split(expression, delimiter);
     }
 
-    private static boolean isEmpty(String str) {
-        return str == null || str.isBlank();
+    private static String removeDelimiter(String expression) {
+        int beginIndex = expression.indexOf('\n') + 1;
+        return expression.substring(beginIndex);
+    }
+
+    private static String[] split(String str, String customDelimiter) {
+        return str.split(customDelimiter);
+    }
+
+    private static String extractDelimiter(String expression) {
+        if (!hasCustomDelimiter(expression)) {
+            return DEFAULT_DELIMITER;
+        }
+
+        int beginIndex = 1;
+        int endIndex = expression.indexOf('\n');
+        return expression.substring(beginIndex, endIndex);
+    }
+
+    private static boolean hasCustomDelimiter(String expression) {
+        return expression.startsWith("\\");
     }
 }
