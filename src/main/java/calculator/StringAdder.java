5b4f5b6cce47c20cdbd7cd416e903c611367e808
henry174 <dltmdwns812@daum.net>
2023-11-03 22:07:34 +0900

    StringAdder 리팩터링: 한 메서드에 오직 한 단계의 들여쓰기(indent)만 한다.

--- src/main/java/calculator/StringAdder.java
+++ src/main/java/calculator/StringAdder.java
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 public class StringAdder {
 
     /* 커스텀 구분자로 사용할 수 있는 정규식 특수 문자 목록. 이스케이핑 처리가 필요한지 확인하기 위해 사용 */
-    public static final List<Character> REGEX_SPECIAL_CHARACTER_LIST = List.of('{', '}', '[', ']', '+', '*', '.', '$', '^', '|', '?');
+    private static final List<Character> REGEX_SPECIAL_CHARACTER_LIST = List.of('{', '}', '[', ']', '+', '*', '.', '$', '^', '|', '?');
 
     /** 기본적으로 사용되는 구분자 목록 */
     private static final List<String> DEFAULT_DELIMITER_LIST = List.of(",", ":");
@@ -24,21 +24,21 @@ public class StringAdder {
      * 기본 구분자는 , 또는 :입니다.
      * 사용자가 직접 구분자를 정의하려면 //과 \n 사이에 구분자를 넣으십시오.
      *
-     * @param inputString 구분자로 구분되는 정수들을 포함하는 문자열
+     * @param input 구분자로 구분되는 정수들을 포함하는 문자열
      *
      * @return 해당 정수들의 합
      */
-    public static int sum(String inputString) {
-        if (inputString == null) {
+    public static int sum(String input) {
+        if (input == null) {
             return 0;
         }
 
-        if (inputString.isEmpty()) {
+        if (input.isEmpty()) {
             return 0;
         }
 
-        String customDelimiter = extractCustomDelimiter(inputString);
-        String trimedInputString = trimCustomDelimiterDeclaration(inputString);
+        String customDelimiter = extractCustomDelimiter(input);
+        String trimedInputString = trimCustomDelimiterDeclaration(input);
 
         List<String> totalDelimiterList = new ArrayList<>(DEFAULT_DELIMITER_LIST);
         if (!customDelimiter.isEmpty()) {
@@ -72,45 +72,41 @@ public class StringAdder {
     private static List<Integer> convertStringListToIntList(List<String> stringValueList) {
         List<Integer> intValueList = new ArrayList<>();
         for(String stringValue : stringValueList) {
-            try {
-                Integer.parseInt(stringValue);
-            }
-            catch (NumberFormatException e) {
-                throw new IllegalArgumentException("허용되지 않는 값: " + stringValue);
-            }
-
-            int intValue = Integer.parseInt(stringValue);
-            if (intValue < 0) {
-                throw new IllegalArgumentException("허용되지 않는 값: " + intValue);
-            }
-            intValueList.add(intValue);
+            intValueList.add(convertStringValueToIntValue(stringValue));
         }
         return intValueList;
+    }
+
+    /**
+     * 문자열로 되어 있는 값을 정수형으로 바꿉니다.
+     * 이 과정에서 허용되지 않는 값일 경우 예외를 던집니다.
+     * 허용되는 값은 0 또는 양의 정수만입니다.
+     * @param stringValue 정수형으로 바꾸려는 값
+     * @return 정수형으로 바뀐 값
+     */
+    private static int convertStringValueToIntValue(String stringValue) {
+        Integer intValue = null;
+        try {
+            intValue = Integer.parseInt(stringValue);
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("허용되지 않는 값: " + stringValue);
+        }
+
+        if (intValue < 0) {
+            throw new IllegalArgumentException("허용되지 않는 값: " + intValue);
+        }
+        return intValue;
     }
 
     private static List<String> splitIntegerListByDelimiters(String integerList, List<String> delimiterList) {
         String splitRegex = "";
         for (String delimiter : delimiterList) {
-            splitRegex += "|" + escapingRegexSpecialCharacter(delimiter);
+            splitRegex += "|" + escapingStringForRegex(delimiter);
         }
         splitRegex = splitRegex.substring(1);
 
         return Arrays.asList(integerList.split(splitRegex));
-    }
-
-    /**
-     * 주어진 문자열에 구분자가 있는지 확인합니다.
-     * @param integerList 구분자로 구분되는 정수를 포함하는 문자열
-     * @return 구분자가 있다면 true를 반환
-     */
-    private static boolean doesContainDelimiter(String integerList, List<String> delimiterList) {
-        for (String delimiter : delimiterList) {
-            if (integerList.contains(delimiter)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     /**
@@ -149,21 +145,27 @@ public class StringAdder {
      * @param code 정규식에 넣으려고 하는 문자열
      * @return 이스케이핑 처리가 된 문자열
      */
-    private static String escapingRegexSpecialCharacter(String code) {
+    private static String escapingStringForRegex(String code) {
         StringBuilder escapedString = new StringBuilder();
 
         for (int p = 0; p < code.length(); p++) {
-            char original = code.charAt(p);
-
-            if (REGEX_SPECIAL_CHARACTER_LIST.contains(original)) {
-                escapedString.append('\\')
-                        .append(original);
-            }
-            else {
-                escapedString.append(original);
-            }
+            escapedString.append(escapingCharacterForRegex(code.charAt(p)));
         }
 
         return escapedString.toString();
+    }
+
+    /**
+     * 주어진 한 글자가 이스케이핑이 필요한 글자면 이스케이핑하여 반환합니다.
+     * 아니라면 그 글자를 문자열로 변환하여 반환합니다.
+     * @param code 이스케이핑 시도할 글자
+     * @return 이스케이핑이 필요할 경우 이스케이핑이 된 문자열
+     */
+    private static String escapingCharacterForRegex(char code) {
+        if (REGEX_SPECIAL_CHARACTER_LIST.contains(code)) {
+            return "//" + String.valueOf(code);
+        }
+
+        return String.valueOf(code);
     }
 }
