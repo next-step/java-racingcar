c527f6f2fb556a1c924a64fda9ad065f21f3b060
taegoon <hantaegune@gmail.com>
2023-11-03 23:24:00 +0900

    step2 StringAddCalculator 업데이트 1. 코드 블럭 추가 2. 디폴트값 상수로 변경 3. IllegalArgumentException로 에러 명시 4. 숫자10 이상 인식 가능 하게 수정 5. 기타 중복 코드 제거 ..

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,60 +1,63 @@
 package study;
 
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
+    public static final String REGEX_PATTERN_COMPILE = "//(.)\n(.*)";
+    public static final String REGEX_SPLIT = "[,|:]";
+    public static final int ZERO = 0;
+
+
     public static int splitAndSum(String text){
-        int sum =0;
 
-        // 1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)
-        if (text == null ||text.isEmpty()) return sum;
+        String[] tokens ;
 
-        // 6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)
-        if (text.contains("-")) throw new RuntimeException("음수를 전달할 경우 RuntimeException 예외가 발생!!");
-
-        // 2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)
-        if(text.length()==1 && integerOrNot(text)){
-            int number = convertStringToInt(text);
-            return number;
+        // splitAndSum_null_또는_빈문자
+        if(text == null || text.isEmpty()){
+            return ZERO;
         }
 
-        // 3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)
-        // 4. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)
-        String[] numbers = text.split(",|:");
-        // 앞 단계의 구분자가 없는 경우도 split()을 활용해 구현할 수 있는지 검토해 본다.
-        for (int i = 0; i < numbers.length; i++) {
-            if(integerOrNot(numbers[i])) sum += convertStringToInt(numbers[i]);
-            if(i+1==numbers.length) return sum;
-        }
+        // 음수 체크
+        negativeCheck(text);
 
-
-        // 5. “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\n1;2;3” => 6)
-        // java.util.regex 패키지의 Matcher, Pattern import
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = Pattern.compile(REGEX_PATTERN_COMPILE).matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
-            String[] tokens= m.group(2).split(customDelimiter);
-            // 덧셈 구현
-            for (int i = 0; i < tokens.length; i++) {
-                if(integerOrNot(tokens[i])) sum += convertStringToInt(tokens[i]);
-                if(i+1==tokens.length)return sum;
-
-            }
+            tokens= m.group(2).split(customDelimiter);
+            return loop(tokens);
         }
 
-        return 0;
+        tokens = text.split(REGEX_SPLIT);
+        return loop(tokens);
+    }
+
+    private static void negativeCheck(String text) {
+        if(text.contains("-1")){
+            throw new IllegalArgumentException("문자열 중 음수가 존재 합니다");
+        }
+    }
+
+    private static int loop(String[] tokens) {
+        int sum  = 0;
+        for(String str : tokens){
+            sum = getSum(sum, str);
+        }
+        return sum;
+    }
+
+    private static int getSum(int sum, String str) {
+        if(isNumber(str)){
+            sum += Integer.parseInt(str);
+        }
+        return sum;
     }
 
     // 숫자 여부 판단
-    public static boolean integerOrNot(String str){
+    public static boolean isNumber(String str){
         return str.matches("-?\\d+");
-    }
-
-    // String Convert to Number
-    public static int convertStringToInt(String str){
-        return Integer.parseInt(str);
     }
 
 }
