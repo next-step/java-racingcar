57eba3bdf26147b4c7a1d34098c847cb19b57716
birdieHyun <flaehdan@gmail.com>
2023-11-03 12:16:49 +0900

    feat: 커스텀 예외 문자로 숫자 합 구하기 기능 구현

--- src/main/java/study/StringSum.java
+++ src/main/java/study/StringSum.java
@@ -1,14 +1,24 @@
 package study;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringSum {
 
 
     public int sumStringByDelimiter(String data) {
-        String[] split = data.split(",|:");
+        String delimiter = ",|:";
 
-        return Arrays.stream(split)
+        if (data.startsWith("//")) {
+            Matcher m = Pattern.compile("//(.)\n(.*)").matcher(data);
+            if (m.find()) {
+                delimiter = m.group(1);
+                data = m.group(2);
+            }
+        }
+
+        return Arrays.stream(data.split(delimiter))
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
