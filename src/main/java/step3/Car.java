9888e795fcea05ade3a2502fb6418af5e707ecfa
kimgwanghoon <khkimykk@naver.com>
2023-11-03 13:43:14 +0900

    feat: 자동차 기능 구현

--- src/main/java/step3/Car.java
+++ src/main/java/step3/Car.java
@@ -1,5 +1,31 @@
 package step3;
 
 public class Car {
+    private int position;
 
+    public Car(int initPosition) {
+        this.position = initPosition;
+    }
+
+    public int nowPosition() {
+        return position;
+    }
+
+    public void move(int moveNumber) {
+        if (isMoveAbleCheck(moveNumber)) {
+            position++;
+        }
+    }
+
+    private boolean isMoveAbleCheck(int moveNumber) {
+        return moveNumber >= 4;
+    }
+
+    public String drawPosition() {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < position; i++) {
+            result.append("-");
+        }
+        return result.toString();
+    }
 }
