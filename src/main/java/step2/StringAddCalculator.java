1fdc475ce49aed079d6458d49234277b5b910b39
Myunghoon Ju <myunghoonju@gmail.com>
2023-11-03 22:42:17 +0900

    2단계 문자열 덧셈 계산기 (#4865)
    * init commit
    
    * 2단계 문자열 덧셈 계산기
    
    * 리뷰반영
    
    - 함수 인수명 변경 data to numberStr
    - findPattern(), geNumbers()  함수 통합
    - 테스트 수정(jUnit annotations 추가)

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,47 @@
+package step2;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final String BASIC_DELIMITER = ",|:";
+    private static final String SPECIAL_DELIMITER = "//(.)\n(.*)";
+
+    private static final String NEGATIVE_NUMBER_ERROR = "문자열에 음수가 포함되어 있습니다.";
+
+    private static final Pattern SPECIAL_PATTERN = Pattern.compile(SPECIAL_DELIMITER);
+
+    public static int splitAndSum(String numberStr) {
+        if (numberStr == null || numberStr.isBlank()) {
+            return 0;
+        }
+
+        List<String> numbers = split(numberStr);
+        checkPositive(numbers);
+
+        return numbers.stream()
+                      .mapToInt(Integer::parseInt)
+                      .sum();
+    }
+
+    private static List<String> split(String numberStr) {
+        Matcher m = SPECIAL_PATTERN.matcher(numberStr);
+        if (m.find()) {
+            String delimiter = m.group(1);
+            return List.of(m.group(2).split(delimiter));
+        }
+
+        return List.of(numberStr.split(BASIC_DELIMITER));
+    }
+
+    private static void checkPositive(List<String> numbers) {
+        for (String number : numbers) {
+            int parsedInt = Integer.parseInt(number);
+            if (parsedInt < 0) {
+                throw new IllegalArgumentException(NEGATIVE_NUMBER_ERROR + " - " + number);
+            }
+        }
+    }
+}
---
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -0,0 +1,54 @@
+package step2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class StringAddCalculatorTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void splitAndSum_null_또는_빈문자(String input) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1"})
+    public void splitAndSum_숫자하나(String input) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2"})
+    public void splitAndSum_쉼표구분자(String input) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1,2,3"})
+    public void splitAndSum_negative(String input) {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(input)).isInstanceOf(RuntimeException.class);
+    }
+}
