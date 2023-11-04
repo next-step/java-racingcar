fd2998baca67c8b154c29f268826e0135856e653
kyunghyun-park <qqqq_o@naver.com>
2023-11-05 01:48:17 +0900

    refactor: 랜덤 인스턴스 상수화 및 상수 네이밍 변경

--- src/main/java/carracing/domain/Car.java
+++ src/main/java/carracing/domain/Car.java
@@ -3,25 +3,25 @@ package carracing.domain;
 import java.util.Random;
 
 public class Car {
-    public static final int CAN_MOVE_VALUE = 4;
+    private static final int MIN_MOVE_VALUE = 4;
+    private static final int INITIAL_POSITION = 1;
+    private static final Random RANDOM = new Random();
     private int position;
 
     public Car() {
-        this.position = 0;
+        this.position = INITIAL_POSITION;
     }
 
     public void move() {
-        Random random = new Random();
-        int randomValue = random.nextInt(10);
-
-        if(randomValue >= CAN_MOVE_VALUE) {
+        int randomValue = RANDOM.nextInt(10);
+        if(randomValue >= MIN_MOVE_VALUE) {
             position += 1;
         }
     }
 
     //테스트를 위한 오버로딩ㅠㅠ
     public void move(int randomValue) {
-        if(randomValue >= CAN_MOVE_VALUE) {
+        if(randomValue >= MIN_MOVE_VALUE) {
             position += 1;
         }
     }
