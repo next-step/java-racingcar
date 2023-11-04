c04f27b87edc4058337e286814cd00fc94e59ef4
박상권 <79737870+pawoo0211@users.noreply.github.com>
2023-11-04 17:29:28 +0900

    1단계 - 학습 테스트 실습 (#4920)
    * Initial Commit
    
    * Initial Commit
    
    * Commit Test
    
    * [step1] 문자열 테스트 코드 작성
    
    * [step1] 문자열 테스트 코드 추가 작성
    
    * [step1] Set 자료형 테스트 코드 작성
    
    * [step1] Set 요구사항에 따른 테스트 코드 추가 및 추가 리팩터링

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,44 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@DisplayName("Set 자료형 관련 테스트")
+public class SetTest {
+    private Set<Integer> numbers = new HashSet<>();
+
+    @BeforeEach
+    void setUp() {
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set의 크기를 확인한다.")
+    @Test
+    void checkSetSize() {
+        int result = numbers.size();
+        Assertions.assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("Set의 요소를 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void checkSetValue(int input) {
+        Assertions.assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @DisplayName("입력된 값이 Set 변수에 존재하는지 존재하지 않는지 확인한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    void checkSetValue(int input, boolean expected) {
+        Assertions.assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,61 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("문자열 자료형 관련 테스트")
+public class StringTest {
+
+    @DisplayName("콤마를 기준으로 숫자를 분리한다.")
+    @Test
+    void splitNumber() {
+        // given
+        String numberOneAndTwo = "1,2";
+        String numberOne = "1";
+
+        // then
+        String[] splitOneAndTwoNumber = numberOneAndTwo.split(",");
+        String[] splitOneNumber = numberOne.split(",");
+
+        // then
+        Assertions.assertThat(splitOneAndTwoNumber).containsExactly("1", "2");
+        Assertions.assertThat(splitOneNumber).containsExactly("1");
+    }
+
+    @DisplayName("괄호를 제거하고 콤마를 기준으로 숫자를 분리한다.")
+    @Test
+    void splitNumberAfterDeleteParenthesis() {
+        // given
+        String number = "(1,2)";
+
+        // when
+        String result = number.substring(1, number.length()-1);
+
+        // then
+        Assertions.assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("문자열에서 특정 위치의 문자를 가져온다.")
+    @Test
+    void extractChar() {
+        // given
+        String testData = "abc";
+
+        // when
+        char extractData = testData.charAt(0);
+
+        // then
+        Assertions.assertThat(extractData).isEqualTo('a');
+    }
+
+    @DisplayName("존재하지 않는 문자열의 인덱스에 접근하면 예외가 발생한다.")
+    @Test
+    void indexBoundException() {
+        // given
+        String testData = "abc";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> {
+            testData.charAt(-1);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+}
