d2e7a65ab4bd8bb0b34e6101442b4c8222590771
pawoo0211 <pawoo0211@naver.com>
2023-11-04 14:08:31 +0900

    [step1] 문자열 테스트 코드 추가 작성

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -20,4 +20,30 @@ public class StringTest {
         Assertions.assertThat(splitOneAndTwoNumber).contains("1");
         Assertions.assertThat(splitOneNumber).containsExactly("1");
     }
+
+    @DisplayName("괄호를 제거하고 콤마를 기준으로 숫자를 분리한다.")
+    @Test
+    void splitNumberAfterDeleteParenthesis() {
+        // given
+        String number = "(1,2)";
+
+        // when
+        String result = number.substring(1, number.length()-1);
+
+        // then
+        Assertions.assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("문자열에서 특정 위치의 문자를 가져온다.")
+    @Test
+    void extractChar() {
+        // given
+        String testData = "abc";
+
+        // when
+        char extractData = testData.charAt(0);
+
+        // then
+        Assertions.assertThat(extractData).isEqualTo('a');
+    }
 }
