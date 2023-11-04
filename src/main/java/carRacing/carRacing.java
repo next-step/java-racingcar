8fb849d8d4312421bd228cd29744b305f60059f0
chocheolhee <whcjfgml92@naver.com>
2023-11-04 21:52:43 +0900

    refactor: 사용자 입력 로직 변경

--- src/main/java/carRacing/carRacing.java
+++ src/main/java/carRacing/carRacing.java
@@ -1,19 +1,12 @@
 package carRacing;
 
-import java.util.Scanner;
-
 public class carRacing {
     public static void main(String[] args) {
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        scanner();
+        InputScanner inputScanner = new InputScanner();
+        inputScanner.inputCarCount();
+        inputScanner.inputTrialCount();
 
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        scanner();
     }
 
-    private static void scanner() {
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextInt();
-    }
 }
