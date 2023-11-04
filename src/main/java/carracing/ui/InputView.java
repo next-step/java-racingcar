f14e726fe995c5375494648b6a68698fd2a811b3
kyunghyun-park <qqqq_o@naver.com>
2023-11-04 23:18:53 +0900

    refactor: 입력받는 텍스트 출력 부분 이동 및 Scanner 자원 닫기

--- src/main/java/carracing/ui/InputView.java
+++ src/main/java/carracing/ui/InputView.java
@@ -3,13 +3,17 @@ package carracing.ui;
 import java.util.Scanner;
 
 public class InputView {
-    private static Scanner sc = new Scanner(System.in);
-
     public static int inputCarCount() {
-        return sc.nextInt();
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        try (Scanner sc = new Scanner(System.in)){
+            return sc.nextInt();
+        }
     }
 
     public static int inputTryCount() {
-        return sc.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        try (Scanner sc = new Scanner(System.in)){
+            return sc.nextInt();
+        }
     }
 }
