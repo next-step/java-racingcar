480f00bc5ee175614378385a6cd5bb63eb4a524e
g0rani <pjw0405@naver.com>
2023-11-04 22:00:06 +0900

    test: 반복되는 자동차 움직임에 대한 테스트 코드 변경
    - 반복되는 테스트에 대한 내용을 ParameterizedTest로 변경
    - 반복 되는 코드가 있다 하여도 경우에 따라 2가지로 나눔

--- src/test/java/racinggame/domain/CarTest.java
+++ src/test/java/racinggame/domain/CarTest.java
@@ -1,9 +1,10 @@
 package racinggame.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import racinggame.domain.strategy.MovingStrategy;
 
 class CarTest {
@@ -12,62 +13,39 @@ class CarTest {
     void 자동차를_생성하면_처음_거리는_0이다() {
         MovingStrategy movingStrategy = new TestMovingStrategy(3);
         MovingValidator movingValidator = new MovingValidator(movingStrategy);
+
         Car actual = new Car(movingValidator);
         Car expected = new Car(new MovingDistance(), movingValidator);
 
         assertThat(actual).isEqualTo(expected);
     }
 
-    @Test
-    void 자동차는_4에서_9_사이에서는_움직인다다() {
+    @ParameterizedTest(name = "자동차는 {0}에서는 움직인다")
+    @CsvSource({"4, 1", "9, 1"})
+    void 자동차는_4에서_9_사이에서는_움직인다(int movingValue, int movingDistance) {
+        MovingStrategy movingStrategy = new TestMovingStrategy(movingValue);
+        MovingValidator movingValidator = new MovingValidator(movingStrategy);
+        Car actual = new Car(movingValidator);
 
-        assertAll(
-            () -> {
-                MovingStrategy movingStrategy = new TestMovingStrategy(4);
-                MovingValidator movingValidator = new MovingValidator(movingStrategy);
-                Car actual = new Car(movingValidator);
-                actual.move();
-                Car expected = new Car(new MovingDistance(1), movingValidator);
+        actual.move();
 
-                assertThat(actual).isEqualTo(expected);
-            },
-            () -> {
-                MovingStrategy movingStrategy = new TestMovingStrategy(9);
-                MovingValidator movingValidator = new MovingValidator(movingStrategy);
-                Car actual = new Car(movingValidator);
-                actual.move();
-                Car expected = new Car(new MovingDistance(1), movingValidator);
+        Car expected = new Car(new MovingDistance(movingDistance), movingValidator);
 
-                assertThat(actual).isEqualTo(expected);
-            }
-        );
-
+        assertThat(actual).isEqualTo(expected);
     }
 
-    @Test
-    void 자동차는_0에서_3_사이에서는_움직일_수_없다() {
+    @ParameterizedTest(name = "자동차는 {0}에서 움직일 수 없다")
+    @CsvSource({"0, 0", "3, 0"})
+    void 자동차는_0에서_3_사이에서는_움직일_수_없다(int movingValue, int movingDistance) {
+        MovingStrategy movingStrategy = new TestMovingStrategy(movingValue);
+        MovingValidator movingValidator = new MovingValidator(movingStrategy);
+        Car actual = new Car(movingValidator);
 
-        assertAll(
-            () -> {
-                MovingStrategy movingStrategy = new TestMovingStrategy(0);
-                MovingValidator movingValidator = new MovingValidator(movingStrategy);
-                Car actual = new Car(movingValidator);
-                actual.move();
-                Car expected = new Car(new MovingDistance(0), movingValidator);
+        actual.move();
 
-                assertThat(actual).isEqualTo(expected);
-            },
-            () -> {
-                MovingStrategy movingStrategy = new TestMovingStrategy(3);
-                MovingValidator movingValidator = new MovingValidator(movingStrategy);
-                Car actual = new Car(movingValidator);
-                actual.move();
-                Car expected = new Car(new MovingDistance(0), movingValidator);
+        Car expected = new Car(new MovingDistance(movingDistance), movingValidator);
 
-                assertThat(actual).isEqualTo(expected);
-            }
-        );
-
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
@@ -76,6 +54,7 @@ class CarTest {
         MovingValidator movingValidator = new MovingValidator(movingStrategy);
         Car car = new Car(movingValidator);
         car.move();
+
         MovingDistance actual = car.movingDistance();
         MovingDistance expected = new MovingDistance(1);
 
