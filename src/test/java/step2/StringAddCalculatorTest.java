9fdd357a7e7ff05282d21524f635d57470510fea
jane096 <jes7077@naver.com>
2023-11-04 22:40:50 +0900

    테스트코드 메서드명 변경

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -13,7 +13,7 @@ public class StringAddCalculatorTest {
     @ParameterizedTest
     @DisplayName("null이나 빈 문자열이 들어온다면 0을 반환한다.")
     @NullAndEmptySource
-    public void splitAndSum_null_또는_빈문자(String text) {
+    public void splitAndSumNullOrEmptyString(String text) {
         int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(0);
 
@@ -23,35 +23,35 @@ public class StringAddCalculatorTest {
 
     @Test
     @DisplayName("숫자 하나만 넣으면 입력한 숫자를 반환한다.")
-    public void splitAndSum_숫자하나() throws Exception {
+    public void splitAndSumOneNumber() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     @DisplayName("쉼표 구분자 기준으로 숫자의 합을 반환한다.")
-    public void splitAndSum_쉼표구분자() throws Exception {
+    public void splitAndSumWithCommaSeparator() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     @DisplayName("쉼표 또는 콜론 구분자를 기준으로 숫자희 합을 반환한다.")
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+    public void splitAndSumWithCommaAndSemiColon() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     @DisplayName("커스텀한 구분자를 넣었을 때 숫자들의 합을 반환한다.")
-    public void splitAndSum_custom_구분자() throws Exception {
+    public void splitAndSumCustomSeparator() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     @DisplayName("음수를 입력했을 경우 예외가 발생한다.")
-    public void splitAndSum_negative() throws Exception {
+    public void splitAndSumWithNegativeNumber() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
