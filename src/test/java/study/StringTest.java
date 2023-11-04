49ac96ea1f5645651ca07fe044b8fc4261289573
eddy.yeong <ldy9155@naver.com>
2023-11-05 02:15:35 +0900

    feat: 자동차 경주 2단계-문자열 덧셈 계산기 및 1단계-학습테스트 실습 보완 추가 (#4914)

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,18 +1,30 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
     @Test
-    void split(){
-          String[] result = "1,2".split(",");
-          assertThat(result).containsExactly("1","2");
+    @DisplayName("Split 테스트")
+    public void split(){
+        java.lang.String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1","2");
     }
 
     @Test
-    void substring(){
-        String result = "(1,2)".substring(1, 4);
+    @DisplayName("SubString 테스트")
+    void subString(){
+        java.lang.String result = "(1,2)".substring(1,4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("예외 발생 테스트")
+    void charAt(){
+        assertThatThrownBy(() -> {
+            char result = "abc".charAt(5);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("index out of range");
     }
 }
