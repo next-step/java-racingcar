fe1016376bf4d13906d46b378ff86f0e1461af8d
kyunghyun-park <qqqq_o@naver.com>
2023-11-05 01:03:57 +0900

    refactor: cars 인스턴스 변수로 수정

--- src/main/java/carracing/Racing.java
+++ src/main/java/carracing/Racing.java
@@ -5,8 +5,14 @@ import carracing.domain.Cars;
 import carracing.ui.InputView;
 
 public class Racing {
+    private final Cars cars;
+
+    public Racing(Cars cars) {
+        this.cars = cars;
+    }
+
     public static void main(String[] args) {
-        Racing racing = new Racing();
+        Racing racing = new Racing(new Cars());
         racing.start();
     }
 
@@ -14,20 +20,20 @@ public class Racing {
         int carCount = InputView.inputCarCount();
         int tryCount = InputView.inputTryCount();
 
-        Cars cars = createCar(carCount);
-        go(cars, tryCount);
+        createCar(carCount);
+        go(tryCount);
     }
-
-    private void go(Cars cars, int tryCount) {
+    private void go(int tryCount) {
         cars.move(tryCount);
     }
 
-    public Cars createCar(int carCount) {
-        Cars cars = new Cars();
+    public void createCar(int carCount) {
         for (int i = 0; i < carCount; i++) {
             cars.addCar(new Car());
         }
+    }
 
+    public Cars getCars() {
         return cars;
     }
 }
