317b4a1699fcff90a106d9de08e929919806f5cc
g0rani <pjw0405@naver.com>
2023-11-04 23:14:13 +0900

    feat: 자동차 이름 글자수 제한 구현
    - 자동차 이름 입력 밧에 대한 글자수 제한 구현
    - custom한 exception 을 이용

--- src/main/java/racinggame/domain/CarName.java
+++ src/main/java/racinggame/domain/CarName.java
@@ -1,16 +1,26 @@
 package racinggame.domain;
 
+import racinggame.domain.exception.InputOverFiveLengthException;
+
 public class CarName {
 
+    public static final int MAX_LENGTH = 5;
     private String name;
 
     public CarName(String name) {
         if (isBlank(name)) {
             throw new RuntimeException();
         }
+        if (isOverFiveLength(name)) {
+            throw new InputOverFiveLengthException();
+        }
     }
 
     private static boolean isBlank(String text) {
         return text == null || text.isBlank();
+    }
+
+    private boolean isOverFiveLength(String name) {
+        return name.length() > MAX_LENGTH;
     }
 }
