0d1d7a7a52d7371cba9262b93067a0727e246ce5
jeseungkim <qazwsx3745@naver.com>
2023-11-04 15:05:18 +0900

    step2

--- src/test/java/FuncTest/debugTest.java
+++ src/test/java/FuncTest/debugTest.java
@@ -1,2 +1,23 @@
-package FuncTest;public class debugTest {
+package FuncTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class debugTest {
+    @Test
+    void debug3() {
+        String str = "//;\n1;2;3";
+        assertThat(str.substring(str.indexOf("\n")+1)).isEqualTo("1;2;3");
+    }
+    @Test
+    void debug2() {
+        assertThat(",|:|" + ";").isEqualTo(",|:|;");
+    }
+    @Test
+    void debug() {
+        String str = "//;\n1;2;3";
+        assertThat(str.substring(str.indexOf("//")+2,str.indexOf("\n"))).isEqualTo(";");
+    }
+
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,22 +1,36 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
+import org.junit.Test;
+
+import static calculator.Calculator.cal;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-public class CTest {
-
+public class CalculatorTest {
     @Test
-    void null_값() {
-        int result = Calculator.cal(null);
-        assertThat(result).isEqualTo(0);
-
-
+    public void minusException() {
+        assertThatThrownBy( () -> cal("-1,2,3")).isInstanceOf(RuntimeException.class);
     }
-
-
     @Test
-    void 숫자_하나() {
-        assertThat()
+    public void customGubunMark() {
+        assertThat(cal("//;\n1;2;3")).isEqualTo(6);
+    }
+    @Test
+    public void commaColonSum() {
+        assertThat(cal("1,2:3")).isEqualTo(6);
+    }
+    @Test
+    public void sum() {
+        assertThat(cal("1,2")).isEqualTo(3);
+    }
+    @Test
+    public void 숫자반환() {
+        assertThat(cal("1")).isEqualTo(1);
+    }
+    @Test
+    public void null_빈문자열_값() {
+        assertThat(cal(null)).isEqualTo(0);
     }
 }
