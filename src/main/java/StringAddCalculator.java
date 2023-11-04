9f1a8d4cfeeaf6c08bf2d1e13055398cc6ef1fe1
pilming44 <pilming44@naver.com>
2023-11-04 14:44:13 +0900

    refactor: 리뷰내용을 반영해서 StringAddCalculator 리팩토링

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -2,6 +2,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String DEFAULT_DELIMITER = ",|:";
+
     public int splitAndSum(String input) {
         if(input == null || input.isEmpty()){
             //값이 없다면 0리턴
@@ -10,36 +14,47 @@ public class StringAddCalculator {
         //스플릿
         String[] values = this.split(input);
 
-        return this.sum(values);
+        return this.sum(parsingInts(values));
     }
 
     private String[] split(String input) {
         //커스텀 구분자
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        Matcher m = CUSTOM_PATTERN.matcher(input);
 
         if (m.find()) {
             //커스텀 패턴이 있다면 커스텀 구분자로 스플릿
-            String delimiter = m.group(1);
+            String customDelimiter = m.group(1);
             String values = m.group(2);
-            return values.split(delimiter);
+            return values.split(customDelimiter);
         }
 
         //일반 구분자로 값 리턴
-        return input.split(",|:");
+        return input.split(DEFAULT_DELIMITER);
     }
 
-    private int sum(String[] values) {
+    private int sum(int[] intValues) {
         int sum = 0;
 
-        for(String value : values) {
-            int intValue = Integer.parseInt(value);
+        for (int intValue : intValues) {
+            sum += intValue;
+        }
+
+        return sum;
+    }
+
+    private int[] parsingInts(String[] values) {
+        int[] ints = new int[values.length];
+
+        for(int index = 0; index < values.length; index++) {
+            int intValue = Integer.parseInt(values[index]);
 
             if(intValue < 0) {
                 //음수일경우 RuntimeException 발생
                 throw new RuntimeException("음수를 입력할 수 없습니다.");
             }
-            sum += intValue;
+            ints[index] = intValue;
         }
-        return sum;
+        return ints;
     }
+
 }
