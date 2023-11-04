5929441b9902089ff2ba81304f34707ac336d957
jane096 <jes7077@naver.com>
2023-11-05 00:05:29 +0900

    정규표현식 수정

--- src/main/java/step2/constants/RegexConstants.java
+++ src/main/java/step2/constants/RegexConstants.java
@@ -2,5 +2,5 @@ package step2.constants;
 
 public class RegexConstants {
 
-    public static final String EXTRACT_NUMBER_DYNAMIC_SEPARATOR = "[`\\n;:,\\{\\}\\[\\]\\\\\\/]";
+    public static final String EXTRACT_NUMBER_DYNAMIC_SEPARATOR = "[\\n;:,/]";
 }
