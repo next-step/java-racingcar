0ae0198b0732de51824b792a01e5916e176758ab
hhj <hghgusrl@naver.com>
2023-11-04 13:15:53 +0900

    Step2

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -55,27 +56,42 @@ public class StringAddCalculator {
 
     private static int getNumber(String[] tokens) {
         int number;
-        int[] list = Arrays.stream(tokens)
-                .mapToInt(Integer::parseInt)
-                .filter(num -> {
-                    if(num < 0){
-                        throw new RuntimeException();
-                    }
-                    return true;
-                })
-                .toArray();
-        number = Arrays.stream(list).sum();
+        int[] list = getIntArray(tokens);
+        number = getSum(list);
         return number;
     }
 
+    private static int[] getIntArray(String[] tokens) {
+
+        int[] intList = new int[tokens.length];
+        for(int i=0; i<tokens.length; i++){
+            validNumber(tokens, i);
+            intList[i] = Integer.parseInt(tokens[i]);
+        }
+
+        return intList;
+    }
+
+    private static void validNumber(String[] tokens, int i) {
+        if(Integer.parseInt(tokens[i]) < 0){
+            throw new RuntimeException("음수는 불가능");
+        }
+    }
+
+    private static int getSum(int[] list) {
+        int sum = 0;
+        for(int i=0; i<list.length; i++){
+            sum += list[i];
+        }
+        return sum;
+    }
+
     private static String[] getStrings(String text) {
-        String[] tokens= text.split(SPLIT_TEXT);
-        return tokens;
+        return text.split(SPLIT_TEXT);
     }
 
     private static Matcher getMatcher(String text) {
-        Matcher matcher = pattern.matcher(text);
-        return matcher;
+        return pattern.matcher(text);
     }
 
 }
