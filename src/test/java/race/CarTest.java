528ddef93300c82975fcc79598f537b025183430
qwe5507 <apvmffkdls@gmail.com>
2023-11-05 01:26:34 +0900

    test: go(), move() 테스트 더 상세하게 변경

--- src/test/java/race/CarTest.java
+++ src/test/java/race/CarTest.java
@@ -2,14 +2,18 @@ package race;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
-    @DisplayName("자동차가 이동 할 때, 4이상의 값이 나오면 이동한다.")
-    @Test
-    void goTest() {
+    @DisplayName("자동차가 이동 할 때, 4~9 값이 나오면 전진한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void goTest(int number) {
         // given
-        final Car car = new Car(() -> 9);
+        final Car car = new Car(() -> number);
 
         // when
         car.move();
@@ -18,11 +22,12 @@ class CarTest {
         assertThat(car.toString()).isEqualTo("-");
     }
 
-    @DisplayName("자동차가 이동 할 때, 4미만의 값이 나오면 이동하지 않는다.")
-    @Test
-    void stopTest() {
+    @DisplayName("자동차가 이동 할 때, 0~3 값이 나오면 전진하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void stopTest(int number) {
         // given
-        final Car car = new Car(() -> 2);
+        final Car car = new Car(() -> number);
 
         // when
         car.move();
