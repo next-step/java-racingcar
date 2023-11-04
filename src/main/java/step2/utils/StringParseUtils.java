e4e511b51cfa4ef15890d9e18420c014e67e994a
jane096 <jes7077@naver.com>
2023-11-04 22:05:06 +0900

    PositiveNumber 클래스 생성, isNullOrEmpty() primitive로 리턴타입 변경

--- src/main/java/step2/utils/StringParseUtils.java
+++ src/main/java/step2/utils/StringParseUtils.java
@@ -1,5 +1,7 @@
 package step2.utils;
 
+import step2.PositiveNumber;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,24 +10,14 @@ import static step2.constants.RegexConstants.EXTRACT_NUMBER_ONLY;
 
 public class StringParseUtils {
 
-    public static Boolean isNullOrEmpty(String target) {
+    public static boolean isNullOrEmpty(String target) {
         return target == null || target.isEmpty();
-    }
-
-    public static int positiveNumber(String number) {
-        int num = Integer.parseInt(number);
-
-        if (num < 0) {
-            throw new RuntimeException("음수는 더할 수 없습니다.");
-        }
-
-        return num;
     }
 
     public static List<Integer> parseString(String numbers) {
         return Arrays.stream(numbers.split(EXTRACT_NUMBER_ONLY))
                 .filter(e -> !isNullOrEmpty(e))
-                .map(StringParseUtils::positiveNumber)
+                .map(PositiveNumber::get)
                 .collect(Collectors.toList());
     }
 }
