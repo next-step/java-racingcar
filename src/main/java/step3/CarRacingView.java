751bb67f4eff3655c8020710369b5f0681508d05
jonghunbaek <qor6028@naver.com>
2023-11-04 19:29:05 +0900

    feat : 자동차 경주의 조건 입력, 결과 출력을 담당하는 뷰 클래스 생성

---
+++ src/main/java/step3/CarRacingView.java
@@ -0,0 +1,22 @@
+package step3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CarRacingView {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        while (true) {
+            System.out.println("자동차 대수는 몇 대 인가요?");
+            int numOfCar = Integer.parseInt(br.readLine());
+
+            System.out.println("시도할 횟수는 몇 회 인가요?");
+            int numOfTry = Integer.parseInt(br.readLine());
+
+            System.out.println("실행 결과");
+        }
+    }
+}
