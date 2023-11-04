f764d2317c6d00bdfa43efc2484fa3a180303ad8
kyunghyun-park <qqqq_o@naver.com>
2023-11-04 21:48:33 +0900

    feat: 사용자의 자동차 대수, 시도 회수 입력 추가

---
+++ src/main/java/carracing/InputView.java
@@ -0,0 +1,15 @@
+package carracing;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static int inputCarCount() {
+        return sc.nextInt();
+    }
+
+    public static int inputTryCount() {
+        return sc.nextInt();
+    }
+}
---
+++ src/main/java/carracing/Racing.java
@@ -0,0 +1,11 @@
+package carracing;
+
+public class Racing {
+    public static void main(String[] args) {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carCount = InputView.inputCarCount();
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int tryCount = InputView.inputTryCount();
+    }
+}
