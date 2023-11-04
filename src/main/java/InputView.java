45d4a68cc7ebbc76371c8c28bb32df85a7ee9365
sunggyupaik <melon9751@naver.com>
2023-11-04 10:22:39 +0900

    [refactor] 입력과 출력을 리팩토링 하라
    입력과 출력을 별도의 클래스에서 작업하도록 리팩토링합니다

---
+++ src/main/java/InputView.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+	public static List<Car> inputCars(Scanner scanner) {
+		List<Car> cars = new ArrayList<>();
+
+		System.out.println("자동차 대수는 몇 대 인가요?");
+		int carsNumber = scanner.nextInt();
+		for(int i=0; i<carsNumber; i++) {
+			Car car = new Car();
+			cars.add(car);
+		}
+
+		return cars;
+	}
+
+	public static int tryingNumber(Scanner scanner) {
+		System.out.println("시도할 회수는 몇 회 인가요?");
+		int tryingNumber = scanner.nextInt();
+
+		return tryingNumber;
+	}
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,23 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		List<Car> cars = new ArrayList<>();
+		List<Car> cars = InputView.inputCars(scanner);
+		int tryingTimes = InputView.tryingNumber(scanner);
 
-		System.out.println("자동차 대수는 몇 대 인가요?");
-		int carsNumber = scanner.nextInt();
-		for(int i=0; i<carsNumber; i++) {
-			Car car = new Car();
-			cars.add(car);
-		}
-
-		System.out.println("시도할 회수는 몇 회 인가요?");
-		int tryingTimes = scanner.nextInt();
-
-		System.out.println("실행 결과");
+		ResultView.resultPhrase();
 		for(int i=0; i<tryingTimes; i++) {
-			for(Car car : cars) {
-				car.tryMoving(new RandomRange(0, 9));
-				car.printMovingResult();
-			}
-			System.out.println("");
+			racing(cars);
 		}
+	}
+
+	public static void racing(List<Car> cars) {
+		for(Car car : cars) {
+			car.tryMoving(new RandomRange(0, 9));
+			ResultView.printMovingResult(car.movingDistance());
+		}
+		ResultView.empty();
 	}
 }
