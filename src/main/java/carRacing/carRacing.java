7628488c33f69466d05bb3125a8a663b40a6f39d
chocheolhee <whcjfgml92@naver.com>
2023-11-04 21:37:58 +0900

    feat: 사용자에게 입력 받을 수 있다

--- src/main/java/carRacing/carRacing.java
+++ src/main/java/carRacing/carRacing.java
@@ -1,2 +1,19 @@
-package carRacing;public class carRacing {
+package carRacing;
+
+import java.util.Scanner;
+
+public class carRacing {
+    public static void main(String[] args) {
+
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        scanner();
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        scanner();
+    }
+
+    private static void scanner() {
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+    }
 }
