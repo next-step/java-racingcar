064e7ab34cc400b8c97670f65172ceda5c436a33
kyunghyun-park <qqqq_o@naver.com>
2023-11-05 00:37:59 +0900

    fix: 입력 시 NoSuchElementException오류로 인해 수정

--- src/main/java/carracing/ui/InputView.java
+++ src/main/java/carracing/ui/InputView.java
@@ -3,17 +3,14 @@ package carracing.ui;
 import java.util.Scanner;
 
 public class InputView {
+    private static Scanner sc = new Scanner(System.in);
     public static int inputCarCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        try (Scanner sc = new Scanner(System.in)){
-            return sc.nextInt();
-        }
+        return sc.nextInt();
     }
 
     public static int inputTryCount() {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        try (Scanner sc = new Scanner(System.in)){
-            return sc.nextInt();
-        }
+        return sc.nextInt();
     }
 }
