a03ad5381c5e522930a8fea88aac8fc901c5caf8
yumble <hnjn1101@gmail.com>
2023-11-04 13:08:18 +0900

    fix: delimiter 상수 리스트 사용 -> delimiter 직접 반환

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,11 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
     private static final String DELIMITER_JOIN = "|";
-    private static List<String> delimiters = new ArrayList<>(Arrays.asList(",", ":"));
+    private static final String DEFAULT_DELIMITER = ",|:";
     private static final int NULL_VALUE = 0;
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
@@ -17,33 +14,23 @@ public class Calculator {
             return NULL_VALUE;
         }
 
-        return sum(toInts(split(extractText(text))));
+        return sum(toInts(splitText(text)));
     }
 
     private static boolean isBlank(String text) {
         return text == null || text.isBlank();
     }
 
-    private static String extractText(String text) {
+    private static String[] splitText(String text) {
         Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
         if (m.find()) {
-            addDelimiters(m);
-            return m.group(2);
+            return m.group(2).split(makeDelimiters(m.group(1)));
         }
-        return text;
+        return text.split(DEFAULT_DELIMITER);
     }
 
-    private static void addDelimiters(Matcher m) {
-        String customDelimiter = m.group(1);
-        delimiters.add(customDelimiter);
-    }
-
-    private static String[] split(String text) {
-        return text.split(makeDelimiters(delimiters));
-    }
-
-    private static String makeDelimiters(List<String> delimiters) {
-        return String.join(DELIMITER_JOIN, delimiters);
+    private static String makeDelimiters(String customerDelimiter) {
+        return String.join(DELIMITER_JOIN, DEFAULT_DELIMITER, customerDelimiter);
     }
 
     private static int[] toInts(String[] values) {
