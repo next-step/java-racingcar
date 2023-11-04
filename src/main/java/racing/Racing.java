3b31a6794a568c606499ba9f50e2ac28905c4707
chunghyunleeme <chunghyun.dev@gmail.com>
2023-11-04 17:11:00 +0900

    chore: participatingCars 변수 privat 으로 변경

--- src/main/java/racing/Racing.java
+++ src/main/java/racing/Racing.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Racing {
-    ParticipatingCars participatingCars;
+    private ParticipatingCars participatingCars;
 
     public Racing() {
         this.participatingCars = new ParticipatingCars();
