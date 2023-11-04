dcfd00afc68c988c90bf566e9ca8991f054183db
jane096 <jes7077@naver.com>
2023-11-04 22:06:59 +0900

    중복된 책임을 가진 CalculatorUtils 제거, StringAddCalculator로 로직이동

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,10 +1,15 @@
 package step2;
 
-import static step2.utils.CalculatorUtils.sumStringValues;
+import static step2.utils.StringParseUtils.isNullOrEmpty;
+import static step2.utils.StringParseUtils.parseString;
 
 public class StringAddCalculator {
 
-    public static int splitAndSum(String target) {
-        return sumStringValues(target);
+    public static int splitAndSum(String numbers) {
+        if (isNullOrEmpty(numbers)) {
+            return 0;
+        }
+
+        return parseString(numbers).stream().reduce(0, Integer::sum);
     }
 }
