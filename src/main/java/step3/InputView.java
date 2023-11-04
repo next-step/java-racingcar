4ef9bfedeac6451899e147c862a5526019974602
ch200203 <ch200205@naver.com>
2023-11-05 00:09:36 +0900

    feat : 시도할 횟수을 입력받는 기능 추가

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String JOIN_RACING_CAR_INPUT_MESSAGE = "레이싱에 참여할 자동차는 몇대 인가요?";
+    private static final String RACING_TRY_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇회 인가요?";
     private static Scanner scan = new Scanner(System.in);
 
     public int inputRacingCarCount() {
         System.out.println(JOIN_RACING_CAR_INPUT_MESSAGE);
-        int racingCarCount = scan.nextInt();
-        validateInputRacingCarCount(racingCarCount);
-        return racingCarCount;
+        return validateInputNumber(scan.nextInt());
     }
 
-    private void validateInputRacingCarCount(int racingCarCount) {
-        if (racingCarCount <= 0) {
+    public int inputTryCount() {
+        System.out.println(RACING_TRY_COUNT_INPUT_MESSAGE);
+        return validateInputNumber(scan.nextInt());
+    }
+
+    private int validateInputNumber(int inputNumber) {
+        if (inputNumber <= 0) {
             throw new IllegalArgumentException("입력하신 값이 올바르지 않습니다. 다시 확인해주세요");
         }
+        return inputNumber;
     }
 
 }
--- src/main/java/step3/RacingController.java
+++ src/main/java/step3/RacingController.java
@@ -1,9 +1,14 @@
 package step3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingController {
 
     public static void run() {
         InputView inputView = new InputView();
         inputView.inputRacingCarCount();
+        inputView.inputTryCount();
     }
+
 }
