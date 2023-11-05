eff9f5e104b92b785915de7e973248786f1427d7
qwe5507 <apvmffkdls@gmail.com>
2023-11-06 00:15:22 +0900

    refactor: String 상수화

--- src/main/java/racewinner/CarRacing.java
+++ src/main/java/racewinner/CarRacing.java
@@ -4,6 +4,9 @@ import racewinner.expteion.InvalidDelimiterException;
 import racewinner.expteion.InputEmptyException;
 
 public class CarRacing {
+    private final static String INPUT_EMPTY_MSG = "빈 값을 입력 할 수 없습니다.";
+    private final static String INVALID_DELIMITER_MSG = "','구분자 외의 다른 구분자를 사용 할 수 없습니다.";
+    private final static String SPLIT_DELIMITER = ",";
     private final CarList carList = new CarList();
     public CarRacing(String inputCarListName) {
         validationInput(inputCarListName);
@@ -16,22 +19,22 @@ public class CarRacing {
     }
 
     private static String[] splitInputText(final String inputCarListName) {
-        String[] carNameList = inputCarListName.split(",");
+        String[] carNameList = inputCarListName.split(SPLIT_DELIMITER);
         return carNameList;
     }
 
     private void validationInput(final String inputCarListName) {
         if (isInputNullOrEmpty(inputCarListName)) {
-            throw new InputEmptyException("빈 값을 입력 할 수 없습니다.");
+            throw new InputEmptyException(INPUT_EMPTY_MSG);
         }
 
         if (isInputCommaNotContain(inputCarListName)) {
-            throw new InvalidDelimiterException("','구분자 외의 다른 구분자를 사용 할 수 없습니다.");
+            throw new InvalidDelimiterException(INVALID_DELIMITER_MSG);
         }
     }
 
     private boolean isInputCommaNotContain(final String inputCarListName) {
-        return !inputCarListName.contains(",");
+        return !inputCarListName.contains(SPLIT_DELIMITER);
     }
 
     private boolean isInputNullOrEmpty(final String inputCarListName) {
