d4824f95825ed60d0b15d522069f947ee6a97e3e
chocheolhee <whcjfgml92@naver.com>
2023-11-05 20:59:47 +0900

    feat: 요구사항 기능 테스트 코드 작성

--- src/main/java/carRacing/Car.java
+++ src/main/java/carRacing/Car.java
@@ -4,12 +4,10 @@ public class Car {
 
     private static final int STANDARD_MOVE_VALUE = 4;
     private static final int START_POSITION = 1;
-    private final RandomCreate randomCreate;
     private int position;
 
     public Car() {
         this.position = START_POSITION;
-        this.randomCreate = new RandomCreate();
     }
 
     public void move() {
@@ -18,8 +16,17 @@ public class Car {
         }
     }
 
+    public void move(int value) {
+        if (value >= 4) {
+            position += 1;
+        }
+    }
+
     private boolean checkStandardMove() {
-        return randomCreate.randomNumber() >= STANDARD_MOVE_VALUE;
+        RandomCreate randomCreate = new RandomCreate();
+        int randomNumber = randomCreate.randomNumber();
+
+        return randomNumber >= STANDARD_MOVE_VALUE;
     }
 
     public int getPosition() {
--- src/test/java/carRacing/CarRacingTest.java
+++ src/test/java/carRacing/CarRacingTest.java
@@ -9,9 +9,33 @@ import static org.assertj.core.api.Assertions.*;
 public class CarRacingTest {
 
     @Test
+    @DisplayName("사용자가 전진하는 조건의 수(4)를 입력하면 자동차는 전진한다.")
+    void game() {
+        // given
+        Car car = new Car();
+        RandomCreate randomCreate = new RandomCreate(4);
+
+        // when
+        car.move(randomCreate.randomNumber());
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(2);
+    }
+
+    @Test
     @DisplayName("사용자가 자동차 게임 실행 후 음수를 입력하면 RuntimeException이 발생한다.")
     public void negative_validate() throws Exception {
         assertThatThrownBy(() -> new RacingGame(-1,-4))
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("0부터 9 사이의 random 숫자 생성")
+    public void createRandomNumber() {
+        RandomCreate randomCreate = new RandomCreate();
+
+        int randomNumber = randomCreate.randomNumber();
+
+        assertThat(randomNumber).isLessThan(10);
     }
 }
