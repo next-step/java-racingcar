155e5d0cf86a746f1013e655f972fcac8193cc7c
sunggyupaik <melon9751@naver.com>
2023-11-06 01:04:25 +0900

    [feat] 자동차 경주 우승자 확인을 구현하라
    자동차 경주 우승자를 구현합니다. 우승자는 1명 이상 일 수 있습니다

--- src/main/java/carracing/Main.java
+++ src/main/java/carracing/Main.java
@@ -4,8 +4,12 @@ import carracing.car.Car;
 import carracing.car.MovingStrategy;
 import carracing.car.RandomRange;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 	public static void main(String[] args) {
@@ -24,7 +28,22 @@ public class Main {
 		for (Car car : cars) {
 			car.move(movingStrategy);
 			ResultView.printMovingResult(car);
+			List<Car> winners = winner(cars);
 		}
 		ResultView.empty();
+	}
+
+	private static List<Car> winner(List<Car> cars) {
+		new ArrayList<>();
+		int maxMovingDistance = cars.stream()
+				.map(Car::movingDistance)
+				.max(Comparator.comparingInt(movingDistance -> movingDistance))
+				.orElseThrow(NoSuchElementException::new);
+
+		List<Car> winners = cars.stream()
+				.filter(car -> car.movingDistance() == maxMovingDistance)
+				.collect(Collectors.toList());
+
+		return winners;
 	}
 }
