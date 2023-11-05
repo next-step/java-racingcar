bc3394f46ec65892525546ed7f1cfe2a2ccfc733
HeeDoo Logan Jeong <78134917+jhd7130@users.noreply.github.com>
2023-11-05 22:03:14 +0900

    리펙토링 메서드 분리

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,34 +1,45 @@
 public class StringCalculator {
 
     private static final String DEFAULT_DELIMETER = ",:";
-    private static final String CUSTOME_DELIMETER_START_POINT = "//";
     private static final String CUSTOME_DELIMETER_END_POINT = "\n";
-    public static int cal(String o) {
-        if (o == null || o.isBlank()) {
+    public static int cal(String targetOfSplit) {
+        if (StringCalculator.isNullOrEmpty(targetOfSplit)) {
             return 0;
         }
+        String[] targetOfSum = StringCalculator.split(targetOfSplit);
+        return StringCalculator.sum(targetOfSum);
+    }
 
+    private static boolean isNullOrEmpty(String targetOfSplit) {
+        return targetOfSplit == null || targetOfSplit.isBlank();
+    }
+
+    private static String[] split(String targetOfSplit) {
         String finalDelimeter = DEFAULT_DELIMETER;
 
-        if (o.contains(CUSTOME_DELIMETER_END_POINT)) {
-            int customDelimeterEndPointIndex = o.indexOf(CUSTOME_DELIMETER_END_POINT);
-            char c = o.charAt(customDelimeterEndPointIndex - 1);
+        if (targetOfSplit.contains(CUSTOME_DELIMETER_END_POINT)) {
+            int customDelimeterEndPointIndex = targetOfSplit.indexOf(CUSTOME_DELIMETER_END_POINT);
+            char customDelimeter = targetOfSplit.charAt(customDelimeterEndPointIndex - 1);
+            finalDelimeter = DEFAULT_DELIMETER + customDelimeter;
 
-            o = o.substring(customDelimeterEndPointIndex + 1);
-
-            finalDelimeter = DEFAULT_DELIMETER + c;
+            targetOfSplit = targetOfSplit.substring(customDelimeterEndPointIndex + 1);
         }
+        return targetOfSplit.split("["+ finalDelimeter +"]");
+    }
 
-
-        String[] split = o.split("["+ finalDelimeter +"]");
+    private static int sum(String[] split) {
         int sum = 0;
         for (String a : split) {
-            int i = Integer.valueOf(a);
-            if (i < 0) {
-                throw new RuntimeException();
-            }
+            int i = Integer.parseInt(a);
+            checkNegativeNumber(i);
             sum += i;
         }
         return sum;
+    }
+
+    private static void checkNegativeNumber(int i) {
+        if (i < 0) {
+            throw new RuntimeException();
+        }
     }
 }
