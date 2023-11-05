fc93791fa9047eb7dbe34ee314fb14393a29ae8e
chocheolhee <whcjfgml92@naver.com>
2023-11-05 15:31:22 +0900

    refactor: 기존 입력 받는 클래스 ui 클래스로 분리

--- src/main/java/carRacing/carRacing.java
+++ src/main/java/carRacing/carRacing.java
@@ -1,14 +1,10 @@
 package carRacing;
 
+import carRacing.ui.InputView;
+
 public class carRacing {
     public static void main(String[] args) {
-
-        InputScanner inputScanner = new InputScanner();
-        inputScanner.inputCarCount();
-        inputScanner.inputTrialCount();
-
-        RacingGame game = new RacingGame(inputScanner.carCount(),inputScanner.trialCount());
+        RacingGame game = new RacingGame(InputView.inputCarCount(), InputView.inputTrialCount());
         game.gameStart();
     }
-
 }
