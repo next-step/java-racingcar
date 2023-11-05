ea235026958eb8201e66e4863a38b2c48fa2a4fd
이창섭 <ventulus95@gmail.com>
2023-11-05 21:58:40 +0900

    [:recycle:]refact: 해당하는 테스트 리펙토링

--- src/test/java/ch3/CarRacingTest.java
+++ src/test/java/ch3/CarRacingTest.java
@@ -2,18 +2,23 @@ package ch3;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CarRacingTest {
 
+    InputView inputView = new InputView();
+    ResultView resultView = new ResultView();
+    CarRacing carRacing = new CarRacing(inputView, resultView);
+
     @Test
     void 자동차_경주는_input_view_result_view를_만들어진다() {
-        InputView inputView = new InputView();
-        ResultView resultView = new ResultView();
-        CarRacing carRacing = new CarRacing(inputView, resultView);
-        assertEquals(inputView, carRacing.getInputView());
-        assertEquals(resultView, carRacing.getResultView());
+        assertAll(() -> {
+            assertEquals(inputView, carRacing.getInputView());
+            assertEquals(resultView, carRacing.getResultView());
+        });
     }
+
 
 }
