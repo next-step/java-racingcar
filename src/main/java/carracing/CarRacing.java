2d6cfe389441bc89e1576af33854a8bcfc6be3b3
jeseungkim <qazwsx3745@naver.com>
2023-11-05 15:56:42 +0900

    docs: Update 요구사항 on README.md

--- src/main/java/carracing/CarRacing.java
+++ src/main/java/carracing/CarRacing.java
@@ -2,10 +2,23 @@ package carracing;
 
 import java.util.Scanner;
 
+
 public class CarRacing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    Scanner scanner = new Scanner(System.in);
-    String value = scanner.nextLine();
-    int number = scanner.nextInt();
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carNumber = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int tryNumber = scanner.nextInt();
 
+        System.out.println("실행 결과");
+
+
+
+    }
+
+
+    public static void car() {
+    }
 }
