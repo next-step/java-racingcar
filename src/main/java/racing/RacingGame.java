cec759f3214dc1d70679d6e56bacb9c19a470ac1
jeounpar <cdex6531@ajou.ac.kr>
2023-11-05 20:08:27 +0900

    feat : 자동차 게임 기능 구현 완료

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -1,6 +1,6 @@
 package racing;
 
-import java.util.Random;
+import java.util.*;
 
 public class RacingGame {
 
@@ -17,4 +17,38 @@ public class RacingGame {
 		}
 		return car;
 	}
+
+	public static List<Integer> makeRandomList(int cars) {
+		List<Integer> randomList = new ArrayList<>();
+		for (int i = 0; i < cars; i++) {
+			randomList.add(random());
+		}
+		return randomList;
+	}
+
+	public static void moveAll(Map<String, String> carMap, int cars, List<Integer> randomList) {
+		for (int i = 0; i < cars; i++) {
+			carMap.put(Integer.toString(i), move(carMap.get(Integer.toString(i)), randomList.get(i)));
+		}
+	}
+
+	public static Map<String, String> init(int cars) {
+		Map<String, String> carMap = new HashMap<>();
+		for (int i = 0; i < cars; i++) {
+			carMap.put(Integer.toString(i), "-");
+		}
+		return carMap;
+	}
+
+	public static Map<String, String> start(int cars, int attempts) {
+		Map<String, String> carMap = init(cars);
+
+		while (attempts-- > 0) {
+			List<Integer> randomList = makeRandomList(cars);
+			moveAll(carMap, cars, randomList);
+		}
+
+		return carMap;
+	}
+
 }
--- src/test/java/racing/RacingGameTest.java
+++ src/test/java/racing/RacingGameTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
@@ -23,4 +28,60 @@ public class RacingGameTest {
 	void dont_move(Integer random, String expected) {
 		assertThat(RacingGame.move("-", random)).isEqualTo(expected);
 	}
+
+	@Test
+	@DisplayName("3대의 자동차가 한번에 모두 전진을 시도할 때 필요한 random ArrayList를 만든다")
+	void make_random_list() {
+		int cars = 3;
+
+		List<Integer> randomList = RacingGame.makeRandomList(cars);
+
+		for (int i = 0; i < cars; i++) {
+			assertThat(randomList.get(i)).isGreaterThanOrEqualTo(0)
+					.isLessThanOrEqualTo(9);
+		}
+	}
+
+	@Test
+	@DisplayName("자동차의 초기 상태는 모두 \"-\" 이다")
+	void init() {
+		int cars = 3;
+		Map<String, String> carMap = RacingGame.init(cars);
+
+		for (String car : carMap.values()) {
+			assertThat(car).isEqualTo("-");
+		}
+	}
+
+	@Test
+	@DisplayName("3대의 자동차가 한번에 모두 전진한다")
+	void move_all_cars() {
+		int cars = 3;
+		Map<String, String> carList = RacingGame.init(cars);
+		List<Integer> randomList = new ArrayList<>();
+		for (int i = 0; i < cars; i++) {
+			carList.put(Integer.toString(i), "-");
+			randomList.add(5);
+		}
+
+
+		RacingGame.moveAll(carList, cars, randomList);
+
+		for (int i = 0; i < cars; i++) {
+			assertThat(carList.get(Integer.toString(i))).isEqualTo("--");
+		}
+	}
+
+//	@Test
+//	@DisplayName("3대의 자동차를 5번 동안 전진한다. 이 때 5번 모두 random값이 4이상이 나와서 전진한다.")
+//	void start() {
+//		int cars = 3;
+//		int attempts = 5;
+//
+//		Map<String, String> result = RacingGame.start(cars, attempts);
+//
+//		for (int i = 0; i < cars; i++) {
+//			assertThat(result.get(Integer.toString(i))).isEqualTo("------");
+//		}
+//	}
 }
