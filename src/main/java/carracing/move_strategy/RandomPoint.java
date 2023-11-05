491d2f39e55e3a6eef2cdff435be4ab721c026b5
hoon25 <cchoon95@hanmail.net>
2023-11-05 23:01:10 +0900

    refactor(Random): ThreadLocalRandom으로 변경

--- src/main/java/carracing/move_strategy/RandomPoint.java
+++ src/main/java/carracing/move_strategy/RandomPoint.java
@@ -1,9 +1,9 @@
 package carracing.move_strategy;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomPoint {
-    public static final Random random = new Random();
+    public static final ThreadLocalRandom random = ThreadLocalRandom.current();
 
     public static int getPoint() {
         return random.nextInt(10);
