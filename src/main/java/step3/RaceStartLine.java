9f3ede1984598479daa02519f07c90e316613393
jane096 <jes7077@naver.com>
2023-11-06 00:54:32 +0900

    PlayingRacingCar -> RaceStartLine으로 클래스명 변경

--- src/main/java/step3/RaceStartLine.java
+++ src/main/java/step3/RaceStartLine.java
@@ -2,7 +2,7 @@ package step3;
 
 import java.util.Scanner;
 
-public class PlayRacingCar {
+public class RaceStartLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
