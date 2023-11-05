e407ff8ad3f9610726c9633302ba31804ced9ce2
g0rani <pjw0405@naver.com>
2023-11-05 20:49:51 +0900

    refactor: 예외처리에 대한 메세지 변경
    - 예외명과 관련이 없는 메세지 수정

--- src/main/java/racinggame/domain/exception/InputIsBlankException.java
+++ src/main/java/racinggame/domain/exception/InputIsBlankException.java
@@ -2,7 +2,7 @@ package racinggame.domain.exception;
 
 public class InputIsBlankException extends RuntimeException {
 
-    public static final String DEFAULT_MESSAGE = "이름은 5자를 초과 할 수 없습니다.";
+    public static final String DEFAULT_MESSAGE = "이름은 공백 혹은 null을 입력 할 수 없습니다.";
 
     public InputIsBlankException() {
         super(DEFAULT_MESSAGE);
