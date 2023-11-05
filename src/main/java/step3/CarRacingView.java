e21f7ac85d0485c477ec70a05452283910fa38c0
jonghunbaek <qor6028@naver.com>
2023-11-05 21:27:45 +0900

    refactor : Drawing객체를 생성해 자동차의 움직임을 콘솔창에 그리도록 변경

--- src/main/java/step3/CarRacingView.java
+++ src/main/java/step3/CarRacingView.java
@@ -9,15 +9,37 @@ public class CarRacingView {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         CarRacing carRacing = new CarRacing();
+        Drawing drawing = new Drawing();
 
-        while (true) {
-            System.out.println("자동차 대수는 몇 대 인가요?");
-            int numOfCar = Integer.parseInt(br.readLine());
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int numOfCar = Integer.parseInt(br.readLine());
+        carRacing.createCarsOf(numOfCar);
 
-            System.out.println("시도할 횟수는 몇 회 인가요?");
-            int numOfTry = Integer.parseInt(br.readLine());
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        int numOfTry = Integer.parseInt(br.readLine());
 
-            System.out.println("실행 결과");
+        System.out.println("실행 결과");
+
+        for (int i=0; i<numOfTry; i++) {
+            carRacing.moveCar();
+
+            int[] positions = findPositions(carRacing);
+            drawing.drawCarRacing(positions, numOfCar);
+
+            System.out.println();
         }
     }
+
+    private static int[] findPositions(CarRacing carRacing) {
+        int numOfCar = carRacing.getCars().getNumOfCars();
+
+        int[] positions = new int[numOfCar];
+        for (int i=0; i<numOfCar; i++) {
+            positions[i] = carRacing.getCars().findCarBy(i).getCurrentPosition();
+        }
+
+        return positions;
+    }
+
+
 }
