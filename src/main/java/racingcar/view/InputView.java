9337a4b967c4f353eb03acc1295c40c4faeee65e
엄태권 <akfls221@gmail.com>
2023-11-06 03:08:19 +0900

    fix : private 생성자로 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,9 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+    private InputView() {
+    }
+
     public static int getCarCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         return getScannerInput();
