05d7b4c08ae7bb93b5745c41c080cabb5b670281
HyunggiPark <phg9587@naver.com>
2023-11-05 16:27:01 +0900

    feat: 사용자로부터 자동차 대수 및 시도할 횟수를 입력받는다.

--- src/main/java/step3/main/Main.java
+++ src/main/java/step3/main/Main.java
@@ -1,11 +1,15 @@
 package step3.main;
 
+import step3.input.InputView;
+import step3.input.UserInput;
+
 public class Main {
     public static void main(String[] args) {
         //inputView
-
-        //게임 실행
+        final UserInput userInput = InputView.input();
         
+        //게임 실행
+
         //resultView
     }
 }
