3c9ff86b9aa9a568d187c8f3e72f2953ffec5082
beyondorder <youofsea@gmail.com>
2023-11-05 13:49:14 +0900

    Player 구현

--- src/main/java/racingCar/Player.java
+++ src/main/java/racingCar/Player.java
@@ -1,2 +1,9 @@
-package racingCar;public class Player {
+package racingCar;
+
+public class Player {
+
+    public static void main(String[] args) {
+        RacingCarGame game = new RacingCarGame();
+        game.play();
+    }
 }
