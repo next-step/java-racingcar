033b6e65b406816586cc5aad2e0f0a2d85cc6d39
beyondorder <youofsea@gmail.com>
2023-11-05 13:48:49 +0900

    출력부 구현

--- src/main/java/racingCar/ResultView.java
+++ src/main/java/racingCar/ResultView.java
@@ -1,2 +1,23 @@
-package racingCar;public class ResultView {
+package racingCar;
+
+import java.util.List;
+
+public class ResultView {
+
+    private String resultMessage = "실행 결과";
+    private final List<String> result;
+
+    ResultView(List<String> result){
+        this.result = result;
+    }
+
+    ResultView(List<String> result, String resultMessage){
+        this.result = result;
+        this.resultMessage = resultMessage;
+    }
+    public void printResult(){
+        System.out.println(this.resultMessage);
+        result.forEach(System.out::println);
+    }
+
 }
