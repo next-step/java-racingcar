38cd821c235d2bac6b372a92159219dde890d9c8
beyondorder <youofsea@gmail.com>
2023-11-05 13:48:36 +0900

    RacingCarGame 구현

--- src/main/java/racingCar/RacingCarGame.java
+++ src/main/java/racingCar/RacingCarGame.java
@@ -1,16 +1,42 @@
 package racingCar;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class RacingCar {
+public class RacingCarGame {
 
-    static final private int PIVOT_NUMBER_TO_FORWARD = 4;
-    static final private int CLOSED_END_NUMBER_TO_GET_RANDOM_NUMBER_FROM_ZERO = 10;
-    private final static String CAR_COUNT_QUESTION = "자동차 대수는 몇 대 인가요?";
-    private final static String GAME_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
-    void racing(){
-        int carCount = new InputView(CAR_COUNT_QUESTION).input();
-        int gameCount = new InputView(GAME_COUNT_QUESTION).input();
+    private static final int PIVOT_NUMBER_TO_FORWARD = 4;
+    private static final int CLOSED_END_NUMBER_TO_GET_RANDOM_NUMBER_FROM_ZERO = 10;
+
+    private static final String CAR_COUNT_QUESTION = "자동차 대수는 몇 대 인가요?";
+    private static final String GAME_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
+
+    private int CAR_COUNT;
+    private int GAME_COUNT;
+    public void play(){
+        CAR_COUNT = new InputView(CAR_COUNT_QUESTION).intInput();
+        GAME_COUNT = new InputView(GAME_COUNT_QUESTION).intInput();
+
+        List<Car> carList = new ArrayList<>();
+        for(int i=0; i<CAR_COUNT; i++){
+            carList.add(new Car());
+        }
+
+        for(int i=0; i<GAME_COUNT; i++){
+            for(Car car: carList){
+                if(isSatisfiedForward()){
+                    car.forward();
+                }
+            }
+        }
+
+        List<String> distanceList = new ArrayList<>();
+        for(Car car: carList){
+            distanceList.add(car.distance());
+        }
+        new ResultView(distanceList).printResult();
+
     }
 
     private boolean isSatisfiedForward(){
