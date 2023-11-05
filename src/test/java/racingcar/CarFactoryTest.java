ed4d3db0cefe63b3a13643e854c39d1407e37481
KimGwangHoon <khkimykk@naver.com>
2023-11-05 15:19:19 +0900

    test: 경기차량 생성 factory 테스트

--- src/test/java/racingcar/CarFactoryTest.java
+++ src/test/java/racingcar/CarFactoryTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 import racingcar.domain.CarFactory;
@@ -8,36 +9,55 @@ import racingcar.domain.CarFactory;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarFactoryTest {
     private CarFactory carFactory;
+    private final String DEFAULT_CAR_NAME = "pobi,crong,honux";
 
     public void setUp() {
         this.carFactory = new CarFactory();
     }
 
-    @DisplayName("입력2만큼 자동차만들기")
+    @DisplayName("차량이름으로 차량생성")
     @Test
-    public void 입력수만큼차량생성() {
+    public void 차량이름으로_차량생성() {
         //given
         setUp();
-        int create = 2;
         //when
-        carFactory.participants(create);
+        carFactory.participants(DEFAULT_CAR_NAME);
         List<Car> cars = carFactory.carsInfo();
         //then
-        assertThat(cars.size()).isEqualTo(create);
+        assertThat(cars.size()).isEqualTo(3);
     }
 
-    @DisplayName("입력0으로 자동차생성된 자동차 없음")
-    @Test
-    public void 자동차생성실패() {
-        //given
-        setUp();
-        int create = 0;
-        //when
-        carFactory.participants(create);
-        List<Car> cars = carFactory.carsInfo();
-        assertThat(cars.size()).isEqualTo(create);
+    @DisplayName("차량 생성 실패")
+    @Nested
+    class CreateCarsFaild {
+        @DisplayName("차량 이름이 5글자 이상일때")
+        @Test
+        public void 차량_이름이_5글자_이상일때() {
+            //given
+            setUp();
+            String carName = "pobid";
+            //when
+            //then
+            assertThatThrownBy(() -> carFactory.participants(carName))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("자동차 이름은 5자를 초과 할 수 없습니다.");
+        }
+
+        @DisplayName("차량 이름이 0글자 일때")
+        @Test
+        public void 차량_이름이_0글자_일때() {
+            //given
+            setUp();
+            String carName = "pobi,,honux";
+            //when
+            //then
+            assertThatThrownBy(() -> carFactory.participants(carName))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("자동차 이름이 없는 경우가 있습니다.");
+        }
     }
 }
