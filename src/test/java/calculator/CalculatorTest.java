c58b5e64437c125efa1a9af3cffbe744a55948f6
DongGyu Kim <kimss7334@naver.com>
2023-11-05 10:57:49 +0900

    Step2 (#4898)
    * fix: step1 리뷰 사항 반영, split 학습 테스트 추가
    
    * feat: step2 요구사항 구현
    
    * fix: 상수값 수정
    
    * fix: #4898 step2 PR 리뷰사항 수정

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,65 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static calculator.AddCalculator.getSum;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class CalculatorTest {
+    @DisplayName("null 또는 빈 문자열 일시 0을 반환한다")
+    @Test
+    void splitAndSum_null_또는_빈문자() {
+        int result = getSum(null);
+        assertThat(result).isZero();
+
+        result = getSum("");
+        assertThat(result).isZero();
+    }
+
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    @Test
+    void splitAndSum_숫자하나() {
+        int result = getSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @Test
+    void splitAndSum_쉼표구분자() {
+        int result = getSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @Test
+    void splitAndSum_쉼표_또는_콜론_구분자() {
+        int result = getSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @Test
+    void splitAndSum_custom_구분자() {
+        int result = getSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("//와 \\n 문자 만 존재하면 0을 반환한다.")
+    @Test
+    public void custom_구분자_only() {
+        int result = getSum("//;\n");
+        assertThat(result).isZero();
+    }
+
+
+    @DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생해야 한다.")
+    @Test
+    void splitAndSum_negative() {
+        assertThatThrownBy(() -> getSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,20 +1,18 @@
 package study;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringTest {
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 분리되고, \"1\"만 split 했을 때 1을 포함하는 배열을 반환한다.")
     @Test
     void split_req1() {
-        /*
-         * "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
-         * "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
-         */
-
         // given
         String givenString1 = "1,2";
         String givenString2 = "1";
@@ -28,12 +26,9 @@ class StringTest {
         assertThat(splitStringArray2).containsExactly("1");
     }
 
+    @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환한다.")
     @Test
-    void  split_req2() {
-        /*
-         * "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
-         */
-
+    void split_req2() {
         // given
         String givenString = "(1,2)";
 
@@ -44,15 +39,9 @@ class StringTest {
         assertThat(result).isEqualTo("1,2");
     }
 
-    @Test
     @DisplayName("String.charAt() 은 특정 위치의 문자를 가져오고, 위치 값 범위를 벗어나면 StringIndexOutOfBoundsException을 발생시킨다.")
-    void  split_req3() {
-        /*
-         * "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
-         * String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
-         * JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
-         */
-
+    @Test
+    void split_req3() {
         // given
         String givenString = "abc";
         int testIndex = givenString.length();
@@ -65,7 +54,59 @@ class StringTest {
         assertThatThrownBy(() -> givenString.charAt(testIndex))
                 .isInstanceOf(StringIndexOutOfBoundsException.class)
                 .hasMessageContaining("String index out of range");
-//                .hasMessage()
-
     }
+
+    @DisplayName(",를 포함하지 않아도 문자열이 포함된 String[]을 반환한다.")
+    @Test
+    void splitTest() {
+        assertThat("1".split(","))
+                .hasSize(1)
+                .containsExactly("1");
+    }
+
+    @DisplayName(", 또는 : 으로 문자열을 split 한다")
+    @Test
+    void splitDelimiterTest() {
+        assertThat("1,2:3".split("[,:]"))
+                .hasSize(3)
+                .containsExactly("1", "2", "3");
+    }
+
+    @DisplayName("//와 \\n 사이 문자열을 추출한다")
+    @Test
+    void delimiterParseTest() {
+        // given
+        String testString = "//;\\n123";
+        String delimiter = null;
+        Pattern pattern = Pattern.compile("//(.*?)\\\\n(.*?)");
+
+        // when
+        Matcher matcher = pattern.matcher(testString);
+        matcher.find();
+        delimiter = matcher.group(1);
+
+
+        assertThat(delimiter)
+                .isEqualTo(";");
+    }
+
+    @DisplayName("//와 \\n 사이 문자열 없다면 빈 문자열을 리턴한다")
+    @Test
+    void delimiterParseTest2() {
+        // given
+        String testString = "//\\n123";
+        String delimiter = null;
+        Pattern pattern = Pattern.compile("//(.*?)\\\\n");
+
+        // when
+        Matcher matcher = pattern.matcher(testString);
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+        }
+
+        assertThat(delimiter)
+                .isEqualTo("");
+    }
+
+
 }
