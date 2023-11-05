d1b80fa8a0fbee9236770ef2ec95bc57f0c88493
pilming44 <pilming44@naver.com>
2023-11-06 01:27:33 +0900

    style: 사용하지않는 import 제거

--- src/test/java/racingCar/RacingTest.java
+++ src/test/java/racingCar/RacingTest.java
@@ -1,11 +1,9 @@
 package racingCar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingTest {
