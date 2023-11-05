972bec9aebb4badb9ac47287cdfbd5af699c0e18
jikimee64 <jikimee64@gmail.com>
2023-11-05 14:23:59 +0900

    feat: 자동차 이름 5자 이하로 입력

---
+++ src/main/java/racinggame/Name.java
@@ -0,0 +1,20 @@
+package racinggame;
+
+public class Name {
+
+    private static final int CAR_NAME_MAX_SIZE = 5;
+
+    private String carName;
+
+    public Name(String carName) {
+        validateCarNameSize(carName);
+        this.carName = carName;
+    }
+
+    public void validateCarNameSize(String carName) {
+        if (carName.length() > CAR_NAME_MAX_SIZE) {
+            throw new ExceedNameSizeException();
+        }
+    }
+
+}
---
+++ src/test/java/racinggame/NameTest.java
@@ -0,0 +1,21 @@
+package racinggame;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NameTest {
+
+    @Test
+    @DisplayName("실패 - 자동차 이름을 5자 초과 하여 입력시 예외가 발생한다.")
+    void fail_car_name_size() {
+        // given
+        String carName = "abcdef";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Name(carName))
+                .isInstanceOf(ExceedNameSizeException.class)
+                .hasMessage("자동차의 이름은 5자 이하만 입력 가능합니다.");
+    }
+
+}
