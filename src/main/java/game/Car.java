bde292e34abe93494857fbe914c90017e645b8ac
henry174 <dltmdwns812@daum.net>
2023-11-05 10:57:40 +0900

    자동차 이동 구현

--- src/main/java/game/Car.java
+++ src/main/java/game/Car.java
@@ -12,12 +12,11 @@ public class Car {
      * @param initPosition 초기 위치. 0 이상이여야 합니다.
      */
     public Car(int initPosition) {
-        if (initPosition < 0) {
-            throw new IllegalArgumentException("위치는 0 이상이어야 하지만 " + initPosition + "이 주어졌습니다.");
-        }
+        validatePosition(initPosition);
 
         this.position = initPosition;
     }
+
 
     /**
      * 초기 위치가 1인 자동차를 생성합니다.
@@ -40,5 +39,34 @@ public class Car {
         }
 
         return trajectory.toString();
+    }
+
+    /**
+     * 자동차를 앞으로 또는 뒤로 움직입니다.
+     *
+     * @param distance 움직일 거리. 양수면 앞으로 이동, 음수면 뒤로 이동, 0이면 움직이지 않습니다.
+     */
+    public void move(int distance) {
+        this.position += distance;
+
+        validatePosition(this.position);
+    }
+
+    // 이 이하로 단순 getter 모음
+
+    /**
+     * 자동차의 현재 위치
+     * @return 자동차의 현재 위치. 위치는 0 이상입니다.
+     */
+    public int getPosition() {
+        return this.position;
+    }
+
+    // 이 이하로 validation 함수 모음
+
+    private static void validatePosition(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("위치는 0 이상이어야 하지만 " + position + "이 되었습니다.");
+        }
     }
 }
--- src/test/java/game/CarTest.java
+++ src/test/java/game/CarTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarTest {
     @Test
@@ -14,5 +15,27 @@ public class CarTest {
 
         assertThat(mySummerCar.getMoveTrajectory())
                 .isEqualTo("-----");
+    }
+
+    @Test
+    @DisplayName("자동차 움직이기 테스트")
+    public void moveCar() {
+        Car mySummerCar = new Car(1);
+
+        mySummerCar.move(4);
+        assertThat(mySummerCar.getPosition())
+                .isEqualTo(5);
+
+        mySummerCar.move(0);
+        assertThat(mySummerCar.getPosition())
+                .isEqualTo(5);
+
+        mySummerCar.move(-2);
+        assertThat(mySummerCar.getPosition())
+                .isEqualTo(3);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            mySummerCar.move(-5);
+        });
     }
 }
