95c5462b45a0735a2e50e01254b21cd038156412
hyeongiii <hyeongiii0811@gmail.com>
2023-11-06 06:47:33 +0900

    step2: 숫자 하나를 문자열로 입력할 경우, 해당 숫자를 반환하는 기능 구현

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -1,10 +1,12 @@
 package step2;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringAddCalculatorTest {
 
@@ -13,6 +15,13 @@ public class StringAddCalculatorTest {
     @EmptySource
     @NullSource
     void nullOrEmptyStringTest(String text) {
-        Assertions.assertThat(StringAddCalculator.splitAndSum(text)).isEqualTo(0);
+        assertThat(StringAddCalculator.splitAndSum(text)).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자 하나를 문자열로 입력할 경우, 해당 숫자를 반환한다.")
+    @CsvSource(value = {"1:1", "2:2", "3:3"}, delimiter = ':')
+    void singleNumberTest(String text, int expected) {
+        assertThat(StringAddCalculator.splitAndSum(text)).isEqualTo(expected);
     }
 }
