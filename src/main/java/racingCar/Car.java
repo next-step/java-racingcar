6fc46e43594bd35ae4561193f1db36a40b5a605b
pilming44 <pilming44@naver.com>
2023-11-06 00:29:31 +0900

    feat, docs: 이동 조건에 따른 전진 구현, 문서 업데이트

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -3,13 +3,29 @@ package racingCar;
 public class Car {
     private int currentPosition = 0;
 
+    //한번에 이동하는 거리
     public static final int MOVE_DISTANCE = 1;
+
+    //이동조건
+    public static final int MOVE_CONDITION = 4;
 
     public void moveForward() {
         currentPosition += MOVE_DISTANCE;
     }
 
+    public void moveAttempt(int value) {
+        if(movable(value)) {
+            moveForward();
+        }
+    }
+
     public int getCurrentPosition() {
         return currentPosition;
+    }
+    private boolean movable(int value) {
+        if (value < MOVE_CONDITION) {
+            return false;
+        }
+        return true;
     }
 }
