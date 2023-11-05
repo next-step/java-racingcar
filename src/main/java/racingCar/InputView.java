ec61ccd49dbe9c86f8768c10ffcdf93a3fd1f953
beyondorder <youofsea@gmail.com>
2023-11-05 13:46:45 +0900

    입력부 구현

--- src/main/java/racingCar/InputView.java
+++ src/main/java/racingCar/InputView.java
@@ -1,2 +1,18 @@
-package racingCar;public class InputView {
+package racingCar;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner scanner = new Scanner(System.in);
+    private final String question;
+
+    InputView(String question){
+        this.question = question;
+    }
+
+    public int intInput(){
+        System.out.println(this.question);
+        return scanner.nextInt();
+    }
+
 }
--- src/main/java/racingCar/RacingCarGame.java
+++ src/main/java/racingCar/RacingCarGame.java
@@ -6,9 +6,11 @@ public class RacingCar {
 
     static final private int PIVOT_NUMBER_TO_FORWARD = 4;
     static final private int CLOSED_END_NUMBER_TO_GET_RANDOM_NUMBER_FROM_ZERO = 10;
-
+    private final static String CAR_COUNT_QUESTION = "자동차 대수는 몇 대 인가요?";
+    private final static String GAME_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
     void racing(){
-
+        int carCount = new InputView(CAR_COUNT_QUESTION).input();
+        int gameCount = new InputView(GAME_COUNT_QUESTION).input();
     }
 
     private boolean isSatisfiedForward(){
