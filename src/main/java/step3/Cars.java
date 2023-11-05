fbf4b08f321a6d98a59535bf924f480215a26d01
jane096 <jes7077@naver.com>
2023-11-06 00:53:40 +0900

    최소 랜덤숫자 상수명 변경 validation 클래스명 변경으로 인한 수정

--- src/main/java/step3/Cars.java
+++ src/main/java/step3/Cars.java
@@ -1,6 +1,6 @@
 package step3;
 
-import step3.validation.ValidateCarsInput;
+import step3.validation.ValidateRacingCarInput;
 
 import java.util.Scanner;
 
@@ -8,17 +8,17 @@ import static step3.constants.QuestionConstants.HOW_MANY_CARS;
 
 public class Cars {
 
-    private static final int MINIMUM_PLAYING_TIMES = 4;
+    private static final int MINIMUM_RANDOM_VALUE = 4;
 
     public static int carsInput(Scanner scanner) {
         System.out.println(HOW_MANY_CARS);
         int cars = scanner.nextInt();
 
-        return ValidateCarsInput.positiveNumber(cars);
+        return ValidateRacingCarInput.positiveNumber(cars);
     }
 
     public static boolean isMovable(int times) {
-        return times >= MINIMUM_PLAYING_TIMES;
+        return times >= MINIMUM_RANDOM_VALUE;
     }
 
     public static String go(String currentStatus) {
