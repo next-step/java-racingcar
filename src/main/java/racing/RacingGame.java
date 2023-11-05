2145a03bc0fbe61a25add6be84cfc5ac254ca639
jeounpar <cdex6531@ajou.ac.kr>
2023-11-05 19:24:30 +0900

    feat : random 값이 4이상일 경우 자동차를 전진하는 메서드

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -7,8 +7,14 @@ public class RacingGame {
 	private static final Random RANDOM = new Random();
 	private static final int BOUND = 10;
 
-
 	public static int random() {
 		return RANDOM.nextInt(BOUND);
+	}
+
+	public static String move(String car, int random) {
+		if (random >= 4) {
+			return car + "-";
+		}
+		return car;
 	}
 }
--- src/test/java/racing/RacingGameTest.java
+++ src/test/java/racing/RacingGameTest.java
@@ -4,15 +4,26 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class RacingGameTest {
 
 	@Test
 	@DisplayName("nextInt(10)의 값은 0이상 9이하이다")
 	void nextInt() {
-		Assertions.assertThat(RacingGame.random())
+		assertThat(RacingGame.random())
 				.isGreaterThanOrEqualTo(0)
 				.isLessThanOrEqualTo(9);
 	}
 
+	@Test
+	@DisplayName("자동차는 random값이 4이상일 때 전진한다")
+	void move() {
+		String car = "-";
+
+		String after = RacingGame.move(car, 4);
+
+		assertThat(after).isEqualTo(car + "-");
+	}
 
 }
