ebae614a8a35a89c417a36a566ebd7dfdf167e4e
HyunggiPark <phg9587@naver.com>
2023-11-05 19:46:21 +0900

    refact: main 메서드 리팩터링

--- src/main/java/step3/main/Main.java
+++ src/main/java/step3/main/Main.java
@@ -14,18 +14,30 @@ public class Main {
         //inputView
         final UserInput userInput = InputView.input();
 
-        //게임 실행
-        List<RacingCar> cars = new ArrayList<>();
-        for (int i = 0; i < userInput.getCarNumber(); i++) {
-            cars.add(RacingCar.build());
-        }
+        List<RacingCar> cars = makeCars(userInput.getCarNumber());
 
+        //게임 실행
+        startRacing(cars, userInput.getTryNumber());
+    }
+
+    private static void startRacing(final List<RacingCar> cars, final int tryNumber) {
         //resultView
         System.out.println("실행 결과");
-        for (int i = 0; i < userInput.getTryNumber(); i++) {
+
+        for (int i = 0; i < tryNumber; i++) {
             tryMove(cars);
             ResultView.printTryResult(cars);
         }
+    }
+
+    private static List<RacingCar> makeCars(final int carNumber) {
+        List<RacingCar> cars = new ArrayList<>();
+
+        for (int i = 0; i < carNumber; i++) {
+            cars.add(RacingCar.build());
+        }
+
+        return cars;
     }
 
     private static void tryMove(final List<RacingCar> cars) {
