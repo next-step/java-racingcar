c7167fd6f322093cad2caae78eb5cbd7da8f8ef2
jonghunbaek <qor6028@naver.com>
2023-11-05 20:22:36 +0900

    test : 인덱스를 전달받아 해당하는 위치의 Car객체를 반환하는 테스트
    - 인덱스 범위를 벗어나는 테스트도 같이 작성

--- src/main/java/step3/Cars.java
+++ src/main/java/step3/Cars.java
@@ -7,9 +7,7 @@ public class Cars {
 
     private List<Car> cars = new ArrayList<>();
 
-    public Cars() {
-
-    }
+    public Cars() {}
 
     public Cars(int numOfCars) {
         for (int i=0; i<numOfCars; i++) {
@@ -19,5 +17,17 @@ public class Cars {
 
     public int getNumOfCars() {
         return cars.size();
+    }
+
+    public Car getCarBy(int idx) {
+        if (isOutOfIndex(idx)) {
+            throw new IndexOutOfBoundsException("해당 번호의 차량은 존재하지 않습니다.");
+        }
+
+        return cars.get(idx);
+    }
+
+    private boolean isOutOfIndex(int idx) {
+        return idx >= cars.size() || idx < 0;
     }
 }
--- src/test/java/step3/CarsTest.java
+++ src/test/java/step3/CarsTest.java
@@ -3,6 +3,8 @@ package step3;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,15 +26,30 @@ class CarsTest {
     }
 
     @DisplayName("인자로 인덱스를 받아 해당 인덱스의 Car객체를 반환한다.")
-    @Test
-    void getCarBy() {
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,2,3,4})
+    void getCarBy(int idx) {
         // given
-        Cars cars = new Cars();
+        int numOfCars = 5;
+        Cars cars = new Cars(numOfCars);
 
         // when
-
+        Car car = cars.getCarBy(idx);
 
         // then
+        assertThat(car).isNotNull();
+    }
 
+    @DisplayName("인자로 인덱스를 받아 해당 인덱스의 Car객체를 반환할 때 인덱스의 범위가 벗어나면 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 5})
+    void getCarByWhenOufOfIndex(int idx) {
+        // given
+        int numOfCars = 5;
+        Cars cars = new Cars(numOfCars);
+
+        // when & then
+        assertThatThrownBy(() -> cars.getCarBy(idx)).isInstanceOf(IndexOutOfBoundsException.class)
+            .hasMessage("해당 번호의 차량은 존재하지 않습니다.");
     }
 }
