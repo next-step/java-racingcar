760fb86836da97455ea6149201ea142f6d2f4582
pilming44 <pilming44@naver.com>
2023-11-06 01:12:45 +0900

    feat: 경주에 Car 배열 추가

--- src/main/java/racingCar/Racing.java
+++ src/main/java/racingCar/Racing.java
@@ -3,6 +3,7 @@ package racingCar;
 public class Racing {
     private int carCount;
     private int roundCount;
+    private Car[] cars;
 
     public final int MINIMUM_CAR_COUNT = 2;
     public final int MINIMUM_ROUND_COUNT = 1;
@@ -11,6 +12,11 @@ public class Racing {
     public Racing (int inputCarCount, int inputRoundCount) {
         carCount = inputCarCount;
         roundCount = inputRoundCount;
+
+        cars = new Car[carCount];
+        for (int i = 0; i < carCount; i++) {
+            cars[i] = new Car();
+        }
     }
 
     public boolean holdPossible() {
@@ -26,5 +32,9 @@ public class Racing {
 
     public int getRoundCount() {
         return roundCount;
+    }
+
+    public Car[] getCars() {
+        return cars;
     }
 }
