cc86b93d63198a914d1427278b2910b36f885265
cjsrhd94 <cjsrhd94@gmail.com>
2023-11-05 20:00:55 +0900

    fix: 랜덤 값이 추출되지 않는 현상 수정

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,13 +1,12 @@
 package racingcar;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class RacingGame {
 	private static final int START_LOCATION = 1;
 	private static final int STANDARD_SHUFFLE_NUM = 4;
-	private static final List<Integer> NUM_BOX = List.of(0,1,2,3,4,5,6,7,8,9);
 	private final List<Car> cars = new ArrayList<>();
 	private final List<Round> rounds = new ArrayList<>();
 
@@ -49,8 +48,7 @@ public class RacingGame {
 	}
 
 	private int shuffleNumBox() {
-		Collections.shuffle(NUM_BOX);
-		return NUM_BOX.iterator().next();
+		return new Random().nextInt(10);
 	}
 
 	public List<Car> getCars() {
