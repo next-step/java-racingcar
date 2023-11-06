9d732c3dcc75ce473076aacdcd629e08c9718772
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-06 15:54:23 +0900

    feat : 콜론을 구분자로 가지는 문자열을 전달하면 구분자를 기준으로 분리한 숫자의 합하는 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,26 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
+
+    public static final String COMMA = ",";
+    public static final String COLUMN = ":";
+
     public long calculate(String value) {
-        int sum = 0;
-        for (String val : value.split(",")) {
-            sum += Integer.parseInt(val);
+        if (containsComma(value)) {
+            return calculateSum(value, COMMA);
         }
+        return calculateSum(value, COLUMN);
+    }
+
+    private boolean containsComma(String value) {
+        return value.contains(",");
+    }
+
+    private long calculateSum(String value, String regex) {
+        long sum = 0;
+        sum += Arrays.stream(value.split(regex)).mapToInt(Integer::parseInt).sum();
         return sum;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -20,4 +20,18 @@ public class CalculatorTest {
         // then
         assertThat(sum).isEqualTo(4);
     }
+
+    @Test
+    @DisplayName("콜론을 가지는 문자열을 전달한 경우 구분자를 기준으로 분리한 각 숫자의 합을 구함")
+    void column_sum() {
+        // given
+        Calculator calculator = new Calculator();
+        String given = "1:3";
+
+        // when
+        long sum = calculator.calculate(given);
+
+        // then
+        assertThat(sum).isEqualTo(4);
+    }
 }
