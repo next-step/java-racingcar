1d43d77c7ffd7fe0792e5536c45f5211f046eda3
pilming44 <pilming44@naver.com>
2023-11-06 21:17:17 +0900

    refactor: inputView scanner변수처리, sout을 메서드로 분리

--- src/main/java/racingCar/InputView.java
+++ src/main/java/racingCar/InputView.java
@@ -3,15 +3,17 @@ package racingCar;
 import java.util.Scanner;
 
 public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
     public static int carCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        Scanner scanner = new Scanner(System.in);
+        inputPrintln("자동차 대수는 몇 대 인가요?");
         return scanner.nextInt();
     }
 
     public static int roundCount() {
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        Scanner scanner = new Scanner(System.in);
+        inputPrintln("시도할 회수는 몇 회 인가요?");
         return scanner.nextInt();
+    }
+    private static void inputPrintln(String message) {
+        System.out.println(message);
     }
 }
