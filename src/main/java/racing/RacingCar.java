8fec27076786579e2e2ef1972128bbc8b2fb4412
hyoin <kslove1111@gmail.com>
2023-11-06 22:50:26 +0900

    Refactor: 메소드 분리 리펙토링

--- src/main/java/racing/RacingCar.java
+++ src/main/java/racing/RacingCar.java
@@ -4,26 +4,42 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RacingCar {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int[] carCount = new int[scanner.nextInt()];
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int raceCount = scanner.nextInt();
+    static String DISTANCE = "-";
 
-        System.out.println("실행 결과");
+    public static void main(String[] args) {
+        print("자동차 대수는 몇 대 인가요?");
+        int[] carDistance = new int[input()];
+        print("시도할 회수는 몇 회 인가요?");
+        int raceCount = input();
+
+        print("실행 결과");
         for (int i=0; i<raceCount; i++) {
-            for (int j=0; j<carCount.length; j++) {
-                Random random = new Random();
-                if (random.nextInt(10) >= 4) {
-                    carCount[j]++;
-                }
-                for (int k=0; k<carCount[i]; k++) {
-                    System.out.print("-");
-                }
-                System.out.println();
-            }
-            System.out.println();
+            carDistance = race(carDistance);
+            print("");
         }
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
+    }
+
+    private static int input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    private static boolean move() {
+        Random random = new Random();
+        return random.nextInt(10) >= 4;
+    }
+
+    private static int[] race(int[] carDistance) {
+        for (int i=0; i<carDistance.length; i++) {
+            if (move()) {
+                carDistance[i]++;
+            }
+            print(DISTANCE.repeat(carDistance[i]));
+        }
+        return carDistance;
     }
 }
