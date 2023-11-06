3c55230df6b6df5be2ae913be243f7e6df56c34a
parkje0927 <parkje09270@gmail.com>
2023-11-06 19:56:03 +0900

    refactor: 게임을 실행하는 main 메소드에서 input, play, output 이 수행되도록 개선

---
+++ src/main/java/racingcargame/Game.java
@@ -0,0 +1,23 @@
+package racingcargame;
+
+import java.util.List;
+
+public class Game {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        int carNumber = inputView.getCarNumber();
+        int playCount = inputView.getPlayCount();
+
+        RacingPlay racingPlay = new RacingPlay();
+        OutputView outputView = new OutputView();
+
+        List<Car> carList = racingPlay.readyForRacing(carNumber);
+
+        System.out.println("실행 결과");
+        for (int i = 0; i < playCount; i++) {
+            carList = racingPlay.playRacingCar(carList);
+            outputView.output(carList);
+        }
+    }
+}
