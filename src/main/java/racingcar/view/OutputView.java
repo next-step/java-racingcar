66724bb6cd617a915cf94fecbf7f2e5776d34b32
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-06 23:50:36 +0900

    feat : OutputView 게임 실행 결과를 출력하는 기능 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,14 @@
+package racingcar.view;
+
+public class OutputView {
+
+    private static final String EXECUTION_RESULT = "실행 결과";
+
+    public void printGameResultMent() {
+        System.out.println(EXECUTION_RESULT);
+    }
+
+    public void printSingleGameResult(String gameResult) {
+        System.out.println(gameResult);
+    }
+}
