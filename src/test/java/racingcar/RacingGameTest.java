f600db03146839266234840b972ac11e72147b50
cjsrhd94 <cjsrhd94@gmail.com>
2023-11-06 10:56:37 +0900

    test: 테스트 이름 변경

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,10 +1,6 @@
 package racingcar;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,12 +8,8 @@ public class RacingGameTest {
 
 	@ParameterizedTest
 	@ValueSource(ints = {1, 2, 3})
-	void 레이싱_게임을_초기화하면_입력받은_숫자만큼_차들이_초기화된다(int num) {
+	void 레이싱_게임을_초기화하면_입력받은_숫자만큼_차량이_초기화된다(int num) {
 		RacingGame game = new RacingGame(num);
 		Assertions.assertThat(game.getCars().size()).isEqualTo(num);
-	}
-
-	@Test
-	void 레이싱_게임을_진행한다() {
 	}
 }
