264faac140d984840332c89e489fe37ebf0d62b9
pilming44 <pilming44@naver.com>
2023-11-07 01:16:11 +0900

    refactor: 리뷰 반영해서 Racing 클래스 리팩토링
    - 경주에 관련된 메서드만 포함

--- src/main/java/racingCar/Racing.java
+++ src/main/java/racingCar/Racing.java
@@ -1,65 +1,27 @@
 package racingCar;
 
-import java.util.Random;
+import racingCar.movableStrategy.RandomStrategy;
 
 public class Racing {
-    private static int carCount;
-    private static int roundCount;
-    private static Car[] cars;
-
     public static final int MINIMUM_CAR_COUNT = 2;
     public static final int MINIMUM_ROUND_COUNT = 1;
-
+    private int roundCount;
+    private Cars cars;
 
     public Racing(int inputCarCount, int inputRoundCount) {
-        carCount = inputCarCount;
-        roundCount = inputRoundCount;
+        this.roundCount = inputRoundCount;
 
-        cars = new Car[carCount];
-        for (int i = 0; i < carCount; i++) {
-            cars[i] = new Car();
+        cars = new Cars(inputCarCount, new RandomStrategy(Car.MOVE_CONDITION, 10));
+    }
+
+    public boolean holdPossible() {
+        if (cars.carsReady(MINIMUM_CAR_COUNT) && roundCount >= MINIMUM_ROUND_COUNT) {
+            return true;
         }
+        return false;
     }
 
-    public static boolean holdPossible() {
-        if (carCount < MINIMUM_CAR_COUNT || roundCount < MINIMUM_ROUND_COUNT) {
-            return false;
-        }
-        return true;
+    public void race() {
+        cars.race(roundCount);
     }
-
-    public int getCarCount() {
-        return carCount;
-    }
-
-    public int getRoundCount() {
-        return roundCount;
-    }
-
-    public Car[] getCars() {
-        return cars;
-    }
-
-    public static void main(String[] args) {
-        Racing racing = new Racing(InputView.carCount(), InputView.roundCount());
-
-        //개최조건 미 충족시 종료
-        if (!holdPossible()) {
-            ResultView.noRace();
-            return;
-        }
-
-        //경주시작
-        ResultView.raceStart();
-        for (int i = 0; i < roundCount; i++) {
-            for (int j = 0; j < carCount; j++) {
-                Random random = new Random();
-                cars[j].moveAttempt(random.nextInt(10));
-                ResultView.positionPrint(cars[j].getCurrentPosition());
-            }
-            ResultView.roundFinish();
-        }
-    }
-
-
 }
--- src/test/java/racingCar/RacingTest.java
+++ src/test/java/racingCar/RacingTest.java
@@ -22,25 +22,4 @@ class RacingTest {
         Racing racing = new Racing(inputCarCount, inputRacingCount);
         assertThat(racing.holdPossible()).isTrue();
     }
-
-    @ParameterizedTest
-    @CsvSource(value = {"2:1", "4:3", "3:7"}, delimiter = ':')
-    @DisplayName("입력받은 값으로 경주를 세팅한다.")
-    void Racing_세팅(int inputCarCount, int inputRoundCount) {
-        Racing racing = new Racing(inputCarCount, inputRoundCount);
-        int racingCarCount = racing.getCarCount();
-        int racingRoundCount = racing.getRoundCount();
-
-        //경주정보 세팅
-        assertThat(racingCarCount).isEqualTo(inputCarCount);
-        assertThat(racingRoundCount).isEqualTo(inputRoundCount);
-
-        //자동사 대수가 제대로 생성됐는지 확인
-        assertThat(racing.getCars().length).isEqualTo(racingCarCount);
-
-        //자동차들이 제대로 세팅됐는지 확인
-        for (int i = 0; i < racingCarCount; i++) {
-            assertThat(racing.getCars()[i]).isNotNull();
-        }
-    }
 }
