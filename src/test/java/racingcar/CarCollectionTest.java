db7f416c5b9d91159e0dd640c577e8e6e1d0e0c0
KimGwangHoon <khkimykk@naver.com>
2023-11-06 23:43:16 +0900

    style: CarCollectionTest 변수명 수정

--- src/test/java/racingcar/CarCollectionTest.java
+++ src/test/java/racingcar/CarCollectionTest.java
@@ -12,11 +12,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarCollectionTest {
-    private CarCollection carFactory;
+    private CarCollection carCollection;
     private final List<String> DEFAULT_CAR_NAME = List.of("pobi","crong","honux");
 
     public void setUp() {
-        this.carFactory = new CarCollection();
+        this.carCollection = new CarCollection();
     }
 
     @DisplayName("차량이름으로 차량생성")
@@ -25,8 +25,8 @@ public class CarCollectionTest {
         //given
         setUp();
         //when
-        carFactory.participants(DEFAULT_CAR_NAME);
-        List<Car> cars = carFactory.getCars();
+        carCollection.participants(DEFAULT_CAR_NAME);
+        List<Car> cars = carCollection.getCars();
         //then
         assertThat(cars.size()).isEqualTo(3);
     }
@@ -36,10 +36,10 @@ public class CarCollectionTest {
     public void 차량_생성_후_이동값_4_부여_1칸이동() {
         //given
         setUp();
-        carFactory.participants(DEFAULT_CAR_NAME);
+        carCollection.participants(DEFAULT_CAR_NAME);
         //when
-        carFactory.moveCars(new FixtureMoveStrategy(4));
-        List<Car> cars = carFactory.getCars();
+        carCollection.moveCars(new FixtureMoveStrategy(4));
+        List<Car> cars = carCollection.getCars();
         //then
         assertThat(cars.get(0).position()).isEqualTo(1);
         assertThat(cars.get(1).position()).isEqualTo(1);
@@ -51,10 +51,10 @@ public class CarCollectionTest {
     public void 차량_생성_후_이동값_3_부여_0칸이동() {
         //given
         setUp();
-        carFactory.participants(DEFAULT_CAR_NAME);
+        carCollection.participants(DEFAULT_CAR_NAME);
         //when
-        carFactory.moveCars(new FixtureMoveStrategy(3));
-        List<Car> cars = carFactory.getCars();
+        carCollection.moveCars(new FixtureMoveStrategy(3));
+        List<Car> cars = carCollection.getCars();
         //then
         assertThat(cars.get(0).position()).isEqualTo(0);
         assertThat(cars.get(1).position()).isEqualTo(0);
@@ -72,7 +72,7 @@ public class CarCollectionTest {
             List<String> carName = List.of("pobids");
             //when
             //then
-            assertThatThrownBy(() -> carFactory.participants(carName))
+            assertThatThrownBy(() -> carCollection.participants(carName))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("자동차 이름은 5자를 초과 할 수 없습니다.");
         }
@@ -85,7 +85,7 @@ public class CarCollectionTest {
             List<String> carName = List.of("pobi","","honux");
             //when
             //then
-            assertThatThrownBy(() -> carFactory.participants(carName))
+            assertThatThrownBy(() -> carCollection.participants(carName))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("자동차 이름이 없는 경우가 있습니다.");
         }
