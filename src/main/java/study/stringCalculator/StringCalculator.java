e0ae2b2983fe6fb8bc44b5cd20c559e5c5d53f5d
sudo-init <76156608+sudo-init@users.noreply.github.com>
2023-11-06 21:25:12 +0900

    2단계  문자열 덧셈 계산기 (#4904)
    * split study test
    
    * split study test
    
    * step 1 practice
    
    * first commit
    
    * step2 mission completed.
    
    * step2 refactoring completed.

---
+++ src/main/java/study/stringCalculator/StringCalculator.java
@@ -0,0 +1,37 @@
+package study.stringCalculator;
+
+public class StringCalculator {
+
+    public final int ZERO_RESULT = 0;
+    private Splitter splitter;
+
+    public StringCalculator() {
+        splitter = new Splitter();
+    }
+
+    public int calculate(String input_string) {
+        if (isEmpty(input_string)) {
+            return ZERO_RESULT;
+        }
+        int[] numbers = splitter.getNumbers(input_string);
+
+        return getSum(numbers);
+    }
+
+    private boolean isEmpty(String expression) {
+        return expression == null || expression.isBlank();
+    }
+
+    private int getSum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum = add(sum, number);
+        }
+        return sum;
+    }
+
+    private int add(int sum, int number) {
+        return sum + number;
+    }
+
+}
---
+++ src/test/java/study/StringCalculatorTest.java
@@ -0,0 +1,56 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import study.stringCalculator.StringCalculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static study.Step2.calculateString;
+
+public class StringCalculatorTest {
+
+    @Test
+    @DisplayName("Test empty string check")
+    void checkStringIsBlank() {
+        int result = calculateString(null);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("Test, split and add with general splitter")
+    void generalSplitterStringAddCalc() {
+        int result = calculateString("1,2,3");
+        int result2 = calculateString("1,2:3");
+        assertThat(result).isEqualTo(6);
+        assertThat(result2).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("Test, split and add with custom splitter")
+    void customSplitterStringAddCalc() {
+        int result = calculateString("//-\n1-2-3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("Test, an error occurs if it is a value other than a number.")
+    void notNumberError() {
+        // 일반 구분자일 때
+        assertThrows(IllegalArgumentException.class, () -> calculateString("1;-;3"));
+
+        //특수 구분자 일 떄
+        assertThrows(IllegalArgumentException.class, () -> calculateString("//;\n1;-;3"));
+    }
+
+    @Test
+    @DisplayName("Test, an error occurs if it is negative number.")
+    void negativeNumberError() {
+        // 일반 구분자일 때
+        assertThrows(RuntimeException.class, () -> calculateString("1;-2;3"));
+
+        // 특수 구분자 일 떄
+        assertThrows(RuntimeException.class, () -> calculateString("//;\n1;-2;3"));
+    }
+}
