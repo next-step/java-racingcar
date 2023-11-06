77b1b62d12d37b33514c1656077470f9c178576a
jeseungkim <qazwsx3745@naver.com>
2023-11-06 18:58:12 +0900

    feat: update temporarily

--- src/main/java/carracing/CarRacing.java
+++ src/main/java/carracing/CarRacing.java
@@ -8,9 +8,9 @@ public class CarRacing {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("자동차 대수는 몇 대 인가요?");
-        int carNumber = scanner.nextInt();
+        String carNumber = scanner.nextLine();
         System.out.println("시도할 회수는 몇 회 인가요?");
-        int tryNumber = scanner.nextInt();
+        String tryNumber = scanner.nextLine();
 
         System.out.println("실행 결과");
 
@@ -19,6 +19,24 @@ public class CarRacing {
     }
 
 
-    public static void car() {
+    public static int carInputProcess(String text) {
+//        int result = 0;
+
+        if(isBlank(text)) {
+            return 0;
+        }
+
+        if(text.length() > 1) {
+            return toInt(text.substring(0,1));
+        }
+
+        return toInt(text);
+    }
+
+    private static boolean isBlank(String text) {
+        return text == null || text.isBlank();
+    }
+    private static int toInt(String text) {
+        return Integer.parseInt(text);
     }
 }
--- src/test/java/carracing/CarRacingTest.java
+++ src/test/java/carracing/CarRacingTest.java
@@ -2,9 +2,17 @@ package carracing;
 
 import org.junit.jupiter.api.Test;
 
+import static carracing.CarRacing.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarRacingTest {
     @Test
-    void test() {
-        CarRacing.car();
+    void normalReturn() {
+        assertThat(carInputProcess("1")).isEqualTo(1);
+    }
+
+    @Test
+    void nullOrBlank() {
+        assertThat(carInputProcess("")).isEqualTo(0);
     }
 }
