1a1dcc19eb11143b98c22f387eb8326754a2c560
김태현 <thxogusrla@gmail.com>
2023-11-06 19:32:02 +0900

    test - 메서드 명 수정

--- src/test/java/car/CarTest.java
+++ src/test/java/car/CarTest.java
@@ -1,8 +1,6 @@
 package car;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,13 +10,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @ValueSource(ints = {4, 9})
     @DisplayName("전진 테스트")
     void 전진(int number) {
         Car car = new Car();
         int initPosition = car.currentPosition();
 
-        car.move(number);
+        car.moveAndRecord(number);
 
         assertThat(car.currentPosition()).isEqualTo(initPosition + 1);
     }
@@ -30,7 +28,7 @@ public class CarTest {
         Car car = new Car();
         int initPosition = car.currentPosition();
 
-        car.move(number);
+        car.moveAndRecord(number);
 
         assertThat(car.currentPosition()).isEqualTo(initPosition);
     }
@@ -41,4 +39,5 @@ public class CarTest {
     void 자동창_생성_크기(int number) {
         assertThat(generate(number)).hasSize(number);
     }
+
 }
