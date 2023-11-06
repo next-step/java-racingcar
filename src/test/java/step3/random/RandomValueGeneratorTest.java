691215fb1d200fc6adb41675353f36d601b6bfd2
HyunggiPark <phg9587@naver.com>
2023-11-06 12:21:35 +0900

    refact: 무작위 값을 검증하는 테스트를 여러번 반복해서 실행하는 것으로 확실하게 값을 검증

--- src/test/java/step3/random/RandomValueGeneratorTest.java
+++ src/test/java/step3/random/RandomValueGeneratorTest.java
@@ -1,12 +1,12 @@
 package step3.random;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RandomValueGeneratorTest {
-    @Test
+    @RepeatedTest(value = 10)
     @DisplayName("0에서 9이하의 랜덤 값을 생성한다.")
     void generateRandomValueFromZeroToNine() {
         //given
