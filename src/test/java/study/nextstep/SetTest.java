07573c149ee10feb387517d1780af84422518fa5
areum-j <119103981+areumj0507@users.noreply.github.com>
2023-11-06 09:15:52 +0900

    feat: 학습 테스트 실습 커밋 (#4974)

---
+++ src/test/java/study/nextstep/SetTest.java
@@ -0,0 +1,42 @@
+package study.nextstep;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    public void testSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void testContains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:TRUE", "2:TRUE", "3:TRUE", "4:FALSE", "5:FALSE"}, delimiter = ':')
+    public void testContainsTrueOrFalse(int number, boolean flag) {
+        assertThat(numbers.contains(number)).isEqualTo(flag);
+    }
+}
