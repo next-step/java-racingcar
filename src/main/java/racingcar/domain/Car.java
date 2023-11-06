4a174497d021417d298098d4d04e0320e9a5261b
kimbro97 <rlagudwog@gmail.com>
2023-11-06 19:41:07 +0900

    refactor: 상수와 인스턴스 변수 위치 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,8 +1,8 @@
 package racingcar.domain;
 
 public class Car {
-    private int position = 0;
     private static final int MAX_FORWARD_NUMBER = 4;
+    private int position = 0;
 
     public void move(int randomNo) {
         if (randomNo >= MAX_FORWARD_NUMBER) {
