68fc2830932f75cb958127c6e6bc6a65f1c8a239
jeonghuntech <jeonghun.tech@gmail.com>
2023-11-06 12:24:31 +0900

    feat: 입력/출력 개발
    프로토타입 작성, 추후 수정 예정

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,24 @@
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+
+    public int readCarCount() {
+        System.out.println(CAR_COUNT_MESSAGE);
+        return readCount();
+    }
+
+    public int readAttemptCount() {
+        System.out.println(CAR_COUNT_MESSAGE);
+        return readCount();
+    }
+
+    private int readCount() {
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        return scanner.nextInt();
+    }
+}
---
+++ src/main/java/racingcar/view/ResultView.java
@@ -0,0 +1,7 @@
+package racingcar.view;
+
+public class ResultView {
+    private void printResult() {
+        System.out.println("실행 결과");
+    }
+}
