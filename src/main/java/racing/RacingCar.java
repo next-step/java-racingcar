f0462176c6850e747f0b393348655b24c870244a
qweadzs <qweadzs@naver.com>
2023-11-06 22:33:00 +0900

    feat: 2단계 - RacingCar 객체 로직 개발

--- src/main/java/racing/RacingCar.java
+++ src/main/java/racing/RacingCar.java
@@ -1,4 +1,30 @@
 package racing;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class RacingCar {
+    private final static String ICON = "-";
+
+    private final Random random = new Random();
+    private final StringBuilder progress;
+
+    public RacingCar(StringBuilder progress) {
+        this.progress = progress;
+    }
+
+    public void race(){
+        if (getRandomInt() < 4) {
+            return;
+        }
+        this.progress.append(ICON);
+    }
+
+    public int getRandomInt(){
+        return random.nextInt(10);
+    }
+
+    public StringBuilder getProgress() {
+        return progress;
+    }
 }
--- src/test/java/racing/RacingCarTest.java
+++ src/test/java/racing/RacingCarTest.java
@@ -1,13 +1,19 @@
 package racing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class RacingCarTest {
+    RacingCar car;
+    @BeforeEach
+    void init(){
+        car = new RacingCar(new StringBuilder(""));
+    }
     @Test
     void random값은_0_9_사이이다() throws Exception {
-        //given
-        //when
-        //then
+        assertThat(car.getRandomInt()).isLessThan(10);
     }
     @Test
     void random값이_4이상_전진() throws Exception {
