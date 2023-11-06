6886233261910331bad341a1917f450a0a1fcf81
namuhuchutong <namuhuchutong@gmail.com>
2023-11-07 02:36:15 +0900

    feat: 콘솔 입력 기능 및 공백, 숫자 입력 검증 기능 구현

--- src/main/java/me/namuhuchutong/step4/UserRequest.java
+++ src/main/java/me/namuhuchutong/step4/UserRequest.java
@@ -7,8 +7,15 @@ public class UserRequest {
     private final int times;
 
     public UserRequest(String names, int times) {
+        validateEmptyString(names);
         this.names = names;
         this.times = times;
+    }
+
+    private void validateEmptyString(String names) {
+        if (names.isBlank()) {
+            throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
+        }
     }
 
     public String getNames() {
