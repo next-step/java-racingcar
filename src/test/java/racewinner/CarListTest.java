e574a105516b7b3d401c3300670559314ee69639
qwe5507 <apvmffkdls@gmail.com>
2023-11-06 10:47:04 +0900

    test: winnerList 테스트 코드 추가

--- src/test/java/racewinner/CarListTest.java
+++ src/test/java/racewinner/CarListTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racewinner.expteion.CarNameTooLongException;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarListTest {
@@ -20,4 +23,19 @@ public class CarListTest {
                 .isInstanceOf(CarNameTooLongException.class);
     }
 
+    @DisplayName("가장 높은 step의 자동차들이 리턴된다.")
+    @Test
+    void winnerTest() {
+        //given
+        CarList carList = new CarList(() -> true);
+
+        //when
+        carList.add("bobo");
+        carList.add("popo");
+        List<String> winnerList = carList.findWinner();
+
+        //then
+        assertThat(winnerList).contains("bobo");
+        assertThat(winnerList).contains("popo");
+    }
 }
