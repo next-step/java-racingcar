f79ba6c47d3370844a4f78b161ee596fe3d1dd7b
cjsrhd94 <cjsrhd94@gmail.com>
2023-11-06 10:22:22 +0900

    refactor: showProgressByCar 메서드 추출

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -5,10 +5,14 @@ import java.util.List;
 public class ResultView {
 	public void showResult(List<Round> rounds) {
 		for (Round round : rounds) {
-			for (Car car : round.getCars()) {
-				System.out.println("-".repeat(car.getLocation()));
-			}
+			showProgressByCar(round.getCars());
 			System.out.println("");
+		}
+	}
+
+	private void showProgressByCar(List<Car> cars) {
+		for (Car car : cars) {
+			System.out.println("-".repeat(car.getLocation()));
 		}
 	}
 }
