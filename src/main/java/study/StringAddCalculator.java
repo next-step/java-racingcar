659bac797c7a0a4b36e61a80890f0ab91cca99cb
Hyojung Han <hyojunghan1016@gmail.com>
2023-11-06 13:15:36 +0900

    🚀 2단계 - 문자열 덧셈 계산기 (#4952)
    * test: step1 피드백 내용 보완적용 && feat: step2 StringAddCalculator 기능 추가
    
    * docs: README.md 내용 수정
    
    * refactor: getsum 함수명 변경 -> paseIntAndGetSum
    
    * refactor : getStrings 함수명 변경 -> SplitAndGetStrings
    
    * refactor : step2 보완사항 적용
    
    * refactor : step3 소스 분리

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,57 @@
+package study;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    public int splitAndSum(String input) {
+        boolean isAZeroResult = input == "" || input == null;
+
+        if (isAZeroResult) {
+            return 0;
+        }
+
+        String[] stringTypeNumbers = SplitAndGetStrings(input);
+        return parseIntAndGetSum(stringTypeNumbers);
+    }
+
+    private int parseIntAndGetSum(String[] stringTypeNumbers) {
+        int sum = 0;
+        for (String stringTypeNumber : stringTypeNumbers) {
+            sum += getParseInt(stringTypeNumber);
+        }
+        return sum;
+    }
+
+    private int getParseInt(String stringTypeNumber) throws RuntimeException {
+        try {
+            return validateNegativeNumber(Integer.parseInt(stringTypeNumber));
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int validateNegativeNumber(int number) {
+        if (number < 0) {
+            throw new NumberFormatException("For input string : " + number);
+        }
+        return number;
+    }
+
+    private String[] SplitAndGetStrings(String input) {
+
+        String[] stringTypeNumbers;
+        Matcher matcher = CUSTOM_PATTERN.matcher(input);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            stringTypeNumbers = matcher.group(2).split("\\" + customDelimiter);
+        } else {
+            stringTypeNumbers = input.split(",|:");
+        }
+        return stringTypeNumbers;
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,13 +9,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
     @Test
-    void split() {
+    void splitTest() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
 
     @Test
-    void split2(){
+    void subStringTest(){
         String result = "(1,2)".substring(1, 4);
         assertThat(result).contains("1,2");
     }
@@ -23,9 +23,6 @@ public class StringTest {
     @Test
     @DisplayName("Index 위치를 벗어나는 경우, 오류가 발생한다.")
     void exceptionTest() {
-//        assertThrows(IndexOutOfBoundsException.class,
-//                () -> "abc".charAt(4));
-
         assertThatThrownBy(() -> "abc".charAt(4)).isInstanceOf(IndexOutOfBoundsException.class);
     }
 }
