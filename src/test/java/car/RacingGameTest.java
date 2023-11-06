a4b9a869051a5d7ac6ca21fbbf4acf8fb275ef78
yumble <hnjn1101@gmail.com>
2023-11-06 23:06:45 +0900

    fix: test 코드 가독성 추가 및 초기 setup코드 추가

--- src/test/java/car/RacingGameTest.java
+++ src/test/java/car/RacingGameTest.java
@@ -1,15 +1,26 @@
 package car;
 
-import static car.RacingGame.createCars;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class RacingGameTest {
+    private Car car;
+    private RacingGame racingGame;
+    private static final int START_DISTANCE = 0;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car(START_DISTANCE);
+        racingGame = new RacingGame(new RandomNumberGenerator());
+    }
+
+    @BeforeEach
     @ParameterizedTest
     @CsvSource(value = {"1:1", "2:2", "3:3"},delimiter = ':')
     public void 자동차_생성_테스트(Integer userInput, Integer numberOfCars) {
-        assertThat(createCars(userInput)).hasSize(numberOfCars);
+        assertThat(racingGame.createCars(userInput)).hasSize(numberOfCars);
     }
 }
--- src/test/java/car/RandomNumberGeneratorTest.java
+++ src/test/java/car/RandomNumberGeneratorTest.java
@@ -2,12 +2,20 @@ package car;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RandomNumberGeneratorTest {
+    private RandomNumberGenerator randomNumberGenerator;
+
+    @BeforeEach
+    void setRandomNumberGenerator() {
+        randomNumberGenerator = new RandomNumberGenerator();
+    }
+
     @Test
     void 숫자_범위_확인_테스트() {
-        int randomNumber = RandomNumberGenerator.makeRandomNumber();
+        int randomNumber = randomNumberGenerator.makeRandomNumber();
         assertTrue(randomNumber >= 0 && randomNumber < 10);
     }
 }
