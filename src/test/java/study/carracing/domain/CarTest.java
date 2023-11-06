8b943632d41ced38b6bdf495892bd34393392680
hyunyyxp <qqqq_o@naver.com>
2023-11-06 18:36:43 +0900

    test: 랜덤값에 따른 자동차 전진 여부 테스트 수정

--- src/test/java/study/carracing/domain/CarTest.java
+++ src/test/java/study/carracing/domain/CarTest.java
@@ -1,10 +1,10 @@
 package study.carracing.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
@@ -12,24 +12,23 @@ class CarTest {
     @ValueSource(ints = {4, 5, 6, 7, 8, 9, 10})
     @DisplayName("random 값이 4이상일 경우 자동차가 1칸 전진한다.")
     void moveCar(int randomValue) {
-        Car car = new Car();
+        Car car = new Car(new RandomGeneratorImpl());
 
-        car.move(randomValue);
+        car.go(randomValue);
 
         int position = car.getPosition();
         assertThat(position).isEqualTo(2);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
+    @ValueSource(ints = {0, 1, 2, 3})
     @DisplayName("random 값이 4미만일 경우 자동차는 움직이지 않는다.")
     void dontMoveCar(int randomValue) {
-        Car car = new Car();
+        Car car = new Car(new RandomGeneratorImpl());
 
-        car.move(randomValue);
+        car.go(randomValue);
 
         int position = car.getPosition();
-
         assertThat(position).isEqualTo(1);
     }
 }
