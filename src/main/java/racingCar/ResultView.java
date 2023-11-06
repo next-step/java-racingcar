65c6331c41efb58eb781cbcffe7df3ea2bcc8970
pilming44 <pilming44@naver.com>
2023-11-07 00:13:06 +0900

    refactor: ResultView 출력 메서드 분리 및 위치출력 리팩토링

--- src/main/java/racingCar/ResultView.java
+++ src/main/java/racingCar/ResultView.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 
 public class ResultView {
     public static void noRace() {
-        System.out.println("경주 시작 조건이 충족되지 않아 경주가 개최되지 않았습니다.");
+        resultPrintln("경주 시작 조건이 충족되지 않아 경주가 개최되지 않았습니다.");
     }
 
     public static void positionPrint(int position) {
-        String displayPosition = "";
+        StringBuilder displayPosition = new StringBuilder();
         for (int i = 0; i < position; i++) {
-            displayPosition += "-";
+            displayPosition.append("-");
         }
-        System.out.println(displayPosition);
+        resultPrintln(displayPosition.toString());
     }
 
     public static void raceStart() {
-        System.out.println("....경주 시작!....");
+        resultPrintln("....경주 시작!....");
     }
 
     public static void roundFinish() {
-        System.out.println("....라운드 종료!....");
+        resultPrintln("....라운드 종료!....");
+    }
+
+    private static void resultPrintln(String message) {
+        System.out.println(message);
     }
 }
