d0bfc87bee8501af2a832b35d36f896d25a93ba8
namuhuchutong <namuhuchutong@gmail.com>
2023-11-06 18:51:33 +0900

    feat: 자동차 이름 구분자 기능 구현

---
+++ src/main/java/me/namuhuchutong/step4/StringParser.java
@@ -0,0 +1,20 @@
+package me.namuhuchutong.step4;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StringParser {
+
+    private static final String DEFAULT_PATTERN = ",";
+
+    public static List<String> parse(String text) {
+        return parse(text, DEFAULT_PATTERN);
+    }
+
+    public static List<String> parse(String text, String pattern) {
+        pattern = pattern == null ? DEFAULT_PATTERN : pattern;
+        return Stream.of(text.split(pattern))
+                     .collect(Collectors.toList());
+    }
+}
