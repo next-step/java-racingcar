527e72692765f959ee338ffd44b5cd665b0cd370
hyeonjinnoh <hyeonjin.noh@sixshop.com>
2023-11-06 12:49:47 +0900

    feat: 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다

--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 public class ResultView {
 
     private static final String RESULT_MESSAGE = "\n실행 결과";
+    private static final String COLON = " : ";
     private static final String TRACKING_SYMBOL = "-";
 
     public static void printResultInfoMessage() {
@@ -13,20 +14,25 @@ public class ResultView {
     }
 
     public static void printRoundResult(List<Car> cars) {
-        printAllPosition(cars);
+        printRoundResultPerCar(cars);
         System.out.println();
     }
 
-    private static void printAllPosition(List<Car> cars) {
-        cars.stream().forEach(ResultView::printPosition);
+    private static void printRoundResultPerCar(List<Car> cars) {
+        cars.stream().forEach(ResultView::printRoundResult);
     }
 
-    private static void printPosition(Car car) {
-        printTrackingSymbol(car.position());
+    private static void printRoundResult(Car car) {
+        printName(car.name());
+        printPosition(car.position());
         System.out.println();
     }
 
-    private static void printTrackingSymbol(int position) {
+    private static void printName(String name) {
+        System.out.print(name + COLON);
+    }
+
+    private static void printPosition(int position) {
         IntStream.range(0, position).forEach(i -> System.out.print(TRACKING_SYMBOL));
     }
 
