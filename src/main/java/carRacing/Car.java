c69987ef901d34dd45d3aea8dccb8520b0aafa90
geunnseung <bsg1017@naver.com>
2023-11-07 02:53:57 +0900

    :recycle: Car 클래스 리팩토링 - carMove 메소드 리팩토링

--- src/main/java/carRacing/Car.java
+++ src/main/java/carRacing/Car.java
@@ -5,13 +5,19 @@ import java.util.Random;
 public class Car {
 
 	private int index = 0;
+	private static final int MINIMUM_TO_MOVE = 4;
+	private static final int BOUND = 10;
 	private Random random = new Random();
-	private int randomNum = random.nextInt(10);
+	private int randomNum = random.nextInt(BOUND);
 
 	public int moveCar(int randomNum) {
-		if (randomNum >= 4) {
+		if (randomNum >= MINIMUM_TO_MOVE) {
 			index++;
 		}
+		return index;
+	}
+
+	public int getIndex() {
 		return index;
 	}
 }
