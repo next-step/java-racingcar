4dd1a1924687fe4e2cdeab70db154376f5ade9e9
kimdg1105 <kimss7334@naver.com>
2023-11-07 20:32:42 +0900

    feat: step4 기능구현-2 (resultView 자동차이름 출력 추가)

--- src/main/java/racing/RacingGameApplication.java
+++ src/main/java/racing/RacingGameApplication.java
@@ -1,16 +1,22 @@
 package racing;
 
+import common.StringSpliter;
+import racing.domain.CarMoveStrategy;
 import racing.domain.RacingGame;
 import racing.view.InputView;
 import racing.view.ResultView;
 
+import java.util.Random;
+
 public class RacingGameApplication {
 
     public static void main(String[] args) {
-        int carCount = InputView.generateCarCountQuestion();
+        String userNameAnswer = InputView.generateUserNameQuestion();
+        String[] userNames = StringSpliter.getSplittedString(userNameAnswer);
+        int carCount = userNames.length;
         int gameCount = InputView.generateGameCountQuestion();
 
-        RacingGame racingGame = new RacingGame(carCount, gameCount);
+        RacingGame racingGame = new RacingGame(userNameAnswer, carCount, gameCount, new CarMoveStrategy(new Random()));
 
         while (racingGame.isProgress()) {
             racingGame.doRacing();
--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -7,17 +7,20 @@ import java.util.List;
 
 public class ResultView {
     private static final String PROGRESS_MARK = "-";
+    private static final String USER_NAME_POSTFIX = " : ";
 
     private ResultView() {
     }
 
     public static void printProgressView(RacingGame racingGame) {
         List<Car> racingCarList = racingGame.getRacingCars();
-        racingCarList.forEach(o -> printProgress(o.getPosition()));
+        racingCarList.forEach(o -> printProgress(o.getUserName(), o.getPosition()));
     }
 
-    private static void printProgress(int position) {
+    private static void printProgress(String userName, int position) {
         StringBuilder sb = new StringBuilder();
+
+        sb.append(userName).append(USER_NAME_POSTFIX);
         for (int i = 0; i < position; i++) {
             sb.append(PROGRESS_MARK);
         }
