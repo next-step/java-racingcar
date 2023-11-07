96c9bb0401802bf1b7adbfdb86de526cc5cc2ae1
jane096 <jes7077@naver.com>
2023-11-08 00:32:54 +0900

    테스트코드 수정

--- src/test/java/step3/RacingCarTests.java
+++ src/test/java/step3/RacingCarTests.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import step3.model.Car;
+import step3.model.Race;
+import step3.model.RaceBoard;
+import step3.view.RaceResultView;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -17,21 +21,21 @@ public class RacingCarTests {
     @ValueSource(ints = {3})
     @DisplayName("랜덤으로 뽑혔던 숫자가 4보다 작을 경우 false를 반환한다.")
     void canRaceStart(int times) {
-        assertThat(Car.isMovable(times)).isFalse();
+        assertThat(new Car().isMovable(times)).isFalse();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5})
     @DisplayName("입력한 자동차 대수 크기에 맞게 레이스 보드 배열이 생성된다.")
     void generateBoard(int cars) {
-        assertThat(new RaceBoard(cars).generate().length).isEqualTo(5);
+        assertThat(new RaceBoard(cars).generate().size()).isEqualTo(5);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3})
-    @DisplayName("입력한 자동차 대수가 3대라면 초기 레이스 보드는 [-, -, -] 형태이다.")
+    @DisplayName("입력한 자동차 대수가 3대라면 초기 레이스 보드는 3이다.")
     void generateBoardArrays(int cars) {
-        assertThat(Arrays.toString(new RaceBoard(cars).generate())).isEqualTo("[-, -, -]");
+        assertThat(new RaceBoard(cars).generate().size()).isEqualTo(3);
     }
 
     @Test
@@ -46,30 +50,30 @@ public class RacingCarTests {
     @CsvSource(value = {"2:5"}, delimiter = ':')
     @DisplayName("4 이상의 랜덤숫자가 나왔다면 자동차를 1칸 전진시킨다.")
     void carMoves(int cars, int randomValue) {
-        String[] board = new RaceBoard(cars).generate();
-        assertThat(Car.isMovable(randomValue)).isTrue();
+        List<Car> carList = new RaceBoard(cars).generate();
+        assertThat(new Car().isMovable(randomValue)).isTrue();
 
-        String carMoved = new Car(board[cars- 1]).go();
-        assertThat(carMoved).isEqualTo("--");
+        carList.get(0).go();
+        assertThat(carList.get(0).getCurrentStatus()).isEqualTo(2);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"2:3"}, delimiter = ':')
     @DisplayName("4보다 작은 랜덤숫자가 나왔다면 자동차를 전진시키지 않는다.")
     void carDoNotMove(int cars, int randomValue) {
-        String[] board = new RaceBoard(cars).generate();
-        assertThat(Car.isMovable(randomValue)).isFalse();
+        List<Car> carList = new RaceBoard(cars).generate();
+        assertThat(new Car().isMovable(randomValue)).isFalse();
 
-        assertThat(Arrays.toString(board)).isEqualTo("[-, -]");
+        assertThat(carList.get(0).getCurrentStatus()).isEqualTo(1);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3})
     @DisplayName("결과를 담은 배열 값이 출력되는지 검증한다.")
     void showResult(int cars) {
-        String[] board = new RaceBoard(cars).generate();
+        List<Car> carList = new RaceBoard(cars).generate();
 
-        RaceResult raceResult = mock(RaceResult.class);
+        RaceResultView raceResult = mock(RaceResultView.class);
         raceResult.show();
         verify(raceResult, times(1));
     }
