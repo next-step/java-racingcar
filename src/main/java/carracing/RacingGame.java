0cfec31f18f18aac9c5da7baad160dd1f3f7da06
chocheolhee <whcjfgml92@naver.com>
2023-11-07 22:43:59 +0900

    feat: 자동차에 이름을 부여할 수 있고 5글자 초과 시 에러를 발생한다.

--- src/main/java/carracing/RacingGame.java
+++ src/main/java/carracing/RacingGame.java
@@ -3,7 +3,11 @@ package carracing;
 import carracing.ui.ResultView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static carracing.util.InputValueValidate.splitCommaInputCarName;
+import static carracing.util.InputValueValidate.verifyExceedFiveCarName;
 
 public class RacingGame {
 
@@ -11,28 +15,46 @@ public class RacingGame {
     private final int trialCount;
     private final RandomNumberGenerator random = new RandomNumberGenerator();
 
-    public RacingGame(int carCount, int trialCount) {
-        this.carList = cars(negativeNumberCheck(carCount));
+    public RacingGame(String carName, int trialCount) {
+        this.carList = cars(carName);
         this.trialCount = negativeNumberCheck(trialCount);
     }
 
     public void gameStart() {
         for (int i = 0; i < trialCount; i++) {
-            for (Car car : carList) {
-                ResultView.print(car.getPosition());
-                car.move(random.getRandomNumber());
-            }
+            racingGameProcess();
             System.out.println();
         }
     }
 
-    private List<Car> cars(int carCount) {
+    private List<Car> cars(String carName) {
+        return makeCarList(carName);
+    }
+
+    private void racingGameProcess() {
+        for (Car car : carList) {
+            ResultView.print(car.getPosition());
+            car.move(random.getRandomNumber());
+        }
+    }
+
+    private static List<Car> makeCarList(String carName) {
+        String[] names = splitCommaInputCarName(carName);
+        checkExceedFiveCarName(names);
+        System.out.println(Arrays.toString(names));
+
         List<Car> carList = new ArrayList<>();
-        for (int i = 0; i < carCount; i++) {
+        for (int i = 0; i < names.length; i++) {
             Car car = new Car();
             carList.add(car);
         }
         return carList;
+    }
+
+    private static void checkExceedFiveCarName(String[] names) {
+        for (String name : names) {
+            verifyExceedFiveCarName(name);
+        }
     }
 
     private static int negativeNumberCheck(int number) {
--- src/test/java/carracing/CarRacingTest.java
+++ src/test/java/carracing/CarRacingTest.java
@@ -3,8 +3,9 @@ package carracing;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static carracing.util.InputValueValidate.splitCommaInputCarName;
+import static carracing.util.InputValueValidate.verifyExceedFiveCarName;
+import static org.assertj.core.api.Assertions.*;
 
 public class CarRacingTest {
 
@@ -39,19 +40,38 @@ public class CarRacingTest {
     @Test
     @DisplayName("사용자가 자동차 게임 실행 후 음수를 입력하면 RuntimeException이 발생한다.")
     public void negative_validate() throws Exception {
-        assertThatThrownBy(() -> new RacingGame(-1, -4))
+        String carName = "abc";
+        int trialCount = -4;
+        assertThatThrownBy(() -> new RacingGame(carName, trialCount))
                 .isInstanceOf(RuntimeException.class);
     }
 
     @Test
     @DisplayName("0부터 9 사이의 random 숫자 생성")
-    public void createRandomNumber() {
+    public void create_random_number() {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
         for (int i = 0; i < 50; i++) {
             int randomNumber = randomNumberGenerator.getRandomNumber();
             assertThat(randomNumber).isBetween(0, 9);
         }
-
     }
+
+    @Test
+    @DisplayName("쉼표 기준으로 자동차의 이름 3개 입력 시 배열 크기 확인")
+    public void split_comma_car_name() {
+        String carName = "a,bb,ccc";
+
+        String[] names = splitCommaInputCarName(carName);
+
+        assertThat(names).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("자동차의 이름은 5를 초과할 수 없다.")
+    public void exceed_five_car_name_validate() {
+        assertThatThrownBy(() -> verifyExceedFiveCarName("abcde"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
 }
