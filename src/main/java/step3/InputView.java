237a7aa42148395b81563263e26731e53d6e387e
hyeongiii <hyeongiii0811@gmail.com>
2023-11-07 23:17:17 +0900

    feat: 입력 값 받기 기능 구현 (step3)

---
+++ src/main/java/step3/InputView.java
@@ -0,0 +1,37 @@
+package step3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+    protected static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    protected static final String QUESTION_ABOUT_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
+    protected static final String QUESTION_ABOUT_ATTEMPT_COUNT = "시도할 회수는 몇 회 인가요?";
+
+    public int inputCarCount() {
+        int carCount = 0;
+        System.out.println(QUESTION_ABOUT_CAR_COUNT);
+        try {
+            carCount = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return carCount;
+    }
+
+    public int inputAttemptCount()  {
+        int attemptCount = 0;
+        System.out.println(QUESTION_ABOUT_ATTEMPT_COUNT);
+
+        try {
+            attemptCount = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return attemptCount;
+    }
+
+}
