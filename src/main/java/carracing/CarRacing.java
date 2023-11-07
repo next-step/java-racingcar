cffcc0e7f394b7e0e8d090cf3c6b5e9e7e2220da
jeseungkim <qazwsx3745@naver.com>
2023-11-07 20:44:39 +0900

    feat: racing game first submit

--- src/main/java/carracing/CarRacing.java
+++ src/main/java/carracing/CarRacing.java
@@ -1,41 +1,62 @@
 package carracing;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class CarRacing {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("자동차 대수는 몇 대 인가요?");
-        String carNumber = scanner.nextLine();
+        int carNumber = carInputProcess(scanner.nextLine());
         System.out.println("시도할 회수는 몇 회 인가요?");
-        String tryNumber = scanner.nextLine();
+        int tryNumber = carInputProcess(scanner.nextLine());
 
+        letsRace(tryNumber, carNumber);
+    }
+
+    private static void letsRace(int tryNumber, int carNumber) {
+        Random random = new Random();
+        String[] carGarage = new String[carNumber];
+        Arrays.fill(carGarage, "");
+
+        System.out.println();
         System.out.println("실행 결과");
 
+        for (int i = 0; i < tryNumber; i++) {
 
-
+            for (int j = 0; j < carNumber; j++) {
+                int randomRun = random.nextInt(10);
+                if (randomRun >= 4) {
+                    carGarage[j] += '-';
+                }
+                System.out.println(carGarage[j]);
+            }
+            System.out.println();
+        }
     }
-
 
     public static int carInputProcess(String text) {
-//        int result = 0;
+        try {
+            if (isBlank(text)) {
+                return 0;
+            }
+            if (text.contains("-")) throw new IllegalArgumentException();
 
-        if(isBlank(text)) {
-            return 0;
+            return toInt(text);
+        } catch (NumberFormatException e) {
+            System.out.println("숫자만 입력 가능합니다." + e.getMessage());
         }
-
-        if(text.length() > 1) {
-            return toInt(text.substring(0,1));
-        }
-
         return toInt(text);
     }
+
 
     private static boolean isBlank(String text) {
         return text == null || text.isBlank();
     }
+
     private static int toInt(String text) {
         return Integer.parseInt(text);
     }
--- src/test/java/carracing/CarRacingTest.java
+++ src/test/java/carracing/CarRacingTest.java
@@ -1,11 +1,27 @@
+
 package carracing;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static carracing.CarRacing.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarRacingTest {
+
+
+    @Test
+    void isMinus() {
+        assertThatThrownBy(() -> carInputProcess("-1")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void inputValid() {
+        assertThatThrownBy(() -> carInputProcess("dd")).isInstanceOf(NumberFormatException.class);
+    }
+
     @Test
     void normalReturn() {
         assertThat(carInputProcess("1")).isEqualTo(1);
