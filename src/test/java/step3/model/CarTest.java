b88c40818234592759a72d349516b32fe91b1cc4
jinyounglee <binaryzero@naver.com>
2023-11-07 18:27:17 +0900

    TEST: test코드 추가

--- src/test/java/step3/model/CarTest.java
+++ src/test/java/step3/model/CarTest.java
@@ -1,16 +1,18 @@
 package step3.model;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CarTest {
 
-    @Test
-    @DisplayName("자동차가 움직이면 -로 측정한다.")
-    void 자동차가_전진하면_bar로_측정한다() {
-        Car car = new Car();
-//        Assertions.assertThat(car.moving(new Moving())).isEqualTo("-");
+    Car 한칸_움직인_차 = new Car();
+
+    @BeforeEach
+    void init() {
+        한칸_움직인_차.move(4);
     }
 
     @Test
@@ -18,7 +20,7 @@ class CarTest {
     void 차는_한칸씩_전진한다() {
         Car car = new Car();
         car.move(5);
-        Assertions.assertThat(car.getMovingDistance()).isEqualTo("-");
+        assertThat(car.getMovingDistance()).isEqualTo("-");
     }
 
     @Test
@@ -26,7 +28,22 @@ class CarTest {
     void 차는_4_를_얻어도_한칸_이동한다() {
         Car car = new Car();
         car.move(4);
-        Assertions.assertThat(car.getMovingDistance()).isEqualTo("-");
+        assertThat(car.getMovingDistance()).isEqualTo("-");
+    }
+
+    @Test
+    @DisplayName("4미만을 받으면 찾는 움직이지 않는다.")
+    void 차는_4미만의_값을_얻으면_이동하지_않는다() {
+        Car car = new Car();
+        car.move(3);
+        assertThat(car.getMovingDistance()).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("기존에 한칸 이동한 차량은 4이상의 값을 얻으면 한칸 더 이동한다. ")
+    void 기존에_전진한_차량은_4이상의_값을_얻으면_한칸_더_이동한다() {
+        한칸_움직인_차.move(4);
+        assertThat(한칸_움직인_차.getMovingDistance()).isEqualTo("--");
     }
 
 }
