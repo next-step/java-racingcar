162bd759c84e42e9ae8e23baa037148bce3a77be
엄태권 <akfls221@gmail.com>
2023-11-08 03:08:25 +0900

    fix : 불필요한 import 제거

--- src/test/java/racingcar/domain/car/PositionTest.java
+++ src/test/java/racingcar/domain/car/PositionTest.java
@@ -3,8 +3,6 @@ package racingcar.domain.car;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
