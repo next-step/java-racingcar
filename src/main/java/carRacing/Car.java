973dec75e15cfa0034527df15f285f0ff240ecf5
geunnseung <bsg1017@naver.com>
2023-11-07 17:02:16 +0900

    :recycle: Car, InputView 리팩토링

--- src/main/java/carRacing/Car.java
+++ src/main/java/carRacing/Car.java
@@ -1,15 +1,12 @@
 package carRacing;
 
-import java.util.Random;
-
 public class Car {
 
 	public int index = 0;
 	private RandomNumMaker ran = new RandomNumMaker();
-	public boolean isRanNumAboveFour = ran.isAboveBoundary();
 
 	public void moveCar() {
-		if (isRanNumAboveFour) {
+		if (ran.isAboveBoundary()){
 			index++;
 		}
 	}
--- src/main/java/carRacing/InputView.java
+++ src/main/java/carRacing/InputView.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class InputView {
 
+	private static final int MINIMUM_NATURAL_NUM = 1;
 	private Scanner sc = new Scanner(System.in);
 
 	public int inputCarCount() {
-		System.out.println("자동차 대수는 몇 대 인가요?");
-		return sc.nextInt();
+		return getValidCount("자동차 대수는 몇 대 인가요?");
 	}
 
 	public int inputMoveCount() {
-		System.out.println("시도할 회수는 몇 회 인가요?");
+		return getValidCount("시도할 회수는 몇 회 인가요?");
+	}
+
+	public int getValidCount(String text) {
+		System.out.println(text);
+		if (sc.nextInt() < MINIMUM_NATURAL_NUM){
+			throw new IllegalArgumentException("1 이상의 자연수를 입력해주세요.");
+		}
 		return sc.nextInt();
 	}
 }
