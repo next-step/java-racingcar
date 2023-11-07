496950eb6fe5956568f1583d546fc1f0881f7f2d
birdie <flaehdan@gmail.com>
2023-11-07 09:29:36 +0900

    test: 입력을 입력받으면 자동차를 생성할 수 있도록 테스트 코드 먼저 작성

--- src/test/java/study/racing/domain/CarsTest.java
+++ src/test/java/study/racing/domain/CarsTest.java
@@ -3,23 +3,48 @@ package study.racing.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class CarsTest {
 
     @Test
-    @DisplayName("자동차들에게 움직이지 않도록 할 수 있다..")
-    void doNotMoveCarsTest() {
+    @DisplayName("이름의 리스트를 입력받으면 자동차들을 생성할 수 있다.")
+    void generateCarsTest() {
         // given
-        Cars cars = new Cars(new CarsNumber("2"));
-        DoNotMoveGenerator doNotMoveGenerator = new DoNotMoveGenerator();
-
+        Names names = new Names(
+                List.of(
+                        new Name("car1"),
+                        new Name("car2"),
+                        new Name("car3")
+                )
+        );
         // when
-        cars.move(doNotMoveGenerator, new TryCounts("1"));
+        Cars cars = new Cars(names);
 
         // then
-        assertThat(cars).isEqualTo(new Cars(new CarsNumber("2")));
+        assertAll(
+                () -> assertThat(cars.getValue().get(0)).isEqualTo(new Car(new Name("car1"), new Position(0))),
+                () -> assertThat(cars.getValue().get(1)).isEqualTo(new Car(new Name("car2"), new Position(0))),
+                () -> assertThat(cars.getValue().get(2)).isEqualTo(new Car(new Name("car3"), new Position(0)))
+        );
     }
+
+//    @Test
+//    @DisplayName("자동차들에게 움직이지 않도록 할 수 있다..")
+//    void doNotMoveCarsTest() {
+//        // given
+//        Cars cars = new Cars(new CarsNumber("2"));
+//        DoNotMoveGenerator doNotMoveGenerator = new DoNotMoveGenerator();
+//
+//        // when
+//        cars.move(doNotMoveGenerator, new TryCounts("1"));
+//
+//        // then
+//        assertThat(cars).isEqualTo(new Cars(new CarsNumber("2")));
+//    }
 
     static class DoNotMoveGenerator implements NumberGenerator {
 
