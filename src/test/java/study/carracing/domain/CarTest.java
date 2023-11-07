3dc815cf6256474d13e32f856d42d0150a09d8cf
hyunyyxp <qqqq_o@naver.com>
2023-11-07 18:32:06 +0900

    test: 전진 조건 전략 패턴 제거에 따른 테스트 코드 수정

--- src/test/java/study/carracing/domain/CarTest.java
+++ src/test/java/study/carracing/domain/CarTest.java
@@ -1,22 +1,21 @@
 package study.carracing.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import study.carracing.util.RandomGeneratorImpl;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9, 10})
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     @DisplayName("random 값이 4이상일 경우 자동차가 1칸 전진한다.")
     void moveCar(int randomValue) {
-        Car car = new Car(new RandomGeneratorImpl());
+        Car car = new Car();
 
-        car.go(randomValue);
+        car.move(randomValue);
 
         int position = car.getPosition();
         assertThat(position).isEqualTo(2);
@@ -26,9 +25,9 @@ class CarTest {
     @ValueSource(ints = {0, 1, 2, 3})
     @DisplayName("random 값이 4미만일 경우 자동차는 움직이지 않는다.")
     void dontMoveCar(int randomValue) {
-        Car car = new Car(new RandomGeneratorImpl());
+        Car car = new Car();
 
-        car.go(randomValue);
+        car.move(randomValue);
 
         int position = car.getPosition();
         assertThat(position).isEqualTo(1);
@@ -38,12 +37,11 @@ class CarTest {
     @CsvSource(value = {"1:'--'", "2:'---'", "3:'----'", "4:'-----'", "5:'------'"}, delimiter = ':')
     @DisplayName("차가 전진 회수만큼 대쉬(-)가 출력된다.")
     void carToString(int forward, String expected) {
-        Car car = new Car(new RandomGeneratorImpl());
+        Car car = new Car();
 
         for (int i = 0; i < forward; i++) {
-            car.go(4);
+            car.move(4);
         }
-
 
         String actual = car.toString();
         assertThat(actual).isEqualTo(expected);
