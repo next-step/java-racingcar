8ca39356e383857baafb75bea6eb2a449521b1cd
areumj0507 <areumj0507@gmail.com>
2023-11-07 21:48:25 +0900

    feat: 메소드 분리

--- src/main/java/study/nextstep/StringAddCalculator.java
+++ src/main/java/study/nextstep/StringAddCalculator.java
@@ -1,40 +1,62 @@
 package study.nextstep;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    private static final String prefix = "//";
     private static final String suffix = "\n";
 
     public static int splitAndSum(String example) {
-        if(example.isEmpty()) {
+        if(example == null || example.isEmpty()) {
             return 0;
         }
 
-        return Arrays.stream(numberArray(example))
-                .map(str -> {
-                    if(!Character.isDigit(str.charAt(0))) {
-                        throw new RuntimeException();
-                    }
-
-                    if(Integer.parseInt(str) < 0) {
-                        throw new RuntimeException();
-                    }
-
-                    return Integer.parseInt(str);
+        return numberList(example)
+                .stream()
+                .map(num -> {
+                    checkPositiveNumber(num);
+                    return Integer.parseInt(num);
                 }).reduce(0, Integer::sum);
     }
 
-    private static String[] numberArray(String example) {
-        StringBuilder separators = new StringBuilder("[,|:");
+    private static List<String> numberList(String example) {
+        Map<String, String> result = separatorAndTrimText(example);
+        return Arrays.asList(result.get("text")
+                .split(result.get("separator")));
+    }
 
-        if(example.contains(prefix) && example.contains(suffix)) {
-            separators.append("|")
-                    .append(example, example.indexOf(prefix) + prefix.length(), example.indexOf(suffix));
+    private static Map<String, String> separatorAndTrimText(String example) {
+        Map<String, String> result = new HashMap<>();
+        StringBuilder separators = new StringBuilder("[,|:");
+        String separator = customSeperator(example);
+
+        if(!separator.isEmpty()) {
+            separators.append("|").append(separator);
             example = example.substring(example.indexOf(suffix)).trim();
         }
-        separators.append("]");
 
-        return example.split(separators.toString());
+        separators.append("]");
+        result.put("separator", separators.toString());
+        result.put("text", example);
+        return result;
+    }
+
+    private static String customSeperator(String example) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(example);
+        return m.find() ? m.group(1) : "";
+    }
+
+    private static void checkPositiveNumber(String number) {
+        if(!Character.isDigit(number.charAt(0))) {
+            throw new RuntimeException();
+        }
+
+        if(Integer.parseInt(number) < 0) {
+            throw new RuntimeException();
+        }
     }
 }
