17b3ca3459a6c8fa7df393d708e3bc926021ceac
ch200203 <ch200205@naver.com>
2023-11-07 22:24:51 +0900

    test : 테스트 코드 분리 및 추가

--- src/test/java/step3/RacingCarTest.java
+++ src/test/java/step3/RacingCarTest.java
@@ -6,27 +6,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import step3.domain.Car;
-import step3.domain.CarFactory;
+import step3.domain.RacingGame;
+import step3.domain.Round;
 import step3.utils.RandomUtils;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarTest {
 
     @Test
-    @DisplayName("사용자가 입력한 숫자만큼 자동차가 생성되야 한다.")
-    void initCarsInputNumber() {
-        List<Car> cars = CarFactory.createCars(3);
-        assertThat(cars).hasSize(3);
-    }
-
-    @Test
-    @DisplayName("생성된 자동차의 기본 포지션은 1 이어야 한다.")
+    @DisplayName("생성된 자동차의 기본 포지션은 0 이어야 한다.")
     void initCarPositionIs1() {
         Car car = new Car();
-        assertThat(car.getCarPosition()).isEqualTo(1);
+        assertThat(car.getCarPosition()).isEqualTo(0);
     }
 
     @Test
@@ -39,11 +35,26 @@ public class RacingCarTest {
 
     @ParameterizedTest
     @DisplayName("4 이상인 경우 자동차는 전진한다.")
-    @CsvSource(value = {"1,1", "4, 2", "9, 2"}, delimiter = ',')
+    @CsvSource(value = {"1,0", "4, 1", "9, 1"}, delimiter = ',')
     void racingCarMoveForward(int value, int expected) {
         Car car = new Car();
         car.tryMove(value);
         assertThat(car.getCarPosition()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("라운드 별로 자동차의 상태가 개별로 저장 되어야 한다.")
+    void eachRoundHaveOwnCars() {
+        int tryCountNumber = 3;
+        int racingCarCountNumber = 3;
+        RacingGame racingGame = new RacingGame(racingCarCountNumber, tryCountNumber);
+        racingGame.play();
+        List<Round> rounds = racingGame.getRounds();
+        Set<String> address = new HashSet<>();
+        rounds.stream().forEach(round ->
+                round.getRoundCarStatus().getCars().forEach(car -> address.add(car.toString())));
+
+        assertThat(address).hasSize(tryCountNumber * racingCarCountNumber);
     }
 
 }
