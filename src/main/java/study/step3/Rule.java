42641e2171efbe833d75ecd00c48eb6bc33b6efc
gilbertlim <9ilbert.app@gmail.com>
2023-11-07 12:01:26 +0900

    feature: 입력 값 validation 로직 추가

--- src/main/java/study/step3/Rule.java
+++ src/main/java/study/step3/Rule.java
@@ -6,6 +6,7 @@ public class Rule {
     private final int movingTimes;
 
     public Rule(int racingLines, int movingTimes) {
+        validate(racingLines, movingTimes);
         this.numberOfCars = racingLines;
         this.movingTimes = movingTimes;
     }
@@ -17,4 +18,23 @@ public class Rule {
     public int getMovingTimes() {
         return movingTimes;
     }
+
+    private static void validate(int racingLines, int movingTimes) {
+        if (!isValidRacingLines(racingLines)) {
+            throw new RacingException("RuleValidationError: 자동차 대수는 2대 이상이여야 합니다.");
+        }
+        if (!isValidMovingTimes(movingTimes)) {
+            throw new RacingException("RuleValidationError: 시도 회수는 최소 1회 이상이어야 합니다.");
+        }
+    }
+
+    private static boolean isValidRacingLines(int racingLines) {
+        return racingLines >= 2;
+    }
+
+    private static boolean isValidMovingTimes(int movingTimes) {
+        return movingTimes >= 1;
+    }
+
+
 }
