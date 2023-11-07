fd0b4539ed5863005b1badea0c35b739f44b6876
pilming44 <pilming44@naver.com>
2023-11-08 00:35:10 +0900

    feat: InputView racerNames추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -13,6 +13,10 @@ public class InputView {
         inputPrintln("시도할 회수는 몇 회 인가요?");
         return scanner.nextInt();
     }
+    public static String racerNames() {
+        inputPrintln("경주에 참가하는 레이서 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine();
+    }
     private static void inputPrintln(String message) {
         System.out.println(message);
     }
