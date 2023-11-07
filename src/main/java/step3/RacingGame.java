5529ad7cf6d0708f311e3cecd0de1f0d6a190453
Hanna Choi <44824602+gkssk4163@users.noreply.github.com>
2023-11-07 23:47:54 +0900

    refactor: static 변수 제거

--- src/main/java/step3/RacingGame.java
+++ src/main/java/step3/RacingGame.java
@@ -8,27 +8,29 @@ import java.util.Random;
 
 public class RacingGame {
 
-    static RacingCar[] racingCars;
-
     public static void gameStart() {
         int carNum = InputView.askNumberOfCars();
         int attemptNum = InputView.askNumberOfAttempts();
 
         // 초기화
-        racingCars = new RacingCar[carNum];
-        for (int carNo = 0; carNo < carNum; carNo++) {
-            racingCars[carNo] = new RacingCar();
-        }
+        RacingCar[] racingCars = new RacingCar[carNum];
+        initRacingCars(racingCars);
 
         // 레이싱 진행
         ResultView.title();
         for (int attempt = 0; attempt < attemptNum; attempt++) {
-            racing();
+            racing(racingCars);
             ResultView.printResult(racingCars);
         }
     }
 
-    private static void racing() {
+    private static void initRacingCars(RacingCar[] racingCars) {
+        for (int carNo = 0; carNo < racingCars.length; carNo++) {
+            racingCars[carNo] = new RacingCar();
+        }
+    }
+
+    private static void racing(RacingCar[] racingCars) {
         for (RacingCar racingCar : racingCars) {
             moveCar(racingCar);
         }
