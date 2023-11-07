da4853316a256dcb32a4e07c3a55b7650d9bb2ea
ms-93@hanmail.net <ms-93@hanmail.net>
2023-11-08 06:36:29 +0900

    test: 테스트 리소스 반영

--- src/test/java/racingcar/ValidationCheckTest.java
+++ src/test/java/racingcar/ValidationCheckTest.java
@@ -2,27 +2,42 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.service.ValidationCheck;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class InputViewTest {
-//    @Test
-//    @DisplayName("입력값이 null이나 빈문자가 들어오면 오류가 나는지 테스트")
-//    public void ValidationCheck_null_빈문자() {
-//        assertThatThrownBy(() -> ValidationCheck.checkInputCarCount(null)).isInstanceOf(NullPointerException.class);
-//    }
-//
-//    @Test
-//    @DisplayName("입력값이 음수값이 들어오면 오류가 나는지 테스트")
-//    public void ValidationCheck_음수() {
-//        assertThatThrownBy(() -> ValidationCheck.checkInputCarCount(-1)).isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//    @Test
-//    @DisplayName("입력값이 숫자가 아닌 문자가 들어오면 오류가 나는지 테스트")
-//    public void ValidationCheck_문자() {
-//        String input = "a";
-//        assertThatThrownBy(() -> ValidationCheck.checkInputCarCount(Integer.parseInt(input))).isInstanceOf(IllegalArgumentException.class);
-//    }
+public class ValidationCheckTest {
+    @ParameterizedTest
+    @DisplayName("입력된 자동차 이름은 5자를 초과할 수 없다.")
+    @ValueSource(strings = {"zbcdef", "123455"})
+    public void ValidationCheck_자동차이름_글자수(String input) {
+        assertThatThrownBy(() -> ValidationCheck.checkInputCarName(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자를 초과할 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName(("입력된 자동차 이름은 공백이나 null이 될 수 없다."))
+    @NullAndEmptySource
+    public void ValidationCheck_자동차이름_공백_null(String input) {
+        assertThatThrownBy(() -> ValidationCheck.checkInputCarName(input))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessageContaining("자동차 이름은 공백이 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("시도할 횟수는 음수가 될 수 없다.")
+    public void ValidationCheck_시도횟수_음수() {
+        assertThatThrownBy(() -> ValidationCheck.checkInputTryCount(-1)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("시도할 횟수에 숫자가 아닌 문자가 들어올 수 없다.")
+    public void ValidationCheck_시도횟수_문자() {
+        String input = "a";
+        assertThatThrownBy(() -> ValidationCheck.checkInputTryCount(Integer.parseInt(input))).isInstanceOf(IllegalArgumentException.class);
+    }
 }
