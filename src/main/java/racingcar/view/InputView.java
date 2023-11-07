d1f6c501a5ab982de7ffaab354731b3b92c5b101
jeonghuntech <jeonghun.tech@gmail.com>
2023-11-08 03:44:19 +0900

    refactor:

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,6 +6,12 @@ public class InputView {
     private static final String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
     private static final String ATTEMPT_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
 
+    private final Scanner scanner;
+
+    public InputView() {
+        scanner = new Scanner(System.in);
+    }
+
     public int readCarCount() {
         System.out.println(CAR_COUNT_MESSAGE);
         return readCount();
@@ -17,7 +23,6 @@ public class InputView {
     }
 
     private int readCount() {
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 }
