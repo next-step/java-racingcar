6c3aa41acee72c2bb5704a483334fa5df664e94f
엄태권 <akfls221@gmail.com>
2023-11-08 04:32:30 +0900

    feat : view 화면 우승자 출력 기능 추가

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -3,6 +3,8 @@ package racingcar.view;
 import racingcar.domain.car.Car;
 import racingcar.domain.car.Cars;
 
+import java.util.List;
+
 public class ResultView {
 
     private static final String DEFAULT_PRINT_PATTERN = "-";
@@ -15,5 +17,9 @@ public class ResultView {
             System.out.println(car.getCarName() + " : " + DEFAULT_PRINT_PATTERN.repeat(car.getPosition()));
         }
         System.out.println("===========================================");
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println(String.join(", ", winners) + "가 최종 우승했습니다.");
     }
 }
