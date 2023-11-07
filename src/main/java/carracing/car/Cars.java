efdb1f472b79644d6e648ad76eace265f79c8e84
sunggyupaik <melon9751@naver.com>
2023-11-07 22:19:14 +0900

    [feat] 자동차 생성의 유효성을 추가하라
    경주 우승자를 확인할 때 아무런 자동차도 입력받지 않으면 예외를 던지는
    유효성을 추가합니다

--- src/main/java/carracing/car/Cars.java
+++ src/main/java/carracing/car/Cars.java
@@ -6,6 +6,10 @@ public class Cars {
 	private final List<Car> cars;
 
 	public Cars(List<Car> cars) {
+		if (cars.isEmpty()) {
+			throw new IllegalArgumentException("자동차는 최소 1대 이상이어야 합니다");
+		}
+
 		this.cars = cars;
 	}
 
--- src/test/java/carracing/car/CarsTest.java
+++ src/test/java/carracing/car/CarsTest.java
@@ -2,11 +2,21 @@ package carracing.car;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CarsTest {
+	@Test
+	void newObject_emptyCars_throwsException() {
+		List<Car> emptyCars = new ArrayList<>();
+
+		assertThatThrownBy(() -> new Cars(emptyCars))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
 	@Test
 	void winners_threeCars_oneWinners() {
 		Car car1 = new Car(10, "abc");
