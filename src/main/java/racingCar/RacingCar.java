4842b08c10b30d5e2cf5a677c608bcd69ca30c16
sunga <eert2260@naver.com>
2023-11-08 21:07:35 +0900

    feat : 조건에 따른 정지 또는 전진 결과 반환

---
+++ src/main/java/racingCar/RacingCar.java
@@ -0,0 +1,9 @@
+package racingCar;
+
+public class RacingCar {
+
+    public static int moveCar(int randomValue){
+        if(randomValue<=3) return 0;
+        return 1;
+    }
+}
--- src/test/java/racingCar/racingCarTest.java
+++ src/test/java/racingCar/racingCarTest.java
@@ -2,10 +2,19 @@ package racingCar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class racingCarTest {
+
+    @ParameterizedTest
+    @DisplayName("입력값이 3이하일 경우 0(정지)을 반환하고, 4이상일 경우 1(전진)을 반환한다.")
+    @CsvSource(value = {"0:0","1:0","2:0","3:0","4:1","5:1","6:1","7:1","8:1","9:1"}, delimiter = ':')
+    public void 정지_또는_전진_조건_테스트(int input, int expected){
+        assertThat(RacingCar.moveCar(input)).isEqualTo(expected);
+    }
 
     @Test
     @DisplayName("0에서 9사이의 랜덤값을 반환한다.")
