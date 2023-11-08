dc2df458bcce506b490fba749db98b767e219253
choomi1217 <whdudal1217@naver.com>
2023-11-08 17:57:09 +0900

    style: 클래스 내부 요소 정렬

--- src/main/java/study/racingcar/Car.java
+++ src/main/java/study/racingcar/Car.java
@@ -2,16 +2,16 @@ package study.racingcar;
 
 public class Car {
 
-    private final RandomGenerator randomGenerator;
-
     private static final int MOVE_CONDITION = 4;
+
+    private final RandomGenerator randomGenerator;
 
     public Car(RandomGenerator randomGenerator) {
         this.randomGenerator = randomGenerator;
     }
 
     public MoveStatus canMove() {
-        if (pickRandomValue() >= MOVE_CONDITION){
+        if (pickRandomValue() >= MOVE_CONDITION) {
             return MoveStatus.MOVE;
         }
         return MoveStatus.STOP;
