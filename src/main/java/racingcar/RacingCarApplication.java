c3d1167302fe6000bb57f0d7f3a975ad623c947c
h3yon <khykhy1006@gmail.com>
2023-11-09 02:27:57 +0900

    refactor: makeCars 로직 이동

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -4,19 +4,12 @@ import racingcar.domain.Car;
 import racingcar.domain.RaceCommand;
 import racingcar.view.ResultView;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import static racingcar.view.InputView.inputRaceCommand;
 
 public class RacingCarApplication {
     public static void main(String[] args) {
         RaceCommand raceCommand = inputRaceCommand();
-        List<Car> cars = IntStream.range(0, raceCommand.getCarCount())
-                                  .mapToObj(i -> new Car(0))
-                                  .collect(Collectors.toList());
-        ResultView.startAndDisplay(cars,
+        ResultView.startAndDisplay(Car.makeCarsBy(raceCommand.getCarCount()),
                                    raceCommand.getTryCount());
     }
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,11 +1,21 @@
 package racingcar.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Car {
     private final static int MOVE_CONDITION = 4;
     private int position;
 
     public Car(int position) {
         this.position = position;
+    }
+
+    public static List<Car> makeCarsBy(int carCount) {
+        return IntStream.range(0, carCount)
+                        .mapToObj(i -> new Car(0))
+                        .collect(Collectors.toList());
     }
 
     public int getPosition() {
