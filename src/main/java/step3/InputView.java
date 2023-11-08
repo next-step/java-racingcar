5994a683de1f81431df201033ac4b4b7e99f4a71
hyeongiii <hyeongiii0811@gmail.com>
2023-11-08 23:55:40 +0900

    refactor: InputView 입력 로직 단순화 (step3)

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -1,37 +1,22 @@
 package step3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class InputView {
-    protected static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    protected static final Scanner scanner = new Scanner(System.in);
     protected static final String QUESTION_ABOUT_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
     protected static final String QUESTION_ABOUT_ATTEMPT_COUNT = "시도할 회수는 몇 회 인가요?";
+    
+    private InputView() {}
 
     public static int inputCarCount() {
-        int carCount = 0;
         System.out.println(QUESTION_ABOUT_CAR_COUNT);
-        try {
-            carCount = Integer.parseInt(br.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return carCount;
+        return scanner.nextInt();
     }
 
     public static int inputAttemptCount()  {
-        int attemptCount = 0;
         System.out.println(QUESTION_ABOUT_ATTEMPT_COUNT);
-
-        try {
-            attemptCount = Integer.parseInt(br.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return attemptCount;
+        return scanner.nextInt();
     }
 
 }
