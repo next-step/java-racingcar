2afbe369ea472daa0a6606b6916acb7023fa48b7
choomi1217 <whdudal1217@naver.com>
2023-11-09 01:53:39 +0900

    refactor: 임포트 정리

--- src/test/java/study/racingcar/RandomGeneratorTest.java
+++ src/test/java/study/racingcar/RandomGeneratorTest.java
@@ -1,18 +1,16 @@
-package study.racingcar.fake;
+package study.racingcar;
 
+import java.util.Random;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-
-import java.util.Random;
-import study.racingcar.RandomGenerator;
 
 public class RandomGeneratorTest {
 
     @DisplayName("0에서 9 사이에서 random 값 생성")
     @RepeatedTest(100)
-    void generateRandomValueBetweenZeroToNine(){
+    void generateRandomValueBetweenZeroToNine() {
         RandomGenerator randomGenerator = new RandomGenerator(new Random());
-        Assertions.assertThat(randomGenerator.number()).isBetween(1,10);
+        Assertions.assertThat(randomGenerator.number()).isBetween(1, 10);
     }
 }
