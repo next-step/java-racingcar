025dfbfa0cf46d5d9a838a5d7a9e16df3457fdb8
frank-jeongwooshin <maecsan12@gmail.com>
2023-11-08 23:36:33 +0900

    step2-Second Trial

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,75 +2,66 @@ package calculator;
 
 public class Calculator {
 
-    public static final String DELIMITER = ",|:";
-    public static int cal(String text)
-    {
-        if(isBlank(text))
-        {
+    private static final String DELIMITER = ",|:";
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int CUSTOM_TEXT_INDEX = 2;
+
+    public static int calculate(String text) {
+        if(isBlank(text)) {
             return 0;
         }
         return sum(toInts(split(text)));
-    }
-
-    private static String[] split(String text)
-    {
-        if(isCustom(text))
-        {
-            return customSplit(text);
-        }
-        return text.split(DELIMITER);
-    }
-
-    private static boolean isCustom(String text)
-    {
-        if(text.split("//|\n").length == 3)
-        {
-            return true;
-        }
-        return false;
-    }
-
-    private static String[] customSplit(String text)
-    {
-        String[] textList = text.split("//|\n");
-        return textList[2].split(textList[1]);
     }
 
     private static boolean isBlank(String text) {
         return text == null || text.isBlank();
     }
 
-    private static int sum(int[] numbers)
-    {
+    private static String[] split(String text) {
+        if(isCustom(text)) {
+            return customSplit(text);
+        }
+        return text.split(DELIMITER);
+    }
+
+    private static int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; ++i) {
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
+    }
+
+    private static int sum(int[] numbers) {
         includeMinus(numbers);
         int sum = 0;
-        for(int number: numbers)
-        {
+        for(int number: numbers) {
             sum += number;
         }
         return sum;
     }
 
-    private static void includeMinus(int[] numbers)
-    {
-        try {
-            for (int i : numbers)
-            {
-                System.out.println(i);
-                if(i < 0) throw new RuntimeException("DO NOT INPUT MINUS NUMBER");
-            }
-        } catch (RuntimeException e) {
-            throw new RuntimeException(e);
+    private static boolean isCustom(String text) {
+        if(text.split("//|\n").length == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    private static String[] customSplit(String text) {
+        String[] textList = text.split("//|\n");
+        return textList[CUSTOM_TEXT_INDEX].split(textList[CUSTOM_DELIMITER_INDEX]);
+    }
+
+    private static void includeMinus(int[] numbers) {
+        for (int number : numbers) {
+            validatePositive(number);
         }
     }
 
-    private static int[] toInts(String[] values)
-    {
-        int[] numbers = new int[values.length];
-        for (int i = 0; i < values.length; ++i)
-        {
-            numbers[i] = Integer.parseInt(values[i]);
+    private static void validatePositive(int number){
+        if(number < 0) {
+            throw new IllegalArgumentException("DO NOT INPUT MINUS NUMBER");
         }
-        return numbers;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,53 +1,53 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static calculator.Calculator.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class CalculatorTest
-{
-    // 1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)
-    @Test
-    void null_빈문자열_값()
-    {
-        assertThat(cal(null)).isEqualTo(0);
-        assertThat(cal("")).isEqualTo(0);
+public class CalculatorTest {
+    @DisplayName("문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)")
+    @ParameterizedTest(name = "입력값: {0}")
+    @NullAndEmptySource
+    void null_빈문자열_값(String input) {
+        assertThat(calculate(input)).isEqualTo(0);
     }
 
-    // 2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)
-    @Test
-    void 숫자_하나()
-    {
-        assertThat(cal("1")).isEqualTo(1);
+    @DisplayName("2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)")
+    @ParameterizedTest
+    @CsvSource(value = {"1,1", "0,0", "9,9"})
+    void 숫자_하나(String input, int expected) {
+        assertThat(calculate(input)).isEqualTo(expected);
     }
 
-    // 3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)
-    @Test
-    void 콤마_구분자()
-    {
-        assertThat(cal("1,2,3")).isEqualTo(6);
+    @DisplayName("3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3", "2,3,1,0", "2,2,2"})
+    void 콤마_구분자(String input) {
+        assertThat(calculate(input)).isEqualTo(6);
     }
 
-    // 4. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)
+    @DisplayName("4. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)")
     @Test
-    void 콜론_구분자()
-    {
-        assertThat(cal("1:2:3")).isEqualTo(6);
+    void 콜론_구분자() {
+        assertThat(calculate("1:2:3")).isEqualTo(6);
     }
 
-    // 5. “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\n1;2;3” => 6)
+    @DisplayName("5. “//”와 “\\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\\n1;2;3” => 6)")
     @Test
-    void 커스텀_구분자()
-    {
-        assertThat(cal("//;\n1;2;3")).isEqualTo(6);
+    void 커스텀_구분자() {
+        assertThat(calculate("//;\n1;2;3")).isEqualTo(6);
     }
 
-    // 6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)
+    @DisplayName("6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)")
     @Test
-    void 음수()
-    {
-        assertThatThrownBy(()->cal("-1,2,3")).isInstanceOf(RuntimeException.class);
+    void 음수() {
+        assertThatThrownBy(()-> calculate("-1,2,3")).isInstanceOf(RuntimeException.class);
     }
 }
