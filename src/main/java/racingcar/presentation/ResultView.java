3ee21b6eb1f7571a05341e9089649a06f6f78ef4
yoohojeong <yh132@naver.com>
2023-11-08 16:40:55 +0900

    feat : 이름이 부여된 자동차들의 경주 경로 및 경기 결과 우승자 출력

--- src/main/java/racingcar/presentation/ResultView.java
+++ src/main/java/racingcar/presentation/ResultView.java
@@ -1,24 +1,42 @@
 package racingcar.presentation;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
 
-public class OutputView {
+public class ResultView {
 
         private static final String PATH_SIGN = "-";
+        private static final String BETWEEN_NAME_AND_PATH_SIGN = " : ";
         private static final String NEW_LINE = "\n";
+        private static final String COMMA = ",";
+        private static final String RESULT_VIEW_TITLE = "실행결과";
 
-        private OutputView() {
+        private ResultView() {
+        }
+
+        public static void printResultViewTitle(){
+                System.out.println(NEW_LINE + RESULT_VIEW_TITLE);
         }
 
         public static void printPositionForAllCarsInCurrentRound(List<Car> cars) {
-                cars.forEach(car-> printCurrentPosition(car.getCurrentPosition()));
+                cars.forEach(ResultView::printCurrentPosition);
                 printNewLine();
         }
 
-        private static void printCurrentPosition(int currentPosition) {
-                PATH_SIGN.repeat(currentPosition);
+        public static void printWinner(List<Car> cars){
+                String winnersInOneLine = cars.stream().map(Car::getName).collect(Collectors.joining(COMMA));
+                System.out.println(winnersInOneLine + "가 최종 우승했습니다.");
+        }
+
+        private static void printCurrentPosition(Car car) {
+                printName(car.getName());
+                System.out.print(PATH_SIGN.repeat(car.getCurrentPosition()));
                 printNewLine();
+        }
+
+        private static void printName(String name){
+                System.out.print(name + BETWEEN_NAME_AND_PATH_SIGN);
         }
 
         private static void printNewLine(){
