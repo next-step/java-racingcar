3c076bd7dc72c65b00fda0ebfa7db441e7d749fa
NQ <lol1219@snu.ac.kr>
2023-11-08 17:57:28 +0900

    [STEP2] 문자열 덧셈 계산기 완료 (#4925)
    * feat: [STEP2] 문자열 덧셈 계산기 완료
    
    * refactor : [STEP2] 문자열 덧셈 계산기

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import java.util.Objects;
+
+public class Calculator {
+    private static final int DEFAULT_RETURN_VALUE = 0;
+
+    private Calculator() {}
+
+    public static int sumText(String text) {
+        if (isBlank(text)) {
+            return DEFAULT_RETURN_VALUE;
+        }
+        return sum(TextParser.parsedText(text));
+    }
+
+    private static boolean isBlank(String text) {
+        return Objects.isNull(text) || text.isBlank();
+    }
+
+    private static int sum(int[] intValues) {
+        int sum = DEFAULT_RETURN_VALUE;
+        for (int value : intValues) {
+            sum += value;
+        }
+        return sum;
+    }
+}
---
+++ src/main/java/calculator/TextParser.java
@@ -0,0 +1,46 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TextParser {
+
+    private static final Pattern DELIMITER_PATTERN = Pattern.compile("//(.)\\n(.*)");
+    private static final String DEFAULT_DELIMITER = "[,:]";
+
+    private TextParser() {}
+
+    public static int[] parsedText(String inputString) {
+        return toInt(toSplitText(inputString).split(delimiter(inputString)));
+    }
+
+    private static String delimiter(String text) {
+        String delimiter = DEFAULT_DELIMITER;
+        Matcher matcher = delimiterMatcher(text);
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+        }
+        return delimiter;
+    }
+
+    private static Matcher delimiterMatcher(String text) {
+        return DELIMITER_PATTERN.matcher(text);
+    }
+
+    private static String toSplitText(String text) {
+        Matcher matcher = delimiterMatcher(text);
+        if (matcher.find()) {
+            text = matcher.group(2);
+        }
+        return text;
+    }
+
+    private static int[] toInt(String[] stringValues) {
+        int valueLength = stringValues.length;
+        int[] intValues = new int[valueLength];
+        for (int i = 0; i < valueLength; i++) {
+            intValues[i] = Integer.parseUnsignedInt(stringValues[i]);
+        }
+        return intValues;
+    }
+}
