ab302e2462fc8b09f7465530e1d6689a72acd065
yoohojeong <yh132@naver.com>
2023-11-08 16:42:20 +0900

    test : 자동차경주(우승자) 단위테스트 구현

--- src/test/java/racingcar/RacingCarGameTest.java
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -2,26 +2,34 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.common.utils.TextManipulator;
 import racingcar.domain.Car;
-import racingcar.domain.SimpleRacingCarGame;
+import racingcar.domain.RacingCarGame;
 
-class SimpleRacingCarGameTest {
+class RacingCarGameTest {
 
     @Test
-    void 입력한_자동차_갯수만큼_자동차_생성() {
-        int carCount = 5;
-        int round = 3;
-        SimpleRacingCarGame game = new SimpleRacingCarGame(carCount);
-        assertThat(game.joinCars()).hasSize(carCount);
+    void 입력한_자동차_이름_갯수만큼_해당_이름을_가진_자동차_생성() {
+        String carNamesInOneText = "aaaaa, bbbbb, ccccc, ddddd, eeeee";
+        String[] carNames = TextManipulator.splitTextByComma(carNamesInOneText);
+        RacingCarGame game = new RacingCarGame(carNames);
+        assertThat(game.joinCars()).hasSize(carNames.length);
+    }
+
+    @Test
+    void 자동차_이름이_5자를_초과할_경우_에러() {
+        String carNamesInOneText = "aaaaaa";
+        String[] carNames = TextManipulator.splitTextByComma(carNamesInOneText);
+        RacingCarGame game = new RacingCarGame(carNames);
+        assertThatThrownBy(game::joinCars).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 랜덤_값이_4이상일_경우_전진() {
-        Car car = new Car();
+        Car car = new Car("name");
         boolean isFourOrMoreRandomValue = car.movedForwardIfCan();
         int movedPosition = car.getCurrentPosition();
         if(isFourOrMoreRandomValue){
@@ -34,9 +42,10 @@ class SimpleRacingCarGameTest {
 
     @Test
     void 자동차_경주_1라운드_정상_실행(){
-        List<Car> cars = new ArrayList<>();
-        SimpleRacingCarGame game = new SimpleRacingCarGame(cars);
-        assertThatCode(()->game.moveOneRound(cars)).doesNotThrowAnyException();
+        String carNamesInOneText = "aaaaa, bbbbb, ccccc, ddddd, eeeee";
+        String[] carNames = TextManipulator.splitTextByComma(carNamesInOneText);
+        RacingCarGame game = new RacingCarGame(carNames);
+        assertThatCode(()->game.moveOneRound(game.joinCars())).doesNotThrowAnyException();
     }
 
 }
