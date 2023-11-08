5e3faac12b345861b46f6dab299690e3cd9cca4c
choomi1217 <whdudal1217@naver.com>
2023-11-09 01:56:04 +0900

    refactor: 테스트코드 메소드 명 변경

--- src/test/java/study/racingcar/ScoreTest.java
+++ src/test/java/study/racingcar/ScoreTest.java
@@ -22,14 +22,14 @@ class ScoreTest {
 
     @DisplayName("차의 움짐임에 따라 점수를 char로 반환 - 차가 움직였을 때")
     @Test
-    void a(){
+    void whenCarMoveMustReturnCharacter(){
         Score score = new Score(carAlwaysReturnAboveFour);
         assertThat(score.score()).isEqualTo(Score.MOVE_SCORE);
     }
 
     @DisplayName("차의 움짐임에 따라 점수를 char로 반환 - 차가 움직이지 않았을 때")
     @Test
-    void b(){
+    void whenCarStopMustReturnCharacter(){
         Score score = new Score(carAlwaysReturnBelowFour);
         assertThat(score.score()).isEqualTo(Score.STOP_SCORE);
     }
