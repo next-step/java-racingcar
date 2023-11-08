8e1e5c3ef06095fa291dab2e4b126337829c1937
yoohojeong <yh132@naver.com>
2023-11-08 16:39:18 +0900

    feat : 자동차에 이름 부여 - 단, 자동차 이름은 5자를 초과할 수 없다

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,17 +1,22 @@
 package racingcar.domain;
 
 import java.util.Random;
+import racingcar.common.utils.TextLimitSizeValidator;
 
 public class Car {
 
         private static final int RANDOM_BOUND = 10;
         private static final int START_INIT_VALUE = 1;
         private static final int LOWER_LIMIT = 4;
+        private static final int NAME_LENGTH_LIMIT = 5;
         private static final Random random = new Random();
+        private final String name;
         private int currentPosition;
 
-        public Car() {
+        public Car(String name) {
+                this.name = name;
                 currentPosition = START_INIT_VALUE;
+                errorIfNameLengthLimitOver();
         }
 
         public void moveForward() {
@@ -26,8 +31,18 @@ public class Car {
                 return ableToMove;
         }
 
+        private void errorIfNameLengthLimitOver(){
+                if (TextLimitSizeValidator.isOverLimitSize(this.name, NAME_LENGTH_LIMIT)){
+                        throw new IllegalArgumentException("자동차 이름은" + NAME_LENGTH_LIMIT + "자 이상만 입력 가능합니다.");
+                }
+        }
+
         private boolean isAbleToMove() {
                 return random.nextInt(RANDOM_BOUND) >= LOWER_LIMIT;
+        }
+
+        public String getName(){
+                return name;
         }
 
         public int getCurrentPosition() {
