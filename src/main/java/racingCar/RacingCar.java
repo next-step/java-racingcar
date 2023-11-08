47d810ff7560678738ceece87a2f1dcefe71dbc6
Hyesooo <hyesookim95@gmail.com>
2023-11-08 13:20:08 +0900

    feat: racing 기능 추가

---
+++ src/main/java/racingCar/RacingCar.java
@@ -0,0 +1,34 @@
+package racingCar;
+
+import java.util.Random;
+
+public class RacingCar {
+    private int carCount;
+    private int chance;
+    private Car[] cars;
+
+    public RacingCar(int carCount, int chance) {
+        this.carCount = carCount;
+        this.chance = chance;
+        initCars(carCount);
+    }
+
+    private void initCars(int carCount) {
+        this.cars = new Car[carCount];
+        for (int i = 0; i < carCount; i++) {
+            this.cars[i] = new Car();
+        }
+    }
+
+    public void racing() {
+        for (int i = 0; i < chance; i++) {
+            for (int j = 0; j < carCount; j++) {
+                this.cars[j].moveCar(getRandomValue());
+            }
+        }
+    }
+
+    private int getRandomValue() {
+        return new Random().nextInt(10);
+    }
+}
