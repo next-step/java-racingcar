86a3356afcafaf3925b1ecbe8d61b308c6312961
ByungChan Lee <lbc2415@gmail.com>
2023-11-08 19:57:10 +0900

    refactor: refactored for InputView to add getNumCar and getNumAttempt

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -3,16 +3,16 @@ package racing;
 import java.util.Scanner;
 
 public class InputView {
-    static int numCar;
-    static int numAttempt;
+    private static int numCar;
+    private static int numAttempt;
     public static void display() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("자동차 대수는 몇 대 인가요?");
-        int numCar = getNumberOfCar(scanner);
+        numCar = scanner.nextInt();;
 
         System.out.println("시도할 회수는 몇 회 인가요?");
-        int numAttempt = getNumAttempt(scanner);
+        numAttempt = scanner.nextInt();;
 
         System.out.println("자동차의 수: " + numCar);
         System.out.println("시도 횟수: " + numAttempt);
@@ -20,11 +20,12 @@ public class InputView {
         System.out.println("실행결과");
     }
 
-    private static int getNumberOfCar(Scanner scanner) {
-        return scanner.nextInt();
+    public static int getNumberOfCar() {
+        return numCar;
     }
 
-    private static int getNumAttempt(Scanner scanner) {
-        return scanner.nextInt();
+    public static int getNumAttempt() {
+        return numAttempt;
     }
+
 }
--- src/main/java/racing/RacingCarGame.java
+++ src/main/java/racing/RacingCarGame.java
@@ -5,9 +5,9 @@ public class RacingCarGame {
     public static void main (String[] arg) {
         InputView.display();
 
-        GameSetup gameSetup = new GameSetup(InputView.numCar, InputView.numAttempt);
+        GameSetup gameSetup = new GameSetup(InputView.getNumberOfCar(), InputView.getNumAttempt());
 
-        for (int i = 0; i < InputView.numAttempt; i++) {
+        for (int i = 0; i < InputView.getNumAttempt(); i++) {
             gameSetup.moveCar();
 
             System.out.println("******** "+(i+1)+" 라운드 ********"); // 라운드별 구분자
