ce420f94dd1de2d362c2e4286cee89ed24454a43
cjsrhd94 <cjsrhd94@gmail.com>
2023-11-09 00:09:35 +0900

    refactor: 테스트 코드 최적화

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -10,8 +10,7 @@ public class RacingGameTest {
 
 	@Test
 	void 레이싱_게임_시작시_유저를_입력받아_초기화한다() {
-		Function<String, RacingGame> gameInit = RacingGame::new;
-		RacingGame game = gameInit.apply("rac1,rac2,rac3");
+		RacingGame game = new RacingGame("rac1,rac2,rac3");
 
 		assertThat(game.getCars().get(0).getName()).isEqualTo("rac1");
 		assertThat(game.getCars().get(1).getName()).isEqualTo("rac2");
@@ -20,9 +19,8 @@ public class RacingGameTest {
 
 	@Test
 	void 레이싱_전략만큼_차량을_전진시킨다() {
-		Function<String, RacingGame> gameInit = RacingGame::new;
-		RacingGame game = gameInit.apply("rac1,rac2,rac3");
-		MoveStrategy strategy = () -> new FixedStrategy().move();
+		RacingGame game = new RacingGame("rac1,rac2,rac3");
+		MoveStrategy strategy = () -> 4;
 
 		game.play(strategy);
 
@@ -34,9 +32,8 @@ public class RacingGameTest {
 	// hmm...
 	@Test
 	void 점수를_가장_많이_얻은_유저들이_우승자가_된다() {
-		Function<String, RacingGame> gameInit = RacingGame::new;
-		RacingGame game = gameInit.apply("rac1,rac2,rac3");
-		MoveStrategy strategy = () -> new FixedStrategy().move();
+		RacingGame game = new RacingGame("rac1,rac2,rac3");
+		MoveStrategy strategy = () -> 4;
 
 		game.play(strategy);
 		assertThat(game.getWinners().get(0).getName()).isEqualTo("rac1");
