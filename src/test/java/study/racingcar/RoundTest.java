2476ac9f744a0b71b37c872b5474c8fc7cb7f70e
choomi1217 <whdudal1217@naver.com>
2023-11-09 01:54:03 +0900

    test: 테스트코드 개선

--- src/test/java/study/racingcar/RoundTest.java
+++ src/test/java/study/racingcar/RoundTest.java
@@ -1,15 +1,17 @@
 package study.racingcar;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import study.racingcar.fake.FailRandomGenratorStub;
+import study.racingcar.fake.ScoreBoardStub;
+import study.racingcar.fake.SuccessRandomGenratorStub;
 
 public class RoundTest {
     private Cars movingCars;
@@ -27,12 +29,45 @@ public class RoundTest {
         stopedCars = Cars.from(stopedCarList);
     }
 
-    @DisplayName("라운드가 정상 진행")
+    @DisplayName("라운드의 race()가 예외를 던지지 않음")
     @Test
-    void a(){
-        Round round = new Round(movingCars);
+    void roundRacingWithoutException(){
+        Round round = emptyRound();
         ScoreBoard scoreBoard = new ScoreBoard();
         assertThatNoException().isThrownBy(() -> round.race(scoreBoard));
+    }
+
+    @DisplayName("라운드 race() - 차가 움직임")
+    @Test
+    void roundRacingWhenCarMoving(){
+        Round round = new Round(movingCars);
+        ScoreBoardStub scoreBoard = new ScoreBoardStub();
+        round.race(scoreBoard);
+
+        scoreBoard.scores().forEach(score -> {
+            score.keySet().forEach(car -> {
+                assertThat(car.canMove()).isEqualTo(MoveStatus.MOVE);
+            });
+        });
+    }
+
+    @DisplayName("라운드 race() - 차가 멈춤")
+    @Test
+    void roundRacingWhenCarStop(){
+        Round round = new Round(stopedCars);
+        ScoreBoardStub scoreBoard = new ScoreBoardStub();
+        round.race(scoreBoard);
+
+        scoreBoard.scores().forEach(score -> {
+            score.keySet().forEach(car -> {
+                assertThat(car.canMove()).isEqualTo(MoveStatus.STOP);
+            });
+        });
+    }
+
+    Round emptyRound(){
+        List<Car> carList = List.of(new Car(new RandomGenerator(new Random())));
+        return new Round(new Cars(carList));
     }
 
 }
