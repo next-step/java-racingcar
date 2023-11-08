6b089ba446410f8dee642b88be4f9439e478b890
gilbertlim <9ilbert.app@gmail.com>
2023-11-08 23:41:09 +0900

    feat: 자동자 전진 기능 구현

--- src/main/java/study/step4/Car.java
+++ src/main/java/study/step4/Car.java
@@ -4,11 +4,31 @@ public class Car {
 
     private final String name;
 
+    private int position = 1;
+
     public Car(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int number) {
+        if (isForwardCondition(number)) {
+            forward();
+        }
+    }
+
+    private static boolean isForwardCondition(int number) {
+        return number >= 4;
+    }
+
+    private void forward() {
+        position += 1;
     }
 }
