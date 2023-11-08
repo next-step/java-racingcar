248fd5a309947d1fb0c116c9168d333039527360
gilbertlim <9ilbert.app@gmail.com>
2023-11-09 01:09:49 +0900

    feat: 자동차 경주 우승자 출력

--- src/main/java/study/step4/ResultView.java
+++ src/main/java/study/step4/ResultView.java
@@ -1,5 +1,6 @@
 package study.step4;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ResultView {
@@ -9,5 +10,13 @@ public class ResultView {
             System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
         }
         System.out.println();
+    }
+
+    public static void showWinners(List<Car> cars) {
+        List<String> winnerNames = new ArrayList<>();
+        for (Car car: cars) {
+            winnerNames.add(car.getName());
+        }
+        System.out.println(String.join(", ", winnerNames) + "가 최종 우승했습니다.");
     }
 }
--- src/main/java/study/step4/Step4Application.java
+++ src/main/java/study/step4/Step4Application.java
@@ -3,6 +3,7 @@ package study.step4;
 import java.util.List;
 
 import static study.step4.InputView.requestRule;
+import static study.step4.ResultView.showWinners;
 
 public class Step4Application {
 
@@ -10,5 +11,6 @@ public class Step4Application {
         Racing racing = new Racing(new RandomNumberGenerator());
         Rule rule = requestRule();
         List<Car> result = racing.run(rule);
+        showWinners(result);
     }
 }
