366a5fbd195d40259f9fa83a1a1ecc93962c9ba9
choomi1217 <whdudal1217@naver.com>
2023-11-09 03:00:41 +0900

    refactor: scores 메소드 분리

--- src/main/java/study/racingcar/ScoreBoard.java
+++ src/main/java/study/racingcar/ScoreBoard.java
@@ -17,21 +17,35 @@ public class ScoreBoard {
 
     public List<Map<Car, String>> scores() {
         LinkedList<Map<Car, String>> result = new LinkedList<>();
+        makeResult(result);
+        return result;
+    }
+
+    private void makeResult(LinkedList<Map<Car, String>> result) {
         grids.forEach(scoreEachRound -> {
             if (result.isEmpty()) {
-                result.add(
-                    scoreEachRound.roundScores().stream()
-                        .collect(Collectors.toMap(Score::whozScore,
-                            score -> String.valueOf(score.score().moveString()))));
+                firstRoundResult(result, scoreEachRound);
                 return;
             }
-            Map<Car, String> last = result.getLast();
-            result.add(scoreEachRound.roundScores().stream()
-                .map(score -> Map.entry(score.whozScore(),
-                    last.get(score.whozScore()) + score.score().moveString()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+            firstAfterRoundResult(result, scoreEachRound);
         });
-        return result;
+    }
+
+    private static void firstAfterRoundResult(LinkedList<Map<Car, String>> result,
+        ScoreEachRound scoreEachRound) {
+        Map<Car, String> last = result.getLast();
+        result.add(scoreEachRound.roundScores().stream()
+            .map(score -> Map.entry(score.whozScore(),
+                last.get(score.whozScore()) + score.score().moveString()))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+    }
+
+    private static void firstRoundResult(LinkedList<Map<Car, String>> result,
+        ScoreEachRound scoreEachRound) {
+        result.add(
+            scoreEachRound.roundScores().stream()
+                .collect(Collectors.toMap(Score::whozScore,
+                    score -> String.valueOf(score.score().moveString()))));
     }
 
 }
