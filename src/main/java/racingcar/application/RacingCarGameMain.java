0a5f675435cf147ddd35486f3b25314344d7bd6a
yoohojeong <yh132@naver.com>
2023-11-08 16:41:52 +0900

    feat : 출력 요구사항에 맞게 Main 함수 수정

--- src/main/java/racingcar/application/RacingCarGameMain.java
+++ src/main/java/racingcar/application/RacingCarGameMain.java
@@ -4,25 +4,29 @@ import static racingcar.presentation.InputView.scanAndGetValues;
 
 import java.util.List;
 import java.util.stream.IntStream;
+import racingcar.common.utils.TextManipulator;
 import racingcar.domain.Car;
-import racingcar.domain.SimpleRacingCarGame;
-import racingcar.presentation.OutputView;
+import racingcar.domain.RacingCarGame;
+import racingcar.presentation.ResultView;
 import racingcar.presentation.dto.RacingGameInput;
 
 public class RacingCarGameMain {
 
         public static void main(String args[]) {
                 RacingGameInput inputValue = scanAndGetValues();
-                int carCount = inputValue.getCarCount();
+                String carNamesInOneText = inputValue.getCarNamesInOneText();
                 int round = inputValue.getRound();
+                String[] carNames = TextManipulator.splitTextByComma(carNamesInOneText);
 
-                SimpleRacingCarGame game = new SimpleRacingCarGame(carCount);
+                RacingCarGame game = new RacingCarGame(carNames);
                 List<Car> cars = game.joinCars();
 
+                ResultView.printResultViewTitle();
+                ResultView.printPositionForAllCarsInCurrentRound(cars);
                 IntStream.range(0, round).forEach(thisRound -> {
                         game.moveOneRound(cars);
-                        OutputView.printPositionForAllCarsInCurrentRound(cars);
+                        ResultView.printPositionForAllCarsInCurrentRound(cars);
                 });
+                ResultView.printWinner(game.findWinners(cars));
         }
-
 }
