3d100013f1d836248232ea7953a47625423bca5c
namuhuchutong <namuhuchutong@gmail.com>
2023-11-09 12:01:40 +0900

    refactor: 피드백 반영 - 불필요한 테스트 준비 과정 삭제

--- src/test/java/me/namuhuchutong/racingcar/RacingCarServiceTest.java
+++ src/test/java/me/namuhuchutong/racingcar/RacingCarServiceTest.java
@@ -5,31 +5,22 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 import me.namuhuchutong.racingcar.domain.CarNames;
 import me.namuhuchutong.racingcar.domain.RacingCarService;
-import me.namuhuchutong.racingcar.domain.rule.RacingRule;
 import me.namuhuchutong.racingcar.dto.RacingResult;
 import me.namuhuchutong.racingcar.dto.UserInputInformation;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RacingCarServiceTest {
 
-    private RacingRule alwaysMoveRule;
-
-    @BeforeEach
-    void setUp() {
-        alwaysMoveRule = () -> true;
-    }
-
     @DisplayName("경주 결과의 총 횟수와 주어진 경주 횟수가 동일해야 한다.")
     @Test
-    void test() {
+    void racing_result_is_equal_to_given_times() {
         //given
         CarNames names = CarNames.from(List.of("a", "b"));
         UserInputInformation inputInformation = new UserInputInformation(names, 3);
 
         //when
-        RacingResult racingResult = new RacingCarService(alwaysMoveRule).runRacing(inputInformation);
+        RacingResult racingResult = new RacingCarService(() -> true).runRacing(inputInformation);
 
         //then
         assertThat(racingResult.getTotalRaceCount()).isEqualTo(inputInformation.getTimes());
