b2dc1de99ea78149c778c555d5ff0c6306f679e9
hypernova1 <hypemova@gmail.com>
2023-11-09 17:38:01 +0900

    refactor: 패키지 구조 변경

--- src/main/java/step2/StringConverter.java
+++ src/main/java/step2/StringConverter.java
@@ -1,4 +1,4 @@
-package study;
+package step2;
 
 public class StringConverter {
 
--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -1,6 +1,5 @@
-package study;
+package step1;
 
-import org.assertj.core.util.Strings;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
