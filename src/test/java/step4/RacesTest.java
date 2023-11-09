47f85949a26085458ad6276956a4f24aac646614
jane096 <jes7077@naver.com>
2023-11-09 22:19:00 +0900

    RacesTest 추가

--- src/test/java/step4/RacesTest.java
+++ src/test/java/step4/RacesTest.java
@@ -3,11 +3,13 @@ package step4;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import step4.model.Races;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class RacesTest {
 
@@ -17,5 +19,14 @@ public class RacesTest {
     void raceGenerateTest(String janeCar, String johnCar) {
         Races races = new Races(Arrays.asList(janeCar, johnCar));
         assertThat(races.getCars()).size().isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4})
+    @DisplayName("랜덤숫자를 넘긴다면 레이스가 시작된다.")
+    void raceStartTest(int condition) {
+        Races races = mock(Races.class);
+        races.start(condition);
+        verify(races, times(1)).start(condition);
     }
 }
