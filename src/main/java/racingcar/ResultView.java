10388ffb9ede1d760d6a27d9b8bd0fe4727ecaf5
Hyesooo <hyesookim95@gmail.com>
2023-11-09 16:00:36 +0900

    feat: 자동차명 출력 기능 추가

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -2,6 +2,7 @@ package racingcar;
 
 public class ResultView {
     public static void printRacingResult(Car car) {
+        System.out.print(car.getName() + " : ");
         for (int j = 0; j < car.getPosition(); j++) {
             System.out.print('-');
         }
