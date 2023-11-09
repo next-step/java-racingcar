83ec9f9d2acbc753d43db85decad7f2e6e484ed4
thkim1 <thxogusrla@gmail.com>
2023-11-09 10:45:40 +0900

    test: step3 코드로 이동 및 테스트 추가 및 pr 반영

--- src/test/java/car/CarTest.java
+++ src/test/java/car/CarTest.java
@@ -1,12 +1,22 @@
 package car;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void Car_객체_생성() {
+        car = new Car("test");
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {4, 9})
@@ -30,6 +40,26 @@ public class CarTest {
         car.move(number);
 
         assertThat(car.currentPosition()).isEqualTo(initPosition);
+    }
+
+
+    @DisplayName("생성자 테스트")
+    @Test
+    void Car_생성자_멤버변수() {
+        assertThat(car.carName()).isEqualTo("test");
+        assertThat(car.currentPosition()).isEqualTo(0);
+    }
+
+    @DisplayName("자동차 이름 길이")
+    @Test
+    void 자동차_이름_길이체크() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Car("InvalidNameTest");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Car("");
+        });
     }
 
 }
--- src/test/java/car/IoMainTest.java
+++ src/test/java/car/IoMainTest.java
@@ -1,4 +1,4 @@
-package champion;
+package car;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,26 +9,32 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class InputViewTest {
+public class IoMainTest {
 
     private List<Car> cars = new ArrayList<Car>();
+    private List<Car> dupleWinners = new ArrayList<Car>();
 
     @BeforeEach
     void setCars() {
         cars.add(new Car("pobi", 3));
         cars.add(new Car("crong", 5));
         cars.add(new Car("honux", 6));
+
+        dupleWinners.add(new Car("pobi", 4));
+        dupleWinners.add(new Car("crong", 6));
+        dupleWinners.add(new Car("honux", 6));
     }
 
     @DisplayName("split 테스트")
     @Test
     void 문자열_split_테스트() {
-        assertThat(InputView.splitNames("pobi,crong,honux")).hasSize(3);
+        assertThat(IoMain.splitNames("pobi,crong,honux")).hasSize(3);
     }
 
     @DisplayName("우승자 테스트")
     @Test
     void 우승자_테스트() {
-        assertThat(InputView.getChampionString(cars)).isEqualTo("honux");
+        assertThat(IoMain.winnersString(cars)).isEqualTo("honux");
+        assertThat(IoMain.winnersString(dupleWinners)).isEqualTo("crong, honux");
     }
 }
