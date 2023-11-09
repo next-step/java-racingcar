ed5ee680d491ce9b4b01bdb0f93e6474d8927ea3
hyunyyxp <qqqq_o@naver.com>
2023-11-09 11:48:28 +0900

    test: 추가한 예외에 대한 테스트 수정

--- src/test/java/study/carracing/util/ValidatorTest.java
+++ src/test/java/study/carracing/util/ValidatorTest.java
@@ -2,16 +2,25 @@ package study.carracing.util;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import study.carracing.exceptions.InvalidNameException;
+import study.carracing.exceptions.InvalidNumberException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ValidatorTest {
 
     @Test
+    @DisplayName("숫자 이외의 값 또는 음수 입력 시 예외가 발생한다.")
+    void validateNumber() {
+        assertThatThrownBy(() -> Validator.validateNumber(-1))
+                .isInstanceOf(InvalidNumberException.class);
+    }
+
+    @Test
     @DisplayName("형식에 맞지 않는 자동차의 이름을 입력할 시 IllegalArgumentException예외가 발생한다.")
     void carsNamePattern() {
         assertThatThrownBy(() -> Validator.validateCarName("wrong pattern"))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(InvalidNameException.class)
                 .hasMessageContaining("형식에 맞지 않는 값이 입력됐습니다.");
     }
 
@@ -19,7 +28,7 @@ class ValidatorTest {
     @DisplayName("자동차의 이름이 5자를 초과할 시 RunTimeException이 발생한다.")
     void carNameLength() {
         assertThatThrownBy(() -> Validator.validateCarName("racingcar,pobi"))
-                .isInstanceOf(RuntimeException.class)
+                .isInstanceOf(InvalidNameException.class)
                 .hasMessageContaining("자동차의 이름은 5자를 초과할 수 없습니다.");
     }
 }
