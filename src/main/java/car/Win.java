e8751ba909afbb1ff7ff0ed21318b11d40e5976d
thkim1 <thxogusrla@gmail.com>
2023-11-09 14:25:58 +0900

    feat: Win 객체 추가

---
+++ src/main/java/car/Win.java
@@ -0,0 +1,44 @@
+package car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Win {
+    private final List<Car> allCar;
+
+    public Win(List<Car> allCar) {
+        this.allCar = allCar;
+    }
+
+    public List<String> winnersNameList() {
+        List<Car> cars = allCarList();
+        List<String> winnersName = new ArrayList<String>();
+
+        int max = maxCarPosition();
+
+        for (Car car : cars) {
+            addWinner(car, max, winnersName);
+        }
+
+        return winnersName;
+    }
+
+    private void addWinner(Car car, int max, List<String> winners) {
+        if (max == car.currentPosition()) {
+            winners.add(car.carName());
+        }
+    }
+
+    private int maxCarPosition() {
+        int max = 0;
+
+        for (Car car : allCarList()) {
+            max = max <= car.currentPosition() ? car.currentPosition() : 0;
+        }
+        return max;
+    }
+
+    private List<Car> allCarList() {
+        return this.allCar;
+    }
+}
---
+++ src/test/java/car/WinTest.java
@@ -0,0 +1,32 @@
+package car;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinTest {
+    private Win allCars;
+    private List<Car> cars;
+
+    @BeforeEach
+    void setCars() {
+        cars = new ArrayList<Car>();
+
+        cars.add(new Car("pobi", 3));
+        cars.add(new Car("crong", 5));
+        cars.add(new Car("honux", 6));
+
+        allCars = new Win(cars);
+    }
+
+    @DisplayName("우승자 리스트")
+    @Test
+    void 우승자_리스트() {
+        assertThat(allCars.winnersNameList().contains("honux")).isTrue();
+    }
+}
