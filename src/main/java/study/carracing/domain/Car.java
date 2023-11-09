1e18e2ab933179fcf27b83abac84ee6fd960d950
hyunyyxp <qqqq_o@naver.com>
2023-11-09 17:37:30 +0900

    refactor: 자동차 이름에 대한 검증 로직 Car 객체로 이동

--- src/main/java/study/carracing/domain/Car.java
+++ src/main/java/study/carracing/domain/Car.java
@@ -1,20 +1,24 @@
 package study.carracing.domain;
 
+import study.carracing.exceptions.InvalidNameException;
+
 public class Car {
 
     private static final int INITIAL_POSITION = 1;
     private static final String PROGRESS_DASH = "-";
     private static final int MOVE_CONDITION_NUMBER = 4;
+    private static final int MAX_NAME_LENGTH = 5;
 
     private int position;
     private final String name;
 
     public Car(String name) {
-        this.name = name;
-        this.position = INITIAL_POSITION;
+        this(INITIAL_POSITION, name);
     }
 
     public Car(int position, String name) {
+        validateNameBlank(name.trim());
+        validateNameLength(name.trim());
         this.position = position;
         this.name = name;
     }
@@ -27,6 +31,18 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    private void validateNameBlank(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new InvalidNameException("자동차의 이름이 입력되지 않았습니다.");
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new InvalidNameException("자동차의 이름은 5자를 초과할 수 없습니다." + name);
+        }
     }
 
     public String getName() {
--- src/main/java/study/carracing/util/Validator.java
+++ src/main/java/study/carracing/util/Validator.java
@@ -8,9 +8,7 @@ import java.util.regex.Pattern;
 public class Validator {
     private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
     private static final Pattern NAMES_PATTERN = Pattern.compile("^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$");
-    private static final int MAX_NAME_LENGTH = 5;
     private static final String NAMES_DELIMITER = ",";
-    private static final int MIN_NAME_LENGTH = 1;
 
     public static int validateNumber(int number) {
         if (!isNumber(number)) {
@@ -25,17 +23,7 @@ public class Validator {
             throw new InvalidNameException("형식에 맞지 않는 값이 입력됐습니다." + names);
         }
 
-        for (String name : names.split(NAMES_DELIMITER)) {
-            checkNameLengthAndThrow(name);
-        }
-
         return names.split(NAMES_DELIMITER);
-    }
-
-    private static void checkNameLengthAndThrow(String name) {
-        if (!(name.length() >= MIN_NAME_LENGTH && name.length() <= MAX_NAME_LENGTH)) {
-            throw new InvalidNameException("자동차의 이름은 5자를 초과할 수 없습니다." + name);
-        }
     }
 
     private static boolean isNumber(int number) {
