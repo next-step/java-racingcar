556501e44f12b3e86f71321153c44afc7b92584e
gilbertlim <9ilbert.app@gmail.com>
2023-11-10 14:17:38 +0900

    refactor: 자동차 이름 원시값 포장(CarName VO)

--- src/main/java/study/step4/Car.java
+++ src/main/java/study/step4/Car.java
@@ -1,36 +1,22 @@
 package study.step4;
 
-import study.step4.exception.CarException;
-
 public class Car {
 
-    private final String name;
+    private final CarName name;
 
     private int position = 1;
 
     public Car(String name) {
-        validate(name);
-        this.name = name;
+        this.name = new CarName(name);
     }
 
     public Car(String name, int position) {
-        this.name = name;
+        this.name = new CarName(name);
         this.position = position;
     }
 
-    private void validate(String name) {
-        if (!isValidName(name)) {
-            throw new CarException("CarNameError: 자동차 이름은 1자 이상 5자 입니다.");
-        }
-    }
-
-    private boolean isValidName(String name) {
-        int length = name.length();
-        return length >= 1 && length <= 5;
-    }
-
     public String getName() {
-        return name;
+        return name.toString();
     }
 
     public int getPosition() {
