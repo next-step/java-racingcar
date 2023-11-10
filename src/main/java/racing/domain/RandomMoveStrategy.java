9fbdc893c145b133ddb7c7e8b81f3c29cd5127d6
hhj <abc@def.com>
2023-11-10 10:57:19 +0900

    refactor:불필요한 코드 삭제

--- src/main/java/racing/domain/RandomMoveStrategy.java
+++ src/main/java/racing/domain/RandomMoveStrategy.java
@@ -5,12 +5,9 @@ import java.util.Random;
 public class RandomMoveStrategy implements MoveStrategy{
 
     static final int MOVE_CHECK_NUMBER = 4;
-    Random random = new Random();
+    private Random random = new Random();
     @Override
     public boolean movable() {
-        if(random.nextInt(10) >= MOVE_CHECK_NUMBER){
-            return true;
-        }
-        return false;
+        return random.nextInt(10) >= MOVE_CHECK_NUMBER;
     }
 }
