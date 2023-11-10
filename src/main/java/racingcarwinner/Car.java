98412a53c91db1420902907ed5a236c46b933be8
parkje0927 <parkje09270@gmail.com>
2023-11-10 15:14:12 +0900

    refactor: 자동차 이름의 길이에 대한 validation 확인, 이동 시 랜덤 숫자의 기준 확인을 Car 객체에게 위임

--- src/main/java/racingcarwinner/Car.java
+++ src/main/java/racingcarwinner/Car.java
@@ -5,9 +5,18 @@ public class Car {
     private String name;
     private int location;
 
-    public Car(String name, int location) {
+    public Car(final String name, final int location) {
+        final int STANDARD = 5;
+        if (isLengthGreaterThanStandard(name, STANDARD)) {
+            System.out.println("자동차 이름은 " + STANDARD + "글자를 초과할 수 없습니다.");
+            throw new IllegalStateException("자동차 이름 길이 기준 초과 오류");
+        }
         this.name = name;
         this.location = location;
+    }
+
+    public static Car createDefaultCar(final String name) {
+        return new Car(name, 1);
     }
 
     public String getName() {
@@ -18,11 +27,17 @@ public class Car {
         return location;
     }
 
-    public static Car createDefaultCar(String name) {
-        return new Car(name, 1);
+    public boolean isLengthGreaterThanStandard(String name, int standard) {
+        return name.length() > standard;
     }
 
-    public void move() {
-        this.location += 1;
+    public void move(int randomNumber) {
+        if (randomNumber >= 4) {
+            this.location++;
+        }
+    }
+
+    public boolean isMaxLocation(int maxLocation) {
+        return this.location == maxLocation;
     }
 }
