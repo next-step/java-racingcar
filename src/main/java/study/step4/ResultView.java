fc8e49183e0a2ac615a47a8bc1bf2a24be61c039
gilbertlim <9ilbert.app@gmail.com>
2023-11-10 13:17:41 +0900

    refactor: 메서드 분리

--- src/main/java/study/step4/ResultView.java
+++ src/main/java/study/step4/ResultView.java
@@ -12,11 +12,15 @@ public class ResultView {
         System.out.println();
     }
 
-    public static void showWinners(List<Car> cars) {
+    public static List<String> getWinners(List<Car> cars) {
         List<String> winnerNames = new ArrayList<>();
         for (Car car: cars) {
             winnerNames.add(car.getName());
         }
+        return winnerNames;
+    }
+
+    public static void show(List<String> winnerNames) {
         System.out.println(String.join(", ", winnerNames) + "가 최종 우승했습니다.");
     }
 }
--- src/main/java/study/step4/Step4Application.java
+++ src/main/java/study/step4/Step4Application.java
@@ -3,7 +3,8 @@ package study.step4;
 import java.util.List;
 
 import static study.step4.InputView.requestRule;
-import static study.step4.ResultView.showWinners;
+import static study.step4.ResultView.getWinners;
+import static study.step4.ResultView.show;
 
 public class Step4Application {
 
@@ -11,6 +12,6 @@ public class Step4Application {
         Racing racing = new Racing(new RandomNumberGenerator());
         Rule rule = requestRule();
         List<Car> result = racing.run(rule);
-        showWinners(result);
+        show(getWinners(result));
     }
 }
