b9960e6c0fb9fadf7bb517bea1b239cf36dd40ba
ByungChan Lee <lbc2415@gmail.com>
2023-11-10 23:46:07 +0900

    refactor: refactored moving assertCar to CarTest from RacingTest

---
+++ src/test/java/racing/CarTest.java
@@ -0,0 +1,25 @@
+package racing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @Test
+    void assertCar() {
+        Car car = new Car();
+        int testValueLessThanFour = 3;
+        int testValueGreaterThanFour = 5;
+
+        car.moveCar();
+
+        assertThat(car.getDistance()).isEqualTo(1);
+
+        car.validateForMove(testValueLessThanFour);
+        assertThat(car.getDistance()).isEqualTo(1);
+
+        car.validateForMove(testValueGreaterThanFour);
+        assertThat(car.getDistance()).isEqualTo(2);
+    }
+}
--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -36,21 +36,4 @@ class RacingTest {
         });
         assertThat(sumAfterStartGame.get()).isGreaterThan(0);
     }
-
-    @Test
-    void assertCar() {
-        Car car = new Car();
-        int testValueLessThanFour = 3;
-        int testValueGreaterThanFour = 5;
-
-        car.moveCar();
-
-        assertThat(car.getDistance()).isEqualTo(1);
-
-        car.validateForMove(testValueLessThanFour);
-        assertThat(car.getDistance()).isEqualTo(1);
-
-        car.validateForMove(testValueGreaterThanFour);
-        assertThat(car.getDistance()).isEqualTo(2);
-    }
 }
