876572a794e9342bbf4083dc00fff7f2a7248091
pawoo0211 <pawoo0211@naver.com>
2023-11-11 00:12:20 +0900

    [step4] test: 자동차 경주(우승자)

--- src/test/java/racingcar/application/RacingCarGameServiceTest.java
+++ src/test/java/racingcar/application/RacingCarGameServiceTest.java
@@ -3,20 +3,32 @@ package racingcar.application;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Cars;
 import racingcar.ui.dto.RacingGameRequest;
 import racingcar.ui.dto.RacingGameResponse;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 @DisplayName("자동자 경주 서비스 테스트")
 public class RacingCarGameServiceTest {
-    private final int INITIAL_CAR_NUMBER = 5;
     private final int END_TURN = 5;
-
+    private String inputCars = "pobi,crong,honux";
 
     @DisplayName("각 자동차가 움직인 상태 값을 가진 응답 객체를 생성한다.")
     @Test
     void getAllCarMoveCount() {
         RacingCarGameService service = new RacingCarGameService();
-        Assertions.assertThat(service.start(new RacingGameRequest(INITIAL_CAR_NUMBER, END_TURN)))
+        Assertions.assertThat(service.start(new RacingGameRequest(inputCars, END_TURN)))
                 .isInstanceOf(RacingGameResponse.class);
+    }
+
+    @DisplayName("게임 종료 횟수가 0 이하일 경우 예외가 발생한다.")
+    @Test
+    void checkEndTurn() {
+        int endTurn = 0;
+        RacingCarGameService service = new RacingCarGameService();
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            service.start(new RacingGameRequest(inputCars, endTurn));
+        });
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -11,20 +11,19 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 @DisplayName("자동차 도메인 테스트")
 public class CarTest {
-    private final int INITIAL_CAR_NUMBER = 5;
-
+    private final String INPUT_CARS = "pobi,crong,honux";
     private Cars cars;
 
     @BeforeEach
     void setUp() {
-        cars = new Cars(INITIAL_CAR_NUMBER);
+        cars = new Cars(INPUT_CARS);
     }
 
     @DisplayName("랜덤 값에 따라 자동차가 멈추거나 전진한다.")
     @ParameterizedTest
     @CsvSource(value = {"1,0","4,1"}, delimiter = ',')
     void stopOrMove(int randomValue, int expected) {
-        Car car = new Car();
+        Car car = new Car("kwon");
         car.stopOrMove(randomValue);
         Assertions.assertThat(car.getMoveCount()).isEqualTo(expected);
     }
@@ -32,26 +31,26 @@ public class CarTest {
     @DisplayName("생성된 자동차를 자동차 리스트에 추가한다.")
     @Test
     void addCar() {
-        cars.addCar(new Car());
-        Assertions.assertThat(cars.getNumberOfCar()).isEqualTo(INITIAL_CAR_NUMBER + 1);
+        int existingCarNumber = INPUT_CARS.split(",").length;
+        cars.addCar(new Car("kwon"));
+        Assertions.assertThat(cars.getNumberOfCar()).isEqualTo(existingCarNumber + 1);
     }
 
     @DisplayName("추가한 자동차들이 자동차 리스트에 존재하는지 확인한다.")
     @Test
     void getAllCar() {
-        Car car1 = new Car();
-        Car car2 = new Car();
+        Car car1 = new Car("kwon");
+        Car car2 = new Car("park");
         cars.addCar(car1);
         cars.addCar(car2);
         Assertions.assertThat(cars.getAllCar()).contains(car1, car2);
     }
 
-    @DisplayName("입력된 자동차의 수가 0일 경우 예외가 발생한다.")
+    @DisplayName("자동차의 이름이 5글자가 넘어가면 예외가 발생한다.")
     @Test
     void inputCarNumberException() {
-        int carNumber = 0;
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            Cars newCars = new Cars(carNumber);
+            new Cars("sangkwon");
         });
     }
 }
