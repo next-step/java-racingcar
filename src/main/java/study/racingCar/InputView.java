0bc8c0f62c3356f566331ca8bc5c99dad7ac82e4
joomin <jmnl225@github.com>
2023-11-10 12:20:33 +0900

    <feat> 기능목록3 : InputView입력기능 추가

--- src/main/java/study/racingCar/InputView.java
+++ src/main/java/study/racingCar/InputView.java
@@ -1,25 +1,22 @@
-package study;
+package study.racingCar;
 
 import java.util.Scanner;
 
 public class InputView {
 
-    public InputView(){
+    private Scanner scanner;
 
+    public InputView() {
+        this.scanner = new Scanner(System.in);
     }
 
-    public static String askCarCount(){
-        Scanner scanner = new Scanner(System.in);
-
+    public String inputCars(){
         System.out.println("자동차 대수는 몇 대 인가요?");
         return scanner.nextLine();
     }
 
-    public static String askTryCount(){
-        Scanner scanner = new Scanner(System.in);
-
+    public String inputRounds(){
         System.out.println("시도할 회수는 몇 회 인가요?");
         return scanner.nextLine();
     }
-
 }
---
+++ src/main/java/study/racingCar/ResultView.java
@@ -0,0 +1,4 @@
+package study.racingCar;
+
+public class ResultView {
+}
