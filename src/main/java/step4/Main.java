dfac98e49ee9ab14cde6e1bb3b1e446e44e069ae
jinyounglee <hs25823@naver.com>
2023-11-10 17:35:02 +0900

    FEAT: - RacingApplication 클래스 생성, view 클래스 생성

---
+++ src/main/java/step4/Main.java
@@ -0,0 +1,8 @@
+package step4;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/step4/model/Car.java
@@ -0,0 +1,10 @@
+package step4.model;
+
+public class Car {
+
+    private final String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
