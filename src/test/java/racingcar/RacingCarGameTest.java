793aa6a80a05a20f8f003ebd5ad5607c8dcd3ce7
yoohojeong <yh132@naver.com>
2023-11-11 09:17:42 +0900

    test : 우승자찾기 테스트 추가

--- src/test/java/racingcar/RacingCarGameTest.java
+++ src/test/java/racingcar/RacingCarGameTest.java
@@ -4,48 +4,66 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import racingcar.common.utils.TextManipulator;
 import racingcar.domain.Car;
 import racingcar.domain.RacingCarGame;
+import racingcar.domain.Winners;
 
 class RacingCarGameTest {
 
     @Test
     void 입력한_자동차_이름_갯수만큼_해당_이름을_가진_자동차_생성() {
         String carNamesInOneText = "aaaaa, bbbbb, ccccc, ddddd, eeeee";
+        int round = 5;
         String[] carNames = TextManipulator.splitTextByComma(carNamesInOneText);
-        RacingCarGame game = new RacingCarGame(carNames);
-        assertThat(game.joinCars()).hasSize(carNames.length);
+        RacingCarGame game = new RacingCarGame(round, carNames);
+        assertThat(game.cars()).hasSize(carNames.length);
     }
 
     @Test
     void 자동차_이름이_5자를_초과할_경우_에러() {
-        String carNamesInOneText = "aaaaaa";
-        String[] carNames = TextManipulator.splitTextByComma(carNamesInOneText);
-        RacingCarGame game = new RacingCarGame(carNames);
-        assertThatThrownBy(game::joinCars).isInstanceOf(IllegalArgumentException.class);
+        String text = "aaaaaa";
+        assertThatThrownBy(() ->new Car(text)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 랜덤_값이_4이상일_경우_전진() {
         Car car = new Car("name");
-        boolean isFourOrMoreRandomValue = car.movedForwardIfCan();
-        int movedPosition = car.getCurrentPosition();
-        if(isFourOrMoreRandomValue){
-            assertThat(movedPosition).isEqualTo(2);
-        }
-        if(!isFourOrMoreRandomValue){
-            assertThat(movedPosition).isEqualTo(1);
-        }
+        int randomNo = 4;
+        car.movedForwardIfCan(randomNo);
+        assertThat(car.currentPosition().getNumber()).isEqualTo(2);
+    }
+
+    @Test
+    void 랜덤_값이_4미만일_경우_전진하지_않음() {
+        Car car = new Car("name");
+        int randomNo = 3;
+        car.movedForwardIfCan(randomNo);
+        assertThat(car.currentPosition().getNumber()).isEqualTo(1);
     }
 
     @Test
     void 자동차_경주_1라운드_정상_실행(){
         String carNamesInOneText = "aaaaa, bbbbb, ccccc, ddddd, eeeee";
+        int round = 5;
         String[] carNames = TextManipulator.splitTextByComma(carNamesInOneText);
-        RacingCarGame game = new RacingCarGame(carNames);
-        assertThatCode(()->game.moveOneRound(game.joinCars())).doesNotThrowAnyException();
+        RacingCarGame game = new RacingCarGame(round, carNames);
+        assertThatCode(()->game.moveOneRound()).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 우승자_찾기(){
+        Car car1 = new Car("car1");
+        Car car2 = new Car("car2");
+        Car car3 = new Car("car3");
+        Car car4 = new Car("car4");
+        car1.currentPosition().addOnePosition();
+        car2.currentPosition().addOnePosition();
+        List<Car> cars = List.of(car1, car2, car3, car4);
+        List<Car> winners = List.of(car1, car2);
+        assertThat(Winners.findWinners(cars).containsAll(winners)).isTrue();
     }
 
 }
