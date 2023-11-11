08102fe0db4029aa0a812e4198aa0acfe6372935
pilming44 <pilming44@naver.com>
2023-11-11 10:27:59 +0900

    test: 리뷰내용 반영으로 인한 테스트코드 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -9,24 +9,44 @@ import static org.assertj.core.api.Assertions.*;
 public class CarTest {
     @DisplayName("자동차를 생성하면 초기 위치는 0 이다.")
     @Test
-    void Car_초기위치() {
-        Car car = new Car(() -> true);
+    void 초기위치_0() {
+        Car car = new Car("noName", () -> true);
         assertThat(car.currentPosition()).isZero();
     }
 
     @DisplayName("자동차가 전진할수있다.")
     @Test
-    void Car_전진() {
-        Car car = new Car(() -> true);
+    void 전진() {
+        Car car = new Car("noName", () -> true);
         car.move();
         assertThat(car.currentPosition()).isEqualTo(car.MOVE_DISTANCE);
     }
 
     @DisplayName("자동차가 멈춘다")
     @Test
-    void Car_멈춤() {
-        Car car = new Car(() -> false);
+    void 멈춤() {
+        Car car = new Car("noName", () -> false);
         car.move();
         assertThat(car.currentPosition()).isZero();
+    }
+
+    @DisplayName("입력된 레이서 이름 문자열의 공백을 제거")
+    @Test
+    void 이름_공백_제거() {
+        assertThat(Car.replaceAllEmptySpaceInName(" 공 백 문 자 "))
+                .isEqualTo("공백문자");
+    }
+    @DisplayName("입력된 문자열을 쉼표로 구분한다.")
+    @Test
+    void 이름_쉼표로_구분() {
+        assertThat(Car.nameSplitByComma("choi,jae,hyeon"))
+                .containsExactly("choi", "jae", "hyeon");
+    }
+    @DisplayName("구분된 문자열은 5자를 초과하면 IllegalArgumentException 이 발생한다.")
+    @Test
+    void 이름_글자수_초과() {
+        assertThatThrownBy(() -> {
+            String[] value = Car.nameSplitByComma("choi,jae,hyeon,choiJaeHyeon");
+        }).isInstanceOf(RuntimeException.class).hasMessage("이름은 " + Car.MAXIMUM_NAME_LENGTH + "자를 초과할 수 없습니다.");
     }
 }
--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -2,10 +2,11 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.movablestrategy.RandomRacingStrategy;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
     @DisplayName("경주의 최소 차량 조건을 충족한다.")
@@ -16,17 +17,21 @@ class CarsTest {
     }
 
     @DisplayName("구분된 문자열을 이름으로 문자열 개수만큼 자동차를 생성한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"pobi,jh,choi", "choi", "mon, fri, sat, sun"})
-    void Cars_레이서_인원만큼_Car_생성(String input) {
-        Cars cars = new Cars(input);
+    @Test
+    void Cars_레이서_인원만큼_Car_생성() {
+        Cars cars = new Cars("mon, fri, sat, sun", new RandomRacingStrategy());
 
-        String[] racerNames = RacerNameValidator.splitByComma(input);
-        assertThat(cars.size()).isEqualTo(racerNames.length);
+        assertThat(cars.size()).isEqualTo(4);
+        List<Car> expectedCars = List.of(
+                new Car("mon", new RandomRacingStrategy()),
+                new Car("fri", new RandomRacingStrategy()),
+                new Car("sat", new RandomRacingStrategy()),
+                new Car("sun", new RandomRacingStrategy())
+        );
 
-        for (String name : racerNames) {
-            assertThat(cars.isParticipate(name)).isTrue();
-        }
+        List<Car> carsList = cars.getCars();
+
+        assertThat(carsList).containsExactlyInAnyOrderElementsOf(expectedCars);
     }
 
     @DisplayName("가장 빠른사람이 한 명 일떄 이름을 리턴한다.")
@@ -35,11 +40,9 @@ class CarsTest {
         Car movableCar = new Car("a", () -> true);
         movableCar.move();
         Car pauseCar = new Car("b", () -> false);
-        Cars cars = new Cars();
-        cars.add(movableCar);
-        cars.add(pauseCar);
+        Cars cars = new Cars(List.of(movableCar, pauseCar));
 
-        assertThat(cars.mostFastest()).isEqualTo("a");
+        assertThat(cars.mostFastestCar()).containsExactly(movableCar);
     }
 
     @DisplayName("가장 빠른사람이 한 명 이상일떄 이름을 쉼표(,)로 구분해서 리턴한다.")
@@ -47,12 +50,10 @@ class CarsTest {
     void Cars_가장_빠른_레이서들() {
         Car movableCar = new Car("a", () -> true);
         movableCar.move();
-        Car pauseCar = new Car("b", () -> true);
-        pauseCar.move();
-        Cars cars = new Cars();
-        cars.add(movableCar);
-        cars.add(pauseCar);
+        Car anotherMovableCar = new Car("b", () -> true);
+        anotherMovableCar.move();
+        Cars cars = new Cars(List.of(movableCar, anotherMovableCar));
 
-        assertThat(cars.mostFastest()).isEqualTo("a,b");
+        assertThat(cars.mostFastestCar()).containsExactly(movableCar, anotherMovableCar);
     }
 }
