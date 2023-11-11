a9c0a1d1a3f4b0bb6670756134d91523ca3691ce
Hanna Choi <44824602+gkssk4163@users.noreply.github.com>
2023-11-11 10:27:23 +0900

    refactor: 상수 final 선언

--- src/main/java/step3/domain/RacingCarRule.java
+++ src/main/java/step3/domain/RacingCarRule.java
@@ -2,7 +2,7 @@ package step3.domain;
 
 public class RacingCarRule {
 
-    static private int MOVABLE_LOWER_BOUND = 4;
+    private static final int MOVABLE_LOWER_BOUND = 4;
 
     public boolean canMove(int movingCondition) {
         return movingCondition >= MOVABLE_LOWER_BOUND;
