d438daf1dcd14a01d9abbfa09ebfe836b39dafeb
kimbro97 <rlagudwog@gmail.com>
2023-11-11 23:54:28 +0900

    refactor: 검증 메서드 추출작업

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,19 +1,24 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int NAME_MAX_LENGTH = 5;
     private String name;
-    private int position = 0;
+    private int position;
 
     public Car(String name) {
         this(name, 0);
     }
 
     public Car(String name, int position) {
-        if (name.length() > 5) {
+        if (isNameLength(name)) {
             throw new RuntimeException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
         this.name = name;
         this.position = position;
+    }
+
+    private boolean isNameLength(String name) {
+        return name.length() > NAME_MAX_LENGTH;
     }
 
     public void move(MoveStrategy moveStrategy) {
