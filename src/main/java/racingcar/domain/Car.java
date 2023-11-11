e084479e684fe6f825ebde3e7d9c97bcc62eeba5
yoohojeong <yh132@naver.com>
2023-11-11 09:21:56 +0900

    feat : Name, Position 관련 함수 제거

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,51 +1,35 @@
 package racingcar.domain;
 
-import java.util.Random;
-import racingcar.common.utils.TextLimitSizeValidator;
-
 public class Car {
 
-        private static final int RANDOM_BOUND = 10;
         private static final int LOWER_LIMIT = 4;
-        private static final int NAME_LENGTH_LIMIT = 5;
-        private static final Random random = new Random();
-        private final String name;
+        private final Name name;
         private final Position currentPosition;
 
         public Car(String name) {
-                this.name = name;
+                this.name = new Name(name);
                 this.currentPosition = new Position();
-                errorIfNameLengthLimitOver();
         }
 
-        public void moveForward() {
+        public void movedForwardIfCan(int randomNo) {
+                if (isAbleToMove(randomNo)) {
+                        this.moveForward();
+                }
+        }
+
+        private void moveForward() {
                 currentPosition.addOnePosition();
         }
 
-        public boolean movedForwardIfCan() {
-                boolean ableToMove = isAbleToMove();
-                if (ableToMove) {
-                        this.moveForward();
-                }
-                return ableToMove;
+        private boolean isAbleToMove(int randomNo) {
+                return randomNo >= LOWER_LIMIT;
         }
 
-        private void errorIfNameLengthLimitOver(){
-                if (TextLimitSizeValidator.isOverLimitSize(this.name, NAME_LENGTH_LIMIT)){
-                        throw new IllegalArgumentException("자동차 이름은" + NAME_LENGTH_LIMIT + "자 이상만 입력 가능합니다.");
-                }
+        public Position currentPosition() {
+                return currentPosition;
         }
 
-        private boolean isAbleToMove() {
-                return random.nextInt(RANDOM_BOUND) >= LOWER_LIMIT;
-        }
-
-        public String getName(){
+        public Name name(){
                 return name;
         }
-
-        public int getCurrentPosition() {
-                return currentPosition.getPosition();
-        }
-
 }
