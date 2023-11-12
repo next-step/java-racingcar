402fc16c1d406000c5290e6e195dbe2f124ad24b
jane096 <jes7077@naver.com>
2023-11-12 18:08:51 +0900

    불필요한 import 제거

--- src/test/java/step4/RacesTest.java
+++ src/test/java/step4/RacesTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import step4.model.Car;
 import step4.model.ForwardMoveStrategy;
 import step4.model.Races;
