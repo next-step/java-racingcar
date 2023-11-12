091b2ff510cfd2060f4fc4d0a1fef68dad16a1bd
Jiwon Nam <njw724@naver.com>
2023-11-12 17:36:15 +0900

    test: 경계값 테스트 변경

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -9,17 +9,13 @@ import static racing.Car.*;
 
 public class CarTest {
 
-
-    @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9, 10})
-    void 랜덤값이_4이상이면_전진한다(int randomNumber) {
-        assertThat(isMove(randomNumber)).isTrue();
+    @Test
+    void 랜덤값이_4이상이면_전진한다() {
+        assertThat(isMove(4)).isTrue();
     }
 
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3})
-    void 랜덤값이_4미만이면_전진하지_못한다(int randomNumber) {
-        assertThat(isMove(randomNumber)).isFalse();
+    @Test
+    void 랜덤값이_4미만이면_전진하지_못한다() {
+        assertThat(isMove(3)).isFalse();
     }
 }
