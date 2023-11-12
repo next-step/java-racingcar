64bdc8590dd5b972c473e81a86ad201804b28e40
pawoo0211 <pawoo0211@naver.com>
2023-11-12 15:57:03 +0900

    [step4] refactor: Wrapping Name

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,26 +2,20 @@ package racingcar.domain;
 
 public class Car implements Comparable<Car> {
 
-    private static final int STANDARD_CAR_NAME_LENGTH = 5;
-
-    private String name;
+    private Name name;
     private int moveCount = 0;
 
     public Car(String carName) {
-        if (carName.length() > STANDARD_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException(String
-                    .format("Input Car name : %s, car name must be less than 6 characters.", carName));
-        }
-        name = carName;
+        name = new Name(carName);
     }
 
-    public Car(String name, int moveCount) {
-        this.name = name;
+    public Car(String carName, int moveCount) {
+        name = new Name(carName);
         this.moveCount = moveCount;
     }
 
     public String getName() {
-        return name;
+        return name.getName();
     }
 
     public int getMoveCount() {
@@ -38,7 +32,7 @@ public class Car implements Comparable<Car> {
 
     public Car stopOrMove(Strategy strategy) {
         if (strategy.isMovable()) {
-            return new Car(name, moveCount + 1);
+            return new Car(name.getName(), moveCount + 1);
         }
         return this;
     }
