e704b2e8560f771e1df417a8965127593c9cf7b5
kyunghyun-park <qqqq_o@naver.com>
2023-11-12 16:26:14 +0900

    test: 전진 전략패턴 추가에 따른 테스트 수정

--- src/test/java/study/carracing/domain/CarTest.java
+++ src/test/java/study/carracing/domain/CarTest.java
@@ -2,43 +2,18 @@ package study.carracing.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import study.carracing.fake.TestMoveStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
-
-    @ParameterizedTest
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    @DisplayName("random 값이 4이상일 경우 자동차가 1칸 전진한다.")
-    void moveCar(int randomValue) {
-        Car car = new Car(new Name("car1"));
-
-        car.move(randomValue);
-
-        int position = car.getPosition();
-        assertThat(position).isEqualTo(2);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3})
-    @DisplayName("random 값이 4미만일 경우 자동차는 움직이지 않는다.")
-    void dontMoveCar(int randomValue) {
-        Car car = new Car(new Name("car1"));
-
-        car.move(randomValue);
-
-        int position = car.getPosition();
-        assertThat(position).isEqualTo(1);
-    }
 
     @Test
     @DisplayName("이름이 car1인 자동차의 위치가 2일 때 toString 호출 시 'car1 : --'가 출력된다.")
     void carToString() {
         Car car = new Car(new Name("car1"));
 
-        car.move(4);
+        car.move(new TestMoveStrategy(true));
 
         assertThat(car.toString()).isEqualTo("car1 : --");
     }
@@ -48,9 +23,8 @@ class CarTest {
     void setPosition() {
         Car car = new Car(new Position(4), new Name("car1"));
 
-        car.move(5);
+        car.move(new TestMoveStrategy(true));
 
         assertThat(car.getPosition()).isEqualTo(5);
     }
-
 }
