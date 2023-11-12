f66470542717d59850d5753a51d76464f7f5c324
USER <minjoo.kim1@linecorp.com>
2023-11-12 18:04:19 +0900

    feat: add RacingCarProject

---
+++ src/main/java/racingcar/RacingCarProject.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class RacingCarProject {
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        racingGame.play();
+    }
+}
