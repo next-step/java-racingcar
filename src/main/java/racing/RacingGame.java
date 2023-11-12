fdd58fce48486d4f0b067d308e0537f31179df56
Jiwon Nam <njw724@naver.com>
2023-11-12 17:37:38 +0900

    refactor: 클래스 비즈니스 로직 수정

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -1,10 +1,42 @@
 package racing;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class RacingGame {
     public static void main(String[] args) {
-        int carCount = InputView.getCarCount();
-        int retryCount = InputView.getRetryCount();
-        Car car = new Car(carCount);
-        car.start(retryCount);
+
+        InputView inputView = new InputView();
+        int carCount = inputView.getCarCount();
+        int retryCount = inputView.getRetryCount();
+
+        List<Car> cars = createCar(carCount);
+
+        ResultView resultView = new ResultView();
+        System.out.println("실행 결과");
+        raceCars(cars, retryCount, resultView);
+
+    }
+
+    private static List<Car> createCar(int carCount) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < carCount; i++) {
+            cars.add(new Car());
+        }
+        return cars;
+    }
+
+    private static void raceCars(List<Car> cars, int retryCount, ResultView resultView) {
+        for (int i = 0; i < retryCount; i++) {
+            moveCars(cars);
+            resultView.print(cars);
+        }
+    }
+
+    private static void moveCars(List<Car> cars) {
+        for (Car car : cars) {
+            car.move();
+        }
     }
 }
