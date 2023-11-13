541a37d791f32111cb4274c30e8bdacd67df446b
JongWoon.Lee <whddns262@gmail.com>
2023-11-13 23:47:42 +0900

    feat : 사용자는 몇 대의 자동차로 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.

--- src/main/java/racingcar/controller/RacingGameRunner.java
+++ src/main/java/racingcar/controller/RacingGameRunner.java
@@ -1,6 +1,14 @@
 package racingcar.controller;
 
+import racingcar.model.StringParser;
+import racingcar.view.InputView;
+
 public class RacingGameRunner {
     public static void main(String[] args) {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String input = InputView.enter();
+        String[] names = StringParser.split(input);
+        System.out.println("시도할 회수는 몇회인가요?");
+        int numbers = InputView.enterNumber();
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,16 @@
 package racingcar.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String enter(){
+        return scanner.nextLine();
+    }
+
+    public static int enterNumber(){
+        return scanner.nextInt();
+    }
+
 }
