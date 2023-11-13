7b47ba6e0342cc418928910edbf3232d9656cfb6
JongWoon.Lee <whddns262@gmail.com>
2023-11-13 23:16:28 +0900

    test : 자동차 생성 테스트 코드 삽입 및 name getter 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,12 +2,16 @@ package racingcar.model;
 
 public class Car {
 
-    private String name;
+    private final String name;
 
     public Car(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,27 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CarTest {
+    @Test
+    void test_자동차_이름을_5자_초과하여_생성() {
+        String name = "일곱글자입니다";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Car(name);
+        });
+    }
+
+    @Test
+    void test_자동차_정상적으로_생성() {
+        String name = "다섯글자임";
+        Car car = new Car(name);
+
+        assertEquals(name, car.getName());
+    }
+
+}
