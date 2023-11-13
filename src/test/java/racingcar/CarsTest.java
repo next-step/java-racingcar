12394c54fb28e5aa5961999c08872b7b7773d686
pilming44 <pilming44@naver.com>
2023-11-13 21:49:20 +0900

    test: 전략을 Car 필드에서 제외하면서 테스트코드 변경

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -2,7 +2,9 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.movablestrategy.RandomRacingStrategy;
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Racing;
 
 import java.util.List;
 
@@ -19,41 +21,36 @@ class CarsTest {
     @DisplayName("구분된 문자열을 이름으로 문자열 개수만큼 자동차를 생성한다.")
     @Test
     void Cars_레이서_인원만큼_Car_생성() {
-        Cars cars = new Cars("mon, fri, sat, sun", new RandomRacingStrategy());
+        Cars cars = new Cars("mon, fri, sat, sun");
 
         assertThat(cars.size()).isEqualTo(4);
-        List<Car> expectedCars = List.of(
-                new Car("mon", new RandomRacingStrategy()),
-                new Car("fri", new RandomRacingStrategy()),
-                new Car("sat", new RandomRacingStrategy()),
-                new Car("sun", new RandomRacingStrategy())
-        );
 
         List<Car> carsList = cars.getCars();
 
-        assertThat(carsList).containsExactlyInAnyOrderElementsOf(expectedCars);
+        assertThat(carsList)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactly(new Car("mon"), new Car("fri"), new Car("sat"), new Car("sun"));
     }
 
-    @DisplayName("가장 빠른사람이 한 명 일떄 이름을 리턴한다.")
+    @DisplayName("가장 빠른 자동차가 한 대 일떄 그 자동차만 포함한 리스트를 반환한다.")
     @Test
-    void Cars_가장_빠른_레이서() {
-        Car movableCar = new Car("a", () -> true);
-        movableCar.move();
-        Car pauseCar = new Car("b", () -> false);
+    void Cars_가장_빠른_자동차() {
+        Car movableCar = new Car("a", 3);
+        Car pauseCar = new Car("b", 0);
         Cars cars = new Cars(List.of(movableCar, pauseCar));
 
+        assertThat(cars.mostFastestCar().size()).isEqualTo(1);
         assertThat(cars.mostFastestCar()).containsExactly(movableCar);
     }
 
-    @DisplayName("가장 빠른사람이 한 명 이상일떄 이름을 쉼표(,)로 구분해서 리턴한다.")
+    @DisplayName("가장 빠른 자동차가 두 대 이상일떄 자동차 리스트를 반환한다.")
     @Test
-    void Cars_가장_빠른_레이서들() {
-        Car movableCar = new Car("a", () -> true);
-        movableCar.move();
-        Car anotherMovableCar = new Car("b", () -> true);
-        anotherMovableCar.move();
-        Cars cars = new Cars(List.of(movableCar, anotherMovableCar));
+    void Cars_가장_빠른_자동차들() {
+        Car samePositionCar1 = new Car("a", 3);
+        Car samePositionCar2 = new Car("b", 3);
+        Cars cars = new Cars(List.of(samePositionCar1, samePositionCar2));
 
-        assertThat(cars.mostFastestCar()).containsExactly(movableCar, anotherMovableCar);
+        assertThat(cars.size()).isEqualTo(2);
+        assertThat(cars.mostFastestCar()).containsExactly(samePositionCar1, samePositionCar2);
     }
 }
