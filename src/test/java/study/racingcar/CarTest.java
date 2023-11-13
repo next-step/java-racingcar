0dbf009057190eca0ab4accbd827a934bb59d85e
choomi1217 <whdudal1217@naver.com>
2023-11-13 21:44:49 +0900

    test: 자동차 생성 조건에 따른 단위 테스트 추가

--- src/test/java/study/racingcar/CarTest.java
+++ src/test/java/study/racingcar/CarTest.java
@@ -2,6 +2,7 @@ package study.racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Random;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,23 +22,40 @@ public class CarTest {
     private
 
     @BeforeEach
-    void init(){
+    void init() {
         carAlwaysReturnAboveFour = new Car("name");
         carAlwaysReturnBelowFour = new Car("name");
     }
 
+    @DisplayName("자동차의 이름이 5글자 이하면 아무런 예외도 발생하지 않습니다.")
+    @Test
+    void doesNotThroeAnyExceptionWhenCarNameLessThan5Characters(){
+        assertThatNoException().isThrownBy(() -> new Car("Four"));
+    }
+
+    @DisplayName("자동차의 이름이 5글자를 넘어가면 IllegalArgumentException이 발생합니다.")
+    @Test
+    void throwIllegalArgumentExceptionWhenCarNameMoreThan5Characters() {
+        assertThatThrownBy(() -> new Car("More than 5 characters")).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
     @DisplayName("자동차가 전진합니다.")
     @Test
-    void carMustMoveForward(){
-        assertThatNoException().isThrownBy(() -> carAlwaysReturnAboveFour.canMove(successRandomGenratorStub));
-        assertThat(carAlwaysReturnAboveFour.canMove(successRandomGenratorStub)).isEqualTo(MoveStatus.MOVE);
+    void carMustMoveForward() {
+        assertThatNoException().isThrownBy(
+            () -> carAlwaysReturnAboveFour.canMove(successRandomGenratorStub));
+        assertThat(carAlwaysReturnAboveFour.canMove(successRandomGenratorStub)).isEqualTo(
+            MoveStatus.MOVE);
     }
 
     @DisplayName("자동차가 정지합니다.")
     @Test
-    void carMustMoveBackward(){
-        assertThatNoException().isThrownBy(() -> carAlwaysReturnBelowFour.canMove(failRandomGenratorStub));
-        assertThat(carAlwaysReturnBelowFour.canMove(failRandomGenratorStub)).isEqualTo(MoveStatus.STOP);
+    void carMustMoveBackward() {
+        assertThatNoException().isThrownBy(
+            () -> carAlwaysReturnBelowFour.canMove(failRandomGenratorStub));
+        assertThat(carAlwaysReturnBelowFour.canMove(failRandomGenratorStub)).isEqualTo(
+            MoveStatus.STOP);
     }
 
 }
