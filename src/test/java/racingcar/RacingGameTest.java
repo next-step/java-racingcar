6757ccca5a3ebee7ad90d98d670c025c7127fee2
kmj990929 <kmj990929@dgist.ac.kr>
2023-11-13 22:38:56 +0900

    test: add DisplayName

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +18,7 @@ public class RacingGameTest {
 //    }
 
     @Test
+    @DisplayName("random 값은 0~9 사이의 값으로 제한")
     public void random_범위() {
         int random = RacingGame.getRandomValue();
         assertThat(random)
@@ -25,6 +27,7 @@ public class RacingGameTest {
     }
 
     @Test
+    @DisplayName("moveOrStop의 인수가 4 이상이면 전진, 미만이면 멈춤(location 변경 없음)")
     public void 전진_여부() {
         RacingCar car = new RacingCar("abc");
         car.moveOrStop(1);
@@ -37,6 +40,7 @@ public class RacingGameTest {
     }
 
     @Test
+    @DisplayName("자동차 이름은 5자 이하로 설정되어야 함")
     public void 자동차_이름_5자_이하() {
         Assertions.assertThatThrownBy(() -> new RacingCar("abcdefg"))
                         .isInstanceOf(RuntimeException.class);
