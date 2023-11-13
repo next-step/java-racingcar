68fefc8231341e1bfd623979e144afe681d1f320
JongWoon.Lee <whddns262@gmail.com>
2023-11-14 00:07:11 +0900

    feat : 전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,6 +2,8 @@ package racingcar.model;
 
 public class Car {
 
+    private final int SUCCESS = 4;
+    private int position;
     private final String name;
 
     public Car(String name) {
@@ -9,9 +11,19 @@ public class Car {
             throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
         this.name = name;
+        this.position =  0;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int number) {
+        if(number >= SUCCESS)
+            position++;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -24,4 +24,17 @@ public class CarTest {
         assertEquals(name, car.getName());
     }
 
+    @Test
+    void test_성공하여_앞으로_한칸_전진() {
+        Car car = new Car("성공");
+        car.move(4);
+        assertEquals(1, car.getPosition());
+    }
+
+    @Test
+    void test_실패하여_정지() {
+        Car car = new Car("실패");
+        car.move(0);
+        assertEquals(0, car.getPosition());
+    }
 }
