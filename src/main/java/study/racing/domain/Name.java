20de1a6fe6720693d2ec28bfdcd7d7903eec3bd0
birdie <flaehdan@gmail.com>
2023-11-13 10:16:03 +0900

    feat: 이름 공백일 경우 예외 발생

--- src/main/java/study/racing/domain/Name.java
+++ src/main/java/study/racing/domain/Name.java
@@ -4,17 +4,23 @@ import java.util.Objects;
 
 public class Name {
 
+    private static final int MAX_NAME_LENGTH = 5;
     private final String value;
 
     public Name(String value) {
-        if (value.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        if (!nameValid()) {
+            throw new IllegalArgumentException("이름은 1글자 이상, 5자 이하만 가능합니다.");
         }
         this.value = value;
     }
 
     public String getValue() {
         return value;
+    }
+
+    private boolean nameValid() {
+
+        return !value.isEmpty() && !value.isBlank() && value.length() <= MAX_NAME_LENGTH;
     }
 
     @Override
