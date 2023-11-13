c135a6dd5cc4f673de595279fc4caa2884ab771f
JongWoon.Lee <whddns262@gmail.com>
2023-11-13 23:09:12 +0900

    feat : 각 자동차에 이름을 부여할 수 있다, 자동차 이름은 5자를 초과할 수 없다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,36 +1,13 @@
 package racingcar.model;
 
 public class Car {
-    private static final int RANDOM_VALUE_RANGE = 9;
-    private static final int SUCCESS_MINIMUM_VALUE = 4;
-    private int position;
 
-    public Car() {
-        this.position = 1;
-    }
+    private String name;
 
-    public void moveCar(boolean canMove) {
-        if (canMove) {
-            position++;
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
+        this.name = name;
     }
-
-    public void moveOnce() {
-        int randomNumber = generateRandomNumber();
-        boolean canMove = tryToMove(randomNumber);
-        moveCar(canMove);
-    }
-
-    public int getPosition() {
-        return position;
-    }
-
-    public boolean tryToMove(int random) {
-        return random >= SUCCESS_MINIMUM_VALUE;
-    }
-
-    public int generateRandomNumber() {
-        return (int) (Math.floor(Math.random() * (Car.RANDOM_VALUE_RANGE + 1)));
-    }
-
 }
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,48 +1,5 @@
 package racingcar;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import racingcar.model.Car;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class RacingGameTest {
 
-    @Test
-    void car_값이_4이상인_경우_1칸_전진한다() {
-        Car car = new Car();
-        car.moveCar(car.tryToMove(4));
-        assertEquals(car.getPosition(), 2);
-    }
-
-    @Test
-    void car_값이_4미만인_경우_정지한다() {
-        Car car = new Car();
-        car.moveCar(car.tryToMove(0));
-        assertEquals(car.getPosition(), 1);
-    }
-
-    @DisplayName("전진")
-    @Test
-    void car_주어진_횟수_동안_랜덤값을_구하여_자동차가_전진할_수_있다() {
-        Car car = new Car();
-        int position = 2;
-
-        int randomNumber = 4;
-        boolean result = car.tryToMove(randomNumber);
-        car.moveCar(result);
-        assertEquals(car.getPosition(), position);
-    }
-
-    @DisplayName("정지")
-    @Test
-    void car_주어진_횟수_동안_랜덤값을_구하여_자동차가_멈출_수_있다() {
-        Car car = new Car();
-        int position = 1;
-
-        int randomNumber = 1;
-        boolean result = car.tryToMove(randomNumber);
-        car.moveCar(result);
-        assertEquals(car.getPosition(), position);
-    }
 }
