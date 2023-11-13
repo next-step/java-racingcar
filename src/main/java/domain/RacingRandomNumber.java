70929ba2acfe444f874ec672bff74ef6cb762057
이창섭 <ventulus95@gmail.com>
2023-11-14 00:39:06 +0900

    Refact: 상수처리

--- src/main/java/domain/RacingRandomNumber.java
+++ src/main/java/domain/RacingRandomNumber.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class RacingRandomNumber {
 
+    public static final int MAX_BOUND = 10;
     private Integer number;
 
     public RacingRandomNumber(Integer number) {
@@ -11,7 +12,7 @@ public class RacingRandomNumber {
     }
 
     public RacingRandomNumber(Random random) {
-        this.number = random.nextInt(10);
+        this.number = random.nextInt(MAX_BOUND);
     }
 
     public Integer number() {
