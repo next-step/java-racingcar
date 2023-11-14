fea1c228177ab047cc0c67ae4e26fcd90be4f1a4
perhona <perhona@kakao.com>
2023-11-14 22:48:15 +0900

    test(step4): 자동차 이름 부여 테스트, 이름 제한 예외 테스트 추가

--- src/main/java/racingCar/car/Car.java
+++ src/main/java/racingCar/car/Car.java
@@ -3,25 +3,28 @@ package racingCar.car;
 import racingCar.util.NumberSupplier;
 
 public class Car {
-    public int distance;
-    public String name;
-    private final NumberSupplier numberSupplier;
+	public int distance;
+	public String name;
+	private final NumberSupplier numberSupplier;
 
-    public Car(int distance, NumberSupplier numberSupplier) {
-        this.distance = distance;
-        this.numberSupplier = numberSupplier;
-    }
+	public Car(int distance, NumberSupplier numberSupplier) {
+		this.distance = distance;
+		this.numberSupplier = numberSupplier;
+	}
 
-    public Car(int distance, NumberSupplier numberSupplier, String name) {
-        this.distance = distance;
-        this.numberSupplier = numberSupplier;
-        this.name = name;
-    }
+	public Car(int distance, NumberSupplier numberSupplier, String name) {
+		this.distance = distance;
+		this.numberSupplier = numberSupplier;
+		if (name.length() > 5) {
+			throw new RuntimeException("자동차 이름은 5자를 초과할 수 없습니다.");
+		}
+		this.name = name;
+	}
 
-    public void move() {
-        int randomNumber = numberSupplier.get();
-        if (randomNumber > 3) {
-            distance++;
-        }
-    }
+	public void move() {
+		int randomNumber = numberSupplier.get();
+		if (randomNumber > 3) {
+			distance++;
+		}
+	}
 }
