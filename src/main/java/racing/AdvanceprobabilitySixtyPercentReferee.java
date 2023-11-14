f2cc28a379847f8bdb05a41205f07756df0937ec
HeeDoo Logan Jeong <78134917+jhd7130@users.noreply.github.com>
2023-11-15 01:21:50 +0900

    referee에서 votewinner제거 participationCar에 추가

--- src/main/java/racing/AdvanceprobabilitySixtyPercentReferee.java
+++ src/main/java/racing/AdvanceprobabilitySixtyPercentReferee.java
@@ -1,7 +1,5 @@
 package racing;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class AdvanceprobabilitySixtyPercentReferee implements Referee {
@@ -12,38 +10,5 @@ public class AdvanceprobabilitySixtyPercentReferee implements Referee {
         random.nextInt(10);
         return random.nextInt(10) > STANDARD_NUMBER_TO_JUDGE_TO_MOVE;
     }
-
-    @Override
-    public Winners voteWinner(List<Car> participationCars) {
-        Car winner = this.findWinner(participationCars);
-        return this.findSameWinner(participationCars,winner);
-    }
-
-    private Winners findSameWinner(List<Car> participationCars, Car winner) {
-        List<Car> winners = new ArrayList<>();
-        for (Car each : participationCars) {
-            this.addSameDistanceCar(winner, each, winners);
-        }
-        return Winners.defaultOf(winners);
-    }
-    private Car findWinner(List<Car> participationCars) {
-        int size = participationCars.size();
-        Car winner = null;
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = 1; j < size; j++) {
-                winner = participationCars.get(i).winnerIs(participationCars.get(j));
-            }
-        }
-        return winner;
-    }
-
-    private void addSameDistanceCar(Car winner, Car each, List<Car> winners) {
-        if (each.equalsDistance(winner)) {
-            winners.add(each);
-        }
-    }
-
-
-
 
 }
