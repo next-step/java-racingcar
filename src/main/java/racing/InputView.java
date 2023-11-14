a5b9fcfefe05fd91a78648b64afe1aa7378ae8b0
Jiwon Nam <njw724@naver.com>
2023-11-15 00:23:54 +0900

    feaet: 자동차 이름 받는 기능 추가

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -6,6 +6,11 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+    public String getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine();
+    }
+
     public int getCarCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         return getNumberInput();
