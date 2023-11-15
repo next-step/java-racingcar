b49a780292b7e6cbfe7eb96d4dde5450dc6ac3f3
joomin <jmnl225@github.com>
2023-11-15 16:03:11 +0900

    <feat> add InputView.class

--- src/main/java/study/racingCar/ResultView.java
+++ src/main/java/study/racingCar/ResultView.java
@@ -1,11 +1,4 @@
 package study.racingCar;
 
 public class ResultView {
-
-    static void printRacing(int position){
-        for (int i = 0; i < position; i++) {
-            System.out.print("-");
-        }
-    }
-
 }
