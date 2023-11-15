b0641349833e8664db92544038e923da16e288e4
hyoin <kslove1111@gmail.com>
2023-11-15 17:23:44 +0900

    Revert "refactor: 자동차 이름 체크 메서드 이동(Car -> InputView)"
    This reverts commit dc0561ae48e898a4e7212b3b0c41a12c91436f4d.

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -8,8 +8,18 @@ public class Car {
     private int position;
 
     public Car(final String name, final int position) {
-        this.name = name;
+        this.name = checkName(name);
         this.position = position;
+    }
+
+    private String checkName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5자를 넘을 수 없습니다.");
+        }
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("자동차의 이름은 1자 이상이여야 합니다.");
+        }
+        return name;
     }
 
     public String getName() {
