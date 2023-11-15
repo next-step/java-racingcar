356c468ce6dd90ae31505b049c2318a266a9dbb0
ldy9155 <ldy9155@naver.com>
2023-11-15 21:24:52 +0900

    refactor: '0' 매직넘버 DEFALT_POSITION 상수로 정의

--- src/main/java/carracing/Car.java
+++ src/main/java/carracing/Car.java
@@ -2,22 +2,27 @@ package carracing;
 
 public class Car {
 
+    private static final int DEFAULT_POSITION = 0;
     private int position;
+
     public Car() {
-        this(0);
+        this(DEFAULT_POSITION);
     }
+
     public Car(int position) {
         this.position = position;
     }
+
     public String getPositionRepeat() {
         return "-".repeat(position);
     }
+
     public int getPosition() {
         return position;
     }
 
     public void move(int value) {
-        if(value >= 4){
+        if (value >= 4) {
             position++;
         }
     }
