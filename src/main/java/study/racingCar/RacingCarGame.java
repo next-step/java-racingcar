1e3fb83192799ce9c672f1c2e0dda8ebb8a42c51
joomin <jmnl225@github.com>
2023-11-15 17:02:08 +0900

    <feat> StringToIntConverter 리팩토링

--- src/main/java/study/racingCar/RacingCarGame.java
+++ src/main/java/study/racingCar/RacingCarGame.java
@@ -3,8 +3,21 @@ package study.racingCar;
 public class RacingCarGame {
 
     public static void main(String[] args){
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        StringToIntConverter converter = new StringToIntConverter();
+        String carNames = "";
+        int tryCount = 0;
 
+        try{
 
+            carNames = inputView.inputCarNames();
+            tryCount = converter.converter(inputView.inputTryCount());
+
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("시도할 횟수는 0보다 큰 숫자로만 입력해주세요.");
+        }
 
     }
 
