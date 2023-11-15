0c926f9d3928710bb8f5ea9e5f621d27f133bc1d
joomin <jmnl225@github.com>
2023-11-15 17:35:21 +0900

    <feat> carGameTest 테스트코드 작성중

---
+++ src/main/java/study/racingCar/Car.java
@@ -0,0 +1,25 @@
+package study.racingCar;
+
+import java.util.Objects;
+
+public class Car {
+
+    private final String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
