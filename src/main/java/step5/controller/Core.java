ccab8870f9ad11a05a5e194adde3934156c635a8
HeeDoo Logan Jeong <78134917+jhd7130@users.noreply.github.com>
2023-11-18 01:29:47 +0900

    controller 역할 core 객체 생성

--- src/main/java/step5/controller/Core.java
+++ src/main/java/step5/controller/Core.java
@@ -1,7 +1,22 @@
 package step5.controller;
 
+import step5.domain.Racing;
+import step5.domain.Referee;
+import step5.domain.Winners;
+
 public class Core {
     public static void main(String[] args) {
+        // inputview
+        String names = "포비, 구름, 가둥";
+        int howManyTry = 4;
+
+        Referee referee = new Referee();
+        Racing racing = Racing.defaultOf(names,referee,howManyTry);
+        racing.start();
+        Winners winners = racing.winnerIs();
+
+
+
 
     }
 }
