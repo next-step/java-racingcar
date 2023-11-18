7528bbc505c8a2be93ac0eecaed152f01327b2b0
yumble <hnjn1101@gmail.com>
2023-11-18 18:34:23 +0900

    fix: getDistance()로 비교가 아닌 객체 비교로 테스트코드 수정

--- src/test/java/car/model/CarTest.java
+++ src/test/java/car/model/CarTest.java
@@ -19,13 +19,13 @@ public class CarTest {
     @ValueSource(ints = {0, 1, 2, 3})
     public void 랜덤숫자가_4미만이면_멈추는지_테스트(int randomNumber) {
         car.game(randomNumber);
-        assertThat(car.getDistance()).isEqualTo(0);
+        assertThat(car).isEqualTo(new Car(START_DISTANCE, "car"));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     public void 랜덤숫자가_4이상이면_전진하는지_테스트(int randomNumber) {
         car.game(randomNumber);
-        assertThat(car.getDistance()).isEqualTo(1);
+        assertThat(car).isEqualTo(new Car(1, "car"));
     }
 }
