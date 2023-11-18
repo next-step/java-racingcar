a8636dac2e30628a0fecaefaa4a23278e3ff7628
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-19 01:15:43 +0900

    feat : PositiveNumber 에 양수가 아닌 값들 유효성 검증 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,9 +9,9 @@ public class Calculator {
     private static final String COLUMN = ":";
     public static final String DEFAULT_REGEX = COMMA + "|" + COLUMN;
     private static final String CUSTOM_REGEX = "//(.)\n(.*)";
-    private static final int ZERO = 0;
+    private static final String ZERO = "0";
 
-    public long calculate(String text) {
+    public PositiveNumber calculate(String text) {
         if (isNothing(text)) {
             return calculateSumWithNothing();
         }
@@ -32,8 +32,8 @@ public class Calculator {
         return text == null || text.isBlank() || text.isEmpty();
     }
 
-    private long calculateSumWithNothing() {
-        return ZERO;
+    private PositiveNumber calculateSumWithNothing() {
+        return new PositiveNumber(ZERO);
     }
 
     private Matcher getMatcher(String text) {
@@ -44,7 +44,7 @@ public class Calculator {
         return matcher.find();
     }
 
-    private long calculateSumWithCustomRegex(Matcher matcher) {
+    private PositiveNumber calculateSumWithCustomRegex(Matcher matcher) {
         return calculateSum(matcher.group(2), createDelimiter(matcher));
     }
 
@@ -52,40 +52,24 @@ public class Calculator {
         return matcher.group(1);
     }
 
-    private long calculateSum(String target, String customDelimiter) {
-        long sum = 0L;
-        for (String number : target.split(customDelimiter)) {
-            sum += parse(number);
+    private PositiveNumber calculateSum(String target, String customDelimiter) {
+        PositiveNumber sum = new PositiveNumber(ZERO);
+        for (String value : target.split(customDelimiter)) {
+            PositiveNumber number = new PositiveNumber(value);
+            sum = sum.plus(number);
         }
         return sum;
-    }
-
-    private long parse(String number) {
-        long targetNumber;
-        try {
-            targetNumber = Long.parseLong(number);
-            if (isMinus(targetNumber)) {
-                throw new RuntimeException("음수는 입력할 수 없습니다.");
-            }
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("숫자 이외의 값은 입력할 수 없습니다.");
-        }
-        return targetNumber;
-    }
-
-    private boolean isMinus(long targetNumber) {
-        return targetNumber < 0;
     }
 
     private boolean containsDefaultRegex(String text) {
         return text.contains(COMMA) || text.contains(COLUMN);
     }
 
-    private long calculateSumWithDefaultRegex(String text) {
+    private PositiveNumber calculateSumWithDefaultRegex(String text) {
         return calculateSum(text, DEFAULT_REGEX);
     }
 
-    private long calculateSimpleNumber(String text) {
-        return Long.parseLong(text);
+    private PositiveNumber calculateSimpleNumber(String text) {
+        return new PositiveNumber(text);
     }
 }
