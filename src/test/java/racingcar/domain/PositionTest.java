9240e1d606cae644f2e76f2b6198f847afff8ff9
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-20 18:13:34 +0900

    test : PositionTest 포지션이 1칸 앞으로 움직이는 테스트 추가

--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -1,10 +1,13 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static racingcar.domain.Position.POSITION_CANNOT_BE_NEGATIVE_NUMBER_EXCEPTION;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class PositionTest {
@@ -17,5 +20,19 @@ public class PositionTest {
         assertThatThrownBy(() -> new Position(given))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage(POSITION_CANNOT_BE_NEGATIVE_NUMBER_EXCEPTION);
+    }
+
+    @ParameterizedTest
+    @DisplayName("움직이면 포지션이 1칸 앞으로 이동한다.")
+    @CsvSource(value = {"1, 2", "0, 1", "15, 16"})
+    void move(long given, long expected) {
+        // given
+        Position position = new Position(given);
+
+        // when
+        Position result = position.move();
+
+        // then
+        assertThat(result).isEqualTo(new Position(expected));
     }
 }
