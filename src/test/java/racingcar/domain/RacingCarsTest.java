a31848983b7c3a5e45921b4e3546794d57581d45
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-20 18:50:36 +0900

    test : RacingCarsTest 1회 경주를 진행하는 테스트 추가

--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -6,8 +6,37 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RacingCarsTest {
+
+    @ParameterizedTest
+    @DisplayName("1회 경주를 진행한다.")
+    @CsvSource(value = {"4, 4, 3, 3", "3, 3, 2, 2"})
+    void race_once(int randomNumber, long position1, long position2, long position3) {
+        // given
+        List<RacingCar> result = createRacingCars();
+        RacingCars racingCars = new RacingCars(result);
+
+        // when
+        racingCars.raceOnce(new DoubleRandomService(randomNumber));
+
+        // then
+        assertThat(result).isEqualTo(Arrays.asList(
+                new RacingCar("k3", position1),
+                new RacingCar("k5", position2),
+                new RacingCar("k7", position3)
+        ));
+    }
+
+    private List<RacingCar> createRacingCars() {
+        return Arrays.asList(
+                new RacingCar("k3", 3),
+                new RacingCar("k5", 2),
+                new RacingCar("k7", 2)
+        );
+    }
 
     @Test
     @DisplayName("1회 경주 결과를 알려준다.")
@@ -24,14 +53,6 @@ public class RacingCarsTest {
                         + "k5 : --\n"
                         + "k7 : --\n"
                         + "\n");
-    }
-
-    private List<RacingCar> createRacingCars() {
-        return Arrays.asList(
-                new RacingCar("k3", 3),
-                new RacingCar("k5", 2),
-                new RacingCar("k7", 2)
-        );
     }
 
     @Test
