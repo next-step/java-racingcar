cf04376d9c075c98fc12547b7070dc6614d40d01
hyeongiii <hyeongiii0811@gmail.com>
2023-11-22 06:09:43 +0900

    style: 한줄 코드 {} 블럭 생성 (step4)

--- src/main/java/step4/domain/Name.java
+++ src/main/java/step4/domain/Name.java
@@ -25,7 +25,9 @@ public class Name {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) return false;
         Name name1 = (Name) o;
         return Objects.equals(name, name1.name);
