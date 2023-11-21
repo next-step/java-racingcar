fd5b631348926cf9c46e01e58fad124fab5cd5d1
Min Kyu Jeon <16min99@gmail.com>
2023-11-22 01:16:55 +0900

    2단계 문자열 덧셈 계산기 (#5175)
    * 기능 요구사항
    쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환 (예: “” => 0, "1,2" => 3, "1,2,3" => 6, “1,2:3” => 6)
    앞의 기본 구분자(쉼표, 콜론)외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다. 예를 들어 “//;\n1;2;3”과 같이 값을 입력할 경우 커스텀 구분자는 세미콜론(;)이며, 결과 값은 6이 반환되어야 한다.
    문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw한다.
    
    * fix: 예외처리 오류 수정

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,6 +3,9 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -19,20 +22,39 @@ public class StringTest {
         String myString = "(1,2)";
         String expectedResult = "1,2";
 
-        String result = myString.substring(1,4);
+        String result = myString.substring(1, 4);
 
         assertThat(result).isEqualTo(expectedResult);
     }
 
     @Test
     @DisplayName("String의 length를 넘어서는 index를 참조하면 StringIndexOutOfBoundsException 발생한다")
-    void 요구사항3(){
+    void 요구사항3() {
         String myString = "abc";
         int outOfIndex = 5;
 
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             myString.charAt(outOfIndex);
         }).isInstanceOf(StringIndexOutOfBoundsException.class)
-                .hasMessageContaining("String index out of range: "+outOfIndex);
+                .hasMessageContaining("String index out of range: " + outOfIndex);
+    }
+
+    @Test
+    void 연속된_delimiter를_split하면_어떻게되는가() {
+        String test = ",,1";
+        for (String str : test.split(",")) {
+            System.out.println(str);
+        }
+    }
+
+    @Test
+    void Matcher_Pattern는_어떻게쓰는가(){
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher("//;\n1;2;3");
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            System.out.println(customDelimiter);
+            String[] tokens= m.group(2).split(customDelimiter);
+            System.out.println(tokens.length);
+        }
     }
 }
