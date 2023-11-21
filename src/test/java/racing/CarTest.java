80aeaee69d171de6029e081bc0b7d760981e44d1
Jiwon Nam <njw724@naver.com>
2023-11-21 20:09:55 +0900

    test: Cars 단위 테스트 클래스 생성 및 테스트 코드 이동

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -57,30 +57,4 @@ public class CarTest {
         String[] carNames = splitCarName("njw,njw2,njw3");
         assertThat(carNames).containsExactly("njw", "njw2", "njw3");
     }
-
-    @Test
-    void 경기가_끝나면_우승한_자동차를_가져올_수_있다() {
-        Car car1 = new Car("A", 3);
-        Car car2 = new Car("B", 2);
-        Car car3 = new Car("C", 1);
-        Cars cars = new Cars(List.of(car1, car2, car3));
-
-        String winners = cars.getWinner();
-
-        assertThat(winners).isEqualTo("A");
-    }
-
-    @Test
-    void 경기가_끝나면_우승한_자동차를_가져올_수_있다_여러명() {
-        Car car1 = new Car("Q", 5);
-        Car car2 = new Car("W", 2);
-        Car car3 = new Car("E", 5);
-
-        Cars cars = new Cars(List.of(car1, car2, car3));
-
-        String winners = cars.getWinner();
-
-        assertThat(winners).isEqualTo("Q,E");
-    }
-
 }
