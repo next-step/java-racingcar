980c12318102e4d1d98b4b2c6ca515d8ac27b2e8
hyeongiii <hyeongiii0811@gmail.com>
2023-11-22 21:31:55 +0900

    test: Cars 이동한 최대값 찾기 (step4)

--- src/test/java/step4/domain/CarsTest.java
+++ src/test/java/step4/domain/CarsTest.java
@@ -1,10 +1,11 @@
 package step4.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step4.strategy.MovingStrategy;
 import step4.strategy.RandomNumMovingStrategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
 
@@ -15,6 +16,14 @@ public class CarsTest {
         MovingStrategy movingStrategy = new RandomNumMovingStrategy();
         Cars cars = new Cars(nameArr, movingStrategy);
 
-        Assertions.assertThat(cars.carCount()).isEqualTo(3);
+        assertThat(cars.carCount()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("자동차들 중 가장 많이 이동한 거리 값을 구한다.")
+    void findMaxDistance() {
+        Cars cars = new Cars(new String[]{"귤", "오렌지", "망고" }, new int[]{1, 3, 1});
+
+        assertThat(cars.findMaxDistance()).isEqualTo(3);
     }
 }
