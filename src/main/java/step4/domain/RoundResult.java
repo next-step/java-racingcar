9594d3d5d89710c5e5f1788c722f5c59c5016d0c
hyeongiii <hyeongiii0811@gmail.com>
2023-11-22 21:40:12 +0900

    refactor: RoundResult 불변 컬랙션 반환 (step4)

--- src/main/java/step4/domain/RoundResult.java
+++ src/main/java/step4/domain/RoundResult.java
@@ -1,13 +1,14 @@
 package step4.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class RoundResult {
     private final List<Integer> roundList = new ArrayList<>();
 
     public List<Integer> roundList() {
-        return this.roundList;
+        return Collections.unmodifiableList(this.roundList);
     }
 
     public void add(int distance) {
