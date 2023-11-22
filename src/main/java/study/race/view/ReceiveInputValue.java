1aac7dffb95b743632718dcf7f82abeda88f9e36
sudo-init <dep.em.k.0905@gmail.com>
2023-11-22 18:54:35 +0900

    commit test

--- src/main/java/study/race/view/ReceiveInputValue.java
+++ src/main/java/study/race/view/ReceiveInputValue.java
@@ -5,17 +5,11 @@ import java.util.Scanner;
 public class ReceiveInputValue {
 
     private Scanner scanner;
-    private final String CAR_INT_INPUT_MESSAGE = "Enter the number of cars.";
     private final String CAR_STRING_INPUT_MESSAGE = "Enter the names of the cars to race(Separation by comma).";
     private final String ROUND_INPUT_MESSAGE = "Enter how many rounds are we going to do.";
 
     public ReceiveInputValue() {
         this.scanner = new Scanner(System.in);
-    }
-
-    public int receiveInputValueForCar() {
-        System.out.println(CAR_INT_INPUT_MESSAGE);
-        return this.scanner.nextInt();
     }
 
     public String receiveInputValueForCarNames() {
