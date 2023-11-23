4d4094e3b5dc30440c91053895eeacf8fcab9d72
Jiwon Nam <njw724@naver.com>
2023-11-24 01:47:28 +0900

    refactor: 랜덤 객체 인스턴스 단에서 생성하도록 수정

--- src/main/java/racing/domain/RandomMoveStrategy.java
+++ src/main/java/racing/domain/RandomMoveStrategy.java
@@ -6,10 +6,11 @@ public class RandomMoveStrategy implements MoveStrategy{
 
     public static final int MAX_RANDOM_NUMBER = 10;
     public static final int MOVE_CONDITION = 4;
+    private final Random random = new Random();
 
     @Override
     public boolean movable() {
-        int randomNumber = new Random().nextInt(MAX_RANDOM_NUMBER);
+        int randomNumber = random.nextInt(MAX_RANDOM_NUMBER);
         return randomNumber >= MOVE_CONDITION;
     }
 }
