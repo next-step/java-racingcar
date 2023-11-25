a8cdf774082eeedb5cb62fbb14e9da1264b856c6
ldy9155 <ldy9155@naver.com>
2023-11-26 01:51:53 +0900

    feat: 위치 음수 체크 기능 추가 및 테스트 코드 추가

--- src/main/java/racinggame/domain/Position.java
+++ src/main/java/racinggame/domain/Position.java
@@ -2,22 +2,20 @@ package racinggame.domain;
 
 public class Position {
 
-    private int position;
-
-    public Position() {
-        this(0);
-    }
+    private final int position;
 
     public Position(int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("위치는 음수가 될수 없습니다");
+        }
         this.position = position;
     }
 
-    public void move() {
-        position = position + 1;
+    public Position move() {
+        return new Position(position + 1);
     }
 
     public int getPosition() {
         return position;
     }
-
 }
