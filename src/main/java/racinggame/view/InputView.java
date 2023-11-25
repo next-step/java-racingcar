7f654570d999c26aebfcb51c3eece505e1cc4472
ldy9155 <ldy9155@naver.com>
2023-11-26 01:55:17 +0900

    feat: 자동차게임 입력 오류 시, 재귀기능 추가 및 문서 수정

--- src/main/java/racinggame/view/InputView.java
+++ src/main/java/racinggame/view/InputView.java
@@ -20,12 +20,22 @@ public class InputView {
     public static RacingInputDTO printRequestGameValue() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String carName = scanner.nextLine();
-        validateInputCarName(carName);
+        try {
+            validateInputCarName(carName);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return printRequestGameValue();
+        }
         List<String> carNames = Arrays.asList(carName.split(","));
 
         System.out.println("시도할 회수는 몇회인가요?");
         String gameCountString = scanner.nextLine();
-        validateInputGameCount(gameCountString);
+        try {
+            validateInputGameCount(gameCountString);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return printRequestGameValue();
+        }
 
         return new RacingInputDTO(carNames, Integer.parseInt(gameCountString));
     }
