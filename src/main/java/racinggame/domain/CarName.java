5e49a19fd3d0514f0a47d74a2a7a17f3675b1405
ldy9155 <ldy9155@naver.com>
2023-11-26 01:48:47 +0900

    feat: 자동차이름 검증 기능 추가

--- src/main/java/racinggame/domain/CarName.java
+++ src/main/java/racinggame/domain/CarName.java
@@ -2,10 +2,21 @@ package racinggame.domain;
 
 public class CarName {
 
-    private String carName;
+    private final String carName;
+    private final int MAX_NAME_LENGTH = 5;
 
     public CarName(String carName) {
+        validateCarName(carName);
         this.carName = carName;
+    }
+
+    private void validateCarName(String carName) {
+        if(carName == null || carName.isBlank()){
+            throw new IllegalArgumentException("자동차 이름이 없습니다.");
+        }
+        if(carName.length() > MAX_NAME_LENGTH){
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과 할 수 없습니다.");
+        }
     }
 
     public String getCarName() {
