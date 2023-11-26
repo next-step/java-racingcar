2cafb3b260a8e0bea9bd4cc2c96ff44c67b5bc73
ldy9155 <ldy9155@naver.com>
2023-11-26 22:07:25 +0900

    refactor: 상수 위치 최상단으로 변경

--- src/main/java/racinggame/domain/Car.java
+++ src/main/java/racinggame/domain/Car.java
@@ -2,10 +2,10 @@ package racinggame.domain;
 
 public class Car {
 
-    private CarName carName;
-    private Position position;
     private final int MIN_MOVEMENT_VALUE = 4;
     private final int NON_NEGATIVE_CHECK = 0;
+    private CarName carName;
+    private Position position;
 
     public Car(String name) {
         this(name, 0);
