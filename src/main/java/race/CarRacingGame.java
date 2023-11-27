eaac642da8e86314f8336b40af7d9bc323f1d375
kgi912 <rudrbdlf1123@gmail.com>
2023-11-28 00:10:07 +0900

    feat: random 값이 4 이상일 경우 자동차 전진 & 입력받은 횟수 만큼 게임을 진행

--- src/main/java/race/CarRacingGame.java
+++ src/main/java/race/CarRacingGame.java
@@ -1,10 +1,25 @@
 package race;
 
+import java.util.ArrayList;
+import java.util.List;
+import race.domain.Car;
+import race.util.RandomNumberUtil;
 import race.view.InputView;
 
 public class CarRacingGame {
     public static void main(String[] args) {
         int numberOfCars = InputView.inputNumberOfCars();
         int numberOfTry = InputView.inputNumberOfTry();
+
+        List<Car> carList = new ArrayList<>();
+        for(int i = 0; i < numberOfCars; i++) {
+            carList.add(new Car());
+        }
+
+        for(int i = 0; i < numberOfTry; i++) {
+            for(Car car : carList){
+                car.move(RandomNumberUtil.getRandomNumber());
+            }
+        }
     }
 }
