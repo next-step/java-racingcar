565e035937cd959fa1ad5aaf17e4d2c5e5059ad7
ldy9155 <ldy9155@naver.com>
2023-12-01 20:20:26 +0900

    test: 리팩토링에 따른 테스트코드 변경 및 winner테스트 코드 추가

--- src/test/java/racinggame/domain/CarTest.java
+++ src/test/java/racinggame/domain/CarTest.java
@@ -3,9 +3,10 @@ package racinggame.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racinggame.domain.rule.TestFalseMoveStrategy;
+import racinggame.domain.rule.TestTrueMoveStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
@@ -16,31 +17,25 @@ class CarTest {
         car = new Car("A", 0);
     }
 
-    @DisplayName("자동차 전진은 입력된 값이 4이상의 경우 position에 1만큼 더해 준다.")
+    @DisplayName("자동차 move에 true를 전달하면 position에 1만큼 더해 준다.")
     @Test
     void 자동차_전진_성공_테스트() {
         //when
-        car.move(4);
+        car.move(new TestTrueMoveStrategy());
         int result = car.getPosition();
 
         //then
         assertThat(result).isEqualTo(1);
     }
 
-    @DisplayName("자동차 전진은 입력된 값이 3이하의 경우 position 변경이 없다.")
+    @DisplayName("자동차 move에 false를 전달하면 position 변경이 없다.")
     @Test
     void 자동차_전진_불가_테스트() {
         //when
-        car.move(3);
+        car.move(new TestFalseMoveStrategy());
         int result = car.getPosition();
 
         //then
         assertThat(result).isEqualTo(0);
-    }
-
-    @DisplayName("자동차 전진은 입력된 값이 음수인 경우 오류가 발생된다.")
-    @Test
-    void 자동차_전진_오류_테스트() {
-        assertThrows(IllegalArgumentException.class, () -> car.move(-1));
     }
 }
