e1954c973c07577b0489d924f0a70d4d8f327ad6
ldy9155 <ldy9155@naver.com>
2023-12-03 14:46:36 +0900

    refactor: 컨트롤러에서 게임시작 부분을 service layer로 변경

--- src/main/java/racinggame/controller/CarGameController.java
+++ src/main/java/racinggame/controller/CarGameController.java
@@ -1,16 +1,10 @@
 package racinggame.controller;
 
-import racinggame.domain.Cars;
-import racinggame.domain.Winner;
 import racinggame.dto.RacingInputDTO;
 import racinggame.dto.RacingResultDTO;
-import racinggame.dto.RacingRoundResultDTO;
+import racinggame.service.RacingGame;
 import racinggame.view.InputView;
 import racinggame.view.ResultView;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class CarGameController {
 
@@ -24,22 +18,13 @@ public class CarGameController {
     }
 
     public void playGame() {
-        RacingInputDTO racingInputDTO = InputView.printRequestGameValue();
         try {
-            RacingResultDTO racingResultDTO = start(racingInputDTO);
+            RacingInputDTO racingInputDTO = InputView.printRequestGameValue();
+            RacingResultDTO racingResultDTO = RacingGame.start(racingInputDTO);
             ResultView.printResult(racingResultDTO);
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             playGame();
         }
-    }
-
-    private RacingResultDTO start(RacingInputDTO racingInputDTO) {
-        Cars carList = Cars.createCars(racingInputDTO.getCarNames());
-        List<RacingRoundResultDTO> racingRoundResultDTOS = IntStream.rangeClosed(1, racingInputDTO.getGameCount())
-                .mapToObj(round -> new RacingRoundResultDTO(carList.carMove()))
-                .collect(Collectors.toList());
-        Winner winner = new Winner(carList.getCarList());
-        return new RacingResultDTO(carList.getCarList(), winner.getWinners(), racingRoundResultDTOS);
     }
 }
