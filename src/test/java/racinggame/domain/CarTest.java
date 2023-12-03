140ca46dedad1814a3a0e30eba191dfbf7b726f0
ldy9155 <ldy9155@naver.com>
2023-12-03 14:49:33 +0900

    test: 전략패턴 수정 및 Cars 객체 테스트코드 추가

--- src/test/java/racinggame/domain/CarTest.java
+++ src/test/java/racinggame/domain/CarTest.java
@@ -1,28 +1,23 @@
 package racinggame.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racinggame.domain.rule.TestFalseMoveStrategy;
-import racinggame.domain.rule.TestTrueMoveStrategy;
+import racinggame.rule.TestFalseMoveStrategy;
+import racinggame.rule.TestTrueMoveStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
-    private Car car;
-
-    @BeforeEach
-    void setup() {
-        car = new Car("A", 0);
-    }
-
     @DisplayName("자동차 move에 true를 전달하면 position에 1만큼 더해 준다.")
     @Test
     void 자동차_전진_성공_테스트() {
+        //given
+        Car carA = new Car("A", 0, new TestTrueMoveStrategy());
+
         //when
-        car.move(new TestTrueMoveStrategy());
-        int result = car.getPosition();
+        carA.move();
+        int result = carA.getPosition();
 
         //then
         assertThat(result).isEqualTo(1);
@@ -31,9 +26,12 @@ class CarTest {
     @DisplayName("자동차 move에 false를 전달하면 position 변경이 없다.")
     @Test
     void 자동차_전진_불가_테스트() {
+        //given
+        Car carB = new Car("B", 0, new TestFalseMoveStrategy());
+
         //when
-        car.move(new TestFalseMoveStrategy());
-        int result = car.getPosition();
+        carB.move();
+        int result = carB.getPosition();
 
         //then
         assertThat(result).isEqualTo(0);
