6f9c16afaba081d31da4034532e2b1041f8ca38f
ParkSeolDev <parkseol.dev@gmail.com>
2023-12-16 15:02:14 +0900

    fix : step2 누락된 함수 추가

--- src/main/java/calculator/CustomSplitter.java
+++ src/main/java/calculator/CustomSplitter.java
@@ -4,12 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class CustomSplitter {
-    private static final String DEFAULT_SEPARATOR = ",|:";
     private static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
     private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_SEPARATOR);
     public String[] split(String input){
         Matcher m = CUSTOM_PATTERN.matcher(input);
         String customSeparator = m.group(1);
         return m.group(2).split(customSeparator);
+    }
+    public boolean isCustomText(String input){
+        Matcher matcher = CUSTOM_PATTERN.matcher(input);
+        if (matcher.find()){
+            return true;
+        }
+        return false;
     }
 }
