5d69d08cc403a199c930a7e75048c425768ad561
ParkSeolDev <parkseol.dev@gmail.com>
2023-12-20 21:53:53 +0900

    refactor : findWinners 메서드 분리

--- src/main/java/racingcar/model/Winners.java
+++ src/main/java/racingcar/model/Winners.java
@@ -10,21 +10,30 @@ public class Winners {
 		winners = new ArrayList<>();
 	}
 
+	public List<Car> winners() {
+		return winners;
+	}
+
 	public void findWinners(Cars cars) {
 		int maxDistance = 0;
 		for (int carNum = 0; carNum < cars.size(); carNum++) {
-			if (cars.car(carNum).distance() == maxDistance) {
-				winners.add(cars.car(carNum));
-			}
-			if (cars.car(carNum).distance() > maxDistance) {
-				winners = new ArrayList<>();
-				winners.add(cars.car(carNum));
-				maxDistance = cars.car(carNum).distance();
-			}
+			addWinnersIfEqualToMax(cars, maxDistance, carNum);
+			maxDistance = addWinnersIfGreaterThanMax(cars, maxDistance, carNum);
 		}
 	}
 
-	public List<Car> winners() {
-		return winners;
+	private void addWinnersIfEqualToMax(Cars cars, int maxDistance, int carNum) {
+		if (cars.car(carNum).distance() == maxDistance) {
+			winners.add(cars.car(carNum));
+		}
+	}
+
+	private int addWinnersIfGreaterThanMax(Cars cars, int maxDistance, int carNum) {
+		if (cars.car(carNum).distance() > maxDistance) {
+			winners = new ArrayList<>();
+			winners.add(cars.car(carNum));
+			maxDistance = cars.car(carNum).distance();
+		}
+		return maxDistance;
 	}
 }
