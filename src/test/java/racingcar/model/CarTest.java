5dbf0d62f0f9fea780aa1d40939d9817a633f1cf
ParkSeolDev <parkseol.dev@gmail.com>
2023-12-21 16:47:21 +0900

    chore : 쓰지 않는 import 제거

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,8 +2,6 @@ package racingcar.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
