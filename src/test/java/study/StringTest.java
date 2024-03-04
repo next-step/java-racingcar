7a97a36a763574afed587d059dab7f8b9d179b6a
동원 <ai06021@gmail.com>
2024-03-04 14:43:19 +0900

    request2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,5 +10,15 @@ public class StringTest {
     void split() {
         String[] division = "1,2".split(",");
         assertThat(division).containsExactly("1", "2");
+
+        String[] word = "1".split(",");
+        assertThat(word).containsExactly("1");
+    }
+
+
+    @Test
+    void substring() {
+        String word = "(1,2)".substring(1, 4);
+        assertThat(word).isEqualTo("1,2");
     }
 }
