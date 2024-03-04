71b155760782a42b4fbd3841fbff7717685b932e
Danden1 <yongha.kwon12@gmail.com>
2024-03-04 22:27:25 +0900

    test: Set 학습 테스트 요구사항 2 구현

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -27,4 +29,14 @@ public class SetCollectionTest {
     void testSetSize() {
         assertThat(numbers.size()).isEqualTo(3);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("Set에 1,2,3이 존재하는 지 테스트")
+    void testSetContainsNumber(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+
+
 }
