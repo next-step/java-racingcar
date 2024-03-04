708dfcd097d0c80043761a7e08fb7f1e9015772e
leeedohyun <ldh19738@gmail.com>
2024-03-05 07:11:28 +0900

    🚀 1단계 - 학습 테스트 실습 (#5202)

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,41 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+    
+    @Test
+    void Set의_크기_확인_테스트() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+    
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void Set에_특정_값이_존재하는지_테스트(final int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void Set에_입력한_값에_따라_결과값이_다른_경우_테스트(final int value, final boolean expected) {
+        assertThat(numbers.contains(value)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,56 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+    @Test
+    void 문자열_분할_테스트() {
+        // given
+        final String str = "1,2";
+
+        // when
+        final String[] split = str.split(",");
+
+        // then
+        assertThat(split).containsExactly("1", "2");
+    }
+    
+    @Test
+    void 문자열_자르기_테스트() {
+        // given
+        final String str = "(1,2)";
+        
+        // when
+        final String substring = str.substring(1, str.length() - 1);
+        
+        // then
+        assertThat(substring).isEqualTo("1,2");
+    }
+    
+    @Test
+    void 문자열에서_특정_위치의_문자_가져오기_테스트() {
+        // given
+        final String str = "abc";
+        
+        // when
+        final char charAt = str.charAt(1);
+        
+        // then
+        assertThat(charAt).isEqualTo('b');
+    }
+    
+    @Test
+    void 문자열에서_위치_값이_벗어나면_예외_발생() {
+        // given
+        final String str = "abc";
+        
+        // when
+        
+        // then
+        assertThatThrownBy(() -> str.charAt(3))
+            .isInstanceOf(IndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range: 3");
+    }
+}
