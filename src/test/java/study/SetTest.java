77b356e9dbeea034bd50622c5231b2f7cf146f98
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-05 03:21:38 +0900

    요구사항2를 만족하는 학습 테스트를 구현하라

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -28,5 +30,10 @@ public class SetTest {
         assertThat(size).isEqualTo(3);
     }
 
-
+    @DisplayName("contains()메서드는 Set에서 그 값이 존재하는지 확인하는 boolean 값을 반환한다")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains_return_true(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
 }
