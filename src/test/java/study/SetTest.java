fde44ab518eb959f2f6eb2a46d601ab1cae20650
kylekim2123 <kyle2123@naver.com>
2024-03-04 18:39:55 +0900

    [STEP 1-4] test: set의 크기를 확인하는 테스트 코드 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void checkSetSize() {
+        assertThat(numbers).hasSize(3);
+    }
+}
