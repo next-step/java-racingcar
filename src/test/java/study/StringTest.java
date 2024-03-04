6b30ad1f0475baa68526aaf5f92f627a42e330e4
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-05 02:25:19 +0900

    요구사항 1을 만족하는 학습 테스트를 구현하라

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,8 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 
     @Test
-    void split() {
+    void split_by_comma() {
         String[] result = "1,2".split(",");
-        assertThat(result).containsExactly("1", "2");
+        assertThat(result).contains("1");
     }
+    @Test
+    void split() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+
 }
