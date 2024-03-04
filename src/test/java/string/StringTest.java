bae9940dd601ab2541ed6c3f365f80b5583d8e6a
m5s3 <devmyoungsoo2@gmail.com>
2024-03-04 20:44:19 +0900

    feat(Requirement3): 요구사항 3 구현

--- src/test/java/string/StringTest.java
+++ src/test/java/string/StringTest.java
@@ -1,8 +1,10 @@
 package string;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -17,4 +19,17 @@ public class StringTest {
         assertThat(existComma.split(",")).contains("1","2");
         assertThat(nonExistComma.split(",")).containsExactly("1");
     }
+
+    @Test
+    @DisplayName("문자열에서 존재하지 index 에 접근하면 예외가 발생한다.")
+    void step1_requirement3() {
+        // Given
+        String target = "abc";
+
+        // When & Then
+        assertThatThrownBy(() -> target.charAt(target.length()))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+
 }
