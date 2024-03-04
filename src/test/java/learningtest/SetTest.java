563b7fbd8f6ceaf4cc313c851758d725baf2a564
deepredk <deepred.kim@gmail.com>
2024-03-04 20:30:03 +0900

    Set Collection에 대한 학습 테스트

---
+++ src/test/java/learningtest/SetTest.java
@@ -0,0 +1,46 @@
+package learningtest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size는 Set에 담긴 요소의 개수를 반환한다")
+    void size() throws Exception {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3 })
+    @DisplayName("Set에 해당 요소가 담겼다면 contains는 True를 반환한다")
+    void contains(int input) throws Exception {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = { "1:true", "2:true", "3:true", "0:false", "4:false" }, delimiter = ':')
+    @DisplayName("contains는 Set에 해당 요소가 담겼는지 여부를 반환한다")
+    void contains2(int input, boolean expected) throws Exception {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
