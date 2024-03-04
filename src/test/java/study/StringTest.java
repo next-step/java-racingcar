38d1c9e091b8d53ec836a858b38b3ec1cf354794
namhyeop <skaguq@gmail.com>
2024-03-05 01:10:00 +0900

    split study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,2 +1,16 @@
-package study;public class StringTest {
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    public void split(){
+        String[] result = "1,2".split(",");
+
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1", "2");
+    }
 }
