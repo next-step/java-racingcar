56bf7906e0524d791cb389d195263fb831d1753a
leeedohyun <ldh19738@gmail.com>
2024-03-04 20:49:27 +0900

    test: Set의 크기 테스트

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,26 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+    
+    @Test
+    void Set의_크기_확인_테스트() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
