ddb6d626927f91ac1bc0f1ac77977015da1c6dcc
leeedohyun <ldh19738@gmail.com>
2024-03-04 20:50:49 +0900

    test: Set에 값이 존재하는지 테스트

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SetTest {
 
@@ -22,5 +24,11 @@ class SetTest {
     @Test
     void Set의_크기_확인_테스트() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+    
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void Set에_특정_값이_존재하는지_테스트(final int value) {
+        assertThat(numbers.contains(value)).isTrue();
     }
 }
