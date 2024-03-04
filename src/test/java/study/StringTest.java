739b6b913d8638c84818e82c7bd1843b4d4a0494
Danden1 <yongha.kwon12@gmail.com>
2024-03-04 22:18:08 +0900

    test: String 학습 테스트 요구사항 3 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -25,5 +26,24 @@ public class StringTest {
 
         assertThat(resultData).isEqualTo("1,2");
     }
+
+    @Test
+    @DisplayName("String charAt(index) 를 이용할 때 정상 범위일 경우, 해당 문자 반환")
+    void testStringCharAtValidIndex() {
+        String testData = "abc";
+        int lastIndex = testData.length() -1;
+
+        assertThat(testData.charAt(lastIndex)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("String charAt(index) 를 이용할 때 범위를 벗어나면, StringIndexOutOfBoundsException 발생")
+    void testStringCharAtInvalidIndex() {
+        String testData = "abc";
+
+        assertThatThrownBy(() -> testData.charAt(testData.length()))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
 
 }
