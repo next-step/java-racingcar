ddcd0d959696b678e6b32f95b1b7849192d7a822
leeedohyun <ldh19738@gmail.com>
2024-03-05 08:56:29 +0900

    feat: 입력이 null 또는 빈문자인 경우 0을 반환

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class StringAddCalculator {
+    public static int splitAndSum(final String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        return 1;
+    }
+}
