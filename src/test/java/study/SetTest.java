12e59c7edbccbce86e3264d0cf0309334b773d4d
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-05 03:12:26 +0900

    요구사항1을 만족하는 학습 테스트를 구현하라

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,32 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test @DisplayName("size()메서드는 numbers set의 크기를 반환한다")
+    void size() {
+        int size = numbers.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+
+}
