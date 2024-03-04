56b2361adf1b321cb14a22c72c17b528ad82f0b0
leeedohyun <ldh19738@gmail.com>
2024-03-04 20:55:10 +0900

    test: Set에_입력한_값에_따라_결과값이_다른_경우_테스트

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class SetTest {
@@ -30,5 +31,11 @@ class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void Set에_특정_값이_존재하는지_테스트(final int value) {
         assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void Set에_입력한_값에_따라_결과값이_다른_경우_테스트(final int value, final boolean expected) {
+        assertThat(numbers.contains(value)).isEqualTo(expected);
     }
 }
