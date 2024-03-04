3e5ebda4d45271950cb73bdd3aff07321f2641fe
kwanik-kor <rhksdlr134@naver.com>
2024-03-04 21:44:33 +0900

    test: Collection size 테스트 코드 추가

---
+++ src/test/java/step1/SetCollectionTest.java
@@ -0,0 +1,32 @@
+package step1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size() 메서드를 통해 Collection의 크기를 검사한다")
+    @Test
+    void sizeOfSet() {
+        final int size = numbers.size();
+
+        assertEquals(3, size);
+    }
+
+}
