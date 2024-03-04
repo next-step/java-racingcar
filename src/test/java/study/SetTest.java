d9f8ba37e747c52912381ce26565e03900abb247
hong yuseok <jericoh@naver.com>
2024-03-04 22:36:02 +0900

    Test: 테스트 코드 리팩토링

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,8 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -27,15 +29,21 @@ public class SetTest {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
-    void testContainsOnlyTrueCase(int input) {
-        assertThat(numbers.contains(input)).isTrue();
-    }
+    @Nested
+    @DisplayName("contains() 테스트")
+    class ContainsTest {
+        @ParameterizedTest
+        @ValueSource(ints = {1, 2, 3})
+        @DisplayName("set에 포함되어 있는 경우 true를 반환한다.")
+        void testOnlyTrueCase(int input) {
+            assertThat(numbers.contains(input)).isTrue();
+        }
 
-    @ParameterizedTest
-    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    void testContainsVariousCase(int input, boolean expected) {
-        assertThat(numbers.contains(input)).isEqualTo(expected);
+        @ParameterizedTest
+        @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+        @DisplayName("set에 포함되어 있는 경우 true를 그렇지 않은 경우 false를 반환한다.")
+        void testVariousCase(int input, boolean expected) {
+            assertThat(numbers.contains(input)).isEqualTo(expected);
+        }
     }
 }
