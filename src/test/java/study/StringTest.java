63ef18aed484bd2d41fc17e45c99b04d3b761225
동원 <ai06021@gmail.com>
2024-03-04 15:24:21 +0900

    request3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringTest {
 
@@ -20,5 +23,12 @@ public class StringTest {
     void substring() {
         String word = "(1,2)".substring(1, 4);
         assertThat(word).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 가져와서 에러 발생여부 확인")
+    void charAt() {
+        String word = "abc";
+        assertThatThrownBy(() -> word.charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class).hasMessageContaining("String index out of range:");
     }
 }
