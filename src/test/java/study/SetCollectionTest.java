3e737dda58ea79027ea58f49ad5f1bed236efef0
Danden1 <yongha.kwon12@gmail.com>
2024-03-04 22:21:27 +0900

    test: Set 학습 테스트 요구사항 1 구현

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetCollectionTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set Collection의 크기 확인")
+    void testSetSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
