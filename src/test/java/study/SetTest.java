fcfea7ea8b036c53a7eb8241aba5b7b558b13759
Duyan Kim <devduyan@gmail.com>
2024-03-05 23:30:19 +0900

    test: Set Collection 학습테스트 요구사항1 - size

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,32 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("요구사항1: set의 크기를 확인할 수 있다")
+    void set_size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+}
