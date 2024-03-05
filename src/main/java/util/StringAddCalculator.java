795b6d41f61c2c57eebb07a7e72ff4b551ad665c
kwanik-kor <rhksdlr134@naver.com>
2024-03-05 22:48:41 +0900

    feat: 숫자변환 및 유효성 검증 로직 추가

--- src/main/java/util/StringAddCalculator.java
+++ src/main/java/util/StringAddCalculator.java
@@ -15,6 +15,8 @@ public class StringAddCalculator {
      *
      * @param value 문자열
      * @return 분리한 숫자의 합. 단, null이나 빈 문자열의 경우 0을 반환한다.
+     * @throws NumberFormatException 구분자에 의해 분리된 문자열 중 숫자가 아닌 값이 있는 경우
+     * @throws IllegalArgumentException 구분자에 의해 분리된 문자열 중 음수가 있는 경우
      */
     public static int splitAndSum(String value) {
         if (isBlank(value)) {
@@ -41,5 +43,19 @@ public class StringAddCalculator {
     private static Matcher customDelimiterMatcher(String value) {
         return Pattern.compile(CUSTOM_DELIMITER_PATTERN_REGEXP)
                 .matcher(value);
+    }
+
+    private static int mapToInt(String value) {
+        int intValue = Integer.parseInt(value);
+
+        assertPositive(intValue);
+
+        return intValue;
+    }
+
+    private static void assertPositive(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("문자열 덧셈 계산기는 음수를 지원하지 않습니다.");
+        }
     }
 }
