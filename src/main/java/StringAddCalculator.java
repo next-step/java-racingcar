163bfe3cf10dada6988e17cadd6b91ba5fcdda03
Shinny <siniya979@gmail.com>
2024-03-06 00:14:49 +0900

    refactor : 문자열 덧셈 계산기 리팩토링
    1. 배열을 리스트로 변경하였다( 불공변 )
    2. 정규식을 상수로 변경하였다.
    3. Pattern.compile 이 매번 실행되는 것을 막기 위해 Pattern 객체 자체를 상수로 변경하였다.

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,40 +1,48 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+
+    private static final int EMPTY_INPUT_RESULT = 0;
+
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER_REGEXP = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEXP);
+
     public static int splitAndSum(String text){
 
-        if(checkEmptyOrNull(text)) return 0;
+        if(checkEmptyOrNull(text)) return EMPTY_INPUT_RESULT;
 
-        String[] tokens= stringSplit(text);
+        List<String> tokens= List.of(stringSplit(text));
         checkNegativeNum(tokens);
 
         return sum(tokens);
     }
 
     private static String[] stringSplit(String text){
-
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        // 숫자를 직접 사용하는 매직 넘버 지양하기
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
             return m.group(2).split(customDelimiter);
         }
-        return text.split(",|:");
+        return text.split(DEFAULT_DELIMITER);
     }
 
     private static boolean checkEmptyOrNull(String text){
-        return text == null || text.isEmpty();
+        return text == null || text.isBlank();
     }
 
-    private static void checkNegativeNum(String[] text){
-        boolean flag = Arrays.stream(text).mapToInt(Integer::parseInt).anyMatch(n -> n < 0);
+    private static void checkNegativeNum(List<String> text){
+        boolean flag = text.stream().mapToInt(Integer::parseInt).anyMatch(n -> n < 0);
         if(flag)
             throw new RuntimeException("Negative Number is not allowed");
     }
 
-    private static int sum(String[] tokens){
-        return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
+    private static int sum(List<String> tokens){
+        return tokens.stream().mapToInt(Integer::parseInt).sum();
     }
 
 }
