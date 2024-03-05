7eed379e0bf8bb4f7bda2bf7501496a010dade00
이동옥 <moveok@idong-og-ui-MacBookAir.local>
2024-03-05 21:08:07 +0900

    [2-1] size 메소드

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,31 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size()를 활용해 Set의 크기를 확인하는 Test")
+    void size() {
+        int setSize = numbers.size();
+        assertThat(setSize).isEqualTo(3);
+    }
+
+}
