49b6f32420dd9f5850146d1d093cfff9dcaa03af
sunghyun <tjdgus2319@gmail.com>
2024-03-05 23:32:22 +0900

    요구사항 1 누락내용 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,20 +1,24 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 public class StringTest {
+
     @Test
-    void split() {
+    void testSplit() {
         String[] result = "1,2".split(",");
-        assertThat(result).containsExactly("1","2");
+        String[] result2 = "1".split(",");
+
+        assertThat(result).containsExactly("1", "2");
+        assertThat(result2).contains("1");
     }
 
     @Test
-    void substring() {
-        String result = "(1,2)".substring(1,4);
+    void testSubstring() {
+        String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
     }
-
 }
