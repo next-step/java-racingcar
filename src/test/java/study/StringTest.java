d826ef24c89c8576a347ea48548a371b3bae92b7
Sangmin <sm9171@nate.com>
2024-03-05 11:28:10 +0900

    feat: Add StringTest

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,48 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+public class StringTest {
+
+	@DisplayName("split 메서드를 활용한 문자열 분리")
+	@Test
+	void split() {
+		// given
+		String input = "1,2";
+
+		// when
+		String[] split = input.split(",");
+
+		// then
+		assertThat(split).containsExactly("1", "2");
+	}
+
+	@DisplayName("substring 메서드를 활용한 문자열 추출")
+	@Test
+	void removeParentheses() {
+		// given
+		String input = "(1,2)";
+
+		// when
+		String actual = input.substring(1, input.length() - 1);
+
+		// then
+		assertThat(actual).isEqualTo("1,2");
+	}
+
+	@DisplayName("charAt 메서드를 활용한 문자 추출 예외 처리")
+	@Test
+	void getSpecificCharacters() {
+		// given
+		String input = "abc";
+
+		// when & then
+		assertThatExceptionOfType(IndexOutOfBoundsException.class)
+				.isThrownBy(() -> {
+					input.charAt(3);
+				}).withMessageMatching("String index out of range: \\d");
+	}
+}
