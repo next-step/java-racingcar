3a01ea11bae8203a7501ef18dbbe87b69ac4a257
Dongock <58582148+Dongock@users.noreply.github.com>
2024-03-05 21:46:02 +0900

    Initial Commit (#5233)
    * split study test
    
    * [1-2] substring 메소드
    
    * [1-3] charAt 메소드
    
    * [2-1] size 메소드
    
    * [2-2] contains 메소드
    
    * [2-1] assertJ의 hasSize 이용
    
    * [2-3] csvSource annotation
    
    ---------
    
    Co-authored-by: 이동옥 <moveok@idong-og-ui-MacBookAir.local>

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,48 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size()를 활용해 Set의 크기를 확인하는 Test")
+    void size() {
+        int setSize = numbers.size();
+        assertThat(numbers).hasSize(setSize);
+    }
+
+    @DisplayName("Set의 contains()를 이용해 값이 존재하는지 확인하는 Test")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @DisplayName("JUnit CsvSource를 이용한 Test")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void csvSource(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StringTest {
+
+    @Test
+    void split1() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void split2() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    void subString() {
+        String substring = "(1,2)".substring(1, 4);
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 Test")
+    void charAt1() {
+        char charAt = "abc".charAt(0);
+        assertThat(charAt).isEqualTo("a");
+    }
+
+    @Test
+    @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값 벗어났을 때 Exception Test")
+    void charAt2() {
+        int charLength = "abc".length();
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            "abc".charAt(charLength+1);
+        });
+    }
+}
