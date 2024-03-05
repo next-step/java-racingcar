7785ea87b8b7609cbde8622bee9e82725d8afa41
park-cheonho <cheonho08@naver.com>
2024-03-05 15:13:26 +0900

    test: Set Collection에 대한 학습 테스트 - 요구사항3

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -33,7 +34,6 @@ public class SetTest {
     @Test
     @DisplayName("Set Collection에 대한 학습 테스트 - 요구사항 1")
     void checkSize(){
-        //when then
         assertThat(numbers.size()).isEqualTo(3);
     }
 
@@ -41,9 +41,13 @@ public class SetTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     void checkSetWhatContains(int testValue){
-        //when then -> ParameterizedTest
         assertThat(numbers.contains(testValue)).isTrue();
     }
 
-    // 요구사항 3
+    @DisplayName("Set Collection에 대한 학습 테스트 - 요구사항 3")
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void judgeParamValSuitableForSet(int input, boolean expected){
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
 }
