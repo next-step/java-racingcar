033dfe2385b44da82051e89a7bdb02a9851cce3c
yushin <kkad45@naver.com>
2024-03-05 22:56:58 +0900

    feat/step1 SetCollection 요구사항2 구현

--- src/test/java/study/step1/SetTest.java
+++ src/test/java/study/step1/SetTest.java
@@ -3,6 +3,8 @@ package study.step1;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,4 +26,9 @@ public class SetTest {
         Assertions.assertThat(numbers).hasSize(3);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contains(Integer input) {
+        Assertions.assertThat(numbers).contains(input);
+    }
 }
