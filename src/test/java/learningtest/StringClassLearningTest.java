b97df4db376e048ed130b0c4100cb46280d60ffa
hellonayeon <hellonykwon@gmail.com>
2024-03-05 22:13:40 +0900

    Test: String 학습테스트 - charAt() 메서드 테스트

--- src/test/java/learningtest/StringClassLearningTest.java
+++ src/test/java/learningtest/StringClassLearningTest.java
@@ -2,6 +2,7 @@ package learningtest;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.text.MessageFormat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -57,4 +58,48 @@ public class StringClassLearningTest {
         }
     }
 
+    // 요구사항 3
+    // "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+    // String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+    // JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+
+    @Nested
+    @DisplayName("charAt() 메서드 학습 테스트")
+    class ChatAt {
+
+        @Test
+        @DisplayName("[성공] 문자열 'a,b,c' 에서 2번째 위치의 문자 가져오기")
+        void getTwiceCharFromABCString() {
+            String testString = "a,b,c";
+            char actual = testString.charAt(2);
+
+            assertThat(actual).isEqualTo('b');
+        }
+
+        @Test
+        @DisplayName("[실패] 인덱스 == 문자열 길이, 문자열 위치 값을 벗어나면 StringIndexOutOfBoundsException 발생")
+        void charAtOutOfIndex() {
+            String testString = "a,b,c";
+            int index = testString.length();
+
+            assertStringIndexOutOfBoundsException(testString, index);
+        }
+
+        @Test
+        @DisplayName("[실패] 인덱스 == -1, 인덱스 범위를 넘어선 문자를 가져올 경우 StringIndexOutOfBoundsException 발생")
+        void chatAtMinusIndex() {
+            String testString = "a,b,c";
+            int index = -1;
+
+            assertStringIndexOutOfBoundsException(testString, index);
+        }
+    }
+
+    private void assertStringIndexOutOfBoundsException(String testString, int index) {
+        assertThatThrownBy(() -> {
+            testString.charAt(index);
+        })
+            .isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessage(MessageFormat.format("String index out of range: {0}", index));
+    }
 }
