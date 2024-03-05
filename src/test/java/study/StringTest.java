f945e7860415554fe62702dca6c93bf57bfbf723
이동옥 <moveok@idong-og-ui-MacBookAir.local>
2024-03-05 20:47:06 +0900

    [1-2] substring 메소드

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,8 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 
     @Test
-    void split() {
+    void split1() {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
+        assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    void split2() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    void subString() {
+        String substring = "(1,2)".substring(1, 4);
+        assertThat(substring).isEqualTo("1,2");
+    }
+    
 }
