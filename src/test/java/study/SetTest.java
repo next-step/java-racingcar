a94024c5591e5003ef2ae0b378a38951cdd7d715
park-cheonho <cheonho08@naver.com>
2024-03-05 14:41:30 +0900

    test: Set Collection에 대한 학습 테스트 - 요구사항 1

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,9 +2,13 @@ package study;
 
 import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Description("Set Collection에 대한 학습 테스트")
 public class SetTest {
@@ -24,7 +28,13 @@ public class SetTest {
 
     // Test Case 구현
 
-    // 요구사항 1
+    @Test
+    @DisplayName("Set Collection에 대한 학습 테스트 - 요구사항 1")
+    void checkSize(){
+
+        //when then
+        assertThat(numbers.size()).isEqualTo(3);
+    }
 
     // 요구사항 2
 
