df231942e6f3a3491f66b80ec79c1f30943aca95
park-cheonho <cheonho08@naver.com>
2024-03-05 14:54:24 +0900

    test: Set Collection에 대한 학습 테스트 - 요구사항 2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,8 @@ import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -31,12 +33,17 @@ public class SetTest {
     @Test
     @DisplayName("Set Collection에 대한 학습 테스트 - 요구사항 1")
     void checkSize(){
-
         //when then
         assertThat(numbers.size()).isEqualTo(3);
     }
 
-    // 요구사항 2
+    @DisplayName("Set Collection에 대한 학습 테스트 - 요구사항 2")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void checkSetWhatContains(int testValue){
+        //when then -> ParameterizedTest
+        assertThat(numbers.contains(testValue)).isTrue();
+    }
 
     // 요구사항 3
 }
