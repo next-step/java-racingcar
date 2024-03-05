6af8ba5a4c8a2137869184ab8d9edda9056ced8d
yushin <kkad45@naver.com>
2024-03-05 23:05:27 +0900

    feat/step1 SetCollection 요구사항3 구현

--- src/test/java/study/step1/SetTest.java
+++ src/test/java/study/step1/SetTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -27,8 +28,20 @@ public class SetTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3})
+    @ValueSource(ints = {1, 2, 3})
     void contains(Integer input) {
         Assertions.assertThat(numbers).contains(input);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true"})
+    void containsSuccess(Integer number, Boolean result) {
+        Assertions.assertThat(numbers.contains(number)).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4,false", "null,false"}, nullValues = "null")
+    void containsFail(Integer number, Boolean result) {
+        Assertions.assertThat(numbers.contains(number)).isEqualTo(result);
     }
 }
