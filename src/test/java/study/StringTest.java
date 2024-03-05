47cee810d7d465073135048b6614ef4fe0fe6717
이동옥 <moveok@idong-og-ui-MacBookAir.local>
2024-03-05 20:58:34 +0900

    [1-3] charAt 메소드

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringTest {
 
@@ -25,5 +27,20 @@ public class StringTest {
         String substring = "(1,2)".substring(1, 4);
         assertThat(substring).isEqualTo("1,2");
     }
-    
+
+    @Test
+    @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 Test")
+    void charAt1() {
+        char charAt = "abc".charAt(0);
+        assertThat(charAt).isEqualTo("a");
+    }
+
+    @Test
+    @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값 벗어났을 때 Exception Test")
+    void charAt2() {
+        int charLength = "abc".length();
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            "abc".charAt(charLength+1);
+        });
+    }
 }
