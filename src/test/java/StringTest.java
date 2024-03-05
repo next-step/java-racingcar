e1460223e0ea45e110ffb884fb291be5903565a2
김정희 <jeongheekim@Jeongheeui-MacBookAir.local>
2024-03-06 00:04:55 +0900

    feat. 요구사항1

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author jeongheekim
+ * @date 3/5/24
+ */
+public class StringTest {
+
+    @Test
+    @DisplayName("String 변수값을 ,로 split하면 해당 원소를 순서대로 포함한다.")
+    void stringSplitElementOrderlyContainsTest() {
+        String str = "1,2";
+        String[] split = str.split(",");
+        assertThat(split).containsExactly("1","2");
+
+    }
+
+    @Test
+    @DisplayName("String 변수값을 ,로 split하면 해당 원소를 포함한다.")
+    void stringSplitElementContainsTest() {
+        String str = "1,";
+        String[] split = str.split(",");
+        assertThat(split).contains("1");
+
+    }
+
+}
