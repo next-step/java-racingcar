209c8d6d46572b16e83b32dbe613b637ad3e15fb
leejinwoo <leejinwoo1126@gmail.com>
2024-03-05 10:03:07 +0900

    test: String 클래스에 대한 학습 테스트

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,38 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    void split() {
+        String given = "1,2";
+
+        String[] result = given.split(",");
+
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String given = "(1,2)";
+
+        String result = given.substring(1, given.length() - 1);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("음수 값이나 문자열의 길이를 초과할 경우 StringIndexOutOfBoundsException 반환한다")
+    @Test
+    void charAt() {
+        String given = "abc";
+
+        assertThatThrownBy(() -> given.charAt(-1))
+                .isInstanceOfAny(StringIndexOutOfBoundsException.class);
+    }
+
+}
