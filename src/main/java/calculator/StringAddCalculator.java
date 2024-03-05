cb4cd6da093c437ade6e8e1ea9b843451c166a60
leeedohyun <ldh19738@gmail.com>
2024-03-05 09:36:27 +0900

    style: 변수명 수정

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,25 +4,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    public static int splitAndSum(final String s) {
-        if (s == null || s.isEmpty()) {
+    public static int splitAndSum(final String line) {
+        if (line == null || line.isEmpty()) {
             return 0;
         }
 
-        final Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
+        final Matcher m = Pattern.compile("//(.)\n(.*)").matcher(line);
         if (m.find()) {
             final String customDelimiter = m.group(1);
             final String[] tokens = m.group(2).split(customDelimiter);
             return calculateSum(tokens);
         }
 
-        final String[] strings = s.split(",|:");
-        return calculateSum(strings);
+        final String[] tokens = line.split(",|:");
+        return calculateSum(tokens);
     }
 
-    private static int calculateSum(final String[] strings) {
+    private static int calculateSum(final String[] tokens) {
         int sum = 0;
-        for (final String value : strings) {
+        for (final String value : tokens) {
             final int number = convertStringToInt(value);
             validatePositive(number);
             sum += number;
