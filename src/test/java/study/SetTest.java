2dd7478450e36693162c6cb772978d4741fa246e
kylekim2123 <kyle2123@naver.com>
2024-03-05 19:43:07 +0900

    [STEP 1] 학습 테스트 실습 (kylekim2123, 김영주) (#5198)
    * [STEP 1-1] test: 문자열 자르기 기능에 대한 정상 테스트 코드 추가
    
    * [STEP 1-2] test: 부분 문자열 기능에 대한 정상 테스트 코드 추가
    
    * [STEP 1-3] test: 특정 위치의 문자 조회 기능에 대한 예외 테스트 코드 추가
    
    * [STEP 1-4] test: set의 크기를 확인하는 테스트 코드 추가
    
    * [STEP 1-5] test: set에 특정 원소가 존재하는지 확인하는 테스트 코드 추가
    
    * [STEP 1-6] test: set에 특정 원소가 존재하는지 혹은 존재하지 않는지 확인하는 테스트 코드 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,44 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void checkSetSize() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void containsNumber(int number) {
+        assertThat(numbers).contains(number);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void containsOrNotContainsNumber(int number, boolean isContained) {
+        assertThat(numbers.contains(number))
+                .isEqualTo(isContained);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,43 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+    @Test
+    void splitStringsByComma() {
+        final String input = "1,2";
+        final String[] splitInput = input.split(",");
+
+        assertThat(splitInput).containsExactly("1", "2");
+    }
+
+    @Test
+    void splitSingleStringByComma() {
+        final String input = "1";
+        final String[] splitInput = input.split(",");
+
+        assertThat(splitInput).containsExactly("1");
+    }
+
+    @Test
+    void subString() {
+        final String input = "(1,2)";
+        final String subStringInput = input.substring(1, input.length() - 1);
+
+        assertThat(subStringInput).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("chatAt 메서드를 통해 범위가 벗어나는 문자를 가져오는 경우 예외가 발생한다.")
+    void getCharacterAtIndexOutOfRange() {
+        final String input = "abc";
+        final int indexOutOfRange = input.length();
+
+        assertThatThrownBy(() -> input.charAt(indexOutOfRange))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
