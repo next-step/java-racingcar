42741e51b982235e3b056d3df361d55ff1ff4279
jaejeong1 <wowjd555@gmail.com>
2024-03-06 22:06:28 +0900

    requirement 3 : charAt test

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @DisplayName("1,2 를 , 로 split 하면 1과 2가 반환된다")
@@ -33,5 +34,26 @@ public class StringTest {
         String result = str.substring(1, str.length() - 1);
 
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @DisplayName("abc 값이 주어졌을 때 charAt 메소드를 활용해 특정 위치의 문자를 가져온다")
+    @Test
+    public void test4() {
+        String str = "abc";
+
+        char result = str.charAt(1);
+
+        assertThat(result).isEqualTo('b');
+    }
+
+    @DisplayName("abc 값이 주어졌을 때 chatAt 메서드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException 이 발생한다")
+    @Test
+    public void test5() {
+        String str = "abc";
+
+        assertThatThrownBy(() -> {
+            str.charAt(str.length());
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
