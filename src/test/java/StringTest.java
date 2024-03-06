43fcd121108db4104034c46fd3a47845f55d1dae
jaejeong1 <wowjd555@gmail.com>
2024-03-06 22:00:20 +0900

    requirement 2 : substring test

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -23,4 +23,15 @@ public class StringTest {
 
         assertThat(result).containsExactly("1");
     }
+
+
+    @DisplayName("(1,2) 값이 주어졌을 때 String 의 substring 메소드를 활용해 () 을 제거하고 1,2를 반환한다")
+    @Test
+    public void test3() {
+        String str = "(1,2)";
+
+        String result = str.substring(1, str.length() - 1);
+
+        assertThat(result).isEqualTo("1,2");
+    }
 }
