d34ca0e50590a5d93cb5580ee962083856f30684
deepredk <deepred.kim@gmail.com>
2024-03-06 22:35:31 +0900

    feat: 하나의 자동차가 전진을 시도한다

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+public class Car {
+
+    private final DrivingStrategy drivingStrategy;
+    private int drivingDistance = 0;
+
+    public Car(DrivingStrategy drivingStrategy) {
+        this.drivingStrategy = drivingStrategy;
+    }
+
+    public void drive() {
+        if (drivingStrategy.canDrive()) {
+            drivingDistance++;
+        }
+    }
+
+    public int drivingDistance() {
+        return drivingDistance;
+    }
+}
