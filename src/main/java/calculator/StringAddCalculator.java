ef96f693ead0251d9571ce8276894615f4cf69a6
park-cheonho <cheonho08@naver.com>
2024-03-06 15:06:07 +0900

    feat: 믄자열 덧셈 계산기 추가기능

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,6 +1,13 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+
+    private static String CUSTOM_PATTERN = "//(.)\n(.*)";
+    private static String DEFAULT_PATTERN = ",|:";
+
     public static int splitAndSum(String input){
          if(input == null || input.isEmpty()) {
             return 0;
@@ -10,16 +17,27 @@ public class StringAddCalculator {
     private static int calculate(String input) {
         int result = 0;
         String[] numbers = split(input);
-        for(String number : numbers) {
+         for(String number : numbers) {
             result += parseNumber(number);
         }
         return result;
     }
     // 문자열 분리
     public static String[] split(String input) {
-        return input.split(",");
+
+        Matcher m = Pattern.compile(CUSTOM_PATTERN).matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens= m.group(2).split(customDelimiter);
+            return tokens;
+        }
+        return input.split(DEFAULT_PATTERN);
     }
-    public static int parseNumber(String input){
-        return Integer.parseInt(input);
+    public static int parseNumber(String input) {
+        int number = Integer.parseInt(input);
+        if(number < 0) {
+            throw new RuntimeException("음수를 입력 할 수 없습니다. 입력값을 확인해주세요.");
+        }
+        return number;
     }
 }
