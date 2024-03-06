a2e20c2705c49e902131e324ecaf5c9c1ef94648
박소민 <qkrthals0524@naver.com>
2024-03-06 21:07:35 +0900

    StringAddCalculator 구현 및 테스트

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,42 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        if (input.contains("//") && input.contains("\n")) {
+            String separator = customSeparator(input);
+            String custom_input = input.substring(input.indexOf("\n")+1);
+            String[] inputs = custom_input.split(separator);
+            return sum(inputs);
+        }
+
+        String[] inputs = input.split(",|:");
+        return sum(inputs);
+    }
+
+    private static String customSeparator(String input) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (!matcher.find()) {
+            throw new RuntimeException("정규식 패턴에 일치하는 구분자가 없습니다.");
+        }
+        return matcher.group(1);
+    }
+
+    private static int sum(String[] inputs) {
+        int result = 0;
+        for (String input:inputs) {
+            int item = Integer.parseInt(input);
+            if (item < 0) {
+                throw new RuntimeException();
+            }
+            result+=item;
+        }
+        return result;
+    }
+
+}
