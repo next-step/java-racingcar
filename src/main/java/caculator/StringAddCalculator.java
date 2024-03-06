da70a593c27641102b8a288da64de3183b4aa0c9
hong yuseok <jericoh@naver.com>
2024-03-06 22:23:05 +0900

    Refactor: 메서드 분리, Stream을 이용한 코드 개선, Regex 상수 처리

--- src/main/java/caculator/StringAddCalculator.java
+++ src/main/java/caculator/StringAddCalculator.java
@@ -1,37 +1,49 @@
 package caculator;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.util.Objects.isNull;
 
 public class StringAddCalculator {
+    private static final String REGEX_FOR_DEFAULT_DELIMITER = ",|:";
+    private static final String REGEX_FOR_CUSTOM_DELIMITER = "//(.)\n(.*)";
+
     public static int splitAndSum(String input) {
-        int result = 0;
-
-        if(isNull(input) || input.isEmpty()) {
-           return result;
+        if(isNullOrEmpty(input)) {
+           return 0;
         }
 
-        String[] numbers;
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            numbers= m.group(2).split(customDelimiter);
-        }else{
-            numbers = input.split(",|:");
-        }
+        int[] numbers = getNumbersFromInput(input);
 
-        for(int i = 0; i < numbers.length; i++) {
-            int number = Integer.parseInt(numbers[i]);
+        return getSumOfNumbers(numbers);
+    }
 
-            if(number < 0) {
-                throw new RuntimeException();
-            }
+    private static boolean isNullOrEmpty(String input) {
+        return isNull(input) || input.isEmpty();
+    }
 
-            result += number;
-        }
+    private static int[] getNumbersFromInput(String input) {
+        Matcher customDelimiterMatcher = Pattern.compile(REGEX_FOR_CUSTOM_DELIMITER).matcher(input);
 
-        return result;
+        String[] numbers = customDelimiterMatcher.find() ?
+                customDelimiterMatcher.group(2).split(customDelimiterMatcher.group(1)) :
+                input.split(REGEX_FOR_DEFAULT_DELIMITER);
+
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private static int getSumOfNumbers(int[] numbers) {
+        return Arrays.stream(numbers)
+                .reduce(0, (sum, number) -> {
+                    if(number < 0) {
+                        throw new RuntimeException();
+                    }
+
+                    return sum + number;
+                });
     }
 }
