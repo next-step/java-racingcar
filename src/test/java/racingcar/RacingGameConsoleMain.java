ea93f60ebe61d0a6fb219b030e7be9fbdeea730d
deepredk <deepred.kim@gmail.com>
2024-03-06 23:07:37 +0900

    feat: UI 구현

---
+++ src/test/java/racingcar/RacingGameConsoleMain.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class RacingGameConsoleMain {
+
+    public static void main(String[] args) {
+        RacingGameConsole racingGameConsole = new RacingGameConsole();
+        racingGameConsole.start();
+    }
+}
