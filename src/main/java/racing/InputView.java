450cb7f495d1dc141a5245416aa3e3259f156317
nimoh <spakers38@gmail.com>
2024-03-06 21:23:09 +0900

    feat: [Step 3] 시도할 회수를 입력 받고 값을 반환하는 InputView.inpuutedTryCount() 구현

--- src/main/java/racing/InputView.java
+++ src/main/java/racing/InputView.java
@@ -5,17 +5,32 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public static int inputtedCarCount() {
+    public static int inputtedCarCount(Scanner scanner) {
 
         System.out.println("자동차 대수는 몇 대 인가요?");
-
         while (true) {
-            try (Scanner scanner = new Scanner(System.in)) {
+            try {
                 int result = scanner.nextInt();
-                scanner.nextLine();
+                scanner.nextLine();     // 버퍼 제거
                 return result;
             } catch (InputMismatchException e) {
                 System.out.println("숫자를 입력해주세요");
+                scanner.nextLine();     // 버퍼 제거
+            }
+        }
+    }
+
+    public static int inputtedTryCount(Scanner scanner) {
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        while (true) {
+            try {
+                int result = scanner.nextInt();
+                scanner.nextLine();     // 버퍼 제거
+                return result;
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력해주세요");
+                scanner.nextLine();     // 버퍼 제거
             }
         }
     }
