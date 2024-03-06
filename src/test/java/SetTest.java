e0612f8ba8c2b40aa8469f9477a4adbcd6a409ef
jaejeong1 <wowjd555@gmail.com>
2024-03-06 22:13:48 +0900

    requirement 3 : parameterized test

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -32,5 +33,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int input) {
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @DisplayName("HashSet에 1, 1, 2, 3을 추가하고 contains 메서드로 1, 2, 3을 호출하면 true를 반환, 4, 5를 호출하면 false를 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
