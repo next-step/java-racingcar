25a193bdf9ae2708882702aaf543670d9ceb31a1
nimoh <spakers38@gmail.com>
2024-03-06 22:30:27 +0900

    test: [Step 3] Car와 Position 클래스 단위 테스트 추가

---
+++ src/test/java/racing/CarTest.java
@@ -0,0 +1,55 @@
+package racing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CarTest {
+
+    Car car;
+
+    @BeforeEach
+    void before() {
+        car = new Car();
+    }
+
+    @Test
+    @DisplayName("Car 객체 생성 시 position은 1 && 파라미터와 Car의 위치가 일치하면 true")
+    void Car_constructor() {
+        assertThat(car.hasPosition(1)).isTrue();
+        assertThat(car.hasPosition(2)).isFalse();
+    }
+
+    @Test
+    @DisplayName("forward() 호출 시 position 1 증가")
+    void forward() {
+        int startPosition = 1;
+        assertThat(car.hasPosition(startPosition)).isTrue();
+        car.forward();
+        assertThat(car.hasPosition(startPosition + 1)).isTrue();
+    }
+
+    @Test
+    @DisplayName("carPositions() 호출 시 List<Car>를 List<Position>으로 변환")
+    void carPositions() {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+
+        car1.forward();
+        car1.forward();
+        car2.forward();
+
+        List<Car> carList = List.of(car1, car2, car3);
+
+        List<Position> positions = Car.carPositions(carList);
+
+        assertThat(positions).containsSubsequence(new Position(3), new Position(2), new Position(1));
+
+    }
+
+}
---
+++ src/test/java/racing/PositionTest.java
@@ -0,0 +1,34 @@
+package racing;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PositionTest {
+
+    @ParameterizedTest(name = "Position이 {arguments}인 경우 동치비교 ")
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void hasPosition(int input) {
+        Position position = new Position(input);
+        assertThat(position.hasPosition(new Position(input))).isTrue();
+    }
+
+    @ParameterizedTest(name = "[{index}] position이 {0}일 때, add({1})")
+    @CsvSource(value = {"2, 3", "1,5", "2,7", "3,4", "1,6"})
+    @DisplayName("add() 호출 시 파라미터 만큼 position 필드에 더함")
+    void add(int augend, int addend) {
+        Position position = new Position(augend);
+        position.add(addend);
+        assertThat(position.hasPosition(new Position(augend + addend))).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    @DisplayName("position 필드가 같으면 동등한 Position 객체")
+    void testEquals(int input) {
+        assertThat(new Position(input)).isEqualTo(new Position(input));
+    }
+}
