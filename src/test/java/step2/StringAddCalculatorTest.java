eab4c2f15c65683aa64cb49fda416a935b82434d
sinterhoo <whwlsdn96@gmail.com>
2024-03-07 00:19:53 +0900

    자동차 경주 2단계 - 문자열 덧셈 계산기

---
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -0,0 +1,57 @@
+package step2;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringAddCalculatorTest {
+
+    private final StringAddCalculator stringAddCalculator = new StringAddCalculator();
+
+    @Test
+    public void splitAndSum_null_또는_빈문자() {
+        int result = stringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = stringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_숫자하나() throws Exception {
+        int result = stringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_쉼표구분자() throws Exception {
+        int result = stringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+        int result = stringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = stringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() throws Exception {
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void 소수점을_가진_값이면_예외를_터트린다() throws Exception {
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("1,2.5,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
