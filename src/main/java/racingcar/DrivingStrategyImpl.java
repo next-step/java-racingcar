59a687ed89f02ae21cf79815f9b4185d776f8c47
deepredk <deepred.kim@gmail.com>
2024-03-07 15:51:13 +0900

    refactor: 매직넘버를 상수로 만들어 이름 붙임

--- src/main/java/racingcar/DrivingStrategyImpl.java
+++ src/main/java/racingcar/DrivingStrategyImpl.java
@@ -4,10 +4,13 @@ import java.util.Random;
 
 public class DrivingStrategyImpl implements DrivingStrategy {
 
+    public static final int RANGE_UPPER_BOUND = 10;
+    public static final int THRESHOLD = 4;
+
     private final Random random = new Random();
 
     @Override
     public boolean canDrive() {
-        return random.nextInt(10) >= 4;
+        return random.nextInt(RANGE_UPPER_BOUND) >= THRESHOLD;
     }
 }
