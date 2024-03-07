8759597c8ee5e556df83f307cc9adf47ba5a51f7
동원 <ai06021@gmail.com>
2024-03-07 12:48:57 +0900

    빈문자열_null

---
+++ src/test/java/calculator/StringAddCalculator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+public class StringAddCalculator {
+
+
+    String text;
+
+    static int splitAndSum(String text) {
+        if (isEmpty(text))
+            return 0;
+        return 0;
+    }
+
+
+    static boolean isEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+}
---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class StringAddCalculatorTest {
+
+    @DisplayName("빈문자 또는 null")
+    @Test
+    public void EmptyCheck() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+
+        result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+}
