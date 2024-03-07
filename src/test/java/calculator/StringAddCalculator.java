073d84515a8559f7c6191b11e034da59839cde60
동원 <ai06021@gmail.com>
2024-03-07 13:09:58 +0900

    구분자커스텀확인

--- src/test/java/calculator/StringAddCalculator.java
+++ src/test/java/calculator/StringAddCalculator.java
@@ -2,17 +2,28 @@ package calculator;
 
 public class StringAddCalculator {
 
-
     String text;
+    private static String division = ",:";
+
+
 
     static int splitAndSum(String text) {
         if (isEmpty(text))
             return 0;
+        CustomDivisionCheck(text);
+
         return 0;
     }
 
-
-    static boolean isEmpty(String text) {
+    private static boolean isEmpty(String text) {
         return text == null || text.isEmpty();
+    }
+
+    private static void CustomDivisionCheck(String text) {
+        if (text.contains("//") && text.contains("\n")) {
+            int start = text.indexOf("//");
+            int end = text.indexOf("\n");
+            division = text.substring(start + 1, end);
+        }
     }
 }
