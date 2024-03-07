09c50af91a950aa21ad9d2d9bb41dda15523a069
동원 <ai06021@gmail.com>
2024-03-07 18:41:37 +0900

    fix : premier delete

--- src/test/java/calculator/StringAddCalculator.java
+++ src/test/java/calculator/StringAddCalculator.java
@@ -7,9 +7,6 @@ public class StringAddCalculator {
         if (isEmpty(text))
             return 0;
         text = CustomDivisionCheck(text);
-
-        // System.out.println(text);
-        // 테스트를 통해서는 결과를 틀렸다는것과 에러메시지만 보여서 그런지 자꾸 System.out.을 통해서 값을 찍어보는데 괜찮을까요?
         return StringDivision(text);
     }
 
@@ -46,16 +43,4 @@ public class StringAddCalculator {
         if (num < 0)
             throw new RuntimeException("음수입니다");
     }
-
-    /* 의문사항 메서드 안에서 다른 메서드를 호출하는것이 좋은가?
-
-    ex) 문자열 -> 문자열 배열로 만든후 정수형 배열로 만든후 합을 구하는 작업을
-    splitAndSum 메인 메서드안에서 차례대로 하나씩 하는것이 좋은지
-
-    문자열을 정수형 배열로 변환후 합까지 구하는 메서드를(중간 과정의 구하는 메서드?)
-    ex) StringChangeIntAndToTalSum -> 이런 느낌의 메서드 안에서 과정을 전부 처리후 결과반환
-
-    -> 너무 [메서드 안의 메서드] -> [메서드 안의 메서드] 와 같이 타고가지는 않는지 걱정이 됨
-
-    */
 }
