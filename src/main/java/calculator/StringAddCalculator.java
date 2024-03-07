3a7fb89c6a46da4901f3605afdcd929dfd759c5a
westkitekim <westkite418@gmail.com>
2024-03-07 16:10:04 +0900

    feat: 커스텀/디폴트 구분자로 덧셈기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import com.sun.javafx.image.IntPixelGetter;
+
+import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     private static final String DEFAULT_SEPARATOR = "[,|:]";
+    private static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
+    private static final int DELIMITER_INDEX = 1;
+    private static final int TOKEN_INDEX = 2;
 
     public static int splitAndSum(String text) {
         /*
@@ -13,15 +22,32 @@ public class StringAddCalculator {
         // 1) 검증
         // 1-1) 빈 문자열 또는 null 값을 입력할 경우 0을 반환
         // 1-2) 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
-        int valRslt;
-        valRslt = validate(text);
-        if (-1 != valRslt) {
-            return valRslt;
+        int validateRsltFlag;
+        int totalSum = 0;
+
+        validateRsltFlag = validate(text);
+        if (validateRsltFlag >= 0) {
+            return validateRsltFlag;
         }
 
         validateNegativeNumberAndThrow(text);
+        
+        String[] numbers = splitBySeparator(text);
+        for (String num : numbers) {
+            totalSum += Integer.parseInt(num);
+        }
 
-        return 0;
+        return totalSum;
+    }
+
+    private static String[] splitBySeparator(String text) {
+        Matcher customMatcher = Pattern.compile(CUSTOM_SEPARATOR).matcher(text);
+        if (customMatcher.find()) {
+            return customMatcher.group(TOKEN_INDEX)
+                    .split(customMatcher.group(DELIMITER_INDEX));
+        }
+
+        return text.split(DEFAULT_SEPARATOR);
     }
 
     private static void validateNegativeNumberAndThrow(String text) {
@@ -31,11 +57,11 @@ public class StringAddCalculator {
     }
 
     private static int validate(String text) {
-        int validateRsltFlag = -1;
         if (isNullOrBlank(text)) return 0;
+        validateNegativeNumberAndThrow(text);
         if (isSingleNumeric(text)) return Integer.parseInt(text);
 
-        return validateRsltFlag;
+        return -1;
     }
 
     private static boolean isSingleNumeric(String text) {
