33447ede2fd52e59b97322bc41889e7f6e84d082
김지훈 <jihoon@wemakeprice.com>
2024-03-07 18:00:49 +0900

    feat: contains와 containsExactly 차이 학습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -15,6 +16,27 @@ public class StringTest {
 
 		assertThat(result).contains(expect);
 		assertThat(result).containsExactly(expect);
+	}
+
+	@Test
+	@DisplayName("AssertThat.contains는 순서를 체크하지 않는다.")
+	void test_contains_success() {
+		String aspect = "1,2";
+
+		String[] result = aspect.split(",");
+		String[] expect = {"2", "1"};
+
+		assertThat(result).contains(expect); // fail
+	}
+	@Test
+	@DisplayName("AssertThat.containsExactly 순서까지 체크한다.")
+	void test_containsExactly_fail() {
+		String aspect = "1,2";
+
+		String[] result = aspect.split(",");
+		String[] expect = {"2", "1"};
+
+		assertThat(result).containsExactly(expect); // fail
 	}
 
 }
