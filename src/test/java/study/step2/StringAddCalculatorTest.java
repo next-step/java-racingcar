f1d68b720a0e6fef8d8ab39502cf900d48394dcf
JJewon <eldertree777@gmail.com>
2024-03-07 19:33:50 +0900

    [feed] @DisplayName 통해 테스트 설명 추가

--- src/test/java/study/step2/StringAddCalculatorTest.java
+++ src/test/java/study/step2/StringAddCalculatorTest.java
@@ -1,6 +1,7 @@
 package study.step2;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step2.StringAddCalculator;
 
@@ -9,6 +10,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
     @Test
+    @DisplayName("null 또는 빈문자 입력 테스트")
     public void splitAndSum_null_또는_빈문자() {
         int result = StringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
@@ -18,30 +20,35 @@ public class StringAddCalculatorTest {
     }
 
     @Test
+    @DisplayName("숫자 하나 입력 테스트")
     public void splitAndSum_숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
+    @DisplayName("쉼표구분자 입력 테스트")
     public void splitAndSum_쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("쉼표 또는 콜론 구분자 입력 테스트")
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("커스텀 구분자 입력 테스트")
     public void splitAndSum_custom_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("음수나 문자 입력시 예외 발생 테스트")
     public void splitAndSum_negative() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
