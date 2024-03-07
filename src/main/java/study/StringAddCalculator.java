f4a518bf6385b11fdfda482b3c6a1bece62cc5e8
Danden1 <yongha.kwon12@gmail.com>
2024-03-07 20:22:46 +0900

    feat: 문자열 덧셈 계산기 구현

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,50 @@
+package study;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final Pattern splitRegex = Pattern.compile("//(.)\\n(.*)");
+    public static int splitAndSum(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        Matcher matcher = splitRegex.matcher(input);
+        if (matcher.find()) {
+            return sumUsingCustomDelimiter(input, matcher);
+        }
+
+        String[] numbers = input.split(",|:");
+
+        return sum(numbers);
+    }
+
+
+    private static int sumUsingCustomDelimiter(String input, Matcher matcher) {
+        String customDelimiter = matcher.group(1);
+        String[] numbers = matcher.group(2).split(customDelimiter);
+
+        return sum(numbers);
+    }
+
+    private static int sum(String[] numbers) {
+        int total = 0;
+
+        for (String number : numbers) {
+            int parseNumber = Integer.parseInt(number);
+
+            if (parseNumber < 0) {
+                throw new RuntimeException("음수가 입력되었습니다.");
+            }
+
+            total += parseNumber;
+        }
+
+        return total;
+    }
+
+
+
+}
