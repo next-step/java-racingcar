e24e149416999e787c7af22c1610b347dd2adf75
김지훈 <jihoon@wemakeprice.com>
2024-03-07 18:20:42 +0900

    chore: Set.size() 기능 테스트

--- src/test/java/study/collections/SetTest.java
+++ src/test/java/study/collections/SetTest.java
@@ -1,9 +1,12 @@
 package study.collections;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SetTest {
@@ -17,6 +20,16 @@ public class SetTest {
 		numbers.add(1);
 		numbers.add(2);
 		numbers.add(3);
+	}
+
+
+	@Test
+	@DisplayName("size 메서드는 컬랙션의 길이를 반환한다.")
+	void test_size() {
+		int expect = 3;
+		int result = numbers.size();
+
+		assertThat(result).isEqualTo(expect);
 	}
 
 }
