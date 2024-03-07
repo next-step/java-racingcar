7ea3ce446b77ba56ab6435c79457cad4110b6949
이동옥 <moveok@idong-og-ui-MacBookAir.local>
2024-03-07 09:17:04 +0900

    define: 문자열덧셈계산기 기능 정리

---
+++ src/test/java/study/StringCalculatorTest.java
@@ -0,0 +1,18 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class StringCalculatorTest {
+
+
+    @ParameterizedTest
+    @DisplayName("")
+    @ValueSource(strings = {""})
+    void test(String input) {
+
+    }
+
+
+}
