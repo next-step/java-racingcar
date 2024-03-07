8500783f77c5c846f63fd1830e52c3af01354b86
naayoungkwon <kny8092@gmail.com>
2024-03-08 01:06:12 +0900

    feat: UI class 작성
    - InputView, ResultView 클래스로 UI 로직 분리

---
+++ src/main/java/racingCar/InputView.java
@@ -0,0 +1,15 @@
+package racingCar;
+
+import java.util.Scanner;
+
+public class InputView {
+
+  Scanner scanner;
+
+  public InputView(){
+    this.scanner = new Scanner(System.in);
+  }
+  public int inputInteger(){
+    return scanner.nextInt();
+  }
+}
---
+++ src/main/java/racingCar/ResultView.java
@@ -0,0 +1,9 @@
+package racingCar;
+
+public class ResultView {
+
+  public void print(String result){
+    System.out.println(result);
+  }
+
+}
