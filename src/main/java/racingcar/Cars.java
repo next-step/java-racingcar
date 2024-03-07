9766ff787c8298d14fefa9186761219670c9e793
deepredk <deepred.kim@gmail.com>
2024-03-07 15:47:38 +0900

    refactor: Cars에서 불필요하게 driveStrategy를 필드로 가지지 않게 함

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -6,11 +6,9 @@ import java.util.stream.Collectors;
 
 public class Cars {
 
-    private final DrivingStrategy drivingStrategy;
     private final List<Car> cars = new ArrayList<>();
 
     public Cars(DrivingStrategy drivingStrategy, int amount) {
-        this.drivingStrategy = drivingStrategy;
         for (int i = 0; i < amount; i++) {
             Car car = new Car(drivingStrategy);
             cars.add(car);
