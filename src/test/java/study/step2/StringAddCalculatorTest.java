529e6e35bfa27657553b1aa180b708bcc640ed50
JJewon <eldertree777@gmail.com>
2024-03-07 20:17:28 +0900

    [feed] @ParameterizedTest 적용 하여 null or 빈문자 테스트

--- src/test/java/study/step2/StringAddCalculatorTest.java
+++ src/test/java/study/step2/StringAddCalculatorTest.java
@@ -3,19 +3,21 @@ package study.step2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import step2.StringAddCalculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
-    @Test
+    @ParameterizedTest
     @DisplayName("null 또는 빈문자 입력 테스트")
-    public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
+    @ValueSource(strings = {""})
+    @NullSource
+    public void splitAndSum_null_또는_빈문자(String input) {
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(0);
     }
 
