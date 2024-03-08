dd36a44dcbfce2485c7c8cd73bac4a72fb3dd9a3
hellonayeon <hellonykwon@gmail.com>
2024-03-08 18:30:30 +0900

    Test: 숫자 하나를 문자열로 입력하는 경우의 테스트케이스 작성 (요구사항2)

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("문자열 덧셈 계산기 테스트")
@@ -21,4 +22,25 @@ public class StringAddCalculatorTest {
         int actual = StringAddCalculator.sum(null);
         assertThat(actual).isZero();
     }
+
+    @Nested
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환")
+    class SingleNumberStringCase {
+
+        @Test
+        @DisplayName("문자열 '1'를 입력하면 숫자 1를 반환")
+        void returnInt1WhenInputStr1() {
+            int actual = StringAddCalculator.sum("1");
+            assertThat(actual).isEqualTo(1);
+        }
+
+        @Test
+        @DisplayName("문자열 '9'를 입력하면 숫자 9를 반환")
+        void returnInt9WhenInputStr9() {
+            int actual = StringAddCalculator.sum("9");
+            assertThat(actual).isEqualTo(9);
+        }
+
+    }
+
 }
