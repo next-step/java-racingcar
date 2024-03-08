44be83d5a21dfbfa7112cc97012f8e5731a4146b
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-09 02:08:47 +0900

    refactor: 테스트 코드 패키지 변경

--- src/test/java/racinggame/domain/AlwaysMoveCondition.java
+++ src/test/java/racinggame/domain/AlwaysMoveCondition.java
@@ -1,4 +1,4 @@
-package racinggame;
+package racinggame.domain;
 
 import racinggame.domain.MoveCondition;
 
--- src/test/java/racinggame/domain/CarTest.java
+++ src/test/java/racinggame/domain/CarTest.java
@@ -1,4 +1,4 @@
-package racinggame;
+package racinggame.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
