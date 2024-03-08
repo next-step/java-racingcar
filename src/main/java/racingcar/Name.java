8c9ca50aba4d4b7bb236af522b3e2b0029c8b3ef
deepredk <deepred.kim@gmail.com>
2024-03-08 22:08:39 +0900

    refactor: Name 이름 길이 검증 시 trim 한번만 하도록 변경
    - null 체크를 먼저하여 처음부터 trim()을 한 뒤 trim된 값을 assert 메서드에 전달함

--- src/main/java/racingcar/Name.java
+++ src/main/java/racingcar/Name.java
@@ -1,19 +1,22 @@
 package racingcar;
 
+import java.util.Objects;
+
 public class Name {
 
     private final String name;
 
     public Name(String name) {
-        assertValidName(name);
+        Objects.requireNonNull(name);
         this.name = name.trim();
+
+        assertValidName(this.name);
     }
 
     private void assertValidName(String name) {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("자동차 이름은 1자 이상이어야 합니다");
         }
-        name = name.trim();
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다");
         }
