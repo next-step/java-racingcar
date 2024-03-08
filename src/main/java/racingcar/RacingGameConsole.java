f750e2ae4990ca6f97fb61783691a0ffdfb49e1d
deepredk <deepred.kim@gmail.com>
2024-03-08 13:13:38 +0900

    refactor: RacingGameConsole 메서드 분리

--- src/main/java/racingcar/RacingGameConsole.java
+++ src/main/java/racingcar/RacingGameConsole.java
@@ -4,22 +4,37 @@ import java.util.Scanner;
 
 public class RacingGameConsole {
 
-    private final Scanner scanner = new Scanner(System.in);
+    private static final String WINNER_DELIMITER = ", ";
+
+    private static final Scanner scanner = new Scanner(System.in);
 
     public void start() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String[] carNames = scanner.nextLine().split(",");
+        String[] carNames = inputCarNames();
+        int round = inputRound();
 
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int round = scanner.nextInt();
-
-        DrivingStrategyImpl drivingStrategy = new DrivingStrategyImpl();
-        RacingGame racingGame = new RacingGame(drivingStrategy, carNames);
+        RacingGame racingGame = new RacingGame(new DrivingStrategyImpl(), carNames);
         racingGame.drive(round);
 
+        printResult(racingGame);
+        printWinners(racingGame);
+    }
+
+    private String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine().split(",");
+    }
+
+    private int inputRound() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
+    }
+
+    private static void printResult(RacingGame racingGame) {
         System.out.println("\n실행 결과");
         System.out.println(racingGame.getResult());
+    }
 
-        System.out.printf("\n%s가 최종 우승했습니다.", String.join(", ", racingGame.getWinnerNames()));
+    private static void printWinners(RacingGame racingGame) {
+        System.out.printf("\n%s가 최종 우승했습니다.", String.join(WINNER_DELIMITER, racingGame.getWinnerNames()));
     }
 }
