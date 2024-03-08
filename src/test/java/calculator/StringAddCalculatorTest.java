2f8083cea53aebf97c31a5f949dcc02d00c98050
hellonayeon <hellonykwon@gmail.com>
2024-03-08 18:17:02 +0900

    Test: 빈 문자열을 입력할 경우 0 반환 검증 (요구사항 1-1)

---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("문자열 덧셈 계산기 테스트")
+public class StringAddCalculatorTest {
+
+    @Test
+    @DisplayName("빈 문자열을 입력할 경우 0을 반환")
+    void return0WhenEmptyString() {
+        int result = StringAddCalculator.sum("");
+        assertThat(result).isZero();
+    }
+}
