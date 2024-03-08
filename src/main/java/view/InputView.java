6122ab4ad3977e2693ef0743c02a8fd1f75af3a6
leeedohyun <ldh19738@gmail.com>
2024-03-08 13:20:25 +0900

    feat: 입력받을 때 메시지 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class InputView {
 
     public int inputRacingCarNumber() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
         final Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 
     public int inputTryNumber() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
         final Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
