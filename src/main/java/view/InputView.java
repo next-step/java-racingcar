c4b70970593c1e4c911f891ca31745550b5ce20c
leeedohyun <ldh19738@gmail.com>
2024-03-08 13:11:55 +0900

    feat: 게임에 이용할 자동차 수 입력 기능 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    public int inputRacingCarNumber() {
+        final Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+}
