c947052d00ce845f1003de7411bece4d801ccfdf
leejinwoo <leejinwoo1126@gmail.com>
2024-03-08 13:22:46 +0900

    import optimize

--- src/test/java/util/RandomUtilTest.java
+++ src/test/java/util/RandomUtilTest.java
@@ -3,10 +3,9 @@ package util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomUtilTest {
-
 
     @Test
     @DisplayName("0 ~ 9 사이의 정수 값을 반환한다")
