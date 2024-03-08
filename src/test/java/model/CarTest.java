193f350d591d7fa79b44fc271a0b9d0b3780374b
leejinwoo <leejinwoo1126@gmail.com>
2024-03-08 11:06:11 +0900

    test: Car 클래스 move() 경계값 테스트

---
+++ src/test/java/model/CarTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @Test
+    void move_랜덤값4이상인경우_자동차는_전진한다() {
+        Car car = new Car();
+        car.move(4);
+        assertThat(car.isSamePosition(2)).isTrue();
+    }
+
+    @Test
+    void move_랜덤값4미만인경우_자동차는_현위치를유지한다() {
+        Car car = new Car();
+        car.move(3);
+        assertThat(car.isSamePosition(1)).isTrue();
+    }
+}
