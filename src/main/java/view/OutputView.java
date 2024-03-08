e84b71dc11ec5dd0ec18c346b8e23a965c98e036
leeedohyun <ldh19738@gmail.com>
2024-03-08 14:15:40 +0900

    refactor: for문 없애고 String repeat 적용

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,17 +5,16 @@ import java.util.List;
 import model.RacingCar;
 
 public class OutputView {
-    
+
+    private static final String MOVEMENT_INDICATOR = "-";
+
     public void printResultMessage() {
         System.out.println("\n실행 결과");
     }
 
     public void printResult(final List<RacingCar> racingCars) {
         for (final RacingCar racingCar : racingCars) {
-            for (int i = 0; i < racingCar.getLocation(); i++) {
-                System.out.print("-");
-            }
-            System.out.println();
+            System.out.println(MOVEMENT_INDICATOR.repeat(racingCar.getLocation()));
         }
         System.out.println();
     }
