7b2561f41e3a407baeafcc1c5c68f9f8c31b0460
leejinwoo <leejinwoo1126@gmail.com>
2024-03-08 11:15:05 +0900

    refactor: Position 객체로 상태값 관리하도록 Car 변경

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,23 +1,34 @@
 package model;
 
+import java.util.Objects;
+
 public class Car {
-    private int position;
+    private final Position position;
 
     public Car() {
         this(1);
     }
 
     public Car(int position) {
-        this.position = position;
+        this.position = new Position(position);
     }
 
     public void move(int random) {
         if (random >= 4) {
-            this.position += 1;
+            this.position.move();
         }
     }
 
-    public boolean isSamePosition(int other) {
-        return this.position == other;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(position, car.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
