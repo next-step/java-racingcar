7cbb5e380e629bee5f9bea401aef486e5dc9b00c
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-09 02:01:38 +0900

    feat: position getter 추가

--- src/main/java/racinggame/domain/Position.java
+++ src/main/java/racinggame/domain/Position.java
@@ -6,12 +6,12 @@ public class Position {
 
     private final int position;
 
-    public Position() {
-        this(0);
-    }
-
     public Position(int position) {
         this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
     }
 
     @Override
