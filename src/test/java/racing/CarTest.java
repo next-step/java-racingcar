778ac6d1744f2e70db0f24f724797158b43b9b37
nimoh <spakers38@gmail.com>
2024-03-08 15:27:43 +0900

    refactor: [Step 3] 피드백 반영 - 테스트 하나에 하나의 단위테스트

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -3,6 +3,8 @@ package racing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,10 +18,20 @@ class CarTest {
     }
 
     @Test
-    @DisplayName("Car 객체 생성 시 position은 0 && 파라미터와 Car의 위치가 일치하면 true")
+    @DisplayName("Car 객체 생성 시 position은 0")
     void Car_constructor() {
         assertThat(car.hasPosition(0)).isTrue();
-        assertThat(car.hasPosition(1)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    @DisplayName("파라미터와 position 동치 비교")
+    void hasPosition(int input) {
+        for (int i = 0; i < input; i++) {
+            car.forward();
+        }
+        assertThat(car.hasPosition(input)).isTrue();
+        assertThat(car.hasPosition(input+1)).isFalse();
     }
 
     @Test
