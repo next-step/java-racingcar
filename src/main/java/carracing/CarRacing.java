e6d597b72a3657ab0f24ed658eb6953465ab0e56
hong yuseok <jericoh@naver.com>
2024-03-09 05:28:22 +0900

    feat: 예외 처리 추가

--- src/main/java/carracing/CarRacing.java
+++ src/main/java/carracing/CarRacing.java
@@ -4,9 +4,12 @@ import static carracing.InputView.*;
 
 public class CarRacing {
     public static void main(String[] args) {
-        int enteredNumberOfCars = enteredNumber(NUMBER_OF_CARS_INPUT_GUIDE_MESSAGE);
-        int enteredNumberOfAttempts = enteredNumber(NUMBER_OF_ATTEMPTS_INPUT_GUIDE_MESSAGE);
+        try {
+            int enteredNumberOfCars = enteredNumber(NUMBER_OF_CARS_INPUT_GUIDE_MESSAGE);
+            int enteredNumberOfAttempts = enteredNumber(NUMBER_OF_ATTEMPTS_INPUT_GUIDE_MESSAGE);
 
-
+        }catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
