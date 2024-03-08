89f703222fd633c89b929eefc68344a453bb694d
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-09 00:21:45 +0900

    test: car 주행, 정지 테스트 수정

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -1,25 +1,34 @@
 package racinggame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("도메인 객체 Car 테스트")
 public class CarTest {
 
-    @Test
-    public void 주행() {
-        Car car = new Car();
-        boolean result = car.run(new AlwaysMoveCondition());
+    private Car car;
+
+    @BeforeEach
+    void initCar() {
+        car = new Car();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 9})
+    public void 주행(int condition) {
+        boolean result = car.run(new MoveCondition(condition));
 
         assertThat(result).isTrue();
     }
 
-    @Test
-    public void 정지() {
-        Car car = new Car();
-        boolean result = car.run(new NeverMoveCondition());
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3})
+    public void 정지(int condition) {
+        boolean result = car.run(new MoveCondition(condition));
 
         assertThat(result).isFalse();
     }
