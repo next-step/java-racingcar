ae3c660d3646d899ec7282a51a97006e5f7cad20
leejinwoo <leejinwoo1126@gmail.com>
2024-03-08 11:13:07 +0900

    feat: Position 클래스 생성(Wrapper class)

---
+++ src/main/java/model/Position.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.Objects;
+
+public class Position {
+    private int position;
+
+    public Position(int position) {
+        this.position = position;
+    }
+
+    public void move() {
+        this.position += 1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
