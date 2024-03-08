b36efc42b2a5dac339f71c45475a56f4a3686dda
Dongock <58582148+Dongock@users.noreply.github.com>
2024-03-08 21:40:55 +0900

    문자열 덧셈 계산기 (#5294)
    * define: 문자열덧셈계산기 기능 정리
    
    * feat:문자열덧셈계산기(2-1) : 비어있는 input check test
    
    * define: null과 blank 구분 정의
    
    * feat:문자열덧셈계산기(2-1) : input null, blank check test구분
    
    * feat:문자열덧셈계산기(2-1) : input null, blank process test
    
    * define: 구분자가 연속으로 올 시 처리 정의
    
    * define: blank와 문자 사이 blank 처리 방법 변경
    
    * feat:문자열덧셈계산기(2-1) : 실제 구현 및 구현체 test 적용
    
    * refactor:[2-1] nullCheck 변경
    
    * feat:[2] 한자리 숫자가 들어오면 return
    
    * feat: 기능 구현 완료(1차)
    
    ---------
    
    Co-authored-by: 이동옥 <moveok@idong-og-ui-MacBookAir.local>

---
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -0,0 +1,78 @@
+package StringCalculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+    private static StringCalculator stringCalculator = new StringCalculator();
+    private static final String NUMERIC_REGEXP = "\\d";
+    private static final String DEFAULT_SEPARATOR = ",|:";
+    private static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
+    private static final int PATTERN_GROUP_NUM = 1;
+    private static final int INPUT_GROUP_NUM = 2;
+    private StringCalculator() {}
+
+    public static StringCalculator getInstance() {
+        return stringCalculator;
+    }
+
+    public int calculate(String input) {
+        if (nullCheck(input)) {
+            return 0;
+        }
+
+        input = deleteBlank(input);
+
+        if (isNumeric(input)) {
+            return Integer.parseInt(input);
+        }
+
+        String[] tokens = splitString(input);
+
+        if (!isValidTokens(tokens)) {
+            throw new RuntimeException("숫자를 입력해주세요");
+        }
+
+        return sumTokens(tokens);
+
+    }
+
+    private int sumTokens(String[] tokens) {
+        int result = 0;
+        for (String token : tokens) {
+            result += Integer.parseInt(token);
+        }
+        return result;
+    }
+
+    public boolean isValidTokens(String[] tokens) {
+        long count = Arrays.stream(tokens)
+                .filter(token -> !token.startsWith("-"))
+                .filter(this::isNumeric)
+                .count();
+        return count == tokens.length;
+    }
+
+
+    public boolean isNumeric(String input) {
+        return Pattern.matches(NUMERIC_REGEXP, input);
+    }
+
+    public String deleteBlank(String input) {
+        return input.replace(" ", "");
+    }
+
+    public boolean nullCheck(String input) {
+        return input == null || input.isBlank();
+    }
+
+    public String[] splitString(String input) {
+        Matcher m = Pattern.compile(CUSTOM_SEPARATOR).matcher(input);
+        if (m.find()) {
+            return m.group(INPUT_GROUP_NUM).split(m.group(PATTERN_GROUP_NUM));
+        }
+        return input.split(DEFAULT_SEPARATOR);
+    }
+}
---
+++ src/test/java/study/StringCalculatorTest.java
@@ -0,0 +1,72 @@
+package study;
+
+import StringCalculator.StringCalculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator = StringCalculator.getInstance();
+
+    @ParameterizedTest
+    @DisplayName("들어온 문자열이 null일 때 Valid Test")
+    @NullAndEmptySource
+    void inputNullTest(String input) {
+        assertThat(input == null || input.isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("들어온 문자열 내부에 blank가 존재할 때 Valid Test")
+    @ValueSource(strings = {"", " ", "   "})
+    void inputBlankTest(String input) {
+        String trimInput = input.trim();
+        assertThat(trimInput.isBlank()).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("들어온 문자열이 null일 때 변환 Test")
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   ", "        "})
+    void inputNullProcessTest(String input) {
+        String checkInput = stringCalculator.nullCheck(input) ? "0" : input ;
+        assertThat(checkInput).isEqualTo("0");
+    }
+
+    @ParameterizedTest
+    @DisplayName("들어온 문자열 내부에 blank가 존재할 때 변환 Test")
+    @ValueSource(strings = {"a    a", "b c", " a b "})
+    void inputBlankProcessTest(String input) {
+        String trimInput = stringCalculator.deleteBlank(input);
+        assertThat(trimInput.contains(" ")).isFalse();
+    }
+
+    @ParameterizedTest
+    @DisplayName("들어온 문자열이 한자리 숫자일 때 확인 Test")
+    @ValueSource(strings = {"1", "2"})
+    void inputNumericProcessTest(String input) {
+        boolean isNumeric = stringCalculator.isNumeric(input);
+        assertThat(isNumeric).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("들어온 문자열이 한자리 숫자가 아닐 때 확인 Test")
+    @ValueSource(strings = {"22", "a", "a2"})
+    void inputNotNumericProcessTest(String input) {
+        boolean isNumeric = stringCalculator.isNumeric(input);
+        assertThat(isNumeric).isFalse();
+    }
+
+    @ParameterizedTest
+    @DisplayName("음수 split 확인 Test")
+    @ValueSource(strings = {"-1:1:1", "-1,2", "1:-1", "2:1:-1"})
+    void minusNumberSplitTest(String input) {
+        String[] arrays = input.split(",|:");
+        assertThat(arrays).contains("-1");
+    }
+
+}
