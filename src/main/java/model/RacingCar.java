4378bcf714a7a34c3c3d02f5a2125231e56487cb
leeedohyun <ldh19738@gmail.com>
2024-03-08 12:54:44 +0900

    refactor: 매직 넘버 상수로 리팩토링

--- src/main/java/model/RacingCar.java
+++ src/main/java/model/RacingCar.java
@@ -2,10 +2,12 @@ package model;
 
 public class RacingCar {
 
+    private static final int MOVE_THRESHOLD = 4;
+
     private int location;
 
     public void move(final int randomNumber) {
-        if (randomNumber >= 4) {
+        if (randomNumber >= MOVE_THRESHOLD) {
             location++;
         }
     }
