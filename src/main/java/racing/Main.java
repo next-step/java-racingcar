48ee6b47248450f5b5692b98cfd10ea013246601
nimoh <spakers38@gmail.com>
2024-03-08 14:01:02 +0900

    refactor: [Step 3] 피드백 반영 - Scanner 객체를 static으로 수정

--- src/main/java/racing/Main.java
+++ src/main/java/racing/Main.java
@@ -3,8 +3,11 @@ package racing;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Race race = new Race(new InputView(new Scanner(System.in)));
+        Race race = new Race(new InputView(scanner));
         race.start();
     }
 }
