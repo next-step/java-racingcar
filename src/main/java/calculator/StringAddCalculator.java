4ad84b3bea74ac1fed6ff18f74f809d46dfe411b
hellonayeon <hellonykwon@gmail.com>
2024-03-08 18:33:12 +0900

    Feature: sum 메서드 수정 - 입력된 문자열에 대해 정수형으로 파싱한 결과를 리턴

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -6,7 +6,7 @@ public class StringAddCalculator {
         if (str == null || str.isEmpty()) {
             return 0;
         }
-        return -1;
+        return Integer.parseInt(str);
     }
 
 }
