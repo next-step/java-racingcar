e7f68d37bfa66c5981d35242147a8e8f0d58b210
hellonayeon <hellonykwon@gmail.com>
2024-03-08 18:26:00 +0900

    Feature: 문자열 덧셈 계산기 클래스 생성, 덧셈 메서드(sum) 추가

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class StringAddCalculator {
+
+    public static int sum(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+        return -1;
+    }
+
+}
