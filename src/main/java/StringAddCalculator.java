8115cf3a29946ef69b05bd36bc18fdd1913dce80
박소민 <qkrthals0524@naver.com>
2024-03-08 22:33:17 +0900

    split 함수 생성 및 general line break 속성 설정

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -2,42 +2,37 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+
     private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(?<key>;)\n.*");
     private static final String COMMON_SEPARATOR = ",|:";
+
     public static int splitAndSum(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        if (input.contains("//") && input.contains("\n")) {
-            String separator = customSeparator(input);
-            String custom_input = input.substring(input.indexOf("\n")+1);
-            String[] inputs = custom_input.split(separator);
-            return sum(inputs);
-        }
-
-        String[] inputs = input.split(COMMON_SEPARATOR);
+        String[] inputs = split(input);
         return sum(inputs);
     }
 
-    private static String customSeparator(String input) {
+    private static String[] split(String input) {
         Matcher matcher = CUSTOM_PATTERN.matcher(input);
-        if (!matcher.find()) {
-            throw new RuntimeException("정규식 패턴에 일치하는 구분자가 없습니다.");
+        if (matcher.find()) {
+            return input.substring(input.indexOf("\n") + 1).split(matcher.group("key"));
         }
-        return matcher.group("key");
+        return input.split(COMMON_SEPARATOR);
     }
 
     private static int sum(String[] inputs) {
         int result = 0;
-        for (String input:inputs) {
+        for (String input : inputs) {
             int item = Integer.parseInt(input);
             if (item < 0) {
                 throw new RuntimeException();
             }
-            result+=item;
+            result += item;
         }
         return result;
     }
-
 }
+
