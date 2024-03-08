a74bdb33bf40a9e0eb0695c391cc23fbf87f66f1
Jihoon Kim <78334008+DevRunner21@users.noreply.github.com>
2024-03-08 16:31:53 +0900

    [Step1] 학습 테스트 (#5287)
    * chore: upgrade java8 -> java11, gradle, junit version
    
    * project init
    
    * feat: split 테스트
    
    * feat: contains와 containsExactly 차이 학습
    
    * feat: String.subString 테스트
    
    * feat: String.chatAt 성공 테스트
    
    * feat: String.chatAt IndexOutOfBoundsException 발생 테스트
    
    * chore: Collection.Set 테스트 환경 세팅
    
    * chore: Set.size() 기능 테스트
    
    * feat: Set.contains() 기능 테스트
    
    * feat: Set.contains() 성공,실패 케이스 한번에 테스트
    
    ---------
    
    Co-authored-by: javajigi <javajigi@gmail.com>
    Co-authored-by: 김지훈 <jihoon@wemakeprice.com>

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,79 @@
+package study;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+	@Test
+	void test_split() {
+		String input = "1,2";
+
+		String[] result = input.split(",");
+		String[] expect = {"1", "2"};
+
+		assertThat(result).contains(expect);
+		assertThat(result).containsExactly(expect);
+	}
+
+	@Test
+	@DisplayName("AssertThat.contains는 순서를 체크하지 않는다.")
+	void test_contains_success() {
+		String aspect = "1,2";
+
+		String[] result = aspect.split(",");
+		String[] expect = {"2", "1"};
+
+		assertThat(result).contains(expect); // fail
+	}
+	@Test
+	@DisplayName("AssertThat.containsExactly 순서까지 체크한다.")
+	void test_containsExactly_fail() {
+		String aspect = "1,2";
+
+		String[] result = aspect.split(",");
+		String[] expect = {"2", "1"};
+
+		assertThat(result).containsExactly(expect); // fail
+	}
+
+	@Test
+	void test_subString() {
+		String aspect = "(1,2)";
+		String expect = "1,2";
+
+		String result = aspect.substring(1, 4);
+
+		assertThat(result).isEqualTo(expect);
+	}
+
+	@Test
+	void test_chatAt() {
+		String aspect = "abc";
+		char expect = 'b';
+
+		char result = aspect.charAt(1);
+
+		assertThat(result).isEqualTo(expect);
+	}
+
+	@Test
+	@DisplayName("chatAt은 인자로 대상 문자열 보다 큰 인덱스를 넘기면, IndexOutOfBoundsException을 던진다.")
+	void test_chatAt_IndexOutOfBoundsException() {
+		String aspect = "abc";
+		int overSizeIndex = aspect.length() + 100;
+
+		assertThatThrownBy(() -> {
+			aspect.charAt(overSizeIndex);
+		}).isInstanceOf(IndexOutOfBoundsException.class)
+			.hasMessageContaining("String index out of range: 103");
+
+		assertThatExceptionOfType(IndexOutOfBoundsException.class)
+			.isThrownBy(() -> {
+				aspect.charAt(overSizeIndex);
+			});
+	}
+
+}
---
+++ src/test/java/study/collections/SetTest.java
@@ -0,0 +1,61 @@
+package study.collections;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+
+	@Test
+	@DisplayName("size 메서드는 컬랙션의 길이를 반환한다.")
+	void test_size() {
+		int expect = 3;
+		int result = numbers.size();
+
+		assertThat(result).isEqualTo(expect);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1,2,3})
+	@DisplayName("contains 메서드는 컬랙션 안에 인자의 포함여부를 반환한다.")
+	void test_contains(int input) {
+		assertThat(numbers.contains(input)).isTrue();
+	}
+
+	@Test
+	@DisplayName("contains 메서드는 인자로 컬랙션 안에 없는 값을 넘길 경우, 실패한다.)")
+	void test_contains_fail() {
+		int noContainedValue = 1000;
+
+		assertThat(numbers.contains(noContainedValue)).isFalse();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true"}, delimiter = ':')
+	@DisplayName("contains 메서드는 컬랙션 안에 인자의 포함여부를 반환한다.")
+	void test_contains_all_case(int input, boolean expected) {
+
+		assertThat(numbers.contains(input)).isEqualTo(expected);
+	}
+
+}
