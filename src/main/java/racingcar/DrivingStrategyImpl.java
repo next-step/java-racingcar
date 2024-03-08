fd61ba6ba256e57b7f0a67741d0863c53069bffb
deepredk <deepred.kim@gmail.com>
2024-03-08 22:05:46 +0900

    refactor: random 멤버변수 상수 컨벤션에 맞춰 RANDOM으로 이름 변경

--- src/main/java/racingcar/DrivingStrategyImpl.java
+++ src/main/java/racingcar/DrivingStrategyImpl.java
@@ -7,10 +7,10 @@ public class DrivingStrategyImpl implements DrivingStrategy {
     private static final int RANGE_UPPER_BOUND = 10;
     private static final int THRESHOLD = 4;
 
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
 
     @Override
     public boolean canDrive() {
-        return random.nextInt(RANGE_UPPER_BOUND) >= THRESHOLD;
+        return RANDOM.nextInt(RANGE_UPPER_BOUND) >= THRESHOLD;
     }
 }
