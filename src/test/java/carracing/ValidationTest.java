a290995f090eee855198bbe31fce7ce32a18804d
hong yuseok <jericoh@naver.com>
2024-03-09 05:00:13 +0900

    test: 입력값 검증에 대한 테스트 코드 수정

--- src/test/java/carracing/ValidationTest.java
+++ src/test/java/carracing/ValidationTest.java
@@ -4,24 +4,14 @@ package carracing;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ValidationTest {
     @ParameterizedTest
-    @NullAndEmptySource
-    @DisplayName("null 또는 빈 문자열을 입력하는 경우 false를 반환한다.")
-    void testBlankValidation(String input) {
-        assertThat(Validation.isNonBlank(input)).isFalse();
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"null:false", "abcd:false", "-1:false", "0:false", "1.123:false", "1,a12:false", "1:true", "2:true", "10:true"}, nullValues = {"null"}, delimiter = ':')
+    @CsvSource(value = {"null:false", "'':false", "'   ':false", "abcd:false", "-1:false", "0:false", "1.123:false", "1,a12:false", "1:true", "2:true", "10:true"}, nullValues = {"null"}, delimiter = ':')
     @DisplayName("양의 정수를 입력한 경우 true 그렇지 않은 경우 false를 반환한다.")
-    void testCharValidation(String input, boolean expected) {
+    void testIsPositiveInteger(String input, boolean expected) {
         assertThat(Validation.isPositiveInteger(input)).isEqualTo(expected);
     }
-
-
 }
