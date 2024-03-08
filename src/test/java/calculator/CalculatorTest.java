9e75f92480f006b6a2f7d01309c6a3ef7e02e212
Sangmin <sm9171@nate.com>
2024-03-08 09:37:15 +0900

    test: Create additionCalculateTest failure case

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CalculatorTest {
+
+	@DisplayName("커스텀 문자열이 들어갈 때 덧셈 계산")
+	@Test
+	void additionCalculate() {
+		// given
+		String input = "1,2;3";
+		Calculator calculator = new Calculator();
+
+		// when
+		int result = calculator.additionCalculate(input);
+
+		// then
+		assertThat(result).isEqualTo(6);
+	}
+}
