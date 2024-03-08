ea16cb13cac786e0503c0c74a50711d3324409f9
hong yuseok <jericoh@naver.com>
2024-03-09 05:07:44 +0900

    feat: 입력값 검증 기능 구현

---
+++ src/main/java/carracing/Validation.java
@@ -0,0 +1,18 @@
+package carracing;
+
+
+import java.util.regex.Pattern;
+
+import static java.util.Objects.*;
+
+public class Validation {
+    private static final Pattern PATTERN_FOR_POSITIVE_INTEGER = Pattern.compile("^(0*[1-9][0-9]*)$");
+
+    private Validation() {
+
+    }
+
+    public static boolean isPositiveInteger(String input) {
+        return !isNull(input) && !input.isBlank() && PATTERN_FOR_POSITIVE_INTEGER.matcher(input).find();
+    }
+}
