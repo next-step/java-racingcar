ee488e40db6205df3bbe295b8bbfa2c69ce331e7
leejinwoo <leejinwoo1126@gmail.com>
2024-03-09 21:47:26 +0900

    test: RacingRecord 결과 검증하도록 변경, 테스트 분리

--- src/test/java/model/RacingGameTest.java
+++ src/test/java/model/RacingGameTest.java
@@ -1,42 +1,38 @@
 package model;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingGameTest {
 
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("racingGameProvider")
-    void racingGame(String title, List<Car> actualCarList, List<Car> expectedCarList) {
-        RacingGame actual = new RacingGame(actualCarList);
-        actual.run(1);
+    @Test
+    void runByTrueStrategy() {
+        MoveStrategy trueMoveStrategy = () -> true;
+        List<Car> cars = Arrays.asList(new Car(1, trueMoveStrategy), new Car(2, trueMoveStrategy), new Car(3, trueMoveStrategy));
 
-        RacingGame expected = new RacingGame(expectedCarList);
+        int turn = 1;
+        RacingGame racingGame = new RacingGame(cars);
+        RacingRecord result = racingGame.run(turn);
 
-        assertThat(actual).isEqualTo(expected);
+        assertThat(result.racingRecord(turn))
+                .containsExactly(new Record(2), new Record(3), new Record(4));
     }
 
-    private static Stream<Arguments> racingGameProvider() {
-        final MoveStrategy trueMoveStrategy = () -> true;
-        final MoveStrategy falseMoveStrategy = () -> false;
-        return Stream.of(
-                Arguments.arguments(
-                        "이동 전략이 true일때 자동차 위치는 1씩 증가한다",
-                        Arrays.asList(new Car(1, trueMoveStrategy), new Car(2, trueMoveStrategy), new Car(3, trueMoveStrategy)),
-                        Arrays.asList(new Car(2, trueMoveStrategy), new Car(3, trueMoveStrategy), new Car(4, trueMoveStrategy))),
-                Arguments.arguments(
-                        "이동 전략이 false일때 자동차는 현 위치를 유지한다",
-                        Arrays.asList(new Car(1, falseMoveStrategy), new Car(2, falseMoveStrategy), new Car(3, falseMoveStrategy)),
-                        Arrays.asList(new Car(1, falseMoveStrategy), new Car(2, falseMoveStrategy), new Car(3, falseMoveStrategy))
-                )
-        );
+    @Test
+    void runByFalseStrategy() {
+        MoveStrategy falseMoveStrategy = () -> false;
+        List<Car> cars = Arrays.asList(new Car(1, falseMoveStrategy), new Car(2, falseMoveStrategy), new Car(3, falseMoveStrategy));
+
+        int turn = 1;
+        RacingGame racingGame = new RacingGame(cars);
+        RacingRecord result = racingGame.run(turn);
+
+        assertThat(result.racingRecord(turn))
+                .containsExactly(new Record(1), new Record(2), new Record(3));
     }
 
 }
