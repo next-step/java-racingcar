0b815fb8b825b0360e7c2514a2fde19ef162c251
leeedohyun <ldh19738@gmail.com>
2024-03-09 17:51:00 +0900

    refactor: Scanner 상수로 분리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public int inputRacingCarNumber() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        final Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 
     public int inputTryNumber() {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        final Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 }
