a494136d6eb0147bc686035a9001852c82108fb2
hong yuseok <jericoh@naver.com>
2024-03-09 17:02:15 +0900

    test: Car에 대한 테스트 코드 수정 - move()에 대한 테스트 목적이 드러나도록 수정

--- src/test/java/carracing/CarTest.java
+++ src/test/java/carracing/CarTest.java
@@ -2,19 +2,21 @@ package carracing;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
     @ParameterizedTest
-    @ValueSource(ints = {0, 1})
-    @DisplayName("")
-    void testMove(int strategyReturn) {
+    @CsvSource(value = {"1:0:0", "1:1:1", "1:5:5", "2:3:6"}, delimiter = ':')
+    @DisplayName("Car의 postion에 Strategy 반환 값을 더해줍니다.")
+    void testMove(int strategyReturn, int moveCount, int expected) {
         Car car = new Car(() -> strategyReturn);
 
-        car.move();
+        for(int i = 0; i < moveCount; i++) {
+            car.move();
+        }
 
-        assertThat(car.position()).isEqualTo(strategyReturn);
+        assertThat(car.position()).isEqualTo(expected);
     }
 }
