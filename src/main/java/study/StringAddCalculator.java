7ec94cf30e30294a3ed3f2cb0424db20f70d8f0f
Duyan Kim <arbolkim@gmail.com>
2024-03-09 20:07:31 +0900

    [step2] 문자열 덧셈 계산기 (#5292)
    * feat: 문자열 덧셈 계산기
    
    * test: 음수 문자열 테스트 코드 추가
    
    * feat: Pattern 상수화
    
    * feat: 에러메세지 수정
    
    * fix: Pattern 상수 수정

---
+++ src/main/java/study/StringAddCalculator.java
@@ -0,0 +1,53 @@
+package study;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+    private static final Pattern pattern = Pattern.compile(CUSTOM_DELIMITER);
+
+    static int calculate(String text) {
+        if (isEmptyString(text)) {
+            return 0;
+        }
+        return sum(splitByPattern(text));
+    }
+
+    private static boolean isEmptyString(String text) {
+        return text == null || text.isBlank();
+    }
+
+    private static int sum(String[] values) {
+        int result = 0;
+
+        for (String value : values) {
+            result += validateNumber(value);
+        }
+        return result;
+    }
+
+    private static int validateNumber(String value) {
+        int number = Integer.parseInt(value);
+        if (number < 0) {
+            throw new IllegalArgumentException("음수는 입력 가능하지 않습니다");
+        }
+        return number;
+    }
+
+    private static String[] splitByPattern(String text) {
+        Matcher matcher = pattern.matcher(text);
+        return getStrings(text, matcher);
+    }
+
+    private static String[] getStrings(String text, Matcher matcher) {
+        if (matcher.find()) {
+            String delimiter = matcher.group(1);
+            return matcher.group(2).split(delimiter);
+        }
+        return text.split(DEFAULT_DELIMITER);
+    }
+
+}
---
+++ src/test/java/study/StringAddCalculatorTest.java
@@ -0,0 +1,55 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringAddCalculatorTest {
+
+    @Test
+    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환한다")
+    void check_empty_string() {
+        assertThat(StringAddCalculator.calculate(null)).isEqualTo(0);
+        assertThat(StringAddCalculator.calculate("")).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    void convert_string_to_number() {
+        assertThat(StringAddCalculator.calculate("1")).isEqualTo(1);
+        assertThat(StringAddCalculator.calculate("123")).isEqualTo(123);
+        assertThatThrownBy(() -> StringAddCalculator.calculate("-10"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다")
+    void split_and_add_numbers() {
+        String text = "1,2";
+        assertThat(StringAddCalculator.calculate(text)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다")
+    void split_with_colon() {
+        String text = "1,2:3";
+        assertThat(StringAddCalculator.calculate(text)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자를 지정할 수 있다")
+    void custom_delimiter() {
+        String text = "//!\n1!2!3!";
+        assertThat(StringAddCalculator.calculate(text)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생해야 한다")
+    void number_exception() {
+        String text = "-1,2,3";
+        assertThatThrownBy(() -> StringAddCalculator.calculate(text))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
