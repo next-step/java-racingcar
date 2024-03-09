630264880ae4b732612e7f98a5f82aaf541c1a8b
동원 <ai06021@gmail.com>
2024-03-09 14:12:39 +0900

    style : 매개변수 추가

--- src/test/java/racingcar/CarRacing.java
+++ src/test/java/racingcar/CarRacing.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
@@ -8,10 +9,11 @@ public class CarRacing {
     static final int GO_CONDITION = 4;
     static final int GO_CONDITION_START_INDEX = 0;
     static final int GO_CONDITION_END_INDEX = 9;
+    static List[] cars;
 
     static void Racing() {
-        List<Character>[] cars = new List[CarCount()];
-        int tryCount = MoveCount();
+        makeCars(CarCount(count()));
+        int tryCount = MoveCount(count());
         while (tryCount > 0) {
             MoveCar(cars);
             tryCount--;
@@ -19,35 +21,42 @@ public class CarRacing {
         }
     }
 
-    private static int Count() {
+    static int count() {
         Scanner sc = new Scanner(System.in);
         int cnt = sc.nextInt();
         if (cnt <= 0) throw new IllegalArgumentException("잘못된 입력값 입니다");
         return cnt;
     }
 
-    private static int CarCount() {
+    static int CarCount(int input) {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        return Count();
+        return input;
     }
 
-    private static int MoveCount() {
+    static void makeCars(int input) {
+        cars = new List[input];
+        for (int i = 0; i < input; i++) {
+            cars[i] = new LinkedList<>();
+        }
+    }
+
+    static int MoveCount(int input) {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        return Count();
+        return input;
     }
 
-    private static void MoveCar(List<Character>[] cars) {
+    static void MoveCar(List<Character>[] cars) {
         for (List<Character> car : cars) {
-            if (StopOrGo()) car.add('-');
+            if (StopOrGo(GO_CONDITION_START_INDEX, GO_CONDITION_END_INDEX, GO_CONDITION)) car.add('-');
             CarLocation(car);
         }
     }
 
-    private static boolean StopOrGo() {
-        return ThreadLocalRandom.current().nextInt(GO_CONDITION_START_INDEX, GO_CONDITION_END_INDEX) >= GO_CONDITION;
+    static boolean StopOrGo(int start, int end, int condition) {
+        return ThreadLocalRandom.current().nextInt(start, end) >= condition;
     }
 
-    private static void CarLocation(List<Character> Car) {
+    static void CarLocation(List<Character> Car) {
         for (Character c : Car) {
             System.out.print(c);
         }
