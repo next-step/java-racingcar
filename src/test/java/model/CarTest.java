55adae588d761f97825d81225e5d0930ec956a12
leejinwoo <leejinwoo1126@gmail.com>
2024-03-09 12:42:23 +0900

    test: MoveStrategy 사용하도록 변경, 테스트 제거

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,6 +1,5 @@
 package model;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,22 +8,20 @@ class CarTest {
 
     @Test
     void move_랜덤값4이상인경우_자동차는_전진한다() {
-        Car car = new Car();
-        car.move(4);
-        assertThat(car).isEqualTo(new Car(1));
+        MoveStrategy trueMoveStrategy = () -> true;
+        Car car = new Car(trueMoveStrategy);
+        car.move();
+
+        assertThat(car).isEqualTo(new Car(1, trueMoveStrategy));
     }
 
     @Test
     void move_랜덤값4미만인경우_자동차는_현위치를유지한다() {
-        Car car = new Car();
-        car.move(3);
-        assertThat(car).isEqualTo(new Car());
+        MoveStrategy falseMoveStrategy = () -> false;
+        Car car = new Car(falseMoveStrategy);
+        car.move();
+
+        assertThat(car).isEqualTo(new Car(falseMoveStrategy));
     }
 
-    @DisplayName("현재 자동차 위치를 hyphen으로 변환한 문자열을 반환한다")
-    @Test
-    void hyphenatePosition() {
-        Car car = new Car(5);
-        assertThat(car.hyphenatePosition()).isEqualTo("-----");
-    }
 }
