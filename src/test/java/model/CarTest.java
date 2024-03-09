dd68dc40e40a7bc9b9ad8f10731c3ead679600f6
leejinwoo <leejinwoo1126@gmail.com>
2024-03-09 22:20:56 +0900

    test: name 추가에 따른 CarTest 수정

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -9,19 +9,19 @@ class CarTest {
     @Test
     void move_랜덤값4이상인경우_자동차는_전진한다() {
         MoveStrategy trueMoveStrategy = () -> true;
-        Car car = new Car(trueMoveStrategy);
+        Car car = new Car("test1", trueMoveStrategy);
         car.move();
 
-        assertThat(car).isEqualTo(new Car(1, trueMoveStrategy));
+        assertThat(car).isEqualTo(new Car("test1", 1, trueMoveStrategy));
     }
 
     @Test
     void move_랜덤값4미만인경우_자동차는_현위치를유지한다() {
         MoveStrategy falseMoveStrategy = () -> false;
-        Car car = new Car(falseMoveStrategy);
+        Car car = new Car("test1", falseMoveStrategy);
         car.move();
 
-        assertThat(car).isEqualTo(new Car(falseMoveStrategy));
+        assertThat(car).isEqualTo(new Car("test1", falseMoveStrategy));
     }
 
 }
