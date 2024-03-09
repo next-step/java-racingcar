4b29e356f8631c18d4e891ca97e4a9d0f716d2a5
leejinwoo <leejinwoo1126@gmail.com>
2024-03-09 22:40:53 +0900

    refactor: RacingGame 생성자 정리, record() 수정

--- src/main/java/model/RacingGame.java
+++ src/main/java/model/RacingGame.java
@@ -1,18 +1,9 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
     private final List<Car> cars;
-
-    public RacingGame(int numOfCars) {
-        this(numOfCars, new RandomMove());
-    }
-
-    public RacingGame(int numOfCars, MoveStrategy moveStrategy) {
-        this.cars = participate(numOfCars, moveStrategy);
-    }
 
     public RacingGame(List<Car> cars) {
         if (cars.isEmpty()) {
@@ -20,15 +11,6 @@ public class RacingGame {
         }
 
         this.cars = cars;
-    }
-
-    private List<Car> participate(int numOfCars, MoveStrategy moveStrategy) {
-        List<Car> result = new ArrayList<>();
-        for (int i = 1; i <= numOfCars; i++) {
-            result.add(new Car(moveStrategy));
-        }
-
-        return result;
     }
 
     public RacingRecord run(int turn) {
@@ -49,7 +31,7 @@ public class RacingGame {
 
     private void record(int turn, RacingRecord racingRecord) {
         for (Car car : cars) {
-            racingRecord.record(turn, car);
+            racingRecord.save(turn, car.toRecord());
         }
     }
 }
