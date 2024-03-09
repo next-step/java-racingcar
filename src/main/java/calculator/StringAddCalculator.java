bc9e9e9b51d37154c1ae4a15ede84f698c6cfd32
hellonayeon <hellonykwon@gmail.com>
2024-03-09 11:29:06 +0900

    Feature: 커스텀 구분자 파싱 로직 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,10 +1,25 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
     public static int sum(String str) {
         if (str == null || str.isEmpty()) {
             return 0;
+        }
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(str);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens= m.group(2).split(customDelimiter);
+
+            int sum = 0;
+            for (String s : tokens) {
+                sum += Integer.parseInt(s);
+            }
+            return sum;
         }
 
         String[] strArray = str.split("[,:]");
