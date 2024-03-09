4c065602d6fdf213bc2fdde3ff8be51513bfdd05
pado0 <82827406+pado0@users.noreply.github.com>
2024-03-09 21:35:20 +0900

    feat: 자동차 경주게임 구현 (#5322)

---
+++ src/main/java/step3_racing_car/GameRound.java
@@ -0,0 +1,15 @@
+package step3_racing_car;
+
+public class GameRound {
+    RacingCars racingCars;
+    public GameRound(RacingCars racingCars) {
+        this.racingCars = racingCars;
+    }
+
+    // game round 진행시 이동 전략을 선택할 수 있다.
+    void progressAllRound(int roundCount, MovingStrategy movingStrategy) {
+        for (int i = 0; i < roundCount; i++) {
+            racingCars.movePosition(movingStrategy.getMovingDistance());
+        }
+    }
+}
