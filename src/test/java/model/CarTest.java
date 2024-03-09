7995a8904812ba9de2c3a577cfbdceeae9edae4e
leejinwoo <leejinwoo1126@gmail.com>
2024-03-09 23:16:51 +0900

    import optimize

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,7 +1,6 @@
 package model;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
--- src/test/java/model/RacingGameTest.java
+++ src/test/java/model/RacingGameTest.java
@@ -1,6 +1,5 @@
 package model;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
