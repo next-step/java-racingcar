d0b3b0fd3d1f92e96186fbf2b1756f30a395f811
leejinwoo <leejinwoo1126@gmail.com>
2024-03-09 10:04:59 +0900

    test: @RepeatedTest 반영

--- src/test/java/util/RandomUtilTest.java
+++ src/test/java/util/RandomUtilTest.java
@@ -1,17 +1,15 @@
 package util;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomUtilTest {
 
-    @Test
+    @RepeatedTest(value = 100)
     @DisplayName("0 ~ 9 사이의 정수 값을 반환한다")
     void randomNumber() {
-        for (int i = 1; i <= 1000; i++) {
-            assertThat(RandomUtil.randomNumber()).isBetween(0, 9);
-        }
+        assertThat(RandomUtil.randomNumber()).isBetween(0, 9);
     }
 }
