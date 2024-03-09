fcb764985ec8c31d3fcca977f03eb94238ff9e6b
m5s3 <devmyoungsoo2@gmail.com>
2024-03-09 22:34:23 +0900

    refactor: 함수 쪼개는 리펙토링 진행

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,37 +1,64 @@
 package calculator;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
 
+    private static final String REGEX_CUSTOM_DELIMITER = "//(.*)\n(.*)";
+    private static final int EMPTY = 0;
+
     public static int splitAndSum(String text) {
-        //String REGEX_DELIMITER = "([,:])|(//(.)\n(.*))";
-        String REGEX_DELIMITER = "//(.*)\n(.*)";
-        if (Objects.isNull(text) || text.isEmpty()) {
-            return 0;
+        if (isEmpty(text)) {
+            return EMPTY;
         }
-        Matcher matcher = Pattern.compile(REGEX_DELIMITER).matcher(text);
-        if (matcher.find()) {
+
+        return sum(toInts(convertStringToStrings(text)));
+    }
+
+    private static String[] convertStringToStrings(String text) {
+        String[] strings = splitCustom(text);
+        if (!isEmpty(strings)) {
+            return strings;
+        }
+        return split(text);
+    }
+
+    private static boolean isEmpty(String[] strings) {
+        return strings.length == 0;
+    }
+
+    private static String[] split(String text) {
+        return text.split("[,:]");
+    }
+
+    private static String[] splitCustom(String text) {
+        Matcher matcher = Pattern.compile(REGEX_CUSTOM_DELIMITER).matcher(text);
+        if (matcher.matches()) {
             String delimiter = matcher.group(1);
-            System.out.println("delimiter = " + delimiter);
-            String[] strings = matcher.group(2).split(delimiter);
-            int result = 0;
-            for (String string : strings) {
-                System.out.println("string = " + string);
-                result += Integer.parseInt(string);
-            }
-            return result;
+            return matcher.group(2).split(delimiter);
         }
-        String[] strings = text.split(",|:");
+        return new String[]{};
+    }
+
+    private static Boolean isEmpty(String text) {
+        return Objects.isNull(text) || text.isEmpty();
+    }
+
+    private static int sum(int[] numbers) {
         int result = 0;
-        for (String string : strings) {
-            System.out.println("string = " + string);
-            result += Integer.parseInt(string);
+        for (int number : numbers) {
+            result += number;
         }
         return result;
+    }
+
+    private static int[] toInts(String[] strings) {
+        int[] numbers = new int[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            numbers[i] = Integer.parseInt(strings[i]);
+        }
+        return numbers;
     }
 }
