7d2485f9e71181b1d5cb0a9f0cfe2ab36ee8e3e7
naayoungkwon <kny8092@gmail.com>
2024-03-10 01:32:12 +0900

    refactor: Test 별 설명 추가
    - step2 code review 반영하여 DisplayName 활용

--- src/test/java/racingCar/RacingCarGameTest.java
+++ src/test/java/racingCar/RacingCarGameTest.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RacingCarGameTest {
 
   @Test
+  @DisplayName("자동차 경주 게임판을 만들었을 때 예외가 발생하지 않는다")
   void 자동차_경주_게임판_초기화(){
     RacingCarGame racingCarGame = new RacingCarGame();
 
@@ -16,6 +18,7 @@ public class RacingCarGameTest {
   }
 
   @Test
+  @DisplayName("자동차 경주 게임을 시작하면 경기 라운드 수 만큼의 결과를 전달받는다")
   void 자동차_경주_게임판_시작(){
     int carNum = 3;
     int gameTryNum = 5;
