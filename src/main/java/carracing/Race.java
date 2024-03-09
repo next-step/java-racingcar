f8ba82d47488ffe7572441753ff2f8acf2f4bb3d
hong yuseok <jericoh@naver.com>
2024-03-09 18:03:20 +0900

    feat: Race 구현

---
+++ src/main/java/carracing/Race.java
@@ -0,0 +1,46 @@
+package carracing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Race {
+    private int numberOfCars;
+    private int numberOfAttempts;
+
+    public Race(int numberOfCars, int numberOfAttempts) {
+        this.numberOfCars = numberOfCars;
+        this.numberOfAttempts = numberOfAttempts;
+    }
+
+    public List<RaceStatus> start() {
+        return doRace(participatedCarList());
+    }
+
+    private List<Car> participatedCarList() {
+        List<Car> participatedCarList = new ArrayList<>();
+        for(int i = 0; i < numberOfCars; i++) {
+            participatedCarList.add(new Car(new RandomStrategy()));
+        }
+
+        return participatedCarList;
+    }
+
+    private List<RaceStatus> doRace(List<Car> carList) {
+        List<RaceStatus> raceStatusList = new ArrayList<>();
+        for(int i = 0; i < numberOfAttempts; i++) {
+            carList.forEach(Car::move);
+            raceStatusList.add(raceStatus(carList));
+        }
+
+        return raceStatusList;
+    }
+
+    private RaceStatus raceStatus(List<Car> carList) {
+        List<Integer> positionOfCarList = carList.stream()
+                .map(Car::position)
+                .collect(Collectors.toList());
+
+        return new RaceStatus(positionOfCarList);
+    }
+}
