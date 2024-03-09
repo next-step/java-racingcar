166badd9d187297e3efea01f4dc067338b89575c
naayoungkwon <kny8092@gmail.com>
2024-03-10 01:19:10 +0900

    [step2] Calculator code review 반영
    - 2N을 N으로 줄여 입력을 바로 검증하도록 변경

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,19 +6,12 @@ import java.util.regex.Pattern;
 public class Calculator {
 
   public static int calculate(String text) {
-    // Indent 1까지만 허용하고 그 이상을 리팩토링 대상으로 보자
-    // Indent를 줄이는 방법은 메소드로 추출하는 것이다.
-    // Indent 정말 최대 2까지 하고 그 이후는 stream, lambda를 사용해서 리팩토링 해보자
-    // stream, lambda 처음부터 남발하면 OOP 연습하기 어렵다.
-    // 코드가 2N 이기는 하지만, 성능에 크게 무리가 없으며, 문제가 있으면 그때 합치면 된다.
 
     if (isBlank(text)) {
       return 0;
     }
 
-    int[] numbers = toInts(split(text));
-    verifyPositive(numbers);
-    return sum(numbers);
+    return sum(toInts(split(text)));
   }
 
   private static boolean isBlank(String text) {
@@ -45,16 +38,16 @@ public class Calculator {
   private static int[] toInts(String[] values) {
     int[] numbers = new int[values.length];
     for (int i = 0; i < values.length; i++) {
-     numbers[i] = Integer.parseInt(values[i]);
+      int number = Integer.parseInt(values[i]);
+      verifyPositive(number);
+      numbers[i] = number;
     }
     return numbers;
   }
 
-  private static void verifyPositive(int[] numbers) {
-    for (int number : numbers) {
-      if (number < 0) {
-        throw new RuntimeException("음수는 입력으로 올 수 없습니다.");
-      }
+  private static void verifyPositive(int number) {
+    if (number < 0) {
+      throw new RuntimeException("음수는 입력으로 올 수 없습니다.");
     }
   }
 
