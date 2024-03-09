44a896f3466368c03f48210f78c64108675122c6
hong yuseok <jericoh@naver.com>
2024-03-09 16:44:57 +0900

    feat: Car 구현

---
+++ src/main/java/carracing/Car.java
@@ -0,0 +1,18 @@
+package carracing;
+
+public class Car {
+    private int position;
+    private MovingStrategy movingStrategy;
+
+    public Car(MovingStrategy movingStrategy) {
+        this.movingStrategy = movingStrategy;
+    }
+
+    public int position() {
+        return this.position;
+    }
+
+    public void move() {
+        position += movingStrategy.move();
+    }
+}
