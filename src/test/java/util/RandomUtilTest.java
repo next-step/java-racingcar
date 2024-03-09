45c6a574ce45129eea039cbd8349e82f519e0c73
leejinwoo <leejinwoo1126@gmail.com>
2024-03-09 20:44:39 +0900

    test: RandomUtilTest 인스턴스 사용하도록 변경

--- src/test/java/util/RandomUtilTest.java
+++ src/test/java/util/RandomUtilTest.java
@@ -7,9 +7,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomUtilTest {
 
+    private final RandomUtil randomUtil = new RandomUtil();
+
     @RepeatedTest(value = 100)
     @DisplayName("0 ~ 9 사이의 정수 값을 반환한다")
     void randomNumber() {
-        assertThat(RandomUtil.randomNumber()).isBetween(0, 9);
+        assertThat(randomUtil.randomNumber()).isBetween(0, 9);
     }
 }
