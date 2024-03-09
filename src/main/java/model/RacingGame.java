ed1214cce10c3d613e900f8bdbff62d1a488a65d
leejinwoo <leejinwoo1126@gmail.com>
2024-03-09 12:44:29 +0900

    refactor: RacingGame 피드백 반영

--- src/main/java/model/RacingGame.java
+++ src/main/java/model/RacingGame.java
@@ -1,37 +1,69 @@
 package model;
 
-import util.RandomUtil;
+import view.ResultView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class RacingGame {
     private final List<Car> carList;
-    private final RacingRecord racingRecord;
 
-    public RacingGame() {
-        this.carList = new ArrayList<>();
-        this.racingRecord = new RacingRecord();
+    public RacingGame(int numOfCars) {
+        this(numOfCars, new RandomMove());
     }
 
-    public void participate(int count) {
-        for (int i = 1; i <= count; i++) {
-            this.carList.add(new Car());
+    public RacingGame(int numOfCars, MoveStrategy moveStrategy) {
+        this.carList = participate(numOfCars, moveStrategy);
+    }
+
+    public RacingGame(List<Car> cars) {
+        this.carList = cars;
+    }
+
+    private List<Car> participate(int numOfCars, MoveStrategy moveStrategy) {
+        List<Car> result = new ArrayList<>();
+        for (int i = 1; i <= numOfCars; i++) {
+            result.add(new Car(moveStrategy));
         }
+
+        return result;
     }
 
-    public void run() {
+    public ResultView run(int turn) {
+        ResultView resultView = new ResultView();
+        for (int t = 1; t <= turn; t++) {
+            move();
+            record(resultView);
+        }
+
+        return resultView;
+    }
+
+    public void move() {
         for (Car car : carList) {
-            int random = RandomUtil.randomNumber();
-            car.move(random);
-            racingRecord.record(car);
+            car.move();
+        }
+    }
+
+    private void record(ResultView resultView) {
+        for (Car car : carList) {
+            resultView.record(car);
         }
 
-        racingRecord.appendNewLine();
+        resultView.appendNewLine();
     }
 
-    public String racingResult() {
-        return racingRecord.toString();
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RacingGame that = (RacingGame) o;
+        return Objects.equals(carList, that.carList);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(carList);
+    }
 }
