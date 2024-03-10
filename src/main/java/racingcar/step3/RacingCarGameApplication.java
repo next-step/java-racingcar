b0ce65d20aecf1080f78885240b5013731f87cb1
yejikwon <yeeji77@naver.com>
2024-03-11 02:43:41 +0900

    feat: 구현한 내용을 확인할 수 있는 main 메서드 생성

---
+++ src/main/java/racingcar/step3/RacingCarGameApplication.java
@@ -0,0 +1,33 @@
+package racingcar.step3;
+
+import racingcar.step3.domain.Car;
+import racingcar.step3.domain.RacingCarGame;
+import racingcar.step3.domain.RacingCarGameResult;
+import racingcar.step3.ui.InputValue;
+import racingcar.step3.ui.InputView;
+import racingcar.step3.ui.ResultView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCarGameApplication {
+
+  public static void main(String[] args) {
+    new RacingCarGameApplication().run(args);
+  }
+
+  public void run(String[] args) {
+    InputValue input = InputView.receive();
+
+
+    List<Car> cars = new ArrayList<>();
+    for (int i = 1; i <= input.getNumberOfCars(); i++) {
+      String carName = "car" + i;
+      cars.add(new Car(carName));
+    }
+
+    RacingCarGame racingCarGame = new RacingCarGame(input.getTryCount(), cars);
+    List<RacingCarGameResult> results = racingCarGame.start();
+    ResultView.printResult(results);
+  }
+}
