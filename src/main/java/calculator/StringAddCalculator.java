08b01f73c753657524b8258f51d314de77bd692c
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-10 22:08:43 +0900

    refactor 객체지향 프로그래밍 숙달 과정이기 때문에 그 원칙에 맞춰 static method를 지양하자

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,7 +3,7 @@ package calculator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class StringAddCalculator {
+public class StringAddCalculator {
 
     private static final int ZERO = 0;
     private static final int CUSTOM_SEPARATOR = 1;
@@ -11,28 +11,28 @@ public final class StringAddCalculator {
     private static final String ONE_NUMBER_REGEX = "\\d+";
     private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
 
-    public static int calculate(String text) {
+    public int calculate(String text) {
         if (text == null || text.isBlank()) {
             return ZERO;
         }
         return sum(toPositiveInts(parse(text)));
     }
 
-    private static String[] toPositiveInts(String[] texts) {
+    private String[] toPositiveInts(String[] texts) {
         for (String text : texts) {
             isNegative(text);
         }
         return texts;
     }
 
-    private static void isNegative(String text) {
+    private void isNegative(String text) {
         if (Integer.parseInt(text) < 0) {
             throw new IllegalArgumentException(String
                     .format("text(%s)는 음수입니다.", text));
         }
     }
 
-    private static int sum(String[] numbers) {
+    private int sum(String[] numbers) {
         int number = 0;
         for (String s : numbers) {
             number += Integer.parseInt(s);
@@ -40,14 +40,14 @@ public final class StringAddCalculator {
         return number;
     }
 
-    private static String[] parse(String text) {
+    private String[] parse(String text) {
         if (!(text.contains(",") || text.contains(":") || text.matches(ONE_NUMBER_REGEX))) {
             return customParse(match(text));
         }
         return text.split(",|:");
     }
 
-    private static String[] customParse(Matcher matcher) {
+    private String[] customParse(Matcher matcher) {
         if (!matcher.find()) {
             throw new IllegalArgumentException("주어진 문자열은 CUSTOM_PATTERN의 형태와 일치하지 않습니다.");
         }
@@ -55,7 +55,7 @@ public final class StringAddCalculator {
         return matcher.group(INPUT_STRING).split(matcher.group(CUSTOM_SEPARATOR));
     }
 
-    private static Matcher match(String text) {
+    private Matcher match(String text) {
         return CUSTOM_PATTERN.matcher(text);
     }
 
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,47 +1,47 @@
 package calculator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static calculator.StringAddCalculator.calculate;
 import static org.assertj.core.api.Assertions.*;
 
 public class StringAddCalculatorTest {
 
+    StringAddCalculator calculator = new StringAddCalculator();
+
     @Test
     void negativeTest() {
         assertThatThrownBy(() -> {
-            calculate("-1,3,2");
+            calculator.calculate("-1,3,2");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(String.format("text(%s)는 음수입니다.", "-1"));
     }
 
     @Test
     void customTest() {
-        int number = calculate("//;\n1;2;3");
+        int number = calculator.calculate("//;\n1;2;3");
         assertThat(number).isEqualTo(6);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1,2=3", "1:2=3", "1:2,3=6", "1:2,3:9=15"}, delimiter = '=')
     void colonAndCommaTest(String input, int expected) {
-        int number = calculate(input);
+        int number = calculator.calculate(input);
         assertThat(number).isEqualTo(expected);
     }
 
     @Test
     void oneNumberTest2() {
-        int number = calculate("111");
+        int number = calculator.calculate("111");
         assertThat(number).isEqualTo(111);
     }
 
     @Test
     void oneNumberTest() {
-        int number = calculate("1");
+        int number = calculator.calculate("1");
         assertThat(number).isEqualTo(1);
     }
 
@@ -49,7 +49,7 @@ public class StringAddCalculatorTest {
     @NullAndEmptySource
     @ValueSource(strings = {" ", ""})
     void isNullAndBlankTest(String input) {
-        int number = calculate(input);
+        int number = calculator.calculate(input);
         assertThat(number).isEqualTo(0);
     }
 
