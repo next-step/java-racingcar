cacc1910d85684479177e0871aaa86c597ce9622
dongock <okdong23@naver.com>
2024-03-10 13:17:39 +0900

    chore: 패키지 구조 정리(StringCaculator)

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,6 +1,6 @@
-package study;
+package stringcalculator;
 
-import StringCalculator.StringCalculator;
+import stringcalculator.StringCalculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
