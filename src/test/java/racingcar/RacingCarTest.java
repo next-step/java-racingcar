ede4301bf3177457ce1679a2436914aa798934da
hellonayeon <hellonykwon@gmail.com>
2024-03-11 01:14:39 +0900

    Test: AssertJ.isBetween 메서드 경계값 테스트 작성

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -3,7 +3,7 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class RacingCarTest {
 
@@ -41,6 +41,20 @@ public class RacingCarTest {
     @DisplayName("[성공] 0~9 사이의 랜덤한 값을 생성한다.")
     void 랜덤값_생성() {
         assertThat(RandomValueGenerator.generate()).isBetween(0, 9);
+    }
+
+    @Test
+    @DisplayName("[학습 테스트] AssertJ.isBetween() 경계값")
+    void isBetween_메서드_경계값() {
+        assertThat(0).isBetween(0, 9);
+        assertThat(9).isBetween(0, 9);
+
+        assertThatThrownBy(() -> {
+            assertThat(-1).isBetween(0, 9);
+        }).isInstanceOf(AssertionError.class);
+        assertThatThrownBy(() -> {
+            assertThat(10).isBetween(0, 9);
+        }).isInstanceOf(AssertionError.class);
     }
 
 }
