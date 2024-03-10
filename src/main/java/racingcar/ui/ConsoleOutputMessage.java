a8063c0b06090f3594bfd4b409ee2e9fc7f2ec72
hellonayeon <hellonykwon@gmail.com>
2024-03-11 01:47:21 +0900

    Refactor: 콘솔 출력 메시지 열거형 이름 수정

--- src/main/java/racingcar/ui/ConsoleOutputMessage.java
+++ src/main/java/racingcar/ui/ConsoleOutputMessage.java
@@ -2,8 +2,8 @@ package racingcar.ui;
 
 public enum ConsoleOutputMessage {
 
-    CAR_COUNT("자동차 대수는 몇 대 인가요?"),
-    MOVE_COUNT("시도할 횟수는 몇 회 인가요?");
+    HOW_MANY_CARS("자동차 대수는 몇 대 인가요?"),
+    HOW_MANY_TRYS("시도할 횟수는 몇 회 인가요?");
 
     ConsoleOutputMessage(String message) {
 
