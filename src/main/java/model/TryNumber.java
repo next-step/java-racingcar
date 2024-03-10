16fdbefd5c54ab5cd0b4ce2b7ac3a0a030f183d4
leeedohyun <ldh19738@gmail.com>
2024-03-10 17:28:05 +0900

    feat: 시도 횟수가 양수가 아니면 예외 발생

--- src/main/java/model/TryNumber.java
+++ src/main/java/model/TryNumber.java
@@ -5,10 +5,17 @@ public class TryNumber {
     private final int value;
 
     public TryNumber(final int value) {
+        validatePositive(value);
         this.value = value;
     }
 
     public int getValue() {
         return value;
+    }
+
+    private void validatePositive(final int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("양수만 입력할 수 있습니다.");
+        }
     }
 }
