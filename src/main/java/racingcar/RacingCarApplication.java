a357d233328f5d8112866fcd4f4cf0611d27532b
Danden1 <yongha.kwon12@gmail.com>
2024-03-10 23:50:33 +0900

    feat: 게임을 실행할 수 있는 controller 와 main 함수 추가

---
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import racingcar.application.RacingCarService;
+import racingcar.domain.MoveCommand;
+import racingcar.infra.MoveCommandImpl;
+import racingcar.infra.RandomNumber;
+import racingcar.presentation.InputView;
+import racingcar.presentation.RacingCarController;
+import racingcar.presentation.ResultView;
+
+public class RacingCarApplication {
+
+    public static void main(String[] args) {
+        RacingCarService racingCarService = racingCarService(moveCommand());
+        RacingCarController racingCarController = racingCarController(racingCarService);
+
+        racingCarController.startGame();
+    }
+
+
+    private static RacingCarService racingCarService(MoveCommand moveCommand) {
+        return new RacingCarService(moveCommand);
+    }
+
+    private static MoveCommand moveCommand() {
+        return new MoveCommandImpl(new RandomNumber());
+    }
+
+    private static RacingCarController racingCarController(RacingCarService racingCarService) {
+        return new RacingCarController(new InputView(), new ResultView(), racingCarService);
+    }
+}
