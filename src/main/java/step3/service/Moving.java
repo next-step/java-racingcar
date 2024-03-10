275068f3eb1e18afca136c6974b9f202916ea221
kwanik-kor <rhksdlr134@naver.com>
2024-03-10 22:28:56 +0900

    feat: 자동차 움직임 정책 구현

---
+++ src/main/java/step3/service/Moving.java
@@ -0,0 +1,18 @@
+package step3.service;
+
+/**
+ * 자동차의 움직이는 방법
+ */
+public interface Moving {
+
+    /**
+     * @return 움직이는 거리
+     */
+    int movingDistance();
+
+    /**
+     * @return 움직일 수 있는지 여부
+     */
+    boolean movable();
+
+}
---
+++ src/main/java/step3/service/RandomMoving.java
@@ -0,0 +1,24 @@
+package step3.service;
+
+import java.util.Random;
+
+public class RandomMoving implements Moving {
+    private final static int MOVING_DISTANCE = 1;
+    private final static int MOVABLE_CRITERIA_LOWER_BOUND = 4;
+    private final Random random = new Random();
+
+    @Override
+    public int movingDistance() {
+        return MOVING_DISTANCE;
+    }
+
+    @Override
+    public boolean movable() {
+        return randomNumber() >= MOVABLE_CRITERIA_LOWER_BOUND;
+    }
+
+    private int randomNumber() {
+        return random.nextInt(10);
+    }
+
+}
