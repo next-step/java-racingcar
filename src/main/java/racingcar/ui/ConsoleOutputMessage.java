e65a016fcd99bbbc96bf72899f56aa57cafe23cc
hellonayeon <hellonykwon@gmail.com>
2024-03-11 02:58:18 +0900

    Feature: '실행 결과' 콘솔 출력 메시지 타입 추가

--- src/main/java/racingcar/ui/ConsoleOutputMessage.java
+++ src/main/java/racingcar/ui/ConsoleOutputMessage.java
@@ -3,7 +3,8 @@ package racingcar.ui;
 public enum ConsoleOutputMessage {
 
     HOW_MANY_CARS("자동차 대수는 몇 대 인가요?"),
-    HOW_MANY_TRYS("시도할 횟수는 몇 회 인가요?");
+    HOW_MANY_TRYS("시도할 횟수는 몇 회 인가요?"),
+    EXECUTE_RESULT("실행 결과");
 
     private final String message;
 
