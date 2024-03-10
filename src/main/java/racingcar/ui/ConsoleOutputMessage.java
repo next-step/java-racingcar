32225c39534642c0ede5b2bdc3fab993ee6a2545
hellonayeon <hellonykwon@gmail.com>
2024-03-11 02:48:19 +0900

    Feature: 콘솔 출력 메시지 열거형 수정, toString 오버라이딩

--- src/main/java/racingcar/ui/ConsoleOutputMessage.java
+++ src/main/java/racingcar/ui/ConsoleOutputMessage.java
@@ -5,7 +5,14 @@ public enum ConsoleOutputMessage {
     HOW_MANY_CARS("자동차 대수는 몇 대 인가요?"),
     HOW_MANY_TRYS("시도할 횟수는 몇 회 인가요?");
 
-    ConsoleOutputMessage(String message) {
+    private final String message;
 
+    ConsoleOutputMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
     }
 }
