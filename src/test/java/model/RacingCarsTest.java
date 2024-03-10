b28fa76822d328077f4fbdb92191f46a8e0d271f
leeedohyun <ldh19738@gmail.com>
2024-03-10 15:18:32 +0900

    test: 우승자 선정 테스트

--- src/test/java/model/RacingCarsTest.java
+++ src/test/java/model/RacingCarsTest.java
@@ -19,4 +19,33 @@ class RacingCarsTest {
         // then
         assertThat(racingCars.getRacingCars()).hasSize(3);
     }
+
+    @Test
+    void 우승자_한_명_테스트() {
+        // given
+        final RacingCar pobi = new RacingCar(new Location(1), new Name("pobi"));
+        final RacingCar crong = new RacingCar(new Location(4), new Name("crong"));
+        final RacingCar honux = new RacingCar(new Location(2), new Name("honux"));
+        final RacingCars racingCars = new RacingCars(List.of(pobi, crong, honux));
+
+        // when
+        final List<Name> winners = racingCars.findWinners();
+
+        // then
+        assertThat(winners).containsExactly(new Name("crong"));
+    }
+
+    @Test
+    void 우승자_두_명_테스트() {
+        final RacingCar pobi = new RacingCar(new Location(1), new Name("pobi"));
+        final RacingCar crong = new RacingCar(new Location(4), new Name("crong"));
+        final RacingCar honux = new RacingCar(new Location(4), new Name("honux"));
+        final RacingCars racingCars = new RacingCars(List.of(pobi, crong, honux));
+
+        // when
+        final List<Name> winners = racingCars.findWinners();
+
+        // then
+        assertThat(winners).containsExactly(new Name("crong"), new Name("honux"));
+    }
 }
