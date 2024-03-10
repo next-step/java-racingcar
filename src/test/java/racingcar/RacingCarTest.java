ac3a8b8f70099ad6f350646f5cacc89d8da8b8a5
hellonayeon <hellonykwon@gmail.com>
2024-03-11 02:03:56 +0900

    Test: 자동차 리스트 생성 단위 테스트 작성

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -57,4 +57,18 @@ public class RacingCarTest {
         }).isInstanceOf(AssertionError.class);
     }
 
+    @Test
+    @DisplayName("[성공] 자동차 3개를 가진 자동차 리스트를 생성한다.")
+    void 자동차_리스트_생성_자동차3() {
+        Cars cars = new Cars(3);
+        assertThat(cars.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("[성공] 자동차 1개를 가진 자동차 리스트를 생성한다.")
+    void 자동차_리스트_생성_자동차1() {
+        Cars cars = new Cars(1);
+        assertThat(cars.size()).isEqualTo(1);
+    }
+
 }
