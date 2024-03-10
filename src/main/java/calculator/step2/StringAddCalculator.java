8056f0f25564d87a18f3605f04a1b46d2764cd28
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-10 15:11:57 +0900

    test 음수임을 정확하게 판단해서 테스트해라

--- src/main/java/calculator/step2/StringAddCalculator.java
+++ src/main/java/calculator/step2/StringAddCalculator.java
@@ -8,7 +8,7 @@ public final class StringAddCalculator {
     private static final int ZERO = 0;
     private static final int CUSTOM_SEPARATOR = 1;
     private static final int INPUT_STRING = 2;
-    private static final String NEGATIVE = "-";
+//    private static final String NEGATIVE = "-";
     private static final String ONE_NUMBER_REGEX = "\\d+";
     private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
 
@@ -17,7 +17,7 @@ public final class StringAddCalculator {
             return ZERO;
         }
 
-        if (text.contains(NEGATIVE)) {
+        if (hasContainNegative(text)) {
             throw new IllegalArgumentException(String
                     .format("text(%s)엔 음수가 포함되어 있습니다. 음수는 허용되지 않습니다.", text));
         }
@@ -26,6 +26,17 @@ public final class StringAddCalculator {
             return sum(parse(text));
         }
         return sum(customParse(match(text)));
+    }
+
+    private static boolean hasContainNegative(String text) {
+        String[] stringNumbers = parse(text);
+        for (String stringNumber : stringNumbers) {
+            int number = Integer.parseInt(stringNumber);
+            if (number < 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static int sum(String[] numbers) {
