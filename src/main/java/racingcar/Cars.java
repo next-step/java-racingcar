3ff40984eed5522350c032b8d258ef0419419f55
hellonayeon <hellonykwon@gmail.com>
2024-03-11 02:04:22 +0900

    Feature: 자동차리스트 일급 컬렉션 작성

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    private final List<Car> carList;
+
+    public Cars(int size) {
+        this.carList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            this.carList.add(new Car(new CarMoveCondition()));
+        }
+    }
+
+    public int size() {
+        return carList.size();
+    }
+}
