a16002332786e50c51572a267aa60376452bfded
Sangmin <sm9171@nate.com>
2024-03-10 14:58:10 +0900

    [step2] 문자열 덧셈 계산기 (#5303)
    * feat: Add StringTest
    
    * Fix: Change StringTest DisplayName
    
    * Feat: Add SetCollectionTest
    
    * Docs: Add TODO.md
    
    * test: Create additionCalculateTest failure case
    
    * test: Rename displayName
    * 커스텀 문자열이 들어갈 때 덧셈 계산 -> 덧셈 계산
    
    * test: Change additionCalculateTest to success case
    
    * test: Refactor additionCalculateTest
    
    * test: Add additionCalculateWithNullOrEmptyInputValue SuccessCase
    
    * test: Add additionCalculateWithSingleNumber SuccessCase
    
    * test: Add additionCalculateWithCustomSign FailCase
    
    * test: Fix additionCalculateWithCustomSign FailCase
    * input값 수정 //;\\n1;2;3 -> //;\n1;2;3
    
    * test: Change additionCalculateWithCustomSign SuccessCase
    
    * test: Refactor additionCalculateWithCustomSign
    
    * test: Refactor additionCalculateWithCustomSign
    
    * test: Add additionCalculateRuntimeException FailCase
    
    * test: Change additionCalculateRuntimeException SuccessCase
    
    * fix : Modify modifications
    * 내부에서만 사용되는 상수 private으로 변경
    * 패턴을 static 상수메서드로 분리
    * 매직넘버를 상수로 변경
    * 오류메시지 작성

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,51 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+
+	private static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
+	public static final int CUSTOM_DELIMITER_INDEX = 1;
+	public static final int NUBERS_INDEX = 2;
+
+	public int additionCalculate(final String input) {
+		if (input == null || input.equals("")) {
+			return 0;
+		}
+
+		Matcher matcher = getMatcher(input);
+
+		if (matcher.find()) {
+			return calculateAdditionWithCustomSign(matcher);
+		}
+
+		return calculateAddition(input.split(",|:"));
+	}
+
+	private static Matcher getMatcher(String input) {
+		return Pattern.compile(CUSTOM_SEPARATOR).matcher(input);
+	}
+
+	private static int calculateAdditionWithCustomSign(final Matcher matcher) {
+		String customDelimiter = matcher.group(CUSTOM_DELIMITER_INDEX);
+		String[] numbers = matcher.group(NUBERS_INDEX).split(customDelimiter);
+		return calculateAddition(numbers);
+	}
+
+	private static int calculateAddition(final String[] number) {
+		int result = 0;
+
+		for (int i = 0; i < number.length; i++) {
+			checkNegativeNumber(number, i);
+			result += Integer.parseInt(number[i]);
+		}
+		return result;
+	}
+
+	private static void checkNegativeNumber(final String[] number, final int i) {
+		if (Integer.parseInt(number[i]) < 0) {
+			throw new RuntimeException("음수는 입력할 수 없습니다. 0 이상의 숫자를 입력해주세요.");
+		}
+	}
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,81 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class CalculatorTest {
+
+	@DisplayName("덧셈 계산")
+	@Test
+	void additionCalculate() {
+		// given
+		String input = "1,2:3";
+		Calculator calculator = new Calculator();
+
+		// when
+		int result = calculator.additionCalculate(input);
+
+		// then
+		assertThat(result).isEqualTo(6);
+	}
+
+	@DisplayName("빈값이나 null값으로 덧셈 계산을 하면 0을 반환한다.")
+	@ParameterizedTest
+	@NullAndEmptySource
+	void additionCalculateWithNullOrEmptyInputValue(String input) {
+		// given
+		Calculator calculator = new Calculator();
+
+		// when
+		int result = calculator.additionCalculate(input);
+
+		// then
+		assertThat(result).isEqualTo(0);
+	}
+
+	@DisplayName("숫자 하나 입력시에 그 숫자를 그대로 반환한다.")
+	@Test
+	void additionCalculateWithSingleNumber() {
+		// given
+		String input = "3";
+		Calculator calculator = new Calculator();
+
+		// when
+		int result = calculator.additionCalculate(input);
+
+		// then
+		assertThat(result).isEqualTo(3);
+	}
+
+	@DisplayName("'//'와 '\n' 사이에 위치하는 문자를 덧셈기호로 사용할수 있다.")
+	@Test
+	void additionCalculateWithCustomSign() {
+		// given
+		String input = "//;\n1;2;3";
+		Calculator calculator = new Calculator();
+
+		// when
+		int result = calculator.additionCalculate(input);
+
+		// then
+		assertThat(result).isEqualTo(6);
+	}
+
+	@DisplayName("숫자 이외의 값이나 음수를 입력시에 RuntimeException")
+	@ParameterizedTest
+	@ValueSource(strings = {"wege", "32g:23g", "-1:3"})
+	void additionCalculateRuntimeException(String input) {
+		// given
+		Calculator calculator = new Calculator();
+
+		// when&then
+		assertThatThrownBy(() -> calculator.additionCalculate(input))
+				.isInstanceOf(RuntimeException.class);
+	}
+}
