c3ecc6a3c4cc29e5817517f1224a949c9bb48edf
Sangmin <sm9171@nate.com>
2024-03-10 18:58:54 +0900

    refactor : Change position field initial value * 0 -> 1

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,12 +3,20 @@ package racingcar;
 public class Car {
     private static final int ADVANCE_CONDITION_NUMBER = 4;
 
-    private int position = 0;
+    private int position = 1;
 
     public void move(int random) {
         if (random >= ADVANCE_CONDITION_NUMBER) {
             position++;
         }
+    }
+
+    public String getPositionWithHyphen() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
     }
 
     public int getPosition() {
