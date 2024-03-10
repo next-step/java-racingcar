1e362ee9442ba006defd44ad44fe05444819b8b7
yushin <kkad45@naver.com>
2024-03-10 12:38:58 +0900

    feat/step3 코드 리팩토링

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -1,5 +1,6 @@
 package step3;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
@@ -8,9 +9,21 @@ public class InputView {
     public static Racing createRacing() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("자동차 대수는 몇 대 인가요?");
-        int carNumber = scanner.nextInt();
+        int carNumber = createIntNumber(scanner);
         System.out.println("시도할 횟수는 몇 회 인가여?");
-        int tryNumber = scanner.nextInt();
+        int tryNumber = createIntNumber(scanner);
         return new Racing(carNumber, tryNumber);
+    }
+
+    private static int createIntNumber(Scanner scanner) {
+        try {
+            int carNumber = scanner.nextInt();
+            if (carNumber < 0) {
+                throw new InputMismatchException();
+            }
+            return carNumber;
+        } catch (InputMismatchException exception) {
+            throw new InputMismatchException("양수의 숫자만 입력해주세요");
+        }
     }
 }
--- src/main/java/step3/RandomNumberCreator.java
+++ src/main/java/step3/RandomNumberCreator.java
@@ -1,5 +1,9 @@
 package step3;
 
 public interface RandomNumberCreator {
+    /**
+     * 랜덤 숫자 반환
+     * @return
+     */
     int getRandomNumber();
 }
