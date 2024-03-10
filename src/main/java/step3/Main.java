b635c5d248cc669910be87c950305b2a2fb3b2f9
kwanik-kor <rhksdlr134@naver.com>
2024-03-10 21:18:02 +0900

    feat: 자동차 경주 Input 구현 완료

---
+++ src/main/java/step3/Main.java
@@ -0,0 +1,16 @@
+package step3;
+
+public class Main {
+
+    public void playRacingGame() {
+        final int carCount = RacingInputView.carCount();
+        final int tryCount = RacingInputView.tryCount();
+    }
+
+    public static void main(String[] args) {
+        final Main main = new Main();
+
+        main.playRacingGame();
+    }
+
+}
