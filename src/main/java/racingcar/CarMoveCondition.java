3b57436b1134e0c93c4836be17595042cc359f47
hellonayeon <hellonykwon@gmail.com>
2024-03-11 02:42:56 +0900

    Refactor: static 메서드만 존재하는 CarMoveCondition 클래스 private 생성자 추가

--- src/main/java/racingcar/CarMoveCondition.java
+++ src/main/java/racingcar/CarMoveCondition.java
@@ -2,6 +2,10 @@ package racingcar;
 
 public class CarMoveCondition {
 
+    private CarMoveCondition() {
+
+    }
+
     public static boolean isMovable(int value) {
         return value >= 4;
     }
