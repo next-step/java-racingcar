41ebe5ec4c645ebf8253f6750d5dac09c311e22a
yushin <kkad45@naver.com>
2024-03-10 12:16:14 +0900

    feat/step3 racing 클래스에 CarList 변수 추가

--- src/main/java/step3/Racing.java
+++ src/main/java/step3/Racing.java
@@ -1,8 +1,13 @@
 package step3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Racing {
     private final int carNumber;
     private final int tryNumber;
+    private final CarList carList;
+
     public static Racing createRacing(int carNumber, int tryNumber) {
         return new Racing(carNumber, tryNumber);
     }
@@ -10,6 +15,15 @@ public class Racing {
     public Racing(int carNumber, int tryNumber) {
         this.carNumber = carNumber;
         this.tryNumber = tryNumber;
+        this.carList = createCarList(carNumber);
+    }
+
+    private CarList createCarList(int carNumber) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < carNumber; i++) {
+            cars.add(new Car());
+        }
+        return CarList.from(cars, new RandomCreator());
     }
 
 
