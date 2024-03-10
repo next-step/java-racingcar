096352a74c29b3f0d08b8e4381482bbf44829f42
hong yuseok <jericoh@naver.com>
2024-03-10 22:30:00 +0900

    refactor: 리뷰 수정 사항 반영 - 정적 상수 대문자 수정

--- src/main/java/carracing/model/RandomStrategy.java
+++ src/main/java/carracing/model/RandomStrategy.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class RandomStrategy implements MovingStrategy {
     private static final int RANDOM_INTEGER_BOUND_ZERO_TO_NINE = 10;
     private static final int RANDOM_MOVABLE_CRITERIA = 4;
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
 
     @Override
     public boolean isMovable() {
-        return random.nextInt(RANDOM_INTEGER_BOUND_ZERO_TO_NINE) >= RANDOM_MOVABLE_CRITERIA;
+        return RANDOM.nextInt(RANDOM_INTEGER_BOUND_ZERO_TO_NINE) >= RANDOM_MOVABLE_CRITERIA;
     }
 }
