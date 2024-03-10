2142b170b59404cf1e49d91e853aff0056f63ba8
dongock <okdong23@naver.com>
2024-03-10 14:37:50 +0900

    feat:[3-1] Car move Test

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,15 @@
 package racingcar;
 
 public class Car {
+    private int distance = 0;
+
+    public int getDistance() {
+        return this.distance;
+    }
+
+    public void move(int data) {
+        if (data >= 4) {
+            distance += 1;
+        }
+    }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,6 +2,8 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,5 +16,42 @@ public class CarTest {
         assertThat(car).isInstanceOf(Car.class);
     }
 
+    @Test
+    @DisplayName("만들어진 Car의 초기 거리값 Test")
+    void compareDistanceTest() {
+        // given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        // when
+        int car1Dinstance = car1.getDistance();
+        int car2Dinstance = car2.getDistance();
+        //then
+        assertThat(car1Dinstance).isEqualTo(car2Dinstance);
+    }
 
+    @ParameterizedTest
+    @DisplayName("주어진 값에 대한 Car의 정지 Test")
+    @ValueSource(ints = {0, 1})
+    void stopCarTest(int data) {
+        // given
+        Car car = new Car();
+        // when
+        car.move(data);
+        int distance = car.getDistance();
+        // then
+        assertThat(distance).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("주어진 값에 대한 Car의 정지 Test")
+    @ValueSource(ints = {4, 9})
+    void moveCarTest(int data) {
+        // given
+        Car car = new Car();
+        // when
+        car.move(data);
+        int distance = car.getDistance();
+        // then
+        assertThat(distance).isEqualTo(1);
+    }
 }
