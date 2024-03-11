82f9b7de32c18e30b93150841e3db57df13b61df
deepredk <deepred.kim@gmail.com>
2024-03-11 21:00:20 +0900

    test: Winners 기본 테스트 케이스 추가

---
+++ src/test/java/racingcar/WinnersTest.java
@@ -0,0 +1,24 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinnersTest {
+
+    @Test
+    @DisplayName("우승자를 정상적으로 추려낸다")
+    void from() {
+        Winners winners = Winners.from(
+            List.of(
+                new Car("a", 1),
+                new Car("b", 1),
+                new Car("c", 0)
+            )
+        );
+        assertThat(winners.names())
+            .containsExactly("a", "b");
+    }
+}
