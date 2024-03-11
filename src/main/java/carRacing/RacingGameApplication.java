e0505b08c7ea670c4145a03e49b315deb83c8985
Shinny <siniya979@gmail.com>
2024-03-11 22:45:58 +0900

    Refactor: 클래스명 변경
    접미사 Impl 대신, 클래스의 목적성이 명확히 들어나도록 클래스명을 변경함

--- src/main/java/carRacing/RacingGameApplication.java
+++ src/main/java/carRacing/RacingGameApplication.java
@@ -3,17 +3,16 @@ package carRacing;
 import carRacing.car.Cars;
 import carRacing.common.InputHandler;
 import carRacing.common.RandomGenerator;
-import carRacing.common.RandomGeneratorImpl;
+import carRacing.common.MovementRandomGenerator;
 import carRacing.race.RacingGame;
 import carRacing.race.RacingGameUI;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class RacingGameApplication {
     public static void main(String[] args) throws Exception {
 
-        RandomGenerator randomGenerator = RandomGeneratorImpl.of();
+        RandomGenerator randomGenerator = MovementRandomGenerator.of();
         Cars cars = Cars.of(new ArrayList<>());
         RacingGameUI ui = new RacingGameUI();
         RacingGame racingGame = RacingGame.of(randomGenerator,cars,ui);
--- src/main/java/carRacing/common/MovementRandomGenerator.java
+++ src/main/java/carRacing/common/MovementRandomGenerator.java
@@ -2,14 +2,14 @@ package carRacing.common;
 
 import java.util.Random;
 
-public class RandomGeneratorImpl implements RandomGenerator{
+public class MovementRandomGenerator implements RandomGenerator{
     private static final Random random = new Random();
     private static final int RANDOM_RANGE = 10;
 
-    protected RandomGeneratorImpl(){}
+    protected MovementRandomGenerator(){}
 
-    public static RandomGeneratorImpl of(){
-        return new RandomGeneratorImpl();
+    public static MovementRandomGenerator of(){
+        return new MovementRandomGenerator();
     }
 
     @Override
