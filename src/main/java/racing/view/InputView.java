2f3b25f335b73ca80020ca5ee0d7b01282e9a8b6
astrid-dm <himax_01@naver.com>
2024-03-11 20:45:29 +0900

    feat : 입/출력 클래스 수정

--- src/main/java/racing/view/InputView.java
+++ src/main/java/racing/view/InputView.java
@@ -1,39 +1,76 @@
 package racing.view;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
     private Scanner scanner = new Scanner(System.in);
+    private static final String VALID_CHARACTER = "^[a-zA-Z,]*$";
+    private static final String DELIMITER = ",";
+
+    public String[] inputStrings(String question) {
+        outputQuestion(question);
+        String[] inputStrings = getStringsInput();
+        return inputStrings;
+    }
+
+    private void outputQuestion(String question) {
+        System.out.println(question);
+    }
+
+    private String[] getStringsInput() {
+        String inputString = scanner.nextLine();
+
+        while (!isValidInputString(inputString) || !isValidNameLength(inputString)) {
+            System.out.println("공백 없이 쉼표를 기준으로 나누어, 5글자 이하의 자동차의 이름을 입력해주세요.\n(ex: nicky,kim)");
+            inputString = scanner.nextLine();
+        }
+
+        return separateNames(inputString);
+    }
+
+    private boolean isValidInputString(String inputString) {
+        return inputString.matches(VALID_CHARACTER);
+    }
+
+    private boolean isValidNameLength(String inputString) {
+        String[] names = separateNames(inputString);
+
+        return Arrays.stream(names).allMatch(this::isShorterThanSix);
+    }
+
+    private String[] separateNames(String inputString) {
+        String[] names = inputString.split(DELIMITER);
+        return names;
+    }
+
+    private boolean isShorterThanSix(String value) {
+        return !value.isBlank() && value.length() <= 5;
+    }
 
     public int inputNumber(String question) {
-        System.out.println(question);
+        outputQuestion(question);
         int number = getPositiveNumberInput();
         return number;
     }
 
     private int getPositiveNumberInput() {
-        int number;
+        String inputString = scanner.next();
 
-        while (true) {
-            acceptOnlyNumber();
-            number = scanner.nextInt();
-            if (isPositive(number)) {
-                break;
-            }
-            System.out.println("양수만 입력해 주시길 바랍니다.");
+        while (!isPositiveNumber(inputString)) {
+            System.out.println("0을 초과하는 숫자만 입력해 주시길 바랍니다.\n(ex: 5)");
+            inputString = scanner.next();
         }
 
-        return number;
+        return Integer.parseInt(inputString);
     }
 
-    private void acceptOnlyNumber() {
-        while (!scanner.hasNextInt()) {
-            System.out.println("0을 초과하는 숫자만 입력해 주시길 바랍니다.");
-            scanner.next();
+    private boolean isPositiveNumber(String input) {
+        try {
+            int number = Integer.parseInt(input);
+            return number > 0;
+        } catch (NumberFormatException e) {
+            return false;
         }
-    }
-
-    private boolean isPositive(int number) {
-        return number > 0;
     }
 }
--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -3,6 +3,7 @@ package racing.view;
 import racing.model.Car;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
     private static final String NEW_LINE = System.lineSeparator();
@@ -14,12 +15,16 @@ public class ResultView {
             appendNewLine();
         }
 
+        record(getWinnerNames(cars) + "(이)가 최종 우승했습니다.");
+
         return getResult();
     }
 
     private void getCarPosition(List<Car> cars, int index) {
         for (int i = 0; i < cars.size(); i++) {
-            record(cars.get(i).getPosition(index));
+            String carName = cars.get(i).getName();
+            String carPosition = cars.get(i).getPosition(index);
+            record(carName + " : " + carPosition);
         }
     }
 
@@ -28,22 +33,42 @@ public class ResultView {
         appendResultHeader();
     }
 
-    public void appendResultHeader() {
+    private void appendResultHeader() {
         result.append("실행 결과");
         appendNewLine();
     }
 
-    public void appendNewLine() {
+    private void appendNewLine() {
         result.append(NEW_LINE);
     }
 
-    public void record(String position) {
-        result.append(position);
+    private void record(String history) {
+        result.append(history);
         appendNewLine();
     }
 
+    private String getWinnerNames(List<Car> cars) {
+        List<Car> topScores = getTheHighestScore(cars);
+
+        String winnerNames = topScores.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+
+        return winnerNames;
+    }
+
+    private List<Car> getTheHighestScore(List<Car> cars) {
+        int maxScore = cars.stream().mapToInt(Car::getScore).max().orElse(0);
+
+        List<Car> topScores = cars.stream()
+                .filter(car -> car.getScore() == maxScore)
+                .collect(Collectors.toList());
+
+        return topScores;
+    }
+
     /**
-     * Test Code 작성을 위한 getter() 메서드
+     * Test Code 작성을 위한 pulbic getter() 메서드
      */
     public String getResult() {
         return result.toString();
