507fc27020eaefee5096cf62efb5689a5e9a1e96
jaehyeoklee <leejh9049@gmail.com>
2024-03-11 19:42:20 +0900

    [refactor] 패키지 명 수정

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,4 +1,4 @@
-package StringAddCalculator;
+package calculator;
 
 import java.util.Arrays;
 import java.util.regex.Matcher;
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,4 +1,4 @@
-package StringAddCalculator;
+package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
