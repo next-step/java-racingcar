d3db4fed6be4a77e4109ae9ffc03f51e558a2d25
유지웅 <poujis0@gmail.com>
2024-03-11 21:42:24 +0900

    [feat] MoveStrategy 입력값이 0미만 10이상일 때 Exception 발생

--- src/main/java/racingCar/MoveStrategy.java
+++ src/main/java/racingCar/MoveStrategy.java
@@ -2,7 +2,15 @@ package racingCar;
 
 public class MoveStrategy {
     private static final int DISTANCE_STANDARD = 4;
+    private static final int MAX_DISTANCE = 9;
+    private static final int MIN_DISTANCE = 0;
     public boolean isMovable(int distance){
+        validDistance(distance);
         return distance >= DISTANCE_STANDARD;
+    }
+    private void validDistance(int distance){
+        if(distance > MAX_DISTANCE || MIN_DISTANCE > distance){
+            throw new IllegalArgumentException("입력범위를 벗어났습니다.");
+        }
     }
 }
--- src/test/java/racingCar/MoveStrategyTest.java
+++ src/test/java/racingCar/MoveStrategyTest.java
@@ -1,10 +1,14 @@
 package racingCar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Random;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class MoveStrategyTest {
 
@@ -12,7 +16,7 @@ public class MoveStrategyTest {
     private MoveStrategy strategy;
 
     @BeforeEach
-    void init(){
+    void init() {
         strategy = new MoveStrategy();
     }
 
@@ -24,6 +28,13 @@ public class MoveStrategyTest {
     @RepeatedTest(value = 10)
     void Random값이_4미만이면_false를_반환한다() {
         assertThat(strategy.isMovable(createRandomDistance(0, 3))).isEqualTo(false);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -3, 10, 11, 12})
+    void 입력값이_0미만10이상일때_Error(int input){
+        assertThatThrownBy(() -> strategy.isMovable(input))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     private int createRandomDistance(int min, int max) {
