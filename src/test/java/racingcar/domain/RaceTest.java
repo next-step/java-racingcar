152d6ce552f6fbbad846f95b84fa7b291348137c
kylekim2123 <kyle2123@naver.com>
2024-03-12 04:35:15 +0900

    [STEP 3] test: Race 클래스의 정적 팩토리 메서드에 대해 정상 케이스 테스트 코드 작성

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -1,19 +1,26 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import racingcar.domain.Race;
-
 class RaceTest {
+
+    @Test
+    @DisplayName("양의 정수의 자동차 대수와 레이싱 시도 횟수를 전달하면 정상적으로 Race 객체를 생성한다.")
+    void setUp_PositiveCarCountAndPlayingCount_Race() {
+        assertThat(Race.setUp(1, 1))
+                .isNotNull();
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
     @DisplayName("Race 객체 생성 시, 0 혹은 음의 정수의 자동차 대수를 전달하면 IllegalArgumentException을 던진다.")
-    void race_NegativeOrZeroCarCount_IllegalArgumentException(final int negativeOrZeroCarCount) {
+    void setUp_NegativeOrZeroCarCount_IllegalArgumentException(final int negativeOrZeroCarCount) {
         assertThatThrownBy(() -> Race.setUp(negativeOrZeroCarCount, 1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차의 대수는 자연수만 가능합니다.");
@@ -22,7 +29,7 @@ class RaceTest {
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
     @DisplayName("Race 객체 생성 시, 0 혹은 음의 정수의 레이싱 시도 횟수를 전달하면 IllegalArgumentException을 던진다.")
-    void race_NegativeOrZeroPlayingCount_IllegalArgumentException(final int negativeOrZeroPlayingCount) {
+    void setUp_NegativeOrZeroPlayingCount_IllegalArgumentException(final int negativeOrZeroPlayingCount) {
         assertThatThrownBy(() -> Race.setUp(1, negativeOrZeroPlayingCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("레이싱 시도 횟수는 자연수만 가능합니다.");
