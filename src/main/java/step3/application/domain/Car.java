fb02c379918e83ed72841efbd8a5467a32ad7fc0
jy.lee <ljyda214@gmail.com>
2024-03-12 08:59:37 +0900

    FEAT : 차량의 이동을 위한 move 메소드 구현

--- src/main/java/step3/application/domain/Car.java
+++ src/main/java/step3/application/domain/Car.java
@@ -1,10 +1,29 @@
 package step3.application.domain;
 
+import error.ErrorMessage;
+
 public class Car {
+
     private final int id;
-    private int position = 0;
+    private int location;
+    public static final int INIT_POSITION = 0;
 
     public Car(int id) {
+        validateId(id);
         this.id = id;
+        this.location = INIT_POSITION;
+    }
+
+    private void validateId(int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException(ErrorMessage.ERR_INVALID_NUMBER.print());
+        }
+    }
+
+    public int move(boolean isMovable) {
+        if (isMovable) {
+            this.location += 1;
+        }
+        return this.location;
     }
 }
