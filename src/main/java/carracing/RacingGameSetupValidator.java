57488dc34687d049ab05ba4b74d7e0b060d5fc84
hong yuseok <jericoh@naver.com>
2024-03-12 01:12:23 +0900

    feat: isValidLengthString() 구현 - isValidLengthString() 구현 - RacingGameSetupValidator에서 메서드 파라미터에 final 키워드 추가

--- src/main/java/carracing/RacingGameSetupValidator.java
+++ src/main/java/carracing/RacingGameSetupValidator.java
@@ -11,11 +11,15 @@ public class RacingGameSetupValidator {
 
     }
 
-    public static boolean isNonBlank(String input) {
+    public static boolean isNonBlank(final String input) {
         return !isNull(input) && !input.isBlank();
     }
 
-    public static boolean isPositiveInteger(String input) {
+    public static boolean isPositiveInteger(final String input) {
         return isNonBlank(input) && PATTERN_FOR_POSITIVE_INTEGER.matcher(input).find();
+    }
+
+    public static boolean isValidLengthString(final String input, final int maxLength) {
+        return isNonBlank(input) && input.length() <= maxLength;
     }
 }
