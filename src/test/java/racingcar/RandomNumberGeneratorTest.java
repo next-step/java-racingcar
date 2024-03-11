4080678d1aa5320913058eebbfeb862847ecf94b
kylekim2123 <kyle2123@naver.com>
2024-03-11 17:00:36 +0900

    [STEP 3] test: 0이상 9이하의 난수를 정상적으로 생성하는지 확인하는 테스트 코드 추가

---
+++ src/test/java/racingcar/RandomNumberGeneratorTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.RandomNumberGenerator.generateRandomNumberBetweenZeroAndNine;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RandomNumberGeneratorTest {
+
+    @Test
+    @DisplayName("0이상 9이하의 난수를 정상적으로 생성한다.")
+    void generate_RandomNumber_BetweenZeroAndNine() {
+        assertThat(generateRandomNumberBetweenZeroAndNine()).isBetween(0, 9);
+    }
+}
