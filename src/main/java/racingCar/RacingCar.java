9ca4504393a95142763387832f786eac58914dcb
유지웅 <poujis0@gmail.com>
2024-03-11 21:44:28 +0900

    [feat] RacingCar 입력값이 0미만 10이상일 때 Exception 발생

--- src/main/java/racingCar/RacingCar.java
+++ src/main/java/racingCar/RacingCar.java
@@ -1,5 +1,8 @@
 package racingCar;
 
+import static racingCar.MoveStrategy.MAX_DISTANCE;
+import static racingCar.MoveStrategy.MIN_DISTANCE;
+
 public class RacingCar {
 
     private static final int START_LOCATION = 0;
@@ -16,8 +19,14 @@ public class RacingCar {
     }
 
     public void move(int distance) {
+        validDistance(distance);
         if (strategy.isMovable(distance)) {
             this.location++;
+        }
+    }
+    private void validDistance(int distance){
+        if(distance > MAX_DISTANCE  || MIN_DISTANCE > distance){
+            throw new IllegalArgumentException("입력범위를 벗어났습니다.");
         }
     }
 }
--- src/test/java/racingCar/RacingCarTest.java
+++ src/test/java/racingCar/RacingCarTest.java
@@ -2,6 +2,7 @@ package racingCar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -57,5 +58,12 @@ public class RacingCarTest {
             car.move(input);
             assertThat(car.findCurrentLocation()).isEqualTo(0);
         });
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -3, 10, 11, 12})
+    void 입력값이_0미만10이상일때_Error(int input){
+        assertThatThrownBy(() -> car.move(input))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
