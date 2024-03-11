55ca8c5a11afaa112e62407a6efafe1463c67d7d
leeedohyun <ldh19738@gmail.com>
2024-03-11 09:41:50 +0900

    test: 이름이 비어있는 경우 예외 테스트

--- src/test/java/model/NameTest.java
+++ src/test/java/model/NameTest.java
@@ -1,6 +1,7 @@
 package model;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,6 +10,12 @@ class NameTest {
     @ParameterizedTest
     @ValueSource(strings = {"ponito", "nextstep"})
     void 자동차_이름이_5자를_초과하면_예외가_발생한다(final String name) {
-        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> new Name(name));
+        assertThatIllegalArgumentException().isThrownBy(() -> new Name(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 이름이_null이거나_빈_문자열인_경우_예외가_발생한다(final String name) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Name(name));
     }
 }
