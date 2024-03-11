664519193bf342f338bd8bbbcb6050dfe4d6bb6b
hong yuseok <jericoh@naver.com>
2024-03-12 00:27:00 +0900

    test: isNonBlank()에 대한 테스트 코드 작성

--- src/test/java/carracing/RacingGameSetupValidatorTest.java
+++ src/test/java/carracing/RacingGameSetupValidatorTest.java
@@ -3,10 +3,18 @@ package carracing;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingGameSetupValidatorTest {
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("isNonBlank(): null 또는 \"\"이 입력된 경우 false를 반환한다.")
+    void testIsNonBlank(String input) {
+        assertThat(RacingGameSetupValidator.isNonBlank(input)).isEqualTo(false);
+    }
+
     @ParameterizedTest
     @CsvSource(value = {"null:false", "'':false", "'   ':false", "abcd:false", "-1:false", "0:false", "1.123:false", "1,a12:false", "1:true", "2:true", "10:true"}, nullValues = {"null"}, delimiter = ':')
     @DisplayName("isPositiveInteger(): 양의 정수를 입력한 경우 true 그렇지 않은 경우 false를 반환한다.")
