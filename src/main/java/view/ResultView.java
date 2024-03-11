012046ea870a0c8526e482b522f1faa7e549228c
jente-myoungsoo <86216686+myoungsoo-jente@users.noreply.github.com>
2024-03-11 19:11:23 +0900

    refactor(ResultView): 매직 리터럴 제거

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class ResultView {
 
+    private final static String END_OF_LINE = "\n";
+
     public static void start() {
         System.out.println("실행 결과");
     }
@@ -12,7 +14,7 @@ public class ResultView {
     public static void showProcess(List<Car> cars) {
         StringBuilder sb = new StringBuilder();
         for (Car car : cars) {
-            sb.append(car).append("\n");
+            sb.append(car).append(END_OF_LINE);
         }
         System.out.println(sb);
     }
