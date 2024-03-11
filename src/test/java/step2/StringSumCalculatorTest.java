0055228b06e084349aa45a98581fbfde102c80ea
develiberta <develiberta@gmail.com>
2024-03-11 23:49:12 +0900

    step2: 문자열 덧셈 계산기 (#5366)
    Co-authored-by: develiberta <develiberta@github.com>

--- src/test/java/step2/StringSumCalculatorTest.java
+++ src/test/java/step2/StringSumCalculatorTest.java
@@ -10,40 +10,40 @@ public class StringSumCalculatorTest {
 
     @Test
     public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
+        int result = StringSumCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
-        result = StringAddCalculator.splitAndSum("");
+        result = StringSumCalculator.splitAndSum("");
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     public void splitAndSum_숫자하나() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1");
+        int result = StringSumCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2");
+        int result = StringSumCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
+        int result = StringSumCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_custom_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        int result = StringSumCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_negative() throws Exception {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+        assertThatThrownBy(() -> StringSumCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
