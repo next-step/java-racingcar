d0004d14baf8679f6de14117d4e9bfb67801360c
namhyeop <skaguq@gmail.com>
2024-03-12 01:24:14 +0900

    feat : [step 1-3] 랜덤번호 생성기 구현

---
+++ src/main/java/carRace/RandomNumberGenerator.java
@@ -0,0 +1,16 @@
+package carRace;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+
+    private static final Random randomNumberGenerator = new Random();
+    private static final int RANDOM_NUMBER_BOUND = 10;
+
+    private RandomNumberGenerator() {
+    }
+
+    public static int createRandomNumber(){
+        return randomNumberGenerator.nextInt(RANDOM_NUMBER_BOUND);
+    }
+}
