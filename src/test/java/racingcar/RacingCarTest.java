d24b01d9a13b17d18141ffebb016e20937d4ecdd
Sangmin <sm9171@nate.com>
2024-03-11 21:26:54 +0900

    test: 요구사항 수정

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,29 +2,39 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Random;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RacingCarTest {
-    private static final int ADVANCE_CONDITION_NUMBER = 4;
+	@DisplayName("값이 4 이상일 경우 전진한다.")
+	@ParameterizedTest
+	@ValueSource(ints = {4, 5, 6, 7, 8, 9})
+	public void advancePositionFourOver(int number) {
+		//given
+		Car car = new Car();
 
-    @DisplayName("랜덤값이 4 이상일 경우 전진한다.")
-    @Test
-    public void advancePositionFourOver() {
-        //given
-        Car car = new Car();
-        int random = new Random().nextInt(10);
+		//when
+		car.move(number);
 
-        //when
-        car.move(random);
+		//then
+		assertThat(car.getPosition()).isEqualTo(2);
+	}
 
-        //then
-        if(random >= ADVANCE_CONDITION_NUMBER) {
-            assertThat(car.getPosition()).isEqualTo(1);
-            return;
-        }
-        assertThat(car.getPosition()).isEqualTo(0);
-    }
+	@DisplayName("값이 4 미만일때는 움직이지 않는다.")
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	public void noAdvancePositionFourUnder(int number) {
+		//given
+		Car car = new Car();
+
+		//when
+		car.move(number);
+
+		//then
+		assertThat(car.getPosition()).isEqualTo(1);
+	}
 }
