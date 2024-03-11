d745f46670b2505c64d92fc6306fcea868b166b3
dongock <okdong23@naver.com>
2024-03-11 22:32:05 +0900

    refactor: [car] 상수 접근제어자 변경

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,8 +1,9 @@
 package racingcar.model;
 
 public class Car {
-    public static final int MOVE_STANDARD = 4;
-    public static final int MOVE_DISTANCE = 1;
+    private static final int MOVE_STANDARD = 4;
+    private static final int MOVE_DISTANCE = 1;
+
     private int distance = 0;
 
     public int getDistance() {
