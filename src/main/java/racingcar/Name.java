8613bed669ec82476ff9aac8772fe7a70e6296e0
deepredk <deepred.kim@gmail.com>
2024-03-11 21:19:21 +0900

    feat: Name에 equals, hashCode 구현

--- src/main/java/racingcar/Name.java
+++ src/main/java/racingcar/Name.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.Objects;
+
 public class Name {
 
     private final String value;
@@ -25,5 +27,22 @@ public class Name {
 
     public String value() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
