b4948d2fdbcd64da9c136bd7d3f0aa27e64e92f4
westkitekim <westkite418@gmail.com>
2024-03-11 21:46:59 +0900

    refactor: 테스트코드 파라미터 어노테이션 활용

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,19 +14,20 @@ class StringAddCalculatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다")
-    public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
+    public void splitAndSum_null_또는_빈문자(String text) {
+        int result = StringAddCalculator.splitAndSum(text);
         assertThat(result).isEqualTo(0);
 
         result = StringAddCalculator.splitAndSum("");
         assertThat(result).isEqualTo(0);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:2", "9:9"}, delimiter = ':')
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
-    public void splitAndSum_숫자하나() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1");
-        assertThat(result).isEqualTo(1);
+    public void splitAndSum_숫자하나(String input, int expected) throws Exception {
+        int actual = StringAddCalculator.splitAndSum(input);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
