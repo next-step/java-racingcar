5bb3c4da5e66b284603348b9828d8f88812e2d22
Sangmin <sm9171@nate.com>
2024-03-11 20:05:38 +0900

    refactor: CUSTOM_SEPARATOR_PATTERN 상수화

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Calculator {
 
-	private static final String CUSTOM_SEPARATOR = "//(.)\n(.*)";
+	private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("//(.)\n(.*)");
 	private static final int CUSTOM_DELIMITER_INDEX = 1;
 	private static final int NUMBERS_INDEX = 2;
 
@@ -24,11 +24,7 @@ public class Calculator {
 	}
 
 	private static Matcher getMatcher(String input) {
-		return patternCompile().matcher(input);
-	}
-
-	private static Pattern patternCompile() {
-		return Pattern.compile(CUSTOM_SEPARATOR);
+		return CUSTOM_SEPARATOR_PATTERN.matcher(input);
 	}
 
 	private static int calculateAdditionWithCustomSign(final Matcher matcher) {
