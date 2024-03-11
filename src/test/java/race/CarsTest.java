71c77574c6e5942c6ae449e0ac8f554c6c5146ab
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-11 16:50:04 +0900

    test 자동차 3개를 1회 전진하면 각각 -를 반환하는 테스트를 작성해라

--- src/test/java/race/CarsTest.java
+++ src/test/java/race/CarsTest.java
@@ -7,6 +7,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarsTest {
 
     @Test
+    void three_cars() {
+        Cars cars = new Cars(3);
+        String mileage1 = cars.forward(1, 1);
+        String mileage2 = cars.forward(2, 1);
+        String mileage3 = cars.forward(3, 1);
+
+        assertThat(mileage1).isEqualTo("-");
+        assertThat(mileage2).isEqualTo("-");
+        assertThat(mileage2).isEqualTo("-");
+    }
+
+    @Test
     void two_cars() {
         Cars cars = new Cars(2);
         String mileage1 = cars.forward(1, 1);
