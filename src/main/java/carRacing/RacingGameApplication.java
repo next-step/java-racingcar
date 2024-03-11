9ed92b98d036b2f4b08ff70ec0d20e2489ed2ff4
Shinny <siniya979@gmail.com>
2024-03-11 22:20:02 +0900

    Refactor: Try-finally 로 묶어줌
    메인에서 try-finally 로 묶어 Scanner 가 어플리케이션 종료시에 닫힐 수 있도록 하였다.

--- src/main/java/carRacing/RacingGameApplication.java
+++ src/main/java/carRacing/RacingGameApplication.java
@@ -1,6 +1,7 @@
 package carRacing;
 
 import carRacing.car.Cars;
+import carRacing.common.InputHandler;
 import carRacing.common.RandomGenerator;
 import carRacing.common.RandomGeneratorImpl;
 import carRacing.race.RacingGame;
@@ -12,14 +13,20 @@ import java.util.Scanner;
 public class RacingGameApplication {
     public static void main(String[] args) throws Exception {
 
-        RandomGenerator randomGenerator = new RandomGeneratorImpl();
+        RandomGenerator randomGenerator = RandomGeneratorImpl.of();
         Cars cars = Cars.of(new ArrayList<>());
         RacingGameUI ui = new RacingGameUI();
         RacingGame racingGame = RacingGame.of(randomGenerator,cars,ui);
 
-        String carNames = ui.askCarsNames();
-        int trialCount = ui.askTrialCount();
-        racingGame.startRacing(carNames, trialCount);
+        InputHandler inputHandler = InputHandler.of();
+
+        try{
+            String carNames = inputHandler.askCarsNames();
+            int trialCount = inputHandler.askTrialCount();
+            racingGame.startRacing(carNames, trialCount);
+        }finally {
+            inputHandler.closeScanner();
+        }
 
     }
 }
