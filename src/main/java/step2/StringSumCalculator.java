2f68440b6a27cf2e7e041c74f7c331dd7a9bd1ce
develiberta <develiberta@github.com>
2024-03-11 21:59:36 +0900

    step2: 문자열 덧셈 계산기

--- src/main/java/step2/StringSumCalculator.java
+++ src/main/java/step2/StringSumCalculator.java
@@ -3,7 +3,7 @@ package step2;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class StringAddCalculator {
+public class StringSumCalculator {
 
     private static final String DELIMITER_COMMA = ",";
     private static final String DELIMITER_COLON = ":";
@@ -11,9 +11,7 @@ public class StringAddCalculator {
     private static final String DELIMITER = DELIMITER_COMMA + SPLIT_OR + DELIMITER_COLON;
 
     public static int splitAndSum(String string) {
-        if (nullOrEmpty(string)) {
-            return 0;
-        }
+        if (nullOrEmpty(string)) return 0;
         String[] stringArray = splitByDelimiter(string);
         int[] intArray = intArrayFromStringArray(stringArray);
         return intArraySum(intArray);
@@ -21,34 +19,25 @@ public class StringAddCalculator {
 
     private static String[] splitByDelimiter(String string) {
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(string);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
-        }
+        if (m.find()) return m.group(2).split(m.group(1));
         return string.split(DELIMITER);
     }
 
     private static int intArraySum(int[] intArray) {
         int sum = 0;
-        for (int i = 0; i < intArray.length; i++) {
-            sum += intArray[i];
-        }
+        for (int j : intArray) sum += j;
         return sum;
     }
 
     private static int[] intArrayFromStringArray(String[] stringArray) {
         int[] intArray = new int[stringArray.length];
-        for (int i = 0; i < stringArray.length; i++) {
-            intArray[i] = intFromString(stringArray[i]);
-        }
+        for (int i = 0; i < stringArray.length; i++) intArray[i] = intFromString(stringArray[i]);
         return intArray;
     }
 
     private static int intFromString(String string) {
         int result = Integer.parseInt(string);
-        if (positive(result) == false) {
-            throw new RuntimeException();
-        }
+        if (!positive(result)) throw new RuntimeException();
         return result;
     }
 
