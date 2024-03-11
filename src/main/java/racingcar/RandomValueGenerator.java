f1d7382f6127b2aecc27373e33ca336fb61e6b23
hellonayeon <hellonykwon@gmail.com>
2024-03-12 00:09:20 +0900

    Refactor: (Feedback) 숫자 리터럴 상수 처리

--- src/main/java/racingcar/RandomValueGenerator.java
+++ src/main/java/racingcar/RandomValueGenerator.java
@@ -5,12 +5,13 @@ import java.util.Random;
 public class RandomValueGenerator {
 
     private static final Random RANDOM = new Random();
+    private static final int RANDOM_BOUND = 10;
 
     private RandomValueGenerator() {
 
     }
 
     public static int generate() {
-        return RANDOM.nextInt(10);
+        return RANDOM.nextInt(RANDOM_BOUND);
     }
 }
