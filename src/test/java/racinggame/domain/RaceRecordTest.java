494f8429b35dae2c9e64eae5db8d07b2f8b8b177
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-12 02:06:29 +0900

    test: getWinner 테스트 코드 추가

---
+++ src/test/java/racinggame/domain/RaceRecordTest.java
@@ -0,0 +1,39 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RaceRecordTest {
+
+    private List<Car> cars = new ArrayList<>();
+
+    @BeforeEach
+    void init() {
+        cars.add(new Car(new Position(1),"one"));
+        cars.add(new Car(new Position(2),"two"));
+        cars.add(new Car(new Position(3),"three"));
+        cars.add(new Car(new Position(4),"winner1"));
+        cars.add(new Car(new Position(4),"winner2"));
+
+    }
+
+    @Test
+    void getCars() {
+
+    }
+
+    @Test
+    void getWinners() {
+        RaceRecord givenRecord = new RaceRecord(cars);
+
+        List<Car> result = givenRecord.getWinners();
+
+        assertThat(result).contains(cars.get(4), cars.get(3));
+    }
+}
