73a082b15d0fa22f6c0ad5f2fc048211668ec203
vieworksjosunhee@gmail.com <vieworksjosunhee@gmail.com>
2024-03-11 10:11:50 +0900

    feat: [2단계 - 문자열 덧셈 계산기] 요구사항 7 만족

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -12,23 +12,31 @@ public class Calculator {
         int result = 0;
         String[] strings;
 
-        if ( (str.equals("")) | (str.equals(null) ) ){  // 요구사항 1
-            return 0;
-        } else if (str.startsWith("//"))    // 요구사항 6
-        {
-            str = str.replace("//", "");
-            str = str.replace("\\n", "");
-            String customDelimiter = String.valueOf(str.charAt(0));
-            // str = str.replace(customDelimiter, "");
-            strings = str.split( customDelimiter );
-            strings[0] = "0";
-        } else {    // 요구사항 2, 3, 4, 5
-            strings = str.split(",|:");
+        try {
+            if ((str.equals("")) | (str.equals(null))) {  // 요구사항 1
+                return 0;
+            } else if (str.startsWith("//"))    // 요구사항 6
+            {
+                str = str.replace("//", "");
+                str = str.replace("\\n", "");
+                String customDelimiter = String.valueOf(str.charAt(0));
+                strings = str.split(customDelimiter);
+                strings[0] = "0";
+            } else {    // 요구사항 2, 3, 4, 5
+                strings = str.split(",|:");
+            }
+        } catch (Exception exception) {
+            throw new RuntimeException("RuntimeException");
         }
 
         int[] ints = new int[strings.length];
-        for (int i = 0; i < strings.length; i++ ) {
-            ints[i] = Integer.valueOf(strings[i]);
+        try {
+            for (int i = 0; i < strings.length; i++) {
+                ints[i] = Integer.valueOf(strings[i]);
+            }
+        } catch (Exception exception)
+        {
+            throw new RuntimeException("RuntimeException");
         }
         for (int i = 0; i < ints.length; i++ ) {
             ThrowRuntimeExceptionIfNegativeValue(ints[i]);
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -48,7 +48,7 @@ public class CalculatorTest {
     @DisplayName("(7) 문자열 계산기에 숫자 이외의 값을 전달하는 경우 RuntimeException 예외를 throw")
     @ValueSource(strings = {"a", "//;\\n", "///;\\n"})
     public void noNumber(String str){
-        Assertions.assertThatThrownBy(() -> {Calculator.sum(str); }).isEqualTo(RuntimeException.class);
+        Assertions.assertThatThrownBy(() -> {Calculator.sum(str); }).hasMessage("RuntimeException");
     }
 
     @Test
