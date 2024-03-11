0926d227bd774f69d58847fc7d946c4021206a2c
Shinny <siniya979@gmail.com>
2024-03-11 22:20:53 +0900

    Refactor: input,output 분리
    기존 input 메서드 삭제

--- src/main/java/carRacing/race/RacingGameUI.java
+++ src/main/java/carRacing/race/RacingGameUI.java
@@ -7,19 +7,6 @@ import java.util.Scanner;
 public class RacingGameUI {
     private static final String TRACK_SYMBOL = "-";
 
-    public String askCarsNames(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        scanner.close();
-        return scanner.nextLine();
-    }
-
-    public int askTrialCount(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        scanner.close();
-        return scanner.nextInt();
-    }
     public void printTrack(Cars cars, int trialCount) {
         System.out.println((trialCount + 1) + "회차 이동");
         cars.forEach(car -> {
