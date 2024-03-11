10e0d027942bba3d896633282216c60dbaab7bc0
dongock <okdong23@naver.com>
2024-03-11 22:55:03 +0900

    refactor:[RandomNumberStrategy] 상수 추가

--- src/main/java/racingcar/model/RandomNumberStrategy.java
+++ src/main/java/racingcar/model/RandomNumberStrategy.java
@@ -4,9 +4,11 @@ import java.util.Random;
 
 public class RandomNumberStrategy implements MoveStrategy {
 
+    private static final int MAX_NUMBER = 10;
+
     @Override
     public int getMoveNumber() {
         Random random = new Random();
-        return new Random().nextInt(10);
+        return new Random().nextInt(MAX_NUMBER);
     }
 }
