efcb02bbb64c14dcc0b2a5e664ae23fbb8ac43b5
유지웅 <poujis0@gmail.com>
2024-03-11 21:26:03 +0900

    [feat] RacingCar 4~9 사이의 숫자 입력 시 위치가 변경되는 move 로직 개발

--- src/main/java/racingCar/RacingCar.java
+++ src/main/java/racingCar/RacingCar.java
@@ -3,13 +3,21 @@ package racingCar;
 public class RacingCar {
 
     private static final int START_LOCATION = 0;
+    private final MoveStrategy strategy;
     int location;
 
-    public RacingCar() {
+    public RacingCar(MoveStrategy strategy) {
+        this.strategy = strategy;
         this.location = START_LOCATION;
     }
 
     public int findCurrentLocation() {
         return this.location;
+    }
+
+    public void move(int distance) {
+        if (strategy.isMovable(distance)) {
+            this.location++;
+        }
     }
 }
--- src/test/java/racingCar/RacingCarTest.java
+++ src/test/java/racingCar/RacingCarTest.java
@@ -2,13 +2,37 @@ package racingCar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RacingCarTest {
+
+    private final MoveStrategy strategy = new MoveStrategy();
+    private RacingCar car;
+
+    @BeforeEach
+    void init() {
+        car = new RacingCar(strategy);
+    }
+
     @Test
-    void 생성된_RacingCar의_초기위치는_0(){
-        RacingCar car = new RacingCar();
+    void 생성된_RacingCar의_초기위치는_0() {
         assertThat(car.findCurrentLocation()).isEqualTo(0);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 한대의_RacingCar_이동_성공(int input) {
+        car.move(input);
+        assertThat(car.findCurrentLocation()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 한대의_RacingCar_이동_실패(int input) {
+        car.move(input);
+        assertThat(car.findCurrentLocation()).isEqualTo(0);
+    }
 }
