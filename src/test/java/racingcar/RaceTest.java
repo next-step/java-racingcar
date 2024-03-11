142e9c9ce655643ffc9beca813b0b94d91348428
kylekim2123 <kyle2123@naver.com>
2024-03-12 03:55:19 +0900

    [STEP 3] feat: 자동차 대수와 시도 횟수를 받아 실제 경주를 진행하는 비즈니스 로직 추가

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -6,22 +6,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class RacingTest {
+class RaceTest {
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
-    @DisplayName("race 메서드에 0 혹은 음의 정수의 자동차 대수를 전달하면 IllegalArgumentException을 던진다.")
+    @DisplayName("Race 객체 생성 시, 0 혹은 음의 정수의 자동차 대수를 전달하면 IllegalArgumentException을 던진다.")
     void race_NegativeOrZeroCarCount_IllegalArgumentException(final int negativeOrZeroCarCount) {
-        assertThatThrownBy(() -> Racing.race(negativeOrZeroCarCount, 1))
+        assertThatThrownBy(() -> Race.setUp(negativeOrZeroCarCount, 1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차의 대수는 자연수만 가능합니다.");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
-    @DisplayName("race 메서드에 0 혹은 음의 정수의 레이싱 시도 횟수를 전달하면 IllegalArgumentException을 던진다.")
+    @DisplayName("Race 객체 생성 시, 0 혹은 음의 정수의 레이싱 시도 횟수를 전달하면 IllegalArgumentException을 던진다.")
     void race_NegativeOrZeroPlayingCount_IllegalArgumentException(final int negativeOrZeroPlayingCount) {
-        assertThatThrownBy(() -> Racing.race(1, negativeOrZeroPlayingCount))
+        assertThatThrownBy(() -> Race.setUp(1, negativeOrZeroPlayingCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("레이싱 시도 횟수는 자연수만 가능합니다.");
     }
