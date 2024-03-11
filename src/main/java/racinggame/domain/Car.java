715b12fda84c257a54930607e1c56f5dba87f6d3
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-12 02:07:01 +0900

    refactor: getWinner 테스트 코드를 위한 equals 구현

--- src/main/java/racinggame/domain/Car.java
+++ src/main/java/racinggame/domain/Car.java
@@ -1,5 +1,7 @@
 package racinggame.domain;
 
+import java.util.Objects;
+
 public class Car {
 
     private Position position;
@@ -30,5 +32,18 @@ public class Car {
 
     public Car copy() {
         return new Car(position, name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(position, car.position) && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, name);
     }
 }
