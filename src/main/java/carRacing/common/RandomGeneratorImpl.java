efc1a144b822a93511c146f3e0db691f1de09300
Shinny <siniya979@gmail.com>
2024-03-11 22:21:36 +0900

    Refactor: 생성자 변경
    기본 생정자의 접근제어자를 강화하고 메서드를 통해서 생성할 수 있도록 변경

--- src/main/java/carRacing/common/RandomGeneratorImpl.java
+++ src/main/java/carRacing/common/RandomGeneratorImpl.java
@@ -6,6 +6,12 @@ public class RandomGeneratorImpl implements RandomGenerator{
     private static final Random random = new Random();
     private static final int RANDOM_RANGE = 10;
 
+    protected RandomGeneratorImpl(){}
+
+    public static RandomGeneratorImpl of(){
+        return new RandomGeneratorImpl();
+    }
+
     @Override
     public int generate() {
         return random.nextInt(RANDOM_RANGE);
