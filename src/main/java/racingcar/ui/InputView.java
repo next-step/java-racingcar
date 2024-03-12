76117b4aca30f813cfdb31b8af6760fdbd07a5a7
ksh <tjdgus2319@gmail.com>
2024-03-12 14:04:44 +0900

    feat: 입력 화면 구현
    feat: 사용자가 입력 받을 수 있는 기능 구현
    
    fix: 입력 화면 메소드 print로 변경

---
+++ src/main/java/racingcar/ui/InputView.java
@@ -0,0 +1,22 @@
+package racingcar.ui;
+
+public class InputView {
+    private static InputView instance = new InputView();
+
+    private InputView() {
+
+    }
+
+    public static InputView getInstance() {
+        return instance;
+    }
+
+    public void printNumberOfCar() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+    }
+
+    public void printNumberOfAttempt() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+    }
+
+}
