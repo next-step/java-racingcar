8c39582d8b23757217f29b459ce02da4f27125aa
ksh <tjdgus2319@gmail.com>
2024-03-12 15:58:26 +0900

    feat: 자동차가 랜덤 값에 의해 전진하는 기능 구현

--- src/main/java/racingcar/Racingcar.java
+++ src/main/java/racingcar/Racingcar.java
@@ -2,15 +2,44 @@ package racingcar;
 
 import racingcar.ui.InputView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Racingcar {
     private static final int RANDOM_VALUE = 10;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numberOfCar = inputNumberOfCar(scanner);
-        int numberOfAttempt = inputNumberOfAttempt(scanner);
+        gameLogic(inputNumberOfCar(scanner), inputNumberOfAttempt(scanner));
+    }
+
+    public static void gameLogic(int numberOfCar, int numberOfAttempt) {
+        List<Car> carList = initCarList(numberOfCar);
+
+        carList.stream().forEach(car -> IntStream.range(0, numberOfAttempt).forEach(i -> moveCar(car)));
+    }
+
+    private static void moveCar(Car car) {
+        if (isMovingForward(getRandomValue())) {
+            car.move();
+        }
+    }
+
+    private static List<Car> initCarList(int numberOfCar) {
+        return new ArrayList<>(numberOfCar);
+    }
+
+    public static boolean isMovingForward(int value) {
+        return value > 3;
+    }
+
+    public static int getRandomValue() {
+        Random random = new Random();
+        int randomValue = random.nextInt(RANDOM_VALUE);
+        return randomValue;
     }
 
     private static int inputNumberOfCar(Scanner scanner) {
@@ -23,9 +52,4 @@ public class Racingcar {
         return scanner.nextInt();
     }
 
-    public static int getRandomValue() {
-        Random random = new Random();
-        int randomValue = random.nextInt(RANDOM_VALUE);
-        return randomValue;
-    }
 }
