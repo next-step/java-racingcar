929a974241979f8d10bee65908acc7cc6b0952d5
ksh <tjdgus2319@gmail.com>
2024-03-12 23:55:33 +0900

    refactor: 랜덤 매니저 클래스 테스트 이름 변경

--- src/test/java/car/RandomTest.java
+++ src/test/java/car/RandomTest.java
@@ -2,7 +2,7 @@ package car;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.RandomManaager;
+import racingcar.RandomManager;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -10,7 +10,7 @@ public class RandomTest {
     @Test
     @DisplayName("0~9 사이의 랜덤값을 구하는 테스트")
     public void testRandom() {
-        int randomValue = RandomManaager.getInstance().getRandomValue();
+        int randomValue = RandomManager.getInstance().getRandomValue();
         assertThat(randomValue < 10).isTrue();
     }
 
