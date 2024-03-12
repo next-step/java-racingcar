ba2854a6c2c4a16c4330c02919fb59a5ae3c8121
Sangmin <sm9171@nate.com>
2024-03-12 16:17:28 +0900

    test: 우승자 출력하기 테스트 작성 - 실패 테스트

--- src/test/java/racingcar/ResultViewTest.java
+++ src/test/java/racingcar/ResultViewTest.java
@@ -18,4 +18,36 @@ public class ResultViewTest {
 		// then
 		assertThat(result).contains("audi");
 	}
+
+	@DisplayName("우승자 출력하기")
+	@Test
+	void printWinners() {
+		// given
+		Car hyundaiCar = Car.createCar("hyundai");
+		Car samsungCar = Car.createCar("samsung");
+		Car kiaCar = Car.createCar("kia");
+
+		hyundaiCar.move(5);
+		hyundaiCar.move(6);
+		hyundaiCar.move(7);
+
+		samsungCar.move(4);
+		samsungCar.move(5);
+		samsungCar.move(6);
+
+		kiaCar.move(3);
+		kiaCar.move(2);
+		kiaCar.move(7);
+
+		Cars cars = new Cars();
+		cars.add(hyundaiCar);
+		cars.add(samsungCar);
+		cars.add(kiaCar);
+
+		// when
+		String viewWinner = ResultView.viewWinner(cars);
+
+		// then
+		assertThat(viewWinner).isEqualTo("hyundai, samsung가 최종 우승했습니다.");
+	}
 }
