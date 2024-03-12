c5fad8d6f9875347689a2e88a1c406cb13652f6b
develiberta <develiberta@github.com>
2024-03-12 22:14:34 +0900

    style: 개행 제거

--- src/main/java/step3/domain/RacingCars.java
+++ src/main/java/step3/domain/RacingCars.java
@@ -10,7 +10,6 @@ public class RacingCars {
     public RacingCars(int numberOfCars) {
         racingCars = new ArrayList<>();
         for (int i = 0; i < numberOfCars; i++) racingCars.add(new RacingCar(i));
-
     }
 
     public List<RacingCar> list() {
