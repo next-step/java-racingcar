221e29890f6f583c117416d93532144be197ded6
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-12 14:00:02 +0900

    test 주어진 횟수 그리고 주어진 자동차 수만큼 자동차가 생성되고 -만큼 전진한다.

--- src/test/java/race/CarsTest.java
+++ src/test/java/race/CarsTest.java
@@ -1,5 +1,6 @@
 package race;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -7,11 +8,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest {
 
+    private Cars cars;
+
+    @BeforeEach
+    void init() {
+        cars = new Cars(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:---", "1:---", "2:---"}, delimiter = ':')
+    void three_cars_three_times_go(int input, String expected) {
+        cars.getCarList().get(input).go();
+        cars.getCarList().get(input).go();
+        cars.getCarList().get(input).go();
+
+        assertThat(cars.getCarList().get(input).getMileage()).isEqualTo(expected);
+    }
+
+
     @ParameterizedTest
     @CsvSource(value = {"0:-", "1:-", "2:-"}, delimiter = ':')
     void three_cars_go(int input, String expected) {
-        Cars cars = new Cars(3);
         cars.getCarList().get(input).go();
+
         assertThat(cars.getCarList().get(input).getMileage()).isEqualTo(expected);
     }
 
