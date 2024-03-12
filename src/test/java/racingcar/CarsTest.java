28b9ebb751a3126e3066ae24ed50d667242bbdf0
dongock <okdong23@naver.com>
2024-03-12 23:44:02 +0900

    refactor:[4-2] CarsTest @BeforeEach로 기본 CarNames 생성

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,24 +1,27 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.model.CarNames;
-import racingcar.model.MoveStrategy;
 import racingcar.model.Cars;
+import racingcar.model.MoveStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
 
+    private CarNames carNames;
+
+    @BeforeEach
+    void setUp() {
+        carNames = new CarNames("car1,car2,car3");
+    }
+
     @ParameterizedTest
     @DisplayName("입력받은 차 이름에 따라 자동차들을 생성하는 Cars Test")
-    @ValueSource(strings = {"car1", "car1,car2,car3"})
-    void makeCarsTest(String carName) {
-        // given
-        CarNames carNames = new CarNames(carName);
-
+    void makeCarsTest() {
         // when
         Cars cars = new Cars(carNames);
 
@@ -26,33 +29,33 @@ public class CarsTest {
         assertThat(cars).hasSize(carNames.getCarNameCount());
     }
 
-//    @ParameterizedTest
-//    @DisplayName("시도할 횟수를 Car가 받고 Car 이동 명령 (정지) Test")
-//    @CsvSource(value = {"1:1", "1:2", "1:3"}, delimiter = ':')
-//    void orderStopCarsTest(int tryNumber, int moveNumber) {
-//        // given
-//        Cars cars = new Cars(5);
-//
-//        // when
-//        cars.tryMove(tryNumber, () -> moveNumber);
-//
-//        // then
-//        assertThat(cars).extracting("distance").containsExactly(0,0,0,0,0);
-//    }
-//
-//    @ParameterizedTest
-//    @DisplayName("시도할 횟수를 Car가 받고 Car 이동 명령 (이동) Test")
-//    @CsvSource(value = {"1:4", "1:5", "1:6"}, delimiter = ':')
-//    void orderMoveCarsTest(int tryNumber, int moveNumber) {
-//        // given
-//        Cars cars = new Cars(5);
-//
-//        // when
-//        cars.tryMove(tryNumber, () -> moveNumber);
-//
-//        // then
-//        assertThat(cars).extracting("distance").containsExactly(1,1,1,1,1);
-//    }
+    @ParameterizedTest
+    @DisplayName("시도할 횟수를 Car가 받고 Car 이동 명령 (정지) Test")
+    @CsvSource(value = {"1:1", "1:2", "1:3"}, delimiter = ':')
+    void orderStopCarsTest(int tryNumber, int moveNumber) {
+        // given
+        Cars cars = new Cars(carNames);
+
+        // when
+        cars.tryMove(tryNumber, () -> moveNumber);
+
+        // then
+        assertThat(cars).extracting("distance").containsExactly(0,0,0,0,0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("시도할 횟수를 Car가 받고 Car 이동 명령 (이동) Test")
+    @CsvSource(value = {"1:4", "1:5", "1:6"}, delimiter = ':')
+    void orderMoveCarsTest(int tryNumber, int moveNumber) {
+        // given
+        Cars cars = new Cars(carNames);
+
+        // when
+        cars.tryMove(tryNumber, () -> moveNumber);
+
+        // then
+        assertThat(cars).extracting("distance").containsExactly(1,1,1,1,1);
+    }
 
     static class TestMoveStrategy implements MoveStrategy {
         private final int moveNumber;
