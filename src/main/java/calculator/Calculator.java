49ce3eb99c721aa7b585761b748448155a0d5654
Jihoon Kim <78334008+DevRunner21@users.noreply.github.com>
2024-03-12 09:32:32 +0900

    [Step2] 문자열 계산기 (#5357)
    * chore: upgrade java8 -> java11, gradle, junit version
    
    * chore: upgrade java8 -> java11, gradle, junit version
    
    * feat: 빈 문자열 또는 null 값을 입력할 경우 0을 반환
    
    * feat: 문자열 입력 시 계산 로직 구현
    
    - 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
    - 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환
    
    * feat: 구분자에 컴마(,) 이외에 콜론(:)을 사용할 수 있도록 추가
    
    * feat: “//”와 “\n” 문자 사이에 커스텀 구분자를 지정
    
    * refactor: separateNumbers 메서드 리펙토링
    
    * feat: 음수를 전달할 경우 RuntimeException 예외가 발생
    
    * refactor: 숫자 1개 입력 시 값 반환 로직 수정
    
    - 입력값을 그대로 체크하도록 변경 ( 구분자+숫자 1개 입력시 케이스 고려 )
    
    ---------
    
    Co-authored-by: javajigi <javajigi@gmail.com>
    Co-authored-by: 김지훈 <jihoon@wemakeprice.com>

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,64 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator {
+
+	public static final String DEFAULT_DELIMITER_CHECK_REGEX = ",|:";
+	public static final String CUSTOM_DELIMITER_CHECK_REGEX = "//(.)\n(.*)";
+
+	public String calculate(String input) {
+		if ( (input == null) || (input.isEmpty()) ) {
+			return "0";
+		}
+
+		// 숫자 한개만 입력 시 입력값 그대로 반환
+		if (isNumber(input)) {
+			return input;
+		}
+
+		String[] numbers = separateNumbers(input);
+
+		checkInvalidInput(numbers);
+
+		return sum(numbers).toString();
+	}
+
+	private void checkInvalidInput(String[] numbers) {
+		for (String number : numbers) {
+			validateInputNumber(number);
+		}
+	}
+
+	private void validateInputNumber(String number) {
+		if (isNumber(number) || Integer.valueOf(number) < 0) {
+			throw new RuntimeException("음수 or 숫자 이외의 값을 입력하셨습니다.");
+		}
+	}
+
+	private static String[] separateNumbers(String input) {
+		// 구분자 검사
+		Matcher m = Pattern.compile(CUSTOM_DELIMITER_CHECK_REGEX).matcher(input);
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			return m.group(2).split(customDelimiter);
+		}
+
+		return input.split(DEFAULT_DELIMITER_CHECK_REGEX);
+	}
+
+	private Integer sum(String[] numbers) {
+		int result = 0;
+		for (String number : numbers) {
+			result += Integer.parseInt(number);
+		}
+
+		return result;
+	}
+
+	private boolean isNumber(String input) {
+		return input != null && input.matches("[-+]?\\d*\\.?\\d+");
+	}
+
+}
