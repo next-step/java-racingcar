4a6ec6c7f3100770ec991f760ddc97e5eada5762
jeonsanggi <jsg2861@gmail.com>
2024-03-12 21:53:58 +0900

    feature - main Application 추가

---
+++ src/main/java/racingcar/RacingApplication.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RacingApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int n = scanner.nextInt();
+
+        List<Vehicle> participateCar = getParticipateCar(n);
+        Game game = new RacingGame(participateCar);
+
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int round = scanner.nextInt();
+
+        Game gameRunner = new GameRunner(game);
+        System.out.println("실행 결과");
+        for (int i = 0; i < round; i++) {
+            gameRunner.start();
+        }
+
+    }
+
+    private static List<Vehicle> getParticipateCar(int n) {
+        List<Vehicle> vehicles = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            vehicles.add(new Car());
+        }
+        return vehicles;
+    }
+}
