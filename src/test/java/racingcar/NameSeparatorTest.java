27bfa40315227f940f928496cc0130714f2dc0fd
hellonayeon <hellonykwon@gmail.com>
2024-03-12 23:51:22 +0900

    Test: 이름 구분자 단위 테스트 작성

---
+++ src/test/java/racingcar/NameSeparatorTest.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.NameSeparator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NameSeparatorTest {
+
+    @Test
+    @DisplayName("[성공] 쉼표로 구분된 이름 문자열을 분리한다.")
+    void 쉼표_문자열_구분() {
+        // When
+        NameSeparator nameSeparator = new NameSeparator("pobi,crong,honux");
+        String[] actual = nameSeparator.separate();
+
+        // Then
+        assertThat(actual).containsExactlyInAnyOrder("pobi", "crong", "honux");
+    }
+
+    @Nested
+    @DisplayName("[학습 테스트] String.split()")
+    class StringSplitLearningTest {
+
+        @Test
+        @DisplayName("빈 문자열이 들어왔는데 쉼표로 split 하는 경우")
+        void 빈_문자열_split() {
+            assertThat("".split(",")).hasSize(1); // not throw npe
+        }
+    }
+}
