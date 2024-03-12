cad48349afd5e0652f5f7f2bf31af65b3c18c19c
westkitekim <westkite418@gmail.com>
2024-03-13 07:33:40 +0900

    feat: UI OutputView 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+import racingcar.domain.RacingCar;
+
+import java.util.List;
+
+public class OutputView {
+    private static final String POSITION_DELIMITOR = "-";
+    public static void printResultCarPosition(List<RacingCar> cars) {
+        for (RacingCar car : cars) {
+            printCarPosition(car.getPosition());
+        }
+        System.out.println();
+    }
+
+    private static void printCarPosition(int positionNumber) {
+        for (int i = 0; i < positionNumber; i++) {
+            System.out.print(POSITION_DELIMITOR);
+        }
+        System.out.println();
+    }
+}
