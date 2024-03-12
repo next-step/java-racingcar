9da107c7f2cc1172966cda12675934c695a9ba0b
jaejeong1 <wowjd555@gmail.com>
2024-03-12 22:31:23 +0900

    feat: 주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다. 전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다.

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,19 +1,27 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class RacingCar {
+    public static void main(String[] args) {
+        RacingCar racingCar = new RacingCar();
+        racingCar.inputCarAndMoveCount();
+        racingCar.start();
+    }
+
     private int carCount;
     private int moveCount;
     private static final Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
+    private static final int rangeRandom = 10;
+    private static final int cutMove = 4;
 
-   public void inputCarAndMoveCount() {
-       System.out.println("자동차 대수는 몇 대 인가요?");
-       this.carCount = scanner.nextInt();
-       System.out.println(this.carCount);
+    public void inputCarAndMoveCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        this.carCount = scanner.nextInt();
 
-       System.out.println("시도할 회수는 몇 회 인가요?");
-       this.moveCount = scanner.nextInt();
-       System.out.println(this.moveCount);
-   }
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        this.moveCount = scanner.nextInt();
+    }
 
     public int getCarCount() {
         return this.carCount;
@@ -24,10 +32,39 @@ public class RacingCar {
     }
 
     public void setCarCount(int carCount) {
-       this.carCount = carCount;
+        this.carCount = carCount;
     }
 
     public void setMoveCount(int moveCount) {
-       this.moveCount = moveCount;
-   }
+        this.moveCount = moveCount;
+    }
+
+    public void start() {
+        System.out.println("실행 결과");
+        int[] moveBoard = new int[carCount];
+
+        for (int i = 0; i < moveCount; i++) {
+            for (int j = 0; j < carCount; j++) {
+                int randomValue = createRandomValue();
+                if (isAbleToMove(randomValue)) {
+                    moveBoard[j] += 1;
+                }
+            }
+            for (int k = 0; k < carCount; k++) {
+                for (int x = 0; x < moveBoard[k]; x++) {
+                    System.out.print("-");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    private boolean isAbleToMove(int randomValue) {
+        return randomValue >= cutMove;
+    }
+
+    private int createRandomValue() {
+        return random.nextInt(rangeRandom);
+    }
 }
