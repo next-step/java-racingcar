ceb50321cbbe832221fef63901b09aa286856db8
namhyeop <skaguq@gmail.com>
2024-03-13 00:14:25 +0900

    feat : [step3] Main문 구현

---
+++ src/main/java/carRace/Main.java
@@ -0,0 +1,18 @@
+package carRace;
+
+import static carRace.Car.createCar;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RaceHost raceHost = new RaceHost(0, 0);
+        raceHost.inputRaceInformation();
+
+        List<Car> carList = createCar(raceHost.carMount());
+
+        raceHost.playGame(carList);
+        raceHost.printRaceInfo(carList);
+    }
+}
