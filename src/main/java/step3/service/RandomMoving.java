c7580ce78da89efb030e3caf13fc4f6380030193
kwanik-kor <rhksdlr134@naver.com>
2024-03-12 21:11:04 +0900

    refactor: static final 선언 순서 변경

--- src/main/java/step3/service/RandomMoving.java
+++ src/main/java/step3/service/RandomMoving.java
@@ -3,8 +3,7 @@ package step3.service;
 import java.util.Random;
 
 public class RandomMoving implements Moving {
-    private final static int MOVING_DISTANCE = 1;
-    private final static int MOVABLE_CRITERIA_LOWER_BOUND = 4;
+    private static final int MOVABLE_CRITERIA_LOWER_BOUND = 4;
     private final Random random = new Random();
 
     @Override
