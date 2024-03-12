3de1240fe2579386e92f532d003cf2f4d6c4e76c
pawn <devrunner21@gmail.com>
2024-03-12 17:09:07 +0900

    refactor: [추가반영] 문자열 분리 로직 별로 클래스로 분리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,6 @@
 package calculator;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Calculator {
-
-	public static final String DEFAULT_DELIMITER_CHECK_REGEX = ",|:";
-	public static final String CUSTOM_DELIMITER_CHECK_REGEX = "//(.)\n(.*)";
-	public static final Pattern CUSTOM_DELIMITER_CHECK_PATTERN = Pattern.compile(CUSTOM_DELIMITER_CHECK_REGEX);
-
 
 	public String calculate(String input) {
 		if ( (input == null) || (input.isEmpty()) ) {
@@ -20,7 +12,7 @@ public class Calculator {
 			return input;
 		}
 
-		String[] numbers = separateNumbers(input);
+		String[] numbers = CustomSeparator.separateNumbers(input);
 
 		checkInvalidInput(numbers);
 
@@ -37,17 +29,6 @@ public class Calculator {
 		if (isNumber(number) || Integer.valueOf(number) < 0) {
 			throw new RuntimeException("음수 or 숫자 이외의 값을 입력하셨습니다.");
 		}
-	}
-
-	private static String[] separateNumbers(String input) {
-		// 구분자 검사
-		Matcher m = CUSTOM_DELIMITER_CHECK_PATTERN.matcher(input);
-		if (m.find()) {
-			String customDelimiter = m.group(1);
-			return m.group(2).split(customDelimiter);
-		}
-
-		return input.split(DEFAULT_DELIMITER_CHECK_REGEX);
 	}
 
 	private Integer sum(String[] numbers) {
