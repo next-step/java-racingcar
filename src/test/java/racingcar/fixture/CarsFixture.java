6c600280c62882dd7d26c9e85a2adfe7fe6e212e
deepredk <deepred.kim@gmail.com>
2024-03-12 22:46:05 +0900

    refactor: `final static` -> `static final`

--- src/test/java/racingcar/fixture/CarsFixture.java
+++ src/test/java/racingcar/fixture/CarsFixture.java
@@ -5,7 +5,7 @@ import racingcar.domain.Cars;
 
 public class CarsFixture {
 
-    public final static List<String> CAR_NAMES = List.of("car1", "car2", "car3");
+    public static final List<String> CAR_NAMES = List.of("car1", "car2", "car3");
 
     public static Cars cars() {
         return Cars.from(CAR_NAMES);
