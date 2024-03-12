f56ec136561038c1b357bf262085e4de04c70433
hong yuseok <jericoh@naver.com>
2024-03-13 01:41:02 +0900

    refactor: RaceRecord → RaceResultOfCar로 인한 코드 및 테스트 코드 변경

--- src/main/java/carracing/model/Race.java
+++ src/main/java/carracing/model/Race.java
@@ -1,50 +1,35 @@
 package carracing.model;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Race {
-    private final NumberOfCars numberOfCars;
+    private final List<NameOfCar> nameOfCarList;
     private final NumberOfAttempts numberOfAttempts;
 
 
-    public Race(NumberOfCars numberOfCars, NumberOfAttempts numberOfAttempts) {
-        this.numberOfCars = numberOfCars;
+    public Race(List<NameOfCar> nameOfCarList, NumberOfAttempts numberOfAttempts) {
+        this.nameOfCarList = nameOfCarList;
         this.numberOfAttempts = numberOfAttempts;
     }
 
-    public List<RaceRecord> start() {
+    public List<RaceResultOfCar> start() {
         return doRace(participatedCarList());
     }
 
     private List<Car> participatedCarList() {
-        List<Car> participatedCarList = new ArrayList<>();
-
-        for (int i = 0; i < numberOfCars.numberOfCars(); i++) {
-            participatedCarList.add(new Car());
-        }
-
-        return participatedCarList;
-    }
-
-    private List<RaceRecord> doRace(List<Car> carList) {
-        List<RaceRecord> raceRecordList = new ArrayList<>();
-
-        for (int i = 0; i < numberOfAttempts.numberOfAttempts(); i++) {
-            boolean isLastAttempt = numberOfAttempts.isSame(i + 1);
-            carList.forEach(car -> car.move(new RandomStrategy()));
-            raceRecordList.add(raceStatus(carList, isLastAttempt));
-        }
-
-        return raceRecordList;
-    }
-
-    private RaceRecord raceStatus(List<Car> carList, boolean isLastAttempt) {
-        List<Integer> positionOfCarList = carList.stream()
-                .map(Car::position)
+        return nameOfCarList.stream()
+                .map(Car::newCar)
                 .collect(Collectors.toList());
+    }
 
-        return new RaceRecord(positionOfCarList, isLastAttempt);
+    private List<RaceResultOfCar> doRace(List<Car> carList) {
+        for (int i = 0; i < numberOfAttempts.numberOfAttempts(); i++) {
+            carList.forEach(car -> car.move(new RandomStrategy()));
+        }
+
+        return carList.stream()
+                .map(Car::raceResult)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/carracing/model/RaceTest.java
+++ src/test/java/carracing/model/RaceTest.java
@@ -3,20 +3,21 @@ package carracing.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RaceTest {
     @Test
-    @DisplayName("start(): 주어진 시도 횟수 크기의 List<RaceRecord>를 반환한다.")
+    @DisplayName("start(): 자동차 대수 크기의 List<RaceResultOfCar>를 반환한다.")
     void testStart() {
-        NumberOfCars numberOfCars = NumberOfCars.newNumber("3");
+        List<NameOfCar> nameOfCarList = Arrays.asList(NameOfCar.newName("a"), NameOfCar.newName("b"));
         NumberOfAttempts numberOfAttempts = NumberOfAttempts.newNumber("5");
 
-        Race race = new Race(numberOfCars, numberOfAttempts);
-        List<RaceRecord> raceRecordList = race.start();
+        Race race = new Race(nameOfCarList, numberOfAttempts);
+        List<RaceResultOfCar> raceResultOfCarList = race.start();
 
-        assertThat(raceRecordList.size()).isEqualTo(numberOfAttempts.numberOfAttempts());
+        assertThat(raceResultOfCarList.size()).isEqualTo(nameOfCarList.size());
     }
 }
