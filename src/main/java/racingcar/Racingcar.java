34bb4f92ffcf1cd677766a5a3ec1ad691a247fcb
ksh <tjdgus2319@gmail.com>
2024-03-12 17:41:18 +0900

    fix: 시간복잡도 생각하여, 로직 재 수정

--- src/main/java/racingcar/Racingcar.java
+++ src/main/java/racingcar/Racingcar.java
@@ -1,57 +1,79 @@
 package racingcar;
 
 import racingcar.ui.InputView;
+import racingcar.ui.ResultView;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Racingcar {
     private static final int RANDOM_VALUE = 10;
+    private static final int MOVE_CONDITION = 3;
+    private static final int MAX_INPUT_VALUE = 10000;
+    private static final Random random = new Random();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        gameLogic(inputNumberOfCar(scanner), inputNumberOfAttempt(scanner));
+
+        int numberOfCar = inputNumberOfCar(scanner);
+        int numberOfAttempt = inputNumberOfAttempt(scanner);
+
+        if (!isMaximumInput(numberOfCar, numberOfAttempt)) {
+            gameLogic(numberOfCar, numberOfAttempt);
+        }
+    }
+
+    public static boolean isMaximumInput(int numberOfCar, int numberOfAttempt) {
+        if(numberOfCar > MAX_INPUT_VALUE && numberOfAttempt > MAX_INPUT_VALUE)
+            return true;
+        return false;
     }
 
     public static void gameLogic(int numberOfCar, int numberOfAttempt) {
         List<Car> carList = initCarList(numberOfCar);
-
-        carList.stream().forEach(car -> IntStream.range(0, numberOfAttempt).forEach(i -> car.addMoveHistory(isMovingForward(getRandomValue()))));
+        IntStream.range(0, numberOfAttempt).forEach(i -> moveCarList(carList));
     }
 
-    private static void moveCar(Car car) {
-        if (isMovingForward(getRandomValue())) {
-            car.move();
-        }
+    private static void moveCarList(List<Car> carList) {
+        carList.stream().forEach(car -> {
+            car.move(isMovingForward(getRandomValue()));
+            ResultView.getInstance().printMove(car);
+        });
+        ResultView.getInstance().printLineBreak();
     }
 
     private static List<Car> initCarList(int numberOfCar) {
         List<Car> carList = new ArrayList<>(numberOfCar);
-        IntStream.range(0,numberOfCar).forEach(i -> carList.add(new Car()));
+        IntStream.range(0, numberOfCar).forEach(i -> carList.add(new Car()));
         return carList;
     }
 
     public static boolean isMovingForward(int value) {
-        return value > 3;
+        return value > MOVE_CONDITION;
     }
 
     public static int getRandomValue() {
-        Random random = new Random();
         int randomValue = random.nextInt(RANDOM_VALUE);
         return randomValue;
     }
 
     private static int inputNumberOfCar(Scanner scanner) {
         InputView.getInstance().printNumberOfCar();
-        return scanner.nextInt();
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("정수 타입이 아닙니다!" + e);
+            throw new RuntimeException();
+        }
     }
 
     private static int inputNumberOfAttempt(Scanner scanner) {
         InputView.getInstance().printNumberOfAttempt();
-        return scanner.nextInt();
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("정수 타입이 아닙니다!" + e);
+            throw new RuntimeException();
+        }
     }
-
 }
