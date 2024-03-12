1cb89c762307e65892264a592af5e0bc5ab105db
Sangmin <sm9171@nate.com>
2024-03-12 15:12:28 +0900

    test: Rename RacingCarTest to CarTest

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Random;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
-public class RacingCarTest {
+public class CarTest {
 	@DisplayName("값이 4 이상일 경우 전진한다.")
 	@ParameterizedTest
 	@ValueSource(ints = {4, 5, 6, 7, 8, 9})
