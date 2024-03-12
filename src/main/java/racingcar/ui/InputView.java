c353f02657fed6a790fb5aeb5522ab135a228372
ksh <tjdgus2319@gmail.com>
2024-03-12 14:04:44 +0900

    fix: 입력 화면 메소드 print로 변경

--- src/main/java/racingcar/ui/InputView.java
+++ src/main/java/racingcar/ui/InputView.java
@@ -11,11 +11,11 @@ public class InputView {
         return instance;
     }
 
-    public void inputNumberOfCar() {
+    public void printNumberOfCar() {
         System.out.println("자동차 대수는 몇 대 인가요?");
     }
 
-    public void inputNumberOfAttempt() {
+    public void printNumberOfAttempt() {
         System.out.println("시도할 회수는 몇 회 인가요?");
     }
 
