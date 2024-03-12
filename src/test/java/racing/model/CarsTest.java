9552ac5292307ffcd2f4066703cd17e41a26fe21
astrid-dm <himax_01@naver.com>
2024-03-13 00:02:40 +0900

    test : tc 수정

--- src/test/java/racing/model/CarsTest.java
+++ src/test/java/racing/model/CarsTest.java
@@ -1,9 +1,9 @@
 package racing.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import racing.view.ResultView;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -12,20 +12,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CarsTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"kim", "nick", "june"})
-    @DisplayName("레이싱 경주 시작 메서드 호출 성공 (참여자의 이름 정상 출력)")
-    void racingGame__ShouldWinAllDrivers(String input) {
+    @CsvSource(value = {"0:kim", "1:nick", "2:june"}, delimiter = ':')
+    @DisplayName("자동차가 다같이 움직여 모두 승리")
+    void Cars__ShouldWinAllDrivers(int index, String name) {
         // given
         String[] names = new String[]{"kim", "nick", "june"};
-        ResultView resultView = new ResultView();
-        
+        CarMovementStrategy carMovementStrategy = () -> true;
+
         // when
         Cars cars = new Cars(names);
-        List<Car> raceDoneCars = cars.getCurrentSituation();
-        resultView.appendCurrentSituation(raceDoneCars);
-        String result = resultView.getResult();
+        cars.moveOrNot(carMovementStrategy);
+        List<Car> winners = cars.getTheHighestScoreDrivers();
 
         // then
-        assertThat(result.contains(input)).isTrue();
+        assertThat(winners.get(index).getName()).isEqualTo(name);
+    }
+
+    @Test
+    @DisplayName("자동차가 랜덤으로 움직여 최소한 1명의 승자가 존재")
+    void Cars__ShouldWinAtLeastOneDriver() {
+        // given
+        String[] names = new String[]{"kim", "nick", "june"};
+        CarMovementStrategy carMovementStrategy = new RandomMovementStrategy();
+
+        // when
+        Cars cars = new Cars(names);
+        cars.moveOrNot(carMovementStrategy);
+        List<Car> winners = cars.getTheHighestScoreDrivers();
+
+        // then
+        assertThat(winners.size()).isGreaterThan(0);
     }
 }
