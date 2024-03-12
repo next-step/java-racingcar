f68d4cf87dd13096dbfd88688436a1c965422217
ksh <tjdgus2319@gmail.com>
2024-03-12 15:58:40 +0900

    test: 자동차의 전진 조건 테스트

--- src/test/java/racingCarTest.java
+++ src/test/java/racingCarTest.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.Car;
 import racingcar.Racingcar;
+
+import java.util.Random;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -12,6 +17,14 @@ public class racingCarTest {
     public void testRandom() {
         int randomValue = Racingcar.getRandomValue();
         assertThat(randomValue < 10).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("4이상일 경우 전진하는 조건인지 테스트")
+    @CsvSource({"3,false", "5,true", "8,true"})
+    public void testIsMoving(int input, boolean output) {
+        boolean isMoving = Racingcar.isMovingForward(input);
+        assertThat(isMoving).isEqualTo(output);
     }
 
     @Test
