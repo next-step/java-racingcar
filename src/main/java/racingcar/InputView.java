68a8987ab0b3a66e6216377b37099bff30a0ca72
Sangmin <sm9171@nate.com>
2024-03-12 15:42:44 +0900

    refactor: inputCarNamesQuestion, inputCountQuestion로 분리 * 질문과 input 순서때문에 밖으로 뺌.

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -6,13 +6,17 @@ public class InputView {
 	private static final String INPUT_CAR_NAMES_TEXT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String INPUT_COUNT_TEXT = "시도할 회수는 몇 회 인가요?";
 
-	public static String[] inputCarNames(final String inputNames) {
+	public static void inputCarNamesQuestion() {
 		print(INPUT_CAR_NAMES_TEXT);
+	}
+	public static void inputCountQuestion() {
+		print(INPUT_COUNT_TEXT);
+	}
+	public static String[] inputCarNames(final String inputNames) {
 		return inputNames.split(",");
 	}
 
 	public static Integer inputCount(final Integer inputCount) {
-		print(INPUT_COUNT_TEXT);
 		return inputCount;
 	}
 
--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -9,7 +9,9 @@ public class RacingCar {
 	private static final Scanner INPUT_VALUE = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		inputCarNamesQuestion();
 		String[] carNames = inputCarNames(INPUT_VALUE.nextLine());
+		inputCountQuestion();
 		int count = inputCount(INPUT_VALUE.nextInt());
 
 		result(carNames, count);
