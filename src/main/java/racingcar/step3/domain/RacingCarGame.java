1c5908f477ff15b57566967fd267af12973bec7d
yejikwon <yeeji77@naver.com>
2024-03-12 23:52:57 +0900

    refactor: 자동차 경주(RacingCarGame) 객체 정보 수정

--- src/main/java/racingcar/step3/domain/RacingCarGame.java
+++ src/main/java/racingcar/step3/domain/RacingCarGame.java
@@ -1,38 +1,26 @@
 package racingcar.step3.domain;
 
-import racingcar.step3.util.RandomNumberUtil;
+import racingcar.step3.dto.RacingCarGameResultDto;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCarGame {
 
-  private static final int RANDOM_NUMBER_BOUND = 10;
-
-  private final int rounds;
+  private final int round;
   private final List<Car> cars;
 
-  public RacingCarGame(int rounds, List<Car> cars) {
-    this.rounds = rounds;
+  public RacingCarGame(int round, List<Car> cars) {
+    this.round = round;
     this.cars = cars;
   }
 
-  public List<RacingCarGameResult> start() {
-    List<RacingCarGameResult> results = new ArrayList<>();
-    for (int round = 1; round <= rounds; round++) {
-      results.add(racing(round));
-    }
+  public RacingCarGameResultDto start() {
+    cars.forEach(Car::move);
+    List<RacingCarGameResultDto.RaceRecord> raceRecord = cars.stream()
+        .map(car -> new RacingCarGameResultDto.RaceRecord(car.getCarName(), car.getPosition()))
+        .collect(Collectors.toList());
 
-    return results;
-  }
-
-  private RacingCarGameResult racing(int round) {
-    List<RacingCarGameResult.RaceRecord> records = new ArrayList<>();
-    cars.forEach(car -> {
-      car.move(RandomNumberUtil.generate(RANDOM_NUMBER_BOUND));
-      records.add(new RacingCarGameResult.RaceRecord(car, car.getMoveCount()));
-    });
-
-    return new RacingCarGameResult(round, records);
+    return new RacingCarGameResultDto(round, raceRecord);
   }
 }
