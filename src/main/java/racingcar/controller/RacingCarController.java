f78b6742abc788b0ee0c2a3a374f01bc2982b402
westkitekim <westkite418@gmail.com>
2024-03-13 09:43:05 +0900

    feat: Controller 신규

---
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,19 @@
+package racingcar.controller;
+
+import racingcar.domain.RacingCarGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarController {
+    public static void main(String[] args) {
+        int carCount = InputView.inputCarCount();
+        int trialCount = InputView.inputTrialCount();
+
+        RacingCarGame racingCarGame = new RacingCarGame(carCount);
+
+        System.out.println("실행 결과");
+        while (trialCount-- > 0) {
+            OutputView.printCarPositionByPeriod(racingCarGame.playRoundsAndReturnCarList());
+        }
+    }
+}
