41112d3e067077288d53c4ea82303ba01d2c0508
유지웅 <poujis0@gmail.com>
2024-03-13 22:44:29 +0900

    [feat] 자동차 이름은 5자를 초과할 수 없는 로직 개발

---
+++ src/test/java/racingCar/CarNameTest.java
@@ -0,0 +1,25 @@
+package racingCar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarNameTest {
+
+    @Test
+    @DisplayName("이름이 5자 미만일 시 이름 return")
+    void CarName_Correct_Length() {
+        CarName carName = new CarName("pobi");
+        assertThat(carName.findName()).isEqualTo("pobi");
+    }
+
+    @Test
+    @DisplayName("이름이 5자 초과할 시 Exception 발생")
+    void CarName_Length_Longer_Than_Five_Then_Exception() {
+        assertThatThrownBy(() -> new CarName("Lionel Messi"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
