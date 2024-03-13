1df72e94244c324e5b2db8d3b33e28002b321c99
Jinnie-J <jjesweet@naver.com>
2024-03-13 23:28:41 +0900

    feat: 자동차 경주 게임 로직 구현

---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+    private RandomUtil randomUtil = new RandomUtil();
+    private List<Car> cars = new ArrayList<>();
+
+    public void start(InputValue input){
+        for(int i=0; i<input.getNumberOfCars(); i++){
+            cars.add(new Car());
+        }
+        ResultView.printResultMessage();
+        for(int i=0; i<input.getTryCount(); i++){
+            forward();
+            System.out.println();
+        }
+    }
+    public void forward(){
+        for(Car car: cars){
+            car.move(this.randomUtil.randomCount(10));
+        }
+        ResultView.printResult(cars);
+    }
+}
