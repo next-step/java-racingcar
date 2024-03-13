ed2b7377658b1d7fbcefab6beada6182b5a027ef
Sangmin <sm9171@nate.com>
2024-03-13 21:06:11 +0900

    refactor: RacingCar에 있던 view 관련 메소드를 InputView에 getCountAndCarNames 메소드 안에 집어넣음

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,17 +1,21 @@
 package racingcar;
 
-public class InputView {
+import java.util.Scanner;
 
+public class InputView {
 
 	private static final String INPUT_CAR_NAMES_TEXT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 	private static final String INPUT_COUNT_TEXT = "시도할 회수는 몇 회 인가요?";
+	private static final Scanner INPUT_VALUE = new Scanner(System.in);
 
-	public static void inputCarNamesQuestion() {
+	private static void inputCarNamesQuestion() {
 		print(INPUT_CAR_NAMES_TEXT);
 	}
-	public static void inputCountQuestion() {
+
+	private static void inputCountQuestion() {
 		print(INPUT_COUNT_TEXT);
 	}
+
 	public static String[] inputCarNames(final String inputNames) {
 		return inputNames.split(",");
 	}
@@ -22,5 +26,14 @@ public class InputView {
 
 	private static void print(final String text) {
 		System.out.println(text);
+	}
+
+	public static InputDto getCountAndCarNames() {
+		inputCarNamesQuestion();
+		String[] carNames = inputCarNames(INPUT_VALUE.nextLine());
+		inputCountQuestion();
+		int count = inputCount(INPUT_VALUE.nextInt());
+
+		return new InputDto(count, carNames);
 	}
 }
--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,18 +1,15 @@
 package racingcar;
 
-import java.util.Scanner;
-
-import static racingcar.InputView.*;
 import static racingcar.ResultView.result;
 
 public class RacingCar {
-	private static final Scanner INPUT_VALUE = new Scanner(System.in);
+
 
 	public static void main(String[] args) {
-		inputCarNamesQuestion();
-		String[] carNames = inputCarNames(INPUT_VALUE.nextLine());
-		inputCountQuestion();
-		int count = inputCount(INPUT_VALUE.nextInt());
+		InputDto countAndCarNames = InputView.getCountAndCarNames();
+
+		int count = countAndCarNames.getCount();
+		String[] carNames = countAndCarNames.getCarNames();
 
 		result(carNames, count);
 	}
