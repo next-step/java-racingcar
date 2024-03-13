04442d689016610a0a470ab25b6167811eae30c1
동원 <ai06021@gmail.com>
2024-03-13 12:39:41 +0900

    feat : start_location

---
+++ src/test/java/racingcar/RacingApplication.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import racingcar.controller.RacingController;
+
+public class RacingApplication {
+    public static void main(String[] args) {
+
+        new RacingController().racing();
+    }
+}
