33c3ca5d208ff20bf124d56e9136ae95a0b37758
westkitekim <westkite418@gmail.com>
2024-03-13 09:33:04 +0900

    feat: 테스트케이스 작성 - 자동차 리스트 생성

---
+++ src/test/java/racingcar/domain/RacingCarGameTest.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingCarGameTest {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 4})
+    @DisplayName("입력된 자동차 대수만큼 RacingCar 객체를 생성한다")
+    void createCarCount(int carCount) {
+        List<RacingCar> carList = RacingCarGame.createCarList(carCount);
+        int actual = carList.size();
+        int expected = carCount;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
