5e846d2bad2099f1c01f906454b7307ecdcc54fa
bellbpng <bellbpng@naver.com>
2024-03-13 21:59:32 +0900

    private 으로 기본 생성자 제한

--- src/main/java/Calculator/StringAddCal.java
+++ src/main/java/Calculator/StringAddCal.java
@@ -4,7 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCal {
-
+    private StringAddCal(){
+        // 생성자 접근을 private 으로 제한하여 객체 생성 방지
+        // static 메소드로만 이루어져 있기 때문에 객체 호출이 필요 없음
+    };
     public static int AddNumbers(String text) {
         if(text == null) return 0;
         if(text.isBlank()) return 0;
