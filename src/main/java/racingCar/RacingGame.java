47a6150a1d72bc04e7a52e3bdc7eff390afebc9a
유지웅 <poujis0@gmail.com>
2024-03-14 00:10:40 +0900

    [ref] 자동차 경주 시 이름 자연스럽게 변경

--- src/main/java/racingCar/RacingGame.java
+++ src/main/java/racingCar/RacingGame.java
@@ -9,10 +9,10 @@ public class RacingGame {
 
     public void run() {
         RacingCars cars = new RacingCars(CarFactory.createCars(inputNamesOfCars()));
-        tryMove(cars, inputNumberOfTry());
+        doRacing(cars, inputNumberOfTry());
     }
 
-    private void tryMove(RacingCars cars, int numberOfTries) {
+    private void doRacing(RacingCars cars, int numberOfTries) {
         for (int i = 0; i < numberOfTries; i++) {
             cars.moveCars();
             printResult(cars);
