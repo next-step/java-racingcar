8787a0aa8a89012fa35dc75dca12a838bae51b83
park-cheonho <cheonho08@naver.com>
2024-03-13 17:50:17 +0900

    fix: random 객체 생성 수정

--- src/main/java/racing/domain/RandomStrategy.java
+++ src/main/java/racing/domain/RandomStrategy.java
@@ -2,12 +2,15 @@ package racing.domain;
 
 import java.util.Random;
 
-public class RandomStrategy implements MoveStrategy{
+public class RandomStrategy implements MoveStrategy {
 
+    private static final Random random = new Random();
     private static final int FORWARDING_CONDITION = 4;
+    private static final int MAX_NUMBER = 10;
+
 
     @Override
     public boolean isMovable() {
-        return new Random().nextInt(10) > FORWARDING_CONDITION;
+        return random.nextInt(MAX_NUMBER) > FORWARDING_CONDITION;
     }
 }
