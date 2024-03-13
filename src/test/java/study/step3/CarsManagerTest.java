5ddbbba815b08705b3bb209d930fd6f086dcb682
JJewon <eldertree777@gmail.com>
2024-03-14 02:05:54 +0900

    [refactor] MethodSource를 통한 테스트

--- src/test/java/study/step3/CarsManagerTest.java
+++ src/test/java/study/step3/CarsManagerTest.java
@@ -1,31 +1,57 @@
 package study.step3;
 
+import org.assertj.core.internal.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import step3.Model.Car;
 import step3.Model.CarsManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsManagerTest {
-    @Test
-    @DisplayName("차량들의 위치를 반환하는 기능")
-    void getCarsDistanceTest() {
+    @ParameterizedTest(name = "차량 위치 : {1}")
+    @MethodSource("initCar")
+    @DisplayName("초기 차량 위치에 대해 반환한다.")
+    void getCarsDistanceTest(List<Car> cars, List<Integer> positions) {
         //given
-        Car car1 = new Car(1);
-        Car car2 = new Car(2);
-        Car car3 = new Car(3);
-        List<Car> cars = Arrays.asList(car1, car2, car3);
         CarsManager carsManager = new CarsManager(cars);
 
         //when
-        List<Integer> expected = Arrays.asList(1, 2, 3);
+        List<Integer> expected = positions;
         List<Integer> result = carsManager.getCarsPosition();
 
         //then
         assertThat(result).isEqualTo(expected);
+    }
+
+    private static List<Integer> givenPositions(int... positions) {
+        return Arrays.stream(positions)
+                .mapToObj(Integer::new)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Car> givenCars(List<Integer> positions) {
+        return positions.stream()
+                .map(position -> new Car(position))
+                .collect(Collectors.toList());
+    }
+
+    static Stream<Arguments> initCar() {
+        return Stream.of(
+                Arguments.arguments(givenCars(givenPositions(1, 2, 3)), givenPositions(1, 2, 3)),
+                Arguments.arguments(givenCars(givenPositions(2, 3, 4)), givenPositions(2, 3, 4)),
+                Arguments.arguments(givenCars(givenPositions(3, 4, 5)), givenPositions(3, 4, 5)),
+                Arguments.arguments(givenCars(givenPositions(4, 5, 6)), givenPositions(4, 5, 6)),
+                Arguments.arguments(givenCars(givenPositions(5, 6, 7)), givenPositions(5, 6, 7))
+        );
     }
 }
