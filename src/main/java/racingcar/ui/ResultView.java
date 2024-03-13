65bd7440ec2667121f25b85e9e6687205aa60522
ksh <tjdgus2319@gmail.com>
2024-03-13 23:54:40 +0900

    feat: 자동차 이름 출력하는 기능

--- src/main/java/racingcar/ui/ResultView.java
+++ src/main/java/racingcar/ui/ResultView.java
@@ -5,6 +5,9 @@ import racingcar.model.Car;
 import java.util.List;
 
 public class ResultView {
+    private static final String PRINT_DISTANCE = "-";
+    private static final String NAME_DELIMITER = ": ";
+
     private static ResultView instance = new ResultView();
 
     private ResultView() {
@@ -20,10 +23,15 @@ public class ResultView {
     }
 
     private static void printCarDistance(Car car) {
+        System.out.print(getCarName(car));
         for (int i = 0; i < car.getDistance(); i++) {
-            System.out.print("-");
+            System.out.print(PRINT_DISTANCE);
         }
         System.out.println();
+    }
+
+    private static String getCarName(Car car) {
+        return car.getName() + NAME_DELIMITER;
     }
 
     public void printLineBreak() {
