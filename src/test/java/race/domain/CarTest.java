11a4b45e09b99843ad1571e1524eb73432205b33
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-14 02:31:46 +0900

    test cartest의 중복을 제거하고 어떤 반환값을 테스트하는지 명시하라

--- src/test/java/race/domain/CarTest.java
+++ src/test/java/race/domain/CarTest.java
@@ -1,6 +1,7 @@
 package race.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,26 +16,12 @@ public class CarTest {
         car = new Car(1, "임형준");
     }
 
+    @DisplayName("go메서드의 인자(randomNumber)를 고정해 움직인 거리를 반환한다")
     @ParameterizedTest
     @CsvSource(value = {"4:4", "3:1"}, delimiter = ':')
     void go_three_times(int randomNumber, int expected) {
         car.go(randomNumber);
         car.go(randomNumber);
-        car.go(randomNumber);
-        assertThat(car.getPosition()).isEqualTo(expected);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"4:3", "3:1"}, delimiter = ':')
-    void go_twice(int randomNumber, int expected) {
-        car.go(randomNumber);
-        car.go(randomNumber);
-        assertThat(car.getPosition()).isEqualTo(expected);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"4:2", "3:1"}, delimiter = ':')
-    void go(int randomNumber, int expected) {
         car.go(randomNumber);
         assertThat(car.getPosition()).isEqualTo(expected);
     }
