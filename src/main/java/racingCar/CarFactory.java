1f533844b42630ee1e7b8b71d1303acd10280e41
유지웅 <poujis0@gmail.com>
2024-03-13 23:18:24 +0900

    [feat] 이름 입력을 통한 자동차 생성

--- src/main/java/racingCar/CarFactory.java
+++ src/main/java/racingCar/CarFactory.java
@@ -1,13 +1,28 @@
 package racingCar;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class CarFactory {
+
+    public static final String NAME_SEPARATOR = ",";
+
     public static List<RacingCar> createCars(int numberOfCars) {
         return IntStream.range(0, numberOfCars)
             .mapToObj(i -> new RacingCar())
             .collect(Collectors.toList());
+    }
+
+    public static List<RacingCar> createCars(String namesOfCars) {
+        return Arrays.stream(splitNames(namesOfCars))
+            .map(CarName::new)
+            .map(RacingCar::new)
+            .collect(Collectors.toList());
+    }
+
+    private static String[] splitNames(String namesOfCars) {
+        return namesOfCars.split(NAME_SEPARATOR);
     }
 }
