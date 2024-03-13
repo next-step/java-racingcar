131755135a0a8a2533aa84b3132c7c0fd3f58f52
westkitekim <westkite418@gmail.com>
2024-03-13 23:23:26 +0900

    style: 불필요한 주석 제거

--- src/main/java/racingcar/domain/RacingCarGame.java
+++ src/main/java/racingcar/domain/RacingCarGame.java
@@ -8,12 +8,11 @@ import java.util.stream.Stream;
 
 public class RacingCarGame {
     private List<RacingCar> carList;
-    
+
     public RacingCarGame(int carCount) {
         this.carList = createCarList(carCount);
     }
 
-    // 정적 팩토리 메서드 구현
     public static List<RacingCar> createCarList(int carCount) {
         return Stream.generate(RacingCar::new)
                 .limit(carCount)
