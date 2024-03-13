c28c68dc34456ef48faec611d8194d457220cf9b
westkitekim <westkite418@gmail.com>
2024-03-13 11:17:00 +0900

    refactor: 랜덤값 생성클래스 최대값 상수화

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -3,7 +3,8 @@ package racingcar.util;
 import java.util.Random;
 
 public class RandomNumberGenerator {
+    private static final int MAX_NUMBER = 10;
     public static int generateRandomNumber() {
-        return new Random().nextInt(10);
+        return new Random().nextInt(MAX_NUMBER);
     }
 }
