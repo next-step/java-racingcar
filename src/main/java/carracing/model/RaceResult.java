33f761535f908d9f104c7754ea2f4a828fb1f9b6
hong yuseok <jericoh@naver.com>
2024-03-13 21:46:34 +0900

    feat: RaceResult 구현

---
+++ src/main/java/carracing/model/RaceResult.java
@@ -0,0 +1,35 @@
+package carracing.model;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+public class RaceResult {
+    private static final String WRONG_RACE_RESULT_GUIDE_MESSAGE = "잘못된 경기 결과입니다.";
+    private static final String COMMA_SPACE_DELIMITER = ", ";
+    private final List<RaceRecordOfCar> recordOfCarList;
+
+    private RaceResult(final List<RaceRecordOfCar> recordOfCarList) {
+        this.recordOfCarList = recordOfCarList;
+    }
+
+    public static RaceResult newResult(final List<RaceRecordOfCar> recordOfCarList) {
+        return new RaceResult(recordOfCarList);
+    }
+
+    public String winner() {
+        int maxPosition = maxPositionOfCars();
+
+        return recordOfCarList.stream()
+                .filter(raceRecordOfCar -> raceRecordOfCar.samePosition(maxPosition))
+                .map(RaceRecordOfCar::carName)
+                .collect(Collectors.joining(COMMA_SPACE_DELIMITER));
+    }
+
+    private int maxPositionOfCars() {
+        return recordOfCarList.stream()
+                .mapToInt(RaceRecordOfCar::position)
+                .max()
+                .orElseThrow(() -> new NoSuchElementException(WRONG_RACE_RESULT_GUIDE_MESSAGE));
+    }
+}
