0dd89ced0a139d1fbcb88517ac0402b499ed9590
westkitekim <westkite418@gmail.com>
2024-03-13 12:17:06 +0900

    feat: 테스트코드 구현 - 자동차 전진 및 정지

--- src/test/java/racingcar/domain/RacingCarGameTest.java
+++ src/test/java/racingcar/domain/RacingCarGameTest.java
@@ -1,8 +1,11 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.strategyPattern.MoveStrategy;
+import racingcar.util.RandomNumberGenerator;
 
 import java.util.List;
 
@@ -18,5 +21,31 @@ class RacingCarGameTest {
         int expected = carCount;
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("전략에 따라 랜덤값이 0이상 3이하인 경우 자동차는 정지한다")
+    void playRoundsAndReturnCarList_정지() {
+        RacingCar racingCar = new RacingCar();
+        int beforePostion = racingCar.getPosition();
+        MoveStrategy noMoveStrategy = () -> false;
+
+        racingCar.moveForward(noMoveStrategy);
+
+        int afterPosition = racingCar.getPosition();
+        assertThat(beforePostion).isEqualTo(afterPosition);
+    }
+
+    @Test
+    @DisplayName("전략에 따라 랜덤값이 0이상 3이하인 경우 자동차는 정지한다")
+    void playRoundsAndReturnCarList_전진() {
+        RacingCar racingCar = new RacingCar();
+        int beforePostion = racingCar.getPosition();
+        MoveStrategy noMoveStrategy = () -> true;
+
+        racingCar.moveForward(noMoveStrategy);
+
+        int afterPosition = racingCar.getPosition();
+        assertThat(beforePostion + 1).isEqualTo(afterPosition);
     }
 }
