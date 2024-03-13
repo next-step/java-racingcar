7395a22100eadd6a80fe33bbccf29c35067ab545
Jinnie-J <jjesweet@naver.com>
2024-03-13 23:40:17 +0900

    refactor: 변수 값 상수로 치환

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,14 +1,17 @@
 package racingcar;
 
 public class Car {
+    private static final int MOVE_STANDARD = 4;
+    private static final int DEFAULT_DISTANCE = 1;
     private int position = 0;
 
-    public void move(int number){
-        if (number >= 4){
-            this.position +=1;
+    public void move(int number) {
+        if (number >= MOVE_STANDARD) {
+            this.position += DEFAULT_DISTANCE;
         }
     }
-    public int getPosition(){
+
+    public int getPosition() {
         return this.position;
     }
 }
