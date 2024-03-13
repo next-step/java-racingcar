df0774fc47823cbddd25a0426e3f4276e6bd0169
Jinnie-J <jjesweet@naver.com>
2024-03-13 23:27:55 +0900

    feat: 유효성 검사 로직 추가

---
+++ src/main/java/racingcar/Validator.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+    private static final Pattern NUMERIC_PATTERN = Pattern.compile("\\d");
+    public static final int MIN_DISTANCE = 0;
+    public static final int MAX_DISTANCE = 9;
+
+
+    public void nullCheck(String input) {
+        if (input == null || input.isBlank()) {
+            throw new RuntimeException("빈 값이 입력되었습니다.");
+        }
+    }
+
+    public void numericCheck(String input) {
+        Matcher matcher = NUMERIC_PATTERN.matcher(input);
+        if (!matcher.find()) {
+            throw new RuntimeException("숫자만 입력해주세요.");
+        }
+        numberCheck(input);
+    }
+
+    private void numberCheck(String input) {
+        Integer number = Integer.parseInt(input);
+        if (number > MAX_DISTANCE || number < MIN_DISTANCE) {
+            throw new RuntimeException("0-9 사이의 숫자를 입력해주세요.");
+        }
+    }
+
+}
