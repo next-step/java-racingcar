1d6e2ef73d4bbc1f38b6ca4a35fa1ba5b373f217
nimoh <spakers38@gmail.com>
2024-03-13 19:36:34 +0900

    refactor: [Step 4] Winners equals(), hashCode() 추가

--- src/main/java/racing/Winners.java
+++ src/main/java/racing/Winners.java
@@ -1,6 +1,7 @@
 package racing;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Winners {
     private static final String WINNER_DELIMITER = ",";
@@ -9,6 +10,19 @@ public class Winners {
 
     public Winners(List<Car> winners) {
         this.winners = winners;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Winners winners1 = (Winners) o;
+        return Objects.equals(winners, winners1.winners);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(winners);
     }
 
     @Override
