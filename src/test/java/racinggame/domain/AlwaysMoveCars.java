8b65ea5391df8b055d09bbe727b07b39b452573f
boy0516 <60382864+boy0516@users.noreply.github.com>
2024-03-14 01:14:10 +0900

    test: 테스트용 class 추가

--- src/test/java/racinggame/domain/AlwaysMoveCars.java
+++ src/test/java/racinggame/domain/AlwaysMoveCars.java
@@ -5,32 +5,15 @@ import java.util.List;
 
 public class AlwaysMoveCars extends Cars{
 
-    private final List<Car> cars;
-
     public AlwaysMoveCars(CarsNames carsNames) {
-        super(carsNames);
-        this.cars = initialCars(carsNames);
+        super(initialCars(carsNames));
     }
 
     private static List<Car> initialCars(CarsNames carsNames) {
         List<Car> cars = new ArrayList<>();
         for (int i = 0; i < carsNames.size(); i++) {
-            cars.add(new Car(carsNames.getName(i)));
+            cars.add(new AlwaysMoveCar(carsNames.getName(i)));
         }
         return cars;
-    }
-
-    @Override
-    public void raceStart(RaceCount raceCount, RaceRecorder recorder) {
-        for (int i = 0; i < raceCount.value(); i++) {
-            runCars();
-            recorder.record(cars);
-        }
-    }
-
-    private void runCars() {
-        for (Car car : cars) {
-            car.run(new AlwaysMoveCondition());
-        }
     }
 }
--- src/test/java/racinggame/domain/NeverMoveCars.java
+++ src/test/java/racinggame/domain/NeverMoveCars.java
@@ -1,36 +1,20 @@
 package racinggame.domain;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class NeverMoveCars extends Cars{
 
-    private final List<Car> cars;
-
     public NeverMoveCars(CarsNames carsNames) {
-        super(carsNames);
-        this.cars = initialCars(carsNames);
+        super(initialCars(carsNames));
     }
 
     private static List<Car> initialCars(CarsNames carsNames) {
         List<Car> cars = new ArrayList<>();
         for (int i = 0; i < carsNames.size(); i++) {
-            cars.add(new Car(carsNames.getName(i)));
+            cars.add(new NeverMoveCar(carsNames.getName(i)));
         }
         return cars;
-    }
-
-    @Override
-    public void raceStart(RaceCount raceCount, RaceRecorder recorder) {
-        for (int i = 0; i < raceCount.value(); i++) {
-            runCars();
-            recorder.record(cars);
-        }
-    }
-
-    private void runCars() {
-        for (Car car : cars) {
-            car.run(new NeverMoveCondition());
-        }
     }
 }
