a96c3aef4fca46af5bbd7418d4ddfc84216e02e6
westkitekim <westkite418@gmail.com>
2024-03-13 13:08:15 +0900

    style: 개행 누락

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class RandomNumberGenerator {
     private static final int MAX_NUMBER = 10;
+
     public static int generateRandomNumber() {
         return new Random().nextInt(MAX_NUMBER);
     }
