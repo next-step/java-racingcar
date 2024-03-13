c946f89da303782dcf7c23feaf37ed4dce71e934
auspicJade <159258432+auspicJade@users.noreply.github.com>
2024-03-13 13:29:51 +0900

    2단계 - 문자열 덧셈 계산기 (#5393)
    * SetTest refactored
    
    * StringTest refactored
    
    * StringSumCaculator test with unit test
    
    * StringCaculator created with codes refactored (+ test code modified
    
    ---------
    
    Co-authored-by: ljg294 <ljg29494@gmail.com>

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -1,3 +1,5 @@
+package step1;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,12 +29,8 @@ public class SetTest {
     @Test
     @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현.")
     void sizeTest(){
-        int actual = numbers.size();
         int expected = 3;
-        assertThat(actual).isEqualTo(expected);
-
-        int wrongExpected = 4;
-        assertThat(actual).isNotEqualTo(wrongExpected);
+        assertThat(numbers).hasSize(expected);
     }
 
 //    요구사항 2
@@ -40,23 +38,26 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 중복 코드 없이 구현.")
     void containsTest(int value){
-        assertThat(numbers.contains(value)).isTrue();
+        assertThat(numbers).contains(value);
     }
 
-//    요구사항 3
+    //    요구사항 3
     @ParameterizedTest
-    @CsvSource(value = {"1,2,3:true", "4,5:false"}, delimiter = ':')
+    @CsvSource(value = {
+            "1, true",
+            "2, true",
+            "3, true",
+            "4, false",
+            "5, false"
+    }, delimiter = ',')
     @DisplayName("Set의 contains() 메소드를 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현")
-    void containsTestWithDifferentResults(String input, String expectedResult){
-        String[] inputComponents = input.split(",");
-        int[] valueArray = Stream.of(inputComponents)
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        boolean expected = Boolean.parseBoolean(expectedResult);
+    void containsTestWithDifferentResults(int actual,
+                                          Boolean expectedResult) {
+        if(expectedResult)
+            assertThat(numbers).contains(actual);
 
-        for(int actual : valueArray){
-            assertThat(numbers.contains(actual)).isEqualTo(expected);
-        }
+        if(!expectedResult)
+            assertThat(numbers).doesNotContain(actual);
     }
 
 }
