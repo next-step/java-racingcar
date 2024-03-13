8adb33fe06ae7cd0c97f5cda66d0f7f69c3be30c
westkitekim <westkite418@gmail.com>
2024-03-14 00:01:42 +0900

    refactor: 난수생성로직 전략패턴 및 상수로 변경 1. RandomNumberGenerator 사용하지 않는 유틸리티 객체 삭제 2. 난수 생성 로직 전략패턴 클래스에서 구현으로 변경

--- src/main/java/racingcar/domain/strategyPattern/StrategyRandomMove.java
+++ src/main/java/racingcar/domain/strategyPattern/StrategyRandomMove.java
@@ -1,12 +1,18 @@
 package racingcar.domain.strategyPattern;
 
-import racingcar.util.RandomNumberGenerator;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class StrategyRandomMove implements MoveStrategy{
+    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
+    private static final int RANDOM_BOUND = 10;
     private static final int MOVABLE_STANDARD_NUMBER = 4;
 
     @Override
     public boolean isMovable() {
-        return RandomNumberGenerator.generateRandomNumber() >= MOVABLE_STANDARD_NUMBER;
+        return getNumber() >= MOVABLE_STANDARD_NUMBER;
+    }
+
+    private int getNumber() {
+        return random.nextInt(RANDOM_BOUND);
     }
 }
