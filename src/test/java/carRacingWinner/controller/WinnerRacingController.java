4ecfcb729b7c50e49fad9623d78e579009b003db
동원 <ai06021@gmail.com>
2024-03-14 18:53:50 +0900

    feat : name divison

--- src/test/java/carRacingWinner/controller/WinnerRacingController.java
+++ src/test/java/carRacingWinner/controller/WinnerRacingController.java
@@ -8,13 +8,27 @@ public class WinnerRacingController {
 
     }
 
-    public String carsNameInput() {
+    public String[] carsNameInput() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return input();
+        String[] names = division(input());
+        return names;
     }
 
     public String input() {
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
+    }
+
+    public String[] division(String names) {
+        String[] words = names.split(",");
+        for (String word : words) {
+            if (lengthCheck(word))
+                throw new IllegalArgumentException("길이가 깁니다");
+        }
+        return words;
+    }
+
+    public boolean lengthCheck(String name) {
+        return name.length() > 5;
     }
 }
