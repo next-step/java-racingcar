11537a2b4f6bc3cc95273dbbebc1b5cfbcf6904c
westkitekim <westkite418@gmail.com>
2024-03-14 14:01:23 +0900

    refactor: 전략객체 상수화

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,11 +1,14 @@
 package racingcar.controller;
 
 import racingcar.domain.RacingCarGame;
+import racingcar.domain.strategyPattern.MoveStrategy;
 import racingcar.domain.strategyPattern.StrategyRandomMove;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class RacingCarController {
+
+    private static final StrategyRandomMove strategyRandomMove = new StrategyRandomMove();
     public static void main(String[] args) {
         int carCount = InputView.inputCarCount();
         int trialCount = InputView.inputTrialCount();
@@ -14,7 +17,7 @@ public class RacingCarController {
 
         System.out.println("실행 결과");
         while (trialCount-- > 0) {
-            OutputView.printCarPositionByPeriod(racingCarGame.playRoundsAndReturnCarList(new StrategyRandomMove()));
+            OutputView.printCarPositionByPeriod(racingCarGame.playRoundsAndReturnCarList(strategyRandomMove));
         }
     }
 }
