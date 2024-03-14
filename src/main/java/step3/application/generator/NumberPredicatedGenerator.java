2f7c7b7205effacc920e560ba1ee320c034fb033
jy.lee <ljyda214@gmail.com>
2024-03-14 17:52:29 +0900

    FEAT : 테스트용 구현체 추가

---
+++ src/main/java/step3/application/generator/NumberPredicatedGenerator.java
@@ -0,0 +1,9 @@
+package step3.application.generator;
+
+public class NumberPredicatedGenerator implements NumberGenerator {
+
+    @Override
+    public int drawNumber() {
+        return 0;
+    }
+}
