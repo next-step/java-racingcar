36fccfaa937e42ecefdd44710f2d3eeb35b2af97
jy.lee <ljyda214@gmail.com>
2024-03-14 18:58:33 +0900

    FEAT : RacingCars 테스트 추가

--- src/main/java/step3/application/generator/NumberPredicatedGenerator.java
+++ src/main/java/step3/application/generator/NumberPredicatedGenerator.java
@@ -2,8 +2,13 @@ package step3.application.generator;
 
 public class NumberPredicatedGenerator implements NumberGenerator {
 
+    private int order;
     @Override
     public int drawNumber() {
-        return 0;
+        order += 1;
+        if (order % 2 == 0) {
+            return 0;
+        }
+        return 5;
     }
 }
