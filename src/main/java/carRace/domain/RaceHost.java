e32f03a04f566126c8051c8e3ba970f559a76716
namhyeop <skaguq@gmail.com>
2024-03-15 00:53:03 +0900

    feat : [step4] 경기진행자 필드 fianl로 수정

--- src/main/java/carRace/domain/RaceHost.java
+++ src/main/java/carRace/domain/RaceHost.java
@@ -14,23 +14,18 @@ public class RaceHost {
 
     private final int raceTryCount;
 
-    public RaceHost(int carCount, int raceTryCount) {
-        this.carCount = carCount;
+    public RaceHost(final int raceTryCount) {
         this.raceTryCount = raceTryCount;
     }
 
-    public int carCount() {
-        return this.carCount;
-    }
-
-    public void playGame(List<Car> cars) {
+    public void playGame(final List<Car> cars) {
         for (int gameCount = 0; gameCount < raceTryCount; gameCount++) {
             moveCar(cars);
             OutputView.printRaceResult(cars);
         }
     }
 
-    private void moveCar(List<Car> cars) {
+    private void moveCar(final List<Car> cars) {
         List<CompletableFuture<Void>> runningCars = cars.stream()
             .map(car -> CompletableFuture.runAsync(() -> car.move(RandomNumberGenerator.createRandomNumber())))
             .collect(Collectors.toList());
@@ -38,13 +33,13 @@ public class RaceHost {
         waitCars(runningCars);
     }
 
-    private void waitCars(List<CompletableFuture<Void>> runningCars) {
+    private void waitCars(final List<CompletableFuture<Void>> runningCars) {
         CompletableFuture<Void> waitCars = CompletableFuture.allOf(
             runningCars.toArray(new CompletableFuture[ZERO]));
         ranAllCars(waitCars);
     }
 
-    private void ranAllCars(CompletableFuture<Void> waitCars) {
+    private void ranAllCars(final CompletableFuture<Void> waitCars) {
         waitCars.join();
     }
 }
