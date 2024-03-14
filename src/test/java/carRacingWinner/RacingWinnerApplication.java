dc3ad6b97e077fe32035e3c71cab9e7154d0ffbd
동원 <ai06021@gmail.com>
2024-03-14 18:42:06 +0900

    feat : code start location

--- src/test/java/carRacingWinner/RacingWinnerApplication.java
+++ src/test/java/carRacingWinner/RacingWinnerApplication.java
@@ -1,9 +1,11 @@
 package carRacingWinner;
 
+import carRacingWinner.controller.WinnerRacingController;
+
 public class RacingWinnerApplication {
 
     public static void main(String[] args) {
 
-
+        new WinnerRacingController().racingStart();
     }
 }
