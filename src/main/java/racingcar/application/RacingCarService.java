3444258997d62cab7a6d0ea8718063bed870a504
Danden1 <yongha.kwon12@gmail.com>
2024-03-14 23:42:27 +0900

    feat: 우승 차량 제공 및 자동자들 만드는 service 구현

--- src/main/java/racingcar/application/RacingCarService.java
+++ src/main/java/racingcar/application/RacingCarService.java
@@ -1,42 +1,51 @@
 package racingcar.application;
 
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.domain.MoveCommand;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RacingCarService {
     private final MoveCommand moveCommand;
 
-    private final List<Car> cars = new ArrayList<>();
+    private Cars cars;
 
     public RacingCarService(MoveCommand moveCommand) {
         this.moveCommand = moveCommand;
     }
 
-    public void initCars(int carNumber) {
-        for (int i = 0; i < carNumber; i++) {
-            cars.add(new Car("asd"));
+    public void initCars(List<String> carNames) {
+        cars = new Cars(makeCars(carNames));
+    }
+
+    private List<Car> makeCars(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
         }
+
+        return cars;
     }
 
     public List<Car> cars() {
-        List<Car> copyCars = new ArrayList<>();
-        for (Car car : cars) {
-            copyCars.add(new Car(car));
-        }
-
-        return copyCars;
+        return cars.getCars();
     }
 
     public void moveCar() {
-        if (cars.isEmpty()) {
+        if (cars == null) {
             throw new IllegalStateException("차량이 없습니다. 먼저 차량을 만들어주세요.");
         }
-        for (Car car : cars) {
-            car.move(moveCommand.command());
-        }
+
+        cars.move(moveCommand);
+    }
+
+    public List<Car> winnerCars() {
+        return cars.getWinnerCars();
     }
 
 }
--- src/test/java/racingcar/RacingCarServiceTest.java
+++ src/test/java/racingcar/RacingCarServiceTest.java
@@ -4,40 +4,96 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.application.RacingCarService;
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
 import racingcar.domain.MoveCommand;
+import racingcar.domain.MoveType;
 import racingcar.utils.TestNumber;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingCarServiceTest {
 
+    //차량 이름 길이는 5글자를 초과하면 안되고 중복되면 안됨.
+    private final List<String> validCarNames = List.of("test1", "test2", "test3");
+
     @ParameterizedTest
-    @DisplayName("N 대의 차량을 M 번 움직였을 경우 테스트")
+    @DisplayName("중복되지 않은 차량(이름)을 M 번 움직였을 경우 테스트")
     @CsvSource(value = {"0:0","1:0","2:0","3:0","4:2","5:2","6:2","7:2","8:2","9:2"}, delimiter = ':')
     void testMoveCar(int number, int expectedPosition) {
-        int countCar = 2;
         int repeatCount = 2;
 
         RacingCarService racingCarService = new RacingCarService(new MoveCommand(new TestNumber(number)));
-        racingCarService.initCars(countCar);
+        racingCarService.initCars(validCarNames);
 
         for (int i = 0; i < repeatCount; i++) {
             racingCarService.moveCar();
         }
 
-        assertThat(racingCarService.cars()).hasSize(countCar);
-        for (Car car : racingCarService.cars()) {
+        List<Car> cars = racingCarService.cars();
+        for (int i = 0; i < cars.size(); i++) {
+            Car car = cars.get(i);
             assertThat(car.getPosition()).isEqualTo(expectedPosition);
+            assertThat(car.getName()).isEqualTo(validCarNames.get(i));
         }
     }
 
     @Test
-    @DisplayName("N 대의 차량을 만들기 전에, 이동하면 예외 발생")
+    @DisplayName("우승 차량 반환 테스트")
+    public void testWinnerCars() {
+        List<Integer> carPositions = List.of(1,2,2);
+        List<String> winnerCarNames = List.of("test2", "test3");
+        Cars cars = makeCars(validCarNames, carPositions);
+        List<Car> winners = cars.getWinnerCars();
+
+        for (int i = 0; i < winners.size(); i++) {
+            Car winner = winners.get(i);
+            assertThat(winner.getName()).isEqualTo(winnerCarNames.get(i));
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("차량 이름 중복된 경우 예외 발생")
+    @ValueSource(ints = {0,1,2,3,4,5,6,7,8,9})
+    void testDuplicateCarNames(int number) {
+        RacingCarService racingCarService = new RacingCarService(new MoveCommand(new TestNumber(number)));
+
+        String carName = "test";
+        List<String> duplicateCarNames = List.of(carName, carName);
+
+        assertThatThrownBy(() -> racingCarService.initCars(duplicateCarNames)).isInstanceOf(RuntimeException.class);
+    }
+
+
+    @Test
+    @DisplayName("차량을 만들기 전에, 이동하면 예외 발생")
     void testMoveCarBeforeMakeCars() {
         RacingCarService racingCarService = new RacingCarService(new MoveCommand(new TestNumber(0)));
         assertThatThrownBy(racingCarService::moveCar).isInstanceOf(RuntimeException.class);
+    }
+
+    private Cars makeCars(List<String> carNames, List<Integer> carPositions) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < carNames.size(); i++) {
+            cars.add(makeCar(carNames.get(i), carPositions.get(i)));
+        }
+
+        return new Cars(cars);
+    }
+
+    private Car makeCar(String name, int position) {
+        Car car = new Car(name);
+
+        for (int i = 0; i < position; i++) {
+            car.move(MoveType.FORWARD);
+        }
+
+        return car;
     }
 }
