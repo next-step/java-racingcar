ad746534d8d821f0205ee0ca008c7f49490a463c
namhyeop <skaguq@gmail.com>
2024-03-15 00:55:31 +0900

    feat : [step4] 심판 클래스 테스트 코드 추가

---
+++ src/test/java/carRace/domain/RefereeTest.java
@@ -0,0 +1,50 @@
+package carRace.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RefereeTest {
+
+    @Test
+    @DisplayName("심판은 가장 멀리 이동한 자동차의 이름을 찾아야한다.")
+    public void Referee_Should_Find_Name_Of_Car_Move_Furthest_Among_Cars() throws Exception{
+        //given
+        Referee sutReferee = new Referee();
+        Car namhyoepCar = new Car("namhyoepCar", 3);
+        Car momoCar = new Car("momoCar", 4);
+        Car nanaCar = new Car("nanaCar", 5);
+        List<Car> cars = Arrays.asList(namhyoepCar, momoCar, nanaCar);
+
+        //when
+        List<String> winners = sutReferee.findWinners(cars);
+
+        //then
+        assertThat(winners).doesNotContain(namhyoepCar.carName());
+        assertThat(winners).doesNotContain(momoCar.carName());
+        assertThat(winners).contains(nanaCar.carName());
+    }
+
+    @Test
+    @DisplayName("심판 가장 높은 점수를 가진 우승자가 2이명 우승자도 2명으로 판단한다.")
+    public void Referee_Should_Declare_Multiple_Winners_If_Scores_Equal() throws Exception{
+        //given
+        Referee sutReferee = new Referee();
+        Car namhyoepCar = new Car("namhyoepCar", 3);
+        Car momoCar = new Car("momoCar", 5);
+        Car nanaCar = new Car("nanaCar", 5);
+        List<Car> cars = Arrays.asList(namhyoepCar, momoCar, nanaCar);
+
+        //when
+        List<String> winners = sutReferee.findWinners(cars);
+
+        //then
+        assertThat(winners).doesNotContain(namhyoepCar.carName());
+        assertThat(winners).contains(momoCar.carName());
+        assertThat(winners).contains(nanaCar.carName());
+    }
+}
