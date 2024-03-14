d39febbafaa63f62466a42b5e67d8f1b74e27cc7
동원 <ai06021@gmail.com>
2024-03-14 18:42:38 +0900

    feat : input cars name

--- src/test/java/carRacingWinner/controller/WinnerRacingController.java
+++ src/test/java/carRacingWinner/controller/WinnerRacingController.java
@@ -1,4 +1,20 @@
 package carRacingWinner.controller;
 
-public class RacingController {
+import java.util.Scanner;
+
+public class WinnerRacingController {
+
+    public void racingStart() {
+
+    }
+
+    public String carsNameInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return input();
+    }
+
+    public String input() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
 }
