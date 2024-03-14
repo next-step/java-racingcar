dccd959225c694ee81c4bdee9f903239f06172f5
namhyeop <skaguq@gmail.com>
2024-03-15 00:57:05 +0900

    feat : [step4] main문 경기 수상식 메소드 추가

--- src/main/java/carRace/Main.java
+++ src/main/java/carRace/Main.java
@@ -2,17 +2,22 @@ package carRace;
 
 import static carRace.domain.Car.createCars;
 
+import carRace.domain.Referee;
 import carRace.view.InputView;
-import carRace.view.OutputView;
 import carRace.domain.Car;
 import carRace.domain.RaceHost;
+import carRace.view.OutputView;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        RaceHost raceHost = new RaceHost(InputView.inputRaceCarCount(), InputView.inputRaceTryCount());
-        List<Car> cars = createCars(raceHost.carCount());
+        String input = InputView.inputCarsName();
+        RaceHost raceHost = new RaceHost(InputView.inputRaceTryCount());
+
+        List<Car> cars = createCars(Arrays.asList(input.split(",")));
         raceHost.playGame(cars);
+        raceHost.progressAwardsCeremony(cars);
     }
 }
