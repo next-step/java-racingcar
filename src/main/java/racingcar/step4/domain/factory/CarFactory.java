198e6381dc781adfb5f2f97d29500826d37737e1
yejikwon <yeeji77@naver.com>
2024-03-14 17:43:10 +0900

    feat: 자동차, 자동차 경주 factory 구현

---
+++ src/main/java/racingcar/step4/domain/factory/CarFactory.java
@@ -0,0 +1,35 @@
+package racingcar.step4.domain.factory;
+
+import racingcar.step4.domain.Car;
+import racingcar.step4.domain.strategy.RandomCarMoveStrategy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static racingcar.step4.domain.CarName.MUST_EXCEED_CERTAIN_LENGTH;
+import static racingcar.step4.domain.CarName.NAME_MINIMUM_LENGTH;
+
+public class CarFactory {
+
+  public static final String NAME_SEPARATOR = ",";
+
+  private CarFactory() {
+  }
+
+  public static List<Car> of(String inputName) {
+    return manufacture(inputName);
+  }
+
+  private static List<Car> manufacture(String inputName) {
+    if (Objects.isNull(inputName) || inputName.length() < NAME_MINIMUM_LENGTH) {
+      throw new IllegalArgumentException(String.format(MUST_EXCEED_CERTAIN_LENGTH, NAME_MINIMUM_LENGTH, inputName));
+    }
+
+    String[] names = inputName.split(NAME_SEPARATOR);
+    return Arrays.stream(names)
+        .map(name -> new Car(name, new RandomCarMoveStrategy()))
+        .collect(Collectors.toList());
+  }
+}
