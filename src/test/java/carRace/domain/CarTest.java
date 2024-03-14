fe5be73e93a91ecc8451f1a7f65cee0df7fdd93a
namhyeop <skaguq@gmail.com>
2024-03-15 00:51:38 +0900

    feat : [step4] 자동차 테스트 코드 추가

--- src/test/java/carRace/domain/CarTest.java
+++ src/test/java/carRace/domain/CarTest.java
@@ -1,7 +1,11 @@
 package carRace.domain;
 
+import static carRace.domain.Car.createCars;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,14 +13,32 @@ import org.junit.jupiter.params.provider.CsvSource;
 class CarTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"2:false", "3:false", "4:true", "5:true"}, delimiter = ':')
     @DisplayName("자동차가 전달받는 값이 4보다 큰 경우 동작해야한다.")
-    public void Car_Should_Not_Move_If_Input_Greater_Than_Four(int input, boolean expected) throws Exception{
+    @CsvSource(value = {"2:0", "3:0", "4:1", "5:1"}, delimiter = ':')
+    public void Car_Should_Not_Move_If_Input_Greater_Than_Four(int input, int expected)
+        throws Exception {
         //given
-        Car sutCar = new Car();
+        Car sutCar = new Car("namhyeopCar");
         //when
-        boolean isMove = sutCar.move(input);
+        sutCar.move(input);
         //then
-        assertThat(isMove).isEqualTo(expected);
+        assertThat(sutCar.moveCount()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("입력 받은 자동차 이름이 자동차를 생성할 때 설정되야 한다")
+    @CsvSource(value = {"namhyeop,jihwan,nara:3", "momo,mimi,uha,momo:4"}, delimiter = ':')
+    public void Input_Car_Name_Should_Be_Set_Car(String input, int expectedSize) {
+        //given
+        List<String> inputNames = Arrays.asList(input.split(","));
+
+        //when
+        List<String> carNames = createCars(inputNames).stream()
+            .map(car -> car.carName())
+            .collect(Collectors.toList());
+
+        //then
+        assertThat(inputNames).isEqualTo(carNames);
+        assertThat(inputNames.size()).isEqualTo(expectedSize);
     }
 }
