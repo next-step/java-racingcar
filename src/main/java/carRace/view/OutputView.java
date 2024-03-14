195b2baa77043b179b813e83d6f1747e45ddcaed
namhyeop <skaguq@gmail.com>
2024-03-15 00:52:35 +0900

    feat : [step4] 추가요건으로 인한 입출력 수정

--- src/main/java/carRace/view/OutputView.java
+++ src/main/java/carRace/view/OutputView.java
@@ -2,21 +2,29 @@ package carRace.view;
 
 import carRace.domain.Car;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class OutputView {
 
     private static final String RACE_MARK = "_";
 
+    private static final String COLON = " : ";
+
     private OutputView() {
     }
 
     public static void printRaceResult(List<Car> cars) {
         cars.stream().forEach(car -> {
+            System.out.print(car.carName() + COLON);
             IntStream.range(0, car.moveCount())
                 .forEach(count -> System.out.print(RACE_MARK));
             System.out.println();
         });
         System.out.println();
+    }
+
+    public static void printWinnersResult(List<String> carNames) {
+        System.out.println(carNames.stream().collect(Collectors.joining(", ", "", "가 최종 우승했습니다.")));
     }
 }
