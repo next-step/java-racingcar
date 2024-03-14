0c4f6321ea01e04f6dfbf10365cb94c3a9a645a8
Danden1 <yongha.kwon12@gmail.com>
2024-03-15 01:40:09 +0900

    feat: 자동차 이름 입력 받는 기능, 자동차 이름과 위치 정보 제공하는 기능, 우승 자동차 출력 기능 추가

--- src/main/java/racingcar/presentation/ResultView.java
+++ src/main/java/racingcar/presentation/ResultView.java
@@ -1,21 +1,28 @@
 package racingcar.presentation;
 
 import racingcar.domain.Car;
+import racingcar.domain.Cars;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
     private final String CAR_POSITION_PRINT_UNIT = "-";
 
-    public void printResult(List<Car> cars) {
+    public void printMoveResult(List<Car> cars) {
         for (Car car : cars) {
-            System.out.println(carPosition(car));
+            System.out.println(car.getName() + " : " + carPosition(car));
         }
         System.out.println();
     }
 
+    public void printWinner(List<Car> winners) {
+        List<String> winnerNames = winners.stream().map(Car::getName).collect(Collectors.toList());
+        System.out.println(String.join(",", winnerNames) + " 가 최종 우승했습니다.");
+    }
+
     private String carPosition(Car car) {
-        return CAR_POSITION_PRINT_UNIT.repeat(car.getPosition()+1);
+        return CAR_POSITION_PRINT_UNIT.repeat(car.getPosition() + 1);
     }
 }
