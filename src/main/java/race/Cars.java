b67f7dbaae1677d9746b92206bd281fa084e80eb
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-14 17:58:40 +0900

    REFACTOR: modified addForward method to private - because this method fix the object value

--- src/main/java/race/Cars.java
+++ src/main/java/race/Cars.java
@@ -8,8 +8,8 @@ public class Cars {
     private static ExtractRandom extractRandom = new ExtractRandom();
     private static final int STANDARD = 4;
 
-    public List<Car> createCar(int number) {
-        for (int i=0; i<number; i++) {
+    public List<Car> createCar(int size) {
+        for (int i=0; i<size; i++) {
             carList.add(new Car(0));
         }
         return carList;
@@ -25,9 +25,10 @@ public class Cars {
         }
     }
 
-    public void addForward(Car car) {
+    private void addForward(Car car) {
         if (canForward(extractRandom.getRandomNumber())) {
             car.location += 1;
         }
     }
+
 }
