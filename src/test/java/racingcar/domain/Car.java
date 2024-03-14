9d50a2daed1fc24c453cf5a2b43e7ce0ac48fbec
동원 <ai06021@gmail.com>
2024-03-14 12:31:09 +0900

    refacotr : 위치정보 확인

--- src/test/java/racingcar/domain/Car.java
+++ src/test/java/racingcar/domain/Car.java
@@ -1,16 +1,13 @@
 package racingcar.domain;
 
 public class Car {
-    private static final String display = "-";
     int location;
 
     public void move() {
         location++;
     }
 
-    public void position() {
-        for (int i = 0; i <= location; i++) {
-            System.out.print(display);
-        }
+    public int location() {
+        return location;
     }
 }
