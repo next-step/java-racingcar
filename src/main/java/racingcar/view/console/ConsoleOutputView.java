d8daa48b1459adeac23f0e5724708b16b7927170
kylekim2123 <kyle2123@naver.com>
2024-03-14 12:07:27 +0900

    [STEP 3] refactor: 자동차 대수와 시도 횟수 입력 메시지를 OutputView에서 관리하도록 변경

--- src/main/java/racingcar/view/console/ConsoleOutputView.java
+++ src/main/java/racingcar/view/console/ConsoleOutputView.java
@@ -8,4 +8,14 @@ public class ConsoleOutputView implements OutputView {
     public void printOneLine(final String text) {
         System.out.println(text);
     }
+
+    @Override
+    public void printCarCountInputMessage() {
+        printOneLine(CAR_COUNT_INPUT_MESSAGE);
+    }
+
+    @Override
+    public void printPlayingCountInputMessage() {
+        printOneLine(PLAYING_COUNT_INPUT_MESSAGE);
+    }
 }
