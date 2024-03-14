cf19e3e5da3831439d430d6bcb70f3eda0eafb5f
동원 <ai06021@gmail.com>
2024-03-14 12:32:20 +0900

    feat : input값 가져오기

--- src/test/java/racingcar/controller/RacingController.java
+++ src/test/java/racingcar/controller/RacingController.java
@@ -10,21 +10,35 @@ import java.util.concurrent.ThreadLocalRandom;
 public class RacingController {
 
 
-    private final RacingService racingService = new RacingService();
+    private final RacingService racingService;
 
-    public RacingController() {
+
+    public RacingController(RacingService racingService) {
+        this.racingService = racingService;
     }
 
     public void racing() {
 
-//        makeCars(count());
-        racingService.makeCars();
+        racingService.makeCars(carCount());
+        racingService.totalTry(moveCount());
 
-        int tryCount = racingService.moveCount();
-        while (tryCount > 0) {
-            racingService.moveCar();
-            tryCount--;
-            System.out.println();
-        }
+        racingService.racing();
+    }
+
+    private int count() {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        if (cnt <= 0) throw new IllegalArgumentException("잘못된 입력값 입니다");
+        return cnt;
+    }
+
+    public int carCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return count();
+    }
+
+    public int moveCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return count();
     }
 }
