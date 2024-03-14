41573c3c66c95d7c3012d8f8ec6118e15b0de14c
Jioo Jung <64303211+jioome@users.noreply.github.com>
2024-03-14 20:43:23 +0900

    fix: modify string test

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -1,7 +1,19 @@
 package calculator;
 
-public class Cal {
-	public static int cal(Object o) {
-		return 0;
+public class Calculator {
+	public static int calculate(String text) {
+		if (text == null || text.isBlank()) {
+			return 0;
+		}
+		String[] values = text.split(",");
+		return sum(values);
+	}
+
+	private static int sum(String[] values) {
+		int result = 0;
+		for (String value : values) {
+			result += Integer.parseInt(value);
+		}
+		return result;
 	}
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,14 +1,25 @@
 package calculator;
 
+import static calculator.Calculator.*;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 import org.junit.jupiter.api.Test;
 
-public class CalTest {
+public class CalculatorTest {
 
 	@Test
-	void null_값(){
-		int result = Cal.cal(null);
-		assertThat(result).isEqualTo(0);
+	void 문자_하나() {
+		assertThat(calculate("1")).isEqualTo(1);
+	}
+
+	@Test
+	void null_값_빈문자() {
+		assertThat(calculate(null)).isEqualTo(0);
+		assertThat(calculate("")).isEqualTo(0);
+	}
+
+	@Test
+	void 컴마_구분자() {
+		assertThat(calculate("1,2")).isEqualTo(3);
 	}
 }
