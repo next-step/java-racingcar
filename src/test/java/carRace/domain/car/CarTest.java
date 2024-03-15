0a94c3297af85a883d2a35ccdb72b5ec00043f57
namhyeop <skaguq@gmail.com>
2024-03-16 03:44:34 +0900

    refactor : [step4] CarNames 테스트 코드 수정

--- src/test/java/carRace/domain/car/CarTest.java
+++ src/test/java/carRace/domain/car/CarTest.java
@@ -1,12 +1,8 @@
 package carRace.domain.car;
 
-import static carRace.domain.car.Car.createCars;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import carRace.domain.randomNumber.RandomNumber;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -19,9 +15,11 @@ class CarTest {
     public void Car_Should_Not_Move_If_Input_Greater_Than_Four(int input, int expected)
         throws Exception {
         //given
-        Car sutCar = new Car("namhyeopCar");
+        Car sutCar = new Car(new CarName("namhyeopCar"));
+
         //when
         sutCar.move(new RandomNumber(input));
+
         //then
         assertThat(sutCar.getMoveDistance().getMoveDistance()).isEqualTo(expected);
     }
@@ -31,15 +29,11 @@ class CarTest {
     @CsvSource(value = {"namhyeop,jihwan,nara:3", "momo,mimi,uha,momo:4"}, delimiter = ':')
     public void Input_Car_Name_Should_Be_Set_Car(String input, int expectedSize) {
         //given
-        List<String> inputNames = Arrays.asList(input.split(","));
+        CarNames sutInputNames = new CarNames(input.split(","));
 
         //when
-        List<String> carNames = createCars(inputNames).stream()
-            .map(car -> car.carName())
-            .collect(Collectors.toList());
-
         //then
-        assertThat(inputNames).isEqualTo(carNames);
-        assertThat(inputNames.size()).isEqualTo(expectedSize);
+        assertThat(sutInputNames.getCarNames().size()).isEqualTo(expectedSize);
+        assertThat(sutInputNames.getCarNames().size()).isEqualTo(expectedSize);
     }
 }
