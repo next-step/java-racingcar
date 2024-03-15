a0c7fafcbb59b7a70f837d0c2301a10299773bc0
namhyeop <skaguq@gmail.com>
2024-03-16 03:45:07 +0900

    refactor : [step4] 불필요한 import문 제거

--- src/main/java/carRace/error/exception/CarNameExceedException.java
+++ src/main/java/carRace/error/exception/CarNameExceedException.java
@@ -1,8 +1,5 @@
 package carRace.error.exception;
 
-import carRace.error.ErrorMessage;
-import java.text.MessageFormat;
-
 public class CarNameExceedException extends RuntimeException{
 
     public CarNameExceedException(String message) {
