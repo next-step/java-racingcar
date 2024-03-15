905b8ae5f386e802f1dae6a83e1d47d817b185aa
namhyeop <skaguq@gmail.com>
2024-03-16 03:41:21 +0900

    refactor : [step4] Car 객체 CarGrous로 수정

--- src/main/java/carRace/domain/RaceHost/RaceHost.java
+++ src/main/java/carRace/domain/RaceHost/RaceHost.java
@@ -1,7 +1,8 @@
 package carRace.domain.RaceHost;
 
 import carRace.domain.Referee.Referee;
-import carRace.domain.car.Car;
+import carRace.domain.car.CarGroups;
+import carRace.domain.car.CarNames;
 import carRace.domain.randomNumber.RandomNumber;
 import carRace.util.RandomNumberGenerator;
 import carRace.view.OutputView;
@@ -19,15 +20,15 @@ public class RaceHost {
         this.raceTryCount = raceTryCount;
     }
 
-    public void playGame(final List<Car> cars) {
+    public void playGame(final CarGroups cars) {
         for (int gameCount = 0; gameCount < raceTryCount; gameCount++) {
             moveCar(cars);
             OutputView.printRaceResult(cars);
         }
     }
 
-    private void moveCar(final List<Car> cars) {
-        List<CompletableFuture<Void>> runningCars = cars.stream()
+    private void moveCar(final CarGroups cars) {
+        List<CompletableFuture<Void>> runningCars = cars.getCarGroups().stream()
             .map(car -> CompletableFuture.runAsync(
                 () -> car.move(new RandomNumber(RandomNumberGenerator.createRandomNumber()))))
             .collect(Collectors.toList());
@@ -45,9 +46,9 @@ public class RaceHost {
         waitCars.join();
     }
 
-    public void progressAwardsCeremony(final List<Car> cars) {
+    public void progressAwardsCeremony(final CarGroups cars) {
         Referee referee = new Referee();
-        List<String> winners = referee.findWinners(cars);
+        CarNames winners = referee.findWinners(cars);
         OutputView.printWinnersResult(winners);
     }
 }
