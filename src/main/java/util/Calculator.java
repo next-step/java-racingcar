e35c855b1caddb9d237f47614cbc0098dc198190
Sangmin <sm9171@nate.com>
2024-03-16 00:06:38 +0900

    refactor : Calculator 클래스를 domain에서 util로 이동

--- src/main/java/util/Calculator.java
+++ src/main/java/util/Calculator.java
@@ -1,4 +1,4 @@
-package domain;
+package util;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import domain.Calculator;
+import util.Calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
