3c9a6baaa8b6680dfc443d602f94bb061fb6741b
namhyeop <skaguq@gmail.com>
2024-03-16 03:40:01 +0900

    refactor : [step4] CarGroups 원시클래스 분리

--- src/main/java/carRace/domain/Referee/Referee.java
+++ src/main/java/carRace/domain/Referee/Referee.java
@@ -1,6 +1,9 @@
 package carRace.domain.Referee;
 
 import carRace.domain.car.Car;
+import carRace.domain.car.CarGroups;
+import carRace.domain.car.CarName;
+import carRace.domain.car.CarNames;
 import carRace.domain.car.MoveDistance;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -8,21 +11,22 @@ import java.util.stream.Collectors;
 
 public class Referee {
 
-    public List<String> findWinners(final List<Car> cars) {
+    public CarNames findWinners(final CarGroups cars) {
         return findCarNameWithWinningScore(cars, findWinningScore(cars));
     }
 
-    private MoveDistance findWinningScore(final List<Car> cars) {
-        return cars.stream()
+    private MoveDistance findWinningScore(final CarGroups cars) {
+        return cars.getCarGroups().stream()
             .map(Car::getMoveDistance)
             .max(MoveDistance::compareTo)
             .orElseThrow(NoSuchElementException::new);
     }
 
-    private List<String> findCarNameWithWinningScore(final List<Car> cars, final MoveDistance moveDistance){
-        return cars.stream()
+    private CarNames findCarNameWithWinningScore(final CarGroups cars, final MoveDistance moveDistance){
+        List<CarName> carNames = cars.getCarGroups().stream()
             .filter(car -> car.getMoveDistance().equals(moveDistance))
-            .map(Car::carName)
+            .map(Car::getCarName)
             .collect(Collectors.toList());
+        return new CarNames(carNames);
     }
 }
--- src/test/java/carRace/domain/RaceHost/RaceHostTest.java
+++ src/test/java/carRace/domain/RaceHost/RaceHostTest.java
@@ -4,6 +4,9 @@ import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import carRace.domain.car.Car;
+import carRace.domain.car.CarGroups;
+import carRace.domain.car.CarName;
+import carRace.domain.car.CarNames;
 import carRace.domain.randomNumber.RandomNumber;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -17,11 +20,13 @@ class RaceHostTest {
         //given
         int raceTryNumber = 1000;
         RaceHost raceHost = new RaceHost(raceTryNumber);
-        List<Car> cars = Car.createCars(asList("namhyeop"));
+        CarGroups carGroups = new CarGroups(new CarNames(asList(new CarName("namhyeop"))));
 
         //when
-        raceHost.playGame(cars);
-        Car raceFinishedCar = cars.stream().findAny().get();
+        raceHost.playGame(carGroups);
+        Car raceFinishedCar = carGroups.getCarGroups().stream()
+            .findAny()
+            .get();
 
         //자동차에 발급받은 랜덤 번호중에서 4보다 큰 경우의 개수와 경주 기록 길이가 같아야 한다.
         int moveDistance = raceFinishedCar
