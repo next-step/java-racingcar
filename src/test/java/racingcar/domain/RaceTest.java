df5fa39418ecfac15dd0066999dbdc2a0930594f
kylekim2123 <kyle2123@naver.com>
2024-03-15 14:43:45 +0900

    [STEP 4] test: 자동차 이름 목록에 중복이 있다면 예외 처리하는 테스트 코드 작성

--- src/test/java/racingcar/domain/RaceTest.java
+++ src/test/java/racingcar/domain/RaceTest.java
@@ -34,4 +34,23 @@ class RaceTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("레이싱 시도 횟수는 자연수만 가능합니다.");
     }
+
+    @Test
+    @DisplayName("자동차 이름 목록을 전달하면 정상적으로 Race 객체를 생성한다.")
+    void new_CarNames_Race() {
+        final String[] carNames = {"kyle", "alex", "haley"};
+
+        assertThat(new Race(carNames, 1))
+                .isNotNull();
+    }
+
+    @Test
+    @DisplayName("Race 객체 생성 시, 중복된 자동차 이름 목록을 전달하면 예외를 던진다.")
+    void new_DuplicatedCarNames_Exception() {
+        final String[] duplicatedCarNames = {"kyle", "kyle", "alex"};
+
+        assertThatThrownBy(() -> new Race(duplicatedCarNames, 1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 중복될 수 없습니다.");
+    }
 }
