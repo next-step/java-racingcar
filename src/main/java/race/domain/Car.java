1d624cfb4e495f09220718e84d5dc40aaec62f06
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-15 15:38:05 +0900

    refactor car가 이름에 대한 검증 책임을 갖게 하라

--- src/main/java/race/domain/Car.java
+++ src/main/java/race/domain/Car.java
@@ -6,6 +6,14 @@ public class Car {
     private final String name;
 
     public Car(int position, String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("공백없이 정확한 이름을 입력해주세요.");
+        }
+
+        if (position < 0) {
+            throw new IllegalArgumentException(String.format("(%s)은 음수입니다. 차의 위치(position)는 0이상의 양의 정수만 허용됩니다.", position));
+        }
+
         this.position = position;
         this.name = name;
     }
