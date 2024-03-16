f4d6b5f17b11172e5924b3af1c5724d90d82f2c8
Jioo Jung <64303211+jioome@users.noreply.github.com>
2024-03-16 17:56:51 +0900

    step2: 문자열 덧셈 계산기

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -1,19 +1,42 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.platform.commons.util.StringUtils;
+
 public class Calculator {
 	public static int calculate(String text) {
-		if (text == null || text.isBlank()) {
+		if (StringUtils.isBlank(text)) {
 			return 0;
 		}
-		String[] values = text.split(",");
+		String[] values = splitValues(text);
 		return sum(values);
+	}
+
+	private static String[] splitValues(String text) {
+		String[] values;
+
+		Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+		if (matcher.find()){
+			String customDelimiter = matcher.group(1);
+			values  = matcher.group(2).split(customDelimiter);
+		} else {
+			values = text.split(",|:");
+		}
+		return values;
 	}
 
 	private static int sum(String[] values) {
 		int result = 0;
 		for (String value : values) {
-			result += Integer.parseInt(value);
+			int num = Integer.parseInt(value);
+			if (num < 0) {
+				throw new RuntimeException();
+			}
+			result += num;
 		}
 		return result;
 	}
+
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -10,6 +10,7 @@ public class CalculatorTest {
 	@Test
 	void 문자_하나() {
 		assertThat(calculate("1")).isEqualTo(1);
+		assertThat(calculate("2")).isEqualTo(2);
 	}
 
 	@Test
@@ -21,5 +22,21 @@ public class CalculatorTest {
 	@Test
 	void 컴마_구분자() {
 		assertThat(calculate("1,2")).isEqualTo(3);
+	}
+
+	@Test
+	void 콜론_구분자() {
+		assertThat(calculate("3:7")).isEqualTo(10);
+	}
+
+	@Test
+	void 커스텀_구분자() {
+		assertThat(calculate("//;\n1;2;3")).isEqualTo(6);
+	}
+
+	@Test
+	void 음수_예외() throws Exception{
+		assertThatThrownBy(() -> calculate("-1,2,3"))
+			.isInstanceOf(RuntimeException.class);
 	}
 }
