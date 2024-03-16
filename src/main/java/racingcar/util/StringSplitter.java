3ceba2a8840ea065f5f36c55edee1769ec9eeabc
kylekim2123 <kyle2123@naver.com>
2024-03-16 16:43:22 +0900

    [STEP 4] refactor: 구분자를 기준으로 자르는 로직에 대해, 구분자를 매개변수로 받아 사용할 수 있도록 변경

--- src/main/java/racingcar/util/StringSplitter.java
+++ src/main/java/racingcar/util/StringSplitter.java
@@ -3,15 +3,14 @@ package racingcar.util;
 public final class StringSplitter {
 
     private static final String BLANK_TEXT_MESSAGE = "빈문자열, 공백, 개행은 구분자를 이용해 자를 수 없습니다.";
-    private static final String DELIMITER = ",";
 
     private StringSplitter() {
     }
 
-    public static String[] split(final String text) {
+    public static String[] split(final String text, final String delimiter) {
         validateTextIsNotBlank(text);
 
-        return text.split(DELIMITER);
+        return text.split(delimiter);
     }
 
     private static void validateTextIsNotBlank(final String text) {
--- src/test/java/racingcar/util/StringSplitterTest.java
+++ src/test/java/racingcar/util/StringSplitterTest.java
@@ -16,17 +16,18 @@ class StringSplitterTest {
     @ParameterizedTest
     @MethodSource("texts")
     @DisplayName("특정 구분자를 기준으로 문자열을 구분하여 목록을 생성한다.")
-    void split_Text_Strings(final String text, final String[] expectedStrings) {
-        final String[] actualStrings = StringSplitter.split(text);
+    void split_Text_Strings(final String text, final String delimiter, final String[] expectedStrings) {
+        final String[] actualStrings = StringSplitter.split(text, delimiter);
 
         assertThat(actualStrings).isEqualTo(expectedStrings);
     }
 
     static Stream<Arguments> texts() {
         return Stream.of(
-                Arguments.of("kyle,alex", new String[] {"kyle", "alex"}),
-                Arguments.of("kyle,alex,haley", new String[] {"kyle", "alex", "haley"}),
-                Arguments.of("kyle", new String[] {"kyle"})
+                Arguments.of("kyle,alex", ",", new String[] {"kyle", "alex"}),
+                Arguments.of("kyle:alex", ":", new String[] {"kyle", "alex"}),
+                Arguments.of("kyle,alex,haley", ",", new String[] {"kyle", "alex", "haley"}),
+                Arguments.of("kyle", ":", new String[] {"kyle"})
         );
     }
 
@@ -34,7 +35,7 @@ class StringSplitterTest {
     @ValueSource(strings = {"", " ", "     ", "\n"})
     @DisplayName("문자열 자체가 빈문자열, 공백, 개행인 경우 예외를 던진다.")
     void split_Blank_Exception(final String blankText) {
-        Assertions.assertThatThrownBy(() -> StringSplitter.split(blankText))
+        Assertions.assertThatThrownBy(() -> StringSplitter.split(blankText, ","))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("빈문자열, 공백, 개행은 구분자를 이용해 자를 수 없습니다.");
     }
