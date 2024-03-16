9994631fcaa6f56c2fe3e870297fce9c582421a0
kylekim2123 <kyle2123@naver.com>
2024-03-16 20:43:53 +0900

    [STEP 4] test: MovementStrategy 관련 테스트 코드 추가

---
+++ src/test/java/racingcar/domain/MovementStrategyTest.java
@@ -0,0 +1,37 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.TestRacingCarConfig.basicRule;
+import static racingcar.TestRacingCarConfig.moveForwardNumberGenerator;
+import static racingcar.TestRacingCarConfig.stopNumberGenerator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MovementStrategyTest {
+
+    @Test
+    @DisplayName("자동차 이동 규칙과 조건수 생성기를 전달하여 자동차 이동 전략 객체를 생성한다.")
+    void new_RuleAndNumberGenerator_MovementStrategy() {
+        assertThat(new MovementStrategy(basicRule(), moveForwardNumberGenerator()))
+                .isNotNull();
+    }
+
+    @Test
+    @DisplayName("canMoveForward 메서드는 자동차 이동 전략에 따라 전진 조건에 해당하면, true를 반환한다.")
+    void canMoveForward_ForwardCondition_True() {
+        final MovementStrategy movementStrategy = new MovementStrategy(basicRule(), moveForwardNumberGenerator());
+
+        assertThat(movementStrategy.canMoveForward())
+                .isTrue();
+    }
+
+    @Test
+    @DisplayName("canMoveForward 메서드는 자동차 이동 전략에 따라 정지 조건에 해당하면, false를 반환한다.")
+    void canMoveForward_StopCondition_False() {
+        final MovementStrategy movementStrategy = new MovementStrategy(basicRule(), stopNumberGenerator());
+
+        assertThat(movementStrategy.canMoveForward())
+                .isFalse();
+    }
+}
