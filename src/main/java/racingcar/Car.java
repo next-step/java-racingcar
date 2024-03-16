9127473835dc2356a95d01c5652be841da651463
hhhhhwi <aab555586@gmail.com>
2024-03-16 22:23:55 +0900

    refactor: Car 객체의 움직임 대신 위치의 멤버 변수를 갖도록 수정

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,16 +3,19 @@ package racingcar;
 import java.util.Random;
 
 public class Car {
-	private boolean[] moving;
-	public Car(int numberOfMove) {
-		moving = new boolean[numberOfMove];
+	private int position;
+
+	public Car() {
+		position = 0;
 	}
 
-	public void moveOrStop(int trial) {
-		moving[trial] = new Random().nextInt(10) >= 4;
+	public void move() {
+		if(new Random().nextInt(10) >= 4) {
+			position++;
+		}
 	}
 
-	public boolean[] getMoving() {
-		return moving;
+	public int getPosition() {
+		return position;
 	}
 }
