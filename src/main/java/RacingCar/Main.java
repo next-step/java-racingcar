98fcdd942d68b0edf0ec7a12578de90d94bcebb4
shgpwn <nhn@AL01555617.local>
2024-03-16 18:37:20 +0900

    refactor : [step3] Controller를 담당하는 로직 분리

---
+++ src/main/java/RacingCar/Main.java
@@ -0,0 +1,9 @@
+package RacingCar;
+
+import RacingCar.Controller.RacingCarController;
+
+public class Main {
+    public static void main(String[] args) {
+        new RacingCarController().run();
+    }
+}
