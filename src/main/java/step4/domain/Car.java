a2896856def26de57dd460889ed10c0c0d60f545
kwanik-kor <rhksdlr134@naver.com>
2024-03-17 14:45:44 +0900

    feat: 자동차 이름 생성자 유효성 검증 테스트 추가

--- src/main/java/step4/domain/Car.java
+++ src/main/java/step4/domain/Car.java
@@ -3,6 +3,8 @@ package step4.domain;
 import step4.view.result.CarMovementResult;
 
 public class Car {
+    private static final int CAR_NAME_MAX_LENGTH = 5;
+
     private final String name;
     private int moveCount;
 
@@ -11,7 +13,14 @@ public class Car {
     }
 
     private Car(String name) {
+        assertNameLengthUnderFive(name);
         this.name = name;
+    }
+
+    private void assertNameLengthUnderFive(String name) {
+        if (CAR_NAME_MAX_LENGTH < name.length()) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 
     public void move(Moving movingStrategy) {
--- src/test/java/step4/CarTest.java
+++ src/test/java/step4/CarTest.java
@@ -1,5 +1,6 @@
 package step4;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import step4.domain.Car;
 import step4.domain.Moving;
@@ -7,9 +8,18 @@ import step4.domain.RandomMoving;
 import step4.view.result.CarMovementResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarTest {
 
+    @DisplayName("자동차는 5자 초과의 이름을 가질 수 없다.")
+    @Test
+    void constructor() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Car.withName("다섯글자다다"));
+    }
+
+    @DisplayName("차량 움직임 테스트")
     @Test
     void move() {
         final Car car = Car.withName("test");
