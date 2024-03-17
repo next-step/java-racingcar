5ba598197a7c1f11d325f01161e62647c2d2d2b4
hbae <alex950107@gmail.com>
2024-03-17 20:17:31 +0900

    refactor: [step3] 인스턴스 매번 호출 이슈 수정(#STEP3)

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class RandomNumberGenerator {
 
     private static final int RANDOM_INT_MAX_LIMIT = 10;
+    private static final Random random = new Random();
     public static Integer getRandomNumber() {
-        Random random = new Random();
         return random.nextInt(RANDOM_INT_MAX_LIMIT);
     }
 }
