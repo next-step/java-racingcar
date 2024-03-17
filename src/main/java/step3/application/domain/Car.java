c54d08c0cf068ba89acd97ca0e94fb24d27a3fac
jy.lee <ljyda214@gmail.com>
2024-03-17 14:35:49 +0900

    CHG : 필드 타입 변경 및 메소드 반환 타입 변경

--- src/main/java/step3/application/domain/Car.java
+++ src/main/java/step3/application/domain/Car.java
@@ -1,33 +1,34 @@
 package step3.application.domain;
 
 import error.ErrorMessage;
+import step3.application.domain.model.MovementLog;
 
 public class Car {
 
-    private final int id;
+    private final String name;
     private int location;
     public static final int INIT_LOCATION = 0;
 
-    public Car(int id) {
-        validateId(id);
-        this.id = id;
-        this.location = INIT_LOCATION;
+    public Car(String name) {
+        this(name, INIT_LOCATION);
     }
 
-    public int verifyLocation() {
-        return location;
+    public Car(String  name, int position) {
+        validateName(name);
+        this.name = name;
+        this.location = position;
     }
 
-    private void validateId(int id) {
-        if (id < 0) {
+    private void validateName(String name) {
+        if (name.isBlank() || name.length() > 5) {
             throw new IllegalArgumentException(ErrorMessage.ERR_INVALID_NUMBER.print());
         }
     }
 
-    public int move(boolean isMovable) {
+    public MovementLog move(boolean isMovable) {
         if (isMovable) {
             this.location += 1;
         }
-        return this.location;
+        return new MovementLog(this.name, this.location);
     }
 }
