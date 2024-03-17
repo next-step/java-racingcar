8113eff80e85d9e03fbf9fd6382e33bfa8f0a236
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-17 14:58:25 +0900

    CHORE : to avoid creating CarFactory objects

--- src/test/java/race/domain/CarFactoryTest.java
+++ src/test/java/race/domain/CarFactoryTest.java
@@ -4,37 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class CarFactoryTest {
-//    @Test
-//    void 자동차_리스트_생성() {
-//        CarFactory carFactory = new CarFactory();
-//        int carCount = 1;
-//        List<Car> carList = carFactory.createCars(carCount);
-//        assertThat(carList).hasSize(1);
-//    }
+    @Test
+    void 자동차_리스트_생성() {
+        List<Car> carList = CarFactory.createCars("pobi,nana,kai");
+        assertThat(carList).hasSize(3);
+    }
 
     @Test
     void split() {
-        CarFactory carFactory = new CarFactory();
-        List<String> value = carFactory.splitName("pobi,nana,kai");
+        List<String> value = CarFactory.splitName("pobi,nana,kai");
         assertThat(value).contains("pobi");
         assertThat(value).containsExactly("pobi","nana","kai");
-    }
-
-    @Test
-    void 자동차_이름_길이_성공() {
-        CarFactory carFactory = new CarFactory();
-        String name = "abcde";
-        assertThat(carFactory.checkNameLength(name)).isTrue();
-    }
-
-    @Test
-    void 자동차_이름_길이_실패() {
-        CarFactory carFactory = new CarFactory();
-        String name = "abcdef";
-        assertThat(carFactory.checkNameLength(name)).isFalse();
     }
 
 }
