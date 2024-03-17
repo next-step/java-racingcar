5c9ffc32ee66c9f82a2493b6f62b9597dc6f106c
hbae <alex950107@gmail.com>
2024-03-17 18:27:48 +0900

    feat: [step3] 사용자 입력값 구현(#STEP3)

---
+++ src/main/java/racing/RacingApplication.java
@@ -0,0 +1,15 @@
+package racing;
+
+import java.util.Scanner;
+
+public class RacingApplication {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carCount = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int tryCount = scanner.nextInt();
+    }
+
+}
