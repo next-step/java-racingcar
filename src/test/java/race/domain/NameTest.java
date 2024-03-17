0918057c2babb4d5ae53aec79d6572fff76af718
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-17 23:20:23 +0900

    Test : Create NameTest for creating Name object, check the length of name, check that name is null

--- src/test/java/race/domain/NameTest.java
+++ src/test/java/race/domain/NameTest.java
@@ -1,2 +1,28 @@
-package race.domain;public class NameTest {
+package race.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NameTest {
+    @Test
+    void name_생성() {
+        Name name = new Name("pobi");
+        assertThat(name).isEqualTo(new Name("pobi"));
+    }
+
+    @Test
+    void name_null_값() {
+        assertThatThrownBy(
+                () -> new Name("")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void name_길이() {
+        assertThatThrownBy(
+                () -> new Name("abcdef")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
 }
