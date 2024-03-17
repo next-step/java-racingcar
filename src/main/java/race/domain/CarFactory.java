fc25aec652411e26cc41bd2edbd7741532fdea9a
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-17 14:51:46 +0900

    CHORE : modified all method to static method

--- src/main/java/race/domain/CarFactory.java
+++ src/main/java/race/domain/CarFactory.java
@@ -7,29 +7,17 @@ import java.util.List;
 public class CarFactory {
 
     private static final String COMMA = ",";
-    private static final int LENGTH_STANDARD = 5;
 
-    public List<Car> createCars(String names) {
+    public static List<Car> createCars(String names) {
         List<Car> carList = new ArrayList<>();
 
         for (String name : splitName(names)) {
-            lengthException(name);
             carList.add(new Car(name,0));
         }
         return carList;
     }
 
-    public List<String> splitName(String names) {
+    public static List<String> splitName(String names) {
         return new ArrayList<>(Arrays.asList(names.split(COMMA)));
-    }
-
-    public void lengthException(String name) {
-        if (!checkNameLength(name)) {
-            throw new IllegalArgumentException("자동차 이름 길이는 5 이하여야 합니다.");
-        }
-    }
-
-    public boolean checkNameLength(String name) {
-        return name.length() <= LENGTH_STANDARD;
     }
 }
