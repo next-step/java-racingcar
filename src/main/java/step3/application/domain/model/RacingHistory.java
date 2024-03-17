6ea1636d90de1003fd428da87881a988a634738e
jy.lee <ljyda214@gmail.com>
2024-03-17 14:54:06 +0900

    TEST : RaceGame 테스트 코드 추가

--- src/main/java/step3/application/domain/model/RacingHistory.java
+++ src/main/java/step3/application/domain/model/RacingHistory.java
@@ -1,6 +1,7 @@
 package step3.application.domain.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class RacingHistory {
     private final List<OneMoveRecord> racingRecords;
@@ -11,5 +12,19 @@ public class RacingHistory {
 
     public List<OneMoveRecord> getRacingRecords() {
         return racingRecords;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (!(o instanceof RacingHistory)) return false;
+        RacingHistory that = (RacingHistory) o;
+        return Objects.equals(racingRecords, that.racingRecords);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(racingRecords);
     }
 }
---
+++ src/test/java/step3/application/domain/RaceGameTest.java
@@ -0,0 +1,50 @@
+package step3.application.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import step3.application.domain.factory.MovablePredicatedGenerator;
+import step3.application.domain.model.MovementLog;
+import step3.application.domain.model.OneMoveRecord;
+import step3.application.domain.model.RacingHistory;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class RaceGameTest {
+
+    public static Stream<Arguments> move() {
+        List<MovementLog> movementLogs = List.of(
+                new MovementLog("pobi", 1),
+                new MovementLog("crong", 1),
+                new MovementLog("honux", 2)
+        );
+        return Stream.of(
+                arguments(
+                        List.of(
+                                new Car("pobi", 1),
+                                new Car("crong", 1),
+                                new Car("honux", 2)
+                        ),
+                        new RacingHistory(
+                                List.of(
+                                        new OneMoveRecord(movementLogs),
+                                        new OneMoveRecord(movementLogs),
+                                        new OneMoveRecord(movementLogs)
+                                )
+                        )
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("move")
+    void moveCars(List<Car> cars, RacingHistory expected) {
+        RaceGame raceGame = new RaceGame(cars, new MovablePredicatedGenerator());
+        RacingHistory result = raceGame.startRace(3);
+        assertThat(result).isEqualTo(expected);
+    }
+}
