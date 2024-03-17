fd3737864f853d97228cc82d635f619b725424df
jy.lee <ljyda214@gmail.com>
2024-03-17 14:43:35 +0900

    CHG : 클래스의 인스턴스 변수 타입 변경 및 테스트 코드 변경

--- src/main/java/step3/application/domain/model/OneMoveRecord.java
+++ src/main/java/step3/application/domain/model/OneMoveRecord.java
@@ -1,15 +1,29 @@
 package step3.application.domain.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class OneMoveRecord {
-    private final List<Integer> carLocation;
+    private final List<MovementLog> carLocation;
 
-    public OneMoveRecord(List<Integer> carLocation) {
+    public OneMoveRecord(List<MovementLog> carLocation) {
         this.carLocation = carLocation;
     }
 
-    public List<Integer> getCarLocation() {
+    public List<MovementLog> getCarLocation() {
         return carLocation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OneMoveRecord)) return false;
+        OneMoveRecord that = (OneMoveRecord) o;
+        return Objects.equals(carLocation, that.carLocation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carLocation);
     }
 }
--- src/test/java/step3/application/domain/RacingCarsTest.java
+++ src/test/java/step3/application/domain/RacingCarsTest.java
@@ -1,37 +1,45 @@
 package step3.application.domain;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import step3.application.domain.factory.MovablePredicatedGenerator;
+import step3.application.domain.model.MovementLog;
 import step3.application.domain.model.OneMoveRecord;
-import step3.application.generator.NumberPredicatedGenerator;
+import step3.application.generator.MovableGenerator;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class RacingCarsTest {
 
-    private RacingCars racingCars;
-    private OneMoveRecord oneMoveRecord;
-
-    @BeforeEach
-    void setUp() {
-        racingCars = new RacingCars(
-                List.of(
-                        new Car(1),
-                        new Car(2),
-                        new Car(3),
-                        new Car(4)
-
-                ),
-                new NumberPredicatedGenerator()
+    public static Stream<Arguments> move() {
+        return Stream.of(
+                arguments(
+                        List.of(
+                                new Car("pobi", 1),
+                                new Car("crong", 1),
+                                new Car("honux", 2)
+                        ),
+                        new OneMoveRecord(List.of(
+                                new MovementLog("pobi", 1),
+                                new MovementLog("crong", 1),
+                                new MovementLog("honux", 2)
+                        ))
+                )
         );
     }
 
-    @Test
-    void moveCars() {
-        OneMoveRecord result = racingCars.moveCars();
-        assertThat(result.getCarLocation())
-                .containsExactlyInAnyOrderElementsOf(List.of(0, 1, 0, 1));
+    public final MovableGenerator generator = new MovablePredicatedGenerator();
+
+    @ParameterizedTest
+    @MethodSource("move")
+    void test(List<Car> cars, OneMoveRecord result) {
+        RacingCars racingCars = new RacingCars(cars, generator);
+        OneMoveRecord oneMoveRecord = racingCars.moveCars();
+        assertThat(oneMoveRecord).isEqualTo(result);
     }
 }
