6faf228d85d90cd6606c9d5113165ae65fdae050
hbae <alex950107@gmail.com>
2024-03-17 19:39:29 +0900

    refactor: [step3] RacingApplication에서 Scanner 의존성 분리(#STEP3)

--- src/main/java/racing/RacingApplication.java
+++ src/main/java/racing/RacingApplication.java
@@ -1,15 +1,12 @@
 package racing;
 
-import java.util.Scanner;
+import ui.InputView;
 
 public class RacingApplication {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        int carCount = scanner.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int tryCount = scanner.nextInt();
+        int carCount = InputView.getCarCount();
+        int tryCount = InputView.getTryCount();
         RacingService.race(carCount, tryCount);
     }
 
