1802ad159d651e80fa412dbe997188500375f8ff
jy.lee <ljyda214@gmail.com>
2024-03-17 14:37:54 +0900

    FEAT : 신규 타입 추가

--- src/main/java/error/ErrorMessage.java
+++ src/main/java/error/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     ERR_NUMBER_RANGE_MESSAGE ("음수값이 입력되었습니다. 양수를 입력해 주세요."),
     ERR_NUMBER_FORMAT_MESSAGE("숫자외의 값이 입력되었습니다. 숫자를 입력해 주세요") ,
-    ERR_INVALID_NUMBER("적절하지 못한 값이 입력되었습니다.");
+    ERR_INVALID_NUMBER("적절하지 못한 값이 입력되었습니다."),
+    ERR_INVALID_NAME("");
 
     private final String message;
 
