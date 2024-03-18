9e9451b006420e57ee46c5bd39e3084e31c8cf9e
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-18 09:44:48 +0900

    test : delete getMax test

--- src/test/java/race/domain/CarTest.java
+++ src/test/java/race/domain/CarTest.java
@@ -1,7 +1,7 @@
 package race.domain;
 
 import org.junit.jupiter.api.Test;
-import race.constant.Definition;
+import race.fixture.CarFixtures;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,16 +9,11 @@ public class CarTest {
 
     @Test
     void 전진() {
-        assertThat(Definition.POBI.canForward(4)).isTrue();
+        assertThat(CarFixtures.POBI.canForward(4)).isTrue();
     }
 
     @Test
     void 정지() {
-        assertThat(Definition.POBI.canForward(3)).isFalse();
-    }
-
-    @Test
-    void 최댓값() {
-        assertThat(Math.max(1, 2)).isEqualTo(2);
+        assertThat(CarFixtures.POBI.canForward(3)).isFalse();
     }
 }
