a771461f4c470447c536e6ab7b7a9f90b855ef1e
Sangmin <sm9171@nate.com>
2024-03-18 15:08:09 +0900

    feat : Position vo에 equals,hashcode 구현

--- src/main/java/domain/Position.java
+++ src/main/java/domain/Position.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Position {
 	private int position;
 
@@ -16,5 +18,18 @@ public class Position {
 
 	public Integer getPosition() {
 		return position;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		final Position position1 = (Position) o;
+		return position == position1.position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(position);
 	}
 }
