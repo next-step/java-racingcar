6282f17758196471b522a9959c1e24e47f2a5f7c
kylekim2123 <kyle2123@naver.com>
2024-03-18 18:15:24 +0900

    [STEP 4] refactor: 화면 출력 객체의 타입을 구현 타입에서 인터페이스 타입으로 변경

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -9,11 +9,12 @@ import static racingcar.config.RacingCarConfig.rule;
 import racingcar.controller.RacingGame;
 import racingcar.domain.MovementStrategy;
 import racingcar.view.RacingScreen;
+import racingcar.view.RacingView;
 
 public class RacingCarApplication {
 
     public static void main(String[] args) {
-        final RacingScreen racingView = new RacingScreen(inputView(), outputView(), resultFormatter());
+        final RacingView racingView = new RacingScreen(inputView(), outputView(), resultFormatter());
         final MovementStrategy movementStrategy = new MovementStrategy(rule(), numberGenerator());
 
         new RacingGame(racingView, movementStrategy).play();
