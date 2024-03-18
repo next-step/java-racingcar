9d392c803053db6315bd67cfe1e281e57d64c6bd
dongock <okdong23@naver.com>
2024-03-18 22:03:04 +0900

    refactor: [1-10] Winners 정리

--- src/main/java/racingcar/model/Winners.java
+++ src/main/java/racingcar/model/Winners.java
@@ -14,7 +14,6 @@ public class Winners {
     }
 
     private void choiceWinner(Cars cars) {
-
         int maxDistance = cars.stream()
                 .mapToInt(Car::getDistance)
                 .max()
@@ -23,13 +22,6 @@ public class Winners {
         for (Car car : cars) {
             pickWinners(car, maxDistance);
         }
-    }
-
-    private int compareDistance(int carDistance, int distance) {
-        if (carDistance >= distance) {
-            return carDistance;
-        }
-        return distance;
     }
 
     private void pickWinners(Car car, int distance) {
