a8dfa98069c8b72ea14857fe30d443562f205174
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-18 09:48:47 +0900

    TEST : rewrite the code

--- src/test/java/race/domain/NameTest.java
+++ src/test/java/race/domain/NameTest.java
@@ -2,8 +2,7 @@ package race.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class NameTest {
     @Test
@@ -14,15 +13,15 @@ public class NameTest {
 
     @Test
     void name_null_값() {
-        assertThatThrownBy(
-                () -> new Name("")
-        ).isInstanceOf(IllegalArgumentException.class);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Name(""))
+                .withMessageMatching("자동차 이름이 비어있습니다.");
     }
 
     @Test
     void name_길이() {
-        assertThatThrownBy(
-                () -> new Name("abcdef")
-        ).isInstanceOf(IllegalArgumentException.class);
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Name("abcdef"))
+                .withMessageMatching("자동차 이름의 길이는 5 이하여야 합니다.");
     }
 }
--- src/test/java/race/domain/WinnerTest.java
+++ src/test/java/race/domain/WinnerTest.java
@@ -1,7 +1,7 @@
 package race.domain;
 
 import org.junit.jupiter.api.Test;
-import race.constant.Definition;
+import race.fixture.CarFixtures;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +12,7 @@ public class WinnerTest {
     @Test
     void 우승자_찾기() {
         Winner winner = new Winner();
-        List<Car> carList = Arrays.asList(Definition.A, Definition.B, Definition.C);
+        List<Car> carList = Arrays.asList(CarFixtures.A, CarFixtures.B, CarFixtures.C);
         List<String> winnerNames = winner.findWinner(carList);
         assertThat(winnerNames).containsExactlyInAnyOrder("a","b");
     }
@@ -20,7 +20,7 @@ public class WinnerTest {
     @Test
     void 최댓값_찾기() {
         Winner winner = new Winner();
-        List<Car> carList = List.of(Definition.A, Definition.D, Definition.C);
+        List<Car> carList = List.of(CarFixtures.A, CarFixtures.D, CarFixtures.C);
 
         assertThat(winner.findMax(carList)).isEqualTo(4);
     }
