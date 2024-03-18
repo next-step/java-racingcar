6deb61107b4599bffe155a0127bfd1e97f0b63b5
dongock <okdong23@naver.com>
2024-03-18 23:11:46 +0900

    refactor: [1-12] Name Vo 생성

---
+++ src/main/java/racingcar/domain/Name.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+public class Name {
+    private final String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
