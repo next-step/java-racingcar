08d473e5e1127b3732f6aea8f7d1a0df103c828b
hhhhhwi <aab555586@gmail.com>
2024-03-18 23:32:32 +0900

    refactor: 자동차 개수 입력 -> 자동차 이름들을 입력하는 것으로 inputView 변경

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,17 +3,15 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
-	private static final String NUMBER_OF_CAR_INPUT_GUIDE = "자동차 대수는 몇 대 인가요?";
-	private static final String NUMBER_OF_TRIAL_INPUT_GUIDE = "시도할 회수는 몇 회 인가요?";
 	private static final Scanner scanner = new Scanner(System.in);
 
-	public static int inputNumberOfCar() {
-		System.out.println(NUMBER_OF_CAR_INPUT_GUIDE);
-		return scanner.nextInt();
+	public static String inputNamesOfCar() {
+		System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+		return scanner.nextLine();
 	}
 
 	public static int inputNumberOfTrial() {
-		System.out.println(NUMBER_OF_TRIAL_INPUT_GUIDE);
+		System.out.println("시도할 회수는 몇 회 인가요?");
 		return scanner.nextInt();
 	}
 }
