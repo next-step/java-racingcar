0c1928c038c640e8a902603ea5e959800c4a999c
gjtmdwns <gjtmdwns0815@naver.com>
2024-03-18 10:47:20 +0900

    REFACTOR : run RacingMain by RacingController

--- src/main/java/race/RacingMain.java
+++ src/main/java/race/RacingMain.java
@@ -1,2 +1,9 @@
-package race;public class RacingMain {
+package race;
+
+import race.controller.RacingController;
+
+public class RacingMain {
+    public static void main(String[] args) {
+        new RacingController().startRace();
+    }
 }
