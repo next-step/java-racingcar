51aecbe9a70c534e247096307c94467d97340e37
westkitekim <westkite418@gmail.com>
2024-03-20 08:46:11 +0900

    feat: 입력부 출력 메세지 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
     }
 
     public static int inputCarCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return scanner.nextInt();
     }
 
