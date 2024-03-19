3ad0148a1a4012dbbf5efdbfd4d59e250189abc6
nahyun0423 <skgus0423@naver.com>
2024-03-19 12:54:19 +0900

    chore : 코드 정리

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,13 +1,9 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Random;
-
+-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
-
-    private Random random = new Random();
 
     @Test
     @DisplayName("랜덤값이_4이상이면_move증가")
