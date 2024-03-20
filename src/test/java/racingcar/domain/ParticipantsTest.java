de2feff225074510653976a5c055b23d372b8d32
westkitekim <westkite418@gmail.com>
2024-03-20 15:45:26 +0900

    test: 참가자 이름 중복 테스트코드 작성

---
+++ src/test/java/racingcar/domain/ParticipantsTest.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ParticipantsTest {
+    @Test
+    @DisplayName("참가자 이름 중복 테스트")
+    void createParticipants_duplicate() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Participants.from("charmander,pikachu,charmander"))
+                .withMessageMatching("중복된 이름이 존재합니다. 참가자 이름을 다시 확인해주세요.");
+    }
+}
