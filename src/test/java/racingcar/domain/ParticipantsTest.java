9dce0de1f806510f012551dbf5d586b16f957a37
westkitekim <westkite418@gmail.com>
2024-03-20 15:58:36 +0900

    test: Participants 입력 검증(중복, 미입력) 테스트코드 작성

--- src/test/java/racingcar/domain/ParticipantsTest.java
+++ src/test/java/racingcar/domain/ParticipantsTest.java
@@ -2,12 +2,24 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.*;
 
 class ParticipantsTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("전체 참가자 이름 미입력시 에러를 던진다.")
+    void from_이름미입력(String name) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Participant.from(name))
+                .withMessageMatching("전체 참가자 이름을 입력하세요.");
+    }
+
     @Test
-    @DisplayName("참가자 이름 중복 테스트")
+    @DisplayName("전체 참가자 이름 중복 테스트")
     void createParticipants_duplicate() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> Participants.from("charmander,pikachu,charmander"))
