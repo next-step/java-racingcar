90ff587b5821675dddecad6012973fdcbc0ff88e
westkitekim <westkite418@gmail.com>
2024-03-20 15:51:36 +0900

    style: 클래스명 변경에 따른 에러메세지 변경

--- src/main/java/racingcar/domain/Participant.java
+++ src/main/java/racingcar/domain/Participant.java
@@ -12,11 +12,11 @@ public class Participant {
 
     private static void validate(String name) {
         if (null == name || name.length() == 0 || name.trim().length() == 0) {
-            throw new IllegalArgumentException("자동차 이름을 입력하세요.");
+            throw new IllegalArgumentException("개인 참가자 이름을 입력하세요.");
         }
 
         if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("자동차 이름의 길이는 " + MAX_NAME_LENGTH + "글자를 초과할 수 없습니다.");
+            throw new IllegalArgumentException("개인 참가자 이름의 길이는 " + MAX_NAME_LENGTH + "글자를 초과할 수 없습니다.");
         }
     }
 }
--- src/test/java/racingcar/domain/ParticipantTest.java
+++ src/test/java/racingcar/domain/ParticipantTest.java
@@ -10,18 +10,18 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 class ParticipantTest {
     @ParameterizedTest
     @NullAndEmptySource
-    @DisplayName("자동차 이름 미입력시 에러를 던진다.")
+    @DisplayName("개인 참가자 이름 미입력시 에러를 던진다.")
     void from_이름미입력(String name) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> Participant.from(name))
-                .withMessageMatching("자동차 이름을 입력하세요.");
+                .withMessageMatching("개인 참가자 이름을 입력하세요.");
     }
 
     @Test
-    @DisplayName("자동차 이름의 길이가 5를 초과하면 에러를 던진다.")
+    @DisplayName("개인 참가자 이름의 길이가 5를 초과하면 에러를 던진다.")
     void from_글자수제한() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> Participant.from("CHARMANDER"))
-                .withMessageMatching("자동차 이름의 길이는 5글자를 초과할 수 없습니다.");
+                .withMessageMatching("개인 참가자 이름의 길이는 5글자를 초과할 수 없습니다.");
     }
 }
