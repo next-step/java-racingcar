d734ed22793483f070dfcf6e8ec2f2852ccecb29
westkitekim <westkite418@gmail.com>
2024-03-20 15:46:03 +0900

    style: 사용하지 않는 코드 제거

--- src/test/java/racingcar/domain/ParticipantsTest.java
+++ src/test/java/racingcar/domain/ParticipantsTest.java
@@ -2,7 +2,6 @@ package racingcar.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
 import static org.assertj.core.api.Assertions.*;
 
