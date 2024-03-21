4e984e50d55153d013f0458c4797a6ed3f9381b2
westkitekim <westkite418@gmail.com>
2024-03-21 09:18:01 +0900

    test: 자동차 생성 테스트코드 기존로직 변경

--- src/test/java/racingcar/domain/RacingCarGameTest.java
+++ src/test/java/racingcar/domain/RacingCarGameTest.java
@@ -7,17 +7,19 @@ import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.domain.strategy.MoveStrategy;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingCarGameTest {
-    @ParameterizedTest
-    @ValueSource(ints = {1, 2, 4})
+    @Test
     @DisplayName("입력된 자동차 대수만큼 RacingCar 객체를 생성한다")
-    void createCarList_자동차대수(int carCount) {
-        List<Car> carList = RacingCarGame.createRacingCars(carCount);
-        int actual = carList.size();
-        int expected = carCount;
+    void createCarList_자동차대수() {
+        String participantsNames = "a, b, c, d";
+        Participants participants = Participants.from(participantsNames);
+        Map<Participant, Car> carList = RacingCarGame.createRacingCars(participants);
+        int actual = participantsNames.split(",").length;
+        int expected = carList.keySet().size();
 
         assertThat(actual).isEqualTo(expected);
     }
@@ -36,7 +38,7 @@ class RacingCarGameTest {
     }
 
     @Test
-    @DisplayName("전략에 따라 랜덤값이 0이상 3이하인 경우 자동차는 정지한다")
+    @DisplayName("전략에 따라 랜덤값이 4이상 9이하 경우 자동차는 전진한다")
     void playRoundsAndReturnCarList_전진() {
         Car car = new Car();
         int beforePostion = car.getPosition();
