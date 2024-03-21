e6ca9d921adb5e9ea4e65c765201e4b51c64eb4b
Jaejin Jeong <sojournre@gmail.com>
2024-03-21 22:36:19 +0900

    refactor: 리뷰 요청사항 반영중

--- src/main/java/racing/RacingMain.java
+++ src/main/java/racing/RacingMain.java
@@ -6,6 +6,7 @@ import java.util.List;
 import static racing.InputView.inputCarCount;
 import static racing.InputView.inputTryCount;
 import static racing.ResultView.printResult;
+import static racing.ResultView.printResultMessage;
 
 public class RacingMain {
     public static void main(String[] args) {
@@ -17,8 +18,7 @@ public class RacingMain {
             cars.add(new Car());
         }
 
-        System.out.println();
-        System.out.println("실행 결과");
+        printResultMessage();
 
         for (int i = 0; i < tryCount; i++) {
             printResult(cars);
@@ -26,6 +26,8 @@ public class RacingMain {
             carMove(cars);
         }
     }
+
+
 
     private static void carMove(List<Car> cars) {
         for (Car  car : cars) {
