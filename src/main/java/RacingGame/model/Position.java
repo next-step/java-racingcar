efdb9809c4d258b6e86f1fa1d00f53a697fe6063
JJewon <eldertree777@gmail.com>
2024-03-23 02:59:34 +0900

    [refactor] PositionCache 생성

--- src/main/java/RacingGame/model/Position.java
+++ src/main/java/RacingGame/model/Position.java
@@ -22,4 +22,27 @@ public class Position {
     public boolean equals(int number) {
         return this.number == number;
     }
+
+    private static class PositionCache {
+        static final int low = 0;
+        static final int high = 10;
+        static final Position[] cache;
+
+        static {
+            int size = high + 1;
+            Position[] positions = new Position[size];
+            for (int i = 0; i < positions.length; i++) {
+                positions[i] = new Position(i);
+            }
+
+            cache = positions;
+        }
+    }
+
+    public static Position valueOf(int number) {
+        if (number >= PositionCache.low && number <= PositionCache.high) {
+            return PositionCache.cache[number];
+        }
+        return new Position(number);
+    }
 }
