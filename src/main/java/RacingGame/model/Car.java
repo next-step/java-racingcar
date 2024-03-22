135930ac48487a73a6b81eb83b5514fb27a69512
JJewon <eldertree777@gmail.com>
2024-03-23 02:34:55 +0900

    [refactor] 유효성 검사 일급객체에서 진행

--- src/main/java/RacingGame/model/Car.java
+++ src/main/java/RacingGame/model/Car.java
@@ -1,19 +1,13 @@
 package RacingGame.model;
 
 public class Car {
-    private static final int CAR_LENGTH_EXCEED = 5;
     private static final int INITAIL_CAR_POSITION = 0;
-    private static final String CARNAME_LENGTH_EXCEED = String.format("차량의 이름은 %d 글자가 넘어갈 수 없습니다", CAR_LENGTH_EXCEED);
     private static final String DEFAULT_CAR_NAME = "NULL";
 
     private final Name name;
     private final Position position;
 
     public Car(String name, int position) {
-        if (name.length() > CAR_LENGTH_EXCEED) {
-            throw new IllegalArgumentException(CARNAME_LENGTH_EXCEED);
-        }
-
         this.name = new Name(name);
         this.position = new Position(position);
     }
