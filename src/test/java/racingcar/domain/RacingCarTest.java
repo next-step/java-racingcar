bcdf2133c794ab77caf397c19a52ab15ffbfce26
Jioo Jung <64303211+jioome@users.noreply.github.com>
2024-03-23 17:47:34 +0900

    docs: todo list

--- src/test/java/racingcar/domain/RacingCarTest.java
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static racingcar.domain.Car.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +17,6 @@ public class RacingCarTest {
 	// 시도 횟수
 	List<Car> carList = new ArrayList<Car>();
 	Random random = new Random();
-	private static final int MIN_THRESHOLD = 4;
 
 	@Test
 	@DisplayName("전진하는 경우")
