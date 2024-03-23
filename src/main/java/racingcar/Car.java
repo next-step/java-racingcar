0b49263a3ade4bc575cc93b12e80f80179194dcc
jjihye-hyun <jjihyehyun@gmail.com>
2024-03-23 18:36:26 +0900

    feat: 자동차 이름 5자 초과 유효성 검사 기능 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int location;
+
+    public Car(String name) throws Exception {
+        checkCarNameLength(name);
+        this.name = name;
+        this.location = 0;
+    }
+
+    private void checkCarNameLength(String name) throws Exception {
+        if (name.length() > 5) {
+            throw new Exception("자동차 이름 길이 초과 (최대 5자)");
+        }
+    }
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CarTest {
+
+    @Test
+    void 자동차이름_5자초과() {
+        Exception e = assertThrows(Exception.class, () -> new Car("abcdef"));
+        assertThat(e.getMessage()).isEqualTo("자동차 이름 길이 초과 (최대 5자)");
+    }
+
+}
