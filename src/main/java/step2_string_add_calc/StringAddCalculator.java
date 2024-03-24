e4da3589b31e1023a39a9aa1b86ee1c8f17874fe
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-03-24 22:55:07 +0900

    [step2] req3-testcase-Success
    - 앞 단계의 구분자가 없는 경우도 split()을 활용가능

--- src/main/java/step2_string_add_calc/StringAddCalculator.java
+++ src/main/java/step2_string_add_calc/StringAddCalculator.java
@@ -1,8 +1,13 @@
 package step2_string_add_calc;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
     public static int splitAndSum(String inputStr) {
-        if (inputStr == null || inputStr.isEmpty()) return 0;
-        return Integer.parseInt(inputStr);
+        if (inputStr == null || inputStr.isEmpty())
+            return 0;
+        return Arrays.stream(inputStr.split(","))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
