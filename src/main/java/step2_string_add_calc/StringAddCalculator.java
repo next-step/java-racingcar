6a7603c6364661cafd24861eaa85381fbccd9d92
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-03-24 23:39:34 +0900

    [step2] req6-testcase-Success

--- src/main/java/step2_string_add_calc/StringAddCalculator.java
+++ src/main/java/step2_string_add_calc/StringAddCalculator.java
@@ -12,14 +12,20 @@ public class StringAddCalculator {
         Matcher m = Pattern.compile("//(.)\n(.*)")
                 .matcher(inputStr);
         if (m.find()) {
-            return calculate(m.group(1), m.group(2));
+            return splitAndSum(m.group(1), m.group(2));
         }
-        return calculate("[,:]", inputStr);
+        return splitAndSum("[,:]", inputStr);
     }
 
-    private static int calculate(String delimiter, String inputStr) {
+    private static int splitAndSum(String delimiter, String inputStr) {
         return Arrays.stream(inputStr.split(delimiter))
-                .mapToInt(Integer::parseInt)
+                .mapToInt(s -> {
+                    int number = Integer.parseInt(s); // 문자열을 정수로 변환
+                    if (number < 0) { // 변환된 정수가 음수인 경우
+                        throw new RuntimeException("음수 입력은 불가능합니다.");
+                    }
+                    return number; // 변환된 양수 정수 반환
+                })
                 .sum();
     }
 }
