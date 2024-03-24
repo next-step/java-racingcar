47293bce46395b734ae750321fc3cc20839f9045
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-03-24 22:51:11 +0900

    [step2] req3-testcase-Fail
    - 앞 단계의 구분자가 없는 경우도 split()을 활용가능

--- src/test/java/step2_string_add_calc/StringAddCalculatorTest.java
+++ src/test/java/step2_string_add_calc/StringAddCalculatorTest.java
@@ -10,16 +10,22 @@ class StringAddCalculatorTest {
 
     @Test
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다")
-    void returnZero_emptyOrNullTest(){
+    void returnZero_emptyOrNullTest() {
         assertThat(splitAndSum(null)).isEqualTo(0);
         assertThat(splitAndSum("")).isEqualTo(0);
     }
 
     @Test
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
-    void returnInputWhen(){
+    void returnInputWhen() {
         assertThat(splitAndSum("1")).isEqualTo(1);
         assertThat(splitAndSum("77")).isEqualTo(77);
     }
 
+    @Test
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    void returnSum_InputTwoNumbersWithDelimiting() {
+        assertThat(splitAndSum("2,4")).isEqualTo(6);
+        assertThat(splitAndSum("10,4")).isEqualTo(14);
+    }
 }
