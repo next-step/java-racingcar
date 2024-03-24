2b88126649ce4bfe728b46a3e6c986dfac994c38
jjihye-hyun <jjihyehyun@gmail.com>
2024-03-24 21:44:18 +0900

    refactor: Car 이름 부여 분리

--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 public class CarRacing {
 
-    int times;
-    List<Car> cars;
+    private int times;
+    private List<Car> cars;
 
     public CarRacing(int times, String inputNames) throws Exception {
         this.times = times;
         this.cars = new ArrayList<>();
+        grantCarName(inputNames);
+    }
 
+    private void grantCarName(String inputNames) throws Exception {
         String[] carNames = inputNames.split(",");
         for (String carName : carNames) {
             cars.add(new Car(carName));
