4a520af3281e691e17ce1776e5e8c21203bc7d9d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-03-24 22:47:55 +0900

    [step2] req2-testcase-Fail

--- src/test/java/step2_string_add_calc/StringAddCalculatorTest.java
+++ src/test/java/step2_string_add_calc/StringAddCalculatorTest.java
@@ -4,14 +4,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static step2_string_add_calc.StringAddCalculator.*;
 
 class StringAddCalculatorTest {
 
     @Test
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다")
-    void returnZero_emptyOrNull(){
-        assertThat(StringAddCalculator.splitAndSum(null)).isEqualTo(0);
-        assertThat(StringAddCalculator.splitAndSum("")).isEqualTo(0);
+    void returnZero_emptyOrNullTest(){
+        assertThat(splitAndSum(null)).isEqualTo(0);
+        assertThat(splitAndSum("")).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    void returnInputWhen(){
+        assertThat(splitAndSum("1")).isEqualTo(1);
+        assertThat(splitAndSum("77")).isEqualTo(77);
     }
 
 }
