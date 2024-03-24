0766d643fed4a5ea7646c69fc51d68ca0fd9e6aa
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-03-24 22:43:37 +0900

    [step2] req1-testcase-fail
    - 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다

--- src/test/java/step2_string_add_calc/StringAddCalculatorTest.java
+++ src/test/java/step2_string_add_calc/StringAddCalculatorTest.java
@@ -1,6 +1,18 @@
 package step2_string_add_calc;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
 import static org.assertj.core.api.Assertions.*;
+
 class StringAddCalculatorTest {
+
+    @Test
+    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다")
+    void returnZero_emptyOrNull(){
+        assertThat(StringAddCalculator.splitAndSum(null)).isEqualTo(0);
+        assertThat(StringAddCalculator.splitAndSum("")).isEqualTo(0);
+    }
 
 }
