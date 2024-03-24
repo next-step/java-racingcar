83ac822d17c99d852823205df1d5087502c00900
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-03-24 23:02:26 +0900

    [step2] req5-testcase-Success

--- src/main/java/step2_string_add_calc/StringAddCalculator.java
+++ src/main/java/step2_string_add_calc/StringAddCalculator.java
@@ -1,12 +1,24 @@
 package step2_string_add_calc;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
     public static int splitAndSum(String inputStr) {
         if (inputStr == null || inputStr.isEmpty())
             return 0;
-        return Arrays.stream(inputStr.split(",|:"))
+        // java.util.regex 패키지의 Matcher, Pattern import
+        Matcher m = Pattern.compile("//(.)\n(.*)")
+                .matcher(inputStr);
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return Arrays.stream(m.group(2).split(customDelimiter))
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+        }
+        return Arrays.stream(inputStr.split("[,:]"))
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
