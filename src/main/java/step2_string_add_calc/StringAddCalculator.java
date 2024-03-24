d1fc15bd63a8fa7560cb1b34ebed6dd2f7b14f17
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-03-24 22:49:32 +0900

    [step2] req2-testcase-Success

--- src/main/java/step2_string_add_calc/StringAddCalculator.java
+++ src/main/java/step2_string_add_calc/StringAddCalculator.java
@@ -2,6 +2,7 @@ package step2_string_add_calc;
 
 public class StringAddCalculator {
     public static int splitAndSum(String inputStr) {
-        return 0;
+        if (inputStr == null || inputStr.isEmpty()) return 0;
+        return Integer.parseInt(inputStr);
     }
 }
