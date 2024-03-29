e55e69a2d3f4021eb72c96a9a992f381226670e3
jjihye-hyun <jjihyehyun@gmail.com>
2024-03-29 21:50:03 +0900

    refactor: Scanner 인스턴스 분리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,10 +3,9 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
     public static String userInput(String message) {
         System.out.println(message);
-
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 }
