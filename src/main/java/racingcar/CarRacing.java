0b03891693df5293c2e14dff67b15792e362b5de
jjihye-hyun <jjihyehyun@gmail.com>
2024-03-31 15:11:21 +0900

    refactor: cars 컬렉션 분리

--- src/main/java/racingcar/CarRacing.java
+++ src/main/java/racingcar/CarRacing.java
@@ -1,37 +1,18 @@
 package racingcar;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class CarRacing {
-
     private int times;
-    private List<Car> cars;
-    private Random random;
+    private RacingCars racingCars;
 
     public CarRacing(int times, String inputNames) throws Exception {
         this.times = times;
-        this.cars = new ArrayList<>();
-        grantCarName(inputNames);
-        random = new Random();
-    }
-
-    private void grantCarName(String inputNames) throws Exception {
-        String[] carNames = inputNames.split(",");
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
+        this.racingCars = new RacingCars(inputNames);
     }
 
     public List<Car> run() {
-        List<Car> results = new ArrayList<>();
-        for (Car car : cars) {
-            int condition = random.nextInt(10);
-            car.move(condition);
-            results.add(car);
-        }
-        return results;
+        return racingCars.run();
     }
 
 }
