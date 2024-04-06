5f11a0dc1ba21c5bc9bad687a508bf8a1b9b24ee
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-04-06 11:32:29 +0900

    [step3] 객체를 이용한 프로그램 실행

--- src/main/java/step3_CarRacing/CarRacingGame.java
+++ src/main/java/step3_CarRacing/CarRacingGame.java
@@ -7,10 +7,20 @@ public class CarRacingGame {
         Scanner scanner = new Scanner(System.in);
         System.out.println("자동차 대수는 몇 대 인가요?");
         int numberOfCars = scanner.nextInt();
+
         scanner.nextLine(); // nextInt() 사용 후 nextLine() 호출 시 줄바꿈 문자 처리
+
         System.out.println("시도할 회수는 몇 회 인가요?");
-        int numberOfTries = scanner.nextInt();
-        System.out.printf("자동차 대수: %d, 시도할 회수: %d%n", numberOfCars, numberOfTries);
+        int numberOfRounds = scanner.nextInt();
+
+        Race race = new Race(numberOfCars);
+
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < numberOfRounds; i++) {
+            race.runRound();
+            race.getCars().forEach(car -> System.out.println("-".repeat(car.getDistance())));
+            System.out.println();
+        }
     }
 
 
