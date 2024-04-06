10a063086a318d0a70ed047b176b1cc1e50ac267
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-04-06 10:50:01 +0900

    [step3] String.format대신 printf 사용

--- src/main/java/step3_CarRacing/CarRacingGame.java
+++ src/main/java/step3_CarRacing/CarRacingGame.java
@@ -10,7 +10,7 @@ public class CarRacingGame {
         scanner.nextLine(); // nextInt() 사용 후 nextLine() 호출 시 줄바꿈 문자 처리
         System.out.println("시도할 회수는 몇 회 인가요?");
         int numberOfTries = scanner.nextInt();
-        System.out.println(String.format("자동차 대수: %d, 시도할 회수: %d", numberOfCars, numberOfTries));
+        System.out.printf("자동차 대수: %d, 시도할 회수: %d%n", numberOfCars, numberOfTries);
     }
 
 
