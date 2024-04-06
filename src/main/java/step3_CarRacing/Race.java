8fc50d1c61c512854ff0edddcd034ba0378385c7
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-04-06 11:13:26 +0900

    [step3] OOP설계(Race): 개선
    - 초기화 streawm 이용
    - 0 이하시 에러
    - runRound 추가

--- src/main/java/step3_CarRacing/Race.java
+++ src/main/java/step3_CarRacing/Race.java
@@ -2,12 +2,26 @@ package step3_CarRacing;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Race {
-    private final List<Car> cars = new ArrayList<>();
+    private final Random random;
+    private final List<Car> cars;
+
     public Race(int numberOfCars) {
-        for (int i = 0; i < numberOfCars; i++) {
-            cars.add(new Car());
-        }
+        if (numberOfCars < 0) throw new IllegalArgumentException("0이상이어야 함");
+        random = new Random();
+
+        cars = new ArrayList<>();
+        IntStream.range(0, numberOfCars).forEach(car -> cars.add(new Car()));
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void runRound() {
+        cars.forEach(car -> car.move(random.nextInt(10)));
     }
 }
