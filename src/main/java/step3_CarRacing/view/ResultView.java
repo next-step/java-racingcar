0a37b27d7a8e6b0ac7534f2646b67b78a9054edc
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-04-15 11:41:21 +0900

    [step3] 워닝 수정

--- src/main/java/step3_CarRacing/view/ResultView.java
+++ src/main/java/step3_CarRacing/view/ResultView.java
@@ -1,7 +1,6 @@
 package step3_CarRacing.view;
 
 import step3_CarRacing.Car;
-import step3_CarRacing.Race;
 
 import java.util.List;
 
@@ -11,7 +10,7 @@ public class ResultView {
         System.out.println();
     }
     public void printResultHeader() {
-        System.out.printf("실행 결과\n");
+        System.out.print("실행 결과\n");
     }
 /*
 
