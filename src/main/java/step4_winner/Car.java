0ff931b457519d4fc242ba77b5c24f7f78ac86fa
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-04-15 15:54:34 +0900

    feat(car): name 필드
    - 필드 추가
    - 생성자 추가
    - 5자 제한 추가
    - getter 추가
    - 테스트도 따라서 추가

--- src/main/java/step4_winner/Car.java
+++ src/main/java/step4_winner/Car.java
@@ -1,15 +1,20 @@
 package step4_winner;
 
-public class Car {
+import lombok.Getter;
 
+@Getter
+public class Car {
     private int distance = 1;
+    private final String name;
+
+    public Car(String inputName) {
+        if (inputName.length() > 5)
+            throw new IllegalArgumentException("5자를 초과할 수 없습니다");
+        this.name = inputName;
+    }
 
     public void move(int randomValue) {
         if (randomValue >= 4)
             distance++;
-    }
-
-    public int getDistance() {
-        return distance;
     }
 }
--- src/test/java/step4_winner/CarTest.java
+++ src/test/java/step4_winner/CarTest.java
@@ -3,21 +3,47 @@ package step4_winner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
 
     @Test
     @DisplayName("자동차의 기본 거리는 1이고, move에 4이상의 값이 주어지면 distance가 증가한다")
     void CarMoveTest() {
-        Car car = new Car();
+
+        // GIVEN
+        String legalName = "test1";
+
+        // WHEN
+        Car car = new Car(legalName); //이름 넣도록 추가
+
+        // THEN
         assertThat(car.getDistance()).isEqualTo(1);
-        //움직이지 않음
+        assertThat(car.getName()).isEqualTo("test1");
+
+        // WHEN : 안움직임
         car.move(3);
+        // THEN
         assertThat(car.getDistance()).isEqualTo(1);
-        //무조건 움직임
+
+        // WHEN : 움직임
         car.move(4);
+        // THEN
         assertThat(car.getDistance()).isGreaterThan(1);
     }
 
+    @Test
+    @DisplayName("자동차 이름은 5자를 초과할 수 없다")
+    void carNameNotLongerThan5Test() {
+        // GIVEN
+        String IllegalCarName = "5charover";
+
+        // WHEN
+        Throwable IllegalThrown = catchThrowable(() -> new Car(IllegalCarName));
+
+        // THEN
+        assertThat(IllegalThrown)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("5자를 초과할 수 없습니다");
+    }
 }
