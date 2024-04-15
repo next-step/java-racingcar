d906437b5b1d49820dc061c95794a6f9d5ef26f5
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-04-15 18:51:24 +0900

    feat(result): 자동차 이름과 함께 움직임 출력

--- src/test/java/step4_winner/CarRacingWinnerTest.java
+++ src/test/java/step4_winner/CarRacingWinnerTest.java
@@ -3,15 +3,17 @@ package step4_winner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.NoSuchElementException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 class CarRacingWinnerTest {
 
@@ -30,13 +32,11 @@ class CarRacingWinnerTest {
         System.setOut(stdOut);
     }
 
-    @ParameterizedTest
-    // GIVEN
-    @ValueSource(strings = {"test1,test2,test3", "a,b,c,d,e,f,g"})
-    @DisplayName("전달받은 자동차 리스트로 Input 테스트")
-    void CarRacingWinnerInputTest(String input) {
-        // GIVEN
-        provideInput(input,"1");
+    @Test
+    @DisplayName("Input Test")
+    void CarRacingWinnerInputTest() {
+        // GIVEN 1번만 시도
+        provideInput("a,b,c,d,e,f,g","1");
 
         // WHEN
         CarRacingWinner.main(new String[]{});
@@ -44,8 +44,27 @@ class CarRacingWinnerTest {
         // THEN
         String outputStreamCaptorString = outContent.toString();
         String raceResult = outputStreamCaptorString.split("\n실행 결과\n", 2)[1];
-        System.out.println(raceResult);
 
+        long linesCount = raceResult.lines().count();
+        assertThat(linesCount).isEqualTo(8L);
+
+        long emptyLinesCount = raceResult.lines().filter(String::isEmpty).count();
+        assertThat(emptyLinesCount).isEqualTo(1L);
+
+    }
+
+    @Test
+    @DisplayName("입력된 값이 없으면 NoSuchElementException")
+    void throwNoSuchElementExceptionIfNoInput() {
+        // GIVEN
+        provideInput("","");
+
+        // WHEN
+        Throwable IllegalThrown = catchThrowable(() -> CarRacingWinner.main(new String[]{}));
+
+        // THEN
+        assertThat(IllegalThrown)
+                .isInstanceOf(NoSuchElementException.class);
     }
 
     private void provideInput(String... strings) {
