9fd75c950de9ff83d65bef2d223ade4e26958e66
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2024-04-16 10:07:25 +0900

    Step3 PR request (#5539)
    * [step3] ready
    
    * [step3] 자동차 대수와 시도할 회수가 정확하게 입력된다 - 테스트 성공
    
    * [step3] 변수명 수정
    
    * [step3] String.format대신 printf 사용
    
    * [step3] OOP설계(Car)
    
    - Car는 전진한 거리 distance를 가진다.
    - Car는 random값이 4 이상이면 거리증가
    
    * [step3] OOP설계(Race): Race에는 n대의 차가 참여
    
    * [step3] OOP설계(Race): 개선
    
    - 초기화 streawm 이용
    - 0 이하시 에러
    - runRound 추가
    
    * [step3] 객체를 이용한 프로그램 실행
    
    * [step3] 리팩토링 - car 처음 distance 초기값 0->1
    
    - 아예 차가 나타나지 않는 현상 방지
    
    * [step3] CarMoveTest 추가
    
    * [step3] CarMoveTest 개선
    
    * [step3] RaceRoundTest: race가 1번이라도 열리면 100대중 1개는 전진 한다
    
    * [step3] MainOutputTest (미완성)
    
    * [step3] 테스트 성공
    
    * [test] 테스트 성공
    
    * [test] 필요 없는 부분 정리
    
    * [step3] 사용하지 않는provideOutput 메서드 삭제 -
    
    * [step3] resultView 생성
    
    * [step3] 워닝 수정
    
    * [step3] runraces로 운영 분리
    
    * [step3] InputView 추가
    
    * [step3] InputView 반영
    
    * [step3] 라이브러리 버전업+롬복 추가
    
    * [step3] 정리및 빈 LINE CHECK 추가
    
    * [step3] action 정리

---
+++ src/test/java/step3_CarRacing/CarRacingGameTest.java
@@ -0,0 +1,87 @@
+package step3_CarRacing;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.*;
+
+
+class CarRacingGameTest {
+    private final InputStream stdIn = System.in;
+    private final PrintStream stdOut = System.out;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUpPrintStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void restoreSystemIn() {
+        System.setIn(stdIn);
+        System.setOut(stdOut);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2,3,9",
+            "2,4,12",
+            "3,1,4",
+            "3,2,8",
+            "12,1234,16042",
+            "100,1,101",
+            "100,2,202",
+            "100,50,5050",
+            "100,73,7373",
+            "8562,1,8563",
+    })
+    @DisplayName("csvSource로 부터 받은 데이터와 예상메시지가 일치해야 한다.")
+    public void MainOutputTest(String expectedCars, String expectedTries, long expectedLines) {
+        // GIVEN
+        provideInput(expectedCars, expectedTries);
+
+        // WHEN
+        CarRacingGame.main(new String[]{});
+
+        // THEN
+        String outputStreamCaptorString = outContent.toString();
+        String raceResult = outputStreamCaptorString.split("\n실행 결과\n", 2)[1];
+
+        long linesCount = raceResult.lines().count();
+        assertThat(linesCount).isEqualTo(expectedLines);
+
+        long emptyLinesCount = raceResult.lines().filter(String::isEmpty).count();
+        assertThat(emptyLinesCount).isEqualTo(Long.parseLong(expectedTries));
+    }
+
+    private void provideInput(String... strings) {
+        String inputStr = String.join("\n", strings);
+        System.setIn(new ByteArrayInputStream(inputStr.getBytes(UTF_8)));
+    }
+
+    @Test
+    @DisplayName("value4가 4이상이면 움직이고 그러지 않으면 움직이지 않는다.")
+    void CarMoveTest() {
+        Car car = new Car();
+        car.move(4); // 무조건 움직임
+        assertThat(car.getDistance()).isEqualTo(2);
+        car.move(3); // 무조건 안움직임
+        assertThat(car.getDistance()).isEqualTo(2);
+    }
+
+    @RepeatedTest(10000)
+    @DisplayName("100개의 차가 참가한 race가 일단 열리면, 100대중 1개는 전진 한다")
+    public void RaceRoundTest() {
+        Race race = new Race(100);
+        race.runRound();
+        boolean anyCarMoved = race.getCars().stream().anyMatch(car -> car.getDistance() > 1);
+        assertThat(anyCarMoved).isTrue();
+    }
+}
