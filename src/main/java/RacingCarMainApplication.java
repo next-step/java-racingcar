005c47d72db68f1999d815707d2a993e783d76b1
jaejeong1 <wowjd555@gmail.com>
2024-05-07 11:02:41 +0900

    feat: MainApplication 구현

---
+++ src/main/java/RacingCarMainApplication.java
@@ -0,0 +1,20 @@
+public class RacingCarMainApplication {
+    public static void main(String[] args) {
+        String names = Printer.askNameOfCars();
+        int tryCount = Printer.askNumberOfTry();
+
+        RacingCar racingCar = new RacingCar(names);
+        MovePolicy randomMove = new RandomMove();
+
+        Cars racedCars = null;
+
+        Printer.printResultTitle();
+
+        for(int i=0; i<tryCount; i++) {
+            racedCars = racingCar.move(randomMove);
+            Printer.printResult(racedCars);
+        }
+
+        Printer.printWinner(racedCars);
+    }
+}
