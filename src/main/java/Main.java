89023fb74b0bc5173a769e4bb11759284ffd3a28
Hyeong gyu choi <thankyouoov@naver.com>
2024-06-09 22:47:29 +0900

    add input name of cars logic

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import view.InputView;
+
+public class Main {
+    public static void main(String[] args){
+        InputView inputView = new InputView();
+    }
+
+}
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static String getCarsName() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+
+}
