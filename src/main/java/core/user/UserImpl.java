8343eea21f9f502f7de67df9ab2b939c0749b7e2
kkoma2623 <jiwook.dev@gmail.com>
2024-06-09 19:31:49 +0900

    edit to use util print functions

--- src/main/java/core/user/UserImpl.java
+++ src/main/java/core/user/UserImpl.java
@@ -1,20 +1,21 @@
 package core.user;
 
+import core.util.Util;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class UserImpl implements UserInterface {
 
+  static Util util = new Util();
   public UserImpl() {
   }
 
   @Override
   public List<String> getCarNames() {
     String userCarNamesCmd = null;
-    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    util.printMsg("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     do {
       Scanner sc = new Scanner(System.in);
       userCarNamesCmd = sc.nextLine();
@@ -38,7 +39,7 @@ public class UserImpl implements UserInterface {
       if (5 >= car.length()) {
         continue;
       }
-      System.out.println("[ERROR] 모든 자동차 이름은 5글자 이하여야합니다.");
+      util.printErrorMsg("모든 자동차 이름은 5글자 이하여야합니다.");
       return false;
     }
     return true;
@@ -55,7 +56,7 @@ public class UserImpl implements UserInterface {
         return false;
       }
     } catch (InputMismatchException e) {
-      System.out.println("[ERROR] 쉼표(,) 사용을 정확히 해주세요");
+      util.printErrorMsg("쉼표(,) 사용을 정확히 해주세요");
       return false;
     }
     return true;
@@ -65,7 +66,7 @@ public class UserImpl implements UserInterface {
     for (String carName : carNames) {
       String trimmedPart = carName.trim();
       if (trimmedPart.isEmpty()) {
-        System.out.println("[ERROR] 자동차 이름이 비어있습니다.");
+        util.printErrorMsg("자동차 이름이 비어있습니다.");
         return true;
       }
     }
@@ -74,7 +75,7 @@ public class UserImpl implements UserInterface {
 
   private static boolean isCommaInStartOrEnd(String userCarNamesCmd) {
     if (userCarNamesCmd.startsWith(",") || userCarNamesCmd.endsWith(",")) {
-      System.out.println("[ERROR] 자동차 이름 입력을 쉼표(,) 로 시작하거나 끝나면 안됩니다.");
+      util.printErrorMsg("자동차 이름 입력을 쉼표(,) 로 시작하거나 끝나면 안됩니다.");
       return true;
     }
     return false;
