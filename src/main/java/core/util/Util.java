1e25842895f74248330d78e90183af6c05b0aa4b
kkoma2623 <jiwook.dev@gmail.com>
2024-06-09 19:31:01 +0900

    make utility

---
+++ src/main/java/core/util/Util.java
@@ -0,0 +1,12 @@
+package core.util;
+
+public class Util {
+
+  public void printErrorMsg(String msg) {
+    System.out.println("[ERROR] " + msg);
+  }
+
+  public void printMsg(String msg) {
+    System.out.println(msg);
+  }
+}
