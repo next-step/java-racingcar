ba6c2e66f45b3cfc6bb73cefdd8e9c5820e9826a
jureamer <wngud4950@gmail.com>
2024-09-23 21:55:47 +0900

    implement set class test 3

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -32,5 +33,15 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void setContainsOneTwoThree(int input) {
         assertTrue(input >= 1 && input <= 3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1: true", "2: true", "3: true", "4: false", "5: false"}, delimiter = ':')
+    void setContainsOneTwoThreeFourFive(int input, boolean expected) {
+        if (input <= 3) {
+            assertTrue(expected);
+        } else {
+            assertFalse(expected);
+        }
     }
 }
