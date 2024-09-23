caf33640023798e14d17f1537ff5251093d1dd6b
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-09-23 16:54:35 +0900

    null과 empty를 추가하는 로직 부분 추가
    null과 empty를 추가하는 로직 부분 추가

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,14 @@
+public class StringAddCalculator {
+
+    public static int splitAndSum(String text) {
+        if (checkNullOrEmpty(text)) {
+            return 0;
+        }
+
+        return 0;
+    }
+
+    private static boolean checkNullOrEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+}
