891737a6ec39356d0c510a16693b6484fc629c53
sinyebin <dpqls0625@naver.com>
2024-09-23 23:35:03 +0900

    24.09.23 syb Set Collection 요구사항2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -22,7 +24,24 @@ public class SetTest {
 
     @Test
     @DisplayName("Set_크기_확인")
-    void checkSetSize(){
+    void checkSetSize() {
         assertThat(numbers.size()).isEqualTo(3);
     }
+
+    @Test
+    @DisplayName("Set_존재_확인")
+    void checkContains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set_존재_확인에서_중복제거")
+    void checkContainsDeleteDuplicate(int num) {
+        assertThat(numbers.contains(num)).isTrue();
+    }
+
+
 }
