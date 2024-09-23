d6e0cbfbd80259b5b44081c43efc49b3fcaaac48
hunko <rhwlgns4386@naver.com>
2024-09-24 01:02:21 +0900

    test : contains함수 사용 로직으로 변경
    힌트 확인 후 contains 및 containsExactly 사용 로직으로 변경

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,14 +10,16 @@ public class StringTest {
     @Test
     public void 숫자_두개_분리_테스트(){
         String[] split = "1,2".split(",");
-        assertThat(split.length).isEqualTo(2);
+        assertThat(split).hasSize(2);
         assertThat(split).contains("1","2");
+        assertThat(split).containsExactly("1","2");
     }
 
     @Test
     public void 숫자_한개_분리_테스트(){
         String[] split = "1".split(",");
-        assertThat(split.length).isEqualTo(1);
+        assertThat(split).hasSize(1);
         assertThat(split).contains("1");
+        assertThat(split).containsExactly("1");
     }
 }
