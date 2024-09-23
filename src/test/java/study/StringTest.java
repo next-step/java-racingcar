186ad8d294c960af4df7282453ceec6fb4eb69c2
배영현 <byh2949@gmail.com>
2024-09-23 21:52:21 +0900

    String 클래스 요구사항2, 요구사항3 commit / Set 요구사항1, 요구사항2, 요구사항3 commit
    String 클래스 요구사항2 substring 테스트 코드 작성, 요구사항3 charAt() 범위 valid, invalid 테스트 코드 작성
    Set 요구사항1 size() 메소드를 활용해 Set의 크기를 확인, 요구사항2 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인(ParameterizedTest / ValueSource), 요구사항3 contains 메소드를 활용해 파라미터에 따라 의 값이 존재하는지를 확인(ParameterizedTest / csvSource)

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,14 +1,51 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void split() {
         String[] result = "1,2".split(",");
+
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1","2");
+    }
+
+    @Test
+    void substring() {
+        String data = "(1,2)".substring(1,4);
+
+        assertThat(data).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt() 메소드를 통해 특정 위치의 값을 반환한다.")
+    @Test
+    void charAt_validIndex() {
+        String data = "abc";
+
+        assertThat(data.charAt(0)).isEqualTo('a');
+        assertThat(data.charAt(1)).isEqualTo('b');
+        assertThat(data.charAt(2)).isEqualTo('c');
+
+    }
+
+    @DisplayName("charAt() 메소드에서 위치를 벗어나면  StringIndexOutOfBoundsException이 발생한다.")
+    @Test
+    void charAt_invalidIndex() {
+        String data = "abc";
+
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    data.charAt(-1);
+        }).withMessageMatching("String index out of range: -?\\d+");
+
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    data.charAt(3);
+        }).withMessageMatching("String index out of range: -?\\d+");
+
     }
 }
