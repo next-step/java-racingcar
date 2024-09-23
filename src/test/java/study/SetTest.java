9faf0eb31deb01f13fc60ff5f549a2b99bc342a5
BISohn27 <87046370+BISohn27@users.noreply.github.com>
2024-09-24 07:15:47 +0900

    string test, set test - step 1 (#5561)

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,54 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void check_set_size() {
+        int size = numbers.size();
+
+        assertThat(size).isEqualTo(3);
+    }
+
+    @Test
+    void check_contained_elements() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void check_contained_element_with_parameterized_value(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false"}, delimiter = ':')
+    void check_contained_element_with_parameterized_csv(String input, String expected) {
+        assertThat(numbers.contains(Integer.parseInt(input))).isEqualTo(Boolean.parseBoolean(expected));
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,67 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    void split_string() {
+        String test = "1,2";
+
+        String[] split = split(test);
+
+        assertThat(split).contains("1", "2");
+    }
+
+    @Test
+    void split_single_string() {
+        String test = "1";
+
+        String[] split = split(test);
+
+        assertThat(split).containsExactly("1");
+    }
+
+    private String[] split(String target) {
+        return target.split(",");
+    }
+
+    @Test
+    void remove_str() {
+        String test = "(1,2)";
+
+        String removed = test.substring(test.indexOf("(") + 1, test.indexOf(")"));
+
+        assertThat(removed).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("주어진 문자열에서 인덱스를 가지고 특정 문자를 가져온다")
+    void get_specific_char() {
+        String test = "abc";
+
+        char target = 'c';
+        char result = specificChar(test, 2);
+
+        assertThat(result).isEqualTo(target);
+    }
+
+    @Test
+    @DisplayName("주어진 문자열의 길이를 넘어가는 인덱스가 주어질 때 예외가 발생한다")
+    void throw_out_of_bound_exception() {
+        String test = "abc";
+
+        Assertions.assertThatThrownBy(() -> specificChar(test, test.length()))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining(String.valueOf(test.length()));
+    }
+
+    private char specificChar(String str, int index) {
+        return str.charAt(index);
+    }
+
+}
