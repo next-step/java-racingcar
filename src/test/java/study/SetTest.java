068a8928bbfd04163d21102ce96ca8bde6e918cf
Joonkb <byeongjunkim@naver.com>
2024-09-23 23:56:29 +0900

    step-1 SetTest - 요구사항 1 구현

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 인터페이스의 size() 메서드를 활용하여 Set의 크기를 확인한다.")
+    void sizeOfSetTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
