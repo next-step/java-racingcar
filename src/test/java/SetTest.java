2273afc168b8b3a8f5fc8ed6fde29a70694ccc1e
한주형 <wngud4950@gmail.com>
2024-09-24 01:33:52 +0900

    1단계 - 학습 테스트 실습 (#5560)
    * implement string class test 1
    
    * implement string class test 2
    
    * implement string class test 3
    
    * implement set class test 1
    
    * implement set class test 2
    
    * implement set class test 3
    
    ---------
    
    Co-authored-by: juhyeong.han <juhyeong.han@anaotomage.co.kr>

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 메소드를 활용해 Set 크기를 확인한다.")
+    void checkSetSize() {
+        int setSize = numbers.size();
+        assertThat(setSize).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void setContainsOneTwoThree(int input) {
+        assertTrue(input >= 1 && input <= 3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1: true", "2: true", "3: true", "4: false", "5: false"}, delimiter = ':')
+    void setContainsOneTwoThreeFourFive(int input, boolean expected) {
+        if (input <= 3) {
+            assertTrue(expected);
+        } else {
+            assertFalse(expected);
+        }
+    }
+}
