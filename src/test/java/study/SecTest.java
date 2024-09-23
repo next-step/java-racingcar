ac18a36429cab01bd84e6bc43123dca9b568eaab
hwan2-99 <thh1233123@gmail.com>
2024-09-23 19:52:39 +0900

    test: set 크기 확인 테스트 작성

---
+++ src/test/java/study/SecTest.java
@@ -0,0 +1,26 @@
+package study;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class SecTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 크기를 확인한다")
+    void sizeTest() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
