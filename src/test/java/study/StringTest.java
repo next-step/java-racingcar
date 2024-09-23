a722b2d268a4deff39c6646a13a8e165a4c4cb40
Joonkb <byeongjunkim@naver.com>
2024-09-23 23:31:19 +0900

    step-1 요구사항 2 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -13,4 +13,15 @@ public class StringTest {
         assertThat(result).contains("2");
         assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    void substring() {
+        String data = "(1,2)";
+
+        int startIdx = data.indexOf("(");
+        int endIdx = data.indexOf(")");
+        String result = data.substring(startIdx + 1, endIdx);
+
+        assertThat(result).isEqualTo("1,2");
+    }
 }
