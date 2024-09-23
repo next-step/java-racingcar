eb548ce33697796d1a9531a91e30c55e56b8fcec
zooucat <zooucat@gmail.com>
2024-09-24 02:16:12 +0900

    test: Add unit tests for StringAddCalculator
    - Implemented parameterized tests to validate handling of null and empty strings.
    - Added tests for single digit string inputs to ensure correct summation.
    - Created tests for various formatted input strings, including delimiters and custom separators.
    - Added test to check for exception handling with negative numbers.

---
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -0,0 +1,86 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class StringAddCalculatorTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("빈 문자열 또는 null 값을 입력하는 경우 0을 반환한다.")
+    void test01(final String text) {
+        // when
+        final int result = StringAddCalculator.splitAndSum(text);
+
+        // then
+        assertThat(result).isZero();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {"1", "2", "3", "4", "5", "6", "7", "8", "9"})
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    void test02(final String text) {
+        // when
+        final int result = StringAddCalculator.splitAndSum(text);
+
+        // then
+        assertThat(result).isEqualTo(Integer.parseInt(text));
+    }
+
+    @Test
+    @DisplayName("\"1,2\" 문자열을 입력했을 때 3을 반환한다.")
+    void test03() {
+        // given
+        final var text = "1,2";
+
+        // when
+        final int result = StringAddCalculator.splitAndSum(text);
+
+        // then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("\"1,2:3\" 문자열을 입력했을 때 6을 반환한다.")
+    void test04() {
+        // given
+        final var text = "1,2:3";
+
+        // when
+        final int result = StringAddCalculator.splitAndSum(text);
+
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("\"//;\n1;2;3\" 문자열을 입력했을 때 6을 반환한다.")
+    void test05() {
+        // given
+        final var text = "//;\n1;2;3";
+
+        // when
+        final int result = StringAddCalculator.splitAndSum(text);
+
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("\"-1,2,3\" 문자열을 입력했을 때 예외가 발생한다.")
+    void test06() {
+        // given
+        final var text = "-1,2,3";
+
+        // when & then
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(text))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+}
