0a9a00e1a464436da2fe98526861f990ad594022
hunko <rhwlgns4386@naver.com>
2024-09-24 00:55:48 +0900

    test : 숫자 두개 분리 테스트

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,18 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+
+    @Test
+    public void 숫자_두개_분리_테스트(){
+        String[] split = "1,2".split(",");
+        assertThat(split.length).isEqualTo(2);
+        assertThat(split).contains("1","2");
+    }
+
+
+}
