1a6fffc1567334117742328931b1a2a6dcae413d
jureamer <wngud4950@gmail.com>
2024-09-23 21:36:37 +0900

    implement set class test 1

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 메소드를 활용해 Set 크기를 확인한다.")
+    void checkSetSize() {
+        int setSize = numbers.size();
+        assertThat(setSize).isEqualTo(3);
+    }
+}
