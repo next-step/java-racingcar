21b1f1ca61ba7c13fbd9c67c1699bc3832dc4038
hwan2-99 <thh1233123@gmail.com>
2024-09-23 17:52:09 +0900

    test: 문자 반환 예외 테스트 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,6 @@
 package study;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,5 +19,16 @@ public class StringTest {
         char result = value.charAt(1);
 
         assertThat(result).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자를 가져올 때 위치값을 벗어나면 예외가 발생한다.")
+    void indexExceptionTest() {
+        String value = "abc";
+
+        assertThatThrownBy(() -> {
+            char result = value.charAt(4);
+        })
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
