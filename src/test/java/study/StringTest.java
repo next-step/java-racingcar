ea3b01d80f4bd7c9dbe327fc70339671cf597012
hwan2-99 <thh1233123@gmail.com>
2024-09-23 17:45:08 +0900

    test: 문자 반환 테스트 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -9,5 +10,14 @@ public class StringTest {
     void split() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자를 성공적으로 반환한다.")
+    void IndexTest() {
+        String value = "abc";
+        char result = value.charAt(1);
+
+        assertThat(result).isEqualTo('b');
     }
 }
