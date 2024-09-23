5cc2608e3252502398202ac4ff170b23e700366c
owljoa <needshield@gmail.com>
2024-09-23 23:38:17 +0900

    charAt StringIndexOutOfBoundsException test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -21,5 +23,15 @@ public class StringTest {
     void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("대상 문자열의 인덱스 범위를 넘어가는 값이 입력되면 StringIndexOutOfBoundsException이 발생한다")
+    void charAt() {
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    "abc".charAt(-1);
+                }).withMessageMatching("String index out of range: -?\\d+");
+
     }
 }
