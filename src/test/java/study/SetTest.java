22169519f3c35d9ac5b3e656dd97fe6cdce796c3
sinyebin <dpqls0625@naver.com>
2024-09-23 23:25:11 +0900

    24.09.23 syb Set Collection 요구사항1

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set_크기_확인")
+    void checkSetSize(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
