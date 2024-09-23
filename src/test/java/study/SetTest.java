632f1db9b38ee0e8224887679729a34fb3c16b51
Joonkb <byeongjunkim@naver.com>
2024-09-24 00:01:38 +0900

    step-1 SetTest 요구사항 2-3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,9 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -26,5 +29,19 @@ public class SetTest {
     @DisplayName("Set 인터페이스의 size() 메서드를 활용하여 Set의 크기를 확인한다.")
     void sizeOfSetTest() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @DisplayName("ParameterizedTest를 활용하여 원소의 존재여부 테스트")
+    @ValueSource(ints = {1, 2, 3})
+    void containsTrueTest(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("ParameterizedTest를 사용하여 원소의 존재여부 테스트")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsTrueAndFalseTest(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
     }
 }
