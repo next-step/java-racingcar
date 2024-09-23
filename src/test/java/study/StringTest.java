e11189cf74e92a801a3565d3e1f0d16709ba2d4c
Joonkb <byeongjunkim@naver.com>
2024-09-23 22:59:26 +0900

    step - 1 요구사항 구현

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,16 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
+        assertThat(result).containsExactly("1", "2");
+    }
+}
