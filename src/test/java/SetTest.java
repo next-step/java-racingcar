c89a4876462c783e3cf847973ec23d049d2ff329
jureamer <wngud4950@gmail.com>
2024-09-23 21:47:42 +0900

    implement set class test 2

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,12 +1,14 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class SetTest {
     private Set<Integer> numbers;
 
@@ -24,5 +26,11 @@ public class SetTest {
     void checkSetSize() {
         int setSize = numbers.size();
         assertThat(setSize).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void setContainsOneTwoThree(int input) {
+        assertTrue(input >= 1 && input <= 3);
     }
 }
