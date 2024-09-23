2b5cb0f0846162f583bc29cbff68f04b171eacb9
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-09-23 16:55:52 +0900

    정규 표현식으로 split하는 메소드, 숫자 더하는 메소드, 음수 확인하는 메소드 추가
    정규 표현식으로 split하는 메소드, 숫자 더하는 메소드, 음수 확인하는 메소드 추가

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
 
     public static int splitAndSum(String text) {
@@ -5,10 +8,44 @@ public class StringAddCalculator {
             return 0;
         }
 
+        if (text.contains("//")) {
+            String[] numbers = splitStringWithRegex(text);
+            return addNumbers(numbers);
+        }
+
         return 0;
     }
 
     private static boolean checkNullOrEmpty(String text) {
         return text == null || text.isEmpty();
+    }
+
+    private static String[] splitStringWithRegex(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+
+        if (m.find()) {
+            String delimiter = m.group(1);
+            return m.group(2).split(delimiter);
+        }
+        return new String[0];
+    }
+
+    private static int addNumbers(String[] numbers) {
+        checkMinusNumber(numbers);
+
+        int res = 0;
+        for (String number : numbers) {
+            res += Integer.parseInt(number);
+        }
+
+        return res;
+    }
+
+    private static void checkMinusNumber(String[] numbers) {
+        for (String number : numbers) {
+            if (Integer.parseInt(number) < 0) {
+                throw new RuntimeException();
+            }
+        }
     }
 }
