3e438ceea40dd5b375f8e3b26ad5ce751f492247
jureamer <wngud4950@gmail.com>
2024-09-23 21:47:42 +0900

    implement set class test 2

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -2,7 +2,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+
 import org.junit.jupiter.params.provider.CsvSource;
+
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
