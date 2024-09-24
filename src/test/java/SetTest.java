be85da8dcd5fa73991e0a406f9277a3b5c839438
Hoseop.Kim <kimho314@gmail.com>
2024-09-24 15:45:16 +0900

    [feat] : Set Collection 요구사항 1 테스트 코드 작성

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,29 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+
+    @Test
+    @DisplayName("set size 테스트")
+    void setSizeTest() {
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+
+}
