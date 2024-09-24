a14755322417b4c1977256f5235193b8b2d31a58
seulpi <s_bee92@naver.com>
2024-09-24 23:02:48 +0900

    1단계 학습 테스트 실습 완룔

--- src/test/java/stydy/SetTest.java
+++ src/test/java/stydy/SetTest.java
@@ -1,4 +1,58 @@
 package stydy;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // 요구사항1 Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.
+    @Test
+    @DisplayName("Set Method Size Check")
+    void size() {
+        int result = numbers.size();
+        assertThat(result).isEqualTo(3);
+    }
+
+    /* 요구사항2
+        Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
+        구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
+        JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
+     */
+    @DisplayName("ParameterizedTest 연습1")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains1(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    /* 요구사항3
+        요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
+        예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다..
+     */
+    @DisplayName("ParameterizedTest 연습2")
+    @ParameterizedTest
+    @CsvSource(value = {"1=true", "2=true", "3=true", "4=false"}, delimiter = '=')
+    void contains2(int number, boolean result) {
+        assertThat(numbers.contains(number)).isEqualTo(result);
+    }
 }
--- src/test/java/stydy/StringTest.java
+++ src/test/java/stydy/StringTest.java
@@ -1,8 +1,9 @@
 package stydy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
@@ -11,5 +12,33 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    // 요구사항2 "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
+    @Test
+    void subString() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+
+    /* 요구사항3
+        "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+        String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundssException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+        JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+    */
+    @Test
+    @DisplayName("문자 해당 위치 찾기")
+    void findWordLocation() {
+        String word = "abc";
+
+        assertThatThrownBy(() -> {
+            word.charAt(-1);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+
+        assertThatThrownBy(() -> {
+            word.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+
     }
 }
