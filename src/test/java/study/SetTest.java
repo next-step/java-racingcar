226a95f53a43a37ea3b20954f26bf987c9923181
hoya <sv05159@naver.com>
2024-09-24 22:28:20 +0900

    Set contains study test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -30,9 +30,9 @@ public class SetTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3})
-    @DisplayName("contains() - Set에 존재하는 요소에 대해 참을 반환한다.")
-    void contains_ShouldReturnTrueForExistingElement(int input) {
-        assertThat(numbers.contains(input)).isTrue();
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("contains() - Set에 존재하는 요소에 대해 참을 반환하고, 존재하지 않는 요소에 대해 거짓을 반환한다.")
+    void contains_ShouldReturnTrueForExistingElement(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
