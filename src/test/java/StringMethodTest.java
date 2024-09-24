d4667d7546411f12fc579ac5c11431e3985d2ade
Slowth-KIM <rt.slowth@gmail.com>
2024-09-24 19:47:42 +0900

    generate substringTest

--- src/test/java/StringMethodTest.java
+++ src/test/java/StringMethodTest.java
@@ -35,4 +35,19 @@ public class StringMethodTest {
                 .as("\"1\"을 ,로 split 했을 때 1만 포함하는 배열이 반환되는지 확인한다")
                 .contains("1");
     }
+    
+    @DisplayName("(1,2) 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 1,2를 반환한다.")
+    @Test
+    void substringTest() {
+        // Given
+        String input = "(1,2)";
+
+        // When
+        String actual = input.substring(1, input.length() - 1);
+
+        // Then
+        assertThat(actual)
+                .as("(1,2) 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 1,2를 반환한다.")
+                .isEqualTo("1,2");
+    }
 }
