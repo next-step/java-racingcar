233c46ce0cf3c0a96f35d08c4dc727aec3f241db
Slowth-KIM <rt.slowth@gmail.com>
2024-09-24 19:54:15 +0900

    add countSetSize test

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인한다.")
+    @Test
+    void countSetSize() {
+        // Then
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
