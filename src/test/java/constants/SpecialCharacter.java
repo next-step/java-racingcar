a2598d85ecea87ffcec07d5eb9c0f3fbe26439e2
zooucat <zooucat@gmail.com>
2024-09-24 09:26:55 +0900

    🚀 1단계: 학습 테스트 실습 (#5567)
    * test: Add tests for String class
    
    - split
    - substring
    - charAt
    
    * chore: Remove unused package imports in StringTest.java
    
    * test: Add tests for Set class
    
    - size
    - contains
    
    * refactor: Simplify test for numbers size
    
    - Updated test to directly assert the size of numbers using hasSize method.

---
+++ src/test/java/constants/SpecialCharacter.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class SpecialCharacter {
+
+    public static final String COMMA = ",";
+
+    public static final String OPEN_PARENTHESIS = "(";
+    public static final String CLOSE_PARENTHESIS = ")";
+
+}
