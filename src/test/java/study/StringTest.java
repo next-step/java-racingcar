a1f4b7a013e33cefaea5a49cf445b4c10d4ee3f9
JongMinCh0i <wonderland314592@gmail.com>
2024-09-24 11:07:36 +0900

    [test] String 클래스에 대한 학습 테스트 - 요구사항 3
    - 요구사항 3
      - "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
      - String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
      - JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,12 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -42,5 +46,26 @@ public class StringTest {
 
         // when & then
         assertThat(testForParenthesis.substring(1, 4)).isEqualTo("1,2");
+    }
+
+    @DisplayName("charAt 메서드를 활용해 특정 위치의 문자를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"abc"})
+    public void testForCharAt(String inputValue) {
+
+        // when & then
+        assertThat('a').isEqualTo(inputValue.charAt(0));
+        assertThat('b').isEqualTo(inputValue.charAt(1));
+        assertThat('c').isEqualTo(inputValue.charAt(2));
+    }
+
+    @DisplayName("charAt 으로 주어진 문자열의 길이보다 큰 값을 조회 할 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"abc"})
+    public void testForCharAtException(String inputValue) {
+
+        // when & then
+        assertThatThrownBy(() -> inputValue.charAt(inputValue.length() + 1))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
