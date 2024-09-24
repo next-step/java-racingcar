204a741a43841af6e561dea9d7fb1a9ddc7698b6
Hoseop.Kim <kimho314@gmail.com>
2024-09-24 15:43:13 +0900

    [feat] : 요구사항 3 테스트 코드 작성

--- src/test/java/StringClassTest.java
+++ src/test/java/StringClassTest.java
@@ -1,4 +1,5 @@
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringClassTest {
@@ -19,5 +20,30 @@ public class StringClassTest {
         final String input = "(1,2)";
         String substring = input.substring(1, 4);
         Assertions.assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 메소드 테스트")
+    void charAtTest() {
+        final String input = "abc";
+
+        for (int i = 0; i < 3; i++) {
+            char ch = input.charAt(i);
+            int idx = input.indexOf(ch);
+            Assertions.assertThat(idx).isEqualTo(i);
+        }
+    }
+
+    @Test
+    @DisplayName("charAt 메소드 테스트 - 인덱스 범위 벗어났을때 에러 테스트")
+    void charAtTest_IndexOutOfBoundsException() {
+        final String input = "abc";
+        int testIdx = input.length();
+
+        Assertions.assertThatThrownBy(() -> {
+                    char ch = input.charAt(testIdx);
+                }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+
     }
 }
