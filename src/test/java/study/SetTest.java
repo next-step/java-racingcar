fb5fd4f5f9f17124123077a4d030e8df68d795b8
jeonghyeonkwon <givejeong2468@gmail.com>
2024-09-25 01:48:38 +0900

    SetTest - add setup method

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,20 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+}
