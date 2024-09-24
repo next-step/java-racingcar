90d2e1b23541885af57832951ed8fcaf7ba39b47
JongMinCh0i <wonderland314592@gmail.com>
2024-09-24 12:46:26 +0900

    [test] Set Collection 에 대한 학습 테스트 - 요구사항 1
    - 요구사항 1
      - Set의 size() 메소드를 활용해 Set의 크기를 확인하는 학습테스트를 구현한다.

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void testForSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
