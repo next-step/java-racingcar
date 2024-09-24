ee2f642ce18f4d98671ac02f8a685e6d54b469cb
jeonghyeonkwon <givejeong2468@gmail.com>
2024-09-25 01:52:02 +0900

    SetTest - contains method(use valuesSorce)

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,6 +27,12 @@ public class SetTest {
     @Test
     void size(){
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void containsValue(int input){
+        assertThat(numbers.contains(input)).isTrue();
     }
 
 }
