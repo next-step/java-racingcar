c4f19a1a1260e896a57c559d1a632412a7068808
StoneHee99 <kiit0901@gmail.com>
2024-09-24 23:09:48 +0900

    test: size() 메서드가 Set의 크기를 잘 반환하는지

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("size() 메서드가 Set의 크기를 잘 반환하는지")
+    @Test
+    void sizeLearningTest() {
+        assertThat(numbers).hasSize(3);
+    }
+}
