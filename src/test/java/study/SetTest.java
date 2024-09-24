5583acac6fb834b6d18481707f0a864e8e780e74
kim yoonho <84076800+mitsui3point@users.noreply.github.com>
2024-09-24 12:51:38 +0900

    test(study): add string, set 학습테스트 (#5568)

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,54 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() 메소드로 set 의 크기를 구한다.")
+    void size() {
+        int result = numbers.size();
+        assertThat(result).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains() 메소드로 set 내에 존재하는 값들은 참을 반환한다.")
+    void contains(int number) {
+        boolean result = numbers.contains(number);
+        assertThat(result).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,true",
+            "2,true",
+            "3,true",
+            "4,false",
+            "5,false"}, delimiter = ',')
+    @DisplayName("contains() 메소드로 set 내에 값들이 존재하는지 확인한다.")
+    void containsTrueOrFalse(int number, boolean expected) {
+        boolean actual = numbers.contains(number);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
