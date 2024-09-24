ea551f087dff3fa1493489e53400e205b8d2c59c
jeonghyeonkwon <givejeong2468@gmail.com>
2024-09-25 01:53:01 +0900

    SetTest - contains method(use csvSource)

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -33,6 +34,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void containsValue(int input){
         assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true","2:true","3:true","4:false","5:false"}, delimiter = ':')
+    void containsCsv(int input, boolean expected){
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 
 }
