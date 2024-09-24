cea09c178ec6507ee40d8e7398761b2faa8f49ec
Hoseop.Kim <kimho314@gmail.com>
2024-09-24 15:28:12 +0900

    [feat] : 요구사항 1 테스트 코드 작성

---
+++ src/test/java/StringClassTest.java
@@ -0,0 +1,16 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringClassTest {
+    @Test
+    void splitTest() {
+        final String input1 = "1,2";
+
+        String[] split1 = input1.split(",");
+        Assertions.assertThat(split1).containsExactly("1", "2");
+
+        final String input2 = "1";
+        String[] split2 = input2.split(",");
+        Assertions.assertThat(split2).containsExactly("1");
+    }
+}
