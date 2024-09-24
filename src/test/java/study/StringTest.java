1a3d55eb0466148d7f5b9c067d8cfd8c34d529f6
StoneHee99 <kiit0901@gmail.com>
2024-09-24 21:52:31 +0900

    test: "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,14 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+    @Test
+    void splitLearningTest1() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+}
