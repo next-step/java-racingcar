5933407209d01c8b1cc93a59144b7955e22bd5db
Tom <80812697+jongminch0i@users.noreply.github.com>
2024-09-24 21:17:22 +0900

    [chore] 네이밍 규칙 설정에 따른 패키지 이동 - 패키지 네이밍 규칙   - 이후 작성될 미션들과 구분하기 위해서 별도의 패키지를 생성한다   - 네이밍 규칙은 step_ + 1,2,3.. 와 같이 점미사에 해당 미션의 회차를 추가한다

---
+++ src/test/java/study/step_1/SetTest.java
@@ -0,0 +1,46 @@
+package study.step_1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void testForSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void testContains(int checkValue) {
+        assertThat(numbers.contains(checkValue)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2", "3", "4", "5"})
+    void testRemove(int checkValue) {
+        if (1 <= checkValue && checkValue < 4) {
+            assertThat(numbers.contains(checkValue)).isTrue();
+        } else {
+            assertThat(numbers.contains(checkValue)).isFalse();
+        }
+    }
+}
