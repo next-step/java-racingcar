9c2ced064db73b0b95da8a4bc6e08f809866a7ea
hoya <sv05159@naver.com>
2024-09-24 22:04:32 +0900

    Set size study test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size() - Set의 크기는 중복을 제외한 유일한 값의 개수만큼이다.")
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
