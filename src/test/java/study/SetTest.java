82835e20f79c3bc94081b5cbdc1b21ba4b5fd634
StoneHee99 <kiit0901@gmail.com>
2024-09-24 23:15:52 +0900

    chore: ParameterizedTest를 활용한 중복 코드 제거

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SetTest {
     private Set<Integer> numbers;
@@ -27,10 +29,9 @@ class SetTest {
     }
 
     @DisplayName("numbers에 1, 2, 3 값이 모두 존재하는지")
-    @Test
-    void containsLearningTest() {
-        assertThat(numbers.contains(1)).isTrue();
-        assertThat(numbers.contains(2)).isTrue();
-        assertThat(numbers.contains(3)).isTrue();
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void containsLearningTest(int element) {
+        assertThat(numbers.contains(element)).isTrue();
     }
 }
