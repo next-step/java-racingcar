85637fc822c568f09e8d761ffb1c27e07cd48e81
jeonghyeonkwon <givejeong2468@gmail.com>
2024-09-25 01:35:22 +0900

    StringTest - substring method

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -21,7 +21,14 @@ public class StringTest {
     }
 
 
+    @DisplayName("substring() 사용")
+    @Test
+    void substring(){
+        String given = "(1,2)";
 
+        String result = given.substring(1, given.length()-1);
 
+        assertThat(result).isEqualTo("1,2");
+    }
 
 }
