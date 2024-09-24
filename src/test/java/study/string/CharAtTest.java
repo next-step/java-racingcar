facff0b467a9b0ba7a26e8108ce30fdbffd28f7b
hunko <rhwlgns4386@naver.com>
2024-09-24 20:45:33 +0900

    refactoring : 중복 코드 리펙토링
    index테스트 리펙토링 및 공통으로 쓰는 string 공통필드로 변경

--- src/test/java/study/string/CharAtTest.java
+++ src/test/java/study/string/CharAtTest.java
@@ -2,23 +2,25 @@ package study.string;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CharAtTest {
+    private static final String ABC ="abc";
 
-    @Test
+    @ParameterizedTest
     @DisplayName("a추출 테스트")
-    public void popATest(){
-        char a = "abc".charAt(0);
-        assertThat(a).isEqualTo('a');
-        assertThat(a).isNotEqualTo("a");
+    @CsvSource(value = {"0,a","1,b","2,c"})
+    public void popATest(int index,char result){
+        assertThat(ABC.charAt(index)).isEqualTo(result);
     }
 
     @Test
     @DisplayName("인덱스 밖 요소 추출")
     public void popOutIndex(){
-        assertThatThrownBy(()->"abc".charAt(-1)).isInstanceOf(StringIndexOutOfBoundsException.class);
+        assertThatThrownBy(()->ABC.charAt(-1)).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
