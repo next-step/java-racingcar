18052bb2b66a592685e45ab4547ae955b3c368d2
owljoa <needshield@gmail.com>
2024-09-24 22:43:54 +0900

    set contains test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,5 +26,11 @@ public class SetTest {
     void size() {
         int size = numbers.size();
         assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
