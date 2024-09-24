fe0ae6168143b62b8d189ae440de8563068714f9
tjdjdjrdl123 <lee-won-suk@github.io>
2024-09-24 23:08:16 +0900

    [리팩토링][Step1] 1단계 StringClass테스트 및 Set Collection 학습 테스트
    1. 네이밍 고려 및 수정 추가
    2. Nested와 DisplayName 동시 사용시 DisplayName이 안나오던 문제 수정

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class SetTest {
-    static Set<Integer> numbers;
+    private Set<Integer> numbers;
 
     @BeforeEach
     void setUp() {
@@ -23,38 +23,36 @@ public class SetTest {
 
     @Nested
     @DisplayName("첫번째 요구사항 클래스")
-    class firstRequiredTest {
+    class UsingSizeTest {
         /**
          * 요구사항 : size() 메서드를 활용해서 Set의 크기 확인 테스트 구현
          */
         @Test
         @DisplayName(value = "Size()메서드로 크기 확인 테스트")
-        public void sizeEqualTest() {
-            assertThat(SetTest.numbers).hasSize(3);
+        public void checkSizeEqualTest() {
+            assertThat(numbers).hasSize(3);
         }
     }
 
     @Nested
     @DisplayName("두번째 요구사항 클래스")
-    class secondRequiredTest {
+    class UsingParameterizedTest {
 
-        @DisplayName(value = "1,2,3값 존재여부 ParameterizedTest로 중복 제거 테스트")
-        @ParameterizedTest()
+        @ParameterizedTest(name =  "1,2,3값 존재여부 ParameterizedTest로 중복 제거 테스트 {index}번쨰값 : {0}")
         @ValueSource(ints = {1, 2, 3})
         void contains(int number) {
-            assertThat(SetTest.numbers.contains(number)).isTrue();
+            assertThat(numbers.contains(number)).isTrue();
         }
     }
 
     @Nested
     @DisplayName("세번째 요구사항 클래스")
-    class thirdRequiredTest {
+    class ParameterizeWithCsvSourceTest {
 
-        @DisplayName(value = "parameterizedTest 실패케이스 테스트")
-        @ParameterizedTest()
+        @ParameterizedTest(name =  "parameterizedTest 실패케이스 테스트 {index}번쨰값 : {0}")
         @CsvSource({"1","2","3","4","5"})
-        void contains(int number) {
-            assertThat(SetTest.numbers.contains(number)).isTrue();
+        void containsIncludeFalseData(int number) {
+            assertThat(numbers.contains(number)).isTrue();
         }
 
     }
--- src/test/java/StringClassTest.java
+++ src/test/java/StringClassTest.java
@@ -7,21 +7,21 @@ public class StringClassTest {
 
     @Nested
     @DisplayName("첫번째 요구사항 클래스")
-    class firstRequiredTest {
+    class NumberTest {
           private String NumbersData = "1,2";
           private String NumberData = "1";
           private String [] array;
 
         @Test
         @DisplayName(value = "1,2 split으로 분리여부 확인 테스트")
-        public void numbersSplitTest() {
+        public void splitNumbersTest() {
             array = NumbersData.split(",");
             assertThat(array).contains("1","2");
         }
 
         @Test
         @DisplayName(value = "1 split후 1만 포함하는지 확인 테스트")
-        public void oneNumberSplitTest() {
+        public void splitNumberTest() {
             array = NumberData.split(",");
             assertThat(array).containsExactlyInAnyOrder("1");
         }
@@ -30,12 +30,12 @@ public class StringClassTest {
 
     @Nested
     @DisplayName("두번째 요구사항 클래스")
-    class secondRequiredTest {
+    class UseSubStringTest {
         private String require2NumberData = "(1,2)";
 
         @Test
         @DisplayName(value = "(1,2) subString사용해서 1,2만 반환 테스트")
-        public void require2Test() {
+        public void shouldReturnNumberTest() {
             String result = require2NumberData.substring(1,4);
             assertThat("1,2").isEqualTo(result);
         }
@@ -44,19 +44,19 @@ public class StringClassTest {
 
     @Nested
     @DisplayName("세번째 요구사항 클래스")
-    class thirdRequiredTest {
+    class StringDataTest {
         private String require2abcData = "abc";
 
         @Test
         @DisplayName(value = "abc에서 특정위치 문자 가져오는 테스트")
-        public void require2charAtTest() {
+        public void getCharFromStringDataTest() {
             char result = require2abcData.charAt(0);
             assertThat('a').isEqualTo(result);
         }
 
         @Test
         @DisplayName(value = "abc 데이터 indexOutOfBoundsException 발생 테스트")
-        public void require2charAtExceptionTest() {
+        public void makeOutOfBOundsExceptionFromCharAtTest() {
             assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
                     .isThrownBy(() ->{
                         require2abcData.charAt(4);
