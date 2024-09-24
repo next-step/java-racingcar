b77f6b5eed81331d6d177071010823df4253706a
Hoseop.Kim <kimho314@gmail.com>
2024-09-24 15:59:43 +0900

    [feat] : Set Collection 요구사항 3 테스트 코드 작성

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,5 +35,14 @@ public class SetTest {
     @DisplayName("set의 값 존재 여부 테스트")
     void setContainsElementsTest(int idx) {
         Assertions.assertThat(numbers.contains(idx)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("set의 다양한 결과값 테스트")
+    void setContainsMultiValueTest(String value, String expected) {
+        int inputValue = Integer.parseInt(value);
+        boolean expectedResult = Boolean.parseBoolean(expected);
+        Assertions.assertThat(numbers.contains(inputValue)).isEqualTo(expectedResult);
     }
 }
