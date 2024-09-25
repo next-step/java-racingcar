69b6c7e3c115071c87cedd5e9f9feaf9189dea04
mitsui3point <rladbsgh0331@gmail.com>
2024-09-25 23:24:58 +0900

    feat(SplitUtil): update integers 숫자 이외의 값 또는 음수를 전달하는 경우 예외 처리
    test(SplitUtil): add negativeNumberOrNotNumberSplitError

--- src/main/java/calculator/util/SplitUtil.java
+++ src/main/java/calculator/util/SplitUtil.java
@@ -7,16 +7,18 @@ import java.util.regex.Pattern;
 public class SplitUtil {
     private static final String COMMA_OR_SEMI_COLON_REGEX = ",|;";
     private static final String CUSTOM_REGEX = "//(.)\n(.*)";
-    private static final int EMPTY_RESULT = 0;
+    private static final String EMPTY_RESULT = "0";
 
     public static Integer[] integers(String input) {
         if (isInputEmpty(input)) {
             return emptyIntegers();
         }
+
         Matcher matcher = customDelimiterMatcher(input);
         if (isMatcherFind(matcher)) {
             return customIntegers(matcher);
         }
+
         return defaultIntegers(input);
     }
 
@@ -28,27 +30,46 @@ public class SplitUtil {
         return input == null || input.isEmpty();
     }
 
-    private static Integer[] emptyIntegers() {
-        return new Integer[]{EMPTY_RESULT};
-    }
-
-    private static Integer[] defaultIntegers(String input) {
-        return Arrays.stream(input.split(COMMA_OR_SEMI_COLON_REGEX))
-                .map(Integer::parseInt)
-                .toArray(Integer[]::new);
-    }
-
-    private static Integer[] customIntegers(Matcher matcher) {
-        String customDelimiter = matcher.group(1);
-        String[] splitResult = matcher.group(2)
-                .split(customDelimiter);
-        return Arrays.stream(splitResult)
-                .map(Integer::parseInt)
-                .toArray(Integer[]::new);
-    }
-
     private static Matcher customDelimiterMatcher(String input) {
         return Pattern.compile(CUSTOM_REGEX)
                 .matcher(input);
+    }
+
+    private static Integer[] emptyIntegers() {
+        return parsedIntegers(new String[]{EMPTY_RESULT});
+    }
+
+    private static Integer[] defaultIntegers(String input) {
+        return parsedIntegers(input.split(COMMA_OR_SEMI_COLON_REGEX));
+    }
+
+    private static Integer[] customIntegers(Matcher matcher) {
+        String delimiter = matcher.group(1);
+        String numberStrings = matcher.group(2);
+
+        return parsedIntegers(numberStrings.split(delimiter));
+    }
+
+    private static Integer[] parsedIntegers(String[] splitResults) {
+        return Arrays.stream(splitResults)
+                .map(SplitUtil::parseInt)
+                .toArray(Integer[]::new);
+    }
+
+    private static int parseInt(String result) {
+        try {
+            int parsedInteger = Integer.parseInt(result);
+            validateNegativeNumber(parsedInteger);
+            return parsedInteger;
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("문자는 입력할 수 없습니다.");
+        }
+    }
+
+    private static void validateNegativeNumber(int parsedInteger) {
+        boolean isNegativeNumber = parsedInteger < 0;
+        if (isNegativeNumber) {
+            throw new RuntimeException("0 이상의 숫자 외에는 입력할 수 없습니다.");
+        }
     }
 }
--- src/test/java/calculator/util/SplitUtilTest.java
+++ src/test/java/calculator/util/SplitUtilTest.java
@@ -4,24 +4,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SplitUtilTest {
 
     @ParameterizedTest
     @CsvSource(value = {
-            ":0",
-            "'':0",
-            "1:1",
-            "1,2:1,2",
-            "1;2:1,2",
-            "1,2,3:1,2,3",
-            "1;2,3:1,2,3",
-            "1,2;3:1,2,3",
-            "1;2;3:1,2,3",
+            ":0", "'':0",
+            "0:0", "1:1",
+            "1,2:1,2", "1;2:1,2",
+            "1,2,3:1,2,3", "1;2,3:1,2,3", "1,2;3:1,2,3", "1;2;3:1,2,3",
     }, delimiter = ':')
     @DisplayName("쉼표(,), 세미콜론(;) 을 포함한 문자열이 오면 숫자 배열로 분리한다.")
     void commaOrSemiColonSplit(String input, String results) {
@@ -38,6 +35,18 @@ public class SplitUtilTest {
         Integer[] expected = new Integer[]{1, 2, 3};
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "a,1,2", "a;1,2", "a,1;2", "a;1;2",
+            "-1,1,2", "-1;1,2", "-1,1;2", "-1;1;2",
+    })
+    @DisplayName("숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다.")
+    void negativeNumberOrNotNumberSplitError(String input) {
+        assertThatThrownBy(() -> SplitUtil.integers(input))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("입력할 수 없습니다.");
     }
 
     private static Integer[] expectedResults(String results) {
