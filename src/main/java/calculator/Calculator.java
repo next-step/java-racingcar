60653c415bd07e8ae772ed3c810c4dd3d4e016b7
hunko <rhwlgns4386@naver.com>
2024-09-25 20:55:29 +0900

    feat : comma 구분자 더하기 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,11 +2,11 @@ package calculator;
 
 public class Calculator {
     public int sum(String input) {
-        String[] split = input.split(":");
+        String[] split = input.split("[:,]");
 
-        int sum=0;
+        int sum = 0;
         for (String number : split) {
-            sum+=Integer.parseInt(number);
+            sum += Integer.parseInt(number);
         }
         return sum;
     }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -8,9 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CalculatorTest {
 
     @Test
-    @DisplayName("콜론구분자 더하기")
-    public void sumWithColonDelimiter(){
-        Calculator calculator=new Calculator();
+    @DisplayName("콜론 구분자 더하기")
+    public void sumWithColonDelimiter() {
+        Calculator calculator = new Calculator();
         assertThat(calculator.sum("1:2")).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("콤마 구분자 더하기")
+    public void sumWithCommaDelimiter() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.sum("1,2")).isEqualTo(3);
     }
 }
