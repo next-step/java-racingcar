b493188081746c2c1a7b153b6c4c108df86f9f92
sookyshin <sookyshin@smilegate.com>
2024-09-25 19:58:39 +0900

    feat: 테스트 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -13,9 +13,6 @@ public class StringTest {
         String num = "1,2";
         String[] splitExactlyStr = num.split(",");
         assertThat(splitExactlyStr).containsExactly("1","2");
-
-        String[] splitStr = "1".split(",");
-        assertThat(splitStr).contains("1");
     }
 
     @Test
@@ -29,6 +26,13 @@ public class StringTest {
     @Test
     @DisplayName("charAt 테스트")
     void charAtTest() {
+        String charStr = "abc";
+        assertThat(charStr.charAt(1)).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("charAt Exception 테스트")
+    void charAtExceptionTest() {
         String charStr = "abc";
         assertThatThrownBy(() -> {
             charStr.charAt(4);
