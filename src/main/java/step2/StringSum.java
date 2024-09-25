fc8dd47944ef3ca86701c90959c4fb6205ad7eb2
leewonsuk <tjdjdjrdl123@naver.com>
2024-09-26 00:44:08 +0900

    [기능][step2] 커스텀 구분자 반환 메서드 추가

--- src/main/java/step2/StringSum.java
+++ src/main/java/step2/StringSum.java
@@ -7,6 +7,15 @@ public class StringSum {
 
     private String[] numberStringArray;
 
+
+    /**
+     *
+     * 문자열들어오면 숫자 합 반환해주는 메서드
+     */
+    public int splictAndSum(String reqStr) {
+       //TODO
+        return 0;
+    }
     /**
      * 구분자 기준으로 숫자들 분리
      */
@@ -18,8 +27,20 @@ public class StringSum {
      * 문자열 배열 내부 값들 합 반환
      */
     public int addArrayData(String [] Array){
-        int sum= Arrays.stream(Array).map(Integer::parseInt).mapToInt(i -> i).sum();
-        return sum;
+        return Arrays.stream(Array).map(Integer::parseInt).mapToInt(i -> i).sum();
+    }
+
+    /**
+     *  커스텀 구분자 기능
+     *  문자열 앞부분의 //과 \n 사이 위치문자 찾기
+     * 1. 정규표현식으로 해당 부분 찾고 가운데 반환
+     * 2. //다음자리문자열 반환
+     */
+    public String customDelimiter (String reqStr) {
+        if (reqStr.contains("//") && reqStr.contains("\\n")) {
+            return reqStr.substring(2,3);
+        }
+        return "";
     }
 
 }
