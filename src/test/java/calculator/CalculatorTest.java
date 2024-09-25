0f91d808fd767e9051e7d9b511bd1d91d5451828
hunko <rhwlgns4386@naver.com>
2024-09-25 21:20:22 +0900

    refactoring : 반복되는 객체생성로직 리펙토링

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,45 +7,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
+    private Calculator calculator=new Calculator();
+
     @Test
     @DisplayName("콜론 구분자 더하기")
     public void sumWithColonDelimiter() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.sum("1:2")).isEqualTo(3);
     }
 
     @Test
     @DisplayName("콤마 구분자 더하기")
     public void sumWithCommaDelimiter() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.sum("1,2")).isEqualTo(3);
     }
 
     @Test
     @DisplayName("콤마와 콜론 혼합 더하기")
     public void sumWithCommaAndColonDelimiter() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.sum("1,2:3")).isEqualTo(6);
     }
 
     @Test
     @DisplayName("빈 문자 더하기")
     public void sumWithEmptyString() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.sum("")).isEqualTo(0);
     }
 
     @Test
     @DisplayName("공백 문자 더하기")
     public void sumWithBlankString() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.sum(" ")).isEqualTo(0);
     }
 
     @Test
     @DisplayName("null 더하기")
     public void sumWithNull() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.sum(null)).isEqualTo(0);
     }
 }
