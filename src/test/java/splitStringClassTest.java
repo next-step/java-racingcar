1a4f3e72c23b1bac48dcd9e9d9c848a20dfced7f
haein-side <kimhi1640@gmail.com>
2024-09-26 05:41:14 +0900

    test: 1,2를 ,로 split한 경우 테스트 구현

---
+++ src/test/java/splitStringClassTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class splitStringClassTest {
+
+    @Test
+    void split1And2String() throws Exception {
+        // given
+        String testString = "1,2";
+
+        // when
+        String[] result = testString.split(",");
+
+        // then
+        assertThat(result).contains("1", "2");
+    }
+}
