6fb14b278dbc6f84fea6bbe2daca595115399da0
이윤선 <yoonseon3@gmail.com>
2024-09-25 14:58:03 +0900

    test: set size 테스트 케이스 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메서드가 올바른 size를 반환하는지")
+    void size_returns_collect_size_when_set_is_size() {
+        assertThat(numbers).hasSize(3);
+    }
+
+}
