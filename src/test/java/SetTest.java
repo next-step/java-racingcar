a5604b6f4f4d4ea22a48ea6d3e2f95be20eaf69e
DOHEE KIM <rt.slowth@gmail.com>
2024-09-25 12:58:38 +0900

    [slowth-kim] Step 1 : 1단계 - 학습 테스트 실습 (#5579)
    * generate SplitString class
    
    * generate splitWithCommaForTwoElementsTest
    
    * generate splitWithCommaForOneElementTest
    
    * generate substringTest
    
    * generate charAtTests
    
    * remove SplitString class
    
    * add countSetSize test
    
    * add contains test using ParameterizedTest
    
    * add containsEachElement test using ParameterizedTest, CsvSource

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인한다.")
+    @Test
+    void countSetSize() {
+        // Then
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Set에 특정 값이 포함되어 있는지 확인한다")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})  // 테스트할 값들 제공
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @DisplayName("Set에 특정 값이 포함되어 있는지 여부를 확인한다")
+    @ParameterizedTest
+    @CsvSource({
+            "1, true",
+            "2, true",
+            "3, true",
+            "4, false",
+            "5, false"
+    })
+    void containsEachElement(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+
+}
