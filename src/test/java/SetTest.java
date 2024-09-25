6d3fd52d1d525dd038c8032109e8dbf0d4dcea94
soohyun <developer.shkim@gmail.com>
2024-09-25 12:27:19 +0900

    test: 1단계 학습 테스트 실습

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    public void size_ShouldReturnSizeOfSet() {
+        int result = numbers.size();
+
+        assertThat(result).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void contains_ShouldReturnWhetherOrNotItContains(int number) {
+        boolean result = numbers.contains(number);
+
+        assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    public void contains_ShouldReturnWhetherOrNotItContainsWithCsv(int number, boolean expectedResult) {
+        boolean result = numbers.contains(number);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+}
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    public void split_ShouldReturnSplitValues() {
+        String target = "1,2";
+        String[] result = target.split(",");
+
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    public void split_ShouldReturnSplitValue() {
+        String target = "1";
+        String[] result = target.split(",");
+
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    public void substring_ShouldReturnSubstring() {
+        String target = "(1,2)";
+        String result = target.substring(1, 4);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    public void charAt_ShouldReturnSpecifiedChar() {
+        String target = "abc";
+        char[] expectedResult = {'a', 'b', 'c'};
+
+        for (int i = 0; i < target.length(); i++) {
+            char c = target.charAt(i);
+            assertThat(c).isEqualTo(expectedResult[i]);
+        }
+    }
+
+    @Test
+    @DisplayName("charAt 으로 length 이상의 인덱스의 char 를 추출하려고 하면 예외를 발생시킨다.")
+    public void charAt_ShouldThrowStringIndexOutOfBoundsException() {
+        String target = "abc";
+
+        Random random = new Random();
+        int invalidIndex = random.nextInt(Integer.MAX_VALUE) + target.length();
+
+        assertThatThrownBy(() -> {
+            target.charAt(invalidIndex);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class).hasMessageContaining("String index out of range: " + invalidIndex);
+    }
+}
