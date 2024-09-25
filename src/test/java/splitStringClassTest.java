654a9ed4039bb610fa8c83b8f58884520c227914
haein-side <kimhi1640@gmail.com>
2024-09-26 05:41:59 +0900

    test: 1을 ,로 split했을 때 1만을 포함하는 배열 반환 테스트 구현

--- src/test/java/splitStringClassTest.java
+++ src/test/java/splitStringClassTest.java
@@ -16,4 +16,16 @@ class splitStringClassTest {
         // then
         assertThat(result).contains("1", "2");
     }
+
+    @Test
+    public void split1() throws Exception {
+        // given
+        String testString = "1";
+
+        // when
+        String[] result = testString.split(",");
+
+        // then
+        assertThat(result).containsExactly("1");
+    }
 }
