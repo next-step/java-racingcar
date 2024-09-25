bc6e9433366037d805ef0ef524265fe3d832b83c
BISohn27 <skylord418@gmail.com>
2024-09-25 12:44:07 +0900

    use static constant and customized exception for a better readability

--- src/main/java/calculator/CustomizedTokenCalculator.java
+++ src/main/java/calculator/CustomizedTokenCalculator.java
@@ -4,10 +4,10 @@ public class CustomizedTokenCalculator extends Calculator {
 
     @Override
     protected String[] operands(String str) {
-        String[] splitResult = str.split("\n");
+        String[] splitResult = str.split(Constants.CUSTOMIZED_SEPARATOR_SUFFIX);
 
         String customizedToken = splitResult[0]
-            .substring(splitResult[0].lastIndexOf("/") + 1);
+            .substring(splitResult[0].lastIndexOf(Constants.CUSTOMIZED_SEPARATOR_PREFIX) + 1);
 
         return splitResult[1].split("[" + customizedToken + "]");
     }
