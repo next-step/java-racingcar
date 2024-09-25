2d8449a558b1e45f92cbcc41ca62bdbfae3ff4c6
owljoa <needshield@gmail.com>
2024-09-26 00:48:23 +0900

    add custom delimiter pattern to recognize

--- src/main/java/caculator/StringCalculator.java
+++ src/main/java/caculator/StringCalculator.java
@@ -2,27 +2,53 @@ package caculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
     private static final String COMMA = ",";
     private static final String COLON = ":";
+    private static final String CUSTOM_DELIMITER_MATCH_PATTERN = "//(.)\\\\n(.*)";
+    private static final int CUSTOM_DELIMITER_MATCH_GROUP_NUMBER = 1;
+    private static final int PARSING_TARGET_MATCH_GROUP_NUMBER = 2;
 
     public static int splitAndSum(String text) {
         if (text == null || text.isBlank()) {
             return 0;
         }
 
-        String regexForSplit = makeRegexForSplit();
-        String[] tokens = text.split(regexForSplit);
-        List<Integer> parsedNumbers = parseTokens(tokens);
-        int sum = sum(parsedNumbers);
+        Matcher matched = Pattern.compile(CUSTOM_DELIMITER_MATCH_PATTERN).matcher(text);
+        if (matched.find()) {
+            return splitAndSumWithCustomDelimiter(matched);
+        }
 
-        return sum;
+        String[] tokens = split(text, null);
+        List<Integer> parsedNumbers = parseTokens(tokens);
+
+        return sum(parsedNumbers);
     }
 
-    private static String makeRegexForSplit() {
-        return "[" + COMMA + COLON + "]";
+    private static int splitAndSumWithCustomDelimiter(Matcher matched) {
+        String customDelimiter = matched.group(CUSTOM_DELIMITER_MATCH_GROUP_NUMBER);
+        String parsingTargetText = matched.group(PARSING_TARGET_MATCH_GROUP_NUMBER);
+
+        String[] tokens = split(parsingTargetText, customDelimiter);
+        List<Integer> parsedNumbers = parseTokens(tokens);
+
+        return sum(parsedNumbers);
+    }
+
+    private static String[] split(String parsingTargetText, String customDelimiter) {
+        String regexForSplit = makeRegexForSplit(customDelimiter);
+        return parsingTargetText.split(regexForSplit);
+    }
+
+    private static String makeRegexForSplit(String customDelimiter) {
+        if (customDelimiter == null) {
+            return "[" + COMMA + COLON + "]";
+        }
+        return "[" + COMMA + COLON + customDelimiter + "]";
     }
 
     private static List<Integer> parseTokens(String[] tokens) {
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -49,4 +49,14 @@ public class StringCalculatorTest {
         // then
         assertThat(result).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"//;\\n1;2;3|6", "//#\\n1#2#3|6", "//$\\n1,2$3|6", "//%\\n1%2,3:4|10"}, delimiter = '|')
+    void splitAndSum_custom_구분자(String input, int expected) {
+        // when
+        int result = StringCalculator.splitAndSum(input);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
 }
