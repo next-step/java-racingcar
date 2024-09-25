036c440a53530f5931cc648f435916c3d88adfa6
이윤선 <yoonseon3@gmail.com>
2024-09-26 02:59:51 +0900

    feature: step2 문자열 덧셈 계산기 구현

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,57 @@
+package step2;
+
+import java.util.Arrays;
+
+import static step2.util.StringUtils.hasText;
+
+public class StringAddCalculator {
+
+    private StringAddCalculator() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    private static final String REGEX_COMMA_OR_COLON = "[,:]";
+    private static final String PREFIX = "//";
+    private static final String SUFFIX = "\n";
+
+    public static int splitAndSum(String input) {
+        if (!hasText(input)) {
+            return 0;
+        }
+        return sum(makeArray(input));
+    }
+
+    private static int sum(String[] array) {
+        return Arrays.stream(array)
+                .mapToInt(StringAddCalculator::parseIntOrThrow)
+                .sum();
+    }
+
+    private static int parseIntOrThrow(String str) {
+        var num = Integer.parseInt(str);
+        if (num < 0) throw new RuntimeException();
+        return num;
+    }
+
+    private static String[] makeArray(String input) {
+        if (isCustomDelimiter(input)) {
+            var delimiter = makeCustomDelimiter(input);
+            var customInput = makeCustomInput(input);
+            return customInput.split(delimiter);
+        }
+        return input.split(REGEX_COMMA_OR_COLON);
+    }
+
+    private static String makeCustomInput(String input) {
+        return input.substring(input.indexOf(SUFFIX) + SUFFIX.length());
+    }
+
+    private static String makeCustomDelimiter(String input) {
+        return input.substring(input.indexOf(PREFIX) + PREFIX.length(),
+                input.indexOf(SUFFIX));
+    }
+
+    private static boolean isCustomDelimiter(String input) {
+        return input.contains(PREFIX) && input.contains(SUFFIX);
+    }
+}
---
+++ src/main/java/step2/util/StringUtils.java
@@ -0,0 +1,12 @@
+package step2.util;
+
+public class StringUtils {
+
+    private StringUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static boolean hasText(String str) {
+        return str != null && !str.trim().isEmpty();
+    }
+}
