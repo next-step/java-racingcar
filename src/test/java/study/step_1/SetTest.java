c1598b7d3e1acdb0a8a60f8ee7ca1b23a593a218
JongMinCh0i <wonderland314592@gmail.com>
2024-09-25 09:16:28 +0900

    [chore] 네이밍 규칙 설정에 따른 패키지 이동
    - 패키지 네이밍 규칙
      - 이후 작성될 미션들과 구분하기 위해서 별도의 패키지를 생성한다
      - 네이밍 규칙은 step_ + 1,2,3.. 와 같이 점미사에 해당 미션의 회차를 추가한다

--- src/test/java/study/step_1/SetTest.java
+++ src/test/java/study/step_1/SetTest.java
@@ -1,4 +1,4 @@
-package study;
+package study.step_1;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- src/test/java/study/step_1/StringTest.java
+++ src/test/java/study/step_1/StringTest.java
@@ -1,4 +1,4 @@
-package study;
+package study.step_1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
