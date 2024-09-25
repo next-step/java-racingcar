90f9b5ad35230769712171976186dc7fc4818e28
yun <90806941+yunji1201@users.noreply.github.com>
2024-09-26 00:21:42 +0900

    refactor : 메소드명 변경 split() -> splitTwoNum()

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 
     @Test
-    void split() {
+    void splitTwoNum() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
