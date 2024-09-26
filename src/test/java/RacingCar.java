0fba2b121d91fd655a0fef0d03bb836e692a57aa
jureamer <wngud4950@gmail.com>
2024-09-27 06:38:39 +0900

    feat: add functionality to receive input value from the user

--- src/test/java/RacingCar.java
+++ src/test/java/RacingCar.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class RacingCar {
+import java.util.Random;
+import java.util.Scanner;
+
+public class RacingCar {
+    private int[] car;
+    private int carCount;
+    private int moveCount;
+    public RacingCar() {
+        System.out.println("자동차 대수는 몇 대인가요?");
+        Scanner scanner = new Scanner(System.in);
+        int carCount = scanner.nextInt();
+
+        this.carCount = carCount;
+        this.car = new int[carCount];
+        System.out.println(carCount);
+
+        System.out.println("시도할 회수는 몇 회인가요?");
+        int moveCount = scanner.nextInt();
+        this.moveCount = moveCount;
+        System.out.println(moveCount);
+    }
 }
