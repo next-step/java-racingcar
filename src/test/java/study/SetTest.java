bb020a67e17711ecf490e9488e3915e66eba9a15
yun <90806941+yunji1201@users.noreply.github.com>
2024-09-26 09:23:22 +0900

    Step1 (#5615)
    * test : "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 테스트
    
    * refactor : 메소드명 변경 split() -> splitTwoNum()
    
    * test : "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 테스트
    
    * test : "(1,2)" 값에서 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하는 테스트
    
    * test : charAt() 메소드로 특정 위치의 문자 가져오는 테스트
    
    * test : 위치 값 벗어났을 떄의 StringIndexOutOfBoundsException 발생 테스트
    
    * test : @DisplayName 사용하여 가독성 향상
    
    * test : @BeforeEach로 테스트 메서드가 실행되기 전에 매번 실행되는 메서드 정의
    
    * test : size() 메소드를 통해 Set의 크기를 확인하는 테스트
    
    * test : Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인 테스트
    
    * test : @ParameterizedTest @ValueSource 활용해 중복 코드 제거
    
    * test : @CsvSource 활용하여 true/false 검증을 위한 파라미터화 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 size() 메소드로 Set의 크기를 확인")
+    void setSize() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set의 contains() 메소드로 1,2,3의 값이 존재하는지 확인")
+    void setContains(int values) {
+        assertThat(numbers.contains(values)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    @DisplayName("Set의 contains() 메소드로 1,2,3 값이 존재하는지 아닌지 확인")
+    void setContainsWithBoolean(int values, boolean expected) {
+        assertThat(numbers.contains(values)).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,50 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("문자열 '1,2'를 ','로 분리하여 두 개의 숫자로 나누어 결과 확인")
+    void splitTwoNum() {
+        String[] result = "1,2".split(",");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("문자열 '1'을 ','로 분리하여 하나의 숫자를 포함하는 배열 확인")
+    void splitOneNum() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("substring() 메서드로 문자열 '(1,2)'에서 괄호를 제거하고 내용 추출")
+    void substringParentheses() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() 메서드를 사용하여 특정 위치의 문자 추출")
+    void getCharacterUsingCharAt() {
+        String str = "abc";
+        char result = str.charAt(0);
+        assertThat(result).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("인덱스가 범위를 벗어날 경우 예외 발생 확인")
+    void StringIndexOutOfBoundsExceptionTest() {
+        String str = "abc";
+
+        assertThatThrownBy(() -> {
+            str.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3"); // String index out of range: 3
+    }
+
+}
