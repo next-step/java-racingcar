b4478f269fe2bb96eb116cebcc9fc245e75f9d28
BISohn27 <skylord418@gmail.com>
2024-09-26 10:35:23 +0900

    feat : add racing info
    InfoView to get racing info from user

---
+++ src/main/java/racing/RacingCar.java
@@ -0,0 +1,8 @@
+package racing;
+
+public class RacingCar {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        RacingInput input = inputView.racingInput();
+    }
+}
---
+++ src/main/java/racing/RacingInput.java
@@ -0,0 +1,20 @@
+package racing;
+
+public class RacingInput {
+
+    private final int numOfCar;
+    private final int numOfAttempt;
+
+    public RacingInput(int numOfCar, int numOfAttempt) {
+        this.numOfCar = numOfCar;
+        this.numOfAttempt = numOfAttempt;
+    }
+
+    public int getNumOfCar() {
+        return numOfCar;
+    }
+
+    public int getNumOfAttempt() {
+        return numOfAttempt;
+    }
+}
