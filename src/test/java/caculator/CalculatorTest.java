56e50606cb966d021d13fc2d8018f62e14e2e391
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-09-26 21:23:05 +0900

    Separate the text split method functionality

--- src/test/java/caculator/CalculatorTest.java
+++ src/test/java/caculator/CalculatorTest.java
@@ -9,41 +9,45 @@ public class CalculatorTest {
     
     @Test
     public void splitAndSum_null_또는_빈문자() throws Exception {
-        int result = calculator.splitString(null);
-        assertThat(result).isEqualTo(0);
-
-        result = calculator.splitString("");
+        String[] textSplitArr = calculator.splitText(null);
+        int result = calculator.calculate(textSplitArr);
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     public void splitAndSum_숫자하나() throws Exception {
-        int result = calculator.splitString("1");
+        String[] textSplitArr = calculator.splitText("1");
+        int result = calculator.calculate(textSplitArr);
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     public void splitAndSum_쉼표구분자() throws Exception {
-        int result = calculator.splitString("1,2");
+        String[] textSplitArr = calculator.splitText("1,2");
+        int result = calculator.calculate(textSplitArr);
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        int result = calculator.splitString("1,2:3");
+        String[] textSplitArr = calculator.splitText("1,2:3");
+        int result = calculator.calculate(textSplitArr);
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_custom_구분자() throws Exception {
-        int result = calculator.splitString("//;\n1;2;3");
+        String[] textSplitArr = calculator.splitText("//;\n1;2;3");
+        int result = calculator.calculate(textSplitArr);
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_negative() throws Exception {
-        assertThatThrownBy(() -> calculator.splitString("-1,2,3"))
-                .isInstanceOf(RuntimeException.class);
+        String[] textSplitArr = calculator.splitText("-1,2,3");
+        assertThatThrownBy(() -> calculator.validatePositive(textSplitArr))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("음수가 포함되어 있습니다");
     }
 }
 
