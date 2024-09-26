8b3732b1337bd51ca1cb934eeb86035465695cac
Terry <terry@channel.io>
2024-09-26 18:48:53 +0900

    string test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,7 +1,9 @@
 package study;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
@@ -10,4 +12,27 @@ public class StringTest {
     String[] result = "1,2".split(",");
     assertThat(result).containsExactly("1", "2");
   }
+
+  @Test
+  void substring() {
+    String result = "(1,2)".substring(1, 4);
+    assertThat(result).isEqualTo("1,2");
+  }
+
+  @DisplayName("Find a character with specific location")
+  @Test
+  void charAt() {
+    Character result = "abc".charAt(1);
+    assertThat(result).isEqualTo('b');
+  }
+
+  @DisplayName("Invalid index throws IndexOutOfBoundsException")
+  @Test
+  void charAtException() {
+    assertThatExceptionOfType(IndexOutOfBoundsException.class)
+        .isThrownBy(() -> {
+          "abc".charAt(5);
+        }).withMessageMatching("String index out of range: 5");
+  }
+
 }
