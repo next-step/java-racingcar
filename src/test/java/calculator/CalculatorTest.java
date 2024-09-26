c1e40b60f7dc34899e8269ff2d44353c4172ee8d
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-09-26 22:39:07 +0900

    reduce indent

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,49 +9,55 @@ public class CalculatorTest {
     
     @Test
     public void splitAndSum_null_또는_빈문자() throws Exception {
-        String[] textSplitArr = calculator.splitText(null);
-        int[] numbers = calculator.StringArrToIntArr(textSplitArr);
-        int result = calculator.calculate(numbers);
-        assertThat(result).isEqualTo(0);
+        assertThat(calculator.calculate
+                (calculator.stringArrToIntArr
+                        (calculator.splitText(null))
+                )
+        ).isEqualTo(0);
     }
 
     @Test
     public void splitAndSum_숫자하나() throws Exception {
-        String[] textSplitArr = calculator.splitText("1");
-        int[] numbers = calculator.StringArrToIntArr(textSplitArr);
-        int result = calculator.calculate(numbers);
-        assertThat(result).isEqualTo(1);
+        assertThat(calculator.calculate
+                (calculator.stringArrToIntArr
+                        (calculator.splitText("1"))
+                )
+        ).isEqualTo(1);
     }
 
     @Test
     public void splitAndSum_쉼표구분자() throws Exception {
-        String[] textSplitArr = calculator.splitText("1,2");
-        int[] numbers = calculator.StringArrToIntArr(textSplitArr);
-        int result = calculator.calculate(numbers);
-        assertThat(result).isEqualTo(3);
+        assertThat(calculator.calculate
+                (calculator.stringArrToIntArr
+                        (calculator.splitText("1,2"))
+                )
+        ).isEqualTo(3);
     }
 
     @Test
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        String[] textSplitArr = calculator.splitText("1,2:3");
-        int[] numbers = calculator.StringArrToIntArr(textSplitArr);
-        int result = calculator.calculate(numbers);
-        assertThat(result).isEqualTo(6);
+        assertThat(calculator.calculate
+                (calculator.stringArrToIntArr
+                        (calculator.splitText("1,2:3"))
+                )
+        ).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_custom_구분자() throws Exception {
-        String[] textSplitArr = calculator.splitText("//;\n1;2;3");
-        int[] numbers = calculator.StringArrToIntArr(textSplitArr);
-        int result = calculator.calculate(numbers);
-        assertThat(result).isEqualTo(6);
+        assertThat(calculator.calculate
+                (calculator.stringArrToIntArr
+                        (calculator.splitText("//;\n1;2;3"))
+                )
+        ).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_negative() throws Exception {
-        String[] textSplitArr = calculator.splitText("-1,2,3");
-        int[] numbers = calculator.StringArrToIntArr(textSplitArr);
-        assertThatThrownBy(() -> calculator.validatePositive(numbers))
+        assertThatThrownBy(() -> calculator.stringArrToIntArr(
+                calculator.splitText("-1,2,3")
+                )
+        )
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("음수가 포함되어 있습니다");
     }
