e0e0ba722ba92873ccf2a48b156f38aea04fcc1d
zooucat <zooucat@gmail.com>
2024-09-26 23:38:49 +0900

    feat: Add RandomUtils class for generating random numbers

--- src/main/java/calculator/utils/StringUtils.java
+++ src/main/java/calculator/utils/StringUtils.java
@@ -2,7 +2,9 @@ package calculator.utils;
 
 public class StringUtils {
 
-    private StringUtils() {}
+    private StringUtils() {
+        throw new IllegalStateException("utility class");
+    }
 
     public static boolean isBlank(final String text) {
         return text == null || text.trim().isEmpty();
