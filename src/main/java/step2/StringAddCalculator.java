85a6af213acdc7b856699909c09001603c93ca57
Ujoonnee <dnwns0933@naver.com>
2024-09-27 02:24:55 +0900

    refactor: 빈 문자열 체크 메서드 추출, 숫자 파싱 메서드 추출

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -4,44 +4,51 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringAddCalculator {
 
     public static int splitAndSum(String input) {
-        if (input == null || input.isEmpty()) {
+        boolean isEmptyInput = "0".equals(convertToZeroIfEmpty(input));
+        if (isEmptyInput) {
             return 0;
         }
 
         List<String> listToSum = split(input);
 
         return sum(listToSum);
-
     }
 
     private static List<String> split(String input) {
-        String delimiter = ",|:";
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
-        if (matcher.matches()) {
-            delimiter = matcher.group(1);
-            input = matcher.group(2);
-        }
+        String defaultDelimiter =  ",|:";
 
-        return Arrays.asList(input.split(delimiter));
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+        boolean hasCustomDelimiter = matcher.matches();
+        String delimiter = hasCustomDelimiter ? matcher.group(1) : defaultDelimiter;
+        String target = hasCustomDelimiter ? matcher.group(2) : input;
+
+        return Arrays.stream(target.split(delimiter))
+                .map(StringAddCalculator::convertToZeroIfEmpty)
+                .collect(Collectors.toList());
     }
 
     private static int sum(List<String> listToSum) {
         return listToSum.stream()
-                .mapToInt(stringNumber -> {
-                    stringNumber = (stringNumber == null || stringNumber.isEmpty()) ? "0" : stringNumber;
-
-                    int parsedNumber = Integer.parseInt(stringNumber);
-                    if (parsedNumber < 0) {
-                        throw new RuntimeException("음수는 사용할 수 없습니다.");
-
-                    } else {
-                        return parsedNumber;
-                    }
-                })
+                .mapToInt(StringAddCalculator::parsePositiveInt)
                 .sum();
+    }
+
+    private static String convertToZeroIfEmpty(String number) {
+        return (number == null || number.isEmpty()) ? "0" : number;
+    }
+
+    private static int parsePositiveInt(String stringNumber) {
+        int parsedNumber = Integer.parseInt(stringNumber);
+        if (parsedNumber < 0) {
+            throw new RuntimeException("음수는 사용할 수 없습니다.");
+
+        } else {
+            return parsedNumber;
+        }
     }
 }
