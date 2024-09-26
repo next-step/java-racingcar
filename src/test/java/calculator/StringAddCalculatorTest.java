5ee472b40293fe300753be292bc5e3848b06a859
hoya <sv05159@naver.com>
2024-09-27 00:48:02 +0900

    test: 숫자 이외의 값

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -44,4 +44,10 @@ public class StringAddCalculatorTest {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
+
+    @Test
+    public void splitAndSum_숫자_이외의_값() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3,a,!,호"))
+                .isInstanceOf(RuntimeException.class);
+    }
 }
