b65c25c6fa2ba0e33ccceb96cc9f4ed69e3ddc97
jonghyeon <jonghyl@smilegate.net>
2024-09-27 20:39:25 +0900

    feat: 비어있거나 null인 문자의 경우 0을 반환하는 기능 추가

--- src/main/java/com/warmup/step2/StringAddCalculator.java
+++ src/main/java/com/warmup/step2/StringAddCalculator.java
@@ -3,6 +3,17 @@ package com.warmup.step2;
 public class StringAddCalculator {
 
     static public int splitAndSum(String input) {
+        if(isNullOrEmpty(input)){
+            return 0;
+        }
+
         return Integer.parseInt(input.trim());
+    }
+
+    /**
+     * string 문자열에 대해 Null, Empty 여부를 반환
+     */
+    static private boolean isNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
     }
 }
--- src/test/java/com/warmup/step2/StringAddCalculatorTest.java
+++ src/test/java/com/warmup/step2/StringAddCalculatorTest.java
@@ -7,9 +7,26 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
     @Test
+    public void isEqualTo_빈문자열_0_반환() {
+        String numberStr = "";
+        int expectedResult = 0;
+
+        assertThat(StringAddCalculator.splitAndSum(numberStr)).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void isEqualTo_NULL_0_반환() {
+        String numberStr = null;
+        int expectedResult = 0;
+
+        assertThat(StringAddCalculator.splitAndSum(numberStr)).isEqualTo(expectedResult);
+    }
+
+    @Test
     public void isEqualTo_하나의_문자열을_숫자형으로_반환() {
         String numberStr = "1";
+        int expectedResult = 1;
 
-        assertThat(StringAddCalculator.splitAndSum(numberStr)).isEqualTo(1);
+        assertThat(StringAddCalculator.splitAndSum(numberStr)).isEqualTo(expectedResult);
     }
 }
