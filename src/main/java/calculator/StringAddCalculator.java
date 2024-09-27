7df25b4d1fafca9f4d523853ca97c7c074eacd0c
ujoonnee <dnwns0933@naver.com>
2024-09-27 16:21:12 +0900

    feature: 쉼표나 콜론을 구분자로 분리한 숫자의 합을 반환하는 기능 추가

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,6 +3,14 @@ package calculator;
 public final class StringAddCalculator {
 
     public static int splitAndSum(final String input) {
-        return 0;
+        String delimiter = ",|:";
+        String[] split = input.split(delimiter);
+
+        int sum = 0;
+        for (String number : split) {
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
     }
 }
