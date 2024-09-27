bab5620a1ba7f2741022381496b64c6082527005
StoneHee99 <kiit0901@gmail.com>
2024-09-27 13:41:50 +0900

    refactor: 메서드 분리

--- src/main/java/calculator/StringAdditionCalculator.java
+++ src/main/java/calculator/StringAdditionCalculator.java
@@ -3,10 +3,23 @@ package calculator;
 public class StringAdditionCalculator {
 
     public static int calculate(String input) {
-        String[] numbers = input.split(",");
+        int[] numbers = extractNumbers(input);
+        return sum(numbers);
+    }
+
+    private static int[] extractNumbers(String input) {
+        String[] inputArray = input.split(",");
+        int[] numbers = new int[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            numbers[i] = Integer.parseInt(inputArray[i]);
+        }
+        return numbers;
+    }
+
+    private static int sum(int[] numbers) {
         int result = 0;
-        for (String number : numbers) {
-            result += Integer.parseInt(number);
+        for (int number : numbers) {
+            result += number;
         }
         return result;
     }
