b2c295bb6b377a35e41332254e10988488503c4a
StoneHee99 <kiit0901@gmail.com>
2024-09-27 13:52:54 +0900

    test: 콜론과 쉼표 모두 구분자로 두어도 잘 동작하는지

--- src/test/java/calculator/StringAdditionCalculatorTest.java
+++ src/test/java/calculator/StringAdditionCalculatorTest.java
@@ -19,4 +19,10 @@ class StringAdditionCalculatorTest {
         assertThat(StringAdditionCalculator.calculate("2:5")).isEqualTo(7);
     }
 
+    @DisplayName("콜론과 쉼표를 모두 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 숫자의 합을 잘 반환하는지")
+    @Test
+    void calculateTest_withCommaAndColon() {
+        assertThat(StringAdditionCalculator.calculate("1,2:3")).isEqualTo(6);
+    }
+
 }
