13f68ac87657490d6601396de92adcbda579b4c0
hanseounggyun <hanseu9839@gmail.com>
2024-09-27 12:51:35 +0900

    리팩토링: 2단계 코드리뷰 반영

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,32 +4,50 @@ import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class StringCalculator {
-    public static int splitAndSum(String input) {
-        String delimiter = "[,:]";
+    private static final int DELIMITER = 1;
+    private static final int EXPRESSION = 2;
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_DELIMITER);
 
-        input = check(input);
 
-        if (input.startsWith("//")) {
-            Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-            if (m.find()) {
-                delimiter = m.group(1);
-                input = m.group(2);
-            }
-        }
-
-        return sum(input.split(delimiter));
+    public static int splitAndSum(String sentence) {
+        return sum(toIntArray(split(check(sentence))));
     }
 
-    public static int sum(String[] sentences) {
-        return Arrays.stream(sentences)
-                .mapToInt(Integer::parseInt)
-                .peek(n -> {
-                    if (n < 0) {
-                        throw new RuntimeException("정수만 입력해주세요.");
-                    }
-                })
-                .sum();
+    public static String[] split(String sentence) {
+        Matcher m = CUSTOM_PATTERN.matcher(sentence);
+        if (m.find()) {
+            String delimiter = m.group(DELIMITER);
+            String expression = m.group(EXPRESSION);
+            return expression.split(delimiter);
+        }
+
+        return sentence.split(DEFAULT_DELIMITER);
+    }
+
+    public static int[] toIntArray(String[] sentences) {
+        int[] numbers = new int[sentences.length];
+        for (int i = 0; i < sentences.length; i++) {
+            numbers[i] = toInt(sentences[i]);
+        }
+
+        return numbers;
+    }
+
+    public static int toInt(String sentence) {
+        int number = Integer.parseInt(sentence);
+        if (number < 0) {
+            throw new RuntimeException("정수만 입력해주세요.");
+        }
+
+        return number;
+    }
+
+    public static int sum(int[] numbers) {
+        return Arrays.stream(numbers).sum();
     }
 
     public static String check(String sentence) {
