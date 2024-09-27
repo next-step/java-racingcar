9d2201dac8bf4d157fe76e43d4e6e51e7067fbcb
최수봉 <soobong.choi@meshkorea.net>
2024-09-27 23:31:29 +0900

    feat: 숫자 여러개를 구분자와 함께 전달하면, 구분된 숫자를 모두 더한 값을 반환한다.

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -3,15 +3,38 @@ package calculator;
 public class Calculator {
 
     public static int calculate(String targetString) {
-        int targetNumber = Integer.parseInt(targetString);
-        checkMinus(targetNumber);
-        return targetNumber;
+        if (isDelimiter(targetString)) {
+            String[] splitStringArray = targetString.split("[;,]");
+            return loopAndAdd(splitStringArray);
+        }
+        return Integer.parseInt(targetString);
     }
 
-    private static void checkMinus(int targetNumber) {
-        if (targetNumber < 0) {
-            throw new RuntimeException();
+    private static int loopAndAdd(String[] splitStringArray) {
+        int result = 0;
+        for (String addNumber : splitStringArray) {
+            validate(addNumber);
+            result += Integer.parseInt(addNumber);
+        }
+        return result;
+    }
+
+    private static boolean isDelimiter(String targetString) {
+        return targetString.contains(",") || targetString.contains(";");
+    }
+
+    private static void validate(String targetString) {
+        try {
+            int number = Integer.parseInt(targetString);
+            validateMinus(number);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("숫자가 아닌 값이거나 음수입니다.");
         }
     }
 
+    private static void validateMinus(int targetNumber) {
+        if (targetNumber < 0) {
+            throw new RuntimeException("연산 대상 숫자가 음수입니다.");
+        }
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -16,10 +16,25 @@ class CalculatorTest {
     }
 
     @Test
+    void 숫자_두개를_콤마_세미콜론과_함께_전달하면_두_숫자를_더한다() {
+        //given
+        String twoNumberWithDelimiter = "1;2";
+        //then
+        assertThat(Calculator.calculate(twoNumberWithDelimiter)).isEqualTo(3);
+    }
+
+    @Test
+    void 숫자_여러개를_콤마_세미콜론과_함께_전달하면_여러_숫자를_더한다() {
+        //given
+        String twoNumberWithDelimiter = "1;2;50,2";
+        //then
+        assertThat(Calculator.calculate(twoNumberWithDelimiter)).isEqualTo(55);
+    }
+
+    @Test
     void 숫자이외의_값을_전달하면_RuntimeException_반환() {
         //given
         String notNumber = "문자";
-
         //then
         assertThatThrownBy(() -> {
             Calculator.calculate(notNumber);
@@ -29,12 +44,10 @@ class CalculatorTest {
     @Test
     void 음수를_전달하면_RuntimeException_반환() {
         //given
-        String negativeNumber = "-1";
-
+        String negativeNumber = "1;-1";
         //then
         assertThatThrownBy(() -> {
             Calculator.calculate(negativeNumber);
         }).isInstanceOf(RuntimeException.class);
     }
-
 }
