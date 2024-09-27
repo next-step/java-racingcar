d98c3384521b2936f3829d9aa7b47fc0764df804
jonghyeon <jonghyl@smilegate.net>
2024-09-27 20:25:11 +0900

    chore: 테스트에 DisplayName추가

--- src/test/java/com/warmup/step1/StringTest.java
+++ src/test/java/com/warmup/step1/StringTest.java
@@ -1,5 +1,6 @@
 package com.warmup.step1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
+    @DisplayName("문자열 배열이 ','를 기준으로 잘 쪼개어 지는지 여부")
     @Test
     public void isContain_ShouldContainsExpectedSplitStringArray() {
         String[] expectedValue = {"1", "2"};
@@ -14,6 +16,7 @@ public class StringTest {
         assertThat(StringHelper.splitter("1,2")).containsExactly(expectedValue);
     }
 
+    @DisplayName("괄호를 가지고 있는 문자열 배열이 ','를 기준으로 잘 쪼개어 지는지 여부")
     @Test
     public void isContain_ShouldContainsExpectedSplitHasParenthesesStringArray() {
         String[] expectedValue = {"1", "2"};
@@ -21,6 +24,7 @@ public class StringTest {
         assertThat(StringHelper.splitter("(1,2)")).containsExactly(expectedValue);
     }
 
+    @DisplayName("String 문자열의 길이에 맞지 않은 인덱스 접근을 하는지 여부")
     @Test
     public void isEquals_ShouldEqualExpectedValue() {
         char expectedValue = 'a';
