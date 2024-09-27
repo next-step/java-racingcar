a53f26ea4ebeddb1fee8fa1f7a5bbd52c42844ca
sinsu <sinsu19012@gmail.com>
2024-09-27 18:03:35 +0900

    feat: 자동차가 이동 조건의 따라 전진 또는 멈추는 기능 개발

---
+++ src/main/java/RandomMovementCondition.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class RandomMovementCondition implements MovementCondition {
+    private final int MAX_MOVE_CONDITION_VALUE = 10;
+    private final int MIN_MOVE_CONDITION_VALUE = 4;
+    private final Random random = new Random();
+
+    @Override
+    public boolean canMove() {
+        return random.nextInt(MAX_MOVE_CONDITION_VALUE) >= MIN_MOVE_CONDITION_VALUE;
+    }
+}
