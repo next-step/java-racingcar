83066aff522434168a051a97f568629019f1ee56
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-09-27 22:40:08 +0900

    feat: 자동차경주 실행 메인클래스

---
+++ src/main/java/CarGameMain.java
@@ -0,0 +1,11 @@
+import game.CarGame;
+
+public class CarGameMain {
+
+    public static void main(String[] args) {
+        CarGame carGame = new CarGame();
+        carGame.readyRace();
+        carGame.startRace();
+    }
+
+}
