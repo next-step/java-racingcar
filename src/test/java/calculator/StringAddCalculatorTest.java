2ffdd7eb347eba1828977aa6fbb0b18928e5a3ac
Ujoonnee <dnwns0933@naver.com>
2024-09-28 03:57:55 +0900

    test: 커스텀 구분자를 사용하는 테스트 케이스 추가

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -1,8 +1,14 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,6 +31,14 @@ public class StringAddCalculatorTest {
     @ParameterizedTest
     @CsvSource(value = {"1-1", "2,-2", "3:-3", "4,:-4", ",5-5", ":,6-6", ",7,-7"}, delimiter = '-')
     void 숫자_한개는_그대로_반환한다(String input, int expected) {
+        int result = StringAddCalculator.splitAndSum(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @CsvSource(value = {"//@\\n1@2-3", "///\\n3/4-7", "// \n5 6-11", "//#$\n7#$8-15"}, delimiter = '-')
+    @DisplayName("//와 \\n 사이의 문자를 커스텀구분자로 사용해 분리한 합을 반환한다.")
+    void 커스텀_구분자를_사용할수_있다(String input, int expected) {
         int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(expected);
     }
