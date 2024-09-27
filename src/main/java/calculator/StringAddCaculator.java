3802d3c30e2561a87f04128fb8f518a624f78526
skdev0619 <skdev0619@naver.com>
2024-09-27 16:50:00 +0900

    ,을 구분자로 문자열을 쪼개서 합을 구한다

--- src/main/java/calculator/StringAddCaculator.java
+++ src/main/java/calculator/StringAddCaculator.java
@@ -7,7 +7,22 @@ public class StringAddCaculator {
         if(text == null || text.isEmpty()){
             return 0;
         }
-        return sum;
+        return getSum(toInts(text.split(",")));
+    }
 
+    private static int[] toInts(String[] textArray){
+        int[] numbers = new int[textArray.length];
+        for(int i = 0; i < textArray.length; i++){
+            numbers[i] = Integer.parseInt(textArray[i]);
+        }
+        return numbers;
+    }
+
+    private static int getSum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
--- src/test/java/calculator/StringAddCaculatorTest.java
+++ src/test/java/calculator/StringAddCaculatorTest.java
@@ -15,4 +15,10 @@ class StringAddCaculatorTest {
         assertThat(splitAndSum("0")).isZero();
         assertThat(splitAndSum(null)).isZero();
     }
+
+    @DisplayName(",을 구분자로 문자열을 쪼개서 합을 구한다")
+    @Test
+    void splitAndSumByComma(){
+        assertThat(splitAndSum("1,2,3")).isEqualTo(6);
+    }
 }
