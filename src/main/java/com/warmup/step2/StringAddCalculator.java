0f4d05d3203122ab685f7009be0b84bf73e0b6e8
jonghyeon <jonghyeonLee.90@gmail.com>
2024-09-27 22:47:42 +0900

    refactor: 잘못 선언한 접근 제어자 수정

--- src/main/java/com/warmup/step2/StringAddCalculator.java
+++ src/main/java/com/warmup/step2/StringAddCalculator.java
@@ -2,7 +2,7 @@ package com.warmup.step2;
 
 public class StringAddCalculator {
 
-    static public int splitAndSum(String input) {
+    public static int splitAndSum(String input) {
         if (isNullOrEmpty(input)) {
             return 0;
         }
@@ -32,7 +32,7 @@ public class StringAddCalculator {
     /**
      * string 문자열에 대해 Null, Empty 여부를 반환
      */
-    static private boolean isNullOrEmpty(String input) {
+    private static boolean isNullOrEmpty(String input) {
         return input == null || input.isEmpty();
     }
 }
