4adb63d93f67df5e70bddbcbe1435a8344e669c4
둥둥둥오리 <62438578+hanseu9839@users.noreply.github.com>
2024-09-27 11:18:27 +0900

    Step2: 문자열 덧셈 계산기  (#5620)
    * 리팩토링 : 1단계 코드 리뷰 리팩토링
    
    * step2 : 문자열 계산기 기능 구현
    
    * step2 : 문자열 계산기 테스트 코드 구현
    
    * 리팩토리 : production code 패키지 위치 main으로 변경

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -13,7 +13,7 @@ public class StringTest {
     @Test
     @DisplayName("문자열 split 테스트")
     void split() {
-        String []splitResult = "1,2".split(",");
+        String[] splitResult = "1,2".split(",");
 
         assertThat(splitResult).containsExactly("1", "2");
     }
@@ -21,7 +21,7 @@ public class StringTest {
 
     @Test
     @DisplayName("문자열 substring 테스트")
-    void substring(){
+    void substring() {
         String result = "(1,2)".substring(1, 4);
 
         assertThat(result).isEqualTo("1,2");
@@ -33,7 +33,7 @@ public class StringTest {
     void chatAt_success() {
 
         assertAll(
-                ()-> assertEquals("abc".charAt(0), 'a'),
+                () -> assertEquals("abc".charAt(0), 'a'),
                 () -> assertEquals("abc".charAt(1), 'b'),
                 () -> assertEquals("abc".charAt(2), 'c')
         );
@@ -41,10 +41,10 @@ public class StringTest {
 
     @Test
     @DisplayName("문자열 chatAt 실패 테스트")
-    void charAt_fail(){
-
-        assertThatThrownBy(() -> "abc".charAt(3)).isInstanceOf(IndexOutOfBoundsException.class).hasMessageMatching("String index out of range: \\d+");
-
+    void charAt_fail() {
+        assertThatThrownBy(() -> "abc".charAt(3))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageMatching("String index out of range: \\d+");
     }
 
 
