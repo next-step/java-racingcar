90963c28ce44924b87aba23ae29a63852761bf40
hwan2-99 <thh1233123@gmail.com>
2024-09-28 08:58:14 +0900

    feat: 입력한 문자열 분리 후 덧셈 메서드 작성

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -3,16 +3,26 @@ package step2;
 public class StringAddCalculator {
     private static final String DELIMITER = ",|:";
     private static final int MIN_NUMBER = 0;
+    private static final int MIN_SIZE = 1;
 
     public static int splitAndSum(final String inputValue) {
         if(isBlank(inputValue)) {
             return 0;
         }
 
-        return 0;
+        return sum(inputValue);
     }
 
     private static boolean isBlank(final String inputValue) {
         return inputValue == null || inputValue.isEmpty();
+    }
+
+    private static int sum(final String inputValue) {
+        String[] values = inputValue.split(DELIMITER);
+        int sum = 0;
+        for(String value : values) {
+            sum += Integer.parseInt(value);
+        }
+        return sum;
     }
 }
