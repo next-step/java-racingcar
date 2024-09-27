0969be3819c0694e5d85575d25c9d0485bf0a5c1
ujoonnee <dnwns0933@naver.com>
2024-09-27 17:02:04 +0900

    refactor: split, sum 로직 분리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -3,13 +3,23 @@ package calculator;
 public final class StringAddCalculator {
 
     public static int splitAndSum(final String input) {
+        String[] split = split(input);
+
+        return sum(split);
+    }
+
+    private static String[] split(final String input) {
         if (input == null || input.isEmpty()) {
-            return 0;
+            return new String[0];
         }
 
         String delimiter = ",|:";
         String[] split = input.split(delimiter);
 
+        return split;
+    }
+
+    private static int sum(final String[] split) {
         int sum = 0;
         for (String number : split) {
             sum += Integer.parseInt(number);
