6148d2aed15070dc93d9371404da65020d8b075c
최수봉 <soobong.choi@meshkorea.net>
2024-09-27 09:47:21 +0900

    feat: 음수 값 전달시 RuntimeException 반환

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -7,7 +7,14 @@ public class Calculator {
 
   public int calculate(String targetString) {
     int targetNumber = Integer.parseInt(targetString);
+    checkMinus(targetNumber);
     return targetNumber;
+  }
+
+  private void checkMinus(int targetNumber) {
+    if (targetNumber < 0) {
+      throw new RuntimeException();
+    }
   }
 
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,12 +7,25 @@ public class CalculatorTest {
 
     @Test
     void 숫자이외의_값을_전달하면_RuntimeException_반환() {
+        //given
         String notNumber = "문자";
         Calculator calculator = new Calculator();
 
         //then
         Assertions.assertThatThrownBy(() -> {
             calculator.calculate(notNumber);
+        }).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 음수를_전달하면_RuntimeException_반환() {
+        //given
+        String negativeNumber = "-1";
+        Calculator calculator = new Calculator();
+
+        //then
+        Assertions.assertThatThrownBy(() -> {
+            calculator.calculate(negativeNumber);
         }).isInstanceOf(RuntimeException.class);
     }
 }
