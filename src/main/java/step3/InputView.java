de1723d287dc5d509e2b555830a01e21d92533b6
sinyebin <dpqls0625@naver.com>
2024-09-28 00:36:43 +0900

    feat 24.09.28 syb 자동차 input 에러처리

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -1,10 +1,32 @@
 package step3;
 
+import static step3.ResultView.printCarCount;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
-    public static int inputCountCar(){
+    public static int inputCountCar() {
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        int carCount = 0;
+        boolean isValid = false;
+        while (!isValid) {
+            printCarCount();
+            try {
+                carCount = scanner.nextInt();
+                isValid = isPositiveNum(carCount);
+            } catch (InputMismatchException ex) {
+                scanner.next();
+            }
+        }
+        return carCount;
     }
+
+    public static boolean isPositiveNum(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        return true;
+    }
+
 }
