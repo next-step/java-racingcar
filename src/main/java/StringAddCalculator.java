4f9474d187841712e94f8a9ee630c6c06b466bf1
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-09-27 22:03:35 +0900

    feat : 요구사항 3 구현 - 숫자 두개를 컴마 구분자로 입력할 경우 두 숫자의 합 반환

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,12 +1,12 @@
 public class StringAddCalculator {
-    public static void validNegative(int num) throws RuntimeException{
+    public static void validNegative(int num) throws RuntimeException {
         if (num < 0) {
             throw new RuntimeException();
         }
     }
 
     public static int emptyOrNull(String input) {
-        if(input == null||input.isEmpty()) {
+        if (input == null || input.isEmpty()) {
             return 0;
         }
         return 1;
@@ -14,5 +14,14 @@ public class StringAddCalculator {
 
     public static int processOneNumberString(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static int splitAndSumWithComma(String input) {
+        String[] splited = input.split(",");
+        int summation = 0;
+        for (String s : splited) {
+            summation += Integer.parseInt(s);
+        }
+        return summation;
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -25,10 +25,18 @@ class StringAddCalculatorTest {
 
     @Test
     @DisplayName("숫자 하나를 입력할 경우 해당 숫자를 반환한다")
-    void inputOneNumberStringReturnToInteger(){
-        int intNum = StringAddCalculator.processOneNumberString("1");
+    void inputOneNumberStringReturnToInteger() {
+        int result = StringAddCalculator.processOneNumberString("1");
 
-        assertThat(intNum).isEqualTo(1);
+        assertThat(result).isEqualTo(1);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings ={"1,2"})
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환")
+    void splitAndSumWithComma(String input) {
+        int result = StringAddCalculator.splitAndSumWithComma(input);
+
+        assertThat(result).isEqualTo(3);
+    }
 }
