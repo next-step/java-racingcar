64936dceedfef5a8a06e4de29f38fdd44b35e754
hwan2-99 <thh1233123@gmail.com>
2024-09-28 09:09:32 +0900

    refactor: custom 구분자 분리 메서드 매직넘버 상수로 선언

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -6,10 +6,12 @@ import java.util.regex.Pattern;
 public class StringAddCalculator {
     private static final String DELIMITER = ",|:";
     private static final Pattern CUSTOM_DELEMITER_PATTERN = Pattern.compile("//(.)\\n(.*)");
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int TARGET_STRING_INDEX = 2;
     private static final int MIN_NUMBER = 0;
 
     public static int splitAndSum(final String inputValue) {
-        if(isBlank(inputValue)) {
+        if (isBlank(inputValue)) {
             return 0;
         }
 
@@ -23,7 +25,7 @@ public class StringAddCalculator {
     private static int sum(final String inputValue) {
         String[] values = splitString(inputValue);
         int sum = 0;
-        for(String value : values) {
+        for (String value : values) {
             sum += Integer.parseInt(value);
         }
         return sum;
@@ -32,8 +34,8 @@ public class StringAddCalculator {
     private static String[] splitString(final String inputValue) {
         Matcher m = CUSTOM_DELEMITER_PATTERN.matcher(inputValue);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+            String customDelimiter = m.group(CUSTOM_DELIMITER_INDEX);
+            return m.group(TARGET_STRING_INDEX).split(customDelimiter);
         }
         return inputValue.split(DELIMITER);
     }
