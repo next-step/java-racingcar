d729b7d102c8f1208c3762c49f699f7b8bfc08f0
hwan2-99 <thh1233123@gmail.com>
2024-09-28 09:07:50 +0900

    feat: custom 구분자 분리 후 덧셈 메서드 작성

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,9 +1,12 @@
 package step2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     private static final String DELIMITER = ",|:";
+    private static final Pattern CUSTOM_DELEMITER_PATTERN = Pattern.compile("//(.)\\n(.*)");
     private static final int MIN_NUMBER = 0;
-    private static final int MIN_SIZE = 1;
 
     public static int splitAndSum(final String inputValue) {
         if(isBlank(inputValue)) {
@@ -18,11 +21,20 @@ public class StringAddCalculator {
     }
 
     private static int sum(final String inputValue) {
-        String[] values = inputValue.split(DELIMITER);
+        String[] values = splitString(inputValue);
         int sum = 0;
         for(String value : values) {
             sum += Integer.parseInt(value);
         }
         return sum;
+    }
+
+    private static String[] splitString(final String inputValue) {
+        Matcher m = CUSTOM_DELEMITER_PATTERN.matcher(inputValue);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+        return inputValue.split(DELIMITER);
     }
 }
