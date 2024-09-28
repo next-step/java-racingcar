c1b5e34e85f9a3352607d92b2a190da3fd5b18b3
최수봉 <soobong.choi@meshkorea.net>
2024-09-28 12:53:09 +0900

    refactor: 기본 구분자, 커스텀 구분자일 때 분기처리를 리팩토링
    - 삼항연산자와 메서드 추출로 코드를 좀 더 간결하게 분리
    - 테스트 패키지의 Calculator를 메인 패키지로 이동

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,28 +4,35 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
-    public static final String CUSTOM_DELIMITER_CHECK_REGEX = "//(.)\n(.*)";
-    public static final String DEFAULT_DELIMITER_CHECK_REGEX = "[:,]";
+    public static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+    public static final String DEFAULT_DELIMITER_REGEX = "[:,]";
 
     public static int calculate(String targetString) {
         if (isNullOfEmpty(targetString)) {
             return 0;
         }
 
-        if (hasCustomDelimiter(targetString)) {
-            Matcher m = Pattern.compile(CUSTOM_DELIMITER_CHECK_REGEX).matcher(targetString);
-            if (m.find()) {
-                String customDelimiter = Pattern.quote(m.group(1));
-                String[] tokens = m.group(2).split(customDelimiter);
-                return loopAndAdd(tokens);
-            }
-        }
+        return hasCustomDelimiter(targetString)
+                ? calculateWithCustomDelimiter(targetString)
+                : calculateWithDefaultDelimiter(targetString);
+    }
 
+    private static int calculateWithDefaultDelimiter(String targetString) {
         if (hasDefaultDelimiter(targetString)) {
-            String[] tokens = targetString.split(DEFAULT_DELIMITER_CHECK_REGEX);
-            return loopAndAdd(tokens);
+            String[] tokens = targetString.split(DEFAULT_DELIMITER_REGEX);
+            return sumTokens(tokens);
         }
-        return Integer.parseInt(targetString);
+        return Integer.parseInt(targetString); // 구분자 없이 단일 숫자일 때
+    }
+
+    private static int calculateWithCustomDelimiter(String targetString) {
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(targetString);
+        if (m.find()) {
+            String customDelimiter = Pattern.quote(m.group(1));
+            String[] tokens = m.group(2).split(customDelimiter);
+            return sumTokens(tokens);
+        }
+        return 0;
     }
 
     private static boolean hasCustomDelimiter(String targetString) {
@@ -36,7 +43,7 @@ public class Calculator {
         return targetString == null || targetString.isEmpty();
     }
 
-    private static int loopAndAdd(String[] tokens) {
+    private static int sumTokens(String[] tokens) {
         int result = 0;
         for (String addNumber : tokens) {
             validate(addNumber);
