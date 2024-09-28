996e213e54929a6dd5c3cdc299f936ec8fe01929
hwan2-99 <thh1233123@gmail.com>
2024-09-29 01:56:52 +0900

    test: 입력받은 문자에 숫자가 아닌 문자가 포함 되어있을경우 예외처리 메서드 검증하는 테스트 작성

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -44,4 +44,10 @@ public class StringAddCalculatorTest {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
+
+    @Test
+    public void splitAndSum_숫자가_아닌_문자() throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("하나,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
 }
