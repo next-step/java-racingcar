3a190855d5af6f12b22f911c3756823f1b73699d
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-09-28 22:00:20 +0900

    이름 규칙에 맞춰서 수정
    이름 규칙에 맞춰서 수정

--- src/main/java/race/InputView.java
+++ src/main/java/race/InputView.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public RaceInput input() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        int carCount = scanner.nextInt();
+        int carCount = SCANNER.nextInt();
 
         System.out.println("시도할 회수는 몇 회 인가요?");
-        int tryCount = scanner.nextInt();
+        int tryCount = SCANNER.nextInt();
 
         return new RaceInput(carCount, tryCount);
     }
