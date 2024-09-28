8dfdcc3d322277a92613d5a59f5b729a01a6a854
jara <jara0701@gmail.com>
2024-09-28 13:46:59 +0900

    Step 1 - 학습 테스트 (#5644)
    * stringTest implemented
    
    * setTest implemented
    
    * blank 삭제

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,40 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
+    }
+
+    @Test
+    void splitWithoutComma() {
+        String[] result = "1".split(",");
+        assertThat(result).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("charAt으로 지정된 위치에 있는 char값을 가져오는지 확인")
+    void charAt() {
+        String target = "abc";
+        assertThat(target.charAt(0)).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("charAt으로 stirng 범위 밖 값을 가져오려고 하면 exception을 던져주는지 확인")
+    void charAtOutOfBounds() {
+        String target = "abc";
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+            target.charAt(target.length());
+        }).withMessageMatching("String index out of range: \\d+");
+        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+            target.charAt(-1);
+        }).withMessageMatching("String index out of range: -\\d+");
+    }
+}
