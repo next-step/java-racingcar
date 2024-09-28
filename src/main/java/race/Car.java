978642597835d3611f2a183528a761c1f9b25544
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-09-28 20:56:35 +0900

    자동차 전진 여부를 확인하는 메소드와 전진 메소드 추가
    자동차 전진 여부를 확인하는 메소드와 전진 메소드 추가

--- src/main/java/race/Car.java
+++ src/main/java/race/Car.java
@@ -4,9 +4,20 @@ import java.util.Random;
 
 public class Car {
 
+    private int forwardResult = 1;
     private static final int randomRange = 10;
 
     public int getConditionNumber() {
         return new Random().nextInt(randomRange);
+    }
+
+    public void forward(int conditionNumber) {
+        if (conditionNumber >= 4) {
+            forwardResult++;
+        }
+    }
+
+    public int getForwardResult() {
+        return forwardResult;
     }
 }
--- src/test/java/race/CarTest.java
+++ src/test/java/race/CarTest.java
@@ -14,4 +14,24 @@ class CarTest {
 
         assertThat(conditionNumber).isBetween(0, 9);
     }
+
+    @Test
+    void 자동차를_생성하면_전진하는_방향이_하나_생성된다() {
+        Car car = new Car();
+
+        int forwardResult = car.getForwardResult();
+
+        assertThat(forwardResult).isEqualTo(1);
+    }
+
+    @Test
+    void 자동차_진행조건인_4이상의_수가_주어지면_한칸_전진한다() {
+        Car car = new Car();
+
+        int conditionNumber = 4;
+
+        car.forward(conditionNumber);
+
+        assertThat(car.getForwardResult()).isEqualTo(2);
+    }
 }
