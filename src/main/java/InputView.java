d6ab6830a41a1b620297bb727f9b26cfcf312b04
Hoseop.Kim <kimho314@gmail.com>
2024-09-28 14:18:31 +0900

    [feat] : 3던계 - 시도할 횟수 입력 기능

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -6,6 +6,18 @@ public class InputView {
     public static int getNumberOfCars() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         int num = Integer.parseInt(SC.nextLine());
+        if (num < 0) {
+            throw new IllegalArgumentException("0이상 입력 해주세요");
+        }
+        return num;
+    }
+
+    public static int getNumberOfGames() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int num = Integer.parseInt(SC.nextLine());
+        if (num < 0) {
+            throw new IllegalArgumentException("0이상 입력 해주세요");
+        }
         return num;
     }
 }
--- src/main/java/RacingCarGameManagement.java
+++ src/main/java/RacingCarGameManagement.java
@@ -2,5 +2,7 @@ public class RacingCarGameManagement {
     public static void main(String[] args) {
         int numberOfCars = InputView.getNumberOfCars();
         System.out.println(numberOfCars);
+        int numberOfGames = InputView.getNumberOfGames();
+        System.out.println(numberOfGames);
     }
 }
