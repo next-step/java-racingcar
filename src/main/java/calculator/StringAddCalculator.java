dbeafaf0484811587c9eea526c1ec19acd4fb2b1
Ujoonnee <69345466+Ujoonnee@users.noreply.github.com>
2024-09-29 03:43:10 +0900

    refactor: 커스텀 구분자 여부에 따라 사용할 구분자와 합산할 문자열을 구해오는 메서드 추출

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,6 +14,71 @@ public final class StringAddCalculator {
         return sum(listToSum);
     }
 
+    private static List<String> split(final String input) {
+        if (input == null || input.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        String delimiterRegex = getDelimiterRegex(input);
+        String target = getTarget(input);
+        String[] split = target.split(delimiterRegex);
+
+        return filterEmptyString(split);
+    }
+
+    private static String getDelimiterRegex(final String input) {
+        final String DEFAULT_DELIMITER_REGEX = ",|:";
+        String delimiterRegex = DEFAULT_DELIMITER_REGEX;
+
+        boolean isCustomDelimiter = input.startsWith("//");
+        if (!isCustomDelimiter) {
+            return DEFAULT_DELIMITER_REGEX;
+        }
+
+        final String CUSTOM_DELIMITER_REGEX = "//(.*)\n(.*)";
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(input);
+        if (matcher.matches()) {
+            delimiterRegex = matcher.group(1);
+            checkValidDelimiter(delimiterRegex);
+        }
+
+        return delimiterRegex;
+    }
+
+    private static String getTarget(final String input) {
+        boolean isCustomDelimiter = input.startsWith("//");
+        if (!isCustomDelimiter) {
+            return input;
+        }
+
+        final String CUSTOM_DELIMITER_REGEX = "//(.*)\n(.*)";
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(input);
+        if (matcher.matches()) {
+            return matcher.group(2);
+        }
+
+        return input;
+    }
+
+    private static List<String> filterEmptyString(String[] split) {
+        List<String> list = new ArrayList<>();
+        for (String number : split) {
+            if (!number.isEmpty()) {
+                list.add(number);
+            }
+        }
+        return list;
+    }
+
+    private static void checkValidDelimiter(String delimiterRegex) {
+        List<String> forbiddenCharacters = List.of("$", "^", "*", "(", ")", "{", "}", "[", "]", "+", "?", "|", ".");
+        for (String item : forbiddenCharacters) {
+            if (delimiterRegex.contains(item)) {
+                throw new RuntimeException("사용할 수 없는 구분자입니다.");
+            }
+        }
+    }
+
     private static List<Integer> convertStringsToIntegers(final List<String> split) {
         List<Integer> result = new ArrayList<>();
         for (String number : split) {
@@ -24,56 +88,19 @@ public final class StringAddCalculator {
         return result;
     }
 
-    private static List<String> split(final String input) {
-        List<String> result = new ArrayList<>();
-        if (input == null || input.isEmpty()) {
-            return result;
-        }
-
-        final String DEFAULT_DELIMITER_REGEX = ",|:";
-        final String CUSTOM_DELIMITER_REGEX = "//(.*)\n(.*)";
-        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(input);
-        boolean isCustomDelimiter = matcher.matches();
-
-        String delimiterRegex;
-        if (isCustomDelimiter) {
-            delimiterRegex = matcher.group(1);
-
-            String[] list = {"$", "^", "*", "(", ")", "{", "}", "[", "]", "+", "?", "|", "."};
-            final List<String> FORBIDDEN_DELIMITERS = List.of(list);
-
-            for (String item : FORBIDDEN_DELIMITERS) {
-                if (delimiterRegex.contains(item)) {
-                    throw new RuntimeException("사용할 수 없는 구분자입니다.");
-                }
-            }
-
-        } else {
-            delimiterRegex = DEFAULT_DELIMITER_REGEX;
-        }
-
-        String target = isCustomDelimiter ? matcher.group(2) : input;
-
-        String[] split = target.split(delimiterRegex);
-        for (String number : split) {
-            if (!number.isEmpty()) {
-                result.add(number);
-            }
-        }
-
-        return result;
-    }
-
     private static int sum(final List<Integer> list) {
         int sum = 0;
         for (int number : list) {
-            if (number < 0) {
-                throw new RuntimeException("음수를 계산할 수 없습니다.");
-            }
-
+            checkNaturalNumber(number);
             sum += number;
         }
 
         return sum;
+    }
+
+    private static void checkNaturalNumber(int number) {
+        if (number < 0) {
+            throw new RuntimeException("음수를 계산할 수 없습니다.");
+        }
     }
 }
