b186b554913def67a9964e9ff90f41b0d8e8fc35
sinyebin <dpqls0625@naver.com>
2024-09-28 23:08:48 +0900

    feat 24.09.28 syb 전진 횟수 더하기

--- src/main/java/step3/RacingCar.java
+++ src/main/java/step3/RacingCar.java
@@ -2,6 +2,8 @@ package step3;
 
 import static step3.InputView.inputCountCar;
 import static step3.InputView.inputTryCount;
+import static step3.ResultView.printGoDistance;
+import static step3.ResultView.printResultMessage;
 
 import java.util.Random;
 
@@ -12,6 +14,7 @@ public class RacingCar {
     public static void startRacing(){
         int car[] = makeRacingCar(inputCountCar());
         int tryCount = makeTryCount(inputTryCount());
+        getGameResult(car, tryCount);
     }
 
     public static int[] makeRacingCar(int carCount){
@@ -22,6 +25,25 @@ public class RacingCar {
         return tryCount;
     }
 
+    private static void getGameResult(int[] car, int tryCount) {
+        printResultMessage();
+        for(int i=0;i<tryCount;i++){
+            car=carMove(car);
+
+        }
+    }
+
+    private static int[] carMove(int[] car){
+        for(int i=0;i<car.length;i++){
+            if(isMove()){
+                car[i]+=1;
+            }
+            printMove(car);
+            System.out.println();
+        }
+        return car;
+    }
+
     public static boolean isMove(){
         Random random = new Random();
         int randomNumber = random.nextInt(10);
@@ -29,5 +51,12 @@ public class RacingCar {
             return true;
         }
         return false;
+    }
+
+    public static void printMove(int[] car){
+        for (int i=0;i<car.length;i++){
+            printGoDistance(car[i]);
+            System.out.println();
+        }
     }
 }
--- src/main/java/step3/ResultView.java
+++ src/main/java/step3/ResultView.java
@@ -2,11 +2,25 @@ package step3;
 
 public class ResultView {
     private static String PRINT_CAR_COUNT = "자동차 대수는 몇 대 인가요?";
-    private static String PRINT_TRY_COUNT= "시도할 회수는 몇 회 인가요?";
-    public static void printCarCount(){
+    private static String PRINT_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
+    private static String PRINT_RESULT_MESSAGE = "실행 결과";
+    private static String PRINT_GO_DISTANCE="-";
+
+    public static void printCarCount() {
         System.out.println(PRINT_CAR_COUNT);
     }
-    public static void printTryCount(){
+
+    public static void printTryCount() {
         System.out.println(PRINT_TRY_COUNT);
+    }
+
+    public static void printResultMessage(){
+        System.out.println(PRINT_RESULT_MESSAGE);
+    }
+
+    public static void printGoDistance(int distance){
+        for(int i=0;i<distance;i++){
+            System.out.print(PRINT_GO_DISTANCE);
+        }
     }
 }
