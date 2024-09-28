64b057bcb51f3c89222e5adee9eb7dc7b01eec2d
hanseounggyun <hanseu9839@gmail.com>
2024-09-28 23:01:51 +0900

    refactor: inputViewTest 불필요한 import 제거

--- src/test/java/racing/ui/InputViewTest.java
+++ src/test/java/racing/ui/InputViewTest.java
@@ -1,6 +1,5 @@
 package racing.ui;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racing.car.ui.InputView;
@@ -13,16 +12,13 @@ public class InputViewTest {
 
     @Test
     @DisplayName("input 메서드 실행 시, 입력한 숫자를 반환해야한다.")
-    void input_메서드_테스트(){
-       Scanner scanner = new Scanner("3");
-       InputView view = new InputView(scanner);
+    void input_메서드_테스트() {
+        Scanner scanner = new Scanner("3");
+        InputView view = new InputView(scanner);
 
-       int number = view.input();
+        int number = view.input();
 
-       assertThat(number).isEqualTo(3);
+        assertThat(number).isEqualTo(3);
     }
-
-
-
 
 }
