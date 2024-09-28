67ee6fbce9746fd2fe99cbe9732efb9b7e95fb90
JongMinCh0i <wonderland314592@gmail.com>
2024-09-28 16:30:30 +0900

    [test] 테스트 케이스 추가
    - , 혹은 : 구분자와 하나의 숫자를 포함하는 문자열을 전달 받았을 경우 하나의 숫자를 정수형으로 반환한다
    - , 혹은 : 구분자와 하나 이상의 숫자를 포함하지 않는 문자열을 전달 받았으면 0을 반환한다

--- src/test/java/study/step_2/StringAddCalculatorTest.java
+++ src/test/java/study/step_2/StringAddCalculatorTest.java
@@ -8,11 +8,28 @@ import study.step_1.StringAddCalculator;
 
 public class StringAddCalculatorTest {
 
-    @DisplayName(", 혹은 : 구분자와 숫자를 포함하는 문자열을 전달 받았을 경우 구분자를 기준으로 분리한 각 숫자의 합을 정수형으로 반환한다.")
+    @DisplayName(", 혹은 : 구분자와 하나 이상의 숫자를 포함하는 문자열을 전달 받았을 경우 구분자를 기준으로 분리한 각 숫자의 합을 정수형으로 반환한다.")
     @ParameterizedTest
-    @CsvSource(value = {"1,2 3" , "1,2,3 6", "1:2:3 6", "1:2,4 7"}, delimiter = ' ')
+    @CsvSource(value = {"1,2 3" , "1,2,3 6", "1:2:3 6", "1:2,4 7", "1, 1", "1: 1", ":1 1", ",1 1"}, delimiter = ' ')
     public void splitAndSum(String input, String expectResult) {
         StringAddCalculator calculator = new StringAddCalculator();
         Assertions.assertThat(calculator.splitAndSum(input)).isEqualTo(Integer.parseInt(expectResult));
     }
+
+    @DisplayName(", 혹은 : 구분자와 하나의 숫자를 포함하는 문자열을 전달 받았을 경우에 구분자를 기준으로 분리한 숫자를 정수형으로 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1, 1", "1: 1", ":1 1", ",1 1"}, delimiter = ' ')
+    public void splitAndSum_단일문자를_입력받을떄(String input, String expectResult) {
+        StringAddCalculator calculator = new StringAddCalculator();
+        Assertions.assertThat(calculator.splitAndSum(input)).isEqualTo(Integer.parseInt(expectResult));
+    }
+
+    @DisplayName(", 혹은 : 구분자와 하나 이상의 숫자를 포함하지 않는 문자열을 전달 받았으면 0을 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {", 0", ": 0"}, delimiter = ' ')
+    public void splitAndSum_숫자를_입력받지_못했을때(String input, String expectResult) {
+        StringAddCalculator calculator = new StringAddCalculator();
+        Assertions.assertThat(calculator.splitAndSum(input)).isEqualTo(Integer.parseInt(expectResult));
+    }
+
 }
