b959a963a23afdec7299add4cfb314faebcb7cc8
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-09-28 22:01:00 +0900

    Main 클래스에 레이스 실행 추가
    Main 클래스에 레이스 실행 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import race.Car;
+import race.CarRace;
+import race.CarRandomNumber;
 import race.InputView;
 import race.RaceInput;
+import race.RandomNumber;
 
 public class Main {
+
+    private static final int RANDOM_RANGE = 10;
+
     public static void main(String[] args) {
 
         InputView inputView = new InputView();
         RaceInput raceInput = inputView.input();
+
+        List<Car> cars = initCars(raceInput.getCarCount());
+
+        CarRace carRace = new CarRace(raceInput.getCarCount(), raceInput.getTryCount(), cars);
+
+        carRace.start();
+    }
+
+    private static List<Car> initCars(int carCount) {
+        List<Car> cars = new ArrayList<>();
+        RandomNumber randomNumberGenerator = new CarRandomNumber(RANDOM_RANGE);
+
+        for(int i = 0; i < carCount; i++) {
+            cars.add(new Car(randomNumberGenerator));
+        }
+
+        return cars;
     }
 }
