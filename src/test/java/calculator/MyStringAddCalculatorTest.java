35eb17614b822b7ab8379bc4f45f078603dbe323
Slowth-KIM <rt.slowth@gmail.com>
2024-09-28 14:51:02 +0900

    rename from StringCalculatorTest to MyStringAddCalculator

--- src/test/java/calculator/MyStringAddCalculatorTest.java
+++ src/test/java/calculator/MyStringAddCalculatorTest.java
@@ -1,25 +1,28 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class StringCalculatorTest {
+public class MyStringAddCalculatorTest {
 
     @ParameterizedTest
     @NullSource
     @ValueSource(strings = {"", "   "})
     void 빈_문자열_입력_시_0_반환(final String input){
-        int result = StringCalculator.splitAndSum(input);
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(0);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "123", "45678912"})
     void 숫자만_입력시_숫자_반환(final String input ){
-        int result = StringCalculator.splitAndSum(input);
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(Integer.parseInt(input));
     }
 
@@ -35,7 +38,7 @@ public class StringCalculatorTest {
             "1,2,3,4,=10"
     }, delimiter = '=')
     void 쉼표로_구분된_여러_숫자_입력_시_합_반환(final String input, final Integer expected){
-        Integer result = StringCalculator.splitAndSum(input);
+        Integer result = StringAddCalculator.splitAndSum(input);
         assertThat(expected).isEqualTo(result);
     }
 
@@ -48,7 +51,7 @@ public class StringCalculatorTest {
             "1,2,3:4,=10"
     }, delimiter = '=')
     void 구분자가_쉼표와_콜론일때_합_반환(final String input, final Integer expected){
-        Integer result = StringCalculator.splitAndSum(input);
+        Integer result = StringAddCalculator.splitAndSum(input);
         assertThat(expected).isEqualTo(result);
     }
 
@@ -63,7 +66,14 @@ public class StringCalculatorTest {
             "'//,|\n1,2|3,4', 10"
     })
     void 커스텀_구분자를_사용한_합_반환(final String input, final Integer expected) {
-        Integer result = StringCalculator.splitAndSum(input);
+        Integer result = StringAddCalculator.splitAndSum(input);
         assertThat(expected).isEqualTo(result);
     }
+
+    @Test
+    void 음수_입력_시_RuntimeException_발생() {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
 }
