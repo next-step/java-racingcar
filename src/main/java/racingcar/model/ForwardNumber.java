42ec4ce81045ea5aa9c5c1f60a0946cca51c5cdf
mitsui3point <rladbsgh0331@gmail.com>
2024-09-28 17:23:28 +0900

    feat(racingcar): update ForwardNumber(동등비교)
    test(racingcar): add 동등비교

--- src/main/java/racingcar/model/ForwardNumber.java
+++ src/main/java/racingcar/model/ForwardNumber.java
@@ -2,10 +2,28 @@ package racingcar.model;
 
 import racingcar.exception.ForwardNumberException;
 
+import java.util.Objects;
+
 public class ForwardNumber {
+    private final int number;
+
     public ForwardNumber(int number) {
         if (number < 0 || number > 9) {
             throw new ForwardNumberException("0~9 사이의 숫자 외에는 전진숫자로 허용되지 않습니다.");
         }
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ForwardNumber that = (ForwardNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
--- src/test/java/racingcar/model/ForwardNumberTest.java
+++ src/test/java/racingcar/model/ForwardNumberTest.java
@@ -1,15 +1,18 @@
 package racingcar.model;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.exception.ForwardNumberException;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class ForwardNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
     void number_값은_0_9_사이이다(int number) {
-        Assertions.assertThatNoException().isThrownBy(() -> {
+        assertThatNoException().isThrownBy(() -> {
             ForwardNumber forwardNumber = new ForwardNumber(number);
         });
     }
@@ -17,8 +20,16 @@ public class ForwardNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {-1, Integer.MIN_VALUE, 10, Integer.MAX_VALUE})
     void number_값은_0미만_9초과시_에러(int number) {
-        Assertions.assertThatThrownBy(() -> {
+        assertThatThrownBy(() -> {
             ForwardNumber forwardNumber = new ForwardNumber(number);
         }).isInstanceOf(ForwardNumberException.class).hasMessage("0~9 사이의 숫자 외에는 전진숫자로 허용되지 않습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+    void 동등_비교(int number) {
+        ForwardNumber actual = new ForwardNumber(number);
+        ForwardNumber expected = new ForwardNumber(number);
+        assertThat(actual).isEqualTo(expected);
     }
 }
