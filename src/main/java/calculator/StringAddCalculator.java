90b3af27a082a33293bd56818292c78332b9e08c
Jooho Kim <61738279+Hoya517@users.noreply.github.com>
2024-09-28 09:31:11 +0900

    [자동차 경주 > 2단계] 코드 리뷰 요청 (#5631)
    * refactor: step1 코드 리뷰 반영
    
    - @DisplayName 수정
    - 단언문 기능 사용
    - 성공/실패 테스트 분리
    
    https://github.com/next-step/java-racingcar/pull/5587/files
    
    * feat: calculator with TDD
    
    * test: 숫자 이외의 값
    
    * refactor: 음수 검증
    
    * fix: 상수 이름 교체
    
    * refactor: 유틸리티 클래스 생성자 제한
    
    유틸리티 클래스는 일반적으로 인스턴스화할 필요가 없는 클래스이며, 생성자 제한으로 객체 생성을 차단하거나 싱글톤 패턴으로 인스턴스를 고유할 수 있다. 유틸리티 클래스는 상태 없이 기능만 제공하는 경우에 적합하고, 싱글톤 패턴은 상태를 공유하는 객체에서 사용된다.
    
    * refactor: 매개변수에 다른 함수의 호출 결과 반환값을 넣지 않기 (디버깅 불편)
    
    * refactor: Pattern.compile() 상수 분리
    
    재사용성: 패턴을 상수로 만들어 반복적으로 compile을 호출할 필요가 없다.
    
    * refactor: 매직넘버 상수로 변경(가독성)
    
    가독성: 숫자가 어떤 의미를 가지는지 명확해져 코드를 읽는 사람이 쉽게 이해할 수 있습니다.
    
    * refactor: 불필요한 테스트 삭제 (가독성)
    
    가독성: 결과가 당연한 테스트가 여러개라면 굳이 필요한 테스트일까?

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,63 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    public static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    public static final String DEFAULT_DELIMITER = "[,:]";
+    public static final int CUSTOM_DELIMITER_GROUP = 1;
+    public static final int TEXT_GROUP = 2;
+    public static final int BLANK_STRING_SUM = 0;
+
+    private StringAddCalculator() {
+        throw new UnsupportedOperationException("Utility class cannot be instantiated.");
+    }
+
+    public static int splitAndSum(String text) {
+        if (isBlank(text)) {
+            return BLANK_STRING_SUM;
+        }
+        String[] split = split(text);
+        int[] intArray = toIntArray(split);
+        return sum(intArray);
+    }
+
+    private static int sum(int[] numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            result += number;
+        }
+        return result;
+    }
+
+    private static int[] toIntArray(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = toInt(values[i]);
+        }
+        return numbers;
+    }
+
+    private static int toInt(String values) {
+        int number = Integer.parseInt(values);
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
+    private static String[] split(String text) {
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(CUSTOM_DELIMITER_GROUP);
+            return m.group(TEXT_GROUP).split(customDelimiter);
+        }
+        return text.split(DEFAULT_DELIMITER);
+    }
+
+    private static boolean isBlank(String text) {
+        return text == null || text.isEmpty();
+    }
+}
