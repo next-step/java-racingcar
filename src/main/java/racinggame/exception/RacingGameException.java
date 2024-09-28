905052742ad9505bd0e2c26fe72b9eff41d9abf5
SangHyun Kim <ksh9891@gmail.com>
2024-09-28 19:49:46 +0900

    feat(racing): exception 추가

---
+++ src/main/java/racinggame/exception/RacingGameException.java
@@ -0,0 +1,7 @@
+package racinggame.exception;
+
+public class RacingGameException extends RuntimeException {
+    public RacingGameException(final String message) {
+        super(message);
+    }
+}
