192383055011a81541b80956d034f4af08af838f
Slowth-KIM <rt.slowth@gmail.com>
2024-09-28 20:33:49 +0900

    feedback : DisplayName 활용, @NullAndEmptySource 사용

--- src/test/java/calculator/MyStringAddCalculatorTest.java
+++ src/test/java/calculator/MyStringAddCalculatorTest.java
@@ -1,31 +1,35 @@
 package calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MyStringAddCalculatorTest {
 
+    @DisplayName("빈 문자열 입력시 0을 반환")
     @ParameterizedTest
-    @NullSource
+    @NullAndEmptySource
     @ValueSource(strings = {"", "   "})
-    void 빈_문자열_입력_시_0_반환(final String input){
+    void emptyInputReturnsZeroTest(final String input){
         int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(0);
     }
 
+    @DisplayName("숫자만 입력시 숫자를 반환")
     @ParameterizedTest
     @ValueSource(strings = {"1", "123", "45678912"})
-    void 숫자만_입력시_숫자_반환(final String input ){
+    void digitInputReturnsIntTest(final String input ){
         int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(Integer.parseInt(input));
     }
 
 
+    @DisplayName("쉼표로 구분된 여러 숫자 입력시 한 반환")
     @ParameterizedTest
     @CsvSource(value = {
             "1,2=3",
@@ -36,12 +40,13 @@ class MyStringAddCalculatorTest {
             "1,2,3,4=10",
             "1,2,3,4,=10"
     }, delimiter = '=')
-    void 쉼표로_구분된_여러_숫자_입력_시_합_반환(final String input, final Integer expected){
+    void commaSeparatedSumTest(final String input, final Integer expected){
         Integer result = StringAddCalculator.splitAndSum(input);
         assertThat(expected).isEqualTo(result);
     }
 
 
+    @DisplayName("구분자가 쉼표와 콜론일 때 합 반화")
     @ParameterizedTest
     @CsvSource(value = {
             "1,2:=3",
@@ -49,11 +54,12 @@ class MyStringAddCalculatorTest {
             "1,2:3,4=10",
             "1,2,3:4,=10"
     }, delimiter = '=')
-    void 구분자가_쉼표와_콜론일때_합_반환(final String input, final Integer expected){
+    void sumWhenCommaAndColonDelimiterTest(final String input, final Integer expected){
         Integer result = StringAddCalculator.splitAndSum(input);
         assertThat(expected).isEqualTo(result);
     }
 
+    @DisplayName("커스텀 구분자를 사용한 합 반환")
     @ParameterizedTest
     @CsvSource(value = {
             "'//;\n1;2;3', 6",
@@ -64,19 +70,22 @@ class MyStringAddCalculatorTest {
             "'//;\n1;2;3;4', 10",
             "'//,|\n1,2|3,4', 10"
     })
-    void 커스텀_구분자를_사용한_합_반환(final String input, final Integer expected) {
+    void customDelimiterSumTest(final String input, final Integer expected) {
         Integer result = StringAddCalculator.splitAndSum(input);
         assertThat(expected).isEqualTo(result);
     }
 
+
+    @DisplayName("음수 입력 시 RuntimeException 발생")
     @Test
-    void 음수_입력_시_RuntimeException_발생() {
+    void negativeInputThrowsTest() {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 
+    @DisplayName("숫자가 아닌 값 입력시 RuntimeException 발생")
     @Test
-    void 숫자가_아닌_값_입력_시_RuntimeException_발생() {
+    void nonNumericInputThrowsTest() {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("1,a,3"))
                 .isInstanceOf(RuntimeException.class);
     }
