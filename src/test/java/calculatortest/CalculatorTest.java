f7c958edb0cfad9fbf5e588f26063d3626a18df8
jara <jara0701@gmail.com>
2024-09-28 21:29:19 +0900

    와일드카드 임포트 제거

--- src/test/java/calculatortest/CalculatorTest.java
+++ src/test/java/calculatortest/CalculatorTest.java
@@ -3,7 +3,8 @@ package calculatortest;
 import calculator.Calculator;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public final class CalculatorTest {
     private final Calculator calculator = new Calculator();
