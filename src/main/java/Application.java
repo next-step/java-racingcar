d0e07eddeecf980c3b2c248422c9f78cdda60b10
sinsu <sinsu19012@gmail.com>
2024-09-29 21:49:31 +0900

    feat: 자동차의 상태를 출력하는 기능 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,17 @@
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         RacingCarInputView racingCarInputView = new RacingCarInputView();
 
-        racingCarInputView.getCarCount();
-        racingCarInputView.getAttempts();
+        int carCount = racingCarInputView.getCarCount();
+        int attempts = racingCarInputView.getAttempts();
+
+        RacingGame racingGame = new RacingGame(carCount,attempts,new RandomMovementCondition());
+        List<List<Integer>> allStates = racingGame.run();
+
+        RacingCarOutputView racingCarOutputView = new RacingCarOutputView();
+
+        racingCarOutputView.printGame(allStates);
     }
 }
