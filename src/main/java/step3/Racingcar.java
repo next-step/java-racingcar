c1de0acec55b05bbd0d3fbf7e3b4151e3af713b5
ryumina-edu <fbalsk8897@gmail.com>
2024-09-29 21:15:49 +0900

    feat: input validation

---
+++ src/main/java/step3/Racingcar.java
@@ -0,0 +1,44 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Racingcar {
+
+    public static void main(String[] args) {
+        execute();
+    }
+
+    public static void execute() {
+        Scanner scanner = new Scanner(System.in);
+        String car = scanner.nextLine();
+        String round = scanner.nextLine();
+
+        validateInput(car, round);
+
+        // 게임 시작
+    }
+
+    private static void validateInput(String carInput, String roundInput) {
+        int car;
+        int round;
+
+        if (carInput == null || carInput.isEmpty()) {
+            throw new IllegalArgumentException("자동차 수 : 올바른 입력값이 아닙니다.");
+        }
+
+        if (roundInput == null || roundInput.isEmpty()) {
+            throw new IllegalArgumentException("게임 진행 횟수 : 올바른 입력값이 아닙니다.");
+        }
+
+        try {
+            car = Integer.parseInt(carInput);
+            round = Integer.parseInt(roundInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값은 숫자(양수)만 입력 가능합니다.");
+        }
+
+        if (car < 0 || round < 0) {
+            throw new IllegalArgumentException("입력값은 양수만 입력 가능합니다.");
+        }
+    }
+}
