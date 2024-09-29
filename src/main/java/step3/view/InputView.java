e0dfe67e71e739078b9b255782037e3e6c0b3284
hwan2-99 <thh1233123@gmail.com>
2024-09-29 11:21:10 +0900

    feat: 시도할 횟수 입력메서드 구현

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
 
     public InputView() {
     }
@@ -14,7 +15,16 @@ public class InputView {
         return inputCarCount();
     }
 
+    public static int getTryCount() {
+        System.out.println(INPUT_TRY_COUNT_MESSAGE);
+        return inputTryCount();
+    }
+
     private static int inputCarCount(){
+        return scanner.nextInt();
+    }
+
+    private static int inputTryCount(){
         return scanner.nextInt();
     }
 }
--- src/main/java/step3/view/RacingCarApplication.java
+++ src/main/java/step3/view/RacingCarApplication.java
@@ -3,5 +3,6 @@ package step3.view;
 public class RacingCarApplication {
     public static void main(String[] args) {
         int carCount = InputView.getCarCount();
+        int tryCount = InputView.getTryCount();
     }
 }
