191e045e4fc469af6e8fd2bda27025fd3b7a31b0
Park Jungwook <cco2416@gmail.com>
2024-09-29 13:48:59 +0900

    refactor : step2 comment 반영

--- src/main/java/study/step2/StringAddCalculator.java
+++ src/main/java/study/step2/StringAddCalculator.java
@@ -41,7 +41,7 @@ public class StringAddCalculator {
     private static int parseIntOrThrow(String value) {
         int number = Integer.parseInt(value);
         if (number < 0) {
-            throw new RuntimeException();
+            throw new RuntimeException("음수는 입력할 수 없습니다.");
         }
         return number;
     }
