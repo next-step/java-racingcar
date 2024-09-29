b032a5482a4999a81b29db6847906360a963a8a4
Joonkb <byeongjunkim@naver.com>
2024-09-29 16:42:43 +0900

    [feat] 랜덤한 숫자 반환하는 기능 테스트 및 구현

---
+++ src/test/java/racingcar/utils/NumberGeneratorTest.java
@@ -0,0 +1,17 @@
+package racingcar.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumberGeneratorTest {
+
+    @Test
+    @DisplayName("랜덤값이 0에서 9 사이의 값인지 확인한다.")
+    void 랜덤값_반환() {
+        int randomNumber = NumberGenerator.randomNumber();
+        assertThat(randomNumber).isGreaterThan(0)
+                .isLessThan(9);
+    }
+}
