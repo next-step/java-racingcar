2113573dbf1dec7978b0f09d6ab8ecb44b14a7b3
hwan2-99 <thh1233123@gmail.com>
2024-09-29 11:29:33 +0900

    refactor: 동일한 로직을 가진 메서드 하나로 통합

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -6,25 +6,31 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private static final String MIN_COUNT_ERROR_MESSAGE = "최소 1이상의 숫자를 입력하셔야 합니다.";
+    private static final int MIN_COUNT = 1;
 
     public InputView() {
     }
 
     public static int getCarCount() {
         System.out.println(INPUT_CAR_COUNT_MESSAGE);
-        return inputCarCount();
+        return inputCountValue();
     }
 
     public static int getTryCount() {
         System.out.println(INPUT_TRY_COUNT_MESSAGE);
-        return inputTryCount();
+        return inputCountValue();
     }
 
-    private static int inputCarCount(){
-        return scanner.nextInt();
+    private static int inputCountValue() {
+        int value = scanner.nextInt();
+        validateMinCount(value);
+        return value;
     }
 
-    private static int inputTryCount(){
-        return scanner.nextInt();
+    private static void validateMinCount(int inputValue) {
+        if (inputValue < MIN_COUNT) {
+            throw new IllegalArgumentException(MIN_COUNT_ERROR_MESSAGE);
+        }
     }
 }
