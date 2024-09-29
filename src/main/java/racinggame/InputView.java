c6f96c89d1bdebc975da52a770a54088d46146ee
이윤선 <yoonseon3@gmail.com>
2024-09-30 01:08:00 +0900

    feature: 게임 시도할 횟수를 입력받는다.

--- src/main/java/racinggame/InputView.java
+++ src/main/java/racinggame/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String CAR_COUNT_QUESTION = "자동차 대수는 몇 대 인가요?";
+    private static final String TRY_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
     private static final Scanner scanner = new Scanner(System.in);
 
     private InputView() {
@@ -14,4 +15,10 @@ public class InputView {
         System.out.println(CAR_COUNT_QUESTION);
         return scanner.nextInt();
     }
+
+    public static int readTryCount() {
+        System.out.println(TRY_COUNT_QUESTION);
+        return scanner.nextInt();
+    }
+
 }
