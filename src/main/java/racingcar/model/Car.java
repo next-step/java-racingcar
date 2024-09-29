8ff5b488fda7d3cd7abab12c0e2f1bed0bff773d
mitsui3point <rladbsgh0331@gmail.com>
2024-09-29 17:08:53 +0900

    feat(racingcar): modify Car field status -> statuses
    test(racingcar): add method 자동차의_주어진_이동_횟수_동안_전진_멈춤_상태를_저장한다

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -3,20 +3,23 @@ package racingcar.model;
 import racingcar.model.enums.Status;
 import racingcar.model.wrapper.ForwardNumber;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Car {
-    private Status currentStatus;
+    private final List<Status> statuses;
 
     public Car() {
+        this.statuses = new ArrayList<>();
     }
 
-    protected Car(Status currentStatus) {
-        this.currentStatus = currentStatus;
+    public Car(List<Status> statuses) {
+        this.statuses = statuses;
     }
 
     public void move(ForwardNumber forwardNumber) {
-        this.currentStatus = Status.extractByNumber(forwardNumber);
+        this.statuses.add(Status.extractByNumber(forwardNumber));
     }
 
     @Override
@@ -24,11 +27,11 @@ public class Car {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return currentStatus == car.currentStatus;
+        return Objects.equals(statuses, car.statuses);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(currentStatus);
+        return Objects.hash(statuses);
     }
 }
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,9 +1,12 @@
 package racingcar.model;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.model.enums.Status;
 import racingcar.model.wrapper.ForwardNumber;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,8 +17,8 @@ public class CarTest {
         Car car = new Car();
         car.move(new ForwardNumber(number));
 
-        Car expected = new Car();
-        expected.move(new ForwardNumber(number));
+        Status status = Status.extractByNumber(new ForwardNumber(number));
+        Car expected = new Car(List.of(status));
 
         assertThat(car).isEqualTo(expected);
     }
@@ -26,7 +29,7 @@ public class CarTest {
         Car car = new Car();
         car.move(new ForwardNumber(number));
 
-        Car expected = new Car(Status.FORWARD);
+        Car expected = new Car(List.of(Status.FORWARD));
 
         assertThat(car).isEqualTo(expected);
     }
@@ -37,7 +40,21 @@ public class CarTest {
         Car car = new Car();
         car.move(new ForwardNumber(number));
 
-        Car expected = new Car(Status.STOP);
+        Car expected = new Car(List.of(Status.STOP));
+
+        assertThat(car).isEqualTo(expected);
+    }
+
+    @Test
+    void 자동차의_주어진_이동_횟수_동안_전진_멈춤_상태를_저장한다() {
+        Car car = new Car();
+        car.move(new ForwardNumber(4));
+        car.move(new ForwardNumber(3));
+        car.move(new ForwardNumber(2));
+        car.move(new ForwardNumber(5));
+
+        List<Status> statuses = List.of(Status.FORWARD, Status.STOP, Status.STOP, Status.FORWARD);
+        Car expected = new Car(statuses);
 
         assertThat(car).isEqualTo(expected);
     }
