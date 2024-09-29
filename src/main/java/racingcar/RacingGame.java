408849f090055db02f36beba7e9c51a4d7c464f6
mitsui3point <rladbsgh0331@gmail.com>
2024-09-29 23:42:09 +0900

    feat(racingcar): modify RacingGame constructor
    test(racingcar): add method 이동은_최소_1번_이상이다, 이동_1번_미만_오류

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,11 +4,16 @@ import racingcar.exception.RacingGameException;
 
 public class RacingGame {
     private final int round;
+    private final int movement;
 
-    public RacingGame(int round) {
+    public RacingGame(int round, int movement) {
         if (round < 1) {
             throw new RacingGameException("자동차는 최소 1대 이상이어야 합니다.");
         }
+        if (movement < 1) {
+            throw new RacingGameException("이동은 최소 1번 이상이어야 합니다.");
+        }
         this.round = round;
+        this.movement = movement;
     }
 }
--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,25 +1,44 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.exception.RacingGameException;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingGameTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4})
     void 자동차의_갯수는_최소_1대_이상이다(int round) {
-        Assertions.assertThatNoException().isThrownBy(() -> {
-            RacingGame racingGame = new RacingGame(round);
+        assertThatNoException().isThrownBy(() -> {
+            RacingGame racingGame = new RacingGame(round, 1);
         });
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, -1, Integer.MIN_VALUE})
-    void 자동차의_갯수는_최소_1대_미만이면_오류(int round) {
-        Assertions.assertThatThrownBy(() -> {
-                    RacingGame racingGame = new RacingGame(round);
-                }).isInstanceOf(RacingGameException.class)
+    void 자동차의_갯수_1대_미만이면_오류(int round) {
+        assertThatThrownBy(() -> {
+            RacingGame racingGame = new RacingGame(round, 1);
+        }).isInstanceOf(RacingGameException.class)
                 .hasMessage("자동차는 최소 1대 이상이어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    void 이동은_최소_1번_이상이다(int movement) {
+        assertThatNoException().isThrownBy(() -> {
+            RacingGame racingGame = new RacingGame(1, movement);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, Integer.MIN_VALUE})
+    void 이동_1번_미만_오류(int movement) {
+        assertThatThrownBy(() -> {
+            RacingGame racingGame = new RacingGame(1, movement);
+        }).isInstanceOf(RacingGameException.class)
+                .hasMessage("이동은 최소 1번 이상이어야 합니다.");
     }
 }
