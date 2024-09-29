e48faae54e272358be091b3043b3e688dcf60daf
StoneHee99 <kiit0901@gmail.com>
2024-09-29 23:54:58 +0900

    test: 실패하는 테스트 작성

--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -13,4 +13,12 @@ class CarTest {
         Car car = Car.createCar();
         assertThat(car.getCurrentPosition()).isZero();
     }
+
+    @DisplayName("자동차가 생성된 랜덤 난수가 4 이상인 경우 한 칸 전진하는지")
+    @Test
+    void carMoveTest() {
+        Car car = Car.createCar();
+        car.move();
+        assertThat(car.getCurrentPosition()).isOne();
+    }
 }
