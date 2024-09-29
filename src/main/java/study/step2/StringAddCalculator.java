df8e40ef5759bec153c5648b9bb2d3fc334fa93e
Jungwook Park <48744330+Park1122@users.noreply.github.com>
2024-09-29 13:34:12 +0900

    2단계 - 문자열 덧셈 계산기 (#5637)
    * chore : 1단계 과제 폴더 정리
    
    * feat : StringAddCalculator 구현
    
    * refactor : StringAddCalculator 리팩토링

---
+++ src/main/java/study/step2/StringAddCalculator.java
@@ -0,0 +1,49 @@
+package study.step2;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//(.)\n(.*)";
+    private static final int GROUP_INDEX_ONE = 1;
+    private static final int GROUP_INDEX_TWO = 2;
+
+    public static int splitAndSum(String input) {
+        if (!hasText(input)) {
+            return 0;
+        }
+        String[] splitValues = split(input);
+        return sum(splitValues);
+    }
+
+    private static boolean hasText(String input) {
+        return input != null && !input.isEmpty();
+    }
+
+    private static String[] split(String input) {
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER_PREFIX).matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(GROUP_INDEX_ONE);
+            return m.group(GROUP_INDEX_TWO).split(customDelimiter);
+        }
+        return input.split(DEFAULT_DELIMITER);
+    }
+
+    private static int sum(String[] values) {
+        return Arrays.stream(values)
+                .mapToInt(StringAddCalculator::parseIntOrThrow)
+                .sum();
+    }
+
+    private static int parseIntOrThrow(String value) {
+        int number = Integer.parseInt(value);
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
+}
