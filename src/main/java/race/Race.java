bcccec389c14b8dd43a1f7008f322b84b50dc233
owljoa <needshield@gmail.com>
2024-09-29 19:14:49 +0900

    refactor: 전진을 위한 최소 숫자 상수화

--- src/main/java/race/Race.java
+++ src/main/java/race/Race.java
@@ -56,39 +56,4 @@ public class Race {
     private static int generateIntBetween0and9() {
         return new Random().nextInt(RANDOM_UPPER_LIMIT);
     }
-
-    public static boolean canGo(int number) {
-        return number >= 4;
-    }
-
-    public static int[] initiateCarStates(int carCount) {
-        return new int[carCount];
-    }
-
-    public static void moveCarForward(int[] carStates, int carIndex) {
-        carStates[carIndex]++;
-    }
-
-    public static void start() {
-        RaceInput raceInput = InputView.inputCarCountAndRandomStopOrGoCount();
-        int[] carStates = initiateCarStates(raceInput.carCount());
-
-        ResultView.printResultTitle();
-        for (int stopOrGoRound = 0; stopOrGoRound < raceInput.randomStopOrGoCount(); stopOrGoRound++) {
-            runStopOrGoRound(raceInput.carCount(), carStates);
-        }
-    }
-
-    private static void runStopOrGoRound(int carCount, int[] carStates) {
-        for (int carIndex = 0; carIndex < carCount; carIndex++) {
-            moveCarForwardIfCanGo(carStates, carIndex);
-        }
-        ResultView.printCarStates(carStates);
-    }
-
-    private static void moveCarForwardIfCanGo(int[] carStates, int carIndex) {
-        if (canGo(generateIntBetween0and9())) {
-            moveCarForward(carStates, carIndex);
-        }
-    }
 }
