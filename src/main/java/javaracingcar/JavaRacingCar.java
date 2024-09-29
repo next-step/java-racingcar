a10e9f80d197f389cf864ad868765a7612c290b9
jeonghyeonkwon <givejeong2468@gmail.com>
2024-09-29 16:47:33 +0900

    feat: random값에 따른 boolean 메소드 생성

--- src/main/java/javaracingcar/JavaRacingCar.java
+++ src/main/java/javaracingcar/JavaRacingCar.java
@@ -7,12 +7,24 @@ public class JavaRacingCar {
     private Random random;
 
     private static final int RANDOM_VALUE_RANGE = 10;
+    private static final int MIN_MOVING_RANGE = 4;
+    private static final int MAX_MOVING_RANGE = 9;
 
-    public JavaRacingCar(){
+    public JavaRacingCar() {
         random = new Random();
     }
+
     protected int random() {
         int randomValue = random.nextInt(RANDOM_VALUE_RANGE);
         return randomValue;
+    }
+
+    protected boolean isMove(int randomValue) {
+
+        if (MIN_MOVING_RANGE <= randomValue && randomValue <= MAX_MOVING_RANGE) {
+            return true;
+        }
+
+        return false;
     }
 }
--- src/test/java/javaracingcar/JavaRacingCarTest.java
+++ src/test/java/javaracingcar/JavaRacingCarTest.java
@@ -7,7 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JavaRacingCarTest {
 
-
     @Test
     void javaRacingCar_Random_범위_0에서_9사이인지() {
         JavaRacingCar racingCar = new JavaRacingCar();
@@ -16,4 +15,50 @@ public class JavaRacingCarTest {
 
         assertThat(result).isGreaterThanOrEqualTo(0).isLessThanOrEqualTo(9);
     }
+
+    @Test
+    void javaRacingCar_randomValue_0인_값(){
+        JavaRacingCar racingCar = new JavaRacingCar();
+
+        boolean result = racingCar.isMove(0);
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void javaRacingCar_randomValue_3인_값(){
+        JavaRacingCar racingCar = new JavaRacingCar();
+
+        boolean result = racingCar.isMove(3);
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void javaRacingCar_randomValue_4인_값(){
+        JavaRacingCar racingCar = new JavaRacingCar();
+
+        boolean result = racingCar.isMove(4);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void javaRacingCar_randomValue_9인_값(){
+        JavaRacingCar racingCar = new JavaRacingCar();
+
+        boolean result = racingCar.isMove(9);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void javaRacingCar_randomValue_10인_값(){
+        JavaRacingCar racingCar = new JavaRacingCar();
+
+        boolean result = racingCar.isMove(10);
+
+        assertThat(result).isFalse();
+    }
+
 }
