bd02dfe29181b88f60cfb80d653e6788f244bbf8
SeongJunWi <dnltjdwns235@gmail.com>
2024-09-29 21:38:51 +0900

    2단계 - 문자열 덧셈 계산기 누락한 빈문자열,null 0 처리 테스트 추가 커스텀 구분자 테스트 / 기능 추가 및 리팩토링

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -1,7 +1,36 @@
 package step2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+
 	public static int calculate(String text) {
-		return Integer.parseInt(text);
+		String CUSTOM_DELIMITER = "//(.*)\\n(.*)";
+		String DEFAULT_DELIMITER = ",|:";
+
+		if (text == null || text.isEmpty()) {
+			return 0;
+		}
+
+		Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(text);
+		if (matcher.find()) {
+			String customSeperator = matcher.group(1);
+			String numberArrayText = matcher.group(2);
+			return splitByDelimiterAndSum(numberArrayText, DEFAULT_DELIMITER + "|" + customSeperator);
+		}
+		return splitByDelimiterAndSum(text, DEFAULT_DELIMITER);
+	}
+
+	private static int splitByDelimiterAndSum(String text, String DELIMITER) {
+		return parseStringToInt(text.split(DELIMITER));
+	}
+
+	private static int parseStringToInt(String[] StringArray) {
+		int sumResult = 0;
+		for (String s : StringArray) {
+			sumResult += Integer.parseInt(s);
+		}
+		return sumResult;
 	}
 }
