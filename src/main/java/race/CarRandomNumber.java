7dc9775ea769f20e3906558c9e77aef3b5359c7f
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-09-29 17:13:45 +0900

    refactor: Random range RandomNumber 구현체가 지니도록 수정, 매직 넘저 상수 처리
    Random range RandomNumber 구현체가 지니도록 수정, 매직 넘저 상수 처리

--- src/main/java/race/CarRandomNumber.java
+++ src/main/java/race/CarRandomNumber.java
@@ -4,16 +4,15 @@ import java.util.Random;
 
 public class CarRandomNumber implements RandomNumber {
 
-    private int randomRange;
-
+    private final static int RANDOM_RANGE = 10;
+    private final static int FORWARD_NUMBER = 4;
     private final static Random RANDOM = new Random();
 
-    public CarRandomNumber(int randomRange) {
-        this.randomRange = randomRange;
-    }
-
     @Override
-    public int randomNumber() {
-        return RANDOM.nextInt(randomRange);
+    public boolean isForward() {
+        if (RANDOM.nextInt(RANDOM_RANGE) >= FORWARD_NUMBER) {
+            return true;
+        }
+        return false;
     }
 }
