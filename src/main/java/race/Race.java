219285a703b330c39232a5918a109f909cb74d9e
owljoa <needshield@gmail.com>
2024-09-29 18:23:44 +0900

    feat: 0~9 사이 랜덤값 생성

---
+++ src/main/java/race/Race.java
@@ -0,0 +1,11 @@
+package race;
+
+import java.util.Random;
+
+public class Race {
+    public static final int RANDOM_UPPER_LIMIT = 10;
+
+    private static int generateIntBetween0and9() {
+        return new Random().nextInt(RANDOM_UPPER_LIMIT);
+    }
+}
