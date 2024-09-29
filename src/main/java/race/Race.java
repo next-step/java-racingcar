5952513c67fa0d1e7ca95dcb501dbf090731fcee
owljoa <needshield@gmail.com>
2024-09-29 19:12:42 +0900

    feat: 경주 시작 - 단위기능 연결 및 main 함수 추가

--- src/main/java/race/Race.java
+++ src/main/java/race/Race.java
@@ -65,8 +65,30 @@ public class Race {
         return new int[carCount];
     }
 
-
     public static void moveCarForward(int[] carStates, int carIndex) {
         carStates[carIndex]++;
+    }
+
+    public static void start() {
+        RaceInput raceInput = InputView.inputCarCountAndRandomStopOrGoCount();
+        int[] carStates = initiateCarStates(raceInput.carCount());
+
+        ResultView.printResultTitle();
+        for (int stopOrGoRound = 0; stopOrGoRound < raceInput.randomStopOrGoCount(); stopOrGoRound++) {
+            runStopOrGoRound(raceInput.carCount(), carStates);
+        }
+    }
+
+    private static void runStopOrGoRound(int carCount, int[] carStates) {
+        for (int carIndex = 0; carIndex < carCount; carIndex++) {
+            moveCarForwardIfCanGo(carStates, carIndex);
+        }
+        ResultView.printCarStates(carStates);
+    }
+
+    private static void moveCarForwardIfCanGo(int[] carStates, int carIndex) {
+        if (canGo(generateIntBetween0and9())) {
+            moveCarForward(carStates, carIndex);
+        }
     }
 }
