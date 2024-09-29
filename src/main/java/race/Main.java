f94887c179b2e90c75e29215cf23a7b8d142f534
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-09-29 17:14:37 +0900

    refactor: Main 클래스 패키지 이동 및 자동차 초기화 메소드 책임 CarRace로 변경
    Main 클래스 패키지 이동 및 자동차 초기화 메소드 책임 CarRace로 변경

--- src/main/java/race/Main.java
+++ src/main/java/race/Main.java
@@ -5,27 +5,14 @@ import java.util.List;
 
 public class Main {
 
-    private static final int RANDOM_RANGE = 10;
-
     public static void main(String[] args) {
 
         RaceInput raceInput = InputView.input();
 
-        List<Car> cars = initCars(raceInput.getCarCount());
+        CarRace carRace = new CarRace(raceInput.getCarCount(), raceInput.getTryCount(), new CarRandomNumber());
 
-        CarRace carRace = new CarRace(raceInput.getCarCount(), raceInput.getTryCount(), cars);
-
+        ResultView.output(carRace.getCars());
         carRace.start();
     }
 
-    private static List<Car> initCars(int carCount) {
-        List<Car> cars = new ArrayList<>();
-        RandomNumber randomNumberGenerator = new CarRandomNumber(RANDOM_RANGE);
-
-        for(int i = 0; i < carCount; i++) {
-            cars.add(new Car(randomNumberGenerator));
-        }
-
-        return cars;
-    }
 }
