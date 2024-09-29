42a50646920140ae1597d1152a78cb85b907f705
jeonghyeonkwon <givejeong2468@gmail.com>
2024-09-29 13:42:17 +0900

    refactor: 문자열 덧셈 계산기 피드백 사항 수정
    - Pattern 객체 상수화
    - 계산기 기본 값 상수
    - 음수 검증 메소드 리턴 값 void로 수정

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -9,19 +9,23 @@ public class StringAddCalculator {
 
     private static final String DEFAULT_DELIMITER = ",|:";
     private static final String CUSTOM_DELIMITER_REGEXP = "//(.)\\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEXP);
+
+    private static final int DEFAULT_CALCULATOR_VALUE = 0;
+
 
     private StringAddCalculator() {
     }
 
     public static int splitAndSum(String text) {
 
-        if (text == null) return 0;
+        if (text == null) return DEFAULT_CALCULATOR_VALUE;
 
         Map<String, String> delimiterAndTextMap = getCustomDelimiterAndText(DEFAULT_DELIMITER, text);
         String confirmDelimiter = delimiterAndTextMap.get("delimiter");
         String confirmText = delimiterAndTextMap.get("text");
 
-        if (confirmText.isEmpty()) return 0;
+        if (confirmText.isEmpty()) return DEFAULT_CALCULATOR_VALUE;
 
         return getSum(toInts(getSplitByDelimiter(confirmDelimiter, confirmText)));
     }
@@ -39,12 +43,13 @@ public class StringAddCalculator {
     }
 
     private static int toInt(String stringNumber) {
-        return validateNegativeNumber(parse(stringNumber));
+        int parseIntNumber = parse(stringNumber);
+        validateNegativeNumber(parseIntNumber);
+        return parseIntNumber;
     }
 
-    private static int validateNegativeNumber(int number) {
+    private static void validateNegativeNumber(int number) {
         if (number < 0) throw new IllegalArgumentException("음수 입력");
-        return number;
     }
 
     private static int parse(String token) throws NumberFormatException {
@@ -52,7 +57,7 @@ public class StringAddCalculator {
     }
 
     private static int getSum(int[] numbers) throws RuntimeException {
-        int result = 0;
+        int result = DEFAULT_CALCULATOR_VALUE;
 
         for (int number : numbers) {
             result += number;
@@ -64,7 +69,7 @@ public class StringAddCalculator {
     private static Map<String, String> getCustomDelimiterAndText(String delimiter, String text) {
         Map<String, String> result = new HashMap<>();
 
-        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEXP).matcher(text);
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
 
         if (matcher.find()) {
             result.put("delimiter", matcher.group(1));
