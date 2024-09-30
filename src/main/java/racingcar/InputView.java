2ceeef3d02674b99573f0a4bfe9bf3e224c49480
ming <devel.moon17@gmail.com>
2024-09-30 23:50:25 +0900

    fix: 단위테스트 3단계 - 자동차 경주 - private/public으로만 구분
    ---------------------------------------
    
    단위테스트 3단계 - 자동차 경주 1차 피드백 내용
    
    - 절차지향 > 객체지향으로 구현하기, 요구사항으로부터 객체를 추출하여 행위를 호출하도록 작성하기
    - 특별한 경우가 아니라면 public/private 정도로만 구분하기
    - 요구사항을 바탕으로 나온 기능 목록만 남기고, 커밋단위로 체크하기
    - 출력은 ResultView 에서 처리
    - 매직넘버/매직리터럴은 상수로 표현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -6,12 +6,12 @@ public class InputView {
 
     private static Scanner scanner = new Scanner(System.in);
 
-    static int inputCarCount() {
+    public static int inputCarCount() {
         System.out.println("자동차 대수는 몇 대 인가요?");
         return scanner.nextInt();
     }
 
-    static int inputLoop() {
+    public static int inputLoop() {
         System.out.println("시도할 횟수는 몇 회 인가요?");
         return scanner.nextInt();
     }
