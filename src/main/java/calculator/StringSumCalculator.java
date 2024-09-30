c60305783e1cf7d6e20100f386f78f3ea38ba33c
heechul <khc4190@naver.com>
2024-09-30 17:41:16 +0900

    콜론 구분자 구현 및 리팩토링

--- src/main/java/calculator/StringSumCalculator.java
+++ src/main/java/calculator/StringSumCalculator.java
@@ -2,12 +2,20 @@ package calculator;
 
 public class StringSumCalculator {
 
+    public static final String DELIMITER = "[,:]";
+
     public static int sum(final String text) {
         int result = 0;
-        if(text == null || text.isEmpty()){
+        if (text == null || text.isEmpty()) {
             return result;
         }
-        String[] split = text.split(",");
+
+        return sumResult(text);
+    }
+
+    private static int sumResult(String text) {
+        int result = 0;
+        String[] split = text.split(DELIMITER);
         for (String s : split) {
             result += Integer.parseInt(s);
         }
