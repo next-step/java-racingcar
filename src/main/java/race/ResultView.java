c10dcb6c610cf8ef7d07537bf9ae83d6768e4e55
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-09-30 19:38:48 +0900

    refactor: 출력부분에서만 사용할 getter로 수정
    출력부분에서만 사용할 getter로 수정

--- src/main/java/race/ResultView.java
+++ src/main/java/race/ResultView.java
@@ -4,9 +4,18 @@ import java.util.List;
 
 public class ResultView {
 
+    private static final String EXPRESS_STRING = "-";
+
     public static void output(List<Car> cars) {
         for (Car car : cars) {
-            System.out.println(car.getCarPosition());
+            carForwardPrint(car);
+        }
+        System.out.println();
+    }
+
+    private static void carForwardPrint(Car car) {
+        for (int i = 0; i < car.getForwardResult(); i++) {
+            System.out.print(EXPRESS_STRING);
         }
         System.out.println();
     }
