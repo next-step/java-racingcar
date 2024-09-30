5e387750f75e52f1b3b547f11b099d681b3fe5b7
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-09-30 14:29:30 +0900

    fix: RacingCarGame 객체를 spy하여 랜덤하게 출력되는 decideForward 메소드를 true로만 반환되게 하여 테스트코드 수정

--- src/test/java/RacingCarTest.java
+++ src/test/java/RacingCarTest.java
@@ -1,8 +1,7 @@
 import car.RacingCar;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,11 +15,10 @@ public class RacingCarTest {
     }
 
     @DisplayName("입력값이 4이상이면 차가 전진, 4미만이면 정지한다.")
-    @ParameterizedTest
-    @CsvSource(value = {"1:1", "4:2"}, delimiter = ':')
-    void forwardTest(int input, int distance) {
-        car.forward(input);
-        assertThat(car.getDistance()).isEqualTo(distance);
+    @Test
+    void forwardTest() {
+        car.forward();
+        assertThat(car.getDistance()).isEqualTo(1);
     }
 
 }
