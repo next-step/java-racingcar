20dc2b20d5c4489f5f91c9e2c258014c0afdfd34
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-09-30 23:52:22 +0900

    feat: 객체 copy메소드

--- src/main/java/car/CarStrategy.java
+++ src/main/java/car/CarStrategy.java
@@ -4,8 +4,9 @@ public interface CarStrategy {
 
     //차량 전진 메소드
     void forward();
-    //차량 거리 출력 메소드
-    void result();
+
     int getDistance();
+
+    CarStrategy copy();
 
 }
--- src/main/java/car/RacingCar.java
+++ src/main/java/car/RacingCar.java
@@ -1,11 +1,17 @@
 package car;
 
-public class RacingCar implements CarStrategy{
+public class RacingCar implements CarStrategy {
 
     private final static int FORWARD_DISTANCE = 1; //전진하는 거리
-    private final static String DISTANCE_DISPLAY = "-";
 
     private int distance = 0; //현재 전진한 거리
+
+    public RacingCar() {
+    }
+
+    public RacingCar(int distance) {
+        this.distance = distance;
+    }
 
     //차를 전진한다.
     @Override
@@ -14,13 +20,14 @@ public class RacingCar implements CarStrategy{
     }
 
     @Override
-    public void result() {
-        System.out.println(DISTANCE_DISPLAY.repeat(distance));
-    }
-
-    @Override
     public int getDistance() {
         return distance;
+    }
+
+    //객체를 복사한다.
+    @Override
+    public CarStrategy copy() {
+        return new RacingCar(this.distance);
     }
 
 }
