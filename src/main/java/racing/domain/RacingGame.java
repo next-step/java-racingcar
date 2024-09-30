71b364c351d52e15a09afc78fd6de0271d567773
StoneHee99 <kiit0901@gmail.com>
2024-09-30 20:50:44 +0900

    test: 실패하는 테스트 생성

--- src/main/java/racing/domain/RacingGame.java
+++ src/main/java/racing/domain/RacingGame.java
@@ -1,4 +1,5 @@
 package racing.domain;
 
 public class RacingGame {
+    
 }
--- src/test/java/racing/domain/RacingGameTest.java
+++ src/test/java/racing/domain/RacingGameTest.java
@@ -1,5 +1,15 @@
 package racing.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RacingGameTest {
 
+    @DisplayName("생성자를 통해 자동차 대수와 전진 수를 잘 생성하는지")
+    @Test
+    void setUpTest() {
+        RacingGame newGame = RacingGame.setUp(1, 5);
+        assertThat(newGame.getRoundCount()).isEqualTo(1);
+        assertThat(newGame.getCarCount()).isEqualTo(5);
+    }
 }
