c90d49fc5db563672a9766d849a8eb6870638070
Hoseop.Kim <kimho314@gmail.com>
2024-09-30 20:23:21 +0900

    [feat] : 자동차 이름은 쉼표(,)를 기준으루 구분하는 로직

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -20,4 +20,9 @@ public class InputView {
         }
         return num;
     }
+
+    public static String[] getNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return SC.nextLine().split(",");
+    }
 }
--- src/main/java/RacingCarGameManagement.java
+++ src/main/java/RacingCarGameManagement.java
@@ -44,6 +44,10 @@ public class RacingCarGameManagement {
     }
 
     public static void main(String[] args) {
+        String[] split = InputView.getNames();
+        for (String name : split) {
+            System.out.println(name);
+        }
         int numberOfCars = InputView.getNumberOfCars();
         int numberOfGames = InputView.getNumberOfGames();
         RandomUtil randomUtil = new RandomUtil();
