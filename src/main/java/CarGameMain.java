04182c8d2cfc3873936d4203d076ed00155be5ad
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-09-30 14:28:13 +0900

    fix: main메소드 수정

--- src/main/java/CarGameMain.java
+++ src/main/java/CarGameMain.java
@@ -1,11 +1,28 @@
-import game.CarGame;
+import game.GameStrategy;
+import game.PreparedCarGame;
+import game.RacingCarGame;
+import uiview.InputView;
 
 public class CarGameMain {
 
+    private final static InputView inputView = new InputView();
+
     public static void main(String[] args) {
-        CarGame carGame = new CarGame();
-        carGame.readyRace();
+
+        PreparedCarGame carGame = new PreparedCarGame();
+
+        int carNum = inputView.inputCarNum();
+        int attemptNum = inputView.inputAttemptNum();
+
+        carGame.ready(racingCarGame(carNum, attemptNum));
         carGame.startRace();
+
+    }
+
+    public static GameStrategy racingCarGame(int carNum, int attemptNum) {
+        RacingCarGame carGame = new RacingCarGame();
+        carGame.readyRace(carNum, attemptNum);
+        return carGame;
     }
 
 }
