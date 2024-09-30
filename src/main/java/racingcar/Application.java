1457ba2ef46c5828d79645a8492c12e506b776d1
최수봉 <soobong.choi@meshkorea.net>
2024-09-30 17:13:22 +0900

    feat: 사용자로부터 경주에 필요한 값을 입력받는 기능 구현

---
+++ src/main/java/racingcar/Application.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+public class Application {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        int competitors = inputView.getCompetitors();
+        int forwardAttempts = inputView.getForwardAttempts();
+    }
+}
---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public int getCompetitors() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return scanner.nextInt();
+    }
+
+    public int getForwardAttempts() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
+    }
+}
