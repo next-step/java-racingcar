58d790c2230ad4dc05cc48161cae2379f08388d5
StoneHee99 <kiit0901@gmail.com>
2024-09-30 21:09:20 +0900

    test: 실패하는 테스트 생성

--- src/test/java/racing/domain/RacingGameTest.java
+++ src/test/java/racing/domain/RacingGameTest.java
@@ -14,4 +14,12 @@ class RacingGameTest {
         assertThat(newGame.getRoundCount()).isEqualTo(1);
         assertThat(newGame.getCars()).hasSize(5);
     }
+
+    @DisplayName("게임이 시작되면 라운드 수 만큼의 결과 객체를 잘 반환하는지")
+    @Test
+    void startTest() {
+        RacingGame newGame = RacingGame.setUp(1, 5);
+        List<GameResult> results = newGame.start();
+        assertThat(results).hasSize(1);
+    }
 }
