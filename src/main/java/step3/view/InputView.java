aefb264ca08a8cbece66b532d3c96187b042f325
hwan2-99 <thh1233123@gmail.com>
2024-09-30 22:25:33 +0900

    refactor: 하드코딩된 숫자 리터럴 문자 상수활용하여 선언

--- src/main/java/step3/view/InputView.java
+++ src/main/java/step3/view/InputView.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final int MIN_COUNT = 1;
     private static final String INPUT_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
-    private static final String MIN_COUNT_ERROR_MESSAGE = "최소 1이상의 숫자를 입력하셔야 합니다.";
+    private static final String MIN_COUNT_ERROR_MESSAGE = "최소 " + MIN_COUNT + "이상의 숫자를 입력하셔야 합니다.";
     private static final String INCORRECT_TYPE_ERROR_MESSAGE = "숫자만 입력가능합니다.";
-    private static final int MIN_COUNT = 1;
 
-    public InputView() {
+    private InputView() {
     }
 
     public static int getCarCount() {
