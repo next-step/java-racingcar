01bf0ad2adc42005d84433498124b185a5b57afd
BISohn27 <skylord418@gmail.com>
2024-09-30 23:53:15 +0900

    refactor : update RandomNumRaceRule
    refactor RandomNumRaceRule class not to create new Random class but to use static Random class and to set seed before using the class

--- src/main/java/racing/race/RandomNumRaceRule.java
+++ src/main/java/racing/race/RandomNumRaceRule.java
@@ -9,7 +9,10 @@ public class RandomNumRaceRule implements RaceRule {
     private final static int RANDOM_NUMBER_BOUND = 10;
     private final static int MIN_NUM_OF_FORWARD = 4;
 
+    private final static Random RANDOM = new Random();
+
     public boolean isForward() {
-        return new Random().nextInt(RANDOM_NUMBER_BOUND) >= MIN_NUM_OF_FORWARD;
+        RANDOM.setSeed(System.currentTimeMillis());
+        return RANDOM.nextInt(RANDOM_NUMBER_BOUND) >= MIN_NUM_OF_FORWARD;
     }
 }
