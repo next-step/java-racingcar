2350ca7d814c0fa0173068f2251ff556b7925970
jonghyeon <jonghyl@smilegate.net>
2024-09-30 19:28:20 +0900

    feat: 문자열에 대시(-)를 추가하는 함수 구현

--- src/main/java/com/warmup/step3/CarRacing.java
+++ src/main/java/com/warmup/step3/CarRacing.java
@@ -3,6 +3,13 @@ package com.warmup.step3;
 public class CarRacing {
 
     /**
+     * 문자열에 대시를 추가하는 함수
+     */
+    public static String addDash(String input) {
+        return input + "-";
+    }
+
+    /**
      * 전진 여부 판단
      */
     public static boolean isForward(int input) {
--- src/test/java/com/warmup/step3/CarRacingTest.java
+++ src/test/java/com/warmup/step3/CarRacingTest.java
@@ -1,11 +1,19 @@
 package com.warmup.step3;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CarRacingTest {
+    @Test
+    public void isEquals_문자열_뒤에_대쉬_추가() {
+        String expected = "--";
+
+        assertThat(CarRacing.addDash("-")).isEqualTo(expected);
+    }
+
     @ParameterizedTest
     @CsvSource(value = {"1:false", "3:false", "4:true", "5:true"}, delimiter = ':')
     public void isEquals_4이상의_숫자는_TRUE_그외의_숫자는_FALSE(int number, boolean expected) {
