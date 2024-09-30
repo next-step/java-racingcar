6930178160b2daf697516c35df3225f9d549b580
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-09-30 14:27:51 +0900

    feat: RacingCar뿐만아니라 CarStrategy를 상속받은 클래스 객체들을 출력할수있도록 수정

--- src/main/java/uiview/ResultView.java
+++ src/main/java/uiview/ResultView.java
@@ -1,15 +1,15 @@
 package uiview;
 
-import car.RacingCar;
+import car.CarStrategy;
 
 import java.util.List;
 
 public class ResultView {
 
     //현재 레이싱결과를 출력한다.
-    public void resultView(List<RacingCar> cars) {
-        for (RacingCar car : cars) {
-            car.result();
+    public void resultView(List<CarStrategy> cars) {
+        for (CarStrategy carStrategy : cars) {
+            carStrategy.result();
         }
         System.out.println();
     }
