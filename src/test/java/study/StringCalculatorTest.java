9823cca003c69b4f9f42d15fb16d237638f52bf8
kkksusu161 <kkksusu161@naver.com>
2024-09-30 13:30:59 +0900

    #step2 StringCalculator (#5666)
    Co-authored-by: kimsuyeondev <rhaehl@naver.com>

---
+++ src/test/java/study/StringCalculatorTest.java
@@ -0,0 +1,53 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class StringCalculatorTest {
+
+    @Test
+    void 음수() {
+        assertThatThrownBy(() -> {
+            StringCalculator.splitAndSum("-2,1:");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀_구분자_mismatch() {
+        assertThatThrownBy(() -> {
+            StringCalculator.splitAndSum("/;1;2;3");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    void 커스텀_구분자() {
+        assertThat(StringCalculator.splitAndSum("//;\n1;2;3")).isEqualTo(6);
+    }
+
+    @Test
+    void 빈값_또는_null() {
+        assertThat(StringCalculator.splitAndSum(null)).isEqualTo(0);
+        assertThat(StringCalculator.splitAndSum("")).isEqualTo(0);
+    }
+
+    @Test
+    void 콜론_구분자() {
+        int result = StringCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    void 컴마_구분자() {
+        assertThat(StringCalculator.splitAndSum("1,2")).isEqualTo(3);
+    }
+
+    @Test
+    void 숫자_하나() {
+        assertThat(StringCalculator.splitAndSum("0")).isEqualTo(0);
+    }
+
+
+}
