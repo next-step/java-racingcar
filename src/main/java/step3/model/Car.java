9574ff7cfcae07fe714de929ad76cfbe809a8c24
hwan2-99 <thh1233123@gmail.com>
2024-09-30 22:12:52 +0900

    refactor: 테스트만을 위한 메서드 삭제

--- src/main/java/step3/model/Car.java
+++ src/main/java/step3/model/Car.java
@@ -6,7 +6,7 @@ public class Car {
     private static final int INITIAL_COUNT = 0;
     private static final int MOVE_MIN_NUMBER = 4;
     private static final int RANDOM_MAX_NUMBER = 10;
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
 
     private int moveCount;
 
@@ -19,16 +19,12 @@ public class Car {
     }
 
     public void moveOrStop(){
-        move(getRandomValue());
-    }
-
-    public void move(int randomValue) {
-        if(randomValue >= MOVE_MIN_NUMBER){
+        if(isMove()){
             moveCount++;
         }
     }
 
-    private int getRandomValue(){
-        return random.nextInt(RANDOM_MAX_NUMBER);
+    private boolean isMove(){
+        return RANDOM.nextInt(RANDOM_MAX_NUMBER) >= MOVE_MIN_NUMBER;
     }
 }
--- src/test/java/step3/model/CarTest.java
+++ src/test/java/step3/model/CarTest.java
@@ -10,18 +10,4 @@ class CarTest {
         Car car = new Car();
         assertThat(car.getMoveCount()).isEqualTo(0);
     }
-
-    @Test
-    void 랜덤숫자가_4이상일경우_전진한다(){
-        Car car = new Car();
-        car.move(4);
-        assertThat(car.getMoveCount()).isEqualTo(1);
-    }
-
-    @Test
-    void 랜덤숫자가_4미만일경우_전진안한다(){
-        Car car = new Car();
-        car.move(3);
-        assertThat(car.getMoveCount()).isEqualTo(0);
-    }
 }
