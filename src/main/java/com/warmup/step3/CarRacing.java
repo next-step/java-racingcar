7c71ef5ef07a679388b1c401ec8cff28ae301a71
jonghyeon <jonghyl@smilegate.net>
2024-09-30 20:02:40 +0900

    feat: 자동차 대수와 시도활 횟수를 입력받는 메인 함수 구현

--- src/main/java/com/warmup/step3/CarRacing.java
+++ src/main/java/com/warmup/step3/CarRacing.java
@@ -1,8 +1,21 @@
 package com.warmup.step3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class CarRacing {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carNumber = scanner.nextInt();
+
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        int tryNumber = scanner.nextInt();
+
+        System.out.println("자동차 대수: " + carNumber + " 시도할 횟수: " + tryNumber);
+    }
 
     /**
      * 입력한 숫자만큼의 문자열 배열 반환
