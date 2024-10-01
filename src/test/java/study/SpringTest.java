5f73799d3bf8e5e8d6c72a5c413df38043da9678
maeng24 <maengindex@gmail.com>
2024-10-01 21:56:58 +0900

    step1 include feedback

--- src/test/java/study/SpringTest.java
+++ src/test/java/study/SpringTest.java
@@ -11,23 +11,20 @@ public class SpringTest {
     @Test
     void split() {
         String[] result = "1,2".split(",");
-//        assertThat(result).contains("1");
-        assertThat(result).containsExactly("1","2");
+        assertThat(result).containsExactly("1", "2");
     }
 
     @Test
     void subString() {
-        String result = "(1,2)".substring(1,4);
+        String result = "(1,2)".substring(1, 4);
         assertThat(result).contains("1,2");
     }
 
     @DisplayName("요구사항 3 - 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 Exception이 발생하는 부분에 대한 학습 테스트")
     @Test
     void charAt() {
-        String result = "abc";
-
         assertThatThrownBy(()->
-                result.charAt(result.length()+1))
+                "abc".charAt(4))
                 .isInstanceOf(IndexOutOfBoundsException.class)
                 .hasMessageContaining("index out of range");
     }
