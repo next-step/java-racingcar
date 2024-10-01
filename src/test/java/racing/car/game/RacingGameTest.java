6bd091dab20160a1134a6fa21115414ed87fbc6c
kimsuyeondev <rhaehl@naver.com>
2024-10-01 23:38:30 +0900

    #step3 피드백반영 - 주어진_횟수만큼_자동차에_랜덤값_넣기 검증 추가

--- src/test/java/racing/car/game/RacingGameTest.java
+++ src/test/java/racing/car/game/RacingGameTest.java
@@ -1,6 +1,9 @@
 package racing.car.game;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import static org.mockito.Mockito.*;
 
 
 public class RacingGameTest {
@@ -8,9 +11,13 @@ public class RacingGameTest {
     @Test
     void 주어진_횟수만큼_자동차에_랜덤값_넣기() {
         //given
-        int count = 4; //4회
-        int carCount = 3; //3대
-        RacingGame.countCarGoAndStop(count, carCount);
-    }
+        int count = 4;
+        int carCount = 3;
 
+        try(MockedStatic<RacingGame> staticRacingGame = mockStatic(RacingGame.class)) {
+            RacingGame.countCarGoAndStop(count, carCount);
+            //잘 호출되었는지 확인
+            staticRacingGame.verify(() -> RacingGame.countCarGoAndStop(count, carCount), times(1));
+        }
+    }
 }
