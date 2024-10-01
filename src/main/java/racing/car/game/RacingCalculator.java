cebb1bc1018ef19f68a837f9f36dab190b6b9efe
kimsuyeondev <rhaehl@naver.com>
2024-10-02 02:52:30 +0900

    #step3 객체지향 설계로 변경 후 - 0~9까지 랜덤값을 구하는 기능

--- src/main/java/racing/car/game/RacingCalculator.java
+++ src/main/java/racing/car/game/RacingCalculator.java
@@ -3,32 +3,11 @@ package racing.car.game;
 import racing.car.util.NumberUtil;
 
 public class RacingCalculator {
-
     private static final int MIN_BOUND = 0;
     private static final int MAX_BOUND = 9;
 
-    /**
-     * 값이 4 이상이면 1 4미만이면 0을 반환
-     * */
-    public static int goAndStop(int number) {
-        if (number < MIN_BOUND || number > MAX_BOUND) {
-            throw new IllegalArgumentException();
-        }
-
-        if (number >= 4) {
-            return 1;
-        }
-        return 0;
+    public static int getRandomNumber() {
+        return NumberUtil.randomInt(MIN_BOUND, MAX_BOUND);
     }
 
-    /**
-     * 랜덤 값을 자동차 배열에 넣는 기능
-     * */
-    public static int[] carToArrayRandomValues(int[] cars) {
-        for (int i = 0; i < cars.length; i++) {
-            int randomNumber = goAndStop(NumberUtil.randomInt(MIN_BOUND, MAX_BOUND));
-            cars[i] = cars[i] + randomNumber;
-        }
-        return cars;
-    }
 }
--- src/main/java/racing/car/util/NumberUtil.java
+++ src/main/java/racing/car/util/NumberUtil.java
@@ -3,9 +3,9 @@ package racing.car.util;
 import java.util.Random;
 
 public class NumberUtil {
+    private static  Random random = new Random();
 
     public static int randomInt(int min, int max) {
-        Random random = new Random();
         return random.nextInt((max - min) + 1) + min;
     }
 }
