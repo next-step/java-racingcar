fef5964d2aab4168a4b791c7382ebca2ddb91553
maeng24 <maengindex@gmail.com>
2024-10-01 14:58:57 +0900

    substring study test

--- src/test/java/study/SpringTest.java
+++ src/test/java/study/SpringTest.java
@@ -12,4 +12,10 @@ public class SpringTest {
 //        assertThat(result).contains("1");
         assertThat(result).containsExactly("1","2");
     }
+
+    @Test
+    void subString() {
+        String result = "(1,2)".substring(1,4);
+        assertThat(result).contains("1,2");
+    }
 }
