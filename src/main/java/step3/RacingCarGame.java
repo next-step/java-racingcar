3f93a0074ee97d509e6d1b3a468574907cb70864
ryumina-edu <fbalsk8897@gmail.com>
2024-10-01 21:16:53 +0900

    refactor: create InputView

--- src/main/java/step3/RacingCarGame.java
+++ src/main/java/step3/RacingCarGame.java
@@ -2,7 +2,6 @@ package step3;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class RacingCarGame {
     private final CarEngine carEngine;
@@ -15,19 +14,17 @@ public class RacingCarGame {
     public static void main(String[] args) {
         CarEngine carEngine = new RandomEngine();
         RacingCarGame racingcar = new RacingCarGame(carEngine);
-        racingcar.execute();
+
+        InputView inputView = new InputView();
+        inputView.receiveAndViewAndValidate();
+
+        racingcar.execute(inputView.getCarCount(), inputView.getRound());
     }
 
-    public void execute() {
-        Scanner scanner = new Scanner(System.in);
-        String car = scanner.nextLine();
-        String round = scanner.nextLine();
+    public void execute(int carCount, int round) {
+        Cars cars = new Cars(createCars(carCount));
 
-        InputValidator.validate(car, round);
-
-        Cars cars = new Cars(createCars(Integer.parseInt(car)));
-
-        race(cars, Integer.parseInt(round));
+        race(cars, round);
     }
 
     private List<Car> createCars(int car) {
@@ -41,7 +38,6 @@ public class RacingCarGame {
 
     private void race(Cars cars, int round) {
         PrintView printView = new PrintView();
-        printView.input(cars.getCarList().size(), round);
 
         printView.print("실행 결과");
         for (int i = 0; i < round; i++) {
