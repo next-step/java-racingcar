830d8e80d8b95e89fcbb460825eaf127c977a088
jara <jara0701@gmail.com>
2024-10-01 16:59:47 +0900

    feat ResultView.java: ResultView.java 구현
    레이싱 결과 출력을 담당하는 ResultView.java를 구현

---
+++ src/main/java/racingcar/ui/ResultView.java
@@ -0,0 +1,42 @@
+package racingcar.ui;
+
+import racingcar.dto.RacingCarStateDTO;
+import racingcar.dto.RacingCarStatesDTO;
+import racingcar.dto.RacingResultDTO;
+import racingcar.dto.RacingWrapResultDTO;
+
+public class ResultView {
+    private static String RESULT_PRINT_HEADER = "실행 결과";
+
+    public static void printRacingResult(RacingResultDTO result, int tryNumber, int carNumber) {
+        printHeader();
+        printWrapResults(result);
+    }
+
+    private static void printWrapResults(RacingResultDTO result) {
+        for (RacingWrapResultDTO wrapResult: result.wrapResults) {
+            RacingCarStatesDTO carStates = wrapResult.carStates;
+            printCarSates(carStates);
+            addBlankLine();
+        }
+    }
+
+    private static void printCarSates(RacingCarStatesDTO carStates) {
+        for(RacingCarStateDTO carState : carStates.catStates) {
+            printCarSate(carState);
+        }
+    }
+
+    private static void printCarSate(RacingCarStateDTO carState) {
+        String dashes = "-".repeat(carState.position);
+        System.out.println(dashes);
+    }
+
+    private static void printHeader() {
+        System.out.println(RESULT_PRINT_HEADER);
+    }
+
+    private static void addBlankLine() {
+        System.out.println();
+    }
+}
