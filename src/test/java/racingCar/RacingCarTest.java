3d5de124bc0ddbe2b75eb9a2014450bd7fe41074
skdev0619 <skdev0619@naver.com>
2024-10-01 21:32:21 +0900

    테스트 코드 보완

--- src/test/java/racingCar/RacingCarTest.java
+++ src/test/java/racingCar/RacingCarTest.java
@@ -1,6 +1,5 @@
 package racingCar;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,26 +9,46 @@ import static org.assertj.core.api.Assertions.*;
 
 class RacingCarTest {
 
-    @DisplayName("자동차는 이동한 만큼 값의 위치가 변한다")
+    @DisplayName("자동차의 상태 값이 4미만이면 전진할 수 없으므로 위치가 변하지 않는다")
     @Test
     void cannotMoveIfUnderFour() {
         //given
-        Car car = new Car(1);
+        Car car = new Car();
+        car.applyMoveCondition(1);
 
         //when
+        int currentPosition = car.getPosition();
         car.move();
         car.move();
 
         //then
-        assertThat(car.getPosition()).isEqualTo(2);
+        assertThat(car.getPosition()).isEqualTo(currentPosition);
     }
 
-    @DisplayName("자동차의 상태 값은 0-9사의의 숫자를 가지고 있다")
+    @DisplayName("자동차의 상태 값이 4이상이면 이동한 수만큼 위치가 증가한다")
+    @Test
+    void canMoveIfAtLeastFour(){
+        //given
+        Car car = new Car();
+        car.applyMoveCondition(4);
+
+        //when
+        int currentPosition = car.getPosition();
+        car.move();
+        car.move();
+
+        //then
+        assertThat(car.getPosition()).isEqualTo(currentPosition + 2);
+    }
+
+    @DisplayName("자동차의 전진 조건의 값은 0-9사의의 숫자를 가지고 있다")
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
-    void checkStateBetween0And9(int state){
+    void checkConditionRange(int moveCondition){
         //given
-        Car car = new Car(state);
+        Car car = new Car();
+        car.applyMoveCondition(moveCondition);
+
         //when, then
         assertThat(car.getMoveCondition()).isBetween(0, 9);
     }
