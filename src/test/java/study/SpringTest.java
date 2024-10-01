c46bdadb65b148eefd6fab361e1cadbcf78ccb5e
maeng24 <maengindex@gmail.com>
2024-10-01 15:15:54 +0900

    charAt IndexOutOfBoundsException study test

--- src/test/java/study/SpringTest.java
+++ src/test/java/study/SpringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SpringTest {
 
@@ -17,5 +19,16 @@ public class SpringTest {
     void subString() {
         String result = "(1,2)".substring(1,4);
         assertThat(result).contains("1,2");
+    }
+
+    @DisplayName("요구사항 3 - 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 Exception이 발생하는 부분에 대한 학습 테스트")
+    @Test
+    void charAt() {
+        String result = "abc";
+
+        assertThatThrownBy(()->
+                result.charAt(result.length()+1))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("index out of range");
     }
 }
