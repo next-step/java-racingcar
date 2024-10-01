29ecc350544aa8c2915a65647fa1ec6ca7076c27
SeongJunWi <dnltjdwns235@gmail.com>
2024-10-01 13:43:33 +0900

    Revert "2단계 - 문자열 덧셈 계산기"
    This reverts commit 3d9de5f61ee59694bdf27c2d7406d2ebdb135dbb.

--- src/main/java/step2/StringAddCalculator.java
+++ src/main/java/step2/StringAddCalculator.java
@@ -7,24 +7,32 @@ public final class StringAddCalculator {
 	private static final String CUSTOM_SEPERATOR_REGEX = "//(.*)\\n(.*)";
 	private static final String DEFAULT_SEPERATOR = ",|:";
 	private static final Pattern CUSTOM_SEPERATOR_PATTERN = Pattern.compile(CUSTOM_SEPERATOR_REGEX);
-	private static final int CUSTOM_SEPERATOR_GROUP_INDEX = 1;
-	private static final int INT_LIST_GROUP_INDEX = 2;
+	private static final int CUSTOM_SEPERATOR_GROUP = 1;
+	private static final int INT_LIST_GROUP = 2;
 
 	public static int calculate(String text) {
 		if (isEmptyVal(text)) {
 			return 0;
 		}
+		String seperator = getSeperator(text);
+		String result = getResultText(text);
+		return parseAndSum(result.split(seperator));
+	}
 
+	private static String getResultText(String text) {
 		Matcher matcher = CUSTOM_SEPERATOR_PATTERN.matcher(text);
-		String result = text;
-		String separator = DEFAULT_SEPERATOR;
-
 		if (matcher.find()) {
-			separator = DEFAULT_SEPERATOR + "|" + matcher.group(CUSTOM_SEPERATOR_GROUP_INDEX);
-			result = matcher.group(INT_LIST_GROUP_INDEX);
+			return matcher.group(INT_LIST_GROUP);
 		}
+		return text;
+	}
 
-		return parseAndSum(result.split(separator));
+	private static String getSeperator(String text) {
+		Matcher matcher = CUSTOM_SEPERATOR_PATTERN.matcher(text);
+		if (matcher.find()) {
+			return DEFAULT_SEPERATOR + "|" + matcher.group(CUSTOM_SEPERATOR_GROUP);
+		}
+		return DEFAULT_SEPERATOR;
 	}
 
 	private static boolean isEmptyVal(String text) {
