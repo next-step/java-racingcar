eb254d3d60c278aa89dc130b09e81ef5dcd97fec
배영현 <byh2949@gmail.com>
2024-10-01 13:05:18 +0900

    refactor : InputView, RacingDisplay 수정
    InputView 메서드 static으로 변경
    RacingDisplay InputVIew, ResultView 인스턴스 제거

--- src/main/java/carracing/InputView.java
+++ src/main/java/carracing/InputView.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public static final Scanner scanner = new Scanner(System.in);
+    public static final Scanner SCANNER = new Scanner(System.in);
 
-    public Integer inputCarView() {
+    public static Integer inputCarView() {
         System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 
-    public Integer inputMoveView() {
+    public static Integer inputMoveView() {
         System.out.println("시도할 회수는 몇 회 인가요?");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 }
--- src/main/java/carracing/RacingDisplay.java
+++ src/main/java/carracing/RacingDisplay.java
@@ -1,14 +1,12 @@
 package carracing;
 
 public class RacingDisplay {
-    private static final InputView inputView = new InputView();
-    private static final ResultView resultView = new ResultView();
 
     private static final RacingCar RACING_CAR = new RacingCar();
 
     public static void main(String[] args) {
-        int carNums = inputView.inputCarView();
-        int moveNums = inputView.inputMoveView();
+        int carNums = InputView.inputCarView();
+        int moveNums = InputView.inputMoveView();
 
         ResultView.lineChange();
         ResultView.resultPrintResultComment();
