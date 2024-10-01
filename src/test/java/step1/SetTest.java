68acf1a4ea4bd4ee5d217f4f2e5140ed32aeef8f
Hyeyoung Kim <sunzero0116@gmail.com>
2024-10-01 15:18:54 +0900

    Step1 학습 테스트 실습 (#5698)
    * test : String 클래스에 대한 학습 테스트
    
    * test : Set 클래스에 대한 학습 테스트
    
    * test : String 클래스에 대한 학습 테스트

---
+++ src/test/java/step1/SetTest.java
@@ -0,0 +1,45 @@
+package step1;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set의 크기를 확인")
+    void checkSize() {
+        assertThat(numbers).hasSize(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("1, 2, 3의 값이 존재하는지 확인")
+    void checkValues(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("1, 2, 3의 값이 존재하고 나머지는 존재하지 않은지 확인")
+    void checkValuesWithBoolean(int value, boolean expected) {
+        assertThat(numbers.contains(value)).isEqualTo(expected);
+    }
+}
