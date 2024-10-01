545803f636a0fb0ab3b2a9799af8fb10d2ddc427
Slowth-KIM <rt.slowth@gmail.com>
2024-10-01 19:52:31 +0900

    test : 자동차 이동 로직 테스트 추가 완료

---
+++ src/test/java/racinggame/RacingCarMovementTest.java
@@ -0,0 +1,72 @@
+package racinggame;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racinggame.racingcar.RacingCars;
+import racinggame.racingcar.RandomMoveStrategy;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RacingCarMovementTest {
+
+    private RacingCars racingCars;
+
+    @BeforeEach
+    void setUp() {
+        racingCars = new RacingCars();
+    }
+
+    @DisplayName("자동차 정지 테스트")
+    @Test
+    void stopRacingCar() {
+        racingCars.addCar(() -> false);
+        racingCars.moveAll();
+
+        List<Integer> positions = racingCars.getCurrentPositionsRepresentation();
+        assertThat(positions).containsExactly(0);
+    }
+
+    @DisplayName("자동차 전진 테스트")
+    @Test
+    void moveRacingCar() {
+        racingCars.addCar(() -> true);
+        racingCars.moveAll();
+
+        List<Integer> positions = racingCars.getCurrentPositionsRepresentation();
+        assertThat(positions).containsExactly(1);
+    }
+
+    @DisplayName("다중 이동 테스트")
+    @Test
+    void moveMultipleRacingCar() {
+        racingCars.addCar(() -> true);
+
+        for (int i = 0; i < 5; i++) {
+            racingCars.moveAll();
+        }
+
+        List<Integer> positions = racingCars.getCurrentPositionsRepresentation();
+        assertThat(positions).containsExactly(5);
+    }
+
+    @DisplayName("다중 자동차 테스트")
+    @Test
+    void testMultipleCars() {
+        racingCars.addCar(() -> true);  // 항상 움직임
+        racingCars.addCar(() -> false); // 항상 멈춤
+        racingCars.addCar(new RandomMoveStrategy()); // 실제 랜덤 전략 사용
+
+        for (int i = 0; i < 4; i++) {
+            racingCars.moveAll();
+        }
+
+        List<Integer> positions = racingCars.getCurrentPositionsRepresentation();
+        System.out.println("positions.toString() = " + positions.toString());
+        assertThat(positions.get(0)).isEqualTo(4);
+        assertThat(positions.get(1)).isEqualTo(0);
+        assertThat(positions.get(2)).isBetween(0, 4);
+    }
+}
