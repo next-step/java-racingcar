08c11d744a530e2bffe72613e7929daddc2cc205
ryumina-edu <fbalsk8897@gmail.com>
2024-10-01 19:59:44 +0900

    refactor: extract static constants

--- src/main/java/step3/RandomEngine.java
+++ src/main/java/step3/RandomEngine.java
@@ -3,10 +3,10 @@ package step3;
 import java.util.Random;
 
 public class RandomEngine implements CarEngine {
+    private static final Random random = new Random();
     private static final int RANDOM_BOUND = 10;
 
     public int getPower() {
-        Random random = new Random();
         return random.nextInt(RANDOM_BOUND);
     }
 }
