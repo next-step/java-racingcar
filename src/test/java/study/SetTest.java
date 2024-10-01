6813e000097ad055ba6851ae68ca14641eddc7f3
maeng24 <maengindex@gmail.com>
2024-10-01 15:36:54 +0900

    Set contains study test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -27,5 +29,12 @@ public class SetTest {
     @Test
     void sizeTest() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("요구사항 2 - contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트, JUnit의 ParameterizedTest를 활용해 중복 테스트 코드 제거")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
