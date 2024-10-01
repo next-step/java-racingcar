a684eef89f63ba03dd90f426dac18d0a97286ad2
StoneHee99 <kiit0901@gmail.com>
2024-10-02 01:18:17 +0900

    feat: 최종 우승자 출력 기능

--- src/main/java/racing/ui/OutputView.java
+++ src/main/java/racing/ui/OutputView.java
@@ -1,5 +1,6 @@
 package racing.ui;
 
+import java.util.List;
 import racing.model.CarInfo;
 import racing.model.RoundResult;
 import racing.model.collection.CarInfoList;
@@ -8,6 +9,7 @@ import racing.model.collection.GameResult;
 public class OutputView {
 
     private static final String OUTPUT_START_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE_SUFFIX = "가 최종 우승했습니다.";
 
     public OutputView() {
         throw new UnsupportedOperationException("유틸형 클래스는 생성할 수 없습니다");
@@ -20,6 +22,7 @@ public class OutputView {
             printResult(result);
             printBlankLine();
         }
+        printWinners(gameResult.getWinners());
     }
 
     private static void printResult(RoundResult result) {
@@ -32,6 +35,11 @@ public class OutputView {
     private static String convertCarInfoToGage(CarInfo info) {
         return info.getName() + " : "
                 + "-".repeat(Math.max(0, info.getPosition()));
+    }
+
+    private static void printWinners(List<String> winners) {
+        String winnerNames = String.join(", ", winners);
+        System.out.println(winnerNames + WINNER_MESSAGE_SUFFIX);
     }
 
     private static void printBlankLine() {
