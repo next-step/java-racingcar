436e5f7928fb0ceb44ea2b3cdddc0b58d84f6710
skdev0619 <skdev0619@naver.com>
2024-10-02 16:30:38 +0900

    차의 이름으로 레이싱 카 리스트 만들도록 구현

--- src/main/java/racingCar/step4/RacingCarGame.java
+++ src/main/java/racingCar/step4/RacingCarGame.java
@@ -3,10 +3,33 @@ package racingCar.step4;
 import racingCar.step3.ResultView;
 
 public class RacingCarGame {
+
+    private static final String CAR_NAME_SEPARATOR = ",";
+
     public void gameStart(){
         String carNames = InputView.getCarNamesInputFromUser();
         int racingCount = InputView.getRacingCountInputFromUser();
 
         ResultView.printResultMessage();
+        startRace(racingCount, createRacingCars(carNames));
+    }
+
+    private Car[] createRacingCars(String carNames) {
+        return createCarsFromNames(splitCarName(carNames));
+    }
+
+    private static Car[] createCarsFromNames(String[] carNames) {
+        Car[] cars = new Car[carNames.length];
+        for (int i = 0; i < carNames.length; i++) {
+            cars[i] = new Car(carNames[i]);
+        }
+        return cars;
+    }
+
+    private static String[] splitCarName(String carNames) {
+        return carNames.split(CAR_NAME_SEPARATOR);
+    }
+
+    private void startRace(int racingCount, Car[] cars) {
     }
 }
