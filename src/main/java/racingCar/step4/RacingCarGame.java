63e36f37b9206b4e1051907edeec5e7aba6f65eb
skdev0619 <skdev0619@naver.com>
2024-10-02 15:50:54 +0900

    실행결과 메세지 출력하도록 구현

--- src/main/java/racingCar/step4/RacingCarGame.java
+++ src/main/java/racingCar/step4/RacingCarGame.java
@@ -1,12 +1,12 @@
 package racingCar.step4;
 
+import racingCar.step3.ResultView;
+
 public class RacingCarGame {
     public void gameStart(){
         String carNames = InputView.getCarNamesInputFromUser();
         int racingCount = InputView.getRacingCountInputFromUser();
 
-        System.out.format("차 이름 : %s", carNames);
-        System.out.format("레이싱 횟수 : %d", racingCount);
-
+        ResultView.printResultMessage();
     }
 }
---
+++ src/main/java/racingCar/step4/ResultView.java
@@ -0,0 +1,10 @@
+package racingCar.step4;
+
+public class ResultView {
+    private static final String RESULT_MESSAGE = "실행결과";
+    private static final String STATE_PRINT_FORMAT = "-";
+
+    public static void printResultMessage(){
+        System.out.println(RESULT_MESSAGE);
+    }
+}
