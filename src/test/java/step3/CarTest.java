8fb356f512e82335ee07835798fec94f7d4b3100
Ryumina-edu <fbalsk8897@gmail.com>
2024-10-02 22:57:41 +0900

    test: step4 - car test

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -9,9 +9,9 @@ public class CarTest {
 
     @Test
     @DisplayName("자동차 생성시 초기 위치 0을 반환한다.")
-    void 자동차_생성() {
-        Car car1 = new Car();
-        Car car2 = new Car();
+    void 자동차_생성_초기위치() {
+        Car car1 = new Car("mina");
+        Car car2 = new Car("susu");
 
         assertThat(car1.getPosition()).isEqualTo(0);
         assertThat(car2.getPosition()).isEqualTo(0);
@@ -20,7 +20,7 @@ public class CarTest {
     @Test
     @DisplayName("4이상의 숫자가 주어질 경우 자동차의 위치를 +1 하여 반영한다.")
     void 자동차_전진_4이상() {
-        Car car1 = new Car();
+        Car car1 = new Car("mina");
         assertThat(car1.getPosition()).isEqualTo(0);
 
         CarEngine carEngine = new TestEngine(4);
@@ -31,11 +31,21 @@ public class CarTest {
     @Test
     @DisplayName("4미만의 숫자가 주어질 경우 자동차의 위치는 변함이 없어야 한다.")
     void 자동차_멈춤_4미만() {
-        Car car1 = new Car();
+        Car car1 = new Car("mina");
         assertThat(car1.getPosition()).isEqualTo(0);
 
         CarEngine carEngine = new TestEngine(3);
         car1.move(carEngine);
         assertThat(car1.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차 생성시 자동차 이름은 mina, susu를 반환해야 한다.")
+    void 자동차_생성_이름() {
+        Car car1 = new Car("mina");
+        Car car2 = new Car("susu");
+
+        assertThat(car1.getName()).isEqualTo("mina");
+        assertThat(car2.getName()).isEqualTo("susu");
     }
 }
