9648c3926149d4871d796d8449d5d6de2a877775
jeonghyeonkwon <givejeong2468@gmail.com>
2024-10-02 16:40:30 +0900

    refactor : 구현 완료 된 것 리팩토링
    - 구분자 상수화
    - split, 이름 검증 메소드 분리

--- src/main/java/javaracingcarwinner/controller/JavaRacingCar.java
+++ src/main/java/javaracingcarwinner/controller/JavaRacingCar.java
@@ -8,16 +8,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class JavaRacingCar {
+    private static final String SPLIT_DELIMITER = ",";
     private final List<RacingCar> cars = new ArrayList<>();
 
     public JavaRacingCar(String text) throws RuntimeException {
-        List<String> names = Arrays.stream(text.split(",")).collect(Collectors.toList());
+        List<String> names = split(text);
 
         for (String name : names) {
-            if (name.length() > 5) throw new IllegalArgumentException("5자 이상 입력");
+            validateName(name);
             cars.add(new RacingCar(name));
         }
 
+    }
+
+    private List<String> split(String text) {
+        return Arrays.stream(text.split(SPLIT_DELIMITER)).collect(Collectors.toList());
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("5자 이상 입력");
+        }
     }
 
     public List<RacingCar> cars() {
