cd24c86e37e3e79e38eea0685ade27018c5af8c3
이윤선 <yoonseon3@gmail.com>
2024-10-02 16:12:10 +0900

    refactor: Car 매직넘버 DEFAULT_POSITION로 추춡

--- src/main/java/racinggame/domain/Car.java
+++ src/main/java/racinggame/domain/Car.java
@@ -2,14 +2,15 @@ package racinggame.domain;
 
 public class Car {
     public static final int MINIMUM_MOVE_NUMBER = 4;
+    public static final int DEFAULT_POSITION = 0;
     private int position;
 
-    private Car() {
-        this.position = 0;
+    private Car(int position) {
+        this.position = position;
     }
 
     public static Car create() {
-        return new Car();
+        return new Car(DEFAULT_POSITION);
     }
 
     public int getPosition() {
