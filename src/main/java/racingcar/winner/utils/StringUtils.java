fa18102c20fb77ec713a96c8ab867ee0d8268d5b
Joonkb <byeongjunkim@naver.com>
2024-10-03 00:43:24 +0900

    [feat] 반복횟수만큼 문자 반복하는 문자열 반환하는 기능 구현 및 테스트

--- src/main/java/racingcar/winner/utils/StringUtils.java
+++ src/main/java/racingcar/winner/utils/StringUtils.java
@@ -4,6 +4,9 @@ public class StringUtils {
 
     private static final int MAX_NAME_LENGTH = 5;
 
+    private StringUtils() {
+    }
+
     public static String[] split(String input) {
         String[] values = input.split(",");
         validateInputValues(values);
@@ -20,5 +23,12 @@ public class StringUtils {
         if (value.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
+    }
+    public static String repeatCharacter(Character character, int count) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            result.append(character);
+        }
+        return result.toString();
     }
 }
--- src/test/java/racingcar/winner/utils/StringUtilsTest.java
+++ src/test/java/racingcar/winner/utils/StringUtilsTest.java
@@ -5,21 +5,28 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static racingcar.winner.utils.StringUtils.split;
 
 class StringUtilsTest {
 
     @Test
     @DisplayName("자동차 이름을 쉼표로 분리하여 배열을 반환한다.")
     void split_쉼표() {
-        String[] names = StringUtils.split("pobi,crong,honux");
+        String[] names = split("pobi,crong,honux");
         assertThat(names).containsExactly("pobi", "crong", "honux");
     }
 
     @Test
     @DisplayName("자동차 이름이 5글자 초과시 IllegalArgumentException를 반환한다.")
     void split_쉼표_5글자초과() {
-        assertThatThrownBy(() -> StringUtils.split("Byeongjoon,pobi"))
+        assertThatThrownBy(() -> split("Byeongjoon,pobi"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 5자를 초과할 수 없습니다.");
+    }
+
+    @Test
+    void 문자_반복() {
+        String result = StringUtils.repeatCharacter('-', 5);
+        assertThat(result).isEqualTo("-----");
     }
 }
