7a9436828e51d960247c154d480dfd43c5b338bf
jeonghyeonkwon <givejeong2468@gmail.com>
2024-10-02 18:08:50 +0900

    refactor: util 파일 2depth 수정

--- src/main/java/javaracingcarwinner/util/RacingUtil.java
+++ src/main/java/javaracingcarwinner/util/RacingUtil.java
@@ -9,22 +9,41 @@ public class RacingUtil {
 
     public static List<RacingCar> whoIsWinners(List<RacingCar> cars) {
         final List<RacingCar> winners = new ArrayList<>();
-        int maximumLocation = 0;
+
+        final int winnerLocation = winnerLocation(cars);
 
         for (RacingCar car : cars) {
-            int location = car.location();
-            if (maximumLocation < location) {
-                maximumLocation = location;
-            }
-        }
-
-        for (RacingCar car : cars) {
-            if (car.location() == maximumLocation) {
-                winners.add(car);
-            }
+            addWinner(winners, winnerLocation, car);
         }
 
         return winners;
+    }
+
+    private static void addWinner(List<RacingCar> winners, int winnerLocation, RacingCar car) {
+        if (isWinner(winnerLocation, car.location())) {
+            winners.add(car);
+        }
+    }
+
+    private static boolean isWinner(int winnerLocation, int location) {
+        return winnerLocation == location;
+    }
+
+    private static int winnerLocation(List<RacingCar> cars) {
+        int maximumLocation = 0;
+
+        for (RacingCar car : cars) {
+            maximumLocation = updateWinnerLocation(maximumLocation, car.location());
+        }
+
+        return maximumLocation;
+    }
+
+    private static int updateWinnerLocation(int maximumLocation, int location) {
+        if (maximumLocation < location) {
+            return location;
+        }
+        return maximumLocation;
     }
 
 }
