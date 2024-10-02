cdc76bb06ed9cad06758337febd25bb4234687f0
Joonkb <byeongjunkim@naver.com>
2024-10-02 23:33:16 +0900

    [feat] 자동차 배열을 반환하는 기능 구현 및 테스트 - 아래 커밋 최신화

--- src/main/java/racingcar/winner/controller/GameController.java
+++ src/main/java/racingcar/winner/controller/GameController.java
@@ -1,2 +1,44 @@
-package racingcar.winner.controller;public class GameController {
+package racingcar.winner.controller;
+
+import racingcar.winner.domain.Car;
+import racingcar.winner.utils.NumberGenerator;
+import racingcar.winner.utils.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameController {
+
+    private final List<Car> cars;
+    private int numberOfTries;
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public GameController(String names, int numberOfTries) {
+        this.cars = createCars(names);
+        this.numberOfTries = numberOfTries;
+    }
+
+    private List<Car> createCars(String names) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : StringUtils.split(names)) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+    public void start() {
+        for (int round = 1; round < numberOfTries; ++round) {
+            playRound();
+        }
+    }
+
+    private void playRound() {
+        for (Car car : cars) {
+            int randomNumber = NumberGenerator.generateRandomNumber();
+            car.moveForward(randomNumber);
+        }
+    }
 }
---
+++ src/main/java/racingcar/winner/domain/GameResult.java
@@ -0,0 +1,2 @@
+package racingcar.winner.domain;public class GameResult {
+}
