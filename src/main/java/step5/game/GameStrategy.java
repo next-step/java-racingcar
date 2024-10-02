d4130ce88123760448308fd38eef686a632b3488
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-02 22:09:56 +0900

    fix: 예외처리, 게임상황여부등 기능들을 RacingCarGame으로 의임

--- src/main/java/step5/game/GameStrategy.java
+++ src/main/java/step5/game/GameStrategy.java
@@ -9,15 +9,13 @@ public interface GameStrategy {
     //경기를 준비한다.
     void readyRace(String carName, int attemptNum);
 
-    //자동차를 가져온다.
-    List<CarStrategy> getRacingCars();
-
-    //전진 시도횟수를 가져온다.
-    int getAttemptNum();
+    void startRace();
 
     //자동차 전진을 1회 시도한다.
     void attemptForwardCar();
 
-    List<String> getWinners();
+    List<String> winnerRace();
+
+    List<List<CarStrategy>>  getRaceSituation();
 
 }
--- src/main/java/step5/game/RacingCarGame.java
+++ src/main/java/step5/game/RacingCarGame.java
@@ -1,14 +1,14 @@
 package step5.game;
 
-import step4.car.CarStrategy;
-import step4.car.RacingCar;
-import step4.game.GameStrategy;
-import step4.message.ExceptionMessage;
-import step4.util.RandomUtil;
-import step4.util.StringUtil;
+import step5.car.CarStrategy;
+import step5.car.RacingCar;
+import step5.message.ExceptionMessage;
+import step5.util.RandomUtil;
+import step5.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCarGame implements GameStrategy {
 
@@ -16,9 +16,10 @@ public class RacingCarGame implements GameStrategy {
     private final static int miniMumCarNum = 2; //레이싱 시작을 위한 최소 자동차 수
     private final static int miniMumAttemptNum = 1; //레이싱 시작을 위한 최소 시도 횟수
 
+    private final List<List<CarStrategy>> raceSituation = new ArrayList<>();
+
     private List<CarStrategy> cars = new ArrayList<>();
     private int attemptNum;
-    private int topDistance = 0;
 
     public RacingCarGame() {
     }
@@ -27,24 +28,21 @@ public class RacingCarGame implements GameStrategy {
         this.cars = cars;
     }
 
+    @Override
+    public void startRace() {
+        checkRestart();
+        for (int i = 0; i < attemptNum; i++) {
+            attemptForwardCar();
+            raceSituation.add(copyCarStrategy());
+        }
+    }
+
     //경기를 준비한다.
     @Override
     public void readyRace(String carName, int attemptNum) {
         this.attemptNum = attemptNum;
         createCar(carName);
         checkMinimumAttempt();
-    }
-
-    //자동자를 조회한다.
-    @Override
-    public List<CarStrategy> getRacingCars() {
-        return this.cars;
-    }
-
-    //시도 횟수를 조회한다.
-    @Override
-    public int getAttemptNum() {
-        return this.attemptNum;
     }
 
     //자동차들을 각각 전진 1회 시도한다.
@@ -57,18 +55,27 @@ public class RacingCarGame implements GameStrategy {
 
     //우승자의 이름을 가져온다.
     @Override
-    public List<String> getWinners() {
-        List<String> winners = new ArrayList<>();
-        for (CarStrategy car : cars) {
-            getTopDistanceCarName(car, winners);
-        }
-        return winners;
+    public List<String> winnerRace() {
+        checkStart();
+        return getTopDistanceCarName(getTopDistance());
     }
 
+    @Override
+    public List<List<CarStrategy>> getRaceSituation() {
+        return raceSituation;
+    }
 
     //차를 전진시킬지 결정한다.
     public boolean decideForward() {
         return RandomUtil.getRandomValuelessThenTen() >= FORWARD_STANDARD;
+    }
+
+    public List<CarStrategy> getCars() {
+        return cars;
+    }
+
+    public int getAttemptNum() {
+        return attemptNum;
     }
 
     //차를 전진시킨다.
@@ -87,15 +94,24 @@ public class RacingCarGame implements GameStrategy {
         }
     }
 
-    //최고거리인 자동차이름을 세팅한다.
-    private void getTopDistanceCarName(CarStrategy car, List<String> winners) {
-        if (this.topDistance < car.getDistance()) {
-            winners.clear();
-            this.topDistance = car.getDistance();
-            winners.add(car.getCarName());
-        } else if (this.topDistance == car.getDistance()) {
-            winners.add(car.getCarName());
+    //자동차의 최고 거리를 구한다.
+    private int getTopDistance() {
+        int topDistance = 0;
+        for (CarStrategy car : cars) {
+            topDistance = Math.max(topDistance, car.getDistance());
         }
+        return topDistance;
+    }
+
+    //최고거리인 자동차이름을 세팅한다.
+    private List<String> getTopDistanceCarName(int topDistance) {
+        List<String> winner = new ArrayList<>();
+        for (CarStrategy car : cars) {
+            if(car.isWinner(topDistance)) {
+                winner.add(car.getCarName());
+            }
+        }
+        return winner;
     }
 
     //레이싱에 참가하는 자동차가 1대 이하인지 체크
@@ -110,4 +126,24 @@ public class RacingCarGame implements GameStrategy {
             throw new IllegalArgumentException(ExceptionMessage.MINIMUN_ATTEMPT_EXCEPTION.message());
     }
 
+    //레이싱 재시작하는지를 체크한다.
+    private void checkRestart() {
+        if (!this.raceSituation.isEmpty())
+            throw new IllegalArgumentException(ExceptionMessage.GAME_ALREADY_START.message());
+    }
+
+    //시작을 했는지 안했는지 체크한다.
+    protected void checkStart() {
+        if (this.raceSituation.isEmpty())
+            throw new IllegalArgumentException(ExceptionMessage.GAME_NOT_START.message());
+    }
+
+    //CarStrategy 객체를 새로운주소로 copy한다.
+    private List<CarStrategy> copyCarStrategy() {
+        return this.cars.stream()
+                .map(CarStrategy::copy)
+                .collect(Collectors.toList());
+    }
+
 }
+
