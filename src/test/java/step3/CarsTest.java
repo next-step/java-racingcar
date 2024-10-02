2ade01667d9fd64c42d9ca753768565e6a38ce0b
Ryumina-edu <fbalsk8897@gmail.com>
2024-10-02 22:55:57 +0900

    test: step4 - cars test

--- src/test/java/step3/CarsTest.java
+++ src/test/java/step3/CarsTest.java
@@ -13,9 +13,9 @@ public class CarsTest {
     @Test
     @DisplayName("4이상의 숫자가 주어졌을 때 모든 자동차의 위치는 1이어야 한다.")
     void 자동차_이동_4이상_숫자() {
-        Car car1 = new Car();
-        Car car2 = new Car();
-        Car car3 = new Car();
+        Car car1 = new Car("mina");
+        Car car2 = new Car("susu");
+        Car car3 = new Car("jina");
 
         List<Car> carList = new ArrayList<>();
         carList.add(car1);
@@ -36,9 +36,9 @@ public class CarsTest {
     @Test
     @DisplayName("4미만의 숫자가 주어졌을 때 모든 자동차의 위치는 0이어야 한다.")
     void 자동차_이동_4미만_숫자() {
-        Car car1 = new Car();
-        Car car2 = new Car();
-        Car car3 = new Car();
+        Car car1 = new Car("mina");
+        Car car2 = new Car("susu");
+        Car car3 = new Car("jina");
 
         List<Car> carList = new ArrayList<>();
         carList.add(car1);
@@ -54,5 +54,26 @@ public class CarsTest {
         assertThat(cars.getCarList().get(0).getPosition()).isEqualTo(0);
         assertThat(cars.getCarList().get(1).getPosition()).isEqualTo(0);
         assertThat(cars.getCarList().get(2).getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("모든 자동차의 위치가 동일할 때, winner는 3명이어야 한다.")
+    void winner() {
+        Car car1 = new Car("mina");
+        Car car2 = new Car("susu");
+        Car car3 = new Car("jina");
+
+        List<Car> carList = new ArrayList<>();
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+
+        Cars cars = new Cars(carList);
+
+        CarEngine carEngine = new TestEngine(4);
+
+        cars.moveAll(carEngine);
+
+        assertThat(cars.getWinner().size()).isEqualTo(3);
     }
 }
