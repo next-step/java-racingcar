6f3fe7e4142a2cfbff7d285312bc0385a09a3a85
hoya <sv05159@naver.com>
2024-10-02 10:47:11 +0900

    refactor: extract magic number

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,13 +1,17 @@
 package racingcar;
 
 public class Car {
+    public static final int MIN_VALUE = 0;
+    public static final int MAX_VALUE = 9;
+    public static final int MOVABLE_VALUE = 4;
+
     private int moveCount;
 
-    public void move(int random) {
-        if (random < 0 || random > 9) {
-            throw new IllegalArgumentException("Only accepts integers between 0 and 9: " + random);
+    public void move(int value) {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new IllegalArgumentException("Only accepts integers between 0 and 9: " + value);
         }
-        if (random >= 4) moveCount++;
+        if (value >= MOVABLE_VALUE) moveCount++;
     }
 
     public int getMoveCount() {
