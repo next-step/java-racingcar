8642590200139ace57da6a395dc5758993b2c547
이윤선 <yoonseon3@gmail.com>
2024-10-02 18:32:44 +0900

    chore: 불필요 상수 제거

--- src/main/java/racinggame/ui/InputView.java
+++ src/main/java/racinggame/ui/InputView.java
@@ -9,7 +9,6 @@ public class InputView {
     private static final String CARS_NAME_QUESTION = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String TRY_COUNT_QUESTION = "시도할 회수는 몇 회 인가요?";
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String DELIMITER = ",";
 
     private InputView() {
         throw new IllegalStateException("Utility class");
@@ -18,7 +17,7 @@ public class InputView {
     public static List<String> readCarNames() {
         System.out.println(CARS_NAME_QUESTION);
         String carNames = scanner.nextLine();
-        return arrayToList(carNames.split(DELIMITER));
+        return arrayToList(carNames.split(","));
     }
 
     public static int readTryCount() {
