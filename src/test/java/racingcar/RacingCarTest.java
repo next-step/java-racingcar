3c62003a20182d6f683b4b7b7c9b1c560f148fdb
heechul <khc4190@naver.com>
2024-10-02 19:17:06 +0900

    feat: 테스트에 싱글톤 제거
    - 테스트 싱글톤 제거

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -10,35 +10,39 @@ public class RacingCarTest {
     @Test
     @DisplayName("라운드가 끝나면 매치가 줄어들고, 0 이하면 실행하지 않는다.")
     void 매치_라운드_감소() {
-        RacingCar.getInstance().setMatch(5);
-        RacingCar.getInstance().startMatch();
-        assertThat(RacingCar.getInstance().getMatch()).isEqualTo(4);
-        RacingCar.getInstance().setMatch(0);
-        RacingCar.getInstance().startMatch();
-        assertThat(RacingCar.getInstance().getMatch()).isEqualTo(0);
+        RacingCar racingCar = new RacingCar();
+        racingCar.setMatch(5);
+        racingCar.startMatch();
+        assertThat(racingCar.getMatch()).isEqualTo(4);
+        racingCar.setMatch(0);
+        racingCar.startMatch();
+        assertThat(racingCar.getMatch()).isEqualTo(0);
     }
 
     @Test
     @DisplayName("랜덤 값이 4 이상일 경우 전진한다.")
     void 랜덤값_4_이상_전진() {
-        String result = RacingCar.getInstance().moveForward("-", 4);
+        RacingCar racingCar = new RacingCar();
+        String result = racingCar.moveForward("-", 4);
         assertThat(result).isEqualTo("--");
-        result = RacingCar.getInstance().moveForward("--", 1);
+        result = racingCar.moveForward("--", 1);
         assertThat(result).isEqualTo("--");
     }
 
     @Test
     @DisplayName("사용자 입력에 맞춰 매치 수를 세팅한다.")
     void initialize_match() {
-        RacingCar.getInstance().setMatch(5);
-        assertThat(RacingCar.getInstance().getMatch()).isEqualTo(5);
+        RacingCar racingCar = new RacingCar();
+        racingCar.setMatch(5);
+        assertThat(racingCar.getMatch()).isEqualTo(5);
     }
 
     @Test
     @DisplayName("사용자 입력에 맞춰 자동차를 세팅한다.")
     void initialize_car() {
-        RacingCar.getInstance().initializeCar(5);
-        assertThat(RacingCar.getInstance().getCars()).hasSize(5);
+        RacingCar racingCar = new RacingCar();
+        racingCar.initializeCar(5);
+        assertThat(racingCar.getCars()).hasSize(5);
     }
 
     @Test
