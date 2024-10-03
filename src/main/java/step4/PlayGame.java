48a63c420f2d37a5cd527f9eefae5d869cb31ddc
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-03 23:30:55 +0900

    refactor : 메서드 분리 및 라운드 수 예외처리 추가

--- src/main/java/step4/PlayGame.java
+++ src/main/java/step4/PlayGame.java
@@ -15,16 +15,26 @@ public class PlayGame {
     }
 
     public void gameStart(int round) {
+        checkRoundNum(round);
         resultView.printResultView();
         for (int i = 0; i < round; i++) {
-            for (Car car : cars.getCars()) {
-                resultView.printCarName(car);
-                resultView.printCarLocation(car.getLocation());
-                car.move(RandomNumberGenerator.getRandomGenerator());
-            }
+            playRound();
             resultView.separateRound();
         }
         resultView.printWinners(cars.getWinners());
+    }
+
+    private void playRound() {
+        for (Car car : cars.getCars()) {
+            resultView.printCarName(car);
+            resultView.printCarLocation(car.getLocation());
+            car.move(RandomNumberGenerator.getRandomGenerator());
+        }
+    }
+
+    private static void checkRoundNum(int round) {
+        if(round <1)
+            throw new IllegalArgumentException("최소 한 라운드 이상은 진행해야함");
     }
 
 }
