728f2b41553c3047b1e5d7d989604f5623e5556f
Slowth-KIM <rt.slowth@gmail.com>
2024-10-03 17:18:09 +0900

    test : 자동차 생성 테스트에 이름 추가 및 테스트 완료

--- src/test/java/racinggame/RacingCarCreationTest.java
+++ src/test/java/racinggame/RacingCarCreationTest.java
@@ -2,8 +2,14 @@ package racinggame;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racinggame.racingcar.CarName;
+import racinggame.racingcar.CarPosition;
 import racinggame.racingcar.MoveStrategy;
 import racinggame.racingcar.RacingCars;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,9 +19,11 @@ class RacingCarCreationTest {
     @DisplayName("단일 자동차 객체 생성")
     void singleCarCreation() {
         MoveStrategy dummyStrategy = () -> false;
-        RacingCars racingCars = RacingCars.create(1, dummyStrategy);
+        List<String> nameList = List.of("a");
+        RacingCars racingCars = RacingCars.create(1, dummyStrategy, nameList);
 
         assertThat(racingCars.count()).isEqualTo(1);
+        assertThat(racingCars.getCarNames()).containsOnly("a");
         assertThat(racingCars.getCurrentPositionsRepresentation()).containsOnly(0);
     }
 
@@ -23,9 +31,11 @@ class RacingCarCreationTest {
     @DisplayName("여러 대의 자동차 객체 생성")
     void multipleCarCreation() {
         MoveStrategy dummyStrategy = () -> false;
-        RacingCars racingCars = RacingCars.create(3, dummyStrategy);
+        List<String> nameList = Arrays.asList("a", "b", "c");
+        RacingCars racingCars = RacingCars.create(3, dummyStrategy, nameList);
 
         assertThat(racingCars.count()).isEqualTo(3);
+        assertThat(racingCars.getCarNames()).isEqualTo(nameList);
         assertThat(racingCars.getCurrentPositionsRepresentation()).containsOnly(0);
     }
 }
