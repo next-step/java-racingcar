d84ae79c17871276dce38789e1071f79fe699b27
hwan2-99 <thh1233123@gmail.com>
2024-10-03 17:33:59 +0900

    refactor: 임의의 값 생성할 경우 메서드가 호출될 때 마다 객체생성을 피하기 위한 정적 변수 생성

--- src/main/java/step3/model/MoveStrategy.java
+++ src/main/java/step3/model/MoveStrategy.java
@@ -3,9 +3,10 @@ package step3.model;
 import java.util.Random;
 
 public class MoveStrategy {
+    private static final Random RANDOM = new Random();
     private static final int RANDOM_MAX_NUMBER = 10;
 
     public int generateNumber() {
-        return new Random().nextInt(RANDOM_MAX_NUMBER);
+        return RANDOM.nextInt(RANDOM_MAX_NUMBER);
     }
 }
