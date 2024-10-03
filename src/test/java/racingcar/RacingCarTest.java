1551f212091728a5f6edda4241d1e38fbaf12a86
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-03 16:21:44 +0900

    test : 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우에만 전진하는 테스트

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -2,6 +2,11 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,4 +28,23 @@ public class RacingCarTest {
         assertThat(car.getPosition()).isEqualTo(0);
     }
 
+    @ParameterizedTest
+    @MethodSource("randomNum")
+    @DisplayName("자동차가 이동하는 값이 4 이상일 경우에만 전진하고 그렇지않으면 멈춰있는지 확인")
+    void carMovesOrStopsByRandomNum(int num) {
+        Car car = new Car();
+        car.move(num);
+
+        System.out.println("num :: " + num);
+        if (num >= 4) {
+            assertThat(car.getPosition()).isEqualTo(1);
+            return;
+        }
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    static IntStream randomNum() {
+        Random random = new Random();
+        return IntStream.generate(() -> random.nextInt(10)).limit(1);
+    }
 }
