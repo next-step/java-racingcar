c5460dd41317e582625d0371d1806677e95e54d4
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-03 21:19:28 +0900

    refactor : 미사용 메서드 및 상수 삭제, 상수 추가

--- src/main/java/step4/util/InputUtils.java
+++ src/main/java/step4/util/InputUtils.java
@@ -3,7 +3,7 @@ package step4.util;
 import java.util.Scanner;
 
 public class InputUtils {
-    private static final String INPUT_NUMBER_OF_CAR = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_NAME_OF_CAR = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_NUMBER_OF_MOVEMENT = "시도할 회수는 몇 회 인가요?";
 
     private static Scanner scanner = new Scanner(System.in);
@@ -12,9 +12,6 @@ public class InputUtils {
         throw new AssertionError("생성자 호출 금지");
     }
 
-    public static int inputCarView() {
-        return InputNumber(INPUT_NUMBER_OF_CAR);
-    }
 
     public static int inputMoveView() {
         return InputNumber(INPUT_NUMBER_OF_MOVEMENT);
@@ -26,6 +23,7 @@ public class InputUtils {
     }
 
     public static String[] inputCarNameView() {
+        System.out.println(INPUT_NAME_OF_CAR);
         Scanner scanner = new Scanner(System.in);
         return scanner.next().split(",");
     }
