596224355ed42036de410c41d76b55bfbc05f3fd
heechul <khc4190@naver.com>
2024-10-03 19:33:05 +0900

    feat: racing service 추가
    - 매치 컨트롤

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,5 +1,46 @@
 package racingcar.service;
 
+import racingcar.domain.RacingCar;
+import racingcar.domain.RandomMove;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingService {
 
+    private final List<RacingCar> racingCar = new ArrayList<>();
+    private int matchCount;
+
+    public RacingService(int carNumber, int matchCount) {
+        this.matchCount = matchCount;
+        for (int i = 0; i < carNumber; i++) {
+            racingCar.add(new RacingCar());
+        }
+    }
+
+    public boolean isMatching() {
+        return matchCount > 0;
+    }
+
+    public void match() {
+        if (matchCount > 0) {
+            for (RacingCar car : racingCar) {
+                car.move(RandomMove.getInstance().movable());
+            }
+            matchCount--;
+        }
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public String getRacingCarsStatus() {
+        StringBuilder sb = new StringBuilder();
+        for (RacingCar car : racingCar) {
+            sb.append(car.getStatus());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
