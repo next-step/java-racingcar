8f8435f4253415e3747dfbfdf97328c1998bc689
jara <jara0701@gmail.com>
2024-10-03 15:49:22 +0900

    feat RacingCarSimulator.java: simulate 구현
    simulate 구현

--- src/main/java/racingcar/RacingCarSimulator.java
+++ src/main/java/racingcar/RacingCarSimulator.java
@@ -1,12 +1,36 @@
 package racingcar;
 
+import racingcar.dto.RacingCarStatesDTO;
 import racingcar.dto.RacingResultDTO;
 import racingcar.dto.RacingWrapResultDTO;
 
 public class RacingCarSimulator {
     public static RacingResultDTO simulate(int carNumber, int tryNumber) {
+        RacingCar[] racingCars = createRacingCars(carNumber);
+        return simulateRacing(tryNumber, racingCars);
+    }
 
+    private static RacingResultDTO simulateRacing(int tryNumber, RacingCar[] racingCars) {
+        RacingWrapResultDTO[] wrapResults = new RacingWrapResultDTO[tryNumber];
+        for (int i = 0; i < tryNumber; i++) {
+            wrapResults[i] = simulateWrap(i, racingCars);
+        }
+        return new RacingResultDTO(wrapResults);
+    }
 
-        return new RacingResultDTO(new RacingWrapResultDTO[]{});
+    private static RacingWrapResultDTO simulateWrap(int wrapNo, RacingCar[] racingCars) {
+        for (RacingCar racingCar : racingCars) {
+            racingCar.race();
+        }
+        RacingCarStatesDTO carStates = new RacingCarStatesDTO(racingCars);
+        return new RacingWrapResultDTO(wrapNo, carStates);
+    }
+
+    private static RacingCar[] createRacingCars(int carNumber) {
+        RacingCar[] racingCars = new RacingCar[carNumber];
+        for (int i = 0; i < carNumber; i++) {
+            racingCars[i] = new RacingCar(i);
+        }
+        return racingCars;
     }
 }
