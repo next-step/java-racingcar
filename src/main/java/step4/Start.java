3a35fefbaf2c7c787fd5f0cf26dae1bc73ed9c66
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-03 21:13:12 +0900

    chore : 자동차_이름입력_테스트 작성

--- src/main/java/step4/Start.java
+++ src/main/java/step4/Start.java
@@ -6,7 +6,7 @@ import step4.view.ResultView;
 
 public class Start {
     public static void main(String[] args) {
-        Cars cars = new Cars(InputUtils.inputCarView());
+        Cars cars = new Cars(InputUtils.inputCarNameView());
         ResultView resultView = new ResultView();
         PlayGame play = new PlayGame(cars.getCars(),resultView);
         play.gameStart(InputUtils.inputMoveView());
--- src/test/java/step4/CarRaceWithNameTest.java
+++ src/test/java/step4/CarRaceWithNameTest.java
@@ -1,15 +1,18 @@
 package step4;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import step4.util.InputUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import step4.vehicle.Cars;
 
 public class CarRaceWithNameTest {
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi,crong,honux"})
     @DisplayName("자동차 이름 입력 테스트")
-    void 자동차_이름입력_테스트(){
-        Cars car = new Cars(InputUtils.inputCarNameView());
+    void 자동차_이름입력_테스트(String input){
+        Cars car = new Cars(input.split(","));
+        Assertions.assertThat(car).hasNoNullFieldsOrProperties();
     }
 
 }
