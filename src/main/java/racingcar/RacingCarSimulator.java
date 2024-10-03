0dfc0a013fad4f9f81a199851c64f618d0e799ac
jara <jara0701@gmail.com>
2024-10-03 14:19:01 +0900

    feat RecingCarSimulator.java: RecingCarSimulator.java 인터페이스 만듬
    RecingCarSimulator class 인터페이스를 만듬

---
+++ src/main/java/racingcar/RacingCarSimulator.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import racingcar.dto.RacingResultDTO;
+import racingcar.dto.RacingWrapResultDTO;
+
+public class RacingCarSimulator {
+    public RacingResultDTO simulate(int carNumber, int tryNumber) {
+        return new RacingResultDTO(new RacingWrapResultDTO[]{});
+    }
+}
---
+++ src/test/java/racingcar/RacingCarSimulatorTest.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.dto.RacingResultDTO;
+import racingcar.dto.RacingWrapResultDTO;
+
+public class RacingCarSimulatorTest {
+
+    final static RacingCarSimulator simulator = new RacingCarSimulator();
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,3,5,7,9})
+    @DisplayName("결과는 입력한 트라이 횟수와 같은 수의 wrapResult를 가집니다")
+    void wrapResultsSizeTest(int tryNumber){
+        RacingResultDTO result = simulator.simulate(1, tryNumber);
+        Assertions.assertEquals(tryNumber, result.wrapResults.length);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,3,5,7,9})
+    @DisplayName("wrapResult는 입력한 차의 수와 같은 수의 차량 상태를 가집니다")
+    void carStateSizeTest(int carNumber){
+        RacingResultDTO result = simulator.simulate(carNumber, 1);
+        RacingWrapResultDTO[] wrapResults = result.wrapResults;
+        for(RacingWrapResultDTO wrapResult: wrapResults) {
+            Assertions.assertEquals(carNumber, wrapResult.carStates.catStates.length);
+        }
+    }
+}
