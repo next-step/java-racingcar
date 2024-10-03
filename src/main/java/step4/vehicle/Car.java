1e6a91a4695d47459f13d2c7299b54624babf983
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-03 21:09:32 +0900

    refactor : Car클래스 생성자 추가

--- src/main/java/step4/vehicle/Car.java
+++ src/main/java/step4/vehicle/Car.java
@@ -6,6 +6,11 @@ public class Car {
     private static final int MOVE_CONDITION = 4;
 
     private int location = 1;
+    private String name ;
+
+    Car(String name){
+        this.name = name;
+    }
 
     public void move(NumberGenerator generator) {
         if (isMovable(generator.getValue())) {
