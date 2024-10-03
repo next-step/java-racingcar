6ebdf5563edf171e785e30961628d366920f7e5a
이윤선 <yoonseon3@gmail.com>
2024-10-04 01:14:44 +0900

    test: getMaxPosition 메서드가 가장높은 위치의 Car position을 반환한다.

--- src/test/java/racinggame/domain/CarsTest.java
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -16,9 +16,19 @@ class CarsTest {
         assertThat(cars).isNotNull();
     }
 
+    @Test
+    @DisplayName("getMaxPosition 메서드가 가장높은 위치의 Car position을 반환한다.")
+    void getMaxPositionTest() {
+        Cars cars = initCars();
+        int correctMoveNumber = 5;
+        cars.getCarList().get(0).move(correctMoveNumber);
+        cars.getCarList().get(0).move(correctMoveNumber);
+        cars.getCarList().get(1).move(correctMoveNumber);
+        assertThat(cars.getMaxPosition()).isEqualTo(2);
+    }
+
     private Cars initCars() {
         List<String> carNames = List.of("자동차1", "자동차2", "자동차3");
-        Cars cars = new Cars(carNames);
-        return cars;
+        return new Cars(carNames);
     }
 }
