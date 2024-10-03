fcf3931a5a102ce179c57fb428db04e88e56cec9
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-03 20:57:55 +0900

    refactor : Cars 클래스 메서드 파라미터 수정 - 생성자 , initCars (int -> String[])

--- src/main/java/step4/vehicle/Cars.java
+++ src/main/java/step4/vehicle/Cars.java
@@ -6,16 +6,16 @@ import java.util.List;
 public class Cars {
     private final List<Car> cars = new ArrayList<>();
 
-    public Cars(int num) {
-        if (num < 2) {
+    public Cars(String[] names) {
+        if (names.length < 2) {
             throw new IllegalArgumentException("게임을 진행할 수 없음");
         }
-        initCars(num);
+        initCars(names);
     }
 
-    void initCars(int num) {
-        for (int i = 0; i < num; i++) {
-            cars.add(new Car());
+    void initCars(String[] names) {
+        for (String name : names) {
+            cars.add(new Car(name));
         }
     }
 
--- src/test/java/step4/CarRaceWithNameTest.java
+++ src/test/java/step4/CarRaceWithNameTest.java
@@ -2,11 +2,14 @@ package step4;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import step4.util.InputUtils;
+import step4.vehicle.Cars;
 
 public class CarRaceWithNameTest {
     @Test
     @DisplayName("자동차 이름 입력 테스트")
     void 자동차_이름입력_테스트(){
+        Cars car = new Cars(InputUtils.inputCarNameView());
     }
 
 }
