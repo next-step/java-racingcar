c5a72f4e6802e20ce4c3c8128aab71177b998988
JongMinCh0i <wonderland314592@gmail.com>
2024-10-03 19:26:33 +0900

    [feat] RacingCar 에 대한 유닛테스트 수행

---
+++ src/main/java/study/step_4/service/RacingCar.java
@@ -0,0 +1,43 @@
+package study.step_4.service;
+
+import java.util.Random;
+
+public class RacingCar {
+
+    private String name;
+    private int distance = 0;
+
+    public RacingCar(String name, int distance) {
+        this.name = name;
+        this.distance = distance;
+    }
+
+    Random random = new Random();
+
+    public int drive(int bound, int condition) {
+        if (canMove(bound, condition)) {
+            Move();
+        }
+        return distance;
+    }
+
+    public int getRandomValue(int bound) {
+        return random.nextInt(bound);
+    }
+
+    private boolean canMove(int bound, int condition) {
+        return getRandomValue(bound) >= condition;
+    }
+
+    private void Move() {
+        distance += 1;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
