0008f2f6d6183ee017a46e34138c71c717255adb
한주형 <91880235+Jureamer@users.noreply.github.com>
2024-10-03 09:02:32 +0900

    test: add inputview test code

--- src/main/java/race/InputView.java
+++ src/main/java/race/InputView.java
@@ -1,17 +1,36 @@
 package race;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private final Scanner SCANNER = new Scanner(System.in);
+    private final Scanner scanner;
     private final String CAR_NAME_SPLIT_REGEX = "\\s*,\\s*";
+    private final int MIN_CAR_COUNT = 1;
+    private final int MAX_CAR_COUNT = 5;
+    private final int MIN_RACE_COUNT = 1;
+    private final int MAX_RACE_COUNT = 9;
 
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public InputView() {
+        this(new Scanner(System.in));
+    }
     public List<String> receiveCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String carNamesString = SCANNER.nextLine();
+        String carNamesString = scanner.nextLine();
         List<String> carNames = List.of(carNamesString.split(CAR_NAME_SPLIT_REGEX));
+        validateCarNamesCount(carNames);
         return carNames;
+    }
+
+    private void validateCarNamesCount(List<String> carNames) {
+        if (carNames.size() < MIN_CAR_COUNT || carNames.size() > MAX_CAR_COUNT) {
+            throw new IllegalArgumentException("경주 할 자동차 이름을 1~5대 사이로 입력 해 주시기 바랍니다.");
+        }
     }
 
     public int receiveRaceCount() {
@@ -19,24 +38,24 @@ public class InputView {
 
         while (true) {
             try {
-                int raceCount = SCANNER.nextInt();
+                int raceCount = scanner.nextInt();
                 validateRaceCount(raceCount);
                 return raceCount;
-            } catch (NumberFormatException e) {
-                System.out.println("1 ~ 9 사이의 유효한 숫자를 입력 해주세요.");
+            } catch (InputMismatchException e) {
+                throw new InputMismatchException("1 ~ 9 사이의 유효한 숫자를 입력 해주세요.");
             }
         }
     }
 
     public void closeScanner() {
-        if (SCANNER != null) {
-            SCANNER.close();
+        if (scanner != null) {
+            scanner.close();
         }
     }
 
     private void validateRaceCount(int raceCount) {
-        if (raceCount <= 1) {
-            throw new NumberFormatException("1 ~ 9 사이의 양수를 입력해주세요.");
+        if (raceCount < MIN_RACE_COUNT || raceCount > MAX_RACE_COUNT) {
+            throw new IllegalArgumentException("우");
         }
     }
 }
---
+++ src/test/java/race/InputViewTest.java
@@ -0,0 +1,69 @@
+package race;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputViewTest {
+    @ParameterizedTest
+    @ValueSource(strings = {",", "Car1,Car2,Car3,Car4,Car5,Car6"})
+    @DisplayName("입력한 자동차 대수가 범위를 초과할 경우 IllegalArgumentException 발생한다.")
+    public void givenCarCountOverRange_shouldReturnIllegalArgumentException(String input) {
+        Scanner scanner = new Scanner(input);
+        InputView inputView = new InputView(scanner);
+
+        assertThatThrownBy(inputView::receiveCarNames)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("경주 할 자동차 이름을 1~5대 사이로 입력 해 주시기 바랍니다.");
+    }
+
+    @Test
+    @DisplayName("자동차 이름을 입력할 경우 자동차 이름의 리스트로 반환한다.")
+    public void givenCarNames_shouldReturnCarNameList() {
+        Scanner scanner = new Scanner("Car1,Car2");
+        InputView inputView = new InputView(scanner);
+
+        assertThat(inputView.receiveCarNames()).containsExactly("Car1", "Car2");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"test", "/", "10ad"})
+    @DisplayName("경주 횟수가 숫자로 입력되지 않을 경우 InputMismatchException이 발생한다.")
+    public void givenRaceCountIsWrong_shouldReturnInputMismatchException(String input) {
+        Scanner scanner = new Scanner(input);
+        InputView inputView = new InputView(scanner);
+
+        assertThatThrownBy(inputView::receiveRaceCount)
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessage("1 ~ 9 사이의 유효한 숫자를 입력 해주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "10"})
+    @DisplayName("경주 횟수가 범위를 벗어날 경우 IllegalArgumentException이 발생한다.")
+    public void givenRaceCountOverRange_shouldReturnIllegalArgumentException(String input) {
+        Scanner scanner = new Scanner(input);
+        InputView inputView = new InputView(scanner);
+
+        assertThatThrownBy(inputView::receiveRaceCount)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1 ~ 9 사이의 양수를 입력해주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "9"})
+    @DisplayName("경주 횟수가 정상적으로 입력될 경우 입력된 경주 횟수를 정수로 반환한다.")
+    public void givenRaceCountIsNormal_shouldReturnRaceCount(String input) {
+        Scanner scanner = new Scanner(input);
+        InputView inputView = new InputView(scanner);
+
+        assertThat(inputView.receiveRaceCount()).isEqualTo(Integer.parseInt(input));
+    }
+}
