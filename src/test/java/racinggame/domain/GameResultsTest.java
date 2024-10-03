1c0efae1747ba8b5122a66b1579323f2c71f458a
이윤선 <yoonseon3@gmail.com>
2024-10-04 01:33:18 +0900

    test: getGameResultCarPositionByIndex 메서드가 게임 결과에 저장된 자동차 위치를 반환한다.

--- src/test/java/racinggame/domain/GameResultsTest.java
+++ src/test/java/racinggame/domain/GameResultsTest.java
@@ -23,6 +23,19 @@ class GameResultsTest {
         assertThat(gameResults.carCount()).isEqualTo(3);
     }
 
+    @Test
+    @DisplayName("getGameResultCarPositionByIndex 메서드가 게임 결과에 저장된 자동차 위치를 반환한다.")
+    void getGameResultCarPositionByIndexTest() {
+        List<String> carNames = List.of("자동차1", "자동차2", "자동차3");
+        Cars cars = new Cars(carNames);
+        int correctMoveNumber = 5;
+        cars.getCarList().get(0).move(correctMoveNumber);
+        GameResults gameResults = new GameResults(cars);
+        assertThat(gameResults.getGameResultCarPositionByIndex(0)).isOne();
+        assertThat(gameResults.getGameResultCarPositionByIndex(1)).isZero();
+        assertThat(gameResults.getGameResultCarPositionByIndex(2)).isZero();
+    }
+
     private GameResults initGameResults() {
         List<String> carNames = List.of("자동차1", "자동차2", "자동차3");
         return new GameResults(new Cars(carNames));
