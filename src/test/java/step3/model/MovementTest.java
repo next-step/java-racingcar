7b385ca22caa87c63dd32acdd284fad5b3edc79a
hwan2-99 <thh1233123@gmail.com>
2024-10-03 17:54:52 +0900

    test: 자동차 전진 테스트 작성

--- src/test/java/step3/model/MovementTest.java
+++ src/test/java/step3/model/MovementTest.java
@@ -10,4 +10,11 @@ class MovementTest {
     void 자동차_움직임_수치_객체_생성() {
         assertThat(new Movement().getMoveCount()).isEqualTo(0);
     }
+
+    @Test
+    void 자동차_전진_확인() {
+        Movement movement = new Movement();
+        movement.move();
+        assertThat(movement.getMoveCount()).isEqualTo(1);
+    }
 }
