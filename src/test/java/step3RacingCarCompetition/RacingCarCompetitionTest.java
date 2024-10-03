82fc987ba46108f9fdaa385d0c04eb91dcf9beec
tjdjdjrdl123 <lee-won-suk@github.io>
2024-10-03 13:44:27 +0900

    [test] 3단계 - 자동차 경주 테스트코드 수정
    1. 테스트가 어렵게 하는 랜덤값을 TestRandomNumberGenerator 목객체를 만들어서 고정된 랜덤값을 갖도록 수정
    2 신규 메서드 테스트 코드 추가

--- src/test/java/step3RacingCarCompetition/RacingCarCompetitionTest.java
+++ src/test/java/step3RacingCarCompetition/RacingCarCompetitionTest.java
@@ -7,43 +7,62 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingCarCompetitionTest {
     RacingCarCompetition racingCarCompetition;
     RacingCarInfo racingCarInfo;
+    RandomGenerator randomGenerator;
+    RandomGenerator testRandomMockGenerator;
+    Car car;
+    final static int TOTAL_NUMBER_OF_CARS = 2;
+    final static int TOTAL_MOVING_COUNT = 2;
 
     @BeforeEach
     void init() {
+        car = new Car();
+        randomGenerator = new RandomNumber(10);
+        testRandomMockGenerator = new TestRandomNumberGenerator();
         racingCarCompetition = new RacingCarCompetition();
-        racingCarInfo = new RacingCarInfo(2);
-        racingCarInfo.recordGoInRacingCarData(0, "-");
-        racingCarInfo.recordGoInRacingCarData(1, "--");
+        racingCarInfo = new RacingCarInfo(TOTAL_NUMBER_OF_CARS, TOTAL_MOVING_COUNT, testRandomMockGenerator);
     }
 
     @DisplayName(value = "랜덤값 범위 확인 테스트")
     @RepeatedTest(5)
     void randomNumberTest() {
-        assertThat(racingCarCompetition.randomNumber()).isBetween(0, 9);
+        assertThat(randomGenerator.generateRandom()).isBetween(0, 9);
     }
 
-    @DisplayName(value = "이동, 정지 테스트")
+    @DisplayName(value = "값 저장 및 데이터 가져오기 테스트. ")
     @ParameterizedTest()
-    @CsvSource({"3,''", "4,-"})
-    void movingStopDecisionTest(int randomNumber, String isGoStop) {
-        assertThat(racingCarCompetition.moveStopDecision(randomNumber)).isEqualTo(isGoStop);
+    @CsvSource({"1,4", "2,44"})
+    void carMethodTest(int movingCount, String resultCarMovingData) {
+        car.recordCarData(TOTAL_MOVING_COUNT, testRandomMockGenerator);
+        assertThat(car.findEachMovingData(movingCount)).isEqualTo(resultCarMovingData);
     }
 
-    @Test
-    @DisplayName(value = "차 이동상태 저장 기능 테스트")
-    void recordRacingCarDataTest() {
-        racingCarInfo.recordGoInRacingCarData(0, "-");
-        assertThat(racingCarInfo.findRacingCarData(0)).isEqualTo("--");
+    @DisplayName(value = "RacingCarInfo 데이터 가져오는 여부 테스트. 현재 데이터 ={44,44}")
+    @Test()
+    void findEachRoundRacingCarDataTest() {
+        List<String> resultData = racingCarInfo.findEachRoundRacingCarData(2);
+        assertThat(resultData).isEqualTo(List.of("44", "44"));
     }
 
-    @Test
+    @DisplayName(value = "차 이동상태 판단 기능 테스트")
+    @ParameterizedTest()
+    @CsvSource(value = {"3,''", "4,-"})
+    void moveStopDecisionTest(char randomNumber, String resultMovingString) {
+        assertThat(ResultView.moveStopDecision(randomNumber))
+                .isEqualTo(resultMovingString);
+    }
+
     @DisplayName(value = "차 이동상태 저장 확인 테스트, 차2대,이동 2번")
-    void findRacingCarDataTest() {
-        assertThat(racingCarInfo.findRacingCarData(1)).isEqualTo("--");
+    @ParameterizedTest()
+    @CsvSource(value = {"33,''", "34,-", "43,-", "44,--"})
+    void findRacingCarDataTest(String movingData, String expectedResultString) {
+        assertThat(ResultView.convertMovingData(movingData))
+                .isEqualTo(expectedResultString);
     }
 }
