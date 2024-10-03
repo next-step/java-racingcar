ca16bd47abcb09e1c865087da4716d8cc2994f12
heechul <khc4190@naver.com>
2024-10-03 13:58:25 +0900

    정적 클래스 -> 싱글톤 클래스 변환

--- src/main/java/calculator/StringSumCalculator.java
+++ src/main/java/calculator/StringSumCalculator.java
@@ -4,18 +4,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringSumCalculator {
+    private static StringSumCalculator INSTANCE = null;
 
-    public static final String DEFAULT_DELIMITER = "[,:]";
-    public static final String CUSTOM_DELIMITER = "//(.)\n(.*)";
-    public static final Pattern pattern = Pattern.compile(CUSTOM_DELIMITER);
-    public static final int DELIMITER_GROUP = 1;
-    public static final int TEXT_GROUP = 2;
+    public final String DEFAULT_DELIMITER = "[,:]";
+    public final String CUSTOM_DELIMITER = "//(.)\n(.*)";
+    public final Pattern pattern = Pattern.compile(CUSTOM_DELIMITER);
+    public final int DELIMITER_GROUP = 1;
+    public final int TEXT_GROUP = 2;
 
-    private StringSumCalculator(){
+    private StringSumCalculator() {
 
     }
 
-    public static int sum(final String text) {
+    public static StringSumCalculator getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new StringSumCalculator();
+        }
+        return INSTANCE;
+    }
+
+    public int sum(final String text) {
         if (isBlank(text)) {
             return 0;
         }
@@ -24,11 +32,11 @@ public class StringSumCalculator {
         return sumNumbers(numbers);
     }
 
-    private static boolean isBlank(String text) {
+    private boolean isBlank(String text) {
         return text == null || text.isBlank();
     }
 
-    private static int[] getNumbers(final String[] split) {
+    private int[] getNumbers(final String[] split) {
         final int[] result = new int[split.length];
         for (int i = 0; i < split.length; i++) {
             final int number = getPositiveNumber(split[i]);
@@ -37,11 +45,11 @@ public class StringSumCalculator {
         return result;
     }
 
-    private static int getPositiveNumber(final String split) {
+    private int getPositiveNumber(final String split) {
         int number;
         try {
             number = Integer.parseInt(split);
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new RuntimeException();
         }
         if (number < 0) {
@@ -50,7 +58,7 @@ public class StringSumCalculator {
         return number;
     }
 
-    private static int sumNumbers(final int[] split) {
+    private int sumNumbers(final int[] split) {
         int result = 0;
         for (int number : split) {
             result += number;
@@ -58,7 +66,7 @@ public class StringSumCalculator {
         return result;
     }
 
-    private static String[] getSplit(final String text) {
+    private String[] getSplit(final String text) {
         String[] split = text.split(DEFAULT_DELIMITER);
         final Matcher matcher = pattern.matcher(text);
         if (matcher.find()) {
