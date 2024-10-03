03ffaabd013795897bdc2b735d9a50b8c4496cef
jara <jara0701@gmail.com>
2024-10-03 15:47:51 +0900

    refactor RacingCarStateDTO.java, RacingCarStatesDTO.java: 생성자 수정
    생성자에서 인자로 RacingCar를 받도록 수정

--- src/main/java/racingcar/dto/RacingCarStateDTO.java
+++ src/main/java/racingcar/dto/RacingCarStateDTO.java
@@ -1,11 +1,13 @@
 package racingcar.dto;
 
+import racingcar.RacingCar;
+
 public final class RacingCarStateDTO {
     final int carNo;
     public final int position;
 
-    public RacingCarStateDTO(int carNo, int position) {
-        this.carNo = carNo;
-        this.position = position;
+    public RacingCarStateDTO(RacingCar racingCar) {
+        this.carNo = racingCar.getCarNo();
+        this.position = racingCar.getPosition();
     }
 }
--- src/main/java/racingcar/dto/RacingCarStatesDTO.java
+++ src/main/java/racingcar/dto/RacingCarStatesDTO.java
@@ -1,9 +1,16 @@
 package racingcar.dto;
 
+import racingcar.RacingCar;
+
 public final class RacingCarStatesDTO {
     public final RacingCarStateDTO[] catStates;
 
-    public RacingCarStatesDTO(RacingCarStateDTO[] catStates) {
-        this.catStates = catStates;
+    public RacingCarStatesDTO(RacingCar[] racingCars) {
+        int racingCarsNum = racingCars.length;
+        RacingCarStateDTO[] carStates = new RacingCarStateDTO[racingCarsNum];
+        for (int i = 0; i < racingCarsNum; i++) {
+            carStates[i] = new RacingCarStateDTO(racingCars[i]);
+        }
+        this.catStates = carStates;
     }
 }
