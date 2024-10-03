50f7a1d5d875be38961a914a81c77a26edf51227
이윤선 <yoonseon3@gmail.com>
2024-10-04 01:54:28 +0900

    test: getCarPosition 메서드가 특정 라운드의 특정 자동차 위치를 반환한다.

--- src/test/java/racinggame/domain/GameRoundsTest.java
+++ src/test/java/racinggame/domain/GameRoundsTest.java
@@ -13,12 +13,27 @@ class GameRoundsTest {
     @DisplayName("getRoundCount 메서드가 게임 라운드 수를 반환한다.")
     void getRoundCountTest() {
         GameRounds gameRounds = initGameRounds();
-        gameRounds.save(initGameResults());
         assertThat(gameRounds.getRoundCount()).isOne();
     }
 
+    @Test
+    @DisplayName("getCarPosition 메서드가 특정 라운드의 특정 자동차 위치를 반환한다.")
+    void getCarPositionTest() {
+        List<String> carNames = List.of("자동차1", "자동차2", "자동차3");
+        Cars cars = new Cars(carNames);
+        int correctMoveNumber = 5;
+        cars.getCarList().get(0).move(correctMoveNumber);
+        cars.getCarList().get(0).move(correctMoveNumber);
+        GameResults gameResults = new GameResults(cars);
+        GameRounds gameRounds = new GameRounds();
+        gameRounds.save(gameResults);
+        assertThat(gameRounds.getCarPosition(0, 0)).isEqualTo(2);
+    }
+
     private GameRounds initGameRounds() {
-        return new GameRounds();
+        GameRounds gameRounds = new GameRounds();
+        gameRounds.save(initGameResults());
+        return gameRounds;
     }
 
     private GameResults initGameResults() {
