219c9a9694b57cf0af5bf9bca6ff9d4ed7583cf3
owljoa <needshield@gmail.com>
2024-10-04 13:00:37 +0900

    refactor: 입력 요청을 위한 메시지 출력을 출력용 객체로 이동

--- src/main/java/race/InputView.java
+++ src/main/java/race/InputView.java
@@ -3,16 +3,13 @@ package race;
 import java.util.Scanner;
 
 public class InputView {
-    public static final String CAR_NAMES_REQUEST_STATEMENT = "경주할 자동차 이름을 입력하세요(각 이름은 5글자 이하, 자동차는 최소 2대 이상이며, 쉼표(,)를 기준으로 구분)";
-    public static final String GAME_COUNT_REQUEST_STATEMENT = "시도할 회수는 몇 회 인가요? (1 이상의 숫자 입력)";
-
     public static String inputCarName() {
-        System.out.println(CAR_NAMES_REQUEST_STATEMENT);
+        ResultView.printCarNamesRequestMessage();
         return getInputString();
     }
 
     public static int inputGameCount() {
-        System.out.println(GAME_COUNT_REQUEST_STATEMENT);
+        ResultView.printGameCountRequestMessage();
         return getInputInteger();
     }
 
--- src/main/java/race/ResultView.java
+++ src/main/java/race/ResultView.java
@@ -3,11 +3,20 @@ package race;
 import java.util.List;
 
 public class ResultView {
+    private static final String CAR_NAMES_REQUEST_STATEMENT = "경주할 자동차 이름을 입력하세요(각 이름은 5글자 이하, 자동차는 최소 2대 이상이며, 쉼표(,)를 기준으로 구분)";
+    private static final String GAME_COUNT_REQUEST_STATEMENT = "시도할 회수는 몇 회 인가요? (1 이상의 숫자 입력)";
     private static final String CAR_STATE_MARKER = "-";
     public static final String RESULT_TITLE = "실행 결과";
     public static final String CAR_NUMBER_POSTFIX = "번 차: ";
     public static final String LINE_BREAK = "%n";
 
+    public static void printCarNamesRequestMessage() {
+        System.out.println(CAR_NAMES_REQUEST_STATEMENT);
+    }
+
+    public static void printGameCountRequestMessage() {
+        System.out.println(GAME_COUNT_REQUEST_STATEMENT);
+    }
 
     public static void printResultTitle() {
         System.out.printf(LINE_BREAK + RESULT_TITLE + LINE_BREAK);
