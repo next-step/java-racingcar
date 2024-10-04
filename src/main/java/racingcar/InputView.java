ed4b0e7ffbc977a3186718d5d71a8048e017e732
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-04 19:43:52 +0900

    refactor : private 생성자로 불필요한 인스턴스화 막기

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private InputView() {
+        
+    }
 
     public static int getCarNumber() {
         System.out.println("자동차 개수는 몇 개 인가요?");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 
     public static int getRoundNumber() {
         System.out.println("시도 할 횟수는 몇 번 인가요?");
-        return scanner.nextInt();
+        return SCANNER.nextInt();
     }
 }
