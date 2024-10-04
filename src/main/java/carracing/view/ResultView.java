316f60c80a20030eb005be638c3cabf1625e8719
green <green@mobidoc.us>
2024-10-05 00:07:25 +0900

    refactor: step5 ResultView에서 우승자 이름을 찾아서 출력

--- src/main/java/carracing/view/ResultView.java
+++ src/main/java/carracing/view/ResultView.java
@@ -1,27 +1,45 @@
 package carracing.view;
 
-import carracing.domain.record.CarRecord;
+import carracing.domain.Car;
 import carracing.domain.record.RoundRecord;
-import carracing.domain.record.RoundRecords;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
-    public void showCarRacingResult(RoundRecords roundRecords) {
+    public void showCarRacingResult(List<RoundRecord> roundRecords) {
         System.out.println("\n실행 결과");
-        for (RoundRecord roundRecord : roundRecords.getRoundRecords()) {
+
+        for (RoundRecord roundRecord : roundRecords) {
             showRoundResult(roundRecord);
         }
+
         showWinnerNames(roundRecords);
     }
 
     private void showRoundResult(RoundRecord roundRecord) {
-        for (CarRecord carRecord : roundRecord.getCarRecords()) {
+        for (Car carRecord : roundRecord.getCars()) {
             System.out.printf("%-5s : %s%n", carRecord.getName(), "-".repeat(carRecord.getPosition()));
         }
+
         System.out.println();
     }
 
-    private void showWinnerNames(RoundRecords roundRecords) {
-        System.out.printf("%s가 최종 우승했습니다.", String.join(", ", roundRecords.findWinners()));
+    private void showWinnerNames(List<RoundRecord> roundRecords) {
+        RoundRecord lastRound = getLastRoundRecord(roundRecords);
+        List<String> winnerNames = getLeadingCarNames(lastRound);
+        System.out.printf("%s가 최종 우승했습니다.", String.join(", ", winnerNames));
+    }
+
+    private RoundRecord getLastRoundRecord(List<RoundRecord> roundRecords) {
+        return roundRecords.get(roundRecords.size() - 1);
+    }
+
+    private static List<String> getLeadingCarNames(RoundRecord roundRecord) {
+        return roundRecord.getLeadingCar()
+                .stream()
+                .map(Car::getName)
+                .collect(Collectors.toUnmodifiableList());
     }
 }
