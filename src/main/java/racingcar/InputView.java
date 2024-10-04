dd1384c63f8be8a95473bb029899c75d83be6573
김혜영 <sunzero0116@gmail.com>
2024-10-04 19:29:49 +0900

    delete : 불필요한 함수 제거

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -11,11 +11,6 @@ public class InputView {
         return inputName.split(",");
     }
 
-    public static int getCarCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return SCANNER.nextInt();
-    }
-
     public static int getRacingCount() {
         System.out.println("시도할 횟수는 몇 회 인가요?");
         return SCANNER.nextInt();
