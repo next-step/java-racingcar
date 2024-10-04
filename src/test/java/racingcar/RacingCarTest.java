6876f6071e4d78efe91dc672e881d4e5dec95df4
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-04 19:40:44 +0900

    refactor : 중복된 내용의 테스트 메소드 제거

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -12,22 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarTest {
 
-    @Test
-    @DisplayName("자동차가 이동하는 값이 4 이상일 경우 전진하는지 확인")
-    void carMovesWhenFourOrMore() {
-        Car car = new Car();
-        car.move(4);
-        assertThat(car.getPosition()).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("자동차가 이동하는 값이 4 이하일 경우 멈춰있는지 확인")
-    void carStopWhenLessThanFour() {
-        Car car = new Car();
-        car.move(3);
-        assertThat(car.getPosition()).isEqualTo(0);
-    }
-
     @ParameterizedTest
     @MethodSource("randomNum")
     @DisplayName("자동차가 이동하는 값이 4 이상일 경우에만 전진하고 그렇지않으면 멈춰있는지 확인")
