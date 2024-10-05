2967f230627fd40f64f1a8502b914e59af4e97e2
skdev0619 <skdev0619@naver.com>
2024-10-05 23:08:58 +0900

    레이싱 게임 테스트 추가

--- src/test/java/racingCar/step5/domain/RacingCarGameTest.java
+++ src/test/java/racingCar/step5/domain/RacingCarGameTest.java
@@ -1,10 +1,8 @@
 package racingCar.step5.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingCar.step5.domain.dto.CarDto;
 import racingCar.step5.domain.dto.RacingCarGameResult;
 import racingCar.step5.domain.dto.WinnerDto;
 
@@ -20,25 +18,12 @@ class RacingCarGameTest {
         cars = RacingCars.ofNames("1번,2번");
     }
 
-    @DisplayName("레이싱 게임 실행 후, 실행 결과를 알 수 있다")
-    @Test
-    void showRacingCarGameResult(){
-        RacingCarGame game = new RacingCarGame(cars, new RandomNumber(4));
-
-        RacingCarGameResult gameResult = game.start();
-
-        assertThat(gameResult.getRacingResults()).containsExactlyInAnyOrder(
-                new CarDto("1번", 1)
-                , new CarDto("2번", 1)
-        );
-    }
-
     @DisplayName("레이싱 게임 후, 우승자을 알 수 있다")
     @Test
     void getWinners(){
         RacingCarGame game = new RacingCarGame(cars, new RandomNumber(4));
 
-        RacingCarGameResult gameResult = game.start();
+        RacingCarGameResult gameResult = game.start(5);
 
         assertThat(gameResult.getWinners()).containsExactlyInAnyOrder(
                 new WinnerDto("1번")
