1215c354f936c522b188773e1c5133ad1907356e
ryumina-edu <fbalsk8897@gmail.com>
2024-10-06 00:12:13 +0900

    refactor: extract static constant

--- src/main/java/step3/domain/Name.java
+++ src/main/java/step3/domain/Name.java
@@ -1,11 +1,13 @@
 package step3.domain;
 
 public class Name {
+    private static final int ALLOW_LENGTH = 5;
+
     private final String name;
 
     public Name(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        if (name.length() > ALLOW_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 " + ALLOW_LENGTH + "자를 초과할 수 없습니다.");
         }
         this.name = name;
     }
