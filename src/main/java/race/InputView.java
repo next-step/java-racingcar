03bd69255ce05e0ddf4c77ceb6ef2d71b77d27b7
한주형 <91880235+Jureamer@users.noreply.github.com>
2024-10-05 13:47:22 +0900

    refactor: apply step4 feedback

--- src/main/java/race/InputView.java
+++ src/main/java/race/InputView.java
@@ -8,11 +8,9 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private final Scanner scanner;
-    private final String CAR_NAME_SPLIT_REGEX = "\\s*,\\s*";
-    private final int MIN_CAR_COUNT = 1;
-    private final int MAX_CAR_COUNT = 5;
-    private final int MIN_RACE_COUNT = 1;
-    private final int MAX_RACE_COUNT = 9;
+    private static final int MAX_CAR_COUNT = 5;
+    private static final int MIN_RACE_COUNT = 1;
+    private static final int MAX_RACE_COUNT = 9;
 
     public InputView(Scanner scanner) {
         this.scanner = scanner;
@@ -27,18 +25,18 @@ public class InputView {
         String carNamesString = scanner.nextLine();
         return Arrays.stream(carNamesString.split(","))
                 .map(String::trim)
-                .collect(Collectors.collectingAndThen(Collectors.toList(),
+                .collect(Collectors.collectingAndThen(Collectors.toUnmodifiableList(),
                         list -> {
                             validateCarNamesCount(list);
-                            return List.copyOf(list);
+                            return list;
                         }));
 
 
     }
 
     private void validateCarNamesCount(List<String> carNames) {
-        if (carNames.size() < MIN_CAR_COUNT || carNames.size() > MAX_CAR_COUNT) {
-            throw new IllegalArgumentException("경주 할 자동차 이름을 1~5대 사이로 입력 해 주시기 바랍니다.");
+        if (carNames.isEmpty() || carNames.size() > MAX_CAR_COUNT) {
+            throw new IllegalArgumentException("자동차는 1~5대까지 입력 가능합니다.");
         }
     }
 
