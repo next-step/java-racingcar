a332664bd0c2d5c80b0e5d7f70b39edba8170457
김혜영 <sunzero0116@gmail.com>
2024-10-05 22:25:21 +0900

    test : Cars 테스트 코드 추가

---
+++ src/test/java/racingcar/CarsTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+    @BeforeEach
+    void setUp() {
+        Cars.clear();
+    }
+
+    @Test
+    @DisplayName("차 리스트에 차를 추가하는지 확인")
+    void addCar() {
+        Car car = new Car("thanker");
+        Cars.addCar(car);
+
+        assertThat(Cars.getCars()).hasSize(1);
+        assertThat(Cars.getCars()).contains(car);
+    }
+
+    @Test
+    @DisplayName("차 리스트를 가져오는지 확인")
+    void getCars() {
+        Car car1 = new Car("thanker");
+        Car car2 = new Car("pobi");
+        Cars.addCar(car1);
+        Cars.addCar(car2);
+
+        assertThat(Cars.getCars()).hasSize(2);
+        assertThat(Cars.getCars()).containsExactly(car1, car2);
+    }
+}
