e81ffe17edf28eec5f73a74ee0b78e2777354b1b
hwan2-99 <thh1233123@gmail.com>
2024-10-05 21:20:54 +0900

    test: 우승자 구하는 테스트 작성

--- src/test/java/step3/model/CarsTest.java
+++ src/test/java/step3/model/CarsTest.java
@@ -34,17 +34,4 @@ class CarsTest {
 
         assertThat(movements).anyMatch(movement -> movement > 0);
     }
-
-    @Test
-    void 우승자_생성_확인() {
-        Car hwan2 = new Car("hwan2", new Movement(3));
-        Car chan = new Car("chan", new Movement());
-        Car ming = new Car("ming", new Movement(2));
-
-        Cars cars = new Cars(Arrays.asList(hwan2, chan, ming));
-        RaceWinners raceWinners = cars.getRaceWinners();
-
-        assertThat(raceWinners.getWinnerNames()).hasSize(1);
-        assertThat(raceWinners.getWinnerNames()).contains("hwan2");
-    }
 }
