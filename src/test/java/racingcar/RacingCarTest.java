0f49201cf38c036451f05e9ac0870c585745afa2
jara <jara0701@gmail.com>
2024-10-05 12:09:23 +0900

    refactor RacingCarTest.java: unused import 제거
    사용하지 않는 cavResource 제거

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
