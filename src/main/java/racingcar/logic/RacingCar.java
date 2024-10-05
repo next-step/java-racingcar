a5c7ed8af5e319dbe4c6f01515311f1aa6f8e1fe
jara <jara0701@gmail.com>
2024-10-05 23:17:51 +0900

    refactor RacingCar.java: carName 클래스 적용
    차 이름을 나타내는 carName 인스턴스로 변경

--- src/main/java/racingcar/logic/RacingCar.java
+++ src/main/java/racingcar/logic/RacingCar.java
@@ -7,18 +7,18 @@ import java.util.Objects;
 public class RacingCar {
     private static final int INITIAL_POSITION = 0;
 
-    private final String name;
+    private final CarName name;
     private final int carNo;
     private Position position;
 
-    private RacingCar(String name, int carNo) {
+    private RacingCar(CarName name, int carNo) {
         this.name = name;
         this.carNo = carNo;
         this.position = Position.valueOf(INITIAL_POSITION);
     }
 
     public static RacingCar valueOf(String name, int carNo) {
-        return new RacingCar(name, carNo);
+        return new RacingCar(CarName.valueOf(name), carNo);
     }
 
     public void race(MovableStrategy movableStrategy) {
@@ -28,7 +28,7 @@ public class RacingCar {
     }
 
     public String getName() {
-        return this.name;
+        return this.name.getCarName();
     }
 
     public int getCarNo() {
