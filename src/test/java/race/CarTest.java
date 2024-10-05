14d8d44c3623067e077ca6cdbc5258c1b5a1fed9
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-05 16:28:21 +0900

    refactor: Positon과 중복되는 테스트 코드 삭제
    Positon과 중복되는 테스트 코드 삭제

--- src/test/java/race/CarTest.java
+++ src/test/java/race/CarTest.java
@@ -2,23 +2,15 @@ package race;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
-
-    private ForwardCheck forwardCheckGenerator;
-
-    @BeforeEach
-    void initRandomNumber() {
-        forwardCheckGenerator = new CarForwardCheck();
-    }
 
     @Test
     void 자동차를_생성하면_전진하는_방향이_하나_생성된다() {
         Car car = new Car("tCar");
 
-        assertThat(car.getForwardResult()).isEqualTo(1);
+        assertThat(car.getPosition()).isEqualTo(1);
     }
 
     @Test
@@ -27,7 +19,7 @@ class CarTest {
 
         car.forward(4);
 
-        assertThat(car.getForwardResult()).isEqualTo(2);
+        assertThat(car.getPosition()).isEqualTo(2);
     }
 
     @Test
@@ -36,20 +28,5 @@ class CarTest {
         assertThatThrownBy(() -> new Car(carName))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("CarName is too long");
-    }
-
-    @Test
-    void 자동차_위치가_가장_멀리간_위치인지_확인한다() {
-        Car car = new Car("testC", 4);
-
-        assertThat(car.isMaxPosition(4)).isTrue();
-    }
-
-    @Test
-    void 자동차_위치와_현재_가장_멀리간_위치중에서_더_큰값을_반환한다() {
-        Car car = new Car("testC", 4);
-        int maxPosition = 5;
-
-        assertThat(car.compareWithMaxPosition(maxPosition)).isEqualTo(5);
     }
 }
