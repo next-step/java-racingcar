3951bda25e0ab33a4dc454b0c95e4391a8a32fd7
hoya <sv05159@naver.com>
2024-10-05 22:07:23 +0900

    refactor: 개행문자 변경
    - System.lineSeparator() 를 사용하면 OS 관계없이 처리가능

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -6,6 +6,7 @@ public class ResultView {
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
     private static final String CAR_POSITION_MARKER = "-";
     public static final String WINNER_ANNOUNCEMENT_MESSAGE = "가 최종 우승했습니다.";
+    public static final String LINE_SEPARATOR = System.lineSeparator();
 
     private ResultView() {
         throw new UnsupportedOperationException("Utility class");
@@ -13,7 +14,7 @@ public class ResultView {
 
     public static void printHistory(RaceResult raceResult, List<Car> winners) {
         StringBuilder result = new StringBuilder();
-        result.append("\n").append(EXECUTION_RESULT_MESSAGE).append("\n");
+        result.append(LINE_SEPARATOR).append(EXECUTION_RESULT_MESSAGE).append(LINE_SEPARATOR);
         for (AttemptResult attemptResult : raceResult.getAttemptResults()) {
             appendPosition(attemptResult, result);
         }
@@ -37,8 +38,8 @@ public class ResultView {
 
     private static void appendPosition(AttemptResult attemptResult, StringBuilder result) {
         for (Car car : attemptResult.getCars()) {
-            result.append(car.getName()).append(" : ").append(CAR_POSITION_MARKER.repeat(Math.max(0, car.getPosition()))).append("\n");
+            result.append(car.getName()).append(" : ").append(CAR_POSITION_MARKER.repeat(Math.max(0, car.getPosition()))).append(LINE_SEPARATOR);
         }
-        result.append("\n");
+        result.append(LINE_SEPARATOR);
     }
 }
