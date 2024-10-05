611b3a5f5936f3e63561461b602786b559b64d13
hoya <sv05159@naver.com>
2024-10-05 17:26:08 +0900

    refactor: 매직넘버 상수 추출

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -4,10 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
+    public static final String DELIMITER = ",";
+
     private final MoveStrategy moveStrategy;
 
     public RacingGame(MoveStrategy moveStrategy) {
         this.moveStrategy = moveStrategy;
+    }
+
+    public List<Car> createCars(String carNames) {
+        List<Car> cars = new ArrayList<>();
+        String[] names = carNames.split(DELIMITER);
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+        return cars;
     }
 
     public RaceResult race(List<Car> cars, int attemptCount) {
@@ -26,14 +37,5 @@ public class RacingGame {
             positions.add(new Car(car.getPosition(), car.getName()));
         }
         return new AttemptResult(positions);
-    }
-
-    public List<Car> createCars(String carNames) {
-        List<Car> cars = new ArrayList<>();
-        String[] names = carNames.split(",");
-        for (String name : names) {
-            cars.add(new Car(name));
-        }
-        return cars;
     }
 }
