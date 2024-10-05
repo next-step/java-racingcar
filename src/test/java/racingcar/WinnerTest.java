814d248e47491ef80cec4c3add8c999a4a114078
김혜영 <sunzero0116@gmail.com>
2024-10-05 18:03:58 +0900

    chore : Winner 테스트 메소드명 변경

--- src/test/java/racingcar/WinnerTest.java
+++ src/test/java/racingcar/WinnerTest.java
@@ -11,22 +11,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 class WinnerTest {
     @Test
     @DisplayName("우승자를 가져오는지 확인")
-    void getWinner() {
+    void getWinners() {
         RacingGame.cars = new ArrayList<>();
         Car car1 = new Car("thanker");
         Car car2 = new Car("pobi");
         RacingGame.cars.add(car1);
         RacingGame.cars.add(car2);
+
         car1.move(true);
         car2.move(false);
 
         List<Car> winners = Winner.getWinners();
         assertThat(winners).hasSize(1);
-        assertThat(winners).extracting(Car::getName).containsExactly("thanker");
+        assertThat(winners).extracting(Car::getName)
+                           .containsExactly("thanker");
     }
+
     @Test
     @DisplayName("우승자 여러명을 가져오는지 확인")
-    void getWinners() {
+    void getWinners_여러명() {
         RacingGame.cars = new ArrayList<>();
         Car car1 = new Car("thanker");
         Car car2 = new Car("pobi");
@@ -41,6 +44,7 @@ class WinnerTest {
 
         List<Car> winners = Winner.getWinners();
         assertThat(winners).hasSize(2);
-        assertThat(winners).extracting(Car::getName).containsExactlyInAnyOrder("thanker", "pobi");
+        assertThat(winners).extracting(Car::getName)
+                           .containsExactlyInAnyOrder("thanker", "pobi");
     }
 }
