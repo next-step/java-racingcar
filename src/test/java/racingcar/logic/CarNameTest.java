ec7a0ef1c6e52bd603441ab81be23873d15b3f6c
jara <jara0701@gmail.com>
2024-10-05 23:09:34 +0900

    test CarName.java: CarName 테스트 작성
    carName 필드를 포장하는 CarName 클래스의 테스트 작성

---
+++ src/test/java/racingcar/logic/CarNameTest.java
@@ -0,0 +1,45 @@
+package racingcar.logic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class CarNameTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"car1", "car2", "car3"})
+    @DisplayName("생성하면 차 이름을 가지고 있습니다.")
+    public void getCarNameTest(String carName) {
+        CarName carNameInstance = CarName.valueOf(carName);
+        Assertions.assertEquals(carName, carNameInstance.getCarName());
+    }
+
+    @Test
+    @DisplayName("다섯 글자 이상의 차 이름을 생성 시도 시, 에러가 발생합니다")
+    public void tooLongCarNameTest() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            CarName.valueOf("1234567");
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"car1", "car2", "car3"})
+    @DisplayName("같은 이름을 가지면 equals()가 true다")
+    public void equalsTest(String carName) {
+        CarName carNameInstance1 = CarName.valueOf(carName);
+        CarName carNameInstance2 = CarName.valueOf(carName);
+        Assertions.assertEquals(carNameInstance1, carNameInstance2);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"car1", "car2", "car3"})
+    @DisplayName("같은 position 을 가지면 같은 hashCode()가 true다")
+    public void hashCodeTest(String carName) {
+        CarName carNameInstance1 = CarName.valueOf(carName);
+        CarName carNameInstance2 = CarName.valueOf(carName);
+        Assertions.assertEquals(carNameInstance1.hashCode(), carNameInstance2.hashCode());
+    }
+}
