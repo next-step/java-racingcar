2109f1ee8437fc2c0ab984a063ce09cebeea0c03
hoya <sv05159@naver.com>
2024-10-05 22:09:29 +0900

    refactor: 문맥 분리
    - 변수 할당, 함수 호출, 결과 출력 등 의미적으로 분리되는 코드간에 개행으로 문맥 분리하면 가독성을 높일 수 있음.

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -6,10 +6,12 @@ public class RacingCarApplication {
     public static void main(String[] args) {
         String carNames = InputView.getCarCount();
         int attemptCount = InputView.getAttemptCount();
+
         RacingGame racingGame = new RacingGame(new FourOrMore(new SystemRandomHolder()));
         List<Car> cars = racingGame.createCars(carNames);
         RaceResult raceResult = racingGame.race(cars, attemptCount);
         List<Car> winners = racingGame.getWinners(raceResult.getLastAttemptResult().getCars());
+
         ResultView.printHistory(raceResult, winners);
     }
 }
