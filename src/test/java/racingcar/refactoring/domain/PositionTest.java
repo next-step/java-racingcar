7405d08901d31ffb1556f0b319b79f2d54577f0e
Joonkb <byeongjunkim@naver.com>
2024-10-06 16:16:48 +0900

    [refactor] 원시값 포장을 위한 Position 객체 생성

--- src/test/java/racingcar/refactoring/domain/PositionTest.java
+++ src/test/java/racingcar/refactoring/domain/PositionTest.java
@@ -1,4 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.refactoring.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class PositionTest {
-  
+
+    @Test
+    @DisplayName("Position이 1이 더해졌는지 확인한다.")
+    void addPositionTest() {
+        Position position = new Position(0);
+        assertThat(position.addPosition()).isEqualTo(new Position(1));
+    }
 }
