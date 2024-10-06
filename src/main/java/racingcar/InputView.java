4297b545ca64f3e2e65a77d090f91bc6310f4fd3
ming <devel.moon17@gmail.com>
2024-10-07 00:56:16 +0900

    refactor: 단위테스트 4단계 - 불필요한 코드 정리

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -6,15 +6,9 @@ public class InputView {
 
     private static Scanner scanner = new Scanner(System.in);
 
-    public static int inputCarCount() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return scanner.nextInt();
-    }
-
     public static String[] inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)");
-        String inputName = scanner.nextLine();
-        return inputName.split(",");
+        return scanner.nextLine().split(",");
     }
 
     public static int inputLoop() {
