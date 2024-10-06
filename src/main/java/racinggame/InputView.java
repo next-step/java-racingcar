e0e65b85dd6ac5d0e6045cc266cde21f521816f8
hunko <rhwlgns4386@naver.com>
2024-10-06 16:52:11 +0900

    refactor : 로직 함수화

--- src/main/java/racinggame/InputView.java
+++ src/main/java/racinggame/InputView.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public static InputDto read(){
-        System.out.println("자동차 대수는 몇 대 인가요?");
+    public static InputDto readCount() {
         Scanner scanner = new Scanner(System.in);
-        int carCount = scanner.nextInt();
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int repeat = scanner.nextInt();
-        return new InputDto(carCount,repeat);
+        int carCount = readCount("자동차 대수는 몇 대 인가요?", scanner);
+        int repeat = readCount("시도할 회수는 몇 회 인가요?", scanner);
+        return new InputDto(carCount, repeat);
+    }
+
+    private static int readCount(String message, Scanner scanner) {
+        System.out.println(message);
+        return  scanner.nextInt();
     }
 
 }
--- src/main/java/racinggame/RacingMain.java
+++ src/main/java/racinggame/RacingMain.java
@@ -8,18 +8,22 @@ import racinggame.SnapShotStore.SnapShot;
 public class RacingMain {
 
     public static void main(String[] args) {
-        InputDto read = InputView.read();
-        List<List<Integer>> radoms=new ArrayList<>();
-        for(int count=0;count< read.getRepeatCount();count++){
-            radoms.add(carCapercity(read.getCarCount()));
-        }
+        InputDto read = InputView.readCount();
         RacingGame racingGame = new RacingGame(read.getCarCount());
-        racingGame.start(radoms);
+        racingGame.start(radoms(read.getRepeatCount(),read.getCarCount()));
         SnapShot snapShot = racingGame.matchResult();
         OutPutView.render(snapShot);
     }
 
-    private static List<Integer> carCapercity(int carCount) {
+    private static List<List<Integer>> radoms(int repeatCount,int carCount) {
+        List<List<Integer>> radoms=new ArrayList<>();
+        for(int count=0;count< repeatCount;count++){
+            radoms.add(carCapacity(carCount));
+        }
+        return radoms;
+    }
+
+    private static List<Integer> carCapacity(int carCount) {
         Random random = new Random();
         List<Integer> result = new ArrayList<>();
         for(int count=0;count< carCount;count++){
