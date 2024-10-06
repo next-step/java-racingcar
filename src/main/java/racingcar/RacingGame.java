46f7574c2c70370bdda73f90cdbeab1c66c93dab
김혜영 <sunzero0116@gmail.com>
2024-10-06 18:41:59 +0900

    feat : 순서 변경

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -3,8 +3,8 @@ package racingcar;
 public class RacingGame {
     public static void main(String[] args) {
         String[] carNames = InputView.getCarName();
-        int racingCount = InputView.getRacingCount();
         Race race = new Race(carNames);
+        int racingCount = InputView.getRacingCount();
         race.run(racingCount);
     }
 }
