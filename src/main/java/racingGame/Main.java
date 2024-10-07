bbbce6a5febe4e2b606b8d0ab9e1b7d1b61442e0
pum005 <pum005@naver.com>
2024-10-08 00:04:00 +0900

    refactor : main

--- src/main/java/racingGame/Main.java
+++ src/main/java/racingGame/Main.java
@@ -1,14 +1,10 @@
 package racingGame;
 
 import racingGame.controller.RacingGame;
-import racingGame.ui.input.ConsoleInputView;
-import racingGame.ui.input.InputView;
 
 public class Main {
     public static void main(String[] args) {
-        InputView inputView = new ConsoleInputView();
-
-        RacingGame game = new RacingGame(inputView.getCarCount(), inputView.getMoveCount());
+        RacingGame game = new RacingGame();
 
         game.play();
     }
