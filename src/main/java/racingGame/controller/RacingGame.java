60abc28b4f5732cd50bd52042dc0ed746c0798c3
pum005 <pum005@naver.com>
2024-10-07 23:57:55 +0900

    refactor : controller stateless class 로 변경

--- src/main/java/racingGame/controller/RacingGame.java
+++ src/main/java/racingGame/controller/RacingGame.java
@@ -1,48 +1,28 @@
 package racingGame.controller;
 
-import racingGame.model.car.Car;
+import racingGame.model.car.CarMovement;
+import racingGame.model.car.TryNo;
 import racingGame.service.RacingGameService;
+import racingGame.ui.input.ConsoleInputView;
+import racingGame.ui.input.InputView;
 import racingGame.ui.output.ConsoleOutputView;
 import racingGame.ui.output.OutputView;
 
-import java.util.List;
-
 public class RacingGame {
 
-    private static final int MINIMUM_MOVE_COUNT = 1;
+    private final RacingGameService gameService = new RacingGameService();
+    private final InputView input = new ConsoleInputView();
+    private final OutputView output = new ConsoleOutputView();
 
-    private static final String MOVE_COUNT_ERROR_MESSAGE = "최소 1회 이상은 입력해주세요!";
-
-    private int moveCount;
-    private int round;
-    private final RacingGameService gameService;
-    private final OutputView output;
-
-    public RacingGame(int carCount, int moveCount) {
-        validateMoveCount(moveCount);
-
-        this.gameService = new RacingGameService(carCount);
-        this.output = new ConsoleOutputView();
-        this.moveCount = moveCount;
-        this.round = 0;
-    }
+    public RacingGame() {}
 
     public void play() {
+        int carCount = input.getCarCount();
+        TryNo moveCount = new TryNo(input.getMoveCount());
+
         output.printInitMessage();
-        while (round < moveCount) {
-            List<Car> cars =  gameService.moveAllCars();
-            output.printCarGraph(cars);
-            round++;
-        }
+        CarMovement carMovement = gameService.moveCarsForAllRounds(carCount, moveCount);
+        output.printCarGraph(carMovement);
     }
 
-    public int getRound() {
-        return this.round;
-    }
-
-    private void validateMoveCount(int moveCount) {
-        if (moveCount < MINIMUM_MOVE_COUNT) {
-            throw new IllegalArgumentException(MOVE_COUNT_ERROR_MESSAGE);
-        }
-    }
 }
