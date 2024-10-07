8c386f3da80403275cc3964d4d2c6fa2ef4b618c
pum005 <pum005@naver.com>
2024-10-07 23:58:36 +0900

    refactor : service stateless class 로 변경

--- src/main/java/racingGame/service/RacingGameService.java
+++ src/main/java/racingGame/service/RacingGameService.java
@@ -1,47 +1,39 @@
 package racingGame.service;
 
+import racingGame.model.car.Cars;
+import racingGame.model.car.CarMovement;
 import racingGame.model.car.Car;
+import racingGame.model.car.TryNo;
 import racingGame.model.numbergenerator.NumberGenerator;
 import racingGame.model.numbergenerator.RandomNumberGenerator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class RacingGameService {
 
-    private static final String CAR_COUNT_ERROR_MESSAGE = "최소 2대 이상은 입력해주세요!";
+    private static final int MINIMUM_MOVEMENT_VALUE = 4;
 
-    private static final int MINIMUM_CAR_COUNT = 2;
+    private final NumberGenerator numberGenerator = new RandomNumberGenerator();
 
-    private List<Car> cars;
-    private NumberGenerator numberGenerator;
+    public RacingGameService() {}
 
-    public RacingGameService(int carCount) {
-        validateCarCount(carCount);
-        cars = createCars(carCount);
-        this.numberGenerator = new RandomNumberGenerator();
-    }
+    public CarMovement moveCarsForAllRounds(int carCount, TryNo tryNo) {
+        Cars cars = new Cars(carCount);
+        CarMovement ret = new CarMovement();
 
-    public List<Car> moveAllCars() {
-        for (Car car : cars) {
-            int number = numberGenerator.generateNumber();
-            car.move(number);
+        for (int i = 0; i < tryNo.getTryNo(); i++) {
+            Cars car = moveCarsByOneStep(cars);
+            ret.addOneStepProgress(car);
         }
-        return cars;
-    }
 
-    private List<Car> createCars(int carCount) {
-        List<Car> ret = new ArrayList<>();
-        for (int i = 0 ; i < carCount; i++) {
-            ret.add(new Car());
-        }
         return ret;
     }
 
-    private void validateCarCount(int carCount) {
-        if (carCount < MINIMUM_CAR_COUNT) {
-            throw new IllegalArgumentException(CAR_COUNT_ERROR_MESSAGE);
+    private Cars moveCarsByOneStep(Cars cars) {
+        for (Car car : cars.getCarGroup()) {
+            car.move(() ->
+                numberGenerator.generateNumber() >= MINIMUM_MOVEMENT_VALUE
+            );
         }
+        return cars;
     }
 
 }
--- src/test/java/racingGame/service/RacingGameServiceTest.java
+++ src/test/java/racingGame/service/RacingGameServiceTest.java
@@ -2,40 +2,32 @@ package racingGame.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingGame.model.car.Car;
-
-import java.util.List;
+import racingGame.model.car.CarMovement;
+import racingGame.model.car.TryNo;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class RacingGameServiceTest {
 
     @ParameterizedTest
     @ValueSource(ints = {2, 3, 4, 5, 6, 7})
-    @DisplayName("생성자 메소드는 자동차 대수가 유효할 때, 객체를 생성할 수 있다.")
-    public void 생성자_성공_테스트(final int carCount) {
-        assertDoesNotThrow(() -> new RacingGameService(carCount));
+    @DisplayName("생성자 메소드는 항상 객체를 생성할 수 있다.")
+    public void 생성자_성공_테스트() {
+        assertDoesNotThrow(() -> new RacingGameService());
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 1})
-    @DisplayName("생성자 메소드는 자동차 대수가 유효하지 않으면, 에러를 반환한다.")
-    public void 생성자_실패_테스트(final int carCount) {
-        assertThatThrownBy(() -> new RacingGameService(carCount))
-                .isInstanceOf(RuntimeException.class);
+    @CsvSource(value = {"2:1", "3:4", "2:10"}, delimiter = ':')
+    @DisplayName("moveCarsForAllRounds 메소드는 입력한 라운드 수만큼, 시도 되어야 한다.")
+    public void moveCarsForAllRounds_메소드_테스트(final int carCount, final int tryNo) {
+        RacingGameService gameService = new RacingGameService();
+
+        CarMovement expected = gameService.moveCarsForAllRounds(carCount, new TryNo(tryNo));
+
+        assertThat(expected.getProgress()).hasSize(tryNo);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {2, 3 ,5})
-    @DisplayName("moveAllCars 메소드는 자동차 대수 만큼, 객체를 반환한다.")
-    public void moveAllCars_메소드_테스트(final int carCount) {
-        RacingGameService gameService = new RacingGameService(carCount);
-
-        List<Car> expected = gameService.moveAllCars();
-
-        assertThat(expected).hasSize(carCount);
-    }
 }
