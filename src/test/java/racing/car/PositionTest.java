9dcfc84228aa66d068fc6bd5173f4fe0618e33a0
kimsuyeondev <rhaehl@naver.com>
2024-10-09 00:21:03 +0900

    git 충돌해결

--- src/test/java/racing/car/PositionTest.java
+++ src/test/java/racing/car/PositionTest.java
@@ -27,4 +27,15 @@ class PositionTest {
         assertThat(position.increase()).isEqualTo(new Position(5));
     }
 
+    @Test
+    void test(){
+        Car car1 = new Car("ㅁㅁ", 0);
+        car1.isGo(7);
+        Car car2 = car1;
+        car2.isGo(7);
+
+        assertThat(car1.getPosition()).isEqualTo(1);
+        assertThat(car2.getPosition()).isEqualTo(2);
+    }
+
 }
