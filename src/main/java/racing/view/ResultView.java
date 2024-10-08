bcd94eebe1beac12ae73138c07f7db5e18281c1d
kimsuyeondev <rhaehl@naver.com>
2024-10-09 00:18:42 +0900

    git 충돌해결

--- src/main/java/racing/view/ResultView.java
+++ src/main/java/racing/view/ResultView.java
@@ -5,7 +5,7 @@ import racing.domain.car.Cars;
 import racing.domain.result.ResultRacing;
 import racing.domain.result.ResultRacings;
 
-public class ResultView implements ResultHandler {
+public class ResultView {
 
     public void racingPrint(ResultRacings resultRacings, int cycle) {
         int round = 0;
@@ -28,12 +28,10 @@ public class ResultView implements ResultHandler {
         System.out.println();
     }
 
-    @Override
     public void endCycle() {
         System.out.println();
     }
 
-    @Override
     public void winnerPrint(Cars winner) {
         System.out.println("우승자 발표!");
         for (Car car : winner.getItems()) {
