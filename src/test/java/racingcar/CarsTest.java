7e56e548f8477321d0db5dcce10af8f0f167cfcd
ming <devel.moon17@gmail.com>
2024-10-09 01:34:56 +0900

    refactor: 단위테스트 4단계 피드백 반영 - 경기 진행 테스트 코드 작성

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -20,6 +20,14 @@ public class CarsTest {
     }
 
     @Test
+    @DisplayName("입력한 수만큼 라운드를 진행하고, 자동차 수만큼 결과값을 기록한다.")
+    void 경기_진행() {
+        Cars cars = Cars.fromCarNames(Arrays.asList("name", "name2", "name3"));
+        assertThat(cars.runRace(5)).hasSize(5);
+        assertThat(cars.runRace(5).get(0)).hasSize(3);
+    }
+
+    @Test
     @DisplayName("자동차 중 1명 이상의 우승자를 구한다.")
     void 우승자_구하기() {
         List<Car> carList = new ArrayList<>();
