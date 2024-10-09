ea596ee7232fa1b40f95123df22c59ad92976c32
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-09 12:11:08 +0900

    test : 자동차 이름이 5글자 이상인 경우 에러를 반환하는지 확인하는 테스트

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,8 +3,29 @@ package racingcar;
 public class Car {
 
     public static final int MOVE_CONDITION = 4;
+    private static final int MAX_NAME_LENGTH = 5;
+    private final String name;
 
     private int currentPosition;
+
+    public Car(final String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, int position) {
+        validationCarName(name);
+        this.name = name.trim();
+        this.currentPosition = position;
+    }
+
+    private static void validationCarName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("자동차 이름은 값이 존재해야 합니다.");
+        }
+        if (name.trim().length() >= MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5 미만의 글자여야 합니다.");
+        }
+    }
 
     public int move(int randomNumber) {
         if (randomNumber >= MOVE_CONDITION) {
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -3,28 +3,38 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.Random;
-import java.util.stream.IntStream;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingCarTest {
 
-    @Test
+    @ParameterizedTest
     @DisplayName("자동차가 이동하는 값이 4 이상일 경우 전진하는지 확인")
-    void carMovesWhenFourOrMore() {
-        Car car = new Car();
+    @ValueSource(strings = {"yun"})
+    void carMovesWhenFourOrMore(String name) {
+        Car car = new Car(name);
         car.move(4);
         assertThat(car.getCurrentPosition()).isEqualTo(1);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("자동차가 이동하는 값이 4 이하일 경우 멈춰있는지 확인")
-    void carStopWhenLessThanFour() {
-        Car car = new Car();
+    @ValueSource(strings = {"yun"})
+    void carStopWhenLessThanFour(String name) {
+        Car car = new Car(name);
         car.move(3);
         assertThat(car.getCurrentPosition()).isEqualTo(0);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"longName"})
+    @DisplayName("자동차 이름이 5글자 이상인 경우 에러를 반환하는지 확인")
+    void carNameThrowsExceptionWhenTooLong(String name) {
+        assertThatThrownBy(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5 미만의 글자여야 합니다.");
+    }
+
 }
