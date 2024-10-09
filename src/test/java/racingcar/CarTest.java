f1813302ff465c418fa117672f656a354fbb03c3
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-09 22:21:04 +0900

    refactor : 각 도메인에 맞게 테스트 분리

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,17 +1,13 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class RacingCarTest {
+public class CarTest {
 
     @ParameterizedTest
     @DisplayName("자동차가 이동하는 값이 4 이상일 경우 전진하는지 확인")
@@ -38,20 +34,6 @@ public class RacingCarTest {
         assertThatThrownBy(() -> new Car(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 5 미만의 글자여야 합니다.");
-    }
-
-    @Test
-    @DisplayName("가장 많은 거리를 간 자동차를 우승자로 반환하는지 확인")
-    void findWinner() {
-        Car testCar1 = new Car("yun1", 3);
-        Car testCar2 = new Car("yun2", 2);
-        Car testCar3 = new Car("yun3", 1);
-        List<Car> cars = Arrays.asList(testCar1, testCar2, testCar3);
-
-        List<Car> winners = Winners.findWinners(cars);
-
-        assertThat(winners).hasSize(1);
-        assertThat(winners).contains(testCar1);
     }
 
 }
