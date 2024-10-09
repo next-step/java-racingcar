1854dceb132a9228fa72449d32e4cf97bef5d7fe
BananMoon <younji1115@gmail.com>
2024-10-09 22:17:16 +0900

    refactor: extracting API 활용하여 객체의 값을 비교한다. (단, dto 클래스는 단위테스트 대상이 아니다.)

--- src/test/java/carracing/domain/carracing/dto/CarsMoveStatusHistoryTest.java
+++ src/test/java/carracing/domain/carracing/dto/CarsMoveStatusHistoryTest.java
@@ -2,13 +2,17 @@ package carracing.domain.carracing.dto;
 
 import carracing.domain.carracing.Car;
 import carracing.domain.carracing.Cars;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
+/**
+ * dto 클래스는 단위 테스트 대상이 아니다.
+ */
 class CarsMoveStatusHistoryTest {
     @DisplayName("자동차 경주 시도 횟수 별 차들의 이동 상태를 저장한다.")
     @Test
@@ -37,11 +41,11 @@ class CarsMoveStatusHistoryTest {
         Cars carsByFirstMoveTry = carsMoveStatusHistory.carsBy(0);
         Cars carsBySecondMoveTry = carsMoveStatusHistory.carsBy(1);
 
-        assertThat(carsByFirstMoveTry.get()).hasSize(2);
-        assertThat(carsBySecondMoveTry.get()).hasSize(2);
-        assertThat(carsByFirstMoveTry.get().get(0).isSamePosition(0)).isTrue();
-        assertThat(carsByFirstMoveTry.get().get(1).isSamePosition(0)).isTrue();
-        assertThat(carsBySecondMoveTry.get().get(0).isSamePosition(1)).isTrue();
-        assertThat(carsBySecondMoveTry.get().get(1).isSamePosition(0)).isTrue();
+        assertThat(carsByFirstMoveTry).extracting("cars", as(InstanceOfAssertFactories.LIST))
+                .extracting("name.value", "position.value")
+                .contains(tuple("moon", 0), tuple("zi", 0));
+        assertThat(carsBySecondMoveTry).extracting("cars", as(InstanceOfAssertFactories.LIST))
+                .extracting("name.value", "position.value")
+                .contains(tuple("moon", 1), tuple("zi", 0));
     }
 }
