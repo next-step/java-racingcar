b26765b28e4e96408eb6f6586670b951bf5779c0
jara <jara0701@gmail.com>
2024-10-09 12:34:34 +0900

    refactor RacingCarSimulator.java: 주요 로직을 carRacing 으로 옮김
    simulator 로직 대부분을 carRacing 사용하도록 변경

--- src/main/java/racingcar/domain/RacingCarSimulator.java
+++ src/main/java/racingcar/domain/RacingCarSimulator.java
@@ -1,75 +1,19 @@
 package racingcar.domain;
 
-import racingcar.dto.RacingCarStatesDTO;
 import racingcar.dto.RacingResultDTO;
-import racingcar.dto.RacingWrapResultDTO;
-import racingcar.domain.movableStrategy.RandomMovableStrategy;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class RacingCarSimulator {
     public static RacingResultDTO simulate(List<String> carNames, int tryNumber) {
-        List<RacingCar> racingCars = createRacingCars(carNames);
-        return simulateRacing(tryNumber, racingCars);
+        CarRacing carRacing = CarRacing.valueOf(carNames);
+        proceedRacing(tryNumber, carRacing);
+        return RacingResultDTO.valueOf(carRacing);
     }
 
-    private static RacingResultDTO simulateRacing(int tryNumber, List<RacingCar> racingCars) {
-        List<RacingWrapResultDTO> wrapResults = proceedWraps(tryNumber, racingCars);
-        List<String> winners = findWinners(racingCars);
-        return RacingResultDTO.valueOf(winners, wrapResults);
-    }
-
-    private static List<RacingWrapResultDTO> proceedWraps(int tryNumber, List<RacingCar> racingCars) {
-        List<RacingWrapResultDTO> wrapResults = initializeWrapResults(racingCars);
+    private static void proceedRacing(int tryNumber, CarRacing carRacing) {
         for (int i = 0; i < tryNumber; i++) {
-            wrapResults.add(simulateWrap(i + 1, racingCars));
+            carRacing.proceedWrap();
         }
-        return wrapResults;
-    }
-
-    private static List<RacingWrapResultDTO> initializeWrapResults(List<RacingCar> racingCars) {
-        List<RacingWrapResultDTO> wrapResults = new ArrayList<>();
-        wrapResults.add(RacingWrapResultDTO.valueOf(0, RacingCarStatesDTO.valueOf(racingCars)));
-        return wrapResults;
-    }
-
-    private static List<String> findWinners(List<RacingCar> racingCars) {
-        int topDistance = findTopDistance(racingCars);
-        return findCarsWithTopDistance(racingCars, topDistance);
-    }
-
-    private static List<String> findCarsWithTopDistance(List<RacingCar> racingCars, int topDistance) {
-        List<String> carsWithTopDistance = new ArrayList<>();
-        for (RacingCar car : racingCars) {
-            updateCarsWithTopDistance(topDistance, car, carsWithTopDistance);
-        }
-        return carsWithTopDistance;
-    }
-
-    private static void updateCarsWithTopDistance(int topDistance, RacingCar car, List<String> carsWithTopDistance) {
-        if (car.isSamePosition(topDistance)) {
-            carsWithTopDistance.add(car.getName());
-        }
-    }
-
-    private static int findTopDistance(List<RacingCar> racingCars) {
-        return Collections.max(racingCars, Comparator.comparingInt(RacingCar::getPosition)).getPosition();
-    }
-
-    private static RacingWrapResultDTO simulateWrap(int wrapNo, List<RacingCar> racingCars) {
-        racingCars.forEach(racingCar -> racingCar.race(RandomMovableStrategy.getInstance()));
-        RacingCarStatesDTO carStates = RacingCarStatesDTO.valueOf(racingCars);
-        return RacingWrapResultDTO.valueOf(wrapNo, carStates);
-    }
-
-    private static List<RacingCar> createRacingCars(List<String> carNames) {
-        List<RacingCar> racingCars = new ArrayList<>();
-        for (int carNumber = 0; carNumber < carNames.size(); carNumber++) {
-            racingCars.add(RacingCar.valueOf(carNames.get(carNumber), carNumber));
-        }
-        return racingCars;
     }
 }
