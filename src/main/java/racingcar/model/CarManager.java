e4ea980f9dfffba998b7ed24b4cafc67d5bc7f90
jeonga1022 <pja1048@gmail.com>
2024-10-09 22:35:03 +0900

    feat: 차이름 중복 금지

--- src/main/java/racingcar/model/CarManager.java
+++ src/main/java/racingcar/model/CarManager.java
@@ -1,7 +1,9 @@
 package racingcar.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarManager {
     private final List<Car> cars;
@@ -9,9 +11,32 @@ public class CarManager {
 
 
     public CarManager(List<Car> cars) {
+        validate(cars);
+
         this.cars = cars;
     }
 
+    private void validate(List<Car> cars) {
+        if (cars == null || cars.isEmpty()) {
+            throw new IllegalArgumentException("차는 최소 1대 이상 필요 합니다.");
+        }
+
+        validateUniqueCarName(cars);
+    }
+
+    private void validateUniqueCarName(List<Car> cars) {
+        Set<String> names = new HashSet<>();
+
+        for (Car car : cars) {
+            hasDuplicateNames(names, car.getName());
+        }
+    }
+
+    private void hasDuplicateNames(Set<String> names, String name) {
+        if (!names.add(name)) {
+            throw new IllegalArgumentException("차 이름 \"" + name + "\"이 중복되었습니다.");
+        }
+    }
 
     public List<Car> getCars() {
         return cars;
