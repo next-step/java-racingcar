0138a245eb57dcfdfb9783150bec298592d6df13
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-10-10 11:06:53 +0900

    ## refactor : InputView 클래스 수정 1. 입력이 빈문자열인 경우, 입력에 공백이 포함된 경우에 대한 처리 로직 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,14 +9,16 @@ public class InputView {
 
     public String[] enterCarNames() {
         String[] enteredCarNames = new String[0];
-        boolean vaildCarName = false;
+        boolean validCarName = false;
 
-        while (!vaildCarName) {
+        while (!validCarName) {
             System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분");
-            enteredCarNames = scanner.next().split(",");
-            vaildCarName = checkCarNameLength(enteredCarNames);
+
+            enteredCarNames = scanner.nextLine().split(",");
+            removeSpaces(enteredCarNames);
+            validCarName = isValidCarName(enteredCarNames);
         }
-        
+
         return enteredCarNames;
     }
 
@@ -25,12 +27,26 @@ public class InputView {
         return scanner.nextInt();
     }
 
-    private boolean checkCarNameLength(String[] enteredCarNames) {
+    private boolean isValidCarName(String[] enteredCarNames) {
         for (String carName : enteredCarNames) {
-            return carName.length() < LIMIT_CAR_NAME_LENGTH;
+            if (carName.isEmpty()) {
+                System.out.println("이름은 비어있을 수 없습니다. 다시 입력해주세요");
+                return false;
+            }
+
+            if(carName.length() > LIMIT_CAR_NAME_LENGTH) {
+                System.out.println("이름은 5글자를 초과할 수 없습니다. 다시 입력해주세요");
+                return false;
+            }
         }
-        
+
         return true;
     }
-    
+
+    private void removeSpaces(String[] enteredCarNames) {
+        for (int i = 0; i < enteredCarNames.length; i++) {
+            enteredCarNames[i] = enteredCarNames[i].trim();
+        }
+    }
+
 }
