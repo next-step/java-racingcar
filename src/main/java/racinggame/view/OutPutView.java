45faf3c9297c5f28854a0685867ee21fcd62b8c1
hunko <rhwlgns4386@naver.com>
2024-10-13 14:34:10 +0900

    refactor : 출력 로직 함수화

--- src/main/java/racinggame/view/OutPutView.java
+++ src/main/java/racinggame/view/OutPutView.java
@@ -1,29 +1,36 @@
 package racinggame.view;
 
 import racinggame.dto.CarDto;
-import racinggame.dto.History;
 import racinggame.dto.Result;
 
 public class OutPutView {
 
-    private static final String OUTPUT_CHAR = "-";
+    private static final String WINNER_COUNT_CHAR = "-";
 
     public static void render(Result result) {
         System.out.println("실행 결과");
+        renderHistory(result);
+        renderWinnerName(result);
+    }
+
+    private static void renderHistory(Result result) {
         for (int count = 0; count < result.repeatCount(); count++) {
             renderCars(result, count);
         }
-        System.out.println(String.format("%s가 최종 우승했습니다.",String.join(",",result.getWinner())));
+    }
+
+    private static void renderWinnerName(Result result) {
+        System.out.println(String.format("%s가 최종 우승했습니다.", String.join(",", result.getWinner())));
     }
 
     private static void renderCars(Result result, int count) {
         for (CarDto carDto : result.getSnapShot(count)) {
-            System.out.println(String.format("%s : %s",carDto.name(),renderHyphen(carDto.position())));
+            System.out.println(String.format("%s : %s", carDto.name(), renderWinnerCount(carDto.position())));
         }
         System.out.println();
     }
 
-    private static String renderHyphen(int count) {
-        return OUTPUT_CHAR.repeat(count);
+    private static String renderWinnerCount(int count) {
+        return WINNER_COUNT_CHAR.repeat(count);
     }
 }
