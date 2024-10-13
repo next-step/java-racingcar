d99edb91d8c7c5635a4696554ef5c1ba6c0c7759
hunko <rhwlgns4386@naver.com>
2024-10-13 09:17:08 +0900

    feat : 자동차 이름 입력 및 검증 기능 구현

--- src/main/java/racinggame/domain/Car.java
+++ src/main/java/racinggame/domain/Car.java
@@ -8,13 +8,18 @@ public class Car {
     private static final int DEFAULT_POSITION = 1;
 
     private int position;
+    private String name;
 
-    public Car(int position) {
+    public Car(int position, String name) {
+        if (name == null || name.isBlank() || 4 < name.length()) {
+            throw new IllegalArgumentException("자동차의 이름의 길이는 1이상 4이하여야 합니다.");
+        }
         this.position = position;
+        this.name = name;
     }
 
-    public static Car defaultCar(){
-        return new Car(DEFAULT_POSITION);
+    public static Car defaultCar() {
+        return new Car(DEFAULT_POSITION, "");
     }
 
     public void move(int number) {
--- src/test/java/racinggame/domain/CarTest.java
+++ src/test/java/racinggame/domain/CarTest.java
@@ -1,19 +1,30 @@
 package racinggame.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import racinggame.domain.Car;
 
 public class CarTest {
 
     @ParameterizedTest
     @CsvSource(value = {"4,1","3,0"})
     public void 자동차_전진테스트(int capacity,int position) {
-        Car car = new Car(0);
+        String name = "test";
+        Car car = new Car(0, name);
         car.move(capacity);
-        assertThat(car).isEqualTo(new Car(position));
+        assertThat(car).isEqualTo(new Car(position, name));
+    }
+
+    @Test
+    public void 자동차_이름_길이검증_테스트(){
+        assertThatThrownBy(()->new Car(0,"")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->new Car(0,null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->new Car(0,"nexts")).isInstanceOf(IllegalArgumentException.class);
+        assertThatCode(()->new Car(0,"next")).doesNotThrowAnyException();
     }
 
 }
