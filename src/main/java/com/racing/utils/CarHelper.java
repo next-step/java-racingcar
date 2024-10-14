8dd4a62672fa274e28597de7e609366f287f99c6
jonghyeon <jonghyl@smilegate.net>
2024-10-14 20:04:28 +0900

    feat: carName의 validation을 체크하는 기능 추가

--- src/main/java/com/racing/utils/CarHelper.java
+++ src/main/java/com/racing/utils/CarHelper.java
@@ -4,8 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CarHelper {
+    private static final int VALID_CAR_NAME = 6;
+
     public static List<String> splitCarName(String carNames) {
         String[] names = carNames.split(",");
+
         return Arrays.asList(names);
+    }
+
+    public static boolean isValidCarName(String carName) {
+        if (carName.isEmpty()) {
+            return false;
+        }
+        
+        return carName.length() < VALID_CAR_NAME;
     }
 }
