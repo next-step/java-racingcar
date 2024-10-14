edd0876c13271b17c28af79c146a631293925cb1
jonghyeon <jonghyl@smilegate.net>
2024-10-14 19:28:54 +0900

    refactor: 헬퍼 클래스에서 인터페이스를 상속받아 실제 구현, 테스트 코드 추가

--- src/main/java/com/racing/utils/RacingHelper.java
+++ src/main/java/com/racing/utils/RacingHelper.java
@@ -5,9 +5,18 @@ import java.util.Random;
 /**
  * 레이싱에서 사용 할 헬퍼 메서드를 모아놓은 클래스
  */
-public class RacingHelper {
+public class RacingHelper implements RandomMover {
     private static final int RANDOM_NUMBER_RANGE = 10;
     private static final int FORWARD_MINIMUM_NUMBER = 4;
+
+    /**
+     * Helper 클래스 내부에서 shouldMove 직접 구현
+     */
+    @Override
+    public boolean shouldMove() {
+        int randomNumber = getRandomNumber();
+        return isForward(randomNumber);
+    }
 
     /**
      * 0~9의 랜덤 값 반환
