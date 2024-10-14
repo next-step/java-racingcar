83700ed6fa32c99e3906855560ab1ace2bdc6208
jonghyeon <jonghyl@smilegate.net>
2024-10-14 20:20:45 +0900

    feat: 자동차 이름을 저장할 수 있도록 객체에 필드 선언

--- src/main/java/com/racing/domain/Car.java
+++ src/main/java/com/racing/domain/Car.java
@@ -5,20 +5,22 @@ import com.racing.utils.RacingHelper;
 public class Car {
 
     private int state = 0;
+    private String name;
 
     // 기본 생성자
     public Car() {
-        this(0); // 기본값 0으로 다른 생성자 호출
+        this(0, ""); // 기본값 0으로 다른 생성자 호출
     }
 
     // 상태 지정 생성자
-    public Car(int state) {
+    public Car(int state, String name) {
         this.state = state;
+        this.name = name;
     }
 
     public void move() {
         RacingHelper racingHelper = new RacingHelper();
-        
+
         if (racingHelper.shouldMove()) {
             state++;
         }
@@ -26,5 +28,9 @@ public class Car {
 
     public String displayState() {
         return "-".repeat(state);
+    }
+
+    public String displayCarName() {
+        return name;
     }
 }
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -10,10 +10,18 @@ public class CarTest {
     @Test
     @DisplayName("입력한 숫자 만큼 '-'를 반복하여 반환")
     public void isEquals_RepeatDash() {
-        Car car = new Car(6);
+        Car car = new Car(6, "");
         String expected = "------";
 
         assertThat(car.displayState()).isEqualTo(expected);
     }
 
+    @Test
+    @DisplayName("입력한 CarName을 반환")
+    public void isEquals_CarName() {
+        String expected = "poby";
+        Car car = new Car(6, "poby");
+
+        assertThat(car.displayCarName()).isEqualTo(expected);
+    }
 }
