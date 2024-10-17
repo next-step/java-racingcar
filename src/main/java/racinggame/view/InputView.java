1668139898fb948f54c05f88555634406701915d
hunko <rhwlgns4386@naver.com>
2024-10-17 10:53:52 +0900

    bug : 1, 3형식의 입력 제대로 안되던 버그 픽스

--- src/main/java/racinggame/view/InputView.java
+++ src/main/java/racinggame/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
     public static String readCarNames() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return scanner.next();
+        return scanner.nextLine();
     }
 
     public static int readRepeatCount() {
