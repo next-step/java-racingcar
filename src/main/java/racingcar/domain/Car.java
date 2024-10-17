6c6a8aa2e67ec066ad1a8269d6a977d44fa23aae
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-18 04:22:47 +0900

    refactor : 문자열과 원시값을 클래스로 포장

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,52 +2,27 @@ package racingcar.domain;
 
 public class Car {
 
-    private static final int MOVE_CONDITION = 4;
-    private static final int MAX_NAME_LENGTH = 5;
+    private final CarName carName;
+    private final CarPosition carPosition;
 
-    private final String name;
-    private int currentPosition;
-
-    public Car(final String name) {
-        this(name, 0);
+    public Car(CarName name, CarPosition position) {
+        this.carName = name;
+        this.carPosition = position;
     }
 
-    public Car(String name, int position) {
-        validationCarName(name);
-        this.name = name.trim();
-        this.currentPosition = position;
-    }
-
-    private void validationCarName(String name) {
-        validateNotNullOrEmpty(name);
-        validateNameLength(name);
-    }
-
-    private static void validateNameLength(String name) {
-        if (name.trim().length() >= MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("자동차 이름은 " + MAX_NAME_LENGTH + " 미만의 글자여야 합니다.");
-        }
-    }
-
-    private static void validateNotNullOrEmpty(String name) {
-        if (name == null || name.trim().isBlank()) {
-            throw new IllegalArgumentException("자동차 이름은 값이 존재해야 합니다.");
-        }
-    }
-
-    public int move(int randomNumber) {
-        if (randomNumber >= MOVE_CONDITION) {
-            currentPosition++;
-        }
-        return currentPosition;
-    }
-
-    public int getCurrentPosition() {
-        return currentPosition;
+    public void move(int randomNumber) {
+        carPosition.move(randomNumber);
     }
 
     public String getName() {
-        return name;
+        return carName.getName();
     }
 
+    public int getCurrentPosition() {
+        return carPosition.getCurrentPosition();
+    }
+
+    public boolean isAtPosition(int position) {
+        return carPosition.getCurrentPosition() == position;
+    }
 }
