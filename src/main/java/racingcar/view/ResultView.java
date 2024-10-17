d50c05abba41bf5ca2b341c3d420dde2149b0bb2
yun <90806941+yunji1201@users.noreply.github.com>
2024-10-18 04:32:24 +0900

    refactor : 동시성 이슈를 방어하기 위해 로컬 변수 사용으로 변경 (StringBuffer -> StringBuffer)

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 
 public class ResultView {
 
-    public static final StringBuilder BUILDER = new StringBuilder();
     private static final String LINE = "-".repeat(5);
 
     private ResultView() {
@@ -20,23 +19,25 @@ public class ResultView {
     }
 
     public static void drawCarPositions(List<RacingResult> results) {
+        StringBuffer stringBuffer = new StringBuffer();
+
         for (RacingResult result : results) {
-            BUILDER
+            stringBuffer
                     .append(result.getName())
                     .append(" : ")
                     .append("-".repeat(result.getPosition()))
-                    .append("\n");
+                    .append(System.lineSeparator());
         }
-        System.out.println(BUILDER.toString());
-        BUILDER.setLength(0);
+        System.out.println(stringBuffer.toString());
     }
 
     public static void printWinners(List<Car> winners) {
+        StringBuffer stringBuffer = new StringBuffer();
+
         String winnersName = winners.stream().map(Car::getName)
                 .collect(Collectors.joining(","));
 
-        BUILDER.append(winnersName).append("이(가) 최종 우승 했습니다.");
-        System.out.println(BUILDER.toString());
-        BUILDER.setLength(0);
+        stringBuffer.append(winnersName).append("이(가) 최종 우승 했습니다.");
+        System.out.println(stringBuffer.toString());
     }
 }
