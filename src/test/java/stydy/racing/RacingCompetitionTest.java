451cb396f199f4a483d16cefaa23f11c3605fef5
seulpi <s_bee92@naver.com>
2024-10-21 16:03:24 +0900

    refactor: service 로직 내 인스턴스 변수 수정 및 setter method분리

--- src/test/java/stydy/racing/RacingCompetitionTest.java
+++ src/test/java/stydy/racing/RacingCompetitionTest.java
@@ -1,9 +1,8 @@
 package stydy.racing;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import study.racing.InputView;
 import study.racing.controller.RacingCompetition;
 import study.racing.domain.Car;
 import study.racing.domain.CarRaceInput;
@@ -11,38 +10,27 @@ import study.racing.domain.RacingHistory;
 import study.racing.service.RacingGameWinnerService;
 import study.racing.strategy.MoveUnderOneDigitStrategy;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCompetitionTest {
-    private RacingGameWinnerService racingGameWinnerService;
 
-    @BeforeEach
-    public void setData() {
-        String inputValues = "a,b,c";
-        List<String> carNames = Arrays.stream(inputValues.trim().split(","))
-                .map(String::trim)
-                .collect(Collectors.toList());
-
-        CarRaceInput carRaceInput = new CarRaceInput(carNames, carNames.size(), 5);
-        racingGameWinnerService = new RacingGameWinnerService(carRaceInput);
-    }
 
     @Test
     @DisplayName("우승자 찾기")
     public void findWinner() {
 
-        RacingHistory racingHistory = racingGameWinnerService.raceStart(new MoveUnderOneDigitStrategy());
+        InputView inputView = new InputView();
+        CarRaceInput carRaceInput = inputView.collectUserInput();
+        RacingGameWinnerService racingGameWinnerService = new RacingGameWinnerService();
+        RacingHistory racingHistory = racingGameWinnerService.raceStart(carRaceInput, new MoveUnderOneDigitStrategy());
 
-        List<Car> winners = racingGameWinnerService.findWinners(racingHistory);
+        List<Car> winners = racingGameWinnerService.findWinners(carRaceInput.getCarCount(),racingHistory);
 
         int maxMoveCount = winners.get(0).getMoveCount();
         assertThat(winners).isNotEmpty();
         assertThat(winners).allMatch(car -> car.getMoveCount() == maxMoveCount);
-        assertThat(winners).extracting(Car::getCarName).contains("a", "b", "c");
     }
 
     @Test
