949a6f6fef06db765bafe4d9bbd2a75c5ba8f62c
praivesi <praivesi@gmail.com>
2024-10-25 20:37:13 +0900

    feat: add RacingGame in Step3/ (#5827)
    * test: add sample tests
    
    * WIP: feat, test: add step 2
    
    * env: fix gradle version for M1 mac
    
    * feat: afdd StringCalculator in Step1/
    
    * feat: add StringAdder in Step2/
    
    * feat: add Baseball in Step1/
    
    * feat: add RacingGame in Step3/

---
+++ src/test/java/Step3/RacingCarTest.java
@@ -0,0 +1,34 @@
+package Step3;
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingCarTest {
+
+    RacingCar racingCar = new RacingCar();
+
+    // params: random_value, move_distance
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0",
+            "1, 0",
+            "2, 0",
+            "3, 0",
+            "4, 1",
+            "5, 1",
+            "6, 1",
+            "7, 1",
+            "8, 1",
+            "9, 1"
+    })
+    void move_if_rand_is_equal_or_greater_than_four(int rand, int distance) {
+        int prev_pos = racingCar.getPos();
+
+        racingCar.move(rand);
+
+        assertThat(racingCar.getPos()).isEqualTo(prev_pos + distance);
+    }
+}
