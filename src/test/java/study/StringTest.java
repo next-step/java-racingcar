0c6c402dc26732dab32c8d478a2b04a8c70ef37f
heewonham <heewonham6@gmail.com>
2025-03-10 20:06:14 +0900

    string class study test code
    요구사항 2, 3 반영

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,5 +11,24 @@ public class StringTest {
         String[] result = data.split(",");
         assertThat(result).contains("1");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substringTest() {
+        String data = "(1,2)";
+        String result = data.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAtTest() {
+        String data = "abc";
+        assertThat(data.charAt(0)).isEqualTo('a');
+        assertThat(data.charAt(1)).isEqualTo('b');
+        assertThat(data.charAt(2)).isEqualTo('c');
+        assertThatThrownBy(() -> {
+            data.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
