3a2b6bd9dbeb2ba760829c821a7c675a4fcb8853
baki <hgs6424@naver.com>
2025-03-10 23:35:07 +0900

    add sizeTest (요구사항 1)

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,10 @@
 package study;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,5 +19,17 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @Nested
+    @DisplayName("sizeTest")
+    class SizeTest {
+
+        @Test
+        @DisplayName("size가 반환 되는가?")
+        void sizeTest() {
+            // then
+            Assertions.assertThat(numbers.size()).isEqualTo(3);
+        }
     }
 }
