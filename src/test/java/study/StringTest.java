1bb07a41ee0b6402924d9707853b98d985a7ff0f
picosw <tony.ahn@kakaocorp.com>
2025-03-10 18:08:40 +0900

    학습 테스트 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -10,5 +12,23 @@ public class StringTest {
     void split() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt 메소드를 이용해 특정 위치의 문자를 가져올 수 있다. " +
+            "문자열 길이를 넘어선 index 값이 parameter로 입력되면 StringIndexOutOfBoundsException이 발생한다.")
+    void charAt() {
+        char result = "abc".charAt(1);
+        assertThat(result).isEqualTo('b');
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
