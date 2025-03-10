2098309f6e87bd9f24532af6a9c2d8eb5d7d3eb8
HanSangKkyu <kkyu67@naver.com>
2025-03-10 11:18:07 +0900

    StringTest SetTest

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void request1() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void request2(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5})
+    void request3(int input) {
+        assertThat(numbers.contains(input)).isFalse();
+    }
+}
