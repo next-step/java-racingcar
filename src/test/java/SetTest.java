528d79ba81a7f2bf77e0d51ab73a488b624d44b9
yang Jiyoung <0112jyoung@naver.com>
2025-03-11 22:26:00 +0900

    [test] set 테스트 추가

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("numbers의 크기는 3와 같다.")
+    public void size(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("1,2,3의 값이 set에 존재한다.")
+    public void contains(int number){
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("1, 2, 3에 대해서는 true를 4, 5에 대해서는 false를 반홚나다.")
+    public void containsOver3(int number, boolean expected){
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+
+}
