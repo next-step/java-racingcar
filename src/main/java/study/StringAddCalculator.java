a32f3fbb123b521b720bf6813a1a0cf834a4b53e
baki <hgs6424@naver.com>
2025-03-11 23:56:17 +0900

    add null and empty string check in splitAndSum method

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -2,6 +2,10 @@ package study;
 
 public class StringAddCalculator {
     public static int splitAndSum(String value) {
+        if (value == null || value.isEmpty()) {
+            return 0;
+        }
+
         return 0;
     }
 }
