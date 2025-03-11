e45fb3c9ce70afb5fcee54948013cf696ebc3d33
theo-94 <theo.94@kakaocorp.com>
2025-03-11 18:20:36 +0900

    문자열 덧셈 계산기 구현

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,5 +1,69 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class StringAddCalculator {
-    public static int splitAndSum(String input) {
-        return 0;
+
+    private static final List<String> DEFAULT_DELIMITERS = List.of(",", ":");
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    // 외부에서 호출하는 메소드
+    public int splitAndSum(String input) {
+        if (input == null || input.isEmpty()) return 0;
+        return calculate(input);
+    }
+
+    // 계산 로직 수행
+    private int calculate(String input) {
+        String cleanedInput = removeCustomDelimiterSyntax(input);
+        List<String> delimiters = getDelimiters(input);
+        List<Integer> numbers = parseToIntList(splitByDelimiters(cleanedInput, delimiters));
+        return sum(numbers);
+    }
+
+    // 입력된 문자열에서 커스텀 구분자 부분을 제거
+    private String removeCustomDelimiterSyntax(String input) {
+        return CUSTOM_DELIMITER_PATTERN.matcher(input).replaceFirst("$2");
+    }
+
+    // 기본 구분자와 커스텀 구분자를 포함한 구분자의 리스트를 반환
+    private List<String> getDelimiters(String input) {
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(input);
+
+        if (!m.find()) {
+            return DEFAULT_DELIMITERS;
+        }
+
+        String customDelimiter = m.group(1);
+        return new ArrayList<>(DEFAULT_DELIMITERS) {{
+            add(customDelimiter);
+        }};
+    }
+
+    // 문자열을 구분자를 기준으로 분리
+    private String[] splitByDelimiters(String input, List<String> delimiters) {
+        String regex = String.join("|", delimiters);
+        return input.split(regex);
+    }
+
+    // 문자열 배열을 양의 정수 리스트로 변환
+    private List<Integer> parseToIntList(String[] input) {
+        return Arrays.stream(input)
+                .map(Integer::parseInt)
+                .peek(this::validatePositiveNumber)
+                .collect(Collectors.toList());
+    }
+
+    // 음의 정수가 포함되어 있으면 RuntimeException을 던짐
+    private void validatePositiveNumber(int num) {
+        if (num < 0) throw new RuntimeException("음의 정수는 요구 사항에 없습니다.");
+    }
+
+    // 정수 리스트의 총합을 리턴
+    private int sum(List<Integer> nums) {
+        return nums.stream().reduce(0, Integer::sum);
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,45 +1,53 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
+    private StringAddCalculator stringAddCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringAddCalculator = new StringAddCalculator();
+    }
+
     @Test
     public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
+        int result = stringAddCalculator.splitAndSum(null);
         assertThat(result).isEqualTo(0);
 
-        result = StringAddCalculator.splitAndSum("");
+        result = stringAddCalculator.splitAndSum("");
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     public void splitAndSum_숫자하나() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1");
+        int result = stringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2");
+        int result = stringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
+        int result = stringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_custom_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        int result = stringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
     public void splitAndSum_negative() throws Exception {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+        assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
 }
