33c8f5dc52ba0909775cf4c549ac37dafb8648fe
baki <hgs6424@naver.com>
2025-03-12 00:07:06 +0900

    add support for custom splitters in splitAndSum method

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,11 +1,25 @@
 package study;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
+
     public static int splitAndSum(String value) {
         if (value == null || value.isEmpty()) {
             return 0;
+        }
+
+        var matcher = pattern.matcher(value);
+
+        if (matcher.find()) {
+            var customSpliter = matcher.group(1);
+            var tokens = matcher.group(2).split(customSpliter);
+
+            return Arrays.stream(tokens)
+                    .mapToInt(Integer::parseInt)
+                    .sum();
         }
 
         return Arrays.stream(value.split("[,:]"))
