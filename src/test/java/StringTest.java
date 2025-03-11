3f86c6819cd2a3b0593dd2f9080ad43a151fb300
domo7304 <domo7304@naver.com>
2025-03-11 23:14:29 +0900

    chore(stringTest): String 클래스 학습 테스트, 테스트 문구 변경

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class StringTest {
 
-    @DisplayName("문자열 \"1,2\"를 split 하면 1과 2가 반환된다.")
+    @DisplayName("문자열 \"1,2\"에 대해 spiit()을 활용해 \"1\", \"2\"를 반환할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2"})
     void splitByComma_MultipleValues(String text) {
         assertThat(text.split(",")).containsExactly("1", "2");
     }
 
-    @DisplayName("문자열 \"1\"을 split 하면 1만이 반환된다.")
+    @DisplayName("문자열 \"1\"에 대해 spiit()을 활용해 \"1\"을 반환할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"1"})
     void splitByComma_SingleValue(String text) {
@@ -35,7 +35,7 @@ class StringTest {
         assertThat(text.charAt(1)).isEqualTo('b');
     }
 
-    @DisplayName("문자열 \"abc\"에서 charAt()을 사용해 범위를 벗어난 위치를 조회하면 예외가 발생한다.")
+    @DisplayName("문자열 \"abc\"에 charAt()을 사용해 범위를 벗어난 위치를 조회하면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"abc"})
     void charAt_OutOfBounds_ThrowsException(String text) {
