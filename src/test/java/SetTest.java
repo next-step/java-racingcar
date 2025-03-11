dfd762100e4f47f070b201f12a31f47755c432a0
ParkSeryu <pjh971212@gmail.com>
2025-03-11 17:51:56 +0900

    test: [Set 요구사항 3] - contains 학습 테스트

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -38,5 +39,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void containsTest(int argument) {
         assertThat(numbers.contains(argument)).isTrue();
+    }
+
+    @DisplayName("contains 메소드에 csvSource 활용해 실패하는 케이스도 검증한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "100:false"}, delimiter = ':')
+    void containsCsvSourceTest(int argument, boolean expected) {
+        assertThat(numbers.contains(argument)).isEqualTo(expected);
     }
 }
