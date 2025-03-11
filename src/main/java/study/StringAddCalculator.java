c1af50e68f8e23864052e150e002dbbbd62ca497
baki <hgs6424@naver.com>
2025-03-11 23:59:09 +0900

    refactor splitAndSum method to handle multiple splitter and sum values

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -1,15 +1,15 @@
 package study;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
     public static int splitAndSum(String value) {
         if (value == null || value.isEmpty()) {
             return 0;
         }
 
-        if (Integer.parseInt(value) == 1) {
-            return 1;
-        }
-
-        return 0;
+        return Arrays.stream(value.split("[,:]"))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
