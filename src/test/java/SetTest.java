fbe361560f35d60b478e35a1af4a7292a3a61382
JIHYUN <jh_s_@naver.com>
2025-03-11 09:00:18 +0900

    initial commit (#5842)
    * split study test
    
    * string class & Set Collection study
    
    * Set Collection study
    
    ---------
    
    Co-authored-by: sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,48 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+  private Set<Integer> numbers;
+
+  @BeforeEach
+  void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+  @Test
+  @DisplayName("size() 메소드를 활용해 Set의 크기를 확인한다.")
+  void size() {
+    assertThat(numbers.size()).isEqualTo(3);
+  }
+
+//  @Test
+//  @DisplayName("Set의 contains() 메소드를 활용해 1,2,3의 값이 존재하는지 확인한다.")
+//  void contains() {
+//    assertThat(numbers.contains(1)) .isTrue();
+//    assertThat(numbers.contains(2)) .isTrue();
+//    assertThat(numbers.contains(3)) .isTrue();
+//  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 3})
+  void contains(int number) {
+    assertThat(numbers.contains(number)) .isTrue();
+  }
+
+  @ParameterizedTest
+  @CsvSource(value={"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter=':')
+  void contains(Integer number, boolean expected) {
+    boolean actual = numbers.contains(number);
+    assertThat(actual).isEqualTo(expected);
+  }
+}
