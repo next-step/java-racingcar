76182981d2e916990a68248898af34a4040dd3ab
east.bridge <east.bridge@kakaocorp.com>
2025-03-12 19:11:26 +0900

    Set Collection Test - size() 테스트

---
+++ src/test/java/study/SetCollectionTest.java
@@ -0,0 +1,27 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetCollectionTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
