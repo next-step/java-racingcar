958711a9f7de3383a6707576ebb087211fc043f1
east.bridge <east.bridge@kakaocorp.com>
2025-03-12 19:13:16 +0900

    @ParameterizedTest 를 이용하여 contains 메서드 테스트

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,4 +26,12 @@ public class SetCollectionTest {
     void size() {
         assertThat(numbers.size()).isEqualTo(3);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+
 }
