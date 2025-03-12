bb096cf67d1b4479c9f3f562564dda7d1e860579
east.bridge <east.bridge@kakaocorp.com>
2025-03-12 19:00:50 +0900

    substring test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -11,4 +11,12 @@ public class StringTest {
         assertThat(values).contains("1", "2");
         assertThat(values).containsExactly("1", "2");
     }
+
+    @Test
+    void substring() {
+        String value = "(1,2)";
+        String result = value.substring(1, value.length() - 1);
+        assertThat(result).isEqualTo("1,2");
+    }
+
 }
