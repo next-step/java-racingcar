5a5c6e1c786445640fa74b6ef5541a0f3c68709e
nno0obb <146364491+nno0obb@users.noreply.github.com>
2025-03-12 09:37:26 +0900

    Step1 (#5889)
    * split study test
    
    * StringTest
    
    * SetTest
    
    * Refactor
    
    ---------
    
    Co-authored-by: bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,45 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    void srs1Test() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void srs2Test(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void srs3Test(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+}
