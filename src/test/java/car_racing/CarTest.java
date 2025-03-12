6f51b32a2f8c9934c63f0bd2b4d13dc40c707b90
theo-94 <theo.94@kakaocorp.com>
2025-03-12 21:28:08 +0900

    feat: 입력받은 값을 이용해 자동차 객체를 생성한다.
    - 위치라는 상태와 상태를 변경하는 메소드를 갖는 Car 작성
    - Car에 대한 테스트 작성

---
+++ src/test/java/car_racing/CarTest.java
@@ -0,0 +1,25 @@
+package car_racing;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class CarTest {
+    @ParameterizedTest(name = "자동차를 움직이면 자동차가 움직인 만큼 위치가 변한다. 움직인 값: {0}")
+    @ValueSource(ints = {1, 2, 3})
+    void shouldMoveCarByGivenDistance(int inputDistance) {
+        Car car = new Car();
+        car.move(inputDistance);
+        assertThat(car.getDistance()).isEqualTo(inputDistance);
+    }
+
+    @ParameterizedTest(name = "자동차를 음수만큼 움직이면 에러가 발생한다. 움직인 값: {0}")
+    @ValueSource(ints = {-1, -2, -3})
+    void sholdThrowRuntimeExceptionWhenNegativeDistanceIsGiven(int inputDistance) {
+        Car car = new Car();
+        assertThatThrownBy(() -> car.move(inputDistance))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
