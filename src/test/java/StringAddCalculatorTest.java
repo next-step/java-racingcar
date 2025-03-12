95035510aee6f05bf03fac338b2292eedbd3c5af
theo-94 <theo.94@kakaocorp.com>
2025-03-12 11:17:40 +0900

    [피드백 반영] DisplayName 도입

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -13,7 +14,8 @@ public class StringAddCalculatorTest {
     }
 
     @Test
-    public void splitAndSum_null_또는_빈문자() {
+    @DisplayName("null이나 빈 문자열 입력시 0을 반환하는지")
+    public void shouldReturnZeroWhenInputIsNullOrEmpty() {
         int result = stringAddCalculator.splitAndSum(null);
         assertThat(result).isZero();
 
@@ -28,31 +30,36 @@ public class StringAddCalculatorTest {
     }
 
     @Test
-    public void splitAndSum_숫자하나() throws Exception {
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환하는지")
+    public void shouldReturnNumberWhenSingleNumberIsGiven() {
         int result = stringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    public void splitAndSum_쉼표구분자() throws Exception {
+    @DisplayName("숫자 두 개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환하는지")
+    public void shouldReturnSumWhenTwoNumbersAreCommaSeparated() {
         int result = stringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
+    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있는지")
+    public void shouldSupportCommaOrColonAsDelimiter() {
         int result = stringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_custom_구분자() throws Exception {
+    @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자를 지정할 수 았는지")
+    public void shouldSupportCustomDelimiterBetweenSlashesAndNewline() {
         int result = stringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
-    public void splitAndSum_negative() throws Exception {
+    @DisplayName("음수를 전달할 경우 RuntimeException 예외가 발생하는지")
+    public void shouldThrowRuntimeExceptionWhenNegativeNumberIsGiven() {
         assertThatThrownBy(() -> stringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
     }
