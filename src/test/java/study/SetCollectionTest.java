bd6afa9ff3dfc943171219f1840a94bfdd0359b6
east.bridge <east.bridge@kakaocorp.com>
2025-03-12 19:17:00 +0900

    @ParameterizedTest 에 다른 CsvSource 사용하여 실패케이스 테스트 추가

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -31,6 +32,12 @@ public class SetCollectionTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_실패케이스추가버전(Integer input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 
 
