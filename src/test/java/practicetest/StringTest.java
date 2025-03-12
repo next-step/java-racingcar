28697d316575ccd0c0dc11c7d055204b734675bd
윤서연 (Seoyeon Yoon) <2025996@vieworks.com>
2025-03-12 09:33:00 +0900

    step2: 문자열 덧셈 계산기 구현

--- src/test/java/practicetest/StringTest.java
+++ src/test/java/practicetest/StringTest.java
@@ -1,3 +1,5 @@
+package practicetest;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -6,8 +8,7 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
     @DisplayName("String 클래스의 split 메소드 동작 테스트")
     @Test
-    public void testStringSplit()
-    {
+    public void testStringSplit() {
         String testStr1 = "1,2";
         assertThat(testStr1.split(",")).contains("1", "2");
 
@@ -17,19 +18,17 @@ public class StringTest {
 
     @DisplayName("String 클래스의 substring 메소드 동작 테스트")
     @Test
-    public void testStringSubstring()
-    {
+    public void testStringSubstring() {
         String testStr1 = "(1,2)";
-        assertThat(testStr1.substring(1, testStr1.length()-1)).isEqualTo("1,2");
+        assertThat(testStr1.substring(1, testStr1.length() - 1)).isEqualTo("1,2");
     }
 
     @DisplayName("String 클래스의 charAt 메소드 동작 테스트")
     @Test
-    public void testStringCharAt()
-    {
+    public void testStringCharAt() {
         String testStr1 = "abc";
         assertThatThrownBy(() -> testStr1.charAt(testStr1.length()))
                 .isInstanceOf(StringIndexOutOfBoundsException.class)
-                .hasMessageContaining(String.format("Index out of range: %d은(는) %s의 인덱스를 넘어갑니다.", testStr1.length(), testStr1));
+                .hasMessageContaining(String.format("String index out of range: %d", testStr1.length()));
     }
 }
