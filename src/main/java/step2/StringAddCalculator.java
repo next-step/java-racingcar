941666ce3cea20852cf444ed6231d0905228fe65
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-03-13 01:02:32 +0900

    step2: StringAddCalculator

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,42 @@
+package step2;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    public static int splitAndSum(String input) {
+        if (!isValid(input)) {
+            return 0;
+        }
+        int[] tokens = tokenize(input);
+        return sum(tokens);
+    }
+
+    public static boolean isValid(String input) {
+        if (input == null || input.isEmpty()) {
+            return false;
+        }
+        if (Pattern.matches("(//\\D\\n)*(\\d+\\D)*\\d+", input)) {
+            return true;
+        }
+        throw new RuntimeException();
+    }
+
+    public static int[] tokenize(String input) {
+        String[] delimiters = new String[]{",", ":"};
+        Matcher m = Pattern.compile("//(\\D)\n(.*)").matcher(input);
+        if (m.find()) {
+            delimiters = new String[]{delimiters[0], delimiters[1], m.group(1)};
+            input = m.group(2);
+        }
+
+        String[] tokens = input.split(String.format("[%s]", String.join("", delimiters)));
+        return Arrays.stream(tokens).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public static int sum(int[] tokens) {
+        return Arrays.stream(tokens).sum();
+    }
+}
+
---
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -0,0 +1,75 @@
+package step2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringAddCalculatorTest {
+
+    @Test
+    public void splitAndSum_null() {
+        int result = StringAddCalculator.splitAndSum(null);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_emptyString() {
+        int result = StringAddCalculator.splitAndSum("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void splitAndSum_oneNumber() {
+        int result = StringAddCalculator.splitAndSum("1");
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void splitAndSum_delimComma() {
+        int result = StringAddCalculator.splitAndSum("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void splitAndSum_delimCommaAndColon() {
+        int result = StringAddCalculator.splitAndSum("1,2:3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_delimCustom1() {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_delimCustom2() {
+        int result = StringAddCalculator.splitAndSum("//-\n1-2-3");
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void splitAndSum_negative() {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitAndSum_delimCustomAndNegative() {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("//-\n-1-2-3"))
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitAndSum_blank() {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(" "))
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitAndSum_twoDigits() {
+        int result = StringAddCalculator.splitAndSum("11,22:33");
+        assertThat(result).isEqualTo(66);
+    }
+}
