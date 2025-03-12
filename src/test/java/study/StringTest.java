419b5b722cd4cf912b102d4c335237cbd52cb812
Allen Kim <clymeneAllen@gmail.com>
2025-03-12 09:27:48 +0900

    1단계 - 학습 테스트 실습 PR (#5873)
    * String 클래스에 대한 학습 테스트 구현
    
    * Set에 대한 테스트 구현
    
    ---------
    
    Co-authored-by: allen <allen.k1m@kakaocorp.com>

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,41 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    void Split() {
+        String[] splitText = "1,2".split(",");
+        assertThat(splitText).containsExactly("1", "2");
+    }
+
+    @Test
+    void Substring() {
+        String text = "(1,2)";
+        String substringText = text.substring(1, text.length() - 1);
+        assertThat(substringText).isEqualTo("1,2");
+    }
+
+    @Test
+    void CharAt() {
+        String text = "abc";
+        assertThat(text.charAt(0)).isEqualTo('a');
+        assertThat(text.charAt(1)).isEqualTo('b');
+        assertThat(text.charAt(2)).isEqualTo('c');
+
+        assertThatThrownBy(() -> {
+            text.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range: 3");
+
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> {
+                text.charAt(3);
+            }).withMessageMatching("String index out of range: \\d+");
+    }
+}
