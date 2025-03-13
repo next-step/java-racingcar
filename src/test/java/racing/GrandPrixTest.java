7607d2797988ce59d26da6aa44016e4c378f5593
baki <hgs6424@naver.com>
2025-03-14 00:02:15 +0900

    test: add playForTotalRoundCountTest

--- src/test/java/racing/GrandPrixTest.java
+++ src/test/java/racing/GrandPrixTest.java
@@ -1,6 +1,27 @@
 package racing;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GrandPrixTest {
+
+    @Test
+    @DisplayName("그랑프리는 전체 라운드 수를 입력받아 전체 라운드 수 만큼 진행한다.")
+    void playForTotalRoundCountTest() {
+        // given
+        int totalRound = 5;
+        var grandPrix = new GrandPrix(totalRound);
+        int playedRound = 0;
+
+        // when
+        while (!grandPrix.isFinished()) {
+            grandPrix.play();
+            playedRound++;
+        }
+
+        // then
+        Assertions.assertThat(grandPrix.isFinished()).isTrue();
+        Assertions.assertThat(playedRound).isEqualTo(totalRound);
+    }
 }
