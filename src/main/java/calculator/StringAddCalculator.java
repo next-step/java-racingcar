631e748c0108df23018ac7aa14b1da1638142c81
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-03-14 08:55:12 +0900

    pr review 반영

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,24 +9,25 @@ public class StringAddCalculator {
   private static final Pattern PATTERN = Pattern.compile("//(.)\n(.*)");
 
   public static int splitAndSum(String text) {
-    if (isEmptyText(text))
+    if (isEmptyText(text)) {
       return 0;
-    return sum(toInts(split(text)));
+    }
+    int[] numbers = toInts(split(text));
+    return sum(numbers);
+  }
+
+  private static int[] toInts(String[] numbers) {
+    return Arrays.stream(numbers)
+        .mapToInt(StringAddCalculator::toInt)
+        .toArray();
   }
 
   private static int toInt(String number) {
     int num = Integer.parseInt(number);
-    if(num < 0)
+    if(num < 0) {
       throw new RuntimeException("Negative numbers are not allowed: " + num);
-    return num;
-  }
-
-  private static int[] toInts(String[] numbers) {
-    int[] result = new int[numbers.length];
-    for (int i=0; i<numbers.length; i++) {
-      result[i] = toInt(numbers[i]);
     }
-    return result;
+    return num;
   }
 
   private static boolean isEmptyText(String text) {
@@ -34,17 +36,14 @@ public class StringAddCalculator {
 
   private static String[] split(String text)  {
     Matcher matcher = PATTERN.matcher(text);
-    if (matcher.find()) // custom delimiter 인 경우
+    if (matcher.find()) { // custom delimiter 인 경우
       return matcher.group(2).split(matcher.group(1));
+    }
     return text.split(DELIMITER);
   }
 
   private static int sum(int[] numbers) {
-    int sum = 0;
-    for (int number : numbers) {
-      sum += number;
-    }
-    return sum;
+    return Arrays.stream(numbers).sum();
   }
 
 }
