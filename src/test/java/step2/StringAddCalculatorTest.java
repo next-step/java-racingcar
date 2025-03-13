b61bd79849aab92ca9fb80063863b29f27c4e21d
HongYeseul <yeseul.dev@gmail.com>
2025-03-14 08:46:35 +0900

    refactor(1단계: 문자열 덧셈 계산기) - 테스트 코드 내 ParameterizedTest 로 변경

--- src/test/java/step2/StringAddCalculatorTest.java
+++ src/test/java/step2/StringAddCalculatorTest.java
@@ -2,14 +2,18 @@ package step2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
 
-    @NullAndEmptySource()
+    @NullAndEmptySource
     @DisplayName("null 또는 빈문자를 입력할 경우 0을 반환해야 한다.")
     public void splitAndSum_null_또는_빈문자(String input) {
         int result = StringAddCalculator.splitAndSum(new Input(input));
@@ -19,17 +23,19 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(0);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
-    public void splitAndSum_숫자하나() throws Exception {
-        int result = StringAddCalculator.splitAndSum(new Input("1"));
-        assertThat(result).isEqualTo(1);
+    @ValueSource(strings = {"1", "2", "3"})
+    public void splitAndSum_숫자하나(String input) throws Exception {
+        int result = StringAddCalculator.splitAndSum(new Input(input));
+        assertThat(result).isEqualTo(Integer.parseInt(input));
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("숫자 두 개를 콤마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
-    public void splitAndSum_쉼표구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum(new Input("1,2"));
+    @ValueSource(strings = {"1,2", "1,2,"})
+    public void splitAndSum_쉼표구분자(String input) throws Exception {
+        int result = StringAddCalculator.splitAndSum(new Input(input));
         assertThat(result).isEqualTo(3);
     }
 
@@ -40,17 +46,26 @@ public class StringAddCalculatorTest {
         assertThat(result).isEqualTo(6);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("구분자는 //와 /n 문자를 이용해 커스텀 구분자를 지정할 수 있다.")
-    public void splitAndSum_custom_구분자() throws Exception {
-        int result = StringAddCalculator.splitAndSum(new Input("//;\n1;2;3"));
-        assertThat(result).isEqualTo(6);
+    @ValueSource(strings = {";", ":"})
+    public void splitAndSum_custom_구분자(String delimiter) throws Exception {
+        List<String> inputNumbers = List.of("1", "2", "3");
+        String customDelimiter = "//" + delimiter + "\n";
+
+        int result = StringAddCalculator.splitAndSum(new Input(customDelimiter + String.join(delimiter, inputNumbers)));
+        assertThat(result).isEqualTo(
+                inputNumbers.stream()
+                .mapToInt(Integer::parseInt)
+                .sum()
+        );
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("음수를 전달할 경우 예외가 발생한다.")
-    public void splitAndSum_negative() throws Exception {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(new Input("-1,2,3")))
+    @ValueSource(strings = {"-1", "1,-2,3"})
+    public void splitAndSum_negative(String input) throws Exception {
+        assertThatThrownBy(() -> StringAddCalculator.splitAndSum(new Input(input)))
                 .isInstanceOf(RuntimeException.class);
     }
 }
