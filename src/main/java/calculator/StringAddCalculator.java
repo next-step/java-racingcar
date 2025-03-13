1429c44bef6232c592dd532d336fcfa6478fb736
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-03-13 22:16:45 +0900

    refactoring

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -4,34 +4,44 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-  private static final String delimiter = ",|:";
+  private static final String DELIMITER = ",|:";
+  private static final Pattern PATTERN = Pattern.compile("//(.)\n(.*)");
 
-  public static int splitAndSum(String text) throws RuntimeException {
+  public static int splitAndSum(String text) {
     if (isEmptyText(text))
       return 0;
+    return sum(toInts(split(text)));
+  }
 
-    return sum(split(text));
+  private static int toInt(String number) {
+    int num = Integer.parseInt(number);
+    if(num < 0)
+      throw new RuntimeException("Negative numbers are not allowed: " + num);
+    return num;
+  }
+
+  private static int[] toInts(String[] numbers) {
+    int[] result = new int[numbers.length];
+    for (int i=0; i<numbers.length; i++) {
+      result[i] = toInt(numbers[i]);
     }
+    return result;
+  }
 
   private static boolean isEmptyText(String text) {
     return text == null || text.isEmpty();
   }
 
   private static String[] split(String text)  {
-    Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+    Matcher matcher = PATTERN.matcher(text);
     if (matcher.find()) // custom delimiter 인 경우
       return matcher.group(2).split(matcher.group(1));
-
-    return text.split(delimiter);
+    return text.split(DELIMITER);
   }
 
-    private static int sum(String[] numbers) throws RuntimeException {
+    private static int sum(int[] numbers) {
       int sum = 0;
-      for (String value : numbers) {
-        int number = Integer.parseInt(value);
-        if(number < 0)
-          throw new RuntimeException();
-
+      for (int number : numbers) {
         sum += number;
       }
       return sum;
