423bb86040edb95edb4ee98c5cdd60adb0f6b04a
luka.redwing(이재석)/kakao <luka.redwing@kakaocorp.com>
2025-03-14 00:57:38 +0900

    feat ui 작성

---
+++ src/main/java/ui/ResultView.java
@@ -0,0 +1,25 @@
+package ui;
+
+import java.util.List;
+
+public class ResultView {
+
+    public static void printResult(List<List<Integer>> carPositionsAtTurns) {
+        System.out.println("실행 결과");
+        for (List<Integer> carPositions : carPositionsAtTurns) {
+            printTurnResult(carPositions);
+        }
+    }
+
+    private static void printTurnResult(List<Integer> carPositions) {
+        for (int carPosition : carPositions) {
+            printCarPosition(carPosition);
+        }
+        System.out.println();
+    }
+
+    private static void printCarPosition(int carPosition) {
+        System.out.println("-".repeat(carPosition));
+    }
+
+}
