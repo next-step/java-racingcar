8de5417ff5b492dd6582b0f8144ba7e7bf173608
baki <hgs6424@naver.com>
2025-03-14 00:11:55 +0900

    feat: implement GrandPrix round management

--- src/main/java/racing/GrandPrix.java
+++ src/main/java/racing/GrandPrix.java
@@ -1,5 +1,19 @@
 package racing;
 
 public class GrandPrix {
+    private final int totalRound;
+    private int playedRound;
 
+    public GrandPrix(int totalRound) {
+        this.totalRound = totalRound;
+        this.playedRound = 0;
+    }
+
+    public boolean isFinished() {
+        return playedRound == totalRound;
+    }
+
+    public void play() {
+        playedRound++;
+    }
 }
