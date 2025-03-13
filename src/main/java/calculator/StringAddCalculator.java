59c91dd965aae66a2dcd510d047aeb565c6caea4
jorippppong <duelee75@gmail.com>
2025-03-13 14:17:16 +0900

    refactor: 2단계 피드백 반영

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -7,21 +7,24 @@ public class StringAddCalculator {
 
 	private static final Pattern SPLIT_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 	private static final String DEFAULT_DELIMITER = ",|:";
-	private static final Pattern INTEGER_PATTERN = Pattern.compile("^-?\\d+$");
+	private static final int EMPTY_STRING_RESULT = 0;
+	private static final int DELIMITER_GROUP_INDEX = 1;
+	private static final int TEXT_GROUP_INDEX = 2;
 
 	private StringAddCalculator() {
 		throw new UnsupportedOperationException("유틸 클래스의 인스턴스를 생성할 수 없습니다.");
 	}
 
 	public static int splitAndSum(String text) {
-		if (isNullOrBlank(text))
-			return 0;
+		if (isNullOrBlank(text)) {
+			return EMPTY_STRING_RESULT;
+		}
 
 		String delimiter = DEFAULT_DELIMITER;
 		Matcher m = SPLIT_DELIMITER_PATTERN.matcher(text);
 		if (m.find()) {
-			delimiter = m.group(1);
-			text = m.group(2);
+			delimiter = m.group(DELIMITER_GROUP_INDEX);
+			text = m.group(TEXT_GROUP_INDEX);
 		}
 
 		String[] tokens = split(delimiter, text);
@@ -39,19 +42,22 @@ public class StringAddCalculator {
 	private static int sum(String[] tokens) {
 		int result = 0;
 		for (String token : tokens) {
-			validateToken(token);
-			result += Integer.parseInt(token);
+			result += parseValidInt(token);
 		}
 		return result;
 	}
 
-	private static void validateToken(String token) {
-		if (!INTEGER_PATTERN.matcher(token)
-			.matches()) {
+	private static int parseValidInt(String token) {
+		int parsedValue;
+		try {
+			parsedValue = Integer.parseInt(token);
+		} catch (NumberFormatException e) {
 			throw new RuntimeException("정수만 전달할 수 있습니다: " + token);
 		}
-		if (Integer.parseInt(token) < 0) {
+
+		if (parsedValue < 0) {
 			throw new RuntimeException("음수를 전달할 수 없습니다: " + token);
 		}
+		return parsedValue;
 	}
 }
