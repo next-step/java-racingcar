67771f3800ce8259a7deecb121aea89bcd1da76f
baeksangha <baeksangha@kaist.ac.kr>
2025-03-14 15:31:55 +0900

    feat: 전진 로직 추가

--- src/main/java/edu/nextstep/camp/racingcar/step3/CarRacing.java
+++ src/main/java/edu/nextstep/camp/racingcar/step3/CarRacing.java
@@ -1,15 +1,30 @@
 package edu.nextstep.camp.racingcar.step3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class CarRacing {
     private static final String QUESTION_HOW_MANY_CARS = "자동차 대수는 몇 대 인가요?";
     private static final String QUESTION_HOW_MANY_TRIES = "시도할 회수는 몇 회 인가요?";
 
-    public static void process() {
+    private static final Random random = new Random();
+
+    private CarRacing() {
+        throw new IllegalStateException("인스턴스 생성이 불가능한 클래스입니다.");
+    }
+
+    public static void main(String[] args) {
+        racing();
+    }
+
+    public static void racing() {
         Scanner scanner = new Scanner(System.in);
-        int getCarCount = getCarCount(scanner);
-        int getTryCount = getTryCount(scanner);
+        int carCount = getCarCount(scanner);
+        int tryCount = getTryCount(scanner);
+
+        process(carCount, tryCount);
     }
 
     protected static int getCarCount(Scanner scanner) {
@@ -20,5 +35,37 @@ public class CarRacing {
     protected static int getTryCount(Scanner scanner) {
         System.out.println(QUESTION_HOW_MANY_TRIES);
         return scanner.nextInt();
+    }
+
+    protected static void process(int carCount, int tryCount) {
+        List<String> cars = initializeList(carCount);
+        for (int i = 0; i < tryCount; i++) {
+            moveCars(cars, carCount);
+        }
+    }
+
+    private static List<String> initializeList(int carCount) {
+        List<String> cars = new ArrayList<>();
+        for (int i = 0; i < carCount; i++) {
+            cars.add("");
+        }
+        return cars;
+    }
+
+    protected static void moveCars(List<String> cars, int carCount) {
+        for (int i = 0; i < carCount; i++) {
+            cars.set(i, cars.get(i) + moveOrStay(getRandomNumber()));
+        }
+    }
+
+    protected static String moveOrStay(int number) {
+        if (number >= 4) {
+            return "-";
+        }
+        return "";
+    }
+
+    private static int getRandomNumber() {
+        return random.nextInt(10);
     }
 }
--- src/test/java/edu/nextstep/camp/racingcar/step3/CarRacingTest.java
+++ src/test/java/edu/nextstep/camp/racingcar/step3/CarRacingTest.java
@@ -1,12 +1,13 @@
 package edu.nextstep.camp.racingcar.step3;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.ByteArrayInputStream;
 import java.util.Scanner;
 
-import static edu.nextstep.camp.racingcar.step3.CarRacing.getCarCount;
-import static edu.nextstep.camp.racingcar.step3.CarRacing.getTryCount;
+import static edu.nextstep.camp.racingcar.step3.CarRacing.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarRacingTest {
@@ -20,5 +21,11 @@ class CarRacingTest {
     void 시도_횟수_입력_테스트() {
         String tryCount = "3";
         assertThat(getTryCount(new Scanner(new ByteArrayInputStream(tryCount.getBytes())))).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0,''", "1,''", "2,''", "3,''", "4,-", "5,-", "6,-", "7,-", "8,-", "9,-"})
+    void 자동차_이동_테스트(int num, String expected) {
+        assertThat(moveOrStay(num)).isEqualTo(expected);
     }
 }
