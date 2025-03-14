d038a1d878d6d10162917138f4f7e982367fda96
baki <hgs6424@naver.com>
2025-03-14 13:55:00 +0900

    test: add report test for print result

--- src/test/java/racing/GrandPrixTest.java
+++ src/test/java/racing/GrandPrixTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 class GrandPrixTest {
 
     @Test
@@ -11,7 +13,7 @@ class GrandPrixTest {
     void playForTotalRoundCountTest() {
         // given
         int totalRound = 5;
-        var grandPrix = new GrandPrix(totalRound, 0);
+        var grandPrix = new GrandPrix(totalRound, 0, new Random());
         int playedRound = 0;
 
         // when
@@ -23,5 +25,27 @@ class GrandPrixTest {
         // then
         Assertions.assertThat(grandPrix.isFinished()).isTrue();
         Assertions.assertThat(playedRound).isEqualTo(totalRound);
+    }
+
+    @Test
+    @DisplayName("그랑프리 리포트를 하면 현재 상황을 리턴한다.")
+    void reportTest() {
+        // given
+        var luckyDice = new DiceTest.MockRandom(CarTest.MOVE_THRESHOLD + 1);
+        var totalRound = 5;
+        var carCount = 3;
+        var grandPrix = new GrandPrix(totalRound, carCount, luckyDice);
+        var playTimes = 3;
+
+        for (int i = 0; i < playTimes; i++) {
+            grandPrix.play();
+        }
+
+        // when
+        var report = grandPrix.report();
+
+        // then
+        Assertions.assertThat(report).hasSize(carCount);
+        Assertions.assertThat(report.values()).allMatch(it -> it == playTimes);
     }
 }
