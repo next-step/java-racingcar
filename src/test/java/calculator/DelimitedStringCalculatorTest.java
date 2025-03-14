85a0d0a929a9745f1a4410272b91ccce94bdc4c0
Heesup Kim <51118441+gan-ta@users.noreply.github.com>
2025-03-14 13:31:00 +0900

    2단계 - 문자열 덧셈 계산기 (#5935)
    * 테스트 케이스 설계
    
    * 테스트 로직 추가
    
    * 테스트 로직 추가
    
    * 문자열 덧셈 계산기 구현
    
    * 문자열 덧셈 계산기 구현

---
+++ src/test/java/calculator/DelimitedStringCalculatorTest.java
@@ -0,0 +1,107 @@
+package calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DelimitedStringCalculatorTest {
+
+  @DisplayName("빈 문자열이 주어졌을 때 sum()을 호출하면 0을 반환한다.")
+  @Test
+  void sum_emptyString_returnsZero() {
+    String input = "";
+
+    int result = DelimitedStringCalculator.sum(input);
+
+    assertThat(result).isEqualTo(0);
+  }
+
+  @DisplayName("null이 주어졌을 때 sum()을 호출하면 0을 반환한다.")
+  @Test
+  void sum_null_returnZero(){
+    String input = null;
+
+    int result = DelimitedStringCalculator.sum(input);
+
+    assertThat(result).isEqualTo(0);
+  }
+
+  @DisplayName("구분자가 없이 숫자 하나만 있는 문자열이 주어졌을 때 sum()을 호출하면 입력된 수 그대로 반환한다.")
+  @Test
+  void sum_singleNumberString_returnsSameNumber() {
+    String input = "1";
+
+    int result = DelimitedStringCalculator.sum(input);
+
+    assertThat(result).isEqualTo(1);
+  }
+
+  @DisplayName("\",\"로 구분되는 문자열이 주어졌을 때 sum()을 호출하면 수의 합을 반환한다.")
+  @Test
+  void sum_commaSeparatedString_returnsSumOfNumbers() {
+    String input = "1,2,3";
+
+    int result = DelimitedStringCalculator.sum(input);
+
+    assertThat(result).isEqualTo(6);
+  }
+
+  @DisplayName("\":\"로 구분되는 문자열이 주어졌을 때 sum()을 호출하면 수의 합을 반환한다.")
+  @Test
+  void sum_colonSeparatedString_returnsSumOfNumbers() {
+    String input = "1:2:3";
+
+    int result = DelimitedStringCalculator.sum(input);
+
+    assertThat(result).isEqualTo(6);
+  }
+
+  @DisplayName("커스컴 구분자를 가지는 형식의 문자열이 주어졌을 때 sum()을 호출하면 수의 합을 반환한다.")
+  @Test
+  void sum_customDelimiterString_returnsSumOfNumbers() {
+    String input1 = "//;\n1;2;3";
+    String input2 = "//$\n1$2$4";
+
+    int result1 = DelimitedStringCalculator.sum(input1);
+    int result2 = DelimitedStringCalculator.sum(input2);
+
+    assertThat(result1).isEqualTo(6);
+    assertThat(result2).isEqualTo(7);
+  }
+
+
+  @DisplayName("커스텀 구분자를 입력받은 형태가 아닌데 \",\" 혹은 \":\" 구분자가 아닌 문자열이 주어졌을 떄 sum()을 호출하면 RuntimeException을 던진다.")
+  @Test
+  void sum_invalidDelimiter_throwsRuntimeException() {
+    String input = "1$2";
+
+    Assertions.assertThrows(
+        RuntimeException.class,
+        () -> DelimitedStringCalculator.sum(input)
+    );
+  }
+
+  @DisplayName("음수를 가지는 형식의 문자열이 주어졌을 때 sum()을 호출하면 RuntimeException을 던진다.")
+  @Test
+  void sum_withNegativeValue_throwsRuntimeException() {
+    String input = "-1:2";
+
+    Assertions.assertThrows(
+        RuntimeException.class,
+        () -> DelimitedStringCalculator.sum(input)
+    );
+  }
+
+  @DisplayName("커스텀 구분자를 입력받았을 때 커스텀 구분자랑 숫자 이외 문자열이 주어졌을 때 sum()을 호출하면 RuntimeException을 던진다.")
+  @Test
+  void sum_invalidCharacterWithoutDelimiter_throwsRuntimeException() {
+    String input = "//;\n1;2;3|4";
+
+    Assertions.assertThrows(
+        RuntimeException.class,
+        () -> DelimitedStringCalculator.sum(input)
+    );
+  }
+}
