69d92abee9b22648a36448ea5727a951ac87ba77
CM <57793065+cm0x0x0x0@users.noreply.github.com>
2025-03-14 16:18:29 +0900

    test: Implement StringAddCalculator (#5926)

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,54 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final int DEFAULT_VALUE = 0;
+
+    public static int splitAndSum(String input) {
+        if (input == null || input.isEmpty()) {
+            return DEFAULT_VALUE;
+        }
+
+        switch (input.length()) {
+            case 1:
+                return processSingleCharacter(input);
+            default:
+                return processMultipleCharacter(input);
+        }
+    }
+
+    private static int processSingleCharacter(String input) throws RuntimeException {
+        int num;
+        try {
+            num = Integer.parseInt(input);
+            if (num < 0) {
+                throw new RuntimeException("Negative number");
+            }
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Not number value");
+        }
+
+        return num;
+    }
+
+    private static int processMultipleCharacter(String input) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+
+            return addStringNums(tokens);
+        }
+
+        return addStringNums(input.split(",|:"));
+    }
+
+    private static int addStringNums(String[] tokens) {
+        int num = 0;
+        for (String numStr : tokens) {
+            num += processSingleCharacter(numStr);
+        }
+
+        return num;
+    }
+}
