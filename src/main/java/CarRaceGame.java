0c766a28230b9526f7c7963de82fb54e696fbe37
bb-worm <xofyd99@naver.com>
2025-03-14 20:25:20 +0900

    feat: 자동차 대수 입력받아서 진행

--- src/main/java/CarRaceGame.java
+++ src/main/java/CarRaceGame.java
@@ -1,12 +1,38 @@
+import java.util.ArrayList;
 
 public class CarRaceGame {
-    private static Car car = new Car(0);
+    private static ArrayList<Car> cars = new ArrayList<>();
 
     public static void main(String[] args) {
+        makeCars();
+        race();
+    }
+
+    private static void makeCars() {
+        int carNumber = InputView.inputCarNumber();
+        for (int i=0; i<carNumber; i++) {
+            cars.add(new Car(0));
+        }
+    }
+
+    private static void race() {
         int tryNumber = InputView.inputTryNumber();
         for (int i=0; i<tryNumber; i++) {
+            moveAllCars();
+            printCars();
+        }
+    }
+
+    private static void moveAllCars() {
+        for (Car car : cars) {
             car.randomGo();
+        }
+    }
+
+    private static void printCars() {
+        for (Car car : cars) {
             car.printLocation();
         }
+        System.out.println();
     }
 }
--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
+    public static int inputCarNumber() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return scanner.nextInt();
+    }
+
     public static int inputTryNumber() {
         System.out.println("시도할 회수는 몇 회 인가요?");
         return scanner.nextInt();
