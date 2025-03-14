98b88dc34aff10d7aca57313045003d5c8303d4e
jhm9595 <jhm9595@gmail.com>
2025-03-14 23:46:45 +0900

    콤마 콜론 구분자 완성

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -8,6 +8,12 @@ class StringAddCalculatorTest {
     StringAddCalculator cal = new StringAddCalculator();
 
     @Test
+    @DisplayName(value="4. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)")
+    void 콤마_콜론_구분자_합계(){
+        assertEquals(6, cal.add("1,2:3"));
+    }
+
+    @Test
     @DisplayName(value="3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)")
     void 콤마_합계(){
         assertEquals(3, cal.add("1,2"));
