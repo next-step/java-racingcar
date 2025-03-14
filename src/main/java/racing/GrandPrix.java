58ae86c087d25faab6d36d04e21488820066e617
baki <hgs6424@naver.com>
2025-03-14 13:55:13 +0900

    feat: implement report function

--- src/main/java/racing/GrandPrix.java
+++ src/main/java/racing/GrandPrix.java
@@ -1,6 +1,7 @@
 package racing;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,10 +11,10 @@ public class GrandPrix {
     private final List<Round> rounds;
     private final List<Car> cars;
 
-    public GrandPrix(int totalRound, int totalCars) {
+    public GrandPrix(int totalRound, int totalCars, Random random) {
         this.currentRound = 0;
         this.rounds = IntStream.range(0, totalRound)
-                .mapToObj(i -> new Round(new Random()))
+                .mapToObj(i -> new Round(random))
                 .collect(Collectors.toUnmodifiableList());
         this.cars = IntStream.range(0, totalCars)
                 .mapToObj(i -> new Car())
@@ -28,6 +29,13 @@ public class GrandPrix {
     public void play() {
         rounds.get(currentRound).play(cars);
         currentRound++;
+    }
+
+    public Map<Integer, Integer> report() {
+        // sould return car index and car position
+        return IntStream.range(0, cars.size())
+                .boxed()
+                .collect(Collectors.toMap(i -> i, i -> cars.get(i).getPosition()));
     }
 
 }
