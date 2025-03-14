67bdcd8d1e1d07ce1db770c2c8f4b6c0ad5df95a
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-03-14 11:35:51 +0900

    refactor: step1

--- src/test/java/step1/study/SetTest.java
+++ src/test/java/step1/study/SetTest.java
@@ -1,4 +1,4 @@
-package step1;
+package step1.study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- src/test/java/step1/study/StringTest.java
+++ src/test/java/step1/study/StringTest.java
@@ -1,4 +1,4 @@
-package step1;
+package step1.study;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
