06b53863cc6ee789c8a3dc9c7c521080c6beae7b
melodist <melt.ep00@gmail.com>
2025-03-14 23:35:46 +0900

    Feat: StringAddCalculator 기능 추가 - “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있음

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,4 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
+
+    public static final String CHAR_SLASH = "//";
+    public static final String CHAR_LINE_BREAK = "\\n";
+    public static final String REGEX_DELIMITER = "[,:]";
+
     public static Integer splitAndSum(String text) {
         if (isInvalid(text)) {
             return 0;
@@ -30,6 +38,18 @@ public class StringAddCalculator {
     }
 
     private static String[] splitText(String text) {
-        return text.split("[,:]");
+        String customDelimiter = extractCustomDelimiter(text);
+        if (customDelimiter != null) {
+            text = text.replaceAll(CHAR_SLASH + customDelimiter + CHAR_LINE_BREAK, "");
+            return text.split(customDelimiter);
+        }
+        return text.split(REGEX_DELIMITER);
+    }
+
+    private static String extractCustomDelimiter(String text) {
+        Pattern pattern = Pattern.compile(CHAR_SLASH + "(.)" + CHAR_LINE_BREAK);
+        Matcher matcher = pattern.matcher(text);
+
+        return matcher.find() ? matcher.group(1) : null;
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -35,4 +35,11 @@ public class StringAddCalculatorTest {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
+
+    @DisplayName("“//”와 “\\n” 문자 사이에 커스텀 구분자를 지정할 수 있음")
+    @Test
+    public void splitAndSum_custom_구분자() throws Exception {
+        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+        assertThat(result).isEqualTo(6);
+    }
 }
