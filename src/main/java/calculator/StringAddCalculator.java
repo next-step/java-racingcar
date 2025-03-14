5b621a0cbea71c90cabc92f7c91438ad6b7bfb31
Gukin Han <115940366+gukin-han@users.noreply.github.com>
2025-03-14 12:25:15 +0900

    [자동차 경주 - 단위테스트] 2단계 - 문자열 덧셈 계산기 (#5914)
    * doc: 불필요한 전 단계 요구사항 삭제
    
    * test: TestCase 소스 코드
    
    * test: 테스트 코드 내 displayName 추가
    
    * feat: 문자열 덧셈 계산기능
    
    * refactor: private 메서드 순서를 public 내 순서에 맞게 조정
    
    * refactor: 스트링 배열을 불변 컬렉션 리스트 타입으로 반환하도록 수정

---
+++ src/main/java/calculator/StringAddCalculator.java
@@ -0,0 +1,57 @@
+package calculator;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static final Pattern DEFAULT_DELIMITERS_PATTERN = Pattern.compile("[,:]");
+    private static final Pattern CUSTOM_DELIMITER_PARSE_PATTERN = Pattern.compile("//(.)\n(.*)");
+    public static final String VALID_INTEGER_PATTERN = "-?\\d+";
+
+    public static int splitAndSum(String text) {
+
+        if (text == null ||  text.isEmpty()) {
+            return 0;
+        }
+
+        List<String> tokens = splitIntoTokens(text);
+        return sumPositiveNumbersOrThrow(tokens);
+    }
+
+    private static List<String> splitIntoTokens(String text) {
+        Matcher matcher = CUSTOM_DELIMITER_PARSE_PATTERN.matcher(text);
+
+        if (matcher.find()) {
+            String customDelimiter = Pattern.quote(matcher.group(1));
+            return List.of(matcher.group(2).split(customDelimiter));
+        }
+
+        return List.of(DEFAULT_DELIMITERS_PATTERN.split(text));
+    }
+
+    private static int sumPositiveNumbersOrThrow(List<String> tokens) {
+        int sum = 0;
+        for (String token : tokens) {
+            if (!isValidInteger(token)) {
+                throw new RuntimeException("Invalid input: " + token);
+            }
+
+            int num = Integer.parseInt(token);
+            if (num < 0) {
+                throw new RuntimeException("Negative numbers not allowed: " + token);
+            }
+
+            sum += num;
+        }
+        return sum;
+    }
+
+    private static boolean isValidInteger(String input) {
+        if (input == null || input.isEmpty()) {
+            return false;
+        }
+        return input.matches(VALID_INTEGER_PATTERN);
+    }
+}
