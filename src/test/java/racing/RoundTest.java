f116dd051d59c53d2ed807b525febdfa11d529d5
baki <hgs6424@naver.com>
2025-03-14 13:23:14 +0900

    test: modify round test

--- src/test/java/racing/RoundTest.java
+++ src/test/java/racing/RoundTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 class RoundTest {
 
     @Test
-    @DisplayName("라운드를 play하면 라운드가 종료된다")
-    void playTest() {
+    @DisplayName("라운드를 play하면 해당 라운드가 종료된다")
+    void finishRoundTest() {
         // given
         var round = new Round();
 
