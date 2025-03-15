1aea6a886a577ca99e203532a08282e6e3e52acb
zoolake <junhomoon224@gmail.com>
2025-03-16 00:37:37 +0900

    feat : implements car movement decision logic based on random value

--- src/main/java/racing/RacingGame.java
+++ src/main/java/racing/RacingGame.java
@@ -3,7 +3,10 @@ package racing;
 import java.util.Random;
 
 public class RacingGame {
-    private static final int MAX_VALUE = 9;
+    public static final int MIN_VALUE = 0; // random 최소값
+    public static final int MAX_VALUE = 9; // random 최대값
+
+    public static final int MIN_MOVE_VALUE = 4; // 전진 최소값
 
     public static void main(String[] args) {
         int carCount = RacingGameInputView.getIntegerInput(GameMessage.CAR_COUNT_MESSAGE);
@@ -15,6 +18,10 @@ public class RacingGame {
 
     public static int getRandomValue() {
         return new Random().nextInt(MAX_VALUE);
+    }
+
+    public static boolean isMovable(int input) {
+        return input >= MIN_MOVE_VALUE;
     }
 
 }
--- src/test/java/racing/RacingGameTest.java
+++ src/test/java/racing/RacingGameTest.java
@@ -2,6 +2,8 @@ package racing;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,6 +13,22 @@ class RacingGameTest {
     public void randomValue_is_valid_whenBetweenZeroAndNine() {
         int actual = RacingGame.getRandomValue();
         assertThat(actual).isBetween(0, 9);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {RacingGame.MIN_MOVE_VALUE, RacingGame.MIN_MOVE_VALUE + 2})
+    @DisplayName("Random value가 MIN_MOVE_VALUE 이상이면 전진 가능하다.")
+    public void car_moves_whenValueIsOrMore(int input) {
+        boolean actual = RacingGame.isMovable(input);
+        assertThat(actual).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {RacingGame.MIN_MOVE_VALUE - 1, RacingGame.MIN_MOVE_VALUE - 2})
+    @DisplayName("Random value가 MIN_MOVE_VALUE 미만이면 움직이지 않는다.")
+    public void car_stays_whenValueIsLessThan_MinMoveValue(int input) {
+        boolean actual = RacingGame.isMovable(input);
+        assertThat(actual).isFalse();
     }
 
 }
