81fca4a96c647ecfc30dfedb176a537eb6c78347
정핸모 <gusah009@naver.com>
2025-03-15 11:49:54 +0900

    feat: 시도할 횟수 입력 기능 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -13,4 +13,9 @@ public class InputView {
         System.out.println("자동차 대수는 몇 대 인가요?");
         return scanner.nextInt();
     }
+
+    public int inputTryCount() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        return scanner.nextInt();
+    }
 }
--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         InputView inputView = new InputView(new Scanner(System.in));
         int carCount = inputView.inputCarCount();
+        int tryCount = inputView.inputTryCount();
     }
 }
