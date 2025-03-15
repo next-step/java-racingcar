3b6db948dcff1be3befeb81f620c9edf95882cf7
ekzm8523 <mjw8523@gmail.com>
2025-03-15 12:53:02 +0900

    feat: StringAddCalculator 단일 숫자 String 처리

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -2,7 +2,7 @@ package calculator;
 
 class StringAddCalculator {
 
-    public static int splitAndSum(String str) {
+    public static int run(String str) {
         if (str == null || str.isEmpty()) {
             return 0;
         }
--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -19,7 +19,14 @@ class StringAddCalculatorTest {
     @Test
     @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
     void emptyStringOrNullTest() {
-        assertThat(StringAddCalculator.splitAndSum(null)).isZero();
-        assertThat(StringAddCalculator.splitAndSum("")).isZero();
+        assertThat(StringAddCalculator.run(null)).isZero();
+        assertThat(StringAddCalculator.run("")).isZero();
+    }
+
+    @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    void singleNumberStringTest() {
+        assertThat(StringAddCalculator.run("1")).isEqualTo(1);
+        assertThat(StringAddCalculator.run("3")).isEqualTo(3);
     }
 }
