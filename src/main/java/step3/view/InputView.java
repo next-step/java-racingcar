853b47798e67a27bfd8b1dcafe5e57fb365e1325
HongYeseul <yeseul.dev@gmail.com>
2025-03-15 23:03:13 +0900

    feat(step3): Input, OutputView 클래스 추가

---
+++ src/main/java/step3/view/InputView.java
@@ -0,0 +1,26 @@
+package step3.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static String inputCarCount() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return getInput();
+    }
+
+    public static String inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        return getInput();
+    }
+
+    private static String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static void closeScanner() {
+        scanner.close();
+    }
+}
