7aed8fbd198d293872f96dacbdc51c6dfaa2a95f
HongYeseul <yeseul.dev@gmail.com>
2025-03-16 08:53:19 +0900

    test(step3): 자동차 현재 위치 테스트 코드 추가

--- src/test/java/step3/CarTest.java
+++ src/test/java/step3/CarTest.java
@@ -2,26 +2,31 @@ package step3;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import step3.game.Car;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static step3.game.Car.START_LINE;
 
 public class CarTest {
-
-    public static final int START = 0;
 
     @Test
     @DisplayName("자동차는 전진할 수 있다.")
     void carCanMove() {
-        Car car = new Car(START);
+        Car car = new Car();
         car.move();
         assertThat(car.getDistance()).isEqualTo(1);
     }
 
-    @Test
-    @DisplayName("자동차는 멈출 수 있다.")
-    void carCanStop() {
-        Car car = new Car(START);
-        car.stop();
-        assertThat(car.getDistance()).isEqualTo(0);
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    @DisplayName("자동차의 현재 위치를 알 수 있다.")
+    void carCannotMove(int distance) {
+        Car car = new Car();
+        for (int i = 0; i < distance; i++) {
+            car.move();
+        }
+        assertThat(car.getDistance()).isEqualTo(START_LINE + distance);
     }
 }
