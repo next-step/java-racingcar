fb0377b00baa93ca707211d7cc5caee1e540e2de
lyeo72 <lyeo7272@gmail.com>
2025-03-16 23:17:29 +0900

    test: 쉼표구분자

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,10 +1,28 @@
 package calculator;
 
 public class StringCalculator {
+    private static final String regex = ",";
+
     public static int cal(String str) {
         if (str == null || str.isEmpty()) {
             return 0;
         }
-        return 0;
+        return sum(split(str));
+    }
+
+    private static String[] split(String str) {
+        return str.split(regex);
+    }
+
+    private static int sum(String[] strings) {
+        int result = 0;
+        for (String s : strings) {
+            result += toInts(s);
+        }
+        return result;
+    }
+
+    private static int toInts(String s) {
+        return Integer.parseInt(s);
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
@@ -8,16 +9,24 @@ import org.junit.jupiter.params.provider.NullSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /*
-1. 쉼표 구분자의 경우 구분자 기준으로 분리한 각 숫자의 합
 2. 콜론 구분자의 경우 구분자 기준으로 분리한 각 숫자의 합
 3. 커스텀 구분자를 지정할 수있다(“//;\n) 사이의 값
 4. 음수의 경우 RuntimeException 예외를 처리한다
 7. 숫자 하나(숫자만) 문자열로 입력할 경우 해당 숫자를 반환
 
+1. 쉼표 구분자의 경우 구분자 기준으로 분리한 각 숫자의 합
 5. 빈문자열의 경우 0을 반환
 6. Null 값을 입력할 경우 0 을 반환
  */
 public class StringCalculatorTest {
+
+    @Test
+    @DisplayName("쉼표 구분자")
+    void test1() {
+        int result = StringCalculator.cal("1,2");
+        assertThat(result).isEqualTo(3);
+    }
+
     @ParameterizedTest
     @NullSource
     @EmptySource
@@ -26,5 +35,4 @@ public class StringCalculatorTest {
         int result = StringCalculator.cal(input);
         assertThat(result).isEqualTo(0);
     }
-
 }
