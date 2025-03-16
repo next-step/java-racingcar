cd78ae851cf71b2816891815262d46c8d52568e1
gan-ta <gilmat@naver.com>
2025-03-16 20:51:55 +0900

    refactor: mock 의존성 제거 및 테스트 코드에서 stub 사용으로 수정

--- src/test/java/racing/simulator/RandomCarMovingStrategyTest.java
+++ src/test/java/racing/simulator/RandomCarMovingStrategyTest.java
@@ -3,13 +3,11 @@ package racing.simulator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mockito;
 
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.anyInt;
 
 class RandomCarMovingStrategyTest {
 
@@ -17,21 +15,29 @@ class RandomCarMovingStrategyTest {
   @ParameterizedTest
   @ValueSource(ints = {0, 1, 2, 3})
   void canMove_returnFalse(int randomNum) {
-    Random mockRandom = Mockito.mock(Random.class);
 
-    Mockito.when(mockRandom.nextInt(anyInt())).thenReturn(randomNum);
+    Random randomStub = new Random() {
+      @Override
+      public int nextInt(int bound) {
+        return randomNum;
+      }
+    };
 
-    assertFalse(new RandomCarMovingStrategy(mockRandom).isMove());
+    assertFalse(new RandomCarMovingStrategy(randomStub).isMove());
   }
 
   @DisplayName("움직일지 판단 할 시 4 이상 random 수로 뽑힐시 true를 반환한다.")
   @ParameterizedTest
   @ValueSource(ints = {4, 5, 6, 7, 8, 9})
   void canMove_returnTrue(int randomNum) {
-    Random mockRandom = Mockito.mock(Random.class);
 
-    Mockito.when(mockRandom.nextInt(anyInt())).thenReturn(randomNum);
+    Random randomStub = new Random() {
+      @Override
+      public int nextInt(int bound) {
+        return randomNum;
+      }
+    };
 
-    assertTrue(new RandomCarMovingStrategy(mockRandom).isMove());
+    assertTrue(new RandomCarMovingStrategy(randomStub).isMove());
   }
 }
