d037faba4243ad56bfac73cb0970af9d3a670dc0
selene-p <hwp0223@gmail.com>
2025-03-16 15:28:19 +0900

    feat: 숫자에 따라 자동차의 이동을 구현한다

---
+++ src/main/java/carracing/Car.java
@@ -0,0 +1,20 @@
+package carracing;
+
+public class Car {
+  private final int MOVE_THRESHOLD = 4;
+  private int currentPosition;
+
+  public int getPosition() {
+    return currentPosition;
+  }
+
+  public void move(int number) {
+    if (shouldMove(number)) {
+      currentPosition++;
+    }
+  }
+
+  private boolean shouldMove(int number) {
+    return number >= MOVE_THRESHOLD;
+  }
+}
--- src/test/java/carracing/CarRaceTest.java
+++ src/test/java/carracing/CarRaceTest.java
@@ -3,6 +3,8 @@ package carracing;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -45,4 +47,27 @@ public class CarRaceTest {
     assertEquals(expected, actual);
   }
 
+  @DisplayName("4 이상의 숫자일 때 자동차가 이동하는지를 테스트한다.")
+  @ParameterizedTest
+  @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+  void testCarMove(int number) {
+    Car testCar = new Car();
+    int currentPosition = testCar.getPosition();
+
+    testCar.move(number);
+
+    assertEquals(currentPosition+1, testCar.getPosition());
+  }
+
+  @DisplayName("4 미만의 숫자일 때 자동차가 이동하지 않는지를 테스트한다.")
+  @ParameterizedTest
+  @ValueSource(ints = {0, 1, 2, 3})
+  void testCarDoNotMove(int number) {
+    Car testCar = new Car();
+    int currentPosition = testCar.getPosition();
+
+    testCar.move(number);
+
+    assertEquals(currentPosition, testCar.getPosition());
+  }
 }
