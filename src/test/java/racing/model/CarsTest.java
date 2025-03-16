023005416e58fc702d690dcd53b433a5dc85983c
parkSeryu <pjh971212@gmail.com>
2025-03-16 19:43:50 +0900

    test: 제약사항 체크하면서 빠진 테스트 추가

--- src/test/java/racing/model/CarsTest.java
+++ src/test/java/racing/model/CarsTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racing.fake.FakeNumberGenerator;
@@ -24,6 +25,31 @@ class CarsTest {
 
         // then
         assertThat(cars.getPositions()).isEqualTo(Arrays.asList(1, 1, 1));
+    }
+
+    @DisplayName("리스트 안의 모든 자동차들의 현재 위치를 알 수 있다.")
+    @Test
+    void getPositionsTest() {
+        // given
+        cars = new Cars(makeDummyCars(7), new FakeNumberGenerator());
+
+        // when
+        List<Integer> positions = cars.getPositions();
+
+        // then
+        Assertions.assertAll(() -> assertThat(positions).isNotNull(),
+                () -> assertThat(positions).containsExactly(0, 0, 0, 0, 0, 0, 0));
+    }
+
+    @DisplayName("일급 콜렉션 안의 리스트의 총 개수를 알 수 있다.")
+    @Test
+    void sizeTest() {
+        // given
+        cars = new Cars(makeDummyCars(5), new FakeNumberGenerator());
+
+        // when
+        // then
+        assertThat(cars.size()).isEqualTo(5);
     }
 
     private List<Car> makeDummyCars(int carCount) {
