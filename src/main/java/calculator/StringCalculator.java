c35e4b95b1912547a23e053838144944199ea28a
JiEung2 <181757@jnu.ac.kr>
2025-03-16 15:23:54 +0900

    feat 커스텀 구분자를 지정하여 숫자를 구분하는 메서드 추가
    - split 메서드를 분리하여 split 기능만 하도록 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,6 +1,12 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String REGEX = "//(.)\n(.*)";
 
     public static int toInt(String stringNumber) {
         return Integer.parseInt(stringNumber);
@@ -8,10 +14,19 @@ public class StringCalculator {
 
     public static int calculate(String input) {
         int result = 0;
-        String[] numbers = input.split(",|:");
+        String[] numbers = splitNumbers(input);
         for (String number : numbers) {
             result += toInt(number);
         }
         return result;
+    }
+
+    private static String[] splitNumbers(String input) {
+        Matcher m = Pattern.compile(REGEX).matcher(input);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+        return input.split(DEFAULT_DELIMITER);
     }
 }
