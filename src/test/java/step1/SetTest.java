b86c75d74701648db27338e44fd79722bf11e58a
jlee0505 <hihjlee0505@gmail.com>
2025-03-16 16:43:35 +0900

    폴더 구조 step 별로 정리

--- src/test/java/step1/SetTest.java
+++ src/test/java/step1/SetTest.java
@@ -1,3 +1,5 @@
+package step1;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
--- src/test/java/step1/StringTest.java
+++ src/test/java/step1/StringTest.java
@@ -1,3 +1,5 @@
+package step1;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
