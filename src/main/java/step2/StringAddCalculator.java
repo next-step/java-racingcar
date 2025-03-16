f93a7fe4af808e60adabe804b6eefb5893131661
jaydenkjk <yajungi@hanmail.net>
2025-03-16 23:31:45 +0900

    2단계 - 문자열 덧셈 계산기 (#5985)
    * 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.
    
    * 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.
    
    * 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.
    
    * 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.
    
    * “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다.
    
    * 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다.
    
    * 불필요한 상수 제거
    
    * 메서드 추출
    
    ---------
    
    Co-authored-by: jayden(김준기) <jayden.i@kakaocorp.com>

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,91 @@
+package step2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Integer.parseInt;
+
+public class StringAddCalculator {
+    public static final String REGEX = ",|:";
+    public static final String CUSTOM_REGEX = "//(.)\n(.*)";
+    public static final int DELIMITER_GROUP = 1;
+    public static final int TEXT_GROUP = 2;
+
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_REGEX);
+
+    public static int splitAndSum(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        return sum(toInts(split(text)));
+    }
+
+    public static int sum(List<Integer> numbers) {
+        int result = 0;
+
+        for (int number : numbers) {
+            result += number;
+        }
+
+        return result;
+    }
+
+    public static List<Integer> toInts(String[] values) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String value : values) {
+            int number = getNumber(value);
+            numbers.add(number);
+        }
+
+        return numbers;
+    }
+
+    private static int getNumber(String value) {
+        int number = parseInt(value);
+
+        if (isNegative(number)) {
+            throw new RuntimeException("음수는 허용되지 않습니다.");
+        }
+
+        return number;
+    }
+
+    private static boolean isNegative(int number) {
+        return number < 0;
+    }
+
+    public static String[] split(String text) {
+        String delimiter = detectDelimiter(text);
+        String extractedText = extractText(text);
+
+        return splitText(extractedText, delimiter);
+    }
+
+    private static String detectDelimiter(String text) {
+        Matcher matcher = CUSTOM_PATTERN.matcher(text);
+
+        if (matcher.find()) {
+            return matcher.group(DELIMITER_GROUP);
+        }
+
+        return REGEX;
+    }
+
+    private static String extractText(String text) {
+        Matcher matcher = CUSTOM_PATTERN.matcher(text);
+
+        if (matcher.find()) {
+            return matcher.group(TEXT_GROUP);
+        }
+
+        return text;
+    }
+
+    private static String[] splitText(String text, String delimiter) {
+        return text.split(delimiter);
+    }
+}
