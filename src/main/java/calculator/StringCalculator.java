5ec1d07b1f491b01dce2ac3679465e43682793c2
choincnp <whdudwns7777@gmail.com>
2025-03-16 22:15:49 +0900

    test: Add delimiter(,) test

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,6 +2,18 @@ package calculator;
 
 public class StringCalculator {
     public static int calculate(String text) {
+        if (text.contains(",")) {
+            return getSum(text);
+        }
         return Integer.parseInt(text);
+    }
+
+    private static int getSum(String text) {
+        String[] values = text.split(",");
+        int result = 0;
+        for (String value : values) {
+            result += Integer.parseInt(value.trim());
+        }
+        return result;
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,7 +1,6 @@
 package calculator;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,5 +11,12 @@ public class StringCalculatorTest {
     void parseTest() {
         int result = StringCalculator.calculate("1");
         assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("숫자 두개를 컴마 구분자로 입력할 경우 합을 반환")
+    void delimiterTest() {
+        int result = StringCalculator.calculate("1, 2");
+        assertThat(result).isEqualTo(3);
     }
 }
