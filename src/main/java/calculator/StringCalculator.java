35c023dbbe807e472fd7b54fdfa9b9d5fad3cd4d
JiEung2 <181757@jnu.ac.kr>
2025-03-16 15:03:45 +0900

    feat 숫자 두 개를 컴마 구분자로 구분하여 숫자의 합을 반환하는 메서드 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,4 +5,13 @@ public class StringCalculator {
     public static int toInt(String stringNumber) {
         return Integer.parseInt(stringNumber);
     }
+
+    public static int calculate(String input) {
+        int result = 0;
+        String[] numbers = input.split(",");
+        for (String number : numbers) {
+            result += toInt(number);
+        }
+        return result;
+    }
 }
