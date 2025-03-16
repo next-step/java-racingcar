f096b984c228764c4b71e35c00ad355c8d6c71d8
JiEung2 <181757@jnu.ac.kr>
2025-03-16 16:14:19 +0900

    feat 음수가 전달되었는지 검증하는 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -14,15 +14,30 @@ public class StringCalculator {
 
     public static int calculate(String input) {
         if (isEmptyInput(input)) return 0;
-        return sum(splitNumbers(input));
+        return sum(toInts(splitNumbers(input)));
     }
 
-    private static int sum(String[] numbers) {
+    private static int sum(int[] numbers) {
         int result = 0;
-        for (String number : numbers) {
-            result += toInt(number);
+        for (int number : numbers) {
+            result += number;
         }
         return result;
+    }
+
+    private static int[] toInts(String[] numbers) {
+        int[] result = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            result[i] = validateNegativeNumber(toInt(numbers[i]));
+        }
+        return result;
+    }
+
+    private static int validateNegativeNumber(int number) {
+        if (number < 0) {
+            throw new RuntimeException("음수는 허용되지 않습니다.");
+        }
+        return number;
     }
 
     private static boolean isEmptyInput(String input) {
