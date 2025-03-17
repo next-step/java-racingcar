cc17b14d1f228b81a1e2123efa38bf0cc4f0a016
charles.cheol <charles.cheol@kakaocorp.com>
2025-03-17 18:37:21 +0900

    테스트 메소드명 수정

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -8,40 +9,44 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
 
-    @ParameterizedTest
-    @NullAndEmptySource
-    public void splitAndSum_null_또는_빈문자(String input) {
-        int result = StringAddCalculator.splitAndSum(input);
-        assertThat(result).isEqualTo(0);
-    }
+    @Nested
+    class SplitAndSumTest {
 
-    @Test
-    public void splitAndSum_숫자하나() {
-        int result = StringAddCalculator.splitAndSum("1");
-        assertThat(result).isEqualTo(1);
-    }
+        @ParameterizedTest
+        @NullAndEmptySource
+        public void nullOrEmptyTest(String input) {
+            int result = StringAddCalculator.splitAndSum(input);
+            assertThat(result).isEqualTo(0);
+        }
 
-    @Test
-    public void splitAndSum_쉼표구분자() {
-        int result = StringAddCalculator.splitAndSum("1,2");
-        assertThat(result).isEqualTo(3);
-    }
+        @Test
+        public void singleNumberTest() {
+            int result = StringAddCalculator.splitAndSum("1");
+            assertThat(result).isEqualTo(1);
+        }
 
-    @Test
-    public void splitAndSum_쉼표_또는_콜론_구분자() {
-        int result = StringAddCalculator.splitAndSum("1,2:3");
-        assertThat(result).isEqualTo(6);
-    }
+        @Test
+        public void commaDelimiterTest() {
+            int result = StringAddCalculator.splitAndSum("1,2");
+            assertThat(result).isEqualTo(3);
+        }
 
-    @Test
-    public void splitAndSum_custom_구분자() {
-        int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
-        assertThat(result).isEqualTo(6);
-    }
+        @Test
+        public void commaOrColonTest() {
+            int result = StringAddCalculator.splitAndSum("1,2:3");
+            assertThat(result).isEqualTo(6);
+        }
 
-    @Test
-    public void splitAndSum_negative() {
-        assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
-            .isInstanceOf(RuntimeException.class);
+        @Test
+        public void customDelimiterTest() {
+            int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+            assertThat(result).isEqualTo(6);
+        }
+
+        @Test
+        public void negativeNumberTest() {
+            assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+                .isInstanceOf(RuntimeException.class);
+        }
     }
 }
