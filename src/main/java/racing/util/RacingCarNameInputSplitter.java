6c559b2c23036ab02888fb49db85e4335813fb83
gan-ta <gilmat@naver.com>
2025-03-17 23:21:19 +0900

    feat: RacingCarNameInputSplitter 구현

--- src/main/java/racing/util/RacingCarNameInputSplitter.java
+++ src/main/java/racing/util/RacingCarNameInputSplitter.java
@@ -3,12 +3,22 @@ package racing.util;
 import racing.types.RacingCarName;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCarNameInputSplitter {
 
   private static final int MIN_CAR_COUNT = 2;
 
   public static List<RacingCarName> split(String racingCarNames) {
+    if (racingCarNames == null) {
+      throw new NullPointerException("차 이름들의 값은 null로 들어올 수 없습니다.");
+    }
 
+    List<String> nameList = List.of(racingCarNames.split(","));
+    if (nameList.size() < MIN_CAR_COUNT) {
+      throw new IllegalArgumentException("차 이름들의 값은 2개 이상으로 들어와야 합니다.");
+    }
+
+    return nameList.stream().map(RacingCarName::new).collect(Collectors.toList());
   }
 }
--- src/test/java/racing/util/RacingCarNameInputSplitterTest.java
+++ src/test/java/racing/util/RacingCarNameInputSplitterTest.java
@@ -15,9 +15,7 @@ class RacingCarNameInputSplitterTest {
   @DisplayName("차의 이름들을 받았을 때 모두 5자리 이하 2자리 이상의 이름이면 쉼표(,) 단위로 구분한다.")
   @Test
   void splitCarNames_givenCommaSeparatedString_returnsListOfCarNames() {
-    String input="pobi,crong,honux";
-
-    List<RacingCarName> result = RacingCarNameInputSplitter.split(input);
+    List<RacingCarName> result = RacingCarNameInputSplitter.split("pobi,crong,honux");
 
     assertThat(result)
         .isNotNull()
@@ -29,16 +27,18 @@ class RacingCarNameInputSplitterTest {
   @DisplayName("차의 이름들을 받았을 때 2대 미만이면 RuntimeException을 던진다.")
   @Test
   void throwRuntimeException_whenLessThanTwoCarNamesProvided() {
-    String input="pobi";
-
-    assertThrows(RuntimeException.class, () -> RacingCarNameInputSplitter.split(input));
+    assertThrows(RuntimeException.class, () -> RacingCarNameInputSplitter.split("pobi"));
   }
 
   @DisplayName("차의 이름들을 받았을 때 이름 중 하나라도 빈 문자열이면 RuntimeException을 던진다.")
   @Test
   void throwRuntimeException_whenAnyCarNameIsEmpty() {
-    String input="pobi,,crong";
+    assertThrows(RuntimeException.class, () -> RacingCarNameInputSplitter.split("pobi,,crong"));
+  }
 
-    assertThrows(RuntimeException.class, () -> RacingCarNameInputSplitter.split(input));
+  @DisplayName("차의 이름들을 받았을 때 null이면 RuntimeException을 던진다.")
+  @Test
+  void throwRuntimeException_whenCarNamesNull() {
+    assertThrows(RuntimeException.class, () -> RacingCarNameInputSplitter.split(null));
   }
 }
