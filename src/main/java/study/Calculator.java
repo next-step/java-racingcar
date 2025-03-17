172a3fd62b12a10dc0fb0d3e8f81a4ec372a45f3
kjy2844 <kjy2844@kaist.ac.kr>
2025-03-17 13:37:45 +0900

    refactor : Parsing하는 객체 분리

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -3,39 +3,46 @@ package study;
 import java.util.regex.Pattern;
 
 public class Calculator {
-
-    private static final String DEFAULT_NUMBERS_DELIMITER_REGEX = "[,:]";
-    private static final String CUSTOM_DELIMITER_PREFIX = "//";
-    private static final String NEW_LINE = "\n";
-
     public static int sum(String numbersWithDelimiter) {
         if (numbersWithDelimiter == null || numbersWithDelimiter.isBlank()) {
             return 0;
         }
 
-        return add(parseNonNegativeInts(split(numbersWithDelimiter)));
+        NumberDelimiterParser result = new NumberDelimiterParser(numbersWithDelimiter);
+        return add(parseNonNegativeInts(split(result.numbers, result.delimiter)));
     }
 
-    private static String[] split(String numbersWithDelimiter) {
-        String delimiter = computeDelimiter(numbersWithDelimiter);
-        String stringToSplit = extractStringToSplit(numbersWithDelimiter);
-        return stringToSplit.split(delimiter);
-    }
+    private static class NumberDelimiterParser {
+        private static final String DEFAULT_NUMBERS_DELIMITER_REGEX = "[,:]";
+        private static final String CUSTOM_DELIMITER_PREFIX = "//";
+        private static final String NEW_LINE = "\n";
 
-    private static String computeDelimiter(String numbersWithDelimiter) {
-        if (numbersWithDelimiter.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+        public final String delimiter;
+        public final String numbers;
+
+        public NumberDelimiterParser(String numbersWithDelimiter) {
             int delimiterEnd = numbersWithDelimiter.indexOf(NEW_LINE);
-            return Pattern.quote(numbersWithDelimiter.substring(2, delimiterEnd));
+            this.delimiter = computeDelimiter(numbersWithDelimiter, delimiterEnd);
+            this.numbers = extractStringToSplit(numbersWithDelimiter, delimiterEnd);
         }
-        return DEFAULT_NUMBERS_DELIMITER_REGEX;
+
+        private static String computeDelimiter(String numbersWithDelimiter, int delimiterEnd) {
+            if (numbersWithDelimiter.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+                return Pattern.quote(numbersWithDelimiter.substring(2, delimiterEnd));
+            }
+            return DEFAULT_NUMBERS_DELIMITER_REGEX;
+        }
+
+        private static String extractStringToSplit(String numbersWithDelimiter, int delimiterEnd) {
+            if (numbersWithDelimiter.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+                return numbersWithDelimiter.substring(delimiterEnd + 1);
+            }
+            return numbersWithDelimiter;
+        }
     }
 
-    private static String extractStringToSplit(String numbersWithDelimiter) {
-        if (numbersWithDelimiter.startsWith(CUSTOM_DELIMITER_PREFIX)) {
-            int delimiterEnd = numbersWithDelimiter.indexOf(NEW_LINE);
-            return numbersWithDelimiter.substring(delimiterEnd + 1);
-        }
-        return numbersWithDelimiter;
+    private static String[] split(String numbers, String delimiter) {
+        return numbers.split(delimiter);
     }
 
     private static int[] parseNonNegativeInts(String[] numberStrings) {
