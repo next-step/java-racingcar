c2bd915b1cbef3e38e0fca2ac0129b2d47851003
sean-wd <sean.wd@kakaocorp.com>
2025-03-17 16:29:24 +0900

    refactor: input 개선

--- src/main/java/step3/InputView.java
+++ src/main/java/step3/InputView.java
@@ -4,14 +4,25 @@ import java.util.Scanner;
 
 public class InputView {
 
+  private static final String NUM_CAR_QUESTION = "자동차 대수는 몇 대 인가요?";
+  private static final String NUM_TRY_QUESTION = "시도할 회수는 몇 회 인가요?";
   private final Scanner scanner;
 
   public InputView() {
     scanner = new Scanner(System.in);
   }
 
-  public int askInt(String question) {
-    System.out.println(question);
+  public int getNumCars() {
+    System.out.println(NUM_CAR_QUESTION);
+    return askInt();
+  }
+
+  public int getNumTry() {
+    System.out.println(NUM_TRY_QUESTION);
+    return askInt();
+  }
+
+  private int askInt() {
     return scanner.nextInt();
   }
 }
--- src/main/java/step3/MainApplication.java
+++ src/main/java/step3/MainApplication.java
@@ -6,8 +6,8 @@ public class MainApplication {
 
   public static void main(String[] args) {
     InputView inputView = new InputView();
-    int numCars = inputView.askInt("자동차 대수는 몇 대 인가요?");
-    int numTry = inputView.askInt("시도할 회수는 몇 회 인가요?");
+    int numCars = inputView.getNumCars();
+    int numTry = inputView.getNumTry();
 
     Racer racer = new Racer(numCars, numTry);
     Map<Integer, int[]> result = racer.race();
