ad8f431cc43573cea96e3ac0c07adc5a46733a7f
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-17 21:43:55 +0900

    CarPositionsTest DisplayName 추가

--- src/test/java/step3/CarPositionsTest.java
+++ src/test/java/step3/CarPositionsTest.java
@@ -2,12 +2,14 @@ package step3;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarPositionsTest {
     @Test
+    @DisplayName("CarPositions 객체 생성 후 위치 리스트 크기를 확인한다")
     void addPosition() {
         List<String> positions = List.of("-", "", "--");
         CarPositions carPositions = new CarPositions(positions);
