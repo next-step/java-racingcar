8e404528dfe8a88c1aa9c4fb8517a04dbc407c5c
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-17 19:07:48 +0900

    사용자에게 자동차 대수와 시도할 횟수를 입력받는다. (음수 및 문자는 입력 받을 수 없다.)

---
+++ src/main/java/step3/InputView.java
@@ -0,0 +1,41 @@
+package step3;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int getCarCount() throws InvalidInputException {
+        return readInt("자동차 대수는 몇 대 인가요?");
+    }
+
+    public int getMovementCount() throws InvalidInputException {
+        return readInt("시도할 회수는 몇 회 인가요?");
+    }
+
+    public int readInt(String prompt) throws InvalidInputException {
+        System.out.println(prompt);
+        return validateInput();
+    }
+
+    private int validateInput() throws InvalidInputException {
+        if (scanner.hasNextInt()) {
+            return getPositiveNumber(scanner.nextInt());
+        }
+
+        throw new InvalidInputException("정수를 입력해 주세요.");
+    }
+
+    private int getPositiveNumber(int value) throws InvalidInputException {
+        if (value > 0) {
+            return value;
+        }
+
+        throw new InvalidInputException("0보다 큰 정수를 입력해 주세요.");
+    }
+}
---
+++ src/test/java/step3/InputViewTest.java
@@ -0,0 +1,52 @@
+package step3;
+
+import java.util.Scanner;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewTest {
+
+    @Test
+    @DisplayName("자동차 대수 입력 테스트")
+    void inputCarTest() throws InvalidInputException {
+        Scanner scanner = new Scanner("5");
+        InputView inputView = new InputView(scanner);
+
+        int carCount = inputView.getCarCount();
+        assertThat(carCount).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("시도 횟수 입력 테스트")
+    void inputMovementTest() throws InvalidInputException {
+        Scanner scanner = new Scanner("3");
+        InputView inputView = new InputView(scanner);
+
+        int movementCount = inputView.getMovementCount();
+        assertThat(movementCount).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("자동차 대수에 0을 입력하면 예외 발생")
+    void inputCarTestException() {
+        Scanner scanner = new Scanner("0");
+        InputView inputView = new InputView(scanner);
+
+        assertThatThrownBy(inputView::getCarCount)
+            .isInstanceOf(InvalidInputException.class);
+    }
+
+    @Test
+    @DisplayName("시도 횟수에 문자를 입력하면 예외 발생")
+    void inputMovementTestException() {
+        Scanner scanner = new Scanner("sss");
+        InputView inputView = new InputView(scanner);
+
+        assertThatThrownBy(inputView::getMovementCount)
+            .isInstanceOf(InvalidInputException.class);
+    }
+}
