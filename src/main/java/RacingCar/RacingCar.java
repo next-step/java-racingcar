16df74831c1d5c0d61f27a3765c55129e2b334a4
cm0x0x0x0 <cm0x0x0x0@gmail.com>
2025-03-17 20:08:49 +0900

    feat: Implement RacingCar, RacingCarController

---
+++ src/main/java/RacingCar/RacingCar.java
@@ -0,0 +1,39 @@
+package RacingCar;
+
+import java.util.Random;
+
+public class RacingCar {
+    private int moveCondition;
+    private int position;
+    private Random random;
+
+    public RacingCar(int moveCondition) {
+        this.moveCondition = moveCondition;
+        this.position = 0;
+        this.random = new Random();
+    }
+
+    // ==============================
+    // 🔹 Internal logic methods
+    // ==============================
+    boolean checkMoveCondition(int num) {
+        if (num >= this.moveCondition) {
+            return true;
+        }
+
+        return false;
+    }
+
+    void move() {
+        this.position += 1;
+    }
+
+    int generateRandom(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    int position(){
+        return this.position;
+    }
+}
+
---
+++ src/main/java/RacingCar/RacingCarController.java
@@ -0,0 +1,69 @@
+package RacingCar;
+
+import java.util.Scanner;
+
+public class RacingCarController {
+    private RacingCar[] cars;
+    private int carNum;
+    private int tryNum;
+    private static final int MOVE_CONDITION = 4;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+        scanInputs();
+        printResultNotice();
+        run();
+    }
+
+    // ==============================
+    // 🔹 Input, output methods
+    // ==============================
+    private void scanInputs() {
+        scanCarNum();
+        scanTryNum();
+    }
+
+    private void scanCarNum() {
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        this.carNum = this.scanner.nextInt();
+    }
+
+    private void scanTryNum() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        this.tryNum = this.scanner.nextInt();
+    }
+
+    private void printResultNotice() {
+        System.out.println("실행 결과");
+    }
+
+    // ==============================
+    // 🔹 Internal logic methods
+    // ==============================
+    private void initializeCars() {
+        this.cars = new RacingCar[this.carNum];
+
+        for (int i = 0; i < carNum; i++) {
+            cars[i] = new RacingCar(MOVE_CONDITION);
+        }
+    }
+
+    private void run() {
+        initializeCars();
+        for (int i = 0; i < tryNum; i++) {
+            for (int j = 0; j < carNum; j++) {
+                int randNum = cars[j].generateRandom(0, 9);
+                if (cars[j].checkMoveCondition(randNum)){
+                    cars[j].move();
+                }
+                System.out.println("-".repeat(cars[j].position()));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        RacingCarController controller = new RacingCarController();
+        controller.start();
+    }
+}
