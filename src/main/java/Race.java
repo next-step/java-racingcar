9214519d67f5bfcdbaadb12cf63449b879e9b9f6
kjy2844 <kjy2844@kaist.ac.kr>
2025-03-17 16:01:36 +0900

    feat : 한 라운드를 진행하면 차는 최대 한 칸 움직인다.

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,24 +1,45 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Race {
 
     private final int carCount;
-    private final int roundCount;
-    private final List<Integer> carPositions;
+    private final int totalRounds;
 
-    public Race(int carCount, int roundCount) {
-        if (carCount < 1 || roundCount < 1) {
+    private List<Integer> carPositions;
+    private int currentRound = 0;
+
+    private static final Random random = new Random();
+
+    public Race(int carCount, int totalRounds) {
+        if (carCount < 1 || totalRounds < 1) {
             throw new IllegalArgumentException();
         }
 
         this.carCount = carCount;
-        this.roundCount = roundCount;
+        this.totalRounds = totalRounds;
         this.carPositions = new ArrayList<>(Collections.nCopies(carCount, 0));
     }
 
-    public List<Integer> getCarPostions() {
-        return carPositions;
+    public List<Integer> getCarPositions() {
+        return List.of(carPositions.toArray(new Integer[0]));
+    }
+
+    public void runRound() {
+        if (currentRound >= totalRounds) {
+            throw new IllegalStateException("Race has already finished");
+        }
+
+        for (int i = 0; i < carCount; i++) {
+            carPositions.set(i, move(carPositions.get(i), random.nextInt(10)));
+        }
+        currentRound++;
+    }
+
+    private int move(int initialPosition, int seed) {
+        if (seed >= 4) return initialPosition + 1;
+        return initialPosition;
     }
 }
--- src/test/java/RaceTest.java
+++ src/test/java/RaceTest.java
@@ -1,6 +1,7 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +18,7 @@ class RaceTest {
         });
 
         Race race = new Race(1, 3);
-        assertThat(race.getCarPostions()).hasSize(1);
+        assertThat(race.getCarPositions()).hasSize(1);
     }
 
     @Test
@@ -35,8 +36,33 @@ class RaceTest {
     @DisplayName("경기 시작시 자동차의 위치는 0이다.")
     void carPositionsAtStartAreZero() {
         Race race = new Race(5, 3);
-        for (Integer carPositions : race.getCarPostions()) {
+        for (Integer carPositions : race.getCarPositions()) {
             assertThat(carPositions).isZero();
         }
+    }
+
+    @Test
+    @DisplayName("한 라운드가 진행되면 자동차의 위치는 기존 위치이거나, 기존 위치 + 1이다.")
+    void carPositionsAfterOneRoundAreEitherSameOrIncremented() {
+        Race race = new Race(5, 3);
+        race.runRound();
+        List<Integer> initialPositions = race.getCarPositions();
+
+        race.runRound();
+        List<Integer> finalPositions = race.getCarPositions();
+
+        for (int i = 0; i < initialPositions.size(); i++) {
+            assertThat(finalPositions.get(i)).isIn(initialPositions.get(i), initialPositions.get(i) + 1);
+        }
+    }
+
+    @Test
+    @DisplayName("전체 라운드를 넘어가면 에러가 발생한다.")
+    void exceedingTotalRoundsThrowsError() {
+        Race race = new Race(5, 3);
+        for (int i = 0; i < 3; i++) {
+            race.runRound();
+        }
+        assertThrows(IllegalStateException.class, race::runRound);
     }
 }
