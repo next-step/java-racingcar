aa28913c5b73b38a7e930f34e95483813b8637dc
ParkSeryu <pjh971212@gmail.com>
2025-03-17 18:57:33 +0900

    refactor: 반복마다 자동차들의 상태를 출력하도록 변경

--- src/main/java/racing/RacingGameApplication.java
+++ src/main/java/racing/RacingGameApplication.java
@@ -2,38 +2,26 @@ package racing;
 
 import static racing.view.InputView.getCarCount;
 import static racing.view.InputView.getMoveCount;
-import static racing.view.OutputView.printResult;
 
 import racing.model.Cars;
 import racing.service.RaceService;
 import racing.service.RandomNumberGenerator;
+import racing.view.OutputView;
 
 public class RacingGameApplication {
     private static final int RAND = 10;
 
     public static void main(String[] args) {
-        // 1. INPUT
         int carCount = getCarCount();
         int moveCount = getMoveCount();
 
-        // 2. GAME
         RaceService raceService = new RaceService(new RandomNumberGenerator(RAND));
         Cars cars = raceService.generateCar(carCount);
-        StringBuilder gameStatus = new StringBuilder();
 
+        OutputView.printResultLabel();
         for (int i = 0; i < moveCount; i++) {
             raceService.moveCar(cars);
-            appendCarPosition(cars, gameStatus);
-            gameStatus.append("\n");
-        }
-
-        // 3. OUTPUT
-        printResult(gameStatus.toString());
-    }
-
-    private static void appendCarPosition(Cars cars, StringBuilder gameStatus) {
-        for (Integer position : cars.getPositions()) {
-            gameStatus.append("-".repeat(position)).append("\n");
+            OutputView.printResult(cars);
         }
     }
 }
--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -1,9 +1,22 @@
 package racing.view;
 
+import java.util.List;
+import racing.model.Cars;
+
 public class OutputView {
-    public static void printResult(String gameStatus) {
-        System.out.println("실행 결과");
-        System.out.println(gameStatus);
+
+    public static final String RESULT_LABEL = "실행 결과";
+
+    public static void printResultLabel() {
+        System.out.println(RESULT_LABEL);
+    }
+
+    public static void printResult(Cars cars) {
+        List<Integer> positions = cars.getPositions();
+        for (Integer position : positions) {
+            System.out.println("-".repeat(position));
+        }
+        System.out.println();
     }
 
 }
