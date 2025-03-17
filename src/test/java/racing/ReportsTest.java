8459509d1c0678cb5e8fbf5ecb5aa5171f2fda8e
baki <baki.me@kakaocorp.com>
2025-03-17 18:45:02 +0900

    test: add print test for Reports class with mock ScoreBoard

--- src/test/java/racing/ReportsTest.java
+++ src/test/java/racing/ReportsTest.java
@@ -1,10 +1,13 @@
 package racing;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import racing.Reports.ScoreBoard;
 
 class ReportsTest {
 
@@ -22,5 +25,32 @@ class ReportsTest {
         reports.getReports().forEach(report -> Assertions.assertThat(report.getPosition()).isEqualTo(0));
     }
 
+    @Test
+    @DisplayName("파라미터로 전달된 방법으로 print한다.")
+    void printTest() {
+        // given
+        var testCars = List.of(new Car(), new Car());
+        var reports = new Reports(testCars);
+        var mockPrintStrategy = new MockScoreBoard();
 
+        // when
+        reports.print(mockPrintStrategy);
+
+        // then
+        Assertions.assertThat(mockPrintStrategy.getSize()).isEqualTo(testCars.size());
+        reports.getReports().forEach(score -> Assertions.assertThat(score).isEqualTo(0));
+    }
+
+    static class MockScoreBoard implements ScoreBoard {
+        private final List<Integer> scores = new ArrayList<>();
+
+        @Override
+        public void print(int position) {
+            scores.add(position);
+        }
+
+        int getSize() {
+            return scores.size();
+        }
+    }
 }
