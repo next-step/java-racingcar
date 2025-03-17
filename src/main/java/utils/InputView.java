7d087313d5fcfd322e16a051169182918db16e23
JiEung2 <181757@jnu.ac.kr>
2025-03-17 22:50:36 +0900

    feat 시도할 횟수를 입력받는 기능 추가

--- src/main/java/utils/InputView.java
+++ src/main/java/utils/InputView.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회 인가요?";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputCarCount() {
+        println(INPUT_CAR_COUNT_MESSAGE);
+        return toInt(inputNextLine());
+    }
+
+    public static int inputTryCount() {
+        println(INPUT_TRY_COUNT_MESSAGE);
         return toInt(inputNextLine());
     }
 
@@ -16,5 +23,9 @@ public class InputView {
 
     private static int toInt(String number) {
         return Integer.parseInt(number);
+    }
+
+    private static void println(String message) {
+        System.out.println(message);
     }
 }
