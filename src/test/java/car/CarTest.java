45cb60afd389ad75f473c2cba9bab8bf24081512
JiEung2 <181757@jnu.ac.kr>
2025-03-17 22:29:27 +0900

    test 숫자가 4이상인지 판별하는 기능을 테스트하는 코드 추가

--- src/test/java/car/CarTest.java
+++ src/test/java/car/CarTest.java
@@ -2,6 +2,8 @@ package car;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -12,5 +14,13 @@ public class CarTest {
         Car car = new Car();
         car.move();
         assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("전달받은 숫자가 4 이상일 때 true, 4 미만이라면 false를 반환한다.")
+    @CsvSource(value = {"4:true", "3:false", "5:true", "1:false"}, delimiter = ':')
+    void 숫자_판단(int input, boolean expected) {
+        Car car = new Car();
+        assertThat(car.isAtLeastBaseNumber(input)).isEqualTo(expected);
     }
 }
