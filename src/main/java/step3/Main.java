294f4ee116d3f6a3b05719006e2418bfc5bba2f3
heewonham <heewonham6@gmail.com>
2025-03-17 18:30:26 +0900

    feat : 코드 수정에 따른 메인 함수 분리

--- src/main/java/step3/Main.java
+++ src/main/java/step3/Main.java
@@ -1,8 +1,14 @@
 package step3;
 
 
+import step3.game.Car;
+import step3.game.CarFactory;
+import step3.game.RacingGame;
 import step3.random.DefaultRandomStrategy;
 import step3.util.InputView;
+import step3.util.OutputView;
+
+import java.util.List;
 
 public class Main {
     private static final String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
@@ -10,8 +16,9 @@ public class Main {
     public static void main(String[] args) {
         int carCount = InputView.inputInt(CAR_COUNT_MESSAGE);
         int moveCount = InputView.inputInt(MOVE_COUNT_MESSAGE);
-        RacingGame game = new RacingGame(carCount, moveCount, new DefaultRandomStrategy());
-        System.out.print("\n실행 결과");
-        System.out.println(game.start());
+        List<Car> cars = CarFactory.createCars(carCount);
+        RacingGame game = new RacingGame(moveCount, new DefaultRandomStrategy(), cars);
+        List<List<Integer>> result = game.start();
+        OutputView.printResult(result);
     }
 }
