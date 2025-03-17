a88c5c9165fc525f20710d8bef61aa2811856c1f
pol.o(김선동)/kakao <pol.o@kakaocorp.com>
2025-03-17 18:15:22 +0900

    test: 게임 시작 시 자동차 객체 초기 init 테스트 코드

--- src/test/java/racingcar/CarGeneratorTest.java
+++ src/test/java/racingcar/CarGeneratorTest.java
@@ -1,4 +1,18 @@
 package racingcar;
 
-public class CarGeneratorTest {
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class CarGeneratorTest {
+    private Integer carCount = 3;
+
+    @Test
+    @DisplayName("주어진 수 만큼 자동차가 생성되어야 한다.")
+    void generateCars() {
+        List<Car> cars = new CarGenerator().generateCar(carCount);
+        Assertions.assertThat(cars.size()).isEqualTo(carCount);
+    }
 }
