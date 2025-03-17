a0b10fbbf2d2bd6fe78ab515cbacde2d7868728e
selene-p <hwp0223@gmail.com>
2025-03-17 21:18:50 +0900

    feat: 자동차 경주의 우승자를 가리는 기능 구현

--- src/test/java/step4/carracing/CarRacingTest.java
+++ src/test/java/step4/carracing/CarRacingTest.java
@@ -105,4 +105,25 @@ public class CarRacingTest {
     Assertions.assertEquals(expected, outputStream.toString());
   }
 
+  @DisplayName("자동차 경주의 우승자를 구하는 기능 테스트")
+  @Test
+  void testGetWinners() {
+    List<Car> cars = List.of(
+            new Car("pobi"),
+            new Car("crong"),
+            new Car("honux")
+    );
+    cars.get(0).move(4);
+    cars.get(0).move(4);
+    cars.get(1).move(4);
+    cars.get(1).move(4);
+    cars.get(2).move(1);
+    cars.get(2).move(4);
+
+    List<Car> expected = List.of(cars.get(0), cars.get(1));
+    List<Car> actual = CarRacingJudge.judgeWinners(cars);
+
+    assertEquals(expected, actual);
+  }
+
 }
