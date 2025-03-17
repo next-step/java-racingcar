b6a3232e5cb7739b26bce314dff018e082f1eb6a
yang Jiyoung <0112jyoung@naver.com>
2025-03-17 22:53:40 +0900

    [refactor] cars 변수 불변으로 수정

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
-    private static List<Car> cars;
+    private final List<Car> cars;
 
     public Cars(int carCounts) {
         cars = new ArrayList<>();
