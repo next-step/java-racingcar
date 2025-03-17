a549b4e4a9057172550c8fb36192e0ab5e214a51
minizi <kohminji10@gmail.com>
2025-03-17 21:25:31 +0900

    Step2 - 문자열 덧셈 계산기 (#5989)
    * comment: step1 comment 반영
    
    * feat: step2 StringAddCalculator 구현
    
    * refactor: StringAddCalculator
    
    * docs: step2 todo
    
    * refactor: @ParameterizedTest 활용하여 한 개의 테스트로 변경
    
    * refactor: static final 활용하여 상수 공유
    
    * refactor: stream 활용하는 방식으로 변경
    
    * style: intellij idea code style

---
+++ src/main/java/StringAddCalculator.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String DEFAULT_DELIMITER_REGEX = ",|:";
+
+    public int splitAndSum(String text) {
+        if (isNullOrBlank(text)) return 0;
+
+        return sum(convertToNumbers(splitByDelimiter(text)));
+    }
+
+    private boolean isNullOrBlank(String text) {
+        return text == null || text.isBlank();
+    }
+
+    private String[] splitByDelimiter(String text) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String textWithCustomDelimiter = matcher.group(2);
+            return textWithCustomDelimiter.split(combineDefaultDelimiterWith(customDelimiter));
+        }
+        return text.split(DEFAULT_DELIMITER_REGEX);
+    }
+
+    private String combineDefaultDelimiterWith(String customDelimiter) {
+        return String.format("%s|%s", DEFAULT_DELIMITER_REGEX, Pattern.quote(customDelimiter));
+    }
+
+    private int[] convertToNumbers(String[] tokens) {
+        return Arrays.stream(tokens)
+                .mapToInt(Integer::parseInt)
+                .filter(this::validatePositiveOrZero)
+                .toArray();
+    }
+
+    private boolean validatePositiveOrZero(int number) {
+        if (number < 0) throw new NumberFormatException();
+        return true;
+    }
+
+    private int sum(int[] numbers) {
+        return Arrays.stream(numbers).sum();
+    }
+}
