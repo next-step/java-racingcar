627dde036217a83515030ee4536deec556f0761d
theo-94 <theo.94@kakaocorp.com>
2025-03-17 21:04:53 +0900

    feat: 우승자를 출력한다.
    - PrintService에서 우승자를 출력한다.

--- src/main/java/car_racing/view/service/PrintService.java
+++ src/main/java/car_racing/view/service/PrintService.java
@@ -1,11 +1,15 @@
 package car_racing.view.service;
 
+import car_racing.domain.model.Car;
 import car_racing.domain.model.RaceResults;
 import car_racing.view.model.UserInput;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PrintService {
+    private static final String WINNER_DELIMITER = ",";
+
     public static void showRaceStart() {
         System.out.println("\n실행 결과");
     }
@@ -15,11 +19,20 @@ public class PrintService {
             showEachRound(raceResults.getRaceResultOfNthRound(i));
             System.out.println();
         }
+        showRaceWinners(raceResults.getWinners());
     }
 
     private static void showEachRound(List<String> roundResults) {
         for (String roundResult: roundResults) {
             System.out.println(roundResult);
         }
+    }
+
+    private static void showRaceWinners(List<Car> winnerList) {
+        String winners = winnerList.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(WINNER_DELIMITER));
+
+        System.out.printf("%s가 최종 우승했습니다.", winners);
     }
 }
