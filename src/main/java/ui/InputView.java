2f13872af8814f77d5f91794643144da40932fff
baki <baki.me@kakaocorp.com>
2025-03-17 17:53:28 +0900

    refactor: modify scanner to memeber variable of InputView

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -8,10 +8,10 @@ import java.util.Scanner;
 public class InputView {
     private static final String PARTICIPANTS_CAR_COUNT_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
     private static final String PLAY_TIME_COUNT_INPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static GrandPrix makeGrandPrix() {
         System.out.println(PARTICIPANTS_CAR_COUNT_INPUT_MESSAGE);
-        var scanner = new Scanner(System.in);
         var participantsCarCount = scanner.nextInt();
         System.out.println(PLAY_TIME_COUNT_INPUT_MESSAGE);
         var playTimeCount = scanner.nextInt();
