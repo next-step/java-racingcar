fd2f49ceb2cd9b307c48cf53c1b1b1052706b53a
pol.o(김선동)/kakao <pol.o@kakaocorp.com>
2025-03-17 18:08:26 +0900

    feat: 자동차 객체 및 이동 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,33 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Car {
+
+    private static final int RANDOM_NUMBER_THRESHOLD = 10;
+    private static final int MOVE_THRESHOLD = 4;
+
+    private int position;
+
+    public Car() {
+        this.position = 0;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public void move(Random random) {
+        if (this.isAboveMoveThreshold(random)) {
+            this.position++;
+        }
+    }
+
+    private boolean isAboveMoveThreshold(Random random) {
+        return this.getRandomNumber(random) >= Car.MOVE_THRESHOLD;
+    }
+
+    private int getRandomNumber(Random random) {
+        return random.nextInt(Car.RANDOM_NUMBER_THRESHOLD);
+    }
 }
