a16764cbec61f76ec89628da45d23071e1127f59
pol.o(김선동)/kakao <pol.o@kakaocorp.com>
2025-03-17 18:46:38 +0900

    feat: 자동차 경주 게임 구현

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -1,4 +1,17 @@
 package racingcar;
 
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        InputView inputView = new InputView(new Scanner(System.in));
+        int carCount = inputView.inputCarCount();
+        int tryCount = inputView.inputTryCount();
+        List<Car> cars = new CarGenerator().generateCar(carCount);
+        ResultView resultView = new ResultView();
+        RacingCarGame racingGame = new RacingCarGame(cars, tryCount, resultView);
+        racingGame.startGame(new Random());
+    }
 }
--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -1,4 +1,30 @@
 package racingcar;
 
+import java.util.List;
+import java.util.Random;
+
 public class RacingCarGame {
+    private final List<Car> carList;
+    private final int tryCount;
+    private final ResultView resultView;
+
+    public RacingCarGame(List<Car> carList, int tryCount, ResultView resultView) {
+        this.carList = carList;
+        this.tryCount = tryCount;
+        this.resultView = resultView;
+    }
+
+    public void startGame(Random random) {
+        resultView.printResult();
+        for (int i = 0; i < tryCount; i++) {
+            moveCars(this.carList, random);
+            resultView.printCars(this.carList);
+        }
+    }
+
+    private void moveCars(List<Car> carList, Random random) {
+        for (Car car : carList) {
+            car.move(random);
+        }
+    }
 }
