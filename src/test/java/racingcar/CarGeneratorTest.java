b575a1bb2d3f9c246cabb82c0300ef771c4f76c9
pol.o(김선동)/kakao <pol.o@kakaocorp.com>
2025-03-17 18:33:56 +0900

    style: car 리스트 변수 변경

--- src/test/java/racingcar/CarGeneratorTest.java
+++ src/test/java/racingcar/CarGeneratorTest.java
@@ -7,12 +7,12 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 class CarGeneratorTest {
-    private Integer carCount = 3;
 
     @Test
     @DisplayName("주어진 수 만큼 자동차가 생성되어야 한다.")
     void generateCars() {
-        List<Car> cars = new CarGenerator().generateCar(carCount);
-        Assertions.assertThat(cars.size()).isEqualTo(carCount);
+        final Integer givenCarCount = 3;
+        List<Car> carList = new CarGenerator().generateCar(givenCarCount);
+        Assertions.assertThat(carList.size()).isEqualTo(givenCarCount);
     }
 }
