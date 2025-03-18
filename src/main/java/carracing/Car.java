c1cdd9ebfe30078314d30e0acbae754649f7834a
minakusi <seoyeon737@gmail.com>
2025-03-18 15:40:01 +0900

    refactor: Car distanceString 멤버 변수 삭제 -> getDistanceString에서 생성

--- src/main/java/carracing/Car.java
+++ src/main/java/carracing/Car.java
@@ -4,22 +4,23 @@ public class Car {
     public static final int CAR_MOVING_BOUND = 4;
     private String name;
     private int distance = 1;
-    private String distanceString;
 
     public Car(String name) {
         this.name = name;
-        distanceString = name + " : -";
     }
 
     public void move(int randomNum) {
         if (randomNum >= CAR_MOVING_BOUND) {
             distance++;
-            distanceString += "-";
         }
     }
 
     public String getDistanceString() {
-        return distanceString;
+        String returnString = name + " : ";
+        for (int i = 0; i < distance; ++i) {
+            returnString += "-";
+        }
+        return returnString;
     }
 
     public int getDistance() {
