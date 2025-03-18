41bee240bdf12d3b36ced89a5d762a6a09eb88f8
minji-go <kohminji10@gmail.com>
2025-03-19 01:12:36 +0900

    test: 테스트 리팩토링

--- src/test/java/RacingCarTest.java
+++ src/test/java/RacingCarTest.java
@@ -7,29 +7,33 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class RacingCarTest {
+class RacingCarTest {
     private final RacingCar racingCar = new RacingCar();
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(delimiter = ',', value = {"3,5", "0,0", "1,-1"})
     @DisplayName("자동차 대수와 시도할 회수를 순서대로 입력받는 안내문구를 출력하고 입력받는다.")
-    public void test6() {
-        System.setIn(new ByteArrayInputStream("3\n5\n".getBytes()));
+    void printGuideMessage(int carNumber, int tryCount) {
+        String in = String.format("%s%n%s%n", carNumber, tryCount);
+        System.setIn(new ByteArrayInputStream(in.getBytes()));
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
+
         racingCar.input();
 
-        String expected = "자동차 대수는 몇 대 인가요?\n시도할 회수는 몇 회 인가요?\n\n";
-        assertThat(out.toString().replace("\r\n", "\n")).startsWith(expected);
+        List<String> expected = List.of("자동차 대수는 몇 대 인가요?", "시도할 회수는 몇 회 인가요?");
+        assertThat(out.toString()).contains(expected);
     }
 
     @ParameterizedTest
     @CsvSource(delimiter = ',', value = {"-3,0", "0,0", "3,-1"})
     @DisplayName("자동차 대수와 시도할 회수는 양수여야한다.")
-    public void test9(int carNumber, int tryCount) {
+    void throwIfInputPositive(int carNumber, int tryCount) {
         String in = String.format("%s%n%s%n", carNumber, tryCount);
         System.setIn(new ByteArrayInputStream(in.getBytes()));
 
@@ -38,25 +42,26 @@ public class RacingCarTest {
     }
 
     @Test
-    @DisplayName("주어진 횟수 동안 n대의 자동차는 전진하거나 멈출 수 있으며, 횟수마다 실행 결과를 출력한다.")
-    public void test7() {
+    @DisplayName("주어진 횟수 동안 자동차는 전진하거나 멈출 수 있으며, 횟수마다 실행 결과를 출력한다.")
+    void printRaceResult() {
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
-        racingCar.start(1, 2);
-        String print = out.toString();
-        assertThat(print).containsPattern("실행 결과\n?[-]\n\n?[-]");
+
+        racingCar.start(2, 2);
+
+        assertThat(out.toString()).containsPattern("실행 결과\n-{0,1}\n-{0,1}\n\n-{0,2}\n-{0,2}");
     }
 
     @RepeatedTest(value = 30)
     @DisplayName("0에서 9 사이 random 값을 생성한다.")
-    public void test4() {
+    void createRandomBetween() {
         assertThat(racingCar.createRandom()).isBetween(0, 9);
     }
 
     @ParameterizedTest
     @CsvSource(delimiter = ':', value = { "0:false", "4:true", "9:true" })
     @DisplayName("random 값이 4 이상이면 전진하고, 그렇지 않으면 정지한다.")
-    public void test5(int input, boolean expected) {
+    void determineMoveOrNot(int input, boolean expected) {
         assertThat(racingCar.isGoingForward(input)).isEqualTo(expected);
     }
 
