fb59508dda1a270d291a3d6ce8976432f0dedb5d
YuGyeong <lyg64522@naver.com>
2025-03-18 21:55:52 +0900

    chore: 우승자 찾는 기능 추가
    - 우승자 찾는 메소드 추가

--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Racing {
 
     private final List<Car> carList = new ArrayList<>(); // 자동차 리스트
 
     private final int tryCount; // 시도 횟수
+
+    private static final List<Car> winners = new ArrayList<>(); //우승 자동차
 
     /**
      * @param carName
@@ -39,6 +42,17 @@ public class Racing {
             race();
             ResultView.printRaceRound(carList);
         }
+
+        ResultView.printWinner(findWinner());
+    }
+
+    /**
+     * @return 우승자를 찾습니다.
+     */
+    private List<Car> findWinner() {
+        int maxScore = carList.stream().mapToInt(Car::getPosition).max().orElse(0);
+
+        return carList.stream().filter(car -> car.getPosition() == maxScore).collect(Collectors.toList());
     }
 
     /**
