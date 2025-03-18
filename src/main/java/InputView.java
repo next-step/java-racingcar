806256eb8892511120eac42b1a8aefb535dd28c1
YuGyeong <lyg64522@naver.com>
2025-03-18 21:28:40 +0900

    chore: 자동차 이름 입력 기능 추가
    - 자동차 이름 입력 기능 추가

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,4 +1,3 @@
-import com.sun.source.tree.UsesTree;
 import java.util.Scanner;
 
 public class InputView {
@@ -9,12 +8,13 @@ public class InputView {
         return scanner.nextInt();
     }
 
-    public static int getCarNum() {
-        System.out.print("자동차 대수는 몇 대 인가요?");
-        return scanner.nextInt();
-    }
-
     public static void closeScanner() {
         scanner.close();
+    }
+
+    public static String[] getCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+
+        return scanner.nextLine().split(",");
     }
 }
--- src/main/java/ResultView.java
+++ src/main/java/ResultView.java
@@ -4,8 +4,12 @@ public class ResultView {
 
     public static void printRaceRound(List<Car> carList) {
         for (Car car : carList) {
-            System.out.println("-".repeat(car.getPosition()));
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
         }
         System.out.println("\n");
+    }
+
+    public static void printExecuteResult() {
+        System.out.println("실행 결과");
     }
 }
