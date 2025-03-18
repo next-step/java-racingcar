03b6f2aa517b7e1ae3152e1fac6911e5c2f9d061
jieung <jieung@lamilab.xyz>
2025-03-18 12:36:15 +0900

    feat 자동차가 전달받은 숫자에 따라 전진하고 정지하는 기능 추가

--- src/main/java/car/Car.java
+++ src/main/java/car/Car.java
@@ -4,7 +4,13 @@ public class Car {
     private int position = 0;
     private final static int BASE_NUMBER = 4;
 
-    public void move() {
+    public void move(int randomNumber) {
+        if (isAtLeastBaseNumber(randomNumber)) {
+            goForward();
+        }
+    }
+
+    public void goForward() {
         this.position++;
     }
 
