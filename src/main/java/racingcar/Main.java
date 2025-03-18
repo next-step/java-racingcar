a683625e88f59e220f5a07e4ded49a8b62a51bb4
Gukin Han <gukin.dev@gmail.com>
2025-03-18 15:36:57 +0900

    refactor(InputView): 출력로직을 클래스로 분리

--- src/main/java/racingcar/Main.java
+++ src/main/java/racingcar/Main.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import racingcar.view.InputView;
+import racingcar.view.ResultView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +13,13 @@ public class Main {
     public static final int MOVABLE_THRESHOLD = 4;
     public static final int DEFAULT_MOVING_DISTANCE = 1;
     public static final int RANDOM_BOUND = 10;
-    public static final String DEFAULT_MARKER = "-";
 
     public static void main(String[] args) {
         InputView inputView = new InputView(new Scanner(System.in));
         int carCount = inputView.getCarCount();
         int attempts = inputView.getAttempts();
+
+        ResultView resultView = new ResultView();
 
         System.out.println("\n실행 결과");
 
@@ -27,20 +29,12 @@ public class Main {
         }
 
         Random random = new Random();
-        StringBuilder result = new StringBuilder();
         for (int i = 0; i < attempts; i++) {
             for (Car car : cars) {
                 int randomInt = random.nextInt(RANDOM_BOUND);
                 car.moveOrStop(randomInt, DEFAULT_MOVING_DISTANCE);
-                String trace = car.getTrace(DEFAULT_MARKER);
-                if (trace.isBlank()) {
-                    trace = " ";
-                }
-                result.append(trace).append("\n");
             }
-            System.out.println(result);
-            System.out.println();
+            resultView.printCurrentRacingProgress(cars);
         }
-
     }
 }
