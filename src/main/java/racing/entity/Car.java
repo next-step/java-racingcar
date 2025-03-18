249cbd127109babf32b9d9e6e4b7a56423959dbe
zoolake <junhomoon224@gmail.com>
2025-03-18 16:56:04 +0900

    feat : implement assigning car name

--- src/main/java/racing/entity/Car.java
+++ src/main/java/racing/entity/Car.java
@@ -1,7 +1,9 @@
 package racing.entity;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
     private int position = 1;
+    private String carName;
 
     public Car() {
     }
@@ -10,7 +12,28 @@ public class Car {
         return position;
     }
 
+    public String getCarName() {
+        return carName;
+    }
+
     public void move() {
         position++;
+    }
+
+    public void registerName(String carName) {
+        if (checkNameLength(carName)) {
+            this.carName = carName;
+        }
+    }
+
+    private boolean checkNameLength(String carName) {
+        int carNameLength = carName.length();
+
+        if (carNameLength > MAX_NAME_LENGTH) {
+            String messageFormat = "자동차 이름은 5자 이하여야 합니다. 입력값 : %s(%d자)";
+            throw new IllegalArgumentException(String.format(messageFormat, carName, carNameLength));
+        }
+
+        return true;
     }
 }
--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,11 +1,13 @@
 package racing;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import racing.entity.Car;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
     @ParameterizedTest
@@ -23,5 +25,17 @@ class CarTest {
         // then
         int actual = car.getPosition();
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 MaxNameLength 를 초과하는 경우 IllegalArgumentException 이 발생한다.")
+    public void car_throws_IllegalArgumentException_whenNameIsOverMaxNameLength() {
+        // given
+        Car car = new Car();
+        String carName = "ABCDEF";
+
+        // when & then
+        assertThatThrownBy(() -> car.registerName(carName)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자 이하여야 합니다.");
     }
 }
