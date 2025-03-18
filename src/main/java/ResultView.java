9ba26dc584d87ffe591062795998877595a04b67
YuGyeong <lyg64522@naver.com>
2025-03-18 21:55:52 +0900

    chore: 우승자 찾는 기능 추가
    - 우승자 찾는 메소드 추가

--- src/main/java/ResultView.java
+++ src/main/java/ResultView.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
@@ -11,5 +12,11 @@ public class ResultView {
 
     public static void printExecuteResult() {
         System.out.println("실행 결과");
+    }
+
+    public static void printWinner(List<Car> carList) {
+        String result = carList.stream().map(Car::getName).collect(Collectors.joining(", "));
+
+        System.out.println(result + "가 최종 우승했습니다.");
     }
 }
