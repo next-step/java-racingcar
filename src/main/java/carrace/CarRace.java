06ca9e4ada558acc488a405e2cba80e78ef89613
SeungMin <semicpu@gmail.com>
2025-03-18 21:42:37 +0900

    refactor: created package carrace

--- src/main/java/carrace/CarRace.java
+++ src/main/java/carrace/CarRace.java
@@ -1,3 +1,5 @@
+package carrace;
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
--- src/test/java/carrace/CarRaceTest.java
+++ src/test/java/carrace/CarRaceTest.java
@@ -1,3 +1,5 @@
+package carrace;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
