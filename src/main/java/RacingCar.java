19351584afda07b7b41bd7658cf59d4f5c48f1d4
minji-go <kohminji10@gmail.com>
2025-03-18 23:55:41 +0900

    feat: 사용자의 입력

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,7 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class RacingCar {
     private final Random random = new Random();
+
+    public void input() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carNumber = scanner.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int tryCount = scanner.nextInt();
+        System.out.println();
+    }
+
 
     public int createRandom() {
         return random.nextInt(10);
@@ -10,4 +21,5 @@ public class RacingCar {
     public boolean isGoingForward(int value) {
         return value >= 4;
     }
+
 }
--- src/test/java/RacingCarTest.java
+++ src/test/java/RacingCarTest.java
@@ -1,19 +1,36 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+import java.io.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingCarTest {
     private final RacingCar racingCar = new RacingCar();
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Test
+    @DisplayName("자동차 대수와 시도할 회수를 순서대로 입력받는 안내문구를 출력하고 입력받는다.")
+    public void test6() {
+        System.setIn(new ByteArrayInputStream("3\n5\n".getBytes()));
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        racingCar.input();
+
+        String expected = "자동차 대수는 몇 대 인가요?\n시도할 회수는 몇 회 인가요?\n\n";
+        assertThat(out.toString().replace("\r\n", "\n")).isEqualTo(expected);
+    }
+
+    @RepeatedTest(value = 30)
     @DisplayName("0에서 9 사이 random 값을 생성한다.")
     public void test4() {
         assertThat(racingCar.createRandom()).isBetween(0, 9);
     }
-
 
     @ParameterizedTest
     @CsvSource(delimiter = ':', value = { "0:false", "4:true", "9:true" })
@@ -21,6 +38,7 @@ public class RacingCarTest {
     public void test5(int input, boolean expected) {
         assertThat(racingCar.isGoingForward(input)).isEqualTo(expected);
     }
+
 
 
 }
