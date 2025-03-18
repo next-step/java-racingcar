541f75f477770389f452ce20a7ddff3fe98afed6
sophia.song(송지현)/kakao <sophia.song@kakaocorp.com>
2025-03-18 21:40:40 +0900

    [feat] 자동차 이름 제한 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,8 +5,15 @@ public class Car {
   private int position;
 
   public Car(String name) {
+    validateName(name);
     this.name = name;
     this.position = 0;
+  }
+
+  private void validateName(String name) {
+    if (name.length() > 5) {
+      throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+    }
   }
 
   public void move() {
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,20 @@ class CarTest {
     Car car = new Car(name);
     car.move();
     assertThat(car.getPosition()).isEqualTo(1);
+  }
+
+  @Test
+  @DisplayName("자동차는 5자 이상의 이름을 가질 경우 IllegalArgumentException 예외를 발생시킨다.")
+  void validateName() {
+    assertThatIllegalArgumentException().isThrownBy(() -> new Car("abcdef"));
+  }
+
+  @Test
+  @DisplayName("자동차는 이름을 가질 수 있다.")
+  void getName() {
+    String name = "pobi";
+    Car car = new Car(name);
+    assertThat(car.getName()).isEqualTo(name);
   }
 
 }
