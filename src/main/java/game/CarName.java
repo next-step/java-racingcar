0e52afeeab7779cbcacbba33063bc116e0a0247b
east.bridge <east.bridge@kakaocorp.com>
2025-03-18 13:50:41 +0900

    refactor: validate 메서드에서 값 설정이라는 두 가지일을 하던 것 제거

--- src/main/java/game/CarName.java
+++ src/main/java/game/CarName.java
@@ -18,7 +18,6 @@ public class CarName {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
-        this.name = name;
     }
 
     public String name() {
