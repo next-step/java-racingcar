e1e20f2dccd926167f6c35e83b3242300946199b
yang Jiyoung <0112jyoung@naver.com>
2025-03-18 21:54:50 +0900

    [refactor] 우승자 출력 부분 수정

--- src/main/java/util/ResultView.java
+++ src/main/java/util/ResultView.java
@@ -3,15 +3,16 @@ package util;
 import racingcar.Car;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
-    public static void showResultMessage(){
+    public static void showResultMessage() {
         System.out.println("실행 결과");
     }
 
-    private static void showCarResult(List<Car> cars){
-        for(Car car: cars) {
-            System.out.println(car.getCarName() + ":" +"-".repeat(car.getDistance()));
+    private static void showCarResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getCarName() + ":" + "-".repeat(car.getDistance()));
         }
     }
 
@@ -19,16 +20,15 @@ public class ResultView {
         System.out.println();
     }
 
-    public static void showCarGameResult(List<Car> cars){
+    public static void showCarGameResult(List<Car> cars) {
         showCarResult(cars);
         showEndOfTry();
     }
 
-    public static void showCarGameWinners(List<Car> winners){
-        StringBuilder stringBuilder = new StringBuilder();
-        for(Car car: winners) {
-            stringBuilder.append(car.getCarName()+",");
-        }
-        System.out.println(stringBuilder +"가 최종 우승했습니다.");
+    public static void showCarGameWinners(List<Car> winners) {
+        String winnerNames = winners.stream()
+                .map(Car::getCarName)
+                .collect(Collectors.joining(","));
+        System.out.println(winnerNames + "가 최종 우승했습니다.");
     }
 }
