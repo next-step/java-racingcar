05500570b426302e68c00203cbf7d4a00a53e94e
SeungMin <semicpu@gmail.com>
2025-03-19 01:44:31 +0900

    refactor

---
+++ src/test/java/carrace/app/RaceTest.java
@@ -0,0 +1,36 @@
+package carrace.app;
+
+import carrace.common.Vehicle;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class RaceTest {
+
+    @DisplayName("자동차 대수가 0 이하의 값이면, 에러가 발생합니다.")
+    @Test
+    public void updateCurrentStateTest_Negative() {
+        int negativeNumCar = -1;
+        Vehicle[] emptyCars = {};
+        assertThatThrownBy(() -> Race.updateCurrentState(emptyCars, negativeNumCar))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("초기 상태는 무조건 한칸(-)씩 있어야합니다. ")
+    @Test
+    public void updateCurrentStateTest() {
+        int positiveNumCar = 3;
+        Vehicle[] cars = new Vehicle[positiveNumCar];
+        Race.prepare(cars, positiveNumCar);
+        Arrays.stream(cars)
+                .map(Vehicle::getCurrentPosition)
+                .forEach(
+                        position -> {
+                            assertThat(position).isEqualTo("-");
+                        });
+    }
+}
---
+++ src/test/java/carrace/utils/RandomValueTest.java
@@ -0,0 +1,16 @@
+package carrace.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class RandomValueTest {
+
+    @DisplayName("0 이상 10 미만의 랜덤값이 나와야합니다.")
+    @Test
+    public void generateTest() {
+        int randomValue = RandomValue.generate();
+        assertThat(randomValue).isBetween(0, 10);
+    }
+}
