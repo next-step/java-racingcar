b301e510e04bf12afd0efd88fee1c34e181b7b3b
JiEung2 <181757@jnu.ac.kr>
2025-03-19 23:35:59 +0900

    feat Car 클래스에 이름 필드 추가 및 생성자 추가

--- src/main/java/car/Car.java
+++ src/main/java/car/Car.java
@@ -4,8 +4,13 @@ import utils.NumberGenerator;
 
 public class Car {
     private final static int INITIAL_POSITION = 0;
-    private int position = INITIAL_POSITION;
     private final static int BASE_NUMBER = 4;
+    private int position = INITIAL_POSITION;
+    private final String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
 
     public void move(NumberGenerator numberGenerator) {
         int movableNumber = numberGenerator.generate();
@@ -20,6 +25,10 @@ public class Car {
 
     public int getPosition() {
         return this.position;
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     private boolean isAtLeastBaseNumber(int number) {
