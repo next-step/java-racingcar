d99134f906ef5ea72bb69f27dedc677c981d9a5d
baki <hgs6424@naver.com>
2025-03-19 23:48:18 +0900

    test: add createCarsTest for car create with names

--- src/test/java/racing/GrandPrixTest.java
+++ src/test/java/racing/GrandPrixTest.java
@@ -10,11 +10,11 @@ import java.util.Random;
 class GrandPrixTest {
 
     @Test
-    @DisplayName("자동차의 이름들을 입력 받아 전체 라운드 수 만큼 play한다")
+    @DisplayName("총 라운드 수 를 받아 전체 라운드 수 만큼 play한다")
     void playForTotalRoundCountTest() {
         // given
         int totalRound = 5;
-        var grandPrix = new GrandPrix(totalRound, 0);
+        var grandPrix = new GrandPrix(totalRound, List.of(""));
 
         // when
         while (!grandPrix.isFinished()) {
@@ -23,5 +23,18 @@ class GrandPrixTest {
 
         // then
         Assertions.assertThat(grandPrix.isFinished()).isTrue();
+    }
+
+    @Test
+    @DisplayName("자동차의 이름들을 입력 받아 Car를 생성해 cars에 추가한다.")
+    void createCarsTest() {
+        // given
+        var names = List.of("pobi", "crong", "honux");
+
+        // when
+        var grandPrix = new GrandPrix(5, names);
+
+        // then
+        Assertions.assertThat(grandPrix.getCarCount()).isEqualTo(names.size());
     }
 }
