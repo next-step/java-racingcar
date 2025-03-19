ed23c68866e9f836c669980e1a86e26153ec6dc2
giraffeb <rwpark07@gmail.com>
2025-03-19 21:59:10 +0900

    test: step2 - SetTest 요구사항 1

---
+++ src/test/java/string/SetTest.java
@@ -0,0 +1,29 @@
+package string;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("1,1,2,3을 넣으면 size는 3이다.")
+    void getSize(){
+        assertThat(this.numbers.size()).isEqualTo(3);
+    }
+}
