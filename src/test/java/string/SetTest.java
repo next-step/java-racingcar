a6bd4a68aa337af09283989b6d3ab5fbf4f727e9
giraffeb <rwpark07@gmail.com>
2025-03-19 22:03:01 +0900

    test: step2 - SetTest 요구사항 2

--- src/test/java/string/SetTest.java
+++ src/test/java/string/SetTest.java
@@ -3,6 +3,8 @@ package string;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -23,7 +25,14 @@ public class SetTest {
 
     @Test
     @DisplayName("1,1,2,3을 넣으면 size는 3이다.")
-    void getSize(){
+    void getSize() {
         assertThat(this.numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3 })
+    @DisplayName("1,1,2,3을 넣으면 1,2,3을 포함한다.")
+    void contains(int number) {
+        assertThat(this.numbers.contains(number)).isTrue();
     }
 }
