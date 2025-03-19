1efdbae151882190fca22f7d28f97584fcc98b46
giraffeb <rwpark07@gmail.com>
2025-03-19 21:36:49 +0900

    refact: step1 - StringTest 함수 카멜케이스로 변경

--- src/test/java/string/StringTests.java
+++ src/test/java/string/StringTests.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.beans.Transient;
-
 /**
  * 요구사항 1.
  */
-public class SplitTests {
+public class StringTests {
     @Test
     @DisplayName("\"1,2\"를 분리하면 \"1\"과 \"2\"를 포함하는 배열이 생성된다")
     void split() {
@@ -19,13 +17,13 @@ public class SplitTests {
 
     @Test
     @DisplayName("\"1,\"을 분리하면 \"1\"만 포함하는 배열이 생성된다")
-    void split_one() {
+    void splitOne() {
         assertThat("1".split(",")).containsExactly("1");
     }
 
     @Test
     @DisplayName("\"(1,2)\"을 분리하면 ()를 제외하고 \"1\", \"2\"만 포함하는 배열이 생성된다")
-    void split_without_parentheses() {
+    void splitWithoutParentheses() {
         assertThat("(1,2)".replaceAll("[()]", "").split(",")).containsExactly("1", "2");
     }
 }
