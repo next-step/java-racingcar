8ea70c2a7f7b98bc3cbce0ec619aa5869e12eee3
giraffeb <rwpark07@gmail.com>
2025-03-19 21:51:43 +0900

    test: step1 - StringTest 요구사항 3

--- src/test/java/string/StringTests.java
+++ src/test/java/string/StringTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 /**
  * 요구사항 1.
@@ -25,5 +26,21 @@ public class StringTests {
     @DisplayName("\"(1,2)\"을 분리하면 ()를 제외하고 \"1\", \"2\"만 포함하는 배열이 생성된다")
     void splitWithoutParentheses() {
         assertThat("(1,2)".replaceAll("[()]", "").split(",")).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("\"abc\"의 0번째 문자는 \"a\"이다")
+    void getCharFromIndex() {
+        assertThat("abc".charAt(0)).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("\"abc\"의 3번째 문자를 가져올 경우 StringIndexOutOfBoundsException 에외가 발생한다.")
+    void getCharFromOutBoundIndex() {
+        assertThatThrownBy(() -> {
+            "abc".charAt(3);
+        })
+        .isInstanceOf(StringIndexOutOfBoundsException.class);
+
     }
 }
