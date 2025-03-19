c05105876808c136a32a80dbfaed47a25979a091
JiEung2 <181757@jnu.ac.kr>
2025-03-19 23:36:55 +0900

    fix inputCarCount에서 inputCarNames로 변경
    - 자동차 대수가 아닌 자동차 이름을 입력받는 로직으로 수정

--- src/main/java/utils/InputView.java
+++ src/main/java/utils/InputView.java
@@ -3,13 +3,13 @@ package utils;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String INPUT_CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회 인가요?";
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputCarCount() {
-        println(INPUT_CAR_COUNT_MESSAGE);
-        return toInt(inputNextLine());
+    public static String[] inputCarNames() {
+        println(INPUT_CAR_NAMES_MESSAGE);
+        return splitCarNames(inputNextLine());
     }
 
     public static int inputTryCount() {
@@ -23,6 +23,10 @@ public class InputView {
 
     private static int toInt(String number) {
         return Integer.parseInt(number);
+    }
+
+    private static String[] splitCarNames(String inputNames) {
+        return inputNames.split(",");
     }
 
     private static void println(String message) {
