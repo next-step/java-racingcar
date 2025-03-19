926815e3c8da2bebdf76e61e3061468388f16d97
liter.1(김성수)/kakao <liter.1@kakaocorp.com>
2025-03-19 18:16:23 +0900

    test: Use mock

--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -1,24 +1,38 @@
 package racingcar.car;
 
 import org.junit.jupiter.api.Test;
+import racingcar.movepolicy.MoveStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CarTest {
 
     private Car car;
+    private MoveStrategy moveStrategy;
 
     @Test
     void play_호출시_move가_true면_distance가_증가한다() {
-        car = new Car(() -> true);
+        moveStrategy = mock(MoveStrategy.class);
+        car = new Car(moveStrategy);
+
+        when(moveStrategy.move()).thenReturn(true);
+
         car.play();
+
         assertThat(car.getDistance()).isEqualTo(1);
     }
 
     @Test
     void play_호출시_move가_false면_distance가_증가하지_않는다() {
-        car = new Car(() -> false);
+        moveStrategy = mock(MoveStrategy.class);
+        car = new Car(moveStrategy);
+
+        when(moveStrategy.move()).thenReturn(false);
+
         car.play();
+
         assertThat(car.getDistance()).isEqualTo(0);
     }
 }
--- src/test/java/racingcar/racing/RacingTest.java
+++ src/test/java/racingcar/racing/RacingTest.java
@@ -1,29 +1,37 @@
 package racingcar.racing;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import racingcar.movepolicy.MoveStrategy;
 import racingcar.car.Car;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class RacingTest {
 
-    private final MoveStrategy alwaysMoveStrategy = () -> true;
 
     @Test
     void 자동차_목록을_받아_레이싱_객체를_생성한다() {
-        List<Car> cars = List.of(new Car(alwaysMoveStrategy));
+        MoveStrategy moveStrategy = mock(MoveStrategy.class);
+        List<Car> cars = List.of(new Car(moveStrategy));
         Racing racing = new Racing(cars);
         assertThat(racing).isNotNull();
     }
 
     @Test
     void 라운드가_한_번_진행되면_Car_play가_반드시_호출된다() {
-        List<Car> cars = List.of(new Car(alwaysMoveStrategy));
+        MoveStrategy moveStrategy = mock(MoveStrategy.class);
+        List<Car> cars = List.of(new Car(moveStrategy));
         Racing racing = new Racing(cars);
+
+        when(moveStrategy.move()).thenReturn(true);
+
         racing.playRound();
+
         assertThat(cars.get(0).getDistance()).isEqualTo(1);
     }
 }
