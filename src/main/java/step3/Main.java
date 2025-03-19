4a774428e0b49aa34583eaa3e9c376ce1715fe63
heewonham <heewonham6@gmail.com>
2025-03-19 10:35:22 +0900

    feat : main 함수 수정 및 output view 수정

--- src/main/java/step3/Main.java
+++ src/main/java/step3/Main.java
@@ -3,6 +3,7 @@ package step3;
 
 import step3.game.Car;
 import step3.game.CarFactory;
+import step3.game.GameHistory;
 import step3.game.RacingGame;
 import step3.random.DefaultRandomStrategy;
 import step3.util.InputView;
@@ -18,7 +19,7 @@ public class Main {
         int moveCount = InputView.inputInt(MOVE_COUNT_MESSAGE);
         List<Car> cars = CarFactory.createCars(carNames);
         RacingGame game = new RacingGame(moveCount, new DefaultRandomStrategy(), cars);
-        List<List<Integer>> result = game.start();
-        OutputView.printResult(result);
+        List<List<GameHistory>> result = game.start();
+        OutputView.printResult(result, game.getCurrentWinners());
     }
 }
