50c5625a3f46128374a5ee6aed820513962c420e
YuGyeong <lyg64522@naver.com>
2025-03-19 19:43:31 +0900

    chore: 변수 명 변경
    - 변수 명 변경

--- src/main/java/ApplicationMain.java
+++ src/main/java/ApplicationMain.java
@@ -2,11 +2,11 @@ import java.util.List;
 public class ApplicationMain {
     public static void main(String[] args) {
 
-        List<String> carName = InputView.getCarName();
+        List<String> carNames = InputView.getCarName();
         int tryCount = InputView.getTryCount();
         InputView.closeScanner();
 
-        Racing racing = new Racing(carName, tryCount);
+        Racing racing = new Racing(carNames, tryCount);
 
         racing.start();
 
