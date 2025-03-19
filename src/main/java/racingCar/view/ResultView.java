b6b7f663702f8560685504108ddbf30d9f602f05
lyeo72 <lyeo7272@gmail.com>
2025-03-19 22:41:00 +0900

    feat: 자동차 이름 부여 및 우승자 출력

--- src/main/java/racingCar/view/ResultView.java
+++ src/main/java/racingCar/view/ResultView.java
@@ -3,17 +3,28 @@ package racingCar.view;
 import racingCar.domain.Car;
 import racingCar.domain.Cars;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ResultView {
 
     public static void output() {
         System.out.println("실행결과");
     }
 
-    public static void result(Cars cars) {
+    public static void position(Cars cars) {
         for (Car car : cars.getCars()) {
-            System.out.println("-".repeat(car.getPosition()));
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
         }
         System.out.println();
+    }
+
+    public static void result(List<Car> winners) {
+        String winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(winnerNames + "가 최종 우승했습니다.");
     }
 
 }
