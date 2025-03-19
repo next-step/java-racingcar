2dd7dca78936877959daaacda3aab39c9a8c8ea0
JiEung2 <181757@jnu.ac.kr>
2025-03-20 00:18:44 +0900

    feat 우승자를 판별하는 기능 추가

---
+++ src/main/java/car/WinnerDeterminer.java
@@ -0,0 +1,21 @@
+package car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinnerDeterminer {
+
+    public static List<Car> findWinners(Cars cars) {
+        int maxPosition = findMaxPosition(cars);
+        return cars.getCars().stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
+    }
+
+    private static int findMaxPosition(Cars cars) {
+        return cars.getCars().stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+}
