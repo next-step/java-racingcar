f29f0668d7533019d44f8c648374d260ade5ec7b
JiEung2 <181757@jnu.ac.kr>
2025-03-19 23:36:27 +0900

    fix CarGenerator에 인자로 count가 아닌 carNames를 받도록 수정

--- src/main/java/utils/CarGenerator.java
+++ src/main/java/utils/CarGenerator.java
@@ -2,14 +2,14 @@ package utils;
 
 import car.Car;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class CarGenerator {
-    public static List<Car> generateCars(int count) {
-        return IntStream.range(0, count)
-                .mapToObj(i -> new Car())
+    public static List<Car> generateCars(String[] carNames) {
+        return Arrays.stream(carNames)
+                .map(Car::new)
                 .collect(Collectors.toList());
     }
 }
