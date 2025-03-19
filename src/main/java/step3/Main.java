1944145f5171481a4697a25ee86c2c61b14db21b
heewonham <heewonham6@gmail.com>
2025-03-19 10:31:40 +0900

    feat : carFactory 이름받을 수 있도록 수정 및 테스트 코드 추가

--- src/main/java/step3/Main.java
+++ src/main/java/step3/Main.java
@@ -11,12 +11,12 @@ import step3.util.OutputView;
 import java.util.List;
 
 public class Main {
-    private static final String CAR_COUNT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String MOVE_COUNT_MESSAGE = "시도할 회수는 몇 회 인가요?";
     public static void main(String[] args) {
-        int carCount = InputView.inputInt(CAR_COUNT_MESSAGE);
+        String carNames = InputView.inputString(CAR_NAME_MESSAGE);
         int moveCount = InputView.inputInt(MOVE_COUNT_MESSAGE);
-        List<Car> cars = CarFactory.createCars(carCount);
+        List<Car> cars = CarFactory.createCars(carNames);
         RacingGame game = new RacingGame(moveCount, new DefaultRandomStrategy(), cars);
         List<List<Integer>> result = game.start();
         OutputView.printResult(result);
--- src/main/java/step3/game/CarFactory.java
+++ src/main/java/step3/game/CarFactory.java
@@ -4,10 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarFactory {
-    public static List<Car> createCars(int count) {
+    private final static String CAR_NAME_DELIMITER = ",";
+    public static List<Car> createCars(String names) {
+        if (!names.contains(CAR_NAME_DELIMITER)) {
+            throw new IllegalArgumentException("자동차 이름은 쉼표(,)를 기준으로 구분해야 합니다.");
+        }
+        String[] carNames = names.split(CAR_NAME_DELIMITER);
+        if (carNames.length < 2) {
+            throw new IllegalArgumentException("자동차는 최소 2대 이상이어야 합니다.");
+        }
         List<Car> cars = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            cars.add(new Car());
+        for (int i = 0; i < carNames.length; i++) {
+            cars.add(new Car(carNames[i]));
         }
         return cars;
     }
