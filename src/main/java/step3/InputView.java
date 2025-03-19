f607ccc141efbcb6572a089f2d4d42e3a50866dd
jlee0505 <hihjlee0505@gmail.com>
2025-03-19 21:45:38 +0900

    init: 수도 코드 작성

---
+++ src/main/java/step3/InputView.java
@@ -0,0 +1,7 @@
+package step3;
+
+public class InputView {
+    // InputView:
+    // - 유저에게서 차 갯수를 입력 받는다.
+    // - 유저에게서 게임 시도 횟수를 입력 받는다.
+}
---
+++ src/main/java/step3/ResultView.java
@@ -0,0 +1,6 @@
+package step3;
+
+public class ResultView {
+    // ResultView:
+    // - 최종 결과(List<List<String>>)를 화면에 출력한다.
+}
