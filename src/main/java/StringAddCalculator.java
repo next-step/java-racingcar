b8338f176b5cbb8cf53aa705db55534b86d73344
bo-10000 <seot100@naver.com>
2025-03-19 21:10:59 +0900

    refactor: PR 리뷰 반영

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,34 +1,40 @@
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    private static final Pattern DIVIDER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String DEFAULT_DELIMITER  = ",|:";
+
+    private static int sum(String[] numbers) {
+        return Arrays.stream(numbers)
+                .peek(stringNumber -> {
+                    if (!stringNumber.matches("^[0-9]*$")) {
+                        throw new RuntimeException("숫자가 아닌 값이나 음수가 포함되어 있습니다.");
+                    }
+                })
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
 
-        String[] divider = {",", ":"};
-        String dividerRegex = String.join("|", divider);
+        String delimiter = DEFAULT_DELIMITER;
 
         // Find custom divider
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = DIVIDER_PATTERN.matcher(text);
         if (m.find()) {
-            dividerRegex = m.group(1);
+            delimiter = m.group(1);
             text = m.group(2);
         }
 
-        String[] stringNumbers = text.split(dividerRegex);
+        String[] stringNumbers = text.split(delimiter);
 
-        int sum = 0;
-        for (String stringNumber : stringNumbers) {
-            int intNumber = Integer.parseInt(stringNumber);
-            if (intNumber < 0) {
-                throw new RuntimeException("음수는 불가능합니다.");
-            }
-            sum += intNumber;
-        }
-        return sum;
+        return sum(stringNumbers);
 
     }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,43 +1,50 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringAddCalculatorTest {
-    @Test
-    public void splitAndSum_null_또는_빈문자() {
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("null 또는 빈문자가 들어오면 0을 반환")
+    public void splitAndSum_null_또는_빈문자(String input) {
+        int result = StringAddCalculator.splitAndSum(input);
         assertThat(result).isEqualTo(0);
     }
 
     @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환")
     public void splitAndSum_숫자하나() throws Exception {
         int result = StringAddCalculator.splitAndSum("1");
         assertThat(result).isEqualTo(1);
     }
 
     @Test
+    @DisplayName("쉼표 구분자로 숫자 두개를 입력할 경우 두 숫자의 합을 반환")
     public void splitAndSum_쉼표구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2");
         assertThat(result).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("쉼표 또는 콜론 구분자로 숫자 여러개를 입력할 경우 모두 더한 값을 반환")
     public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("1,2:3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("custom 구분자를 지정할 경우, custom 구분자로 문자열을 분리")
     public void splitAndSum_custom_구분자() throws Exception {
         int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 
     @Test
+    @DisplayName("음수를 전달할 경우 RuntimeException 예외 발생")
     public void splitAndSum_negative() throws Exception {
         assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
                 .isInstanceOf(RuntimeException.class);
