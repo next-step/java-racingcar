fbdbc93f69005168441e1dfc74062933e73fda54
giraffeb <rwpark07@gmail.com>
2025-03-19 21:31:40 +0900

    test: step1 - StringTest 요구사항 2

--- src/test/java/string/SplitTests.java
+++ src/test/java/string/SplitTests.java
@@ -12,14 +12,20 @@ import java.beans.Transient;
  */
 public class SplitTests {
     @Test
-    @DisplayName("\"1,2\"를 쉼표로 분리하면 \"1\"과 \"2\"를 포함하는 배열이 생성된다")
+    @DisplayName("\"1,2\"를 분리하면 \"1\"과 \"2\"를 포함하는 배열이 생성된다")
     void split() {
         assertThat("1,2".split(",")).containsExactly("1", "2");
     }
 
     @Test
-    @DisplayName("\"1,\"을 쉼표로 분리하면 \"1\"만 포함하는 배열이 생성된다")
+    @DisplayName("\"1,\"을 분리하면 \"1\"만 포함하는 배열이 생성된다")
     void split_one() {
         assertThat("1".split(",")).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("\"(1,2)\"을 분리하면 ()를 제외하고 \"1\", \"2\"만 포함하는 배열이 생성된다")
+    void split_without_parentheses() {
+        assertThat("(1,2)".replaceAll("[()]", "").split(",")).containsExactly("1", "2");
     }
 }
