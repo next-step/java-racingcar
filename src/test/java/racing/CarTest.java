40b120d4e59de8c14d10b40b8bd7f53fe1f4d769
yeonjiyeon <dalnim95@naver.com>
2025-03-20 00:27:35 +0900

    feat: 비즈니스 로직 변경에 따른 테스트 코드 수정 및 추가

--- src/test/java/racing/CarTest.java
+++ src/test/java/racing/CarTest.java
@@ -1,13 +1,14 @@
 package racing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 import racing.domain.Car;
 
 public class CarTest {
 
-  Car car = new Car();
+  Car car = new Car("pobi");
 
   @Test
   void 자동차_멈춤() {
@@ -19,6 +20,27 @@ public class CarTest {
   void 자동차_전진() {
     car.driveOrStop(4);
     assertThat(car.getPosition()).isEqualTo(1);
+  }
+
+  @Test
+  void 랜덤값_마이너스() {
+    assertThatThrownBy(() -> car.driveOrStop(-1))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("랜덤 값은 0에서 9 사이여야 합니다.");
+  }
+
+  @Test
+  void 랜덤값_최대초과() {
+    assertThatThrownBy(() -> car.driveOrStop(10))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("랜덤 값은 0에서 9 사이여야 합니다.");
+  }
+
+  @Test
+  void 자동차_이름_5글자_초과() {
+    assertThatThrownBy(() -> new Car("longname"))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("자동차 이름은 5자를 초과할 수 없습니다.");
   }
 }
 
--- src/test/java/racing/RacingTest.java
+++ src/test/java/racing/RacingTest.java
@@ -2,15 +2,33 @@ package racing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import racing.domain.Car;
 import racing.domain.Racing;
 
 class RacingTest {
 
   @Test
   void 자동차_객체_생성() {
-    assertThat(new Racing(3).getCars().size()).isEqualTo(3);
+    String[] carNames = {"pobi", "crong", "honux"};
+    assertThat(new Racing(carNames).getCars().size()).isEqualTo(3);
 
+  }
+
+  @Test
+  void 제일_높은_position_가진_자동차_구하기() {
+    String[] carNames = {"pobi", "crong", "honux"};
+    Racing racing = new Racing(carNames);
+    List<Car> cars = racing.getCars();
+
+    cars.get(0).driveOrStop(0);
+    cars.get(1).driveOrStop(5);
+    cars.get(2).driveOrStop(5);
+
+    List<String> winners = racing.getMaxPosition();
+
+    assertThat(winners).containsExactlyInAnyOrder("crong", "honux");
   }
 
 }
