ead306cefa73bc32243772047539b072adb2d1aa
yeojin yu <yeojin@ninewatt.com>
2025-03-20 11:51:57 +0900

    feat: 우승자 검증 테스트코드 추가

--- src/test/java/step4/racingCar/CarsTest.java
+++ src/test/java/step4/racingCar/CarsTest.java
@@ -1,13 +1,18 @@
 package step4.racingCar;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import step4.racingCar.domain.Car;
 import step4.racingCar.domain.Cars;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class CarsTest {
     @Test
@@ -22,5 +27,21 @@ class CarsTest {
     void negativeCars() {
         String[] names = {};
         assertThatThrownBy(() -> Cars.generateCars(Arrays.asList(names))).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("우승자가 input 값 안에 있는지 확인")
+    void winners() {
+        //given
+        String[] names = {"yeo", "jin", "yu"};
+        Cars cars = Cars.generateCars(Arrays.asList(names));
+
+        //when
+        cars.move();
+        List<Car> winCars = cars.winners();
+        List<String> winners = winCars.stream().map(Car::getName).collect(Collectors.toList());
+
+        //then
+        assertThat(winners).containsAnyOf("yeo", "jin" , "yu");
     }
 }
