d6bc38c84eba7f0105d7d1ad6afa8d0c6928d16a
jongheon1 <heon0128@gmail.com>
2025-03-20 20:26:44 +0900

    feat(carName 검증 로직)

--- src/main/java/step4/CarName.java
+++ src/main/java/step4/CarName.java
@@ -4,7 +4,17 @@ public class CarName {
     private final String carName;
 
     public CarName(String carName) {
+        validateCarName(carName);
         this.carName = carName;
+    }
+
+    private void validateCarName(String carName) {
+        if (carName.isBlank()) {
+            throw new RuntimeException("자동차의 이름은 공백일 수 없습니다.");
+        }
+        if (carName.length() > 5) {
+            throw new RuntimeException("자동차의 이름은 5보다 클 수 없습니다.");
+        }
     }
 
     String getCarName() {
--- src/test/java/step4/CarNameTest.java
+++ src/test/java/step4/CarNameTest.java
@@ -2,7 +2,7 @@ package step4;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class CarNameTest {
 
@@ -10,5 +10,17 @@ public class CarNameTest {
     void createCarName() {
         CarName carName = new CarName("1");
         assertThat(carName.getCarName()).isEqualTo("1");
+    }
+
+    @Test
+    void 이름은_공백일_수_없다() {
+        assertThatThrownBy(() -> new CarName("  "))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 이름은_길이가_5를_초과할_수_없다() {
+        assertThatThrownBy(() -> new CarName("123456"))
+                .isInstanceOf(RuntimeException.class);
     }
 }
