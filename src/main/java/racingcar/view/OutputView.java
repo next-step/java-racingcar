c535ddfd0b20bee0bd71dfe1b036f47d1bae30f3
LeeJin0527 <zinzun@knu.ac.kr>
2025-03-20 13:55:23 +0900

    feat(step4): 최종 우승자 출력기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,31 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static racingcar.message.Message.WINNING_MESSAGE;
+
 public class OutputView {
+
+    private OutputView() {}
+
+    private static final String SEPERATOR = ", ";
 
     public static void print(String message) {
         System.out.println(message);
+    }
+
+    public static void printWinners(List<Car> winners) {
+        String winnerNames = formatWinnersName(winners);
+
+        print(winnerNames + WINNING_MESSAGE);
+    }
+
+    private static String formatWinnersName(List<Car> winners) {
+        return winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(SEPERATOR));
     }
 }
