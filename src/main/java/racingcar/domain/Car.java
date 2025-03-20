4f8efb681293ab1b79a19c3b2ba1f2a25cb09ce1
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-20 11:56:42 +0900

    Car 클래스에 이름을 저장하는 변수를 추가하고 이름의 유효성을 검증하는 validateName 메서드를 추가한다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,23 +2,44 @@ package racingcar.domain;
 
 import java.util.Random;
 
+import racingcar.exception.NameLengthException;
+
 public class Car {
     private static final int MIN_MOVEMENT_THRESHOLD = 4;
     private static final int GO = 1;
     private static final int STOP = 0;
+    public static final int START = 0;
 
     private final Random random = new Random();
 
-    private int position = 0;
+    private int position;
+    private final String name;
 
-    public void move() {
-        if (getMovementStatus(generateRandomNumber()) == GO) {
-            position += GO;
+    public Car(String name) {
+        validateName(name);
+
+        this.name = name;
+        this.position = START;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new NameLengthException("이름은 5자 이하만 가능합니다.");
         }
     }
 
     public int getPosition() {
         return position;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public void move() {
+        if (getMovementStatus(generateRandomNumber()) == GO) {
+            position += GO;
+        }
     }
 
     private int getMovementStatus(int randomNumber) {
@@ -33,4 +54,3 @@ public class Car {
         return random.nextInt(10);
     }
 }
-
---
+++ src/main/java/racingcar/exception/NameLengthException.java
@@ -0,0 +1,7 @@
+package racingcar.exception;
+
+public class NameLengthException extends IllegalArgumentException {
+    public NameLengthException(String message) {
+        super(message);
+    }
+}
