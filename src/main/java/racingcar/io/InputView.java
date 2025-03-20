56932c761af8d6c654e5c4fca575f1aae3931237
jayden(김준기) <jayden.i@kakaocorp.com>
2025-03-20 12:07:14 +0900

    Input 클래스에 자동차 이름을 입력받고 쉼표(,)를 기준으로 분리하는 inputCarNames 메서드를 추가한다.

--- src/main/java/racingcar/io/InputView.java
+++ src/main/java/racingcar/io/InputView.java
@@ -16,6 +16,11 @@ public class InputView {
         return readInt("자동차 대수는 몇 대 인가요?");
     }
 
+    public String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine().split(",");
+    }
+
     public int getMovementCount() throws InvalidInputException {
         return readInt("시도할 회수는 몇 회 인가요?");
     }
