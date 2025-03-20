67941cbc7a719b7bb3f4997cfb63aafcaaf39b12
cm0x0x0x0 <cm0x0x0x0@gmail.com>
2025-03-20 19:57:46 +0900

    test: add parameter to RacingCar Constructor

--- src/test/java/RacingCar/RacingCarTest.java
+++ src/test/java/RacingCar/RacingCarTest.java
@@ -6,17 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RacingCarTest {
     private static final int MOVE_CONDITION = 4;
-
+    private static final String TEST_CAR_NAME = "TEST_CAR";
     @Test
     void moveShouldIncreasePosition(){
-        RacingCar car = new RacingCar(MOVE_CONDITION, new TestNumberGenerator(MOVE_CONDITION));
+        RacingCar car = new RacingCar(MOVE_CONDITION, new TestNumberGenerator(MOVE_CONDITION), TEST_CAR_NAME);
         car.move();
         assertEquals(1, car.position());
     }
 
     @Test
     void moveShouldNotIncreasePosition(){
-        RacingCar car = new RacingCar(MOVE_CONDITION, new TestNumberGenerator(MOVE_CONDITION-1));
+        RacingCar car = new RacingCar(MOVE_CONDITION, new TestNumberGenerator(MOVE_CONDITION-1), TEST_CAR_NAME);
         car.move();
         assertEquals(0, car.position());
     }
