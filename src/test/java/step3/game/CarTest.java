df91b6fa5a0a199ac726d01013936f73ad1aeb0c
HongYeseul <yeseul.dev@gmail.com>
2025-03-21 07:50:55 +0900

    test(step5): 최종 우승자 테스트 코드 추가

--- src/test/java/step3/game/CarTest.java
+++ src/test/java/step3/game/CarTest.java
@@ -2,6 +2,8 @@ package step3.game;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import step3.game.domain.car.Car;
 import step3.game.domain.car.CarName;
 
@@ -11,11 +13,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CarTest {
 
-    @Test
-    @DisplayName("자동차는 전진할 수 있다.")
+    @ParameterizedTest
+    @DisplayName("자동차는 4 이상의 값일 때 전진할 수 있다.")
+    @ValueSource(ints = {4, 5, 6})
     void carCanMove() {
         Car car = new Car(new CarName("test"));
-        Car movedCar = car.move();
+        Car movedCar = car.moveIfPossible(5);
         assertThat(movedCar.getDistance()).isEqualTo(1);
     }
 
