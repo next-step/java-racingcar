b8ef46d564c2b85c7255b696ca4c67b9173a0c9b
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-03-22 01:42:54 +0900

    step5: CarNameList

--- src/test/java/step5/racingcar3/domain/CarNameListTest.java
+++ src/test/java/step5/racingcar3/domain/CarNameListTest.java
@@ -1,5 +1,26 @@
 package step5.racingcar3.domain;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class CarNameListTest {
+
+    @Test
+    @DisplayName("CarNameList 일급컬렉션 생성 테스트")
+    void givenCarNameList_whenCreateCarNameList_thenSuccess() {
+        // given
+        CarName carName1 = new CarName("a");
+        CarName carName2 = new CarName("b");
+        CarName carName3 = new CarName("c");
+        CarName carName4 = new CarName("d");
+        CarName carName5 = new CarName("e");
+
+        // when
+        // then
+        assertDoesNotThrow(() -> new CarNameList(List.of(carName1, carName2, carName3, carName4, carName5)));
+    }
 
 }
