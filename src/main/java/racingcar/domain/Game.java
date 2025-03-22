b5ff116612db883f8d03a061e053b4337610ec2b
Gukin Han <gukin.dev@gmail.com>
2025-03-22 14:19:07 +0900

    feat: 게임 클래스로 분리

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,47 @@
+package racingcar.domain;
+
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
+import java.util.List;
+import java.util.Random;
+
+public class Game {
+    private final InputView inputView;
+    private final ResultView resultView;
+    private final Random random;
+    private final Tracker tracker;
+
+    private Cars cars;
+
+
+    public Game(InputView inputView, ResultView resultView, Random random, Tracker tracker) {
+        this.inputView = inputView;
+        this.resultView = resultView;
+        this.random = random;
+        this.tracker = tracker;
+
+        this.postConstruct();
+    }
+
+    private void postConstruct() {
+        List<String> carNames = inputView.getCarNames();
+        cars = CarFactory.create(carNames);
+    }
+
+    public void start() {
+        int attempts = inputView.getAttempts();
+        this.race(attempts);
+    }
+
+    public void finish() {
+        this.resultView.printResult(tracker.findAllRecords());
+    }
+
+    private void race(int attempts) {
+        for (int i = 0; i < attempts; i++) {
+            cars.race(random);
+            tracker.saveRecords(this.cars);
+        }
+    }
+}
