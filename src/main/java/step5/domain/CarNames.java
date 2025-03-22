1349a635ccd74ea3345a3ee9e82304bcafa0e3a2
jongheon1 <heon0128@gmail.com>
2025-03-22 20:32:53 +0900

    feat(CarNames)

---
+++ src/main/java/step5/domain/CarNames.java
@@ -0,0 +1,20 @@
+package step5.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarNames {
+    private final List<CarName> carNames;
+
+    public CarNames(String[] carNameArray) {
+        ArrayList<CarName> carNames = new ArrayList<>();
+        for (String carName : carNameArray) {
+            carNames.add(new CarName(carName));
+        }
+        this.carNames = carNames;
+    }
+
+    public List<CarName> getCarNames() {
+        return this.carNames;
+    }
+}
