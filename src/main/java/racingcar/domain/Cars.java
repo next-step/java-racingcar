a0232a898ad79f22cb07de36a74611f579d4014f
Gukin Han <gukin.dev@gmail.com>
2025-03-22 17:05:03 +0900

    refactor: 유효성검증의 주체를 생성자로 이동

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,15 +1,16 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Cars {
 
     public static final int RANDOM_BOUND = 10;
-    private final List<Car> cars;
+    private final List<Car> cars = new ArrayList<>();
 
     public Cars(List<Car> cars) {
-        this.cars = cars;
+        this.cars.addAll(cars);
     }
 
     public boolean isEmpty() {
@@ -17,10 +18,6 @@ public class Cars {
     }
 
     public void race(Random random) {
-        if (cars == null || cars.isEmpty()) {
-            throw new IllegalStateException("경주를 하기 위해서는 적어도 한 대의 자동차가 있어야 합니다.");
-        }
-
         for (Car car : cars) {
             car.moveOrStop(random.nextInt(RANDOM_BOUND));
         }
