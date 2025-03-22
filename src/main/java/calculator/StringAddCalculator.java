c825646445a6d82fed06f852ef6b5bffd8604ef4
giraffeb <rwpark07@gmail.com>
2025-03-22 20:32:41 +0900

    feat: step2 - 기본 구분자를 통한 계산기 구현

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,9 +1,23 @@
 package calculator;
 
-public class StringAddCalculator {
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-  public static int splitAndSum(String str) {
-    return 0;
-  }
+public class StringAddCalculator {
+    public static final String DEFAULT_DELIMITERS = "[,:]";
+
+    public static int splitAndSum(String str) {
+        if(str == null) return 0;
+        if(str.isEmpty()) return 0;
+
+        return Arrays.stream(str.split(DEFAULT_DELIMITERS))
+                .mapToInt( tokenString -> {
+                    int number = Integer.parseInt(tokenString);
+                    if(number < 0) throw new RuntimeException();
+
+                    return number;
+                })
+                .sum();
+    }
 
 }
