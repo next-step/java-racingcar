dee8444a132b8e87633d84dfc2f569d41001e776
jlee0505 <hihjlee0505@gmail.com>
2025-03-22 11:12:19 +0900

    feat: 자동차 이름은 5자를 초과할 수 없다.

--- src/main/java/step4/InputView.java
+++ src/main/java/step4/InputView.java
@@ -1,8 +1,7 @@
 package step4;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import javax.naming.InvalidNameException;
+import java.util.*;
 
 
 public class InputView {
@@ -26,7 +25,7 @@ public class InputView {
     }
 
     public List<String> getCarNamesFromUser() {
-        boolean isFirstAttempt = true; // 첫 번째 시도인지 여부를 나타내는 플래그
+        boolean isFirstAttempt = true;
 
         while (true) {
             if (isFirstAttempt) {
@@ -37,11 +36,15 @@ public class InputView {
             String input = scanner.nextLine().trim();
 
             // 빈 입력값인 경우
-            if (input.isEmpty()) {
-                System.out.println("입력이 비어 있습니다. 자동차 이름을 입력해 주세요.");
-                scanner.nextLine();
+            try {
+                if (input.isEmpty()) {
+                    throw new Exception("입력이 비어 있습니다. 자동차 이름을 입력해 주세요.");
+                }
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
                 continue;
             }
+
 
             // 쉼표(,)를 포함하지 않는 경우
             if (!input.contains(",")) {
@@ -49,7 +52,6 @@ public class InputView {
                 continue;
             }
 
-            // 입력값 split 및 트리밍
             String[] splitNames = input.split(",");
             List<String> names = new ArrayList<>();
             for (String name : splitNames) {
@@ -62,17 +64,29 @@ public class InputView {
             // 이름이 2개 미만인 경우
             if (names.size() < 2) {
                 System.out.println("경주를 위해 2개 이상의 차를 입력해 주세요.");
-                continue; // 다시 입력 받기
+                continue;
             }
 
-            // [TODO] 각 이름 검증
-            // 각 이름은 5글자를 초과할 수 없음.
-            // 각 이름은 달라야하며 동명 자동차 존재할 수 없음.
+
+            Set<String> namesHash = new HashSet<>(names);
+            try {
+                for(String name:names) {
+                    // 각 이름은 5글자를 초과할 수 없음.
+                    if(name.length() > 5) {
+                        throw new Exception("자동차 이름은 5글자까지만 가능해요!");
+                    }
+
+                    // 각 이름은 달라야하며 동명 자동차 존재할 수 없음.
+                    if(names.size()!= namesHash.size()){
+                        throw new Exception("자동차 이름은 중복될 수 없어요!");
+                    }
+                }
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
 
             return names; // 유효한 입력값이 있으면 반환
         }
     }
-
-
-
 }
