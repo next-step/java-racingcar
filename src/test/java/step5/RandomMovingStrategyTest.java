d975dba01af02b6ec8083a7079b7a2f297f04eaa
jongheon1 <heon0128@gmail.com>
2025-03-22 19:23:13 +0900

    feat(randomMovingStrategy)

---
+++ src/test/java/step5/RandomMovingStrategyTest.java
@@ -0,0 +1,34 @@
+package step5;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomMovingStrategyTest {
+
+    @Test
+    void 랜덤값이_4면_참을_반환() {
+        RandomMovingStrategy randomMovingStrategy = new RandomMovingStrategy(new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return 4;
+            }
+        });
+
+        assertThat(randomMovingStrategy.movable()).isTrue();
+    }
+
+    @Test
+    void 랜덤값이_3이면_거짓을_반환() {
+        RandomMovingStrategy randomMovingStrategy = new RandomMovingStrategy(new Random() {
+            @Override
+            public int nextInt(int bound) {
+                return 3;
+            }
+        });
+
+        assertThat(randomMovingStrategy.movable()).isFalse();
+    }
+}
