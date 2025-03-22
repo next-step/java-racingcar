d9e3b0bc7eeaa62102a9e6d68b3895ecb7d1bd40
giraffeb <rwpark07@gmail.com>
2025-03-22 21:02:25 +0900

    feat: step2 - 커스텀 구분자 기능

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,16 +1,14 @@
 package calculator;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class StringAddCalculator {
-    public static final String DEFAULT_DELIMITERS = "[,:]";
 
     public static int splitAndSum(String str) {
         if(str == null) return 0;
         if(str.isEmpty()) return 0;
 
-        return Arrays.stream(str.split(DEFAULT_DELIMITERS))
+        return Arrays.stream(StringTokenizer.tokenize(str))
                 .mapToInt( tokenString -> {
                     int number = Integer.parseInt(tokenString);
                     if(number < 0) throw new RuntimeException();
