e9fa4bc23861189df42159529e406413074ab833
myj4513 <loki.ee@kakaocorp.com>
2025-03-23 10:33:16 +0900

    step1 피드백 수정

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,6 +1,5 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +9,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
 
@@ -26,20 +27,20 @@ public class SetTest {
     @Test
     @DisplayName("size() 메서드를 활용해 Set의 사이즈를 구한다.")
     public void sizeTest() {
-        Assertions.assertThat(numbers.size()).isEqualTo(3);
+        assertThat(numbers).hasSize(3);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인한다.")
     public void containsTest(Integer input) {
-        Assertions.assertThat(numbers.contains(input)).isTrue();
+        assertThat(numbers).contains(input);
     }
 
     @ParameterizedTest
     @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false", "6,false"})
     @DisplayName("contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인한다.")
     public void containsTestUsingCsvSource(Integer input, boolean expected) {
-        Assertions.assertThat(numbers.contains(input)).isEqualTo(expected);
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
