d456384f9772314b311f3bfcbfe67efba7733263
jlee0505 <hihjlee0505@gmail.com>
2025-03-24 14:10:08 +0900

    refactor: 불필요한 테스트코드 삭제
    - 비즈니스 로직과 상관 없는 I/O 테스트 삭제
    - 불필요해진 주석 삭제

--- src/test/java/step3/RacingGameTest.java
+++ src/test/java/step3/RacingGameTest.java
@@ -12,27 +12,9 @@ import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// 질문:
-// - 어떤 메소드를 테스트로 작성해야 하는가? 사용자에게 입력을 받는 사소한 메소드도 테스트를 작성하는 게 좋은가?
-// - Scanner을 사용한 메소드는 어떻게 테스트를 작성해야 하는가?
-
 class RacingGameTest {
 
     private RacingGame racingGame;
-
-    @Test
-    void testInputViewGetCarCountFromUser() {
-        String mockInput = "3";
-        InputView inputView = new InputView(new Scanner(new ByteArrayInputStream(mockInput.getBytes())));
-        assertEquals(3, inputView.getCarCountFromUser());
-    }
-
-    @Test
-    void testInputViewGetRoundCountFromUser() {
-        String mockInput = "5";
-        InputView inputView = new InputView(new Scanner(new ByteArrayInputStream(mockInput.getBytes())));
-        assertEquals(5, inputView.getRoundCountFromUser());
-    }
 
     @Test
     void testNoMovement() {
@@ -43,7 +25,7 @@ class RacingGameTest {
                 return 3;
             }
         };
-        Car car = new Car(mockRandom);
+        Car car = new Car();
 
         int initialPosition = car.getPosition();
         car.move();
@@ -60,40 +42,11 @@ class RacingGameTest {
                 return 5;
             }
         };
-        Car car = new Car(mockRandom);
+        Car car = new Car();
 
         int initialPosition = car.getPosition();
         car.move();
 
         assertEquals(initialPosition+1, car.getPosition());
-    }
-
-
-    @Test
-    void testResultViewPrintResults() {
-//        List<List<String>> finalResults = new ArrayList<>();
-//        List<String> round1 = new ArrayList<>();
-//        round1.add("---");
-//        finalResults.add(round1);
-//
-//        List<String> round2 = new ArrayList<>();
-//        round2.add("------");
-//        finalResults.add(round2);
-//
-//        List<String> round3 = new ArrayList<>();
-//        round3.add("-----");
-//        finalResults.add(round3);
-//
-//        ResultView resultView = new ResultView();
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//        System.setOut(new PrintStream(outputStream));
-//
-//        resultView.printResults(finalResults);
-//
-//        String expected = "---\n" +"\n"+
-//                "------\n" +"\n"+
-//                "-----\n"+"\n";
-//
-//        assertEquals(expected, outputStream.toString());
     }
 }
