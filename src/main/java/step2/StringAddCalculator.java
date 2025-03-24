4fabbf9e5b6677a2ca78045543e0b7298033a2b9
hjl <106682461+idisdi@users.noreply.github.com>
2025-03-24 10:59:32 +0900

    2단계 - 문자열 덧셈 계산기 (#5982)
    * feat: StringTest Task.1
    
    * feat: StringTest Task.2
    
    * feat: StringTest Task.3
    
    * feat: SetTest Task.1
    
    * feat: SetTest Task.2
    
    * feat: SetTest Task.2 using ParameterizedTest
    
    * feat: SetTest Task.3
    
    * 피드백 반영
    
    - EOF 표시
    - 경계값 추가로 테스트 보완
    
    * gradle 버전 맞추기
    
    * 폴더 구조 step 별로 정리
    
    * feat: 수도(pseudo) 테스트 작성
    
    - jdk 문제로 jdk 타 벤더사로 재설치. 관련 설정 제거.
    
    * feat: Test 1 작성, 메소드 작성, 및 테스트 통과
    
    * feat: Test 2 작성, 메소드 작성, 및 테스트 통과
    
    * feat: Test 3 작성, 메소드 작성, 및 테스트 통과
    
    * feat: Test 4 작성, 메소드 작성, 및 테스트 통과
    
    - 문제: findCustomerSeparator 메소드를 테스트하는 과정에서, 기존 클래스 내 private static 값이었던 separators 를 테스트하기 어려운 문제
    - 접근: 클래스 내 priavte 공용값이었던 separators를 로컬변수로 취급하는 순수함수를 만들어 반환하는 로직으로 변경
    
    * feat: Test 5 작성, 메소드 작성, 및 테스트 통과
    
    - 변경된 로직에 따라 추가된 테스트 수정 및 수행
    
    * feat: Test 6 작성, 메소드 작성, 및 테스트 통과
    
    - invalid input 검증 테스트
    
    * feat: Test 7 작성, 메소드 작성, 및 테스트 통과
    
    - 모든 메소드를 합쳐 실행하는 최종 메소드 테스트
    - invalid input 역할에 따라 세분화
    
    * refactor:  "" 외 " " 가 전달될 경우 처리
    
    * refactor: 유효성 검사 로직을 정규식을 사용하도록 수정
    
    * refactor: TODO 처리
    
    - stream 사용 없이 구현
    
    * refactor: 불필요한 주석 삭제
    
    ---------
    
    Co-authored-by: jlee0505 <hj960322@gmail.com>

---
+++ src/main/java/step2/StringAddCalculator.java
@@ -0,0 +1,99 @@
+package step2;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringAddCalculator {
+
+    private static String createRegex(String separators) {
+        return "["+separators+"]";
+    }
+
+    public int calculate(String input) {
+        if(checkIfEmptyInput(input)) {
+            return 0;
+        }
+        if(!checkIfValidPattern(input)) {
+            throw new IllegalArgumentException("Invalid Pattern are not allowed");
+        }
+        if(!checkIfOnlyPositiveNumbers(input)) {
+            throw new IllegalArgumentException("Negative numbers or NaN are not allowed");
+        }
+
+        String numbersWithSeparators = extractNumbers(input); // "\\;\n1;2;3" -> "1;2;3"
+        String separators = filterSeparators(input); // SEPARATORS = ",:" -> ",:;"
+        int[] numbers = splitNumbers(numbersWithSeparators, separators); // e,g. "1,2;3" -> [1,2,3]
+        int result = addNumbersInArray(numbers); // [1,2,3] -> 6
+
+        return result;
+    };
+
+    public int[] splitNumbers(String numbers, String separators) {
+        String regex = createRegex(separators);
+        String[] tokens = numbers.split(regex);
+
+        int[] result = new int[tokens.length];
+        for(int i=0; i<tokens.length; i++) {
+            result[i] = Integer.parseInt(tokens[i].trim());
+        }
+
+        return result;
+    }
+
+    public int addNumbersInArray(int[] numbers) {
+        int sum = 0;
+
+        if (numbers == null) {
+            return sum;
+        }
+
+        for (int num:numbers) {
+            sum += num;
+        }
+
+        return sum;
+    }
+
+    private String[] checkIfOnlyNumbers(String numbersWithCustomSeparator) {
+        String[] arr = numbersWithCustomSeparator.split("\n");
+        return arr;
+    }
+
+    public String filterSeparators(String numbersWithCustomSeparator) {
+        String separators = ",:"; // default_separators
+
+        String[] arr = checkIfOnlyNumbers(numbersWithCustomSeparator);
+
+        if (arr.length > 1) {
+            separators += arr[0].substring(2);
+        }
+
+        return separators;
+    }
+
+    public String extractNumbers (String numbersWithCustomSeparator) {
+        String[] arr = checkIfOnlyNumbers(numbersWithCustomSeparator);
+        return arr[arr.length-1];
+    }
+
+    public boolean checkIfValidPattern(String input) {
+        Pattern pattern = Pattern.compile("^//[^\\d]+\\n.*");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    public boolean checkIfOnlyPositiveNumbers(String input) {
+        Pattern pattern = Pattern.compile("^[\\d,:\n]*S");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    public boolean checkIfEmptyInput (String input) {
+        if (input == null || input.isBlank()) {
+            return true;
+        }
+        return false;
+    }
+
+}
