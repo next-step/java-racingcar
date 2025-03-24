0788c21dee724540dc64a12bb7e29adce50c30ac
pol.o(김선동)/kakao <pol.o@kakaocorp.com>
2025-03-24 16:32:47 +0900

    test: 자동차 경주 게임 테스트 코드

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,4 +1,29 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.*;
+
 public class CarTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"다섯글자이상"})
+    @DisplayName("자동차 생성 실패")
+    public void createFail(String name) {
+        // then
+        assertThatThrownBy(() -> new Car(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:0", "2:0", "3:0", "4:1"}, delimiter = ':')
+    @DisplayName("주사위 숫자가 4 이상이 나오면 자동차는 움직일 수 있다")
+    public void moveTest(int input, int expect) {
+        Car car = new Car("test");
+        NumberBasedCarMover carMover = new FixedNumberBasedCarMover(input);
+
+        car.move(carMover);
+
+        assertThat(car.position()).isEqualTo(expect);
+    }
 }
--- src/test/java/racingcar/TryCountTest.java
+++ src/test/java/racingcar/TryCountTest.java
@@ -1,4 +1,16 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class TryCountTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"aaa", "-1"})
+    @DisplayName("tryCount 초기화 실패")
+    public void failed(String tryCountString) {
+        assertThatThrownBy(() -> new TryCount(tryCountString)).isInstanceOf(IllegalArgumentException.class);
+    }
 }
