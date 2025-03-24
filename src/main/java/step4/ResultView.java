eddf0a723d5cfbac91c6a2e902f026ac2ee14974
jlee0505 <hihjlee0505@gmail.com>
2025-03-24 10:40:25 +0900

    feat: 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한명 이상일 수 있다.

--- src/main/java/step4/ResultView.java
+++ src/main/java/step4/ResultView.java
@@ -1,14 +1,42 @@
 package step4;
 
+import java.awt.*;
+import java.sql.Array;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ResultView {
     public void printResults (List<List<String>> finalResults) {
         for (List<String> round : finalResults) {
-            for (String s : round) {
-                System.out.println(s);
-            }
+            printEachRoundResult(round);
             System.out.println();
         }
+        printWinner(finalResults.get(finalResults.size() - 1));
     }
+
+    public void printEachRoundResult (List<String> round) {
+        for (String s : round) {
+            System.out.println(s);
+        }
+    }
+
+    public void printWinner(List<String> lastRound) {
+        List<String> winners = new ArrayList<>();
+        int maxLength = 0;
+
+        for (String result : lastRound) {
+            String[] parts = result.split(" : ");
+            int length = parts[1].length();
+            if (length > maxLength) {
+                maxLength = length;
+                winners.clear();
+                winners.add(parts[0]);
+            } else if (length == maxLength) {
+                winners.add(parts[0]);
+            }
+        }
+
+        System.out.println(String.join(", ", winners) + "가 우승했습니다.");
+    }
+
 }
