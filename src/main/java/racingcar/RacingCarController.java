2183cbbc5de7a963fc562af23640de4aaeed8245
ekzm8523 <mjw8523@gmail.com>
2025-03-26 12:24:09 +0900

    faet: RacingCarController는 레이싱 횟수를 지정할 수 있다.

--- src/main/java/racingcar/RacingCarController.java
+++ src/main/java/racingcar/RacingCarController.java
@@ -7,14 +7,20 @@ import java.util.stream.IntStream;
 public class RacingCarController {
 
     private List<Car> cars;
+    private int numberOfOperations;
 
-    public RacingCarController(int numberOfParticipants) {
+    public RacingCarController(int numberOfParticipants, int numberOfOperations) {
         this.cars = IntStream.range(0, numberOfParticipants)
                 .mapToObj(i -> new Car())
                 .collect(Collectors.toList());
+        this.numberOfOperations = numberOfOperations;
     }
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public int getOperationCount() {
+        return numberOfOperations;
     }
 }
--- src/test/java/racingcar/RacingCarControllerTest.java
+++ src/test/java/racingcar/RacingCarControllerTest.java
@@ -10,7 +10,14 @@ class RacingCarControllerTest {
     @Test
     @DisplayName("n대의 자동차를 생성할 수 있다.")
     void generateCarForParticipantTest() {
-        RacingCarController controller = new RacingCarController(3);
+        RacingCarController controller = new RacingCarController(3, 5);
         assertThat(controller.getCars()).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("지정된 횟수만큼 자동차 경주를 진행할 수 있다.")
+    void controlOperationCountTest() {
+        RacingCarController controller = new RacingCarController(3, 5);
+        assertThat(controller.getOperationCount()).isEqualTo(5);
     }
 }
