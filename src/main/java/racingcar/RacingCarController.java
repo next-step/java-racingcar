29e2d4cddd768f1d8b4e60743485438de9eef19b
ekzm8523 <mjw8523@gmail.com>
2025-03-26 12:37:32 +0900

    feat: RacingCarController 랜덤값으로부터 자동차를 움직이는 run 메서드 생성

--- src/main/java/racingcar/RacingCarController.java
+++ src/main/java/racingcar/RacingCarController.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -8,12 +9,16 @@ public class RacingCarController {
 
     private List<Car> cars;
     private int numberOfOperations;
+    private MovingStrategy movingStrategy;
+    private Random numberGenerator;
 
     public RacingCarController(int numberOfParticipants, int numberOfOperations) {
         this.cars = IntStream.range(0, numberOfParticipants)
                 .mapToObj(i -> new Car())
                 .collect(Collectors.toList());
         this.numberOfOperations = numberOfOperations;
+        this.movingStrategy = new MovingStrategy();
+        this.numberGenerator = new Random();
     }
 
     public List<Car> getCars() {
@@ -23,4 +28,13 @@ public class RacingCarController {
     public int getOperationCount() {
         return numberOfOperations;
     }
+
+    public void run() {
+        for (Car car: cars) {
+            if (movingStrategy.isMoving(numberGenerator.nextInt(10))) {
+                car.move();
+            }
+        }
+    }
+
 }
