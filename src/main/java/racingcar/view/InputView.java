b6b7c6eff1141412adc77bcd1d1bb93dfc1bd048
Park - Jeonghwan <990706leo@gmail.com>
2025-03-26 13:26:48 +0900

    feat: 시도할 횟수를 입력받을 수 있다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,8 +4,14 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public int createCars(){
+    public int createCars() {
         System.out.println("자동차 대수는 몇 대 인가요?");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public int putCoins() {
+        System.out.println("시도할 횟수는 몇 회 인가요?");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
