700055c64f69277974d6b57e9411d586ad0ddb49
ekzm8523 <mjw8523@gmail.com>
2025-03-26 12:07:47 +0900

    feat: MovingStrategy 객체 생성 * 제공된 숫자가 4 이상이면 True를 Return한다. * 제공된 숫자가 3 이하이면 False를 Return한다. * 움직일 수 있는 숫자를 판단하는 임계값은 생성자로 조절할 수 있다. * 제공된 숫자가 0이상 9이하 이외의 수는 예외를 발생한다. * 임계값으로 제공된 숫자가 0이상 9이하 이외의 수는 예외를 발생한다.

---
+++ src/main/java/racingcar/MovingStrategy.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+public class MovingStrategy {
+
+    private final int movableMinNumberThreshold;
+    private final int DEFAULT_MOVABLE_MIN_NUMBER_THRESHOLD = 4;
+    private final int MIN_NUMBER_BOUND = 0;
+    private final int MAX_NUMBER_BOUND = 9;
+
+    public MovingStrategy() {
+        this.movableMinNumberThreshold = DEFAULT_MOVABLE_MIN_NUMBER_THRESHOLD;
+    }
+
+    public MovingStrategy(int movableMinNumberThreshold) {
+        checkValidNumber(movableMinNumberThreshold);
+        this.movableMinNumberThreshold = movableMinNumberThreshold;
+    }
+
+    public boolean isMoving(int number) {
+        checkValidNumber(number);
+        return movableMinNumberThreshold <= number;
+    }
+
+    private void checkValidNumber(int number) {
+        if (number < MIN_NUMBER_BOUND || number > MAX_NUMBER_BOUND) {
+            throw new IllegalArgumentException(
+                    String.format("%d 이상 %d 이하 숫자만 입력하셔야 합니다.", MIN_NUMBER_BOUND, MAX_NUMBER_BOUND)
+            );
+        }
+    }
+
+}
