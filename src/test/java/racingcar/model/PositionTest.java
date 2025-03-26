7c551d11db1e2bd313418ff99ef0bcdd07c870db
LeeJin0527 <zinzun@knu.ac.kr>
2025-03-26 09:58:09 +0900

    test(step4) : PositionTest 위치 기반으로 수정

--- src/test/java/racingcar/model/PositionTest.java
+++ src/test/java/racingcar/model/PositionTest.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,21 +9,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PositionTest {
 
-    @DisplayName("초기 위치가 5인 자동차가 2번 이동 하면 현재 위치보다 2 증가한다.")
+    @DisplayName("incrementPosition() 호출시 1 증가된 Position이 반환된다.")
     @ParameterizedTest
     @CsvSource({
-            "0,1,1,0",
-            "3,4,4,5",
-            "5,4,1,6",
-            "5,1,5,6"
+            "0, 1",
+            "1, 2",
+            "2, 3",
+            "3, 4",
     })
-    void move_initial_move(int initialPosition, int randomValue, int randomValue2, int expectedPosition) {
-        Car initialCar = new Car(new CarName("test"), new Position(initialPosition));
+    void incrementPosition(int initialPosition, int expectedPosition) {
+        Position position = new Position(initialPosition);
 
-        Car movedOnceCar = initialCar.move(randomValue);
-        Car movedTwiceCar = movedOnceCar.move(randomValue2);
+        Position movedPosition = position.incrementPosition();
 
-        assertThat(movedTwiceCar.isAtPosition(new Position(expectedPosition))).isTrue();
+        assertThat(movedPosition.getPosition()).isEqualTo(expectedPosition);
     }
-
 }
