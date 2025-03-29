a12bd8df023817e694167fc2e8278010400c9c65
ekzm8523 <mjw8523@gmail.com>
2025-03-29 15:24:55 +0900

    refactor: Controller의 역할을 Application으로 이동 & 비즈니스로직을 담당하는  RacingGame 객체 분리

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -2,48 +2,31 @@ package racingcar.controller;
 
 import racingcar.domain.Car;
 import racingcar.domain.MovingStrategy;
-import racingcar.view.OutputView;
 
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class RacingCarController {
+public class RacingGame {
 
     private final List<Car> cars;
-    private final int numberOfOperations;
     private final MovingStrategy movingStrategy;
     private final Random numberGenerator;
-    private final OutputView outputView;
 
-    public RacingCarController(int numberOfParticipants, int numberOfOperations, OutputView outputView) {
+    public RacingGame(int numberOfParticipants) {
         this.cars = IntStream.range(0, numberOfParticipants)
                 .mapToObj(i -> new Car())
                 .collect(Collectors.toList());
-        this.numberOfOperations = numberOfOperations;
         this.movingStrategy = new MovingStrategy();
         this.numberGenerator = new Random();
-        this.outputView = outputView;
     }
 
     public List<Car> getCars() {
         return cars;
     }
 
-    public int getOperationCount() {
-        return numberOfOperations;
-    }
-
-    public void run() {
-        outputView.printStartMessage();
-        for (int i = 0; i < numberOfOperations; i++) {
-            tryOneRace();
-            outputView.printResult(getResults());
-        }
-    }
-
-    private List<Integer> getResults() {
+    public List<Integer> getResults() {
         return cars.stream().map(Car::getPosition).collect(Collectors.toList());
     }
 
--- src/test/java/racingcar/controller/RacingCarControllerTest.java
+++ src/test/java/racingcar/controller/RacingCarControllerTest.java
@@ -2,7 +2,6 @@ package racingcar.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racingcar.view.OutputView;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,14 +10,8 @@ class RacingCarControllerTest {
     @Test
     @DisplayName("n대의 자동차를 생성할 수 있다.")
     void generateCarForParticipantTest() {
-        RacingCarController controller = new RacingCarController(3, 5, new OutputView());
+        RacingGame controller = new RacingGame(3);
         assertThat(controller.getCars()).hasSize(3);
     }
 
-    @Test
-    @DisplayName("지정된 횟수만큼 자동차 경주를 진행할 수 있다.")
-    void controlOperationCountTest() {
-        RacingCarController controller = new RacingCarController(3, 5, new OutputView());
-        assertThat(controller.getOperationCount()).isEqualTo(5);
-    }
 }
