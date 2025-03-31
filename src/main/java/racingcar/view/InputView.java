cec10354ce4e2f49412dd286808e93d3d5079f4f
Park - Jeonghwan <990706leo@gmail.com>
2025-03-31 13:19:10 +0900

    feat: 자동차 이름을 입력받을 수 있다

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public int createCars() {
-        System.out.println("자동차 대수는 몇 대 인가요?");
+    private static final String NAME_DELIMITER = ",";
+
+    public String[] readNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        String input = scanner.nextLine();
+        return input.split(NAME_DELIMITER);
     }
 
     public int putCoins() {
