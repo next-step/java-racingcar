f7b357743edae8ac5a0db310336deeb129ead937
SeungMin <semicpu@gmail.com>
2025-04-03 12:04:58 +0900

    refactor: change class name as upper case

--- src/main/java/carrace/main/Game.java
+++ src/main/java/carrace/main/Game.java
@@ -5,7 +5,7 @@ import carrace.utils.InputValidator;
 
 import java.util.Scanner;
 
-public class game {
+public class Game {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
