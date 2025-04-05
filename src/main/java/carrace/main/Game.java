a1cf5303d547b35af1192e8ad6b03865198a2fae
SeungMin <semicpu@gmail.com>
2025-04-05 19:10:48 +0900

    refactor: moved Scanner to InputView

--- src/main/java/carrace/main/Game.java
+++ src/main/java/carrace/main/Game.java
@@ -4,31 +4,23 @@ import carrace.race.Race;
 import carrace.view.InputView;
 import carrace.view.OutputView;
 
-import java.util.Scanner;
-
 public class Game {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
 
         // 게임 준비
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String[] carNames = inputView.receiveCarNames(scanner.nextLine());
-
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        int numTry = inputView.receiveNumTry(scanner.nextInt());
+        String[] carNames = inputView.receiveCarNames();
+        int numTry = inputView.receiveNumTry();
 
         Race race = new Race(carNames);
 
-        // 게임 진행 중의 상태 출력을 위해 outputView를 사용
+        // 의존성을 변경하기위해 start 대신 startSingleRound를 사용
         for (int i = 0; i < numTry; i++) {
             race.startSingleRound();
             outputView.printCurrentState(race.getVehicles());
         }
         outputView.printWinner(race.getWinner());
-
-        scanner.close();
     }
 }
--- src/main/java/carrace/view/InputView.java
+++ src/main/java/carrace/view/InputView.java
@@ -1,10 +1,13 @@
 package carrace.view;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static carrace.rules.CarRaceRules.checkLargerThanMaxCarNameLength;
 
 public class InputView {
+
+    private final Scanner scanner = new Scanner(System.in);
 
     public Integer receiveCarNum(int numCar) {
         System.out.println("자동차 대수는 몇 대 인가요?");
@@ -14,11 +17,23 @@ public class InputView {
         throw new RuntimeException("자동차 대수는 1 이상의 값이어야합니다.");
     }
 
+    public Integer receiveNumTry() {
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        String input = scanner.nextLine();
+        return receiveNumTry(Integer.parseInt(input));
+    }
+
     public Integer receiveNumTry(int numTry) {
         if (numTry > 0) {
             return numTry;
         }
         throw new RuntimeException("시도할 회수는 1 이상의 값이어야 합니다.");
+    }
+
+    public String[] receiveCarNames() {
+        System.out.println("자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String input = scanner.nextLine();
+        return receiveCarNames(input);
     }
 
     public String[] receiveCarNames(String input) {
