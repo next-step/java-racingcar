9c527375e2ee8f5dacc13a043086ab43d4bb7b35
SeungMin <semicpu@gmail.com>
2025-04-05 20:22:10 +0900

    edit: updated outputviewtest

--- src/test/java/carrace/view/OutputViewTest.java
+++ src/test/java/carrace/view/OutputViewTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class OutputViewTest {
@@ -17,5 +18,15 @@ public class OutputViewTest {
 
         assertThatThrownBy(() -> outputView.printWinner(inputWinner))
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("포지션 숫자에 따라 포지션이 정확히 출력되어야합니다.")
+    @Test
+    public void calculateLatestPosition_ValidPosition() {
+        OutputView outputView = new OutputView();
+        String expected = "----";
+        String actual = outputView.calculateLatestPosition(4);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
