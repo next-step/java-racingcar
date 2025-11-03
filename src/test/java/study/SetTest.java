18d4cc1286c6df3a2e2d773cb52e38ecad44dc69
Seungin Yang <rhfpdk92@naver.com>
2025-11-04 08:53:42 +0900

    1단계 - 학습테스트 실습 (#6162)
    * docs: 1단계 요구사항 정리
    
    * feat: 요구사항1 구현
    
    * feat: 요구사항2 구현
    
    * feat: StringTest 요구사항3 구현
    
    * feat: SetTest 요구사항1 구현
    
    * feat: SetTest 요구사항2 구현
    
    * feat: SetTest 요구사항3 구현

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,49 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setup() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size()로 Set의 크기를 확인한다.")
+    void size() {
+        assertThat(numbers.size()).isNotEqualTo(4);
+        assertThat(numbers.size()).isEqualTo(3);
+        assertThat(numbers).containsExactly(1, 2, 3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains()로 요소가 존재하는지 확인한다.")
+    void contains(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("입력값에 따라 결과가 다른 경우를 테스트한다.")
+    void contains2(int input, boolean expected) {
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+
+}
---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,68 @@
+package study;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("문자열을 \",\"로 split했을 때 잘 분리되는지 확인한다.")
+    void split() {
+        String[] result = "1,2".split(",");
+
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("포함하지 않는 문자열로 split했을 때를 확인한다.")
+    void split2() {
+        String[] result = "1".split(",");
+
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1");
+        Assertions.assertArrayEquals(new String[]{"1"}, result);
+    }
+
+    @Test
+    @DisplayName("subString을 활용하여 괄호를 제거한다.")
+    void substring() {
+        String str = "(1,2)";
+        String substring = str.substring(1, str.length() - 1);
+
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("chatAt()으로 특정 위치의 문자를 가져온다.")
+    void charAt() {
+        String str = "abc";
+        char a = str.charAt(0);
+        char b = str.charAt(1);
+        char c = str.charAt(2);
+
+        assertThat(a).isEqualTo('a');
+        assertThat(b).isEqualTo('b');
+        assertThat(c).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("charAt()으로 범위를 벗어난 위치에 접근하면 예외가 발생한다.")
+    void charAt2() {
+        String str = "abc";
+
+        assertThatThrownBy(() -> str.charAt(3))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("Index 3 out of bounds for length 3");
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> str.charAt(3))
+                .withMessageMatching("Index \\d+ out of bounds for length \\d+");
+    }
+}
