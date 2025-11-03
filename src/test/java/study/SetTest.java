33600409bce700c6b7493797259425d12092e2ab
ho-jun97 <hojun9707@naver.com>
2025-11-04 00:25:18 +0900

    test: contains true/false 테스트(@CsvSource 이용하여 false check)

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -34,5 +35,11 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void contains(int value) {
         assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsByCsvSource(int value, boolean expected) {
+        assertThat(numbers.contains(value)).isEqualTo(expected);
     }
 }
