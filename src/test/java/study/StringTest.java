d8d896638600b531c3026edd9350853f19b6d0b1
KJH <rlawogud2075@naver.com>
2025-11-03 20:15:47 +0900

    split study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.*;
 
+
 public class StringTest {
     @Test
     void Split(){
