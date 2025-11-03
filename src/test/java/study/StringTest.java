896458454dc08c7da3bd0f2bb8e5d1fa10c7be3e
ho-jun97 <hojun9707@naver.com>
2025-11-03 21:51:48 +0900

    substring study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -11,4 +11,12 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
+
+    @Test
+    void substring() {
+        String data = "(1,2)";
+
+        String result = data.substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
 }
