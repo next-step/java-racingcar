b1690e8c7d76f95a7febc561540b43aa76b545f6
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-11-03 23:13:11 +0900

    charAt study

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -8,14 +9,28 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
 
     @Test
-    void split(){
+    void split() {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
 
     @Test
-    void substring(){
+    void substring() {
         String result = "(1,2)".substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt")
+    void charAt() {
+        String data = "abc";
+
+        assertThatThrownBy(() -> data.charAt(3))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("Index 3 out of bounds for length 3");
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> data.charAt(3))
+                .withMessageMatching("Index \\d+ out of bounds for length \\d+");
     }
 }
