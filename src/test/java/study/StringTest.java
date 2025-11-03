04284e0a44fe608b259330f02b0ce260ec7592d4
ho-jun97 <hojun9707@naver.com>
2025-11-03 22:26:16 +0900

    test: chatAt 범위 밖일 때의 예외 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,9 +1,11 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -18,5 +20,16 @@ public class StringTest {
 
         String result = data.substring(1, 4);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("data의 범위 밖일 경우 StringIndexOutOfBoundsException 확인하는 테스트")
+    void chatAt() {
+        String data = "abc";
+
+        assertThatThrownBy(() -> {
+            char c = data.charAt(data.length());
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("out of bounds for length");
     }
 }
