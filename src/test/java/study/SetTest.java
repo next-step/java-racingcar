5b10141e653c3fba21b03b61ef5b233735d726c0
ho-jun97 <hojun9707@naver.com>
2025-11-04 00:05:46 +0900

    test: contains 값 존재 테스트(@ParamerizedTest 이용하여 중복제거)

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,9 +3,13 @@ package study;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -23,6 +27,12 @@ public class SetTest {
     void setSizeCheck() {
         int size = numbers.size();
 
-        Assertions.assertThat(size).isEqualTo(3);
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int value) {
+        assertThat(numbers.contains(value)).isTrue();
     }
 }
