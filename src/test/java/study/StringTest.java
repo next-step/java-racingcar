936b339e400e6fcad663dff909e16d06ce1b2a8a
ghtjr410 <ghtjr410@gmail.com>
2025-11-04 22:22:34 +0900

    test: String 클래스의 substring 메서드 동작 검증 테스트 추가
    StringTest.java
    - split 관련 테스트 메서드명 수정
    - substring을 이용해 괄호를 제거하고 내부 문자열만 반환하는 테스트 `removesParentheses` 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,7 +10,7 @@ public class StringTest {
 
     @DisplayName("쉼표를 구분자로 split했을 때 올바르게 분리된다")
     @Test
-    void splitWithComma() {
+    void splitByComma() {
         String[] result = "1,2".split(",");
 
         assertThat(result).containsExactly("1", "2");
@@ -18,9 +18,19 @@ public class StringTest {
 
     @DisplayName("구분자가 없는 문자열을 split했을 때 하나의 요소만 반환한다")
     @Test
-    void splitWithoutDelimiter() {
+    void splitWithoutComma() {
         String[] result = "1".split(",");
 
         assertThat(result).containsExactly("1");
+    }
+
+    @DisplayName("substring으로 괄호를 제거하면 내부 문자열만 반환한다")
+    @Test
+    void removesParentheses() {
+        String input = "(1,2)";
+
+        String result = input.substring(1, input.length() -1);
+
+        assertThat(result).isEqualTo("1,2");
     }
 }
