6b0d89315fd4db3b9c267e40cdd02e67015e3872
yangseung-in <rhfpdk92@naver.com>
2025-11-05 00:49:15 +0900

    feat: 빈 문자열 또는 null 값을 입력할 경우 0을 반환

---
+++ src/main/java/study/step2/StringAddCalculator.java
@@ -0,0 +1,13 @@
+package study.step2;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+
+        return 0;
+    }
+}
