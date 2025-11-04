e5053e58545c61aed9ae8b9396995111509332d6
suco <qwer920414@gmail.com>
2025-11-05 08:03:37 +0900

    첫 리뷰요청 드립니다. (#6165)
    * StringTest
    
    * SetTest

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] result = "1,2".split(",");
+        assertThat(result).contains("1");
+        assertThat(result).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String result = "(1,2)".substring(1, 4);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 사이즈를 벗어나면 StringIndexOutOfBoundsException 발생")
+    void charAt() {
+        assertThatThrownBy(() -> "abc".charAt(3)).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+}
