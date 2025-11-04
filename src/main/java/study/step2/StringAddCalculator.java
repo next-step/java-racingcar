5a74c882c28b6686ae94dcc427359765caf065eb
yangseung-in <rhfpdk92@naver.com>
2025-11-05 00:54:03 +0900

    feat: 구분자로 분리하여 숫자의 합을 반환한다.

--- src/main/java/study/step2/StringAddCalculator.java
+++ src/main/java/study/step2/StringAddCalculator.java
@@ -1,13 +1,21 @@
 package study.step2;
 
 public class StringAddCalculator {
+    private static final String DEFAULT_SEPARATOR = ",|:";
 
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
+        String[] numbers = text.split(DEFAULT_SEPARATOR);
 
+        int sum = 0;
+        for (String number : numbers) {
+            if(!number.isEmpty()) {
+                sum += Integer.parseInt(number);
+            }
+        }
 
-        return 0;
+        return sum;
     }
 }
