a0f354389ddced446061048b2ec00f5d142d3931
yangseung-in <rhfpdk92@naver.com>
2025-11-05 01:09:07 +0900

    feat: 커스텀 구분자 구현

--- src/main/java/study/step2/StringAddCalculator.java
+++ src/main/java/study/step2/StringAddCalculator.java
@@ -1,17 +1,30 @@
 package study.step2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     private static final String DEFAULT_SEPARATOR = ",|:";
+    private static final String CUSTOM_SEPARATOR_REGEX = "//(.)\n(.*)";
 
     public static int splitAndSum(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
-        String[] numbers = text.split(DEFAULT_SEPARATOR);
+        String separator = DEFAULT_SEPARATOR;
+        String numberText = text;
+
+        Matcher m = Pattern.compile(CUSTOM_SEPARATOR_REGEX).matcher(text);
+        if (m.find()) {
+            separator = m.group(1);
+            numberText = m.group(2);
+        }
+
+        String[] numbers = numberText.split(separator);
 
         int sum = 0;
         for (String number : numbers) {
-            if(!number.isEmpty()) {
+            if (!number.isEmpty()) {
                 sum += Integer.parseInt(number);
             }
         }
