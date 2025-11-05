4e8d0ca21920154bd71ed37ad80e6b95674682d9
Junghoon Kim <59908721+kay019@users.noreply.github.com>
2025-11-06 06:04:58 +0900

    split study test

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,15 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    void spilt(){
+        String[] data = "1,2".split(",");
+
+        assertThat(data).contains("1");
+        assertThat(data).containsExactly("1", "2");
+    }
+}
