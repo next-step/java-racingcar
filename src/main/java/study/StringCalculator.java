b66f3fdfb42b34ad660608d45b5a7f5aa4cf17f3
ho-jun97 <hojun9707@naver.com>
2025-11-05 23:35:39 +0900

    feat: 쉼표(,)로 나누는 split와 sum 메소드 구현

--- src/main/java/study/StringCalculator.java
+++ src/main/java/study/StringCalculator.java
@@ -6,4 +6,16 @@ public class StringCalculator {
     public int isEmptyOrNull(String value) {
         return value == null || value.isEmpty() ? 0:-1;
     }
+
+    public String[] split(String s) {
+        return s.split(",");
+    }
+
+    public int sum(String[] splitResult) {
+        int sum = 0;
+        for(String s: splitResult) {
+            sum += Integer.parseInt(s);
+        }
+        return sum;
+    }
 }
