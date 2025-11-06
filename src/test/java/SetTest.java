0b691f319349609d706822130a1be33f313b1dbe
EunJeong Park <50352139+Eundms@users.noreply.github.com>
2025-11-06 21:49:26 +0900

    1단계 - 학습테스트 실습 (#6181)

---
+++ src/test/java/SetTest.java
@@ -0,0 +1,31 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class SetTest {
+  private Set<Integer> numbers;
+  @BeforeEach
+  public void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 3})
+  void _1_2_3을_가진다(int value) {
+    assertThat(numbers.contains(value)).isTrue();
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+  void csvsource로_구성되어있다(int value, boolean expected) {
+    assertThat(numbers.contains(value)).isEqualTo(expected);
+  }
+}
