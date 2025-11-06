c8f8d42cb2b6de9c1d4d07544fa6a88e53a17e79
pobi <pobi@pobi.local>
2025-11-06 09:55:47 +0900

    implment StringCalculator production code

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,4 +1,36 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+    public static int splitAndSum(String text) {
+        int result;
+        if (text == null || text.isEmpty()) {
+            result = 0;
+        } else {
+            Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+            if (m.find()) {
+                String customDelimiter = m.group(1);
+                String[] values = m.group(2).split(customDelimiter);
+                result = sum(values);
+            } else {
+                String[] values = text.split(",|:");
+                result = sum(values);
+            }
+        }
+        return result;
+    }
+
+    private static int sum(String[] values) {
+        int total = 0;
+        for (String value : values) {
+            int number = Integer.parseInt(value);
+            if (number < 0) {
+                throw new RuntimeException("음수를 입력할 수 없습니다.");
+            }
+            total += number;
+        }
+        return total;
+    }
 }
