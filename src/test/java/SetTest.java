7df9cadb6a248ac3b25a0b7e4e75551f04a2f1e0
eunjeong Park <eunjeongpark.eundms@gmail.com>
2025-11-06 23:18:58 +0900

    2단계 - 미션 1차 구현

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -3,6 +3,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -20,12 +21,17 @@ public class SetTest {
   @ParameterizedTest
   @ValueSource(ints = {1, 2, 3})
   void _1_2_3을_가진다(int value) {
-    assertThat(numbers.contains(value)).isTrue();
+    assertThat(numbers).contains(value);
   }
 
   @ParameterizedTest
   @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
   void csvsource로_구성되어있다(int value, boolean expected) {
     assertThat(numbers.contains(value)).isEqualTo(expected);
+  }
+
+  @Test
+  void check_set_size(){
+    assertThat(numbers.size()).isEqualTo(3);
   }
 }
--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -3,37 +3,57 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringAddCalculatorTest {
 
-  @Test
-  public void split_쉼표구분자() throws Exception {
-
-    String[] result = "1,2".split(",");
-    assertThat(result).contains("1","2");
+  @ParameterizedTest
+  @ValueSource(strings = {"1,2", "1:1,1", "1:2"})
+  void splitAndSum_구분자(String input) throws Exception {
+    int result = StringAddCalculator.splitAndSum(input);
+    assertThat(result).isEqualTo(3);
   }
 
   @Test
-  public void split_숫자하나() throws Exception {
-    String[] result = "1".split(",");
-    assertThat(result).contains("1");
+  void splitAndSum_커스텀구분자(){
+    int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+    assertThat(result).isEqualTo(6);
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void empty_or_null_then_return_0(String input) throws Exception {
+    assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(0);
   }
 
   @Test
-  public void clear_괄호() throws Exception {
+  public void splitAndSum_음수() throws Exception {
+    assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+        .isInstanceOf(RuntimeException.class);
+  }
+
+  @Test
+  void splitAndSum_숫자하나() throws Exception {
+    int result = StringAddCalculator.splitAndSum("1");
+    assertThat(result).isEqualTo(1);
+  }
+
+  @Test
+  void input검증_괄호() throws Exception {
     String clear = "(1,2)".substring(1, 4);
     assertThat(clear).isEqualTo("1,2");
   }
 
   @Test
-  public void find_특정위치문자() throws Exception {
-    String find = "(1,2)";
-    assertThat(find.charAt(1)).isEqualTo('1');
+  void find_특정위치문자() throws Exception {
+    assertThat("(1,2)".charAt(1)).isEqualTo('1');
   }
 
   @Test
   @DisplayName("특정 위치 문자 삭제")
-  public void find_특정위치문자_throw_exception() throws Exception {
+  void find_특정위치문자_throw_exception() throws Exception {
     String find = "(1,2)";
     assertThatThrownBy(() -> {
       find.charAt(find.length());
