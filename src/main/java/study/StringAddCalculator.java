3b67322a6a0b6d168cf18f4aee14b56339b9c05e
모재영 <mo@utransfer.com>
2025-11-07 20:59:12 +0900

    refactor : 쉼표구분자 테스트 리팩토링 1. 문자열 분리 메서드 분리 2. 문자열 배열을 정수 배열로 분로 변환 분리 3. adder() 메서드 분리하여 연산 분리

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -3,19 +3,28 @@ package study;
 public class StringAddCalculator {
     
     public static int splitAndSum(String text) {
-        if(isNullOrEmpty(text)) {
-            return 0;
-        }
+        return isNullOrEmpty(text) ? 0 : adder(stringsToInts(splitText(text)));
+    }
+    
+    private static int adder(int[] nums) {
         int result = 0;
-        String[] textNums = text.split(",");
-        for(String textNum: textNums) {
-            result += toInt(textNum);
+        for(int num: nums) {
+            result += num;
         }
         return result;
     }
     
-    private static int toInt(String text) {
-        return Integer.parseInt(text);
+    private static String[] splitText(String text) {
+        return text.split(",");
+    }
+    
+    private static int[] stringsToInts(String[] texts) {
+        int length = texts.length;
+        int[] ints = new int[length];
+        for(int i = 0; i < length; i++) {
+            ints[i] = Integer.parseInt(texts[i]);
+        }
+        return ints;
     }
     
     private static boolean isNullOrEmpty(String text) {
