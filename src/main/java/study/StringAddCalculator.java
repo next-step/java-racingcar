dd6f87fdb578786073b4076772d89fb8877a9d0a
모재영 <mo@utransfer.com>
2025-11-07 20:40:56 +0900

    feat : 쉼표구분자 테스트 통과 기능 개발 완료

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -3,7 +3,15 @@ package study;
 public class StringAddCalculator {
     
     public static int splitAndSum(String text) {
-        return isNullOrEmpty(text) ? 0 : toInt(text);
+        if(isNullOrEmpty(text)) {
+            return 0;
+        }
+        int result = 0;
+        String[] textNums = text.split(",");
+        for(String textNum: textNums) {
+            result += toInt(textNum);
+        }
+        return result;
     }
     
     private static int toInt(String text) {
