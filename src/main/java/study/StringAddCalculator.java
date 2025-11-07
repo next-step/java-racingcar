95d2860c8fc73fc142e6108e37631b9b422d6749
모재영 <mo@utransfer.com>
2025-11-07 20:04:26 +0900

    refactor 1. 상수 리팩토링 2. String 반환이므로 메소드명 isExist 를 defaultIfEmpty 로 메소드명 변경 3. 숫자만 단독으로 입력시  split() 메소드로 그대로 return 되어서 splitText 메소드 내 불필요한 NORMAL_REGEX 분기 제거

--- src/main/java/study/StringAddCalculator.java
+++ src/main/java/study/StringAddCalculator.java
@@ -4,34 +4,34 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-
+    
+    public static final String CUSTOM_REGEX = "//(.)\n(.*)";
+    public static final String NORMAL_REGEX = ",|:";
+    
     public static int splitAndSum(String text) {
         int result = 0;
-        for (String s : splitText(isExist(text))) {
+        for (String s : splitText(defaultIfEmpty(text))) {
             result += parseToInt(s);
         }
         return result;
     }
 
-    private static String isExist(String text) {
-        return (text == null || text.isEmpty()) == true ? "0" : text;
+    private static String defaultIfEmpty(String text) {
+        return (text == null || text.isEmpty()) ? "0" : text;
     }
 
     private static String[] splitText(String text) {
-        if (text.contains(",") || (text.contains(":"))) {
-            return text.split(",|:");
-        }
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = Pattern.compile(CUSTOM_REGEX).matcher(text);
         if (m.find()) {
             return m.group(2).split(m.group(1));
         }
-        return new String[]{text};
+        return text.split(NORMAL_REGEX);
     }
 
     private static int parseToInt(String token) {
         int anInt = Integer.parseInt(token);
         if (anInt < 0) {
-            throw new RuntimeException();
+            throw new RuntimeException("음수가 나오면 안됩니다.");
         }
         return anInt;
     }
