680a4a64c951c5f5bdffdff1596306ee637add2c
username0w <163955522+username0w@users.noreply.github.com>
2025-11-07 17:37:00 +0900

    feat: 자동차 생성 기능 추가

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,5 @@
+package racingcar;
+
+public class Car {
+
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @DisplayName("자동차가 생성된다")
+    @Test
+    void createCar() {
+        Car car = new Car();
+        assertThat(car).isNotNull();
+    }
+}
