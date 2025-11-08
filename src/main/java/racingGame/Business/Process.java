bc60403f118e52b6562946c4f8374b63f7b4b341
모재영 <mo@utransfer.com>
2025-11-08 20:58:43 +0900

    feat : 이동횟수 테스트 통과 및 리패터링 완료

--- src/main/java/racingGame/Business/Process.java
+++ src/main/java/racingGame/Business/Process.java
@@ -8,12 +8,24 @@ public class Process {
     private ArrayList<Car> cars;
     
     public void run(int cars, int moves){
+        init(cars, moves);
+        
+    }
+    
+    private void init(int cars, int moves) {
         generateCar(cars);
+        setMoveCount(moves);
     }
     
     private void generateCar(int num){
         if(num < 1) {
             throw new IllegalArgumentException("참가시킬 자동차 수 입력이 생략되거나, 0이하");
+        }
+    }
+    
+    private void setMoveCount(int moves) {
+        if(moves < 1) {
+            throw new IllegalArgumentException("이동횟수에 입력이 생략되거나, 0이하");
         }
     }
 }
