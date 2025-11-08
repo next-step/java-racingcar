61861ce38648bdb7d6aa081f784b81e505d831fb
모재영 <mo@utransfer.com>
2025-11-08 18:59:02 +0900

    feat : car model 개발

--- src/main/java/racingGame/model/Car.java
+++ src/main/java/racingGame/model/Car.java
@@ -1,5 +1,21 @@
 package racingGame.model;
 
 public class Car {
-
+    
+    private int location;
+    
+    public Car() {
+    }
+    
+    public void init() {
+        this.location = 0;
+    }
+    
+    public void move() {
+        location++;
+    }
+    
+    public int findLocation() {
+        return location;
+    }
 }
