0824351011af062ef3faa150fdc0c5afaed821c8
모재영 <mo@utransfer.com>
2025-11-08 18:50:10 +0900

    feat : 랜덤 유틸 구현 및 리팩터링 완료

--- src/main/java/racingGame/util/RandomUtil.java
+++ src/main/java/racingGame/util/RandomUtil.java
@@ -1,6 +1,17 @@
 package racingGame.util;
 
+import java.util.Random;
+
 public class RandomUtil {
-
-
+    
+    public static final int RANDOM_BOUND = 10;
+    
+    public static int generateInt() {
+        return getRandom().nextInt(RANDOM_BOUND);
+    }
+    
+    private static Random getRandom() {
+        return new Random();
+    }
+    
 }
