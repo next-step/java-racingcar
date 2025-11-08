de396e266980b103683f19ee4b154b99ab5f89a4
모재영 <mo@utransfer.com>
2025-11-08 20:21:24 +0900

    feat : 참가시킬 자동차 수 예외처리 개발

--- src/main/java/racingGame/Business/Process.java
+++ src/main/java/racingGame/Business/Process.java
@@ -1,5 +1,19 @@
 package racingGame.Business;
 
-public class Process {
+import java.util.ArrayList;
+import racingGame.model.Car;
 
+public class Process {
+    
+    private ArrayList<Car> cars;
+    
+    public void run(int cars, int moves){
+        generateCar(cars);
+    }
+    
+    private void generateCar(int num){
+        if(num < 1) {
+            throw new IllegalArgumentException("참가시킬 자동차 수 입력이 생략되거나, 0이하");
+        }
+    }
 }
