961f4509a03df49c9ecb7dc900c8f953fef7ca25
모재영 <mo@utransfer.com>
2025-11-08 20:16:07 +0900

    test : 테스트 수정하여 run() 에 대한 예외를 받을 수있게 변경 완료

--- src/test/java/racingGame/Business/ProcessTest.java
+++ src/test/java/racingGame/Business/ProcessTest.java
@@ -17,58 +17,56 @@ class ProcessTest {
     
     @Test
     void carGenerateResultNullTest() {
-        gp.run(-1,1);
         assertThatThrownBy(() -> {
-            gp.generateCar(-1);
+            gp.run(-1,1);
         }).isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("참가시킬 자동차 수가 없습니다.");
+            .hasMessage("%s","참가시킬 자동차 수가 음수");
     }
 
-//    @Test
-//    void gameTurnSetNullTest() {
-//        gp.run(1,-1);
-//        assertThatThrownBy(() -> {
-//            gp.generateCar(-1);
-//        }).isInstanceOf(IllegalArgumentException.class)
-//            .hasMessage("몇번의 게임을 할지 값을 입력해주세요 ");
-//    }
-//
-//    @Test
-//    void carGenerateResultTest() {
-//        gp.run(3,1);
-//        assertThat(gp.joinCars().size).isEqual(3);
-//    }
-//
-//    @Test
-//    void gameNextTurnTest() {
-//        gp.run(1,2);
-//        assertThat(gp.nowTurn()).isEqual(2);
-//    }
-//
-//    @Test
-//    void carForwardTest() {
-//        gp.run(1, 10);
-//        while(true) {
-//            gp.nextTurn();
-//            if(gp.joinCars.get(0).loaction > 0) {
-//                break;
-//            }
-//        }
-//
-//        assertThat(gp.joinCars.get(0).loaction).isNotEqual(0);
-//    }
-//
-//    @Test
-//    void carsForwardTest() {
-//        gp.run(3, 10);
-//        while(true) {
-//            gp.nextTurn();
-//            if(gp.joinCars.get(0).loaction > 2) {
-//                break;
-//            }
-//        }
-//        for(Car car : gp.joinCars){
-//            assertThat(car.loaction).isNotEqual(0);
-//        }
-//    }
+    @Test
+    void gameTurnSetNullTest() {
+        assertThatThrownBy(() -> {
+            gp.run(1,-1);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("몇번의 게임을 할지 값을 입력해주세요 ");
+    }
+
+    @Test
+    void carGenerateResultTest() {
+        gp.run(3,1);
+        assertThat(gp.joinCars().size).isEqual(3);
+    }
+
+    @Test
+    void gameNextTurnTest() {
+        gp.run(1,2);
+        assertThat(gp.nowTurn()).isEqual(2);
+    }
+
+    @Test
+    void carForwardTest() {
+        gp.run(1, 10);
+        while(true) {
+            gp.nextTurn();
+            if(gp.joinCars.get(0).loaction > 0) {
+                break;
+            }
+        }
+
+        assertThat(gp.joinCars.get(0).loaction).isNotEqual(0);
+    }
+
+    @Test
+    void carsForwardTest() {
+        gp.run(3, 10);
+        while(true) {
+            gp.nextTurn();
+            if(gp.joinCars.get(0).loaction > 2) {
+                break;
+            }
+        }
+        for(Car car : gp.joinCars){
+            assertThat(car.loaction).isNotEqual(0);
+        }
+    }
 }
