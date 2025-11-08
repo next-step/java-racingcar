e3c5dc8ad1ab6f6328ad2077a0b0c78b08e7ff89
모재영 <mo@utransfer.com>
2025-11-08 19:56:08 +0900

    test : 불명확한 기능 및 실행 흐름 다시 재정의하여 테스트 완료

--- src/test/java/racingGame/GameProcessTest.java
+++ src/test/java/racingGame/GameProcessTest.java
@@ -1,79 +1,75 @@
 package racingGame;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIterable;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
+import racingGame.model.Car;
 
 class GameProcessTest {
     
+    Gameprocess gp;
+    
+    @BeforeEach
+    void setUp() {
+        gp = new Gameprocess();
+    }
+    
     @Test
     void carGenerateResultNullTest() {
-        Gameprocess gp = new Gameprocess();
+        gp.run(-1,1);
         assertThatThrownBy(() -> {
             gp.generateCar(-1);
-        }).isInstanceOf(RuntimeException.class)
+        }).isInstanceOf(IllegalArgumentException.class)
             .hasMessage("참가시킬 자동차 수가 없습니다.");
     }
-    
+
     @Test
     void gameTurnSetNullTest() {
-        Gameprocess gp = new Gameprocess();
+        gp.run(1,-1);
         assertThatThrownBy(() -> {
-            gp.setGameTurn(-1);
-        }).isInstanceOf(RuntimeException.class)
+            gp.generateCar(-1);
+        }).isInstanceOf(IllegalArgumentException.class)
             .hasMessage("몇번의 게임을 할지 값을 입력해주세요 ");
     }
-    
+
     @Test
     void carGenerateResultTest() {
-        Gameprocess gp = new Gameprocess();
-        gp.generateCar(3);
+        gp.run(3,1);
         assertThat(gp.joinCars().size).isEqual(3);
     }
-    
+
     @Test
     void gameNextTurnTest() {
-        Gameprocess gp = new Gameprocess();
-        gp.generateCar(1);
-        gp.nextTurn();
-        gp.nextTurn();
+        gp.run(1,2);
         assertThat(gp.nowTurn()).isEqual(2);
     }
-    
+
     @Test
     void carForwardTest() {
-        Gameprocess gp = new Gameprocess();
-        gp.generateCar(1);
+        gp.run(1, 10);
         while(true) {
             gp.nextTurn();
             if(gp.joinCars.get(0).loaction > 0) {
                 break;
             }
         }
-        
+
         assertThat(gp.joinCars.get(0).loaction).isNotEqual(0);
     }
-    
+
     @Test
     void carsForwardTest() {
-        Gameprocess gp = new Gameprocess();
-        gp.generateCar(3);
+        gp.run(3, 10);
         while(true) {
             gp.nextTurn();
             if(gp.joinCars.get(0).loaction > 2) {
                 break;
             }
         }
-        for(car : gp.joinCars){
+        for(Car car : gp.joinCars){
             assertThat(car.loaction).isNotEqual(0);
         }
-
     }
-
 }
