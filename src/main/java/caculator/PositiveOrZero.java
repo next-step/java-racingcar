9f2c373d7d077656790d35c946a037dfe4963834
ghtjr410 <ghtjr410@gmail.com>
2025-11-08 17:09:45 +0900

    refactor: 검증 메서드에서 파라미터 제거, this.value로 직접 접근

--- src/main/java/caculator/PositiveOrZero.java
+++ src/main/java/caculator/PositiveOrZero.java
@@ -4,22 +4,23 @@ import java.util.Objects;
 
 public record PositiveOrZero(int value) {
 
-    public PositiveOrZero {
-        validateInvariant(value);
+    public PositiveOrZero(int value) {
+        this.value = value;
+        validateInvariant();
     }
 
     public PositiveOrZero(String text) {
         this(Integer.parseInt(text));
     }
 
-    private void validateInvariant(int value) {
-        if (isNegative(value)) {
-            throw new IllegalArgumentException("음수는 입력할 수 없습니다: " + value);
+    private void validateInvariant() {
+        if (isNegative()) {
+            throw new IllegalArgumentException("음수는 입력할 수 없습니다: " + this.value);
         }
     }
 
-    private boolean isNegative(int number) {
-        return number < 0;
+    private boolean isNegative() {
+        return this.value < 0;
     }
 
     public PositiveOrZero add(PositiveOrZero other) {
