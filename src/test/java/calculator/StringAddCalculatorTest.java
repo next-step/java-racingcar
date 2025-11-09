4b6242a495c67dd1a5499d6f2f52e5cfb7a65dac
eunjeong Park <eunjeongpark.eundms@gmail.com>
2025-11-09 17:35:34 +0900

    클래스별 테스트 분리

--- src/test/java/calculator/StringAddCalculatorTest.java
+++ src/test/java/calculator/StringAddCalculatorTest.java
@@ -7,39 +7,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringAddCalculatorTest {
-
-  @ParameterizedTest
-  @ValueSource(strings = {"1,2", "1:1,1", "1:2"})
-  void splitAndSum_구분자(String input) throws Exception {
-    int result = StringAddCalculator.splitAndSum(input);
-    assertThat(result).isEqualTo(3);
-  }
-
-  @Test
-  void splitAndSum_커스텀구분자(){
-    int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
-    assertThat(result).isEqualTo(6);
-  }
 
   @ParameterizedTest
   @NullAndEmptySource
   void empty_or_null_then_return_0(String input) throws Exception {
     assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(0);
-  }
-
-  @Test
-  public void splitAndSum_음수() throws Exception {
-    assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
-        .isInstanceOf(RuntimeException.class);
-  }
-
-  @Test
-  void splitAndSum_숫자하나() throws Exception {
-    int result = StringAddCalculator.splitAndSum("1");
-    assertThat(result).isEqualTo(1);
   }
 
   @Test
