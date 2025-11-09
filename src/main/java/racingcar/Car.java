f23c8909df7e12611739a3023605220140ee8143
ghtjr410 <ghtjr410@gmail.com>
2025-11-09 13:16:41 +0900

    refactor: Car 클래스 상수 및 전진 로직 리팩터링
    Car.java
    - 매직 넘버를 의미 있는 상수로 치환
    - 전진 조건 및 유효성 검증 로직을 메서드로 분리
    - 가독성과 유지보수성을 높이기 위한 구조 개선

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,6 +2,10 @@ package racingcar;
 
 public class Car {
     private static final int DEFAULT_DISTANCE = 0;
+    private static final int MIN_RANDOM_VALUE = 0;
+    private static final int MAX_RANDOM_VALUE = 9;
+    private static final int FORWARD_THRESHOLD = 4;
+    private static final int MOVE_DISTANCE = 1;
 
     private int distance;
 
@@ -9,14 +13,26 @@ public class Car {
         this.distance = DEFAULT_DISTANCE;
     }
 
-    public void move(int value) {
-        if (value < 0 || value > 9) {
+    public void move(int randomValue) {
+        validateRandomValue(randomValue);
+
+        if (shouldMoveForward(randomValue)) {
+            this.distance += MOVE_DISTANCE;
+        }
+    }
+
+    private void validateRandomValue(int randomValue) {
+        if (isOutOfRange(randomValue)) {
             throw new IllegalArgumentException("랜덤 값은 0 이상 9 이하이어야 합니다.");
         }
+    }
 
-        if (value >= 4) {
-            this.distance += 1;
-        }
+    private boolean isOutOfRange(int randomValue) {
+        return randomValue < MIN_RANDOM_VALUE || randomValue > MAX_RANDOM_VALUE;
+    }
+
+    private boolean shouldMoveForward(int randomValue) {
+        return randomValue >= FORWARD_THRESHOLD;
     }
 
     public int getDistance() {
