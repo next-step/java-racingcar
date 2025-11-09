d373c3115f73c3e428fa6f32615752b39802b76c
모재영 <mo@utransfer.com>
2025-11-09 20:09:15 +0900

    refactor : 1. isProceedByRandom() 메서드 조건 단순화 2. car 생성 시 기본 시작 위치 상수화

--- src/main/java/racingGame/model/Car.java
+++ src/main/java/racingGame/model/Car.java
@@ -2,10 +2,11 @@ package racingGame.model;
 
 public class Car {
     
+    public static final int INIT_LOCATION = 1;
     private int location;
     
     public Car() {
-        this.location = 1;
+        this.location = INIT_LOCATION;
     }
     
     public void move() {
