f42ca098c07b23bef4510fa12bad25b93af85833
yangseung-in <rhfpdk92@naver.com>
2025-11-09 18:54:27 +0900

    feat: 게임 intput, output 구현

---
+++ src/main/java/study/racing/GameOutput.java
@@ -0,0 +1,14 @@
+package study.racing;
+
+public class GameOutput {
+
+    public void printResult(){
+        System.out.println("실행 결과");
+
+    }
+    public void printRound(){
+        //TODO 추후 구현
+        System.out.println("-");
+
+    }
+}
