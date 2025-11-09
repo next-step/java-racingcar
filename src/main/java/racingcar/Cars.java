540369e5b2977082cc69c9b341f4c2535dd9bec6
ghtjr410 <ghtjr410@gmail.com>
2025-11-09 13:58:56 +0900

    feat: 자동차 그룹 전체 이동 기능 및 랜덤값 생성 전략 추가
    Cars.java
    - moveAll(RandomNumber) 메서드로 모든 자동차를 한 번에 이동시키는 기능 추가
    - 각 자동차의 이동 결과를 조회할 수 있는 getDistances() 메서드 추가
    
    RandomNumber.java
    - 랜덤값 생성을 위한 전략 인터페이스 추가 (테스트 및 확장성 고려)
    
    CarsTest.java
    - 모든 자동차가 동시에 이동하는 moveAll 기능 테스트 추가

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -16,7 +16,23 @@ public class Cars {
         this.values = cars;
     }
 
+    public void moveAll(RandomNumber randomNumber) {
+        for (Car car : values) {
+            car.move(randomNumber.generate());
+        }
+    }
+
     public List<Car> getValues() {
         return List.copyOf(this.values);
+    }
+
+    public List<Integer> getDistances() {
+        List<Integer> distances = new ArrayList<>();
+
+        for (Car car : values) {
+            distances.add(car.getDistance());
+        }
+
+        return distances;
     }
 }
--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -11,8 +11,16 @@ class CarsTest {
 
     @Test
     void 생성자_입력한_대수만큼_자동차를_생성한다() {
-        Cars cars = new Cars(3);
+        assertThat(new Cars(3).getValues().size()).isEqualTo(3);
+    }
 
-        assertThat(cars.getValues().size()).isEqualTo(3);
+    @Test
+    void moveAll_모든_자동차가_이동한다() {
+        Cars cars = new Cars(3);
+        RandomNumber randomNumber = () -> 4;
+
+        cars.moveAll(randomNumber);
+
+        assertThat(cars.getDistances()).containsExactly(1, 1, 1);
     }
 }
