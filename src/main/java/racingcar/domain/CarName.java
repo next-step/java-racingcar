5c8dc33bbeb897aadf3d6f5d088c4e39098eec2e
ghtjr410 <ghtjr410@gmail.com>
2025-11-11 04:58:48 +0900

    feat: 자동차 이름 길이 제한 검증 로직 추가 및 테스트 보완
    CarName.java
    - 자동차 이름이 5자를 초과할 경우 예외를 발생시키는 검증 로직 추가
    
    CarNameTest.java
    - 자동차 이름이 5자를 초과할 때  발생하는지 검증하는 테스트 추가

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -6,5 +6,8 @@ public record CarName(String value) {
         if (value == null || value.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
         }
+        if (value.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 }
--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -18,5 +19,12 @@ class CarNameTest {
         assertThatThrownBy(() -> new CarName(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차 이름은 비어있을 수 없습니다.");
+    }
+
+    @Test
+    void 생성자_5자_초과_예외발생() {
+        assertThatThrownBy(() -> new CarName("일이삼사오육"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자를 초과할 수 없습니다.");
     }
 }
