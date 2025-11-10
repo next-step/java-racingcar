09d3132f8f350543e50cce659378f68040ee0ed6
ho-jun97 <hojun9707@naver.com>
2025-11-10 22:16:43 +0900

    feat: 자동차 객체 처음포지션은 0

---
+++ src/main/java/racingCar/Car.java
@@ -0,0 +1,13 @@
+package racingCar;
+
+public class Car {
+    private final int position;
+
+    public Car() {
+        this.position = 0;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
