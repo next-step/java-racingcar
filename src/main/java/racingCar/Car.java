14950fed48b35956c14bd2d67657edfc37cb58a4
ho-jun97 <hojun9707@naver.com>
2025-11-10 22:59:57 +0900

    feat: 4이상일 경우 전진 구현

--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -1,13 +1,25 @@
 package racingCar;
 
+import java.util.Random;
+
 public class Car {
-    private final int position;
+    private int position;
 
     public Car() {
         this.position = 0;
     }
 
     public int getPosition() {
-        return position;
+        return this.position;
+    }
+
+    public void move(int dis) {
+        if (checkDis(dis)) {
+            this.position += dis;
+        }
+    }
+
+    private boolean checkDis(int dis) {
+        return dis >= 4;
     }
 }
