2156a09fe46d0b9ba7441c1c5ad6e38bed22319b
HEEJIN JEON <wjsgmlwls97@gmail.com>
2025-11-12 05:21:18 +0900

    2단계 - 문자열 덧셈 계산기 (#6199)
    * refactor: 1단계 - 학습 테스트 피드백 반영
    
    * test: 2단계 - 문자열 덧셈 계산기
    
    * refactor: formatResult 제거
    
    * refactor: 불필요한 주석 제거
    
    * refactor: 변수명 수정
    
    * refactor: 피드백 반영 - calculate() 단일 public 메서드로 캡슐화 및 테스트 코드 수정
    
    - calculate() 메서드만 public으로 노출, 나머지 메서드는 private으로 캡슐화
    - 개별 메서드 테스트를 제거하고 calculate() 메서드 통합 테스트로 변경
    - step2 패키지를 calculator로 이름 변경

---
+++ src/main/java/calculator/Delimiter.java
@@ -0,0 +1,27 @@
+package calculator;
+
+public class Delimiter {
+	private static final String DEFAULT_DELIMITERS = ":,";
+	private final String delimiter;
+
+	private Delimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
+
+	public String getDelimiter() {
+		return delimiter;
+	}
+
+	public String[] split(String input) {
+		return input.split(delimiter);
+	}
+
+	public static Delimiter ofDefault() {
+		return new Delimiter("[" + DEFAULT_DELIMITERS + "]");
+	}
+
+	public static Delimiter ofCustom(String input) {
+		String customDelimiter = input.substring(input.indexOf("//") + 2, input.indexOf("\n"));
+		return new Delimiter("[" + DEFAULT_DELIMITERS + customDelimiter + "]");
+	}
+}
