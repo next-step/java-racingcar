58c72be5c8d3f1ac624bf0a200fb11f5d43dbad2
모재영 <mo@utransfer.com>
2025-11-11 10:24:32 +0900

    chore : CarFactoryTest 에 필요없는 코드 제거

--- src/test/java/racingGame/Business/CarFactoryTest.java
+++ src/test/java/racingGame/Business/CarFactoryTest.java
@@ -1,21 +1,14 @@
 package racingGame.Business;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingGame.model.Car;
 import racingGame.model.Rules;
 
 class CarFactoryTest {
     
     private CarFactory carFactory;
     private Rules rules;
-    private final List<Car> cars = new ArrayList<>();
     
     // 자동차 입력받은만큼 생성했는지?
     @ParameterizedTest
