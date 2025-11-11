a0340d61521b696ee635436f3623d9bc22c66291
JoonKyoLee <joonkyolee@gmail.com>
2025-11-11 11:20:51 +0900

    test: 빈 문자열과 null을 테스트하는 과정에서 @NullAndEmptySource를 이용하도록 변경

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -3,16 +3,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class StringAddCalculatorTest {
 
-    @Test
-    void 빈_문자열이나_null일_경우에는_0을_반환한다() {
-        int result = StringAddCalculator.splitAndSum(null);
-        assertThat(result).isEqualTo(0);
-
-        result = StringAddCalculator.splitAndSum("");
-        assertThat(result).isEqualTo(0);
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 빈_문자열이나_null일_경우에는_0을_반환한다(String input) {
+        assertThat(StringAddCalculator.splitAndSum(input)).isEqualTo(0);
     }
 
     @ParameterizedTest
