1e4d03d869e9e18df1ace2fd3027b8e8a993a90a
ghtjr410 <ghtjr410@gmail.com>
2025-11-12 12:58:59 +0900

    feat: 랜덤 숫자 값 객체 추가 및 범위 검증 테스트
    RandomNumber.java
    - 0~9 범위의 랜덤 값을 검증하는 값 객체 추가
    
    RandomNumberTest.java
    - 유효/비유효 범위 값에 대한 생성자 검증 테스트 추가

---
+++ src/test/java/racingcar/domain/RandomNumberTest.java
@@ -0,0 +1,26 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class RandomNumberTest {
+
+    @ParameterizedTest(name = "입력값: {0}")
+    @ValueSource(ints = {0, 9})
+    void 생성자_랜덤값이_0_이상_그리고_9_이하이면_정상적으로_생성(int input) {
+        assertThat(new RandomNumber(input).value()).isEqualTo(input);
+    }
+
+    @ParameterizedTest(name = "입력값: {0}")
+    @ValueSource(ints = {-1, 10})
+    void 생성자_랜덤값이_0_미만_또는_9_초과이면_예외발생(int input) {
+        assertThatThrownBy(() -> new RandomNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("랜덤 값은 0 이상 9 이하이어야 합니다.");
+    }
+}
