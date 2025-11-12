17c7797da36ad8e038f77dc6fdbd1b29349dac47
username0w <163955522+username0w@users.noreply.github.com>
2025-11-13 03:53:29 +0900

    refactor: 자동차 이름 구분자 상수화 및 parse 메서드 분리

--- src/main/java/racingcar/RaceGameApplication.java
+++ src/main/java/racingcar/RaceGameApplication.java
@@ -3,20 +3,32 @@ package racingcar;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RaceGameApplication {
+
+    private static final String CAR_NAME_DELIMITER = ",";
 
     public static void main(String[] args) {
 
         String carNames = InputView.readCarNames();
         int gameCount = InputView.readGameCount();
-        RaceGame raceGame = new RaceGame(Arrays.stream(carNames.split(",")).toList(), gameCount);
+        RaceGame raceGame = new RaceGame(parseCarNames(carNames), gameCount);
 
         ResultView.printResultMessage();
         for (int i = 0; i < gameCount; i++) {
             raceGame.playRound();
             ResultView.printRoundResult(raceGame.cars());
         }
+    }
+
+    private static List<String> parseCarNames(String input) {
+        String[] names = input.split(CAR_NAME_DELIMITER);
+        List<String> carNames = new ArrayList<>();
+        for (String name : names) {
+            carNames.add(name.trim());
+        }
+        return carNames;
     }
 }
