224c416a2c1b0c7e61621c92b55c11063afc1342
julee-0430 <yeonju.lee21@gmail.com>
2025-11-14 23:45:17 +0900

    [test] Car 이름 유효성 검증 테스트 추가

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,23 +1,24 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
-    private Car car;
-
-    @BeforeEach
-    void setUp() {
-        car = new Car("testCar");
+    @Test
+    @DisplayName("Car 이름은 5글자 이하여야 한다.")
+    void validCarNameTest() {
+        assertThatThrownBy(() -> new Car("testName"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5글자를 초과할 수 없습니다.");
     }
 
     @ParameterizedTest
     @CsvSource({"0,0", "3,0", "4,1", "9,1"})
     @DisplayName("4 이상의 숫자를 입력하면 1칸, 4 미만의 숫자를 입력하면 0칸 전진한다.")
     void moveTest(int number, int expectedLocation) {
+        final Car car = new Car("pobi");
         car.makeMove(number);
         assertThat(car.currentLocation()).isEqualTo(expectedLocation);
     }
