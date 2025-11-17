0bfc89f109f354ccb83964dd6b9e90781a82be23
JoonKyoLee <joonkyolee@gmail.com>
2025-11-17 23:56:39 +0900

    refactor: List<Integer> 기반 생성자를 추가

--- src/main/java/racing/domain/RoundResult.java
+++ src/main/java/racing/domain/RoundResult.java
@@ -1,6 +1,7 @@
 package racing.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class RoundResult {
@@ -8,15 +9,23 @@ public class RoundResult {
     private final List<Integer> roundResult;
 
     public RoundResult(Cars cars) {
+        this(toList(cars));
+    }
+
+    public RoundResult(List<Integer> roundResult) {
+        this.roundResult = roundResult;
+    }
+
+    private static List<Integer> toList(Cars cars) {
         List<Integer> roundResult = new ArrayList<>();
 
         for (Car car : cars.getCars()) {
             roundResult.add(car.getPosition());
         }
-        this.roundResult = roundResult;
+        return roundResult;
     }
 
     public List<Integer> getRoundResult() {
-        return roundResult;
+        return Collections.unmodifiableList(roundResult);
     }
 }
