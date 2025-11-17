7ac98d69da189d000ec9f2e5adf5646e2a10e092
username0w <163955522+username0w@users.noreply.github.com>
2025-11-17 10:15:48 +0900

    test: Car 객체 생성 시 CarName 객체를 받도록 테스트 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,27 +1,29 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
-    @DisplayName("이름을 가진 자동차가 생성된다")
-    @ParameterizedTest
-    @ValueSource(strings = {"a", "car05"})
-    void createCar(String name) {
-        Car car = new Car(name);
+    @DisplayName("Car 객체는 CarName 객체를 받아 생성된다")
+    @Test
+    void createCarWithCarName() {
+        CarName carName = new CarName("car1");
+        Car car = new Car(carName);
         assertThat(car).isNotNull();
+        assertThat(car.name()).isEqualTo(carName);
+        assertThat(car.position().asInt()).isEqualTo(0);
     }
 
     @DisplayName("값이 4 이상일 때 자돋차가 이동한다")
     @ParameterizedTest
     @CsvSource({"0,0", "3,0", "4,1", "9,1"})
     void canMoveWhenAtLeast(int number, int expected) {
-        Car car = new Car("car1");
+        Car car = new Car(new CarName("car1"));
         car.moveIfPossible(number);
         assertThat(car.position()).isEqualTo(new Position(expected));
     }
