48fcc750e7c9aa9f672ed63ddc9ca84c5069a59a
JoonKyoLee <joonkyolee@gmail.com>
2025-11-17 16:14:17 +0900

    feat: 랜덤 값을 바탕으로 자동차가 전진 또는 정지하도록 구현

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -3,11 +3,22 @@ package racing.domain;
 public class Car {
 
     private static final int DEFAULT_POSITION = 0;
+    private static final int MOVE_THRESHOLD = 4;
 
     private int position;
 
     public Car() {
         this.position = DEFAULT_POSITION;
+    }
+
+    public void move(int number) {
+        if (canMove(number)) {
+            position++;
+        }
+    }
+
+    private boolean canMove(int number) {
+        return number >= MOVE_THRESHOLD;
     }
 
     public int getPosition() {
--- src/test/java/racing/domain/CarTest.java
+++ src/test/java/racing/domain/CarTest.java
@@ -3,6 +3,8 @@ package racing.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
@@ -10,5 +12,23 @@ public class CarTest {
     void 자동차를_생성하면_첫_위치가_0이_된다() {
         Car car = new Car();
         assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3})
+    void 랜덤_값이_0과3_사이의_값일_때는_전진하지_않는다(int randomNumber) {
+        Car car = new Car();
+        car.move(randomNumber);
+
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 9})
+    void 랜덤_값이_4와9_사이의_값일_때는_전진한다(int randomNumber) {
+        Car car = new Car();
+        car.move(randomNumber);
+
+        assertThat(car.getPosition()).isEqualTo(1);
     }
 }
