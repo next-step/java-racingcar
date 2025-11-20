9e10e5e71c0b7c3fc0d66abaab8e19bf56e4b469
pobi <pobi@pobi.local>
2025-11-20 13:00:38 +0900

    NameTest 개선

--- src/test/java/racinggame/domain/NameTest.java
+++ src/test/java/racinggame/domain/NameTest.java
@@ -4,31 +4,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
     @ParameterizedTest
-    @CsvSource({"a", "abced"})
+    @CsvSource({"a", "abcde"})
     @DisplayName("이름이 유효한 경우")
     void validName(String name) {
         assertThat(new Name(name)).isEqualTo(new Name(name));
     }
 
-    @ParameterizedTest
-    @CsvSource({"abcdef", "verylongname"})
+    @Test
     @DisplayName("이름이 5자를 초과하는 경우 예외 발생")
-    void carNameShouldThrowExceptionWhenMoreThan5(String name) {
-        assertThatThrownBy(() -> new Name(name))
+    void carNameShouldThrowExceptionWhenMoreThan5() {
+        assertThatThrownBy(() -> new Name("abcdef"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("5자");
     }
 
-    @Test
-    @DisplayName("이름이 0자인경우 예외 발생")
-    void carNameShouldThrowExceptionWhenLessThan0() {
-        assertThatThrownBy(() -> new Name(""))
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("이름이 null 또는 빈 문자열")
+    void carNameShouldThrowExceptionWhenLessThan0(String name) {
+        assertThatThrownBy(() -> new Name(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("비어");
     }
