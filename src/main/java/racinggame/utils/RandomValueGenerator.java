6c8bc48b357747b4f7fd86e94317547eea1b71ab
pobi <pobi@pobi.local>
2025-11-20 16:50:32 +0900

    extract constant MAX_BOUND

--- src/main/java/racinggame/utils/RandomValueGenerator.java
+++ src/main/java/racinggame/utils/RandomValueGenerator.java
@@ -3,9 +3,11 @@ package racinggame.utils;
 import java.util.Random;
 
 public class RandomValueGenerator implements ValueGenerator {
+    public static final int MAX_BOUND = 10;
+
     @Override
     public int generate() {
         Random random = new Random();
-        return random.nextInt(10);
+        return random.nextInt(MAX_BOUND);
     }
 }
