ac8aed04270452133ee7bbcd4b409610fea18907
ho-jun97 <hojun9707@naver.com>
2025-11-22 14:30:11 +0900

    test: RoundCount 음수 예외처리

--- src/test/java/racingCar/RoundCountTest.java
+++ src/test/java/racingCar/RoundCountTest.java
@@ -13,4 +13,11 @@ public class RoundCountTest {
     void decrease() {
         Assertions.assertThat(new RoundCount(1).decrease()).isEqualTo(new RoundCount());
     }
+
+    @Test
+    void 라운드_수가_음수가_될수없음() {
+        Assertions.assertThatThrownBy(()->new RoundCount(-1))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("음수가 될 수 없습니다.");
+    }
 }
