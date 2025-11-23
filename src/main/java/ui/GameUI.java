911ce5f7fe405ed61b737226b85a8e6d0ac91d91
이국신 <nw4611@naver.com>
2025-11-23 10:50:02 +0900

    [feat] 실행 결과에 이름을 출력하도록 수정

--- src/main/java/ui/GameUI.java
+++ src/main/java/ui/GameUI.java
@@ -7,13 +7,12 @@ import java.util.List;
 public class GameUI {
     public static void printCarsStatus(List<Car> cars) {
         for (Car car : cars) {
-            printCarPosition(car.getPosition());
+            printCarPosition(car.getName(), car.getPosition());
         }
         System.out.println();
     }
 
-    private static void printCarPosition(int position) {
-        System.out.println("-".repeat(position));
+    private static void printCarPosition(String carName, int position) {
+        System.out.printf("%s : %s%n", carName, "-".repeat(position));
     }
-
 }
