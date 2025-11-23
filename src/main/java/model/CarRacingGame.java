45ff16cc342c52987bdd99a3ece68752e7097156
이국신 <nw4611@naver.com>
2025-11-23 23:45:12 +0900

    [feat] 우승자를 찾는 기능 구현

--- src/main/java/model/CarRacingGame.java
+++ src/main/java/model/CarRacingGame.java
@@ -19,4 +19,30 @@ public class CarRacingGame {
             car.tryMoveForward(RandomNumberGenerator.random());
         }
     }
+
+    public List<String> findWinner(List<Car> cars) {
+        int maxPosition = getMaxPosition(cars);
+        List<String> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (isWinner(car.getPosition(), maxPosition)) {
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
+    }
+
+    private boolean isWinner(int position, int maxPosition) {
+        return position == maxPosition;
+    }
+
+    private int getMaxPosition(List<Car> cars) {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            maxPosition = Math.max(car.getPosition(),  maxPosition);
+        }
+
+        return maxPosition;
+    }
 }
