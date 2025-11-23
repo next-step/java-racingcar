ca6ba69433b58faed12dbe1b8eac1fb166c866e9
이국신 <nw4611@naver.com>
2025-11-23 10:44:58 +0900

    [feat] 실행 결과를 구분하는 UI 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,9 @@ import model.CarRacingGame;
 import java.util.Scanner;
 
 public class Main {
-    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n";
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String CAR_MOVE_COUNT_INPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
+    private static final String GAME_RESULT_MESSAGE = "\n실행 결과";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -15,6 +16,7 @@ public class Main {
 
         System.out.println(CAR_MOVE_COUNT_INPUT_MESSAGE);
         int tryCount = scanner.nextInt();
+        System.out.println(GAME_RESULT_MESSAGE);
         carRacingGame.race(tryCount);
     }
 }
