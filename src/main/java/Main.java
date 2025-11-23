86a431cb4f824bc89ce9ca2b5ce692398f187174
이국신 <nw4611@naver.com>
2025-11-23 10:38:47 +0900

    [feat] 자동차 이름으로 입력을 받음에 따라 Main 함수 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,15 @@ import model.CarRacingGame;
 import java.util.Scanner;
 
 public class Main {
-    private static final String CAR_COUNT_INPUT_MESSAGE = "자동차 대수는 몇 대인가요?";
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n";
     private static final String CAR_MOVE_COUNT_INPUT_MESSAGE = "시도할 회수는 몇 회 인가요?";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        System.out.println(CAR_COUNT_INPUT_MESSAGE);
-        int carCount = scanner.nextInt();
-        CarRacingGame carRacingGame = new CarRacingGame(carCount);
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
+        String carNames = scanner.nextLine();
+        CarRacingGame carRacingGame = new CarRacingGame(carNames.split(","));
 
         System.out.println(CAR_MOVE_COUNT_INPUT_MESSAGE);
         int tryCount = scanner.nextInt();
