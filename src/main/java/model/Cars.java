75d93b7c786b1fc14ad544d24c7666080b91dbd3
이국신 <nw4611@naver.com>
2025-11-25 01:17:15 +0900

    [refactor] 일급 컬렉션에서 자동차를 움직이고 우승자를 찾도록 수정

--- src/main/java/model/Cars.java
+++ src/main/java/model/Cars.java
@@ -1,5 +1,7 @@
 package model;
 
+import util.RandomNumberGenerator;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,5 +27,36 @@ public class Cars {
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public void move() {
+        for (Car car : cars) {
+            car.tryMoveForward(RandomNumberGenerator.random());
+        }
+    }
+
+    public int getMaxPosition() {
+        int maxPosition = 0;
+        for (Car car : cars) {
+            maxPosition = Math.max(car.getPosition(),  maxPosition);
+        }
+
+        return maxPosition;
+    }
+
+    public List<String> getWinners(int maxPosition) {
+        List<String> winners = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (isWinner(car.getPosition(), maxPosition)) {
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
+    }
+
+    private boolean isWinner(int position, int maxPosition) {
+        return position == maxPosition;
     }
 }
