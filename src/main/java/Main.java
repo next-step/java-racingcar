a006cc8a3660aabf1a7334b4d4e7b41b0e1b879d
이국신 <nw4611@naver.com>
2025-11-25 01:18:26 +0900

    [refactor] 리팩토링에 따른 Main 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,20 @@
 import model.CarRacingGame;
-import model.Cars;
-import ui.GameUI;
 import ui.InputView;
-
-import java.util.List;
-import java.util.Scanner;
+import ui.ResultView;
 
 public class Main {
 
     public static void main(String[] args) {
         String carNames = InputView.getCarNames();
         int tryCount = InputView.getTryCount();
+        CarRacingGame carRacingGame = new CarRacingGame(tryCount, carNames);
 
-        CarRacingGame carRacingGame = new CarRacingGame();
-        Cars cars = new Cars(carNames);
+        ResultView.printResultStart();
+        while(!carRacingGame.isFinished()) {
+            carRacingGame.race();
+            ResultView.printRacingStatus(carRacingGame.getCars());
+        }
 
-        carRacingGame.race(tryCount, cars.getCars());
-
-        List<String> winnerNames = carRacingGame.findWinner(cars.getCars());
-        GameUI.printWinners(winnerNames);
+        ResultView.printWinners(carRacingGame.findWinner());
     }
 }
