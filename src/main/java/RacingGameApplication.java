aadfd69eb1c24edccb35ef9e0667f13bbd0a29cb
이국신 <nw4611@naver.com>
2025-12-01 07:35:01 +0900

    [refactor] 클래스 이름 변경

--- src/main/java/RacingGameApplication.java
+++ src/main/java/RacingGameApplication.java
@@ -1,6 +1,6 @@
 import view.InputView;
 
-public class RacingGame {
+public class RacingGameApplication {
     public static void main(String[] args) {
         String carNames = InputView.getCarNames();
         int tryNumber = InputView.getTryNumber();
