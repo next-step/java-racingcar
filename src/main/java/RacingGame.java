974b96592b3ac363e9999d9324a040cd31283b86
이국신 <nw4611@naver.com>
2025-12-01 07:33:24 +0900

    [feat] 시도 횟수를 입력받는 view 구현

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -3,5 +3,6 @@ import view.InputView;
 public class RacingGame {
     public static void main(String[] args) {
         String carNames = InputView.getCarNames();
+        int tryNumber = InputView.getTryNumber();
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String TRY_NUMBER_IUNPUT_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static String getCarNames() {
         System.out.println(CAR_NAMES_INPUT_MESSAGE);
         return scanner.nextLine();
+    }
+
+    public static int getTryNumber() {
+        System.out.println(TRY_NUMBER_IUNPUT_MESSAGE);
+        return scanner.nextInt();
     }
 }
