98fe7e7d39fa8eced39749ff6838ac1a9c3211bd
JoonKyoLee <joonkyolee@gmail.com>
2025-12-08 01:41:39 +0900

    feat: 우승자 출력 기능 구현

--- src/main/java/racing/view/OutputView.java
+++ src/main/java/racing/view/OutputView.java
@@ -1,5 +1,8 @@
 package racing.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import racing.domain.CarName;
 import racing.domain.CarResult;
 import racing.domain.RaceResult;
 import racing.domain.RoundResult;
@@ -9,6 +12,8 @@ public class OutputView {
     private static final String RACE_RESULT_MESSAGE = "실행 결과";
     private static final String CAR_RESULT_FORMAT = "%s : %s";
     private static final String POSITION_MARK = "-";
+    private static final String WINNER_NAME_DELIMITER = ", ";
+    private static final String WINNER_RESULT_FORMAT = "가 최종 우승했습니다.";
 
     public static void printRaceResult(RaceResult raceResult) {
         System.out.println(RACE_RESULT_MESSAGE);
@@ -16,6 +21,7 @@ public class OutputView {
         for (RoundResult roundResult : raceResult.getRaceResult()) {
             printRoundResult(roundResult);
         }
+        printWinner(raceResult);
     }
 
     private static void printRoundResult(RoundResult roundResult) {
@@ -23,6 +29,22 @@ public class OutputView {
             System.out.println(carResultFormat(carResult));
         }
         printNewLine();
+    }
+
+    private static void printWinner(RaceResult raceResult) {
+        List<CarName> winners = raceResult.winners();
+        String winnerNames = joinWinnerNames(winners);
+
+        System.out.println(winnerNames + WINNER_RESULT_FORMAT);
+    }
+
+    private static String joinWinnerNames(List<CarName> winners) {
+        List<String> names = new ArrayList<>();
+
+        for (CarName carName : winners) {
+            names.add(carName.value());
+        }
+        return String.join(WINNER_NAME_DELIMITER, names);
     }
 
     private static void printNewLine() {
