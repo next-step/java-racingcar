2cb9cb108d9474d0a95062e38617b65136ae54cb
JoonKyoLee <joonkyolee@gmail.com>
2025-12-10 13:11:22 +0900

    feat: TryCount 기능 구현 - decrease() 호출 시 시도 회수가 1 감소된 TryCount 반환 - 시도 회수가 0일 경우 종료를 확인할 수 있는 isEnd() 메서드 추가

--- src/main/java/step5/racing/domain/TryCount.java
+++ src/main/java/step5/racing/domain/TryCount.java
@@ -1,10 +1,12 @@
 package step5.racing.domain;
 
+import java.util.Objects;
+
 public class TryCount {
 
     private static final String INVALID_TRY_COUNT_MESSAGE = "시도 회수는 양수여야 합니다.";
 
-    private int tryCount;
+    private final int tryCount;
 
     public TryCount(int tryCount) {
         validate(tryCount);
@@ -15,5 +17,30 @@ public class TryCount {
         if (tryCount < 0) {
             throw new IllegalArgumentException(INVALID_TRY_COUNT_MESSAGE);
         }
+    }
+
+    public TryCount decrease() {
+        return new TryCount(tryCount - 1);
+    }
+
+    public boolean isEnd() {
+        return this.tryCount == 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TryCount other = (TryCount) o;
+        return tryCount == other.tryCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tryCount);
     }
 }
