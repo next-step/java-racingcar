afb68f7373be9850c183348ffcf83182aecb147a
JoonKyoLee <joonkyolee@gmail.com>
2025-12-11 13:03:34 +0900

    feat: RaceResult에서 마지막 라운드에서 우승자를 결정해 반환하도록 구현

--- src/main/java/step5/racing/domain/RaceResult.java
+++ src/main/java/step5/racing/domain/RaceResult.java
@@ -18,4 +18,14 @@ public class RaceResult {
     public List<RoundResult> getRaceResult() {
         return this.raceResult;
     }
+
+    public List<CarName> winners() {
+        List<CarName> winners = new ArrayList<>();
+        RoundResult lastRound = raceResult.getLast();
+
+        for (CarResult result : lastRound.winners()) {
+            winners.add(result.name());
+        }
+        return winners;
+    }
 }
--- src/test/java/step5/racing/domain/RaceResultTest.java
+++ src/test/java/step5/racing/domain/RaceResultTest.java
@@ -37,4 +37,44 @@ public class RaceResultTest {
 
         assertThat(raceResult.getRaceResult()).hasSize(2);
     }
+
+    @Test
+    void 마지막_라운드에서_가장_멀리간_자동차가_우승한다() {
+        RaceResult raceResult = new RaceResult();
+
+        RoundResult round1 = new RoundResult(List.of(
+                new CarResult("자동차1", 1), new CarResult("자동차2", 1)
+        ));
+        RoundResult round2 = new RoundResult(List.of(
+                new CarResult("자동차1", 2), new CarResult("자동차2", 1)
+        ));
+
+        raceResult.addRoundResult(round1);
+        raceResult.addRoundResult(round2);
+
+        assertThat(raceResult.winners()).hasSize(1);
+        assertThat(raceResult.winners())
+                .extracting(carName -> carName)
+                .containsExactly(new CarName("자동차1"));
+    }
+
+    @Test
+    void 마지막_라운드에서_가장_멀리간_자동차가_여러_대라면_모두_우승한다() {
+        RaceResult raceResult = new RaceResult();
+
+        RoundResult round1 = new RoundResult(List.of(
+                new CarResult("자동차1", 1), new CarResult("자동차2", 0)
+        ));
+        RoundResult round2 = new RoundResult(List.of(
+                new CarResult("자동차1", 1), new CarResult("자동차2", 1)
+        ));
+
+        raceResult.addRoundResult(round1);
+        raceResult.addRoundResult(round2);
+
+        assertThat(raceResult.winners()).hasSize(2);
+        assertThat(raceResult.winners())
+                .extracting(carName -> carName)
+                .containsExactly(new CarName("자동차1"), new CarName("자동차2"));
+    }
 }
